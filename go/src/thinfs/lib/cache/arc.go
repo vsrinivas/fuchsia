2c803bfe13a4f388c292f48095c34e104d625b59
Chirantan Ekbote <chirantan@google.com>
2016-04-08 15:19:40 -0700

    Add package lib/cache
    Package cache provides an object cache that uses an adaptive replacement
    policy described by Megiddo & Modha in "Outperforming LRU with an
    Adaptive Replacement Cache Algorithm".
    
    Change-Id: I476528ce1dde671801f4916fad97e31bfb938e0e
    Signed-off-by: Chirantan Ekbote <chirantan@google.com>

---
+++ go/src/thinfs/lib/cache/arc.go
@@ -0,0 +1,250 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cache provides an object cache that uses an adaptive replacement policy
+// described by Mediddo & Modha in "Outperforming LRU with an Adaptive Replacement
+// Cache Algorithm".
+package cache
+
+import (
+	"container/list"
+)
+
+// Key represents a key to an object in the cache.
+type Key interface{}
+
+// Value represents a value associated with a key in the cache.
+type Value interface{}
+
+// sigh...
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// Entry represents a single entry in the cache.
+type Entry struct {
+	// The list.Element corresponding to this entry.
+	elem *list.Element
+
+	// The list.List to which this entry belongs.
+	list *list.List
+
+	// The key for this entry.
+	key Key
+
+	// The value associated with the key for this entry.
+	Value Value
+
+	// Indicates whether this entry has been modified.  Callers must set this field
+	// to true when they modify Value.  The cache will only commit this Entry to the BackingStore
+	// if IsDirty is true.
+	IsDirty bool
+}
+
+// BackingStore defines the interface that the cache expects from the storage medium for which
+// it is acting as a cache.
+type BackingStore interface {
+	// Get is called by the cache when a requested key is not found in the cache and needs
+	// to be read in from the BackingStore.
+	Get(Key) Value
+
+	// Put is called by the cache when a dirty entry is flushed and should be committed to
+	// the BackingStore.
+	Put(Key, Value)
+}
+
+// C represents an object cache.
+type C struct {
+	lru     *list.List // Least recently used.
+	lruhist *list.List // Least recently used history.
+	lfu     *list.List // Least frequently used.
+	lfuhist *list.List // Least frequently used history.
+
+	p       int            // The adaptive parameter.
+	size    int            // Size of the cache.
+	bs      BackingStore   // Backing store for the cache.
+	entries map[Key]*Entry // The actual cache entries.
+}
+
+// New returns a new, initialized Adaptive Replacement Cache.
+func New(size int, bs BackingStore) *C {
+	return &C{
+		lru:     list.New(),
+		lruhist: list.New(),
+		lfu:     list.New(),
+		lfuhist: list.New(),
+		p:       0,
+		size:    size,
+		entries: make(map[Key]*Entry),
+		bs:      bs,
+	}
+}
+
+// evict moves the last Entry from top to the front of bottom, clearing the value associated with the
+// Entry and committing it to the BackingStore, if necessary.
+func (c *C) evict(top, bottom *list.List) {
+	victim := top.Back().Value.(*Entry)
+
+	// Flush the entry.
+	if victim.IsDirty {
+		c.bs.Put(victim.key, victim.Value)
+		victim.IsDirty = false
+	}
+	victim.Value = nil
+
+	// Move onto bottom.
+	top.Remove(victim.elem)
+	victim.elem = bottom.PushFront(victim)
+	victim.list = bottom
+}
+
+// replace implements the REPLACE subroutine from the paper.
+func (c *C) replace(e *Entry) {
+	lrulen := c.lru.Len()
+	if lrulen >= 1 && ((e.list == c.lfuhist && lrulen == c.p) || lrulen > c.p) {
+		c.evict(c.lru, c.lruhist)
+	} else {
+		c.evict(c.lfu, c.lfuhist)
+	}
+}
+
+// remove deletes an Entry from the cache, committing its value to the BackingStore, if necessary.
+func (c *C) remove(e *Entry) {
+	if e.IsDirty {
+		c.bs.Put(e.key, e.Value)
+		e.IsDirty = false
+	}
+	e.Value = nil
+
+	e.list.Remove(e.elem)
+	e.list = nil
+	e.elem = nil
+	delete(c.entries, e.key)
+}
+
+// handleMiss implements Case IV from the paper.
+func (c *C) handleMiss(e *Entry) {
+	if l1len := c.lru.Len() + c.lruhist.Len(); c.size == l1len {
+		if c.lru.Len() < c.size {
+			c.remove(c.lruhist.Back().Value.(*Entry))
+			c.replace(e)
+		} else {
+			c.remove(c.lru.Back().Value.(*Entry))
+		}
+	} else if l2len := c.lfu.Len() + c.lfuhist.Len(); l1len < c.size && l1len+l2len >= c.size {
+		if l1len+l2len == 2*c.size {
+			c.remove(c.lfuhist.Back().Value.(*Entry))
+		}
+		c.replace(e)
+	}
+	e.elem = c.lru.PushFront(e)
+}
+
+// handleHit implements Case I from the paper.
+func (c *C) handleHit(e *Entry) {
+	e.list.Remove(e.elem)
+	e.elem = c.lfu.PushFront(e)
+	e.list = c.lfu
+}
+
+// handleFakeHit implements Case II and Case III from the paper.
+func (c *C) handleFakeHit(e *Entry) {
+	// Adapt p.
+	if e.list == c.lruhist {
+		c.p = min(c.size, c.p+max(c.lfuhist.Len()/c.lruhist.Len(), 1))
+	} else {
+		c.p = max(0, c.p-max(c.lruhist.Len()/c.lfuhist.Len(), 1))
+	}
+	c.replace(e)
+
+	c.handleHit(e)
+}
+
+// Get returns the Entry containing the Value for the Key k, fetching it from the BackingStore
+// if necessary.  Callers _must_ set the IsDirty field for the returned Entry if they change the
+// Value in the Entry to ensure that the change is propagated to the BackingStore.  Callers must
+// also not retain the returned Entry.
+func (c *C) Get(k Key) *Entry {
+	e, ok := c.entries[k]
+	if !ok {
+		e = &Entry{
+			list:    c.lru,
+			key:     k,
+			Value:   c.bs.Get(k),
+			IsDirty: false,
+		}
+		c.entries[k] = e
+
+		c.handleMiss(e)
+	} else if e.list == c.lruhist || e.list == c.lfuhist {
+		e.Value = c.bs.Get(k)
+
+		c.handleFakeHit(e)
+	} else {
+		c.handleHit(e)
+	}
+
+	return e
+}
+
+// Put associates the Value v with the Key k and stores it in the cache.  It additionally marks the
+// Entry dirty so that the new Value will be propagated to the BackingStore.  Callers may wish to
+// use Put when they want to completely replace the Value associated with some Key and want to avoid
+// a potentially expensive lookup for the Key in the BackingStore.
+func (c *C) Put(k Key, v Value) {
+	e, ok := c.entries[k]
+	if !ok {
+		e = &Entry{
+			list:    c.lru,
+			key:     k,
+			Value:   v,
+			IsDirty: true,
+		}
+		c.entries[k] = e
+
+		c.handleMiss(e)
+	} else if e.list == c.lruhist || e.list == c.lfuhist {
+		e.Value = v
+		e.IsDirty = true
+
+		c.handleFakeHit(e)
+	} else {
+		e.Value = v
+		e.IsDirty = true
+
+		c.handleHit(e)
+	}
+}
+
+// Flush commits all dirty Entries in the cache to the BackingStore.
+func (c *C) Flush() {
+	for _, e := range c.entries {
+		if !e.IsDirty {
+			continue
+		}
+
+		c.bs.Put(e.key, e.Value)
+		e.IsDirty = false
+	}
+}
---
+++ go/src/thinfs/lib/cache/arc_test.go
@@ -0,0 +1,204 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cache
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+const (
+	// Arbitrarily chosen.
+	defaultSize   = 571
+	numIterations = 385494
+)
+
+type store struct {
+	get func(Key) Value
+	put func(Key, Value)
+}
+
+func (s store) Get(k Key) Value {
+	return s.get(k)
+}
+
+func (s store) Put(k Key, v Value) {
+	s.put(k, v)
+}
+
+func setUp(t *testing.T) *rand.Rand {
+	seed := time.Now().UTC().UnixNano()
+	t.Log("Seed is ", seed)
+	r := rand.New(rand.NewSource(seed))
+
+	return r
+}
+
+func TestGet(t *testing.T) {
+	r := setUp(t)
+	s := store{
+		get: func(k Key) Value {
+			return Value(k)
+		},
+	}
+
+	arc := New(defaultSize, s)
+	k := r.Int63()
+	if v := arc.Get(k).Value.(int64); v != k {
+		t.Errorf("arc.Get(%v) = %v; want %v\n", k, v, k)
+	}
+}
+
+func TestPut(t *testing.T) {
+	r := setUp(t)
+	key, val := r.Int(), r.Int()
+	var called bool
+
+	s := store{
+		put: func(k Key, v Value) {
+			called = true
+			if k != key {
+				t.Errorf("put: k = %v; want %v\n", k, key)
+			}
+			if v != val {
+				t.Errorf("put: v = %v; want %v\n", v, val)
+			}
+		},
+	}
+
+	arc := New(defaultSize, s)
+	arc.Put(key, val)
+	arc.Flush() // Will call s.put
+
+	if !called {
+		t.Error("arc.Flush() did not call Put on the BackingStore")
+	}
+}
+
+func TestModify(t *testing.T) {
+	r := setUp(t)
+	var called bool
+
+	s := store{
+		get: func(k Key) Value {
+			return Value(k)
+		},
+		put: func(k Key, v Value) {
+			called = true
+			if v.(int64) != ^k.(int64) {
+				t.Errorf("put: Value is %v for Key %v; want %v\n", v, k, ^k.(int64))
+			}
+		},
+	}
+
+	arc := New(defaultSize, s)
+	k := r.Int63()
+	entry := arc.Get(k)
+	entry.Value = ^k
+	entry.IsDirty = true
+
+	arc.Flush()
+
+	if !called {
+		t.Error("arc.Flush() did not call Put on the BackingStore")
+	}
+}
+
+type testEntry struct {
+	isDirty bool
+	value   Value
+}
+
+type op int
+
+const (
+	get op = iota
+	put
+	modify
+)
+
+func getOneElem(m map[Key]*testEntry) Key {
+	for k := range m {
+		return k
+	}
+
+	return nil
+}
+
+func TestFuzz(t *testing.T) {
+	r := setUp(t)
+
+	checker := make(map[Key]*testEntry)
+	s := store{
+		get: func(k Key) Value {
+			checker[k] = &testEntry{
+				isDirty: false,
+				value:   ^k.(int),
+			}
+			return ^k.(int)
+		},
+		put: func(k Key, v Value) {
+			val, ok := checker[k]
+			if !ok {
+				t.Errorf("Putting key %v in the backing store when it shouldn't exist in the cache\n", k)
+			}
+			if !val.isDirty {
+				t.Errorf("Attempting to commit non-dirty entry for key %v to the backing store\n", k)
+			}
+			if val.value != v {
+				t.Errorf("Putting value v = %v for key %v; want %v\n", v, k, val.value)
+			}
+			delete(checker, k)
+		},
+	}
+
+	arc := New(defaultSize, s)
+
+	for i := 0; i < numIterations; i++ {
+		operation := op(r.Intn(3))
+		var k Key
+		if len(checker) > 0 && r.Int()&0x1 == 0 {
+			// Use an existing key.
+			k = getOneElem(checker)
+		} else {
+			k = Key(r.Int())
+		}
+
+		switch operation {
+		case get:
+			entry := arc.Get(k)
+			if entry.Value != checker[k].value {
+				t.Errorf("arc.Get(%v) = %v; want %v\n", k, entry.Value, checker[k].value)
+			}
+		case put:
+			arc.Put(k, Value(k))
+			checker[k] = &testEntry{
+				isDirty: true,
+				value:   Value(k),
+			}
+		case modify:
+			entry := arc.Get(k)
+			entry.IsDirty = true
+			entry.Value = -k.(int)
+			checker[k] = &testEntry{
+				isDirty: true,
+				value:   -k.(int),
+			}
+		}
+	}
+
+	arc.Flush()
+}
