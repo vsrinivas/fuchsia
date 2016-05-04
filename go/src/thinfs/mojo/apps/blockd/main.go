fa292e27e4b2815c9d73b3a3f4c31cd0f80f3b3e
Chirantan Ekbote <chirantan@google.com>
2016-05-04 14:55:21 -0700

    Add a block device mojo application
    Add a simple mojo application for providing the block device service
    through an existing file.
    
    Change-Id: Ica883af990d3c2adfebf8f7ab602d7d9d9c5016a
    Signed-off-by: Chirantan Ekbote <chirantan@google.com>

---
+++ go/src/thinfs/mojo/apps/blockd/main.go
@@ -0,0 +1,125 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+//#include "mojo/public/c/system/handle.h"
+//#include "mojo/public/c/system/result.h"
+import "C"
+
+import (
+	"flag"
+	"os"
+
+	"interfaces/block"
+	"mojo/public/go/application"
+	"mojo/public/go/bindings"
+	"mojo/public/go/system"
+
+	"fuchsia.googlesource.com/thinfs/lib/block/file"
+	blk "fuchsia.googlesource.com/thinfs/mojo/services/block"
+	"github.com/golang/glog"
+)
+
+const defaultBlockSize = 1024
+
+var (
+	path     = flag.String("path", "", "path to backing file")
+	readOnly = flag.Bool("readonly", false, "open the file read-only")
+)
+
+type delegate struct {
+	dev  *blk.Device
+	stub *bindings.Stub
+	conn *application.Connection
+}
+
+func (d *delegate) Initialize(ctx application.Context) {
+	flag.CommandLine.Parse(ctx.Args()[1:])
+	if *path == "" {
+		glog.Fatal("-path is required")
+	}
+
+	opts := os.O_RDWR
+	caps := block.Capabilities_ReadWrite
+	if *readOnly {
+		opts = os.O_RDONLY
+		caps = block.Capabilities_ReadOnly
+	}
+
+	f, err := os.OpenFile(*path, opts, 0666)
+	if err != nil {
+		glog.Fatal("unable to open file: ", err)
+	}
+
+	fdev, err := file.New(f, defaultBlockSize)
+	if err != nil {
+		glog.Fatal("unable to create file device: ", err)
+	}
+
+	d.dev = blk.New(fdev, block.Capabilities(caps))
+}
+
+func (d *delegate) Create(req block.Device_Request) {
+	if d.stub != nil {
+		glog.Fatal("Attempting to create multiple stubs")
+	}
+
+	d.stub = block.NewDeviceStub(req, d.dev, bindings.GetAsyncWaiter())
+	go func() {
+		// The generated bindings currently close the connection as soon as an error
+		// occurs.  The error is returned from ServeRequest so we only need to serve
+		// requests until a non-nil error is returned.  Until the bindings have been
+		// changed to do something more sane, we can keep this loop simple.
+		var err error
+		for err == nil {
+			err = d.stub.ServeRequest()
+		}
+
+		connErr, ok := err.(*bindings.ConnectionError)
+		if !ok || !connErr.Closed() {
+			// Log any error that's not a connection closed error.
+			glog.Error(err)
+		}
+		if err := d.dev.Close(); err != nil {
+			glog.Error(err)
+		}
+	}()
+}
+
+func (d *delegate) AcceptConnection(conn *application.Connection) {
+	// Only allow the first connection.
+	if d.conn != nil {
+		conn.Close()
+		return
+	}
+
+	d.conn = conn
+	conn.ProvideServices(&block.Device_ServiceFactory{d})
+}
+
+func (d *delegate) Quit() {
+	if d.stub != nil {
+		d.stub.Close()
+	}
+}
+
+// MojoMain is the main entry point into the application.
+//export MojoMain
+func MojoMain(handle C.MojoHandle) C.MojoResult {
+	application.Run(&delegate{}, system.MojoHandle(handle))
+	return C.MOJO_RESULT_OK
+}
+
+func main() {}
