abf22352e07d535e0247ed3f0e6f5e96661ad955
Carlos Pizano <cpu@google.com>
2016-06-20 15:09:01 -0700

    [kernel] Add scoped spinlock
    Uses the same code style as auto_lock.h
    
    Change-Id: I70033687d008d517bf5e781d45d01123e8972a38

---
+++ kernel/include/kernel/auto_spinlock.h
@@ -0,0 +1,32 @@
+// Copyright 2016 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <kernel/spinlock.h>
+
+template <unsigned int flags = ARCH_DEFAULT_SPIN_LOCK_FLAG_INTERRUPTS>
+class AutoSpinLock {
+public:
+    inline explicit AutoSpinLock(spin_lock_t* lock) : lock_(lock) {
+        arch_interrupt_save(&state_, flags);
+        spin_lock(lock_);
+    }
+
+    inline ~AutoSpinLock() {
+        spin_unlock(lock_);
+        arch_interrupt_restore(state_, flags);
+    }
+
+    AutoSpinLock(const AutoSpinLock&) = delete;
+    AutoSpinLock& operator=(const AutoSpinLock&) = delete;
+    AutoSpinLock(AutoSpinLock&&) = delete;
+    AutoSpinLock& operator=(AutoSpinLock&&) = delete;
+
+private:
+    spin_lock_t* lock_;
+    spin_lock_saved_state_t state_;
+};
