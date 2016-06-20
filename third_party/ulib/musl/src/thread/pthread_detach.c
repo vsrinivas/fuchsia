68e39577764badf0d2032cb3412684071c74d95f
George Kulakowski <kulakowski@google.com>
2016-06-20 10:45:01 -0700

    [musl] Remove references to __pthread_impl
    Change-Id: I69c3b07b636be4772b0a9d50baa6e638b44b16f5

--- third_party/ulib/musl/src/thread/pthread_detach.c
+++ third_party/ulib/musl/src/thread/pthread_detach.c
@@ -1,12 +1,10 @@
 #include "pthread_impl.h"
 #include <threads.h>
 
-int __pthread_join(pthread_t, void**);
-
 static int __pthread_detach(pthread_t t) {
     /* Cannot detach a thread that's already exiting */
     if (!mxr_mutex_trylock(&t->exitlock))
-        return __pthread_join(t, 0);
+        return pthread_join(t, 0);
     t->detached = 2;
     mxr_mutex_unlock(&t->exitlock);
     return 0;
--- third_party/ulib/musl/src/thread/thrd_join.c
+++ third_party/ulib/musl/src/thread/thrd_join.c
@@ -1,11 +1,10 @@
+#include <pthread.h>
 #include <stdint.h>
 #include <threads.h>
 
-int __pthread_join(thrd_t, void**);
-
 int thrd_join(thrd_t t, int* res) {
     void* pthread_res;
-    __pthread_join(t, &pthread_res);
+    pthread_join(t, &pthread_res);
     if (res)
         *res = (int)(intptr_t)pthread_res;
     return thrd_success;
