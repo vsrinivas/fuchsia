f3f59167ab0b20bd3449d7e8afb429bcbb49d879
Travis Geiselbrecht <travisg@google.com>
2016-06-22 12:20:43 -0700

    [musl] update the PRI* macros to match the underlying integer types for *int*_t
    Change-Id: Ic373c4de514ef5fda20e7b196f75a3d7379443a9

--- third_party/ulib/musl/include/inttypes.h
+++ third_party/ulib/musl/include/inttypes.h
@@ -22,13 +22,8 @@ uintmax_t strtoumax(const char* __restrict, char** __restrict, int);
 intmax_t wcstoimax(const wchar_t* __restrict, wchar_t** __restrict, int);
 uintmax_t wcstoumax(const wchar_t* __restrict, wchar_t** __restrict, int);
 
-#if UINTPTR_MAX == UINT64_MAX
-#define __PRI64 "l"
-#define __PRIPTR "l"
-#else
 #define __PRI64 "ll"
-#define __PRIPTR ""
-#endif
+#define __PRIPTR "l"
 
 #define PRId8 "d"
 #define PRId16 "d"
