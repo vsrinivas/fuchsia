8d9ac061ab05c866fc20d02432efde8bcf82e66f
Ankur Mittal <anmittal@google.com>
2016-06-23 13:00:14 -0700

    [utest] Porting reply-handle-test to use test framework
    Change-Id: I91f6eb7b5cc2786bc6700255265022dc7cfd437e

--- system/utest/reply-handle/reply-handle.c
+++ system/utest/reply-handle/reply-handle.c
@@ -17,81 +17,84 @@
 #include <stdlib.h>
 
 #include <magenta/syscalls.h>
+#include <mxu/unittest.h>
 
 #include <mxio/io.h>
 #include <mxio/util.h>
 
-int main(int argc, char** argv) {
+int reply_handle_test(void) {
+    BEGIN_TEST;
+
     mx_handle_t p1a, p1b, p2a, p2b;
     uintptr_t entry;
     mx_handle_t p;
     mx_status_t r;
     int fd;
+    char msg[128];
 
-    if ((p1a = _magenta_message_pipe_create(&p1b)) < 0) {
-        printf("failed to create pipe1 %d\n", p1a);
-        return -1;
-    }
-    if ((p2a = _magenta_message_pipe_create(&p2b)) < 0) {
-        printf("failed to create pipe2 %d\n", p2a);
-        return -1;
-    }
+    p1a = _magenta_message_pipe_create(&p1b);
+    snprintf(msg, sizeof(msg), "failed to create pipe1 %d\n", p1a);
+    ASSERT_GE(p1a, 0, msg);
+
+    p2a = _magenta_message_pipe_create(&p2b);
+    snprintf(msg, sizeof(msg), "failed to create pipe2 %d\n", p2a);
+    ASSERT_GE(p2a, 0, msg);
 
     // send a message and p2b through p1a
-    if ((r = _magenta_message_write(p1a, "hello", 6, &p2b, 1, 0)) < 0) {
-        printf("failed to write message+handle to p1a %d\n", r);
-    }
+    r = _magenta_message_write(p1a, "hello", 6, &p2b, 1, 0);
+    snprintf(msg, sizeof(msg), "failed to write message+handle to p1a %d\n", r);
+    EXPECT_GE(r, 0, msg);
 
     // create helper process and pass p1b across to it
-    if ((p = _magenta_process_create("helper", 7)) < 0) {
-        printf("couldn't create process %d\n", p);
-        return -1;
-    }
-    if ((fd = open("/boot/bin/reply-handle-helper", O_RDONLY)) < 0) {
-        printf("couldn't open reply-handle-helper %d\n", fd);
-        return -1;
-    }
-    if ((r = mxio_load_elf_fd(p, &entry, fd)) < 0) {
-        printf("couldn't load reply-handle-helper %d\n", r);
-        return -1;
-    }
-    if ((r = _magenta_process_start(p, p1b, entry)) < 0) {
-        printf("process did not start %d\n", r);
-        return -1;
-    }
+    p = _magenta_process_create("helper", 7);
+    snprintf(msg, sizeof(msg), "couldn't create process %d\n", p);
+    ASSERT_GE(p, 0, msg);
+
+    fd = open("/boot/bin/reply-handle-helper", O_RDONLY);
+    snprintf(msg, sizeof(msg), "couldn't open reply-handle-helper %d\n", fd);
+    ASSERT_GE(fd, 0, msg);
+
+    r = mxio_load_elf_fd(p, &entry, fd);
+    snprintf(msg, sizeof(msg), "couldn't load reply-handle-helper %d\n", r);
+    ASSERT_GE(r, 0, msg);
+
+    r = _magenta_process_start(p, p1b, entry);
+    snprintf(msg, sizeof(msg), "process did not start %d\n", r);
+    ASSERT_GE(r, 0, msg);
 
     mx_signals_t pending;
-    if ((r = _magenta_handle_wait_one(p2a, MX_SIGNAL_READABLE | MX_SIGNAL_PEER_CLOSED,
-                                      MX_TIME_INFINITE, &pending, NULL)) < 0) {
-        printf("error waiting on p2a %d\n", r);
-        return -1;
-    }
-    if (!(pending & MX_SIGNAL_READABLE)) {
-        printf("pipe 2a not readable\n");
-        return -1;
-    }
+    r = _magenta_handle_wait_one(p2a, MX_SIGNAL_READABLE | MX_SIGNAL_PEER_CLOSED,
+                                 MX_TIME_INFINITE, &pending, NULL);
+    snprintf(msg, sizeof(msg), "error waiting on p2a %d\n", r);
+    ASSERT_GE(r, 0, msg);
 
-    printf("write handle %x to helper...\n", p2b);
+    ASSERT_TRUE(pending & MX_SIGNAL_READABLE, "pipe 2a not readable");
+
+    unittest_printf("write handle %x to helper...\n", p2b);
     char data[128];
     mx_handle_t h;
     uint32_t dsz = sizeof(data) - 1;
     uint32_t hsz = 1;
-    if ((r = _magenta_message_read(p2a, data, &dsz, &h, &hsz, 0)) < 0) {
-        printf("failed to read reply %d\n", r);
-        return -1;
-    }
-    data[dsz] = 0;
-    printf("reply: '%s' %u %u\n", data, dsz, hsz);
-    if (hsz != 1) {
-        printf("no handle returned\n");
-        return -1;
-    }
-    printf("read handle %x from reply port\n", h);
-    if (h != p2b) {
-        printf("different handle returned\n");
-        return -1;
-    }
+    r = _magenta_message_read(p2a, data, &dsz, &h, &hsz, 0);
+    snprintf(msg, sizeof(msg), "failed to read reply %d\n", r);
+    ASSERT_GE(r, 0, msg);
 
-    printf("success\n");
-    return 0;
+    data[dsz] = 0;
+    unittest_printf("reply: '%s' %u %u\n", data, dsz, hsz);
+    ASSERT_EQ(hsz, 1u, "no handle returned");
+
+    unittest_printf("read handle %x from reply port\n", h);
+    ASSERT_EQ(h, p2b, "different handle returned");
+
+    END_TEST;
+}
+
+BEGIN_TEST_CASE(reply_handle_tests)
+RUN_TEST(reply_handle_test)
+END_TEST_CASE(reply_handle_tests)
+
+int main(void) {
+    // TODO: remove this register once global constructors work
+    unittest_register_test_case(&_reply_handle_tests_element);
+    return unittest_run_all_tests() ? 0 : -1;
 }
