8b822ebb33f369b2b113a76df27921330069a803
Travis Geiselbrecht <travisg@google.com>
2016-06-27 13:26:26 -0700

    [make] remove extraneous line in MAKECONFIGHEADER macro
    Change-Id: Iea9d9fdff1ac1330f73760a38784df41baa8ac91

--- kernel/make/macros.mk
+++ kernel/make/macros.mk
@@ -41,7 +41,6 @@ endef
 define MAKECONFIGHEADER
 	$(MKDIR); \
 	rm -f $1.tmp; \
-	LDEF=`echo $1 | tr '/\\.-' '_' | sed "s/C++/CPP/g;s/c++/cpp/g"`; \
 	echo \#pragma once > $1.tmp; \
 	for d in `echo $($2) | tr '[:lower:]' '[:upper:]'`; do \
 		echo "#define $$d" | sed "s/=/\ /g;s/-/_/g;s/\//_/g;s/\./_/g;s/\//_/g;s/C++/CPP/g" >> $1.tmp; \
