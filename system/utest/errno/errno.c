1e52b389b0ae332a5090897c1ce81d53812442e1
Ankur Mittal <anmittal@google.com>
2016-06-28 11:13:54 -0700

    [utest] Port errno test to use test framework
    Change-Id: I3955ef0fff9eefc187bf0f4a4c13568cf4f1fe87

--- system/utest/errno/errno.c
+++ system/utest/errno/errno.c
@@ -15,37 +15,49 @@
 #include <assert.h>
 #include <errno.h>
 #include <magenta/syscalls.h>
+#include <mxu/unittest.h>
 #include <pthread.h>
 #include <stdio.h>
 
 static void* do_test(void* arg) {
     int thread_no = *(int*)arg;
-    printf("do_test for thread: %d\n", thread_no);
+    unittest_printf("do_test for thread: %d\n", thread_no);
     errno = -thread_no;
     _magenta_nanosleep(300 * 1000 * 1000);
-    printf("comparing result for: %d\n", thread_no);
-    assert(errno == -thread_no);
+    unittest_printf("comparing result for: %d\n", thread_no);
+    EXPECT_EQ(errno, -thread_no, "Incorrect errno for this thread");
     return NULL;
 }
 
-int main(void) {
+bool errno_test(void) {
+    BEGIN_TEST;
     int main_thread = 1, thread_1 = 2, thread_2 = 3;
 
     pthread_t thread2, thread3;
 
-    printf("creating thread: %d\n", thread_1);
+    unittest_printf("creating thread: %d\n", thread_1);
     pthread_create(&thread2, NULL, do_test, &thread_1);
 
-    printf("creating thread: %d\n", thread_2);
+    unittest_printf("creating thread: %d\n", thread_2);
     pthread_create(&thread3, NULL, do_test, &thread_2);
 
     do_test(&main_thread);
 
-    printf("joining thread: %d\n", thread_1);
+    unittest_printf("joining thread: %d\n", thread_1);
     pthread_join(thread2, NULL);
 
-    printf("joining thread: %d\n", thread_2);
+    unittest_printf("joining thread: %d\n", thread_2);
     pthread_join(thread3, NULL);
 
-    return 0;
+    END_TEST;
+}
+
+BEGIN_TEST_CASE(errno_tests)
+RUN_TEST(errno_test)
+END_TEST_CASE(errno_tests)
+
+int main(void) {
+    // TODO: remove this register once global constructors work
+    unittest_register_test_case(&_errno_tests_element);
+    return unittest_run_all_tests() ? 0 : -1;
 }
