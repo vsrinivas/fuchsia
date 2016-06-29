239473a6541248aeb2de3605a634ed1edb5563d8
Ankur Mittal <anmittal@google.com>
2016-06-29 13:08:09 -0700

    [utest] Port threads test to use test framework
    Also put asserts to test the return values
    
    Change-Id: I5012a16cb7e79d1bf8391d894845ea05da3a9a27

--- system/utest/threads/threads.c
+++ system/utest/threads/threads.c
@@ -17,45 +17,58 @@
 #include <time.h>
 
 #include <magenta/syscalls.h>
+#include <mxu/unittest.h>
 
 static int thread_1(void* arg) {
-    printf("thread 1 sleeping for .1 seconds\n");
+    unittest_printf("thread 1 sleeping for .1 seconds\n");
     struct timespec t = (struct timespec){
         .tv_sec = 0,
         .tv_nsec = 100 * 1000 * 1000,
     };
     nanosleep(&t, NULL);
 
-    printf("thread 1 calling _magenta_thread_exit()\n");
+    unittest_printf("thread 1 calling _magenta_thread_exit()\n");
     _magenta_thread_exit();
     return 0;
 }
 
-int main(void) {
+bool threads_test(void) {
+    BEGIN_TEST;
     mx_handle_t handle;
-    printf("Welcome to thread test!\n");
+    unittest_printf("Welcome to thread test!\n");
 
     for (int i = 0; i != 4; ++i) {
         handle = _magenta_thread_create(thread_1, NULL, "thread 1", 9);
-        printf("thread:%d created handle %d\n", i, handle);
+        ASSERT_GT(handle, 0, "Error while creating thread");
+        unittest_printf("thread:%d created handle %d\n", i, handle);
 
         _magenta_handle_wait_one(handle, MX_SIGNAL_SIGNALED, MX_TIME_INFINITE, NULL, NULL);
-        printf("thread:%d joined\n", i);
+        unittest_printf("thread:%d joined\n", i);
 
         _magenta_handle_close(handle);
     }
 
-    printf("Attempting to create thread with a super long name. This should fail\n");
+    unittest_printf("Attempting to create thread with a super long name. This should fail\n");
     handle = _magenta_thread_create(thread_1, NULL,
                                     "01234567890123456789012345678901234567890123456789012345678901234567890123456789", 81);
-    printf("_magenta_thread_create returned %u\n", handle);
+    ASSERT_LT(handle, 0, "Thread creation should have failed");
+    unittest_printf("_magenta_thread_create returned %u\n", handle);
 
-    printf("Attempting to create thread with a null. This should succeed\n");
+    unittest_printf("Attempting to create thread with a null. This should fail\n");
     handle = _magenta_thread_create(thread_1, NULL, NULL, 0);
-    printf("_magenta_thread_create returned %u\n", handle);
+    ASSERT_LT(handle, 0, "Error while creating thread");
+    unittest_printf("_magenta_thread_create returned %u\n", handle);
     _magenta_handle_close(handle);
 
-    printf("thread test done\n");
+    END_TEST;
+}
 
-    return 0;
+BEGIN_TEST_CASE(threads_tests)
+RUN_TEST(threads_test)
+END_TEST_CASE(threads_tests)
+
+int main(void) {
+    // TODO: remove this register once global constructors work
+    unittest_register_test_case(&_threads_tests_element);
+    return unittest_run_all_tests() ? 0 : -1;
 }
