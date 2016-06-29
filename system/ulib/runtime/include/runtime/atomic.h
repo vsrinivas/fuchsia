d80c4bd6708b88b2ada0aa969ad9dc10041a1740
George Kulakowski <kulakowski@google.com>
2016-06-28 17:29:37 -0700

    [runtime][atomics] Don't use pointer to volatile in atomic operations
    The modern builtin calls with memory models (the __atomic_* ones in gcc
    and clang, and the __c11_* ones in clang) do not require a pointer to
    volatile of the type to be atomically modified.
    
    Change-Id: I463178640f32cf77e5a4498cdfa0af1e94b07abf

--- system/ulib/runtime/include/runtime/atomic.h
+++ system/ulib/runtime/include/runtime/atomic.h
@@ -24,65 +24,65 @@ __BEGIN_CDECLS
  * barriers. Defined for with a short name for int, and with longer
  * names for sized integers.
  */
-#define __MAKE_ATOMICS(TYPE, TYPE_NAME)                                                                  \
-    static inline TYPE atomic_swap##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                            \
-        return __atomic_exchange_n(ptr, val, __ATOMIC_SEQ_CST);                                          \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_add##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                             \
-        return __atomic_fetch_add(ptr, val, __ATOMIC_SEQ_CST);                                           \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_and##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                             \
-        return __atomic_fetch_and(ptr, val, __ATOMIC_SEQ_CST);                                           \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_or##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                              \
-        return __atomic_fetch_or(ptr, val, __ATOMIC_SEQ_CST);                                            \
-    }                                                                                                    \
-                                                                                                         \
-    static inline bool atomic_cmpxchg##TYPE_NAME(volatile TYPE* ptr, TYPE* oldval, TYPE newval) {        \
-        return __atomic_compare_exchange_n(ptr, oldval, newval, 0,                                       \
-                                           __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);                          \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_load##TYPE_NAME(volatile TYPE* ptr) {                                      \
-        return __atomic_load_n(ptr, __ATOMIC_SEQ_CST);                                                   \
-    }                                                                                                    \
-                                                                                                         \
-    static inline void atomic_store##TYPE_NAME(volatile TYPE* ptr, TYPE newval) {                        \
-        __atomic_store_n(ptr, newval, __ATOMIC_SEQ_CST);                                                 \
-    }                                                                                                    \
-                                                                                                         \
-    /* relaxed versions of the above */                                                                  \
-    static inline TYPE atomic_swap_relaxed##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                    \
-        return __atomic_exchange_n(ptr, val, __ATOMIC_RELAXED);                                          \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_add_relaxed##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                     \
-        return __atomic_fetch_add(ptr, val, __ATOMIC_RELAXED);                                           \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_and_relaxed##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                     \
-        return __atomic_fetch_and(ptr, val, __ATOMIC_RELAXED);                                           \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_or_relaxed##TYPE_NAME(volatile TYPE* ptr, TYPE val) {                      \
-        return __atomic_fetch_or(ptr, val, __ATOMIC_RELAXED);                                            \
-    }                                                                                                    \
-                                                                                                         \
-    static inline TYPE atomic_cmpxchg_relaxed##TYPE_NAME(volatile TYPE* ptr, TYPE oldval, TYPE newval) { \
-        (void)__atomic_compare_exchange_n(ptr, &oldval, newval, 0,                                       \
-                                          __ATOMIC_RELAXED, __ATOMIC_RELAXED);                           \
-        return oldval;                                                                                   \
-    }                                                                                                    \
-                                                                                                         \
-    static TYPE atomic_load_relaxed##TYPE_NAME(volatile TYPE* ptr) {                                     \
-        return __atomic_load_n(ptr, __ATOMIC_RELAXED);                                                   \
-    }                                                                                                    \
-                                                                                                         \
-    static void atomic_store_relaxed##TYPE_NAME(volatile TYPE* ptr, TYPE newval) {                       \
-        __atomic_store_n(ptr, newval, __ATOMIC_RELAXED);                                                 \
+#define __MAKE_ATOMICS(TYPE, TYPE_NAME)                                                         \
+    static inline TYPE atomic_swap##TYPE_NAME(TYPE* ptr, TYPE val) {                            \
+        return __atomic_exchange_n(ptr, val, __ATOMIC_SEQ_CST);                                 \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_add##TYPE_NAME(TYPE* ptr, TYPE val) {                             \
+        return __atomic_fetch_add(ptr, val, __ATOMIC_SEQ_CST);                                  \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_and##TYPE_NAME(TYPE* ptr, TYPE val) {                             \
+        return __atomic_fetch_and(ptr, val, __ATOMIC_SEQ_CST);                                  \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_or##TYPE_NAME(TYPE* ptr, TYPE val) {                              \
+        return __atomic_fetch_or(ptr, val, __ATOMIC_SEQ_CST);                                   \
+    }                                                                                           \
+                                                                                                \
+    static inline bool atomic_cmpxchg##TYPE_NAME(TYPE* ptr, TYPE* oldval, TYPE newval) {        \
+        return __atomic_compare_exchange_n(ptr, oldval, newval, 0,                              \
+                                           __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);                 \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_load##TYPE_NAME(TYPE* ptr) {                                      \
+        return __atomic_load_n(ptr, __ATOMIC_SEQ_CST);                                          \
+    }                                                                                           \
+                                                                                                \
+    static inline void atomic_store##TYPE_NAME(TYPE* ptr, TYPE newval) {                        \
+        __atomic_store_n(ptr, newval, __ATOMIC_SEQ_CST);                                        \
+    }                                                                                           \
+                                                                                                \
+    /* relaxed versions of the above */                                                         \
+    static inline TYPE atomic_swap_relaxed##TYPE_NAME(TYPE* ptr, TYPE val) {                    \
+        return __atomic_exchange_n(ptr, val, __ATOMIC_RELAXED);                                 \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_add_relaxed##TYPE_NAME(TYPE* ptr, TYPE val) {                     \
+        return __atomic_fetch_add(ptr, val, __ATOMIC_RELAXED);                                  \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_and_relaxed##TYPE_NAME(TYPE* ptr, TYPE val) {                     \
+        return __atomic_fetch_and(ptr, val, __ATOMIC_RELAXED);                                  \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_or_relaxed##TYPE_NAME(TYPE* ptr, TYPE val) {                      \
+        return __atomic_fetch_or(ptr, val, __ATOMIC_RELAXED);                                   \
+    }                                                                                           \
+                                                                                                \
+    static inline TYPE atomic_cmpxchg_relaxed##TYPE_NAME(TYPE* ptr, TYPE oldval, TYPE newval) { \
+        (void)__atomic_compare_exchange_n(ptr, &oldval, newval, 0,                              \
+                                          __ATOMIC_RELAXED, __ATOMIC_RELAXED);                  \
+        return oldval;                                                                          \
+    }                                                                                           \
+                                                                                                \
+    static TYPE atomic_load_relaxed##TYPE_NAME(TYPE* ptr) {                                     \
+        return __atomic_load_n(ptr, __ATOMIC_RELAXED);                                          \
+    }                                                                                           \
+                                                                                                \
+    static void atomic_store_relaxed##TYPE_NAME(TYPE* ptr, TYPE newval) {                       \
+        __atomic_store_n(ptr, newval, __ATOMIC_RELAXED);                                        \
     }
 
 __MAKE_ATOMICS(uint8_t, _uint8)
