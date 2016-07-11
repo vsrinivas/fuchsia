81ce54058cee804fff9dfd2b01ec4a778c52f1eb
George Kulakowski <kulakowski@google.com>
2016-07-11 14:45:35 -0700

    [musl] Move C11 thread create and exit to the real pthread implementations
    Change-Id: I0b8a81d7a15ea5655132e32d09fa8e9194dcb2fb

--- third_party/ulib/musl/src/thread/thrd_create.c
+++ third_party/ulib/musl/src/thread/thrd_create.c
@@ -1,11 +1,9 @@
-#include "pthread_impl.h"
 #include <threads.h>
 
-int __pthread_create(pthread_t* restrict, const pthread_attr_t* restrict, void* (*)(void*),
-                     void* restrict);
+#include "pthread_impl.h"
 
 int thrd_create(thrd_t* thr, thrd_start_t func, void* arg) {
-    int ret = __pthread_create(thr, __ATTRP_C11_THREAD, (void* (*)(void*))func, arg);
+    int ret = pthread_create(thr, __ATTRP_C11_THREAD, (void* (*)(void*))func, arg);
     switch (ret) {
     case 0:
         return thrd_success;
--- third_party/ulib/musl/src/thread/thrd_exit.c
+++ third_party/ulib/musl/src/thread/thrd_exit.c
@@ -1,8 +1,7 @@
-#include "pthread_impl.h"
 #include <threads.h>
 
-_Noreturn void __pthread_exit(void*);
+#include "pthread_impl.h"
 
 _Noreturn void thrd_exit(int result) {
-    __pthread_exit((void*)(intptr_t)result);
+    pthread_exit((void*)(intptr_t)result);
 }
