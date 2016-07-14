52b6f4fbe808acaf1e221d5fc8fcd88b2497da5f
Travis Geiselbrecht <travisg@google.com>
2016-07-13 22:33:42 -0700

    [kernel][x86] set CR4.PGE and fix AMD cpus
    AMD cpus appear to be more picky about unused bits in the inner
    page table levels being set. Make sure we dont set the G bits
    on the inner leaves.
    
    Change-Id: Icbdfdd3395aeba60e7a1da4517d21c9a84439f9c

--- kernel/arch/x86/include/arch/x86/mmu.h
+++ kernel/arch/x86/include/arch/x86/mmu.h
@@ -76,7 +76,7 @@
 #define X86_MMU_LARGE_PAT_WRITE_COMBINING       X86_PAT_LARGE_SELECTOR(7)
 
 /* default flags for inner page directory entries */
-#define X86_KERNEL_PD_FLAGS (X86_MMU_PG_G | X86_MMU_PG_RW | X86_MMU_PG_P)
+#define X86_KERNEL_PD_FLAGS (X86_MMU_PG_RW | X86_MMU_PG_P)
 
 /* default flags for 2MB/4MB/1GB page directory entries */
 #define X86_KERNEL_PD_LP_FLAGS (X86_MMU_PG_G | X86_MMU_PG_PS | X86_MMU_PG_RW | X86_MMU_PG_P)
