a9079132922d80092cb0112711c67ad73be805ee
Todd Eisenberger <teisenbe@google.com>
2016-07-21 16:15:37 -0700

    [crypto][prng] Make PRNG::Draw block until seeded
    Change-Id: I99374ca489728aa58f201e6dfdbeaa1b65dac807

--- kernel/lib/crypto/include/lib/crypto/prng.h
+++ kernel/lib/crypto/include/lib/crypto/prng.h
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <kernel/event.h>
 #include <kernel/mutex.h>
 #include <lib/crypto/cryptolib.h>
 
@@ -27,15 +28,21 @@ public:
     // Re-seed the PRNG by mixing-in new entropy. |size| is in bytes.
     void AddEntropy(const void* data, int size);
 
-    // get pseudo-random output of |size| bytes.
+    // Get pseudo-random output of |size| bytes.  Blocks until at least
+    // kMinEntropy bytes of entropy have been added to this PRNG.
     void Draw(void* out, int size);
 
+    // The minimum amount of entropy (in bytes) the generator requires before
+    // Draw will return data.
+    static constexpr uint64_t kMinEntropy = 32;
 private:
     PRNG(const PRNG&) = delete;
     PRNG& operator=(const PRNG&) = delete;
 
     clPRNG_CTX ctx_;
     mutex_t lock_;
+    uint64_t total_entropy_added_;
+    event_t ready_;
 };
 
 } // namespace crypto
--- kernel/lib/crypto/prng.cpp
+++ kernel/lib/crypto/prng.cpp
@@ -6,23 +6,46 @@
 
 #include <lib/crypto/prng.h>
 
+#include <assert.h>
+#include <compiler.h>
+#include <err.h>
 #include <lib/crypto/cryptolib.h>
 #include <kernel/auto_lock.h>
 
 namespace crypto {
 
 PRNG::PRNG(const void* data, int size) {
+    DEBUG_ASSERT(size >= 0);
+
     lock_ = MUTEX_INITIAL_VALUE(lock_);
     clPRNG_init(&ctx_, data, size);
+    total_entropy_added_ = size;
+
+    const bool enough_entropy = (total_entropy_added_ >= kMinEntropy);
+    ready_ = EVENT_INITIAL_VALUE(ready_, enough_entropy, 0);
 }
 
 void PRNG::AddEntropy(const void* data, int size) {
-    AutoLock guard(lock_);
-    clPRNG_entropy(&ctx_, data, size);
+    uint64_t total;
+    {
+        AutoLock guard(lock_);
+        clPRNG_entropy(&ctx_, data, size);
+        total_entropy_added_ += size;
+        total = total_entropy_added_;
+    }
+    if (total >= kMinEntropy) {
+        event_signal(&ready_, true);
+    }
 }
 
 void PRNG::Draw(void* out, int size) {
     AutoLock guard(lock_);
+    if (unlikely(total_entropy_added_ < kMinEntropy)) {
+        mutex_release(&lock_);
+        status_t status = event_wait(&ready_);
+        ASSERT(status == NO_ERROR);
+        mutex_acquire(&lock_);
+    }
     clPRNG_draw(&ctx_, out, size);
 }
 
