b33150f4ebd902d64c12ca035d81bdf0030391b8
Forrest Reiling <freiling@google.com>
2016-07-21 16:30:00 -0700

    Refactored gn files
    Change-Id: If59efd0b079ab280e6fe41142815343d56c152d1

--- lib/magma/src/sys-driver/platform_buffer/platform_buffer.cc
+++ lib/magma/src/sys-driver/platform_buffer/platform_buffer.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <platform_buffer.h>
+#include "platform_buffer.h"
 
 PlatformBuffer* PlatformBuffer::Create(uint64_t size)
 {
