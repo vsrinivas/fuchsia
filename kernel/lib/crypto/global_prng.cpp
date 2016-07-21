911b261849f5d82c2389c4569f523b4fdfcfba01
Todd Eisenberger <teisenbe@google.com>
2016-07-21 16:40:54 -0700

    [crypto][prng] Seed the global PRNG during boot
    The routine is executed as soon as threads are available to the system
    and draws entropy from the hardware entropy source if available.
    Currently if no source is available, it prints a warning and continues
    onward.
    
    Change-Id: I6080fd386cf504189bff7d5dcfa2cb913d167f50

--- kernel/lib/crypto/global_prng.cpp
+++ kernel/lib/crypto/global_prng.cpp
@@ -7,9 +7,12 @@
 #include <lib/crypto/global_prng.h>
 
 #include <assert.h>
+#include <dev/hw_rng.h>
+#include <err.h>
 #include <kernel/auto_lock.h>
 #include <kernel/mutex.h>
 #include <lib/crypto/prng.h>
+#include <lk/init.h>
 
 namespace crypto {
 
@@ -27,6 +30,31 @@ PRNG* GetInstance() {
     return global_prng;
 }
 
+static void EarlyBootSeed(uint level) {
+    PRNG* prng = GetInstance();
+
+    uint8_t buf[32] = {0};
+    // TODO(security): Have the PRNG reseed based on usage
+    size_t fetched = hw_rng_get_entropy(buf, sizeof(buf), true);
+    if (fetched == 0) {
+        // We made a blocking request, but it returned 0, so there is no PRNG
+        printf("WARNING: System has no entropy source.  It is completely "
+               "unsafe to use this system for any cryptographic applications."
+               "\n");
+        // TODO(security): *CRITICAL* This is a fallback for systems without RNG
+        // hardware that we should remove and attempt to do better.  If this
+        // fallback is used, it breaks all cryptography used on the system.
+        // *CRITICAL*
+        prng->AddEntropy(buf, sizeof(buf));
+        return;
+    }
+    DEBUG_ASSERT(fetched == sizeof(buf));
+    prng->AddEntropy(buf, static_cast<int>(fetched));
+}
+
 } //namespace GlobalPRNG
 
 } // namespace crypto
+
+LK_INIT_HOOK(global_prng, crypto::GlobalPRNG::EarlyBootSeed,
+             LK_INIT_LEVEL_THREADING);
