a7dbc02bb88d5c8eff5b455ee4c8c2b217d2c6de
Sean Klein <smklein@google.com>
2016-07-27 10:18:41 -0700

    [FAT] Added FAT32-exclusive FSInfo structure.
    This structure provides information about the next free cluster and the
    count of total free clusters.
    
    fsinfo will be used exclusively by the FAT package.
    
    Change-Id: I43db26c0a874ee75fccd3cf1786099d50b223c21

---
+++ go/src/thinfs/lib/fs/msdosfs/clusterManager/fat/fsinfo/fsinfo.go
@@ -0,0 +1,110 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package fsinfo describes the FAT32-exclusive FSInfo structure
+package fsinfo
+
+import (
+	"errors"
+	"fmt"
+	"unsafe"
+
+	"fuchsia.googlesource.com/thinfs/lib/fs/msdosfs/bits"
+	"fuchsia.googlesource.com/thinfs/lib/thinio"
+)
+
+const (
+	freeCountOffset       = 488
+	nextFreeClusterOffset = 492
+
+	leadSig  = 0x41615252
+	innerSig = 0x61417272
+	finalSig = 0xAA550000
+
+	// UnknownFlag describes a value (such as the next free cluster) which is not known.
+	UnknownFlag = 0xFFFFFFFF
+)
+
+// fsInfo describes the FAT32 FSInfo block.
+type fsInfo struct {
+	leadSignature     [4]uint8   // 0x41615252
+	reserved1         [480]uint8 // Zero
+	innerSignature    [4]uint8   // 0x61417272
+	freeCountClusters [4]uint8   // The last known count of free clusters or 0xFFFFFFFF
+	nextFreeCluster   [4]uint8   // A hint for the FAT driver or 0xFFFFFFFF
+	reserved2         [12]uint8  // Zero
+	finalSignature    [4]uint8   // 0xAA550000
+}
+
+// Validate verifies the FsInfo is valid.
+func (f *fsInfo) validate() error {
+	// Validate signatures
+	s := bits.GetLE32(f.leadSignature[:])
+	if s != leadSig {
+		return fmt.Errorf("Expected leading signature: %d, but got %d", leadSig, s)
+	}
+	s = bits.GetLE32(f.innerSignature[:])
+	if s != innerSig {
+		return fmt.Errorf("Expected inner signature: %d, but got %d", innerSig, s)
+	}
+	s = bits.GetLE32(f.finalSignature[:])
+	if s != finalSig {
+		return fmt.Errorf("Expected final signature: %d, but got %d", finalSig, s)
+	}
+
+	return nil
+}
+
+// ReadHints reads FS Info from the device (at the offset) and returns the freeCount of clusters and the
+// nextFree cluster (if they are known). Otherwise, returns an error.
+func ReadHints(d *thinio.Conductor, offset int64) (freeCount, nextFree uint32, err error) {
+	var info fsInfo
+	b := (*(*[unsafe.Sizeof(info)]byte)(unsafe.Pointer(&info)))[:]
+
+	_, err = d.ReadAt(b, offset)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if err = info.validate(); err != nil {
+		return 0, 0, err
+	}
+
+	freeCount = bits.GetLE32(info.freeCountClusters[:])
+	nextFree = bits.GetLE32(info.nextFreeCluster[:])
+	if freeCount == UnknownFlag {
+		return 0, 0, errors.New("Unknown free count of clusters")
+	} else if nextFree == UnknownFlag {
+		return 0, 0, errors.New("Unknown next free cluster")
+	}
+	return
+}
+
+// WriteFreeCount takes the offset of the FS Info structure and updates the on-disk value of the
+// "free cluster count".
+func WriteFreeCount(d *thinio.Conductor, offset int64, freeCount uint32) error {
+	b := make([]byte, 4)
+	bits.PutLE32(b, freeCount)
+	_, err := d.WriteAt(b, offset+freeCountOffset)
+	return err
+}
+
+// WriteNextFree takes the offset of the FS Info structure and updates the on-disk value of the
+// "next free cluster".
+func WriteNextFree(d *thinio.Conductor, offset int64, nextFree uint32) error {
+	b := make([]byte, 4)
+	bits.PutLE32(b, nextFree)
+	_, err := d.WriteAt(b, offset+nextFreeClusterOffset)
+	return err
+}
---
+++ go/src/thinfs/lib/fs/msdosfs/clusterManager/fat/fsinfo/fsinfo_test.go
@@ -0,0 +1,161 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fsinfo
+
+import (
+	"testing"
+	"unsafe"
+
+	"fuchsia.googlesource.com/thinfs/lib/fs/msdosfs/bootrecord"
+	"fuchsia.googlesource.com/thinfs/lib/fs/msdosfs/testutil"
+)
+
+// Tests the size of the FS Info structure
+func TestFsInfoSize(t *testing.T) {
+	if sz := unsafe.Sizeof(fsInfo{}); sz != 512 {
+		t.Fatalf("fsInfo struct is %d byte, but it should be 512 bytes", sz)
+	}
+	info := &fsInfo{}
+	align := unsafe.Alignof(*info)
+	if align != 1 {
+		t.Fatalf("Expected fs info to have no alignment requirements, but Alignof is: %d", align)
+	}
+	fcOffset := unsafe.Offsetof(info.freeCountClusters)
+	if freeCountOffset != fcOffset {
+		t.Fatalf("Expected freeCountOffset of %d, but saw %d\n", freeCountOffset, fcOffset)
+	}
+	nfOffset := unsafe.Offsetof(info.nextFreeCluster)
+	if nextFreeClusterOffset != nfOffset {
+		t.Fatalf("Expected nextFreeClusterOffset of %d, but saw %d\n", nextFreeClusterOffset, nfOffset)
+	}
+}
+
+// Tests the FS Info structure can be read and written to.
+func TestFsInfoFAT32ReadWrite(t *testing.T) {
+	fs := testutil.MkfsFAT(t, "1G", 2, 0, 4, 512)
+	defer fs.RmfsFAT()
+	d := fs.GetDevice()
+	defer d.Close()
+
+	// Read the bootrecord, get the offset of the FS Info
+	br, err := bootrecord.New(d)
+	if err != nil {
+		t.Fatal(err)
+	}
+	offset, err := br.FsInfoOffset()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the default hints
+	freeCount, nextFree, err := ReadHints(d, offset)
+
+	if err != nil {
+		t.Fatal(err)
+	} else if freeCount == 0 || freeCount == UnknownFlag {
+		t.Fatalf("Unexpected free count: %d", freeCount)
+	} else if nextFree != 2 {
+		t.Fatalf("Unexpected next free: %d (expected %d)", nextFree, 2)
+	}
+
+	// Write new hints
+	newFreeCount := uint32(10)
+	newNextFree := uint32(15)
+	if err := WriteFreeCount(d, offset, newFreeCount); err != nil {
+		t.Fatal(err)
+	} else if err := WriteNextFree(d, offset, newNextFree); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check the hints we just wrote actually ended up in the right place
+	freeCount, nextFree, err = ReadHints(d, offset)
+	if err != nil {
+		t.Fatal(err)
+	} else if freeCount != newFreeCount {
+		t.Fatalf("Unexpected free count: %d (expected %d)", freeCount, newFreeCount)
+	} else if nextFree != newNextFree {
+		t.Fatalf("Unexpected next free: %d (expected %d)", nextFree, newNextFree)
+	}
+
+	// Write an unknown "next free" hint.
+	if err := WriteNextFree(d, offset, 0xFFFFFFFF); err != nil {
+		t.Fatal(err)
+	} else if _, _, err = ReadHints(d, offset); err == nil {
+		t.Fatal("Expected that an unknown 'next free' hint would cause an error")
+	}
+
+	// Put the "next free" hint back to a valid value, and then repeat the process with an invalid
+	// "free count" hint.
+	if err := WriteNextFree(d, offset, newNextFree); err != nil {
+		t.Fatal(err)
+	} else if err := WriteFreeCount(d, offset, 0xFFFFFFFF); err != nil {
+		t.Fatal(err)
+	} else if _, _, err = ReadHints(d, offset); err == nil {
+		t.Fatal("Expected that an unknown 'free count' hint would cause an error")
+	}
+
+}
+
+// Tests the FS Info structure validates signatures on FAT32
+func TestFsInfoFAT32Invalid(t *testing.T) {
+	doTest := func(byteToModify int64, newVal byte) {
+		fs := testutil.MkfsFAT(t, "1G", 2, 0, 4, 512)
+		d := fs.GetDevice()
+
+		// Read the bootrecord, get the offset of the FS Info
+		br, err := bootrecord.New(d)
+		if err != nil {
+			t.Fatal(err)
+		}
+		offset, err := br.FsInfoOffset()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Mess up the fsInfo signature (make it invalid)
+		if _, err := d.WriteAt([]byte{0}, offset+byteToModify); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, _, err := ReadHints(d, offset); err == nil {
+			t.Fatalf("Expected ReadHints to fail with modified byte at %d", byteToModify)
+		}
+
+		d.Close()
+		fs.RmfsFAT()
+	}
+
+	doTest(0, 0)   // Mess up leading signature
+	doTest(484, 0) // Mess up inner signature
+	doTest(511, 0) // Mess up final signature
+}
+
+// Tests the FS Info structure is not accessible on FAT16
+func TestFsInfoFAT16Invalid(t *testing.T) {
+	fs := testutil.MkfsFAT(t, "10M", 2, 0, 4, 512)
+	defer fs.RmfsFAT()
+	d := fs.GetDevice()
+	defer d.Close()
+
+	// Read the bootrecord, get the offset of the FS Info
+	br, err := bootrecord.New(d)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = br.FsInfoOffset()
+	if err == nil {
+		t.Fatal("Expected FS Info offset to fail for FAT 16")
+	}
+}
