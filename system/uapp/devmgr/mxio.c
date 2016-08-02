dc31cf9ce85a23a5a6e19cb284cd70c9b3efc5ba
Roland McGrath <mcgrathr@google.com>
2016-08-02 16:01:45 -0700

    [uapp] [devmgr] Fix devmgr build after launchpad_launch signature change
    Change-Id: I2f97cbc705b58689a9b24d38b1df2a81e632bf0d

--- system/uapp/devmgr/mxio.c
+++ system/uapp/devmgr/mxio.c
@@ -78,7 +78,7 @@ void devmgr_launch(const char* name, const char* app, const char* arg, const cha
         n += r;
     }
     printf("devmgr: launch %s on %s\n", app, device);
-    mx_handle_t proc = launchpad_launch(name, arg ? 2 : 1, args,
+    mx_handle_t proc = launchpad_launch(name, arg ? 2 : 1, args, NULL,
                                         n, hnd, ids);
     if (proc < 0)
         printf("devmgr: launchpad_launch failed: %d\n", proc);
@@ -112,7 +112,7 @@ void devmgr_launch_devhost(const char* name, mx_handle_t h,
     ids[1] = MX_HND_TYPE_USER1;
     hnd[1] = h;
     printf("devmgr: launch: %s %s %s\n", name, arg0, arg1);
-    mx_handle_t proc = launchpad_launch(name, 3, args, 2, hnd, ids);
+    mx_handle_t proc = launchpad_launch(name, 3, args, NULL, 2, hnd, ids);
     if (proc < 0)
         printf("devmgr: launch failed: %d\n", proc);
     else
