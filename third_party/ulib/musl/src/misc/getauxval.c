f6f35130db08a511ceffddc2f565207e87edd2b9
Roland McGrath <mcgrathr@google.com>
2016-08-04 17:49:09 -0700

    [ulib] [musl] Stub out getauxval
    This API exists to query the auxv, which we do not have at all.
    Handle a couple of known uses, and punt the general case.
    
    Change-Id: Ie21dfea59f67eab7e42b0de8bdfec323af8a2276

--- third_party/ulib/musl/src/misc/getauxval.c
+++ third_party/ulib/musl/src/misc/getauxval.c
@@ -1,14 +1,20 @@
 #include "libc.h"
 #include <errno.h>
 #include <sys/auxv.h>
+#include <sys/param.h>
 
 unsigned long getauxval(unsigned long item) {
-    size_t* auxv = libc.auxv;
-    if (item == AT_SECURE)
+    // We have no auxv, so just special-case a few things we do know about.
+    // TODO(mcgrathr): Maybe handle some more values?  It's unclear what
+    // any users want other than AT_HWCAP, which we don't have.
+
+    switch (item) {
+    case AT_SECURE:
         return libc.secure;
-    for (; *auxv; auxv += 2)
-        if (*auxv == item)
-            return auxv[1];
+    case AT_PAGESZ:
+        return PAGE_SIZE;
+    }
+
     errno = ENOENT;
     return 0;
 }
