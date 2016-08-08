d33bf6531324b4927903c54e1269eca0b41373aa
Petr Hosek <phosek@google.com>
2016-08-08 15:55:20 -0700

    Decouple toolchain and sysroot
    Allow rolloing toolchain versions indepdendently from sysroots.
    
    Change-Id: Idf13117085c04e2aa1532278a28d079a86b376ea

--- update.sh
+++ update.sh
@@ -82,7 +82,23 @@ function download_tarball() {
 # the uploaded artifact and what directories they expect to be untarred from.
 # There's no good reason for these to be different - unify them.
 download_tarball cmake "cmake/${HOST_PLATFORM}" "${SCRIPT_ROOT}"
-download_tarball sdk sdk "${SCRIPT_ROOT}/sdk"
+download_tarball toolchain "toolchain/${HOST_PLATFORM}" "${SCRIPT_ROOT}/toolchain"
+
+function download_sysroot() {
+  local arch="${1}"
+  local base_path="${SCRIPT_ROOT}/sysroot/${arch}-fuchsia"
+  local base_url="sysroot/${arch}"
+  local tar_path="${base_path}.tar.bz2"
+
+  download_file_if_needed "${arch} sysroot" "${FUCHSIA_URL_BASE}/${base_url}" "${base_path}" ".tar.bz2"
+  if [[ -f "${tar_path}" ]]; then
+    (cd -- "${SCRIPT_ROOT}" && tar xf ${tar_path} -C "${SCRIPT_ROOT}/sysroot")
+    rm -f -- "${tar_path}"
+  fi
+}
+
+download_sysroot "aarch64"
+download_sysroot "x86_64"
 
 # build_magenta_tool <name>
 function build_magenta_tool() {
