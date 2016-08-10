00a27105caefd6bcc1a94b61211323515c52f7d2
James Robinson <jamesr@google.com>
2016-08-10 13:49:22 -0700

    Add shlibs to user bootfs for running C++ programs
    This adds libc++abi.so.1, libc++.so.2 and libunwind.so.1 from the
    appropriate sysroot to the bootfs so dynamically linked C++ programs
    can run.
    
    Change-Id: Ia369a04de641437c5a3a3d4a4abcdce6b5a81098

--- gn/process_modules.py
+++ gn/process_modules.py
@@ -9,7 +9,9 @@ import sys
 import paths
 import json
 
+
 class Amalgamation:
+
     def __init__(self):
         self.labels = []
         self.files = []
@@ -54,11 +56,12 @@ def resolve_imports(import_queue, build_root):
 
 def main():
     parser = argparse.ArgumentParser(description="Generate bootfs manifest and"
-            +"list of GN targets for a list of Fuchsia modules")
+                                     + "list of GN targets for a list of Fuchsia modules")
     parser.add_argument("--manifest", help="path to manifest file to generate")
     parser.add_argument("--modules", help="list of modules", default="default")
     parser.add_argument("--build-root", help="path to root of build directory")
     parser.add_argument("--depfile", help="path to depfile to generate")
+    parser.add_argument("--arch", help="architecture being targetted")
     args = parser.parse_args()
 
     amalgamation = resolve_imports(args.modules.split(","), args.build_root)
@@ -68,6 +71,18 @@ def main():
     if not os.path.exists(manifest_dir):
         os.makedirs(manifest_dir)
     with open(os.path.join(args.manifest), "w") as manifest:
+        manifest.write("user.bootfs:\n")
+        libs = ["libc++abi.so.1",
+                "libc++.so.2",
+                "libunwind.so.1"]
+        if args.arch == "x64":
+            sysroot_arch_name = "x86_64-fuchsia"
+        elif args.arch == "arm64":
+            sysroot_arch_name = "aarch-64-fuchsia"
+        lib_root = os.path.join(
+            paths.FUCHSIA_ROOT, "buildtools", "sysroot", sysroot_arch_name, "lib")
+        for lib in libs:
+            manifest.write("lib/%s=%s\n" % (lib, os.path.join(lib_root, lib)))
         for file in amalgamation.files:
             manifest.write("%s=%s\n" % (file["bootfs_path"], file["file"]))
     if args.depfile != "":
@@ -75,7 +90,6 @@ def main():
             f.write("user.bootfs:")
             for path in amalgamation.config_paths:
                 f.write(" " + path)
-
 
     print "\n".join(amalgamation.labels)
     return 0
