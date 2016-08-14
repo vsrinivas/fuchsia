d3021c683a44c58794281f57c9fd48b2da3ad430
Brian Swetland <swetland@google.com>
2016-08-14 02:46:05 -0700

    [build][travis-ci] possibly fix the cache stuff
    Change-Id: Ib4e5b8d371c172f21d1e15febf7269ab0f1e15f3

--- scripts/travis-builds.sh
+++ scripts/travis-builds.sh
@@ -6,8 +6,16 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT
 
+# restore config.mk from the cached configs directory
+# if it exists
+cp -f ./prebuilt/configs/config.mk ./prebuilt/config.mk
+
 echo "Downloading Toolchain"
 ./scripts/download-toolchain
+
+# save config.mk to the configs directory so it will be
+# cached along with the downloaded toolchain
+cp -f ./prebuilt/config.mk ./prebuilt/configs
 
 echo "Starting build '$PROJECT'"
 make $PROJECT -j4 ENABLE_BUILD_SYSROOT=true
