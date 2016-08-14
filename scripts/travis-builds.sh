3156c208278b72a2723b5ba6719cb6a086930e11
Brian Swetland <swetland@google.com>
2016-08-14 00:41:21 -0700

    [build] travis-ci configuration (experimental)
    Change-Id: If5c5f99243abdcfdd245ee32a469f032b74c2189

---
+++ scripts/travis-builds.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Copyright 2016 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+echo "Downloading Toolchain"
+./scripts/download-toolchain
+
+echo "Starting build '$PROJECT'"
+make $PROJECT
