dd82b26247f353b003d578e3bd86fc5dba1e692f
George Kulakowski <kulakowski@google.com>
2016-08-19 10:15:11 -0700

    [make][userspace] Define _ALL_SOURCE for userspace builds
    We have defined some commonly used extensions (around thread and lock
    creation, in particular) that are feature gated behind the _ALL_SOURCE
    macro. Tthese extensions will be used so widely that we do not wish to
    litter magenta source with this define. So instead, have the build
    system define it for everyone (except libc, but it already disables it).
    
    Change-Id: If1db31c88b040c01fb42efe2ea15560025334e7f

--- system/utest/core/c11-mutex/mutex.c
+++ system/utest/core/c11-mutex/mutex.c
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#define _ALL_SOURCE // For MTX_INIT.
 #include <magenta/syscalls.h>
 #include <unittest/unittest.h>
 #include <runtime/mutex.h>
