eb3e0b6d9cde3e266f9402062d2f064b30e768d2
Carlos Pizano <cpu@google.com>
2016-08-19 17:48:36 -0700

    [system][uapp] Add the crashlogger service
    The crash logger service is a regular app that
    takes over the systemwide exception handler. If an
    application crashes it receives the exception and
    proceses it. The basic output is just the cpu state
    but more to come.
    
    Note: Currently it exits after handling a single
    exception request.
    
    Change-Id: Ic2d69ea9f84c863d3e91dbb2817e5cba9a6772bf

---
+++ system/uapp/crashlogger/crashlogger.cpp
@@ -0,0 +1,91 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <magenta/syscalls.h>
+
+enum : uint32_t {
+    EXC_FATAL_PAGE_FAULT,
+    EXC_UNDEFINED_INSTRUCTION,
+    EXC_GENERAL,
+};
+
+const char* exc_type_to_str(uint32_t type) {
+    switch (type) {
+    case EXC_FATAL_PAGE_FAULT:
+        return "fatal page fault";
+    case EXC_UNDEFINED_INSTRUCTION:
+        return "undefined instruction";
+    case EXC_GENERAL:
+        return "general fault";
+    default:
+        return "unknown fault";
+    }
+}
+
+constexpr uint64_t kSysExceptionKey = 1166444u;
+
+void print_error(int line, const char* what = "") {
+    fprintf(stderr, "crashlogger: ln%d : %s\n", line, what);
+}
+
+void output_frame_x86_64(const x86_64_exc_frame_t& frame) {
+    printf(" CS:  %#18llx RIP: %#18llx EFL: %#18llx CR2: %#18lx\n",
+           frame.cs, frame.ip, frame.flags, 0ul);
+    printf(" RAX: %#18llx RBX: %#18llx RCX: %#18llx RDX: %#18llx\n",
+           frame.rax, frame.rbx, frame.rcx, frame.rdx);
+    printf(" RSI: %#18llx RDI: %#18llx RBP: %#18llx RSP: %#18llx\n",
+           frame.rsi, frame.rdi, frame.rbp, frame.user_sp);
+    printf("  R8: %#18llx  R9: %#18llx R10: %#18llx R11: %#18llx\n",
+           frame.r8, frame.r9, frame.r10, frame.r11);
+    printf(" R12: %#18llx R13: %#18llx R14: %#18llx R15: %#18llx\n",
+           frame.r12, frame.r13, frame.r14, frame.r15);
+    printf(" errc: %#18llx\n", frame.err_code);
+}
+
+void process_report(const mx_exception_report_t* report) {
+    auto context = report->context;
+    printf("<== fatal exception: process [%llu] thread [%llu]\n", context.pid, context.tid);
+    printf("<== %s , PC at 0x%lx\n", exc_type_to_str(context.arch.subtype), context.arch.pc);
+
+    if (context.arch_id == ARCH_ID_X86_64) {
+        output_frame_x86_64(context.arch.u.x86_64);
+    } else {
+        // TODO: support other architectures.
+        printf("unsuported architecture .. comming soon.\n");
+    }
+}
+
+int main(int argc, char** argv) {
+    mx_handle_t ex_port = mx_io_port_create(0u);
+    if (ex_port < 0) {
+        print_error(__LINE__);
+        return 1;
+    }
+
+    mx_status_t status = mx_set_system_exception_port(ex_port, kSysExceptionKey, 0);
+    if (status < 0) {
+        print_error(__LINE__, "unable to set exception port");
+        return 1;
+    }
+
+    printf("crashlogger service ready\n");
+
+    while (true) {
+        mx_exception_packet_t packet;
+        mx_io_port_wait(ex_port, &packet, sizeof(packet));
+        if (packet.hdr.key != kSysExceptionKey) {
+            print_error(__LINE__, "invalid crash key");
+            return 1;
+        }
+
+        process_report(&packet.report);
+        break;
+    }
+
+    return 0;
+}
