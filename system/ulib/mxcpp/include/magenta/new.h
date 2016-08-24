2573e77d7448ce5e2196730804cfb47d0c59a864
John Grossman <johngro@google.com>
2016-08-24 13:34:33 -0700

    [usermode][cpp] Add libmagentacpp
    Add a library which provides minimal magenta-style C++ runtime
    support.  The library depends on MUSL, and...
    
    1) Provides a magenta-style new/delete implementation (one which
       requires the use of AllocChecker)
    2) Provides a pure-virtual-was-called panic implementation.
    
    Change-Id: I692ddc7e10001d5da2d9f1d259b726ee20547e53

---
+++ system/ulib/mxcpp/include/magenta/new.h
@@ -0,0 +1,25 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <stddef.h>
+
+class AllocChecker {
+public:
+    AllocChecker();
+    ~AllocChecker();
+    void arm(size_t sz, bool result);
+    bool check();
+private:
+    unsigned state_;
+};
+
+void *operator new(size_t, AllocChecker* ac);
+void *operator new(size_t, void *ptr);
+void *operator new[](size_t, AllocChecker* ac);
+void *operator new[](size_t, void *ptr);
+void operator delete(void *p);
+void operator delete[](void *p);
+
---
+++ system/ulib/mxcpp/rules.mk
@@ -0,0 +1,17 @@
+# Copyright 2016 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+MODULE := $(LOCAL_DIR)
+MODULE_TYPE := userlib
+
+MODULE_SRCS = $(LOCAL_DIR)/new.cpp \
+              $(LOCAL_DIR)/pure_virtual.cpp
+
+MODULE_EXPORT := mxcpp
+MODULE_SO_NAME := mxcpp
+
+MODULE_LIBS += ulib/musl
+
+include make/module.mk
