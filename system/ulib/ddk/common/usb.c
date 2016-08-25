47f0389beb05944c8099580a7d05463bfb5a4f20
John Grossman <johngro@google.com>
2016-08-25 10:28:02 -0700

    [xhci] Fix the build.
    Roll back a small part of a recent change which changed a funciton
    signature.  I suspect that it was not supposed to go in quite yet.
    
    Change-Id: I66725350b18abbf13676eda419c4899013cf8c93

--- system/ulib/ddk/common/usb.c
+++ system/ulib/ddk/common/usb.c
@@ -135,7 +135,7 @@ mx_status_t usb_clear_feature(mx_device_t* device, uint8_t request_type, int fea
 }
 
 // helper function for allocating iotxns for USB transfers
-iotxn_t* usb_alloc_iotxn(uint8_t ep_address, size_t data_size, size_t extra_size) {
+iotxn_t* usb_alloc_iotxn(usb_endpoint_descriptor_t* ep_desc, size_t data_size, size_t extra_size) {
     iotxn_t* txn;
 
     mx_status_t status = iotxn_alloc(&txn, 0, data_size, extra_size);
@@ -145,7 +145,7 @@ iotxn_t* usb_alloc_iotxn(uint8_t ep_address, size_t data_size, size_t extra_size
     txn->protocol = MX_PROTOCOL_USB_DEVICE;
 
     usb_protocol_data_t* data = iotxn_pdata(txn, usb_protocol_data_t);
-    data->ep_address = ep_address;
+    data->ep_address = ep_desc->bEndpointAddress;
 
     return txn;
 }
