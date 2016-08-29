5f2fecc84a3e2fa8b13f0225810b870eafe4f0d6
Roland McGrath <mcgrathr@google.com>
2016-08-29 15:45:50 -0700

    [kernel][syscalls][ulib][magenta] Remove remnants of MAGENTA_DDKCALL_DEF
    Change-Id: Id946fb3b546ca9099e9f1fad6a7c164b06faa693

--- system/ulib/ddk/rules.mk
+++ system/ulib/ddk/rules.mk
@@ -8,8 +8,6 @@ MODULE := $(LOCAL_DIR)
 
 MODULE_TYPE := userlib
 
-MODULE_DEFINES := LIBDDK=1
-
 MODULE_SRCS += \
     $(LOCAL_DIR)/common/hid.c \
     $(LOCAL_DIR)/common/hid-fifo.c \
@@ -18,22 +16,6 @@ MODULE_SRCS += \
     $(LOCAL_DIR)/protocol/input.c \
     $(LOCAL_DIR)/io-alloc.c \
     $(LOCAL_DIR)/iotxn.c \
-
-ifeq ($(ARCH),arm)
-MODULE_SRCS += system/ulib/magenta/syscalls-arm32.S
-else ifeq ($(ARCH),arm64)
-MODULE_SRCS += system/ulib/magenta/syscalls-arm64.S
-else ifeq ($(ARCH),x86)
-    ifeq ($(SUBARCH),x86-64)
-    MODULE_SRCS += system/ulib/magenta/syscalls-x86-64.S
-    else
-    MODULE_SRCS += system/ulib/magenta/syscalls-x86.S
-    endif
-endif
-
-MODULE_DEPS += \
-    ulib/musl \
-    ulib/magenta
 
 MODULE_STATIC_LIBS := ulib/hexdump
 
