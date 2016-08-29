2fe462a2cd31d8d65961dcadac97f8b16a391800
Todd Eisenberger <teisenbe@google.com>
2016-08-29 11:16:31 -0700

    [acpi] Reduce the amount of ACPICA built for the kernel
    This also removes the suspend-to-RAM stub for x86, which broke
    when we moved ACPI out of the kernel anyway.
    
    Change-Id: Iece785ce429c11a00a1489340fc55d2e645b5b6f

--- kernel/arch/x86/rules.mk
+++ kernel/arch/x86/rules.mk
@@ -103,7 +103,6 @@ MODULE_SRCS += \
 	$(SUBARCH_DIR)/bootstrap16.c \
 	$(SUBARCH_DIR)/smp.c \
 	$(SUBARCH_DIR)/start16.S \
-	$(SUBARCH_DIR)/suspend.S \
 	$(SUBARCH_DIR)/uspace_entry.S
 endif # SUBARCH x86-64
 ifeq ($(SUBARCH),x86-32)
