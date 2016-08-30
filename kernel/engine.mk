1c07cefdbe0a8297a30229d9e9d2bb200bf10d99
Brian Swetland <swetland@google.com>
2016-08-30 10:43:02 -0700

    [build] copy .inc as well as .h files include sysroot/include
    Change-Id: Ie9ef2effc0419fb2cf0242eda80722c4250307e3

--- kernel/engine.mk
+++ kernel/engine.mk
@@ -263,11 +263,12 @@ include system/tools/build.mk
 
 ifeq ($(call TOBOOL,$(ENABLE_BUILD_SYSROOT)),true)
 # identify global headers to copy to the sysroot
-GLOBAL_HEADERS := $(shell find system/public -name \*\.h)
+GLOBAL_HEADERS := $(shell find system/public -name \*\.h -or -name \*\.inc)
 GLOBAL_HEADERS := $(patsubst system/public/%,$(BUILDDIR)/sysroot/include/%,$(GLOBAL_HEADERS))
 
 # generate rule to copy them
 $(call copy-dst-src,$(BUILDDIR)/sysroot/include/%.h,system/public/%.h)
+$(call copy-dst-src,$(BUILDDIR)/sysroot/include/%.inc,system/public/%.inc)
 
 SYSROOT_DEPS += $(GLOBAL_HEADERS)
 GENERATED += $(GLOBAL_HEADERS)
