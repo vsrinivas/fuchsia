048091a6e07551792eb599f524ee71e5e9b99a0f
Roland McGrath <mcgrathr@google.com>
2016-09-02 14:13:55 -0700

    [uapp][userboot] Compile string functions into userboot directly
    This avoids the need for musl-static, which we can now remove.
    It also obviates the hidden.h hack, because we can now build
    the libc functions with -fvisibility=hidden too.
    
    Change-Id: Ibcd717550804f9e1c02474f693a4a0371f8f6ef6

--- system/uapp/userboot/rules.mk
+++ system/uapp/userboot/rules.mk
@@ -22,26 +22,17 @@ MODULE_NAME := userboot
 MODULE_SO_NAME := userboot
 MODULE_SO_INSTALL_NAME := -
 
-# We do link against musl-static to get the string functions.  But we
-# carefully use hidden visibility for these so they have no PLT entries.
-# The compiler inlines memcpy et al as builtins and then (often)
-# generates calls to those same functions as an implementaiton detail of
-# its builtins; those generated calls use PLT entries.  Using the
-# -ffreestanding switch would tell the compiler not to inline memcpy et
-# al, so the explicit calls in the source would respect the hidden
-# visibility we used to declare them.  However, even with -ffreestanding
-# the compiler can generate calls to memcpy, memset, etc. on its own.
-# If the compiler generates a call to (e.g. memcpy) in a translation
-# unit where there was no explicit call to that symbol, it won't emit
-# the .hidden marker in the assembly for the symbol, meaning the linker
-# will generate a PLT entry and relocation for it, which we cannot
-# allow.  So don't bother with -ffreestanding, since it doesn't solve
-# the problem anyway and some inlining might be beneficial.  Instead
-# feed every translation unit a header file that stuffs some .hidden
-# declarations into the assembly to cover symbols compilers might use.
-MODULE_COMPILEFLAGS += -include $(LOCAL_DIR)/hidden.h
+# Directly compile in the few functions we need from libc.
+# This doesn't get arch-specific optimized versions, but
+# such optimization isn't very important for userboot.
+userboot-string-functions := memcmp memcpy memset strlen strncmp
+MODULE_SRCS += \
+    $(userboot-string-functions:%=third_party/ulib/musl/src/string/%.c)
 
-MODULE_STATIC_LIBS := ulib/elfload ulib/runtime ulib/musl-static
+# Make sure there are never any PLT entries generated.
+MODULE_COMPILEFLAGS += -fvisibility=hidden
+
+MODULE_STATIC_LIBS := ulib/elfload ulib/runtime
 MODULE_HEADER_DEPS := ulib/magenta
 
 # This generated header lists all the ABI symbols in the vDSO with their
