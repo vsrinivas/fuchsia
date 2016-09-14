3dec67fc7e89331c23a3db36dcfc86f57755ccb9
James Robinson <jamesr@google.com>
2016-09-14 14:24:00 -0700

    Add script to update Dart packages in //third_party/dart-pkg
    This script contains the set of 'root' pubspec.yaml files that constrain the set
    of packages we import. Once we convert the world to this system we should
    probably limit this set to a central location, but getting the requires pulling
    in everything first.
    
    Change-Id: Ie0d92237d49a368c677402ce2a17c954b80ed113

---
+++ paths.py
@@ -0,0 +1,7 @@
+# Copyright 2016 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+FUCHSIA_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
---
+++ update_dart_packages.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Copyright 2016 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import paths
+import subprocess
+import sys
+
+# These are the locations of pubspec files that are roots of the dependency
+# graph.  If they contain conflicting requirements for a package 'pub get' will
+# error our and the conflicts will have to be resolved before the packages can
+# be updated.
+ROOT_PUBSPECS = [
+    'lib/flutter/packages/flutter',
+    'lib/flutter/packages/flutter_test',
+    'apps/modular/handler',
+]
+
+
+def main():
+    flutter_bin_path = os.path.join(paths.FUCHSIA_ROOT, 'lib/flutter/bin')
+    # First, run flutter precache to seed its dart SDK and packages
+    # TODO: Stop relying on flutter's copy of the 'pub' binary. Instead, we
+    # should build the 'dart' binary and use it to run pub.dart
+    subprocess.check_call(
+        [os.path.join(flutter_bin_path, 'flutter'), 'precache'])
+    # Then run importer.py, placing the flutter bin cache on the PATH to pick
+    # up the 'pub' binary
+    importer_path = os.path.join(paths.FUCHSIA_ROOT, 'third_party', 'dart-pkg',
+                                 'importer', 'importer.py')
+    args = [importer_path]
+    env = os.environ
+    env['PATH'] += ":" + \
+        os.path.join(flutter_bin_path, 'cache', 'dart-sdk', 'bin')
+    for root in ROOT_PUBSPECS:
+        args.append(os.path.join(paths.FUCHSIA_ROOT, root))
+    subprocess.check_call(args, env=env)
+
+if __name__ == '__main__':
+    sys.exit(main())
