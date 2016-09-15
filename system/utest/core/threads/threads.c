dd640bc05447aed0529ab7c15620d393b51141fd
Viet-Trung Luu <vtl@google.com>
2016-09-15 12:28:19 -0700

    [utest] Make threads-test just use mxr_...() functions instead of tu_thread_create().
    Also, on that thread, don't use any C standard library functions (since
    TLS won't have been initialized properly).
    
    MG-288 #comment Update threads.c.
    
    Change-Id: Ifaae46efb36578273f8432db4ca6a466f6375552

--- system/utest/core/threads/threads.c
+++ system/utest/core/threads/threads.c
@@ -2,50 +2,45 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stddef.h>
 
 #include <magenta/syscalls.h>
 #include <unittest/unittest.h>
-#include <test-utils/test-utils.h>
 #include <runtime/thread.h>
 
-static intptr_t thread_1(void* arg) {
-    unittest_printf("thread 1 sleeping for .1 seconds\n");
-    struct timespec t = (struct timespec){
-        .tv_sec = 0,
-        .tv_nsec = 100 * 1000 * 1000,
-    };
-    nanosleep(&t, NULL);
-
-    unittest_printf("thread 1 calling mx_thread_exit()\n");
+static intptr_t test_thread_fn(void* arg) {
+    // Note: You shouldn't use C standard library functions from this thread.
+    mx_nanosleep(MX_MSEC(100));
     mx_thread_exit();
     return 0;
 }
 
 bool threads_test(void) {
     BEGIN_TEST;
-    mx_handle_t handle;
-    unittest_printf("Welcome to thread test!\n");
 
-    for (int i = 0; i != 4; ++i) {
-        handle = tu_thread_create(thread_1, NULL, "thread 1");
-        ASSERT_GT(handle, 0, "Error while creating thread");
-        unittest_printf("thread:%d created handle %d\n", i, handle);
+    const mx_size_t stack_size = 256u << 10;
+    mx_handle_t thread_stack_vmo = mx_vmo_create(stack_size);
+    ASSERT_GT(thread_stack_vmo, 0, "");
 
-        mx_handle_wait_one(handle, MX_SIGNAL_SIGNALED, MX_TIME_INFINITE, NULL);
-        unittest_printf("thread:%d joined\n", i);
+    uintptr_t stack = 0u;
+    ASSERT_EQ(mx_process_map_vm(mx_process_self(), thread_stack_vmo, 0, stack_size, &stack,
+                                MX_VM_FLAG_PERM_READ | MX_VM_FLAG_PERM_WRITE), NO_ERROR, "");
+    ASSERT_EQ(mx_handle_close(thread_stack_vmo), NO_ERROR, "");
 
-        mx_handle_close(handle);
-    }
+    mxr_thread_t* thread = NULL;
+    ASSERT_EQ(mxr_thread_create("test_thread", &thread), NO_ERROR, "");
+    ASSERT_EQ(mxr_thread_start(thread, stack, stack_size, test_thread_fn, NULL), NO_ERROR, "");
 
-    unittest_printf("Attempting to create thread with a super long name. This should fail\n");
-    mxr_thread_t *t = NULL;
-    mx_status_t status = mxr_thread_create(
-                                    "01234567890123456789012345678901234567890123456789012345678901234567890123456789", &t);
-    ASSERT_LT(status, 0, "Thread creation should have failed");
-    unittest_printf("mxr_thread_create returned %d\n", status);
+    ASSERT_EQ(mx_handle_wait_one(mxr_thread_get_handle(thread), MX_SIGNAL_SIGNALED,
+                                 MX_TIME_INFINITE, NULL), NO_ERROR, "");
+
+    mxr_thread_destroy(thread);
+
+    // Creating a thread with a super long name should fail.
+    thread = NULL;
+    EXPECT_LT(mxr_thread_create(
+        "01234567890123456789012345678901234567890123456789012345678901234567890123456789",
+        &thread), 0, "Thread creation should have failed (name too long)");
 
     END_TEST;
 }
