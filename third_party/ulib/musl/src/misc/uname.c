0b16aaf20d327ec240206ea86cb1dd1a138815af
George Kulakowski <kulakowski@google.com>
2016-09-16 15:51:48 -0700

    [musl] Stub out uname
    For now, just claim to be a box named fuchsia running Fuchsia.
    
    Change-Id: I766bdd8444f22349a671e278a57443bfe14a7087

--- third_party/ulib/musl/src/misc/uname.c
+++ third_party/ulib/musl/src/misc/uname.c
@@ -1,6 +1,15 @@
-#include "syscall.h"
 #include <sys/utsname.h>
 
 int uname(struct utsname* uts) {
-    return syscall(SYS_uname, uts);
+    // TODO(kulakowski) At least some of this information should come
+    // from some system configuration.
+    *uts = (struct utsname){
+        .sysname = "Fuchsia",
+        .nodename = "fuchsia",
+        .release = "",
+        .version = "",
+        .machine = "",
+        .__domainname = "",
+    };
+    return 0;
 }
