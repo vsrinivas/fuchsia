2a97441bd3860ae64412e7902284b281fcbc06cc
Travis Geiselbrecht <travisg@google.com>
2016-09-20 15:02:19 -0700

    [kernel][trace][x86] add irq exit for x86
    Change-Id: I1ccb02a39202e3bae24c77e44172ae40ed6954d4

--- kernel/arch/x86/faults.c
+++ kernel/arch/x86/faults.c
@@ -343,6 +343,8 @@ void x86_exception_handler(x86_iframe_t *frame)
 
     if (ret != INT_NO_RESCHEDULE)
         thread_preempt(true);
+
+    ktrace_tiny(TAG_IRQ_EXIT, (frame->vector << 8) | arch_curr_cpu_num());
 }
 
 __WEAK uint64_t x86_64_syscall(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
