f5a5fdd635c6fafc56ca3be4ba35170473aad785
George Kulakowski <kulakowski@google.com>
2016-09-20 12:00:27 -0700

    [runtests][mxsh] Pull runtests out into its own binary
    Change-Id: Ic582bee98a1dbcef86fd43a0667737245aa63f69

---
+++ system/uapp/runtests/rules.mk
@@ -0,0 +1,19 @@
+# Copyright 2016 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userapp
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/runtests.c \
+
+MODULE_NAME := runtests
+
+MODULE_LIBS := \
+    ulib/mxio ulib/launchpad ulib/magenta ulib/musl
+
+include make/module.mk
