24f39e3ff2119e8fc8ce9f8aa3b568a4991a7d73
Viet-Trung Luu <vtl@google.com>
2016-09-20 14:02:42 -0700

    Add script to run Fuchsia.
    Change-Id: I36093f7b64c69a8450f8bde7763d4b28f305a8b6

--- paths.py
+++ paths.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
+import os.path
 
 FUCHSIA_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MAGENTA_ROOT = os.path.join(FUCHSIA_ROOT, "magenta")
---
+++ run.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Copyright 2016 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Script to run Fuchsia in a variety of ways."""
+
+
+import argparse
+import os.path
+import paths
+import subprocess
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run Fuchsia.")
+    parser.add_argument("--arch", "-a", choices=["x86-64", "arm64"],
+                        default="x86-64", help="architecture (default: x86-64)")
+    parser.add_argument("--debug", "-d", dest="debug", action="store_true",
+                        default=True, help="use debug build")
+    parser.add_argument("--release", "-r", dest="debug", action="store_false",
+                        help="use release build")
+    parser.add_argument("--graphical-console", "-g", dest="graphical",
+                        action="store_true", default=False,
+                        help="use graphical console")
+    parser.add_argument("--fuchsia-userspace", dest="fuchsia",
+                        action="store_true", default=True,
+                        help="use Fuchsia userspace")
+    parser.add_argument("--magenta-userspace", dest="fuchsia",
+                        action="store_false", help="use only Magenta userspace")
+    parser.add_argument("--boot-server", dest="bootserver", action="store_true",
+                        default=False,
+                        help="run the bootserver instead of running via qemu")
+    parser.add_argument("--verbose", dest="verbose", action="store_true",
+                        default=False, help="you like spew")
+    parser.add_argument("--dry-run", dest="dry_run", action="store_true",
+                        default=False, help="just pretend to run stuff")
+
+    args = parser.parse_args()
+
+    fuchsia_build_dir = os.path.join(paths.FUCHSIA_ROOT, "out",
+                                     ("debug" if args.debug else "release") +
+                                         "-" + args.arch)
+
+    boot_fs = (os.path.join(fuchsia_build_dir, "user.bootfs") if args.fuchsia
+                  else None)
+
+    if args.bootserver:
+        # TODO(vtl): Not sure what to use instead of "pc-" for booting on real
+        # ARM64 hardware.
+        magenta_build_dir = os.path.join(
+            paths.MAGENTA_ROOT, "build-magenta-pc-" + args.arch +
+                                    ("" if args.debug else "-release"))
+        command = [os.path.join(magenta_build_dir, "tools", "bootserver")]
+        command += [os.path.join(magenta_build_dir, "magenta.bin")]
+        if boot_fs:
+            command += [boot_fs]
+    else:
+        command = [os.path.join(paths.MAGENTA_ROOT, "scripts", "run-magenta")]
+        command += ["-a", args.arch]
+        if boot_fs:
+            command += ["-x", boot_fs]
+        if args.graphical:
+            command += ["-g"]
+
+    if args.verbose or args.dry_run:
+        print "Running: " + " ".join(command)
+
+    if args.dry_run:
+        return 0
+
+    return subprocess.call(command)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
