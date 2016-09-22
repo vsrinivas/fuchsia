1dce108aecf1befc769e9f6f5a4228f9aa3696ce
George Kulakowski <kulakowski@google.com>
2016-09-22 11:33:45 -0700

    [magenta] Move the arch register constants to defines, not enums
    This matches the style of of other places in public/magenta.
    
    Change-Id: Ie7f2cbea6f6c254190f05c3bbb361ce281dfd707

--- system/public/magenta/prctl.h
+++ system/public/magenta/prctl.h
@@ -10,23 +10,19 @@ extern "C" {
 
 #if defined(__x86_64__)
 
-enum { ARCH_SET_FS = 0,
-       ARCH_GET_FS = 1,
-       ARCH_SET_GS = 2,
-       ARCH_GET_GS = 3,
-       ARCH_GET_TSC_TICKS_PER_MS = 4 };
+#define ARCH_SET_FS 0
+#define ARCH_GET_FS 1
+#define ARCH_SET_GS 2
+#define ARCH_GET_GS 3
+#define ARCH_GET_TSC_TICKS_PER_MS 4
 
 #elif defined(__aarch64__)
 
-enum {
-    ARCH_SET_TPIDRRO_EL0 = 0,
-};
+#define ARCH_SET_TPIDRRO_EL0 0
 
 #elif defined(__arm__)
 
-enum {
-    ARCH_SET_CP15_READONLY = 0,
-};
+#define ARCH_SET_CP15_READONLY 0
 
 #else
 #error "need to define PRCTL enum for your architecture"
