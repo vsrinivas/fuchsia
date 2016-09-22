2f4eeb81de0547967bf209abc819c5fbd2dcc9a7
Tim Kilbourn <tkilbourn@google.com>
2016-09-22 12:56:56 -0700

    [system] Add ioctl wrappers for block devices
    Change-Id: I2be65249c0059299c6972c33474b0d82862b4e8f

--- system/public/magenta/device/block.h
+++ system/public/magenta/device/block.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <magenta/device/ioctl.h>
+#include <magenta/device/ioctl-wrapper.h>
 
 #define IOCTL_BLOCK_GET_SIZE \
     IOCTL(IOCTL_KIND_DEFAULT, IOCTL_FAMILY_BLOCK, 1)
@@ -17,4 +18,17 @@
 #define IOCTL_BLOCK_RR_PART \
     IOCTL(IOCTL_KIND_DEFAULT, IOCTL_FAMILY_BLOCK, 5)
 
+// ssize_t ioctl_block_get_size(int fd, uint64_t* out);
+IOCTL_WRAPPER_OUT(ioctl_block_get_size, IOCTL_BLOCK_GET_SIZE, uint64_t);
 
+// ssize_t ioctl_block_get_blocksize(int fd, uint64_t* out);
+IOCTL_WRAPPER_OUT(ioctl_block_get_blocksize, IOCTL_BLOCK_GET_BLOCKSIZE, uint64_t);
+
+// ssize_t ioctl_block_get_guid(int fd, char* out, size_t out_len);
+IOCTL_WRAPPER_VAROUT(ioctl_block_get_guid, IOCTL_BLOCK_GET_GUID, char);
+
+// ssize_t ioctl_block_get_name(int fd, char* out, size_t out_len);
+IOCTL_WRAPPER_VAROUT(ioctl_block_get_name, IOCTL_BLOCK_GET_NAME, char);
+
+// ssize_t ioctl_block_rr_part(int fd);
+IOCTL_WRAPPER(ioctl_block_rr_part, IOCTL_BLOCK_RR_PART);
