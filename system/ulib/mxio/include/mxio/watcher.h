332f36f5e236536d2a2ac438110c6e42c317e653
Jerome Poichet <jpoichet@google.com>
2016-09-27 13:30:41 -0700

    [mxio] Adding CDECLS to watcher.h
    Change-Id: I7300c23244e17c0918244b61e2b9805e9caf971a

--- system/ulib/mxio/include/mxio/watcher.h
+++ system/ulib/mxio/include/mxio/watcher.h
@@ -4,7 +4,10 @@
 
 #pragma once
 
+#include <magenta/compiler.h>
 #include <mxio/io.h>
+
+__BEGIN_CDECLS
 
 typedef struct mxio_watcher mxio_watcher_t;
 
@@ -28,4 +31,5 @@ typedef mx_status_t (*watchdir_func_t)(int dirfd, const char* fn, void* cookie);
 // new file is added to the directory, and also, first,
 // for each existing file in the directory.  If cb
 // returns non-zero, stop watching and return NO_ERROR.
-mx_status_t mxio_watch_directory(int dirfd, watchdir_func_t cb, void *cookie);
+mx_status_t mxio_watch_directory(int dirfd, watchdir_func_t cb, void* cookie);
+__END_CDECLS
