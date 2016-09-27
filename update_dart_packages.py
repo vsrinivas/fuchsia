5ed8e0fd4559387e24941cd629af65f3189ed823
P.Y. Laligand <pylaligand@google.com>
2016-09-27 13:07:46 -0700

    Configure the environment for Flutter.
    This is so that "flutter pub get" can be run by the importer.
    "pub get" alone is not sufficient anymore as some packages now have an SDK dependency on Flutter.
    
    Change-Id: I899521c808fe226054d75ae487090e7147719761

--- update_dart_packages.py
+++ update_dart_packages.py
@@ -13,14 +13,14 @@ import sys
 # error our and the conflicts will have to be resolved before the packages can
 # be updated.
 ROOT_PUBSPECS = [
-    'lib/flutter/packages/flutter',
     'lib/flutter/packages/flutter_test',
     'lib/flutter/packages/flutter_tools',
 ]
 
 
 def main():
-    flutter_bin_path = os.path.join(paths.FUCHSIA_ROOT, 'lib/flutter/bin')
+    flutter_path = os.path.join(paths.FUCHSIA_ROOT, 'lib/flutter')
+    flutter_bin_path = os.path.join(flutter_path, 'bin')
     # First, run flutter precache to seed its dart SDK and packages
     # TODO: Stop relying on flutter's copy of the 'pub' binary. Instead, we
     # should build the 'dart' binary and use it to run pub.dart
@@ -32,8 +32,10 @@ def main():
                                  'importer', 'importer.py')
     args = [importer_path]
     env = os.environ
+    env['PATH'] += ":" + flutter_bin_path
     env['PATH'] += ":" + \
         os.path.join(flutter_bin_path, 'cache', 'dart-sdk', 'bin')
+    env['FLUTTER_ROOT'] = flutter_path
     for root in ROOT_PUBSPECS:
         args.append(os.path.join(paths.FUCHSIA_ROOT, root))
     subprocess.check_call(args, env=env)
