b5b0a8ec66ff0183eb5e90a9925ffdc92e3f5ce4
John Grossman <johngro@google.com>
2016-09-27 17:25:05 -0700

    [mxtl] Add an is_pow2 test to algorithm.h
    Add a test (valid only for unsigned integers) which checks to see if a
    value is a power of 2.
    
    Change-Id: I16eff9d1dca7c3c60ac87f1307bd91693755825c

--- system/ulib/mxtl/include/mxtl/algorithm.h
+++ system/ulib/mxtl/include/mxtl/algorithm.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <mxtl/type_support.h>
+
 namespace mxtl {
 
 template<class T>
@@ -19,6 +21,36 @@ constexpr const T& max(const T& a, const T& b) {
 template<class T>
 constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
     return (v < lo) ? lo : (hi < v) ? hi : v;
+}
+
+// is_pow2
+//
+// Test to see if an unsigned integer type is an exact power of two or
+// not.  Note, this needs to use a helper struct because we are not
+// allowed to partially specialize functions (because C++).
+namespace internal {
+template <typename T, typename Enable = void> struct IsPow2Helper;
+
+template <typename T>
+struct IsPow2Helper<T, typename enable_if<is_unsigned_integer<T>::value>::type> {
+    // TODO(johngro) : should 0 be a valid power of two or not?
+    // On the one hand...
+    // lim(X -> 0) log2(X) diverges
+    //
+    // On the other hand hand...
+    // but lim(X -> -inf) 2^X = 0
+    //
+    // For now, we consider 0 to be a power of two.  Users can explicitly check
+    // for zero if they need to exclude it from consideration.
+    static constexpr bool is_pow2(T val) {
+        return (val != 0) && (((val - 1u) & val) == 0);
+    }
+};
+}
+
+template <typename T>
+constexpr bool is_pow2(T val) {
+    return internal::IsPow2Helper<T>::is_pow2(val);
 }
 
 }  // namespace mxtl
--- system/utest/mxtl/algorithm_tests.cpp
+++ system/utest/mxtl/algorithm_tests.cpp
@@ -51,10 +51,32 @@ bool clamp_test() {
     END_TEST;
 }
 
+template <typename T>
+bool is_pow2_test() {
+    BEGIN_TEST;
+
+    T val = 0;
+    EXPECT_FALSE(mxtl::is_pow2<T>(val), "");
+    EXPECT_FALSE(mxtl::is_pow2<T>(static_cast<T>(val - 1)), "");
+
+    for (val = 1u; val; val = static_cast<T>(val << 1u)) {
+        EXPECT_TRUE(mxtl::is_pow2<T>(val), "");
+        EXPECT_FALSE(mxtl::is_pow2<T>(static_cast<T>(val - 5u)), "");
+        EXPECT_FALSE(mxtl::is_pow2<T>(static_cast<T>(val + 5u)), "");
+    }
+
+    END_TEST;
+}
+
 }  // namespace
 
 BEGIN_TEST_CASE(algorithm_tests)
 RUN_NAMED_TEST("min test", min_test)
 RUN_NAMED_TEST("max test", max_test)
 RUN_NAMED_TEST("clamp test", clamp_test)
+RUN_NAMED_TEST("is_pow2<uint8_t>",  is_pow2_test<uint8_t>)
+RUN_NAMED_TEST("is_pow2<uint16_t>", is_pow2_test<uint16_t>)
+RUN_NAMED_TEST("is_pow2<uint32_t>", is_pow2_test<uint32_t>)
+RUN_NAMED_TEST("is_pow2<uint64_t>", is_pow2_test<uint64_t>)
+RUN_NAMED_TEST("is_pow2<size_t>",   is_pow2_test<size_t>)
 END_TEST_CASE(algorithm_tests);
