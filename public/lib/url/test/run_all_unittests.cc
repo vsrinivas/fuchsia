b4222bd6c7e5e7d4b8742e9bb7bdb4e45e33849d
Ian McKellar <ianloic@google.com>
2016-10-04 20:45:22 -0700

    Update ICU usage to support sharing data over mojo.
    Change-Id: I9aad8e9ea5d86972b2b400301a42be9d37d3446e

--- public/lib/url/test/run_all_unittests.cc
+++ public/lib/url/test/run_all_unittests.cc
@@ -2,9 +2,49 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "gtest/gtest.h"
+#include <array>
+#include <mojo/system/main.h>
 
-int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+#include "gtest/gtest.h"
+#include "lib/ftl/logging.h"
+#include "mojo/public/cpp/application/application_impl_base.h"
+#include "mojo/public/cpp/application/run_application.h"
+#include "mojo/public/cpp/application/service_provider_impl.h"
+#include "mojo/services/icu_data/cpp/icu_data.h"
+
+class TestApp : public mojo::ApplicationImplBase {
+ public:
+  TestApp() {}
+
+  void OnInitialize() override {
+    mojo::ApplicationConnectorPtr application_connector;
+    shell()->CreateApplicationConnector(mojo::GetProxy(&application_connector));
+
+    bool icu_success = icu_data::Initialize(application_connector.get());
+    FTL_DCHECK(icu_success);
+
+    // InitGoogleTest needs an argv.
+    std::array<const char*, 1> fake_args({{
+      "test",
+      }});
+    int argc = fake_args.size();
+    ::testing::InitGoogleTest(&argc, const_cast<char **>(fake_args.data()));
+
+    int result = RUN_ALL_TESTS();
+
+    printf("RUN_ALL_TESTS() returned %d", result);
+
+    icu_success = icu_data::Release();
+    FTL_DCHECK(icu_success);
+
+    exit(result);
+  }
+
+ private:
+  MOJO_DISALLOW_COPY_AND_ASSIGN(TestApp);
+};
+
+MojoResult MojoMain(MojoHandle application_request) {
+  TestApp app;
+  return mojo::RunApplication(application_request, &app);
 }
