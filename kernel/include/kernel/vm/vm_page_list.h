vincent100420/magenta

[kernel][vm] implement tree based page tracking per vm_object
Add a new VmPageList object and move page tracking there.

Object creation/teardown is much faster now, and objects should
be able to grow to much larger size, since the allocation to track the
pages is now dynamic.

Change-Id: Iaa30a304c83aa4a6184107f6176f2d899efd5007
Todo: move the page list nodes into a slab

kernel/include/kernel/vm/vm_page_list.h
@@ -1,0 +1,74 @@
+// Copyright 2016 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <mxtl/intrusive_wavl_tree.h>
+#include <mxtl/unique_ptr.h>
+
+struct vm_page;
+
+class VmPageListNode final : public mxtl::WAVLTreeContainable<mxtl::unique_ptr<VmPageListNode>> {
+public:
+    explicit VmPageListNode(uint64_t offset);
+    ~VmPageListNode();
+
+    // nocopy
+    VmPageListNode(const VmPageListNode&) = delete;
+    VmPageListNode& operator=(const VmPageListNode&) = delete;
+
+    static const size_t kPageFanOut = 16;
+
+    // accessors
+    uint64_t offset() const { return _obj_offset; }
+    uint64_t GetKey() const { return _obj_offset; }
+
+    // for every valid page in the node call the passed in function
+    template <typename T>
+    void ForEveryPage(T func) {
+        for (size_t i = 0; i < kPageFanOut; i++) {
+            if (_pages[i]) {
+                func(_pages[i], _obj_offset + i * PAGE_SIZE);
+            }
+        }
+    }
+
+    vm_page* GetPage(size_t index);
+    status_t AddPage(vm_page* p, size_t index);
+
+private:
+    static const uint32_t kMagic = 0x504c5354; // 'PLST'
+    uint32_t _magic = kMagic;
+
+    uint64_t _obj_offset = 0;
+    vm_page* _pages[kPageFanOut] = {};
+};
+
+class VmPageList final {
+public:
+    VmPageList();
+    ~VmPageList();
+
+    // nocopy
+    VmPageList(const VmPageList&) = delete;
+    VmPageList& operator=(const VmPageList&) = delete;
+
+    // walk the page tree, calling the passed in function on every tree node
+    template <typename T>
+    void ForEveryPage(T per_page_func) {
+        for (auto& pl : list_) {
+            pl.ForEveryPage(per_page_func);
+        }
+    }
+
+    status_t AddPage(vm_page*, uint64_t offset);
+    vm_page* GetPage(uint64_t offset);
+    status_t FreePage(uint64_t offset);
+    size_t FreeAllPages();
+
+private:
+    mxtl::WAVLTree<uint64_t, mxtl::unique_ptr<VmPageListNode>> list_;
+};

kernel/kernel/vm/vm_page_list.cpp
@@ -1,0 +1,136 @@
+// Copyright 2016 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <kernel/vm/vm_page_list.h>
+
+#include <err.h>
+#include <inttypes.h>
+#include <kernel/vm.h>
+#include <new.h>
+#include <trace.h>
+
+#include "vm_priv.h"
+
+#define LOCAL_TRACE MAX(VM_GLOBAL_TRACE, 0)
+
+VmPageListNode::VmPageListNode(uint64_t offset)
+    : _obj_offset(offset) {
+    LTRACEF("%p offset %#" PRIx64 "\n", this, _obj_offset);
+}
+
+VmPageListNode::~VmPageListNode() {
+    LTRACEF("%p offset %#" PRIx64 "\n", this, _obj_offset);
+    DEBUG_ASSERT(_magic == kMagic);
+
+    for (auto p : _pages) {
+        DEBUG_ASSERT(p == nullptr);
+    }
+    _magic = 0;
+}
+
+vm_page* VmPageListNode::GetPage(size_t index) {
+    DEBUG_ASSERT(_magic == kMagic);
+    DEBUG_ASSERT(index < kPageFanOut);
+    return _pages[index];
+}
+
+status_t VmPageListNode::AddPage(vm_page* p, size_t index) {
+    DEBUG_ASSERT(_magic == kMagic);
+    DEBUG_ASSERT(index < kPageFanOut);
+    if (_pages[index])
+        return ERR_ALREADY_EXISTS;
+    _pages[index] = p;
+    return NO_ERROR;
+}
+
+VmPageList::VmPageList() {
+    LTRACEF("%p\n", this);
+}
+
+VmPageList::~VmPageList() {
+    LTRACEF("%p\n", this);
+    DEBUG_ASSERT(list_.is_empty());
+}
+
+status_t VmPageList::AddPage(vm_page* p, uint64_t offset) {
+    uint64_t node_offset = ROUNDDOWN(offset, PAGE_SIZE * VmPageListNode::kPageFanOut);
+    size_t index = (offset >> PAGE_SIZE_SHIFT) % VmPageListNode::kPageFanOut;
+
+    LTRACEF_LEVEL(2, "%p page %p, offset %#" PRIx64 " node_offset %#" PRIx64 " index %zu\n",
+                  this, p, offset, node_offset, index);
+
+    // lookup the tree node that holds this page
+    auto pln = list_.find(node_offset);
+    if (!pln.IsValid()) {
+        AllocChecker ac;
+        mxtl::unique_ptr<VmPageListNode> pl = mxtl::unique_ptr<VmPageListNode>(new (&ac) VmPageListNode(node_offset));
+        if (!ac.check())
+            return ERR_NO_MEMORY;
+
+        LTRACEF("allocating new inner node %p\n", pl.get());
+        auto status = pl->AddPage(p, index);
+        DEBUG_ASSERT(status == NO_ERROR);
+
+        list_.insert(mxtl::move(pl));
+    } else {
+        pln->AddPage(p, index);
+    }
+
+    return NO_ERROR;
+}
+
+vm_page* VmPageList::GetPage(uint64_t offset) {
+    uint64_t node_offset = ROUNDDOWN(offset, PAGE_SIZE * VmPageListNode::kPageFanOut);
+    size_t index = (offset >> PAGE_SIZE_SHIFT) % VmPageListNode::kPageFanOut;
+
+    LTRACEF_LEVEL(2, "%p offset %#" PRIx64 " node_offset %#" PRIx64 " index %zu\n",
+                  this, offset, node_offset, index);
+
+    // lookup the tree node that holds this page
+    auto pln = list_.find(node_offset);
+    if (!pln.IsValid()) {
+        return nullptr;
+    }
+
+    return pln->GetPage(index);
+}
+
+status_t VmPageList::FreePage(uint64_t offset) {
+    LTRACEF_LEVEL(2, "%p offset %#" PRIx64 "\n", this, offset);
+
+    PANIC_UNIMPLEMENTED;
+
+    return NO_ERROR;
+}
+
+size_t VmPageList::FreeAllPages() {
+    LTRACEF("%p\n", this);
+
+    list_node list;
+    list_initialize(&list);
+
+    size_t count = 0;
+
+    // per page get a reference to the page pointer inside the page list node
+    auto per_page_func = [&](vm_page*& p, uint64_t offset) {
+        // add the page to our list and null out the inner node
+        list_add_tail(&list, &p->free.node);
+        p = nullptr;
+        count++;
+    };
+
+    // walk the tree in order, freeing all the pages on every node
+    ForEveryPage(per_page_func);
+
+    // return all the pages to the pmm at once
+    __UNUSED auto freed = pmm_free(&list);
+    DEBUG_ASSERT(freed == count);
+
+    // empty the tree
+    list_.clear();
+
+    return count;
+}

vsrinivas/fuchsia

kernel/include/kernel/vm/vm_page_list.h
@@ -1,0 +1,74 @@
+// Copyright 2016 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <mxtl/intrusive_wavl_tree.h>
+#include <mxtl/unique_ptr.h>
+
+struct vm_page;
+
+class VmPageListNode final : public mxtl::WAVLTreeContainable<mxtl::unique_ptr<VmPageListNode>> {
+public:
+    explicit VmPageListNode(uint64_t offset);
+    ~VmPageListNode();
+
+    // nocopy
+    VmPageListNode(const VmPageListNode&) = delete;
+    VmPageListNode& operator=(const VmPageListNode&) = delete;
+
+    static const size_t kPageFanOut = 16;
+
+    // accessors
+    uint64_t offset() const { return obj_offset_; }
+    uint64_t GetKey() const { return obj_offset_; }
+
+    // for every valid page in the node call the passed in function
+    template <typename T>
+    void ForEveryPage(T func) {
+        for (size_t i = 0; i < kPageFanOut; i++) {
+            if (pages_[i]) {
+                func(pages_[i], obj_offset_ + i * PAGE_SIZE);
+            }
+        }
+    }
+
+    vm_page* GetPage(size_t index);
+    status_t AddPage(vm_page* p, size_t index);
+
+private:
+    static const uint32_t kMagic = 0x504c5354; // 'PLST'
+    uint32_t magic_ = kMagic;
+
+    uint64_t obj_offset_ = 0;
+    vm_page* pages_[kPageFanOut] = {};
+};
+
+class VmPageList final {
+public:
+    VmPageList();
+    ~VmPageList();
+
+    // nocopy
+    VmPageList(const VmPageList&) = delete;
+    VmPageList& operator=(const VmPageList&) = delete;
+
+    // walk the page tree, calling the passed in function on every tree node
+    template <typename T>
+    void ForEveryPage(T per_page_func) {
+        for (auto& pl : list_) {
+            pl.ForEveryPage(per_page_func);
+        }
+    }
+
+    status_t AddPage(vm_page*, uint64_t offset);
+    vm_page* GetPage(uint64_t offset);
+    status_t FreePage(uint64_t offset);
+    size_t FreeAllPages();
+
+private:
+    mxtl::WAVLTree<uint64_t, mxtl::unique_ptr<VmPageListNode>> list_;
+};

kernel/kernel/vm/vm_page_list.cpp
@@ -1,0 +1,136 @@
+// Copyright 2016 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <kernel/vm/vm_page_list.h>
+
+#include <err.h>
+#include <inttypes.h>
+#include <kernel/vm.h>
+#include <new.h>
+#include <trace.h>
+
+#include "vm_priv.h"
+
+#define LOCAL_TRACE MAX(VM_GLOBAL_TRACE, 0)
+
+VmPageListNode::VmPageListNode(uint64_t offset)
+    : obj_offset_(offset) {
+    LTRACEF("%p offset %#" PRIx64 "\n", this, obj_offset_);
+}
+
+VmPageListNode::~VmPageListNode() {
+    LTRACEF("%p offset %#" PRIx64 "\n", this, obj_offset_);
+    DEBUG_ASSERT(magic_ == kMagic);
+
+    for (auto p : pages_) {
+        DEBUG_ASSERT(p == nullptr);
+    }
+    magic_ = 0;
+}
+
+vm_page* VmPageListNode::GetPage(size_t index) {
+    DEBUG_ASSERT(magic_ == kMagic);
+    DEBUG_ASSERT(index < kPageFanOut);
+    return pages_[index];
+}
+
+status_t VmPageListNode::AddPage(vm_page* p, size_t index) {
+    DEBUG_ASSERT(magic_ == kMagic);
+    DEBUG_ASSERT(index < kPageFanOut);
+    if (pages_[index])
+        return ERR_ALREADY_EXISTS;
+    pages_[index] = p;
+    return NO_ERROR;
+}
+
+VmPageList::VmPageList() {
+    LTRACEF("%p\n", this);
+}
+
+VmPageList::~VmPageList() {
+    LTRACEF("%p\n", this);
+    DEBUG_ASSERT(list_.is_empty());
+}
+
+status_t VmPageList::AddPage(vm_page* p, uint64_t offset) {
+    uint64_t node_offset = ROUNDDOWN(offset, PAGE_SIZE * VmPageListNode::kPageFanOut);
+    size_t index = (offset >> PAGE_SIZE_SHIFT) % VmPageListNode::kPageFanOut;
+
+    LTRACEF_LEVEL(2, "%p page %p, offset %#" PRIx64 " node_offset %#" PRIx64 " index %zu\n",
+                  this, p, offset, node_offset, index);
+
+    // lookup the tree node that holds this page
+    auto pln = list_.find(node_offset);
+    if (!pln.IsValid()) {
+        AllocChecker ac;
+        mxtl::unique_ptr<VmPageListNode> pl = mxtl::unique_ptr<VmPageListNode>(new (&ac) VmPageListNode(node_offset));
+        if (!ac.check())
+            return ERR_NO_MEMORY;
+
+        LTRACEF("allocating new inner node %p\n", pl.get());
+        auto status = pl->AddPage(p, index);
+        DEBUG_ASSERT(status == NO_ERROR);
+
+        list_.insert(mxtl::move(pl));
+    } else {
+        pln->AddPage(p, index);
+    }
+
+    return NO_ERROR;
+}
+
+vm_page* VmPageList::GetPage(uint64_t offset) {
+    uint64_t node_offset = ROUNDDOWN(offset, PAGE_SIZE * VmPageListNode::kPageFanOut);
+    size_t index = (offset >> PAGE_SIZE_SHIFT) % VmPageListNode::kPageFanOut;
+
+    LTRACEF_LEVEL(2, "%p offset %#" PRIx64 " node_offset %#" PRIx64 " index %zu\n",
+                  this, offset, node_offset, index);
+
+    // lookup the tree node that holds this page
+    auto pln = list_.find(node_offset);
+    if (!pln.IsValid()) {
+        return nullptr;
+    }
+
+    return pln->GetPage(index);
+}
+
+status_t VmPageList::FreePage(uint64_t offset) {
+    LTRACEF_LEVEL(2, "%p offset %#" PRIx64 "\n", this, offset);
+
+    PANIC_UNIMPLEMENTED;
+
+    return NO_ERROR;
+}
+
+size_t VmPageList::FreeAllPages() {
+    LTRACEF("%p\n", this);
+
+    list_node list;
+    list_initialize(&list);
+
+    size_t count = 0;
+
+    // per page get a reference to the page pointer inside the page list node
+    auto per_page_func = [&](vm_page*& p, uint64_t offset) {
+        // add the page to our list and null out the inner node
+        list_add_tail(&list, &p->free.node);
+        p = nullptr;
+        count++;
+    };
+
+    // walk the tree in order, freeing all the pages on every node
+    ForEveryPage(per_page_func);
+
+    // return all the pages to the pmm at once
+    __UNUSED auto freed = pmm_free(&list);
+    DEBUG_ASSERT(freed == count);
+
+    // empty the tree
+    list_.clear();
+
+    return count;
+}