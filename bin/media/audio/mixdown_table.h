c944aacef51614960fccee27309b0f170891034b
Dale Sather <dalesat@google.com>
2016-10-13 14:03:00 -0700

    apps/media: add MixdownTable type
    A MixdownTable specifies mix levels for each input/output channel
    pair on a mixer input. It's basically just an MxN array of Level
    values where M is the number of input channels and N is the number
    of output channels. MixdownTables are mutable for convenience in
    creating new tables. Once submitted to the mixer, however, they
    won't be mutated. The creation static methods, therefore, return
    std::unique_ptr<MixdownTable>.
    
    We'll probably have a similar type that's an array of Gain values.
    That will come in a later CL.
    
    Change-Id: I50bca30418ba2e24fb2722b547bcee92659a4253

---
+++ bin/media/audio/mixdown_table.h
@@ -0,0 +1,109 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <iterator>
+#include <memory>
+
+#include "apps/media/src/audio/level.h"
+#include "lib/ftl/logging.h"
+
+namespace mojo {
+namespace media {
+
+// A mutable rectangular matrix of Levels indicating how input channels should
+// be mixed to ouput channels.
+template <typename TLevel>
+class MixdownTable {
+ public:
+  class iterator
+      : public std::iterator<std::forward_iterator_tag, Level<TLevel>> {
+   public:
+    explicit iterator(Level<TLevel>* ptr) : ptr_(ptr) {}
+    iterator& operator++() {
+      ++ptr_;
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator retval = *this;
+      ++ptr_;
+      return retval;
+    }
+    bool operator==(iterator other) const { return ptr_ == other.ptr_; }
+    bool operator!=(iterator other) const { return !(*this == other); }
+    Level<TLevel>& operator*() const { return *ptr_; }
+    Level<TLevel>& operator->() const { return *ptr_; }
+
+    Level<TLevel>* ptr_;
+  };
+
+  // Creates a mixdown table that produces silence on all output channels.
+  static std::unique_ptr<MixdownTable<TLevel>> CreateSilent(
+      uint32_t in_channel_count,
+      uint32_t out_channel_count) {
+    return std::make_unique<MixdownTable>(in_channel_count, out_channel_count);
+  }
+
+  // Creates a square mixdown table that passes input channels through to
+  // respective output channels with a change in level.
+  static std::unique_ptr<MixdownTable<TLevel>> CreateLevelChange(
+      uint32_t channel_count,
+      Level<TLevel> level) {
+    std::unique_ptr<MixdownTable> table =
+        CreateSilent(channel_count, channel_count);
+
+    for (uint32_t channel = 0; channel < channel_count; ++channel) {
+      table->get_level(channel, channel) = level;
+    }
+
+    return table;
+  }
+
+  // Creates a square mixdown table that passes input channels through to
+  // respective output channels unchanged.
+  static std::unique_ptr<MixdownTable<TLevel>> CreatePassthrough(
+      uint32_t channel_count) {
+    return CreateLevelChange(channel_count, Level<TLevel>::Unity);
+  }
+
+  MixdownTable(uint32_t in_channel_count, uint32_t out_channel_count)
+      : in_channel_count_(in_channel_count),
+        out_channel_count_(out_channel_count),
+        levels_(new Level<TLevel>[in_channel_count * out_channel_count]) {}
+
+  // Returns the number of input channels.
+  uint32_t in_channel_count() { return in_channel_count_; }
+
+  // Returns the number of output channels.
+  uint32_t out_channel_count() { return out_channel_count_; }
+
+  // Gets a non-const reference to the level applied to in_channel when mixing
+  // to out_channel.
+  Level<TLevel>& get_level(uint32_t in_channel, uint32_t out_channel) {
+    FTL_DCHECK(in_channel < in_channel_count_);
+    FTL_DCHECK(out_channel < out_channel_count_);
+    // Level indexing needs to happen such that when iterating across all levels
+    // with an outer out_channel loop and an inner in_channel loop, simply
+    // incrementing a Level pointer is adequate.
+    return levels_[in_channel + out_channel * in_channel_count_];
+  }
+
+  // Returns a forward iterator. All levels in the table can be enumerated by
+  // incrementing this iterator in an outer loop of output channels and an inner
+  // loop of input channels.
+  iterator begin() { return iterator(&levels_[0]); }
+
+  iterator end() {
+    return iterator(&levels_[in_channel_count_ * out_channel_count_]);
+  }
+
+ private:
+  uint32_t in_channel_count_;
+  uint32_t out_channel_count_;
+  std::unique_ptr<Level<TLevel>[]> levels_;
+};
+
+}  // namespace media
+}  // namespace mojo
---
+++ bin/media/audio/test/mixdown_table_test.cc
@@ -0,0 +1,116 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "apps/media/src/audio/mixdown_table.h"
+
+#include "gtest/gtest.h"
+
+namespace mojo {
+namespace media {
+namespace {
+
+// Tests expectations regarding silent mixdown tables.
+TEST(MixdownTableTest, Silent) {
+  static constexpr uint32_t kInChannelCount = 3;
+  static constexpr uint32_t kOutChannelCount = 4;
+
+  std::unique_ptr<MixdownTable<float>> under_test =
+      MixdownTable<float>::CreateSilent(kInChannelCount, kOutChannelCount);
+
+  EXPECT_EQ(kInChannelCount, under_test->in_channel_count());
+  EXPECT_EQ(kOutChannelCount, under_test->out_channel_count());
+
+  for (uint32_t out_channel = 0; out_channel < kOutChannelCount;
+       ++out_channel) {
+    for (uint32_t in_channel = 0; in_channel < kInChannelCount; ++in_channel) {
+      // Level is initially silent.
+      EXPECT_EQ(Level<float>::Silence,
+                under_test->get_level(in_channel, out_channel));
+
+      // When set to unity, the level remains at unity.
+      under_test->get_level(in_channel, out_channel) = Level<float>::Unity;
+      EXPECT_EQ(Level<float>::Unity,
+                under_test->get_level(in_channel, out_channel));
+    }
+  }
+}
+
+// Tests expectations regarding level change mixdown tables.
+TEST(MixdownTableTest, LevelChange) {
+  static constexpr uint32_t kChannels = 3;
+  static const Level<float> kLevel = Level<float>(2.0f);
+
+  std::unique_ptr<MixdownTable<float>> under_test =
+      MixdownTable<float>::CreateLevelChange(kChannels, kLevel);
+
+  EXPECT_EQ(kChannels, under_test->in_channel_count());
+  EXPECT_EQ(kChannels, under_test->out_channel_count());
+
+  for (uint32_t out_channel = 0; out_channel < kChannels; ++out_channel) {
+    for (uint32_t in_channel = 0; in_channel < kChannels; ++in_channel) {
+      if (out_channel == in_channel) {
+        // Levels on the diagonal are kLevel.
+        EXPECT_EQ(kLevel, under_test->get_level(in_channel, out_channel));
+      } else {
+        // Levels not the diagonal are silent.
+        EXPECT_EQ(Level<float>::Silence,
+                  under_test->get_level(in_channel, out_channel));
+      }
+    }
+  }
+}
+
+// Tests expectations regarding passthrough mixdown tables.
+TEST(MixdownTableTest, Passthrough) {
+  static constexpr uint32_t kChannels = 3;
+
+  std::unique_ptr<MixdownTable<float>> under_test =
+      MixdownTable<float>::CreatePassthrough(kChannels);
+
+  EXPECT_EQ(kChannels, under_test->in_channel_count());
+  EXPECT_EQ(kChannels, under_test->out_channel_count());
+
+  for (uint32_t out_channel = 0; out_channel < kChannels; ++out_channel) {
+    for (uint32_t in_channel = 0; in_channel < kChannels; ++in_channel) {
+      if (out_channel == in_channel) {
+        // Levels on the diagonal are unity.
+        EXPECT_EQ(Level<float>::Unity,
+                  under_test->get_level(in_channel, out_channel));
+      } else {
+        // Levels not the diagonal are silent.
+        EXPECT_EQ(Level<float>::Silence,
+                  under_test->get_level(in_channel, out_channel));
+      }
+    }
+  }
+}
+
+// Test that simple increment can be used to enumerate levels.
+TEST(MixdownTableTest, Iteration) {
+  static constexpr uint32_t kInChannelCount = 7;
+  static constexpr uint32_t kOutChannelCount = 2;
+
+  std::unique_ptr<MixdownTable<float>> under_test =
+      MixdownTable<float>::CreateSilent(kInChannelCount, kOutChannelCount);
+
+  EXPECT_EQ(kInChannelCount, under_test->in_channel_count());
+  EXPECT_EQ(kOutChannelCount, under_test->out_channel_count());
+
+  auto iter = under_test->begin();
+
+  for (uint32_t out_channel = 0; out_channel < kOutChannelCount;
+       ++out_channel) {
+    for (uint32_t in_channel = 0; in_channel < kInChannelCount; ++in_channel) {
+      // Level is initially silent.
+      EXPECT_EQ(&under_test->get_level(in_channel, out_channel), &(*iter));
+      ++iter;
+    }
+  }
+
+  EXPECT_EQ(under_test->end(), iter);
+}
+
+}  // namespace
+}  // namespace media
+}  // namespace mojo
