5316c122ebb7d45625be80765cc2f9673ea93e59
Sean Klein <smklein@google.com>
2016-10-16 14:39:10 -0700

    build: Style fixes
    Change-Id: I9dd65226d1efeab9cc40f63ad2f05735edf4acf1

--- go/src/thinfs/scripts/build_fuchsia.py
+++ go/src/thinfs/scripts/build_fuchsia.py
@@ -13,25 +13,25 @@ from subprocess import call
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--fuchsia_root', help='Path to root of Fuchsia project',
-                      required=True)
-  parser.add_argument('--go_binary', help='Path to go binary which can compile Fuchsia programs',
-                      required=True)
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fuchsia-root', help='Path to root of Fuchsia project',
+                        required=True)
+    parser.add_argument('--go-binary', help='Path to go binary which can compile Fuchsia programs',
+                        required=True)
+    args = parser.parse_args()
 
-  scripts_dir = os.path.dirname(os.path.realpath(__file__))
-  os.environ['CGO_ENABLED'] = '1'
-  os.environ['GOPATH'] = os.path.join(args.fuchsia_root, 'go')
-  # TODO(smklein): Remove this usage of gccwrap once we've converted to building
-  # the go runtime with clangwrap.
-  os.environ['MAGENTA'] = os.path.join(args.fuchsia_root, 'magenta')
-  os.environ['CC'] = os.path.join(args.fuchsia_root, 'third_party/go/misc/fuchsia/gccwrap.sh')
-  os.environ['GOOS'] = 'fuchsia'
-  output = 'thinfs'
-  target = os.path.join(scripts_dir, '../magenta/main.go')
-  return call([args.go_binary, 'build', '-o', output, target], env=os.environ)
+    scripts_dir = os.path.dirname(os.path.realpath(__file__))
+    os.environ['CGO_ENABLED'] = '1'
+    os.environ['GOPATH'] = os.path.join(args.fuchsia_root, 'go')
+    # TODO(smklein): Remove this usage of gccwrap once we've converted to building
+    # the go runtime with clangwrap.
+    os.environ['MAGENTA'] = os.path.join(args.fuchsia_root, 'magenta')
+    os.environ['CC'] = os.path.join(args.fuchsia_root, 'third_party/go/misc/fuchsia/gccwrap.sh')
+    os.environ['GOOS'] = 'fuchsia'
+    output = 'thinfs'
+    target = os.path.join(scripts_dir, '../magenta/main.go')
+    return call([args.go_binary, 'build', '-o', output, target], env=os.environ)
 
 
 if __name__ == '__main__':
-      main()
+    main()
