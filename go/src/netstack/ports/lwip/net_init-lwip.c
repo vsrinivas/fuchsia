8201898c79df12812e1c3de83df8cd889718c708
Toshi Kikuchi <toshik@google.com>
2016-10-16 11:47:29 -0700

    lwip: don't wait for dhcp during initialization
    dhcp client will run in background. when an IP address
    is acquired, it is printed on console.
    
    Change-Id: I2be3890791353af85f94c2cd4e9953e20a4d51ef

--- go/src/netstack/ports/lwip/net_init-lwip.c
+++ go/src/netstack/ports/lwip/net_init-lwip.c
@@ -4,14 +4,13 @@
 
 #include "third_party/lwip/ports/fuchsia/include/netif/ethernetif.h"
 #include "third_party/lwip/src/include/lwip/dhcp.h"
+#include "third_party/lwip/src/include/lwip/inet.h"
 #include "third_party/lwip/src/include/lwip/netif.h"
 #include "third_party/lwip/src/include/lwip/tcpip.h"
 
 // initialize a lwip network interface
 
 static struct netif netif;
-static sys_mutex_t mutex_netif_status;
-static sys_sem_t sem_dhcp;
 
 // the callback to signal sem_tcpip_done
 static void lwip_tcpip_init_done(void *arg) {
@@ -20,19 +19,43 @@ static void lwip_tcpip_init_done(void *arg) {
   sys_sem_signal(sem);
 }
 
-static int lwip_netif_has_ip4_addr(void) {
-  return !ip_addr_isany(&netif.ip_addr);
+static int print_ip4_addr(void) {
+  if (!ip_addr_isany(&netif.ip_addr)) {
+    char ip_addr[INET_ADDRSTRLEN];
+    char netmask[INET_ADDRSTRLEN];
+    char gw[INET_ADDRSTRLEN];
+    ipaddr_ntoa_r(&netif.ip_addr, ip_addr, sizeof(ip_addr));
+    ipaddr_ntoa_r(&netif.netmask, netmask, sizeof(netmask));
+    ipaddr_ntoa_r(&netif.gw, gw, sizeof(gw));
+    printf("ip4_addr: %s netmask: %s gw: %s\n", ip_addr, netmask, gw);
+    return 1;
+  }
+  return 0;
 }
 
-// the callback to signal sem_dhcp
+static void print_ip6_addr(int *printed_bits, int idx) {
+  if (netif.ip6_addr_state[idx] & IP6_ADDR_PREFERRED) {
+    char ip6_addr[INET6_ADDRSTRLEN];
+    printf("ip6_addr[%d]: %s\n", idx,
+           ip6addr_ntoa_r(netif_ip6_addr(&netif, idx), ip6_addr,
+                          sizeof(ip6_addr)));
+    *printed_bits = (1 << idx);
+  }
+}
+
+// the callback called every time the netif status changes
 static void lwip_netif_status_callback(struct netif *netif) {
-  sys_mutex_lock(&mutex_netif_status);
-  if (sys_sem_valid(&sem_dhcp) && lwip_netif_has_ip4_addr())
-    sys_sem_signal(&sem_dhcp);
-  sys_mutex_unlock(&mutex_netif_status);
+  static int ip4_addr_printed = 0;
+  if (!ip4_addr_printed) ip4_addr_printed = print_ip4_addr();
+
+  static int ip6_addr_printed_bits = 0;
+  for (int i = 0; i < LWIP_IPV6_NUM_ADDRESSES; i++) {
+    if (!(ip6_addr_printed_bits & (1 << i)))
+      print_ip6_addr(&ip6_addr_printed_bits, i);
+  }
 }
 
-static int lwip_netif_init(unsigned int timeout_in_msec) {
+static int lwip_netif_init(void) {
   sys_sem_t sem_tcpip_done;
 
   // initialize tcpip in lwip
@@ -50,8 +73,6 @@ static int lwip_netif_init(unsigned int timeout_in_msec) {
 
   // create an network interface and run dhcp
 
-  // create a mutex to access netif data
-  sys_mutex_new(&mutex_netif_status);
   // add an interface without passing ip_address/netmask/gateway
   netif_add(&netif, NULL, NULL, NULL, NULL, ethernetif_init, tcpip_input);
   // make it the default interface
@@ -61,43 +82,20 @@ static int lwip_netif_init(unsigned int timeout_in_msec) {
 
   // turn on the interface
   netif_set_up(&netif);
+
+  // create ipv6 linklocal address
   netif_create_ip6_linklocal_address(&netif, 1);
 
-  // create a semaphore to wait for dhcp
-  if (sys_sem_new(&sem_dhcp, 0) != ERR_OK) {
-    LWIP_ASSERT("failed to create semaphore", 0);
-  }
   // start dhcp
   // TODO: handle lease renewal
   dhcp_start(&netif);
 
-  // block until an IP address is assigned by dhcp
-  // will timeout if an IP address is not assigned after 'timeout_in_msec'
-  // milliseconds
-  int ret = 0;
-  if (sys_arch_sem_wait(&sem_dhcp, timeout_in_msec) == SYS_ARCH_TIMEOUT)
-    ret = -1;
-
-  sys_mutex_lock(&mutex_netif_status);
-  // free the semaphore
-  sys_sem_free(&sem_dhcp);
-  // invalid the semaphore (see lwip_netif_status_changed() checks this)
-  sys_sem_set_invalid(&sem_dhcp);
-  sys_mutex_unlock(&mutex_netif_status);
-
-  return ret;
+  return 0;
 }
 
 int net_init(void) {
 #if defined(NETSTACK_DEBUG)
   lwip_debug_flags = LWIP_DBG_ON;
 #endif
-  int ret = lwip_netif_init(5000);
-  if (ret == 0) {
-    // IP address is assigned by dhcp
-    char ip4_addr[IPADDR_STRLEN_MAX];
-    ipaddr_ntoa_r(&netif.ip_addr, ip4_addr, IPADDR_STRLEN_MAX);
-    printf("netstack: ip4_addr: %s\n", ip4_addr);
-  }
-  return ret;
+  return lwip_netif_init();
 }
