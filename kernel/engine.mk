2741ac0b465782d5f3e20c9fca6fe2a26d985317
Viet-Trung Luu <vtl@google.com>
2016-10-18 07:57:37 -0700

    [build] Remove DEPRECATE_COMPAT_SYSCALLS define flag.
    This macro is no longer used.
    
    Change-Id: If47c868ca1f2594fbe0e68978ad597ba00d41a43

--- kernel/engine.mk
+++ kernel/engine.mk
@@ -114,7 +114,6 @@ KERNEL_ASMFLAGS :=
 
 # User space compile flags
 USER_COMPILEFLAGS := -include $(USER_CONFIG_HEADER) -fPIC -D_ALL_SOURCE=1
-USER_COMPILEFLAGS += -DDEPRECATE_COMPAT_SYSCALLS=1
 #TODO: remove once userspace backtracing is smarter
 USER_COMPILEFLAGS += -fno-omit-frame-pointer
 USER_CFLAGS :=
