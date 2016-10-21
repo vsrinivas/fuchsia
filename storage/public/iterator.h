0c616c72f6b732e4480864bf467bc74a404d0d0d
Etienne Membrives <etiennej@google.com>
2016-10-21 12:06:27 +0200

    Improve/fix the Iterator documentation
    Change-Id: I9a220be4aa36be5474dc741b0b4bb7e7f009b131

--- storage/public/iterator.h
+++ storage/public/iterator.h
@@ -17,13 +17,16 @@ class Iterator {
   Iterator() {}
   virtual ~Iterator() {}
 
-  // Advances to the next element in the collection.
+  // Advances to the next element in the collection. Should only be called on a
+  // valid iterator.
   virtual Iterator<T>& Next() = 0;
-  // Returns true iff no elements are available in the collection. It is
-  // invalid to dereference this iterator if Valid() returns true.
+  // Returns false if no elements are available in the collection, or an error
+  // occurred. It is invalid to dereference this iterator or to call |Next()| if
+  // Valid() returns false.
   virtual bool Valid() const = 0;
-  // Returns the current status of the iterator. If Valid() is false, indicates
-  // why the iterator is not valid.
+  // Returns the current status of the iterator. It returns the error status if
+  // an error occurred, Status::OK otherwise. Reaching the end of the iterator
+  // is not an error.
   virtual Status GetStatus() const = 0;
 
   virtual T& operator*() const = 0;
