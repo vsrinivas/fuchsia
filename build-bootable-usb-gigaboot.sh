49ea9b1a8eeac83a688dd57f9d4a650741c568f2
Sean Klein <smklein@google.com>
2016-10-20 18:25:03 -0700

    Updated gigaboot USB building script to be more resilient
    - Quoting bash variables
    - Requsting 'sudo' permission with explicit prompt
    - Umounting even when script fails
    
    This patch does not yet attach command line arguments, since I want to
    be extremely cautious about providing mechanisms for users of this
    script to "automatically" wipe block devices. This script is intended
    for interactivity, not automation.
    
    Change-Id: I67755306f780819c56b3f53f33a5c9c487212819

--- build-bootable-usb-gigaboot.sh
+++ build-bootable-usb-gigaboot.sh
@@ -8,7 +8,7 @@ set -eu
 get_confirmation() {
   echo -n "Press 'y' to confirm: "
   read CONFIRM
-  if [[ $CONFIRM != "y" ]]; then
+  if [[ "$CONFIRM" != "y" ]]; then
     echo "[format_usb] Aborted due to invalid confirmation"
     exit 1
   fi
@@ -20,13 +20,10 @@ if [[ $OSTYPE != "linux-gnu" ]]; then
 fi
 
 SCRIPT_DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd)
-FUCHSIA_DIR=$SCRIPT_DIR/..
+FUCHSIA_DIR="$SCRIPT_DIR/.."
 
-# Ensure the bootloader and magenta have been built prior to formatting USB
-pushd $FUCHSIA_DIR/magenta/bootloader > /dev/null
-make
-popd > /dev/null
-pushd $FUCHSIA_DIR/magenta > /dev/null
+# Ensure Magenta has been built prior to formatting USB
+pushd "$FUCHSIA_DIR/magenta" > /dev/null
 ./scripts/build-magenta-x86-64
 popd > /dev/null
 
@@ -58,8 +55,8 @@ echo " SUCCESS"
 
 # Try to check that the device is a USB stick
 echo -n "[format_usb] Checking if device is USB: $DEVICE ..."
-READLINK_USB=$(readlink -f /sys/class/block/$DEVICE/device | { grep -i "usb" || true; })
-if [[ -z $READLINK_USB ]]; then
+READLINK_USB=$(readlink -f "/sys/class/block/$DEVICE/device" | { grep -i "usb" || true; })
+if [[ -z "$READLINK_USB" ]]; then
   echo " FAILED"
   echo "[format_usb] ERROR: Cannot confirm that device is a USB stick"
   echo "[format_usb] ERROR: Please insert USB stick and retry"
@@ -81,44 +78,58 @@ fi
 echo " SUCCESS"
 
 # Confirm that the user knows what they are doing
-echo "[format_usb] (sudo) Getting info about: $DEVICE_PATH"
-sudo sgdisk -p $DEVICE_PATH
+sudo -v -p "[sudo] Enter password to confirm information about device: "
+sudo sgdisk -p "$DEVICE_PATH"
 echo "[format_usb] ABOUT TO COMPLETELY WIPE / FORMAT: $DEVICE_PATH"
 get_confirmation
 echo "[format_usb] ARE YOU 100% SURE?"
 get_confirmation
 
 echo "[format_usb] Deleting all partition info on USB, creating new GPT"
-sudo sgdisk -og $DEVICE_PATH
+sudo sgdisk -og "$DEVICE_PATH"
 
-SECTOR_SIZE=`cat /sys/block/$DEVICE/queue/hw_sector_size`
+SECTOR_SIZE=`cat "/sys/block/$DEVICE/queue/hw_sector_size"`
 echo "[format_usb] Creating 1GB EFI System Partition"
-sudo sgdisk -n 1:0:1GB -c 1:"EFI System Partition" -t 1:ef00 $DEVICE_PATH
-EFI_PARTITION_PATH=${DEVICE_PATH}1
-sudo mkfs.vfat $EFI_PARTITION_PATH
+sudo sgdisk -n 1:0:1GB -c 1:"EFI System Partition" -t 1:ef00 "$DEVICE_PATH"
+EFI_PARTITION_PATH="${DEVICE_PATH}1"
+sudo mkfs.vfat "$EFI_PARTITION_PATH"
 echo "[format_usb] Creating FAT data partition"
-sudo sgdisk -n 2:0:0 -c 2:"FAT Partition" -t2:0700 $DEVICE_PATH
-DATA_PARTITION_PATH=${DEVICE_PATH}2
-sudo mkfs.vfat $DATA_PARTITION_PATH
+sudo sgdisk -n 2:0:0 -c 2:"FAT Partition" -t2:0700 "$DEVICE_PATH"
+DATA_PARTITION_PATH="${DEVICE_PATH}2"
+sudo mkfs.vfat "$DATA_PARTITION_PATH"
+
+# Function to attempt unmounting a mount point up to three times, sleeping
+# a couple of seconds between attempts.
+function umount_retry() {
+  set +e
+  TRIES=0
+  while (! sudo umount $1); do
+    ((TRIES++))
+    if [[ ${TRIES} > 2 ]]; then
+      echo "[format_usb] Unable to umount $0"
+      exit 1
+    fi
+    sleep 2
+  done
+  set -e
+}
 
 MOUNT_PATH=`mktemp -d`
-sudo mount $EFI_PARTITION_PATH $MOUNT_PATH
-sudo mkdir -p ${MOUNT_PATH}/EFI/BOOT
+sudo mount "$EFI_PARTITION_PATH" "$MOUNT_PATH"
+trap "umount_retry \"${MOUNT_PATH}\" && rm -rf \"${MOUNT_PATH}\" && echo \"Unmounted succesfully\"" INT TERM EXIT
+
+sudo mkdir -p "${MOUNT_PATH}/EFI/BOOT"
 echo -n "Copying Bootloader..."
-sudo cp $FUCHSIA_DIR/magenta/bootloader/out/osboot.efi ${MOUNT_PATH}/EFI/BOOT/BOOTX64.EFI
+sudo cp "$FUCHSIA_DIR/magenta/bootloader/out/osboot.efi" "${MOUNT_PATH}/EFI/BOOT/BOOTX64.EFI"
 echo " SUCCESS"
 echo -n "Copying magenta.bin..."
-sudo cp $FUCHSIA_DIR/magenta/build-magenta-pc-x86-64/magenta.bin ${MOUNT_PATH}/magenta.bin
+sudo cp "$FUCHSIA_DIR/magenta/build-magenta-pc-x86-64/magenta.bin" "${MOUNT_PATH}/magenta.bin"
 echo " SUCCESS"
 echo -n "Copying user.bootfs..."
-sudo cp $FUCHSIA_DIR/out/debug-x86-64/user.bootfs ${MOUNT_PATH}/ramdisk.bin
+sudo cp "$FUCHSIA_DIR/out/debug-x86-64/user.bootfs" "${MOUNT_PATH}/ramdisk.bin"
 echo " SUCCESS"
-echo -n "Syncing / Unmounting EFI partition (this may take a minute)..."
-pushd $MOUNT_PATH > /dev/null
+echo -n "Syncing EFI partition (this may take a minute)..."
+pushd "$MOUNT_PATH" > /dev/null
 sync
 popd > /dev/null
-sudo umount -l $MOUNT_PATH
 echo " SUCCESS"
-rmdir $MOUNT_PATH
-
-echo "USB Formatted Successfully. Ready to boot!"
