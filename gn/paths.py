6bcead38034d28b5c1186bd63b5033b0a04c3af2
Petr Hosek <phosek@google.com>
2016-10-25 15:12:53 -0700

    Use the updated mkbootfs path
    Use the mkbootfs which is built as a part of sysroot build.
    
    Change-Id: I87bb74d92d9742f28c3459bd78d6e9300df4b3e2

--- gn/paths.py
+++ gn/paths.py
@@ -8,7 +8,7 @@ import os
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 FUCHSIA_ROOT = os.path.abspath(os.path.join(SCRIPT_DIR, os.pardir, os.pardir))
 GN_PATH = os.path.join(FUCHSIA_ROOT, "buildtools", "gn")
-MKBOOTFS_PATH = os.path.join(FUCHSIA_ROOT, "buildtools", "mkbootfs")
+MKBOOTFS_PATH = os.path.join(FUCHSIA_ROOT, "out", "build-magenta", "tools", "mkbootfs")
 TOOLCHAIN_PATH = os.path.join(FUCHSIA_ROOT, "buildtools", "toolchain")
 DEBUG_OUT_DIR = os.path.join(FUCHSIA_ROOT, "out", "debug-x86-64")
 RELEASE_OUT_DIR = os.path.join(FUCHSIA_ROOT, "out", "release-x86-64")
