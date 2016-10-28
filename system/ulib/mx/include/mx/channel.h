902451f4cd6560289f43ce4ab32d30f0dc7a67a3
Brian Swetland <swetland@google.com>
2016-10-28 00:31:46 -0700

    [mx][syscalls] msgpipe -> channel
    Change-Id: Icda8bca5d7365784879fca42aadd259d0db8c8a7

--- system/ulib/mx/include/mx/channel.h
+++ system/ulib/mx/include/mx/channel.h
@@ -26,19 +26,13 @@ public:
     mx_status_t read(uint32_t flags,
                      void* bytes, uint32_t num_bytes, uint32_t* actual_bytes,
                      mx_handle_t* handles, uint32_t num_handles, uint32_t* actual_handles) const {
-        uint32_t nb = num_bytes;
-        uint32_t nh = num_handles;
-        mx_status_t result = mx_msgpipe_read(get(), bytes, &nb, handles, &nh, flags);
-        if (actual_bytes)
-            *actual_bytes = nb;
-        if (actual_handles)
-            *actual_handles = nh;
-        return result;
+        return mx_channel_read(get(), flags, bytes, num_bytes, actual_bytes,
+                               handles, num_handles, actual_handles);
     }
 
     mx_status_t write(uint32_t flags, const void* bytes, uint32_t num_bytes,
                       const mx_handle_t* handles, uint32_t num_handles) const {
-        return mx_msgpipe_write(get(), bytes, num_bytes, handles, num_handles, flags);
+        return mx_channel_write(get(), flags, bytes, num_bytes, handles, num_handles);
     }
 };
 
