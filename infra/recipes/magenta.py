ca3165dd23671d22fd91917c442be995e7fe79c0
Petr Hosek <phosek@google.com>
2016-10-30 15:19:35 -0700

    [infra] Update the recipe properties
    This reflects the recent changes in the name of the properties
    passed to the recipe by the CQ.
    
    Change-Id: I89ba7d04f49ce688562723149941568eed756f68

--- infra/recipes/magenta.py
+++ infra/recipes/magenta.py
@@ -19,41 +19,33 @@ DEPS = [
 TARGETS = [ 'magenta-qemu-arm32', 'magenta-qemu-arm64', 'magenta-pc-x86-64' ]
 
 PROPERTIES = {
-    'gerrit': Property(kind=str, help='Gerrit host', default=None,
-                     param_name='gerrit_host'),
-    'patch_project': Property(kind=str, help='Gerrit project', default=None,
-                              param_name='gerrit_project'),
-    'event.patchSet.ref': Property(kind=str, help='Gerrit patch ref',
-                                   default=None, param_name='gerrit_patch_ref'),
-    'repository': Property(kind=str, help='Full url to a Git repository',
-                           default=None, param_name='repo_url'),
-    'refspec': Property(kind=str, help='Refspec to checkout', default='master'),
     'category': Property(kind=str, help='Build category', default=None),
+    'patch_gerrit_url': Property(kind=str, help='Gerrit host', default=None),
+    'patch_project': Property(kind=str, help='Gerrit project', default=None),
+    'patch_ref': Property(kind=str, help='Gerrit patch ref', default=None),
+    'patch_storage': Property(kind=str, help='Patch location', default=None),
+    'patch_repository_url': Property(kind=str, help='URL to a Git repository',
+                              default=None),
     'manifest': Property(kind=str, help='Jiri manifest to use'),
     'remote': Property(kind=str, help='Remote manifest repository'),
     'target': Property(kind=str, help='Target to build'),
 }
 
 
-def RunSteps(api, category, repo_url, refspec, gerrit_host, gerrit_project,
-             gerrit_patch_ref, manifest, remote, target):
-    if category == 'cq':
-        assert gerrit_host.startswith('https://')
-        repo_url = '%s/%s' % (gerrit_host.rstrip('/'), gerrit_project)
-        refspec = gerrit_patch_ref
-
-    assert repo_url and refspec, 'repository url and refspec must be given'
-    assert repo_url.startswith('https://')
+def RunSteps(api, category, patch_gerrit_url, patch_project, patch_ref,
+             patch_storage, patch_repository_url, manifest, remote, target):
+    assert target in TARGETS, 'unsupported target'
 
     api.jiri.ensure_jiri()
+
     api.jiri.set_config('magenta')
 
     api.jiri.init()
     api.jiri.clean_project()
     api.jiri.import_manifest(manifest, remote, overwrite=True)
     api.jiri.update(gc=True)
-    if category == 'cq':
-        api.jiri.patch(gerrit_patch_ref, host=gerrit_host, delete=True, force=True)
+    if patch_ref is not None:
+        api.jiri.patch(patch_ref, host=patch_gerrit_url, delete=True, force=True)
 
     assert 'checkout' not in api.path
     api.path['checkout'] = api.path['slave_build'].join('magenta')
@@ -66,16 +58,14 @@ def RunSteps(api, category, repo_url, refspec, gerrit_host, gerrit_project,
 
 
 def GenTests(api):
-    yield api.test('scheduler') + api.properties(
-        repository='https://fuchsia.googlesource.com/magenta',
-        refspec='master',
+    yield api.test('ci') + api.properties(
         manifest='magenta',
         remote='https://fuchsia.googlesource.com/manifest',
         target='magenta-pc-x86-64',
     )
-    yield api.test('cq') + api.properties.tryserver_gerrit(
-        'magenta',
-        gerrit_host='https://fuchsia-review.googlesource.com',
+    yield api.test('cq_try') + api.properties.tryserver(
+        gerrit_project='magenta',
+        patch_gerrit_url='fuchsia-review.googlesource.com',
         manifest='magenta',
         remote='https://fuchsia.googlesource.com/manifest',
         target='magenta-pc-x86-64',
