f437ed943cf023781c82522ab6b9c67625a7cf02
Jeff Brown <jeffbrown@google.com>
2016-10-31 17:17:07 -0700

    Delete magenta/process.h.
    Change-Id: Ia8fb43eb400c8014b29b258784d84e567b48ad09

--- third_party/ulib/musl/include/magenta/process.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#pragma once
-
-#include <magenta/types.h>
-
-// TODO: remove all uses of this file then delete it
