7ad1fd45f32e7d3a0172320fe60cdd808908136e
George Kulakowski <kulakowski@google.com>
2016-11-02 13:09:28 -0700

    Add Flutter's material fonts
    Change-Id: I053a5fdb4301cc0707f718b0cd7cf1625723bd98

--- bin/fonts/README.md
+++ bin/fonts/README.md
@@ -1,3 +1,11 @@
 # Fonts
 
 An app that provides font data to other apps.
+
+## Provided fonts.
+Currently, we provide the Roboto family of fonts.
+
+We additionaly provide the Matieral Design Fonts, which must be kept
+in sync with Flutter's version of it. See
+[here](https://github.com/flutter/flutter/wiki/Updating-Material-Design-Fonts)
+for their update instructions.
--- bin/fonts/update.sh
+++ bin/fonts/update.sh
@@ -3,10 +3,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-set -e
+set -eu
 
 readonly SCRIPT_ROOT="$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)"
-readonly FUCHSIA_URL_BASE="https://storage.googleapis.com/fuchsia-build/fuchsia"
 
 # download <url> <path>
 function download() {
@@ -15,47 +14,54 @@ function download() {
   curl -f --progress-bar -continue-at=- --location --output "${path}" "${url}"
 }
 
-# download_file_if_needed <name> <url> <base path> <extension>
+# download_file_if_needed <name> <base path> <extension>
 function download_file_if_needed() {
   local name="${1}"
-  local url="${2}"
-  local base_path="${3}"
-  local extension="${4}"
+  local base_path="${2}"
+  local extension="${3}"
 
-  local path="${base_path}${extension}"
+  local path="${base_path}.${extension}"
   local stamp_path="${base_path}.stamp"
-  local requested_hash="$(cat "${base_path}.sha1")"
+  local requested_url="$(cat "${base_path}.version")"
 
-  if [[ ! -f "${stamp_path}" ]] || [[ "${requested_hash}" != "$(cat "${stamp_path}")" ]]; then
+  if [[ ! -f "${stamp_path}" ]] || [[ "${requested_url}" != "$(cat "${stamp_path}")" ]]; then
     echo "Downloading ${name}..."
     rm -f -- "${path}"
-    download "${url}/${requested_hash}" "${path}"
-    echo "${requested_hash}" > "${stamp_path}"
+    download "${requested_url}" "${path}"
+    echo "${requested_url}" > "${stamp_path}"
   fi
 }
 
-# download_tarball <name> <base url> <untar directory>
+# download_tarball <name> <base url> <untar directory> <extension>
 function download_tarball() {
   local name="${1}"
   local base_url="${2}"
   local untar_dir="${3}"
+  local extension="${4}"
   local base_path="${SCRIPT_ROOT}/${untar_dir}"
-  local tar_path="${base_path}.tar.bz2"
+  local tar_path="${base_path}.${extension}"
 
-  download_file_if_needed "${name}" "${FUCHSIA_URL_BASE}/${base_url}" "${base_path}" ".tar.bz2"
+  download_file_if_needed "${name}" "${base_path}" "${extension}"
+  case "x${extension}" in
+    xtar*) local extract_cmd="tar xf" ;;
+    xzip) local extract_cmd="unzip" ;;
+    *) echo "unknown archive type" && exit -1 ;;
+  esac
   if [[ -f "${tar_path}" ]]; then
     mkdir -p -- "${untar_dir}"
-    (cd -- "${untar_dir}" "${tar_path}" && tar xf "${tar_path}")
+    (cd -- "${untar_dir}" "${tar_path}" && ${extract_cmd} "${tar_path}")
     rm -f -- "${tar_path}"
   fi
 }
 
-# download_font <name>
+# download_font <name> <extract>
 function download_font() {
   local name="${1}"
-  download_tarball "${name}" "fonts/${name}" "third_party/${name}"
+  local extension="${2}"
+  download_tarball "${name}" "fonts/${name}" "third_party/${name}" "${extension}"
 }
 
-download_font roboto
-download_font robotocondensed
-download_font robotomono
+download_font material zip
+download_font roboto tar.bz2
+download_font robotocondensed tar.bz2
+download_font robotomono tar.bz2
