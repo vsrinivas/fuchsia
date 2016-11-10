69b8a76c907d782038c485355544e2afceeec5c4
Ian McKellar <ianloic@google.com>
2016-11-10 13:58:44 -0800

    Use //buildtools/ninja
    Change-Id: I6ecb418ccba85935083faf247e91dfaf9af9696a

--- public/lib/fidl/compiler/interfaces/update.sh
+++ public/lib/fidl/compiler/interfaces/update.sh
@@ -10,7 +10,7 @@ OUT_DIR=$PWD/../../out/debug-x86-64
 GEN_DIR=$OUT_DIR/gen
 
 # Rebuild the bindings
-ninja -C $OUT_DIR "../../lib/fidl/compiler/interfaces/fidl_files.fidl^" "../../lib/fidl/compiler/interfaces/fidl_types.fidl^"
+../../buildtools/ninja -C $OUT_DIR "../../lib/fidl/compiler/interfaces/fidl_files.fidl^" "../../lib/fidl/compiler/interfaces/fidl_types.fidl^"
 
 # Copy generated Python bindings
 cp $GEN_DIR/lib/fidl/compiler/interfaces/fidl*.py $PWD/compiler/legacy_generators/pylib/mojom/generate/generated/
