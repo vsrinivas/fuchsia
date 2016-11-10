be20ad9a31e035f37f5ed4e595a0c4d7a2350f0b
Craig Stout <cstout@google.com>
2016-11-09 16:40:22 -0800

    Fix tools path and add vulkan tests
    Change-Id: Ie7ca8ff742ed1f69233931c57664d7b2a588aed4

--- lib/magma/scripts/run.sh
+++ lib/magma/scripts/run.sh
@@ -6,7 +6,7 @@
 
 set -e
 fuchsia_root=`pwd`
-magenta_build_dir=$fuchsia_root/magenta/build-magenta-pc-x86-64
+magenta_build_dir=$fuchsia_root/out/build-magenta/build-magenta-pc-x86-64
 netcp=$magenta_build_dir/tools/netcp
 netruncmd=$magenta_build_dir/tools/netruncmd
 build_dir=$fuchsia_root/out/Debug
@@ -15,4 +15,18 @@ test=$1
 if [ "$test" != "" ]
     then $netcp $build_dir/$test :/tmp/$test && $netruncmd magenta /tmp/$test
     exit
+fi
+
+vulkan_tests=true;
+if $vulkan_tests; then
+	test="state_pool"
+	$netcp $build_dir/$test :/tmp/$test && $netruncmd magenta /tmp/$test
+	test="state_pool_no_free"
+	$netcp $build_dir/$test :/tmp/$test && $netruncmd magenta /tmp/$test
+	test="state_pool_free_list_only"
+	$netcp $build_dir/$test :/tmp/$test && $netruncmd magenta /tmp/$test
+	test="block_pool_no_free"
+	$netcp $build_dir/$test :/tmp/$test && $netruncmd magenta /tmp/$test
+	test="test_wsi_magma"
+	$netcp $build_dir/$test :/tmp/$test && $netruncmd magenta /tmp/$test
 fi
