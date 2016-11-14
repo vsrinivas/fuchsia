13ec64d993a2464e395fccbb4ab9df0667b0a82c
Przemyslaw Pietrzkiewicz <ppi@google.com>
2016-11-14 12:32:45 +0100

    storage: define delegate API to retrieve on-demand objects from sync.
    LE-67 #done
    
    Change-Id: I525e0c1311a11c30589eb7c34177f13d533004f8

---
+++ bin/ledger/storage/public/page_sync_delegate.h
@@ -0,0 +1,39 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef APPS_LEDGER_SRC_STORAGE_PUBLIC_PAGE_SYNC_DELEGATE_H_
+#define APPS_LEDGER_SRC_STORAGE_PUBLIC_PAGE_SYNC_DELEGATE_H_
+
+#include <functional>
+
+#include <mx/datapipe.h>
+
+#include "apps/ledger/src/storage/public/types.h"
+#include "lib/ftl/macros.h"
+
+namespace storage {
+
+// Delegate interface for PageStorage responsible for retrieving on-demand
+// storage objects from the cloud.
+class PageSyncDelegate {
+ public:
+  PageSyncDelegate() {}
+  virtual ~PageSyncDelegate() {}
+
+  // Retrieves the object of the given id from the cloud. The size of the object
+  // is passed to the callback along with the data pipe handle, so that the
+  // client can verify that all data was streamed when draining the pipe.
+  virtual void GetObject(
+      ObjectIdView object_id,
+      std::function<void(Status status,
+                         uint64_t size,
+                         mx::datapipe_consumer data)> callback) = 0;
+
+ private:
+  FTL_DISALLOW_COPY_AND_ASSIGN(PageSyncDelegate);
+};
+
+}  // namespace storage
+
+#endif  // APPS_LEDGER_SRC_STORAGE_PUBLIC_PAGE_SYNC_DELEGATE_H_
