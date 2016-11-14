fc16b2a8895b91bf71695842ca3b58f3507e0024
Dale Sather <dalesat@google.com>
2016-11-14 09:46:00 -0800

    apps/media: fix flog command line processing
    Flog command line processing was broken, because we're now getting
    the app url as argv[0]. Switched to using ftl:command_line.
    
    Change-Id: I9dd06bd671a1a49b0858651d48e1dc4945b7a6cb

--- bin/flog_viewer/flog_viewer_app.cc
+++ bin/flog_viewer/flog_viewer_app.cc
@@ -9,6 +9,7 @@
 #include "apps/media/tools/flog_viewer/channel_handler.h"
 #include "apps/media/tools/flog_viewer/flog_viewer.h"
 #include "apps/modular/lib/app/application_context.h"
+#include "lib/ftl/command_line.h"
 #include "lib/mtl/tasks/message_loop.h"
 
 namespace flog {
@@ -16,8 +17,6 @@ namespace flog {
 class FlogViewerApp {
  public:
   FlogViewerApp(int argc, const char** argv) {
-    ProcessArgs(argc, argv);
-
     std::unique_ptr<modular::ApplicationContext> application_context =
         modular::ApplicationContext::CreateFromStartupInfo();
 
@@ -25,59 +24,70 @@ class FlogViewerApp {
       mtl::MessageLoop::GetCurrent()->PostQuitTask();
     });
 
-    if (delete_all_logs_) {
-      viewer_.DeleteAllLogs();
-    } else {
-      for (uint32_t log_to_delete : logs_to_delete_) {
-        viewer_.DeleteLog(log_to_delete);
+    ftl::CommandLine command_line = ftl::CommandLineFromArgcArgv(argc, argv);
+    bool did_something = false;
+
+    viewer_.set_format(
+        command_line.GetOptionValueWithDefault("format", viewer_.format()));
+
+    std::string string_value;
+    if (command_line.GetOptionValue("channel", &string_value) ||
+        command_line.GetOptionValue("channels", &string_value)) {
+      std::vector<uint32_t> channels;
+      if (!Parse(string_value, &channels)) {
+        Usage();
+        return;
       }
+
+      for (uint32_t channel : channels) {
+        viewer_.AddChannel(channel);
+      }
+
+      did_something = true;
     }
 
-    if (process_last_ || !process_last_label_.empty()) {
-      viewer_.ProcessLastLog(process_last_label_);
-    } else if (log_id_ == 0) {
+    if (command_line.GetOptionValue("last", &string_value)) {
+      viewer_.ProcessLastLog(string_value);
+      did_something = true;
+    }
+
+    for (const std::string& log_id_string : command_line.positional_args()) {
+      std::vector<uint32_t> log_ids;
+      if (!Parse(log_id_string, &log_ids)) {
+        Usage();
+        return;
+      }
+
+      for (uint32_t log_id : log_ids) {
+        viewer_.ProcessLog(log_id);
+      }
+
+      did_something = true;
+    }
+
+    if (command_line.HasOption("delete-all-logs")) {
+      viewer_.DeleteAllLogs();
+    } else if (command_line.GetOptionValue("delete-log", &string_value) ||
+               command_line.GetOptionValue("delete-logs", &string_value)) {
+      std::vector<uint32_t> logs;
+      if (!Parse(string_value, &logs)) {
+        Usage();
+        return;
+      }
+
+      for (uint32_t log : logs) {
+        viewer_.DeleteLog(log);
+      }
+
+      did_something = true;
+    }
+
+    if (!did_something) {
       viewer_.ProcessLogs();
-    } else {
-      viewer_.ProcessLog(log_id_);
     }
   }
 
  private:
-  // Processes arguments.
-  void ProcessArgs(int argc, const char** argv) {
-    for (int i = 0; i < argc; ++i) {
-      const std::string& arg = argv[i];
-
-      std::string string_value;
-      uint32_t numeric_value;
-      std::vector<uint32_t> numeric_values;
-
-      if (MatchOption(arg, "format", &string_value)) {
-        viewer_.set_format(string_value);
-      } else if (MatchOption(arg, "channel", &numeric_values) ||
-                 MatchOption(arg, "channels", &numeric_values)) {
-        for (uint32_t value : numeric_values) {
-          viewer_.AddChannel(value);
-        }
-      } else if (MatchOption(arg, "last", &string_value)) {
-        process_last_label_ = string_value;
-      } else if (MatchOption(arg, "last")) {
-        process_last_ = true;
-      } else if (MatchOption(arg, "delete-log", &numeric_values) ||
-                 MatchOption(arg, "delete-logs", &numeric_values)) {
-        for (uint32_t value : numeric_values) {
-          logs_to_delete_.push_back(value);
-        }
-      } else if (MatchOption(arg, "delete-all-logs")) {
-        delete_all_logs_ = true;
-      } else if (MatchBareArg(arg, &numeric_value)) {
-        log_id_ = numeric_value;
-      } else {
-        Usage();
-      }
-    }
-  }
-
   void Usage() {
     std::cout << std::endl;
     std::cout << "fidl:flog_viewer <args>\"" << std::endl;
@@ -108,39 +118,11 @@ class FlogViewerApp {
     mtl::MessageLoop::GetCurrent()->PostQuitTask();
   }
 
-  bool MatchOption(const std::string& arg, const std::string& option) {
-    return arg.compare(0, 2, "--") == 0 &&
-           arg.compare(2, std::string::npos, option) == 0;
-  }
-
-  bool MatchOption(const std::string& arg,
-                   const std::string& option,
-                   std::string* string_out) {
-    FTL_DCHECK(string_out);
-
-    if (arg.compare(0, 2, "--") != 0 ||
-        arg.compare(2, option.size(), option) != 0 ||
-        arg.compare(2 + option.size(), 1, "=") != 0) {
-      return false;
-    }
-
-    *string_out = arg.substr(2 + option.size() + 1);
-
-    return true;
-  }
-
-  bool MatchOption(const std::string& arg,
-                   const std::string& option,
-                   std::vector<uint32_t>* vector_of_uint32_out) {
+  bool Parse(const std::string& string_value,
+             std::vector<uint32_t>* vector_of_uint32_out) {
     FTL_DCHECK(vector_of_uint32_out);
 
-    if (arg.compare(0, 2, "--") != 0 ||
-        arg.compare(2, option.size(), option) != 0 ||
-        arg.compare(2 + option.size(), 1, "=") != 0) {
-      return false;
-    }
-
-    std::istringstream istream(arg.substr(2 + option.size() + 1));
+    std::istringstream istream(string_value);
     uint32_t value;
     while (istream >> value) {
       vector_of_uint32_out->push_back(value);
@@ -152,19 +134,7 @@ class FlogViewerApp {
     return vector_of_uint32_out->size() != 0 && istream.eof();
   }
 
-  bool MatchBareArg(const std::string& arg, uint32_t* uint32_out) {
-    FTL_DCHECK(uint32_out);
-
-    std::istringstream istream(arg);
-    return static_cast<bool>(istream >> *uint32_out);
-  }
-
   FlogViewer viewer_;
-  uint32_t log_id_ = 0;
-  bool process_last_ = false;
-  std::string process_last_label_;
-  bool delete_all_logs_ = false;
-  std::vector<uint32_t> logs_to_delete_;
 };
 
 }  // namespace flog
