schwa423/mozart

[mozart] Adding modifier to key event
Passing current modifier with key event so it's easier for application
to handle key combos (e.g. CTRL+A in VT100 land is \1)

Change-Id: I1b9c67492b7bb27db3d86452099995fe79cbeb5f

services/input/cpp/formatting.cc
@@ -12,10 +12,29 @@ namespace mozart {
 
 std::ostream& operator<<(std::ostream& os, const KeyData& value) {
   if (value.code_point) {
-    return os << "{character=" << value.code_point << "}";
+    os << "{character=" << value.code_point;
   } else {
-    return os << "{hid=" << ftl::StringPrintf("0x%08X", value.hid_usage) << "}";
+    os << "{hid=" << ftl::StringPrintf("0x%08X", value.hid_usage);
   }
+  if (value.modifiers) {
+    os << ",modifiers";
+    if (value.modifiers & mozart::CAPS_LOCK) {
+      os << ":CAPS_LOCK";
+    }
+    if (value.modifiers & mozart::SHIFT) {
+      os << ":SHIFT";
+    }
+    if (value.modifiers & mozart::CONTROL) {
+      os << ":CONTROL";
+    }
+    if (value.modifiers & mozart::ALT) {
+      os << ":ALT";
+    }
+    if (value.modifiers & mozart::GUI) {
+      os << ":GUI";
+    }
+  }
+  return os << "}";
 }
 
 std::ostream& operator<<(std::ostream& os, const PointerData& value) {

vsrinivas/fuchsia

Change-Id: Id19fcc9d72ee5ea4db6c119afeb3a9d31c77c698

bin/ui/services/input/cpp/formatting.cc
@@ -12,10 +12,29 @@ namespace mozart {
 
 std::ostream& operator<<(std::ostream& os, const KeyData& value) {
   if (value.code_point) {
-    return os << "{character=" << value.code_point << "}";
+    os << "{character=" << value.code_point;
   } else {
-    return os << "{hid=" << ftl::StringPrintf("0x%08X", value.hid_usage) << "}";
+    os << "{hid=" << ftl::StringPrintf("0x%08X", value.hid_usage);
   }
+  if (value.modifiers) {
+    os << ",modifiers";
+    if (value.modifiers & mozart::kModifierCapsLock) {
+      os << ":CAPS_LOCK";
+    }
+    if (value.modifiers & mozart::kModifierShift) {
+      os << ":SHIFT";
+    }
+    if (value.modifiers & mozart::kModifierControl) {
+      os << ":CONTROL";
+    }
+    if (value.modifiers & mozart::kModifierAlt) {
+      os << ":ALT";
+    }
+    if (value.modifiers & mozart::kModifierSuper) {
+      os << ":SUPER";
+    }
+  }
+  return os << "}";
 }
 
 std::ostream& operator<<(std::ostream& os, const PointerData& value) {