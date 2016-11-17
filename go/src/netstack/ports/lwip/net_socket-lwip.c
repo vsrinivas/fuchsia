01736b5fcbc502084683dae9fa5f65a3bea0820b
George Kulakowski <kulakowski@google.com>
2016-11-17 10:29:26 -0800

    Tolerate NULL service in net_getaddrinfo
    Change-Id: I4c0f38bf27f30fd18977fd70a387aa312e8c029d

--- go/src/netstack/ports/lwip/net_socket-lwip.c
+++ go/src/netstack/ports/lwip/net_socket-lwip.c
@@ -381,10 +381,12 @@ int net_getaddrinfo(const char* node, const char* service,
   }
 
   // TODO: do this somewhere else
-  if (strcmp(service, "http") == 0) {
-    service = "80";
-  } else if (strcmp(service, "https") == 0) {
-    service = "443";
+  if (service) {
+    if (strcmp(service, "http") == 0) {
+      service = "80";
+    } else if (strcmp(service, "https") == 0) {
+      service = "443";
+    }
   }
 
   ret = lwip_getaddrinfo(node, service, hints, &lwip_res);
