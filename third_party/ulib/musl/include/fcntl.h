eb2ce965702f8e8e92bb1a57d2493850ce2b6e25
Sean Klein <smklein@google.com>
2016-11-18 14:13:58 -0800

    [musl] Unify fcntl between architectures
    All values taken from generic/fcntl.h, other than F_GETLK, F_SETLK, and
    F_SETLKW, which were taken from x86_64.
    
    The unification of fcntl will make it easier to manipulate flags
    without risking collision on different machines.
    
    This patch only relocates "$ARCH/fcntl.h" to "fcntl.h". It does not
    remove or otherwise modify values.
    
    Change-Id: I293ef627794a5422aa1c1cb1864478abd518a0ad

--- third_party/ulib/musl/include/fcntl.h
+++ third_party/ulib/musl/include/fcntl.h
@@ -18,8 +18,6 @@ extern "C" {
 
 #include <bits/alltypes.h>
 
-#include <bits/fcntl.h>
-
 struct flock {
     short l_type;
     short l_whence;
@@ -38,10 +36,54 @@ int posix_fallocate(int, off_t, off_t);
 #define O_SEARCH O_PATH
 #define O_EXEC O_PATH
 
-#define O_ACCMODE (03 | O_SEARCH)
-#define O_RDONLY 00
-#define O_WRONLY 01
-#define O_RDWR 02
+// clang-format off
+#define O_ACCMODE        (03 | O_SEARCH)
+#define O_RDONLY          00
+#define O_WRONLY          01
+#define O_RDWR            02
+
+#define O_CREAT         0100
+#define O_EXCL          0200
+#define O_NOCTTY        0400
+#define O_TRUNC        01000
+#define O_APPEND       02000
+#define O_NONBLOCK     04000
+#define O_DSYNC       010000
+#define O_SYNC      04010000
+#define O_RSYNC     04010000
+#define O_DIRECTORY  0200000
+#define O_NOFOLLOW   0400000
+#define O_CLOEXEC   02000000
+
+#define O_ASYNC       020000
+#define O_DIRECT      040000
+#define O_LARGEFILE  0100000
+#define O_NOATIME   01000000
+#define O_PATH     010000000
+#define O_TMPFILE  020200000
+// clang-format on
+
+#define O_NDELAY O_NONBLOCK
+
+#define F_DUPFD 0
+#define F_GETFD 1
+#define F_SETFD 2
+#define F_GETFL 3
+#define F_SETFL 4
+
+#define F_GETLK 5
+#define F_SETLK 6
+#define F_SETLKW 7
+
+#define F_SETOWN 8
+#define F_GETOWN 9
+#define F_SETSIG 10
+#define F_GETSIG 11
+
+#define F_SETOWN_EX 15
+#define F_GETOWN_EX 16
+
+#define F_GETOWNER_UIDS 17
 
 #define F_OFD_GETLK 36
 #define F_OFD_SETLK 37
