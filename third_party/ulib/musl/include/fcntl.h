dcca5e39e65ac9de1dbcb4d8ed133e321f071034
Sean Klein <smklein@google.com>
2016-11-18 14:20:35 -0800

    [musl] Add O_NOREMOTE flag to open flags
    This flag will signal for the open operating to access the filesystem
    which is hosting a remote point, rather than the filesystem which is
    being hosted.
    
    Change-Id: Id8d487391d7b2c62436a806068f49098218b1c40

--- third_party/ulib/musl/include/fcntl.h
+++ third_party/ulib/musl/include/fcntl.h
@@ -37,30 +37,34 @@ int posix_fallocate(int, off_t, off_t);
 #define O_EXEC O_PATH
 
 // clang-format off
-#define O_ACCMODE        (03 | O_SEARCH)
-#define O_RDONLY          00
-#define O_WRONLY          01
-#define O_RDWR            02
+#define O_ACCMODE         (03 | O_SEARCH)
+#define O_RDONLY           00
+#define O_WRONLY           01
+#define O_RDWR             02
 
-#define O_CREAT         0100
-#define O_EXCL          0200
-#define O_NOCTTY        0400
-#define O_TRUNC        01000
-#define O_APPEND       02000
-#define O_NONBLOCK     04000
-#define O_DSYNC       010000
-#define O_SYNC      04010000
-#define O_RSYNC     04010000
-#define O_DIRECTORY  0200000
-#define O_NOFOLLOW   0400000
-#define O_CLOEXEC   02000000
+#define O_CREAT          0100
+#define O_EXCL           0200
+#define O_NOCTTY         0400
+#define O_TRUNC         01000
+#define O_APPEND        02000
+#define O_NONBLOCK      04000
+#define O_DSYNC        010000
+#define O_SYNC       04010000
+#define O_RSYNC      04010000
+#define O_DIRECTORY   0200000
+#define O_NOFOLLOW    0400000
+#define O_CLOEXEC    02000000
 
-#define O_ASYNC       020000
-#define O_DIRECT      040000
-#define O_LARGEFILE  0100000
-#define O_NOATIME   01000000
-#define O_PATH     010000000
-#define O_TMPFILE  020200000
+#define O_ASYNC        020000
+#define O_DIRECT       040000
+#define O_LARGEFILE   0100000
+#define O_NOATIME    01000000
+#define O_PATH      010000000
+#define O_TMPFILE   020200000
+
+#ifdef _ALL_SOURCE
+#define O_NOREMOTE 0100000000
+#endif
 // clang-format on
 
 #define O_NDELAY O_NONBLOCK
