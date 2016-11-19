f2d2be136b45fc5545c7b75c43f9badce4254cb1
Adam Barth <abarth@chromium.org>
2016-11-18 16:20:00 -0800

    Improve basic_wm
    Now it works a bit more like a normal window manager.
    
    Change-Id: I200a4e476bf375ea5534be8eb63bccc0e5a6203f

--- bin/ui/examples/basic_wm/main.dart
+++ bin/ui/examples/basic_wm/main.dart
@@ -1,11 +1,11 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:math' as math;
-
 import 'package:apps.modular.lib.app.dart/app.dart';
 import 'package:apps.modular.services.application/application_controller.fidl.dart';
+import 'package:apps.modular.services.application/application_environment.fidl.dart';
+import 'package:apps.modular.services.application/application_environment_host.fidl.dart';
 import 'package:apps.modular.services.application/application_launcher.fidl.dart';
 import 'package:apps.modular.services.application/service_provider.fidl.dart';
 import 'package:apps.mozart.lib.flutter/child_view.dart';
@@ -13,326 +13,58 @@ import 'package:apps.mozart.services.presentation/presenter.fidl.dart';
 import 'package:lib.fidl.dart/bindings.dart';
 import 'package:flutter/material.dart';
 
+import 'window_manager.dart';
+
 final ApplicationContext _context = new ApplicationContext.fromStartupInfo();
 
-class LauncherData {
-  const LauncherData({ this.url, this.title });
-  final String url;
-  final String title;
-}
-
-final List<LauncherData> _kLauncherData = <LauncherData>[
-  const LauncherData(
-    url: 'file:///system/apps/noodles_view',
-    title: 'Noodles'
-  ),
-  const LauncherData(
-    url: 'file:///system/apps/shapes_view',
-    title: 'Shapes'
-  ),
-  new LauncherData(
-    url: 'file:///system/apps/hello_flutter',
-    title: 'Hello Flutter'
-  ),
-];
+final ApplicationEnvironmentProxy _childEnvironment = _initChildEnvironment();
+final ApplicationLauncherProxy _childLauncher = _initLauncher();
 
 class ChildApplication {
-  ChildApplication({ this.controller, this.connection });
+  ChildApplication({
+    this.controller,
+    this.connection,
+    this.title,
+  });
 
-  final ApplicationControllerProxy controller;
-  final ChildViewConnection connection;
-}
-
-const Size _kInitialWindowSize = const Size(200.0, 200.0);
-const double _kWindowPadding = 24.0;
-
-enum WindowSide {
-  topCenter,
-  topRight,
-  bottomRight,
-}
-
-class WindowDecoration extends StatelessWidget {
-  WindowDecoration({
-    Key key,
-    this.side,
-    this.color,
-    this.onTap,
-    this.onPanUpdate
-  }) : super(key: key);
-
-  final WindowSide side;
-  final Color color;
-  final GestureTapCallback onTap;
-  final GestureDragUpdateCallback onPanUpdate;
-
-  @override
-  Widget build(BuildContext context) {
-    double top, right, bottom, left, width, height;
-
-    height = _kWindowPadding;
-
-    if (side == WindowSide.topCenter || side == WindowSide.topRight)
-      top = 0.0;
-
-    if (side == WindowSide.topRight || side == WindowSide.bottomRight) {
-      right = 0.0;
-      width = _kWindowPadding;
-    }
-
-    if (side == WindowSide.topCenter) {
-      left = _kWindowPadding;
-      right = _kWindowPadding;
-    }
-
-    if (side == WindowSide.bottomRight)
-      bottom = 0.0;
-
-    return new Positioned(
-      top: top,
-      right: right,
-      bottom: bottom,
-      left: left,
-      width: width,
-      height: height,
-      child: new GestureDetector(
-        onTap: onTap,
-        onPanUpdate: onPanUpdate,
-        child: new Container(
-          decoration: new BoxDecoration(
-            backgroundColor: color
-          )
-        )
-      )
-    );
-  }
-}
-
-class WindowTitleBar extends StatelessWidget {
-  WindowTitleBar({
-    Key key,
-    this.onClose,
-    this.onMove,
-  }) : super(key: key);
-
-  final VoidCallback onClose;
-  final GestureDragUpdateCallback onMove;
-
-  Widget build(BuildContext context) {
-    return new Row(
-      children: <Widget>[
-        new Flexible(
-          child: new GestureDetector(
-            onPanUpdate: onMove,
-            child: new Container(
-              height: _kWindowPadding,
-              decoration: new BoxDecoration(
-                backgroundColor: Colors.blue[200],
-              ),
-            ),
-          ),
-        ),
-        new GestureDetector(
-          onTap: onClose,
-          child: new Container(
-            width: _kWindowPadding,
-            height: _kWindowPadding,
-            decoration: new BoxDecoration(
-              backgroundColor: Colors.red[200],
-            ),
-            alignment: FractionalOffset.center,
-            child: new Icon(Icons.close),
-          ),
-        ),
-      ]
-    );
-  }
-}
-
-class Window extends StatefulWidget {
-  Window({ Key key, this.child, this.onClose }) : super(key: key);
-
-  final ChildApplication child;
-  final ValueChanged<ChildApplication> onClose;
-
-  @override
-  _WindowState createState() => new _WindowState();
-}
-
-class _WindowState extends State<Window> {
-  Offset _offset = Offset.zero;
-  Size _size = _kInitialWindowSize;
-
-  void _handleResizerDrag(DragUpdateDetails details) {
-    setState(() {
-      _size = new Size(
-        math.max(0.0, _size.width + details.delta.dx),
-        math.max(0.0, _size.height + details.delta.dy)
-      );
-    });
-  }
-
-  void _handleMove(DragUpdateDetails details) {
-    setState(() {
-      _offset += details.delta;
-    });
-  }
-
-  void _handleClose() {
-    config.onClose(config.child);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Positioned(
-      left: _offset.dx,
-      top: _offset.dy,
-      width: _size.width + _kWindowPadding * 0.5,
-      height: _size.height + _kWindowPadding * 1.5,
-      child: new Stack(
-        children: <Widget>[
-          new Positioned(
-            bottom: 0.0,
-            right: 0.0,
-            width: _kWindowPadding,
-            height: _kWindowPadding,
-            child: new GestureDetector(
-              onPanUpdate: _handleResizerDrag,
-              child: new Container(
-                decoration: new BoxDecoration(
-                  backgroundColor: Colors.blue[200],
-                ),
-              )
-            )
-          ),
-          new Positioned.fill(
-            right: _kWindowPadding,
-            bottom: _kWindowPadding,
-            child: new Material(
-              elevation: 8,
-              child: new Column(
-                children: <Widget>[
-                  new WindowTitleBar(
-                    onClose: _handleClose,
-                    onMove: _handleMove,
-                  ),
-                  new Flexible(
-                    child: new ChildView(connection: config.child.connection),
-                  ),
-                ]
-              )
-            )
-          )
-        ]
-      )
-    );
-  }
-}
-
-class LauncherItem extends StatelessWidget {
-  LauncherItem({
-    Key key,
-    this.url,
-    this.child,
-    this.onLaunch
-  }) : super(key: key);
-
-  final String url;
-  final Widget child;
-  final ValueChanged<ChildApplication> onLaunch;
-
-  void _handlePressed() {
+  factory ChildApplication.create(String url, {String title}) {
     final ServiceProviderProxy services = new ServiceProviderProxy();
-    final ApplicationControllerProxy controller = new ApplicationControllerProxy();
+    final ApplicationControllerProxy controller =
+        new ApplicationControllerProxy();
     final ApplicationLaunchInfo launchInfo = new ApplicationLaunchInfo()
       ..url = url
       ..services = services.ctrl.request();
-    _context.launcher.createApplication(launchInfo, controller.ctrl.request());
-    onLaunch(new ChildApplication(
+    _childLauncher.createApplication(launchInfo, controller.ctrl.request());
+    return new ChildApplication(
       controller: controller,
       connection: new ChildViewConnection.connect(services),
-    ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new RaisedButton(
-      onPressed: _handlePressed,
-      child: child
+      title: title,
     );
   }
-}
 
-class Launcher extends StatelessWidget {
-  Launcher({ Key key, this.items }) : super(key: key);
-
-  final List<Widget> items;
-
-  @override
-  Widget build(BuildContext context) {
-    return new ButtonBar(
-      alignment: MainAxisAlignment.center,
-      children: items
+  factory ChildApplication.view(InterfaceHandle<ViewOwner> viewOwner) {
+    return new ChildApplication(
+      connection: new ChildViewConnection(viewOwner),
     );
   }
+
+  final ApplicationControllerProxy controller;
+  final ChildViewConnection connection;
+  final String title;
 }
 
-class WindowManager extends StatefulWidget {
-  WindowManager({ Key key }) : super(key: key);
+final GlobalKey<WindowManagerState> _windowManager =
+    new GlobalKey<WindowManagerState>();
 
-  @override
-  _WindowManagerState createState() => new _WindowManagerState();
-}
-
-class _WindowManagerState extends State<WindowManager> {
-  List<ChildApplication> _windows = <ChildApplication>[];
-
-  void _handleLaunch(ChildApplication child) {
-    setState(() {
-      _windows.add(child);
-    });
-  }
-
-  void _handleClose(ChildApplication child) {
-    setState(() {
-      _windows.remove(child);
-      child.controller?.kill(null);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      child: new Stack(
-        children: <Widget>[
-          new Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            child: new Launcher(items: _kLauncherData.map((LauncherData data) {
-              return new LauncherItem(
-                url: data.url,
-                onLaunch: _handleLaunch,
-                child: new Text(data.title)
-              );
-            }).toList())
-          )
-        ]..addAll(_windows.map((ChildApplication child) {
-          return new Window(
-            key: new ObjectKey(child),
-            onClose: _handleClose,
-            child: child
-          );
-        }))
-      )
-    );
-  }
+void addWindowForChildApplication(ChildApplication application) {
+  _windowManager.currentState.addWindow(new WindowFrame(
+    key: new ObjectKey(application),
+    title: application.title,
+    child: new ChildView(connection: application.connection),
+  ));
 }
 
 class PresenterImpl extends Presenter {
-  PresenterImpl(this._key);
-
-  final GlobalKey<_WindowManagerState> _key;
   final PresenterBinding _binding = new PresenterBinding();
 
   void bind(InterfaceRequest<Presenter> request) {
@@ -341,21 +73,100 @@ class PresenterImpl extends Presenter {
 
   @override
   void present(InterfaceHandle<ViewOwner> viewOwner) {
-    _key.currentState._handleLaunch(
-      new ChildApplication(connection: new ChildViewConnection(viewOwner))
+    addWindowForChildApplication(new ChildApplication.view(viewOwner));
+  }
+}
+
+class ApplicationEnvironmentHostImpl extends ApplicationEnvironmentHost {
+  final ApplicationEnvironmentHostBinding _binding =
+      new ApplicationEnvironmentHostBinding();
+
+  void bind(InterfaceRequest<ApplicationEnvironmentHost> request) {
+    _binding.bind(this, request);
+  }
+
+  InterfaceHandle<ApplicationEnvironmentHost> getInterfaceHandle() {
+    return _binding.wrap(this);
+  }
+
+  @override
+  void getApplicationEnvironmentServices(
+      InterfaceRequest<ServiceProvider> services) {
+    new ServiceProviderImpl()
+      ..bind(services)
+      ..addServiceForName((request) {
+        new PresenterImpl().bind(request);
+      }, Presenter.serviceName)
+      ..defaultConnector = (String serviceName, InterfaceRequest request) {
+        _context.environmentServices
+            .connectToService(serviceName, request.passChannel());
+      };
+  }
+}
+
+final ApplicationEnvironmentHostImpl _environmentHost =
+    new ApplicationEnvironmentHostImpl();
+
+ApplicationEnvironmentProxy _initChildEnvironment() {
+  final ApplicationEnvironmentProxy proxy = new ApplicationEnvironmentProxy();
+  _context.environment.createNestedEnvironment(
+    _environmentHost.getInterfaceHandle(),
+    proxy.ctrl.request(),
+    null,
+    'basic_wm',
+  );
+  return proxy;
+}
+
+ApplicationLauncherProxy _initLauncher() {
+  final ApplicationLauncherProxy proxy = new ApplicationLauncherProxy();
+  _childEnvironment.getApplicationLauncher(proxy.ctrl.request());
+  return proxy;
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Theme(
+      data: new ThemeData.light(),
+      child: new WindowManager(
+        key: _windowManager,
+        wallpaper: new Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              begin: FractionalOffset.topLeft,
+              end: FractionalOffset.bottomRight,
+              colors: <Color>[Colors.green[200], Colors.green[400]],
+            ),
+          ),
+        ),
+        initialWindows: <Widget>[
+          new Positioned(
+            top: 24.0,
+            right: 24.0,
+            child: new FlutterLogo(
+              size: 48.0,
+            ),
+          ),
+          new Positioned(
+            right: 24.0,
+            bottom: 24.0,
+            child: new FloatingActionButton(
+              onPressed: () {
+                addWindowForChildApplication(new ChildApplication.create(
+                  'file:///system/apps/moterm',
+                  title: 'Terminal',
+                ));
+              },
+              child: new Icon(Icons.apps),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
 void main() {
-  GlobalKey<_WindowManagerState> windowManagerKey = new GlobalKey<_WindowManagerState>();
-
-  _context.outgoingServices.addServiceForName((request) {
-    new PresenterImpl(windowManagerKey).bind(request);
-  }, Presenter.serviceName);
-
-  runApp(new MaterialApp(
-    title: 'Basic Window Manager',
-    home: new WindowManager(key: windowManagerKey)
-  ));
+  runApp(new App());
 }
---
+++ bin/ui/examples/basic_wm/window_manager.dart
@@ -0,0 +1,238 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+const Color _kCloseColor = const Color(0xAFEF9A9A);
+const Color _kTitleBarColor = const Color(0xAF90CAF9);
+const Radius _kWindowDecorationCorner = const Radius.circular(8.0);
+const double _kWindowDecorationExtent = 24.0;
+const Size _kInitialWindowSize = const Size(400.0, 200.0);
+
+class WindowTitleBar extends StatelessWidget {
+  WindowTitleBar({
+    Key key,
+    this.title,
+    this.onClosed,
+    this.onActivate,
+    this.onMoved,
+  })
+      : super(key: key);
+
+  final String title;
+  final VoidCallback onClosed;
+  final VoidCallback onActivate;
+  final GestureDragUpdateCallback onMoved;
+
+  Widget build(BuildContext context) {
+    return new Container(
+      height: _kWindowDecorationExtent,
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          new Flexible(
+            child: new GestureDetector(
+              onTap: onActivate,
+              onPanStart: (DragStartDetails details) {
+                onActivate();
+              },
+              onPanUpdate: onMoved,
+              child: new Container(
+                decoration: const BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: _kWindowDecorationCorner,
+                  ),
+                  backgroundColor: _kTitleBarColor,
+                ),
+                alignment: FractionalOffset.centerLeft,
+                padding: const EdgeInsets.only(left: 8.0),
+                child: new Text(title ?? 'Untitled', style: Theme.of(context).textTheme.subhead),
+              ),
+            ),
+          ),
+          new GestureDetector(
+            onTap: onClosed,
+            child: new Container(
+              decoration: const BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topRight: _kWindowDecorationCorner,
+                ),
+                backgroundColor: _kCloseColor,
+              ),
+              alignment: FractionalOffset.center,
+              child: new Icon(Icons.close),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WindowResizer extends StatelessWidget {
+  WindowResizer({Key key, this.onResized}) : super(key: key);
+
+  final GestureDragUpdateCallback onResized;
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onPanUpdate: onResized,
+      child: new Container(
+        width: _kWindowDecorationExtent,
+        height: _kWindowDecorationExtent,
+        decoration: new BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            bottomRight: _kWindowDecorationCorner,
+          ),
+          backgroundColor: _kTitleBarColor,
+        ),
+      ),
+    );
+  }
+}
+
+class WindowFrame extends StatefulWidget {
+  WindowFrame({
+    Key key,
+    this.initialRect,
+    this.title,
+    this.child,
+  })
+      : super(key: key);
+
+  final Rect initialRect;
+  final String title;
+  final Widget child;
+
+  @override
+  _WindowFrameState createState() => new _WindowFrameState();
+}
+
+class _WindowFrameState extends State<WindowFrame> {
+  @override
+  void initState() {
+    super.initState();
+    if (config.initialRect != null) {
+      _offset = config.initialRect.topLeft.toOffset();
+      _size = config.initialRect.size;
+    }
+  }
+
+  Offset _offset = Offset.zero;
+  Size _size = _kInitialWindowSize;
+
+  void _handleResizerDrag(DragUpdateDetails details) {
+    setState(() {
+      _size = new Size(math.max(0.0, _size.width + details.delta.dx),
+          math.max(0.0, _size.height + details.delta.dy));
+    });
+  }
+
+  void _handleMove(DragUpdateDetails details) {
+    setState(() {
+      _offset += details.delta;
+    });
+  }
+
+  void _handleClose() {
+    WindowManager.of(context)?.removeWindow(config);
+  }
+
+  void _handleActive() {
+    WindowManager.of(context)?.activateWindow(config);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Positioned(
+      left: _offset.dx,
+      top: _offset.dy,
+      width: _size.width + _kWindowDecorationExtent * 0.5,
+      height: _size.height + _kWindowDecorationExtent,
+      child: new Stack(
+        children: <Widget>[
+          new Positioned(
+            right: 0.0,
+            bottom: 0.0,
+            child: new WindowResizer(onResized: _handleResizerDrag),
+          ),
+          new Positioned.fill(
+            right: _kWindowDecorationExtent * 0.5,
+            bottom: _kWindowDecorationExtent * 0.5,
+            child: new Column(
+              children: <Widget>[
+                new WindowTitleBar(
+                  title: config.title,
+                  onActivate: _handleActive,
+                  onClosed: _handleClose,
+                  onMoved: _handleMove,
+                ),
+                new Flexible(child: config.child),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WindowManager extends StatefulWidget {
+  WindowManager({
+    Key key,
+    this.wallpaper,
+    this.initialWindows,
+  })
+      : super(key: key);
+
+  final Widget wallpaper;
+  final List<Widget> initialWindows;
+
+  static WindowManagerState of(BuildContext context) {
+    return context.ancestorStateOfType(const TypeMatcher<WindowManagerState>());
+  }
+
+  @override
+  WindowManagerState createState() => new WindowManagerState();
+}
+
+class WindowManagerState extends State<WindowManager> {
+  List<Widget> _windows = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _windows.addAll(config.initialWindows);
+  }
+
+  void addWindow(Widget window) {
+    setState(() {
+      _windows.add(window);
+    });
+  }
+
+  void removeWindow(Widget window) {
+    setState(() {
+      _windows.remove(window);
+    });
+  }
+
+  void activateWindow(Widget window) {
+    setState(() {
+      if (_windows.remove(window))
+        _windows.add(window);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = <Widget>[];
+    if (config.wallpaper != null)
+      children.add(config.wallpaper);
+    children.addAll(_windows);
+    return new Stack(children: children);
+  }
+}
