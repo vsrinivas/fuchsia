ca8c883414a67ffa5981e107c3fdc4e081ae013e
Craig Stout <cstout@google.com>
2016-11-21 17:59:06 -0800

    Add PlatformEvent.
    Change-Id: I083f18a7207a96f0fa61773aebe3e2e5861ff7e1

---
+++ lib/magma/tests/unit_tests/test_platform_event.cc
@@ -0,0 +1,32 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform_event.h"
+#include "gtest/gtest.h"
+#include <thread>
+
+class TestEvent {
+public:
+    static void Test()
+    {
+        auto event = std::shared_ptr<magma::PlatformEvent>(magma::PlatformEvent::Create());
+
+        std::vector<std::thread> threads;
+
+        for (uint32_t iter = 0; iter < 100; iter++) {
+            threads.emplace_back([](std::shared_ptr<magma::PlatformEvent> event) { event->Wait(); },
+                                 event);
+        }
+
+        std::this_thread::yield();
+
+        event->Signal();
+
+        for (uint32_t iter = 0; iter < 100; iter++) {
+            threads[iter].join();
+        }
+    }
+};
+
+TEST(PlatformEvent, Test) { TestEvent::Test(); }
