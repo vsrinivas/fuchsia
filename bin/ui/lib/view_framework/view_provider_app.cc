260f9831e69a5a91744d5f351d3460a020a965a2
Jeff Brown <jeffbrown@google.com>
2016-11-22 11:35:17 -0800

    Let trace mechanism choose default provider name for app.
    Change-Id: I88b2ee1b002c0f909189662f2bfcfd99956b1f3a

--- bin/ui/lib/view_framework/view_provider_app.cc
+++ bin/ui/lib/view_framework/view_provider_app.cc
@@ -12,7 +12,7 @@ ViewProviderApp::ViewProviderApp(ViewFactory factory)
     : application_context_(
           modular::ApplicationContext::CreateFromStartupInfo()),
       service_(application_context_.get(), std::move(factory)) {
-  tracing::InitializeTracer(application_context_.get(), {"app"});
+  tracing::InitializeTracer(application_context_.get(), {});
 }
 
 ViewProviderApp::~ViewProviderApp() {}
