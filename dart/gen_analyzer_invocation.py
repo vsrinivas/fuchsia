feb17b1d209ee1abaf2b7ad21ad80244eecd9a83
P.Y. Laligand <pylaligand@google.com>
2016-11-22 18:01:55 -0800

    Add the package name to the analyzer invocation.
    Change-Id: I951d0a12ab8003cf11766ccd54bcb72586c6389b

--- dart/gen_analyzer_invocation.py
+++ dart/gen_analyzer_invocation.py
@@ -10,23 +10,25 @@ import sys
 
 def main():
   parser = argparse.ArgumentParser(
-      "Generate a script that invokes the Dart analyzer")
-  parser.add_argument("--out", help="Path to the invocation file to generate",
+      'Generate a script that invokes the Dart analyzer')
+  parser.add_argument('--out', help='Path to the invocation file to generate',
                       required=True)
-  parser.add_argument("--source-dir", help="Path to package source",
+  parser.add_argument('--source-dir', help='Path to package source',
                       required=True)
-  parser.add_argument("--dot-packages", help="Path to the .packages file",
+  parser.add_argument('--dot-packages', help='Path to the .packages file',
                       required=True)
-  parser.add_argument("--root-build-dir",
-                      help="Path to root of the build directory", required=True)
-  parser.add_argument("--analyzer-packages",
-                      help="Path to .packages file for the analyzer_cli package",
+  parser.add_argument('--root-build-dir',
+                      help='Path to root of the build directory', required=True)
+  parser.add_argument('--analyzer-packages',
+                      help='Path to .packages file for the analyzer_cli package',
                       required=True)
-  parser.add_argument("--analyzer-main", help="Path to the analyzer executable",
+  parser.add_argument('--analyzer-main', help='Path to the analyzer executable',
                       required=True)
-  parser.add_argument("--dart", help="Path to the Dart executable",
+  parser.add_argument('--dart', help='Path to the Dart executable',
                       required=True)
-  parser.add_argument("--options", help="Path to analysis options")
+  parser.add_argument('--package-name', help='Name of the analyzed package',
+                      required=True)
+  parser.add_argument('--options', help='Path to analysis options')
   args = parser.parse_args()
 
   analyzer_file = os.path.join(args.root_build_dir, args.out)
@@ -34,15 +36,16 @@ def main():
   if not os.path.exists(analyzer_path):
     os.makedirs(analyzer_path)
 
-  options = ""
+  options = ''
   if args.options:
-    options = "--options=%s" % args.options
+    options = '--options=%s' % args.options
 
   analyzer_content = '''#!/bin/sh
 
+echo "Package : %s"
 %s --packages=%s %s --packages=%s %s %s
-''' % (args.dart, args.analyzer_packages, args.analyzer_main, args.dot_packages,
-       options, args.source_dir)
+''' % (args.package_name, args.dart, args.analyzer_packages, args.analyzer_main,
+       args.dot_packages, options, args.source_dir)
 
   with open(analyzer_file, 'w') as file:
       file.write(analyzer_content)
