120c04b9f8b0f1c4f51be21206403c128d77691d
Abdulla Kamar <abdulla@google.com>
2016-11-23 19:55:25 +1100

    [uapp] Add thread stress test.
    This continually creates and joins 1000 threads at a time, and displays timing
    results.
    
    Change-Id: I527e9f810710db928f296bfab50baf6bafa82c3d

---
+++ system/uapp/thread-stress/thread-stress.c
@@ -0,0 +1,51 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+#include <threads.h>
+
+#include <magenta/syscalls.h>
+
+#define NUM_THREADS 1000
+
+static int thread_func(void* arg) {
+  return 0;
+}
+
+static void thread_create(thrd_t* thread) {
+  int ret = thrd_create_with_name(thread, thread_func, NULL, "stress");
+  if (ret != thrd_success) {
+    printf("Failed to create thread: %d", ret);
+  }
+}
+
+static void thread_join(thrd_t thread) {
+  int ret = thrd_join(thread, NULL);
+  if (ret != thrd_success) {
+    printf("Failed to join thread: %d", ret);
+  }
+}
+
+int main(int argc, char** argv) {
+    printf("Running thread stress test...\n");
+    thrd_t thread[NUM_THREADS];
+    while (true) {
+        mx_time_t start = mx_time_get(MX_CLOCK_MONOTONIC);
+        for (int i = 0; i != NUM_THREADS; ++i) {
+            thread_create(&thread[i]);
+        }
+        mx_time_t create = mx_time_get(MX_CLOCK_MONOTONIC);
+        for (int i = 0; i != NUM_THREADS; ++i) {
+            thread_join(thread[i]);
+        }
+        mx_time_t join = mx_time_get(MX_CLOCK_MONOTONIC);
+        printf(
+            "%d threads in %.2fs (create %.2fs, join %.2fs)\n",
+            NUM_THREADS,
+            (join - start) / 1e9,
+            (create - start) / 1e9,
+            (join - create) / 1e9);
+    }
+    return 0;
+}
