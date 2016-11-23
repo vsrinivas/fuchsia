60e8ba16187eca65700803e4f2d0106685504be4
Craig Stout <cstout@google.com>
2016-11-23 11:03:00 -0800

    Add missing cleanup
    Change-Id: If476e8c7d7d81e589f9ac36bd401247420058c18

--- lib/magma/tests/unit_tests/test_msd_context.cc
+++ lib/magma/tests/unit_tests/test_msd_context.cc
@@ -11,18 +11,20 @@ TEST(MsdContext, CreateAndDestroy)
     magma::PlatformDevice* platform_device = TestPlatformDevice::GetInstance();
     ASSERT_NE(platform_device, nullptr);
 
-    auto msd_drv = msd_driver_create();
-    ASSERT_NE(msd_drv, nullptr);
+    auto msd_driver = msd_driver_create();
+    ASSERT_NE(msd_driver, nullptr);
 
-    auto msd_dev = msd_driver_create_device(msd_drv, platform_device->GetDeviceHandle());
-    ASSERT_NE(msd_dev, nullptr);
+    auto msd_device = msd_driver_create_device(msd_driver, platform_device->GetDeviceHandle());
+    ASSERT_NE(msd_device, nullptr);
 
-    auto msd_connection = msd_device_open(msd_dev, 0);
+    auto msd_connection = msd_device_open(msd_device, 0);
     ASSERT_NE(msd_connection, nullptr);
 
     auto msd_context = msd_connection_create_context(msd_connection);
     EXPECT_NE(msd_context, nullptr);
 
-    // just to catch crashes and such
     msd_context_destroy(msd_context);
+    msd_connection_close(msd_connection);
+    msd_device_destroy(msd_device);
+    msd_driver_destroy(msd_driver);
 }
