0e5022ee2929a93eac3f16f7b93320a3d46bfa60
George Kulakowski <kulakowski@google.com>
2016-11-23 17:12:42 -0800

    [musl] Support CLOCK_REALTIME
    This corresponds to MX_CLOCK_UTC. A little more work is needed to
    actually ensure that the RTC is set appropriately, but that isn't this
    function's concern.
    
    Change-Id: I5584855ef0a00f70e68a248b9d3ae0d56b6ad067

--- third_party/ulib/musl/src/time/clock_gettime.c
+++ third_party/ulib/musl/src/time/clock_gettime.c
@@ -1,4 +1,5 @@
 #include "libc.h"
+#include <errno.h>
 #include <stdint.h>
 #include <time.h>
 
@@ -7,7 +8,19 @@
 #define NS_PER_S (1000000000ull)
 
 int __clock_gettime(clockid_t clk, struct timespec* ts) {
-    mx_time_t now = _mx_time_get(MX_CLOCK_MONOTONIC);
+    uint32_t mx_clock;
+    switch (clk) {
+    case CLOCK_MONOTONIC:
+        mx_clock = MX_CLOCK_MONOTONIC;
+        break;
+    case CLOCK_REALTIME:
+        mx_clock = MX_CLOCK_UTC;
+        break;
+    default:
+        errno = EINVAL;
+        return -1;
+    }
+    mx_time_t now = _mx_time_get(mx_clock);
     ts->tv_sec = now / NS_PER_S;
     ts->tv_nsec = now % NS_PER_S;
     return 0;
