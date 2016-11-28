4ef9aacb1da3a30b4ed688bcaaa678099814d6a2
Jeff Brown <jeffbrown@google.com>
2016-11-27 23:49:41 -0800

    Use hardware timestamp counter for ticks.
    This patch matches what ktrace does on x86 so that we can correlate
    userspace trace timestamps with ktrace events.
    
    We should use an architecture-neutral VDSO function instead of inline
    assembly but it doesn't exist yet.
    
    Change-Id: I8e648e25f228daf2c47d10a3b5b31f59f7d8f05f

--- lib/trace/ticks.cc
+++ lib/trace/ticks.cc
@@ -4,9 +4,35 @@
 
 #include "apps/tracing/lib/trace/ticks.h"
 
+#include <magenta/prctl.h>
 #include <magenta/syscalls.h>
 
+#include "lib/ftl/logging.h"
+
 namespace tracing {
+
+// TODO(jeffbrown): Define VDSO functions to provide architecture-neutral
+// access to the tick counter.
+
+#if defined(__x86_64__)
+
+Ticks GetTicksNow() {
+  uint32_t tsc_low;
+  uint32_t tsc_hi;
+  __asm__ __volatile__("rdtsc" : "=a"(tsc_low), "=d"(tsc_hi));
+  return (static_cast<Ticks>(tsc_hi) << 32) | tsc_low;
+}
+
+Ticks GetTicksPerSecond() {
+  uintptr_t ticks_per_ms = 0;
+  mx_status_t status =
+      mx_thread_arch_prctl(0, ARCH_GET_TSC_TICKS_PER_MS, &ticks_per_ms);
+  FTL_CHECK(status == NO_ERROR);
+  FTL_CHECK(ticks_per_ms != 0);
+  return static_cast<Ticks>(ticks_per_ms) * 1000u;
+}
+
+#else
 
 Ticks GetTicksNow() {
   return mx_time_get(MX_CLOCK_MONOTONIC);
@@ -15,5 +41,7 @@ Ticks GetTicksNow() {
 Ticks GetTicksPerSecond() {
   return 1000000000u;
 }
+
+#endif
 
 }  // namespace tracing
