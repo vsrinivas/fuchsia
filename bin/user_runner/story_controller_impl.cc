b17e740777d885d0618088ad898305281472caef
Steffen Meschkat <mesch@google.com>
2016-11-29 16:29:00 -0800

    Provide latest StoryInfo from StoryController.GetInfo().
    Change-Id: Ibead149bb8914a66be6dbd3e093e2730bda86c05

--- bin/user_runner/story_controller_impl.cc
+++ bin/user_runner/story_controller_impl.cc
@@ -27,7 +27,11 @@ StoryControllerImpl::StoryControllerImpl(
 
 // |StoryController|
 void StoryControllerImpl::GetInfo(const GetInfoCallback& callback) {
-  callback(story_data_->story_info->Clone());
+  story_provider_impl_->GetStoryData(
+      story_data_->story_info->id, [this, callback](StoryDataPtr story_data) {
+        story_data_ = std::move(story_data);
+        callback(story_data_->story_info->Clone());
+      });
 }
 
 // |StoryController|
