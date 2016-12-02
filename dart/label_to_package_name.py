6435ece778607a2488bd7c392876d2aa2e86e403
P.Y. Laligand <pylaligand@google.com>
2016-12-02 13:04:31 -0800

    Take target name into account when generating a Dart package name.
    Change-Id: I8fc14b56209740fb04f43d4b99b60718281d5994

--- dart/label_to_package_name.py
+++ dart/label_to_package_name.py
@@ -7,14 +7,24 @@ import string
 import sys
 
 
+# For target //foo/bar:blah, the package name will be foo.bar..blah.
+# For default targets //foo/bar:bar, the package name will be foo.bar.
 def main():
   label = sys.argv[1]
   if not label.startswith("//"):
       print "expected label to start with //, got %s" % label
       return 1
-  # TODO: get_label_info(target_name, "dir") for //foo seems to return
-  # //bar/foo/foo, so strip the last component.
-  print ".".join(label[2:].split("/")[:-1])
+  base = label[2:]
+  separator_index = string.rfind(base, ":")
+  if separator_index < 0:
+      print "could not find target name in label %s" % label
+      return 1
+  path = base[:separator_index].split("/")
+  name = base[separator_index+1:]
+  if path[-1] == name:
+      print ".".join(path)
+  else:
+      print "%s..%s" % (".".join(path), name)
   return 0
 
 if __name__ == '__main__':
