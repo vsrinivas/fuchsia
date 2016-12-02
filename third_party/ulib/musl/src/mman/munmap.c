89aa3a67bf5d42668f96e8c9810184c14d88d1e9
Todd Eisenberger <teisenbe@google.com>
2016-12-02 15:38:59 -0800

    [musl] Make munmap only act on the given range
    Previously, munmap would ignore the *len* argument and unmap
    the entire mapping that contained *start*.
    
    Change-Id: I9905cc7558d4c6047ed5eb7b69c0b5392f98b955

--- third_party/ulib/musl/src/mman/munmap.c
+++ third_party/ulib/musl/src/mman/munmap.c
@@ -10,10 +10,7 @@ int __munmap(void* start, size_t len) {
     __vm_wait();
 
     uintptr_t ptr = (uintptr_t)start;
-    /* NOTE: this currently unmaps the entire region that start was mapped into.
-     * magenta does not yet support partial unmapping.
-     */
-    mx_status_t status = _mx_vmar_unmap(_mx_vmar_root_self(), ptr, 0);
+    mx_status_t status = _mx_vmar_unmap(_mx_vmar_root_self(), ptr, len);
     if (status < 0) {
         errno = EINVAL;
         return -1;
