10483c2573237cfe54e4a60d4918e9b047a75dfa
Doug Evans <dje@google.com>
2016-12-07 15:40:22 -0800

    Add vRun thread action list parser.
    Change-Id: I9e1c4dd072da6914172455db94e2ae3a2ac01a9e

---
+++ bin/gdbserver/thread-action-list.cc
@@ -0,0 +1,156 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "thread-action-list.h"
+
+#include "lib/ftl/logging.h"
+
+#include "util.h"
+
+// N.B. This file is included in the unittest, which does not
+// contain all the server/process/thread logic. Therefore do not
+// #include server.h, process.h, thread.h, etc.
+
+namespace debugserver {
+
+ThreadActionList::Entry::Entry(ThreadActionList::Action action,
+                               mx_koid_t pid,
+                               mx_koid_t tid)
+    : action_(action), pid_(pid), tid_(tid) {
+  FTL_DCHECK(pid_ != 0);
+  // A tid value of zero is ok.
+}
+
+void ThreadActionList::Entry::set_picked_tid(mx_koid_t tid) {
+  FTL_DCHECK(tid != 0);
+  FTL_DCHECK(tid_ == 0);
+  tid_ = tid;
+}
+
+bool ThreadActionList::Entry::Contains(mx_koid_t pid, mx_koid_t tid) const {
+  FTL_DCHECK(pid != 0 && pid != kAll);
+  FTL_DCHECK(tid != 0 && tid != kAll);
+  // A "0" meaning "arbitrary process" is resolved to the current process at
+  // construction time. A "0" meaning "arbitrary thread" must be resolved by
+  // the caller. If it cannot be resolved it is left as zero, and there is
+  // no match.
+  FTL_DCHECK(pid_ != 0);
+  if (pid != pid_ && pid_ != kAll)
+    return false;
+  if (tid != tid_ && tid_ != kAll)
+    return false;
+  return true;
+}
+
+bool ThreadActionList::DecodeAction(char c, ThreadActionList::Action* out_action) {
+  switch (c) {
+    case 'c':
+      *out_action = Action::kContinue;
+      break;
+    case 's':
+      *out_action = Action::kStep;
+      break;
+    default:
+      return false;
+  }
+
+  return true;
+}
+
+const char* ThreadActionList::ActionToString(ThreadActionList::Action action) {
+#define CASE_TO_STR(x) \
+  case x:              \
+    return #x
+  switch (action) {
+    CASE_TO_STR(Action::kNone);
+    CASE_TO_STR(Action::kContinue);
+    CASE_TO_STR(Action::kStep);
+    default:
+      break;
+  }
+#undef CASE_TO_STR
+  return "(unknown)";
+}
+
+ThreadActionList::ThreadActionList(const ftl::StringView& str,
+                                   mx_koid_t cur_proc) {
+  size_t len = str.size();
+  size_t s = 0;
+  Action default_action = Action::kNone;
+
+  if (len == 0) {
+    FTL_LOG(ERROR) << "Empty action string";
+    return;
+  }
+
+  while (s < len) {
+    size_t semi = str.find(';', s);
+    size_t n;
+    if (semi == str.npos)
+      n = len - s;
+    else
+      n = semi - s;
+    if (n == 0) {
+      FTL_LOG(ERROR) << "Missing action: " << str;
+      return;
+    }
+    Action action;
+    if (!DecodeAction(str[s], &action)) {
+      FTL_LOG(ERROR) << "Bad action: " << str;
+      return;
+    }
+    if (n == 1) {
+      if (default_action != Action::kNone) {
+        FTL_LOG(ERROR) << "Multiple default actions: " << str;
+        return;
+      }
+      default_action = action;
+    } else if (str[s + 1] == ':') {
+      bool has_pid;
+      // TODO(dje): koids are uint64_t
+      int64_t pid, tid;
+      if (!util::ParseThreadId(str.substr(s + 2, n - 2), &has_pid, &pid,
+                               &tid)) {
+        FTL_LOG(ERROR) << "Bad thread id in action: " << str;
+        return;
+      }
+      if ((has_pid && pid <= -2) || tid <= -2) {
+        FTL_LOG(ERROR) << "Bad thread id in action: " << str;
+        return;
+      }
+      if (!has_pid || pid == 0)
+        pid = cur_proc;
+      if (pid == -1 && tid != -1) {
+        FTL_LOG(ERROR) << "All processes and one thread: " << str;
+        return;
+      }
+      actions_.push_back(
+          Entry(action, pid == -1 ? kAll : pid, tid == -1 ? kAll : tid));
+    } else {
+      FTL_LOG(ERROR) << "Syntax error in action: " << str;
+      return;
+    }
+    if (semi == str.npos)
+      s = len;
+    else
+      s = semi + 1;
+  }
+
+  default_action_ = default_action;
+  valid_ = true;
+}
+
+ThreadActionList::Action ThreadActionList::GetAction(mx_koid_t pid,
+                                                     mx_koid_t tid) const {
+  FTL_DCHECK(pick_ones_resolved_);
+
+  for (auto e : actions_) {
+    if (e.Contains(pid, tid))
+      return e.action();
+  }
+
+  return default_action_;
+}
+
+}  // namespace debugserver
---
+++ bin/gdbserver/thread-action-list.h
@@ -0,0 +1,106 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <magenta/types.h>
+
+#include "lib/ftl/macros.h"
+#include "lib/ftl/strings/string_view.h"
+
+namespace debugserver {
+
+class Server;
+class Thread;
+
+// Utility class for holding the thread action list argument to vCont packets.
+// https://sourceware.org/gdb/current/onlinedocs/gdb/Packets.html
+
+class ThreadActionList final {
+ public:
+  // The kind of action to perform.
+  enum class Action {
+    // No action specified for this thread.
+    kNone,
+    // Continue the thread.
+    kContinue,
+    // Step the thread one instruction.
+    kStep,
+    // Other actions are not supported yet.
+  };
+
+  // Utility class to hold one entry in ThreadActionList.
+  class Entry final {
+   public:
+    Entry(Action action, mx_koid_t pid, mx_koid_t tid);
+    ~Entry() = default;
+
+    Action action() const { return action_; }
+    mx_koid_t pid() const { return pid_; }
+    mx_koid_t tid() const { return tid_; }
+
+    // Call this to upgrade a "pick one" entry (tid == 0) to the chosen value.
+    void set_picked_tid(mx_koid_t tid);
+
+    bool Contains(mx_koid_t pid, mx_koid_t tid) const;
+
+   private:
+    friend class ThreadActionList;
+    Action action_;
+    // N.B. While the remote protocol defines zero for pid/tid to mean "pick a
+    // random one" zero values do not end up in |pid_|. The "pick one" must be
+    // done before an Entry is created. The "pick one" for |tid_| is resolved
+    // later though, after the Entry is created.
+    mx_koid_t pid_;
+    mx_koid_t tid_;
+  };
+
+  // For pid,tid values, means "all processes" or "all threads".
+  // TODO(dje): This is a legitimate value, we "should" use a different value,
+  // but this is fine for now. The kernel reserves the first 1K, possibly we
+  // could use one of those.
+  static constexpr mx_koid_t kAll = ~0ull;
+
+  static bool DecodeAction(char c, Action* out_action);
+
+  static const char* ActionToString(Action action);
+
+  ThreadActionList(const ftl::StringView& str, mx_koid_t cur_proc);
+
+  ~ThreadActionList() = default;
+
+  bool valid() const { return valid_; }
+
+  // Call this after resolving all zero tid values, which means to "pick one".
+  // This must be called before calling GetAction.
+  // This exists to force caller to resolve zero tids ("pick one") to keep
+  // the resolution code separate. That step may need to evolve. Plus we'd
+  // have to stub out the resolution code in the unittest. Later.
+  void MarkPickOnesResolved() { pick_ones_resolved_ = true; }
+
+  // Return the action for |thread|.
+  Action GetAction(mx_koid_t pid, mx_koid_t tid) const;
+
+  Action default_action() const { return default_action_; }
+  const std::vector<Entry>& actions() const { return actions_; }
+
+ private:
+  ThreadActionList() = default;
+
+  // True if the contents are valid.
+  bool valid_ = false;
+
+  // True if "pick one" tid values have been resolved.
+  bool pick_ones_resolved_ = false;
+
+  Action default_action_ = Action::kNone;
+  std::vector<Entry> actions_;
+
+  FTL_DISALLOW_COPY_AND_ASSIGN(ThreadActionList);
+};
+
+}  // namespace debugserver
