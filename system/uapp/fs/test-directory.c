5d04b8180f3e3ed873286371aba2dde5571acfd4
Sean Klein <smklein@google.com>
2016-12-08 12:40:39 -0800

    [fs] Add max directory test and direntry coalescing test
    The "max directory size" test is currently disabled, as it causes an OOM
    on MemFS.
    
    Change-Id: I23da4be9c78309167aa158e911c84a57c88ddaca

--- system/uapp/fs/test-directory.c
+++ system/uapp/fs/test-directory.c
@@ -63,11 +63,92 @@ void test_directory_large(void) {
     // TODO(smklein): Verify contents
 }
 
-// TODO(smklein): Test direntry coalescing (fragment directory, unlink files,
-// attempt to allocate a larger direntry).
+void test_directory_max(void) {
+    printf("Test Directory (max)\n");
+
+    // Write the maximum number of files to a directory
+    int i = 0;
+    for (;; i++) {
+        char path[LARGE_PATH_LENGTH + 1];
+        TRY(snprintf(path, sizeof(path), "::%0*d", LARGE_PATH_LENGTH - 2, i));
+        if (i % 100 == 0) {
+            printf(" Allocating: %s\n", path);
+        }
+
+        int fd = open(path, O_RDWR | O_CREAT | O_EXCL, 0644);
+        if (fd < 0) {
+            printf("    wrote %d direntries\n", i);
+            break;
+        }
+        TRY(close(fd));
+    }
+
+    // Unlink all those files
+    for (; i >= 0; i--) {
+        char path[LARGE_PATH_LENGTH + 1];
+        TRY(snprintf(path, sizeof(path), "::%0*d", LARGE_PATH_LENGTH - 2, i));
+        TRY(unlink(path));
+    }
+}
+
+void test_directory_coalesce_helper(const int *unlink_order) {
+    const char *files[] = {
+        "::coalesce/aaaaaaaa",
+        "::coalesce/bbbbbbbb",
+        "::coalesce/cccccccc",
+        "::coalesce/dddddddd",
+        "::coalesce/eeeeeeee",
+    };
+    int num_files = sizeof(files) / sizeof(files[0]);
+
+    // Allocate a bunch of files in a directory
+    TRY(mkdir("::coalesce", 0755));
+    for (int i = 0; i < num_files; i++) {
+        int fd = TRY(open(files[i], O_RDWR | O_CREAT | O_EXCL, 0644));
+        TRY(close(fd));
+    }
+
+    // Unlink all those files in the order specified
+    for (int i = 0; i < num_files; i++) {
+        assert(0 <= unlink_order[i] && unlink_order[i] < num_files);
+        TRY(unlink(files[unlink_order[i]]));
+    }
+
+    TRY(unlink("::coalesce"));
+}
+
+void test_directory_coalesce(void) {
+    printf("Test Directory (coalesce)\n");
+
+    // Test some cases of coalescing, assuming the directory was filled
+    // according to allocation order. If it wasn't, this test should still pass,
+    // but there is no mechanism to check the "location of a direntry in a
+    // directory", so this is our best shot at "poking" the filesystem to try to
+    // coalesce.
+
+    // Case 1: Test merge-with-left
+    printf("  Test merge-with-left\n");
+    const int merge_with_left[] = {0, 1, 2, 3, 4};
+    test_directory_coalesce_helper(merge_with_left);
+
+    // Case 2: Test merge-with-right
+    printf("  Test merge-with-right\n");
+    const int merge_with_right[] = {4, 3, 2, 1, 0};
+    test_directory_coalesce_helper(merge_with_right);
+
+    // Case 3: Test merge-with-both
+    printf("  Test merge-with-both\n");
+    const int merge_with_both[] = {1, 3, 2, 0, 4};
+    test_directory_coalesce_helper(merge_with_both);
+}
 
 int test_directory(void) {
+    test_directory_coalesce();
     test_directory_filename_max();
     test_directory_large();
+    // TODO(smklein): Run this when MemFS can execute it without causing an OOM
+#if 0
+    test_directory_max();
+#endif
     return 0;
 }
