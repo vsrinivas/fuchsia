1e99cc4b9b2561489e6cb1189cd58f8b7dbb7b6b
Travis Geiselbrecht <travisg@google.com>
2016-12-15 17:47:23 -0800

    [kernel][arm64] move the mp related ops into a mp.h header
    Change-Id: Iefe33144be2fdf6d060191d9fb9bc3c22b7f23bb

--- kernel/arch/arm64/include/arch/arch_ops.h
+++ kernel/arch/arm64/include/arch/arch_ops.h
@@ -13,6 +13,7 @@
 #include <magenta/compiler.h>
 #include <reg.h>
 #include <arch/arm64.h>
+#include <arch/arm64/mp.h>
 
 __BEGIN_CDECLS
 
@@ -91,47 +92,6 @@ static inline void arch_spinloop_signal(void)
 static inline uint32_t arch_cycle_count(void)
 {
     return (uint32_t)ARM64_READ_SYSREG(pmccntr_el0);
-}
-
-/* use the cpu local thread context pointer to store current_thread */
-static inline struct thread *get_current_thread(void)
-{
-    return (struct thread *)ARM64_READ_SYSREG(tpidr_el1);
-}
-
-static inline void set_current_thread(struct thread *t)
-{
-    ARM64_WRITE_SYSREG(tpidr_el1, (uint64_t)t);
-}
-
-#if WITH_SMP
-static inline uint arch_curr_cpu_num(void)
-{
-    uint64_t mpidr =  ARM64_READ_SYSREG(mpidr_el1);
-    return (uint)((mpidr & ((1U << SMP_CPU_ID_BITS) - 1)) >> 8 << SMP_CPU_CLUSTER_SHIFT) | ((uint)mpidr & 0xff);
-}
-static inline uint arch_max_num_cpus(void)
-{
-    extern uint arm_num_cpus;
-
-    return arm_num_cpus;
-}
-#else
-static inline uint arch_curr_cpu_num(void)
-{
-    return 0;
-}
-static inline uint arch_max_num_cpus(void)
-{
-    return 1;
-}
-#endif
-
-static inline bool arch_in_int_handler(void)
-{
-    extern bool arm64_in_int_handler[SMP_MAX_CPUS];
-
-    return arm64_in_int_handler[arch_curr_cpu_num()];
 }
 
 static inline uint32_t arch_dcache_line_size(void) {
