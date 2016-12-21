4902c42c605e9fb021b5b086fc39d9776bd34eed
Travis Geiselbrecht <travisg@google.com>
2016-12-21 13:47:28 -0800

    [udev][intel-ethernet] add a DID for I210 card
    Change-Id: I258b298d72b6103cd570ea4af15c81e21abf8650

--- system/udev/intel-ethernet/ethernet.c
+++ system/udev/intel-ethernet/ethernet.c
@@ -241,10 +241,11 @@ mx_driver_t _driver_intel_ethernet = {
     },
 };
 
-MAGENTA_DRIVER_BEGIN(_driver_intel_ethernet, "intel-ethernet", "magenta", "0.1", 5)
+MAGENTA_DRIVER_BEGIN(_driver_intel_ethernet, "intel-ethernet", "magenta", "0.1", 6)
     BI_ABORT_IF(NE, BIND_PROTOCOL, MX_PROTOCOL_PCI),
     BI_ABORT_IF(NE, BIND_PCI_VID, 0x8086),
     BI_MATCH_IF(EQ, BIND_PCI_DID, 0x100E), // Qemu
     BI_MATCH_IF(EQ, BIND_PCI_DID, 0x15A3), // Broadwell
     BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1570), // Skylake
+    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1533), // I210 standalone
 MAGENTA_DRIVER_END(_driver_intel_ethernet)
