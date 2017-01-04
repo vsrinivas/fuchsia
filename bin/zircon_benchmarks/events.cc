db5edebf47ce4574250f406292ce1d035d0d6369
Abdulla Kamar <abdulla@google.com>
2017-01-04 15:54:43 +1100

    Add benchmark for event signalling.
    MG-87 #comment
    
    Change-Id: I8ff37cd93232e186045476e1911fe8660bcf89cf

---
+++ bin/zircon_benchmarks/events.cc
@@ -0,0 +1,31 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <benchmark/benchmark.h>
+#include <magenta/syscalls.h>
+
+class Event : public benchmark::Fixture {
+ private:
+  void SetUp(benchmark::State& state) override {
+    if (mx_event_create(0, &event) != NO_ERROR) {
+      state.SkipWithError("Failed to create channel");
+    }
+  }
+
+  void TearDown(benchmark::State& state) override {
+    mx_handle_close(event);
+  }
+
+ protected:
+  mx_handle_t event;
+};
+
+BENCHMARK_F(Event, Signal)(benchmark::State& state) {
+   while (state.KeepRunning()) {
+    if (mx_object_signal(event, 0, 0) != 0) {
+      state.SkipWithError("Failed to signal event");
+      return;
+    }
+  }
+}
