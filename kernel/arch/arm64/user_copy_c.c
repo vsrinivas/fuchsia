e93fe21d9022ced1521a72bead035cc8374bba15
Todd Eisenberger <teisenbe@google.com>
2017-01-12 21:43:58 +0000

    Revert "[arm][user_copy] Fix handling of kernel addresses"
    This reverts commit a6525830db607ab0be2045b76591c8dbe1e28bd4.
    
    Change-Id: I6b4efc9a1f78c7d3e547fdb6a96e26398f11d646

--- kernel/arch/arm64/user_copy_c.c
+++ kernel/arch/arm64/user_copy_c.c
@@ -17,18 +17,6 @@
 
 status_t arch_copy_from_user(void *dst, const void *src, size_t len)
 {
-    // We check if the address is a user pointer, since when ldtr/sttr
-    // cause a data fault, they do not indicate if the fault was caused
-    // by an access attempt to a privileged address.  Because of this, we
-    // don't know if the user copy failed due to being a bad address or
-    // if we just need to run the page fault handler and try again.  This
-    // check lets us skip the ambiguity and always try to run the page fault
-    // handler (since the fault will always be from trying to access an
-    // unprivileged page).
-    if (!is_user_address_range((vaddr_t)src, len)) {
-        return ERR_INVALID_ARGS;
-    }
-
     thread_t *thr = get_current_thread();
     status_t status =
             _arm64_copy_from_user(dst, src, len, &thr->arch.data_fault_resume);
@@ -37,10 +25,6 @@ status_t arch_copy_from_user(void *dst, const void *src, size_t len)
 
 status_t arch_copy_to_user(void *dst, const void *src, size_t len)
 {
-    if (!is_user_address_range((vaddr_t)dst, len)) {
-        return ERR_INVALID_ARGS;
-    }
-
     thread_t *thr = get_current_thread();
     status_t status =
             _arm64_copy_to_user(dst, src, len, &thr->arch.data_fault_resume);
