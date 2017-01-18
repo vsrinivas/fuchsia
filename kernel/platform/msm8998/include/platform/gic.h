hollanderic/magenta

[msm8998] Skeleton project/platform
Not intended to be functional on target, just a starting template for platform and make files.

Change-Id: I8f822ed145fa65ecad9a97443d6f762ea9269edb

kernel/platform/msm8998/include/platform/gic.h
@@ -1,0 +1,15 @@
+// Copyright 2016 The Fuchsia Authors
+// Copyright (c) 2014-2015 Travis Geiselbrecht
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <platform/msm8998.h>
+
+#define GICBASE(n)  (CPUPRIV_BASE_VIRT)
+#define GICD_OFFSET (0x00000)
+#define GICC_OFFSET (0x10000)
+

kernel/platform/msm8998/platform.c
@@ -1,0 +1,191 @@
+// Copyright 2016 The Fuchsia Authors
+// Copyright (c) 2015 Travis Geiselbrecht
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+#include <reg.h>
+#include <err.h>
+#include <debug.h>
+#include <trace.h>
+
+#include <dev/uart.h>
+#include <arch.h>
+#include <lk/init.h>
+#include <kernel/vm.h>
+#include <kernel/spinlock.h>
+#include <dev/timer/arm_generic.h>
+#include <dev/display.h>
+#include <dev/hw_rng.h>
+
+#include <platform.h>
+#include <dev/interrupt.h>
+#include <platform/msm8998.h>
+#include <platform/atag.h>
+
+#include <target.h>
+
+#include <libfdt.h>
+#include <arch/arm64.h>
+#include <arch/arm64/mmu.h>
+
+/* initial memory mappings. parsed by start.S */
+struct mmu_initial_mapping mmu_initial_mappings[] = {
+ /* 1GB of sdram space */
+ {
+     .phys = SDRAM_BASE,
+     .virt = KERNEL_BASE,
+     .size = MEMORY_APERTURE_SIZE,
+     .flags = 0,
+     .name = "memory"
+ },
+#if 0
+ /* peripherals */
+ {
+     .phys = BCM_PERIPH_BASE_PHYS,
+     .virt = BCM_PERIPH_BASE_VIRT,
+     .size = BCM_PERIPH_SIZE,
+     .flags = MMU_INITIAL_MAPPING_FLAG_DEVICE,
+     .name = "bcm peripherals"
+ },
+#endif
+ /* null entry to terminate the list */
+ { 0 }
+};
+
+#define DEBUG_UART 1
+
+extern void intc_init(void);
+
+extern void arm_reset(void);
+
+//static uint8_t * kernel_args;
+
+static pmm_arena_info_t arena = {
+    .name = "sdram",
+    .base = SDRAM_BASE,
+    .size = MEMSIZE,
+    .flags = PMM_ARENA_FLAG_KMAP,
+};
+
+void platform_init_mmu_mappings(void)
+{
+}
+
+void platform_early_init(void)
+{
+
+    uart_init_early();
+
+    //intc_init();
+
+    //arm_generic_timer_init(INTERRUPT_ARM_LOCAL_CNTPNSIRQ, 0);
+
+   /* look for a flattened device tree just before the kernel */
+    const void *fdt = (void *)KERNEL_BASE;
+    int err = fdt_check_header(fdt);
+    if (err >= 0) {
+        /* walk the nodes, looking for 'memory' */
+        int depth = 0;
+        int offset = 0;
+        for (;;) {
+            offset = fdt_next_node(fdt, offset, &depth);
+            if (offset < 0)
+                break;
+
+            /* get the name */
+            const char *name = fdt_get_name(fdt, offset, NULL);
+            if (!name)
+                continue;
+
+            /* look for the 'memory' property */
+            if (strcmp(name, "memory") == 0) {
+                //printf("Found memory in fdt\n");
+                int lenp;
+                const void *prop_ptr = fdt_getprop(fdt, offset, "reg", &lenp);
+                if (prop_ptr && lenp == 0x10) {
+                    /* we're looking at a memory descriptor */
+                    //uint64_t base = fdt64_to_cpu(*(uint64_t *)prop_ptr);
+                    uint64_t len = fdt64_to_cpu(*((const uint64_t *)prop_ptr + 1));
+
+                    /* trim size on certain platforms */
+#if ARCH_ARM
+                    if (len > 1024*1024*1024U) {
+                        len = 1024*1024*1024; /* only use the first 1GB on ARM32 */
+                        //printf("trimming memory to 1GB\n");
+                    }
+#endif
+
+                    /* set the size in the pmm arena */
+                    arena.size = len;
+                }
+            }
+        }
+    }
+
+    /* add the main memory arena */
+    pmm_add_arena(&arena);
+
+    /* reserve the first 64k of ram, which should be holding the fdt */
+    struct list_node list = LIST_INITIAL_VALUE(list);
+    pmm_alloc_range(MEMBASE, 0x80000 / PAGE_SIZE, &list);
+#if WITH_SMP
+    /* start remainder of cpus here, will need to figure this out for the gold vs silver */
+#endif
+}
+
+void platform_init(void)
+{
+    uart_init();
+}
+
+void target_init(void)
+{
+
+}
+
+void platform_dputs(const char* str, size_t len)
+{
+    while (len-- > 0) {
+        char c = *str++;
+        if (c == '\n') {
+            uart_putc(DEBUG_UART, '\r');
+        }
+        uart_putc(DEBUG_UART, c);
+    }
+}
+
+int platform_dgetc(char *c, bool wait)
+{
+    int ret = uart_getc(DEBUG_UART, wait);
+    if (ret == -1)
+        return -1;
+    *c = ret;
+    return 0;
+}
+
+/* Default implementation of panic time getc/putc.
+ * Just calls through to the underlying dputc/dgetc implementation
+ * unless the platform overrides it.
+ */
+__WEAK void platform_pputc(char c)
+{
+    return platform_dputc(c);
+}
+
+__WEAK int platform_pgetc(char *c, bool wait)
+{
+    return platform_dgetc(c, wait);
+}
+
+/* stub out the hardware rng entropy generator, which doesn't exist on this platform */
+size_t hw_rng_get_entropy(void* buf, size_t len, bool block) {
+    return 0;
+}
+
+/* no built in framebuffer */
+status_t display_get_info(struct display_info *info) {
+    return ERR_NOT_FOUND;
+}
+
+

kernel/project/target/msm-trapper.mk
@@ -1,0 +1,14 @@
+# Copyright 2016 The Fuchsia Authors
+# Copyright (c) 2008-2015 Travis Geiselbrecht
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+# main project for qemu-aarch64
+ARCH := arm64
+TARGET := msm-proto1
+ARM_CPU := cortex-a53
+
+
+

vsrinivas/fuchsia

kernel/platform/msm8998/include/platform/gic.h
@@ -1,0 +1,15 @@
+// Copyright 2016 The Fuchsia Authors
+// Copyright (c) 2014-2015 Travis Geiselbrecht
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <platform/msm8998.h>
+
+#define GICBASE(n)  (GICV3_BASE)
+#define GICD_OFFSET (0x00000)
+#define GICC_OFFSET (0x10000)
+

kernel/platform/msm8998/platform.c
@@ -1,0 +1,190 @@
+// Copyright 2016 The Fuchsia Authors
+// Copyright (c) 2015 Travis Geiselbrecht
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+#include <reg.h>
+#include <err.h>
+#include <debug.h>
+#include <trace.h>
+
+#include <dev/uart.h>
+#include <arch.h>
+#include <lk/init.h>
+#include <kernel/vm.h>
+#include <kernel/spinlock.h>
+#include <dev/timer/arm_generic.h>
+#include <dev/display.h>
+#include <dev/hw_rng.h>
+
+#include <platform.h>
+#include <dev/interrupt.h>
+#include <platform/msm8998.h>
+#include <platform/atag.h>
+
+#include <target.h>
+
+#include <libfdt.h>
+#include <arch/arm64.h>
+#include <arch/arm64/mmu.h>
+
+/* initial memory mappings. parsed by start.S */
+struct mmu_initial_mapping mmu_initial_mappings[] = {
+ /* 1GB of sdram space */
+ {
+     .phys = SDRAM_BASE,
+     .virt = KERNEL_BASE,
+     .size = MEMORY_APERTURE_SIZE,
+     .flags = 0,
+     .name = "memory"
+ },
+
+ /* peripherals */
+ {
+     .phys = MSM8998_PERIPH_BASE_PHYS,
+     .virt = MSM8998_PERIPH_BASE_VIRT,
+     .size = MSM8998_PERIPH_SIZE,
+     .flags = MMU_INITIAL_MAPPING_FLAG_DEVICE,
+     .name = "msm peripherals"
+ },
+ /* null entry to terminate the list */
+ { 0 }
+};
+
+#define DEBUG_UART 1
+
+extern void intc_init(void);
+
+extern void arm_reset(void);
+
+//static uint8_t * kernel_args;
+
+static pmm_arena_info_t arena = {
+    .name = "sdram",
+    .base = SDRAM_BASE,
+    .size = MEMSIZE,
+    .flags = PMM_ARENA_FLAG_KMAP,
+};
+
+void platform_init_mmu_mappings(void)
+{
+}
+
+void platform_early_init(void)
+{
+
+    uart_init_early();
+
+    //intc_init();
+
+    //arm_generic_timer_init(INTERRUPT_ARM_LOCAL_CNTPNSIRQ, 0);
+
+   /* look for a flattened device tree just before the kernel */
+    const void *fdt = (void *)KERNEL_BASE;
+    int err = fdt_check_header(fdt);
+    if (err >= 0) {
+        /* walk the nodes, looking for 'memory' */
+        int depth = 0;
+        int offset = 0;
+        for (;;) {
+            offset = fdt_next_node(fdt, offset, &depth);
+            if (offset < 0)
+                break;
+
+            /* get the name */
+            const char *name = fdt_get_name(fdt, offset, NULL);
+            if (!name)
+                continue;
+
+            /* look for the 'memory' property */
+            if (strcmp(name, "memory") == 0) {
+                //printf("Found memory in fdt\n");
+                int lenp;
+                const void *prop_ptr = fdt_getprop(fdt, offset, "reg", &lenp);
+                if (prop_ptr && lenp == 0x10) {
+                    /* we're looking at a memory descriptor */
+                    //uint64_t base = fdt64_to_cpu(*(uint64_t *)prop_ptr);
+                    uint64_t len = fdt64_to_cpu(*((const uint64_t *)prop_ptr + 1));
+
+                    /* trim size on certain platforms */
+#if ARCH_ARM
+                    if (len > 1024*1024*1024U) {
+                        len = 1024*1024*1024; /* only use the first 1GB on ARM32 */
+                        //printf("trimming memory to 1GB\n");
+                    }
+#endif
+
+                    /* set the size in the pmm arena */
+                    arena.size = len;
+                }
+            }
+        }
+    }
+
+    /* add the main memory arena */
+    pmm_add_arena(&arena);
+
+    /* reserve the first 64k of ram, which should be holding the fdt */
+    struct list_node list = LIST_INITIAL_VALUE(list);
+    pmm_alloc_range(MEMBASE, 0x80000 / PAGE_SIZE, &list);
+#if WITH_SMP
+    /* start remainder of cpus here, will need to figure this out for the gold vs silver */
+#endif
+}
+
+void platform_init(void)
+{
+    uart_init();
+}
+
+void target_init(void)
+{
+
+}
+
+void platform_dputs(const char* str, size_t len)
+{
+    while (len-- > 0) {
+        char c = *str++;
+        if (c == '\n') {
+            uart_putc(DEBUG_UART, '\r');
+        }
+        uart_putc(DEBUG_UART, c);
+    }
+}
+
+int platform_dgetc(char *c, bool wait)
+{
+    int ret = uart_getc(DEBUG_UART, wait);
+    if (ret == -1)
+        return -1;
+    *c = ret;
+    return 0;
+}
+
+/* Default implementation of panic time getc/putc.
+ * Just calls through to the underlying dputc/dgetc implementation
+ * unless the platform overrides it.
+ */
+__WEAK void platform_pputc(char c)
+{
+    return platform_dputc(c);
+}
+
+__WEAK int platform_pgetc(char *c, bool wait)
+{
+    return platform_dgetc(c, wait);
+}
+
+/* stub out the hardware rng entropy generator, which doesn't exist on this platform */
+size_t hw_rng_get_entropy(void* buf, size_t len, bool block) {
+    return 0;
+}
+
+/* no built in framebuffer */
+status_t display_get_info(struct display_info *info) {
+    return ERR_NOT_FOUND;
+}
+
+

kernel/project/target/msm-trapper.mk
@@ -1,0 +1,14 @@
+# Copyright 2016 The Fuchsia Authors
+# Copyright (c) 2008-2015 Travis Geiselbrecht
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+# main project for qemu-aarch64
+ARCH := arm64
+TARGET := msm-trapper
+ARM_CPU := cortex-a53
+
+
+