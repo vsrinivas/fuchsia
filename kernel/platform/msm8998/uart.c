615b8ac2ccbf124f7ef7f1c96727c4cec5f8e35c
Mike Voydanoff <voydanoff@google.com>
2017-01-19 09:57:31 -0800

    [msm8998][uart] Implement early UART write support
    UART read, IRQs, DMA, etc not supported yet.
    
    Change-Id: I449e7a50d1c92e38072ee5163bac8097b5010ff8

--- kernel/platform/msm8998/uart.c
+++ kernel/platform/msm8998/uart.c
@@ -8,22 +8,60 @@
 // TODO(gkalsi): Unify the two UART codepaths and use the port parameter to
 // select between the real uart and the miniuart.
 
-#include <assert.h>
-#include <dev/interrupt.h>
+#include <inttypes.h>
 #include <dev/uart.h>
-#include <kernel/thread.h>
 #include <lib/cbuf.h>
 #include <platform/msm8998.h>
-#include <platform/debug.h>
-#include <reg.h>
-#include <stdio.h>
-#include <trace.h>
 
-#define RXBUF_SIZE 16
+#define UART_BASE           (MSM8998_PERIPH_BASE_VIRT + 0xc1b0000)
+#define UART_SR             0x0008
+#define UART_SR_TX_READY    (1 << 2)
+#define UART_SR_TX_EMPTY    (1 << 3)
+
+#define UART_CR             0x0010
+#define UART_CR_CMD_RESET_TX_READY  (3 << 8)
+
+#define UARTDM_NCF_TX       0x40
+#define UARTDM_RF           0x0070
 
 static cbuf_t uart_rx_buf;
 
+static inline void yield(void)
+{
+    __asm__ volatile("yield" ::: "memory");
+}
+
+static void msm_printstr(const char* str, size_t count) {
+    char buffer[4];
+    uint32_t* bufptr = (uint32_t *)buffer;
+    size_t i, j;
+
+    while (!(readl(UART_BASE + UART_SR) & UART_SR_TX_EMPTY)) {
+        yield();
+    }
+    writel(UART_CR_CMD_RESET_TX_READY, UART_BASE + UARTDM_NCF_TX);
+    writel(count, UART_BASE + UARTDM_NCF_TX);
+    readl(UART_BASE + UARTDM_NCF_TX);
+
+    for (i = 0; i < count; ) {
+        size_t num_chars = (count > sizeof(buffer) ? sizeof(buffer) : count);
+
+        for (j = 0; j < num_chars; j++) {
+            buffer[j] = *str++;
+        }
+
+        // wait for TX ready
+        while (!(readl(UART_BASE + UART_SR) & UART_SR_TX_READY))
+            yield();
+
+        writel(*bufptr, UART_BASE + UARTDM_RF);
+
+        i += num_chars;
+    }
+}
+
 int uart_putc(int port, char c) {
+    msm_printstr(&c, 1);
     return 1;
 }
 
