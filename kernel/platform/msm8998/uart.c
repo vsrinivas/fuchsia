cea69cd2cf6ce56e1e35ac60e040449a1b06f5af
Mike Voydanoff <voydanoff@google.com>
2017-01-23 15:13:30 -0800

    [msm8998][uart] Implement uart_getc() in polling mode
    Reading from the UART now works
    Also eliminated use of deprecated registers, and misc cleanup
    
    Change-Id: I481dccfc1488afa4120971fb4920ff0603801d32

--- kernel/platform/msm8998/uart.c
+++ kernel/platform/msm8998/uart.c
@@ -13,20 +13,65 @@
 #include <lib/cbuf.h>
 #include <platform/msm8998.h>
 
-#define UART_BASE           (MSM8998_PERIPH_BASE_VIRT + 0xc1b0000)
-#define UART_SR             0x0008
-#define UART_SR_TX_READY    (1 << 2)
-#define UART_SR_TX_EMPTY    (1 << 3)
+#define UART_BASE   (MSM8998_PERIPH_BASE_VIRT + 0xc1b0000)
 
-#define UART_CR             0x0010
-#define UART_CR_CMD_RESET_TX_READY  (3 << 8)
+#define UART_MR1                            0x0000
+#define UART_MR1_RX_RDY_CTL                 (1 << 7)
 
-#define UARTDM_NCF_TX       0x40
-#define UARTDM_RF           0x0070
+#define UART_MR2                            0x0004
+#define UART_DM_DMRX                        0x0034
+#define UART_DM_N0_CHARS_FOR_TX             0x0040
+
+#define UART_DM_SR                          0x00A4
+#define UART_DM_SR_RXRDY                    (1 << 0)
+#define UART_DM_SR_RXFULL                   (1 << 1)
+#define UART_DM_SR_TXRDY                    (1 << 2)
+#define UART_DM_SR_TXEMT                    (1 << 3)
+#define UART_DM_SR_OVERRUN                  (1 << 4)
+#define UART_DM_SR_PAR_FRAME_ERR            (1 << 5)
+#define UART_DM_SR_RX_BREAK                 (1 << 6)
+#define UART_DM_SR_HUNT_CHAR                (1 << 7)
+
+#define UART_DM_CR                          0x00A8
+#define UART_DM_CR_RX_EN                    (1 << 0)
+#define UART_DM_CR_RX_DISABLE               (1 << 1)
+#define UART_DM_CR_TX_EN                    (1 << 2)
+#define UART_DM_CR_TX_DISABLE               (1 << 3)
+
+#define UART_DM_CR_CMD_RESET_RX             (1 << 4)
+#define UART_DM_CR_CMD_RESET_TX             (2 << 4)
+#define UART_DM_CR_CMD_RESET_ERR            (3 << 4)
+#define UART_DM_CR_CMD_RESET_BRK_CHG_INT    (4 << 4)
+#define UART_DM_CR_CMD_START_BRK            (5 << 4)
+#define UART_DM_CR_CMD_STOP_BRK             (6 << 4)
+#define UART_DM_CR_CMD_RESET_CTS_N          (7 << 4)
+#define UART_DM_CR_CMD_RESET_STALE_INT      (8 << 4)
+#define UART_DM_CR_CMD_SET_RFR              (13 << 4)
+#define UART_DM_CR_CMD_RESET_RFR            (14 << 4)
+
+#define UART_DM_CR_CMD_RESET_TX_READY       (3 << 8)
+#define UART_DM_CR_CMD_FORCE_STALE          (4 << 8)
+#define UART_DM_CR_CMD_ENABLE_STALE_EVENT_  (5 << 8)
+#define UART_DM_CR_CMD_DISABLE_STALE_EVENT  (6 << 8)
+
+#define UART_DM_RXFS                        0x0050
+#define UART_DM_RXFS_RX_BUFFER_STATE(r)     ((r >> 7) & 7)
+#define UART_DM_RXFS_FIFO_STATE(r)          ((r >> 14) | (r & 0x3F))
+
+#define UART_DM_TF                          0x0100
+#define UART_DM_RF(n)                       (0x0140 + 4 * (n))
 
 #define RXBUF_SIZE 16
 
 static cbuf_t uart_rx_buf;
+
+static inline uint32_t uart_read(int offset) {
+    return readl(UART_BASE + offset);
+}
+
+static inline void uart_write(uint32_t val, int offset) {
+    writel(val, UART_BASE + offset);
+}
 
 static inline void yield(void)
 {
@@ -38,12 +83,12 @@ static void msm_printstr(const char* str, size_t count) {
     uint32_t* bufptr = (uint32_t *)buffer;
     size_t i, j;
 
-    while (!(readl(UART_BASE + UART_SR) & UART_SR_TX_EMPTY)) {
+    while (!(uart_read(UART_DM_SR) & UART_DM_SR_TXEMT)) {
         yield();
     }
-    writel(UART_CR_CMD_RESET_TX_READY, UART_BASE + UARTDM_NCF_TX);
-    writel(count, UART_BASE + UARTDM_NCF_TX);
-    readl(UART_BASE + UARTDM_NCF_TX);
+    uart_write(UART_DM_CR_CMD_RESET_TX_READY, UART_DM_N0_CHARS_FOR_TX);
+    uart_write(count, UART_DM_N0_CHARS_FOR_TX);
+    uart_read(UART_DM_N0_CHARS_FOR_TX);
 
     for (i = 0; i < count; ) {
         size_t num_chars = (count > sizeof(buffer) ? sizeof(buffer) : count);
@@ -53,10 +98,10 @@ static void msm_printstr(const char* str, size_t count) {
         }
 
         // wait for TX ready
-        while (!(readl(UART_BASE + UART_SR) & UART_SR_TX_READY))
+        while (!(uart_read(UART_DM_SR) & UART_DM_SR_TXRDY))
             yield();
 
-        writel(*bufptr, UART_BASE + UARTDM_RF);
+        uart_write(*bufptr, UART_DM_TF);
 
         i += num_chars;
     }
@@ -68,6 +113,24 @@ int uart_putc(int port, char c) {
 }
 
 void uart_init(void) {
+    uint32_t temp;
+
+    uart_write(UART_DM_CR_TX_EN | UART_DM_CR_RX_EN, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_RESET_TX, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_RESET_RX, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_RESET_ERR, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_RESET_BRK_CHG_INT, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_RESET_CTS_N, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_SET_RFR, UART_DM_CR);
+
+    uart_write(0xFFFFFF, UART_DM_DMRX);
+    uart_write(UART_DM_CR_CMD_RESET_STALE_INT, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_DISABLE_STALE_EVENT, UART_DM_CR);
+
+    temp = uart_read(UART_MR1);
+    temp |= UART_MR1_RX_RDY_CTL;
+    uart_write(temp, UART_MR1);
+
     cbuf_initialize(&uart_rx_buf, RXBUF_SIZE);
 }
 
@@ -76,12 +139,51 @@ void uart_init_early(void) {
 
 int uart_getc(int port, bool wait) {
     cbuf_t* rxbuf = &uart_rx_buf;
-
     char c;
-    if (cbuf_read_char(rxbuf, &c, wait) == 1)
-        return c;
+    int count = 0;
+    uint32_t val, rxfs, sr;
+    char* bytes;
+    int i;
 
-    return -1;
+    // see if we have chars left from previous read
+    if (cbuf_read_char(rxbuf, &c, false) == 1) {
+        return c;
+    }
+
+    if ((uart_read(UART_DM_SR) & UART_DM_SR_OVERRUN)) {
+        printf("UART overrun!\n");
+        uart_write(UART_DM_CR_CMD_RESET_ERR, UART_DM_CR);
+    }
+
+    do {
+        rxfs = uart_read(UART_DM_RXFS);
+        sr = uart_read(UART_DM_SR);
+        count = UART_DM_RXFS_RX_BUFFER_STATE(rxfs);
+        if (!(sr & UART_DM_SR_RXRDY) && !count) {
+            if (wait) {
+                yield();
+            } else {
+                return -1;
+            }
+        }
+    } while (count == 0);
+
+    uart_write(UART_DM_CR_CMD_FORCE_STALE, UART_DM_CR);
+    val = uart_read(UART_DM_RF(0));
+    uart_read(UART_DM_RF(1));
+
+    uart_write(UART_DM_CR_CMD_RESET_STALE_INT, UART_DM_CR);
+    uart_write(0xFFFFFF, UART_DM_DMRX);
+
+    bytes = (char *)&val;
+    c = bytes[0];
+
+    // save remaining chars for next call
+    for (i = 1; i < count; i++) {
+        cbuf_write_char(rxbuf, bytes[i], false);
+    }
+
+    return c;
 }
 
 void uart_flush_tx(int port) {
