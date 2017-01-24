96f63f761da992b04390f82aad011f0e16a4e177
Alexandre Zani <azani@chromium.org>
2017-01-23 17:35:28 -0800

    Update the user runner to include the resolver service in the environment services.
    Change-Id: I855bd883deef6d0041c983e8411e0dab33151291

--- bin/user_runner/user_runner.cc
+++ bin/user_runner/user_runner.cc
@@ -9,22 +9,24 @@
 #include "apps/ledger/services/internal/internal.fidl.h"
 #include "apps/ledger/services/public/ledger.fidl.h"
 #include "apps/maxwell/services/launcher/launcher.fidl.h"
+#include "apps/maxwell/services/resolver/resolver.fidl.h"
 #include "apps/maxwell/services/suggestion/suggestion_provider.fidl.h"
 #include "apps/modular/lib/app/application_context.h"
 #include "apps/modular/lib/app/connect.h"
 #include "apps/modular/lib/fidl/array_to_string.h"
 #include "apps/modular/lib/fidl/scope.h"
 #include "apps/modular/services/story/story_provider.fidl.h"
-#include "apps/modular/services/user/user_context.fidl.h"
 #include "apps/modular/services/user/focus.fidl.h"
+#include "apps/modular/services/user/user_context.fidl.h"
 #include "apps/modular/services/user/user_runner.fidl.h"
 #include "apps/modular/services/user/user_shell.fidl.h"
 #include "apps/modular/src/story_runner/story_provider_impl.h"
 #include "apps/mozart/services/views/view_provider.fidl.h"
 #include "apps/mozart/services/views/view_token.fidl.h"
 #include "lib/fidl/cpp/bindings/binding_set.h"
-#include "lib/fidl/cpp/bindings/interface_ptr_set.h"
 #include "lib/fidl/cpp/bindings/interface_ptr.h"
+#include "lib/fidl/cpp/bindings/interface_ptr_set.h"
+#include "lib/ftl/functional/make_copyable.h"
 #include "lib/ftl/logging.h"
 #include "lib/ftl/macros.h"
 #include "lib/mtl/tasks/message_loop.h"
@@ -70,8 +72,7 @@ class UserRunnerImpl : public UserRunner {
  public:
   UserRunnerImpl(
       std::shared_ptr<ApplicationContext> application_context,
-      fidl::Array<uint8_t> user_id,
-      const fidl::String& user_shell,
+      fidl::Array<uint8_t> user_id, const fidl::String& user_shell,
       fidl::Array<fidl::String> user_shell_args,
       fidl::InterfaceHandle<ledger::LedgerRepository> ledger_repository,
       fidl::InterfaceHandle<UserContext> user_context,
@@ -79,14 +80,26 @@ class UserRunnerImpl : public UserRunner {
       fidl::InterfaceRequest<UserRunner> user_runner_request)
       : application_context_(application_context),
         binding_(this, std::move(user_runner_request)) {
-    binding_.set_connection_error_handler([this]{
-      delete this;
-    });
+    binding_.set_connection_error_handler([this] { delete this; });
 
     const std::string label = kStoriesScopeLabelPrefix + to_hex_string(user_id);
     ApplicationEnvironmentPtr parent_env;
     application_context_->environment()->Duplicate(parent_env.NewRequest());
+
+    ServiceProviderPtr parent_env_service_provider;
+    parent_env->GetServices(parent_env_service_provider.NewRequest());
     stories_scope_ = std::make_unique<Scope>(std::move(parent_env), label);
+
+    auto resolver_service_provider = GetServiceProvider(
+        "file:///system/apps/resolver_main", nullptr /* user_shell_args */);
+    stories_scope_->AddService<resolver::Resolver>(
+        ftl::MakeCopyable([resolver_service_provider =
+                               std::move(resolver_service_provider)](
+            fidl::InterfaceRequest<resolver::Resolver>
+                resolver_service_request) {
+          ConnectToService(resolver_service_provider.get(),
+                           std::move(resolver_service_request));
+        }));
 
     RunUserShell(user_shell, user_shell_args, std::move(view_owner_request));
 
@@ -126,8 +139,7 @@ class UserRunnerImpl : public UserRunner {
     auto suggestion_provider =
         ConnectToService<maxwell::SuggestionProvider>(maxwell_services.get());
 
-    user_shell_->Initialize(std::move(user_context),
-                            std::move(story_provider),
+    user_shell_->Initialize(std::move(user_context), std::move(story_provider),
                             std::move(suggestion_provider),
                             std::move(focus_controller_request));
   }
@@ -137,15 +149,14 @@ class UserRunnerImpl : public UserRunner {
   void Terminate(const TerminateCallback& done) override {
     FTL_DCHECK(user_shell_.is_bound());
     FTL_LOG(INFO) << "UserRunner::Terminate: Terminating UserRunner.";
-    user_shell_->Terminate([done]{
+    user_shell_->Terminate([done] {
       mtl::MessageLoop::GetCurrent()->PostQuitTask();
       done();
     });
   }
 
   ServiceProviderPtr GetServiceProvider(
-      const fidl::String& url,
-      const fidl::Array<fidl::String>* const args) {
+      const fidl::String& url, const fidl::Array<fidl::String>* const args) {
     auto launch_info = ApplicationLaunchInfo::New();
 
     ServiceProviderPtr services;
@@ -205,8 +216,7 @@ class UserRunnerApp : public UserRunnerFactory {
 
  private:
   // |UserRunnerFactory|
-  void Create(fidl::Array<uint8_t> user_id,
-              const fidl::String& user_shell,
+  void Create(fidl::Array<uint8_t> user_id, const fidl::String& user_shell,
               fidl::Array<fidl::String> user_shell_args,
               fidl::InterfaceHandle<ledger::LedgerRepository> ledger_repository,
               fidl::InterfaceHandle<UserContext> user_context,
@@ -214,8 +224,7 @@ class UserRunnerApp : public UserRunnerFactory {
               fidl::InterfaceRequest<UserRunner> user_runner_request) override {
     new UserRunnerImpl(application_context_, std::move(user_id), user_shell,
                        std::move(user_shell_args), std::move(ledger_repository),
-                       std::move(user_context),
-                       std::move(view_owner_request),
+                       std::move(user_context), std::move(view_owner_request),
                        std::move(user_runner_request));
   }
 
--- lib/fidl/scope.h
+++ lib/fidl/scope.h
@@ -9,6 +9,7 @@
 #include <string>
 
 #include "apps/modular/lib/app/application_context.h"
+#include "apps/modular/lib/app/service_provider_impl.h"
 #include "apps/modular/services/application/application_environment.fidl.h"
 #include "apps/modular/services/application/application_environment_host.fidl.h"
 #include "lib/fidl/cpp/bindings/binding.h"
@@ -16,17 +17,27 @@
 
 namespace modular {
 
-// A simple implementation of the ApplicationEnvironmentHost that just
-// provides fate separation of sets of applications run by one
-// application. The environment services are delegated to the parent
-// environment.
+// A simple implementation of the ApplicationEnvironmentHost that provides fate
+// separation of sets of applications run by one application. The environment
+// services are delegated to the parent environment.
 class Scope : public ApplicationEnvironmentHost {
  public:
   Scope(ApplicationEnvironmentPtr parent_env, const std::string& label)
       : binding_(this), parent_env_(std::move(parent_env)) {
+    ServiceProviderPtr parent_env_service_provider;
+    parent_env_->GetServices(parent_env_service_provider.NewRequest());
+    service_provider_impl_.SetDefaultServiceProvider(
+        std::move(parent_env_service_provider));
     parent_env_->CreateNestedEnvironment(binding_.NewBinding(),
                                          env_.NewRequest(),
                                          env_controller_.NewRequest(), label);
+  }
+
+  template <typename Interface>
+  void AddService(
+      ServiceProviderImpl::InterfaceRequestHandler<Interface> handler,
+      const std::string& service_name = Interface::Name_) {
+    service_provider_impl_.AddService(handler, service_name);
   }
 
   ApplicationEnvironmentPtr& environment() { return env_; }
@@ -35,15 +46,14 @@ class Scope : public ApplicationEnvironmentHost {
   // |ApplicationEnvironmentHost|:
   void GetApplicationEnvironmentServices(
       fidl::InterfaceRequest<ServiceProvider> environment_services) override {
-    // Pass requests for the environment services to the parent
-    // environment.
-    parent_env_->GetServices(std::move(environment_services));
+    service_provider_impl_.AddBinding(std::move(environment_services));
   }
 
   fidl::Binding<ApplicationEnvironmentHost> binding_;
   ApplicationEnvironmentPtr parent_env_;
   ApplicationEnvironmentPtr env_;
   ApplicationEnvironmentControllerPtr env_controller_;
+  ServiceProviderImpl service_provider_impl_;
 };
 
 }  // namespace modular
