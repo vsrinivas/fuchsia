fea2812af6ef18608c8e2799ad6cebfe3d0f644b
Dale Sather <dalesat@google.com>
2017-01-27 17:16:35 -0800

    apps/media: fix a fatal error in problem reporting
    Change-Id: I594454642fa19741fb8d624f45dc2696866f3f0c

--- bin/media/media_service/media_demux_impl.cc
+++ bin/media/media_service/media_demux_impl.cc
@@ -102,7 +102,7 @@ void MediaDemuxImpl::ReportProblem(const std::string& type,
                                    const std::string& details) {
   problem_ = Problem::New();
   problem_->type = type;
-  problem_->details = details.empty() ? nullptr : details;
+  problem_->details = details.empty() ? nullptr : fidl::String(details);
   status_publisher_.SendUpdates();
 }
 
