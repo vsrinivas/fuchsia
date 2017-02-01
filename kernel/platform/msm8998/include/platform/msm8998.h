e144860718b301c7c21e11337e9c20d8de518762
Mike Voydanoff <voydanoff@google.com>
2017-01-31 17:27:37 -0800

    [msm8998][uart] Use IRQ for receiving data instead of polling
    The shell is much snappier now that we are not spinning in a poll loop
    
    Change-Id: Ie786c0346c1a154132de4ac6e9554324d4c7dc94

--- kernel/platform/msm8998/include/platform/msm8998.h
+++ kernel/platform/msm8998/include/platform/msm8998.h
@@ -13,47 +13,11 @@
 
 #define MEMORY_APERTURE_SIZE        (30ULL * 1024 * 1024 * 1024)
 
-//#define iframe arm64_iframe_long
-
 #define MSM8998_GIC_BASE_PHYS       (MSM8998_PERIPH_BASE_PHYS + 0x17a00000)
 #define MSM8998_GIC_BASE_VIRT       (MSM8998_PERIPH_BASE_VIRT + 0x17a00000)
 
-#if 0
-/* map all of 0-1GB into kernel space in one shot */
-#define PERIPHERAL_BASE_PHYS (0)
-#define PERIPHERAL_BASE_SIZE (0x40000000UL) // 1GB
-
-#define PERIPHERAL_BASE_VIRT (0xffffffffc0000000ULL) // -1GB
-
-/* individual peripherals in this mapping */
-#define CPUPRIV_BASE_VIRT   (PERIPHERAL_BASE_VIRT + 0x08000000)
-#define CPUPRIV_BASE_PHYS   (PERIPHERAL_BASE_PHYS + 0x08000000)
-#define CPUPRIV_SIZE        (0x00020000)
-#define UART_BASE           (PERIPHERAL_BASE_VIRT + 0x09000000)
-#define UART_SIZE           (0x00001000)
-#define RTC_BASE            (PERIPHERAL_BASE_VIRT + 0x09010000)
-#define RTC_SIZE            (0x00001000)
-#define FW_CFG_BASE         (PERIPHERAL_BASE_VIRT + 0x09020000)
-#define FW_CFG_SIZE         (0x00001000)
-#define NUM_VIRTIO_TRANSPORTS 32
-#define VIRTIO_BASE         (PERIPHERAL_BASE_VIRT + 0x0a000000)
-#define VIRTIO_SIZE         (NUM_VIRTIO_TRANSPORTS * 0x200)
-#define PCIE_MMIO_BASE_PHYS ((paddr_t)(PERIPHERAL_BASE_PHYS + 0x10000000))
-#define PCIE_MMIO_SIZE      (0x2eff0000)
-#define PCIE_PIO_BASE_PHYS  ((paddr_t)(PERIPHERAL_BASE_PHYS + 0x3eff0000))
-#define PCIE_PIO_SIZE       (0x00010000)
-#define PCIE_ECAM_BASE_PHYS ((paddr_t)(PERIPHERAL_BASE_PHYS + 0x3f000000))
-#define PCIE_ECAM_SIZE      (0x01000000)
-#define GICV2M_FRAME_PHYS   (PERIPHERAL_BASE_PHYS + 0x08020000)
-
-#endif
-
 /* interrupts */
-#define ARM_GENERIC_TIMER_VIRTUAL_INT 27
 #define ARM_GENERIC_TIMER_PHYSICAL_INT 30
-#define UART0_INT       (32 + 1)
-#define PCIE_INT_BASE   (32 + 3)
-#define PCIE_INT_COUNT  (4)
-#define VIRTIO0_INT     (32 + 16)
+#define UART_INT       (32 + 114)
 
 #define MAX_INT 640
--- kernel/platform/msm8998/uart.c
+++ kernel/platform/msm8998/uart.c
@@ -9,6 +9,7 @@
 // select between the real uart and the miniuart.
 
 #include <inttypes.h>
+#include <dev/interrupt.h>
 #include <dev/uart.h>
 #include <lib/cbuf.h>
 #include <platform/msm8998.h>
@@ -19,6 +20,7 @@
 #define UART_MR1_RX_RDY_CTL                 (1 << 7)
 
 #define UART_MR2                            0x0004
+#define UART_DM_IPR                         0x0018
 #define UART_DM_DMRX                        0x0034
 #define UART_DM_N0_CHARS_FOR_TX             0x0040
 
@@ -48,20 +50,53 @@
 #define UART_DM_CR_CMD_RESET_STALE_INT      (8 << 4)
 #define UART_DM_CR_CMD_SET_RFR              (13 << 4)
 #define UART_DM_CR_CMD_RESET_RFR            (14 << 4)
+#define UART_DM_CR_CMD_CLEAR_TX_ERROR       (16 << 4)
+#define UART_DM_CR_CMD_CLEAR_TX_DONE        (17 << 4)
+#define UART_DM_CR_CMD_RESET_BRK_START_INT  (18 << 4)
+#define UART_DM_CR_CMD_RESET_BRK_END_INT    (19 << 4)
+#define UART_DM_CR_CMD_RESET_PAR_FRAME_ERR_INT (20 << 4)
+#define UART_DM_CR_CMD_CLEAR_TX_WR_ERROR_IRQ (25 << 4)
+#define UART_DM_CR_CMD_CLEAR_RX_RD_ERROR_IRQ (26 << 4)
+#define UART_DM_CR_CMD_CLEAR_TX_COMP_IRQ    (27 << 4)
+#define UART_DM_CR_CMD_CLEAR_WWT_IRQ        (28 << 4)
+#define UART_DM_CR_CMD_CLEAR_NO_FINISH_CMD_VIO_IRQ (30 << 4)
 
 #define UART_DM_CR_CMD_RESET_TX_READY       (3 << 8)
 #define UART_DM_CR_CMD_FORCE_STALE          (4 << 8)
-#define UART_DM_CR_CMD_ENABLE_STALE_EVENT_  (5 << 8)
+#define UART_DM_CR_CMD_ENABLE_STALE_EVENT   (5 << 8)
 #define UART_DM_CR_CMD_DISABLE_STALE_EVENT  (6 << 8)
 
 #define UART_DM_RXFS                        0x0050
 #define UART_DM_RXFS_RX_BUFFER_STATE(r)     ((r >> 7) & 7)
 #define UART_DM_RXFS_FIFO_STATE(r)          ((r >> 14) | (r & 0x3F))
 
+#define UART_DM_MISR                        0x00AC
+#define UART_DM_IMR                         0x00B0
+#define UART_DM_ISR                         0x00B4
+
+#define UART_IRQ_TXLEV                      (1 << 0)
+#define UART_IRQ_RXHUNT                     (1 << 1)
+#define UART_IRQ_RXBREAK_CHANGE             (1 << 2)
+#define UART_IRQ_RXSTALE                    (1 << 3)
+#define UART_IRQ_RXLEV                      (1 << 4)
+#define UART_IRQ_DELTA_CTS                  (1 << 5)
+#define UART_IRQ_CURRENT_CTS                (1 << 6)
+#define UART_IRQ_TX_READY                   (1 << 7)
+#define UART_IRQ_TX_ERROR                   (1 << 8)
+#define UART_IRQ_TX_DONE                    (1 << 9)
+#define UART_IRQ_RXBREAK_START              (1 << 10)
+#define UART_IRQ_RXBREAK_END                (1 << 11)
+#define UART_IRQ_PAR_FRAME_ERR_IRQ          (1 << 12)
+#define UART_IRQ_TX_WR_ERROR_IRQ            (1 << 13)
+#define UART_IRQ_RX_RD_ERROR_IRQ            (1 << 14)
+#define UART_IRQ_TXCOMP_IRQ                 (1 << 15)
+#define UART_IRQ_WWT_IRQ                    (1 << 16)
+#define UART_IRQ_NO_FINISH_CMD_VIOL         (1 << 17)
+
 #define UART_DM_TF                          0x0100
 #define UART_DM_RF(n)                       (0x0140 + 4 * (n))
 
-#define RXBUF_SIZE 16
+#define RXBUF_SIZE 128
 
 static cbuf_t uart_rx_buf;
 
@@ -112,8 +147,51 @@ int uart_putc(int port, char c) {
     return 1;
 }
 
+static enum handler_return uart_irq(void *arg)
+{
+    uint32_t misr = uart_read(UART_DM_MISR);
+    bool reschedule = false;
+
+    while (uart_read(UART_DM_SR) & UART_DM_SR_RXRDY) {
+        uint32_t rxfs = uart_read(UART_DM_RXFS);
+        // count is number of words in RX fifo that have data
+        int count = UART_DM_RXFS_FIFO_STATE(rxfs);
+
+        for (int i = 0; i < count; i++) {
+            uint32_t val = uart_read(UART_DM_RF(0));
+            char* bytes = (char *)&val;
+
+            for (int j = 0; j < 4; j++) {
+                // Unfortunately there is no documented way to get number of bytes in each word
+                // so we just need to ignore zero bytes here.
+                // Apparently this problem doesn't exist in DMA mode.
+                char ch = bytes[j];
+                if (ch) {
+                    cbuf_write_char(&uart_rx_buf, ch, false);
+                } else {
+                    break;
+                }
+            }
+        }
+        reschedule = true;
+    }
+
+    if (misr & UART_IRQ_RXSTALE) {
+        uart_write(UART_DM_CR_CMD_RESET_STALE_INT, UART_DM_CR);
+    }
+
+    // ask to receive more
+    uart_write(0xFFFFFF, UART_DM_DMRX);
+    uart_write(UART_DM_CR_CMD_ENABLE_STALE_EVENT, UART_DM_CR);
+
+    return (reschedule ? INT_RESCHEDULE : INT_NO_RESCHEDULE);
+}
+
 void uart_init(void) {
     uint32_t temp;
+
+    // disable interrupts
+    uart_write(0, UART_DM_IMR);
 
     uart_write(UART_DM_CR_TX_EN | UART_DM_CR_RX_EN, UART_DM_CR);
     uart_write(UART_DM_CR_CMD_RESET_TX, UART_DM_CR);
@@ -122,68 +200,31 @@ void uart_init(void) {
     uart_write(UART_DM_CR_CMD_RESET_BRK_CHG_INT, UART_DM_CR);
     uart_write(UART_DM_CR_CMD_RESET_CTS_N, UART_DM_CR);
     uart_write(UART_DM_CR_CMD_SET_RFR, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_CLEAR_TX_DONE, UART_DM_CR);
 
     uart_write(0xFFFFFF, UART_DM_DMRX);
-    uart_write(UART_DM_CR_CMD_RESET_STALE_INT, UART_DM_CR);
-    uart_write(UART_DM_CR_CMD_DISABLE_STALE_EVENT, UART_DM_CR);
+    uart_write(UART_DM_CR_CMD_ENABLE_STALE_EVENT, UART_DM_CR);
 
     temp = uart_read(UART_MR1);
     temp |= UART_MR1_RX_RDY_CTL;
     uart_write(temp, UART_MR1);
 
     cbuf_initialize(&uart_rx_buf, RXBUF_SIZE);
+
+    // enable RX interrupt
+    uart_write(UART_IRQ_RXSTALE, UART_DM_IMR);
+
+    register_int_handler(UART_INT, &uart_irq, NULL);
+    unmask_interrupt(UART_INT);
 }
 
 void uart_init_early(void) {
 }
 
 int uart_getc(int port, bool wait) {
-    cbuf_t* rxbuf = &uart_rx_buf;
-    char c;
-    int count = 0;
-    uint32_t val, rxfs, sr;
-    char* bytes;
-    int i;
-
-    // see if we have chars left from previous read
-    if (cbuf_read_char(rxbuf, &c, false) == 1) {
-        return c;
-    }
-
-    if ((uart_read(UART_DM_SR) & UART_DM_SR_OVERRUN)) {
-        printf("UART overrun!\n");
-        uart_write(UART_DM_CR_CMD_RESET_ERR, UART_DM_CR);
-    }
-
-    do {
-        rxfs = uart_read(UART_DM_RXFS);
-        sr = uart_read(UART_DM_SR);
-        count = UART_DM_RXFS_RX_BUFFER_STATE(rxfs);
-        if (!(sr & UART_DM_SR_RXRDY) && !count) {
-            if (wait) {
-                yield();
-            } else {
-                return -1;
-            }
-        }
-    } while (count == 0);
-
-    uart_write(UART_DM_CR_CMD_FORCE_STALE, UART_DM_CR);
-    val = uart_read(UART_DM_RF(0));
-    uart_read(UART_DM_RF(1));
-
-    uart_write(UART_DM_CR_CMD_RESET_STALE_INT, UART_DM_CR);
-    uart_write(0xFFFFFF, UART_DM_DMRX);
-
-    bytes = (char *)&val;
-    c = bytes[0];
-
-    // save remaining chars for next call
-    for (i = 1; i < count; i++) {
-        cbuf_write_char(rxbuf, bytes[i], false);
-    }
-
-    return c;
+    char ch;
+    size_t count = cbuf_read_char(&uart_rx_buf, &ch, wait);
+    return (count == 1 ? ch : -1);
 }
 
 void uart_flush_tx(int port) {
