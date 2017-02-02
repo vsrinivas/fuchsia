c29c3d5bdbb152923fbcd3ca1fff7ce22ef5e090
George Kulakowski <kulakowski@google.com>
2017-02-02 15:50:12 -0800

    [magenta] Remove unused time conversion utility
    Change-Id: I3bc5d05fa2fb4e3763d9205f6d72eed631f4d53c

--- kernel/lib/magenta/include/magenta/magenta.h
+++ kernel/lib/magenta/include/magenta/magenta.h
@@ -53,12 +53,6 @@ inline lk_time_t mx_time_to_lk(mx_time_t mxt) {
     return static_cast<lk_time_t>(temp & 0xffffffff);
 }
 
-// TODO(vtl): This should use mx_time_t, but currently everything operates using lk_time_t.
-inline lk_time_t timeout_to_deadline(lk_time_t now, lk_time_t timeout) {
-    // Note: |lk_time_t| is a |uint32_t|.
-    return (timeout > UINT32_MAX - now) ? INFINITE_TIME : now + timeout;
-}
-
 mx_status_t magenta_sleep(mx_time_t nanoseconds);
 
 // Determines if this handle is to a Resource object.
