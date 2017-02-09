504df1913750895001eb0d5a08a80a1af9ba7f45
Sean Klein <smklein@google.com>
2017-02-09 14:14:12 -0800

    [fs-management] migrate to new launchpad API
    Change-Id: I9d1004d9c55c6e4349bb89734ee8f94432c18799

--- system/ulib/fs-management/launch.c
+++ system/ulib/fs-management/launch.c
@@ -21,115 +21,56 @@
 #include <mxio/io.h>
 #include <mxio/util.h>
 
-static mx_status_t init_logs(mx_handle_t* hnd, uint32_t* ids, size_t* n) {
-    mx_status_t status;
-    if ((status = mx_log_create(0, &hnd[*n])) != NO_ERROR) {
-        fprintf(stderr, "init_logs: Could not create log\n");
-        return status;
-    }
-    ids[*n] = MX_HND_INFO(MX_HND_TYPE_MXIO_LOGGER, 0 | MXIO_FLAG_USE_FOR_STDIO);
-    *n = *n + 1;
-
-    return NO_ERROR;
-}
-
-static mx_status_t init_stdio(mx_handle_t* hnd, uint32_t* ids, size_t* n) {
-    mx_status_t status;
-    if ((status = mxio_clone_fd(1, 1, hnd + *n, ids + *n)) < 0) {
-        fprintf(stderr, "init_stdio: Could not clone stdout\n");
-        return status;
-    }
-    *n = *n + 1;
-
-    if ((status = mxio_clone_fd(2, 2, hnd + *n, ids + *n)) < 0) {
-        fprintf(stderr, "init_stdio: Could not clone stderr\n");
-        return status;
-    }
-    *n = *n + 1;
-    return NO_ERROR;
-}
 
 mx_status_t launch_logs_async(int argc, const char** argv, mx_handle_t* handles,
                               uint32_t* types, size_t len) {
-    mx_status_t status;
-    mx_handle_t hnd[8];
-    uint32_t ids[8];
-
-    size_t n = 0;
-    if ((status = mxio_clone_root(hnd, ids)) < 0) {
-        fprintf(stderr, "launch: Could not clone mxio root\n");
-        goto fail;
-    }
-    n++;
-    if ((status = init_logs(hnd, ids, &n)) < 0) {
-        goto fail;
-    }
-
-    if (n + len > countof(hnd)) {
-        fprintf(stderr, "launch: Too many handles\n");
-        status = ERR_INVALID_ARGS;
-        goto fail;
-    }
+    launchpad_t* lp;
+    launchpad_create(0, argv[0], &lp);
+    launchpad_clone(lp, LP_CLONE_ALL & (~LP_CLONE_MXIO_STDIO));
+    launchpad_load_from_file(lp, argv[0]);
+    launchpad_set_args(lp, argc, argv);
 
     for (size_t i = 0; i < len; i++) {
-        hnd[n] = handles[i];
-        ids[n] = types[i];
-        n++;
+        launchpad_add_handle(lp, handles[i], types[i]);
+    }
+    mx_handle_t h;
+    mx_log_create(0, &h);
+    if (h != MX_HANDLE_INVALID) {
+        launchpad_add_handle(lp, h, MX_HND_INFO(MX_HND_TYPE_MXIO_LOGGER,
+                                                0 | MXIO_FLAG_USE_FOR_STDIO));
     }
 
-    // Launchpad consumes 'hnd'
-    mx_handle_t proc;
-    if ((proc = launchpad_launch_mxio_etc(argv[0], argc, argv,
-                                          (const char* const*) environ,
-                                          n, hnd, ids)) <= 0) {
-        fprintf(stderr, "launch: cannot launch %s\n", argv[0]);
-        return proc;
+    mx_status_t status;
+    const char* errmsg;
+    if ((status = launchpad_go(lp, NULL, &errmsg)) != NO_ERROR) {
+        printf("fs-management: Cannot launch %s: %d: %s\n", argv[0], status, errmsg);
     }
+    return status;
+}
 
-    mx_handle_close(proc);
-    return status;
-fail:
-    for (size_t i = 0; i < n; i++) {
-        mx_handle_close(hnd[i]);
+static void init_stdio(launchpad_t** lp, int argc, const char** argv,
+                       mx_handle_t* handles, uint32_t* types, size_t len) {
+    launchpad_create(0, argv[0], lp);
+    launchpad_clone(*lp, LP_CLONE_ALL);
+    launchpad_load_from_file(*lp, argv[0]);
+    launchpad_set_args(*lp, argc, argv);
+
+    for (size_t i = 0; i < len; i++) {
+        launchpad_add_handle(*lp, handles[i], types[i]);
     }
-    return status;
 }
 
 mx_status_t launch_stdio_sync(int argc, const char** argv, mx_handle_t* handles,
                               uint32_t* types, size_t len) {
+    launchpad_t* lp;
+    init_stdio(&lp, argc, argv, handles, types, len);
+
     mx_status_t status;
-    mx_handle_t hnd[8];
-    uint32_t ids[8];
-
-    size_t n = 0;
-    if ((status = mxio_clone_root(hnd, ids)) < 0) {
-        fprintf(stderr, "launch: Could not clone mxio root\n");
-        goto fail;
-    }
-    n++;
-    if ((status = init_stdio(hnd, ids, &n)) < 0) {
-        goto fail;
-    }
-
-    if (n + len > countof(hnd)) {
-        fprintf(stderr, "launch: Too many handles\n");
-        status = ERR_INVALID_ARGS;
-        goto fail;
-    }
-
-    for (size_t i = 0; i < len; i++) {
-        hnd[n] = handles[i];
-        ids[n] = types[i];
-        n++;
-    }
-
-    // Launchpad consumes 'hnd'
     mx_handle_t proc;
-    if ((proc = launchpad_launch_mxio_etc(argv[0], argc, argv,
-                                          (const char* const*) environ,
-                                          n, hnd, ids)) <= 0) {
-        fprintf(stderr, "launch: cannot launch %s\n", argv[0]);
-        return proc;
+    const char* errmsg;
+    if ((status = launchpad_go(lp, &proc, &errmsg)) != NO_ERROR) {
+        printf("fs-management: Cannot launch %s: %d: %s\n", argv[0], status, errmsg);
+        return status;
     }
 
     status = mx_object_wait_one(proc, MX_PROCESS_SIGNALED, MX_TIME_INFINITE, NULL);
@@ -138,55 +79,18 @@ mx_status_t launch_stdio_sync(int argc, const char** argv, mx_handle_t* handles,
     }
     mx_handle_close(proc);
     return status;
-fail:
-    for (size_t i = 0; i < n; i++) {
-        mx_handle_close(hnd[i]);
-    }
-    return status;
 }
 
 mx_status_t launch_stdio_async(int argc, const char** argv, mx_handle_t* handles,
                                uint32_t* types, size_t len) {
+    launchpad_t* lp;
+    init_stdio(&lp, argc, argv, handles, types, len);
+
     mx_status_t status;
-    mx_handle_t hnd[8];
-    uint32_t ids[8];
-
-    size_t n = 0;
-    if ((status = mxio_clone_root(hnd, ids)) < 0) {
-        fprintf(stderr, "launch: Could not clone mxio root\n");
-        goto fail;
-    }
-    n++;
-    if ((status = init_stdio(hnd, ids, &n)) < 0) {
-        goto fail;
-    }
-
-    if (n + len > countof(hnd)) {
-        fprintf(stderr, "launch: Too many handles\n");
-        status = ERR_INVALID_ARGS;
-        goto fail;
-    }
-
-    for (size_t i = 0; i < len; i++) {
-        hnd[n] = handles[i];
-        ids[n] = types[i];
-        n++;
-    }
-
-    // Launchpad consumes 'hnd'
-    mx_handle_t proc;
-    if ((proc = launchpad_launch_mxio_etc(argv[0], argc, argv,
-                                          (const char* const*) environ,
-                                          n, hnd, ids)) <= 0) {
-        fprintf(stderr, "launch: cannot launch %s\n", argv[0]);
-        return proc;
-    }
-
-    mx_handle_close(proc);
-    return status;
-fail:
-    for (size_t i = 0; i < n; i++) {
-        mx_handle_close(hnd[i]);
+    const char* errmsg;
+    if ((status = launchpad_go(lp, NULL, &errmsg)) != NO_ERROR) {
+        printf("fs-management: Cannot launch %s: %d: %s\n", argv[0], status, errmsg);
+        return status;
     }
     return status;
 }
