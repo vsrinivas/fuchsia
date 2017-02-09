8b94ee9703d38488c18cfa4aa34db2bb6bafde9b
Przemyslaw Pietrzkiewicz <ppi@google.com>
2017-02-09 13:31:56 +0100

    Add empty example_todo_ledger.
    Change-Id: I38c6eb328f5502f3de0cd41e323c0b0532e44c3c

---
+++ examples/todo_ledger/lib/main.dart
@@ -0,0 +1,73 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:application.lib.app.dart/app.dart';
+import 'package:application.services/service_provider.fidl.dart';
+import 'package:apps.modular.services.story/link.fidl.dart';
+import 'package:apps.modular.services.story/module.fidl.dart';
+import 'package:apps.modular.services.story/story.fidl.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+
+import 'package:flutter/widgets.dart';
+
+final ApplicationContext _appContext = new ApplicationContext.fromStartupInfo();
+
+void _log(String msg) {
+  print('[Hello World Module] $msg');
+}
+
+/// An implementation of the [Module] interface.
+class ModuleImpl extends Module {
+  final ModuleBinding _binding = new ModuleBinding();
+
+  /// Bind an [InterfaceRequest] for a [Module] interface to this object.
+  void bind(InterfaceRequest<Module> request) {
+    _binding.bind(this, request);
+  }
+
+  /// Implementation of the Initialize(Story story, Link link) method.
+  @override
+  void initialize(
+      InterfaceHandle<Story> storyHandle,
+      InterfaceHandle<Link> linkHandle,
+      InterfaceHandle<ServiceProvider> incomingServices,
+      InterfaceRequest<ServiceProvider> outgoingServices) {
+    _log('ModuleImpl::initialize call');
+
+    StoryProxy story = new StoryProxy();
+    story.ctrl.bind(storyHandle);
+
+    LinkProxy link = new LinkProxy();
+    link.ctrl.bind(linkHandle);
+
+    // Do something with the story and link services.
+  }
+
+  /// Implementation of the Stop() => (); method.
+  @override
+  void stop(void callback()) {
+    _log('ModuleImpl::stop call');
+
+    // Do some clean up here.
+
+    // Invoke the callback to signal that the clean-up process is done.
+    callback();
+  }
+}
+
+/// Entry point for this module.
+void main() {
+  _log('Module started with ApplicationContext: $_appContext');
+
+  /// Add [ModuleImpl] to this application's outgoing ServiceProvider.
+  _appContext.outgoingServices.addServiceForName(
+    (request) {
+      _log('Received binding request for Module');
+      new ModuleImpl().bind(request);
+    },
+    Module.serviceName,
+  );
+
+  runApp(new Text("Hello, world!"));
+}
