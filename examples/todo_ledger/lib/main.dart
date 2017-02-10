1c53f4e4619fc74b14385d549cb585e737a0f15f
Przemyslaw Pietrzkiewicz <ppi@google.com>
2017-02-10 15:16:33 +0100

    example_todo_ledger: integrate with Ledger
    This patch integrates the example todo module with Ledger:
    
     - we now register for notifications and update the state whenever todo
       items stored in Ledger change
     - every 3 seconds we add a randomly generated todo item to Ledger
    
    In the next steps we will hook up input from UI.
    
    Change-Id: Ic762f30de89cfa6a6ba819725ce9b2ced739470b

--- examples/todo_ledger/lib/main.dart
+++ examples/todo_ledger/lib/main.dart
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
 import 'package:application.lib.app.dart/app.dart';
 import 'package:application.services/service_provider.fidl.dart';
+import 'package:apps.ledger.services.public/ledger.fidl.dart';
 import 'package:apps.modular.services.story/link.fidl.dart';
 import 'package:apps.modular.services.story/module.fidl.dart';
 import 'package:apps.modular.services.story/story.fidl.dart';
@@ -17,16 +21,107 @@ void _log(String msg) {
   print('[Todo Ledger Example] $msg');
 }
 
-/// An implementation of the [Module] interface.
-class TodoModule extends Module {
-  final ModuleBinding _binding = new ModuleBinding();
+dynamic _handleResponse(String description) {
+  return (Status status) {
+    if (status != Status.ok) {
+      _log("$description: $status");
+    }
+  };
+}
 
-  /// Bind an [InterfaceRequest] for a [Module] interface to this object.
-  void bind(InterfaceRequest<Module> request) {
-    _binding.bind(this, request);
+typedef void ItemsChangedCallback(Map);
+
+class Generator {
+  static const List<String> _actions = const [
+    "acquire",
+    "cancel",
+    "consider",
+    "draw",
+    "evaluate",
+    "celebrate",
+    "find",
+    "identify",
+    "meet with",
+    "plan",
+    "solve",
+    "study",
+    "talk to",
+    "think about",
+    "write an article about",
+    "check out",
+    "order",
+    "write a spec for",
+    "order",
+    "track down",
+    "memorize",
+    "git checkout",
+  ];
+
+  static const List<String> _objects = const [
+    "Christopher Columbus",
+    "PHP",
+    "a better way forward",
+    "a glass of wine",
+    "a good book on C++",
+    "a huge simulation we are all part of",
+    "a nice dinner out",
+    "a sheep",
+    "an AZERTY keyboard",
+    "hipster bars south of Pigalle",
+    "kittens",
+    "manganese",
+    "more cheese",
+    "some bugs",
+    "staticly-typed programming languages",
+    "the cryptographic primitives",
+    "the espresso machine",
+    "the law of gravity",
+    "the neighbor",
+    "the pyramids",
+    "the society",
+    "velocity",
+  ];
+
+  final Random _random = new Random(new DateTime.now().millisecondsSinceEpoch);
+
+  String makeContent() {
+    String action = _actions[_random.nextInt(_actions.length)];
+    String object = _objects[_random.nextInt(_objects.length)];
+    return "$action $object";
   }
 
-  /// Implementation of the Initialize(Story story, Link link) method.
+  List<int> makeKey() {
+    List<int> key = <int>[];
+    for (int i = 0; i < 16; i++) {
+      key.add(_random.nextInt(256));
+    }
+    return key;
+  }
+}
+
+/// An implementation of the [Module] interface.
+///
+/// Note that this class is currently conflating multiple responsibilities that
+/// we might want to separate: module bookkeeping and boilerplate, ledger
+/// access, notifications and data model all live here.
+class TodoModule extends Module implements PageWatcher {
+  final ModuleBinding _binding = new ModuleBinding();
+
+  final PageWatcherBinding _page_watcher_binding = new PageWatcherBinding();
+
+  final LedgerProxy _ledger = new LedgerProxy();
+
+  final PageProxy _page = new PageProxy();
+
+  final StoryProxy _story = new StoryProxy();
+
+  /// Watchers to be notified of changes in the list of items.
+  final List<ItemsChangedCallback> _callbacks = <ItemsChangedCallback>[];
+
+  /// Generator of the todo items.
+  final Generator _generator = new Generator();
+
+  /// Implementation of Module.initialize().
   @override
   void initialize(
       InterfaceHandle<Story> storyHandle,
@@ -35,24 +130,72 @@ class TodoModule extends Module {
       InterfaceRequest<ServiceProvider> outgoingServices) {
     _log('TodoModule::initialize()');
 
-    StoryProxy story = new StoryProxy();
-    story.ctrl.bind(storyHandle);
+    _story.ctrl.bind(storyHandle);
 
-    LinkProxy link = new LinkProxy();
-    link.ctrl.bind(linkHandle);
+    _story.getLedger(_ledger.ctrl.request(), _handleResponse("getLedger"));
+    _ledger.getRootPage(_page.ctrl.request(), _handleResponse("getRootPage"));
 
-    // Do something with the story and link services.
+    PageSnapshotProxy snapshot = new PageSnapshotProxy();
+    _page.getSnapshot(snapshot.ctrl.request(), _page_watcher_binding.wrap(this),
+        _handleResponse("Watch"));
+
+    _readItems(snapshot);
+    _changeItems();
   }
 
-  /// Implementation of the Stop() => (); method.
+  /// Implementation of Module.stop().
   @override
   void stop(void callback()) {
     _log('TodoModule::stop()');
-
-    // Do some clean up here.
+    _ledger.ctrl.stop();
+    _page.ctrl.stop();
+    _story.ctrl.stop();
 
     // Invoke the callback to signal that the clean-up process is done.
     callback();
+  }
+
+  /// Implementation of PageWatcher.onChange().
+  @override
+  void onChange(PageChange pageChange, callback) {
+    PageSnapshotProxy snapshot = new PageSnapshotProxy();
+    callback(snapshot.ctrl.request());
+    _readItems(snapshot);
+  }
+
+  /// Binds an [InterfaceRequest] for a [Module] interface to this object.
+  void bind(InterfaceRequest<Module> request) {
+    _binding.bind(this, request);
+  }
+
+  /// Adds a watcher to be notified when the todo items change.
+  void watch(ItemsChangedCallback callback) => _callbacks.add(callback);
+
+  void _readItems(PageSnapshotProxy snapshot) {
+    snapshot.getEntries(null, null,
+        (Status status, List<Entry> entries, List<int> token) {
+      if (status != Status.ok) {
+        _log("getEntries: $status");
+      }
+
+      Map<List<int>, String> items = <List<int>, String>{};
+      if (entries != null) {
+        for (var entry in entries) {
+          items[entry.key] = UTF8.decode(entry.value.bytes);
+        }
+      }
+
+      _callbacks.forEach((callback) => callback(items));
+      snapshot.ctrl.close();
+    });
+  }
+
+  Future<Null> _changeItems() async {
+    while (true) {
+      await new Future.delayed(const Duration(seconds: 3));
+      _page.put(_generator.makeKey(), UTF8.encode(_generator.makeContent()),
+          _handleResponse("Put"));
+    }
   }
 }
 
@@ -67,17 +210,19 @@ class TodoList extends StatefulWidget {
 
 class TodoListState extends State<TodoList> {
   TodoListState(TodoModule this._module) {
-    _items = ["solve the society", "discover gravity", "bla", "bazinga!"];
+    _module.watch((Map<List<int>, String> items) => setState(() {
+          _items = items;
+        }));
   }
 
   final TodoModule _module;
 
-  List<String> _items;
+  Map<List<int>, String> _items = <List<int>, String>{};
 
   @override
   Widget build(BuildContext context) {
     String res = "";
-    for (String item in _items) {
+    for (String item in _items.values) {
       res += " - $item\n";
     }
     return new Text(res);
@@ -101,6 +246,5 @@ void main() {
     title: 'Todo (Ledger)',
     home: new TodoList(module),
     theme: new ThemeData(primarySwatch: Colors.blue),
-    debugShowCheckedModeBanner: false,
   ));
 }
