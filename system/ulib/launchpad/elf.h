1446ecb4d0da4dcd65a77db3ea05b7d832311d65
Doug Evans <dje@google.com>
2017-02-20 10:43:10 -0800

    [ulib][launchpad] Add comment to elf_load_finish.
    Change-Id: Ibe77a8bca06bdaeaa5598f25690c75720e9896fc

--- system/ulib/launchpad/elf.h
+++ system/ulib/launchpad/elf.h
@@ -30,6 +30,7 @@ size_t elf_load_get_stack_size(elf_load_info_t* info);
 
 // Load the file's segments into the process.
 // If this fails, the state of the process address space is unspecified.
+// Regardless of success/failure this does not consume |vmo|.
 mx_status_t elf_load_finish(mx_handle_t vmar, elf_load_info_t* info,
                             mx_handle_t vmo,
                             mx_handle_t* segments_vmar,
