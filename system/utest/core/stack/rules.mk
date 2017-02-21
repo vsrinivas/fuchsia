248fded37223d5d59d93c01e4cd3e02c1a03c394
Roland McGrath <mcgrathr@google.com>
2017-02-20 16:13:33 -0800

    [utest][core] Add test of stack invariants
    Change-Id: I6d45bed4a0a5ec194d13e1564e928a3a7f019ebf

---
+++ system/utest/core/stack/rules.mk
@@ -0,0 +1,25 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := usertest
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/stack-test.c \
+
+MODULE_NAME := stack-test
+
+MODULE_LIBS := \
+    ulib/unittest ulib/mxio ulib/magenta ulib/musl
+
+MODULE_HEADER_DEPS := ulib/runtime
+
+ifeq ($(call TOBOOL,$(USE_CLANG)),true)
+MODULE_COMPILEFLAGS += -fsanitize=safe-stack -fstack-protector-all
+endif
+
+include make/module.mk
