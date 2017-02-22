5ffb88e1cfc1ba2a6d84bdd9881acdfff349ea0d
Mike Voydanoff <voydanoff@google.com>
2017-02-22 11:11:26 -0800

    [build][mdi] Build system support for MDI
    Set MDI_SRCS to the list of MDI source files build
    Set MDI_DEPS to any extra dependencies, like MDI files included in your MDI sources
    
    Then include make/mdi.mk in your target rules.mk to generate MDI for your build
    
    Change-Id: Iff6750d76a49d22a9be86207df87ec94c21fd1e3

---
+++ kernel/make/mdi.mk
@@ -0,0 +1,34 @@
+# Copyright 2017 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+MDI_BIN := $(BUILDDIR)/mdi.bin
+GEN_HEADER_DIR := $(BUILDDIR)/gen/include
+MDI_HEADER_DIR := $(GEN_HEADER_DIR)/mdi
+MDI_HEADER := $(MDI_HEADER_DIR)/mdi-defs.h
+
+# This is a big hammer, but we need to make sure $(MDI_HEADER) is generated
+# before it is included by any source files
+GLOBAL_SRCDEPS += $(MDI_HEADER)
+GLOBAL_INCLUDES += $(GEN_HEADER_DIR)
+
+MDIGEN := $(BUILDDIR)/tools/mdigen
+
+# add "MDI_" prefix and make header file symbols uppercase
+MDI_HEADER_OPTS := -p "MDI_" -u
+
+$(MDI_BIN): $(MDIGEN) $(MDI_SRCS) $(MDI_DEPS)
+	@echo generating $@
+	@$(MKDIR)
+	$(NOECHO)$(MDIGEN) -o $@ $(MDI_SRCS)
+
+$(MDI_HEADER): $(MDIGEN) $(MDI_SRCS) $(MDI_DEPS)
+	@echo generating $@
+	@$(MKDIR)
+	$(NOECHO)$(MDIGEN) $(MDI_SRCS) -h $@ $(MDI_HEADER_OPTS)
+
+GENERATED += $(MDI_BIN) $(MDI_HEADER)
+ADDITIONAL_BOOTDATA_ITEMS += $(MDI_BIN)
+EXTRA_BUILDDEPS += $(MDI_BIN) $(MDI_HEADER)
