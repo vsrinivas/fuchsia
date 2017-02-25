42126351e65559b43456be5e021696338c8dcd29
George Kulakowski <kulakowski@google.com>
2017-02-25 14:54:46 -0800

    [kernel][magenta] Fix tracing in handle reaper for arm64
    It so happens that on x86 the transitive includes pulled in PRIu64,
    but not on arm64.
    
    Change-Id: I3808637ffd821137a4b1cbfd041c09730c7f4dde

--- kernel/lib/magenta/handle_reaper.cpp
+++ kernel/lib/magenta/handle_reaper.cpp
@@ -4,6 +4,7 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT
 
+#include <inttypes.h>
 #include <lib/dpc.h>
 #include <kernel/mutex.h>
 #include <magenta/dispatcher.h>
