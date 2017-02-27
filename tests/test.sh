bccdde5b8985e8f4e57c7785f25c57f297435f52
Alhaad Gokhale <alhaad@google.com>
2017-02-27 10:47:16 -0800

    Add {suggestion, trigger} tests to modular_tests.json, test.sh.
    Change-Id: Idf55499f20421075968ab193b095d4baedd41e84

--- tests/test.sh
+++ tests/test.sh
@@ -16,5 +16,9 @@ netcp ${FUCHSIA_BUILD_DIR}/child_module :/system/apps/modular_tests/child_module
 netcp ${FUCHSIA_BUILD_DIR}/component_context_test :/system/apps/modular_tests/component_context_test
 netcp ${FUCHSIA_BUILD_DIR}/component_context_test_agent1 :/system/apps/modular_tests/component_context_test_agent1
 netcp ${FUCHSIA_BUILD_DIR}/component_context_test_agent2 :/system/apps/modular_tests/component_context_test_agent2
+netcp ${FUCHSIA_BUILD_DIR}/agent_trigger_test :/system/apps/modular_tests/agent_trigger_test
+netcp ${FUCHSIA_BUILD_DIR}/trigger_test_agent :/system/apps/modular_tests/trigger_test_agent
+netcp ${FUCHSIA_BUILD_DIR}/suggestion_proposal_test_module :/system/apps/modular_tests/suggestion_proposal_test_module
+netcp ${FUCHSIA_BUILD_DIR}/suggestion_test_user_shell :/system/apps/suggestion_test_user_shell
 
 ${FUCHSIA_DIR}/apps/modular/src/test_runner/run_test --test_file=$FUCHSIA_DIR/apps/modular/tests/modular_tests.json
