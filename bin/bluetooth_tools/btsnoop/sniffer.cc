3fc8c478798f0edbb8721f1fbade1d5396d0087f
Arman Uguray <armansito@google.com>
2017-02-27 15:10:21 -0800

    [tools][btsnoop] Added the btsnoop tool
    Added the btsnoop tool which sniffs HCI traffic and logs into a file
    in the standard BTSnoop format.
    
    Change-Id: I42b37a0dc2ba3a1238786f488e1e954f02d436d2

---
+++ bin/bluetooth_tools/btsnoop/sniffer.cc
@@ -0,0 +1,88 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sniffer.h"
+
+#include <iostream>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <magenta/device/bt-hci.h>
+#include <magenta/status.h>
+
+#include "apps/bluetooth/common/byte_buffer.h"
+#include "lib/ftl/logging.h"
+
+namespace btsnoop {
+
+Sniffer::Sniffer(const std::string& hci_dev_path,
+                 const std::string& log_file_path)
+    : hci_dev_path_(hci_dev_path),
+      log_file_path_(log_file_path),
+      handler_key_(0) {}
+
+Sniffer::~Sniffer() {
+  message_loop_.RemoveHandler(handler_key_);
+}
+
+bool Sniffer::Start() {
+  ftl::UniqueFD hci_dev(open(hci_dev_path_.c_str(), O_RDWR));
+  if (!hci_dev.is_valid()) {
+    std::perror("Failed to open HCI device");
+    return false;
+  }
+
+  mx_handle_t handle = MX_HANDLE_INVALID;
+  ssize_t ioctl_status = ioctl_bt_hci_get_snoop_channel(hci_dev.get(), &handle);
+  if (ioctl_status < 0) {
+    std::cout << "Failed to obtain snoop channel handle: "
+              << mx_status_get_string(ioctl_status) << std::endl;
+    return false;
+  }
+
+  FTL_DCHECK(handle != MX_HANDLE_INVALID);
+
+  if (!logger_.Initialize(log_file_path_)) {
+    std::cout << "failed to initialize BTSnoop logger";
+    return false;
+  }
+
+  handler_key_ = message_loop_.AddHandler(
+      this, handle, MX_CHANNEL_READABLE | MX_CHANNEL_PEER_CLOSED);
+  snoop_channel_ = mx::channel(handle);
+  hci_dev_ = std::move(hci_dev);
+
+  message_loop_.Run();
+
+  return true;
+}
+
+void Sniffer::OnHandleReady(mx_handle_t handle, mx_signals_t pending) {
+  FTL_DCHECK(handle == snoop_channel_.get());
+  FTL_DCHECK(pending & (MX_CHANNEL_READABLE | MX_CHANNEL_PEER_CLOSED));
+
+  uint32_t read_size;
+  mx_status_t status = snoop_channel_.read(0u, buffer_, sizeof(buffer_),
+                                           &read_size, nullptr, 0, nullptr);
+  if (status < 0) {
+    std::cout << "Failed to read snoop event bytes: "
+              << mx_status_get_string(status) << std::endl;
+    message_loop_.QuitNow();
+    return;
+  }
+
+  uint8_t flags = buffer_[0];
+  logger_.WritePacket(bluetooth::common::BufferView(buffer_ + 1, read_size - 1),
+                      flags == BT_HCI_SNOOP_FLAG_EVENT, false);
+}
+
+void Sniffer::OnHandleError(mx_handle_t handle, mx_status_t error) {
+  FTL_DCHECK(handle == snoop_channel_.get());
+  std::cout << "Error on snoop channel: " << mx_status_get_string(error)
+            << std::endl;
+  message_loop_.QuitNow();
+}
+
+}  // namespace btsnoop
---
+++ bin/bluetooth_tools/btsnoop/sniffer.h
@@ -0,0 +1,52 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <mx/channel.h>
+
+#include "apps/bluetooth/common/bt_snoop_logger.h"
+#include "apps/bluetooth/hci/hci.h"
+#include "apps/bluetooth/hci/hci_constants.h"
+#include "lib/ftl/files/unique_fd.h"
+#include "lib/ftl/macros.h"
+#include "lib/mtl/tasks/message_loop.h"
+#include "lib/mtl/tasks/message_loop_handler.h"
+
+namespace btsnoop {
+
+class Sniffer final : public ::mtl::MessageLoopHandler {
+ public:
+  Sniffer(const std::string& hci_dev_path, const std::string& log_file_path);
+  ~Sniffer();
+
+  // Starts the packet sniffing loop. Returns false if there is an error while
+  // setting up the snoop file and device snoop channel.
+  bool Start();
+
+ private:
+  // ::mtl::MessageLoopHandler overrides:
+  void OnHandleReady(mx_handle_t handle, mx_signals_t pending) override;
+  void OnHandleError(mx_handle_t handle, mx_status_t error) override;
+
+  std::string hci_dev_path_;
+  std::string log_file_path_;
+
+  ftl::UniqueFD hci_dev_;
+  mx::channel snoop_channel_;
+  bluetooth::common::BTSnoopLogger logger_;
+
+  mtl::MessageLoop::HandlerKey handler_key_;
+  mtl::MessageLoop message_loop_;
+
+  // For now we only sniff command and event packets so make the buffer large
+  // enough to fit the largest command packet plus 1-byte for the snoop flags.
+  uint8_t buffer_[sizeof(bluetooth::hci::CommandHeader) +
+                  bluetooth::hci::kMaxCommandPacketPayloadSize +
+                  1];
+
+  FTL_DISALLOW_COPY_AND_ASSIGN(Sniffer);
+};
+
+}  // namespace btsnoop
