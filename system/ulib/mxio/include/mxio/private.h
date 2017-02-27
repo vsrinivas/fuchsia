781857093ba3c8d07d47193003d37fb2bc7f7a6d
Brian Swetland <swetland@google.com>
2017-02-26 21:08:46 -0800

    [mxio] add needed includes to private.h
    Change-Id: I68abf07e379b1f6d6619be575bd337558debeaa2

--- system/ulib/mxio/include/mxio/private.h
+++ system/ulib/mxio/include/mxio/private.h
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <magenta/types.h>
+#include <stdint.h>
+
 #pragma once
 
 // WARNING: These interfaces exist to allow integration of mxio file
