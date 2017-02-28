7a4c0a61ad60abdde4351eef09a4d9609df00286
George Kulakowski <kulakowski@google.com>
2017-02-28 10:45:57 -0800

    Tweak header guards
    Change-Id: I7239bb09b1d4d31e1b22d37720b6718c12df1ffc

--- lib/trace/writer.h
+++ lib/trace/writer.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef APPS_TRACING_LIB_TRACE_INTERNAL_WRITER_H_
-#define APPS_TRACING_LIB_TRACE_INTERNAL_WRITER_H_
+#ifndef APPS_TRACING_LIB_TRACE_WRITER_H_
+#define APPS_TRACING_LIB_TRACE_WRITER_H_
 
 #include <stdint.h>
 
@@ -1027,4 +1027,4 @@ ToArgumentList(Arg&& arg, Args&&... args) {
 }  // namespace writer
 }  // namespace tracing
 
-#endif  // APPS_TRACING_LIB_TRACE_INTERNAL_WRITER_H_
+#endif  // APPS_TRACING_LIB_TRACE_WRITER_H_
