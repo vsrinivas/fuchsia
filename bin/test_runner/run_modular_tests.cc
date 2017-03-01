47cfd5061530ec62e219a64b033602a57b691f96
Ian McKellar <ianloic@google.com>
2017-03-01 09:55:22 -0800

    run_modular_tests invokes the tests directly
    This means it needs to be run under application_manager but all
    non-serial consoles are like that now.
    
    Change-Id: If0ab0386da064b40d7a1c569f0e885e9814b6386

--- bin/test_runner/run_modular_tests.cc
+++ bin/test_runner/run_modular_tests.cc
@@ -6,19 +6,68 @@
 // starts a test and exits with success or failure based on the success or
 // failure of the test.
 
+#include <launchpad/launchpad.h>
+#include <magenta/processargs.h>
+#include <magenta/status.h>
 #include <iostream>
-#include "apps/modular/src/test_runner/test_runner_client.h"
+
+#include "apps/modular/src/test_runner/test_runner.h"
+#include "apps/modular/src/test_runner/test_runner_config.h"
+#include "lib/ftl/strings/split_string.h"
+#include "lib/ftl/strings/string_printf.h"
+#include "lib/mtl/tasks/message_loop.h"
 
 constexpr char kModularTestsJson[] =
     "/system/apps/modular_tests/modular_tests.json";
 
+class ModularTestRunObserver : public modular::testing::TestRunObserver {
+ public:
+  ModularTestRunObserver(const std::string& test_id) : test_id_(test_id) {}
+  void SendMessage(const std::string& test_id,
+                   const std::string& operation,
+                   const std::string& msg) override {
+    FTL_CHECK(test_id == test_id_);
+  }
+
+  void Teardown(const std::string& test_id, bool success) override {
+    FTL_CHECK(test_id == test_id_);
+    success_ = success;
+    mtl::MessageLoop::GetCurrent()->PostQuitTask();
+  }
+
+  bool success() { return success_; }
+
+ private:
+  std::string test_id_;
+  bool success_;
+};
+
+bool RunTest(std::shared_ptr<app::ApplicationContext> app_context,
+             const std::string& url,
+             const std::vector<std::string>& args) {
+  uint64_t random_number;
+  size_t random_size;
+  mx_cprng_draw(&random_number, sizeof random_number, &random_size);
+  std::string test_id = ftl::StringPrintf("test_%lX", random_number);
+  ModularTestRunObserver observer(test_id);
+  modular::testing::TestRunContext context(app_context, &observer, test_id, url,
+                                           args);
+
+  mtl::MessageLoop::GetCurrent()->Run();
+
+  return observer.success();
+}
+
 int main(int argc, char** argv) {
-  modular::testing::TestRunnerClient client(kModularTestsJson);
+  mtl::MessageLoop loop;
+  modular::testing::TestRunnerConfig config(kModularTestsJson);
+  std::shared_ptr<app::ApplicationContext> app_context =
+      app::ApplicationContext::CreateFromStartupInfo();
 
   std::vector<std::string> test_names(argv + 1, argv + argc);
   if (test_names.empty()) {
     // If no tests were specified, run all tests.
-    test_names = client.test_names();
+    test_names = config.test_names();
   }
 
   std::vector<std::string> unknown;
@@ -26,11 +75,16 @@ int main(int argc, char** argv) {
   std::vector<std::string> succeeded;
 
   for (auto& test_name : test_names) {
-    if (!client.HasTestNamed(test_name)) {
+    if (!config.HasTestNamed(test_name)) {
       unknown.push_back(test_name);
       continue;
     }
-    if (client.RunTest(test_name)) {
+    std::vector<std::string> args =
+        ftl::SplitStringCopy(config.GetTestCommand(test_name), " ",
+                             ftl::kTrimWhitespace, ftl::kSplitWantNonEmpty);
+    auto url = args.front();
+    args.erase(args.begin());
+    if (RunTest(app_context, url, args)) {
       succeeded.push_back(test_name);
     } else {
       failed.push_back(test_name);
@@ -57,7 +111,7 @@ int main(int argc, char** argv) {
       std::cerr << " " << test_name << std::endl;
     }
     std::cerr << "Known tests are:" << std::endl;
-    for (auto& test_name : client.test_names()) {
+    for (auto& test_name : config.test_names()) {
       std::cerr << " " << test_name << std::endl;
     }
   }
--- bin/test_runner/test_runner.cc
+++ bin/test_runner/test_runner.cc
@@ -28,6 +28,8 @@
 // runner environment, without teardown; useful for testing modules, which may
 // not need to tear down device_runner.
 
+#include "apps/modular/src/test_runner/test_runner.h"
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -37,259 +39,84 @@
 #include <string>
 #include <vector>
 
-#include "application/lib/app/application_context.h"
-#include "apps/modular/lib/fidl/scope.h"
-#include "apps/modular/services/test_runner/test_runner.fidl.h"
-#include "apps/modular/src/test_runner/test_runner_store_impl.h"
 #include "lib/ftl/logging.h"
 #include "lib/ftl/strings/split_string.h"
 #include "lib/ftl/strings/string_view.h"
 #include "lib/ftl/synchronization/sleep.h"
-#include "lib/ftl/tasks/one_shot_timer.h"
 #include "lib/mtl/tasks/message_loop.h"
 
-// TODO(vardhan): Make listen port command-line configurable.
-constexpr uint16_t kListenPort = 8342;  // TCP port
-
 namespace modular {
-namespace {
+namespace testing {
 
-class TestRunnerImpl;
-class TestRunnerConnection;
-// TestRunContext represents a single run of a test. Given a test to run, it
-// runs it in a new ApplicationEnvironment and provides the environment a
-// TestRunner service to report completion. When tests are done, their
-// completion is reported back to TestRunnerConnection (which is responsible for
-// deleting TestRunContext). If the child application stops without reporting
-// anything, we declare the test a failure.
-class TestRunContext {
- public:
-  TestRunContext(std::shared_ptr<app::ApplicationContext> app_context,
-                 TestRunnerConnection* connection,
-                 const std::string& test_id,
-                 const std::string& url,
-                 const std::vector<std::string>& args);
-
-  // Called from TestRunnerImpl, the actual implemention of |TestRunner|.
-  void StopTrackingClient(TestRunnerImpl* client, bool crashed);
-  void Fail(const fidl::String& log_message);
-  void Teardown(TestRunnerImpl* teardown_client);
-
- private:
-  app::ApplicationControllerPtr child_app_controller_;
-  std::unique_ptr<Scope> child_env_scope_;
-
-  TestRunnerConnection* const test_runner_connection_;
-  std::vector<std::unique_ptr<TestRunnerImpl>> test_runner_clients_;
-  testing::TestRunnerStoreImpl test_runner_store_;
-
-  // This is a tag that we use to identify the test that was run. For now, it
-  // helps distinguish between multiple test outputs to the device log.
-  const std::string test_id_;
-  bool success_;
-
-  FTL_DISALLOW_COPY_AND_ASSIGN(TestRunContext);
-};
-
-// Implements the TestRunner service which is available in the
-// ApplicationEnvironment of the test processes. Calls made to this service are
-// forwarded and handled by TestRunContext.
-class TestRunnerImpl : public testing::TestRunner {
- public:
-  TestRunnerImpl(fidl::InterfaceRequest<testing::TestRunner> request,
-                 TestRunContext* test_run_context)
-      : binding_(this, std::move(request)),
-        test_run_context_(test_run_context) {
-    binding_.set_connection_error_handler([this]() {
-      if (waiting_for_termination_) {
-        FTL_LOG(INFO) << "Test " << test_name_ << " terminated as expected.";
-        // Client terminated but that was expected.
-        termination_timer_.Stop();
-        if (teardown_after_termination_) {
-          Teardown();
-        } else {
-          test_run_context_->StopTrackingClient(this, false);
-        }
-      } else {
-        test_run_context_->StopTrackingClient(this, true);
-      }
-    });
-  }
-
-  const std::string& GetTestName() const { return test_name_; }
-
-  bool waiting_for_termination() const { return waiting_for_termination_; }
-
-  void TeardownAfterTermination() {
-    teardown_after_termination_ = true;
-  }
-
- private:
-  // |TestRunner|
-  void Identify(const fidl::String& test_name) override {
-    test_name_ = test_name;
-  }
-
-  // |TestRunner|
-  void Fail(const fidl::String& log_message) override {
-    test_run_context_->Fail(log_message);
-  }
-  // |TestRunner|
-  void Done() override { test_run_context_->StopTrackingClient(this, false); }
-  // |TestRunner|
-  void Teardown() override { test_run_context_->Teardown(this); }
-  // |TestRunner|
-  void WillTerminate(double withinSeconds) override {
+TestRunnerImpl::TestRunnerImpl(
+    fidl::InterfaceRequest<testing::TestRunner> request,
+    TestRunContext* test_run_context)
+    : binding_(this, std::move(request)), test_run_context_(test_run_context) {
+  binding_.set_connection_error_handler([this] {
     if (waiting_for_termination_) {
-      Fail(test_name_ + " called WillTerminate more than once.");
-      return;
-    }
-    waiting_for_termination_ = true;
-    termination_timer_.Start(
-        mtl::MessageLoop::GetCurrent()->task_runner().get(),
-        [this, withinSeconds] {
-          FTL_LOG(ERROR) << test_name_ << " termination timed out after "
-                         << withinSeconds << "s.";
-          binding_.set_connection_error_handler(nullptr);
-          Fail("Termination timed out.");
-          if (teardown_after_termination_) {
-            Teardown();
-          }
-          test_run_context_->StopTrackingClient(this, false);
-        },
-        ftl::TimeDelta::FromSecondsF(withinSeconds));
-  }
-
-  fidl::Binding<testing::TestRunner> binding_;
-  TestRunContext* test_run_context_;
-  std::string test_name_ = "UNKNOWN";
-  bool waiting_for_termination_ = false;
-  ftl::OneShotTimer termination_timer_;
-  bool teardown_after_termination_ = false;
-
-  FTL_DISALLOW_COPY_AND_ASSIGN(TestRunnerImpl);
-};
-
-// Represents a client connection, and is self-owned (it will exit the
-// MessageLoop upon completion). TestRunnerConnection receives commands to run
-// tests and runs them one at a time using TestRunContext.
-class TestRunnerConnection {
- public:
-  TestRunnerConnection(int socket_fd,
-                       std::shared_ptr<app::ApplicationContext> app_context)
-      : app_context_(app_context), socket_(socket_fd) {}
-
-  void Start() {
-    FTL_CHECK(!test_context_);
-    ReadAndRunCommand();
-  }
-
-  void SendMessage(const std::string& test_id,
-                   const std::string& operation,
-                   const std::string& msg) {
-    std::stringstream stream;
-    stream << test_id << " " << operation << " " << msg << "\n";
-
-    std::string bytes = stream.str();
-    FTL_CHECK(write(socket_, bytes.data(), bytes.size()) > 0);
-  }
-
-  // Called by TestRunContext when it has finished running its test. This will
-  // trigger reading more commands from TCP socket.
-  void Teardown(const std::string& test_id, bool success) {
-    FTL_CHECK(test_context_);
-
-    // IMPORTANT: leave this log here, exactly as it is. Currently, tests
-    // launched from host (e.g. Linux) grep for this text to figure out the
-    // amount of the log to associate with the test.
-    FTL_LOG(INFO) << "test_runner: teardown " << test_id
-                  << " success=" << success;
-
-    SendMessage(test_id, "teardown", success ? "pass" : "fail");
-    test_context_.reset();
-    Start();
-  }
-
- private:
-  ~TestRunnerConnection() {
-    close(socket_);
-    mtl::MessageLoop::GetCurrent()->PostQuitTask();
-  }
-
-  // Read and entire command (which consists of one line) and return it.
-  // Can be called again to read the next command. Blocks until an entire line
-  // has been read.
-  std::string ReadCommand() {
-    char buf[1024];
-    // Read until we see a new line.
-    auto newline_pos = command_buffer_.find("\n");
-    while (newline_pos == std::string::npos) {
-      ssize_t n = read(socket_, buf, sizeof(buf));
-      if (n <= 0) {
-        return std::string();
+      FTL_LOG(INFO) << "Test " << test_name_ << " terminated as expected.";
+      // Client terminated but that was expected.
+      termination_timer_.Stop();
+      if (teardown_after_termination_) {
+        Teardown();
+      } else {
+        test_run_context_->StopTrackingClient(this, false);
       }
-      command_buffer_.append(buf, n);
-      newline_pos = command_buffer_.find("\n");
+    } else {
+      test_run_context_->StopTrackingClient(this, true);
     }
+  });
+}
 
-    // Consume only until the new line (and leave the rest of the bytes for
-    // subsequent ReadCommand()s.
-    auto retval = command_buffer_.substr(0, newline_pos + 1);
-    command_buffer_.erase(0, newline_pos + 1);
-    return retval;
+const std::string& TestRunnerImpl::test_name() const { return test_name_; }
+
+bool TestRunnerImpl::waiting_for_termination() const {
+  return waiting_for_termination_;
+}
+
+void TestRunnerImpl::TeardownAfterTermination() {
+  teardown_after_termination_ = true;
+}
+
+void TestRunnerImpl::Identify(const fidl::String& test_name) {
+  test_name_ = test_name;
+}
+
+void TestRunnerImpl::Fail(const fidl::String& log_message) {
+  test_run_context_->Fail(log_message);
+}
+
+void TestRunnerImpl::Done() {
+  test_run_context_->StopTrackingClient(this, false);
+}
+
+void TestRunnerImpl::Teardown() { test_run_context_->Teardown(this); }
+
+void TestRunnerImpl::WillTerminate(const double withinSeconds) {
+  if (waiting_for_termination_) {
+    Fail(test_name_ + " called WillTerminate more than once.");
+    return;
   }
-
-  // Read an entire line representing the command to run and run it. When the
-  // test has finished running, TestRunnerConnection::Teardown is invoked. We do
-  // not read any further commands until that has happened.
-  void ReadAndRunCommand() {
-    std::string command = ReadCommand();
-    if (command.empty()) {
-      delete this;
-      return;
-    }
-
-    // command_parse[0] = "run"
-    // command_parse[1] = test_id
-    // command_parse[2] = url
-    // command_parse[3..] = args (optional)
-    std::vector<std::string> command_parse = ftl::SplitStringCopy(
-        command, " ", ftl::kTrimWhitespace, ftl::kSplitWantNonEmpty);
-
-    FTL_CHECK(command_parse.size() >= 3)
-        << "Not enough args. Must be: `run <test id> <command to run>`";
-    FTL_CHECK(command_parse[0] == "run")
-        << command_parse[0] << " is not a supported command.";
-
-    FTL_LOG(INFO) << "test_runner: run " << command_parse[1];
-
-    std::vector<std::string> args;
-    for (size_t i = 3; i < command_parse.size(); i++) {
-      args.push_back(std::move(command_parse[i]));
-    }
-
-    // When TestRunContext is done with the test, it calls
-    // TestRunnerConnection::Teardown().
-    test_context_.reset(new TestRunContext(app_context_, this, command_parse[1],
-                                           command_parse[2], args));
-  }
-
-  std::shared_ptr<app::ApplicationContext> app_context_;
-  std::unique_ptr<TestRunContext> test_context_;
-
-  // Posix fd for the TCP connection.
-  const int socket_;
-  std::string command_buffer_;
-
-  FTL_DISALLOW_COPY_AND_ASSIGN(TestRunnerConnection);
-};
+  waiting_for_termination_ = true;
+  termination_timer_.Start(mtl::MessageLoop::GetCurrent()->task_runner().get(),
+                           [this, withinSeconds] {
+                             FTL_LOG(ERROR) << test_name_
+                                            << " termination timed out after "
+                                            << withinSeconds << "s.";
+                             binding_.set_connection_error_handler(nullptr);
+                             Fail("Termination timed out.");
+                             if (teardown_after_termination_) {
+                               Teardown();
+                             }
+                             test_run_context_->StopTrackingClient(this, false);
+                           },
+                           ftl::TimeDelta::FromSecondsF(withinSeconds));
+}
 
 TestRunContext::TestRunContext(
     std::shared_ptr<app::ApplicationContext> app_context,
-    TestRunnerConnection* connection,
-    const std::string& test_id,
-    const std::string& url,
-    const std::vector<std::string>& args)
+    TestRunObserver* connection, const std::string& test_id,
+    const std::string& url, const std::vector<std::string>& args)
     : test_runner_connection_(connection), test_id_(test_id), success_(true) {
   // 1. Make a child environment to run the command.
   app::ApplicationEnvironmentPtr parent_env;
@@ -335,7 +162,7 @@ void TestRunContext::Fail(const fidl::String& log_msg) {
 
 void TestRunContext::StopTrackingClient(TestRunnerImpl* client, bool crashed) {
   if (crashed) {
-    FTL_LOG(WARNING) << client->GetTestName()
+    FTL_LOG(WARNING) << client->test_name()
                      << " finished without calling modular::testing::Done().";
     test_runner_connection_->Teardown(test_id_, false);
     return;
@@ -354,17 +181,17 @@ void TestRunContext::StopTrackingClient(TestRunnerImpl* client, bool crashed) {
 void TestRunContext::Teardown(TestRunnerImpl* teardown_client) {
   bool waiting_for_termination = false;
   for (auto& client : test_runner_clients_) {
-    if (teardown_client == &*client) {
+    if (teardown_client == client.get()) {
       continue;
     }
     if (client->waiting_for_termination()) {
       client->TeardownAfterTermination();
       FTL_LOG(INFO) << "Teardown blocked by test waiting for termination: "
-                    << client->GetTestName();
+                    << client->test_name();
       waiting_for_termination = true;
       continue;
     }
-    FTL_LOG(ERROR) << "Test " << client->GetTestName()
+    FTL_LOG(ERROR) << "Test " << client->test_name()
                    << " not done before Teardown().";
     success_ = false;
   }
@@ -375,72 +202,5 @@ void TestRunContext::Teardown(TestRunnerImpl* teardown_client) {
   }
 }
 
-// TestRunnerTCPServer is a TCP server that accepts connections and launches
-// them as TestRunnerConnection.
-class TestRunnerTCPServer {
- public:
-  TestRunnerTCPServer(uint16_t port)
-      : app_context_(app::ApplicationContext::CreateFromStartupInfo()) {
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    // 1. Make a TCP socket.
-    // We need to retry because there's a race condition at boot
-    // between netstack initializing and us calling socket().
-    const auto duration = ftl::TimeDelta::FromMilliseconds(200u);
-
-    for (int i = 0; i < 5 * 10; ++i) {
-      listener_ = socket(addr.sin_family, SOCK_STREAM, IPPROTO_TCP);
-      if (listener_ != -1) {
-        break;
-      }
-      ftl::SleepFor(duration);
-    }
-    FTL_CHECK(listener_ != -1);
-
-    // 2. Bind it to an address.
-    FTL_CHECK(bind(listener_, reinterpret_cast<struct sockaddr*>(&addr),
-                   sizeof(addr)) != -1);
-
-    // 3. Make it a listening socket.
-    FTL_CHECK(listen(listener_, 100) != -1);
-  }
-
-  ~TestRunnerTCPServer() { close(listener_); }
-
-  // Blocks until there is a new connection.
-  TestRunnerConnection* AcceptConnection() {
-    int sockfd = accept(listener_, nullptr, nullptr);
-    if (sockfd == -1) {
-      FTL_LOG(INFO) << "accept() oops";
-    }
-    return new TestRunnerConnection(sockfd, app_context_);
-  }
-
- private:
-  int listener_;
-  std::shared_ptr<app::ApplicationContext> app_context_;
-
-  FTL_DISALLOW_COPY_AND_ASSIGN(TestRunnerTCPServer);
-};
-
-}  // namespace
+}  // namespace testing
 }  // namespace modular
-
-int main() {
-  mtl::MessageLoop loop;
-  modular::TestRunnerTCPServer server(kListenPort);
-  while (1) {
-    // TODO(vardhan): Because our sockets are POSIX fds, they don't work with
-    // our message loop, so we do some synchronous operations and have to do
-    // manipulate the message loop to pass control back and forth. Consider
-    // using separate threads for handle message loop vs. fd polling.
-    auto* runner = server.AcceptConnection();
-    loop.task_runner()->PostTask(
-        std::bind(&modular::TestRunnerConnection::Start, runner));
-    loop.Run();
-  }
-  return 0;
-}
