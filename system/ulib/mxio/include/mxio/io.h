b03dad40b6708658b27e86585f5571015284a298
Brian Swetland <swetland@google.com>
2017-03-02 12:00:16 -0800

    [mxio] fix osx build breakage for host tools
    This file should not be included by host-side code, but it's
    going to take some work to unravel.
    
    Change-Id: Ie353fa1e6518cc94afe66561348071ada983fc6a

--- system/ulib/mxio/include/mxio/io.h
+++ system/ulib/mxio/include/mxio/io.h
@@ -11,8 +11,6 @@
 #include <magenta/types.h>
 #include <magenta/compiler.h>
 
-#include <sys/epoll.h>
-
 #define MAX_MXIO_FD 256
 
 // flag on handle args in processargs
@@ -42,10 +40,19 @@
 #define MXIO_MAX_FILENAME NAME_MAX
 
 // events for mxio_wait_fd()
+#ifndef __Fuchsia__
+// This shouldn't be included from host code
+#define MXIO_EVT_READABLE 1
+#define MXIO_EVT_WRITABLE 4
+#define MXIO_EVT_ERROR 8
+#define MXIO_EVT_ALL (1 | 4 | 8)
+#else
+#include <sys/epoll.h>
 #define MXIO_EVT_READABLE EPOLLIN
 #define MXIO_EVT_WRITABLE EPOLLOUT
 #define MXIO_EVT_ERROR EPOLLERR
 #define MXIO_EVT_ALL (EPOLLIN | EPOLLOUT | EPOLLERR)
+#endif
 
 __BEGIN_CDECLS
 
