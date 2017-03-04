a4e3ed16e6830a7ab45eabbaf5e524c01c238918
George Kulakowski <kulakowski@google.com>
2017-03-04 01:41:17 -0800

    [musl][once] Name constants in pthread_once
    Change-Id: Ide46661fe365b8a2a592d5818512f26c52f54e56

--- third_party/ulib/musl/pthread/pthread_once.c
+++ third_party/ulib/musl/pthread/pthread_once.c
@@ -1,39 +1,44 @@
 #include "futex_impl.h"
 #include "pthread_impl.h"
 
+#include <assert.h>
+
+enum {
+    STATE_INIT = 0, // we're the first or the other cancelled; run init
+    STATE_WAIT = 1, // another thread is running init; wait
+    STATE_DONE = 2, // another thread finished running init; just return
+    STATE_WAKE = 3, // another thread is running init, waiters present; wait
+};
+
+static_assert(STATE_INIT == PTHREAD_ONCE_INIT, "");
+
 static void undo(void* control) {
     atomic_int* futex = control;
 
     /* Wake all waiters, since the waiter status is lost when
      * resetting control to the initial state. */
-    if (atomic_exchange(futex, 0) == 3)
+    if (atomic_exchange(futex, STATE_INIT) == STATE_WAKE)
         __wake(futex, -1);
 }
 
 int __pthread_once_full(pthread_once_t* control, void (*init)(void)) {
-    /* Try to enter initializing state. Four possibilities:
-     *  0 - we're the first or the other cancelled; run init
-     *  1 - another thread is running init; wait
-     *  2 - another thread finished running init; just return
-     *  3 - another thread is running init, waiters present; wait */
-
     for (;;)
-        switch (a_cas_shim(control, 0, 1)) {
-        case 0:
+        switch (a_cas_shim(control, STATE_INIT, STATE_WAIT)) {
+        case STATE_INIT:
             pthread_cleanup_push(undo, control);
             init();
             pthread_cleanup_pop(0);
 
-            if (atomic_exchange(control, 2) == 3)
+            if (atomic_exchange(control, STATE_DONE) == STATE_WAKE)
                 __wake(control, -1);
             return 0;
-        case 1:
+        case STATE_WAIT:
             /* If this fails, so will __wait. */
-            a_cas_shim(control, 1, 3);
-        case 3:
-            __wait(control, 0, 3);
+            a_cas_shim(control, STATE_WAIT, STATE_WAKE);
+        case STATE_WAKE:
+            __wait(control, NULL, STATE_WAKE);
             continue;
-        case 2:
+        case STATE_DONE:
             return 0;
         }
 }
@@ -41,7 +46,7 @@ int __pthread_once_full(pthread_once_t* control, void (*init)(void)) {
 int __pthread_once(pthread_once_t* control, void (*init)(void)) {
     /* Return immediately if init finished before, but ensure that
      * effects of the init routine are visible to the caller. */
-    if (atomic_load(control) == 2) {
+    if (atomic_load(control) == STATE_DONE) {
         atomic_thread_fence(memory_order_seq_cst);
         return 0;
     }
