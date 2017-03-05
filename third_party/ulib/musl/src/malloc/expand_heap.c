9cac4897a618a921075a0446f9a4e1c88c61c36e
George Kulakowski <kulakowski@google.com>
2017-03-04 17:34:57 -0800

    [musl][malloc] Remove 32 bit heap expansion code path
    Change-Id: I00ea3437e7d4231a9ac8dcaab3ae1a5be1f4532c

--- third_party/ulib/musl/src/malloc/expand_heap.c
+++ third_party/ulib/musl/src/malloc/expand_heap.c
@@ -11,15 +11,10 @@
 // Size of heap region to use on 64-bit address spaces
 #define HEAP_REGION_SIZE (1ULL << 40)
 
-/* Expand the heap in-place via vmar operations, using different strategies
- * for different address space sizes.
+/* Expand the heap in-place via vmar operations.
  *
  * On 64-bit address spaces, we create a large VMAR for the heap to live in,
  * and allow it to grow contiguously toward higher addresses.
- *
- * On 32-bit address spaces, we create unrelated mappings with an exponential
- * lower bound on growth to make fragmentation asymptotically irrelevant.  We
- * use guard pages here since the heap may be scattered amongst other mappings.
  *
  * The size argument is both an input and an output, since the caller needs
  * to know the size allocated, which will be larger than requested due to page
@@ -45,7 +40,7 @@ void* __expand_heap(size_t* pn) {
     }
 
     uintptr_t area;
-#if _LP64
+
     static mx_handle_t heap_vmar = MX_HANDLE_INVALID;
     static uintptr_t next_heap_offset = GUARD_SIZE;
     if (heap_vmar == MX_HANDLE_INVALID) {
@@ -67,30 +62,6 @@ void* __expand_heap(size_t* pn) {
         goto nomem;
     }
     next_heap_offset += n;
-
-#else  // _LP64
-    /* Allocate a VMAR with space for a GUARD_SIZE guard page on each side */
-    mx_handle_t vmar;
-    size_t total_alloc = n + 2 * GUARD_SIZE;
-    status = _mx_vmar_allocate(_mx_vmar_root_self(), 0, total_alloc,
-                               MX_VM_FLAG_CAN_MAP_READ | MX_VM_FLAG_CAN_MAP_WRITE |
-                                   MX_VM_FLAG_CAN_MAP_SPECIFIC,
-                               &vmar, &area);
-    if (status != NO_ERROR) {
-        _mx_handle_close(vmo);
-        goto nomem;
-    }
-    status = _mx_vmar_map(vmar, GUARD_SIZE, vmo, 0, n,
-                          MX_VM_FLAG_SPECIFIC | MX_VM_FLAG_PERM_READ | MX_VM_FLAG_PERM_WRITE,
-                          &area);
-    _mx_handle_close(vmo);
-    if (status != NO_ERROR) {
-        _mx_vmar_destroy(vmar);
-        _mx_handle_close(vmar);
-        goto nomem;
-    }
-    _mx_handle_close(vmar);
-#endif // _LP64
 
     *pn = n;
     mmap_step++;
