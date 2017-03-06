5082096ba9b51d7774eef1f5dfa11a0810722b55
Benjamin Lerman <qsr@chromium.org>
2017-03-06 10:17:18 +0100

    Add a simple thread class that allows to set its stack's size.
    Change-Id: Ia6f54c601fdcc6768a52a395557527f2819d4db5

---
+++ public/lib/ftl/threading/thread.cc
@@ -0,0 +1,72 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "lib/ftl/threading/thread.h"
+
+#include <limits.h>
+
+#include <algorithm>
+
+namespace ftl {
+
+Thread::Thread(std::function<void(void)> runnable)
+    : runnable_(std::move(runnable)), running_(false) {}
+
+Thread::~Thread() {
+  Join();
+}
+
+bool Thread::Run(size_t stack_size) {
+  if (running_) {
+    return false;
+  }
+
+  pthread_attr_t attr;
+
+  if (pthread_attr_init(&attr) != 0) {
+    return false;
+  }
+
+  stack_size = std::max<size_t>(PTHREAD_STACK_MIN, stack_size);
+
+  if (pthread_attr_setstacksize(&attr, stack_size) != 0) {
+    pthread_attr_destroy(&attr);
+    return false;
+  }
+
+  auto result = pthread_create(&thread_, &attr, &Thread::Entry, this);
+  if (result == 0) {
+    running_ = true;
+  }
+
+  pthread_attr_destroy(&attr);
+
+  return running_;
+}
+
+bool Thread::IsRunning() const {
+  return running_;
+}
+
+void Thread::Main(void) {
+  runnable_();
+}
+
+void* Thread::Entry(void* context) {
+  ((Thread*)context)->Main();
+  return nullptr;
+}
+
+bool Thread::Join() {
+  if (!running_) {
+    return false;
+  }
+
+  if (pthread_join(thread_, nullptr) == 0) {
+    running_ = false;
+  }
+  return !running_;
+}
+
+}  // namespace ftl
---
+++ public/lib/ftl/threading/thread.h
@@ -0,0 +1,39 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_FTL_THREADING_THREAD_H_
+#define LIB_FTL_THREADING_THREAD_H_
+
+#include <pthread.h>
+
+#include <functional>
+
+#include "lib/ftl/macros.h"
+
+namespace ftl {
+
+class Thread {
+ public:
+  static constexpr size_t default_stack_size = 1 * 1024 * 1024;
+
+  explicit Thread(std::function<void(void)> runnable);
+  ~Thread();
+  bool Run(size_t stack_size = default_stack_size);
+  bool IsRunning() const;
+  bool Join();
+
+ private:
+  static void* Entry(void* context);
+  void Main();
+
+  std::function<void(void)> runnable_;
+  pthread_t thread_;
+  bool running_;
+
+  FTL_DISALLOW_COPY_AND_ASSIGN(Thread);
+};
+
+}  // namespace ftl
+
+#endif  // LIB_FTL_THREADING_THREAD_H_
