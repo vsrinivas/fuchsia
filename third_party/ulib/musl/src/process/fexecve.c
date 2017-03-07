2f3a632fd9537b9541ba888f730875dd8fe2c746
George Kulakowski <kulakowski@google.com>
2017-03-07 13:23:23 -0800

    [musl] Remove procfd filename computation from fexecve stub
    Change-Id: Icfeb58b3b8efd13a988ada12b55451fcd864202c

--- third_party/ulib/musl/src/process/fexecve.c
+++ third_party/ulib/musl/src/process/fexecve.c
@@ -1,13 +1,7 @@
 #include <errno.h>
 #include <unistd.h>
 
-void __procfdname(char*, unsigned);
-
 int fexecve(int fd, char* const argv[], char* const envp[]) {
-    char buf[15 + 3 * sizeof(int)];
-    __procfdname(buf, fd);
-    execve(buf, argv, envp);
-    if (errno == ENOENT)
-        errno = EBADF;
+    errno = ENOSYS;
     return -1;
 }
