52335aec6da2f39e5e80931380877430bae1c524
Ross Wang <rosswang@google.com>
2017-03-10 17:21:42 -0800

    Adding retry to net app loader
    On the acer (and potentially other environments), the network service is
    available before the network connection is established. Address this by
    putting in a retry loop. The retry behavior is not yet configurable by the
    client.
    
    Also improving error reporting accordingly, though this is also not yet
    surfaced to the client.
    
    Change-Id: Id26fdeb15fc6137de9f8ff226b09d04c642dce2b

--- bin/network/application_loader/main.cc
+++ bin/network/application_loader/main.cc
@@ -2,14 +2,116 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <unordered_map>
+
 #include "application/lib/app/application_context.h"
 #include "application/services/application_loader.fidl.h"
 #include "apps/network/services/network_service.fidl.h"
 #include "lib/fidl/cpp/bindings/binding_set.h"
 #include "lib/ftl/functional/make_copyable.h"
+#include "lib/ftl/memory/weak_ptr.h"
 #include "lib/mtl/tasks/message_loop.h"
 
 namespace {
+
+class RetryingLoader {
+ public:
+  RetryingLoader(
+      network::URLLoaderPtr url_loader,
+      const std::string& url,
+      const app::ApplicationLoader::LoadApplicationCallback& callback)
+      : url_loader_(std::move(url_loader)),
+        url_(url),
+        callback_(callback),
+        // TODO(rosswang): deadline support
+        quiet_tries_(5),
+        // TODO(rosswang): randomness
+        retry_delay_(ftl::TimeDelta::FromSeconds(1)),
+        weak_ptr_factory_(this) {}
+
+  void Attempt() {
+    url_loader_->Start(
+        NewRequest(), [weak_this = weak_ptr_factory_.GetWeakPtr()](
+                          const network::URLResponsePtr& response) {
+          if (weak_this) {
+            weak_this->ProcessResponse(response);
+          }
+        });
+  }
+
+  void SetDeleter(const ftl::Closure& fn) { deleter_ = fn; }
+
+ private:
+  // Need to create a new request each time because a URLRequest's body can
+  // potentially contain a VMO handle and so can't be cloned.
+  network::URLRequestPtr NewRequest() const {
+    auto request = network::URLRequest::New();
+    request->method = "GET";
+    request->url = url_;
+    request->auto_follow_redirects = true;
+    request->response_body_mode = network::URLRequest::ResponseBodyMode::BUFFER;
+    return request;
+  }
+
+  void ProcessResponse(const network::URLResponsePtr& response) {
+    if (response->status_code == 200) {
+      auto package = app::ApplicationPackage::New();
+      package->data = std::move(response->body->get_buffer());
+      SendResponse(std::move(package));
+    } else if (response->error) {
+      Retry(response);
+    } else {
+      FTL_LOG(WARNING) << "Failed to load application from " << url_ << ": "
+                       << response->status_line << " (" << response->status_code
+                       << ")";
+      SendResponse(nullptr);
+    }
+  }
+
+  void Retry(const network::URLResponsePtr& response) {
+    mtl::MessageLoop::GetCurrent()->task_runner()->PostDelayedTask(
+        [weak_this = weak_ptr_factory_.GetWeakPtr()] {
+          if (weak_this) {
+            weak_this->Attempt();
+          }
+        },
+        retry_delay_);
+
+    if (quiet_tries_ > 0) {
+      FTL_VLOG(2) << "Retrying load of " << url_ << " due to "
+                  << response->error->description << " ("
+                  << response->error->code << ")";
+
+      quiet_tries_--;
+      // TODO(rosswang): Randomness, and factor out the delay fn.
+      retry_delay_ =
+          ftl::TimeDelta::FromSecondsF(retry_delay_.ToSecondsF() * 1.5f);
+    } else if (quiet_tries_ == 0) {
+      FTL_LOG(WARNING) << "Error while attempting to load application from "
+                       << url_ << ": " << response->error->description << " ("
+                       << response->error->code
+                       << "); continuing to retry every "
+                       << retry_delay_.ToSeconds() << " s.";
+      quiet_tries_ = -1;
+    }
+  }
+
+  void SendResponse(app::ApplicationPackagePtr package) {
+    callback_(std::move(package));
+    deleter_();
+  }
+
+  const network::URLLoaderPtr url_loader_;
+  const std::string url_;
+  const app::ApplicationLoader::LoadApplicationCallback callback_;
+  ftl::Closure deleter_;
+  // Tries before an error is printed. No errors will be printed afterwards
+  // either.
+  int quiet_tries_;
+  ftl::TimeDelta retry_delay_;
+
+  ftl::WeakPtrFactory<RetryingLoader> weak_ptr_factory_;
+};
 
 class NetworkApplicationLoader : public app::ApplicationLoader {
  public:
@@ -29,23 +131,12 @@ class NetworkApplicationLoader : public app::ApplicationLoader {
     network::URLLoaderPtr loader;
     net_->CreateURLLoader(loader.NewRequest());
 
-    auto request = network::URLRequest::New();
-    request->method = "GET";
-    request->url = url;
-    request->auto_follow_redirects = true;
-    request->response_body_mode = network::URLRequest::ResponseBodyMode::BUFFER;
-
-    loader->Start(std::move(request),
-                  ftl::MakeCopyable([ loader = std::move(loader), callback ](
-                      network::URLResponsePtr response) {
-                    if (response->status_code == 200) {
-                      auto package = app::ApplicationPackage::New();
-                      package->data = std::move(response->body->get_buffer());
-                      callback(std::move(package));
-                    } else {
-                      callback(nullptr);
-                    }
-                  }));
+    auto retrying_loader =
+        std::make_unique<RetryingLoader>(std::move(loader), url, callback);
+    RetryingLoader* ref = retrying_loader.get();
+    loaders_.emplace(ref, std::move(retrying_loader));
+    ref->SetDeleter([this, ref] { loaders_.erase(ref); });
+    ref->Attempt();
   }
 
  private:
@@ -53,6 +144,7 @@ class NetworkApplicationLoader : public app::ApplicationLoader {
   fidl::BindingSet<app::ApplicationLoader> bindings_;
 
   network::NetworkServicePtr net_;
+  std::unordered_map<RetryingLoader*, std::unique_ptr<RetryingLoader>> loaders_;
 };
 
 }  // namespace
