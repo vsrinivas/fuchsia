0fe034e2ab01942a0fd6edfb6d598235cff013e9
John Grossman <johngro@google.com>
2017-03-13 18:42:29 -0700

    [mx] Flag the mx::channel::call method as const.
    Like channel::read and channel::write, channel::call does not modify
    the state of an mx::channel handle.  Flag this method as const to be
    consistent, and so that methods which received "const mx::channel&"s
    can use call(...).
    
    Change-Id: Ibc9606b3a0d6e816d402ce1577db9800920c0824

--- system/ulib/mx/include/mx/channel.h
+++ system/ulib/mx/include/mx/channel.h
@@ -45,7 +45,7 @@ public:
     mx_status_t call(uint32_t flags, mx_time_t timeout,
                      const mx_channel_call_args_t* args,
                      uint32_t* actual_bytes, uint32_t* actual_handles,
-                     mx_status_t* read_status) {
+                     mx_status_t* read_status) const {
         return mx_channel_call(get(), flags, timeout, args, actual_bytes,
                                actual_handles, read_status);
     }
