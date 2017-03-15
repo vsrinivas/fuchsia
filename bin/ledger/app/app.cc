fb2bf99f049b36e813e4b9a1206488ff3d9fb05d
Benjamin Lerman <qsr@chromium.org>
2017-03-15 09:41:16 +0100

    When starting the Ledger poll /data until it is persistent.
    Due to MG-615, the Ledger might be started before /data is mounted. This
    CL makes the Ledger poll the state of /data until is is mounted.
    If /data is not mounted after 10 seconds, the Ledger will continue, but
    will issue a warning.
    
    Change-Id: I108de68038cc0a495dc9e7edee7ea8d21d14fc42

--- bin/ledger/app/app.cc
+++ bin/ledger/app/app.cc
@@ -2,14 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fcntl.h>
+#include <magenta/device/devmgr.h>
+#include <unistd.h>
+
 #include <memory>
 #include <utility>
-
-#include <errno.h>
 
 #include "application/lib/app/application_context.h"
 #include "apps/ledger/services/internal/internal.fidl.h"
 #include "apps/ledger/src/app/ledger_repository_factory_impl.h"
+#include "apps/ledger/src/backoff/exponential_backoff.h"
 #include "apps/ledger/src/configuration/configuration.h"
 #include "apps/ledger/src/configuration/load_configuration.h"
 #include "apps/ledger/src/environment/environment.h"
@@ -17,11 +20,18 @@
 #include "apps/network/services/network_service.fidl.h"
 #include "apps/tracing/lib/trace/provider.h"
 #include "lib/fidl/cpp/bindings/binding_set.h"
+#include "lib/ftl/files/unique_fd.h"
 #include "lib/ftl/logging.h"
 #include "lib/ftl/macros.h"
+#include "lib/ftl/time/time_delta.h"
+#include "lib/ftl/time/time_point.h"
 #include "lib/mtl/tasks/message_loop.h"
 
 namespace ledger {
+
+constexpr ftl::StringView kPersistentFileSystem = "/data";
+constexpr ftl::StringView kMinFsName = "minfs";
+constexpr ftl::TimeDelta kMaxPollingDelay = ftl::TimeDelta::FromSeconds(10);
 
 // App is the main entry point of the Ledger application.
 //
@@ -83,9 +93,36 @@ class App {
   FTL_DISALLOW_COPY_AND_ASSIGN(App);
 };
 
+void WaitForData() {
+  backoff::ExponentialBackoff backoff(ftl::TimeDelta::FromMilliseconds(10), 2,
+                                      ftl::TimeDelta::FromSeconds(1));
+  ftl::TimePoint now = ftl::TimePoint::Now();
+  while (ftl::TimePoint::Now() - now < kMaxPollingDelay) {
+    ftl::UniqueFD fd(open(kPersistentFileSystem.data(), O_RDWR));
+    FTL_DCHECK(fd.is_valid());
+    char out[128];
+    ssize_t len = ioctl_devmgr_query_fs(fd.get(), out, sizeof(out));
+    FTL_DCHECK(len >= 0);
+
+    ftl::StringView fs_name(out, len);
+    if (fs_name == kMinFsName) {
+      return;
+    }
+
+    usleep(backoff.GetNext().ToMicroseconds());
+  }
+
+  FTL_LOG(WARNING) << kPersistentFileSystem
+                   << " is not persistent. Did you forget to configure it?";
+}
+
 }  // namespace ledger
 
 int main(int argc, const char** argv) {
+  // Poll until /data is persistent. This is need to retrieve the Ledger
+  // configuration.
+  ledger::WaitForData();
+
   mtl::MessageLoop loop;
 
   ledger::App app;
