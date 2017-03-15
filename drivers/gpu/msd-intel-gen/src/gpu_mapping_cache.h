53ebcc6619ab7a2f6ec1f76ef6cc2784ff630926
Forrest Reiling <freiling@google.com>
2017-03-14 17:25:35 -0700

    disable mapping cache by default in debug mode
    Change-Id: I74e0c1380ab96d227052f81bf2ed70c1d4144e58

--- drivers/gpu/msd-intel-gen/src/gpu_mapping_cache.h
+++ drivers/gpu/msd-intel-gen/src/gpu_mapping_cache.h
@@ -14,6 +14,10 @@ class GpuMappingCache {
 public:
     void CacheMapping(std::shared_ptr<GpuMapping> mapping);
 
+    uint64_t memory_footprint() { return memory_footprint_; }
+
+    uint64_t memory_cap() { return memory_cap_; }
+
     static std::unique_ptr<GpuMappingCache> Create(uint64_t memory_cap = kDefaultMemoryCap);
 
 private:
@@ -24,7 +28,11 @@ private:
     std::unordered_map<GpuMapping*, cache_list_t::iterator> cache_iterator_map_;
 
     // Memory footprint management
+#if defined(MSD_INTEL_ENABLE_MAPPING_CACHE)
     static constexpr uint64_t kDefaultMemoryCap = 512 * 1024 * 1024;
+#else
+    static constexpr uint64_t kDefaultMemoryCap = 0;
+#endif
     uint64_t memory_cap_;
     // Right now this tracks total gpu address space held across all address
     // spaces. TODO(MA-153) make this track total pinned pages held by cache
