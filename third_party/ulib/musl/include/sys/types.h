25543c40fa2977eb3669ab1cb191eda0ddf77731
George Kulakowski <kulakowski@google.com>
2017-03-17 15:05:56 -0700

    [musl] Re-introduce major, minor, and makedev macros
    These were removed in I9ed6f259f32c6729ee3c05021d8b375e73f82e9c for
    reasons that are no longer relevant.
    
    These are harmless to have around under the _BSD_SOURCE guard, and
    ease porting of POSIX-y software. For instance, sbase utilities have
    these macros in some places into tar structs.
    
    Change-Id: Id7ee85222f85cce2f89f96c8a2b56c4c610ed049

--- third_party/ulib/musl/include/sys/types.h
+++ third_party/ulib/musl/include/sys/types.h
@@ -69,6 +69,14 @@ typedef long long quad_t;
 typedef unsigned long long u_quad_t;
 #include <endian.h>
 #include <sys/select.h>
+
+#define major(x) ((unsigned)((((x) >> 31 >> 1) & 0xfffff000) | (((x) >> 8) & 0x00000fff)))
+#define minor(x) ((unsigned)((((x) >> 12) & 0xffffff00) | ((x)&0x000000ff)))
+
+#define makedev(x, y)                                                                         \
+    ((((x)&0xfffff000ULL) << 32) | (((x)&0x00000fffULL) << 8) | (((y)&0xffffff00ULL) << 12) | \
+     (((y)&0x000000ffULL)))
+
 #endif
 
 #if defined(_LARGEFILE64_SOURCE) || defined(_GNU_SOURCE)
