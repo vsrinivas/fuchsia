vsrinivas/fuchsia

commit 27162e8b0481b692242224aac3b81570709fa36e

[futex] Reduce internal lock contention between FutexWait()/FutexWake()
FutexWake() needs to be holding FutexContext's internal lock when it
calls cond_signal() to wake the target thread.  Unfortunately, when
the target thread wakes up, cond_wait_timeout() tries to claim that
lock, causing contention -- the target thread will often block again.

There is a simple way to fix this.  Although a condvar wait operation
must be called with the condvar's mutex held (so that the release+wait
is atomic), it is not essential for the condvar wait operation to
reclaim the mutex before returning.  Reclaiming the mutex (if it's
done at all) does not need to be combined atomically with waiting.

So, we can add a non-mutex-reclaiming variant of cond_wait_timeout()
and use that in this case.

This is a minimally invasive way to reduce contention, which preserves
the existing layering in which wait_queue is only used inside
kernel/kernel/.  We could improve on this by using wait_queue
directly, or by reducing the overhead of mutex_t, using
per-hash-bucket locks, etc.

MG-354

Change-Id: I67dfd46a89fc681f2b8df24169e771fd4bb456b4

kernel/lib/magenta/futex_context.cpp
@@ -35,16 +35,24 @@ status_t FutexContext::FutexWait(user_ptr<int> value_ptr, int current_value, mx_
     // If a FutexWake() operation could occur between them, a userland mutex
     // operation built on top of futexes would have a race condition that
     // could miss wakeups.
-    AutoLock lock(lock_);
+    lock_.Acquire();
 
     UserThread* t = UserThread::GetCurrent();
-    if (t->state() == UserThread::State::DYING || t->state() == UserThread::State::DEAD)
+    if (t->state() == UserThread::State::DYING || t->state() == UserThread::State::DEAD) {
+        lock_.Release();
         return ERR_BAD_STATE;
+    }
 
     int value;
     status_t result = value_ptr.copy_from_user(&value);
-    if (result != NO_ERROR) return result;
-    if (value != current_value) return ERR_BAD_STATE;
+    if (result != NO_ERROR) {
+        lock_.Release();
+        return result;
+    }
+    if (value != current_value) {
+        lock_.Release();
+        return ERR_BAD_STATE;
+    }
 
     node = t->futex_node();
     node->set_hash_key(futex_key);
@@ -52,12 +60,14 @@ status_t FutexContext::FutexWait(user_ptr<int> value_ptr, int current_value, mx_
 
     QueueNodesLocked(node);
 
-    // Block current thread
+    // Block current thread.  This releases lock_ and does not reacquire it.
     result = node->BlockThread(&lock_, timeout);
     if (result == NO_ERROR) {
         // All the work necessary for removing us from the hash table was done by FutexWake()
         return NO_ERROR;
     }
+
+    AutoLock lock(lock_);
     // If we got a timeout, we need to remove the thread's node from the
     // wait queue, since FutexWake() didn't do that.
     if (UnqueueNodeLocked(node)) {

kernel/lib/magenta/futex_node.cpp
@@ -97,10 +97,14 @@ FutexNode* FutexNode::RemoveFromHead(FutexNode* list_head, uint32_t count,
     return node;
 }
 
+// This blocks the current thread.  This releases the given mutex (which
+// must be held when BlockThread() is called).  To reduce contention, it
+// does not reclaim the mutex on return.
 status_t FutexNode::BlockThread(Mutex* mutex, mx_time_t timeout) {
     lk_time_t t = mx_time_to_lk(timeout);
 
-    return cond_wait_timeout(&condvar_, mutex->GetInternal(), t);
+    return cond_wait_timeout_without_reclaim(
+        &condvar_, mutex->GetInternal(), t);
 }
 
 void FutexNode::WakeKilledThread() {

kernel/include/kernel/cond.h
@@ -30,6 +30,8 @@ typedef struct cond {
 void cond_init(cond_t *cond);
 void cond_destroy(cond_t *cond);
 status_t cond_wait_timeout(cond_t *cond, mutex_t *mutex, lk_time_t timeout);
+status_t cond_wait_timeout_without_reclaim(cond_t *cond, mutex_t *mutex,
+                                           lk_time_t timeout);
 void cond_signal(cond_t *cond);
 void cond_broadcast(cond_t *cond);
 

kernel/kernel/cond.c
@@ -29,7 +29,9 @@ void cond_destroy(cond_t *cond)
     THREAD_UNLOCK(state);
 }
 
-status_t cond_wait_timeout(cond_t *cond, mutex_t *mutex, lk_time_t timeout)
+static status_t cond_wait_timeout_internal(cond_t *cond, mutex_t *mutex,
+                                           lk_time_t timeout,
+                                           bool reclaim_mutex)
 {
     DEBUG_ASSERT(cond->magic == COND_MAGIC);
     DEBUG_ASSERT(mutex->magic == MUTEX_MAGIC);
@@ -44,11 +46,23 @@ status_t cond_wait_timeout(cond_t *cond, mutex_t *mutex, lk_time_t timeout)
 
     status_t result = wait_queue_block(&cond->wait, timeout);
 
-    mutex_acquire_timeout_internal(mutex, INFINITE_TIME);
+    if (reclaim_mutex)
+        mutex_acquire_timeout_internal(mutex, INFINITE_TIME);
 
     THREAD_UNLOCK(state);
 
     return result;
+}
+
+status_t cond_wait_timeout(cond_t *cond, mutex_t *mutex, lk_time_t timeout)
+{
+    return cond_wait_timeout_internal(cond, mutex, timeout, true);
+}
+
+status_t cond_wait_timeout_without_reclaim(cond_t *cond, mutex_t *mutex,
+                                           lk_time_t timeout)
+{
+    return cond_wait_timeout_internal(cond, mutex, timeout, false);
 }
 
 void cond_signal(cond_t *cond)

kernel/lib/magenta/include/magenta/futex_node.h
@@ -38,8 +38,6 @@ public:
                                      uintptr_t old_hash_key,
                                      uintptr_t new_hash_key);
 
-    // block the current thread, releasing the given mutex while the thread
-    // is blocked
     status_t BlockThread(Mutex* mutex, mx_time_t timeout);
 
     void WakeKilledThread();

commit c91ff13b806bccac42af90e3e44b8b9611e0e9c0

[futex] Fix race condition in futexes
This removes the following two-thread race.

1. Thread A waits on a futex (releases the FutexContext lock)
2. Thread B wakes Thread A (while holding the FutexContext lock)
3. Thread A wakes up, sees it was signaled successfully.
4. Thread A returns to usermode and invokes thread_exit().
5. Thread A exits and has its structures freed.
6. Thread B (while still holding the FutexContext lock) marks Thread A
   as no longer in the futex queues (use-after-free).

The fix is for Thread A to grab the FutexContext lock after waking up,
which forces it to wait for Thread B to mark it as no longer in the
queue.

The bug was introduced in 27162e8b0481b692242224aac3b81570709fa36e -
[futex] Reduce internal lock contention between FutexWait()/FutexWake()

MG-624 #done

Change-Id: Idf1faa5daae4ffd3f236954aef5915e9c856fa08

kernel/lib/magenta/futex_context.cpp
@@ -65,6 +65,13 @@ status_t FutexContext::FutexWait(user_ptr<int> value_ptr, int current_value, mx_
     // Block current thread.  This releases lock_ and does not reacquire it.
     result = node->BlockThread(&lock_, timeout);
     if (result == NO_ERROR) {
+        // Fix/workaround for MG-624:
+        // We must re-acquire the lock here to force this thread to wait until
+        // the WakeThreads() marks this thread as not in the queue anymore.
+        // Otherwise, this thread can exit before it does that, causing
+        // WakeThreads() to scribble on memory.
+        AutoLock lock(&lock_);
+        DEBUG_ASSERT(!node->IsInQueue());
         // All the work necessary for removing us from the hash table was done by FutexWake()
         return NO_ERROR;
     }

kernel/lib/magenta/futex_node.cpp
@@ -21,6 +21,8 @@ FutexNode::FutexNode() {
 FutexNode::~FutexNode() {
     LTRACE_ENTRY;
 
+    DEBUG_ASSERT(!IsInQueue());
+
     THREAD_LOCK(state);
     wait_queue_destroy(&wait_queue_);
     THREAD_UNLOCK(state);