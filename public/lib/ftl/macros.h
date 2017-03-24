816d9f93524f6f7a47ca9c58d0675a5ea02db5fd
Aaron Green <aarongreen@google.com>
2017-03-24 10:53:27 -0700

    [lib/ftl] Add FTL_DISALLOW_MOVE et al. to macros.h
    This CL adds macros to delete the default r-value copy constructor
    and assignment operator. It also fixes the return type of the
    existing macros to match the default signatures.
    
    Cf. //magenta/system/ulib/mxtl/include/mxtl/macros.h
    
    Change-Id: I68c270364fb0e2b1436f2ac7c07338dc4d7d7b17

--- public/lib/ftl/macros.h
+++ public/lib/ftl/macros.h
@@ -7,14 +7,25 @@
 
 #define FTL_DISALLOW_COPY(TypeName) TypeName(const TypeName&) = delete
 
-#define FTL_DISALLOW_ASSIGN(TypeName) void operator=(const TypeName&) = delete
+#define FTL_DISALLOW_ASSIGN(TypeName) \
+  TypeName& operator=(const TypeName&) = delete
+
+#define FTL_DISALLOW_MOVE(TypeName) \
+  TypeName(TypeName&&) = delete;    \
+  TypeName& operator=(TypeName&&) = delete
 
 #define FTL_DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&) = delete;          \
-  void operator=(const TypeName&) = delete
+  TypeName& operator=(const TypeName&) = delete
+
+#define FTL_DISALLOW_COPY_ASSIGN_AND_MOVE(TypeName) \
+  TypeName(const TypeName&) = delete;               \
+  TypeName(TypeName&&) = delete;                    \
+  TypeName& operator=(const TypeName&) = delete;    \
+  TypeName& operator=(TypeName&&) = delete
 
 #define FTL_DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
   TypeName() = delete;                               \
-  FTL_DISALLOW_COPY_AND_ASSIGN(TypeName)
+  FTL_DISALLOW_COPY_ASSIGN_AND_MOVE(TypeName)
 
 #endif  // LIB_FTL_MACROS_H_
