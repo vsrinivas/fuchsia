115b6c9fab175e25a67650335f383a5fb06d8e70
Craig Stout <cstout@google.com>
2017-03-24 13:44:25 -0700

    Easily switch from debug to release
    Change-Id: Iac75c2e1d1d53270b4ba3b47f86d6994772ad768

--- lib/magma/scripts/build.sh
+++ lib/magma/scripts/build.sh
@@ -8,15 +8,18 @@ set -e
 fuchsia_root=`pwd`
 tools_path=$fuchsia_root/buildtools
 magenta_build_dir=$fuchsia_root/out/build-magenta/build-magenta-pc-x86-64
-build_dir=$fuchsia_root/out/debug-x86-64
+build=release
+bootfs=$fuchsia_root/out/user.bootfs
+build_dir=$fuchsia_root/out/$build-x86-64
 
+rm -f $bootfs
 cd $fuchsia_root
-$fuchsia_root/packages/gn/gen.py --modules magma-dev
-$fuchsia_root/buildtools/ninja -C $build_dir
+$fuchsia_root/packages/gn/gen.py --modules magma-dev --$build
+$fuchsia_root/buildtools/ninja -C $build_dir && cp $build_dir/user.bootfs $bootfs
 
 echo "Recommended bootserver command:"
 echo ""
-echo "$magenta_build_dir/tools/bootserver $magenta_build_dir/magenta.bin $build_dir/user.bootfs"
+echo "$magenta_build_dir/tools/bootserver $magenta_build_dir/magenta.bin $fuchsia_root/out/user.bootfs"
 echo ""
 echo "Recommended loglistener command:"
 echo ""
