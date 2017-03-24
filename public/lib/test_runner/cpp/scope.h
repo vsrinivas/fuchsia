ae2f7924f17af21cfd7f9e12d5cd7c07ea6e355a
Brian Goldman <bgoldman@google.com>
2017-03-23 19:18:26 -0700

    Move test runner into a new repo
    Tested by running the Modular tests in conjunction with:
    https://fuchsia-review.googlesource.com/c/23843/
    
    Change-Id: I450fc2db23b45ae7e9815e65389724a17b50793d

---
+++ public/lib/test_runner/cpp/scope.h
@@ -0,0 +1,70 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef APPS_TEST_RUNNER_LIB_SCOPE_H_
+#define APPS_TEST_RUNNER_LIB_SCOPE_H_
+
+#include <memory>
+#include <string>
+
+#include "application/lib/app/application_context.h"
+#include "application/lib/app/service_provider_impl.h"
+#include "application/services/application_environment.fidl.h"
+#include "application/services/application_environment_host.fidl.h"
+#include "lib/fidl/cpp/bindings/binding.h"
+#include "lib/fidl/cpp/bindings/interface_request.h"
+
+namespace test_runner {
+
+// A simple implementation of the ApplicationEnvironmentHost that provides fate
+// separation of sets of applications run by one application. The environment
+// services are delegated to the parent environment.
+class Scope : public app::ApplicationEnvironmentHost {
+ public:
+  Scope(app::ApplicationEnvironmentPtr parent_env, const std::string& label)
+      : binding_(this), parent_env_(std::move(parent_env)) {
+    app::ServiceProviderPtr parent_env_service_provider;
+    parent_env_->GetServices(parent_env_service_provider.NewRequest());
+    service_provider_impl_.SetDefaultServiceProvider(
+        std::move(parent_env_service_provider));
+    parent_env_->CreateNestedEnvironment(binding_.NewBinding(),
+                                         env_.NewRequest(),
+                                         env_controller_.NewRequest(), label);
+  }
+
+  template <typename Interface>
+  void AddService(
+      app::ServiceProviderImpl::InterfaceRequestHandler<Interface> handler,
+      const std::string& service_name = Interface::Name_) {
+    service_provider_impl_.AddService(handler, service_name);
+  }
+
+  app::ApplicationLauncher* GetLauncher() {
+    if (!env_launcher_) {
+      env_->GetApplicationLauncher(env_launcher_.NewRequest());
+    }
+    return env_launcher_.get();
+  }
+
+  app::ApplicationEnvironmentPtr& environment() { return env_; }
+
+ private:
+  // |ApplicationEnvironmentHost|:
+  void GetApplicationEnvironmentServices(
+      fidl::InterfaceRequest<app::ServiceProvider> environment_services)
+      override {
+    service_provider_impl_.AddBinding(std::move(environment_services));
+  }
+
+  fidl::Binding<app::ApplicationEnvironmentHost> binding_;
+  app::ApplicationEnvironmentPtr parent_env_;
+  app::ApplicationEnvironmentPtr env_;
+  app::ApplicationLauncherPtr env_launcher_;
+  app::ApplicationEnvironmentControllerPtr env_controller_;
+  app::ServiceProviderImpl service_provider_impl_;
+};
+
+}  // namespace test_runner
+
+#endif  // APPS_TEST_RUNNER_LIB_SCOPE_H_
---
+++ public/lib/test_runner/cpp/test_runner.h
@@ -0,0 +1,111 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef APPS_TEST_RUNNER_TEST_RUNNER_H_
+#define APPS_TEST_RUNNER_TEST_RUNNER_H_
+
+#include <memory>
+#include "application/lib/app/application_context.h"
+#include "apps/test_runner/services/test_runner.fidl.h"
+#include "apps/test_runner/lib/scope.h"
+#include "apps/test_runner/lib/test_runner_store_impl.h"
+#include "lib/ftl/tasks/one_shot_timer.h"
+
+namespace test_runner {
+
+class TestRunObserver {
+ public:
+  virtual void SendMessage(const std::string& test_id,
+                           const std::string& operation,
+                           const std::string& msg) = 0;
+  virtual void Teardown(const std::string& test_id, bool success) = 0;
+};
+
+class TestRunContext;
+
+// Implements the TestRunner service which is available in the
+// ApplicationEnvironment of the test processes. Calls made to this service are
+// forwarded to and handled by TestRunContext.
+class TestRunnerImpl : public TestRunner {
+ public:
+  TestRunnerImpl(fidl::InterfaceRequest<TestRunner> request,
+                 TestRunContext* test_run_context);
+
+  const std::string& test_name() const;
+
+  bool waiting_for_termination() const;
+
+  // Will be called if this |TestRunner| is waiting for termination while
+  // another one calls |Teardown|. If this is called then when this does
+  // terminate it will call |Teardown|.
+  void TeardownAfterTermination();
+
+  // Indicates if there are test points that haven't been passed yet.
+  bool TestPointsRemaining() { return remaining_test_points_ > 0; }
+
+ private:
+  // |TestRunner|
+  void Identify(const fidl::String& test_name) override;
+  // |TestRunner|
+  void Fail(const fidl::String& log_message) override;
+  // |TestRunner|
+  void Done() override;
+  // |TestRunner|
+  void Teardown() override;
+  // |TestRunner|
+  void WillTerminate(double withinSeconds) override;
+  // |TestRunner|
+  void SetTestPointCount(int64_t count) override;
+  // |TestRunner|
+  void PassTestPoint() override;
+
+  fidl::Binding<TestRunner> binding_;
+  TestRunContext* const test_run_context_;
+  std::string test_name_ = "UNKNOWN";
+  bool waiting_for_termination_ = false;
+  ftl::OneShotTimer termination_timer_;
+  bool teardown_after_termination_ = false;
+  int64_t remaining_test_points_ = -1;
+
+  FTL_DISALLOW_COPY_AND_ASSIGN(TestRunnerImpl);
+};
+
+// TestRunContext represents a single run of a test. Given a test to run, it
+// runs it in a new ApplicationEnvironment and provides the environment a
+// TestRunner service to report completion. When tests are done, their
+// completion is reported back to TestRunObserver (which is responsible for
+// deleting TestRunContext). If the child application stops without reporting
+// anything, we declare the test a failure.
+class TestRunContext {
+ public:
+  TestRunContext(std::shared_ptr<app::ApplicationContext> app_context,
+                 TestRunObserver* connection,
+                 const std::string& test_id,
+                 const std::string& url,
+                 const std::vector<std::string>& args);
+
+  // Called from TestRunnerImpl, the actual implemention of |TestRunner|.
+  void StopTrackingClient(TestRunnerImpl* client, bool crashed);
+  void Fail(const fidl::String& log_message);
+  void Teardown(TestRunnerImpl* teardown_client);
+
+ private:
+  app::ApplicationControllerPtr child_app_controller_;
+  std::unique_ptr<Scope> child_env_scope_;
+
+  TestRunObserver* const test_runner_connection_;
+  std::vector<std::unique_ptr<TestRunnerImpl>> test_runner_clients_;
+  TestRunnerStoreImpl test_runner_store_;
+
+  // This is a tag that we use to identify the test that was run. For now, it
+  // helps distinguish between multiple test outputs to the device log.
+  const std::string test_id_;
+  bool success_;
+
+  FTL_DISALLOW_COPY_AND_ASSIGN(TestRunContext);
+};
+
+}  // namespace test_runner
+
+#endif  // APPS_TEST_RUNNER_TEST_RUNNER_H_
