b8ee77c3a50506a0aa6d8a5bc13ad0a4f5411396
Jim Beveridge <jimbe@chromium.org>
2017-03-27 14:45:28 -0700

    Removes netcp commands from test.sh, adds files to json
    Also adds "$@" to test.sh so that the --server parameter can be
    specified on the command line.
    
    FW-128 #done
    
    Change-Id: Ia254a2209f813c1f3995b63a89610b1b86a408ba

--- tests/test.sh
+++ tests/test.sh
@@ -9,16 +9,4 @@ if [ "${FUCHSIA_BUILD_DIR}" = "" ]; then
   exit -1;
 fi
 
-# TODO(mesch): This is redundant with modular_tests.json, we should read this
-# from there instead.
-netcp ${FUCHSIA_BUILD_DIR}/parent_module :/system/apps/modular_tests/parent_module
-netcp ${FUCHSIA_BUILD_DIR}/child_module :/system/apps/modular_tests/child_module
-netcp ${FUCHSIA_BUILD_DIR}/component_context_test :/system/apps/modular_tests/component_context_test
-netcp ${FUCHSIA_BUILD_DIR}/component_context_test_agent1 :/system/apps/modular_tests/component_context_test_agent1
-netcp ${FUCHSIA_BUILD_DIR}/component_context_test_agent2 :/system/apps/modular_tests/component_context_test_agent2
-netcp ${FUCHSIA_BUILD_DIR}/agent_trigger_test :/system/apps/modular_tests/agent_trigger_test
-netcp ${FUCHSIA_BUILD_DIR}/trigger_test_agent :/system/apps/modular_tests/trigger_test_agent
-netcp ${FUCHSIA_BUILD_DIR}/suggestion_proposal_test_module :/system/apps/modular_tests/suggestion_proposal_test_module
-netcp ${FUCHSIA_BUILD_DIR}/suggestion_test_user_shell :/system/apps/suggestion_test_user_shell
-
-${FUCHSIA_DIR}/apps/test_runner/src/run_test --test_file=$FUCHSIA_DIR/apps/modular/tests/modular_tests.json
+${FUCHSIA_DIR}/apps/test_runner/src/run_test --test_file=$FUCHSIA_DIR/apps/modular/tests/modular_tests.json "$@"
