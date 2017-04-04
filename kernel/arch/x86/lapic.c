72d086918dcd2e28b57e09f2e8b2cfef75b8f971
Todd Eisenberger <teisenbe@google.com>
2017-04-04 16:29:02 -0700

    [x86][timers] Fix race in TSC-deadline mode
    1. Apparently we were writing things in the wrong order, but it was
    working out fine?
    2. We never ran into this due to the time unit being milliseconds, but
    if we were to set a deadline for something like 1ns from now, odds are
    by the time we set it it has passed.  In that case, the timer would
    never fire.
    
    Change-Id: Ib01a4a8603eec78d0fd09b0dface1700bc72f52c

--- kernel/arch/x86/lapic.c
+++ kernel/arch/x86/lapic.c
@@ -305,8 +305,31 @@ void apic_timer_set_tsc_deadline(uint64_t deadline, bool masked) {
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
 
-    write_msr(IA32_TSC_DEADLINE_MSR, deadline);
     *LVT_TIMER_ADDR = timer_config;
+    // Intel recommends using an MFENCE to ensure the LVT_TIMER_ADDR write
+    // takes before the write_msr(), since writes to this MSR are ignored if the
+    // time mode is not DEADLINE.
+    mb();
+    write_msr(IA32_TSC_DEADLINE_MSR, deadline);
+
+    // Serialize the instruction stream to ensure the rdtsc happens after the
+    // write_msr
+    uint32_t ignored;
+    cpuid(0, &ignored, &ignored, &ignored, &ignored);
+
+    const uint64_t now = rdtsc();
+
+    // Serialize the instruction stream to ensure the rdtsc happens before the
+    // read_msr
+    cpuid(0, &ignored, &ignored, &ignored, &ignored);
+
+    uint64_t cur_deadline = read_msr(IA32_TSC_DEADLINE_MSR);
+    if (cur_deadline != 0 && cur_deadline < now) {
+        // If we wrote a deadline that was already past, clear the timer
+        // and simulate the interrupt.
+        write_msr(IA32_TSC_DEADLINE_MSR, 0);
+        apic_send_self_ipi(X86_INT_APIC_TIMER, DELIVERY_MODE_FIXED);
+    }
 
     arch_interrupt_restore(state, 0);
 }
