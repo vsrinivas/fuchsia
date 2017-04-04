f8a9e06501f2aadc4c6a648eb5f2c85ca3225331
Andrew Wilson <apwilson@google.com>
2017-04-04 16:20:40 -0700

    Suggest launching anything from /system/apps.
    Change-Id: I6638ca9d59eda88fd151e93d1ac8ab7a097fab54

--- bin/agents/module_suggester_dart/main.dart
+++ bin/agents/module_suggester_dart/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:convert' show JSON;
+import 'dart:io';
 import 'package:application.lib.app.dart/app.dart';
 import 'package:apps.maxwell.services.suggestion/ask_handler.fidl.dart';
 import 'package:apps.maxwell.services.suggestion/proposal.fidl.dart';
@@ -14,7 +15,16 @@ import 'package:web_view/web_view.dart' as web_view;
 final _proposalPublisher = new ProposalPublisherProxy();
 final _askHandlerBinding = new AskHandlerBinding();
 
-class AskHandlerImpl extends AskHandler {
+void main(List args) {
+  final context = new ApplicationContext.fromStartupInfo();
+  connectToService(context.environmentServices, _proposalPublisher.ctrl);
+  _proposalPublisher.registerAskHandler(
+    _askHandlerBinding.wrap(new _AskHandlerImpl()),
+  );
+  context.close();
+}
+
+class _AskHandlerImpl extends AskHandler {
   static final _urlSubPattern = new RegExp(r"\.[a-z]{2}");
   static final _dashboardSubPattern = new RegExp(r"^das|^fuc|^bui|^sta");
   static final _chatHeadline = "Open Chat";
@@ -23,85 +33,89 @@ class AskHandlerImpl extends AskHandler {
   void ask(UserInput query, void callback(List<Proposal> proposals)) {
     List<Proposal> proposals = new List();
     if (query.text?.contains(_urlSubPattern) ?? false) {
-      final proposal = new Proposal();
-      proposal.id = "launch web_view";
-
-      proposal.display = new SuggestionDisplay();
-      proposal.display.headline = "Go to " + query.text;
-      proposal.display.subheadline = "";
-      proposal.display.details = "";
-      proposal.display.color = 0xff8080ff;
-      proposal.display.iconUrls = new List<String>();
-      proposal.display.imageType = SuggestionImageType.other;
-      proposal.display.imageUrl = "";
-
-      final createStory = new CreateStory();
-      createStory.moduleId = web_view.kWebViewURL;
       final String url =
           query.text.startsWith("http") ? query.text : "https://" + query.text;
-      createStory.initialData = JSON.encode({"url": url});
 
-      final action = new Action();
-      action.createStory = createStory;
-      proposal.onSelected = new List<Action>();
-      proposal.onSelected.add(action);
-
-      proposals.add(proposal);
+      proposals.add(
+        _createProposal(
+          id: 'launch web_view',
+          appUrl: web_view.kWebViewURL,
+          headline: 'Go to ' + query.text,
+          color: 0xff8080ff,
+          initialData: JSON.encode({"url": url}),
+        ),
+      );
     }
     if (query.text?.contains(_dashboardSubPattern) ?? false) {
-      final proposal = new Proposal();
-      proposal.id = "launch dashboard";
-
-      proposal.display = new SuggestionDisplay();
-      proposal.display.headline = "View the Fuchsia Dashboard";
-      proposal.display.subheadline = "";
-      proposal.display.details = "";
-      proposal.display.color = 0xFFFF00C0;
-      proposal.display.iconUrls = new List<String>();
-      proposal.display.imageType = SuggestionImageType.other;
-      proposal.display.imageUrl =
-          "https://avatars2.githubusercontent.com/u/12826430?v=3&s=200";
-
-      final createStory = new CreateStory();
-      createStory.moduleId = "file:///system/apps/dashboard";
-
-      final action = new Action();
-      action.createStory = createStory;
-      proposal.onSelected = new List<Action>();
-      proposal.onSelected.add(action);
-
-      proposals.add(proposal);
+      proposals.add(
+        _createProposal(
+          id: 'launch dashboard',
+          appUrl: 'file:///system/apps/dashboard',
+          headline: 'View the Fuchsia Dashboard',
+          color: 0xFFFF0080, // Fuchsia
+          imageUrl:
+              'https://avatars2.githubusercontent.com/u/12826430?v=3&s=200',
+        ),
+      );
     }
     if ((query.text?.isNotEmpty ?? false) &&
         _chatHeadline.toLowerCase().contains(query.text.toLowerCase())) {
-      final proposal = new Proposal();
-      proposal.id = "open chat";
+      proposals.add(
+        _createProposal(
+          id: 'open chat',
+          appUrl: 'file:///system/apps/chat_story',
+          headline: _chatHeadline,
+          color: 0xFF9C27B0, // Material Purple 500
+        ),
+      );
+    }
 
-      proposal.display = new SuggestionDisplay();
-      proposal.display.headline = _chatHeadline;
-      proposal.display.subheadline = "";
-      proposal.display.details = "";
-      proposal.display.color = 0xFF9C27B0; // Material Purple 500
-      proposal.display.iconUrls = const <String>[];
-      proposal.display.imageType = SuggestionImageType.other;
-      proposal.display.imageUrl = "";
-
-      final createStory = new CreateStory();
-      createStory.moduleId = "file:///system/apps/chat_story";
-
-      final action = new Action()..createStory = createStory;
-      proposal.onSelected = <Action>[action];
-
-      proposals.add(proposal);
+    if (query.text?.isNotEmpty ?? false) {
+      Directory systemApps = new Directory('/system/apps/');
+      systemApps
+          .listSync(followLinks: false)
+          .map((FileSystemEntity fileSystemEntity) => fileSystemEntity.path)
+          .where((String path) =>
+              Uri.parse(path).pathSegments.last.contains(query.text))
+          .forEach((String path) {
+        String name = Uri.parse(path).pathSegments.last;
+        proposals.add(
+          _createProposal(
+            id: 'open $name',
+            appUrl: 'file://$path',
+            headline: 'Launch $name',
+            subheadline: '(This is potentially unsafe)',
+            color: 0xFF000000 + (name.hashCode % 0xFFFFFF),
+          ),
+        );
+      });
     }
     callback(proposals);
   }
 }
 
-void main(List args) {
-  final context = new ApplicationContext.fromStartupInfo();
-  connectToService(context.environmentServices, _proposalPublisher.ctrl);
-  _proposalPublisher
-      .registerAskHandler(_askHandlerBinding.wrap(new AskHandlerImpl()));
-  context.close();
-}
+Proposal _createProposal({
+  String id,
+  String appUrl,
+  String headline,
+  String subheadline,
+  String imageUrl: '',
+  String initialData,
+  int color,
+}) =>
+    new Proposal()
+      ..id = id
+      ..display = (new SuggestionDisplay()
+        ..headline = headline + (subheadline == null ? '' : '\n$subheadline')
+        ..subheadline = ''
+        ..details = ''
+        ..color = color
+        ..iconUrls = const <String>[]
+        ..imageType = SuggestionImageType.other
+        ..imageUrl = imageUrl)
+      ..onSelected = <Action>[
+        new Action()
+          ..createStory = (new CreateStory()
+            ..moduleId = appUrl
+            ..initialData = initialData)
+      ];
