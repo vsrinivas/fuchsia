a7fd3b8e7e47cceec7f7dffa366ff8d44a16ea08
Craig Stout <cstout@google.com>
2017-04-07 16:48:32 -0700

    Support for Iris Graphics 540
    Change-Id: Ie2db441a53815e9bc807f03c19b6efd460667863

--- drivers/gpu/msd-intel-gen/src/device_id.h
+++ drivers/gpu/msd-intel-gen/src/device_id.h
@@ -16,6 +16,7 @@ public:
         switch (device_id) {
             case 0x1912: // Intel(R) HD Graphics 530 (Skylake GT2)
             case 0x1916: // Intel(R) HD Graphics 520 (Skylake GT2)
+            case 0x1926: // Intel(R) Iris Graphics 540 (Skylake GT3e)
             case 0x5916: // Intel(R) HD Graphics 620 (Kabylake GT2)
                 return true;
         }
