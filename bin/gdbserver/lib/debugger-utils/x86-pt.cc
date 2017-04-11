e757104632a9e88f4f458652ea55beeec072dcae
Doug Evans <dje@google.com>
2017-04-10 19:45:25 -0700

    fill out attribution
    Change-Id: I291afd2eec1a5039fa14fa5de7a2d8411733c09b

--- bin/gdbserver/lib/debugger-utils/x86-pt.cc
+++ bin/gdbserver/lib/debugger-utils/x86-pt.cc
@@ -4,6 +4,37 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT
 
+/*
+ * Portions of this file are derived from "simplept".
+ *
+ * Copyright (c) 2015, Intel Corporation
+ * Author: Andi Kleen
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "x86-pt.h"
 
 #include <atomic>
@@ -44,8 +75,6 @@ const ProcessorTraceFeatures* GetProcessorTraceFeatures() {
 
   if (!x86_feature_test(X86_FEATURE_PT))
     return pt;
-
-  // This code is derived from https://github.com/andikleen/simple-pt
 
   unsigned a, b, c, d;
   unsigned max_leaf = __get_cpuid_max(0, nullptr);
