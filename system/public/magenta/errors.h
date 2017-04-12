d6df6386fd7a0ef5966698d022b7d97244fb37b5
Brian Swetland <swetland@google.com>
2017-04-12 12:23:53 -0700

    [magenta][abi] renumber errors.h
    - put things in numerical order
    - move some more common / important errors to the front of
      their respective sections
    - provide ERR_PEER_CLOSED as alias for ERR_REMOTE_CLOSED
    - leave some expansion space between sections
    
    Change-Id: I9de71eb9b5ff3abbb27e3e246dfc8300788285bc

--- system/public/magenta/errors.h
+++ system/public/magenta/errors.h
@@ -17,7 +17,7 @@
 
 // ERR_NO_RESOURCES: The system was not able to allocate some resource
 // needed for the operation.
-#define ERR_NO_RESOURCES (-5)
+#define ERR_NO_RESOURCES (-3)
 
 // ERR_NO_MEMORY: The system was not able to allocate memory needed
 // for the operation.
@@ -25,34 +25,34 @@
 
 // ERR_CALL_FAILED: The second phase of mx_channel_call() did not complete
 // successfully.
-#define ERR_CALL_FAILED (-53)
+#define ERR_CALL_FAILED (-5)
 
 // ERR_INTERRUPTED_RETRY: The system call was interrupted, but should be
 // retried.  This should not be seen outside of the VDSO.
-#define ERR_INTERRUPTED_RETRY (-57)
+#define ERR_INTERRUPTED_RETRY (-6)
 
 // ======= Parameter errors =======
 // ERR_INVALID_ARGS: an argument is invalid, ex. null pointer
 #define ERR_INVALID_ARGS (-10)
 
+// ERR_BAD_HANDLE: A specified handle value does not refer to a handle.
+#define ERR_BAD_HANDLE (-11)
+
 // ERR_WRONG_TYPE: The subject of the operation is the wrong type to
 // perform the operation.
 // Example: Attempting a message_read on a thread handle.
-#define ERR_WRONG_TYPE (-54)
+#define ERR_WRONG_TYPE (-12)
 
 // ERR_BAD_SYSCALL: The specified syscall number is invalid.
-#define ERR_BAD_SYSCALL (-11)
-
-// ERR_BAD_HANDLE: A specified handle value does not refer to a handle.
-#define ERR_BAD_HANDLE (-12)
+#define ERR_BAD_SYSCALL (-13)
 
 // ERR_OUT_OF_RANGE: An argument is outside the valid range for this
 // operation.
-#define ERR_OUT_OF_RANGE (-13)
+#define ERR_OUT_OF_RANGE (-14)
 
 // ERR_BUFFER_TOO_SMALL: A caller provided buffer is too small for
 // this operation.
-#define ERR_BUFFER_TOO_SMALL (-14)
+#define ERR_BUFFER_TOO_SMALL (-15)
 
 // ======= Precondition or state errors =======
 // ERR_BAD_STATE: operation failed because the current state of the
@@ -60,41 +60,9 @@
 // not satisfied
 #define ERR_BAD_STATE (-20)
 
-// ERR_NOT_FOUND: The requested entity is not found.
-#define ERR_NOT_FOUND (-3)
-
-// ERR_ALREADY_EXISTS: An object with the specified identifier
-// already exists.
-// Example: Attempting to create a file when a file already exists
-// with that name.
-#define ERR_ALREADY_EXISTS (-15)
-
-// ERR_ALREADY_BOUND: The operation failed because the named entity
-// is already owned or controlled by another entity. The operation
-// could succeed later if the current owner releases the entity.
-#define ERR_ALREADY_BOUND (-16)
-
 // ERR_TIMED_OUT: The time limit for the operation elapsed before
 // the operation completed.
-#define ERR_TIMED_OUT (-23)
-
-// deprecated: ERR_HANDLE_CLOSED: a handle being waited on was closed
-// bug MG-585 tracks the cleanup.
-#define ERR_HANDLE_CLOSED (-24)
-// ERR_CANCELED: The in-progress operation (e.g. a wait) has been
-// canceled.
-#define ERR_CANCELED (-24)
-
-// ERR_REMOTE_CLOSED: The operation failed because the remote end
-// of the subject of the operation was closed.
-#define ERR_REMOTE_CLOSED (-25)
-
-// ERR_UNAVAILABLE: The subject of the operation is currently unable
-// to perform the operation.
-// Note: This is used when there's no direct way for the caller to
-// observe when the subject will be able to perform the operation
-// and should thus retry.
-#define ERR_UNAVAILABLE (-26)
+#define ERR_TIMED_OUT (-21)
 
 // ERR_SHOULD_WAIT: The operation cannot be performed currently but
 // potentially could succeed if the caller waits for a prerequisite
@@ -104,7 +72,41 @@
 // messages waiting but has an open remote will return ERR_SHOULD_WAIT.
 // Attempting to read from a channel that has no messages waiting
 // and has a closed remote end will return ERR_REMOTE_CLOSED.
-#define ERR_SHOULD_WAIT (-27)
+#define ERR_SHOULD_WAIT (-22)
+
+// deprecated: ERR_HANDLE_CLOSED: a handle being waited on was closed
+// bug MG-585 tracks the cleanup.
+#define ERR_HANDLE_CLOSED (-23)
+// ERR_CANCELED: The in-progress operation (e.g. a wait) has been
+// canceled.
+#define ERR_CANCELED (-23)
+
+// ERR_REMOTE_CLOSED: The operation failed because the remote end
+// of the subject of the operation was closed.
+#define ERR_REMOTE_CLOSED (-24)
+#define ERR_PEER_CLOSED (-24)
+
+// ERR_NOT_FOUND: The requested entity is not found.
+#define ERR_NOT_FOUND (-25)
+
+// ERR_ALREADY_EXISTS: An object with the specified identifier
+// already exists.
+// Example: Attempting to create a file when a file already exists
+// with that name.
+#define ERR_ALREADY_EXISTS (-26)
+
+// ERR_ALREADY_BOUND: The operation failed because the named entity
+// is already owned or controlled by another entity. The operation
+// could succeed later if the current owner releases the entity.
+#define ERR_ALREADY_BOUND (-27)
+
+// ERR_UNAVAILABLE: The subject of the operation is currently unable
+// to perform the operation.
+// Note: This is used when there's no direct way for the caller to
+// observe when the subject will be able to perform the operation
+// and should thus retry.
+#define ERR_UNAVAILABLE (-28)
+
 
 // ======= Permission check errors =======
 // ERR_ACCESS_DENIED: The caller did not have permission to perform
@@ -135,5 +137,5 @@
 #define ERR_BAD_PATH (-50)
 #define ERR_NOT_DIR (-51)
 #define ERR_NOT_FILE (-52)
-#define ERR_FILE_BIG (-55)  // file exceeds FS size limits
-#define ERR_NO_SPACE (-56)  // FS or device space exhausted
+#define ERR_FILE_BIG (-53)  // file exceeds FS size limits
+#define ERR_NO_SPACE (-54)  // FS or device space exhausted
