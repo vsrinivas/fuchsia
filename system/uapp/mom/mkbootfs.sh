7961b20e379c23a91561ab59b44236bcf92eba12
Abdulla Kamar <abdulla@google.com>
2017-04-12 09:51:34 -0700

    [mom] Minor cleanups to MoM.
    This is in preparation to generalise the app to be able to load any
    kernel image.
    
    Change-Id: I9b4a9954cb5ae07a67f6dd38ddd08de38b067a1d

---
+++ system/uapp/mom/mkbootfs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright 2017 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+BUILDDIR="${1:-build-magenta-pc-x86-64}"
+KERNEL="${2:-$BUILDDIR/magenta.bin}"
+
+echo "data/kernel.bin=$KERNEL" > /tmp/mom.manifest
+$BUILDDIR/tools/mkbootfs \
+    --target=boot \
+    -o $BUILDDIR/bootdata-with-kernel.bin \
+    $BUILDDIR/bootdata.bin \
+    /tmp/mom.manifest
--- system/uapp/mom/mom.c
+++ system/uapp/mom/mom.c
@@ -2,15 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <inttypes.h>
-#include <limits.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <hypervisor/guest.h>
 #include <magenta/boot/bootdata.h>
-#include <magenta/process.h>
 #include <magenta/syscalls.h>
 #include <magenta/syscalls/hypervisor.h>
 
@@ -24,41 +22,29 @@ typedef struct {
     bootdata_kernel_t data_kernel;
 } magenta_kernel_t;
 
-static mx_status_t load_kernel_image(void* addr, FILE* file, uintptr_t* guest_entry) {
-    fseek(file, 0, SEEK_END);
-    size_t file_size = ftell(file);
-    size_t header_size = sizeof(magenta_kernel_t);
-
-    if (file_size < header_size) {
-        printf("File is too small to be a magenta image.\n");
-        return ERR_BAD_STATE;
-    }
-
-    if (file_size > kVmoSize - kKernelLoadOffset) {
-        printf("File is too large to be loaded into the vmo.\n");
-        return ERR_BAD_STATE;
-    }
-
-    rewind(file);
-    if (file_size != fread(addr, 1, file_size, file)) {
-        printf("Couldn't copy %d byte image. That's a shame.\n", (int) file_size);
+static mx_status_t load_magenta(int fd, uintptr_t addr, uintptr_t* guest_entry) {
+    uintptr_t header_addr = addr + kKernelLoadOffset;
+    int rc = read(fd, (void*)header_addr, sizeof(magenta_kernel_t));
+    if (rc != sizeof(magenta_kernel_t)) {
+        fprintf(stderr, "Failed to read Magenta kernel header\n");
         return ERR_IO;
     }
 
-    magenta_kernel_t* header = (magenta_kernel_t*) addr;
+    magenta_kernel_t* header = (magenta_kernel_t*)header_addr;
     if (header->hdr_kernel.type != BOOTDATA_KERNEL) {
-        printf("Invalid kernel header type.\n");
-        return ERR_BAD_STATE;
+        fprintf(stderr, "Invalid Magenta kernel header\n");
+        return ERR_IO_DATA_INTEGRITY;
+    }
+    if (header->data_kernel.entry64 >= kVmoSize) {
+        fprintf(stderr, "Kernel entry point is outside of guest physical memory\n");
+        return ERR_IO_DATA_INTEGRITY;
     }
 
-    if (header->data_kernel.entry64 > kVmoSize) {
-        printf("Kernel entry point is outside of addressable space.\n");
-        return ERR_BAD_STATE;
-    }
-
-    if (file_size != (header_size - sizeof(bootdata_kernel_t) + header->hdr_kernel.length)) {
-        printf("File size does not match payload size in header.\n");
-        return ERR_BAD_STATE;
+    uintptr_t data_addr = header_addr + sizeof(magenta_kernel_t);
+    rc = read(fd, (void*)data_addr, kVmoSize - data_addr);
+    if (rc < 0 || (uint64_t)rc < header->hdr_kernel.length - sizeof(bootdata_kernel_t)) {
+        fprintf(stderr, "Failed to read Magenta kernel data\n");
+        return ERR_IO;
     }
 
     *guest_entry = header->data_kernel.entry64;
@@ -67,7 +53,7 @@ static mx_status_t load_kernel_image(void* addr, FILE* file, uintptr_t* guest_en
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        printf("usage: mom <path to magenta.bin>\n");
+        fprintf(stderr, "usage: %s <path to magenta.bin>\n", basename(argv[0]));
         return ERR_INVALID_ARGS;
     }
 
@@ -75,7 +61,7 @@ int main(int argc, char** argv) {
     mx_handle_t hypervisor;
     status = mx_hypervisor_create(MX_HANDLE_INVALID, 0, &hypervisor);
     if (status != NO_ERROR) {
-        printf("Failed to create hypervisor\n");
+        fprintf(stderr, "Failed to create hypervisor\n");
         return status;
     }
 
@@ -83,7 +69,7 @@ int main(int argc, char** argv) {
     mx_handle_t guest_phys_mem;
     status = guest_create_phys_mem(&addr, kVmoSize, &guest_phys_mem);
     if (status != NO_ERROR) {
-        printf("Failed to create guest physical memory\n");
+        fprintf(stderr, "Failed to create guest physical memory\n");
         return status;
     }
 
@@ -91,37 +77,34 @@ int main(int argc, char** argv) {
     mx_handle_t guest;
     status = guest_create(hypervisor, guest_phys_mem, &guest_serial_fifo, &guest);
     if (status != NO_ERROR) {
-        printf("Failed to create guest\n");
+        fprintf(stderr, "Failed to create guest\n");
         return status;
     }
 
     uintptr_t guest_entry = 0;
     status = guest_create_identity_pt(addr, kVmoSize, &guest_entry);
     if (status != NO_ERROR) {
-        printf("Failed to create page table\n");
+        fprintf(stderr, "Failed to create page table\n");
         return status;
     }
 
-    printf("Loading %s\n", argv[1]);
-    FILE* magenta_file = fopen(argv[1], "rb");
-    if (magenta_file == NULL) {
-        printf("%s not found\n", argv[1]);
-        return status;
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "Failed to open Magenta kernel image \"%s\"\n", argv[1]);
+        return ERR_IO;
     }
 
-    status = load_kernel_image((void*)(addr + kKernelLoadOffset), magenta_file, &guest_entry);
-    if (status != NO_ERROR) {
-        printf("Failed to load kernel into VMO\n");
+    status = load_magenta(fd, addr, &guest_entry);
+    close(fd);
+    if (status != NO_ERROR)
         return status;
-    }
-    fclose(magenta_file);
 
 #if __x86_64__
     uintptr_t guest_cr3 = 0;
     status = mx_hypervisor_op(guest, MX_HYPERVISOR_OP_GUEST_SET_CR3, &guest_cr3,
                               sizeof(guest_cr3), NULL, 0);
     if (status != NO_ERROR) {
-        printf("Failed to set guest CR3\n");
+        fprintf(stderr, "Failed to set guest CR3\n");
         return status;
     }
 #endif // __x86_64__
@@ -129,13 +112,13 @@ int main(int argc, char** argv) {
     status = mx_hypervisor_op(guest, MX_HYPERVISOR_OP_GUEST_SET_ENTRY,
                               &guest_entry, sizeof(guest_entry), NULL, 0);
     if (status != NO_ERROR) {
-        printf("Failed to set guest RIP\n");
+        fprintf(stderr, "Failed to set guest RIP\n");
         return status;
     }
 
     status = mx_hypervisor_op(guest, MX_HYPERVISOR_OP_GUEST_ENTER, NULL, 0, NULL, 0);
     if (status != NO_ERROR) {
-        printf("Failed to enter guest\n");
+        fprintf(stderr, "Failed to enter guest\n");
         return status;
     }
 
