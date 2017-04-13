011e0f37dd6f9c96c531c698e7c758bd62436bb8
James Tucker <jftucker@gmail.com>
2017-04-13 13:52:23 -0700

    grubdisk: tools for making magenta bios boot vols
    Change-Id: Id5a216498d070d96dd5f0413e90af0c00c3e3f7f

---
+++ grubdisk/build-grub.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/env.sh
+
+toolchain="$MAGENTA_DIR/prebuilt/downloads/x86_64-elf-6.3.0-$(uname)-x86_64/bin"
+if [[ ! -d $toolchain ]]; then
+  "./$MAGENTA_DIR/scripts/download-toolchain"
+fi
+
+if [[ ! -e $FUCHSIA_OUT_DIR/build-objconv/bin/objconv ]]; then
+  mkdir "$FUCHSIA_OUT_DIR/build-objconv"
+  cd "$FUCHSIA_OUT_DIR/build-objconv"
+  wget http://www.agner.org/optimize/objconv.zip
+  shasum -c 5442e7bf53e8ed261424e4271262807b7ca9eb2468be7577e4197c8ed1be96b6 objconv.zip
+  unzip objconv.zip
+  unzip source.zip
+  mkdir bin
+  g++ -o bin/objconv -O2 *.cpp || exit 1
+fi
+export PATH="$FUCHSIA_OUT_DIR/build-objconv/bin:$PATH"
+
+mkdir "$FUCHSIA_GRUB_DIR"
+cd "$FUCHSIA_GRUB_DIR"
+git clone git://git.savannah.gnu.org/grub.git
+cd grub
+git checkout 007f0b407f72314ec832d77e15b83ea40b160037
+if [[ ! -f configure ]]; then
+  ./autogen.sh
+fi
+if [[ ! -f Makefile ]]; then
+  ./configure --target=x86_64-elf --prefix="$FUCHSIA_GRUB_DIR" \
+    TARGET_CC=$toolchain/x86_64-elf-gcc TARGET_OBJCOPY=$toolchain/x86_64-elf-objcopy \
+    TARGET_STRIP=$toolchain/x86_64-elf-strip TARGET_NM=$toolchain/x86_64-elf-nm \
+    TARGET_RANLIB=$toolchain/x86_64-elf-ranlib
+fi
+make
+make install
+
+echo "Grub tools are available from $FUCHSIA_GRUB_DIR/bin"
---
+++ grubdisk/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+if [[ -z $FUCHSIA_DIR ]]; then
+  source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../env.sh
+fi
+
+export FUCHSIA_GRUB_SCRIPTS_DIR="$FUCHSIA_DIR/scripts/grubdisk"
+export FUCHSIA_GRUB_DIR="$FUCHSIA_OUT_DIR/build-grub"
