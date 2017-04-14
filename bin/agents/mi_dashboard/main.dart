3c09fc2e271d7a35aa5e0e59a3389947d953ee50
Jeffrey Nichols <jwnichols@google.com>
2017-04-14 14:45:50 -0700

    mi_dashboard: serve data from context service
    Connects to the context service, subscribe to a fixed list of topics
    (just one at the moment), caches the data received, and serves this
    data through the http server for /data/context/<topic> requests.
    
    Change-Id: I81de2d4bce7fd161ade810a3d260b4dc2a62aeef

--- bin/agents/mi_dashboard/main.dart
+++ bin/agents/mi_dashboard/main.dart
@@ -6,6 +6,8 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'package:application.lib.app.dart/app.dart';
+import 'package:apps.maxwell.services.context/context_provider.fidl.dart';
+import 'package:lib.fidl.dart/bindings.dart';
 import 'package:path/path.dart' as path;
 
 const _configDir = "/system/data/mi_dashboard";
@@ -20,11 +22,48 @@ int _port = _defaultPort;
 var _webrootPath = _defaultWebrootPath;
 Directory _webrootDirectory;
 
+// TODO(jwnichols): Make sure all the relevant topics are listed here
+// Eventually we may want a method to ContextProvider that subscribes to all
+// topics.
+final _topics = ["/modular_state"];
+final _contextCache = new Map<String,String>();
+
+final _contextProvider = new ContextProviderProxy();
+final _contextListener = new _ContextListenerImpl();
+
 void main(List args) {
+  // Get a handle to the ContextProvider service
+  final app_context = new ApplicationContext.fromStartupInfo();
+  connectToService(app_context.environmentServices, _contextProvider.ctrl);
+  assert(_contextProvider.ctrl.isBound);
+  app_context.close();
+
+  // Subscribe to the topics in |_topics|.
+  ContextQuery query = new ContextQuery();
+  query.topics = _topics;
+  _contextProvider.subscribe(query, _contextListener.getHandle());
+
   // Read the config file from disk
   var configFile = new File(path.join(_configDir,_configFilename));
   configFile.readAsString(encoding: ASCII)
     .then(parseConfigAndStart);
+}
+
+class _ContextListenerImpl extends ContextListener {
+  final ContextListenerBinding _binding = new ContextListenerBinding();
+
+  _ContextListenerImpl();
+
+  InterfaceHandle<ContextListener> getHandle() => _binding.wrap(this);
+
+  @override
+  void onUpdate(ContextUpdate update) {
+    // Cache all context values that we receive
+    update.values.forEach((String key, String value) {
+      // print("[DASHBOARD UPDATE] ${key}: ${value}");
+      _contextCache[key] = value;
+    });
+  }
 }
 
 void parseConfigAndStart(var configString) {
@@ -50,28 +89,59 @@ void parseConfigAndStart(var configString) {
 }
 
 void handleRequest(HttpRequest request) {
-  // TODO(jwnichols): Identify requests requiring return of dynamic data
-  // Probably /data/... requests will return JSON data from the context service
+  // Identify requests requiring return of context data
+  // Such requests will begin with /data/<service>/...
+  var dataRequestPattern = new RegExp("/data/([^/]+)(/.+)");
+  var match = dataRequestPattern.firstMatch(request.requestedUri.path);
+  if (match != null) {
+    var serviceName = match.group(1); // first match group is the service name
+    // print("Returning data for service ${serviceName}");
 
-  // Find the referenced file
-  // path.join does not work in this case, possibly because the request path
-  // may start with a /, so using a simple string concatenation instead
-  var requestPath = "${_webrootDirectory.path}/${request.requestedUri.path}";
-  if (requestPath.endsWith('/'))
-    requestPath += "index.html";
-  var requestFile = new File(requestPath);
-  requestFile.exists().then((exists) {
-      if (exists) {
-        // Make sure the referenced file is within the webroot
-        if (requestFile.uri.path.startsWith(_webrootDirectory.path)) {
-          sendFile(requestFile, request.response);
+    // we are returning JSON
+    request.response.headers.contentType
+      = new ContentType("application", "json", charset: "utf-8");
+
+    // Figure out what service data to return
+    switch(serviceName) {
+      case 'context':
+        //   /data/context/<topic>
+        //     return JSON data from the context service for the given topic
+        var topic = match.group(2);
+        var topicValue = _contextCache[topic];
+        // print("[DASHBOARD] Request for context topic ${topic} with value ${topicValue}");
+        if (topicValue != null) {
+          // Write the data to the response.
+          request.response.write(topicValue);
+          request.response.close();
           return;
         }
-      }
 
-      // if we didn't return before now, send an error response
-      send404(request.response);
-  });
+        // TODO(jwnichols): Report data from other intelligence services. E.g.,
+        //   /data/actionlog/...
+        //   /data/suggestions/...
+    }
+    // Nothing handled the request, so respond with a 404
+    send404(request.response);
+  } else {
+    // Find the referenced file
+    // path.join does not work in this case, possibly because the request path
+    // may start with a /, so using a simple string concatenation instead
+    var requestPath = "${_webrootDirectory.path}/${request.requestedUri.path}";
+    if (requestPath.endsWith('/'))
+      requestPath += "index.html";
+    var requestFile = new File(requestPath);
+    requestFile.exists().then((exists) {
+        if (exists) {
+          // Make sure the referenced file is within the webroot
+          if (requestFile.uri.path.startsWith(_webrootDirectory.path)) {
+            sendFile(requestFile, request.response);
+            return;
+          }
+        } else {
+          send404(request.response);
+        }
+      });
+  }
 }
 
 sendFile(File requestFile, HttpResponse response) async {
