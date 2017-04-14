e023883c4deceb508f0fb178246edd198126fe9b
Todd Eisenberger <teisenbe@google.com>
2017-04-14 16:26:47 -0700

    [mx][time] Mark inline header functions as inline
    Otherwise this can create symbol redefinitions.
    
    Change-Id: I66dbdb42b86fbdcc2fddabe8602b782ddc99c638

--- system/ulib/mx/include/mx/time.h
+++ system/ulib/mx/include/mx/time.h
@@ -10,17 +10,17 @@ namespace mx {
 
 namespace time {
 
-mx_time_t get(uint32_t clock_id) {
+inline mx_time_t get(uint32_t clock_id) {
     return mx_time_get(clock_id);
 }
 
 } // namespace time
 
-mx_status_t nanosleep(uint64_t nanoseconds) {
+inline mx_status_t nanosleep(uint64_t nanoseconds) {
     return mx_nanosleep(nanoseconds);
 }
 
-mx_time_t deadline_after(mx_duration_t nanoseconds) {
+inline mx_time_t deadline_after(mx_duration_t nanoseconds) {
     return mx_deadline_after(nanoseconds);
 }
 
