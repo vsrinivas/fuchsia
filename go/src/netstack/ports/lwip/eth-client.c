32a3d9ade2ee3c985130a0de43e97d495fa2c6af
Todd Eisenberger <teisenbe@google.com>
2017-04-17 14:47:51 -0700

    Make use of mx_deadline_after for relative timeouts
    Change-Id: I64e54a9959ecb2173c8f8149880b82b29d77f161

--- go/src/netstack/ports/lwip/eth-client.c
+++ go/src/netstack/ports/lwip/eth-client.c
@@ -160,9 +160,12 @@ mx_status_t eth_wait_rx(eth_client_t* eth, mx_time_t timeout) {
   mx_status_t status;
   mx_signals_t signals;
 
+  mx_time_t deadline = (timeout != MX_TIME_INFINITE) ? mx_deadline_after(timeout) :
+          MX_TIME_INFINITE;
+
   if ((status = mx_object_wait_one(eth->rx_fifo,
                                    MX_FIFO_READABLE | MX_FIFO_PEER_CLOSED,
-                                   timeout, &signals)) < 0) {
+                                   deadline, &signals)) < 0) {
     return status;
   }
   if (signals & MX_FIFO_PEER_CLOSED) {
