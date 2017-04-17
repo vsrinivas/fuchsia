LlsDimple/magenta

[time] Switch callers of syscalls with timeouts to use conversion function
This is in preparation for moving syscalls to absolute deadlines rather
than relative timeouts.

Change-Id: Ic85abbf76f34fbce7899c2505be840e3d3dcf7ff

system/uapp/audio2/audio-output.cpp
@@ -24,7 +24,7 @@ mx_status_t DoCall(const mx::channel& channel,
                    const ReqType&     req,
                    RespType*          resp,
                    mx::handle*        resp_handle_out = nullptr) {
-    constexpr mx_time_t CALL_TIMEOUT = 100000000u;
+    constexpr mx_time_t CALL_TIMEOUT = MX_MSEC(100);
     mx_channel_call_args_t args;
 
     MX_DEBUG_ASSERT((resp_handle_out == nullptr) || !resp_handle_out->is_valid());
@@ -41,7 +41,8 @@ mx_status_t DoCall(const mx::channel& channel,
     uint32_t bytes, handles;
     mx_status_t read_status, write_status;
 
-    write_status = channel.call(0, CALL_TIMEOUT, &args, &bytes, &handles, &read_status);
+    write_status = channel.call(0, mx_deadline_after(CALL_TIMEOUT), &args, &bytes, &handles,
+                                &read_status);
 
     if (write_status != NO_ERROR) {
         if (write_status == ERR_CALL_FAILED) {
@@ -366,7 +367,7 @@ mx_status_t AudioOutput::Play(AudioSource& source) {
         //
         // TODO: base this on the start time and the number of frames queued
         // instead of just making a number up.
-        mx_nanosleep(30000000);
+        mx_nanosleep(mx_deadline_after(MX_MSEC(30)));
     }
 
     mx_status_t stop_res = StopRingBuffer();

system/utest/core/futex/futex.cpp
@@ -37,10 +37,10 @@ static bool test_futex_wait_timeout() {
 static bool test_futex_wait_timeout_elapsed() {
     BEGIN_TEST;
     int futex_value = 0;
-    constexpr mx_time_t kRelativeDeadline = 500 * 1000 * 1000;
+    constexpr mx_time_t kRelativeDeadline = MX_MSEC(500);
     for (int i = 0; i < 5; ++i) {
         mx_time_t now = mx_time_get(MX_CLOCK_MONOTONIC);
-        mx_status_t rc = mx_futex_wait(&futex_value, 0, kRelativeDeadline);
+        mx_status_t rc = mx_futex_wait(&futex_value, 0, mx_deadline_after(kRelativeDeadline));
         ASSERT_EQ(rc, ERR_TIMED_OUT, "wait should time out");
         mx_time_t elapsed = mx_time_get(MX_CLOCK_MONOTONIC) - now;
         if (elapsed < kRelativeDeadline) {
@@ -69,7 +69,7 @@ public:
     TestThread(volatile int* futex_addr,
                mx_time_t timeout_in_us = MX_TIME_INFINITE)
         : futex_addr_(futex_addr),
-          timeout_in_us_(timeout_in_us) {
+          timeout_in_ns_(timeout_in_us) {
         auto ret = thrd_create_with_name(&thread_, wakeup_test_thread, this, "wakeup_test_thread");
         EXPECT_EQ(ret, thrd_success, "Error during thread creation");
         while (state_ == STATE_STARTED) {
@@ -137,10 +137,11 @@ private:
     static int wakeup_test_thread(void* thread_arg) {
         TestThread* thread = reinterpret_cast<TestThread*>(thread_arg);
         thread->state_ = STATE_ABOUT_TO_WAIT;
-        mx_status_t rc =
-            mx_futex_wait(const_cast<int*>(thread->futex_addr_),
-                                *thread->futex_addr_, thread->timeout_in_us_);
-        if (thread->timeout_in_us_ == MX_TIME_INFINITE) {
+        mx_time_t deadline = thread->timeout_in_ns_ == MX_TIME_INFINITE ? MX_TIME_INFINITE :
+                mx_deadline_after(thread->timeout_in_ns_);
+        mx_status_t rc = mx_futex_wait(const_cast<int*>(thread->futex_addr_),
+                                       *thread->futex_addr_, deadline);
+        if (thread->timeout_in_ns_ == MX_TIME_INFINITE) {
             EXPECT_EQ(rc, NO_ERROR, "Error while wait");
         } else {
             EXPECT_EQ(rc, ERR_TIMED_OUT, "wait should have timedout");
@@ -151,7 +152,7 @@ private:
 
     thrd_t thread_;
     volatile int* futex_addr_;
-    mx_time_t timeout_in_us_;
+    mx_time_t timeout_in_ns_;
     mx_handle_t handle_ = MX_HANDLE_INVALID;
     volatile enum {
         STATE_STARTED = 100,
@@ -239,7 +240,7 @@ bool test_futex_unqueued_on_timeout() {
     BEGIN_TEST;
     volatile int futex_value = 1;
     mx_status_t rc = mx_futex_wait(const_cast<int*>(&futex_value),
-                                         futex_value, 1);
+                                   futex_value, mx_deadline_after(1));
     ASSERT_EQ(rc, ERR_TIMED_OUT, "wait should have timedout");
     TestThread thread(&futex_value);
     // If the earlier futex_wait() did not remove itself from the wait
@@ -256,7 +257,7 @@ bool test_futex_unqueued_on_timeout_2() {
     BEGIN_TEST;
     volatile int futex_value = 10;
     TestThread thread1(&futex_value);
-    TestThread thread2(&futex_value, 200 * 1000 * 1000);
+    TestThread thread2(&futex_value, MX_MSEC(200));
     ASSERT_TRUE(thread2.wait_for_timeout(), "");
     // With the bug present, thread2 was removed but the futex wait queue's
     // tail pointer still points to thread2.  When another thread is
@@ -273,7 +274,7 @@ bool test_futex_unqueued_on_timeout_2() {
 bool test_futex_unqueued_on_timeout_3() {
     BEGIN_TEST;
     volatile int futex_value = 10;
-    TestThread thread1(&futex_value, 400 * 1000 * 1000);
+    TestThread thread1(&futex_value, MX_MSEC(400));
     TestThread thread2(&futex_value);
     TestThread thread3(&futex_value);
     ASSERT_TRUE(thread1.wait_for_timeout(), "");
@@ -351,10 +352,10 @@ bool test_futex_requeue() {
 // itself from the correct queue in that case.
 bool test_futex_requeue_unqueued_on_timeout() {
     BEGIN_TEST;
-    mx_time_t timeout_in_us = 300 * 1000 * 1000;
+    mx_time_t timeout_in_ns = MX_MSEC(300);
     volatile int futex_value1 = 100;
     volatile int futex_value2 = 200;
-    TestThread thread1(&futex_value1, timeout_in_us);
+    TestThread thread1(&futex_value1, timeout_in_ns);
     mx_status_t rc = mx_futex_requeue(
         const_cast<int*>(&futex_value1), 0, futex_value1,
         const_cast<int*>(&futex_value2), INT_MAX);
@@ -482,7 +483,7 @@ static bool test_event_signaling() {
     thrd_create_with_name(&thread2, signal_thread2, NULL, "thread 2");
     thrd_create_with_name(&thread3, signal_thread3, NULL, "thread 3");
 
-    mx_nanosleep(300 * 1000 * 1000);
+    mx_nanosleep(mx_deadline_after(MX_MSEC(300)));
     log("signaling event\n");
     event.signal();
 

system/utest/core/threads/threads.c
@@ -69,7 +69,7 @@ static bool start_thread(mxr_thread_entry_t entry, void* arg,
 static bool start_and_kill_thread(mxr_thread_entry_t entry, void* arg) {
     mxr_thread_t thread;
     ASSERT_TRUE(start_thread(entry, arg, &thread), "");
-    mx_nanosleep(MX_MSEC(100));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(100)));
     ASSERT_EQ(mxr_thread_kill(&thread), NO_ERROR, "");
     ASSERT_EQ(mxr_thread_join(&thread), NO_ERROR, "");
     return true;
@@ -78,7 +78,8 @@ static bool start_and_kill_thread(mxr_thread_entry_t entry, void* arg) {
 static bool test_basics(void) {
     BEGIN_TEST;
     mxr_thread_t thread;
-    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)MX_MSEC(100), &thread), "");
+    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)mx_deadline_after(MX_MSEC(100)),
+                             &thread), "");
     ASSERT_EQ(mx_object_wait_one(mxr_thread_get_handle(&thread),
                                  MX_THREAD_SIGNALED, MX_TIME_INFINITE, NULL),
               NO_ERROR, "");
@@ -143,7 +144,7 @@ static bool test_thread_start_with_zero_instruction_pointer(void) {
     // Give crashlogger a little time to print info about the new thread
     // (since it will start and crash), otherwise that output gets
     // interleaved with the test runner's output.
-    mx_nanosleep(MX_MSEC(100));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(100)));
 
     ASSERT_EQ(mx_handle_close(process), NO_ERROR, "");
     ASSERT_EQ(mx_handle_close(vmar), NO_ERROR, "");
@@ -182,7 +183,8 @@ static bool test_info_task_stats_fails(void) {
     BEGIN_TEST;
     // Spin up a thread.
     mxr_thread_t thread;
-    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)MX_MSEC(100), &thread), "");
+    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)mx_deadline_after(MX_MSEC(100)), &thread),
+                "");
     mx_handle_t thandle = mxr_thread_get_handle(&thread);
     ASSERT_EQ(mx_object_wait_one(thandle,
                                  MX_THREAD_SIGNALED, MX_TIME_INFINITE, NULL),
@@ -212,17 +214,19 @@ static bool test_resume_suspended(void) {
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
 
     // The thread should still be blocked on the event when it wakes up
-    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, MX_MSEC(100), NULL), ERR_TIMED_OUT, "");
+    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, mx_deadline_after(MX_MSEC(100)),
+                                 NULL), ERR_TIMED_OUT, "");
 
     // Check that signaling the event while suspended results in the expected
     // behavior
     ASSERT_EQ(mx_task_suspend(thread_h), NO_ERROR, "");
     // TODO: Use an exception port to wait for the suspend to take effect
-    mx_nanosleep(MX_MSEC(10));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(10)));
 
     ASSERT_EQ(mx_object_signal(event, 0, MX_USER_SIGNAL_0), NO_ERROR, "");
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
-    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, MX_MSEC(100), NULL), NO_ERROR, "");
+    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, mx_deadline_after(MX_MSEC(100)),
+                                 NULL), NO_ERROR, "");
 
     ASSERT_EQ(mx_handle_close(event), NO_ERROR, "");
 
@@ -239,7 +243,7 @@ static bool test_kill_suspended(void) {
     ASSERT_TRUE(start_thread(test_wait_thread_fn, &event, &thread), "");
     mx_handle_t thread_h = mxr_thread_get_handle(&thread);
     ASSERT_EQ(mx_task_suspend(thread_h), NO_ERROR, "");
-    mx_nanosleep(MX_MSEC(10));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(10)));
     ASSERT_EQ(mx_task_kill(thread_h), NO_ERROR, "");
 
     ASSERT_EQ(mx_handle_close(event), NO_ERROR, "");
@@ -252,15 +256,15 @@ static bool test_suspend_sleeping(void) {
 
     mx_time_t now = mx_time_get(MX_CLOCK_MONOTONIC);
 
-    const mx_time_t sleep_time = MX_MSEC(100);
+    const mx_time_t sleep_deadline = mx_deadline_after(MX_MSEC(100));
     mxr_thread_t thread;
 
     // TODO(teisenbe): This code could be made less racy with a deadline sleep
     // mode when we get one.
-    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)sleep_time, &thread), "");
+    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)sleep_deadline, &thread), "");
     mx_handle_t thread_h = mxr_thread_get_handle(&thread);
 
-    mx_nanosleep(sleep_time / 4);
+    mx_nanosleep(sleep_deadline - MX_MSEC(50));
     ASSERT_EQ(mx_task_suspend(thread_h), NO_ERROR, "");
 
     // TODO(teisenbe): Once we wire in exceptions for suspend, check here that
@@ -269,9 +273,10 @@ static bool test_suspend_sleeping(void) {
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
 
     // Wait for the sleep to finish
-    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, 2 * sleep_time, NULL), NO_ERROR, "");
+    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, sleep_deadline + MX_MSEC(50), NULL),
+              NO_ERROR, "");
     mx_time_t elapsed = mx_time_get(MX_CLOCK_MONOTONIC) - now;
-    ASSERT_GE(elapsed, sleep_time, "thread did not sleep long enough");
+    ASSERT_GE(elapsed, sleep_deadline, "thread did not sleep long enough");
 
     END_TEST;
 }
@@ -401,7 +406,7 @@ static bool test_suspend_stops_thread(void) {
     while (arg.v != 2) {
         arg.v = 2;
         // Give the thread a chance to clobber the value
-        mx_nanosleep(MX_MSEC(50));
+        mx_nanosleep(mx_deadline_after(MX_MSEC(50)));
     }
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
     while (arg.v != 1) {

vsrinivas/fuchsia

system/uapp/audio2/audio-output.cpp
@@ -24,7 +24,7 @@ mx_status_t DoCall(const mx::channel& channel,
                    const ReqType&     req,
                    RespType*          resp,
                    mx::handle*        resp_handle_out = nullptr) {
-    constexpr mx_time_t CALL_TIMEOUT = 100000000u;
+    constexpr mx_duration_t CALL_TIMEOUT = MX_MSEC(100);
     mx_channel_call_args_t args;
 
     MX_DEBUG_ASSERT((resp_handle_out == nullptr) || !resp_handle_out->is_valid());
@@ -41,7 +41,8 @@ mx_status_t DoCall(const mx::channel& channel,
     uint32_t bytes, handles;
     mx_status_t read_status, write_status;
 
-    write_status = channel.call(0, CALL_TIMEOUT, &args, &bytes, &handles, &read_status);
+    write_status = channel.call(0, mx_deadline_after(CALL_TIMEOUT), &args, &bytes, &handles,
+                                &read_status);
 
     if (write_status != NO_ERROR) {
         if (write_status == ERR_CALL_FAILED) {
@@ -366,7 +367,7 @@ mx_status_t AudioOutput::Play(AudioSource& source) {
         //
         // TODO: base this on the start time and the number of frames queued
         // instead of just making a number up.
-        mx_nanosleep(30000000);
+        mx_nanosleep(mx_deadline_after(MX_MSEC(30)));
     }
 
     mx_status_t stop_res = StopRingBuffer();

system/utest/core/futex/futex.cpp
@@ -37,10 +37,10 @@ static bool test_futex_wait_timeout() {
 static bool test_futex_wait_timeout_elapsed() {
     BEGIN_TEST;
     int futex_value = 0;
-    constexpr mx_time_t kRelativeDeadline = 500 * 1000 * 1000;
+    constexpr mx_duration_t kRelativeDeadline = MX_MSEC(500);
     for (int i = 0; i < 5; ++i) {
         mx_time_t now = mx_time_get(MX_CLOCK_MONOTONIC);
-        mx_status_t rc = mx_futex_wait(&futex_value, 0, kRelativeDeadline);
+        mx_status_t rc = mx_futex_wait(&futex_value, 0, mx_deadline_after(kRelativeDeadline));
         ASSERT_EQ(rc, ERR_TIMED_OUT, "wait should time out");
         mx_time_t elapsed = mx_time_get(MX_CLOCK_MONOTONIC) - now;
         if (elapsed < kRelativeDeadline) {
@@ -67,9 +67,9 @@ static bool test_futex_wait_bad_address() {
 class TestThread {
 public:
     TestThread(volatile int* futex_addr,
-               mx_time_t timeout_in_us = MX_TIME_INFINITE)
+               mx_duration_t timeout_in_us = MX_TIME_INFINITE)
         : futex_addr_(futex_addr),
-          timeout_in_us_(timeout_in_us) {
+          timeout_in_ns_(timeout_in_us) {
         auto ret = thrd_create_with_name(&thread_, wakeup_test_thread, this, "wakeup_test_thread");
         EXPECT_EQ(ret, thrd_success, "Error during thread creation");
         while (state_ == STATE_STARTED) {
@@ -137,10 +137,11 @@ private:
     static int wakeup_test_thread(void* thread_arg) {
         TestThread* thread = reinterpret_cast<TestThread*>(thread_arg);
         thread->state_ = STATE_ABOUT_TO_WAIT;
-        mx_status_t rc =
-            mx_futex_wait(const_cast<int*>(thread->futex_addr_),
-                                *thread->futex_addr_, thread->timeout_in_us_);
-        if (thread->timeout_in_us_ == MX_TIME_INFINITE) {
+        mx_time_t deadline = thread->timeout_in_ns_ == MX_TIME_INFINITE ? MX_TIME_INFINITE :
+                mx_deadline_after(thread->timeout_in_ns_);
+        mx_status_t rc = mx_futex_wait(const_cast<int*>(thread->futex_addr_),
+                                       *thread->futex_addr_, deadline);
+        if (thread->timeout_in_ns_ == MX_TIME_INFINITE) {
             EXPECT_EQ(rc, NO_ERROR, "Error while wait");
         } else {
             EXPECT_EQ(rc, ERR_TIMED_OUT, "wait should have timedout");
@@ -151,7 +152,7 @@ private:
 
     thrd_t thread_;
     volatile int* futex_addr_;
-    mx_time_t timeout_in_us_;
+    mx_duration_t timeout_in_ns_;
     mx_handle_t handle_ = MX_HANDLE_INVALID;
     volatile enum {
         STATE_STARTED = 100,
@@ -239,7 +240,7 @@ bool test_futex_unqueued_on_timeout() {
     BEGIN_TEST;
     volatile int futex_value = 1;
     mx_status_t rc = mx_futex_wait(const_cast<int*>(&futex_value),
-                                         futex_value, 1);
+                                   futex_value, mx_deadline_after(1));
     ASSERT_EQ(rc, ERR_TIMED_OUT, "wait should have timedout");
     TestThread thread(&futex_value);
     // If the earlier futex_wait() did not remove itself from the wait
@@ -256,7 +257,7 @@ bool test_futex_unqueued_on_timeout_2() {
     BEGIN_TEST;
     volatile int futex_value = 10;
     TestThread thread1(&futex_value);
-    TestThread thread2(&futex_value, 200 * 1000 * 1000);
+    TestThread thread2(&futex_value, MX_MSEC(200));
     ASSERT_TRUE(thread2.wait_for_timeout(), "");
     // With the bug present, thread2 was removed but the futex wait queue's
     // tail pointer still points to thread2.  When another thread is
@@ -273,7 +274,7 @@ bool test_futex_unqueued_on_timeout_2() {
 bool test_futex_unqueued_on_timeout_3() {
     BEGIN_TEST;
     volatile int futex_value = 10;
-    TestThread thread1(&futex_value, 400 * 1000 * 1000);
+    TestThread thread1(&futex_value, MX_MSEC(400));
     TestThread thread2(&futex_value);
     TestThread thread3(&futex_value);
     ASSERT_TRUE(thread1.wait_for_timeout(), "");
@@ -351,10 +352,10 @@ bool test_futex_requeue() {
 // itself from the correct queue in that case.
 bool test_futex_requeue_unqueued_on_timeout() {
     BEGIN_TEST;
-    mx_time_t timeout_in_us = 300 * 1000 * 1000;
+    mx_duration_t timeout_in_ns = MX_MSEC(300);
     volatile int futex_value1 = 100;
     volatile int futex_value2 = 200;
-    TestThread thread1(&futex_value1, timeout_in_us);
+    TestThread thread1(&futex_value1, timeout_in_ns);
     mx_status_t rc = mx_futex_requeue(
         const_cast<int*>(&futex_value1), 0, futex_value1,
         const_cast<int*>(&futex_value2), INT_MAX);
@@ -482,7 +483,7 @@ static bool test_event_signaling() {
     thrd_create_with_name(&thread2, signal_thread2, NULL, "thread 2");
     thrd_create_with_name(&thread3, signal_thread3, NULL, "thread 3");
 
-    mx_nanosleep(300 * 1000 * 1000);
+    mx_nanosleep(mx_deadline_after(MX_MSEC(300)));
     log("signaling event\n");
     event.signal();
 

system/utest/core/threads/threads.c
@@ -69,7 +69,7 @@ static bool start_thread(mxr_thread_entry_t entry, void* arg,
 static bool start_and_kill_thread(mxr_thread_entry_t entry, void* arg) {
     mxr_thread_t thread;
     ASSERT_TRUE(start_thread(entry, arg, &thread), "");
-    mx_nanosleep(MX_MSEC(100));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(100)));
     ASSERT_EQ(mxr_thread_kill(&thread), NO_ERROR, "");
     ASSERT_EQ(mxr_thread_join(&thread), NO_ERROR, "");
     return true;
@@ -78,7 +78,8 @@ static bool start_and_kill_thread(mxr_thread_entry_t entry, void* arg) {
 static bool test_basics(void) {
     BEGIN_TEST;
     mxr_thread_t thread;
-    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)MX_MSEC(100), &thread), "");
+    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)mx_deadline_after(MX_MSEC(100)),
+                             &thread), "");
     ASSERT_EQ(mx_object_wait_one(mxr_thread_get_handle(&thread),
                                  MX_THREAD_SIGNALED, MX_TIME_INFINITE, NULL),
               NO_ERROR, "");
@@ -143,7 +144,7 @@ static bool test_thread_start_with_zero_instruction_pointer(void) {
     // Give crashlogger a little time to print info about the new thread
     // (since it will start and crash), otherwise that output gets
     // interleaved with the test runner's output.
-    mx_nanosleep(MX_MSEC(100));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(100)));
 
     ASSERT_EQ(mx_handle_close(process), NO_ERROR, "");
     ASSERT_EQ(mx_handle_close(vmar), NO_ERROR, "");
@@ -182,7 +183,8 @@ static bool test_info_task_stats_fails(void) {
     BEGIN_TEST;
     // Spin up a thread.
     mxr_thread_t thread;
-    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)MX_MSEC(100), &thread), "");
+    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)mx_deadline_after(MX_MSEC(100)), &thread),
+                "");
     mx_handle_t thandle = mxr_thread_get_handle(&thread);
     ASSERT_EQ(mx_object_wait_one(thandle,
                                  MX_THREAD_SIGNALED, MX_TIME_INFINITE, NULL),
@@ -212,17 +214,19 @@ static bool test_resume_suspended(void) {
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
 
     // The thread should still be blocked on the event when it wakes up
-    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, MX_MSEC(100), NULL), ERR_TIMED_OUT, "");
+    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, mx_deadline_after(MX_MSEC(100)),
+                                 NULL), ERR_TIMED_OUT, "");
 
     // Check that signaling the event while suspended results in the expected
     // behavior
     ASSERT_EQ(mx_task_suspend(thread_h), NO_ERROR, "");
     // TODO: Use an exception port to wait for the suspend to take effect
-    mx_nanosleep(MX_MSEC(10));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(10)));
 
     ASSERT_EQ(mx_object_signal(event, 0, MX_USER_SIGNAL_0), NO_ERROR, "");
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
-    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, MX_MSEC(100), NULL), NO_ERROR, "");
+    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, mx_deadline_after(MX_MSEC(100)),
+                                 NULL), NO_ERROR, "");
 
     ASSERT_EQ(mx_handle_close(event), NO_ERROR, "");
 
@@ -239,7 +243,7 @@ static bool test_kill_suspended(void) {
     ASSERT_TRUE(start_thread(test_wait_thread_fn, &event, &thread), "");
     mx_handle_t thread_h = mxr_thread_get_handle(&thread);
     ASSERT_EQ(mx_task_suspend(thread_h), NO_ERROR, "");
-    mx_nanosleep(MX_MSEC(10));
+    mx_nanosleep(mx_deadline_after(MX_MSEC(10)));
     ASSERT_EQ(mx_task_kill(thread_h), NO_ERROR, "");
 
     ASSERT_EQ(mx_handle_close(event), NO_ERROR, "");
@@ -250,17 +254,15 @@ static bool test_kill_suspended(void) {
 static bool test_suspend_sleeping(void) {
     BEGIN_TEST;
 
-    mx_time_t now = mx_time_get(MX_CLOCK_MONOTONIC);
-
-    const mx_time_t sleep_time = MX_MSEC(100);
+    const mx_time_t sleep_deadline = mx_deadline_after(MX_MSEC(100));
     mxr_thread_t thread;
 
     // TODO(teisenbe): This code could be made less racy with a deadline sleep
     // mode when we get one.
-    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)sleep_time, &thread), "");
+    ASSERT_TRUE(start_thread(test_sleep_thread_fn, (void*)sleep_deadline, &thread), "");
     mx_handle_t thread_h = mxr_thread_get_handle(&thread);
 
-    mx_nanosleep(sleep_time / 4);
+    mx_nanosleep(sleep_deadline - MX_MSEC(50));
     ASSERT_EQ(mx_task_suspend(thread_h), NO_ERROR, "");
 
     // TODO(teisenbe): Once we wire in exceptions for suspend, check here that
@@ -269,9 +271,10 @@ static bool test_suspend_sleeping(void) {
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
 
     // Wait for the sleep to finish
-    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, 2 * sleep_time, NULL), NO_ERROR, "");
-    mx_time_t elapsed = mx_time_get(MX_CLOCK_MONOTONIC) - now;
-    ASSERT_GE(elapsed, sleep_time, "thread did not sleep long enough");
+    ASSERT_EQ(mx_object_wait_one(thread_h, MX_THREAD_SIGNALED, sleep_deadline + MX_MSEC(50), NULL),
+              NO_ERROR, "");
+    const mx_time_t now = mx_time_get(MX_CLOCK_MONOTONIC);
+    ASSERT_GE(now, sleep_deadline, "thread did not sleep long enough");
 
     END_TEST;
 }
@@ -401,7 +404,7 @@ static bool test_suspend_stops_thread(void) {
     while (arg.v != 2) {
         arg.v = 2;
         // Give the thread a chance to clobber the value
-        mx_nanosleep(MX_MSEC(50));
+        mx_nanosleep(mx_deadline_after(MX_MSEC(50)));
     }
     ASSERT_EQ(mx_task_resume(thread_h, 0), NO_ERROR, "");
     while (arg.v != 1) {