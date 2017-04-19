d1748c8babb901bbf06fe96caf61d72b330d466c
Nelly Vouzoukidou <nellyv@google.com>
2017-04-19 16:36:53 +0200

    Update Ledger API on getting the diff from ConflictResolver.
    After change in https://fuchsia-review.googlesource.com/#/c/26957/
    
    Change-Id: Ib4c972fbadb97fb9c3d305c628507312bd0880cd

--- bin/user_runner/conflict_resolver_impl.cc
+++ bin/user_runner/conflict_resolver_impl.cc
@@ -37,42 +37,76 @@ class ConflictResolverImpl::LogConflictDiffCall : Operation<void> {
   }
 
  private:
-  void Run() override { GetDiff(nullptr); }
+  void Run() override {
+    GetDiff(
+        nullptr, "left",
+        [this](fidl::Array<uint8_t> continuation_token,
+               const std::function<void(ledger::Status, ledger::PageChangePtr,
+                                        fidl::Array<uint8_t>)>& callback) {
+          result_provider_->GetLeftDiff(std::move(continuation_token),
+                                        callback);
+        });
 
-  void GetDiff(fidl::Array<uint8_t> continuation_token) {
-    result_provider_->GetDiff(
+    GetDiff(
+        nullptr, "right",
+        [this](fidl::Array<uint8_t> continuation_token,
+               const std::function<void(ledger::Status, ledger::PageChangePtr,
+                                        fidl::Array<uint8_t>)>& callback) {
+          result_provider_->GetRightDiff(std::move(continuation_token),
+                                         callback);
+        });
+  }
+
+  void GetDiff(
+      fidl::Array<uint8_t> continuation_token,
+      std::string left_or_right,
+      std::function<void(fidl::Array<uint8_t>,
+                         const std::function<void(ledger::Status,
+                                                  ledger::PageChangePtr,
+                                                  fidl::Array<uint8_t>)>&)>
+          get_left_or_right_diff) {
+    get_left_or_right_diff(
         std::move(continuation_token),
-        [this](ledger::Status status, ledger::PageChangePtr change_left,
-               ledger::PageChangePtr change_right,
-               fidl::Array<uint8_t> next_token) {
+        [
+          this, left_or_right = std::move(left_or_right), get_left_or_right_diff
+        ](ledger::Status status, ledger::PageChangePtr change,
+          fidl::Array<uint8_t> next_token) {
           if (status != ledger::Status::OK &&
               status != ledger::Status::PARTIAL_RESULT) {
-            FTL_LOG(INFO) << "MergeResultProvider::GetDiff failed with status "
-                          << status;
+            FTL_LOG(INFO)
+                << "Getting diff from MergeResultProvider failed with status "
+                << status;
             return;
           }
-          for (auto& change : change_left->changes) {
-            FTL_LOG(INFO) << "changed right " << to_string(change->key);
-          }
-
-          for (auto& change : change_right->changes) {
-            FTL_LOG(INFO) << "changed left " << to_string(change->key);
+          for (auto& change : change->changes) {
+            FTL_LOG(INFO) << "changed " << left_or_right << " "
+                          << to_string(change->key);
           }
           if (status == ledger::Status::PARTIAL_RESULT) {
-            GetDiff(std::move(next_token));
+            GetDiff(std::move(next_token), std::move(left_or_right),
+                    std::move(get_left_or_right_diff));
           } else {
-            result_provider_->Done([this](ledger::Status status) {
-              if (status != ledger::Status::OK) {
-                FTL_LOG(INFO) << "MergeResultProvider::Done failed with status "
-                              << status;
-              }
-              Done();
-            });
+            ++finished_;
+            CheckIfDone();
           }
         });
   }
 
+  void CheckIfDone() {
+    if (finished_ != 2) {
+      return;
+    }
+    result_provider_->Done([this](ledger::Status status) {
+      if (status != ledger::Status::OK) {
+        FTL_LOG(INFO) << "MergeResultProvider::Done failed with status "
+                      << status;
+      }
+      Done();
+    });
+  }
+
   ledger::MergeResultProviderPtr result_provider_;
+  uint8_t finished_ = 0;
 
   FTL_DISALLOW_COPY_AND_ASSIGN(LogConflictDiffCall);
 };
