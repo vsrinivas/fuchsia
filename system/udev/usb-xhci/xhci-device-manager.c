wlmgithub/magenta

[udev][usb-xhci] Implement scatter/gather support
The usb-xhci driver is now able to handle non-contiguous data buffers
for USB transactions.

As part of this change, we also clean up how iotxns are queued.
Removed the clunky xhci_process_deferred_txns() mechanism, replacing it with
a pipeline with separate lists for transactions waiting to begin processing
and transactions waiting to be completed.

Change-Id: I614ddc99c2abb0424143677313a8eb2885783b77

system/udev/usb-xhci/xhci-device-manager.c
@@ -310,27 +310,30 @@ static bool xhci_stop_endpoint(xhci_t* xhci, uint32_t slot_id, int ep_index) {
         printf("TRB_CMD_STOP_ENDPOINT failed cc: %d\n", cc);
     }
 
-    list_node_t list;
-    list_initialize(&list);
-    list_node_t* node;
+    list_node_t pending_copy;
+    list_node_t queued_copy;
+    list_initialize(&pending_copy);
+    list_initialize(&queued_copy);
 
     mtx_lock(&ep->lock);
 
-    // copy pending requests to a different list so we can complete them outside of the mutex
-    while ((node = list_remove_head(&ep->pending_requests)) != NULL) {
-        list_add_tail(&list, node);
-    }
+    // move pending_txns and queued_txns to a different list so we can complete them outside of the mutex
+    list_move(&ep->pending_txns, &pending_copy);
+    list_move(&ep->queued_txns, &queued_copy);
+
     ep->enabled = false;
 
     mtx_unlock(&ep->lock);
 
     // complete pending requests
     iotxn_t* txn;
-    while ((txn = list_remove_head_type(&list, iotxn_t, node)) != NULL) {
+    iotxn_t* temp;
+    list_for_every_entry_safe(&pending_copy, txn, temp, iotxn_t, node) {
         iotxn_complete(txn, ERR_REMOTE_CLOSED, 0);
     }
-    // and any deferred requests
-    xhci_process_deferred_txns(xhci, ep, true);
+    list_for_every_entry_safe(&queued_copy, txn, temp, iotxn_t, node) {
+        iotxn_complete(txn, ERR_REMOTE_CLOSED, 0);
+    }
     xhci_transfer_ring_free(transfer_ring);
 
     return true;

vsrinivas/fuchsia

system/udev/usb-xhci/xhci-device-manager.c
@@ -82,6 +82,10 @@ static mx_status_t xhci_address_device(xhci_t* xhci, uint32_t slot_id, uint32_t 
     xhci_endpoint_t* ep = &slot->eps[0];
     status = xhci_endpoint_init(ep, TRANSFER_RING_SIZE);
     if (status < 0) return status;
+    ep->transfer_state = calloc(1, sizeof(xhci_transfer_state_t));
+    if (!ep->transfer_state) {
+        return ERR_NO_MEMORY;
+    }
     xhci_transfer_ring_t* transfer_ring = &ep->transfer_ring;
 
     xhci_input_control_context_t* icc = (xhci_input_control_context_t*)xhci->input_context;
@@ -200,6 +204,9 @@ static void xhci_disable_slot(xhci_t* xhci, uint32_t slot_id) {
 
     xprintf("cleaning up slot %d\n", slot_id);
     xhci_slot_t* slot = &xhci->slots[slot_id];
+    for (int i = 0; i < XHCI_NUM_EPS; i++) {
+        xhci_endpoint_free(&slot->eps[i]);
+    }
     io_buffer_release(&slot->buffer);
     memset(slot, 0, sizeof(*slot));
 }
@@ -308,29 +315,36 @@ static bool xhci_stop_endpoint(xhci_t* xhci, uint32_t slot_id, int ep_index) {
         // TRB_CC_CONTEXT_STATE_ERROR is normal here in the case of a disconnected device,
         // since by then the endpoint would already be in error state.
         printf("TRB_CMD_STOP_ENDPOINT failed cc: %d\n", cc);
+        return false;
     }
 
-    list_node_t list;
-    list_initialize(&list);
-    list_node_t* node;
+    free(ep->transfer_state);
+    ep->transfer_state = NULL;
+
+    list_node_t pending_copy;
+    list_node_t queued_copy;
+    list_initialize(&pending_copy);
+    list_initialize(&queued_copy);
 
     mtx_lock(&ep->lock);
 
-    // copy pending requests to a different list so we can complete them outside of the mutex
-    while ((node = list_remove_head(&ep->pending_requests)) != NULL) {
-        list_add_tail(&list, node);
-    }
+    // move pending_txns and queued_txns to a different list so we can complete them outside of the mutex
+    list_move(&ep->pending_txns, &pending_copy);
+    list_move(&ep->queued_txns, &queued_copy);
+
     ep->enabled = false;
 
     mtx_unlock(&ep->lock);
 
     // complete pending requests
     iotxn_t* txn;
-    while ((txn = list_remove_head_type(&list, iotxn_t, node)) != NULL) {
+    iotxn_t* temp;
+    list_for_every_entry_safe(&pending_copy, txn, temp, iotxn_t, node) {
         iotxn_complete(txn, ERR_PEER_CLOSED, 0);
     }
-    // and any deferred requests
-    xhci_process_deferred_txns(xhci, ep, true);
+    list_for_every_entry_safe(&queued_copy, txn, temp, iotxn_t, node) {
+        iotxn_complete(txn, ERR_PEER_CLOSED, 0);
+    }
     xhci_transfer_ring_free(transfer_ring);
 
     return true;
@@ -552,6 +566,10 @@ mx_status_t xhci_enable_endpoint(xhci_t* xhci, uint32_t slot_id, usb_endpoint_de
 
     // xhci_stop_endpoint() will handle the !enable case
     if (enable) {
+        ep->transfer_state = calloc(1, sizeof(xhci_transfer_state_t));
+        if (!ep->transfer_state) {
+            return ERR_NO_MEMORY;
+        }
         ep->enabled = true;
     }
 