6bb66f5db5f4a68402fc1a7a3eda99d4d92d8def
Adam Barth <abarth@chromium.org>
2017-04-20 16:17:56 -0700

    Add ServiceNamespace
    This class will help us bridge between the ServiceProvider and Directory based
    approaches to discovering services. The next patch will teach
    application_manager to use this functionality.
    
    Change-Id: Iff7cc43cfa8bdb00a009748ec03ea5e601ed0968

---
+++ bin/application/lib/svc/service_namespace.cc
@@ -0,0 +1,92 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "application/lib/svc/service_namespace.h"
+
+#include <mxio/util.h>
+#include <utility>
+
+#include "lib/mtl/vfs/vfs_handler.h"
+
+namespace app {
+
+ServiceNamespace::ServiceNamespace()
+    : directory_(new svcfs::VnodeDir(mtl::VFSHandler::Start)) {
+  directory_->RefAcquire();
+}
+
+ServiceNamespace::ServiceNamespace(
+    fidl::InterfaceRequest<app::ServiceProvider> request)
+    : directory_(new svcfs::VnodeDir(mtl::VFSHandler::Start)) {
+  directory_->RefAcquire();
+  AddBinding(std::move(request));
+}
+
+ServiceNamespace::~ServiceNamespace() {
+  directory_->RemoveAllServices();
+  directory_->RefRelease();
+  directory_ = nullptr;
+}
+
+void ServiceNamespace::AddBinding(
+    fidl::InterfaceRequest<app::ServiceProvider> request) {
+  if (request)
+    bindings_.AddBinding(this, std::move(request));
+}
+
+void ServiceNamespace::Close() {
+  bindings_.CloseAllBindings();
+}
+
+void ServiceNamespace::AddServiceForName(ServiceConnector connector,
+                                         const std::string& service_name) {
+  name_to_service_connector_[service_name] = std::move(connector);
+  directory_->AddService(service_name.data(), service_name.length(), this);
+}
+
+mx::channel ServiceNamespace::CloneDirectory() {
+  mx::channel h1, h2;
+  if (mx::channel::create(0, &h1, &h2) < 0)
+    return mx::channel();
+
+  if (directory_->Open(O_DIRECTORY) < 0)
+    return mx::channel();
+
+  if (directory_->Serve(h1.release(), 0) < 0) {
+    directory_->Close();
+    return mx::channel();
+  }
+
+  return h2;
+}
+
+int ServiceNamespace::OpenAsFileDescriptor() {
+  mx::channel directory = CloneDirectory();
+  if (!directory)
+    return -1;
+  mxio_t* io = mxio_remote_create(directory.get(), MX_HANDLE_INVALID);
+  if (!io)
+    return -1;
+  (void) directory.release(); // The directory is now owned by io.
+  return mxio_bind_to_fd(io, -1, 0);
+}
+
+void ServiceNamespace::Connect(const char* name, size_t len,
+                               mx::channel channel) {
+  ConnectCommon(std::string(name, len), std::move(channel));
+}
+
+void ServiceNamespace::ConnectToService(const fidl::String& service_name,
+                                        mx::channel channel) {
+  ConnectCommon(service_name, std::move(channel));
+}
+
+void ServiceNamespace::ConnectCommon(const std::string& service_name,
+                                     mx::channel channel) {
+  auto it = name_to_service_connector_.find(service_name);
+  if (it != name_to_service_connector_.end())
+    it->second(std::move(channel));
+}
+
+}  // namespace app
