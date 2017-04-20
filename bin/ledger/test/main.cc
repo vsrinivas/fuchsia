18427cec6b2926e438afa74832e818cbfc17a7a5
Brian Goldman <bgoldman@google.com>
2017-04-19 19:59:56 -0700

    Report Ledger test results to TestRunner
    This will allow us to move Ledger to the Fuchsia recipe, which runs
    tests inside QEMU by communicating with TestRunner over TCP, rather than
    by putting the tests in an autorun script and parsing the output with
    regular expressions.
    
    Change-Id: Ic82de58aa0c85e42052c98e96b98ce549ac22828

--- bin/ledger/test/main.cc
+++ bin/ledger/test/main.cc
@@ -4,18 +4,20 @@
 
 #include "application/lib/app/application_context.h"
 #include "apps/tracing/lib/trace/provider.h"
+#include "apps/test_runner/services/test_runner.fidl.h"
 #include "gtest/gtest.h"
 #include "lib/mtl/tasks/message_loop.h"
 #include "lib/mtl/threading/thread.h"
 
-// Thread connecting to the environment to allow tracing tests.
-class TracingThread {
+// Thread connecting to the environment to allow tracing tests and reporting
+// test results to test_runner.
+class EnvironmentThread : public testing::EmptyTestEventListener {
  public:
-  TracingThread() {
+  EnvironmentThread() {
     thread_.Run();
     thread_.TaskRunner()->PostTask([this] { InitOnThread(); });
   }
-  ~TracingThread() {
+  ~EnvironmentThread() {
     thread_.TaskRunner()->PostTask([this] { QuitOnThread(); });
     thread_.Join();
   }
@@ -26,17 +28,45 @@ class TracingThread {
         app::ApplicationContext::CreateFromStartupInfoNotChecked();
     if (application_context_->environment()) {
       tracing::InitializeTracer(application_context_.get(), {"ledger_tests"});
+      InitTestReporting();
     }
   }
+
+  void InitTestReporting() {
+    test_runner_ = application_context_->ConnectToEnvironmentService<
+        test_runner::TestRunner>();
+    test_runner_->Identify("ledger_tests");
+  }
+
   void QuitOnThread() { mtl::MessageLoop::GetCurrent()->PostQuitTask(); }
+
+  // testing::EmptyTestEventListener override.
+  // This gets called when all of the tests are done running
+  void OnTestProgramEnd(const ::testing::UnitTest& test) override {
+    if (!test_runner_) {
+      return;
+    }
+
+    bool failed = test.Failed();
+    thread_.TaskRunner()->PostTask([this, failed] {
+      if (failed) {
+        test_runner_->Fail("Failed");
+      }
+      test_runner_->Teardown();
+    });
+  }
 
   mtl::Thread thread_;
   std::unique_ptr<app::ApplicationContext> application_context_;
+  test_runner::TestRunnerPtr test_runner_;
 };
 
 int main(int argc, char** argv) {
-  TracingThread tracing_thread;
+  EnvironmentThread environment_thread;
 
   testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  testing::UnitTest::GetInstance()->listeners().Append(&environment_thread);
+  int status = RUN_ALL_TESTS();
+  testing::UnitTest::GetInstance()->listeners().Release(&environment_thread);
+  return status;
 }
