7241dabd6a8fd3539e59c42fb827b104ec1aac43
Brian Swetland <swetland@google.com>
2017-04-25 15:46:01 -0700

    Update values for MX_HND_TYPE constants to match Magenta
    These should be renamed to the new PA_ names at some point,
    but I'm not sure if there are dependencies on the old names
    somewhere.
    
    Change-Id: I782ef74d445b6e780b33594d9c29d134d4ecfb61

--- public/rust/crates/mxruntime/mxruntime-sys/src/lib.rs
+++ public/rust/crates/mxruntime/mxruntime-sys/src/lib.rs
@@ -15,9 +15,9 @@ pub const MX_HANDLE_INVALID: mx_handle_t = 0;
 // Startup handle types, derived from magenta/system/public/magenta/processargs.h
 
 // Handle to our own process.
-pub const MX_HND_TYPE_PROC_SELF: u32 = 1;
+pub const MX_HND_TYPE_PROC_SELF: u32 = 0x01;
 // Handle to the initial thread of our own process.
-pub const MX_HND_TYPE_THREAD_SELF: u32 = 2;
+pub const MX_HND_TYPE_THREAD_SELF: u32 = 0x02;
 // Handle to the VMO containing the ELF image of the system vDSO.  This
 // handle is duplicable, transferable, readable, and executable, but not
 // writable.  The contents of the VM object should be treated like any
@@ -26,7 +26,7 @@ pub const MX_HND_TYPE_THREAD_SELF: u32 = 2;
 // might create or propagate it on to its children so they can do so.
 // Each process's own vDSO was mapped in by its creator before the
 // process started, its address passed as an argument to entry point.
-pub const MX_HND_TYPE_VDSO_VMO: u32 = 3;
+pub const MX_HND_TYPE_VDSO_VMO: u32 = 0x11;
 // Handle to the VMO used to map the initial thread's stack.  This
 // handle usually has all rights.  The protocol between process creator
 // and new process is that this entire VM object has been mapped in
@@ -36,36 +36,37 @@ pub const MX_HND_TYPE_VDSO_VMO: u32 = 3;
 // calling convention for function entry.  Thus the new process can
 // compute its exact stack bounds by subtracting the size reported by
 // this VMO from the (adjusted back up) initial SP value.
-pub const MX_HND_TYPE_STACK_VMO: u32 = 4;
+pub const MX_HND_TYPE_STACK_VMO: u32 = 0x13;
 // Handle to a VMO containing a bootfs format image.
 // The "arg" field used with this type is a simple ordinal.
-pub const MX_HND_TYPE_BOOTFS_VMO: u32 = 5;
+pub const MX_HND_TYPE_BOOTFS_VMO: u32 = 0x1B;
 // Handle to a Job object which can be used to make child processes. The
 // Job can be the same as the one used to create this process or it can
 // be different.
-pub const MX_HND_TYPE_JOB: u32 = 6;
+pub const MX_HND_TYPE_JOB: u32 = 0x03;
 // Handle types the mxio library uses
-pub const MX_HND_TYPE_MXIO_ROOT: u32 = 0x10;
-pub const MX_HND_TYPE_MXIO_REMOTE: u32 = 0x11;
-pub const MX_HND_TYPE_MXIO_PIPE: u32 = 0x12;
-pub const MX_HND_TYPE_MXIO_EVENT: u32 = 0x13;
-pub const MX_HND_TYPE_MXIO_LOGGER: u32 = 0x14;
-pub const MX_HND_TYPE_MXIO_CWD: u32 = 0x15;
+pub const MX_HND_TYPE_MXIO_ROOT: u32 = 0x30;
+pub const MX_HND_TYPE_MXIO_CWD: u32 = 0x31;
+pub const MX_HND_TYPE_MXIO_REMOTE: u32 = 0x32;
+pub const MX_HND_TYPE_MXIO_PIPE: u32 = 0x33;
+pub const MX_HND_TYPE_MXIO_EVENT: u32 = 0x34;
+pub const MX_HND_TYPE_MXIO_LOGGER: u32 = 0x35;
+pub const MX_HND_TYPE_MXIO_SOCKET: u32 = 0x36;
 // Handle types used by the device manager and device hosts
-pub const MX_HND_TYPE_RESOURCE: u32 = 0x20;
+pub const MX_HND_TYPE_RESOURCE: u32 = 0x3F;
 // Handle types used by the mojo application model
-pub const MX_HND_TYPE_APPLICATION_REQUEST: u32 = 0x100;
-pub const MX_HND_TYPE_APPLICATION_LAUNCHER: u32 = 0x101;
-pub const MX_HND_TYPE_INCOMING_SERVICES: u32 = 0x102;
-pub const MX_HND_TYPE_OUTGOING_SERVICES: u32 = 0x103;
+pub const MX_HND_TYPE_APPLICATION_REQUEST: u32 = 0x40;
+pub const MX_HND_TYPE_APPLICATION_LAUNCHER: u32 = 0x41;
+pub const MX_HND_TYPE_INCOMING_SERVICES: u32 = 0x42;
+pub const MX_HND_TYPE_OUTGOING_SERVICES: u32 = 0x43;
 // Message pipe for dynamic loader service
-pub const MX_HND_TYPE_LOADER_SVC: u32 = 0x200;
+pub const MX_HND_TYPE_LOADER_SVC: u32 = 0x10;
 // VM object handle for the main executable file
-pub const MX_HND_TYPE_EXEC_VMO: u32 = 0x201;
+pub const MX_HND_TYPE_EXEC_VMO: u32 = 0x14;
 // Handle types for one-off use and prototyping
-pub const MX_HND_TYPE_USER0: u32 = 0xFFF0;
-pub const MX_HND_TYPE_USER1: u32 = 0xFFF1;
-pub const MX_HND_TYPE_USER2: u32 = 0xFFF2;
+pub const MX_HND_TYPE_USER0: u32 = 0xF0;
+pub const MX_HND_TYPE_USER1: u32 = 0xF1;
+pub const MX_HND_TYPE_USER2: u32 = 0xF2;
 
 #[link(name="mxio")]
 extern {
