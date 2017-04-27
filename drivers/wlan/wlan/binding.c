adc6fbd46cb5149306c7ad64549ba3e0e567368b
Mike Voydanoff <voydanoff@google.com>
2017-04-27 07:54:32 -0700

    [wlan] Update driver binding macros
    Change-Id: Iadd393a0222de072f9a33f141a5fe71392bac366

--- drivers/wlan/wlan/binding.c
+++ drivers/wlan/wlan/binding.c
@@ -10,12 +10,10 @@
 
 extern mx_status_t wlan_bind(mx_driver_t* driver, mx_device_t* device, void** cookie);
 
-mx_driver_t _driver_wlan = {
-    .ops = {
-        .bind = wlan_bind,
-    },
+static mx_driver_ops_t wlan_driver_ops = {
+    .bind = wlan_bind,
 };
 
-MAGENTA_DRIVER_BEGIN(_driver_wlan, "wlan", "magenta", "0.1", 1)
+MAGENTA_DRIVER_BEGIN(wlan, wlan_driver_ops, "magenta", "0.1", 1)
     BI_MATCH_IF(EQ, BIND_PROTOCOL, MX_PROTOCOL_WLANMAC),
-MAGENTA_DRIVER_END(_driver_wlan)
+MAGENTA_DRIVER_END(wlan)
