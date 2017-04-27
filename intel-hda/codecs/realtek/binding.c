184722b7823622c79c1de82651cd35f12691633d
Mike Voydanoff <voydanoff@google.com>
2017-04-27 07:52:20 -0700

    [intel-hda] Update driver binding macros
    Change-Id: Ie1b6d94d2e7182ae6bf0c088373a8458dec37701

--- intel-hda/codecs/realtek/binding.c
+++ intel-hda/codecs/realtek/binding.c
@@ -10,18 +10,16 @@
 extern mx_status_t realtek_ihda_codec_bind_hook(mx_driver_t*, mx_device_t*, void**);
 extern void        realtek_ihda_codec_unbind_hook(mx_driver_t*, mx_device_t*, void*);
 
-mx_driver_t _driver_realtek_ihda_codec = {
-    .ops = {
-        .init    = NULL,
-        .bind    = realtek_ihda_codec_bind_hook,
-        .unbind  = realtek_ihda_codec_unbind_hook,
-        .release = NULL,
-    },
+static mx_driver_ops_t realtek_ihda_codec_driver_ops = {
+    .init    = NULL,
+    .bind    = realtek_ihda_codec_bind_hook,
+    .unbind  = realtek_ihda_codec_unbind_hook,
+    .release = NULL,
 };
 
-MAGENTA_DRIVER_BEGIN(_driver_realtek_ihda_codec, "realtek-ihda-codec", "magenta", "0.1", 4)
+MAGENTA_DRIVER_BEGIN(realtek_ihda_codec, realtek_ihda_codec_driver_ops, "magenta", "0.1", 4)
     BI_ABORT_IF(NE, BIND_PROTOCOL, MX_PROTOCOL_IHDA_CODEC),
     BI_ABORT_IF(NE, BIND_IHDA_CODEC_VID, 0x10ec),   // Realtek
     BI_MATCH_IF(EQ, BIND_IHDA_CODEC_DID, 0x0255),   // ALC255
     BI_MATCH_IF(EQ, BIND_IHDA_CODEC_DID, 0x0283),   // ALC283
-MAGENTA_DRIVER_END(_driver_realtek_ihda_codec)
+MAGENTA_DRIVER_END(realtek_ihda_codec)
--- intel-hda/controller/binding.c
+++ intel-hda/controller/binding.c
@@ -12,16 +12,14 @@ extern mx_status_t ihda_bind_hook(mx_driver_t*, mx_device_t*, void**);
 extern void        ihda_unbind_hook(mx_driver_t*, mx_device_t*, void*);
 extern mx_status_t ihda_release_hook(mx_driver_t*);
 
-mx_driver_t _driver_intel_hda = {
-    .ops = {
-        .init = ihda_init_hook,
-        .bind = ihda_bind_hook,
-        .unbind = ihda_unbind_hook,
-        .release = ihda_release_hook,
-    },
+static mx_driver_ops_t intel_hda_driver_ops = {
+    .init = ihda_init_hook,
+    .bind = ihda_bind_hook,
+    .unbind = ihda_unbind_hook,
+    .release = ihda_release_hook,
 };
 
-MAGENTA_DRIVER_BEGIN(_driver_intel_hda, "intel-hda", "magenta", "0.1", 8)
+MAGENTA_DRIVER_BEGIN(intel_hda, intel_hda_driver_ops, "magenta", "0.1", 8)
     BI_ABORT_IF(NE, BIND_PROTOCOL, MX_PROTOCOL_PCI),
     BI_ABORT_IF(NE, BIND_PCI_VID, 0x8086),
     BI_MATCH_IF(EQ, BIND_PCI_DID, 0x2668), // Standard (Spec Rev 1.0a; 6/17/2010)
@@ -30,4 +28,4 @@ MAGENTA_DRIVER_BEGIN(_driver_intel_hda, "intel-hda", "magenta", "0.1", 8)
     BI_MATCH_IF(EQ, BIND_PCI_DID, 0xA1F0), // Intel 200/C400 PCH Spec
     BI_MATCH_IF(EQ, BIND_PCI_DID, 0x9D70), // Intel 6th Gen (Skylake) PCH-U/Y I/O Datasheet
     BI_MATCH_IF(EQ, BIND_PCI_DID, 0x9D71), // Intel 7th Gen (Skylake) PCH-U/Y I/O Datasheet
-MAGENTA_DRIVER_END(_driver_intel_hda)
+MAGENTA_DRIVER_END(intel_hda)
