33fa57ef72a6f4a9908c46e8183fea4854a6bbbc
Andrew Wilson <apwilson@google.com>
2017-04-27 16:46:05 -0700

    Handle null MediaQuery.
    Change-Id: Iccb4fb0e054ac2b46c352cb063df833a059adf35

--- bin/ui/lib/flutter/child_view.dart
+++ bin/ui/lib/flutter/child_view.dart
@@ -100,7 +100,8 @@ class ChildViewConnection {
     try {
       launcher.createApplication(launchInfo, controller);
       return new ChildViewConnection.connect(services,
-          childServices: childServices, onAvailable: onAvailable,
+          childServices: childServices,
+          onAvailable: onAvailable,
           onUnavailable: onUnavailable);
     } finally {
       services.ctrl.close();
@@ -126,7 +127,6 @@ class ChildViewConnection {
   final ChildViewConnectionCallback _onAvailableCallback;
   final ChildViewConnectionCallback _onUnavailableCallback;
   InterfaceHandle<ViewOwner> _viewOwner;
-
 
   static int _nextViewKey = 1;
   int _viewKey;
@@ -204,8 +204,13 @@ class ChildViewConnection {
   }
 
   ViewProperties _createViewProperties(
-      int physicalWidth, int physicalHeight, double devicePixelRatio,
-      int insetTop, int insetRight, int insetBottom, int insetLeft) {
+      int physicalWidth,
+      int physicalHeight,
+      double devicePixelRatio,
+      int insetTop,
+      int insetRight,
+      int insetBottom,
+      int insetLeft) {
     if (_currentViewProperties != null &&
         _currentViewProperties.displayMetrics.devicePixelRatio ==
             devicePixelRatio &&
@@ -214,8 +219,7 @@ class ChildViewConnection {
         _currentViewProperties.viewLayout.inset.top == insetTop &&
         _currentViewProperties.viewLayout.inset.right == insetRight &&
         _currentViewProperties.viewLayout.inset.bottom == insetBottom &&
-        _currentViewProperties.viewLayout.inset.left == insetLeft)
-      return null;
+        _currentViewProperties.viewLayout.inset.left == insetLeft) return null;
 
     DisplayMetrics displayMetrics = new DisplayMetrics()
       ..devicePixelRatio = devicePixelRatio;
@@ -237,15 +241,25 @@ class ChildViewConnection {
   }
 
   void _setChildProperties(
-      int physicalWidth, int physicalHeight, double devicePixelRatio,
-      int insetTop, int insetRight, int insetBottom, int insetLeft) {
+      int physicalWidth,
+      int physicalHeight,
+      double devicePixelRatio,
+      int insetTop,
+      int insetRight,
+      int insetBottom,
+      int insetLeft) {
     assert(_attached);
     assert(_attachments == 1);
     assert(_viewKey != null);
     if (_viewContainer == null) return;
-    ViewProperties viewProperties =
-        _createViewProperties(physicalWidth, physicalHeight, devicePixelRatio,
-        insetTop, insetRight, insetBottom, insetLeft);
+    ViewProperties viewProperties = _createViewProperties(
+        physicalWidth,
+        physicalHeight,
+        devicePixelRatio,
+        insetTop,
+        insetRight,
+        insetBottom,
+        insetLeft);
     if (viewProperties == null) return;
     _viewContainer.setChildProperties(
         _viewKey, _sceneVersion++, viewProperties);
@@ -345,8 +359,8 @@ class _RenderChildView extends RenderBox {
     if (_connection != null) {
       _physicalWidth = (size.width * scale).round();
       _physicalHeight = (size.height * scale).round();
-      _connection._setChildProperties(_physicalWidth, _physicalHeight, scale,
-        0, 0, 0, 0);
+      _connection._setChildProperties(
+          _physicalWidth, _physicalHeight, scale, 0, 0, 0, 0);
       assert(() {
         if (_viewContainer == null) {
           _debugErrorMessage ??= new TextPainter(
@@ -472,7 +486,8 @@ class ChildView extends LeafRenderObjectWidget {
   _RenderChildView createRenderObject(BuildContext context) {
     return new _RenderChildView(
       connection: connection,
-      scale: MediaQuery.of(context).devicePixelRatio,
+      scale:
+          (MediaQuery.of(context) ?? const MediaQueryData()).devicePixelRatio,
       hitTestable: hitTestable,
     );
   }
@@ -481,7 +496,8 @@ class ChildView extends LeafRenderObjectWidget {
   void updateRenderObject(BuildContext context, _RenderChildView renderObject) {
     renderObject
       ..connection = connection
-      ..scale = MediaQuery.of(context).devicePixelRatio
+      ..scale =
+          (MediaQuery.of(context) ?? const MediaQueryData()).devicePixelRatio
       ..hitTestable = hitTestable;
   }
 }
