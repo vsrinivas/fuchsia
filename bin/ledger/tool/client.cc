93b2e2f522718da7767afe77ac2ad379f83107e7
Etienne Membrives <etiennej@google.com>
2017-04-28 17:52:04 +0200

    Fix hex computation in ledger_tool
    Change-Id: I29a76e022c70cb252fa9aab44d74583a09ce8af1

--- bin/ledger/tool/client.cc
+++ bin/ledger/tool/client.cc
@@ -51,8 +51,8 @@ std::string ToHexString(ftl::StringView data) {
   std::string ret;
   ret.reserve(data.size() * 2);
   for (size_t i = 0; i < data.size(); ++i) {
-    ret.push_back(kHexadecimalCharacters[data[i] >> 4]);
-    ret.push_back(kHexadecimalCharacters[data[i] & 0xf]);
+    ret.push_back(kHexadecimalCharacters[static_cast<uint8_t>(data[i]) >> 4]);
+    ret.push_back(kHexadecimalCharacters[static_cast<uint8_t>(data[i]) & 0xf]);
   }
   return ret;
 }
