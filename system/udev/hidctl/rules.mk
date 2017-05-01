ebbdee473652aeda9a4b2f749692f9246b28384c
Tim Kilbourn <tkilbourn@google.com>
2017-05-01 14:06:35 -0700

    [hidctl] Disable hidctl driver for now
    Needs to be updated to the MX_PROTOCOL_HIDBUS protocol, which can wait
    since I don't know that anyone is using this driver (yet).
    
    Change-Id: Icff9089ccddc35135fb609ff026863513503cafd

--- system/udev/hidctl/rules.mk
+++ system/udev/hidctl/rules.mk
@@ -2,17 +2,26 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-LOCAL_DIR := $(GET_LOCAL_DIR)
+################################################################################
+#
+# TODO(tkilbourn)
+# This driver is disabled until we can update it to use the new
+# MX_PROTOCOL_HIDBUS protocol, which requires rationalizing all of the device
+# lifecycle events.
+#
+################################################################################
 
-MODULE := $(LOCAL_DIR)
-
-MODULE_TYPE := driver
-
-MODULE_SRCS := $(LOCAL_DIR)/hidctl.c
-
-MODULE_STATIC_LIBS := system/ulib/ddk
-
-MODULE_LIBS := system/ulib/driver system/ulib/magenta system/ulib/c
-
-include make/module.mk
+#LOCAL_DIR := $(GET_LOCAL_DIR)
+#
+#MODULE := $(LOCAL_DIR)
+#
+#MODULE_TYPE := driver
+#
+#MODULE_SRCS := $(LOCAL_DIR)/hidctl.c
+#
+#MODULE_STATIC_LIBS := system/ulib/ddk
+#
+#MODULE_LIBS := system/ulib/driver system/ulib/magenta system/ulib/c
+#
+#include make/module.mk
 
