d0c39343e7c5ca33c07ace8910dc034b2f388fd8
P.Y. Laligand <pylaligand@google.com>
2017-05-02 14:26:32 -0700

    Run Dart analysis on packages.
    This is currently controlled by a GN argument, and disabled by default.
    
    TO-234
    
    Change-Id: Idc7d1030c0f607ca12b2dfb85198f963ca7a80cd

---
+++ dart/run_analysis.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import subprocess
+import sys
+
+def main():
+    parser = argparse.ArgumentParser('Runs analysis on a given package')
+    parser.add_argument('--source-dir', help='Path to package source',
+                        required=True)
+    parser.add_argument('--dot-packages', help='Path to the .packages file',
+                        required=True)
+    parser.add_argument('--dartanalyzer',
+                        help='Path to the Dart analyzer executable',
+                        required=True)
+    parser.add_argument('--dart-sdk', help='Path to the Dart SDK',
+                        required=True)
+    parser.add_argument('--options', help='Path to analysis options',
+                        required=True)
+    parser.add_argument('--stamp', help='File to touch when analysis succeeds',
+                        required=True)
+    args = parser.parse_args()
+
+    call_args = [
+        args.dartanalyzer,
+        "--packages=%s" % args.dot_packages,
+        "--dart-sdk=%s" % args.dart_sdk,
+        "--options=%s" % args.options,
+        args.source_dir,
+        "--fatal-warnings",
+        "--fatal-hints",
+        "--fatal-lints",
+    ]
+
+    call = subprocess.Popen(call_args, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout, stderr = call.communicate()
+    # TODO(pylaligand): only print output if return code is non-zero once
+    # https://github.com/dart-lang/sdk/issues/29498 is fixed.
+    print(stdout + stderr)
+    with open(args.stamp, 'w') as stamp:
+        stamp.write('Success!')
+
+    # TODO(pylaligand): return `call.returncode` once most of the codebase has
+    # been cleaned up.
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
