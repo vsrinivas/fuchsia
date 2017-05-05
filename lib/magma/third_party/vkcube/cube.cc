50a18ceda974223bec80f596ce33e9d3b4aca3a9
Michael Jurka <mikejurka@google.com>
2017-05-05 14:38:42 -0700

    Changes to work with updated vulkan loader
    Change-Id: I42d59cfe8f4dadcf4605dd10e5706e1608945455

--- lib/magma/third_party/vkcube/cube.cc
+++ lib/magma/third_party/vkcube/cube.cc
@@ -2297,9 +2297,8 @@ static void demo_init_vk(struct demo *demo) {
 
     const char *instance_validation_layers_alt2[] = {
         "VK_LAYER_GOOGLE_threading",       "VK_LAYER_LUNARG_parameter_validation",
-        "VK_LAYER_LUNARG_object_tracker",  "VK_LAYER_LUNARG_image",
-        "VK_LAYER_LUNARG_core_validation", "VK_LAYER_LUNARG_swapchain",
-        "VK_LAYER_GOOGLE_unique_objects"
+        "VK_LAYER_LUNARG_object_tracker",  "VK_LAYER_LUNARG_core_validation",
+        "VK_LAYER_LUNARG_swapchain",       "VK_LAYER_GOOGLE_unique_objects"
     };
 
     /* Look for validation layers */
