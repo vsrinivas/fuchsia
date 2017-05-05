b17747e4df486e4c0444a3b55d436ef4147ae473
Craig Stout <cstout@google.com>
2017-05-05 10:52:03 -0700

    Add support for intel skull canyon nuc
    Change-Id: Id0a3e8865955a7ce8d0c528b5775546e19eedee1

--- lib/magma/tests/helper/platform_device_helper.h
+++ lib/magma/tests/helper/platform_device_helper.h
@@ -22,6 +22,7 @@ public:
         switch (device_id) {
             case 0x1916: // Intel(R) HD Graphics 520 (Skylake GT2)
             case 0x5916: // Intel(R) HD Graphics 620 (Kabylake GT2)
+            case 0x193b: // Intel(R) Iris Pro Graphics 580 (Skylake GT4e)
                 return true;
         }
         return false;
