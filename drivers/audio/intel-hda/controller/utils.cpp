76c009f1a187a3c9d18a2bcbeae8a1dde81fbdff
Brian Swetland <swetland@google.com>
2017-05-10 13:38:38 -0700

    [intel-hda] temporarily continue to access dev->props directly
    This part of mx_device_t is ddk internal and will be going away
    in the near future, but for now cheat and define DDK_INTERNAL
    so we don't break.
    
    Change-Id: I842ff6b410620bb8069aa00b85da9aae5262272b

--- drivers/audio/intel-hda/controller/utils.cpp
+++ drivers/audio/intel-hda/controller/utils.cpp
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//TODO: stop using dev->props and dev->prop_count
+#define DDK_INTERNAL
+
 #include <magenta/assert.h>
 #include <ddk/binding.h>
 #include <magenta/process.h>
