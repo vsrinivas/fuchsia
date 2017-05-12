1d33b2095a5caf0168e446e8b2d2bca66e7d3ca3
Jérôme Poichet <jpoichet@google.com>
2017-05-12 15:58:56 -0700

    [mozart] Making IME slightly better
    Handle selection and arrows
    
    Change-Id: I37aadabb6ac91a16c287bf386a7776f8b2e8b193

--- bin/ui/src/ime/ime_impl.cc
+++ bin/ui/src/ime/ime_impl.cc
@@ -54,42 +54,81 @@ void ImeImpl::InjectInput(mozart::InputEventPtr event) {
   if (keyboard->phase == mozart::KeyboardEvent::Phase::PRESSED ||
       keyboard->phase == mozart::KeyboardEvent::Phase::REPEAT) {
     if (keyboard->code_point) {
-      FTL_VLOG(1) << "Appending character (revision = " << state_->revision
-                  << ", text='" << state_->text << "')";
+      FTL_VLOG(1) << "Appending character (state = " << *state_ << "')";
       state_->revision++;
-      state_->composing->start = 0;
-      state_->composing->end = 0;
       std::string text = state_->text.To<std::string>();
       // FIXME (jpoichet) Actually handle UTF8 here
-      text += static_cast<char>(keyboard->code_point);
+      std::string replacement = "";
+      replacement += static_cast<char>(keyboard->code_point);
+      int64_t base = state_->selection->base;
+      base = base == -1 ? 0 : base;
+      int64_t extent = state_->selection->extent;
+      extent = extent == -1 ? 0 : extent;
+      text.replace(base, extent - base, replacement);
       state_->text = fidl::String(text);
-      // FIXME(jpoichet) selection should be taken into account to know where
-      // to insert the character and updated accordingly after insert.
-      state_->selection->base = text.length();
-      state_->selection->extent = text.length();
-      state_->selection->affinity = mozart::TextAffinity::DOWNSTREAM;
+      state_->selection->base = state_->selection->base + replacement.length();
+      state_->selection->extent = state_->selection->base;
 
-      FTL_VLOG(1) << "Notifying (revision = " << state_->revision << ", text='"
-                  << state_->text << "')";
-
+      FTL_VLOG(1) << "Notifying (state = " << *state_ << "')";
       client_->DidUpdateState(state_.Clone(), std::move(event));
-    } else if (keyboard->hid_usage == HID_USAGE_KEY_BACKSPACE) {
-      FTL_VLOG(1) << "Deleting character (revision = " << state_->revision
-                  << ", text='" << state_->text << "')";
-      state_->revision++;
-      state_->composing->start = 0;
-      state_->composing->end = 0;
-      std::string text = state_->text.To<std::string>();
-      text.pop_back();
-      state_->text = fidl::String(text);
-      state_->selection->base = text.length();
-      state_->selection->extent = text.length();
-      state_->selection->affinity = mozart::TextAffinity::DOWNSTREAM;
+    } else {
+      switch (keyboard->hid_usage) {
+        case HID_USAGE_KEY_BACKSPACE: {
+          FTL_VLOG(1) << "Deleting character (state = " << *state_ << "')";
+          std::string text = state_->text.To<std::string>();
+          int64_t base = state_->selection->base;
+          int64_t extent = state_->selection->extent;
+          if (base == -1 && extent == -1) {
+            base = state_->text.size() - 1;
+            extent = base + 1;
+          } else if (base == extent) {
+            if (base == 0) {
+              extent++;
+            } else {
+              base--;
+            }
+          }
+          state_->revision++;
+          text.erase(base, extent - base);
+          state_->text = fidl::String(text);
+          state_->selection->base = base;
+          state_->selection->extent = state_->selection->base;
 
-      FTL_VLOG(1) << "Notifying (revision = " << state_->revision << ", text='"
-                  << state_->text << "')";
+          FTL_VLOG(1) << "Notifying (state = " << *state_ << "')";
+          client_->DidUpdateState(state_.Clone(), std::move(event));
+        } break;
+        case HID_USAGE_KEY_LEFT: {
+          FTL_VLOG(1) << "Moving left (state = " << *state_ << "')";
+          state_->revision++;
+          // TODO(jpoichet) actually pay attention to affinity
+          state_->selection->base = state_->selection->base > 0
+                                        ? state_->selection->base - 1
+                                        : state_->selection->base;
+          if (keyboard->modifiers & mozart::kModifierShift) {
+          } else {
+            state_->selection->extent = state_->selection->base;
+          }
 
-      client_->DidUpdateState(state_.Clone(), std::move(event));
+          FTL_VLOG(1) << "Notifying (state = " << *state_ << "')";
+          client_->DidUpdateState(state_.Clone(), std::move(event));
+        } break;
+        case HID_USAGE_KEY_RIGHT: {
+          FTL_VLOG(1) << "Moving right (state = " << *state_ << "')";
+          state_->revision++;
+          // TODO(jpoichet) actually pay attention to affinity
+          state_->selection->extent =
+              (unsigned)state_->selection->extent < state_->text.size()
+                  ? state_->selection->extent + 1
+                  : state_->selection->extent;
+          if (keyboard->modifiers & mozart::kModifierShift) {
+          } else {
+            state_->selection->base = state_->selection->extent;
+          }
+
+          FTL_VLOG(1) << "Notifying (state = " << *state_ << "')";
+          client_->DidUpdateState(state_.Clone(), std::move(event));
+        } break;
+      }
     }
   }
 }
