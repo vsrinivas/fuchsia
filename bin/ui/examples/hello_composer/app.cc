692ea4da54335646cf7a604b5b234a8b8aa8f756
Josh Gargus <jjosh@google.com>
2017-05-12 16:55:41 -0700

    [mozart] Composer v2 demo uses placeholder Escher rendering.
    The client app now creates a number of Sessions, deletes them
    one by one, and quits.  The server app renders a grid of circles
    using Escher, one for each active Session.
    
    The next step is to actually render the contents of the Sessions.
    
    Change-Id: I5eff134b387b59fc6d535c45eb6bb265d11630b7

--- bin/ui/examples/hello_composer/app.cc
+++ bin/ui/examples/hello_composer/app.cc
@@ -7,6 +7,7 @@
 #include "apps/mozart/services/composition2/composer.fidl.h"
 #include "apps/mozart/services/composition2/session.fidl.h"
 #include "lib/ftl/command_line.h"
+#include "lib/ftl/functional/make_copyable.h"
 #include "lib/ftl/log_settings.h"
 #include "lib/ftl/logging.h"
 #include "lib/mtl/tasks/message_loop.h"
@@ -17,36 +18,54 @@ class HelloComposerApp {
       : application_context_(app::ApplicationContext::CreateFromStartupInfo()),
         loop_(mtl::MessageLoop::GetCurrent()) {
     // Launch composer.
-    app::ServiceProviderPtr services;
+
     auto launch_info = app::ApplicationLaunchInfo::New();
     launch_info->url = "file://system/apps/hello_composer_service";
-    launch_info->services = services.NewRequest();
-    app::ApplicationControllerPtr controller;
+    launch_info->services = services_.NewRequest();
     application_context_->launcher()->CreateApplication(
-        std::move(launch_info), controller.NewRequest());
-    controller.set_connection_error_handler([this] {
+        std::move(launch_info), controller_.NewRequest());
+    controller_.set_connection_error_handler([this] {
       FTL_LOG(INFO) << "Hello Composer service terminated.";
-      loop_->PostQuitTask();
+      loop_->QuitNow();
     });
 
     // Create the composer.
-    app::ConnectToService(services.get(), composer_.NewRequest());
-
-    // Get a session
-    composer_->CreateSession(session_.NewRequest());
-
-    session_.set_connection_error_handler([this] {
-      FTL_LOG(INFO) << "Session terminated.";
-      loop_->PostQuitTask();
-    });
+    app::ConnectToService(services_.get(), composer_.NewRequest());
   }
 
-  void Update() { composer_ = nullptr; }
+  void Update() {
+    // Create a number of sessions, each of which will die after a fixed time.
+
+    // Number of sessions to create.
+    constexpr int kSessionCount = 16;
+    // Number of milliseconds between creation of each session.
+    constexpr int kSessionCreationInterval = 500;
+    // Number of seconds before a session is closed.
+    constexpr int kSessionDuration = 10;
+
+    for (int i = 0; i < kSessionCount; ++i) {
+      loop_->task_runner()->PostDelayedTask(
+          [this]() {
+            FTL_LOG(INFO) << "Creating new Session";
+            mozart2::SessionPtr session;
+            composer_->CreateSession(session.NewRequest());
+            loop_->task_runner()->PostDelayedTask(
+                ftl::MakeCopyable([session = std::move(session)]() {
+                  // Allow SessionPtr to go out of scope, thus closing the
+                  // session.
+                  FTL_LOG(INFO) << "Closing session.";
+                }),
+                ftl::TimeDelta::FromSeconds(kSessionDuration));
+          },
+          ftl::TimeDelta::FromMilliseconds(kSessionCreationInterval * i));
+    }
+  }
 
  private:
   std::unique_ptr<app::ApplicationContext> application_context_;
+  app::ApplicationControllerPtr controller_;
+  app::ServiceProviderPtr services_;
   mozart2::ComposerPtr composer_;
-  mozart2::SessionPtr session_;
   mtl::MessageLoop* loop_;
 };
 
@@ -57,7 +76,14 @@ int main(int argc, const char** argv) {
 
   mtl::MessageLoop loop;
   HelloComposerApp app;
-  loop.task_runner()->PostTask([&app] { app.Update(); });
+  loop.task_runner()->PostDelayedTask([&app] { app.Update(); },
+                                      ftl::TimeDelta::FromSeconds(5));
+  loop.task_runner()->PostDelayedTask(
+      [&loop] {
+        FTL_LOG(INFO) << "Quitting.";
+        loop.QuitNow();
+      },
+      ftl::TimeDelta::FromSeconds(25));
   loop.Run();
   return 0;
 }
--- bin/ui/examples/hello_composer/service.cc
+++ bin/ui/examples/hello_composer/service.cc
@@ -2,11 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cmath>
 #include <memory>
+
+// TODO: why do we need to include this first?
+// It is included by "escher/geometry/types.h" ?!?
+#include <glm/glm.hpp>
 
 #include "application/lib/app/application_context.h"
 #include "apps/mozart/services/composition2/composer.fidl.h"
 #include "apps/mozart/src/composer/composer_impl.h"
+#include "escher/escher.h"
+#include "escher/escher_process_init.h"
+#include "escher/geometry/types.h"
+#include "escher/material/material.h"
+#include "escher/renderer/paper_renderer.h"
+#include "escher/scene/model.h"
+#include "escher/scene/stage.h"
+#include "escher/vk/vulkan_swapchain_helper.h"
+#include "lib/escher/examples/common/demo.h"
+#include "lib/escher/examples/common/demo_harness_fuchsia.h"
 #include "lib/fidl/cpp/bindings/binding_set.h"
 #include "lib/ftl/command_line.h"
 #include "lib/ftl/log_settings.h"
@@ -14,30 +29,115 @@
 #include "lib/ftl/macros.h"
 #include "lib/mtl/tasks/message_loop.h"
 
+static constexpr uint32_t kScreenWidth = 2160;
+static constexpr uint32_t kScreenHeight = 1440;
+
+// Material design places objects from 0.0f to 24.0f.
+static constexpr float kNear = 24.f;
+static constexpr float kFar = 0.f;
+
 namespace mozart {
 namespace composer {
 
 class ComposerImpl;
 
-class HelloComposerService {
+class HelloComposerService : public Demo {
  public:
-  explicit HelloComposerService()
-      : application_context_(app::ApplicationContext::CreateFromStartupInfo()) {
+  explicit HelloComposerService(DemoHarnessFuchsia* harness)
+      : Demo(harness),
+        application_context_(harness->application_context()),
+        renderer_(escher()->NewPaperRenderer()),
+        swapchain_helper_(harness->GetVulkanSwapchain(), renderer_),
+        composer_(std::make_unique<ComposerImpl>()),
+        binding_(composer_.get()) {
     FTL_DCHECK(application_context_);
 
-    application_context_->outgoing_services()->AddService<mozart2::Composer>(
-        [this](fidl::InterfaceRequest<mozart2::Composer> request) {
-          composer_bindings_.AddBinding(std::make_unique<ComposerImpl>(),
-                                        std::move(request));
-        });
+    AddOutgoingServices();
+    InitializeEscherStage();
   }
-  ~HelloComposerService(){};
+
+  ~HelloComposerService() {
+    FTL_LOG(INFO) << "HelloComposerService: shutting down";
+  }
+
+  void DrawFrame() override {
+    size_t session_count = composer_->GetSessionCount();
+    if (session_count == last_session_count_) {
+      return;
+    }
+    last_session_count_ = session_count;
+
+    // Create a grid of circles, where each circle represents a Session in
+    // the Composer.
+    std::vector<escher::Object> objects;
+    size_t grid_size =
+        static_cast<size_t>(ceil(sqrt(static_cast<double>(session_count))));
+    float grid_cell_width = static_cast<float>(kScreenWidth) / grid_size;
+    float grid_cell_height = static_cast<float>(kScreenHeight) / grid_size;
+
+    auto background_material = ftl::MakeRefCounted<escher::Material>();
+    background_material->set_color(escher::vec3(0.8f, 0.8f, 0.8f));
+    objects.push_back(escher::Object::NewRect(
+        escher::vec2(0.f, 0.f), escher::vec2(kScreenWidth, kScreenHeight), 0.f,
+        background_material));
+
+    auto circle_material = ftl::MakeRefCounted<escher::Material>();
+    circle_material->set_color(
+        escher::vec3(63.f / 255.f, 138.f / 255.f, 153.f / 255.f));
+
+    for (size_t y = 0; y < grid_size; ++y) {
+      for (size_t x = 0; x < grid_size; ++x) {
+        if (session_count > y * grid_size + x) {
+          escher::vec2 center(
+              (0.5f + static_cast<float>(x)) * grid_cell_width,
+              (0.5f + static_cast<float>(y)) * grid_cell_height);
+          objects.push_back(escher::Object::NewCircle(
+              center, grid_cell_height * 0.4f, 10.f, circle_material));
+        }
+      }
+    }
+
+    escher::Model model(std::move(objects));
+    swapchain_helper_.DrawFrame(stage_, model);
+  }
 
  private:
-  std::unique_ptr<app::ApplicationContext> application_context_;
+  void AddOutgoingServices() {
+    application_context_->outgoing_services()->AddService<mozart2::Composer>(
+        [this](fidl::InterfaceRequest<mozart2::Composer> request) {
+          if (binding_.is_bound()) {
+            FTL_LOG(WARNING)
+                << "HelloComposerService: composer already bound to client";
+          } else {
+            FTL_LOG(INFO) << "HelloComposerService: binding client to composer";
+            binding_.Bind(std::move(request));
+            binding_.set_connection_error_handler([this] {
+              FTL_LOG(INFO)
+                  << "HelloComposerService: connection to client was closed";
+              composer_.reset();
+              mtl::MessageLoop::GetCurrent()->QuitNow();
+            });
+          }
+        });
+  }
 
-  fidl::BindingSet<mozart2::Composer, std::unique_ptr<ComposerImpl>>
-      composer_bindings_;
+  void InitializeEscherStage() {
+    stage_.Resize(escher::SizeI(kScreenWidth, kScreenHeight), 1.0,
+                  escher::SizeI(0, 0));
+    stage_.set_viewing_volume(
+        escher::ViewingVolume(kScreenWidth, kScreenHeight, kNear, kFar));
+    stage_.set_key_light(escher::DirectionalLight(
+        escher::vec2(1.5f * M_PI, 1.5f * M_PI), 0.15f * M_PI, 0.7f));
+    stage_.set_fill_light(escher::AmbientLight(0.3f));
+  }
+
+  app::ApplicationContext* const application_context_;
+  escher::PaperRendererPtr renderer_;
+  escher::VulkanSwapchainHelper swapchain_helper_;
+  escher::Stage stage_;
+  std::unique_ptr<ComposerImpl> composer_;
+  fidl::Binding<mozart2::Composer> binding_;
+  size_t last_session_count_ = 987654321;
 
   FTL_DISALLOW_COPY_AND_ASSIGN(HelloComposerService);
 };
@@ -46,13 +146,22 @@ class HelloComposerService {
 }  // namespace mozart
 
 int main(int argc, const char** argv) {
+  FTL_LOG(INFO) << "HelloComposerService: entering main()";
+
   auto command_line = ftl::CommandLineFromArgcArgv(argc, argv);
   if (!ftl::SetLogSettingsFromCommandLine(command_line))
     return 1;
 
-  mtl::MessageLoop loop;
-  mozart::composer::HelloComposerService composer_app;
+  auto harness = DemoHarness::New(
+      DemoHarness::WindowParams{"Mozart Composer Example", kScreenWidth,
+                                kScreenHeight, 2, false},
+      DemoHarness::InstanceParams());
 
-  loop.Run();
+  {
+    mozart::composer::HelloComposerService composer_app(
+        static_cast<DemoHarnessFuchsia*>(harness.get()));
+    harness->Run(&composer_app);
+  }
+  harness->Shutdown();
   return 0;
 }
