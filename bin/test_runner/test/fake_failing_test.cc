4df624601b1033a303b6b86e4f2b08e5dfd9d153
Brian Goldman <bgoldman@google.com>
2017-05-17 12:43:25 -0700

    Fake tests for developing Test Runner
    Change-Id: Iaf74439bbdebc868ff407a158c495a4701571f64

---
+++ bin/test_runner/test/fake_failing_test.cc
@@ -0,0 +1,12 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gtest/gtest.h"
+
+// Only one of the two tests will fail.
+TEST(FakeFailingTest, Pass) {}
+
+TEST(FakeFailingTest, Fail) {
+  ASSERT_EQ(0, 1);
+}
