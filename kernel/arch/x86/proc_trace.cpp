97439fee729e14a01edbcc137f401b061cc5f7ae
Doug Evans <dje@google.com>
2017-05-23 11:35:14 -0700

    [kernel][x86] Add cpu info to trace output
    Needed by trace reader.
    
    Change-Id: I9c988f5cdae805176d28fbe7265359adbbb21e7b

--- kernel/arch/x86/proc_trace.cpp
+++ kernel/arch/x86/proc_trace.cpp
@@ -266,6 +266,12 @@ status_t x86_ipt_cpu_mode_start() {
     ktrace(TAG_IPT_START, (uint32_t)nom_freq, 0,
            (uint32_t)kernel_cr3, (uint32_t)(kernel_cr3 >> 32));
 
+    // Emit other sideband info needed by the trace reader.
+    const struct x86_model_info* model_info = x86_get_model();
+    ktrace(TAG_IPT_CPU_INFO, model_info->processor_type,
+           model_info->display_family, model_info->display_model,
+           model_info->stepping);
+
     mp_sync_exec(MP_CPU_ALL, x86_ipt_start_cpu_task, ipt_cpu_state);
     return NO_ERROR;
 }
