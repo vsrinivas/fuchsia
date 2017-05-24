29083688e15c3b60a5172c6ec8d6092da9c07a9d
Erik Gilling <konkers@google.com>
2017-05-24 14:12:19 -0700

    Add display_pipe service.
    Change-Id: I6b6b92c956aab3be80c7c32d4d990d3ea48dc2ee

---
+++ lib/magma/src/display_pipe/display_provider_impl.cc
@@ -0,0 +1,42 @@
+#include "display_provider_impl.h"
+
+#include "lib/ftl/logging.h"
+
+namespace display_pipe {
+DisplayProviderImpl::DisplayProviderImpl() :
+    conn_(new MagmaConnection),
+    image_pipe_(conn_)
+    {
+    if (!conn_->Open()) {
+        FTL_LOG(ERROR) << "Unable to open connection to magma.";
+        exit(1);
+        return;
+    }
+}
+
+DisplayProviderImpl::~DisplayProviderImpl() = default;
+
+void DisplayProviderImpl::GetInfo(const GetInfoCallback& callback) {
+    auto info = display_pipe::DisplayInfo::New();
+    uint32_t display_width, display_height;
+    if (conn_->GetDisplaySize(&display_width, &display_height)) {
+        info->width = display_width;
+        info->height = display_height;
+    } else {
+        FTL_LOG(ERROR) << "Unable to query display size.";
+        exit(1);
+    }
+
+    callback(std::move(info));
+}
+
+void DisplayProviderImpl::BindPipe(
+    ::fidl::InterfaceRequest<mozart2::ImagePipe> pipe) {
+    image_pipe_.AddBinding(std::move(pipe));
+}
+
+void DisplayProviderImpl::AddBinding(
+    fidl::InterfaceRequest<DisplayProvider> request) {
+  bindings_.AddBinding(this, std::move(request));
+}
+};  // namespace display_pipe
