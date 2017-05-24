35648f620fe455db122acdfe6f06a828b91a82d2
Brian Goldman <bgoldman@google.com>
2017-05-24 15:49:58 -0700

    Improve run_test
    - Retry connecting on startup. There is a race condition that causes
      flakes occasionally, because the Fuchsia recipe starts QEMU in the
      background and then runs run_test. If QEMU isn't listening, the socket
      connection will fail.
    - Report a summary of failed tests at the end.
    - Refactor for testability.
    - Split the test-driving behavior into a module and add unit tests. I
      have not yet figured out how to run the unit tests continuously, but
      I used them to develop this version of the script so I think it's
      better to include them than not.
    - Remove loglistener.
    - Replace netcp with scp.
    - Single quotes instead of double quotes, to save my hands :)
    
    Tested with the unit tests, and also variations on:
    src/run_test --server localhost --test_file src/test/fake_tests.json
    
    Change-Id: Iccf5972b93b642cdb456441780b112aaafc7ce71

---
+++ bin/test_runner/driver/driver.py
@@ -0,0 +1,210 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+import os
+
+
+class Color(object):
+  PURPLE = '\033[95m'
+  CYAN = '\033[96m'
+  DARKCYAN = '\033[36m'
+  BLUE = '\033[94m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  RED = '\033[91m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  END = '\033[0m'
+
+
+class Log(object):
+  silence = False
+
+  @classmethod
+  def print_(cls, message):
+    if not cls.silence:
+      print message
+
+  @classmethod
+  def line(cls, status, message):
+    cls.print_('[%s] %s' % (status, message))
+
+  @classmethod
+  def command(cls, command):
+    cls.line(os.path.basename(command[0]), ' '.join(command))
+
+  @classmethod
+  def test_result(cls, result):
+    if result['failed']:
+      status = 'failed'
+    else:
+      status = 'passed'
+    message = '%s (%d ms)' % (result['name'], result['elapsed'])
+    cls.line(status, message)
+
+  @classmethod
+  def full_report(cls, result):
+    stars = '*' * len(result['name'])
+    lines = [
+      Color.RED,
+      stars,
+      result['name'],
+      stars,
+      result['message'].rstrip(),
+      Color.END,
+    ]
+    for line in lines:
+      cls.print_(line)
+
+
+class NoMatchingTest(Exception): pass
+
+
+def get_tests(config, name=None):
+  by_name = {}
+  for test in config['tests']:
+    test = dict(test)
+    flatten_files_to_copy(test)
+    by_name[test['name']] = test
+
+  if name:
+    try:
+      return [by_name[name]]
+    except KeyError:
+      raise NoMatchingTest(name)
+  else:
+    return list(by_name.itervalues())
+
+
+def flatten_files_to_copy(test):
+  copy_map = test.get('copy', {})
+  test['copy'] = []
+  for remote_dir, files in copy_map.iteritems():
+    for filename in files:
+      test['copy'].append((filename, remote_dir))
+
+
+class MultipleDevicesFound(Exception): pass
+
+
+def get_default_device(netls_output):
+  devices = []
+  for line in netls_output.splitlines():
+    name = line.split()[1]
+    devices.append(name)
+
+  if len(devices) > 1:
+    raise MultipleDevicesFound(', '.join(devices))
+
+  return devices[0]
+
+
+class MissingEnvironmentVariable(Exception): pass
+
+
+class FuchsiaTools(object):
+  def __init__(self, env):
+    self.env = env
+
+  def _get_env(self, name):
+    try:
+      return self.env[name]
+    except KeyError:
+      raise MissingEnvironmentVariable(name)
+
+  @property
+  def fuchsia_out_dir(self):
+    return self._get_env('FUCHSIA_OUT_DIR')
+
+  @property
+  def fuchsia_build_dir(self):
+    return self._get_env('FUCHSIA_BUILD_DIR')
+
+  def scp(self, server, filename, remote_dir):
+    ssh_config = os.path.join(self.fuchsia_build_dir, 'ssh-keys/ssh_config')
+    local_path = os.path.join(self.fuchsia_build_dir, filename)
+    command = [
+      'scp',
+      '-F', ssh_config,
+      local_path,
+      server + ':' + remote_dir,
+    ]
+    Log.command(command)
+    return command
+
+  def netaddr(self, device):
+    path = os.path.join(self.fuchsia_out_dir, 'build-magenta/tools/netaddr')
+    command = [
+      path,
+      '--fuchsia',
+      device,
+    ]
+    Log.command(command)
+    return command
+
+  def netls(self):
+    path = os.path.join(self.fuchsia_out_dir, 'build-magenta/tools/netls')
+    command = [
+      path,
+      '--timeout=3000',
+      '--nowait',
+    ]
+    Log.command(command)
+    return command
+
+
+class BadOpCode(Exception): pass
+class WrongTestId(Exception): pass
+
+
+class Driver(object):
+  """Invokes multiple tests on a remote machine and collects results."""
+
+  def __init__(self):
+    self.current_id = None
+    self.current_test = None
+    self.failed = []
+    self.count = 0
+
+  def _on_result(self, result):
+    self.count += 1
+    Log.test_result(result)
+    if result['failed']:
+      Log.full_report(result)
+      self.failed.append(result)
+
+  def start_test(self, id_, test):
+    self.current_id = id_
+    self.current_test = test
+    Log.line('starting', '%s (%s)' % (test['name'], self.current_id))
+    Log.line('executing', test['exec'])
+    return 'run %s %s' % (self.current_id, test['exec'])
+
+  def wait_for_teardown(self, iter_messages):
+    for message in iter_messages:
+      return_id, op, data = message.split(' ', 2)
+      data = data.strip()
+      if return_id != self.current_id:
+        raise WrongTestId('expected %s got %s' % (self.current_id, return_id))
+
+      if op == 'teardown':
+        if data == 'fail':
+          Log.line('failed', self.current_test['name'])
+        break
+      elif op == 'result':
+        self._on_result(json.loads(data))
+      elif op == 'log':
+        Log.line('log', data)
+      else:
+        raise BadOpCode(op)
+
+  def print_summary(self):
+    Log.print_('\nRan %d total tests: %d passed, %d failed' % (
+        self.count, self.count - len(self.failed), len(self.failed)))
+
+    if self.failed:
+      Log.print_('Summary of failures:')
+      for result in self.failed:
+        Log.full_report(result)
---
+++ bin/test_runner/driver/driver_unittests.py
@@ -0,0 +1,150 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import itertools
+import json
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.dirname(__file__))
+from driver import *
+
+Log.silence = True
+
+
+class ConfigTest(unittest.TestCase):
+  COPY_MAP = {
+    '/x': ['file1', 'file2'],
+    '/y': ['file3'],
+  }
+
+  CONFIG_TREE = {
+    'tests': [
+      {'name': 'test1', 'exec': 'command'},
+      {'name': 'test2', 'exec': 'command', 'copy': COPY_MAP},
+    ],
+  }
+
+  def test_get_all_tests(self):
+    tests = get_tests(self.CONFIG_TREE)
+
+    self.assertEqual(len(tests), 2)
+    self.assertItemsEqual(tests[0]['copy'], [])
+
+    expected_files = [
+      ('file1', '/x'),
+      ('file2', '/x'),
+      ('file3', '/y'),
+    ]
+    self.assertItemsEqual(tests[1]['copy'], expected_files)
+
+  def test_get_one_test(self):
+    tests = get_tests(self.CONFIG_TREE, 'test2')
+    self.assertEqual(len(tests), 1)
+    self.assertEqual(tests[0]['name'], 'test2')
+
+  def test_no_matching_test(self):
+    with self.assertRaisesRegexp(NoMatchingTest, 'test3'):
+      get_tests(self.CONFIG_TREE, 'test3')
+
+
+class GetDefaultDeviceTest(unittest.TestCase):
+  NETLS_SINGLE = 'device step-atom-yard-juicy (fe80::5054:4d:fe63:5e7a/7)\n'
+  NETLS_DOUBLE = ('device step-atom-yard-juicy (fe80::5054:4d:fe63:5e7a/7)\n'
+        'device brush-santa-rail-creme (fe80::8eae:4c4d:fef4:350d/7)\n')
+
+  def test_success(self):
+    device = get_default_device(self.NETLS_SINGLE)
+    self.assertEqual(device, 'step-atom-yard-juicy')
+
+  def test_multiple_devices(self):
+    expected_msg = 'step-atom-yard-juicy, brush-santa-rail-creme'
+    with self.assertRaisesRegexp(MultipleDevicesFound, expected_msg):
+      get_default_device(self.NETLS_DOUBLE)
+
+
+class FuchsiaToolsTest(unittest.TestCase):
+  ENV = {'FUCHSIA_OUT_DIR': 'out', 'FUCHSIA_BUILD_DIR': 'build'}
+
+  def test_netaddr(self):
+    tools = FuchsiaTools(self.ENV)
+    self.assertEqual(
+        tools.netaddr('friend'),
+        ['out/build-magenta/tools/netaddr', '--fuchsia', 'friend'])
+
+  def test_netls(self):
+    tools = FuchsiaTools(self.ENV)
+    self.assertEqual(tools.netls()[0], 'out/build-magenta/tools/netls')
+
+  def test_scp(self):
+    tools = FuchsiaTools(self.ENV)
+    self.assertEqual(
+        tools.scp('server', 'file', '/x'),
+        ['scp', '-F', 'build/ssh-keys/ssh_config', 'build/file', 'server:/x'])
+
+  def test_missing_out_dir(self):
+    tools = FuchsiaTools({})
+    with self.assertRaisesRegexp(MissingEnvironmentVariable, 'FUCHSIA_OUT_DIR'):
+      tools.netls()
+
+  def test_missing_build_dir(self):
+    tools = FuchsiaTools({})
+    with self.assertRaisesRegexp(MissingEnvironmentVariable, 'FUCHSIA_BUILD_DIR'):
+      tools.scp('server', 'file', '/x')
+
+
+class DriverTest(unittest.TestCase):
+  TEST = {'name': 'test', 'exec': 'whatever'}
+
+  def result_msg(self, test_id, **result):
+    return '%s result %s' % (test_id, json.dumps(result))
+
+  def test_run_many(self):
+    driver = Driver()
+
+    messages = [
+      self.result_msg('111', name='x', elapsed=0, failed=True, message='!'),
+      self.result_msg('111', name='y', elapsed=0, failed=False, message=''),
+      '111 teardown fail',
+    ]
+    driver.start_test('111', self.TEST)
+    driver.wait_for_teardown(messages)
+    self.assertEqual(driver.count, 2)
+    self.assertEqual(len(driver.failed), 1)
+    self.assertEqual(driver.failed[0]['message'], '!')
+
+    messages = [
+      self.result_msg('222', name='z', elapsed=0, failed=False, message=''),
+      '222 teardown pass',
+    ]
+    driver.start_test('222', self.TEST)
+    driver.wait_for_teardown(messages)
+    self.assertEqual(driver.count, 3)
+    self.assertEqual(len(driver.failed), 1)
+
+  def test_log(self):
+    log = []
+    Log.print_ = log.append
+
+    driver = Driver()
+    driver.start_test('111', self.TEST)
+    driver.wait_for_teardown(['111 log hello', '111 teardown pass'])
+    self.assertIn('[log] hello', log)
+
+  def test_wrong_test_id(self):
+    driver = Driver()
+    driver.start_test('111', self.TEST)
+    with self.assertRaisesRegexp(WrongTestId, 'expected 111 got 222'):
+      driver.wait_for_teardown(['222 teardown pass'])
+
+  def test_bad_op_code(self):
+    driver = Driver()
+    driver.start_test('111', self.TEST)
+    with self.assertRaisesRegexp(BadOpCode, 'party'):
+      driver.wait_for_teardown(['111 party !'])
+
+
+if __name__ == '__main__':
+    unittest.main()
