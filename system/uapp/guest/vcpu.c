285445605184c7b6392b5fccf15aee4efd2248eb
Abdulla Kamar <abdulla@google.com>
2017-05-25 10:47:11 -0700

    [guest] Add basic TPM support.
    This is just enough to disable TPM usage.
    
    Change-Id: I230cd3422cf3f2036068f9c0e5951a0827bd841f

--- system/uapp/guest/vcpu.c
+++ system/uapp/guest/vcpu.c
@@ -15,7 +15,11 @@
 
 /* Memory-mapped device physical addresses. */
 #define LOCAL_APIC_PHYS_BASE                0xfee00000
+#define LOCAL_APIC_PHYS_TOP                 (LOCAL_APIC_PHYS_BASE + PAGE_SIZE - 1)
 #define IO_APIC_PHYS_BASE                   0xfec00000
+#define IO_APIC_PHYS_TOP                    (IO_APIC_PHYS_BASE + PAGE_SIZE - 1)
+#define TPM_PHYS_BASE                       0xfed40000
+#define TPM_PHYS_TOP                        (TPM_PHYS_BASE + 0x5000 - 1)
 
 /* Local APIC register addresses. */
 #define LOCAL_APIC_REGISTER_ID              0x0020
@@ -31,13 +35,16 @@
 #define IO_APIC_IOWIN                       0x10
 
 /* IO APIC register names. */
-#define IO_APIC_REG_ID                      0x00
-#define IO_APIC_REG_VER                     0x01
+#define IO_APIC_REGISTER_ID                 0x00
+#define IO_APIC_REGISTER_VER                0x01
 
 /* IO APIC configuration constants. */
 #define IO_APIC_VERSION                     0x11
 #define FIRST_REDIRECT_OFFSET               0x10
 #define LAST_REDIRECT_OFFSET                (FIRST_REDIRECT_OFFSET + IO_APIC_REDIRECT_OFFSETS - 1)
+
+/* TPM register names. */
+#define TPM_REGISTER_ACCESS                 0x00
 
 static mx_status_t handle_io_port(vcpu_context_t* context, const mx_guest_io_port_t* io_port) {
     io_port_state_t* io_port_state = &context->guest_state->io_port_state;
@@ -111,10 +118,10 @@ static mx_status_t handle_io_apic(vcpu_context_t* context, const mx_guest_mem_tr
         return io_apic_state->select > UINT8_MAX ? ERR_INVALID_ARGS : NO_ERROR;
     case IO_APIC_IOWIN:
         switch (io_apic_state->select) {
-        case IO_APIC_REG_ID:
+        case IO_APIC_REGISTER_ID:
             apply_inst(inst, &io_apic_state->id);
             return NO_ERROR;
-        case IO_APIC_REG_VER:
+        case IO_APIC_REGISTER_VER:
             if (!inst->read || inst->reg == NULL)
                 return ERR_NOT_SUPPORTED;
             // There are two redirect offsets per redirection entry. We return
@@ -128,6 +135,23 @@ static mx_status_t handle_io_apic(vcpu_context_t* context, const mx_guest_mem_tr
             apply_inst(inst, io_apic_state->redirect + i);
             return NO_ERROR;
         }}
+    }
+    return ERR_NOT_SUPPORTED;
+}
+
+static mx_status_t handle_tpm(const mx_guest_mem_trap_t* mem_trap, instruction_t* inst) {
+    MX_ASSERT(mem_trap->guest_paddr >= TPM_PHYS_BASE);
+    mx_vaddr_t offset = mem_trap->guest_paddr - TPM_PHYS_BASE;
+    switch (offset) {
+    case TPM_REGISTER_ACCESS:
+        if (!inst->read)
+            return ERR_NOT_SUPPORTED;
+        if (inst->mem != 1u)
+            return ERR_BAD_STATE;
+        // Respond with all ones to signal an invalid access to device memory.
+        // This should effectively disable any TPM driver.
+        *inst->reg = UINT8_MAX;
+        return NO_ERROR;
     }
     return ERR_NOT_SUPPORTED;
 }
@@ -154,13 +178,16 @@ static mx_status_t handle_mem_trap(vcpu_context_t* context, const mx_guest_mem_t
     } else {
         status = ERR_UNAVAILABLE;
         switch (mem_trap->guest_paddr) {
-        case LOCAL_APIC_PHYS_BASE ... LOCAL_APIC_PHYS_BASE + PAGE_SIZE - 1:
+        case LOCAL_APIC_PHYS_BASE ... LOCAL_APIC_PHYS_TOP:
             status = handle_local_apic(context, mem_trap, &inst);
             break;
-        case IO_APIC_PHYS_BASE ... IO_APIC_PHYS_BASE + PAGE_SIZE - 1:
+        case IO_APIC_PHYS_BASE ... IO_APIC_PHYS_TOP:
             mtx_lock(&context->guest_state->mutex);
             status = handle_io_apic(context, mem_trap, &inst);
             mtx_unlock(&context->guest_state->mutex);
+            break;
+        case TPM_PHYS_BASE ... TPM_PHYS_TOP:
+            status = handle_tpm(mem_trap, &inst);
             break;
         }
     }
