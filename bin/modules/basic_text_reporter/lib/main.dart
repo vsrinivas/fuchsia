520bee43298d81db1a73c570057c6d58d22417d4
Asela Gunawardana <aselag@google.com>
2017-05-25 12:44:55 -0700

    Added basic_text_reporter
    Just publishes text to the context service.
    
    Change-Id: Ibc414f07cb68ce7b9ad21957546263044307e3b6

---
+++ bin/modules/basic_text_reporter/lib/main.dart
@@ -0,0 +1,81 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:application.lib.app.dart/app.dart';
+import 'package:application.services/service_provider.fidl.dart';
+import 'package:apps.modular.services.story/link.fidl.dart';
+import 'package:apps.modular.services.module/module.fidl.dart';
+import 'package:apps.modular.services.module/module_context.fidl.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+
+import 'package:flutter/widgets.dart';
+
+final ApplicationContext _appContext = new ApplicationContext.fromStartupInfo();
+
+/// This is used for keeping the reference around.
+ModuleImpl _module;
+
+void _log(String msg) {
+  print('[Basic Text Reporter Module] $msg');
+}
+
+/// An implementation of the [Module] interface.
+class ModuleImpl extends Module {
+  final ModuleBinding _binding = new ModuleBinding();
+
+  final ModuleContextProxy _moduleContext = new ModuleContextProxy();
+  final LinkProxy _link = new LinkProxy();
+
+  /// Bind an [InterfaceRequest] for a [Module] interface to this object.
+  void bind(InterfaceRequest<Module> request) {
+    _binding.bind(this, request);
+  }
+
+  /// Implementation of the Initialize(ModuleContext story, Link link) method.
+  @override
+  void initialize(
+      InterfaceHandle<ModuleContext> moduleContextHandle,
+      InterfaceHandle<ServiceProvider> incomingServices,
+      InterfaceRequest<ServiceProvider> outgoingServices) {
+    _log('ModuleImpl::initialize call');
+
+    // NOTE: These story / link proxy variables must not be local variables.
+    // When a handle is bound to a proxy and then the proxy variable is garbage
+    // collected before the pipe is properly closed or unbound, the app will
+    // crash due to the leaked handle.
+    _moduleContext.ctrl.bind(moduleContextHandle);
+    _moduleContext.getLink(null, _link.ctrl.request());
+
+    // Do something with the story and link services.
+  }
+
+  /// Implementation of the Stop() => (); method.
+  @override
+  void stop(void callback()) {
+    _log('ModuleImpl::stop call');
+
+    // Do some clean up here.
+    _moduleContext.ctrl.close();
+    _link.ctrl.close();
+
+    // Invoke the callback to signal that the clean-up process is done.
+    callback();
+  }
+}
+
+/// Entry point for this module.
+void main() {
+  _log('Module started with ApplicationContext: $_appContext');
+
+  /// Add [ModuleImpl] to this application's outgoing ServiceProvider.
+  _appContext.outgoingServices.addServiceForName(
+    (request) {
+      _log('Received binding request for Module');
+      _module = new ModuleImpl()..bind(request);
+    },
+    Module.serviceName,
+  );
+
+  runApp(new Text("Here's text!"));
+}
