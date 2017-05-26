347ee0078b12bf2f38c386ddc98e16aaf54e6643
Rob Tsuk <robtsuk@google.com>
2017-05-26 12:34:53 -0700

    Correct naming and expose service connect functions
    Change the rust wrapper naming of well known handles to match
    the naming in processargs.h.
    
    Expose mxio_service_connect() and mxio_service_connect_at() to Rust.
    
    Provide get_service_root() and connect_to_environment_service() as
    found in //application/lib/app/application_context.cc.
    
    Change-Id: I7de2ed6370f8982f9a10fa8620240c654290739c

--- public/rust/crates/mxruntime/mxruntime-sys/src/lib.rs
+++ public/rust/crates/mxruntime/mxruntime-sys/src/lib.rs
@@ -8,16 +8,19 @@
 
 extern crate magenta_sys;
 
+use std::os::raw::c_char;
+
 use magenta_sys::mx_handle_t;
+use magenta_sys::mx_status_t;
 
 pub const MX_HANDLE_INVALID: mx_handle_t = 0;
 
 // Startup handle types, derived from magenta/system/public/magenta/processargs.h
 
 // Handle to our own process.
-pub const MX_HND_TYPE_PROC_SELF: u32 = 0x01;
+pub const PA_PROC_SELF: u32 = 0x01;
 // Handle to the initial thread of our own process.
-pub const MX_HND_TYPE_THREAD_SELF: u32 = 0x02;
+pub const PA_THREAD_SELF: u32 = 0x02;
 // Handle to the VMO containing the ELF image of the system vDSO.  This
 // handle is duplicable, transferable, readable, and executable, but not
 // writable.  The contents of the VM object should be treated like any
@@ -26,7 +29,7 @@ pub const MX_HND_TYPE_THREAD_SELF: u32 = 0x02;
 // might create or propagate it on to its children so they can do so.
 // Each process's own vDSO was mapped in by its creator before the
 // process started, its address passed as an argument to entry point.
-pub const MX_HND_TYPE_VDSO_VMO: u32 = 0x11;
+pub const PA_VMO_VDSO: u32 = 0x11;
 // Handle to the VMO used to map the initial thread's stack.  This
 // handle usually has all rights.  The protocol between process creator
 // and new process is that this entire VM object has been mapped in
@@ -36,39 +39,39 @@ pub const MX_HND_TYPE_VDSO_VMO: u32 = 0x11;
 // calling convention for function entry.  Thus the new process can
 // compute its exact stack bounds by subtracting the size reported by
 // this VMO from the (adjusted back up) initial SP value.
-pub const MX_HND_TYPE_STACK_VMO: u32 = 0x13;
+pub const PA_VMO_STACK: u32 = 0x13;
 // Handle to a VMO containing a bootfs format image.
 // The "arg" field used with this type is a simple ordinal.
-pub const MX_HND_TYPE_BOOTFS_VMO: u32 = 0x1B;
+pub const PA_VMO_BOOTFS: u32 = 0x1B;
 // Handle to a Job object which can be used to make child processes. The
 // Job can be the same as the one used to create this process or it can
 // be different.
-pub const MX_HND_TYPE_JOB: u32 = 0x03;
+pub const PA_JOB_DEFAULT: u32 = 0x03;
 // Handle types the mxio library uses
-pub const MX_HND_TYPE_MXIO_ROOT: u32 = 0x30;
-pub const MX_HND_TYPE_MXIO_CWD: u32 = 0x31;
-pub const MX_HND_TYPE_MXIO_REMOTE: u32 = 0x32;
-pub const MX_HND_TYPE_MXIO_PIPE: u32 = 0x33;
-pub const MX_HND_TYPE_MXIO_EVENT: u32 = 0x34;
-pub const MX_HND_TYPE_MXIO_LOGGER: u32 = 0x35;
-pub const MX_HND_TYPE_MXIO_SOCKET: u32 = 0x36;
-// Handle types used by the device manager and device hosts
-pub const MX_HND_TYPE_RESOURCE: u32 = 0x3F;
-// Handle types used by the mojo application model
-pub const MX_HND_TYPE_APPLICATION_REQUEST: u32 = 0x40;
-pub const MX_HND_TYPE_APPLICATION_LAUNCHER: u32 = 0x41;
-pub const MX_HND_TYPE_INCOMING_SERVICES: u32 = 0x42;
-pub const MX_HND_TYPE_OUTGOING_SERVICES: u32 = 0x43;
+pub const PA_MXIO_ROOT: u32 = 0x30;
+pub const PA_MXIO_CWD: u32 = 0x31;
+pub const PA_MXIO_REMOTE: u32 = 0x32;
+pub const PA_MXIO_PIPE: u32 = 0x33;
+pub const PA_MXIO_EVENT: u32 = 0x34;
+pub const PA_MXIO_LOGGER: u32 = 0x35;
+pub const PA_MXIO_SOCKET: u32 = 0x36;
+// Used by devmgr and devhosts
+pub const PA_RESOURCE: u32 = 0x3F;
+// Handle types used by the application model
+pub const PA_APP_LAUNCHER: u32 = 0x41;
+pub const PA_APP_SERVICES: u32 = 0x43;
 // Message pipe for dynamic loader service
-pub const MX_HND_TYPE_LOADER_SVC: u32 = 0x10;
+pub const PA_SVC_LOADER: u32 = 0x10;
 // VM object handle for the main executable file
-pub const MX_HND_TYPE_EXEC_VMO: u32 = 0x14;
+pub const PA_VMO_EXECUTABLE: u32 = 0x14;
 // Handle types for one-off use and prototyping
-pub const MX_HND_TYPE_USER0: u32 = 0xF0;
-pub const MX_HND_TYPE_USER1: u32 = 0xF1;
-pub const MX_HND_TYPE_USER2: u32 = 0xF2;
+pub const PA_USER0: u32 = 0xF0;
+pub const PA_USER1: u32 = 0xF1;
+pub const PA_USER2: u32 = 0xF2;
 
 #[link(name="mxio")]
 extern {
     pub fn mx_get_startup_handle(id: u32) -> mx_handle_t;
+    pub fn mxio_service_connect(svcpath: *const c_char, h: mx_handle_t) -> mx_status_t;
+    pub fn mxio_service_connect_at(dir: mx_handle_t, path: *const c_char, h: mx_handle_t) -> mx_status_t;
 }
--- public/rust/crates/mxruntime/src/lib.rs
+++ public/rust/crates/mxruntime/src/lib.rs
@@ -8,9 +8,13 @@ extern crate magenta;
 extern crate magenta_sys;
 extern crate mxruntime_sys;
 
-use magenta::Handle;
+use magenta::{Handle, HandleBase, Channel, ChannelOpts, Status};
 
-use magenta_sys::mx_handle_t;
+use magenta_sys::{mx_handle_t, NO_ERROR};
+
+use mxruntime_sys::{mxio_service_connect, mxio_service_connect_at};
+
+use std::ffi::CString;
 
 pub const MX_HANDLE_INVALID: mx_handle_t = 0;
 
@@ -20,13 +24,11 @@ pub const MX_HANDLE_INVALID: mx_handle_t = 0;
 #[repr(u32)]
 pub enum HandleType {
     // Handle types used by the device manager and device hosts
-    Resource = mxruntime_sys::MX_HND_TYPE_RESOURCE,
+    Resource = mxruntime_sys::PA_RESOURCE,
     // Handle types used by the mojo application model
-    ApplicationRequest = mxruntime_sys::MX_HND_TYPE_APPLICATION_REQUEST,
-    ApplicationLauncher = mxruntime_sys::MX_HND_TYPE_APPLICATION_LAUNCHER,
-    IncomingServices = mxruntime_sys::MX_HND_TYPE_INCOMING_SERVICES,
-    OutgoingServices = mxruntime_sys::MX_HND_TYPE_OUTGOING_SERVICES,
-    User0 = mxruntime_sys::MX_HND_TYPE_USER0,
+    ApplicationLauncher = mxruntime_sys::PA_APP_LAUNCHER,
+    OutgoingServices = mxruntime_sys::PA_APP_SERVICES,
+    User0 = mxruntime_sys::PA_USER0,
 }
 
 /// Get a startup handle of the given type, if available.
@@ -38,5 +40,31 @@ pub fn get_startup_handle(htype: HandleType) -> Option<Handle> {
         } else {
             Some(Handle::from_raw(raw))
         }
+    }
+}
+
+pub fn get_service_root() -> Result<Channel, Status> {
+    let (h1, h2) = Channel::create(ChannelOpts::Normal).unwrap();
+    let svc = CString::new("/svc/.").unwrap();
+    let connect_status = unsafe {
+        mxio_service_connect(svc.as_ptr(), h1.raw_handle())
+    };
+    if connect_status == NO_ERROR {
+        Ok(h2)
+    } else {
+        Err(Status::from_raw(connect_status))
+    }
+}
+
+pub fn connect_to_environment_service(service_root: Channel, path: &str) -> Result<Channel, Status> {
+    let (h1, h2) = Channel::create(ChannelOpts::Normal).unwrap();
+    let path_str = CString::new(path).unwrap();
+    let connect_status = unsafe {
+        mxio_service_connect_at(service_root.raw_handle(), path_str.as_ptr(), h1.raw_handle())
+    };
+    if connect_status == NO_ERROR {
+        Ok(h2)
+    } else {
+        Err(Status::from_raw(connect_status))
     }
 }
