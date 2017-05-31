5701cb61bedab26cf2f29136d62906166de5adbf
Benjamin Lerman <qsr@chromium.org>
2017-05-31 17:42:41 +0200

    Update bupsplit.
    Change from C to C++.
    Allow to add data in multiple passes.
    Have a minimal and maximal size for chunks.
    
    Change-Id: Ic77fda5ba82fbdda76cd95ed488217f6246bc96b

--- bin/ledger/third_party/bup/bupsplit.h
+++ bin/ledger/third_party/bup/bupsplit.h
@@ -1,20 +1,20 @@
 /*
  * Copyright 2011 Avery Pennarun. All rights reserved.
- * 
+ *
  * (This license applies to bupsplit.c and bupsplit.h only.)
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in
  *       the documentation and/or other materials provided with the
  *       distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY AVERY PENNARUN ``AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -27,23 +27,60 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef __BUPSPLIT_H
-#define __BUPSPLIT_H
+#ifndef APPS_LEDGER_SRC_THIRD_PARTY_BUP_BUPSPLIT_H_
+#define APPS_LEDGER_SRC_THIRD_PARTY_BUP_BUPSPLIT_H_
 
-#define BUP_BLOBBITS (13)
-#define BUP_BLOBSIZE (1<<BUP_BLOBBITS)
-#define BUP_WINDOWBITS (7)
-#define BUP_WINDOWSIZE (1<<(BUP_WINDOWBITS-1))
+#include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-    
-int bupsplit_find_ofs(const unsigned char *buf, int len, int *bits);
-int bupsplit_selftest(void);
+#include "lib/ftl/macros.h"
+#include "lib/ftl/strings/string_view.h"
 
-#ifdef __cplusplus
-}
-#endif
-    
-#endif /* __BUPSPLIT_H */
+namespace bup {
+
+constexpr uint8_t kBlobBits = 13;
+constexpr uint16_t kBlobSize = 1 << kBlobBits;
+constexpr uint8_t kWindowBits = 7;
+constexpr uint16_t kWindowSize = 1 << (kWindowBits - 1);
+
+// Splits data into chunks between |min_length| and |max_length| of size that
+// are "good" for de-duplication.
+//
+// It achieves that by calculating a rolling hash of the window of
+// |kWindowBits|. The split points are selected when the last |kBlobBits| of the
+// current hash are all 1s. This ensures that if data is removed or inserted in
+// the middle of data, only the 2 split points following the change are
+// modified, and all others stay identical.
+class RollSumSplit {
+ public:
+  // |min_length| is the minimal size of a chunk.
+  // |max_length| is the maximal size of a chunk.
+  RollSumSplit(size_t min_length, size_t max_length);
+
+  // Reset the state of the rolling hash.
+  void Reset();
+
+  // Returns a non-zero value indicating the size of the prefix that is the next
+  // cut, or 0 if all data was consumed without finding the next cut.
+  // If |bits| is not null, and a cut has been found, |*bits| will be the number
+  // of trailing 1s in the current hash. It will always be greater of equals to
+  // |kBlobBits|.
+  size_t Feed(ftl::StringView buffer, size_t* bits);
+
+ private:
+  void Add(uint8_t drop, uint8_t add);
+  void Roll(uint8_t c);
+  uint32_t Digest();
+
+  const size_t min_length_;
+  const size_t max_length_;
+  size_t current_length_;
+  uint64_t s1_, s2_;
+  uint8_t window_[kWindowSize];
+  size_t window_index_;
+
+  FTL_DISALLOW_COPY_AND_ASSIGN(RollSumSplit);
+};
+
+}  // namespace bup
+
+#endif  // APPS_LEDGER_SRC_THIRD_PARTY_BUP_BUPSPLIT_H_
