33b481b61f02ea1269790e141b816372c1555560
Adam Barth <abarth@chromium.org>
2017-05-31 15:40:29 -0700

    Modernize trace.sh
     * Removes @ tricks
     * Uses ssh for command and file transport
    
    Change-Id: I38d165b2610a4d54df6da041fd232c34c8caeab4

--- scripts/trace.sh
+++ scripts/trace.sh
@@ -4,14 +4,8 @@
 # found in the LICENSE file.
 
 # Runs tracing on the remote device.
-#
-# TODO(jeffbrown): Make this more robust overall, probably shouldn't
-# bootstrap every time a program is supplied.  We could make this more
-# self-contained too instead of relying on chrome://tracing.  At the least
-# it would be nice to figure out how to provide a direct link to load the
-# trace.
 
-if [[ -z "${FUCHSIA_DIR}" ]] || [[ -z "${MAGENTA_TOOLS_DIR}" ]]; then
+if [[ -z "${FUCHSIA_DIR}" ]] || [[ -z "${FUCHSIA_BUILD_DIR}" ]]; then
   echo "Fuchsia environment variables not defined" >&2
   echo "Please run 'source [fuchsia-root-dir]/scripts/env.sh' then 'fset'" >&2
   exit 1
@@ -22,18 +16,11 @@ trace2html="${catapult_dir}/tracing/bin/trace2html"
 
 function usage() {
   cat >&2 <<END
-Usage: trace.sh [--duration N] [--bootstrap] [--env name] [--no-html]
-                [program and args...]
+Usage: trace.sh [--duration N] [--no-html] [program and args...]
 
 Runs a trace and downloads the resulting trace file.
 
   --duration N : Traces for N seconds.  Defaults to 10 seconds.
-
-  --bootstrap : Runs the progam within a freshly bootstrapped environment.
-
-  --env name : Specifies the name of the environment within which to run
-               the program.  Defaults to 'boot'.  Ignored if '--bootstrap'
-               is also specified.
 
   --no-html : Does not automatically convert JSON trace file to HTML.
               The JSON file should be viewed using chrome://tracing manually.
@@ -47,8 +34,6 @@ END
 
 duration=10
 html=1
-bootstrap=
-env=boot
 buffer_size=4
 while [[ $# -ne 0 ]]; do
   case $1 in
@@ -58,17 +43,6 @@ while [[ $# -ne 0 ]]; do
         exit 1
       fi
       duration=$2
-      shift
-      ;;
-    --bootstrap)
-      bootstrap=1
-      ;;
-    --env)
-      if [[ $# -lt 2 ]]; then
-        usage
-        exit 1
-      fi
-      env=$2
       shift
       ;;
     --no-html)
@@ -92,26 +66,21 @@ while [[ $# -ne 0 ]]; do
   shift
 done
 
-trace="trace record --duration=${duration} --buffer-size=${buffer_size} $*"
-command=
-if [[ -n "${bootstrap}" ]]; then
-  command="@ bootstrap ${trace}"
-else
-  command="@${env} ${trace}"
-fi
+command="trace record --duration=${duration} --buffer-size=${buffer_size} $*"
 
 # since we can't observe trace completion, add a delay to compensate for lag
 delay=$(( $duration + 30 ))
 trace_file="trace_$(date +%Y-%m-%d_at_%H.%M.%S).json"
+host="$(netaddr --fuchsia)"
 
 rm -f "${trace_file}"
 echo "Starting trace..." \
   && echo "Running: ${command}" \
-  && "${MAGENTA_TOOLS_DIR}/netruncmd" : "${command}" \
+  && ssh -q -F "${FUCHSIA_BUILD_DIR}/ssh-keys/ssh_config" "${host}" "${command}" \
   && echo "Sleeping ${delay} seconds..." \
   && sleep "${delay}" \
   && echo "Downloading trace to ${trace_file}" \
-  && "${MAGENTA_TOOLS_DIR}/netcp" :/tmp/trace.json "${trace_file}"
+  && scp -q -F "${FUCHSIA_BUILD_DIR}/ssh-keys/ssh_config" "[${host}]:/tmp/trace.json" "${trace_file}"
 if [[ $? -ne 0 ]]; then
   echo "Failed" >&2;
   exit 1
