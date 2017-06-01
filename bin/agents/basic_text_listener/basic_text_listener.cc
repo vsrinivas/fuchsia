7cf55e6a86f6f8d5d8235ba484119cc20738c3c5
Travis Martin <travismart@google.com>
2017-06-01 11:06:50 -0700

    Add simple proposal on context update.
    This change gives a dummy "launch web_view" propsal whenever the context
    store is updated, which can be viewed by manually opening the proposal
    list.
    
    Two simple improvements to this CL:
    1. Is there an easy way of launching web_view straight to a given url?
    2. Is there a way of making the proposal list pop up automatically?
    
    Change-Id: I4d174701ecbcfba5a64d598f6c3f22ad3c267482

--- bin/agents/basic_text_listener/basic_text_listener.cc
+++ bin/agents/basic_text_listener/basic_text_listener.cc
@@ -6,15 +6,47 @@
 
 #include "application/lib/app/application_context.h"
 #include "apps/maxwell/services/context/context_provider.fidl.h"
+#include "apps/maxwell/services/suggestion/proposal_publisher.fidl.h"
 #include "lib/mtl/tasks/message_loop.h"
 
 namespace maxwell {
+
+constexpr char kWebViewUrl[] = "file:///system/apps/web_view";
+
+ProposalPtr MkUrlProposal(const std::string& url) {
+  const std::string label = "Launch url: " + url;
+  auto p = Proposal::New();
+  p->id = "launch web_view";
+  auto create_story = CreateStory::New();
+  create_story->module_id = kWebViewUrl;
+
+  // TODO(travismart,thatguy): How to load a specifc URL in web_view?
+  //create_story->initial_data = "{\"view\": {\"uri\": \"http://www.yahoo.com\" } }";
+
+  auto action = Action::New();
+  action->set_create_story(std::move(create_story));
+  p->on_selected.push_back(std::move(action));
+
+  auto d = SuggestionDisplay::New();
+  d->headline = label;
+  d->subheadline = "";
+  d->details = "";
+  d->color = 0xff4285f4;
+  d->icon_urls.push_back("");
+  d->image_url = "";
+  d->image_type = SuggestionImageType::OTHER;
+
+  p->display = std::move(d);
+
+  return p;
+}
 
 class BasicTextListener : ContextListener {
  public:
   BasicTextListener()
       : app_context_(app::ApplicationContext::CreateFromStartupInfo()),
         provider_(app_context_->ConnectToEnvironmentService<ContextProvider>()),
+        proposal_out_(app_context_->ConnectToEnvironmentService<ProposalPublisher>()),
         binding_(this) {
     FTL_LOG(INFO) << "Initializing";
     auto query = ContextQuery::New();
@@ -27,12 +59,16 @@ class BasicTextListener : ContextListener {
   void OnUpdate(ContextUpdatePtr result) override {
     const auto& values = result.get()->values;
     for (auto it = values.cbegin(); it != values.cend(); ++it) {
-      FTL_LOG(INFO) << it.GetKey() << " : " << it.GetValue();
+      const std::string key = it.GetKey();
+      const std::string value = it.GetValue();
+      FTL_LOG(INFO) << key << " : " << value;
+      proposal_out_->Propose(MkUrlProposal(value));
     }
   }
 
   std::unique_ptr<app::ApplicationContext> app_context_;
   ContextProviderPtr provider_;
+  ProposalPublisherPtr proposal_out_;
   fidl::Binding<ContextListener> binding_;
 };
 
