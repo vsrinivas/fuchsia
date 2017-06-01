f7d557dd3708c67699819cb5047cecae87a106ee
Mark Seaborn <mseaborn@google.com>
2017-06-01 11:35:36 -0700

    [netsvc] Add missing copyright notices
    Change-Id: I4aff94d2a2d0aab3b8e00126fc81ac4af9697cc7

--- system/core/netsvc/device_id.c
+++ system/core/netsvc/device_id.c
@@ -1,3 +1,7 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <stdio.h>
 #include <string.h>
 
--- system/core/netsvc/eff_short_wordlist_1.h
+++ system/core/netsvc/eff_short_wordlist_1.h
@@ -1,3 +1,7 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #pragma once
 
 #define DICEWARE_DICTIONARY_SIZE 1296
