a8689c7de4e9f25244792362a4312a5918e63abd
George Kulakowski <kulakowski@google.com>
2017-06-05 11:08:35 -0700

    [launchpad] Remove unused launch.h header
    Both finish_launch and get_mxio_job were migrated away from and
    removed long ago.
    
    Change-Id: I6d9adb806ec651bf3347fa7bd8fb412745d1f448

--- system/ulib/launchpad/mxio.c
+++ system/ulib/launchpad/mxio.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "launch.h"
+#include <launchpad/launchpad.h>
 #include <launchpad/vmo.h>
 
 #include <magenta/process.h>
