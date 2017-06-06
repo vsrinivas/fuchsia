7502996bb42a5c28ed476ad3ce8e50b794711e37
John Grossman <johngro@google.com>
2017-06-06 13:28:17 -0700

    [usb][audio] Remove usb audio driver.
    It is moving out to the drivers/audio repository under Fuchsia.
    
    Change-Id: Ic22d85f00f47d7e5c3552bb22555c8c7519bcb0a

--- system/udev/usb-audio/midi.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2016 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#pragma once
-
-int get_midi_message_length(uint8_t status_byte);
