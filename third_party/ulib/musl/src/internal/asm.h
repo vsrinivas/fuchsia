a80712e129e544e165bbbd3f0507c987b655f7dc
Roland McGrath <mcgrathr@google.com>
2017-06-05 19:26:32 -0700

    [ulib][musl][setjmp][asan] Make longjmp call __asan_handle_no_return
    The ASan runtime wants to be notified when a non-local exit happens.
    
    Change-Id: I66af86955449577595e868776073637010ad7fb3

--- third_party/ulib/musl/src/internal/asm.h
+++ third_party/ulib/musl/src/internal/asm.h
@@ -4,6 +4,10 @@
 
 #pragma once
 
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+
 #define ALIAS_ENTRY(name) \
     .globl name; \
     .type name, %function; \
@@ -19,3 +23,31 @@ name:
 #define END(name) \
     .cfi_endproc; \
     ALIAS_END(name)
+
+#ifdef __aarch64__
+.macro push_regs reg1, reg2
+    stp \reg1, \reg2, [sp, #-16]!
+    .cfi_adjust_cfa_offset 16
+    .cfi_rel_offset \reg1, 0
+    .cfi_rel_offset \reg2, 8
+.endm
+.macro pop_regs reg1, reg2
+    ldp \reg1, \reg2, [sp], #16
+    .cfi_adjust_cfa_offset -16
+    .cfi_same_value \reg1
+    .cfi_same_value \reg2
+.endm
+#endif
+
+#ifdef __x86_64__
+.macro push_reg reg
+    push \reg
+    .cfi_adjust_cfa_offset 8
+    .cfi_rel_offset \reg, 0
+.endm
+.macro pop_reg reg
+    pop \reg
+    .cfi_adjust_cfa_offset -8
+    .cfi_same_value \reg
+.endm
+#endif
