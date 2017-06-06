9d82277767a3824227857194837ce26347165105
Christopher Anderson <cja@google.com>
2017-06-05 18:54:35 -0700

    [i915] Use new PCI protocol API.
    Change-Id: I48dbd3b5075eb517ebc7dfba0f7498cbbb46a1d3

--- system/udev/intel-i915/intel-i915.c
+++ system/udev/intel-i915/intel-i915.c
@@ -144,8 +144,10 @@ static mx_status_t intel_i915_bind(void* ctx, mx_device_t* dev, void** cookie) {
         return ERR_NO_MEMORY;
 
     const pci_config_t* pci_config;
+    size_t config_size;
     mx_handle_t cfg_handle = MX_HANDLE_INVALID;
-    status = pci->get_config(dev, &pci_config, &cfg_handle);
+    status = pci->map_resource(dev, PCI_RESOURCE_CONFIG, MX_CACHE_POLICY_UNCACHED_DEVICE,
+                               (void**)&pci_config, &config_size, &cfg_handle);
     if (status == NO_ERROR) {
         if (pci_config->device_id == INTEL_I915_BROADWELL_DID) {
             // TODO: this should be based on the specific target
@@ -155,18 +157,20 @@ static mx_status_t intel_i915_bind(void* ctx, mx_device_t* dev, void** cookie) {
     }
 
     // map register window
-    status = pci->map_mmio(dev, 0, MX_CACHE_POLICY_UNCACHED_DEVICE,
+    status = pci->map_resource(dev, PCI_RESOURCE_BAR_0, MX_CACHE_POLICY_UNCACHED_DEVICE,
                            &device->regs, &device->regs_size, &device->regs_handle);
     if (status != NO_ERROR) {
+        printf("i915: failed to map bar 0: %d\n", status);
         goto fail;
     }
 
     // map framebuffer window
-    status = pci->map_mmio(dev, 2, MX_CACHE_POLICY_WRITE_COMBINING,
+    status = pci->map_resource(dev, PCI_RESOURCE_BAR_2, MX_CACHE_POLICY_WRITE_COMBINING,
                            &device->framebuffer,
                            &device->framebuffer_size,
                            &device->framebuffer_handle);
     if (status != NO_ERROR) {
+        printf("i915: failed to map bar 2: %d\n", status);
         goto fail;
     }
 
