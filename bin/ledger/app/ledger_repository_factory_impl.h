0791ec5af6f4144d04b922d057e0a05998c8f8b8
Alhaad Gokhale <alhaad@google.com>
2017-06-12 14:19:00 -0700

    Revert "app: allow Framework to erase a given repository"
    This reverts commit a8c8210ff3987ba0412f7e89bdb9bacecad64849.
    
    Change-Id: I79915289b617d0952b6e824d67ee76a0a887227c

--- bin/ledger/app/ledger_repository_factory_impl.h
+++ bin/ledger/app/ledger_repository_factory_impl.h
@@ -11,7 +11,6 @@
 #include "apps/ledger/services/internal/internal.fidl.h"
 #include "apps/ledger/src/app/ledger_repository_impl.h"
 #include "apps/ledger/src/callback/auto_cleanable.h"
-#include "apps/ledger/src/callback/pending_operation.h"
 #include "apps/ledger/src/cloud_sync/public/user_config.h"
 #include "apps/ledger/src/environment/environment.h"
 #include "apps/modular/services/auth/token_provider.fidl.h"
@@ -37,12 +36,6 @@ class LedgerRepositoryFactoryImpl : public LedgerRepositoryFactory {
       fidl::InterfaceRequest<LedgerRepository> repository_request,
       const GetRepositoryCallback& callback) override;
 
-  void EraseRepository(
-      const fidl::String& repository_path,
-      const fidl::String& server_id,
-      fidl::InterfaceHandle<modular::auth::TokenProvider> token_provider,
-      const EraseRepositoryCallback& callback) override;
-
   void CreateRepository(LedgerRepositoryContainer* container,
                         std::string sanitized_path,
                         cloud_sync::UserConfig user_config);
@@ -52,8 +45,6 @@ class LedgerRepositoryFactoryImpl : public LedgerRepositoryFactory {
 
   callback::AutoCleanableMap<std::string, LedgerRepositoryContainer>
       repositories_;
-
-  callback::PendingOperationManager pending_operation_manager_;
 
   FTL_DISALLOW_COPY_AND_ASSIGN(LedgerRepositoryFactoryImpl);
 };
