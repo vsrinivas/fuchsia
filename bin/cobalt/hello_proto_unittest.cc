150308e53c5f6a4a38e21a32cdd8b26a55e870f7
Mitch Rudominer <rudominer@chromium.org>
2017-06-11 23:07:55 -0700

    Adds test of grpc
    Change-Id: Ibcd83c4c0abe9dc4ff5ab9686f653072c07d6d6f

--- bin/cobalt/hello_proto_unittest.cc
+++ bin/cobalt/hello_proto_unittest.cc
@@ -2,11 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "apps/cobalt_client/src/hello.grpc.pb.h"
 #include "apps/cobalt_client/src/hello.pb.h"
+#include "grpc++/grpc++.h"
 #include "gtest/gtest.h"
 
+namespace cobalt {
+namespace fuchsia {
+
+class HelloImpl final : public Hello::Service {
+  grpc::Status SayHello(grpc::ServerContext* context, const Person* request,
+                        HelloResponse* response) override {
+    response->set_greeting(std::string("Hello ") + request->name());
+    return grpc::Status::OK;
+  }
+};
+
 TEST(HelloProto, Hello) {
-  test::Person person;
+  Person person;
   person.set_name("Fred");
-  EXPECT_EQ("Fred", person.name());
+  HelloImpl impl;
+  Hello::Service* service = &impl;
+  grpc::ServerContext context;
+  HelloResponse response;
+  auto status = service->SayHello(&context, &person, &response);
+  ASSERT_TRUE(status.ok());
+  EXPECT_EQ("Hello Fred", response.greeting());
 }
+
+}  // namespace fuchsia
+}  // namespace cobalt
