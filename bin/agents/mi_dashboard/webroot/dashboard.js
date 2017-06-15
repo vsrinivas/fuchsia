7ae33ed338d16cb8e19985caa6ac3a84c889f55b
Jeffrey Nichols <jwnichols@google.com>
2017-06-15 13:04:07 -0700

    [mi_dashboard] Improve Dashboard tabs
    Added an overview tab to the Dashboard and some code to populate it
    based on input coming in from multiple sources. Also modified the
    existing tabs to work better with the Material Components for the
    Web.
    
    Change-Id: I5282886888e3aeec6ce11ae3e335ac334bf0c476

--- bin/agents/mi_dashboard/webroot/dashboard.js
+++ bin/agents/mi_dashboard/webroot/dashboard.js
@@ -3,6 +3,8 @@ const MAX_RECONNECT_INTERVAL = 2000;
 
 var _webSocket = null;
 var _reconnectInterval = RECONNECT_INTERVAL;
+var _focusedStoryId = null;
+var _stories = {};
 var _toolbar = null;
 var _tabBar = null;
 
@@ -77,6 +79,8 @@ function handleWebSocketMessage(evt) {
 }
 
 function handleContextUpdate(context) {
+  updateOverview(context);
+
   $.each(context, function(topic, rawValue) {
     // make a pretty string for the topic's value
     var topicValue;
@@ -107,29 +111,48 @@ function handleContextUpdate(context) {
 }
 
 function handleContextSubscribers(subscribers) {
-  $("#contextSubscriptions").empty().append(subscribers.map(function(update) {
-    var component = $("<td/>").append($("<span/>").addClass("text-capitalize")
-      .text(update.subscriber.type));
+  $("#contextSubscriptions").empty();
+
+  subscribers.forEach(function(update) {
+    // Sample HTML to be created
+    // <li class="mdc-list-item">
+    //   <span class="mdc-list-item__text">
+    //     <span class="agent-url">file:///system/apps/agents/module_suggester</span>
+    //     <span class="mdc-list-item__text__secondary">raw/text</span>
+    //   </span>
+    // </li>
+    // <li class="mdc-list-divider" role="separator"></li>
+    var listText = $('<span/>').addClass('mdc-list-item__text');
+
+    var agentNameText = update.subscriber.type;
     if (update.subscriber.url) {
-      component.append(" ").append($("<span/>").text(update.subscriber.url));
+      agentNameText += ' ' + update.subscriber.url;
     }
     if (update.subscriber.storyId) {
-      component.append(" ").append($("<span/>")
-        .text("from story " + update.subscriber.storyId + ")"));
+      agentNameText += ' from story ' + update.subscriber.storyId;
     }
 
-    var queries = $("<td/>").append(update.queries.map(function(query) {
-      return $("<div/>").addClass("context-query")
-        .append(query.length == 0? "(all topics)" :
-          query.map(function(topic) {
-            return $("<span/>").addClass("context-topic").text(topic);
-          }));
+    listText.append($("<span/>").addClass("subscriber")
+                                .text(agentNameText));
+    listText.append(' ').append(update.queries.map(function(query) {
+      var querySpan = $('<span/>')
+        .addClass('mdc-list-item__text__secondary')
+        .addClass('context-topic');
+      if (query.length == 0) {
+        querySpan.append($('<span/>').text('(all topics)'));
+      } else {
+        query.forEach(function(topic) {
+          querySpan.append($('<span/>').text(topic))
+            .append(' ');
+        })
+      }
+      return querySpan;
     }));
 
-    return $("<tr/>")
-      .append(component)
-      .append(queries);
-  }));
+    $("#contextSubscriptions")
+      .append($('<li/>').addClass('mdc-list-item').append(listText))
+      .append($('<li/>').addClass('mdc-list-divider').attr('role','divider'));
+  });
 }
 
 function handleActionLogReset(actionList) {
@@ -139,13 +162,45 @@ function handleActionLogReset(actionList) {
 }
 
 function handleActionLogAdd(action) {
-  var methodElem = $("<td/>").text(action.method);
-  var componentUrlElem = $("<td/>").text(action.component_url);
-  var parametersElem = $("<td/>").text(action.parameters);
+  // <li class="mdc-list-item">
+  //   <span class="mdc-list-item__text">
+  //     Two-line item
+  //     <span class="mdc-list-item__text__secondary">Secondary text</span>
+  //   </span>
+  // </li>
 
-  $("#actionLog").prepend($("<tr/>").append(methodElem)
-                                    .append(componentUrlElem)
-                                    .append(parametersElem));
+  // Assemble Action Log Panel Data
+  var methodElem = $("<span/>")
+    .addClass('mdc-list-item__text')
+    .text(action.method);
+  var componentUrlElem = $("<span/>")
+    .addClass('mdc-list-item__text__secondary')
+    .text(action.component_url);
+  var parametersElem = $("<span/>")
+    .addClass('mdc-list-item__text__secondary')
+    .text(action.parameters);
+
+  methodElem.append(' ')
+    .append(componentUrlElem)
+    .append(' ')
+    .append(parametersElem);
+
+  $("#actionLog")
+    .prepend($('<li/>').addClass('mdc-list-item').append(methodElem))
+    .prepend($('<li/>').addClass('mdc-list-divider').attr('role','divider'));
+
+  // Assemble Action Log Overview Data
+  methodElem = $('<span/>')
+    .addClass('mdc-list-item__text')
+    .text(action.method);
+  parametersElem = $("<span/>")
+    .addClass('mdc-list-item__text__secondary')
+    .text(action.parameters);
+
+  methodElem.append(" ").append(parametersElem);
+  $('#actionLogOverview')
+    .prepend($('<li/>').addClass('mdc-list-item').append(methodElem))
+    .prepend($('<li/>').addClass('mdc-list-divider').attr('role','divider'));
 }
 
 function attemptReconnect() {
@@ -160,4 +215,105 @@ function attemptReconnect() {
   } else {
     _reconnectInterval = MAX_RECONNECT_INTERVAL;
   }
+}
+
+function updateOverview(context) {
+  $.each(context, function(topic, rawValue) {
+    // loop through the context updates and modify anything related
+    // on the overview panel
+    var storyRegex = /\/story\/id\/([^\/]+)\/(.+)/;
+    var storyRegexResults = topic.match(storyRegex);
+
+    if (storyRegexResults != null && storyRegexResults[1] != null) {
+      // this is a story-related topic
+      var storyId = storyRegexResults[1];
+      var overviewStoryElems = getOrCreateStoryOverviewElements(storyId);
+
+      switch(storyRegexResults[2]) {
+        case "url":
+          setStoryName(rawValue, overviewStoryElems);
+          break;
+
+        case "state":
+          setStoryState(rawValue, overviewStoryElems);
+          break;
+      }
+
+      // move this story to the top of the list
+      // TODO(jwnichols): Figure out the right thing to do here
+      var divider = overviewStoryElems.prev();
+      $('#story-overview-list')
+        .prepend(overviewStoryElems)
+        .prepend(divider);
+    } else if (topic == '/story/focused_id') {
+      if (_focusedStoryId != null) {
+        var oldFocusedStoryElems = getOrCreateStoryOverviewElements(_focusedStoryId);
+        setStoryVisible(false, oldFocusedStoryElems);
+      }
+      var newFocusedStoryId = JSON.parse(rawValue);
+      var newFocusedStoryElems = getOrCreateStoryOverviewElements(newFocusedStoryId);
+      setStoryVisible(true, newFocusedStoryElems);
+      _focusedStoryId = newFocusedStoryId;
+    } else if (topic == '/suggestion_engine/current_query') {
+      var query = rawValue;
+      if (query.length == 0) {
+        $('#askQueryOverview').empty().append($('<i/>').text('No Query'));
+      } else {
+        $('#askQueryOverview').empty().text(query);
+      }
+    }
+  });
+}
+
+function getOrCreateStoryOverviewElements(storyId) {
+  var storyElems = _stories[storyId];
+  if (storyElems == null) {
+    // we need to create the story elements
+    // <li id="b234kj2jn5j34342l3k3" class="mdc-list-item story-list-item story-visible">
+    //   <span class="story-name mdc-list-item__text">
+    //     Story Name
+    //     <span class="story-id mdc-list-item__text__secondary">
+    //       b234kj2jn5j34342l3k3
+    //     </span>
+    //   </span>
+    // </li>
+    var storyListItem = $('<li/>').attr('id',storyId)
+      .addClass('mdc-list-item')
+      .addClass('story-list-item');
+
+    var storyIdElem = $('<span/>').addClass('story-id')
+      .addClass('mdc-list-item__text__secondary')
+      .text(storyId);
+
+    var storyNameElem = $('<span/>').addClass('story-name')
+      .addClass('mdc-list-item__text')
+      .text('Story Name')
+      .append(' ')
+      .append(storyIdElem);
+
+    storyListItem.append(storyNameElem);
+
+    $('#story-overview-list')
+      .append($('<li/>').addClass('mdc-list-divider').attr('role','divider'))
+      .append(storyListItem);
+
+    storyElems = _stories[storyId] = storyListItem;
+  }
+  return storyElems;
+}
+
+function setStoryName(storyName, storyElems) {
+  storyElems.find('span')[0].firstChild.textContent = storyName;
+}
+
+function setStoryVisible(storyVisible, storyElems) {
+  if (storyVisible) {
+    storyElems.addClass('story-visible');
+  } else {
+    storyElems.removeClass('story-visible');
+  }
+}
+
+function setStoryState(storyState, storyElems) {
+  // TODO(jwnichols): Add story state to the overview
 }
