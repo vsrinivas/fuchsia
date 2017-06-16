cd2231da46173b5965f40fd2a19ab45dbb2cd99c
Jocelyn Dang <jocelyndang@google.com>
2017-06-16 10:19:58 -0700

    [hypervisor] Test MX_HYPERVISOR_OP_GUEST_{GET,SET}_GPR
    MG-824
    
    Change-Id: I2bc5ef8f04a889c3cf163ca5205921c64983d574

--- system/utest/hypervisor/guest.c
+++ system/utest/hypervisor/guest.c
@@ -14,52 +14,97 @@ static const uint64_t kVmoSize = 2 << 20;
 extern const char guest_start[];
 extern const char guest_end[];
 
-static bool guest_enter(void) {
-    BEGIN_TEST;
+extern const char guest_set_gpr_start[];
+extern const char guest_set_gpr_end[];
+
+typedef struct test {
+    bool supported;
 
     mx_handle_t hypervisor;
-    mx_status_t status = mx_hypervisor_create(MX_HANDLE_INVALID, 0, &hypervisor);
-    // The hypervisor isn't supported, so don't run the test.
-    if (status == MX_ERR_NOT_SUPPORTED)
+    mx_handle_t guest_phys_mem;
+    mx_handle_t guest_ctl_fifo;
+    mx_handle_t guest;
+
+#if __x86_64__
+    mx_handle_t guest_apic_mem;
+#endif // __x86_64__
+} test_t;
+
+
+static bool setup(test_t *test, const char *start, const char *end) {
+    mx_status_t status = mx_hypervisor_create(MX_HANDLE_INVALID, 0,
+                                              &test->hypervisor);
+    test->supported = status != MX_ERR_NOT_SUPPORTED;
+    if (!test->supported) {
         return true;
+    }
     ASSERT_EQ(status, MX_OK, "");
 
     uintptr_t addr;
-    mx_handle_t guest_phys_mem;
-    ASSERT_EQ(guest_create_phys_mem(&addr, kVmoSize, &guest_phys_mem), MX_OK, "");
+    ASSERT_EQ(guest_create_phys_mem(&addr, kVmoSize, &test->guest_phys_mem),
+              MX_OK, "");
 
-    mx_handle_t guest_ctl_fifo;
-    mx_handle_t guest;
-    ASSERT_EQ(guest_create(hypervisor, guest_phys_mem, &guest_ctl_fifo, &guest), MX_OK, "");
+    ASSERT_EQ(guest_create(test->hypervisor, test->guest_phys_mem,
+                           &test->guest_ctl_fifo, &test->guest), MX_OK, "");
 
     // Setup the guest.
     uintptr_t guest_ip = 0;
-    ASSERT_EQ(guest_create_page_table(addr, kVmoSize, &guest_ip), MX_OK, "");
+    ASSERT_EQ(guest_create_page_table(addr, kVmoSize, &guest_ip),
+              MX_OK, "");
 
 #if __x86_64__
-    memcpy((void*)(addr + guest_ip), guest_start, guest_end - guest_start);
+    memcpy((void*)(addr + guest_ip), start, end - start);
     uintptr_t guest_cr3 = 0;
-    ASSERT_EQ(mx_hypervisor_op(guest, MX_HYPERVISOR_OP_GUEST_SET_ENTRY_CR3,
+    ASSERT_EQ(mx_hypervisor_op(test->guest, MX_HYPERVISOR_OP_GUEST_SET_ENTRY_CR3,
                                &guest_cr3, sizeof(guest_cr3), NULL, 0),
               MX_OK, "");
 
-    mx_handle_t guest_apic_mem;
-    ASSERT_EQ(mx_vmo_create(PAGE_SIZE, 0, &guest_apic_mem), MX_OK, "");
-    ASSERT_EQ(mx_hypervisor_op(guest, MX_HYPERVISOR_OP_GUEST_SET_APIC_MEM,
-                               &guest_apic_mem, sizeof(guest_apic_mem), NULL, 0),
+    ASSERT_EQ(mx_vmo_create(PAGE_SIZE, 0, &test->guest_apic_mem), MX_OK, "");
+    ASSERT_EQ(mx_hypervisor_op(test->guest, MX_HYPERVISOR_OP_GUEST_SET_APIC_MEM,
+                               &test->guest_apic_mem,
+                               sizeof(test->guest_apic_mem), NULL, 0),
               MX_OK, "");
 #endif // __x86_64__
 
-    // Enter the guest.
-    ASSERT_EQ(mx_hypervisor_op(guest, MX_HYPERVISOR_OP_GUEST_SET_ENTRY_IP,
+    ASSERT_EQ(mx_hypervisor_op(test->guest, MX_HYPERVISOR_OP_GUEST_SET_ENTRY_IP,
                                &guest_ip, sizeof(guest_ip), NULL, 0),
               MX_OK, "");
-    ASSERT_EQ(mx_hypervisor_op(guest, MX_HYPERVISOR_OP_GUEST_ENTER, NULL, 0, NULL, 0),
+
+    return true;
+}
+
+static bool teardown(test_t *test) {
+    ASSERT_EQ(mx_handle_close(test->guest), MX_OK, "");
+    ASSERT_EQ(mx_handle_close(test->guest_ctl_fifo), MX_OK, "");
+    ASSERT_EQ(mx_handle_close(test->guest_phys_mem), MX_OK, "");
+    ASSERT_EQ(mx_handle_close(test->hypervisor), MX_OK, "");
+
+#if __x86_64__
+    ASSERT_EQ(mx_handle_close(test->guest_apic_mem), MX_OK, "");
+#endif // __x86_64__
+
+    return true;
+}
+
+static bool guest_enter(void) {
+    BEGIN_TEST;
+
+    test_t test;
+    ASSERT_TRUE(setup(&test, guest_start, guest_end), "");
+    if (!test.supported) {
+        // The hypervisor isn't supported, so don't run the test.
+        return true;
+    }
+
+    // Enter the guest.
+    ASSERT_EQ(mx_hypervisor_op(test.guest, MX_HYPERVISOR_OP_GUEST_ENTER,
+                               NULL, 0, NULL, 0),
               MX_ERR_STOP, "");
 
     mx_guest_packet_t packet[2];
     uint32_t num_packets;
-    ASSERT_EQ(mx_fifo_read(guest_ctl_fifo, packet, sizeof(packet), &num_packets), MX_OK, "");
+    ASSERT_EQ(mx_fifo_read(test.guest_ctl_fifo, packet, sizeof(packet), &num_packets),
+              MX_OK, "");
     ASSERT_EQ(num_packets, 2u, "");
     ASSERT_EQ(packet[0].type, MX_GUEST_PKT_TYPE_PORT_OUT, "");
     ASSERT_EQ(packet[0].port_out.access_size, 1u, "");
@@ -68,20 +113,84 @@ static bool guest_enter(void) {
     ASSERT_EQ(packet[1].port_out.access_size, 1u, "");
     ASSERT_EQ(packet[1].port_out.data[0], 'x', "");
 
-    ASSERT_EQ(mx_handle_close(guest), MX_OK, "");
-    ASSERT_EQ(mx_handle_close(guest_ctl_fifo), MX_OK, "");
-    ASSERT_EQ(mx_handle_close(guest_phys_mem), MX_OK, "");
-    ASSERT_EQ(mx_handle_close(hypervisor), MX_OK, "");
+    ASSERT_TRUE(teardown(&test), "");
+
+    END_TEST;
+}
+
+static bool guest_get_set_gpr(void) {
+    BEGIN_TEST;
+
+    test_t test;
+    ASSERT_TRUE(setup(&test, guest_set_gpr_start, guest_set_gpr_end), "");
+    if (!test.supported) {
+        // The hypervisor isn't supported, so don't run the test.
+        return true;
+    }
+
+    mx_guest_gpr_t guest_gpr = {
+#if __x86_64__
+        .rax = 1u,
+        .rcx = 2u,
+        .rdx = 3u,
+        .rbx = 4u,
+        .rsp = 5u,
+        .rbp = 6u,
+        .rsi = 7u,
+        .rdi = 8u,
+        .r8 = 9u,
+        .r9 = 10u,
+        .r10 = 11u,
+        .r11 = 12u,
+        .r12 = 13u,
+        .r13 = 14u,
+        .r14 = 15u,
+        .r15 = 16u,
+        .flags = 0
+#endif // __x86_64__
+    };
+
+    ASSERT_EQ(mx_hypervisor_op(test.guest, MX_HYPERVISOR_OP_GUEST_SET_GPR,
+                               &guest_gpr, sizeof(guest_gpr), NULL, 0),
+              MX_OK, "");
+
+    // Enter the guest.
+    ASSERT_EQ(mx_hypervisor_op(test.guest, MX_HYPERVISOR_OP_GUEST_ENTER,
+                               NULL, 0, NULL, 0),
+              MX_ERR_STOP, "");
+
+    ASSERT_EQ(mx_hypervisor_op(test.guest, MX_HYPERVISOR_OP_GUEST_GET_GPR,
+                               NULL, 0, &guest_gpr, sizeof(guest_gpr)),
+              MX_OK, "");
 
 #if __x86_64__
-    ASSERT_EQ(mx_handle_close(guest_apic_mem), MX_OK, "");
+    ASSERT_EQ(guest_gpr.rax, 2u, "");
+    ASSERT_EQ(guest_gpr.rcx, 4u, "");
+    ASSERT_EQ(guest_gpr.rdx, 6u, "");
+    ASSERT_EQ(guest_gpr.rbx, 8u, "");
+    ASSERT_EQ(guest_gpr.rsp, 10u, "");
+    ASSERT_EQ(guest_gpr.rbp, 12u, "");
+    ASSERT_EQ(guest_gpr.rsi, 14u, "");
+    ASSERT_EQ(guest_gpr.rdi, 16u, "");
+    ASSERT_EQ(guest_gpr.r8, 18u, "");
+    ASSERT_EQ(guest_gpr.r9, 20u, "");
+    ASSERT_EQ(guest_gpr.r10, 22u, "");
+    ASSERT_EQ(guest_gpr.r11, 24u, "");
+    ASSERT_EQ(guest_gpr.r12, 26u, "");
+    ASSERT_EQ(guest_gpr.r13, 28u, "");
+    ASSERT_EQ(guest_gpr.r14, 30u, "");
+    ASSERT_EQ(guest_gpr.r15, 32u, "");
+    ASSERT_EQ(guest_gpr.flags, 1u, "");
 #endif // __x86_64__
+
+    ASSERT_TRUE(teardown(&test), "");
 
     END_TEST;
 }
 
 BEGIN_TEST_CASE(guest)
 RUN_TEST(guest_enter)
+RUN_TEST(guest_get_set_gpr)
 END_TEST_CASE(guest)
 
 #ifndef BUILD_COMBINED_TESTS
