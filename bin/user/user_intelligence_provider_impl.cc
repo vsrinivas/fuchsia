ea72e4e59773a8116dde9660706fb36c00678e0b
David Yang <dayang@google.com>
2017-06-19 08:26:49 -0700

    Adding Last FM Agent
    Change-Id: Id5fb1489159c4f62d2b45d300b17c87b699d6f55

--- bin/user/user_intelligence_provider_impl.cc
+++ bin/user/user_intelligence_provider_impl.cc
@@ -94,6 +94,7 @@ UserIntelligenceProviderImpl::UserIntelligenceProviderImpl(
   StartAgent("file:///system/apps/agents/module_suggester.dartx");
   StartAgent("file:///system/apps/concert_agent");
   StartAgent("file:///system/apps/music_artist_agent");
+  StartAgent("file:///system/apps/last_fm_agent");
   StartAgent("file:///system/apps/agents/maxwell_btl");
   StartAgent("file:///system/apps/agents/maxwell_entity_focuser");
   StartAgent("file:///system/apps/agents/maxwell_proposal_maker");
