a96a90ee11256309d9b8f3235bf018704b80963d
Sean Klein <smklein@google.com>
2017-06-19 15:59:11 -0700

    [ddktl] Add support for block protocol to ddktl
    Change-Id: Ide0be30a78969c842a86acff3a01a1988c6c5b51

---
+++ system/ulib/ddk/include/ddktl/protocol/block-internal.h
@@ -0,0 +1,59 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <ddktl/device-internal.h>
+#include <magenta/types.h>
+#include <mxtl/type_support.h>
+#include <mxtl/unique_ptr.h>
+
+#include <stdint.h>
+
+namespace ddk {
+namespace internal {
+
+DECLARE_HAS_MEMBER_FN(has_block_set_callbacks, BlockSetCallbacks);
+DECLARE_HAS_MEMBER_FN(has_block_get_info, BlockGetInfo);
+DECLARE_HAS_MEMBER_FN(has_block_read, BlockRead);
+DECLARE_HAS_MEMBER_FN(has_block_write, BlockWrite);
+
+template <typename D>
+constexpr void CheckBlockProtocolSubclass() {
+    static_assert(internal::has_block_set_callbacks<D>::value,
+                  "BlockProtocol subclasses must implement BlockSetCallbacks");
+    static_assert(mxtl::is_same<decltype(&D::BlockSetCallbacks),
+                                void (D::*)(block_callbacks_t*)>::value,
+                  "BlockSetCallbacks must be a non-static member function with signature "
+                  "'void BlockSetCallbacks(block_callbacks_t* cb)', and be visible to "
+                  "ddk::BlockProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+    static_assert(internal::has_block_get_info<D>::value,
+                  "BlockProtocol subclasses must implement BlockGetInfo");
+    static_assert(mxtl::is_same<decltype(&D::BlockGetInfo),
+                                void (D::*)(block_info_t*)>::value,
+                  "BlockStop must be a non-static member function with signature "
+                  "'void BlockGetInfo(block_info_t* info)', and be visible to "
+                  "ddk::BlockProtocol<D> (either because they are public, or because "
+                  "of friendship).");
+    static_assert(internal::has_block_read<D>::value,
+                  "BlockProtocol subclasses must implement BlockRead");
+    static_assert(mxtl::is_same<decltype(&D::BlockRead),
+                                void (D::*)(mx_handle_t, uint64_t, uint64_t, uint64_t, void*)>::value,
+                  "BlockRead must be a non-static member function with signature "
+                  "'void BlockRead(mx_handle_t, uint64_t, uint64_t, uint64_t, void*)', and be "
+                  "visible to ddk::BlockProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+    static_assert(internal::has_block_write<D>::value,
+                  "BlockProtocol subclasses must implement BlockWrite");
+    static_assert(mxtl::is_same<decltype(&D::BlockWrite),
+                                void (D::*)(mx_handle_t, uint64_t, uint64_t, uint64_t, void*)>::value,
+                  "BlockWrite must be a non-static member function with signature "
+                  "'void BlockWrite(mx_handle_t, uint64_t, uint64_t, uint64_t, void*)', and be "
+                  "visible to ddk::BlockProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+}
+
+}  // namespace internal
+}  // namespace ddk
---
+++ system/ulib/ddk/include/ddktl/protocol/block.h
@@ -0,0 +1,90 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <ddk/protocol/block.h>
+#include <ddktl/protocol/block-internal.h>
+#include <magenta/assert.h>
+#include <mxtl/type_support.h>
+#include <mxtl/unique_ptr.h>
+
+// DDK block protocol support
+//
+// :: Mixins ::
+//
+// ddk::BlockIfc and ddk::BlockProtocol are mixin classes that simplify writing DDK drivers that
+// interact with the block protocol. They take care of implementing the function pointer tables
+// and calling into the object that wraps them.
+//
+// :: Examples ::
+//
+// // A driver that implements a MX_PROTOCOL_BLOCK_CORE device
+// class BlockDevice;
+// using BlockDeviceType = ddk::Device<BlockDevice, /* ddk mixins */>;
+//
+// class BlockDevice : public BlockDeviceType,
+//                      public ddk::BlockProtocol<BlockDevice> {
+//   public:
+//     BlockDevice(mx_device_t* parent)
+//       : BlockDeviceType("my-block-device", parent) {}
+//
+//     mx_status_t Bind() {
+//         DdkAdd();
+//     }
+//
+//     void DdkRelease() {
+//         // Clean up
+//     }
+//
+//     void BlockSetCallbacks(block_callbacks_t* cb) {
+//         // Fill out callbacks
+//     }
+//
+//     ...
+//   private:
+//     ...
+// };
+
+namespace ddk {
+
+template <typename D>
+class BlockProtocol : public internal::base_protocol {
+  public:
+    BlockProtocol() {
+        internal::CheckBlockProtocolSubclass<D>();
+        ops_.set_callbacks = SetCallbacks;
+        ops_.get_info = GetInfo;
+        ops_.read = Read;
+        ops_.write = Write;
+
+        // Can only inherit from one base_protocol implemenation
+        MX_ASSERT(ddk_proto_ops_ == nullptr);
+        ddk_proto_id_ = MX_PROTOCOL_BLOCK_CORE;
+        ddk_proto_ops_ = &ops_;
+    }
+
+  private:
+    static void SetCallbacks(void* ctx, block_callbacks_t* cb) {
+        static_cast<D*>(ctx)->BlockSetCallbacks(cb);
+    }
+
+    static void GetInfo(void* ctx, block_info_t* info) {
+        static_cast<D*>(ctx)->BlockGetInfo(info);
+    }
+
+    static void Read(void* ctx, mx_handle_t vmo, uint64_t length, uint64_t vmo_offset,
+                     uint64_t dev_offset, void* cookie) {
+        static_cast<D*>(ctx)->BlockRead(vmo, length, vmo_offset, dev_offset, cookie);
+    }
+
+    static void Write(void* ctx, mx_handle_t vmo, uint64_t length, uint64_t vmo_offset,
+                      uint64_t dev_offset, void* cookie) {
+        static_cast<D*>(ctx)->BlockWrite(vmo, length, vmo_offset, dev_offset, cookie);
+    }
+
+    block_protocol_ops_t ops_ = {};
+};
+
+}  // namespace ddk
