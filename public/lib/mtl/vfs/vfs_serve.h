1ae4864fc0a88b2946f60a5297ccc5cc69e496fb
Adam Barth <abarth@chromium.org>
2017-06-20 13:20:16 -0700

    Add mtl::VFSServe
    This code is repeated in a few places and is slightly subtle.
    
    Change-Id: I4a6baa1a27494cbfe030895d102148a3df4d5497

---
+++ public/lib/mtl/vfs/vfs_serve.h
@@ -0,0 +1,17 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_MTL_VFS_VFS_SERVE_H_
+#define LIB_MTL_VFS_VFS_SERVE_H_
+
+#include <fs/vfs.h>
+#include <mx/channel.h>
+
+namespace mtl {
+
+bool VFSServe(mxtl::RefPtr<fs::Vnode> directory, mx::channel request);
+
+}  // namespace mtl
+
+#endif  // LIB_MTL_VFS_VFS_SERVE_H_
