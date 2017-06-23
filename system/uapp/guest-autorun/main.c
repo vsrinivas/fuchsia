a41a12f47be17f793885dbae044e1c65da626b25
Abdulla Kamar <abdulla@google.com>
2017-06-23 23:25:44 +1000

    [uapp] Add guest-autorun app.
    This app is used as an interim solution to test interaction between
    the hypervisor and a Magenta guest, until we have the ability to
    interact with the guest directly.
    
    I'll be adding more compile-time configurable interactions into this.
    
    Change-Id: I86560f8a254be769656c1417bfef583e3beaf6a2

---
+++ system/uapp/guest-autorun/main.c
@@ -0,0 +1,40 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fcntl.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <magenta/syscalls.h>
+
+const char path[] = "/dev/class/block/000";
+uint8_t buf[PAGE_SIZE];
+
+int main(int argc, char** argv) {
+    int fd;
+    while ((fd = open(path, O_RDWR)) < 0) {
+        mx_status_t status = mx_deadline_after(MX_SEC(1));
+        if (status != MX_OK) {
+            fprintf(stderr, "Failed to sleep\n");
+            return status;
+        }
+    }
+
+    int ret = read(fd, buf, PAGE_SIZE);
+    if (ret != PAGE_SIZE) {
+        fprintf(stderr, "Failed to read a page from \"%s\"\n", path);
+        return MX_ERR_IO;
+    }
+
+    ret = write(fd, buf, PAGE_SIZE);
+    if (ret != PAGE_SIZE) {
+        fprintf(stderr, "Failed to write a page to \"%s\"\n", path);
+        return MX_ERR_IO;
+    }
+
+    fprintf(stderr, "Completed transactions on \"%s\"\n", path);
+    return MX_OK;
+}
---
+++ system/uapp/guest-autorun/rules.mk
@@ -0,0 +1,19 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userapp
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/main.c
+
+MODULE_LIBS := \
+    system/ulib/c \
+    system/ulib/magenta \
+    system/ulib/mxio \
+
+include make/module.mk
