56838f4a5399e2ebb5d13da199cc691aa4de0121
Steffen Meschkat <mesch@google.com>
2017-06-25 00:33:53 +0200

    Test for context links.
    Context links are implemented in
    https://fuchsia-review.googlesource.com/#/c/36165/.
    
    Test runs as user shell. The user shell starts a module that posts
    a @context link, and the user shell registers a context listener
    that picks up the resulting context topic.
    
    TODO in followup: If the context listener is registered too early,
    the context listener connection closes and no context updates are
    received. Laster registration works; marked as HACK().
    
    FW-219
    
    Change-Id: I183e469fa8d5b6368486327aeb39d316726e07e2

---
+++ tests/context_link/context_link_module.cc
@@ -0,0 +1,63 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "apps/modular/lib/testing/component_base.h"
+#include "apps/modular/lib/testing/reporting.h"
+#include "apps/modular/lib/testing/testing.h"
+#include "apps/modular/services/module/module.fidl.h"
+#include "apps/modular/services/story/link.fidl.h"
+#include "apps/mozart/services/views/view_token.fidl.h"
+#include "lib/mtl/tasks/message_loop.h"
+
+using modular::testing::TestPoint;
+
+namespace {
+
+constexpr char kLink[] = "context_link";
+
+class TestApp : modular::testing::ComponentBase<modular::Module> {
+ public:
+  static void New() {
+    new TestApp;  // deleted in Stop()
+  }
+
+ private:
+  TestApp() { TestInit(__FILE__); }
+  ~TestApp() override = default;
+
+  TestPoint initialized_{"Child module initialized"};
+
+  // |Module|
+  void Initialize(
+      fidl::InterfaceHandle<modular::ModuleContext> module_context,
+      fidl::InterfaceHandle<app::ServiceProvider> incoming_services,
+      fidl::InterfaceRequest<app::ServiceProvider> outgoing_services) override {
+    module_context_.Bind(std::move(module_context));
+    initialized_.Pass();
+
+    module_context_->GetLink(kLink, link_.NewRequest());
+
+    link_->Set(nullptr, "{\"@context\":{\"topic\":\"/context_link_test\"}}");
+  }
+
+  TestPoint stopped_{"Child module stopped"};
+
+  // |Module|
+  void Stop(const StopCallback& done) override {
+    stopped_.Pass();
+    DeleteAndQuit(done);
+  }
+
+  modular::ModuleContextPtr module_context_;
+  modular::LinkPtr link_;
+};
+
+}  // namespace
+
+int main(int argc, const char** argv) {
+  mtl::MessageLoop loop;
+  TestApp::New();
+  loop.Run();
+  return 0;
+}
---
+++ tests/context_link/context_link_user_shell.cc
@@ -0,0 +1,279 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <string>
+
+#include "application/lib/app/connect.h"
+#include "application/services/service_provider.fidl.h"
+#include "apps/maxwell/services/context/context_provider.fidl.h"
+#include "apps/maxwell/services/context/context_publisher.fidl.h"
+#include "apps/modular/lib/fidl/array_to_string.h"
+#include "apps/modular/lib/fidl/single_service_view_app.h"
+#include "apps/modular/lib/fidl/view_host.h"
+#include "apps/modular/lib/testing/reporting.h"
+#include "apps/modular/lib/testing/testing.h"
+#include "apps/modular/lib/rapidjson/rapidjson.h"
+#include "apps/modular/services/user/focus.fidl.h"
+#include "apps/modular/services/user/user_context.fidl.h"
+#include "apps/modular/services/user/user_shell.fidl.h"
+#include "apps/mozart/lib/view_framework/base_view.h"
+#include "apps/mozart/services/views/view_manager.fidl.h"
+#include "apps/mozart/services/views/view_provider.fidl.h"
+#include "apps/test_runner/services/test_runner.fidl.h"
+#include "lib/fidl/cpp/bindings/binding.h"
+#include "lib/ftl/command_line.h"
+#include "lib/ftl/functional/make_copyable.h"
+#include "lib/ftl/logging.h"
+#include "lib/ftl/macros.h"
+#include "lib/ftl/tasks/task_runner.h"
+#include "lib/ftl/time/time_delta.h"
+#include "lib/mtl/tasks/message_loop.h"
+
+namespace {
+
+constexpr char kModuleUrl[] = "file:///system/apps/modular_tests/context_link_module";
+constexpr char kTopic[] = "/context_link_test";
+constexpr char kLink[] = "context_link";
+
+// A context provider watcher implementation.
+class ContextListenerImpl : maxwell::ContextListener {
+ public:
+  ContextListenerImpl() : binding_(this) {
+    handler_ = [](fidl::String, fidl::String) {};
+  }
+
+  ~ContextListenerImpl() override = default;
+
+  // Registers itself a watcher on the given story provider. Only one story
+  // provider can be watched at a time.
+  void Listen(maxwell::ContextProvider* const context_provider) {
+    auto query = maxwell::ContextQuery::New();
+    query->topics.resize(0);
+    context_provider->Subscribe(std::move(query), binding_.NewBinding());
+    binding_.set_connection_error_handler(
+        [] { FTL_LOG(WARNING) << "Lost connection to ContextProvider."; });
+  }
+
+  using Handler = std::function<void(fidl::String, fidl::String)>;
+
+  void Handle(const Handler& handler) { handler_ = handler; }
+
+  // Deregisters itself from the watched story provider.
+  void Reset() { binding_.Close(); }
+
+ private:
+  // |ContextListener|
+  void OnUpdate(maxwell::ContextUpdatePtr update) override {
+    FTL_LOG(INFO) << "ContextListenerImpl::OnUpdate()";
+    const auto& values = update->values;
+    for (auto i = values.cbegin(); i != values.cend(); ++i) {
+      FTL_LOG(INFO) << "ContextListenerImpl::OnUpdate() " << i.GetKey() << " "
+                    << i.GetValue();
+      handler_(i.GetKey(), i.GetValue());
+    }
+  }
+
+  fidl::Binding<maxwell::ContextListener> binding_;
+  Handler handler_;
+  FTL_DISALLOW_COPY_AND_ASSIGN(ContextListenerImpl);
+};
+
+// Tests the context links machinery. We start a module that writes a context
+// link and listen for the expected context topic to show up.
+class TestApp : modular::SingleServiceViewApp<modular::UserShell> {
+ public:
+  // The app instance must be dynamic, because it needs to do several things
+  // after its own constructor is invoked. It accomplishes that by being able to
+  // call delete this. Cf. Terminate().
+  static void New() {
+    new TestApp();  // will delete itself in Terminate().
+  }
+
+ private:
+  TestApp() { modular::testing::Init(application_context(), __FILE__); }
+
+  ~TestApp() override = default;
+
+  using TestPoint = modular::testing::TestPoint;
+
+  TestPoint create_view_{"CreateView()"};
+
+  // |SingleServiceViewApp|
+  void CreateView(
+      fidl::InterfaceRequest<mozart::ViewOwner> view_owner_request,
+      fidl::InterfaceRequest<app::ServiceProvider> services) override {
+    create_view_.Pass();
+    view_.reset(new modular::ViewHost(
+        application_context()
+            ->ConnectToEnvironmentService<mozart::ViewManager>(),
+        std::move(view_owner_request)));
+  }
+
+  TestPoint initialize_{"Initialize()"};
+
+  // |UserShell|
+  void Initialize(fidl::InterfaceHandle<modular::UserContext> user_context,
+                  fidl::InterfaceHandle<modular::UserShellContext>
+                      user_shell_context) override {
+    initialize_.Pass();
+
+    user_context_.Bind(std::move(user_context));
+    user_shell_context_.Bind(std::move(user_shell_context));
+
+    user_shell_context_->GetStoryProvider(story_provider_.NewRequest());
+
+    // HACK(mesch): If done here, context provider connection just closes.
+    //user_shell_context_->GetContextProvider(context_provider_.NewRequest());
+    //context_listener_.Listen(context_provider_.get());
+
+    CreateStory();
+  }
+
+  TestPoint create_story_{"CreateStory()"};
+
+  void CreateStory() {
+    story_provider_->CreateStory(kModuleUrl,
+                                 [this](const fidl::String& story_id) {
+                                   story_id_ = story_id;
+                                   create_story_.Pass();
+                                   StartStory();
+                                 });
+  }
+
+  TestPoint start_story_enter_{"StartStory() Enter"};
+  TestPoint start_story_exit_{"StartStory() Exit"};
+
+  void StartStory() {
+    start_story_enter_.Pass();
+
+    user_shell_context_->GetContextProvider(context_provider_.NewRequest());
+    context_listener_.Listen(context_provider_.get());
+    context_listener_.Handle(
+        [this](const fidl::String& key, const fidl::String& value) {
+          GetContextTopic(key, value);
+        });
+
+    story_provider_->GetController(story_id_, story_controller_.NewRequest());
+
+    // Start and show the new story.
+    fidl::InterfaceHandle<mozart::ViewOwner> story_view;
+    story_controller_->Start(story_view.NewRequest());
+    view_->ConnectView(std::move(story_view));
+
+    start_story_exit_.Pass();
+  }
+
+  TestPoint get_context_topic_{"GetContextTopic()"};
+
+  void GetContextTopic(const fidl::String& topic, const fidl::String& value) {
+    if (topic != kTopic) {
+      return;
+    }
+
+    modular::JsonDoc doc;
+    doc.Parse(value);
+
+    if (doc.HasParseError()) {
+      FTL_LOG(ERROR) << "JSON Parse Error";
+      Logout();
+      return;
+    }
+
+    if (!doc.IsObject()) {
+      FTL_LOG(ERROR) << "JSON not an Object";
+      Logout();
+      return;
+    }
+
+    if (!doc.HasMember("@source")) {
+      FTL_LOG(ERROR) << "JSON missing @source";
+      Logout();
+      return;
+    }
+
+    if (!doc["@source"].IsObject()) {
+      FTL_LOG(ERROR) << "JSON @source not an Object";
+      Logout();
+      return;
+    }
+
+    if (!doc["@source"].HasMember("link_name")) {
+      FTL_LOG(ERROR) << "JSON @source missing link_name";
+      Logout();
+      return;
+    }
+
+    if (!doc["@source"]["link_name"].IsString()) {
+      FTL_LOG(ERROR) << "JSON @source link_name not a string";
+      Logout();
+      return;
+    }
+
+    // HACK(mesch): Comparing GetString() to kLink always fails.
+    const std::string link_name{doc["@source"]["link_name"].GetString()};
+    if (link_name != std::string{kLink}) {
+      FTL_LOG(ERROR) << "JSON @source wrong link_name " << link_name;
+      Logout();
+      return;
+    }
+
+    get_context_topic_.Pass();
+
+    context_listener_.Reset();
+    context_listener_.Handle(
+        [this](const fidl::String& key, const fidl::String& value) {
+        });
+
+    Logout();
+  }
+
+  void Logout() { user_context_->Logout(); }
+
+  TestPoint terminate_{"Terminate()"};
+
+  // |UserShell|
+  void Terminate(const TerminateCallback& done) override {
+    terminate_.Pass();
+
+    // A little acrobatics to allow TestPoints, which are data members of this,
+    // to post failure notices to the test runner in their destructors.
+    //
+    // We respond to done first, then asynchronously delete this, then
+    // asynchronously post Teardown() to the test runner, and finally
+    // asynchronously stop the message queue.
+
+    auto binding = PassBinding();  // To invoke done() after delete this.
+
+    delete this;
+
+    modular::testing::Done([done] {
+      done();
+      mtl::MessageLoop::GetCurrent()->PostQuitTask();
+    });
+  }
+
+  std::unique_ptr<modular::ViewHost> view_;
+
+  modular::UserContextPtr user_context_;
+  modular::UserShellContextPtr user_shell_context_;
+  modular::StoryProviderPtr story_provider_;
+
+  fidl::String story_id_;
+  modular::StoryControllerPtr story_controller_;
+
+  maxwell::ContextProviderPtr context_provider_;
+  ContextListenerImpl context_listener_;
+
+  FTL_DISALLOW_COPY_AND_ASSIGN(TestApp);
+};
+
+}  // namespace
+
+int main(int argc, const char** argv) {
+  mtl::MessageLoop loop;
+  TestApp::New();
+  loop.Run();
+  return 0;
+}
