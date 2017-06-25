6a0ae3c71d63e793b49a3ce0480d12756e2090f7
Doug Evans <dje@google.com>
2017-06-25 11:29:22 -0700

    [kernel][assembler] Add .cfi_startproc, .cfi_endproc directives
    ... and switch to using .debug_frame from .eh_frame.
    
    Some functions don't really need .cfi directives, e.g. during early boot.
    These functions are marked with FUNCTION_LABEL instead of FUNCTION
    as now all uses of FUNCTION require a corresponding END_FUNCTION.
    
    MG-870 #comment baby steps
    
    Change-Id: I3c7d182ca91b2f705100880282477bfe930c6633

--- kernel/include/asm.h
+++ kernel/include/asm.h
@@ -8,14 +8,28 @@
 #ifndef __ASM_H
 #define __ASM_H
 
-#define FUNCTION(x) .global x; .type x,STT_FUNC; x:
-#define DATA(x) .global x; .type x,STT_OBJECT; x:
-
-#define LOCAL_FUNCTION(x) .type x,STT_FUNC; x:
-#define LOCAL_DATA(x) .type x,STT_OBJECT; x:
-
-#define END_FUNCTION(x) .size x, . - x
-#define END_DATA(x) .size x, . - x
-
+#ifndef ASSEMBLY
+#error for assembly files only
 #endif
 
+// for functions that don't have an "end" or don't want .cfi_startproc
+#define LOCAL_FUNCTION_LABEL(x) .type x,STT_FUNC; x:
+#define FUNCTION_LABEL(x) .global x; LOCAL_FUNCTION_LABEL(x)
+
+#define LOCAL_FUNCTION(x) LOCAL_FUNCTION_LABEL(x) .cfi_startproc
+#define FUNCTION(x) .global x; LOCAL_FUNCTION(x)
+
+// for local or global functions
+#define END_FUNCTION(x) .cfi_endproc; .size x, . - x
+
+#define LOCAL_DATA(x) .type x,STT_OBJECT; x:
+#define DATA(x) .global x; LOCAL_DATA(x)
+
+// for local or global data
+#define END_DATA(x) .size x, . - x
+
+// We want .debug_frame not .eh_frame.
+// WARNING: This is a subtle side-effect of including this file. Heads up!
+.cfi_sections .debug_frame
+
+#endif
