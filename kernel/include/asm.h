d5343a0fdf95f659e6b561b42c6073da862eca7a
Doug Evans <dje@google.com>
2017-06-25 11:10:43 -0700

    [kernel][x86] Add .cfi directives to assembly
    MG-870 #comment x86 portion
    
    Change-Id: I9a09429eeeadb3c764ef65d4869a854f74c3a30b

--- kernel/include/asm.h
+++ kernel/include/asm.h
@@ -12,6 +12,8 @@
 #error for assembly files only
 #endif
 
+#include <arch/asm_macros.h>
+
 // for functions that don't have an "end" or don't want .cfi_startproc
 #define LOCAL_FUNCTION_LABEL(x) .type x,STT_FUNC; x:
 #define FUNCTION_LABEL(x) .global x; LOCAL_FUNCTION_LABEL(x)
@@ -27,6 +29,14 @@
 
 // for local or global data
 #define END_DATA(x) .size x, . - x
+
+// Some DWARF constants for CFI description.
+#define DW_CFA_val_expression 0x16
+#define DW_OP_lit0            0x30
+
+.macro cfi_register_is_zero reg
+    .cfi_escape DW_CFA_val_expression, \reg, 1, DW_OP_lit0
+.endm
 
 // We want .debug_frame not .eh_frame.
 // WARNING: This is a subtle side-effect of including this file. Heads up!
