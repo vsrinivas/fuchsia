32cb7d776e4c90d98bcb9190c9f2039d7eb6f4d0
Tim Kilbourn <tkilbourn@google.com>
2017-06-26 13:16:07 -0700

    [ddktl] Move ddktl to its own ulib
    Change-Id: I486b6c7fbca906287550930e8122fa464a28137f

--- system/ulib/ddk/rules.mk
+++ system/ulib/ddk/rules.mk
@@ -44,38 +44,3 @@ MODULE_LIBS := \
     system/ulib/c
 
 include make/module.mk
-
-#
-# ddktl-test
-#
-
-MODULE := $(LOCAL_DIR).ddktl-test
-
-MODULE_NAME := ddktl-test
-
-MODULE_TYPE := drivertest
-
-TEST_DIR := $(LOCAL_DIR)/test/ddktl
-
-MODULE_SRCS := \
-    $(TEST_DIR)/ddktl-test.cpp \
-    $(TEST_DIR)/ddktl-test-binding.c \
-    $(TEST_DIR)/device-tests.cpp \
-    $(TEST_DIR)/ethernet-tests.cpp \
-    $(TEST_DIR)/wlan-tests.cpp \
-
-MODULE_STATIC_LIBS := \
-    system/ulib/ddk \
-    system/ulib/mx \
-    system/ulib/mxalloc \
-    system/ulib/mxcpp \
-    system/ulib/mxtl \
-
-MODULE_LIBS := \
-    system/ulib/unittest \
-    system/ulib/mxio \
-    system/ulib/driver \
-    system/ulib/magenta \
-    system/ulib/c \
-
-include make/module.mk
---
+++ system/ulib/ddktl/rules.mk
@@ -0,0 +1,47 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userlib
+
+include make/module.mk
+
+#
+# ddktl-test
+#
+
+MODULE := $(LOCAL_DIR).test
+
+MODULE_NAME := ddktl-test
+
+MODULE_TYPE := drivertest
+
+TEST_DIR := $(LOCAL_DIR)/test
+
+MODULE_SRCS := \
+    $(TEST_DIR)/ddktl-test.cpp \
+    $(TEST_DIR)/ddktl-test-binding.c \
+    $(TEST_DIR)/device-tests.cpp \
+    $(TEST_DIR)/ethernet-tests.cpp \
+    $(TEST_DIR)/wlan-tests.cpp \
+
+MODULE_STATIC_LIBS := \
+    system/ulib/ddk \
+    system/ulib/ddktl \
+    system/ulib/mx \
+    system/ulib/mxalloc \
+    system/ulib/mxcpp \
+    system/ulib/mxtl \
+
+MODULE_LIBS := \
+    system/ulib/unittest \
+    system/ulib/mxio \
+    system/ulib/driver \
+    system/ulib/magenta \
+    system/ulib/c \
+
+include make/module.mk
