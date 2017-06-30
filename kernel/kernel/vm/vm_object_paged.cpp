saradindusengupta/magenta

[kernel][vm] Add support for pinning pages
Change-Id: I01d5f5c5aa0ebc28e495646b4fb2953bb5fc6f47

kernel/kernel/vm/vm_object_paged.cpp
@@ -39,6 +39,11 @@ void ZeroPage(vm_page_t* p) {
     ZeroPage(pa);
 }
 
+void InitializeVmPage(vm_page_t* p) {
+    p->state = VM_PAGE_STATE_OBJECT;
+    p->object.pin_count = 0;
+}
+
 } // namespace
 
 VmObjectPaged::VmObjectPaged(uint32_t pmm_alloc_flags, mxtl::RefPtr<VmObject> parent)
@@ -50,6 +55,11 @@ VmObjectPaged::~VmObjectPaged() {
     canary_.Assert();
 
     LTRACEF("%p\n", this);
+
+    page_list_.ForEveryPage(
+            [](const auto p, uint64_t off) {
+                ASSERT(p->object.pin_count == 0);
+            });
 
     // free all of the pages attached to us
     page_list_.FreeAllPages();
@@ -277,7 +287,7 @@ status_t VmObjectPaged::GetPageLocked(uint64_t offset, uint pf_flags, vm_page_t*
             if (!p_clone)
                 return ERR_NO_MEMORY;
 
-            p_clone->state = VM_PAGE_STATE_OBJECT;
+            InitializeVmPage(p_clone);
 
             // do a direct copy of the two pages
             const void* src = paddr_to_kvaddr(pa);
@@ -323,7 +333,7 @@ status_t VmObjectPaged::GetPageLocked(uint64_t offset, uint pf_flags, vm_page_t*
     if (!p)
         return ERR_NO_MEMORY;
 
-    p->state = VM_PAGE_STATE_OBJECT;
+    InitializeVmPage(p);
 
     // TODO: remove once pmm returns zeroed pages
     ZeroPage(pa);
@@ -398,7 +408,7 @@ status_t VmObjectPaged::CommitRange(uint64_t offset, uint64_t len, uint64_t* com
         p = list_remove_head_type(&page_list, vm_page_t, free.node);
         ASSERT(p);
 
-        p->state = VM_PAGE_STATE_OBJECT;
+        InitializeVmPage(p);
 
         // TODO: remove once pmm returns zeroed pages
         ZeroPage(p);
@@ -471,7 +481,7 @@ status_t VmObjectPaged::CommitRangeContiguous(uint64_t offset, uint64_t len, uin
         vm_page_t* p = list_remove_head_type(&page_list, vm_page_t, free.node);
         ASSERT(p);
 
-        p->state = VM_PAGE_STATE_OBJECT;
+        InitializeVmPage(p);
 
         // TODO: remove once pmm returns zeroed pages
         ZeroPage(p);
@@ -517,6 +527,10 @@ status_t VmObjectPaged::DecommitRange(uint64_t offset, uint64_t len, uint64_t* d
     LTRACEF("start offset %#" PRIx64 ", end %#" PRIx64 ", page_aliged_len %#" PRIx64 "\n", start, end,
             page_aligned_len);
 
+    if (AnyPagesPinnedLocked(start, page_aligned_len)) {
+        return ERR_BAD_STATE;
+    }
+
     // unmap all of the pages in this range on all the mapping regions
     RangeChangeUpdateLocked(start, page_aligned_len);
 
@@ -530,6 +544,98 @@ status_t VmObjectPaged::DecommitRange(uint64_t offset, uint64_t len, uint64_t* d
     }
 
     return NO_ERROR;
+}
+
+status_t VmObjectPaged::Pin(uint64_t offset, uint64_t len) {
+    canary_.Assert();
+
+    AutoLock a(&lock_);
+
+    // verify that the range is within the object
+    if (unlikely(!InRange(offset, len, size_)))
+        return ERR_OUT_OF_RANGE;
+
+    if (unlikely(len == 0))
+        return NO_ERROR;
+
+    const uint64_t start_page_offset = ROUNDDOWN(offset, PAGE_SIZE);
+    const uint64_t end_page_offset = ROUNDUP(offset + len, PAGE_SIZE);
+
+    status_t status = NO_ERROR;
+    uint64_t off;
+    for (off = start_page_offset; off != end_page_offset && status == NO_ERROR; off += PAGE_SIZE) {
+        vm_page_t* p = page_list_.GetPage(off);
+        if (!p) {
+            status = ERR_NOT_FOUND;
+            break;
+        }
+
+        DEBUG_ASSERT(p->state == VM_PAGE_STATE_OBJECT);
+        if (p->object.pin_count == VM_PAGE_OBJECT_MAX_PIN_COUNT) {
+            status = ERR_UNAVAILABLE;
+            break;
+        }
+        p->object.pin_count++;
+    }
+
+    if (status != NO_ERROR) {
+        UnpinLocked(start_page_offset, off - start_page_offset);
+        return status;
+    }
+
+    return NO_ERROR;
+}
+
+void VmObjectPaged::Unpin(uint64_t offset, uint64_t len) {
+    AutoLock a(&lock_);
+    UnpinLocked(offset, len);
+}
+
+void VmObjectPaged::UnpinLocked(uint64_t offset, uint64_t len) {
+    canary_.Assert();
+    DEBUG_ASSERT(lock_.IsHeld());
+
+    // verify that the range is within the object
+    ASSERT(InRange(offset, len, size_));
+
+    if (unlikely(len == 0))
+        return;
+
+    const uint64_t start_page_offset = ROUNDDOWN(offset, PAGE_SIZE);
+    const uint64_t end_page_offset = ROUNDUP(offset + len, PAGE_SIZE);
+
+    uint64_t off;
+    for (off = start_page_offset; off != end_page_offset; off += PAGE_SIZE) {
+        vm_page_t* p = page_list_.GetPage(off);
+        ASSERT_MSG(p != nullptr, "Tried to unpin an uncommitted page");
+
+        DEBUG_ASSERT(p->state == VM_PAGE_STATE_OBJECT);
+        ASSERT(p->object.pin_count > 0);
+        p->object.pin_count--;
+    }
+
+    return;
+}
+
+bool VmObjectPaged::AnyPagesPinnedLocked(uint64_t offset, size_t len) {
+    canary_.Assert();
+    DEBUG_ASSERT(lock_.IsHeld());
+    DEBUG_ASSERT(IS_PAGE_ALIGNED(offset));
+    DEBUG_ASSERT(IS_PAGE_ALIGNED(len));
+
+    const uint64_t start_page_offset = offset;
+    const uint64_t end_page_offset = offset + len;
+
+    bool found_pinned = false;
+    page_list_.ForEveryPageInRange(
+            [&found_pinned, start_page_offset, end_page_offset](const auto p, uint64_t off) {
+                DEBUG_ASSERT(off >= start_page_offset && off < end_page_offset);
+                if (p->object.pin_count > 0) {
+                    found_pinned = true;
+                }
+            }, start_page_offset, end_page_offset);
+
+    return found_pinned;
 }
 
 status_t VmObjectPaged::ResizeLocked(uint64_t s) {
@@ -552,6 +658,9 @@ status_t VmObjectPaged::ResizeLocked(uint64_t s) {
 
         // we're only worried about whole pages to be removed
         if (page_aligned_len > 0) {
+            if (AnyPagesPinnedLocked(start, page_aligned_len)) {
+                return ERR_BAD_STATE;
+            }
             // unmap all of the pages in this range on all the mapping regions
             RangeChangeUpdateLocked(start, page_aligned_len);
 

kernel/kernel/vm/vm_unittest.cpp
@@ -335,6 +335,115 @@ static bool vmo_commit_test(void* context) {
     END_TEST;
 }
 
+// Creates a paged VMO, pins it, and tries operations that should unpin it.
+static bool vmo_pin_test(void* context) {
+    BEGIN_TEST;
+
+    static const size_t alloc_size = PAGE_SIZE * 16;
+    auto vmo = VmObjectPaged::Create(PMM_ALLOC_FLAG_ANY, alloc_size);
+    REQUIRE_NONNULL(vmo, "vmobject creation\n");
+
+    status_t status = vmo->Pin(PAGE_SIZE, alloc_size);
+    EXPECT_EQ(ERR_OUT_OF_RANGE, status, "pinning out of range\n");
+    status = vmo->Pin(PAGE_SIZE, 0);
+    EXPECT_EQ(NO_ERROR, status, "pinning range of len 0\n");
+    status = vmo->Pin(alloc_size + PAGE_SIZE, 0);
+    EXPECT_EQ(ERR_OUT_OF_RANGE, status, "pinning out-of-range of len 0\n");
+
+    status = vmo->Pin(PAGE_SIZE, 3 * PAGE_SIZE);
+    EXPECT_EQ(ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+    status = vmo->Pin(0, alloc_size);
+    EXPECT_EQ(ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+
+    uint64_t n;
+    status = vmo->CommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(NO_ERROR, status, "committing range\n");
+
+    status = vmo->Pin(0, alloc_size);
+    EXPECT_EQ(ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+    status = vmo->Pin(PAGE_SIZE, 4 * PAGE_SIZE);
+    EXPECT_EQ(ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+    status = vmo->Pin(0, 4 * PAGE_SIZE);
+    EXPECT_EQ(ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+
+    status = vmo->Pin(PAGE_SIZE, 3 * PAGE_SIZE);
+    EXPECT_EQ(NO_ERROR, status, "pinning committed range\n");
+
+    status = vmo->DecommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(ERR_BAD_STATE, status, "decommitting pinned range\n");
+    status = vmo->DecommitRange(PAGE_SIZE, PAGE_SIZE, &n);
+    EXPECT_EQ(ERR_BAD_STATE, status, "decommitting pinned range\n");
+    status = vmo->DecommitRange(3 * PAGE_SIZE, PAGE_SIZE, &n);
+    EXPECT_EQ(ERR_BAD_STATE, status, "decommitting pinned range\n");
+
+    vmo->Unpin(PAGE_SIZE, 3 * PAGE_SIZE);
+
+    status = vmo->DecommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(NO_ERROR, status, "decommitting unpinned range\n");
+
+
+    status = vmo->CommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(NO_ERROR, status, "committing range\n");
+    status = vmo->Pin(PAGE_SIZE, 3 * PAGE_SIZE);
+    EXPECT_EQ(NO_ERROR, status, "pinning committed range\n");
+
+    status = vmo->Resize(0);
+    EXPECT_EQ(ERR_BAD_STATE, status, "resizing pinned range\n");
+
+    vmo->Unpin(PAGE_SIZE, 3 * PAGE_SIZE);
+
+    status = vmo->Resize(0);
+    EXPECT_EQ(NO_ERROR, status, "resizing unpinned range\n");
+
+    END_TEST;
+}
+
+// Creates a page VMO and pins the same pages multiple times
+static bool vmo_multiple_pin_test(void* context) {
+    BEGIN_TEST;
+
+    static const size_t alloc_size = PAGE_SIZE * 16;
+    auto vmo = VmObjectPaged::Create(PMM_ALLOC_FLAG_ANY, alloc_size);
+    REQUIRE_NONNULL(vmo, "vmobject creation\n");
+    uint64_t n;
+    status_t status = vmo->CommitRange(0, alloc_size, &n);
+    EXPECT_EQ(NO_ERROR, status, "committing range\n");
+
+    status = vmo->Pin(0, alloc_size);
+    EXPECT_EQ(NO_ERROR, status, "pinning whole range\n");
+    status = vmo->Pin(PAGE_SIZE, 4 * PAGE_SIZE);
+    EXPECT_EQ(NO_ERROR, status, "pinning subrange\n");
+
+    for (unsigned int i = 1; i < VM_PAGE_OBJECT_MAX_PIN_COUNT; ++i) {
+        status = vmo->Pin(0, PAGE_SIZE);
+        EXPECT_EQ(NO_ERROR, status, "pinning first page max times\n");
+    }
+    status = vmo->Pin(0, PAGE_SIZE);
+    EXPECT_EQ(ERR_UNAVAILABLE, status, "page is pinned too much\n");
+
+    vmo->Unpin(0, alloc_size);
+    status = vmo->DecommitRange(PAGE_SIZE, 4 * PAGE_SIZE, &n);
+    EXPECT_EQ(ERR_BAD_STATE, status, "decommitting pinned range\n");
+    status = vmo->DecommitRange(5 * PAGE_SIZE, alloc_size - 5 * PAGE_SIZE, &n);
+    EXPECT_EQ(NO_ERROR, status, "decommitting unpinned range\n");
+
+    vmo->Unpin(PAGE_SIZE, 4 * PAGE_SIZE);
+    status = vmo->DecommitRange(PAGE_SIZE, 4 * PAGE_SIZE, &n);
+    EXPECT_EQ(NO_ERROR, status, "decommitting unpinned range\n");
+
+    for (unsigned int i = 2; i < VM_PAGE_OBJECT_MAX_PIN_COUNT; ++i) {
+        vmo->Unpin(0, PAGE_SIZE);
+    }
+    status = vmo->DecommitRange(0, PAGE_SIZE, &n);
+    EXPECT_EQ(ERR_BAD_STATE, status, "decommitting unpinned range\n");
+
+    vmo->Unpin(0, PAGE_SIZE);
+    status = vmo->DecommitRange(0, PAGE_SIZE, &n);
+    EXPECT_EQ(NO_ERROR, status, "decommitting unpinned range\n");
+
+    END_TEST;
+}
+
 // Creates a vm object, commits odd sized memory.
 static bool vmo_odd_size_commit_test(void* context) {
     BEGIN_TEST;
@@ -636,6 +745,8 @@ VM_UNITTEST(vmm_alloc_contiguous_zero_size_fails)
 VM_UNITTEST(vmaspace_create_smoke_test)
 VM_UNITTEST(vmaspace_alloc_smoke_test)
 VM_UNITTEST(vmo_create_test)
+VM_UNITTEST(vmo_pin_test)
+VM_UNITTEST(vmo_multiple_pin_test)
 VM_UNITTEST(vmo_commit_test)
 VM_UNITTEST(vmo_odd_size_commit_test)
 VM_UNITTEST(vmo_contiguous_commit_test)
@@ -645,5 +756,5 @@ VM_UNITTEST(vmo_dropped_ref_test)
 VM_UNITTEST(vmo_remap_test)
 VM_UNITTEST(vmo_double_remap_test)
 VM_UNITTEST(vmo_read_write_smoke_test)
-VM_UNITTEST(dump_all_aspaces) // Run last
+//VM_UNITTEST(dump_all_aspaces) // Run last
 UNITTEST_END_TESTCASE(vm_tests, "vmtests", "Virtual memory tests", nullptr, nullptr);

vsrinivas/fuchsia

kernel/kernel/vm/vm_object_paged.cpp
@@ -40,6 +40,12 @@ void ZeroPage(vm_page_t* p) {
     ZeroPage(pa);
 }
 
+void InitializeVmPage(vm_page_t* p) {
+    DEBUG_ASSERT(p->state == VM_PAGE_STATE_ALLOC);
+    p->state = VM_PAGE_STATE_OBJECT;
+    p->object.pin_count = 0;
+}
+
 } // namespace
 
 VmObjectPaged::VmObjectPaged(uint32_t pmm_alloc_flags, mxtl::RefPtr<VmObject> parent)
@@ -51,6 +57,12 @@ VmObjectPaged::~VmObjectPaged() {
     canary_.Assert();
 
     LTRACEF("%p\n", this);
+
+    page_list_.ForEveryPage(
+            [](const auto p, uint64_t off) {
+                ASSERT(p->object.pin_count == 0);
+                return MX_ERR_NEXT;
+            });
 
     // free all of the pages attached to us
     page_list_.FreeAllPages();
@@ -119,7 +131,10 @@ void VmObjectPaged::Dump(uint depth, bool verbose) {
     AutoLock a(&lock_);
 
     size_t count = 0;
-    page_list_.ForEveryPage([&count](const auto p, uint64_t) { count++; });
+    page_list_.ForEveryPage([&count](const auto p, uint64_t) {
+        count++;
+        return MX_ERR_NEXT;
+    });
 
     for (uint i = 0; i < depth; ++i) {
         printf("  ");
@@ -134,6 +149,7 @@ void VmObjectPaged::Dump(uint depth, bool verbose) {
                 printf("  ");
             }
             printf("offset %#" PRIx64 " page %p paddr %#" PRIxPTR "\n", offset, p, vm_page_to_paddr(p));
+            return MX_ERR_NEXT;
         };
         page_list_.ForEveryPage(f);
     }
@@ -154,6 +170,7 @@ size_t VmObjectPaged::AllocatedPagesInRange(uint64_t offset, uint64_t len) const
             if (off >= offset && off < offset + new_len) {
                 count++;
             }
+            return MX_ERR_NEXT;
         });
     return count;
 }
@@ -288,7 +305,7 @@ status_t VmObjectPaged::GetPageLocked(uint64_t offset, uint pf_flags, vm_page_t*
             if (!p_clone)
                 return MX_ERR_NO_MEMORY;
 
-            p_clone->state = VM_PAGE_STATE_OBJECT;
+            InitializeVmPage(p_clone);
 
             // do a direct copy of the two pages
             const void* src = paddr_to_kvaddr(pa);
@@ -334,7 +351,7 @@ status_t VmObjectPaged::GetPageLocked(uint64_t offset, uint pf_flags, vm_page_t*
     if (!p)
         return MX_ERR_NO_MEMORY;
 
-    p->state = VM_PAGE_STATE_OBJECT;
+    InitializeVmPage(p);
 
     // TODO: remove once pmm returns zeroed pages
     ZeroPage(pa);
@@ -409,7 +426,7 @@ status_t VmObjectPaged::CommitRange(uint64_t offset, uint64_t len, uint64_t* com
         p = list_remove_head_type(&page_list, vm_page_t, free.node);
         ASSERT(p);
 
-        p->state = VM_PAGE_STATE_OBJECT;
+        InitializeVmPage(p);
 
         // TODO: remove once pmm returns zeroed pages
         ZeroPage(p);
@@ -482,7 +499,7 @@ status_t VmObjectPaged::CommitRangeContiguous(uint64_t offset, uint64_t len, uin
         vm_page_t* p = list_remove_head_type(&page_list, vm_page_t, free.node);
         ASSERT(p);
 
-        p->state = VM_PAGE_STATE_OBJECT;
+        InitializeVmPage(p);
 
         // TODO: remove once pmm returns zeroed pages
         ZeroPage(p);
@@ -528,6 +545,10 @@ status_t VmObjectPaged::DecommitRange(uint64_t offset, uint64_t len, uint64_t* d
     LTRACEF("start offset %#" PRIx64 ", end %#" PRIx64 ", page_aliged_len %#" PRIx64 "\n", start, end,
             page_aligned_len);
 
+    if (AnyPagesPinnedLocked(start, page_aligned_len)) {
+        return MX_ERR_BAD_STATE;
+    }
+
     // unmap all of the pages in this range on all the mapping regions
     RangeChangeUpdateLocked(start, page_aligned_len);
 
@@ -541,6 +562,100 @@ status_t VmObjectPaged::DecommitRange(uint64_t offset, uint64_t len, uint64_t* d
     }
 
     return MX_OK;
+}
+
+status_t VmObjectPaged::Pin(uint64_t offset, uint64_t len) {
+    canary_.Assert();
+
+    AutoLock a(&lock_);
+
+    // verify that the range is within the object
+    if (unlikely(!InRange(offset, len, size_)))
+        return MX_ERR_OUT_OF_RANGE;
+
+    if (unlikely(len == 0))
+        return MX_OK;
+
+    const uint64_t start_page_offset = ROUNDDOWN(offset, PAGE_SIZE);
+    const uint64_t end_page_offset = ROUNDUP(offset + len, PAGE_SIZE);
+
+    status_t status = MX_OK;
+    uint64_t off;
+    for (off = start_page_offset; off != end_page_offset && status == MX_OK; off += PAGE_SIZE) {
+        vm_page_t* p = page_list_.GetPage(off);
+        if (!p) {
+            status = MX_ERR_NOT_FOUND;
+            break;
+        }
+
+        DEBUG_ASSERT(p->state == VM_PAGE_STATE_OBJECT);
+        if (p->object.pin_count == VM_PAGE_OBJECT_MAX_PIN_COUNT) {
+            status = MX_ERR_UNAVAILABLE;
+            break;
+        }
+        p->object.pin_count++;
+    }
+
+    if (status != MX_OK) {
+        UnpinLocked(start_page_offset, off - start_page_offset);
+        return status;
+    }
+
+    return MX_OK;
+}
+
+void VmObjectPaged::Unpin(uint64_t offset, uint64_t len) {
+    AutoLock a(&lock_);
+    UnpinLocked(offset, len);
+}
+
+void VmObjectPaged::UnpinLocked(uint64_t offset, uint64_t len) {
+    canary_.Assert();
+    DEBUG_ASSERT(lock_.IsHeld());
+
+    // verify that the range is within the object
+    ASSERT(InRange(offset, len, size_));
+
+    if (unlikely(len == 0))
+        return;
+
+    const uint64_t start_page_offset = ROUNDDOWN(offset, PAGE_SIZE);
+    const uint64_t end_page_offset = ROUNDUP(offset + len, PAGE_SIZE);
+
+    uint64_t off;
+    for (off = start_page_offset; off != end_page_offset; off += PAGE_SIZE) {
+        vm_page_t* p = page_list_.GetPage(off);
+        ASSERT_MSG(p != nullptr, "Tried to unpin an uncommitted page");
+
+        DEBUG_ASSERT(p->state == VM_PAGE_STATE_OBJECT);
+        ASSERT(p->object.pin_count > 0);
+        p->object.pin_count--;
+    }
+
+    return;
+}
+
+bool VmObjectPaged::AnyPagesPinnedLocked(uint64_t offset, size_t len) {
+    canary_.Assert();
+    DEBUG_ASSERT(lock_.IsHeld());
+    DEBUG_ASSERT(IS_PAGE_ALIGNED(offset));
+    DEBUG_ASSERT(IS_PAGE_ALIGNED(len));
+
+    const uint64_t start_page_offset = offset;
+    const uint64_t end_page_offset = offset + len;
+
+    bool found_pinned = false;
+    page_list_.ForEveryPageInRange(
+            [&found_pinned, start_page_offset, end_page_offset](const auto p, uint64_t off) {
+                DEBUG_ASSERT(off >= start_page_offset && off < end_page_offset);
+                if (p->object.pin_count > 0) {
+                    found_pinned = true;
+                    return MX_ERR_STOP;
+                }
+                return MX_ERR_NEXT;
+            }, start_page_offset, end_page_offset);
+
+    return found_pinned;
 }
 
 status_t VmObjectPaged::ResizeLocked(uint64_t s) {
@@ -563,6 +678,9 @@ status_t VmObjectPaged::ResizeLocked(uint64_t s) {
 
         // we're only worried about whole pages to be removed
         if (page_aligned_len > 0) {
+            if (AnyPagesPinnedLocked(start, page_aligned_len)) {
+                return MX_ERR_BAD_STATE;
+            }
             // unmap all of the pages in this range on all the mapping regions
             RangeChangeUpdateLocked(start, page_aligned_len);
 

kernel/kernel/vm/vm_unittest.cpp
@@ -337,6 +337,115 @@ static bool vmo_commit_test(void* context) {
     END_TEST;
 }
 
+// Creates a paged VMO, pins it, and tries operations that should unpin it.
+static bool vmo_pin_test(void* context) {
+    BEGIN_TEST;
+
+    static const size_t alloc_size = PAGE_SIZE * 16;
+    auto vmo = VmObjectPaged::Create(PMM_ALLOC_FLAG_ANY, alloc_size);
+    REQUIRE_NONNULL(vmo, "vmobject creation\n");
+
+    status_t status = vmo->Pin(PAGE_SIZE, alloc_size);
+    EXPECT_EQ(MX_ERR_OUT_OF_RANGE, status, "pinning out of range\n");
+    status = vmo->Pin(PAGE_SIZE, 0);
+    EXPECT_EQ(MX_OK, status, "pinning range of len 0\n");
+    status = vmo->Pin(alloc_size + PAGE_SIZE, 0);
+    EXPECT_EQ(MX_ERR_OUT_OF_RANGE, status, "pinning out-of-range of len 0\n");
+
+    status = vmo->Pin(PAGE_SIZE, 3 * PAGE_SIZE);
+    EXPECT_EQ(MX_ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+    status = vmo->Pin(0, alloc_size);
+    EXPECT_EQ(MX_ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+
+    uint64_t n;
+    status = vmo->CommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(MX_OK, status, "committing range\n");
+
+    status = vmo->Pin(0, alloc_size);
+    EXPECT_EQ(MX_ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+    status = vmo->Pin(PAGE_SIZE, 4 * PAGE_SIZE);
+    EXPECT_EQ(MX_ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+    status = vmo->Pin(0, 4 * PAGE_SIZE);
+    EXPECT_EQ(MX_ERR_NOT_FOUND, status, "pinning uncommitted range\n");
+
+    status = vmo->Pin(PAGE_SIZE, 3 * PAGE_SIZE);
+    EXPECT_EQ(MX_OK, status, "pinning committed range\n");
+
+    status = vmo->DecommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(MX_ERR_BAD_STATE, status, "decommitting pinned range\n");
+    status = vmo->DecommitRange(PAGE_SIZE, PAGE_SIZE, &n);
+    EXPECT_EQ(MX_ERR_BAD_STATE, status, "decommitting pinned range\n");
+    status = vmo->DecommitRange(3 * PAGE_SIZE, PAGE_SIZE, &n);
+    EXPECT_EQ(MX_ERR_BAD_STATE, status, "decommitting pinned range\n");
+
+    vmo->Unpin(PAGE_SIZE, 3 * PAGE_SIZE);
+
+    status = vmo->DecommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(MX_OK, status, "decommitting unpinned range\n");
+
+
+    status = vmo->CommitRange(PAGE_SIZE, 3 * PAGE_SIZE, &n);
+    EXPECT_EQ(MX_OK, status, "committing range\n");
+    status = vmo->Pin(PAGE_SIZE, 3 * PAGE_SIZE);
+    EXPECT_EQ(MX_OK, status, "pinning committed range\n");
+
+    status = vmo->Resize(0);
+    EXPECT_EQ(MX_ERR_BAD_STATE, status, "resizing pinned range\n");
+
+    vmo->Unpin(PAGE_SIZE, 3 * PAGE_SIZE);
+
+    status = vmo->Resize(0);
+    EXPECT_EQ(MX_OK, status, "resizing unpinned range\n");
+
+    END_TEST;
+}
+
+// Creates a page VMO and pins the same pages multiple times
+static bool vmo_multiple_pin_test(void* context) {
+    BEGIN_TEST;
+
+    static const size_t alloc_size = PAGE_SIZE * 16;
+    auto vmo = VmObjectPaged::Create(PMM_ALLOC_FLAG_ANY, alloc_size);
+    REQUIRE_NONNULL(vmo, "vmobject creation\n");
+    uint64_t n;
+    status_t status = vmo->CommitRange(0, alloc_size, &n);
+    EXPECT_EQ(MX_OK, status, "committing range\n");
+
+    status = vmo->Pin(0, alloc_size);
+    EXPECT_EQ(MX_OK, status, "pinning whole range\n");
+    status = vmo->Pin(PAGE_SIZE, 4 * PAGE_SIZE);
+    EXPECT_EQ(MX_OK, status, "pinning subrange\n");
+
+    for (unsigned int i = 1; i < VM_PAGE_OBJECT_MAX_PIN_COUNT; ++i) {
+        status = vmo->Pin(0, PAGE_SIZE);
+        EXPECT_EQ(MX_OK, status, "pinning first page max times\n");
+    }
+    status = vmo->Pin(0, PAGE_SIZE);
+    EXPECT_EQ(MX_ERR_UNAVAILABLE, status, "page is pinned too much\n");
+
+    vmo->Unpin(0, alloc_size);
+    status = vmo->DecommitRange(PAGE_SIZE, 4 * PAGE_SIZE, &n);
+    EXPECT_EQ(MX_ERR_BAD_STATE, status, "decommitting pinned range\n");
+    status = vmo->DecommitRange(5 * PAGE_SIZE, alloc_size - 5 * PAGE_SIZE, &n);
+    EXPECT_EQ(MX_OK, status, "decommitting unpinned range\n");
+
+    vmo->Unpin(PAGE_SIZE, 4 * PAGE_SIZE);
+    status = vmo->DecommitRange(PAGE_SIZE, 4 * PAGE_SIZE, &n);
+    EXPECT_EQ(MX_OK, status, "decommitting unpinned range\n");
+
+    for (unsigned int i = 2; i < VM_PAGE_OBJECT_MAX_PIN_COUNT; ++i) {
+        vmo->Unpin(0, PAGE_SIZE);
+    }
+    status = vmo->DecommitRange(0, PAGE_SIZE, &n);
+    EXPECT_EQ(MX_ERR_BAD_STATE, status, "decommitting unpinned range\n");
+
+    vmo->Unpin(0, PAGE_SIZE);
+    status = vmo->DecommitRange(0, PAGE_SIZE, &n);
+    EXPECT_EQ(MX_OK, status, "decommitting unpinned range\n");
+
+    END_TEST;
+}
+
 // Creates a vm object, commits odd sized memory.
 static bool vmo_odd_size_commit_test(void* context) {
     BEGIN_TEST;
@@ -702,6 +811,8 @@ VM_UNITTEST(vmm_alloc_contiguous_zero_size_fails)
 VM_UNITTEST(vmaspace_create_smoke_test)
 VM_UNITTEST(vmaspace_alloc_smoke_test)
 VM_UNITTEST(vmo_create_test)
+VM_UNITTEST(vmo_pin_test)
+VM_UNITTEST(vmo_multiple_pin_test)
 VM_UNITTEST(vmo_commit_test)
 VM_UNITTEST(vmo_odd_size_commit_test)
 VM_UNITTEST(vmo_contiguous_commit_test)