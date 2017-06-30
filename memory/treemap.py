d0a1774a53487723542b5afd77c9dfb59b70cf48
Dave Bort <dbort@google.com>
2017-06-30 16:42:03 -0700

    Style/lint pass on memory/treemap.py
    Minor functional changes:
    - JSON/table text is printed on one line
    - The kernel free node is now displayed
    
    Change-Id: Id06fb082ead8da0420953c9cd7852f9429d11b5e

--- memory/treemap.py
+++ memory/treemap.py
@@ -10,10 +10,15 @@ For usage, see
 https://fuchsia.googlesource.com/magenta/+/master/docs/memory.md#Visualize-memory-usage
 """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import cgi
 import collections
 import json
 import sys
+import textwrap
 
 # Magic value for nodes with empty names.
 UNNAMED_NAME = '<unnamed>'
@@ -44,15 +49,33 @@ class Node(object):
         return tag + name
 
 
+# Mapping of unique ID strings to Node objects.
+ids_to_nodes = {}
+
+
 def lookup(node_id):
-    node = nodemap.get(node_id)
+    """Returns or creates the Node associated with an ID string.
+
+    Args:
+        node_id: ID string to look up
+    Returns:
+        A Node object
+    """
+    node = ids_to_nodes.get(node_id)
     if node is None:
         node = Node()
-        nodemap[node_id] = node
+        ids_to_nodes[node_id] = node
     return node
 
 
 def sum_area(node):
+    """Recursively calculates the Node.area values of a subtree.
+
+    Args:
+        node: The Node at the root of the tree to walk
+    Returns:
+        The final area of |node|
+    """
     # Area should either be set explicitly or calculated from the children.
     if node.children and (node.area != 0):
         raise AssertionError(
@@ -62,31 +85,45 @@ def sum_area(node):
     return node.area
 
 
-def format_size(bytes):
-    units = 'BkMGTPE'
-    num_units = len(units)
+def format_size(nbytes):
+    """Formats a size as a human-readable string like "123.4k".
 
+    Units are in powers of 1024, so "k" is technically "kiB", etc.
+    Values smaller than "k" have the suffix "B".
+
+    Exact multiples of a unit are displayed without a decimal;
+    e.g., "17k" means the value is exactly 17 * 1024.
+
+    Otherwise, a decimal is present; e.g., "17.0k" means the value
+    is (17 * 1024) +/- epsilon.
+
+    Args:
+        nbytes: The value to format
+    Returns:
+        The formatted string
+    """
+    units = 'BkMGTPE'
     ui = 0
     r = 0
     whole = True
 
-    while bytes >= 10000 or (bytes != 0 and (bytes & 1023) == 0):
+    while nbytes >= 10000 or (nbytes != 0 and (nbytes & 1023) == 0):
         ui += 1
-        if bytes & 1023:
+        if nbytes & 1023:
             whole = False
-        r = bytes % 1024
-        bytes /= 1024
+        r = nbytes % 1024
+        nbytes //= 1024
 
     if whole:
-        return '{}{}'.format(bytes, units[ui])
+        return '{}{}'.format(nbytes, units[ui])
 
     round_up = (r % 100) >= 50
-    r = (r / 100) + round_up
+    r = (r // 100) + round_up
     if r == 10:
-        bytes += 1
+        nbytes += 1
         r = 0
 
-    return '{}.{}{}'.format(bytes, r, units[ui])
+    return '{}.{}{}'.format(nbytes, r, units[ui])
 
 
 # Enum for tracking VMO reference types.
@@ -98,10 +135,10 @@ def populate_process(process_node, process_record, hide_aggregated=True):
     """Adds the process's child nodes.
 
     Args:
-        process_node: a process's Node
-        process_record: the same process's input record
-        hide_aggregated: if true, do not create Nodes for individual VMOs
-            that have been aggregated by name into a single Node.
+        process_node: A process's Node
+        process_record: The same process's input record
+        hide_aggregated: If true, do not create Nodes for individual VMOs
+                that have been aggregated by name into a single Node
     """
     # If there aren't any VMOs, use the sizes in the record.
     if not process_record.get('vmo_refs', []):
@@ -205,79 +242,89 @@ def populate_process(process_node, process_record, hide_aggregated=True):
     process_node.children.extend(children)
 
 
-# See <https://github.com/evmar/webtreemap/blob/gh-pages/README.markdown#input-format>
-# For a description of this data format.
-def build_tree(node):
+def build_webtreemap(node):
+    """Returns a JSON-able dict tree representing a Node tree.
+
+    See
+    https://github.com/evmar/webtreemap/blob/gh-pages/README.markdown#input-format
+    For a description of this data format.
+
+    Args:
+        node: The Node at the root of the tree to walk
+    Returns:
+        A webtreemap-compatible dict representing the tree
+    """
     return {
-        'name': '{} ({})'.format(node.html_label(), format_size(node.area)),
-        'data': {
-            '$area': node.area,
-            # TODO(dbort): Turn this on and style different node types if
-            # https://github.com/evmar/webtreemap/pull/15 is accepted. Would
-            # define a class like 'webtreemap-symbol-<type>' but there's a bug
-            # in webtreemap.js.
-            # '$symbol': node.type,
-        },
-        'children': map(build_tree, node.children)
+            'name': '{} ({})'.format(node.html_label(), format_size(node.area)),
+            'data': {
+                    '$area': node.area,
+                    # TODO(dbort): Turn this on and style different node types
+                    # if https://github.com/evmar/webtreemap/pull/15 is
+                    # accepted. Would define a class like
+                    # 'webtreemap-symbol-<type>' but there's a bug in
+                    # webtreemap.js.
+                    # '$symbol': node.type,
+            },
+            'children': map(build_webtreemap, node.children)
     }
 
 
-def dump_html(node, depth=0, parent_area=None, total_area=None):
+def dump_html_table(node, depth=0, parent_area=None, total_area=None):
     """Returns an HTML representation of the tree.
 
     Args:
-      node: The root of the tree to dump
-      depth: The depth of the node. Use 0 for the root node.
-      parent_area: The size of the parent node; used to show fractions.
-          Use None for the root node.
-      total_area: The total size of the tree; used to show fractions.
-          Use None for the root node.
+        node: The root of the tree to dump
+        depth: The depth of the node. Use 0 for the root node.
+        parent_area: The size of the parent node; used to show fractions.
+                Use None for the root node.
+        total_area: The total size of the tree; used to show fractions.
+                Use None for the root node.
     Returns:
-      A sequence of HTML lines, joinable by whitespace.
+        A sequence of HTML lines, joinable by whitespace
     """
     lines = []
 
     if not depth:
         # We're the root node. Dump the headers.
         lines.extend([
-            '<style>',
-            'table#tree tr:nth-child(even) {',
-            '    background-color: #eee;',
-            '}',
-            'table#tree tr:nth-child(odd) {',
-            '    background-color: #fff;',
-            '}',
-            'table#tree td {',
-            '    text-align: right;',
-            '    padding-left: 1em;',
-            '    padding-right: 1em;',
-            '    font-family:Consolas,Monaco,Lucida Console,Liberation Mono,'
-            '        DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New,'
-            '        monospace;',
-            '}',
-            'table#tree td.name {',
-            '    text-align: left;',
-            '}',
-            '</style>',
-            '<table id="tree">',
-            '<tr>',
-            '<th>Name</th>',
-            '<th>Size<br/>(bytes/1024^n)</th>',
-            '<th>Size (bytes)</th>',
-            '<th>Fraction of parent</th>',
-            '<th>Fraction of total</th>',
-            '</tr>',
+                '<style>',
+                'table#tree tr:nth-child(even) {',
+                '    background-color: #eee;',
+                '}',
+                'table#tree tr:nth-child(odd) {',
+                '    background-color: #fff;',
+                '}',
+                'table#tree td {',
+                '    text-align: right;',
+                '    padding-left: 1em;',
+                '    padding-right: 1em;',
+                '    font-family:Consolas,Monaco,Lucida Console,',
+                '        Liberation Mono,DejaVu Sans Mono,',
+                '        Bitstream Vera Sans Mono,Courier New,monospace;',
+                '}',
+                'table#tree td.name {',
+                '    text-align: left;',
+                '}',
+                '</style>',
+                '<table id="tree">',
+                '<tr>',
+                '<th>Name</th>',
+                '<th>Size<br/>(bytes/1024^n)</th>',
+                '<th>Size (bytes)</th>',
+                '<th>Fraction of parent</th>',
+                '<th>Fraction of total</th>',
+                '</tr>',
         ])
 
     lines.extend([
-        '<tr>',
-        # Indent the names based on depth.
-        '<td class="name"><span style="color:#bbb">{indent}</span>'
+            '<tr>',
+            # Indent the names based on depth.
+            '<td class="name"><span style="color:#bbb">{indent}</span>'
             '{label}</td>'.format(
                     indent=('|' + '&nbsp;' * 2) * depth,
                     label=node.html_label()),
-        '<td>{fsize}</td>'.format(fsize=format_size(node.area)),
-        '<td>{size}</td>'.format(size=node.area),
+            '<td>{fsize}</td>'.format(fsize=format_size(node.area)),
+            '<td>{size}</td>'.format(size=node.area),
     ])
 
     if depth:
@@ -286,8 +333,9 @@ def dump_html(node, depth=0, parent_area=None, total_area=None):
         tfrac = node.area / float(total_area) if total_area else 0
         for frac in (pfrac, tfrac):
             lines.extend([
-                '<td>{pct:.3f}%&nbsp;<progress value="{frac}"></progress></td>'
-                .format(pct=frac * 100, frac=frac)
+                    ('<td>{pct:.3f}%&nbsp;'
+                     '<progress value="{frac}"></progress></td>')
+                    .format(pct=frac * 100, frac=frac)
             ])
     else:
         lines.append('<td></td>' * 2)
@@ -298,8 +346,8 @@ def dump_html(node, depth=0, parent_area=None, total_area=None):
 
     # Append children by size, largest to smallest.
     def dump_child(child):
-        return dump_html(child, depth=depth+1,
-                parent_area=node.area, total_area=total_area)
+        return dump_html_table(child, depth=depth+1,
+                               parent_area=node.area, total_area=total_area)
 
     children = sorted(node.children, reverse=True, key=lambda n: n.area)
     for line in [dump_child(c) for c in children]:
@@ -311,140 +359,149 @@ def dump_html(node, depth=0, parent_area=None, total_area=None):
     return lines
 
 
-dataset = json.load(sys.stdin)
-nodemap = {}
-root_node = None
-root_job = None
-free_node = None
+def build_tree(dataset):
+    """Builds a Node tree from a set of memgraph records.
 
-for record in dataset:
-    record_type = record['type']
-    # Only read certain types.
-    if record_type not in ('kernel', 'j', 'p'):
-        continue
-    node = lookup(record['id'])
-    node.type = record_type
-    node.koid = record.get('koid', 0)
-    node.name = record['name']
-    if record_type == 'kernel':
-        node.area = record.get('size_bytes', 0)
-    elif record_type == 'j':
-        if record['parent'].startswith('kernel/'):
-            if root_job:
-                print >> sys.stderr, 'error: Found multiple root jobs'
-                sys.exit(1)
-            root_job = node
-    elif record_type == 'p':
-        # Add the process's children, which will determine its area.
-        populate_process(node, record)
-    if not record['parent']:
-        # The root node has an empty parent.
-        if root_node:
-            print >> sys.stderr, 'error: Found multiple root objects'
-            sys.exit(1)
-        root_node = node
-    elif record['id'] == 'kernel/free':
-        # Don't include the free space directly.
-        free_node = node
-    else:
-        parent_node = lookup(record['parent'])
-        parent_node.children.append(node)
+    See
+    https://fuchsia.googlesource.com/magenta/+/master/docs/memory.md#Visualize-memory-usage
+    for an example of generating memgraph JSON data.
 
-if not root_node:
-    print >> sys.stderr, 'error: Did not find root object'
-    sys.exit(1)
+    Args:
+        dataset: A sequence of memgraph records, typically parsed from JSON
+    Returns:
+        The root of the new Node tree
+    """
+    ids_to_nodes.clear()  # Clear out the global registry.
+    root_node = None
+    root_job = None
 
-if not root_job:
-    print >> sys.stderr, 'error: Did not find root job'
-    sys.exit(1)
+    for record in dataset:
+        record_type = record['type']
+        # Only read certain types.
+        if record_type not in ('kernel', 'j', 'p'):
+            continue
+        node = lookup(record['id'])
+        node.type = record_type
+        node.koid = record.get('koid', 0)
+        node.name = record['name']
+        if record_type == 'kernel':
+            node.area = record.get('size_bytes', 0)
+        elif record_type == 'j':
+            if record['parent'].startswith('kernel/'):
+                assert not root_job, 'Found multiple root jobs'
+                root_job = node
+        elif record_type == 'p':
+            # Add the process's children, which will determine its area.
+            populate_process(node, record)
+        if not record['parent']:
+            # The root node has an empty parent.
+            assert not root_node, 'Found multiple root objects'
+            root_node = node
+        else:
+            parent_node = lookup(record['parent'])
+            parent_node.children.append(node)
 
-# A better name for physmem.
-lookup('kernel/physmem').name = 'All physical memory'
+    assert root_node, 'Did not find root object'
+    assert root_job, 'Did not find root job'
 
-# Sum up the job tree. Don't touch kernel entries, which already have
-# the correct sizes.
-sum_area(root_job)
+    # A better name for physmem.
+    lookup('kernel/physmem').name = 'All physical memory'
 
-# The root job is usually named "root";
-# make it more clear that it's a job.
-root_job.name = 'root job'
+    # Sum up the job tree. Don't touch kernel entries, which already have
+    # the correct sizes.
+    sum_area(root_job)
 
-# Give users a hint that processes live in the VMO entry.
-kvmo_node = lookup('kernel/vmo')
-kvmo_node.name = 'VMOs/processes'
+    # The root job is usually named "root";
+    # make it more clear that it's a job.
+    root_job.name = 'root job'
 
-# Create a fake entry to cover the portion of kernel/vmo that isn't
-# covered by the job tree.
-node = lookup('kernel/vmo/unknown')
-node.name = 'unknown (kernel & unmapped)'
-node.area = kvmo_node.area - root_job.area
-kvmo_node.children.append(node)
+    # Give users a hint that processes live in the VMO entry.
+    kvmo_node = lookup('kernel/vmo')
+    kvmo_node.name = 'VMOs/processes'
 
-# Render the tree.
-tree_json = json.dumps(build_tree(root_node), indent=2)
+    # Create a fake entry to cover the portion of kernel/vmo that isn't
+    # covered by the job tree.
+    node = lookup('kernel/vmo/unknown')
+    node.name = 'unknown (kernel & unmapped)'
+    node.area = kvmo_node.area - root_job.area
+    kvmo_node.children.append(node)
 
-# Include the free node in the HTML version.
-if free_node:
-    lookup('kernel/physmem').children.append(free_node)
-tree_html = '\n'.join(dump_html(root_node))
+    return root_node
 
-print '''<!DOCTYPE html>
-<title>Memory usage</title>
-<script>
-var kTree = %(json)s
-</script>
-<link rel='stylesheet' href='https://evmar.github.io/webtreemap/webtreemap.css'>
-<style>
-body {
-  font-family: sans-serif;
-  font-size: 0.8em;
-  margin: 2ex 4ex;
-}
-h1 {
-  font-weight: normal;
-}
-#map {
-  width: 800px;
-  height: 600px;
-  position: relative;
-  cursor: pointer;
-  -webkit-user-select: none;
-}
-.treemap-node-type { font-weight: bold; }
-/* Colorblind-safe colors from http://mkweb.bcgsc.ca/colorblind/ */
-.treemap-node-type-k { color: black; }
-.treemap-node-type-j { color: RGB(213, 94, 0); } /* Vermillion */
-.treemap-node-type-p { color: RGB(0, 114, 178); } /* Blue */
-.treemap-node-type-v { color: RGB(0, 158, 115); } /* Bluish green */
-</style>
 
-<h1>Memory usage</h1>
+def print_html_document(root_node):
+    """Prints to stdout an HTML document that visualizes a Node tree.
 
-<p>Click on a box to zoom in.  Click on the outermost box to zoom out.</p>
+    Args:
+        root_node: The Node at the root of the tree to walk
+    """
+    html = '''\
+    <!DOCTYPE html>
+    <title>Memory usage</title>
+    <script>
+    var kTree = %(json)s
+    </script>
+    <link rel='stylesheet' href='https://evmar.github.io/webtreemap/webtreemap.css'>
+    <style>
+    body {
+      font-family: sans-serif;
+      font-size: 0.8em;
+      margin: 2ex 4ex;
+    }
+    h1 {
+      font-weight: normal;
+    }
+    #map {
+      width: 800px;
+      height: 600px;
+      position: relative;
+      cursor: pointer;
+      -webkit-user-select: none;
+    }
+    .treemap-node-type { font-weight: bold; }
+    /* Colorblind-safe colors from http://mkweb.bcgsc.ca/colorblind/ */
+    .treemap-node-type-k { color: black; }
+    .treemap-node-type-j { color: RGB(213, 94, 0); } /* Vermillion */
+    .treemap-node-type-p { color: RGB(0, 114, 178); } /* Blue */
+    .treemap-node-type-v { color: RGB(0, 158, 115); } /* Bluish green */
+    </style>
 
-<div id='map'></div>
+    <h1>Memory usage</h1>
 
-<script src='https://evmar.github.io/webtreemap/webtreemap.js'></script>
-<script>
-var map = document.getElementById('map');
-appendTreemap(map, kTree);
-</script>
+    <p>Click on a box to zoom in.  Click on the outermost box to zoom out.</p>
 
-<ul style="list-style: none">
-<li><span class="treemap-node-type treemap-node-type-k">K</span>: Kernel memory
-<li><span class="treemap-node-type treemap-node-type-j">J</span>: Job
-<li><span class="treemap-node-type treemap-node-type-p">P</span>: Process
-<li><span class="treemap-node-type treemap-node-type-v">V</span>: VMO
-<ul style="list-style: none">
-    <li> VMO names with <b>[<i>n</i>]</b> suffixes are aggregates of <i>n</i>
-         VMOs that have the same name.
-</ul>
-</ul>
+    <div id='map'></div>
 
-<hr>
-%(html)s
+    <script src='https://evmar.github.io/webtreemap/webtreemap.js'></script>
+    <script>
+    var map = document.getElementById('map');
+    appendTreemap(map, kTree);
+    </script>
 
-''' % {
-    'json': tree_json,
-    'html': tree_html
-}
+    <ul style="list-style: none">
+    <li><span class="treemap-node-type treemap-node-type-k">K</span>: Kernel memory
+    <li><span class="treemap-node-type treemap-node-type-j">J</span>: Job
+    <li><span class="treemap-node-type treemap-node-type-p">P</span>: Process
+    <li><span class="treemap-node-type treemap-node-type-v">V</span>: VMO
+    <ul style="list-style: none">
+        <li> VMO names with <b>[<i>n</i>]</b> suffixes are aggregates of <i>n</i>
+             VMOs that have the same name.
+    </ul>
+    </ul>
+
+    <hr>
+    %(table)s
+    ''' % {
+            'json': json.dumps(build_webtreemap(root_node)),
+            'table': ' '.join(dump_html_table(root_node))
+    }
+    print(textwrap.dedent(html))
+
+
+def main():
+    root_node = build_tree(json.load(sys.stdin))
+    print_html_document(root_node)
+
+
+if __name__ == '__main__':
+    main()
