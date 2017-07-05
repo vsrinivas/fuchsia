c3d6594a5b9e7f5df6581a78f74e87226152fb0e
Adam Barth <abarth@chromium.org>
2017-07-05 13:56:27 -0700

    Remove deprecated include
    This header is not needed by this file and will be removed eventually.
    
    Change-Id: I3aada63d7f1288ba1a1cdc3711aac1058e3b5490

--- bin/media/flog_service/flog_service_impl.cc
+++ bin/media/flog_service/flog_service_impl.cc
@@ -5,7 +5,6 @@
 #include "apps/media/src/flog_service/flog_service_impl.h"
 
 #include "application/lib/app/connect.h"
-#include "application/lib/app/service_provider_impl.h"
 #include "apps/media/src/flog_service/flog_directory.h"
 #include "apps/media/src/flog_service/flog_logger_impl.h"
 #include "apps/media/src/flog_service/flog_reader_impl.h"
