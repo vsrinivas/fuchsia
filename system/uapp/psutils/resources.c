8146182fd5643297fb38422cb962ed8abfd95645
Dave Bort <dbort@google.com>
2017-07-07 12:28:49 -0700

    [uapp][psutils] Improve error messages when getting the root resource
    Change-Id: I7797934a1cdad2ed5635eecfe52bc65ccee06436

--- system/uapp/psutils/resources.c
+++ system/uapp/psutils/resources.c
@@ -5,6 +5,7 @@
 #include "resources.h"
 
 #include <magenta/device/sysinfo.h>
+#include <magenta/status.h>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -20,12 +21,18 @@ mx_status_t get_root_resource(mx_handle_t* root_resource) {
         return MX_ERR_NOT_FOUND;
     }
 
-    size_t n = ioctl_sysinfo_get_root_resource(fd, root_resource);
+    ssize_t n = ioctl_sysinfo_get_root_resource(fd, root_resource);
     close(fd);
     if (n != sizeof(*root_resource)) {
-        fprintf(stderr, "ERROR: Cannot obtain root resource (%zu != %zu)\n",
-                n, sizeof(root_resource));
-        return MX_ERR_NOT_FOUND;
+        if (n < 0) {
+            fprintf(stderr, "ERROR: Cannot obtain root resource: %s (%zd)\n",
+                    mx_status_get_string(n), n);
+            return (mx_status_t)n;
+        } else {
+            fprintf(stderr, "ERROR: Cannot obtain root resource (%zd != %zd)\n",
+                    n, sizeof(root_resource));
+            return MX_ERR_NOT_FOUND;
+        }
     }
     return MX_OK;
 }
