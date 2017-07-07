4669a8ab98dbe20aecd087b39efda70d3bc0a256
Adam Barth <abarth@chromium.org>
2017-07-07 13:35:16 -0700

    Add more unittests
    Change-Id: Idec5a8e9ecd36aa0e8ebfa5bebe996bdf5188322

--- public/lib/ftl/random/uuid_unittest.cc
+++ public/lib/ftl/random/uuid_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "lib/ftl/random/rand.h"
+#include "lib/ftl/random/uuid.h"
 
 #include <stdint.h>
 
@@ -14,20 +14,11 @@
 namespace ftl {
 namespace {
 
-TEST(Random, RandUint64) {
+TEST(Random, Uuid) {
   for (int i = 0; i < 256; ++i) {
-    uint64_t num = RandUint64();
-    (void)num;
-  }
-}
-
-TEST(Random, RandBytes) {
-  size_t sizes[] = {1, 16, 256, 4096};
-
-  for (auto size : sizes) {
-    std::vector<uint8_t> buf;
-    buf.resize(size);
-    EXPECT_TRUE(RandBytes(buf.data(), buf.size()));
+    auto uuid = GenerateUUID();
+    EXPECT_TRUE(IsValidUUID(uuid));
+    EXPECT_TRUE(IsValidUUIDOutputString(uuid));
   }
 }
 
