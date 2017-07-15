09cc5aa04864e72a77fea222702e59582744911a
Christopher Anderson <cja@google.com>
2017-07-14 17:14:47 -0700

    [vmo] Restore user_id debug_assert
    This assert was disabled so there was time to correct
    PCI's "almost but not entirely incorrect" method of
    generating VMOs and VmObjectDispatchers for PCI
    resources.
    
    MG-826 #done
    Change-Id: I617686ad3158e5ae65e5335fc5b32cc8bf1776f9

--- kernel/kernel/vm/vm_object.cpp
+++ kernel/kernel/vm/vm_object.cpp
@@ -81,8 +81,7 @@ status_t VmObject::set_name(const char* name, size_t len) {
 void VmObject::set_user_id(uint64_t user_id) {
     canary_.Assert();
     AutoLock a(&lock_);
-    // TODO(MG-826): Re-enable this once PCI dispatchers are fixed.
-    //DEBUG_ASSERT(user_id_ == 0);
+    DEBUG_ASSERT(user_id_ == 0);
     user_id_ = user_id;
 }
 
