8b958d8ceca710de2d22ebdf02999ab9b8e99e77
Tim Kilbourn <tkilbourn@google.com>
2017-07-20 11:20:09 -0700

    Directory watcher
    Wraps the mxio_watch_directory function to run a closure when files are
    added to a directory.
    
    Change-Id: Icc6f0f728c7bc51509ccf30e4e4ec2a7495b59a5

---
+++ public/rust/crates/fdio/src/lib.rs
@@ -0,0 +1,109 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Bindings for the Magenta mxio library
+
+extern crate magenta;
+extern crate magenta_sys;
+
+mod mxio_sys;
+
+use magenta_sys as sys;
+
+use std::ffi::CStr;
+use std::fs::File;
+use std::os::raw;
+use std::ffi;
+use std::os::unix::ffi::OsStrExt;
+use std::os::unix::io::AsRawFd;
+use std::path::Path;
+
+/// Events that can occur while watching a directory, including files that already exist prior to
+/// running a Watcher.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum WatchEvent {
+    /// A file was added.
+    AddFile,
+
+    /// A file was removed.
+    RemoveFile,
+
+    /// The Watcher has enumerated all the existing files and has started to wait for new files to
+    /// be added.
+    Idle,
+
+    Unknown(i32),
+
+    #[doc(hidden)]
+    #[allow(non_camel_case_types)]
+    // Try to prevent exhaustive matching since this enum may grow if mxio's events expand.
+    __do_not_match,
+}
+
+impl From<raw::c_int> for WatchEvent {
+    fn from(i: raw::c_int) -> WatchEvent {
+        match i {
+            mxio_sys::WATCH_EVENT_ADD_FILE => WatchEvent::AddFile,
+            mxio_sys::WATCH_EVENT_REMOVE_FILE => WatchEvent::RemoveFile,
+            mxio_sys::WATCH_EVENT_IDLE => WatchEvent::Idle,
+            _ => WatchEvent::Unknown(i),
+        }
+    }
+}
+
+impl From<WatchEvent> for raw::c_int {
+    fn from(i: WatchEvent) -> raw::c_int {
+        match i {
+            WatchEvent::AddFile => mxio_sys::WATCH_EVENT_ADD_FILE,
+            WatchEvent::RemoveFile => mxio_sys::WATCH_EVENT_REMOVE_FILE,
+            WatchEvent::Idle => mxio_sys::WATCH_EVENT_IDLE,
+            WatchEvent::Unknown(i) => i as raw::c_int,
+            _ => -1 as raw::c_int,
+        }
+    }
+}
+
+unsafe extern "C" fn watcher_cb<F>(
+    _dirfd: raw::c_int,
+    event: raw::c_int,
+    fn_: *const raw::c_char,
+    watcher: *const raw::c_void,
+) -> sys::mx_status_t
+where
+    F: Sized + FnMut(WatchEvent, &Path) -> Result<(), magenta::Status>,
+{
+    let cb: &mut F = &mut *(watcher as *mut F);
+    let filename = ffi::OsStr::from_bytes(CStr::from_ptr(fn_).to_bytes());
+    match cb(WatchEvent::from(event), Path::new(filename)) {
+        Ok(()) => sys::MX_OK,
+        Err(e) => e as i32,
+    }
+}
+
+/// Runs the given callback for each file in the directory and each time a new file is
+/// added to the directory.
+///
+/// If the callback returns an error, the watching stops, and the magenta::Status is returned.
+///
+/// This function blocks for the duration of the watch operation. The deadline parameter will stop
+/// the watch at the given (absolute) time and return magenta::Status::ErrTimedOut. A deadline of
+/// magenta::MX_TIME_INFINITE will never expire.
+///
+/// The callback may use magenta::ErrStop as a way to signal to the caller that it wants to
+/// stop because it found what it was looking for. Since this error code is not returned by
+/// syscalls or public APIs, the callback does not need to worry about it turning up normally.
+pub fn watch_directory<F>(dir: &File, deadline: sys::mx_time_t, mut f: F) -> magenta::Status
+where
+    F: Sized + FnMut(WatchEvent, &Path) -> Result<(), magenta::Status>,
+{
+    let cb_ptr: *mut raw::c_void = &mut f as *mut _ as *mut raw::c_void;
+    unsafe {
+        magenta::Status::from_raw(mxio_sys::mxio_watch_directory(
+            dir.as_raw_fd(),
+            watcher_cb::<F>,
+            deadline,
+            cb_ptr,
+        ))
+    }
+}
---
+++ public/rust/crates/fdio/src/mxio_sys.rs
@@ -0,0 +1,27 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use magenta_sys as sys;
+use std::os::raw;
+
+#[allow(non_camel_case_types)]
+pub type watchdir_func_t = unsafe extern "C" fn(
+    dirfd: ::std::os::raw::c_int,
+    event: ::std::os::raw::c_int,
+    fn_: *const ::std::os::raw::c_char,
+    cookie: *const ::std::os::raw::c_void,
+) -> sys::mx_status_t;
+
+extern "C" {
+    pub fn mxio_watch_directory(
+        dirfd: raw::c_int,
+        cb: watchdir_func_t,
+        deadline: sys::mx_time_t,
+        cookie: *const raw::c_void,
+    ) -> sys::mx_status_t;
+}
+
+pub const WATCH_EVENT_ADD_FILE: raw::c_int = 1;
+pub const WATCH_EVENT_REMOVE_FILE: raw::c_int = 2;
+pub const WATCH_EVENT_IDLE: raw::c_int = 3;
