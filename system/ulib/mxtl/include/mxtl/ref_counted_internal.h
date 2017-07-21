cdf2f1ce6600d4eca8883ed3fa18ba514b6a58cf
Dave Bort <dbort@google.com>
2017-07-21 15:20:42 -0700

    [mxtl] RefCounted: Add dead- and dying-object asserts
    Would have helped catch JobDispatcher's inappropriate wrapping of raw
    pointers that could be in the middle of destruction.
    
    Change-Id: Icb9a2990398454bf747aaeece7ca5fd31aa40883

--- system/ulib/mxtl/include/mxtl/ref_counted_internal.h
+++ system/ulib/mxtl/include/mxtl/ref_counted_internal.h
@@ -7,6 +7,7 @@
 #include <magenta/assert.h>
 #include <magenta/compiler.h>
 #include <mxtl/atomic.h>
+#include <mxtl/canary.h>
 
 namespace mxtl {
 namespace internal {
@@ -14,41 +15,85 @@ namespace internal {
 template <bool Enabled>
 class AdoptionValidator;
 
+// This will catch:
+// - Double-adoptions
+// - AddRef/Release without adopting first
+// - Wrapping bad pointers
+// - Re-wrapping raw pointers to destroyed objects
 template <>
 class AdoptionValidator<true> {
 public:
-    void Adopt() {
-        MX_DEBUG_ASSERT(!adopted_);
-        adopted_ = true;
+    ~AdoptionValidator() {
+        magic_ = 0;
     }
-    void ValidateAddRef() {  MX_DEBUG_ASSERT(adopted_); }
-    void ValidateRelease() { MX_DEBUG_ASSERT(adopted_); }
+
+    void Adopt() {
+        AssertMagic(kStartingMagic);
+        magic_ = kAdoptedMagic;
+    }
+    void ValidateAddRef() {
+        AssertMagic(kAdoptedMagic);
+    }
+    void ValidateRelease() {
+        AssertMagic(kAdoptedMagic);
+    }
 
 private:
-    bool adopted_ = false;
+    void AssertMagic(const uint32_t expected) const {
+        MX_DEBUG_ASSERT_MSG(magic_ == expected,
+                            "Invalid magic (expect: 0x%02x, got: 0x%02x)\n",
+                            static_cast<unsigned int>(expected),
+                            static_cast<unsigned int>(magic_));
+    }
+
+    // The object has been constructed, but not yet adopted or destroyed.
+    static constexpr uint32_t kStartingMagic = mxtl::magic("RcST");
+
+    // The object has been constructed and adopted, but not destroyed.
+    static constexpr uint32_t kAdoptedMagic = mxtl::magic("RcAD");
+
+    volatile uint32_t magic_ = kStartingMagic;
 };
 
 template <>
 class AdoptionValidator<false> {
 public:
-    void Adopt() { }
-    void ValidateAddRef() { }
-    void ValidateRelease() { }
+    void Adopt() {}
+    void ValidateAddRef() {}
+    void ValidateRelease() {}
 };
 
 template <bool EnableAdoptionValidator>
 class RefCountedBase {
 protected:
-    constexpr RefCountedBase() : ref_count_(1) {}
+    constexpr RefCountedBase()
+        : ref_count_(1) {}
     ~RefCountedBase() {}
     void AddRef() {
         adoption_validator_.ValidateAddRef();
-        ref_count_.fetch_add(1, memory_order_relaxed);
+        const int rc = ref_count_.fetch_add(1, memory_order_relaxed);
+        if (EnableAdoptionValidator) {
+            // This assertion will fire if someone calls AddRef() on a
+            // ref-counted object that has reached ref_count_ == 0 but has not
+            // been destroyed yet. This could happen by manually calling
+            // AddRef(), or re-wrapping such a pointer with WrapRefPtr() or
+            // RefPtr<T>(T*) (both of which call AddRef()). If the object has
+            // already been destroyed, the magic check in
+            // adoption_validator_.ValidateAddRef() should have caught it before
+            // this point.
+            MX_DEBUG_ASSERT_MSG(rc >= 1, "count %d < 1\n", rc);
+        }
     }
     // Returns true if the object should self-delete.
     bool Release() __WARN_UNUSED_RESULT {
         adoption_validator_.ValidateRelease();
-        if (ref_count_.fetch_sub(1, memory_order_release) == 1) {
+        const int rc = ref_count_.fetch_sub(1, memory_order_release);
+        if (EnableAdoptionValidator) {
+            // This assertion will fire if someone manually calls Release()
+            // on a ref-counted object too many times.
+            MX_DEBUG_ASSERT_MSG(rc >= 1, "count %d < 1\n", rc);
+        }
+        if (rc == 1) {
             atomic_thread_fence(memory_order_acquire);
             return true;
         }
@@ -69,5 +114,5 @@ private:
     AdoptionValidator<EnableAdoptionValidator> adoption_validator_;
 };
 
-}  // namespace internal
-}  // namespace mxtl
+} // namespace internal
+} // namespace mxtl
--- system/utest/mxtl/ref_counted_tests.cpp
+++ system/utest/mxtl/ref_counted_tests.cpp
@@ -2,12 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <mxalloc/new.h>
 #include <pthread.h>
-#include <unittest/unittest.h>
+
+#include <mxalloc/new.h>
 #include <mxtl/algorithm.h>
 #include <mxtl/ref_counted.h>
 #include <mxtl/ref_ptr.h>
+#include <unittest/unittest.h>
+
+// If set, will run tests that expect the process to die (usually due to a
+// failed assertion).
+// TODO(dbort): Turn this on if we ever have real death test support. Until
+// then, leave this code here so it continues to compile and is easy to turn on
+// in a local client for manual testing.
+#define RUN_DEATH_TESTS 0
 
 class DestructionTracker : public mxtl::RefCounted<DestructionTracker> {
 public:
@@ -57,6 +65,139 @@ static bool ref_counted_test() {
     END_TEST;
 }
 
+static bool wrap_dead_pointer_asserts() {
+    BEGIN_TEST;
+    if (!RUN_DEATH_TESTS) {
+        unittest_printf_critical(" [SKIPPING]");
+        return true;
+    }
+
+    bool destroyed = false;
+    DestructionTracker* raw = nullptr;
+    {
+        // Create and adopt a ref-counted object, and let it go out of scope.
+        AllocChecker ac;
+        mxtl::RefPtr<DestructionTracker> ptr =
+            mxtl::AdoptRef(new (&ac) DestructionTracker(&destroyed));
+        EXPECT_TRUE(ac.check(), "");
+        raw = ptr.get();
+        EXPECT_FALSE(destroyed, "");
+    }
+    EXPECT_TRUE(destroyed, "");
+
+    // Wrapping the now-destroyed object should trigger an assertion.
+    mxtl::RefPtr<DestructionTracker> zombie = mxtl::WrapRefPtr(raw);
+    /* NOT REACHED */
+    EXPECT_FALSE(true, "Assertion should have fired");
+
+    END_TEST;
+}
+
+static bool extra_release_asserts() {
+    BEGIN_TEST;
+    if (!RUN_DEATH_TESTS) {
+        unittest_printf_critical(" [SKIPPING]");
+        return true;
+    }
+
+    // Create and adopt a ref-counted object.
+    bool destroyed = false;
+    AllocChecker ac;
+    mxtl::RefPtr<DestructionTracker> ptr =
+        mxtl::AdoptRef(new (&ac) DestructionTracker(&destroyed));
+    EXPECT_TRUE(ac.check(), "");
+    DestructionTracker* raw = ptr.get();
+
+    // Manually release once, which should tell us to delete the object.
+    EXPECT_TRUE(raw->Release(), "");
+    // (But it's not deleted since we didn't listen to the return value
+    // of Release())
+    EXPECT_FALSE(destroyed, "");
+
+    // Manually releasing again should trigger the assertion.
+    __UNUSED bool unused = raw->Release();
+    /* NOT REACHED */
+    EXPECT_FALSE(true, "Assertion should have fired");
+
+    END_TEST;
+}
+
+static bool wrap_after_last_release_asserts() {
+    BEGIN_TEST;
+    if (!RUN_DEATH_TESTS) {
+        unittest_printf_critical(" [SKIPPING]");
+        return true;
+    }
+
+    // Create and adopt a ref-counted object.
+    bool destroyed = false;
+    AllocChecker ac;
+    mxtl::RefPtr<DestructionTracker> ptr =
+        mxtl::AdoptRef(new (&ac) DestructionTracker(&destroyed));
+    EXPECT_TRUE(ac.check(), "");
+    DestructionTracker* raw = ptr.get();
+
+    // Manually release once, which should tell us to delete the object.
+    EXPECT_TRUE(raw->Release(), "");
+    // (But it's not deleted since we didn't listen to the return value
+    // of Release())
+    EXPECT_FALSE(destroyed, "");
+
+    // Adding another ref (by wrapping) should trigger the assertion.
+    mxtl::RefPtr<DestructionTracker> zombie = mxtl::WrapRefPtr(raw);
+    /* NOT REACHED */
+    EXPECT_FALSE(true, "Assertion should have fired");
+
+    END_TEST;
+}
+
+static bool unadopted_add_ref_asserts() {
+    BEGIN_TEST;
+    if (!RUN_DEATH_TESTS) {
+        unittest_printf_critical(" [SKIPPING]");
+        return true;
+    }
+
+    // An un-adopted ref-counted object.
+    bool destroyed = false;
+    DestructionTracker obj(&destroyed);
+
+    // Adding a ref (by wrapping) without adopting first should trigger an
+    // assertion.
+    mxtl::RefPtr<DestructionTracker> unadopted = mxtl::WrapRefPtr(&obj);
+    /* NOT REACHED */
+    EXPECT_FALSE(true, "Assertion should have fired");
+
+    END_TEST;
+}
+
+static bool unadopted_release_asserts() {
+    BEGIN_TEST;
+    if (!RUN_DEATH_TESTS) {
+        unittest_printf_critical(" [SKIPPING]");
+        return true;
+    }
+
+    // An un-adopted ref-counted object.
+    bool destroyed = false;
+    DestructionTracker obj(&destroyed);
+
+    // Releasing without adopting first should trigger an assertion.
+    __UNUSED bool unused = obj.Release();
+    /* NOT REACHED */
+    EXPECT_FALSE(true, "Assertion should have fired");
+
+    END_TEST;
+}
+
 BEGIN_TEST_CASE(ref_counted_tests)
 RUN_NAMED_TEST("Ref Counted", ref_counted_test)
+RUN_NAMED_TEST("Wrapping dead pointer should assert", wrap_dead_pointer_asserts)
+RUN_NAMED_TEST("Extra release should assert", extra_release_asserts)
+RUN_NAMED_TEST("Wrapping zero-count pointer should assert",
+               wrap_after_last_release_asserts)
+RUN_NAMED_TEST("AddRef on unadopted object should assert",
+               unadopted_add_ref_asserts)
+RUN_NAMED_TEST("Release on unadopted object should assert",
+               unadopted_release_asserts)
 END_TEST_CASE(ref_counted_tests);
