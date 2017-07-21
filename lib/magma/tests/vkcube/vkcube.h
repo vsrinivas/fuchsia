9b85c139089284ef9c18caaa3b3f818c01e11662
Michael Jurka <mikejurka@google.com>
2017-07-21 14:35:24 -0700

    Revert "Add a header file for vkcube so it can be used from elsewhere."
    We don't need this any more for demos, and reverting
    will make future updates easier.
    
    This reverts commit 3eb07b24e98d0a05c0d76732f23822578d548963.
    
    Change-Id: I7393736a8d22aeb85c0323f81927e8c59d9d936f

--- lib/magma/tests/vkcube/vkcube.h
+++ lib/magma/tests/vkcube/vkcube.h
@@ -1,8 +1,5 @@
 // Copyright 2016 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-extern "C" {
-#include "vkcube.h"
-}
 
-int main(int argc, char** argv) { return test_vk_cube(argc, argv); }
+int test_vk_cube(int argc, char** argv);
