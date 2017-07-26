ff80390dd45556247529c550d9327f2eb738d42e
James Tucker <jftucker@gmail.com>
2017-07-26 16:58:48 -0700

    Add DID 0x5926 for Iris 640
    Change-Id: I9616b42d183500b989400c5d1ec6d4731ee0fccb

--- lib/magma/tests/helper/platform_device_helper.h
+++ lib/magma/tests/helper/platform_device_helper.h
@@ -23,8 +23,9 @@ public:
             case 0x1916: // Intel(R) HD Graphics 520 (Skylake GT2)
             case 0x191E: // Intel(R) HD Graphics 515 (Skylake GT2)
             case 0x193b: // Intel(R) Iris Pro Graphics 580 (Skylake GT4e)
-            case 0x5916:  // Intel(R) HD Graphics 620 (Kabylake GT2)
-            case 0x591E:  // Intel(R) HD Graphics 615 (Kabylake GT2)
+            case 0x5916: // Intel(R) HD Graphics 620 (Kabylake GT2)
+            case 0x591E: // Intel(R) HD Graphics 615 (Kabylake GT2)
+            case 0x5926: // Intel(R) Iris Graphics 640 (Kabylake GT3e)
               return true;
         }
         return false;
