ioito/magenta

[lib][pci] Add library for locked access to config space
This is the first part of a greater (eventually userspace)
pci library for pio access to config space.

Change-Id: I953309e51a6da5dd959a52ec1e7259c0d6dcccf0

kernel/lib/pci/rules.mk
@@ -1,0 +1,16 @@
+# Copyright 2017 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+KERNEL_INCLUDES += $(LOCAL_DIR)/include
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/pio.c
+
+include make/module.mk

vsrinivas/fuchsia

kernel/lib/pci/rules.mk
@@ -1,0 +1,16 @@
+# Copyright 2017 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+KERNEL_INCLUDES += $(LOCAL_DIR)/include
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/pio.cpp
+
+include make/module.mk