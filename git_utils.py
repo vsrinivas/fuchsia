3fa398e050391173ba4fa4353c2486e88ecde373
Przemyslaw Pietrzkiewicz <ppi@google.com>
2017-07-31 16:40:11 +0200

    Add git-file-tidy script to run clang-tidy on all modified files.
    The tool is like the existing `git-file-format`, but for linting using
    clang-tidy. It takes care of generating the compilation database and
    only lints the files that are modified, staged, or were touched by
    commits not in the tracked branch (local commits).
    
    This patch also changes/fixes the behavior of git-file-format --all, so
    that only files in the subtree of the current working directory are
    formatted.
    
    LE-277 add git-file-tidy
    
    Change-Id: Ie5fa9bee2f1f11e706704fc17c365e16e581cd6b

---
+++ git_utils.py
@@ -0,0 +1,58 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os.path
+import subprocess
+
+
+def _get_diff_base():
+    """Returns the newest local commit that is also in the upstream branch, or
+    "HEAD" if there is no upstream branch.
+    """
+    try:
+        upstream = subprocess.check_output([
+            "git", "rev-parse", "--abbrev-ref", "--symbolic-full-name", "@{u}"
+        ]).strip()
+        # Get local commits not in upstream.
+        local_commits = filter(
+            len,
+            subprocess.check_output(
+                ["git", "rev-list", "HEAD", "^" + upstream, "--"]).split("\n"))
+        if not local_commits:
+            return "HEAD"
+
+        # Return parent of the oldest commit.
+        return subprocess.check_output(
+            ["git", "rev-parse", local_commits[-1] + "^"]).strip()
+
+    except subprocess.CalledProcessError:
+        return "HEAD"
+
+
+def get_git_root():
+    """Returns the path of the root of the git repository."""
+    return subprocess.check_output(["git", "rev-parse",
+                                    "--show-toplevel"]).strip()
+
+
+def get_diff_files():
+    """Returns absolute paths to files that are locally modified, staged or
+    touched by any commits introduced on the local branch.
+    """
+
+    list_command = [
+        "git", "diff-index", "--name-only",
+        _get_diff_base()
+    ]
+    git_root_path = get_git_root()
+    paths = filter(len, subprocess.check_output(list_command).split("\n"))
+    return [ os.path.join(git_root_path, x) for x in paths ]
+
+def get_all_files():
+    """Returns absolute paths to all files in the git repo under the current
+    working directory.
+    """
+    list_command = ["git", "ls-files"]
+    paths = filter(len, subprocess.check_output(list_command).split("\n"))
+    return [ os.path.abspath(x) for x in paths ]
