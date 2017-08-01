0d5a3b3f75c84bfbd300c796254cfe0cb5682117
Haowei Wu <haowei@google.com>
2017-08-01 16:50:31 -0700

    [build] Support DISABLE_UTEST=true make option
    In this mode, the test cases under system/utest and thrid_party/utest
    will be skipped during build process. This will help reduce the false
    positives reported by static analyzer as most test cases do not clean up
    acquired resources during test failures.
    
    Change-Id: I01482eba7dc6f927d361398f67154560df69502e

--- kernel/project/virtual/user.mk
+++ kernel/project/virtual/user.mk
@@ -21,13 +21,16 @@ MODULES += $(patsubst %/rules.mk,%,$(wildcard system/core/*/rules.mk))
 MODULES += $(patsubst %/rules.mk,%,$(wildcard system/dev/*/*/rules.mk))
 MODULES += $(patsubst %/rules.mk,%,$(wildcard system/uapp/*/rules.mk))
 MODULES += $(patsubst %/rules.mk,%,$(wildcard system/ulib/*/rules.mk))
+ifeq ($(call TOBOOL,$(DISABLE_UTEST)),false)
 MODULES += $(patsubst %/rules.mk,%,$(wildcard system/utest/*/rules.mk))
+endif
 
 # include all uapp, udev, ulib and utest from third_party/...
 MODULES += $(patsubst %/rules.mk,%,$(wildcard third_party/uapp/*/rules.mk))
 MODULES += $(patsubst %/rules.mk,%,$(wildcard third_party/udev/*/rules.mk))
 MODULES += $(patsubst %/rules.mk,%,$(wildcard third_party/ulib/*/rules.mk))
+ifeq ($(call TOBOOL,$(DISABLE_UTEST)),false)
 MODULES += $(patsubst %/rules.mk,%,$(wildcard third_party/utest/*/rules.mk))
+endif
 
 EXTRA_BUILDDEPS += $(USER_BOOTDATA)
-
