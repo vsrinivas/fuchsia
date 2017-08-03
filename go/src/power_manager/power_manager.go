f5399207cf15aadd796e08be9537387c3284777f
Ankur Mittal <anmittal@google.com>
2017-08-02 17:05:35 -0700

    Add implementation for power manager
    This is basic manager written in golang. This would be replaced with
    rust code as sson as it becomes viable.
    
    Change-Id: Ifaba3f78bbbfe02c721938ab63bdacc7970de3c1

---
+++ go/src/power_manager/power_manager.go
@@ -0,0 +1,171 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"application/lib/app/context"
+	"bufio"
+	"fidl/bindings"
+	"flag"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"sync"
+	"syscall/mx"
+	"syscall/mx/mxerror"
+	"time"
+
+	"apps/power-service/services/power_manager"
+)
+
+var (
+	updateWaitTimeFlag uint
+	logger             = log.New(os.Stdout, "power_manager: ", log.Lshortfile)
+	re                 = regexp.MustCompile("^(c?)([0-9][0-9]?[0-9]?)%$")
+)
+
+func init() {
+	// TODO: Make is interrupt based, tracking BUG: MG-996.
+	flag.UintVar(&updateWaitTimeFlag, "update-wait-time", 60, "Time to sleep between status update in seconds.")
+}
+
+type PowerManager struct {
+	mu             sync.Mutex
+	battery_status power_manager.BatteryStatus
+	watchers       []*power_manager.PowerManagerWatcher_Proxy
+}
+
+func (pm *PowerManager) GetBatteryStatus() (power_manager.BatteryStatus, error) {
+	logger.Println("GetBatteryStatus")
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+
+	if pm.battery_status.Status == power_manager.Status_NotInitialized {
+		pm.mu.Unlock()
+		pm.updateStatus()
+		pm.mu.Lock()
+	}
+
+	return pm.battery_status, nil
+}
+
+func (pm *PowerManager) Watch(watcher power_manager.PowerManagerWatcher_Pointer) error {
+	pmw := power_manager.NewProxyForPowerManagerWatcher(watcher, bindings.GetAsyncWaiter())
+	pm.mu.Lock()
+	pm.watchers = append(pm.watchers, pmw)
+	pm.mu.Unlock()
+	go pmw.OnChangeBatteryStatus(pm.battery_status)
+	return nil
+}
+
+// Updates the status and returns false if battery status
+// cannot be updated in future.
+func (pm *PowerManager) updateStatus() bool {
+	logger.Println("update status")
+
+	f, err := os.Open("/dev/class/battery/000")
+	if err != nil {
+		logger.Printf("Error while getting status: %s\n", err)
+		pm.mu.Lock()
+		pm.battery_status.Status = power_manager.Status_NotAvailable
+		pm.mu.Unlock()
+		return false
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+	b := make([]byte, 10)
+	n, err := r.Read(b)
+	if err != nil {
+		logger.Printf("Warning: Error while getting status: %s\n", err)
+		// Not fatal
+		return true
+	}
+	if n == 0 {
+		logger.Printf("Warning: Battery status format blank.")
+		// Not fatal
+		return true
+	}
+
+	m := re.FindStringSubmatch(string(b[:n-1]))
+	if len(m) != 3 {
+		logger.Printf("Warning: Battery status format wrong: %s, %v\n", string(b[:n-1]), m)
+		// Not fatal
+		return true
+	}
+
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	oldStatus := pm.battery_status
+
+	pm.battery_status.Charging = m[1] == "c"
+	i, _ := strconv.Atoi(m[2])
+	pm.battery_status.Level = uint16(i)
+	pm.battery_status.Status = power_manager.Status_Ok
+	logger.Printf("Battery status: %v", pm.battery_status)
+
+	if oldStatus != pm.battery_status {
+		for _, pmw := range pm.watchers {
+			go pmw.OnChangeBatteryStatus(pm.battery_status)
+		}
+	}
+
+	return true
+}
+
+func (pm *PowerManager) Bind(r power_manager.PowerManager_Request) {
+	s := r.NewStub(pm, bindings.GetAsyncWaiter())
+
+	go func() {
+		for {
+			if err := s.ServeRequest(); err != nil {
+				if mxerror.Status(err) != mx.ErrPeerClosed {
+					log.Println(err)
+				}
+				break
+			}
+		}
+	}()
+}
+
+func main() {
+	logger.Println("start")
+	defer logger.Println("stop")
+
+	watcher, err := NewWatcher("/dev/class/battery")
+	if err != nil {
+		logger.Printf("Error while watching device: %s\n", err)
+		return
+	}
+	pm := &PowerManager{
+		battery_status: power_manager.BatteryStatus{
+			Status:   power_manager.Status_NotInitialized,
+			Level:    uint16(0),
+			Charging: false,
+		},
+	}
+
+	c := context.CreateFromStartupInfo()
+	c.OutgoingService.AddService(&power_manager.PowerManager_ServiceBinder{pm})
+	c.Serve()
+
+	for file := range watcher.C {
+		if file == "000" {
+			// File found, get status
+			watcher.Stop()
+			break
+		}
+	}
+	go func() {
+		for {
+			if !pm.updateStatus() {
+				break
+			}
+			time.Sleep(time.Duration(updateWaitTimeFlag) * time.Second)
+		}
+	}()
+
+	select {}
+}
---
+++ go/src/power_manager/power_manager_test.go
@@ -0,0 +1,93 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"sync/atomic"
+	"syscall/mx"
+	"syscall/mx/mxerror"
+	"testing"
+	"time"
+
+	"application/lib/app/context"
+	"fidl/bindings"
+
+	"apps/power-service/services/power_manager"
+)
+
+type ClientMock struct {
+	pm *power_manager.PowerManager_Proxy
+}
+
+type WatcherMock struct {
+	called uint32
+	pmw    *power_manager.PowerManagerWatcher_Proxy
+}
+
+func (pmw *WatcherMock) OnChangeBatteryStatus(bs power_manager.BatteryStatus) error {
+	atomic.AddUint32(&pmw.called, 1)
+	return nil
+}
+
+func TestProjectManager(t *testing.T) {
+	ctx := context.CreateFromStartupInfo()
+	pmClient := &ClientMock{}
+	r, p := pmClient.pm.NewRequest(bindings.GetAsyncWaiter())
+	pmClient.pm = p
+	ctx.ConnectToEnvService(r)
+	bt, err := pmClient.pm.GetBatteryStatus()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bt.Status == power_manager.Status_NotInitialized {
+		t.Fatalf("Status should not be NotInitialized")
+	}
+	pmClient.pm.Close()
+}
+
+func TestProjectManagerWatcher(t *testing.T) {
+	ctx := context.CreateFromStartupInfo()
+	pmClient := &ClientMock{}
+	r, p := pmClient.pm.NewRequest(bindings.GetAsyncWaiter())
+	pmClient.pm = p
+	ctx.ConnectToEnvService(r)
+
+	pmWatcher := &WatcherMock{called: 0}
+	rw, pw := power_manager.NewChannelForPowerManagerWatcher()
+	s := rw.NewStub(pmWatcher, bindings.GetAsyncWaiter())
+
+	go func() {
+		for {
+			if err := s.ServeRequest(); err != nil {
+				if mxerror.Status(err) != mx.ErrPeerClosed {
+					log.Println(err)
+				}
+				break
+			}
+		}
+	}()
+
+	err := pmClient.pm.Watch(pw)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	timeToWait := 5000 // in ms
+	timeToSleep := 500 // in ms
+	val := uint32(0)
+	for i := 0; i < timeToWait/timeToSleep; i++ {
+		val = atomic.LoadUint32(&pmWatcher.called)
+		if val == 0 {
+			time.Sleep(time.Duration(timeToSleep) * time.Millisecond)
+		} else {
+			break
+		}
+	}
+
+	if val == 0 {
+		t.Fatalf("Watcher should have been called by now")
+	}
+}
