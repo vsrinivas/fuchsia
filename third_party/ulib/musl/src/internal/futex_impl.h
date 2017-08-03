de4889d2733136fd3f2b351252dcc54b14949f59
George Kulakowski <kulakowski@google.com>
2017-08-02 18:21:00 -0700

    [musl] Deduplicate the requeue-ing lock abstraction that condvars use
    Change-Id: I95319f957f8190420e1c3ddc2a67f25facaf6ece

--- third_party/ulib/musl/src/internal/futex_impl.h
+++ third_party/ulib/musl/src/internal/futex_impl.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "atomic.h"
+
 #include <limits.h>
 #include <magenta/syscalls.h>
 #include <stdatomic.h>
@@ -9,4 +11,29 @@ static inline void __wake(atomic_int* futex, int cnt) {
     if (cnt < 0)
         cnt = INT_MAX;
     _mx_futex_wake(futex, cnt);
+}
+
+/* Self-synchronized-destruction-safe lock functions */
+#define UNLOCKED 0
+#define LOCKED_NO_WAITERS 1
+#define LOCKED_MAYBE_WAITERS 2
+
+static inline void lock(atomic_int* l) {
+    if (a_cas_shim(l, UNLOCKED, LOCKED_NO_WAITERS)) {
+        a_cas_shim(l, LOCKED_NO_WAITERS, LOCKED_MAYBE_WAITERS);
+        do
+            __wait(l, UNLOCKED, LOCKED_MAYBE_WAITERS);
+        while (a_cas_shim(l, UNLOCKED, LOCKED_MAYBE_WAITERS));
+    }
+}
+
+static inline void unlock(atomic_int* l) {
+    if (atomic_exchange(l, UNLOCKED) == LOCKED_MAYBE_WAITERS)
+        __wake(l, 1);
+}
+
+static inline void unlock_requeue(atomic_int* l, mx_futex_t* r) {
+    atomic_store(l, UNLOCKED);
+    _mx_futex_requeue(l, /* wake count */ 0, /* l futex value */ UNLOCKED,
+                      r, /* requeue count */ 1);
 }
