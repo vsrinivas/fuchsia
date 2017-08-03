e9cc47b8e5e543b0682e14fdc7a4d07cef95983d
Jeff Brown <jeffbrown@google.com>
2017-08-03 15:39:37 -0700

    Remove unused includes.
    Change-Id: I1a6780fa13ba3034a43c695635168898376d74ca

--- bin/ui/src/scene_manager/resources/gpu_memory.h
+++ bin/ui/src/scene_manager/resources/gpu_memory.h
@@ -9,7 +9,6 @@
 #include "apps/mozart/src/scene_manager/resources/memory.h"
 #include "apps/mozart/src/scene_manager/util/error_reporter.h"
 #include "escher/vk/gpu_mem.h"
-#include "lib/mtl/vmo/shared_vmo.h"
 
 namespace scene_manager {
 
