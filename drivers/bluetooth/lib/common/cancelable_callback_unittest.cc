8dd7ae5c9c687547f09091d3cdaa74c08789f52a
Arman Uguray <armansito@google.com>
2017-08-02 17:32:42 -0700

    [common] Introduce CancelableCallback
    Added a way to create functors that can be canceled safely
    from other threads. CancelableCallbacks are obtained from
    a CancelableCallbackFactory and refer to a common shared
    variable and a lock. The following guarantees are provided:
    
      * Once CancelableCallbackFactory::CancelAll() returns, all
        previously vended CancelableCallbacks become a NOP.
    
      * If CancelAll() is called concurrently with a CancelableCallback
        (which is running on a different thread), CancelAll() blocks.
        This allows callers to manage the destruction of shared
        resources that are bound to a CancelableCallback in a way that
        does not race with the execution of the CancelableCallback.
    
    Change-Id: I3eecfa854b2516d04bede3d13b2e92517173025b

---
+++ drivers/bluetooth/lib/common/cancelable_callback_unittest.cc
@@ -0,0 +1,100 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "apps/bluetooth/lib/common/cancelable_callback.h"
+
+#include <thread>
+#include <unordered_map>
+
+#include "gtest/gtest.h"
+
+#include "lib/ftl/synchronization/sleep.h"
+#include "lib/ftl/time/stopwatch.h"
+#include "lib/mtl/tasks/message_loop.h"
+#include "lib/mtl/threading/create_thread.h"
+
+namespace bluetooth {
+namespace common {
+namespace {
+
+TEST(CancelableCallbackTest, CancelAndRunOnSameThread) {
+  CancelableCallbackFactory<void()> factory;
+
+  EXPECT_FALSE(factory.canceled());
+
+  bool called = false;
+  auto callback = factory.MakeTask([&called] { called = true; });
+
+  callback();
+  EXPECT_TRUE(called);
+
+  called = false;
+  factory.CancelAll();
+  EXPECT_TRUE(factory.canceled());
+
+  callback();
+  EXPECT_FALSE(called);
+}
+
+TEST(CancelableCallbackTest, CancelAndRunOnDifferentThreads) {
+  CancelableCallbackFactory<void()> factory;
+  EXPECT_FALSE(factory.canceled());
+
+  ftl::RefPtr<ftl::TaskRunner> thrd_runner;
+  auto thrd = mtl::CreateThread(&thrd_runner, "CancelableCallbackTest thread");
+
+  bool called = false;
+  auto cb = factory.MakeTask([&called] { called = true; });
+
+  // Make sure the task is canceled before it gets run.
+  factory.CancelAll();
+  EXPECT_TRUE(factory.canceled());
+
+  thrd_runner->PostTask(cb);
+  thrd_runner->PostTask([] { mtl::MessageLoop::GetCurrent()->QuitNow(); });
+
+  if (thrd.joinable()) thrd.join();
+
+  EXPECT_FALSE(called);
+}
+
+TEST(CancelableCallbackTest, CancelAllBlocksDuringCallback) {
+  constexpr int64_t kBlockTimeMs = 100;
+
+  CancelableCallbackFactory<void()> factory;
+  EXPECT_FALSE(factory.canceled());
+
+  std::mutex mtx;
+  std::condition_variable cv;
+  bool ready = false;
+
+  auto callback = [&] {
+    // This makes sure that CancelAll() is called after this.
+    {
+      std::lock_guard<std::mutex> lock(mtx);
+      ready = true;
+    }
+    cv.notify_one();
+
+    ftl::SleepFor(ftl::TimeDelta::FromMilliseconds(kBlockTimeMs));
+  };
+
+  std::thread thrd(factory.MakeTask(callback));
+  thrd.detach();
+
+  std::unique_lock<std::mutex> lock(mtx);
+  cv.wait(lock, [&ready] { return ready; });
+
+  ftl::Stopwatch sw;
+  sw.Start();
+
+  // This should block for at least |kBlockTimeMs| milliseconds as that is how long |callback| will
+  // sleep for.
+  factory.CancelAll();
+  EXPECT_GE(sw.Elapsed().ToMilliseconds(), kBlockTimeMs);
+}
+
+}  // namespace
+}  // namespace common
+}  // namespace bluetooth
