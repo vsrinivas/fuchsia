6c3ec7291c4f7f252574c6406a4601cbdf3a968d
John Grossman <johngro@google.com>
2017-08-04 14:02:00 -0700

    Add a helper function to compute a set of format ranges.
    Add a helper function to compute a set of format ranges given converter
    capabilites read from codec registers.
    
    Change-Id: I2002f0458fbe084c18db0bb120339aabdbc92b5f

--- intel-hda/utils/utils.h
+++ intel-hda/utils/utils.h
@@ -4,11 +4,15 @@
 
 #pragma once
 
+#include <magenta/device/audio.h>
 #include <magenta/types.h>
 #include <mx/channel.h>
 #include <mx/handle.h>
 #include <mx/vmo.h>
 #include <mxtl/type_support.h>
+#include <mxtl/vector.h>
+
+#include "drivers/audio/intel-hda/utils/codec-caps.h"
 
 namespace audio {
 namespace intel_hda {
@@ -31,6 +35,13 @@ mx_status_t ConvertHandle(mx::handle* abstract_handle, T* concrete_handle) {
     concrete_handle->reset(abstract_handle->release());
     return MX_OK;
 }
+
+// Generate a vector of audio stream format ranges given the supplied sample
+// capabilities and max channels.
+mx_status_t MakeFormatRangeList(const SampleCaps& sample_caps,
+                                uint32_t max_channels,
+                                mxtl::Vector<audio_stream_format_range_t>* ranges);
+
 
 }  // namespace intel_hda
 }  // namespace audio
