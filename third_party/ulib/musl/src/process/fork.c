e886377c1e6ee2fcd9142aaa3b360b1553a1f00b
George Kulakowski <kulakowski@google.com>
2017-08-08 16:31:12 -0700

    [musl][fork] Fix the ENOSYS return in fork
    When removing SYS_clone, I unwrapped this particular syscall() macro
    wrongly. This extra-bogus return is confusing some test suites.
    
    Change-Id: If5a1e32cd421f1ce79b078194af25e97adbf026e

--- third_party/ulib/musl/src/process/fork.c
+++ third_party/ulib/musl/src/process/fork.c
@@ -8,19 +8,8 @@ static void dummy(int x) {}
 weak_alias(dummy, __fork_handler);
 
 pid_t fork(void) {
-    pid_t ret;
     __fork_handler(-1);
-    // TODO(kulakowski) Some level of fork emulation.
-    ret = ENOSYS;
-
-    if (!ret) {
-        // TODO(kulakowski): NB: fork assumes that the calling thread
-        // is a pthread, and that the created thread in the new
-        // process will therefore also be a pthread.
-        pthread_t self = __pthread_self();
-        if (self == NULL)
-            __builtin_trap();
-    }
-    __fork_handler(!ret);
-    return ret;
+    __fork_handler(0);
+    errno = ENOSYS;
+    return -1;
 }
