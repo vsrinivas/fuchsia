7ce4fa9ffad63a3954fa126d63d7916ad649542e
Andrew Krieger <andrewkrieger@google.com>
2017-08-08 11:05:33 -0700

    [kernel] Minor fix in cmdline_get
    Previously cmdline_get would match prefixes. For example, if you pass
        kernel.entropy.test=1 kernel.entropy=deadbeef
    then the call
        cmdline_get("kernel.entropy")
    would (before this commit) return the value ".test=1". After this
    change, the prefix-match is ignored, so the example returns "deadbeef".
    
    Change-Id: I8bfd8c7c65211b4d9f592f729f64a7ac2df0423b

--- kernel/kernel/cmdline.c
+++ kernel/kernel/cmdline.c
@@ -71,7 +71,7 @@ const char* cmdline_get(const char* key) {
     unsigned sz = strlen(key);
     const char* ptr = __kernel_cmdline;
     for (;;) {
-        if (!strncmp(ptr, key, sz)) {
+        if (!strncmp(ptr, key, sz) && (ptr[sz] == '=' || ptr[sz] == '\0')) {
             break;
         }
         ptr = strchr(ptr, 0) + 1;
