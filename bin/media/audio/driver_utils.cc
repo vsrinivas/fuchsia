81c544558f2c3ede307bc15a2673d04893f13507
John Grossman <johngro@google.com>
2017-08-08 13:16:52 -0700

    apps/media: Add stream type conversion helpers.
    Add some helper routines for converting between driver stream types
    and media framework stream types.  Hopefully, these can be eliminated
    if we manage to unify stream type encoding when FIDL2 becomes real.
    
    Change-Id: I3fd7f72723e698b0bac7fe004b4dccbd1f888430

---
+++ bin/media/audio/driver_utils.cc
@@ -0,0 +1,114 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <audio-proto-utils/format-utils.h>
+#include <map>
+
+#include "apps/media/src/audio/driver_utils.h"
+
+namespace media {
+namespace driver_utils {
+
+namespace {
+static constexpr audio_sample_format_t AUDIO_SAMPLE_FORMAT_UNSIGNED_8BIT =
+  static_cast<audio_sample_format_t>(AUDIO_SAMPLE_FORMAT_8BIT |
+                                     AUDIO_SAMPLE_FORMAT_FLAG_UNSIGNED);
+
+namespace AST {
+using SampleFormat = AudioStreamType::SampleFormat;
+}
+
+static const std::map<audio_sample_format_t, AST::SampleFormat>
+  kDriverSampleFormatToSampleFormatMap = {
+  { AUDIO_SAMPLE_FORMAT_UNSIGNED_8BIT, AST::SampleFormat::kUnsigned8 },
+  { AUDIO_SAMPLE_FORMAT_16BIT,         AST::SampleFormat::kSigned16 },
+  { AUDIO_SAMPLE_FORMAT_24BIT_IN32,    AST::SampleFormat::kSigned24In32 },
+  { AUDIO_SAMPLE_FORMAT_32BIT_FLOAT,   AST::SampleFormat::kFloat },
+};
+
+static const std::map<AST::SampleFormat, audio_sample_format_t>
+  kSampleFormatToDriverSampleFormatMap = {
+  { AST::SampleFormat::kUnsigned8,    AUDIO_SAMPLE_FORMAT_UNSIGNED_8BIT },
+  { AST::SampleFormat::kSigned16,     AUDIO_SAMPLE_FORMAT_16BIT },
+  { AST::SampleFormat::kSigned24In32, AUDIO_SAMPLE_FORMAT_24BIT_IN32 },
+  { AST::SampleFormat::kFloat,        AUDIO_SAMPLE_FORMAT_32BIT_FLOAT },
+};
+
+}  // anon namespace
+
+bool SampleFormatToDriverSampleFormat(
+    AudioStreamType::SampleFormat sample_format,
+    audio_sample_format_t* driver_sample_format_out) {
+  FTL_DCHECK(driver_sample_format_out != nullptr);
+
+  auto iter = kSampleFormatToDriverSampleFormatMap.find(sample_format);
+  if (iter == kSampleFormatToDriverSampleFormatMap.end()) {
+    return false;
+  }
+
+  *driver_sample_format_out = iter->second;
+  return true;
+}
+
+bool DriverSampleFormatToSampleFormat(
+    audio_sample_format_t driver_sample_format,
+    AudioStreamType::SampleFormat* sample_format_out) {
+
+  auto iter = kDriverSampleFormatToSampleFormatMap.find(driver_sample_format);
+  if (iter == kDriverSampleFormatToSampleFormatMap.end()) {
+    return false;
+  }
+
+  *sample_format_out = iter->second;
+  return true;
+}
+
+void AddAudioStreamTypeSets(
+    audio_stream_format_range_t fmt,
+    std::vector<std::unique_ptr<media::StreamTypeSet>>* typeset_target) {
+  FTL_DCHECK(typeset_target != nullptr);
+
+  uint32_t flags = fmt.sample_formats & AUDIO_SAMPLE_FORMAT_FLAG_MASK;
+  uint32_t noflags = fmt.sample_formats & ~AUDIO_SAMPLE_FORMAT_FLAG_MASK;
+
+  for (uint32_t i = 1; noflags != 0; i <<= 1) {
+    if (!(i & noflags))
+      continue;
+
+    noflags &= ~i;
+
+    AudioStreamType::SampleFormat sample_format;
+    auto driver_sample_format = static_cast<audio_sample_format_t>(i | flags);
+    if (!DriverSampleFormatToSampleFormat(driver_sample_format,
+                                          &sample_format)) {
+      FTL_LOG(WARNING) << "Failed to map driver sample format 0x"
+                       << std::hex << driver_sample_format
+                       << " to AudioStreamType::SampleFormat.  Skipping.";
+      continue;
+    }
+
+    if (fmt.flags & ASF_RANGE_FLAG_FPS_CONTINUOUS) {
+      typeset_target->push_back(AudioStreamTypeSet::Create(
+          { AudioStreamType::kAudioEncodingLpcm },
+          sample_format,
+          Range<uint32_t>(fmt.min_channels, fmt.max_channels),
+          Range<uint32_t>(fmt.min_frames_per_second,
+                          fmt.max_frames_per_second)));
+    } else {
+      audio::utils::FrameRateEnumerator enumerator(fmt);
+      for (uint32_t rate : enumerator) {
+        typeset_target->push_back(AudioStreamTypeSet::Create(
+            { AudioStreamType::kAudioEncodingLpcm },
+            sample_format,
+            Range<uint32_t>(fmt.min_channels, fmt.max_channels),
+            Range<uint32_t>(rate, rate)));
+      }
+    }
+  }
+}
+
+
+}  // namespace driver_utils
+}  // namespace media
+
---
+++ bin/media/audio/driver_utils.h
@@ -0,0 +1,36 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <magenta/device/audio.h>
+
+#include "apps/media/src/framework/types/audio_stream_type.h"
+
+namespace media {
+namespace driver_utils {
+
+// Convert a media framework SampleFormat into an audio stream driver
+// sample_format.  Returns true if the conversion succeed, or false if it does
+// not.
+bool SampleFormatToDriverSampleFormat(
+    AudioStreamType::SampleFormat sample_format,
+    audio_sample_format_t* driver_sample_format_out);
+
+// Convert an audio stream driver sample_format into a media framework
+// SampleFormat.  Returns true if the conversion succeed, or false if it does
+// not.
+bool DriverSampleFormatToSampleFormat(
+    audio_sample_format_t driver_sample_format,
+    AudioStreamType::SampleFormat* sample_format_out);
+
+// Convert the supplied driver interface audio_stream_format_t into
+// AudioStreamTypeSets and add them to the target vector of types.
+void AddAudioStreamTypeSets(
+    audio_stream_format_range_t fmt,
+    std::vector<std::unique_ptr<media::StreamTypeSet>>* typeset_target);
+
+}  // namespace driver_utils
+}  // namespace media
+
