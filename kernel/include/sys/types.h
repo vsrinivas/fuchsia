ad38d274b09ddd47bf5d3fd729cb9bd86588e29e
Carlos Pizano <cpu@chromium.org>
2017-08-07 19:18:47 -0700

    [kernel] remove unused type alias
    these typedefs are no longer used in magenta and we don't
    need more names for the same types.
    
    Change-Id: I47841b78e9c01530cb8f03140ba14eaa26c00295

--- kernel/include/sys/types.h
+++ kernel/include/sys/types.h
@@ -21,8 +21,6 @@ typedef unsigned short u_short;
 typedef unsigned int u_int;
 typedef unsigned long u_long;
 
-typedef long long     off_t;
-
 typedef int status_t;
 
 typedef uintptr_t addr_t;
@@ -42,21 +40,6 @@ enum handler_return {
 
 typedef intptr_t ssize_t;
 #define SSIZE_MAX INTPTR_MAX
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
-
-typedef uint8_t u_int8_t;
-typedef uint16_t u_int16_t;
-typedef uint32_t u_int32_t;
-typedef uint64_t u_int64_t;
 
 #define KB (1024UL)
 #define MB (1024UL * KB)
