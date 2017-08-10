ce5b1518580bf1ded8a50dc526b01974fcb1a511
Benjamin Lerman <qsr@chromium.org>
2017-08-10 17:33:34 +0200

    Add HMAC algorithm.
    Change-Id: I9efe60597fbabe5673b1ac679de7f9ebf9022c26

---
+++ bin/ledger/glue/crypto/hmac.h
@@ -0,0 +1,16 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef APPS_LEDGER_SRC_GLUE_CRYPTO_HMAC_H_
+#define APPS_LEDGER_SRC_GLUE_CRYPTO_HMAC_H_
+
+#include "lib/ftl/strings/string_view.h"
+
+namespace glue {
+// Compute the HMAC defined by RFC 2104 using SHA-256 for the hash algorithm.
+// |key| must be at least 256 bits long.
+std::string SHA256HMAC(ftl::StringView key, ftl::StringView data);
+}
+
+#endif  // APPS_LEDGER_SRC_GLUE_CRYPTO_HMAC_H_
