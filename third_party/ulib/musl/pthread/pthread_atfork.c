3ecf8d29dca3e0cf88a3850142d8a6ad66b0b516
George Kulakowski <kulakowski@google.com>
2017-08-11 09:48:48 -0700

    [musl][pthread] Remove pthread_atfork support
    No reason to support this when we won't support fork.
    
    Change-Id: Ifb181fbb110f6b41355c18c75b3ea087dbb3ed76

--- third_party/ulib/musl/pthread/pthread_atfork.c
+++ third_party/ulib/musl/pthread/pthread_atfork.c
@@ -1,53 +1,7 @@
-#include "libc.h"
+#include <errno.h>
 #include <pthread.h>
-#include <threads.h>
-
-static struct atfork_funcs {
-    void (*prepare)(void);
-    void (*parent)(void);
-    void (*child)(void);
-    struct atfork_funcs *prev, *next;
-} * funcs;
-
-static mtx_t lock;
-
-void __fork_handler(int who) {
-    struct atfork_funcs* p;
-    if (!funcs)
-        return;
-    if (who < 0) {
-        mtx_lock(&lock);
-        for (p = funcs; p; p = p->next) {
-            if (p->prepare)
-                p->prepare();
-            funcs = p;
-        }
-    } else {
-        for (p = funcs; p; p = p->prev) {
-            if (!who && p->parent)
-                p->parent();
-            else if (who && p->child)
-                p->child();
-            funcs = p;
-        }
-        mtx_unlock(&lock);
-    }
-}
 
 int pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) {
-    struct atfork_funcs* new = malloc(sizeof *new);
-    if (!new)
-        return -1;
-
-    mtx_lock(&lock);
-    new->next = funcs;
-    new->prev = 0;
-    new->prepare = prepare;
-    new->parent = parent;
-    new->child = child;
-    if (funcs)
-        funcs->prev = new;
-    funcs = new;
-    mtx_unlock(&lock);
-    return 0;
+    errno = ENOSYS;
+    return -1;
 }
--- third_party/ulib/musl/src/process/fork.c
+++ third_party/ulib/musl/src/process/fork.c
@@ -1,15 +1,7 @@
-#include "libc.h"
-#include "pthread_impl.h"
-#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 
-static void dummy(int x) {}
-
-weak_alias(dummy, __fork_handler);
-
 pid_t fork(void) {
-    __fork_handler(-1);
-    __fork_handler(0);
     errno = ENOSYS;
     return -1;
 }
