shreks7/magenta

[usb] Add beginnings of USB peripheral mode support
This change adds three new DDK protocols for USB peripheral support:

- MX_PROTOCOL_USB_DCI
  The DCI stands for "device controller interface".
  This protocol is implemented by drivers for the actual USB controller hardware.
  It is analogous to the MX_PROTOCOL_USB_HCI protocol for the host controller drivers.

- MX_PROTOCOL_USB_DEVICE
  This is the protocol for the "usb-device" driver, which is the platform independent
  layer to the USB peripheral stack.
  This protocol provides ioctls for configuring how the USB peripheral controller
  appears to the host. It supports ioctls for setting the USB device, configuration
  and string descriptors that the host sees.
  The "usb-device" driver creates devmgr devices with protocol MX_PROTOCOL_USB_FUNCTION
  for each interface in the configuration descriptor.

- MX_PROTOCOL_USB_FUNCTION
  This protocol is what various USB function drivers will bind to.
  A USB function driver implements a specific piece of functionality corresponding
  to a USB interface. For example, a USB HID function driver would allow the Magenta
  device to appear as a HID device to the USB host it is connected to.

This change also adds:

- The "usb-device" driver, which binds to MX_PROTOCOL_USB_DCI and creates child devices
  with protocol MX_PROTOCOL_USB_FUNCTION based on the USB configuration descriptor
  set via ioctl_usb_device_set_config_desc()

- The "usb-function-test" driver. This driver doesn't do anything yet, but it implements
  the MX_PROTOCOL_USB_FUNCTION protocol and will be flushed out to do loop back testing
  for USB peripheral mode.

- The "usb-virtual-bus" driver. This driver creates a virtual USB host controller and
  device controller and proxies USB transactions between the two sides.
  This allows testing of the USB peripheral stack on devices without actual USB peripheral
  controller hardware.

- The "usbctl" command line tool. This can be used to enable or disable the USB

system/public/magenta/device/ioctl.h
@@ -57,6 +57,8 @@
 #define IOCTL_FAMILY_WLAN           0x24
 #define IOCTL_FAMILY_PTY            0x25
 #define IOCTL_FAMILY_NETCONFIG      0x26
+#define IOCTL_FAMILY_USB_DEVICE     0x27
+#define IOCTL_FAMILY_USB_VIRT_BUS   0x28
 
 // IOCTL constructor
 // --K-FFNN

vsrinivas/fuchsia

system/public/magenta/device/ioctl.h
@@ -58,6 +58,8 @@
 #define IOCTL_FAMILY_PTY            0x25
 #define IOCTL_FAMILY_NETCONFIG      0x26
 #define IOCTL_FAMILY_ETHERTAP       0x27
+#define IOCTL_FAMILY_USB_DEVICE     0x28
+#define IOCTL_FAMILY_USB_VIRT_BUS   0x29
 
 // IOCTL constructor
 // --K-FFNN