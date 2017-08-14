0f118a2f01a4f096d0d863e28a5f78cbc9bf8ef8
George Kulakowski <kulakowski@google.com>
2017-08-14 16:28:33 -0700

    [musl][threads] Fix a restrict violation between thrd_create and pthread_create
    Change-Id: Ib3874948fdde47d80a349ae465aaac7a62af013a

--- third_party/ulib/musl/src/thread/thrd_create.c
+++ third_party/ulib/musl/src/thread/thrd_create.c
@@ -7,9 +7,16 @@ static int thrd_create_internal(thrd_t* thr, thrd_start_t func, void* arg, const
     pthread_attr_t attrs = DEFAULT_PTHREAD_ATTR;
     attrs.__name = name;
     attrs.__c11 = 1;
-    int ret = pthread_create(thr, &attrs, (void* (*)(void*))func, arg);
+    // pthread_create marks the first (the pthread_t pointer) and last
+    // (the argument pointer) arguments as restrict. thrd_create does
+    // not. As such, to avoid aliasing violations we use a local
+    // thrd_t and then copy the result out, to guarantee that t and
+    // arg do not alias.
+    thrd_t t;
+    int ret = pthread_create(&t, &attrs, (void* (*)(void*))func, arg);
     switch (ret) {
     case 0:
+        *thr = t;
         return thrd_success;
     case EAGAIN:
         return thrd_nomem;
