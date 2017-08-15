3d70315f262ceee6564962047f08a239b91a8b80
Andrew Krieger <andrewkrieger@google.com>
2017-08-14 17:42:04 -0700

    [kernel][entropy] Add locks in HwRngCollector
    Right now, we only use the entropy collectors in single-threaded mode
    during init. Later on, we'll be reseeding in a multi-threaded system,
    so we should take care when accessing singleton RNGs.
    
    Change-Id: I8769e0a4d054e32682ac8079f1d8d61fd1938969

--- kernel/lib/crypto/include/lib/crypto/entropy/hw_rng_collector.h
+++ kernel/lib/crypto/include/lib/crypto/entropy/hw_rng_collector.h
@@ -8,18 +8,40 @@
 
 #include <lib/crypto/entropy/collector.h>
 #include <magenta/types.h>
+#include <mxtl/mutex.h>
 
 namespace crypto {
 
 namespace entropy {
 
+// An implementation of crypto::entropy::Collector that uses hw_rng_draw_entropy
+// as its entropy source. Currently, this is only supported on x86.
 class HwRngCollector : public Collector {
 public:
+    // Gets the current HwRngCollector instance. Returns MX_ERR_NOT_SUPPORTED if
+    // hw_rng_draw_entropy is not supported.
+    //
+    // This function is thread-safe, and the DrawEntropy() method of the global
+    // HwRngCollector instance is also thread-safe.
     static mx_status_t GetInstance(Collector** ptr);
 
+    // Inherited from crypto::entropy::Collector; see comments there.
+    //
+    // Note that this method internally uses a mutex to prevent multiple
+    // accesses. It is safe to call this method from multiple threads, but it
+    // may block.
+    //
+    // TODO(andrewkrieger): Determine what level of thread safety is needed for
+    // RNG reseeding, and support it more uniformly (e.g. have a thread safety
+    // contract for Collector::DrawEntropy, obeyed by all implementations).
+    size_t DrawEntropy(uint8_t* buf, size_t len) override;
+
+private:
     HwRngCollector();
 
-    size_t DrawEntropy(uint8_t* buf, size_t len) override;
+    DISALLOW_COPY_ASSIGN_AND_MOVE(HwRngCollector);
+
+    mxtl::Mutex lock_;
 };
 
 } // namespace entropy
