3589b540af5f05d09d246abb8d4e1513ae96fdc9
George Kulakowski <kulakowski@google.com>
2017-08-15 14:39:31 -0700

    [utest][timers] Remove use of mx::timer::start in timer tests
    Change-Id: Ibc4e932cea348f075799e93c2d68fda150d0d30e

--- system/utest/timers/timers.cpp
+++ system/utest/timers/timers.cpp
@@ -13,15 +13,7 @@
 #include <unistd.h>
 #include <unittest/unittest.h>
 
-namespace {
-
-enum {
-    USE_SET,
-    USE_START
-};
-}  // namespace
-
-static bool basic_test_op(int mode) {
+static bool basic_test() {
     BEGIN_TEST;
     mx::timer timer;
     ASSERT_EQ(mx::timer::create(0, MX_CLOCK_MONOTONIC, &timer), MX_OK);
@@ -34,10 +26,7 @@ static bool basic_test_op(int mode) {
         const auto deadline_timer = mx_deadline_after(MX_MSEC(50));
         const auto deadline_wait = mx_deadline_after(MX_SEC(1));
         // Timer should fire faster than the wait timeout.
-        if (mode == USE_SET)
-            ASSERT_EQ(timer.set(deadline_timer, 0u), MX_OK);
-        else
-            ASSERT_EQ(timer.start(deadline_timer, 0u, 0u), MX_OK);
+        ASSERT_EQ(timer.set(deadline_timer, 0u), MX_OK);
 
         EXPECT_EQ(timer.wait_one(MX_TIMER_SIGNALED, deadline_wait, &pending), MX_OK);
         EXPECT_EQ(pending, MX_TIMER_SIGNALED | MX_SIGNAL_LAST_HANDLE);
@@ -45,7 +34,7 @@ static bool basic_test_op(int mode) {
     END_TEST;
 }
 
-static bool restart_test_op(int mode) {
+static bool restart_test() {
     BEGIN_TEST;
     mx::timer timer;
     ASSERT_EQ(mx::timer::create(0, MX_CLOCK_MONOTONIC, &timer), MX_OK);
@@ -55,10 +44,7 @@ static bool restart_test_op(int mode) {
         const auto deadline_timer = mx_deadline_after(MX_MSEC(500));
         const auto deadline_wait = mx_deadline_after(MX_MSEC(1));
         // Setting a timer already running is equivalent to a cancel + set.
-        if (mode == USE_SET)
-            ASSERT_EQ(timer.set(deadline_timer, 0u), MX_OK);
-        else
-            ASSERT_EQ(timer.start(deadline_timer, 0u, 0u), MX_OK);
+        ASSERT_EQ(timer.set(deadline_timer, 0u), MX_OK);
 
         EXPECT_EQ(timer.wait_one(MX_TIMER_SIGNALED, deadline_wait, &pending), MX_ERR_TIMED_OUT);
         EXPECT_EQ(pending, MX_SIGNAL_LAST_HANDLE);
@@ -75,9 +61,6 @@ static bool invalid_calls() {
 
     ASSERT_EQ(mx::timer::create(0, MX_CLOCK_MONOTONIC, &timer), MX_OK);
 
-    const auto deadline_timer = mx_deadline_after(MX_MSEC(1));
-    ASSERT_EQ(timer.start(deadline_timer, MX_USEC(2), 0u), MX_ERR_NOT_SUPPORTED);
-
     END_TEST;
 }
 
@@ -86,35 +69,8 @@ static bool edge_cases() {
 
     mx::timer timer;
     ASSERT_EQ(mx::timer::create(0, MX_CLOCK_MONOTONIC, &timer), MX_OK);
-    ASSERT_EQ(timer.start(0u, 0u, 0u), MX_OK);
-    ASSERT_EQ(timer.start(0U, MX_TIMER_MIN_PERIOD, 0u), MX_OK);
+    ASSERT_EQ(timer.set(0u, 0u), MX_OK);
 
-    END_TEST;
-}
-
-static bool periodic() {
-    BEGIN_TEST;
-
-    mx::timer timer;
-    ASSERT_EQ(mx::timer::create(0, MX_CLOCK_MONOTONIC, &timer), MX_OK);
-
-    const auto deadline_timer = mx_deadline_after(MX_MSEC(1));
-    const auto period = MX_USEC(500);
-
-    ASSERT_EQ(timer.start(deadline_timer, period, 0u), MX_OK);
-
-    mx_signals_t pending;
-    auto expected_arrival = deadline_timer;
-
-    for (int ix = 0; ix != 100; ++ix) {
-        EXPECT_EQ(timer.wait_one(MX_TIMER_SIGNALED, MX_TIME_INFINITE, &pending), MX_OK);
-        EXPECT_EQ(pending & MX_TIMER_SIGNALED, MX_TIMER_SIGNALED);
-
-        EXPECT_GT(mx_time_get(MX_CLOCK_MONOTONIC), expected_arrival);
-        expected_arrival += period;
-    }
-
-    EXPECT_EQ(timer.cancel(), MX_OK);
     END_TEST;
 }
 
@@ -129,7 +85,7 @@ static bool restart_race() {
     mx::timer timer;
     ASSERT_EQ(mx::timer::create(0, MX_CLOCK_MONOTONIC, &timer), MX_OK);
     while (mx_time_get(MX_CLOCK_MONOTONIC) - start < kTestDuration) {
-        ASSERT_EQ(timer.start(0u, MX_TIMER_MIN_PERIOD, 0u), MX_OK);
+        ASSERT_EQ(timer.set(mx_deadline_after(MX_USEC(100)), 0u), MX_OK);
     }
 
     EXPECT_EQ(timer.cancel(), MX_OK);
@@ -140,7 +96,7 @@ static bool restart_race() {
 // If the timer is already due at the moment it is started then the signal should be
 // asserted immediately.  Likewise canceling the timer should immediately deassert
 // the signal.
-static bool signals_asserted_immediately_op(int mode) {
+static bool signals_asserted_immediately() {
     BEGIN_TEST;
     mx::timer timer;
     ASSERT_EQ(mx::timer::create(0, MX_CLOCK_MONOTONIC, &timer), MX_OK);
@@ -148,10 +104,7 @@ static bool signals_asserted_immediately_op(int mode) {
     for (int i = 0; i < 100; i++) {
         mx_time_t now = mx_time_get(MX_CLOCK_MONOTONIC);
 
-        if (mode == USE_SET)
-            EXPECT_EQ(timer.set(now, 0u), MX_OK);
-        else
-            EXPECT_EQ(timer.start(now, 0u, 0u), MX_OK);
+        EXPECT_EQ(timer.set(now, 0u), MX_OK);
 
         mx_signals_t pending;
         EXPECT_EQ(timer.wait_one(MX_TIMER_SIGNALED, 0u, &pending), MX_OK);
@@ -166,42 +119,14 @@ static bool signals_asserted_immediately_op(int mode) {
     END_TEST;
 }
 
-static bool basic_test_set() {
-    return basic_test_op(USE_SET);
-}
-
-static bool restart_test_set() {
-    return restart_test_op(USE_SET);
-}
-
-static bool signals_asserted_immediately_set() {
-    return signals_asserted_immediately_op(USE_SET);
-}
-
-static bool basic_test_start() {
-    return basic_test_op(USE_START);
-}
-
-static bool restart_test_start() {
-    return restart_test_op(USE_START);
-}
-
-static bool signals_asserted_immediately_start() {
-    return signals_asserted_immediately_op(USE_START);
-}
-
 
 BEGIN_TEST_CASE(timers_test)
 RUN_TEST(invalid_calls)
-RUN_TEST(basic_test_set)
-RUN_TEST(basic_test_start)
-RUN_TEST(restart_test_set)
-RUN_TEST(restart_test_start)
+RUN_TEST(basic_test)
+RUN_TEST(restart_test)
 RUN_TEST(edge_cases)
-RUN_TEST(periodic)
 RUN_TEST(restart_race)
-RUN_TEST(signals_asserted_immediately_set)
-RUN_TEST(signals_asserted_immediately_start)
+RUN_TEST(signals_asserted_immediately)
 END_TEST_CASE(timers_test)
 
 int main(int argc, char** argv) {
