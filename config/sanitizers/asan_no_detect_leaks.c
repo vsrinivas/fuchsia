adb2582047fac449a7a88837a7c370c76f90428c
Roland McGrath <mcgrathr@google.com>
2017-08-16 16:36:12 -0700

    Add configs/variants for sanitizers (asan, ubsan, sancov)
    This lights up the new `select_variant` build argument mechanism.
    e.g., this works:
      select_variant = [
        {
          host = true
          name = ["protoc"]
          variant = "asan-sancov"
        },
        {
          host = true
          dir = ["//third_party/yasm"]
          variant = "asan_no_detect_leaks"
        },
        {
          host = true
          name = ["dart"]
          variant = "asan_no_detect_leaks"
        },
        {
          host = true
          variant = "asan"
        },
      ]
    to build all the host tools with ASan, and protoc also with coverage,
    but the yasm tools and dart with a variant of ASan that disables leak
    checking by default, because these tools as used in the build have
    some leaks.
    
    Change-Id: I0b53475f286acb9693c9a5fbc3162a38cfbbf28b

---
+++ config/sanitizers/asan_no_detect_leaks.c
@@ -0,0 +1,12 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <sanitizer/asan_interface.h>
+
+// This exists to be built into every executable selected to use the
+// asan_no_detect_leaks variant.  ASan applies the options here before
+// looking at the ASAN_OPTIONS environment variable.
+const char* __asan_default_options(void) {
+  return "detect_leaks=0";
+}
