3cc2efa7b9e601a2db0103ee2140174dff58d426
George Kulakowski <kulakowski@google.com>
2017-08-18 10:59:29 -0700

    [uapp][strerror] Add the C11 error space to the strerror utility
    Change-Id: I2cda2e0fc865d9997ad448eb5c624e58dd956a0f

--- system/uapp/strerror/strerror.c
+++ system/uapp/strerror/strerror.c
@@ -7,6 +7,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <threads.h>
+
+static const char* thrd_error_string(int error) {
+    switch (error) {
+    case thrd_success: return "thrd_success";
+    case thrd_busy: return "thrd_busy";
+    case thrd_error: return "thrd_error";
+    case thrd_nomem: return "thrd_nomem";
+    case thrd_timedout: return "thrd_timedout";
+    default: return "<unknown thrd status>";
+    }
+}
 
 int main(int argc, char** argv) {
     for (int idx = 1; idx < argc; idx++) {
@@ -17,8 +29,10 @@ int main(int argc, char** argv) {
         int error = (int)error_long;
         const char* mx_error = mx_status_get_string((mx_status_t)error);
         char* posix_error = strerror(error);
+        const char* thrd_error = thrd_error_string(error);
         printf("Int value: %d\n", error);
         printf("\tMagenta error: %s\n", mx_error);
         printf("\tPosix error: %s\n", posix_error);
+        printf("\tC11 thread error: %s\n", thrd_error);
     }
 }
