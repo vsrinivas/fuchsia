44d3026a69358f2dba6bfdd15be4768360569bd7
Mark Seaborn <mseaborn@google.com>
2017-08-18 19:20:30 -0700

    [kernel] Remove unused copy_to_user_<type>/copy_from_user_<type> functions
    Change-Id: I2e901829dcf7a2d00226e26297ff4425564fc646

--- kernel/lib/user_copy/include/lib/user_copy.h
+++ kernel/lib/user_copy/include/lib/user_copy.h
@@ -19,35 +19,4 @@ inline status_t copy_from_user_unsafe(void* dst, const void* src, size_t len) {
   return arch_copy_from_user(dst, src, len);
 }
 
-// Convenience functions for common data types.
-#define MAKE_COPY_TO_USER_UNSAFE(name, type) \
-    static inline status_t name ## _unsafe(type *dst, type value) { \
-        return copy_to_user_unsafe(dst, &value, sizeof(type)); \
-    }
-
-MAKE_COPY_TO_USER_UNSAFE(copy_to_user_u8, uint8_t);
-MAKE_COPY_TO_USER_UNSAFE(copy_to_user_u16, uint16_t);
-MAKE_COPY_TO_USER_UNSAFE(copy_to_user_32, int32_t);
-MAKE_COPY_TO_USER_UNSAFE(copy_to_user_u32, uint32_t);
-MAKE_COPY_TO_USER_UNSAFE(copy_to_user_u64, uint64_t);
-MAKE_COPY_TO_USER_UNSAFE(copy_to_user_uptr, uintptr_t);
-MAKE_COPY_TO_USER_UNSAFE(copy_to_user_iptr, intptr_t);
-
-#undef MAKE_COPY_TO_USER_UNSAFE
-
-#define MAKE_COPY_FROM_USER_UNSAFE(name, type) \
-    static inline status_t name ## _unsafe(type *dst, const type *src) { \
-        return copy_from_user_unsafe(dst, src, sizeof(type)); \
-    }
-
-MAKE_COPY_FROM_USER_UNSAFE(copy_from_user_u8, uint8_t);
-MAKE_COPY_FROM_USER_UNSAFE(copy_from_user_u16, uint16_t);
-MAKE_COPY_FROM_USER_UNSAFE(copy_from_user_u32, uint32_t);
-MAKE_COPY_FROM_USER_UNSAFE(copy_from_user_32, int32_t);
-MAKE_COPY_FROM_USER_UNSAFE(copy_from_user_u64, uint64_t);
-MAKE_COPY_FROM_USER_UNSAFE(copy_from_user_uptr, uintptr_t);
-MAKE_COPY_FROM_USER_UNSAFE(copy_from_user_iptr, intptr_t);
-
-#undef MAKE_COPY_FROM_USER
-
 __END_CDECLS
