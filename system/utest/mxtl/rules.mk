64cb3c70813277a29c19dd5a155135f876d889a4
Jeff Brown <jeffbrown@google.com>
2017-08-24 15:37:59 -0700

    [mxtl] Compile most of the mxtl tests on the host.
    Yes, they pass.
    
    Two tests are omitted due to lack of support for mxtl::Mutex on the
    host (MG-1053).
    
    Change-Id: I230541667faa8d17a67869787474e2cd9c215e33

--- system/utest/mxtl/rules.mk
+++ system/utest/mxtl/rules.mk
@@ -4,13 +4,7 @@
 
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-MODULE := $(LOCAL_DIR)
-
-MODULE_TYPE := usertest
-
-#MODULE_SRCS += \
-
-MODULE_SRCS += \
+mxtl_common_tests := \
     $(LOCAL_DIR)/algorithm_tests.cpp \
     $(LOCAL_DIR)/array_tests.cpp \
     $(LOCAL_DIR)/atomic_tests.cpp \
@@ -25,9 +19,7 @@ MODULE_SRCS += \
     $(LOCAL_DIR)/intrusive_wavl_tree_tests.cpp \
     $(LOCAL_DIR)/main.c \
     $(LOCAL_DIR)/recycler_tests.cpp \
-    $(LOCAL_DIR)/ref_counted_tests.cpp \
     $(LOCAL_DIR)/ref_ptr_tests.cpp \
-    $(LOCAL_DIR)/slab_allocator_tests.cpp \
     $(LOCAL_DIR)/string_piece_tests.cpp \
     $(LOCAL_DIR)/string_tests.cpp \
     $(LOCAL_DIR)/type_support_tests.cpp \
@@ -35,7 +27,25 @@ MODULE_SRCS += \
     $(LOCAL_DIR)/unique_ptr_tests.cpp \
     $(LOCAL_DIR)/vector_tests.cpp \
 
+mxtl_device_tests := $(mxtl_common_tests)
+
+# These tests need mxtl::Mutex which currently isn't supported on the host.
+# TODO(MG-1053): Support mxtl::Mutex on the host and make these tests work.
+mxtl_device_tests += \
+    $(LOCAL_DIR)/ref_counted_tests.cpp \
+    $(LOCAL_DIR)/slab_allocator_tests.cpp \
+
+mxtl_host_tests := $(mxtl_common_tests)
+
+# Userspace tests.
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := usertest
+
 MODULE_NAME := mxtl-test
+
+MODULE_SRCS := $(mxtl_device_tests)
 
 MODULE_STATIC_LIBS := \
     system/ulib/mxcpp \
@@ -48,3 +58,30 @@ MODULE_LIBS := \
     system/ulib/magenta \
 
 include make/module.mk
+
+# Host tests.
+
+MODULE := $(LOCAL_DIR).hostapp
+
+MODULE_TYPE := hostapp
+
+MODULE_NAME := mxtl-test
+
+MODULE_SRCS := $(mxtl_host_tests)
+
+MODULE_COMPILEFLAGS := \
+    -Isystem/ulib/mxtl/include \
+    -Isystem/ulib/unittest/include \
+
+MODULE_HOST_LIBS := \
+    system/ulib/mxtl.hostlib \
+    system/ulib/pretty.hostlib \
+    system/ulib/unittest.hostlib \
+
+include make/module.mk
+
+# Clear local variables.
+
+mxtl_common_tests :=
+mxtl_device_tests :=
+mxtl_host_tests :=
