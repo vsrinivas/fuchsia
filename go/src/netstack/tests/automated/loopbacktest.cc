cd7ffe7ee1e1021ace95c58a1842229420608bf8
Toshi Kikuchi <toshik@google.com>
2017-08-26 17:30:13 -0700

    netstack_loopback_test: Add NetDatagramTest
    Add 2 tests:
    - LoopbackDatagramSendto
    - LoopbackDatagramConnectWrite
    
    Renamed the original NetTest to NetStreamTest.
    
    Change-Id: I833eaca24e7e9b7b631dab50a1ba851f21dd0040

--- go/src/netstack/tests/automated/loopbacktest.cc
+++ go/src/netstack/tests/automated/loopbacktest.cc
@@ -13,19 +13,18 @@
 #include <thread>
 
 #include "gtest/gtest.h"
-#include "lib/ftl/macros.h"
 
 namespace netstack {
 namespace {
 
-class NetTest {
+class NetStreamTest {
  public:
-  NetTest() {}
+  NetStreamTest() {}
 };
 
 void StreamRead(struct sockaddr_in* addr, std::string* out) {
   int connfd = socket(AF_INET, SOCK_STREAM, 0);
-  ASSERT_GT(connfd, 0);
+  ASSERT_GE(connfd, 0);
 
   int ret = connect(connfd, (const struct sockaddr*)addr, sizeof(*addr));
   ASSERT_EQ(0, ret) << "connect failed: " << errno;
@@ -39,7 +38,7 @@ void StreamRead(struct sockaddr_in* addr, std::string* out) {
   EXPECT_EQ(close(connfd), 0);
 }
 
-TEST(NetTest, LoopbackStream) {
+TEST(NetStreamTest, LoopbackStream) {
   int server = socket(AF_INET, SOCK_STREAM, 0);
   ASSERT_GT(server, 0);
 
@@ -70,7 +69,7 @@ TEST(NetTest, LoopbackStream) {
 
   thrd.join();
 
-  ASSERT_STREQ(msg, out.c_str());
+  EXPECT_STREQ(msg, out.c_str());
 }
 
 void PollSignal(struct sockaddr_in* addr, short events, short* revents) {
@@ -80,10 +79,7 @@ void PollSignal(struct sockaddr_in* addr, short events, short* revents) {
   int ret = connect(connfd, (const struct sockaddr*)addr, sizeof(*addr));
   ASSERT_EQ(0, ret) << "connect failed: " << errno;
 
-  struct pollfd fds = {
-    .fd = connfd,
-    .events = events,
-  };
+  struct pollfd fds = { connfd, events, 0 };
 
   int n = poll(&fds, 1, 1000); // timeout: 1000ms
   ASSERT_GT(n, 0) << "poll failed: " << errno;
@@ -93,7 +89,7 @@ void PollSignal(struct sockaddr_in* addr, short events, short* revents) {
   *revents = fds.revents;
 }
 
-TEST(NetTest, Shutdown) {
+TEST(NetStreamTest, Shutdown) {
   int server = socket(AF_INET, SOCK_STREAM, 0);
   ASSERT_GT(server, 0);
 
@@ -123,13 +119,149 @@ TEST(NetTest, Shutdown) {
 
   thrd.join();
 
-  ASSERT_EQ(POLLRDHUP, revents);
+  EXPECT_EQ(POLLRDHUP, revents);
 
   ASSERT_EQ(0, close(connfd));
   ASSERT_EQ(0, close(server));
 }
 
-// TODO datagrams
+class NetDatagramTest : public ::testing::Test {
+ protected:
+  NetDatagramTest() {}
+
+  virtual void SetUp() {
+    recvfd_ = socket(AF_INET, SOCK_DGRAM, 0);
+    ASSERT_GE(recvfd_, 0);
+
+    memset(&addr_, 0, sizeof(addr_));
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = 0;
+    addr_.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    int ret = bind(recvfd_, (const struct sockaddr*)&addr_, sizeof(addr_));
+    ASSERT_EQ(0, ret) << "bind failed: " << errno;
+
+    addrlen_ = sizeof(addr_);
+    ret = getsockname(recvfd_, (struct sockaddr*)&addr_, &addrlen_);
+    ASSERT_EQ(0, ret) << "getsockname failed: " << errno;
+
+    ASSERT_EQ(pipe(pipefd_), 0);
+  }
+
+  virtual void TearDown() {
+    EXPECT_EQ(close(recvfd_), 0);
+    close(pipefd_[0]);
+    close(pipefd_[1]);
+  }
+
+  int recvfd_;
+  struct sockaddr_in addr_;
+  socklen_t addrlen_;
+  int pipefd_[2];
+};
+
+const uint8_t kDatagramRead_Success = 1;
+const uint8_t kDatagramRead_Fail = 2;
+
+void NotifySuccess(int pipefd) {
+  uint8_t c = kDatagramRead_Success;
+  EXPECT_EQ(write(pipefd, &c, 1), 1);
+}
+
+void NotifyFail(int pipefd) {
+  uint8_t c = kDatagramRead_Fail;
+  EXPECT_EQ(write(pipefd, &c, 1), 1);
+}
+
+void DatagramRead(int recvfd, std::string* out,
+                  struct sockaddr_in* addr, socklen_t *addrlen,
+                  int pipefd, int timeout) {
+  struct pollfd fds = { recvfd, POLLIN, 0 };
+  int nfds = poll(&fds, 1, timeout);
+  EXPECT_EQ(nfds, 1) << "poll returned: " << nfds << " errno: " << errno;
+  if (nfds != 1) {
+    NotifyFail(pipefd);
+    return;
+  }
+
+  char buf[4096];
+  int nbytes = recvfrom(recvfd, buf, sizeof(buf), 0, (struct sockaddr*)addr,
+                        addrlen);
+  EXPECT_GT(nbytes, 0) << "recvfrom failed: " << errno;
+  if (nbytes < 0) {
+    NotifyFail(pipefd);
+    return;
+  }
+  out->append(buf, nbytes);
+
+  NotifySuccess(pipefd);
+}
+
+TEST_F(NetDatagramTest, LoopbackDatagramSendto) {
+  std::string out;
+  std::thread thrd(DatagramRead, recvfd_,
+                   &out, &addr_, &addrlen_, pipefd_[1], 5000);
+
+  const char* msg = "hello";
+
+  int attempts = 3;
+  int nfd = -1;
+  do {
+    int sendfd = socket(AF_INET, SOCK_DGRAM, 0);
+    ASSERT_GE(sendfd, 0) << "socket failed: " << errno;
+    ASSERT_EQ(sendto(sendfd, msg, strlen(msg), 0, (struct sockaddr*)&addr_,
+                     addrlen_), (ssize_t)strlen(msg)) << "sendto failed: "
+                                                      << errno;
+    ASSERT_EQ(close(sendfd), 0);
+
+    // Check if the reader has received the message.
+    struct pollfd fds = { pipefd_[0], POLLIN, 0 };
+    nfd = poll(&fds, 1, 1000);
+    ASSERT_GE(nfd, 0) << "poll failed: " << errno;
+    if (nfd == 1) {
+      uint8_t c;
+      ASSERT_EQ(read(pipefd_[0], &c, 1), 1);
+      ASSERT_EQ(kDatagramRead_Success, c);
+    }
+  } while (nfd != 1 && --attempts > 0);
+
+  thrd.join();
+
+  EXPECT_STREQ(msg, out.c_str());
+}
+
+TEST_F(NetDatagramTest, LoopbackDatagramConnectWrite) {
+  std::string out;
+  std::thread thrd(DatagramRead, recvfd_,
+                   &out, &addr_, &addrlen_, pipefd_[1], 5000);
+
+  const char* msg = "hello";
+
+  int attempts = 3;
+  int nfd = -1;
+  do {
+    int sendfd = socket(AF_INET, SOCK_DGRAM, 0);
+    ASSERT_GE(sendfd, 0);
+    ASSERT_EQ(connect(sendfd, (struct sockaddr*)&addr_, addrlen_), 0);
+    ASSERT_EQ(write(sendfd, msg, strlen(msg)), (ssize_t)strlen(msg));
+    ASSERT_EQ(close(sendfd), 0);
+
+    // Check if the reader has received the message.
+    struct pollfd fds = { pipefd_[0], POLLIN, 0 };
+    nfd = poll(&fds, 1, 1000);
+    ASSERT_GE(nfd, 0) << "poll failed: " << errno;
+    if (nfd == 1) {
+      uint8_t c;
+      ASSERT_EQ(read(pipefd_[0], &c, 1), 1);
+      ASSERT_EQ(kDatagramRead_Success, c);
+    }
+  } while (nfd != 1 && --attempts > 0);
+
+  thrd.join();
+
+  EXPECT_STREQ(msg, out.c_str());
+}
+
 // TODO port reuse
 
 }  // namespace
