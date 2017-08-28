626caa39f294d769d6e119cb08f4563794c2c48c
Todd Eisenberger <teisenbe@google.com>
2017-08-28 16:05:15 -0700

    [ddktl] Add hidbus protocol definition
    Change-Id: I487a614239a874e5579a0e493bd4593b00d47642

---
+++ system/ulib/ddktl/include/ddktl/protocol/hidbus-internal.h
@@ -0,0 +1,125 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <ddktl/device-internal.h>
+#include <magenta/types.h>
+#include <mxtl/type_support.h>
+#include <mxtl/unique_ptr.h>
+
+#include <stdint.h>
+
+namespace ddk {
+
+class HidBusIfcProxy;
+
+namespace internal {
+
+DECLARE_HAS_MEMBER_FN(has_hidbus_query, HidBusQuery);
+DECLARE_HAS_MEMBER_FN(has_hidbus_start, HidBusStart);
+DECLARE_HAS_MEMBER_FN(has_hidbus_stop, HidBusStop);
+DECLARE_HAS_MEMBER_FN(has_hidbus_get_descriptor, HidBusGetDescriptor);
+DECLARE_HAS_MEMBER_FN(has_hidbus_get_report, HidBusGetReport);
+DECLARE_HAS_MEMBER_FN(has_hidbus_set_report, HidBusSetReport);
+DECLARE_HAS_MEMBER_FN(has_hidbus_get_idle, HidBusGetIdle);
+DECLARE_HAS_MEMBER_FN(has_hidbus_set_idle, HidBusSetIdle);
+DECLARE_HAS_MEMBER_FN(has_hidbus_get_protocol, HidBusGetProtocol);
+DECLARE_HAS_MEMBER_FN(has_hidbus_set_protocol, HidBusSetProtocol);
+
+template <typename D>
+constexpr void CheckHidBusProtocolSubclass() {
+    static_assert(internal::has_hidbus_query<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusQuery");
+    static_assert(mxtl::is_same<decltype(&D::HidBusQuery),
+                                mx_status_t (D::*)(uint32_t options, hid_info_t* info)>::value,
+                  "HidBusQuery must be a non-static member function with signature "
+                  "'mx_status_t HidBusQuery(uint32_t options, hid_info_t* info)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_start<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusStart");
+    static_assert(mxtl::is_same<decltype(&D::HidBusStart),
+                                mx_status_t (D::*)(HidBusIfcProxy proxy)>::value,
+                  "HidBusStart must be a non-static member function with signature "
+                  "'mx_status_t HidBusStart(HidBusIfcProxy proxy)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_stop<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusStop");
+    static_assert(mxtl::is_same<decltype(&D::HidBusStop),
+                                void (D::*)()>::value,
+                  "HidBusStop must be a non-static member function with signature "
+                  "'void HidBusStop()', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_get_descriptor<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusGetDescriptor");
+    static_assert(mxtl::is_same<decltype(&D::HidBusGetDescriptor),
+                                mx_status_t (D::*)(uint8_t desc_type, void** data, size_t* len)>::value,
+                  "HidBusGetDescriptor must be a non-static member function with signature "
+                  "'mx_status_t HidBusGetDescriptor(uint8_t desc_type, void** data, size_t* len)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_get_report<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusGetReport");
+    static_assert(mxtl::is_same<decltype(&D::HidBusGetReport),
+                                mx_status_t (D::*)(uint8_t rpt_type, uint8_t rpt_id, void* data, size_t len)>::value,
+                  "HidBusGetReport must be a non-static member function with signature "
+                  "'mx_status_t HidBusGetReport(uint8_t rpt_type, uint8_t rpt_id, void* data, size_t len)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_set_report<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusSetReport");
+    static_assert(mxtl::is_same<decltype(&D::HidBusSetReport),
+                                mx_status_t (D::*)(uint8_t rpt_type, uint8_t rpt_id, void* data, size_t len)>::value,
+                  "HidBusSetReport must be a non-static member function with signature "
+                  "'mx_status_t HidBusSetReport(uint8_t rpt_type, uint8_t rpt_id, void* data, size_t len)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_get_idle<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusGetIdle");
+    static_assert(mxtl::is_same<decltype(&D::HidBusGetIdle),
+                                mx_status_t (D::*)(uint8_t rpt_id, uint8_t* duration)>::value,
+                  "HidBusGetIdle must be a non-static member function with signature "
+                  "'mx_status_t HidBusGetIdle(uint8_t rpt_id, uint8_t* duration)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_set_idle<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusSetIdle");
+    static_assert(mxtl::is_same<decltype(&D::HidBusSetIdle),
+                                mx_status_t (D::*)(uint8_t rpt_id, uint8_t duration)>::value,
+                  "HidBusSetIdle must be a non-static member function with signature "
+                  "'mx_status_t HidBusSetIdle(uint8_t rpt_id, uint8_t duration)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_get_protocol<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusGetProtocol");
+    static_assert(mxtl::is_same<decltype(&D::HidBusGetProtocol),
+                                mx_status_t (D::*)(uint8_t* protocol)>::value,
+                  "HidBusGetProtocol must be a non-static member function with signature "
+                  "'mx_status_t HidBusGetProtocol(uint8_t* protocol)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+
+    static_assert(internal::has_hidbus_set_protocol<D>::value,
+                  "HidBusProtocol subclasses must implement HidBusSetProtocol");
+    static_assert(mxtl::is_same<decltype(&D::HidBusSetProtocol),
+                                mx_status_t (D::*)(uint8_t protocol)>::value,
+                  "HidBusSetProtocol must be a non-static member function with signature "
+                  "'mx_status_t HidBusSetProtocol(uint8_t protocol)', and be visible to "
+                  "ddk::HidBusProtocol<D> (either because they are public, or because of "
+                  "friendship).");
+}
+
+}  // namespace internal
+}  // namespace ddk
---
+++ system/ulib/ddktl/include/ddktl/protocol/hidbus.h
@@ -0,0 +1,159 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <ddk/protocol/hidbus.h>
+#include <ddktl/protocol/hidbus-internal.h>
+#include <magenta/assert.h>
+#include <mxtl/type_support.h>
+#include <mxtl/unique_ptr.h>
+
+// DDK hidbus protocol support
+//
+// :: Proxies ::
+//
+// ddk::HidBusIfcProxy is simple wrappers around hidbus_ifc_t. It does not own the pointers passed
+// to it.
+//
+// :: Mixins ::
+//
+// ddk::HidBusProtocol is a mixin class that simplifies writing DDK drivers that
+// implement the hidbus protocol. They take care of implementing the function pointer tables
+// and calling into the object that wraps them.
+//
+// :: Examples ::
+//
+// // A driver that implements a MX_PROTOCOL_HIDBUS device
+// class HidBusDevice;
+// using HidBusDeviceType = ddk::Device<HidBusDevice, /* ddk mixins */>;
+//
+// class HidBusDevice : public HidBusDeviceType,
+//                      public ddk::HidBusProtocol<HidBusDevice> {
+//   public:
+//     HidBusDevice(mx_device_t* parent)
+//       : HidBusDeviceType("my-hidbus-device", parent) {}
+//
+//     mx_status_t Bind() {
+//         DdkAdd();
+//     }
+//
+//     void DdkRelease() {
+//         // Clean up
+//     }
+//
+//     mx_status_t HidbusStart(ddk::HidBusIfcProxy proxy) {
+//         // Start hidbus operation
+//         proxy_ = proxy;
+//         return MX_OK;
+//     }
+//
+//     void HidBusQuery(uint32_t options, hid_info_t* info) {
+//         ...
+//     }
+//
+//     ...
+//   private:
+//     ddk::HidbusIfcProxy proxy_;
+//     ...
+// };
+
+namespace ddk {
+
+template <typename D>
+class HidBusProtocol : public internal::base_protocol {
+  public:
+    HidBusProtocol() {
+        internal::CheckHidBusProtocolSubclass<D>();
+        ops_.query = Query;
+        ops_.start = Start;
+        ops_.stop = Stop;
+        ops_.get_descriptor = GetDescriptor;
+        ops_.get_report = GetReport;
+        ops_.set_report = SetReport;
+        ops_.get_idle = GetIdle;
+        ops_.set_idle = SetIdle;
+        ops_.get_protocol = GetProtocol;
+        ops_.set_protocol = SetProtocol;
+
+        // Can only inherit from one base_protocol implemenation
+        MX_ASSERT(ddk_proto_ops_ == nullptr);
+        ddk_proto_id_ = MX_PROTOCOL_HIDBUS;
+        ddk_proto_ops_ = &ops_;
+    }
+
+  private:
+    static mx_status_t Query(void* ctx, uint32_t options, hid_info_t* info) {
+        return static_cast<D*>(ctx)->HidBusQuery(options, info);
+    }
+
+    static mx_status_t Start(void* ctx, hidbus_ifc_t* ifc, void* cookie) {
+        HidBusIfcProxy proxy(ifc, cookie);
+        return static_cast<D*>(ctx)->HidBusStart(proxy);
+    }
+
+    static void Stop(void* ctx) {
+        static_cast<D*>(ctx)->HidBusStop();
+    }
+
+    static mx_status_t GetDescriptor(void* ctx, uint8_t desc_type, void** data, size_t* len) {
+        return static_cast<D*>(ctx)->HidBusGetDescriptor(desc_type, data, len);
+    }
+
+    static mx_status_t GetReport(void* ctx, uint8_t rpt_type, uint8_t rpt_id, void* data,
+                                 size_t len) {
+        return static_cast<D*>(ctx)->HidBusGetReport(rpt_type, rpt_id, data, len);
+    }
+
+    static mx_status_t SetReport(void* ctx, uint8_t rpt_type, uint8_t rpt_id, void* data,
+                                 size_t len) {
+        return static_cast<D*>(ctx)->HidBusSetReport(rpt_type, rpt_id, data, len);
+    }
+
+    static mx_status_t GetIdle(void* ctx, uint8_t rpt_id, uint8_t* duration) {
+        return static_cast<D*>(ctx)->HidBusGetIdle(rpt_id, duration);
+    }
+
+    static mx_status_t SetIdle(void* ctx, uint8_t rpt_id, uint8_t duration) {
+        return static_cast<D*>(ctx)->HidBusSetIdle(rpt_id, duration);
+    }
+
+    static mx_status_t GetProtocol(void* ctx, uint8_t* protocol) {
+        return static_cast<D*>(ctx)->HidBusGetProtocol(protocol);
+    }
+
+    static mx_status_t SetProtocol(void* ctx, uint8_t protocol) {
+        return static_cast<D*>(ctx)->HidBusSetProtocol(protocol);
+    }
+
+    hidbus_protocol_ops_t ops_ = {};
+};
+
+class HidBusIfcProxy {
+  public:
+    HidBusIfcProxy()
+      : ifc_(nullptr), cookie_(nullptr) {}
+
+    HidBusIfcProxy(hidbus_ifc_t* ifc, void* cookie)
+      : ifc_(ifc), cookie_(cookie) {}
+
+    void IoQueue(const uint8_t* buf, size_t len) {
+        ifc_->io_queue(cookie_, buf, len);
+    }
+
+    bool is_valid() const {
+        return ifc_ != nullptr;
+    }
+
+    void clear() {
+        ifc_ = nullptr;
+        cookie_ = nullptr;
+    }
+
+  private:
+    hidbus_ifc_t* ifc_;
+    void* cookie_;
+};
+
+}  // namespace ddk
