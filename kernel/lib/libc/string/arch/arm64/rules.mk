204b5192a1a5b1e6fe8d697682c06512953fb3fb
Roland McGrath <mcgrathr@google.com>
2017-09-05 16:04:26 -0700

    [kernel][arm64] Optimized arm64 memcpy, memset
    These are taken verbatim from Linaro's cortex-strings.  memset is
    from an older version of cortex-strings that does not use NEON.  The
    current cortex-strings memcpy (also now used in libc) never used NEON.
    
    MG-526 #done
    
    Change-Id: Ic01878a1c702c3677103e5db0c67ce114e201183

--- kernel/lib/libc/string/arch/arm64/rules.mk
+++ kernel/lib/libc/string/arch/arm64/rules.mk
@@ -7,3 +7,11 @@
 
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
+ASM_STRING_OPS := memcpy memset
+
+MODULE_SRCS += \
+    third_party/lib/cortex-strings/src/aarch64/memcpy.S \
+    third_party/lib/cortex-strings/no-neon/src/aarch64/memset.S \
+
+# filter out the C implementation
+C_STRING_OPS := $(filter-out $(ASM_STRING_OPS),$(C_STRING_OPS))
