9001f1ddf5b4227d73749d97dc72fc166a20e16d
Ian McKellar <ianloic@google.com>
2017-09-06 13:42:50 -0700

    Follow the dart:fidl.internal split
    Change-Id: If61e343a23be78d090c2c141eba81a07a7650223

--- bin/application/lib/app/dart/app.dart
+++ bin/application/lib/app/dart/app.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:fidl.internal';
+import 'dart:fuchsia';
 
 import 'package:lib.fidl.dart/bindings.dart';
 import 'package:lib.fidl.dart/core.dart' as core;
