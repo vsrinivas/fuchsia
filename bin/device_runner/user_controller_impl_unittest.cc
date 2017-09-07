d8d62f34706c60a78cc7ca195dab803fdcefacf8
Alhaad Gokhale <alhaad@google.com>
2017-09-07 14:05:39 -0700

    Use common FakeApplicationLauncher in tests.
    Change-Id: I0212df4983d73f1f9450d4019bb0c4be3ed2d3bf

--- bin/device_runner/user_controller_impl_unittest.cc
+++ bin/device_runner/user_controller_impl_unittest.cc
@@ -6,60 +6,42 @@
 
 #include "gtest/gtest.h"
 #include "lib/app/fidl/application_launcher.fidl.h"
+#include "peridot/lib/testing/fake_application_launcher.h"
 #include "peridot/lib/testing/test_with_message_loop.h"
 
 namespace modular {
+namespace testing {
 namespace {
-
-class TestApplicationLauncher : app::ApplicationLauncher {
- public:
-  explicit TestApplicationLauncher(
-      fidl::InterfaceRequest<app::ApplicationLauncher> request)
-      : binding_(this, std::move(request)) {}
-
-  app::ApplicationLaunchInfoPtr last_launch_info;
-
- private:
-  void CreateApplication(
-      app::ApplicationLaunchInfoPtr launch_info,
-      fidl::InterfaceRequest<app::ApplicationController> /*request*/) override {
-    last_launch_info = std::move(launch_info);
-  }
-
-  fidl::Binding<app::ApplicationLauncher> binding_;
-
-  FXL_DISALLOW_COPY_AND_ASSIGN(TestApplicationLauncher);
-};
 
 class UserControllerImplTest : public testing::TestWithMessageLoop {};
 
 TEST_F(UserControllerImplTest, StartUserRunner) {
-  app::ApplicationLauncherPtr application_launcher_ptr;
-  TestApplicationLauncher test_application_launcher(
-      application_launcher_ptr.NewRequest());
-  ASSERT_TRUE(test_application_launcher.last_launch_info.is_null());
-
-  std::string url = "test url string";
+  FakeApplicationLauncher launcher;
+  std::string url = "test_url_string";
   auto app_config = AppConfig::New();
   app_config->url = url;
 
   auth::TokenProviderFactoryPtr token_provider_factory_ptr;
   auto token_provider_factory_request = token_provider_factory_ptr.NewRequest();
 
+  bool callback_called = false;
+  launcher.RegisterApplication(
+      url, [&callback_called](
+               app::ApplicationLaunchInfoPtr launch_info,
+               fidl::InterfaceRequest<app::ApplicationController> ctrl) {
+        callback_called = true;
+      });
+
   UserControllerPtr user_controller_ptr;
   UserControllerImpl impl(
-      application_launcher_ptr.get(), app_config.Clone(), app_config.Clone(),
-      app_config.Clone(), std::move(token_provider_factory_ptr),
-      nullptr /* account */, nullptr /* view_owner_request */,
-      user_controller_ptr.NewRequest(), nullptr /* done_callback */);
+      &launcher, app_config.Clone(), app_config.Clone(), app_config.Clone(),
+      std::move(token_provider_factory_ptr), nullptr /* account */,
+      nullptr /* view_owner_request */, user_controller_ptr.NewRequest(),
+      nullptr /* done_callback */);
 
-  EXPECT_TRUE(RunLoopUntil([&test_application_launcher] {
-    return !test_application_launcher.last_launch_info.is_null();
-  }));
-
-  EXPECT_FALSE(test_application_launcher.last_launch_info.is_null());
-  EXPECT_EQ(url, test_application_launcher.last_launch_info->url);
+  EXPECT_TRUE(callback_called);
 }
 
 }  // namespace
+}  // namespace testing
 }  // namespace modular
--- lib/fidl/app_client_unittest.cc
+++ lib/fidl/app_client_unittest.cc
@@ -3,13 +3,16 @@
 // found in the LICENSE file.
 
 #include "peridot/lib/fidl/app_client.h"
+
 #include "gtest/gtest.h"
 #include "lib/app/fidl/application_controller.fidl.h"
 #include "lib/app/fidl/application_launcher.fidl.h"
 #include "peridot/lib/fidl/app_client_unittest.fidl.h"
+#include "peridot/lib/testing/fake_application_launcher.h"
 #include "peridot/lib/testing/test_with_message_loop.h"
 
 namespace modular {
+namespace testing {
 namespace {
 
 constexpr char kTestUrl[] = "some/test/url";
@@ -45,53 +48,37 @@ class TestApplicationController : app::ApplicationController {
   FXL_DISALLOW_COPY_AND_ASSIGN(TestApplicationController);
 };
 
-// TODO(alhaad): Factor out this use into a common fake-application-launcher.
-class TestApplicationLauncher : app::ApplicationLauncher {
- public:
-  explicit TestApplicationLauncher(
-      fidl::InterfaceRequest<app::ApplicationLauncher> request)
-      : binding_(this, std::move(request)) {}
-
-  app::ApplicationLaunchInfoPtr last_launch_info;
-  TestApplicationController controller;
-
- private:
-  void CreateApplication(
-      app::ApplicationLaunchInfoPtr launch_info,
-      fidl::InterfaceRequest<app::ApplicationController> request) override {
-    last_launch_info = std::move(launch_info);
-    controller.Connect(std::move(request));
-  }
-
-  fidl::Binding<app::ApplicationLauncher> binding_;
-
-  FXL_DISALLOW_COPY_AND_ASSIGN(TestApplicationLauncher);
-};
-
 class AppClientTest : public testing::TestWithMessageLoop {};
 
 TEST_F(AppClientTest, BaseRun_Success) {
-  app::ApplicationLauncherPtr application_launcher_ptr;
-  TestApplicationLauncher test_application_launcher(
-      application_launcher_ptr.NewRequest());
+  bool callback_called = false;
+  FakeApplicationLauncher launcher;
+  launcher.RegisterApplication(
+      kTestUrl, [&callback_called](
+                    app::ApplicationLaunchInfoPtr launch_info,
+                    fidl::InterfaceRequest<app::ApplicationController> ctrl) {
+        EXPECT_EQ(kTestUrl, launch_info->url);
+        callback_called = true;
+      });
+  AppClientBase app_client_base(&launcher, GetTestAppConfig());
 
-  AppClientBase app_client_base(application_launcher_ptr.get(),
-                                GetTestAppConfig());
-
-  EXPECT_TRUE(RunLoopUntil([&test_application_launcher] {
-    return !test_application_launcher.last_launch_info.is_null();
-  }));
-
-  EXPECT_EQ(kTestUrl, test_application_launcher.last_launch_info->url);
+  EXPECT_TRUE(callback_called);
 }
 
 TEST_F(AppClientTest, BaseTerminate_Success) {
-  app::ApplicationLauncherPtr application_launcher_ptr;
-  TestApplicationLauncher test_application_launcher(
-      application_launcher_ptr.NewRequest());
+  FakeApplicationLauncher launcher;
+  TestApplicationController controller;
+  bool callback_called = false;
+  launcher.RegisterApplication(
+      kTestUrl, [&callback_called, &controller](
+                    app::ApplicationLaunchInfoPtr launch_info,
+                    fidl::InterfaceRequest<app::ApplicationController> ctrl) {
+        EXPECT_EQ(kTestUrl, launch_info->url);
+        callback_called = true;
+        controller.Connect(std::move(ctrl));
+      });
 
-  AppClientBase app_client_base(application_launcher_ptr.get(),
-                                GetTestAppConfig());
+  AppClientBase app_client_base(&launcher, GetTestAppConfig());
 
   bool app_terminated_callback_called = false;
   app_client_base.Teardown(fxl::TimeDelta::Zero(),
@@ -99,31 +86,30 @@ TEST_F(AppClientTest, BaseTerminate_Success) {
                              app_terminated_callback_called = true;
                            });
 
-  EXPECT_TRUE(RunLoopUntil(
-      [&app_terminated_callback_called, &test_application_launcher] {
-        return app_terminated_callback_called &&
-               test_application_launcher.controller.killed();
-      }));
+  EXPECT_TRUE(callback_called);
+  EXPECT_TRUE(RunLoopUntil([&app_terminated_callback_called, &controller] {
+    return app_terminated_callback_called && controller.killed();
+  }));
 
-  EXPECT_FALSE(test_application_launcher.last_launch_info.is_null());
-  EXPECT_EQ(kTestUrl, test_application_launcher.last_launch_info->url);
-  EXPECT_TRUE(test_application_launcher.controller.killed());
+  EXPECT_TRUE(controller.killed());
 }
 
 TEST_F(AppClientTest, Run_Success) {
-  app::ApplicationLauncherPtr application_launcher_ptr;
-  TestApplicationLauncher test_application_launcher(
-      application_launcher_ptr.NewRequest());
+  bool callback_called = false;
+  FakeApplicationLauncher launcher;
+  launcher.RegisterApplication(
+      kTestUrl, [&callback_called](
+                    app::ApplicationLaunchInfoPtr launch_info,
+                    fidl::InterfaceRequest<app::ApplicationController> ctrl) {
+        EXPECT_EQ(kTestUrl, launch_info->url);
+        callback_called = true;
+      });
 
-  AppClient<test::TerminateService> app_client(application_launcher_ptr.get(),
-                                               GetTestAppConfig());
+  AppClient<test::TerminateService> app_client(&launcher, GetTestAppConfig());
 
-  EXPECT_TRUE(RunLoopUntil([&test_application_launcher] {
-    return !test_application_launcher.last_launch_info.is_null();
-  }));
-
-  EXPECT_EQ(kTestUrl, test_application_launcher.last_launch_info->url);
+  EXPECT_TRUE(callback_called);
 }
 
 }  // namespace
+}  // namespace testing
 }  // namespace modular
