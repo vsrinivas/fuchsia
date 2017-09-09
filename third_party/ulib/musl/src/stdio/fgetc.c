c67a9eb4d0c1193e1d178f6904a9688339ab38bc
Petr Hosek <phosek@google.com>
2017-09-08 20:31:04 -0700

    [libc] Use atomic_load to read FILE->lock
    FILE->lock is atomic_int and so should be read using atomic_load.
    Previously this wasn't reported, but with a new Clang this is
    triggering an error because of -Wtautological-unsigned-zero-compare.
    
    Change-Id: I3fe11f4be356a96d688e90614a9498d3c9eba05e

--- third_party/ulib/musl/src/stdio/fgetc.c
+++ third_party/ulib/musl/src/stdio/fgetc.c
@@ -2,7 +2,7 @@
 
 int fgetc(FILE* f) {
     int c;
-    if (f->lock < 0 || !__lockfile(f))
+    if (atomic_load(&f->lock) < 0 || !__lockfile(f))
         return getc_unlocked(f);
     c = getc_unlocked(f);
     __unlockfile(f);
--- third_party/ulib/musl/src/stdio/putc.c
+++ third_party/ulib/musl/src/stdio/putc.c
@@ -1,7 +1,7 @@
 #include "stdio_impl.h"
 
 int putc(int c, FILE* f) {
-    if (f->lock < 0 || !__lockfile(f))
+    if (atomic_load(&f->lock) < 0 || !__lockfile(f))
         return putc_unlocked(c, f);
     c = putc_unlocked(c, f);
     __unlockfile(f);
