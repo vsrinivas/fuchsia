cb70c1a00c12552ac4214b51d50b9db3a59d68c8
Benjamin Lerman <qsr@chromium.org>
2017-09-12 12:44:15 +0200

    Add ScopedTaskRunner.
    This allows to schedule work that will automatically cancelled when the
    scoped task runner is deleted.
    
    Change-Id: I974f6b672b953e147aae40b5d50563ea00d075c1

---
+++ bin/ledger/callback/scoped_task_runner.cc
@@ -0,0 +1,33 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "apps/ledger/src/callback/scoped_task_runner.h"
+
+namespace callback {
+
+ScopedTaskRunner::ScopedTaskRunner(fxl::RefPtr<fxl::TaskRunner> task_runner)
+    : task_runner_(task_runner), weak_factory_(this) {}
+
+ScopedTaskRunner::~ScopedTaskRunner() {}
+
+void ScopedTaskRunner::PostTask(fxl::Closure task) {
+  task_runner_->PostTask(MakeScoped(std::move(task)));
+}
+
+void ScopedTaskRunner::PostTaskForTime(fxl::Closure task,
+                                       fxl::TimePoint target_time) {
+  task_runner_->PostTaskForTime(MakeScoped(std::move(task)),
+                                std::move(target_time));
+}
+
+void ScopedTaskRunner::PostDelayedTask(fxl::Closure task,
+                                       fxl::TimeDelta delay) {
+  task_runner_->PostDelayedTask(MakeScoped(std::move(task)), std::move(delay));
+}
+
+bool ScopedTaskRunner::RunsTasksOnCurrentThread() {
+  return task_runner_->RunsTasksOnCurrentThread();
+}
+
+}  // namespace callback
