cb219e69b380aca0a7271e2c2842855d7774cac6
John Bauman <jbauman@google.com>
2017-09-13 13:44:13 -0700

    Add initial msd-arm-mali implementation.
    This can load and bind to the device (if it's in the mdi) but do nothing
    else.
    
    Change-Id: Ice5cd67e911944125b9b12e7ed11ba094a232fea

---
+++ drivers/gpu/msd-arm-mali/src/entry_stubs.cc
@@ -0,0 +1,49 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "magma/include/msd_abi/msd.h"
+
+msd_connection_t* msd_device_open(msd_device_t* dev, msd_client_id client_id) { return nullptr; }
+
+uint32_t msd_device_get_id(msd_device_t* dev) { return 0; }
+
+void msd_connection_present_buffer(msd_connection_t* connection, msd_buffer_t* buf,
+                                   struct magma_system_image_descriptor* image_desc,
+                                   uint32_t wait_semaphore_count, uint32_t signal_semaphore_count,
+                                   msd_semaphore_t** semaphores,
+                                   msd_present_buffer_callback_t callback, void* callback_data)
+{
+}
+
+void msd_connection_close(msd_connection_t* connection) {}
+
+msd_context_t* msd_connection_create_context(msd_connection_t* connection) { return nullptr; }
+
+magma_status_t msd_connection_wait_rendering(msd_connection_t* connection, msd_buffer_t* buf)
+{
+    return MAGMA_STATUS_OK;
+}
+
+void msd_context_destroy(msd_context_t* ctx) {}
+
+magma_status_t msd_context_execute_command_buffer(msd_context_t* ctx, msd_buffer_t* cmd_buf,
+                                                  msd_buffer_t** exec_resources,
+                                                  msd_semaphore_t** wait_semaphores,
+                                                  msd_semaphore_t** signal_semaphores)
+{
+    return MAGMA_STATUS_OK;
+}
+
+void msd_context_release_buffer(msd_context_t* context, msd_buffer_t* buffer) {}
+
+msd_buffer_t* msd_buffer_import(uint32_t handle) { return nullptr; }
+
+void msd_buffer_destroy(msd_buffer_t* buf) {}
+
+magma_status_t msd_semaphore_import(uint32_t handle, msd_semaphore_t** semaphore_out)
+{
+    return MAGMA_STATUS_OK;
+}
+
+void msd_semaphore_release(msd_semaphore_t* semaphore) {}
---
+++ drivers/gpu/msd-arm-mali/src/main.cc
@@ -0,0 +1,79 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/device.h>
+#include <ddk/driver.h>
+#include <ddk/protocol/platform-device.h>
+#include <ddk/protocol/platform-devices.h>
+
+#include <zircon/process.h>
+#include <zircon/types.h>
+
+#include <memory>
+
+#include "magma_util/macros.h"
+
+struct arm_mali_device {
+    zx_device_t* parent_device;
+    zx_device_t* zx_device;
+};
+
+static zx_status_t arm_mali_open(void* context, zx_device_t** out, uint32_t flags)
+{
+    return ZX_ERR_NOT_SUPPORTED;
+}
+
+static zx_status_t arm_mali_close(void* context, uint32_t flags) { return ZX_ERR_NOT_SUPPORTED; }
+
+static zx_status_t arm_mali_ioctl(void* context, uint32_t op, const void* in_buf, size_t in_len,
+                                  void* out_buf, size_t out_len, size_t* out_actual)
+{
+    return ZX_ERR_NOT_SUPPORTED;
+}
+
+static void arm_mali_release(void* context) {}
+
+static zx_protocol_device_t arm_mali_device_proto = {
+    .version = DEVICE_OPS_VERSION,
+    .open = arm_mali_open,
+    .close = arm_mali_close,
+    .ioctl = arm_mali_ioctl,
+    .release = arm_mali_release,
+};
+
+static zx_status_t arm_mali_bind(void* context, zx_device_t* parent, void** cookie)
+{
+    dprintf(INFO, "arm_mali_bind: binding\n");
+    auto gpu = std::make_unique<arm_mali_device>();
+    if (!gpu)
+        return ZX_ERR_NO_MEMORY;
+    gpu->parent_device = parent;
+
+    device_add_args_t args = {};
+    args.version = DEVICE_ADD_ARGS_VERSION;
+    args.name = "arm_mali_gpu";
+    args.ctx = gpu.get();
+    args.ops = &arm_mali_device_proto;
+
+    zx_status_t status = device_add(parent, &args, &gpu->zx_device);
+    if (status != ZX_OK)
+        return DRET_MSG(status, "device_add failed");
+
+    return ZX_OK;
+}
+
+zx_driver_ops_t arm_gpu_driver_ops = {
+    .version = DRIVER_OPS_VERSION, .bind = arm_mali_bind,
+};
+
+// clang-format off
+ZIRCON_DRIVER_BEGIN(arm_gpu, arm_gpu_driver_ops, "zircon", "0.1", 4)
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PLATFORM_DEV),
+    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_GENERIC),
+    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_PID, PDEV_PID_GENERIC),
+    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, PDEV_DID_ARM_MALI),
+ZIRCON_DRIVER_END(arm_gpu)
+    // clang-format on
