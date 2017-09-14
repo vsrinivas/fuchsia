00a6b0de5fa73930f7b1790ee773a39d3327e498
George Kulakowski <kulakowski@google.com>
2017-09-14 01:13:09 -0700

    [musl][bits] Collapse identical limits.h
    These differed only in declaration ordering.
    
    Change-Id: Ie0b36da13db2d59f8443333f3e9f24fc658c0e99

--- third_party/ulib/musl/include/bits/limits.h
+++ third_party/ulib/musl/include/bits/limits.h
@@ -1,7 +1,8 @@
-#if defined(__x86_64__)
-#include "x86_64/limits.h"
-#elif defined(__aarch64__)
-#include "aarch64/limits.h"
-#else
-#error Unsupported architecture!
+#if defined(_POSIX_SOURCE) || defined(_POSIX_C_SOURCE) || defined(_XOPEN_SOURCE) || \
+    defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+#define PAGE_SIZE 4096
+#define LONG_BIT 64
 #endif
+
+#define LONG_MAX 0x7fffffffffffffffL
+#define LLONG_MAX 0x7fffffffffffffffLL
