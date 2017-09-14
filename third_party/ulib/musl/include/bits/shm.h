c25131a6a34d9d56952a024c13a1a63640049eb6
George Kulakowski <kulakowski@google.com>
2017-09-14 01:15:08 -0700

    [musl][bits] Collapse identical shm.h bits headers
    Change-Id: Ie04b6d638091d6e5bba1f88b8a351709cb0fdf3f

--- third_party/ulib/musl/include/bits/shm.h
+++ third_party/ulib/musl/include/bits/shm.h
@@ -1,7 +1,24 @@
-#if defined(__x86_64__)
-#include "x86_64/shm.h"
-#elif defined(__aarch64__)
-#include "aarch64/shm.h"
-#else
-#error Unsupported architecture!
-#endif
+#define SHMLBA 4096
+
+struct shmid_ds {
+    struct ipc_perm shm_perm;
+    size_t shm_segsz;
+    time_t shm_atime;
+    time_t shm_dtime;
+    time_t shm_ctime;
+    pid_t shm_cpid;
+    pid_t shm_lpid;
+    unsigned long shm_nattch;
+    unsigned long __pad1;
+    unsigned long __pad2;
+};
+
+struct shminfo {
+    unsigned long shmmax, shmmin, shmmni, shmseg, shmall, __unused[4];
+};
+
+struct shm_info {
+    int __used_ids;
+    unsigned long shm_tot, shm_rss, shm_swp;
+    unsigned long __swap_attempts, __swap_successes;
+};
