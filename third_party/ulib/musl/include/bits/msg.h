1b9bcfc7ae03df463b12e61001e1a5d87f4f4b34
George Kulakowski <kulakowski@google.com>
2017-09-14 01:11:03 -0700

    [musl][bits] Collapse nearly identical msg.h headers
    These only differ in the spelling of padding fields.
    
    Change-Id: I22751f7c8ee7af40cf810c1807a73f043af5aa29

--- third_party/ulib/musl/include/bits/msg.h
+++ third_party/ulib/musl/include/bits/msg.h
@@ -1,7 +1,12 @@
-#if defined(__x86_64__)
-#include "x86_64/msg.h"
-#elif defined(__aarch64__)
-#include "aarch64/msg.h"
-#else
-#error Unsupported architecture!
-#endif
+struct msqid_ds {
+    struct ipc_perm msg_perm;
+    time_t msg_stime;
+    time_t msg_rtime;
+    time_t msg_ctime;
+    unsigned long msg_cbytes;
+    msgqnum_t msg_qnum;
+    msglen_t msg_qbytes;
+    pid_t msg_lspid;
+    pid_t msg_lrpid;
+    unsigned long __unused[2];
+};
