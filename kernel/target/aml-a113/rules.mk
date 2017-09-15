b2e7db3c5610508aec613339bd8cc4408be28211
Eric Holland <hollande@google.com>
2017-09-15 14:51:32 -0700

    [arm64] AMLogic A113 support
    Change-Id: I046d374ac0852347d11dce0fda6841281001ee3c

---
+++ kernel/target/aml-a113/rules.mk
@@ -0,0 +1,40 @@
+# Copyright 2017 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+PLATFORM := generic-arm
+
+DEVICE_TREE := $(GET_LOCAL_DIR)/device-tree.dtb
+
+MEMBASE := 0x00000000
+MEMSIZE ?= 0x80000000   # 2GB
+KERNEL_LOAD_OFFSET := 0x1080000
+
+PERIPH_BASE_PHYS := 0xf0000000U
+PERIPH_SIZE := 0x10200000UL
+PERIPH_BASE_VIRT := 0xffffffffc0000000ULL
+MEMORY_APERTURE_SIZE  := 0xc0000000UL
+
+BOOTLOADER_RESERVE_START := 0
+BOOTLOADER_RESERVE_SIZE := 0x01000000
+
+KERNEL_DEFINES += \
+    PERIPH_BASE_PHYS=$(PERIPH_BASE_PHYS) \
+    PERIPH_BASE_VIRT=$(PERIPH_BASE_VIRT) \
+    PERIPH_SIZE=$(PERIPH_SIZE) \
+    MEMORY_APERTURE_SIZE=$(MEMORY_APERTURE_SIZE) \
+    BOOTLOADER_RESERVE_START=$(BOOTLOADER_RESERVE_START) \
+    BOOTLOADER_RESERVE_SIZE=$(BOOTLOADER_RESERVE_SIZE) \
+    PLATFORM_SUPPORTS_PANIC_SHELL=1 \
+
+include make/kernel_images.mk
+
+# build MDI
+MDI_SRCS := \
+    $(LOCAL_DIR)/aml-a113.mdi \
+
+include make/mdi.mk
