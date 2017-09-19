597686236d2d3586137654576bb8f9a447d0453c
Arman Uguray <armansito@google.com>
2017-09-18 21:42:08 -0700

    [fxl] Add gtest_main target
    This replaces the test_main target from fsl.
    
    Change-Id: I8e950bf359ad5947af38fd44dfd4f054bd31a036

--- public/lib/fxl/test/run_all_unittests.cc
+++ public/lib/fxl/test/run_all_unittests.cc
@@ -4,7 +4,17 @@
 
 #include "gtest/gtest.h"
 
+#include "lib/fxl/command_line.h"
+#include "lib/fxl/log_settings_command_line.h"
+#include "lib/fxl/logging.h"
+
 int main(int argc, char** argv) {
+  auto cl = fxl::CommandLineFromArgcArgv(argc, argv);
+  if (!fxl::SetLogSettingsFromCommandLine(cl)) {
+    FXL_LOG(ERROR) << "Failed to parse log settings from command-line";
+    return EXIT_FAILURE;
+  }
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
