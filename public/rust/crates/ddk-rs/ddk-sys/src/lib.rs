9a8714ea1265de6e5a912f489b0f9cd251bcaba8
Anthony Stange <stange@google.com>
2017-09-25 10:55:46 +0100

    Add basic device operations and implement method to add a new device.
    Change-Id: I6b02af361376bbf75fc7866b2e5111c489b24e8a

--- public/rust/crates/ddk-rs/ddk-sys/src/lib.rs
+++ public/rust/crates/ddk-rs/ddk-sys/src/lib.rs
@@ -4,18 +4,43 @@
 
 #![allow(non_camel_case_types)]
 
+#[macro_use]
+extern crate bitflags;
 extern crate fuchsia_zircon_sys as zircon_sys;
 
 use zircon_sys as sys;
 use sys::{zx_handle_t, zx_off_t, zx_paddr_t, zx_status_t};
 use std::os::raw::c_char;
 
-pub const DRIVER_OPS_VERSION: u64 = 0x2b3490fa40d9f452;
+// References to Zircon DDK's driver.h
+
+// Opaque structs
+#[repr(u8)]
+pub enum zx_device_t {
+    variant1,
+}
+#[repr(u8)]
+pub enum zx_device_prop_t {
+    variant1,
+}
+
+pub const ZX_DEVICE_NAME_MAX: usize = 31;
+const DRIVER_OPS_VERSION: u64 = 0x2b3490fa40d9f452;
+const DEVICE_ADD_ARGS_VERSION: u64 = 0x96a64134d56e88e3;
 
 #[repr(C)]
 pub struct list_node_t {
     pub prev: *mut list_node_t,
     pub next: *mut list_node_t,
+}
+
+impl Default for list_node_t {
+    fn default() -> list_node_t {
+        list_node_t {
+            prev: std::ptr::null_mut(),
+            next: std::ptr::null_mut(),
+        }
+    }
 }
 
 #[repr(C)]
@@ -39,11 +64,18 @@ pub struct zx_driver_t {
     pub status: zx_status_t,
 }
 
-// Opaque struct
-#[repr(u8)]
-pub enum zx_device_t {
-    variant1,
-}
+// References to Zircon DDK's iotxn.h
+
+const IOTXN_OP_READ: u8 = 1;
+const IOTXN_OP_WRITE: u8 = 2;
+
+const IOTXN_CACHE_INVALIDATE: u32 = sys::ZX_VMO_OP_CACHE_INVALIDATE;
+const IOTXN_CACHE_CLEAN: u32 = sys::ZX_VMO_OP_CACHE_CLEAN;
+const IOTXN_CACHE_CLEAN_INVALIDATE: u32 = sys::ZX_VMO_OP_CACHE_CLEAN_INVALIDATE;
+const IOTXN_CACHE_SYNC: u32 = sys::ZX_VMO_OP_CACHE_SYNC;
+
+const IOTXN_SYNC_BEFORE: u8 = 1;
+const IOTXN_SYNC_AFTER: u8 = 2;
 
 pub type iotxn_proto_data_t = [u64; 6];
 pub type iotxn_extra_data_t = [u64; 6];
@@ -82,7 +114,7 @@ pub struct zx_protocol_device_t {
 
     pub get_protocol: Option<extern "C" fn (ctx: *mut u8, proto_id: u32, protocol: *mut u8) -> zx_status_t>,
     pub open: Option<extern "C" fn (ctx: *mut u8, dev_out: *mut *mut zx_device_t, flags: u32) -> zx_status_t>,
-    pub open_at: Option<extern "C" fn (ctx: *mut u8, dev_out: *mut *mut zx_device_t, path: *const char, flags: u32) -> zx_status_t>,
+    pub open_at: Option<extern "C" fn (ctx: *mut u8, dev_out: *mut *mut zx_device_t, path: *const c_char, flags: u32) -> zx_status_t>,
     pub close: Option<extern "C" fn (ctx: *mut u8, flags: u32) -> zx_status_t>,
     pub unbind: Option<extern "C" fn (ctx: *mut u8)>,
     pub release: Option<extern "C" fn (ctx: *mut u8)>,
@@ -93,4 +125,74 @@ pub struct zx_protocol_device_t {
     pub ioctl: Option<extern "C" fn (ctx: *mut u8, op: u32, in_buf: *const u8, in_len: usize, out_buf: *mut u8, out_len: usize, out_actual: *mut usize) -> zx_status_t>,
     pub suspend: Option<extern "C" fn (ctx: *mut u8, flags: u32) -> zx_status_t>,
     pub resume: Option<extern "C" fn (ctx: *mut u8, flags: u32) -> zx_status_t>,
+}
+
+bitflags! {
+    #[repr(C)]
+    pub flags device_add_flags_t: u32 {
+        const DEVICE_ADD_NON_BINDABLE = 1 << 0,
+        const DEVICE_ADD_INSTANCE     = 1 << 1,
+        const DEVICE_ADD_BUSDEV       = 1 << 2,
+    }
+}
+
+// Device Manager API
+#[repr(C)]
+pub struct device_add_args_t {
+    pub version: u64,
+    pub name: *const c_char,
+    pub ctx: *mut u8,
+    pub ops: *mut zx_protocol_device_t,
+    pub props: *mut zx_device_prop_t,
+    pub prop_count: u32,
+    pub proto_id: u32,
+    pub proto_ops: *mut u8,
+    pub busdev_args: *const c_char,
+    pub rsrc: sys::zx_handle_t,
+    pub flags: device_add_flags_t,
+}
+
+impl device_add_args_t {
+    pub fn new() -> device_add_args_t {
+        device_add_args_t {
+            version: DEVICE_ADD_ARGS_VERSION,
+            name: std::ptr::null_mut(),
+            ctx: std::ptr::null_mut(),
+            ops: std::ptr::null_mut(),
+            props: std::ptr::null_mut(),
+            prop_count: 0,
+            proto_id: 0,
+            proto_ops: std::ptr::null_mut(),
+            busdev_args: std::ptr::null_mut(),
+            rsrc: 0,
+            flags: DEVICE_ADD_NON_BINDABLE,
+        }
+    }
+}
+
+#[repr(C)]
+pub struct zx_driver_rec {
+    pub ops: *const zx_driver_ops_t,
+    pub driver: *mut zx_driver_t,
+    pub log_flags: u32,
+}
+
+#[link(name = "ddk")]
+extern "C" {
+    pub fn device_add(parent: *mut zx_device_t, args: *mut device_add_args_t, out: *mut *mut zx_device_t) -> sys::zx_status_t;
+    pub fn device_add_from_driver(driver: *mut zx_driver_t, parent: *mut zx_device_t, args: *mut device_add_args_t, out: *mut *mut zx_device_t) -> sys::zx_status_t;
+    pub fn device_remove(device: *mut zx_device_t) -> sys::zx_status_t;
+    pub fn device_rebind(device: *mut zx_device_t) -> sys::zx_status_t;
+    pub fn device_unbind(device: *mut zx_device_t);
+    pub fn load_firmware(device: *mut zx_device_t, path: *const c_char, fw: *mut sys::zx_handle_t, size: *mut usize) -> sys::zx_status_t;
+    pub fn device_get_name(dev: *mut zx_device_t) -> *const c_char;
+    pub fn device_get_parent(dev: *mut zx_device_t) -> *mut zx_device_t;
+    pub fn device_get_resource(dev: *mut zx_device_t) -> sys::zx_handle_t;
+    pub fn device_get_protocol(dev: *mut zx_device_t, proto_id: u32, protocol: *mut u8) -> sys::zx_status_t;
+    pub fn device_read(dev: *mut zx_device_t, buf: *mut u8, count: usize, off: sys::zx_off_t, actual: *mut usize) -> sys::zx_status_t;
+    pub fn device_write(dev: *mut zx_device_t, buf: *const u8, count: usize, off: sys::zx_off_t, actual: *mut usize) -> sys::zx_status_t;
+    pub fn device_get_size(dev: *mut zx_device_t) -> sys::zx_off_t;
+    pub fn device_ioctl(dev: *mut zx_device_t, op: u32, in_buf: *const u8, in_len: usize, out_buf: *mut u8, out_len: usize, out_actual: *mut usize) -> sys::zx_status_t;
+    pub fn device_iotxn_queue(dev: *mut zx_device_t, txn: *mut iotxn_t) -> sys::zx_status_t;
+    pub fn device_stat_clr_set(dev: *mut zx_device_t, clearflag: sys::zx_signals_t, setflag: sys::zx_signals_t);
 }
--- public/rust/crates/ddk-rs/src/lib.rs
+++ public/rust/crates/ddk-rs/src/lib.rs
@@ -2,7 +2,196 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//! Rust DDK for the Zircon kernel.
+extern crate ddk_sys;
+extern crate fuchsia_zircon as zircon;
+extern crate fuchsia_zircon_sys as zircon_sys;
 
-pub trait Device {
+use std::ffi::{CStr, CString};
+use zircon::Status;
+use zircon_sys as sys;
+use std::slice;
+use std::os::raw::c_char;
+
+pub struct Driver {
+    driver: *mut ddk_sys::zx_driver_t,
+}
+
+impl Driver {
+    pub fn wrap(driver: *mut ddk_sys::zx_driver_t) -> Driver {
+        Driver { driver: driver }
+    }
+}
+
+pub struct Device {
+    device: *mut ddk_sys::zx_device_t,
+}
+
+impl Device {
+    pub fn wrap(device: *mut ddk_sys::zx_device_t) -> Device {
+        Device { device: device }
+    }
+}
+
+pub trait DeviceOps {
+    fn name(&self) -> String;
+
+    fn open(&mut self, flags: u32) -> Status {
+        Status::NoError
+    }
+
+    fn openat(&mut self, path: &str, flags: u32) -> Status {
+        Status::ErrNotSupported
+    }
+
+    fn close(&mut self, flags: u32) -> Status {
+        Status::NoError
+    }
+
+    fn unbind(&mut self) -> Status {
+        Status::NoError
+    }
+
+    fn release(&mut self) -> Status {
+        Status::ErrNotSupported
+    }
+
+    fn read(&mut self, buf: &mut [u8], offset: u64) -> Result<usize, Status> {
+        Err(Status::ErrNotSupported)
+    }
+
+    fn write(&mut self, buf: &[u8], offset: u64) -> Result<usize, Status> {
+        Err(Status::ErrNotSupported)
+    }
+}
+
+pub fn add_device(device_ops: Box<DeviceOps>) -> Result<Device, Status> {
+    let device_name = device_ops.name();
+    if device_name.len() > ddk_sys::ZX_DEVICE_NAME_MAX {
+        return Err(Status::ErrInvalidArgs)
+    }
+
+    let mut device_add_args: ddk_sys::device_add_args_t = ddk_sys::device_add_args_t::new();
+    // TODO(stange): See if it's necessary to double Box device_ops.
+    device_add_args.ctx = Box::into_raw(Box::new(device_ops)) as *mut u8;
+    device_add_args.name = CString::new(device_name).unwrap().as_ptr();
+    unsafe {
+        device_add_args.ops = &mut DEVICE_OPS;
+        let mut ddk_device: *mut ddk_sys::zx_device_t = std::ptr::null_mut();
+        // TODO(stange): Look into passing in parent or getting it from somewhere.
+        let ret = ddk_sys::device_add(std::ptr::null_mut(), &mut device_add_args, &mut ddk_device);
+        match ret {
+            sys::ZX_OK => Ok(Device::wrap(ddk_device)),
+            _ => Err(Status::from_raw(ret)),
+        }
+    }
+}
+
+extern fn ddk_get_protocol(ctx: *mut u8, proto_id: u32, protocol: *mut u8) -> sys::zx_status_t {
+    sys::ZX_ERR_NOT_SUPPORTED
+}
+
+extern fn ddk_open(ctx: *mut u8, dev_out: *mut *mut ddk_sys::zx_device_t, flags: u32) -> sys::zx_status_t {
+    let mut device: Box<Box<DeviceOps>> = unsafe { Box::from_raw(ctx as *mut Box<DeviceOps>) };
+    let ret = device.open(flags);
+    let _ = Box::into_raw(device);
+    ret as sys::zx_status_t
+}
+
+extern fn ddk_open_at(ctx: *mut u8, dev_out: *mut *mut ddk_sys::zx_device_t, path: *const c_char, flags: u32) -> sys::zx_status_t {
+    let mut device: Box<Box<DeviceOps>> = unsafe { Box::from_raw(ctx as *mut Box<DeviceOps>) };
+    let path = unsafe { CStr::from_ptr(path).to_str().unwrap() };
+    let ret = device.openat(path, flags);
+    let _ = Box::into_raw(device);
+    ret as sys::zx_status_t
+}
+
+extern fn ddk_close(ctx: *mut u8, flags: u32) -> sys::zx_status_t {
+    let mut device: Box<Box<DeviceOps>> = unsafe { Box::from_raw(ctx as *mut Box<DeviceOps>) };
+    let ret = device.close(flags);
+    let _ = Box::into_raw(device);
+    ret as sys::zx_status_t
+}
+
+extern fn ddk_unbind(ctx: *mut u8) {
+    let mut device: Box<Box<DeviceOps>> = unsafe { Box::from_raw(ctx as *mut Box<DeviceOps>) };
+    device.unbind();
+    let _ = Box::into_raw(device);
+}
+
+extern fn ddk_release(ctx: *mut u8) {
+    let mut device: Box<Box<DeviceOps>> = unsafe { Box::from_raw(ctx as *mut Box<DeviceOps>) };
+    device.release();
+    // Don't Box::into_raw the device so it can be freed by Rust
+}
+
+extern fn ddk_read(ctx: *mut u8, buf: *mut u8, count: usize, off: sys::zx_off_t, actual: *mut usize) -> sys::zx_status_t {
+    let mut device: Box<Box<DeviceOps>> = unsafe { Box::from_raw(ctx as *mut Box<DeviceOps>) };
+    let rust_buf = unsafe { slice::from_raw_parts_mut(buf, count) };
+    let ret = match device.read(rust_buf, off) {
+        Ok(r) => {
+                unsafe { *actual = r };
+                sys::ZX_OK
+            },
+        // TODO(qwandor): Investigate adding a to_raw method for Status.
+        Err(status) => status as sys::zx_status_t
+    };
+    let _ = Box::into_raw(device);
+    ret
+}
+
+extern fn ddk_write(ctx: *mut u8, buf: *const u8, count: usize, off: sys::zx_off_t, actual: *mut usize) -> sys::zx_status_t {
+    let mut device: Box<Box<DeviceOps>> = unsafe { Box::from_raw(ctx as *mut Box<DeviceOps>) };
+    let rust_buf = unsafe { slice::from_raw_parts(buf, count) };
+    let ret = match device.write(rust_buf, off) {
+        Ok(r) => {
+                unsafe { *actual = r };
+                sys::ZX_OK
+            },
+        Err(status) => status as sys::zx_status_t
+    };
+    let _ = Box::into_raw(device);
+    ret
+}
+
+extern fn ddk_iotxn_queue(ctx: *mut u8, txn: *mut ddk_sys::iotxn_t) {
+}
+
+extern fn ddk_get_size(ctx: *mut u8) -> u64 {
+    0
+}
+
+extern fn ddk_ioctl(ctx: *mut u8, op: u32, in_buf: *const u8, in_len: usize, out_buf: *mut u8, out_len: usize, out_actual: *mut usize) -> sys::zx_status_t {
+    sys::ZX_ERR_NOT_SUPPORTED
+}
+
+extern fn ddk_suspend(ctx: *mut u8, flags: u32) -> sys::zx_status_t {
+    sys::ZX_ERR_NOT_SUPPORTED
+}
+
+extern fn ddk_resume(ctx: *mut u8, flags: u32) -> sys::zx_status_t {
+    sys::ZX_ERR_NOT_SUPPORTED
+}
+
+static mut DEVICE_OPS: ddk_sys::zx_protocol_device_t = ddk_sys::zx_protocol_device_t {
+        version: ddk_sys::DEVICE_OPS_VERSION,
+        get_protocol: Some(ddk_get_protocol),
+        open: Some(ddk_open),
+        open_at: Some(ddk_open_at),
+        close: Some(ddk_close),
+        unbind: Some(ddk_unbind),
+        release: Some(ddk_release),
+        read: Some(ddk_read),
+        write: Some(ddk_write),
+        iotxn_queue: Some(ddk_iotxn_queue),
+        get_size: Some(ddk_get_size),
+        ioctl: Some(ddk_ioctl),
+        suspend: Some(ddk_suspend),
+        resume: Some(ddk_resume),
+};
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
 }
