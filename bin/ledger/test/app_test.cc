c952e8125dbfaea52b13711434f27e1f7b8f424e
Benjamin Lerman <qsr@chromium.org>
2017-09-26 11:25:33 +0200

    Update test harness following //garnet changes.
    Change-Id: I4657c6eb529836c917a36abca2d75ee1741535ce

--- bin/ledger/test/app_test.cc
+++ bin/ledger/test/app_test.cc
@@ -5,24 +5,26 @@
 #include "peridot/bin/ledger/test/app_test.h"
 
 #include "lib/app/cpp/application_context.h"
-#include "lib/fxl/logging.h"
 #include "lib/test_runner/cpp/reporting/gtest_listener.h"
 #include "lib/test_runner/cpp/reporting/reporter.h"
-#include "peridot/bin/ledger/test/get_ledger.h"
+#include "gtest/gtest.h"
+#include "lib/fsl/tasks/message_loop.h"
 
 namespace test {
 
 int TestMain(int argc, char** argv) {
-  test_runner::Reporter reporter(argv[0]);
-  test_runner::GTestListener listener(argv[0], &reporter);
-
-  auto context = app::ApplicationContext::CreateFromStartupInfoNotChecked();
-  reporter.Start(context.get());
+  test_runner::GTestListener listener(argv[0]);
 
   testing::InitGoogleTest(&argc, argv);
   testing::UnitTest::GetInstance()->listeners().Append(&listener);
   int status = RUN_ALL_TESTS();
   testing::UnitTest::GetInstance()->listeners().Release(&listener);
+
+  {
+    fsl::MessageLoop message_loop;
+    auto context = app::ApplicationContext::CreateFromStartupInfoNotChecked();
+    test_runner::ReportResult(argv[0], context.get(), listener.GetResults());
+  }
 
   return status;
 }
