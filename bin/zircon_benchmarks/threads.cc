cc3d63c4f94805b8bee230fe088d0e143c55aa9a
David Stevens <stevensd@google.com>
2017-09-26 09:39:16 -0700

    [zircon][benchmarks] Benchmark thread create-and-join
    Change-Id: I8942a5d1bf7f664675e3ccc571a8d0d0a63dee3d

--- bin/zircon_benchmarks/threads.cc
+++ bin/zircon_benchmarks/threads.cc
@@ -6,38 +6,61 @@
 #include <zircon/process.h>
 #include <zircon/syscalls.h>
 
-class Thread : public benchmark::Fixture {
- private:
-  void SetUp(benchmark::State& state) override {
-    static const char pname[] = "test process";
-    zx_status_t status = zx_process_create(zx_job_default(), pname,
-                         sizeof(pname), 0, &proc, &vmar);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to create process");
-    }
-  }
-
-  void TearDown(benchmark::State& state) override {
-    zx_handle_close(proc);
-    zx_handle_close(vmar);
-  }
-
- protected:
-  zx_handle_t proc;
-  zx_handle_t vmar;
-};
-
+class Thread : public benchmark::Fixture {};
 
 BENCHMARK_F(Thread, Create)(benchmark::State& state) {
   zx_handle_t out;
-  static const char tname[] = "test thread";
+  constexpr char tname[] = "test thread";
   while (state.KeepRunning()) {
-    if (zx_thread_create(proc, tname, sizeof(tname), 0, &out) != ZX_OK) {
+    if (zx_thread_create(zx_process_self(), tname, sizeof(tname), 0, &out) != ZX_OK) {
       state.SkipWithError("Failed to create thread");
       return;
     }
     state.PauseTiming();
     zx_handle_close(out);
+    state.ResumeTiming();
+  }
+}
+
+BENCHMARK_F(Thread, CreateAndJoin)(benchmark::State& state) {
+  zx_handle_t thread;
+  constexpr char tname[] = "test thread";
+  while (state.KeepRunning()) {
+    if (zx_thread_create(zx_process_self(), tname, sizeof(tname), 0, &thread) != ZX_OK) {
+      state.SkipWithError("Failed to create thread");
+      return;
+    }
+
+    uint64_t stack_size = 16 * 1024u;
+    zx_handle_t stack_vmo;
+    if (zx_vmo_create(stack_size, 0, &stack_vmo) != ZX_OK) {
+      state.SkipWithError("Failed to create vmo");
+      return;
+    }
+
+    zx_vaddr_t stack_base;
+    uint32_t perm = ZX_VM_FLAG_PERM_READ | ZX_VM_FLAG_PERM_WRITE;
+    if (zx_vmar_map(zx_vmar_root_self(), 0, stack_vmo, 0, stack_size, perm, &stack_base) != ZX_OK) {
+      state.SkipWithError("Failed to map vmo");
+      return;
+    }
+
+    uintptr_t entry = reinterpret_cast<uintptr_t>(&zx_thread_exit);
+    uintptr_t stack = reinterpret_cast<uintptr_t>(stack_base + stack_size);
+    if (zx_thread_start(thread, entry, stack, 0, 0) != ZX_OK) {
+      state.SkipWithError("Failed to start thread");
+      return;
+    }
+
+    zx_signals_t observed;
+    if (zx_object_wait_one(thread, ZX_THREAD_TERMINATED, ZX_TIME_INFINITE, &observed) != ZX_OK) {
+      state.SkipWithError("Failed to wait for thread");
+      return;
+    }
+
+    state.PauseTiming();
+    zx_handle_close(thread);
+    zx_handle_close(stack_vmo);
     state.ResumeTiming();
   }
 }
