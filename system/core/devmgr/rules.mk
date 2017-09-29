472b3a5dd56016771fcee13eed6a833467785963
Brian Swetland <swetland@google.com>
2017-09-29 12:08:00 -0700

    [devmgr][fshost] enable dedicated filesystem host process
    In the event of a crash in the root, tmpfs, bootfs, or
    block device watcher (that launches minfs, etc), the devmgr
    and devhosts now survive, making the system more debuggable
    and robust.
    
    Change-Id: I7196cb4d33e6a6d2ffd4bcb487db42134bf89e1d

--- system/core/devmgr/rules.mk
+++ system/core/devmgr/rules.mk
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-ENABLE_FSHOST := $(call TOBOOL,$(ENABLE_FSHOST))
+ENABLE_FSHOST := true
 
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
