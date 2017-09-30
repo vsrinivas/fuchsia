hungwenchien/magenta

[kernel][mp] add new header with types and routines to deal with cpu numbers
Add a few more types and switch some apis to using those. No functional
change.

Change-Id: I67add1247cf36d9e6a55f15dd809ffe4bafe06fd

kernel/arch/arm64/mp.cpp
@@ -64,7 +64,7 @@ static uint arch_curr_cpu_num_slow() {
     return arm64_cpu_map[cluster][cpu];
 }
 
-status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_t ipi) {
+status_t arch_mp_send_ipi(mp_ipi_target_t target, cpu_mask_t mask, mp_ipi_t ipi) {
     LTRACEF("target %d mask %#x, ipi %d\n", target, mask, ipi);
 
     // translate the high level target + mask mechanism into just a mask
@@ -72,7 +72,7 @@ status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_t i
         mask = (1ul << SMP_MAX_CPUS) - 1;
     } else if (target == MP_IPI_TARGET_ALL) {
         mask = (1ul << SMP_MAX_CPUS) - 1;
-        mask &= ~(1u << arch_curr_cpu_num());
+        mask &= ~cpu_num_to_mask(arch_curr_cpu_num());
     }
 
     return interrupt_send_ipi(mask, ipi);

kernel/arch/x86/mp.cpp
@@ -77,7 +77,7 @@ status_t x86_allocate_ap_structures(uint32_t *apic_ids, uint8_t cpu_count)
     return MX_OK;
 }
 
-void x86_init_percpu(uint cpu_num)
+void x86_init_percpu(cpu_num_t cpu_num)
 {
     struct x86_percpu *const percpu =
         cpu_num == 0 ? &bp_percpu : &ap_percpus[cpu_num - 1];
@@ -217,7 +217,7 @@ int x86_apic_id_to_cpu_num(uint32_t apic_id)
     return -1;
 }
 
-status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_t ipi)
+status_t arch_mp_send_ipi(mp_ipi_target_t target, cpu_mask_t mask, mp_ipi_t ipi)
 {
     uint8_t vector = 0;
     switch (ipi) {
@@ -244,7 +244,7 @@ status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_t i
 
     ASSERT(x86_num_cpus <= sizeof(mask) * CHAR_BIT);
 
-    mp_cpu_mask_t remaining = mask;
+    cpu_mask_t remaining = mask;
     uint cpu_id = 0;
     while (remaining && cpu_id < x86_num_cpus) {
         if (remaining & 1) {

vsrinivas/fuchsia

kernel/arch/arm64/mp.cpp
@@ -65,7 +65,7 @@ static uint arch_curr_cpu_num_slow() {
     return arm64_cpu_map[cluster][cpu];
 }
 
-zx_status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_t ipi) {
+zx_status_t arch_mp_send_ipi(mp_ipi_target_t target, cpu_mask_t mask, mp_ipi_t ipi) {
     LTRACEF("target %d mask %#x, ipi %d\n", target, mask, ipi);
 
     // translate the high level target + mask mechanism into just a mask
@@ -73,7 +73,7 @@ zx_status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_
         mask = (1ul << SMP_MAX_CPUS) - 1;
     } else if (target == MP_IPI_TARGET_ALL) {
         mask = (1ul << SMP_MAX_CPUS) - 1;
-        mask &= ~(1u << arch_curr_cpu_num());
+        mask &= ~cpu_num_to_mask(arch_curr_cpu_num());
     }
 
     return interrupt_send_ipi(mask, ipi);

kernel/arch/x86/mp.cpp
@@ -78,7 +78,7 @@ zx_status_t x86_allocate_ap_structures(uint32_t *apic_ids, uint8_t cpu_count)
     return ZX_OK;
 }
 
-void x86_init_percpu(uint cpu_num)
+void x86_init_percpu(cpu_num_t cpu_num)
 {
     struct x86_percpu *const percpu =
         cpu_num == 0 ? &bp_percpu : &ap_percpus[cpu_num - 1];
@@ -225,7 +225,7 @@ int x86_apic_id_to_cpu_num(uint32_t apic_id)
     return -1;
 }
 
-zx_status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_t ipi)
+zx_status_t arch_mp_send_ipi(mp_ipi_target_t target, cpu_mask_t mask, mp_ipi_t ipi)
 {
     uint8_t vector = 0;
     switch (ipi) {
@@ -252,7 +252,7 @@ zx_status_t arch_mp_send_ipi(mp_ipi_target_t target, mp_cpu_mask_t mask, mp_ipi_
 
     ASSERT(x86_num_cpus <= sizeof(mask) * CHAR_BIT);
 
-    mp_cpu_mask_t remaining = mask;
+    cpu_mask_t remaining = mask;
     uint cpu_id = 0;
     while (remaining && cpu_id < x86_num_cpus) {
         if (remaining & 1) {