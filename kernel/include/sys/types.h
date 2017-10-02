8c0ecea0ddb85a3c1252b816d13ee36539335ee3
George Kulakowski <kulakowski@google.com>
2017-10-02 12:09:16 -0700

    [kernel][types] Remove unused lk_time_t
    All users have now migrated to zx_time_t and zx_duration_t.
    
    Change-Id: I3033865901422381b6fa7e3f07b87f5aab6b0458

--- kernel/include/sys/types.h
+++ kernel/include/sys/types.h
@@ -26,8 +26,6 @@ typedef uintptr_t addr_t;
 typedef uintptr_t vaddr_t;
 typedef uintptr_t paddr_t;
 
-typedef uint64_t lk_time_t; // nanoseconds
-
 enum handler_return {
     INT_NO_RESCHEDULE = 0,
     INT_RESCHEDULE,
