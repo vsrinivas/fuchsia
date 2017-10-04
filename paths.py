3f6044c66f5420577042cb4894fc0d7a8dbda9d5
Ross Wang <rosswang@google.com>
2017-10-04 10:45:01 -0700

    [git-file-format] Making dartfmt run more robust
    A few times, I've found I don't have the flutter/cache directory. This
    patch adds more helpful error reporting for failed format commands and
    uses the SDK under dart/tools instead.
    
    Change-Id: I026649041a51a9527190cad84c098b02390e55f6

--- paths.py
+++ paths.py
@@ -3,9 +3,18 @@
 # found in the LICENSE file.
 
 import os.path
+import platform
 
 FUCHSIA_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ZIRCON_ROOT = os.path.join(FUCHSIA_ROOT, "zircon")
 BUILDTOOLS_ROOT = os.path.join(FUCHSIA_ROOT, "buildtools")
 FLUTTER_ROOT = os.path.join(FUCHSIA_ROOT, "lib", "flutter")
-DART_ROOT = os.path.join(FLUTTER_ROOT, "bin", "cache", "dart-sdk")
+
+DART_PLATFORM = {
+    "Linux": "linux",
+    "Darwin": "mac",
+    "Windows": "win"
+}[platform.system()]
+
+DART_ROOT = os.path.join(FUCHSIA_ROOT, "dart", "tools", "sdks", DART_PLATFORM,
+                         "dart-sdk")
