e17bf78f0b8c11e817eab084fa81f7158c2a6605
Roland McGrath <mcgrathr@google.com>
2017-10-04 15:56:18 -0700

    [kernel][x86] Use 'rep stosb' to zero bss
    This is optimal on all recent Intel hardware, and less code.
    
    Also get rid of the bss_setup macro from asm.h that was only used
    in start.S.  In the 64-bit case, use PC-relative addressing.
    
    Change-Id: I7eb17868f32bdfe8eb7bd824824c5af66dd0f798

--- kernel/arch/x86/include/arch/x86/asm.h
+++ kernel/arch/x86/include/arch/x86/asm.h
@@ -13,17 +13,3 @@
 #define PHYS_LOAD_ADDRESS (MEMBASE + KERNEL_LOAD_OFFSET)
 #define PHYS_ADDR_DELTA (KERNEL_BASE + KERNEL_LOAD_OFFSET - PHYS_LOAD_ADDRESS)
 #define PHYS(x) ((x) - PHYS_ADDR_DELTA)
-
-/* zeros the bss for the kernel */
-/* trashes a lot of registers */
-.macro bss_setup
-    movl $PHYS(__bss_start), %edi /* starting address of the bss */
-    movl $PHYS(__bss_end), %ecx   /* find the length of the bss in bytes */
-    subl %edi, %ecx
-    shrl $2, %ecx       /* convert to 32 bit words, since the bss is aligned anyway */
-0:
-    movl $0, (%edi)
-    addl $4, %edi
-    loop 0b
-.endm
-
