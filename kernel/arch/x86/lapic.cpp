853e1f1beab59fa5837907261e30e2c9b317c323
Travis Geiselbrecht <travisg@google.com>
2017-10-04 17:53:09 -0700

    [kernel][x86][lapic] switch the register accesses to a helper routine
    This sets things up for using x2apic
    
    Change-Id: I86b03c40c84bf3f316717b457a2f30aab0d4942c

--- kernel/arch/x86/lapic.cpp
+++ kernel/arch/x86/lapic.cpp
@@ -34,32 +34,31 @@ static void *apic_virt_base;
 static uint8_t bsp_apic_id;
 static bool bsp_apic_id_valid;
 
-#define LAPIC_ID_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x020))
-#define LAPIC_VERSION_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x030))
-#define TASK_PRIORITY_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x080))
-#define PROCESSOR_PRIORITY_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x0A0))
-#define EOI_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x0B0))
-#define LOGICAL_DST_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x0D0))
-#define SPURIOUS_IRQ_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x0F0))
-#define IN_SERVICE_ADDR(x) \
-        ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x100 + ((x) << 4)))
-#define TRIGGER_MODE_ADDR(x) \
-        ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x180 + ((x) << 4)))
-#define IRQ_REQUEST_ADDR(x) \
-        ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x200 + ((x) << 4)))
-#define ERROR_STATUS_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x280))
-#define LVT_CMCI_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x2F0))
-#define IRQ_CMD_LOW_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x300))
-#define IRQ_CMD_HIGH_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x310))
-#define LVT_TIMER_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x320))
-#define LVT_THERMAL_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x330))
-#define LVT_PERF_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x340))
-#define LVT_LINT0_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x350))
-#define LVT_LINT1_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x360))
-#define LVT_ERROR_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x370))
-#define INIT_COUNT_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x380))
-#define CURRENT_COUNT_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x390))
-#define DIVIDE_CONF_ADDR ((volatile uint32_t *)((uintptr_t)apic_virt_base + 0x3E0))
+// local apic registers
+// set as an offset into the mmio region here
+#define LAPIC_REG_ID                  (0x020)
+#define LAPIC_REG_VERSION             (0x030)
+#define LAPIC_REG_TASK_PRIORITY       (0x080)
+#define LAPIC_REG_PROCESSOR_PRIORITY  (0x0A0)
+#define LAPIC_REG_EOI                 (0x0B0)
+#define LAPIC_REG_LOGICAL_DST         (0x0D0)
+#define LAPIC_REG_SPURIOUS_IRQ        (0x0F0)
+#define LAPIC_REG_IN_SERVICE(x)       (0x100 + ((x) << 4))
+#define LAPIC_REG_TRIGGER_MODE(x)     (0x180 + ((x) << 4))
+#define LAPIC_REG_IRQ_REQUEST(x)      (0x200 + ((x) << 4))
+#define LAPIC_REG_ERROR_STATUS        (0x280)
+#define LAPIC_REG_LVT_CMCI            (0x2F0)
+#define LAPIC_REG_IRQ_CMD_LOW         (0x300)
+#define LAPIC_REG_IRQ_CMD_HIGH        (0x310)
+#define LAPIC_REG_LVT_TIMER           (0x320)
+#define LAPIC_REG_LVT_THERMAL         (0x330)
+#define LAPIC_REG_LVT_PERF            (0x340)
+#define LAPIC_REG_LVT_LINT0           (0x350)
+#define LAPIC_REG_LVT_LINT1           (0x360)
+#define LAPIC_REG_LVT_ERROR           (0x370)
+#define LAPIC_REG_INIT_COUNT          (0x380)
+#define LAPIC_REG_CURRENT_COUNT       (0x390)
+#define LAPIC_REG_DIVIDE_CONF         (0x3E0)
 
 // Spurious IRQ bitmasks
 #define SVR_APIC_ENABLE (1 << 8)
@@ -85,6 +84,22 @@ static bool bsp_apic_id_valid;
 
 static void apic_error_init(void);
 static void apic_timer_init(void);
+
+static uint32_t lapic_reg_read(size_t offset) {
+    return *((volatile uint32_t *)((uintptr_t)apic_virt_base + offset));
+}
+
+static void lapic_reg_write(size_t offset, uint32_t val) {
+    *((volatile uint32_t *)((uintptr_t)apic_virt_base + offset)) = val;
+}
+
+static void lapic_reg_or(size_t offset, uint32_t bits) {
+    lapic_reg_write(offset, lapic_reg_read(offset) | bits);
+}
+
+static void lapic_reg_and(size_t offset, uint32_t bits) {
+    lapic_reg_write(offset, lapic_reg_read(offset) & bits);
+}
 
 // This function must be called once on the kernel address space
 void apic_vm_init(void)
@@ -129,7 +144,7 @@ void apic_local_init(void)
 
     // Specify the spurious interrupt vector and enable the local APIC
     uint32_t svr = SVR_SPURIOUS_VECTOR(X86_INT_APIC_SPURIOUS) | SVR_APIC_ENABLE;
-    *SPURIOUS_IRQ_ADDR = svr;
+    lapic_reg_write(LAPIC_REG_SPURIOUS_IRQ, svr);
 
     apic_error_init();
     apic_timer_init();
@@ -137,7 +152,7 @@ void apic_local_init(void)
 
 uint8_t apic_local_id(void)
 {
-    return (uint8_t)(*LAPIC_ID_ADDR >> 24);
+    return (uint8_t)(lapic_reg_read(LAPIC_REG_ID) >> 24);
 }
 
 uint8_t apic_bsp_id(void)
@@ -147,7 +162,7 @@ uint8_t apic_bsp_id(void)
 }
 
 static inline void apic_wait_for_ipi_send(void) {
-    while (*IRQ_CMD_LOW_ADDR & ICR_DELIVERY_PENDING);
+    while (lapic_reg_read(LAPIC_REG_IRQ_CMD_LOW) & ICR_DELIVERY_PENDING);
 }
 
 // We only support physical destination modes for now
@@ -165,8 +180,8 @@ void apic_send_ipi(
 
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
-    *IRQ_CMD_HIGH_ADDR = ICR_DST(dst_apic_id);
-    *IRQ_CMD_LOW_ADDR = request;
+    lapic_reg_write(LAPIC_REG_IRQ_CMD_HIGH, ICR_DST(dst_apic_id));
+    lapic_reg_write(LAPIC_REG_IRQ_CMD_LOW, request);
     apic_wait_for_ipi_send();
     arch_interrupt_restore(state, 0);
 }
@@ -178,7 +193,7 @@ void apic_send_self_ipi(uint8_t vector, enum apic_interrupt_delivery_mode dm)
 
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
-    *IRQ_CMD_LOW_ADDR = request;
+    lapic_reg_write(LAPIC_REG_IRQ_CMD_LOW, request);
     apic_wait_for_ipi_send();
     arch_interrupt_restore(state, 0);
 }
@@ -193,8 +208,8 @@ void apic_send_broadcast_self_ipi(
 
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
-    *IRQ_CMD_HIGH_ADDR = ICR_DST_BROADCAST;
-    *IRQ_CMD_LOW_ADDR = request;
+    lapic_reg_write(LAPIC_REG_IRQ_CMD_HIGH, ICR_DST_BROADCAST);
+    lapic_reg_write(LAPIC_REG_IRQ_CMD_LOW, request);
     apic_wait_for_ipi_send();
     arch_interrupt_restore(state, 0);
 }
@@ -209,8 +224,8 @@ void apic_send_broadcast_ipi(
 
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
-    *IRQ_CMD_HIGH_ADDR = ICR_DST_BROADCAST;
-    *IRQ_CMD_LOW_ADDR = request;
+    lapic_reg_write(LAPIC_REG_IRQ_CMD_HIGH, ICR_DST_BROADCAST);
+    lapic_reg_write(LAPIC_REG_IRQ_CMD_LOW, request);
     apic_wait_for_ipi_send();
     arch_interrupt_restore(state, 0);
 }
@@ -218,7 +233,7 @@ void apic_send_broadcast_ipi(
 void apic_issue_eoi(void)
 {
     // Write any value to the EOI address to issue an EOI
-    *EOI_ADDR = 1;
+    lapic_reg_write(LAPIC_REG_EOI, 1);
 }
 
 // If this function returns an error, timer state will not have
@@ -236,37 +251,37 @@ static zx_status_t apic_timer_set_divide_value(uint8_t v) {
         case 128: new_value = 0xa; break;
         default: return ZX_ERR_INVALID_ARGS;
     }
-    *DIVIDE_CONF_ADDR = new_value;
+    lapic_reg_write(LAPIC_REG_DIVIDE_CONF, new_value);
     return ZX_OK;
 }
 
 static void apic_timer_init(void) {
-    *LVT_TIMER_ADDR = LVT_VECTOR(X86_INT_APIC_TIMER) | LVT_MASKED;
+    lapic_reg_write(LAPIC_REG_LVT_TIMER, LVT_VECTOR(X86_INT_APIC_TIMER) | LVT_MASKED);
 }
 
 // Racy; primarily useful for calibrating the timer.
 uint32_t apic_timer_current_count(void) {
-    return *CURRENT_COUNT_ADDR;
+    return lapic_reg_read(LAPIC_REG_CURRENT_COUNT);
 }
 
 void apic_timer_mask(void) {
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
-    *LVT_TIMER_ADDR |= LVT_MASKED;
+    lapic_reg_or(LAPIC_REG_LVT_TIMER, LVT_MASKED);
     arch_interrupt_restore(state, 0);
 }
 
 void apic_timer_unmask(void) {
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
-    *LVT_TIMER_ADDR &= ~LVT_MASKED;
+    lapic_reg_and(LAPIC_REG_LVT_TIMER, ~LVT_MASKED);
     arch_interrupt_restore(state, 0);
 }
 
 void apic_timer_stop(void) {
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
-    *INIT_COUNT_ADDR = 0;
+    lapic_reg_write(LAPIC_REG_INIT_COUNT, 0);
     if (x86_feature_test(X86_FEATURE_TSC_DEADLINE)) {
         write_msr(X86_MSR_IA32_TSC_DEADLINE, 0);
     }
@@ -288,8 +303,8 @@ zx_status_t apic_timer_set_oneshot(uint32_t count, uint8_t divisor, bool masked)
     if (status != ZX_OK) {
         goto cleanup;
     }
-    *LVT_TIMER_ADDR = timer_config;
-    *INIT_COUNT_ADDR = count;
+    lapic_reg_write(LAPIC_REG_LVT_TIMER, timer_config);
+    lapic_reg_write(LAPIC_REG_INIT_COUNT, count);
 cleanup:
     arch_interrupt_restore(state, 0);
     return status;
@@ -307,7 +322,7 @@ void apic_timer_set_tsc_deadline(uint64_t deadline, bool masked) {
     spin_lock_saved_state_t state;
     arch_interrupt_save(&state, 0);
 
-    *LVT_TIMER_ADDR = timer_config;
+    lapic_reg_write(LAPIC_REG_LVT_TIMER, timer_config);
     // Intel recommends using an MFENCE to ensure the LVT_TIMER_ADDR write
     // takes before the write_msr(), since writes to this MSR are ignored if the
     // time mode is not DEADLINE.
@@ -326,8 +341,8 @@ zx_status_t apic_timer_set_periodic(uint32_t count, uint8_t divisor) {
     if (status != ZX_OK) {
         goto cleanup;
     }
-    *LVT_TIMER_ADDR = LVT_VECTOR(X86_INT_APIC_TIMER) | LVT_TIMER_MODE_PERIODIC;
-    *INIT_COUNT_ADDR = count;
+    lapic_reg_write(LAPIC_REG_LVT_TIMER, LVT_VECTOR(X86_INT_APIC_TIMER) | LVT_TIMER_MODE_PERIODIC);
+    lapic_reg_write(LAPIC_REG_INIT_COUNT, count);
 cleanup:
     arch_interrupt_restore(state, 0);
     return status;
@@ -338,9 +353,9 @@ enum handler_return apic_timer_interrupt_handler(void) {
 }
 
 static void apic_error_init(void) {
-    *LVT_ERROR_ADDR = LVT_VECTOR(X86_INT_APIC_ERROR);
+    lapic_reg_write(LAPIC_REG_LVT_ERROR, LVT_VECTOR(X86_INT_APIC_ERROR));
     // Re-arm the error interrupt triggering mechanism
-    *ERROR_STATUS_ADDR = 0;
+    lapic_reg_write(LAPIC_REG_ERROR_STATUS, 0);
 }
 
 enum handler_return apic_error_interrupt_handler(void) {
@@ -348,8 +363,8 @@ enum handler_return apic_error_interrupt_handler(void) {
 
     // This write doesn't effect the subsequent read, but is required prior to
     // reading.
-    *ERROR_STATUS_ADDR = 0;
-    panic("APIC error detected: %u\n", *ERROR_STATUS_ADDR);
+    lapic_reg_write(LAPIC_REG_ERROR_STATUS, 0);
+    panic("APIC error detected: %u\n", lapic_reg_read(LAPIC_REG_ERROR_STATUS));
 }
 
 static int cmd_apic(int argc, const cmd_args *argv, uint32_t flags)
@@ -371,17 +386,17 @@ usage:
           goto notenoughargs;
         uint8_t vec = (uint8_t)argv[2].u;
         apic_send_broadcast_ipi(vec, DELIVERY_MODE_FIXED);
-        printf("irr: %x\n", *IRQ_REQUEST_ADDR(vec / 32));
-        printf("isr: %x\n", *IN_SERVICE_ADDR(vec / 32));
-        printf("icr: %x\n", *IRQ_CMD_LOW_ADDR);
+        printf("irr: %x\n", lapic_reg_read(LAPIC_REG_IRQ_REQUEST(vec / 32)));
+        printf("isr: %x\n", lapic_reg_read(LAPIC_REG_IN_SERVICE(vec / 32)));
+        printf("icr: %x\n", lapic_reg_read(LAPIC_REG_IRQ_CMD_LOW));
     } else if (!strcmp(argv[1].str, "self")) {
         if (argc < 3)
           goto notenoughargs;
         uint8_t vec = (uint8_t)argv[2].u;
         apic_send_self_ipi(vec, DELIVERY_MODE_FIXED);
-        printf("irr: %x\n", *IRQ_REQUEST_ADDR(vec / 32));
-        printf("isr: %x\n", *IN_SERVICE_ADDR(vec / 32));
-        printf("icr: %x\n", *IRQ_CMD_LOW_ADDR);
+        printf("irr: %x\n", lapic_reg_read(LAPIC_REG_IRQ_REQUEST(vec / 32)));
+        printf("isr: %x\n", lapic_reg_read(LAPIC_REG_IN_SERVICE(vec / 32)));
+        printf("icr: %x\n", lapic_reg_read(LAPIC_REG_IRQ_CMD_LOW));
     } else if (!strcmp(argv[1].str, "dump")) {
         if (argc < 3)
             goto notenoughargs;
@@ -408,15 +423,15 @@ void apic_local_debug(void)
     arch_interrupt_save(&state, 0);
 
     printf("apic %02x:\n", apic_local_id());
-    printf("  version: %08x:\n", *LAPIC_VERSION_ADDR);
-    printf("  logical_dst: %08x\n", *LOGICAL_DST_ADDR);
-    printf("  spurious_irq: %08x\n", *SPURIOUS_IRQ_ADDR);
-    printf("  tpr: %02x\n", (uint8_t)*TASK_PRIORITY_ADDR);
-    printf("  ppr: %02x\n", (uint8_t)*PROCESSOR_PRIORITY_ADDR);
+    printf("  version: %08x:\n", lapic_reg_read(LAPIC_REG_VERSION));
+    printf("  logical_dst: %08x\n", lapic_reg_read(LAPIC_REG_LOGICAL_DST));
+    printf("  spurious_irq: %08x\n", lapic_reg_read(LAPIC_REG_SPURIOUS_IRQ));
+    printf("  tpr: %02x\n", (uint8_t)lapic_reg_read(LAPIC_REG_TASK_PRIORITY));
+    printf("  ppr: %02x\n", (uint8_t)lapic_reg_read(LAPIC_REG_PROCESSOR_PRIORITY));
     for (int i = 0; i < 8; ++i)
-        printf("  irr %d: %08x\n", i, *IRQ_REQUEST_ADDR(i));
+        printf("  irr %d: %08x\n", i, lapic_reg_read(LAPIC_REG_IRQ_REQUEST(i)));
     for (int i = 0; i < 8; ++i)
-        printf("  isr %d: %08x\n", i, *IN_SERVICE_ADDR(i));
+        printf("  isr %d: %08x\n", i, lapic_reg_read(LAPIC_REG_IN_SERVICE(i)));
 
     arch_interrupt_restore(state, 0);
 }
