30c9910ab34c634fc2be15968cea4b0453dada6a
James Tucker <raggi@google.com>
2017-10-05 15:31:39 -0700

    grubdisk: fix GOPATH setup
    Change-Id: I87015ad1c93af75aba54d783b42dd580d6448756

--- grubdisk/install.sh
+++ grubdisk/install.sh
@@ -5,5 +5,5 @@
 
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/env.sh
 
-export GOPATH=$GOPATH:$FUCHSIA_ROOT/garnet/go
+export GOPATH=$GOPATH:$FUCHSIA_DIR/garnet/go
 exec go run "$FUCHSIA_GRUB_SCRIPTS_DIR/install.go" "$@"
