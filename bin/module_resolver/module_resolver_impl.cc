b7c18fcaaeca075206b062c862d127398ed943b3
Gabriel Schine <thatguy@google.com>
2017-10-12 14:49:07 -0700

    [resolver] Always return 'print_it' Module.
    This change takes whatever nouns are given in the Daisy and puts them
    onto the ModuleResolverResult verbatim. It sets the |module_id| of the
    result to the path to the "deployed" print_it Module.
    
    MW-289 #done
    
    Change-Id: I14b887839c60e546baed01ab3eb13f1851c4cbca

--- bin/module_resolver/module_resolver_impl.cc
+++ bin/module_resolver/module_resolver_impl.cc
@@ -9,15 +9,36 @@ namespace maxwell {
 ModuleResolverImpl::ModuleResolverImpl() = default;
 ModuleResolverImpl::~ModuleResolverImpl() = default;
 
-void ModuleResolverImpl::Connect(fidl::InterfaceRequest<modular::ModuleResolver> request) {
+void ModuleResolverImpl::Connect(
+    fidl::InterfaceRequest<modular::ModuleResolver> request) {
   bindings_.AddBinding(this, std::move(request));
 }
 
-void ModuleResolverImpl::FindModules(modular::DaisyPtr daisy,
-                   modular::ResolverScoringInfoPtr scoring_info,
-                   const FindModulesCallback& done) {
+void ModuleResolverImpl::FindModules(
+    modular::DaisyPtr daisy,
+    modular::ResolverScoringInfoPtr scoring_info,
+    const FindModulesCallback& done) {
   auto result = modular::FindModulesResult::New();
-  result->modules = fidl::Array<modular::ModuleResolverResultPtr>::New(0);
+  result->modules.resize(0);
+
+  auto print_it = modular::ModuleResolverResult::New();
+  print_it->module_id = "file:///system/bin/print_it_module";
+  print_it->local_name = "doesn't matter";
+  print_it->initial_nouns.mark_non_null();
+
+  for (auto entry : daisy->nouns) {
+    const auto& name = entry.GetKey();
+    const auto& noun = entry.GetValue();
+
+    // Ignore 'text' and 'entity_type' nouns for now.
+    // TODO(thatguy): Don't ignore these types.
+    if (noun->is_entity_reference()) {
+      print_it->initial_nouns[name] = noun->get_entity_reference().Clone();
+    }
+  }
+
+  result->modules.push_back(std::move(print_it));
+
   done(std::move(result));
 }
 
