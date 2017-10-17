716b5d8a3274882c9240d39139939ce48ad1f301
Aaron Green <aarongreen@google.com>
2017-10-17 13:34:25 -0700

    [uboringssl] Add crypto support for zxcrypt
    This CL expands the cryptographic routines supported by the Zricon port
    of BoringSSL to include those needed for zxcrypt.
    
    Change-Id: I5c870fe002ac0c0af03e2c35d67d696aabc8825d

--- third_party/ulib/uboringssl/rules.mk
+++ third_party/ulib/uboringssl/rules.mk
@@ -3,34 +3,54 @@
 # found in the LICENSE file.
 
 LOCAL_DIR := $(GET_LOCAL_DIR)
-SRC_DIR := $(LOCAL_DIR)/crypto
-ASM_DIR := $(LOCAL_DIR)/asm
 
 MODULE := $(LOCAL_DIR)
 
 MODULE_TYPE := userlib
 
+MODULE_COMPILEFLAGS += -fvisibility=hidden -DOPENSSL_NO_THREADS
+
 MODULE_SRCS := \
-    $(SRC_DIR)/cpu-aarch64-zircon.cpp \
+    $(LOCAL_DIR)/crypto/cpu-aarch64-zircon.cpp \
 
 MODULE_SRCS += \
-    $(SRC_DIR)/chacha/chacha.c \
-    $(SRC_DIR)/fipsmodule/sha/sha256.c \
-    $(SRC_DIR)/fipsmodule/sha/sha512.c \
-    $(SRC_DIR)/cpu-intel.c \
-    $(SRC_DIR)/crypto.c \
-    $(SRC_DIR)/mem.c \
+   $(LOCAL_DIR)/crypto/mem.c \
+   $(LOCAL_DIR)/crypto/err/err.c \
+   $(LOCAL_DIR)/crypto/err/err_data.c \
+   $(LOCAL_DIR)/crypto/cpu-intel.c \
+   $(LOCAL_DIR)/crypto/chacha/chacha.c \
+   $(LOCAL_DIR)/crypto/crypto.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/cipher/cipher.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/hmac/hmac.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/md4/md4.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/md5/md5.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/aes/aes.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/digest/digests.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/digest/digest.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/sha/sha512.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/sha/sha1.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/sha/sha256.c \
+   $(LOCAL_DIR)/crypto/hkdf/hkdf.c \
+   $(LOCAL_DIR)/crypto/thread_none.c \
+   $(LOCAL_DIR)/decrepit/xts/xts.c \
 
 ifeq ($(ARCH),arm64)
 MODULE_SRCS += \
-    $(ASM_DIR)/chacha-arm64.S \
-    $(ASM_DIR)/sha256-arm64.S \
-    $(ASM_DIR)/sha512-arm64.S
+    $(LOCAL_DIR)/asm/aes-arm64.S \
+    $(LOCAL_DIR)/asm/chacha-arm64.S \
+    $(LOCAL_DIR)/asm/sha1-arm64.S \
+    $(LOCAL_DIR)/asm/sha256-arm64.S \
+    $(LOCAL_DIR)/asm/sha512-arm64.S \
+
 else ifeq ($(ARCH),x86)
 MODULE_SRCS += \
-    $(ASM_DIR)/chacha-x86-64.S \
-    $(ASM_DIR)/sha256-x86-64.S \
-    $(ASM_DIR)/sha512-x86-64.S
+    $(LOCAL_DIR)/asm/aes-x86-64.S \
+    $(LOCAL_DIR)/asm/chacha-x86-64.S \
+    $(LOCAL_DIR)/asm/md5-x86-64.S \
+    $(LOCAL_DIR)/asm/sha1-x86-64.S \
+    $(LOCAL_DIR)/asm/sha256-x86-64.S \
+    $(LOCAL_DIR)/asm/sha512-x86-64.S\
+
 endif
 
 MODULE_NAME := uboringssl
--- third_party/ulib/uboringssl/scripts/perlasm.sh
+++ third_party/ulib/uboringssl/scripts/perlasm.sh
@@ -4,11 +4,28 @@ BASEDIR=$(dirname $0)
 ASM_DIR=${BASEDIR}/../asm
 SRC_DIR=${BASEDIR}/../crypto
 
+# ARM/aarch64
+
+## Ciphers
+perl ${SRC_DIR}/fipsmodule/aes/asm/aesv8-armx.pl linux64 ${ASM_DIR}/aes-arm64.S
 perl ${SRC_DIR}/chacha/asm/chacha-armv8.pl linux64 ${ASM_DIR}/chacha-arm64.S
+
+## Digests
+perl ${SRC_DIR}/fipsmodule/sha/asm/sha1-armv8.pl linux64 ${ASM_DIR}/sha1-arm64.S
+perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-armv8.pl linux64 ${ASM_DIR}/sha256-arm64.S
+perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-armv8.pl linux64 ${ASM_DIR}/sha512-arm64.S
+
+# Intel/x86-64
+
+## Ciphers
+perl ${SRC_DIR}/fipsmodule/aes/asm/aes-x86_64.pl ${ASM_DIR}/aes-x86-64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/aesni-x86_64.pl ${ASM_DIR}/aesni-x86-64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/bsaes-x86_64.pl ${ASM_DIR}/bsaes-x86-64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/vpaes-x86_64.pl ${ASM_DIR}/vpaes-x86-64.S
 perl ${SRC_DIR}/chacha/asm/chacha-x86_64.pl ${ASM_DIR}/chacha-x86-64.S
 
-perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-armv8.pl linux64 ${ASM_DIR}/sha256-arm64.S
+## Digests
+perl ${SRC_DIR}/fipsmodule/md5/asm/md5-x86_64.pl ${ASM_DIR}/md5-x86-64.S
+perl ${SRC_DIR}/fipsmodule/sha/asm/sha1-x86_64.pl ${ASM_DIR}/sha1-x86-64.S
 perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-x86_64.pl ${ASM_DIR}/sha256-x86-64.S
-
-perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-armv8.pl linux64 ${ASM_DIR}/sha512-arm64.S
 perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-x86_64.pl ${ASM_DIR}/sha512-x86-64.S
