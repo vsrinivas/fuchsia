049fb41bef23a9031e7ea4475ba5ad5ecbe44be0
Przemyslaw Pietrzkiewicz <ppi@google.com>
2017-10-18 15:05:36 +0200

    [ledger] add a factory of cloud provider for e2e tests and benchmarks
    The factory can be used to configure Ledger for end-to-end tests and
    benchmarks that use the real cloud provider.
    
    LE-318 #comment [ledger] add a factory of cloud provider for e2e tests and benchmarks
    
    Change-Id: I41a942dbc5c06799a79498975e811052ff3a6436

---
+++ bin/ledger/test/cloud_provider_firebase_factory.cc
@@ -0,0 +1,67 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "peridot/bin/ledger/test/cloud_provider_firebase_factory.h"
+
+#include <utility>
+
+#include "lib/app/cpp/connect.h"
+#include "lib/fsl/tasks/message_loop.h"
+#include "lib/fsl/threading/create_thread.h"
+#include "lib/fxl/functional/make_copyable.h"
+
+namespace test {
+namespace {
+constexpr char kCloudProviderFirebaseAppUrl[] =
+    "file:///system/apps/cloud_provider_firebase";
+}  // namespace
+
+CloudProviderFirebaseFactory::CloudProviderFirebaseFactory(
+    app::ApplicationContext* application_context)
+    : application_context_(application_context),
+      token_provider_("", "sync_user", "sync_user@example.com", "client_id") {}
+
+CloudProviderFirebaseFactory::~CloudProviderFirebaseFactory() {
+  services_task_runner_->PostTask(
+      [] { fsl::MessageLoop::GetCurrent()->PostQuitTask(); });
+  services_thread_.join();
+}
+
+void CloudProviderFirebaseFactory::Init() {
+  services_thread_ = fsl::CreateThread(&services_task_runner_);
+  app::ServiceProviderPtr child_services;
+  auto launch_info = app::ApplicationLaunchInfo::New();
+  launch_info->url = kCloudProviderFirebaseAppUrl;
+  launch_info->services = child_services.NewRequest();
+  application_context_->launcher()->CreateApplication(
+      std::move(launch_info), cloud_provider_controller_.NewRequest());
+  app::ConnectToService(child_services.get(),
+                        cloud_provider_factory_.NewRequest());
+}
+
+cloud_provider::CloudProviderPtr
+CloudProviderFirebaseFactory::MakeCloudProvider(std::string server_id,
+                                                std::string api_key) {
+  modular::auth::TokenProviderPtr token_provider;
+  services_task_runner_->PostTask(fxl::MakeCopyable(
+      [this, request = token_provider.NewRequest()]() mutable {
+        token_provider_.AddBinding(std::move(request));
+      }));
+
+  auto firebase_config = cloud_provider_firebase::Config::New();
+  firebase_config->server_id = server_id;
+  firebase_config->api_key = api_key;
+
+  cloud_provider::CloudProviderPtr cloud_provider;
+  cloud_provider_factory_->GetCloudProvider(
+      std::move(firebase_config), std::move(token_provider),
+      cloud_provider.NewRequest(), [](cloud_provider::Status status) {
+        if (status != cloud_provider::Status::OK) {
+          FXL_LOG(ERROR) << "Failed to create a cloud provider: " << status;
+        }
+      });
+  return cloud_provider;
+}
+
+}  // namespace test
---
+++ bin/ledger/test/cloud_provider_firebase_factory.h
@@ -0,0 +1,54 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PERIDOT_BIN_LEDGER_TEST_CLOUD_PROVIDER_FIREBASE_FACTORY_H_
+#define PERIDOT_BIN_LEDGER_TEST_CLOUD_PROVIDER_FIREBASE_FACTORY_H_
+
+#include <thread>
+
+#include "lib/app/cpp/application_context.h"
+#include "lib/cloud_provider/fidl/cloud_provider.fidl.h"
+#include "lib/fxl/memory/ref_ptr.h"
+#include "lib/fxl/tasks/task_runner.h"
+#include "peridot/bin/cloud_provider_firebase/fidl/factory.fidl.h"
+#include "peridot/bin/ledger/convert/convert.h"
+#include "peridot/bin/ledger/fidl_helpers/bound_interface_set.h"
+#include "peridot/bin/ledger/test/fake_token_provider.h"
+
+namespace test {
+
+// Manager for real cloud provider backed by fake token provider.
+//
+// This is used to configure Ledger for end-to-end tests and benchmarks that use
+// the real cloud provider.
+class CloudProviderFirebaseFactory {
+ public:
+  CloudProviderFirebaseFactory(app::ApplicationContext* application_context);
+  ~CloudProviderFirebaseFactory();
+
+  void Init();
+
+  cloud_provider::CloudProviderPtr MakeCloudProvider(std::string server_id,
+                                                     std::string api_key);
+
+ private:
+  app::ApplicationContext* application_context_;
+
+  // Thread used to run the fake token manager on.
+  std::thread services_thread_;
+  fxl::RefPtr<fxl::TaskRunner> services_task_runner_;
+
+  ledger::fidl_helpers::BoundInterfaceSet<modular::auth::TokenProvider,
+                                          FakeTokenProvider>
+      token_provider_;
+
+  app::ApplicationControllerPtr cloud_provider_controller_;
+  cloud_provider_firebase::FactoryPtr cloud_provider_factory_;
+
+  FXL_DISALLOW_COPY_AND_ASSIGN(CloudProviderFirebaseFactory);
+};
+
+}  // namespace test
+
+#endif  // PERIDOT_BIN_LEDGER_TEST_CLOUD_PROVIDER_FIREBASE_FACTORY_H_
