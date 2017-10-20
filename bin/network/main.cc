9c09b2af984a600021680ee369f8fa3b3f8c3bd8
Toshi Kikuchi <toshik@google.com>
2017-10-20 11:51:14 -0700

    [network] Call fxl::SetLogSettingsFromCommandLine()
    The current code is using FXL_VLOG(1) everywhere but it has no easy
    way to enable them. This patch allows enabling them by passing --verbose
    option from the command line (e.g. in bootstrap/services.config).
    
    Change-Id: I39c93069f62cc31c859008b24b045b7ec8852ed1

--- bin/network/main.cc
+++ bin/network/main.cc
@@ -2,10 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "lib/fsl/tasks/message_loop.h"
 #include "garnet/bin/network/network_service_delegate.h"
+#include "lib/fsl/tasks/message_loop.h"
+#include "lib/fxl/command_line.h"
+#include "lib/fxl/log_settings_command_line.h"
 
 int main(int argc, const char** argv) {
+  auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
+  if (!fxl::SetLogSettingsFromCommandLine(command_line))
+    return 1;
+
   fsl::MessageLoop loop;
   network::NetworkServiceDelegate delegate;
   loop.Run();
