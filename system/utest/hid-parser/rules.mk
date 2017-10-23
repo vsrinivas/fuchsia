b1f7d54fa4d0eab09b9253924c748c536c2f7c68
Carlos Pizano <cpu@chromium.org>
2017-10-22 17:34:10 -0700

    [system][hid] beginnings of a hid parser
    This is based in Tim K's code. This CL introduces the
    basic hid report item parser with a minimal testing
    harness.
    
    Change-Id: I42bf09ea83622b369e3f5ea16ed0ca5cefbb97c7

---
+++ system/utest/hid-parser/rules.mk
@@ -0,0 +1,22 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := usertest
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/hid-parser-test.cpp \
+
+MODULE_NAME := hidparse-test
+
+MODULE_STATIC_LIBS := \
+    system/ulib/hid-parser
+
+MODULE_LIBS := \
+    system/ulib/fdio system/ulib/unittest system/ulib/c
+
+include make/module.mk
