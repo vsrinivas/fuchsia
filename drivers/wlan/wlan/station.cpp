a807a0104ef20bf1154692b911b6fd6c5d9360e8
Jiwoong Lee <porce@google.com>
2017-10-24 15:55:25 -0700

    [driver][wlan] Specify HT capabilities in AssocReq explicitly
    802.11n effort.
    Each AP demands different HT capability configurations.
    This explicit specification assists ongoing experiments, and clarifies
    the differences in each test setup.
    Still this is a stop gap until the device capabilities can be queried.
    
    NET-232
    
    Change-Id: Ifcd0e4135235f632d9960e095b7889103dd8140e

--- drivers/wlan/wlan/station.cpp
+++ drivers/wlan/wlan/station.cpp
@@ -1005,6 +1005,7 @@ bool Station::IsHTReady() const {
     // return bss_is_ht_capable && client_is_ht_capable && client_is_ht_config;
 
     // TODO(porce): GoogleGuest-Legacy fails to offer DHCP upon
+    // return true;
     return false;
 }
 
@@ -1014,20 +1015,71 @@ HtCapabilities Station::BuildHtCapabilities() const {
     // - Client radio capabilities
     // - Client configuration
 
-    HtCapabilities htc;
-
     // Static cooking for Proof-of-Concept
-    htc.ht_cap_info.set_ldpc_coding_cap(1);
-    htc.ht_cap_info.set_sm_power_save(HtCapabilityInfo::DISABLED);
-    htc.ht_cap_info.set_short_gi_20(1);
-    htc.ht_cap_info.set_tx_stbc(1);
-    htc.ht_cap_info.set_rx_stbc(1);
+    HtCapabilities htc;
+    HtCapabilityInfo& hci = htc.ht_cap_info;
 
-    htc.ampdu_params.set_exponent(3);  // 65535 bytes
-    htc.ampdu_params.set_min_start_spacing(AmpduParams::SIXTEEN_USEC);
-    // htc.ampdu_params.set_min_start_spacing(AmpduParams::EIGHT_USEC);
+    hci.set_ldpc_coding_cap(1);
+    hci.set_chan_width_set(HtCapabilityInfo::TWENTY_ONLY);
+    // hci.set_chan_width_set(HtCapabilityInfo::TWENTY_FORTY);
+    hci.set_sm_power_save(HtCapabilityInfo::DISABLED);
+    hci.set_greenfield(0);
+    hci.set_short_gi_20(1);
+    hci.set_short_gi_40(0);
+    hci.set_tx_stbc(1);
+    hci.set_rx_stbc(1);  // one stream.
+    hci.set_delayed_block_ack(0);
+    hci.set_max_amsdu_len(HtCapabilityInfo::OCTETS_7935);  // Aruba
+    // hci.set_max_amsdu_len(HtCapabilityInfo::OCTETS_3839);  // TP-Link
+    hci.set_dsss_in_40(0);
+    hci.set_intolerant_40(0);
+    hci.set_lsig_txop_protect(0);
 
-    htc.mcs_set.rx_mcs_head.set_bitmask(0xffff);  // MCS 0-15
+    AmpduParams& ampdu = htc.ampdu_params;
+    ampdu.set_exponent(3);                                // 65535 bytes
+    ampdu.set_min_start_spacing(AmpduParams::FOUR_USEC);  // Aruba
+    // ampdu.set_min_start_spacing(AmpduParams::EIGHT_USEC);  // TP-Link
+    // ampdu.set_min_start_spacing(AmpduParams::SIXTEEN_USEC);
+
+    SupportedMcsSet& mcs = htc.mcs_set;
+    mcs.rx_mcs_head.set_bitmask(0xff);  // MCS 0-7
+    // mcs.rx_mcs_head.set_bitmask(0xffff);  // MCS 0-15
+
+    HtExtCapabilities& hec = htc.ht_ext_cap;
+    hec.set_pco(0);
+    hec.set_pco_transition(HtExtCapabilities::PCO_RESERVED);
+    hec.set_mcs_feedback(HtExtCapabilities::MCS_NOFEEDBACK);
+    hec.set_htc_ht_support(0);
+    hec.set_rd_responder(0);
+
+    TxBfCapability& txbf = htc.txbf_cap;
+    txbf.set_implicit_rx(0);
+    txbf.set_rx_stag_sounding(0);
+    txbf.set_tx_stag_sounding(0);
+    txbf.set_rx_ndp(0);
+    txbf.set_tx_ndp(0);
+    txbf.set_implicit(0);
+    txbf.set_calibration(TxBfCapability::CALIBRATION_NONE);
+    txbf.set_csi(0);
+    txbf.set_noncomp_steering(0);
+    txbf.set_comp_steering(0);
+    txbf.set_csi_feedback(TxBfCapability::FEEDBACK_NONE);
+    txbf.set_noncomp_feedback(TxBfCapability::FEEDBACK_NONE);
+    txbf.set_comp_feedback(TxBfCapability::FEEDBACK_NONE);
+    txbf.set_min_grouping(TxBfCapability::MIN_GROUP_ONE);
+    txbf.set_csi_antennas_human(1);           // 1 antenna
+    txbf.set_noncomp_steering_ants_human(1);  // 1 antenna
+    txbf.set_comp_steering_ants_human(1);     // 1 antenna
+    txbf.set_csi_rows_human(1);               // 1 antenna
+    txbf.set_chan_estimation_human(1);        // # space-time stream
+
+    AselCapability& asel = htc.asel_cap;
+    asel.set_asel(0);
+    asel.set_csi_feedback_tx_asel(0);
+    asel.set_explicit_csi_feedback(0);
+    asel.set_antenna_idx_feedback(0);
+    asel.set_rx_asel(0);
+    asel.set_tx_sounding_ppdu(0);
 
     return htc;  // 28 bytes.
 }
