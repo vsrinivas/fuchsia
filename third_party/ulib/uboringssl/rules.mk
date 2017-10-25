4a0340f53684e6d58d81f991ca37ff592941132f
Aaron Green <aarongreen@google.com>
2017-10-25 15:21:49 -0700

    [uboringssl] Add crypto support for AEADs
    This CL expands the cryptographic routines supported by the Zircon port
    of BoringSSL to include those needed for AEADs.
    
    Change-Id: I34fe0122dbb9b8070184c5d98f706b23cf3586d5

--- third_party/ulib/uboringssl/rules.mk
+++ third_party/ulib/uboringssl/rules.mk
@@ -19,18 +19,32 @@ MODULE_SRCS += \
    $(LOCAL_DIR)/crypto/err/err_data.c \
    $(LOCAL_DIR)/crypto/cpu-intel.c \
    $(LOCAL_DIR)/crypto/chacha/chacha.c \
+   $(LOCAL_DIR)/crypto/cipher_extra/e_aesgcmsiv.c \
    $(LOCAL_DIR)/crypto/crypto.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/aes/aes.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/aes/mode_wrappers.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/cipher/aead.c \
    $(LOCAL_DIR)/crypto/fipsmodule/cipher/cipher.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/cipher/e_aes.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/digest/digests.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/digest/digest.c \
    $(LOCAL_DIR)/crypto/fipsmodule/hmac/hmac.c \
    $(LOCAL_DIR)/crypto/fipsmodule/md4/md4.c \
    $(LOCAL_DIR)/crypto/fipsmodule/md5/md5.c \
-   $(LOCAL_DIR)/crypto/fipsmodule/aes/aes.c \
-   $(LOCAL_DIR)/crypto/fipsmodule/digest/digests.c \
-   $(LOCAL_DIR)/crypto/fipsmodule/digest/digest.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/modes/cbc.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/modes/cfb.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/modes/ctr.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/modes/gcm.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/modes/ofb.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/modes/polyval.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/rand/ctrdrbg.c \
+   $(LOCAL_DIR)/crypto/fipsmodule/rand/rand.c \
    $(LOCAL_DIR)/crypto/fipsmodule/sha/sha512.c \
    $(LOCAL_DIR)/crypto/fipsmodule/sha/sha1.c \
    $(LOCAL_DIR)/crypto/fipsmodule/sha/sha256.c \
    $(LOCAL_DIR)/crypto/hkdf/hkdf.c \
+   $(LOCAL_DIR)/crypto/rand_extra/forkunsafe.c \
+   $(LOCAL_DIR)/crypto/rand_extra/fuchsia.c \
    $(LOCAL_DIR)/crypto/thread_none.c \
    $(LOCAL_DIR)/decrepit/xts/xts.c \
 
@@ -42,6 +56,7 @@ MODULE_COMPILEFLAGS += -DOPENSSL_NO_ASM
 MODULE_SRCS += \
     $(LOCAL_DIR)/asm/aes-arm64.S \
     $(LOCAL_DIR)/asm/chacha-arm64.S \
+    ${LOCAL_DIR}/asm/ghash-arm64.S \
     $(LOCAL_DIR)/asm/sha1-arm64.S \
     $(LOCAL_DIR)/asm/sha256-arm64.S \
     $(LOCAL_DIR)/asm/sha512-arm64.S \
@@ -49,11 +64,18 @@ MODULE_SRCS += \
 else ifeq ($(ARCH),x86)
 MODULE_SRCS += \
     $(LOCAL_DIR)/asm/aes-x86-64.S \
+    $(LOCAL_DIR)/asm/aes128gcmsiv-x86-64.S \
+    ${LOCAL_DIR}/asm/aesni-x86-64.S \
+    ${LOCAL_DIR}/asm/aesnigcm-x86-64.S \
+    ${LOCAL_DIR}/asm/bsaes-x86-64.S \
     $(LOCAL_DIR)/asm/chacha-x86-64.S \
+    ${LOCAL_DIR}/asm/ghash-x86-64.S \
     $(LOCAL_DIR)/asm/md5-x86-64.S \
+    ${LOCAL_DIR}/asm/rdrand-x86-64.S \
     $(LOCAL_DIR)/asm/sha1-x86-64.S \
     $(LOCAL_DIR)/asm/sha256-x86-64.S \
     $(LOCAL_DIR)/asm/sha512-x86-64.S\
+    ${LOCAL_DIR}/asm/vpaes-x86-64.S \
 
 endif
 
--- third_party/ulib/uboringssl/scripts/perlasm.sh
+++ third_party/ulib/uboringssl/scripts/perlasm.sh
@@ -7,8 +7,9 @@ SRC_DIR=${BASEDIR}/../crypto
 # ARM/aarch64
 
 ## Ciphers
-perl ${SRC_DIR}/fipsmodule/aes/asm/aesv8-armx.pl linux64 ${ASM_DIR}/aes-arm64.S
 perl ${SRC_DIR}/chacha/asm/chacha-armv8.pl linux64 ${ASM_DIR}/chacha-arm64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/aesv8-armx.pl linux64 ${ASM_DIR}/aes-arm64.S
+perl ${SRC_DIR}/fipsmodule/modes/asm/ghashv8-armx.pl linux64 ${ASM_DIR}/ghash-arm64.S
 
 ## Digests
 perl ${SRC_DIR}/fipsmodule/sha/asm/sha1-armv8.pl linux64 ${ASM_DIR}/sha1-arm64.S
@@ -18,14 +19,20 @@ perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-armv8.pl linux64 ${ASM_DIR}/sha512-arm
 # Intel/x86-64
 
 ## Ciphers
-perl ${SRC_DIR}/fipsmodule/aes/asm/aes-x86_64.pl ${ASM_DIR}/aes-x86-64.S
-perl ${SRC_DIR}/fipsmodule/aes/asm/aesni-x86_64.pl ${ASM_DIR}/aesni-x86-64.S
-perl ${SRC_DIR}/fipsmodule/aes/asm/bsaes-x86_64.pl ${ASM_DIR}/bsaes-x86-64.S
-perl ${SRC_DIR}/fipsmodule/aes/asm/vpaes-x86_64.pl ${ASM_DIR}/vpaes-x86-64.S
-perl ${SRC_DIR}/chacha/asm/chacha-x86_64.pl ${ASM_DIR}/chacha-x86-64.S
+perl ${SRC_DIR}/chacha/asm/chacha-x86_64.pl '' ${ASM_DIR}/chacha-x86-64.S
+perl ${SRC_DIR}/cipher_extra/asm/aes128gcmsiv-x86_64.pl '' ${ASM_DIR}/aes128gcmsiv-x86-64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/aes-x86_64.pl '' ${ASM_DIR}/aes-x86-64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/aesni-x86_64.pl '' ${ASM_DIR}/aesni-x86-64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/bsaes-x86_64.pl '' ${ASM_DIR}/bsaes-x86-64.S
+perl ${SRC_DIR}/fipsmodule/aes/asm/vpaes-x86_64.pl '' ${ASM_DIR}/vpaes-x86-64.S
+perl ${SRC_DIR}/fipsmodule/modes/asm/aesni-gcm-x86_64.pl linux64 ${ASM_DIR}/aesnigcm-x86-64.S
+perl ${SRC_DIR}/fipsmodule/modes/asm/ghash-x86_64.pl linux64 ${ASM_DIR}/ghash-x86-64.S
 
 ## Digests
-perl ${SRC_DIR}/fipsmodule/md5/asm/md5-x86_64.pl ${ASM_DIR}/md5-x86-64.S
-perl ${SRC_DIR}/fipsmodule/sha/asm/sha1-x86_64.pl ${ASM_DIR}/sha1-x86-64.S
-perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-x86_64.pl ${ASM_DIR}/sha256-x86-64.S
-perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-x86_64.pl ${ASM_DIR}/sha512-x86-64.S
+perl ${SRC_DIR}/fipsmodule/md5/asm/md5-x86_64.pl '' ${ASM_DIR}/md5-x86-64.S
+perl ${SRC_DIR}/fipsmodule/sha/asm/sha1-x86_64.pl '' ${ASM_DIR}/sha1-x86-64.S
+perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-x86_64.pl '' ${ASM_DIR}/sha256-x86-64.S
+perl ${SRC_DIR}/fipsmodule/sha/asm/sha512-x86_64.pl '' ${ASM_DIR}/sha512-x86-64.S
+
+## RNGs
+perl ${SRC_DIR}/fipsmodule/rand/asm/rdrand-x86_64.pl '' ${ASM_DIR}/rdrand-x86-64.S
