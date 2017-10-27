06382823f3c71b0f7418273b4befa64b96ca9afd
Adam Barth <abarth@chromium.org>
2017-10-27 15:57:38 -0700

    [env.sh] Move zset back to env.sh
    It turns out we don't need zset in fx after all.
    
    Change-Id: I8942dacf0ff13a292b706d96b7c3071ca4864a26

--- devshell/lib/env.sh
+++ devshell/lib/env.sh
@@ -29,68 +29,6 @@ function __patched_path {
     echo "${stripped}${new_component}"
 }
 
-### zset: set zircon build properties
-
-function zset-usage {
-  cat >&2 <<END
-Usage: zset x86-64|arm64|rpi3|odroidc2|hikey960
-Sets zircon build options.
-END
-}
-
-function zset {
-  if [[ $# -ne 1 ]]; then
-    zset-usage
-    return 1
-  fi
-
-  local settings="$*"
-
-  case $1 in
-    x86-64)
-      export ZIRCON_PROJECT=zircon-pc-x86-64
-      export ZIRCON_ARCH=x86-64
-      export ZIRCON_BUILD_TARGET=x86_64
-      ;;
-    arm64)
-      export ZIRCON_PROJECT=zircon-qemu-arm64
-      export ZIRCON_ARCH=arm64
-      export ZIRCON_BUILD_TARGET=aarch64
-      ;;
-    rpi3)
-      export ZIRCON_PROJECT=zircon-rpi3-arm64
-      export ZIRCON_ARCH=arm64
-      export ZIRCON_BUILD_TARGET=rpi3
-      ;;
-    odroidc2)
-      export ZIRCON_PROJECT=zircon-odroidc2-arm64
-      export ZIRCON_ARCH=arm64
-      export ZIRCON_BUILD_TARGET=odroidc2
-      ;;
-    hikey960)
-      export ZIRCON_PROJECT=zircon-hikey960-arm64
-      export ZIRCON_ARCH=arm64
-      export ZIRCON_BUILD_TARGET=hikey960
-      ;;
-    *)
-      zset-usage
-      return 1
-  esac
-
-  export ZIRCON_BUILD_ROOT="${FUCHSIA_OUT_DIR}/build-zircon"
-  export ZIRCON_BUILD_DIR="${ZIRCON_BUILD_ROOT}/build-${ZIRCON_PROJECT}"
-  export ZIRCON_TOOLS_DIR="${ZIRCON_BUILD_ROOT}/tools"
-  export ZIRCON_BUILD_REV_CACHE="${ZIRCON_BUILD_DIR}/build.rev";
-  export ZIRCON_SETTINGS="${settings}"
-  export ENVPROMPT_INFO="${ZIRCON_ARCH}"
-
-  # add tools to path, removing prior tools directory if any
-  export PATH="$(__patched_path \
-      "${FUCHSIA_DIR}/zircon/build-[^/]*/tools" \
-      "${ZIRCON_TOOLS_DIR}"
-  )"
-}
-
 ### fupdate-path: add useful tools to the PATH
 
 # Add tools to path, removing prior tools directory if any. This also
