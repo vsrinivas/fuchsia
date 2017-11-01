857b3a695ed299d32acb3b8deb63bf4ab589b03e
Travis Geiselbrecht <travisg@google.com>
2017-10-31 22:07:06 -0700

    [kernel][arm64] fix bug in MP_IPI_TARGET_ALL_BUT_LOCAL
    Bad test in an if statement, move to a switch statement so the compiler
    will catch stuff liek this in the future.
    
    Change-Id: Iadc7264c100f67d73970f5279bee6c6644083078

--- kernel/arch/arm64/mp.cpp
+++ kernel/arch/arm64/mp.cpp
@@ -69,11 +69,16 @@ zx_status_t arch_mp_send_ipi(mp_ipi_target_t target, cpu_mask_t mask, mp_ipi_t i
     LTRACEF("target %d mask %#x, ipi %d\n", target, mask, ipi);
 
     // translate the high level target + mask mechanism into just a mask
-    if (target == MP_IPI_TARGET_ALL) {
-        mask = (1ul << SMP_MAX_CPUS) - 1;
-    } else if (target == MP_IPI_TARGET_ALL) {
-        mask = (1ul << SMP_MAX_CPUS) - 1;
-        mask &= ~cpu_num_to_mask(arch_curr_cpu_num());
+    switch (target) {
+        case MP_IPI_TARGET_ALL:
+            mask = (1ul << SMP_MAX_CPUS) - 1;
+            break;
+        case MP_IPI_TARGET_ALL_BUT_LOCAL:
+            mask = (1ul << SMP_MAX_CPUS) - 1;
+            mask &= ~cpu_num_to_mask(arch_curr_cpu_num());
+            break;
+        case MP_IPI_TARGET_MASK:
+            ;
     }
 
     return interrupt_send_ipi(mask, ipi);
