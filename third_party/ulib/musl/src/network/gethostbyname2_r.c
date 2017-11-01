4161b4be74acbce32d222ded4d747d93e317efec
Toshi Kikuchi <toshik@google.com>
2017-10-31 21:21:42 -0700

    [musl] rewrite gethostbyname2_r to use getaddrinfo
    __lookup_name is not working on fuchsia currenly.
    Rewrite gethostbyname2_r to use getaddrinfo, which is implemented
    in fdio.
    
    Change-Id: Idd5e64097ab15d2959802730bf5eaa06e4903d4b

--- third_party/ulib/musl/src/network/gethostbyname2_r.c
+++ third_party/ulib/musl/src/network/gethostbyname2_r.c
@@ -1,83 +1,86 @@
-#define _GNU_SOURCE
-
-#include "lookup.h"
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <stdint.h>
 #include <string.h>
-#include <sys/socket.h>
+
+static int roundup(int x, int m) {
+    return (((x + m - 1)) & ~(m - 1));
+}
 
 int gethostbyname2_r(const char* name, int af, struct hostent* h, char* buf, size_t buflen,
                      struct hostent** res, int* err) {
-    struct address addrs[MAXADDRS];
-    char canon[256];
-    int i, cnt;
-    size_t align, need;
+    *err = 0;
+    *res = NULL;
 
-    *res = 0;
-    cnt = __lookup_name(addrs, canon, name, af, AI_CANONNAME);
-    if (cnt < 0)
-        switch (cnt) {
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = af;
+    hints.ai_socktype = SOCK_STREAM;
+
+    struct addrinfo *addrinfo;
+    int eai = getaddrinfo(name, NULL, &hints, &addrinfo);
+    if (eai != 0) {
+        switch (eai) {
         case EAI_NONAME:
             *err = HOST_NOT_FOUND;
-            return ENOENT;
+            break;
         case EAI_AGAIN:
             *err = TRY_AGAIN;
-            return EAGAIN;
+            break;
         default:
-        case EAI_FAIL:
             *err = NO_RECOVERY;
-            return EBADMSG;
-        case EAI_MEMORY:
-        case EAI_SYSTEM:
-            *err = NO_RECOVERY;
-            return errno;
-        case 0:
             break;
         }
+        return 0;
+    }
 
     h->h_addrtype = af;
-    h->h_length = af == AF_INET6 ? 16 : 4;
+    h->h_length = (af == AF_INET) ? 4 : 16;
 
-    /* Align buffer */
-    align = -(uintptr_t)buf & (sizeof(char*) - 1);
+    int namelen = strlen(name);
 
-    need = 4 * sizeof(char*);
-    need += (cnt + 1) * (sizeof(char*) + h->h_length);
-    need += strlen(name) + 1;
-    need += strlen(canon) + 1;
-    need += align;
-
-    if (need > buflen)
-        return ERANGE;
-
-    buf += align;
-    h->h_aliases = (void*)buf;
-    buf += 3 * sizeof(char*);
-    h->h_addr_list = (void*)buf;
-    buf += (cnt + 1) * sizeof(char*);
-
-    h->h_name = h->h_aliases[0] = buf;
-    strcpy(h->h_name, canon);
-    buf += strlen(h->h_name) + 1;
-
-    if (strcmp(h->h_name, name)) {
-        h->h_aliases[1] = buf;
-        strcpy(h->h_aliases[1], name);
-        buf += strlen(h->h_aliases[1]) + 1;
-    } else
-        h->h_aliases[1] = 0;
-
-    h->h_aliases[2] = 0;
-
-    for (i = 0; i < cnt; i++) {
-        h->h_addr_list[i] = (void*)buf;
-        buf += h->h_length;
-        memcpy(h->h_addr_list[i], addrs[i].addr, h->h_length);
+    int n_addr = 0;
+    for (struct addrinfo *ap = addrinfo; ap != NULL; ap = ap->ai_next) {
+        ++n_addr;
     }
-    h->h_addr_list[i] = 0;
 
+    size_t need = roundup(namelen + 1, sizeof(char*)); // h_name
+    need += sizeof(char*);                             // h_aliases
+    need += sizeof(char*) * (n_addr + 1);              // h_addr_list
+    need += h->h_length * n_addr;                      // addrs that h_addr_list points to
+
+    if (need > buflen) {
+        freeaddrinfo(addrinfo);
+        *err = -1;
+        return ERANGE;
+    }
+
+    memcpy(buf, name, namelen + 1);
+    h->h_name = buf;
+    buf += roundup(namelen + 1, sizeof(char*));
+
+    h->h_aliases = (char**)buf;
+    h->h_aliases[0] = NULL;
+    buf += sizeof(char*);
+
+    h->h_addr_list = (char**)buf;
+    buf += sizeof(char*) * (n_addr + 1);
+
+    n_addr = 0;
+    for (struct addrinfo *ap = addrinfo; ap != NULL; ap = ap->ai_next) {
+        if (af == AF_INET) {
+            struct sockaddr_in* sin = (struct sockaddr_in*)ap->ai_addr;
+            memcpy(buf, &sin->sin_addr, h->h_length);
+        } else if (af == AF_INET6) {
+            struct sockaddr_in6* sin6 = (struct sockaddr_in6*)ap->ai_addr;
+            memcpy(buf, &sin6->sin6_addr, h->h_length);
+        }
+        h->h_addr_list[n_addr++] = buf;
+        buf += h->h_length;
+    }
+    h->h_addr_list[n_addr] = NULL;
+
+    freeaddrinfo(addrinfo);
     *res = h;
     return 0;
 }
