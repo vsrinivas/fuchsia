7900d664f875be06d1b1266ad39a87a13fce038f
Travis Geiselbrecht <travisg@google.com>
2017-10-31 20:57:30 -0700

    [kernel][arm64] spiff up the sysreg command a bit
    -Match against lower case
    -Add a few more registers
    
    Change-Id: I03212aa72b51c534a780a5e5913c4c338541fd2e

--- kernel/arch/arm64/sysreg.cpp
+++ kernel/arch/arm64/sysreg.cpp
@@ -16,43 +16,44 @@
 #include <lib/console.h>
 
 #define SYSREG_READ_COMMAND(sysreg_string) \
-    if (!strcmp(regname,#sysreg_string)){          \
+    if (!strncasecmp(regname,#sysreg_string, sizeof(#sysreg_string))){          \
         printf(#sysreg_string " = %016lx\n", ARM64_READ_SYSREG(sysreg_string));   \
         return 0; \
-    }
-static uint64_t read_sysregs(const char *regname) {
+    } else
 
-    SYSREG_READ_COMMAND(MPIDR_EL1)
+static uint64_t read_sysregs(const char *regname) {
     SYSREG_READ_COMMAND(ACTLR_EL1)
     SYSREG_READ_COMMAND(CCSIDR_EL1)
     SYSREG_READ_COMMAND(CLIDR_EL1)
     SYSREG_READ_COMMAND(CSSELR_EL1)
     SYSREG_READ_COMMAND(MIDR_EL1)
+    SYSREG_READ_COMMAND(MPIDR_EL1)
     SYSREG_READ_COMMAND(SCTLR_EL1)
     SYSREG_READ_COMMAND(SPSR_EL1)
     SYSREG_READ_COMMAND(TCR_EL1)
-    SYSREG_READ_COMMAND(TPIDR_EL1)
     SYSREG_READ_COMMAND(TPIDRRO_EL0)
-    SYSREG_READ_COMMAND(VBAR_EL1)
+    SYSREG_READ_COMMAND(TPIDR_EL1)
     SYSREG_READ_COMMAND(TTBR0_EL1)
     SYSREG_READ_COMMAND(TTBR1_EL1)
+    SYSREG_READ_COMMAND(VBAR_EL1)
 
 //Generic Timer regs
     SYSREG_READ_COMMAND(CNTFRQ_EL0)
     SYSREG_READ_COMMAND(CNTKCTL_EL1)
-    SYSREG_READ_COMMAND(CNTP_CTL_EL0)
-    SYSREG_READ_COMMAND(CNTP_CVAL_EL0)
-    SYSREG_READ_COMMAND(CNTP_TVAL_EL0)
     SYSREG_READ_COMMAND(CNTPCT_EL0)
     SYSREG_READ_COMMAND(CNTPS_CTL_EL1)
     SYSREG_READ_COMMAND(CNTPS_CVAL_EL1)
     SYSREG_READ_COMMAND(CNTPS_TVAL_EL1)
+    SYSREG_READ_COMMAND(CNTP_CTL_EL0)
+    SYSREG_READ_COMMAND(CNTP_CVAL_EL0)
+    SYSREG_READ_COMMAND(CNTP_TVAL_EL0)
+    SYSREG_READ_COMMAND(CNTVCT_EL0)
     SYSREG_READ_COMMAND(CNTV_CTL_EL0)
     SYSREG_READ_COMMAND(CNTV_CVAL_EL0)
     SYSREG_READ_COMMAND(CNTV_TVAL_EL0)
-    SYSREG_READ_COMMAND(CNTVCT_EL0)
-
-    printf("Could not find register %s in list (you may need to add it to kernel/kernel/sysreg.c)\n",regname);
+    {
+        printf("Could not find register %s in list (you may need to add it to kernel/kernel/sysreg.c)\n",regname);
+    }
     return 0;
 }
 
