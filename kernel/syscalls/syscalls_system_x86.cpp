2bdba62c8117495508ecfc1db6574d5e8889f6eb
Todd Eisenberger <teisenbe@google.com>
2017-11-02 12:05:47 -0700

    [syscalls][powerctl] Finish implementing S-state transition
    This should be enough to let us resume execution.  We still have a lot
    of work around bringing devices back.
    
    ZX-1184
    
    Change-Id: Ie309cf091db76fdbfe3bf0ab14d30a4fef344bf4

--- kernel/syscalls/syscalls_system_x86.cpp
+++ kernel/syscalls/syscalls_system_x86.cpp
@@ -8,9 +8,15 @@
 
 #include <arch/arch_ops.h>
 #include <arch/mp.h>
+#include <fbl/auto_call.h>
 #include <inttypes.h>
+#include <kernel/timer.h>
+#include <platform.h>
 #include <trace.h>
+#include <vm/vm_aspace.h>
 
+#include <arch/x86/bootstrap16.h>
+#include <arch/x86/acpi.h>
 extern "C" {
 #include <acpica/acpi.h>
 #include <acpica/accommon.h>
@@ -18,6 +24,83 @@ extern "C" {
 }
 
 #define LOCAL_TRACE 0
+
+namespace {
+
+// This thread performs the work for suspend/resume.  We use a separate thread
+// rather than the invoking thread to let us lean on the context switch code
+// path to persist all of the usermode thread state that is not saved on a plain
+// mode switch.
+zx_status_t suspend_thread(void* raw_arg) {
+    auto arg = reinterpret_cast<const zx_system_powerctl_arg_t*>(raw_arg);
+    uint8_t target_s_state = arg->acpi_transition_s_state.target_s_state;
+    uint8_t sleep_type_a = arg->acpi_transition_s_state.sleep_type_a;
+    uint8_t sleep_type_b = arg->acpi_transition_s_state.sleep_type_b;
+
+    // Acquire resources for suspend and resume if necessary.
+    fbl::RefPtr<VmAspace> temp_aspace;
+    x86_realmode_entry_data* bootstrap_data;
+    struct x86_realmode_entry_data_registers regs;
+    paddr_t bootstrap_ip;
+    zx_status_t status;
+    status = x86_bootstrap16_acquire(reinterpret_cast<uintptr_t>(_x86_suspend_wakeup),
+                                     &temp_aspace,
+                                     reinterpret_cast<void**>(&bootstrap_data),
+                                     &bootstrap_ip);
+    if (status != ZX_OK) {
+        return status;
+    }
+    auto bootstrap_cleanup = fbl::MakeAutoCall([&bootstrap_data]() {
+        x86_bootstrap16_release(bootstrap_data);
+    });
+
+    // Setup our resume path
+    ACPI_TABLE_FACS* facs = nullptr;
+    ACPI_STATUS acpi_status = AcpiGetTable((char *)ACPI_SIG_FACS, 1,
+                                           reinterpret_cast<ACPI_TABLE_HEADER**>(&facs));
+    if (acpi_status != AE_OK) {
+        return ZX_ERR_BAD_STATE;
+    }
+    acpi_status = AcpiHwSetFirmwareWakingVector(facs, bootstrap_ip, 0);
+    if (acpi_status != AE_OK) {
+        return ZX_ERR_BAD_STATE;
+    }
+    auto wake_vector_cleanup = fbl::MakeAutoCall([facs]() {
+        AcpiHwSetFirmwareWakingVector(facs, 0, 0);
+    });
+
+    bootstrap_data->registers_ptr = reinterpret_cast<uintptr_t>(&regs);
+
+    arch_disable_ints();
+
+    // Save system state.
+    platform_suspend();
+    arch_suspend();
+
+    // Do the actual suspend
+    TRACEF("Entering x86_acpi_transition_s_state\n");
+    acpi_status = x86_acpi_transition_s_state(&regs, target_s_state,
+                                              sleep_type_a, sleep_type_b);
+    if (acpi_status != AE_OK) {
+        arch_enable_ints();
+        TRACEF("x86_acpi_transition_s_state failed: %x\n", acpi_status);
+        return ZX_ERR_INTERNAL;
+    }
+    TRACEF("Left x86_acpi_transition_s_state\n");
+
+    // If we're here, we've resumed and need to restore our CPU context
+    DEBUG_ASSERT(arch_ints_disabled());
+
+    arch_resume();
+    platform_resume();
+    timer_thaw_percpu();
+
+    DEBUG_ASSERT(arch_ints_disabled());
+    arch_enable_ints();
+    return ZX_OK;
+}
+
+} // namespace
 
 zx_status_t arch_system_powerctl(uint32_t cmd, const zx_system_powerctl_arg_t* arg) {
     if (cmd != ZX_SYSTEM_POWERCTL_ACPI_TRANSITION_S_STATE) {
@@ -38,15 +121,42 @@ zx_status_t arch_system_powerctl(uint32_t cmd, const zx_system_powerctl_arg_t* a
         return ZX_ERR_BAD_STATE;
     }
 
-    arch_disable_ints();
-    // TODO(teisenbe): Set up return vector!
-    TRACEF("Entering AcpiHwLegacySleepFinal\n");
-    ACPI_STATUS acpi_status = AcpiHwLegacySleepFinal(target_s_state,
-                                                     sleep_type_a, sleep_type_b);
-    arch_enable_ints();
-    if (acpi_status != AE_OK) {
-        TRACEF("AcpiHwLegacySleepFinal failed: %x\n", acpi_status);
-        return ZX_ERR_INTERNAL;
+    // Acquire resources for suspend and resume if necessary.
+    zx_status_t status;
+    if (target_s_state < 5) {
+        // If we're not shutting down, prepare a resume path and execute the
+        // suspend on a separate thread (see comment on |suspend_thread()| for
+        // explanation).
+        thread_t* t = thread_create("suspend-thread", suspend_thread,
+                                    const_cast<zx_system_powerctl_arg_t*>(arg),
+                                    HIGHEST_PRIORITY, DEFAULT_STACK_SIZE);
+        if (!t) {
+            return ZX_ERR_NO_MEMORY;
+        }
+
+        status = thread_resume(t);
+        ASSERT(status == ZX_OK);
+
+        zx_status_t retcode;
+        status = thread_join(t, &retcode, ZX_TIME_INFINITE);
+        ASSERT(status == ZX_OK);
+
+        if (retcode != ZX_OK) {
+            return retcode;
+        }
+    } else {
+        struct x86_realmode_entry_data_registers regs;
+
+        DEBUG_ASSERT(target_s_state == 5);
+        arch_disable_ints();
+
+        ACPI_STATUS acpi_status = x86_acpi_transition_s_state(&regs, target_s_state,
+                                                              sleep_type_a, sleep_type_b);
+        arch_enable_ints();
+        if (acpi_status != AE_OK) {
+            return ZX_ERR_INTERNAL;
+        }
     }
+
     return ZX_OK;
 }
