e575809121723d2d14ab0c5274b7c911f36b36e8
Brian Swetland <swetland@google.com>
2017-11-03 13:58:04 -0700

    [build] remove ENANBLE_NEW_FB option
    We flipped this switch some time ago and there is now no going back.
    
    Change-Id: I3ec1fe06d77e68460bde32f5943bd2053a625be6

--- system/dev/display/framebuffer/rules.mk
+++ system/dev/display/framebuffer/rules.mk
@@ -2,8 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-ifeq ($(ENABLE_NEW_FB),true)
-
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
@@ -17,5 +15,3 @@ MODULE_STATIC_LIBS := system/ulib/ddk
 MODULE_LIBS := system/ulib/driver system/ulib/zircon system/ulib/c
 
 include make/module.mk
-
-endif
