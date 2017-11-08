2f9b9a753f8513cb7b0880edb8f0497b64f92cea
Bert Muthalaly <bert@somethingdoneright.net>
2017-11-08 12:45:57 -0500

    Test that invoking ping increases ICMP sent count.
    ping against localhost currently doesn't resolve to anything, but the
    ICMP sent count still increases, so this ends up being a nice
    integration test that doesn't rely on being able to reach the network.
    
    Change-Id: I342dbef3532224e9eea31a3b57d27606bfe9c450

--- go/src/netstack/netstat/netstat_test.go
+++ go/src/netstack/netstat/netstat_test.go
@@ -6,18 +6,14 @@ package main
 
 import (
 	"os/exec"
+	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 )
 
 func TestICMPOutputPresence(t *testing.T) {
-	cmd := exec.Command("/system/bin/netstat", "-s")
-	raw, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Errorf("%v\n", err)
-	}
-
-	out := string(raw)
+	out := output(t, "/system/bin/netstat", "-s")
 	metrics := []string{"ICMP messages received", "input ICMP message failed.", "ICMP messages sent", "ICMP messages failed", "ICMP input histogram", "ICMP output histogram"}
 
 	for _, m := range metrics {
@@ -25,4 +21,53 @@ func TestICMPOutputPresence(t *testing.T) {
 			t.Errorf("ICMP metric \"%s\" not present", m)
 		}
 	}
+}
+
+func TestICMPSentCount(t *testing.T) {
+	reqs := parseSentCount(t, output(t, "/system/bin/netstat", "-s"))
+
+	ping := exec.Command("/boot/bin/ping", "-c", "1", "localhost")
+	err := ping.Run()
+	switch err.(type) {
+	case *exec.Error:
+		t.Fatalf("Failed to run ping: %v", err)
+	case *exec.ExitError:
+		// fallthrough, ping doesn't have to successfully resolve the host for sent count to increase
+	}
+
+	reqsAfter := parseSentCount(t, output(t, "/system/bin/netstat", "-s"))
+
+	if !(reqsAfter > reqs) {
+		t.Errorf("echo request count did not increase after running ping: before %d, after %d", reqs, reqsAfter)
+	}
+}
+
+func output(t *testing.T, command string, args ...string) string {
+	cmd := exec.Command(command, args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("Couldn't execute %s: %v\n", command, err)
+		if out != nil {
+			t.Errorf("Combined output:\n%s", out)
+		}
+		t.Fail()
+	}
+
+	return string(out)
+}
+
+var sentCountMatcher *regexp.Regexp = regexp.MustCompile("([0-9]+) ICMP messages sent")
+
+func parseSentCount(t *testing.T, nsOut string) int64 {
+	matches := sentCountMatcher.FindStringSubmatch(nsOut)
+	if len(matches) == 0 {
+		t.Fatalf("Did not match %+v in %s", sentCountMatcher, nsOut)
+	}
+
+	count, err := strconv.ParseInt(matches[1], 10, 64)
+	if err != nil {
+		t.Errorf("Could not parse echo request count from string: %s", matches[0])
+	}
+
+	return count
 }
