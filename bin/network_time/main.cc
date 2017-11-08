2b3e70449255721c0a288e0cdb112f9a1a22448e
Adam Barth <abarth@google.com>
2017-11-08 15:26:21 -0800

    [network_time] Sandbox network time
    The network_time process requires access to the real-time clock, which is in
    /dev/misc. Eventually, we'll need to have finer-grained access to /dev/misc
    because /dev/misc has some very powerful entry points.
    
    Change-Id: Ia4de06468ba74b97b226a1d1a453fc0da26d4921

--- bin/network_time/main.cc
+++ bin/network_time/main.cc
@@ -2,23 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "garnet/bin/network_time/time_service.h"
 #include "lib/fxl/command_line.h"
 #include "lib/fxl/log_settings_command_line.h"
-
-#include "garnet/bin/network_time/logging.h"
-#include "garnet/bin/network_time/time_service.h"
 
 int main(int argc, char** argv) {
   auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
   if (!fxl::SetLogSettingsFromCommandLine(command_line))
     return 1;
-  TS_LOG(INFO) << "starting";
-  if (argc != 2) {
-    TS_LOG(ERROR) << "Usage: " << argv[0] << " <roughtime-servers.json>";
-    return EINVAL;
-  }
-
-  timeservice::TimeService service(argv[1]);
+  timeservice::TimeService service("/pkg/data/roughtime-servers.json");
   service.Run();
   return 0;
 }
