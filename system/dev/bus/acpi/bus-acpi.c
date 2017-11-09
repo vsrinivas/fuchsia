pandarong/zircon

[ddk] simplify get_auxdata
* pass a single string as the argument to get_auxdata
* introduce a fixed-size message between the pci and pci proxy
* use a more/cookie mechanism instead of get-nth to get
  multiple children data

Change-Id: I505bb97ad82a5e7f71bc8fdc2953575a32926639

system/dev/bus/acpi/bus-acpi.c
@@ -82,11 +82,16 @@ typedef struct {
 } publish_acpi_device_ctx_t;
 
 typedef struct {
-    uint8_t n;
+    uint8_t max;
     uint8_t i;
     zx_status_t st;
+    uint32_t* more;
+    uint32_t* cookie;
     auxdata_i2c_device_t* data;
 } pci_child_auxdata_ctx_t;
+
+typedef struct {
+} acpi_auxdata_ctx_t;
 
 zx_handle_t root_resource_handle;
 
@@ -156,13 +161,12 @@ static ACPI_STATUS pci_child_data_resources_callback(ACPI_RESOURCE* res, void* c
     return AE_CTRL_TERMINATE;
 }
 
-static ACPI_STATUS pci_child_data_callback(ACPI_HANDLE object, uint32_t nesting_level,
-                                       void* context, void** out_value) {
+static ACPI_STATUS pci_child_data_callback(ACPI_HANDLE object,
+                                           uint32_t nesting_level,
+                                           void* context, void** out_value) {
     pci_child_auxdata_ctx_t* ctx = (pci_child_auxdata_ctx_t*)context;
-    uint32_t i = ctx->i++;
-    if (i < ctx->n) {
-        return AE_OK;
-    } else if (i > ctx->n) {
+    if (ctx->i++ > ctx->max) {
+        *ctx->more = 1;
         return AE_CTRL_TERMINATE;
     }
 
@@ -190,25 +194,27 @@ static ACPI_STATUS pci_child_data_callback(ACPI_HANDLE object, uint32_t nesting_
     return AE_CTRL_TERMINATE;
 }
 
-static zx_status_t pciroot_op_get_auxdata(void* context, auxdata_type_t type,
-                                          void* _args, size_t args_len,
-                                          void* out_data, size_t out_len) {
+static zx_status_t pciroot_op_get_auxdata(void* context, const char* args,
+                                          void* data, uint32_t bytes,
+                                          uint32_t* actual) {
     acpi_device_t* dev = (acpi_device_t*)context;
-    if (type != AUXDATA_PCI_CHILD_NTH_DEVICE) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
 
-    auxdata_args_pci_child_nth_device_t* args = _args;
-    if (args->child_type != AUXDATA_DEVICE_I2C) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
-
-    if (out_len != sizeof(auxdata_i2c_device_t)) {
+    char type[16];
+    uint32_t bus_id, dev_id, func_id;
+    if (sscanf(args, "%s,%02x:%02x:%02x", type, bus_id, dev_id, func_id) < 0) {
         return ZX_ERR_INVALID_ARGS;
     }
 
+    if (strcmp(type, "i2c-child")) {
+        return ZX_ERR_NOT_SUPPORTED;
+    }
+
+    if (bytes < (2 * sizeof(uint32_t))) {
+        return ZX_ERR_BUFFER_TOO_SMALL;
+    }
+
     ACPI_HANDLE pci_node = NULL;
-    uint32_t addr = (args->dev_id << 16) | args->func_id;
+    uint32_t addr = (dev_id << 16) | func_id;
 
     // Look for the child node with this device and function id
     ACPI_STATUS acpi_status = AcpiWalkNamespace(ACPI_TYPE_DEVICE, dev->ns_node, 1,
@@ -221,16 +227,18 @@ static zx_status_t pciroot_op_get_auxdata(void* context, auxdata_type_t type,
         return ZX_ERR_NOT_FOUND;
     }
 
-    // Look for the nth child for this pci node and fill in data
+    // Look for as many children as can fit in the provided buffer
     pci_child_auxdata_ctx_t ctx = {
-        .n = args->n,
+        .max = (bytes - 2 * sizeof(uint32_t)) / sizeof(auxdata_i2c_device_t),
         .i = 0,
         .st = ZX_ERR_NOT_FOUND,
-        .data = out_data,
+        .more = data,
+        .cookie = data + sizeof(uint32_t),
+        .data = data + 2 * sizeof(uint32_t),
     };
 
-    acpi_status = AcpiWalkNamespace(ACPI_TYPE_DEVICE, pci_node, 1, pci_child_data_callback,
-                                    NULL, &ctx, NULL);
+    acpi_status = AcpiWalkNamespace(ACPI_TYPE_DEVICE, pci_node, 1,
+                                    pci_child_data_callback, NULL, &ctx, NULL);
     if ((acpi_status != AE_OK) && (acpi_status != AE_CTRL_TERMINATE)) {
         return acpi_to_zx_status(acpi_status);
     }

system/dev/bus/pci/kpci.c
@@ -36,56 +36,38 @@ static zx_status_t kpci_rxrpc(void* ctx, zx_handle_t h) {
 #if PROXY_DEVICE
     return ZX_ERR_NOT_SUPPORTED;
 #else
-    uint32_t actual;
-    zx_status_t st = zx_channel_read(h, 0, NULL, NULL, 0, 0, &actual, NULL);
-    if ((st != ZX_OK) && (st != ZX_ERR_BUFFER_TOO_SMALL)) {
-        return st;
-    }
-
-    pci_req_auxdata_nth_device_t req;
-    if (actual > sizeof(req)) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
-
-    st = zx_channel_read(h, 0, &req, NULL, sizeof(req), 0, &actual, NULL);
+    pci_msg_t req;
+    st = zx_channel_read(h, 0, &req, NULL, sizeof(req), 0, NULL, NULL);
     if (st != ZX_OK) {
         return st;
     }
 
-    if ((req.hdr.op != PCI_OP_GET_AUXDATA) ||
-        (req.auxdata_type != AUXDATA_NTH_DEVICE) ||
-        (req.args.child_type != AUXDATA_DEVICE_I2C)) {
-        return ZX_ERR_NOT_SUPPORTED;
+    pci_msg_t resp = {
+        .txid = msg.txid,
+        .reserved0 = 0,
+        .flags = 0,
+        .datalen = 0,
+    };
+    if (req.ordinal != PCI_OP_GET_AUXDATA) {
+        resp.ordinal = ZX_ERR_NOT_SUPPORTED;
+        goto out;
     }
 
-    kpci_device_t* device = ctx;
-    if (device->pciroot.ctx == NULL) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
+    char args[32];
+    snprintf(args, sizeof(args), "%s,%02x:%02x:%02x", req.data,
+            device->info.bus_id, device->info.dev_id, device->info.func_id);
 
-    auxdata_args_pci_child_nth_device_t args = {
-        .bus_id = device->info.bus_id,
-        .dev_id = device->info.dev_id,
-        .func_id = device->info.func_id,
-        .n = req.args.n,
-        .child_type = req.args.child_type,
-    };
-
-    pci_resp_auxdata_i2c_nth_device_t resp = {
-        .hdr = {
-            .txid = req.hdr.txid,
-            .len = sizeof(resp),
-            .op = req.hdr.op,
-            .status = ZX_OK,
-        },
-    };
-    st = pciroot_get_auxdata(&device->pciroot, AUXDATA_PCI_CHILD_NTH_DEVICE,
-                             &args, sizeof(args),
-                             &resp.device, sizeof(resp.device));
+    size_t actual;
+    st = pciroot_get_auxdata(&device->pciroot, args, resp.data, PCI_MAX_DATA,
+                             &resp.device, &actual);
     if (st != ZX_OK) {
-        resp.hdr.status = st;
+        resp.ordinal = st;
+    } else {
+        resp.ordinal = ZX_OK;
+        resp.datalen = actual;
     }
 
+out:
     return zx_channel_write(h, 0, &resp, sizeof(resp), NULL, 0);
 #endif
 }

system/dev/serial/intel-serialio/i2c/controller.c
@@ -718,6 +718,52 @@ static zx_status_t intel_serialio_i2c_device_specific_init(
     return ZX_ERR_NOT_SUPPORTED;
 }
 
+void zx_status_t intel_serialio_add_devices(zx_device_t* parent,
+                                            pci_protocol_t* pci) {
+    // get child info from aux data
+    // space for 1 device plus header (more + cookie)
+    uint8_t childdata[sizeof(auxdata_i2c_device_t) + sizeof(uint32_t) * 2];
+    size_t actual;
+    char args[16];
+    uint32_t more;
+    uint32_t cookie = 0;
+    auxdata_i2c_device_t* child;
+    uint32_t bus_speed = 0;
+    // TODO: this seems nonstandard to device model
+    for (;;) {
+        memset(childdata, 0, sizeof(childdata));
+        status = pci_get_auxdata(pci, "i2c-child", childdata, sizeof(childdata),
+                                 &actual);
+        if (status != ZX_OK) {
+            break;
+        }
+        if (actual != sizeof(childdata)) {
+            break;
+        }
+        more = *(uint32_t*)(childdata);
+        cookie = *(uint32_t*)(childdata + sizeof(uint32_t));
+        child = (auxdata_i2c_device_t*)(childdata + 2 * sizeof(uint32_t));
+        if (child->protocol_id == ZX_PROTOCOL_I2C_HID) {
+            if (bus_speed && bus_speed != child->bus_speed) {
+                zxlogf(ERROR, "i2c: cannot add devices with different bus speeds (%u, %u)\n",
+                        bus_speed, child->bus_speed);
+                break;
+            }
+            if (!bus_speed) {
+                intel_serialio_i2c_set_bus_frequency(parent, child->bus_speed);
+                bus_speed = child->bus_speed;
+            }
+            intel_serialio_i2c_add_slave(parent,
+                    child->ten_bit ? I2C_10BIT_ADDRESS : I2C_7BIT_ADDRESS,
+                    child->address);
+        }
+        if (!more) {
+            break;
+        }
+    }
+
+}
+
 zx_status_t intel_serialio_bind_i2c(zx_device_t* dev) {
     pci_protocol_t pci;
     if (device_get_protocol(dev, ZX_PROTOCOL_PCI, &pci))
@@ -830,36 +876,7 @@ zx_status_t intel_serialio_bind_i2c(zx_device_t* dev) {
         "reg=%p regsize=%ld\n",
         device->regs, device->regs_size);
 
-    // get child info from aux data
-    auxdata_args_nth_device_t auxdata_args = {
-        .child_type = AUXDATA_DEVICE_I2C,
-        .n = 0,
-    };
-    auxdata_i2c_device_t child;
-    uint32_t bus_speed = 0;
-    // TODO: this seems nonstandard to device model
-    do {
-        memset(&child, 0, sizeof(child));
-        status = pci_get_auxdata(&pci, AUXDATA_NTH_DEVICE, &auxdata_args, sizeof(auxdata_args),
-                                 &child, sizeof(child));
-        if (status == ZX_OK) {
-            if (child.protocol_id == ZX_PROTOCOL_I2C_HID) {
-                if (bus_speed && bus_speed != child.bus_speed) {
-                    zxlogf(ERROR, "i2c: cannot add devices with different bus speeds (%u, %u)\n",
-                            bus_speed, child.bus_speed);
-                    break;
-                }
-                if (!bus_speed) {
-                    intel_serialio_i2c_set_bus_frequency(device, child.bus_speed);
-                    bus_speed = child.bus_speed;
-                }
-                intel_serialio_i2c_add_slave(device,
-                        child.ten_bit ? I2C_10BIT_ADDRESS : I2C_7BIT_ADDRESS,
-                        child.address);
-            }
-        }
-    } while ((status == ZX_OK) && auxdata_args.n++);
-
+    intel_serialio_add_devices(device, &pci);
 
     zx_handle_close(config_handle);
     return ZX_OK;

vsrinivas/fuchsia

system/dev/bus/acpi/bus-acpi.c
@@ -82,9 +82,8 @@ typedef struct {
 } publish_acpi_device_ctx_t;
 
 typedef struct {
-    uint8_t n;
+    uint8_t max;
     uint8_t i;
-    zx_status_t st;
     auxdata_i2c_device_t* data;
 } pci_child_auxdata_ctx_t;
 
@@ -136,7 +135,7 @@ static ACPI_STATUS find_pci_child_callback(ACPI_HANDLE object, uint32_t nesting_
 
 static ACPI_STATUS pci_child_data_resources_callback(ACPI_RESOURCE* res, void* context) {
     pci_child_auxdata_ctx_t* ctx = (pci_child_auxdata_ctx_t*)context;
-    auxdata_i2c_device_t* child = ctx->data;
+    auxdata_i2c_device_t* child = ctx->data + ctx->i;
 
     if (res->Type != ACPI_RESOURCE_TYPE_SERIAL_BUS) {
         return AE_NOT_FOUND;
@@ -151,18 +150,14 @@ static ACPI_STATUS pci_child_data_resources_callback(ACPI_RESOURCE* res, void* c
     child->address = i2c->SlaveAddress;
     child->bus_speed = i2c->ConnectionSpeed;
 
-    ctx->st = ZX_OK;
-
     return AE_CTRL_TERMINATE;
 }
 
-static ACPI_STATUS pci_child_data_callback(ACPI_HANDLE object, uint32_t nesting_level,
-                                       void* context, void** out_value) {
+static ACPI_STATUS pci_child_data_callback(ACPI_HANDLE object,
+                                           uint32_t nesting_level,
+                                           void* context, void** out_value) {
     pci_child_auxdata_ctx_t* ctx = (pci_child_auxdata_ctx_t*)context;
-    uint32_t i = ctx->i++;
-    if (i < ctx->n) {
-        return AE_OK;
-    } else if (i > ctx->n) {
+    if ((ctx->i + 1) > ctx->max) {
         return AE_CTRL_TERMINATE;
     }
 
@@ -182,33 +177,37 @@ static ACPI_STATUS pci_child_data_callback(ACPI_HANDLE object, uint32_t nesting_
     // call _CRS to get i2c info
     acpi_status = AcpiWalkResources(object, (char*)"_CRS",
                                     pci_child_data_resources_callback, ctx);
-    if (acpi_status != AE_OK) {
-        ctx->st = acpi_to_zx_status(acpi_status);
+    if ((acpi_status == AE_OK) || (acpi_status == AE_CTRL_TERMINATE)) {
+        ctx->i += 1;
     }
-
-    // terminate child walk once the the nth device is found
-    return AE_CTRL_TERMINATE;
+    return AE_OK;
 }
 
-static zx_status_t pciroot_op_get_auxdata(void* context, auxdata_type_t type,
-                                          void* _args, size_t args_len,
-                                          void* out_data, size_t out_len) {
+static zx_status_t pciroot_op_get_auxdata(void* context, const char* args,
+                                          void* data, uint32_t bytes,
+                                          uint32_t* actual) {
     acpi_device_t* dev = (acpi_device_t*)context;
-    if (type != AUXDATA_PCI_CHILD_NTH_DEVICE) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
 
-    auxdata_args_pci_child_nth_device_t* args = _args;
-    if (args->child_type != AUXDATA_DEVICE_I2C) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
-
-    if (out_len != sizeof(auxdata_i2c_device_t)) {
+    char type[16];
+    uint32_t bus_id, dev_id, func_id;
+    int n;
+    if ((n = sscanf(args, "%[^,],%02x:%02x:%02x", type, &bus_id, &dev_id, &func_id)) != 4) {
         return ZX_ERR_INVALID_ARGS;
     }
 
+    zxlogf(SPEW, "bus-acpi: get_auxdata type '%s' device %02x:%02x:%02x\n", type,
+           bus_id, dev_id, func_id);
+
+    if (strcmp(type, "i2c-child")) {
+        return ZX_ERR_NOT_SUPPORTED;
+    }
+
+    if (bytes < (2 * sizeof(uint32_t))) {
+        return ZX_ERR_BUFFER_TOO_SMALL;
+    }
+
     ACPI_HANDLE pci_node = NULL;
-    uint32_t addr = (args->dev_id << 16) | args->func_id;
+    uint32_t addr = (dev_id << 16) | func_id;
 
     // Look for the child node with this device and function id
     ACPI_STATUS acpi_status = AcpiWalkNamespace(ACPI_TYPE_DEVICE, dev->ns_node, 1,
@@ -221,21 +220,26 @@ static zx_status_t pciroot_op_get_auxdata(void* context, auxdata_type_t type,
         return ZX_ERR_NOT_FOUND;
     }
 
-    // Look for the nth child for this pci node and fill in data
+    // Look for as many children as can fit in the provided buffer
     pci_child_auxdata_ctx_t ctx = {
-        .n = args->n,
+        .max = bytes / sizeof(auxdata_i2c_device_t),
         .i = 0,
-        .st = ZX_ERR_NOT_FOUND,
-        .data = out_data,
+        .data = data,
     };
 
-    acpi_status = AcpiWalkNamespace(ACPI_TYPE_DEVICE, pci_node, 1, pci_child_data_callback,
-                                    NULL, &ctx, NULL);
+    acpi_status = AcpiWalkNamespace(ACPI_TYPE_DEVICE, pci_node, 1,
+                                    pci_child_data_callback, NULL, &ctx, NULL);
     if ((acpi_status != AE_OK) && (acpi_status != AE_CTRL_TERMINATE)) {
+        *actual = 0;
         return acpi_to_zx_status(acpi_status);
     }
 
-    return ctx.st;
+    *actual = ctx.i * sizeof(auxdata_i2c_device_t);
+
+    zxlogf(SPEW, "bus-acpi: get_auxdata '%s' %u devs actual %u\n",
+           args, ctx.i, *actual);
+
+    return ZX_OK;
 }
 
 static pciroot_protocol_ops_t pciroot_proto = {
@@ -640,8 +644,6 @@ static ACPI_STATUS acpi_ns_walk_callback(ACPI_HANDLE object, uint32_t nesting_le
             zxlogf(ERROR, "acpi-bus: acpi error 0x%x in I2C1._PS0\n", acpi_status);
         }
     }
-
-    zxlogf(TRACE, "acpi-bus: handle %p nesting level %d\n", (void*)object, nesting_level);
 
     publish_acpi_device_ctx_t* ctx = (publish_acpi_device_ctx_t*)context;
     zx_device_t* parent = ctx->parent;

system/dev/bus/pci/kpci.c
@@ -36,56 +36,40 @@ static zx_status_t kpci_rxrpc(void* ctx, zx_handle_t h) {
 #if PROXY_DEVICE
     return ZX_ERR_NOT_SUPPORTED;
 #else
-    uint32_t actual;
-    zx_status_t st = zx_channel_read(h, 0, NULL, NULL, 0, 0, &actual, NULL);
-    if ((st != ZX_OK) && (st != ZX_ERR_BUFFER_TOO_SMALL)) {
-        return st;
-    }
-
-    pci_req_auxdata_nth_device_t req;
-    if (actual > sizeof(req)) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
-
-    st = zx_channel_read(h, 0, &req, NULL, sizeof(req), 0, &actual, NULL);
+    pci_msg_t req;
+    zx_status_t st = zx_channel_read(h, 0, &req, NULL, sizeof(req), 0, NULL, NULL);
     if (st != ZX_OK) {
         return st;
     }
 
-    if ((req.hdr.op != PCI_OP_GET_AUXDATA) ||
-        (req.auxdata_type != AUXDATA_NTH_DEVICE) ||
-        (req.args.child_type != AUXDATA_DEVICE_I2C)) {
-        return ZX_ERR_NOT_SUPPORTED;
+    pci_msg_t resp = {
+        .txid = req.txid,
+        .reserved0 = 0,
+        .flags = 0,
+        .datalen = 0,
+    };
+    if (req.ordinal != PCI_OP_GET_AUXDATA) {
+        resp.ordinal = ZX_ERR_NOT_SUPPORTED;
+        goto out;
     }
+
+    zxlogf(SPEW, "pci: rpc-in op %d args '%s'\n", req.ordinal, req.data);
 
     kpci_device_t* device = ctx;
-    if (device->pciroot.ctx == NULL) {
-        return ZX_ERR_NOT_SUPPORTED;
-    }
+    char args[32];
+    snprintf(args, sizeof(args), "%s,%02x:%02x:%02x", req.data,
+            device->info.bus_id, device->info.dev_id, device->info.func_id);
 
-    auxdata_args_pci_child_nth_device_t args = {
-        .bus_id = device->info.bus_id,
-        .dev_id = device->info.dev_id,
-        .func_id = device->info.func_id,
-        .n = req.args.n,
-        .child_type = req.args.child_type,
-    };
-
-    pci_resp_auxdata_i2c_nth_device_t resp = {
-        .hdr = {
-            .txid = req.hdr.txid,
-            .len = sizeof(resp),
-            .op = req.hdr.op,
-            .status = ZX_OK,
-        },
-    };
-    st = pciroot_get_auxdata(&device->pciroot, AUXDATA_PCI_CHILD_NTH_DEVICE,
-                             &args, sizeof(args),
-                             &resp.device, sizeof(resp.device));
+    uint32_t actual;
+    st = pciroot_get_auxdata(&device->pciroot, args, resp.data, req.outlen, &actual);
     if (st != ZX_OK) {
-        resp.hdr.status = st;
+        resp.ordinal = st;
+    } else {
+        resp.ordinal = ZX_OK;
+        resp.datalen = actual;
     }
 
+out:
     return zx_channel_write(h, 0, &resp, sizeof(resp), NULL, 0);
 #endif
 }

system/dev/serial/intel-serialio/i2c/controller.c
@@ -718,6 +718,46 @@ static zx_status_t intel_serialio_i2c_device_specific_init(
     return ZX_ERR_NOT_SUPPORTED;
 }
 
+static void intel_serialio_add_devices(intel_serialio_i2c_device_t* parent,
+                                       pci_protocol_t* pci) {
+    // get child info from aux data, max 4
+    // TODO: this seems nonstandard to device model
+    uint8_t childdata[sizeof(auxdata_i2c_device_t) * 4];
+    memset(childdata, 0, sizeof(childdata));
+
+    uint32_t actual;
+    zx_status_t status = pci_get_auxdata(pci, "i2c-child", childdata, sizeof(childdata),
+                             &actual);
+    if (status != ZX_OK) {
+        return;
+    }
+
+    auxdata_i2c_device_t* child = (auxdata_i2c_device_t*)childdata;
+    uint32_t count = actual / sizeof(auxdata_i2c_device_t);
+    uint32_t bus_speed = 0;
+    while (count--) {
+        zxlogf(SPEW, "i2c: got child bus_master=%d ten_bit=%d address=0x%x bus_speed=%u"
+                     " protocol_id=0x%08x\n",
+               child->bus_master, child->ten_bit, child->address, child->bus_speed,
+               child->protocol_id);
+
+        if (child->protocol_id == ZX_PROTOCOL_I2C_HID) {
+            if (bus_speed && bus_speed != child->bus_speed) {
+                zxlogf(ERROR, "i2c: cannot add devices with different bus speeds (%u, %u)\n",
+                        bus_speed, child->bus_speed);
+            }
+            if (!bus_speed) {
+                intel_serialio_i2c_set_bus_frequency(parent, child->bus_speed);
+                bus_speed = child->bus_speed;
+            }
+            intel_serialio_i2c_add_slave(parent,
+                    child->ten_bit ? I2C_10BIT_ADDRESS : I2C_7BIT_ADDRESS,
+                    child->address);
+        }
+        child += 1;
+    }
+}
+
 zx_status_t intel_serialio_bind_i2c(zx_device_t* dev) {
     pci_protocol_t pci;
     if (device_get_protocol(dev, ZX_PROTOCOL_PCI, &pci))
@@ -830,36 +870,7 @@ zx_status_t intel_serialio_bind_i2c(zx_device_t* dev) {
         "reg=%p regsize=%ld\n",
         device->regs, device->regs_size);
 
-    // get child info from aux data
-    auxdata_args_nth_device_t auxdata_args = {
-        .child_type = AUXDATA_DEVICE_I2C,
-        .n = 0,
-    };
-    auxdata_i2c_device_t child;
-    uint32_t bus_speed = 0;
-    // TODO: this seems nonstandard to device model
-    do {
-        memset(&child, 0, sizeof(child));
-        status = pci_get_auxdata(&pci, AUXDATA_NTH_DEVICE, &auxdata_args, sizeof(auxdata_args),
-                                 &child, sizeof(child));
-        if (status == ZX_OK) {
-            if (child.protocol_id == ZX_PROTOCOL_I2C_HID) {
-                if (bus_speed && bus_speed != child.bus_speed) {
-                    zxlogf(ERROR, "i2c: cannot add devices with different bus speeds (%u, %u)\n",
-                            bus_speed, child.bus_speed);
-                    break;
-                }
-                if (!bus_speed) {
-                    intel_serialio_i2c_set_bus_frequency(device, child.bus_speed);
-                    bus_speed = child.bus_speed;
-                }
-                intel_serialio_i2c_add_slave(device,
-                        child.ten_bit ? I2C_10BIT_ADDRESS : I2C_7BIT_ADDRESS,
-                        child.address);
-            }
-        }
-    } while ((status == ZX_OK) && auxdata_args.n++);
-
+    intel_serialio_add_devices(device, &pci);
 
     zx_handle_close(config_handle);
     return ZX_OK;