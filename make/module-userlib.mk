5cb5d61f9d66c289395cf7a900d2143fc48821ed
Aaron Green <aarongreen@google.com>
2017-11-09 14:00:09 -0800

    [make] Fix error message typo
    Change-Id: I76094f5d7ad6e5353d93418e7c1d43420e749eb3

--- make/module-userlib.mk
+++ make/module-userlib.mk
@@ -9,7 +9,7 @@ ifneq ($(MODULE_DEPS),)
 $(error $(MODULE) $(MODULE_TYPE) modules must use MODULE_{LIBS,STATIC_LIBS}, not MODULE_DEPS)
 endif
 ifneq ($(MODULE_HOST_LIBS)$(MODULE_HOST_SYSLIBS),)
-$(error $(MODULE) $(MODULE_TYPE) modules must not use MODULE_{LIBS,STATIC_LIBS}, not MODULE_HOST_{LIBS,SYSLIBS})
+$(error $(MODULE) $(MODULE_TYPE) modules must use MODULE_{LIBS,STATIC_LIBS}, not MODULE_HOST_{LIBS,SYSLIBS})
 endif
 
 # Things that are library-like but not "userlib" do not
