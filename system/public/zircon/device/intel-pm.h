91f81445d8b4a69eaae20a105bca707cee15b5a7
Doug Evans <dje@google.com>
2017-11-11 14:25:13 -0800

    [dev][cpu-trace] Remove temporary files for garnet transition
    garnet now refers to these files using their new location: device/cpu-trace
    
    Change-Id: Ice78c89916c2543fd7e20a34ee92a6360982b98a

--- system/public/zircon/device/intel-pm.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This file is temporary to sequence the move into cpu-trace/.
-#include <zircon/device/cpu-trace/intel-pm.h>
