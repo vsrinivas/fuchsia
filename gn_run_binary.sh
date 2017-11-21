08fcf8aa6d046f688979330df82d0a8ac30af56a
Roland McGrath <mcgrathr@google.com>
2017-11-21 12:18:35 -0800

    Set *SAN_SYMBOLIZER_PATH in environment when running host tools
    This is necessary to enable nice symbolized error reporting when
    host tools are built with sanitizer runtimes.
    
    TO-558 #comment fixes symbolization for sanitized host tools in GN build
    
    Change-Id: Ied522728d4c4cbb5adbb2e9ee92ea2353d05fb7f

--- gn_run_binary.sh
+++ gn_run_binary.sh
@@ -1,9 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
 # Copyright 2016 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 # Helper script to run an arbitrary binary produced by the current build.
+# The first argument is the bin directory of the toolchain, where
+# llvm-symbolizer can be found.  The second argument is the binary to run,
+# and remaining arguments are passed to that binary.
 
-"./${1}" "${@:2}"
+clang_bindir="$1"
+shift
+
+binary="$1"
+shift
+
+case "$binary" in
+/*) ;;
+*) binary="./$binary" ;;
+esac
+
+# Make sure any sanitizer runtimes that might be included in the binary
+# can find llvm-symbolizer.
+symbolizer="${clang_bindir}/llvm-symbolizer"
+export ASAN_SYMBOLIZER_PATH="$symbolizer"
+export LSAN_SYMBOLIZER_PATH="$symbolizer"
+export MSAN_SYMBOLIZER_PATH="$symbolizer"
+export UBSAN_SYMBOLIZER_PATH="$symbolizer"
+export TSAN_OPTIONS="$TSAN_OPTIONS external_symbolizer_path=$symbolizer"
+
+exec "$binary" ${1+"$@"}
