1956103a90b4168d99abf8aca70859aab2ef58b1
Mark Seaborn <mseaborn@google.com>
2017-11-27 13:34:21 -0800

    [benchmarks] Add test for the round trip time of pthread condvars
    This is similar to FutexTest except that it tests a standard condvar
    interface that will be more commonly used than direct use of the
    Zircon futex syscalls.
    
    Change-Id: I646c296ee9654ac26ecef0ba0362be90b03457fb

--- bin/zircon_benchmarks/round_trips.cc
+++ bin/zircon_benchmarks/round_trips.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <pthread.h>
 #include <thread>
 #include <vector>
 
@@ -377,6 +378,66 @@ class FutexTest {
   volatile int futex2_ = 0;  // Signals from server to client.
 };
 
+// Test the round trip time for waking up threads using pthread condition
+// variables (condvars).  Condvars are implemented using futexes, so we
+// expect this to be a bit slower than FutexTest due to the overhead that
+// pthread's condvar implementation adds.
+class PthreadCondvarTest {
+ public:
+  PthreadCondvarTest() {
+    FXL_CHECK(pthread_mutex_init(&mutex_, nullptr) == 0);
+    FXL_CHECK(pthread_cond_init(&condvar1_, nullptr) == 0);
+    FXL_CHECK(pthread_cond_init(&condvar2_, nullptr) == 0);
+    thread_ = std::thread([this]() { ThreadFunc(); });
+  }
+
+  ~PthreadCondvarTest() {
+    // Tell the thread to shut down.
+    FXL_CHECK(pthread_mutex_lock(&mutex_) == 0);
+    state_ = EXIT;
+    FXL_CHECK(pthread_cond_signal(&condvar1_) == 0);
+    FXL_CHECK(pthread_mutex_unlock(&mutex_) == 0);
+
+    thread_.join();
+
+    FXL_CHECK(pthread_cond_destroy(&condvar1_) == 0);
+    FXL_CHECK(pthread_cond_destroy(&condvar2_) == 0);
+    FXL_CHECK(pthread_mutex_destroy(&mutex_) == 0);
+  }
+
+  void Run() {
+    FXL_CHECK(pthread_mutex_lock(&mutex_) == 0);
+    // Wake the child.
+    state_ = WAKE_CHILD;
+    FXL_CHECK(pthread_cond_signal(&condvar1_) == 0);
+    // Wait for the reply.
+    while (state_ != REPLY_TO_PARENT)
+      FXL_CHECK(pthread_cond_wait(&condvar2_, &mutex_) == 0);
+    FXL_CHECK(pthread_mutex_unlock(&mutex_) == 0);
+  }
+
+ private:
+  void ThreadFunc() {
+    FXL_CHECK(pthread_mutex_lock(&mutex_) == 0);
+    for (;;) {
+      if (state_ == EXIT)
+        break;
+      if (state_ == WAKE_CHILD) {
+        state_ = REPLY_TO_PARENT;
+        FXL_CHECK(pthread_cond_signal(&condvar2_) == 0);
+      }
+      FXL_CHECK(pthread_cond_wait(&condvar1_, &mutex_) == 0);
+    }
+    FXL_CHECK(pthread_mutex_unlock(&mutex_) == 0);
+  }
+
+  std::thread thread_;
+  pthread_mutex_t mutex_;
+  pthread_cond_t condvar1_;  // Signals from parent to child.
+  pthread_cond_t condvar2_;  // Signals from child to parent.
+  enum { INITIAL, WAKE_CHILD, REPLY_TO_PARENT, EXIT } state_ = INITIAL;
+};
+
 struct ThreadFuncEntry {
   const char* name;
   ThreadFunc func;
@@ -441,6 +502,7 @@ void RegisterRoundTripBenchmarks() {
   RegisterTestMultiProc<ChannelCallTest>("RoundTrip_ChannelCall");
   RegisterTestMultiProc<PortTest>("RoundTrip_Port");
   RegisterTest<FutexTest>("RoundTrip_Futex_SingleProcess");
+  RegisterTest<PthreadCondvarTest>("RoundTrip_PthreadCondvar_SingleProcess");
 }
 
 void RunSubprocess(const char* func_name) {
