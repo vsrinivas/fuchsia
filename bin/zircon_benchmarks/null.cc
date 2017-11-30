96486431ea101cffbef26d2331a7d78882954cb2
Mark Seaborn <mseaborn@google.com>
2017-11-30 14:51:07 -0800

    [benchmarks] Add a null test for measuring the benchmarking system's overhead
    Change-Id: I48be47b83479e91038bf3032b1b5f1cf147ec41a

---
+++ bin/zircon_benchmarks/null.cc
@@ -0,0 +1,21 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "test_runner.h"
+
+namespace {
+
+// This is a test that does nothing.  This is useful for measuring the
+// overhead of the performance testing framework.  There will be some
+// overhead involved in calling the function (via a direct or indirect
+// call if the call is not inlined) and collecting timing data around
+// the function call.
+void NullTest() {}
+
+__attribute__((constructor))
+void RegisterTests() {
+  fbenchmark::RegisterTestFunc<NullTest>("Null");
+}
+
+}  // namespace
