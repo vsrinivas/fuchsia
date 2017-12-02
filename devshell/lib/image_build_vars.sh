ddb358c71d1da535cf8a74fffee644e4e3e84b54
James Tucker <raggi@google.com>
2017-12-01 18:23:20 -0800

    [paver] improve scripts UX
    Change-Id: Ia2d2aba683d13ee4c33ad94b913e0b084e0f7f31

--- devshell/lib/image_build_vars.sh
+++ devshell/lib/image_build_vars.sh
@@ -5,6 +5,53 @@
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/vars.sh
 fx-config-read
 
+fx-machine-types() {
+  echo "Available machine types:"
+  echo "  ram"
+  echo "  efi"
+  echo "  cros"
+  echo "Machine type aliases:"
+  echo "  acer"
+  echo "  nuc"
+  echo "  pixel"
+  echo "  vboot"
+}
+
+fx-ensure-vboot-tools() {
+  # TODO(INTK-60): remove once futility is in //buildtools
+  local vboot_reference="${FUCHSIA_DIR}/third_party/vboot_reference"
+  local futility_bin="${vboot_reference}/build/futility/futility"
+  local cgpt_bin="${vboot_reference}/build/cgpt/cgpt"
+  export CGPT="${cgpt_bin}"
+
+  local LDFLAGS=
+  local HAVE_MACOS=0
+  local OPENSSL_DIR=
+  if [[ $(uname) == "Darwin" ]]; then
+    HAVE_MACOS=1
+    if which brew > /dev/null; then
+      OPENSSL_DIR=$(dirname $(dirname $(dirname $(brew ls openssl | grep include/openssl | head -n 1))))
+    fi
+  else
+    LDFLAGS="-lpthread"
+  fi
+  if [[ ! -x "${futility_bin}" ]]; then
+    if [[ ! -f "/usr/include/uuid/uuid.h" ]]; then
+      if [[ $(uname) == "Linux" ]]; then
+        echo "Installing uuid-dev..."
+        sudo apt-get install -y uuid-dev
+      fi
+    fi
+    make -C "${vboot_reference}" LDFLAGS="${LDFLAGS}" HAVE_MACOS="${HAVE_MACOS}" OPENSSL_DIR="${OPENSSL_DIR}" "${futility_bin}"
+  fi
+  # TODO(raggi): patch cgpt to build on mac
+  if [[ -z "$HAVE_MACOS" ]]; then
+    if [[ ! -x "${cgpt_bin}" ]]; then
+      make -C "${vboot_reference}" LDFLAGS="${LDFLAGS}" HAVE_MACOS="${HAVE_MACOS}" OPENSSL_DIR="${OPENSSL_DIR}" "${cgpt_bin}"
+    fi
+  fi
+}
+
 zircon_bin="zircon.bin"
 ramdisk_bin="bootdata-blobstore-${ZIRCON_PROJECT}.bin"
 
