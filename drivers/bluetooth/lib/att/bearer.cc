AlexxNica/garnet

[bt][common] Move NewBuffer to test_helpers, add Ptr aliases
Moved NewBuffer() to test_helpers.h as it was being internally redefined in
multiple tests. Also defined unique_ptr alias types for ByteBuffer and
MutableByteBuffer.

Change-Id: I06d40115043cba596f5f985295f301bf6d6cdab5

drivers/bluetooth/lib/att/bearer.cc
@@ -117,11 +117,10 @@ OpCode MatchingTransactionCode(OpCode transaction_end_code) {
 
 }  // namespace
 
-Bearer::PendingTransaction::PendingTransaction(
-    OpCode opcode,
-    TransactionCallback callback,
-    ErrorCallback error_callback,
-    std::unique_ptr<common::ByteBuffer> pdu)
+Bearer::PendingTransaction::PendingTransaction(OpCode opcode,
+                                               TransactionCallback callback,
+                                               ErrorCallback error_callback,
+                                               common::ByteBufferPtr pdu)
     : opcode(opcode),
       callback(callback),
       error_callback(error_callback),
@@ -275,7 +274,7 @@ void Bearer::ShutDownInternal(bool due_to_timeout) {
     closed_cb_();
 }
 
-bool Bearer::StartTransaction(std::unique_ptr<common::ByteBuffer> pdu,
+bool Bearer::StartTransaction(common::ByteBufferPtr pdu,
                               const TransactionCallback& callback,
                               const ErrorCallback& error_callback) {
   FXL_DCHECK(pdu);
@@ -285,12 +284,12 @@ bool Bearer::StartTransaction(std::unique_ptr<common::ByteBuffer> pdu,
   return SendInternal(std::move(pdu), callback, error_callback);
 }
 
-bool Bearer::SendWithoutResponse(std::unique_ptr<common::ByteBuffer> pdu) {
+bool Bearer::SendWithoutResponse(common::ByteBufferPtr pdu) {
   FXL_DCHECK(pdu);
   return SendInternal(std::move(pdu), {}, {});
 }
 
-bool Bearer::SendInternal(std::unique_ptr<common::ByteBuffer> pdu,
+bool Bearer::SendInternal(common::ByteBufferPtr pdu,
                           const TransactionCallback& callback,
                           const ErrorCallback& error_callback) {
   if (!is_open()) {
@@ -424,8 +423,7 @@ void Bearer::UnregisterHandler(HandlerId id) {
   }
 }
 
-bool Bearer::EndTransaction(TransactionId id,
-                            std::unique_ptr<common::ByteBuffer> pdu) {
+bool Bearer::EndTransaction(TransactionId id, common::ByteBufferPtr pdu) {
   FXL_DCHECK(thread_checker_.IsCreationThreadCurrent());
   FXL_DCHECK(pdu);
 

drivers/bluetooth/lib/att/bearer.h
@@ -114,7 +114,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
   using TransactionCallback = std::function<void(const PacketReader& packet)>;
   using ErrorCallback = std::function<
       void(bool timeout, ErrorCode protocol_error, Handle attr_in_error)>;
-  bool StartTransaction(std::unique_ptr<common::ByteBuffer> pdu,
+  bool StartTransaction(common::ByteBufferPtr pdu,
                         const TransactionCallback& callback,
                         const ErrorCallback& error_callback);
 
@@ -123,7 +123,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
   //
   // Returns false if the packet is malformed or does not correspond to a
   // command or notification.
-  bool SendWithoutResponse(std::unique_ptr<common::ByteBuffer> pdu);
+  bool SendWithoutResponse(common::ByteBufferPtr pdu);
 
   // Handlers are functions that are invoked when the Bearer receives a PDU that
   // is not tied to a locally initiated transaction (see StartTransaction()).
@@ -153,8 +153,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
 
   // Ends a currently pending transaction. Returns false if |pdu| is malformed
   // or if |id| and |pdu| do not match a pending transaction.
-  bool EndTransaction(TransactionId id,
-                      std::unique_ptr<common::ByteBuffer> pdu);
+  bool EndTransaction(TransactionId id, common::ByteBufferPtr pdu);
 
   // Ends a request transaction with an error response.
   bool ReplyWithError(TransactionId id, Handle handle, ErrorCode error_code);
@@ -176,7 +175,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
     PendingTransaction(OpCode opcode,
                        TransactionCallback callback,
                        ErrorCallback error_callback,
-                       std::unique_ptr<common::ByteBuffer> pdu);
+                       common::ByteBufferPtr pdu);
 
     // Required fields
     OpCode opcode;
@@ -186,7 +185,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
     ErrorCallback error_callback;
 
     // Holds the pdu while the transaction is in the send queue.
-    std::unique_ptr<common::ByteBuffer> pdu;
+    common::ByteBufferPtr pdu;
   };
   using PendingTransactionPtr = std::unique_ptr<PendingTransaction>;
 
@@ -238,7 +237,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
     std::unique_ptr<async::Task> timeout_task_;
   };
 
-  bool SendInternal(std::unique_ptr<common::ByteBuffer> pdu,
+  bool SendInternal(common::ByteBufferPtr pdu,
                     const TransactionCallback& callback,
                     const ErrorCallback& error_callback);
 

vsrinivas/fuchsia

drivers/bluetooth/lib/att/bearer.cc
@@ -119,11 +119,10 @@ OpCode MatchingTransactionCode(OpCode transaction_end_code) {
 
 }  // namespace
 
-Bearer::PendingTransaction::PendingTransaction(
-    OpCode opcode,
-    TransactionCallback callback,
-    ErrorCallback error_callback,
-    std::unique_ptr<common::ByteBuffer> pdu)
+Bearer::PendingTransaction::PendingTransaction(OpCode opcode,
+                                               TransactionCallback callback,
+                                               ErrorCallback error_callback,
+                                               common::ByteBufferPtr pdu)
     : opcode(opcode),
       callback(callback),
       error_callback(error_callback),
@@ -274,7 +273,7 @@ void Bearer::ShutDownInternal(bool due_to_timeout) {
     closed_cb_();
 }
 
-bool Bearer::StartTransaction(std::unique_ptr<common::ByteBuffer> pdu,
+bool Bearer::StartTransaction(common::ByteBufferPtr pdu,
                               const TransactionCallback& callback,
                               const ErrorCallback& error_callback) {
   FXL_DCHECK(pdu);
@@ -284,12 +283,12 @@ bool Bearer::StartTransaction(std::unique_ptr<common::ByteBuffer> pdu,
   return SendInternal(std::move(pdu), callback, error_callback);
 }
 
-bool Bearer::SendWithoutResponse(std::unique_ptr<common::ByteBuffer> pdu) {
+bool Bearer::SendWithoutResponse(common::ByteBufferPtr pdu) {
   FXL_DCHECK(pdu);
   return SendInternal(std::move(pdu), {}, {});
 }
 
-bool Bearer::SendInternal(std::unique_ptr<common::ByteBuffer> pdu,
+bool Bearer::SendInternal(common::ByteBufferPtr pdu,
                           const TransactionCallback& callback,
                           const ErrorCallback& error_callback) {
   if (!is_open()) {
@@ -380,7 +379,7 @@ void Bearer::UnregisterHandler(HandlerId id) {
   handlers_.erase(opcode);
 }
 
-bool Bearer::Reply(TransactionId tid, std::unique_ptr<common::ByteBuffer> pdu) {
+bool Bearer::Reply(TransactionId tid, common::ByteBufferPtr pdu) {
   FXL_DCHECK(thread_checker_.IsCreationThreadCurrent());
   FXL_DCHECK(pdu);
 

drivers/bluetooth/lib/att/bearer.h
@@ -114,7 +114,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
   using TransactionCallback = std::function<void(const PacketReader& packet)>;
   using ErrorCallback = std::function<
       void(bool timeout, ErrorCode protocol_error, Handle attr_in_error)>;
-  bool StartTransaction(std::unique_ptr<common::ByteBuffer> pdu,
+  bool StartTransaction(common::ByteBufferPtr pdu,
                         const TransactionCallback& callback,
                         const ErrorCallback& error_callback);
 
@@ -123,7 +123,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
   //
   // Returns false if the packet is malformed or does not correspond to a
   // command or notification.
-  bool SendWithoutResponse(std::unique_ptr<common::ByteBuffer> pdu);
+  bool SendWithoutResponse(common::ByteBufferPtr pdu);
 
   // A Handler is a function that gets invoked when the Bearer receives a PDU
   // that is not tied to a locally initiated transaction (see
@@ -152,7 +152,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
   // Ends a currently pending transaction with the given response or
   // confirmation |pdu|. Returns false if |pdu| is malformed or if |id| and
   // |pdu| do not match a pending transaction.
-  bool Reply(TransactionId tid, std::unique_ptr<common::ByteBuffer> pdu);
+  bool Reply(TransactionId tid, common::ByteBufferPtr pdu);
 
   // Ends a request transaction with an error response.
   bool ReplyWithError(TransactionId id, Handle handle, ErrorCode error_code);
@@ -174,7 +174,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
     PendingTransaction(OpCode opcode,
                        TransactionCallback callback,
                        ErrorCallback error_callback,
-                       std::unique_ptr<common::ByteBuffer> pdu);
+                       common::ByteBufferPtr pdu);
 
     // Required fields
     OpCode opcode;
@@ -184,7 +184,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
     ErrorCallback error_callback;
 
     // Holds the pdu while the transaction is in the send queue.
-    std::unique_ptr<common::ByteBuffer> pdu;
+    common::ByteBufferPtr pdu;
   };
   using PendingTransactionPtr = std::unique_ptr<PendingTransaction>;
 
@@ -236,7 +236,7 @@ class Bearer final : public fxl::RefCountedThreadSafe<Bearer> {
     std::unique_ptr<async::Task> timeout_task_;
   };
 
-  bool SendInternal(std::unique_ptr<common::ByteBuffer> pdu,
+  bool SendInternal(common::ByteBufferPtr pdu,
                     const TransactionCallback& callback,
                     const ErrorCallback& error_callback);
 