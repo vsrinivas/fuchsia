7697c4dfd5189a9ba37ce5c5150768c6a41ec04c
Abdulla Kamar <abdulla@google.com>
2017-12-03 12:11:44 -0800

    [guest][scripts] Add support for arm64 to mklinux
    This should allow us to compile for arm64 or x86, no matter what the
    host platform is.
    
    ZX-1430 #comment
    
    Change-Id: I40309b9f50f44fa8e069c59bcfd108fe66d194a4

--- bin/guest/scripts/mklinux.sh
+++ bin/guest/scripts/mklinux.sh
@@ -10,27 +10,47 @@
 
 set -e
 
-LINUXDIR=/tmp/linux
-DEFCONFIG=machina_defconfig
+usage() {
+  echo "usage: ${0} [options] {arm64, x86}"
+  echo
+  echo "  -d [defconfig]  Defconfig to use"
+  echo "  -l [linux-dir]  Linux source dir"
+  echo
+  exit 1
+}
 
 while getopts "c:d:" OPT; do
   case $OPT in
-    c) DEFCONFIG="$OPTARG" ;;
-    d) LINUXDIR="$OPTARG" ;;
+  d) DEFCONFIG="${OPTARG}";;
+  l) LINUX_DIR="${OPTARG}";;
+  *) usage;;
   esac
 done
 
-echo "Building linux with $DEFCONFIG in $LINUXDIR"
+case "${1}" in
+arm64)
+  declare -x ARCH=arm64;
+  declare -x CROSS_COMPILE=aarch64-linux-gnu-;;
+x86)
+  declare -x ARCH=x86;
+  declare -x CROSS_COMPILE=x86_64-linux-gnu-;;
+*)
+  usage;;
+esac
+
+declare -r LINUX_DIR=${LINUX_DIR:-/tmp/linux}
+declare -r DEFCONFIG=${DEFCONFIG:-machina_defconfig}
+echo "Building Linux with ${DEFCONFIG} in ${LINUX_DIR}"
 
 # Shallow clone the repository.
-if [ ! -d "$LINUXDIR" ]; then
-  git clone --depth 1 https://zircon-guest.googlesource.com/third_party/linux "$LINUXDIR"
+if [ ! -d "${LINUX_DIR}" ]; then
+  git clone --depth 1 https://zircon-guest.googlesource.com/third_party/linux "${LINUX_DIR}"
 fi
 
 # Update the repository.
-cd "$LINUXDIR"
+cd "${LINUX_DIR}"
 git pull
 
 # Build Linux.
-make "$DEFCONFIG"
+make "${DEFCONFIG}"
 make -j $(getconf _NPROCESSORS_ONLN)
