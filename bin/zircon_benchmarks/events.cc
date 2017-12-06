e9d9a371b38ad93a1b5f5336716c3cdb1f21601c
Ruchira Ravoori <ravoorir@ravoorir.mtv.corp.google.com>
2017-12-06 12:38:19 -0800

    Add benchmark support for duplicate/replace handles and waitforone/many objects
    Change-Id: I8df78de410a2d6fae3a6a827c325046bcd712ac6

--- bin/zircon_benchmarks/events.cc
+++ bin/zircon_benchmarks/events.cc
@@ -48,9 +48,93 @@ BENCHMARK_F(Event, Close)(benchmark::State& state) {
 }
 
 BENCHMARK_F(Event, Signal)(benchmark::State& state) {
-   while (state.KeepRunning()) {
+  while (state.KeepRunning()) {
     if (zx_object_signal(event, 0, 0) != 0) {
       state.SkipWithError("Failed to signal event");
+      return;
+    }
+  }
+}
+
+BENCHMARK_F(Event, Duplicate)(benchmark::State& state) {
+  zx_handle_t dup_event;
+  while (state.KeepRunning()) {
+    if (zx_handle_duplicate(event, ZX_RIGHT_SAME_RIGHTS, &dup_event) != 0) {
+      state.SkipWithError("Failed to duplicate event");
+      return;
+    }
+    state.PauseTiming();
+    zx_handle_close(dup_event);
+    state.ResumeTiming();
+  }
+}
+
+BENCHMARK_F(Event, Replace)(benchmark::State& state) {
+  zx_handle_t dup_event;
+  zx_handle_t replaced_event;
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    if (zx_handle_duplicate(event, ZX_RIGHT_SAME_RIGHTS, &dup_event) != 0) {
+      state.SkipWithError("Failed to duplicate event");
+      return;
+    }
+    state.ResumeTiming();
+    if (zx_handle_replace(dup_event, ZX_RIGHT_SAME_RIGHTS, &replaced_event) != 0) {
+      zx_handle_close(dup_event);
+      state.SkipWithError("Failed to replace event");
+      return;
+    }
+    state.PauseTiming();
+    zx_handle_close(replaced_event);
+    state.ResumeTiming();
+  }
+}
+
+class WaitItems : public benchmark::Fixture {
+ private:
+  void SetUp(benchmark::State& state) override {
+    for (size_t i = 0; i < num_items; i++) {
+      if (zx_event_create(0, &(items[i].handle)) != 0) {
+        state.SkipWithError("Failed to create event");
+        return;
+      }
+      items[i].waitfor = ZX_EVENT_SIGNALED;
+    }
+  }
+
+  void TearDown(benchmark::State& state) override {
+    for (size_t i = 0; i < num_items; i++) {
+      zx_handle_close(items[i].handle);
+    }
+  }
+
+ protected:
+  static constexpr size_t num_items = 4u;
+  zx_wait_item_t items[num_items];
+};
+
+BENCHMARK_F(WaitItems, WaitForAlreadySignaledEvent)(benchmark::State& state) {
+  if (zx_object_signal(items[0].handle, 0u, ZX_EVENT_SIGNALED) != 0) {
+    state.SkipWithError("Failed to signal event");
+    return;
+  }
+  while (state.KeepRunning()) {
+    zx_signals_t pending = 0;
+    if (zx_object_wait_one(items[0].handle, ZX_EVENT_SIGNALED, 0u, &pending) != 0) {
+      state.SkipWithError("Failed to get signaled event");
+      return;
+    }
+  }
+}
+
+BENCHMARK_F(WaitItems, WaitForManyWithAlreadySignaledEvent)(benchmark::State& state) {
+  if (zx_object_signal(items[0].handle, 0u, ZX_EVENT_SIGNALED) != 0) {
+    state.SkipWithError("Failed to signal event");
+    return;
+  }
+  while (state.KeepRunning()) {
+    if (zx_object_wait_many(items, num_items, 0u) != 0) {
+      state.SkipWithError("Failed to get signaled event");
       return;
     }
   }
