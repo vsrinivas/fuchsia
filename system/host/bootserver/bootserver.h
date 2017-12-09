942aefaf31d4790814293275c0a71da18f175ff8
James Tucker <raggi@google.com>
2017-12-08 16:54:13 -0800

    [bootserver][tftp] use full ip packets by default
    Change-Id: Ib6caac09ea29c7815a518c95f12013d479fa7913

--- system/host/bootserver/bootserver.h
+++ system/host/bootserver/bootserver.h
@@ -9,7 +9,7 @@ void update_status(size_t bytes_so_far);
 int tftp_xfer(struct sockaddr_in6* addr, const char* fn, const char* name);
 int netboot_xfer(struct sockaddr_in6* addr, const char* fn, const char* name);
 
-#define DEFAULT_TFTP_BLOCK_SZ 1024
+#define DEFAULT_TFTP_BLOCK_SZ 1428
 #define DEFAULT_TFTP_WIN_SZ 1024
 #define DEFAULT_US_BETWEEN_PACKETS 20
 
