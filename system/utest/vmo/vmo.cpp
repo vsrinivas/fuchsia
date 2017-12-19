10cbccb6c535c62025c4826f545f2598c19fff3b
Travis Geiselbrecht <travisg@google.com>
2017-12-18 16:13:05 -0800

    [tests][vmo] add a long running test to try to detect unmap TLB coherency issues
    Actually doesn't seem to reproduce the problem, but is a decent stress
    test anyway.
    
    Change-Id: If86bc05f9578e6d6cd02a7bcd90c94796419828e

--- system/utest/vmo/vmo.cpp
+++ system/utest/vmo/vmo.cpp
@@ -8,12 +8,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <threads.h>
 
 #include <zircon/process.h>
 #include <zircon/syscalls.h>
 #include <zircon/syscalls/object.h>
 #include <fbl/algorithm.h>
+#include <fbl/atomic.h>
+#include <fbl/function.h>
 #include <pretty/hexdump.h>
 #include <unittest/unittest.h>
 
@@ -1309,6 +1313,100 @@ bool vmo_clone_rights_test() {
     END_TEST;
 }
 
+bool vmo_unmap_coherency() {
+    BEGIN_TEST;
+
+    // This is an expensive test to try to detect a multi-cpu coherency
+    // problem with TLB flushing of unmap operations
+    //
+    // algorithm: map a relatively large committed VMO.
+    // Create a worker thread that simply walks through the VMO writing to
+    // each page.
+    // In the main thread continually decommit the vmo with a little bit of
+    // a gap between decommits to allow the worker thread to bring it all back in.
+    // If the worker thread appears stuck by not making it through a loop in
+    // a reasonable time, we have failed.
+
+    // allocate a vmo
+    const size_t len = 32*1024*1024;
+    zx_handle_t vmo;
+    zx_status_t status = zx_vmo_create(len, 0, &vmo);
+    EXPECT_EQ(ZX_OK, status, "vm_object_create");
+
+    // do a regular map
+    uintptr_t ptr = 0;
+    status = zx_vmar_map(zx_vmar_root_self(), 0, vmo, 0, len,
+                         ZX_VM_FLAG_PERM_READ | ZX_VM_FLAG_PERM_WRITE,
+                         &ptr);
+    EXPECT_EQ(ZX_OK, status, "map");
+    EXPECT_NE(0u, ptr, "map address");
+
+    // create a worker thread
+    struct worker_args {
+        size_t len;
+        uintptr_t ptr;
+        fbl::atomic<bool> exit;
+        fbl::atomic<bool> exited;
+        fbl::atomic<size_t> count;
+    } args = {};
+    args.len = len;
+    args.ptr = ptr;
+
+    auto worker = [](void *_args) -> int {
+        worker_args* a = (worker_args*)_args;
+
+        unittest_printf("ptr %#" PRIxPTR " len %zu\n",
+                a->ptr, a->len);
+
+        while (!a->exit.load()) {
+            // walk through the mapping, writing to every page
+            for (size_t off = 0; off < a->len; off += PAGE_SIZE) {
+                *(uint32_t*)(a->ptr + off) = 99;
+            }
+
+            a->count.fetch_add(1);
+        }
+
+        unittest_printf("exiting worker\n");
+
+        a->exited.store(true);
+
+        return 0;
+    };
+
+    thrd_t t;
+    thrd_create(&t, worker, &args);
+
+    const zx_time_t max_duration = ZX_SEC(30);
+    const zx_time_t max_wait = ZX_SEC(1);
+    zx_time_t start = zx_time_get(ZX_CLOCK_MONOTONIC);
+    for (;;) {
+        // wait for it to loop at least once
+        zx_time_t t = zx_time_get(ZX_CLOCK_MONOTONIC);
+        size_t last_count = args.count.load();
+        while (args.count.load() <= last_count) {
+            if (zx_time_get(ZX_CLOCK_MONOTONIC) - t > max_wait) {
+                UNITTEST_FAIL_TRACEF("looper appears stuck!\n");
+                break;
+            }
+        }
+
+        // decommit the vmo
+        status = zx_vmo_op_range(vmo, ZX_VMO_OP_DECOMMIT, 0, len, nullptr, 0);
+        EXPECT_EQ(0, status, "vm decommit");
+
+        if (zx_time_get(ZX_CLOCK_MONOTONIC) - start > max_duration)
+            break;
+    }
+
+    // stop the thread and wait for it to exit
+    args.exit.store(true);
+    while (args.exited.load() == false)
+        ;
+
+    END_TEST;
+}
+
 BEGIN_TEST_CASE(vmo_tests)
 RUN_TEST(vmo_create_test);
 RUN_TEST(vmo_read_write_test);
@@ -1332,6 +1430,7 @@ RUN_TEST(vmo_clone_test_4);
 RUN_TEST(vmo_clone_decommit_test);
 RUN_TEST(vmo_clone_commit_test);
 RUN_TEST(vmo_clone_rights_test);
+RUN_TEST_LARGE(vmo_unmap_coherency);
 END_TEST_CASE(vmo_tests)
 
 int main(int argc, char** argv) {
