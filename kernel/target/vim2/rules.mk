e6e19820e185b503e4d12e762724cb247629824e
Mike Voydanoff <voydanoff@google.com>
2017-12-23 15:20:12 -0800

    [vim2] Add Khadas VIM2 build
    The VIM2 is very similar to the original VIM board,
    but has an Amlogic S912 instead of the S905X.
    
    Kernel configuration is identical, except VIM2 has a second CPU cluster
    and a different set of reserved ranges.
    
    system/dev/board/vim2 is identical to system/dev/board/vim,
    except the framebuffer address for the display driver hack is different.
    We will refactor the board drivers to avoid code duplication in a subsequent change.
    
    Change-Id: I65bd2620cf261975fa0854ef48f4e3f214e00e7e

---
+++ kernel/target/vim2/rules.mk
@@ -0,0 +1,19 @@
+# Copyright 2017 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+PLATFORM := generic-arm
+
+KERNEL_LOAD_OFFSET := 0x1080000
+
+include make/kernel-images.mk
+
+PLATFORM_VID := 4   # PDEV_VID_KHADAS
+PLATFORM_PID := 2   # PDEV_PID_VIM2
+PLATFORM_BOARD_NAME := vim2
+
+MDI_SRCS := $(LOCAL_DIR)/vim2.mdi
