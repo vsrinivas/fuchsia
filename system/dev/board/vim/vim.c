3e85ff18df3539f3935e899e76061c78c1f5714e
Mike Voydanoff <voydanoff@google.com>
2018-01-02 13:21:04 -0800

    [board][vim][vim2] Use same platform bus board driver for VIM and VIM2
    So far the only difference is the address of the framebuffer for the
    hacked display driver, so we can use the same driver for both boards.
    
    Change-Id: I485e487a7dc319aa2cb4a131f65e4b75d8cf149b

--- system/dev/board/vim/vim.c
+++ system/dev/board/vim/vim.c
@@ -23,20 +23,29 @@
 #include "vim.h"
 #include "vim-hw.h"
 
-static const pbus_mmio_t display_mmios[] = {
+// display MMIO for VIM board
+static const pbus_mmio_t vim_display_mmios[] = {
     {
         .base = 0x3d800000,
         .length = 1920 * 1080 * 2,
     },
 };
 
-static const pbus_dev_t display_dev = {
+// display MMIO for VIM2 board
+static const pbus_mmio_t vim2_display_mmios[] = {
+    {
+        .base = 0xbd851000,
+        .length = 1920 * 1080 * 2,
+    },
+};
+
+static pbus_dev_t display_dev = {
     .name = "display",
     .vid = PDEV_VID_KHADAS,
     .pid = PDEV_PID_VIM,
     .did = PDEV_PID_VIM_DISPLAY,
-    .mmios = display_mmios,
-    .mmio_count = countof(display_mmios),
+    .mmios = vim_display_mmios,
+    .mmio_count = countof(vim_display_mmios),
 };
 
 static zx_status_t vim_bus_get_protocol(void* ctx, uint32_t proto_id, void* out) {
@@ -118,6 +127,11 @@ static zx_status_t vim_bus_bind(void* ctx, zx_device_t* parent) {
         zxlogf(ERROR, "vim_usb_init failed: %d\n", status);
     }
 
+    // VIM2 board has different framebuffer address
+    if (!strcmp(pbus_get_board_name(&bus->pbus), "vim2")) {
+        display_dev.mmios = vim2_display_mmios;
+    }
+
     if ((status = pbus_device_add(&bus->pbus, &display_dev, 0)) != ZX_OK) {
         zxlogf(ERROR, "vim_usb_init could not add display_dev: %d\n", status);
         return status;
@@ -136,8 +150,9 @@ static zx_driver_ops_t vim_bus_driver_ops = {
     .bind = vim_bus_bind,
 };
 
-ZIRCON_DRIVER_BEGIN(vim_bus, vim_bus_driver_ops, "zircon", "0.1", 3)
+ZIRCON_DRIVER_BEGIN(vim_bus, vim_bus_driver_ops, "zircon", "0.1", 4)
     BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PLATFORM_BUS),
     BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_KHADAS),
     BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_PID, PDEV_PID_VIM),
+    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_PID, PDEV_PID_VIM2),
 ZIRCON_DRIVER_END(vim_bus)
