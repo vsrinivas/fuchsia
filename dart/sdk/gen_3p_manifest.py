582b46d5f4fbe07e982d473b1a4a8e787fa535ce
P.Y. Laligand <pylaligand@google.com>
2018-01-04 13:19:57 -0800

    [dart][sdk] Allow dart_library targets to be included in an SDK.
    DX-66
    
    Change-Id: I8a159c35b1ff098b319e98337b773ddfa0e64a7d

---
+++ dart/sdk/gen_3p_manifest.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import os
+import sys
+
+FUCHSIA_ROOT = os.path.dirname(  # $root
+    os.path.dirname(             # build
+    os.path.dirname(             # dart
+    os.path.dirname(             # sdk
+    os.path.abspath(__file__)))))
+
+sys.path += [os.path.join(FUCHSIA_ROOT, 'third_party', 'pyyaml', 'lib')]
+import yaml
+
+
+def main():
+    parser = argparse.ArgumentParser('Builds a manifest file with 3p deps')
+    parser.add_argument('--out',
+                        help='Path to the output file',
+                        required=True)
+    parser.add_argument('--name',
+                        help='Name of the original package',
+                        required=True)
+    parser.add_argument('--specs',
+                        help='Path to spec files of 3p dependencies',
+                        nargs='*')
+    args = parser.parse_args()
+
+    deps = {}
+    for spec in args.specs:
+        with open(spec, 'r') as spec_file:
+            manifest = yaml.safe_load(spec_file)
+            name = manifest['name']
+            if name.startswith('flutter'):
+                deps[name] = {
+                    'sdk': 'flutter',
+                }
+            else:
+                version = manifest['version']
+                deps[name] = '^%s' % version
+
+    manifest = {
+        'name': '%s_third_party' % args.name,
+        'dependencies': deps,
+    }
+    with open(args.out, 'w') as out_file:
+        yaml.dump(manifest, out_file, default_flow_style=False)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ dart/sdk/sort_deps.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+
+def print_list(name, content):
+    quoted = map(lambda c: '"%s"' % c, content)
+    print('%s = [%s]' % (name, ', '.join(quoted)))
+
+def main():
+    deps = sys.argv[1:]
+    def is_3p(dep):
+        return dep.startswith('//third_party')
+    print_list('third_party', filter(is_3p, deps))
+    print_list('local', filter(lambda d: not is_3p(d), deps))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
