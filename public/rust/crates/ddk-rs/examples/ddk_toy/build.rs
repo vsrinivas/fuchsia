c61b396afe822e1980311937d951f3cde6b805b8
Andrew Walbran <qwandor@google.com>
2018-01-11 16:15:30 +0000

    [rust][ddk] Fixing DDK fargo build after changes to add workspace.
    Change-Id: I915d427b4eb4748836ff10ef5e013181deac2e43

--- public/rust/crates/ddk-rs/examples/ddk_toy/build.rs
+++ public/rust/crates/ddk-rs/examples/ddk_toy/build.rs
@@ -2,20 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-extern crate gcc;
+extern crate cc;
 
 use std::env;
 
 // TODO(stange): Figure out how to avoid requiring users to use GCC to build
 // a binding.c
 fn main() {
-
     let fuchsia_root = match env::var("FUCHSIA_ROOT") {
         Ok(val) => val,
         Err(error) => panic!("Please set FUCHSIA_ROOT {}", error),
     };
 
-    gcc::Build::new()
+    cc::Build::new()
                  .file("src/binding.c")
                  .compiler(format!("{}/buildtools/linux-x64/clang/bin/clang", fuchsia_root))
                  .flag(format!("--sysroot={}/out/build-zircon/build-user-x86-64/sysroot", fuchsia_root).as_str())
