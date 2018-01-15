45c1df53cecb07cdb43c270ab0fc4c603c7f5515
Tim Kilbourn <tkilbourn@google.com>
2018-01-14 23:46:58 -0800

    [rust][wlanstack] Tokio file watching
    Proof-of-concept that uses the fuchsia-vfs-watcher crate to monitor for
    changes in the /dev/class/ethernet directory.
    
    Change-Id: I830459a74684bae7c9bd09ddde0daa54ac303e20

--- bin/wlanstack/src/main.rs
+++ bin/wlanstack/src/main.rs
@@ -2,6 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![deny(warnings)]
+
+extern crate failure;
+extern crate fuchsia_vfs_watcher;
+extern crate futures;
+extern crate tokio_core;
+
+use failure::{Error, ResultExt};
+use fuchsia_vfs_watcher::*;
+use futures::stream::Stream;
+use std::fs::File;
+use tokio_core::reactor;
+
+const ETH_PATH: &str = "/dev/class/ethernet";
+
 fn main() {
-    println!("Hello from wlanstack2!");
+    if let Err(e) = main_res() {
+        println!("Error: {:?}", e);
+    }
+}
+
+fn main_res() -> Result<(), Error> {
+    println!("Starting wlanstack2");
+
+    let mut core = reactor::Core::new().context("error creating event loop")?;
+    let handle = core.handle();
+
+    let dev_dir = File::open(ETH_PATH)?;
+    let w = Watcher::new(&dev_dir, &handle).context("error creating watcher")?.for_each(|msg| {
+        match msg.event {
+            WatchEvent::EXISTING => println!("{:?} existing", msg.filename),
+            WatchEvent::ADD_FILE => println!("{:?} added", msg.filename),
+            WatchEvent::REMOVE_FILE => println!("{:?} removed", msg.filename),
+            WatchEvent::IDLE => println!("idle"),
+            e => println!("unknown watch event: {:?}", e),
+        }
+        Ok(())
+    });
+
+    core.run(w).map_err(|e| e.into())
 }
