84445229ea9ac649923b1e4df611fe83612e859d
Raph Levien <raph@google.com>
2018-01-17 14:18:16 -0800

    [xi_editor] Re-enable xi core
    This patch adds a xi_core service. It also adds the xi-editor repo to
    the manifest and requires the third party crates updated (it contains
    a Cargo.lock change for that).
    
    Change-Id: I57c54a6bb086a3b29bfe5ce2d4e94d772e386633

---
+++ bin/xi_core/src/main.rs
@@ -0,0 +1,107 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Main process for Fuchsia builds - uses fidl rather than stdin / stdout
+
+#![deny(warnings)]
+#![feature(conservative_impl_trait)]
+
+extern crate xi_core_lib;
+extern crate xi_rpc;
+
+extern crate fuchsia_zircon as zx;
+extern crate mxruntime;
+extern crate fidl;
+extern crate byteorder;
+extern crate failure;
+extern crate futures;
+extern crate fuchsia_app as app;
+extern crate fuchsia_async as async;
+extern crate garnet_public_lib_xi_fidl;
+
+use std::io;
+use std::sync::Arc;
+use std::thread;
+
+use failure::{Error, ResultExt};
+use futures::future;
+
+use garnet_public_lib_xi_fidl::Json;
+
+use app::server::ServicesServer;
+
+use zx::{AsHandleRef, Signals, Socket, Status, Time};
+
+use xi_rpc::RpcLoop;
+use xi_core_lib::MainState;
+
+// TODO: this should be moved into fuchsia_zircon.
+pub struct BlockingSocket(Arc<Socket>);
+
+impl io::Read for BlockingSocket {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let wait_sigs = Signals::SOCKET_READABLE | Signals::SOCKET_PEER_CLOSED;
+        let signals = self.0.wait_handle(wait_sigs, Time::INFINITE)?;
+        if signals.contains(Signals::SOCKET_PEER_CLOSED) {
+            return Ok(0);
+        }
+        self.0.read(buf).or_else(|status|
+            if status == Status::PEER_CLOSED {
+                Ok(0)
+            } else {
+                Err(status.into())
+            }
+        )
+    }
+}
+
+impl io::Write for BlockingSocket {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.write(buf).map_err(Into::into)
+        // TODO: handle case where socket is full (wait and retry)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+fn editor_main(sock: Socket) {
+    eprintln!("editor_main");
+    let mut state = MainState::new();
+    let arc_sock = Arc::new(sock);
+    let my_in = io::BufReader::new(BlockingSocket(arc_sock.clone()));
+    let my_out = BlockingSocket(arc_sock);
+    let mut rpc_looper = RpcLoop::new(my_out);
+
+    let _ = rpc_looper.mainloop(|| my_in, &mut state);
+}
+
+fn json_server() -> impl Json::Server {
+    Json::Impl {
+        state: (),
+        connect_socket: |_state, socket| {
+            eprintln!("connect_socket");
+            let _ = thread::spawn(move || editor_main(socket));
+            future::ok(())
+        }
+    }
+}
+
+fn main() {
+    if let Err(e) = main_xi() {
+        eprintln!("xi-core: Error: {:?}", e);
+    }
+}
+
+fn main_xi() -> Result<(), Error> {
+    let mut executor = async::Executor::new().context("unable to create executor")?;
+
+    let server = ServicesServer::new().add_service(|| Json::Dispatcher(json_server()))
+    .start()
+    .map_err(|e| e.context("error starting service server"))?;
+
+    let n_threads = 2;
+    executor.run(server, n_threads)
+}
---
+++ public/rust/fidl_crates/garnet_public_lib_xi_fidl/src/lib.rs
@@ -0,0 +1,6 @@
+#![deny(warnings)]
+#![deny(missing_debug_implementations)]
+#![allow(unused_imports)]
+#![allow(unused_extern_crates)]
+
+include!(concat!(env!("FUCHSIA_GEN_ROOT"), "/garnet/public/lib/xi/fidl/garnet_public_lib_xi_fidl.rs"));
