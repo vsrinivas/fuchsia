0ec2ec55a813ccdc9562d9daabbcf1877b26fb53
Carlos Pizano <cpu@chromium.org>
2018-01-20 20:10:38 -0800

    [garnet][input] cleanup of input_reader
    -Demote DeviceInfo to struct, it was slacking
    -Make const stuff const
    -Remove std::bind() cause its evil
    -use set_trigger() instead of blowing away waiter
    -document a bit
    
    Change-Id: Ib0cce6f753d8340410a3f585808083ec40982c70

--- bin/ui/input_reader/input_reader.cc
+++ bin/ui/input_reader/input_reader.cc
@@ -9,43 +9,36 @@
 #include <zircon/device/display.h>
 #include "lib/fsl/tasks/message_loop.h"
 
-#define DEV_INPUT "/dev/class/input"
-#define DEV_CONSOLE "/dev/class/framebuffer"
-#define DEV_VC "000"
-
 namespace mozart {
 namespace input {
 
-InputReader::DeviceInfo::DeviceInfo(
-    std::unique_ptr<InputInterpreter> interpreter,
-    std::unique_ptr<async::AutoWait> waiter)
-    : interpreter_(std::move(interpreter)), waiter_(std::move(waiter)) {}
+constexpr char kInputDevPath[] = "/dev/class/input";
+constexpr char kFramebuffPath[] = "/dev/class/framebuffer";
+// The path to the virtual console should come from some system header
+// see  MZ-432.
+constexpr char kVirtConsole[] = "000";
 
-InputReader::DeviceInfo::~DeviceInfo() {}
+struct DeviceInfo {
+  std::unique_ptr<InputInterpreter> interpreter;
+  std::unique_ptr<async::AutoWait> waiter;
+};
 
 InputReader::InputReader(mozart::InputDeviceRegistry* registry,
                          bool ignore_console)
-    : registry_(registry), ignore_console_(ignore_console) {}
+    : registry_(registry), ignore_console_(ignore_console) {
+       FXL_CHECK(registry_);
+    }
 
-InputReader::~InputReader() {
-  while (!devices_.empty()) {
-    DeviceRemoved(devices_.begin()->first);
-  }
-}
+InputReader::~InputReader() {}
 
 void InputReader::Start() {
-  FXL_CHECK(registry_);
-
   device_watcher_ = fsl::DeviceWatcher::Create(
-      DEV_INPUT, [this](int dir_fd, std::string filename) {
-        std::unique_ptr<InputInterpreter> interpreter =
-            InputInterpreter::Open(dir_fd, filename, registry_);
-        if (interpreter)
-          DeviceAdded(std::move(interpreter));
+      kInputDevPath, [this](int dir_fd, std::string filename) {
+        DeviceAdded(InputInterpreter::Open(dir_fd, filename, registry_));
       });
 
   console_watcher_ = fsl::DeviceWatcher::Create(
-      DEV_CONSOLE, [this](int dir_fd, std::string filename) {
+      kFramebuffPath, [this](int dir_fd, std::string) {
         WatchDisplayOwnershipChanges(dir_fd);
       });
 }
@@ -53,7 +46,7 @@ void InputReader::Start() {
 // Register to receive notifications that display ownership has changed
 void InputReader::WatchDisplayOwnershipChanges(int dir_fd) {
   // Open gfx console's device and receive display_watcher through its ioctl
-  int gfx_console_fd = openat(dir_fd, DEV_VC, O_RDWR);
+  int gfx_console_fd = openat(dir_fd, kVirtConsole, O_RDWR);
   if (gfx_console_fd >= 0) {
     ssize_t result = ioctl_display_get_ownership_change_event(
         gfx_console_fd, &display_ownership_event_);
@@ -63,10 +56,9 @@ void InputReader::WatchDisplayOwnershipChanges(int dir_fd) {
       display_ownership_waiter_ = std::make_unique<async::AutoWait>(
           fsl::MessageLoop::GetCurrent()->async(), display_ownership_event_,
           signals);
-
       display_ownership_waiter_->set_handler(
-          std::bind(&InputReader::OnDisplayHandleReady, this,
-                    std::placeholders::_2, std::placeholders::_3));
+        fbl::BindMember(this, &InputReader::OnDisplayHandleReady));
+
       zx_status_t status = display_ownership_waiter_->Begin();
       FXL_CHECK(status == ZX_OK);
     } else {
@@ -76,32 +68,35 @@ void InputReader::WatchDisplayOwnershipChanges(int dir_fd) {
     }
     close(gfx_console_fd);
   } else {
-    FXL_DLOG(ERROR) << "Failed to open " << DEV_VC << ": errno=" << errno;
+    FXL_DLOG(ERROR) << "Failed to open " << kVirtConsole << ": errno=" << errno;
   }
 }
 
 void InputReader::DeviceRemoved(zx_handle_t handle) {
-  FXL_VLOG(1) << "Input device " << devices_.at(handle)->interpreter()->name()
+  FXL_VLOG(1) << "Input device " << devices_.at(handle)->interpreter->name()
               << " removed";
   devices_.erase(handle);
 }
 
 void InputReader::DeviceAdded(std::unique_ptr<InputInterpreter> interpreter) {
+  if (!interpreter)
+    return;
+
   FXL_VLOG(1) << "Input device " << interpreter->name() << " added ";
   zx_handle_t handle = interpreter->handle();
-  zx_signals_t signals = ZX_USER_SIGNAL_0;
 
   auto wait = std::make_unique<async::AutoWait>(
-      fsl::MessageLoop::GetCurrent()->async(), handle, signals);
-  wait->set_handler(std::bind(&InputReader::OnDeviceHandleReady, this, handle,
-                              std::placeholders::_2, std::placeholders::_3));
+      fsl::MessageLoop::GetCurrent()->async(), handle, ZX_USER_SIGNAL_0);
+
+  wait->set_handler([this, handle](
+    async_t*, zx_status_t status, const zx_packet_signal_t* signal) {
+     return OnDeviceHandleReady(handle, status, signal);
+  });
 
   zx_status_t status = wait->Begin();
   FXL_CHECK(status == ZX_OK);
 
-  std::unique_ptr<DeviceInfo> info =
-      std::make_unique<DeviceInfo>(std::move(interpreter), std::move(wait));
-  devices_.emplace(handle, std::move(info));
+  devices_.emplace(handle, new DeviceInfo {std::move(interpreter), std::move(wait)});
 }
 
 async_wait_result_t InputReader::OnDeviceHandleReady(
@@ -116,10 +111,10 @@ async_wait_result_t InputReader::OnDeviceHandleReady(
   }
 
   zx_signals_t pending = signal->observed;
-  InputInterpreter* interpreter = devices_[handle]->interpreter();
-
   FXL_DCHECK(pending & ZX_USER_SIGNAL_0);
-  bool ret = interpreter->Read(!display_owned_ && !ignore_console_);
+
+  auto discard = !(display_owned_ || ignore_console_);
+  bool ret = devices_[handle]->interpreter->Read(discard);
   if (!ret) {
     // This will destroy the waiter.
     DeviceRemoved(handle);
@@ -130,6 +125,7 @@ async_wait_result_t InputReader::OnDeviceHandleReady(
 }
 
 async_wait_result_t InputReader::OnDisplayHandleReady(
+    async_t* async,
     zx_status_t status,
     const zx_packet_signal_t* signal) {
   if (status != ZX_OK) {
@@ -142,23 +138,13 @@ async_wait_result_t InputReader::OnDisplayHandleReady(
   zx_signals_t pending = signal->observed;
   if (pending & ZX_USER_SIGNAL_0) {
     display_owned_ = false;
-    display_ownership_waiter_ = std::make_unique<async::AutoWait>(
-        fsl::MessageLoop::GetCurrent()->async(), display_ownership_event_,
-        ZX_USER_SIGNAL_1);
-    display_ownership_waiter_->set_handler(
-        std::bind(&InputReader::OnDisplayHandleReady, this,
-                  std::placeholders::_2, std::placeholders::_3));
-    zx_status_t waiter_status = display_ownership_waiter_->Begin();
+    display_ownership_waiter_->set_trigger(ZX_USER_SIGNAL_1);
+    auto waiter_status = display_ownership_waiter_->Begin();
     FXL_CHECK(waiter_status == ZX_OK);
   } else if (pending & ZX_USER_SIGNAL_1) {
     display_owned_ = true;
-    display_ownership_waiter_ = std::make_unique<async::AutoWait>(
-        fsl::MessageLoop::GetCurrent()->async(), display_ownership_event_,
-        ZX_USER_SIGNAL_0);
-    display_ownership_waiter_->set_handler(
-        std::bind(&InputReader::OnDisplayHandleReady, this,
-                  std::placeholders::_2, std::placeholders::_3));
-    zx_status_t waiter_status = display_ownership_waiter_->Begin();
+    display_ownership_waiter_->set_trigger(ZX_USER_SIGNAL_0);
+    auto waiter_status = display_ownership_waiter_->Begin();
     FXL_CHECK(waiter_status == ZX_OK);
   }
 
--- bin/ui/input_reader/input_reader.h
+++ bin/ui/input_reader/input_reader.h
@@ -18,28 +18,26 @@
 namespace mozart {
 namespace input {
 
+struct DeviceInfo;
+
+// InputReader does four things:
+// 1- Watches who owns the display, which can be us, or the console.
+// 2- Watches new devices that are added to dev/class/input and then
+//    create an InputInterpreter for each one.
+// 3- When the device is ready for read call InputInterpreter::Read()
+// 4- When devices are removed, undo #2 and #3.
+//
+// |ignore_console| in the ctor indicates that the reader will
+// process device input even if the console owns the display.
 class InputReader {
  public:
   InputReader(mozart::InputDeviceRegistry* registry,
               bool ignore_console = false);
   ~InputReader();
+
   void Start();
 
  private:
-  class DeviceInfo {
-   public:
-    DeviceInfo(std::unique_ptr<InputInterpreter> interpreter,
-               std::unique_ptr<async::AutoWait> waiter);
-    ~DeviceInfo();
-
-    InputInterpreter* interpreter() { return interpreter_.get(); }
-
-   private:
-    std::unique_ptr<InputInterpreter> interpreter_;
-    std::unique_ptr<async::AutoWait> waiter_;
-
-    FXL_DISALLOW_COPY_AND_ASSIGN(DeviceInfo);
-  };
 
   void WatchDisplayOwnershipChanges(int dir_fd);
 
@@ -49,17 +47,18 @@ class InputReader {
   async_wait_result_t OnDeviceHandleReady(zx_handle_t handle,
                                           zx_status_t status,
                                           const zx_packet_signal_t* signal);
-  async_wait_result_t OnDisplayHandleReady(zx_status_t status,
+  async_wait_result_t OnDisplayHandleReady(async_t* async,
+                                           zx_status_t status,
                                            const zx_packet_signal_t* signal);
 
-  mozart::InputDeviceRegistry* registry_;
+  mozart::InputDeviceRegistry* const registry_;
+  const bool ignore_console_;
 
   std::map<zx_handle_t, std::unique_ptr<DeviceInfo>> devices_;
   std::unique_ptr<fsl::DeviceWatcher> device_watcher_;
   std::unique_ptr<fsl::DeviceWatcher> console_watcher_;
   zx_handle_t display_ownership_event_;
   std::unique_ptr<async::AutoWait> display_ownership_waiter_;
-  bool ignore_console_;
   bool display_owned_ = true;
 
   FXL_DISALLOW_COPY_AND_ASSIGN(InputReader);
