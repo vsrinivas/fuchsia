80f923c36129c5d5e4921946e92a85efc966a360
P.Y. Laligand <pylaligand@google.com>
2018-01-21 00:48:57 -0800

    [sdk] Added a build step to check that SDK manifests are properly formatted.
    Now also installing SDK definitions under //out/<build-type>/sdks/<sdk-name>.
    
    TO-641
    
    Change-Id: I40486ec33f7dd75e4987011c0da800f6862ee030

---
+++ tools/README.md
@@ -0,0 +1,3 @@
+This directory contains a collection of tools used in the GN build:
+- [JSON validator](json_validator/): verifies that JSON files match a given
+  schema.
---
+++ tools/json_validator/main.cc
@@ -0,0 +1,69 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fstream>
+#include <string>
+
+#include "rapidjson/document.h"
+#include "rapidjson/schema.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
+
+// Reads the content of a file into a JSON document.
+int ReadDocument(std::string file, Document* document) {
+  std::ifstream file_stream(file);
+  if (!file_stream.is_open()) {
+    fprintf(stderr, "Error: unable to open file %s.\n", file.c_str());
+    return 1;
+  }
+  std::string content((std::istreambuf_iterator<char>(file_stream)),
+                       std::istreambuf_iterator<char>());
+  file_stream.close();
+  if (document->Parse(content).HasParseError()) {
+    fprintf(stderr, "Error: unable to parse JSON in file %s.\n", file.c_str());
+    return 1;
+  }
+  return 0;
+}
+
+int main(int argc, const char** argv) {
+  if (argc != 4) {
+    printf("Usage: %s <schema> <file> <stamp>\n", argv[0]);
+    return 1;
+  }
+  std::string schema_path = argv[1];
+  std::string file_path = argv[2];
+  std::string stamp_path = argv[3];
+
+  Document schema_document;
+  if (ReadDocument(schema_path, &schema_document) != 0) {
+    return 1;
+  }
+  Document file_document;
+  if (ReadDocument(file_path, &file_document) != 0) {
+    return 1;
+  }
+
+  SchemaDocument schema(schema_document);
+  SchemaValidator validator(schema);
+  if (!file_document.Accept(validator)) {
+    fprintf(stderr, "Error: the file %s is invalid according to schema %s.\n",
+            file_path.c_str(), schema_path.c_str());
+    StringBuffer sb;
+    validator.GetInvalidSchemaPointer().StringifyUriFragment(sb);
+    fprintf(stderr, " - location in schema     %s\n", sb.GetString());
+    fprintf(stderr, " - affected keyword       %s\n",
+            validator.GetInvalidSchemaKeyword());
+    sb.Clear();
+    validator.GetInvalidDocumentPointer().StringifyUriFragment(sb);
+    fprintf(stderr, " - document reference     %s\n", sb.GetString());
+    return 1;
+  }
+
+  std::ofstream stamp(stamp_path);
+  stamp.close();
+
+  return 0;
+}
