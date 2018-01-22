06f7de06c565728cfe175183a09275e371bb6d6a
Maria Glukhova <mariagl@google.com>
2018-01-22 10:53:13 +0100

    [ledger] Add a backlog benchmark.
    This benchmark models the situation when the new connection opens to the
    page that already has a lot of data (commits, entries) and measures the
    time it takes to download all these changes.
    
    Some specific cases are covered by tspec files:
     * page with one big entry
     * page with one big entry and updates to it
     * page with many big entries
     * page with many small entries (currently doesn't work due to LE-401)
     * page with one small entry and many commits to it (currently doesn't
     work due to LE-397).
    
    LE-394 #done
    
    Change-Id: If2de5b02a3e3dae2601f78d7ca39bc4fcdf37940

---
+++ bin/ledger/tests/benchmark/backlog/backlog.cc
@@ -0,0 +1,307 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "peridot/bin/ledger/tests/benchmark/backlog/backlog.h"
+
+#include <iostream>
+//#include <cmath>
+
+#include <trace/event.h>
+
+#include "lib/cloud_provider/fidl/cloud_provider.fidl.h"
+#include "lib/fsl/tasks/message_loop.h"
+#include "lib/fsl/vmo/strings.h"
+#include "lib/fxl/command_line.h"
+#include "lib/fxl/files/directory.h"
+#include "lib/fxl/functional/make_copyable.h"
+#include "lib/fxl/logging.h"
+#include "lib/fxl/strings/string_number_conversions.h"
+#include "peridot/bin/ledger/testing/get_ledger.h"
+#include "peridot/bin/ledger/testing/quit_on_error.h"
+#include "peridot/bin/ledger/testing/run_with_tracing.h"
+#include "peridot/lib/callback/waiter.h"
+#include "peridot/lib/convert/convert.h"
+
+namespace {
+constexpr fxl::StringView kStoragePath = "/data/benchmark/ledger/backlog";
+constexpr fxl::StringView kUniqueKeyCountFlag = "unique-key-count";
+constexpr fxl::StringView kValueSizeFlag = "value-size";
+constexpr fxl::StringView kCommitCountFlag = "commit-count";
+constexpr fxl::StringView kRefsFlag = "refs";
+constexpr fxl::StringView kServerIdFlag = "server-id";
+
+constexpr fxl::StringView kRefsOnFlag = "on";
+constexpr fxl::StringView kRefsOffFlag = "off";
+
+constexpr size_t kKeySize = 64;
+const std::string kUserDirectory = "/backlog_user";
+
+void PrintUsage(const char* executable_name) {
+  std::cout << "Usage: " << executable_name << " --" << kUniqueKeyCountFlag
+            << "=<int> --" << kValueSizeFlag << "=<int> --" << kCommitCountFlag
+            << "=<int> --" << kRefsFlag << "=(" << kRefsOnFlag << "|"
+            << kRefsOffFlag << ") --" << kServerIdFlag << "=<string>"
+            << std::endl;
+}
+
+}  // namespace
+
+namespace test {
+namespace benchmark {
+
+BacklogBenchmark::BacklogBenchmark(
+    size_t unique_key_count,
+    size_t value_size,
+    size_t commit_count,
+    PageDataGenerator::ReferenceStrategy reference_strategy,
+    std::string server_id)
+    : application_context_(app::ApplicationContext::CreateFromStartupInfo()),
+      cloud_provider_firebase_factory_(application_context_.get()),
+      sync_watcher_binding_(this),
+      unique_key_count_(unique_key_count),
+      value_size_(value_size),
+      commit_count_(commit_count),
+      reference_strategy_(reference_strategy),
+      server_id_(std::move(server_id)),
+      writer_tmp_dir_(kStoragePath),
+      reader_tmp_dir_(kStoragePath),
+      done_writing_(false) {
+  FXL_DCHECK(unique_key_count_ > 0);
+  FXL_DCHECK(value_size_ > 0);
+  FXL_DCHECK(commit_count_ > 0);
+  cloud_provider_firebase_factory_.Init();
+}
+
+void BacklogBenchmark::SyncStateChanged(
+    ledger::SyncState download,
+    ledger::SyncState upload,
+    const SyncStateChangedCallback& callback) {
+  if (on_sync_state_changed_) {
+    on_sync_state_changed_(download, upload);
+  }
+  callback();
+}
+
+void BacklogBenchmark::Run() {
+  // Name of the storage directory currently identifies the user. Ensure the
+  // most nested directory has the same name to make the ledgers sync.
+  std::string writer_path = writer_tmp_dir_.path() + kUserDirectory;
+  FXL_DCHECK(files::CreateDirectory(writer_path));
+
+  cloud_provider::CloudProviderPtr cloud_provider_writer;
+  cloud_provider_firebase_factory_.MakeCloudProvider(
+      server_id_, "backlog", cloud_provider_writer.NewRequest());
+  ledger::Status status = test::GetLedger(
+      fsl::MessageLoop::GetCurrent(), application_context_.get(),
+      &writer_controller_, std::move(cloud_provider_writer), "backlog",
+      writer_path, &writer_);
+  QuitOnError(status, "Get writer ledger");
+
+  status =
+      test::GetPageEnsureInitialized(fsl::MessageLoop::GetCurrent(), &writer_,
+                                     nullptr, &writer_page_, &page_id_);
+  QuitOnError(status, "Writer page initialization");
+
+  WaitForWriterUpload();
+  TRACE_ASYNC_BEGIN("benchmark", "populate and upload", 0);
+  TRACE_ASYNC_BEGIN("benchmark", "populate", 0);
+  Populate();
+}
+
+void BacklogBenchmark::Populate() {
+  int transaction_size = static_cast<int>(
+      ceil(static_cast<double>(unique_key_count_) / commit_count_));
+  int key_count = std::max(unique_key_count_, commit_count_);
+  FXL_LOG(INFO) << "Transaction size: " << transaction_size
+                << ", key count: " << key_count << ".";
+  auto keys = generator_.MakeKeys(key_count, kKeySize, unique_key_count_);
+  page_data_generator_.Populate(
+      &writer_page_, std::move(keys), value_size_, transaction_size,
+      reference_strategy_, ledger::Priority::EAGER,
+      [this](ledger::Status status) {
+        if (status != ledger::Status::OK) {
+          benchmark::QuitOnError(status, "PageGenerator::Populate");
+          return;
+        }
+        done_writing_ = true;
+        TRACE_ASYNC_END("benchmark", "populate", 0);
+      });
+}
+
+void BacklogBenchmark::WaitForWriterUpload() {
+  on_sync_state_changed_ = [this](ledger::SyncState download,
+                                  ledger::SyncState upload) {
+    if ((upload == ledger::SyncState::IDLE) && done_writing_) {
+      on_sync_state_changed_ = nullptr;
+      TRACE_ASYNC_END("benchmark", "populate and upload", 0);
+      // Stop watching sync state for this page.
+      sync_watcher_binding_.Unbind();
+      ConnectReader();
+      return;
+    }
+  };
+  writer_page_->SetSyncStateWatcher(
+      sync_watcher_binding_.NewBinding(),
+      benchmark::QuitOnErrorCallback("Page::SetSyncStateWatcher"));
+}
+
+void BacklogBenchmark::ConnectReader() {
+  std::string reader_path = reader_tmp_dir_.path() + kUserDirectory;
+  FXL_DCHECK(files::CreateDirectory(reader_path));
+
+  cloud_provider::CloudProviderPtr cloud_provider_reader;
+  cloud_provider_firebase_factory_.MakeCloudProvider(
+      server_id_, "backlog", cloud_provider_reader.NewRequest());
+  ledger::Status status = test::GetLedger(
+      fsl::MessageLoop::GetCurrent(), application_context_.get(),
+      &reader_controller_, std::move(cloud_provider_reader), "backlog",
+      reader_path, &reader_);
+  QuitOnError(status, "ConnectReader");
+
+  TRACE_ASYNC_BEGIN("benchmark", "download", 0);
+  TRACE_ASYNC_BEGIN("benchmark", "get page", 0);
+  reader_->GetPage(std::move(page_id_), reader_page_.NewRequest(),
+                   [this](ledger::Status status) {
+                     if (benchmark::QuitOnError(status, "GetPage")) {
+                       return;
+                     }
+                     TRACE_ASYNC_END("benchmark", "get page", 0);
+                     WaitForReaderDownload();
+                   });
+}
+
+void BacklogBenchmark::WaitForReaderDownload() {
+  previous_state_ = ledger::SyncState::IDLE;
+  on_sync_state_changed_ = [this](ledger::SyncState download,
+                                  ledger::SyncState upload) {
+    if (download == ledger::SyncState::IDLE &&
+        previous_state_ != ledger::SyncState::IDLE) {
+      on_sync_state_changed_ = nullptr;
+      TRACE_ASYNC_END("benchmark", "download", 0);
+      GetReaderSnapshot();
+      return;
+    }
+    // Workaround to skip first (IDLE, IDLE) state before the download starts,
+    // see LE-369
+    previous_state_ = download;
+  };
+  reader_page_->SetSyncStateWatcher(
+      sync_watcher_binding_.NewBinding(),
+      benchmark::QuitOnErrorCallback("Page::SetSyncStateWatcher"));
+}
+
+void BacklogBenchmark::GetReaderSnapshot() {
+  reader_page_->GetSnapshot(reader_snapshot_.NewRequest(), nullptr, nullptr,
+                            benchmark::QuitOnErrorCallback("GetSnapshot"));
+  TRACE_ASYNC_BEGIN("benchmark", "get all entries", 0);
+  GetEntriesStep(nullptr, unique_key_count_);
+}
+
+void BacklogBenchmark::CheckStatusAndGetMore(ledger::Status status,
+                                             size_t entries_left,
+                                             fidl::Array<uint8_t> next_token) {
+  if ((status != ledger::Status::OK) &&
+      (status != ledger::Status::PARTIAL_RESULT)) {
+    QuitOnError(status, "PageSnapshot::GetEntries");
+  }
+  if (status == ledger::Status::OK) {
+    TRACE_ASYNC_END("benchmark", "get all entries", 0);
+    FXL_DCHECK(entries_left == 0);
+    FXL_DCHECK(!next_token);
+    ShutDown();
+    return;
+  }
+  FXL_DCHECK(next_token);
+  GetEntriesStep(std::move(next_token), entries_left);
+}
+
+void BacklogBenchmark::GetEntriesStep(fidl::Array<uint8_t> token,
+                                      size_t entries_left) {
+  FXL_DCHECK(entries_left > 0);
+  TRACE_ASYNC_BEGIN("benchmark", "get entries partial", entries_left);
+  if (reference_strategy_ == PageDataGenerator::ReferenceStrategy::INLINE) {
+    reader_snapshot_->GetEntriesInline(
+        nullptr, std::move(token),
+        fxl::MakeCopyable([this, entries_left](ledger::Status status,
+                                               auto entries,
+                                               auto next_token) mutable {
+          TRACE_ASYNC_END("benchmark", "get entries partial", entries_left);
+          CheckStatusAndGetMore(status, entries_left - entries.size(),
+                                std::move(next_token));
+        }));
+  } else {
+    reader_snapshot_->GetEntries(
+        nullptr, std::move(token),
+        fxl::MakeCopyable([this, entries_left](ledger::Status status,
+                                               auto entries,
+                                               auto next_token) mutable {
+          TRACE_ASYNC_END("benchmark", "get entries partial", entries_left);
+          CheckStatusAndGetMore(status, entries_left - entries.size(),
+                                std::move(next_token));
+        }));
+  }
+}
+
+void BacklogBenchmark::ShutDown() {
+  writer_controller_->Kill();
+  writer_controller_.WaitForResponseUntil(zx::deadline_after(zx::sec(5)));
+  reader_controller_->Kill();
+  reader_controller_.WaitForResponseUntil(zx::deadline_after(zx::sec(5)));
+
+  fsl::MessageLoop::GetCurrent()->PostQuitTask();
+}
+
+}  // namespace benchmark
+}  // namespace test
+
+int main(int argc, const char** argv) {
+  fxl::CommandLine command_line = fxl::CommandLineFromArgcArgv(argc, argv);
+
+  std::string unique_key_count_str;
+  size_t unique_key_count;
+  std::string value_size_str;
+  size_t value_size;
+  std::string commit_count_str;
+  size_t commit_count;
+  std::string reference_strategy_str;
+  std::string server_id;
+  if (!command_line.GetOptionValue(kUniqueKeyCountFlag.ToString(),
+                                   &unique_key_count_str) ||
+      !fxl::StringToNumberWithError(unique_key_count_str, &unique_key_count) ||
+      unique_key_count <= 0 ||
+      !command_line.GetOptionValue(kValueSizeFlag.ToString(),
+                                   &value_size_str) ||
+      !fxl::StringToNumberWithError(value_size_str, &value_size) ||
+      value_size <= 0 ||
+      !command_line.GetOptionValue(kCommitCountFlag.ToString(),
+                                   &commit_count_str) ||
+      !fxl::StringToNumberWithError(commit_count_str, &commit_count) ||
+      commit_count <= 0 ||
+      !command_line.GetOptionValue(kRefsFlag.ToString(),
+                                   &reference_strategy_str) ||
+      !command_line.GetOptionValue(kServerIdFlag.ToString(), &server_id)) {
+    PrintUsage(argv[0]);
+    return -1;
+  }
+
+  test::benchmark::PageDataGenerator::ReferenceStrategy reference_strategy;
+  if (reference_strategy_str == kRefsOnFlag) {
+    reference_strategy =
+        test::benchmark::PageDataGenerator::ReferenceStrategy::REFERENCE;
+  } else if (reference_strategy_str == kRefsOffFlag) {
+    reference_strategy =
+        test::benchmark::PageDataGenerator::ReferenceStrategy::INLINE;
+  } else {
+    std::cerr << "Unknown option " << reference_strategy_str << " for "
+              << kRefsFlag.ToString() << std::endl;
+    PrintUsage(argv[0]);
+    return -1;
+  }
+
+  fsl::MessageLoop loop;
+  test::benchmark::BacklogBenchmark app(unique_key_count, value_size,
+                                        commit_count, reference_strategy,
+                                        server_id);
+  return test::benchmark::RunWithTracing(&loop, [&app] { app.Run(); });
+}
---
+++ bin/ledger/tests/benchmark/backlog/backlog.h
@@ -0,0 +1,105 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PERIDOT_BIN_LEDGER_TESTS_BENCHMARK_BACKLOG_BACKLOG_H_
+#define PERIDOT_BIN_LEDGER_TESTS_BENCHMARK_BACKLOG_BACKLOG_H_
+
+#include <memory>
+#include <vector>
+
+#include "lib/app/cpp/application_context.h"
+#include "lib/fxl/files/scoped_temp_dir.h"
+#include "lib/ledger/fidl/ledger.fidl.h"
+#include "peridot/bin/ledger/testing/cloud_provider_firebase_factory.h"
+#include "peridot/bin/ledger/testing/data_generator.h"
+#include "peridot/bin/ledger/testing/page_data_generator.h"
+#include "peridot/lib/firebase_auth/testing/fake_token_provider.h"
+
+namespace test {
+namespace benchmark {
+
+// Benchmark that measures time taken by a page connection to upload all local
+// changes to the cloud; and for another connection to the same page to download
+// all these changes.
+//
+// In contrast to the sync benchmark, backlog benchmark initiates the second
+// connection only after the first one has uploaded all changes. It is designed
+// to model the situation of adding new device instead of continuous
+// synchronisation.
+//
+// Cloud sync needs to be configured on the device in order for the benchmark to
+// run.
+//
+// Parameters:
+//   --unique-key-count=<int> the number of unique keys to populate the page
+//   with.
+//   --value-size=<int> the size of values to populate the page with.
+//   --commit-count=<int> the number of commits made to the page.
+//   If this number is smaller than unique-key-count, changes will be bundled
+//   into transactions. If it is bigger, some or all of the changes will use the
+//   same keys, modifying the value.
+//   --refs=(on|off) reference strategy: on to put values as references, off to
+//     put them as FIDL arrays.
+//   --server-id=<string> the ID of the Firebase instance to use for syncing
+class BacklogBenchmark : public ledger::SyncWatcher {
+ public:
+  BacklogBenchmark(size_t unique_key_count,
+                   size_t value_size,
+                   size_t commit_count,
+                   PageDataGenerator::ReferenceStrategy reference_strategy,
+                   std::string server_id);
+
+  void Run();
+
+  // ledger::SyncWatcher:
+  void SyncStateChanged(ledger::SyncState download,
+                        ledger::SyncState upload,
+                        const SyncStateChangedCallback& callback) override;
+
+ private:
+  void Populate();
+  void WaitForWriterUpload();
+  void ConnectReader();
+  void WaitForReaderDownload();
+
+  void GetReaderSnapshot();
+  void GetEntriesStep(fidl::Array<uint8_t> token, size_t entries_left);
+  void CheckStatusAndGetMore(ledger::Status status,
+                             size_t entries_read,
+                             fidl::Array<uint8_t> next_token);
+
+  void ShutDown();
+
+  DataGenerator generator_;
+  PageDataGenerator page_data_generator_;
+  std::unique_ptr<app::ApplicationContext> application_context_;
+  CloudProviderFirebaseFactory cloud_provider_firebase_factory_;
+  fidl::Binding<ledger::SyncWatcher> sync_watcher_binding_;
+  const size_t unique_key_count_;
+  const size_t value_size_;
+  const size_t commit_count_;
+  const PageDataGenerator::ReferenceStrategy reference_strategy_;
+  const std::string server_id_;
+  files::ScopedTempDir writer_tmp_dir_;
+  files::ScopedTempDir reader_tmp_dir_;
+  app::ApplicationControllerPtr writer_controller_;
+  app::ApplicationControllerPtr reader_controller_;
+  ledger::LedgerPtr writer_;
+  ledger::LedgerPtr reader_;
+  fidl::Array<uint8_t> page_id_;
+  ledger::PagePtr writer_page_;
+  ledger::PagePtr reader_page_;
+  ledger::PageSnapshotPtr reader_snapshot_;
+  std::function<void(ledger::SyncState, ledger::SyncState)>
+      on_sync_state_changed_;
+  ledger::SyncState previous_state_;
+  bool done_writing_;
+
+  FXL_DISALLOW_COPY_AND_ASSIGN(BacklogBenchmark);
+};
+
+}  // namespace benchmark
+}  // namespace test
+
+#endif  // PERIDOT_BIN_LEDGER_TESTS_BENCHMARK_BACKLOG_BACKLOG_H_
