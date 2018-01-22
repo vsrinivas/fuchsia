3a95dc98c08a7efc28aacd8bbbab241cba59cf6c
Gary Miguel <garymm@google.com>
2018-01-22 13:20:13 -0800

    [ledger] Remove timeouts from LedgerStorageTest.
    LE-385
    
    Change-Id: Ie2edf4d144be202bfd1c9b2341f87d8572a0f2d4

--- bin/ledger/storage/impl/ledger_storage_unittest.cc
+++ bin/ledger/storage/impl/ledger_storage_unittest.cc
@@ -12,8 +12,11 @@
 #include "lib/fxl/macros.h"
 #include "peridot/bin/ledger/coroutine/coroutine_impl.h"
 #include "peridot/bin/ledger/encryption/fake/fake_encryption_service.h"
+#include "peridot/bin/ledger/testing/set_when_called.h"
 #include "peridot/lib/callback/capture.h"
 #include "peridot/lib/gtest/test_with_message_loop.h"
+
+using ledger::SetWhenCalled;
 
 namespace storage {
 namespace {
@@ -45,24 +48,27 @@ TEST_F(LedgerStorageTest, CreateGetCreatePageStorage) {
   PageId page_id = "1234";
   Status status;
   std::unique_ptr<PageStorage> page_storage;
-
+  bool called;
   storage_.GetPageStorage(
-      page_id, callback::Capture(MakeQuitTask(), &status, &page_storage));
-  EXPECT_FALSE(RunLoopWithTimeout());
+      page_id, callback::Capture(SetWhenCalled(&called), &status, &page_storage));
+  RunLoopUntilIdle();
+  ASSERT_TRUE(called);
   EXPECT_EQ(Status::NOT_FOUND, status);
   EXPECT_EQ(nullptr, page_storage);
 
   storage_.CreatePageStorage(
-      page_id, callback::Capture(MakeQuitTask(), &status, &page_storage));
-  EXPECT_FALSE(RunLoopWithTimeout());
+      page_id, callback::Capture(SetWhenCalled(&called), &status, &page_storage));
+  RunLoopUntilIdle();
+  ASSERT_TRUE(called);
   ASSERT_EQ(Status::OK, status);
   ASSERT_NE(nullptr, page_storage);
   ASSERT_EQ(page_id, page_storage->GetId());
 
   page_storage.reset();
   storage_.GetPageStorage(
-      page_id, callback::Capture(MakeQuitTask(), &status, &page_storage));
-  EXPECT_FALSE(RunLoopWithTimeout());
+      page_id, callback::Capture(SetWhenCalled(&called), &status, &page_storage));
+  RunLoopUntilIdle();
+  ASSERT_TRUE(called);
   EXPECT_EQ(Status::OK, status);
   EXPECT_NE(nullptr, page_storage);
 }
@@ -70,25 +76,29 @@ TEST_F(LedgerStorageTest, CreateGetCreatePageStorage) {
 TEST_F(LedgerStorageTest, CreateDeletePageStorage) {
   PageId page_id = "1234";
   Status status;
+  bool called;
   std::unique_ptr<PageStorage> page_storage;
   storage_.CreatePageStorage(
-      page_id, callback::Capture(MakeQuitTask(), &status, &page_storage));
-  EXPECT_FALSE(RunLoopWithTimeout());
+      page_id, callback::Capture(SetWhenCalled(&called), &status, &page_storage));
+  RunLoopUntilIdle();
+  ASSERT_TRUE(called);
   ASSERT_EQ(Status::OK, status);
   ASSERT_NE(nullptr, page_storage);
   ASSERT_EQ(page_id, page_storage->GetId());
   page_storage.reset();
 
   storage_.GetPageStorage(
-      page_id, callback::Capture(MakeQuitTask(), &status, &page_storage));
-  EXPECT_FALSE(RunLoopWithTimeout());
+      page_id, callback::Capture(SetWhenCalled(&called), &status, &page_storage));
+  RunLoopUntilIdle();
+  ASSERT_TRUE(called);
   EXPECT_EQ(Status::OK, status);
   EXPECT_NE(nullptr, page_storage);
 
   EXPECT_TRUE(storage_.DeletePageStorage(page_id));
   storage_.GetPageStorage(
-      page_id, callback::Capture(MakeQuitTask(), &status, &page_storage));
-  EXPECT_FALSE(RunLoopWithTimeout());
+      page_id, callback::Capture(SetWhenCalled(&called), &status, &page_storage));
+  RunLoopUntilIdle();
+  ASSERT_TRUE(called);
   EXPECT_EQ(Status::NOT_FOUND, status);
   EXPECT_EQ(nullptr, page_storage);
 }
