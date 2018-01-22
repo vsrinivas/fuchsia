6666e6e9955e6cbee1da4dcab3c8bc09ae243701
Mark Seaborn <mseaborn@google.com>
2018-01-22 12:43:16 -0800

    [kernel][cbuf] Remove unused cbuf functions
    The following were unused:
    
    cbuf_read()
    cbuf_peek_etc()
    cbuf_peek_write()
    cbuf_write_etc()
    cbuf_write()
    cbuf_advance_write()
    cbuf_space_used()
    cbuf_size()
    cbuf_reset()
    
    Change-Id: If3a30177fc7af5d4a3f1b6790698b8936578a6d1

--- kernel/lib/cbuf/include/lib/cbuf.h
+++ kernel/lib/cbuf/include/lib/cbuf.h
@@ -15,9 +15,6 @@
 
 __BEGIN_CDECLS
 
-#define CBUF_WRITE_FLAG_CANRESCHEDULE 0x1
-#define CBUF_WRITE_FLAG_NOFILL        0x2
-
 typedef struct cbuf {
     uint head;
     uint tail;
@@ -50,111 +47,6 @@ void cbuf_initialize(cbuf_t *cbuf, size_t len);
 void cbuf_initialize_etc(cbuf_t *cbuf, size_t len, void *buf);
 
 /**
- * cbuf_read
- *
- * Read up to buflen bytes in to the supplied buffer.
- *
- * @param[in] cbuf The cbuf instance to read from.
- * @param[in] buf A pointer to a buffer to read data into.  If NULL, cbuf_read
- * will skip up to the next buflen bytes from the cbuf.
- * @param[in] buflen The maximum number of bytes to read from the cbuf.
- * @param[in] block When true, will cause the caller to block until there is at
- * least one byte available to read from the cbuf.
- *
- * @return The actual number of bytes which were read (or skipped).
- */
-size_t cbuf_read(cbuf_t *cbuf, void *buf, size_t buflen, bool block);
-
-/**
- * cbuf_peek_etc
- *
- * Peek at the data available for read/write in the cbuf right now.  Fills out a
- * pair of iovec structures describing the (up to) two currently available
- * contiguous regions.  In the case of a read peak, this call does not actually
- * consume the data.
- *
- * @param[in] cbuf The cbuf instance to peek into.
- * @param[out] A pointer to two iovec structures to hold the description of the
- * contiguous regions.  NOTE: regions must point to a chunk of memory which is
- * at least sizeof(iovec_t) * 2 bytes long.
- * @param[in] writable When true, calculate the currently writable regions of
- * the cbuf.  Otherwise, calculate the currently readable regions of the cbuf.
- *
- * @return The total number of bytes which could be read/written.
- */
-size_t cbuf_peek_etc(cbuf_t *cbuf, iovec_t *regions, bool writable);
-
-/**
- * cbuf_peek
- *
- * @see cbuf_peek_etc
- *
- * Alias which peeks at the data available for read in the cbuf right now.
- */
-static inline size_t cbuf_peek(cbuf_t *cbuf, iovec_t *regions) {
-    return cbuf_peek_etc(cbuf, regions, false);
-}
-
-/**
- * cbuf_peek_write
- *
- * @see cbuf_peek_etc
- *
- * Alias which peeks at the space available for write in the cbuf right now.
- */
-static inline size_t cbuf_peek_write(cbuf_t *cbuf, iovec_t *regions) {
-    return cbuf_peek_etc(cbuf, regions, true);
-}
-
-/**
- * cbuf_write_etc
- *
- * Write up to len bytes from the supplied buffer into the cbuf.
- *
- * @param[in] cbuf The cbuf instance to write to.
- * @param[in] buf A pointer to a buffer to read data from.  If NULL, cbuf_write
- * will skip up to the next len bytes in the cbuf, filling with zeros instead of
- * supplied data.
- * @param[in] len The maximum number of bytes to write to the cbuf.
- * @param[in] flags May contain any valid combination of CBUF_WRITE_FLAGS.
- *   - CBUF_WRITE_FLAG_CANRECHEDULE Signals rescheduling policy passed through
- *     to the internal event when signaling the event to indicate that there is
- *     now data in the buffer to be read.
- *   - CBUF_WRITE_FLAG_NOFILL When set, and the buf parameter is NULL, do not
- *     fill the internal cbuf with zeros.  Instead, simply advance the write
- *     pointer.
- *
- * @return The number of bytes which were written (or skipped).
- */
-size_t cbuf_write_etc(cbuf_t *cbuf, const void *buf, size_t len, uint32_t flags);
-
-/**
- * cbuf_write
- *
- * @see cbuf_write_etc
- *
- * Alias which write (or zeros) len bytes to the cbuf.
- */
-static inline size_t cbuf_write(cbuf_t *cbuf, const void *buf, size_t len, bool reschedule) {
-    return cbuf_write_etc(cbuf, buf, len, reschedule ? CBUF_WRITE_FLAG_CANRESCHEDULE : 0);
-}
-
-/**
- * cbuf_advance_write
- *
- * @see cbuf_write_etc
- *
- * Alias which advances the cbuf write pointer, writing nothing to the
- * underlying buffer in the process.
- */
-static inline size_t cbuf_advance_write(cbuf_t *cbuf, size_t len, bool reschedule) {
-    return cbuf_write_etc(cbuf, NULL, len,
-                          reschedule
-                          ? CBUF_WRITE_FLAG_NOFILL | CBUF_WRITE_FLAG_CANRESCHEDULE
-                          : CBUF_WRITE_FLAG_NOFILL);
-}
-
-/**
  * cbuf_space_avail
  *
  * @param[in] cbuf The cbuf instance to query
@@ -163,41 +55,6 @@ static inline size_t cbuf_advance_write(cbuf_t *cbuf, size_t len, bool reschedul
  * number of bytes which can currently be written)
  */
 size_t cbuf_space_avail(cbuf_t *cbuf);
-
-/**
- * cbuf_space_used
- *
- * @param[in] cbuf The cbuf instance to query
- *
- * @return The number of used bytes in the cbuf (IOW - the maximum number of
- * bytes which can currently be read).
- */
-size_t cbuf_space_used(cbuf_t *cbuf);
-
-/**
- * cbuf_size
- *
- * @param[in] cbuf The cbuf instance to query
- *
- * @return The size of the cbuf's underlying data buffer.
- */
-static inline size_t cbuf_size(cbuf_t *cbuf)
-{
-    return (1UL << cbuf->len_pow2);
-}
-
-/**
- * cbuf_reset
- *
- * Reset the cbuf instance, discarding any data which may be in the buffer at
- * the moment.
- *
- * @param[in] cbuf The cbuf instance to reset.
- */
-static inline void cbuf_reset(cbuf_t *cbuf)
-{
-    cbuf_read(cbuf, NULL, cbuf_size(cbuf), false);
-}
 
 /* special cases for dealing with a single char of data */
 size_t cbuf_read_char(cbuf_t *cbuf, char *c, bool block);
