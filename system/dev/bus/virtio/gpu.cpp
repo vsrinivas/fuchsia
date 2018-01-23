6166348548c0c5ba869c420dc8cb151f91d1519f
Tim Detwiler <tjdetwiler@google.com>
2018-01-22 20:09:47 -0800

    [virtio][gpu] Code cleanup
    Change-Id: Ib141c88e9c6201c2a1def1f680c2e644514007cc

--- system/dev/bus/virtio/gpu.cpp
+++ system/dev/bus/virtio/gpu.cpp
@@ -4,31 +4,38 @@
 
 #include "gpu.h"
 
-#include <assert.h>
-#include <ddk/debug.h>
+#include <inttypes.h>
+#include <string.h>
+#include <sys/param.h>
+
 #include <ddk/debug.h>
 #include <fbl/auto_call.h>
 #include <fbl/auto_lock.h>
-#include <inttypes.h>
-#include <pretty/hexdump.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/param.h>
 #include <zircon/compiler.h>
 
 #include "trace.h"
 #include "utils.h"
-
 #include "virtio_gpu.h"
 
 #define LOCAL_TRACE 0
 
 namespace virtio {
 
+namespace {
+
+zx_status_t to_zx_status(uint32_t type) {
+    LTRACEF("response type %#x\n", type);
+    if (type != VIRTIO_GPU_RESP_OK_NODATA) {
+        return ZX_ERR_NO_MEMORY;
+    }
+    return ZX_OK;
+}
+
+} // namespace
+
 // DDK level ops
 
-// queue an iotxn. iotxn's are always completed by its complete() op
+// Queue an iotxn. iotxn's are always completed by its complete() op
 zx_status_t GpuDevice::virtio_gpu_set_mode(void* ctx, zx_display_info_t* info) {
     GpuDevice* gd = static_cast<GpuDevice*>(ctx);
 
@@ -91,68 +98,60 @@ GpuDevice::~GpuDevice() {
     cnd_destroy(&flush_cond_);
 }
 
-zx_status_t GpuDevice::send_command_response(const void* cmd, size_t cmd_len, void** _res, size_t res_len) {
-    LTRACEF("dev %p, cmd %p, cmd_len %zu, res %p, res_len %zu\n", this, cmd, cmd_len, _res, res_len);
+template <typename RequestType, typename ResponseType>
+void GpuDevice::send_command_response(const RequestType* cmd, ResponseType** res) {
+    size_t cmd_len = sizeof(RequestType);
+    size_t res_len = sizeof(ResponseType);
+    LTRACEF("dev %p, cmd %p, cmd_len %zu, res %p, res_len %zu\n", this, cmd, cmd_len, res, res_len);
 
-    /* Keep this single message at a time */
+    // Keep this single message at a time
     sem_wait(&request_sem_);
     fbl::MakeAutoCall([this]() { sem_post(&request_sem_); });
 
     uint16_t i;
     struct vring_desc* desc = vring_.AllocDescChain(2, &i);
-    assert(desc);
+    ZX_ASSERT(desc);
 
     memcpy(gpu_req_, cmd, cmd_len);
 
     desc->addr = gpu_req_pa_;
-    desc->len = (uint32_t)cmd_len;
-    desc->flags |= VRING_DESC_F_NEXT;
+    desc->len = static_cast<uint32_t>(cmd_len);
+    desc->flags = VRING_DESC_F_NEXT;
 
-    /* set the second descriptor to the response with the write bit set */
+    // Set the second descriptor to the response with the write bit set
     desc = vring_.DescFromIndex(desc->next);
-    assert(desc);
+    ZX_ASSERT(desc);
 
-    void* res = (void*)((uint8_t*)gpu_req_ + cmd_len);
-    *_res = res;
+    *res = reinterpret_cast<ResponseType*>(static_cast<uint8_t*>(gpu_req_) + cmd_len);
     zx_paddr_t res_phys = gpu_req_pa_ + cmd_len;
     memset(res, 0, res_len);
 
     desc->addr = res_phys;
-    desc->len = (uint32_t)res_len;
+    desc->len = static_cast<uint32_t>(res_len);
     desc->flags = VRING_DESC_F_WRITE;
 
-    /* submit the transfer */
+    // Submit the transfer & wait for the response
     vring_.SubmitChain(i);
-
-    /* kick it off */
     vring_.Kick();
-
-    /* wait for result */
     sem_wait(&response_sem_);
-
-    return ZX_OK;
 }
 
 zx_status_t GpuDevice::get_display_info() {
     LTRACEF("dev %p\n", this);
 
-    /* construct the get display info message */
+    // Construct the get display info message
     virtio_gpu_ctrl_hdr req;
     memset(&req, 0, sizeof(req));
     req.type = VIRTIO_GPU_CMD_GET_DISPLAY_INFO;
 
-    /* send the message and get a response */
+    // Send the message and get a response
     virtio_gpu_resp_display_info* info;
-    auto err = send_command_response(&req, sizeof(req), (void**)&info, sizeof(*info));
-    if (err < ZX_OK) {
-        return ZX_ERR_NOT_FOUND;
-    }
-
-    /* we got response */
+    send_command_response(&req, &info);
     if (info->hdr.type != VIRTIO_GPU_RESP_OK_DISPLAY_INFO) {
         return ZX_ERR_NOT_FOUND;
     }
 
+    // We got a response
     LTRACEF("response:\n");
     for (int i = 0; i < VIRTIO_GPU_MAX_SCANOUTS; i++) {
         if (info->pmodes[i].enabled) {
@@ -160,7 +159,7 @@ zx_status_t GpuDevice::get_display_info() {
                     info->pmodes[i].r.x, info->pmodes[i].r.y, info->pmodes[i].r.width, info->pmodes[i].r.height,
                     info->pmodes[i].flags);
             if (pmode_id_ < 0) {
-                /* save the first valid pmode we see */
+                // Save the first valid pmode we see
                 memcpy(&pmode_, &info->pmodes[i], sizeof(pmode_));
                 pmode_id_ = i;
             }
@@ -173,9 +172,9 @@ zx_status_t GpuDevice::get_display_info() {
 zx_status_t GpuDevice::allocate_2d_resource(uint32_t* resource_id, uint32_t width, uint32_t height) {
     LTRACEF("dev %p\n", this);
 
-    assert(resource_id);
+    ZX_ASSERT(resource_id);
 
-    /* construct the request */
+    // Construct the request
     virtio_gpu_resource_create_2d req;
     memset(&req, 0, sizeof(req));
 
@@ -186,24 +185,19 @@ zx_status_t GpuDevice::allocate_2d_resource(uint32_t* resource_id, uint32_t widt
     req.width = width;
     req.height = height;
 
-    /* send the command and get a response */
+    // Send the command and get a response
     struct virtio_gpu_ctrl_hdr* res;
-    auto err = send_command_response(&req, sizeof(req), (void**)&res, sizeof(*res));
-    assert(err == ZX_OK);
+    send_command_response(&req, &res);
 
-    /* see if we got a valid response */
-    LTRACEF("response type 0x%x\n", res->type);
-    err = (res->type == VIRTIO_GPU_RESP_OK_NODATA) ? ZX_OK : ZX_ERR_NO_MEMORY;
-
-    return err;
+    return to_zx_status(res->type);
 }
 
 zx_status_t GpuDevice::attach_backing(uint32_t resource_id, zx_paddr_t ptr, size_t buf_len) {
     LTRACEF("dev %p, resource_id %u, ptr %#" PRIxPTR ", buf_len %zu\n", this, resource_id, ptr, buf_len);
 
-    assert(ptr);
+    ZX_ASSERT(ptr);
 
-    /* construct the request */
+    // Construct the request
     struct {
         struct virtio_gpu_resource_attach_backing req;
         struct virtio_gpu_mem_entry mem;
@@ -217,22 +211,16 @@ zx_status_t GpuDevice::attach_backing(uint32_t resource_id, zx_paddr_t ptr, size
     req.mem.addr = ptr;
     req.mem.length = (uint32_t)buf_len;
 
-    /* send the command and get a response */
+    // Send the command and get a response
     struct virtio_gpu_ctrl_hdr* res;
-    auto err = send_command_response(&req, sizeof(req), (void**)&res, sizeof(*res));
-    assert(err == ZX_OK);
-
-    /* see if we got a valid response */
-    LTRACEF("response type 0x%x\n", res->type);
-    err = (res->type == VIRTIO_GPU_RESP_OK_NODATA) ? ZX_OK : ZX_ERR_NO_MEMORY;
-
-    return err;
+    send_command_response(&req, &res);
+    return to_zx_status(res->type);
 }
 
 zx_status_t GpuDevice::set_scanout(uint32_t scanout_id, uint32_t resource_id, uint32_t width, uint32_t height) {
     LTRACEF("dev %p, scanout_id %u, resource_id %u, width %u, height %u\n", this, scanout_id, resource_id, width, height);
 
-    /* construct the request */
+    // Construct the request
     virtio_gpu_set_scanout req;
     memset(&req, 0, sizeof(req));
 
@@ -243,22 +231,16 @@ zx_status_t GpuDevice::set_scanout(uint32_t scanout_id, uint32_t resource_id, ui
     req.scanout_id = scanout_id;
     req.resource_id = resource_id;
 
-    /* send the command and get a response */
+    // Send the command and get a response
     virtio_gpu_ctrl_hdr* res;
-    auto err = send_command_response(&req, sizeof(req), (void**)&res, sizeof(*res));
-    assert(err == ZX_OK);
-
-    /* see if we got a valid response */
-    LTRACEF("response type 0x%x\n", res->type);
-    err = (res->type == VIRTIO_GPU_RESP_OK_NODATA) ? ZX_OK : ZX_ERR_NO_MEMORY;
-
-    return err;
+    send_command_response(&req, &res);
+    return to_zx_status(res->type);
 }
 
 zx_status_t GpuDevice::flush_resource(uint32_t resource_id, uint32_t width, uint32_t height) {
     LTRACEF("dev %p, resource_id %u, width %u, height %u\n", this, resource_id, width, height);
 
-    /* construct the request */
+    // Construct the request
     virtio_gpu_resource_flush req;
     memset(&req, 0, sizeof(req));
 
@@ -268,22 +250,16 @@ zx_status_t GpuDevice::flush_resource(uint32_t resource_id, uint32_t width, uint
     req.r.height = height;
     req.resource_id = resource_id;
 
-    /* send the command and get a response */
+    // Send the command and get a response
     virtio_gpu_ctrl_hdr* res;
-    auto err = send_command_response(&req, sizeof(req), (void**)&res, sizeof(*res));
-    assert(err == ZX_OK);
-
-    /* see if we got a valid response */
-    LTRACEF("response type 0x%x\n", res->type);
-    err = (res->type == VIRTIO_GPU_RESP_OK_NODATA) ? ZX_OK : ZX_ERR_NO_MEMORY;
-
-    return err;
+    send_command_response(&req, &res);
+    return to_zx_status(res->type);
 }
 
 zx_status_t GpuDevice::transfer_to_host_2d(uint32_t resource_id, uint32_t width, uint32_t height) {
     LTRACEF("dev %p, resource_id %u, width %u, height %u\n", this, resource_id, width, height);
 
-    /* construct the request */
+    // Construct the request
     virtio_gpu_transfer_to_host_2d req;
     memset(&req, 0, sizeof(req));
 
@@ -294,16 +270,10 @@ zx_status_t GpuDevice::transfer_to_host_2d(uint32_t resource_id, uint32_t width,
     req.offset = 0;
     req.resource_id = resource_id;
 
-    /* send the command and get a response */
+    // Send the command and get a response
     virtio_gpu_ctrl_hdr* res;
-    auto err = send_command_response(&req, sizeof(req), (void**)&res, sizeof(*res));
-    assert(err == ZX_OK);
-
-    /* see if we got a valid response */
-    LTRACEF("response type 0x%x\n", res->type);
-    err = (res->type == VIRTIO_GPU_RESP_OK_NODATA) ? ZX_OK : ZX_ERR_NO_MEMORY;
-
-    return err;
+    send_command_response(&req, &res);
+    return to_zx_status(res->type);
 }
 
 void GpuDevice::Flush() {
@@ -324,40 +294,29 @@ void GpuDevice::virtio_gpu_flusher() {
 
         LTRACEF("flushing\n");
 
-        /* transfer to host 2d */
-        auto err = transfer_to_host_2d(display_resource_id_, pmode_.r.width, pmode_.r.height);
-        if (err < 0) {
+        zx_status_t status = transfer_to_host_2d(display_resource_id_, pmode_.r.width, pmode_.r.height);
+        if (status != ZX_OK) {
             LTRACEF("failed to flush resource\n");
             continue;
         }
 
-        /* resource flush */
-        err = flush_resource(display_resource_id_, pmode_.r.width, pmode_.r.height);
-        if (err < 0) {
+        status = flush_resource(display_resource_id_, pmode_.r.width, pmode_.r.height);
+        if (status != ZX_OK) {
             LTRACEF("failed to flush resource\n");
             continue;
         }
     }
 }
 
-int GpuDevice::virtio_gpu_flusher_entry(void* arg) {
-    GpuDevice* gd = static_cast<GpuDevice*>(arg);
-
-    gd->virtio_gpu_flusher();
-
-    return 0;
-}
-
 zx_status_t GpuDevice::virtio_gpu_start() {
-    zx_status_t err;
 
     LTRACEF("dev %p\n", this);
 
-    /* get the display info and see if we find a valid pmode */
-    err = get_display_info();
-    if (err < 0) {
+    // Get the display info and see if we find a valid pmode
+    zx_status_t status = get_display_info();
+    if (status != ZX_OK) {
         zxlogf(ERROR, "%s: failed to get display info\n", tag());
-        return err;
+        return status;
     }
 
     if (pmode_id_ < 0) {
@@ -369,38 +328,42 @@ zx_status_t GpuDevice::virtio_gpu_start() {
            pmode_.r.x, pmode_.r.y, pmode_.r.width, pmode_.r.height,
            pmode_.flags);
 
-    /* allocate a resource */
-    err = allocate_2d_resource(&display_resource_id_, pmode_.r.width, pmode_.r.height);
-    if (err < 0) {
+    // Allocate a resource
+    status = allocate_2d_resource(&display_resource_id_, pmode_.r.width, pmode_.r.height);
+    if (status != ZX_OK) {
         zxlogf(ERROR, "%s: failed to allocate 2d resource\n", tag());
-        return err;
+        return status;
     }
 
-    /* attach a backing store to the resource */
+    // Attach a backing store to the resource
     size_t len = pmode_.r.width * pmode_.r.height * 4;
 
-    err = map_contiguous_memory(len, (uintptr_t*)&fb_, &fb_pa_);
-    if (err < 0) {
+    status = map_contiguous_memory(len, reinterpret_cast<uintptr_t*>(&fb_), &fb_pa_);
+    if (status != ZX_OK) {
         zxlogf(ERROR, "%s: failed to allocate framebuffer, wanted 0x%zx bytes\n", tag(), len);
         return ZX_ERR_NO_MEMORY;
     }
 
     LTRACEF("framebuffer at %p, 0x%zx bytes\n", fb_, len);
 
-    err = attach_backing(display_resource_id_, fb_pa_, len);
-    if (err < 0) {
+    status = attach_backing(display_resource_id_, fb_pa_, len);
+    if (status != ZX_OK) {
         zxlogf(ERROR, "%s: failed to attach backing store\n", tag());
-        return err;
+        return status;
     }
 
-    /* attach this resource as a scanout */
-    err = set_scanout(pmode_id_, display_resource_id_, pmode_.r.width, pmode_.r.height);
-    if (err < 0) {
+    // Attach this resource as a scanout
+    status = set_scanout(pmode_id_, display_resource_id_, pmode_.r.width, pmode_.r.height);
+    if (status != ZX_OK) {
         zxlogf(ERROR, "%s: failed to set scanout\n", tag());
-        return err;
+        return status;
     }
 
-    // run a worker thread to shove in flush events
+    // Run a worker thread to shove in flush events
+    auto virtio_gpu_flusher_entry = [](void* arg) {
+        static_cast<GpuDevice*>(arg)->virtio_gpu_flusher();
+        return 0;
+    };
     thrd_create_with_name(&flush_thread_, virtio_gpu_flusher_entry, this, "virtio-gpu-flusher");
     thrd_detach(flush_thread_);
 
@@ -411,9 +374,8 @@ zx_status_t GpuDevice::virtio_gpu_start() {
     display_proto_ops_.get_framebuffer = virtio_gpu_get_framebuffer;
     display_proto_ops_.flush = virtio_gpu_flush;
 
-    // initialize the zx_device and publish us
-    // point the ctx of our DDK device at ourself
-
+    // Initialize the zx_device and publish us
+    // Point the ctx of our DDK device at ourself
     device_add_args_t args = {};
     args.version = DEVICE_ADD_ARGS_VERSION;
     args.name = "virtio-gpu";
@@ -422,30 +384,19 @@ zx_status_t GpuDevice::virtio_gpu_start() {
     args.proto_id = ZX_PROTOCOL_DISPLAY;
     args.proto_ops = &display_proto_ops_;
 
-    auto status = device_add(bus_device_, &args, &bus_device_);
-    if (status < 0) {
+    status = device_add(bus_device_, &args, &bus_device_);
+    if (status != ZX_OK) {
         device_ = nullptr;
         return status;
     }
 
     LTRACE_EXIT;
-
     return ZX_OK;
-}
-
-int GpuDevice::virtio_gpu_start_entry(void* arg) {
-
-    GpuDevice* gd = static_cast<GpuDevice*>(arg);
-
-    gd->virtio_gpu_start();
-
-    return 0;
 }
 
 zx_status_t GpuDevice::Init() {
     LTRACE_ENTRY;
 
-    // reset the device
     DeviceReset();
 
     struct virtio_gpu_config config;
@@ -455,34 +406,34 @@ zx_status_t GpuDevice::Init() {
     LTRACEF("num_scanouts 0x%x\n", config.num_scanouts);
     LTRACEF("reserved 0x%x\n", config.reserved);
 
-    // ack and set the driver status bit
+    // Ack and set the driver status bit
     DriverStatusAck();
 
     // XXX check features bits and ack/nak them
 
-    // allocate the main vring
-    auto err = vring_.Init(0, 16);
-    if (err < 0) {
+    // Allocate the main vring
+    zx_status_t status = vring_.Init(0, 16);
+    if (status != ZX_OK) {
         zxlogf(ERROR, "%s: failed to allocate vring\n", tag());
-        return err;
+        return status;
     }
 
-    // allocate a gpu request
-    auto r = map_contiguous_memory(PAGE_SIZE, (uintptr_t*)&gpu_req_, &gpu_req_pa_);
-    if (r < 0) {
-        zxlogf(ERROR, "%s: cannot alloc gpu_req buffers %d\n", tag(), r);
-        return r;
+    // Allocate a GPU request
+    status = map_contiguous_memory(PAGE_SIZE, (uintptr_t*)&gpu_req_, &gpu_req_pa_);
+    if (status != ZX_OK) {
+        zxlogf(ERROR, "%s: cannot alloc gpu_req buffers %d\n", tag(), status);
+        return status;
     }
 
     LTRACEF("allocated gpu request at %p, physical address %#" PRIxPTR "\n", gpu_req_, gpu_req_pa_);
 
-    // start the interrupt thread
     StartIrqThread();
-
-    // set DRIVER_OK
     DriverStatusOk();
 
-    // start a worker thread that runs through a sequence to finish initializing the gpu
+    // Start a worker thread that runs through a sequence to finish initializing the GPU
+    auto virtio_gpu_start_entry = [](void* arg) {
+        return static_cast<GpuDevice*>(arg)->virtio_gpu_start();
+    };
     thrd_create_with_name(&start_thread_, virtio_gpu_start_entry, this, "virtio-gpu-starter");
     thrd_detach(start_thread_);
 
@@ -492,34 +443,33 @@ zx_status_t GpuDevice::Init() {
 void GpuDevice::IrqRingUpdate() {
     LTRACE_ENTRY;
 
-    // parse our descriptor chain, add back to the free queue
+    // Parse our descriptor chain, add back to the free queue
     auto free_chain = [this](vring_used_elem* used_elem) {
-        uint32_t i = (uint16_t)used_elem->id;
-        struct vring_desc* desc = vring_.DescFromIndex((uint16_t)i);
-        __UNUSED auto head_desc = desc; // save the first element
+        uint16_t i = static_cast<uint16_t>(used_elem->id);
+        struct vring_desc* desc = vring_.DescFromIndex(i);
         for (;;) {
             int next;
 
             if (desc->flags & VRING_DESC_F_NEXT) {
                 next = desc->next;
             } else {
-                /* end of chain */
+                // End of chain
                 next = -1;
             }
 
-            vring_.FreeDesc((uint16_t)i);
+            vring_.FreeDesc(i);
 
-            if (next < 0)
+            if (next < 0) {
                 break;
-            i = next;
-            desc = vring_.DescFromIndex((uint16_t)i);
+            }
+            i = static_cast<uint16_t>(next);
+            desc = vring_.DescFromIndex(i);
         }
-
-        // notify the request thread
+        // Notify the request thread
         sem_post(&response_sem_);
     };
 
-    // tell the ring to find free chains and hand it back to our lambda
+    // Tell the ring to find free chains and hand it back to our lambda
     vring_.IrqRingUpdate(free_chain);
 }
 
--- system/dev/bus/virtio/gpu.h
+++ system/dev/bus/virtio/gpu.h
@@ -1,18 +1,19 @@
 // Copyright 2016 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #pragma once
+
+#include <semaphore.h>
+#include <stdlib.h>
+
+#include <ddk/protocol/display.h>
+#include <fbl/unique_ptr.h>
+#include <zircon/compiler.h>
 
 #include "device.h"
 #include "ring.h"
 #include "virtio_gpu.h"
-
-#include <fbl/unique_ptr.h>
-#include <semaphore.h>
-#include <stdlib.h>
-#include <zircon/compiler.h>
-
-#include <ddk/protocol/display.h>
 
 namespace virtio {
 
@@ -42,8 +43,10 @@ private:
     static zx_status_t virtio_gpu_get_framebuffer(void* ctx, void** framebuffer);
     static void virtio_gpu_flush(void* ctx);
 
-    // internal routines
-    zx_status_t send_command_response(const void* cmd, size_t cmd_len, void** _res, size_t res_len);
+    // Internal routines
+    template <typename RequestType, typename ResponseType>
+    void send_command_response(const RequestType* cmd, ResponseType** res);
+
     zx_status_t get_display_info();
     zx_status_t allocate_2d_resource(uint32_t* resource_id, uint32_t width, uint32_t height);
     zx_status_t attach_backing(uint32_t resource_id, zx_paddr_t ptr, size_t buf_len);
@@ -52,7 +55,6 @@ private:
     zx_status_t transfer_to_host_2d(uint32_t resource_id, uint32_t width, uint32_t height);
 
     zx_status_t virtio_gpu_start();
-    static int virtio_gpu_start_entry(void* arg);
     thrd_t start_thread_ = {};
 
     // the main virtio ring
@@ -65,28 +67,24 @@ private:
     void* gpu_req_ = nullptr;
     zx_paddr_t gpu_req_pa_ = 0;
 
-    // a saved copy of the display
+    // A saved copy of the display
     virtio_gpu_resp_display_info::virtio_gpu_display_one pmode_ = {};
     int pmode_id_ = -1;
 
-    // resource id that is set as scanout
+    // Resource id that is set as scanout
     uint32_t display_resource_id_ = 0;
 
-    // next resource id
     uint32_t next_resource_id_ = -1;
 
-    // framebuffer
     void* fb_ = nullptr;
     zx_paddr_t fb_pa_ = 0;
 
-    // request condition
     fbl::Mutex request_lock_;
     sem_t request_sem_;
     sem_t response_sem_;
 
-    // flush thread
+    // Flush thread
     void virtio_gpu_flusher();
-    static int virtio_gpu_flusher_entry(void* arg);
     thrd_t flush_thread_ = {};
     fbl::Mutex flush_lock_;
     cnd_t flush_cond_ = {};
