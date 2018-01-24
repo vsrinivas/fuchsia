396e216c87c5c8d073c6b5b69c277403b77c02a1
Brian Goldman <bgoldman@google.com>
2018-01-24 13:33:19 -0800

    [lib] Document TestBackoff
    See: https://google.github.io/styleguide/cppguide.html#Class_Comments
    
    Change-Id: Iff28c23f30dc77bba79e14ff1eec937795030b6f

--- lib/backoff/testing/test_backoff.h
+++ lib/backoff/testing/test_backoff.h
@@ -11,6 +11,8 @@
 
 namespace backoff {
 
+// Implementation of backoff that always returns a time delta of zero, and keeps
+// track of method calls.
 class TestBackoff : public Backoff {
  public:
   TestBackoff();
@@ -20,11 +22,15 @@ class TestBackoff : public Backoff {
   fxl::TimeDelta GetNext() override;
   void Reset() override;
 
+  // Set a function to be notified when GetNext() is called.
   void SetOnGetNext(fxl::Closure on_get_next);
 
   fxl::TimeDelta backoff_to_return = fxl::TimeDelta::FromSeconds(0);
 
+  // Number of times GetNext() was called.
   int get_next_count = 0;
+
+  // Number of times Reset() was called.
   int reset_count = 0;
 
  private:
