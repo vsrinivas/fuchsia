56f8493ffbbb869eb0a68e7f00beb1391991fe28
Dustin Green <dustingreen@google.com>
2018-01-26 21:39:40 -0800

    Script for concatenating compile_commands.json(s).
    Change-Id: I584c0632d8a609f3c80aa453fa629fb3b00c8087

---
+++ editors/cat_compile_commands.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Logically concatenates given compile_commands.json files to stdout.
+
+Filenames of compile_commands.json files are provided as arguments.  A logical
+concatenation of all the compile commands is output to stdout.
+
+Example:
+    topaz$ ./scripts/cat_compile_commands.py zircon/compile_commands.json \
+    out/debug_x86-64/compile_commands.json > compile_commands.json
+
+The output is suitable for use with vscode + cquery which needs a single
+compile_commands.json for the whole editor workspace.  This way a single vscode
+workspace can handle zircon + other layers of fuchsia, by adding the topaz
+folder with the combined compile_commands.json in it.
+
+Todo:
+    * For zircon, in docs that mention "bear make -j20", see also this script.
+    * For rest of fuchsia, integrate something like
+      "~/topaz/buildtools/ninja -v -C \
+      /usr/local/google/home/dustingreen/topaz/out/debug-x86-64 \
+      -t compdb cc cxx objc objcxx x64-shared_cc x64-shared_cxx \
+      > ~/topaz/out/debug-x86-64/compile_commands.json
+      into the build, since it is fast.
+    * Document all the steps to get a nicely working vscode + cquery workspace
+      with zircon + the rest of fuchsia in a combined editor workspace.
+"""
+
+import json
+import sys
+
+
+def main():
+    """Cat compile_commands.json files given as args to stdout"""
+    data = []
+    for arg in sys.argv[1:]:
+        data += json.load(open(arg))
+    json.dump(data, sys.stdout, indent=True)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
