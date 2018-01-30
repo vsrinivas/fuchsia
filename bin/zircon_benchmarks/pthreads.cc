aad328b8e1203fe0ed3a17d795ab2b084b16efc5
Kendal Harland <kjharland@google.com>
2018-01-30 10:17:41 -0800

    [zircon_benchmarks] Add a basic pthread test
    Change-Id: I61a4801b28ee5d45bb4ea67720169a86c0dad26d

---
+++ bin/zircon_benchmarks/pthreads.cc
@@ -0,0 +1,30 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <pthread.h>
+
+#include "lib/fxl/logging.h"
+#include "test_runner.h"
+
+namespace {
+
+// A no-op helper entry-point for pthreads in this fixture.
+void* ExitImmediately(void* arg) {
+  return nullptr;
+}
+
+// Benchmark for creating and joining on a pthread with a body that does
+// nothing.
+void PThreadCreateAndJoinTest() {
+  pthread_t thread;
+  FXL_CHECK(pthread_create(&thread, nullptr, ExitImmediately, nullptr) == 0);
+  FXL_CHECK(pthread_join(thread, nullptr) == 0);
+}
+
+__attribute__((constructor))
+void RegisterTests() {
+  fbenchmark::RegisterTestFunc<PThreadCreateAndJoinTest>("PThreadCreateAndJoinTest");
+}
+
+}  // namespace
