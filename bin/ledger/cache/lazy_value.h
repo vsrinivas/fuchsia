c6b81b3ef5aa31488a1a274553f98d156be73cec
Benjamin Lerman <qsr@chromium.org>
2018-01-31 16:19:35 +0100

    [ledger] Add utility class to handle asynchronously generated lazy value.
    Change-Id: I743f3eeb160d05305a742502b4003d6384e2c046

---
+++ bin/ledger/cache/lazy_value.h
@@ -0,0 +1,72 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PERIDOT_BIN_LEDGER_CACHE_LAZY_VALUE_H_
+#define PERIDOT_BIN_LEDGER_CACHE_LAZY_VALUE_H_
+
+#include <functional>
+#include <vector>
+
+namespace cache {
+// Implements a self-populating lazy value.
+//
+// This class allows to setup a lazy value that will be generated asynchronously
+// the first time it is requested.
+//
+// V is the type of the lazy value. V must be default-constructible and either
+//   copyable or movable.
+// S is the type of the success status for the data generator.
+template <typename V, typename S>
+class LazyValue {
+ public:
+  // Constructor.
+  //
+  // |ok_status| is the success status of the generator.
+  // |generator| generates the value. It takes a callback to returns its result.
+  //   It must return |ok_status| as a status when the request is successful.
+  //   Any other return value is considered a failure.
+  LazyValue(S ok_status,
+            std::function<void(std::function<void(S, V)>)> generator)
+      : ok_status_(ok_status),
+        generator_(std::move(generator)),
+        value_set_(false) {}
+
+  // Retrieves the value and returns it to |callback|.
+  //
+  // If the value is cached, |callback| will be called synchronously. Otherwise,
+  // |generator| will be called, and depending on its implementation, |callback|
+  // might be called synchronously or not.
+  void Get(const std::function<void(S, const V&)>& callback) {
+    if (value_set_) {
+      callback(ok_status_, value_);
+      return;
+    }
+    requests_.push_back(callback);
+    if (requests_.size() == 1) {
+      generator_([this](S status, V value) {
+        auto callbacks = std::move(requests_);
+        requests_.clear();
+
+        if (status == ok_status_) {
+          value_ = std::move(value);
+          value_set_ = true;
+        }
+        for (const auto& callback : callbacks) {
+          callback(status, value_);
+        }
+      });
+    }
+  }
+
+ private:
+  S ok_status_;
+  std::function<void(std::function<void(S, V)>)> generator_;
+  V value_;
+  bool value_set_;
+  std::vector<std::function<void(S, const V&)>> requests_;
+};
+
+}  // namespace cache
+
+#endif  // PERIDOT_BIN_LEDGER_CACHE_LRU_CACHE_H_
---
+++ bin/ledger/cache/lazy_value_unittest.cc
@@ -0,0 +1,96 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "peridot/bin/ledger/cache/lazy_value.h"
+
+#include "peridot/lib/callback/capture.h"
+#include "peridot/lib/callback/set_when_called.h"
+
+#include "gtest/gtest.h"
+
+namespace cache {
+namespace {
+TEST(LazyValueTest, SimpleGet) {
+  auto generator = [](std::function<void(size_t, size_t)> callback) {
+    callback(0, 1);
+  };
+
+  LazyValue<size_t, size_t> cache(0, generator);
+
+  bool called;
+  size_t status;
+  size_t value;
+  cache.Get(
+      callback::Capture(callback::SetWhenCalled(&called), &status, &value));
+  ASSERT_TRUE(called);
+  EXPECT_EQ(0u, status);
+  EXPECT_EQ(1u, value);
+}
+
+TEST(LazyValueTest, FailingGenerator) {
+  size_t nb_called = 0;
+  auto generator = [&nb_called](std::function<void(size_t, size_t)> callback) {
+    ++nb_called;
+    callback(1, 0);
+  };
+
+  LazyValue<size_t, size_t> cache(0, generator);
+
+  bool called;
+  size_t status;
+  size_t value;
+
+  cache.Get(
+      callback::Capture(callback::SetWhenCalled(&called), &status, &value));
+  ASSERT_TRUE(called);
+  EXPECT_EQ(1u, status);
+  EXPECT_EQ(1u, nb_called);
+
+  cache.Get(
+      callback::Capture(callback::SetWhenCalled(&called), &status, &value));
+  ASSERT_TRUE(called);
+  EXPECT_EQ(1u, status);
+  EXPECT_EQ(2u, nb_called);
+}
+
+TEST(LazyValueTest, CacheCallback) {
+  size_t nb_called = 0;
+  std::function<void(size_t, size_t)> generator_callback;
+  auto generator = [&nb_called, &generator_callback](
+                       std::function<void(size_t, size_t)> callback) {
+    ++nb_called;
+    generator_callback = callback;
+  };
+
+  LazyValue<size_t, size_t> cache(0, generator);
+
+  bool called1, called2;
+  size_t status1, status2;
+  size_t value1, value2;
+
+  cache.Get(
+      callback::Capture(callback::SetWhenCalled(&called1), &status1, &value1));
+
+  EXPECT_FALSE(called1);
+  EXPECT_EQ(1u, nb_called);
+
+  cache.Get(
+      callback::Capture(callback::SetWhenCalled(&called2), &status2, &value2));
+
+  EXPECT_FALSE(called2);
+  EXPECT_EQ(1u, nb_called);
+
+  generator_callback(0, 42);
+
+  ASSERT_TRUE(called1);
+  ASSERT_TRUE(called2);
+  EXPECT_EQ(1u, nb_called);
+  EXPECT_EQ(0u, status1);
+  EXPECT_EQ(42u, value1);
+  EXPECT_EQ(0u, status2);
+  EXPECT_EQ(42u, value2);
+}
+
+}  // namespace
+}  // namespace cache
