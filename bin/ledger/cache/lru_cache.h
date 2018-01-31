80bd3e6e3f849d89c8c5bbdfdf270ef63900e0bc
Benjamin Lerman <qsr@chromium.org>
2018-01-31 14:17:24 +0100

    [ledger] Add a LRUCache class.
    Change-Id: I25f5b3ab59537e41971352786d47b38d872d9b08

---
+++ bin/ledger/cache/lru_cache.h
@@ -0,0 +1,101 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PERIDOT_BIN_LEDGER_CACHE_LRU_CACHE_H_
+#define PERIDOT_BIN_LEDGER_CACHE_LRU_CACHE_H_
+
+#include <functional>
+#include <list>
+#include <map>
+#include <set>
+#include <vector>
+
+#include "lib/fxl/logging.h"
+
+namespace cache {
+// Implements a self-populating LRU cache.
+//
+// This class allows the user to provide a generator for its data, and will
+// cache a given number of entries, discarding least used entries once its reach
+// its maximum capacity.
+//
+// K is the type of the key of the cached data.
+// V is the type of the cached data.
+// S is the type of the success status for the data generator.
+template <typename K, typename V, typename S>
+class LRUCache {
+ public:
+  // Constructor.
+  //
+  // |size| is the maximum capacity of the cache.
+  // |ok_status| is the success status of the generator.
+  // |generator| generates the value to be cached for the given key. It takes a
+  //   callback to retruns its result. It must return |ok_status| as a status
+  //   when the request is successful. Any other return value is considered a
+  //   failure.
+  LRUCache(size_t size,
+           S ok_status,
+           std::function<void(K, std::function<void(S, V)>)> generator)
+      : size_(size), ok_status_(ok_status), generator_(std::move(generator)) {}
+
+  // Retrieves the value for |key| and returns it to |callback|.
+  //
+  // If the value is cached, |callback| will be called synchronously. Otherwise,
+  // |generator| will be called, and depending on its implementation, |callback|
+  // might be called synchronously or not.
+  void Get(const K& key, const std::function<void(S, const V&)>& callback) {
+    auto iterator = map_.find(key);
+    if (iterator != map_.end()) {
+      // Move the list iterator to the front.
+      auto list_iterator = iterator->second;
+      values_.splice(values_.begin(), values_, list_iterator);
+
+      // Call back.
+      callback(ok_status_, iterator->second->second);
+      return;
+    }
+    auto request_iterator = requests_.find(key);
+    if (request_iterator != requests_.end()) {
+      request_iterator->second.push_back(callback);
+      return;
+    }
+    requests_[key].push_back(callback);
+    generator_(key, [this, key](S status, V value) {
+      auto request_iterator = requests_.find(key);
+      FXL_DCHECK(request_iterator != requests_.end());
+      auto callbacks = std::move(request_iterator->second);
+      requests_.erase(request_iterator);
+
+      if (status != ok_status_) {
+        for (const auto& callback : callbacks) {
+          callback(status, value);
+        }
+        return;
+      }
+
+      values_.emplace_front(key, value);
+      map_[std::move(key)] = values_.begin();
+      if (values_.size() > size_) {
+        map_.erase(values_.rbegin()->first);
+        values_.pop_back();
+      }
+      for (const auto& callback : callbacks) {
+        callback(status, value);
+      }
+    });
+  }
+
+ private:
+  using ValueList = std::list<std::pair<K, V>>;
+  ValueList values_;
+  std::map<K, typename ValueList::iterator> map_;
+  std::map<K, std::vector<std::function<void(S, V)>>> requests_;
+  size_t size_;
+  S ok_status_;
+  std::function<void(K, std::function<void(S, V)>)> generator_;
+};
+
+}  // namespace cache
+
+#endif  // PERIDOT_BIN_LEDGER_CACHE_LRU_CACHE_H_
---
+++ bin/ledger/cache/lru_cache_unittest.cc
@@ -0,0 +1,143 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "peridot/bin/ledger/cache/lru_cache.h"
+
+#include "peridot/lib/callback/capture.h"
+#include "peridot/lib/callback/set_when_called.h"
+
+#include "gtest/gtest.h"
+
+namespace cache {
+namespace {
+TEST(LRUCacheTest, SimpleGet) {
+  auto generator = [](size_t i, std::function<void(size_t, size_t)> callback) {
+    callback(0, 2 * i);
+  };
+
+  LRUCache<size_t, size_t, size_t> cache(200, 0, generator);
+
+  bool called;
+  size_t status;
+  size_t value;
+  cache.Get(
+      0, callback::Capture(callback::SetWhenCalled(&called), &status, &value));
+  ASSERT_TRUE(called);
+  EXPECT_EQ(0u, status);
+  EXPECT_EQ(0u, value);
+
+  cache.Get(
+      42, callback::Capture(callback::SetWhenCalled(&called), &status, &value));
+  ASSERT_TRUE(called);
+  EXPECT_EQ(0u, status);
+  EXPECT_EQ(84u, value);
+}
+
+TEST(LRUCacheTest, FailingGenerator) {
+  size_t nb_called = 0;
+  auto generator = [&nb_called](size_t i,
+                                std::function<void(size_t, size_t)> callback) {
+    ++nb_called;
+    callback(1, 0);
+  };
+
+  LRUCache<size_t, size_t, size_t> cache(200, 0, generator);
+
+  bool called;
+  size_t status;
+  size_t value;
+
+  cache.Get(
+      0, callback::Capture(callback::SetWhenCalled(&called), &status, &value));
+  ASSERT_TRUE(called);
+  EXPECT_EQ(1u, status);
+  EXPECT_EQ(1u, nb_called);
+
+  cache.Get(
+      0, callback::Capture(callback::SetWhenCalled(&called), &status, &value));
+  ASSERT_TRUE(called);
+  EXPECT_EQ(1u, status);
+  EXPECT_EQ(2u, nb_called);
+}
+
+TEST(LRUCacheTest, CacheCallback) {
+  size_t nb_called = 0;
+  std::function<void(size_t, size_t)> generator_callback;
+  auto generator = [&nb_called, &generator_callback](
+                       size_t i, std::function<void(size_t, size_t)> callback) {
+    ++nb_called;
+    generator_callback = callback;
+  };
+
+  LRUCache<size_t, size_t, size_t> cache(200, 0, generator);
+
+  bool called1, called2;
+  size_t status1, status2;
+  size_t value1, value2;
+
+  cache.Get(0, callback::Capture(callback::SetWhenCalled(&called1), &status1,
+                                 &value1));
+
+  EXPECT_FALSE(called1);
+  EXPECT_EQ(1u, nb_called);
+
+  cache.Get(0, callback::Capture(callback::SetWhenCalled(&called2), &status2,
+                                 &value2));
+
+  EXPECT_FALSE(called2);
+  EXPECT_EQ(1u, nb_called);
+
+  generator_callback(0, 42);
+
+  ASSERT_TRUE(called1);
+  ASSERT_TRUE(called2);
+  EXPECT_EQ(1u, nb_called);
+  EXPECT_EQ(0u, status1);
+  EXPECT_EQ(42u, value1);
+  EXPECT_EQ(0u, status2);
+  EXPECT_EQ(42u, value2);
+}
+
+TEST(LRUCacheTest, LRUPolicy) {
+  size_t nb_called = 0;
+  std::function<void(size_t, size_t)> generator_callback;
+  auto generator = [&nb_called](size_t i,
+                                std::function<void(size_t, size_t)> callback) {
+    ++nb_called;
+    callback(0u, 0u);
+  };
+
+  LRUCache<size_t, size_t, size_t> cache(3, 0, generator);
+
+  size_t status;
+  size_t value;
+
+  cache.Get(0, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(1u, nb_called);
+  cache.Get(0, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(1u, nb_called);
+  cache.Get(1, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(2u, nb_called);
+  cache.Get(2, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(3u, nb_called);
+  cache.Get(0, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(3u, nb_called);
+  cache.Get(1, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(3u, nb_called);
+  cache.Get(2, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(3u, nb_called);
+  cache.Get(3, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(4u, nb_called);
+  cache.Get(1, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(4u, nb_called);
+  cache.Get(2, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(4u, nb_called);
+  cache.Get(3, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(4u, nb_called);
+  cache.Get(0, callback::Capture([] {}, &status, &value));
+  EXPECT_EQ(5u, nb_called);
+}
+
+}  // namespace
+}  // namespace cache
