461a3e5d1c822d6928b5436ccedf10e24210d567
Brian Swetland <swetland@google.com>
2018-02-01 13:40:24 -0800

    [block][mbr] remove support for iotxns
    Change-Id: Idaddcd726d1e2a8211298c5e296ff217b9df20a0

--- system/dev/block/mbr/mbr.c
+++ system/dev/block/mbr/mbr.c
@@ -13,6 +13,7 @@
 #include <threads.h>
 
 #include <ddk/binding.h>
+#include <ddk/debug.h>
 #include <ddk/device.h>
 #include <ddk/driver.h>
 #include <ddk/protocol/block.h>
@@ -27,16 +28,6 @@
 #define MBR_PARTITION_ENTRY_SIZE 16
 #define MBR_NUM_PARTITIONS 4
 #define MBR_BOOT_SIGNATURE 0xAA55
-
-#define TRACE 0
-
-#if TRACE
-#define xprintf(fmt...) printf(fmt)
-#else
-#define xprintf(fmt...) \
-    do {                \
-    } while (0)
-#endif
 
 // ATTN: MBR supports 8 bit partition types instead of GUIDs. Here we define
 // mappings between partition type and GUIDs that zircon understands. When
@@ -76,21 +67,6 @@ typedef struct mbrpart_device {
 
     atomic_int writercount;
 } mbrpart_device_t;
-
-
-static void mbr_read_sync_complete(iotxn_t* txn, void* cookie) {
-    // Used to synchronize iotxn_calls.
-    completion_signal((completion_t*)cookie);
-}
-
-static inline bool is_writer(uint32_t flags) {
-    return (flags & O_RDWR || flags & O_WRONLY);
-}
-
-static uint64_t getsize(mbrpart_device_t* dev) {
-    // Returns the size of the partition referred to by dev.
-    return dev->partition.sector_partition_length * ((uint64_t) dev->info.block_size);
-}
 
 static zx_status_t mbr_ioctl(void* ctx, uint32_t op, const void* cmd,
                              size_t cmdlen, void* reply, size_t max,
@@ -182,33 +158,6 @@ static void mbr_queue(void* ctx, block_op_t* bop) {
     mbr->bp.ops->queue(mbr->bp.ctx, bop);
 }
 
-static void mbr_iotxn_queue(void* ctx, iotxn_t* txn) {
-    mbrpart_device_t* dev = ctx;
-    if (txn->offset % dev->info.block_size) {
-        iotxn_complete(txn, ZX_ERR_INVALID_ARGS, 0);
-        return;
-    }
-    if (txn->length % dev->info.block_size) {
-        iotxn_complete(txn, ZX_ERR_INVALID_ARGS, 0);
-        return;
-    }
-    if ((txn->offset >= getsize(dev)) || (getsize(dev) - txn->offset < txn->length)) {
-        iotxn_complete(txn, ZX_ERR_OUT_OF_RANGE, 0);
-        return;
-    }
-    // transactions from read()/write() may be truncated
-    txn->offset = to_parent_offset(dev, txn->offset);
-    if (txn->length == 0) {
-        iotxn_complete(txn, ZX_OK, 0);
-    } else {
-        iotxn_queue(dev->parent, txn);
-    }
-}
-
-static zx_off_t mbr_getsize(void* ctx) {
-    return getsize(ctx);
-}
-
 static void mbr_unbind(void* ctx) {
     mbrpart_device_t* device = ctx;
     device_remove(device->zxdev);
@@ -219,40 +168,42 @@ static void mbr_release(void* ctx) {
     free(device);
 }
 
-static zx_status_t mbr_open(void* ctx, zx_device_t** dev_out,
-                            uint32_t flags) {
-    mbrpart_device_t* device = ctx;
-    zx_status_t status = ZX_OK;
-    if (is_writer(flags) && (atomic_exchange(&device->writercount, 1) == 1)) {
-        xprintf("Partition cannot be opened as writable (open elsewhere)\n");
-        status = ZX_ERR_ALREADY_BOUND;
-    }
-    return status;
-}
-
-static zx_status_t mbr_close(void* ctx, uint32_t flags) {
-    mbrpart_device_t* device = ctx;
-    if (is_writer(flags)) {
-        atomic_fetch_sub(&device->writercount, 1);
-    }
-    return ZX_OK;
+static zx_off_t mbr_get_size(void* ctx) {
+    mbrpart_device_t* dev = ctx;
+    //TODO: use query() results, *but* fvm returns different query and getsize
+    // results, and the latter are dynamic...
+    return device_get_size(dev->parent);
 }
 
 static zx_protocol_device_t mbr_proto = {
     .version = DEVICE_OPS_VERSION,
     .ioctl = mbr_ioctl,
-    .iotxn_queue = mbr_iotxn_queue,
-    .get_size = mbr_getsize,
+    .get_size = mbr_get_size,
     .unbind = mbr_unbind,
     .release = mbr_release,
-    .open = mbr_open,
-    .close = mbr_close,
 };
 
 static block_protocol_ops_t block_ops = {
     .query = mbr_query,
     .queue = mbr_queue,
 };
+
+static void mbr_read_sync_complete(block_op_t* bop, zx_status_t status) {
+    bop->command = status;
+    completion_signal((completion_t*)bop->cookie);
+}
+
+static zx_status_t vmo_read(zx_handle_t vmo, void* data, uint64_t off, size_t len) {
+    size_t actual;
+    zx_status_t status = zx_vmo_read(vmo, data, off, len, &actual);
+    if (status != ZX_OK) {
+        return status;
+    }
+    if (actual == len) {
+        return ZX_OK;
+    }
+    return ZX_ERR_INTERNAL;
+}
 
 static int mbr_bind_thread(void* arg) {
     mbrpart_device_t* first_dev = (mbrpart_device_t*)arg;
@@ -265,69 +216,60 @@ static int mbr_bind_thread(void* arg) {
     memcpy(&bp, &first_dev->bp, sizeof(bp));
 
     block_info_t block_info;
-    size_t block_op_size = sizeof(block_op_t);
+    size_t block_op_size;
+    bp.ops->query(bp.ctx, &block_info, &block_op_size);
 
-    iotxn_t* txn = NULL;
-
-    if (bp.ops == NULL) {
-        size_t actual;
-        ssize_t rc = device_ioctl(dev, IOCTL_BLOCK_GET_INFO, NULL, 0,
-                                  &block_info, sizeof(block_info), &actual);
-        if (rc < 0 || actual != sizeof(block_info)) {
-            xprintf("mbr: Could not get block size for dev=%s, retcode = %zd\n",
-                    dev->name, rc);
-            goto unbind;
-        }
-    } else {
-        bp.ops->query(bp.ctx, &block_info, &block_op_size);
+    zx_handle_t vmo = ZX_HANDLE_INVALID;
+    block_op_t* bop = calloc(1, block_op_size);
+    if (bop == NULL) {
+        goto unbind;
     }
 
     // We need to read at least 512B to parse the MBR. Determine if we should
     // read the device's block size or we should ready exactly 512B.
-    size_t iotxn_size = 0;
+    size_t iosize = 0;
     if (block_info.block_size >= MBR_SIZE) {
-        iotxn_size = block_info.block_size;
+        iosize = block_info.block_size;
     } else {
         // Make sure we're reading some multiple of the block size.
-        iotxn_size = DIV_ROUND_UP(MBR_SIZE, block_info.block_size) * block_info.block_size;
+        iosize = DIV_ROUND_UP(MBR_SIZE, block_info.block_size) * block_info.block_size;
     }
 
-    zx_status_t st = iotxn_alloc(&txn, IOTXN_ALLOC_CONTIGUOUS, iotxn_size);
-    if (st != ZX_OK) {
-        xprintf("mbr: failed to allocate iotxn, retcode = %d\n", st);
+    if (zx_vmo_create(iosize, 0, &vmo) != ZX_OK) {
+        zxlogf(ERROR, "mbr: cannot allocate vmo\n");
         goto unbind;
     }
+
 
     completion_t cplt = COMPLETION_INIT;
-    txn->opcode = IOTXN_OP_READ;
-    txn->offset = 0;
-    txn->length = iotxn_size;
-    txn->complete_cb = mbr_read_sync_complete;
-    txn->cookie = &cplt;
 
-    iotxn_queue(dev, txn);
+    bop->command = BLOCK_OP_READ;
+    bop->rw.vmo = vmo;
+    bop->rw.length = iosize / block_info.block_size;
+    bop->rw.offset_dev = 0;
+    bop->rw.offset_vmo = 0;
+    bop->rw.pages = NULL;
+    bop->completion_cb = mbr_read_sync_complete;
+    bop->cookie = &cplt;
+
+    bp.ops->queue(bp.ctx, bop);
     completion_wait(&cplt, ZX_TIME_INFINITE);
 
-    if (txn->status != ZX_OK) {
-        xprintf("mbr: could not read mbr from device, retcode = %d\n",
-                txn->status);
-        goto unbind;
-    }
-
-    if (txn->actual < MBR_SIZE) {
-        xprintf("mbr: expected to read %u bytes but only read %" PRIu64 "\n",
-                MBR_SIZE, txn->actual);
+    if (bop->command != ZX_OK) {
+        zxlogf(ERROR, "mbr: could not read mbr from device, retcode = %d\n", bop->command);
         goto unbind;
     }
 
     uint8_t buffer[MBR_SIZE];
-    iotxn_copyfrom(txn, buffer, MBR_SIZE, 0);
     mbr_t* mbr = (mbr_t*)buffer;
+    if (vmo_read(vmo, buffer, 0, MBR_SIZE) != ZX_OK) {
+        goto unbind;
+    }
 
     // Validate the MBR boot signature.
     if (mbr->boot_signature != MBR_BOOT_SIGNATURE) {
-        xprintf("mbr: invalid mbr boot signature, expected 0x%04x got 0x%04x\n",
-                MBR_BOOT_SIGNATURE, mbr->boot_signature);
+        zxlogf(ERROR, "mbr: invalid mbr boot signature, expected 0x%04x got 0x%04x\n",
+               MBR_BOOT_SIGNATURE, mbr->boot_signature);
         goto unbind;
     }
 
@@ -340,10 +282,10 @@ static int mbr_bind_thread(void* arg) {
             continue;
         }
 
-        xprintf("mbr: found partition, entry = %d, type = 0x%02x, "
-                "start = %u, length = %u\n",
-                partition_count + 1, entry->type, entry->start_sector_lba,
-                entry->sector_partition_length);
+        zxlogf(SPEW, "mbr: found partition, entry = %d, type = 0x%02x, "
+               "start = %u, length = %u\n",
+               partition_count + 1, entry->type, entry->start_sector_lba,
+               entry->sector_partition_length);
 
         mbrpart_device_t* pdev;
         // use first_dev for first partition
@@ -352,7 +294,7 @@ static int mbr_bind_thread(void* arg) {
         } else {
             pdev = calloc(1, sizeof(*pdev));
             if (!pdev) {
-                xprintf("mbr: out of memory\n");
+                zxlogf(ERROR, "mbr: out of memory\n");
                 goto unbind;
             }
             pdev->parent = dev;
@@ -378,29 +320,26 @@ static int mbr_bind_thread(void* arg) {
                 .ctx = pdev,
                 .ops = &mbr_proto,
                 .proto_id = ZX_PROTOCOL_BLOCK_CORE,
-                .proto_ops = (bp.ops == NULL) ? NULL : &block_ops,
+                .proto_ops = &block_ops,
             };
 
-            if ((st = device_add(dev, &args, &pdev->zxdev)) != ZX_OK) {
-                xprintf("mbr: device_add failed, retcode = %d\n", st);
+            if (device_add(dev, &args, &pdev->zxdev) != ZX_OK) {
                 free(pdev);
                 continue;
             }
         }
     }
 
-    iotxn_release(txn);
-
+    free(bop);
+    zx_handle_close(vmo);
     return 0;
 unbind:
-    if (txn)
-        iotxn_release(txn);
-
+    free(bop);
+    zx_handle_close(vmo);
     if (first_dev) {
         // handle case where no partitions were found
         device_remove(first_dev->zxdev);
     }
-
     return -1;
 }
 
@@ -418,8 +357,10 @@ static zx_status_t mbr_bind(void* ctx, zx_device_t* parent) {
     device->parent = parent;
 
     if (device_get_protocol(parent, ZX_PROTOCOL_BLOCK, &device->bp) != ZX_OK) {
-        printf("WARNING: block device '%s': does not support new protocol\n",
+        zxlogf(ERROR, "mbr: ERROR: block device '%s': does not support block protocol\n",
                device_get_name(parent));
+        free(device);
+        return ZX_ERR_NOT_SUPPORTED;
     }
 
     char name[128];
@@ -431,13 +372,12 @@ static zx_status_t mbr_bind(void* ctx, zx_device_t* parent) {
         .ctx = device,
         .ops = &mbr_proto,
         .proto_id = ZX_PROTOCOL_BLOCK_CORE,
-        .proto_ops = (device->bp.ops == NULL) ? NULL : &block_ops,
+        .proto_ops = &block_ops,
         .flags = DEVICE_ADD_INVISIBLE,
     };
 
     zx_status_t status = device_add(parent, &args, &device->zxdev);
     if (status != ZX_OK) {
-        printf("mbr device_add failed\n");
         free(device);
         return status;
     }
