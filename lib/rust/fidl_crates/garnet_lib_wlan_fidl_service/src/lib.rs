ec0ce6ac34bf5df40b55d5ae5f4b7d0f0ee0f564
Tim Kilbourn <tkilbourn@google.com>
2018-02-05 00:56:06 -0800

    [wlan] Add FIDL device service definitions
    Device service is responsible for configuring the phys and interfaces of
    the wlan devices in the system.
    
    NET-451
    
    Change-Id: I022408f67b3c060103a02198752d09e5ba470ce0

---
+++ lib/rust/fidl_crates/garnet_lib_wlan_fidl_service/src/lib.rs
@@ -0,0 +1,6 @@
+#![deny(warnings)]
+#![deny(missing_debug_implementations)]
+#![allow(unused_imports)]
+#![allow(unused_extern_crates)]
+
+include!(concat!(env!("FUCHSIA_GEN_ROOT"), "/garnet/lib/wlan/fidl/garnet_lib_wlan_fidl_service.rs"));
