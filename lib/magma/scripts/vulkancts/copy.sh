ad1da0e39e85cf77120f2870e26be09f4cb1bdc1
Craig Stout <cstout@google.com>
2018-02-06 13:33:32 -0800

    [magma] Update cts copy script
    Change-Id: Id266fff10b169e6fbf12212d7eed51793670e46c

--- lib/magma/scripts/vulkancts/copy.sh
+++ lib/magma/scripts/vulkancts/copy.sh
@@ -7,7 +7,7 @@
 set -e
 fuchsia_root=`pwd`
 tools_path=$fuchsia_root/buildtools
-platform=${1:-pc-x86-64}
+platform=${1:-x86-64}
 build_dir=$fuchsia_root/out/build-vulkancts-$platform
 dest_dir=/data/vulkancts
 netaddr=$fuchsia_root/out/build-zircon/tools/netaddr
@@ -21,7 +21,7 @@ ssh_config="-F $fuchsia_root/out/debug-$fuchsia_platform/ssh-keys/ssh_config"
 
 ssh $ssh_config `$netaddr --fuchsia` mkdir -p $dest_dir
 scp $ssh_config $build_dir/external/vulkancts/modules/vulkan/deqp-vk-stripped [`$netaddr --fuchsia`]:$dest_dir/deqp-vk
-scp $ssh_config third_party/vulkan-cts/external/vulkancts/mustpass/1.0.1/vk-default.txt [`$netaddr --fuchsia`]:$dest_dir/vk-default.txt
+scp $ssh_config third_party/vulkan-cts/external/vulkancts/mustpass/1.0.2/vk-default.txt [`$netaddr --fuchsia`]:$dest_dir/vk-default.txt
 scp $ssh_config -pr $fuchsia_root/third_party/vulkan-cts/external/vulkancts/data/* [`$netaddr --fuchsia`]:$dest_dir
 scp $ssh_config $fuchsia_root/garnet/lib/magma/scripts/vulkancts/run.sh [`$netaddr --fuchsia`]:$dest_dir
 scp $ssh_config $build_dir/executor/executor [`$netaddr --fuchsia`]:$dest_dir
