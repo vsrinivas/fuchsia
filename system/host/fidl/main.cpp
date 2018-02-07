9e0fc6708bb34abbde603122d28a615a91ff82f7
George Kulakowski <kulakowski@google.com>
2018-02-06 18:17:33 -0800

    [fidl] Add response file support for the list files to compile
    Change-Id: Ibaba925e91c1ea244be5a0c4bda1f341db33df87

--- system/host/fidl/main.cpp
+++ system/host/fidl/main.cpp
@@ -31,16 +31,38 @@ namespace {
     std::cout << "    fidl json JSON_PATH [FIDL_FILE...]\n";
     std::cout << "        Parses the FIDL_FILEs and generates JSON intermediate data\n";
     std::cout << "        into JSON_PATH.\n";
+    std::cout << "\n";
+    std::cout << "The [FIDL_FILE...] arguments can also be provided via a response\n";
+    std::cout << "    file, denoted as `@filepath'. The contents of the file at\n";
+    std::cout << "    `filepath' will be interpreted as a whitespace-delimited list\n";
+    std::cout << "    of files to parse.\n";
     std::cout.flush();
     exit(1);
 }
 
+std::fstream Open(std::string filename, std::ios::openmode mode) {
+    std::fstream stream;
+    stream.open(filename, mode);
+    if (!stream.is_open()) {
+        Usage();
+    }
+    return stream;
+}
+
 class Arguments {
 public:
-    Arguments(int count, char** arguments)
+    virtual ~Arguments() {}
+
+    virtual std::string Claim() = 0;
+    virtual bool Remaining() const = 0;
+};
+
+class ArgvArguments : public Arguments {
+public:
+    ArgvArguments(int count, char** arguments)
         : count_(count), arguments_(const_cast<const char**>(arguments)) {}
 
-    std::string Claim() {
+    std::string Claim() override {
         if (count_ < 1) {
             Usage();
         }
@@ -50,18 +72,60 @@ public:
         return argument;
     }
 
-    bool Remaining() { return count_ > 0; }
+    bool Remaining() const override { return count_ > 0; }
+
+    bool HeadIsResponseFile() {
+        if (count_ != 1) {
+            return false;
+        }
+        return arguments_[0][0] == '@';
+    }
 
 private:
     int count_;
     const char** arguments_;
 };
 
-std::fstream Open(std::string filename) {
-    std::fstream stream;
-    stream.open(filename, std::ios::out);
-    return stream;
-}
+class ResponseFileArguments : public Arguments {
+public:
+    ResponseFileArguments(fidl::StringView filename) : file_(Open(filename, std::ios::in)) {
+        ConsumeWhitespace();
+    }
+
+    std::string Claim() override {
+        std::string argument;
+        while (Remaining() && !IsWhitespace()) {
+            argument.push_back(file_.get());
+        }
+        ConsumeWhitespace();
+        return argument;
+    }
+
+    bool Remaining() const override {
+        return !file_.eof();
+    }
+
+private:
+    bool IsWhitespace() {
+        switch (file_.peek()) {
+        case ' ':
+        case '\n':
+        case '\r':
+        case '\t':
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    void ConsumeWhitespace() {
+        while (Remaining() && IsWhitespace()) {
+            file_.get();
+        }
+    }
+
+    std::fstream file_;
+};
 
 enum struct Behavior {
     CStructs,
@@ -129,11 +193,12 @@ bool GenerateJSON(fidl::Library* library, std::fstream json_output) {
 } // namespace
 
 int main(int argc, char* argv[]) {
-    Arguments args(argc, argv);
-    // Parse the program name.
-    args.Claim();
+    auto argv_args = std::make_unique<ArgvArguments>(argc, argv);
 
-    std::string behavior_argument = args.Claim();
+    // Parse the program name.
+    argv_args->Claim();
+
+    std::string behavior_argument = argv_args->Claim();
     Behavior behavior;
     std::fstream output_file;
     if (behavior_argument == "c-structs") {
@@ -142,23 +207,35 @@ int main(int argc, char* argv[]) {
         if (argc < 2) {
             return 1;
         }
-        output_file = Open(args.Claim());
+        output_file = Open(argv_args->Claim(), std::ios::out);
     } else if (behavior_argument == "json") {
         behavior = Behavior::JSON;
         // Parse a file name to write output to.
         if (argc < 2) {
             return 1;
         }
-        output_file = Open(args.Claim());
+        output_file = Open(argv_args->Claim(), std::ios::out);
     } else {
         Usage();
+    }
+
+    // Either continue with the list of fidl files to compile, or else
+    // with a response file.
+    std::unique_ptr<ResponseFileArguments> response_file_args;
+    Arguments* args = argv_args.get();
+    if (argv_args->HeadIsResponseFile()) {
+        std::string response = argv_args->Claim();
+        // Drop the leading '@'.
+        fidl::StringView response_file = response.data() + 1;
+        response_file_args = std::make_unique<ResponseFileArguments>(response_file);
+        args = response_file_args.get();
     }
 
     fidl::SourceManager source_manager;
     fidl::IdentifierTable identifier_table;
     fidl::ErrorReporter error_reporter;
     fidl::Library library;
-    if (!Parse(&args, &source_manager, &identifier_table, &error_reporter, &library)) {
+    if (!Parse(args, &source_manager, &identifier_table, &error_reporter, &library)) {
         return 1;
     }
 
