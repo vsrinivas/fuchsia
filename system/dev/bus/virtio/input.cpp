4f77caeaa8d351321c81ee6df5460ca1b54588f4
Alex Legg <alexlegg@google.com>
2018-02-07 15:02:16 -0800

    [virtio][input] Allocate io buffers contiguously
    Required for io_buffer_phys without doing a physmap.
    
    Change-Id: If2cb05991cd6ed9530c42ff909ce55e63fd7027e

--- system/dev/bus/virtio/input.cpp
+++ system/dev/bus/virtio/input.cpp
@@ -271,7 +271,8 @@ zx_status_t InputDevice::Init() {
     // TODO: Avoid multiple allocations, allocate enough for all buffers once.
     for (uint16_t id = 0; id < kEventCount; ++id) {
         static_assert(sizeof(virtio_input_event_t) <= PAGE_SIZE, "");
-        status = io_buffer_init(&buffers_[id], sizeof(virtio_input_event_t), IO_BUFFER_RO);
+        status = io_buffer_init(&buffers_[id], sizeof(virtio_input_event_t),
+                                IO_BUFFER_RO | IO_BUFFER_CONTIG);
         if (status != ZX_OK) {
             zxlogf(ERROR, "Failed to allocate I/O buffers: %s\n", zx_status_get_string(status));
             return status;
