eda4d8c3a84a6a1d48a2444e59923b0927bda9a0
Brett Wilson <brettw@chromium.org>
2018-02-09 09:42:08 -0800

    [debugger] Add a stub debugger router.
    This does nothing currently. It is a placeholder for the target side of the
    remote debugger interface, and potentially a new home for a crash logger.
    
    Change-Id: I3e6f5459b3eafba96bb84211264d111c24abb937

---
+++ system/core/debug-router/rules.mk
@@ -0,0 +1,23 @@
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userapp
+MODULE_GROUP := core
+
+MODULE_NAME := debug-router
+
+# app main
+MODULE_SRCS := \
+    $(LOCAL_DIR)/debug-router.cpp \
+
+MODULE_LIBS := \
+    system/ulib/zircon \
+    system/ulib/c \
+    system/ulib/fdio
+
+include make/module.mk
