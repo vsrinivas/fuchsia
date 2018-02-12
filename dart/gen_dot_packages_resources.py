d8d5241d95e9a4d80ed83ac629368c52834c2517
Zachary Anderson <zra@google.com>
2018-02-12 07:57:40 -0800

    [flutter] Adds a config file to a source package
    Adds a file to the source package describing the package
    containing the main.dart entrypoint so that Flutter
    doesn't have to infer it based on the app uri.
    
    Change-Id: I0126e70c49eab336e464f495bcbd962c266aa09f

--- dart/gen_dot_packages_resources.py
+++ dart/gen_dot_packages_resources.py
@@ -16,16 +16,30 @@ import urlparse
 def main():
   parser = argparse.ArgumentParser(
       description="Generate a manifest from a Dart .packages file")
+  parser.add_argument("--contents-out",
+      help="Output contents file to be included in the manifest",
+      required=True)
   parser.add_argument("--dot-packages",
       help="Path to .packages file to translate",
       required=True)
   parser.add_argument("--dot-packages-out",
-      help="Output .packages file for the package.",
+      help="Output .packages file to be included in the manifest",
       required=True)
   parser.add_argument("--manifest-out",
       help="Output manifest file.",
       required=True)
+  parser.add_argument("--package",
+      help="The name of the package containing the lib/main.dart entrypoint",
+      required=True)
   args = parser.parse_args()
+
+  # TODO(zra): At some point, it will likely make sense for the flutter content
+  # handler to understand a metadata file describing the contents of the
+  # package it needs to run. For now, we'll simply write the name of the
+  # source package containing lib/main.dart, so that the content handler doesn't
+  # have to guess it from the request url.
+  with open(args.contents_out, 'w') as cfile:
+    cfile.write(args.package)
 
   with open(args.dot_packages) as dpfile, \
        open(args.manifest_out, 'w') as mfile, \
@@ -35,7 +49,7 @@ def main():
       package_name = package_and_uri[0]
       path = urlparse.urlparse(package_and_uri[1])
       path_len = len(path.path)
-      manifest_lib_path = os.path.join('data', package_name, 'lib')
+      manifest_lib_path = os.path.join('data', 'dart-pkg', package_name, 'lib')
       for root, dirs, files in os.walk(path.path):
         for f in files:
           # TODO(zra): What other files are needed?
@@ -46,7 +60,8 @@ def main():
           manifest_path = os.path.join(manifest_lib_path, relative_path)
           mfile.write(manifest_path + '=' + full_path + '\n')
       dpoutfile.write(package_name + ':file:///pkg/' + manifest_lib_path + '/\n')
-    mfile.write(os.path.join('data', '.packages') + '=' + args.dot_packages_out + '\n')
+    mfile.write(os.path.join('data', 'dart-pkg', '.packages') + '=' + args.dot_packages_out + '\n')
+    mfile.write(os.path.join('data', 'dart-pkg', 'contents') + '=' + args.contents_out + '\n')
   return 0
 
 
