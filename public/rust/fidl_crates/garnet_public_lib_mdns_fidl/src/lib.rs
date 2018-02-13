a9f4ee784588b055d5ce44d8d015606da308d76f
Craig Tiller <ctiller@google.com>
2018-02-13 09:11:00 -0800

    [rust][mdns] Add Rust wrappers for mdns fidl service
    Change-Id: Ibe1bf8d70b51bb1c5fd71622b4c5078af02fc03a

---
+++ public/rust/fidl_crates/garnet_public_lib_mdns_fidl/src/lib.rs
@@ -0,0 +1,6 @@
+#![deny(warnings)]
+#![deny(missing_debug_implementations)]
+#![allow(unused_imports)]
+#![allow(unused_extern_crates)]
+
+include!(concat!(env!("FUCHSIA_GEN_ROOT"), "/garnet/public/lib/mdns/fidl/garnet_public_lib_mdns_fidl.rs"));
