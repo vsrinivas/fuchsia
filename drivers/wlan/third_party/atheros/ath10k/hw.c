d9b52c10a35ca0cd9bfbabd2ea3978d2d43e7ce9
Josh Conner <joshconner@google.com>
2018-02-14 11:54:39 -0800

    [ath10k] Port hw.c module
    Mostly converting int return types to ZX status codes.
    Also some whitespace cleanup.
    
    Change-Id: If4a7c2d2ccf25e8c61ea5345eeadb848c478affd

--- drivers/wlan/third_party/atheros/ath10k/hw.c
+++ drivers/wlan/third_party/atheros/ath10k/hw.c
@@ -15,12 +15,12 @@
  */
 
 #include <stdint.h>
-#include <zircon/syscalls.h>
-#include <linux/bitops.h>
+
 #include "core.h"
 #include "hw.h"
 #include "hif.h"
 #include "wmi-ops.h"
+#include "linuxisms.h"
 #include "bmi.h"
 
 const struct ath10k_hw_regs qca988x_regs = {
@@ -151,39 +151,39 @@ const struct ath10k_hw_regs qca4019_regs = {
 };
 
 const struct ath10k_hw_values qca988x_values = {
-    .rtc_state_val_on       = 3,
-    .ce_count           = 8,
-    .msi_assign_ce_max      = 7,
-    .num_target_ce_config_wlan  = 7,
-    .ce_desc_meta_data_mask     = 0xFFFC,
-    .ce_desc_meta_data_lsb      = 2,
+    .rtc_state_val_on               = 3,
+    .ce_count                       = 8,
+    .msi_assign_ce_max              = 7,
+    .num_target_ce_config_wlan      = 7,
+    .ce_desc_meta_data_mask         = 0xFFFC,
+    .ce_desc_meta_data_lsb          = 2,
 };
 
 const struct ath10k_hw_values qca6174_values = {
-    .rtc_state_val_on       = 3,
-    .ce_count           = 8,
-    .msi_assign_ce_max      = 7,
-    .num_target_ce_config_wlan  = 7,
-    .ce_desc_meta_data_mask     = 0xFFFC,
-    .ce_desc_meta_data_lsb      = 2,
+    .rtc_state_val_on               = 3,
+    .ce_count                       = 8,
+    .msi_assign_ce_max              = 7,
+    .num_target_ce_config_wlan      = 7,
+    .ce_desc_meta_data_mask         = 0xFFFC,
+    .ce_desc_meta_data_lsb          = 2,
 };
 
 const struct ath10k_hw_values qca99x0_values = {
-    .rtc_state_val_on       = 5,
-    .ce_count           = 12,
-    .msi_assign_ce_max      = 12,
-    .num_target_ce_config_wlan  = 10,
-    .ce_desc_meta_data_mask     = 0xFFF0,
-    .ce_desc_meta_data_lsb      = 4,
+    .rtc_state_val_on               = 5,
+    .ce_count                       = 12,
+    .msi_assign_ce_max              = 12,
+    .num_target_ce_config_wlan      = 10,
+    .ce_desc_meta_data_mask         = 0xFFF0,
+    .ce_desc_meta_data_lsb          = 4,
 };
 
 const struct ath10k_hw_values qca9888_values = {
-    .rtc_state_val_on       = 3,
-    .ce_count           = 12,
-    .msi_assign_ce_max      = 12,
-    .num_target_ce_config_wlan  = 10,
-    .ce_desc_meta_data_mask     = 0xFFF0,
-    .ce_desc_meta_data_lsb      = 4,
+    .rtc_state_val_on               = 3,
+    .ce_count                       = 12,
+    .msi_assign_ce_max              = 12,
+    .num_target_ce_config_wlan      = 10,
+    .ce_desc_meta_data_mask         = 0xFFF0,
+    .ce_desc_meta_data_lsb          = 4,
 };
 
 const struct ath10k_hw_values qca4019_values = {
@@ -212,16 +212,16 @@ static struct ath10k_hw_ce_regs_addr_map qcax_dmax = {
 };
 
 static struct ath10k_hw_ce_ctrl1 qcax_ctrl1 = {
-    .addr       = 0x00000010,
-    .hw_mask    = 0x0007ffff,
-    .sw_mask    = 0x0007ffff,
-    .hw_wr_mask = 0x00000000,
-    .sw_wr_mask = 0x0007ffff,
-    .reset_mask = 0xffffffff,
-    .reset      = 0x00000080,
-    .src_ring   = &qcax_src_ring,
-    .dst_ring   = &qcax_dst_ring,
-    .dmax       = &qcax_dmax,
+    .addr           = 0x00000010,
+    .hw_mask        = 0x0007ffff,
+    .sw_mask        = 0x0007ffff,
+    .hw_wr_mask     = 0x00000000,
+    .sw_wr_mask     = 0x0007ffff,
+    .reset_mask     = 0xffffffff,
+    .reset          = 0x00000080,
+    .src_ring       = &qcax_src_ring,
+    .dst_ring       = &qcax_dst_ring,
+    .dmax           = &qcax_dmax,
 };
 
 static struct ath10k_hw_ce_regs_addr_map qcax_cmd_halt_status = {
@@ -231,10 +231,10 @@ static struct ath10k_hw_ce_regs_addr_map qcax_cmd_halt_status = {
 };
 
 static struct ath10k_hw_ce_cmd_halt qcax_cmd_halt = {
-    .msb        = 0x00000000,
-    .mask       = GENMASK(0, 0),
+    .msb            = 0x00000000,
+    .mask           = GENMASK(0, 0),
     .status_reset   = 0x00000000,
-    .status     = &qcax_cmd_halt_status,
+    .status         = &qcax_cmd_halt_status,
 };
 
 static struct ath10k_hw_ce_regs_addr_map qcax_host_ie_cc = {
@@ -245,28 +245,28 @@ static struct ath10k_hw_ce_regs_addr_map qcax_host_ie_cc = {
 
 static struct ath10k_hw_ce_host_ie qcax_host_ie = {
     .copy_complete_reset    = 0x00000000,
-    .copy_complete      = &qcax_host_ie_cc,
+    .copy_complete          = &qcax_host_ie_cc,
 };
 
 static struct ath10k_hw_ce_host_wm_regs qcax_wm_reg = {
-    .dstr_lmask = 0x00000010,
-    .dstr_hmask = 0x00000008,
-    .srcr_lmask = 0x00000004,
-    .srcr_hmask = 0x00000002,
-    .cc_mask    = 0x00000001,
-    .wm_mask    = 0x0000001E,
-    .addr       = 0x00000030,
+    .dstr_lmask     = 0x00000010,
+    .dstr_hmask     = 0x00000008,
+    .srcr_lmask     = 0x00000004,
+    .srcr_hmask     = 0x00000002,
+    .cc_mask        = 0x00000001,
+    .wm_mask        = 0x0000001E,
+    .addr           = 0x00000030,
 };
 
 static struct ath10k_hw_ce_misc_regs qcax_misc_reg = {
-    .axi_err    = 0x00000400,
+    .axi_err        = 0x00000400,
     .dstr_add_err   = 0x00000200,
     .srcr_len_err   = 0x00000100,
     .dstr_mlen_vio  = 0x00000080,
     .dstr_overflow  = 0x00000040,
     .srcr_overflow  = 0x00000020,
-    .err_mask   = 0x000007E0,
-    .addr       = 0x00000038,
+    .err_mask       = 0x000007E0,
+    .addr           = 0x00000038,
 };
 
 static struct ath10k_hw_ce_regs_addr_map qcax_src_wm_low = {
@@ -282,10 +282,10 @@ static struct ath10k_hw_ce_regs_addr_map qcax_src_wm_high = {
 };
 
 static struct ath10k_hw_ce_dst_src_wm_regs qcax_wm_src_ring = {
-    .addr       = 0x0000004c,
-    .low_rst    = 0x00000000,
-    .high_rst   = 0x00000000,
-    .wm_low     = &qcax_src_wm_low,
+    .addr           = 0x0000004c,
+    .low_rst        = 0x00000000,
+    .high_rst       = 0x00000000,
+    .wm_low         = &qcax_src_wm_low,
     .wm_high        = &qcax_src_wm_high,
 };
 
@@ -301,31 +301,31 @@ static struct ath10k_hw_ce_regs_addr_map qcax_dst_wm_high = {
 };
 
 static struct ath10k_hw_ce_dst_src_wm_regs qcax_wm_dst_ring = {
-    .addr       = 0x00000050,
-    .low_rst    = 0x00000000,
-    .high_rst   = 0x00000000,
-    .wm_low     = &qcax_dst_wm_low,
-    .wm_high    = &qcax_dst_wm_high,
+    .addr           = 0x00000050,
+    .low_rst        = 0x00000000,
+    .high_rst       = 0x00000000,
+    .wm_low         = &qcax_dst_wm_low,
+    .wm_high        = &qcax_dst_wm_high,
 };
 
 struct ath10k_hw_ce_regs qcax_ce_regs = {
-    .sr_base_addr       = 0x00000000,
-    .sr_size_addr       = 0x00000004,
-    .dr_base_addr       = 0x00000008,
-    .dr_size_addr       = 0x0000000c,
-    .ce_cmd_addr        = 0x00000018,
-    .misc_ie_addr       = 0x00000034,
-    .sr_wr_index_addr   = 0x0000003c,
-    .dst_wr_index_addr  = 0x00000040,
-    .current_srri_addr  = 0x00000044,
-    .current_drri_addr  = 0x00000048,
-    .host_ie_addr       = 0x0000002c,
-    .ctrl1_regs     = &qcax_ctrl1,
-    .cmd_halt       = &qcax_cmd_halt,
-    .host_ie        = &qcax_host_ie,
-    .wm_regs        = &qcax_wm_reg,
-    .misc_regs      = &qcax_misc_reg,
-    .wm_srcr        = &qcax_wm_src_ring,
+    .sr_base_addr           = 0x00000000,
+    .sr_size_addr           = 0x00000004,
+    .dr_base_addr           = 0x00000008,
+    .dr_size_addr           = 0x0000000c,
+    .ce_cmd_addr            = 0x00000018,
+    .misc_ie_addr           = 0x00000034,
+    .sr_wr_index_addr       = 0x0000003c,
+    .dst_wr_index_addr      = 0x00000040,
+    .current_srri_addr      = 0x00000044,
+    .current_drri_addr      = 0x00000048,
+    .host_ie_addr           = 0x0000002c,
+    .ctrl1_regs             = &qcax_ctrl1,
+    .cmd_halt               = &qcax_cmd_halt,
+    .host_ie                = &qcax_host_ie,
+    .wm_regs                = &qcax_wm_reg,
+    .misc_regs              = &qcax_misc_reg,
+    .wm_srcr                = &qcax_wm_src_ring,
     .wm_dstr                = &qcax_wm_dst_ring,
 };
 
@@ -439,7 +439,10 @@ void ath10k_hw_fill_survey_time(struct ath10k* ar, struct survey_info* survey,
 /* The firmware does not support setting the coverage class. Instead this
  * function monitors and modifies the corresponding MAC registers.
  */
-static void ath10k_hw_qca988x_set_coverage_class(struct ath10k* ar, int16_t value) {
+static void ath10k_hw_qca988x_set_coverage_class(struct ath10k* ar,
+        int16_t value) {
+    ZX_ASSERT(0);
+#if 0 // NEEDS PORTING
     uint32_t slottime_reg;
     uint32_t slottime;
     uint32_t timeout_reg;
@@ -571,6 +574,7 @@ store_regs:
 
 unlock:
     mtx_unlock(&ar->conf_mutex);
+#endif // NEEDS PORTING
 }
 
 /**
@@ -585,8 +589,9 @@ unlock:
  *
  * Return: 0 if successfully enable the pll, otherwise EINVAL
  */
-static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
-    int ret, wait_limit;
+static zx_status_t ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
+    zx_status_t ret;
+    int wait_limit;
     uint32_t clk_div_addr, pll_init_addr, speed_addr;
     uint32_t addr, reg_val, mem_val;
     struct ath10k_hw_params* hw;
@@ -597,7 +602,7 @@ static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
     if (ar->regs->core_clk_div_address == 0 ||
             ar->regs->cpu_pll_init_address == 0 ||
             ar->regs->cpu_speed_address == 0) {
-        return -EINVAL;
+        return ZX_ERR_INVALID_ARGS;
     }
 
     clk_div_addr = ar->regs->core_clk_div_address;
@@ -607,13 +612,13 @@ static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
     /* Read efuse register to find out the right hw clock configuration */
     addr = (RTC_SOC_BASE_ADDRESS | EFUSE_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* sanitize if the hw refclk index is out of the boundary */
     if (MS(reg_val, EFUSE_XTAL_SEL) > ATH10K_HW_REFCLK_COUNT) {
-        return -EINVAL;
+        return ZX_ERR_INVALID_ARGS;
     }
 
     hw_clk = &hw->hw_clk[MS(reg_val, EFUSE_XTAL_SEL)];
@@ -621,67 +626,67 @@ static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
     /* Set the rnfrac and outdiv params to bb_pll register */
     addr = (RTC_SOC_BASE_ADDRESS | BB_PLL_CONFIG_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     reg_val &= ~(BB_PLL_CONFIG_FRAC_MASK | BB_PLL_CONFIG_OUTDIV_MASK);
     reg_val |= (SM(hw_clk->rnfrac, BB_PLL_CONFIG_FRAC) |
                 SM(hw_clk->outdiv, BB_PLL_CONFIG_OUTDIV));
     ret = ath10k_bmi_write_soc_reg(ar, addr, reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* Set the correct settle time value to pll_settle register */
     addr = (RTC_WMAC_BASE_ADDRESS | WLAN_PLL_SETTLE_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     reg_val &= ~WLAN_PLL_SETTLE_TIME_MASK;
     reg_val |= SM(hw_clk->settle_time, WLAN_PLL_SETTLE_TIME);
     ret = ath10k_bmi_write_soc_reg(ar, addr, reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* Set the clock_ctrl div to core_clk_ctrl register */
     addr = (RTC_SOC_BASE_ADDRESS | SOC_CORE_CLK_CTRL_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     reg_val &= ~SOC_CORE_CLK_CTRL_DIV_MASK;
     reg_val |= SM(1, SOC_CORE_CLK_CTRL_DIV);
     ret = ath10k_bmi_write_soc_reg(ar, addr, reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* Set the clock_div register */
     mem_val = 1;
     ret = ath10k_bmi_write_memory(ar, clk_div_addr, &mem_val,
                                   sizeof(mem_val));
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* Configure the pll_control register */
     addr = (RTC_WMAC_BASE_ADDRESS | WLAN_PLL_CONTROL_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     reg_val |= (SM(hw_clk->refdiv, WLAN_PLL_CONTROL_REFDIV) |
                 SM(hw_clk->div, WLAN_PLL_CONTROL_DIV) |
                 SM(1, WLAN_PLL_CONTROL_NOPWD));
     ret = ath10k_bmi_write_soc_reg(ar, addr, reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* busy wait (max 1s) the rtc_sync status register indicate ready */
@@ -689,8 +694,8 @@ static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
     addr = (RTC_WMAC_BASE_ADDRESS | RTC_SYNC_STATUS_OFFSET);
     do {
         ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-        if (ret) {
-            return -EINVAL;
+        if (ret != ZX_OK) {
+            return ZX_ERR_INVALID_ARGS;
         }
 
         if (!MS(reg_val, RTC_SYNC_STATUS_PLL_CHANGING)) {
@@ -703,21 +708,21 @@ static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
     } while (wait_limit > 0);
 
     if (MS(reg_val, RTC_SYNC_STATUS_PLL_CHANGING)) {
-        return -EINVAL;
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* Unset the pll_bypass in pll_control register */
     addr = (RTC_WMAC_BASE_ADDRESS | WLAN_PLL_CONTROL_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     reg_val &= ~WLAN_PLL_CONTROL_BYPASS_MASK;
     reg_val |= SM(0, WLAN_PLL_CONTROL_BYPASS);
     ret = ath10k_bmi_write_soc_reg(ar, addr, reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* busy wait (max 1s) the rtc_sync status register indicate ready */
@@ -725,8 +730,8 @@ static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
     addr = (RTC_WMAC_BASE_ADDRESS | RTC_SYNC_STATUS_OFFSET);
     do {
         ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-        if (ret) {
-            return -EINVAL;
+        if (ret != ZX_OK) {
+            return ZX_ERR_INVALID_ARGS;
         }
 
         if (!MS(reg_val, RTC_SYNC_STATUS_PLL_CHANGING)) {
@@ -739,52 +744,52 @@ static int ath10k_hw_qca6174_enable_pll_clock(struct ath10k* ar) {
     } while (wait_limit > 0);
 
     if (MS(reg_val, RTC_SYNC_STATUS_PLL_CHANGING)) {
-        return -EINVAL;
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* Enable the hardware cpu clock register */
     addr = (RTC_SOC_BASE_ADDRESS | SOC_CPU_CLOCK_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     reg_val &= ~SOC_CPU_CLOCK_STANDARD_MASK;
     reg_val |= SM(1, SOC_CPU_CLOCK_STANDARD);
     ret = ath10k_bmi_write_soc_reg(ar, addr, reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* unset the nopwd from pll_control register */
     addr = (RTC_WMAC_BASE_ADDRESS | WLAN_PLL_CONTROL_OFFSET);
     ret = ath10k_bmi_read_soc_reg(ar, addr, &reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     reg_val &= ~WLAN_PLL_CONTROL_NOPWD_MASK;
     ret = ath10k_bmi_write_soc_reg(ar, addr, reg_val);
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* enable the pll_init register */
     mem_val = 1;
     ret = ath10k_bmi_write_memory(ar, pll_init_addr, &mem_val,
                                   sizeof(mem_val));
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
     /* set the target clock frequency to speed register */
     ret = ath10k_bmi_write_memory(ar, speed_addr, &hw->target_cpu_freq,
                                   sizeof(hw->target_cpu_freq));
-    if (ret) {
-        return -EINVAL;
+    if (ret != ZX_OK) {
+        return ZX_ERR_INVALID_ARGS;
     }
 
-    return 0;
+    return ZX_OK;
 }
 
 const struct ath10k_hw_ops qca988x_ops = {
@@ -792,7 +797,12 @@ const struct ath10k_hw_ops qca988x_ops = {
 };
 
 static int ath10k_qca99x0_rx_desc_get_l3_pad_bytes(struct htt_rx_desc* rxd) {
-    return MS(rxd->msdu_end.qca99x0.info1, RX_MSDU_END_INFO1_L3_HDR_PAD);
+    ZX_ASSERT(0);
+    return 0;
+#if 0 // NEEDS PORTING
+    return MS(rxd->msdu_end.qca99x0.info1,
+              RX_MSDU_END_INFO1_L3_HDR_PAD);
+#endif // NEEDS PORTING
 }
 
 const struct ath10k_hw_ops qca99x0_ops = {
