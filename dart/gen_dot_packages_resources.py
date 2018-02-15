307c34e136c63d9da02c99015c6bfc6a3e4b74f7
Zach Anderson <zra@google.com>
2018-02-15 10:20:14 -0800

    [flutter] Cleanup source package assembly script
    Change-Id: I3ff3d327941be0c3c6cedf8add53f03b708f48b1

--- dart/gen_dot_packages_resources.py
+++ dart/gen_dot_packages_resources.py
@@ -14,6 +14,65 @@ import subprocess
 import sys
 import urlparse
 
+# Returns the set of files touched by gen_snapshot.
+def GenSnapshotDeps(args):
+  depfile_sources = None
+  cmd = [
+    args.gen_snapshot,
+    '--print_dependencies',
+    '--dependencies_only',
+    '--packages=' + args.dot_packages,
+    '--vm_snapshot_data=/dev/null',
+    '--isolate_snapshot_data=/dev/null',
+  ]
+  for url_mapping in args.url_mapping:
+    cmd.append("--url_mapping=" + url_mapping)
+  cmd.append(args.main_dart)
+  try:
+    result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+  except subprocess.CalledProcessError as e:
+    print ("gen_snapshot failed: " + ' '.join(cmd) + "\n" +
+           "output: " + e.output)
+    return 1
+  if result:
+    depfile_sources = set(result.strip().split('\n'))
+  return depfile_sources
+
+
+def WritePackageToManifest(manifest_file, dot_packages_file, depfile_sources,
+                           package_name, package_path):
+  path = urlparse.urlparse(package_path)
+  path_len = len(path.path)
+  manifest_lib_path = os.path.join('data', 'dart-pkg', package_name, 'lib')
+  for root, dirs, files in os.walk(path.path):
+    for f in files:
+      # TODO(zra): What other files are needed?
+      if not f.endswith('.dart'):
+        continue
+      full_path = os.path.join(root, f)
+      # Only include files that are in depfile_sources
+      if depfile_sources == None or full_path in depfile_sources:
+        relative_path = full_path[path_len:]
+        manifest_path = os.path.join(manifest_lib_path, relative_path)
+        manifest_file.write(manifest_path + '=' + full_path + '\n')
+  dot_packages_file.write(
+      package_name + ':file:///pkg/' + manifest_lib_path + '/\n')
+
+
+def WriteManifest(args, depfile_sources):
+  with open(args.dot_packages) as dpfile, \
+       open(args.manifest_out, 'w') as mfile, \
+       open(args.dot_packages_out, 'w') as dpoutfile:
+    for line in dpfile:
+      package_and_uri = line.strip().split(':', 1)
+      WritePackageToManifest(mfile, dpoutfile, depfile_sources,
+                             package_and_uri[0], package_and_uri[1])
+    mfile.write(os.path.join('data', 'dart-pkg', '.packages') + '=' +
+                args.dot_packages_out + '\n')
+    mfile.write(os.path.join('data', 'dart-pkg', 'contents') + '=' +
+                args.contents_out + '\n')
+
+
 def main():
   parser = argparse.ArgumentParser(
       description="Generate a manifest from a Dart .packages file")
@@ -27,9 +86,11 @@ def main():
       help="Output .packages file to be included in the manifest",
       required=True)
   parser.add_argument("--gen-snapshot",
-      help="Path to gen_snapshot")
+      help="Path to gen_snapshot",
+      required=True)
   parser.add_argument("--main-dart",
-      help="main.dart entrypoint of the program.")
+      help="main.dart entrypoint of the program.",
+      required=True)
   parser.add_argument("--manifest-out",
       help="Output manifest file.",
       required=True)
@@ -44,21 +105,7 @@ def main():
 
   # Invoke gen_snapshot to get the list of .dart files used by the program.
   # This is used to filter out unused files from the package.
-  depfile_sources = None
-  if args.gen_snapshot != None:
-    cmd = [
-      args.gen_snapshot,
-      '--print_dependencies',
-      '--dependencies_only',
-      '--packages=' + args.dot_packages,
-      '--vm_snapshot_data=/dev/null',
-      '--isolate_snapshot_data=/dev/null',
-    ]
-    for url_mapping in args.url_mapping:
-      cmd.append("--url_mapping=" + url_mapping)
-    cmd.append(args.main_dart)
-    result = subprocess.check_output(cmd)
-    depfile_sources = set(result.strip().split('\n'))
+  depfile_sources = GenSnapshotDeps(args)
 
   # TODO(zra): At some point, it will likely make sense for the flutter content
   # handler to understand a metadata file describing the contents of the
@@ -68,29 +115,7 @@ def main():
   with open(args.contents_out, 'w') as cfile:
     cfile.write(args.package)
 
-  with open(args.dot_packages) as dpfile, \
-       open(args.manifest_out, 'w') as mfile, \
-       open(args.dot_packages_out, 'w') as dpoutfile:
-    for line in dpfile:
-      package_and_uri = line.strip().split(':', 1)
-      package_name = package_and_uri[0]
-      path = urlparse.urlparse(package_and_uri[1])
-      path_len = len(path.path)
-      manifest_lib_path = os.path.join('data', 'dart-pkg', package_name, 'lib')
-      for root, dirs, files in os.walk(path.path):
-        for f in files:
-          # TODO(zra): What other files are needed?
-          if not f.endswith('.dart'):
-            continue
-          full_path = os.path.join(root, f)
-          # Only include files that gen_snapshot found.
-          if depfile_sources == None or full_path in depfile_sources:
-            relative_path = full_path[path_len:]
-            manifest_path = os.path.join(manifest_lib_path, relative_path)
-            mfile.write(manifest_path + '=' + full_path + '\n')
-      dpoutfile.write(package_name + ':file:///pkg/' + manifest_lib_path + '/\n')
-    mfile.write(os.path.join('data', 'dart-pkg', '.packages') + '=' + args.dot_packages_out + '\n')
-    mfile.write(os.path.join('data', 'dart-pkg', 'contents') + '=' + args.contents_out + '\n')
+  WriteManifest(args, depfile_sources)
   return 0
 
 
