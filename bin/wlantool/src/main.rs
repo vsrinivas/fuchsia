4f5e47db4e61a053adada3cec818a91c9b339816
Tim Kilbourn <tkilbourn@google.com>
2018-02-18 22:28:23 -0800

    [wlantool] Create and destroy interfaces
    NET-457
    
    Change-Id: Ifab64c07b4cc531586a3536428437639aae3a7e7

--- bin/wlantool/src/main.rs
+++ bin/wlantool/src/main.rs
@@ -9,12 +9,13 @@ extern crate clap;
 extern crate failure;
 extern crate fidl;
 extern crate fuchsia_app;
-extern crate garnet_lib_wlan_fidl_service;
+extern crate garnet_lib_wlan_fidl as wlan;
+extern crate garnet_lib_wlan_fidl_service as wlan_service;
 extern crate tokio_core;
 
 use clap::{App, Arg, ArgMatches, SubCommand};
 use failure::{Error, ResultExt};
-use garnet_lib_wlan_fidl_service::DeviceService;
+use wlan_service::DeviceService;
 use tokio_core::reactor;
 
 // TODO(tkilbourn): decide whether to move to yaml or macros
@@ -23,6 +24,23 @@ const CMD_IFACE: &str = "iface";
 const CMD_LIST: &str = "list";
 const CMD_NEW: &str = "new";
 const CMD_DEL: &str = "del";
+
+arg_enum!{
+    #[derive(PartialEq, Debug)]
+    enum RoleArg {
+        Client,
+        Ap
+    }
+}
+
+impl ::std::convert::From<RoleArg> for wlan::MacRole {
+    fn from(arg: RoleArg) -> Self {
+        match arg {
+            RoleArg::Client => wlan::MacRole::Client,
+            RoleArg::Ap => wlan::MacRole::Ap,
+        }
+    }
+}
 
 fn main() {
     if let Err(e) = main_res() {
@@ -48,13 +66,33 @@ fn main_res() -> Result<(), Error> {
                         .about("creates a new iface device")
                         .arg(
                             Arg::with_name("phy_id")
+                                .short("p")
+                                .long("phy")
                                 .required(true)
+                                .takes_value(true)
                                 .help("id of the phy that will host the iface"),
+                        )
+                        .arg(
+                            Arg::with_name("role")
+                                .short("r")
+                                .long("role")
+                                .takes_value(true)
+                                .possible_values(&RoleArg::variants())
+                                .case_insensitive(true)
+                                .help("role of the new iface (CLIENT or AP)"),
                         ),
                 )
                 .subcommand(
                     SubCommand::with_name(CMD_DEL)
                         .about("destroys an iface device")
+                        .arg(
+                            Arg::with_name("phy_id")
+                                .short("p")
+                                .long("phy")
+                                .required(true)
+                                .takes_value(true)
+                                .help("id of the phy that will host the iface"),
+                        )
                         .arg(
                             Arg::with_name("iface_id")
                                 .required(true)
@@ -66,7 +104,7 @@ fn main_res() -> Result<(), Error> {
 
     match matches.subcommand() {
         (CMD_PHY, Some(m)) => do_phy(m)?,
-        (CMD_IFACE, _) => println!("NOT IMPLEMENTED"),
+        (CMD_IFACE, Some(m)) => do_iface(m)?,
         _ => println!("{}", matches.usage()),
     }
 
@@ -86,6 +124,37 @@ fn do_phy(args: &ArgMatches) -> Result<(), Error> {
             let response_fut = wlan_dev.list_phys();
             let response = core.run(response_fut).context("error getting response")?;
             println!("response: {:?}", response);
+        }
+        _ => println!("{}", args.usage()),
+    }
+    Ok(())
+}
+
+fn do_iface(args: &ArgMatches) -> Result<(), Error> {
+    let mut core = reactor::Core::new().context("error creating event loop")?;
+    let handle = core.handle();
+
+    let wlan_dev = fuchsia_app::client::connect_to_service::<DeviceService::Service>(&handle)
+        .context("failed to connect to device service")?;
+
+    match args.subcommand() {
+        (CMD_NEW, Some(create_args)) => {
+            let req = wlan_service::CreateIfaceRequest {
+                phy_id: value_t!(create_args, "phy_id", u16).unwrap(),
+                role: value_t!(create_args, "role", RoleArg).unwrap_or(RoleArg::Client).into(),
+            };
+            let response_fut = wlan_dev.create_iface(req);
+            let response = core.run(response_fut).context("error getting response")?;
+            println!("response: {:?}", response);
+        }
+        (CMD_DEL, Some(del_args)) => {
+            let iface_id = value_t!(del_args, "iface_id", u16).unwrap();
+            let req = wlan_service::DestroyIfaceRequest {
+                phy_id: value_t!(del_args, "phy_id", u16).unwrap(),
+                iface_id: iface_id,
+            };
+            wlan_dev.destroy_iface(req)?;
+            println!("deleted iface {:?}", iface_id);
         }
         _ => println!("{}", args.usage()),
     }
