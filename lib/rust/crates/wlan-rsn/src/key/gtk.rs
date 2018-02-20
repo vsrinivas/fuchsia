1140234307ee5aa1f10c7ed0d26943e387de1860
Robert Hahn <hahnr@google.com>
2018-02-20 10:39:02 -0600

    [wlan][rsn] Adds group key hierarchy
    NET-506
    
    Change-Id: I8c96071194cad22be31d55188fafdc85ccc82e8a

---
+++ lib/rust/crates/wlan-rsn/src/key/gtk.rs
@@ -0,0 +1,40 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use cipher::Cipher;
+use crypto_utils::prf;
+use {Error, Result};
+
+#[derive(Debug)]
+pub struct Gtk {
+    gtk: Vec<u8>,
+    tk_len: usize,
+
+    // TODO(hahnr): Add TKIP Tx/Rx MIC support (IEEE 802.11-2016, 12.8.2).
+}
+
+impl Gtk {
+    pub fn tk(&self) -> &[u8] {
+        &self.gtk[0..self.tk_len]
+    }
+}
+
+// IEEE 802.11-2016, 12.7.1.4
+pub fn new(gmk: &[u8], aa: &[u8; 6], gnonce: &[u8; 32], cipher: &Cipher) -> Result<Gtk> {
+    let tk_bits = cipher.tk_bits().ok_or_else(|| Error::PtkHierarchyUnsupportedCipherError)?;
+
+    // data length = 6 (aa) + 32 (gnonce)
+    let mut data: [u8; 38] = [0; 38];
+    data[0..6].copy_from_slice(&aa[..]);
+    data[6..].copy_from_slice(&gnonce[..]);
+
+    let gtk_bytes = prf(gmk, "Group key expansion", &data, tk_bits as usize)?;
+    let gtk = Gtk {
+        gtk: gtk_bytes,
+        tk_len: (tk_bits / 8) as usize,
+    };
+    Ok(gtk)
+}
+
+// TODO(hahnr): Add tests.
