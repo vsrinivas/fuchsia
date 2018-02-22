fca1ccf2febbf5af6cda308499226faa23c6baa0
Adam Barth <abarth@google.com>
2018-02-21 16:24:55 -0800

    [pty] Fix docs
    The path in the docs wasn't correct.
    
    Change-Id: I826c1aef55ace44a44cacadd398f2994c0af43c4

--- system/public/zircon/device/pty.h
+++ system/public/zircon/device/pty.h
@@ -6,14 +6,13 @@
 
 #include <stdint.h>
 #include <zircon/device/device.h>
-#include <zircon/device/ioctl.h>
 #include <zircon/device/ioctl-wrapper.h>
+#include <zircon/device/ioctl.h>
 #include <zircon/types.h>
-
 
 // A PTY (pseudoterminal) emulates terminal devices, with a
 // "server" side (which represents the keyboard+monitor side
-// of the terminal and is obtained by opening /dev/class/misc/pty)
+// of the terminal and is obtained by opening /dev/misc/ptmx)
 // and a number of "client" sides which are obtained by doing
 // an open_at(server_pty_fd, "0", O_RDWR) or
 // open_at(client_0_fd, "#", O_RDWR).
@@ -22,8 +21,6 @@
 // the open_at().  The first Client PTY *must* be 0, and it is
 // the only Client PTY that is allowed to create additional
 // Client PTYs, receive Events, etc.  It is the Controlling PTY.
-
-
 
 // IOCTLs allowed on Client PTYs
 // -----------------------------
@@ -55,8 +52,6 @@ typedef struct {
     uint32_t height;
 } pty_window_size_t;
 
-
-
 // IOCTLs allowed on the Controlling PTY
 // -------------------------------------
 
@@ -75,16 +70,14 @@ typedef struct {
 #define IOCTL_PTY_READ_EVENTS \
     IOCTL(IOCTL_KIND_DEFAULT, IOCTL_FAMILY_PTY, 0x13)
 
-#define PTY_EVENT_HANGUP      (1u) // no active client
-#define PTY_EVENT_INTERRUPT   (2u) // ^c
-#define PTY_EVENT_SUSPEND     (4u) // ^z
-#define PTY_EVENT_MASK        (7u) // all events
+#define PTY_EVENT_HANGUP (1u)    // no active client
+#define PTY_EVENT_INTERRUPT (2u) // ^c
+#define PTY_EVENT_SUSPEND (4u)   // ^z
+#define PTY_EVENT_MASK (7u)      // all events
 
 // When an event is pending, this signal is asserted
 // On the Controlling Client PTY
 #define PTY_SIGNAL_EVENT DEVICE_SIGNAL_OOB
-
-
 
 // IOCTLs allowed on the Server PTY
 // --------------------------------
@@ -95,8 +88,6 @@ typedef struct {
 // out: none
 #define IOCTL_PTY_SET_WINDOW_SIZE \
     IOCTL(IOCTL_KIND_DEFAULT, IOCTL_FAMILY_PTY, 0x20)
-
-
 
 IOCTL_WRAPPER_IN(ioctl_pty_clr_set_feature, IOCTL_PTY_CLR_SET_FEATURE, pty_clr_set_t);
 IOCTL_WRAPPER_OUT(ioctl_pty_get_window_size, IOCTL_PTY_GET_WINDOW_SIZE, pty_window_size_t);
