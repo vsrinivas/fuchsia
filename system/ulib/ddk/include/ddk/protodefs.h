4731d2a6c804289c0b7b0567d78c46bacf3d1393
Craig Stout <cstout@google.com>
2018-02-23 11:59:52 -0800

    [ddk] Add protocol for INTEL_GPU_CORE
    Change-Id: If8d8819104104caf228a15ccadbc474f9210b2fa

--- system/ulib/ddk/include/ddk/protodefs.h
+++ system/ulib/ddk/include/ddk/protodefs.h
@@ -63,6 +63,7 @@ DDK_PROTOCOL_DEF(I2C_HID,        'pIHD', "i2c-hid", 0)
 DDK_PROTOCOL_DEF(SERIAL,         'pSer', "serial", 0)
 DDK_PROTOCOL_DEF(SERIAL_DRIVER,  'pSrd', "serial-driver", 0)
 DDK_PROTOCOL_DEF(CLK,            'pCLK', "clock", 0)
+DDK_PROTOCOL_DEF(INTEL_GPU_CORE, 'pIGC', "intel-gpu-core", 0)
 // Protocol definition at garnet/magma/src/magma_util/platform/zircon/zircon_platform_ioctl.h
 DDK_PROTOCOL_DEF(GPU,            'pGPU', "gpu", 0)
 #undef DDK_PROTOCOL_DEF
