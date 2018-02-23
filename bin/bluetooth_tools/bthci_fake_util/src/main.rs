c91a79bbd5375f0217eb4b55bfdf70cf3fa90dc4
Benjamin Brittain <bwb@google.com>
2018-02-23 09:49:04 -0800

    [bt][testing] Adds bthci-fake-util to control fake bluetooth hci driver
    NET-375
    
    Change-Id: I8ef6ddd12641eb422870f58d957f737932595afa

---
+++ bin/bluetooth_tools/bthci_fake_util/src/main.rs
@@ -0,0 +1,61 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[macro_use]
+extern crate failure;
+extern crate rand;
+
+#[macro_use]
+extern crate fdio;
+extern crate fuchsia_bluetooth as bluetooth;
+extern crate fuchsia_zircon as zircon;
+
+use bluetooth::hci;
+use failure::Error;
+use rand::Rng;
+use std::fs::{File, OpenOptions};
+use std::path::Path;
+
+fn usage(appname: &str) -> (){
+    eprintln!("usage: {} [add|rm]", appname);
+}
+
+fn open_rdwr<P: AsRef<Path>>(path: P) -> Result<File, Error> {
+    OpenOptions::new().read(true).write(true).open(path).map_err(|e| e.into())
+}
+
+fn rm_device(dev_name: &str) -> Result<(), Error> {
+    let path = Path::new(hci::DEV_TEST).join(dev_name);
+    let dev = open_rdwr(path.clone())?;
+
+    hci::destroy_device(&dev)
+        .map(|_| println!("{:?} destroyed", path))
+}
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+    let appname = &args[0];
+    if args.len() < 2 {
+        usage(appname);
+        return;
+    }
+    // TODO(bwb) better arg parsing
+    let command = &args[1];
+
+    match command.as_str() {
+        "add" => {
+            hci::create_and_bind_device();
+        }
+        "rm" => {
+            if args.len() < 3 {
+                usage(appname);
+                return;
+            }
+            rm_device(args[2].as_str());
+        }
+        _ => {
+            usage(appname);
+        }
+    };
+}
