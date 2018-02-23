853f22b072e2f9d3ac41371f08f8d5ef30a7d113
Ankur Mittal <anmittal@google.com>
2018-02-22 16:15:19 -0800

    [logger] Add Abi test for zircon C ABI
    Adding test so that it fails when there is a change in zircon ABI. At
    that point zircon should be manually rolled forward to garnet.
    
    US-424
    
    Change-Id: I596988833c3f9487002cbfb953cd8006fc07ecbe

---
+++ bin/logger/test/logger_tests.cc
@@ -0,0 +1,29 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <syslog/wire_format.h>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+// This function will fail to build when zircon ABI changes
+// and we will need to manually roll changes.
+TEST(CAbi, Abi) {
+  static_assert(FX_LOG_MAX_DATAGRAM_LEN == 2032, "");
+  static_assert(sizeof(fx_log_metadata_t) == 32, "");
+  fx_log_packet_t packet;
+  static_assert(sizeof(packet.data) == 2000, "");
+
+  // test alignment
+  static_assert(offsetof(fx_log_packet_t, data) == 32, "");
+  static_assert(offsetof(fx_log_packet_t, metadata) == 0, "");
+  static_assert(offsetof(fx_log_metadata_t, pid) == 0, "");
+  static_assert(offsetof(fx_log_metadata_t, tid) == 8, "");
+  static_assert(offsetof(fx_log_metadata_t, time) == 16, "");
+  static_assert(offsetof(fx_log_metadata_t, severity) == 24, "");
+  static_assert(offsetof(fx_log_metadata_t, dropped_logs) == 28, "");
+}
+
+}  // namespace
