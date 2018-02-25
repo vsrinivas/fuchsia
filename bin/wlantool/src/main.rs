ae534dc8195adcb030e884e1434c37a36dbe3c27
Tim Kilbourn <tkilbourn@google.com>
2018-02-25 10:28:44 -0800

    [wlantool] Use structopt for argument parsing
    NET-457
    
    Change-Id: Ie9376233385969b40eb79cb4cdc683e5f5e795ac

--- bin/wlantool/src/main.rs
+++ bin/wlantool/src/main.rs
@@ -11,35 +11,21 @@ extern crate fidl;
 extern crate fuchsia_app;
 extern crate garnet_lib_wlan_fidl as wlan;
 extern crate garnet_lib_wlan_fidl_service as wlan_service;
+#[macro_use]
+extern crate structopt;
 extern crate tokio_core;
 
-use clap::{App, Arg, ArgMatches, SubCommand};
 use failure::{Error, ResultExt};
+use structopt::StructOpt;
 use wlan_service::DeviceService;
 use tokio_core::reactor;
 
-// TODO(tkilbourn): decide whether to move to yaml or macros
-const CMD_PHY: &str = "phy";
-const CMD_IFACE: &str = "iface";
-const CMD_LIST: &str = "list";
-const CMD_NEW: &str = "new";
-const CMD_DEL: &str = "del";
+mod opts;
+use opts::*;
 
-arg_enum!{
-    #[derive(PartialEq, Debug)]
-    enum RoleArg {
-        Client,
-        Ap
-    }
-}
-
-impl ::std::convert::From<RoleArg> for wlan::MacRole {
-    fn from(arg: RoleArg) -> Self {
-        match arg {
-            RoleArg::Client => wlan::MacRole::Client,
-            RoleArg::Ap => wlan::MacRole::Ap,
-        }
-    }
+struct ExecContext {
+    core: reactor::Core,
+    wlan_svc: <DeviceService::Service as fidl::FidlService>::Proxy,
 }
 
 fn main() {
@@ -49,114 +35,55 @@ fn main() {
 }
 
 fn main_res() -> Result<(), Error> {
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .subcommand(
-            SubCommand::with_name(CMD_PHY)
-                .about("commands for wlan phy devices")
-                .subcommand(SubCommand::with_name(CMD_LIST).about("lists phy devices")),
-        )
-        .subcommand(
-            SubCommand::with_name(CMD_IFACE)
-                .about("commands for wlan iface devices")
-                .subcommand(SubCommand::with_name(CMD_LIST).about("lists iface devices"))
-                .subcommand(
-                    SubCommand::with_name(CMD_NEW)
-                        .about("creates a new iface device")
-                        .arg(
-                            Arg::with_name("phy_id")
-                                .short("p")
-                                .long("phy")
-                                .required(true)
-                                .takes_value(true)
-                                .help("id of the phy that will host the iface"),
-                        )
-                        .arg(
-                            Arg::with_name("role")
-                                .short("r")
-                                .long("role")
-                                .takes_value(true)
-                                .possible_values(&RoleArg::variants())
-                                .case_insensitive(true)
-                                .help("role of the new iface (CLIENT or AP)"),
-                        ),
-                )
-                .subcommand(
-                    SubCommand::with_name(CMD_DEL)
-                        .about("destroys an iface device")
-                        .arg(
-                            Arg::with_name("phy_id")
-                                .short("p")
-                                .long("phy")
-                                .required(true)
-                                .takes_value(true)
-                                .help("id of the phy that will host the iface"),
-                        )
-                        .arg(
-                            Arg::with_name("iface_id")
-                                .required(true)
-                                .help("iface id to destroy"),
-                        ),
-                ),
-        )
-        .get_matches();
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 
-    match matches.subcommand() {
-        (CMD_PHY, Some(m)) => do_phy(m)?,
-        (CMD_IFACE, Some(m)) => do_iface(m)?,
-        _ => println!("{}", matches.usage()),
+    let core = reactor::Core::new().context("error creating event loop")?;
+    let handle = core.handle();
+    let cx = ExecContext {
+        core: core,
+        wlan_svc: fuchsia_app::client::connect_to_service::<DeviceService::Service>(&handle)
+            .context("failed to connect to device service")?,
+    };
+
+    match opt {
+        Opt::Phy(cmd) => do_phy(cmd, cx),
+        Opt::Iface(cmd) => do_iface(cmd, cx),
     }
-
-    Ok(())
 }
 
-fn do_phy(args: &ArgMatches) -> Result<(), Error> {
-    let mut core = reactor::Core::new().context("error creating event loop")?;
-    let handle = core.handle();
-
-    let wlan_dev = fuchsia_app::client::connect_to_service::<DeviceService::Service>(&handle)
-        .context("failed to connect to device service")?;
-
-    match args.subcommand() {
-        (CMD_LIST, _) => {
+fn do_phy(cmd: opts::PhyCmd, mut cx: ExecContext) -> Result<(), Error> {
+    match cmd {
+        opts::PhyCmd::List => {
             // TODO(tkilbourn): add timeouts to prevent hanging commands
-            let response_fut = wlan_dev.list_phys();
-            let response = core.run(response_fut).context("error getting response")?;
+            let response_fut = cx.wlan_svc.list_phys();
+            let response = cx.core.run(response_fut).context("error getting response")?;
             println!("response: {:?}", response);
+            Ok(())
         }
-        _ => println!("{}", args.usage()),
     }
-    Ok(())
 }
 
-fn do_iface(args: &ArgMatches) -> Result<(), Error> {
-    let mut core = reactor::Core::new().context("error creating event loop")?;
-    let handle = core.handle();
-
-    let wlan_dev = fuchsia_app::client::connect_to_service::<DeviceService::Service>(&handle)
-        .context("failed to connect to device service")?;
-
-    match args.subcommand() {
-        (CMD_NEW, Some(create_args)) => {
+fn do_iface(cmd: opts::IfaceCmd, mut cx: ExecContext) -> Result<(), Error> {
+    match cmd {
+        opts::IfaceCmd::New { phy_id, role } => {
             let req = wlan_service::CreateIfaceRequest {
-                phy_id: value_t!(create_args, "phy_id", u16).unwrap(),
-                role: value_t!(create_args, "role", RoleArg).unwrap_or(RoleArg::Client).into(),
+                phy_id: phy_id,
+                role: role.into(),
             };
-            let response_fut = wlan_dev.create_iface(req);
-            let response = core.run(response_fut).context("error getting response")?;
+            let response_fut = cx.wlan_svc.create_iface(req);
+            let response = cx.core.run(response_fut).context("error getting response")?;
             println!("response: {:?}", response);
+            Ok(())
         }
-        (CMD_DEL, Some(del_args)) => {
-            let iface_id = value_t!(del_args, "iface_id", u16).unwrap();
+        opts::IfaceCmd::Delete { phy_id, iface_id } => {
             let req = wlan_service::DestroyIfaceRequest {
-                phy_id: value_t!(del_args, "phy_id", u16).unwrap(),
+                phy_id: phy_id,
                 iface_id: iface_id,
             };
-            wlan_dev.destroy_iface(req)?;
+            cx.wlan_svc.destroy_iface(req)?;
             println!("deleted iface {:?}", iface_id);
+            Ok(())
         }
-        _ => println!("{}", args.usage()),
     }
-    Ok(())
 }
---
+++ bin/wlantool/src/opts.rs
@@ -0,0 +1,68 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use wlan;
+
+arg_enum!{
+    #[derive(PartialEq, Copy, Clone, Debug)]
+    pub enum RoleArg {
+        Client,
+        Ap
+    }
+}
+
+impl ::std::convert::From<RoleArg> for wlan::MacRole {
+    fn from(arg: RoleArg) -> Self {
+        match arg {
+            RoleArg::Client => wlan::MacRole::Client,
+            RoleArg::Ap => wlan::MacRole::Ap,
+        }
+    }
+}
+
+#[derive(StructOpt, Debug)]
+pub enum Opt {
+    #[structopt(name = "phy")]
+    /// commands for wlan phy devices
+    Phy(PhyCmd),
+
+    #[structopt(name = "iface")]
+    /// commands for wlan iface devices
+    Iface(IfaceCmd),
+}
+
+#[derive(StructOpt, Copy, Clone, Debug)]
+pub enum PhyCmd {
+    #[structopt(name = "list")]
+    /// lists phy devices
+    List,
+}
+
+#[derive(StructOpt, Copy, Clone, Debug)]
+pub enum IfaceCmd {
+    #[structopt(name = "new")]
+    /// creates a new iface device
+    New {
+        #[structopt(short = "p", long = "phy", raw(required = "true"))]
+        /// id of the phy that will host the iface
+        phy_id: u16,
+
+        #[structopt(short = "r", long = "role", raw(possible_values = "&RoleArg::variants()"),
+                    default_value = "Client", raw(case_insensitive = "true"))]
+        /// role of the new iface
+        role: RoleArg,
+    },
+
+    #[structopt(name = "del")]
+    /// destroys an iface device
+    Delete {
+        #[structopt(short = "p", long = "phy", raw(required = "true"))]
+        /// id of the phy that hosts the iface
+        phy_id: u16,
+
+        #[structopt(raw(required = "true"))]
+        /// iface id to destroy
+        iface_id: u16,
+    },
+}
