13f5645ee4c03ac0f3a24984325486c1bbf49e1d
Petr Hosek <phosek@google.com>
2018-02-28 10:07:14 -0800

    [fidl] Re-enable FIDL compiler fuzz target
    Now that the libFuzzer can be used with libc++ on the host, we can
    re-enable the FIDL compiler fuzz target.
    
    Change-Id: Iaf11087a517b32db54775126aa01539a562d80ea

---
+++ public/lib/fidl/fuzz/compiler.cc
@@ -0,0 +1,28 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/error_reporter.h>
+#include <fidl/lexer.h>
+#include <fidl/parser.h>
+#include <fidl/source_file.h>
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  std::string string_data(reinterpret_cast<const char*>(data), size);
+  // The lexer needs the last character to be zero.
+  string_data.append(1, 0);
+
+  fidl::SourceManager source_manager;
+  fidl::IdentifierTable identifier_table;
+  fidl::ErrorReporter error_reporter;
+  const fidl::SourceFile* source = source_manager.CreateSource(string_data);
+  fidl::Lexer lexer(*source, &identifier_table);
+  fidl::Parser parser(&lexer, &error_reporter);
+
+  auto raw_ast = parser.Parse();
+  if (!parser.Ok())
+    return 1;
+
+  return 0;
+}
