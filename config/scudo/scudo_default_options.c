9777bf1a80b04cd95f6eac2121a1fa84355f79b8
Roland McGrath <mcgrathr@google.com>
2018-03-02 16:11:09 -0800

    [config] Make scudo available as a variant
    This moves the Scudo support GN code into //build/config/scudo.
    
    There is now a "scudo" variant available via `select_variant`.
    The variant uses the new `scudo_default_options` build argument
    to control the built-in defaults for what can otherwised by tuned
    at runtime via the `SCUDO_OPTIONS` environment variable.
    
    The magic to ignore `use_scudo` in incompatible sanitizer
    variants is gone for now.  Until we're ready to decide on the
    final integration plan for Scudo, just don't set `use_scudo`
    and then use variants like `asan`.
    
    SEC-19 #comment Now available as a build variant.
    
    Change-Id: Idfcf3b39ba813a610364853fb0b1fcdfdd3fe021

---
+++ config/scudo/scudo_default_options.c
@@ -0,0 +1,14 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <sanitizer/scudo_interface.h>
+
+// This exists to be built into every executable selected to use the
+// scudo variant.  Scudo applies the options here before
+// looking at the SCUDO_OPTIONS environment variable.
+const char* __scudo_default_options(void) {
+  // This macro is defined by BUILD.gn from the `scudo_default_options` GN
+  // build argument.
+  return SCUDO_DEFAULT_OPTIONS;
+}
