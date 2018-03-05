17b4c3282decc706fca95650387e43bfadb118c3
Craig Stout <cstout@google.com>
2018-03-05 13:32:46 -0800

    [magma] Update build-doom3.sh
    Change-Id: I7cd753ef26daa5363ea7a772d1e886d1698eeec6

--- lib/magma/scripts/build-doom3.sh
+++ lib/magma/scripts/build-doom3.sh
@@ -11,11 +11,11 @@ build=${1:-debug-x86-64}
 builddir=out/build-doom3-$build
 
 if [[ $build == *"aarch64" ]]; then
-	shared_path=arm64
+	shared_path=arm64-shared
 	system_processor=aarch64
 
 else
-	shared_path=x64
+	shared_path=x64-shared
 	system_processor=x86_64
 fi
 
@@ -26,13 +26,16 @@ else
 fi
 
 export VULKAN_INCLUDE_DIR=$fuchsia_root/third_party/vulkan_loader_and_validation_layers/include
-export FUCHSIA_LIB_DIR=$fuchsia_root/out/$build/$shared_path-shared
+export SDL_INCLUDE_DIR=$fuchsia_root/third_party/sdl/include
+export FUCHSIA_LIB_DIR=$fuchsia_root/out/$build/$shared_path
 
 sysroot=$fuchsia_root/out/$build/sdks/zircon_sysroot/sysroot
 ninja_path=$fuchsia_root/buildtools/ninja
 
 mkdir -p $builddir
 pushd $builddir
-cmake $fuchsia_root/third_party/RBDOOM-3-BFG/neo -GNinja -DVULKAN=TRUE -DFFMPEG=FALSE -DCMAKE_PREFIX_PATH=$fuchsia_root/out/build-sdl-$build/install -DCMAKE_BUILD_TYPE=$cmake_build_type -DFUCHSIA_SYSTEM_PROCESSOR=$system_processor -DCMAKE_MAKE_PROGRAM=$ninja_path -DFUCHSIA_SYSROOT=$sysroot -DCMAKE_TOOLCHAIN_FILE=$fuchsia_root/build/Fuchsia.cmake
+cmake $fuchsia_root/third_party/RBDOOM-3-BFG/neo -GNinja -DVULKAN=TRUE -DFFMPEG=FALSE -DCMAKE_BUILD_TYPE=$cmake_build_type -DFUCHSIA_SYSTEM_PROCESSOR=$system_processor -DCMAKE_MAKE_PROGRAM=$ninja_path -DFUCHSIA_SYSROOT=$sysroot -DCMAKE_TOOLCHAIN_FILE=$fuchsia_root/build/Fuchsia.cmake
 $ninja_path
 popd
+
+cp -v $builddir/libRBDoom3BFG.so $FUCHSIA_LIB_DIR
