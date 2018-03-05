845979febc2efec7681462432f0d7ded0dab6de1
Roland McGrath <mcgrathr@google.com>
2018-03-04 18:22:09 -0800

    [build-zircon.sh] Simplify script for reunified Zircon builds
    The -p switch is gone.  The -t switch takes a GN target_cpu value.
    With no -t switch, *both* arm64 and x64 are built.
    
    For each target enabled, build just one build-${target_cpu} containing
    the full Zircon build.  build-${target_cpu}-ulib contains the
    alternate (ASan) libraries.  The build-user-* directories are gone.
    
    Change-Id: Id42d9e78f4b1b286f30acd9c2328a3788fc4092d

--- build-zircon.sh
+++ build-zircon.sh
@@ -6,10 +6,6 @@
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
 
-readonly HOST_ARCH=$(uname -m)
-readonly HOST_OS=$(uname | tr '[:upper:]' '[:lower:]')
-readonly HOST_TRIPLE="${HOST_ARCH}-${HOST_OS}"
-
 JOBS=`getconf _NPROCESSORS_ONLN` || {
   Cannot get number of processors
   exit 1
@@ -25,8 +21,7 @@ usage() {
   echo "  -V: Level 2 verbosity"
   echo "  -A: Build with ASan"
   echo "  -H: Build host tools with ASan"
-  echo "  -p <projects>: Kernel projects to build."
-  echo "  -t <target>: Kernel target to build."
+  echo "  -t <target>: Architecture (GN style) to build, instead of all"
   echo "  -o <outdir>: Directory in which to put the build-zircon directory."
   echo ""
   echo "Note: Passing extra arguments to make is not supported."
@@ -37,6 +32,7 @@ declare CLEAN="${CLEAN:-false}"
 declare HOST_ASAN="${HOST_ASAN:-false}"
 declare OUTDIR="${OUTDIR:-${ROOT_DIR}/out}"
 declare VERBOSE="${VERBOSE:-0}"
+declare -a ARCHLIST=(arm64 x64)
 
 while getopts "AcHht:p:o:vV" opt; do
   case "${opt}" in
@@ -45,56 +41,16 @@ while getopts "AcHht:p:o:vV" opt; do
     H) HOST_ASAN="true" ;;
     h) usage ; exit 0 ;;
     o) OUTDIR="${OPTARG}" ;;
-    t) TARGET="${OPTARG}" ;;
-    p) PROJECTS="${OPTARG}" ;;
+    t) ARCHLIST=("${OPTARG}") ;;
     v) VERBOSE="1" ;;
     V) VERBOSE="2" ;;
     *) usage 1>&2 ; exit 1 ;;
   esac
 done
 
-make_zircon_common() {
-  make --no-print-directory -C "${ROOT_DIR}/zircon" \
-    -j ${JOBS} DEBUG_BUILDROOT=../../zircon "$@"
-}
-
-make_zircon_target() {
-  make_zircon_common \
-    BUILDROOT=${ZIRCON_BUILDROOT} TOOLS=${OUTDIR}/build-zircon/tools "$@"
-}
-
-if [[ "${PROJECTS}" = "" ]]; then
-    if [[ "${TARGET}" != "" ]]; then
-        PROJECTS="${TARGET}"
-    else
-        PROJECTS="x64" # Default
-    fi
-fi
-
-have_arm64=false
-have_x64=false
-IFS=','
-for project in $PROJECTS; do
-    case "$project" in
-    *x64*) have_x64=true ;;
-    *) have_arm64=true ;;
-    esac
-done
-declare -a ARCHLIST
-if $have_arm64; then
-    ARCHLIST+=(arm64)
-fi
-if $have_x64; then
-    ARCHLIST+=(x64)
-fi
-readonly -a ARCHLIST
-if [[ "${#ARCHLIST[@]}" == 0 ]]; then
-    echo >&2 "Cannot figure out architectures from $PROJECTS"
-    exit 2
-fi
-
 readonly ASAN CLEAN HOST_ASAN PROJECTS OUTDIR VERBOSE
 readonly ZIRCON_BUILDROOT="${OUTDIR}/build-zircon"
+readonly -a ARCHLIST
 
 if [[ "${CLEAN}" = "true" ]]; then
   rm -rf -- "${ZIRCON_BUILDROOT}"
@@ -116,22 +72,28 @@ else
   readonly ASAN_ULIB=true
 fi
 
+make_zircon_common() {
+  (test $QUIET -ne 0 || set -x
+   exec make --no-print-directory -C "${ROOT_DIR}/zircon" \
+             -j ${JOBS} DEBUG_BUILDROOT=../../zircon "$@")
+}
+
 # Build host tools.
 make_zircon_common \
   BUILDDIR=${OUTDIR}/build-zircon HOST_USE_ASAN="${HOST_ASAN}" tools
 
+make_zircon_target() {
+  make_zircon_common \
+    BUILDROOT=${ZIRCON_BUILDROOT} TOOLS=${OUTDIR}/build-zircon/tools "$@"
+}
+
 for ARCH in "${ARCHLIST[@]}"; do
-    # Build primary userland and sysroot.
-    make_zircon_target PROJECT="user-${ARCH}" \
-        BUILDDIR_SUFFIX= USE_ASAN="${ASAN_ZIRCON}" user-only
+    # Build full bootloader, kernel, userland, and sysroot.
+    make_zircon_target PROJECT="${ARCH}" \
+        BUILDDIR_SUFFIX= USE_ASAN="${ASAN_ZIRCON}"
+
     # Build alternate shared libraries (ASan).
-    make_zircon_target PROJECT="user-${ARCH}" \
+    make_zircon_target PROJECT="${ARCH}" \
         BUILDDIR_SUFFIX=-ulib USE_ASAN="${ASAN_ULIB}" \
         ENABLE_ULIB_ONLY=true ENABLE_BUILD_SYSROOT=false
-done
-
-# Build kernels and bootloaders.
-for project in $PROJECTS; do
-    make_zircon_target PROJECT="$project" kernel-only
-    make_zircon_target PROJECT="$project" bootloader
 done
