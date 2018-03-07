5bf0bed3424275ec7222d153a0daf90a20723c89
Dale Sather <dalesat@google.com>
2018-03-07 10:35:37 -0800

    [fxl] add type_converter.h
    TypeConverter is a template for type conversion. It allows
    conversions to be done like so:
    
      X x = fxl::To<X>(y);
    
    This code was adapted from FIDL's TypeConverter.
    
    Change-Id: Ic4787f3ffba45824615269c0f5c883c1b7c6265b

---
+++ public/lib/fxl/type_converter.h
@@ -0,0 +1,43 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_FXL_TYPE_CONVERTER_H_
+#define LIB_FXL_TYPE_CONVERTER_H_
+
+namespace fxl {
+
+// Specialize the following class:
+//   template <typename T, typename U> struct TypeConverter;
+// to perform disired type conversions. Here, T is the target type; U is the
+// input type.
+//
+// To convert from type Y to type X, create a specialization of TypeConverter
+// like this:
+//
+//   namespace fxl {
+//
+//   template <>
+//   struct TypeConverter<X, Y> {
+//     static X Convert(const Y& input);
+//   };
+//
+//   } // namespace fxl
+//
+// With this specialization, it's possible to write code like this:
+//
+//   Y y;
+//   X x = fxl::To<X>(y);
+//
+
+template <typename T, typename U>
+struct TypeConverter;
+
+template <typename T, typename U>
+inline T To(const U& obj) {
+  return TypeConverter<T, U>::Convert(obj);
+};
+
+}  // namespace fxl
+
+#endif  // LIB_FXL_TYPE_CONVERTER_H_
