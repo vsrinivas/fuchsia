44edccd8c3ca219a5cedc7df0af287f631b5e8d5
Ankur Mittal <anmittal@google.com>
2018-03-08 15:27:30 -0800

    [syslog] Add go client library.
    US-426 #comment
    
    Change-Id: I74a13aa3f50b6e41b5e2c4269c96667d1abcc877

---
+++ go/src/syslog_tests/syslog_test.go
@@ -0,0 +1,233 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+	"syscall/zx"
+	"syslog/logger"
+	"testing"
+)
+
+var (
+	Pid = uint64(os.Getpid())
+)
+
+func TestLogSimpleWithWriter(t *testing.T) {
+	options := logger.GetDefaultInitOptions()
+	tmpFile, err := ioutil.TempFile("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpFile.Name())
+	options.ConsoleWriter = tmpFile
+	logger, err := logger.NewLogger(options)
+	if err != nil {
+		t.Fatal(err)
+	}
+	logger.Infof("integer: %d", 10)
+	tmpFile.Sync()
+	tmpFile.Seek(0, 0)
+	var buf bytes.Buffer
+	io.Copy(&buf, tmpFile)
+	expected := "INFO: integer: 10\n"
+	got := buf.String()
+	if !strings.HasSuffix(got, expected) {
+		t.Errorf("%q should have ended in %q", got, expected)
+	}
+	if !strings.Contains(got, fmt.Sprintf("[%d]", Pid)) {
+		t.Errorf("%q should contains %d", got, Pid)
+	}
+}
+func setup(t *testing.T, tags ...string) (zx.Socket, *logger.Logger) {
+	options := logger.GetDefaultInitOptions()
+	options.Tags = tags
+	sin, sout, err := zx.NewSocket(logger.SOCKET_DATAGRAM)
+	if err != nil {
+		t.Fatal(err)
+	}
+	options.LogServiceChannel = &sout
+	log, err := logger.NewLogger(options)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return sin, log
+}
+
+func checkoutput(t *testing.T, sin zx.Socket, expectedMsg string, severity logger.LogLevel, tags ...string) {
+	var data [logger.SOCKET_BUFFER_LEN]byte
+	if n, err := sin.Read(data[:], 0); err != nil {
+		t.Fatal(err)
+	} else {
+		if n <= 32 {
+			t.Fatalf("got invalid data: %v", data[:n])
+		}
+		got_pid := binary.LittleEndian.Uint64(data[0:8])
+		got_tid := binary.LittleEndian.Uint64(data[8:16])
+		got_time := binary.LittleEndian.Uint64(data[16:24])
+		got_severity := int32(binary.LittleEndian.Uint32(data[24:28]))
+		got_dropped_logs := int32(binary.LittleEndian.Uint32(data[28:32]))
+
+		if Pid != got_pid {
+			t.Errorf("pid error, got: %d, want: %d", got_pid, Pid)
+		}
+
+		if 0 != got_tid {
+			t.Errorf("tid error, got: %d, want: %d", got_tid, 0)
+		}
+
+		if int32(severity) != got_severity {
+			t.Errorf("severity error, got: %d, want: %d", got_severity, severity)
+		}
+
+		if got_time <= 0 {
+			t.Errorf("time %d should be greater than zero", got_time)
+		}
+
+		if 0 != got_dropped_logs {
+			t.Errorf("dropped logs error, got: %d, want: %d", got_dropped_logs, 0)
+		}
+		pos := 32
+		for i, tag := range tags {
+			length := len(tag)
+			if data[pos] != byte(length) {
+				t.Fatalf("tag iteration %d: expected data to be %d at pos %d, got %d", i, length, pos, data[pos])
+			}
+			pos = pos + 1
+			got_tag := string(data[pos : pos+length])
+			if tag != got_tag {
+				t.Fatalf("tag iteration %d: expected tag %q , got %q", i, tag, got_tag)
+			}
+			pos = pos + length
+		}
+
+		if data[pos] != 0 {
+			t.Fatalf("byte before msg start should be zero, got: %d, %v", data[pos], data[32:n])
+		}
+
+		msg_got := string(data[pos+1 : n-1])
+		if expectedMsg != msg_got {
+			t.Fatalf("expected msg:%q, got %q", expectedMsg, msg_got)
+		}
+		if data[n-1] != 0 {
+			t.Fatalf("last byte should be zero, got: %d, %v", data[pos], data[32:n])
+		}
+	}
+}
+
+// Test simple logging with socket
+func TestLogSimple(t *testing.T) {
+	sin, log := setup(t)
+	format := "integer: %d"
+	log.Infof(format, 10)
+	expectedMsg := fmt.Sprintf(format, 10)
+	checkoutput(t, sin, expectedMsg, logger.InfoLevel)
+}
+
+func TestLogWithLocalTag(t *testing.T) {
+	sin, log := setup(t)
+	format := "integer: %d"
+	log.InfoTf("local_tag", format, 10)
+	expectedMsg := fmt.Sprintf(format, 10)
+	checkoutput(t, sin, expectedMsg, logger.InfoLevel, "local_tag")
+}
+
+func TestLogWithGlobalTags(t *testing.T) {
+	sin, log := setup(t, "gtag1", "gtag2")
+	format := "integer: %d"
+	log.InfoTf("local_tag", format, 10)
+	expectedMsg := fmt.Sprintf(format, 10)
+	checkoutput(t, sin, expectedMsg, logger.InfoLevel, "gtag1", "gtag2", "local_tag")
+}
+
+func TestLoggerSeverity(t *testing.T) {
+	sin, log := setup(t)
+	format := "integer: %d"
+	log.SetSeverity(logger.WarningLevel)
+	log.Infof(format, 10)
+	if n, err := sin.Read(make([]byte, 0), 0); err != nil {
+		t.Fatal(err)
+	} else if n != 0 {
+		t.Fatalf("expected n: 0, got %d", n)
+	}
+	log.Warnf(format, 10)
+	expectedMsg := fmt.Sprintf(format, 10)
+	checkoutput(t, sin, expectedMsg, logger.WarningLevel)
+}
+
+func TestLoggerVerbosity(t *testing.T) {
+	sin, log := setup(t)
+	format := "integer: %d"
+	log.VLogf(2, format, 10)
+	if n, err := sin.Read(make([]byte, 0), 0); err != nil {
+		t.Fatal(err)
+	} else if n != 0 {
+		t.Fatalf("expected n: 0, got %d", n)
+	}
+	log.SetVerbosity(2)
+	log.VLogf(2, format, 10)
+	expectedMsg := fmt.Sprintf(format, 10)
+	checkoutput(t, sin, expectedMsg, logger.LogLevel(-2))
+}
+
+func TestGlobalTagLimits(t *testing.T) {
+	options := logger.GetDefaultInitOptions()
+	tags := [logger.MAX_GLOBAL_TAGS + 1]string{}
+	for i := 0; i < len(tags); i++ {
+		tags[i] = "a"
+	}
+	options.Tags = tags[:]
+	if _, err := logger.NewLogger(options); err != logger.ErrInvalidArg {
+		t.Fatalf("expected error 'ErrInvalidArg' got: %s", err)
+	}
+	options.Tags = tags[0:logger.MAX_GLOBAL_TAGS]
+	var tag [logger.MAX_TAG_LEN + 1]byte
+	for i := 0; i < len(tag); i++ {
+		tag[i] = 65
+	}
+	options.Tags[1] = string(tag[:])
+	if _, err := logger.NewLogger(options); err != logger.ErrInvalidArg {
+		t.Fatalf("expected error 'ErrInvalidArg' got: %s", err)
+	}
+}
+
+func TestLocalTagLimits(t *testing.T) {
+	sin, log := setup(t)
+	format := "integer: %d"
+	var tag [logger.MAX_TAG_LEN + 1]byte
+	for i := 0; i < len(tag); i++ {
+		tag[i] = 65
+	}
+	log.InfoTf(string(tag[:]), format, 10)
+	expectedMsg := fmt.Sprintf(format, 10)
+	checkoutput(t, sin, expectedMsg, logger.InfoLevel, string(tag[:logger.MAX_TAG_LEN]))
+}
+
+func TestMessageLenLimit(t *testing.T) {
+	sin, log := setup(t)
+	msgLen := logger.SOCKET_BUFFER_LEN - 32 - 1 - 1 // 1 for starting and ending null bytes
+	format := fmt.Sprintf("%%0%ddstart", msgLen-3)
+	if err := log.Infof(format, 10); err != nil {
+		if err2 := logger.ToErrMsgTooLong(err); err2 == nil {
+			t.Fatal(err)
+		} else {
+			if err2.Msg != "start" {
+				t.Fatalf("expected 'start' got %q", err2.Msg)
+			}
+		}
+	}
+	expectedMsg := fmt.Sprintf(format, 10)
+	expectedMsgBytes := []byte(expectedMsg)
+	expectedMsgBytes = expectedMsgBytes[:msgLen]
+	copy(expectedMsgBytes[msgLen-3:msgLen], "...")
+	expectedMsg = string(expectedMsgBytes[:]) // "<0000...ntimes>10..."
+	checkoutput(t, sin, expectedMsg, logger.InfoLevel)
+}
---
+++ public/lib/syslog/go/src/syslog/logger/logger.go
@@ -0,0 +1,379 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package logger
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strings"
+	"sync/atomic"
+	"syscall/zx"
+)
+
+var (
+	ErrNotInitialized   = errors.New("default logger not initialized")
+	ErrInitialized      = errors.New("default logger is already initialized")
+	ErrInvalidArg       = errors.New("Invalid arguments to logger")
+	ErrMsgTooLongString = "Msg too long. it was truncated"
+)
+
+// This is returned when msg is truncated. It contains the truncated part.
+type ErrMsgTooLong struct {
+	Msg string // rest of the message
+}
+
+func (e ErrMsgTooLong) Error() string {
+	return ErrMsgTooLongString
+}
+
+const (
+	MAX_GLOBAL_TAGS   = 4
+	MAX_TAG_LEN       = 63
+	SOCKET_BUFFER_LEN = 2032
+	SOCKET_DATAGRAM   = 1 << 0
+)
+
+type LogLevel int32
+
+const (
+	InfoLevel LogLevel = iota
+	WarningLevel
+	ErrorLevel
+	FatalLevel
+)
+
+func (ll LogLevel) String() string {
+	switch ll {
+	case InfoLevel:
+		return "INFO"
+	case WarningLevel:
+		return "WARNING"
+	case ErrorLevel:
+		return "ERROR"
+	case FatalLevel:
+		return "FATAL"
+	default:
+		if int(ll) < 0 {
+			return fmt.Sprintf("VLOG(%d)", -(int(ll)))
+		}
+		return "INVALID"
+	}
+}
+
+type LogInitOptions struct {
+	Loglevel          LogLevel
+	ConsoleWriter     io.Writer
+	LogServiceChannel *zx.Socket
+	Tags              []string
+}
+
+func GetDefaultInitOptions() LogInitOptions {
+	return LogInitOptions{
+		Loglevel:          InfoLevel,
+		ConsoleWriter:     nil,
+		LogServiceChannel: nil,
+		Tags:              nil,
+	}
+}
+
+type Logger struct {
+	logLevel    LogLevel
+	tags        []string
+	socket      *zx.Socket
+	writer      io.Writer
+	tagString   string
+	pid         uint64
+	droppedLogs uint32
+}
+
+func (l *Logger) setTags(tags []string) error {
+	if len(tags) > MAX_GLOBAL_TAGS {
+		return ErrInvalidArg
+	}
+	for _, tag := range tags {
+		if len(tag) > MAX_TAG_LEN {
+			return ErrInvalidArg
+		}
+	}
+	if l.writer != nil {
+		l.tagString = strings.Join(tags, ", ")
+	}
+	l.tags = tags
+	return nil
+}
+
+func NewLogger(options LogInitOptions) (*Logger, error) {
+	l := Logger{
+		logLevel: options.Loglevel,
+		writer:   options.ConsoleWriter,
+		socket:   options.LogServiceChannel,
+		pid:      uint64(os.Getpid()),
+	}
+	if options.ConsoleWriter == nil && options.LogServiceChannel == nil {
+		// TODO (anmittal): change it to connect to garnet service
+		l.writer = os.Stderr
+	}
+	if err := l.setTags(options.Tags); err != nil {
+		return nil, err
+	}
+	return &l, nil
+}
+
+func (l *Logger) logToWriter(time zx.Time, logLevel LogLevel, tag, msg string) error {
+	if len(l.tagString) != 0 {
+		if len(tag) != 0 {
+			tag = fmt.Sprintf("%s, %s", l.tagString, tag)
+		} else {
+			tag = l.tagString
+		}
+	}
+	_, err := io.WriteString(l.writer, fmt.Sprintf("[%05d.%06d][%d][0][%s] %s: %s\n", time/1000000000, (time/1000)%1000000, l.pid, tag, logLevel, msg))
+	return err
+}
+
+func (l *Logger) logToSocket(time zx.Time, logLevel LogLevel, tag, msg string) error {
+	var buffer [SOCKET_BUFFER_LEN]byte
+	binary.LittleEndian.PutUint64(buffer[0:8], l.pid)
+	binary.LittleEndian.PutUint64(buffer[8:16], 0) // golang doesn't have tid
+	binary.LittleEndian.PutUint64(buffer[16:24], uint64(time))
+	binary.LittleEndian.PutUint32(buffer[24:28], uint32(logLevel))
+	binary.LittleEndian.PutUint32(buffer[28:32], atomic.LoadUint32(&l.droppedLogs))
+	pos := 32
+
+	// Write global tags
+	for _, tag := range l.tags {
+		length := len(tag)
+		if length == 0 {
+			continue
+		}
+		buffer[pos] = byte(length)
+		pos = pos + 1
+		copy(buffer[pos:pos+length], tag)
+		pos = pos + length
+	}
+
+	// Write local tags
+	if len(tag) != 0 {
+		length := len(tag)
+		buffer[pos] = byte(length)
+		pos = pos + 1
+		copy(buffer[pos:pos+length], tag)
+		pos = pos + length
+	}
+
+	//write msg
+	buffer[pos] = 0
+	pos = pos + 1
+	errMsgTooLong := ErrMsgTooLong{""}
+	if len(msg) >= SOCKET_BUFFER_LEN-pos {
+		// truncate
+		errMsgTooLong.Msg = msg[SOCKET_BUFFER_LEN-pos-4:]
+		msg = msg[:SOCKET_BUFFER_LEN-pos-4]
+		copy(buffer[pos:], msg)
+		copy(buffer[pos+len(msg):], "...")
+		pos = pos + len(msg) + 3
+	} else {
+		copy(buffer[pos:], msg)
+		pos = pos + len(msg)
+	}
+
+	buffer[pos] = 0
+	if _, err := l.socket.Write(buffer[:pos+1], 0); err != nil {
+		atomic.AddUint32(&l.droppedLogs, 1)
+		return err
+	}
+	if errMsgTooLong.Msg != "" {
+		return errMsgTooLong
+	}
+	return nil
+}
+
+func (l *Logger) logf(callDepth int, logLevel LogLevel, tag string, format string, a ...interface{}) error {
+	if l.logLevel > logLevel {
+		return nil
+	}
+	time := zx.Sys_clock_get(0) //MONOTONIC
+	msg := fmt.Sprintf(format, a...)
+	if logLevel >= ErrorLevel {
+		_, file, line, ok := runtime.Caller(callDepth)
+		if !ok {
+			file = "???"
+			line = 0
+		} else {
+			short := file
+			for i := len(file) - 1; i > 0; i-- {
+				if file[i] == '/' {
+					short = file[i+1:]
+					break
+				}
+			}
+			file = short
+		}
+		msg = fmt.Sprintf("%s(%d): %s ", file, line, msg)
+	}
+	if len(tag) > MAX_TAG_LEN {
+		tag = tag[:MAX_TAG_LEN]
+	}
+	if logLevel == FatalLevel {
+		defer os.Exit(1)
+	}
+	if l.writer != nil {
+		return l.logToWriter(time, logLevel, tag, msg)
+	} else {
+		return l.logToSocket(time, logLevel, tag, msg)
+	}
+}
+
+func (l *Logger) SetSeverity(logLevel LogLevel) {
+	l.logLevel = logLevel
+}
+
+func (l *Logger) SetVerbosity(verbosity int) {
+	l.logLevel = LogLevel(-verbosity)
+}
+
+func (l *Logger) Infof(format string, a ...interface{}) error {
+	return l.logf(2, InfoLevel, "", format, a...)
+}
+
+func (l *Logger) Warnf(format string, a ...interface{}) error {
+	return l.logf(2, WarningLevel, "", format, a...)
+}
+
+func (l *Logger) Errorf(format string, a ...interface{}) error {
+	return l.logf(2, ErrorLevel, "", format, a...)
+}
+
+func (l *Logger) Fatalf(format string, a ...interface{}) error {
+	return l.logf(2, FatalLevel, "", format, a...)
+}
+
+func (l *Logger) VLogf(verbosity int, format string, a ...interface{}) error {
+	return l.logf(2, LogLevel(-verbosity), "", format, a...)
+}
+
+func (l *Logger) InfoTf(tag, format string, a ...interface{}) error {
+	return l.logf(2, InfoLevel, tag, format, a...)
+}
+
+func (l *Logger) WarnTf(tag, format string, a ...interface{}) error {
+	return l.logf(2, WarningLevel, tag, format, a...)
+}
+
+func (l *Logger) ErrorTf(tag, format string, a ...interface{}) error {
+	return l.logf(2, ErrorLevel, tag, format, a...)
+}
+
+func (l *Logger) FatalTf(tag, format string, a ...interface{}) error {
+	return l.logf(2, FatalLevel, tag, format, a...)
+}
+
+func (l *Logger) VLogTf(verbosity int, tag, format string, a ...interface{}) error {
+	return l.logf(2, LogLevel(-verbosity), tag, format, a...)
+}
+
+var defaultLogger *Logger
+
+func logf(callDepth int, logLevel LogLevel, tag string, format string, a ...interface{}) error {
+	l := GetDefaultLogger()
+	if l != nil {
+		return l.logf(callDepth+1, logLevel, tag, format, a...)
+	} else {
+		return ErrNotInitialized
+	}
+}
+
+// Public APIs for default logger
+
+func GetDefaultLogger() *Logger {
+	return defaultLogger
+}
+
+func InitDefaultLogger() error {
+	return InitDefaultLoggerWithConfig(GetDefaultInitOptions())
+}
+
+func InitDefaultLoggerWithTags(tags ...string) error {
+	o := GetDefaultInitOptions()
+	o.Tags = tags
+	return InitDefaultLoggerWithConfig(o)
+}
+
+func InitDefaultLoggerWithConfig(options LogInitOptions) error {
+	if defaultLogger != nil {
+		return ErrInitialized
+	}
+
+	if l, err := NewLogger(options); err != nil {
+		return err
+	} else {
+		defaultLogger = l
+	}
+	return nil
+}
+
+func SetSeverity(logLevel LogLevel) {
+	if l := GetDefaultLogger(); l != nil {
+		l.logLevel = logLevel
+	}
+}
+
+func SetVerbosity(verbosity int) {
+	if l := GetDefaultLogger(); l != nil {
+		l.logLevel = LogLevel(-verbosity)
+	}
+}
+
+func Infof(format string, a ...interface{}) error {
+	return logf(2, InfoLevel, "", format, a...)
+}
+
+func Warnf(format string, a ...interface{}) error {
+	return logf(2, WarningLevel, "", format, a...)
+}
+
+func Errorf(format string, a ...interface{}) error {
+	return logf(2, ErrorLevel, "", format, a...)
+}
+
+func Fatalf(format string, a ...interface{}) error {
+	return logf(2, FatalLevel, "", format, a...)
+}
+
+func VLogf(verbosity int, format string, a ...interface{}) error {
+	return logf(2, LogLevel(-verbosity), "", format, a...)
+}
+
+func InfoTf(tag, format string, a ...interface{}) error {
+	return logf(2, InfoLevel, tag, format, a...)
+}
+
+func WarnTf(tag, format string, a ...interface{}) error {
+	return logf(2, WarningLevel, tag, format, a...)
+}
+
+func ErrorTf(tag, format string, a ...interface{}) error {
+	return logf(2, ErrorLevel, tag, format, a...)
+}
+
+func FatalTf(tag, format string, a ...interface{}) error {
+	return logf(2, FatalLevel, tag, format, a...)
+}
+
+func VLogTf(verbosity int, tag, format string, a ...interface{}) error {
+	return logf(2, LogLevel(-verbosity), tag, format, a...)
+}
+
+func ToErrMsgTooLong(e error) *ErrMsgTooLong {
+	if err, ok := e.(ErrMsgTooLong); ok {
+		return &err
+	} else {
+		return nil
+	}
+}
