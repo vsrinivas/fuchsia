441cf6b3ac29621ca867085d4e93c03175905cbf
P.Y. Laligand <pylaligand@google.com>
2018-03-09 10:04:02 -0800

    [pkg] Update the visualization tool for build packages.
    TO-866
    
    Change-Id: I47f7cff36579c7b18ad7054654863bd58339207a

--- visualize_module_tree.py
+++ visualize_module_tree.py
@@ -3,33 +3,74 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Generates a DOT file representing the hierarchy of build modules.
-
+import argparse
 import json
 import os
-import paths
 import sys
+
+FUCHSIA_ROOT = os.path.dirname(  # $root
+    os.path.dirname(             # scripts
+    os.path.abspath(__file__)))
+
+
+def get_package_imports(package):
+    with open(os.path.join(FUCHSIA_ROOT, package), 'r') as package_file:
+        data = json.load(package_file)
+    return data['imports'] if 'imports' in data else []
+
+
+def get_package_id(package):
+    return package.replace('/', '_')
+
+
+def get_package_nick(package):
+    return package.split('/', 2)[2]
 
 
 def main():
-    packages_directory = os.path.join(paths.FUCHSIA_ROOT, 'packages', 'gn')
-    print('digraph fuchsia {')
-    for directory, _, files in os.walk(packages_directory):
-        modules = [file for file in files
-                   if file.find('.') == -1 and file != 'default']
-        sanitize = lambda name: name.replace('-', '_')
-        for module in modules:
-            with open(os.path.join(directory, module)) as content:
-                data = json.load(content)
-                if 'imports' not in data:
-                    continue
-                id = sanitize(module)
-                print('%s [label="%s"];' % (id, module))
-                for dep in data['imports']:
-                    print('%s -> %s;' % (id, sanitize(dep)))
-        break
-    print('}')
+    parser = argparse.ArgumentParser(
+            description=('Creates a graph of a build package hierarchy'))
+    parser.add_argument('--package',
+                        help='Path to the build package file to analyze',
+                        required=True)
+    parser.add_argument('--output',
+                        help='Path to the generated .dot file',
+                        required=True)
+    args = parser.parse_args()
+
+    # Build the dependency tree of packages.
+    packages = [args.package]
+    deps = {}
+    while packages:
+        current = packages.pop(0)
+        imports = get_package_imports(current)
+        deps[current] = imports
+        new_packages = [p for p in imports if p not in deps]
+        packages.extend(new_packages)
+
+    layers = {}
+    for package in deps:
+        parts = package.split('/', 2)
+        if len(parts) != 3 or parts[1] != 'packages':
+            raise Exception('Unexpected directory structure for %s' % package)
+        layers.setdefault(parts[0], []).append(package)
+
+    with open(args.output, 'w') as out:
+        out.write('digraph fuchsia {\n')
+        for index, pair in enumerate(layers.iteritems()):
+            layer, packages = pair
+            out.write('subgraph cluster_%s {\n' % index)
+            out.write('label="%s";\n' % layer)
+            for package in packages:
+                out.write('%s [label="%s"];\n' % (get_package_id(package),
+                                                  get_package_nick(package)))
+            for package in packages:
+                dep_ids = [get_package_id(d) for d in deps[package]]
+                out.write('%s -> { %s }\n' % (get_package_id(package),
+                                              ' '.join(dep_ids)))
+            out.write('}\n')
+        out.write('}\n')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
