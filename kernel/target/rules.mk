c38497fd3698875d458395044e79bf606b182df7
Mike Voydanoff <voydanoff@google.com>
2018-03-10 14:39:34 -0800

    [build] Generate global list of all boards when doing "make packages"
    Change-Id: I0d45223f0913eb03d1bdba5ff80bf66bb2e9fc11

--- kernel/target/rules.mk
+++ kernel/target/rules.mk
@@ -14,3 +14,14 @@ MODULE_SRCS += \
 
 include make/module.mk
 
+# copy our all-boards.list for the fuchsia build
+# this file should contain only boards that we want to support outside of zircon
+BOARD_LIST_SRC := $(LOCAL_DIR)/all-boards.list
+BOARD_LIST_DEST := $(BUILDDIR)/export/all-boards.list
+
+$(BOARD_LIST_DEST): $(BOARD_LIST_SRC)
+	$(call BUILDECHO,copying $@)
+	@$(MKDIR)
+	$(NOECHO)cp $< $@
+
+packages: $(BOARD_LIST_DEST)
