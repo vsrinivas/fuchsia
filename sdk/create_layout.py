99515d485b02268b85a7963aefe3895ea3a5b2c2
P.Y. Laligand <pylaligand@google.com>
2018-03-09 23:24:02 -0800

    [sdk] Use Python API for SDK constructs.
    TO-785
    
    Change-Id: I733d3902d687ee0cfb9c2b7c17daa07336ee6dc5

--- sdk/create_layout.py
+++ sdk/create_layout.py
@@ -10,6 +10,14 @@ import os
 import shutil
 import sys
 
+FUCHSIA_ROOT = os.path.dirname(  # $root
+    os.path.dirname(             # scripts
+    os.path.dirname(             # sdk
+    os.path.abspath(__file__))))
+
+sys.path += [os.path.join(FUCHSIA_ROOT, 'build', 'sdk')]
+from sdk_common import Atom
+
 
 def make_dir(path, is_dir=False):
     '''Creates the directory at the given path.'''
@@ -25,8 +33,8 @@ def make_dir(path, is_dir=False):
 
 def install_cpp_atom(atom, metadata, output):
     '''Installs an atom from the "c-pp" domain.'''
-    name = atom['id']['name']
-    type = atom['tags']['type']
+    name = atom.id.name
+    type = atom.tags['type']
     if type == 'compiled_shared':
         install_cpp_prebuilt_atom(atom, metadata, output)
     elif type == 'sources':
@@ -37,22 +45,22 @@ def install_cpp_atom(atom, metadata, output):
 
 def install_cpp_prebuilt_atom(atom, metadata, output):
     '''Installs a prebuilt atom from the "c-pp" domain.'''
-    name = atom['id']['name']
-    if atom['tags']['arch'] != 'target':
+    name = atom.id.name
+    if atom.tags['arch'] != 'target':
         print('Only libraries compiled for a target are supported, '
               'skipping %s.' % name)
-    for file in atom['files']:
-        destination = file['destination']
+    for file in atom.files:
+        destination = file.destination
         extension = os.path.splitext(destination)[1][1:]
         if extension == 'so':
             dest = os.path.join(output, 'arch', metadata['target-arch'],
                                 destination)
             make_dir(dest)
-            shutil.copyfile(file['source'], dest)
-        elif extension == 'h':
+            shutil.copyfile(file.source, dest)
+        elif extension == 'h' or extension == 'modulemap':
             dest = os.path.join(output, 'pkg', name, destination)
             make_dir(dest)
-            shutil.copyfile(file['source'], dest)
+            shutil.copyfile(file.source, dest)
         else:
             raise Exception('Error: unknow file extension "%s" for %s.' %
                             (extension, name))
@@ -60,25 +68,26 @@ def install_cpp_prebuilt_atom(atom, metadata, output):
 
 def install_cpp_source_atom(atom, metadata, output):
     '''Installs a source atom from the "c-pp" domain.'''
-    name = atom['id']['name']
-    for file in atom['files']:
-        dest = os.path.join(output, 'pkg', name, file['destination'])
+    name = atom.id.name
+    for file in atom.files:
+        dest = os.path.join(output, 'pkg', name, file.destination)
         make_dir(dest)
-        shutil.copyfile(file['source'], dest)
+        shutil.copyfile(file.source, dest)
 
 
 def install_exe_atom(atom, metadata, output):
     '''Installs an atom from the "exe" domain.'''
-    name = atom['id']['name']
-    if atom['tags']['arch'] != 'host':
+    name = atom.id.name
+    if atom.tags['arch'] != 'host':
         print('Only host executables are supported, skipping %s.' % name)
         return
-    files = atom['files']
+    files = atom.files
     if len(files) != 1:
         raise Exception('Error: executable with multiple files: %s' % name)
-    destination = os.path.join(output, 'tools', files[0]['destination'])
+    file = files[0]
+    destination = os.path.join(output, 'tools', file.destination)
     make_dir(destination)
-    shutil.copyfile(files[0]['source'], destination)
+    shutil.copyfile(file.source, destination)
 
 
 def main():
@@ -98,7 +107,7 @@ def main():
     # Read the contents of the manifest.
     with open(args.manifest, 'r') as manifest_file:
         manifest = json.load(manifest_file)
-    atoms = manifest['atoms']
+    atoms = [Atom(a) for a in manifest['atoms']]
     metadata = manifest['meta']
 
     # Verify that the manifest only contains supporterd domains.
@@ -107,7 +116,7 @@ def main():
         'exe',
     }
     unknown_domains = set(filter(lambda d: d not in supported_domains,
-                                 [a['id']['domain'] for a in atoms]))
+                                 [a.id.domain for a in atoms]))
     if unknown_domains:
         print('The following domains are not currently supported: %s' %
               ', '.join(unknown_domains))
@@ -115,7 +124,7 @@ def main():
 
     # Install the various atoms in the output directory.
     for atom in atoms:
-        domain = atom['id']['domain']
+        domain = atom.id.domain
         if domain == 'c-pp':
             install_cpp_atom(atom, metadata, args.output)
         elif domain == 'exe':
