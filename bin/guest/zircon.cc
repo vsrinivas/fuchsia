69f9271c2530ec81f5e09cb322d087e6a191c800
Alex Legg <alexlegg@google.com>
2018-03-16 13:19:38 +1100

    [guest] Set MDI cpu_count and memory map length.
    This will be used on arm64 for Zircon guests. (ACPI tables are used on
    x86 and DTS is used for Linux guests on arm64.)
    
    Change-Id: Ia094a4db35c81e10ab872505fc418b42ba36271b

--- bin/guest/zircon.cc
+++ bin/guest/zircon.cc
@@ -11,11 +11,22 @@
 #include <unistd.h>
 
 #include <fbl/unique_fd.h>
+#include <mdi/mdi.h>
 #include <zircon/assert.h>
 #include <zircon/boot/bootdata.h>
+#include <zircon/mdi.h>
 
 #include "garnet/bin/guest/kernel.h"
 #include "garnet/lib/machina/guest.h"
+
+// clang-format off
+// These MDI ids are taken from zircon/system/public/zircon/mdi/zircon.mdi.
+static constexpr uint32_t kMdiCpuMap      = 0x08000002;
+static constexpr uint32_t kMdiLength      = 0x0180000d;
+static constexpr uint32_t kMdiCpuClusters = 0x08000064;
+static constexpr uint32_t kMdiCpuCount    = 0x00000065;
+static constexpr uint32_t kMdiMemMap      = 0x080000c8;
+// clang-format on
 
 #if __aarch64__
 static constexpr uintptr_t kKernelOffset = 0;
@@ -64,9 +75,80 @@ static zx_status_t load_cmdline(const std::string& cmdline,
   return ZX_OK;
 }
 
-static zx_status_t load_bootfs(const int fd,
-                               const machina::PhysMem& phys_mem,
-                               const uintptr_t bootdata_off) {
+static zx_status_t find_mdi(const machina::PhysMem& phys_mem,
+                            const uintptr_t data_off, uint32_t data_len,
+                            mdi_node_ref_t* mdi_root) {
+  uintptr_t off = data_off;
+  while (off < data_off + data_len) {
+    auto hdr = phys_mem.as<bootdata_t>(off);
+    uint32_t entry_size = sizeof(*hdr) + BOOTDATA_ALIGN(hdr->length);
+    if (hdr->type == BOOTDATA_MDI) {
+      return mdi_init(hdr, entry_size, mdi_root);
+    }
+    if (off > UINT64_MAX - entry_size) {
+      return ZX_ERR_NOT_FOUND;
+    }
+    off += entry_size;
+  }
+  return ZX_ERR_NOT_FOUND;
+}
+
+static zx_status_t mdi_find_grandchild(mdi_node_ref_t* node,
+                                       uint32_t grandparent_id,
+                                       uint32_t target_id, uint32_t target_type,
+                                       mdi_node_ref_t* result) {
+  mdi_node_ref_t parent;
+  zx_status_t status = mdi_find_node(node, grandparent_id, &parent);
+  if (status != ZX_OK) {
+    return status;
+  }
+  if (mdi_child_count(&parent) != 1) {
+    return ZX_ERR_NOT_SUPPORTED;
+  }
+  mdi_node_ref_t child;
+  status = mdi_first_child(&parent, &child);
+  if (status != ZX_OK) {
+    return status;
+  }
+  status = mdi_find_node(&child, target_id, result);
+  if (status != ZX_OK || mdi_node_type(result) != target_type) {
+    return status;
+  }
+  return ZX_OK;
+}
+
+static zx_status_t set_mem_map_size(mdi_node_ref_t* mdi_root, uint64_t size) {
+  mdi_node_ref_t length;
+  zx_status_t status = mdi_find_grandchild(mdi_root, kMdiMemMap, kMdiLength,
+                                           MDI_UINT64, &length);
+  if (status != ZX_OK) {
+    FXL_LOG(ERROR) << "Failed to find MDI memory map length node";
+  }
+  mdi_node_t* mut_node = const_cast<mdi_node_t*>(length.node);
+  mut_node->value.u64 = size;
+  return ZX_OK;
+}
+
+static zx_status_t set_cpu_count(mdi_node_ref_t* mdi_root, uint8_t num_cpus) {
+  mdi_node_ref_t cpu_map;
+  zx_status_t status = mdi_find_node(mdi_root, kMdiCpuMap, &cpu_map);
+  if (status != ZX_OK) {
+    FXL_LOG(ERROR) << "Failed to find MDI cpu map node";
+    return status;
+  }
+  mdi_node_ref_t cpu_count;
+  status = mdi_find_grandchild(&cpu_map, kMdiCpuClusters, kMdiCpuCount,
+                               MDI_UINT8, &cpu_count);
+  if (status != ZX_OK) {
+    FXL_LOG(ERROR) << "Failed to find MDI cpu count node";
+  }
+  mdi_node_t* mut_node = const_cast<mdi_node_t*>(cpu_count.node);
+  mut_node->value.u8 = num_cpus;
+  return ZX_OK;
+}
+
+static zx_status_t load_bootfs(const int fd, const machina::PhysMem& phys_mem,
+                               const uintptr_t bootdata_off, uint8_t num_cpus) {
   bootdata_t ramdisk_hdr;
   ssize_t ret = read(fd, &ramdisk_hdr, sizeof(bootdata_t));
   if (ret != sizeof(bootdata_t)) {
@@ -86,11 +168,27 @@ static zx_status_t load_bootfs(const int fd,
   uintptr_t data_off =
       bootdata_off + sizeof(bootdata_t) + BOOTDATA_ALIGN(container_hdr->length);
 
-  ret =
-      read(fd, phys_mem.as<void>(data_off, ramdisk_hdr.length), ramdisk_hdr.length);
+  ret = read(fd, phys_mem.as<void>(data_off, ramdisk_hdr.length),
+             ramdisk_hdr.length);
   if (ret < 0 || (size_t)ret != ramdisk_hdr.length) {
     FXL_LOG(ERROR) << "Failed to read BOOTFS image data";
     return ZX_ERR_IO;
+  }
+
+  mdi_node_ref_t mdi_root;
+  zx_status_t status =
+      find_mdi(phys_mem, data_off, ramdisk_hdr.length, &mdi_root);
+  if (status != ZX_OK) {
+    FXL_LOG(ERROR) << "Failed to find MDI header";
+    return status;
+  }
+  status = set_mem_map_size(&mdi_root, static_cast<uint64_t>(phys_mem.size()));
+  if (status != ZX_OK) {
+    return status;
+  }
+  status = set_cpu_count(&mdi_root, num_cpus);
+  if (status != ZX_OK) {
+    return status;
   }
 
   container_hdr->length += BOOTDATA_ALIGN(ramdisk_hdr.length) +
@@ -159,8 +257,7 @@ static zx_status_t read_bootdata(const machina::PhysMem& phys_mem,
 }
 
 zx_status_t setup_zircon(const GuestConfig cfg,
-                         const machina::PhysMem& phys_mem,
-                         uintptr_t* guest_ip,
+                         const machina::PhysMem& phys_mem, uintptr_t* guest_ip,
                          uintptr_t* boot_ptr) {
   // Read the kernel image.
   zx_status_t status = load_kernel(cfg.kernel_path(), phys_mem, kKernelOffset);
@@ -181,8 +278,9 @@ zx_status_t setup_zircon(const GuestConfig cfg,
 
   // Load the kernel command line.
   status = load_cmdline(cfg.cmdline(), phys_mem, kRamdiskOffset);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   // If we have been provided a BOOTFS image, load it.
   if (!cfg.ramdisk_path().empty()) {
@@ -192,9 +290,11 @@ zx_status_t setup_zircon(const GuestConfig cfg,
       return ZX_ERR_IO;
     }
 
-    status = load_bootfs(boot_fd.get(), phys_mem, kRamdiskOffset);
-    if (status != ZX_OK)
+    status =
+        load_bootfs(boot_fd.get(), phys_mem, kRamdiskOffset, cfg.num_cpus());
+    if (status != ZX_OK) {
       return status;
+    }
   }
 
   *boot_ptr = kRamdiskOffset;
