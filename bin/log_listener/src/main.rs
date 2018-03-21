d96b163768ac58796245b2e6c6fb6c1ce536554d
Ankur Mittal <anmittal@google.com>
2018-03-21 16:40:09 -0700

    [log_listener] Implement.
    Add code to connect to Log service and listen for logs
    
    Change-Id: I667a7963ea48928a79f966c0831dce89ab1bc029

--- bin/log_listener/src/main.rs
+++ bin/log_listener/src/main.rs
@@ -3,12 +3,27 @@
 // found in the LICENSE file.
 
 #![deny(warnings)]
+#![allow(stable_features)]
+#![feature(conservative_impl_trait)]
 
+extern crate failure;
+extern crate fidl;
+extern crate fuchsia_app as app;
+extern crate fuchsia_async as async;
+extern crate fuchsia_zircon as zx;
+extern crate futures;
+
+use app::client::connect_to_service;
+use failure::{Error, ResultExt};
+use futures::future::ok as fok;
+use std::collections::HashMap;
 use std::env;
+use std::io::{stdout, Write};
 
 // Include the generated FIDL bindings for the `Logger` service.
 extern crate fidl_logger;
-use fidl_logger::LogFilterOptions;
+use fidl_logger::{LogFilterOptions, LogListener, LogListenerImpl, LogListenerMarker, LogMarker,
+                  LogMessage};
 
 const FX_LOG_MAX_TAGS: usize = 5;
 const FX_LOG_MAX_TAG_LEN: usize = 63;
@@ -157,24 +172,212 @@ fn parse_flags(args: &[String]) -> Result<LogFilterOptions, String> {
     return Ok(options);
 }
 
+struct Listener<W: Write + Send> {
+    // stores pid, dropped_logs
+    dropped_logs: HashMap<u64, u32>,
+    writer: W,
+}
+
+impl<W> Listener<W>
+where
+    W: Write + Send,
+{
+    fn log(&mut self, message: &LogMessage) {
+        let tags = message.tags.join(", ");
+        writeln!(
+            self.writer,
+            "[{:05}.{:06}][{}][{}][{}] {}: {}",
+            message.time / 1000000000,
+            (message.time / 1000) % 1000000,
+            message.pid,
+            message.tid,
+            tags,
+            get_log_level(message.severity),
+            message.msg
+        ).expect("should not fail");
+        if message.dropped_logs > 0
+            && self.dropped_logs
+                .get(&message.pid)
+                .map(|d| d < &message.dropped_logs)
+                .unwrap_or(true)
+        {
+            writeln!(
+                self.writer,
+                "[{:05}.{:06}][{}][{}][{}] WARNING: Dropped logs count: {}",
+                message.time / 1000000000,
+                (message.time / 1000) % 1000000,
+                message.pid,
+                message.tid,
+                tags,
+                message.dropped_logs
+            ).expect("should not fail");
+            self.dropped_logs.insert(message.pid, message.dropped_logs);
+        }
+    }
+}
+
+fn get_log_level(level: i32) -> String {
+    match level {
+        0 => "INFO".to_string(),
+        1 => "WARNING".to_string(),
+        2 => "ERROR".to_string(),
+        3 => "FATAL".to_string(),
+        l => {
+            if l > 3 {
+                "INVALID".to_string()
+            } else {
+                format!("VLOG({})", -l)
+            }
+        }
+    }
+}
+
+fn log_listener<W>(listener: Listener<W>) -> impl LogListener
+where
+    W: Send + Write,
+{
+    LogListenerImpl {
+        state: listener,
+        log: |listener, message, _| {
+            listener.log(&message);
+            fok(())
+        },
+    }
+}
+
+fn run_log_listener(mut options: Option<Box<LogFilterOptions>>) -> Result<(), Error> {
+    let mut executor = async::Executor::new().context("Error creating executor")?;
+    let logger = connect_to_service::<LogMarker>()?;
+    let (log_listener_local, log_listener_remote) = zx::Channel::create()?;
+    let log_listener_local = async::Channel::from_channel(log_listener_local)?;
+    let mut listener_ptr =
+        fidl::endpoints2::ClientEnd::<LogListenerMarker>::new(log_listener_remote);
+    logger
+        .listen(&mut listener_ptr, &mut options)
+        .context("failed to register listener")?;
+
+    let l = Listener {
+        dropped_logs: HashMap::new(),
+        writer: stdout(),
+    };
+    let listener_fut = log_listener(l).serve(log_listener_local);
+    executor
+        .run_singlethreaded(listener_fut)
+        .map_err(Into::into)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 && (args[1] == "--help" || args[1] == "-h") {
         println!("{}\n", help(args[0].as_ref()));
         return;
     }
-    let _options = match parse_flags(&args[1..]) {
+    let options = match parse_flags(&args[1..]) {
         Err(e) => {
             eprintln!("{}\n{}\n", e, help(args[0].as_ref()));
             return;
         }
         Ok(o) => o,
     };
+
+    if let Err(e) = run_log_listener(Some(Box::new(options))) {
+        eprintln!("LogListener: Error: {:?}", e);
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    extern crate tempdir;
+
     use super::*;
+    use self::tempdir::TempDir;
+
+    use std::fs::File;
+    use std::io::Read;
+
+    #[test]
+    fn test_log_fn() {
+        let _executor = async::Executor::new().expect("unable to create executor");
+        let tmp_dir = TempDir::new("test").expect("should have created tempdir");
+        let file_path = tmp_dir.path().join("tmp_file");
+        let tmp_file = File::create(&file_path).expect("should have created file");
+        let mut l = Listener {
+            dropped_logs: HashMap::new(),
+            writer: tmp_file,
+        };
+
+        // test log levels
+        let mut message = LogMessage {
+            pid: 123,
+            tid: 321,
+            severity: 0,
+            time: 76352234564,
+            msg: "hello".to_string(),
+            dropped_logs: 0,
+            tags: vec![],
+        };
+        l.log(&message);
+
+        for level in vec![1, 2, 3, 4, 11, -1, -3] {
+            message.severity = level;
+            l.log(&message);
+        }
+        let mut expected = "".to_string();
+        for level in vec![
+            "INFO", "WARNING", "ERROR", "FATAL", "INVALID", "INVALID", "VLOG(1)", "VLOG(3)"
+        ] {
+            expected.push_str(&format!("[00076.352234][123][321][] {}: hello\n", level));
+        }
+
+        //test tags
+        message.severity = 0;
+        message.tags = vec!["tag1".to_string()];
+        l.log(&message);
+        expected.push_str("[00076.352234][123][321][tag1] INFO: hello\n");
+
+        message.tags.push("tag2".to_string());
+        l.log(&message);
+        expected.push_str("[00076.352234][123][321][tag1, tag2] INFO: hello\n");
+
+        //test time
+        message.time = 636253000631621;
+        l.log(&message);
+        let s = "[636253.000631][123][321][tag1, tag2] INFO: hello\n";
+        expected.push_str(s);
+
+        //test dropped logs
+        message.dropped_logs = 1;
+        l.log(&message);
+        expected.push_str(s);
+        expected.push_str("[636253.000631][123][321][tag1, tag2] WARNING: Dropped logs count: 1\n");
+        l.log(&message);
+        // will not print log count again
+        expected.push_str(s);
+
+        //change pid and test
+        message.pid = 1234;
+        l.log(&message);
+        expected.push_str("[636253.000631][1234][321][tag1, tag2] INFO: hello\n");
+        expected
+            .push_str("[636253.000631][1234][321][tag1, tag2] WARNING: Dropped logs count: 1\n");
+
+        // switch back pid and test
+        message.pid = 123;
+        l.log(&message);
+        expected.push_str(s);
+        message.dropped_logs = 2;
+        l.log(&message);
+        expected.push_str(s);
+        expected.push_str("[636253.000631][123][321][tag1, tag2] WARNING: Dropped logs count: 2\n");
+
+        let mut tmp_file = File::open(&file_path).expect("should have opened the file");
+        let mut content = String::new();
+        tmp_file
+            .read_to_string(&mut content)
+            .expect("something went wrong reading the file");
+
+        assert_eq!(content, expected);
+    }
 
     mod parse_flags {
         use super::*;
