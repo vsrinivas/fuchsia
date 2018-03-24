e1fc4460e88061929d3fd027a827568e33e3ac91
Abdulla Kamar <abdulla@google.com>
2018-03-24 20:46:11 +1100

    [virtio] Add virtio-vsock definitions
    PD-115 #comment
    
    Change-Id: I323a0ccc833ddf1a865e82a2c282eac2e543b050

---
+++ system/ulib/virtio/include/virtio/vsock.h
@@ -0,0 +1,55 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <stdint.h>
+#include <zircon/compiler.h>
+
+// clang-format off
+
+// For virtio_vsock_hdr_t.
+#define VIRTIO_VSOCK_TYPE_STREAM            1
+
+#define VIRTIO_VSOCK_OP_INVALID             0
+#define VIRTIO_VSOCK_OP_REQUEST             1
+#define VIRTIO_VSOCK_OP_RESPONSE            2
+#define VIRTIO_VSOCK_OP_RST                 3
+#define VIRTIO_VSOCK_OP_SHUTDOWN            4
+#define VIRTIO_VSOCK_OP_RW                  5
+#define VIRTIO_VSOCK_OP_CREDIT_UPDATE       6
+#define VIRTIO_VSOCK_OP_CREDIT_REQUEST      7
+
+#define VIRTIO_VSOCK_FLAG_SHUTDOWN_RECV     1
+#define VIRTIO_VSOCK_FLAG_SHUTDOWN_SEND     2
+
+// For virtio_vsock_event_t.
+#define VIRTIO_VSOCK_EVENT_TRANSPORT_RESET  0
+
+// clang-format on
+
+__BEGIN_CDECLS;
+
+typedef struct virtio_vsock_config {
+  uint64_t guest_cid;
+} __PACKED virtio_vsock_config_t;
+
+typedef struct virtio_vsock_hdr {
+  uint64_t src_cid;
+  uint64_t dst_cid;
+  uint32_t src_port;
+  uint32_t dst_port;
+  uint32_t len;
+  uint16_t type;
+  uint16_t op;
+  uint32_t flags;
+  uint32_t buf_alloc;
+  uint32_t fwd_cnt;
+} __PACKED virtio_vsock_hdr_t;
+
+typedef struct virtio_vsock_event {
+  uint32_t id;
+} __PACKED virtio_vsock_event_t;
+
+__END_CDECLS;
