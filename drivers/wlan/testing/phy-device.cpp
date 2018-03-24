NERD-Repo/garnet

[wlan][testing] Implement wlan phy FIDL2 dispatcher
Adds an ioctl to connect to the FIDL dispatcher on a wlanphy device.

NET-547

Change-Id: I90c90492cb1499856a50cea412a4852066550364

drivers/wlan/testing/phy-device.cpp
@@ -4,6 +4,8 @@
 
 #include "phy-device.h"
 
+#include "driver.h"
+#include "iface-device.h"
 #include "garnet/lib/wlan/fidl/iface.fidl.h"
 #include "garnet/lib/wlan/fidl/phy.fidl.h"
 #include "iface-device.h"
@@ -49,6 +51,8 @@ PhyDevice::PhyDevice(zx_device_t* device) : parent_(device) {}
 zx_status_t PhyDevice::Bind() {
     zxlogf(INFO, "wlan::testing::phy::PhyDevice::Bind()\n");
 
+    dispatcher_ = std::make_unique<wlan::dispatcher::Dispatcher<wlan::Phy>>(wlanphy_async_t());
+
     device_add_args_t args = {};
     args.version = DEVICE_ADD_ARGS_VERSION;
     args.name = "wlanphy-test";
@@ -58,14 +62,18 @@ zx_status_t PhyDevice::Bind() {
     args.proto_ops = &wlanphy_test_ops;
 
     zx_status_t status = device_add(parent_, &args, &zxdev_);
-    if (status != ZX_OK) { printf("wlanphy-test: could not add test device: %d\n", status); }
+    if (status != ZX_OK) {
+        printf("wlanphy-test: could not add test device: %d\n", status);
+        return status;
+    }
+
     return status;
 }
 
 void PhyDevice::Unbind() {
     zxlogf(INFO, "wlan::testing::PhyDevice::Unbind()\n");
     std::lock_guard<std::mutex> guard(lock_);
-    dead_ = true;
+    dispatcher_.reset();
     device_remove(zxdev_);
 }
 
@@ -77,92 +85,195 @@ void PhyDevice::Release() {
 zx_status_t PhyDevice::Ioctl(uint32_t op, const void* in_buf, size_t in_len, void* out_buf,
                              size_t out_len, size_t* out_actual) {
     zxlogf(INFO, "wlan::testing::phy::PhyDevice::Ioctl()\n");
-    zx_status_t status = ZX_ERR_NOT_SUPPORTED;
     switch (op) {
+    case IOCTL_WLANPHY_CONNECT:
+        zxlogf(INFO, "wlanphy ioctl: connect\n");
+        return Connect(in_buf, in_len);
     case IOCTL_WLANPHY_QUERY:
         zxlogf(INFO, "wlanphy ioctl: query len=%zu\n", out_len);
-        status = Query(static_cast<uint8_t*>(out_buf), out_len, out_actual);
-        break;
+        return Query(static_cast<uint8_t*>(out_buf), out_len, out_actual);
     case IOCTL_WLANPHY_CREATE_IFACE:
         zxlogf(INFO, "wlanphy ioctl: create if inlen=%zu outlen=%zu\n", in_len, out_len);
-        status = CreateIface(in_buf, in_len, out_buf, out_len, out_actual);
-        break;
+        return CreateIface(in_buf, in_len, out_buf, out_len, out_actual);
     case IOCTL_WLANPHY_DESTROY_IFACE:
         zxlogf(INFO, "wlanphy ioctl: destroy if inlen=%zu\n", in_len);
-        status = DestroyIface(in_buf, in_len);
         *out_actual = 0;
-        break;
+        return DestroyIface(in_buf, in_len);
     default:
         zxlogf(ERROR, "wlanphy ioctl: unknown (%u)\n", op);
-        break;
+        return ZX_ERR_NOT_SUPPORTED;
     }
-    return status;
+}
+
+namespace {
+wlan::WlanPhyInfo get_info() {
+    WlanPhyInfo info;
+    info.supported_phys.resize(0);
+    info.driver_features.resize(0);
+    info.mac_roles.resize(0);
+    info.caps.resize(0);
+    info.bands.resize(0);
+
+    info.supported_phys->push_back(wlan::SupportedPhy::DSSS);
+    info.supported_phys->push_back(wlan::SupportedPhy::CCK);
+    info.supported_phys->push_back(wlan::SupportedPhy::OFDM);
+    info.supported_phys->push_back(wlan::SupportedPhy::HT);
+
+    info.mac_roles->push_back(wlan::MacRole::CLIENT);
+    info.mac_roles->push_back(wlan::MacRole::AP);
+
+    info.caps->push_back(wlan::Capability::SHORT_PREAMBLE);
+    info.caps->push_back(wlan::Capability::SHORT_SLOT_TIME);
+
+    BandInfo band24;
+    band24.description = "2.4 GHz";
+    band24.ht_caps.ht_capability_info = 0x01fe;
+    auto& band24mcs = band24.ht_caps.supported_mcs_set;
+    std::fill(band24mcs.begin(), band24mcs.end(), 0);
+    band24mcs[0] = 0xff;
+    band24mcs[3] = 0x80;
+    band24mcs[12] = 0x10;
+    band24.basic_rates.reset(std::vector<uint8_t>({2, 4, 11, 22, 12, 18, 24, 36, 48, 72, 96, 108}));
+    band24.supported_channels.base_freq = 2417;
+    band24.supported_channels.channels.reset(
+            std::vector<uint8_t>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}));
+
+    info.bands->push_back(std::move(band24));
+
+    BandInfo band5;
+    band5.description = "5 GHz";
+    band5.ht_caps.ht_capability_info = 0x01fe;
+    auto& band5mcs = band5.ht_caps.supported_mcs_set;
+    std::fill(band5mcs.begin(), band5mcs.end(), 0);
+    band5mcs[0] = 0xff;
+    band5mcs[1] = 0xff;
+    band5mcs[3] = 0x80;
+    band5mcs[12] = 0x10;
+    band5.basic_rates.reset(std::vector<uint8_t>({12, 18, 24, 36, 48, 72, 96, 108}));
+    band5.supported_channels.base_freq = 5000;
+    band5.supported_channels.channels.reset(
+            std::vector<uint8_t>(
+                {36, 38,  40,  42,  44,  46,  48,  50,  52,  54,  56,  58,
+                60,  62,  64,  100, 102, 104, 106, 108, 110, 112, 114, 116,
+                118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140,
+                149, 151, 153, 155, 157, 159, 161, 165, 184, 188, 192, 196}));
+
+    info.bands->push_back(std::move(band5));
+    return info;
+}
+}  // namespace
+
+void PhyDevice::Query(QueryCallback callback) {
+    zxlogf(INFO, "wlan::testing::phy::PhyDevice::Query()\n");
+    QueryResponse resp;
+    resp.info = get_info();
+    callback(std::move(resp));
+}
+
+void PhyDevice::CreateIface(CreateIfaceRequest req,
+                            CreateIfaceCallback callback) {
+    zxlogf(INFO, "CreateRequest: role=%u\n", req.role);
+    std::lock_guard<std::mutex> guard(lock_);
+    CreateIfaceResponse resp;
+
+    // We leverage wrapping of unsigned ints to cycle back through ids to find an unused one.
+    bool found_unused = false;
+    uint16_t id = next_id_;
+    while (!found_unused) {
+        if (ifaces_.count(id) > 0) {
+            id++;
+            // If we wrap all the way around, something is very wrong.
+            if (next_id_ == id) { break; }
+        } else {
+            found_unused = true;
+        }
+    }
+    ZX_DEBUG_ASSERT(found_unused);
+    if (!found_unused) {
+        resp.status_ = ZX_ERR_NO_RESOURCES;
+        callback(std::move(resp));
+        return;
+    }
+
+    // Create the interface device and bind it.
+    auto macdev = std::make_unique<IfaceDevice>(zxdev_);
+    zx_status_t status = macdev->Bind();
+    if (status != ZX_OK) {
+        zxlogf(ERROR, "could not bind child wlanmac device: %d\n", status);
+        resp.status_ = status;
+        callback(std::move(resp));
+        return;
+    }
+
+    // Memory management follows the device lifecycle at this point. The only way an interface
+    // can be removed is through this phy device, either through a "destroy interface" ioctl or
+    // by the phy going away, so it should be safe to store the raw pointer.
+    ifaces_[id] = macdev.release();
+
+    // Since we successfully used the id, increment the next id counter.
+    next_id_ = id + 1;
+
+    resp.status_ = ZX_OK;
+    callback(std::move(resp));
+}
+
+void PhyDevice::DestroyIface(DestroyIfaceRequest req,
+                             DestroyIfaceCallback callback) {
+    zxlogf(INFO, "DestroyRequest: id=%u\n", req.id);
+
+    DestroyIfaceResponse resp;
+
+    std::lock_guard<std::mutex> guard(lock_);
+    auto intf = ifaces_.find(req.id);
+    if (intf == ifaces_.end()) {
+        resp.status_ = ZX_ERR_NOT_FOUND;
+        callback(std::move(resp));
+        return;
+    }
+
+    device_remove(intf->second->zxdev());
+    // Remove the device from our map. We do NOT free the memory, since the devhost owns it and will
+    // call release when it's safe to free the memory.
+    ifaces_.erase(req.id);
+
+    resp.status_ = ZX_OK;
+    callback(std::move(resp));
+}
+
+zx_status_t PhyDevice::Connect(const void* buf, size_t len) {
+    if (buf == nullptr || len < sizeof(zx_handle_t)) {
+        return ZX_ERR_BUFFER_TOO_SMALL;
+    }
+
+    zx_handle_t hnd = *reinterpret_cast<const zx_handle_t*>(buf);
+    zx::channel chan(hnd);
+
+    zx_status_t status = dispatcher_->AddBinding(std::move(chan), this);
+    if (status != ZX_OK) { return status; }
+    return ZX_OK;
 }
 
 zx_status_t PhyDevice::Query(uint8_t* buf, size_t len, size_t* actual) {
     zxlogf(INFO, "wlan::testing::PhyDevice::Query()\n");
-    std::lock_guard<std::mutex> guard(lock_);
-    if (dead_) { return ZX_ERR_PEER_CLOSED; }
+    return ZX_ERR_NOT_SUPPORTED;
+    //std::lock_guard<std::mutex> guard(lock_);
+    //if (dead_) {
+    //    return ZX_ERR_PEER_CLOSED;
+    //}
 
-    auto info = wlan::phy::WlanPhyInfo::New();
-    info->supported_phys = f1dl::Array<wlan::phy::SupportedPhy>::New(0);
-    info->driver_features = f1dl::Array<wlan::phy::DriverFeature>::New(0);
-    info->mac_roles = f1dl::Array<wlan::phy::MacRole>::New(0);
-    info->caps = f1dl::Array<wlan::phy::Capability>::New(0);
-    info->bands = f1dl::Array<wlan::phy::BandInfoPtr>::New(0);
-
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::DSSS);
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::CCK);
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::OFDM);
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::HT);
-
-    info->mac_roles.push_back(wlan::phy::MacRole::CLIENT);
-    info->mac_roles.push_back(wlan::phy::MacRole::AP);
-
-    info->caps.push_back(wlan::phy::Capability::SHORT_PREAMBLE);
-    info->caps.push_back(wlan::phy::Capability::SHORT_SLOT_TIME);
-
-    auto band24 = wlan::phy::BandInfo::New();
-    band24->ht_caps = wlan::phy::HtCapabilities::New();
-    band24->supported_channels = wlan::phy::ChannelList::New();
-    band24->description = "2.4 GHz";
-    band24->ht_caps->ht_capability_info = 0x01fe;
-    band24->ht_caps->supported_mcs_set.reset(
-        std::vector<uint8_t>{0xff, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                             0x10, 0x00, 0x00, 0x00});
-    band24->basic_rates.reset(std::vector<uint8_t>{2, 4, 11, 22, 12, 18, 24, 36, 48, 72, 96, 108});
-    band24->supported_channels->base_freq = 2417;
-    band24->supported_channels->channels.reset(
-        std::vector<uint8_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14});
-
-    info->bands.push_back(std::move(band24));
-
-    auto band5 = wlan::phy::BandInfo::New();
-    band5->ht_caps = wlan::phy::HtCapabilities::New();
-    band5->supported_channels = wlan::phy::ChannelList::New();
-    band5->description = "5 GHz";
-    band5->ht_caps->ht_capability_info = 0x01fe;
-    band5->ht_caps->supported_mcs_set.reset(std::vector<uint8_t>{0xff, 0xff, 0x00, 0x80, 0x00, 0x00,
-                                                                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                                                                 0x10, 0x00, 0x00, 0x00});
-    band5->basic_rates.reset(std::vector<uint8_t>{12, 18, 24, 36, 48, 72, 96, 108});
-    band5->supported_channels->base_freq = 5000;
-    band5->supported_channels->channels.reset(std::vector<uint8_t>{
-        36,  38,  40,  42,  44,  46,  48,  50,  52,  54,  56,  58,  60,  62,  64,  100,
-        102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132,
-        134, 136, 138, 140, 149, 151, 153, 155, 157, 159, 161, 165, 184, 188, 192, 196});
-
-    info->bands.push_back(std::move(band5));
-
-    if (len < info->GetSerializedSize()) { return ZX_ERR_BUFFER_TOO_SMALL; }
-    if (!info->Serialize(buf, info->GetSerializedSize(), actual)) { return ZX_ERR_IO; }
-    return ZX_OK;
+    //auto info = get_info();
+    //if (len < info->GetSerializedSize()) { return ZX_ERR_BUFFER_TOO_SMALL; }
+    //if (!info->Serialize(buf, info->GetSerializedSize(), actual)) { return ZX_ERR_IO; }
+    //return ZX_OK;
 }
 
 zx_status_t PhyDevice::CreateIface(const void* in_buf, size_t in_len, void* out_buf, size_t out_len,
                                    size_t* out_actual) {
     auto req = wlan::phy::CreateIfaceRequest::New();
-    if (!req->Deserialize(const_cast<void*>(in_buf), in_len)) { return ZX_ERR_IO; }
+    if (!req->Deserialize(const_cast<void*>(in_buf), in_len)) {
+        return ZX_ERR_IO;
+    }
+
     zxlogf(INFO, "CreateRequest: role=%u\n", req->role);
     std::lock_guard<std::mutex> guard(lock_);
     if (dead_) { return ZX_ERR_PEER_CLOSED; }
@@ -189,6 +300,7 @@ zx_status_t PhyDevice::CreateIface(const void* in_buf, size_t in_len, void* out_
     if (out_len < info->GetSerializedSize()) { return ZX_ERR_BUFFER_TOO_SMALL; }
     if (!info->Serialize(out_buf, info->GetSerializedSize(), out_actual)) { return ZX_ERR_IO; }
 
+
     // Create the interface device and bind it.
     auto macdev = std::make_unique<IfaceDevice>(zxdev_);
     zx_status_t status = macdev->Bind();
@@ -211,6 +323,7 @@ zx_status_t PhyDevice::CreateIface(const void* in_buf, size_t in_len, void* out_
 
     return ZX_OK;
 }
+
 
 zx_status_t PhyDevice::DestroyIface(const void* in_buf, size_t in_len) {
     auto req = wlan::phy::DestroyIfaceRequest::New();

vsrinivas/fuchsia

Change-Id: Ia4e3ea4bf2660a1109b222d5f431b5e0cbf2c9c5

drivers/wlan/testing/phy-device.cpp
@@ -4,8 +4,7 @@
 
 #include "phy-device.h"
 
-#include "garnet/lib/wlan/fidl/iface.fidl.h"
-#include "garnet/lib/wlan/fidl/phy.fidl.h"
+#include "driver.h"
 #include "iface-device.h"
 
 #include <ddk/debug.h>
@@ -49,6 +48,8 @@ PhyDevice::PhyDevice(zx_device_t* device) : parent_(device) {}
 zx_status_t PhyDevice::Bind() {
     zxlogf(INFO, "wlan::testing::phy::PhyDevice::Bind()\n");
 
+    dispatcher_ = std::make_unique<wlan::async::Dispatcher<wlan_device::Phy>>(wlanphy_async_t());
+
     device_add_args_t args = {};
     args.version = DEVICE_ADD_ARGS_VERSION;
     args.name = "wlanphy-test";
@@ -59,13 +60,14 @@ zx_status_t PhyDevice::Bind() {
 
     zx_status_t status = device_add(parent_, &args, &zxdev_);
     if (status != ZX_OK) { printf("wlanphy-test: could not add test device: %d\n", status); }
+
     return status;
 }
 
 void PhyDevice::Unbind() {
     zxlogf(INFO, "wlan::testing::PhyDevice::Unbind()\n");
     std::lock_guard<std::mutex> guard(lock_);
-    dead_ = true;
+    dispatcher_.reset();
     device_remove(zxdev_);
 }
 
@@ -77,95 +79,86 @@ void PhyDevice::Release() {
 zx_status_t PhyDevice::Ioctl(uint32_t op, const void* in_buf, size_t in_len, void* out_buf,
                              size_t out_len, size_t* out_actual) {
     zxlogf(INFO, "wlan::testing::phy::PhyDevice::Ioctl()\n");
-    zx_status_t status = ZX_ERR_NOT_SUPPORTED;
     switch (op) {
-    case IOCTL_WLANPHY_QUERY:
-        zxlogf(INFO, "wlanphy ioctl: query len=%zu\n", out_len);
-        status = Query(static_cast<uint8_t*>(out_buf), out_len, out_actual);
-        break;
-    case IOCTL_WLANPHY_CREATE_IFACE:
-        zxlogf(INFO, "wlanphy ioctl: create if inlen=%zu outlen=%zu\n", in_len, out_len);
-        status = CreateIface(in_buf, in_len, out_buf, out_len, out_actual);
-        break;
-    case IOCTL_WLANPHY_DESTROY_IFACE:
-        zxlogf(INFO, "wlanphy ioctl: destroy if inlen=%zu\n", in_len);
-        status = DestroyIface(in_buf, in_len);
-        *out_actual = 0;
-        break;
+    case IOCTL_WLANPHY_CONNECT:
+        zxlogf(INFO, "wlanphy ioctl: connect\n");
+        return Connect(in_buf, in_len);
     default:
         zxlogf(ERROR, "wlanphy ioctl: unknown (%u)\n", op);
-        break;
+        return ZX_ERR_NOT_SUPPORTED;
     }
-    return status;
 }
 
-zx_status_t PhyDevice::Query(uint8_t* buf, size_t len, size_t* actual) {
-    zxlogf(INFO, "wlan::testing::PhyDevice::Query()\n");
-    std::lock_guard<std::mutex> guard(lock_);
-    if (dead_) { return ZX_ERR_PEER_CLOSED; }
+namespace {
+wlan_device::PhyInfo get_info() {
+    wlan_device::PhyInfo info;
+    info.supported_phys.resize(0);
+    info.driver_features.resize(0);
+    info.mac_roles.resize(0);
+    info.caps.resize(0);
+    info.bands.resize(0);
 
-    auto info = wlan::phy::WlanPhyInfo::New();
-    info->supported_phys = f1dl::VectorPtr<wlan::phy::SupportedPhy>::New(0);
-    info->driver_features = f1dl::VectorPtr<wlan::phy::DriverFeature>::New(0);
-    info->mac_roles = f1dl::VectorPtr<wlan::phy::MacRole>::New(0);
-    info->caps = f1dl::VectorPtr<wlan::phy::Capability>::New(0);
-    info->bands = f1dl::VectorPtr<wlan::phy::BandInfoPtr>::New(0);
+    info.supported_phys->push_back(wlan_device::SupportedPhy::DSSS);
+    info.supported_phys->push_back(wlan_device::SupportedPhy::CCK);
+    info.supported_phys->push_back(wlan_device::SupportedPhy::OFDM);
+    info.supported_phys->push_back(wlan_device::SupportedPhy::HT);
 
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::DSSS);
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::CCK);
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::OFDM);
-    info->supported_phys.push_back(wlan::phy::SupportedPhy::HT);
+    info.mac_roles->push_back(wlan_device::MacRole::CLIENT);
+    info.mac_roles->push_back(wlan_device::MacRole::AP);
 
-    info->mac_roles.push_back(wlan::phy::MacRole::CLIENT);
-    info->mac_roles.push_back(wlan::phy::MacRole::AP);
+    info.caps->push_back(wlan_device::Capability::SHORT_PREAMBLE);
+    info.caps->push_back(wlan_device::Capability::SHORT_SLOT_TIME);
 
-    info->caps.push_back(wlan::phy::Capability::SHORT_PREAMBLE);
-    info->caps.push_back(wlan::phy::Capability::SHORT_SLOT_TIME);
+    wlan_device::BandInfo band24;
+    band24.description = "2.4 GHz";
+    band24.ht_caps.ht_capability_info = 0x01fe;
+    auto& band24mcs = band24.ht_caps.supported_mcs_set;
+    std::fill(band24mcs.begin(), band24mcs.end(), 0);
+    band24mcs[0] = 0xff;
+    band24mcs[3] = 0x80;
+    band24mcs[12] = 0x10;
+    band24.basic_rates.reset(std::vector<uint8_t>({2, 4, 11, 22, 12, 18, 24, 36, 48, 72, 96, 108}));
+    band24.supported_channels.base_freq = 2417;
+    band24.supported_channels.channels.reset(
+            std::vector<uint8_t>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}));
 
-    auto band24 = wlan::phy::BandInfo::New();
-    band24->ht_caps = wlan::phy::HtCapabilities::New();
-    band24->supported_channels = wlan::phy::ChannelList::New();
-    band24->description = "2.4 GHz";
-    band24->ht_caps->ht_capability_info = 0x01fe;
-    band24->ht_caps->supported_mcs_set.reset(
-        std::vector<uint8_t>{0xff, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                             0x10, 0x00, 0x00, 0x00});
-    band24->basic_rates.reset(std::vector<uint8_t>{2, 4, 11, 22, 12, 18, 24, 36, 48, 72, 96, 108});
-    band24->supported_channels->base_freq = 2417;
-    band24->supported_channels->channels.reset(
-        std::vector<uint8_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14});
+    info.bands->push_back(std::move(band24));
 
-    info->bands.push_back(std::move(band24));
+    wlan_device::BandInfo band5;
+    band5.description = "5 GHz";
+    band5.ht_caps.ht_capability_info = 0x01fe;
+    auto& band5mcs = band5.ht_caps.supported_mcs_set;
+    std::fill(band5mcs.begin(), band5mcs.end(), 0);
+    band5mcs[0] = 0xff;
+    band5mcs[1] = 0xff;
+    band5mcs[3] = 0x80;
+    band5mcs[12] = 0x10;
+    band5.basic_rates.reset(std::vector<uint8_t>({12, 18, 24, 36, 48, 72, 96, 108}));
+    band5.supported_channels.base_freq = 5000;
+    band5.supported_channels.channels.reset(
+            std::vector<uint8_t>(
+                {36, 38,  40,  42,  44,  46,  48,  50,  52,  54,  56,  58,
+                60,  62,  64,  100, 102, 104, 106, 108, 110, 112, 114, 116,
+                118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140,
+                149, 151, 153, 155, 157, 159, 161, 165, 184, 188, 192, 196}));
 
-    auto band5 = wlan::phy::BandInfo::New();
-    band5->ht_caps = wlan::phy::HtCapabilities::New();
-    band5->supported_channels = wlan::phy::ChannelList::New();
-    band5->description = "5 GHz";
-    band5->ht_caps->ht_capability_info = 0x01fe;
-    band5->ht_caps->supported_mcs_set.reset(std::vector<uint8_t>{0xff, 0xff, 0x00, 0x80, 0x00, 0x00,
-                                                                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                                                                 0x10, 0x00, 0x00, 0x00});
-    band5->basic_rates.reset(std::vector<uint8_t>{12, 18, 24, 36, 48, 72, 96, 108});
-    band5->supported_channels->base_freq = 5000;
-    band5->supported_channels->channels.reset(std::vector<uint8_t>{
-        36,  38,  40,  42,  44,  46,  48,  50,  52,  54,  56,  58,  60,  62,  64,  100,
-        102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132,
-        134, 136, 138, 140, 149, 151, 153, 155, 157, 159, 161, 165, 184, 188, 192, 196});
+    info.bands->push_back(std::move(band5));
+    return info;
+}
+}  // namespace
 
-    info->bands.push_back(std::move(band5));
-
-    if (len < info->GetSerializedSize()) { return ZX_ERR_BUFFER_TOO_SMALL; }
-    if (!info->Serialize(buf, info->GetSerializedSize(), actual)) { return ZX_ERR_IO; }
-    return ZX_OK;
+void PhyDevice::Query(QueryCallback callback) {
+    zxlogf(INFO, "wlan::testing::phy::PhyDevice::Query()\n");
+    wlan_device::QueryResponse resp;
+    resp.info = get_info();
+    callback(std::move(resp));
 }
 
-zx_status_t PhyDevice::CreateIface(const void* in_buf, size_t in_len, void* out_buf, size_t out_len,
-                                   size_t* out_actual) {
-    auto req = wlan::phy::CreateIfaceRequest::New();
-    if (!req->Deserialize(const_cast<void*>(in_buf), in_len)) { return ZX_ERR_IO; }
-    zxlogf(INFO, "CreateRequest: role=%u\n", req->role);
+void PhyDevice::CreateIface(wlan_device::CreateIfaceRequest req,
+                            CreateIfaceCallback callback) {
+    zxlogf(INFO, "CreateRequest: role=%u\n", req.role);
     std::lock_guard<std::mutex> guard(lock_);
-    if (dead_) { return ZX_ERR_PEER_CLOSED; }
+    wlan_device::CreateIfaceResponse resp;
 
     // We leverage wrapping of unsigned ints to cycle back through ids to find an unused one.
     bool found_unused = false;
@@ -180,25 +173,20 @@ zx_status_t PhyDevice::CreateIface(const void* in_buf, size_t in_len, void* out_
         }
     }
     ZX_DEBUG_ASSERT(found_unused);
-    if (!found_unused) { return ZX_ERR_NO_RESOURCES; }
-
-    // Build the response now, so if the return buffer is too small we find out before we create the
-    // device.
-    auto info = wlan::iface::WlanIfaceInfo::New();
-    info->id = id;
-    if (out_len < info->GetSerializedSize()) { return ZX_ERR_BUFFER_TOO_SMALL; }
-    if (!info->Serialize(out_buf, info->GetSerializedSize(), out_actual)) { return ZX_ERR_IO; }
+    if (!found_unused) {
+        resp.status = ZX_ERR_NO_RESOURCES;
+        callback(std::move(resp));
+        return;
+    }
 
     // Create the interface device and bind it.
     auto macdev = std::make_unique<IfaceDevice>(zxdev_);
     zx_status_t status = macdev->Bind();
     if (status != ZX_OK) {
-        // Set the actual length of the output to zero and clear the first few bytes to make sure
-        // the serialized response isn't incorrectly interpreted.
-        *out_actual = 0;
-        memset(out_buf, 0, std::min<size_t>(out_len, 64));
         zxlogf(ERROR, "could not bind child wlanmac device: %d\n", status);
-        return status;
+        resp.status = status;
+        callback(std::move(resp));
+        return;
     }
 
     // Memory management follows the device lifecycle at this point. The only way an interface
@@ -209,25 +197,44 @@ zx_status_t PhyDevice::CreateIface(const void* in_buf, size_t in_len, void* out_
     // Since we successfully used the id, increment the next id counter.
     next_id_ = id + 1;
 
-    return ZX_OK;
+    resp.info.id = id;
+    resp.status = ZX_OK;
+    callback(std::move(resp));
 }
 
-zx_status_t PhyDevice::DestroyIface(const void* in_buf, size_t in_len) {
-    auto req = wlan::phy::DestroyIfaceRequest::New();
-    if (!req->Deserialize(const_cast<void*>(in_buf), in_len)) { return ZX_ERR_IO; }
-    zxlogf(INFO, "DestroyRequest: id=%u\n", req->id);
+void PhyDevice::DestroyIface(wlan_device::DestroyIfaceRequest req,
+                             DestroyIfaceCallback callback) {
+    zxlogf(INFO, "DestroyRequest: id=%u\n", req.id);
+
+    wlan_device::DestroyIfaceResponse resp;
 
     std::lock_guard<std::mutex> guard(lock_);
-    if (dead_) { return ZX_ERR_PEER_CLOSED; }
-
-    auto intf = ifaces_.find(req->id);
-    if (intf == ifaces_.end()) { return ZX_ERR_NOT_FOUND; }
+    auto intf = ifaces_.find(req.id);
+    if (intf == ifaces_.end()) {
+        resp.status = ZX_ERR_NOT_FOUND;
+        callback(std::move(resp));
+        return;
+    }
 
     device_remove(intf->second->zxdev());
     // Remove the device from our map. We do NOT free the memory, since the devhost owns it and will
     // call release when it's safe to free the memory.
-    ifaces_.erase(req->id);
+    ifaces_.erase(req.id);
 
+    resp.status = ZX_OK;
+    callback(std::move(resp));
+}
+
+zx_status_t PhyDevice::Connect(const void* buf, size_t len) {
+    if (buf == nullptr || len < sizeof(zx_handle_t)) {
+        return ZX_ERR_BUFFER_TOO_SMALL;
+    }
+
+    zx_handle_t hnd = *reinterpret_cast<const zx_handle_t*>(buf);
+    zx::channel chan(hnd);
+
+    zx_status_t status = dispatcher_->AddBinding(std::move(chan), this);
+    if (status != ZX_OK) { return status; }
     return ZX_OK;
 }
 