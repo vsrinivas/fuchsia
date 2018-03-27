ee6677babbe8f2bca1b637367e11717620ee61de
Shai Barack <shayba@google.com>
2018-03-27 14:06:27 -0700

    [syscall][vmo] removal of zx_vmo_{read,write}() actual parameter 11/n
    Stop using zx_vmo_read_old in Garnet
    
    ZX-1802
    
    Change-Id: Ic577a76cd79eddbf4d363255f9d361f42455be02

--- lib/farfs/file_system.cc
+++ lib/farfs/file_system.cc
@@ -153,10 +153,9 @@ bool FileSystem::GetFileAsString(fxl::StringView path, std::string* result) {
   }
   std::string data;
   data.resize(entry.data_length);
-  size_t actual;
-  zx_status_t status = zx_vmo_read_old(vmo_, &data[0], entry.data_offset,
-                                       entry.data_length, &actual);
-  if (status != ZX_OK || actual != entry.data_length)
+  zx_status_t status = zx_vmo_read(vmo_, &data[0], entry.data_offset,
+                                       entry.data_length);
+  if (status != ZX_OK)
     return false;
   result->swap(data);
   return true;
