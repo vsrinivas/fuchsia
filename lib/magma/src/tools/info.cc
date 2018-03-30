61205e64eb85977664d43dc353f834265dd997b4
David Stevens <stevensd@google.com>
2018-03-30 15:12:19 -0700

    [magma] Remove unnecessary reference to displays
    The Intel MSD driver has supported IOCTL_MAGMA_DUMP_STATUS on the gpu
    device for a while, so there's no need to potentially fall back to the
    display device.
    
    Change-Id: I81b38afc6bbf1f360f8bd4508df16ab09d2065ce

--- lib/magma/src/tools/info.cc
+++ lib/magma/src/tools/info.cc
@@ -9,19 +9,14 @@
 #include "magma_util/macros.h"
 #include "magma_util/platform/zircon/zircon_platform_ioctl.h"
 
-const char* kDeviceName = "/dev/class/display/000";
 const char* kGpuDeviceName = "/dev/class/gpu/000";
 
 int main(int argc, char** argv)
 {
     int fd = open(kGpuDeviceName, O_RDONLY);
     if (fd < 0) {
-        // TODO(MA-349): Use only /dev/class/gpu.
-        fd = open(kDeviceName, O_RDONLY);
-        if (fd < 0) {
-            printf("Failed to open display device %s or %s\n", kGpuDeviceName, kDeviceName);
-            return -1;
-        }
+        printf("Failed to open magma device %s\n", kGpuDeviceName);
+        return -1;
     }
 
     int ret = fdio_ioctl(fd, IOCTL_MAGMA_DUMP_STATUS, nullptr, 0, nullptr, 0);
