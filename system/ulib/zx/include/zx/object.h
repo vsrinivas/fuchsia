7f1bb78381971bab953c05e7936e0f28a3848536
Benjamin Lerman <qsr@chromium.org>
2018-04-03 17:48:48 +0200

    [zx] All comparaison operators on object_base
    FIDL-121 #comment Add comparaison on handles.
    
    Change-Id: If76cbed088b2c1c6f85c7c445c611b7d1be33111

--- system/ulib/zx/include/zx/object.h
+++ system/ulib/zx/include/zx/object.h
@@ -194,6 +194,22 @@ template <typename T> bool operator!=(const object<T>& a, const object<T>& b) {
     return !(a == b);
 }
 
+template <typename T> bool operator<(const object<T>& a, const object<T>& b) {
+  return a.get() < b.get();
+}
+
+template <typename T> bool operator>(const object<T>& a, const object<T>& b) {
+  return a.get() > b.get();
+}
+
+template <typename T> bool operator<=(const object<T>& a, const object<T>& b) {
+  return !(a.get() > b.get());
+}
+
+template <typename T> bool operator>=(const object<T>& a, const object<T>& b) {
+  return !(a.get() < b.get());
+}
+
 template <typename T> bool operator==(zx_handle_t a, const object<T>& b) {
     return a == b.get();
 }
@@ -202,12 +218,44 @@ template <typename T> bool operator!=(zx_handle_t a, const object<T>& b) {
     return !(a == b);
 }
 
+template <typename T> bool operator<(zx_handle_t a, const object<T>& b) {
+  return a < b.get();
+}
+
+template <typename T> bool operator>(zx_handle_t a, const object<T>& b) {
+  return a > b.get();
+}
+
+template <typename T> bool operator<=(zx_handle_t a, const object<T>& b) {
+  return !(a > b.get());
+}
+
+template <typename T> bool operator>=(zx_handle_t a, const object<T>& b) {
+  return !(a < b.get());
+}
+
 template <typename T> bool operator==(const object<T>& a, zx_handle_t b) {
     return a.get() == b;
 }
 
 template <typename T> bool operator!=(const object<T>& a, zx_handle_t b) {
     return !(a == b);
+}
+
+template <typename T> bool operator<(const object<T>& a, zx_handle_t b) {
+  return a.get() < b;
+}
+
+template <typename T> bool operator>(const object<T>& a, zx_handle_t b) {
+  return a.get() > b;
+}
+
+template <typename T> bool operator<=(const object<T>& a, zx_handle_t b) {
+  return !(a.get() > b);
+}
+
+template <typename T> bool operator>=(const object<T>& a, zx_handle_t b) {
+  return !(a.get() < b);
 }
 
 // Wraps a handle to an object to provide type-safe access to its operations
