38f12efb656be7dd7ac9c15e162dd43fe29e555a
John Bauman <jbauman@google.com>
2018-04-03 16:09:53 -0700

    [magma] Add PlatformAddressRange
    This allocates a vmar from the root vmar.
    
    MA-358
    Test: go/magma-tps#L0
    
    Change-Id: I2d4f6f058e76a121bfd9a6484a795d128624663d

---
+++ lib/magma/src/magma_util/platform/platform_address_range.h
@@ -0,0 +1,25 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PLATFORM_ADDRESS_RANGE_H
+#define PLATFORM_ADDRESS_RANGE_H
+
+#include <cstdint>
+#include <memory>
+
+namespace magma {
+
+class PlatformAddressRange {
+public:
+    static std::unique_ptr<PlatformAddressRange> Create(uint64_t size);
+
+    virtual ~PlatformAddressRange() {}
+
+    virtual uint64_t address() = 0;
+    virtual uint64_t size() = 0;
+};
+
+} // namespace magma
+
+#endif // PLATFORM_ADDRESS_RANGE_H
---
+++ lib/magma/src/magma_util/platform/zircon/zircon_platform_address_range.cc
@@ -0,0 +1,41 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform_address_range.h"
+
+#include "magma_util/macros.h"
+#include <zx/vmar.h>
+#include <zx/vmo.h>
+
+namespace magma {
+
+class ZirconPlatformAddressRange : public PlatformAddressRange {
+public:
+    ~ZirconPlatformAddressRange() override {}
+
+    ZirconPlatformAddressRange(zx::vmar vmar, uint64_t address, uint64_t size)
+        : vmar_(std::move(vmar)), address_(address), size_(size)
+    {
+    }
+
+    uint64_t address() override { return address_; }
+    uint64_t size() override { return size_; }
+
+private:
+    zx::vmar vmar_;
+    uint64_t address_;
+    uint64_t size_;
+};
+
+std::unique_ptr<PlatformAddressRange> PlatformAddressRange::Create(uint64_t size)
+{
+    uint64_t address;
+    zx::vmar vmar;
+    zx_status_t status = zx::vmar::root_self()->allocate(0, size, 0, &vmar, &address);
+    if (status != ZX_OK)
+        return DRETP(nullptr, "zx_vmar_allocate failed: %x", status);
+    return std::make_unique<ZirconPlatformAddressRange>(std::move(vmar), address, size);
+}
+
+} // namespace magma
