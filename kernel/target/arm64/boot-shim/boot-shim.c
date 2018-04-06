wulalaya2py/zircon

[arm64][boot-shim] Add support for printing to QEMU uart
and also some cleanup from review comments I missed in the last CL

Change-Id: I90082a841cbdb4970a4cde81065b4429eed41780

kernel/target/arm64/boot-shim/boot-shim.c
@@ -3,21 +3,28 @@
 // found in the LICENSE file.
 
 #include "boot-shim.h"
+#include "debug.h"
 
-static void fail(void) {
+#define ROUNDUP(a, b) (((a) + ((b)-1)) & ~((b)-1))
+
+static void fail(const char* message) {
+    uart_print(message);
+    __builtin_trap();
     while (1) {}
 }
 
 uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
+    uart_print("boot_shim: hi there!\n");
+
     // sanity check the bootdata headers
     // it must start with a container record followed by a kernel record
     if (kernel->hdr_file.type != BOOTDATA_CONTAINER || kernel->hdr_file.extra != BOOTDATA_MAGIC ||
         kernel->hdr_file.magic != BOOTITEM_MAGIC || kernel->hdr_kernel.type != BOOTDATA_KERNEL ||
         kernel->hdr_kernel.magic != BOOTITEM_MAGIC) {
-        fail();
+        fail("zircon_kernel_t sanity check failed\n");
     }
 
-    void* kernel_base;
+    uintptr_t kernel_base;
     uint32_t bootdata_size = kernel->hdr_file.length + sizeof(bootdata_t);
     uint32_t kernel_size = kernel->hdr_kernel.length + 2 * sizeof(bootdata_t);
 
@@ -26,22 +33,22 @@ uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
         // we must relocate the kernel after the rest of the bootdata.
 
         // round up to align new kernel location
-        bootdata_size = ((bootdata_size + (KERNEL_ALIGN - 1)) / KERNEL_ALIGN) * KERNEL_ALIGN;
-        kernel_base = (void *)kernel + bootdata_size;
+        bootdata_size = ROUNDUP(bootdata_size, KERNEL_ALIGN);
+        kernel_base = (uintptr_t)kernel + bootdata_size;
 
         // poor-man's memcpy, since we don't have a libc in here
-        uint64_t* src = (uint64_t *)kernel;
-        uint64_t* dest = (uint64_t *)kernel_base;
-        uint64_t* end = (uint64_t *)((void *)src + kernel_size);
-        end = (uint64_t *)(((uint64_t)end + 7) & ~7);
+        uint64_t* src = (uint64_t*)kernel;
+        uint64_t* dest = (uint64_t*)kernel_base;
+        uint64_t* end = (uint64_t*)((void*)src + kernel_size);
+        end = (uint64_t*)BOOTDATA_ALIGN((uintptr_t)end);
 
         while (src < end) {
             *dest++ = *src++;
         }
     } else {
-        kernel_base = (void *)kernel;
+        kernel_base = (uintptr_t)kernel;
     }
 
     // return kernel entry point address
-    return (uint64_t)kernel_base + kernel->data_kernel.entry64;
+    return kernel_base + kernel->data_kernel.entry64;
 }

kernel/target/arm64/boot-shim/debug.c
@@ -1,0 +1,31 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+#include "debug.h"
+
+// #define QEMU_PRINT
+
+#ifdef QEMU_PRINT
+static volatile uint32_t* uart_fifo_dr = (uint32_t *)0x09000000;
+static volatile uint32_t* uart_fifo_fr = (uint32_t *)0x09000018;
+
+static void uart_pputc(char c)
+{
+    /* spin while fifo is full */
+    while (*uart_fifo_fr & (1<<5))
+        ;
+    *uart_fifo_dr = c;
+}
+
+void uart_print(const char* str) {
+    char ch;
+    while ((ch = *str++)) {
+        uart_pputc(ch);
+    }
+}
+#else
+void uart_print(const char* str) {
+}
+#endif

kernel/target/arm64/boot-shim/debug.h
@@ -1,0 +1,7 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+void uart_print(const char* str);

vsrinivas/fuchsia

kernel/target/arm64/boot-shim/boot-shim.c
@@ -3,21 +3,27 @@
 // found in the LICENSE file.
 
 #include "boot-shim.h"
+#include "debug.h"
 
-static void fail(void) {
+#define ROUNDUP(a, b) (((a) + ((b)-1)) & ~((b)-1))
+
+static void fail(const char* message) {
+    uart_puts(message);
     while (1) {}
 }
 
 uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
+    uart_puts("boot_shim: hi there!\n");
+
     // sanity check the bootdata headers
     // it must start with a container record followed by a kernel record
     if (kernel->hdr_file.type != BOOTDATA_CONTAINER || kernel->hdr_file.extra != BOOTDATA_MAGIC ||
         kernel->hdr_file.magic != BOOTITEM_MAGIC || kernel->hdr_kernel.type != BOOTDATA_KERNEL ||
         kernel->hdr_kernel.magic != BOOTITEM_MAGIC) {
-        fail();
+        fail("zircon_kernel_t sanity check failed\n");
     }
 
-    void* kernel_base;
+    uintptr_t kernel_base;
     uint32_t bootdata_size = kernel->hdr_file.length + sizeof(bootdata_t);
     uint32_t kernel_size = kernel->hdr_kernel.length + 2 * sizeof(bootdata_t);
 
@@ -26,22 +32,22 @@ uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
         // we must relocate the kernel after the rest of the bootdata.
 
         // round up to align new kernel location
-        bootdata_size = ((bootdata_size + (KERNEL_ALIGN - 1)) / KERNEL_ALIGN) * KERNEL_ALIGN;
-        kernel_base = (void *)kernel + bootdata_size;
+        bootdata_size = ROUNDUP(bootdata_size, KERNEL_ALIGN);
+        kernel_base = (uintptr_t)kernel + bootdata_size;
 
         // poor-man's memcpy, since we don't have a libc in here
-        uint64_t* src = (uint64_t *)kernel;
-        uint64_t* dest = (uint64_t *)kernel_base;
-        uint64_t* end = (uint64_t *)((void *)src + kernel_size);
-        end = (uint64_t *)(((uint64_t)end + 7) & ~7);
+        uint64_t* src = (uint64_t*)kernel;
+        uint64_t* dest = (uint64_t*)kernel_base;
+        uint64_t* end = (uint64_t*)((void*)src + kernel_size);
+        end = (uint64_t*)BOOTDATA_ALIGN((uintptr_t)end);
 
         while (src < end) {
             *dest++ = *src++;
         }
     } else {
-        kernel_base = (void *)kernel;
+        kernel_base = (uintptr_t)kernel;
     }
 
     // return kernel entry point address
-    return (uint64_t)kernel_base + kernel->data_kernel.entry64;
+    return kernel_base + kernel->data_kernel.entry64;
 }

kernel/target/arm64/boot-shim/debug.c
@@ -1,0 +1,31 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+#include "debug.h"
+
+// #define QEMU_PRINT
+
+#ifdef QEMU_PRINT
+static volatile uint32_t* uart_fifo_dr = (uint32_t *)0x09000000;
+static volatile uint32_t* uart_fifo_fr = (uint32_t *)0x09000018;
+
+static void uart_pputc(char c)
+{
+    /* spin while fifo is full */
+    while (*uart_fifo_fr & (1<<5))
+        ;
+    *uart_fifo_dr = c;
+}
+
+void uart_puts(const char* str) {
+    char ch;
+    while ((ch = *str++)) {
+        uart_pputc(ch);
+    }
+}
+#else
+void uart_puts(const char* str) {
+}
+#endif

kernel/target/arm64/boot-shim/debug.h
@@ -1,0 +1,7 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+void uart_puts(const char* str);