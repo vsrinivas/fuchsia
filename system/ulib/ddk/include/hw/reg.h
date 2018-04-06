1f746c107831e02913be0333438d593abe6e2fd5
Yvonne Yip <yky@google.com>
2018-04-06 16:15:40 -0700

    [ddk] Add RMWREG* macros
    Change-Id: I4cf55048cfe12e2adbd480e08de75899b00e09fc

--- system/ulib/ddk/include/hw/reg.h
+++ system/ulib/ddk/include/hw/reg.h
@@ -11,6 +11,15 @@
 #define REG32(addr) ((volatile uint32_t *)(uintptr_t)(addr))
 #define REG64(addr) ((volatile uint64_t *)(uintptr_t)(addr))
 
+#define RMWREG8(addr, startbit, width, val) \
+    *REG8(addr) = (*REG8(addr) & ~(((1 << (width)) - 1) << (startbit))) | ((val) << (startbit))
+#define RMWREG16(addr, startbit, width, val) \
+    *REG16(addr) = (*REG16(addr) & ~(((1 << (width)) - 1) << (startbit))) | ((val) << (startbit))
+#define RMWREG32(addr, startbit, width, val) \
+    *REG32(addr) = (*REG32(addr) & ~(((1 << (width)) - 1) << (startbit))) | ((val) << (startbit))
+#define RMWREG64(addr, startbit, width, val) \
+    *REG64(addr) = (*REG64(addr) & ~(((1ull << (width)) - 1) << (startbit))) | ((val) << (startbit))
+
 #define writeb(v, a) (*REG8(a) = (v))
 #define readb(a) (*REG8(a))
 #define set_bitsb(v, a) (*REG8(a) |= (v))
