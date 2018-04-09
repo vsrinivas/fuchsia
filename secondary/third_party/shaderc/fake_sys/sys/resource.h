950ede7291c97b7f0ebb10333b81c869fb60bfc4
Josh Gargus <jjosh@google.com>
2018-04-09 15:38:33 -0700

    [secondary] Add BUILD.gn etc. for shaderc and friends.
    shaderc has two targets that need BUILD.gn files (libshaderc
    and libshaderc_util), and depends on two libraries that also
    need them (glslang and spirv-tools).
    
    An additional workaround is required for the absence of
    <sys/resource.h> on Fuchsia.  It suffices to put an empty
    resource.h on the include path.
    
    Change-Id: I9f6ab691182967002f409e0e5b5cdc5180d25f93

---
+++ secondary/third_party/shaderc/fake_sys/sys/resource.h
@@ -0,0 +1,5 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Fake version of <sys/resource.h> to allow shaderc and friends to compile.
