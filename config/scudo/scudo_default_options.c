c8eea9ae3c12f7a9a1776c48146438e3ef515f82
Julia Hansbrough <flowerhack@google.com>
2018-04-10 15:50:06 -0700

    [scudo] Set visibility of __scudo_default_options.
    When linking Scudo in dynamically, __scudo_default_options defaults to
    being anonymous, so the runtime can't find it.
    
    Add attributes to make it visible.
    
    Change-Id: I503f54b41c5cfcbc0a7634c3207f6abd3d4c66f2

--- config/scudo/scudo_default_options.c
+++ config/scudo/scudo_default_options.c
@@ -3,11 +3,12 @@
 // found in the LICENSE file.
 
 #include <sanitizer/scudo_interface.h>
+#include <zircon/compiler.h>
 
 // This exists to be built into every executable selected to use the
 // scudo variant.  Scudo applies the options here before
 // looking at the SCUDO_OPTIONS environment variable.
-const char* __scudo_default_options(void) {
+__EXPORT const char* __scudo_default_options(void) {
   // This macro is defined by BUILD.gn from the `scudo_default_options` GN
   // build argument.
   return SCUDO_DEFAULT_OPTIONS;
