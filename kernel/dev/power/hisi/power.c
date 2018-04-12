DeDf/zircon

[kernel][arm64] Remove remaining use of MDI from the arm64 kernel
Kernel drivers are now configured via bootdata passed from the boot shims.

Change-Id: I056b9b2c74a99226d98416b0308480b85460e918

kernel/dev/power/hisi/power.c
@@ -7,10 +7,9 @@
 #include <arch/arm64/periphmap.h>
 #include <dev/power.h>
 #include <dev/psci.h>
-#include <mdi/mdi.h>
-#include <mdi/mdi-defs.h>
 #include <pdev/driver.h>
 #include <pdev/power.h>
+#include <zircon/boot/kernel-drivers.h>
 #include <stdio.h>
 
 #define PMU_HRST_OFFSET         0x404
@@ -18,8 +17,8 @@
 #define SCTRL_DDR_BYPASS        (1 << 31)
 #define SCTRL_REBOOT_OFFSET     0x4
 
-static uint64_t sctrl_base;
-static uint64_t pmu_base;
+static vaddr_t sctrl_base;
+static vaddr_t pmu_base;
 
 static void hisi_reboot(enum reboot_flags flags) {
     volatile void* sctrl = (volatile void *)sctrl_base;
@@ -42,35 +41,17 @@ static const struct pdev_power_ops hisi_power_ops = {
     .shutdown = hisi_shutdown,
 };
 
-static void hisi_power_init(mdi_node_ref_t* node, uint level) {
-    bool got_sctrl = false;
-    bool got_pmu = false;
+static void hisi_power_init(const void* driver_data) {
+    const bootdata_hisilicon_power_driver_t* driver = driver_data;
+    ASSERT(driver->sctrl_phys && driver->pmu_phys);
 
-    mdi_node_ref_t child;
-    mdi_each_child(node, &child) {
-        switch (mdi_id(&child)) {
-        case MDI_HISI_POWER_SCTRL_BASE_PHYS:
-            got_sctrl = !mdi_node_uint64(&child, &sctrl_base);
-            break;
-        case MDI_HISI_POWER_PMU_BASE_PHYS:
-            got_pmu = !mdi_node_uint64(&child, &pmu_base);
-            break;
-        }
-    }
-
-    if (!got_sctrl) {
-        panic("hisi power: MDI_HISI_POWER_SCTRL_BASE_VIRT not defined\n");
-    }
-    if (!got_pmu) {
-        panic("hisi power uart: MDI_HISI_POWER_PMU_BASE_VIRT not defined\n");
-    }
 
     // get virtual addresses of our peripheral bases
-    sctrl_base = periph_paddr_to_vaddr(sctrl_base);
-    pmu_base = periph_paddr_to_vaddr(pmu_base);
+    sctrl_base = periph_paddr_to_vaddr(driver->sctrl_phys);
+    pmu_base = periph_paddr_to_vaddr(driver->pmu_phys);
     ASSERT(sctrl_base && pmu_base);
 
     pdev_register_power(&hisi_power_ops);
 }
 
-LK_PDEV_INIT(hisi_power_init, MDI_HISI_POWER, hisi_power_init, LK_INIT_LEVEL_PLATFORM);
+LK_PDEV_INIT(hisi_power_init, KDRV_HISILICON_POWER, hisi_power_init, LK_INIT_LEVEL_PLATFORM);

kernel/dev/uart/nxp-imx/uart.c
@@ -12,10 +12,9 @@
 #include <dev/interrupt.h>
 #include <dev/uart.h>
 #include <platform/debug.h>
-#include <mdi/mdi.h>
-#include <mdi/mdi-defs.h>
 #include <pdev/driver.h>
 #include <pdev/uart.h>
+#include <zircon/boot/kernel-drivers.h>
 
 /* Registers */
 #define MX8_URXD                    (0x00)
@@ -60,9 +59,9 @@
 #define RXBUF_SIZE 32
 
 
-// values read from MDI
+// values read from bootdata
 static bool initialized = false;
-static uint64_t uart_base = 0;
+static vaddr_t uart_base = 0;
 static uint32_t uart_irq = 0;
 static cbuf_t uart_rx_buf;
 // static cbuf_t uart_tx_buf;
@@ -197,7 +196,7 @@ static const struct pdev_uart_ops uart_ops = {
     .dputs = imx_dputs,
 };
 
-static void imx_uart_init(mdi_node_ref_t* node, uint level)
+static void imx_uart_init(const void* driver_data)
 {
     uint32_t regVal;
 
@@ -243,35 +242,16 @@ static void imx_uart_init(mdi_node_ref_t* node, uint level)
     unmask_interrupt(uart_irq);
 }
 
-static void imx_uart_init_early(mdi_node_ref_t* node, uint level) {
-    uint64_t uart_base_phys = 0;
-    bool got_uart_base_phys = false;
-    bool got_uart_irq = false;
+static void imx_uart_init_early(const void* driver_data) {
+    const bootdata_kernel_driver_t* driver = driver_data;
+    ASSERT(driver->mmio_phys && driver->irq);
 
-    mdi_node_ref_t child;
-    mdi_each_child(node, &child) {
-        switch (mdi_id(&child)) {
-        case MDI_BASE_PHYS:
-            got_uart_base_phys = !mdi_node_uint64(&child, &uart_base_phys);
-            break;
-        case MDI_IRQ:
-            got_uart_irq = !mdi_node_uint32(&child, &uart_irq);
-            break;
-        }
-    }
-
-    if (!got_uart_base_phys) {
-        panic("imx uart: uart_base_phys not defined\n");
-    }
-    if (!got_uart_irq) {
-        panic("imx uart: uart_irq not defined\n");
-    }
-
-    uart_base = periph_paddr_to_vaddr(uart_base_phys);
+    uart_base = periph_paddr_to_vaddr(driver->mmio_phys);
     ASSERT(uart_base);
+    uart_irq = driver->irq;
 
     pdev_register_uart(&uart_ops);
 }
 
-LK_PDEV_INIT(imx_uart_init_early, MDI_ARM_NXP_IMX_UART, imx_uart_init_early, LK_INIT_LEVEL_PLATFORM_EARLY);
-LK_PDEV_INIT(imx_uart_init, MDI_ARM_NXP_IMX_UART, imx_uart_init, LK_INIT_LEVEL_PLATFORM);
+LK_PDEV_INIT(imx_uart_init_early, KDRV_NXP_IMX_UART, imx_uart_init_early, LK_INIT_LEVEL_PLATFORM_EARLY);
+LK_PDEV_INIT(imx_uart_init, KDRV_NXP_IMX_UART, imx_uart_init, LK_INIT_LEVEL_PLATFORM);

kernel/target/arm64/board/hikey960/boot-shim-config.h
@@ -57,3 +57,54 @@ static const bootdata_mem_range_t mem_config[] = {
         .length = 0x40000,
     },
 };
+
+static const bootdata_kernel_driver_t uart_driver = {
+    .mmio_phys = 0xfff32000,
+    .irq = 111,
+};
+
+static const bootdata_arm_gicv2_driver_t gicv2_driver = {
+    .mmio_phys = 0xe82b0000,
+    .gicd_offset = 0x1000,
+    .gicc_offset = 0x2000,
+    .gich_offset = 0x4000,
+    .gicv_offset = 0x6000,
+    .ipi_base = 13,
+};
+
+static const bootdata_arm_psci_driver_t psci_driver = {
+    .use_hvc = false,
+};
+
+static const bootdata_arm_generic_timer_driver_t timer_driver = {
+    .irq_phys = 30,
+    .irq_virt = 27,
+};
+
+static const bootdata_hisilicon_power_driver_t power_driver = {
+    .sctrl_phys = 0xfff0a000,
+    .pmu_phys = 0xfff34000,
+};
+
+static void append_board_bootdata(bootdata_t* bootdata) {
+    // add CPU configuration
+    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, 0, &cpu_config,
+                    sizeof(bootdata_cpu_config_t) +
+                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
+
+    // add memory configuration
+    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_config,
+                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+
+    // add kernel drivers
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_PL011_UART, &uart_driver,
+                    sizeof(uart_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V2, &gicv2_driver,
+                    sizeof(gicv2_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_PSCI, &psci_driver,
+                    sizeof(psci_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER, &timer_driver,
+                    sizeof(timer_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_HISILICON_POWER, &power_driver,
+                    sizeof(power_driver));
+}

kernel/target/arm64/board/qemu/boot-shim-config.h
@@ -25,3 +25,59 @@ static const bootdata_mem_range_t mem_config[] = {
         .length = 0x40000000,
     },
 };
+
+static const bootdata_kernel_driver_t uart_driver = {
+    .mmio_phys = 0x09000000,
+    .irq = 33,
+};
+
+static const bootdata_arm_gicv3_driver_t gicv3_driver = {
+    .mmio_phys = 0x08000000,
+    .gicd_offset = 0x00000,
+    .gicr_offset = 0xa0000,
+    .gicr_stride = 0x20000,
+    .ipi_base = 13,
+    .optional = true,
+};
+
+static const bootdata_arm_gicv2_driver_t gicv2_driver = {
+    .mmio_phys = 0x08000000,
+    .msi_frame_phys = 0x08020000,
+    .gicd_offset = 0x00000,
+    .gicc_offset = 0x10000,
+    .ipi_base = 13,
+    .optional = true,
+    .use_msi = true,
+};
+
+static const bootdata_arm_psci_driver_t psci_driver = {
+    .use_hvc = true,
+};
+
+static const bootdata_arm_generic_timer_driver_t timer_driver = {
+    .irq_phys = 30,
+    .irq_virt = 27,
+};
+
+static void append_board_bootdata(bootdata_t* bootdata) {
+    // add CPU configuration
+    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, 0, &cpu_config,
+                    sizeof(bootdata_cpu_config_t) +
+                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
+
+    // add memory configuration
+    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_config,
+                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+
+    // add kernel drivers
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_PL011_UART, &uart_driver,
+                    sizeof(uart_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V3, &gicv3_driver,
+                    sizeof(gicv3_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V2, &gicv2_driver,
+                    sizeof(gicv2_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_PSCI, &psci_driver,
+                    sizeof(psci_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER, &timer_driver,
+                    sizeof(timer_driver));
+}

kernel/target/arm64/board/vim2/boot-shim-config.h
@@ -52,3 +52,56 @@ static const bootdata_mem_range_t mem_config[] = {
         .length = 0x9000000,
     },
 };
+
+static const bootdata_kernel_driver_t uart_driver = {
+    .mmio_phys = 0xc81004c0,
+    .irq = 225,
+};
+
+static const bootdata_arm_gicv2_driver_t gicv2_driver = {
+    .mmio_phys = 0xc4300000,
+    .gicd_offset = 0x1000,
+    .gicc_offset = 0x2000,
+    .gich_offset = 0x4000,
+    .gicv_offset = 0x6000,
+    .ipi_base = 5,
+};
+
+static const bootdata_arm_psci_driver_t psci_driver = {
+    .use_hvc = false,
+    .reboot_args = { 1, 0, 0 },
+    .reboot_bootloader_args = { 4, 0, 0 },
+};
+
+static const bootdata_arm_generic_timer_driver_t timer_driver = {
+    .irq_phys = 30,
+};
+
+static const bootdata_amlogic_hdcp_driver_t hdcp_driver = {
+    .preset_phys = 0xc1104000,
+    .hui_phys = 0xc883c000,
+    .hdmitx_phys = 0xc883a000,
+};
+
+static void append_board_bootdata(bootdata_t* bootdata) {
+    // add CPU configuration
+    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, 0, &cpu_config,
+                    sizeof(bootdata_cpu_config_t) +
+                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
+
+    // add memory configuration
+    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_config,
+                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+
+    // add kernel drivers
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_AMLOGIC_UART, &uart_driver,
+                    sizeof(uart_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V2, &gicv2_driver,
+                    sizeof(gicv2_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_PSCI, &psci_driver,
+                    sizeof(psci_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER, &timer_driver,
+                    sizeof(timer_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_AMLOGIC_HDCP, &hdcp_driver,
+                    sizeof(hdcp_driver));
+}

kernel/target/arm64/boot-shim/boot-shim.c
@@ -9,9 +9,18 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <zircon/boot/bootdata.h>
+#include <zircon/boot/kernel-drivers.h>
 
 // uncomment to dump device tree at boot
 // #define PRINT_DEVICE_TREE
+
+// used in boot-shim-config.h and in this file below
+static void append_bootdata(bootdata_t* container, uint32_t type, uint32_t extra,
+                            const void* payload, uint32_t length);
+
+// defined in boot-shim-config.h
+static void append_board_bootdata(bootdata_t* container);
 
 // Include board specific definitions
 #include "boot-shim-config.h"
@@ -103,13 +112,13 @@ static void read_device_tree(void* device_tree, device_tree_context_t* ctx) {
 }
 #endif // HAS_DEVICE_TREE
 
-static void append_bootdata(bootdata_t* container, uint32_t type, const void* payload,
-                            uint32_t length) {
+static void append_bootdata(bootdata_t* container, uint32_t type, uint32_t extra,
+                            const void* payload, uint32_t length) {
     bootdata_t* dest = (bootdata_t*)((uintptr_t)container + container->length + sizeof(bootdata_t));
 
     dest->type = type;
     dest->length = length;
-    dest->extra = 0;
+    dest->extra = extra;
     dest->flags = 0;
     dest->reserved0 = 0;
     dest->reserved1 = 0;
@@ -171,14 +180,8 @@ uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
     }
 #endif // HAS_DEVICE_TREE
 
-    // append cpu configuration from boot-shim-config.h
-    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, &cpu_config,
-                    sizeof(bootdata_cpu_config_t) +
-                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
-
-    // append memory configuration from boot-shim-config.h
-    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, &mem_config,
-                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+    // add board specific bootdata
+    append_board_bootdata(bootdata);
 
 #if HAS_DEVICE_TREE
     // look for optional RAM size in device tree
@@ -192,14 +195,14 @@ uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
         uart_puts("Setting RAM size device tree value: ");
         uart_print_hex(ctx.memory);
         uart_puts("\n");
-        append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, &mem_range, sizeof(mem_range));
+        append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_range, sizeof(mem_range));
     } else {
         uart_puts("RAM size not found in device tree\n");
     }
 
     // append kernel command line
     if (ctx.cmdline && ctx.cmdline_length) {
-        append_bootdata(bootdata, BOOTDATA_CMDLINE, ctx.cmdline, ctx.cmdline_length);
+        append_bootdata(bootdata, BOOTDATA_CMDLINE, 0, ctx.cmdline, ctx.cmdline_length);
     }
 #endif // HAS_DEVICE_TREE
 

vsrinivas/fuchsia

kernel/dev/power/hisi/power.c
@@ -7,10 +7,9 @@
 #include <arch/arm64/periphmap.h>
 #include <dev/power.h>
 #include <dev/psci.h>
-#include <mdi/mdi.h>
-#include <mdi/mdi-defs.h>
 #include <pdev/driver.h>
 #include <pdev/power.h>
+#include <zircon/boot/driver-config.h>
 #include <stdio.h>
 
 #define PMU_HRST_OFFSET         0x404
@@ -18,8 +17,8 @@
 #define SCTRL_DDR_BYPASS        (1 << 31)
 #define SCTRL_REBOOT_OFFSET     0x4
 
-static uint64_t sctrl_base;
-static uint64_t pmu_base;
+static vaddr_t sctrl_base;
+static vaddr_t pmu_base;
 
 static void hisi_reboot(enum reboot_flags flags) {
     volatile void* sctrl = (volatile void *)sctrl_base;
@@ -42,35 +41,18 @@ static const struct pdev_power_ops hisi_power_ops = {
     .shutdown = hisi_shutdown,
 };
 
-static void hisi_power_init(mdi_node_ref_t* node, uint level) {
-    bool got_sctrl = false;
-    bool got_pmu = false;
+static void hisi_power_init(const void* driver_data, uint32_t length) {
+    ASSERT(length >= sizeof(dcfg_hisilicon_power_driver_t));
+    const dcfg_hisilicon_power_driver_t* driver = driver_data;
+    ASSERT(driver->sctrl_phys && driver->pmu_phys);
 
-    mdi_node_ref_t child;
-    mdi_each_child(node, &child) {
-        switch (mdi_id(&child)) {
-        case MDI_HISI_POWER_SCTRL_BASE_PHYS:
-            got_sctrl = !mdi_node_uint64(&child, &sctrl_base);
-            break;
-        case MDI_HISI_POWER_PMU_BASE_PHYS:
-            got_pmu = !mdi_node_uint64(&child, &pmu_base);
-            break;
-        }
-    }
-
-    if (!got_sctrl) {
-        panic("hisi power: MDI_HISI_POWER_SCTRL_BASE_VIRT not defined\n");
-    }
-    if (!got_pmu) {
-        panic("hisi power uart: MDI_HISI_POWER_PMU_BASE_VIRT not defined\n");
-    }
 
     // get virtual addresses of our peripheral bases
-    sctrl_base = periph_paddr_to_vaddr(sctrl_base);
-    pmu_base = periph_paddr_to_vaddr(pmu_base);
+    sctrl_base = periph_paddr_to_vaddr(driver->sctrl_phys);
+    pmu_base = periph_paddr_to_vaddr(driver->pmu_phys);
     ASSERT(sctrl_base && pmu_base);
 
     pdev_register_power(&hisi_power_ops);
 }
 
-LK_PDEV_INIT(hisi_power_init, MDI_HISI_POWER, hisi_power_init, LK_INIT_LEVEL_PLATFORM);
+LK_PDEV_INIT(hisi_power_init, KDRV_HISILICON_POWER, hisi_power_init, LK_INIT_LEVEL_PLATFORM);

kernel/dev/uart/nxp-imx/uart.c
@@ -12,10 +12,9 @@
 #include <dev/interrupt.h>
 #include <dev/uart.h>
 #include <platform/debug.h>
-#include <mdi/mdi.h>
-#include <mdi/mdi-defs.h>
 #include <pdev/driver.h>
 #include <pdev/uart.h>
+#include <zircon/boot/driver-config.h>
 
 /* Registers */
 #define MX8_URXD                    (0x00)
@@ -60,9 +59,9 @@
 #define RXBUF_SIZE 32
 
 
-// values read from MDI
+// values read from bootdata
 static bool initialized = false;
-static uint64_t uart_base = 0;
+static vaddr_t uart_base = 0;
 static uint32_t uart_irq = 0;
 static cbuf_t uart_rx_buf;
 // static cbuf_t uart_tx_buf;
@@ -197,7 +196,7 @@ static const struct pdev_uart_ops uart_ops = {
     .dputs = imx_dputs,
 };
 
-static void imx_uart_init(mdi_node_ref_t* node, uint level)
+static void imx_uart_init(const void* driver_data, uint32_t length)
 {
     uint32_t regVal;
 
@@ -243,35 +242,17 @@ static void imx_uart_init(mdi_node_ref_t* node, uint level)
     unmask_interrupt(uart_irq);
 }
 
-static void imx_uart_init_early(mdi_node_ref_t* node, uint level) {
-    uint64_t uart_base_phys = 0;
-    bool got_uart_base_phys = false;
-    bool got_uart_irq = false;
+static void imx_uart_init_early(const void* driver_data, uint32_t length) {
+    ASSERT(length >= sizeof(dcfg_simple_t));
+    const dcfg_simple_t* driver = driver_data;
+    ASSERT(driver->mmio_phys && driver->irq);
 
-    mdi_node_ref_t child;
-    mdi_each_child(node, &child) {
-        switch (mdi_id(&child)) {
-        case MDI_BASE_PHYS:
-            got_uart_base_phys = !mdi_node_uint64(&child, &uart_base_phys);
-            break;
-        case MDI_IRQ:
-            got_uart_irq = !mdi_node_uint32(&child, &uart_irq);
-            break;
-        }
-    }
-
-    if (!got_uart_base_phys) {
-        panic("imx uart: uart_base_phys not defined\n");
-    }
-    if (!got_uart_irq) {
-        panic("imx uart: uart_irq not defined\n");
-    }
-
-    uart_base = periph_paddr_to_vaddr(uart_base_phys);
+    uart_base = periph_paddr_to_vaddr(driver->mmio_phys);
     ASSERT(uart_base);
+    uart_irq = driver->irq;
 
     pdev_register_uart(&uart_ops);
 }
 
-LK_PDEV_INIT(imx_uart_init_early, MDI_ARM_NXP_IMX_UART, imx_uart_init_early, LK_INIT_LEVEL_PLATFORM_EARLY);
-LK_PDEV_INIT(imx_uart_init, MDI_ARM_NXP_IMX_UART, imx_uart_init, LK_INIT_LEVEL_PLATFORM);
+LK_PDEV_INIT(imx_uart_init_early, KDRV_NXP_IMX_UART, imx_uart_init_early, LK_INIT_LEVEL_PLATFORM_EARLY);
+LK_PDEV_INIT(imx_uart_init, KDRV_NXP_IMX_UART, imx_uart_init, LK_INIT_LEVEL_PLATFORM);

kernel/target/arm64/board/hikey960/boot-shim-config.h
@@ -57,3 +57,54 @@ static const bootdata_mem_range_t mem_config[] = {
         .length = 0x40000,
     },
 };
+
+static const dcfg_simple_t uart_driver = {
+    .mmio_phys = 0xfff32000,
+    .irq = 111,
+};
+
+static const dcfg_arm_gicv2_driver_t gicv2_driver = {
+    .mmio_phys = 0xe82b0000,
+    .gicd_offset = 0x1000,
+    .gicc_offset = 0x2000,
+    .gich_offset = 0x4000,
+    .gicv_offset = 0x6000,
+    .ipi_base = 13,
+};
+
+static const dcfg_arm_psci_driver_t psci_driver = {
+    .use_hvc = false,
+};
+
+static const dcfg_arm_generic_timer_driver_t timer_driver = {
+    .irq_phys = 30,
+    .irq_virt = 27,
+};
+
+static const dcfg_hisilicon_power_driver_t power_driver = {
+    .sctrl_phys = 0xfff0a000,
+    .pmu_phys = 0xfff34000,
+};
+
+static void append_board_bootdata(bootdata_t* bootdata) {
+    // add CPU configuration
+    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, 0, &cpu_config,
+                    sizeof(bootdata_cpu_config_t) +
+                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
+
+    // add memory configuration
+    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_config,
+                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+
+    // add kernel drivers
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_PL011_UART, &uart_driver,
+                    sizeof(uart_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V2, &gicv2_driver,
+                    sizeof(gicv2_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_PSCI, &psci_driver,
+                    sizeof(psci_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER, &timer_driver,
+                    sizeof(timer_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_HISILICON_POWER, &power_driver,
+                    sizeof(power_driver));
+}

kernel/target/arm64/board/qemu/boot-shim-config.h
@@ -25,3 +25,59 @@ static const bootdata_mem_range_t mem_config[] = {
         .length = 0x40000000,
     },
 };
+
+static const dcfg_simple_t uart_driver = {
+    .mmio_phys = 0x09000000,
+    .irq = 33,
+};
+
+static const dcfg_arm_gicv3_driver_t gicv3_driver = {
+    .mmio_phys = 0x08000000,
+    .gicd_offset = 0x00000,
+    .gicr_offset = 0xa0000,
+    .gicr_stride = 0x20000,
+    .ipi_base = 13,
+    .optional = true,
+};
+
+static const dcfg_arm_gicv2_driver_t gicv2_driver = {
+    .mmio_phys = 0x08000000,
+    .msi_frame_phys = 0x08020000,
+    .gicd_offset = 0x00000,
+    .gicc_offset = 0x10000,
+    .ipi_base = 13,
+    .optional = true,
+    .use_msi = true,
+};
+
+static const dcfg_arm_psci_driver_t psci_driver = {
+    .use_hvc = true,
+};
+
+static const dcfg_arm_generic_timer_driver_t timer_driver = {
+    .irq_phys = 30,
+    .irq_virt = 27,
+};
+
+static void append_board_bootdata(bootdata_t* bootdata) {
+    // add CPU configuration
+    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, 0, &cpu_config,
+                    sizeof(bootdata_cpu_config_t) +
+                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
+
+    // add memory configuration
+    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_config,
+                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+
+    // add kernel drivers
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_PL011_UART, &uart_driver,
+                    sizeof(uart_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V3, &gicv3_driver,
+                    sizeof(gicv3_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V2, &gicv2_driver,
+                    sizeof(gicv2_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_PSCI, &psci_driver,
+                    sizeof(psci_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER, &timer_driver,
+                    sizeof(timer_driver));
+}

kernel/target/arm64/board/vim2/boot-shim-config.h
@@ -52,3 +52,56 @@ static const bootdata_mem_range_t mem_config[] = {
         .length = 0x9000000,
     },
 };
+
+static const dcfg_simple_t uart_driver = {
+    .mmio_phys = 0xc81004c0,
+    .irq = 225,
+};
+
+static const dcfg_arm_gicv2_driver_t gicv2_driver = {
+    .mmio_phys = 0xc4300000,
+    .gicd_offset = 0x1000,
+    .gicc_offset = 0x2000,
+    .gich_offset = 0x4000,
+    .gicv_offset = 0x6000,
+    .ipi_base = 5,
+};
+
+static const dcfg_arm_psci_driver_t psci_driver = {
+    .use_hvc = false,
+    .reboot_args = { 1, 0, 0 },
+    .reboot_bootloader_args = { 4, 0, 0 },
+};
+
+static const dcfg_arm_generic_timer_driver_t timer_driver = {
+    .irq_phys = 30,
+};
+
+static const dcfg_amlogic_hdcp_driver_t hdcp_driver = {
+    .preset_phys = 0xc1104000,
+    .hui_phys = 0xc883c000,
+    .hdmitx_phys = 0xc883a000,
+};
+
+static void append_board_bootdata(bootdata_t* bootdata) {
+    // add CPU configuration
+    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, 0, &cpu_config,
+                    sizeof(bootdata_cpu_config_t) +
+                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
+
+    // add memory configuration
+    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_config,
+                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+
+    // add kernel drivers
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_AMLOGIC_UART, &uart_driver,
+                    sizeof(uart_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V2, &gicv2_driver,
+                    sizeof(gicv2_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_PSCI, &psci_driver,
+                    sizeof(psci_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER, &timer_driver,
+                    sizeof(timer_driver));
+    append_bootdata(bootdata, BOOTDATA_KERNEL_DRIVER, KDRV_AMLOGIC_HDCP, &hdcp_driver,
+                    sizeof(hdcp_driver));
+}

kernel/target/arm64/boot-shim/boot-shim.c
@@ -9,9 +9,18 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <zircon/boot/bootdata.h>
+#include <zircon/boot/driver-config.h>
 
 // uncomment to dump device tree at boot
 // #define PRINT_DEVICE_TREE
+
+// used in boot-shim-config.h and in this file below
+static void append_bootdata(bootdata_t* container, uint32_t type, uint32_t extra,
+                            const void* payload, uint32_t length);
+
+// defined in boot-shim-config.h
+static void append_board_bootdata(bootdata_t* container);
 
 // Include board specific definitions
 #include "boot-shim-config.h"
@@ -103,13 +112,13 @@ static void read_device_tree(void* device_tree, device_tree_context_t* ctx) {
 }
 #endif // HAS_DEVICE_TREE
 
-static void append_bootdata(bootdata_t* container, uint32_t type, const void* payload,
-                            uint32_t length) {
+static void append_bootdata(bootdata_t* container, uint32_t type, uint32_t extra,
+                            const void* payload, uint32_t length) {
     bootdata_t* dest = (bootdata_t*)((uintptr_t)container + container->length + sizeof(bootdata_t));
 
     dest->type = type;
     dest->length = length;
-    dest->extra = 0;
+    dest->extra = extra;
     dest->flags = 0;
     dest->reserved0 = 0;
     dest->reserved1 = 0;
@@ -171,14 +180,8 @@ uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
     }
 #endif // HAS_DEVICE_TREE
 
-    // append cpu configuration from boot-shim-config.h
-    append_bootdata(bootdata, BOOTDATA_CPU_CONFIG, &cpu_config,
-                    sizeof(bootdata_cpu_config_t) +
-                    sizeof(bootdata_cpu_cluster_t) * cpu_config.cluster_count);
-
-    // append memory configuration from boot-shim-config.h
-    append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, &mem_config,
-                    sizeof(bootdata_mem_range_t) * countof(mem_config));
+    // add board specific bootdata
+    append_board_bootdata(bootdata);
 
 #if HAS_DEVICE_TREE
     // look for optional RAM size in device tree
@@ -192,14 +195,14 @@ uint64_t boot_shim(void* device_tree, zircon_kernel_t* kernel) {
         uart_puts("Setting RAM size device tree value: ");
         uart_print_hex(ctx.memory);
         uart_puts("\n");
-        append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, &mem_range, sizeof(mem_range));
+        append_bootdata(bootdata, BOOTDATA_MEM_CONFIG, 0, &mem_range, sizeof(mem_range));
     } else {
         uart_puts("RAM size not found in device tree\n");
     }
 
     // append kernel command line
     if (ctx.cmdline && ctx.cmdline_length) {
-        append_bootdata(bootdata, BOOTDATA_CMDLINE, ctx.cmdline, ctx.cmdline_length);
+        append_bootdata(bootdata, BOOTDATA_CMDLINE, 0, ctx.cmdline, ctx.cmdline_length);
     }
 #endif // HAS_DEVICE_TREE
 