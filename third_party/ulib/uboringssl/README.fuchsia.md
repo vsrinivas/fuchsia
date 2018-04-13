6ec7422777cc932e001271891fc68b67fe3393f7
Aaron Green <aarongreen@google.com>
2018-04-13 12:38:20 -0700

    [uboringssl] Align uboringssl with the //third_party/boringssl
    This CL moves the assembly files and updates rules.mk to be more
    consistent with //third_party/boringssl's layout.  This allows the
    scripts to check the subset and generate build files to be removed;
    these are handled by roll_boringssl.go.  The README has similarly been
    simplified.
    
    ZX-1991 #comment Adds the necessary changes
    
    Change-Id: Ifc09dd27cd90c5c5381182fcc4f27e5400b97d05

--- third_party/ulib/uboringssl/README.fuchsia.md
+++ third_party/ulib/uboringssl/README.fuchsia.md
@@ -7,45 +7,22 @@ cryptographic operations in the kernel.
 
 ## Changes
 
-Changes from the upstream files are limited to just three files:
-  * [cpu-aarch64-zircon.cpp]: Stubs out ARM 64 capability detection. See [ZX-1357].
-  * [err_data.c]: Auto-generated error data.
+Changes from the upstream files are limited to a single files:
   * [base.h]: BORINGSSL_NO_CXX and OPENSSL_NO_THREADS added to Fuchsia case.
 
 All other code is unchanged from BoringSSL.
 
 ## Updating
 
-1. This subset does not include tests.  Before updating from Fuchsia's [BoringSSL], maintainers
-should first ensure the unit tests in that [package] pass before updating the subset for Zircon.
-
-2. A [check-boringssl.go] script recognizes these changes and will identify any other changes
-introduce when updating BoringSSL.  Maintainers should port these differences until the script exits
-cleanly.
-
-3. Additionally, the changed files listed above should be manually inspected and copied or
-edited as needed.  Care should be taken to minimize the differences between files in uboringssl and
-BoringSSL.  Only the minimum number of changes necessary to limit pulling in excessive dependencies
-should be added.
-
-4. A [perlasm.sh] script produces the assembly files from the source tree's Perl scripts.
-This script should be re-run after the source tree has been updated.
-
-5. Finally, maintainers should update this file with the new [revision] of BoringSSL to
-provide an easy way to confirm uboringssl was checked.
+Use the [roll_boringssl.go] script in Fuchsia's BoringSSL [package].
 
 ## License
 
 All code under this directory is covered by the same [license] as BoringSSL.
 
 [BoringSSL]: https://fuchsia.googlesource.com/third_party/boringssl/+/master/README.md
-[cpu-aarch64-zircon.cpp]: crypto/cpu-aarch64-zircon.cpp
-[ZX-1357]: https://fuchsia.atlassian.net/browse/ZX-1357
-[err_data.c]: crypto/err/err_data.c
 [base.h]: include/openssl/base.h
 [package]: https://fuchsia.googlesource.com/garnet/+/master/packages/boringssl
-[check-boringssl.go]: scripts/check-boringssl.go
-[perlasm.sh]: scripts/perlasm.sh
 [license]: https://fuchsia.googlesource.com/third_party/boringssl/+/master/LICENSE
 
 [//]: # (UPDATE THE DIGEST WHEN ROLLING BORINGSSL)
--- third_party/ulib/uboringssl/rules.mk
+++ third_party/ulib/uboringssl/rules.mk
@@ -8,76 +8,87 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 SHARED_COMPILEFLAGS := \
     -fvisibility=hidden -Wno-unused-function -include $(LOCAL_DIR)/stack-note.S
 
+CRYPTO_DIR=$(LOCAL_DIR)/crypto
 SHARED_SRCS := \
-    $(LOCAL_DIR)/crypto/chacha/chacha.c \
-    $(LOCAL_DIR)/crypto/cipher_extra/e_aesgcmsiv.c \
-    $(LOCAL_DIR)/crypto/cpu-aarch64-zircon.cpp \
-    $(LOCAL_DIR)/crypto/cpu-intel.c \
-    $(LOCAL_DIR)/crypto/crypto.c \
-    $(LOCAL_DIR)/crypto/err/err.c \
-    $(LOCAL_DIR)/crypto/err/err_data.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/aes/aes.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/aes/mode_wrappers.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/cipher/aead.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/cipher/cipher.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/cipher/e_aes.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/digest/digest.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/digest/digests.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/hmac/hmac.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/md4/md4.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/md5/md5.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/modes/cbc.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/modes/cfb.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/modes/ctr.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/modes/gcm.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/modes/ofb.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/modes/polyval.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/rand/ctrdrbg.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/rand/rand.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/sha/sha1.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/sha/sha256.c \
-    $(LOCAL_DIR)/crypto/fipsmodule/sha/sha512.c \
-    $(LOCAL_DIR)/crypto/hkdf/hkdf.c \
-    $(LOCAL_DIR)/crypto/mem.c \
-    $(LOCAL_DIR)/crypto/rand_extra/forkunsafe.c \
-    $(LOCAL_DIR)/crypto/rand_extra/fuchsia.c \
-    $(LOCAL_DIR)/crypto/thread_none.c \
-    $(LOCAL_DIR)/decrepit/xts/xts.c \
+    $(CRYPTO_DIR)/chacha/chacha.c \
+    $(CRYPTO_DIR)/cipher_extra/e_aesgcmsiv.c \
+    $(CRYPTO_DIR)/cpu-aarch64-fuchsia.c \
+    $(CRYPTO_DIR)/cpu-arm.c \
+    $(CRYPTO_DIR)/cpu-intel.c \
+    $(CRYPTO_DIR)/crypto.c \
+    $(CRYPTO_DIR)/err/err.c \
+    $(CRYPTO_DIR)/fipsmodule/aes/aes.c \
+    $(CRYPTO_DIR)/fipsmodule/aes/mode_wrappers.c \
+    $(CRYPTO_DIR)/fipsmodule/cipher/aead.c \
+    $(CRYPTO_DIR)/fipsmodule/cipher/cipher.c \
+    $(CRYPTO_DIR)/fipsmodule/cipher/e_aes.c \
+    $(CRYPTO_DIR)/fipsmodule/digest/digest.c \
+    $(CRYPTO_DIR)/fipsmodule/digest/digests.c \
+    $(CRYPTO_DIR)/fipsmodule/hmac/hmac.c \
+    $(CRYPTO_DIR)/fipsmodule/md4/md4.c \
+    $(CRYPTO_DIR)/fipsmodule/md5/md5.c \
+    $(CRYPTO_DIR)/fipsmodule/modes/cbc.c \
+    $(CRYPTO_DIR)/fipsmodule/modes/cfb.c \
+    $(CRYPTO_DIR)/fipsmodule/modes/ctr.c \
+    $(CRYPTO_DIR)/fipsmodule/modes/gcm.c \
+    $(CRYPTO_DIR)/fipsmodule/modes/ofb.c \
+    $(CRYPTO_DIR)/fipsmodule/modes/polyval.c \
+    $(CRYPTO_DIR)/fipsmodule/rand/ctrdrbg.c \
+    $(CRYPTO_DIR)/fipsmodule/rand/rand.c \
+    $(CRYPTO_DIR)/fipsmodule/sha/sha1.c \
+    $(CRYPTO_DIR)/fipsmodule/sha/sha256.c \
+    $(CRYPTO_DIR)/fipsmodule/sha/sha512.c \
+    $(CRYPTO_DIR)/hkdf/hkdf.c \
+    $(CRYPTO_DIR)/mem.c \
+    $(CRYPTO_DIR)/rand_extra/forkunsafe.c \
+    $(CRYPTO_DIR)/rand_extra/fuchsia.c \
+    $(CRYPTO_DIR)/thread_none.c \
+
+# TODO(aarongreen): Replace or get upstream to support more fully.
+DECREPIT_DIR=$(LOCAL_DIR)/decrepit
+SHARED_SRCS += \
+    $(DECREPIT_DIR)/xts/xts.c \
+
+# Auto-generated sources
+SHARED_SRCS += \
+    $(LOCAL_DIR)/err_data.c \
 
 ifeq ($(ARCH),arm64)
 # TODO(aarongreen): Workaround for the non-hidden OPENSSL_armcap_P symbol, which causes arm/clang to
 # fail to link.  Remove when resolved upstream.
 SHARED_COMPILEFLAGS += -DOPENSSL_NO_ASM
 
+ASM_DIR = $(LOCAL_DIR)/linux-aarch64/crypto
 SHARED_SRCS += \
-    $(LOCAL_DIR)/asm/aes-arm64.S \
-    $(LOCAL_DIR)/asm/chacha-arm64.S \
-    $(LOCAL_DIR)/asm/ghash-arm64.S \
-    $(LOCAL_DIR)/asm/sha1-arm64.S \
-    $(LOCAL_DIR)/asm/sha256-arm64.S \
-    $(LOCAL_DIR)/asm/sha512-arm64.S \
+    $(ASM_DIR)/chacha/chacha-armv8.S \
+    $(ASM_DIR)/fipsmodule/sha512-armv8.S \
+    $(ASM_DIR)/fipsmodule/ghashv8-armx64.S \
+    $(ASM_DIR)/fipsmodule/sha1-armv8.S \
+    $(ASM_DIR)/fipsmodule/sha256-armv8.S \
+    $(ASM_DIR)/fipsmodule/aesv8-armx64.S \
 
 else ifeq ($(ARCH),x86)
+
+ASM_DIR = $(LOCAL_DIR)/linux-x86_64/crypto
 SHARED_SRCS += \
-    $(LOCAL_DIR)/asm/aes128gcmsiv-x86-64.S \
-    $(LOCAL_DIR)/asm/aesnigcm-x86-64.S \
-    $(LOCAL_DIR)/asm/aesni-x86-64.S \
-    $(LOCAL_DIR)/asm/aes-x86-64.S \
-    $(LOCAL_DIR)/asm/bsaes-x86-64.S \
-    $(LOCAL_DIR)/asm/chacha-x86-64.S \
-    $(LOCAL_DIR)/asm/ghash-x86-64.S \
-    $(LOCAL_DIR)/asm/md5-x86-64.S \
-    $(LOCAL_DIR)/asm/rdrand-x86-64.S \
-    $(LOCAL_DIR)/asm/sha1-x86-64.S \
-    $(LOCAL_DIR)/asm/sha256-x86-64.S \
-    $(LOCAL_DIR)/asm/sha512-x86-64.S\
-    $(LOCAL_DIR)/asm/vpaes-x86-64.S \
+    $(ASM_DIR)/cipher_extra/aes128gcmsiv-x86_64.S \
+    $(ASM_DIR)/chacha/chacha-x86_64.S \
+    $(ASM_DIR)/fipsmodule/sha256-x86_64.S \
+    $(ASM_DIR)/fipsmodule/ghash-x86_64.S \
+    $(ASM_DIR)/fipsmodule/bsaes-x86_64.S \
+    $(ASM_DIR)/fipsmodule/aesni-x86_64.S \
+    $(ASM_DIR)/fipsmodule/sha1-x86_64.S \
+    $(ASM_DIR)/fipsmodule/vpaes-x86_64.S \
+    $(ASM_DIR)/fipsmodule/md5-x86_64.S \
+    $(ASM_DIR)/fipsmodule/aes-x86_64.S \
+    $(ASM_DIR)/fipsmodule/sha512-x86_64.S \
+    $(ASM_DIR)/fipsmodule/aesni-gcm-x86_64.S \
+    $(ASM_DIR)/fipsmodule/rdrand-x86_64.S \
 
 else
 $(error Unsupported architecture)
 
 endif
-
 
 # userlib
 MODULE := $(LOCAL_DIR)
