6c618702a44f879ca33fbb3d5bf6589f567e2257
Jeff Brown <jeffbrown@google.com>
2018-04-13 18:35:13 -0700

    [trace] Fix trace-engine event loop.
    Simplify the logic to handle hard shutdown of the dispatcher
    and fix a bug which caused the trace tests to be flaky.
    
    Bug: ZX-1989
    Change-Id: I64335a642f0c9d7457759241c38c95c7a05f1536

--- system/ulib/trace-engine/engine.cpp
+++ system/ulib/trace-engine/engine.cpp
@@ -8,12 +8,12 @@
 
 #include <zircon/assert.h>
 
-#include <lib/async/cpp/wait.h>
-#include <lib/zx/event.h>
 #include <fbl/atomic.h>
 #include <fbl/auto_lock.h>
 #include <fbl/mutex.h>
 #include <fbl/vector.h>
+#include <lib/async/cpp/wait.h>
+#include <lib/zx/event.h>
 #include <trace-engine/instrumentation.h>
 
 #include "context_impl.h"
@@ -216,40 +216,6 @@ zx_status_t trace_stop_engine(zx_status_t disposition) {
 
 namespace {
 
-// Handle status == ZX_ERR_CANCELED passed to handle_event().
-// Returns true if processing can continue (all holders of the trace context
-// have released it), false if not.
-// Upon a return of false callers are expected to shut down the trace engine.
-
-bool handle_async_dispatcher_shutdown() {
-    // Stop the engine, in case it hasn't noticed yet.
-    trace_stop_engine(ZX_ERR_CANCELED);
-
-    // There may still be outstanding references to the trace context.
-    // We don't know when or whether they will be cleared but we can't complete
-    // shut down until they are gone since there might still be live pointers
-    // into the trace buffer so allow a brief timeout.  If the release event
-    // hasn't been signaled by then, declare the trace engine dead in the water
-    // to prevent dangling pointers.  This situations should be very rare as it
-    // only occurs when the asynchronous dispatcher is shutting down, typically
-    // just prior to process exit.
-    auto status = g_event.wait_one(
-        SIGNAL_CONTEXT_RELEASED,
-        zx::deadline_after(kSynchronousShutdownTimeout),
-        nullptr);
-    if (status != ZX_OK) {
-        // Uh oh.
-        printf("Timed out waiting for %u trace context references to be released "
-               "after %lu ns while the asynchronous dispatcher was shutting down.\n"
-               "Tracing will no longer be available in this process.",
-               g_context_refs.load(fbl::memory_order_relaxed),
-               kSynchronousShutdownTimeout.get());
-        return false;
-    }
-
-    return true;
-}
-
 void handle_all_observers_started() {
     // TODO(TO-530): Allow indicating an observer failed to start.
 
@@ -298,9 +264,38 @@ void handle_context_released(async_t* async) {
 
     // Notify the handler about the final disposition.
     handler->ops->trace_stopped(handler, async, disposition, buffer_bytes_written);
+}
 
-    // Note: There's no need to clear SIGNAL_CONTEXT_RELEASED as we're about
-    // to shutdown.
+// Handles the case where the asynchronous dispatcher has encountered an error
+// and will no longer be servicing the wait callback.  Consequently, this is
+// our last chance to stop the engine and await for all contexts to be released.
+void handle_hard_shutdown(async_t* async) {
+    // Stop the engine, in case it hasn't noticed yet.
+    trace_stop_engine(ZX_ERR_CANCELED);
+
+    // There may still be outstanding references to the trace context.
+    // We don't know when or whether they will be cleared but we can't complete
+    // shut down until they are gone since there might still be live pointers
+    // into the trace buffer so allow a brief timeout.  If the release event
+    // hasn't been signaled by then, declare the trace engine dead in the water
+    // to prevent dangling pointers.  This situations should be very rare as it
+    // only occurs when the asynchronous dispatcher is shutting down, typically
+    // just prior to process exit.
+    auto status = g_event.wait_one(
+        SIGNAL_CONTEXT_RELEASED,
+        zx::deadline_after(kSynchronousShutdownTimeout),
+        nullptr);
+    if (status == ZX_OK) {
+        handle_context_released(async);
+        return;
+    }
+
+    // Uh oh.
+    printf("Timed out waiting for %u trace context references to be released "
+           "after %lu ns while the asynchronous dispatcher was shutting down.\n"
+           "Tracing will no longer be available in this process.",
+           g_context_refs.load(fbl::memory_order_relaxed),
+           kSynchronousShutdownTimeout.get());
 }
 
 void handle_event(async_t* async, async_wait_t* wait,
@@ -308,38 +303,19 @@ void handle_event(async_t* async, async_wait_t* wait,
     // Note: This function may get both SIGNAL_ALL_OBSERVERS_STARTED
     // and SIGNAL_CONTEXT_RELEASED at the same time.
 
-    // Assume we want to wait for the next event.
-    for (;;) {
-        bool wait_again = true;
-
-        // Handle the case where the asynchronous dispatcher is being shut down.
-        if (status != ZX_OK) {
-            ZX_DEBUG_ASSERT(status == ZX_ERR_CANCELED);
-            if (!handle_async_dispatcher_shutdown())
-                return;
-            // Still want to process SIGNAL_CONTEXT_RELEASED if present,
-            // so we don't return just yet.
-            wait_again = false;
-        }
-
-        // If async dispatcher is being shut down, ignore any started observers.
-        if (status == ZX_OK &&
-            (signal->observed & SIGNAL_ALL_OBSERVERS_STARTED)) {
+    if (status == ZX_OK) {
+        if (signal->observed & SIGNAL_ALL_OBSERVERS_STARTED) {
             handle_all_observers_started();
         }
-
-        // Also cleanup if async dispatcher is being shut down.
-        if (status != ZX_OK ||
-            (signal->observed & SIGNAL_CONTEXT_RELEASED)) {
+        if (signal->observed & SIGNAL_CONTEXT_RELEASED) {
             handle_context_released(async);
-            wait_again = false;
+            return; // trace engine is completely stopped now
         }
-
-        if (!wait_again)
-            return;
         status = async_begin_wait(async, &g_event_wait);
-        if (status == ZX_OK)
-            return;
+    }
+
+    if (status != ZX_OK) {
+        handle_hard_shutdown(async);
     }
 }
 
@@ -424,7 +400,7 @@ zx_status_t trace_register_observer(zx_handle_t event) {
             return ZX_ERR_INVALID_ARGS;
     }
 
-    g_observers.push_back(Observer{event,false});
+    g_observers.push_back(Observer{event, false});
     return ZX_OK;
 }
 
