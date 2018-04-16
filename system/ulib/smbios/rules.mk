ecc6575d6e08ffe3fb1579675a0d576219f43629
Todd Eisenberger <teisenbe@google.com>
2018-04-16 13:37:31 -0700

    [lib][smbios] Copy smbios code into library
    This is preparation for allowing usermode to discover device
    manufacturer/product information.
    
    ZX-2003 #comment Create libary
    
    Change-Id: I58d5b66bb8baa72943627b73ade61b67de999288

---
+++ system/ulib/smbios/rules.mk
@@ -0,0 +1,19 @@
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userlib
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/smbios.cpp \
+
+MODULE_LIBS := \
+	system/ulib/c \
+	system/ulib/fbl \
+	system/ulib/zircon \
+
+include make/module.mk
