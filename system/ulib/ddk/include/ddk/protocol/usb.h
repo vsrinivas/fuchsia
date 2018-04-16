77d28c159a48d030cc3c05c37a49d844dc905310
John Grossman <johngro@google.com>
2018-04-16 14:47:45 -0700

    [usb][audio] Add const flags to DDK's USB protocol.
    The DDK supplied USB protocol has no reason to demand that it be
    passed a mutable pointer to the usb_protocol_t structure in order to
    do it's job.  Reflect this by adding a const annotation to the
    structure pointer in each of the protocol defined functions.
    
    Change-Id: I072bc3f5b4fbc383053e22e87146831f60e44dc8

--- system/ulib/ddk/include/ddk/protocol/usb.h
+++ system/ulib/ddk/include/ddk/protocol/usb.h
@@ -43,71 +43,71 @@ typedef struct usb_protocol {
     void* ctx;
 } usb_protocol_t;
 
-static inline zx_status_t usb_req_alloc(usb_protocol_t* usb, usb_request_t** out,
+static inline zx_status_t usb_req_alloc(const usb_protocol_t* usb, usb_request_t** out,
                                         uint64_t data_size, uint8_t ep_address) {
     return usb->ops->req_alloc(usb->ctx, out, data_size, ep_address);
 }
 
-static inline zx_status_t usb_req_alloc_vmo(usb_protocol_t* usb, usb_request_t** out,
+static inline zx_status_t usb_req_alloc_vmo(const usb_protocol_t* usb, usb_request_t** out,
                                              zx_handle_t vmo_handle, uint64_t vmo_offset,
                                              uint64_t length, uint8_t ep_address) {
     return usb->ops->req_alloc_vmo(usb->ctx, out, vmo_handle, vmo_offset, length, ep_address);
 }
 
-static inline zx_status_t usb_req_init(usb_protocol_t* usb, usb_request_t* req,
+static inline zx_status_t usb_req_init(const usb_protocol_t* usb, usb_request_t* req,
                                        zx_handle_t vmo_handle, uint64_t vmo_offset,
                                        uint64_t length, uint8_t ep_address) {
     return usb->ops->req_init(usb->ctx, req, vmo_handle, vmo_offset, length, ep_address);
 }
 
 // synchronously executes a control request on endpoint zero
-static inline zx_status_t usb_control(usb_protocol_t* usb, uint8_t request_type, uint8_t request,
-                                      uint16_t value, uint16_t index, void* data, size_t length,
-                                      zx_time_t timeout, size_t* out_length) {
+static inline zx_status_t usb_control(const usb_protocol_t* usb, uint8_t request_type,
+                                      uint8_t request, uint16_t value, uint16_t index, void* data,
+                                      size_t length, zx_time_t timeout, size_t* out_length) {
     return usb->ops->control(usb->ctx, request_type, request, value, index, data, length, timeout,
                              out_length);
 }
 
-static inline zx_status_t usb_get_descriptor(usb_protocol_t* usb, uint8_t request_type,
+static inline zx_status_t usb_get_descriptor(const usb_protocol_t* usb, uint8_t request_type,
                                              uint16_t type, uint16_t index, void* data,
                                              size_t length, zx_time_t timeout, size_t* out_length) {
     return usb_control(usb, request_type | USB_DIR_IN, USB_REQ_GET_DESCRIPTOR,
                        (uint16_t)(type << 8 | index), 0, data, length, timeout, out_length);
 }
 
-static inline zx_status_t usb_get_status(usb_protocol_t* usb, uint8_t request_type, uint16_t index,
-                                         void* data, size_t length, zx_time_t timeout,
-                                         size_t* out_length) {
+static inline zx_status_t usb_get_status(const usb_protocol_t* usb, uint8_t request_type,
+                                         uint16_t index, void* data, size_t length,
+                                         zx_time_t timeout, size_t* out_length) {
     return usb_control(usb, request_type | USB_DIR_IN, USB_REQ_GET_STATUS, 0, index, data, length,
                        timeout, out_length);
 }
 
-static inline zx_status_t usb_set_feature(usb_protocol_t* usb, uint8_t request_type, uint16_t feature,
-                                          uint16_t index, zx_time_t timeout) {
+static inline zx_status_t usb_set_feature(const usb_protocol_t* usb, uint8_t request_type,
+                                          uint16_t feature, uint16_t index, zx_time_t timeout) {
     return usb_control(usb, request_type, USB_REQ_SET_FEATURE, feature, index, NULL, 0, timeout,
                        NULL);
 }
 
-static inline zx_status_t usb_clear_feature(usb_protocol_t* usb, uint8_t request_type, uint16_t feature,
-                                            uint16_t index, zx_time_t timeout) {
+static inline zx_status_t usb_clear_feature(const usb_protocol_t* usb, uint8_t request_type,
+                                            uint16_t feature, uint16_t index, zx_time_t timeout) {
     return usb_control(usb, request_type, USB_REQ_CLEAR_FEATURE, feature, index, NULL, 0, timeout,
                        NULL);
 }
 
-static inline void usb_request_queue(usb_protocol_t* usb, usb_request_t* usb_request) {
+static inline void usb_request_queue(const usb_protocol_t* usb, usb_request_t* usb_request) {
     return usb->ops->request_queue(usb->ctx, usb_request);
 }
 
-static inline usb_speed_t usb_get_speed(usb_protocol_t* usb) {
+static inline usb_speed_t usb_get_speed(const usb_protocol_t* usb) {
     return usb->ops->get_speed(usb->ctx);
 }
 
-static inline zx_status_t usb_set_interface(usb_protocol_t* usb, int interface_number,
+static inline zx_status_t usb_set_interface(const usb_protocol_t* usb, int interface_number,
                                             int alt_setting) {
     return usb->ops->set_interface(usb->ctx, interface_number, alt_setting);
 }
 
-static inline zx_status_t usb_set_configuration(usb_protocol_t* usb, int configuration) {
+static inline zx_status_t usb_set_configuration(const usb_protocol_t* usb, int configuration) {
     return usb->ops->set_configuration(usb->ctx, configuration);
 }
 
@@ -115,34 +115,34 @@ static inline zx_status_t usb_set_configuration(usb_protocol_t* usb, int configu
 // Endpoints will be halted if the device returns a STALL in response to a USB transaction.
 // When that occurs, the transaction will fail with ERR_IO_REFUSED.
 // usb_reset_endpoint() the endpoint to normal running state.
-static inline zx_status_t usb_reset_endpoint(usb_protocol_t* usb, uint8_t ep_address) {
+static inline zx_status_t usb_reset_endpoint(const usb_protocol_t* usb, uint8_t ep_address) {
     return usb->ops->reset_endpoint(usb->ctx, ep_address);
 }
 
 // returns the maximum amount of data that can be transferred on an endpoint in a single transaction.
-static inline size_t usb_get_max_transfer_size(usb_protocol_t* usb, uint8_t ep_address) {
+static inline size_t usb_get_max_transfer_size(const usb_protocol_t* usb, uint8_t ep_address) {
     return usb->ops->get_max_transfer_size(usb->ctx, ep_address);
 }
 
-static inline uint32_t usb_get_device_id(usb_protocol_t* usb) {
+static inline uint32_t usb_get_device_id(const usb_protocol_t* usb) {
     return usb->ops->get_device_id(usb->ctx);
 }
 
-static inline void usb_get_device_descriptor(usb_protocol_t* usb,
+static inline void usb_get_device_descriptor(const usb_protocol_t* usb,
                                              usb_device_descriptor_t* out_desc) {
     usb->ops->get_device_descriptor(usb->ctx, out_desc);
 }
 
 // returns the USB descriptors for the USB device or interface
 // the returned value is de-allocated with free()
-static inline zx_status_t usb_get_descriptor_list(usb_protocol_t* usb, void** out_descriptors,
+static inline zx_status_t usb_get_descriptor_list(const usb_protocol_t* usb, void** out_descriptors,
                                                   size_t* out_length) {
     return usb->ops->get_descriptor_list(usb->ctx, out_descriptors, out_length);
 }
 
 // returns the USB descriptors following the interface's existing descriptors
 // the returned value is de-allocated with free()
-static inline zx_status_t usb_get_additional_descriptor_list(usb_protocol_t* usb,
+static inline zx_status_t usb_get_additional_descriptor_list(const usb_protocol_t* usb,
                                                              void** out_descriptors,
                                                              size_t* out_length) {
     return usb->ops->get_additional_descriptor_list(usb->ctx, out_descriptors, out_length);
@@ -150,12 +150,12 @@ static inline zx_status_t usb_get_additional_descriptor_list(usb_protocol_t* usb
 
 // marks the interface as claimed and appends the interface descriptor to the
 // interface's existing descriptors.
-static inline zx_status_t usb_claim_interface(usb_protocol_t* usb,
+static inline zx_status_t usb_claim_interface(const usb_protocol_t* usb,
                                               usb_interface_descriptor_t* intf, size_t length) {
     return usb->ops->claim_interface(usb->ctx, intf, length);
 }
 
-static inline zx_status_t usb_cancel_all(usb_protocol_t* usb, uint8_t ep_address) {
+static inline zx_status_t usb_cancel_all(const usb_protocol_t* usb, uint8_t ep_address) {
     return usb->ops->cancel_all(usb->ctx, ep_address);
 }
 
