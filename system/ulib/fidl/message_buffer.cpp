42677229946b274c608e0bce674e9a6cf887ca8b
Gary Miguel <garymm@google.com>
2018-04-17 09:46:10 -0700

    [fidl] MessageBuffer: Avoid possible overflow and check malloc ret val.
    We promote the two 32-bit operands to 64 bits early in order to avoid overflow.
    The result was already being promoted to 64 bits.
    
    When malloc returns NULL, failing with a specific error makes debugging easier
    (vs seeing a segmentation fault later on when the buffer is used).
    
    Part of the security review of core FIDL 2 code.
    
    SEC-41 # comment: done
    
    Change-Id: Ic9d2c8ebafa111e427ac7f1e6fc923ad4fcd426b

--- system/ulib/fidl/message_buffer.cpp
+++ system/ulib/fidl/message_buffer.cpp
@@ -3,30 +3,31 @@
 // found in the LICENSE file.
 
 #include <lib/fidl/cpp/message_buffer.h>
+#include <zircon/assert.h>
 
 #include <stdlib.h>
 
 namespace fidl {
 namespace {
 
-uint32_t GetPadding(uint32_t offset) {
+uint64_t AddPadding(uint32_t offset) {
     constexpr uint32_t kMask = alignof(zx_handle_t) - 1;
-    return offset & kMask;
+    // Cast before addition to avoid overflow.
+    return static_cast<uint64_t>(offset) + static_cast<uint64_t>(offset & kMask);
 }
 
 size_t GetAllocSize(uint32_t bytes_capacity, uint32_t handles_capacity) {
-    return bytes_capacity + GetPadding(bytes_capacity) +
-        sizeof(zx_handle_t) * handles_capacity;
+    return AddPadding(bytes_capacity) + sizeof(zx_handle_t) * handles_capacity;
 }
 
 } // namespace
 
 MessageBuffer::MessageBuffer(uint32_t bytes_capacity,
                              uint32_t handles_capacity)
-    : buffer_(static_cast<uint8_t*>(
-          malloc(GetAllocSize(bytes_capacity, handles_capacity)))),
+    : buffer_(static_cast<uint8_t*>(malloc(GetAllocSize(bytes_capacity, handles_capacity)))),
       bytes_capacity_(bytes_capacity),
       handles_capacity_(handles_capacity) {
+    ZX_ASSERT_MSG(buffer_, "malloc returned NULL in MessageBuffer::MessageBuffer()");
 }
 
 MessageBuffer::~MessageBuffer() {
@@ -34,8 +35,7 @@ MessageBuffer::~MessageBuffer() {
 }
 
 zx_handle_t* MessageBuffer::handles() const {
-    return reinterpret_cast<zx_handle_t*>(
-        buffer_ + bytes_capacity_ + GetPadding(bytes_capacity_));
+    return reinterpret_cast<zx_handle_t*>(buffer_ + AddPadding(bytes_capacity_));
 }
 
 Message MessageBuffer::CreateEmptyMessage() {
