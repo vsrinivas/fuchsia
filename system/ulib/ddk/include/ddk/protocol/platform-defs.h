ff8c8edebf4f7b7abc5a692418a97dd8c62ecea4
Craig Stout <cstout@google.com>
2018-04-17 14:58:59 -0700

    [ddk] Add PDEV_DID_GPU_VSL_GC
    Verisilicon GC series GPU.
    
    Change-Id: I99df74ab7f2c4b0b85c62b37c3fd3bc1c1fa0250

--- system/ulib/ddk/include/ddk/protocol/platform-defs.h
+++ system/ulib/ddk/include/ddk/protocol/platform-defs.h
@@ -22,6 +22,7 @@ __BEGIN_CDECLS;
 #define PDEV_DID_DW_I2C             9   // Designware I2C
 #define PDEV_DID_DW_PCIE            10  // Designware PCIe
 #define PDEV_DID_LED2472G           11  // RPi Sense Hat LED2472G
+#define PDEV_DID_GPU_VSL_GC         12  // Verisilicon GC series GPU
 
 // QEMU emulator
 #define PDEV_VID_QEMU               1
