1309b195f4f361335156c6a39212d24fe7c4368e
Roland McGrath <mcgrathr@google.com>
2018-04-18 10:42:32 -0700

    [host][merkleroot] Support @rspfile arguments, manifest output format
    With -m MANIFEST instead of -o OUTPUT, write the output in
    manifest file format, i.e. `MERKLEROOT=FILENAME` lines.
    
    Change-Id: Ifd13e67651cfca365e6eeb034976f950356bbbbc

--- system/host/merkleroot/merkleroot.cpp
+++ system/host/merkleroot/merkleroot.cpp
@@ -15,14 +15,106 @@
 #include <digest/digest.h>
 #include <digest/merkle-tree.h>
 #include <fbl/alloc_checker.h>
+#include <fbl/string.h>
 #include <fbl/unique_ptr.h>
+#include <fbl/vector.h>
 
 using digest::Digest;
 using digest::MerkleTree;
 
 static void usage(char** argv) {
-    fprintf(stderr, "Usage: %s [-o OUTPUT] FILE...\n", argv[0]);
+    fprintf(stderr, "Usage: %s [-o OUTPUT | -m MANIFEST] FILE...\n", argv[0]);
+    fprintf(stderr, "\n\
+With -o, OUTPUT gets the same format normally written to stdout: HASH - FILE.\n\
+With -m, MANIFEST gets \"manifest file\" format: HASH=FILE.\n\
+Any argument may be \"@RSPFILE\" to be replaced with the contents of RSPFILE.\n\
+");
     exit(1);
+}
+
+static int handle_argument(char** argv, FILE* outf, const char* separator,
+                           const char* arg) {
+    if (arg[0] == '@') {
+        FILE* rspfile = fopen(&arg[1], "r");
+        if (!rspfile) {
+            perror(&arg[1]);
+            return 1;
+        }
+        while (!feof(rspfile) && !ferror(rspfile)) {
+            char* filename = nullptr;
+            if (fscanf(rspfile, " %ms", &filename) == 1) {
+                handle_argument(argv, outf, separator, filename);
+                free(filename);
+            }
+        }
+        int result = ferror(rspfile);
+        if (result) {
+            perror(&arg[1]);
+        }
+        fclose(rspfile);
+        return result;
+    }
+
+    // Buffer one intermediate node's worth at a time.
+    fbl::unique_ptr<uint8_t[]> tree;
+    char strbuf[Digest::kLength * 2 + 1];
+    Digest digest;
+    struct stat info;
+    if (stat(arg, &info) < 0) {
+        perror("stat");
+        fprintf(stderr, "[-] Unable to stat '%s'.\n", arg);
+        fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    if (!S_ISREG(info.st_mode)) {
+        return 0;
+    }
+    size_t len = MerkleTree::GetTreeLength(info.st_size);
+    fbl::AllocChecker ac;
+    tree.reset(new (&ac) uint8_t[len]);
+    if (!ac.check()) {
+        fprintf(stderr, "[-] Failed to allocate tree of %zu bytes.\n", len);
+        return 1;
+    }
+    int fd = open(arg, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        fprintf(stderr, "[-] Failed to open '%s.\n", arg);
+        return 1;
+    }
+    void* data = nullptr;
+    if (info.st_size != 0) {
+        data = mmap(NULL, info.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    }
+    if (close(fd) < 0) {
+        perror("close");
+        fprintf(stderr, "[-] Failed to close '%s'\n", arg);
+        return 1;
+    }
+    if (info.st_size != 0 && data == MAP_FAILED) {
+        perror("mmap");
+        fprintf(stderr, "[-] Failed to mmap '%s.\n", arg);
+        return 1;
+    }
+    zx_status_t rc =
+        MerkleTree::Create(data, info.st_size, tree.get(), len, &digest);
+    if (info.st_size != 0 && munmap(data, info.st_size) != 0) {
+        perror("munmap");
+        fprintf(stderr, "[-] Failed to munmap '%s.\n", arg);
+        return 1;
+    }
+    if (rc != ZX_OK) {
+        fprintf(stderr, "[-] Merkle tree creation failed: %d\n", rc);
+        return 1;
+    }
+    rc = digest.ToString(strbuf, sizeof(strbuf));
+    if (rc != ZX_OK) {
+        fprintf(stderr, "[-] Unable to print Merkle tree root: %d\n", rc);
+        return 1;
+    }
+    fprintf(outf, "%s%s%s\n", strbuf, separator, arg);
+
+    return 0;
 }
 
 int main(int argc, char** argv) {
@@ -30,8 +122,10 @@ int main(int argc, char** argv) {
     if (argc < 2) {
         usage(argv);
     }
+
     int argi = 1;
-    if (!strcmp(argv[1], "-o")) {
+    bool manifest = !strcmp(argv[1], "-m");
+    if (manifest || !strcmp(argv[1], "-o")) {
         if (argc < 4) {
             usage(argv);
         }
@@ -42,68 +136,11 @@ int main(int argc, char** argv) {
             return 1;
         }
     }
+    const char* separator = manifest ? "=" : " - ";
 
-    // Buffer one intermediate node's worth at a time.
-    struct stat info;
-    fbl::AllocChecker ac;
-    void* data = nullptr;
-    fbl::unique_ptr<uint8_t[]> tree(nullptr);
-    char strbuf[Digest::kLength * 2 + 1];
-    Digest digest;
     for (; argi < argc; ++argi) {
-        const char* arg = argv[argi];
-        if (stat(arg, &info) < 0) {
-            perror("stat");
-            fprintf(stderr, "[-] Unable to stat '%s'.\n", arg);
-            fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+        if (handle_argument(argv, outf, separator, argv[argi]))
             return 1;
-        }
-        if (!S_ISREG(info.st_mode)) {
-            continue;
-        }
-        size_t len = MerkleTree::GetTreeLength(info.st_size);
-        tree.reset(new (&ac) uint8_t[len]);
-        if (!ac.check()) {
-            fprintf(stderr, "[-] Failed to allocate tree of %zu bytes.\n", len);
-            return 1;
-        }
-        int fd = open(arg, O_RDONLY);
-        if (fd < 0) {
-            perror("open");
-            fprintf(stderr, "[-] Failed to open '%s.\n", arg);
-            return 1;
-        }
-        if (info.st_size != 0) {
-            data = mmap(NULL, info.st_size, PROT_READ, MAP_SHARED, fd, 0);
-        }
-        if (close(fd) < 0) {
-            perror("close");
-            fprintf(stderr, "[-] Failed to close '%s'\n", arg);
-            return 1;
-        }
-        if (info.st_size == 0 && data == MAP_FAILED) {
-            perror("mmap");
-            fprintf(stderr, "[-] Failed to mmap '%s.\n", arg);
-            return 1;
-        }
-        zx_status_t rc =
-            MerkleTree::Create(data, info.st_size, tree.get(), len, &digest);
-        if (info.st_size != 0 && munmap(data, info.st_size) != 0) {
-            perror("munmap");
-            fprintf(stderr, "[-] Failed to munmap '%s.\n", arg);
-            return 1;
-        }
-        if (rc != ZX_OK) {
-            fprintf(stderr, "[-] Merkle tree creation failed: %d\n", rc);
-            return 1;
-        }
-        rc = digest.ToString(strbuf, sizeof(strbuf));
-        if (rc != ZX_OK) {
-            fprintf(stderr, "[-] Unable to print Merkle tree root: %d\n", rc);
-            return 1;
-        }
-        fprintf(outf, "%s - %s\n", strbuf, arg);
     }
-
     return 0;
 }
