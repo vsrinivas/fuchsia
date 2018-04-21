7ff003d67632755b41a4b65fd7f274c5b94e1dca
Josh Gargus <jjosh@google.com>
2018-04-20 18:01:11 -0700

    [escher] Add a generic ObjectPool.
    This pool continguously allocates objects from blocks of memory.
    It supports both constructing/destroying objects as they are
    allocated/freed, and also when whole blocks of memory are
    allocated/freed.  The latter approach is useful for managing
    Vulkan resources such as descriptor sets, which must be allocated
    in batches.
    
    SCN-691 #done
    
    Change-Id: I7a0a80982e251b4d822c128a3d623e28ebff16b8

---
+++ public/lib/escher/util/object_pool.h
@@ -0,0 +1,133 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "lib/fxl/logging.h"
+
+namespace escher {
+
+// Default policy for constructing and destroying ObjectPool objects.  Each
+// object is constructed and destroyed one-by-one.
+template <typename T>
+class DefaultObjectPoolPolicy {
+ public:
+  // Default construction policy is to use placement-new.
+  template <typename... Args>
+  inline void InitializePoolObject(T* ptr, Args&&... args) {
+    new (ptr) T(std::forward<Args>(args)...);
+  }
+
+  // Default destruction policy is to invoke the destructor in-place.
+  inline void DestroyPoolObject(T* ptr) { ptr->~T(); }
+
+  // Default block initialization policy is to do nothing; each object is
+  // constructed one-by-one via InitializePoolObject().
+  inline void InitializePoolObjectBlock(T* objects,
+                                        size_t block_index,
+                                        size_t num_objects) {}
+
+  // Default block destruction policy is to do nothing; each object is destroyed
+  // one-by-one via DestroyPoolObject().
+  inline void DestroyPoolObjectBlock(T* objects,
+                                     size_t block_index,
+                                     size_t num_objects) {}
+};
+
+// An ObjectPool is an allocator for objects of type T.  The underlying memory
+// is allocated in contiguous chunks.  The default policy is to construct the
+// objects as they are allocated (via InitializePoolObject()) and destroy them
+// as they are freed (via DestroyPoolObject()).  However, some objects such as
+// Vulkan descriptor sets must be allocated in batches.  For these cases, the
+// ObjectPool can be parameterized with a different PolicyT object.
+template <typename T, typename PolicyT = DefaultObjectPoolPolicy<T>>
+class ObjectPool {
+ public:
+  template <typename... Args>
+  ObjectPool(Args&&... args) : policy_(std::forward<Args>(args)...) {}
+  ~ObjectPool() { Clear(); }
+
+  // Allocate an object from the pool, constructing it with the specified
+  // arguments.
+  template <typename... Args>
+  T* Allocate(Args&&... args) {
+    if (vacants_.empty()) {
+      AllocateBlock();
+    }
+    T* ptr = vacants_.back();
+    vacants_.pop_back();
+
+    policy_.InitializePoolObject(ptr, std::forward<Args>(args)...);
+    return ptr;
+  }
+
+  // Free the object, releasing it back to the pool for subsequent re-use.
+  void Free(T* ptr) {
+    policy_.DestroyPoolObject(ptr);
+    vacants_.push_back(ptr);
+  }
+
+  // Return the number of objects that can be held in the initial block
+  // allocation.
+  static size_t InitialBlockSize() { return 64U; }
+
+  // Return the number of objects that can be held in the "block_index-th"
+  // allocation.  Each block is twice the size of the previous one.
+  static size_t NumObjectsInBlock(size_t block_index) {
+    return InitialBlockSize() << block_index;
+  }
+
+  // Return the number of objects that have been allocated but not freed.
+  size_t UnfreedObjectCount() const {
+    // Get the total count of all objects.
+    size_t num_objects = 0;
+    const size_t num_blocks = blocks_.size();
+    for (size_t i = 0; i < num_blocks; ++i) {
+      num_objects += NumObjectsInBlock(i);
+    }
+    return num_objects - vacants_.size();
+  }
+
+  // Release all pool resources.  Illegal to call while there are still unfreed
+  // objects.
+  void Clear() {
+    FXL_DCHECK(UnfreedObjectCount() == 0);
+
+    const size_t num_blocks = blocks_.size();
+    for (size_t i = 0; i < num_blocks; ++i) {
+      const size_t num_objects = NumObjectsInBlock(i);
+      policy_.DestroyPoolObjectBlock(blocks_[i].get(), i, num_objects);
+    }
+
+    vacants_.clear();
+    blocks_.clear();
+  }
+
+ private:
+  // Allocate a block of memory, twice as big a the previous one.
+  void AllocateBlock() {
+    const size_t block_index = blocks_.size();
+    const size_t num_objects = NumObjectsInBlock(block_index);
+    T* ptr = static_cast<T*>(malloc(num_objects * sizeof(T)));
+    blocks_.emplace_back(ptr);
+    policy_.InitializePoolObjectBlock(ptr, block_index, num_objects);
+    vacants_.reserve(vacants_.capacity() + num_objects);
+    for (size_t i = 0; i < num_objects; ++i) {
+      vacants_.push_back(ptr + i);
+    }
+  }
+
+  struct MallocDeleter {
+    void operator()(T* ptr) { ::free(ptr); }
+  };
+
+  PolicyT policy_;
+  std::vector<T*> vacants_;
+  std::vector<std::unique_ptr<T, MallocDeleter>> blocks_;
+};
+
+}  // namespace escher
