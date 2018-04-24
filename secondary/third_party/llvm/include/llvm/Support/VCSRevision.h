fad9ddad3fa109cced3b4abc6bdaa87b3dc2e2d4
Brett Wilson <brettw@chromium.org>
2018-04-24 10:49:59 -0700

    [llvm] Add LLVM build files.
    These build files are sufficient to compile the X86 disassembler on an
    x64 host machine.
    
    Change-Id: I0ec6b80f65cfade9c37f0d60f580769a3da2c663

---
+++ secondary/third_party/llvm/include/llvm/Support/VCSRevision.h
@@ -0,0 +1,5 @@
+/* When compiling in-LLVM tree, this file is populated by a revisioin from SVN
+ * that is defined as LLVM_REVISION. To support non-SVN cases, the code is
+ * designed to work if this value is not defined, as long as this file exists.
+ */
+#undef LLVM_REVISION
