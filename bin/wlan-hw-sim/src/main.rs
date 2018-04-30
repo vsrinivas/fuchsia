4ac99dcc24fd6e3a34ee1275ec8bddb6ae961e5a
Greg Bonik <gbonik@google.com>
2018-04-30 15:18:28 -0700

    [wlan] Generate fake beacons on channel 6 in wlan-hw-sim
    $ run wlan-hw-sim
    $ wlan scan
    => Should see a "fakenet" network in scan results
    
    NET-663
    
    Test: this code itself is part of future test infra
    Change-Id: I92f281077e945e75864e211a6d7e16e6dea4debb

--- bin/wlan-hw-sim/src/main.rs
+++ bin/wlan-hw-sim/src/main.rs
@@ -6,13 +6,16 @@
 extern crate byteorder;
 extern crate failure;
 extern crate fuchsia_async as async;
+extern crate fuchsia_zircon as zx;
 extern crate wlantap_client;
 extern crate fidl_wlan_device;
 extern crate fidl_wlantap;
 extern crate futures;
 
 use std::sync::{Arc, Mutex};
+use futures::prelude::*;
 use wlantap_client::{Wlantap, WlantapListener};
+use zx::prelude::*;
 
 mod mac_frames;
 
@@ -62,6 +65,7 @@ fn create_wlantap_config() -> fidl_wlantap::WlantapPhyConfig {
 
 struct State {
     current_channel: fidl_wlan_device::Channel,
+    frame_buf: Vec<u8>,
 }
 
 impl State {
@@ -72,6 +76,7 @@ impl State {
                 cbw: 0,
                 secondary80: 0
             },
+            frame_buf: vec![]
         }
     }
 }
@@ -89,12 +94,80 @@ impl WlantapListener for Listener {
     }
 }
 
+
+const CHANNEL: u8 = 6;
+const BSS_ID: [u8; 6] = [ 0x62, 0x73, 0x73, 0x62, 0x73, 0x73 ];
+
+fn send_beacon(frame_buf: &mut Vec<u8>, channel: &fidl_wlan_device::Channel,
+               proxy: &fidl_wlantap::WlantapPhyProxy)
+    -> Result<(), failure::Error>
+{
+    frame_buf.clear();
+    mac_frames::MacFrameWriter::<&mut Vec<u8>>::new(frame_buf)
+        .beacon(
+            &mac_frames::MgmtHeader{
+                frame_control: mac_frames::FrameControl(0), // will be filled automatically
+                duration: 0,
+                addr1: mac_frames::BROADCAST_ADDR.clone(),
+                addr2: BSS_ID.clone(),
+                addr3: BSS_ID.clone(),
+                seq_control: mac_frames::SeqControl {
+                    frag_num: 0,
+                    seq_num: 123
+                },
+                ht_control: None
+            },
+            &mac_frames::BeaconFields{
+                timestamp: 0,
+                beacon_interval: 100,
+                capability_info: 0,
+            })?
+        .ssid("fakenet".as_bytes())?
+        .supported_rates(&[0x82, 0x84, 0x8b, 0x0c, 0x12, 0x96, 0x18, 0x24])?
+        .dsss_parameter_set(CHANNEL)?;
+
+    let rx_info = &mut fidl_wlantap::WlanRxInfo {
+        rx_flags: 0,
+        valid_fields: 0,
+        phy: 0,
+        data_rate: 0,
+        chan: fidl_wlan_device::Channel { // TODO(FIDL-54): use clone()
+            primary: channel.primary,
+            cbw: channel.cbw,
+            secondary80: channel.secondary80
+        },
+        mcs: 0,
+        rssi: 0,
+        rcpi: 0,
+        snr: 0,
+    };
+    proxy.rx(&mut 0, frame_buf, rx_info)?;
+    Ok(())
+}
+
 fn main() -> Result<(), failure::Error> {
     let mut exec = async::Executor::new()?;
     let wlantap = Wlantap::open()?;
     let state = Arc::new(Mutex::new(State::new()));
     let listener = Listener{ state: state.clone() };
     let (proxy, server) = wlantap.create_phy(create_wlantap_config(), listener)?;
-    exec.run_singlethreaded(server)?;
+    let beacon_timer = async::Interval::new(102_400_000.nanos())?
+        .for_each(move |_| {
+            let state = &mut *state.lock().map_err(|e| {
+                eprintln!("beacon timer callback: Failed to lock mutex: {:?}", e);
+                zx::Status::INTERNAL
+            })?;
+            if state.current_channel.primary == CHANNEL {
+                eprintln!("sending beacon!");
+                send_beacon(&mut state.frame_buf, &state.current_channel, &proxy);
+            }
+            Ok(())
+        })
+        .map(|_| ())
+        .recover::<Never, _>(|e| eprintln!("error running beacon timer: {:?}", e));
+    exec.run_singlethreaded(
+        server
+            .recover(|e| eprintln!("error running wlantap event server: {:?}", e))
+            .join(beacon_timer));
     Ok(())
 }
