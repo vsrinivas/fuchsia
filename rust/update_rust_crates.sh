bef26c324d553bbc32a83bce0e605f5daa766ee4
Raph Levien <raph@google.com>
2018-04-30 15:38:15 -0700

    [rust] Use rustc from toolchain
    Apparently this script uses cargo from the toolchain but a locally
    installed rustc, and the skew was causing problems (I have 1.22,
    which is pretty old, doesn't support incremental). This patch uses
    both from the same source.
    
    Change-Id: Iba7426bf09e01bff54985b734e3fc4ec200f1ba9

--- rust/update_rust_crates.sh
+++ rust/update_rust_crates.sh
@@ -7,6 +7,7 @@ case "$(uname -s)" in
   Darwin*) OS=mac-x64;;
   *) echo "Error: unrecognized OS"; exit 1;;
 esac
+export RUSTC=$FUCHSIA_ROOT/buildtools/$OS/rust/bin/rustc
 (cd $FUCHSIA_ROOT; $FUCHSIA_ROOT/buildtools/$OS/rust/bin/cargo run \
   --manifest-path $FUCHSIA_ROOT/third_party/rust-mirrors/cargo-vendor/Cargo.toml \
   -- vendor --sync $FUCHSIA_ROOT/garnet/Cargo.toml $FUCHSIA_ROOT/third_party/rust-crates/vendor)
