4014bf01cba8563ee579023ac274218da0209fa0
Amanda Tait <atait@google.com>
2018-05-15 15:15:15 -0400

    [netstack] Add Message creation from byte buffers
    This change adds a public static method to Message which allows the
    instantiation of new Message values from byte buffers conforming to the
    DHCP protocol. The change also adds supporting helper methods,
    constants, and unit tests.
    
    test: passes unit tests
    
    NET-752 #progress
    
    Change-Id: Icec0384a759cce907352b60cf430004efe7af01f

--- bin/dhcpd/src/protocol.rs
+++ bin/dhcpd/src/protocol.rs
@@ -4,9 +4,25 @@
 #![deny(warnings)]
 #![allow(unused)] // TODO(atait): Remove once there are non-test clients
 
+extern crate byteorder;
+
 const IPV4_ADDR_LEN: usize = 4;
 const MAC_ADDR_LEN: usize = 6;
 
+const OP_IDX: usize = 0;
+const HTYPE_IDX: usize = 1;
+const HLEN_IDX: usize = 2;
+const HOPS_IDX: usize = 3;
+const XID_IDX: usize = 4;
+const SECS_IDX: usize = 8;
+const FLAGS_IDX: usize = 10;
+const CIADDR_IDX: usize = 12;
+const YIADDR_IDX: usize = 16;
+const SIADDR_IDX: usize = 20;
+const GIADDR_IDX: usize = 24;
+const CHADDR_IDX: usize = 28;
+const SNAME_IDX: usize = 44;
+const FILE_IDX: usize = 108;
 const OPTIONS_START_IDX: usize = 236;
 
 const ETHERNET_HTYPE: u8 = 1;
@@ -29,6 +45,7 @@ const THREE_BYTE_LEN: usize = 24;
 /// All fields in `Message` follow the naming conventions outlined in the RFC.
 /// Note that `Message` does not expose `htype`, `hlen`, or `hops` fields, as
 /// these fields are effectively constants.
+#[derive(Debug, PartialEq)]
 pub struct Message {
     pub op: OpCode,
     pub xid: u32,
@@ -54,7 +71,7 @@ pub struct Message {
 
 impl Message {
     /// Instantiates a new `Message` with default field values.
-    pub fn new() -> Message {
+    pub fn new() -> Self {
         let msg = Message {
             op: OpCode::BOOTREQUEST,
             xid: 0,
@@ -70,6 +87,70 @@ impl Message {
             options: Vec::new(),
         };
         msg
+    }
+
+    /// Instantiates a new `Message` from a byte buffer conforming to the DHCP
+    /// protocol as defined RFC 2131. Returns `None` if the buffer is malformed.
+    /// Any malformed configuration options will be skipped over, leaving only
+    /// well formed `ConfigOption`s in the final `Message`.
+    pub fn from_buffer(buf: &[u8]) -> Option<Self> {
+        use self::byteorder::{BigEndian, ByteOrder};
+
+        if buf.len() < OPTIONS_START_IDX {
+            return None;
+        }
+
+        let mut msg = Message::new();
+        let op = buf.get(OP_IDX)?;
+        msg.op = OpCode::from(*op)?;
+        msg.xid = BigEndian::read_u32(&buf[XID_IDX..SECS_IDX]);
+        msg.secs = BigEndian::read_u16(&buf[SECS_IDX..FLAGS_IDX]);
+        msg.bdcast_flag = buf[FLAGS_IDX] > 0;
+        Message::buf_into_addr(&buf[CIADDR_IDX..YIADDR_IDX], &mut msg.ciaddr);
+        Message::buf_into_addr(&buf[YIADDR_IDX..SIADDR_IDX], &mut msg.yiaddr);
+        Message::buf_into_addr(&buf[SIADDR_IDX..GIADDR_IDX], &mut msg.siaddr);
+        Message::buf_into_addr(&buf[GIADDR_IDX..CHADDR_IDX], &mut msg.giaddr);
+        Message::buf_into_addr(&buf[CHADDR_IDX..CHADDR_IDX + 6], &mut msg.chaddr);
+        msg.sname = Message::buf_to_msg_string(&buf[SNAME_IDX..FILE_IDX])?;
+        msg.file = Message::buf_to_msg_string(&buf[FILE_IDX..OPTIONS_START_IDX])?;
+        Message::buf_into_options(&buf[OPTIONS_START_IDX..], &mut msg.options);
+
+        Some(msg)
+    }
+
+    fn buf_into_addr(buf: &[u8], addr: &mut [u8]) {
+        addr.copy_from_slice(buf);
+    }
+
+    fn buf_to_msg_string(buf: &[u8]) -> Option<String> {
+        use std::str;
+        let maybe_string = str::from_utf8(buf);
+        match maybe_string.ok() {
+            Some(string) => Some(string.trim_right_matches('\x00').to_string()),
+            None => None,
+        }
+    }
+
+    fn buf_into_options(buf: &[u8], options: &mut Vec<ConfigOption>) {
+        let mut opt_idx = 0;
+        while opt_idx < buf.len() && buf[opt_idx] != END_OP {
+            let opt_code = buf[opt_idx];
+            let opt_len: usize = match opt_code {
+                0 | END_OP => 1, // fixed length option
+                _ => match buf.get(opt_idx + 1) {
+                    // variable length option
+                    Some(len) => (len + 2) as usize,
+                    None => 1 as usize,
+                },
+            };
+            let maybe_opt = ConfigOption::from_buffer(&buf[opt_idx..opt_idx + opt_len]);
+            opt_idx += opt_len;
+            let opt = match maybe_opt {
+                Some(opt) => opt,
+                None => continue,
+            };
+            options.push(opt);
+        }
     }
 
     /// Consumes the calling `Message` to serialize it into a buffer of bytes.
@@ -161,15 +242,104 @@ fn test_serialize_returns_correct_bytes() {
     assert_eq!(bytes[bytes.len() - 1], 255u8);
 }
 
+#[test]
+fn test_message_from_buffer_returns_correct_message() {
+    use std::string::ToString;
+
+    let mut buf = Vec::new();
+    buf.push(1u8);
+    buf.push(1u8);
+    buf.push(6u8);
+    buf.push(0u8);
+    buf.extend_from_slice(b"\x00\x00\x00\x2A");
+    buf.extend_from_slice(b"\x04\x00");
+    buf.extend_from_slice(b"\x00\x00");
+    buf.extend_from_slice(b"\x00\x00\x00\x00");
+    buf.extend_from_slice(b"\xC0\xA8\x01\x01");
+    buf.extend_from_slice(b"\x00\x00\x00\x00");
+    buf.extend_from_slice(b"\x00\x00\x00\x00");
+    buf.extend_from_slice(b"\x00\x00\x00\x00\x00\x00");
+    buf.extend_from_slice(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00");
+    buf.extend_from_slice(b"relay.example.com");
+    let mut old_len = buf.len();
+    let mut unused_bytes = SNAME_LEN - b"relay.example.com".len();
+    buf.resize(old_len + unused_bytes, 0u8);
+    buf.extend_from_slice(b"boot.img");
+    old_len = buf.len();
+    unused_bytes = FILE_LEN - b"boot.img".len();
+    buf.resize(old_len + unused_bytes, 0u8);
+    buf.extend_from_slice(b"\x01\x04\xFF\xFF\xFF\x00");
+    buf.extend_from_slice(b"\x00");
+    buf.extend_from_slice(b"\x00");
+    buf.extend_from_slice(b"\x0D\x02\xAA\xBB");
+    buf.extend_from_slice(b"\xFF");
+
+    let got = Message::from_buffer(&buf).unwrap();
+
+    let opt_want1 = ConfigOption {
+        code: 1,
+        value: vec![255, 255, 255, 0],
+    };
+    let opt_want2 = ConfigOption {
+        code: 0,
+        value: vec![],
+    };
+    let opt_want3 = ConfigOption {
+        code: 0,
+        value: vec![],
+    };
+    let opt_want4 = ConfigOption {
+        code: 13,
+        value: vec![0xAA, 0xBB],
+    };
+    let want = Message {
+        op: OpCode::BOOTREQUEST,
+        xid: 42,
+        secs: 1024,
+        bdcast_flag: false,
+        ciaddr: [0, 0, 0, 0],
+        yiaddr: [192, 168, 1, 1],
+        siaddr: [0, 0, 0, 0],
+        giaddr: [0, 0, 0, 0],
+        chaddr: [0, 0, 0, 0, 0, 0],
+        sname: "relay.example.com".to_string(),
+        file: "boot.img".to_string(),
+        options: vec![opt_want1, opt_want2, opt_want3, opt_want4],
+    };
+
+    assert_eq!(got, want);
+}
+
+#[test]
+fn test_message_from_too_short_buffer_returns_none() {
+    let buf = vec![0u8, 0u8, 0u8];
+
+    let got = Message::from_buffer(&buf);
+
+    assert_eq!(got, None);
+}
+
 /// A DHCP protocol op-code as defined in RFC 2131.
 ///
 /// `OpCode::BOOTREQUEST` should only appear in protocol messages from the
 /// client, and conversely `OpCode::BOOTREPLY` should only appear in messages
 /// from the server.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum OpCode {
     BOOTREQUEST = 1,
     BOOTREPLY = 2,
+}
+
+impl OpCode {
+    /// Instantiates an `OpCode` from its `u8` raw value. Returns None for an
+    /// invalid `OpCode` raw value.
+    pub fn from(val: u8) -> Option<Self> {
+        match val {
+            1 => Some(OpCode::BOOTREQUEST),
+            2 => Some(OpCode::BOOTREPLY),
+            _ => None,
+        }
+    }
 }
 
 /// A vendor extension/configuration option for DHCP protocol messages.
@@ -177,6 +347,7 @@ pub enum OpCode {
 /// `ConfigOption`s can be fixed or variable length per RFC 1533. When
 /// `value` is left empty, the `ConfigOption` will be treated as a fixed
 /// length field.
+#[derive(Debug, PartialEq)]
 pub struct ConfigOption {
     pub code: u8,
     pub value: Vec<u8>,
