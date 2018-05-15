1e50fbfe91b9004cbadba85f48a7ec881c42d99f
Doug Evans <dje@google.com>
2018-05-14 18:24:20 -0700

    [lib][debugger_utils] Watch for fxl verbosity flags in unittests
    Change-Id: I359f405a6cca10cb78d099fb1d5150b149de660c

--- lib/debugger_utils/run_all_unittests.cc
+++ lib/debugger_utils/run_all_unittests.cc
@@ -2,9 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/fxl/command_line.h>
+#include <lib/fxl/log_settings_command_line.h>
+
 #include "gtest/gtest.h"
 
+static void SetVerbosityFromArgv(int argc, char** argv) {
+  auto cl = fxl::CommandLineFromArgcArgv(argc, argv);
+  fxl::SetLogSettingsFromCommandLine(cl);
+}
+
 int main(int argc, char** argv) {
+  SetVerbosityFromArgv(argc, argv);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
