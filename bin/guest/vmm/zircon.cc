0f229cf58d32f66eea7bac990097dc7c622f7c11
Abdulla Kamar <abdulla@google.com>
2018-05-16 10:12:42 -0700

    [guest][vmm] Migrate to ZBI for Zircon bootloader
    PD-150 #done
    
    Change-Id: Ie4cfa1da6d2f36fae5f349f4ea3ae861cca72edb

--- bin/guest/vmm/zircon.cc
+++ bin/guest/vmm/zircon.cc
@@ -12,8 +12,8 @@
 
 #include <fbl/unique_fd.h>
 #include <zircon/assert.h>
-#include <zircon/boot/bootdata.h>
 #include <zircon/boot/driver-config.h>
+#include <zircon/boot/image.h>
 
 #include "garnet/bin/guest/vmm/kernel.h"
 #include "garnet/lib/machina/guest.h"
@@ -21,38 +21,38 @@
 #if __aarch64__
 static constexpr uintptr_t kKernelOffset = 0;
 
-static bootdata_mem_range_t mem_config[] = {
+static zbi_mem_range_t mem_config[] = {
     {
-        .type = BOOTDATA_MEM_RANGE_RAM,
+        .type = ZBI_MEM_RANGE_RAM,
         .paddr = 0,
         .length = 0x40000000,  // set to phys_mem.size()
     },
     {
-        .type = BOOTDATA_MEM_RANGE_PERIPHERAL,
+        .type = ZBI_MEM_RANGE_PERIPHERAL,
         .paddr = 0xe8100000,
         .length = 0x17f00000,
     },
     {
         // Reserved for RTC
-        .type = BOOTDATA_MEM_RANGE_RESERVED,
+        .type = ZBI_MEM_RANGE_RESERVED,
         .paddr = 0x09010000,
         .length = 0x1000,  // 4KB
     },
     {
         // Reserved for MMIO
-        .type = BOOTDATA_MEM_RANGE_RESERVED,
+        .type = ZBI_MEM_RANGE_RESERVED,
         .paddr = 0x06fe0000,
         .length = 0x1000000,  // 16MB
     },
     {
         // Reserved for ECAM
-        .type = BOOTDATA_MEM_RANGE_RESERVED,
+        .type = ZBI_MEM_RANGE_RESERVED,
         .paddr = 0x2e000000,
         .length = 0x1000000,  // 16MB
     },
 };
 
-static constexpr bootdata_platform_id_t kPlatformId = {
+static constexpr zbi_platform_id_t kPlatformId = {
     .vid = 3,  // PDEV_VID_GOOGLE
     .pid = 2,  // PDEV_PID_MACHINA
     .board_name = "machina",
@@ -96,30 +96,30 @@ static constexpr uintptr_t kKernelOffset = 0x100000;
 #include "garnet/lib/machina/arch/x86/e820.h"
 #endif
 
-static bool is_bootdata(const bootdata_t* header) {
-  return header->type == BOOTDATA_CONTAINER &&
-         header->length > sizeof(bootdata_t) &&
-         header->extra == BOOTDATA_MAGIC && header->flags & BOOTDATA_FLAG_V2 &&
-         header->magic == BOOTITEM_MAGIC;
+static bool is_zbi(const zbi_header_t* header) {
+  return header->type == ZBI_TYPE_CONTAINER &&
+         header->length > sizeof(zbi_header_t) &&
+         header->extra == ZBI_CONTAINER_MAGIC &&
+         header->flags & ZBI_FLAG_VERSION && header->magic == ZBI_ITEM_MAGIC;
 }
 
-static void set_bootdata(bootdata_t* header, uint32_t type, uint32_t extra,
-                         uint32_t len) {
+static void set_zbi(zbi_header_t* header, uint32_t type, uint32_t extra,
+                    uint32_t len) {
   // Guest memory is initially zeroed, so we skip fields that must be zero.
   header->type = type;
   header->length = len;
   header->extra = extra;
-  header->flags = BOOTDATA_FLAG_V2;
-  header->magic = BOOTITEM_MAGIC;
-  header->crc32 = BOOTITEM_NO_CRC32;
+  header->flags = ZBI_FLAG_VERSION;
+  header->magic = ZBI_ITEM_MAGIC;
+  header->crc32 = ZBI_ITEM_NO_CRC32;
 }
 
 static zx_status_t load_cmdline(const std::string& cmdline,
                                 const machina::PhysMem& phys_mem,
-                                const uintptr_t bootdata_off) {
-  auto container_hdr = phys_mem.as<bootdata_t>(bootdata_off);
+                                const uintptr_t zbi_off) {
+  auto container_hdr = phys_mem.as<zbi_header_t>(zbi_off);
   const uintptr_t data_off =
-      bootdata_off + sizeof(bootdata_t) + BOOTDATA_ALIGN(container_hdr->length);
+      zbi_off + sizeof(zbi_header_t) + ZBI_ALIGN(container_hdr->length);
 
   const size_t cmdline_len = cmdline.size() + 1;
   if (cmdline_len > UINT32_MAX || data_off + cmdline_len > phys_mem.size()) {
@@ -127,36 +127,35 @@ static zx_status_t load_cmdline(const std::string& cmdline,
     return ZX_ERR_OUT_OF_RANGE;
   }
 
-  auto cmdline_hdr = phys_mem.as<bootdata_t>(data_off);
-  set_bootdata(cmdline_hdr, BOOTDATA_CMDLINE, 0,
-               static_cast<uint32_t>(cmdline_len));
+  auto cmdline_hdr = phys_mem.as<zbi_header_t>(data_off);
+  set_zbi(cmdline_hdr, ZBI_TYPE_CMDLINE, 0, static_cast<uint32_t>(cmdline_len));
   memcpy(cmdline_hdr + 1, cmdline.c_str(), cmdline_len);
 
-  container_hdr->length += static_cast<uint32_t>(sizeof(bootdata_t)) +
-                           BOOTDATA_ALIGN(cmdline_hdr->length);
+  container_hdr->length += static_cast<uint32_t>(sizeof(zbi_header_t)) +
+                           ZBI_ALIGN(cmdline_hdr->length);
   return ZX_OK;
 }
 
 static zx_status_t load_bootfs(const int fd, const machina::PhysMem& phys_mem,
-                               const uintptr_t bootdata_off) {
-  bootdata_t ramdisk_hdr;
-  ssize_t ret = read(fd, &ramdisk_hdr, sizeof(bootdata_t));
-  if (ret != sizeof(bootdata_t)) {
+                               const uintptr_t zbi_off) {
+  zbi_header_t ramdisk_hdr;
+  ssize_t ret = read(fd, &ramdisk_hdr, sizeof(zbi_header_t));
+  if (ret != sizeof(zbi_header_t)) {
     FXL_LOG(ERROR) << "Failed to read BOOTFS image header";
     return ZX_ERR_IO;
   }
-  if (!is_bootdata(&ramdisk_hdr)) {
+  if (!is_zbi(&ramdisk_hdr)) {
     FXL_LOG(ERROR) << "Invalid BOOTFS image header";
     return ZX_ERR_IO_DATA_INTEGRITY;
   }
-  if (ramdisk_hdr.length > phys_mem.size() - bootdata_off) {
+  if (ramdisk_hdr.length > phys_mem.size() - zbi_off) {
     FXL_LOG(ERROR) << "BOOTFS image is too large";
     return ZX_ERR_OUT_OF_RANGE;
   }
 
-  auto container_hdr = phys_mem.as<bootdata_t>(bootdata_off);
+  auto container_hdr = phys_mem.as<zbi_header_t>(zbi_off);
   uintptr_t data_off =
-      bootdata_off + sizeof(bootdata_t) + BOOTDATA_ALIGN(container_hdr->length);
+      zbi_off + sizeof(zbi_header_t) + ZBI_ALIGN(container_hdr->length);
 
   ret = read(fd, phys_mem.as<void>(data_off, ramdisk_hdr.length),
              ramdisk_hdr.length);
@@ -165,132 +164,130 @@ static zx_status_t load_bootfs(const int fd, const machina::PhysMem& phys_mem,
     return ZX_ERR_IO;
   }
 
-  container_hdr->length += BOOTDATA_ALIGN(ramdisk_hdr.length) +
-                           static_cast<uint32_t>(sizeof(bootdata_t));
+  container_hdr->length += ZBI_ALIGN(ramdisk_hdr.length) +
+                           static_cast<uint32_t>(sizeof(zbi_header_t));
 
   return ZX_OK;
 }
 
-static zx_status_t create_bootdata(const machina::PhysMem& phys_mem,
-                                   uintptr_t bootdata_off, uint32_t num_cpus) {
-  if (BOOTDATA_ALIGN(bootdata_off) != bootdata_off) {
+static zx_status_t create_zbi(const machina::PhysMem& phys_mem,
+                              uintptr_t zbi_off, uint32_t num_cpus) {
+  if (ZBI_ALIGN(zbi_off) != zbi_off) {
     return ZX_ERR_INVALID_ARGS;
   }
 
 #if __aarch64__
-  // bootdata_cpu_config_t is variable length
-  uint8_t cpu_config[sizeof(bootdata_cpu_config_t) +
-                     sizeof(bootdata_cpu_cluster_t)] = {};
-  auto header = reinterpret_cast<bootdata_cpu_config_t*>(cpu_config);
+  // zbi_cpu_config_t is variable length
+  uint8_t cpu_config[sizeof(zbi_cpu_config_t) + sizeof(zbi_cpu_cluster_t)] = {};
+  auto header = reinterpret_cast<zbi_cpu_config_t*>(cpu_config);
   header->cluster_count = 1;
   header->clusters[0].cpu_count = num_cpus;
 
-  const size_t bootdata_len =
-      BOOTDATA_ALIGN(sizeof(cpu_config)) + sizeof(bootdata_t) +
-      BOOTDATA_ALIGN(sizeof(mem_config)) + sizeof(bootdata_t) +
-      BOOTDATA_ALIGN(sizeof(kPlatformId)) + sizeof(bootdata_t) +
-      BOOTDATA_ALIGN(sizeof(kUartDriver)) + sizeof(bootdata_t) +
-      BOOTDATA_ALIGN(sizeof(kGicV2Driver)) + sizeof(bootdata_t) +
-      BOOTDATA_ALIGN(sizeof(kGicV3Driver)) + sizeof(bootdata_t) +
-      BOOTDATA_ALIGN(sizeof(kPsciDriver)) + sizeof(bootdata_t) +
-      BOOTDATA_ALIGN(sizeof(kTimerDriver)) + sizeof(bootdata_t);
+  const size_t zbi_len =
+      ZBI_ALIGN(sizeof(cpu_config)) + sizeof(zbi_header_t) +
+      ZBI_ALIGN(sizeof(mem_config)) + sizeof(zbi_header_t) +
+      ZBI_ALIGN(sizeof(kPlatformId)) + sizeof(zbi_header_t) +
+      ZBI_ALIGN(sizeof(kUartDriver)) + sizeof(zbi_header_t) +
+      ZBI_ALIGN(sizeof(kGicV2Driver)) + sizeof(zbi_header_t) +
+      ZBI_ALIGN(sizeof(kGicV3Driver)) + sizeof(zbi_header_t) +
+      ZBI_ALIGN(sizeof(kPsciDriver)) + sizeof(zbi_header_t) +
+      ZBI_ALIGN(sizeof(kTimerDriver)) + sizeof(zbi_header_t);
 #elif __x86_64__
   const size_t e820_size = machina::e820_size(phys_mem.size());
-  const size_t bootdata_len = sizeof(bootdata_t) +
-                              BOOTDATA_ALIGN(sizeof(uint64_t)) +
-                              sizeof(bootdata_t) + BOOTDATA_ALIGN(e820_size);
+  const size_t zbi_len = sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(uint64_t)) +
+                         sizeof(zbi_header_t) + ZBI_ALIGN(e820_size);
 #endif
-  if (bootdata_off + bootdata_len + sizeof(bootdata_t) > phys_mem.size()) {
+  if (zbi_off + zbi_len + sizeof(zbi_header_t) > phys_mem.size()) {
     return ZX_ERR_BUFFER_TOO_SMALL;
   }
 
   // Bootdata container.
-  auto container_hdr = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(container_hdr, BOOTDATA_CONTAINER, BOOTDATA_MAGIC,
-               static_cast<uint32_t>(bootdata_len));
-  bootdata_off += sizeof(bootdata_t);
+  auto container_hdr = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(container_hdr, ZBI_TYPE_CONTAINER, ZBI_CONTAINER_MAGIC,
+          static_cast<uint32_t>(zbi_len));
+  zbi_off += sizeof(zbi_header_t);
 
 #if __aarch64__
   // CPU config
-  auto bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_CPU_CONFIG, 0, sizeof(cpu_config));
-  memcpy(bootdata_header + 1, &cpu_config, sizeof(cpu_config));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(cpu_config));
+  auto zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_CPU_CONFIG, 0, sizeof(cpu_config));
+  memcpy(zbi_header + 1, &cpu_config, sizeof(cpu_config));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(cpu_config));
 
   // Memory config
   mem_config[0].length = phys_mem.size();
-  bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_MEM_CONFIG, 0, sizeof(mem_config));
-  memcpy(bootdata_header + 1, &mem_config, sizeof(mem_config));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(mem_config));
+  zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_MEM_CONFIG, 0, sizeof(mem_config));
+  memcpy(zbi_header + 1, &mem_config, sizeof(mem_config));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(mem_config));
 
   // platform ID
-  bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_PLATFORM_ID, 0, sizeof(kPlatformId));
-  memcpy(bootdata_header + 1, &kPlatformId, sizeof(kPlatformId));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(kPlatformId));
+  zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_PLATFORM_ID, 0, sizeof(kPlatformId));
+  memcpy(zbi_header + 1, &kPlatformId, sizeof(kPlatformId));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(kPlatformId));
 
   // uart driver
-  bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_KERNEL_DRIVER, KDRV_PL011_UART,
-               sizeof(kUartDriver));
-  memcpy(bootdata_header + 1, &kUartDriver, sizeof(kUartDriver));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(kUartDriver));
+  zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_KERNEL_DRIVER, KDRV_PL011_UART,
+          sizeof(kUartDriver));
+  memcpy(zbi_header + 1, &kUartDriver, sizeof(kUartDriver));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(kUartDriver));
 
   // gicv2 driver
-  bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V2,
-               sizeof(kGicV2Driver));
-  memcpy(bootdata_header + 1, &kGicV2Driver, sizeof(kGicV2Driver));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(kGicV2Driver));
+  zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_KERNEL_DRIVER, KDRV_ARM_GIC_V2,
+          sizeof(kGicV2Driver));
+  memcpy(zbi_header + 1, &kGicV2Driver, sizeof(kGicV2Driver));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(kGicV2Driver));
 
   // gicv3 driver
-  bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GIC_V3,
-               sizeof(kGicV3Driver));
-  memcpy(bootdata_header + 1, &kGicV3Driver, sizeof(kGicV3Driver));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(kGicV3Driver));
+  zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_KERNEL_DRIVER, KDRV_ARM_GIC_V3,
+          sizeof(kGicV3Driver));
+  memcpy(zbi_header + 1, &kGicV3Driver, sizeof(kGicV3Driver));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(kGicV3Driver));
 
   // psci driver
-  bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_PSCI,
-               sizeof(kPsciDriver));
-  memcpy(bootdata_header + 1, &kPsciDriver, sizeof(kPsciDriver));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(kPsciDriver));
+  zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_KERNEL_DRIVER, KDRV_ARM_PSCI,
+          sizeof(kPsciDriver));
+  memcpy(zbi_header + 1, &kPsciDriver, sizeof(kPsciDriver));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(kPsciDriver));
 
   // timer driver
-  bootdata_header = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(bootdata_header, BOOTDATA_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER,
-               sizeof(kTimerDriver));
-  memcpy(bootdata_header + 1, &kTimerDriver, sizeof(kTimerDriver));
-  bootdata_off += sizeof(bootdata_t) + BOOTDATA_ALIGN(sizeof(kTimerDriver));
+  zbi_header = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(zbi_header, ZBI_TYPE_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER,
+          sizeof(kTimerDriver));
+  memcpy(zbi_header + 1, &kTimerDriver, sizeof(kTimerDriver));
+  zbi_off += sizeof(zbi_header_t) + ZBI_ALIGN(sizeof(kTimerDriver));
 
   return ZX_OK;
 #elif __x86_64__
   // ACPI root table pointer.
-  auto acpi_rsdp_hdr = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(acpi_rsdp_hdr, BOOTDATA_ACPI_RSDP, 0, sizeof(uint64_t));
-  bootdata_off += sizeof(bootdata_t);
-  *phys_mem.as<uint64_t>(bootdata_off) = machina::kAcpiOffset;
+  auto acpi_rsdp_hdr = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(acpi_rsdp_hdr, ZBI_TYPE_ACPI_RSDP, 0, sizeof(uint64_t));
+  zbi_off += sizeof(zbi_header_t);
+  *phys_mem.as<uint64_t>(zbi_off) = machina::kAcpiOffset;
 
   // E820 memory map.
-  bootdata_off += BOOTDATA_ALIGN(sizeof(uint64_t));
-  auto e820_table_hdr = phys_mem.as<bootdata_t>(bootdata_off);
-  set_bootdata(e820_table_hdr, BOOTDATA_E820_TABLE, 0,
-               static_cast<uint32_t>(e820_size));
-  bootdata_off += sizeof(bootdata_t);
-  return machina::create_e820(phys_mem, bootdata_off);
+  zbi_off += ZBI_ALIGN(sizeof(uint64_t));
+  auto e820_table_hdr = phys_mem.as<zbi_header_t>(zbi_off);
+  set_zbi(e820_table_hdr, ZBI_TYPE_E820_TABLE, 0,
+          static_cast<uint32_t>(e820_size));
+  zbi_off += sizeof(zbi_header_t);
+  return machina::create_e820(phys_mem, zbi_off);
 #endif
 }
 
-static zx_status_t read_bootdata(const machina::PhysMem& phys_mem,
-                                 uintptr_t* guest_ip) {
+static zx_status_t read_zbi(const machina::PhysMem& phys_mem,
+                            uintptr_t* guest_ip) {
   auto kernel_hdr = phys_mem.as<zircon_kernel_t>(kKernelOffset);
-  if (kernel_hdr->hdr_kernel.type != BOOTDATA_KERNEL) {
+  if (kernel_hdr->hdr_kernel.type != ZBI_TYPE_KERNEL) {
     FXL_LOG(ERROR) << "Invalid Zircon kernel header";
     return ZX_ERR_IO_DATA_INTEGRITY;
   }
-  *guest_ip = kernel_hdr->data_kernel.entry64;
+  *guest_ip = kernel_hdr->data_kernel.entry;
   return ZX_OK;
 }
 
@@ -302,13 +299,13 @@ zx_status_t setup_zircon(const GuestConfig cfg,
   if (status != ZX_OK) {
     return status;
   }
-  status = read_bootdata(phys_mem, guest_ip);
+  status = read_zbi(phys_mem, guest_ip);
   if (status != ZX_OK) {
     return status;
   }
 
   // Create the BOOTDATA container.
-  status = create_bootdata(phys_mem, kRamdiskOffset, cfg.num_cpus());
+  status = create_zbi(phys_mem, kRamdiskOffset, cfg.num_cpus());
   if (status != ZX_OK) {
     FXL_LOG(ERROR) << "Failed to create BOOTDATA";
     return status;
