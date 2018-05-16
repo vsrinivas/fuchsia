8d75fd4bdc1ddcf9642218955edfb2e1713cb766
Benjamin Lerman <qsr@chromium.org>
2018-05-16 18:49:15 +0200

    [scoped_tmpfs] Add an utility class to create a temporary tmpfs
    Change-Id: Iec05e6f608c4054c042832a70fa3e10364d7c2c9

---
+++ lib/scoped_tmpfs/scoped_tmpfs.cc
@@ -0,0 +1,43 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "peridot/lib/scoped_tmpfs/scoped_tmpfs.h"
+
+#include <fdio/util.h>
+#include <zircon/processargs.h>
+
+#include "lib/fxl/logging.h"
+
+namespace ledger {
+
+namespace {
+async_loop_config_t MakeConfig() {
+  async_loop_config_t result = kAsyncLoopConfigMakeDefault;
+  result.make_default_for_current_thread = false;
+  return result;
+}
+}  // namespace
+
+ScopedTmpFS::ScopedTmpFS() : config_(MakeConfig()), loop_(&config_) {
+  zx_status_t status = loop_.StartThread("tmpfs_thread");
+  FXL_CHECK(status == ZX_OK);
+  zx_handle_t root_handle;
+  status = memfs_create_filesystem(loop_.async(), &memfs_, &root_handle);
+  FXL_CHECK(status == ZX_OK);
+  uint32_t type = PA_FDIO_REMOTE;
+  int fd;
+  status = fdio_create_fd(&root_handle, &type, 1, &fd);
+  FXL_CHECK(status == ZX_OK);
+  root_fd_.reset(fd);
+}
+
+ScopedTmpFS::~ScopedTmpFS() {
+  root_fd_.reset();
+  completion_t unmounted;
+  memfs_free_filesystem(memfs_, &unmounted);
+  zx_status_t status = completion_wait(&unmounted, ZX_SEC(3));
+  FXL_DCHECK(status == ZX_OK);
+}
+
+}  // namespace ledger
