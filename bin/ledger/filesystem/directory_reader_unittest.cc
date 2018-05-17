1ffa34fc3bdf1e226fae7373b623d06465aeb7cf
Benjamin Lerman <qsr@chromium.org>
2018-05-17 02:42:35 +0200

    [filesystem] Name test file with the name of the implementation
    Change-Id: Ib51f296c0caa60c7bb0a842ca71fa347761014ee

--- bin/ledger/filesystem/directory_reader_unittest.cc
+++ bin/ledger/filesystem/directory_reader_unittest.cc
@@ -1,8 +1,8 @@
 // Copyright 2018 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include "peridot/bin/ledger/filesystem/directory_reader.h"
-#include "peridot/bin/ledger/filesystem/get_directory_content_size.h"
 
 #include <set>
 
@@ -10,17 +10,15 @@
 #include "lib/fxl/files/directory.h"
 #include "lib/fxl/files/file.h"
 #include "lib/fxl/files/scoped_temp_dir.h"
-#include "lib/gtest/test_with_loop.h"
 
 namespace ledger {
 namespace {
 
-using FilesystemHelpersTest = gtest::TestWithLoop;
-
 const std::string kFileContent = "file content";
 
-TEST_F(FilesystemHelpersTest, GetDirectoryEntries) {
+TEST(DirectoryReaderTest, GetDirectoryEntries) {
   files::ScopedTempDir temp_dir;
+
   ASSERT_TRUE(files::CreateDirectory(temp_dir.path() + "/foo"));
   ASSERT_TRUE(files::WriteFile(temp_dir.path() + "/bar", kFileContent.data(),
                                kFileContent.size()));
@@ -36,18 +34,6 @@ TEST_F(FilesystemHelpersTest, GetDirectoryEntries) {
         return true;
       }));
   EXPECT_EQ(expected_entries.size(), 0u);
-}
-
-TEST_F(FilesystemHelpersTest, GetDirectoryContentSize) {
-  files::ScopedTempDir temp_dir;
-  ASSERT_TRUE(files::CreateDirectory(temp_dir.path() + "/foo"));
-  ASSERT_TRUE(files::WriteFile(temp_dir.path() + "/bar", kFileContent.data(),
-                               kFileContent.size()));
-  ASSERT_TRUE(files::WriteFile(temp_dir.path() + "/foo/baz",
-                               kFileContent.data(), kFileContent.size()));
-  uint64_t directory_size = 0;
-  ASSERT_TRUE(GetDirectoryContentSize(temp_dir.path(), &directory_size));
-  ASSERT_EQ(directory_size, 2 * kFileContent.size());
 }
 
 }  // namespace
---
+++ bin/ledger/filesystem/get_directory_content_size_unittest.cc
@@ -0,0 +1,30 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "peridot/bin/ledger/filesystem/get_directory_content_size.h"
+
+#include "gtest/gtest.h"
+#include "lib/fxl/files/directory.h"
+#include "lib/fxl/files/file.h"
+#include "lib/fxl/files/scoped_temp_dir.h"
+
+namespace ledger {
+namespace {
+
+const std::string kFileContent = "file content";
+
+TEST(GetDirectoryContentSizeTest, GetDirectoryContentSize) {
+  files::ScopedTempDir temp_dir;
+  ASSERT_TRUE(files::CreateDirectory(temp_dir.path() + "/foo"));
+  ASSERT_TRUE(files::WriteFile(temp_dir.path() + "/bar", kFileContent.data(),
+                               kFileContent.size()));
+  ASSERT_TRUE(files::WriteFile(temp_dir.path() + "/foo/baz",
+                               kFileContent.data(), kFileContent.size()));
+  uint64_t directory_size = 0;
+  ASSERT_TRUE(GetDirectoryContentSize(temp_dir.path(), &directory_size));
+  ASSERT_EQ(directory_size, 2 * kFileContent.size());
+}
+
+}  // namespace
+}  // namespace ledger
