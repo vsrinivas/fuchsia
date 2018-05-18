156bb3703fc340ca96d3fb03229ef6bae4bbf521
John Grossman <johngro@google.com>
2018-05-18 10:55:21 -0700

    [ddk][pci] Add const specifiers to DDK PCI thunks.
    There is no reason for these thunks to need to modify the contents of the
    pci_protocol_t structure which is passed to them, they should take a const
    pci_protocol_t* instead of a mutable pointer.
    
    Change-Id: I5f60703518885eb25eb7c50778538a666afc8606

--- system/ulib/ddk/include/ddk/protocol/pci.h
+++ system/ulib/ddk/include/ddk/protocol/pci.h
@@ -74,91 +74,98 @@ typedef struct pci_protocol {
     void* ctx;
 } pci_protocol_t;
 
-static inline zx_status_t pci_get_bar(pci_protocol_t* pci, uint32_t res_id,
-                                           zx_pci_bar_t* out_info) {
+static inline zx_status_t pci_get_bar(const pci_protocol_t* pci, uint32_t res_id,
+                                      zx_pci_bar_t* out_info) {
     return pci->ops->get_bar(pci->ctx, res_id, out_info);
 }
 
-static inline zx_status_t pci_map_bar(pci_protocol_t* pci, uint32_t bar_id,
-                                           uint32_t cache_policy, void** vaddr, size_t* size,
-                                           zx_handle_t* out_handle) {
+static inline zx_status_t pci_map_bar(const pci_protocol_t* pci, uint32_t bar_id,
+                                      uint32_t cache_policy, void** vaddr, size_t* size,
+                                      zx_handle_t* out_handle) {
     return pci->ops->map_bar(pci->ctx, bar_id, cache_policy, vaddr, size, out_handle);
 }
 
-static inline zx_status_t pci_enable_bus_master(pci_protocol_t* pci, bool enable) {
+static inline zx_status_t pci_enable_bus_master(const pci_protocol_t* pci, bool enable) {
     return pci->ops->enable_bus_master(pci->ctx, enable);
 }
 
-static inline zx_status_t pci_get_bti(pci_protocol_t* pci, uint32_t index, zx_handle_t* bti) {
+static inline zx_status_t pci_get_bti(const pci_protocol_t* pci, uint32_t index, zx_handle_t* bti) {
     return pci->ops->get_bti(pci->ctx, index, bti);
 }
 
-static inline zx_status_t pci_reset_device(pci_protocol_t* pci) {
+static inline zx_status_t pci_reset_device(const pci_protocol_t* pci) {
     return pci->ops->reset_device(pci->ctx);
 }
 
-static inline zx_status_t pci_map_interrupt(pci_protocol_t* pci, int which_irq,
+static inline zx_status_t pci_map_interrupt(const pci_protocol_t* pci, int which_irq,
                                             zx_handle_t* out_handle) {
     return pci->ops->map_interrupt(pci->ctx, which_irq, out_handle);
 }
 
-static inline zx_status_t pci_query_irq_mode(pci_protocol_t* pci, zx_pci_irq_mode_t mode,
-                                                  uint32_t* out_max_irqs) {
+static inline zx_status_t pci_query_irq_mode(const pci_protocol_t* pci, zx_pci_irq_mode_t mode,
+                                             uint32_t* out_max_irqs) {
     return pci->ops->query_irq_mode(pci->ctx, mode, out_max_irqs);
 }
 
-static inline zx_status_t pci_set_irq_mode(pci_protocol_t* pci, zx_pci_irq_mode_t mode,
+static inline zx_status_t pci_set_irq_mode(const pci_protocol_t* pci, zx_pci_irq_mode_t mode,
                                            uint32_t requested_irq_count) {
     return pci->ops->set_irq_mode(pci->ctx, mode, requested_irq_count);
 }
 
-static inline zx_status_t pci_get_device_info(pci_protocol_t* pci,
+static inline zx_status_t pci_get_device_info(const pci_protocol_t* pci,
                                               zx_pcie_device_info_t* out_info) {
     return pci->ops->get_device_info(pci->ctx, out_info);
 }
 
-static inline zx_status_t pci_config_read8(pci_protocol_t* pci, uint16_t offset, uint8_t* value) {
+static inline zx_status_t pci_config_read8(const pci_protocol_t* pci,
+                                           uint16_t offset, uint8_t* value) {
     uint32_t value_;
     zx_status_t st = pci->ops->config_read(pci->ctx, offset, sizeof(uint8_t), &value_);
     *value = value_ & UINT8_MAX;
     return st;
 }
 
-static inline zx_status_t pci_config_read16(pci_protocol_t* pci, uint16_t offset, uint16_t* value) {
+static inline zx_status_t pci_config_read16(const pci_protocol_t* pci,
+                                            uint16_t offset, uint16_t* value) {
     uint32_t value_;
     zx_status_t st = pci->ops->config_read(pci->ctx, offset, sizeof(uint16_t), &value_);
     *value = value_ & UINT16_MAX;
     return st;
 }
 
-static inline zx_status_t pci_config_read32(pci_protocol_t* pci, uint16_t offset, uint32_t* value) {
+static inline zx_status_t pci_config_read32(const pci_protocol_t* pci,
+                                            uint16_t offset, uint32_t* value) {
     return pci->ops->config_read(pci->ctx, offset, sizeof(uint32_t), value);
 }
 
-static inline zx_status_t pci_config_write8(pci_protocol_t* pci, uint16_t offset, uint8_t value) {
+static inline zx_status_t pci_config_write8(const pci_protocol_t* pci,
+                                            uint16_t offset, uint8_t value) {
     return pci->ops->config_write(pci->ctx, offset, sizeof(uint8_t), value);
 }
 
-static inline zx_status_t pci_config_write16(pci_protocol_t* pci, uint16_t offset, uint16_t value) {
+static inline zx_status_t pci_config_write16(const pci_protocol_t* pci,
+                                             uint16_t offset, uint16_t value) {
     return pci->ops->config_write(pci->ctx, offset, sizeof(uint16_t), value);
 }
 
-static inline zx_status_t pci_config_write32(pci_protocol_t* pci, uint16_t offset, uint32_t value) {
+static inline zx_status_t pci_config_write32(const pci_protocol_t* pci,
+                                             uint16_t offset, uint32_t value) {
     return pci->ops->config_write(pci->ctx, offset, sizeof(uint32_t), value);
 }
 
-static inline uint8_t pci_get_next_capability(pci_protocol_t* pci, uint8_t type, uint8_t offset) {
+static inline uint8_t pci_get_next_capability(const pci_protocol_t* pci,
+                                              uint8_t type, uint8_t offset) {
     return pci->ops->get_next_capability(pci->ctx, type, offset);
 }
 
-static inline uint8_t pci_get_first_capability(pci_protocol_t* pci, uint8_t type) {
+static inline uint8_t pci_get_first_capability(const pci_protocol_t* pci, uint8_t type) {
     // the next_capability method will always look at the second byte next
     // pointer to fetch the next capability. By offsetting the CapPtr field
     // by -1 we can pretend we're working with a normal capability entry
     return pci_get_next_capability(pci, kPciCfgCapabilitiesPtr - 1u, type);
 }
 
-static inline zx_status_t pci_get_auxdata(pci_protocol_t* pci,
+static inline zx_status_t pci_get_auxdata(const pci_protocol_t* pci,
                                           const char* args, void* data,
                                           uint32_t bytes, uint32_t* actual) {
     return pci->ops->get_auxdata(pci->ctx, args, data, bytes, actual);
