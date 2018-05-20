7c69d08e1f57cdf651604bb2eacbb7be1ea9a15f
Josh Conner <joshconner@google.com>
2018-05-20 09:50:29 -0700

    [ath10k] Disable thermal
    Someday we may want to perform thermal throttling, but today is not
    that day.
    
    NET-137
    
    Test: compiles and runs on ath10k branch.
    Change-Id: I5c6c51c391593e0bd045c8b92168c47ae127936f

--- drivers/wlan/third_party/atheros/ath10k/thermal.h
+++ drivers/wlan/third_party/atheros/ath10k/thermal.h
@@ -36,7 +36,7 @@ struct ath10k_thermal {
     int temperature;
 };
 
-#if IS_REACHABLE(CONFIG_THERMAL)
+#if CONFIG_THERMAL
 int ath10k_thermal_register(struct ath10k* ar);
 void ath10k_thermal_unregister(struct ath10k* ar);
 void ath10k_thermal_event_temperature(struct ath10k* ar, int temperature);
