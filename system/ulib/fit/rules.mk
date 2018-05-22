69914ab37a1d276cbca9dfade77be306a5138ef5
Jeff Brown <jeffbrown@google.com>
2018-05-22 16:48:56 -0700

    [fit] Define fit::function.
    Introduce a new library, libfit, which is intended to contain a
    small number of abstractions that serve as a kind of "annex" to
    the C++ 14 standard library.
    
    This code has been derived from fbl::Function with the following
    changes:
    - adopt an API style similar to that of std::function
    - use hand-rolled vtables to improve code generation
    - add missing target() member function
    - drop AllocChecker overloads
    
    Added an empty file to make the linker happy, as was previously
    done for libfbl way back when it too was header-only.
    
    Change-Id: I6412a1827e34a9ac63444a9bc07c692ec52d763e

---
+++ system/ulib/fit/rules.mk
@@ -0,0 +1,33 @@
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+fit_srcs := \
+    $(LOCAL_DIR)/empty.c \
+
+#
+# Userspace library.
+#
+
+MODULE := $(LOCAL_DIR)
+MODULE_TYPE := userlib
+MODULE_PACKAGE := src
+MODULE_SRCS := $(fit_srcs)
+
+include make/module.mk
+
+#
+# Host library.
+#
+
+MODULE := $(LOCAL_DIR).hostlib
+MODULE_TYPE := hostlib
+MODULE_SRCS := $(fit_srcs)
+
+include make/module.mk
+
+# Clear local variables.
+
+fit_srcs :=
