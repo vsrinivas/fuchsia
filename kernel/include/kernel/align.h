dfe0cf32fa95bc887dc5213f715c55253b5f92b9
Travis Geiselbrecht <travisg@google.com>
2018-05-23 18:14:59 -0700

    [kernel][alignment] add a new attribute to mark certain hot structures as exlusive and cpu aligned
    Marking a certain variable or struct as requiring its own cache line
    helps reduce any false aliasing with any other nearby variables.
    
    Change-Id: Ia46bc7e8a208b32d7ef8402303d8b09abe02078b

--- kernel/include/kernel/align.h
+++ kernel/include/kernel/align.h
@@ -7,6 +7,11 @@
 #pragma once
 
 #include <arch/defines.h>
+#include <zircon/compiler.h>
 
 // Use to align structures on cache lines to avoid cpu aliasing.
 #define __CPU_ALIGN __ALIGNED(MAX_CACHE_LINE)
+
+// Similar to above, but put in a special section to make sure no other
+// variable shares the same cache line.
+#define __CPU_ALIGN_EXCLUSIVE __CPU_ALIGN __SECTION(".data.cpu_align_exclusive")
