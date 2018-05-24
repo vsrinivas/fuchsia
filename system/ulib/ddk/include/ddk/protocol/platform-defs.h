bc02c68f2ca4235476891645f8d1aeb0d54be2ff
Mohan Srinivasan <srmohan@google.com>
2018-05-24 15:34:33 -0700

    [amlogic][rawnand] Add Amlogic RAW_NAND controller DID (prepping for commiting the NAND drivers).
    Change-Id: I873d60a48ca245f4b04776ea8d7bdcfdc9477d44

--- system/ulib/ddk/include/ddk/protocol/platform-defs.h
+++ system/ulib/ddk/include/ddk/protocol/platform-defs.h
@@ -74,6 +74,7 @@ __BEGIN_CDECLS;
 #define PDEV_DID_AMLOGIC_SCPI       10
 #define PDEV_DID_AMLOGIC_DISPLAY    11
 #define PDEV_DID_AMLOGIC_VIDEO      12
+#define PDEV_DID_AMLOGIC_RAW_NAND   13
 
 // Broadcom
 #define PDEV_VID_BROADCOM           6
