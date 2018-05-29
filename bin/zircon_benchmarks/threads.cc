99c650b122cc84daf9741c71438df1b5347918c2
Mark Seaborn <mseaborn@google.com>
2018-05-29 11:29:31 -0700

    [benchmarks] Convert Thread/CreateAndJoin test to use the perftest library
    We drop the use of PauseTiming/ResumeTiming() around the
    zx_handle_close() calls.  The cost of those is trivial in comparison
    with the rest of the test, and we measure the cost of
    zx_handle_close() in other tests, so it's not worth making this into a
    multi-step test.
    
    TO-651
    
    Change-Id: I65c0aa4b3b802514cd44e924590fa27647dba610

--- bin/zircon_benchmarks/threads.cc
+++ bin/zircon_benchmarks/threads.cc
@@ -2,51 +2,43 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <benchmark/benchmark.h>
+#include <perftest/perftest.h>
 #include <zircon/process.h>
 #include <zircon/syscalls.h>
 
-class Thread : public benchmark::Fixture {};
+#include "lib/fxl/logging.h"
 
-BENCHMARK_F(Thread, CreateAndJoin)(benchmark::State& state) {
+namespace {
+
+bool ThreadCreateAndJoinTest() {
   zx_handle_t thread;
   constexpr char tname[] = "test thread";
-  while (state.KeepRunning()) {
-    if (zx_thread_create(zx_process_self(), tname, sizeof(tname), 0, &thread) != ZX_OK) {
-      state.SkipWithError("Failed to create thread");
-      return;
-    }
+  FXL_CHECK(zx_thread_create(zx_process_self(), tname, sizeof(tname), 0, &thread) == ZX_OK);
 
-    uint64_t stack_size = 16 * 1024u;
-    zx_handle_t stack_vmo;
-    if (zx_vmo_create(stack_size, 0, &stack_vmo) != ZX_OK) {
-      state.SkipWithError("Failed to create vmo");
-      return;
-    }
+  uint64_t stack_size = 16 * 1024u;
+  zx_handle_t stack_vmo;
+  FXL_CHECK(zx_vmo_create(stack_size, 0, &stack_vmo) == ZX_OK);
 
-    zx_vaddr_t stack_base;
-    uint32_t perm = ZX_VM_FLAG_PERM_READ | ZX_VM_FLAG_PERM_WRITE;
-    if (zx_vmar_map(zx_vmar_root_self(), 0, stack_vmo, 0, stack_size, perm, &stack_base) != ZX_OK) {
-      state.SkipWithError("Failed to map vmo");
-      return;
-    }
+  zx_vaddr_t stack_base;
+  uint32_t perm = ZX_VM_FLAG_PERM_READ | ZX_VM_FLAG_PERM_WRITE;
+  FXL_CHECK(zx_vmar_map(zx_vmar_root_self(), 0, stack_vmo, 0, stack_size, perm, &stack_base)
+            == ZX_OK);
 
-    uintptr_t entry = reinterpret_cast<uintptr_t>(&zx_thread_exit);
-    uintptr_t stack = reinterpret_cast<uintptr_t>(stack_base + stack_size);
-    if (zx_thread_start(thread, entry, stack, 0, 0) != ZX_OK) {
-      state.SkipWithError("Failed to start thread");
-      return;
-    }
+  uintptr_t entry = reinterpret_cast<uintptr_t>(&zx_thread_exit);
+  uintptr_t stack = reinterpret_cast<uintptr_t>(stack_base + stack_size);
+  FXL_CHECK(zx_thread_start(thread, entry, stack, 0, 0) == ZX_OK);
 
-    zx_signals_t observed;
-    if (zx_object_wait_one(thread, ZX_THREAD_TERMINATED, ZX_TIME_INFINITE, &observed) != ZX_OK) {
-      state.SkipWithError("Failed to wait for thread");
-      return;
-    }
+  zx_signals_t observed;
+  FXL_CHECK(zx_object_wait_one(thread, ZX_THREAD_TERMINATED, ZX_TIME_INFINITE, &observed) == ZX_OK);
 
-    state.PauseTiming();
-    zx_handle_close(thread);
-    zx_handle_close(stack_vmo);
-    state.ResumeTiming();
-  }
+  zx_handle_close(thread);
+  zx_handle_close(stack_vmo);
+  return true;
+}
+
+void RegisterTests() {
+  perftest::RegisterSimpleTest<ThreadCreateAndJoinTest>("Thread/CreateAndJoin");
+}
+PERFTEST_CTOR(RegisterTests);
+
 }
