84cb192c15d01a6e3470f6e10ad76116fef79494
James Tucker <raggi@google.com>
2018-05-29 18:41:09 -0700

    [images] all scripts use build gen'd image paths
    This should avoid future breakage due to builds moving images around.
    
    BLD-167 #done
    
    Change-Id: I2393603c23eb62c84bfd7ba043f0075dfa70c4cc

--- devshell/lib/image_build_vars.sh
+++ devshell/lib/image_build_vars.sh
@@ -11,42 +11,16 @@ fx-machine-types() {
   echo "  netboot - alias for ram"
   echo "  bootfs  - ramboot with /system from bootfs (deprecated)"
   echo "  zedboot - ramboot zedboot into zedboot"
-  echo "  efi     - pave an EFI device (e.g. nuc)"
-  echo "  cros    - pave a cros device (e.g. pixelbook)"
+  echo "  efi     - pave an EFI device (e.g. acer,nuc)"
+  echo "  cros    - pave a cros device (e.g. pixelbook,chromebook)"
   echo "Machine type aliases:"
   echo "  acer"
   echo "  nuc"
   echo "  pixel"
   echo "  vboot"
-  echo "Available board types for ${FUCHSIA_ARCH}:"
-  cat "${board_list_file}"
 	echo
   echo "Note: currently efi,cros,acer,nuc,pixel and vboot all have the exact"
   echo "same behavior and target specific differences occur client side."
 }
 
-# TODO Remove ${board} variable after build system builds common images
-case "${FUCHSIA_ARCH}" in
-x64)
-  board="pc"
-  ;;
-arm64)
-  board="qemu"
-  ;;
-*)
-  echo >&2 "Unknown zircon board for arch: \"$FUCHSIA_ARCH\""
-  exit 1
-esac
-
-board_list_file="${FUCHSIA_BUILD_DIR}/zircon-boards.list"
-
-zircon_bin="zircon.bin"
-ramdisk_bin="bootdata-blob-${board}.bin"
-
-images_dir="images"
-cmdline_txt="${images_dir}/cmdline.txt"
-efi_block="${images_dir}/local-${board}.esp.blk"
-fvm_block="${images_dir}/fvm.blk"
-fvm_sparse_block="${images_dir}/fvm.sparse.blk"
-fvm_data_sparse_block="${images_dir}/fvm.data.sparse.blk"
-kernc_vboot="${images_dir}/zircon-${board}.vboot"
+source "${FUCHSIA_BUILD_DIR}"/{,zedboot_}image_paths.sh
