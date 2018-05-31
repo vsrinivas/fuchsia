ece705cc94933a4a94fb3194feec3ebd8a13e3f7
Joshua Seaton <joshuaseaton@google.com>
2018-05-31 08:24:36 -0700

    [build] Populate host tests in $(BUILDDIR)/host_tests
    With a recent change of mcgrathr@'s host tests in Garnet+ are now
    populated in //out/<arch>/host_tests:
    https://fuchsia-review.googlesource.com/c/build/+/156373
    This change is for parity in the naming.
    
    BUG: IN-357
    Change-Id: I92cb5e761ce5cc5301bacf88169b29fd523bf5c0

--- make/module-hosttest.mk
+++ make/module-hosttest.mk
@@ -6,6 +6,6 @@
 
 # tests only differ from apps by their install location
 
-MODULE_HOSTAPP_BIN := $(BUILDDIR)/tests/$(MODULE_NAME)
+MODULE_HOSTAPP_BIN := $(BUILDDIR)/host_tests/$(MODULE_NAME)
 
 include make/module-hostapp.mk
