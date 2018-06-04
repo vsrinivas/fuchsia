ba3560dfb3f3cb331dc9b8cea25afe16ce112ce0
Ankur Mittal <anmittal@google.com>
2018-06-04 13:13:37 -0700

    [appmgr] CP-41: Integration test for "ls /hub".
    Change-Id: Ic336b8b8238da1665dca0ad839e7d84f60a1ad42

---
+++ bin/appmgr/integration_tests/hub_integration_test.cc
@@ -0,0 +1,34 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <glob.h>
+
+#include "gtest/gtest.h"
+#include "lib/fxl/strings/string_printf.h"
+
+namespace fuchsia {
+namespace sys {
+namespace {
+
+TEST(ProbeHub, Component) {
+  auto glob_str = fxl::StringPrintf("/hub/c/sysmgr/*/out/debug");
+  glob_t globbuf;
+  ASSERT_EQ(glob(glob_str.data(), 0, NULL, &globbuf), 0)
+      << glob_str << " does not exist.";
+  ASSERT_EQ(globbuf.gl_pathc, 1u);
+  globfree(&globbuf);
+}
+
+TEST(ProbeHub, Realm) {
+  auto glob_str = fxl::StringPrintf("/hub/r/sys/*/c/");
+  glob_t globbuf;
+  ASSERT_EQ(glob(glob_str.data(), 0, NULL, &globbuf), 0)
+      << glob_str << " does not exist.";
+  ASSERT_EQ(globbuf.gl_pathc, 1u);
+  globfree(&globbuf);
+}
+
+}  // namespace
+}  // namespace sys
+}  // namespace fuchsia
