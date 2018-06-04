de731d858590f177755f2f0f028b3ed957101416
Garratt <garratt@google.com>
2018-06-04 10:25:51 -0700

    [sysmem] System memory buffer API.
    The first use-case for this abstraction is to enable image data to
    be efficiently transferred between the CPU, GPU, video codecs, display
    controller, camera service, and other components of the graphics
    pipeline, taking hardware constraints into account.
    
    Rather than directly allocating memory, clients contact a
    system memory allocator service and submit their plea for buffers
    along with a description of how they intend to use those buffers.
    When allocating shared buffers, the system memory allocator
    takes care of coordinate the requirements among all clients to
    optimize memory transfer between subsystems.
    
    This CL was moved from Garnet into Zircon.  The previous
    version was at garnet/+/155698.
    
    TEST= This is just the interface.  The tests will be part of the server
    stub.
    
    Change-Id: If4e328dfd82b7a1e9f0e13c28eb34e8b6f4e2a47

---
+++ system/fidl/sysmem/rules.mk
@@ -0,0 +1,22 @@
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := fidl
+
+MODULE_PACKAGE := fidl
+
+MODULE_FIDL_LIBRARY := fuchsia.sysmem
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/allocator.fidl \
+	$(LOCAL_DIR)/collections.fidl \
+	$(LOCAL_DIR)/formats.fidl \
+	$(LOCAL_DIR)/image_formats.fidl \
+	$(LOCAL_DIR)/usages.fidl
+
+include make/module.mk
