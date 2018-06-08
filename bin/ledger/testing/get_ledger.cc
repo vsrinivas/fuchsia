52807a69cfd1a0f9445e6e3da5223a5dad79d3fa
Adam Barth <abarth@google.com>
2018-06-08 09:47:54 -0700

    [fidl] Prepare for WaitForResponse deprecation
    Now that FIDL supports events, WaitForResponse and WaitForResponseUntil
    are footguns because the next message that comes out of the channel
    might be an event rather than a response to the message you just sent.
    
    We're going to remove these methods from InterfacePtr. Clients should
    migrate to InterfaceSyncPtr or, in the case of tests, to spinning their
    async::Loop until the proper message arrives (or UntilIdle).
    
    This CL introduces an #ifdef that whitelists the existing clients. Once
    we work through migrating the clients to more robust APIs, we'll remove
    the methods.
    
    Change-Id: I7e9313d0bf66ef480ebc1ecfa366be8ea23be78e

--- bin/ledger/testing/get_ledger.cc
+++ bin/ledger/testing/get_ledger.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#define FIDL_ENABLE_LEGACY_WAIT_FOR_RESPONSE
+
 #include "peridot/bin/ledger/testing/get_ledger.h"
 
 #include <fuchsia/ledger/cloud/firebase/cpp/fidl.h>
--- bin/ledger/tests/benchmark/update_entry/update_entry.cc
+++ bin/ledger/tests/benchmark/update_entry/update_entry.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#define FIDL_ENABLE_LEGACY_WAIT_FOR_RESPONSE
+
 #include "peridot/bin/ledger/tests/benchmark/update_entry/update_entry.h"
 
 #include <lib/zx/time.h>
