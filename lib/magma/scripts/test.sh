fe5a310676d622d70bf009caced70d7dfdc42579
Craig Stout <cstout@google.com>
2018-06-12 13:23:37 -0700

    [magma] Use /tmp instead of /data
    Change-Id: I7729144211945a179e4cf44a03b002e6b98d324d

--- lib/magma/scripts/test.sh
+++ lib/magma/scripts/test.sh
@@ -9,11 +9,11 @@ fuchsia_root=`pwd`
 netaddr=$fuchsia_root/out/build-zircon/tools/netaddr
 test_out=/tmp/magma_test_out
 
-scripts/fx shell 'rm -rf /data/magma_test_out; export GTEST_OUTPUT=xml:/data/magma_test_out/ && . /system/bin/magma_autorun'
+scripts/fx shell 'rm -rf /tmp/magma_test_out; export GTEST_OUTPUT=xml:/tmp/magma_test_out/ && . /system/bin/magma_autorun'
 
 rm -rf $test_out
 mkdir $test_out
-scripts/fx scp [`$netaddr --fuchsia`]:/data/magma_test_out/*.xml $test_out
+scripts/fx scp [`$netaddr --fuchsia`]:/tmp/magma_test_out/*.xml $test_out
 
 echo "Grepping for failures:"
 grep failures $test_out/* | grep -v 'failures=\"0\"'
