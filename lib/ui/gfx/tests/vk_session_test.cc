241a63d9693708f53e156b68f4eb9bd373ba99d6
Josh Gargus <jjosh@google.com>
2018-06-14 10:54:48 -0700

    [escher] Fix CQ tests that require Vulkan.
    The bot "garnet-x64-debug-swift_canyon" appears to support Vulkan
    but not presentation of Vulkan swapchains.  For some reason this
    started to cause tests to fail (maybe this bot just came online?)
    
    Since none of our tests should require presentation, this CL
    modifies VkSessionTest::CreateEngine to create a VkDevice without
    presentation support.
    
    SCN-779 #done
    INTK-396 #comment
    
    Change-Id: If019306c59afc775b623b8dbee96e7e8f74d2f68

--- lib/ui/gfx/tests/vk_session_test.cc
+++ lib/ui/gfx/tests/vk_session_test.cc
@@ -12,21 +12,14 @@ namespace test {
 std::unique_ptr<Engine> VkSessionTest::CreateEngine() {
   // Initialize Vulkan.
   escher::VulkanInstance::Params instance_params(
-      {{},
-       {VK_EXT_DEBUG_REPORT_EXTENSION_NAME, VK_KHR_SURFACE_EXTENSION_NAME,
-        VK_KHR_MAGMA_SURFACE_EXTENSION_NAME},
-       true});
+      {{}, {VK_EXT_DEBUG_REPORT_EXTENSION_NAME}, false});
 
-// Only enable Vulkan validation layers when in debug mode.
-#if !defined(NDEBUG)
   instance_params.layer_names.insert("VK_LAYER_LUNARG_standard_validation");
-#endif
   auto vulkan_instance =
       escher::VulkanInstance::New(std::move(instance_params));
-  auto surface = CreateVulkanMagmaSurface(vulkan_instance->vk_instance());
   auto vulkan_device = escher::VulkanDeviceQueues::New(
       vulkan_instance,
-      {{VK_KHR_EXTERNAL_SEMAPHORE_FUCHSIA_EXTENSION_NAME}, surface});
+      {{VK_KHR_EXTERNAL_SEMAPHORE_FUCHSIA_EXTENSION_NAME}, vk::SurfaceKHR()});
 
   escher_ = std::make_unique<escher::Escher>(vulkan_device);
 
