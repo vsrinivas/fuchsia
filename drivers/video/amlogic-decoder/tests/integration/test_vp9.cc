48830f3e703d0228bd0f27ca949d100311562299
John Bauman <jbauman@google.com>
2018-06-21 15:44:51 -0700

    [amlogic-video] Parse IVF files in VP9 test
    Each frame in an IVF file may actually be a superframe that contains
    multiple real frames. So we need to parse the individual frames out and
    put amlogic's special delimiters in front of them.
    
    TEST: amlogic_decoder_tests=true
    
    Change-Id: I82faaabfaef284b9a6a11c6abef202f9edee7b50

--- drivers/video/amlogic-decoder/tests/integration/test_vp9.cc
+++ drivers/video/amlogic-decoder/tests/integration/test_vp9.cc
@@ -3,13 +3,153 @@
 // found in the LICENSE file.
 
 #include "amlogic-video.h"
+
+#include <byteswap.h>
+
 #include "gtest/gtest.h"
 #include "hevcdec.h"
+#include "lib/fxl/logging.h"
 #include "tests/test_support.h"
 #include "vp9_decoder.h"
 
-#include "bear-vp9.aml.h"
+#include "bear-vp9.ivf.h"
 #include "macros.h"
+
+struct __attribute__((__packed__)) IvfHeader {
+  uint32_t signature;
+  uint16_t version;
+  uint16_t header_length;
+  uint32_t fourcc;
+  uint16_t width;
+  uint16_t height;
+  uint32_t frame_rate;
+  uint32_t time_scale;
+  uint32_t frame_count;
+  uint32_t unused;
+};
+
+struct __attribute__((__packed__)) IvfFrameHeader {
+  uint32_t size_bytes;
+  uint64_t presentation_timestamp;
+};
+
+std::vector<uint32_t> TryParseSuperframeHeader(const uint8_t* data,
+                                               uint32_t frame_size) {
+  std::vector<uint32_t> frame_sizes;
+  if (frame_size < 1)
+    return frame_sizes;
+  uint8_t superframe_header = data[frame_size - 1];
+
+  // Split out superframes into separate frames - see
+  // https://storage.googleapis.com/downloads.webmproject.org/docs/vp9/vp9-bitstream-specification-v0.6-20160331-draft.pdf
+  // Annex B.
+  if ((superframe_header & 0xc0) != 0xc0)
+    return frame_sizes;
+  uint8_t bytes_per_framesize = ((superframe_header >> 3) & 3) + 1;
+  uint8_t superframe_count = (superframe_header & 7) + 1;
+  uint32_t superframe_index_size = 2 + bytes_per_framesize * superframe_count;
+  if (superframe_index_size > frame_size)
+    return frame_sizes;
+  if (data[frame_size - superframe_index_size] != superframe_header) {
+    return frame_sizes;
+  }
+  const uint8_t* index_data = &data[frame_size - superframe_index_size + 1];
+  uint32_t total_size = 0;
+  for (uint32_t i = 0; i < superframe_count; i++) {
+    uint32_t sub_frame_size;
+    switch (bytes_per_framesize) {
+      case 1:
+        sub_frame_size = index_data[i];
+        break;
+      case 2:
+        sub_frame_size = reinterpret_cast<const uint16_t*>(index_data)[i];
+        break;
+      case 4:
+        sub_frame_size = reinterpret_cast<const uint32_t*>(index_data)[i];
+        break;
+      default:
+        DECODE_ERROR("Unsupported bytes_per_framesize: %d\n",
+                     bytes_per_framesize);
+        frame_sizes.clear();
+        return frame_sizes;
+    }
+    total_size += sub_frame_size;
+    if (total_size > frame_size) {
+      DECODE_ERROR("Total superframe size too large: %u > %u\n", total_size,
+                   frame_size);
+      frame_sizes.clear();
+      return frame_sizes;
+    }
+    frame_sizes.push_back(sub_frame_size);
+  }
+  return frame_sizes;
+}
+
+void SplitSuperframe(const uint8_t* data, uint32_t frame_size,
+                     std::vector<uint8_t>* output_vector) {
+  std::vector<uint32_t> frame_sizes =
+      TryParseSuperframeHeader(data, frame_size);
+
+  if (frame_sizes.empty())
+    frame_sizes.push_back(frame_size);
+  uint32_t frame_offset = 0;
+  uint32_t total_frame_bytes = 0;
+  for (auto& size : frame_sizes) {
+    total_frame_bytes += size;
+  }
+  const uint32_t kOutputHeaderSize = 16;
+  uint32_t output_offset = output_vector->size();
+  // This can be called multiple times on the same output_vector overall, but
+  // should be amortized O(1), since resizing larger inserts elements at the end
+  // and inserting elements at the end is amortized O(1) for std::vector.  Also,
+  // for now this is for testing.
+  output_vector->resize(output_offset + total_frame_bytes +
+                        kOutputHeaderSize * frame_sizes.size());
+  uint8_t* output = &(*output_vector)[output_offset];
+  for (auto& size : frame_sizes) {
+    FXL_DCHECK(output + 16 - output_vector->data() <=
+               static_cast<int64_t>(output_vector->size()));
+    *reinterpret_cast<uint32_t*>(output) = bswap_32(size + 4);
+    output += 4;
+    *reinterpret_cast<uint32_t*>(output) = ~bswap_32(size + 4);
+    output += 4;
+    *output++ = 0;
+    *output++ = 0;
+    *output++ = 0;
+    *output++ = 1;
+    *output++ = 'A';
+    *output++ = 'M';
+    *output++ = 'L';
+    *output++ = 'V';
+
+    FXL_DCHECK(output + size - output_vector->data() <=
+               static_cast<int64_t>(output_vector->size()));
+    memcpy(output, &data[frame_offset], size);
+    output += size;
+    frame_offset += size;
+  }
+  FXL_DCHECK(output - output_vector->data() ==
+             static_cast<int64_t>(output_vector->size()));
+}
+
+std::vector<uint8_t> ConvertIvfToAmlV(const uint8_t* data, uint32_t length) {
+  uint32_t offset = sizeof(IvfHeader);
+  std::vector<uint8_t> output_vector;
+  while (offset < length) {
+    auto frame_header = reinterpret_cast<const IvfFrameHeader*>(data + offset);
+    uint32_t frame_size = frame_header->size_bytes;
+    uint32_t data_offset = offset + sizeof(IvfFrameHeader);
+    if (data_offset + frame_size > length) {
+      DECODE_ERROR("Invalid IVF file, truncating\n");
+      return output_vector;
+    }
+
+    SplitSuperframe(data + data_offset, frame_size, &output_vector);
+
+    offset = data_offset + frame_size;
+  }
+  return output_vector;
+}
 
 class TestVP9 {
  public:
@@ -44,7 +184,8 @@ class TestVP9 {
             wait_valid.set_value();
         });
 
-    video->ParseVideo(bear_vp9_aml, bear_vp9_aml_len);
+    auto aml_data = ConvertIvfToAmlV(bear_vp9_ivf, bear_vp9_ivf_len);
+    video->ParseVideo(aml_data.data(), aml_data.size());
     EXPECT_EQ(std::future_status::ready,
               wait_valid.get_future().wait_for(std::chrono::seconds(1)));
 
