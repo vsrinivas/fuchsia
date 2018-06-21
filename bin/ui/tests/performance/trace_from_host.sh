3ff33ae802b93b01f3fddc19c79d0af35d68476f
Michael Jurka <mikejurka@google.com>
2018-06-21 11:52:40 -0700

    [scenic] Improve docs for trace_from_host.sh
    TEST=Ran this command and verified stats were printed:
    
    trace_from_host.sh trace_clipping_shadows \
        —clipping_enabled --screen_space_shadows
    
    Change-Id: I5980eede840cc96fa5f320842ed70cc818d0b760

--- bin/ui/tests/performance/trace_from_host.sh
+++ bin/ui/tests/performance/trace_from_host.sh
@@ -10,9 +10,30 @@ then
 Usage:
 trace_from_host.sh base_file_name [renderer_params...]
 
+1. Kills device_runner and all scenic-related processes.
+2. Prompts the user (giving time to put the system in a specific state)
+3. Takes a trace
+4. Outputs:
+     trace: trace-$base_file_name.out
+     scenic performance stats: benchmarks-$base_file_name.out
+
+Options:
+
 --nokill    Will not kill scenic/device_runner
 
+Flags for renderer_params:
+
+--unshadowed
+--screen_space_shadows
+--shadow_map
+--moment_shadow_map
+
+--clipping_enabled
+--clipping_disabled
+
 See renderer_params.cc for more arguments.
+
+
 
 example: trace_from_host.sh trace_clipping_shadows --clipping_enabled --screen_space_shadows
 
@@ -27,9 +48,9 @@ then
   echo "Killing processes and setting renderer params: $@"
   (set -x; fx shell "killall root_presenter; killall scenic; killall device_runner; killall view_manager; killall flutter*; killall set_root_view")
   (set -x; fx shell "set_renderer_params --render_continuously $@")
-  echo "Press control-C after device_runner starts..."
+  echo "== Press control-C after device_runner starts. =="
   (set -x; fx shell "run device_runner")
-  echo "Press 'return' to start tracing..."
+  echo "== Press 'return' to start tracing. =="
   read
 fi
 DATE=`date +%Y-%m-%dT%H:%M:%S`
