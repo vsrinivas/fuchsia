523f04aeecd80511ed30d389082f639ea2212481
Adam Barth <abarth@google.com>
2018-06-21 15:39:11 -0700

    [fidl] Update namespace for InterfaceRequestHandler
    This declaration moved.
    
    Test: No behavior change
    Change-Id: I4b9d64edbf15b46898deac5b87caf85d7055ee49

--- lib/fidl/scope.h
+++ lib/fidl/scope.h
@@ -27,10 +27,8 @@ class Scope {
   Scope(const Scope* parent_scope, const std::string& label);
 
   template <typename Interface>
-  void AddService(
-      fuchsia::sys::ServiceProviderImpl::InterfaceRequestHandler<Interface>
-          handler,
-      const std::string& service_name = Interface::Name_) {
+  void AddService(fidl::InterfaceRequestHandler<Interface> handler,
+                  const std::string& service_name = Interface::Name_) {
     service_provider_bridge_.AddService(std::move(handler), service_name);
   }
 
