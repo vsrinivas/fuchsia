70cbba0836532085ac884c55b8dca7ce540ee857
Greg Bonik <gbonik@google.com>
2018-06-21 14:31:55 -0700

    [wlanstack2] Tests for device management
    NET-779 #done
    
    TEST=Test-only change
    
    Change-Id: I71aaff73a24cbd52ce3648ad285adf788d13e6d5

--- bin/wlanstack/src/service.rs
+++ bin/wlanstack/src/service.rs
@@ -198,3 +198,264 @@ fn get_iface_stats(ifaces: &Arc<IfaceMap>, iface_id: u16)
         .into_future()
         .and_then(|iface| iface.stats_sched.get_stats())
 }
+
+#[cfg(test)]
+mod tests {
+    use async;
+    use device::{self, PhyDevice, PhyMap, IfaceDevice, IfaceMap};
+    use fidl::endpoints2::create_endpoints;
+    use fidl_sme;
+    use futures::prelude::*;
+    use futures::channel::mpsc;
+    use station;
+    use stats_scheduler::{self, StatsRequest};
+    use std::sync::Arc;
+    use wlan::{self, PhyRequest, PhyRequestStream};
+    use wlan_service::{self, PhyListItem};
+    use wlan_dev;
+    use zx;
+
+    #[test]
+    fn list_two_phys() {
+        let _exec = async::Executor::new().expect("Failed to create an executor");
+        let (phy_map, _phy_map_events) = PhyMap::new();
+        let phy_map = Arc::new(phy_map);
+        let (phy_null, _phy_null_stream) = fake_phy("/dev/null");
+        let (phy_zero, _phy_zero_stream) = fake_phy("/dev/zero");
+        phy_map.insert(10u16, phy_null);
+        phy_map.insert(20u16, phy_zero);
+        let mut list = super::list_phys(&phy_map).phys;
+        list.sort_by_key(|p| p.phy_id);
+        assert_eq!(vec![
+            PhyListItem{ phy_id: 10u16, path: "/dev/null".to_string() },
+            PhyListItem{ phy_id: 20u16, path: "/dev/zero".to_string() },
+        ], list)
+    }
+
+    #[test]
+    fn query_phy_success() {
+        let mut exec = async::Executor::new().expect("Failed to create an executor");
+        let (phy_map, _phy_map_events) = PhyMap::new();
+        let phy_map = Arc::new(phy_map);
+        let (phy, phy_stream) = fake_phy("/dev/null");
+        phy_map.insert(10u16, phy);
+
+        // Initiate a QueryPhy request. The returned future should not be able
+        // to produce a result immediately
+        let mut query_fut = super::query_phy(&phy_map, 10u16);
+        assert_eq!(Ok(Async::Pending), exec.run_until_stalled(&mut query_fut));
+
+        // The call above should trigger a Query message to the phy.
+        // Pretend that we are the phy and read the message from the other side.
+        let (responder, _phy_stream) = match exec.run_until_stalled(&mut phy_stream.next()) {
+            Ok(Async::Ready((Some(PhyRequest::Query{ responder }), stream))) => (responder, stream),
+            _ => panic!("phy_stream returned unexpected result")
+        };
+
+        // Reply with a fake phy info
+        responder.send(&mut wlan::QueryResponse {
+            status: zx::sys::ZX_OK,
+            info: fake_phy_info(),
+        }).expect("failed to send QueryResponse");
+
+        // Our original future should complete now, and return the same phy info
+        let response = match exec.run_until_stalled(&mut query_fut) {
+            Ok(Async::Ready((zx::Status::OK, Some(response)))) => response,
+            other => panic!("query_fut returned unexpected result: {:?}", other),
+        };
+        assert_eq!(fake_phy_info(), response.info);
+    }
+
+    #[test]
+    fn query_phy_not_found() {
+        let mut exec = async::Executor::new().expect("Failed to create an executor");
+        let (phy_map, _phy_map_events) = PhyMap::new();
+        let phy_map = Arc::new(phy_map);
+
+        let mut query_fut = super::query_phy(&phy_map, 10u16);
+        assert_eq!(Ok(Async::Ready((zx::Status::NOT_FOUND, None))),
+                   exec.run_until_stalled(&mut query_fut));
+    }
+
+    #[test]
+    fn create_iface_success() {
+        let mut exec = async::Executor::new().expect("Failed to create an executor");
+        let (phy_map, _phy_map_events) = PhyMap::new();
+        let phy_map = Arc::new(phy_map);
+
+        let (phy, phy_stream) = fake_phy("/dev/null");
+        phy_map.insert(10u16, phy);
+
+        // Initiate a CreateIface request. The returned future should not be able
+        // to produce a result immediately
+        let mut create_fut = super::create_iface(&phy_map, wlan_service::CreateIfaceRequest {
+            phy_id: 10,
+            role: wlan::MacRole::Client,
+        });
+        assert_eq!(Ok(Async::Pending), exec.run_until_stalled(&mut create_fut));
+
+        // The call above should trigger a CreateIface message to the phy.
+        // Pretend that we are the phy and read the message from the other side.
+        let (req, responder, _phy_stream) = match exec.run_until_stalled(&mut phy_stream.next()) {
+            Ok(Async::Ready((Some(PhyRequest::CreateIface{ req, responder }), stream))) =>
+                (req, responder, stream),
+            _ => panic!("phy_stream returned unexpected result")
+        };
+
+        // Since we requested the Client role, the request to the phy should also have
+        // the Client role
+        assert_eq!(wlan::MacRole::Client, req.role);
+
+        // Pretend that we created an interface device with id 123 and send a response
+        responder.send(&mut wlan::CreateIfaceResponse {
+            status: zx::sys::ZX_OK,
+            info: wlan::IfaceInfo{ id: 123 },
+        }).expect("failed to send CreateIfaceResponse");
+
+        // Now, our original future should resolve into a response
+        let response = match exec.run_until_stalled(&mut create_fut) {
+            Ok(Async::Ready((zx::Status::OK, Some(response)))) => response,
+            other => panic!("create_fut returned unexpected result: {:?}", other),
+        };
+        // This assertion likely needs to change once we figure out a solution
+        // to the iface id problem.
+        assert_eq!(123, response.iface_id);
+    }
+
+    #[test]
+    fn create_iface_not_found() {
+        let mut exec = async::Executor::new().expect("Failed to create an executor");
+        let (phy_map, _phy_map_events) = PhyMap::new();
+        let phy_map = Arc::new(phy_map);
+
+        let mut fut = super::create_iface(&phy_map, wlan_service::CreateIfaceRequest {
+            phy_id: 10,
+            role: wlan::MacRole::Client,
+        });
+        assert_eq!(Ok(Async::Ready((zx::Status::NOT_FOUND, None))),
+                   exec.run_until_stalled(&mut fut));
+    }
+
+    #[test]
+    fn get_client_sme_success() {
+        let mut exec = async::Executor::new().expect("Failed to create an executor");
+        let (iface_map, _iface_map_events) = IfaceMap::new();
+        let iface_map = Arc::new(iface_map);
+
+        let mut iface = fake_client_iface("/dev/null");
+        iface_map.insert(10, iface.iface);
+
+        let (proxy, server) = create_endpoints().expect("failed to create a pair of SME endpoints");
+        assert_eq!(zx::Status::OK, super::get_client_sme(&iface_map, 10, server));
+
+        // Expect to get a new FIDL client in the stream
+        let endpoint = iface.new_sme_clients.try_next()
+            .expect("expected a message in new_sme_clients")
+            .expect("didn't expect new_sme_clients stream to end");
+        let sme_stream = endpoint.into_stream().expect("failed to create stream for endpoint");
+
+        // Verify that `proxy` is indeed connected to `sme_stream`
+        let (_scan_proxy, scan_txn) = create_endpoints()
+            .expect("failed to create a pair of scan txn endpoints");
+        proxy.scan(&mut fake_scan_request(), scan_txn).expect("failed to send a scan request");
+
+        let req = match exec.run_until_stalled(&mut sme_stream.next()) {
+            Ok(Async::Ready((Some(fidl_sme::ClientSmeRequest::Scan{ req, .. }), _))) => req,
+            _ => panic!("sme_stream returned unexpected result")
+        };
+        assert_eq!(fake_scan_request(), req);
+    }
+
+    #[test]
+    fn get_client_sme_not_found() {
+        let mut _exec = async::Executor::new().expect("Failed to create an executor");
+        let (iface_map, _iface_map_events) = IfaceMap::new();
+        let iface_map = Arc::new(iface_map);
+
+        let (_proxy, server) = create_endpoints().expect("failed to create a pair of SME endpoints");
+        assert_eq!(zx::Status::NOT_FOUND, super::get_client_sme(&iface_map, 10, server));
+    }
+
+    #[test]
+    fn get_client_sme_wrong_role() {
+        let mut _exec = async::Executor::new().expect("Failed to create an executor");
+        let (iface_map, _iface_map_events) = IfaceMap::new();
+        let iface_map = Arc::new(iface_map);
+
+        let iface = fake_ap_iface("/dev/null");
+        iface_map.insert(10, iface.iface);
+
+        let (_proxy, server) = create_endpoints().expect("failed to create a pair of SME endpoints");
+        assert_eq!(zx::Status::NOT_SUPPORTED, super::get_client_sme(&iface_map, 10, server));
+    }
+
+    fn fake_phy(path: &str) -> (PhyDevice, PhyRequestStream) {
+        let (proxy, server) = create_endpoints::<wlan::PhyMarker>()
+            .expect("fake_phy: create_endpoints() failed");
+        let device = wlan_dev::Device::new(path).expect(&format!("fake_phy: failed to open {}", path));
+        let stream = server.into_stream().expect("fake_phy: failed to create stream");
+        (PhyDevice { proxy, device }, stream)
+    }
+
+    struct FakeClientIface {
+        iface: IfaceDevice,
+        _stats_requests: Box<Stream<Item = StatsRequest, Error = Never>>,
+        new_sme_clients: mpsc::UnboundedReceiver<station::ClientSmeEndpoint>,
+    }
+
+    fn fake_client_iface(path: &str) -> FakeClientIface {
+        let device = wlan_dev::Device::new(path)
+            .expect(&format!("fake_client_iface: failed to open {}", path));
+        let (sme_sender, sme_receiver) = mpsc::unbounded();
+        let (stats_sched, stats_requests) = stats_scheduler::create_scheduler();
+        let iface = IfaceDevice {
+            sme_server: device::SmeServer::Client(sme_sender),
+            stats_sched,
+            _device: device
+        };
+        FakeClientIface {
+            iface,
+            _stats_requests: Box::new(stats_requests),
+            new_sme_clients: sme_receiver
+        }
+    }
+
+    struct FakeApIface {
+        iface: IfaceDevice,
+        _stats_requests: Box<Stream<Item = StatsRequest, Error = Never>>,
+    }
+
+    fn fake_ap_iface(path: &str) -> FakeApIface {
+        let device = wlan_dev::Device::new(path)
+            .expect(&format!("fake_client_iface: failed to open {}", path));
+        let (stats_sched, stats_requests) = stats_scheduler::create_scheduler();
+        let iface = IfaceDevice {
+            sme_server: device::SmeServer::_Ap,
+            stats_sched,
+            _device: device
+        };
+        FakeApIface {
+            iface,
+            _stats_requests: Box::new(stats_requests),
+        }
+    }
+
+    fn fake_phy_info() -> wlan::PhyInfo {
+        wlan::PhyInfo {
+            id: 10,
+            dev_path: Some("/dev/null".to_string()),
+            hw_mac_address: [ 0x67, 0x62, 0x6f, 0x6e, 0x69, 0x6b ],
+            supported_phys: Vec::new(),
+            driver_features: Vec::new(),
+            mac_roles: Vec::new(),
+            caps: Vec::new(),
+            bands: Vec::new(),
+        }
+    }
+
+    fn fake_scan_request() -> fidl_sme::ScanRequest {
+        fidl_sme::ScanRequest{
+            timeout: 41
+        }
+    }
+}
