8bf6108e6d2027dba133a111ac5293e30c32e2ef
Aaron Green <aarongreen@google.com>
2018-06-22 15:10:12 +0000

    Revert "[zxcrypt] Zero-copy reads"
    This reverts commit 0137fff03c613c941bc268d263d2770532cc2948.
    
    Reason for revert: CL appears to have introduced test flake in TestVmoFirstBlockFvm_AES256_XTS_SHA256.
    
    Original change's description:
    > [zxcrypt] Zero-copy reads
    > 
    > This CL refactors the zxcrypt encrypted block device filter driver. this
    > driver uses an internal buffer to hide cryptographic transformations
    > from the upper layers.  For example, if a filesystem uses some memory to
    > write a block and then reads from that memory, it may reasonably expect
    > to find the plaintext data it wrote.
    > 
    > However, in the case of reads the previous contents of the buffer are
    > always clobbered.  As a result, there's no downside to doing the
    > decryption in-place, and the internal buffer can be avoided.
    > 
    > As part of this refactor, efforts were also made to lean more on atomics
    > where possible and reduce locking to only those concerns which directly
    > affect the internal write buffer.  As a consequence, the lifecycle code
    > around DdkUnbind and DdkRelease was also improved.
    > 
    > ZX-1811 #comment Should improve, performace, will measure and update
    > 
    > Test: Ran zxcrypt unit tests.
    > 
    > Change-Id: I929e8499ee9f0d359bede2f75530b75da5e86070
    
    TBR=smklein@google.com,aarongreen@google.com,joshlf@google.com
    
    Change-Id: I1fe983ba124108d4ae6361ac7729949f503e4309
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- system/dev/block/zxcrypt/worker.cpp
+++ system/dev/block/zxcrypt/worker.cpp
@@ -2,14 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <inttypes.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <threads.h>
 
 #include <crypto/cipher.h>
 #include <ddk/debug.h>
-#include <fbl/auto_call.h>
 #include <lib/zx/port.h>
 #include <zircon/listnode.h>
 #include <zircon/status.h>
@@ -34,22 +32,19 @@ Worker::Worker() : device_(nullptr) {}
 
 Worker::~Worker() {}
 
-zx_status_t Worker::Start(Device* device, const Volume& volume, zx::port&& port) {
+zx_status_t Worker::Start(Device* device, const Volume& volume, const zx::port& port) {
     zx_status_t rc;
 
     if (!device) {
         zxlogf(ERROR, "bad parameters: device=%p\n", device);
         return ZX_ERR_INVALID_ARGS;
     }
-    device_ = device;
-
     if ((rc = volume.Bind(crypto::Cipher::kEncrypt, &encrypt_)) != ZX_OK ||
         (rc = volume.Bind(crypto::Cipher::kDecrypt, &decrypt_)) != ZX_OK) {
-        zxlogf(ERROR, "failed to bind ciphers: %s\n", zx_status_get_string(rc));
         return rc;
     }
-
-    port_ = fbl::move(port);
+    device_ = device;
+    port.duplicate(ZX_RIGHT_SAME_RIGHTS, &port_);
 
     if (thrd_create(&thrd_, Thread, this) != thrd_success) {
         zxlogf(ERROR, "failed to start thread\n");
@@ -62,106 +57,64 @@ zx_status_t Worker::Start(Device* device, const Volume& volume, zx::port&& port)
 zx_status_t Worker::Loop() {
     zx_status_t rc;
     ZX_DEBUG_ASSERT(device_);
-
-    zxlogf(TRACE, "worker %p starting...\n", this);
     zx_port_packet_t packet;
-    while (true) {
-        // Read request
-        if ((rc = port_.wait(zx::time::infinite(), &packet)) != ZX_OK) {
-            zxlogf(ERROR, "failed to read request: %s\n", zx_status_get_string(rc));
-            return rc;
-        }
-        if (packet.status == ZX_ERR_STOP) {
-            zxlogf(TRACE, "worker %p stopping.\n", this);
-            return ZX_OK;
+
+    // Use the first request as a signal that the device is ready
+    if ((rc = port_.wait(zx::time::infinite(), &packet)) != ZX_OK ||
+        packet.status != ZX_ERR_NEXT) {
+        zxlogf(ERROR, "failed to start worker: %s\n", zx_status_get_string(rc));
+        return rc;
+    }
+
+    block_info_t info;
+    size_t op_size;
+    device_->BlockQuery(&info, &op_size);
+
+    do {
+        block_op_t* block = reinterpret_cast<block_op_t*>(packet.user.u64[0]);
+        extra_op_t* extra = BlockToExtra(block, op_size);
+
+        uint32_t length;
+        uint64_t offset_dev, offset_vmo;
+        if (mul_overflow(extra->length, info.block_size, &length) ||
+            mul_overflow(extra->offset_dev, info.block_size, &offset_dev) ||
+            mul_overflow(extra->offset_vmo, info.block_size, &offset_vmo)) {
+            device_->BlockRelease(block, ZX_ERR_OUT_OF_RANGE);
+            continue;
         }
 
-        // Dispatch request
-        block_op_t* block = reinterpret_cast<block_op_t*>(packet.user.u64[0]);
-        zxlogf(TRACE, "worker %p processing I/O request %p\n", this, block);
         switch (block->command & BLOCK_OP_MASK) {
         case BLOCK_OP_WRITE:
-            device_->BlockForward(block, EncryptWrite(block));
+            if ((rc = zx_vmo_read(extra->vmo, extra->data, offset_vmo, length)) != ZX_OK ||
+                (rc = encrypt_.Encrypt(extra->data, offset_dev, length, extra->data) != ZX_OK)) {
+                device_->BlockRelease(block, rc);
+                continue;
+            }
+            device_->BlockForward(block);
             break;
 
         case BLOCK_OP_READ:
-            device_->BlockComplete(block, DecryptRead(block));
+            if ((rc = decrypt_.Decrypt(extra->data, offset_dev, length, extra->data)) != ZX_OK ||
+                (rc = zx_vmo_write(extra->vmo, extra->data, offset_vmo, length)) != ZX_OK) {
+                device_->BlockRelease(block, rc);
+                continue;
+            }
+            device_->BlockRelease(block, ZX_OK);
             break;
 
         default:
-            device_->BlockComplete(block, ZX_ERR_NOT_SUPPORTED);
+            device_->BlockRelease(block, ZX_ERR_NOT_SUPPORTED);
         }
-    }
-}
-
-zx_status_t Worker::EncryptWrite(block_op_t* block) {
-    zx_status_t rc;
-    extra_op_t* extra = BlockToExtra(block, device_->op_size());
-
-    // Convert blocks to bytes
-    uint32_t length;
-    uint64_t offset_dev, offset_vmo;
-    if (mul_overflow(block->rw.length, device_->block_size(), &length) ||
-        mul_overflow(block->rw.offset_dev, device_->block_size(), &offset_dev) ||
-        mul_overflow(extra->offset_vmo, device_->block_size(), &offset_vmo)) {
-        zxlogf(ERROR,
-               "overflow; length=%" PRIu32 "; offset_dev=%" PRIu64 "; offset_vmo=%" PRIu64 "\n",
-               block->rw.length, block->rw.offset_dev, extra->offset_vmo);
-        return ZX_ERR_OUT_OF_RANGE;
-    }
-
-    // Copy and encrypt the plaintext
-    if ((rc = zx_vmo_read(extra->vmo, extra->data, offset_vmo, length)) != ZX_OK) {
-        zxlogf(ERROR, "zx_vmo_read() failed: %s\n", zx_status_get_string(rc));
-        return rc;
-    }
-    if ((rc = encrypt_.Encrypt(extra->data, offset_dev, length, extra->data) != ZX_OK)) {
-        zxlogf(ERROR, "failed to encrypt: %s\n", zx_status_get_string(rc));
-        return rc;
-    }
-
-    return ZX_OK;
-}
-
-zx_status_t Worker::DecryptRead(block_op_t* block) {
-    zx_status_t rc;
-
-    // Convert blocks to bytes
-    uint32_t length;
-    uint64_t offset_dev, offset_vmo;
-    if (mul_overflow(block->rw.length, device_->block_size(), &length) ||
-        mul_overflow(block->rw.offset_dev, device_->block_size(), &offset_dev) ||
-        mul_overflow(block->rw.offset_vmo, device_->block_size(), &offset_vmo)) {
-        zxlogf(ERROR,
-               "overflow; length=%" PRIu32 "; offset_dev=%" PRIu64 "; offset_vmo=%" PRIu64 "\n",
-               block->rw.length, block->rw.offset_dev, block->rw.offset_vmo);
-        return ZX_ERR_OUT_OF_RANGE;
-    }
-
-    // Map the ciphertext
-    zx_handle_t root = zx_vmar_root_self();
-    uintptr_t address;
-    constexpr uint32_t flags = ZX_VM_FLAG_PERM_READ | ZX_VM_FLAG_PERM_WRITE;
-    if ((rc = zx_vmar_map(root, 0, block->rw.vmo, offset_vmo, length, flags, &address)) != ZX_OK) {
-        zxlogf(ERROR, "zx::vmar::root_self().map() failed: %s\n", zx_status_get_string(rc));
-        return rc;
-    }
-    auto cleanup =
-        fbl::MakeAutoCall([root, address, length]() { zx_vmar_unmap(root, address, length); });
-
-    // Decrypt in place
-    uint8_t* data = reinterpret_cast<uint8_t*>(address) + offset_vmo;
-    if ((rc = decrypt_.Decrypt(data, offset_dev, length, data)) != ZX_OK) {
-        zxlogf(ERROR, "failed to decrypt: %s\n", zx_status_get_string(rc));
-        return rc;
-    }
-
-    return ZX_OK;
+    } while ((rc = port_.wait(zx::time::infinite(), &packet)) == ZX_OK &&
+             packet.status == ZX_ERR_NEXT);
+    return rc;
 }
 
 zx_status_t Worker::Stop() {
     zx_status_t rc;
+
     thrd_join(thrd_, &rc);
+
     return rc;
 }
 
--- system/dev/block/zxcrypt/worker.h
+++ system/dev/block/zxcrypt/worker.h
@@ -27,12 +27,15 @@ public:
     Worker();
     ~Worker();
 
-    // Starts the worker, which will service requests sent from the given |device| on the given
-    // |port|.  Cryptographic operations will use the key material from the given |volume|.
-    zx_status_t Start(Device* device, const Volume& volume, zx::port&& port);
+    // Starts the worker, which will service requests pulled from the given |queue| for the given
+    // |device|.  Cryptographic operations will use the region of memory starting at the given
+    // |base|, and the ciphers will be initialized according to key data stored in the given
+    // |volume|.
+    zx_status_t Start(Device* device, const Volume& volume, const zx::port& port);
 
-    // Thread body.  Reads an I/O request from the |port_| and dispatches it between |EncryptWrite|
-    // and |DecryptRead|.
+    // Thread body. Encrypts write requests and and forwards them to the parent device using
+    // Device::BlockForward.  Decrypts read responses and completes them back to the requester using
+    // Device::BlockRelease.  This method should not be called directly; use |Start| instead.
     zx_status_t Loop();
 
     // Asks the worker to stop.  This call blocks until the worker has finished processing the
@@ -42,24 +45,14 @@ public:
 private:
     DISALLOW_COPY_ASSIGN_AND_MOVE(Worker);
 
-    // Copies the plaintext data to be written to the write buffer location given in |block|'s extra
-    // information, and encrypts it before sending it to the parent device.
-    zx_status_t EncryptWrite(block_op_t* block);
-
-    // Maps the ciphertext data in |block|, and decrypts it in place before completing the block op.
-    zx_status_t DecryptRead(block_op_t* block);
-
     // The cipher objects used to perform cryptographic.  See notes on "random access" in
     // crypto/cipher.h.
     crypto::Cipher encrypt_;
     crypto::Cipher decrypt_;
-
     // The device associated with this worker.
     Device* device_;
-
-    // The port to wait for I/O request on, as given by the device.
+    // The port to wait for I/O request on.  Duplicate of the device's port.
     zx::port port_;
-
     // The executing thread for this worker
     thrd_t thrd_;
 };
