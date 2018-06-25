913769a408817ec186c2cae61af3a8b006977396
Joshua Seaton <joshuaseaton@google.com>
2018-06-24 18:50:51 -0700

    [async] Remove MessageLoop from shapes a11y app.
    fsl::MessageLoop is being deprecated and replaced with async::Loop.
    
    Change-Id: Ia0bafe31a2b0b8f4e17e3c922a0e2775a3e1604d
    Tested: no behavior change.
    Bug: ZX-1819

--- examples/ui/shapes_a11y/main.cc
+++ examples/ui/shapes_a11y/main.cc
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/async-loop/cpp/loop.h>
 #include <trace-provider/provider.h>
 
 #include "garnet/examples/ui/shapes_a11y/shapes_a11y_view.h"
-#include "lib/fsl/tasks/message_loop.h"
 #include "lib/ui/view_framework/view_provider_app.h"
 
 int main(int argc, const char** argv) {
-  fsl::MessageLoop loop;
+  async::Loop loop(&kAsyncLoopConfigMakeDefault);
   trace::TraceProvider trace_provider(loop.async());
   // Launching server app
 
