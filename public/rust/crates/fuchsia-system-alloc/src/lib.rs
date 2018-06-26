be9beffb6b143a499915dce11b73929413f2fb4a
Taylor Cramer <cramertj@google.com>
2018-06-26 14:31:09 -0700

    [rust] Cleanup COPYRIGHT and comment in fuchsia-system-alloc
    Test: n/a
    
    Change-Id: If4a0a58fe9b7bd9fcb1faf38d581afbc371d20a8

--- public/rust/crates/fuchsia-system-alloc/src/lib.rs
+++ public/rust/crates/fuchsia-system-alloc/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,9 +8,6 @@
 //
 // This will conflict with any other libraries that set the global allocator,
 // and should be used uniquely on Fuchsia.
-// In the future, it might be good to break this declaration out into its own
-// crate, but for now it's convenient to add it here so that Fuchsia apps
-// depending on fuchsia_zircon will use the system allocator.
 use std::alloc::System;
 #[global_allocator]
 static ALLOC: System = System;
