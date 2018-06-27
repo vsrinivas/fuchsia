4f33481b0e19a3dab005378092e4a9c3ae1859e8
Adam Barth <abarth@google.com>
2018-06-27 15:25:49 -0700

    [dash] Switch to fdio_spawn_etc
    Most codepaths that create processes should use fdio_spawn rather than
    launchpad directly.
    
    Test: Manually verified that the shell continues to work.
    Change-Id: I0e9b21f17ac7b21b92a5bedd5bacf121dee8ff02

--- third_party/uapp/dash/src/process.h
+++ third_party/uapp/dash/src/process.h
@@ -4,17 +4,18 @@
 
 #pragma once
 
+#include <lib/fdio/spawn.h>
 #include <zircon/types.h>
 
 union node;
 
-int process_launch(int argc, const char* const* argv, const char* path,
+int process_launch(const char* const* argv, const char* path,
                    int index, zx_handle_t* process, zx_handle_t job,
-                   zx_status_t* status_out, const char** errmsg);
+                   zx_status_t* status_out, char err_msg[FDIO_SPAWN_ERR_MSG_MAX_LENGTH]);
 
 zx_status_t process_subshell(union node* n, const char* const* envp,
                              zx_handle_t* process, zx_handle_t job,
-                             int *fds, const char** errmsg);
+                             int *fds, char err_msg[FDIO_SPAWN_ERR_MSG_MAX_LENGTH]);
 
 // Waits for the process to terminate and returns the exit code for the process.
 int process_await_termination(zx_handle_t process, zx_handle_t job, bool blocking);
