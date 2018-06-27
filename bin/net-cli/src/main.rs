334f0fbae363593b161432a1175eff2b615ba335
Tim Kilbourn <tkilbourn@google.com>
2018-06-27 08:46:21 -0700

    [net-cli] Commandline tool for fushia.net.stack interfaces
    Implements list, get, enable, and disable for network interfaces.
    
    NET-1097
    Test: manual testing against Go netstack
    
    Change-Id: I0eb25e9a2cf389e2fe673e5139d34250c7ca10a6

---
+++ bin/net-cli/src/main.rs
@@ -0,0 +1,92 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![deny(warnings)]
+
+extern crate failure;
+extern crate fidl;
+extern crate fidl_fuchsia_net_stack as netstack;
+extern crate fuchsia_app as component;
+#[macro_use]
+extern crate fuchsia_async as async;
+extern crate fuchsia_zircon as zx;
+extern crate futures;
+#[macro_use]
+extern crate structopt;
+
+use component::client::connect_to_service;
+use failure::{Error, Fail, ResultExt};
+use futures::prelude::*;
+use netstack::{StackMarker, StackProxy};
+use structopt::StructOpt;
+
+mod opts;
+use opts::*;
+
+fn main() -> Result<(), Error> {
+    let opt = Opt::from_args();
+    let mut exec = async::Executor::new().context("error creating event loop")?;
+    let stack = connect_to_service::<StackMarker>().context("failed to connect to netstack")?;
+
+    match opt {
+        Opt::If(cmd) => exec.run_singlethreaded(do_if(cmd, stack)),
+        Opt::Fwd(cmd) => exec.run_singlethreaded(do_fwd(cmd, stack)),
+    }
+}
+
+many_futures!(IfFut, [List, GetInfo, Enable, Disable, AddrAdd, AddrDel,]);
+
+fn do_if(cmd: opts::IfCmd, stack: StackProxy) -> impl Future<Item = (), Error = Error> {
+    match cmd {
+        IfCmd::List => IfFut::List({
+            stack
+                .list_interfaces()
+                .map_err(|e| e.context("error getting response").into())
+                .map(|response| println!("interfaces: {:02x?}", response))
+        }),
+        IfCmd::Get { id } => IfFut::GetInfo({
+            stack
+                .get_interface_info(id)
+                .map_err(|e| e.context("error getting response").into())
+                .map(move |response| println!("interface {}: {:02x?}", id, response))
+        }),
+        IfCmd::Enable { id } => IfFut::Enable({
+            stack
+                .enable_interface(id)
+                .map_err(|e| e.context("error getting response").into())
+                .map(move |response| {
+                    if let Some(e) = response {
+                        println!("Error enabling interface {}: {:?}", id, e)
+                    } else {
+                        println!("Interface {} enabled", id)
+                    }
+                })
+        }),
+        IfCmd::Disable { id } => IfFut::Disable({
+            stack
+                .disable_interface(id)
+                .map_err(|e| e.context("error getting response").into())
+                .map(move |response| {
+                    if let Some(e) = response {
+                        println!("Error disabling interface {}: {:?}", id, e)
+                    } else {
+                        println!("Interface {} disabled", id)
+                    }
+                })
+        }),
+        IfCmd::Addr(AddrCmd::Add { .. }) => IfFut::AddrAdd({
+            println!("{:?} not implemented!", cmd);
+            futures::future::ok(())
+        }),
+        IfCmd::Addr(AddrCmd::Del { .. }) => IfFut::AddrDel({
+            println!("{:?} not implemented!", cmd);
+            futures::future::ok(())
+        }),
+    }
+}
+
+fn do_fwd(cmd: opts::FwdCmd, _stack: StackProxy) -> impl Future<Item = (), Error = Error> {
+    println!("{:?} not implemented!", cmd);
+    futures::future::ok(())
+}
---
+++ bin/net-cli/src/opts.rs
@@ -0,0 +1,77 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[derive(StructOpt, Debug)]
+pub enum Opt {
+    #[structopt(name = "if")]
+    /// commands for network interfaces
+    If(IfCmd),
+
+    #[structopt(name = "fwd")]
+    /// commands for forwarding tables
+    Fwd(FwdCmd),
+}
+
+#[derive(StructOpt, Clone, Debug)]
+pub enum IfCmd {
+    #[structopt(name = "list")]
+    /// lists network interfaces
+    List,
+    #[structopt(name = "get")]
+    /// queries a network interface
+    Get {
+        #[structopt(raw(required = "true"))]
+        /// id of the network interface to query
+        id: u64,
+    },
+    #[structopt(name = "enable")]
+    /// enables a network interface
+    Enable {
+        #[structopt(raw(required = "true"))]
+        /// id of the network interface to enable
+        id: u64,
+    },
+    #[structopt(name = "disable")]
+    /// disables a network interface
+    Disable {
+        #[structopt(raw(required = "true"))]
+        /// id of the network interface to disable
+        id: u64,
+    },
+    #[structopt(name = "addr")]
+    /// commands for updating network interface addresses
+    Addr(AddrCmd),
+}
+
+#[derive(StructOpt, Clone, Debug)]
+pub enum AddrCmd {
+    #[structopt(name = "add")]
+    /// adds an address to the network interface
+    Add {
+        #[structopt(raw(required = "true"))]
+        /// id of the network interface
+        id: u64,
+        #[structopt(raw(required = "true"))]
+        addr: String,
+        #[structopt(raw(required = "true"))]
+        prefix: u8,
+    },
+    #[structopt(name = "del")]
+    /// deletes an address from the network interface
+    Del {
+        #[structopt(raw(required = "true"))]
+        /// id of the network interface
+        id: u64,
+        #[structopt(raw(required = "true"))]
+        addr: String,
+    },
+}
+
+#[derive(StructOpt, Copy, Clone, Debug)]
+pub enum FwdCmd {
+    #[structopt(name = "list")]
+    /// lists forwarding table entries
+    List,
+    // TODO: add/del
+}
