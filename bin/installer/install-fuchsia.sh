3ab0dede76272b50cb575b78b1332e015d7458d9
James Tucker <raggi@google.com>
2018-06-29 17:43:06 -0700

    [installer] image names no longer contain boards
    Test: manual.
    
    Change-Id: Ie8eb05a95b37deb206b6dea45a98616a0e3e89dc

--- bin/installer/install-fuchsia.sh
+++ bin/installer/install-fuchsia.sh
@@ -4,36 +4,48 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# This script version of install-fuchsia exists because the /install filesystem
-# (indeed all minfs filesystems) can no longer provide executable binaries.
-
 INSTALL_PATH="/install"
 PAVER="/boot/bin/install-disk-image"
 
-if [ ! -f "${INSTALL_PATH}/fvm.sparse.blk" ]; then
-  echo "Installer files are missing!"
-  exit 1
-fi
+# TODO(raggi): template this from the build instead.
+IMAGES="fvm.sparse.blk local.esp.blk zircon.vboot"
 
 if [ ! -e "${PAVER}" ]; then
   echo "Paver \"install-disk-image\" is missing!"
   exit 1
 fi
 
-if [ -f "${INSTALL_PATH}/fvm.sparse.blk" ]; then
- if ! "${PAVER}" install-fvm --file "${INSTALL_PATH}/fvm.sparse.blk"; then
-  exit $?
- fi
-fi
-
-for file in "${INSTALL_PATH}/local-*.esp.blk"; do
-  if ! "${PAVER}" install-efi --file "${file}"; then
-    exit $?
+for file in $IMAGES; do
+  img="${INSTALL_PATH}/${file}"
+  if [ ! -f "$img" ]; then
+    echo "Missing required image file: $img" >&2
+    exit 1
   fi
 done
 
-for file in "${INSTALL_PATH}/zircon-*.vboot"; do
-  if ! "${PAVER}" install-kernc --file "${file}"; then
-    exit $?
-  fi
+for file in $IMAGES; do
+  img="${INSTALL_PATH}/${file}"
+
+  typ=""
+  case "$file" in
+    fvm*|*.fvm.blk|*.sparse.blk)
+      typ="fvm"
+      ;;
+    *esp.blk|*efi.blk)
+      typ="efi"
+      ;;
+    *.vboot)
+      typ="kernc"
+      ;;
+    *)
+      echo "Unknown paver image type for $file" >&2
+      exit 1
+      ;;
+  esac
+
+  set -x
+  "${PAVER}" install-${typ} --file "${img}" || exit $?
+  set +x
 done
+
+echo -n reboot > /dev/misc/dmctl
