185ff730ca9f1b40d5298f320f82c0d02973a89e
Przemyslaw Pietrzkiewicz <ppi@google.com>
2018-07-03 15:00:31 +0200

    [ledger] test page sync status in SyncIntegrationTest
    This patch:
    
     - verifies that page sync state eventually becomes idle on both ends
       for serial and concurrent sync scenarios
     - uses the sync state to make the serial sync scenario actually serial,
       so that we actually wait until download finishes before starting the
       second connection
    
    This is inspired by (but different and might be worth expanding in a
    follow-up) from testing proposed in the unfinished
    https://fuchsia-review.googlesource.com/c/peridot/+/134415
    
    LE-369 #comment [ledger] test page sync status in SyncIntegrationTest
    
    Test: ledger_integration_tests
    
    Change-Id: I2d735c04d61c502227f789051f3c5f663be5fad1

--- bin/ledger/tests/integration/sync/sync_tests.cc
+++ bin/ledger/tests/integration/sync/sync_tests.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <lib/callback/capture.h>
+#include <lib/fidl/cpp/binding.h>
 #include <lib/fidl/cpp/optional.h>
 #include <lib/fsl/vmo/strings.h>
 
@@ -13,6 +14,34 @@ namespace test {
 namespace integration {
 namespace sync {
 namespace {
+
+class SyncWatcherImpl : public ledger::SyncWatcher {
+ public:
+  SyncWatcherImpl() : binding_(this) {}
+  ~SyncWatcherImpl() override {}
+
+  auto NewBinding() { return binding_.NewBinding(); }
+
+  bool Equals(ledger::SyncState download, ledger::SyncState upload) {
+    return download == download_state && upload == upload_state;
+  }
+
+  ledger::SyncState download_state = ledger::SyncState::PENDING;
+  ledger::SyncState upload_state = ledger::SyncState::PENDING;
+
+ private:
+  // ledger::SyncWatcher:
+  void SyncStateChanged(ledger::SyncState download, ledger::SyncState upload,
+                        SyncStateChangedCallback callback) override {
+    download_state = download;
+    upload_state = upload;
+    callback();
+  }
+
+  fidl::Binding<ledger::SyncWatcher> binding_;
+
+  FXL_DISALLOW_COPY_AND_ASSIGN(SyncWatcherImpl);
+};
 
 class SyncIntegrationTest : public IntegrationTest {
  protected:
@@ -45,36 +74,78 @@ class SyncIntegrationTest : public IntegrationTest {
     } while (token);
     return ::testing::AssertionSuccess();
   }
+
+  std::unique_ptr<SyncWatcherImpl> WatchPageSyncState(ledger::PagePtr* page) {
+    auto watcher = std::make_unique<SyncWatcherImpl>();
+
+    ledger::Status status = ledger::Status::INTERNAL_ERROR;
+    (*page)->SetSyncStateWatcher(watcher->NewBinding(),
+                                 callback::Capture(QuitLoopClosure(), &status));
+    RunLoop();
+    EXPECT_EQ(ledger::Status::OK, status);
+
+    return watcher;
+  }
+
+  bool WaitUntilSyncIsIdle(SyncWatcherImpl* watcher) {
+    return RunLoopUntil([watcher] {
+      return watcher->Equals(ledger::SyncState::IDLE, ledger::SyncState::IDLE);
+    });
+  }
 };
 
+// Verifies that a new page entry is correctly synchronized between two Ledger
+// app instances.
+//
+// In this test the app instances connect to the cloud one after the other: the
+// first instance uploads data to the cloud and shuts down, and only after that
+// the second instance is created and connected.
 TEST_P(SyncIntegrationTest, SerialConnection) {
-  auto instance1 = NewLedgerAppInstance();
-  auto page = instance1->GetTestPage();
-  ledger::Status status;
-  page->Put(convert::ToArray("Hello"), convert::ToArray("World"),
-            callback::Capture(QuitLoopClosure(), &status));
-  RunLoop();
-  ASSERT_EQ(ledger::Status::OK, status);
   ledger::PageId page_id;
-  page->GetId(callback::Capture(QuitLoopClosure(), &page_id));
+  ledger::Status status;
+
+  // Create the first instance and write the page entry.
+  auto instance1 = NewLedgerAppInstance();
+  auto page1 = instance1->GetTestPage();
+  auto page1_state_watcher = WatchPageSyncState(&page1);
+  page1->Put(convert::ToArray("Hello"), convert::ToArray("World"),
+             callback::Capture(QuitLoopClosure(), &status));
   RunLoop();
 
+  // Retrieve the page ID so that we can later connect to the same page from
+  // another app instance.
+  ASSERT_EQ(ledger::Status::OK, status);
+  page1->GetId(callback::Capture(QuitLoopClosure(), &page_id));
+  RunLoop();
+
+  // Wait until the sync state becomes idle.
+  EXPECT_TRUE(WaitUntilSyncIsIdle(page1_state_watcher.get()));
+
+  // Create the second instance, connect to the same page and download the
+  // data.
   auto instance2 = NewLedgerAppInstance();
-  page = instance2->GetPage(fidl::MakeOptional(page_id), ledger::Status::OK);
-  EXPECT_TRUE(RunLoopUntil([this, &page] {
+  auto page2 =
+      instance2->GetPage(fidl::MakeOptional(page_id), ledger::Status::OK);
+  auto page2_state_watcher = WatchPageSyncState(&page2);
+
+  // TODO(ppi): just wait until sync is idle rather than retrieving entries when
+  // LE-369 is fixed. Before that we can get the sync state of IDLE, IDLE before
+  // any synchronization happens.
+  EXPECT_TRUE(RunLoopUntil([this, &page2] {
     fidl::VectorPtr<ledger::Entry> entries;
-    if (!GetEntries(page.get(), &entries)) {
+    if (!GetEntries(page2.get(), &entries)) {
       return true;
     }
     return !entries->empty();
   }));
 
   ledger::PageSnapshotPtr snapshot;
-  page->GetSnapshot(snapshot.NewRequest(), fidl::VectorPtr<uint8_t>::New(0),
-                    nullptr, callback::Capture(QuitLoopClosure(), &status));
+  page2->GetSnapshot(snapshot.NewRequest(), fidl::VectorPtr<uint8_t>::New(0),
+                     nullptr, callback::Capture(QuitLoopClosure(), &status));
   RunLoop();
   ASSERT_EQ(ledger::Status::OK, status);
   std::unique_ptr<ledger::InlinedValue> inlined_value;
+
   snapshot->GetInline(
       convert::ToArray("Hello"),
       callback::Capture(QuitLoopClosure(), &status, &inlined_value));
@@ -82,18 +153,29 @@ TEST_P(SyncIntegrationTest, SerialConnection) {
   ASSERT_EQ(ledger::Status::OK, status);
   ASSERT_TRUE(inlined_value);
   ASSERT_EQ("World", convert::ToString(inlined_value->value));
+
+  // Verify that the sync state of the second page connection eventually becomes
+  // idle.
+  EXPECT_TRUE(WaitUntilSyncIsIdle(page2_state_watcher.get()));
 }
 
+// Verifies that a new page entry is correctly synchronized between two Ledger
+// app instances.
+//
+// In this test the app instances connect to the cloud concurrently: the second
+// instance is already connected when the first instance writes the entry.
 TEST_P(SyncIntegrationTest, ConcurrentConnection) {
   auto instance1 = NewLedgerAppInstance();
   auto instance2 = NewLedgerAppInstance();
 
   auto page1 = instance1->GetTestPage();
+  auto page1_state_watcher = WatchPageSyncState(&page1);
   ledger::PageId page_id;
   page1->GetId(callback::Capture(QuitLoopClosure(), &page_id));
   RunLoop();
   auto page2 =
       instance2->GetPage(fidl::MakeOptional(page_id), ledger::Status::OK);
+  auto page2_state_watcher = WatchPageSyncState(&page2);
 
   ledger::Status status;
   page1->Put(convert::ToArray("Hello"), convert::ToArray("World"),
@@ -101,6 +183,9 @@ TEST_P(SyncIntegrationTest, ConcurrentConnection) {
   RunLoop();
   ASSERT_EQ(ledger::Status::OK, status);
 
+  // TODO(ppi): just wait until sync is idle rather than retrieving entries when
+  // LE-369 is fixed. Before that we can get the sync state of IDLE, IDLE before
+  // any synchronization happens.
   EXPECT_TRUE(RunLoopUntil([this, &page2] {
     fidl::VectorPtr<ledger::Entry> entries;
     if (!GetEntries(page2.get(), &entries)) {
@@ -122,6 +207,10 @@ TEST_P(SyncIntegrationTest, ConcurrentConnection) {
   ASSERT_EQ(ledger::Status::OK, status);
   ASSERT_TRUE(inlined_value);
   ASSERT_EQ("World", convert::ToString(inlined_value->value));
+
+  // Verify that the sync states of both pages eventually become idle.
+  EXPECT_TRUE(WaitUntilSyncIsIdle(page1_state_watcher.get()));
+  EXPECT_TRUE(WaitUntilSyncIsIdle(page2_state_watcher.get()));
 }
 
 INSTANTIATE_TEST_CASE_P(SyncIntegrationTest, SyncIntegrationTest,
