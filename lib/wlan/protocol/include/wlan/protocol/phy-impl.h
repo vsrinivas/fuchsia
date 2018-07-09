608555e9a8bb19c368a0e172a11991cc857e97d6
Josh Conner <joshconner@google.com>
2018-07-09 16:11:56 -0700

    [wlan] Update to reflect wlanphy_info reorg
    Remove reference to "common.h", pull interface id out of a struct,
    and reference wlanphy_info_t instead of wlan_info_t.
    
    NET-1069
    
    Test: Builds.
    Change-Id: I543daa8bab54fbe9125db9f83f77e61476904626

--- lib/wlan/protocol/include/wlan/protocol/phy-impl.h
+++ lib/wlan/protocol/include/wlan/protocol/phy-impl.h
@@ -4,19 +4,15 @@
 
 #pragma once
 
-#include <wlan/protocol/common.h>
+#include <wlan/protocol/phy.h>
 #include <zircon/compiler.h>
 
 __BEGIN_CDECLS
 
-typedef struct wlan_iface_info {
-    uint16_t id;
-} wlan_iface_info_t;
-
 typedef struct wlanphy_impl_protocol_ops {
 
     // Get information about the capabilities of the physical device
-    zx_status_t (*query)(void* ctx, wlan_info_t* info);
+    zx_status_t (*query)(void* ctx, wlanphy_info_t* info);
 
     // Create a new interface with the specified role, returning the interface attributes.
     // Some common error codes are:
@@ -24,7 +20,7 @@ typedef struct wlanphy_impl_protocol_ops {
     // ZX_ERR_NOT_SUPPORTED: device does not support the specified role
     zx_status_t (*create_iface)(void* ctx,
                                 uint16_t role,
-                                wlan_iface_info_t* info);
+                                uint16_t* iface_id);
 
     // Destroy the interface with the matching id.
     zx_status_t (*destroy_iface)(void* ctx, uint16_t id);
