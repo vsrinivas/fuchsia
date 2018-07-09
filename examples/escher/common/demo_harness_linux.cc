288de0e5d2b960da161f0a30ac2ac70332538c81
Josh Gargus <jjosh@google.com>
2018-07-09 00:30:09 -0700

    [escher] Fix typo that broke Escher Linux demos.
    TEST=Linux demos now compile.
    
    Change-Id: Ib61b71cd1feb1998f2130068369e5f3ca0a5313e

--- examples/escher/common/demo_harness_linux.cc
+++ examples/escher/common/demo_harness_linux.cc
@@ -133,7 +133,9 @@ std::unique_ptr<DemoHarness> DemoHarness::New(
 }
 
 DemoHarnessLinux::DemoHarnessLinux(WindowParams window_params)
-    : DemoHarness(window_params), filesystem_(escher::HackFilesystem::New()) {}
+    : DemoHarness(window_params) {
+  filesystem_ = escher::HackFilesystem::New();
+}
 
 void DemoHarnessLinux::InitWindowSystem() {
   FXL_CHECK(!g_harness);
