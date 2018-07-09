NERD-Repo/garnet

[bt][gatt] Add Client.WriteWithoutResponse
Added a GATT client API function for the "Write Without Response"
procedure.

NET-1076 #done
Test: 1. bt_host_unittests --gtest_filter=GATT_*
      2. a. Connect to BLE mouse using bt-le-central;
         b. Connect to HID service;
         b. Write to report mode characteristic using "write-chr -w 0 0"

Change-Id: Ia2d2fd3f3202cd82342c322366956a65e137b0a4

bin/bluetooth/tools/bt-le-central/src/common/gatt.rs
@@ -228,6 +228,19 @@ fn write_characteristic(
         })
 }
 
+fn write_without_response(client: GattClientPtr, id: u64, value: Vec<u8>)
+    -> impl Future<Item = (), Error = Error> {
+    future::result(
+        client
+            .read()
+            .active_proxy
+            .as_ref()
+            .unwrap()
+            .write_without_response(id, &mut value.into_iter())
+            .map_err(|_| BTError::new("Failed to send message").into())
+    )
+}
+
 // ===== REPL =====
 
 fn do_help() -> FutureResult<(), Error> {
@@ -317,15 +330,20 @@ fn do_read_chr(args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = (),
     Right(read_characteristic(client, id))
 }
 
-fn do_write_chr(args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = (), Error = Error> {
+fn do_write_chr(mut args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = (), Error = Error> {
     if args.len() < 1 {
-        println!("usage: write-chr <id> <value>");
+        println!("usage: write-chr [-w] <id> <value>");
         return left_ok!();
     }
 
     if client.read().active_proxy.is_none() {
         println!("no service connected");
         return left_ok!();
+    }
+
+    let without_response: bool = args[0] == "-w";
+    if without_response {
+        args.remove(0);
     }
 
     let id: u64 = match args[0].parse() {
@@ -343,7 +361,13 @@ fn do_write_chr(args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = ()
             println!("invalid value");
             left_ok!()
         }
-        Ok(v) => Right(write_characteristic(client, id, v)),
+        Ok(v) => Right(
+            if without_response {
+                Left(write_without_response(client, id, v))
+            } else {
+                Right(write_characteristic(client, id, v))
+            }
+        )
     }
 }
 

drivers/bluetooth/host/fidl/gatt_remote_service_server.cc
@@ -121,6 +121,11 @@ void GattRemoteServiceServer::WriteCharacteristic(
   service_->WriteCharacteristic(id, value.take(), std::move(cb));
 }
 
+void GattRemoteServiceServer::WriteWithoutResponse(
+    uint64_t id, ::fidl::VectorPtr<uint8_t> value) {
+  service_->WriteWithoutResponse(id, value.take());
+}
+
 void GattRemoteServiceServer::NotifyCharacteristic(
     uint64_t id,
     bool enable,

drivers/bluetooth/lib/gatt/remote_service_manager_unittest.cc
@@ -852,6 +852,49 @@ TEST_F(GATT_RemoteServiceManagerTest, WriteCharSendsWriteRequest) {
   EXPECT_EQ(kStatus, status);
 }
 
+TEST_F(GATT_RemoteServiceManagerTest, WriteWithoutResponseNotSupported) {
+  ServiceData data(1, 3, kTestServiceUuid1);
+  auto service = SetUpFakeService(data);
+
+  // No "write without response" property.
+  CharacteristicData chr(0, 2, 3, kTestUuid3);
+  SetupCharacteristics(service, {{chr}});
+
+  bool called = false;
+  fake_client()->set_write_without_rsp_callback(
+      [&](auto, const auto&) { called = true; });
+
+  service->WriteWithoutResponse(0, std::vector<uint8_t>());
+  RunLoopUntilIdle();
+  EXPECT_FALSE(called);
+}
+
+TEST_F(GATT_RemoteServiceManagerTest, WriteWithoutResponseSuccess) {
+  constexpr att::Handle kValueHandle = 3;
+  const std::vector<uint8_t> kValue{{'t', 'e', 's', 't'}};
+
+  ServiceData data(1, kValueHandle, kTestServiceUuid1);
+  auto service = SetUpFakeService(data);
+
+  CharacteristicData chr(Property::kWriteWithoutResponse, 2, kValueHandle,
+                         kTestUuid3);
+  SetupCharacteristics(service, {{chr}});
+
+  bool called = false;
+  fake_client()->set_write_without_rsp_callback(
+      [&](att::Handle handle, const auto& value) {
+        EXPECT_EQ(kValueHandle, handle);
+        EXPECT_TRUE(std::equal(kValue.begin(), kValue.end(), value.begin(),
+                               value.end()));
+        called = true;
+      });
+
+  service->WriteWithoutResponse(0, kValue);
+  RunLoopUntilIdle();
+
+  EXPECT_TRUE(called);
+}
+
 TEST_F(GATT_RemoteServiceManagerTest, EnableNotificationsAfterShutDown) {
   ServiceData data(1, 2, kTestServiceUuid1);
   auto service = SetUpFakeService(data);

vsrinivas/fuchsia

bin/bluetooth/tools/bt-le-central/src/common/gatt.rs
@@ -228,6 +228,19 @@ fn write_characteristic(
         })
 }
 
+fn write_without_response(client: GattClientPtr, id: u64, value: Vec<u8>)
+    -> impl Future<Item = (), Error = Error> {
+    future::result(
+        client
+            .read()
+            .active_proxy
+            .as_ref()
+            .unwrap()
+            .write_characteristic_without_response(id, &mut value.into_iter())
+            .map_err(|_| BTError::new("Failed to send message").into())
+    )
+}
+
 // ===== REPL =====
 
 fn do_help() -> FutureResult<(), Error> {
@@ -317,15 +330,20 @@ fn do_read_chr(args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = (),
     Right(read_characteristic(client, id))
 }
 
-fn do_write_chr(args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = (), Error = Error> {
+fn do_write_chr(mut args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = (), Error = Error> {
     if args.len() < 1 {
-        println!("usage: write-chr <id> <value>");
+        println!("usage: write-chr [-w] <id> <value>");
         return left_ok!();
     }
 
     if client.read().active_proxy.is_none() {
         println!("no service connected");
         return left_ok!();
+    }
+
+    let without_response: bool = args[0] == "-w";
+    if without_response {
+        args.remove(0);
     }
 
     let id: u64 = match args[0].parse() {
@@ -343,7 +361,13 @@ fn do_write_chr(args: Vec<&str>, client: GattClientPtr) -> impl Future<Item = ()
             println!("invalid value");
             left_ok!()
         }
-        Ok(v) => Right(write_characteristic(client, id, v)),
+        Ok(v) => Right(
+            if without_response {
+                Left(write_without_response(client, id, v))
+            } else {
+                Right(write_characteristic(client, id, v))
+            }
+        )
     }
 }
 

drivers/bluetooth/host/fidl/gatt_remote_service_server.cc
@@ -121,6 +121,11 @@ void GattRemoteServiceServer::WriteCharacteristic(
   service_->WriteCharacteristic(id, value.take(), std::move(cb));
 }
 
+void GattRemoteServiceServer::WriteCharacteristicWithoutResponse(
+    uint64_t id, ::fidl::VectorPtr<uint8_t> value) {
+  service_->WriteCharacteristicWithoutResponse(id, value.take());
+}
+
 void GattRemoteServiceServer::NotifyCharacteristic(
     uint64_t id,
     bool enable,

drivers/bluetooth/lib/gatt/remote_service_manager_unittest.cc
@@ -852,6 +852,49 @@ TEST_F(GATT_RemoteServiceManagerTest, WriteCharSendsWriteRequest) {
   EXPECT_EQ(kStatus, status);
 }
 
+TEST_F(GATT_RemoteServiceManagerTest, WriteWithoutResponseNotSupported) {
+  ServiceData data(1, 3, kTestServiceUuid1);
+  auto service = SetUpFakeService(data);
+
+  // No "write without response" property.
+  CharacteristicData chr(0, 2, 3, kTestUuid3);
+  SetupCharacteristics(service, {{chr}});
+
+  bool called = false;
+  fake_client()->set_write_without_rsp_callback(
+      [&](auto, const auto&) { called = true; });
+
+  service->WriteCharacteristicWithoutResponse(0, std::vector<uint8_t>());
+  RunLoopUntilIdle();
+  EXPECT_FALSE(called);
+}
+
+TEST_F(GATT_RemoteServiceManagerTest, WriteWithoutResponseSuccess) {
+  constexpr att::Handle kValueHandle = 3;
+  const std::vector<uint8_t> kValue{{'t', 'e', 's', 't'}};
+
+  ServiceData data(1, kValueHandle, kTestServiceUuid1);
+  auto service = SetUpFakeService(data);
+
+  CharacteristicData chr(Property::kWriteWithoutResponse, 2, kValueHandle,
+                         kTestUuid3);
+  SetupCharacteristics(service, {{chr}});
+
+  bool called = false;
+  fake_client()->set_write_without_rsp_callback(
+      [&](att::Handle handle, const auto& value) {
+        EXPECT_EQ(kValueHandle, handle);
+        EXPECT_TRUE(std::equal(kValue.begin(), kValue.end(), value.begin(),
+                               value.end()));
+        called = true;
+      });
+
+  service->WriteCharacteristicWithoutResponse(0, kValue);
+  RunLoopUntilIdle();
+
+  EXPECT_TRUE(called);
+}
+
 TEST_F(GATT_RemoteServiceManagerTest, EnableNotificationsAfterShutDown) {
   ServiceData data(1, 2, kTestServiceUuid1);
   auto service = SetUpFakeService(data);