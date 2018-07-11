4b9fe27f426a4eed10c6eebc78920020298645fb
James Robinson <jamesr@google.com>
2018-07-11 16:58:15 -0700

    [vulkan] Add a loader service for vulkan client drivers
    This adds a service for the vulkan client loader (libvulkan.so) to
    request the vulkan client driver library for the system. The client
    libraries are shared libraries that must match up with the device
    driver, so they can't be packaged along with the program that wants to
    use vulkan. Instead, a program using vulkan packages the vulkan loader
    which queries this service for the library to load.
    
    For now, the loader just opens the library in /system/lib and returns a
    VMO handle. In the future, this service will load the library from a
    package.
    
    DX-303 #comment
    MA-416 #comment
    
    Test: Run programs that load Vulkan
    
    Change-Id: I9dfe66e3aaf61a3a43508e62bbf8792648917f2b

---
+++ bin/vulkan_loader/main.cc
@@ -0,0 +1,65 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuchsia/vulkan/loader/cpp/fidl.h>
+
+#include <fcntl.h>
+#include <lib/app/cpp/outgoing.h>
+#include <lib/app/cpp/startup_context.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/fdio/io.h>
+#include <lib/fidl/cpp/binding_set.h>
+#include <lib/fxl/command_line.h>
+#include <lib/fxl/log_settings_command_line.h>
+#include <lib/fxl/logging.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// Implements the vulkan loader's Loader service which provides the client
+// driver portion to the loader as a VMO.
+class LoaderImpl : public fuchsia::vulkan::loader::Loader {
+ public:
+  LoaderImpl() = default;
+  ~LoaderImpl() final = default;
+
+  // Adds a binding for fuchsia::vulkan::loader::Loader to |outgoing|
+  void Add(const fuchsia::sys::Outgoing& outgoing) {
+    outgoing.AddPublicService(bindings_.GetHandler(this));
+  }
+
+ private:
+  // fuchsia::vulkan::loader::Loader impl
+  void Get(fidl::StringPtr name, GetCallback callback) {
+    // TODO(MA-470): Load this from a package's data directory, not /system/lib
+    std::string load_path = "/system/lib/" + *name;
+    int fd = open(load_path.c_str(), O_RDONLY);
+    if (fd < 0) {
+      FXL_LOG(ERROR) << "Could not open path " << load_path;
+      callback({});
+      return;
+    }
+    zx::vmo vmo;
+    zx_status_t status = fdio_get_vmo_clone(fd, vmo.reset_and_get_address());
+    close(fd);
+    if (status != ZX_OK) {
+      FXL_LOG(ERROR) << "Could not clone vmo: " << status;
+    }
+    callback(std::move(vmo));
+  }
+
+  fidl::BindingSet<fuchsia::vulkan::loader::Loader> bindings_;
+};
+
+int main(int argc, const char* const* argv) {
+  async::Loop loop(&kAsyncLoopConfigMakeDefault);
+  fxl::SetLogSettingsFromCommandLine(fxl::CommandLineFromArgcArgv(argc, argv));
+
+  auto context = fuchsia::sys::StartupContext::CreateFromStartupInfo();
+
+  LoaderImpl loader_impl;
+  loader_impl.Add(context->outgoing());
+
+  loop.Run();
+  return 0;
+}
