ede89c0f2e74e86edac40b27e02d2fc39fed5508
Fabio Riccardi <fricc@google.com>
2018-07-13 17:56:44 -0700

    [USB Video] Moving usb-video driver to garnet -- fix vmar integration
    zx::vmar::root_self() turned from reference to pointer while the driver was being moved between repositories
    
    TEST = everything builds correctly now
    
    Change-Id: I8f8fc6c47fb038f594ca3881cb165036cb829c1b

--- drivers/usb_video/video-buffer.cpp
+++ drivers/usb_video/video-buffer.cpp
@@ -13,7 +13,7 @@ namespace usb {
 
 VideoBuffer::~VideoBuffer() {
     if (virt_ != nullptr) {
-        zx::vmar::root_self().unmap(reinterpret_cast<uintptr_t>(virt_), size_);
+        zx::vmar::root_self()->unmap(reinterpret_cast<uintptr_t>(virt_), size_);
     }
 }
 
@@ -34,9 +34,9 @@ zx_status_t VideoBuffer::Create(zx::vmo&& vmo,
 
     void* virt;
     uint32_t flags =  ZX_VM_FLAG_PERM_READ | ZX_VM_FLAG_PERM_WRITE;
-    status = zx::vmar::root_self().map(0u, vmo,
-                                       0u, size,
-                                       flags, reinterpret_cast<uintptr_t*>(&virt));
+    status = zx::vmar::root_self()->map(0u, vmo,
+                                        0u, size,
+                                        flags, reinterpret_cast<uintptr_t*>(&virt));
 
     if (status != ZX_OK) {
         zxlogf(ERROR, "failed to map VMO, got error: %d\n", status);
