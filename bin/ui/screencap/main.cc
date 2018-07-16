520e8613e60264b0a7e7dd26936ceba4437c1a35
Gary Miguel <garymm@google.com>
2018-07-16 10:54:27 -0700

    [screencap] Call Scenic::GetDisplayInfo before TakeScreenshot
    This is needed to work around the fact that the TakeScreenshot API is
    not where it should be (see SCN-678), and so it doesn't wait for
    everything to start up nicely like Session-based APIs do.
    
    Test: Ran flutter screencap test locally
    Bug: IN-428 #comment
    Change-Id: If25e81fa459736ea65b684c34135b777d7a8bcaa

--- bin/ui/screencap/main.cc
+++ bin/ui/screencap/main.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 
@@ -25,8 +26,110 @@ const int kRed = 0xd01c8b;
 const int kMinExpectedPixels = 950000;  // Typical value is ~1.5M
 const int kMinPixelsForReport = 50000;
 
+class ScreenshotTaker {
+ public:
+  explicit ScreenshotTaker(async::Loop* loop, bool output_screen)
+      : loop_(loop), output_screen_(output_screen),
+        context_(component::StartupContext::CreateFromStartupInfo()) {
+    // Connect to the Scenic service.
+    scenic_ =
+        context_->ConnectToEnvironmentService<fuchsia::ui::scenic::Scenic>();
+    scenic_.set_error_handler([this] {
+      FXL_LOG(ERROR) << "Lost connection to Scenic service.";
+      encountered_error_ = true;
+      loop_->Quit();
+    });
+  }
+
+  bool encountered_error() const { return encountered_error_; }
+
+  void TakeScreenshot() {
+    // If we wait for a call back from GetDisplayInfo, we are guaranteed that
+    // the GFX system is initialized, which is a prerequisite for taking a
+    // screenshot. TODO(SCN-678): Remove call to GetDisplayInfo once bug done.
+    scenic_->GetDisplayInfo(
+        [this](fuchsia::ui::gfx::DisplayInfo /*unused*/) {
+          TakeScreenshotInternal();
+        });
+  }
+
+ private:
+  void TakeScreenshotInternal() {
+    scenic_->TakeScreenshot([this](
+                               fuchsia::ui::scenic::ScreenshotData screenshot,
+                               bool status) {
+      std::vector<uint8_t> imgdata;
+      if (!status || !fsl::VectorFromVmo(screenshot.data, &imgdata)) {
+        FXL_LOG(ERROR) << "TakeScreenshot failed";
+        encountered_error_ = true;
+        loop_->Quit();
+        return;
+      }
+
+      std::map<int, int> histogram;
+      if (output_screen_) {
+        std::cout << "P6\n";
+        std::cout << screenshot.info.width << "\n";
+        std::cout << screenshot.info.height << "\n";
+        std::cout << 255 << "\n";
+      }
+
+      const uint8_t* pchannel = &imgdata[0];
+      for (uint32_t pixel = 0;
+           pixel < screenshot.info.width * screenshot.info.height; pixel++) {
+        uint8_t rgb[] = {pchannel[2], pchannel[1], pchannel[0]};
+        if (output_screen_) {
+          std::cout.write(reinterpret_cast<const char*>(rgb), 3);
+        } else {
+          int rgb_value = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
+          if (histogram.find(rgb_value) != histogram.end()) {
+            histogram[rgb_value] = histogram[rgb_value] + 1;
+          } else {
+            histogram[rgb_value] = 1;
+          }
+        }
+        pchannel += 4;
+      }
+      if (!output_screen_) {
+        // For success, there should be at least 1M green or red pixels combined
+        // The typical number is > 1.5M
+        if (histogram[kGreen] + histogram[kRed] > kMinExpectedPixels) {
+          printf("success\n");
+        } else {
+          printf("failure\n");
+          printf("black: %d, white: %d, green: %d, red: %d\n",
+                 histogram[kBlack], histogram[kWhite], histogram[kGreen],
+                 histogram[kRed]);
+          // To help debug failures, if the majority of values aren't already
+          // expected, output the values that were over a threshold count.
+          if (histogram[kBlack] + histogram[kWhite] + histogram[kGreen] +
+                  histogram[kRed] <
+              kMinExpectedPixels) {
+            const int MIN_REPORT_THRESHOLD = kMinPixelsForReport;
+            std::map<int, int>::iterator it;
+            for (const auto& pair : histogram) {
+              if (pair.second > MIN_REPORT_THRESHOLD) {
+                printf("Pixel 0x%06x occurred %d times\n", pair.first,
+                       pair.second);
+              }
+            }
+          }
+          encountered_error_ = true;
+        }
+      }
+      loop_->Quit();
+    });
+  }
+  async::Loop* loop_;
+  const bool output_screen_;
+  std::unique_ptr<component::StartupContext> context_;
+  bool encountered_error_ = false;
+  fuchsia::ui::scenic::ScenicPtr scenic_;
+};
+
+
+
 int main(int argc, const char** argv) {
-  int return_code = 0;
   bool output_screen = true;
   auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
   if (!fxl::SetLogSettingsFromCommandLine(command_line))
@@ -49,80 +152,10 @@ int main(int argc, const char** argv) {
 
   async::Loop loop(&kAsyncLoopConfigMakeDefault);
   trace::TraceProvider trace_provider(loop.dispatcher());
-  std::unique_ptr<component::StartupContext> app_context(
-      component::StartupContext::CreateFromStartupInfo());
-  // Connect to the SceneManager service.
-  auto scenic =
-      app_context->ConnectToEnvironmentService<fuchsia::ui::scenic::Scenic>();
-  scenic.set_error_handler([&loop, &return_code] {
-    FXL_LOG(ERROR) << "Lost connection to Scenic service.";
-    return_code = 1;
-    loop.Quit();
-  });
-  scenic->TakeScreenshot([&loop, &return_code, output_screen](
-                             fuchsia::ui::scenic::ScreenshotData screenshot,
-                             bool status) {
-    std::vector<uint8_t> imgdata;
-    if (!status || !fsl::VectorFromVmo(screenshot.data, &imgdata)) {
-      FXL_LOG(ERROR) << "TakeScreenshot failed";
-      return_code = 1;
-      loop.Quit();
-      return;
-    }
 
-    std::map<int, int> histogram;
-    if (output_screen) {
-      std::cout << "P6\n";
-      std::cout << screenshot.info.width << "\n";
-      std::cout << screenshot.info.height << "\n";
-      std::cout << 255 << "\n";
-    }
-
-    const uint8_t* pchannel = &imgdata[0];
-    for (uint32_t pixel = 0;
-         pixel < screenshot.info.width * screenshot.info.height; pixel++) {
-      uint8_t rgb[] = {pchannel[2], pchannel[1], pchannel[0]};
-      if (output_screen) {
-        std::cout.write(reinterpret_cast<const char*>(rgb), 3);
-      } else {
-        int rgb_value = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
-        if (histogram.find(rgb_value) != histogram.end()) {
-          histogram[rgb_value] = histogram[rgb_value] + 1;
-        } else {
-          histogram[rgb_value] = 1;
-        }
-      }
-      pchannel += 4;
-    }
-    if (!output_screen) {
-      // For success, there should be at least 1M green or red pixels combined
-      // The typical number is > 1.5M
-      if (histogram[kGreen] + histogram[kRed] > kMinExpectedPixels) {
-        printf("success\n");
-      } else {
-        printf("failure\n");
-        printf("black: %d, white: %d, green: %d, red: %d\n", histogram[kBlack],
-               histogram[kWhite], histogram[kGreen], histogram[kRed]);
-        // To help debug failures, if the majority of values aren't already
-        // expected, output the values that were over a threshold count.
-        if (histogram[kBlack] + histogram[kWhite] + histogram[kGreen] +
-                histogram[kRed] <
-            kMinExpectedPixels) {
-          const int MIN_REPORT_THRESHOLD = kMinPixelsForReport;
-          std::map<int, int>::iterator it;
-          for (const auto& pair : histogram) {
-            if (pair.second > MIN_REPORT_THRESHOLD) {
-              printf("Pixel 0x%06x occurred %d times\n", pair.first,
-                     pair.second);
-            }
-          }
-        }
-        return_code = 30;
-      }
-    }
-    loop.Quit();
-  });
+  ScreenshotTaker taker(&loop, output_screen);
+  taker.TakeScreenshot();
   loop.Run();
 
-  return return_code;
+  return taker.encountered_error() ? EXIT_FAILURE : EXIT_SUCCESS;
 }
