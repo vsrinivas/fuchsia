38fb3f58a091ad37155223021be25143e280721c
Leo Lung <leolung@google.com>
2018-07-17 11:18:23 -0700

    [components] Example of 2 binaries in a pkg
    The following executions behave as expected:
    
    $ pkgfs/packages/component_hello_world/0/bin/app
    Hello, World!
    
    $ pkgfs/packages/component_hello_world/0/bin/app2
    Hello, World2!
    
    $ run component_hello_world
    [01993.739] 01103.01117> svchost: warning: Using deprecated path to
    fuchsia.sys.Launcher.
    Hello, World!
    
    $ run component_hello_world2
    [02001.066] 01103.01117> svchost: warning: Using deprecated path to
    fuchsia.sys.Launcher.
    [02001.082] 03150.03211> [ERROR:garnet/bin/appmgr/root_loader.cc(71)]
    Could not load url: file://component_hello_world2
    component_hello_world2 exited without a return code
    
    CP-19 #progress
    
    TESTED=none
    
    Change-Id: I57937b826225370563ffa4b8f8c4795925ba152a

---
+++ examples/components/hello_world.cc
@@ -0,0 +1,11 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, World!\n";
+  return 0;
+}
