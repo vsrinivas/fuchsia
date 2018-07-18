47d6320c6a45038d34e172fb8aaaedfca51553d6
Ankur Mittal <anmittal@google.com>
2018-07-17 17:36:16 -0700

    [logger] Make reading klogs more efficient.
    This change was orignally made as a temporary solution for CP-101 but
    that issue is fixed by
    https://fuchsia-review.googlesource.com/c/garnet/+/175918.
    
    Landing this change as this is a better way of doing things.
    
    TEST=logger integration tests
    
    Change-Id: Ie9b1a105c2af8e7ec115611a2c5923c4ef12ce85

--- bin/logger/src/klogger.rs
+++ bin/logger/src/klogger.rs
@@ -31,45 +31,54 @@ where
     let f = loop_fn((callback, h), |(callback, handle)| {
         // TODO: change OnSignals to wrap this so that is is not created again and again.
         async::OnSignals::new(&handle, Signals::LOG_READABLE).and_then(|_| {
-            let mut buf = [0; ZX_LOG_RECORD_MAX as usize];
-            let status = unsafe {
-                zx::sys::zx_log_read(handle.raw_handle(), ZX_LOG_RECORD_MAX, buf.as_mut_ptr(), 0)
-            };
+            loop {
+                let mut buf = [0; ZX_LOG_RECORD_MAX as usize];
+                let status = unsafe {
+                    zx::sys::zx_log_read(
+                        handle.raw_handle(),
+                        ZX_LOG_RECORD_MAX,
+                        buf.as_mut_ptr(),
+                        0,
+                    )
+                };
 
-            if status == zx::sys::ZX_ERR_SHOULD_WAIT {
-                return Ok(Loop::Continue((callback, handle)));
-            }
-            if status < 0 {
-                return Err(zx::Status::from_raw(status));
-            }
-            let mut l = LogMessage {
-                time: LittleEndian::read_u64(&buf[8..16]),
-                pid: LittleEndian::read_u64(&buf[16..24]),
-                tid: LittleEndian::read_u64(&buf[24..32]),
-                severity: fidl_fuchsia_logger::LogLevelFilter::Info as i32,
-                dropped_logs: 0,
-                tags: vec!["klog".to_string()],
-                msg: String::new(),
-            };
-            let data_len = LittleEndian::read_u16(&buf[4..8]) as usize;
-            l.msg = match String::from_utf8(buf[32..(32 + data_len)].to_vec()) {
-                Err(e) => {
-                    eprintln!("invalid log record: {:?}", e);
+                if status == zx::sys::ZX_ERR_SHOULD_WAIT {
                     return Ok(Loop::Continue((callback, handle)));
                 }
-                Ok(s) => s,
-            };
-            if l.msg.len() > 0 && l.msg.as_bytes()[l.msg.len() - 1] == '\n' as u8 {
-                l.msg.pop();
+                if status < 0 {
+                    return Err(zx::Status::from_raw(status));
+                }
+                let mut l = LogMessage {
+                    time: LittleEndian::read_u64(&buf[8..16]),
+                    pid: LittleEndian::read_u64(&buf[16..24]),
+                    tid: LittleEndian::read_u64(&buf[24..32]),
+                    severity: fidl_fuchsia_logger::LogLevelFilter::Info as i32,
+                    dropped_logs: 0,
+                    tags: vec!["klog".to_string()],
+                    msg: String::new(),
+                };
+                let data_len = LittleEndian::read_u16(&buf[4..8]) as usize;
+                l.msg = match String::from_utf8(buf[32..(32 + data_len)].to_vec()) {
+                    Err(e) => {
+                        eprintln!("logger: invalid log record: {:?}", e);
+                        continue;
+                    }
+                    Ok(s) => s,
+                };
+                if l.msg.len() > 0 && l.msg.as_bytes()[l.msg.len() - 1] == '\n' as u8 {
+                    l.msg.pop();
+                }
+                let size = logger::METADATA_SIZE + 5/*tag*/ + l.msg.len() + 1;
+                callback(l, size);
             }
-            let size = logger::METADATA_SIZE + 5/*tag*/ + l.msg.len() + 1;
-            callback(l, size);
-            Ok(Loop::Continue((callback, handle)))
         })
     });
 
     async::spawn(f.recover(|e| {
-        eprintln!("not able to apply listener to kernel logs, failed: {:?}", e)
+        eprintln!(
+            "logger: not able to apply listener to kernel logs, failed: {:?}",
+            e
+        )
     }));
 
     Ok(())
