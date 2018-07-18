1850ae29197667c7372bf55a49ea23a1b2e28246
Leo Lung <leolung@google.com>
2018-07-18 14:32:45 -0700

    [appmgr] Refactor root_loader for easier reading
    Make it have less indentation, and add some comments according to my
    understanding.
    
    TESTED=appmgr_integration_tests, ran garnet/default
    
    Change-Id: Ib09406f5d5036c305dd982df130a7c6bedb8243a

--- bin/appmgr/root_loader.cc
+++ bin/appmgr/root_loader.cc
@@ -26,52 +26,77 @@ void RootLoader::LoadComponent(fidl::StringPtr url,
                                LoadComponentCallback callback) {
   std::string path = GetPathFromURL(url);
   if (path.empty()) {
-    // TODO(abarth): Support URL schemes other than file:// by querying the host
-    // for an application runner.
+    // If we see a scheme other than file://, it's an error. Other schemes are
+    // handled by CreateComponent, which invokes the appropriate runner.
     FXL_LOG(ERROR) << "Cannot load " << url
                    << " because the scheme is not supported.";
-  } else {
-    fxl::UniqueFD fd(open(path.c_str(), O_RDONLY));
-    if (!fd.is_valid() && path[0] != '/') {
-      if (path.find('/') == std::string::npos) {
-        // TODO(abarth): We're currently hardcoding version 0 of the package,
-        // but we'll eventually need to do something smarter.
-        std::string pkg_path =
-            fxl::Concatenate({"/pkgfs/packages/", path, "/0"});
-        fd.reset(open(pkg_path.c_str(), O_DIRECTORY | O_RDONLY));
-        if (fd.is_valid()) {
-          zx::channel directory = fsl::CloneChannelFromFileDescriptor(fd.get());
-          if (directory) {
-            fuchsia::sys::Package package;
-            package.directory = std::move(directory);
-            package.resolved_url = fxl::Concatenate({"file://", pkg_path});
-            callback(fidl::MakeOptional(std::move(package)));
-            return;
-          }
-        }
-      }
-      for (const auto& entry : {"/system/bin", "/system/pkgs"}) {
-        std::string qualified_path =
-            fxl::Concatenate({fxl::StringView(entry), "/", path});
-        fd.reset(open(qualified_path.c_str(), O_RDONLY));
-        if (fd.is_valid()) {
-          path = qualified_path;
-          break;
-        }
-      }
-    }
-    fsl::SizedVmo data;
-    if (fd.is_valid() && fsl::VmoFromFd(std::move(fd), &data)) {
-      fuchsia::sys::Package package;
-      package.data = fidl::MakeOptional(std::move(data).ToTransport());
-      package.resolved_url = fxl::Concatenate({"file://", path});
-      callback(fidl::MakeOptional(std::move(package)));
-      return;
-    }
-    FXL_LOG(ERROR) << "Could not load url: " << url;
+    callback(nullptr);
+    return;
   }
 
-  callback(nullptr);
+  // 1. Try to load the URL directly, if the path is valid. If the path is valid
+  // but we cannot load the component, exit immediately.
+  fxl::UniqueFD fd(open(path.c_str(), O_RDONLY));
+  if (fd.is_valid() || path[0] == '/') {
+    if (!LoadComponentWithProcess(fd, path, callback)) {
+      FXL_LOG(ERROR) << "Could not load url: " << url;
+      callback(nullptr);
+    }
+    return;
+  }
+
+  // 2. Try to load the URL from /pkgfs.
+  if (path.find('/') == std::string::npos &&
+      LoadComponentFromPkgfs(path, callback)) {
+    return;
+  }
+
+  // 3. Try to load the URL from /system, if we cannot from /pkgfs.
+  for (const auto& entry : {"/system/bin", "/system/pkgs"}) {
+    std::string qualified_path =
+        fxl::Concatenate({fxl::StringView(entry), "/", path});
+    fd.reset(open(qualified_path.c_str(), O_RDONLY));
+    if (fd.is_valid()) {
+      path = qualified_path;
+      break;
+    }
+  }
+  if (!fd.is_valid() || !LoadComponentWithProcess(fd, path, callback)) {
+    FXL_LOG(ERROR) << "Could not load url: " << url;
+    callback(nullptr);
+  }
+}
+
+bool RootLoader::LoadComponentFromPkgfs(std::string& path,
+                                        LoadComponentCallback callback) {
+  // TODO(abarth): We're currently hardcoding version 0 of the package,
+  // but we'll eventually need to do something smarter.
+  std::string pkg_path = fxl::Concatenate({"/pkgfs/packages/", path, "/0"});
+  fxl::UniqueFD fd(open(pkg_path.c_str(), O_DIRECTORY | O_RDONLY));
+  if (fd.is_valid()) {
+    zx::channel directory = fsl::CloneChannelFromFileDescriptor(fd.get());
+    if (directory) {
+      fuchsia::sys::Package package;
+      package.directory = std::move(directory);
+      package.resolved_url = fxl::Concatenate({"file://", pkg_path});
+      callback(fidl::MakeOptional(std::move(package)));
+      return true;
+    }
+  }
+  return false;
+}
+
+bool RootLoader::LoadComponentWithProcess(fxl::UniqueFD& fd, std::string& path,
+                                          LoadComponentCallback callback) {
+  fsl::SizedVmo data;
+  if (fsl::VmoFromFd(std::move(fd), &data)) {
+    fuchsia::sys::Package package;
+    package.data = fidl::MakeOptional(std::move(data).ToTransport());
+    package.resolved_url = fxl::Concatenate({"file://", path});
+    callback(fidl::MakeOptional(std::move(package)));
+    return true;
+  }
+  return false;
 }
 
 void RootLoader::AddBinding(
--- bin/appmgr/root_loader.h
+++ bin/appmgr/root_loader.h
@@ -9,22 +9,33 @@
 #include <lib/zx/vmo.h>
 
 #include "lib/fidl/cpp/binding_set.h"
+#include "lib/fsl/io/fd.h"
 #include "lib/fxl/macros.h"
 
 namespace component {
 
+// Class to load components at their provided URLs.
 class RootLoader : public fuchsia::sys::Loader {
  public:
   explicit RootLoader();
   ~RootLoader() override;
 
+  // Tries to locate a component at the given URL, and then invokes the callback
+  // on said component.
   void LoadComponent(fidl::StringPtr url,
                      LoadComponentCallback callback) override;
 
+  // Binds FIDL requests to Loader.
   void AddBinding(fidl::InterfaceRequest<fuchsia::sys::Loader> request);
 
  private:
   fidl::BindingSet<fuchsia::sys::Loader> bindings_;
+
+  bool LoadComponentFromPkgfs(std::string& path,
+                              LoadComponentCallback callback);
+
+  bool LoadComponentWithProcess(fxl::UniqueFD& fd, std::string& path,
+                                LoadComponentCallback callback);
 
   FXL_DISALLOW_COPY_AND_ASSIGN(RootLoader);
 };
