d4cabeeb6b4541c0834061744b1d2cedecf8da31
Zac Bowling <zbowling@google.com>
2018-07-23 15:58:39 -0700

    [bt][hog] Implement HID over GATT driver
    NET-1002
    
    Test: NET-1002
    Change-Id: I50c52e41b977e9bfd5c448f6275e88b83724dc6f

---
+++ drivers/bluetooth/profiles/gatt/hid/binding.c
@@ -0,0 +1,21 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <ddk/binding.h>
+#include <ddk/device.h>
+#include <ddk/driver.h>
+
+#include <zircon/types.h>
+extern zx_status_t bthog_bind(void* ctx, zx_device_t* device);
+
+static zx_driver_ops_t bthog_driver_ops = {
+    .version = DRIVER_OPS_VERSION,
+    .bind = bthog_bind
+};
+
+// clang-format off
+ZIRCON_DRIVER_BEGIN(bthog, bthog_driver_ops, "fuchsia", "0.1", 2)
+  BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_BT_GATT_SVC),
+  BI_MATCH_IF(EQ, BIND_BT_GATT_SVC_UUID16, 0x1812),
+ZIRCON_DRIVER_END(bthog)
