6d2f8f07bf801fd106b0c2be76af90bfb03a1ce8
Przemyslaw Pietrzkiewicz <ppi@google.com>
2018-07-24 16:40:02 +0200

    [cloud_dashboard] New cloud dashboard compatible with Firestore.
    This patch replaces the current cloud dashboard in-place with a new
    version, compatible with the new Firestore cloud provider.
    
    LE-530 #done
    
    Test: `ng serve` in web/cloud_dashboard then try deleting data.
    Change-Id: Id860271ad5c89a8ac3cba4ab0f2a163bfa18249e

--- web/cloud_dashboard/src/app/app.component.ts
+++ web/cloud_dashboard/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import {Component} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
-import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentChangeAction, QueryDocumentSnapshot} from 'angularfire2/firestore';
 import * as firebase from 'firebase/app';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {from, Observable, Observer} from 'rxjs';
+import {bufferCount, concat, concatMap, map, mergeMap, take, tap, toArray} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,28 +11,33 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ledger Dashboard';
-  delayBetweenEraseStagesMillis = 3000;
+  readonly title = 'Ledger Cloud Dashboard';
+  readonly batchSize = 25;
+
   authenticated = false;
   uid = null;
-  version = 0;
+  version = '0';
   loading = true;
   deleteInProgress = false;
 
   user: Observable<firebase.User>;
-  devices: Observable<any[]>;
-  root: AngularFireList<any[]>;
+  usersCollection: AngularFirestoreCollection;
+  userDocument: AngularFirestoreDocument<any>;
+  versionsCollection: AngularFirestoreCollection;
+  versionDocument: AngularFirestoreDocument<any>;
+  devicesCollection: AngularFirestoreCollection;
+  devices: Observable<any>;
+  allCollections: Array<AngularFirestoreCollection> = [];
+  deletedDocumentCount = 0;
 
-  constructor(public afAuth: AngularFireAuth, private db: AngularFireDatabase) {
+  constructor(public afAuth: AngularFireAuth, private db: AngularFirestore) {
     this.user = afAuth.authState;
 
     this.afAuth.authState.subscribe((state) => {
       this.authenticated = state != null;
       if (this.authenticated) {
         this.uid = state.uid;
-        this.registerWatchers();
-      } else {
-        this.unregisterWatchers();
+        this.loadVersions();
       }
     });
   }
@@ -48,7 +53,13 @@ export class AppComponent {
   }
 
   rootPath() {
-    return '/__default__V/' + this.uid;
+    return 'users/' + this.uid;
+  }
+
+  versionDocumentPath(version: number) {
+    let path = `${this.rootPath()}/${version}/default_document`;
+    console.log(path);
+    return path;
   }
 
   versionPath() {
@@ -56,44 +67,125 @@ export class AppComponent {
   }
 
   userDevicesPath() {
-    return this.versionPath() + '/__metadata/devices';
+    return this.versionPath() + '/default_document/devices';
   }
 
-  registerWatchers() {
-    this.root = this.db.list(this.rootPath());
-    this.root.snapshotChanges()
-        .pipe(map(
-            actions => actions.map(a => ({key: a.key, ...a.payload.val()}))))
-        .subscribe(version_objects => {
-          let next_version = 0;
-          for (const version_object of version_objects) {
-            if (Number(version_object.key) > Number(next_version)) {
-              next_version = Number(version_object.key);
+  loadVersions() {
+    this.usersCollection = this.db.collection('users');
+    this.userDocument = this.usersCollection.doc(this.uid);
+    this.versionsCollection = this.userDocument.collection('versions');
+    this.versionsCollection.snapshotChanges()
+        .pipe(this.unwrap())
+        .subscribe(versionObjects => {
+          let nextVersion = '0';
+          for (const versionObject of versionObjects) {
+            if (Number(versionObject.id) > Number(nextVersion)) {
+              nextVersion = versionObject.id;
             }
           }
-          this.version = next_version;
-          this.devices =
-              this.db.list(this.userDevicesPath())
-                  .snapshotChanges()
-                  .pipe(
-                      map(actions => actions.map(
-                              a => ({key: a.key, value: a.payload.val()}))));
+          this.version = nextVersion;
+          this.versionDocument = this.versionsCollection.doc(this.version);
           this.loading = false;
-          return version_objects.map(version_object => version_object.key);
+          this.loadDevices();
+          this.loadAllCollections();
         });
   }
 
-  unregisterWatchers() {
-    this.devices = null;
-    this.root = null;
+  loadDevices() {
+    this.devicesCollection = this.versionDocument.collection('devices');
+    this.devices = this.devicesCollection.snapshotChanges().pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        })));
+  }
+
+  // Assembles the list of all collections of the user.
+  loadAllCollections() {
+    const schema = {
+      'devices': {},
+      'namespaces': {'pages': {'commit-log': {}, 'objects': {}}}
+    };
+    this.loadNested(this.versionsCollection, schema)
+        .pipe(toArray())
+        .subscribe((result) => this.allCollections = result);
+  }
+
+  // Returns an observable which recursively streams nested collections under
+  // |collection| according to the given schema.
+  loadNested(collection: AngularFirestoreCollection, schema):
+      Observable<AngularFirestoreCollection> {
+    return collection.snapshotChanges().pipe(
+        this.unwrap(), take(1),
+
+        mergeMap(documents => {
+          let result = [from([collection])];
+          for (const document of documents) {
+            for (const subcollectionName in schema) {
+              const subcollection =
+                  collection.doc(document.id).collection(subcollectionName);
+              const subcollectionObservable =
+                  this.loadNested(subcollection, schema[subcollectionName]);
+              result.push(subcollectionObservable);
+            }
+          }
+          return result;
+        }),
+        mergeMap((a) => a));
+  }
+
+  // Pipeable operator which unwraps document snapshots so that they contain the
+  // key as |id| along with data fields.
+  unwrap() {
+    return map((actions: DocumentChangeAction<any>[]) => actions.map(a => {
+      const data = a.payload.doc.data();
+      const id = a.payload.doc.id;
+      return {id, ...data};
+    }))
   }
 
   erase() {
     this.deleteInProgress = true;
-    this.db.list(this.userDevicesPath()).remove();
-    setTimeout(() => {
-      this.root.remove();
-      this.deleteInProgress = false;
-    }, this.delayBetweenEraseStagesMillis);
+    this.deletedDocumentCount = 0;
+    this.eraseCollections(this.allCollections);
+  }
+
+  // Erases all documents in the given collection in batches.
+  eraseCollections(collections: Array<AngularFirestoreCollection>) {
+    from(collections)
+        .pipe(
+            concatMap(
+                collection => collection.snapshotChanges().pipe(
+                    take(1), concatMap((v) => v))),
+            bufferCount(this.batchSize),
+            concatMap((documents) => this.deleteDocs(documents)))
+        .subscribe(
+            (count: number) => {
+              this.deletedDocumentCount += count;
+            },
+            (err) => {
+              console.log(err);
+            },
+            () => {
+              this.deleteInProgress = false;
+              this.loadVersions();
+            });
+  }
+
+  // Returns an Observable which deletes the given documents and streams the
+  // number of deleted documents.
+  deleteDocs(documents: DocumentChangeAction<any>[]): Observable<number> {
+    let result = Observable.create((o: Observer<number>) => {
+      let batch = this.db.firestore.batch();
+      for (const document in documents) {
+        batch.delete(documents[document].payload.doc.ref);
+      }
+      batch.commit().then(() => {
+        o.next(documents.length);
+        o.complete();
+      });
+    });
+    return result;
   }
 }
--- web/cloud_dashboard/src/app/app.module.ts
+++ web/cloud_dashboard/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatProgressSpinnerModule, MatToolbarModule} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 
 import {environment} from '../environments/environment';
 
@@ -12,9 +12,14 @@ import {AppComponent} from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule, AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule, AngularFireAuthModule, MatButtonModule,
-    MatCardModule, MatToolbarModule
+    BrowserModule,                                          //
+    AngularFireModule.initializeApp(environment.firebase),  //
+    AngularFirestoreModule,                                 //
+    AngularFireAuthModule,                                  //
+    MatButtonModule,                                        //
+    MatCardModule,                                          //
+    MatProgressSpinnerModule,                               //
+    MatToolbarModule                                        //
   ],
   providers: [],
   bootstrap: [AppComponent]
