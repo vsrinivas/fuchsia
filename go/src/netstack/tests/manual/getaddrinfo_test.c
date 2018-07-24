66dfb0fd4a0f3d9c389e40e58ba2cf5b8af9da91
Toshi Kikuchi <toshik@google.com>
2018-07-24 13:50:57 -0700

    [netstack][test] update getaddrinfo_test to improve the coverage
    NET-1187
    
    Test: manually runs getaddrinfo_test with different args
    Change-Id: I8ec0b1bb6d4765cf1d5db76a767a2e4bb6f3beb4

--- go/src/netstack/tests/manual/getaddrinfo_test.c
+++ go/src/netstack/tests/manual/getaddrinfo_test.c
@@ -4,44 +4,98 @@
 
 #include <arpa/inet.h>
 #include <errno.h>
+#include <getopt.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-const char* sa_to_str(const struct sockaddr* sa, char* str, size_t strlen) {
+const char* sa_to_string(const struct sockaddr* sa, char* buf, size_t buflen) {
+  if (sa == NULL) {
+    return "<null>";
+  }
+
+  char addrstr[INET6_ADDRSTRLEN];
   if (sa->sa_family == AF_INET) {
     struct sockaddr_in* sin = (struct sockaddr_in*)sa;
-    return inet_ntop(AF_INET, &sin->sin_addr, str, strlen);
+    size_t n = snprintf(buf, buflen,
+                        "inet4: %s (port %d)\n",
+                        inet_ntop(AF_INET, &sin->sin_addr, addrstr, sizeof(addrstr)),
+                        ntohs(sin->sin_port));
+    if (n >= buflen) {
+      return "<error: buffer overflow>";
+    }
+    return buf;
   } else if (sa->sa_family == AF_INET6) {
     struct sockaddr_in6* sin6 = (struct sockaddr_in6*)sa;
-    return inet_ntop(AF_INET6, &sin6->sin6_addr, str, strlen);
-  } else {
-    return NULL;
+    size_t n = snprintf(buf, buflen,
+                        "inet6: %s (port %d)\n",
+                        inet_ntop(AF_INET6, &sin6->sin6_addr, addrstr, sizeof(addrstr)),
+                        ntohs(sin6->sin6_port));
+    if (n >= buflen) {
+      return "<error: buffer overflow>";
+    }
+    return buf;
   }
+  return "<error: unknown family>";
 }
 
 void print_ai(struct addrinfo* ai) {
-  char str[INET6_ADDRSTRLEN];
-  printf("family %d, socktype %d, flags %d, protocol %d, addrlen %d, addr %s\n",
+  char sa_string_buf[256 + INET6_ADDRSTRLEN];
+  printf("family %d, socktype %d, flags %d, protocol %d, addrlen %d, addr %s",
          ai->ai_family, ai->ai_socktype, ai->ai_flags, ai->ai_protocol,
          ai->ai_addrlen,
-         ai->ai_addr ? sa_to_str(ai->ai_addr, str, sizeof(str)) : "NULL");
+         sa_to_string(ai->ai_addr, sa_string_buf, sizeof(sa_string_buf)));
 }
 
-int getaddrinfo_test(const char* address, int family) {
-  switch (family) {
-    case AF_UNSPEC:
-      printf("looking up %s ...\n", address);
-      break;
-    case AF_INET:
-      printf("looking up %s for v4 (family %d)...\n", address, family);
-      break;
-    case AF_INET6:
-      printf("looking up %s for v6 (family %d)...\n", address, family);
-      break;
+const char *name_to_string(const char* name) {
+  if (name == NULL) {
+    return "<null>";
+  } else {
+    return name;
   }
+}
+
+const char *family_to_string(int family) {
+  if (family == AF_UNSPEC) {
+  } else if (family == AF_INET) {
+    return "inet4";
+  } else if (family == AF_INET6) {
+    return "inet6";
+  }
+  return "unknown";
+}
+
+const char *eai_to_string(int eai) {
+  switch (eai) {
+  case EAI_BADFLAGS:
+    return "EAI_BADFLAGS";
+  case EAI_NONAME:
+    return "EAI_NONAME";
+  case EAI_AGAIN:
+    return "EAI_AGAIN";
+  case EAI_FAIL:
+    return "EAI_FAIL";
+  case EAI_FAMILY:
+    return "EAI_FAMILY";
+  case EAI_SOCKTYPE:
+    return "EAI_SOCKTYPE";
+  case EAI_SERVICE:
+    return "EAI_SERVICE";
+  case EAI_MEMORY:
+    return "EAI_MEMORY";
+  case EAI_SYSTEM:
+    return "EAI_SYSTEM";
+  case EAI_OVERFLOW:
+    return "EAI_OVERFLOW";
+  }
+  return "<unknown error>";
+}
+
+int getaddrinfo_test(const char* node, const char* service, int family) {
+  printf("looking up node=%s service=%s family=%s...\n",
+         name_to_string(node), name_to_string(service), family_to_string(family));
 
   struct addrinfo hints;
   struct addrinfo *result, *rp;
@@ -50,10 +104,14 @@ int getaddrinfo_test(const char* address, int family) {
   hints.ai_family = family;
   hints.ai_socktype = SOCK_STREAM;
 
-  int r;
-  r = getaddrinfo(address, NULL, &hints, &result);
-  if (r != 0) {
-    printf("getaddrinfo failed (%d, errno = %d)\n", r, errno);
+  int eai;
+  eai = getaddrinfo(node, service, &hints, &result);
+  if (eai != 0) {
+    if (eai == EAI_SYSTEM) {
+      printf("getaddrinfo failed (%s, errno = %d)\n", eai_to_string(eai), errno);
+    } else {
+      printf("getaddrinfo failed (%s)\n", eai_to_string(eai));
+    }
     return -1;
   }
 
@@ -67,30 +125,41 @@ int getaddrinfo_test(const char* address, int family) {
   return 0;
 }
 
-void usage() { printf("usage: getaddrinfo_test address [4 or 6]\n"); }
+void usage() {
+  printf("usage: getaddrinfo_test [-n node][-s service][-f inet4 or inet6]\n");
+}
 
 int main(int argc, char** argv) {
-  if (argc != 2 && argc != 3) {
-    usage();
-    return 1;
-  }
-
-  char* name = argv[1];
-
+  char *node = NULL;
+  char *service = NULL;
   int family = AF_UNSPEC;
-  if (argc == 3) {
-    int n = atoi(argv[2]);
-    if (n == 4) {
-      family = AF_INET;
-    } else if (n == 6) {
-      family = AF_INET6;
-    } else {
+
+  int opt;
+  while ((opt = getopt(argc, argv, "n:s:f:")) != -1) {
+    switch (opt) {
+    case 'n':
+      node = optarg;
+      break;
+    case 's':
+      service = optarg;
+      break;
+    case 'f':
+      if (strcmp(optarg, "inet4") == 0) {
+        family = AF_INET;
+      } else if (strcmp(optarg, "inet6") == 0) {
+        family = AF_INET6;
+      } else {
+        usage();
+        return 1;
+      }
+      break;
+    default:
       usage();
       return 1;
     }
   }
 
-  if (getaddrinfo_test(name, family) < 0) {
+  if (getaddrinfo_test(node, service, family) < 0) {
     return 1;
   }
 
