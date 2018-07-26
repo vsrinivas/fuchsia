fd66692bd94cd6e632c5ca2703de80b6527dc4e0
Abdulla Kamar <abdulla@google.com>
2018-07-27 07:52:33 +1000

    [zircon_guest] Silence mkgpt.sh
    This reduces the build spew from mkgpt.sh.
    
    BLD-229 #done
    
    Test: Built zircon_guest.
    Change-Id: I5f74792019a3128ba6f83f6258bb1ea69ec5617e

--- bin/guest/pkg/zircon_guest/mkgpt.sh
+++ bin/guest/pkg/zircon_guest/mkgpt.sh
@@ -9,31 +9,25 @@
 set -eo pipefail
 
 ZIRCON_GUEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUILDDIR="${ZIRCON_GUEST_DIR}/../../../../../out"
-MINFS="${BUILDDIR}/build-zircon/tools/minfs"
+BUILD_DIR="${ZIRCON_GUEST_DIR}/../../../../../out"
+MINFS="${BUILD_DIR}/build-zircon/tools/minfs"
 
 usage() {
-    echo "usage: ${0} [-f] {x64|arm64}"
+    echo "usage: ${0} {x64|arm64}"
     echo ""
-    echo "    -f Force a rebuild even if the artifact already exists."
     echo "    -o Output directory for image files."
     echo ""
     exit 1
 }
 
-declare FORCE="${FORCE:-false}"
-declare OUTDIR="${BUILDDIR}"
-
-while getopts "fo:" opt; do
+while getopts "o:" opt; do
   case "${opt}" in
-    f) FORCE="true" ;;
-    o) OUTDIR=${OPTARG} ;;
+    o) OUT_DIR=${OPTARG} ;;
     *) usage ;;
   esac
 done
 shift $((OPTIND - 1))
 
-readonly "${FORCE}"
 case "${1}" in
 arm64|x64)
   ARCH="$1";;
@@ -41,12 +35,12 @@ arm64|x64)
   usage;;
 esac
 
-
+declare -r OUT_DIR="${OUT_DIR:-${BUILD_DIR}}"
 # Zircon's block-watcher will auto mount GPT partitions with this GUID as
 # the system partition.
 declare -r ZIRCON_SYSTEM_GUID="606B000B-B7C7-4653-A7D5-B737332C899D"
-declare -r ZIRCON_GPT_IMAGE="${OUTDIR}/zircon.gpt"
-declare -r ZIRCON_SYSTEM_IMAGE="${OUTDIR}/system.minfs"
+declare -r ZIRCON_GPT_IMAGE="${OUT_DIR}/zircon.gpt"
+declare -r ZIRCON_SYSTEM_IMAGE="${OUT_DIR}/system.minfs"
 
 # sgdisk is used to manipulate GPT partition tables.
 check_sgdisk() {
@@ -69,7 +63,7 @@ generate_system_image() {
   ${MINFS} ${image} mkdir ::/bin
 
   # Copy binaries from system/uapp into the system image.
-  for app_path in `find "${BUILDDIR}/build-zircon/build-${ARCH}/system/uapp" -iname "*.elf"`; do
+  for app_path in `find "${BUILD_DIR}/build-zircon/build-${ARCH}/system/uapp" -iname "*.elf"`; do
     local exe_name=`basename "${app_path}"`
     # Strip the '.elf' file extension.
     local app="${exe_name%.*}"
@@ -90,7 +84,11 @@ generate_gpt_image() {
   local sys_start_sector=2048
   local sys_end_sector=$((${sys_part_size} + ${sys_start_sector}))
 
-  dd if=/dev/zero of="${image}" bs=512 count="$((${sys_end_sector} + 2048))"
+  dd status=none \
+     if=/dev/zero \
+     of="${image}" \
+     bs=512 \
+     count="$((${sys_end_sector} + 2048))"
 
   sgdisk --new 1:${sys_start_sector}:${sys_end_sector} ${image}
   sgdisk --typecode 1:${ZIRCON_SYSTEM_GUID}  ${image}
@@ -98,7 +96,8 @@ generate_gpt_image() {
 
    # Copy bytes from the system image into the correct location in the GPT
    # image.
-   dd if="${system_image}" \
+   dd status=none \
+      if="${system_image}" \
       of="${image}" \
       bs=512 \
       seek="${sys_start_sector}" \
@@ -108,11 +107,5 @@ generate_gpt_image() {
 
 check_sgdisk
 
-# Are the requested targets up-to-date?
-if [ "${FORCE}" != "true" ] && [ -f "${ZIRCON_GPT_IMAGE}" ]; then
-  echo "GPT image already exists. Pass -f to force a rebuild."
-  exit 0
-fi
-
 generate_system_image "${ZIRCON_SYSTEM_IMAGE}" "30"
-generate_gpt_image "${ZIRCON_GPT_IMAGE}" "${ZIRCON_SYSTEM_IMAGE}"
+generate_gpt_image "${ZIRCON_GPT_IMAGE}" "${ZIRCON_SYSTEM_IMAGE}" > /dev/null
