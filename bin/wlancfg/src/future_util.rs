24b55a4ff9872fdeec0ea70ffb7b18a10f8a3fcd
Greg Bonik <gbonik@google.com>
2018-07-26 13:13:40 -0700

    [wlancfg] Tests for future_util.rs
    NET-1173
    
    TEST=This change adds new tests
    
    Change-Id: Ia652f4d593a45732439b2decc8cd4546fdf26b0e

--- bin/wlancfg/src/future_util.rs
+++ bin/wlancfg/src/future_util.rs
@@ -25,3 +25,41 @@ pub fn retry_until<T, E, FUNC, FUT>(retry_interval: zx::Duration, mut f: FUNC)
                     .right_future()
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async;
+    use zx::prelude::*;
+
+    #[test]
+    fn first_try() {
+        let mut exec = async::Executor::new().expect("failed to create an executor");
+        let mut fut = retry_until::<_, Never, _, _>(5.seconds(), || future::ok(Some(123)));
+        assert_eq!(Ok(Async::Ready(123)), exec.run_until_stalled(&mut fut));
+    }
+
+    #[test]
+    fn third_try() {
+        let mut exec = async::Executor::new().expect("failed to create an executor");
+        let mut countdown = 3;
+        let start = 0.seconds().after_now();
+        let mut fut = retry_until::<_, Never, _, _>(
+            5.seconds(),
+            move || {
+                countdown -= 1;
+                if countdown == 0 {
+                    future::ok(Some(123))
+                } else {
+                    future::ok(None)
+                }
+            });
+        assert_eq!(Ok(Async::Pending), exec.run_until_stalled(&mut fut));
+        let first_timeout = exec.wake_next_timer().expect("expected a pending timer");
+        assert!(first_timeout >= start + 5.seconds());
+        assert_eq!(Ok(Async::Pending), exec.run_until_stalled(&mut fut));
+        let second_timeout = exec.wake_next_timer().expect("expected a pending timer");
+        assert_eq!(first_timeout + 5.seconds(), second_timeout);
+        assert_eq!(Ok(Async::Ready(123)), exec.run_until_stalled(&mut fut));
+    }
+}
