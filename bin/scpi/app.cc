a3390419205de1cbb61841ea72ad57e8a4491097
Brijen Raval <braval@google.com>
2018-07-26 10:55:12 -0700

    [system-dashboard] Add CPU usage functionality
    Adding support to calculate current CPU usage
    to existing FIDL interface
    
    Test: Ran unittests manually
    ZX-2358 #comment CPU Utilization support
    
    Change-Id: I4fe985afbd3b90e826848a3348e23a0e78c7c238

--- bin/scpi/app.cc
+++ bin/scpi/app.cc
@@ -6,7 +6,10 @@
 #include <ddk/protocol/scpi.h>
 #include <lib/fdio/watcher.h>
 #include <stdio.h>
+#include <zircon/device/sysinfo.h>
 #include <zircon/device/thermal.h>
+#include <zircon/status.h>
+#include <zircon/syscalls/object.h>
 #include "lib/component/cpp/startup_context.h"
 
 namespace scpi {
@@ -19,6 +22,40 @@ App::App(std::unique_ptr<component::StartupContext> context)
     : context_(std::move(context)) {}
 
 App::~App() {}
+
+zx::handle App::GetRootResource() {
+  const int fd = open("/dev/misc/sysinfo", O_RDWR);
+  if (fd == 0)
+    return {};
+
+  zx_handle_t root_resource;
+  auto n = ioctl_sysinfo_get_root_resource(fd, &root_resource);
+  close(fd);
+
+  FXL_DCHECK(n == sizeof(root_resource));
+
+  return zx::handle(root_resource);
+}
+
+size_t App::ReadCpuCount(const zx::handle& root_resource) {
+  size_t actual, available;
+  zx_status_t err = root_resource.get_info(ZX_INFO_CPU_STATS, nullptr, 0,
+                                           &actual, &available);
+  if (err != ZX_OK) {
+    return 0;
+  }
+  return available;
+}
+
+bool App::ReadCpuStats() {
+  size_t actual, available;
+
+  zx_status_t err = root_resource_handle_.get_info(
+      ZX_INFO_CPU_STATS, &cpu_stats_[0],
+      num_cores_ * sizeof(zx_info_cpu_stats), &actual, &available);
+
+  return (err == ZX_OK);
+}
 
 zx_status_t App::Start() {
   fxl::UniqueFD dirfd(open(kThermalDir, O_DIRECTORY | O_RDONLY));
@@ -49,6 +86,10 @@ zx_status_t App::Start() {
     return ZX_ERR_UNAVAILABLE;
   }
 
+  root_resource_handle_ = GetRootResource();
+  num_cores_ = ReadCpuCount(root_resource_handle_);
+  cpu_stats_.reserve(num_cores_);
+  last_cpu_stats_.reserve(num_cores_);
   context_->outgoing().AddPublicService(bindings_.GetHandler(this));
   return ZX_OK;
 }
@@ -105,6 +146,35 @@ void App::GetSystemStatus(GetSystemStatusCallback callback) {
     callback(fuchsia::scpi::Status::ERR_FAN_LEVEL, std::move(info));
     return;
   }
+
+  zx_time_t delay = ZX_SEC(1);
+
+  if (!ReadCpuStats()) {
+    fprintf(stderr, "ERROR: Failed to get cpu_stats_ \n");
+    callback(fuchsia::scpi::Status::ERR_CPU_STATS, std::move(info));
+    return;
+  }
+
+  last_cpu_stats_.swap(cpu_stats_);
+  sleep(1);
+
+  if (!ReadCpuStats()) {
+    fprintf(stderr, "ERROR: Failed to get cpu_stats_ \n");
+    callback(fuchsia::scpi::Status::ERR_CPU_STATS, std::move(info));
+    return;
+  }
+
+  zx_time_t idle_time, busy_time;
+  double busypercent_sum = 0;
+  for (size_t i = 0; i<num_cores_; i++) {
+    idle_time = cpu_stats_[i].idle_time - last_cpu_stats_[i].idle_time;
+    busy_time = delay - (idle_time > delay? delay : idle_time);
+    double busypercent = (busy_time * 100)/(double)delay;
+    busypercent_sum += busypercent;
+  }
+
+  info.cpu_utilization = busypercent_sum/num_cores_;
+
   callback(fuchsia::scpi::Status::OK, std::move(info));
 }
 
--- bin/scpi/app.h
+++ bin/scpi/app.h
@@ -24,9 +24,16 @@ class App : public fuchsia::scpi::SystemController {
  private:
   App(const App&) = delete;
   App& operator=(const App&) = delete;
+  zx::handle GetRootResource();
+  size_t ReadCpuCount(const zx::handle& root_resource);
+  bool ReadCpuStats();
   std::unique_ptr<component::StartupContext> context_;
   fidl::BindingSet<fuchsia::scpi::SystemController> bindings_;
   fxl::UniqueFD fd_;
+  zx::handle root_resource_handle_;
+  std::vector<zx_info_cpu_stats_t> cpu_stats_;
+  std::vector<zx_info_cpu_stats_t> last_cpu_stats_;
+  size_t num_cores_;
 };
 
 }  // namespace scpi
