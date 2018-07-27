3d8b1c787741ed9fdc23e94a555a458e7c234153
Rebecca Silberstein <silberst@google.com>
2018-07-27 16:44:51 -0700

    [garnet] wlan-smoke-test: add command line options
    Add command line args for the target ssid, target password
    and a flag indicating if the connection should be forced
    if the interface is already connected to the target network.
    For the optional password:"" is used as the default (and
    for open networks).
    
    usage:
    wlan-smoke-test <target_ssid> [-p <target_pwd>] [-s]
    
    Bug: NET-1095
    Test: build and boot garnet, manually confirm output
    Test: fx run-test wlan_smoke_test_bin_test_rustc
    Change-Id: I630c32fd6594436fc62334bab0a2e8b6e9579043

--- bin/wlan-smoke-test/src/main.rs
+++ bin/wlan-smoke-test/src/main.rs
@@ -8,6 +8,7 @@
 #[macro_use]
 extern crate serde_derive;
 
+mod opts;
 mod wlan_service_util;
 
 use failure::{Error, ResultExt, err_msg};
@@ -16,24 +17,29 @@ use fidl_fuchsia_wlan_device_service::{DeviceServiceMarker, DeviceServiceProxy};
 use fidl_fuchsia_wlan_sme as fidl_sme;
 use fuchsia_app::client::connect_to_service;
 use fuchsia_async as fasync;
-use fuchsia_syslog::{self as syslog, fx_log, fx_log_err};
+use fuchsia_syslog::{self as syslog, fx_log, fx_log_err, fx_log_info};
 use fuchsia_zircon as zx;
 use serde::{Serialize};
 use serde::ser::{Serializer, SerializeMap, SerializeStruct};
 use std::collections::HashMap;
 use std::process;
 use std::fmt;
+use structopt::StructOpt;
+use crate::opts::Opt;
 
 type WlanService = DeviceServiceProxy;
 
 fn main() -> Result<(), Error> {
     syslog::init_with_tags(&["wlan-smoke-test"]).expect("should not fail");
 
+    let opt = Opt::from_args();
+    fx_log_info!("{:?}", opt);
+
     // create objects to hold test objects and results
     let mut test_results: TestResults = Default::default();
 
     let mut test_pass = true;
-    if let Err(e) = run_test(&mut test_results) {
+    if let Err(e) = run_test(opt, &mut test_results) {
         test_pass = false;
         test_results.error_message = e.to_string();
     }
@@ -47,7 +53,7 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn run_test(test_results: &mut TestResults)
+fn run_test(opt: Opt, test_results: &mut TestResults)
         -> Result<(), Error>
 {
     let mut test_pass = true;
@@ -61,7 +67,7 @@ fn run_test(test_results: &mut TestResults)
                 .context("wlan-smoke-test: failed to query wlanservice iface list")?;
         test_results.query_wlan_service_iface_list = true;
 
-        if wlan_iface_ids.len() < 1 {
+        if wlan_iface_ids.is_empty() {
             return Err(err_msg("Did not find wlan interfaces"));
         };
         test_results.wlan_discovered = true;
@@ -83,17 +89,33 @@ fn run_test(test_results: &mut TestResults)
             test_results.iface_objects.insert(iface, iface_object);
         }
 
-        let target_ssid = "HotspotNoInet";
-        let target_pwd = "";
-
         // now that we have interfaces...  let's try to use them!
         for (_, wlan_iface) in test_results.iface_objects.iter_mut() {
-            let connect_result = await!(
-                wlan_service_util::connect_to_network(&wlan_iface.sme_proxy,
-                                                         target_ssid.as_bytes().to_vec(),
-                                                         target_pwd.as_bytes().to_vec()))?;
-            if connect_result {
+            let mut requires_disconnect = false;
+            // first check if we are connected to the target network already
+            if is_connect_to_target_network_needed(opt.stay_connected,
+                                                   &opt.target_ssid,
+                                                   &wlan_iface.initial_status) {
+                let connect_result = await!(wlan_service_util::connect_to_network(
+                            &wlan_iface.sme_proxy,
+                            opt.target_ssid.as_bytes().to_vec(),
+                            opt.target_pwd.as_bytes().to_vec()));
+
+                match connect_result {
+                    Ok(true) => {
+                        wlan_iface.connection_success = true;
+                        requires_disconnect = true;
+                    },
+                    _ => continue
+                };
+            } else {
+                // connection already established, mark as successful
                 wlan_iface.connection_success = true;
+            }
+
+            // after testing, check if we need to disconnect
+            if requires_disconnect {
+                // TODO(NET-1095): disconnect any test-established connections when complete
             }
 
             // if any of the checks failed, throw an error to indicate a part of
@@ -164,4 +186,15 @@ impl WlanIface {
 
 fn report_results(test_results: &TestResults) {
     println!("Test Results: {}", serde_json::to_string_pretty(&test_results).unwrap());
+}
+
+fn is_connect_to_target_network_needed<T: AsRef<[u8]>>(stay_connected: bool,
+                                       target_ssid: T,
+                                       status: &fidl_sme::ClientStatusResponse) -> bool {
+    if !stay_connected {
+        // doesn't matter if we are connected, we will force a reconnection
+        return true;
+    }
+    // TODO: add check if we are already connected, if so, make sure the ssid matches
+    return true
 }
