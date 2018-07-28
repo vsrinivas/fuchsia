8008f7720be946f4084c92b3c5443d323c542de3
Adam Barth <abarth@google.com>
2018-07-27 17:13:52 -0700

    [terminal] Actually rasterize some text
    Test: Manual: run tiles terminal
    Change-Id: Ic696705a60fc0afde145317f4d51b31dc75c568f

---
+++ bin/terminal/src/canvas.rs
@@ -0,0 +1,183 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![deny(warnings)]
+
+extern crate font_rs;
+extern crate shared_buffer;
+
+use self::shared_buffer::SharedBuffer;
+use canvas::font_rs::font::{parse, Font, FontError, GlyphBitmap};
+use std::collections::HashMap;
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub struct Size {
+    pub width: u32,
+    pub height: u32,
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub struct Rect {
+    pub left: u32,
+    pub top: u32,
+    pub right: u32,
+    pub bottom: u32,
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub struct Paint {
+    pub fg: Color,
+    pub bg: Color,
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+struct Glyph(u16);
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct GlyphDescriptor {
+    size: u32,
+    glyph: Glyph,
+}
+
+pub struct FontFace<'a> {
+    font: Font<'a>,
+    glyph_cache: HashMap<GlyphDescriptor, GlyphBitmap>,
+}
+
+pub struct FontDescription<'a, 'b: 'a> {
+    pub face: &'a mut FontFace<'b>,
+    pub size: u32,
+    pub baseline: i32,
+}
+
+impl<'a> FontFace<'a> {
+    pub fn new(data: &'a [u8]) -> Result<FontFace<'a>, FontError> {
+        Ok(FontFace {
+            font: parse(data)?,
+            glyph_cache: HashMap::new(),
+        })
+    }
+
+    fn get_glyph(&mut self, glyph: Glyph, size: u32) -> &GlyphBitmap {
+        let font = &self.font;
+        let Glyph(glyph_id) = glyph;
+        self.glyph_cache
+            .entry(GlyphDescriptor { size, glyph })
+            .or_insert_with(|| font.render_glyph(glyph_id, size).unwrap())
+    }
+
+    fn lookup_glyph(&self, scalar: char) -> Option<Glyph> {
+        self.font
+            .lookup_glyph_id(scalar as u32)
+            .map(|id| Glyph(id))
+    }
+}
+
+pub struct Canvas {
+    // Assumes a pixel format of BGRA8 and a color space of sRGB.
+    buffer: SharedBuffer,
+    stride: u32,
+}
+
+const BYTES_PER_PIXEL: u32 = 4;
+
+impl Canvas {
+    pub fn new(buffer: SharedBuffer, stride: u32) -> Canvas {
+        Canvas { buffer, stride }
+    }
+
+    #[inline]
+    fn write_color_at_offset(&mut self, offset: usize, color: Color) {
+        let pixel = [color.b, color.g, color.r, color.a];
+        self.buffer.write_at(offset, &pixel);
+    }
+
+    #[inline]
+    fn set_pixel_at_offset(&mut self, offset: usize, value: u8, paint: &Paint) {
+        match value {
+            0 => (),
+            255 => self.write_color_at_offset(offset, paint.fg),
+            _ => {
+                let fg = &paint.fg;
+                let bg = &paint.bg;
+                let a = ((value as u32) * (fg.a as u32)) >> 8;
+                let blend_factor = ((bg.a as u32) * (255 - a)) >> 8;
+                let pixel = [
+                    (((bg.b as u32) * blend_factor + (fg.b as u32) * a) >> 8) as u8,
+                    (((bg.g as u32) * blend_factor + (fg.g as u32) * a) >> 8) as u8,
+                    (((bg.r as u32) * blend_factor + (fg.r as u32) * a) >> 8) as u8,
+                    (blend_factor + (fg.a as u32)) as u8,
+                ];
+                self.buffer.write_at(offset, &pixel);
+            }
+        }
+    }
+
+    fn draw_glyph_at(&mut self, glyph: &GlyphBitmap, x: i32, y: i32, paint: &Paint) {
+        let glyph_data = &glyph.data.as_slice();
+        let col_stride = BYTES_PER_PIXEL as i32;
+        let row_stride = self.stride as i32;
+        let mut row_offset = y * row_stride + x * col_stride;
+        for glyph_row in glyph_data.chunks(glyph.width) {
+            let mut offset = row_offset;
+            if offset > 0 {
+                for pixel in glyph_row {
+                    let value = *pixel;
+                    self.set_pixel_at_offset(offset as usize, value, paint);
+                    offset += col_stride;
+                }
+            }
+            row_offset += row_stride;
+        }
+    }
+
+    pub fn fill_rect(&mut self, rect: &Rect, color: Color) {
+        let col_stride = BYTES_PER_PIXEL;
+        let row_stride = self.stride;
+        for y in rect.top..rect.bottom {
+            for x in rect.left..rect.right {
+                let offset = y * row_stride + x * col_stride;
+                self.write_color_at_offset(offset as usize, color);
+            }
+        }
+    }
+
+    pub fn fill_text(
+        &mut self, text: &str, point: Point, size: Size, font: &mut FontDescription, paint: &Paint,
+    ) {
+        let mut x = point.x;
+        let advance = size.width;
+        for scalar in text.chars() {
+            let cell = Rect {
+                left: x,
+                top: point.y,
+                right: x + advance,
+                bottom: point.y + size.height,
+            };
+            self.fill_rect(&cell, paint.bg);
+            if scalar != ' ' {
+                if let Some(glyph_id) = font.face.lookup_glyph(scalar) {
+                    let glyph = font.face.get_glyph(glyph_id, font.size);
+                    let x = cell.left as i32 + glyph.left;
+                    let y = cell.top as i32 + font.baseline + glyph.top;
+                    self.draw_glyph_at(glyph, x, y, paint);
+                }
+            }
+            x += advance;
+        }
+    }
+}
--- bin/terminal/src/main.rs
+++ bin/terminal/src/main.rs
@@ -19,6 +19,9 @@ extern crate fuchsia_zircon;
 extern crate futures;
 extern crate parking_lot;
 
+mod canvas;
+
+use canvas::{Canvas, Color, FontDescription, FontFace, Paint, Point, Size};
 use component::client::connect_to_service;
 use component::server::ServiceFactory;
 use failure::{Error, ResultExt};
@@ -36,9 +39,16 @@ use futures::{FutureExt, StreamExt};
 use parking_lot::Mutex;
 use scenic::{ScenicMarker, ScenicProxy, SessionListenerMarker, SessionListenerRequest,
              SessionMarker};
+use std::env;
 use std::sync::Arc;
 
+static FONT_DATA: &'static [u8] =
+    include_bytes!("../../fonts/third_party/robotomono/RobotoMono-Regular.ttf");
+
+type FontFacePtr = Arc<Mutex<FontFace<'static>>>;
+
 struct ViewController {
+    face: FontFacePtr,
     _view: ViewProxy,
     session: SessionPtr,
     import_node: ImportNode,
@@ -51,8 +61,8 @@ type ViewControllerPtr = Arc<Mutex<ViewController>>;
 
 impl ViewController {
     pub fn new(
-        view_listener_request: ServerEnd<ViewListenerMarker>, view: ViewProxy, mine: EventPair,
-        scenic: ScenicProxy,
+        face: FontFacePtr, view_listener_request: ServerEnd<ViewListenerMarker>, view: ViewProxy,
+        mine: EventPair, scenic: ScenicProxy,
     ) -> Result<ViewControllerPtr, Error> {
         let (session_listener_client, session_listener_server) = Channel::create()?;
         let session_listener = ClientEnd::new(session_listener_client);
@@ -63,6 +73,7 @@ impl ViewController {
         scenic.create_session(session_request, Some(session_listener))?;
         let session = Session::new(session_proxy);
         let view_controller = ViewController {
+            face,
             _view: view,
             session: session.clone(),
             import_node: ImportNode::new(session.clone(), mine),
@@ -132,13 +143,14 @@ impl ViewController {
             (Some(metrics), Some(logical_size)) => (metrics, logical_size),
             _ => return,
         };
-        let physical_width = (logical_size.width / metrics.scale_x) as u32;
-        let physical_height = (logical_size.height / metrics.scale_y) as u32;
+        let physical_width = (logical_size.width * metrics.scale_x) as u32;
+        let physical_height = (logical_size.height * metrics.scale_y) as u32;
+        let stride = physical_width * 4;
         let info = images::ImageInfo {
             transform: images::Transform::Normal,
             width: physical_width,
             height: physical_height,
-            stride: physical_width * 4,
+            stride,
             pixel_format: images::PixelFormat::Bgra8,
             color_space: images::ColorSpace::Srgb,
             tiling: images::Tiling::Linear,
@@ -149,16 +161,34 @@ impl ViewController {
                 .image_cycler
                 .acquire(info)
                 .expect("failed to allocate buffer");
-            let mut buffer = guard.image().buffer();
-            let cyan = [255, 255, 0, 255];
-            let len = buffer.len();
-            for i in 0..(len / 4) {
-                buffer.write_at(i * 4, &cyan);
-            }
+            let mut face = self.face.lock();
+            let mut canvas = Canvas::new(guard.image().buffer(), stride);
+            let paint = Paint {
+                fg: Color {
+                    r: 0xFF,
+                    g: 0xDE,
+                    b: 0xAD,
+                    a: 0xFF,
+                },
+                bg: Color {
+                    r: 0x00,
+                    g: 0x00,
+                    b: 0x8b,
+                    a: 0xFF,
+                },
+            };
+            let point = Point { x: 0, y: 0 };
+            let size = Size { width: 14, height: 22 };
+            let mut font = FontDescription {
+                face: &mut face,
+                size: 20,
+                baseline: 18,
+            };
+            canvas.fill_text("$ echo \"hello, world!\"", point, size, &mut font, &paint);
         }
 
         let node = self.image_cycler.node();
-        node.set_scale(metrics.scale_x, metrics.scale_y, 1.0);
+        node.set_scale(1.0 / metrics.scale_x, 1.0 / metrics.scale_y, 1.0);
         node.set_translation(logical_size.width / 2.0, logical_size.height / 2.0, 0.0);
         self.present();
     }
@@ -192,6 +222,7 @@ impl ViewController {
 }
 
 struct App {
+    face: FontFacePtr,
     view_manager: ViewManagerProxy,
     view_controllers: Vec<ViewControllerPtr>,
 }
@@ -202,6 +233,7 @@ impl App {
     pub fn new() -> Result<AppPtr, Error> {
         let view_manager = connect_to_service::<ViewManagerMarker>()?;
         Ok(Arc::new(Mutex::new(App {
+            face: Arc::new(Mutex::new(FontFace::new(FONT_DATA).unwrap())),
             view_manager,
             view_controllers: vec![],
         })))
@@ -240,6 +272,7 @@ impl App {
         let (scenic, scenic_request) = create_endpoints::<ScenicMarker>()?;
         self.view_manager.get_scenic(scenic_request)?;
         self.view_controllers.push(ViewController::new(
+            self.face.clone(),
             view_listener_request,
             view,
             mine,
@@ -264,6 +297,8 @@ impl ServiceFactory for ViewProvider {
 }
 
 fn main() -> Result<(), Error> {
+    env::set_var("RUST_BACKTRACE", "1");
+
     let mut executor = async::Executor::new().context("Error creating executor")?;
     let app = App::new()?;
 
