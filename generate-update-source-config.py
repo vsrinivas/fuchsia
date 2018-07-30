cf3e34ecf59c6676e5ec0b811edab49c6e57dc72
Kevin Wells <kevinwells@google.com>
2018-07-30 10:14:37 -0700

    [add-update-source] Port script to python2
    Change-Id: Idf471fa3ebc1e229528e82b982495cf99cf193a1

--- generate-update-source-config.py
+++ generate-update-source-config.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright 2018 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+from __future__ import print_function
 
 import argparse
 import collections
