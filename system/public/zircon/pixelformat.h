2a5a61ffad0cb90bd9deaafbc121148be3388d6f
Adam Barth <abarth@chromium.org>
2018-07-29 22:39:38 -0700

    [zircon] Update pixelformat.h to match rubric
    Test: No behavior change
    Change-Id: I9e3b6a6dd6a494ac9f1671c3b7c954f7aa308f21

--- system/public/zircon/pixelformat.h
+++ system/public/zircon/pixelformat.h
@@ -2,25 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#pragma once
+#ifndef ZIRCON_PIXELFORMAT_H_
+#define ZIRCON_PIXELFORMAT_H_
 
 #include <stdint.h>
 
 typedef uint32_t zx_pixel_format_t;
 // clang-format off
 
-#define ZX_PIXEL_FORMAT_NONE       (0x00000000)
+#define ZX_PIXEL_FORMAT_NONE       ((zx_pixel_format_t)0x00000000)
 
-#define ZX_PIXEL_FORMAT_RGB_565    (0x00020001)
-#define ZX_PIXEL_FORMAT_RGB_332    (0x00010002)
-#define ZX_PIXEL_FORMAT_RGB_2220   (0x00010003)
-#define ZX_PIXEL_FORMAT_ARGB_8888  (0x00040004)
-#define ZX_PIXEL_FORMAT_RGB_x888   (0x00040005)
-#define ZX_PIXEL_FORMAT_MONO_8     (0x00010007)
-#define ZX_PIXEL_FORMAT_GRAY_8     (0x00010007)
+#define ZX_PIXEL_FORMAT_RGB_565    ((zx_pixel_format_t)0x00020001)
+#define ZX_PIXEL_FORMAT_RGB_332    ((zx_pixel_format_t)0x00010002)
+#define ZX_PIXEL_FORMAT_RGB_2220   ((zx_pixel_format_t)0x00010003)
+#define ZX_PIXEL_FORMAT_ARGB_8888  ((zx_pixel_format_t)0x00040004)
+#define ZX_PIXEL_FORMAT_RGB_x888   ((zx_pixel_format_t)0x00040005)
+#define ZX_PIXEL_FORMAT_MONO_8     ((zx_pixel_format_t)0x00010007)
+#define ZX_PIXEL_FORMAT_GRAY_8     ((zx_pixel_format_t)0x00010007)
 
 // to be removed:
-#define ZX_PIXEL_FORMAT_MONO_1     (0x00000006)
-
+#define ZX_PIXEL_FORMAT_MONO_1     ((zx_pixel_format_t)0x00000006)
 
 #define ZX_PIXEL_FORMAT_BYTES(pf)  (((pf) >> 16) & 7)
+
+#endif // ZIRCON_PIXELFORMAT_H_
