a3b2a9880e26d08d7c592749526ee9f02850bf5b
Adam Barth <abarth@chromium.org>
2018-07-29 22:34:33 -0700

    [zircon] Update limits.h to match rubric
    Test: No behavior change
    Change-Id: I199e80c8010cedf3500447a78d24b9259e22eab9

--- system/public/zircon/limits.h
+++ system/public/zircon/limits.h
@@ -2,9 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#pragma once
+#ifndef ZIRCON_LIMITS_H_
+#define ZIRCON_LIMITS_H_
 
-#define ZX_PAGE_SHIFT (12UL)
-#define ZX_PAGE_SIZE  (1UL << ZX_PAGE_SHIFT)
-#define ZX_PAGE_MASK  (ZX_PAGE_SIZE - 1UL)
+#include <stdint.h>
 
+#define ZX_PAGE_SHIFT ((uint32_t)12u)
+#define ZX_PAGE_SIZE ((uint32_t)(1u << ZX_PAGE_SHIFT))
+#define ZX_PAGE_MASK (ZX_PAGE_SIZE - 1u)
+
+#endif // ZIRCON_LIMITS_H_
