dc4703b6cde88c2fe2fb879a59d5e99050b98825
Mark Seaborn <mseaborn@google.com>
2018-07-31 16:24:33 -0700

    [benchmarks] Convert channels.cc to use the perftest library
    This eliminates some duplication: Before, the Write and Read tests did
    the same thing but with Pause/ResumeTiming() calls around different
    steps.  The perftest library handles this via multi-step tests
    instead, and it can now track the throughput (bytes per second) too.
    
    Results on garnet-x64-perf-swift_canyon (Intel NUC):
    
    Before this change (gbenchmark version):
    
    Benchmark                                                       Time           CPU Iterations
    ---------------------------------------------------------------------------------------------
    Channel/Write/64                                              686 ns        689 ns    1030447   88.5897MB/s
    Channel/Write/1024                                            711 ns        713 ns     968216   1.33683GB/s
    Channel/Write/32k                                            2339 ns       2342 ns     299642   13.0316GB/s
    Channel/Write/64k                                            3972 ns       3976 ns     180093   15.3526GB/s
    Channel/Read/64                                               726 ns        730 ns     947233   83.6181MB/s
    Channel/Read/1024                                             751 ns        755 ns     934755   1.26257GB/s
    Channel/Read/32k                                             2306 ns       2309 ns     302373   13.2165GB/s
    Channel/Read/64k                                             4092 ns       4096 ns     176184   14.9029GB/s
    
    After this change (perftest version):
    
          Mean    Std dev        Min        Max     Median Unit         Mean Mbytes/sec Test case
           651         44        641       1944        647 nanoseconds           93.760 Channel/WriteRead/64bytes
           323         29        317       1205        320 nanoseconds              N/A Channel/WriteRead/64bytes.write
           328         17        321        739        326 nanoseconds              N/A Channel/WriteRead/64bytes.read
           695         36        684       1235        690 nanoseconds         1405.393 Channel/WriteRead/1024bytes
           347         24        339        857        345 nanoseconds              N/A Channel/WriteRead/1024bytes.write
           348         17        340        726        345 nanoseconds              N/A Channel/WriteRead/1024bytes.read
          3785         80       3708       6141       3781 nanoseconds         8255.953 Channel/WriteRead/32768bytes
          1891         66       1832       3813       1888 nanoseconds              N/A Channel/WriteRead/32768bytes.write
          1894         20       1853       2328       1893 nanoseconds              N/A Channel/WriteRead/32768bytes.read
          7517        131       7368      10572       7508 nanoseconds         8314.788 Channel/WriteRead/65536bytes
          3505        104       3405       6029       3497 nanoseconds              N/A Channel/WriteRead/65536bytes.write
          4012         44       3919       4705       4010 nanoseconds              N/A Channel/WriteRead/65536bytes.read
    
    Bug: TO-651
    Test: "/pkgfs/packages/zircon_benchmarks/0/bin/app -p"
    Change-Id: I4328962a8beed4d1e672d9b99836e740e2d706fe

--- bin/zircon_benchmarks/channels.cc
+++ bin/zircon_benchmarks/channels.cc
@@ -2,88 +2,49 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <string>
 #include <vector>
 
-#include <benchmark/benchmark.h>
-#include <zircon/process.h>
-#include <zircon/syscalls.h>
+#include <fbl/string_printf.h>
+#include <lib/zx/channel.h>
+#include <perftest/perftest.h>
 
-#include "channels.h"
+namespace {
 
-class Channel : public benchmark::Fixture {
- private:
-  void SetUp(benchmark::State& state) override {
-    if (zx_channel_create(0, &in, &out) != ZX_OK) {
-      state.SkipWithError("Failed to create channel");
-    }
+// Measure the times taken to enqueue and then dequeue a message from a
+// Zircon channel, on a single thread.  This does not involve any
+// cross-thread wakeups.
+bool ChannelWriteReadTest(perftest::RepeatState* state, uint32_t message_size) {
+  state->DeclareStep("write");
+  state->DeclareStep("read");
+  state->SetBytesProcessedPerRun(message_size);
+
+  zx::channel channel1;
+  zx::channel channel2;
+  ZX_ASSERT(zx::channel::create(0, &channel1, &channel2) == ZX_OK);
+  std::vector<char> buffer(message_size);
+
+  while (state->KeepRunning()) {
+    ZX_ASSERT(channel1.write(0, buffer.data(), buffer.size(), nullptr, 0)
+              == ZX_OK);
+    state->NextStep();
+    ZX_ASSERT(channel2.read(0, buffer.data(), buffer.size(), nullptr, nullptr,
+                            0, nullptr) == ZX_OK);
   }
-
-  void TearDown(benchmark::State& state) override {
-    zx_handle_close(in);
-    zx_handle_close(out);
-  }
-
- protected:
-  zx_handle_t in;
-  zx_handle_t out;
-};
-
-BENCHMARK_DEFINE_F(Channel, Write)(benchmark::State& state) {
-  std::vector<char> buffer(state.range(0));
-  zx_status_t status;
-  while (state.KeepRunning()) {
-    status = zx_channel_write(in, 0, buffer.data(), buffer.size(), nullptr, 0);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to write to channel");
-      return;
-    }
-
-    // Make sure we drain the channel.
-    state.PauseTiming();
-    status = zx_channel_read(out, 0, buffer.data(), nullptr,
-                             buffer.size(), 0, nullptr, nullptr);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to read from channel");
-      return;
-    }
-    state.ResumeTiming();
-  }
-  state.SetBytesProcessed(state.iterations() * state.range(0));
+  return true;
 }
-BENCHMARK_REGISTER_F(Channel, Write)
-    ->Arg(64)
-    ->Arg(1 * 1024)
-    ->Arg(32 * 1024)
-    ->Arg(64 * 1024);
 
-BENCHMARK_DEFINE_F(Channel, Read)(benchmark::State& state) {
-  std::vector<char> buffer(state.range(0));
-  zx_status_t status;
-  uint64_t bytes_processed = 0;
-  while (state.KeepRunning()) {
-    // Write the data to read into the channel.
-    state.PauseTiming();
-    status = zx_channel_write(in, 0, buffer.data(), buffer.size(), nullptr, 0);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to write to channel");
-      return;
-    }
-    state.ResumeTiming();
-
-    uint32_t bytes_read;
-    status = zx_channel_read(out, 0, buffer.data(), nullptr,
-                             buffer.size(), 0, &bytes_read, nullptr);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to read from channel");
-      return;
-    }
-    bytes_processed += bytes_read;
+void RegisterTests() {
+  static const unsigned kMessageSizesInBytes[] = {
+    64,
+    1 * 1024,
+    32 * 1024,
+    64 * 1024,
+  };
+  for (auto message_size : kMessageSizesInBytes) {
+    auto name = fbl::StringPrintf("Channel/WriteRead/%ubytes", message_size);
+    perftest::RegisterTest(name.c_str(), ChannelWriteReadTest, message_size);
   }
-  state.SetBytesProcessed(bytes_processed);
 }
-BENCHMARK_REGISTER_F(Channel, Read)
-    ->Arg(64)
-    ->Arg(1 * 1024)
-    ->Arg(32 * 1024)
-    ->Arg(64 * 1024);
+PERFTEST_CTOR(RegisterTests);
+
+}  // namespace
