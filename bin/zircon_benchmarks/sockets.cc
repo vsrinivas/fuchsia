ac97e6172124ca301d4f907244e786fd16c95ea8
Mark Seaborn <mseaborn@google.com>
2018-08-01 15:49:16 -0700

    [benchmarks] Convert sockets.cc to use the perftest library
    This is similar to the change for channels.cc.
    
    This eliminates some duplication: Before, the Write and Read tests did
    the same thing but with Pause/ResumeTiming() calls around different
    steps.  The perftest library handles this via multi-step tests
    instead, and it can now track the throughput (bytes per second) too.
    
    Results on garnet-x64-perf-swift_canyon (Intel NUC):
    
    Before this change (gbenchmark version):
    
    Benchmark                                                       Time           CPU Iterations
    ---------------------------------------------------------------------------------------------
    Socket/Write/64                                               673 ns        676 ns    1029531   90.2346MB/s
    Socket/Write/1024                                             697 ns        700 ns     994733   1.36183GB/s
    Socket/Write/32k                                             2578 ns       2579 ns     273277    11.832GB/s
    Socket/Write/64k                                             4292 ns       4293 ns     157342   14.2188GB/s
    Socket/Read/64                                                670 ns        673 ns    1041515   90.6933MB/s
    Socket/Read/1024                                              690 ns        694 ns    1004810   1.37491GB/s
    Socket/Read/32k                                              2614 ns       2619 ns     268950    11.654GB/s
    Socket/Read/64k                                              4615 ns       4622 ns     153593   13.2046GB/s
    
    After this change (perftest version):
    
          Mean    Std dev        Min        Max     Median Unit         Mean Mbytes/sec Test case
           673         61        657       2385        668 nanoseconds           90.711 Socket/WriteRead/64bytes
           340         45        329       1615        336 nanoseconds              N/A Socket/WriteRead/64bytes.write
           333         18        323        771        331 nanoseconds              N/A Socket/WriteRead/64bytes.read
           713         35        698       1401        710 nanoseconds         1368.813 Socket/WriteRead/1024bytes
           361         22        352        912        359 nanoseconds              N/A Socket/WriteRead/1024bytes.write
           352         16        342        698        351 nanoseconds              N/A Socket/WriteRead/1024bytes.read
          4442        198       4363      10568       4433 nanoseconds         7035.334 Socket/WriteRead/32768bytes
          2201        183       2152       7937       2193 nanoseconds              N/A Socket/WriteRead/32768bytes.write
          2241         39       2169       3175       2240 nanoseconds              N/A Socket/WriteRead/32768bytes.read
          8618        217       8494      14840       8599 nanoseconds         7252.146 Socket/WriteRead/65536bytes
          4323        189       4240       9931       4315 nanoseconds              N/A Socket/WriteRead/65536bytes.write
          4295         66       4200       5759       4286 nanoseconds              N/A Socket/WriteRead/65536bytes.read
    
    Bug: TO-651
    Test: "/pkgfs/packages/zircon_benchmarks/0/bin/app -p"
    Change-Id: I751b1405542d6b521661321f7ce9c19936b60ab1

--- bin/zircon_benchmarks/sockets.cc
+++ bin/zircon_benchmarks/sockets.cc
@@ -2,95 +2,54 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <benchmark/benchmark.h>
-#include <zircon/syscalls.h>
+#include <vector>
 
-class Socket : public benchmark::Fixture {
- private:
-  void SetUp(benchmark::State& state) override {
-    if (zx_socket_create(0, &in, &out) != ZX_OK) {
-      state.SkipWithError("Failed to create socket");
-    }
-  }
+#include <fbl/string_printf.h>
+#include <lib/zx/socket.h>
+#include <perftest/perftest.h>
 
-  void TearDown(benchmark::State& state) override {
-    zx_handle_close(in);
-    zx_handle_close(out);
-  }
+namespace {
 
- protected:
-  zx_handle_t in;
-  zx_handle_t out;
-};
+// Measure the times taken to enqueue and then dequeue some bytes from a
+// Zircon socket, on a single thread.  This does not involve any
+// cross-thread wakeups.
+bool SocketWriteReadTest(perftest::RepeatState* state, uint32_t message_size) {
+  state->DeclareStep("write");
+  state->DeclareStep("read");
+  state->SetBytesProcessedPerRun(message_size);
 
-BENCHMARK_DEFINE_F(Socket, Write)(benchmark::State& state) {
-  std::vector<char> buffer(state.range(0));
-  zx_status_t status;
-  uint64_t bytes_processed = 0;
-  while (state.KeepRunning()) {
+  zx::socket socket1;
+  zx::socket socket2;
+  ZX_ASSERT(zx::socket::create(ZX_SOCKET_STREAM, &socket1, &socket2) == ZX_OK);
+  std::vector<char> buffer(message_size);
+
+  while (state->KeepRunning()) {
     size_t bytes_written;
-    status = zx_socket_write(
-        in, 0, buffer.data(), buffer.size(), &bytes_written);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to write to socket");
-      return;
-    }
-    bytes_processed += bytes_written;
-
-    // Make sure we drain the socket.
-    state.PauseTiming();
-    size_t bytes_read;
-    status = zx_socket_read(out, 0, buffer.data(), buffer.size(), &bytes_read);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to read from socket");
-      return;
-    }
-    if (bytes_read != buffer.size()) {
-      state.SkipWithError("Did not read all data from socket");
-      return;
-    }
-    state.ResumeTiming();
-  }
-  state.SetBytesProcessed(bytes_processed);
-}
-BENCHMARK_REGISTER_F(Socket, Write)
-    ->Arg(64)
-    ->Arg(1 * 1024)
-    ->Arg(32 * 1024)
-    ->Arg(64 * 1024);
-
-BENCHMARK_DEFINE_F(Socket, Read)(benchmark::State& state) {
-  std::vector<char> buffer(state.range(0));
-  zx_status_t status;
-  uint64_t bytes_processed = 0;
-  while (state.KeepRunning()) {
-    // Write the data to read into the socket.
-    state.PauseTiming();
-    size_t bytes_written;
-    status = zx_socket_write(
-        in, 0, buffer.data(), buffer.size(), &bytes_written);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to write to socket");
-      return;
-    }
-    state.ResumeTiming();
+    ZX_ASSERT(socket1.write(0, buffer.data(), buffer.size(), &bytes_written)
+              == ZX_OK);
+    ZX_ASSERT(bytes_written == buffer.size());
+    state->NextStep();
 
     size_t bytes_read;
-    status = zx_socket_read(out, 0, buffer.data(), buffer.size(), &bytes_read);
-    if (status != ZX_OK) {
-      state.SkipWithError("Failed to read from socket");
-      return;
-    }
-    if (bytes_read != buffer.size()) {
-      state.SkipWithError("Did not read all data from socket");
-      return;
-    }
-    bytes_processed += bytes_read;
+    ZX_ASSERT(socket2.read(0, buffer.data(), buffer.size(), &bytes_read)
+              == ZX_OK);
+    ZX_ASSERT(bytes_read == buffer.size());
   }
-  state.SetBytesProcessed(bytes_processed);
+  return true;
 }
-BENCHMARK_REGISTER_F(Socket, Read)
-    ->Arg(64)
-    ->Arg(1 * 1024)
-    ->Arg(32 * 1024)
-    ->Arg(64 * 1024);
+
+void RegisterTests() {
+  static const unsigned kMessageSizesInBytes[] = {
+    64,
+    1 * 1024,
+    32 * 1024,
+    64 * 1024,
+  };
+  for (auto message_size : kMessageSizesInBytes) {
+    auto name = fbl::StringPrintf("Socket/WriteRead/%ubytes", message_size);
+    perftest::RegisterTest(name.c_str(), SocketWriteReadTest, message_size);
+  }
+}
+PERFTEST_CTOR(RegisterTests);
+
+}  // namespace
