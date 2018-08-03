c5dc10a5cf27ec2b61e4cc5f9a89a49cb3005d7b
Mark Seaborn <mseaborn@google.com>
2018-08-03 14:09:39 -0700

    [benchmarks] Convert pseudo_dir.cc to use the perftest library
    Bug: TO-651
    Test: "/pkgfs/packages/zircon_benchmarks/0/bin/app -p"
    Change-Id: I028e10af14f3aeb2c989007c23ffa11a9f6d9561

--- bin/zircon_benchmarks/pseudo_dir.cc
+++ bin/zircon_benchmarks/pseudo_dir.cc
@@ -5,13 +5,15 @@
 #include <random>
 #include <vector>
 
-#include <benchmark/benchmark.h>
 #include <fbl/ref_ptr.h>
 #include <fbl/string_printf.h>
 #include <fs/pseudo-dir.h>
 #include <fs/pseudo-file.h>
 #include <lib/fxl/strings/string_printf.h>
+#include <perftest/perftest.h>
 #include <zircon/syscalls.h>
+
+namespace {
 
 std::vector<std::string> MakeDeterministicNamesList(int length) {
   std::vector<std::string> ret;
@@ -24,25 +26,18 @@ std::vector<std::string> MakeDeterministicNamesList(int length) {
   return ret;
 }
 
-namespace {
-
-class PseudoDir : public benchmark::Fixture {};
-
-}  // namespace
-
 // Benchmark baseline creation time.
-BENCHMARK_DEFINE_F(PseudoDir, Create)(benchmark::State& state) {
-  while (state.KeepRunning()) {
-    benchmark::DoNotOptimize(fs::PseudoDir());
+bool PseudoDirCreateTest(perftest::RepeatState* state) {
+  while (state->KeepRunning()) {
+    perftest::DoNotOptimize(fs::PseudoDir());
   }
+  return true;
 }
-
-BENCHMARK_REGISTER_F(PseudoDir, Create);
 
 // Benchmark the time to remove an existing entry and add it back.
 // Parameterized by the number of files.
-BENCHMARK_DEFINE_F(PseudoDir, RemoveAdd)(benchmark::State& state) {
-  auto file_names = MakeDeterministicNamesList(state.range(0));
+bool PseudoDirRemoveAddTest(perftest::RepeatState* state, int file_count) {
+  auto file_names = MakeDeterministicNamesList(file_count);
 
   auto dir = fbl::AdoptRef(new fs::PseudoDir());
   auto file = fbl::AdoptRef(new fs::UnbufferedPseudoFile());
@@ -53,19 +48,18 @@ BENCHMARK_DEFINE_F(PseudoDir, RemoveAdd)(benchmark::State& state) {
 
   int i = 0;
 
-  while (state.KeepRunning()) {
+  while (state->KeepRunning()) {
     dir->RemoveEntry(file_names[i]);
     dir->AddEntry(file_names[i], file);
     i = (i + 1) % file_names.size();
   }
+  return true;
 }
-
-BENCHMARK_REGISTER_F(PseudoDir, RemoveAdd)->Range(1, 1 << 14);
 
 // Benchmark the time to lookup an existing entry.
 // Parameterized by the number of files.
-BENCHMARK_DEFINE_F(PseudoDir, Lookup)(benchmark::State& state) {
-  auto file_names = MakeDeterministicNamesList(state.range(0));
+bool PseudoDirLookupTest(perftest::RepeatState* state, int file_count) {
+  auto file_names = MakeDeterministicNamesList(file_count);
 
   auto dir = fbl::AdoptRef(new fs::PseudoDir());
   auto file = fbl::AdoptRef(new fs::UnbufferedPseudoFile());
@@ -77,20 +71,19 @@ BENCHMARK_DEFINE_F(PseudoDir, Lookup)(benchmark::State& state) {
   int i = 0;
   fbl::RefPtr<fs::Vnode> out;
 
-  while (state.KeepRunning()) {
+  while (state->KeepRunning()) {
     dir->Lookup(&out, file_names[i]);
     i = (i + 1) % file_names.size();
   }
+  return true;
 }
-
-BENCHMARK_REGISTER_F(PseudoDir, Lookup)->Range(1, 1 << 14);
 
 // Benchmark the time to read out a directory.
 // Parameterized by the number of files and the size of the output buffer.
-BENCHMARK_DEFINE_F(PseudoDir, Readdir)(benchmark::State& state) {
-  auto file_names = MakeDeterministicNamesList(state.range(0));
-  size_t len = state.range(1);
-  std::vector<char> buffer(len);
+bool PseudoDirReaddirTest(perftest::RepeatState* state, int file_count,
+                          int buffer_size) {
+  auto file_names = MakeDeterministicNamesList(file_count);
+  std::vector<char> buffer(buffer_size);
 
   auto dir = fbl::AdoptRef(new fs::PseudoDir());
   auto file = fbl::AdoptRef(new fs::UnbufferedPseudoFile());
@@ -99,13 +92,38 @@ BENCHMARK_DEFINE_F(PseudoDir, Readdir)(benchmark::State& state) {
     dir->AddEntry(name, file);
   }
 
-  while (state.KeepRunning()) {
+  while (state->KeepRunning()) {
     fs::vdircookie_t cookie;
     size_t real_len;
-    while (dir->Readdir(&cookie, buffer.data(), len, &real_len) != ZX_OK) {
+    while (dir->Readdir(&cookie, buffer.data(), buffer.size(), &real_len) !=
+           ZX_OK) {
+    }
+  }
+  return true;
+}
+
+void RegisterTests() {
+  perftest::RegisterTest("PseudoDir/Create", PseudoDirCreateTest);
+
+  for (int file_count = 1; file_count <= 1 << 14; file_count *= 8) {
+    auto name = fbl::StringPrintf("PseudoDir/RemoveAdd/%d", file_count);
+    perftest::RegisterTest(name.c_str(), PseudoDirRemoveAddTest, file_count);
+  }
+
+  for (int file_count = 1; file_count <= 1 << 14; file_count *= 8) {
+    auto name = fbl::StringPrintf("PseudoDir/Lookup/%d", file_count);
+    perftest::RegisterTest(name.c_str(), PseudoDirLookupTest, file_count);
+  }
+
+  for (int buffer_size = 1; buffer_size <= 64; buffer_size *= 8) {
+    for (int file_count = 1; file_count <= 1 << 14; file_count *= 8) {
+      auto name = fbl::StringPrintf("PseudoDir/Readdir/%d/%dk", file_count,
+                                    buffer_size);
+      perftest::RegisterTest(name.c_str(), PseudoDirReaddirTest, file_count,
+                             buffer_size * 1024);
     }
   }
 }
+PERFTEST_CTOR(RegisterTests);
 
-BENCHMARK_REGISTER_F(PseudoDir, Readdir)
-    ->Ranges({{1, 1 << 14}, {1 << 10, 1 << 16}});
+}  // namespace
