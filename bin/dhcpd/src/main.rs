3b35dd792b29c670593ad97da2e5a0e8571904a6
Amanda Tait <atait@google.com>
2018-08-07 15:32:22 -0400

    [dhcpd] Add release expired lease
    This change adds a timer interval to main(), such that expired DHCP
    leases are recycled on each firing of the timer.
    
    NET-752 #progress
    
    TESTED=run dhcpd &; run dhcpd_test_client &;
    
    Change-Id: I96e3456d7a46d7a513d78ec8b4c79827ce192a8c

--- bin/dhcpd/src/main.rs
+++ bin/dhcpd/src/main.rs
@@ -6,11 +6,12 @@
 extern crate dhcp;
 extern crate failure;
 extern crate fuchsia_async as async;
+extern crate fuchsia_zircon as zx;
 extern crate futures;
 extern crate getopts;
 
-use async::Executor;
 use async::net::UdpSocket;
+use async::{Executor, Interval};
 use dhcp::configuration;
 use dhcp::protocol::{Message, SERVER_PORT};
 use dhcp::server::Server;
@@ -20,23 +21,37 @@ use futures::prelude::*;
 use getopts::Options;
 use std::env;
 use std::io;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::{IpAddr, SocketAddr};
 use std::sync::{Arc, Mutex};
+use zx::DurationNum;
 
 /// A buffer size in excess of the maximum allowable DHCP message size.
 const BUF_SZ: usize = 1024;
 const DEFAULT_CONFIG_PATH: &str = "/pkg/data/config.json";
+/// The rate in seconds at which expiration DHCP leases are recycled back into the managed address
+/// pool. The current value of 5 is meant to facilitate manual testing.
+// TODO(atait): Replace with Duration type after it has been updated to const fn.
+const EXPIRATION_INTERVAL_SECS: u64 = 5;
 
 fn main() -> Result<(), Error> {
-    println!("dhcpd: starting...");
     let mut exec = Executor::new().context("error creating executor")?;
-    let config_path = get_config_path()?;
-    let (server, server_ip) = build_server(config_path)?;
-    let udp_socket = bind_server_socket(server_ip)?;
-    build_and_run_event_loop(&mut exec, udp_socket, server)
+    let path = get_server_config_file_path()?;
+    let config = configuration::load_server_config_from_file(path)?;
+    let server_ip = config.server_ip;
+    let socket_addr = SocketAddr::new(IpAddr::V4(server_ip), SERVER_PORT);
+    let udp_socket = UdpSocket::bind(&socket_addr).context("unable to bind socket")?;
+    let server = Arc::new(Mutex::new(Server::from_config(config)));
+    let msg_handling_loop = define_msg_handling_loop_future(udp_socket, server.clone());
+    let lease_expiration_handler = define_lease_expiration_handler_future(server.clone());
+
+    println!("dhcpd: starting server");
+    exec.run_singlethreaded(msg_handling_loop.join(lease_expiration_handler))
+        .context("failed to start event loop")?;
+    println!("dhcpd: server shutting down");
+    Ok(())
 }
 
-fn get_config_path() -> Result<String, Error> {
+fn get_server_config_file_path() -> Result<String, Error> {
     let args: Vec<String> = env::args().collect();
     let program = &args[0];
     let mut opts = Options::new();
@@ -54,60 +69,55 @@ fn get_config_path() -> Result<String, Error> {
     }
 }
 
-fn build_server(path: String) -> Result<(Arc<Mutex<Server>>, Ipv4Addr), Error> {
-    let config = configuration::load_server_config_from_file(path)?;
-    let server_ip = config.server_ip;
-    Ok((Arc::new(Mutex::new(Server::from_config(config))), server_ip))
-}
-
-fn bind_server_socket(server_ip: Ipv4Addr) -> Result<UdpSocket, Error> {
-    let addr = SocketAddr::new(IpAddr::V4(server_ip), SERVER_PORT);
-    let udp_socket = UdpSocket::bind(&addr).context("unable to bind socket")?;
-    Ok(udp_socket)
-}
-
-fn build_and_run_event_loop(exec: &mut Executor, sock: UdpSocket, server: Arc<Mutex<Server>>) -> Result<(), Error> {
-    let event_loop = future::loop_fn(sock, |sock| {
+fn define_msg_handling_loop_future(
+    sock: UdpSocket, server: Arc<Mutex<Server>>,
+) -> impl Future<Item = UdpSocket, Error = Error> {
+    future::loop_fn(sock, move |sock| {
         // Cloning the server ARC and then moving the value into the first and_then() call
         // allows the server value to live multiple loop iterations.
         let server = server.clone();
         let buf = vec![0u8; BUF_SZ];
         sock.recv_from(buf)
-        .map_err(|_e| {
-            failure::err_msg("dhcpd: unable to receive buffer")
-        }).and_then(move |(sock, buf, received, addr)| {
-            println!("dhcpd: received {} bytes", received);
-            match Message::from_buffer(&buf) {
-                None => Err(failure::err_msg("dhcpd: unable to parse buffer")),
-                Some(msg) => {
-                    println!("dhcpd: msg parsed: {:?}", msg);
-                    // This call should not block because the server is single-threaded.
-                    match server.lock().unwrap().dispatch(msg) {
-                        None => Err(failure::err_msg("dhcpd: invalid Message")),
-                        Some(response) => {
-                            println!("dhcpd: msg dispatched to server {:?}", response);
-                            let response_buffer = response.serialize();
-                            println!("dhcpd: response serialized");
-                            return Ok((sock, response_buffer, addr));
+            .map_err(|_e| failure::err_msg("unable to receive buffer"))
+            .and_then(move |(sock, buf, received, addr)| {
+                println!("dhcpd: received {} bytes", received);
+                match Message::from_buffer(&buf) {
+                    None => Err(failure::err_msg("unable to parse buffer")),
+                    Some(msg) => {
+                        println!("dhcpd: msg parsed {:?}", msg);
+                        // This call should not block because the server is single-threaded.
+                        match server.lock().unwrap().dispatch(msg) {
+                            None => Err(failure::err_msg("invalid message")),
+                            Some(response) => {
+                                println!("dhcpd: msg dispatched to server {:?}", response);
+                                let response_buffer = response.serialize();
+                                println!("dhcpd: response serialized");
+                                Ok((sock, response_buffer, addr))
+                            }
                         }
                     }
                 }
-            }
-        }).map_err(|e| {
-            io::Error::new(io::ErrorKind::InvalidData, e.compat())
-        }).and_then(|result| {
-            let (sock, buf, addr) = result;
-            sock.send_to(buf, addr)
-        }).map_err(|e| {
-            e.context("dhcpd: unable to send response")
-        }).and_then(|sock| {
-            println!("dhcpd: response sent. Continuing event loop.");
-            Ok(Loop::Continue(sock))
-        })
-    });
+            }).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.compat()))
+            .and_then(|(sock, buf, addr)| sock.send_to(buf, addr))
+            .map_err(|e| e.context("unable to send response").into())
+            .and_then(|sock| {
+                println!("dhcpd: response sent");
+                println!("dhcpd: continuing event loop");
+                Ok(Loop::Continue(sock))
+            })
+    })
+}
 
-    println!("dhcpd: starting event loop...");
-    exec.run_singlethreaded(event_loop).context("could not run futures")?;
-    println!("dhcpd: shutting down...");
-    Ok(())
+fn define_lease_expiration_handler_future(
+    server: Arc<Mutex<Server>>,
+) -> impl Future<Item = (), Error = Error> {
+    let expiration_interval: Interval<Error> = Interval::new(EXPIRATION_INTERVAL_SECS.seconds());
+    expiration_interval
+        .for_each(move |()| {
+            println!("dhcpd: interval timer fired");
+            server.lock().unwrap().release_expired_leases();
+            println!("dhcpd: expired leases released");
+            Ok(())
+        }).map(|_| ())
+        .map_err(|e| e.context("interval timer failed").into())
 }
