56200131b377a6aad60a528a239c0f8db6efad81
James Tucker <raggi@google.com>
2018-08-08 13:37:28 -0700

    [bin/thermd] clang-format
    IN-371 #comment
    
    Test: CQ / n/a
    Change-Id: I82f3f81b87874931476dd7eede2d3a9de4e8b601

--- bin/thermd/thermd.cc
+++ bin/thermd/thermd.cc
@@ -4,8 +4,8 @@
 
 #include <zircon/device/sysinfo.h>
 #include <zircon/device/thermal.h>
-#include <zircon/syscalls/system.h>
 #include <zircon/syscalls.h>
+#include <zircon/syscalls/system.h>
 
 #include <lib/async-loop/cpp/loop.h>
 
@@ -23,265 +23,268 @@
 
 static zx_handle_t root_resource;
 
-static uint32_t pl1_mw; // current PL1 value
+static uint32_t pl1_mw;  // current PL1 value
 
 #define PL1_MIN 2500
 #define PL1_MAX 7000
 
-static constexpr uint32_t COOL_TEMP_THRESHOLD = 50; // degrees in kelvins below threshold before
-                                                    // we adjust PL value
+static constexpr uint32_t COOL_TEMP_THRESHOLD =
+    50;  // degrees in kelvins below threshold before
+         // we adjust PL value
 
 static zx_status_t get_root_resource(zx_handle_t* root_resource) {
-    int fd = open("/dev/misc/sysinfo", O_RDWR);
-    if (fd < 0) {
-        return ZX_ERR_NOT_FOUND;
-    }
+  int fd = open("/dev/misc/sysinfo", O_RDWR);
+  if (fd < 0) {
+    return ZX_ERR_NOT_FOUND;
+  }
 
-    ssize_t n = ioctl_sysinfo_get_root_resource(fd, root_resource);
-    close(fd);
-    if (n != sizeof(*root_resource)) {
-        if (n < 0) {
-            return (zx_status_t)n;
-        } else {
-            return ZX_ERR_NOT_FOUND;
-        }
+  ssize_t n = ioctl_sysinfo_get_root_resource(fd, root_resource);
+  close(fd);
+  if (n != sizeof(*root_resource)) {
+    if (n < 0) {
+      return (zx_status_t)n;
+    } else {
+      return ZX_ERR_NOT_FOUND;
     }
-    return ZX_OK;
+  }
+  return ZX_OK;
 }
 
 static zx_status_t set_pl1(uint32_t target) {
-    zx_system_powerctl_arg_t arg = {
-        .x86_power_limit = {
-            .power_limit = target,
-            .time_window = 0,
-            .clamp = 1,
-            .enable = 1,
-        },
-    };
-    zx_status_t st = zx_system_powerctl(root_resource, ZX_SYSTEM_POWERCTL_X86_SET_PKG_PL1, &arg);
-    if (st != ZX_OK) {
-        fprintf(stderr, "ERROR: Failed to set PL1 to %d: %d\n", target, st);
-        return st;
-    }
-    pl1_mw = target;
-    TRACE_COUNTER("thermal", "throttle", 0, "pl1", target);
-    return ZX_OK;
+  zx_system_powerctl_arg_t arg = {
+      .x86_power_limit =
+          {
+              .power_limit = target,
+              .time_window = 0,
+              .clamp = 1,
+              .enable = 1,
+          },
+  };
+  zx_status_t st = zx_system_powerctl(root_resource,
+                                      ZX_SYSTEM_POWERCTL_X86_SET_PKG_PL1, &arg);
+  if (st != ZX_OK) {
+    fprintf(stderr, "ERROR: Failed to set PL1 to %d: %d\n", target, st);
+    return st;
+  }
+  pl1_mw = target;
+  TRACE_COUNTER("thermal", "throttle", 0, "pl1", target);
+  return ZX_OK;
 }
 
 static uint32_t to_celsius(uint32_t val) {
-    // input is 10th of a kelvin
-    return (val * 10 - 27315) / 100;
+  // input is 10th of a kelvin
+  return (val * 10 - 27315) / 100;
 }
 
 static uint32_t to_kelvin(uint32_t celsius) __attribute__((unused));
 
 static uint32_t to_kelvin(uint32_t celsius) {
-    // return in 10th of a kelvin
-    return (celsius * 100 + 27315) / 10;
+  // return in 10th of a kelvin
+  return (celsius * 100 + 27315) / 10;
 }
 
-static zx_status_t thermal_device_added(int dirfd, int event, const char* name, void* cookie) {
-    if (event != WATCH_EVENT_ADD_FILE) {
-        return ZX_OK;
-    }
-    if (!strcmp("000", name)) {
-        // Device found, terminate watcher
-        return ZX_ERR_STOP;
-    } else {
-        return ZX_OK;
-    }
+static zx_status_t thermal_device_added(int dirfd, int event, const char* name,
+                                        void* cookie) {
+  if (event != WATCH_EVENT_ADD_FILE) {
+    return ZX_OK;
+  }
+  if (!strcmp("000", name)) {
+    // Device found, terminate watcher
+    return ZX_ERR_STOP;
+  } else {
+    return ZX_OK;
+  }
 }
 
 static void start_trace(void) {
-    // Create a message loop
-    static async::Loop loop(&kAsyncLoopConfigNoAttachToThread);
-    static trace::TraceProvider trace_provider(loop.dispatcher());
-    static bool started = false;
-    if (!started) {
-        printf("thermd: start trace\n");
-        loop.StartThread();
-        started = true;
-    }
+  // Create a message loop
+  static async::Loop loop(&kAsyncLoopConfigNoAttachToThread);
+  static trace::TraceProvider trace_provider(loop.dispatcher());
+  static bool started = false;
+  if (!started) {
+    printf("thermd: start trace\n");
+    loop.StartThread();
+    started = true;
+  }
 }
 
 static bool check_platform() {
-    unsigned int a, b, c, d;
-    unsigned int leaf_num = 0x80000002;
-    char brand_string[50];
-    memset(brand_string, 0, sizeof(brand_string));
-    for (int i = 0; i < 3; i++) {
-        if (!__get_cpuid(leaf_num + i, &a, &b, &c, &d)) {
-            return false;
-        }
-        memcpy(brand_string + (i * 16)     , &a, sizeof(uint32_t));
-        memcpy(brand_string + (i * 16) +  4, &b, sizeof(uint32_t));
-        memcpy(brand_string + (i * 16) +  8, &c, sizeof(uint32_t));
-        memcpy(brand_string + (i * 16) + 12, &d, sizeof(uint32_t));
+  unsigned int a, b, c, d;
+  unsigned int leaf_num = 0x80000002;
+  char brand_string[50];
+  memset(brand_string, 0, sizeof(brand_string));
+  for (int i = 0; i < 3; i++) {
+    if (!__get_cpuid(leaf_num + i, &a, &b, &c, &d)) {
+      return false;
     }
-    // Only run thermd for processors used in Pixelbooks. The PL1 min/max settings
-    // are specified by the chipset.
-    if (strstr(brand_string, "i5-7Y57") || strstr(brand_string, "i7-7Y75")) {
-        return true;
-    } else {
-        return false;
-    }
+    memcpy(brand_string + (i * 16), &a, sizeof(uint32_t));
+    memcpy(brand_string + (i * 16) + 4, &b, sizeof(uint32_t));
+    memcpy(brand_string + (i * 16) + 8, &c, sizeof(uint32_t));
+    memcpy(brand_string + (i * 16) + 12, &d, sizeof(uint32_t));
+  }
+  // Only run thermd for processors used in Pixelbooks. The PL1 min/max settings
+  // are specified by the chipset.
+  if (strstr(brand_string, "i5-7Y57") || strstr(brand_string, "i7-7Y75")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 int main(int argc, char** argv) {
-
-    if (!check_platform()) {
-        return 0;
-    }
-
-    printf("thermd: started\n");
-
-    start_trace();
-
-    zx_status_t st = get_root_resource(&root_resource);
-    if (st != ZX_OK) {
-        fprintf(stderr, "ERROR: Failed to get root resource: %d\n", st);
-        return -1;
-    }
-
-    zx_nanosleep(zx_deadline_after(ZX_SEC(3)));
-
-    int dirfd = open("/dev/class/thermal", O_DIRECTORY | O_RDONLY);
-    if (dirfd < 0) {
-        fprintf(stderr, "ERROR: Failed to open /dev/class/thermal: %d (errno %d)\n",
-                dirfd, errno);
-        return -1;
-    }
-
-    st = fdio_watch_directory(dirfd, thermal_device_added, ZX_TIME_INFINITE, NULL);
-
-    if (st != ZX_ERR_STOP) {
-        fprintf(stderr, "ERROR: watcher terminating without finding sensors, "
-                        "terminating thermd...\n");
-        return -1;
-    }
-
-    // first sensor is ambient sensor
-    // TODO: come up with a way to detect this is the ambient sensor
-    int fd = open("/dev/class/thermal/000", O_RDWR);
-    if (fd < 0) {
-        fprintf(stderr, "ERROR: Failed to open sensor: %d\n", fd);
-        return -1;
-    }
-
-    uint32_t temp;
-    ssize_t rc = read(fd, &temp, sizeof(temp));
-    if (rc != sizeof(temp)) {
-        return rc;
-    }
-    TRACE_COUNTER("thermal", "temp", 0, "ambient-c", to_celsius(temp));
-
-    thermal_info_t info;
-    rc = ioctl_thermal_get_info(fd, &info);
-    if (rc != sizeof(info)) {
-        fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
-        return rc;
-    }
-
-    TRACE_COUNTER("thermal", "trip-point", 0,
-                  "passive-c", to_celsius(info.passive_temp),
-                  "critical-c", to_celsius(info.critical_temp));
-
-    zx_handle_t h = ZX_HANDLE_INVALID;
-    rc = ioctl_thermal_get_state_change_event(fd, &h);
-    if (rc != sizeof(h)) {
-        fprintf(stderr, "ERROR: Failed to get event: %zd\n", rc);
-        return rc;
-    }
-
-    if (info.max_trip_count == 0) {
-        fprintf(stderr, "Trip points not supported, exiting\n");
-        return 0;
-    }
-
-    // Set a trip point
-    trip_point_t tp = {
-        .id = 0,
-        .temp = info.passive_temp,
-    };
-    rc = ioctl_thermal_set_trip(fd, &tp);
-    if (rc) {
-        fprintf(stderr, "ERROR: Failed to set trip point: %zd\n", rc);
-        return rc;
-    }
-
-    // Update info
-    rc = ioctl_thermal_get_info(fd, &info);
-    if (rc != sizeof(info)) {
-        fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
-        return rc;
-    }
-    TRACE_COUNTER("thermal", "trip-point", 0,
-                  "passive-c", to_celsius(info.passive_temp),
-                  "critical-c", to_celsius(info.critical_temp),
-                  "active0-c", to_celsius(info.active_trip[0]));
-
-    // set PL1 to 7 watts (EDP)
-    set_pl1(PL1_MAX);
-
-    for (;;) {
-        zx_signals_t observed = 0;
-        st = zx_object_wait_one(h, ZX_USER_SIGNAL_0,
-                                zx_deadline_after(ZX_SEC(1)),
-                                &observed);
-        if ((st != ZX_OK) && (st != ZX_ERR_TIMED_OUT)) {
-            fprintf(stderr, "ERROR: Failed to wait on event: %d\n", st);
-            return st;
-        }
-        if (observed & ZX_USER_SIGNAL_0) {
-            rc = ioctl_thermal_get_info(fd, &info);
-            if (rc != sizeof(info)) {
-                fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
-                return rc;
-            }
-            if (info.state) {
-
-                set_pl1(PL1_MIN); // decrease power limit
-
-                rc = read(fd, &temp, sizeof(temp));
-                if (rc != sizeof(temp)) {
-                    fprintf(stderr, "ERROR: Failed to read temperature: %zd\n", rc);
-                    return rc;
-                }
-            } else {
-                TRACE_COUNTER("thermal", "event", 0, "spurious", to_celsius(temp));
-            }
-        }
-        if (st == ZX_ERR_TIMED_OUT) {
-            rc = read(fd, &temp, sizeof(temp));
-            if (rc != sizeof(temp)) {
-                fprintf(stderr, "ERROR: Failed to read temperature: %zd\n", rc);
-                return rc;
-            }
-            TRACE_COUNTER("thermal", "temp", 0, "ambient-c", to_celsius(temp));
-
-            // increase power limit if the temperature dropped enough
-            if ((temp < info.active_trip[0] - COOL_TEMP_THRESHOLD) &&
-                (pl1_mw != PL1_MAX)) {
-                // make sure the state is clear
-                rc = ioctl_thermal_get_info(fd, &info);
-                if (rc != sizeof(info)) {
-                    fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
-                    return rc;
-                }
-                if (!info.state) {
-                    set_pl1(PL1_MAX);
-                }
-            }
-
-            if ((temp > info.active_trip[0]) && (pl1_mw != PL1_MIN)) {
-                set_pl1(PL1_MIN); // decrease power limit
-            }
-        }
-    }
-
-    close(fd);
-
-    printf("thermd terminating: %d\n", st);
-
+  if (!check_platform()) {
     return 0;
+  }
+
+  printf("thermd: started\n");
+
+  start_trace();
+
+  zx_status_t st = get_root_resource(&root_resource);
+  if (st != ZX_OK) {
+    fprintf(stderr, "ERROR: Failed to get root resource: %d\n", st);
+    return -1;
+  }
+
+  zx_nanosleep(zx_deadline_after(ZX_SEC(3)));
+
+  int dirfd = open("/dev/class/thermal", O_DIRECTORY | O_RDONLY);
+  if (dirfd < 0) {
+    fprintf(stderr, "ERROR: Failed to open /dev/class/thermal: %d (errno %d)\n",
+            dirfd, errno);
+    return -1;
+  }
+
+  st =
+      fdio_watch_directory(dirfd, thermal_device_added, ZX_TIME_INFINITE, NULL);
+
+  if (st != ZX_ERR_STOP) {
+    fprintf(stderr,
+            "ERROR: watcher terminating without finding sensors, "
+            "terminating thermd...\n");
+    return -1;
+  }
+
+  // first sensor is ambient sensor
+  // TODO: come up with a way to detect this is the ambient sensor
+  int fd = open("/dev/class/thermal/000", O_RDWR);
+  if (fd < 0) {
+    fprintf(stderr, "ERROR: Failed to open sensor: %d\n", fd);
+    return -1;
+  }
+
+  uint32_t temp;
+  ssize_t rc = read(fd, &temp, sizeof(temp));
+  if (rc != sizeof(temp)) {
+    return rc;
+  }
+  TRACE_COUNTER("thermal", "temp", 0, "ambient-c", to_celsius(temp));
+
+  thermal_info_t info;
+  rc = ioctl_thermal_get_info(fd, &info);
+  if (rc != sizeof(info)) {
+    fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
+    return rc;
+  }
+
+  TRACE_COUNTER("thermal", "trip-point", 0, "passive-c",
+                to_celsius(info.passive_temp), "critical-c",
+                to_celsius(info.critical_temp));
+
+  zx_handle_t h = ZX_HANDLE_INVALID;
+  rc = ioctl_thermal_get_state_change_event(fd, &h);
+  if (rc != sizeof(h)) {
+    fprintf(stderr, "ERROR: Failed to get event: %zd\n", rc);
+    return rc;
+  }
+
+  if (info.max_trip_count == 0) {
+    fprintf(stderr, "Trip points not supported, exiting\n");
+    return 0;
+  }
+
+  // Set a trip point
+  trip_point_t tp = {
+      .id = 0,
+      .temp = info.passive_temp,
+  };
+  rc = ioctl_thermal_set_trip(fd, &tp);
+  if (rc) {
+    fprintf(stderr, "ERROR: Failed to set trip point: %zd\n", rc);
+    return rc;
+  }
+
+  // Update info
+  rc = ioctl_thermal_get_info(fd, &info);
+  if (rc != sizeof(info)) {
+    fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
+    return rc;
+  }
+  TRACE_COUNTER("thermal", "trip-point", 0, "passive-c",
+                to_celsius(info.passive_temp), "critical-c",
+                to_celsius(info.critical_temp), "active0-c",
+                to_celsius(info.active_trip[0]));
+
+  // set PL1 to 7 watts (EDP)
+  set_pl1(PL1_MAX);
+
+  for (;;) {
+    zx_signals_t observed = 0;
+    st = zx_object_wait_one(h, ZX_USER_SIGNAL_0, zx_deadline_after(ZX_SEC(1)),
+                            &observed);
+    if ((st != ZX_OK) && (st != ZX_ERR_TIMED_OUT)) {
+      fprintf(stderr, "ERROR: Failed to wait on event: %d\n", st);
+      return st;
+    }
+    if (observed & ZX_USER_SIGNAL_0) {
+      rc = ioctl_thermal_get_info(fd, &info);
+      if (rc != sizeof(info)) {
+        fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
+        return rc;
+      }
+      if (info.state) {
+        set_pl1(PL1_MIN);  // decrease power limit
+
+        rc = read(fd, &temp, sizeof(temp));
+        if (rc != sizeof(temp)) {
+          fprintf(stderr, "ERROR: Failed to read temperature: %zd\n", rc);
+          return rc;
+        }
+      } else {
+        TRACE_COUNTER("thermal", "event", 0, "spurious", to_celsius(temp));
+      }
+    }
+    if (st == ZX_ERR_TIMED_OUT) {
+      rc = read(fd, &temp, sizeof(temp));
+      if (rc != sizeof(temp)) {
+        fprintf(stderr, "ERROR: Failed to read temperature: %zd\n", rc);
+        return rc;
+      }
+      TRACE_COUNTER("thermal", "temp", 0, "ambient-c", to_celsius(temp));
+
+      // increase power limit if the temperature dropped enough
+      if ((temp < info.active_trip[0] - COOL_TEMP_THRESHOLD) &&
+          (pl1_mw != PL1_MAX)) {
+        // make sure the state is clear
+        rc = ioctl_thermal_get_info(fd, &info);
+        if (rc != sizeof(info)) {
+          fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
+          return rc;
+        }
+        if (!info.state) {
+          set_pl1(PL1_MAX);
+        }
+      }
+
+      if ((temp > info.active_trip[0]) && (pl1_mw != PL1_MIN)) {
+        set_pl1(PL1_MIN);  // decrease power limit
+      }
+    }
+  }
+
+  close(fd);
+
+  printf("thermd terminating: %d\n", st);
+
+  return 0;
 }
--- bin/thermd/thermd_arm.cc
+++ bin/thermd/thermd_arm.cc
@@ -2,162 +2,174 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <zircon/device/sysinfo.h>
-#include <zircon/device/thermal.h>
-#include <zircon/device/gpu.h>
-#include <zircon/syscalls/system.h>
-#include <zircon/syscalls.h>
-#include <zircon/syscalls/port.h>
-#include <lib/fdio/watcher.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
+#include <lib/fdio/watcher.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <zircon/device/gpu.h>
+#include <zircon/device/sysinfo.h>
+#include <zircon/device/thermal.h>
+#include <zircon/syscalls.h>
+#include <zircon/syscalls/port.h>
+#include <zircon/syscalls/system.h>
 
 // TODO(braval): Combine thermd & thermd_arm and have a unified
 // code for the thermal deamon
-static zx_status_t thermal_device_added(int dirfd, int event, const char* name, void* cookie) {
-    if (event != WATCH_EVENT_ADD_FILE) {
-        return ZX_OK;
-    }
-    if (!strcmp("000", name)) {
-        // Device found, terminate watcher
-        return ZX_ERR_STOP;
-    } else {
-        return ZX_OK;
-    }
+static zx_status_t thermal_device_added(int dirfd, int event, const char* name,
+                                        void* cookie) {
+  if (event != WATCH_EVENT_ADD_FILE) {
+    return ZX_OK;
+  }
+  if (!strcmp("000", name)) {
+    // Device found, terminate watcher
+    return ZX_ERR_STOP;
+  } else {
+    return ZX_OK;
+  }
 }
 
 int main(int argc, char** argv) {
-    printf("thermd: started\n");
+  printf("thermd: started\n");
 
-    // TODO(braval): This sleep is not needed here but leaving it here
-    // since the Intel thermd has it. Clean up when both deamons are
-    // unified
-    zx_nanosleep(zx_deadline_after(ZX_SEC(3)));
+  // TODO(braval): This sleep is not needed here but leaving it here
+  // since the Intel thermd has it. Clean up when both deamons are
+  // unified
+  zx_nanosleep(zx_deadline_after(ZX_SEC(3)));
 
-    int dirfd = open("/dev/class/thermal", O_DIRECTORY | O_RDONLY);
-    if (dirfd < 0) {
-        fprintf(stderr, "ERROR: Failed to open /dev/class/thermal: %d (errno %d)\n",
-                dirfd, errno);
-        return -1;
-    }
+  int dirfd = open("/dev/class/thermal", O_DIRECTORY | O_RDONLY);
+  if (dirfd < 0) {
+    fprintf(stderr, "ERROR: Failed to open /dev/class/thermal: %d (errno %d)\n",
+            dirfd, errno);
+    return -1;
+  }
 
-    zx_status_t st = fdio_watch_directory(dirfd, thermal_device_added, ZX_TIME_INFINITE, NULL);
-    if (st != ZX_ERR_STOP) {
-        fprintf(stderr, "ERROR: watcher terminating without finding sensors, "
-                        "terminating thermd...\n");
-        return -1;
-    }
+  zx_status_t st =
+      fdio_watch_directory(dirfd, thermal_device_added, ZX_TIME_INFINITE, NULL);
+  if (st != ZX_ERR_STOP) {
+    fprintf(stderr,
+            "ERROR: watcher terminating without finding sensors, "
+            "terminating thermd...\n");
+    return -1;
+  }
 
-    // first device is the one we are interested
-    int fd = open("/dev/class/thermal/000", O_RDWR);
-    if (fd < 0) {
-        fprintf(stderr, "ERROR: Failed to open sensor: %d (errno %d) \n", fd, errno);
-        return -1;
-    }
+  // first device is the one we are interested
+  int fd = open("/dev/class/thermal/000", O_RDWR);
+  if (fd < 0) {
+    fprintf(stderr, "ERROR: Failed to open sensor: %d (errno %d) \n", fd,
+            errno);
+    return -1;
+  }
 
-    // first device is the one we are interested
-    int fd_gpu = open("/dev/class/gpu-thermal/000", O_RDONLY);
-    if (fd_gpu < 0) {
-        fprintf(stderr, "ERROR: Failed to open gpu: %d (errno %d) \n", fd_gpu, errno);
-        return -1;
-    }
+  // first device is the one we are interested
+  int fd_gpu = open("/dev/class/gpu-thermal/000", O_RDONLY);
+  if (fd_gpu < 0) {
+    fprintf(stderr, "ERROR: Failed to open gpu: %d (errno %d) \n", fd_gpu,
+            errno);
+    return -1;
+  }
 
-    thermal_device_info_t info;
-    ssize_t rc = ioctl_thermal_get_device_info(fd, &info);
-    if (rc != sizeof(info)) {
-        fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
-        return rc;
-    }
+  thermal_device_info_t info;
+  ssize_t rc = ioctl_thermal_get_device_info(fd, &info);
+  if (rc != sizeof(info)) {
+    fprintf(stderr, "ERROR: Failed to get thermal info: %zd\n", rc);
+    return rc;
+  }
 
-    if (info.num_trip_points == 0) {
-        fprintf(stderr, "Trip points not supported, exiting\n");
-        return 0;
-    }
-
-    if (!info.active_cooling && !info.passive_cooling) {
-        fprintf(stderr, "ERROR: No active or passive cooling present on device, "
-                        "terminating thermd...\n");
-        return 0;
-    }
-
-    zx_handle_t port = ZX_HANDLE_INVALID;
-    rc = ioctl_thermal_get_state_change_port(fd, &port);
-    if (rc != sizeof(port)) {
-        fprintf(stderr, "ERROR: Failed to get event: %zd\n", rc);
-        return rc;
-    }
-
-    for (;;) {
-        zx_port_packet_t packet;
-        st = zx_port_wait(port, ZX_TIME_INFINITE, &packet);
-        if (st != ZX_OK) {
-            fprintf(stderr, "ERROR: Failed to wait on port: %d\n", st);
-            return st;
-        }
-
-        uint32_t trip_idx = (uint32_t)packet.key;
-        if (trip_idx > info.num_trip_points) {
-            fprintf(stderr, "Invalid trip index: terminating thermd\n");
-            return -1;
-        }
-
-        if (info.passive_cooling) {
-            int32_t big_cluster_opp = info.trip_point_info[trip_idx].big_cluster_dvfs_opp;
-            int32_t little_cluster_opp = info.trip_point_info[trip_idx].little_cluster_dvfs_opp;
-            dvfs_info_t dvfs_info;
-
-            if (big_cluster_opp != -1) {
-                dvfs_info.power_domain   = BIG_CLUSTER_POWER_DOMAIN;
-                dvfs_info.op_idx         = big_cluster_opp;
-                rc = ioctl_thermal_set_dvfs_opp(fd, &dvfs_info);
-                if (rc) {
-                    fprintf(stderr, "ERROR: Failed to set DVFS OPP for big cluster: %zd\n", rc);
-                    return rc;
-                }
-            }
-
-            if (little_cluster_opp != -1) {
-                dvfs_info.power_domain   = LITTLE_CLUSTER_POWER_DOMAIN;
-                dvfs_info.op_idx         = little_cluster_opp;
-                rc = ioctl_thermal_set_dvfs_opp(fd, &dvfs_info);
-                if (rc) {
-                    fprintf(stderr, "ERROR: Failed to set DVFS OPP for little cluster: %zd\n", rc);
-                    return rc;
-                }
-            }
-        }
-
-        if (info.active_cooling) {
-            int32_t fan_level = info.trip_point_info[trip_idx].fan_level;
-            if (fan_level != -1) {
-                rc = ioctl_thermal_set_fan_level(fd, &fan_level);
-                if (rc) {
-                    fprintf(stderr, "ERROR: Failed to set fan level: %zd\n", rc);
-                    return rc;
-                }
-            }
-        }
-
-        if (info.gpu_throttling) {
-            int gpu_clk_freq_source = info.trip_point_info[trip_idx].gpu_clk_freq_source;
-            if (gpu_clk_freq_source != -1) {
-                rc = ioctl_gpu_set_clk_freq_source(fd_gpu, &gpu_clk_freq_source);
-                if (rc) {
-                    fprintf(stderr, "ERROR: Failed to change gpu clock freq source: %zd\n", rc);
-                    return rc;
-                }
-            }
-        }
-
-    }
-
-    close(fd);
-
-    printf("thermd terminating: %d\n", st);
-
+  if (info.num_trip_points == 0) {
+    fprintf(stderr, "Trip points not supported, exiting\n");
     return 0;
+  }
+
+  if (!info.active_cooling && !info.passive_cooling) {
+    fprintf(stderr,
+            "ERROR: No active or passive cooling present on device, "
+            "terminating thermd...\n");
+    return 0;
+  }
+
+  zx_handle_t port = ZX_HANDLE_INVALID;
+  rc = ioctl_thermal_get_state_change_port(fd, &port);
+  if (rc != sizeof(port)) {
+    fprintf(stderr, "ERROR: Failed to get event: %zd\n", rc);
+    return rc;
+  }
+
+  for (;;) {
+    zx_port_packet_t packet;
+    st = zx_port_wait(port, ZX_TIME_INFINITE, &packet);
+    if (st != ZX_OK) {
+      fprintf(stderr, "ERROR: Failed to wait on port: %d\n", st);
+      return st;
+    }
+
+    uint32_t trip_idx = (uint32_t)packet.key;
+    if (trip_idx > info.num_trip_points) {
+      fprintf(stderr, "Invalid trip index: terminating thermd\n");
+      return -1;
+    }
+
+    if (info.passive_cooling) {
+      int32_t big_cluster_opp =
+          info.trip_point_info[trip_idx].big_cluster_dvfs_opp;
+      int32_t little_cluster_opp =
+          info.trip_point_info[trip_idx].little_cluster_dvfs_opp;
+      dvfs_info_t dvfs_info;
+
+      if (big_cluster_opp != -1) {
+        dvfs_info.power_domain = BIG_CLUSTER_POWER_DOMAIN;
+        dvfs_info.op_idx = big_cluster_opp;
+        rc = ioctl_thermal_set_dvfs_opp(fd, &dvfs_info);
+        if (rc) {
+          fprintf(stderr,
+                  "ERROR: Failed to set DVFS OPP for big cluster: %zd\n", rc);
+          return rc;
+        }
+      }
+
+      if (little_cluster_opp != -1) {
+        dvfs_info.power_domain = LITTLE_CLUSTER_POWER_DOMAIN;
+        dvfs_info.op_idx = little_cluster_opp;
+        rc = ioctl_thermal_set_dvfs_opp(fd, &dvfs_info);
+        if (rc) {
+          fprintf(stderr,
+                  "ERROR: Failed to set DVFS OPP for little cluster: %zd\n",
+                  rc);
+          return rc;
+        }
+      }
+    }
+
+    if (info.active_cooling) {
+      int32_t fan_level = info.trip_point_info[trip_idx].fan_level;
+      if (fan_level != -1) {
+        rc = ioctl_thermal_set_fan_level(fd, &fan_level);
+        if (rc) {
+          fprintf(stderr, "ERROR: Failed to set fan level: %zd\n", rc);
+          return rc;
+        }
+      }
+    }
+
+    if (info.gpu_throttling) {
+      int gpu_clk_freq_source =
+          info.trip_point_info[trip_idx].gpu_clk_freq_source;
+      if (gpu_clk_freq_source != -1) {
+        rc = ioctl_gpu_set_clk_freq_source(fd_gpu, &gpu_clk_freq_source);
+        if (rc) {
+          fprintf(stderr,
+                  "ERROR: Failed to change gpu clock freq source: %zd\n", rc);
+          return rc;
+        }
+      }
+    }
+  }
+
+  close(fd);
+
+  printf("thermd terminating: %d\n", st);
+
+  return 0;
 }
