42e60da252e5bb2393d588068edb78f824fe6465
Benjamin Brittain <bwb@google.com>
2018-08-13 14:56:28 -0700

    [bt][rust][2018] Convert bt-fake-hci to Rust Edition 2018
    Test: Compiles
    
    Change-Id: I76a66061a1b76597376180ea832f8792854534e1

--- bin/bluetooth/tools/bt-fake-hci/src/main.rs
+++ bin/bluetooth/tools/bt-fake-hci/src/main.rs
@@ -2,34 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![deny(warnings)]
-
-extern crate failure;
-extern crate rand;
-extern crate fdio;
-extern crate fuchsia_bluetooth as bluetooth;
-extern crate fuchsia_zircon as zircon;
-
+#[deny(warnings)]
 use failure::Error;
+use fuchsia_bluetooth::hci;
+use rand::Rng;
 use std::fs::{File, OpenOptions};
 use std::path::Path;
 
-use bluetooth::hci;
-
-fn usage(appname: &str) -> (){
+fn usage(appname: &str) -> () {
     eprintln!("usage: {} [add|rm]", appname);
 }
 
 fn open_rdwr<P: AsRef<Path>>(path: P) -> Result<File, Error> {
-    OpenOptions::new().read(true).write(true).open(path).map_err(|e| e.into())
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(path)
+        .map_err(|e| e.into())
 }
 
 fn rm_device(dev_name: &str) -> Result<(), Error> {
     let path = Path::new(hci::DEV_TEST).join(dev_name);
     let dev = open_rdwr(path.clone())?;
 
-    hci::destroy_device(&dev)
-        .map(|_| println!("{:?} destroyed", path))
+    hci::destroy_device(&dev).map(|_| println!("{:?} destroyed", path))
 }
 
 fn main() {
@@ -53,7 +49,7 @@ fn main() {
                 usage(appname);
                 return;
             }
-            let _ = rm_device(args[2].as_str());
+            rm_device(args[2].as_str());
         }
         _ => {
             usage(appname);
