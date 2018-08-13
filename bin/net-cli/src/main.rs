4cbd4ead2d4ade9f9410b77e2c80f1cde80be61c
Tim Kilbourn <tkilbourn@google.com>
2018-08-13 01:05:08 -0700

    [net_cli] Use async/await
    Test: CQ + manual testing
    Change-Id: Ife0074453ae8e893c2b792b3be1a91b1d5d37e4d

--- bin/net-cli/src/main.rs
+++ bin/net-cli/src/main.rs
@@ -2,15 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![feature(futures_api, pin, arbitrary_self_types)]
+#![feature(async_await, await_macro, futures_api, pin, arbitrary_self_types)]
 #![deny(warnings)]
 
-use failure::{Error, Fail, ResultExt};
+use failure::{Error, ResultExt};
 use fidl_fuchsia_net as net;
 use fidl_fuchsia_net_stack::{self as netstack, StackMarker, StackProxy};
 use fuchsia_app::client::connect_to_service;
-use fuchsia_async::{self as fasync, unsafe_many_futures};
-use futures::prelude::*;
+use fuchsia_async as fasync;
 use structopt::StructOpt;
 
 mod opts;
@@ -23,112 +22,82 @@ fn main() -> Result<(), Error> {
     let mut exec = fasync::Executor::new().context("error creating event loop")?;
     let stack = connect_to_service::<StackMarker>().context("failed to connect to netstack")?;
 
-    match opt {
-        Opt::If(cmd) => exec.run_singlethreaded(do_if(cmd, stack)),
-        Opt::Fwd(cmd) => exec.run_singlethreaded(do_fwd(cmd, stack)),
-    }
+    let fut = async {
+        match opt {
+            Opt::If(cmd) => await!(do_if(cmd, stack)),
+            Opt::Fwd(cmd) => await!(do_fwd(cmd, stack)),
+        }
+    };
+    exec.run_singlethreaded(fut)
 }
 
-unsafe_many_futures!(
-    IfFut,
-    [List, GetInfo, Enable, Disable, AddrAdd, AddrDel, Error]
-);
-
-fn do_if(cmd: opts::IfCmd, stack: StackProxy) -> impl Future<Output = Result<(), Error>> {
+async fn do_if(cmd: opts::IfCmd, stack: StackProxy) -> Result<(), Error> {
     match cmd {
-        IfCmd::List => IfFut::List({
-            stack
-                .list_interfaces()
-                .map_err(|e| e.context("error getting response").into())
-                .map_ok(|response| {
-                    for info in response {
-                        println!("{}", pretty::InterfaceInfo::from(info));
-                    }
-                })
-        }),
-        IfCmd::Get { id } => IfFut::GetInfo({
-            stack
-                .get_interface_info(id)
-                .map_err(|e| e.context("error getting response").into())
-                .map_ok(move |response| {
-                    if let Some(e) = response.1 {
-                        println!("Error getting interface {}: {:?}", id, e)
-                    } else {
-                        println!("{}", pretty::InterfaceInfo::from(*response.0.unwrap()))
-                    }
-                })
-        }),
-        IfCmd::Enable { id } => IfFut::Enable({
-            stack
-                .enable_interface(id)
-                .map_err(|e| e.context("error getting response").into())
-                .map_ok(move |response| {
-                    if let Some(e) = response {
-                        println!("Error enabling interface {}: {:?}", id, e)
-                    } else {
-                        println!("Interface {} enabled", id)
-                    }
-                })
-        }),
-        IfCmd::Disable { id } => IfFut::Disable({
-            stack
-                .disable_interface(id)
-                .map_err(|e| e.context("error getting response").into())
-                .map_ok(move |response| {
-                    if let Some(e) = response {
-                        println!("Error disabling interface {}: {:?}", id, e)
-                    } else {
-                        println!("Interface {} disabled", id)
-                    }
-                })
-        }),
-        IfCmd::Addr(AddrCmd::Add { id, addr, prefix }) => IfFut::AddrAdd({
-            let parsed_addr = match parse_ip_addr(&addr) {
-                Ok(a) => a,
-                Err(e) => return IfFut::Error({ futures::future::ready(Err(e)) }),
-            };
+        IfCmd::List => {
+            let response = await!(stack.list_interfaces()).context("error getting response")?;
+            for info in response {
+                println!("{}", pretty::InterfaceInfo::from(info));
+            }
+        }
+        IfCmd::Get { id } => {
+            let response = await!(stack.get_interface_info(id)).context("error getting response")?;
+            if let Some(e) = response.1 {
+                println!("Error getting interface {}: {:?}", id, e)
+            } else {
+                println!("{}", pretty::InterfaceInfo::from(*response.0.unwrap()))
+            }
+        }
+        IfCmd::Enable { id } => {
+            let response = await!(stack.enable_interface(id)).context("error getting response")?;
+            if let Some(e) = response {
+                println!("Error enabling interface {}: {:?}", id, e)
+            } else {
+                println!("Interface {} enabled", id)
+            }
+        }
+        IfCmd::Disable { id } => {
+            let response = await!(stack.disable_interface(id)).context("error getting response")?;
+            if let Some(e) = response {
+                println!("Error disabling interface {}: {:?}", id, e)
+            } else {
+                println!("Interface {} disabled", id)
+            }
+        }
+        IfCmd::Addr(AddrCmd::Add { id, addr, prefix }) => {
+            let parsed_addr = parse_ip_addr(&addr)?;
             let mut fidl_addr = netstack::InterfaceAddress {
                 ip_address: parsed_addr,
                 prefix_len: prefix,
                 peer_address: None,
             };
-            stack
-                .add_interface_address(id, &mut fidl_addr)
-                .map_err(|e| e.context("error setting interface address").into())
-                .map_ok(move |response| {
-                    if let Some(e) = response {
-                        println!("Error adding interface address {}: {:?}", id, e)
-                    } else {
-                        println!(
-                            "Address {} added to interface {}",
-                            pretty::InterfaceAddress::from(&fidl_addr),
-                            id
-                        )
-                    }
-                })
-        }),
-        IfCmd::Addr(AddrCmd::Del { .. }) => IfFut::AddrDel({
+            let response = await!(stack.add_interface_address(id, &mut fidl_addr))
+                .context("error setting interface address")?;
+            if let Some(e) = response {
+                println!("Error adding interface address {}: {:?}", id, e)
+            } else {
+                println!("Address {} added to interface {}",
+                         pretty::InterfaceAddress::from(&fidl_addr),
+                         id)
+            }
+        }
+        IfCmd::Addr(AddrCmd::Del { .. }) => {
             println!("{:?} not implemented!", cmd);
-            futures::future::ready(Ok(()))
-        }),
+        }
     }
+    Ok(())
 }
 
-unsafe_many_futures!(FwdFut, [List,]);
-
-fn do_fwd(cmd: opts::FwdCmd, stack: StackProxy) -> impl Future<Output = Result<(), Error>> {
+async fn do_fwd(cmd: opts::FwdCmd, stack: StackProxy) -> Result<(), Error> {
     match cmd {
-        FwdCmd::List => FwdFut::List({
-            stack
-                .get_forwarding_table()
-                .map_err(|e| e.context("error retrieving forwarding table").into())
-                .map_ok(move |response| {
-                    for entry in response {
-                        println!("{}", pretty::ForwardingEntry::from(entry));
-                    }
-                })
-        }),
+        FwdCmd::List => {
+            let response = await!(stack.get_forwarding_table())
+                .context("error retrieving forwarding table")?;
+            for entry in response {
+                println!("{}", pretty::ForwardingEntry::from(entry));
+            }
+        }
     }
+    Ok(())
 }
 
 fn parse_ip_addr(addr: &str) -> Result<net::IpAddress, Error> {
