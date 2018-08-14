2bacccacc6b7c8d5afda0a70e9138e6e41e3bac3
Dustin Green <dustingreen@google.com>
2018-08-14 02:39:30 -0700

    [Codec] use DeviceWatcher for HW codecs
    Using DeviceWatcher allows late discovery of HW codecs, such as when a
    HW codec's devhost crashes and re-starts.
    
    Device discovery is kept in order, and more-recently-discovered devices
    take priority over less-recently-discovered, mainly to allow for async
    noticing that an old device is gone while a replacment has already
    started.
    
    Tested: use_h264_decoder_test, kill devhost, use_h264_decoder_test
    Change-Id: Ib46cec34575fe38b26fa6a5eed578b704d7fd9bd

--- bin/media/codec_factory/codec_factory_app.cc
+++ bin/media/codec_factory/codec_factory_app.cc
@@ -7,6 +7,7 @@
 #include "codec_factory_impl.h"
 
 #include <fuchsia/mediacodec/cpp/fidl.h>
+#include <lib/fsl/io/device_watcher.h>
 #include <lib/svc/cpp/services.h>
 #include <trace-provider/provider.h>
 #include <zircon/device/media-codec.h>
@@ -15,19 +16,38 @@
 
 namespace codec_factory {
 
-namespace {}  // namespace
+namespace {
+
+constexpr char kDeviceClass[] = "/dev/class/media-codec";
+
+}  // namespace
 
 CodecFactoryApp::CodecFactoryApp(async::Loop* loop) : loop_(loop) {
   // TODO(dustingreen): Determine if this is useful and if we're holding it
   // right.
-  trace::TraceProvider trace_provider(loop->dispatcher());
+  trace::TraceProvider trace_provider(loop_->dispatcher());
 
   // We pump |loop| in here, so it's important that
   // component::StartupContext::CreateFromStartupInfo() happen after
   // DiscoverMediaCodecDrivers(), else the pumping of the loop will drop the
   // incoming request for CodecFactory before AddServiceForName() below has had
   // a chance to register for it.
-  DiscoverMediaCodecDrivers();
+  DiscoverMediaCodecDriversAndListenForMoreAsync();
+
+  // We _rely_ on the driver to either fail the channel or send OnCodecList().
+  // We don't set a timeout here because under different conditions this could
+  // take different duration.
+  zx_status_t run_result;
+  do {
+    run_result = loop_->Run(zx::time::infinite(), true);
+  } while (run_result == ZX_OK && !existing_devices_discovered_);
+  if (run_result != ZX_OK) {
+    // ignore/skip the driver that failed the channel already
+    // The ~codec_factory takes care of un-binding.
+    return;
+  }
+  FXL_LOG(INFO)
+      << "discovery of all pre-existing devices done - serving CodecFactory";
 
   // We delay doing this until we're completely ready to add services, because
   // CreateFromStartupInfo() binds to |loop| implicitly, so we don't want any
@@ -47,82 +67,6 @@ CodecFactoryApp::CodecFactoryApp(async::Loop* loop) : loop_(loop) {
       fuchsia::mediacodec::CodecFactory::Name_);
 }
 
-void CodecFactoryApp::DiscoverMediaCodecDrivers() {
-  // TODO(dustingreen): Enumerate/watch the /dev/class/media-codec dir for
-  // devices, including across devhost failure/replacement. For the moment we
-  // just open device 000, and don't try to re-open it should it fail.  The
-  // DeviceWatcher class can help with this.
-  constexpr char kDeviceName[] = "/dev/class/media-codec/000";
-
-  int fd = ::open(kDeviceName, O_RDONLY);
-  if (fd < 0) {
-    printf("Failed to open \"%s\" (res %d errno %d)\n", kDeviceName, fd, errno);
-    return;
-  }
-
-  zx::channel client_factory_channel;
-  ssize_t res =
-      ::fdio_ioctl(fd, MEDIA_CODEC_IOCTL_GET_CODEC_FACTORY_CHANNEL, nullptr, 0,
-                   &client_factory_channel, sizeof(client_factory_channel));
-  ::close(fd);
-
-  if (res != sizeof(client_factory_channel)) {
-    printf("Failed to obtain channel (res %zd)\n", res);
-    // ignore/skip the driver that failed the ioctl
-    return;
-  }
-
-  std::shared_ptr<fuchsia::mediacodec::CodecFactoryPtr> codec_factory =
-      std::make_shared<fuchsia::mediacodec::CodecFactoryPtr>();
-  codec_factory->set_error_handler([this, factory = codec_factory.get()] {
-    hw_codecs_.remove_if(
-        [factory](const std::unique_ptr<CodecListEntry>& entry) {
-          return factory == entry->factory.get();
-        });
-  });
-
-  fidl::VectorPtr<fuchsia::mediacodec::CodecDescription> driver_codec_list;
-  FXL_DCHECK(!driver_codec_list);
-  codec_factory->events().OnCodecList =
-      [&driver_codec_list](
-          fidl::VectorPtr<fuchsia::mediacodec::CodecDescription> codec_list) {
-        driver_codec_list = std::move(codec_list);
-      };
-  codec_factory->Bind(std::move(client_factory_channel), loop_->dispatcher());
-  // We _rely_ on the driver to either fail the channel or send OnCodecList().
-  // We don't set a timeout here because under different conditions this could
-  // take different duration.
-  zx_status_t run_result;
-  do {
-    run_result = loop_->Run(zx::time::infinite(), true);
-  } while (run_result == ZX_OK && !driver_codec_list);
-  if (run_result != ZX_OK) {
-    // ignore/skip the driver that failed the channel already
-    // The ~codec_factory takes care of un-binding.
-    return;
-  }
-  // We're no longer interested in OnCodecList events from the driver's
-  // CodecFactory, should the driver send any more.  Sending more is not legal,
-  // but disconnect this event just in case, since we don't want the old lambda
-  // that touches local_codec_list.
-  codec_factory->events().OnCodecList =
-      [](fidl::VectorPtr<fuchsia::mediacodec::CodecDescription> codec_list) {};
-
-  FXL_DCHECK(driver_codec_list);
-  for (auto& codec_description : driver_codec_list.get()) {
-    FXL_LOG(INFO) << "CodecFactory::DiscoverMediaCodecDrivers() registering:"
-                  << " codec_type: "
-                  << fidl::ToUnderlying(codec_description.codec_type)
-                  << " mime_type: " << codec_description.mime_type;
-    hw_codecs_.emplace_back(std::make_unique<CodecListEntry>(CodecListEntry{
-        .description = std::move(codec_description),
-        .factory = std::move(codec_factory),
-    }));
-  }
-
-  printf("CodecFactory::DiscoverMediaCodecDrivers() success.\n");
-}
-
 const fuchsia::mediacodec::CodecFactoryPtr* CodecFactoryApp::FindHwDecoder(
     fit::function<bool(const fuchsia::mediacodec::CodecDescription&)>
         is_match) {
@@ -135,6 +79,204 @@ const fuchsia::mediacodec::CodecFactoryPtr* CodecFactoryApp::FindHwDecoder(
     return nullptr;
   }
   return (*iter)->factory.get();
+}
+
+void CodecFactoryApp::DiscoverMediaCodecDriversAndListenForMoreAsync() {
+  // We use fsl::DeviceWatcher::CreateWithIdleCallback() instead of
+  // fsl::DeviceWatcher::Create() because the CodecFactory service is started on
+  // demand, and we don't want to start serving CodecFactory until we've
+  // discovered and processed all existing media-codec devices.  That way, the
+  // first time a client requests a HW-backed codec, we robustly consider all
+  // codecs provided by pre-existing devices.  The request for a HW-backed Codec
+  // will have a much higher probability of succeeding vs. if we just discovered
+  // pre-existing devices async.  This doesn't prevent the possiblity that the
+  // device might not exist at the moment the CodecFactory is started, but as
+  // long as the device does exist by then, this will ensure the device's codecs
+  // are considered, including for the first client request.
+  device_watcher_ = fsl::DeviceWatcher::CreateWithIdleCallback(
+      kDeviceClass,
+      [this](int dir_fd, std::string filename) {
+        FXL_LOG(INFO) << "DeviceWatcher callback got filename: " << filename;
+
+        std::string device_path = std::string(kDeviceClass) + "/" + filename;
+
+        int fd = ::openat(dir_fd, filename.c_str(), O_RDONLY);
+        if (fd < 0) {
+          // This is ok (locally here) if the device (filename) really did go
+          // away in the meantime.
+          int snap_errno = errno;
+          FXL_LOG(INFO) << "Failed to open device by filename - resulting fd: "
+                        << fd << " errno: " << snap_errno
+                        << " device_path: " << device_path;
+          return;
+        }
+
+        zx::channel client_factory_channel;
+        ssize_t res = ioctl_media_codec_get_codec_factory_channel(
+            fd, client_factory_channel.reset_and_get_address());
+        ::close(fd);
+
+        if (res != sizeof(client_factory_channel)) {
+          // This could be ok (locally here) if the device's devhost exited
+          // already or similar.
+          FXL_LOG(INFO) << "Failed to obtain channel - res: " << res
+                        << " device_path: " << device_path;
+          // ignore/skip the driver that failed the ioctl
+          return;
+        }
+
+        // From here on in the current lambda, we're doing stuff that can't fail
+        // here locally (at least, not without exiting the whole process).  The
+        // error handler will handle channel error async.
+
+        auto discovery_entry = std::make_unique<DeviceDiscoveryEntry>();
+        discovery_entry->device_path = device_path;
+
+        discovery_entry->codec_factory =
+            std::make_shared<fuchsia::mediacodec::CodecFactoryPtr>();
+        discovery_entry->codec_factory->set_error_handler(
+            [this, device_path,
+             factory = discovery_entry->codec_factory.get()] {
+              FXL_LOG(INFO) << "Device's CodecFactoryPtr error handler - "
+                               "cancelling discovery or de-registering: "
+                            << device_path;
+              // Any given factory won't be in both lists, but will be in one or
+              // the other by the time this error handler runs.
+              device_discovery_queue_.remove_if(
+                  [factory](
+                      const std::unique_ptr<DeviceDiscoveryEntry>& entry) {
+                    return factory == entry->codec_factory.get();
+                  });
+              // Perhaps the removed discovery item was the first item in the
+              // list; maybe now the new first item in the list can be
+              // processed.
+              PostDiscoveryQueueProcessing();
+
+              hw_codecs_.remove_if(
+                  [factory](const std::unique_ptr<CodecListEntry>& entry) {
+                    return factory == entry->factory.get();
+                  });
+            });
+
+        discovery_entry->codec_factory->events().OnCodecList =
+            [this, discovery_entry = discovery_entry.get()](
+                fidl::VectorPtr<fuchsia::mediacodec::CodecDescription>
+                    codec_list) {
+              discovery_entry->driver_codec_list = std::move(codec_list);
+              // In case discovery_entry is the first item which is now ready to
+              // process, process the discovery queue.
+              PostDiscoveryQueueProcessing();
+
+              // We're no longer interested in OnCodecList events from the
+              // driver's CodecFactory, should the driver send any more. Sending
+              // more is not legal, but disconnect this event just in case,
+              // since we don't want the old lambda that touches
+              // driver_codec_list (this lambda).
+              discovery_entry->codec_factory->events().OnCodecList = nullptr;
+            };
+
+        discovery_entry->codec_factory->Bind(std::move(client_factory_channel),
+                                             loop_->dispatcher());
+
+        device_discovery_queue_.emplace_back(std::move(discovery_entry));
+      },
+      [this] {
+        FXL_LOG(INFO) << "DeviceWatcher idle_callback";
+        // The idle_callback indicates that all pre-existing devices have been
+        // seen, and by the time this item reaches the front of the discovery
+        // queue, all pre-existing devices have all been processed.
+        device_discovery_queue_.emplace_back(
+            std::make_unique<DeviceDiscoveryEntry>());
+        PostDiscoveryQueueProcessing();
+      });
+
+  FXL_LOG(INFO)
+      << "CodecFactory::DiscoverMediaCodecDriversAndListenForMoreAsync() ran.";
+}
+
+void CodecFactoryApp::PostDiscoveryQueueProcessing() {
+  async::PostTask(
+      loop_->dispatcher(),
+      fit::bind_member(this, &CodecFactoryApp::ProcessDiscoveryQueue));
+}
+
+void CodecFactoryApp::ProcessDiscoveryQueue() {
+  // Both startup and steady-state use this processing loop.
+  //
+  // In startup, we care about ordering of the discovery queue because we want
+  // to allow serving of CodecFactory as soon as all pre-existing devices are
+  // done processing.  We care that pre-existing devices are before
+  // newly-discovered devices in the queue.  As far as startup is concerned,
+  // there are other ways we could track this without using a queue, but a queue
+  // works, and using the queue allows startup to share code with steady-state.
+  //
+  // In steady-state, we care (a little) about ordering of the discovery queue
+  // because we want (to a limited degree, for now) to prefer a
+  // more-recently-discovered device over a less-recently-discovered device (for
+  // now at least), so to make that robust, we preserve the device discovery
+  // order through the codec discovery sequence, to account for the possibility
+  // that an previously discovered device may have only just recently sent
+  // OnCodecList before failing; without the device_discovery_queue_ that
+  // previously-discovered device's OnCodecList could re-order vs. the
+  // replacement device's OnCodecList.
+  //
+  // The device_discovery_queue_ marginally increases the odds of a client
+  // request picking up a replacement devhost instead of an old devhost that
+  // failed quickly and which we haven't yet noticed is gone.  This devhost
+  // replacement case is the main motivation for caring about the device
+  // discovery order in the first place (at least for now), since it should be
+  // robustly the case that discovery of the old devhost happens before
+  // discovery of the replacement devhost.
+  //
+  // The ordering of the hw_codec_ list is the main way in which
+  // more-recently-discovered codecs are prefered over less-recently-discovered
+  // codecs.  The device_discovery_queue_ just makes the hw_codec_ ordering
+  // exactly correspond to the device discovery order (reversed) even when
+  // devices are discovered near each other in time.
+  //
+  // None of this changes the fact that a replacement devhost's arrival can race
+  // with a client's request, so if a devhost fails and is replaced, it's quite
+  // possible the client will see the Codec interface just fail.  Even if this
+  // were mitigated, it wouldn't change the fact that a devhost failure later
+  // would result in Codec interface failure at that time, so failures near the
+  // start aren't really much different than async failures later. It can make
+  // sense for a client to retry a low number of times (if the client wants to
+  // work despite a devhost not always fully working), even if the Codec failure
+  // happens quite early.
+  while (!device_discovery_queue_.empty()) {
+    std::unique_ptr<DeviceDiscoveryEntry>& front =
+        device_discovery_queue_.front();
+    if (!front->codec_factory) {
+      // All pre-existing devices have been processed.
+      //
+      // Now the CodecFactory can begin serving (shortly).
+      existing_devices_discovered_ = true;
+      // The marker has done its job, so remove the marker.
+      device_discovery_queue_.pop_front();
+      return;
+    }
+
+    if (!front->driver_codec_list) {
+      // The first item is not yet ready.  The current method will get re-posted
+      // when the first item is potentially ready.
+      return;
+    }
+    FXL_DCHECK(front->driver_codec_list);
+
+    for (auto& codec_description : front->driver_codec_list.get()) {
+      FXL_LOG(INFO) << "registering - codec_type: "
+                    << fidl::ToUnderlying(codec_description.codec_type)
+                    << " mime_type: " << codec_description.mime_type
+                    << " device_path: " << front->device_path;
+      hw_codecs_.emplace_front(std::make_unique<CodecListEntry>(CodecListEntry{
+          .description = std::move(codec_description),
+          // shared_ptr<>
+          .factory = front->codec_factory,
+      }));
+    }
+
+    device_discovery_queue_.pop_front();
+  }
 }
 
 }  // namespace codec_factory
--- bin/media/codec_factory/codec_factory_app.h
+++ bin/media/codec_factory/codec_factory_app.h
@@ -11,8 +11,8 @@
 #include <fuchsia/mediacodec/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/component/cpp/startup_context.h>
-
-#include "lib/fxl/logging.h"
+#include <lib/fsl/io/device_watcher.h>
+#include <lib/fxl/logging.h>
 
 namespace codec_factory {
 
@@ -27,7 +27,7 @@ class CodecFactoryApp {
   // return from the caller's work on the FIDL thread, as the next item of work
   // on the FIDL thread could ~CodecFactoryPtr or similar.
   //
-  // This method can return nullptr if a HW decoder isn't found.
+  // This method can return nullptr if a HW decoder isn't found...
   const fuchsia::mediacodec::CodecFactoryPtr* FindHwDecoder(
       fit::function<bool(const fuchsia::mediacodec::CodecDescription&)>
           is_match);
@@ -37,16 +37,19 @@ class CodecFactoryApp {
  private:
   struct CodecListEntry {
     fuchsia::mediacodec::CodecDescription description;
+
     // When a HW-accelerated CodecFactory supports more than one sort of codec,
     // the CodecFactory will have multiple entries that share the CodecFactory
     // via the shared_ptr<> here.  The relevant entries co-own the
     // CodecFactoryPtr, and a shared_ptr<> ref is only transiently held by any
-    // other code (not posted; not sent across threads).  FWIW, this
-    // shared_ptr<> is used in a manner analogous to a Rust RC.
+    // other code (not posted; not sent across threads).
     std::shared_ptr<fuchsia::mediacodec::CodecFactoryPtr> factory;
   };
 
-  void DiscoverMediaCodecDrivers();
+  void DiscoverMediaCodecDriversAndListenForMoreAsync();
+
+  void PostDiscoveryQueueProcessing();
+  void ProcessDiscoveryQueue();
 
   std::unique_ptr<component::StartupContext> startup_context_;
 
@@ -81,7 +84,41 @@ class CodecFactoryApp {
   // first matching item in the list is selected, if any.
   //
   // This is only read or written from the main FIDL thread.
+  //
+  // As new devices are discovered, their codecs go at the start of the list and
+  // will be used in favor of previously-discovered devices.  If an old device
+  // devhost exits, its entry will be eventually removed from this list thanks
+  // to that device's local CodecFactory channel closing.
+  //
+  // This list is ordered by reverse discovery order.
   std::list<std::unique_ptr<CodecListEntry>> hw_codecs_;
+
+  std::unique_ptr<fsl::DeviceWatcher> device_watcher_;
+
+  // This queue is to ensure we process discovered devices in the order
+  // discovered, so that devices discovered later take priority over devices
+  // discovered earlier.  We can be concurrently waiting for more than one
+  // device's codec list, but we won't add a device's codec descriptions to
+  // hw_codecs_ until temporally after all previously-discovered devices.
+  //
+  // This list is ordered by discovery order.
+  struct DeviceDiscoveryEntry {
+    // !driver_codec_list until OnCodecList() has been seen from the
+    // codec_factory.
+    fidl::VectorPtr<fuchsia::mediacodec::CodecDescription> driver_codec_list;
+
+    // We don't really need a shared_ptr<> until hw_codecs_ (to allow it to be
+    // just a flat list).  However, using a shared_ptr<> here seems more
+    // readable than using unique_ptr<> here, especially given that
+    // fuchsia::mediacodec::CodecFactoryPtr is very similar to a unique_ptr<>
+    // itself.
+    std::shared_ptr<fuchsia::mediacodec::CodecFactoryPtr> codec_factory;
+
+    // Purely as FYI for log output.
+    std::string device_path;
+  };
+  std::list<std::unique_ptr<DeviceDiscoveryEntry>> device_discovery_queue_;
+  bool existing_devices_discovered_ = false;
 
   FXL_DISALLOW_COPY_AND_ASSIGN(CodecFactoryApp);
 };
