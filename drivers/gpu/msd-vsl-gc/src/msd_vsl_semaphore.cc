d23a1b826966c963e4db3cab1baae00b29cd9608
Craig Stout <cstout@google.com>
2018-08-14 19:48:11 +0100

    [msd-vsl-gc] Add semaphore
    Test:
    imx8mevk:MsdSemaphore
    
    Change-Id: Iea3924c6af1025e1c57ead34895adc78ed7ee55d

--- drivers/gpu/msd-vsl-gc/src/msd_vsl_semaphore.cc
+++ drivers/gpu/msd-vsl-gc/src/msd_vsl_semaphore.cc
@@ -2,11 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "msd.h"
+#include "msd_vsl_semaphore.h"
 
 magma_status_t msd_semaphore_import(uint32_t handle, msd_semaphore_t** semaphore_out)
 {
-    return MAGMA_STATUS_UNIMPLEMENTED;
+    auto semaphore = magma::PlatformSemaphore::Import(handle);
+    if (!semaphore)
+        return DRET_MSG(MAGMA_STATUS_INVALID_ARGS, "couldn't import semaphore handle 0x%x", handle);
+    *semaphore_out =
+        new MsdVslAbiSemaphore(std::shared_ptr<magma::PlatformSemaphore>(std::move(semaphore)));
+    return MAGMA_STATUS_OK;
 }
 
-void msd_semaphore_release(msd_semaphore_t* semaphore) {}
+void msd_semaphore_release(msd_semaphore_t* semaphore)
+{
+    delete MsdVslAbiSemaphore::cast(semaphore);
+}
