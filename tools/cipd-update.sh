c598a93104740c99d607c6af920b5b60e7f095bf
Petr Hosek <phosek@google.com>
2018-08-15 10:33:56 -0700

    [tools] Move internal CIPD packages into its own ensure files
    There are both public and internal packages, we want public ones to
    be always installed.
    
    Test: CQ
    Change-Id: I01db6a57e18399f458d20e490672d1d6e73202a0

--- tools/cipd-update.sh
+++ tools/cipd-update.sh
@@ -9,10 +9,18 @@ readonly SCRIPT_ROOT="$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)"
 readonly FUCHSIA_ROOT="${SCRIPT_ROOT}/../.."
 readonly GARNET_ROOT="${FUCHSIA_ROOT}/garnet"
 readonly BUILDTOOLS_DIR="${FUCHSIA_ROOT}/buildtools"
+readonly CIPD="${BUILDTOOLS_DIR}/cipd"
 
 INTERNAL_ACCESS=false
-if [[ "$(${BUILDTOOLS_DIR}/cipd ls fuchsia_internal)" != "No matching packages." ]]; then
-  ${BUILDTOOLS_DIR}/cipd ensure -ensure-file "${SCRIPT_ROOT}/cipd.ensure" -root ${GARNET_ROOT} -log-level warning
+if [[ "$(${CIPD} ls fuchsia_internal)" != "No matching packages." ]]; then
   INTERNAL_ACCESS=true
 fi
 echo "internal_access = ${INTERNAL_ACCESS}" > "${SCRIPT_ROOT}/cipd.gni"
+
+declare -a ENSURE_FILES=("${SCRIPT_ROOT}/cipd.ensure")
+if $INTERNAL_ACCESS; then
+  ENSURE_FILES+=("${SCRIPT_ROOT}/cipd_internal.ensure")
+fi
+
+(sed '/^\$/!d' "${ENSURE_FILES[@]}" && sed '/^\$/d' "${ENSURE_FILES[@]}") |
+  ${CIPD} ensure -ensure-file - -root ${GARNET_ROOT} -log-level warning
