aa1e78e71fe990c0d53a7543c31c763dfff72e76
Derek Gonyeo <dgonyeo@google.com>
2018-08-15 16:22:13 -0700

    [gn] Add persist-logs.gni library
    Add a library to facilitate persisting logs with given tags to disk.
    
    Test: manual
    Change-Id: Iba0fbb98e5bb4253682b28cc39f5e0329e11b3a9

---
+++ gn/gen_persistent_log_config.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import json
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser('Generate a sysmgr config to persist logs')
+    parser.add_argument('name', help='Name of the config')
+    parser.add_argument('path', help='Path to the package file')
+    parser.add_argument('--tags', help='Tag to filter for', nargs='+')
+    parser.add_argument('--ignore-tags', help='Tag to ignore', nargs='+')
+    args = parser.parse_args()
+
+    tag_args = []
+    if args.tags:
+        for t in args.tags:
+            tag_args += [ '--tag', t ]
+
+    ignore_tag_args = []
+    if args.ignore_tags:
+        for t in args.ignore_tags:
+            ignore_tag_args += [ '--ignore-tag', t ]
+
+    with open(args.path, 'w') as f:
+        json.dump({'apps':[["log_listener", "--file", "/data/logs."+args.name] +
+            tag_args + ignore_tag_args] }, f)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
