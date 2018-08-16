4ced8282db84378f588b063b807814f59125e971
Abdulla Kamar <abdulla@google.com>
2018-08-16 15:30:20 +1000

    [board][machina] Update arm64 addresses
    Move all addresses to 0x800000000 or above.
    
    Test: Ran "zircon_guest" and "debian_guest".
    Change-Id: Ibffc896d1936101e22f381504a57b8e183fbfd69

--- system/dev/board/machina/machina.h
+++ system/dev/board/machina/machina.h
@@ -7,14 +7,12 @@
 // Map all of 0-1GB into kernel space in one shot.
 #define PERIPHERAL_BASE_PHYS (0)
 #define PERIPHERAL_BASE_SIZE (0x40000000UL) // 1GB
-#define PERIPHERAL_BASE_VIRT (0xffffffffc0000000ULL) // -1GB
 
 // Individual peripherals in this mapping.
-#define RTC_BASE            (PERIPHERAL_BASE_VIRT + 0x09010000)
-#define RTC_BASE_PHYS       (PERIPHERAL_BASE_PHYS + 0x09010000)
-#define RTC_SIZE            (0x00001000)
-#define PCIE_MMIO_BASE_PHYS ((zx_paddr_t)(PERIPHERAL_BASE_PHYS + 0x10000000))
-#define PCIE_MMIO_SIZE      (0x2eff0000)
-#define PCIE_ECAM_BASE_PHYS ((zx_paddr_t)(PERIPHERAL_BASE_PHYS + 0x3f000000))
-#define PCIE_ECAM_SIZE      (0x01000000)
+#define PCIE_ECAM_BASE_PHYS ((zx_paddr_t)(PERIPHERAL_BASE_PHYS + 0x808100000))
+#define PCIE_ECAM_SIZE      (0x100000)
+#define PCIE_MMIO_BASE_PHYS ((zx_paddr_t)(PERIPHERAL_BASE_PHYS + 0x808200000))
+#define PCIE_MMIO_SIZE      (0x100000)
 #define PCIE_INT_BASE       (32)
+#define RTC_BASE_PHYS       ((zx_paddr_t)(PERIPHERAL_BASE_PHYS + 0x808301000))
+#define RTC_SIZE            (0x1000)
