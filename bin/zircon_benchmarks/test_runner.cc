8f6a43117cf711c3978c7b137fb728729468a820
Mark Seaborn <mseaborn@google.com>
2018-08-17 14:00:28 -0700

    [benchmarks] Cleanup: Remove use of the gflags library
    The command line argument processing that zircon_benchmarks does used
    to be more complicated: There used to be some other arguments that
    were handled by channels.cc.
    
    Now that we handle only a single string argument, there's no need to
    use the gflags library.
    
    Bug: none
    Test: /pkgfs/packages/zircon_benchmarks/0/test/zircon_benchmarks
    Change-Id: Ifcdb0ab567d95dcce1fdfc63f7172d34e100affe

--- bin/zircon_benchmarks/test_runner.cc
+++ bin/zircon_benchmarks/test_runner.cc
@@ -2,22 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <gflags/gflags.h>
 #include <perftest/perftest.h>
 
 #include "round_trips.h"
 
-// Command line argument used internally for launching subprocesses.
-DEFINE_string(subprocess, "", "Launch a process to run the named function");
-
 int main(int argc, char** argv) {
   // Check for the argument used by test cases for launching subprocesses.
-  // We check for it before calling gflags because gflags gives an error for
-  // any options it does not recognize, such as those accepted by
-  // perftest::PerfTestMain().
-  if (argc >= 2 && strcmp(argv[1], "--subprocess") == 0) {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
-    RunSubprocess(FLAGS_subprocess.c_str());
+  if (argc == 3 && strcmp(argv[1], "--subprocess") == 0) {
+    RunSubprocess(argv[2]);
     return 0;
   }
 
