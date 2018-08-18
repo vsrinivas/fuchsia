1c23e3117c26c2435fdf9144463ad49446069053
Gary Miguel <garymm@google.com>
2018-08-17 17:59:13 -0700

    [fidl] Implement compatibility test server in go.
    Test: Added the server to the compatibility test, it passes.
    Bug: FIDL-187 #comment
    Change-Id: I4426033d772168a954b82070c31cab418f9ac5b8

--- bin/fidl_compatibility_test/run_fidl_compatibility_test_garnet.sh
+++ bin/fidl_compatibility_test/run_fidl_compatibility_test_garnet.sh
@@ -1,4 +1,4 @@
 #!/boot/bin/sh
 
-export FIDL_COMPATIBILITY_TEST_SERVERS=fidl_compatibility_test_server_cpp
+export FIDL_COMPATIBILITY_TEST_SERVERS=fidl_compatibility_test_server_cpp,fidl_compatibility_test_server_go
 /pkgfs/packages/fidl_compatibility_test_bin/0/bin/app
