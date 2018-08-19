9fb59d74272a21a349327078072aa46b17bb5fc3
Michael Jurka <mikejurka@google.com>
2018-08-19 13:04:37 -0700

    [display_manager] Add copyright headers.
    TEST=no behavior change
    
    Change-Id: I0c9e8107780919c3e91f39be520b786c67af007f

--- bin/display_manager/display_manager_impl.cc
+++ bin/display_manager/display_manager_impl.cc
@@ -1,7 +1,12 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "display_manager_impl.h"
 #include "lib/fxl/logging.h"
 
 namespace display {
+
 DisplayManagerImpl::DisplayManagerImpl()
     : DisplayManagerImpl(component::StartupContext::CreateFromStartupInfo()) {}
 
