d6f127c1da8643be10feb6a15ba92922ef3dbf3c
Nathaniel Manista <nathaniel@google.com>
2018-08-22 16:52:03 +0000

    [fsl] WriteStringToSocket comment typo fix
    Test: Nope; this is a comment-only change.
    
    Change-Id: I5991820aa7a476c970fb637a66588f141e824800

--- public/lib/fsl/socket/strings.h
+++ public/lib/fsl/socket/strings.h
@@ -22,7 +22,7 @@ FXL_EXPORT bool BlockingCopyToString(zx::socket source, std::string* contents);
 FXL_EXPORT bool BlockingCopyFromString(fxl::StringView source,
                                        const zx::socket& destination);
 
-// Copies the string |contents| to a temporary socket and returns the
+// Copies the string |source| to a temporary socket and returns the
 // consumer handle.
 FXL_EXPORT zx::socket WriteStringToSocket(fxl::StringView source);
 
