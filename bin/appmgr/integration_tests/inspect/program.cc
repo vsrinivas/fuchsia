2aca701b1c19f8e65637791f9362800440437424
Christopher R. Johnson <crjohns@google.com>
2018-08-21 17:16:49 -0700

    [inspect][tests] Integration tests for inspection.
    TEST=fx run-test inspect_integration_tests
    CP-59: #comment
    
    Change-Id: I634268e2a8850150e16408c1cb5011c381ff4360

---
+++ bin/appmgr/integration_tests/inspect/program.cc
@@ -0,0 +1,64 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "lib/async-loop/cpp/loop.h"
+#include "lib/component/cpp/exposed_object.h"
+#include "lib/component/cpp/startup_context.h"
+
+const char* VALUE = "value";
+
+class Item : public component::ExposedObject {
+ public:
+  Item() : ExposedObject(UniqueName("item-")) {
+    object()->set_metric(VALUE, component::IntMetric(0));
+  }
+
+  uint64_t size() { return object()->name().size() + 8; }
+
+  void add_value(int64_t value) { object()->add_metric(VALUE, value); }
+};
+
+class Table : public component::ExposedObject {
+ public:
+  Table(const std::string& name) : ExposedObject("table-" + name) {
+    object()->set_metric(
+        {"item_size"},
+        component::CallbackMetric([this](component::Metric* out_metric) {
+          uint64_t sum = 0;
+          for (const auto& item : items_) {
+            sum += item->size();
+          }
+          out_metric->SetUInt(sum);
+        }));
+    object()->set_prop({"version"}, "1.0");
+  }
+
+  std::shared_ptr<Item> NewItem(int64_t value) {
+    auto ret = std::make_shared<Item>();
+    items_.emplace_back(ret);
+    ret->add_value(value);
+    ret->set_parent(object());
+    return ret;
+  }
+
+ private:
+  std::vector<std::shared_ptr<Item>> items_;
+};
+
+int main(int argc, const char** argv) {
+  async::Loop loop(&kAsyncLoopConfigAttachToThread);
+  auto context = component::StartupContext::CreateFromStartupInfo();
+
+  Table t1("t1"), t2("t2");
+  t1.set_parent(context->outgoing().object_dir());
+  t2.set_parent(context->outgoing().object_dir());
+
+  t1.NewItem(10);
+  t1.NewItem(100);
+
+  t2.NewItem(4);
+
+  loop.Run();
+  return 0;
+}
---
+++ bin/appmgr/integration_tests/inspect/test.cc
@@ -0,0 +1,106 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuchsia/inspect/cpp/fidl.h>
+#include <fuchsia/sys/cpp/fidl.h>
+#include <lib/async/default.h>
+#include <lib/fdio/util.h>
+
+#include "gmock/gmock.h"
+#include "lib/component/cpp/environment_services.h"
+#include "lib/component/cpp/testing/test_util.h"
+#include "lib/component/cpp/testing/test_with_environment.h"
+#include "lib/fxl/files/glob.h"
+#include "lib/fxl/strings/substitute.h"
+#include "lib/svc/cpp/services.h"
+
+namespace component {
+namespace {
+
+using ::fxl::Substitute;
+using ::testing::ElementsAre;
+using testing::EnclosingEnvironment;
+using ::testing::UnorderedElementsAre;
+
+const char kTestComponent[] = "inspect_test_app";
+
+class InspectTest : public component::testing::TestWithEnvironment {
+ protected:
+  InspectTest() {
+    fuchsia::sys::LaunchInfo launch_info;
+    launch_info.url = kTestComponent;
+
+    environment_ = CreateNewEnclosingEnvironment("test");
+    WaitForEnclosingEnvToStart(environment_.get());
+    environment_->CreateComponent(std::move(launch_info),
+                                  controller_.NewRequest());
+  }
+  ~InspectTest() { CheckShutdown(); }
+
+  void CheckShutdown() {
+    controller_->Kill();
+    bool done = false;
+    controller_.events().OnTerminated =
+        [&done](int64_t code, fuchsia::sys::TerminationReason reason) {
+          ASSERT_EQ(fuchsia::sys::TerminationReason::EXITED, reason);
+          done = true;
+        };
+    ASSERT_TRUE(
+        RunLoopWithTimeoutOrUntil([&done] { return done; }, zx::sec(10)));
+  }
+
+  std::string GetObjectPath(const std::string& relative_path) {
+    files::Glob glob(
+        Substitute("/hub/r/test/*/c/$0/*/out/objects", kTestComponent));
+    if (glob.size() == 0) {
+      return "";
+    }
+
+    std::string path = *glob.begin();
+
+    return Substitute("$0/$1", path, relative_path);
+  }
+
+  std::vector<std::string> GetGlob(const std::string& path) {
+    files::Glob glob(GetObjectPath("*"));
+    return std::vector<std::string>{glob.begin(), glob.end()};
+  }
+
+ private:
+  std::unique_ptr<EnclosingEnvironment> environment_;
+  fuchsia::sys::ComponentControllerPtr controller_;
+};
+
+TEST_F(InspectTest, InspectTopLevel) {
+  EXPECT_THAT(
+      GetGlob(GetObjectPath("*")),
+      ElementsAre(GetObjectPath("table-t1"), GetObjectPath("table-t2")));
+}
+
+MATCHER_P2(UIntMetric, name, value, "") {
+  return arg.key == name && arg.value.is_uint_value() &&
+         arg.value.uint_value() == (uint64_t)value;
+}
+
+TEST_F(InspectTest, InspectOpenRead) {
+  fuchsia::inspect::InspectSyncPtr inspect;
+
+  ASSERT_EQ(ZX_OK,
+            fdio_service_connect(GetObjectPath("table-t1/.channel").c_str(),
+                                 inspect.NewRequest().TakeChannel().release()));
+
+  fidl::VectorPtr<fidl::StringPtr> children;
+  ASSERT_EQ(ZX_OK, inspect->ListChildren(&children));
+  EXPECT_THAT(*children, ElementsAre("item-0x0", "item-0x1"));
+
+  fuchsia::inspect::Object obj;
+  ASSERT_EQ(ZX_OK, inspect->ReadData(&obj));
+  EXPECT_EQ("table-t1", obj.name);
+  EXPECT_THAT(*obj.properties, UnorderedElementsAre(fuchsia::inspect::Property(
+                                   {.key = "version", .value = "1.0"})));
+  EXPECT_THAT(*obj.metrics, UnorderedElementsAre(UIntMetric("item_size", 32)));
+}
+
+}  // namespace
+}  // namespace component
