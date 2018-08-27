4f48a08979fa8dbcff1a1ff580ad9e0ce8f61543
Gary Bressler <geb@google.com>
2018-08-27 14:08:36 -0700

    [cmx] Migrate more cmx files in cp to 'services'.
    This completes the migration of service sandboxes in core platform.
    
    TESTED=ran all garnet tests
    
    Change-Id: I3a2bd2094f2e25830a30c669e1a4ce4e4fa5f55f

--- bin/chrealm/integration_tests/get_message_client.cc
+++ bin/chrealm/integration_tests/get_message_client.cc
@@ -2,44 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstdio>
+#include <stdio.h>
 
 #include <fuchsia/sys/cpp/fidl.h>
 #include <fuchsia/testing/chrealm/cpp/fidl.h>
+#include <lib/async-loop/cpp/loop.h>
 #include <zircon/status.h>
 #include <zircon/types.h>
 #include <zx/channel.h>
-
-#include "lib/component/cpp/environment_services.h"
+#include "lib/component/cpp/startup_context.h"
 #include "lib/svc/cpp/services.h"
 
 int main(int argc, const char** argv) {
   if (argc != 1) {
-    fprintf(stderr, "Usage: %s", argv[0]);
+    fprintf(stderr, "Usage: %s\n", argv[0]);
     return 1;
   }
 
-  fuchsia::sys::EnvironmentSyncPtr env;
-  fuchsia::sys::ServiceProviderSyncPtr svc;
-  component::ConnectToEnvironmentService(env.NewRequest());
-  zx_status_t status = env->GetServices(svc.NewRequest());
-  if (status != ZX_OK) {
-    fprintf(stderr, "Getting services failed: %s\n",
-            zx_status_get_string(status));
-    return 1;
-  }
-  fuchsia::testing::chrealm::TestServiceSyncPtr test_svc;
-  status = svc->ConnectToService(fuchsia::testing::chrealm::TestService::Name_,
-                                 test_svc.NewRequest().TakeChannel());
-  if (status != ZX_OK) {
-    fprintf(stderr, "Connecting to service failed: %s\n",
-            zx_status_get_string(status));
-    return 1;
-  }
+  async::Loop loop(&kAsyncLoopConfigAttachToThread);
+  auto startup_context = component::StartupContext::CreateFromStartupInfo();
+  fuchsia::testing::chrealm::TestServicePtr test_svc;
+  startup_context->ConnectToEnvironmentService(test_svc.NewRequest());
 
-  fidl::StringPtr msg;
-  test_svc->GetMessage(&msg);
-  printf("%s", msg->c_str());
+  test_svc->GetMessage(
+      [&loop](fidl::StringPtr msg) {
+        printf("%s", msg->c_str());
+        loop.Quit();
+      });
+  loop.Run();
 
   return 0;
 }
