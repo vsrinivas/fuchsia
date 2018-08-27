f6989098421d7a28a5122e12c4cee982564e9e7f
Michael Jurka <mikejurka@google.com>
2018-08-27 11:19:58 -0700

    [scenic] Enable image_grid_cpp tests.
    Tests were previously commented out due to
    unresolved bash script errors.
    
    TEST=fx shell "/pkgfs/packages/garnet_benchmarks/0/bin/benchmarks.sh /tmp --catapult-converter-args --bots fake_bot_name --masters fuchsia.try --execution-timestamp-ms 1534438419097 --log-url https://fake_url"
    
    Change-Id: I0556f86ab42e885de22d1eb05a8a2cd6d10a86a4

--- tests/benchmarks/gfx_benchmarks.sh
+++ tests/benchmarks/gfx_benchmarks.sh
@@ -10,58 +10,65 @@
 # Scenic performance tests.
 RUN_SCENIC_BENCHMARK="/pkgfs/packages/scenic_benchmarks/0/bin/run_scenic_benchmark.sh"
 
+
+# Arguments to runbench_exec for scenic benchmarks:
+#
+# runbench_exec
+#   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
+#   "${RUN_SCENIC_BENCHMARK}"         # Scenic benchmark runner, followed by
+#                                     #   its arguments.
+#   "${OUT_DIR}"                      # Output directory.
+#   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
+#   "${BENCHMARK}"                    # Label for benchmark.
+#   "test_binary"                     # Binary that is being benchmarked.
+#   --unshadowed --clipping_disabled  # Arguments.
+
 # hello_scenic
-runbench_exec "${OUT_DIR}/scenic.hello_scenic_benchmark.json" \
-    "${RUN_SCENIC_BENCHMARK}" "${OUT_DIR}" \
-    "${OUT_DIR}/scenic.hello_scenic_benchmark.json" \
-    "scenic.hello_scenic_benchmark" \
+BENCHMARK="scenic.hello_scenic_benchmark"
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
+    "${RUN_SCENIC_BENCHMARK}"                \
+    "${OUT_DIR}"                             \
+    "${OUT_DIR}/${BENCHMARK}.json"           \
+    "${BENCHMARK}"                           \
     "hello_scenic"
 
-#BENCHMARK="scenic.hello_scenic_benchmark"
-#runbench_exec "${OUT_DIR}/scenic.hello_scenic_benchmark.json" \
-#    "${RUN_SCENIC_BENCHMARK}" \       # scenic benchmark runner
-#    "${OUT_DIR}" \                    # output directory
-#    "${OUT_DIR}/scenic.hello_scenic_benchmark.json" \  # output file path
-#    "${BENCHMARK}" \                  # label for benchmark
-#    "hello_scenic"
-
 # image_grid_cpp
-#BENCHMARK="scenic.image_grid_cpp_noclipping_noshadows_benchmark"
-#runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-#    "${RUN_SCENIC_BENCHMARK}" \       # scenic benchmark runner
-#    "${OUT_DIR}" \                    # output directory
-#    "${OUT_DIR}/${BENCHMARK}.json" \  # output file path
-#    "${BENCHMARK}" \                  # label for benchmark
-#    "set_root_view image_grid_cpp" --unshadowed --clipping_disabled
-#
-#BENCHMARK="scenic.image_grid_cpp_noshadows_benchmark"
-#runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-#    "${RUN_SCENIC_BENCHMARK}" \       # scenic benchmark runner
-#    "${OUT_DIR}" \                    # output directory
-#    "${OUT_DIR}/${BENCHMARK}.json" \  # output file path
-#    "${BENCHMARK}" \                  # label for benchmark
-#    "set_root_view image_grid_cpp" --unshadowed --clipping_enabled
-#
-#BENCHMARK="scenic.image_grid_cpp_ssdo_benchmark"
-#runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-#    "${RUN_SCENIC_BENCHMARK}" \       # scenic benchmark runner
-#    "${OUT_DIR}" \                    # output directory
-#    "${OUT_DIR}/${BENCHMARK}.json" \  # output file path
-#    "${BENCHMARK}" \                  # label for benchmark
-#    "set_root_view image_grid_cpp" --screen_space_shadows --clipping_enabled
-#
-#BENCHMARK="scenic.image_grid_cpp_shadow_map_benchmark"
-#runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-#    "${RUN_SCENIC_BENCHMARK}" \       # scenic benchmark runner
-#    "${OUT_DIR}" \                    # output directory
-#    "${OUT_DIR}/${BENCHMARK}.json" \  # output file path
-#    "${BENCHMARK}" \                  # label for benchmark
-#    "set_root_view image_grid_cpp" --shadow_map --clipping_enabled
-#
-#BENCHMARK="scenic.image_grid_cpp_moment_shadow_map_benchmark"
-#runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-#    "${RUN_SCENIC_BENCHMARK}" \       # scenic benchmark runner
-#    "${OUT_DIR}" \                    # output directory
-#    "${OUT_DIR}/${BENCHMARK}.json" \  # output file path
-#    "${BENCHMARK}" \                  # label for benchmark
-#    "set_root_view image_grid_cpp" --moment_shadow_map --clipping_enabled
+BENCHMARK="scenic.image_grid_cpp_noclipping_noshadows_benchmark"
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
+    "${RUN_SCENIC_BENCHMARK}"                \
+    "${OUT_DIR}"                             \
+    "${OUT_DIR}/${BENCHMARK}.json"           \
+    "${BENCHMARK}"                           \
+    "set_root_view image_grid_cpp" --unshadowed --clipping_disabled
+
+BENCHMARK="scenic.image_grid_cpp_noshadows_benchmark"
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
+    "${RUN_SCENIC_BENCHMARK}"                \
+    "${OUT_DIR}"                             \
+    "${OUT_DIR}/${BENCHMARK}.json"           \
+    "${BENCHMARK}"                           \
+    "set_root_view image_grid_cpp" --unshadowed --clipping_enabled
+
+BENCHMARK="scenic.image_grid_cpp_ssdo_benchmark"
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
+    "${RUN_SCENIC_BENCHMARK}"                \
+    "${OUT_DIR}"                             \
+    "${OUT_DIR}/${BENCHMARK}.json"           \
+    "${BENCHMARK}"                           \
+    "set_root_view image_grid_cpp" --screen_space_shadows --clipping_enabled
+
+BENCHMARK="scenic.image_grid_cpp_shadow_map_benchmark"
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
+    "${RUN_SCENIC_BENCHMARK}"                \
+    "${OUT_DIR}"                             \
+    "${OUT_DIR}/${BENCHMARK}.json"           \
+    "${BENCHMARK}"                           \
+    "set_root_view image_grid_cpp" --shadow_map --clipping_enabled
+
+BENCHMARK="scenic.image_grid_cpp_moment_shadow_map_benchmark"
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
+    "${RUN_SCENIC_BENCHMARK}"                \
+    "${OUT_DIR}"                             \
+    "${OUT_DIR}/${BENCHMARK}.json"           \
+    "${BENCHMARK}"                           \
+    "set_root_view image_grid_cpp" --moment_shadow_map --clipping_enabled
