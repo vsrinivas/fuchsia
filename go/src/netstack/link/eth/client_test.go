5f25509f9083219061c182587a620392bf20d6d9
Tamir Duberstein <tamird@gmail.com>
2018-08-28 16:35:17 -0400

    [netstack] use public interfaces in test
    TESTED=fx run-test netstack_link_eth_test
    
    Change-Id: I1bfba791706714c2fff35c9537e99f244ff4ec18

--- go/src/netstack/link/eth/client_test.go
+++ go/src/netstack/link/eth/client_test.go
@@ -2,51 +2,201 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(tamird): move this to package eth_test when
-// https://fuchsia-review.googlesource.com/c/garnet/+/181782 is submitted.
-package eth
+package eth_test
 
 import (
+	"math"
+	"syscall/zx"
 	"testing"
 
 	"fidl/zircon/ethernet"
+
+	"netstack/link/eth"
 )
 
+type device struct {
+	testing.TB
+
+	getInfo            func() (ethernet.Info, error)
+	getFifos           func() (int32, *ethernet.Fifos, error)
+	setIoBuffer        func(zx.VMO) (int32, error)
+	start              func() (int32, error)
+	stop               func() error
+	listenStart        func() (int32, error)
+	listenStop         func() error
+	setClientName      func(string) (int32, error)
+	getStatus          func() (uint32, error)
+	setPromiscuousMode func(bool) (int32, error)
+}
+
+func (d *device) GetInfo() (ethernet.Info, error) {
+	fn := d.getInfo
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to GetInfo")
+	}
+	return fn()
+}
+
+func (d *device) GetFifos() (int32, *ethernet.Fifos, error) {
+	fn := d.getFifos
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to GetFifos")
+	}
+	return fn()
+}
+
+func (d *device) SetIoBuffer(h zx.VMO) (int32, error) {
+	fn := d.setIoBuffer
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to SetIoBuffer")
+	}
+	return fn(h)
+}
+
+func (d *device) Start() (int32, error) {
+	fn := d.start
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to Start")
+	}
+	return fn()
+}
+
+func (d *device) Stop() error {
+	fn := d.stop
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to Stop")
+	}
+	return fn()
+}
+
+func (d *device) ListenStart() (int32, error) {
+	fn := d.listenStart
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to ListenStart")
+	}
+	return fn()
+}
+
+func (d *device) ListenStop() error {
+	fn := d.listenStop
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to ListenStop")
+	}
+	return fn()
+}
+
+func (d *device) SetClientName(name string) (int32, error) {
+	fn := d.setClientName
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to SetClientName")
+	}
+	return fn(name)
+}
+
+func (d *device) GetStatus() (uint32, error) {
+	fn := d.getStatus
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to GetStatus")
+	}
+	return fn()
+}
+
+func (d *device) SetPromiscuousMode(enabled bool) (int32, error) {
+	fn := d.setPromiscuousMode
+	if fn == nil {
+		d.Helper()
+		d.Fatal("unexpected call to SetPromiscuousMode")
+	}
+	return fn(enabled)
+}
+
 func TestClient_AllocForSend(t *testing.T) {
-	arena, err := NewArena()
+	arena, err := eth.NewArena()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	c := Client{
-		arena: arena,
-		fifos: ethernet.Fifos{
-			TxDepth: 1,
+	d := device{
+		TB: t,
+		getInfo: func() (ethernet.Info, error) {
+			return ethernet.Info{}, nil
+		},
+		setIoBuffer: func(zx.VMO) (int32, error) {
+			return int32(zx.ErrOk), nil
+		},
+		start: func() (int32, error) {
+			return int32(zx.ErrOk), nil
+		},
+		setClientName: func(string) (int32, error) {
+			return int32(zx.ErrOk), nil
 		},
 	}
 
-	if txDepthMin := uint32(1); c.fifos.TxDepth < txDepthMin {
-		t.Fatalf("%s is a no-op when txDepth is less than %d", t.Name(), txDepthMin)
-	}
+	saturateArena := func() (func(), uint32) {
+		// This value must be large enough to allow us to saturate the arena, but small enough to
+		// avoid running the system out of memory (the client allocates buffers of this length).
+		const txDepth = math.MaxUint16
 
-	// Arrange for arena allocation to fail.
-	arena.mu.freebufs = arena.mu.freebufs[:0]
+		d := d
+		d.getFifos = func() (int32, *ethernet.Fifos, error) {
+			return int32(zx.ErrOk), &ethernet.Fifos{
+				TxDepth: txDepth,
+			}, nil
+		}
+		d.stop = func() error {
+			return nil
+		}
+		c, err := eth.NewClient(t.Name(), "topo", &d, arena, func(eth.State) {})
+		if err != nil {
+			t.Fatal(err)
+		}
+		var allocations uint32
+		for c.AllocForSend() != nil {
+			allocations++
+		}
+		if allocations >= txDepth {
+			t.Fatalf("allocations = %d, txDepth = %d (want allocations < txDepth)", allocations, txDepth)
+		}
+		// txDepth > 1; txDepth < allocations (arena depth).
+		return c.Close, allocations/2 + 1
+	}
+	freeArena, txDepth := saturateArena()
+
+	c, err := func() (*eth.Client, error) {
+		d := d
+		d.getFifos = func() (int32, *ethernet.Fifos, error) {
+			return int32(zx.ErrOk), &ethernet.Fifos{
+				TxDepth: txDepth,
+			}, nil
+		}
+		return eth.NewClient(t.Name(), "topo", &d, arena, func(eth.State) {})
+	}()
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if got := c.AllocForSend(); got != nil {
-		t.Fatalf("AllocForSend() = %v, want %v", got, nil)
+		t.Fatalf("AllocForSend() = %v, want = %v (arena should be saturated)", got, nil)
 	}
 
-	// Arrange for arena allocation to succeed.
-	arena.mu.freebufs = arena.mu.freebufs[:cap(arena.mu.freebufs)]
+	freeArena()
 
-	// Saturate the client.
-	for i := c.fifos.TxDepth; i > 0; i-- {
-		if got := c.AllocForSend(); got == nil {
-			t.Fatalf("AllocForSend() = %v, want non-nil", got)
+	for i := uint32(0); i < txDepth; i++ {
+		if c.AllocForSend() == nil {
+			t.Fatalf("AllocForSend() = nil, want non-nil (client allocations = %d/%d)", i, txDepth)
 		}
 	}
 
 	if got := c.AllocForSend(); got != nil {
-		t.Fatalf("AllocForSend() = %v, want %v", got, nil)
+		t.Fatalf("AllocForSend() = %v, want = %v (client should be saturated)", got, nil)
 	}
 }
