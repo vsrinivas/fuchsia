3225abf0fa11750a9edf62f7ee9a8e51e8dfc71d
Michael Jurka <mikejurka@google.com>
2018-08-28 21:41:11 -0700

    [scenic] Benchmark image_grid_cpp with three instances
    TEST=fx shell "/pkgfs/packages/garnet_benchmarks/0/bin/benchmarks.sh /tmp --catapult-converter-args --bots fake_bot_name --masters fuchsia.try --execution-timestamp-ms 1534438419097 --log-url https://fake_url"
    
    Change-Id: Ieb7e610b04b0beee5953f891cc774631ba6f3ffc

--- tests/benchmarks/gfx_benchmarks.sh
+++ tests/benchmarks/gfx_benchmarks.sh
@@ -8,67 +8,83 @@
 # benchmarks.sh.
 
 # Scenic performance tests.
-RUN_SCENIC_BENCHMARK="/pkgfs/packages/scenic_benchmarks/0/bin/run_scenic_benchmark.sh"
 
-
-# Arguments to runbench_exec for scenic benchmarks:
+# bench(): Helper function for running Scenic benchmarks in this file.
 #
-# runbench_exec
-#   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
-#   "${RUN_SCENIC_BENCHMARK}"         # Scenic benchmark runner, followed by
-#                                     #   its arguments.
-#   "${OUT_DIR}"                      # Output directory.
-#   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
-#   "${BENCHMARK}"                    # Label for benchmark.
-#   "test_binary"                     # Binary that is being benchmarked.
-#   --unshadowed --clipping_disabled  # Arguments.
+# Arguments:
+#     $1         Label for benchmark.
+#     $2         Command that is being benchmarked.
+#     $3         Arguments to set_renderer_params
+bench () {
+  BENCHMARK=$1
+  COMMAND=$2
+  RENDERER_PARAMS=$3
+  # Example of using runbench_exec:
+  # runbench_exec
+  #   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
+  #   "${RUN_SCENIC_BENCHMARK}"         # Scenic benchmark runner, followed by
+  #                                     #   its arguments.
+  #   "${OUT_DIR}"                      # Output directory.
+  #   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
+  #   "${BENCHMARK}"                    # Label for benchmark.
+  #   "test_binary"                     # Command that is being benchmarked.
+  #   --unshadowed --clipping_disabled  # Arguments to set_renderer_params.
+  runbench_exec "${OUT_DIR}/${BENCHMARK}.json"                           \
+      "/pkgfs/packages/scenic_benchmarks/0/bin/run_scenic_benchmark.sh"  \
+      "${OUT_DIR}"                                                       \
+      "${OUT_DIR}/${BENCHMARK}.json"                                     \
+      "${BENCHMARK}"                                                     \
+      "${COMMAND}" ${RENDERER_PARAMS}
+}
 
+#
 # hello_scenic
-BENCHMARK="fuchsia.scenic.hello_scenic"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "hello_scenic"
+#
+bench "fuchsia.scenic.hello_scenic" "hello_scenic" ""
 
+#
 # image_grid_cpp
-BENCHMARK="fuchsia.scenic.image_grid_cpp_noclipping_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_cpp" --unshadowed --clipping_disabled
+#
+bench "fuchsia.scenic.image_grid_cpp_noclipping_noshadows" \
+      "set_root_view image_grid_cpp" \
+      "--unshadowed --clipping_disabled"
 
-BENCHMARK="fuchsia.scenic.image_grid_cpp_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_cpp" --unshadowed --clipping_enabled
+bench "fuchsia.scenic.image_grid_cpp_noshadows" \
+      "set_root_view image_grid_cpp" \
+      "--unshadowed --clipping_enabled"
 
-BENCHMARK="fuchsia.scenic.image_grid_cpp_ssdo"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_cpp" --screen_space_shadows --clipping_enabled
+bench "fuchsia.scenic.image_grid_cpp_ssdo" \
+      "set_root_view image_grid_cpp" \
+      "--screen_space_shadows --clipping_enabled"
 
-BENCHMARK="fuchsia.scenic.image_grid_cpp_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_cpp" --shadow_map --clipping_enabled
+bench "fuchsia.scenic.image_grid_cpp_shadow_map" \
+      "set_root_view image_grid_cpp" \
+      "--shadow_map --clipping_enabled"
 
-BENCHMARK="fuchsia.scenic.image_grid_cpp_moment_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_cpp" --moment_shadow_map --clipping_enabled
+bench "fuchsia.scenic.image_grid_cpp_moment_shadow_map" \
+      "set_root_view image_grid_cpp" \
+      "--moment_shadow_map --clipping_enabled"
+
+#
+# image_grid_cpp x3
+#
+IMAGE_GRID_CPP_X3_COMMAND="set_root_view tile_view image_grid_cpp image_grid_cpp image_grid_cpp"
+bench "fuchsia.scenic.image_grid_cpp_x3_noclipping_noshadows" \
+      "${IMAGE_GRID_CPP_X3_COMMAND}" \
+      "--unshadowed --clipping_disabled"
+
+bench "fuchsia.scenic.image_grid_cpp_x3_noshadows" \
+      "${IMAGE_GRID_CPP_X3_COMMAND}" \
+      "--unshadowed --clipping_enabled"
+
+bench "fuchsia.scenic.image_grid_cpp_x3_ssdo" \
+      "${IMAGE_GRID_CPP_X3_COMMAND}" \
+      "--screen_space_shadows --clipping_enabled"
+
+bench "fuchsia.scenic.image_grid_cpp_x3_shadow_map" \
+      "${IMAGE_GRID_CPP_X3_COMMAND}" \
+      "--shadow_map --clipping_enabled"
+
+bench "fuchsia.scenic.image_grid_cpp_x3_moment_shadow_map" \
+      "${IMAGE_GRID_CPP_X3_COMMAND}" \
+      "--moment_shadow_map --clipping_enabled"
