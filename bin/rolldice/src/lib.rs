31c71b4a44537ba6f618ba9eba9d09b9adb8faba
Tim Kilbourn <tkilbourn@google.com>
2018-08-29 21:40:47 -0700

    [rolldice] Update to Rust 2018
    Test: CQ (no functional changes)
    Change-Id: I970c2c9825f67307111370d1848830a46873857a

--- bin/rolldice/src/lib.rs
+++ bin/rolldice/src/lib.rs
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-extern crate rand;
-
 use rand::{Rand, Rng};
 use std::fmt;
 use std::iter::Iterator;
--- bin/rolldice/src/main.rs
+++ bin/rolldice/src/main.rs
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-extern crate rand;
-extern crate rolldice;
-#[macro_use] extern crate structopt;
-
 use rand::{thread_rng, Rng};
 use structopt::StructOpt;
 use rolldice::*;
