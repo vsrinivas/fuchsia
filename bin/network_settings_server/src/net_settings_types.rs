65dcbca93fe0c7416e0f5397d465d1e68956b73a
rubenbrunk <rubenbrunk@google.com>
2018-09-04 11:53:41 -0700

    [wlan-config] Initial check-in of wlan configuration server.
    - Adds server providing JSON RPC to allow configuration of a WiFi AP in
    experimental.
    
    NET-1428
    
    Test: Exercise RPC endpoints with curl, e.g.:
      In fx shell run server via:
      run fuchsia-pkg://fuchsia.com/network_settings_server#meta/network_settings_server.cmx
    
      On LAN run curl commands to interact with RPC endpoints, e.g.:
      curl -X GET '192.168.42.42:80/wan_info'
      curl -X PUT '192.168.42.42:80/wan_info' -d '{"addr":{"V4":[[1,1,2,3],32]},"dynamically_assigned":true}'
    
    Change-Id: Icaf8782a36e11b03d2ec366a393609db3f538cdb

---
+++ bin/network_settings_server/src/net_settings_types.rs
@@ -0,0 +1,30 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum NetAddress {
+    V4([u8; 4], u32),
+    V6([u8; 16], u32),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct WANConfiguration {
+    pub addr: NetAddress,
+    pub dynamically_assigned: bool, // Was addr assigned by dhcp?
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct DHCPConfiguration {
+    pub dns_server_addr: NetAddress,
+    pub netmask: NetAddress,
+    pub addr_range: (NetAddress, NetAddress),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct APConfiguration {
+    pub ssid: Vec<u8>,
+    pub password: String,
+}
