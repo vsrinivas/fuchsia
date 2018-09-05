8b0f8d9d8d350afd732d05ef68b91638cb204bc9
Alex Legg <alexlegg@google.com>
2018-09-05 10:41:49 +1000

    [linux_guest] Warn and create dummies when images don't exist
    Instead of failing to build when the images are not downloaded from
    CIPD, just print a warning and create a dummy image. The integration
    tests will fail in this case.
    
    TEST=Build with and without images
    
    Change-Id: I69e067f162037262a210b3f6d27c7c7921fb3434

---
+++ bin/guest/pkg/linux_guest/check_image.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+set -eo pipefail
+
+usage() {
+  echo "usage: ${0} {source_image} {destination_image}"
+  exit 1
+}
+
+if [ "$#" -ne 2 ]; then
+  usage
+fi
+
+declare -r SOURCE_IMAGE=${1}
+declare -r TARGET_IMAGE=${2}
+
+if [ -f "${SOURCE_IMAGE}" ]; then
+  cp -f "${SOURCE_IMAGE}" "${TARGET_IMAGE}"
+else
+  echo "WARNING: ${SOURCE_IMAGE} not found, using a dummy image. See" \
+       "garnet/bin/guest/README.md for manual build instructions."
+  touch "${TARGET_IMAGE}"
+fi
