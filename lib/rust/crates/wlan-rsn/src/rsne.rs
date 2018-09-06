9e4d3f5414288e58f6fdd27ad8e0ca4f0384383d
Kiet Tran <kiettran@google.com>
2018-09-05 20:00:26 -0700

    [wlan][rsn] Add RSN capabilities struct and RSNE test
    Add RSN capabilities bitfield struct as well as a test to verify that
    our RSNE representation matches what we expect from the original buffer.
    
    NET-1441 #comment
    
    Test: manual connection on protected WiFi; wlan_rsn_lib_test_rustc
    Change-Id: I6a8bc9bf14719a2d5a3c24e3403003e394355706

--- lib/rust/crates/wlan-rsn/src/rsne.rs
+++ lib/rust/crates/wlan-rsn/src/rsne.rs
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use bitfield::{bitfield, bitfield_debug, bitfield_fields, bitfield_struct};
 use bytes::{BufMut, Bytes};
 use crate::akm;
 use crate::cipher;
@@ -26,9 +27,30 @@ pub struct Rsne {
     pub group_data_cipher_suite: Option<cipher::Cipher>,
     pub pairwise_cipher_suites: Vec<cipher::Cipher>,
     pub akm_suites: Vec<akm::Akm>,
-    pub rsn_capabilities: Option<u16>,
+    pub rsn_capabilities: Option<RsnCapabilities>,
     pub pmkids: Vec<pmkid::Pmkid>,
     pub group_mgmt_cipher_suite: Option<cipher::Cipher>,
+}
+
+bitfield!{
+    #[derive(PartialOrd, PartialEq, Clone)]
+    pub struct RsnCapabilities(u16);
+    impl Debug;
+    preauth, set_preauth: 0;
+    no_pairwise, set_no_pairwise: 1;
+    ptksa_replay_counter, set_ptksa_replay_counter: 3, 2;
+    gtksa_replay_counter, set_gtksa_replay_counter: 5, 4;
+    mgmt_frame_protection_req, set_mgmt_frame_protection_req: 6;
+    mgmt_frame_protection_cap, set_mgmt_frame_protection_cap: 7;
+    joint_multiband, set_joint_multiband: 8;
+    peerkey_enabled, set_peerkey_enabled: 9;
+    ssp_amsdu_cap, set_ssp_amsdu_cap: 10;
+    ssp_amsdu_req, set_ssp_amsdu_req: 11;
+    pbac, set_pbac: 12;
+    extended_key_id, set_extended_key_id: 13;
+    // bit 14-15 reserved
+
+    value, _: 15, 0;
 }
 
 impl Rsne {
@@ -107,7 +129,7 @@ impl Rsne {
 
         match self.rsn_capabilities.as_ref() {
             None => return,
-            Some(caps) => buf.put_u16_le(*caps),
+            Some(caps) => buf.put_u16_le(caps.value()),
         };
 
         if self.pmkids.is_empty() {
@@ -155,7 +177,7 @@ named!(pub from_bytes<&[u8], Rsne>,
            pairwise_list: count!(cipher, pairwise_count.unwrap_or(0) as usize)  >>
            akm_count: if_remaining!(le_u16) >>
            akm_list: count!(akm, akm_count.unwrap_or(0) as usize)  >>
-           rsn_capabilities: if_remaining!(le_u16) >>
+           rsn_capabilities: if_remaining!(|x| { le_u16(x).map(RsnCapabilities) }) >>
            pmkid_count: if_remaining!(le_u16) >>
            pmkid_list: count!(read_pmkid, pmkid_count.unwrap_or(0) as usize)  >>
            group_mgmt_cipher_suite: if_remaining!(cipher) >>
@@ -181,7 +203,7 @@ mod tests {
     fn bench_parse_with_nom(b: &mut Bencher) {
         let frame: Vec<u8> = vec![
             0x30, 0x2A, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x04, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x04,
-            0x01, 0x00, 0x00, 0x0f, 0xac, 0x02, 0x00, 0x00, 0x01, 0x00, 0x01, 0x02, 0x03, 0x04,
+            0x01, 0x00, 0x00, 0x0f, 0xac, 0x02, 0xa8, 0x04, 0x01, 0x00, 0x01, 0x02, 0x03, 0x04,
             0x05, 0x06, 0x07, 0x08, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x00, 0x0f,
             0xac, 0x04,
         ];
@@ -192,7 +214,7 @@ mod tests {
     fn test_as_bytes() {
         let frame: Vec<u8> = vec![
             0x30, 0x2A, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x04, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x04,
-            0x01, 0x00, 0x00, 0x0f, 0xac, 0x02, 0x00, 0x00, 0x01, 0x00, 0x01, 0x02, 0x03, 0x04,
+            0x01, 0x00, 0x00, 0x0f, 0xac, 0x02, 0xa8, 0x04, 0x01, 0x00, 0x01, 0x02, 0x03, 0x04,
             0x05, 0x06, 0x07, 0x08, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x00, 0x0f,
             0xac, 0x04,
         ];
@@ -211,7 +233,7 @@ mod tests {
     fn test_short_buffer() {
         let frame: Vec<u8> = vec![
             0x30, 0x2A, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x04, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x04,
-            0x01, 0x00, 0x00, 0x0f, 0xac, 0x02, 0x00, 0x00, 0x01, 0x00, 0x01, 0x02, 0x03, 0x04,
+            0x01, 0x00, 0x00, 0x0f, 0xac, 0x02, 0xa8, 0x04, 0x01, 0x00, 0x01, 0x02, 0x03, 0x04,
             0x05, 0x06, 0x07, 0x08, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x00, 0x0f,
             0xac, 0x04,
         ];
@@ -224,5 +246,78 @@ mod tests {
         let left_over = buf.split_off(rsne_len);
         assert_eq!(&buf[..], &frame[..]);
         assert!(left_over.iter().all(|b| *b == 0));
+    }
+
+    #[test]
+    fn test_rsn_fields_representation() {
+        let frame: Vec<u8> = vec![
+            0x30, // element id
+            0x2A, // length
+            0x01, 0x00, // version
+            0x00, 0x0f, 0xac, 0x04, // group data cipher suite
+            0x01, 0x00, // pairwise cipher suite count
+            0x00, 0x0f, 0xac, 0x04, // pairwise cipher suite list
+            0x01, 0x00, // akm suite count
+            0x00, 0x0f, 0xac, 0x02, // akm suite list
+            0xa8, 0x04, // rsn capabilities
+            0x01, 0x00, // pmk id count
+
+            // pmk id list
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11,
+
+            0x00, 0x0f, 0xac, 0x04, // group management cipher suite
+        ];
+        let result = from_bytes(&frame);
+        assert!(result.is_done());
+        let rsne = result.unwrap().1;
+
+        assert_eq!(rsne.version, 1);
+        assert_eq!(rsne.len(), 0x2a + 2);
+
+        let oui: &[u8] = &[0x00, 0x0f, 0xac];
+        assert!(rsne.group_data_cipher_suite.is_some());
+        assert_eq!(rsne.group_data_cipher_suite,
+                   Some(cipher::Cipher{ oui: Bytes::from(oui), suite_type: cipher::CCMP_128}));
+        assert_eq!(rsne.pairwise_cipher_suites.len(), 1);
+        assert_eq!(rsne.pairwise_cipher_suites[0].oui, Bytes::from(oui));
+        assert_eq!(rsne.pairwise_cipher_suites[0].suite_type, cipher::CCMP_128);
+        assert_eq!(rsne.akm_suites.len(), 1);
+        assert_eq!(rsne.akm_suites[0].suite_type, akm::PSK);
+
+        let rsn_capabilities = rsne.rsn_capabilities.expect("should have RSN capabilities");
+        assert_eq!(rsn_capabilities.preauth(), false);
+        assert_eq!(rsn_capabilities.no_pairwise(), false);
+        assert_eq!(rsn_capabilities.ptksa_replay_counter(), 2);
+        assert_eq!(rsn_capabilities.gtksa_replay_counter(), 2);
+        assert!(!rsn_capabilities.mgmt_frame_protection_req());
+        assert!(rsn_capabilities.mgmt_frame_protection_cap());
+        assert!(!rsn_capabilities.joint_multiband());
+        assert!(!rsn_capabilities.peerkey_enabled());
+        assert!(rsn_capabilities.ssp_amsdu_cap());
+        assert!(!rsn_capabilities.ssp_amsdu_req());
+        assert!(!rsn_capabilities.pbac());
+        assert!(!rsn_capabilities.extended_key_id());
+
+        assert_eq!(rsn_capabilities.value(), 0xa8 + (0x04 << 8));
+
+        let pmkids: &[u8] = &[0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x0A, 0x0B,
+                              0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11];
+        assert_eq!(rsne.pmkids.len(), 1);
+        assert_eq!(rsne.pmkids[0], Bytes::from(pmkids));
+
+        assert_eq!(rsne.group_mgmt_cipher_suite,
+                   Some(cipher::Cipher{ oui: Bytes::from(oui), suite_type: cipher::CCMP_128}));
+    }
+
+    #[test]
+    fn test_rsn_capabilities_setters() {
+        let mut rsn_caps = RsnCapabilities(0u16);
+        rsn_caps.set_ptksa_replay_counter(2);
+        rsn_caps.set_gtksa_replay_counter(2);
+        rsn_caps.set_mgmt_frame_protection_cap(true);
+        rsn_caps.set_ssp_amsdu_cap(true);
+
+        assert_eq!(rsn_caps.value(), 0xa8 + (0x04 << 8));
     }
 }
