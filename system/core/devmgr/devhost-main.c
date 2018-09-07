539067305e91b9b2fc0d9e6486d4bae6adbf3f83
Roland McGrath <mcgrathr@google.com>
2018-09-07 13:26:04 -0700

    [devmgr][devhost][asan] Suppress ODR violation detection in devhost
    Bug: SEC-27
    Test: CQ & manual testing with multiple Garnet drivers separately using C++
    Change-Id: I127221f10fa573d2d1f48ee0230d147f06a718a9

--- system/core/devmgr/devhost-main.c
+++ system/core/devmgr/devhost-main.c
@@ -2,8 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <zircon/compiler.h>
+
 int device_host_main(int argc, char** argv);
 
 int main(int argc, char** argv) {
     device_host_main(argc, argv);
 }
+
+// All drivers have a pure C ABI.  But each individual driver might statically
+// link in its own copy of some C++ library code.  Since no C++ language
+// relationships leak through the driver ABI, each driver is its own whole
+// program from the perspective of the C++ language rules.  But the ASan
+// runtime doesn't understand this and wants to diagnose ODR violations when
+// the same global is defined in multiple drivers, which is likely with C++
+// library use.  There is no real way to teach the ASan instrumentation or
+// runtime about symbol visibility and isolated worlds within the program, so
+// the only thing to do is suppress the ODR violation detection.  This
+// unfortunately means real ODR violations within a single C++ driver won't be
+// caught either.
+#if __has_feature(address_sanitizer)
+#include <sanitizer/asan_interface.h>
+const char* __asan_default_options(void) {
+    return "detect_odr_violation=0";
+}
+#endif
