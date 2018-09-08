e327fc71637e122c01aef4c8ac27de28aa7bbd77
Arman Uguray <armansito@google.com>
2018-09-07 17:38:37 -0700

    [bt][tests] Support waiting for RemoteDevice events
    - HostTest now tracks RemoteDevice updates.
    - Added a new Future type for waiting for RemoteDevice updates based
    on a partial match. Generalized some of the common Future boilerplate.
    - test_start_discovery now expects a device state change.
    
    NET-1364
    TEST=bt-host-integration-tests
    
    Change-Id: I0bf277ed9fa9e395c9c5e0fbd11b1b4f7eff4199

--- bin/bluetooth/tests/integration/tests/host_integration.rs
+++ bin/bluetooth/tests/integration/tests/host_integration.rs
@@ -3,16 +3,10 @@
 // found in the LICENSE file.
 
 #![deny(warnings)]
-#![feature(
-    arbitrary_self_types,
-    async_await,
-    await_macro,
-    futures_api,
-    pin
-)]
+#![feature(arbitrary_self_types, async_await, await_macro, futures_api, pin)]
 
 use fidl_fuchsia_bluetooth::Bool;
-use fidl_fuchsia_bluetooth_control::{AdapterInfo, AdapterState};
+use fidl_fuchsia_bluetooth_control::{AdapterInfo, AdapterState, RemoteDevice};
 use fidl_fuchsia_bluetooth_host::{HostEvent, HostProxy};
 use fuchsia_async::{self as fasync, TimeoutExt};
 use fuchsia_bluetooth::error::Error as BtError;
@@ -26,9 +20,11 @@ use failure::{Error, ResultExt};
 use parking_lot::RwLock;
 use slab::Slab;
 use std::borrow::Borrow;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Write};
 use std::path::PathBuf;
+use std::pin::Pin;
 use std::sync::Arc;
 
 mod common;
@@ -49,6 +45,9 @@ struct HostTest {
     // Current bt-host adapter state.
     host_info: AdapterInfo,
 
+    // All known remote devices, indexed by their identifiers.
+    remote_devices: HashMap<String, RemoteDevice>,
+
     // Tasks that are interested in being woken up when the adapter state changes.
     host_state_tasks: Slab<task::Waker>,
 }
@@ -62,6 +61,7 @@ impl HostTest {
             host_path: host_path,
             host_proxy: host,
             host_info: info,
+            remote_devices: HashMap::new(),
             host_state_tasks: Slab::new(),
         }))
     }
@@ -73,7 +73,13 @@ impl HostTest {
             .try_for_each(move |evt| {
                 match evt {
                     HostEvent::OnAdapterStateChanged { state } => {
-                        test_state.write().handle_adapter_state_change(state);
+                        test_state.write().handle_adapter_state_changed(state);
+                    }
+                    HostEvent::OnDeviceUpdated { device } => {
+                        test_state.write().handle_device_updated(device);
+                    }
+                    HostEvent::OnDeviceRemoved { identifier } => {
+                        test_state.write().handle_device_removed(identifier);
                     }
                     // TODO(armansito): handle other events
                     evt => {
@@ -81,11 +87,12 @@ impl HostTest {
                     }
                 }
                 future::ready(Ok(()))
-            }).err_into()
+            })
+            .err_into()
     }
 
     // Returns a Future that resolves when the bt-host transitions to a state that matches the
-    // valid fields of |target|. For example, if |target_state| is
+    // valid fields of `target`. For example, if `target_state` is
     //
     //    AdapterState {
     //        local_name: None,
@@ -102,9 +109,30 @@ impl HostTest {
     fn on_adapter_state_change(
         test_state: HostTestPtr, target_state: AdapterState,
     ) -> impl Future<Output = Result<AdapterState, Error>> {
+        let err_msg = format!(
+            "timed out waiting for adapter state (expected: {:?})",
+            target_state
+        );
         AdapterStateFuture::new(test_state, target_state)
-            .on_timeout(TIMEOUT.seconds().after_now(), || {
-                Err(BtError::new("timed out waiting for adapter state change").into())
+            .on_timeout(TIMEOUT.seconds().after_now(), move || {
+                Err(BtError::new(err_msg.as_str()).into())
+            })
+    }
+
+    // Returns a Future that resolves when the state of a particular RemoteDevice matches
+    // `target_state`. If `id` is None, then the Future will resolve when any device matches
+    // `target_state`. Otherwise, the Future will resolve when the state of the requested device
+    // changes.
+    fn on_device_update<'a>(
+        test_state: HostTestPtr, id: Option<&'a str>, target_state: RemoteDeviceExpectation,
+    ) -> impl Future<Output = Result<(), Error>> + 'a {
+        let err_msg = format!(
+            "timed out waiting for remote device state (expected: {:?})",
+            target_state
+        );
+        RemoteDeviceStateFuture::new(test_state, id, Some(target_state))
+            .on_timeout(TIMEOUT.seconds().after_now(), move || {
+                Err(BtError::new(err_msg.as_str()).into())
             })
     }
 
@@ -123,7 +151,7 @@ impl HostTest {
     }
 
     // Handle the OnAdapterStateChanged event.
-    fn handle_adapter_state_change(&mut self, state_change: AdapterState) {
+    fn handle_adapter_state_changed(&mut self, state_change: AdapterState) {
         let base = match self.host_info.state {
             Some(ref state) => clone_host_state(state.borrow()),
             None => AdapterState {
@@ -138,6 +166,19 @@ impl HostTest {
         self.notify_host_state_changed();
     }
 
+    // Handle the OnDeviceUpdated event
+    fn handle_device_updated(&mut self, device: RemoteDevice) {
+        self.remote_devices
+            .insert(device.identifier.clone(), device);
+        self.notify_host_state_changed();
+    }
+
+    // Handle the OnDeviceRemoved event
+    fn handle_device_removed(&mut self, id: String) {
+        self.remote_devices.remove(&id);
+        self.notify_host_state_changed();
+    }
+
     fn notify_host_state_changed(&mut self) {
         for task in &self.host_state_tasks {
             task.1.wake();
@@ -146,7 +187,7 @@ impl HostTest {
     }
 }
 
-// Returns the value of |field| in |delta| if it's valid. Otherwise returns the value in |base|.
+// Returns the value of `field` in `delta` if it's valid. Otherwise returns the value in `base`.
 macro_rules! updated_field {
     ($base:ident, $delta:ident, $field:ident) => {
         match $delta.$field {
@@ -156,7 +197,7 @@ macro_rules! updated_field {
     };
 }
 
-// Applies |delta| to |base|.
+// Applies `delta` to `base`.
 fn apply_delta(base: AdapterState, delta: AdapterState) -> AdapterState {
     AdapterState {
         local_name: updated_field!(base, delta, local_name),
@@ -166,17 +207,45 @@ fn apply_delta(base: AdapterState, delta: AdapterState) -> AdapterState {
     }
 }
 
+// Macro for comparing optional fields within a target against fields in `base`. Fields of `target`
+// are assumed to have the Option type while the optionality of a `base` field is determined by
+// the syntax. For example, given:
+//
+//   struct A {
+//     foo: i64,
+//     bar: Option<String>,
+//   }
+//
+//   struct Target {
+//     foo: Option<i64>,
+//     bar: Option<String>,
+//   }
+//
+// Fields can be matched by invoking: compare_fields!(a, b, [foo, (bar)])
 macro_rules! compare_field {
+    ($base:ident, $target:ident, &$field:ident) => {
+        compare_field!($base, $target, &$field)
+    };
+
+    // Compare fields assuming the base field is not an Option type.
     ($base:ident, $target:ident, $field:ident) => {
-        match $target.$field {
+        match &$target.$field {
             None => None,
-            ref value => Some(*value == $base.$field),
+            Some(value) => Some(value == &$base.$field),
+        }
+    };
+
+    // Compare fields assuming the base field is an Option type.
+    ($base:ident, $target:ident, ($field:ident)) => {
+        match &$target.$field {
+            None => None,
+            value => Some(value == &$base.$field),
         }
     };
 }
 
 macro_rules! compare_fields {
-    ($base:ident, $target:ident, [$($field:ident),*]) => {{
+    ($base:ident, $target:ident, [$($field:tt),*]) => {{
         let mut all_none = true;
         $({
             if let Some(matched) = compare_field!($base, $target, $field) {
@@ -190,29 +259,71 @@ macro_rules! compare_fields {
     }};
 }
 
-// Returns true if all valid fields of |target| match their equivalents in |base|. Returns false if
-// there are any mismatches or if all fields of |target| are None.
+// Struct for comparing individual fields a RemoteDevice.
+#[derive(Clone, Debug, Default)]
+struct RemoteDeviceExpectation {
+    name: Option<String>,
+    connected: Option<bool>,
+    bonded: Option<bool>,
+}
+
+// Returns true if all valid fields of `target` match their equivalents in `base`. Returns false if
+// there are any mismatches or if all fields of `target` are None.
 fn compare_adapter_states(base: &AdapterState, target: &AdapterState) -> bool {
     compare_fields!(
         base,
         target,
-        [local_name, discoverable, discovering, local_service_uuids]
+        [
+            (local_name),
+            (discoverable),
+            (discovering),
+            (local_service_uuids)
+        ]
     )
 }
 
-struct AdapterStateFuture {
+fn compare_remote_device(base: &RemoteDevice, target: &RemoteDeviceExpectation) -> bool {
+    compare_fields!(base, target, [(name), connected, bonded])
+}
+
+struct StateUpdateFutureInner {
     test_state: HostTestPtr,
     waker_key: Option<usize>,
+}
+
+impl StateUpdateFutureInner {
+    fn new(test: HostTestPtr) -> StateUpdateFutureInner {
+        StateUpdateFutureInner {
+            test_state: test,
+            waker_key: None,
+        }
+    }
+
+    fn maybe_remove_waker(&mut self) {
+        if let Some(key) = self.waker_key {
+            self.test_state.write().remove_task(key);
+            self.waker_key = None;
+        }
+    }
+
+    fn store_task(&mut self, lw: &task::LocalWaker) {
+        let key = self.test_state.write().store_task(lw.clone().into_waker());
+        self.waker_key = Some(key);
+    }
+}
+
+// A future that resolves when the bt-host's AdapterState changes to a certain target value.
+struct AdapterStateFuture {
+    inner: StateUpdateFutureInner,
 
     // The expected adapter state.
     target_host_state: AdapterState,
 }
 
 impl AdapterStateFuture {
-    fn new(test: HostTestPtr, target_state: AdapterState) -> AdapterStateFuture {
+    fn new(test: HostTestPtr, target_state: AdapterState) -> Self {
         AdapterStateFuture {
-            test_state: test,
-            waker_key: None,
+            inner: StateUpdateFutureInner::new(test),
             target_host_state: target_state,
         }
     }
@@ -224,22 +335,89 @@ impl std::marker::Unpin for AdapterStateFuture {}
 impl Future for AdapterStateFuture {
     type Output = Result<AdapterState, Error>;
 
-    fn poll(mut self: std::pin::Pin<&mut Self>, lw: &task::LocalWaker) -> Poll<Self::Output> {
-        // Remove existing waker, if any.
-        if let Some(key) = self.waker_key {
-            self.test_state.write().remove_task(key);
-            self.waker_key = None;
-        }
-
-        let states_match: bool = match &self.test_state.read().host_info.state {
+    fn poll(mut self: Pin<&mut Self>, lw: &task::LocalWaker) -> Poll<Self::Output> {
+        self.inner.maybe_remove_waker();
+        let states_match: bool = match &self.inner.test_state.read().host_info.state {
             None => false,
             Some(state) => compare_adapter_states(state.borrow(), &self.target_host_state),
         };
         if states_match {
             Poll::Ready(Ok(clone_host_state(&self.target_host_state)))
         } else {
-            let key = self.test_state.write().store_task(lw.clone().into_waker());
-            self.waker_key = Some(key);
+            self.inner.store_task(lw);
+            Poll::Pending
+        }
+    }
+}
+
+// A future that resolves when the state of a remote device changes to a target value.
+struct RemoteDeviceStateFuture<'a> {
+    inner: StateUpdateFutureInner,
+
+    // The identifier of the desired RemoteDevice. If None, then this Future can resolve with any
+    // device.
+    target_dev_id: Option<&'a str>,
+
+    // The expected state a RemoteDevice is expected to reach. If the state is
+    // None then the Future will resolve when the device gets removed. If both `target_dev_state`
+    // and `target_dev_id` are None, then the Future will resolve when any device gets removed.
+    target_dev_state: Option<RemoteDeviceExpectation>,
+}
+
+impl<'a> RemoteDeviceStateFuture<'a> {
+    fn new(
+        test: HostTestPtr, target_id: Option<&'a str>,
+        target_state: Option<RemoteDeviceExpectation>,
+    ) -> Self {
+        RemoteDeviceStateFuture {
+            inner: StateUpdateFutureInner::new(test),
+            target_dev_id: target_id,
+            target_dev_state: target_state,
+        }
+    }
+
+    fn look_for_match(&self) -> bool {
+        match &self.target_dev_id {
+            None => self.match_any_device(&self.target_dev_state),
+            Some(id) => self.match_device_by_id(id, &self.target_dev_state),
+        }
+    }
+
+    fn match_any_device(&self, target: &Option<RemoteDeviceExpectation>) -> bool {
+        target.as_ref().map_or(false, |target| {
+            self.inner
+                .test_state
+                .read()
+                .remote_devices
+                .values()
+                .find(|dev| compare_remote_device(dev, &target))
+                .is_some()
+        })
+    }
+
+    fn match_device_by_id(&self, id: &'a str, target: &Option<RemoteDeviceExpectation>) -> bool {
+        match self.inner.test_state.read().remote_devices.get(id) {
+            None => target.is_none(),
+            Some(dev) => match target {
+                None => false,
+                Some(target) => compare_remote_device(dev, target),
+            },
+        }
+    }
+}
+
+impl<'a> std::marker::Unpin for RemoteDeviceStateFuture<'a> {}
+
+#[must_use = "futures do nothing unless polled"]
+impl<'a> Future for RemoteDeviceStateFuture<'a> {
+    type Output = Result<(), Error>;
+
+    fn poll(mut self: Pin<&mut Self>, lw: &task::LocalWaker) -> Poll<Self::Output> {
+        self.inner.maybe_remove_waker();
+        if self.look_for_match() {
+            Poll::Ready(Ok(()))
+        } else {
+            self.inner.store_task(lw);
             Poll::Pending
         }
     }
@@ -394,7 +572,8 @@ macro_rules! expect_eq {
             Err(BtError::new(&format!(
                 "failed - expected '{}', found: '{}'",
                 expected_val, actual_val
-            )).into())
+            ))
+            .into())
         }
     }};
 }
@@ -460,7 +639,6 @@ async fn test_discoverable(test_state: HostTestPtr) -> Result<(), Error> {
 }
 
 // Tests that host state updates when discovery is started and stopped.
-// TODO(armansito): Test for remote device events.
 // TODO(armansito): Test for FakeHciDevice state changes.
 async fn test_discovery(test_state: HostTestPtr) -> Result<(), Error> {
     // Start discovery. "discovering" should get set to true.
@@ -474,6 +652,20 @@ async fn test_discovery(test_state: HostTestPtr) -> Result<(), Error> {
     await!(HostTest::on_adapter_state_change(
         test_state.clone(),
         discovering_state
+    ))?;
+
+    // The host should discover a fake device.
+    // TODO(armansito): The name is currently hard-coded in
+    // garnet/drivers/bluetooth/hci/fake/fake-device.cpp:89. Configure this dynamically when it is
+    // supported.
+    let new_device = RemoteDeviceExpectation {
+        name: Some("Fake".to_string()),
+        ..Default::default()
+    };
+    await!(HostTest::on_device_update(
+        test_state.clone(),
+        None,
+        new_device
     ))?;
 
     // Stop discovery. "discovering" should get set to false.
