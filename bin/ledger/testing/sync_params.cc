bb85ed7c8d3fb132da8a341b0abd4d7a96facc87
Benjamin Lerman <qsr@chromium.org>
2018-09-11 17:58:32 +0200

    [ledger] Request the sync params from the network
    Infra bots will expose the sync params through the network. This CL
    updates SyncParams to request the credentials from the network if
    everything else fails.
    
    TEST=ledger_e2e_sync passes when exposing the sync params at:
         http://10.0.2.2:8081/ledger_e2e_sync_credentials
    
    LE-603 #done
    
    Change-Id: Icb37e2bc612f04d0496cf99f011a518ec5fab056

--- bin/ledger/testing/sync_params.cc
+++ bin/ledger/testing/sync_params.cc
@@ -6,12 +6,16 @@
 
 #include <iostream>
 
+#include <fuchsia/net/oldhttp/cpp/fidl.h>
+#include <lib/fsl/vmo/strings.h>
 #include <lib/fxl/files/file.h>
 
 #include "peridot/lib/firebase_auth/testing/credentials.h"
 #include "peridot/lib/firebase_auth/testing/json_schema.h"
 
 namespace {
+
+namespace http = ::fuchsia::net::oldhttp;
 
 constexpr fxl::StringView kSyncParamsSchema = R"({
   "type": "object",
@@ -32,6 +36,10 @@ constexpr fxl::StringView kGnCredentialsPathArg =
 constexpr fxl::StringView kCredentialsDefaultPath =
     "/pkg/data/sync_credentials.json";
 
+// URL that the sync infra bots use to pass the sync credentials to the tests.
+constexpr fxl::StringView kCredentialsFetchUrl =
+    "http://10.0.2.2:8081/ledger_e2e_sync_credentials";
+
 void WarnIncorrectSyncParams() {
   std::cerr << "Missing the sync parameters." << std::endl;
   std::cerr << "This binary needs an ID of a configured Firestore instance "
@@ -47,6 +55,67 @@ void WarnIncorrectSyncParams() {
             << kCredentialsPathFlag << "=<file path>" << std::endl;
   std::cerr << "if the access credentials are not embedded in the binary "
             << "at build." << std::endl;
+}
+
+// Fetches the sync credentials from the network. This method is synchronous and
+// blocks until credentials are retrieved. This is intended exclusively for
+// infra bots that will expose the credentials over the network when running
+// sync tests.
+bool FetchCredentials(component::StartupContext* startup_context,
+                      std::string* credentials_path, std::string* credentials) {
+  *credentials_path = kCredentialsFetchUrl.ToString();
+
+  fidl::SynchronousInterfacePtr<http::HttpService> network_service;
+  startup_context->ConnectToEnvironmentService(network_service.NewRequest());
+  fidl::SynchronousInterfacePtr<http::URLLoader> url_loader;
+
+  zx_status_t status =
+      network_service->CreateURLLoader(url_loader.NewRequest());
+  if (status != ZX_OK) {
+    return false;
+  }
+
+  http::URLRequest request;
+  request.method = "GET";
+  request.url = kCredentialsFetchUrl.ToString();
+  request.response_body_mode = http::ResponseBodyMode::SIZED_BUFFER;
+  http::URLResponse response;
+
+  status = url_loader->Start(std::move(request), &response);
+  if (status != ZX_OK) {
+    return false;
+  }
+
+  if (response.error || response.status_code != 200) {
+    return false;
+  }
+
+  return fsl::StringFromVmo(response.body->sized_buffer(), credentials);
+}
+
+// Extracts the credentials content. This function will return |true| if it
+// finds the credentials in either:
+// - The command line
+// - The default location in the running package
+// - Over the network.
+// In that case, |credentials| will contain the content of the credentials file
+// and |credentials_path| the path to the file.
+// If it cannot find the credentials, this function will return |false|, and
+// |credentials_path| will contain the path of the last tried location.
+bool GetCredentialsContent(const fxl::CommandLine& command_line,
+                           component::StartupContext* startup_context,
+                           std::string* credentials_path,
+                           std::string* credentials) {
+  if (command_line.GetOptionValue(kCredentialsPathFlag.ToString(),
+                                  credentials_path)) {
+    return files::ReadFileToString(*credentials_path, credentials);
+  }
+  *credentials_path = kCredentialsDefaultPath.ToString();
+  if (files::IsFile(*credentials_path)) {
+    return files::ReadFileToString(*credentials_path, credentials);
+  }
+
+  return FetchCredentials(startup_context, credentials_path, credentials);
 }
 
 }  // namespace
@@ -79,21 +148,12 @@ bool ParseSyncParamsFromCommandLine(const fxl::CommandLine& command_line,
                                     component::StartupContext* startup_context,
                                     SyncParams* sync_params) {
   std::string credentials;
-  std::string credentials_path = kCredentialsDefaultPath.ToString();
-  if (!files::ReadFileToString(credentials_path, &credentials)) {
-    std::cerr << "Cannot access the default credentials location: "
-              << kCredentialsDefaultPath << std::endl;
-
-    if (!command_line.GetOptionValue(kCredentialsPathFlag.ToString(),
-                                     &credentials_path)) {
-      WarnIncorrectSyncParams();
-      return false;
-    }
-
-    if (!files::ReadFileToString(credentials_path, &credentials)) {
-      std::cerr << "Cannot access " << credentials_path << std::endl;
-      return false;
-    }
+  std::string credentials_path;
+  if (!GetCredentialsContent(command_line, startup_context, &credentials_path,
+                             &credentials)) {
+    std::cerr << "Cannot access " << credentials_path << std::endl;
+    WarnIncorrectSyncParams();
+    return false;
   }
 
   rapidjson::Document document;
@@ -103,7 +163,7 @@ bool ParseSyncParamsFromCommandLine(const fxl::CommandLine& command_line,
               << std::endl;
     return false;
   }
-  auto sync_params_schema =  json_schema::InitSchema(kSyncParamsSchema);
+  auto sync_params_schema = json_schema::InitSchema(kSyncParamsSchema);
   if (!json_schema::ValidateSchema(document, *sync_params_schema)) {
     std::cerr << "Cannot parse credentials at " << credentials_path
               << std::endl;
