a5e3fb5c9354f7975ac74ced746470cc03f2c34c
James Tucker <raggi@google.com>
2018-09-11 10:57:11 -0700

    cat.sh: elide empty lines, for symbolizer
    The symbolizer will break if it encounters empty lines. As a result,
    cat.sh is patched to no longer produce empty lines.
    
    Change-Id: Icfec4f6b759022e88b1d9259c3c7f517f8dbf21e

--- cat.sh
+++ cat.sh
@@ -5,6 +5,7 @@
 
 # cat.sh output_file [file, ...]
 # cat.sh concatenates all file arguments into output_file, separated by lines.
+# cat.sh elides empty lines (in order to avoid breaking the symbolizer)
 
 # Note: `cat $output $@` is not equivalent - that does not produce new lines
 # between inputs.
@@ -13,5 +14,8 @@ readonly output="$1"
 shift 1
 for file in "$@"
 do
-  echo "$(<"${file}")"
+  val="$(<"${file}")"
+  if [ -n "${val}" ]; then
+    echo "${val}"
+  fi
 done > "${output}"
