db6fcbb553decf49a0e209ffea5f10196ccb73d8
Aaron Green <aarongreen@google.com>
2018-09-13 10:31:17 -0700

    [hid-parser]  Add hid-parser fuzzer
    This CL adds a simple fuzzer that executes the Parse method.
    
    SEC-44 #comment Add a fuzzer to test with
    
    Tests: Manual tested using uapp/fuzz
    
    Change-Id: I1825d1c298f5455067dfa8f5b95367708fd837fe

--- system/utest/hid-parser/rules.mk
+++ system/utest/hid-parser/rules.mk
@@ -25,3 +25,22 @@ MODULE_LIBS := \
     system/ulib/c
 
 include make/module.mk
+
+
+MODULE := $(LOCAL_DIR).fuzzer
+
+MODULE_TYPE := fuzztest
+
+MODULE_SRCS = \
+    $(LOCAL_DIR)/hid-parser-fuzztest.cpp
+
+MODULE_STATIC_LIBS := \
+    system/ulib/hid-parser \
+    system/ulib/fbl \
+
+MODULE_LIBS := \
+    system/ulib/fdio \
+    system/ulib/unittest \
+    system/ulib/c \
+
+include make/module.mk
