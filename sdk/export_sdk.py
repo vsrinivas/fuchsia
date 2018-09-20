0aeb9390aedf6bfd7b22685218a4f7a414e89ca5
P.Y. Laligand <pylaligand@google.com>
2018-09-20 11:18:58 -0700

    [sdk] Export SDKs for use in the build using the same manifest as for building archives.
    Also cleaned up how SDK artifacts are laid out in the build output directory.
    
    Test: ran a full build without any trouble.
    Bug: DX-340
    Change-Id: Ieb66f184d025320d20e7d092c1c23a87d35e701e

--- sdk/export_sdk.py
+++ sdk/export_sdk.py
@@ -34,45 +34,19 @@ def main():
     parser.add_argument('--manifest',
                         help='Path to the SDK\'s manifest file',
                         required=True)
-    parser.add_argument('--domains',
-                        help='List of domains to export',
-                        nargs='*')
-    parser.add_argument('--depname',
-                        help='Name of the depfile target',
-                        required=True)
-    parser.add_argument('--depfile',
-                        help='Path to the depfile to generate',
-                        required=True)
-    parser.add_argument('--arch',
-                        help='Target architecture',
-                        required=True)
     args = parser.parse_args()
-
-    if len(args.domains) != 1 and args.domains[0] != 'cpp':
-        print('Only the "cpp" domain is supported at the moment.')
-        return 1
 
     # Remove any existing output.
     shutil.rmtree(args.out_dir, True)
 
     with open(args.manifest, 'r') as manifest_file:
-        manifest = json.load(manifest_file)
+        mappings = map(lambda l: l.strip().split('=', 1),
+                       manifest_file.readlines())
 
-    atoms = manifest['atoms']
-    if len(atoms) != 1 or atoms[0]['id']['name'] != 'system':
-        print(atoms)
-        print('Only the sysroot atom is supported.')
-        return 1
-    atom = atoms[0]
-
-    base = os.path.join(args.out_dir, 'arch', args.arch, 'sysroot')
-    for file in atom['files']:
-        destination = os.path.join(base, file['destination'])
+    for dest, source in mappings:
+        destination = os.path.join(args.out_dir, dest)
         make_dir(destination)
-        shutil.copyfile(file['source'], destination)
-
-    with open(args.depfile, 'w') as dep_file:
-        dep_file.write('%s:\n' % args.depname)
+        shutil.copy2(source, destination)
 
     with open(args.stamp_file, 'w') as stamp_file:
         stamp_file.write('Now go use it\n')
