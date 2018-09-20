4f92d2058b5c4e7ab69fa24fd83aa6ff0017a491
Arman Uguray <armansito@google.com>
2018-09-20 12:34:13 -0700

    [bt-pairing-tool] Refactor the user interface
    - Factored out individual pairing method prompts into helper functions.
    - Cleaned up printed messages to be more consistent and friendlier.
      Pairing request messages now display the device name and address.
    - Fixed handling of single-char input (y/n). The input character now
      gets displayed and works over fx shell.
    - Extraneous characters are no longer buffered during a single-char
      prompt, fixing the issue described in NET-1514.
    
    NET-1517 #done
    NET-1514 #done
    TEST=1. Test PasskeyDisplay and Consent methods by pairing a mouse and
            a keyboard.
         2. Test that typing characters other than y/n in a confirmation
            prompt has no effect.
         3. Confirm that the device name and address involved in the request
            are shown.
         4. Confirm that pairing can be accepted/rejected using fx shell.
    
    Change-Id: Ie24414305e8e87bc94a8dc212d379e9398acb586

--- bin/bluetooth/tools/bt-pairing-tool/src/pairing.rs
+++ bin/bluetooth/tools/bt-pairing-tool/src/pairing.rs
@@ -2,80 +2,175 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use fuchsia_async as fasync;
-use fidl_fuchsia_bluetooth_control::{PairingMethod, PairingDelegateRequest, PairingDelegateRequestStream};
-use fidl::endpoints::RequestStream;
-use futures::prelude::*;
 use failure::Error;
-use futures::{Future};
-use std::io::BufRead;
-use std::io;
+use fidl::endpoints::RequestStream;
+use fidl_fuchsia_bluetooth_control::{PairingDelegateRequest, PairingDelegateRequestStream,
+                                     PairingMethod};
+use fuchsia_async as fasync;
+use fuchsia_bluetooth::error::Error as BtError;
+use futures::prelude::*;
+use futures::Future;
+use std::io::{self, Read, Write};
+
+fn print_and_flush(msg: &str) {
+    print!("{}", msg);
+    io::stdout().flush().unwrap();
+}
+
+macro_rules! print_and_flush {
+    ($fmt:expr) => {
+        print_and_flush($fmt);
+    };
+    ($fmt:expr, $($arg:tt),*) => {
+        print_and_flush(&format!($fmt, $($arg),*));
+    };
+}
+
+fn handle_confirm(val: char) -> bool {
+    if val == 'y' {
+        println!("Accepted pairing");
+        true
+    } else {
+        println!("Rejected pairing");
+        false
+    }
+}
+
+// Prompt the user for a single character input with the given |msg|. Blocks
+// until the user enters one of the characters in |allowed|.
+fn prompt_for_char(msg: &str, allowed: &[char]) -> Result<char, Error> {
+    print_and_flush!("{}: ", msg);
+    while let Some(input) = io::stdin().bytes().next() {
+        match input {
+            Ok(input) => {
+                let input = input as char;
+                if allowed.contains(&input) {
+                    println!("{}", input);
+                    return Ok(input);
+                }
+            }
+            Err(e) => {
+                println!("Failed to read input: {:?}", e);
+                return Err(e.into());
+            }
+        };
+    }
+    unreachable!();
+}
+
+fn prompt_for_consent() -> bool {
+    let val = prompt_for_char("Accept? (y/n)", &['y', 'n']).unwrap_or_else(|err| {
+        eprintln!("Failed to read input: {:#?}", err);
+        'n'
+    });
+    handle_confirm(val)
+}
+
+fn prompt_for_comparison(passkey: Option<String>) -> bool {
+    match passkey {
+        None => {
+            println!("No passkey provided for 'Passkey Comparison'!");
+            false
+        }
+        Some(passkey) => {
+            let msg = format!("Is the passkey '{}' displayed on device? (y/n)", passkey);
+            let val = prompt_for_char(&msg, &['y', 'n']).unwrap_or_else(|err| {
+                eprintln!("Failed to read input: {:#?}", err);
+                'n'
+            });
+            handle_confirm(val)
+        }
+    }
+}
+
+fn prompt_for_remote_input(passkey: Option<String>) -> bool {
+    match passkey {
+        None => {
+            println!("No passkey provided for 'Passkey Display'!");
+            false
+        }
+        Some(passkey) => {
+            println!("Enter the passkey '{}' on the peer.", passkey);
+            true
+        }
+    }
+}
+
+fn prompt_for_local_input() -> Option<String> {
+    print_and_flush!("Enter the passkey displayed on the peer (or nothing to reject): ");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            let input = input.trim().to_string();
+            if input.len() == 0 {
+                None
+            } else {
+                Some(input)
+            }
+        }
+        Err(e) => {
+            println!("Failed to receive passkey: {}", e);
+            None
+        }
+    }
+}
 
 pub fn pairing_delegate(channel: fasync::Channel) -> impl Future<Output = Result<(), Error>> {
     let stream = PairingDelegateRequestStream::from_channel(channel);
-    stream.try_for_each(move |evt| {
-        match evt {
-            PairingDelegateRequest::OnPairingComplete { device_id, status, control_handle: _ } => {
-                eprintln!("Pairing complete for {}: {:?}", device_id, status);
-            },
-            PairingDelegateRequest::OnPairingRequest { device, method, displayed_passkey, responder } => {
-                let mut resp_str = None;
-
-                eprintln!("Pairing Request received from remote device: {}", device.identifier);
-
-                let mut input = String::new();
-                let stdin = io::stdin();
-                let mut handle = stdin.lock();
-
-                eprintln!("Accept? (y,N)");
-                let mut resp_bool = match handle.read_line(&mut input) {
-                    Ok(_) => {
-                        let input = input.trim();
-                        if input == "Y" || input == "y" {
-                            eprintln!("Initializing pairing");
-                            true
-                        } else {
-                            eprintln!("Pairing Rejected");
-                            false
+    stream
+        .try_for_each(move |evt| {
+            match evt {
+                PairingDelegateRequest::OnPairingComplete {
+                    device_id,
+                    status,
+                    control_handle: _,
+                } => {
+                    println!(
+                        "Pairing complete for peer (id: {}, status: {})",
+                        device_id,
+                        match status.error {
+                            None => "success".to_string(),
+                            Some(error) => format!("{}", BtError::from(*error)),
                         }
-                    }
-                    Err(e) => {
-                        eprintln!("Declining pairing: {}", e);
-                        false
-                    }
-                };
-                match method {
-                    PairingMethod::PasskeyComparison=> {
-                        if let Some(remote_key) = displayed_passkey {
-                            eprintln!("Remote Key: {}", remote_key);
+                    );
+                }
+                PairingDelegateRequest::OnPairingRequest {
+                    device,
+                    method,
+                    displayed_passkey,
+                    responder,
+                } => {
+                    println!(
+                        "Pairing request from peer: {}",
+                        match &device.name {
+                            Some(name) => format!("{} ({})", name, &device.address),
+                            None => device.address,
                         }
-                    },
-                    PairingMethod::PasskeyDisplay => {
-                        if let Some(remote_key) = displayed_passkey {
-                            eprintln!("Please input code {} on peer", remote_key);
+                    );
+
+                    let (confirm, entered_passkey) = match method {
+                        PairingMethod::Consent => (prompt_for_consent(), None),
+                        PairingMethod::PasskeyComparison => {
+                            (prompt_for_comparison(displayed_passkey), None)
                         }
-                    },
-                    PairingMethod::Consent => { /* Handled in the general case */ },
-                    PairingMethod::PasskeyEntry => {
-                        eprintln!("Input Remote passkey:");
-                        match handle.read_line(&mut input) {
-                            Ok(_) => {
-                                let s = input.trim().to_string();
-                                resp_str = Some(s);
-                            }
-                            Err(_) => {
-                                resp_bool = false;
-                            }
-                        };
-                    },
-                };
-                let _ = responder.send(resp_bool, resp_str.as_ref().map(String::as_str));
-            },
-            PairingDelegateRequest::OnRemoteKeypress { device_id, keypress, control_handle: _ } => {
-                eprintln!("Device: {} | {:?}", device_id, keypress);
-            }
-        };
-        future::ready(Ok(()))
-    })
-    .map_err(|e| e.into())
+                        PairingMethod::PasskeyDisplay => {
+                            (prompt_for_remote_input(displayed_passkey), None)
+                        }
+                        PairingMethod::PasskeyEntry => match prompt_for_local_input() {
+                            None => (false, None),
+                            passkey => (true, passkey),
+                        },
+                    };
+                    let _ = responder.send(confirm, entered_passkey.as_ref().map(String::as_ref));
+                }
+                PairingDelegateRequest::OnRemoteKeypress {
+                    device_id,
+                    keypress,
+                    control_handle: _,
+                } => {
+                    eprintln!("Device: {} | {:?}", device_id, keypress);
+                }
+            };
+            future::ready(Ok(()))
+        }).map_err(|e| e.into())
 }
