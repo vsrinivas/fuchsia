74a0e59d1a28ea667ec65c4d8f9763b46c185275
Benjamin Lerman <qsr@chromium.org>
2018-09-21 17:36:49 +0200

    [random] Introduce an injectable random class
    This will allow to control the randomness when running tests.
    
    LE-611
    
    TEST=SystemRandomTest, TestRandomTest
    
    Change-Id: I8b495a98df5679fe61a44528cafb52110bdcd9c4

---
+++ lib/rng/system_random.h
@@ -0,0 +1,20 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PERIDOT_LIB_RNG_SYSTEM_RANDOM_H_
+#define PERIDOT_LIB_RNG_SYSTEM_RANDOM_H_
+
+#include "peridot/lib/rng/random.h"
+
+namespace rng {
+
+// Implementation of |Random| that uses the system RNG.
+class SystemRandom final : public Random {
+ private:
+  void InternalDraw(void* buffer, size_t buffer_size) override;
+};
+
+}  // namespace rng
+
+#endif  // PERIDOT_LIB_RNG_SYSTEM_RANDOM_H_
