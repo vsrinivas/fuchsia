8d608a11bbce2eb2339ccf6827ba285eb7c46dc6
Mark Seaborn <mseaborn@google.com>
2018-09-21 15:06:51 -0700

    [benchmarks] Move the example's test suite name to the .tspec file
    Stop using the "--test-suite" argument in this case.
    
    Bug: IN-422
    Test: check output from perf trybot
    Change-Id: Ifa52cfac036f9305a63ac04b1e170399e44b0dbb

--- tests/benchmarks/benchmarks.sh
+++ tests/benchmarks/benchmarks.sh
@@ -17,7 +17,6 @@ runbench_read_arguments "$@"
 runbench_exec "${OUT_DIR}/benchmark_example.json" \
     trace record \
     --spec-file=/pkgfs/packages/benchmark/0/data/benchmark_example.tspec \
-    --test-suite=fuchsia.benchmark_example \
     --benchmark-results-file="${OUT_DIR}/benchmark_example.json"
 
 # Performance tests implemented in the Zircon repo.
