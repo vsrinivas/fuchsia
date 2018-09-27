0d17a5e76e1f1b14fd6bbc6b6de16c184a7ab811
Bert Muthalaly <bert@somethingdoneright.net>
2018-09-27 12:21:52 +0000

    [netstack] add netstack_bridge_test
    Ported from:
    
    https://fuchsia-review.googlesource.com/c/third_party/netstack/+/129955
    https://fuchsia-review.googlesource.com/c/third_party/netstack/+/137295
    
    TESTED=fx run-test netstack_bridge_test
    
    PAIR=tamird
    
    Change-Id: I621d17185078bc6c8f30a3d5e9663a6bb10729c8

---
+++ go/src/netstack/link/bridge/bridge_test.go
@@ -0,0 +1,139 @@
+package bridge_test
+
+import (
+	"bytes"
+	"testing"
+
+	"netstack/link/bridge"
+
+	"github.com/google/netstack/tcpip"
+	"github.com/google/netstack/tcpip/buffer"
+	"github.com/google/netstack/tcpip/header"
+	"github.com/google/netstack/tcpip/link/channel"
+	"github.com/google/netstack/tcpip/network/ipv4"
+	"github.com/google/netstack/tcpip/stack"
+	"github.com/google/netstack/tcpip/transport/tcp"
+	"github.com/google/netstack/waiter"
+)
+
+func makeStack() *stack.Stack {
+	return stack.New([]string{ipv4.ProtocolName}, []string{tcp.ProtocolName}, stack.Options{})
+}
+
+func makeStackWithChannel(linkaddr tcpip.LinkAddress, addr tcpip.Address) (*stack.Stack, *channel.Endpoint, *tcpip.Error) {
+	s := makeStack()
+	id, ep := channel.New(1, 100, linkaddr)
+	if err := s.CreateNIC(1, id); err != nil {
+		return nil, nil, err
+	}
+	if err := s.AddAddress(1, header.IPv4ProtocolNumber, addr); err != nil {
+		return nil, nil, err
+	}
+	return s, ep, nil
+}
+
+func link(a, b *channel.Endpoint) {
+	for p := range a.C {
+		var vv buffer.VectorisedView
+		if p.Header != nil {
+			vv = buffer.NewVectorisedView(len(p.Header)+len(p.Payload), []buffer.View{p.Header, p.Payload})
+		} else {
+			vv = buffer.NewVectorisedView(len(p.Payload), []buffer.View{p.Payload})
+		}
+		b.Inject(p.Proto, vv)
+	}
+}
+
+func TestBridge(t *testing.T) {
+	s1addr := tcpip.Address([]byte{192, 168, 42, 10})
+	s1, s1ep, err := makeStackWithChannel(tcpip.LinkAddress(bytes.Repeat([]byte{1}, 6)), s1addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s2addr := tcpip.Address([]byte{10, 0, 0, 1})
+	s2, s2ep, err := makeStackWithChannel(tcpip.LinkAddress(bytes.Repeat([]byte{2}, 6)), s2addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s1.SetRouteTable([]tcpip.Route{
+		{
+			Destination: s2addr,
+			Mask:        "\xff\xff\xff\xff",
+			NIC:         1,
+		},
+	})
+
+	{
+		_, ep1 := channel.New(1, 100, tcpip.LinkAddress(bytes.Repeat([]byte{3}, 6)))
+		_, ep2 := channel.New(1, 100, tcpip.LinkAddress(bytes.Repeat([]byte{4}, 6)))
+		if err := makeStack().CreateNIC(1, stack.RegisterLinkEndpoint(bridge.New([]stack.LinkEndpoint{ep1, ep2}))); err != nil {
+			t.Fatal(err)
+		}
+		go link(s1ep, ep1)
+		go link(ep1, s1ep)
+		go link(ep2, s2ep)
+		go link(s2ep, ep2)
+	}
+
+	wq := new(waiter.Queue)
+	s1txep, err := s1.NewEndpoint(tcp.ProtocolNumber, ipv4.ProtocolNumber, wq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s1txep.Close()
+	s2txep, err := s2.NewEndpoint(tcp.ProtocolNumber, ipv4.ProtocolNumber, wq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s2txep.Close()
+
+	s2fulladdr := tcpip.FullAddress{Addr: s2addr, Port: 8080}
+	if err := s2txep.Bind(s2fulladdr, nil); err != nil {
+		t.Fatal(err)
+	}
+	if err := s2txep.Listen(1); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := s1txep.Connect(s2fulladdr); err != tcpip.ErrConnectStarted {
+		t.Logf("s1.Stats() = %+v", s1.Stats())
+		t.Fatal(err)
+	}
+
+	// Wait for the inbound TCP connection.
+	{
+		waitEntry, notifyCh := waiter.NewChannelEntry(nil)
+		wq.EventRegister(&waitEntry, waiter.EventIn)
+		<-notifyCh
+		wq.EventUnregister(&waitEntry)
+	}
+	ep, wq, err := s2txep.Accept()
+	if err != nil {
+		t.Logf("s2.Stats() = %+v", s1.Stats())
+		t.Fatal(err)
+	}
+
+	const payload = "hello"
+	if _, err := s1txep.Write(tcpip.SlicePayload(payload), tcpip.WriteOptions{To: &s2fulladdr}); err != nil {
+		t.Logf("s1.Stats() = %+v", s1.Stats())
+		t.Fatal(err)
+	}
+
+	// Wait for the inbound packet.
+	{
+		waitEntry, notifyCh := waiter.NewChannelEntry(nil)
+		wq.EventRegister(&waitEntry, waiter.EventIn)
+		<-notifyCh
+		wq.EventUnregister(&waitEntry)
+	}
+	recvd, _, err := ep.Read(nil)
+	if err != nil {
+		t.Logf("s2.Stats() = %+v", s1.Stats())
+		t.Fatal(err)
+	}
+	if got := string(recvd); got != payload {
+		t.Errorf("got Read(...) = %v, want = %v", got, payload)
+	}
+}
