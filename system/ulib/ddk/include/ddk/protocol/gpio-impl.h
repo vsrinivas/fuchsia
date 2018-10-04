6b9bc1ead3980067ef1028edc35e0bfab8516d70
Suraj Malhotra <surajmalhotra@google.com>
2018-10-04 10:52:08 -0700

    [ddk][ddktl] Migrate gpio-impl protocol to autogenerated version.
    Tested: Build succeeds.
    Change-Id: I1926724148b62d1d6f7d9f56370fe722fce5df41

--- system/ulib/ddk/include/ddk/protocol/gpio-impl.h
+++ system/ulib/ddk/include/ddk/protocol/gpio-impl.h
@@ -1,6 +1,9 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+// WARNING: THIS FILE IS MACHINE GENERATED. DO NOT EDIT.
+//          MODIFY system/fidl/protocols/gpio_impl.fidl INSTEAD.
 
 #pragma once
 
@@ -10,67 +13,68 @@
 
 __BEGIN_CDECLS;
 
-typedef struct {
-    zx_status_t (*config_in)(void* ctx, uint32_t pin, uint32_t flags);
-    zx_status_t (*config_out)(void* ctx, uint32_t pin, uint8_t initial_value);
-    zx_status_t (*set_alt_function)(void* ctx, uint32_t pin, uint64_t function);
-    zx_status_t (*read)(void* ctx, uint32_t pin, uint8_t* out_value);
-    zx_status_t (*write)(void* ctx, uint32_t pin, uint8_t value);
-    zx_status_t (*get_interrupt)(void* ctx, uint32_t pin, uint32_t flags, zx_handle_t* out_handle);
-    zx_status_t (*release_interrupt)(void* ctx, uint32_t pin);
-    zx_status_t (*set_polarity)(void* ctx, uint32_t pin, uint32_t polarity);
+// Forward declarations
+
+typedef struct gpio_impl_protocol gpio_impl_protocol_t;
+
+// Declarations
+
+typedef struct gpio_impl_protocol_ops {
+    zx_status_t (*config_in)(void* ctx, uint32_t index, uint32_t flags);
+    zx_status_t (*config_out)(void* ctx, uint32_t index, uint8_t initial_value);
+    zx_status_t (*set_alt_function)(void* ctx, uint32_t index, uint64_t function);
+    zx_status_t (*read)(void* ctx, uint32_t index, uint8_t* out_value);
+    zx_status_t (*write)(void* ctx, uint32_t index, uint8_t value);
+    zx_status_t (*get_interrupt)(void* ctx, uint32_t index, uint32_t flags, zx_handle_t* out_irq);
+    zx_status_t (*release_interrupt)(void* ctx, uint32_t index);
+    zx_status_t (*set_polarity)(void* ctx, uint32_t index, gpio_polarity_t polarity);
 } gpio_impl_protocol_ops_t;
 
-typedef struct {
+struct gpio_impl_protocol {
     gpio_impl_protocol_ops_t* ops;
     void* ctx;
-} gpio_impl_protocol_t;
+};
 
-// configures a GPIO for input
-static inline zx_status_t gpio_impl_config_in(const gpio_impl_protocol_t* gpio, uint32_t pin,
+// Configures a GPIO for input.
+static inline zx_status_t gpio_impl_config_in(const gpio_impl_protocol_t* proto, uint32_t index,
                                               uint32_t flags) {
-    return gpio->ops->config_in(gpio->ctx, pin, flags);
+    return proto->ops->config_in(proto->ctx, index, flags);
 }
-
-// configures a GPIO for output
-static inline zx_status_t gpio_impl_config_out(const gpio_impl_protocol_t* gpio, uint32_t pin,
+// Configures a GPIO for output.
+static inline zx_status_t gpio_impl_config_out(const gpio_impl_protocol_t* proto, uint32_t index,
                                                uint8_t initial_value) {
-    return gpio->ops->config_out(gpio->ctx, pin, initial_value);
+    return proto->ops->config_out(proto->ctx, index, initial_value);
 }
-
-// configures the GPIO pin for an alternate function (I2C, SPI, etc)
-// the interpretation of "function" is platform dependent
-static inline zx_status_t gpio_impl_set_alt_function(const gpio_impl_protocol_t* gpio, uint32_t pin,
-                                                     uint64_t function) {
-    return gpio->ops->set_alt_function(gpio->ctx, pin, function);
+// Configures the GPIO pin for an alternate function (I2C, SPI, etc)
+// the interpretation of "function" is platform dependent.
+static inline zx_status_t gpio_impl_set_alt_function(const gpio_impl_protocol_t* proto,
+                                                     uint32_t index, uint64_t function) {
+    return proto->ops->set_alt_function(proto->ctx, index, function);
 }
-
-// reads the current value of a GPIO (0 or 1)
-static inline zx_status_t gpio_impl_read(const gpio_impl_protocol_t* gpio, uint32_t pin,
+// Reads the current value of a GPIO (0 or 1).
+static inline zx_status_t gpio_impl_read(const gpio_impl_protocol_t* proto, uint32_t index,
                                          uint8_t* out_value) {
-    return gpio->ops->read(gpio->ctx, pin, out_value);
+    return proto->ops->read(proto->ctx, index, out_value);
 }
-
-// sets the current value of the GPIO (any non-zero value maps to 1)
-static inline zx_status_t gpio_impl_write(const gpio_impl_protocol_t* gpio, uint32_t pin,
+// Sets the current value of the GPIO (any non-zero value maps to 1).
+static inline zx_status_t gpio_impl_write(const gpio_impl_protocol_t* proto, uint32_t index,
                                           uint8_t value) {
-    return gpio->ops->write(gpio->ctx, pin, value);
+    return proto->ops->write(proto->ctx, index, value);
+}
+// Gets an interrupt object pertaining to a particular GPIO pin.
+static inline zx_status_t gpio_impl_get_interrupt(const gpio_impl_protocol_t* proto, uint32_t index,
+                                                  uint32_t flags, zx_handle_t* out_irq) {
+    return proto->ops->get_interrupt(proto->ctx, index, flags, out_irq);
+}
+// Release the interrupt.
+static inline zx_status_t gpio_impl_release_interrupt(const gpio_impl_protocol_t* proto,
+                                                      uint32_t index) {
+    return proto->ops->release_interrupt(proto->ctx, index);
+}
+// Set GPIO polarity.
+static inline zx_status_t gpio_impl_set_polarity(const gpio_impl_protocol_t* proto, uint32_t index,
+                                                 gpio_polarity_t polarity) {
+    return proto->ops->set_polarity(proto->ctx, index, polarity);
 }
 
-// gets an interrupt object pertaining to a particular GPIO pin
-static inline zx_status_t gpio_impl_get_interrupt(const gpio_impl_protocol_t* gpio, uint32_t pin,
-                                                  uint32_t flags, zx_handle_t* out_handle) {
-    return gpio->ops->get_interrupt(gpio->ctx, pin, flags, out_handle);
-}
-
-// release the interrupt
-static inline zx_status_t gpio_impl_release_interrupt(const gpio_impl_protocol_t* gpio, uint32_t pin) {
-    return gpio->ops->release_interrupt(gpio->ctx, pin);
-}
-
-// Set GPIO polarity
-static inline zx_status_t gpio_impl_set_polarity(const gpio_impl_protocol_t* gpio, uint32_t pin,
-                                                 uint32_t polarity) {
-    return gpio->ops->set_polarity(gpio->ctx, pin, polarity);
-}
 __END_CDECLS;
--- system/ulib/ddktl/include/ddktl/protocol/gpio-impl.h
+++ system/ulib/ddktl/include/ddktl/protocol/gpio-impl.h
@@ -2,47 +2,60 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// WARNING: THIS FILE IS MACHINE GENERATED. DO NOT EDIT.
+//          MODIFY system/fidl/protocols/gpio_impl.fidl INSTEAD.
+
 #pragma once
 
-#include <ddk/driver.h>
 #include <ddk/protocol/gpio-impl.h>
+#include <ddk/protocol/gpio.h>
 #include <ddktl/device-internal.h>
 #include <zircon/assert.h>
+#include <zircon/compiler.h>
+#include <zircon/types.h>
 
 #include "gpio-impl-internal.h"
 
-// DDK GPIO implementation protocol support.
+// DDK gpio-impl-protocol support
 //
 // :: Proxies ::
 //
-// ddk::GpioImplProtocolProxy is a simple wrappers around gpio_impl_protocol_t. It does
-// not own the pointers passed to it.
+// ddk::GpioImplProtocolProxy is a simple wrapper around
+// gpio_impl_protocol_t. It does not own the pointers passed to it
 //
 // :: Mixins ::
 //
-// ddk::GpioImplProtocol is a mixin class that simplifies writing DDK drivers that
-// implement the GPIO implementation protocol.
+// ddk::GpioImplProtocol is a mixin class that simplifies writing DDK drivers
+// that implement the gpio-impl protocol. It doesn't set the base protocol.
 //
 // :: Examples ::
 //
 // // A driver that implements a ZX_PROTOCOL_GPIO_IMPL device.
-// class GpioImplDevice;
+// class GpioImplDevice {
 // using GpioImplDeviceType = ddk::Device<GpioImplDevice, /* ddk mixins */>;
 //
 // class GpioImplDevice : public GpioImplDeviceType,
 //                        public ddk::GpioImplProtocol<GpioImplDevice> {
 //   public:
 //     GpioImplDevice(zx_device_t* parent)
-//       : GpioImplDeviceType("my-GpioImpl-device", parent) {}
+//         : GpioImplDeviceType("my-gpio-impl-protocol-device", parent) {}
 //
 //     zx_status_t GpioImplConfigIn(uint32_t index, uint32_t flags);
+//
 //     zx_status_t GpioImplConfigOut(uint32_t index, uint8_t initial_value);
+//
 //     zx_status_t GpioImplSetAltFunction(uint32_t index, uint64_t function);
+//
 //     zx_status_t GpioImplRead(uint32_t index, uint8_t* out_value);
+//
 //     zx_status_t GpioImplWrite(uint32_t index, uint8_t value);
-//     zx_status_t GpioImplGetInterrupt(uint32_t index, uint32_t flags, zx_handle_t *out_handle);
+//
+//     zx_status_t GpioImplGetInterrupt(uint32_t index, uint32_t flags, zx_handle_t* out_irq);
+//
 //     zx_status_t GpioImplReleaseInterrupt(uint32_t index);
-//     zx_status_t GpioImplSetPolarity(uint32_t index, uint32_t polarity);
+//
+//     zx_status_t GpioImplSetPolarity(uint32_t index, gpio_polarity_t polarity);
+//
 //     ...
 // };
 
@@ -62,7 +75,7 @@ public:
         ops_.release_interrupt = GpioImplReleaseInterrupt;
         ops_.set_polarity = GpioImplSetPolarity;
 
-        // Can only inherit from one base_protocol implemenation
+        // Can only inherit from one base_protocol implementation.
         ZX_ASSERT(ddk_proto_id_ == 0);
         ddk_proto_id_ = ZX_PROTOCOL_GPIO_IMPL;
         ddk_proto_ops_ = &ops_;
@@ -72,65 +85,83 @@ protected:
     gpio_impl_protocol_ops_t ops_ = {};
 
 private:
+    // Configures a GPIO for input.
     static zx_status_t GpioImplConfigIn(void* ctx, uint32_t index, uint32_t flags) {
         return static_cast<D*>(ctx)->GpioImplConfigIn(index, flags);
     }
+    // Configures a GPIO for output.
     static zx_status_t GpioImplConfigOut(void* ctx, uint32_t index, uint8_t initial_value) {
         return static_cast<D*>(ctx)->GpioImplConfigOut(index, initial_value);
     }
+    // Configures the GPIO pin for an alternate function (I2C, SPI, etc)
+    // the interpretation of "function" is platform dependent.
     static zx_status_t GpioImplSetAltFunction(void* ctx, uint32_t index, uint64_t function) {
         return static_cast<D*>(ctx)->GpioImplSetAltFunction(index, function);
     }
+    // Reads the current value of a GPIO (0 or 1).
     static zx_status_t GpioImplRead(void* ctx, uint32_t index, uint8_t* out_value) {
         return static_cast<D*>(ctx)->GpioImplRead(index, out_value);
     }
+    // Sets the current value of the GPIO (any non-zero value maps to 1).
     static zx_status_t GpioImplWrite(void* ctx, uint32_t index, uint8_t value) {
         return static_cast<D*>(ctx)->GpioImplWrite(index, value);
     }
+    // Gets an interrupt object pertaining to a particular GPIO pin.
     static zx_status_t GpioImplGetInterrupt(void* ctx, uint32_t index, uint32_t flags,
-                                        zx_handle_t* out_handle) {
-        return static_cast<D*>(ctx)->GpioImplGetInterrupt(index, flags, out_handle);
+                                            zx_handle_t* out_irq) {
+        return static_cast<D*>(ctx)->GpioImplGetInterrupt(index, flags, out_irq);
     }
+    // Release the interrupt.
     static zx_status_t GpioImplReleaseInterrupt(void* ctx, uint32_t index) {
         return static_cast<D*>(ctx)->GpioImplReleaseInterrupt(index);
     }
-    static zx_status_t GpioImplSetPolarity(void* ctx, uint32_t index, uint32_t polarity) {
+    // Set GPIO polarity.
+    static zx_status_t GpioImplSetPolarity(void* ctx, uint32_t index, gpio_polarity_t polarity) {
         return static_cast<D*>(ctx)->GpioImplSetPolarity(index, polarity);
     }
 };
 
 class GpioImplProtocolProxy {
 public:
-    GpioImplProtocolProxy(gpio_impl_protocol_t* proto)
-        : ops_(proto->ops), ctx_(proto->ctx) {}
+    GpioImplProtocolProxy() : ops_(nullptr), ctx_(nullptr) {}
+    GpioImplProtocolProxy(const gpio_impl_protocol_t* proto) : ops_(proto->ops), ctx_(proto->ctx) {}
 
     void GetProto(gpio_impl_protocol_t* proto) {
         proto->ctx = ctx_;
         proto->ops = ops_;
     }
-
+    bool is_valid() { return ops_ != nullptr; }
+    void clear() {
+        ctx_ = nullptr;
+        ops_ = nullptr;
+    }
+    // Configures a GPIO for input.
     zx_status_t ConfigIn(uint32_t index, uint32_t flags) {
         return ops_->config_in(ctx_, index, flags);
     }
+    // Configures a GPIO for output.
     zx_status_t ConfigOut(uint32_t index, uint8_t initial_value) {
         return ops_->config_out(ctx_, index, initial_value);
     }
+    // Configures the GPIO pin for an alternate function (I2C, SPI, etc)
+    // the interpretation of "function" is platform dependent.
     zx_status_t SetAltFunction(uint32_t index, uint64_t function) {
         return ops_->set_alt_function(ctx_, index, function);
     }
+    // Reads the current value of a GPIO (0 or 1).
     zx_status_t Read(uint32_t index, uint8_t* out_value) {
         return ops_->read(ctx_, index, out_value);
     }
-    zx_status_t Write(uint32_t index, uint8_t value) {
-        return ops_->write(ctx_, index, value);
+    // Sets the current value of the GPIO (any non-zero value maps to 1).
+    zx_status_t Write(uint32_t index, uint8_t value) { return ops_->write(ctx_, index, value); }
+    // Gets an interrupt object pertaining to a particular GPIO pin.
+    zx_status_t GetInterrupt(uint32_t index, uint32_t flags, zx_handle_t* out_irq) {
+        return ops_->get_interrupt(ctx_, index, flags, out_irq);
     }
-    zx_status_t GetInterrupt(uint32_t index, uint32_t flags, zx_handle_t* out_handle) {
-        return ops_->get_interrupt(ctx_, index, flags, out_handle);
-    }
-    zx_status_t ReleaseInterrupt(uint32_t index) {
-        return ops_->release_interrupt(ctx_, index);
-    }
-    zx_status_t SetPolarity(uint32_t index, uint32_t polarity) {
+    // Release the interrupt.
+    zx_status_t ReleaseInterrupt(uint32_t index) { return ops_->release_interrupt(ctx_, index); }
+    // Set GPIO polarity.
+    zx_status_t SetPolarity(uint32_t index, gpio_polarity_t polarity) {
         return ops_->set_polarity(ctx_, index, polarity);
     }
 
