9c52b38f2581f456bb3a36cd42d10a5009bcce40
Todd Eisenberger <teisenbe@google.com>
2018-10-05 11:15:25 -0700

    [hid] Add boot protocol report structs
    These are being moved from the input IOCTL header.
    
    Test: Compiled
    Change-Id: I687ece695b170fa8782a5237abd093e6926893ef

---
+++ system/ulib/hid/include/hid/boot.h
@@ -0,0 +1,24 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <stdint.h>
+#include <zircon/compiler.h>
+
+__BEGIN_CDECLS
+
+typedef struct hid_boot_kbd_report {
+    uint8_t modifier;
+    uint8_t reserved;
+    uint8_t usage[6];
+} __attribute__((packed)) hid_boot_kbd_report_t;
+
+typedef struct hid_boot_mouse_report {
+    uint8_t buttons;
+    int8_t rel_x;
+    int8_t rel_y;
+} __attribute__((packed)) hid_boot_mouse_report_t;
+
+__END_CDECLS
