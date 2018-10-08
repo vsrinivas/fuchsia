1f6e95fcf7383d50fe47002f3b33e5e866980112
Benjamin Lerman <qsr@chromium.org>
2018-10-08 13:54:13 +0200

    [callback] Simplify callback::Capture implementation
    Use a lambda instead of a recursive templated internal class.
    
    TEST=CaptureTest still passes
    
    Change-Id: Ic4a9ecf945af38ba121b988ba42845750d4f1e98

--- public/lib/callback/capture.h
+++ public/lib/callback/capture.h
@@ -9,47 +9,15 @@
 #include <utility>
 
 namespace callback {
-namespace internal {
-
-template <typename C, typename... T>
-class CaptureLambda {};
-
-template <typename C>
-class CaptureLambda<C> {
- public:
-  explicit CaptureLambda(C callback) : callback_(std::move(callback)) {}
-
-  void operator()() { callback_(); }
-
- private:
-  C callback_;
-};
-
-template <typename C, typename T1, typename... T>
-class CaptureLambda<C, T1, T...> {
- public:
-  CaptureLambda(C callback, T1* t1, T*... args)
-      : t1_(t1), sub_lambda_(std::move(callback), args...) {}
-
-  template <typename V1, typename... V>
-  void operator()(V1 v1, V&&... args) {
-    if (t1_)
-      *t1_ = std::move(v1);
-    sub_lambda_(std::forward<V>(args)...);
-  }
-
- private:
-  T1* const t1_;
-  CaptureLambda<C, T...> sub_lambda_;
-};
-
-}  // namespace internal
 
 // Takes a callback, and a list of pointers. Returns a lambda that takes a list
 // of objects, saves these in the pointed variables and runs the callback.
 template <typename C, typename... T>
 auto Capture(C callback, T*... ptrs) {
-  return internal::CaptureLambda<C, T...>(std::move(callback), ptrs...);
+  return [callback = std::move(callback), ptrs...](auto&&... values) mutable {
+    std::tie(*ptrs...) = std::make_tuple(std::forward<decltype(values)>(values)...);
+    callback();
+  };
 }
 
 }  // namespace callback
