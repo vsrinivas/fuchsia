6175d32d4ae5cd6c234ebeb941d3db8bc82460f2
Tim Detwiler <tjdetwiler@google.com>
2018-10-08 14:51:48 -0700

    [wayland][bridge] Simplify wl_display/wl_registry.
    Now that we provide the registry via the Client, we can simplify some
    of the plumbing here and also remove the EventSender type in lieu of
    just using the Client::post method.
    
    Test: weston-info still sees wl_shm formats.
    Change-Id: I74b246be1ad98f92939617309cc0955942cbb815

--- bin/wayland/bridge/src/display.rs
+++ bin/wayland/bridge/src/display.rs
@@ -35,15 +35,9 @@ impl Display {
 
         // Add the global wl_display object. We unwrap here since the object map
         // is empty so failure should not be possible.
-        let display_recevier = DisplayReceiver {
-            registry: client.registry(),
-            event_sender: EventSender {
-                chan: client.chan(),
-            },
-        };
         client
             .objects()
-            .add_object(WlDisplay, DISPLAY_SINGLETON_OBJECT_ID, display_recevier)
+            .add_object(WlDisplay, DISPLAY_SINGLETON_OBJECT_ID, DisplayReceiver)
             .unwrap();
 
         // Start polling the channel for messages.
@@ -51,54 +45,22 @@ impl Display {
     }
 }
 
-/// A simple type for writing interface events to a channel.
-///
-/// TODO(tjdetwiler): We'll want to make this a bit more sophisticated,
-/// including not requiring the caller to pass in the object id and we'll most
-/// likely want some form of batching.
-#[derive(Clone)]
-pub struct EventSender {
-    chan: Arc<fasync::Channel>,
-}
-
-impl EventSender {
-    /// Serialize |event| into a buffer and write it to the zircon channel.
-    pub fn send<E: wl::IntoMessage>(&self, sender: u32, event: E) -> Result<(), Error> {
-        let mut message = event.into_message(sender)?;
-        let (bytes, mut handles) = message.take();
-        self.chan.write(&bytes, &mut handles)?;
-        Ok(())
-    }
-}
-
 /// An implementation of wl_display.
-struct DisplayReceiver {
-    registry: Arc<Mutex<wl::Registry>>,
-    event_sender: EventSender,
-}
+struct DisplayReceiver;
 
 impl wl::RequestReceiver<WlDisplay> for DisplayReceiver {
     fn receive(
-        display: wl::ObjectRef<Self>, request: WlDisplayRequest, client: &mut wl::Client,
+        this: wl::ObjectRef<Self>, request: WlDisplayRequest, client: &mut wl::Client,
     ) -> Result<(), Error> {
-        let map = client.objects();
-        let this = display.get(map)?;
         match request {
             WlDisplayRequest::GetRegistry { registry } => {
-                let receiver = RegistryReceiver {
-                    id: registry,
-                    registry: this.registry.clone(),
-                    event_sender: this.event_sender.clone(),
-                };
-                receiver.report_globals()?;
-                map.add_object(WlRegistry, registry, receiver)?;
+                RegistryReceiver::report_globals(registry, client)?;
+                client.objects().add_object(WlRegistry, registry, RegistryReceiver)?;
                 Ok(())
             }
             WlDisplayRequest::Sync { callback } => {
-                this.event_sender
-                    .send(callback, WlCallbackEvent::Done { callback_data: 0 })?;
-                this.event_sender
-                    .send(display.id(), WlDisplayEvent::DeleteId { id: callback })?;
+                client.post(callback, WlCallbackEvent::Done { callback_data: 0 })?;
+                client.post(this.id(), WlDisplayEvent::DeleteId { id: callback })?;
                 Ok(())
             }
         }
@@ -106,25 +68,20 @@ impl wl::RequestReceiver<WlDisplay> for DisplayReceiver {
 }
 
 /// An implementation of wl_registry.
-struct RegistryReceiver {
-    registry: Arc<Mutex<wl::Registry>>,
-
-    // TODO(tjdetwiler): We'll need a more scalable way to handle these.
-    id: wl::ObjectId,
-    event_sender: EventSender,
-}
+struct RegistryReceiver;
 
 impl RegistryReceiver {
     /// Sends a wl_registry::global event for each entry in the |wl::Registry|.
-    pub fn report_globals(&self) -> Result<(), Error> {
-        for (name, global) in self.registry.lock().globals().iter().enumerate() {
-            self.event_sender.send(
-                self.id,
+    pub fn report_globals(this: wl::ObjectId, client: &wl::Client) -> Result<(), Error> {
+        let registry = client.registry();
+        for (name, global) in registry.lock().globals().iter().enumerate() {
+            client.post(
+                this,
                 WlRegistryEvent::Global {
                     name: name as u32,
                     interface: global.interface().into(),
                     version: global.version(),
-                },
+                }
             )?;
         }
         Ok(())
@@ -133,10 +90,10 @@ impl RegistryReceiver {
 
 impl wl::RequestReceiver<WlRegistry> for RegistryReceiver {
     fn receive(
-        this: wl::ObjectRef<Self>, request: WlRegistryRequest, client: &mut wl::Client,
+        _this: wl::ObjectRef<Self>, request: WlRegistryRequest, client: &mut wl::Client,
     ) -> Result<(), Error> {
         let WlRegistryRequest::Bind { name, id, .. } = request;
-        let registry = this.get(client.objects())?.registry.clone();
+        let registry = client.registry();
         let (spec, receiver) = {
             let mut lock = registry.lock();
             if let Some(global) = lock.globals().get_mut(name as usize) {
