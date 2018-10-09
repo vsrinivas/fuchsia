992d06a11cb2b726f63d9e75b850e8a28ee7021c
Mike Voydanoff <voydanoff@google.com>
2018-10-08 20:58:15 -0700

    [arm64][imx8mmevk] Add support for booting on the imx8mmevk board
    Added arm64 magic to boot shim header to make bootloader happy.
    
    TEST: boots into shell on imx8mmevk
    
    Change-Id: Ia893839786a753c6b98dc927a7925fe6c91b693f

---
+++ kernel/target/arm64/board/imx8mmevk/rules.mk
@@ -0,0 +1,20 @@
+# Copyright 2018 The Fuchsia Authors
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+PLATFORM_BOARD_NAME := imx8mmevk
+PLATFORM_USE_SHIM := true
+PLATFORM_USE_MKBOOTIMG := true
+PLATFORM_DTB := mkbootimg
+PLATFORM_DTB_OFFSET := 0x03000000
+PLATFORM_USE_AVB := true
+
+PLATFORM_KERNEL_OFFSET := 0x00080000
+PLATFORM_MEMBASE := 0x40000000
+PLATFORM_CMDLINE := netsvc.netboot=true
+PLATFORM_BOOT_PARTITION_SIZE := 33554432
+
+include make/board.mk
--- system/ulib/ddk/include/ddk/platform-defs.h
+++ system/ulib/ddk/include/ddk/platform-defs.h
@@ -112,6 +112,7 @@ __BEGIN_CDECLS;
 // NXP
 #define PDEV_VID_NXP                9
 #define PDEV_PID_IMX8MEVK           1
+#define PDEV_PID_IMX8MMEVK          2
 
 #define PDEV_DID_IMX_GPIO           1
 #define PDEV_DID_IMX_DISPLAY        2
