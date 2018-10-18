bde688b074fc118f05b0f9664dc5e3d44d3d438d
Eric Wang <eyw@google.com>
2018-10-18 11:32:14 -0700

    [wlantool] Display Minstrel attempts/success
    Test: Cosmetic change only.
    Change-Id: I71834e375083d54e1652be4622ca3606faaa551b

--- bin/wlantool/src/main.rs
+++ bin/wlantool/src/main.rs
@@ -428,13 +428,15 @@ async fn get_peer_addrs(wlan_svc: WlanSvc, iface_id: u16, peer_addr: Option<MacA
 }
 
 fn print_minstrel_stats(mut peer: Box<Peer>) {
-    println!("{}, max_tp: {}, max_probability: {}", MacAddr(peer.mac_addr),
-            peer.max_tp, peer.max_probability);
-    println!("     TxVector                          succ_c  att_c   succ_t  att_t   \
-       probability throughput");
+    let total_attempts : f64 = peer.entries.iter().map(|e| e.attempts_total as f64).sum();
+    let total_success : f64 = peer.entries.iter().map(|e| e.success_total as f64).sum();
+    println!("{}, max_tp: {}, max_probability: {}, attempts/success: {:.6}", MacAddr(peer.mac_addr),
+            peer.max_tp, peer.max_probability, total_attempts / total_success);
+    println!("     TxVector                            succ_c   att_c  succ_t   att_t \
+           probability throughput");
     peer.entries.sort_by(|l, r| l.tx_vector_idx.cmp(&r.tx_vector_idx));
     for e in peer.entries {
-        println!("{}{} {:<36} {:<7} {:<7} {:<7} {:<7} {:>11.4} {:>10.3}",
+        println!("{}{} {:<36} {:7} {:7} {:7} {:7} {:11.4} {:10.3}",
             if e.tx_vector_idx == peer.max_tp { "T" } else { " " },
             if e.tx_vector_idx == peer.max_probability {"P"} else {" "},
             e.tx_vec_desc, e.success_cur, e.attempts_cur,
