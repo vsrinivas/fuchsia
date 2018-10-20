e54aa5581de3a5c954bf311ddf70e41d2fa2dec6
Robert Hahn <hahnr@google.com>
2018-10-19 18:16:40 -0700

    [wlan][common] Move BufferWriter/Reader into common
    Test=CQ
    
    Change-Id: Ie23ecf6cbbea7d196be551425c4eda06e5602468

--- lib/wlan/mlme/tests/buffer_utils_unittest.cpp
+++ lib/wlan/mlme/tests/buffer_utils_unittest.cpp
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <wlan/mlme/buffer_reader.h>
-#include <wlan/mlme/buffer_writer.h>
+#include <wlan/common/buffer_reader.h>
+#include <wlan/common/buffer_writer.h>
 
 #include <gtest/gtest.h>
 
