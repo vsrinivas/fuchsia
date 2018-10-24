d4d6574c025a88833f989897e617ba0c4bd48e8f
James Tucker <raggi@google.com>
2018-10-24 15:02:28 -0700

    [devshell] `serve`/`pave`/`netboot` replace `boot`
    The `pave` and `netboot` commands use scripts produced by the build, and are
    build/machine configuration specific. The `boot` script is deprecated,
    scheduled for future removal. The `serve` script now delegates to `pave`
    instead of `boot`. Some old non-working artifacts are removed.
    
    Change-Id: I8a94b15b64d1a69223714400857bc29d7083a89e

--- devshell/lib/image_build_vars.sh
+++ devshell/lib/image_build_vars.sh
@@ -5,24 +5,5 @@
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/vars.sh
 fx-config-read
 
-fx-machine-types() {
-  echo "Available machine types:"
-  echo "  ram     - ramboot with /system and /pkgfs from blobs"
-  echo "  netboot - alias for ram"
-  echo "  bootfs  - ramboot with /system from bootfs (deprecated)"
-  echo "  zedboot - ramboot zedboot into zedboot"
-  echo "  efi     - pave an EFI device (e.g. acer,nuc)"
-  echo "  cros    - pave a cros device (e.g. pixelbook,chromebook)"
-  echo "  arm     - pave an ARM device (e.g. vim2)"
-  echo "Machine type aliases:"
-  echo "  acer"
-  echo "  nuc"
-  echo "  pixel"
-  echo "  vboot"
-  echo
-  echo "Note: currently efi,cros,acer,nuc,pixel and vboot all have the exact"
-  echo "same behavior and target specific differences occur client side."
-}
-
 source "${FUCHSIA_BUILD_DIR}"/image_paths.sh
 source "${FUCHSIA_BUILD_DIR}"/zedboot_image_paths.sh
