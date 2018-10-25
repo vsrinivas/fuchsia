vsrinivas/fuchsia

commit ccb65e3f591002c5732b8d266daa544e4da78c24

[ddk][ddktl] Migrate intel-gpu-core protocol to autogenerated version.
Tested: Build succeeds.
Change-Id: Ic15f71064f885f657d8231c03fcdd3ba4b560d22

system/dev/display/intel-i915/interrupts.cpp
@@ -77,7 +77,7 @@ int Interrupts::IrqLoop() {
         {
             fbl::AutoLock lock(&lock_);
             if (interrupt_ctrl.reg_value() & interrupt_mask_) {
-                interrupt_cb_(interrupt_cb_data_, interrupt_ctrl.reg_value());
+                interrupt_cb_.callback(interrupt_cb_.ctx, interrupt_ctrl.reg_value());
             }
         }
 
@@ -135,14 +135,13 @@ void Interrupts::EnableHotplugInterrupts() {
     }
 }
 
-zx_status_t Interrupts::SetInterruptCallback(zx_intel_gpu_core_interrupt_callback_t callback,
-                                             void *data, uint32_t interrupt_mask) {
+zx_status_t Interrupts::SetInterruptCallback(const zx_intel_gpu_core_interrupt_t* callback,
+                                             uint32_t interrupt_mask) {
     fbl::AutoLock lock(&lock_);
-    if (callback != nullptr && interrupt_cb_ != nullptr) {
+    if (callback != nullptr && interrupt_cb_.callback != nullptr) {
         return ZX_ERR_ALREADY_BOUND;
     }
-    interrupt_cb_ = callback;
-    interrupt_cb_data_ = data;
+    interrupt_cb_ = *callback;
     interrupt_mask_ = interrupt_mask;
     return ZX_OK;
 }

system/dev/display/intel-i915/intel-i915.cpp
@@ -76,10 +76,10 @@ static zx_status_t get_pci_bti(void* ctx, uint32_t index, zx_handle_t* bti_out) 
 }
 
 static zx_status_t register_interrupt_callback(void* ctx,
-                                               zx_intel_gpu_core_interrupt_callback_t callback,
-                                               void* data, uint32_t interrupt_mask) {
+                                               const zx_intel_gpu_core_interrupt_t* callback,
+                                               uint32_t interrupt_mask) {
     return static_cast<i915::Controller*>(ctx)
-            ->RegisterInterruptCallback(callback, data, interrupt_mask);
+            ->RegisterInterruptCallback(callback, interrupt_mask);
 }
 
 static zx_status_t unregister_interrupt_callback(void* ctx) {
@@ -108,7 +108,7 @@ static zx_status_t gtt_insert(void* ctx, uint64_t addr, zx_handle_t buffer, uint
 }
 
 static zx_intel_gpu_core_protocol_ops_t i915_gpu_core_protocol_ops = {
-    .read_pci_config_16 = read_pci_config_16,
+    .read_pci_config16 = read_pci_config_16,
     .map_pci_mmio = map_pci_mmio,
     .unmap_pci_mmio = unmap_pci_mmio,
     .get_pci_bti = get_pci_bti,
@@ -1661,13 +1661,14 @@ zx_status_t Controller::GetPciBti(uint32_t index, zx_handle_t* bti_out) {
     return pci_get_bti(&pci_, index, bti_out);
 }
 
-zx_status_t Controller::RegisterInterruptCallback(zx_intel_gpu_core_interrupt_callback_t callback,
-                                                  void* data, uint32_t interrupt_mask) {
-    return interrupts_.SetInterruptCallback(callback, data, interrupt_mask);
+zx_status_t Controller::RegisterInterruptCallback(const zx_intel_gpu_core_interrupt_t* callback,
+                                                  uint32_t interrupt_mask) {
+    return interrupts_.SetInterruptCallback(callback, interrupt_mask);
 }
 
 zx_status_t Controller::UnregisterInterruptCallback() {
-    interrupts_.SetInterruptCallback(nullptr, nullptr, 0);
+    constexpr zx_intel_gpu_core_interrupt_t kNoCallback = {nullptr, nullptr};
+    interrupts_.SetInterruptCallback(&kNoCallback, 0);
     return ZX_OK;
 }
 

system/dev/display/intel-i915/intel-i915.h
@@ -91,8 +91,8 @@ public:
     zx_status_t MapPciMmio(uint32_t pci_bar, void** addr_out, uint64_t* size_out);
     zx_status_t UnmapPciMmio(uint32_t pci_bar);
     zx_status_t GetPciBti(uint32_t index, zx_handle_t* bti_out);
-    zx_status_t RegisterInterruptCallback(zx_intel_gpu_core_interrupt_callback_t callback,
-                                          void* data, uint32_t interrupt_mask);
+    zx_status_t RegisterInterruptCallback(const zx_intel_gpu_core_interrupt_t* callback,
+                                          uint32_t interrupt_mask);
     zx_status_t UnregisterInterruptCallback();
     uint64_t GttGetSize();
     zx_status_t GttAlloc(uint64_t page_count, uint64_t* addr_out);

system/dev/display/intel-i915/interrupts.h
@@ -27,8 +27,8 @@ public:
     void Destroy();
 
     void EnablePipeVsync(registers::Pipe pipe, bool enable);
-    zx_status_t SetInterruptCallback(zx_intel_gpu_core_interrupt_callback_t callback,
-                                     void* data, uint32_t interrupt_mask);
+    zx_status_t SetInterruptCallback(const zx_intel_gpu_core_interrupt_t* callback,
+                                     uint32_t interrupt_mask);
 
     int IrqLoop();
 private:
@@ -40,8 +40,7 @@ private:
     zx::handle irq_;
     thrd_t irq_thread_;
 
-    zx_intel_gpu_core_interrupt_callback_t interrupt_cb_ __TA_GUARDED(lock_);
-    void* interrupt_cb_data_ __TA_GUARDED(lock_);
+    zx_intel_gpu_core_interrupt_t interrupt_cb_ __TA_GUARDED(lock_);
     uint32_t interrupt_mask_ __TA_GUARDED(lock_) = 0;
 
     mtx_t lock_;

system/fidl/ddk/protocols/intel-gpu-core.banjo
@@ -1,0 +1,58 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+library ddk.protocol.intel_gpu_core;
+
+using zx;
+
+const uint32 IMAGE_TYPE_X_TILED = 1;
+const uint32 IMAGE_TYPE_Y_LEGACY_TILED = 2;
+const uint32 IMAGE_TYPE_YF_TILED = 3;
+
+[Layout="ddk-callback"]
+interface ZxIntelGpuCoreInterrupt {
+    1: Callback(uint32 master_interrupt_control) -> ();
+};
+
+[Layout="ddk-protocol"]
+interface ZxIntelGpuCore {
+    /// Reads 16 bits from pci config space; returned in |value_out|.
+    1: ReadPciConfig16(uint16 addr) -> (zx.status s, uint16 value);
+
+    /// Maps the given |pci_bar|; address returned in |addr_out|, size in bytes returned in
+    /// |size_out|.
+    2: MapPciMmio(uint32 pci_bar) -> (zx.status s, vector<void>? buf);
+
+    /// Unmaps the given |pci_bar|.
+    3: UnmapPciMmio(uint32 pci_bar) -> (zx.status s);
+
+    /// Returns a bus transaction initiator.
+    4: GetPciBti(uint32 index) -> (zx.status s, handle<bti> bti);
+
+    /// Registers the given |callback| to be invoked with parameter |data| when an interrupt occurs
+    /// matching |interrupt_mask|.
+    5: RegisterInterruptCallback(ZxIntelGpuCoreInterrupt callback,
+                                 uint32 interrupt_mask) -> (zx.status s);
+
+    /// Un-registers a previously registered interrupt callback.
+    6: UnregisterInterruptCallback() -> (zx.status s);
+
+    /// Returns the size of the GTT (global translation table) in bytes.
+    7: GttGetSize() -> (uint64 size);
+
+    /// Allocates a region of the GTT of the given |page_count|, returning the page-aligned virtual
+    /// address in |addr_out|.
+    8: GttAlloc(uint64 page_count) -> (zx.status s, uint64 addr);
+
+    /// Frees the GTT allocation given by |addr|.
+    9: GttFree(uint64 addr) -> (zx.status s);
+
+    /// Clears the page table entries for the GTT allocation given by |addr|.
+    10: GttClear(uint64 addr) -> (zx.status s);
+
+    /// Inserts page tables entries for the GTT allocation given by |addr| for the vmo represented by
+    /// handle |buffer|, at the given |page_offset| and |page_count|. Takes ownership of |buffer|.
+    11: GttInsert(uint64 addr, handle<vmo> buffer, uint64 page_offset,
+                  uint64 page_count) -> (zx.status s);
+};

system/ulib/ddk/include/ddk/protocol/intel-gpu-core.h
@@ -1,6 +1,9 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+// WARNING: THIS FILE IS MACHINE GENERATED. DO NOT EDIT.
+//          MODIFY system/fidl/protocols/intel_gpu_core.fidl INSTEAD.
 
 #pragma once
 
@@ -9,59 +12,109 @@
 
 __BEGIN_CDECLS;
 
-#define IMAGE_TYPE_X_TILED 1
-#define IMAGE_TYPE_Y_LEGACY_TILED 2
-#define IMAGE_TYPE_YF_TILED 3
+// Forward declarations
 
-typedef void (*zx_intel_gpu_core_interrupt_callback_t)(void* data,
-                                                       uint32_t master_interrupt_control);
+typedef struct zx_intel_gpu_core_interrupt zx_intel_gpu_core_interrupt_t;
+typedef struct zx_intel_gpu_core_protocol zx_intel_gpu_core_protocol_t;
+
+// Declarations
+
+#define IMAGE_TYPE_Y_LEGACY_TILED UINT32_C(2)
+
+struct zx_intel_gpu_core_interrupt {
+    void (*callback)(void* ctx, uint32_t master_interrupt_control);
+    void* ctx;
+};
 
 typedef struct zx_intel_gpu_core_protocol_ops {
-    // Reads 16 bits from pci config space; returned in |value_out|.
-    zx_status_t (*read_pci_config_16)(void* ctx, uint16_t addr, uint16_t* value_out);
-
-    // Maps the given |pci_bar|; address returned in |addr_out|, size in bytes returned in
-    // |size_out|.
-    zx_status_t (*map_pci_mmio)(void* ctx, uint32_t pci_bar, void** addr_out, uint64_t* size_out);
-
-    // Unmaps the given |pci_bar|.
+    zx_status_t (*read_pci_config16)(void* ctx, uint16_t addr, uint16_t* out_value);
+    zx_status_t (*map_pci_mmio)(void* ctx, uint32_t pci_bar, void** out_buf_buffer,
+                                size_t* buf_size);
     zx_status_t (*unmap_pci_mmio)(void* ctx, uint32_t pci_bar);
-
-    // Returns a bus transaction initiator.
-    zx_status_t (*get_pci_bti)(void* ctx, uint32_t index, zx_handle_t* bti_out);
-
-    // Registers the given |callback| to be invoked with parameter |data| when an interrupt occurs
-    // matching |interrupt_mask|.
+    zx_status_t (*get_pci_bti)(void* ctx, uint32_t index, zx_handle_t* out_bti);
     zx_status_t (*register_interrupt_callback)(void* ctx,
-                                               zx_intel_gpu_core_interrupt_callback_t callback,
-                                               void* data, uint32_t interrupt_mask);
-
-    // Un-registers a previously registered interrupt callback.
+                                               const zx_intel_gpu_core_interrupt_t* callback,
+                                               uint32_t interrupt_mask);
     zx_status_t (*unregister_interrupt_callback)(void* ctx);
-
-    // Returns the size of the GTT (global translation table) in bytes.
     uint64_t (*gtt_get_size)(void* ctx);
-
-    // Allocates a region of the GTT of the given |page_count|, returning the page-aligned virtual
-    // address in |addr_out|.
-    zx_status_t (*gtt_alloc)(void* ctx, uint64_t page_count, uint64_t* addr_out);
-
-    // Frees the GTT allocation given by |addr|.
+    zx_status_t (*gtt_alloc)(void* ctx, uint64_t page_count, uint64_t* out_addr);
     zx_status_t (*gtt_free)(void* ctx, uint64_t addr);
-
-    // Clears the page table entries for the GTT allocation given by |addr|.
     zx_status_t (*gtt_clear)(void* ctx, uint64_t addr);
-
-    // Inserts page tables entries for the GTT allocation given by |addr| for the vmo represented by
-    // handle |buffer|, at the given |page_offset| and |page_count|. Takes ownership of |buffer|.
     zx_status_t (*gtt_insert)(void* ctx, uint64_t addr, zx_handle_t buffer, uint64_t page_offset,
                               uint64_t page_count);
-
 } zx_intel_gpu_core_protocol_ops_t;
 
-typedef struct zx_intel_gpu_core_protocol {
+struct zx_intel_gpu_core_protocol {
     zx_intel_gpu_core_protocol_ops_t* ops;
     void* ctx;
-} zx_intel_gpu_core_protocol_t;
+};
+
+// Reads 16 bits from pci config space; returned in |value_out|.
+static inline zx_status_t
+zx_intel_gpu_core_read_pci_config16(const zx_intel_gpu_core_protocol_t* proto, uint16_t addr,
+                                    uint16_t* out_value) {
+    return proto->ops->read_pci_config16(proto->ctx, addr, out_value);
+}
+// Maps the given |pci_bar|; address returned in |addr_out|, size in bytes returned in
+// |size_out|.
+static inline zx_status_t zx_intel_gpu_core_map_pci_mmio(const zx_intel_gpu_core_protocol_t* proto,
+                                                         uint32_t pci_bar, void** out_buf_buffer,
+                                                         size_t* buf_size) {
+    return proto->ops->map_pci_mmio(proto->ctx, pci_bar, out_buf_buffer, buf_size);
+}
+// Unmaps the given |pci_bar|.
+static inline zx_status_t
+zx_intel_gpu_core_unmap_pci_mmio(const zx_intel_gpu_core_protocol_t* proto, uint32_t pci_bar) {
+    return proto->ops->unmap_pci_mmio(proto->ctx, pci_bar);
+}
+// Returns a bus transaction initiator.
+static inline zx_status_t zx_intel_gpu_core_get_pci_bti(const zx_intel_gpu_core_protocol_t* proto,
+                                                        uint32_t index, zx_handle_t* out_bti) {
+    return proto->ops->get_pci_bti(proto->ctx, index, out_bti);
+}
+// Registers the given |callback| to be invoked with parameter |data| when an interrupt occurs
+// matching |interrupt_mask|.
+static inline zx_status_t
+zx_intel_gpu_core_register_interrupt_callback(const zx_intel_gpu_core_protocol_t* proto,
+                                              const zx_intel_gpu_core_interrupt_t* callback,
+                                              uint32_t interrupt_mask) {
+    return proto->ops->register_interrupt_callback(proto->ctx, callback, interrupt_mask);
+}
+// Un-registers a previously registered interrupt callback.
+static inline zx_status_t
+zx_intel_gpu_core_unregister_interrupt_callback(const zx_intel_gpu_core_protocol_t* proto) {
+    return proto->ops->unregister_interrupt_callback(proto->ctx);
+}
+// Returns the size of the GTT (global translation table) in bytes.
+static inline uint64_t zx_intel_gpu_core_gtt_get_size(const zx_intel_gpu_core_protocol_t* proto) {
+    return proto->ops->gtt_get_size(proto->ctx);
+}
+// Allocates a region of the GTT of the given |page_count|, returning the page-aligned virtual
+// address in |addr_out|.
+static inline zx_status_t zx_intel_gpu_core_gtt_alloc(const zx_intel_gpu_core_protocol_t* proto,
+                                                      uint64_t page_count, uint64_t* out_addr) {
+    return proto->ops->gtt_alloc(proto->ctx, page_count, out_addr);
+}
+// Frees the GTT allocation given by |addr|.
+static inline zx_status_t zx_intel_gpu_core_gtt_free(const zx_intel_gpu_core_protocol_t* proto,
+                                                     uint64_t addr) {
+    return proto->ops->gtt_free(proto->ctx, addr);
+}
+// Clears the page table entries for the GTT allocation given by |addr|.
+static inline zx_status_t zx_intel_gpu_core_gtt_clear(const zx_intel_gpu_core_protocol_t* proto,
+                                                      uint64_t addr) {
+    return proto->ops->gtt_clear(proto->ctx, addr);
+}
+// Inserts page tables entries for the GTT allocation given by |addr| for the vmo represented by
+// handle |buffer|, at the given |page_offset| and |page_count|. Takes ownership of |buffer|.
+static inline zx_status_t zx_intel_gpu_core_gtt_insert(const zx_intel_gpu_core_protocol_t* proto,
+                                                       uint64_t addr, zx_handle_t buffer,
+                                                       uint64_t page_offset, uint64_t page_count) {
+    return proto->ops->gtt_insert(proto->ctx, addr, buffer, page_offset, page_count);
+}
+
+#define IMAGE_TYPE_YF_TILED UINT32_C(3)
+
+#define IMAGE_TYPE_X_TILED UINT32_C(1)
 
 __END_CDECLS;

system/ulib/ddktl/include/ddktl/protocol/intel-gpu-core-internal.h
@@ -1,0 +1,91 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// WARNING: THIS FILE IS MACHINE GENERATED. DO NOT EDIT.
+//          MODIFY system/fidl/protocols/intel_gpu_core.fidl INSTEAD.
+
+#pragma once
+
+#include <ddk/protocol/intel-gpu-core.h>
+#include <fbl/type_support.h>
+
+namespace ddk {
+namespace internal {
+
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_read_pci_config16,
+                                     ZxIntelGpuCoreReadPciConfig16,
+                                     zx_status_t (C::*)(uint16_t addr, uint16_t* out_value));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_map_pci_mmio,
+                                     ZxIntelGpuCoreMapPciMmio,
+                                     zx_status_t (C::*)(uint32_t pci_bar, void** out_buf_buffer,
+                                                        size_t* buf_size));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_unmap_pci_mmio,
+                                     ZxIntelGpuCoreUnmapPciMmio,
+                                     zx_status_t (C::*)(uint32_t pci_bar));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_get_pci_bti,
+                                     ZxIntelGpuCoreGetPciBti,
+                                     zx_status_t (C::*)(uint32_t index, zx_handle_t* out_bti));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(
+    has_zx_intel_gpu_core_protocol_register_interrupt_callback,
+    ZxIntelGpuCoreRegisterInterruptCallback,
+    zx_status_t (C::*)(const zx_intel_gpu_core_interrupt_t* callback, uint32_t interrupt_mask));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_unregister_interrupt_callback,
+                                     ZxIntelGpuCoreUnregisterInterruptCallback,
+                                     zx_status_t (C::*)());
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_gtt_get_size,
+                                     ZxIntelGpuCoreGttGetSize, uint64_t (C::*)());
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_gtt_alloc,
+                                     ZxIntelGpuCoreGttAlloc,
+                                     zx_status_t (C::*)(uint64_t page_count, uint64_t* out_addr));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_gtt_free, ZxIntelGpuCoreGttFree,
+                                     zx_status_t (C::*)(uint64_t addr));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_gtt_clear,
+                                     ZxIntelGpuCoreGttClear, zx_status_t (C::*)(uint64_t addr));
+DECLARE_HAS_MEMBER_FN_WITH_SIGNATURE(has_zx_intel_gpu_core_protocol_gtt_insert,
+                                     ZxIntelGpuCoreGttInsert,
+                                     zx_status_t (C::*)(uint64_t addr, zx_handle_t buffer,
+                                                        uint64_t page_offset, uint64_t page_count));
+
+template <typename D>
+constexpr void CheckZxIntelGpuCoreProtocolSubclass() {
+    static_assert(internal::has_zx_intel_gpu_core_protocol_read_pci_config16<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreReadPciConfig16(uint16_t addr, uint16_t* out_value");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_map_pci_mmio<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreMapPciMmio(uint32_t pci_bar, void** out_buf_buffer, "
+                  "size_t* buf_size");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_unmap_pci_mmio<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreUnmapPciMmio(uint32_t pci_bar");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_get_pci_bti<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreGetPciBti(uint32_t index, zx_handle_t* out_bti");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_register_interrupt_callback<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreRegisterInterruptCallback(const "
+                  "zx_intel_gpu_core_interrupt_t* callback, uint32_t interrupt_mask");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_unregister_interrupt_callback<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreUnregisterInterruptCallback(");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_gtt_get_size<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "uint64_t ZxIntelGpuCoreGttGetSize(");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_gtt_alloc<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreGttAlloc(uint64_t page_count, uint64_t* out_addr");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_gtt_free<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreGttFree(uint64_t addr");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_gtt_clear<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreGttClear(uint64_t addr");
+    static_assert(internal::has_zx_intel_gpu_core_protocol_gtt_insert<D>::value,
+                  "ZxIntelGpuCoreProtocol subclasses must implement "
+                  "zx_status_t ZxIntelGpuCoreGttInsert(uint64_t addr, zx_handle_t buffer, uint64_t "
+                  "page_offset, uint64_t page_count");
+}
+
+} // namespace internal
+} // namespace ddk

commit ae5b51c51b060f9e9a4f9f47d126fe5307df2b00

[intel-i915] Fix unregister interrupt callback
This was broken by ccb65e3f591002c5732b8d266daa544e4da78c24:
magma intel hardware tests asserting in debug mode.

Test:
pixelbook:go/magma-tps#L0

Change-Id: Ie612da7e9056d58a4a7eb9669fecb2cb3b0fb1a7

system/dev/display/intel-i915/interrupts.cpp
@@ -138,7 +138,7 @@ void Interrupts::EnableHotplugInterrupts() {
 zx_status_t Interrupts::SetInterruptCallback(const zx_intel_gpu_core_interrupt_t* callback,
                                              uint32_t interrupt_mask) {
     fbl::AutoLock lock(&lock_);
-    if (callback != nullptr && interrupt_cb_.callback != nullptr) {
+    if (callback->callback != nullptr && interrupt_cb_.callback != nullptr) {
         return ZX_ERR_ALREADY_BOUND;
     }
     interrupt_cb_ = *callback;