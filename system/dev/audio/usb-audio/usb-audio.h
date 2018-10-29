fcbe4779a77096170087b7d735342e484abbee41
Ruchira Ravoori <ravoorir@google.com>
2018-10-29 12:35:26 -0700

    [zircon][usb]Allocate more req size to acommodate internal contexts
    This is part2 of my previous changeset, https://fuchsia-review.googlesource.com/c/zircon/+/218979
    I just broke this down into 2 changesets, so as not to change too many
    clients in 1 change.
    
    Test:
    vim2: Plugged in a usb mass storage device
          and read using dd on it.
    Pixelbook: Netbooted pixelbook. Plugged in a usb mass storage
               device and read some blocks using dd.
               audio tone
    Astro: Netbooted. Verified that the usb ethernet adapter showed up.
    
    Change-Id: I6136b937988958a460b369b368c84a60199cb528

--- system/dev/audio/usb-audio/usb-audio.h
+++ system/dev/audio/usb-audio/usb-audio.h
@@ -18,11 +18,13 @@ __BEGIN_CDECLS
 
 zx_status_t usb_midi_sink_create(zx_device_t* device, usb_protocol_t* usb, int index,
                                  const usb_interface_descriptor_t* intf,
-                                 const usb_endpoint_descriptor_t* ep);
+                                 const usb_endpoint_descriptor_t* ep,
+                                 const size_t req_size);
 
 zx_status_t usb_midi_source_create(zx_device_t* device, usb_protocol_t* usb, int index,
                                    const usb_interface_descriptor_t* intf,
-                                   const usb_endpoint_descriptor_t* ep);
+                                   const usb_endpoint_descriptor_t* ep,
+                                   const size_t req_size);
 
 __END_CDECLS
 
