36a6daf8589dbb6978209f5b5d3a435d93e21b42
Ed James <edjames@google.com>
2018-10-30 12:27:55 -0700

    [vulkan] Add id for Intel(R) HD Graphics 510 (Skylake GT1)
    Change-Id: I601a0cce7ce73d986ae9db06d5cdca459758ac9c
    Test: booted, works now

--- drivers/gpu/msd-intel-gen/src/device_id.h
+++ drivers/gpu/msd-intel-gen/src/device_id.h
@@ -12,6 +12,7 @@ public:
     static bool is_gen9(uint32_t device_id)
     {
         switch (device_id) {
+            case 0x1906: // Intel(R) HD Graphics 510 (Skylake GT1)
             case 0x1912: // Intel(R) HD Graphics 530 (Skylake GT2)
             case 0x1916: // Intel(R) HD Graphics 520 (Skylake GT2)
             case 0x191E: // Intel(R) HD Graphics 515 (Skylake GT2)
