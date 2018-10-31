6510c9ebec20a744efbce44d9ff16f45ddaeeba2
Bert Muthalaly <stijlist@google.com>
2018-10-31 13:02:01 -0400

    [netstack] Verify initial DHCP state when dhcp is configured
    Tested: fx run-test netstack_test
    Change-Id: I99706033e17b295bb73d0fcb313a60e76d72d15a

--- go/src/netstack/netstack_test.go
+++ go/src/netstack/netstack_test.go
@@ -14,25 +14,13 @@ import (
 	"testing"
 )
 
-const testDeviceName string = "testdevice"
+const (
+	testDeviceName string = "testdevice"
+	testTopoPath   string = "/fake/ethernet/device"
+)
 
 func TestNicName(t *testing.T) {
-	arena, err := eth.NewArena()
-	if err != nil {
-		t.Fatal(err)
-	}
-	ns := &Netstack{
-		arena:    arena,
-		ifStates: make(map[tcpip.NICID]*ifState),
-	}
-	ns.mu.stack = stack.New(
-		[]string{
-			ipv4.ProtocolName,
-			ipv6.ProtocolName,
-			arp.ProtocolName,
-		}, nil, stack.Options{})
-
-	OnInterfacesChanged = func() {}
+	ns := newNetstack(t)
 
 	ifs, err := ns.addEth("/fake/ethernet/device", netstack.InterfaceConfig{Name: testDeviceName}, &ethernetext.Device{
 		TB:                t,
@@ -59,5 +47,76 @@ func TestNicName(t *testing.T) {
 	}
 	if ifs.nic.Name != testDeviceName {
 		t.Errorf("ifs.nic.Name = %v, want %v", ifs.nic.Name, testDeviceName)
+	}
+}
+
+func TestInitialDhcpConfiguration(t *testing.T) {
+	ns := newNetstack(t)
+
+	ipAddressConfig := netstack.IpAddressConfig{}
+	ipAddressConfig.SetDhcp(true)
+
+	d := deviceForAddEth(ethernet.Info{}, t)
+	ifs, err := ns.addEth("/fake/ethernet/device", netstack.InterfaceConfig{Name: testDeviceName, IpAddressConfig: ipAddressConfig}, &d)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ifs.dhcpState.client == nil {
+		t.Errorf("no dhcp client")
+	}
+	if ifs.dhcpState.enabled == false {
+		t.Errorf("dhcp disabled")
+	}
+}
+
+func newNetstack(t *testing.T) *Netstack {
+	arena, err := eth.NewArena()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ns := &Netstack{
+		arena:    arena,
+		ifStates: make(map[tcpip.NICID]*ifState),
+	}
+	ns.mu.stack = stack.New(
+		[]string{
+			ipv4.ProtocolName,
+			ipv6.ProtocolName,
+			arp.ProtocolName,
+		}, nil, stack.Options{})
+
+	OnInterfacesChanged = func() {}
+	return ns
+}
+
+// Returns an ethernetext.Device struct that implements
+// ethernet.Device and can be started and stopped.
+//
+// Reports the passed in ethernet.Info when Device#GetInfo is called.
+func deviceForAddEth(info ethernet.Info, t *testing.T) ethernetext.Device {
+	return ethernetext.Device{
+		TB: t,
+		GetInfoImpl: func() (ethernet.Info, error) {
+			return info, nil
+		},
+		SetClientNameImpl: func(string) (int32, error) { return 0, nil },
+		GetStatusImpl: func() (uint32, error) {
+			return uint32(eth.LinkUp), nil
+		},
+		GetFifosImpl: func() (int32, *ethernet.Fifos, error) {
+			return int32(zx.ErrOk), &ethernet.Fifos{
+				TxDepth: 1,
+			}, nil
+		},
+		SetIoBufferImpl: func(zx.VMO) (int32, error) {
+			return int32(zx.ErrOk), nil
+		},
+		StartImpl: func() (int32, error) {
+			return int32(zx.ErrOk), nil
+		},
+		StopImpl: func() error {
+			return nil
+		},
 	}
 }
