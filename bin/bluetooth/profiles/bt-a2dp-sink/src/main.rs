NERD-Repo/garnet

[bt][avdtp] Add initial AVDTP library
Add AVDTP crate, which implements the Audio/Video Distribution
Transport Protocol.

A request stream is generated for remote requests, and commands are
asynchronously completed based on responses from the remote peer.

NET-1550 #done
NET-1549 #done

Test: fx run-test avdtp_lib_test_rustc
Change-Id: I047b5c98744b338216bb6848032c08036b69d2bc

bin/bluetooth/profiles/bt-a2dp-sink/src/main.rs
@@ -3,99 +3,160 @@
 // found in the LICENSE file.
 
 #![deny(warnings)]
+#![feature(futures_api, pin, async_await, arbitrary_self_types, await_macro)]
 
 #[macro_use]
 extern crate failure;
 
+use avdtp;
 use failure::{Error, ResultExt};
 use fidl_fuchsia_bluetooth_bredr::*;
-use futures::{future, TryFutureExt, TryStreamExt};
-use parking_lot::RwLock;
+use fuchsia_async as fasync;
+use fuchsia_zircon as zx;
+use futures::{future, TryFutureExt, TryStreamExt, StreamExt};
+use std::collections::HashSet;
+
+fn get_available_stream_info() -> Result<Vec<avdtp::StreamInformation>, avdtp::Error> {
+    // TODO(jamuraa): retrieve available streams from Media
+    let s = avdtp::StreamInformation::new(
+        1,
+        false,
+        avdtp::MediaType::Audio,
+        avdtp::EndpointType::Sink,
+    )?;
+    Ok(vec![s])
+}
+
+fn start_media_stream(sock: zx::Socket) -> Result<(), zx::Status> {
+    let mut stream_sock = fasync::Socket::from_socket(sock)?;
+    fuchsia_async::spawn((async move {
+        let mut total_bytes = 0;
+        while let Some(item) = await!(stream_sock.next()) {
+            match item {
+                Ok(pkt) => {
+                    // TODO(jamuraa): secode the media stream into packets and frames
+                    // TODO(jamuraa): deliver audio frames to media
+                    total_bytes += pkt.len();
+                    eprint!(
+                        "Media Packet received: +{} bytes = {} \r",
+                        pkt.len(),
+                        total_bytes
+                    );
+                },
+                Err(e) => return Err(e),
+            }
+        };
+        eprintln!("Media stream finished");
+        Ok(())
+    }).unwrap_or_else(|e| eprintln!("Error in media stream: {:?}", e)));
+    Ok(())
+}
+
+fn handle_request(r: avdtp::Request) -> Result<(), avdtp::Error> {
+    eprintln!("Handling {:?} from peer..", r);
+    match r {
+        avdtp::Request::Discover { responder } => {
+            let streams = get_available_stream_info()?;
+            responder.send(&streams)
+        }
+        //_ => Ok(()),
+    }
+}
+
+async fn init() -> Result<(), Error> {
+    let profile_svc = fuchsia_app::client::connect_to_service::<ProfileMarker>()
+            .context("Failed to connect to Bluetooth profile service")?;
+
+    let mut service_def = make_profile_service_definition();
+    let (status, service_id) = await!(profile_svc.add_service(&mut service_def, SecurityLevel::EncryptionOptional, false))?;
+
+    eprintln!("Registered Service {:} for A2DP Sink", service_id);
+
+    if let Some(e) = status.error {
+        return Err(format_err!("Couldn't add A2DP sink service: {:?}", e));
+    }
+
+    let mut remotes: HashSet<String> = HashSet::new();
+    let mut evt_stream = profile_svc.take_event_stream();
+    while let Some(evt) = await!(evt_stream.next()) {
+        if evt.is_err() {
+            return Err(evt.err().unwrap().into());
+        }
+        match evt.unwrap() {
+            ProfileEvent::OnConnected {
+                device_id,
+                service_id: _,
+                channel,
+                protocol,
+            } => {
+                eprintln!( "Connection from {}: {:?} {:?}!", device_id, channel, protocol);
+                if remotes.contains(&device_id) {
+                    eprintln!("{} already connected: connecting media channel", device_id);
+                    let _ = start_media_stream(channel);
+                    continue;
+                }
+                eprintln!("Adding new peer for {}", device_id);
+                let peer = match avdtp::Peer::new(channel) {
+                    Ok(peer) => peer,
+                    Err(e) => {
+                        eprintln!("Error adding peer: {:?}", e);
+                        continue;
+                    }
+                };
+                // Start handling the requests
+                let stream = peer.take_request_stream();
+                fuchsia_async::spawn(async move {
+                    let x = await!(stream.try_for_each(|r| future::ready(handle_request(r))));
+                    eprintln!("Peer Disconnected: {:?}", x);
+                });
+                remotes.insert(device_id);
+            }
+            ProfileEvent::OnDisconnected {
+                device_id,
+                service_id,
+            } => {
+                eprintln!("Device {} disconnected from {}", device_id, service_id);
+                remotes.remove(&device_id);
+            }
+        }
+    };
+    Ok(())
+}
 
 fn main() -> Result<(), Error> {
     let mut executor = fuchsia_async::Executor::new().context("error creating executor")?;
 
-    let profile_svc = RwLock::new(
-        fuchsia_app::client::connect_to_service::<ProfileMarker>()
-            .context("Failed to connect to Bluetooth profile service")?,
-    );
+    executor.run_singlethreaded(init())
+}
 
-    let evt_stream = profile_svc.read().take_event_stream();
-
-    let event_fut = evt_stream.try_for_each(move |evt| match evt {
-        ProfileEvent::OnConnected {
-            device_id,
-            service_id: _,
-            channel,
-            protocol,
-        } => {
-            eprintln!("Connection from {}: {:?} {:?}!", device_id, channel, protocol);
-            future::ready(Ok(()))
-        }
-        ProfileEvent::OnDisconnected {
-            device_id,
-            service_id,
-        } => {
-            eprintln!("Device {} disconnected from {}", device_id, service_id);
-            future::ready(Ok(()))
-        }
-    });
-
-    // Create and register the service
-    let mut service_def = ServiceDefinition {
+fn make_profile_service_definition() -> ServiceDefinition {
+    ServiceDefinition {
         service_class_uuids: vec![String::from("110B")], // Audio Sink UUID
         protocol_descriptors: vec![
             ProtocolDescriptor {
                 protocol: ProtocolIdentifier::L2Cap,
-                params: vec![
-                    DataElement {
-                        type_: DataElementType::UnsignedInteger,
-                        size: 2,
-                        data: DataElementData::Integer(PSM_AVDTP),
-                    },
-                ],
+                params: vec![DataElement {
+                    type_: DataElementType::UnsignedInteger,
+                    size: 2,
+                    data: DataElementData::Integer(PSM_AVDTP),
+                }],
             },
             ProtocolDescriptor {
                 protocol: ProtocolIdentifier::Avdtp,
-                params: vec![
-                    DataElement {
-                        type_: DataElementType::UnsignedInteger,
-                        size: 2,
-                        data: DataElementData::Integer(0x0103), // Indicate v1.3
-                    },
-                ],
+                params: vec![DataElement {
+                    type_: DataElementType::UnsignedInteger,
+                    size: 2,
+                    data: DataElementData::Integer(0x0103), // Indicate v1.3
+                }],
             },
         ],
-        profile_descriptors: vec![
-            ProfileDescriptor {
-                profile_id: ServiceClassProfileIdentifier::AdvancedAudioDistribution,
-                major_version: 1,
-                minor_version: 3,
-            },
-        ],
+        profile_descriptors: vec![ProfileDescriptor {
+            profile_id: ServiceClassProfileIdentifier::AdvancedAudioDistribution,
+            major_version: 1,
+            minor_version: 3,
+        }],
         additional_protocol_descriptors: None,
         information: vec![],
         additional_attributes: None,
-    };
-
-    let mut service_id = 0;
-
-    let connect_fut = profile_svc
-        .read()
-        .add_service(&mut service_def, SecurityLevel::EncryptionOptional, false)
-        .map_err(|_e| format_err!("can't send message"))
-        .and_then(|(status, id)| {
-            future::ready(match status.error {
-                None => {
-                    service_id = id;
-                    Ok(())
-                }
-                Some(_e) => Err(format_err!("couldn't add service")),
-            })
-        });
-
-    let fut = connect_fut.and_then(|_| event_fut.map_err(Into::into));
-    executor.run_singlethreaded(fut).map_err(Into::into)
-    // TODO(jamuraa): hook into media server to provide audio
-    // TODO(jamuraa): accept connections and pass to AVDTP layer
+    }
 }

vsrinivas/fuchsia

bin/bluetooth/profiles/bt-a2dp-sink/src/main.rs
@@ -3,99 +3,171 @@
 // found in the LICENSE file.
 
 #![deny(warnings)]
+#![feature(futures_api, pin, async_await, arbitrary_self_types, await_macro)]
 
 #[macro_use]
 extern crate failure;
 
+use bt_avdtp as avdtp;
 use failure::{Error, ResultExt};
 use fidl_fuchsia_bluetooth_bredr::*;
-use futures::{future, TryFutureExt, TryStreamExt};
-use parking_lot::RwLock;
+use fuchsia_async as fasync;
+use fuchsia_syslog::{fx_log, fx_log_info, fx_log_warn, fx_vlog};
+use fuchsia_zircon as zx;
+use futures::{future, StreamExt, TryFutureExt, TryStreamExt};
+use std::collections::HashSet;
 
-fn main() -> Result<(), Error> {
-    let mut executor = fuchsia_async::Executor::new().context("error creating executor")?;
-
-    let profile_svc = RwLock::new(
-        fuchsia_app::client::connect_to_service::<ProfileMarker>()
-            .context("Failed to connect to Bluetooth profile service")?,
-    );
-
-    let evt_stream = profile_svc.read().take_event_stream();
-
-    let event_fut = evt_stream.try_for_each(move |evt| match evt {
-        ProfileEvent::OnConnected {
-            device_id,
-            service_id: _,
-            channel,
-            protocol,
-        } => {
-            eprintln!("Connection from {}: {:?} {:?}!", device_id, channel, protocol);
-            future::ready(Ok(()))
-        }
-        ProfileEvent::OnDisconnected {
-            device_id,
-            service_id,
-        } => {
-            eprintln!("Device {} disconnected from {}", device_id, service_id);
-            future::ready(Ok(()))
-        }
-    });
-
-    // Create and register the service
-    let mut service_def = ServiceDefinition {
+/// Make the SDP definidion for the A2DP sink service.
+fn make_profile_service_definition() -> ServiceDefinition {
+    ServiceDefinition {
         service_class_uuids: vec![String::from("110B")], // Audio Sink UUID
         protocol_descriptors: vec![
             ProtocolDescriptor {
                 protocol: ProtocolIdentifier::L2Cap,
-                params: vec![
-                    DataElement {
-                        type_: DataElementType::UnsignedInteger,
-                        size: 2,
-                        data: DataElementData::Integer(PSM_AVDTP),
-                    },
-                ],
+                params: vec![DataElement {
+                    type_: DataElementType::UnsignedInteger,
+                    size: 2,
+                    data: DataElementData::Integer(PSM_AVDTP),
+                }],
             },
             ProtocolDescriptor {
                 protocol: ProtocolIdentifier::Avdtp,
-                params: vec![
-                    DataElement {
-                        type_: DataElementType::UnsignedInteger,
-                        size: 2,
-                        data: DataElementData::Integer(0x0103), // Indicate v1.3
-                    },
-                ],
+                params: vec![DataElement {
+                    type_: DataElementType::UnsignedInteger,
+                    size: 2,
+                    data: DataElementData::Integer(0x0103), // Indicate v1.3
+                }],
             },
         ],
-        profile_descriptors: vec![
-            ProfileDescriptor {
-                profile_id: ServiceClassProfileIdentifier::AdvancedAudioDistribution,
-                major_version: 1,
-                minor_version: 3,
-            },
-        ],
+        profile_descriptors: vec![ProfileDescriptor {
+            profile_id: ServiceClassProfileIdentifier::AdvancedAudioDistribution,
+            major_version: 1,
+            minor_version: 3,
+        }],
         additional_protocol_descriptors: None,
         information: vec![],
         additional_attributes: None,
-    };
+    }
+}
 
-    let mut service_id = 0;
+fn get_available_stream_info() -> Result<Vec<avdtp::StreamInformation>, avdtp::Error> {
+    // TODO(jamuraa): retrieve available streams from Media
+    let s = avdtp::StreamInformation::new(
+        1,
+        false,
+        avdtp::MediaType::Audio,
+        avdtp::EndpointType::Sink,
+    )?;
+    Ok(vec![s])
+}
 
-    let connect_fut = profile_svc
-        .read()
-        .add_service(&mut service_def, SecurityLevel::EncryptionOptional, false)
-        .map_err(|_e| format_err!("can't send message"))
-        .and_then(|(status, id)| {
-            future::ready(match status.error {
-                None => {
-                    service_id = id;
-                    Ok(())
+fn start_media_stream(sock: zx::Socket) -> Result<(), zx::Status> {
+    let mut stream_sock = fasync::Socket::from_socket(sock)?;
+    fuchsia_async::spawn(
+        (async move {
+            let mut total_bytes = 0;
+            while let Some(item) = await!(stream_sock.next()) {
+                match item {
+                    Ok(pkt) => {
+                        // TODO(jamuraa): decode the media stream into packets and frames
+                        // TODO(jamuraa): deliver audio frames to media
+                        total_bytes += pkt.len();
+                        eprint!(
+                            "Media Packet received: +{} bytes = {} \r",
+                            pkt.len(),
+                            total_bytes
+                        );
+                    }
+                    Err(e) => return Err(e),
                 }
-                Some(_e) => Err(format_err!("couldn't add service")),
-            })
-        });
+            }
+            fx_log_info!(tag: "a2dp-sink", "Media stream finished");
+            Ok(())
+        })
+            .unwrap_or_else(|e| fx_log_info!(tag: "a2dp-sink", "Error in media stream: {:?}", e)),
+    );
+    Ok(())
+}
 
-    let fut = connect_fut.and_then(|_| event_fut.map_err(Into::into));
-    executor.run_singlethreaded(fut).map_err(Into::into)
-    // TODO(jamuraa): hook into media server to provide audio
-    // TODO(jamuraa): accept connections and pass to AVDTP layer
+fn handle_request(r: avdtp::Request) -> Result<(), avdtp::Error> {
+    fx_vlog!(tag: "a2dp-sink", 1, "Handling {:?} from peer..", r);
+    match r {
+        avdtp::Request::Discover { responder } => {
+            let streams = get_available_stream_info()?;
+            responder.send(&streams)
+        }
+        //_ => Ok(()),
+    }
+}
+
+async fn init() -> Result<(), Error> {
+    let profile_svc = fuchsia_app::client::connect_to_service::<ProfileMarker>()
+        .context("Failed to connect to Bluetooth profile service")?;
+
+    let mut service_def = make_profile_service_definition();
+    let (status, service_id) = await!(profile_svc.add_service(
+        &mut service_def,
+        SecurityLevel::EncryptionOptional,
+        false
+    ))?;
+
+    fx_log_info!(tag: "a2dp-sink", "Registered Service ID {}", service_id);
+
+    if let Some(e) = status.error {
+        return Err(format_err!("Couldn't add A2DP sink service: {:?}", e));
+    }
+
+    let mut remotes: HashSet<String> = HashSet::new();
+    let mut evt_stream = profile_svc.take_event_stream();
+    while let Some(evt) = await!(evt_stream.next()) {
+        if evt.is_err() {
+            return Err(evt.err().unwrap().into());
+        }
+        match evt.unwrap() {
+            ProfileEvent::OnConnected {
+                device_id,
+                service_id: _,
+                channel,
+                protocol,
+            } => {
+                fx_log_info!(tag: "a2dp-sink", "Connection from {}: {:?} {:?}!", device_id, channel, protocol);
+                if remotes.contains(&device_id) {
+                    fx_log_info!(tag: "a2dp-sink", "{} already connected: connecting media channel", device_id);
+                    let _ = start_media_stream(channel);
+                    continue;
+                }
+                fx_vlog!(tag: "a2dp-sink", 1, "Adding new peer for {}", device_id);
+                let peer = match avdtp::Peer::new(channel) {
+                    Ok(peer) => peer,
+                    Err(e) => {
+                        fx_log_warn!(tag: "a2dp-sink", "Error adding peer: {:?}", e);
+                        continue;
+                    }
+                };
+                // Start handling the requests
+                let stream = peer.take_request_stream();
+                fuchsia_async::spawn(
+                    async move {
+                        let x = await!(stream.try_for_each(|r| future::ready(handle_request(r))));
+                        fx_log_info!(tag: "a2dp-sink", "Peer Disconnected: {:?}", x);
+                    },
+                );
+                remotes.insert(device_id);
+            }
+            ProfileEvent::OnDisconnected {
+                device_id,
+                service_id,
+            } => {
+                fx_log_info!(tag: "a2dp-sink", "Device {} disconnected from {}", device_id, service_id);
+                remotes.remove(&device_id);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let mut executor = fuchsia_async::Executor::new().context("error creating executor")?;
+
+    executor.run_singlethreaded(init())
 }