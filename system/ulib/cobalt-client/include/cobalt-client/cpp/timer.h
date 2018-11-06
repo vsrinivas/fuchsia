f4e33f397b24cb0adf657afde284556abb290dc8
Gianfranco Valentino <gevalentino@google.com>
2018-11-06 09:43:57 -0800

    [cobalt-client]: Timer update.
    Previously I had the assumption that we would know what we are logging
    a priori, which is not always true. For example, when Operation A
    has multiple branches and different branches map to different event
    codes, we dont know which branch we are gonna pick until it is picked.
    At this point binding the timer to a specific histogram, is less
    useful than simply providing a fs::Ticker equivalent.
    
    For this reason, removed the histogram as part of the timer,
    and we can do something like:
    Timer timer(is_collecting);
    
    .....
        hitogram.Add(timer.End());
    TESTS= cobalt-client-test
    
    Change-Id: I93731f983112b0a061130c8f6f7bab9d7f98cffc

--- system/ulib/cobalt-client/include/cobalt-client/cpp/timer.h
+++ system/ulib/cobalt-client/include/cobalt-client/cpp/timer.h
@@ -10,54 +10,42 @@
 #include <zircon/assert.h>
 
 namespace cobalt_client {
+namespace internal {
 
-// RAII Timer.
-//
-// This class is moveable, but not copyable or assignable.
-//
-// void InterestingFunction() {
-//    cobalt_client::Timer timer(my_interesting_histogram_, is_collecting_);
-//    ...;
-// }
-class Timer {
+template <typename Clock> class TimerBase {
 public:
-    // Returns the amount of nanoseconds that a |zx::ticks| represent. It is used as the
-    // default for |ticks_to_units_| in a |Timer|.
-    static int64_t TicksToNs(zx::ticks ticks) { return fzl::TicksToNs(ticks).to_nsecs(); }
+    explicit TimerBase(bool is_collecting) : start_(is_collecting ? Clock::now().get() : 0) {}
+    TimerBase(const TimerBase&) = delete;
+    TimerBase(TimerBase&& other) : start_(other.start_) { other.start_ = zx::ticks(0); }
+    TimerBase& operator=(const TimerBase&) = delete;
+    TimerBase& operator=(TimerBase&& other) = delete;
+    ~TimerBase() = default;
 
-    Timer(Histogram metric, bool is_collecting, int64_t (*ticks_to_unit)(zx::ticks) = TicksToNs)
-        : metric_(metric), ticks_to_unit_(ticks_to_unit),
-          start_(is_collecting ? zx::ticks::now().get() : 0) {
-        ZX_DEBUG_ASSERT(ticks_to_unit_ != nullptr);
-    }
-
-    Timer(const Timer&) = delete;
-    Timer(Timer&& other)
-        : metric_(other.metric_), ticks_to_unit_(other.ticks_to_unit_), start_(other.start_) {
-        other.Cancel();
-    }
-
-    Timer& operator=(const Timer&) = delete;
-    Timer& operator=(Timer&& other) = delete;
-    ~Timer() { End(); }
-
-    // Stops the timer and logs the duration.
-    void End() {
-        if (start_.get() > 0) {
-            int64_t delta = ticks_to_unit_(zx::ticks::now() - start_);
-            ZX_DEBUG_ASSERT(delta >= 0);
-            metric_.Add(static_cast<uint64_t>(delta));
-            start_ = zx::ticks(0);
+    // Returns the duration since creation. If |is_collecting| is false, will return
+    // 0.
+    zx::duration End() {
+        if (start_.get() == 0) {
+            return zx::duration(0);
         }
+        return fzl::TicksToNs(Clock::now() - start_);
     }
 
-    // Prevents the timer from logging any duration.
-    void Cancel() { start_ = zx::ticks(0); }
+    // Reserts the timer. If |is_collecting| is false, has no effect.
+    void Reset() {
+        if (start_.get() == 0) {
+            return;
+        }
+        start_ = Clock::now();
+    }
 
 private:
-    Histogram metric_;
-    int64_t (*ticks_to_unit_)(zx::ticks);
     zx::ticks start_;
 };
+} // namespace internal
+
+// Utility class for measuring the amount of ticks in an interval.
+//
+// This class is moveable, but not copyable or assignable.
+using Timer = internal::TimerBase<zx::ticks>;
 
 } // namespace cobalt_client
