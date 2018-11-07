27032e547dda938af0f61b55704f728132c0a889
Greg Bonik <gbonik@google.com>
2018-11-06 16:17:33 -0800

    [wlan] Add utilities for comparing ranges in tests
    Useful for comparing spans etc.
    
    TEST=CQ
    
    Change-Id: If33153a99abf9ef078bcbb33eed853cb5028dc6b

---
+++ lib/wlan/mlme/tests/test_utils.h
@@ -0,0 +1,50 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GARNET_LIB_WLAN_MLME_TESTS_TEST_UTILS_H_
+#define GARNET_LIB_WLAN_MLME_TESTS_TEST_UTILS_H_
+
+#include <gtest/gtest.h>
+#include <algorithm>
+
+namespace wlan {
+namespace test_utils {
+
+template <typename T> struct IteratorTypes {
+    typedef typename T::iterator iterator;
+    typedef typename T::const_iterator const_iterator;
+};
+
+template <typename T, size_t N> struct IteratorTypes<T[N]> {
+    typedef T* iterator;
+    typedef const T* const_iterator;
+};
+
+template <typename T> struct RangeWrapper {
+    explicit RangeWrapper(const T& range) : range(range) {}
+
+    typedef typename IteratorTypes<T>::iterator iterator;
+    typedef typename IteratorTypes<T>::const_iterator const_iterator;
+
+    const T& range;
+
+    const_iterator begin() const { return std::begin(range); }
+
+    const_iterator end() const { return std::end(range); }
+
+    template <typename U> bool operator==(const RangeWrapper<U>& other) const {
+        return std::equal(begin(), end(), other.begin(), other.end());
+    }
+};
+
+}  // namespace test_utils
+}  // namespace wlan
+
+#define EXPECT_RANGES_EQ(a, b) \
+    EXPECT_EQ(test_utils::RangeWrapper((a)), test_utils::RangeWrapper((b)))
+
+#define ASSERT_RANGES_EQ(a, b) \
+    ASSERT_EQ(test_utils::RangeWrapper((a)), test_utils::RangeWrapper((b)))
+
+#endif  // GARNET_LIB_WLAN_MLME_TESTS_TEST_UTILS_H_
