ada1ec31cc4c1c7dbfe8a1fc626b6a5281dd1f84
Benjamin Brittain <bwb@google.com>
2018-11-08 10:28:28 -0800

    [tel][lib] Add support for ioctl
    Test: None
    
    Change-Id: I9e332125c5c46d0b374f966b56263060d6254f8e

--- lib/rust/qmi/src/lib.rs
+++ lib/rust/qmi/src/lib.rs
@@ -35,3 +35,31 @@ const IOCTL_QMI_GET_CHANNEL: raw::c_int = make_ioctl!(
     fdio_sys::IOCTL_FAMILY_QMI,
     0
 );
+
+pub fn set_network_status(device: &File, state: bool) -> Result<(), zx::Status> {
+    let mut handle: zx::sys::zx_handle_t = zx::sys::ZX_HANDLE_INVALID;
+
+    // This call is safe because the callee does not retain any data from the call, and the return
+    // value ensures that the handle is a valid handle to a zx::channel.
+    unsafe {
+        match ioctl_raw(
+            device.as_raw_fd(),
+            IOCTL_QMI_SET_NETWORK,
+            &state as *const _ as *const raw::c_void,
+            ::std::mem::size_of::<bool>(),
+            ::std::ptr::null_mut(),
+            0,
+        ) as i32
+        {
+            e if e < 0 => Err(zx::Status::from_raw(e)),
+            e => Ok(e),
+        }?;
+        Ok(())
+    }
+}
+
+const IOCTL_QMI_SET_NETWORK: raw::c_int = make_ioctl!(
+    fdio_sys::IOCTL_KIND_DEFAULT,
+    fdio_sys::IOCTL_FAMILY_QMI,
+    1
+);
