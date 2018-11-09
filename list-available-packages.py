e25be4cc0209a53bc4690e6762e1122670a4d9ae
James Tucker <raggi@google.com>
2018-11-09 14:28:42 -0800

    [build-push] only enable installing packages
    This change filters out build packages that are deprecated_system_image=true.
    
    Change-Id: I944df8e16253ff3207f4aa78485300c1264cc413

--- list-available-packages.py
+++ list-available-packages.py
@@ -9,14 +9,22 @@ import sys
 import json
 
 def main():
-    parser = argparse.ArgumentParser(description=("List all targets in the available set"))
+    parser = argparse.ArgumentParser(description=("List all targets in the pushable available set"))
     parser.add_argument('--build-dir', action='store', required=True)
 
     args = parser.parse_args()
     with open(os.path.join(args.build_dir, "packages.json")) as f:
       data = json.load(f)
 
-    for tgt in list(data["available"]):
+    available_build_packages = set(data["available"])
+    with open(os.path.join(args.build_dir, "amber-files", "repository", "targets.json")) as f:
+      data = json.load(f)
+
+    published_packages = set([s.split('/')[1] for s in data['signed']['targets'].keys()])
+
+    available_packages = published_packages & available_build_packages
+
+    for tgt in available_packages:
       print(tgt)
 
 if __name__ == '__main__':
