7b381b297f6bfac18d5396f7b32e339a1012753b
Tim Detwiler <tjdetwiler@google.com>
2018-11-13 10:49:18 +1100

    [vmm][virtio_block] Fix large writes.
    We were incorrectly calculating the file offest for file writes that
    that required multiple FIDL file writes. Fix the issue and back-fill
    unit test coverage.
    
    Test: Updated unit test fails without block dispatcher patch.
          biscotti can remount a btrfs filesystem across reboots now.
    Change-Id: I80ece2bb22dd41ec49c1e20b5a7b967b0231ff3a

--- bin/guest/vmm/device/virtio_block_test.cc
+++ bin/guest/vmm/device/virtio_block_test.cc
@@ -2,8 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <fbl/unique_fd.h>
-#include <lib/fzl/fdio.h>
+#include <lib/fdio/util.h>
 #include <virtio/block.h>
 
 #include "garnet/bin/guest/vmm/device/test_with_device.h"
@@ -13,15 +16,17 @@
 static constexpr char kVirtioBlockUrl[] = "virtio_block";
 static constexpr uint16_t kNumQueues = 1;
 static constexpr uint16_t kQueueSize = 16;
+static constexpr size_t kQueueDataSize = 10 * fuchsia::io::MAX_BUF;
 
 static constexpr char kVirtioBlockId[] = "block-id";
 static constexpr size_t kNumSectors = 2;
 static constexpr uint8_t kSectorBytes[kNumSectors] = {0xab, 0xcd};
 
+
 class VirtioBlockTest : public TestWithDevice {
  protected:
   VirtioBlockTest()
-      : request_queue_(phys_mem_, PAGE_SIZE * kNumQueues, kQueueSize) {}
+      : request_queue_(phys_mem_, kQueueDataSize * kNumQueues, kQueueSize) {}
 
   void SetUp() override {
     // Launch device process.
@@ -31,12 +36,19 @@ class VirtioBlockTest : public TestWithDevice {
     ASSERT_EQ(ZX_OK, status);
 
     // Setup block file.
+    // Open the file twice; once to get a FilePtr to provide to the
+    // virtio_block process and another to retain to verify the file
+    // contents.
     char path_template[] = "/tmp/block.XXXXXX";
-    fbl::unique_fd fd = CreateBlockFile(path_template);
-    ASSERT_TRUE(fd);
-    fzl::FdioCaller fdio(std::move(fd));
+    fd_ = CreateBlockFile(path_template);
+    ASSERT_TRUE(fd_);
+    zx_handle_t handle;
+    status = fdio_get_service_handle(fd_.release(), &handle);
+    FXL_CHECK(status == ZX_OK) << "Failed to get block file handle";
     fuchsia::io::FilePtr file;
-    file.Bind(zx::channel(fdio.borrow_channel()));
+    file.Bind(zx::channel(zx::channel(handle)));
+    fd_ = fbl::unique_fd(open(path_template, O_RDWR));
+    ASSERT_TRUE(fd_);
 
     // Start device execution.
     services.ConnectToService(block_.NewRequest());
@@ -52,13 +64,14 @@ class VirtioBlockTest : public TestWithDevice {
     VirtioQueueFake* queues[kNumQueues] = {&request_queue_};
     for (size_t i = 0; i < kNumQueues; i++) {
       auto q = queues[i];
-      q->Configure(PAGE_SIZE * i, PAGE_SIZE);
+      q->Configure(kQueueDataSize * i, kQueueDataSize);
       status = block_->ConfigureQueue(i, q->size(), q->desc(), q->avail(),
                                       q->used());
       ASSERT_EQ(ZX_OK, status);
     }
   }
 
+  fbl::unique_fd fd_;
   fuchsia::guest::device::VirtioBlockSyncPtr block_;
   VirtioQueueFake request_queue_;
 
@@ -263,15 +276,22 @@ TEST_F(VirtioBlockTest, Write) {
 TEST_F(VirtioBlockTest, WriteMultipleDescriptors) {
   virtio_blk_req_t header = {
       .type = VIRTIO_BLK_T_OUT,
+      .sector = 0,
   };
-  std::vector<uint8_t> sector_1(machina::kBlockSectorSize, UINT8_MAX);
-  std::vector<uint8_t> sector_2(machina::kBlockSectorSize, UINT8_MAX);
+
+  // Ensure we're writing enough to overflow a single file write transaction.
+  static constexpr size_t kTestBlockSize = 2 * fuchsia::io::MAX_BUF;
+  static_assert(kTestBlockSize % machina::kBlockSectorSize == 0);
+  static_assert((2 * kTestBlockSize) + sizeof(header) + 1 <= kQueueDataSize);
+
+  std::vector<uint8_t> block_1(kTestBlockSize, 0xff);
+  std::vector<uint8_t> block_2(kTestBlockSize, 0xab);
   uint8_t* blk_status;
   zx_status_t status =
       DescriptorChainBuilder(request_queue_)
           .AppendReadableDescriptor(&header, sizeof(header))
-          .AppendReadableDescriptor(sector_1.data(), sector_1.size())
-          .AppendReadableDescriptor(sector_2.data(), sector_2.size())
+          .AppendReadableDescriptor(block_1.data(), block_1.size())
+          .AppendReadableDescriptor(block_2.data(), block_2.size())
           .AppendWritableDescriptor(&blk_status, sizeof(*blk_status))
           .Build();
   ASSERT_EQ(ZX_OK, status);
@@ -282,6 +302,14 @@ TEST_F(VirtioBlockTest, WriteMultipleDescriptors) {
   ASSERT_EQ(ZX_OK, status);
 
   EXPECT_EQ(VIRTIO_BLK_S_OK, *blk_status);
+
+  std::vector<uint8_t> result(2 * kTestBlockSize, 0);
+  ASSERT_EQ(pread(fd_.get(), result.data(), result.size(), 0),
+            static_cast<ssize_t>(result.size()));
+  ASSERT_EQ(memcmp(result.data(), block_1.data(), block_1.size()), 0);
+  ASSERT_EQ(
+      memcmp(result.data() + block_1.size(), block_2.data(), block_2.size()),
+      0);
 }
 
 TEST_F(VirtioBlockTest, Sync) {
