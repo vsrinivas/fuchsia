af1e98fc05054f3755356a664b5ea786e261892e
Craig Tiller <ctiller@google.com>
2018-11-12 14:34:52 -0800

    [overnet] Re-enable routable message fuzzer
    Test: ran fuzzer
    
    Change-Id: Ia1ac5c9c1c70111e31f7bf9e02354243751bcc96

--- lib/overnet/protocol/routable_message_fuzzer.cc
+++ lib/overnet/protocol/routable_message_fuzzer.cc
@@ -2,29 +2,24 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "routing_header.h"
+#include "routable_message.h"
 
 using namespace overnet;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   // Parse data as a header.
-  auto status = RoutingHeader::Parse(&data, data + size, NodeId(1), NodeId(2));
+  auto status = RoutableMessage::Parse(Slice::FromCopiedBuffer(data, size),
+                                       NodeId(1), NodeId(2));
   if (status.is_ok()) {
     // If parsed ok: rewrite, between different endpoints.
-    RoutingHeader::Writer writer(status.get(), NodeId(3), NodeId(4));
-    std::vector<uint8_t> encoded;
-    encoded.resize(writer.wire_length());
-    uint8_t* new_data = encoded.data();
-    auto end = writer.Write(new_data);
-    assert(end - new_data == writer.wire_length());
+    Slice written =
+        status->message.Write(NodeId(3), NodeId(4), status->payload);
     // And re-parse.
-    auto status2 = RoutingHeader::Parse(const_cast<const uint8_t**>(&new_data),
-                                        new_data + writer.wire_length(),
-                                        NodeId(4), NodeId(3));
-    assert(new_data == encoded.data() + encoded.size());
+    auto status2 = RoutableMessage::Parse(written, NodeId(4), NodeId(3));
     // Should parse ok, and get the same result.
     assert(status2.is_ok());
-    assert(*status == *status2);
+    assert(status->message == status2->message);
+    assert(status->payload == status2->payload);
   }
   return 0;  // Non-zero return values are reserved for future use.
 }
