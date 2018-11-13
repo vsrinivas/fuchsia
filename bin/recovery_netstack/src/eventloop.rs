dfbaffeba4513f74e8bbfbb24e75e5c59dbaec72
Wesley Aptekar-Cassels <wesleyac@google.com>
2018-11-13 14:11:53 -0500

    [rust-netstack] Add timer implementation
    Change-Id: I4707e0c00b3e437b6d153c681b2cdeacadcbc74d
    Test: None

--- bin/recovery_netstack/src/eventloop.rs
+++ bin/recovery_netstack/src/eventloop.rs
@@ -5,21 +5,25 @@
 //! The special-purpose event loop used by the recovery netstack.
 
 use ethernet as eth;
+use fuchsia_async as fasync;
 use fuchsia_zircon as zx;
 
 use std::fs::File;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
 
+use futures::future::{AbortHandle, Abortable};
 use futures::prelude::*;
 
 use netstack_core::{
-    add_device_route, receive_frame, set_ip_addr, Context, DeviceId, DeviceLayerEventDispatcher,
-    EventDispatcher, Ipv4Addr, Mac, StackState, Subnet, TimerId, TransportLayerEventDispatcher,
-    UdpEventDispatcher,
+    add_device_route, handle_timeout, receive_frame, set_ip_addr, Context, DeviceId,
+    DeviceLayerEventDispatcher, EventDispatcher, Ipv4Addr, Mac, StackState, Subnet, TimerId,
+    TransportLayerEventDispatcher, UdpEventDispatcher,
 };
 
 /// The event loop.
 pub struct EventLoop {
-    ctx: Context<EventLoopInner>,
+    ctx: Arc<Mutex<Context<EventLoopInner>>>,
 }
 
 pub const DEFAULT_ETH: &str = "/dev/class/ethernet/000";
@@ -51,33 +55,47 @@ impl EventLoop {
         let mut state = StackState::default();
         let eth_id = state.add_ethernet_device(Mac::new(mac.octets));
         let mut event_loop = EventLoop {
-            ctx: Context::new(
+            ctx: Arc::new(Mutex::new(Context::new(
                 state,
                 EventLoopInner {
                     device_id: eth_id,
                     eth_client,
+                    event_loop: None,
+                    timers: vec![],
                 },
-            ),
+            ))),
         };
+        event_loop.ctx.lock().unwrap().dispatcher().event_loop = Some(Arc::clone(&event_loop.ctx));
 
-        await!(event_loop.ctx.dispatcher().eth_client.start())?;
-        // Hardcoded subnet: if you update the IPADDR above to use a network that's not /24, update
-        // this as well.
-        let fixed_subnet = Subnet::new(Ipv4Addr::new([192, 168, 1, 0]), 24);
-        set_ip_addr(&mut event_loop.ctx, eth_id, FIXED_IPADDR, fixed_subnet);
-        add_device_route(&mut event_loop.ctx, fixed_subnet, eth_id);
+        {
+            let mut ctx = event_loop.ctx.lock().unwrap();
+
+            await!(ctx.dispatcher().eth_client.start())?;
+            // Hardcoded subnet: if you update the IPADDR above to use a network that's not /24, update
+            // this as well.
+            let fixed_subnet = Subnet::new(Ipv4Addr::new([192, 168, 1, 0]), 24);
+            set_ip_addr(&mut ctx, eth_id, FIXED_IPADDR, fixed_subnet);
+            add_device_route(&mut ctx, fixed_subnet, eth_id);
+        }
 
         let mut buf = [0; 2048];
-        let mut events = event_loop.ctx.dispatcher().eth_client.get_stream();
+        let mut events = event_loop
+            .ctx
+            .lock()
+            .unwrap()
+            .dispatcher()
+            .eth_client
+            .get_stream();
         while let Some(evt) = await!(events.try_next())? {
             match evt {
                 eth::Event::StatusChanged => {
-                    let status = await!(event_loop.ctx.dispatcher().eth_client.get_status())?;
+                    let mut ctx = event_loop.ctx.lock().unwrap();
+                    let status = await!(ctx.dispatcher().eth_client.get_status())?;
                     println!("ethernet status: {:?}", status);
                 }
                 eth::Event::Receive(rx) => {
                     let len = rx.read(&mut buf);
-                    receive_frame(&mut event_loop.ctx, eth_id, &mut buf[..len]);
+                    receive_frame(&mut event_loop.ctx.lock().unwrap(), eth_id, &mut buf[..len]);
                 }
             }
         }
@@ -85,26 +103,74 @@ impl EventLoop {
     }
 }
 
+struct TimerInfo {
+    time: Instant,
+    id: TimerId,
+    abort_handle: AbortHandle,
+}
+
 struct EventLoopInner {
     device_id: DeviceId,
     eth_client: eth::Client,
+    // This creates an Arc cycle.
+    // This is difficult to avoid, since the event loop needs to be able to pass the timeout
+    // handlers a mutable reference to the event loop itself, so that they can send packets/etc.
+    // The Arc cycle shouldn't be a problem, however, since we expect the event loop to live for
+    // as long as the netstack is alive.
+    // The Option is just to allow a concrete instance of this type to be created - the creator is
+    // expected to uphold the invariant that event_loop is not None by the time the EventLoopInner
+    // is used.
+    event_loop: Option<Arc<Mutex<Context<EventLoopInner>>>>,
+    timers: Vec<TimerInfo>,
 }
 
 impl EventDispatcher for EventLoopInner {
-    fn schedule_timeout(
-        &mut self, _duration: std::time::Duration, _id: TimerId,
-    ) -> Option<std::time::Instant> {
+    fn schedule_timeout(&mut self, duration: Duration, id: TimerId) -> Option<Instant> {
+        // We need to separately keep track of the time at which the future completes (a Zircon
+        // Time object) and the time at which the user expects it to complete. You cannot convert
+        // between Zircon Time objects and std::time::Instant objects (since std::time::Instance is
+        // opaque), so we generate two different time objects to keep track of.
+        let zircon_time = fuchsia_zircon::Time::after(fuchsia_zircon::Duration::from(duration));
+        let rust_time = Instant::now() + duration;
+
+        let old_timer = self.cancel_timeout(id);
+
+        let event_loop = Arc::clone(self.event_loop.as_ref().unwrap());
+        let timeout = async move {
+            await!(fasync::Timer::new(zircon_time));
+            handle_timeout(&mut event_loop.lock().unwrap(), id);
+            event_loop.lock().unwrap().dispatcher().cancel_timeout(id);
+        };
+
+        let (abort_handle, abort_registration) = AbortHandle::new_pair();
+        self.timers.push(TimerInfo {
+            time: rust_time,
+            id,
+            abort_handle,
+        });
+
+        let timeout = Abortable::new(timeout, abort_registration);
+        let timeout = timeout.unwrap_or_else(|_| ());
+
+        fasync::spawn_local(timeout);
+        old_timer
+    }
+
+    fn schedule_timeout_instant(&mut self, _time: Instant, _id: TimerId) -> Option<Instant> {
+        // It's not possible to convert a std::time::Instant to a Zircon Time, so this API will
+        // need some more thought. Punting on this until we need it.
         unimplemented!()
     }
 
-    fn schedule_timeout_instant(
-        &mut self, _time: std::time::Instant, _id: TimerId,
-    ) -> Option<std::time::Instant> {
-        unimplemented!()
-    }
-
-    fn cancel_timeout(&mut self, id: TimerId) -> Option<std::time::Instant> {
-        unimplemented!()
+    fn cancel_timeout(&mut self, id: TimerId) -> Option<Instant> {
+        let index = self.timers.iter().enumerate().find_map(|x| {
+            if x.1.id == id {
+                Some(x.0)
+            } else {
+                None
+            }
+        })?;
+        Some(self.timers.remove(index).time)
     }
 }
 
