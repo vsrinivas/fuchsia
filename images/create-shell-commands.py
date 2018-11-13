55e31ef146bb763195f975375b9e675ad1256fd8
James Tucker <raggi@google.com>
2018-11-13 12:22:27 -0800

    [shell-commands] an aggregate package for tools
    The shell-commands package provides an aggregate bin directory of trampolines
    that can be added to a shell $PATH in order to execute command line tools
    from all built available packages.
    
    Bug: PKG-54 #comment shell-commands package
    Test: modified the rolldice package to contain a shell=true command and ran it from /pkgfs/packages/shell-commands/0/bin/rolldice.
    Change-Id: Id691179a6f2b90a8f6e6d101c03f3924c88d6031

---
+++ images/create-shell-commands.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import sys
+import os
+import shlex
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Create trampolines and a manifest for a set of shell commands',
+        fromfile_prefix_chars='@')
+    parser.convert_arg_line_to_args = shlex.split
+    parser.add_argument('--trampoline-dir', required=True,
+                        help='Directory in which to create trampolines')
+    parser.add_argument('--output-manifest', required=True,
+                        help='Output manifest path')
+    parser.add_argument('--uri', action='append', default=[],
+                        help='A command URI to create an entry for')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.trampoline_dir):
+        os.makedirs(args.trampoline_dir)
+
+    commands = dict()
+
+    for uri in args.uri:
+        name = uri.split('#')[-1]
+        name = os.path.split(name)[-1]
+        if name in commands:
+            sys.stderr.write('Duplicate shell command name: %s\n' % name)
+            return 1
+        path = os.path.join(args.trampoline_dir, name)
+        with open(path, 'w') as f:
+            f.write('#!resolve %s' % uri)
+        commands[name] = path
+
+    with open(args.output_manifest, 'w') as output:
+        for name, path in commands.items():
+            output.write('bin/%s=%s\n' % (name, path))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
