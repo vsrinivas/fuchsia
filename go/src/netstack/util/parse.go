879d354a24bcb94a27df38c6bc135dd08920dc3c
Tamir Duberstein <tamird@gmail.com>
2018-11-20 19:29:32 -0500

    [netstack] implement util using stdlib's net
    Now that we're importing `net`, we may as well go all the way.
    
    Tested: fx run-host-tests netstack_util_test
    Change-Id: I0dd823d6de0e09661f1490d5bbb03206dc1526c3

--- go/src/netstack/util/parse.go
+++ go/src/netstack/util/parse.go
@@ -8,75 +8,37 @@ package util
 import (
 	"net"
 
-	"errors"
-	"strconv"
-	"strings"
-
 	"github.com/google/netstack/tcpip"
 )
 
 func ApplyMask(addr tcpip.Address, mask tcpip.AddressMask) tcpip.Address {
-	if len(addr) != len(mask) {
-		return ""
-	}
-	subnet := []byte(addr)
-	for i := 0; i < len(subnet); i++ {
-		subnet[i] &= mask[i]
-	}
-	return tcpip.Address(subnet)
+	return tcpip.Address(net.IP(addr).Mask(net.IPMask(mask)))
 }
 
-// Copied from pkg net to avoid taking a dependency.
 func CIDRMask(ones, bits int) tcpip.AddressMask {
-	// header.IPv4AddressSize, header.IPv6AddressSize
-	if bits != 8*4 && bits != 8*16 {
-		return tcpip.AddressMask("")
-	}
-	if ones < 0 || ones > bits {
-		return tcpip.AddressMask("")
-	}
-	l := bits / 8
-	m := make([]byte, l)
-	n := uint(ones)
-	for i := 0; i < l; i++ {
-		if n >= 8 {
-			m[i] = 0xff
-			n -= 8
-			continue
-		}
-		m[i] = ^byte(0xff >> n)
-		n = 0
-	}
-	return tcpip.AddressMask(m)
+	return tcpip.AddressMask(net.CIDRMask(ones, bits))
 }
 
-var errInvalidCIDRNotation = errors.New("CIDR notation invalid")
-
-func ParseCIDR(subnet string) (tcpip.Address, tcpip.Subnet, error) {
-	split := strings.Split(subnet, "/")
-	if len(split) != 2 {
-		return tcpip.Address(""), tcpip.Subnet{}, errInvalidCIDRNotation
+func ipToAddress(ip net.IP) tcpip.Address {
+	if v4 := ip.To4(); v4 != nil {
+		return tcpip.Address(v4)
 	}
-	addr := Parse(split[0])
-	ones, err := strconv.ParseInt(split[1], 10, 8)
+	return tcpip.Address(ip)
+}
 
+func ParseCIDR(s string) (tcpip.Address, tcpip.Subnet, error) {
+	ip, subnet, err := net.ParseCIDR(s)
 	if err != nil {
-		return tcpip.Address(""), tcpip.Subnet{}, err
+		return "", tcpip.Subnet{}, err
 	}
-
-	mask := CIDRMask(int(ones), 8*len(addr))
-	sn, err := tcpip.NewSubnet(ApplyMask(addr, mask), mask)
-	return addr, sn, err
+	sn, err := tcpip.NewSubnet(tcpip.Address(subnet.IP), tcpip.AddressMask(subnet.Mask))
+	if err != nil {
+		return "", tcpip.Subnet{}, err
+	}
+	return ipToAddress(ip), sn, nil
 }
 
 // Parse parses the string representation of an IPv4 or IPv6 address.
 func Parse(src string) tcpip.Address {
-	ip := net.ParseIP(src)
-	if ip == nil {
-		return ""
-	}
-	if ip4 := ip.To4(); ip4 != nil {
-		return tcpip.Address(ip4)
-	}
-	return tcpip.Address(ip)
+	return ipToAddress(net.ParseIP(src))
 }
