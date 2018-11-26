a6d74740fb166877bb99e691313aeda5b83fd5b1
Pascal Perez <pascallouis@google.com>
2018-11-26 21:17:31 +0000

    [fidl][cpp] Revert "Table support: tests, and fixes"
    This reverts commit 38d0fba04d8e55f2c98a8cf22bb2f20a5cc55af7.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [fidl][cpp] Table support: tests, and fixes
    > 
    > Porting the table encoding tests from Go support, and addressing various
    > encoding issues:
    > - incorrect order of num bytes and num handles
    > - presence flags in envelopes
    > - empty table support and canonical representation
    > 
    > Test: fx build-push fidl_tests && fx shell runtests -t fidl_cpp_unittests
    > Change-Id: I288203dda7004441bef55bfd0c43d4a8d6956d66
    
    TBR=thatguy@google.com,ctiller@google.com,pascallouis@google.com
    
    Change-Id: I819ae2c217758f3139e06bfbb8e46b0b4228855d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- public/lib/fidl/cpp/roundtrip_test.cc
+++ public/lib/fidl/cpp/roundtrip_test.cc
@@ -4,7 +4,6 @@
 
 // Check that we can decode things that we can encode.
 
-#include <iostream>
 #include <fidl/test/misc/cpp/fidl.h>
 #include <lib/fidl/internal.h>
 #include "gtest/gtest.h"
@@ -49,77 +48,6 @@ Output RoundTrip(const Input& input) {
 TEST(SimpleStruct, SerializeAndDeserialize) {
   Int64Struct input{1};
   EXPECT_EQ(input, RoundTrip<Int64Struct>(input));
-}
-
-bool cmp_payload(const uint8_t* actual, size_t actual_size,
-                 const uint8_t* expected, size_t expected_size) {
-  bool pass = true;
-  for (size_t i = 0; i < actual_size && i < expected_size; i++) {
-    if (actual[i] != expected[i]) {
-      pass = false;
-      std::cout << "element[" << i << "]: " <<
-        "actual=" << +actual[i] << " " <<
-        "expected=" << +expected[i] <<
-        "\n";
-    }
-  }
-  if (actual_size != expected_size) {
-    pass = false;
-    std::cout << "element[...]: " <<
-      "actual.size=" << +actual_size << " " <<
-      "expected.size=" << +expected_size <<
-      "\n";
-  }
-  return pass;
-}
-
-template <class Input>
-bool ValueToBytes(const Input& input, const std::vector<uint8_t>& expected) {
-  fidl::Encoder enc(0xfefefefe);
-  auto offset = enc.Alloc(CodingTraits<SimpleTable>::encoded_size);
-  fidl::Clone(input).Encode(&enc, offset);
-  auto msg = enc.GetMessage();
-  auto payload = msg.payload();
-  return cmp_payload(
-    reinterpret_cast<const uint8_t*>(payload.data()), payload.actual(),
-    reinterpret_cast<const uint8_t*>(expected.data()), expected.size()
-  );
-}
-
-TEST(SimpleTable, CheckEmptyTable) {
-  SimpleTable input;
-
-  auto expected = std::vector<uint8_t>{
-    0, 0, 0, 0, 0, 0, 0, 0, // max ordinal
-    255, 255, 255, 255, 255, 255, 255, 255, // alloc present
-  };
-
-  EXPECT_TRUE(ValueToBytes(input, expected));
-}
-
-TEST(SimpleTable, CheckBytesWithXY) {
-  SimpleTable input;
-  input.set_x(42);
-  input.set_y(67);
-
-  auto expected = std::vector<uint8_t>{
-    5, 0, 0, 0, 0, 0, 0, 0, // max ordinal
-    255, 255, 255, 255, 255, 255, 255, 255, // alloc present
-    8, 0, 0, 0, 0, 0, 0, 0, // envelope 1: num bytes / num handles
-    255, 255, 255, 255, 255, 255, 255, 255, // alloc present
-    0, 0, 0, 0, 0, 0, 0, 0, // envelope 2: num bytes / num handles
-    0, 0, 0, 0, 0, 0, 0, 0, // no alloc
-    0, 0, 0, 0, 0, 0, 0, 0, // envelope 3: num bytes / num handles
-    0, 0, 0, 0, 0, 0, 0, 0, // no alloc
-    0, 0, 0, 0, 0, 0, 0, 0, // envelope 4: num bytes / num handles
-    0, 0, 0, 0, 0, 0, 0, 0, // no alloc
-    8, 0, 0, 0, 0, 0, 0, 0, // envelope 5: num bytes / num handles
-    255, 255, 255, 255, 255, 255, 255, 255, // alloc present
-    42, 0, 0, 0, 0, 0, 0, 0, // field X
-    67, 0, 0, 0, 0, 0, 0, 0, // field Y
-  };
-
-  EXPECT_TRUE(ValueToBytes(input, expected));
 }
 
 TEST(SimpleTable, SerializeAndDeserialize) {
