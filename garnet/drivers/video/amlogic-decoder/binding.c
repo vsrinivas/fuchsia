1c1e9f2947ba14cca1c583679f62076ea633554f
Dustin Green <dustingreen@google.com>
2018-11-28 12:34:34 -0800

    [amlogic-video] fix binding program indenting
    Tested: indent change only
    Change-Id: I761c295d0619f495471f7cbd057d26275be43ad3

--- garnet/drivers/video/amlogic-decoder/binding.c
+++ garnet/drivers/video/amlogic-decoder/binding.c
@@ -21,8 +21,10 @@ static zx_driver_ops_t amlogic_video_driver_ops = {
     // process
 };
 
+// clang-format off
 ZIRCON_DRIVER_BEGIN(amlogic_video, amlogic_video_driver_ops, "zircon", "0.1", 4)
-BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PDEV),
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PDEV),
     BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_AMLOGIC),
     BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, PDEV_DID_AMLOGIC_VIDEO),
-    ZIRCON_DRIVER_END(amlogic_video)
+ZIRCON_DRIVER_END(amlogic_video)
+// clang-format on
