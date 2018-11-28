9830a3a5db1f4dc8203839975abeb4741bdbb65e
Ross Wang <rosswang@google.com>
2018-11-28 12:17:38 -0800

    [callback] Simplify ScopedTaskRunner tests
    This is in preparation for adding more tests for a thread-safe
    ScopedTaskRunner API. This leverages async::TestLoop instead of a stub
    dispatcher implementation.
    
    Test: fx shell runtests -t garnet_lib_unittests
    Change-Id: I3bed45374755883e3e114a5a39a6962b9d495e9e

--- public/lib/callback/scoped_task_runner_unittest.cc
+++ public/lib/callback/scoped_task_runner_unittest.cc
@@ -4,64 +4,41 @@
 
 #include "lib/callback/scoped_task_runner.h"
 
-#include <lib/async-testutils/dispatcher_stub.h>
-#include <lib/async/task.h>
+#include <lib/async-testutils/test_loop.h>
 
 #include "gtest/gtest.h"
 
 namespace callback {
 namespace {
 
-inline void InvokeTaskHandler(async_dispatcher_t* dispatcher,
-                              async_task_t* task) {
-  task->handler(dispatcher, task, ZX_OK);
-}
-
-class FakeDispatcher : public async::DispatcherStub {
- public:
-  zx_status_t PostTask(async_task_t* task) override {
-    tasks.push_back(task);
-    return ZX_OK;
-  }
-  std::vector<async_task_t*> tasks;
-};
-
 TEST(ScopedTaskRunner, DelegateToDispatcher) {
-  FakeDispatcher dispatcher;
+  async::TestLoop loop;
 
   uint8_t called = 0;
   auto increment_call = [&called] { ++called; };
-  ScopedTaskRunner task_runner(&dispatcher);
+  ScopedTaskRunner task_runner(loop.dispatcher());
   task_runner.PostTask(increment_call);
   task_runner.PostDelayedTask(increment_call, zx::sec(0));
   task_runner.PostTaskForTime(increment_call, zx::time(0));
 
-  EXPECT_EQ(3u, dispatcher.tasks.size());
-  for (const auto& task : dispatcher.tasks) {
-    InvokeTaskHandler(&dispatcher, task);
-  }
-
+  loop.RunUntilIdle();
   EXPECT_EQ(3u, called);
 }
 
 TEST(ScopedTaskRunner, CancelOnDeletion) {
-  FakeDispatcher dispatcher;
+  async::TestLoop loop;
 
   uint8_t called = 0;
   auto increment_call = [&called] { ++called; };
 
   {
-    ScopedTaskRunner task_runner(&dispatcher);
+    ScopedTaskRunner task_runner(loop.dispatcher());
     task_runner.PostTask(increment_call);
     task_runner.PostDelayedTask(increment_call, zx::sec(0));
     task_runner.PostTaskForTime(increment_call, zx::time(0));
   }
 
-  EXPECT_EQ(3u, dispatcher.tasks.size());
-  for (const auto& task : dispatcher.tasks) {
-    InvokeTaskHandler(&dispatcher, task);
-  }
-
+  loop.RunUntilIdle();
   EXPECT_EQ(0u, called);
 }
 
