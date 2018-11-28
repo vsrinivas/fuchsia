3a8dab9a939efeeb5e18454595a558f9972f7a42
Ross Wang <rosswang@google.com>
2018-11-28 10:59:40 -0800

    [callback] Make ScopedTaskRunner thread-safe
    This augments ScopedTaskRunner to allow for thread-safe implementations, and
    adds a default implementation that is unsynchronized but allows callers to
    use it in a thread-safe way.
    
    A ScopedTaskRunner based on fxl::WeakPtr is not thread-safe because
    fxl::WeakPtrFactory does flag twiddling and does not allow for a shut-down
    state, which can lead to race conditions when tasks are posted from a thread
    other than the dispatch thread.
    
    Also adds more unit tests.
    
    Depends-On: https://fuchsia-review.googlesource.com/c/peridot/+/229101
    Test: fx shell runtests -t garnet_lib_unittests
    Change-Id: Ie9f1b2ee7b77cdadb0f94f5200311b8bd527ad07

--- public/lib/callback/scoped_task_runner.cc
+++ public/lib/callback/scoped_task_runner.cc
@@ -2,20 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "lib/callback/scoped_task_runner.h"
+#include <lib/callback/scoped_task_runner.h>
 
 #include <utility>
 
 #include <lib/async/cpp/task.h>
-#include <lib/async/default.h>
-#include <lib/fit/function.h>
 
 namespace callback {
 
-ScopedTaskRunner::ScopedTaskRunner(async_dispatcher_t* dispatcher)
-    : dispatcher_(dispatcher), weak_factory_(this) {}
+TaskController::TaskController() = default;
+TaskController::~TaskController() = default;
 
-ScopedTaskRunner::~ScopedTaskRunner() {}
+void SimpleTaskController::ShutDown() { alive_ = false; }
+
+void SimpleTaskController::RunTask(fit::closure task) {
+  if (alive_)
+    task();
+}
+
+ScopedTaskRunner::ScopedTaskRunner(async_dispatcher_t* dispatcher)
+    : ScopedTaskRunner(SimpleTaskController::Type{}, dispatcher) {}
+
+ScopedTaskRunner::~ScopedTaskRunner() { ShutDown(); }
+
+void ScopedTaskRunner::ShutDown() { controller_->ShutDown(); }
 
 void ScopedTaskRunner::PostTask(fit::closure task) {
   async::PostTask(dispatcher_, MakeScoped(std::move(task)));
@@ -28,6 +38,28 @@ void ScopedTaskRunner::PostTaskForTime(fit::closure task,
 
 void ScopedTaskRunner::PostDelayedTask(fit::closure task, zx::duration delay) {
   async::PostDelayedTask(dispatcher_, MakeScoped(std::move(task)), delay);
+}
+
+void ScopedTaskRunner::PostPeriodicTask(fit::closure task,
+                                        zx::duration interval,
+                                        bool invoke_immediately) {
+  if (invoke_immediately) {
+    // TODO(rosswang): this is actually incorrect as it may execute the first
+    // instance of this task on the wrong thread. Add a test and fix in a
+    // subsequent CL.
+    task();
+  }
+
+  // We live with re-wrapping this task every iteration to greatly simplify the
+  // implementation. It's possible to wrap once, but as the scoped task would
+  // then need to capture itself, we'd have to declare a dedicated functor.
+  // Since this area is not performance-critical, err on the side of simplicity
+  // for now.
+  PostDelayedTask(
+      [this, task = std::move(task), interval]() mutable {
+        PostPeriodicTask(std::move(task), interval, true);
+      },
+      interval);
 }
 
 }  // namespace callback
--- public/lib/callback/scoped_task_runner_unittest.cc
+++ public/lib/callback/scoped_task_runner_unittest.cc
@@ -2,16 +2,100 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "lib/callback/scoped_task_runner.h"
+#include <lib/callback/scoped_task_runner.h>
 
+#include <gtest/gtest.h>
 #include <lib/async-testutils/test_loop.h>
-
-#include "gtest/gtest.h"
+#include <lib/fxl/macros.h>
 
 namespace callback {
 namespace {
 
-TEST(ScopedTaskRunner, DelegateToDispatcher) {
+constexpr zx::duration kInterval = zx::min(4);  // to save the world
+
+struct MoveOnly {
+  MoveOnly(MoveOnly&&) = default;
+  bool called;
+  FXL_DISALLOW_COPY_AND_ASSIGN(MoveOnly);
+};
+
+TEST(ScopedTaskRunnerTest, RunsTaskInScope) {
+  bool called = false;
+  ScopedTaskRunner tasks;
+  tasks.MakeScoped([&] { called = true; })();
+  EXPECT_TRUE(called);
+}
+
+TEST(ScopedTaskRunnerTest, TaskWithArg) {
+  bool called = false;
+  ScopedTaskRunner tasks;
+  tasks.MakeScoped([&](bool value) { called = value; })(true);
+  EXPECT_TRUE(called);
+}
+
+TEST(ScopedTaskRunnerTest, TaskWithRefArg) {
+  bool called = false;
+  ScopedTaskRunner tasks;
+  tasks.MakeScoped([](bool& called) { called = true; })(called);
+  EXPECT_TRUE(called);
+}
+
+TEST(ScopedTaskRunnerTest, TaskWithMoveOnlyArg) {
+  bool called = false;
+  ScopedTaskRunner tasks;
+  tasks.MakeScoped([&](MoveOnly arg) { called = arg.called; })({true});
+  EXPECT_TRUE(called);
+}
+
+TEST(ScopedTaskRunnerTest, TaskWithMoveOnlyCapture) {
+  bool called = false;
+  ScopedTaskRunner tasks;
+  MoveOnly move_only{true};
+  tasks.MakeScoped([&called, move_only = std::move(move_only)] {
+    called = move_only.called;
+  })();
+  EXPECT_TRUE(called);
+}
+
+TEST(ScopedTaskRunnerTest, CancelsTaskOutOfScope) {
+  bool called = false;
+  fit::closure task;
+  {
+    ScopedTaskRunner tasks;
+    task = tasks.MakeScoped([&] { called = true; });
+  }
+  task();
+  EXPECT_FALSE(called);
+}
+
+TEST(ScopedTaskRunnerTest, ExplicitShutdown) {
+  bool called = false;
+  ScopedTaskRunner tasks;
+  auto task = tasks.MakeScoped([&] { called = true; });
+  tasks.ShutDown();
+  task();
+  EXPECT_FALSE(called);
+}
+
+TEST(ScopedTaskRunnerTest, MakeScopedAfterShutdown) {
+  bool called = false;
+  ScopedTaskRunner tasks;
+  tasks.ShutDown();
+  tasks.MakeScoped([&] { called = true; })();
+  EXPECT_FALSE(called);
+}
+
+TEST(ScopedTaskRunnerTest, DestroyDuringTaskOnThread) {
+  bool called = false;
+  auto* tasks = new ScopedTaskRunner;
+  tasks->MakeScoped([&] {
+    delete tasks;
+    called = true;
+  })();
+  EXPECT_TRUE(called);
+}
+
+TEST(ScopedTaskRunner, PostTaskAndFriends) {
   async::TestLoop loop;
 
   uint8_t called = 0;
@@ -25,7 +109,7 @@ TEST(ScopedTaskRunner, DelegateToDispatcher) {
   EXPECT_EQ(3u, called);
 }
 
-TEST(ScopedTaskRunner, CancelOnDeletion) {
+TEST(ScopedTaskRunner, PostTaskAndFriendsCancelOnDeletion) {
   async::TestLoop loop;
 
   uint8_t called = 0;
@@ -40,6 +124,21 @@ TEST(ScopedTaskRunner, CancelOnDeletion) {
 
   loop.RunUntilIdle();
   EXPECT_EQ(0u, called);
+}
+
+TEST(ScopedTaskRunnerTest, PostPeriodicTask) {
+  async::TestLoop loop;
+  int called = 0;
+  {
+    ScopedTaskRunner tasks(loop.dispatcher());
+    tasks.PostPeriodicTask([&] { called++; }, kInterval, false);
+    loop.RunFor(kInterval * 4);
+  }
+
+  EXPECT_EQ(4, called);
+
+  loop.RunFor(kInterval * 4);
+  EXPECT_EQ(4, called);
 }
 
 }  // namespace
