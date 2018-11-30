fab6fc697a77fb30ad1b3d8f8b7fc30159b09bee
Rob Tsuk <robtsuk@google.com>
2018-11-30 10:12:46 -0800

    [rust] No longer put “edition” in cargo-features
    The cargo feature `edition` is now stable and is no longer necessary
    to be listed in the manifest.
    
    Testing: manual
    Change-Id: Ia536e6fc990ffc353c5b41123ce9a9c81f72791d

--- rust/write_cargo_toml.py
+++ rust/write_cargo_toml.py
@@ -31,8 +31,6 @@ CARGO_TOML_CONTENTS = '''\
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-cargo-features = ["edition"]
-
 [package]
 name = "%(package_name)s"
 version = "%(version)s"
