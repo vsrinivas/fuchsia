3486bf38857b41da823436c59e8cf8748da31e74
Christopher R. Johnson <crjohns@google.com>
2018-11-30 15:01:00 -0800

    [inspect] Heap implementation for inspect vmo.
    This change contains the implementation for a buddy-allocated heap
    stored in a VMO. The VMO may extend up to an upper limit to accomodate
    allocations.
    
    The Heap supports Allocate() and Free(), and it checks that all blocks
    are properly freed before destruction (asserts only in debug builds).
    
    TEST=zircon$ runtests -t inspect-test
    CF-215: #progress
    
    Change-Id: I049302c44cc1c0ad4455ac39daacae37393bba49

---
+++ system/ulib/inspect/include/lib/inspect/block.h
@@ -0,0 +1,140 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include "limits.h"
+
+#include <fbl/function.h>
+#include <fbl/string.h>
+#include <fbl/string_piece.h>
+#include <fbl/type_support.h>
+
+#include <zircon/types.h>
+
+namespace inspect {
+
+enum class BlockType {
+    kFree = 0,
+    kReserved = 1,
+    kHeader = 2,
+    kObjectValue = 3,
+    kIntValue = 4,
+    kUintValue = 5,
+    kDoubleValue = 6,
+    kStringValue = 7,
+    kExtent = 8,
+    kName = 9,
+    kTombstone = 10
+};
+
+namespace internal {
+
+using BlockOrder = uint32_t;
+using BlockIndex = uint64_t;
+
+// Returns the smallest order such that (kMinOrderSize << order) >= size.
+// Size must be non-zero.
+constexpr BlockOrder FitOrder(size_t size) {
+    auto ret = 64 - __builtin_clzl(size - 1) - kMinOrderShift;
+    return static_cast<BlockOrder>(ret);
+}
+
+// Structure of the block header and payload.
+struct Block {
+    union {
+        uint64_t header;
+        char header_data[8];
+    };
+    union {
+        int64_t i64;
+        uint64_t u64;
+        double f64;
+        char data[8];
+    } payload;
+};
+
+static_assert(sizeof(Block) == 16, "Block header must be 16 bytes");
+static_assert(sizeof(Block) == kMinOrderSize,
+              "Minimum allocation size must exactly hold a block header");
+
+// Describes the layout of a bit-field packed into a 64-bit word.
+template <size_t begin, size_t end>
+struct Field {
+    static_assert(begin < sizeof(uint64_t) * 8, "begin is out of bounds");
+    static_assert(end < sizeof(uint64_t) * 8, "end is out of bounds");
+    static_assert(begin <= end, "begin must not be larger than end");
+    static_assert(end - begin + 1 < 64, "must be a part of a word, not a whole word");
+
+    static constexpr uint64_t kMask = (uint64_t(1) << (end - begin + 1)) - 1;
+
+    template <typename T>
+    static constexpr uint64_t Make(T value) {
+        return static_cast<uint64_t>(value) << begin;
+    }
+
+    template <typename U>
+    static constexpr U Get(uint64_t word) {
+        return static_cast<U>((word >> (begin % 64)) & kMask);
+    }
+
+    static constexpr void Set(uint64_t* word, uint64_t value) {
+        *word = (*word & ~(kMask << begin)) | (value << begin);
+    }
+};
+
+// Describes the base fields present for all blocks.
+struct BlockFields {
+    using Order = Field<0, 3>;
+    using Type = Field<4, 7>;
+};
+
+struct HeaderBlockFields : public BlockFields {
+    using Version = Field<8, 31>;
+    using MagicNumber = Field<32, 63>;
+};
+
+struct FreeBlockFields : public BlockFields {
+    using NextFreeBlock = Field<8, 35>;
+};
+
+// Describes the fields common to all value blocks.
+struct ValueBlockFields : public BlockFields {
+    using ParentIndex = Field<8, 35>;
+    using NameIndex = Field<36, 63>;
+};
+
+struct PropertyBlockPayload {
+    using TotalLength = Field<0, 31>;
+    using ExtentIndex = Field<32, 59>;
+};
+
+struct ExtentBlockFields : public BlockFields {
+    using NextExtentIndex = Field<8, 35>;
+};
+
+struct NameBlockFields : public BlockFields {
+    using Length = Field<8, 19>;
+};
+
+constexpr BlockOrder GetOrder(const Block* block) {
+    return BlockFields::Order::Get<BlockOrder>(block->header);
+}
+
+constexpr BlockType GetType(const Block* block) {
+    return BlockFields::Type::Get<BlockType>(block->header);
+}
+
+constexpr size_t PayloadCapacity(BlockOrder order) {
+    return OrderToSize(order) - sizeof(Block::header);
+}
+
+constexpr size_t BlockSizeForPayload(size_t payload_size) {
+    return fbl::max(payload_size + sizeof(Block::header), kMinOrderSize);
+}
+
+constexpr size_t kMaxPayloadSize = kMaxOrderSize - sizeof(Block::header);
+
+} // namespace internal
+} // namespace inspect
---
+++ system/ulib/inspect/rules.mk
@@ -0,0 +1,58 @@
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userlib
+
+MODULE_COMPILEFLAGS += -fvisibility=hidden
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/heap.cpp \
+    $(LOCAL_DIR)/scanner.cpp \
+
+MODULE_HEADER_DEPS := \
+    system/ulib/zircon-internal \
+
+MODULE_STATIC_LIBS := \
+    system/ulib/fbl \
+    system/ulib/fzl \
+    system/ulib/syslog \
+    system/ulib/zx \
+    system/ulib/zxcpp \
+
+MODULE_PACKAGE := src
+
+include make/module.mk
+
+MODULE := $(LOCAL_DIR).test
+
+MODULE_TYPE := usertest
+
+MODULE_NAME := inspect-test
+
+TEST_DIR := $(LOCAL_DIR)/test
+
+MODULE_SRCS := \
+    $(TEST_DIR)/main.cpp \
+    $(TEST_DIR)/heap_tests.cpp \
+    $(TEST_DIR)/scanner_tests.cpp \
+
+MODULE_STATIC_LIBS := \
+    system/ulib/inspect \
+    system/ulib/fbl \
+    system/ulib/fzl \
+    system/ulib/zx \
+    system/ulib/zxcpp \
+
+MODULE_LIBS := \
+    system/ulib/c \
+    system/ulib/fdio  \
+    system/ulib/unittest \
+    system/ulib/zircon \
+    system/ulib/unittest \
+
+include make/module.mk
