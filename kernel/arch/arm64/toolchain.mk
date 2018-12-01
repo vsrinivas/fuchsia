5ba5efdb14d48c3b87b386c363d47c13c3f82924
Travis Geiselbrecht <travisg@google.com>
2018-11-30 20:34:14 -0800

    [make][toolchain] make sure the system doesn't bomb out of gcc isn't available
    In systems where only clang is available, continue.
    
    Synchronize the two toolchain.mk files and get rid of old fallback
    android toolchain logic in the arm64 path.
    
    tested: build with and without the appropriate variables set, with and without USE_CLANG set
    Change-Id: I8d8746bf21a073079cd0bb1ef2f48aac63580b99

--- kernel/arch/arm64/toolchain.mk
+++ kernel/arch/arm64/toolchain.mk
@@ -5,27 +5,23 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT
 
+# arm64 GCC toolchain
 ifndef ARCH_arm64_TOOLCHAIN_INCLUDED
 ARCH_arm64_TOOLCHAIN_INCLUDED := 1
 
 ifndef ARCH_arm64_TOOLCHAIN_PREFIX
 ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-elf-
+endif
 FOUNDTOOL=$(shell which $(ARCH_arm64_TOOLCHAIN_PREFIX)gcc)
-ifeq ($(FOUNDTOOL),)
-ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-linux-android-
-FOUNDTOOL=$(shell which $(ARCH_arm64_TOOLCHAIN_PREFIX)gcc)
-ifeq ($(FOUNDTOOL),)
-$(error cannot find toolchain, please set ARCH_arm64_TOOLCHAIN_PREFIX or add it to your path)
-endif
-endif
-endif
 
-endif
+endif # ifndef ARCH_arm64_TOOLCHAIN_INCLUDED
 
 # Clang
 ifeq ($(call TOBOOL,$(USE_CLANG)),true)
 FOUNDTOOL=$(shell which $(CLANG_TOOLCHAIN_PREFIX)clang)
+endif # USE_CLANG==true
+
 ifeq ($(FOUNDTOOL),)
-$(error cannot find toolchain, please set CLANG_TOOLCHAIN_PREFIX or add it to your path)
-endif
+$(error cannot find toolchain, please set ARCH_arm64_TOOLCHAIN_PREFIX, \
+        CLANG_TOOLCHAIN_PREFIX, or add either to your path)
 endif
