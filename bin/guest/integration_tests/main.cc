3ce783ecb4e84fa2c5997078257887169b0b3528
Alex Legg <alexlegg@google.com>
2018-12-03 09:39:57 +1100

    [guest][integration] Remove check for supported hardware
    No longer necessary now that the the environment is specified in
    BUILD.gn and respected by CQ.
    
    TEST=run-test guest_integration_tests
    
    Change-Id: I630d26780016ea45338dbdfd2a46399cd9511419

--- bin/guest/integration_tests/main.cc
+++ bin/guest/integration_tests/main.cc
@@ -2,18 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fcntl.h>
 #include <string>
 
-#include <fbl/unique_fd.h>
-#include <fuchsia/sys/cpp/fidl.h>
-#include <fuchsia/sysinfo/c/fidl.h>
 #include <gmock/gmock.h>
-#include <lib/fdio/util.h>
 #include <lib/fxl/logging.h>
 #include <lib/fxl/strings/string_printf.h>
-#include <lib/zx/socket.h>
-#include <zircon/syscalls/hypervisor.h>
 
 #include "guest_test.h"
 
@@ -101,45 +94,4 @@ TEST_F(LinuxMultiprocessorGuestTest, LaunchGuest) {
   std::string result;
   EXPECT_EQ(Execute("echo \"test\"", &result), ZX_OK);
   EXPECT_EQ(result, "test\n");
-}
-
-zx_status_t hypervisor_supported() {
-  fbl::unique_fd fd(open("/dev/misc/sysinfo", O_RDWR));
-  if (!fd) {
-    return ZX_ERR_IO;
-  }
-
-  zx::channel channel;
-  zx_status_t status =
-      fdio_get_service_handle(fd.release(), channel.reset_and_get_address());
-  if (status != ZX_OK) {
-    return status;
-  }
-
-  zx::resource resource;
-  zx_status_t fidl_status = fuchsia_sysinfo_DeviceGetHypervisorResource(
-      channel.get(), &status, resource.reset_and_get_address());
-  if (fidl_status != ZX_OK) {
-    return fidl_status;
-  } else if (status != ZX_OK) {
-    return status;
-  }
-
-  zx::guest guest;
-  zx::vmar vmar;
-  return zx::guest::create(resource, 0, &guest, &vmar);
-}
-
-int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
-
-  zx_status_t status = hypervisor_supported();
-  if (status == ZX_ERR_NOT_SUPPORTED) {
-    FXL_LOG(INFO) << "Hypervisor is not supported";
-    return ZX_OK;
-  } else if (status != ZX_OK) {
-    return status;
-  }
-
-  return RUN_ALL_TESTS();
 }
