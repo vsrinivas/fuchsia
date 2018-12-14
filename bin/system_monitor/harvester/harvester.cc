84fd790d81b3a71e46bee650904f89e5ea38e6b5
Dave Schuyler <dschuyler@chromium.org>
2018-12-13 17:53:18 -0800

    [system_monitor] Initial executable
    Baby steps. This adds an executable for the Harvester. Right now it just prints
    a line of text. Note that this is intentionally not part of garnet/packages/all
    at this time.
    
    Test: by hand. Add system_monitor package, build and run
    $ tools/system_monitor_harvester
    in the Fuchsia terminal. It should print a line of text and exit.
    
    Change-Id: I60a2ca90704589acc786e72195d250b2db3b7f62

---
+++ bin/system_monitor/harvester/harvester.cc
@@ -0,0 +1,18 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+
+/*
+    Hey, so there's not a whole lot here. That's because this is a work in
+    progress. Starting from something like a hello world program, this will
+    progress into a System Monitor for Fuchsia.
+
+    See also: ./README.md
+*/
+
+int main(int argc, char** argv) {
+  printf("System Monitor Harvester - wip\n");
+  return 0;
+}
