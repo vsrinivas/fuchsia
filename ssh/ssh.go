cc0f00395097c52538db56e9829cabca55be0558
Kendal Harland <kjharland@google.com>
2018-12-20 16:23:00 -0800

    [ssh] Use net.UDPAddr in Connect
    This allows IPv6 or IPv4 addresses.
    
    Change-Id: Ice162fa80ad3e91a510a17cda400d57a13d82d78

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -10,6 +10,7 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"net"
 	"time"
 
 	"fuchsia.googlesource.com/tools/retry"
@@ -49,7 +50,7 @@ func GenerateKeyPair(bitSize int) ([]byte, []byte, error) {
 
 // Connect establishes a new SSH connection to a server with the given
 // address and port, using the provided user name and private key.
-func Connect(ctx context.Context, address string, port int, user string, privateKey []byte) (*ssh.Client, error) {
+func Connect(ctx context.Context, addr *net.UDPAddr, user string, privateKey []byte) (*ssh.Client, error) {
 	signer, err := ssh.ParsePrivateKey(privateKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse the private key: %v", err)
@@ -64,16 +65,34 @@ func Connect(ctx context.Context, address string, port int, user string, private
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
+	address := addr.String()
+	protocol, err := protocol(addr)
+	if err != nil {
+		return nil, err
+	}
+
 	var client *ssh.Client
-	addr := fmt.Sprintf("%s:%d", address, port)
 	// TODO: figure out optimal backoff time and number of retries
 	err = retry.Retry(ctx, retry.WithMaxRetries(retry.NewConstantBackoff(time.Second), 10), func() error {
-		client, err = ssh.Dial("tcp", addr, config)
+		client, err = ssh.Dial(protocol, address, config)
 		return err
 	})
 	if err != nil {
-		return nil, fmt.Errorf("cannot connect to \"%s\": %v", addr, err)
+		return nil, fmt.Errorf("cannot connect to '%s' address, '%s': %v", protocol, address, err)
 	}
 
 	return client, nil
+}
+
+// Returns the protocol to use to SSH into a device.
+func protocol(address *net.UDPAddr) (string, error) {
+	if address.IP.To4() != nil {
+		return "tcp", nil // IPv4
+	}
+
+	if address.IP.To16() != nil {
+		return "tcp6", nil // IPv6
+	}
+
+	return "", fmt.Errorf("cannot infer protocol for address '%s'", address.String())
 }
