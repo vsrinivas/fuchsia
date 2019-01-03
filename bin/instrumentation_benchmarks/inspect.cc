e2e00aa6c4f74187d46e07f6fcb2395730659785
Chris Phoenix <cphoenix@google.com>
2019-01-02 18:07:41 -0800

    [benchmarks] Add property tests.
    Add tests for small and large string and vector properties.
    
    Test: /pkgfs/packages/instrumentation_benchmarks/0/test/instrumentation_benchmarks -p
    CF-205 #progress
    
    Change-Id: If6c58977d2ceb1337ab9edf2e1053477ad414e3d

--- bin/instrumentation_benchmarks/inspect.cc
+++ bin/instrumentation_benchmarks/inspect.cc
@@ -11,32 +11,78 @@
 #include <perftest/perftest.h>
 #include <zircon/syscalls.h>
 
+#include <iostream>
+#include <sstream>
+
 #include "lib/component/cpp/exposed_object.h"
 
 namespace {
 
-const char kValue[] = "value";
+using ByteVector = component::Property::ByteVector;
 
-class Item : public component::ExposedObject {
+const char kValue[] = "value";
+const int kSmallPropertySize = 8;
+const int kLargePropertySize = 10000;
+
+class NumericItem : public component::ExposedObject {
  public:
-  Item() : ExposedObject(UniqueName("item-")) {
+  NumericItem() : ExposedObject(UniqueName("itemN-")) {
     object_dir().set_metric(kValue, component::IntMetric(0));
   }
   void increment() { object_dir().add_metric(kValue, 1); }
 };
 
+class PropertyItem : public component::ExposedObject {
+ public:
+  PropertyItem() : ExposedObject(UniqueName("itemS-")) {
+    object_dir().set_prop(kValue, component::Property());
+  }
+  void set(std::string str_value) { object_dir().set_prop(kValue, std::move(str_value)); }
+  void set(ByteVector vector_value) { object_dir().set_prop(kValue, std::move(vector_value)); }
+};
+
 // Measure the time taken to increment an IntMetric.
 bool TestIncrement(perftest::RepeatState* state) {
-  Item* item = new Item();
+  NumericItem item;
+  while (state->KeepRunning()) {
+    item.increment();
+  }
+  return true;
+}
+
+// Measure the time taken to change a String property.
+bool TestString(perftest::RepeatState* state, int size) {
+  PropertyItem item;
+  std::string string;
+  string.resize(size, 'a');
+  while (state->KeepRunning()) {
+    item.set(string);
+  }
+  return true;
+}
+
+// Measure the time taken to change a ByteVector property.
+bool TestVector(perftest::RepeatState* state, int size) {
+  PropertyItem* item = new PropertyItem();
+  ByteVector vector;
+  vector.resize(size, 'a');
   ZX_ASSERT(item != nullptr);
   while (state->KeepRunning()) {
-    item->increment();
+    item->set(vector);
   }
   return true;
 }
 
 void RegisterTests() {
   perftest::RegisterTest("Inspect/Increment", TestIncrement);
+  perftest::RegisterTest(fxl::StringPrintf("Inspect/String%d", kSmallPropertySize).c_str(),
+                         TestString, kSmallPropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Inspect/String%d", kLargePropertySize).c_str(),
+                         TestString, kLargePropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Inspect/Vector%d", kSmallPropertySize).c_str(),
+                         TestVector, kSmallPropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Inspect/Vector%d", kLargePropertySize).c_str(),
+                         TestVector, kLargePropertySize);
 }
 PERFTEST_CTOR(RegisterTests);
 
