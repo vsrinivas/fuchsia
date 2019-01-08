7236db78be316474e9d1ce36125037c43e3ae833
Joshua Seaton <joshuaseaton@google.com>
2019-01-08 14:36:33 -0800

    [runtests][host] Avoid shared objects.
    Shared objects are needed for some tests at the same directory level,
    though we need to make sure to skip them.
    
    Test: ran locally; specific so's skipped
    Change-Id: I4918bd7f480f857e1fbd01c915d72ac29be2bd20

--- system/host/runtests/runtests.cpp
+++ system/host/runtests/runtests.cpp
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <time.h>
 
+#include <vector>
+
 #include <fbl/string.h>
 #include <fbl/vector.h>
 #include <runtests-utils/posix-run-test.h>
@@ -37,9 +39,21 @@ private:
 
 } // namespace
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
     PosixStopwatch stopwatch;
-    return runtests::DiscoverAndRunTests(&runtests::PosixRunTest, argc, argv,
+    // TODO(IN-819): Temporary work-around: shared objects need to be copied
+    // into $root_build_dir/host_tests, but at the same time they cannot be run.
+    std::vector<const char*> so_filters = {
+      "-t", "libfostr_shared.so",
+      "-t", "libfostr_shared.dylib",
+    };
+    std::vector<const char*> argv_with_so_filters(argv, argv + argc);
+    argv_with_so_filters.insert(argv_with_so_filters.end(),
+                                so_filters.begin(), so_filters.end());
+
+    return runtests::DiscoverAndRunTests(&runtests::PosixRunTest,
+                                         argv_with_so_filters.size(),
+                                         argv_with_so_filters.data(),
                                          /*default_test_dirs=*/{}, &stopwatch,
                                          /*syslog_file_name=*/"");
 }
