b9972a13acb8a14b999ec0f7a0ec65a71c6109a6
Doug Evans <dje@google.com>
2019-01-09 11:44:54 -0800

    [trace-benchmark] Add test code sizes to output
    Tested: trace-benchmark + verify expected output, CQ
    
    Change-Id: I307d715c5d9b099125d2123a6a24b67163d0a4a0

--- system/uapp/trace-benchmark/benchmarks.cpp
+++ system/uapp/trace-benchmark/benchmarks.cpp
@@ -5,6 +5,7 @@
 #include "benchmarks.h"
 
 #include <inttypes.h>
+#include <stdarg.h>
 #include <stdio.h>
 
 #include <fbl/function.h>
@@ -54,76 +55,115 @@ public:
         }
     }
 
+    // Utility to print a line of text in the same format as RunAndMeasure.
+    void Print(const char* fmt, ...) {
+        fputs(kTestOutputPrefix, stdout);
+        va_list args;
+        va_start(args, fmt);
+        vprintf(fmt, args);
+        va_end(args);
+    }
+
 private:
     const bool enabled_;
     // nullptr if |!enabled_|.
     const BenchmarkSpec* spec_;
 };
 
-#define RUN_NILADIC_DURATION_TEST(test_name, DURATION_MACRO, category_literal)     \
-    do {                                                                           \
-        runner.Run(#DURATION_MACRO " macro with " test_name ": " category_literal, \
-                   [] {                                                            \
-                       DURATION_MACRO(category_literal, "name");                   \
-                   });                                                             \
+} // namespace
+
+#define MAKE_TEST_SYMBOL_NAME(prefix, DURATION_MACRO, test_symbol_name, category) \
+    prefix##DURATION_MACRO##test_symbol_name##category
+
+// clang-format doesn't understand the leading #
+// clang-format off
+#define MAKE_TEST_SYMBOL_STRING(prefix, DURATION_MACRO, test_symbol_name, category) \
+    #prefix #DURATION_MACRO #test_symbol_name #category
+// clang-format on
+
+// N.B. In order for this to work, this code cannot live in the anonymous
+// namespace. Otherwise the compiler attaches that namespace to the section
+// name which will no longer be a valid C symbol (which is what we need in
+// order for the linker to generate the __{start,stop}_SECNAME symbols.
+#define RUN_TEST(test_name, pretty_test_name, macro_name, enabled_prefix, category, expression)                 \
+    do {                                                                                                        \
+        const char* full_test_name =                                                                            \
+            #macro_name " macro with " pretty_test_name ": " #category;                                         \
+        struct Test {                                                                                           \
+            static void Run()                                                                                   \
+                __attribute__((__section__(MAKE_TEST_SYMBOL_STRING(test_, macro_name, test_name, category)))) { \
+                expression;                                                                                     \
+            }                                                                                                   \
+        };                                                                                                      \
+        runner.Run(full_test_name, Test::Run);                                                                  \
+        extern const char MAKE_TEST_SYMBOL_NAME(__start_test_, macro_name, test_name, category)[];              \
+        extern const char MAKE_TEST_SYMBOL_NAME(__stop_test_, macro_name, test_name, category)[];               \
+        size_t test_size =                                                                                      \
+            (MAKE_TEST_SYMBOL_NAME(__stop_test_, macro_name, test_name, category) -                             \
+             MAKE_TEST_SYMBOL_NAME(__start_test_, macro_name, test_name, category));                            \
+        runner.Print("test size: %zu bytes\n", test_size);                                                      \
     } while (0)
 
-#define RUN_DURATION_TEST(test_name, DURATION_MACRO, category_literal, ...)        \
-    do {                                                                           \
-        runner.Run(#DURATION_MACRO " macro with " test_name ": " category_literal, \
-                   [] {                                                            \
-                       DURATION_MACRO(category_literal, "name",                    \
-                                      __VA_ARGS__);                                \
-                   });                                                             \
-    } while (0)
+// The niladic version is needed to portably work around the trailing comma
+// problem when using __VA_ARGS__.
+#define RUN_NILADIC_DURATION_TEST(test_name, pretty_test_name, DURATION_MACRO, enabled_prefix, category) \
+    RUN_TEST(test_name, pretty_test_name, DURATION_MACRO, enabled_prefix, category,                      \
+             DURATION_MACRO(enabled_prefix #category, "name"))
 
-#define DURATION_TEST(DURATION_MACRO, category_literal)          \
-    RUN_NILADIC_DURATION_TEST("0 arguments",                     \
-                              DURATION_MACRO, category_literal); \
-                                                                 \
-    RUN_DURATION_TEST("1 int32 argument",                        \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", 1);                                  \
-                                                                 \
-    RUN_DURATION_TEST("1 double argument",                       \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", 1.);                                 \
-                                                                 \
-    RUN_DURATION_TEST("1 string argument",                       \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", "string1");                          \
-                                                                 \
-    RUN_DURATION_TEST("4 int32 arguments",                       \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", 1, "k2", 2, "k3", 3, "k4", 4);       \
-                                                                 \
-    RUN_DURATION_TEST("4 double arguments",                      \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", 1., "k2", 2., "k3", 3., "k4", 4.);   \
-                                                                 \
-    RUN_DURATION_TEST("4 string arguments",                      \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", "string1", "k2", "string2",          \
-                      "k3", "string3", "k4", "string4");         \
-                                                                 \
-    RUN_DURATION_TEST("8 int32 arguments",                       \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", 1, "k2", 2, "k3", 3, "k4", 4,        \
-                      "k5", 5, "k6", 6, "k7", 7, "k8", 8);       \
-                                                                 \
-    RUN_DURATION_TEST("8 double arguments",                      \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", 1., "k2", 2., "k3", 3., "k4", 4.,    \
-                      "k5", 4., "k6", 5., "k7", 7., "k8", 8.);   \
-                                                                 \
-    RUN_DURATION_TEST("8 string arguments",                      \
-                      DURATION_MACRO, category_literal,          \
-                      "k1", "string1", "k2", "string2",          \
-                      "k3", "string3", "k4", "string4",          \
-                      "k5", "string5", "k6", "string6",          \
+#define RUN_DURATION_TEST(test_name, pretty_test_name, DURATION_MACRO, enabled_prefix, category, ...) \
+    RUN_TEST(test_name, pretty_test_name, DURATION_MACRO, enabled_prefix, category,                   \
+             DURATION_MACRO(enabled_prefix #category, "name", __VA_ARGS__))
+
+#define DURATION_TEST(DURATION_MACRO, enabled_prefix, category)          \
+    RUN_NILADIC_DURATION_TEST(zero_args, "0 arguments",                  \
+                              DURATION_MACRO, enabled_prefix, category); \
+                                                                         \
+    RUN_DURATION_TEST(one_int32_arg, "1 int32 argument",                 \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", 1);                                          \
+                                                                         \
+    RUN_DURATION_TEST(one_double_arg, "1 double argument",               \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", 1.);                                         \
+                                                                         \
+    RUN_DURATION_TEST(one_string_arg, "1 string argument",               \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", "string1");                                  \
+                                                                         \
+    RUN_DURATION_TEST(four_int32_args, "4 int32 arguments",              \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", 1, "k2", 2, "k3", 3, "k4", 4);               \
+                                                                         \
+    RUN_DURATION_TEST(four_double_args, "4 double arguments",            \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", 1., "k2", 2., "k3", 3., "k4", 4.);           \
+                                                                         \
+    RUN_DURATION_TEST(four_string_args, "4 string arguments",            \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", "string1", "k2", "string2",                  \
+                      "k3", "string3", "k4", "string4");                 \
+                                                                         \
+    RUN_DURATION_TEST(eight_int32_args, "8 int32 arguments",             \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", 1, "k2", 2, "k3", 3, "k4", 4,                \
+                      "k5", 5, "k6", 6, "k7", 7, "k8", 8);               \
+                                                                         \
+    RUN_DURATION_TEST(eight_double_args, "8 double arguments",           \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", 1., "k2", 2., "k3", 3., "k4", 4.,            \
+                      "k5", 4., "k6", 5., "k7", 7., "k8", 8.);           \
+                                                                         \
+    RUN_DURATION_TEST(eight_string_args, "8 string arguments",           \
+                      DURATION_MACRO, enabled_prefix, category,          \
+                      "k1", "string1", "k2", "string2",                  \
+                      "k3", "string3", "k4", "string4",                  \
+                      "k5", "string5", "k6", "string6",                  \
                       "k7", "string7", "k8", "string8");
 
-void RunBenchmarks(bool tracing_enabled, const BenchmarkSpec* spec) {
+// This is not in the anonymous namespace so that the namespace name
+// doesn't enter into the computation of the location of Test::Run which
+// is put in its own section.
+static void RunBenchmarks(bool tracing_enabled, const BenchmarkSpec* spec) {
     Runner runner(tracing_enabled, spec);
 
     runner.Run("is enabled", [] {
@@ -163,28 +203,27 @@ void RunBenchmarks(bool tracing_enabled, const BenchmarkSpec* spec) {
         });
     }
 
-    DURATION_TEST(TRACE_DURATION_BEGIN, "+enabled");
-    DURATION_TEST(TRACE_DURATION, "+enabled");
+    DURATION_TEST(TRACE_DURATION_BEGIN, "+", enabled);
+    DURATION_TEST(TRACE_DURATION, "+", enabled);
 
-    runner.Run("TRACE_VTHREAD_DURATION_BEGIN macro with 0 arguments", [] {
-        TRACE_VTHREAD_DURATION_BEGIN("+enabled", "name", "vthread", 1, zx_ticks_get());
-    });
+    // There's no real need (yet) to test vthread support with multiple
+    // variations of arguments. If we did that for all macros the S/N ratio
+    // of the output would drop too much.
+    RUN_TEST(zero_args, "0 arguments", TRACE_VTHREAD_DURATION_BEGIN, "+", enabled,
+             TRACE_VTHREAD_DURATION_BEGIN("+enabled", "name", "vthread", 1, zx_ticks_get()));
 
     if (tracing_enabled) {
-        DURATION_TEST(TRACE_DURATION_BEGIN, "-disabled");
-        DURATION_TEST(TRACE_DURATION, "-disabled");
+        DURATION_TEST(TRACE_DURATION_BEGIN, "-", disabled);
+        DURATION_TEST(TRACE_DURATION, "-", disabled);
 
-        runner.Run("TRACE_VTHREAD_DURATION_BEGIN macro with 0 arguments for disabled category", [] {
-            TRACE_VTHREAD_DURATION_BEGIN("-disabled", "name", "vthread", 1, zx_ticks_get());
-        });
+        RUN_TEST(zero_args, "0 arguments", TRACE_VTHREAD_DURATION_BEGIN, "-", disabled,
+                 TRACE_VTHREAD_DURATION_BEGIN("+enabled", "name", "vthread", 1, zx_ticks_get()));
     }
 }
 
-} // namespace
-
 void RunTracingDisabledBenchmarks() {
     static const BenchmarkSpec spec = {
-        "disabled",
+        "tracing off",
         TRACE_BUFFERING_MODE_ONESHOT, // unused
         0,
         kDefaultRunIterations,
--- system/uapp/trace-benchmark/runner.h
+++ system/uapp/trace-benchmark/runner.h
@@ -12,6 +12,9 @@
 
 #include <utility>
 
+// Lines of text for each result are prefixed with this.
+constexpr const char* kTestOutputPrefix = "  - ";
+
 constexpr unsigned kWarmUpIterations = 10;
 // N.B. This value can't be so large that the buffer fills in oneshot mode.
 // The benchmark will assert-fail if the buffer fills: Otherwise the benchmark
@@ -49,8 +52,9 @@ void RunAndMeasure(const char* test_name, const char* spec_name,
     setup();
     float warm_up_time = Measure(kWarmUpIterations, closure);
     teardown();
-    printf("  - warm-up: %u iterations in %.3f us, %.3f us per iteration\n",
-           kWarmUpIterations, warm_up_time, warm_up_time / kWarmUpIterations);
+    printf("%swarm-up: %u iterations in %.3f us, %.3f us per iteration\n",
+           kTestOutputPrefix, kWarmUpIterations,
+           warm_up_time, warm_up_time / kWarmUpIterations);
 
     float run_times[kNumTestRuns];
     for (unsigned i = 0; i < kNumTestRuns; ++i) {
@@ -71,13 +75,13 @@ void RunAndMeasure(const char* test_name, const char* spec_name,
     }
     float average = cumulative / kNumTestRuns;
 
-    printf("  - run: %u test runs, %u iterations per run\n",
-           kNumTestRuns, iterations);
-    printf("  - total (usec): min: %.3f, max: %.3f, ave: %.3f\n",
-           min, max, average);
-    printf("  - per-iteration (usec): min: %.3f\n",
+    printf("%srun: %u test runs, %u iterations per run\n",
+           kTestOutputPrefix, kNumTestRuns, iterations);
+    printf("%stotal (usec): min: %.3f, max: %.3f, ave: %.3f\n",
+           kTestOutputPrefix, min, max, average);
+    printf("%sper-iteration (usec): min: %.3f\n",
            // The static cast is to avoid a "may change value" warning.
-           min / static_cast<float>(iterations));
+           kTestOutputPrefix, min / static_cast<float>(iterations));
 }
 
 template <typename T>
