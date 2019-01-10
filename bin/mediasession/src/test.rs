decaace097d665b8a07cbb18468186f1f4055971
Payton Turnage <turnage@google.com>
2019-01-10 11:04:32 -0800

    [mediasession] Disable tests
    Disable pending MTWN-220
    
    TEST: fx run-test mediasession_tests
    Change-Id: I035231c12b631d7c5cee2247079a5d39db38a4b3

--- bin/mediasession/src/test.rs
+++ bin/mediasession/src/test.rs
@@ -2,4 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-mod integration;
+// TODO(turnage): Re-enable; MTWN-220
+// mod integration;
