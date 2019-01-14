a1ab423c33a32659cc3deeb69caafddfa627091d
Braden Kell <bradenkell@google.com>
2019-01-14 06:55:32 -0800

    [cleo][sensors] Add tests for LTR-578ALS sensor
    ZX-2788
    Test: runtests -t ltr-578als-test
    
    Change-Id: Idcbe1dc042732302dc8dfa1e8fc4e529ca140f82

--- system/dev/light/lite-on/rules.mk
+++ system/dev/light/lite-on/rules.mk
@@ -36,3 +36,42 @@ MODULE_BANJO_LIBS := \
     system/banjo/ddk-protocol-platform-device \
 
 include make/module.mk
+
+MODULE := $(LOCAL_DIR).test
+
+MODULE_NAME := ltr-578als-test
+
+MODULE_TYPE := usertest
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/ltr-578als.cpp \
+    $(LOCAL_DIR)/ltr-578als-test.cpp \
+
+MODULE_STATIC_LIBS := \
+    system/dev/lib/fake_ddk \
+    system/dev/lib/mock-hidbus-ifc \
+    system/dev/lib/mock-i2c \
+    system/ulib/ddk \
+    system/ulib/ddktl \
+    system/ulib/fbl \
+    system/ulib/fit \
+    system/ulib/hid \
+    system/ulib/simplehid \
+    system/ulib/sync \
+    system/ulib/zx \
+    system/ulib/zxcpp \
+
+MODULE_LIBS := \
+    system/ulib/driver \
+    system/ulib/c \
+    system/ulib/unittest \
+    system/ulib/zircon \
+
+MODULE_BANJO_LIBS := \
+    system/banjo/ddk-protocol-clk \
+    system/banjo/ddk-protocol-gpio \
+    system/banjo/ddk-protocol-hidbus \
+    system/banjo/ddk-protocol-i2c \
+    system/banjo/ddk-protocol-platform-device \
+
+include make/module.mk
