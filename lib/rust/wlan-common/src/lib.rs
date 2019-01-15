1ab054d02d845412cea2851e5593cce58db41acf
Jiwoong Lee <porce@google.com>
2019-01-15 14:23:42 -0800

    [wlan][common] Rust crate wlan-common
    This is a place holder to host common modules
    to be used for SME, MLME, and binaries written in Rust.
    
    WLAN-913
    Test: Build
    
    Change-Id: Ib62e5755455faaf17fc60b7bc6b16ae0e25b4a45

---
+++ lib/rust/wlan-common/src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Crate wlan-common hosts common libraries
+//! to be used for WLAN SME, MLME, and binaries written in Rust.
