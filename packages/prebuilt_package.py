33b82af5b1bc16b9383d0ad47c72901c234c4da0
Kevin Wells <kevinwells@google.com>
2019-01-15 18:23:31 -0800

    [build] Populate path field in blobs.json
    Now that "pm expand" can generate the blobs.json file, use its output
    instead of generating our own so this template/script doesn't need to
    parse the meta/contents to generate a correct package path for all the
    blobs in the package.
    
    Test: build
    Change-Id: I32039acfd257f358e90fb105d9ccab7a73c40063

--- packages/prebuilt_package.py
+++ packages/prebuilt_package.py
@@ -31,29 +31,18 @@ def findblobs(workdir):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--pm-tool', help='Path to pm tool')
-    parser.add_argument('--merkleroot-tool', help='Path to merkleroot tool')
     parser.add_argument('--name', help='Name of prebuilt package')
     parser.add_argument('--archive', help='Path to archive containing prebuilt package')
     parser.add_argument('--workdir', help='Path to working directory')
     parser.add_argument('--manifest', help='Manifest file to generate')
-    parser.add_argument('--blobs-rsp', help='Blobs response file to generate')
     parser.add_argument('--system-rsp', help='System response file to generate')
     parser.add_argument('--ids-txt', help='ids.txt file to generate')
-    parser.add_argument('--blobs-json', help='blobs.json file to generate')
-    parser.add_argument('--meta-merkle', help='meta.far.merkle file to generate')
 
     args = parser.parse_args()
 
     extract(args.pm_tool, args.archive, args.workdir)
 
-    meta_far = os.path.join(args.workdir, 'meta.far')
-
-    merkle = subprocess.check_output([args.merkleroot_tool, meta_far]).split()[0]
-
     bloblist = findblobs(args.workdir)
-
-    with open(args.meta_merkle, 'w') as f:
-        f.write(merkle)
 
     with open(args.manifest, 'w') as f:
         for blob in bloblist:
@@ -65,26 +54,6 @@ def main():
 
     with open(args.ids_txt, 'w') as f:
         f.truncate(0)
-
-    with open(args.blobs_json, 'w') as f:
-        blobs = []
-        blobs.append({"source_path": meta_far,
-                      "path": "meta/",
-                      "merkle": merkle,
-                      "size": os.path.getsize(meta_far)})
-        for blob in bloblist:
-            source_path = blob['path']
-            path = ''
-            merkle = blob['name']
-            size = blob['size']
-            blobs.append({"source_path": source_path,
-                          "path": path,
-                          "merkle": merkle,
-                          "size": size})
-        json.dump(blobs, f)
-
-    with open(args.blobs_rsp, 'w') as f:
-        f.write('--input=%s\n' % args.blobs_json)
 
     return 0
 
