af10981f29bb1d9e59c8e287aae4218efe71d139
Payton Turnage <turnage@google.com>
2019-01-17 15:24:33 -0800

    [mediacodec] Add missing cr header.
    TEST: no behaviour change.
    Change-Id: Ie70a7090740b7e4f9e614b2484b49a61c739eb51

--- bin/media/codecs/sw/ffmpeg/avcodec_context.h
+++ bin/media/codecs/sw/ffmpeg/avcodec_context.h
@@ -1,3 +1,7 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef GARNET_BIN_MEDIA_CODECS_SW_FFMPEG_AVCODEC_CONTEXT_H_
 #define GARNET_BIN_MEDIA_CODECS_SW_FFMPEG_AVCODEC_CONTEXT_H_
 
