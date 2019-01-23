vsrinivas/fuchsia

commit 50f37a986604d048f2cbba4dd51a746daab68274

"[garnet] Re-reland "Remove MakeCopyable dependencies""
This reverts commit d88a17fa05445bba54b5e8de4248999b0e1b38eb.

Reason for revert: Fixed one merge issue and rebuilt successfully

Original change's description:
> Revert "[garnet] Reland "Remove MakeCopyable dependencies""
> 
> This reverts commit 91ad810ecba010b25eda2e38e868a0f7ddead853.
> 
> Reason for revert: CQ didn't re-run tests after merge (due to new conflicts). Merged prematurely.
> 
> Original change's description:
> > [garnet] Reland "Remove MakeCopyable dependencies"
> > 
> > This reverts commit 077444b5ce477a01a0d268f82e0a0cb123b2c20b.
> > 
> > Reason for revert: relanding (revert revert) after resolving build error with merge of https://fuchsia-review.googlesource.com/c/peridot/+/245012
> > 
> > Original change's description:
> > > Revert "[garnet] Remove MakeCopyable dependencies"
> > >
> > > This reverts commit e5dee7e7d47a9d28092ad00c45f50192af4b18a5.
> > >
> > > Reason for revert: this CL breaks peridot (fxl::Closure no longer has
> > > the same semantics), which prevents garnet from rolling.
> > >
> > > Original change's description:
> > > > [garnet] Remove MakeCopyable dependencies
> > > >
> > > > [Confirmed this compiles under both x64 and arm64]
> > > >
> > > > This change replaces many std::function references with
> > > > the new fit::function, allowing functions to be passed with
> > > > move-only semantics, which is more efficient than "copy",
> > > > and safer than pass-by-reference. It also allows lambdas to
> > > > include move-only objects without requiring the MakeCopyable
> > > > workaround.
> > > >
> > > > Note that this covers all of garnet (except for eventually
> > > > removing MakeCopyable itself). Changes are forthcoming in
> > > > topaz and peridot.
> > > >
> > > > Issue: ZX-3289
> > > > Test: No behavior change
> > > > Change-Id: I3fbc402e37652f0dac807441b00ba935cf403a93
> > >
> > > TBR=abarth@google.com,richkadel@google.com
> > >

public/lib/escher/renderer/batch_gpu_uploader.cc
@@ -4,6 +4,8 @@
 
 #include "lib/escher/renderer/batch_gpu_uploader.h"
 
+#include <lib/fit/function.h>
+
 #include "lib/escher/util/trace_macros.h"
 #include "lib/escher/vk/gpu_mem.h"
 #include "lib/fxl/logging.h"
@@ -257,7 +259,7 @@ void BatchGpuUploader::PostWriter(
 
 void BatchGpuUploader::PostReader(
     std::unique_ptr<BatchGpuUploader::Reader> reader,
-    std::function<void(escher::BufferPtr buffer)> callback) {
+    fit::function<void(escher::BufferPtr buffer)> callback) {
   FXL_DCHECK(frame_);
   if (!reader) {
     return;
@@ -276,7 +278,7 @@ void BatchGpuUploader::PostReader(
   reader.reset();
 }
 
-void BatchGpuUploader::Submit(const std::function<void()>& callback) {
+void BatchGpuUploader::Submit(fit::function<void()> callback) {
   if (dummy_for_tests_) {
     FXL_LOG(WARNING) << "Dummy BatchGpuUploader for tests, skip submit";
     return;
@@ -298,8 +300,7 @@ void BatchGpuUploader::Submit(const std::function<void()>& callback) {
                    [callback, read_callbacks = std::move(read_callbacks_)]() {
                      for (auto& pair : read_callbacks) {
                        auto buffer = pair.first;
-                       auto read_callback = pair.second;
-                       read_callback(buffer);
+                       pair.second(buffer);
                      }
                      if (callback) {
                        callback();

commit 19d1bb013dd306642078ecc3edd1e0722b45b838

[escher] use a move-closure
This change allows this code to compile once more. It was previously
broken in 50f37a986604d048f2cbba4dd51a746daab68274.

Test: None
Change-Id: I52d515af894fa2f42c1b0a7d57e2cb5737a88cba

public/lib/escher/renderer/batch_gpu_uploader.cc
@@ -297,7 +297,8 @@ void BatchGpuUploader::Submit(fit::function<void()> callback) {
 
   TRACE_DURATION("gfx", "BatchGpuUploader::SubmitBatch");
   frame_->EndFrame(std::move(batched_commands_done_semaphore_),
-                   [callback, read_callbacks = std::move(read_callbacks_)]() {
+                   [callback = std::move(callback),
+                    read_callbacks = std::move(read_callbacks_)]() {
                      for (auto& pair : read_callbacks) {
                        auto buffer = pair.first;
                        pair.second(buffer);