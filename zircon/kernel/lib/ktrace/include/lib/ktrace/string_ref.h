630257cf70c45e98669473812af54a7ad3d1f031
Corey Tabaka <eieio@google.com>
2019-01-22 16:32:54 -0800

    [kernel][ktrace] Add StringRef type to handle tracer strings.
    Add the StringRef type to handle internalized strings with ids for
    more generalized support of the ktrace subsystem. This is intented
    to replace the current method of defining tracer strings in do-
    while-false blocks that is incompatible with RAII and other scope-
    aware tracing.
    
    Due to a limitation in how section attributes are currently
    handled, this implementation uses a runtime lock-free, wait-free
    linked list to register strings in the string table. This can be
    removed in favor of a linker script-defined array once Clang
    and/or GCC have the appropriate support.
    
    Bug: ZX-3518
    Test: Manual testing in a subsequent integration patch.
    Change-Id: Ie529ca37509adc795b8ae5426c4c34250c1eacb6

---
+++ zircon/kernel/lib/ktrace/include/lib/ktrace/string_ref.h
@@ -0,0 +1,66 @@
+// Copyright 2019 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <ktl/atomic.h>
+#include <zircon/types.h>
+
+// Represents an internalized string that may be referenced in traces by id to
+// improve the efficiency of labels and other strings. This type does not define
+// constructors or a destructor and contains trivially constructible members so
+// that it may be aggregate-initialized to avoid static initializers and guards.
+struct StringRef {
+    static constexpr int kInvalidId = -1;
+
+    const char* string{nullptr};
+    ktl::atomic<int> id{kInvalidId};
+    StringRef* next{nullptr};
+
+    // Returns the numeric id for this string ref. If this is the first runtime
+    // encounter with this string ref a new id is generated and the string ref
+    // is added to the global linked list.
+    int GetId() {
+        const int ref_id = id.load(ktl::memory_order_relaxed);
+        return ref_id == kInvalidId ? Register(this) : ref_id;
+    }
+
+    // Returns the head of the global string ref linked list.
+    static StringRef* head() { return head_.load(ktl::memory_order_acquire); }
+
+private:
+    // TODO(ZX-3498): Replace runtime lock-free linked list with comdat linker
+    // sections once the toolchain supports it.
+    static int Register(StringRef* string_ref);
+
+    static ktl::atomic<int> id_counter_;
+    static ktl::atomic<StringRef*> head_;
+};
+
+// String literal template operator that generates a unique StringRef instance
+// for the given string literal. Every invocation for a given string literal
+// value returns the same StringRef instance, such that the set of StringRef
+// instances behaves as an internalized string table.
+//
+// This implementation uses the N3599 extension supported by Clang and GCC.
+// C++20 ratified a slightly different syntax that is simple to switch to, once
+// available, without affecting call sites.
+// TODO(ZX-3488): Update to C++20 syntax when available.
+//
+// References:
+//     http://open-std.org/JTC1/SC22/WG21/docs/papers/2013/n3599.html
+//     http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/p0424r2.pdf
+//
+// Example:
+//     ktrace_probe("probe_name"_stringref, ...);
+//
+template <typename T, T... chars>
+inline StringRef* operator""_stringref() {
+    static const char storage[] = {chars..., '\0'};
+    static StringRef string_ref{storage};
+    return &string_ref;
+}
+
---
+++ zircon/kernel/lib/ktrace/string_ref.cpp
@@ -0,0 +1,49 @@
+// Copyright 2019 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <ktl/atomic.h>
+#include <lib/ktrace.h>
+#include <lib/ktrace/string_ref.h>
+
+int StringRef::Register(StringRef* string_ref) {
+    // Return the id if the string ref is already registered.
+    int id = string_ref->id.load(ktl::memory_order_relaxed);
+    if (id != kInvalidId) {
+        return id;
+    }
+
+    // Try to set the id of the string ref. When there is a race with other
+    // threads or CPUs only one will succeed, in which case the id counter will
+    // harmlessly skip values equal to the number of agents racing between here
+    // and the check above.
+    const int new_id = id_counter_.fetch_add(1, ktl::memory_order_relaxed);
+    while (!string_ref->id.compare_exchange_weak(id, new_id,
+                                                 ktl::memory_order_relaxed,
+                                                 ktl::memory_order_relaxed)) {
+        // If another agent set the id first simply return the id.
+        if (id != kInvalidId && id != new_id)
+            return id;
+    }
+
+    // Emit a name record the first time this string ref is encountered at
+    // runtime. This is ignored if tracing is not active and is replayed at the
+    // beginning of subsequent tracing sessions.
+    ktrace_name_etc(TAG_PROBE_NAME, string_ref->id, 0, string_ref->string, true);
+
+    // Register the string ref in the global linked list. When there is a race
+    // above only the winning agent that set the id will continue to this point.
+    string_ref->next = head_.load(ktl::memory_order_relaxed);
+    while (!head_.compare_exchange_weak(string_ref->next, string_ref,
+                                        ktl::memory_order_release,
+                                        ktl::memory_order_relaxed)) {
+    }
+
+    return new_id;
+}
+
+ktl::atomic<int> StringRef::id_counter_{StringRef::kInvalidId + 1};
+ktl::atomic<StringRef*> StringRef::head_{nullptr};
+
