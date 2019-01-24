0569dad21dad7562a0040d79b0170e824b2577b1
Venkatesh Srinivas <venkateshs@google.com>
2019-01-24 23:06:57 +0000

    [scsilib] Definitions of INQUIRY, MODE SENSE (6) commands
    Structure definitions for the INQUIRY and MODE SENSE (6) commands.
    
    * INQUIRY is used to get standard product data for all SCSI Targets and
      Vital Product Data pages (not yet supported). It is used extensively
      during the SCSI target probe and enumeration phase.
    
    * MODE SENSE (6) lets us read SCSI target readonly state and whether/not
      'Force Unit Access' is available.
    
    ZX-2314
    
    Tested: Built
    
    Change-Id: I2e258533e86643758b13710d0864c249a200b3b5

--- system/dev/bus/virtio/scsilib.h
+++ system/dev/bus/virtio/scsilib.h
@@ -31,6 +31,67 @@ struct TestUnitReadyCDB {
 
 static_assert(sizeof(TestUnitReadyCDB) == 6, "TestUnitReady CDB must be 6 bytes");
 
+struct InquiryCDB {
+    Opcode opcode;
+    // reserved_and_evpd(0) is 'Enable Vital Product Data'
+    uint8_t reserved_and_evpd;
+    uint8_t page_code;
+    // allocation_length is in network-byte-order.
+    uint16_t allocation_length;
+    uint8_t control;
+} __PACKED;
+
+static_assert(sizeof(InquiryCDB) == 6, "Inquiry CDB must be 6 bytes");
+
+// Standard INQUIRY Data Header.
+struct InquiryData {
+   // Peripheral Device Type Header and qualifier.
+   uint8_t peripheral_device_type;
+   // removable(7) is the 'Removable' bit.
+   uint8_t removable;
+   uint8_t version;
+   // response_data_format_and_control(3 downto 0) is Response Data Format
+   // response_data_format_and_control(4) is HiSup
+   // response_data_format_and_control(5) is NormACA
+   uint8_t response_data_format_and_control;
+   uint8_t additional_length;
+   // Various control bits, unused currently.
+   uint8_t control[3];
+   uint8_t t10_vendor_id[8];
+   uint8_t product_id[16];
+   uint8_t product_revision[4];
+   uint8_t drive_serial_number[8];
+} __PACKED;
+
+static_assert(offsetof(InquiryData, t10_vendor_id) == 8, "T10 Vendor ID is at offset 8");
+static_assert(offsetof(InquiryData, product_id) == 16, "Product ID is at offset 16");
+
+struct ModeSense6CDB {
+    Opcode opcode;
+    // If disable_block_descriptors(4) is '1', device will not return Block Descriptors.
+    uint8_t disable_block_descriptors;
+    // page_code(7 downto 6) is 'page control'. Should be 00h for current devices.
+    uint8_t page_code;
+    uint8_t subpage_code;
+    uint8_t allocation_length;
+    uint8_t control;
+} __PACKED;
+
+static_assert(sizeof(ModeSense6CDB) == 6, "Mode Sense 6 CDB must be 6 bytes");
+
+struct ModeSense6ParameterHeader {
+    uint8_t mode_data_length;
+    // 00h is 'Direct Access Block Device'
+    uint8_t medium_type;
+    // For Direct Access Block Devices:
+    // device_specific_parameter(7) is write-protected bit
+    // device_specific_parameter(4) is disable page out/force unit access available
+    uint8_t device_specific_parameter;
+    uint8_t block_descriptor_length;
+} __PACKED;
+
+static_assert(sizeof(ModeSense6ParameterHeader) == 4, "Mode sense 6 parameters must be 4 bytes");
+
 class Disk;
 using DeviceType = ddk::Device<Disk, ddk::GetSizable, ddk::Unbindable>;
 
