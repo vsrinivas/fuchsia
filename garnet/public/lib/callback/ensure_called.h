7a16cb3eb013f28cb7fc63cf75df27763696f811
Ambre Williams <ambre@google.com>
2019-01-24 15:24:05 +0100

    [callback] Add callback::EnsureCalled
    EnsureCalled wraps a callback to ensure that it is called with default
    arguments on destruction if it is not called otherwise.
    
    TEST=unittests (fx run-test garnet_lib_tests)
    
    Change-Id: I791d35f44667d66db5c11bee29db21fd63a69bfb

---
+++ garnet/public/lib/callback/ensure_called.h
@@ -0,0 +1,73 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_CALLBACK_ENSURE_CALLED_H_
+#define LIB_CALLBACK_ENSURE_CALLED_H_
+
+#include <optional>
+#include <tuple>
+#include <utility>
+
+#include "lib/fxl/logging.h"
+#include "lib/fxl/macros.h"
+
+namespace callback {
+
+template <typename T, typename... ArgType>
+class EnsureCalled {
+ public:
+  EnsureCalled() = default;
+
+  explicit EnsureCalled(T&& function, ArgType&&... args)
+      : closure_(std::pair(
+            std::move(function),
+            std::tuple<ArgType...>(std::forward<ArgType>(args)...))){};
+
+  EnsureCalled(EnsureCalled&& other) { *this = std::move(other); }
+
+  EnsureCalled& operator=(EnsureCalled&& other) {
+    CallDefaultIfNeeded();
+
+    // Assigning |closure_| directly does not work because lambdas are
+    // move-constructible but not move-assignable. We construct a new pair in
+    // place instead.
+    if (other.closure_) {
+      closure_.emplace(*other.TakeClosure());
+    }
+    return *this;
+  }
+
+  ~EnsureCalled() { CallDefaultIfNeeded(); }
+
+  auto operator()(ArgType... args) {
+    FXL_DCHECK(closure_);
+    auto closure = TakeClosure();
+    return std::invoke(std::move(closure->first),
+                       std::forward<ArgType>(args)...);
+  }
+
+  explicit operator bool() const { return closure_.has_value(); }
+
+ private:
+  void CallDefaultIfNeeded() {
+    if (!closure_) {
+      return;
+    }
+
+    auto closure = TakeClosure();
+    std::apply(std::move(closure->first), std::move(closure->second));
+  }
+
+  std::optional<std::pair<T, std::tuple<ArgType...>>> TakeClosure() {
+    auto closure = std::move(closure_);
+    closure_ = std::nullopt;
+    return closure;
+  }
+
+  std::optional<std::pair<T, std::tuple<ArgType...>>> closure_;
+};
+
+}  // namespace callback
+
+#endif  // LIB_CALLBACK_ENSURE_CALLED_H_
---
+++ garnet/public/lib/callback/ensure_called_unittest.cc
@@ -0,0 +1,100 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "lib/callback/ensure_called.h"
+
+#include <tuple>
+#include <utility>
+
+#include "gtest/gtest.h"
+#include "lib/callback/capture.h"
+#include "lib/callback/set_when_called.h"
+#include "lib/fxl/macros.h"
+
+namespace callback {
+namespace {
+
+TEST(EnsureCalled, NormalCall) {
+  bool called;
+  int called_with = 0;
+
+  {
+    auto callable = EnsureCalled(
+        callback::Capture(callback::SetWhenCalled(&called), &called_with), 1);
+    EXPECT_TRUE(callable);
+
+    callable(2);
+    EXPECT_TRUE(called);
+    EXPECT_EQ(called_with, 2);
+
+    EXPECT_FALSE(callable);
+    called = false;
+  }
+
+  EXPECT_FALSE(called);
+}
+
+TEST(EnsureCalled, DestructorCall) {
+  bool called;
+  int called_with = 0;
+
+  {
+    auto call = EnsureCalled(
+        callback::Capture(callback::SetWhenCalled(&called), &called_with), 1);
+  }
+
+  EXPECT_TRUE(called);
+  EXPECT_EQ(called_with, 1);
+}
+
+TEST(EnsureCalled, MoveAssign) {
+  bool called_internal;
+  bool called_external;
+
+  auto make_callback = [](bool* called) {
+    return [called]() { *called = true; };
+  };
+  auto callback_internal = make_callback(&called_internal);
+  auto callback_external = make_callback(&called_external);
+
+  auto external = EnsureCalled(std::move(callback_external));
+
+  {
+    auto internal = EnsureCalled(std::move(callback_internal));
+    external = std::move(internal);
+    EXPECT_FALSE(internal);
+    EXPECT_TRUE(called_external);
+  }
+  EXPECT_FALSE(called_internal);
+
+  external();
+  EXPECT_TRUE(called_internal);
+}
+
+TEST(EnsureCalled, MoveConstruct) {
+  bool called;
+
+  {
+    auto callback = EnsureCalled(callback::SetWhenCalled(&called));
+
+    {
+      auto callback2 = std::move(callback);
+      EXPECT_FALSE(callback);
+      EXPECT_TRUE(callback2);
+      EXPECT_FALSE(called);
+    }
+    EXPECT_TRUE(called);
+    called = false;
+  }
+  EXPECT_FALSE(called);
+}
+
+TEST(EnsureCalled, EnsureCalledReturn) {
+  auto callback = EnsureCalled([] { return true; });
+  bool b = callback();
+  EXPECT_TRUE(b);
+}
+
+}  // namespace
+}  // namespace callback
