0ff93f55b0edc34528a7e7212edf8023a031d962
Zach Bush <zmbush@google.com>
2019-01-29 12:18:13 -0800

    [lib/cobalt] Add utils for constructing cobalt::ProjectProfile
    Adds utilites to load from, filename, string, base64 string, and Vmo.
    
    Test: public/lib/cobalt/cpp/project_profile_unittest.cc
    Change-Id: I441ce061348f62184d5e32acc4f8852ae1941da9

---
+++ public/lib/cobalt/cpp/project_profile.cc
@@ -0,0 +1,42 @@
+#include "garnet/public/lib/cobalt/cpp/project_profile.h"
+
+#include "lib/fsl/vmo/file.h"
+#include "lib/fsl/vmo/strings.h"
+#include "third_party/cobalt/util/crypto_util/base64.h"
+
+namespace cobalt {
+
+fuchsia::cobalt::ProjectProfile ProjectProfileFromBase64String(
+    const std::string &encoded_cfg) {
+  std::string cfg;
+  cobalt::crypto::Base64Decode(encoded_cfg, &cfg);
+
+  return ProjectProfileFromString(cfg);
+}
+
+fuchsia::cobalt::ProjectProfile ProjectProfileFromString(
+    const std::string &cfg) {
+  fsl::SizedVmo config_vmo;
+  bool success = fsl::VmoFromString(cfg, &config_vmo);
+  FXL_CHECK(success)
+      << "Could not convert Cobalt metrics registry string into VMO";
+
+  return ProjectProfileFromVmo(std::move(config_vmo));
+}
+
+fuchsia::cobalt::ProjectProfile ProjectProfileFromFile(
+    const std::string &filename) {
+  fsl::SizedVmo config_vmo;
+  bool success = fsl::VmoFromFilename(filename, &config_vmo);
+  FXL_CHECK(success) << "Could not read Cobalt metrics registry file into VMO";
+
+  return ProjectProfileFromVmo(std::move(config_vmo));
+}
+
+fuchsia::cobalt::ProjectProfile ProjectProfileFromVmo(fsl::SizedVmo vmo) {
+  fuchsia::cobalt::ProjectProfile profile;
+  profile.config = std::move(vmo).ToTransport();
+  return profile;
+}
+
+}  // namespace cobalt
