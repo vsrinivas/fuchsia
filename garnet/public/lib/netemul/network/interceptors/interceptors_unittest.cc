acf9a75fafef62b8f66a5d4a2006f7758f8a01ae
Bruno Dal Bo <brunodalbo@google.com>
2019-01-29 13:29:23 -0800

    [netemul] Network conditions - Latency
    Adds emulated packet delivery latency foundation. Will be hooked into
    Network interface's NetworkConfig.
    
    TEST: Added unit test to interceptors tests. Run with fx run-test
    netemul_lib_test
    
    Change-Id: Ic5d7da2e0864767f868196ceca5451ad28aebec6

--- garnet/public/lib/netemul/network/interceptors/interceptors_unittest.cc
+++ garnet/public/lib/netemul/network/interceptors/interceptors_unittest.cc
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "lib/gtest/real_loop_fixture.h"
+#include <lib/gtest/real_loop_fixture.h>
+#include "latency.h"
 #include "packet_loss.h"
 
 namespace netemul {
@@ -10,6 +11,17 @@ namespace testing {
 
 uint8_t gNextRandomNumber = 0;
 uint8_t TestRNG() { return gNextRandomNumber; }
+
+class ControlledRand {
+ public:
+  ControlledRand(int64_t, int64_t) {}
+
+  int64_t Next() { return NextRand; }
+
+  static int64_t NextRand;
+};
+
+int64_t ControlledRand::NextRand = 0;
 
 class InterceptorsTest : public gtest::RealLoopFixture {
  public:
@@ -67,6 +79,69 @@ TEST_F(InterceptorsTest, PacketLossControlledRand) {
   gNextRandomNumber = 49;
   loss.Intercept(MakeSingleBytePacket(static_cast<uint8_t>(1)));
   EXPECT_EQ(pass_count, 2);
+}
+
+TEST_F(InterceptorsTest, LatencyRealRand) {
+  int pass_count = 0;
+  interceptor::Latency latency(
+      5, 1, [&pass_count](InterceptPacket packet) { pass_count++; });
+
+  for (int i = 0; i < 5; i++) {
+    latency.Intercept(MakeSingleBytePacket(static_cast<uint8_t>(i)));
+  }
+
+  EXPECT_EQ(pass_count, 0);
+  ASSERT_TRUE(RunLoopWithTimeoutOrUntil(
+      [&pass_count]() { return pass_count == 5; }, zx::sec(2)));
+}
+
+TEST_F(InterceptorsTest, LatencyControlledRand) {
+  ControlledRand::NextRand = 10;
+  int pass_count = 0;
+  zx::time after;
+  interceptor::Latency<ControlledRand> latency(
+      0, 0, [&pass_count, &after](InterceptPacket packet) {
+        pass_count++;
+        after = zx::clock::get_monotonic();
+      });
+  auto bef = zx::clock::get_monotonic();
+  latency.Intercept(MakeSingleBytePacket(static_cast<uint8_t>(0)));
+
+  EXPECT_EQ(pass_count, 0);
+  ASSERT_TRUE(RunLoopWithTimeoutOrUntil(
+      [&pass_count]() { return pass_count == 1; }, zx::sec(2)));
+
+  auto diff = (after - bef).to_msecs();
+  std::cout << "measured latency = " << diff << "ms" << std::endl;
+  // the "diff" time should be ~10ms based on the value set on NextRand.
+  // check that it's between 5 and 15.
+  EXPECT_TRUE(diff >= 5 && diff <= 15);
+}
+
+TEST_F(InterceptorsTest, LatencyFlush) {
+  int pass_count = 0;
+  interceptor::Latency latency(
+      15, 0, [&pass_count](InterceptPacket packet) { pass_count++; });
+
+  for (int i = 0; i < 5; i++) {
+    latency.Intercept(MakeSingleBytePacket(static_cast<uint8_t>(i)));
+  }
+  EXPECT_EQ(pass_count, 0);
+
+  // flush all packets
+  auto vec = latency.Flush();
+  EXPECT_EQ(vec.size(), 5ul);
+  uint8_t c = 0;
+  for (auto& p : vec) {
+    EXPECT_EQ(p.data()[0], c);
+    c++;
+  }
+
+  // give it enough time so it *would* call the callback
+  RunLoopWithTimeout(zx::msec(20));
+
+  // no packets should have passed
+  EXPECT_EQ(pass_count, 0);
 }
 
 }  // namespace testing
