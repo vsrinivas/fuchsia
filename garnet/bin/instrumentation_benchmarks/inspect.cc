7ef6e658d72851cdda857256457052657be3c099
Chris Phoenix <cphoenix@google.com>
2019-01-29 20:54:25 -0800

    [benchmarks] Expose Property operations
    Lifecycle, Set, and ToFidl, for small and big std::string and ByteVector.
    Lifecycle and Set for callback Properties
    
    Test: /pkgfs/packages/instrumentation_benchmarks/0/test/instrumentation_benchmarks -p
    CF-205 #progress
    
    Change-Id: I5d486b26c5fb64ace85c512cb8e916de4a285b36

--- garnet/bin/instrumentation_benchmarks/inspect.cc
+++ garnet/bin/instrumentation_benchmarks/inspect.cc
@@ -25,6 +25,7 @@ using component::IntMetric;
 using component::UIntMetric;
 using component::DoubleMetric;
 using component::CallbackMetric;
+using component::Property;
 
 const char kValue[] = "value";
 const int kSmallPropertySize = 8;
@@ -334,6 +335,90 @@ bool TestCallbackMetricToFidl(perftest::RepeatState* state) {
   return true;
 }
 
+bool TestStringPropertyLifecycle(perftest::RepeatState* state, int size) {
+  std::string data;
+  data.resize(size, 'a');
+  state->DeclareStep("Create");
+  state->DeclareStep("Destroy");
+  while (state->KeepRunning()) {
+    Property item(data);
+    state->NextStep();
+  }
+  return true;
+}
+
+bool TestStringPropertySet(perftest::RepeatState* state, int size) {
+  std::string data;
+  data.resize(size, 'a');
+  Property item(data);
+  while (state->KeepRunning()) {
+    item.Set(data);
+  }
+  return true;
+}
+
+bool TestStringPropertyToFidl(perftest::RepeatState* state, int size) {
+  std::string data;
+  data.resize(size, 'a');
+  Property item(data);
+  while (state->KeepRunning()) {
+    perftest::DoNotOptimize(item.ToFidl("a_name"));
+  }
+  return true;
+}
+
+bool TestVectorPropertyLifecycle(perftest::RepeatState* state, int size) {
+  ByteVector data;
+  data.resize(size, 'a');
+  state->DeclareStep("Create");
+  state->DeclareStep("Destroy");
+  while (state->KeepRunning()) {
+    Property item(data);
+    state->NextStep();
+  }
+  return true;
+}
+
+bool TestVectorPropertySet(perftest::RepeatState* state, int size) {
+  ByteVector data;
+  data.resize(size, 'a');
+  Property item(data);
+  while (state->KeepRunning()) {
+    item.Set(data);
+  }
+  return true;
+}
+
+bool TestVectorPropertyToFidl(perftest::RepeatState* state, int size) {
+  ByteVector data;
+  data.resize(size, 'a');
+  Property item(data);
+  while (state->KeepRunning()) {
+    perftest::DoNotOptimize(item.ToFidl("a_name"));
+  }
+  return true;
+}
+
+bool TestCallbackPropertyLifecycle(perftest::RepeatState* state) {
+  state->DeclareStep("Create");
+  state->DeclareStep("Destroy");
+  while (state->KeepRunning()) {
+    Property item = Property([]() { return "a"; });
+    state->NextStep();
+  }
+  return true;
+}
+
+bool TestCallbackPropertySet(perftest::RepeatState* state) {
+  Property item = Property([]() { return "a"; });
+  while (state->KeepRunning()) {
+    item.Set([]() { return "a"; });
+  }
+  return true;
+}
+
+// TODO(maybe): Profile ToFidl() of callback Property, with big/small string/vector?
+
 // /pkgfs/packages/instrumentation_benchmarks/0/test/instrumentation_benchmarks -p
 
 void RegisterTests() {
@@ -382,6 +467,49 @@ void RegisterTests() {
   perftest::RegisterTest("Expose/CallbackMetric/Set", TestCallbackMetricSet);
   perftest::RegisterTest("Expose/CallbackMetric/ToString", TestCallbackMetricToString);
   perftest::RegisterTest("Expose/CallbackMetric/ToFidl", TestCallbackMetricToFidl);
+
+  perftest::RegisterTest(fxl::StringPrintf("Expose/StringProperty/Lifecycle/%d",
+                                           kSmallPropertySize).c_str(),
+                         TestStringPropertyLifecycle, kSmallPropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/StringProperty/Set/%d",
+                                           kSmallPropertySize).c_str(),
+                         TestStringPropertySet, kSmallPropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/StringProperty/ToFidl/%d",
+                                           kSmallPropertySize).c_str(),
+                       TestStringPropertyToFidl, kSmallPropertySize);
+
+  perftest::RegisterTest(fxl::StringPrintf("Expose/StringProperty/Lifecycle/%d",
+                                           kLargePropertySize).c_str(),
+                         TestStringPropertyLifecycle, kLargePropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/StringProperty/Set/%d",
+                                           kLargePropertySize).c_str(),
+                         TestStringPropertySet, kLargePropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/StringProperty/ToFidl/%d",
+                                           kLargePropertySize).c_str(),
+                       TestStringPropertyToFidl, kLargePropertySize);
+
+  perftest::RegisterTest(fxl::StringPrintf("Expose/VectorProperty/Lifecycle/%d",
+                                           kSmallPropertySize).c_str(),
+                       TestVectorPropertyLifecycle, kSmallPropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/VectorProperty/Set/%d",
+                                           kSmallPropertySize).c_str(),
+                       TestVectorPropertySet, kSmallPropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/VectorProperty/ToFidl/%d",
+                                           kSmallPropertySize).c_str(),
+                       TestVectorPropertyToFidl, kSmallPropertySize);
+
+  perftest::RegisterTest(fxl::StringPrintf("Expose/VectorProperty/Lifecycle/%d",
+                                           kLargePropertySize).c_str(),
+                       TestVectorPropertyLifecycle, kLargePropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/VectorProperty/Set/%d",
+                                           kLargePropertySize).c_str(),
+                       TestVectorPropertySet, kLargePropertySize);
+  perftest::RegisterTest(fxl::StringPrintf("Expose/VectorProperty/ToFidl/%d",
+                                           kLargePropertySize).c_str(),
+                       TestVectorPropertyToFidl, kLargePropertySize);
+
+  perftest::RegisterTest("Expose/CallbackProperty/Lifecycle", TestCallbackPropertyLifecycle);
+  perftest::RegisterTest("Expose/CallbackProperty/Set", TestCallbackPropertySet);
 }
 PERFTEST_CTOR(RegisterTests);
 
