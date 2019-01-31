b5fb1c26b2c0a7b3b5a316b8a66710e81cb10fc7
Tamir Duberstein <tamird@gmail.com>
2019-01-30 19:48:05 -0500

    [netstack] simplify connect_twice_test
    Test: fx run-image-test connect_twice_test
    Change-Id: I206999092af748b484c07da5df4fe7d7632443b5

--- go/src/netstack/tests/manual/connect_twice_test.c
+++ go/src/netstack/tests/manual/connect_twice_test.c
@@ -7,25 +7,28 @@
 #include <sys/socket.h>
 
 int main() {
-  int ret;
-
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    fprintf(stderr, "socket failed: %s\n", strerror(errno));
+    exit(1);
+  }
+
   struct sockaddr_in addr = {
     .sin_family = AF_INET,
     .sin_addr = {
       .s_addr = htonl(INADDR_LOOPBACK),
     },
   };
-  if ((ret = bind(server_fd, (const struct sockaddr*)&addr, sizeof(addr))) != 0) {
+  if (bind(server_fd, (const struct sockaddr*)&addr, sizeof(addr)) != 0) {
     fprintf(stderr, "bind failed: %s\n", strerror(errno));
     exit(1);
   }
   socklen_t addrLen = sizeof(addr);
-  if ((ret = getsockname(server_fd, (struct sockaddr*)&addr, &addrLen)) != 0) {
+  if (getsockname(server_fd, (struct sockaddr*)&addr, &addrLen) != 0) {
     fprintf(stderr, "getsockname failed: %s\n", strerror(errno));
     exit(1);
   }
-  if ((ret = listen(server_fd, 1)) != 0) {
+  if (listen(server_fd, 1) != 0) {
     fprintf(stderr, "listen failed: %s\n", strerror(errno));
     exit(1);
   }
@@ -35,13 +38,13 @@ int main() {
   struct sockaddr_in bad_addr = addr;
   bad_addr.sin_port++;
 
-  if ((ret = connect(client_fd, (const struct sockaddr*)&bad_addr, sizeof(bad_addr))) == 0) {
+  if (connect(client_fd, (const struct sockaddr*)&bad_addr, sizeof(bad_addr)) == 0) {
     fprintf(stderr, "bad connect succeeded\n");
     exit(1);
   }
   fprintf(stderr, "connect failed as expected: %s\n", strerror(errno));
 
-  if ((ret = connect(client_fd, (const struct sockaddr*)&addr, sizeof(addr))) != 0) {
+  if (connect(client_fd, (const struct sockaddr*)&addr, sizeof(addr)) != 0) {
     fprintf(stderr, "connect failed: %s\n", strerror(errno));
     exit(1);
   }
