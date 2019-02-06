4c6725dda564cc19f1878a784fdda790c3197b2f
Francois Rousseau <frousseau@google.com>
2019-02-05 18:27:43 -0800

    [backtrace-request] add comments to explain the magic
    TESTED=just comments
    
    Change-Id: Ie2e47202311995102d22ec8a8de42b602672d57d

--- zircon/system/ulib/backtrace-request/include/lib/backtrace-request/backtrace-request.h
+++ zircon/system/ulib/backtrace-request/include/lib/backtrace-request/backtrace-request.h
@@ -7,20 +7,33 @@
 
 #include <stdint.h>
 
+// Special value we put in the first register to let the exception handler know
+// that we are just requesting a backtrace and we should resume the thread.
 #define BACKTRACE_REQUEST_MAGIC ((uint64_t)0xee726573756d65ee)
 
-// Invoke this function to cause crashlogger to print a backtrace
-// and resume the thread without killing the process.
-
+// Prints a backtrace, resuming the thread without killing the process.
 static inline void backtrace_request(void) {
+    // Two instructions: one that sets a software breakpoint ("int3" on x64,
+    // "brk" on arm64) and one that writes the "magic" value in the first
+    // register ("a" on x64, "x0" on arm64).
+    //
+    // We set a software breakpoint to trigger the exception handling in
+    // crashsvc, which will print the debug info, including the backtrace.
+    //
+    // We write the "magic" value in the first register so that the exception
+    // handler can check for it and resume the thread if present.
 #ifdef __x86_64__
-    __asm__ ("int3" : : "a" (BACKTRACE_REQUEST_MAGIC));
+    __asm__("int3"
+            :
+            : "a"(BACKTRACE_REQUEST_MAGIC));
 #endif
 #ifdef __aarch64__
     // This is what gdb uses.
-    __asm__ ("mov x0, %0\n"
-             "\tbrk 0"
-             : : "r" (BACKTRACE_REQUEST_MAGIC) : "x0");
+    __asm__("mov x0, %0\n"
+            "\tbrk 0"
+            :
+            : "r"(BACKTRACE_REQUEST_MAGIC)
+            : "x0");
 #endif
 }
 
