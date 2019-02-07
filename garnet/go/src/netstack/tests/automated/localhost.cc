f31219c47fc3c71d207909e6d2f1c6d529954943
Tamir Duberstein <tamird@gmail.com>
2019-02-07 16:48:16 -0500

    [netstack] move tests into a single file
    Test: None
    Change-Id: Ibc9a2b684a72e40d034181241b49eb00e1f76ca1

--- garnet/go/src/netstack/tests/automated/localhost.cc
+++ garnet/go/src/netstack/tests/automated/localhost.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,33 @@
 #include <netdb.h>
 
 #include "gtest/gtest.h"
+
+TEST(AcceptTest, Localhost) {
+  int serverfd = socket(AF_INET6, SOCK_STREAM, 0);
+  ASSERT_GE(serverfd, 0) << strerror(errno);
+
+  struct sockaddr_in6 serveraddr = {};
+  serveraddr.sin6_family = AF_INET6;
+  serveraddr.sin6_addr = IN6ADDR_LOOPBACK_INIT;
+  socklen_t serveraddrlen = sizeof(serveraddr);
+  ASSERT_EQ(bind(serverfd, (sockaddr*)&serveraddr, serveraddrlen), 0)
+      << strerror(errno);
+  ASSERT_EQ(getsockname(serverfd, (sockaddr*)&serveraddr, &serveraddrlen), 0)
+      << strerror(errno);
+  ASSERT_EQ(serveraddrlen, sizeof(serveraddr));
+  ASSERT_EQ(listen(serverfd, 1), 0) << strerror(errno);
+
+  int clientfd = socket(AF_INET6, SOCK_STREAM, 0);
+  ASSERT_GE(clientfd, 0) << strerror(errno);
+  ASSERT_EQ(connect(clientfd, (sockaddr*)&serveraddr, serveraddrlen), 0)
+      << strerror(errno);
+
+  struct sockaddr_in connaddr;
+  socklen_t connaddrlen = sizeof(connaddr);
+  int connfd = accept(serverfd, (sockaddr*)&connaddr, &connaddrlen);
+  ASSERT_GE(connfd, 0) << strerror(errno);
+  ASSERT_GT(connaddrlen, sizeof(connaddr));
+}
 
 TEST(GetAddrInfoTest, Localhost) {
   struct addrinfo hints;
@@ -53,4 +80,15 @@ TEST(GetAddrInfoTest, Localhost) {
   }
   EXPECT_EQ(i, 2);
   freeaddrinfo(result);
+}
+
+TEST(GetSockNameTest, Localhost) {
+  int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+  ASSERT_GE(sockfd, 0) << strerror(errno);
+
+  struct sockaddr sa;
+  socklen_t len = sizeof(sa);
+  ASSERT_EQ(getsockname(sockfd, &sa, &len), 0) << strerror(errno);
+  ASSERT_GT(len, sizeof(sa));
+  ASSERT_EQ(sa.sa_family, AF_INET6);
 }
