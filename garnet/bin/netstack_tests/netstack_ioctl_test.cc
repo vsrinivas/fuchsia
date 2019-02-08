32b87bd0913ede9910622c323fb72dd28915383c
Bert Muthalaly <stijlist@google.com>
2019-02-08 10:07:14 -0500

    [netstack] Create hermetic ioctl tests
    Bug: NET-2083
    Tested: fx run-test netstack_integration_tests -- --gtest_filter="*NetstackIoctl*"
    Change-Id: I2ec4a2c024655449396a53cc8e9832f52cbd3c87

---
+++ garnet/bin/netstack_tests/netstack_ioctl_test.cc
@@ -0,0 +1,119 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <ddk/protocol/ethernet.h>
+#include <fbl/auto_call.h>
+#include <fuchsia/hardware/ethernet/cpp/fidl.h>
+#include <fuchsia/net/stack/cpp/fidl.h>
+#include <fuchsia/netstack/cpp/fidl.h>
+#include <lib/fdio/util.h>
+#include <lib/fdio/watcher.h>
+#include <lib/fidl/cpp/interface_handle.h>
+#include <lib/netstack/c/netconfig.h>
+#include <lib/zx/socket.h>
+#include <zircon/device/ethertap.h>
+#include <zircon/status.h>
+#include <zircon/types.h>
+
+#include <string>
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "gtest/gtest.h"
+
+#include "lib/component/cpp/testing/test_util.h"
+#include "lib/component/cpp/testing/test_with_environment.h"
+#include "lib/netemul/network/ethernet_client.h"
+#include "lib/netemul/network/ethertap_client.h"
+#include "lib/netemul/network/ethertap_types.h"
+#include "lib/sys/cpp/file_descriptor.h"
+#include "lib/sys/cpp/termination_reason.h"
+
+namespace {
+class NetstackIoctlTest : public component::testing::TestWithEnvironment {};
+
+const char kNetstackUrl[] =
+    "fuchsia-pkg://fuchsia.com/netstack#meta/netstack.cmx";
+const char kTestIoctlClientUrl[] =
+    "fuchsia-pkg://fuchsia.com/test_ioctl_client#meta/test_ioctl_client.cmx";
+const char kTopoPath[] = "/fake/topo/path";
+const char kInterfaceName[] = "en0";
+
+TEST_F(NetstackIoctlTest, RunIoctlClient) {
+  auto services = CreateServices();
+
+  fuchsia::sys::LaunchInfo netstack_launch_info;
+  netstack_launch_info.url = kNetstackUrl;
+  netstack_launch_info.out = sys::CloneFileDescriptor(STDOUT_FILENO);
+  netstack_launch_info.err = sys::CloneFileDescriptor(STDERR_FILENO);
+  services->AddServiceWithLaunchInfo(std::move(netstack_launch_info),
+                                     fuchsia::netstack::Netstack::Name_);
+
+  // There's not currently a way to register one component for multiple
+  // services. Duplicate component URLs will only be launched once, so there
+  // will only be one netstack process running in this hermetic environment.
+  fuchsia::sys::LaunchInfo socket_provider_launch_info;
+  socket_provider_launch_info.url = kNetstackUrl;
+  socket_provider_launch_info.out = sys::CloneFileDescriptor(STDOUT_FILENO);
+  socket_provider_launch_info.err = sys::CloneFileDescriptor(STDERR_FILENO);
+  services->AddServiceWithLaunchInfo(std::move(socket_provider_launch_info),
+                                     fuchsia::net::SocketProvider::Name_);
+
+  auto env = CreateNewEnclosingEnvironment("NetstackLaunchTest_IoctlTest",
+                                           std::move(services));
+  ASSERT_TRUE(WaitForEnclosingEnvToStart(env.get()));
+
+  // Create a tap device so both the loopback and non-loopback code paths in
+  // ioctl_netc_get_ifinfo are exercised.
+  auto eth_config = netemul::EthertapConfig("IoctlTest");
+  auto tap = netemul::EthertapClient::Create(eth_config);
+  ASSERT_TRUE(tap) << "failed to create ethertap device";
+  tap->SetLinkUp(true);
+
+  netemul::EthernetClientFactory eth_factory;
+  auto eth = eth_factory.RetrieveWithMAC(eth_config.mac);
+  ASSERT_TRUE(eth) << "failed to retrieve ethernet client";
+
+  fuchsia::netstack::NetstackPtr netstack;
+  env->ConnectToService(netstack.NewRequest());
+
+  fuchsia::netstack::InterfaceConfig config;
+  config.name = kInterfaceName;
+  config.ip_address_config.set_dhcp(true);
+
+  uint32_t eth_id;
+  netstack->AddEthernetDevice(std::move(kTopoPath), std::move(config),
+                              eth->device(),
+                              [&eth_id](uint32_t id) { eth_id = id; });
+  ASSERT_TRUE(RunLoopUntil([&eth_id] { return eth_id != 0; }));
+
+  fuchsia::sys::LaunchInfo client_launch_info;
+  client_launch_info.url = kTestIoctlClientUrl;
+  client_launch_info.out = sys::CloneFileDescriptor(STDOUT_FILENO);
+  client_launch_info.err = sys::CloneFileDescriptor(STDERR_FILENO);
+  auto controller = env.get()->CreateComponent(std::move(client_launch_info));
+
+  bool wait = false;
+  int64_t exit_code;
+  fuchsia::sys::TerminationReason term_reason;
+  controller.events().OnTerminated =
+      [&wait, &exit_code, &term_reason](
+          int64_t retcode, fuchsia::sys::TerminationReason reason) {
+        wait = true;
+        exit_code = retcode;
+        term_reason = reason;
+      };
+  EXPECT_TRUE(RunLoopUntil([&wait] { return wait; }));
+  EXPECT_EQ(exit_code, 0);
+  EXPECT_EQ(term_reason, fuchsia::sys::TerminationReason::EXITED)
+      << "got: " << sys::TerminationReasonToString(term_reason);
+}
+
+}  // namespace
