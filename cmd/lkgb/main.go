9145a26d2f5421b9874730ab0f3ce77b942550ed
kjharland <kjharland@google.com>
2019-02-11 19:54:34 -0800

    [lkgb] Add a tool to find the "last known good build"
    Finds the last known good build for a given builder.
    
    e.g. $ lkgb fuchsia/ci/fuchsia-x64-release
    
    IN-990 #comment
    
    Change-Id: Ibb19e2edb41dd17c004f53ce54183f89fce3591b

---
+++ cmd/lkgb/main.go
@@ -0,0 +1,108 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"fuchsia.googlesource.com/tools/buildbucket"
+	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+	"google.golang.org/genproto/protobuf/field_mask"
+
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+)
+
+// Command line flags
+var (
+	// LUCI authentication flags.
+	authFlags authcli.Flags
+
+	// A flag for reading a BuildBucket Builder ID from command line args.
+	builderIDFlag = buildbucket.BuilderID()
+
+	// Whether to show help and exit.
+	help bool
+)
+
+func usage() {
+	fmt.Printf(`lkgb [flags] builder-id
+lkgb returns the "Last Known Good Build" id for a given builder.  builder-id must have the
+form "project/bucket/builder". For example: "fuchsia/ci/fuchsia-x64-release".
+`)
+	flag.PrintDefaults()
+}
+
+func init() {
+	authFlags.Register(flag.CommandLine, chromeinfra.DefaultAuthOptions())
+	flag.BoolVar(&help, "h", false, "Whether to show usage and exit")
+	flag.Usage = usage
+}
+
+func main() {
+	flag.Parse()
+	if help || flag.NArg() == 0 {
+		flag.Usage()
+		return
+	}
+
+	if err := execute(context.Background(), flag.Arg(0)); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func execute(ctx context.Context, input string) error {
+	opts, err := authFlags.Options()
+	if err != nil {
+		return fmt.Errorf("failed to get auth options: %v", err)
+	}
+
+	if err := builderIDFlag.Set(input); err != nil {
+		return fmt.Errorf("invalid builder id %q: %v", input, err)
+	}
+
+	client, err := buildbucket.NewBuildsClient(ctx, buildbucket.DefaultHost, opts)
+	if err != nil {
+		return fmt.Errorf("failed to create builds client: %v", err)
+	}
+
+	builderID := builderIDFlag.Get().(buildbucketpb.BuilderID)
+	buildID, err := getBuildID(ctx, client, builderID)
+	if err != nil {
+		return fmt.Errorf("failed to get build: %v", err)
+	}
+
+	fmt.Printf("%d\n", buildID)
+	return nil
+}
+
+func getBuildID(ctx context.Context, client buildbucketpb.BuildsClient, builderID buildbucketpb.BuilderID) (int64, error) {
+	response, err := client.SearchBuilds(ctx, &buildbucketpb.SearchBuildsRequest{
+		Predicate: &buildbucketpb.BuildPredicate{
+			Builder: &builderID,
+			Status:  buildbucketpb.Status_SUCCESS,
+		},
+		Fields: &field_mask.FieldMask{
+			Paths: []string{"builds.*.id"},
+		},
+		PageSize: 1,
+	})
+
+	if err != nil {
+		return 0, fmt.Errorf("search failed: %v", err)
+	}
+	if response.Builds == nil || len(response.Builds) == 0 {
+		return 0, errors.New("no build found")
+	}
+
+	return response.Builds[0].Id, nil
+}
--- go.mod
+++ go.mod
@@ -11,11 +11,8 @@ require (
 	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
 	github.com/julienschmidt/httprouter v1.2.0 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/maruel/subcommands v0.0.0-20181220013616-967e945be48b // indirect
-	github.com/maruel/ut v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/smartystreets/assertions v0.0.0-20190116191733-b6c0e53d7304 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c // indirect
 	github.com/texttheater/golang-levenshtein v0.0.0-20180516184445-d188e65d659e // indirect
