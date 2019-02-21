7ebb5fe9f92e3a63d5a09066b13138d6baa23d52
Craig Stout <cstout@google.com>
2019-02-21 12:33:15 -0500

    [magma] Add trace init to libmagma
    Change-Id: Ic5874e80745ad00cf76b39d926782f3f0635b8eb

---
+++ garnet/lib/magma/src/libmagma/trace_init.cc
@@ -0,0 +1,15 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform_trace.h"
+
+class PlatformTraceInit {
+public:
+    PlatformTraceInit()
+    {
+        if (magma::PlatformTrace::Get()) {
+            magma::PlatformTrace::Get()->Initialize();
+        }
+    }
+} init_platform_trace;
