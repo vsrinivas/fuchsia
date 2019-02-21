0542095b337f2c5347fb38020c09dcf753f30eb8
Eric Wang <eyw@google.com>
2019-02-20 16:12:30 -0800

    [wlan][rust-mlme] Define EthernetIIHdr
    Change-Id: Icbb1e53319ecb321d7fe5c0b9e347fa2d8b93a39

--- garnet/lib/rust/wlan-common/src/mac.rs
+++ garnet/lib/rust/wlan-common/src/mac.rs
@@ -672,6 +672,24 @@ impl LlcHdr {
     }
 }
 
+// IEEE Std 802.3-2015, 3.1.1
+#[derive(FromBytes, AsBytes, Unaligned)]
+#[repr(C, packed)]
+pub struct EthernetIIHdr {
+    pub da: MacAddr,
+    pub sa: MacAddr,
+    pub ether_type_be: [u8; 2], // In network byte order (big endian).
+}
+
+impl EthernetIIHdr {
+    pub fn ether_type(&self) -> u16 {
+        BigEndian::read_u16(&self.ether_type_be)
+    }
+    pub fn set_ether_type(&mut self, val: u16) {
+        BigEndian::write_u16(&mut self.ether_type_be, val)
+    }
+}
+
 // IEEE Std 802.11-2016, 9.3.2.2.2
 #[derive(FromBytes, AsBytes, Unaligned)]
 #[repr(C, packed)]
@@ -1166,5 +1184,33 @@ mod tests {
             }
         }
         assert_eq!((true, true), found_msdus);
+    }
+
+    #[test]
+    fn eth_hdr_big_endian() {
+        let mut bytes: Vec<u8> = vec![
+            1, 2, 3, 4, 5, 6, // dst_addr
+            7, 8, 9, 10, 11, 12, // src_addr
+            13, 14, // ether_type
+            99, 99, // trailing bytes
+        ];
+        let (mut hdr, body) =
+            LayoutVerified::<_, EthernetIIHdr>::new_unaligned_from_prefix(&mut bytes[..])
+                .expect("cannot create ethernet header.");
+        assert_eq!(hdr.da, [1u8, 2, 3, 4, 5, 6]);
+        assert_eq!(hdr.sa, [7u8, 8, 9, 10, 11, 12]);
+        assert_eq!(hdr.ether_type(), 13 << 8 | 14);
+        assert_eq!(hdr.ether_type_be, [13u8, 14]);
+        assert_eq!(body, [99, 99]);
+
+        hdr.set_ether_type(0x888e);
+        assert_eq!(hdr.ether_type_be, [0x88, 0x8e]);
+        #[rustfmt::skip]
+        assert_eq!(
+            &[1u8, 2, 3, 4, 5, 6,
+            7, 8, 9, 10, 11, 12,
+            0x88, 0x8e,
+            99, 99],
+            &bytes[..]);
     }
 }
