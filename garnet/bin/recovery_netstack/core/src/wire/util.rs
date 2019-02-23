9308ea39e161a3114e675d66c93f037814767e75
Bruno Dal Bo <brunodalbo@google.com>
2019-02-22 16:41:38 -0800

    [recovery-netstack] Sequential record-parsing util
    - Adds utility functions to have iterators over sequential records in a
    buffer.
    - Will be used to parse IGMP v3 Membership Reports
    
    TEST: Added tests in wire::util::records::test
    Change-Id: If0112435c198159ce46b9b15b24fa029c3ea8094

--- garnet/bin/recovery_netstack/core/src/wire/util.rs
+++ garnet/bin/recovery_netstack/core/src/wire/util.rs
@@ -4,6 +4,7 @@
 
 pub use self::checksum::*;
 pub use self::options::*;
+pub use self::records::*;
 
 /// Whether `size` fits in a `u16`.
 pub fn fits_in_u16(size: usize) -> bool {
@@ -257,6 +258,434 @@ mod checksum {
                 };
                 assert_eq!(updated, from_scratch);
             }
+        }
+    }
+}
+
+mod records {
+    use packet::BufferView;
+    use std::ops::Deref;
+    use zerocopy::ByteSlice;
+
+    /// A parsed set of arbitrary sequential records.
+    ///
+    /// `Records` represents a pre-parsed set of records whose structure is
+    /// enforced by the impl in `O`.
+    #[derive(Debug)]
+    pub struct Records<B, R: RecordsImplLimit> {
+        bytes: B,
+        limit: R::Limit,
+    }
+
+    /// An iterator over the records contained inside a `Records` instance.
+    pub struct RecordsIter<'a, R: RecordsImpl<'a>> {
+        bytes: &'a [u8],
+        limit: R::Limit,
+    }
+
+    /// Trait that specifies the type of errors to emit
+    /// when parsing sequential records.
+    pub trait RecordsImplErr {
+        type Error;
+    }
+
+    /// Trait that provides a way to limit the amount of
+    /// records read from a buffer. Some protocols will have some sort of header
+    /// preceding the records that will indicate the number of records to follow
+    /// (e.g. igmp), while others will have that information inline
+    /// (e.g. IP options).
+    ///
+    /// This crate provides `usize` and `()` implementations for `RecordsLimit`
+    /// that can be provided as the `Limit` type in the `RecordsImpl` trait.
+    /// The `usize` impl will limit to `n` records read, while `()` will not
+    /// impose any limit.
+    pub trait RecordsLimit {
+        /// Decrements the current value held by 1.
+        /// Implementers should `panic` if `decrement` is called
+        /// when `has_more` would've returned `false`.
+        fn decrement(&mut self);
+        /// Returns whether more records are available. For a simple
+        /// counter implementation, this will return if counter is > 0.
+        fn has_more(&self) -> bool;
+    }
+
+    impl RecordsLimit for () {
+        fn decrement(&mut self) {}
+
+        fn has_more(&self) -> bool {
+            true
+        }
+    }
+
+    impl RecordsLimit for usize {
+        fn decrement(&mut self) {
+            *self = self.checked_sub(1).expect("Can't decrement counter below 0");
+        }
+
+        fn has_more(&self) -> bool {
+            *self > 0
+        }
+    }
+
+    /// Trait that provides record limiting to `RecordsImpl` implementers.
+    pub trait RecordsImplLimit {
+        /// A limit type that can be used to limit record-reading.
+        /// See the `RecordsLimit` trait.
+        type Limit: Sized + RecordsLimit + Clone;
+    }
+
+    /// An implementation of a records parser.
+    ///
+    /// `RecordsImpl` provides functions to parse sequential records.
+    ///  It is required in order to construct a `Records` or
+    /// `RecordsIter`.
+    pub trait RecordsImpl<'a>: RecordsImplErr + RecordsImplLimit {
+        /// The type of a single record; the output from the `parse` function.
+        ///
+        /// For long or variable-length data, the user is advised to make
+        /// `Output` a reference into the bytes passed to `parse`. This is
+        /// achievable because of the lifetime parameter to this trait.
+        type Output;
+
+        /// If Some(Self::Error), will emit the provided constant as an
+        /// error if the provided buffer is exhausted while `Self::Limit` still
+        /// reports `has_more` as `true`.
+        const EXACT_LIMIT_ERROR: Option<Self::Error> = None;
+
+        /// Parse a record.
+        ///
+        /// `parse` takes a kind byte and variable-length data associated and
+        /// returns `Ok(Some(o))` if the the record is successfully parsed as
+        /// `o`, `Ok(None)` if data is not malformed but the implementer can't
+        /// extract a concrete object (e.g. record is an unimplemented
+        /// enumeration, but we can still safely "skip" it),
+        /// and `Err(err)` if the `data` was malformed for the attempted
+        /// record parsing.
+        ///
+        ///  When returning `Ok(None)` it's the implementer's responsibility to
+        ///  nonetheless skip the record (which may not be possible for some
+        ///  implementations, in which case it should return an `Err`).
+        ///
+        /// `parse` must be deterministic, or else `Records::parse` cannot
+        /// guarantee that future iterations will not produce errors (and
+        /// panic).
+        fn parse<BV: BufferView<&'a [u8]>>(
+            data: &mut BV,
+        ) -> Result<Option<Self::Output>, Self::Error>;
+    }
+
+    impl<B, R> Records<B, R>
+    where
+        B: ByteSlice,
+        R: for<'a> RecordsImpl<'a>,
+    {
+        /// Parse a set of records with an informed limit.
+        ///
+        /// `parse_with_limit` parses `bytes` as a sequence of records.
+        /// `limit` is used to specify how many records are expected to be
+        /// parsed.
+        ///
+        /// If `limit` is a `usize`, then `parse_with_limit` will stop
+        /// after having found that many records. If `R::EXACT_LIMIT_ERROR` is
+        /// provided, that error will be returned in case `bytes` is exhausted
+        /// and the limit hasn't been reached OR the limit has been reached and
+        /// there are leftover bytes.
+        ///
+        /// If `limit` is a `()`, then it will be ignored, and records will
+        /// be parsed until `bytes` is exhausted.
+        ///
+        /// `parse_with_limit` performs a single pass over all of the records
+        /// to verify that they are well-formed.
+        /// Once `parse_with_limit` returns successfully, the resulting
+        /// `Records` can be used to construct infallible iterators.
+        pub fn parse_with_limit(bytes: B, limit: R::Limit) -> Result<Records<B, R>, R::Error> {
+            // First, do a single pass over the bytes to detect any errors up
+            // front. Once this is done, since we have a reference to `bytes`,
+            // these bytes can't change out from under us, and so we can treat
+            // any iterator over these bytes as infallible. This makes a few
+            // assumptions, but none of them are that big of a deal. In all
+            // cases, breaking these assumptions would just result in a runtime
+            // panic.
+            // - B could return different bytes each time
+            // - R::parse could be non-deterministic
+            let mut lim = limit.clone();
+            let mut b = LongLivedBuff::new(bytes.deref());
+            while next::<_, R>(&mut b, &mut lim)?.is_some() {}
+            Ok(Records { bytes, limit })
+        }
+    }
+
+    impl<B, R> Records<B, R>
+    where
+        B: ByteSlice,
+        R: for<'a> RecordsImpl<'a, Limit = ()>,
+    {
+        /// Parses a set of records.
+        ///
+        /// Equivalent to calling `parse_with_limit` with `limit = ()`.
+        pub fn parse(bytes: B) -> Result<Records<B, R>, R::Error> {
+            Self::parse_with_limit(bytes, ())
+        }
+    }
+
+    impl<B: Deref<Target = [u8]>, R> Records<B, R>
+    where
+        R: for<'a> RecordsImpl<'a>,
+    {
+        /// Get the underlying bytes.
+        ///
+        /// `bytes` returns a reference to the byte slice backing this
+        /// `Options`.
+        pub fn bytes(&self) -> &[u8] {
+            &self.bytes
+        }
+    }
+
+    impl<'a, B, R> Records<B, R>
+    where
+        B: 'a + ByteSlice,
+        R: RecordsImpl<'a>,
+    {
+        /// Create an iterator over options.
+        ///
+        /// `iter` constructs an iterator over the records. Since the records
+        /// were validated in `parse`, then so long as `from_kind` and
+        /// `from_data` are deterministic, the iterator is infallible.
+        pub fn iter(&'a self) -> RecordsIter<'a, R> {
+            RecordsIter { bytes: &self.bytes, limit: self.limit.clone() }
+        }
+    }
+
+    impl<'a, R> Iterator for RecordsIter<'a, R>
+    where
+        R: RecordsImpl<'a>,
+    {
+        type Item = R::Output;
+
+        fn next(&mut self) -> Option<R::Output> {
+            // use match rather than expect because expect requires that Err: Debug
+            let mut bytes = LongLivedBuff::new(self.bytes);
+            #[allow(clippy::match_wild_err_arm)]
+            let result = match next::<_, R>(&mut bytes, &mut self.limit) {
+                Ok(o) => o,
+                Err(_) => panic!("already-validated options should not fail to parse"),
+            };
+            self.bytes = bytes.into_rest();
+            result
+        }
+    }
+
+    /// Gets the next entry for a set of sequential records in `bytes`.
+    ///
+    /// On return, `bytes` will be pointing to the start of where a next record would be.
+    fn next<'a, BV, R>(bytes: &mut BV, limit: &mut R::Limit) -> Result<Option<R::Output>, R::Error>
+    where
+        R: RecordsImpl<'a>,
+        BV: BufferView<&'a [u8]>,
+    {
+        loop {
+            // if we run out of bytes or limit is exhausted,
+            // we stop the parsing here.
+            if bytes.is_empty() || !limit.has_more() {
+                // If only one of the conditions is met and R::EXACT_LIMIT_ERROR
+                // is specified, we return the exact limit error.
+                return match R::EXACT_LIMIT_ERROR {
+                    Some(_) if bytes.is_empty() ^ !limit.has_more() => {
+                        Err(R::EXACT_LIMIT_ERROR.unwrap())
+                    }
+                    _ => Ok(None),
+                };
+            }
+
+            limit.decrement();
+            match R::parse(bytes) {
+                Ok(Some(o)) => return Ok(Some(o)),
+                Ok(None) => {}
+                Err(err) => return Err(err),
+            }
+        }
+    }
+
+    /// A wrapper around the implementation of `BufferView` for slices.
+    ///
+    /// `LongLivedBuff` is a thin wrapper around `&[u8]` meant to
+    /// provide an implementation of `BufferView` that returns slices tied to
+    /// the same lifetime as the slice that `LongLivedBuff` was created with.
+    /// This is in contrast to the more widely used `&'b mut &'a [u8]`
+    /// `BufferView` implementer that returns slice references tied to
+    /// lifetime `b`.
+    struct LongLivedBuff<'a>(&'a [u8]);
+
+    impl<'a> LongLivedBuff<'a> {
+        /// Creates a new `LongLivedBuff` around a slice reference with
+        /// lifetime `a`.
+        /// All slices returned by the `BufferView` impl of `LongLivedBuff` are
+        /// guaranteed to return slice references tied to the same lifetime `a`.
+        pub fn new(data: &'a [u8]) -> LongLivedBuff<'a> {
+            LongLivedBuff::<'a>(data)
+        }
+    }
+
+    impl<'a> AsRef<[u8]> for LongLivedBuff<'a> {
+        fn as_ref(&self) -> &[u8] {
+            self.0
+        }
+    }
+
+    impl<'a> packet::BufferView<&'a [u8]> for LongLivedBuff<'a> {
+        fn take_front(&mut self, n: usize) -> Option<&'a [u8]> {
+            if self.0.len() >= n {
+                let (prefix, rest) = std::mem::replace(&mut self.0, &[]).split_at(n);
+                std::mem::replace(&mut self.0, rest);
+                Some(prefix)
+            } else {
+                None
+            }
+        }
+
+        fn take_back(&mut self, n: usize) -> Option<&'a [u8]> {
+            if self.0.len() >= n {
+                let split = self.0.len() - n;
+                let (rest, suffix) = std::mem::replace(&mut self.0, &[]).split_at(n);
+                std::mem::replace(&mut self.0, rest);
+                Some(suffix)
+            } else {
+                None
+            }
+        }
+
+        fn into_rest(self) -> &'a [u8] {
+            self.0
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        use packet::BufferView;
+        use zerocopy::{AsBytes, FromBytes, LayoutVerified, Unaligned};
+
+        const DUMMY_BYTES: [u8; 16] = [
+            0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02,
+            0x03, 0x04,
+        ];
+
+        #[derive(Debug, AsBytes, FromBytes, Unaligned)]
+        #[repr(C)]
+        struct DummyRecord {
+            a: [u8; 2],
+            b: u8,
+            c: u8,
+        }
+
+        fn parse_dummy_rec<'a, BV>(
+            data: &mut BV,
+        ) -> Result<Option<LayoutVerified<&'a [u8], DummyRecord>>, ()>
+        where
+            BV: BufferView<&'a [u8]>,
+        {
+            match data.take_obj_front::<DummyRecord>() {
+                Some(res) => Ok(Some(res)),
+                None => Err(()),
+            }
+        }
+
+        #[derive(Debug)]
+        struct LimitlessRecordImpl;
+
+        impl RecordsImplLimit for LimitlessRecordImpl {
+            type Limit = ();
+        }
+
+        impl RecordsImplErr for LimitlessRecordImpl {
+            type Error = ();
+        }
+
+        impl<'a> RecordsImpl<'a> for LimitlessRecordImpl {
+            type Output = LayoutVerified<&'a [u8], DummyRecord>;
+
+            fn parse<BV: BufferView<&'a [u8]>>(
+                data: &mut BV,
+            ) -> Result<Option<Self::Output>, Self::Error> {
+                parse_dummy_rec(data)
+            }
+        }
+
+        #[derive(Debug)]
+        struct LimitedRecordImpl;
+
+        impl RecordsImplLimit for LimitedRecordImpl {
+            type Limit = usize;
+        }
+
+        impl RecordsImplErr for LimitedRecordImpl {
+            type Error = ();
+        }
+
+        impl<'a> RecordsImpl<'a> for LimitedRecordImpl {
+            type Output = LayoutVerified<&'a [u8], DummyRecord>;
+
+            fn parse<BV: BufferView<&'a [u8]>>(
+                data: &mut BV,
+            ) -> Result<Option<Self::Output>, Self::Error> {
+                parse_dummy_rec(data)
+            }
+        }
+
+        #[derive(Debug)]
+        struct ExactLimitRecordImpl;
+
+        impl RecordsImplLimit for ExactLimitRecordImpl {
+            type Limit = usize;
+        }
+
+        impl RecordsImplErr for ExactLimitRecordImpl {
+            type Error = ();
+        }
+
+        impl<'a> RecordsImpl<'a> for ExactLimitRecordImpl {
+            type Output = LayoutVerified<&'a [u8], DummyRecord>;
+            const EXACT_LIMIT_ERROR: Option<Self::Error> = Some(());
+
+            fn parse<BV: BufferView<&'a [u8]>>(
+                data: &mut BV,
+            ) -> Result<Option<Self::Output>, Self::Error> {
+                parse_dummy_rec(data)
+            }
+        }
+
+        fn check_parsed_record(rec: &DummyRecord) {
+            assert_eq!(rec.a[0], 0x01);
+            assert_eq!(rec.a[1], 0x02);
+            assert_eq!(rec.b, 0x03);
+        }
+
+        #[test]
+        fn all_records_parsing() {
+            let parsed = Records::<_, LimitlessRecordImpl>::parse(&DUMMY_BYTES[..]).unwrap();
+            assert_eq!(parsed.iter().count(), 4);
+            for rec in parsed.iter() {
+                check_parsed_record(rec.deref());
+            }
+        }
+
+        #[test]
+        fn limit_records_parsing() {
+            let limit: usize = 2;
+            let parsed =
+                Records::<_, LimitedRecordImpl>::parse_with_limit(&DUMMY_BYTES[..], limit).unwrap();
+            assert_eq!(parsed.iter().count(), limit);
+            for rec in parsed.iter() {
+                check_parsed_record(rec.deref());
+            }
+        }
+
+        #[test]
+        fn exact_limit_records_parsing() {
+            Records::<_, ExactLimitRecordImpl>::parse_with_limit(&DUMMY_BYTES[..], 2)
+                .expect_err("fails if all the buffer hasn't been parsed");
+            Records::<_, ExactLimitRecordImpl>::parse_with_limit(&DUMMY_BYTES[..], 5)
+                .expect_err("fails if can't extract enough records");
         }
     }
 }
