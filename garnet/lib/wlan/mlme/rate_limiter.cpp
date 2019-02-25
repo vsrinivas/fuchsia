e9a4e1c92218be960529e047762dd722aff922ce
Greg Bonik <gbonik@google.com>
2019-02-25 15:19:39 -0800

    [wlan][mesh] Add a simple rate limiter implementation
    To be used for outgoing PERR elements.
    
    Change-Id: I8a8ba4c5c1874cf93d7b964c8f00579365c804ba

---
+++ garnet/lib/wlan/mlme/rate_limiter.cpp
@@ -0,0 +1,21 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <wlan/mlme/rate_limiter.h>
+
+namespace wlan {
+
+RateLimiter::RateLimiter(zx::duration period, size_t max_events_per_period)
+    : period_(period), max_events_per_period_(max_events_per_period) {}
+
+bool RateLimiter::RecordEvent(zx::time now) {
+    while (!events_.empty() && now >= events_.front() + period_) {
+        events_.pop();
+    }
+    if (events_.size() >= max_events_per_period_) { return false; }
+    events_.push(now);
+    return true;
+}
+
+}  // namespace wlan
---
+++ garnet/lib/wlan/mlme/tests/rate_limiter_unittest.cpp
@@ -0,0 +1,30 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+#include <wlan/mlme/rate_limiter.h>
+
+namespace wlan {
+
+TEST(RateLimiter, SingleEvent) {
+    RateLimiter limiter(zx::duration(ZX_MSEC(100)), 1);
+    EXPECT_TRUE(limiter.RecordEvent(zx::time(ZX_MSEC(2000))));
+    EXPECT_FALSE(limiter.RecordEvent(zx::time(ZX_MSEC(2099))));
+    EXPECT_TRUE(limiter.RecordEvent(zx::time(ZX_MSEC(2100))));
+    EXPECT_FALSE(limiter.RecordEvent(zx::time(ZX_MSEC(2101))));
+    EXPECT_FALSE(limiter.RecordEvent(zx::time(ZX_MSEC(2199))));
+    EXPECT_TRUE(limiter.RecordEvent(zx::time(ZX_MSEC(2200))));
+}
+
+TEST(RateLimiter, TwoEvents) {
+    RateLimiter limiter(zx::duration(ZX_MSEC(100)), 2);
+    EXPECT_TRUE(limiter.RecordEvent(zx::time(ZX_MSEC(2000))));
+    EXPECT_TRUE(limiter.RecordEvent(zx::time(ZX_MSEC(2050))));
+    EXPECT_FALSE(limiter.RecordEvent(zx::time(ZX_MSEC(2099))));
+    EXPECT_TRUE(limiter.RecordEvent(zx::time(ZX_MSEC(2100))));
+    EXPECT_FALSE(limiter.RecordEvent(zx::time(ZX_MSEC(2149))));
+    EXPECT_TRUE(limiter.RecordEvent(zx::time(ZX_MSEC(2150))));
+}
+
+}  // namespace wlan
