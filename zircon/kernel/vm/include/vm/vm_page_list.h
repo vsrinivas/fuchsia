4e60d9a12c84fa5a4cd98241e9f0860cecced478
Nick Maniscalco <maniscalco@google.com>
2019-02-26 09:07:32 -0800

    [kernel][vm] Refactor VmPageList templates to remove duplicate code
    Use private static template methods to eliminate need for const and
    non-const implementations of ForEveryPage and ForEveryPageInRange.
    
    No behavior change.
    
    Change-Id: I1cca339387bd329286b68f993a8e730f8298c333

--- zircon/kernel/vm/include/vm/vm_page_list.h
+++ zircon/kernel/vm/include/vm/vm_page_list.h
@@ -30,55 +30,15 @@ public:
     uint64_t GetKey() const { return obj_offset_; }
 
     // for every valid page in the node call the passed in function
-    template <typename T>
-    zx_status_t ForEveryPage(T func, uint64_t start_offset, uint64_t end_offset) {
-        DEBUG_ASSERT(end_offset >= start_offset);
-        size_t start = 0;
-        size_t end = kPageFanOut;
-        if (start_offset > obj_offset_) {
-            start = (start_offset - obj_offset_) / PAGE_SIZE;
-        }
-        if (end_offset < obj_offset_) {
-            return ZX_ERR_NEXT;
-        }
-        if (end_offset < obj_offset_ + kPageFanOut * PAGE_SIZE) {
-            end = (end_offset - obj_offset_) / PAGE_SIZE;
-        }
-        for (size_t i = start; i < end; i++) {
-            if (pages_[i]) {
-                zx_status_t status = func(pages_[i], obj_offset_ + i * PAGE_SIZE);
-                if (unlikely(status != ZX_ERR_NEXT)) {
-                    return status;
-                }
-            }
-        }
-        return ZX_ERR_NEXT;
+    template <typename F>
+    zx_status_t ForEveryPage(F func, uint64_t start_offset, uint64_t end_offset) {
+        return ForEveryPage(this, func, start_offset, end_offset);
     }
 
     // for every valid page in the node call the passed in function
-    template <typename T>
-    zx_status_t ForEveryPage(T func, uint64_t start_offset, uint64_t end_offset) const {
-        DEBUG_ASSERT(end_offset >= start_offset);
-        size_t start = 0;
-        size_t end = kPageFanOut;
-        if (start_offset > obj_offset_) {
-            start = (start_offset - obj_offset_) / PAGE_SIZE;
-        }
-        if (end_offset < obj_offset_) {
-            return ZX_ERR_NEXT;
-        }
-        if (end_offset < obj_offset_ + kPageFanOut * PAGE_SIZE) {
-            end = (end_offset - obj_offset_) / PAGE_SIZE;
-        }
-        for (size_t i = start; i < end; i++) {
-            if (pages_[i]) {
-                zx_status_t status = func(pages_[i], obj_offset_ + i * PAGE_SIZE);
-                if (unlikely(status != ZX_ERR_NEXT)) {
-                    return status;
-                }
-            }
-        }
-        return ZX_ERR_NEXT;
+    template <typename F>
+    zx_status_t ForEveryPage(F func, uint64_t start_offset, uint64_t end_offset) const {
+        return ForEveryPage(this, func, start_offset, end_offset);
     }
 
     vm_page* GetPage(size_t index);
@@ -95,6 +55,31 @@ public:
     }
 
 private:
+    template <typename S, typename F>
+    static zx_status_t ForEveryPage(S self, F func, uint64_t start_offset, uint64_t end_offset) {
+        DEBUG_ASSERT(end_offset >= start_offset);
+        size_t start = 0;
+        size_t end = kPageFanOut;
+        if (start_offset > self->obj_offset_) {
+            start = (start_offset - self->obj_offset_) / PAGE_SIZE;
+        }
+        if (end_offset < self->obj_offset_) {
+            return ZX_ERR_NEXT;
+        }
+        if (end_offset < self->obj_offset_ + kPageFanOut * PAGE_SIZE) {
+            end = (end_offset - self->obj_offset_) / PAGE_SIZE;
+        }
+        for (size_t i = start; i < end; i++) {
+            if (self->pages_[i]) {
+                zx_status_t status = func(self->pages_[i], self->obj_offset_ + i * PAGE_SIZE);
+                if (unlikely(status != ZX_ERR_NEXT)) {
+                    return status;
+                }
+            }
+        }
+        return ZX_ERR_NEXT;
+    }
+
     fbl::Canary<fbl::magic("PLST")> canary_;
 
     uint64_t obj_offset_ = 0;
@@ -143,118 +128,45 @@ public:
     DISALLOW_COPY_ASSIGN_AND_MOVE(VmPageList);
 
     // walk the page tree, calling the passed in function on every tree node
-    template <typename T>
-    zx_status_t ForEveryPage(T per_page_func) {
-        for (auto& pl : list_) {
-            zx_status_t status = pl.ForEveryPage(per_page_func, pl.offset(),
-                                                 pl.offset() + pl.kPageFanOut * PAGE_SIZE);
-            if (unlikely(status != ZX_ERR_NEXT)) {
-                if (status == ZX_ERR_STOP) {
-                    break;
-                }
-                return status;
-            }
-        }
-        return ZX_OK;
+    template <typename F>
+    zx_status_t ForEveryPage(F per_page_func) {
+        return ForEveryPage(this, per_page_func);
     }
 
     // walk the page tree, calling the passed in function on every tree node
-    template <typename T>
-    zx_status_t ForEveryPage(T per_page_func) const {
-        for (auto& pl : list_) {
-            zx_status_t status = pl.ForEveryPage(per_page_func, pl.offset(),
-                                                 pl.offset() + pl.kPageFanOut * PAGE_SIZE);
-            if (unlikely(status != ZX_ERR_NEXT)) {
-                if (status == ZX_ERR_STOP) {
-                    break;
-                }
-                return status;
-            }
-        }
-        return ZX_OK;
+    template <typename F>
+    zx_status_t ForEveryPage(F per_page_func) const {
+        return ForEveryPage(this, per_page_func);
     }
 
     // walk the page tree, calling the passed in function on every tree node
-    template <typename T>
-    zx_status_t ForEveryPageInRange(T per_page_func, uint64_t start_offset, uint64_t end_offset) {
-        // Find the first node with a start after start_offset; if start_offset
-        // is in a node, it'll be in the one before it.
-        auto start = --list_.upper_bound(start_offset);
-        if (!start.IsValid()) {
-            start = list_.begin();
-        }
-        const auto end = list_.lower_bound(end_offset);
-        for (auto itr = start; itr != end; ++itr) {
-            auto& pl = *itr;
-            zx_status_t status = pl.ForEveryPage(per_page_func, start_offset, end_offset);
-            if (unlikely(status != ZX_ERR_NEXT)) {
-                if (status == ZX_ERR_STOP) {
-                    break;
-                }
-                return status;
-            }
-        }
-        return ZX_OK;
+    template <typename F>
+    zx_status_t ForEveryPageInRange(F per_page_func,
+                                    uint64_t start_offset, uint64_t end_offset) {
+        return ForEveryPageInRange(this, per_page_func, start_offset, end_offset);
     }
 
-    template <typename T>
-    zx_status_t ForEveryPageInRange(T per_page_func, uint64_t start_offset,
-                                    uint64_t end_offset) const {
-        // Find the first node with a start after start_offset; if start_offset
-        // is in a node, it'll be in the one before it.
-        auto start = --list_.upper_bound(start_offset);
-        if (!start.IsValid()) {
-            start = list_.begin();
-        }
-        const auto end = list_.lower_bound(end_offset);
-        for (auto itr = start; itr != end; ++itr) {
-            auto& pl = *itr;
-            zx_status_t status = pl.ForEveryPage(per_page_func, start_offset, end_offset);
-            if (unlikely(status != ZX_ERR_NEXT)) {
-                if (status == ZX_ERR_STOP) {
-                    break;
-                }
-                return status;
-            }
-        }
-        return ZX_OK;
+    // walk the page tree, calling the passed in function on every tree node
+    template <typename F>
+    zx_status_t ForEveryPageInRange(F per_page_func,
+                                    uint64_t start_offset, uint64_t end_offset) const {
+        return ForEveryPageInRange(this, per_page_func, start_offset, end_offset);
     }
 
-    template <typename T1, typename T2>
-    zx_status_t ForEveryPageAndGapInRange(T1 per_page_func, T2 per_gap_func,
+    // walk the page tree, calling |per_page_func| on every page and |per_gap_func| on every gap
+    template <typename PAGE_FUNC, typename GAP_FUNC>
+    zx_status_t ForEveryPageAndGapInRange(PAGE_FUNC per_page_func, GAP_FUNC per_gap_func,
+                                          uint64_t start_offset, uint64_t end_offset) {
+        return ForEveryPageAndGapInRange(this, per_page_func, per_gap_func,
+                                         start_offset, end_offset);
+    }
+
+    // walk the page tree, calling |per_page_func| on every page and |per_gap_func| on every gap
+    template <typename PAGE_FUNC, typename GAP_FUNC>
+    zx_status_t ForEveryPageAndGapInRange(PAGE_FUNC per_page_func, GAP_FUNC per_gap_func,
                                           uint64_t start_offset, uint64_t end_offset) const {
-
-        uint64_t expected_next_off = start_offset;
-        auto per_page_wrapper_fn = [&expected_next_off, end_offset, per_page_func, per_gap_func]
-                    (const auto p, uint64_t off) {
-            zx_status_t status = ZX_ERR_NEXT;
-            if (expected_next_off != off) {
-                status = per_gap_func(expected_next_off, off);
-            }
-            if (status == ZX_ERR_NEXT) {
-                status = per_page_func(p, off);
-            }
-            expected_next_off = off + PAGE_SIZE;
-            // Prevent the last call to per_gap_func
-            if (status == ZX_ERR_STOP) {
-                expected_next_off = end_offset;
-            }
-            return status;
-        };
-
-        zx_status_t status = ForEveryPageInRange(per_page_wrapper_fn, start_offset, end_offset);
-        if (status != ZX_OK) {
-            return status;
-        }
-
-        if (expected_next_off != end_offset) {
-            status = per_gap_func(expected_next_off, end_offset);
-            if (status != ZX_ERR_NEXT && status != ZX_ERR_STOP) {
-                return status;
-            }
-        }
-
-        return ZX_OK;
+        return ForEveryPageAndGapInRange(this, per_page_func, per_gap_func,
+                                         start_offset, end_offset);
     }
 
     zx_status_t AddPage(vm_page*, uint64_t offset);
@@ -275,5 +187,81 @@ public:
     VmPageSpliceList TakePages(uint64_t offset, uint64_t length);
 
 private:
+    template <typename S, typename F>
+    static zx_status_t ForEveryPage(S self, F per_page_func) {
+        for (auto& pl : self->list_) {
+            zx_status_t status = pl.ForEveryPage(per_page_func, pl.offset(),
+                                                 pl.offset() + pl.kPageFanOut * PAGE_SIZE);
+            if (unlikely(status != ZX_ERR_NEXT)) {
+                if (status == ZX_ERR_STOP) {
+                    break;
+                }
+                return status;
+            }
+        }
+        return ZX_OK;
+    }
+
+    template <typename S, typename F>
+    static zx_status_t ForEveryPageInRange(S self, F per_page_func,
+                                           uint64_t start_offset, uint64_t end_offset) {
+        // Find the first node with a start after start_offset; if start_offset
+        // is in a node, it'll be in the one before it.
+        auto start = --(self->list_.upper_bound(start_offset));
+        if (!start.IsValid()) {
+            start = self->list_.begin();
+        }
+        const auto end = self->list_.lower_bound(end_offset);
+        for (auto itr = start; itr != end; ++itr) {
+            auto& pl = *itr;
+            zx_status_t status = pl.ForEveryPage(per_page_func, start_offset, end_offset);
+            if (unlikely(status != ZX_ERR_NEXT)) {
+                if (status == ZX_ERR_STOP) {
+                    break;
+                }
+                return status;
+            }
+        }
+        return ZX_OK;
+    }
+
+    template <typename S, typename PAGE_FUNC, typename GAP_FUNC>
+    static zx_status_t ForEveryPageAndGapInRange(S self,
+                                                 PAGE_FUNC per_page_func, GAP_FUNC per_gap_func,
+                                                 uint64_t start_offset, uint64_t end_offset) {
+        uint64_t expected_next_off = start_offset;
+        auto per_page_wrapper_fn = [&expected_next_off, end_offset, per_page_func, per_gap_func]
+                    (const auto p, uint64_t off) {
+            zx_status_t status = ZX_ERR_NEXT;
+            if (expected_next_off != off) {
+                status = per_gap_func(expected_next_off, off);
+            }
+            if (status == ZX_ERR_NEXT) {
+                status = per_page_func(p, off);
+            }
+            expected_next_off = off + PAGE_SIZE;
+            // Prevent the last call to per_gap_func
+            if (status == ZX_ERR_STOP) {
+                expected_next_off = end_offset;
+            }
+            return status;
+        };
+
+        zx_status_t status =
+            ForEveryPageInRange(self, per_page_wrapper_fn, start_offset, end_offset);
+        if (status != ZX_OK) {
+            return status;
+        }
+
+        if (expected_next_off != end_offset) {
+            status = per_gap_func(expected_next_off, end_offset);
+            if (status != ZX_ERR_NEXT && status != ZX_ERR_STOP) {
+                return status;
+            }
+        }
+
+        return ZX_OK;
+    }
+
     fbl::WAVLTree<uint64_t, ktl::unique_ptr<VmPageListNode>> list_;
 };
