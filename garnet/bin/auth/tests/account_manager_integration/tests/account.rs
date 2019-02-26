5634d0e306a67431ee6db125c243a3ce985d94ef
Jody Sankey <jsankey@google.com>
2019-02-25 18:32:18 -0800

    [auth] Run rustfmt on account manager integration test.
    Also migrated to create_endpoints instead of raw channels.
    
    Change-Id: Ida01c8c86692af7f9a407425db7a32d572aa2333

--- garnet/bin/auth/tests/account_manager_integration/tests/account.rs
+++ garnet/bin/auth/tests/account_manager_integration/tests/account.rs
@@ -3,13 +3,12 @@
 // found in the LICENSE file.
 
 use failure::{format_err, Error};
-use fidl::endpoints::{ClientEnd, ServerEnd};
+use fidl::endpoints::create_endpoints;
 use fidl_fuchsia_auth::AuthStateSummary;
 use fidl_fuchsia_auth_account::{
-    AccountManagerMarker, AccountManagerProxy, AccountProxy, LocalAccountId, Status,
+    AccountManagerMarker, AccountManagerProxy, LocalAccountId, Status,
 };
 use fuchsia_async as fasync;
-use fuchsia_zircon as zx;
 use futures::prelude::*;
 
 /// Executes the supplied test function against a connected AccountManagerProxy.
@@ -22,9 +21,7 @@ where
     let proxy = fuchsia_app::client::connect_to_service::<AccountManagerMarker>()
         .expect("Failed to connect to account manager service");;
 
-    executor
-        .run_singlethreaded(test_fn(proxy))
-        .expect("Executor run failed.")
+    executor.run_singlethreaded(test_fn(proxy)).expect("Executor run failed.")
 }
 
 /// Calls provision_new_account on the supplied account_manager, returning an error on any
@@ -34,10 +31,7 @@ async fn provision_new_account(
 ) -> Result<LocalAccountId, Error> {
     match await!(account_manager.provision_new_account())? {
         (Status::Ok, Some(new_account_id)) => Ok(*new_account_id),
-        (status, _) => Err(format_err!(
-            "ProvisionNewAccount returned status: {:?}",
-            status
-        )),
+        (status, _) => Err(format_err!("ProvisionNewAccount returned status: {:?}", status)),
     }
 }
 
@@ -62,47 +56,36 @@ fn test_account_functionality() {
         assert_ne!(account_1.id, account_2.id);
 
         // Connect a channel to one of these accounts and verify it's usable.
-        let (_, acp_client_chan) = zx::Channel::create()?;
-        let (account_server_chan, account_client_chan) = zx::Channel::create()?;
+        let (acp_client_end, _) = create_endpoints()?;
+        let (account_client_end, account_server_end) = create_endpoints()?;
         assert_eq!(
             await!(account_manager.get_account(
                 &mut account_1,
-                ClientEnd::new(acp_client_chan),
-                ServerEnd::new(account_server_chan)
+                acp_client_end,
+                account_server_end
             ))?,
             Status::Ok
         );
-        let account =
-            AccountProxy::new(fasync::Channel::from_channel(account_client_chan).unwrap());
-        let account_auth_state = match await!(account.get_auth_state())? {
+        let account_proxy = account_client_end.into_proxy()?;
+        let account_auth_state = match await!(account_proxy.get_auth_state())? {
             (Status::Ok, Some(auth_state)) => *auth_state,
             (status, _) => return Err(format_err!("GetAuthState returned status: {:?}", status)),
         };
         assert_eq!(account_auth_state.summary, AuthStateSummary::Unknown);
 
         // Connect a channel to the account's default persona and verify it's usable.
-        let (persona_server_chan, persona_client_chan) = zx::Channel::create()?;
-        assert_eq!(
-            await!(account.get_default_persona(ServerEnd::new(persona_server_chan)))?.0,
-            Status::Ok
-        );
-        let persona =
-            AccountProxy::new(fasync::Channel::from_channel(persona_client_chan).unwrap());
-        let persona_auth_state = match await!(persona.get_auth_state())? {
+        let (persona_client_end, persona_server_end) = create_endpoints()?;
+        assert_eq!(await!(account_proxy.get_default_persona(persona_server_end))?.0, Status::Ok);
+        let persona_proxy = persona_client_end.into_proxy()?;
+        let persona_auth_state = match await!(persona_proxy.get_auth_state())? {
             (Status::Ok, Some(auth_state)) => *auth_state,
             (status, _) => return Err(format_err!("GetAuthState returned status: {:?}", status)),
         };
         assert_eq!(persona_auth_state.summary, AuthStateSummary::Unknown);
 
         // Delete both accounts and verify they are removed.
-        assert_eq!(
-            await!(account_manager.remove_account(&mut account_1))?,
-            Status::Ok
-        );
-        assert_eq!(
-            await!(account_manager.remove_account(&mut account_2))?,
-            Status::Ok
-        );
+        assert_eq!(await!(account_manager.remove_account(&mut account_1))?, Status::Ok);
+        assert_eq!(await!(account_manager.remove_account(&mut account_2))?, Status::Ok);
         assert_eq!(await!(account_manager.get_account_ids())?, vec![]);
 
         Ok(())
