6e67eb721225927f076f924251d80349cff6397a
ankitdave <ankitdave@google.com>
2019-02-27 15:59:41 -0800

    [a11y] Updating semantic tree manager.
    This cl adds semantic tree code in the new semantic tree manager.
    
    Testing:
      * Code Builds
    
    MI4-1736 #comment
    
    Change-Id: I8cdd8d335873885e3ecdc11a1e65e918ef409f7a

--- garnet/bin/a11y/a11y_manager/semantics/semantic_tree_impl.cc
+++ garnet/bin/a11y/a11y_manager/semantics/semantic_tree_impl.cc
@@ -1,16 +1,96 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "semantic_tree_impl.h"
 
 namespace a11y_manager {
+
+const int kRootNode = 0;
+
+// Internal helper function to check if a point is within a bounding box.
+bool SemanticTreeImpl::BoxContainsPoint(
+    const fuchsia::ui::gfx::BoundingBox& box, const escher::vec2& point) const {
+  return box.min.x <= point.x && box.max.x >= point.x && box.min.y <= point.y &&
+         box.max.y >= point.y;
+}
+
+const fuchsia::accessibility::semantics::NodePtr SemanticTreeImpl::HitTest(
+    const std::unordered_map<uint32_t, fuchsia::accessibility::semantics::Node>&
+        nodes,
+    uint32_t starting_node_id, escher::vec4 coordinates) const {
+  auto it = nodes.find(starting_node_id);
+  if (it == nodes.end()) {
+    return nullptr;
+  }
+  escher::mat4 transform =
+      scenic_impl::gfx::Unwrap(*it->second.data()->transform());
+  escher::vec4 local_coordinates = transform * coordinates;
+  escher::vec2 point(local_coordinates[0], local_coordinates[1]);
+
+  if (!BoxContainsPoint(*it->second.data()->location(), point)) {
+    return nullptr;
+  }
+  for (auto child : *it->second.children_hit_test_order()) {
+    auto node = HitTest(nodes, child, local_coordinates);
+    if (node != nullptr) {
+      return node;
+    }
+  }
+  auto node_ptr = fuchsia::accessibility::semantics::Node::New();
+  it->second.Clone(node_ptr.get());
+  return node_ptr;
+}
+
+fuchsia::accessibility::semantics::NodePtr
+SemanticTreeImpl::GetHitAccessibilityNode(fuchsia::math::PointF point) {
+  escher::vec4 coordinate(point.x, point.y, 0, 1);
+  return HitTest(nodes_, kRootNode, coordinate);
+}
+
+fuchsia::accessibility::semantics::NodePtr
+SemanticTreeImpl::GetAccessibilityNode(uint32_t node_id) {
+  auto node_it = nodes_.find(node_id);
+  if (node_it == nodes_.end()) {
+    return nullptr;
+  }
+  auto node_ptr = fuchsia::accessibility::semantics::Node::New();
+  node_it->second.Clone(node_ptr.get());
+  return node_ptr;
+}
+
 void SemanticTreeImpl::Commit() {
-  // TODO
+  // TODO(MI4-2038): Update Commit method to detect cycles in tree
+  // and cleanup dangling subtrees.
+
+  // Commit uncommitted nodes.
+  for (auto& node_it : uncommitted_nodes_) {
+    nodes_[*node_it.node_id()] = std::move(node_it);
+  }
+  uncommitted_nodes_.clear();
+
+  // Remove nodes marked for deletion.
+  for (auto& node_it : uncommitted_deletes_) {
+    nodes_.erase(node_it);
+  }
+  uncommitted_deletes_.clear();
 }
 
 void SemanticTreeImpl::UpdateSemanticNodes(
     std::vector<fuchsia::accessibility::semantics::Node> nodes) {
-  // TODO
+  // TODO(MI4-2038): Update/Delete should happen in the order in which the
+  // request is received.
+  uncommitted_nodes_.insert(uncommitted_nodes_.end(),
+                            std::make_move_iterator(nodes.begin()),
+                            std::make_move_iterator(nodes.end()));
 }
 
 void SemanticTreeImpl::DeleteSemanticNodes(std::vector<uint32_t> node_ids) {
-  // TODO
+  // TODO(MI4-2038): Update/Delete should happen in the order in which the
+  // request is received.
+  uncommitted_deletes_.insert(uncommitted_deletes_.end(),
+                              std::make_move_iterator(node_ids.begin()),
+                              std::make_move_iterator(node_ids.end()));
 }
+
 }  // namespace a11y_manager
--- garnet/bin/a11y/a11y_manager/semantics/semantic_tree_impl.h
+++ garnet/bin/a11y/a11y_manager/semantics/semantic_tree_impl.h
@@ -1,28 +1,93 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef GARNET_BIN_A11Y_A11Y_MANAGER_SEMANTICS_SEMANTIC_TREE_IMPL_H_
 #define GARNET_BIN_A11Y_A11Y_MANAGER_SEMANTICS_SEMANTIC_TREE_IMPL_H_
 
+#include <unordered_map>
+
 #include <fuchsia/accessibility/semantics/cpp/fidl.h>
-#include "lib/fidl/cpp/binding_set.h"
+#include <fuchsia/math/cpp/fidl.h>
+#include <lib/fidl/cpp/binding_set.h>
+#include <lib/fxl/macros.h>
+
+#include "garnet/lib/ui/gfx/util/unwrap.h"
 
 namespace a11y_manager {
+
 class SemanticTreeImpl
     : public fuchsia::accessibility::semantics::SemanticTree {
  public:
   explicit SemanticTreeImpl(zx::event view_ref)
       : view_ref_(std::move(view_ref)) {}
 
- private:
-  // TODO(MI4-1736): Implement this based on intern code (semantic_tree.cc)
+  ~SemanticTreeImpl() override = default;
 
-  // |fuchsia::accessibility::semantics::SemanticsTree|:
+  // Provides a way to perform hit-testing for a front-end node with local view
+  // hit coordinates from Scenic. Returns the deepest node that the input
+  // touches. Currently, this only supports 2D hit-tests using bounding boxes.
+  fuchsia::accessibility::semantics::NodePtr GetHitAccessibilityNode(
+      fuchsia::math::PointF point);
+
+  // Provides a way to query a node with node id. This method returns
+  // a copy of the queried node. It may return a nullptr if no node is found.
+  fuchsia::accessibility::semantics::NodePtr GetAccessibilityNode(
+      uint32_t node_id);
+
+  // TODO(MI4-1736): Implement OnAccessibilityActionRequested().
+  // TODO(MI4-1736): Add logging support.
+
+ private:
+  // Semantic Tree for a particular view. Each client is responsible for
+  // maintaining the state of their tree. Nodes can be added, updated or
+  // deleted. Because the size of an update may exceed FIDL transfer limits,
+  // clients are responsible for breaking up changes into multiple update
+  // and delete calls that conform to these limits. The commit function must
+  // always be called at the end of a full update push to signal the end of
+  // an update.
+  // |fuchsia::accessibility::semantics::SemanticsTree|
   void Commit() override;
 
+  // |fuchsia::accessibility::semantics::SemanticsTree|
   void UpdateSemanticNodes(
       std::vector<fuchsia::accessibility::semantics::Node> nodes) override;
 
+  // |fuchsia::accessibility::semantics::SemanticsTree|
   void DeleteSemanticNodes(std::vector<uint32_t> node_ids) override;
 
+  // Internal recursive hit-test function using the cached tree. Returns a
+  // null pointer if no hit nodes were found. Returns a copy of the node
+  // (but not the subtree), to prevent tree modification.
+  // NOTE: This is a 2D hit test and only operates on bounding boxes of
+  // semantics nodes.
+  const fuchsia::accessibility::semantics::NodePtr HitTest(
+      const std::unordered_map<uint32_t,
+                               fuchsia::accessibility::semantics::Node>& nodes,
+      uint32_t starting_node_id, escher::vec4 coordinates) const;
+
+  // Internal helper function to check if a point is within a bounding box.
+  bool BoxContainsPoint(const fuchsia::ui::gfx::BoundingBox& box,
+                        const escher::vec2& point) const;
+
+  // List of committed, cached nodes for each front-end. We represent semantics
+  // tree as a map of local node ids to the actual node objects. All query
+  // operations should use the node information from these trees.
+  std::unordered_map<uint32_t /*node_id*/,
+                     fuchsia::accessibility::semantics::Node>
+      nodes_;
+
+  // List of nodes that should be updated or added to the
+  // tree on the next commit.
+  std::vector<fuchsia::accessibility::semantics::Node> uncommitted_nodes_;
+
+  // List of local node ids that should be removed from the next commit.
+  std::vector<uint32_t> /*node_ids*/ uncommitted_deletes_;
+
   zx::event view_ref_;
+
+  FXL_DISALLOW_COPY_AND_ASSIGN(SemanticTreeImpl);
 };
 }  // namespace a11y_manager
+
 #endif  // GARNET_BIN_A11Y_A11Y_MANAGER_SEMANTICS_SEMANTIC_TREE_IMPL_H_
