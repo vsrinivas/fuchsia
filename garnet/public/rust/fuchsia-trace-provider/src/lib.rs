c4750440ae746315926caa0e5ea1cb62b5ae6366
Tim Detwiler <tjdetwiler@google.com>
2019-02-27 13:04:27 -0500

    [rust][trace-provider] Add rust trace-provider crate.
    This is intended to be a short term solution to enable the collection
    of traces from rust programs. We reuse the C++ trace-provider library
    which depends on libasync. To support this we create a new thread to run
    a libasync loop to host this trace-provider.
    
    Test: Collected a trace of a rust program.
    Change-Id: I368a43dd176748c6bfd76c4daff4309d7ed0f7fc

---
+++ garnet/public/rust/fuchsia-trace-provider/src/lib.rs
@@ -0,0 +1,21 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Creates a trace provider service that enables traces created by a process
+/// to be collected by the system trace manager.
+///
+/// Typically applications would call this method early in their main function
+/// to enable them to be eligible to produce traces.
+pub fn trace_provider_create() {
+    unsafe {
+        sys::trace_provider_create_rust();
+    }
+}
+
+mod sys {
+    #[link(name = "rust-trace-provider")]
+    extern "C" {
+        pub fn trace_provider_create_rust();
+    }
+}
---
+++ garnet/public/rust/fuchsia-trace-provider/trace_provider.cc
@@ -0,0 +1,30 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <thread>
+
+#include <lib/async-loop/cpp/loop.h>
+#include <trace-provider/provider.h>
+#include <zircon/compiler.h>
+
+__BEGIN_CDECLS
+
+void trace_provider_create_rust() __attribute__((visibility("default")));
+
+__END_CDECLS
+
+// The C++ trace provider API depends on libasync. Create a new thread here
+// to run a libasync loop here to host that trace provider.
+//
+// This is intended to be a temporary solution until we have a proper rust
+// trace-provider implementation.
+static void trace_provider_thread_entry() {
+  async::Loop loop(&kAsyncLoopConfigNoAttachToThread);
+  trace::TraceProvider trace_provider(loop.dispatcher());
+  loop.Run();
+}
+
+void trace_provider_create_rust() {
+  new std::thread(&trace_provider_thread_entry);
+}
