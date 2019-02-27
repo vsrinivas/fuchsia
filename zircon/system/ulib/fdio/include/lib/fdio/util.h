97d52637dc00baf768177e495be4f2c28b10dffc
Adam Barth <abarth@google.com>
2019-02-27 09:50:54 -0800

    [fdio] Add PA_FDIO_SOCKET to util.h
    This declaration is deprecated but needed by old versions of googletest.
    Once all clients update to newer versions of googltest, we can remove
    this entire header.
    
    Change-Id: I320f66700000e22a82ad7ebcf837c51ce2c5ae24

--- zircon/system/ulib/fdio/include/lib/fdio/util.h
+++ zircon/system/ulib/fdio/include/lib/fdio/util.h
@@ -5,17 +5,19 @@
 #ifndef LIB_FDIO_UTIL_H_
 #define LIB_FDIO_UTIL_H_
 
-#include <zircon/types.h>
-#include <zircon/compiler.h>
-#include <stdint.h>
-#include <unistd.h>
+#include <lib/fdio/directory.h>
 #include <lib/fdio/fd.h>
 #include <lib/fdio/fdio.h>
-#include <lib/fdio/directory.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <zircon/compiler.h>
+#include <zircon/processargs.h>
+#include <zircon/types.h>
 
 __BEGIN_CDECLS
 
 // BEGIN DEPRECATED ------------------------------------------------------------
+#define PA_FDIO_SOCKET PA_FD
 zx_status_t fdio_create_fd(zx_handle_t* handles, uint32_t* types, size_t hcount, int* fd_out);
 // END DEPRECATED --------------------------------------------------------------
 
