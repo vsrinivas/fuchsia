b61585f711c96daa215c4e6b9c841c850dbabdd3
Ryan Macnak <rmacnak@google.com>
2019-02-27 11:03:01 -0800

    [bin/traceutil] Prepare for Dart timeline streams being exposed as Fuchsia tracing categories.
    Change-Id: Ic6abbdbb6751ecdd2fd03c3f04b0425b18e36115

--- garnet/bin/traceutil/actions.go
+++ garnet/bin/traceutil/actions.go
@@ -13,7 +13,7 @@ import (
 	"github.com/golang/glog"
 )
 
-const defaultCategories = "app,benchmark,gfx,input,kernel:meta,kernel:sched,ledger,magma,modular,motown,view,flutter,dart"
+const defaultCategories = "app,benchmark,gfx,input,kernel:meta,kernel:sched,ledger,magma,modular,motown,view,flutter,dart,dart:compiler,dart:dart,dart:debugger,dart:embedder,dart:gc,dart:isolate,dart:profiler,dart:vm"
 
 type captureTraceConfig struct {
 	Categories           string
