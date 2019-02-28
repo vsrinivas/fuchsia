0c1455fab5d0a1c9eb186f84dd6c26ae76f46d9d
Brett Wilson <brettw@google.com>
2019-02-28 12:55:36 -0800

    [debugger] Move settings verbs to a separate file.
    These are getting big and it helps to have the settings-related cmopmand
    code in a separate file.
    
    There should be no behavior change.
    
    Change-Id: Ie3ac1ea8ea637a2fd4146801f8ee8d4509563cb6

--- garnet/bin/zxdb/console/verbs.h
+++ garnet/bin/zxdb/console/verbs.h
@@ -138,6 +138,7 @@ void AppendBreakpointVerbs(std::map<Verb, VerbRecord>* verbs);
 void AppendControlVerbs(std::map<Verb, VerbRecord>* verbs);
 void AppendMemoryVerbs(std::map<Verb, VerbRecord>* verbs);
 void AppendProcessVerbs(std::map<Verb, VerbRecord>* verbs);
+void AppendSettingsVerbs(std::map<Verb, VerbRecord>* verbs);
 void AppendSharedVerbs(std::map<Verb, VerbRecord>* verbs);
 void AppendSymbolVerbs(std::map<Verb, VerbRecord>* verbs);
 void AppendSystemVerbs(std::map<Verb, VerbRecord>* verbs);
