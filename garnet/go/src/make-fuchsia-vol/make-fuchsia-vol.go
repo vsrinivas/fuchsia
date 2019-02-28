e92385a44485f9bd0aaa4e890d358ae0b12d9dce
Venkatesh Srinivas <venkateshs@google.com>
2019-02-28 17:34:34 -0500

    [garnet] make-fuchsia-vol: Update patch to bootx64.efi
    After the gn transition, the path to bootx64.efi changed, from
    build-x64 to efi-x64-clang. Update make-fuchsia-vol (used by fx
    gce) to account for the new path.
    
    BLD-377
    
    Tested:
    
    fx gce create-fuchsia-image now works.
    
    Change-Id: Ieb794e6a9fe87f505fb3cf40ed7c6716230f5d65

--- garnet/go/src/make-fuchsia-vol/make-fuchsia-vol.go
+++ garnet/go/src/make-fuchsia-vol/make-fuchsia-vol.go
@@ -131,7 +131,7 @@ func main() {
 
 	if *bootloader == "" {
 		needZirconBuildDir()
-		*bootloader = filepath.Join(*zirconBuildDir, "bootloader/bootx64.efi")
+		*bootloader = filepath.Join(*zirconBuildDir, "../efi-x64-clang/bootx64.efi")
 	}
 	if _, err := os.Stat(*bootloader); err != nil {
 		log.Fatalf("cannot read %q: %s", *bootloader, err)
