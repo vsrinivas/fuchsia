771384d4ee2d368eabb9e0f75858b01ca28021e0
Nick Pollard <nickpollard@gmail.com>
2019-03-04 15:16:22 -0800

    [bt][bt-gap] Output top-level bt-gap errors to syslog
    When bt-gap errors and returns Error from the top level function, log
    that error to syslog before returning it from the process. This aids in
    diagnostics if another component requires a service from bt-gap, and
    bt-gap fails during execution, as the error will now be easily visible
    by reading syslog.
    
    test: Run bt-gap in a situation where it will fail (For a motivating
    example, in a hermetic testing environment where the Stash service is
    not avaialble), and then run `$ fx syslog` and check for the error
    output.
    
    Change-Id: Ic1f0320289f15f36b96f4b2f0db9f34e90c7820b

--- garnet/bin/bluetooth/bt-gap/src/main.rs
+++ garnet/bin/bluetooth/bt-gap/src/main.rs
@@ -2,14 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![feature(
-    futures_api,
-    await_macro,
-    async_await
-)]
+#![feature(futures_api, await_macro, async_await)]
 
 // Macros used to serialize bonding data FIDL types for persistent storage.
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
 use {
     failure::{Error, ResultExt},
@@ -21,13 +18,13 @@ use {
     fuchsia_app::server::ServicesServer,
     fuchsia_async as fasync,
     fuchsia_bluetooth::util,
-    fuchsia_syslog::{self as syslog, fx_log_info},
+    fuchsia_syslog::{self as syslog, fx_log_err, fx_log_info},
     futures::{TryFutureExt, TryStreamExt},
 };
 
 use crate::{
-    adapters::{*, AdapterEvent::*},
-    host_dispatcher::{*, HostService::*},
+    adapters::{AdapterEvent::*, *},
+    host_dispatcher::{HostService::*, *},
 };
 
 mod services;
@@ -42,9 +39,18 @@ const BT_GAP_COMPONENT_ID: &'static str = "bt-gap";
 fn main() -> Result<(), Error> {
     syslog::init_with_tags(&["bt-gap"]).expect("Can't init logger");
     fx_log_info!("Starting bt-gap...");
+    let result = run().context("Error running BT-GAP");
+    if let Err(e) = &result {
+        fx_log_err!("{:?}", e)
+    };
+    Ok(result?)
+}
 
+fn run() -> Result<(), Error> {
     let mut executor = fasync::Executor::new().context("Error creating executor")?;
-    let stash = executor.run_singlethreaded(store::stash::init_stash(BT_GAP_COMPONENT_ID))?;
+    let stash = executor
+        .run_singlethreaded(store::stash::init_stash(BT_GAP_COMPONENT_ID))
+        .context("Error initializing Stash service")?;
 
     let hd = HostDispatcher::new(stash);
     let watch_hd = hd.clone();
@@ -67,23 +73,26 @@ fn main() -> Result<(), Error> {
     let server = ServicesServer::new()
         .add_service((ControlMarker::NAME, move |chan: fasync::Channel| {
             control_service(control_hd.clone(), chan)
-        })).add_service((CentralMarker::NAME, move |chan: fasync::Channel| {
+        }))
+        .add_service((CentralMarker::NAME, move |chan: fasync::Channel| {
             fx_log_info!("Connecting CentralService to Adapter");
             fasync::spawn(central_hd.clone().request_host_service(chan, LeCentral))
-        })).add_service((PeripheralMarker::NAME, move |chan: fasync::Channel| {
+        }))
+        .add_service((PeripheralMarker::NAME, move |chan: fasync::Channel| {
             fx_log_info!("Connecting Peripheral Service to Adapter");
             fasync::spawn(peripheral_hd.clone().request_host_service(chan, LePeripheral))
-        })).add_service((ProfileMarker::NAME, move |chan: fasync::Channel| {
+        }))
+        .add_service((ProfileMarker::NAME, move |chan: fasync::Channel| {
             fx_log_info!("Connecting Profile Service to Adapter");
             fasync::spawn(profile_hd.clone().request_host_service(chan, Profile))
-        })).add_service((Server_Marker::NAME, move |chan: fasync::Channel| {
+        }))
+        .add_service((Server_Marker::NAME, move |chan: fasync::Channel| {
             fx_log_info!("Connecting Gatt Service to Adapter");
             fasync::spawn(gatt_hd.clone().request_host_service(chan, LeGatt))
-        })).start()?;
+        }))
+        .start()?;
 
-    executor
-        .run_singlethreaded(server.try_join(host_watcher))
-        .map(|_| ())
+    executor.run_singlethreaded(server.try_join(host_watcher)).map(|_| ())
 }
 
 fn control_service(hd: HostDispatcher, chan: fasync::Channel) {
