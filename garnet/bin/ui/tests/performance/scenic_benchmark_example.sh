c233766bfa7fa455bb5c1e58161ed72ca2e4d176
Mikael Pessa <mikaelpessa@gmail.com>
2019-03-04 15:14:34 -0800

    [scenic] Fix scenic_benchmark_example.sh
    Change-Id: I66a0e5b455559a89a4787eee6e457ae56eb1a77b

--- garnet/bin/ui/tests/performance/scenic_benchmark_example.sh
+++ garnet/bin/ui/tests/performance/scenic_benchmark_example.sh
@@ -5,12 +5,17 @@
 # found in the LICENSE file.
 
 # Example script for running a basic benchmark
+#
+# To run, just run the script directly from the terminal:
+# /pkgfs/packages/scenic_benchmarks/0/bin/scenic_benchmark_example.sh
+# or with fx:
+# fx shell '/pkgfs/packages/scenic_benchmarks/0/bin/scenic_benchmark_example.sh'
+#
 
 killall scenic.cmx; killall basemgr; killall root_presenter.cmx; killall set_root_view; killall tiles; killall tiles.cmx
 
 # Run benchmark
 /pkgfs/packages/scenic_benchmarks/0/bin/run_scenic_benchmark.sh \
-    --out_dir /data \
     --out_file /data/bench \
     --benchmark_label BasicLatencyBenchmark \
     --sleep_before_trace 3 \
