207399ecf2ac58fdf7fd3643c4b53165b910de2d
Ina Huh <ihuh@google.com>
2019-03-04 10:33:48 -0800

    [health_checker] Add tool to check device health.
    Currently it uses the netboot tool to discover the netsvc and fuchsia
    addresses of the device and then prints true if it can discover both.
    
    Bug: IN-791 #comment
    Change-Id: I5a94083a0f81481ee73d1f45dea5be736ccdeaee

---
+++ cmd/health_checker/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"time"
+	"fuchsia.googlesource.com/tools/botanist"
+	"fuchsia.googlesource.com/tools/netboot"
+)
+
+const usage = `usage: health_checker [options]
+
+Checks the health of the attached device by checking to see if it can
+discover and ping the device's netsvc address. A healthy device should be
+running in Zedboot.
+`
+
+// Command line flag values
+var (
+	timeout    time.Duration
+	configFile string
+)
+
+const (
+	healthyState = "healthy"
+	unhealthyState = "unhealthy"
+)
+
+// DeviceHealthProperties contains health properties of a hardware device.
+type HealthCheckResult struct {
+	// Nodename is the hostname of the device that we want to boot on.
+	Nodename string `json:"nodename"`
+
+	// State is the health status of the device (either "healthy" or "unhealthy").
+	State string `json:"state"`
+
+	// ErrorMsg is the error message provided by the health check.
+	ErrorMsg string `json:"error_msg"`
+}
+
+func checkHealth(n *netboot.Client, nodename string) HealthCheckResult {
+	netsvcAddr, err := n.Discover(nodename, false)
+	if err != nil {
+		err = fmt.Errorf("Failed to discover netsvc addr: %v\n", err)
+		return HealthCheckResult{nodename, unhealthyState, err.Error()}
+	}
+	netsvcIpAddr := &net.IPAddr{IP: netsvcAddr.IP, Zone: netsvcAddr.Zone}
+	cmd := exec.Command("ping", "-6", netsvcIpAddr.String(), "-c", "1")
+	if _, err = cmd.Output(); err != nil {
+		err = fmt.Errorf("Failed to ping netsvc addr %s: %v\n", netsvcIpAddr, err)
+		return HealthCheckResult{nodename, unhealthyState, err.Error()}
+	}
+
+	// Device should be in Zedboot, so fuchsia address should be unpingable
+	fuchsiaAddr, err := n.Discover(nodename, true)
+	if err != nil {
+		err = fmt.Errorf("Failed to discover fuchsia addr: %v\n", err)
+		return HealthCheckResult{nodename, unhealthyState, err.Error()}
+	}
+	fuchsiaIpAddr := &net.IPAddr{IP: fuchsiaAddr.IP, Zone: fuchsiaAddr.Zone}
+	cmd = exec.Command("ping", "-6", fuchsiaIpAddr.String(), "-c", "1")
+	if _, err = cmd.Output(); err == nil {
+		return HealthCheckResult{nodename, unhealthyState, "Device is in Fuchsia, should be in Zedboot."}
+	}
+	return HealthCheckResult{nodename, healthyState, ""}
+}
+
+func printHealthCheckResults(checkResults []HealthCheckResult) error {
+	output, err := json.Marshal(checkResults)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(output))
+	return nil
+}
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, usage)
+		flag.PrintDefaults()
+	}
+
+	// First set the flags ...
+	flag.StringVar(&configFile, "config", "/etc/botanist/config.json",
+		"The path of the json config file that contains the nodename of the device. Format is defined in https://fuchsia.googlesource.com/tools/+/master/botanist/common.go")
+	flag.DurationVar(&timeout, "timeout", 3*time.Second,
+		"The timeout for checking each device. The format should be a value acceptable to time.ParseDuration.")
+}
+
+func main() {
+	flag.Parse()
+	client := netboot.NewClient(timeout)
+	var devices []botanist.DeviceProperties
+	devices, err := botanist.LoadDeviceProperties(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var checkResultSlice []HealthCheckResult
+	for _, deviceProperties := range devices {
+		nodename := deviceProperties.Nodename
+		if nodename == "" {
+			log.Fatal("Failed to retrieve nodename from config file")
+		}
+		checkResult := checkHealth(client, nodename)
+		checkResultSlice = append(checkResultSlice, checkResult)
+	}
+	if err = printHealthCheckResults(checkResultSlice); err != nil {
+		log.Fatal(err)
+	}
+}
+
