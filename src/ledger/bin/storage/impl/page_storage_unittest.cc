ae1b2ec74a5e04d70b7635dcb8342a0f0ec88c8a
Maria Glukhova <mariagl@google.com>
2019-03-07 10:58:37 +0100

    [ledger] Disable a flaking test.
    ledger_tests:ledger_unittests:PageStorageTest.FullDownloadAfterPartial
    is flaking. We have to disable it before attempting to fix it.
    
    TEST=fx run-test ledger_tests -t ledger_unittests reports 1 disabled
    test
    FLK-80
    
    Change-Id: I7abdb5df3fa079cdf9310fb21e4ce2bc8472090c

--- src/ledger/bin/storage/impl/page_storage_unittest.cc
+++ src/ledger/bin/storage/impl/page_storage_unittest.cc
@@ -1465,7 +1465,7 @@ TEST_F(PageStorageTest, GetObjectFromSync) {
                Status::NOT_CONNECTED_ERROR);
 }
 
-TEST_F(PageStorageTest, FullDownloadAfterPartial) {
+TEST_F(PageStorageTest, DISABLED_FullDownloadAfterPartial) {
   std::string data_str = RandomString(environment_.random(), 2 * 65536 + 1);
   int64_t offset = 0;
   int64_t size = 128;
