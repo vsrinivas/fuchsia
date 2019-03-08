4f8d9862d558de1091334f87b3229e04c83a2cd8
Eric Wang <eyw@google.com>
2019-03-08 15:51:27 -0800

    [wlan][rust-mlme] Introduce FakeDevice for testing
    Test: Included
    Change-Id: Ie0678e2a8d4534dc6aee9426b52df73d73ebde38

--- garnet/lib/rust/wlan-mlme/src/device.rs
+++ garnet/lib/rust/wlan-mlme/src/device.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use std::ffi::c_void;
+use {fuchsia_zircon as zx, std::ffi::c_void};
 
 #[repr(C)]
 pub struct Device {
@@ -13,5 +13,56 @@ pub struct Device {
 impl Device {
     pub fn deliver_ethernet(&self, slice: &[u8]) -> i32 {
         (self.deliver_ethernet)(self.device, slice.as_ptr(), slice.len())
+    }
+}
+
+#[cfg(test)]
+#[derive(Default)]
+pub struct FakeDevice {
+    pub eth_queue: Vec<Vec<u8>>,
+}
+
+#[cfg(test)]
+impl FakeDevice {
+    pub extern "C" fn deliver_ethernet(device: *mut c_void, data: *const u8, len: usize) -> i32 {
+        assert!(!device.is_null());
+        assert!(!data.is_null());
+        assert!(len != 0);
+        // safe here because slice will not outlive data
+        let slice = unsafe { std::slice::from_raw_parts(data, len) };
+        // safe here because device_ptr alwyas points to self
+        unsafe {
+            (*(device as *mut Self)).eth_queue.push(slice.to_vec());
+        }
+        zx::sys::ZX_OK
+    }
+
+    pub fn reset(&mut self) {
+        self.eth_queue.clear();
+    }
+
+    pub fn as_device(&mut self) -> Device {
+        Device {
+            device: self as *mut Self as *mut c_void,
+            deliver_ethernet: Self::deliver_ethernet,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fake_device_deliver_ethernet() {
+        let mut fake_device = FakeDevice::default();
+        assert_eq!(fake_device.eth_queue.len(), 0);
+        let first_frame = [5; 32];
+        let second_frame = [6; 32];
+        assert_eq!(fake_device.as_device().deliver_ethernet(&first_frame[..]), zx::sys::ZX_OK);
+        assert_eq!(fake_device.as_device().deliver_ethernet(&second_frame[..]), zx::sys::ZX_OK);
+        assert_eq!(fake_device.eth_queue.len(), 2);
+        assert_eq!(&fake_device.eth_queue[0], &first_frame);
+        assert_eq!(&fake_device.eth_queue[1], &second_frame);
     }
 }
