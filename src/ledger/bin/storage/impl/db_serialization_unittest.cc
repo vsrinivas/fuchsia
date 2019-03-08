42a9c6d0ff8c46893028f9a0ae84befe7ebcb352
Nelly Vouzoukidou <nellyv@google.com>
2019-03-08 16:47:19 +0100

    [ledger] Add versioning test for DbSerialization.
    LE-717
    
    Test:DbSerialization.SerializationVersionControl
    
    Add test that will fail whenever someone makes a serialization version
    update. It should be used as a guard against accidental changes to the
    stored row format.
    
    Change-Id: I02ec696d70f7c3714d15427d273dac2c7609ed88

--- src/ledger/bin/storage/impl/db_serialization_unittest.cc
+++ src/ledger/bin/storage/impl/db_serialization_unittest.cc
@@ -19,6 +19,79 @@ using ::testing::StartsWith;
 
 using DbSerialization = ::ledger::TestWithEnvironment;
 
+// Allows to create correct std::strings with \0 bytes inside from C-style
+// string constants.
+std::string operator"" _s(const char* str, size_t size) {
+  return std::string(str, size);
+}
+
+// This test makes sure nothing has changed in the rows serialization. If this
+// breaks, it means action needs to be taken to avoid breaking backward
+// compatibility.
+TEST_F(DbSerialization, SerializationVersionControl) {
+  // Head row.
+  EXPECT_EQ("heads/head", HeadRow::GetKeyFor("head"));
+
+  // Merge row.
+  EXPECT_EQ("merges/parent1/parent2/merge",
+            MergeRow::GetKeyFor("merge", "parent1", "parent2"));
+
+  // Commit row.
+  EXPECT_EQ("commits/commit", CommitRow::GetKeyFor("commit"));
+
+  // Object row.
+  EXPECT_EQ("objects/object", ObjectRow::GetKeyFor(ObjectDigest("object")));
+
+  // Reference row
+  fxl::StringView destination = "destination";
+  char destination_size = static_cast<char>(destination.size());
+  fxl::StringView destination_size_str(&destination_size, 1);
+  EXPECT_EQ(fxl::Concatenate({"refcounts/", destination_size_str,
+                              "destination/object/eager/source"}),
+            ReferenceRow::GetKeyForObject(ObjectDigest("source"),
+                                          ObjectDigest("destination"),
+                                          KeyPriority::EAGER));
+  EXPECT_EQ(fxl::Concatenate({"refcounts/", destination_size_str,
+                              "destination/object/lazy/source"}),
+            ReferenceRow::GetKeyForObject(ObjectDigest("source"),
+                                          ObjectDigest("destination"),
+                                          KeyPriority::LAZY));
+  EXPECT_EQ(
+      fxl::Concatenate(
+          {"refcounts/", destination_size_str, "destination/commit/source"}),
+      ReferenceRow::GetKeyForCommit("source", ObjectDigest("destination")));
+
+  // Unsynced Commit row.
+  EXPECT_EQ("unsynced/commits/commit", UnsyncedCommitRow::GetKeyFor("commit"));
+
+  // Object Status row.
+  ObjectIdentifier identifier(1u, 2u, ObjectDigest("object"));
+  // |ObjectIdentifier|s are serialized using FlatBuffers.
+  std::string identifier_serialization =
+      "\x10\0\0\0\0\0\n\0\x10\0\x4\0\b\0\f\0\n\0\0\0\x1\0\0\0\x2\0\0\0\x4\0\0\0\x6\0\0\0object\0\0"_s;
+
+  // Object Status: Transient row.
+  EXPECT_EQ(
+      fxl::Concatenate({"transient/object_digests/", identifier_serialization}),
+      ObjectStatusRow::GetKeyFor(PageDbObjectStatus::TRANSIENT, identifier));
+
+  // Object Status: Local row.
+  EXPECT_EQ(
+      fxl::Concatenate({"local/object_digests/", identifier_serialization}),
+      ObjectStatusRow::GetKeyFor(PageDbObjectStatus::LOCAL, identifier));
+
+  // Object Status: Synced row.
+  EXPECT_EQ(
+      fxl::Concatenate({"synced/object_digests/", identifier_serialization}),
+      ObjectStatusRow::GetKeyFor(PageDbObjectStatus::SYNCED, identifier));
+
+  // Sync Metadata row.
+  EXPECT_EQ("sync_metadata/metadata", SyncMetadataRow::GetKeyFor("metadata"));
+
+  // Sync Metadata row.
+  EXPECT_EQ("page_is_online", PageIsOnlineRow::kKey);
+}
+
 TEST_F(DbSerialization, MergeRow) {
   const CommitId commit1 = RandomCommitId(environment_.random());
   const CommitId commit2 = RandomCommitId(environment_.random());
