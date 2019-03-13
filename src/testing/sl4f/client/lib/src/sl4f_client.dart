7976afd633a7008074e5d2fc022c5b5caf4b80f3
Isma Cortes Vazquez <isma@google.com>
2019-03-13 15:20:04 -0700

    [sl4f][e2e] Add util methods to start sl4f via SSH, and use a more specific env variable for getting the IP address of the DUT.
    TESTED=made and ran a local test with this.
    
    Change-Id: I3c403e4c87cd28cb950781aac18291608649d245

--- src/testing/sl4f/client/lib/src/sl4f_client.dart
+++ src/testing/sl4f/client/lib/src/sl4f_client.dart
@@ -6,8 +6,9 @@
 // sl4f.dart, which would cause problems for src files trying to import this
 // file.
 
+import 'dart:async';
 import 'dart:convert';
-import 'dart:io' show Platform;
+import 'dart:io' show Platform, Process, SocketException;
 
 import 'package:http/http.dart' as http;
 
@@ -22,16 +23,30 @@ class JsonRpcException implements Exception {
   }
 }
 
+/// Any exception when dealing with the SL4F server itself.
+class Sl4fException implements Exception {
+  String error;
+  Sl4fException(this.error);
+}
+
+/// Handles the SL4F server and communication with it.
 class Sl4f {
   final _client = http.Client();
+
+  static const _sshUser = 'fuchsia';
+  static const _sl4fComponentUrl =
+      'fuchsia-pkg://fuchsia.com/sl4f#meta/sl4f.cmx';
+  static const _sl4fComponentName = 'sl4f.cmx';
+
+  /// Hostname of the device under test.
   String host;
 
   Sl4f(this.host) {
     print('Target host: $host');
   }
 
-  /// Constructs an SL4F client from the IP_ADDR environment variable.
-  Sl4f.fromEnvironment() : this(Platform.environment['IP_ADDR']);
+  /// Constructs an SL4F client from the FUCHSIA_IPV4_ADDR environment variable.
+  Sl4f.fromEnvironment() : this(Platform.environment['FUCHSIA_IPV4_ADDR']);
 
   /// Closes the underlying HTTP client. This need not be called if the
   /// Sl4f client is closed instead.
@@ -39,6 +54,7 @@ class Sl4f {
     _client.close();
   }
 
+  /// Sends a JSON-RPC request to SL4F.
   Future<dynamic> request(String method, [dynamic params]) async {
     // Although params is optional, this will pass a null params if it is
     // omitted. This is actually required by our SL4F server (although it is
@@ -56,5 +72,81 @@ class Sl4f {
     }
 
     return response['result'];
+  }
+
+  /// Starts the SL4F server on the host using ssh.
+  ///
+  /// If no ssh key path is given, it's taken from the FUCHSIA_SSH_KEY env var.
+  /// It returns throws a StateError if after this is ran SL4F has not started.
+  Future<void> startServer({String sshKeyPath}) async {
+    if (await _isRunning()) {
+      return;
+    }
+
+    final result = await _runSshCommand('run -d $_sl4fComponentUrl',
+        sshKeyPath: sshKeyPath);
+    if (!result) {
+      // TODO(isma): We should use a better exception type.
+      throw Sl4fException('Sl4f has not started.');
+    }
+
+    for (var i = 0; i < 3; i++) {
+      await Future.delayed(Duration(seconds: 2));
+      if (await _isRunning()) {
+        print('SL4F has started.');
+        return;
+      }
+    }
+    throw Sl4fException('Sl4f has not started.');
+  }
+
+  /// SSHs into the device and starts the SL4F server.
+  ///
+  /// If no ssh key path is given, it's taken from the FUCHSIA_SSH_KEY env var.
+  /// It returns false if it failed to call killall on the device.
+  Future<void> stopServer({String sshKeyPath}) async {
+    if (!await _runSshCommand('killall $_sl4fComponentName',
+        sshKeyPath: sshKeyPath)) {
+      throw Sl4fException('Could not stop Sl4f.');
+    }
+  }
+
+  /// Runs the given ssh command on the host.
+  Future<bool> _runSshCommand(String cmd, {String sshKeyPath}) async {
+    sshKeyPath ??= Platform.environment['FUCHSIA_SSH_KEY'];
+
+    print('Running over ssh: $cmd');
+    final result = await Process.run(
+        'ssh',
+        [
+          '-i',
+          sshKeyPath,
+          '-o',
+          'UserKnownHostsFile=/dev/null',
+          '-o',
+          'StrictHostKeyChecking=no',
+          '$_sshUser@$host',
+          cmd
+        ],
+        // If not ran in shell it doesn't seem like the PATH is searched.
+        runInShell: true);
+    if (result.exitCode != 0) {
+      print(result.stderr);
+      return false;
+    }
+    return true;
+  }
+
+  /// Sends an empty http request to the server to verify if it's Listening on
+  /// port 80.
+  Future<bool> _isRunning() async {
+    try {
+      await http.get(Uri.http(host, '/')).timeout(Duration(seconds: 10));
+    } on SocketException {
+      return false;
+    } on TimeoutException {
+      return false;
+    }
+    return true;
   }
 }
