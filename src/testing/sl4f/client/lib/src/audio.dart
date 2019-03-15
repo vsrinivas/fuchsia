1b022485d11df8a3d825a5e4bfc6bbc4e4c89e86
Pat Erley <perley@google.com>
2019-03-15 10:04:19 -0700

    [sl4f][e2e][dart] Soft transition APIs
    There was at least 1 internal consumer that was being built.  By
    breaking this API, we broke their build.
    
    TEST: Built 1 internal target with this, then updated them to the
          new APIs.
    Change-Id: Ib65f3279edb197d3aff845c0890798634d685a64

--- src/testing/sl4f/client/lib/src/audio.dart
+++ src/testing/sl4f/client/lib/src/audio.dart
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
 import 'dart:io' show File;
+import 'dart:typed_data' show Uint8List;
 import 'sl4f_client.dart';
 
 class Audio {
@@ -16,21 +18,56 @@ class Audio {
     _sl4f.close();
   }
 
-  // TODO(perley)
-  Future<void> putPlayback(File file) {
-    return Future.value(null);
+  // TODO(perley): Drop this after migrating internal clients.
+  Future<void> putPlayback(File file) async {
+    return await putInputAudio(0, file);
   }
 
-  Future<void> startCapture() {
-    return Future.value(null);
+  Future<void> putInputAudio(int index, File file) async {
+    final audioBytes = file.readAsBytesSync();
+    await _sl4f.request('audio_facade.PutInputAudio', {
+      'index': index,
+      'data': base64Encode(audioBytes),
+    });
   }
 
-  Future<AudioTrack> getCapture() {
-    return Future.value(AudioTrack());
+  Future<void> startInputInjection(int index) async {
+    await _sl4f.request('audio_facade.StartInputInjection', {
+      'index': index,
+    });
+  }
+
+  // TODO(perley): Drop this after migrating internal clients.
+  Future<void> startCapture() async {
+    return await startOutputSave();
+  }
+
+  Future<void> startOutputSave() async {
+    await _sl4f.request('audio_facade.StartOutputSave');
+  }
+
+  // TODO(perley): Drop this after migrating internal clients.
+  Future<AudioTrack> getCapture() async {
+    await stopOutputSave();
+    return await getOutputAudio();
+  }
+
+  Future<void> stopOutputSave() async {
+    await _sl4f.request('audio_facade.StopOutputSave');
+  }
+
+  Future<AudioTrack> getOutputAudio() async {
+    // The response body is just base64encoded audio
+    String response = await _sl4f.request('audio_facade.GetOutputAudio');
+    return AudioTrack()
+      ..audioData = base64Decode(response)
+      ..isSilence = response.isEmpty;
   }
 }
 
 class AudioTrack {
   // TODO: Implement a proper way to test this and a proper audio container.
-  bool get isSilence => false;
+  bool isSilence = false;
+  // This is a wav file
+  Uint8List audioData;
 }
