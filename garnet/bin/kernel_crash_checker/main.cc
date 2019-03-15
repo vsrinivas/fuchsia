a98f4e646319cb5b3ca56778b5e44c38c40cb4ee
Francois Rousseau <frousseau@google.com>
2019-03-15 16:05:33 -0700

    [crash] migrate kernel_crash_checker to sys/component
    * include clean up
    
    TESTED=`k crash`
    
    Change-Id: I824fa7af427743d3e77f0aca7e452b02307b4b35

--- garnet/bin/kernel_crash_checker/main.cc
+++ garnet/bin/kernel_crash_checker/main.cc
@@ -3,46 +3,36 @@
 // found in the LICENSE file.
 
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
+
+#include <string>
 
 #include <fuchsia/crash/cpp/fidl.h>
 #include <fuchsia/net/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
-#include <lib/component/cpp/startup_context.h>
-#include <lib/fdio/directory.h>
-#include <lib/fdio/fd.h>
-#include <lib/fdio/fdio.h>
-#include <lib/fidl/cpp/synchronous_interface_ptr.h>
 #include <lib/fsl/vmo/file.h>
 #include <lib/fsl/vmo/strings.h>
 #include <lib/fxl/logging.h>
+#include <lib/sys/cpp/startup_context.h>
 #include <lib/syslog/cpp/logger.h>
-#include <lib/zx/vmo.h>
 #include <zircon/errors.h>
 #include <zircon/status.h>
-#include <zircon/syscalls.h>
-#include <zircon/types.h>
 
 #include "src/lib/files/file.h"
 #include "src/lib/files/unique_fd.h"
 
 class CrashAnalyzer {
  public:
-  explicit CrashAnalyzer()
-      : context_(component::StartupContext::CreateFromStartupInfo()) {
+  CrashAnalyzer() : context_(sys::StartupContext::CreateFromStartupInfo()) {
     FXL_DCHECK(context_);
   }
 
   void ProcessCrashlog(fuchsia::mem::Buffer crashlog) {
     fuchsia::crash::AnalyzerSyncPtr analyzer;
-    context_->ConnectToEnvironmentService(analyzer.NewRequest());
+    context_->svc()->Connect(analyzer.NewRequest());
     FXL_DCHECK(analyzer);
 
-    zx_status_t out_status;
+    zx_status_t out_status = ZX_ERR_UNAVAILABLE;
     const zx_status_t status =
         analyzer->ProcessKernelPanicCrashlog(std::move(crashlog), &out_status);
     if (status != ZX_OK) {
@@ -56,7 +46,7 @@ class CrashAnalyzer {
   }
 
  private:
-  std::unique_ptr<component::StartupContext> context_;
+  std::unique_ptr<sys::StartupContext> context_;
 };
 
 int main(int argc, char** argv) {
@@ -66,26 +56,27 @@ int main(int argc, char** argv) {
   fxl::UniqueFD fd(open(filepath, O_RDONLY));
   if (!fd.is_valid()) {
     FX_LOGS(INFO) << "no kernel crash log found";
-    return 0;
+    return EXIT_SUCCESS;
   }
 
   fsl::SizedVmo crashlog_vmo;
   if (!fsl::VmoFromFd(std::move(fd), &crashlog_vmo)) {
     FX_LOGS(ERROR) << "error loading kernel crash log into VMO";
-    return 1;
+    return EXIT_FAILURE;
   }
 
   std::string crashlog_str;
   if (!fsl::StringFromVmo(crashlog_vmo, &crashlog_str)) {
     FX_LOGS(ERROR) << "error converting kernel crash log VMO to string";
-    return 1;
+    return EXIT_FAILURE;
   }
   FX_LOGS(INFO) << "dumping log from previous kernel panic:\n" << crashlog_str;
 
   async::Loop loop(&kAsyncLoopConfigAttachToThread);
   fuchsia::net::ConnectivityPtr connectivity =
-      component::StartupContext::CreateFromStartupInfo()
-          ->ConnectToEnvironmentService<fuchsia::net::Connectivity>();
+      sys::StartupContext::CreateFromStartupInfo()
+          ->svc()
+          ->Connect<fuchsia::net::Connectivity>();
   connectivity.events().OnNetworkReachable = [&crashlog_vmo](bool reachable) {
     if (!reachable) {
       return;
@@ -95,5 +86,5 @@ int main(int argc, char** argv) {
   };
   loop.Run();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
