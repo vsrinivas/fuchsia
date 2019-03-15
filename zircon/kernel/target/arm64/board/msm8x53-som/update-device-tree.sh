535b3ed7e7a5f7c90aad847dd69eb5ec31a3b28c
Mike Voydanoff <voydanoff@google.com>
2019-03-15 07:07:18 -0700

    [board][msm8x53-som] Add device tree support for other board variants.
    TEST: boots on other board variants.
    
    Change-Id: I5dc3d4fa67f69e6c4d47fcdf14148b72f4233b85

--- zircon/kernel/target/arm64/board/msm8x53-som/update-device-tree.sh
+++ zircon/kernel/target/arm64/board/msm8x53-som/update-device-tree.sh
@@ -1,1 +1,4 @@
 dtc -I dts -o device-tree.dtb -O dtb device-tree.dts
+dtc -I dts -o device-tree-2.dtb -O dtb device-tree-2.dts
+dtc -I dts -o device-tree-3.dtb -O dtb device-tree-3.dts
+cat device-tree-2.dtb device-tree-3.dtb >> device-tree.dtb
