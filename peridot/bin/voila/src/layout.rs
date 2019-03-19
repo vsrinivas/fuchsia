0374f9d9d2da346cb16cf8841cb691c5c1f5cf94
Przemek Pietrzkiewicz <ppi@google.com>
2019-03-19 14:14:33 +0000

    [voila] migrate layout to euclid and fix inset
    This patch follows
    https://fuchsia-review.googlesource.com/c/fuchsia/+/255074 and changes
    the geometry primitives used in Voila to align with Carnelian.
    
    This is in preparation of extending the layout logic to cover a
    controller toggle for connectivity (to be added in subsequent CLs).
    
    We also fix a bug in inset.
    
    LE-663
    
    Test: tiles_ctl add fuchsia-pkg://fuchsia.com/voila#meta/voila.cmx
    Change-Id: I3885d50914c5d7353356b58d0cca0264508d3ca2

--- peridot/bin/voila/src/layout.rs
+++ peridot/bin/voila/src/layout.rs
@@ -2,14 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use carnelian::Size;
-use fidl_fuchsia_math::RectF;
+use carnelian::{Point, Rect, Size};
 use fidl_fuchsia_ui_gfx::{BoundingBox, Vec3, ViewProperties};
 use fuchsia_scenic::{EntityNode, ViewHolder};
 
 /// Container for data related to a single child view displaying an emulated session.
 pub struct ChildViewData {
-    bounds: Option<RectF>,
+    bounds: Option<Rect>,
     host_node: EntityNode,
     host_view_holder: ViewHolder,
 }
@@ -36,17 +35,15 @@ pub fn layout(child_views: &mut [&mut ChildViewData], size: &Size) -> Result<(),
     let tile_height = size.height;
     let tile_width = (size.width / num_views as f32).floor();
     for (column_index, view) in child_views.iter_mut().enumerate() {
-        let tile_bounds = RectF {
-            height: tile_height,
-            width: tile_width,
-            x: column_index as f32 * tile_width,
-            y: 0.0,
-        };
+        let tile_bounds = Rect::new(
+            Point::new(column_index as f32 * tile_width, 0.0),
+            Size::new(tile_width, tile_height),
+        );
         let tile_bounds = inset(&tile_bounds, 5.0);
         let view_properties = ViewProperties {
             bounding_box: BoundingBox {
                 min: Vec3 { x: 0.0, y: 0.0, z: 0.0 },
-                max: Vec3 { x: tile_bounds.width, y: tile_bounds.height, z: 0.0 },
+                max: Vec3 { x: tile_bounds.size.width, y: tile_bounds.size.height, z: 0.0 },
             },
             inset_from_min: Vec3 { x: 0.0, y: 0.0, z: 0.0 },
             inset_from_max: Vec3 { x: 0.0, y: 0.0, z: 0.0 },
@@ -54,21 +51,23 @@ pub fn layout(child_views: &mut [&mut ChildViewData], size: &Size) -> Result<(),
             downward_input: false,
         };
         view.host_view_holder.set_view_properties(view_properties);
-        view.host_node.set_translation(tile_bounds.x, tile_bounds.y, 0.0);
+        view.host_node.set_translation(tile_bounds.origin.x, tile_bounds.origin.y, 0.0);
         view.bounds = Some(tile_bounds);
     }
     Ok(())
 }
 
-fn inset(rect: &RectF, border: f32) -> RectF {
-    let inset = border.min(rect.width / 0.3).min(rect.height / 0.3);
+/// Reduces the given bounds to leave the given amount of padding around it.
+///
+/// The added padding is no bigger than a third of the smaller dimension of the
+/// bounds.
+fn inset(rect: &Rect, padding: f32) -> Rect {
+    let inset = padding.min(rect.size.width / 3.0).min(rect.size.height / 3.0);
     let double_inset = inset * 2.0;
-    RectF {
-        x: rect.x + inset,
-        y: rect.y + inset,
-        width: rect.width - double_inset,
-        height: rect.height - double_inset,
-    }
+    Rect::new(
+        Point::new(rect.origin.x + inset, rect.origin.y + inset),
+        Size::new(rect.size.width - double_inset, rect.size.height - double_inset),
+    )
 }
 
 #[cfg(test)]
@@ -77,23 +76,35 @@ mod tests {
 
     #[test]
     fn inset_empty_returns_empty() {
-        let empty = RectF { x: 0.0, y: 0.0, width: 0.0, height: 0.0 };
+        let empty = Rect::zero();
 
         let result = inset(&empty, 2.0);
-        assert_eq!(result.x, 0.0);
-        assert_eq!(result.y, 0.0);
-        assert_eq!(result.width, 0.0);
-        assert_eq!(result.height, 0.0);
+
+        assert_eq!(result, Rect::zero());
     }
 
     #[test]
     fn inset_non_empty_works_correctly() {
-        let empty = RectF { x: 1.0, y: 3.0, width: 10.0, height: 8.0 };
+        let bounds = Rect::new(Point::new(1.0, 3.0), Size::new(10.0, 8.0));
 
-        let result = inset(&empty, 2.0);
-        assert_eq!(result.x, 3.0);
-        assert_eq!(result.y, 5.0);
-        assert_eq!(result.width, 6.0);
-        assert_eq!(result.height, 4.0);
+        let result = inset(&bounds, 2.0);
+
+        assert_eq!(result.origin.x, 3.0);
+        assert_eq!(result.origin.y, 5.0);
+        assert_eq!(result.size.width, 6.0);
+        assert_eq!(result.size.height, 4.0);
+    }
+
+    #[test]
+    fn inset_padding_is_at_most_a_third_of_dimension() {
+        let bounds = Rect::new(Point::new(0.0, 0.0), Size::new(9.0, 10.0));
+
+        let result = inset(&bounds, 5.0);
+
+        // Verify that the actual padding was 3.0, not 5.0.
+        assert_eq!(result.origin.x, 3.0);
+        assert_eq!(result.origin.y, 3.0);
+        assert_eq!(result.size.width, 3.0);
+        assert_eq!(result.size.height, 4.0);
     }
 }
