f60106cf3ff26b308aaa868506b9154747306f71
Yifei Teng <yifeit@google.com>
2019-03-19 19:06:56 +0000

    [fs] Reland "Hierarchical directory rights" step 1: C fdio
    Adds the C fdio portion of changes in
    https://fuchsia-review.googlesource.com/c/fuchsia/+/249124/
    such that they can be picked up by any upstream software which
    depends on a binary prebuilt fdio from the SDK.
    
    TEST: CQ
    Change-Id: Ibb69fec85d97c84f1040a60b4d55eadde0830f18

--- zircon/system/public/zircon/device/vfs.h
+++ zircon/system/public/zircon/device/vfs.h
@@ -7,36 +7,36 @@
 
 #include <zircon/types.h>
 
+// NOTE: All the defines here with the exception of ZX_FS_MAX_RIGHTS and ZX_FS_RIGHTS_MASK are
+// mirrored from the constants in io.fidl, and their values must be kept in sync.
+// The FIDL definition is the source of truth:
+// https://fuchsia.googlesource.com/fuchsia/+/master/zircon/system/fidl/fuchsia-io/io.fidl
+// Refer to link for documentation on detailed semantics of the flags.
+// TODO(yifeit): Assert that these values are kept in sync with io.fidl. Cannot do it here as this
+// header is used for both Fuchsia and host.
+
 // Rights
-// The file may be read.
-#define ZX_FS_RIGHT_READABLE 0x00000001
-// The file may be written.
-#define ZX_FS_RIGHT_WRITABLE 0x00000002
-// The connection can mount and unmount filesystems.
-#define ZX_FS_RIGHT_ADMIN 0x00000004
-#define ZX_FS_RIGHTS 0x0000FFFF
+#define ZX_FS_RIGHT_READABLE      0x00000001U
+#define ZX_FS_RIGHT_WRITABLE      0x00000002U
+#define ZX_FS_RIGHT_ADMIN         0x00000004U
+// All known rights.
+#define ZX_FS_RIGHTS              (ZX_FS_RIGHT_READABLE | ZX_FS_RIGHT_WRITABLE | ZX_FS_RIGHT_ADMIN)
+// A mask for all possible rights including future extensions.
+#define ZX_FS_RIGHTS_SPACE        0x0000FFFFU
+// NOTE: Reserving lower 16 bits for future rights extensions. Flags should start at 0x00010000.
 
 // Flags
-// If the file does not exist, it will be created.
-#define ZX_FS_FLAG_CREATE 0x00010000
-// The file must not exist, otherwise an error will be returned.
-// Ignored without ZX_FS_FLAG_CREATE.
-#define ZX_FS_FLAG_EXCLUSIVE 0x00020000
-// Truncates the file before using it.
-#define ZX_FS_FLAG_TRUNCATE 0x00040000
-// Returns an error if the opened file is not a directory.
-#define ZX_FS_FLAG_DIRECTORY 0x00080000
-// The file is opened in append mode, seeking to the end of the file before each
-// write.
-#define ZX_FS_FLAG_APPEND 0x00100000
-// If the endpoint of this request refers to a mount point, open the local
-// directory, not the remote mount.
-#define ZX_FS_FLAG_NOREMOTE 0x00200000
-// The file underlying file should not be opened, just a reference to the file.
-#define ZX_FS_FLAG_VNODE_REF_ONLY 0x00400000
-// When the file has been opened, the server should transmit a description event.
-// This event will be transmitted either on success or failure.
-#define ZX_FS_FLAG_DESCRIBE 0x00800000
+#define ZX_FS_FLAG_CREATE            0x00010000U
+#define ZX_FS_FLAG_EXCLUSIVE         0x00020000U
+#define ZX_FS_FLAG_TRUNCATE          0x00040000U
+#define ZX_FS_FLAG_DIRECTORY         0x00080000U
+#define ZX_FS_FLAG_APPEND            0x00100000U
+#define ZX_FS_FLAG_NOREMOTE          0x00200000U
+#define ZX_FS_FLAG_VNODE_REF_ONLY    0x00400000U
+#define ZX_FS_FLAG_DESCRIBE          0x00800000U
+#define ZX_FS_FLAG_POSIX             0x01000000U
+#define ZX_FS_FLAG_NOT_DIRECTORY     0x02000000U
+#define ZX_FS_FLAG_CLONE_SAME_RIGHTS 0x04000000U
 
 // Watch event messages are sent via the provided channel and take the form:
 // { uint8_t event; uint8_t namelen; uint8_t name[namelen]; }
@@ -51,4 +51,5 @@ typedef struct {
 #define VFS_TYPE_BLOBFS 0x9e694d21
 #define VFS_TYPE_MINFS 0x6e694d21
 #define VFS_TYPE_MEMFS 0x3e694d21
+
 #endif  // SYSROOT_ZIRCON_DEVICE_VFS_H_
