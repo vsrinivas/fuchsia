a5a0bd01bf3b27eaec586e8783009ce3a143cfc8
Oliver Hunt <ojh@google.com>
2019-03-20 05:40:03 +0000

    [webpki-roots-fuchsia] Initial commit
    Bug: NET-2152
    
    A simple crate to expose /config/ssl/cert.pem as a drop in replacement
    for webpki_roots. Simply creates a chain of lazy_static! globals to ensure
    we obtain the correct lifetime constraints to deal with the reference laden
    webpki root types.
    
    Change-Id: I257b045bfa90440d557c5e24c3b6b6cda4410357

---
+++ garnet/lib/rust/webpki-roots-fuchsia/src/lib.rs
@@ -0,0 +1,69 @@
+#![deny(warnings)]
+extern crate base64;
+extern crate untrusted;
+extern crate webpki;
+
+#[macro_use]
+extern crate lazy_static;
+
+use webpki::trust_anchor_util::cert_der_as_trust_anchor;
+
+static CERT_PATH: &'static str = "/config/ssl/cert.pem";
+
+lazy_static! {
+    // To meet the required lifetime constraints we need to chain together
+    // a series of statics.
+    static ref RAW_DATA: String = {
+        // I could have an environment variable override this, but i'd want it keyed
+        // on being a dev build so you couldn't harm trust in prod
+        std::fs::read_to_string(CERT_PATH).expect("Unable to find root store")
+    };
+    static ref CERT_DERS: Vec<Vec<u8>> = {
+        let lines: Vec<&str> = RAW_DATA
+            .split('\n')
+            .filter(|l| !l.is_empty() && !l.starts_with(':') && !l.starts_with('#'))
+            .collect();
+        let mut cert_ders = vec![];
+        let mut i = 0;
+        while i < lines.len() {
+            let start = i;
+            if lines[i] != "-----BEGIN CERTIFICATE-----" {
+                panic!("Missing certificate prefix");
+            }
+            while i < lines.len() && lines[i] != "-----END CERTIFICATE-----" {
+                i += 1;
+            }
+            if i == lines.len() {
+                panic!("Missing certificate suffix");
+            }
+            let end = i;
+            i += 1;
+            let cert_base64 = &lines[start + 1..end].join("");
+
+            let cert_bytes = base64::decode(cert_base64.as_bytes())
+                .expect("Invalid base64 encoding in root store");
+            cert_ders.push(cert_bytes);
+        }
+        cert_ders
+    };
+    static ref ROOTS: Vec<webpki::TrustAnchor<'static>> = {
+        CERT_DERS.iter().map(|cert_bytes| {
+            untrusted::Input::from(&cert_bytes)
+                .cert_der_as_trust_anchor(cert_der)
+                .expect("Parsing root certificate failed")
+        })
+    };
+
+    pub static ref TLS_SERVER_ROOTS: webpki::TLSServerTrustAnchors<'static> =
+        webpki::TLSServerTrustAnchors(&ROOTS);
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_load() {
+        let webpki::TLSServerTrustAnchors(roots) =
+            &crate::TLS_SERVER_ROOTS as &webpki::TLSServerTrustAnchors<'static>;
+        assert_ne!(roots.len(), 0);
+    }
+}
