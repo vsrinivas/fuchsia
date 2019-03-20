380df238ef23f8ee1d22779878a1e2aca542f448
Mohan Srinivasan <srmohan@google.com>
2019-03-20 19:45:47 +0000

    [dev][acpi] Remove ACPI code from the x86 board driver.
    Separate ACPI code from the x86 board driver. In future, we can move
    the ACPI namespace walk code and drivers into a separate library.
    
    Test: Boot NUC, Pixelbook and make sure ACPI devices are initialized.
    Change-Id: I0b8a5de90adeb70777d74947aeb64eff25666cf9

---
+++ zircon/system/dev/board/x86/include/acpi.h
@@ -0,0 +1,17 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <ddk/device.h>
+#include <ddk/protocol/platform/bus.h>
+#include <zircon/compiler.h>
+
+__BEGIN_CDECLS
+
+zx_status_t acpi_init(void);
+zx_status_t publish_acpi_devices(zx_device_t* parent, zx_device_t* sys_root, zx_device_t* acpi_root);
+zx_status_t acpi_suspend(uint32_t flags);
+
+__END_CDECLS
