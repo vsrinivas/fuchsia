9dcce9b005a95cbf6bcdb3b25e430c42afb4cd60
Adam Barth <abarth@google.com>
2019-03-22 21:44:27 +0000

    [url] Use lib/icu_data/cpp
    Rather than duplicating the code.
    
    Change-Id: I2da5f92f79dd42d9f3254a435dd6d64e865066bd

--- src/lib/url/url_unittests_main.cc
+++ src/lib/url/url_unittests_main.cc
@@ -2,35 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "gtest/gtest.h"
-#include "lib/fsl/vmo/file.h"
-#include "lib/fsl/vmo/sized_vmo.h"
-#include "lib/fxl/logging.h"
-#include "third_party/icu/source/common/unicode/errorcode.h"
-#include "third_party/icu/source/common/unicode/udata.h"
+#include <gtest/gtest.h>
+#include <lib/fxl/logging.h>
 
-constexpr char kIcuDataPath[] = "/pkg/data/icudtl.dat";
+#include "src/lib/icu_data/cpp/icu_data.h"
 
 int main(int argc, char **argv) {
-  fsl::SizedVmo icu_data;
-  if (!fsl::VmoFromFilename(kIcuDataPath, &icu_data)) {
-    FXL_LOG(ERROR) << "Unable to load ICU data. Timezone data unavailable.";
-    return 1;
-  }
-
-  // Maps the ICU VMO into this process.
-  uintptr_t icu_data_addr = 0;
-  if (zx_vmar_map(zx_vmar_root_self(), ZX_VM_PERM_READ, 0, icu_data.vmo().get(),
-                  0, icu_data.size(), &icu_data_addr) != ZX_OK) {
-    FXL_LOG(ERROR) << "Unable to map ICU data into process.";
-    return 1;
-  }
-
-  // Initialize ICU with this data.
-  UErrorCode icu_set_data_status = U_ZERO_ERROR;
-  udata_setCommonData(reinterpret_cast<void *>(icu_data_addr),
-                      &icu_set_data_status);
-  if (icu_set_data_status != U_ZERO_ERROR) {
+  if (!icu_data::Initialize()) {
     FXL_LOG(ERROR) << "Unable to set common ICU data. "
                    << "Timezone data unavailable.";
     return 1;
