a754dee90c3d431fe4981afede21781047df135b
Carl Norum <cjn@google.com>
2019-03-22 23:33:59 +0000

    [pty] switch pty tests to use FIDL interface
    Change-Id: I12aec41b5ac858758d8ee1156058b3163fb56fd3

--- zircon/system/utest/pty/pty-test.cpp
+++ zircon/system/utest/pty/pty-test.cpp
@@ -15,8 +15,6 @@
 #include <lib/fdio/io.h>
 #include <lib/fzl/fdio.h>
 
-#include <zircon/device/pty.h>
-
 // returns an int to avoid sign errors from ASSERT_*()
 static int fd_signals(int fd) {
     uint32_t signals = 0;
@@ -107,140 +105,182 @@ static zx_status_t open_client(int fd, uint32_t client_id, int* out_fd) {
 static bool pty_test(void) {
     BEGIN_TEST;
 
-    int ps = open("/dev/misc/ptmx", O_RDWR | O_NONBLOCK);
-    ASSERT_GE(ps, 0, "");
+    fbl::unique_fd ps(open("/dev/misc/ptmx", O_RDWR | O_NONBLOCK));
+    ASSERT_EQ(bool(ps), true, "");
 
-    int pc;
-    ASSERT_EQ(open_client(ps, 0, &pc), ZX_OK);
-    ASSERT_GE(pc, 0, "");
+    fzl::UnownedFdioCaller ps_io(ps.get());
+
+    int pc_fd;
+    ASSERT_EQ(open_client(ps.get(), 0, &pc_fd), ZX_OK);
+    ASSERT_GE(pc_fd, 0, "");
+
+    fbl::unique_fd pc(pc_fd);
+    ASSERT_EQ(bool(pc), true, "");
+
+    fzl::UnownedFdioCaller pc_io(pc.get());
 
     char tmp[32];
 
-    ASSERT_EQ(fd_signals(ps), POLLOUT, "");
-    ASSERT_EQ(fd_signals(pc), POLLOUT, "");
+    ASSERT_EQ(fd_signals(ps.get()), POLLOUT, "");
+    ASSERT_EQ(fd_signals(pc.get()), POLLOUT, "");
 
     // nothing to read
-    ASSERT_EQ(read(ps, tmp, 32), -1, "");
+    ASSERT_EQ(read(ps.get(), tmp, 32), -1, "");
     ASSERT_EQ(errno, EAGAIN, "");
-    ASSERT_EQ(read(pc, tmp, 32), -1, "");
+    ASSERT_EQ(read(pc.get(), tmp, 32), -1, "");
     ASSERT_EQ(errno, EAGAIN, "");
 
     // write server, read client
-    ASSERT_EQ(write(ps, "xyzzy", 5), 5, "");
-    ASSERT_EQ(fd_signals(pc), POLLIN | POLLOUT, "");
+    ASSERT_EQ(write(ps.get(), "xyzzy", 5), 5, "");
+    ASSERT_EQ(fd_signals(pc.get()), POLLIN | POLLOUT, "");
 
     memset(tmp, 0xee, 5);
-    ASSERT_EQ(read(pc, tmp, 5), 5, "");
+    ASSERT_EQ(read(pc.get(), tmp, 5), 5, "");
     ASSERT_EQ(memcmp(tmp, "xyzzy", 5), 0, "");
-    ASSERT_EQ(fd_signals(pc), POLLOUT, "");
+    ASSERT_EQ(fd_signals(pc.get()), POLLOUT, "");
 
     // write client, read server
-    ASSERT_EQ(write(pc, "xyzzy", 5), 5, "");
-    ASSERT_EQ(fd_signals(ps), POLLIN | POLLOUT, "");
+    ASSERT_EQ(write(pc.get(), "xyzzy", 5), 5, "");
+    ASSERT_EQ(fd_signals(ps.get()), POLLIN | POLLOUT, "");
 
     memset(tmp, 0xee, 5);
-    ASSERT_EQ(read(ps, tmp, 5), 5, "");
+    ASSERT_EQ(read(ps.get(), tmp, 5), 5, "");
     ASSERT_EQ(memcmp(tmp, "xyzzy", 5), 0, "");
-    ASSERT_EQ(fd_signals(ps), POLLOUT, "");
+    ASSERT_EQ(fd_signals(ps.get()), POLLOUT, "");
 
     // write server until full, then drain
-    ASSERT_EQ(write_full(ps), 4096, "");
-    ASSERT_EQ(fd_signals(ps), 0, "");
-    ASSERT_EQ(read_all(pc), 4096, "");
-    ASSERT_EQ(fd_signals(ps), POLLOUT, "");
+    ASSERT_EQ(write_full(ps.get()), 4096, "");
+    ASSERT_EQ(fd_signals(ps.get()), 0, "");
+    ASSERT_EQ(read_all(pc.get()), 4096, "");
+    ASSERT_EQ(fd_signals(ps.get()), POLLOUT, "");
 
     // write client until full, then drain
-    ASSERT_EQ(write_full(pc), 4096, "");
-    ASSERT_EQ(fd_signals(pc), 0, "");
-    ASSERT_EQ(read_all(ps), 4096, "");
-    ASSERT_EQ(fd_signals(pc), POLLOUT, "");
+    ASSERT_EQ(write_full(pc.get()), 4096, "");
+    ASSERT_EQ(fd_signals(pc.get()), 0, "");
+    ASSERT_EQ(read_all(ps.get()), 4096, "");
+    ASSERT_EQ(fd_signals(pc.get()), POLLOUT, "");
 
     // verify no events pending
     uint32_t events;
-    ASSERT_EQ(ioctl_pty_read_events(pc, &events), (ssize_t)sizeof(events), "");
+    zx_status_t status;
+
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceReadEvents(
+        pc_io.borrow_channel(), &status, &events), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
     ASSERT_EQ(events, 0u, "");
 
     // write a ctrl-c
-    ASSERT_EQ(write(ps, "\x03", 1), 1, "");
+    ASSERT_EQ(write(ps.get(), "\x03", 1), 1, "");
 
     // should be an event now
-    ASSERT_EQ(ioctl_pty_read_events(pc, &events), (ssize_t)sizeof(events), "");
-    ASSERT_EQ(events, PTY_EVENT_INTERRUPT, "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceReadEvents(
+        pc_io.borrow_channel(), &status, &events), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
+    ASSERT_EQ(events, fuchsia_hardware_pty_EVENT_INTERRUPT, "");
 
     // should vanish once we read it
-    ASSERT_EQ(ioctl_pty_read_events(pc, &events), (ssize_t)sizeof(events), "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceReadEvents(
+        pc_io.borrow_channel(), &status, &events), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
     ASSERT_EQ(events, 0u, "");
 
     // write something containing a special char
     // should write up to and including the special char
     // converting the special char to a signal
-    ASSERT_EQ(write(ps, "hello\x03world", 11), 6, "");
-    ASSERT_EQ(read(pc, tmp, 6), 5, "");
+    ASSERT_EQ(write(ps.get(), "hello\x03world", 11), 6, "");
+    ASSERT_EQ(read(pc.get(), tmp, 6), 5, "");
     ASSERT_EQ(memcmp(tmp, "hello", 5), 0, "");
-    ASSERT_EQ(ioctl_pty_read_events(pc, &events), (ssize_t)sizeof(events), "");
-    ASSERT_EQ(events, PTY_EVENT_INTERRUPT, "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceReadEvents(
+        pc_io.borrow_channel(), &status, &events), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
+    ASSERT_EQ(events, fuchsia_hardware_pty_EVENT_INTERRUPT, "");
 
-    pty_window_size_t ws;
-    ASSERT_EQ(ioctl_pty_get_window_size(pc, &ws), (ssize_t)sizeof(ws), "");
+
+    fuchsia_hardware_pty_WindowSize ws;
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceGetWindowSize(
+        pc_io.borrow_channel(), &status, &ws), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
     ASSERT_EQ(ws.width, 0u, "");
     ASSERT_EQ(ws.height, 0u, "");
     ws.width = 80;
     ws.height = 25;
-    ASSERT_EQ(ioctl_pty_set_window_size(ps, &ws), 0, "");
-    ASSERT_EQ(ioctl_pty_get_window_size(pc, &ws), (ssize_t)sizeof(ws), "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceSetWindowSize(
+        ps_io.borrow_channel(), &ws, &status), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceGetWindowSize(
+        pc_io.borrow_channel(), &status, &ws), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
     ASSERT_EQ(ws.width, 80u, "");
     ASSERT_EQ(ws.height, 25u, "");
 
     // verify that we don't get events for special chars in raw mode
-    pty_clr_set_t cs = {
-        .clr = 0,
-        .set = PTY_FEATURE_RAW,
-    };
-    ASSERT_EQ(ioctl_pty_clr_set_feature(pc, &cs), 0, "");
-    ASSERT_EQ(write(ps, "\x03", 1), 1, "");
-    ASSERT_EQ(read(pc, tmp, 1), 1, "");
+    uint32_t features;
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceClrSetFeature(
+        pc_io.borrow_channel(), 0, fuchsia_hardware_pty_FEATURE_RAW, &status, &features),
+        ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
+    ASSERT_EQ(features & fuchsia_hardware_pty_FEATURE_RAW, fuchsia_hardware_pty_FEATURE_RAW, "");
+    ASSERT_EQ(write(ps.get(), "\x03", 1), 1, "");
+    ASSERT_EQ(read(pc.get(), tmp, 1), 1, "");
     ASSERT_EQ(tmp[0], '\x03', "");
-    ASSERT_EQ(ioctl_pty_read_events(pc, &events), (ssize_t)sizeof(events), "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceReadEvents(
+        pc_io.borrow_channel(), &status, &events), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
     ASSERT_EQ(events, 0u, "");
 
     // create a second client
-    int pc1;
-    ASSERT_EQ(open_client(pc, 1, &pc1), ZX_OK);
-    ASSERT_GE(pc1, 0, "");
+    int pc1_fd = openat(pc.get(), "1", O_RDWR | O_NONBLOCK);
+    ASSERT_GE(pc1_fd, 0, "");
+
+    fbl::unique_fd pc1(pc1_fd);
+    ASSERT_EQ(bool(pc1), true, "");
+
+    fzl::UnownedFdioCaller pc1_io(pc1.get());
 
     // reads/writes to non-active client should block
-    ASSERT_EQ(fd_signals(pc1), 0, "");
-    ASSERT_EQ(write(pc1, "test", 4), -1, "");
+    ASSERT_EQ(fd_signals(pc1.get()), 0, "");
+    ASSERT_EQ(write(pc1.get(), "test", 4), -1, "");
     ASSERT_EQ(errno, EAGAIN, "");
-    ASSERT_EQ(read(pc1, tmp, 4), -1, "");
+    ASSERT_EQ(read(pc1.get(), tmp, 4), -1, "");
     ASSERT_EQ(errno, EAGAIN, "");
 
     uint32_t n = 2;
-    ASSERT_EQ(ioctl_pty_make_active(pc, &n), ZX_ERR_NOT_FOUND, "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceMakeActive(
+        pc_io.borrow_channel(), n, &status), ZX_OK, "");
+    ASSERT_EQ(status, ZX_ERR_NOT_FOUND, "");
 
     // non-controlling client cannot change active client
-    ASSERT_EQ(ioctl_pty_make_active(pc1, &n), ZX_ERR_ACCESS_DENIED, "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceMakeActive(
+        pc1_io.borrow_channel(), n, &status), ZX_OK, "");
+    ASSERT_EQ(status, ZX_ERR_ACCESS_DENIED, "");
 
     // but controlling client can
     n = 1;
-    ASSERT_EQ(ioctl_pty_make_active(pc, &n), ZX_OK, "");
-    ASSERT_EQ(fd_signals(pc), 0, "");
-    ASSERT_EQ(fd_signals(pc1), POLLOUT, "");
-    ASSERT_EQ(write(pc1, "test", 4), 4, "");
-    ASSERT_EQ(read(ps, tmp, 4), 4, "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceMakeActive(
+        pc_io.borrow_channel(), n, &status), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
+    ASSERT_EQ(fd_signals(pc.get()), 0, "");
+    ASSERT_EQ(fd_signals(pc1.get()), POLLOUT, "");
+    ASSERT_EQ(write(pc1.get(), "test", 4), 4, "");
+    ASSERT_EQ(read(ps.get(), tmp, 4), 4, "");
     ASSERT_EQ(memcmp(tmp, "test", 4), 0, "");
 
     // make sure controlling client observes departing active client
-    close(pc1);
-    ASSERT_EQ(fd_signals(pc), POLLHUP | POLLPRI, "");
-    ASSERT_EQ(ioctl_pty_read_events(pc, &events), (ssize_t)sizeof(events), "");
-    ASSERT_EQ(events, PTY_EVENT_HANGUP, "");
+    pc1_io.reset();
+    pc1.reset();
+    ASSERT_EQ(fd_signals(pc.get()), POLLHUP | POLLPRI, "");
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceReadEvents(
+        pc_io.borrow_channel(), &status, &events), ZX_OK, "");
+    ASSERT_EQ(status, ZX_OK, "");
+    ASSERT_EQ(events, fuchsia_hardware_pty_EVENT_HANGUP, "");
 
-    // verify that server observes depature of last client
-    close(pc);
-    ASSERT_EQ(fd_signals(ps), POLLHUP | POLLIN, "");
+    // verify that server observes departure of last client
+    pc_io.reset();
+    pc.reset();
+    ASSERT_EQ(fd_signals(ps.get()), POLLHUP | POLLIN, "");
 
-    close(ps);
+    ps_io.reset();
+    ps.reset();
 
     END_TEST;
 }
@@ -248,15 +288,19 @@ static bool pty_test(void) {
 bool not_a_pty_test(void) {
     BEGIN_TEST;
 
-    int root_dir = open("/", O_DIRECTORY | O_RDONLY);
-    EXPECT_GE(root_dir, 0, "");
+    fbl::unique_fd root_dir(open("/", O_DIRECTORY | O_RDONLY));
+    ASSERT_EQ(bool(root_dir), true, "");
 
-    // Calling pty ioctls such as 'get window size' should fail
+    fzl::UnownedFdioCaller io(root_dir.get());
+
+    // Sending pty messages such as 'get window size' should fail
     // properly on things that are not ptys.
-    pty_window_size_t ws;
-    EXPECT_EQ(ioctl_pty_get_window_size(root_dir, &ws), ZX_ERR_NOT_SUPPORTED, "");
+    fuchsia_hardware_pty_WindowSize ws;
+    zx_status_t status;
+    ASSERT_EQ(fuchsia_hardware_pty_DeviceGetWindowSize(
+        io.borrow_channel(), &status, &ws), ZX_ERR_BAD_HANDLE, "");
 
-    close(root_dir);
+    io.reset();
 
     END_TEST;
 }
