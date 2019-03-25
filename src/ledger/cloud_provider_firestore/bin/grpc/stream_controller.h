391048151b4a70ce0a5673fb5d5126a56b690b0e
Przemek Pietrzkiewicz <ppi@google.com>
2019-03-25 10:22:38 +0000

    [ledger] add stronger dchecks for firestore completion queue logic
    This patch adds stricter DCHECKs hoping to shed some light on LE-726 and
    LE-721.
    
    Test: compiles and passes CQ
    Change-Id: If728bc25bad9fa758ae0ea5a3cb03ecdc472fd14

--- src/ledger/cloud_provider_firestore/bin/grpc/stream_controller.h
+++ src/ledger/cloud_provider_firestore/bin/grpc/stream_controller.h
@@ -27,19 +27,22 @@ class StreamController {
   ~StreamController() {
     // The class cannot go away while completion queue operations are pending,
     // as they reference member function objects as operation tags.
-    FXL_DCHECK(pending_cq_operations_ == 0);
+    FXL_DCHECK(IsEmpty());
   }
 
-  bool IsEmpty() const { return pending_cq_operations_ == 0; }
+  bool IsEmpty() const { return !pending_finish_call_ && !pending_start_call_; }
 
   // Attempts to start the stream.
   void StartCall(fit::function<void(bool)> callback) {
+    FXL_DCHECK(!pending_start_call_);
+
     on_connected_ = [this, callback = std::move(callback)](bool ok) {
-      pending_cq_operations_--;
+      FXL_DCHECK(pending_start_call_);
+      pending_start_call_ = false;
       callback(ok);
     };
 
-    pending_cq_operations_++;
+    pending_start_call_ = true;
     grpc_stream_->StartCall(&on_connected_);
   }
 
@@ -49,12 +52,14 @@ class StreamController {
   // operations fail early. For that, call TryCancel() on the associated client
   // context.
   void Finish(fit::function<void(bool, grpc::Status)> callback) {
+    FXL_DCHECK(!pending_finish_call_);
     on_finish_ = [this, callback = std::move(callback)](bool ok) {
-      pending_cq_operations_--;
+      FXL_DCHECK(pending_finish_call_);
+      pending_finish_call_ = false;
       callback(ok, status_);
     };
 
-    pending_cq_operations_++;
+    pending_finish_call_ = true;
     grpc_stream_->Finish(&status_, &on_finish_);
   }
 
@@ -62,8 +67,8 @@ class StreamController {
   // gRPC stream handler.
   GrpcStream* const grpc_stream_;
 
-  // Count of pending async tasks posted on the completion queue.
-  int pending_cq_operations_ = 0;
+  bool pending_start_call_ = false;
+  bool pending_finish_call_ = false;
 
   // Callables posted as CompletionQueue tags:
   fit::function<void(bool)> on_connected_;
