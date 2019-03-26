342532cb1890b9b158a292fa8aa01a3149aea1de
Jeff Belgum <belgum@google.com>
2019-03-26 18:34:25 +0000

    [cobalt][rust] Add support for referring to projects by name.
    Also add method call for sending EVENT_OCCURRED log events
    
    TEST=CQ
    
    Change-Id: I9304627cbccb4e0ac1f77b7c2f582b4fe182a36d

--- garnet/public/rust/fuchsia-cobalt/src/lib.rs
+++ garnet/public/rust/fuchsia-cobalt/src/lib.rs
@@ -5,10 +5,11 @@
 #![feature(async_await, await_macro, futures_api)]
 
 use {
-    failure::{format_err, Error, ResultExt},
+    failure::{bail, Error, ResultExt},
     fdio, fidl,
     fidl_fuchsia_cobalt::{
-        HistogramBucket, LoggerFactoryMarker, LoggerProxy, ProjectProfile, ReleaseStage, Status,
+        HistogramBucket, LoggerFactoryMarker, LoggerProxy, ProjectProfile,
+        ReleaseStage, Status,
     },
     fidl_fuchsia_mem as fuchsia_mem,
     futures::{channel::mpsc, prelude::*, StreamExt},
@@ -24,6 +25,9 @@ use {
 };
 
 enum EventValue {
+    Event {
+        event_code: u32,
+    },
     Count {
         event_code: u32,
         count: i64,
@@ -49,9 +53,14 @@ pub struct CobaltSender {
 }
 
 impl CobaltSender {
+    pub fn log_event(&mut self, metric_id: u32, event_code: u32) {
+        let event_value = EventValue::Event { event_code };
+        self.log_event_value(metric_id, event_value);
+    }
+
     pub fn log_event_count(&mut self, metric_id: u32, event_code: u32, count: i64) {
         let event_value = EventValue::Count { event_code, count };
-        self.log_event(metric_id, event_value);
+        self.log_event_value(metric_id, event_value);
     }
 
     pub fn log_elapsed_time(&mut self, metric_id: u32, event_code: u32, elapsed_micros: i64) {
@@ -59,15 +68,15 @@ impl CobaltSender {
             event_code,
             elapsed_micros,
         };
-        self.log_event(metric_id, event_value);
+        self.log_event_value(metric_id, event_value);
     }
 
     pub fn log_int_histogram(&mut self, metric_id: u32, values: Vec<HistogramBucket>) {
         let event_value = EventValue::IntHistogram { values };
-        self.log_event(metric_id, event_value);
+        self.log_event_value(metric_id, event_value);
     }
 
-    fn log_event(&mut self, metric_id: u32, value: EventValue) {
+    fn log_event_value(&mut self, metric_id: u32, value: EventValue) {
         let event = Event { metric_id, value };
         if self.sender.try_send(event).is_err() {
             let was_blocked = self
@@ -87,14 +96,59 @@ impl CobaltSender {
     }
 }
 
+pub fn serve_with_project_name(buffer_size: usize, project_name: &str) -> (CobaltSender, impl Future<Output = ()>) {
+    let (sender, receiver) = mpsc::channel(buffer_size);
+    let sender = CobaltSender {
+        sender,
+        is_blocked: Arc::new(AtomicBool::new(false)),
+    };
+    let project_name = project_name.to_string();
+    let fut = async move {
+        let logger = match await!(get_cobalt_logger_with_project_name(project_name)) {
+            Ok(logger) => logger,
+            Err(e) => {
+                error!("Error obtaining a Cobalt Logger: {}", e);
+                return;
+            }
+        };
+        await!(send_cobalt_events(logger, receiver))
+    };
+    (sender, fut)
+}
+
 pub fn serve(buffer_size: usize, config_path: &str) -> (CobaltSender, impl Future<Output = ()>) {
     let (sender, receiver) = mpsc::channel(buffer_size);
     let sender = CobaltSender {
         sender,
         is_blocked: Arc::new(AtomicBool::new(false)),
     };
-    let fut = send_cobalt_events(config_path.to_string(), receiver);
+    let config_path = config_path.to_string();
+    let fut = async move {
+        let logger = match await!(get_cobalt_logger(config_path)) {
+            Ok(logger) => logger,
+            Err(e) => {
+                error!("Error obtaining a Cobalt Logger: {}", e);
+                return;
+            }
+        };
+        await!(send_cobalt_events(logger, receiver))
+    };
     (sender, fut)
+}
+
+async fn get_cobalt_logger_with_project_name(project_name: String) -> Result<LoggerProxy, Error> {
+    let (logger_proxy, server_end) =
+        fidl::endpoints::create_proxy().context("Failed to create endpoints")?;
+    let logger_factory = fuchsia_app::client::connect_to_service::<LoggerFactoryMarker>()
+        .context("Failed to connect to the Cobalt LoggerFactory")?;
+
+    let res = await!(logger_factory.create_logger_from_project_name(
+        &project_name,
+        ReleaseStage::Ga,
+        server_end,
+    ));
+    handle_cobalt_factory_result(res, "Failed to obtain Logger")?;
+    Ok(logger_proxy)
 }
 
 async fn get_cobalt_logger(config_path: String) -> Result<LoggerProxy, Error> {
@@ -125,55 +179,50 @@ fn handle_cobalt_factory_result(
 ) -> Result<(), failure::Error> {
     match r {
         Ok(Status::Ok) => Ok(()),
-        Ok(other) => Err(format_err!("{}: {:?}", context, other)),
-        Err(e) => Err(format_err!("{}: {}", context, e)),
+        Ok(other) => bail!("{}: {:?}", context, other),
+        Err(e) => bail!("{}: {}", context, e),
     }
 }
 
-async fn send_cobalt_events(cobalt_config_path: String, mut receiver: mpsc::Receiver<Event>) {
-    let logger = match await!(get_cobalt_logger(cobalt_config_path)) {
-        Ok(logger) => logger,
-        Err(e) => {
-            error!("Error obtaining a Cobalt Logger: {}", e);
-            return;
-        }
-    };
-
+async fn send_cobalt_events(logger: LoggerProxy, mut receiver: mpsc::Receiver<Event>) {
     let mut is_full = false;
     while let Some(event) = await!(receiver.next()) {
-        match event.value {
+        let resp = match event.value {
+            EventValue::Event { event_code } => {
+                await!(logger.log_event(
+                    event.metric_id, event_code
+                ))
+            }
             EventValue::Count { event_code, count } => {
-                let resp = await!(logger.log_event_count(
+                await!(logger.log_event_count(
                     event.metric_id,
                     event_code,
                     "",
                     0, // TODO report a period duration once the backend supports it.
                     count
-                ));
-                handle_cobalt_response(resp, event.metric_id, &mut is_full);
+                ))
             }
             EventValue::ElapsedTime {
                 event_code,
                 elapsed_micros,
             } => {
-                let resp = await!(logger.log_elapsed_time(
+                await!(logger.log_elapsed_time(
                     event.metric_id,
                     event_code,
                     "",
                     elapsed_micros
-                ));
-                handle_cobalt_response(resp, event.metric_id, &mut is_full);
+                ))
             }
             EventValue::IntHistogram { mut values } => {
-                let resp = await!(logger.log_int_histogram(
+                await!(logger.log_int_histogram(
                     event.metric_id,
                     0,
                     "",
                     &mut values.iter_mut()
-                ));
-                handle_cobalt_response(resp, event.metric_id, &mut is_full);
+                ))
             }
-        }
+        };
+        handle_cobalt_response(resp, event.metric_id, &mut is_full);
     }
 }
 
@@ -191,24 +240,21 @@ fn throttle_cobalt_error(
     match resp {
         Ok(Status::BufferFull) => {
             if !was_full {
-                Err(format_err!(
-                    "Cobalt buffer became full. Cannot report the stats"
-                ))
-            } else {
-                Ok(())
+                bail!("Cobalt buffer became full. Cannot report the stats")
             }
+            Ok(())
         }
         Ok(Status::Ok) => Ok(()),
-        Ok(other) => Err(format_err!(
+        Ok(other) => bail!(
             "Cobalt returned an error for metric {}: {:?}",
             metric_id,
             other
-        )),
-        Err(e) => Err(format_err!(
+        ),
+        Err(e) => bail!(
             "Failed to send event to Cobalt for metric {}: {}",
             metric_id,
             e
-        )),
+        ),
     }
 }
 
