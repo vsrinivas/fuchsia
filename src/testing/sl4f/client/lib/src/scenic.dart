3acdd688080a6448412e084cfc9464634b3d4648
Isma Cortes Vazquez <isma@google.com>
2019-03-28 22:13:52 +0000

    [sl4f][e2e] Save screenshots to FUCHSIA_TEST_OUTDIR if specified
    TESTED=FUCHSIA_IPV4_ADDR=.. FUCHSIA_SSH_KEY=.. FUCHSIA_TEST_OUTDIR=$PWD ./out/default/host_x64/mytest
    followed by inspection of the files in cwd.
    
    Change-Id: Iec4cc98b9b936a0516daa62971fb3d7824b66520

--- src/testing/sl4f/client/lib/src/scenic.dart
+++ src/testing/sl4f/client/lib/src/scenic.dart
@@ -3,16 +3,38 @@
 // found in the LICENSE file.
 
 import 'dart:convert' show base64Decode;
+import 'dart:io' show Directory, File, FileSystemException, Platform;
 import 'dart:typed_data' show Uint8List;
 
-import 'package:image/image.dart';
+import 'package:image/image.dart' show encodePng, Image;
 
 import 'sl4f_client.dart';
 
 class Scenic {
+  /// Environment variable for the directory to dump images in. If this var is
+  /// present, screenshots can optionally be dumped in there.
+  static const _dumpDirectoryEnvVar = 'FUCHSIA_TEST_OUTDIR';
+
   final Sl4f _sl4f;
 
-  Scenic([Sl4f sl4f]) : _sl4f = sl4f ?? Sl4f.fromEnvironment();
+  /// Directory to dump screenshots taken.
+  final String _dumpDirectory;
+
+  /// Constructs a [Scenic] object.
+  ///
+  /// It can optionally take an [Sl4f] object, if not passed, one will be
+  /// created using the environment variables to connect to the device. If a
+  /// [dumpDirectory] is specified, or if the FUCHSIA_TEST_OUTDIR env variable
+  /// is defined, screenshots can be dumped there using [dumpImage].
+  Scenic([Sl4f sl4f, String dumpDirectory])
+      : _sl4f = sl4f ?? Sl4f.fromEnvironment(),
+        _dumpDirectory =
+            dumpDirectory ?? Platform.environment[_dumpDirectoryEnvVar] {
+    // Has to be sync because this is a constructor.
+    if (_dumpDirectory != null && !Directory(_dumpDirectory).existsSync()) {
+      throw FileSystemException('Not found or not a directory', dumpDirectory);
+    }
+  }
 
   /// Closes the underlying HTTP client. This need not be called if the
   /// Sl4f client is closed instead.
@@ -20,7 +42,11 @@ class Scenic {
     _sl4f.close();
   }
 
-  Future<Image> takeScreenshot() async {
+  /// Captures the screen of the device.
+  ///
+  /// Returns an Image in RGBA format. If a [dumpName] is provided, the picture
+  /// is also dumped with that name as prefix.
+  Future<Image> takeScreenshot({String dumpName}) async {
     final Map<String, dynamic> response =
         await _sl4f.request('scenic_facade.TakeScreenshot');
     final Map<String, dynamic> info = response['info'];
@@ -36,6 +62,25 @@ class Scenic {
       bytes[quad + 2] = blue;
     }
 
-    return Image.fromBytes(info['width'], info['height'], bytes);
+    final image = Image.fromBytes(info['width'], info['height'], bytes);
+    if (dumpName != null) {
+      // Write to the file asynchronously so the test can continue.
+      // ignore: unawaited_futures
+      dumpImage(image, dumpName);
+    }
+    return image;
+  }
+
+  /// Dumps the image to the dump directory if specified.
+  Future<void> dumpImage(Image image, [String name]) async {
+    if (_dumpDirectory == null) {
+      return;
+    }
+
+    final filename =
+        '${DateTime.now().toIso8601String()}-${name ?? 'screenshot'}.png';
+
+    await File([_dumpDirectory, filename].join('/'))
+        .writeAsBytes(encodePng(image));
   }
 }
