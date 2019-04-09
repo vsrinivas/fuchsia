e53c2d43e7af668f3d401d44b4efa530905a626d
Ross Wang <rosswang@google.com>
2019-04-09 23:11:25 +0000

    [screenshot][chore] Remove histogram from screencap tool
    Now that the flutter screencap test is doing its own histogram, we can remove
    it from the screencap tool.
    
    Test: fx screenshot --png -o ss.png
    Change-Id: Ic644368328dad3e3d2b94c012dbe17e46e7d8066

--- garnet/bin/ui/screencap/main.cc
+++ garnet/bin/ui/screencap/main.cc
@@ -2,13 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstdlib>
-#include <iostream>
-#include <memory>
-
 #include <fuchsia/ui/scenic/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <trace-provider/provider.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
 
 #include "lib/component/cpp/startup_context.h"
 #include "lib/fsl/vmo/vector.h"
@@ -16,20 +16,10 @@
 #include "src/lib/fxl/log_settings_command_line.h"
 #include "src/lib/fxl/logging.h"
 
-// These are the only values we want to track
-const int kBlack = 0x000000;
-const int kWhite = 0xeeeeee;
-const int kGreen = 0x4dac26;
-const int kRed = 0xd01c8b;
-
-const int kMinExpectedPixels = 950000;  // Typical value is ~1.5M
-const int kMinPixelsForReport = 50000;
-
 class ScreenshotTaker {
  public:
-  explicit ScreenshotTaker(async::Loop* loop, bool output_screen)
+  explicit ScreenshotTaker(async::Loop* loop)
       : loop_(loop),
-        output_screen_(output_screen),
         context_(component::StartupContext::CreateFromStartupInfo()) {
     // Connect to the Scenic service.
     scenic_ =
@@ -67,13 +57,10 @@ class ScreenshotTaker {
             return;
           }
 
-          std::map<int, int> histogram;
-          if (output_screen_) {
-            std::cout << "P6\n";
-            std::cout << screenshot.info.width << "\n";
-            std::cout << screenshot.info.height << "\n";
-            std::cout << 255 << "\n";
-          }
+          std::cout << "P6\n";
+          std::cout << screenshot.info.width << "\n";
+          std::cout << screenshot.info.height << "\n";
+          std::cout << 255 << "\n";
 
           FXL_LOG(INFO) << "capturing pixels";
           const uint8_t* pchannel = &imgdata[0];
@@ -81,53 +68,13 @@ class ScreenshotTaker {
                pixel < screenshot.info.width * screenshot.info.height;
                pixel++) {
             uint8_t rgb[] = {pchannel[2], pchannel[1], pchannel[0]};
-            if (output_screen_) {
-              std::cout.write(reinterpret_cast<const char*>(rgb), 3);
-            } else {
-              int rgb_value = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
-              if (histogram.find(rgb_value) != histogram.end()) {
-                histogram[rgb_value] = histogram[rgb_value] + 1;
-              } else {
-                histogram[rgb_value] = 1;
-              }
-            }
+            std::cout.write(reinterpret_cast<const char*>(rgb), 3);
             pchannel += 4;
-          }
-          if (!output_screen_) {
-            // For success, there should be at least 1M green or red pixels
-            // combined The typical number is > 1.5M
-            if (histogram[kGreen] + histogram[kRed] > kMinExpectedPixels) {
-              FXL_LOG(INFO) << "success";
-              printf("success\n");
-            } else {
-              FXL_LOG(INFO) << "failure";
-              printf("failure\n");
-              printf("black: %d, white: %d, green: %d, red: %d\n",
-                     histogram[kBlack], histogram[kWhite], histogram[kGreen],
-                     histogram[kRed]);
-              // To help debug failures, if the majority of values aren't
-              // already expected, output the values that were over a threshold
-              // count.
-              if (histogram[kBlack] + histogram[kWhite] + histogram[kGreen] +
-                      histogram[kRed] <
-                  kMinExpectedPixels) {
-                const int MIN_REPORT_THRESHOLD = kMinPixelsForReport;
-                std::map<int, int>::iterator it;
-                for (const auto& pair : histogram) {
-                  if (pair.second > MIN_REPORT_THRESHOLD) {
-                    printf("Pixel 0x%06x occurred %d times\n", pair.first,
-                           pair.second);
-                  }
-                }
-              }
-              encountered_error_ = true;
-            }
           }
           loop_->Quit();
         });
   }
   async::Loop* loop_;
-  const bool output_screen_;
   std::unique_ptr<component::StartupContext> context_;
   bool encountered_error_ = false;
   fuchsia::ui::scenic::ScenicPtr scenic_;
@@ -135,37 +82,25 @@ class ScreenshotTaker {
 
 int main(int argc, const char** argv) {
   FXL_LOG(INFO) << "starting screen capture";
-  bool output_screen = true;
   auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
   if (!fxl::SetLogSettingsFromCommandLine(command_line))
     return 1;
 
-  const auto& positional_args = command_line.positional_args();
-  if (!positional_args.empty()) {
-    if (positional_args.size() == 1 &&
-        positional_args[0].compare("-histogram") == 0) {
-      output_screen = false;
-      FXL_LOG(INFO) << "in histogram mode";
-    } else {
-      FXL_LOG(ERROR) << "Usage: screencap\n"
-                     << "Takes a screenshot in PPM format and writes it "
-                     << "to stdout.\n"
-                     << "To write to a file, redirect stdout, e.g.: "
-                     << "screencap > \"${DST}\"";
-      return 1;
-    }
+  if (!command_line.positional_args().empty()) {
+    FXL_LOG(ERROR) << "Usage: screencap\n"
+                   << "Takes a screenshot in PPM format and writes it "
+                   << "to stdout.\n"
+                   << "To write to a file, redirect stdout, e.g.: "
+                   << "screencap > \"${DST}\"";
+    return 1;
   }
-  FXL_LOG(INFO) << "setting up event loop";
 
   async::Loop loop(&kAsyncLoopConfigAttachToThread);
   trace::TraceProvider trace_provider(loop.dispatcher());
 
-  FXL_LOG(INFO) << "starting taker";
-  ScreenshotTaker taker(&loop, output_screen);
+  ScreenshotTaker taker(&loop);
   taker.TakeScreenshot();
-  FXL_LOG(INFO) << "starting Run()";
   loop.Run();
 
-  FXL_LOG(INFO) << "returning result";
   return taker.encountered_error() ? EXIT_FAILURE : EXIT_SUCCESS;
 }
