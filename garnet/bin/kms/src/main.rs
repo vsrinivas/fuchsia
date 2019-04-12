298bad81f503b2ff46e4091ce0fd711d2a955056
Taylor Cramer <cramertj@google.com>
2019-04-12 17:54:57 +0000

    [rust][kms] kms to fuchsia-component
    Change-Id: Ibe7744e8d3984ac634174ae735e225f7e4acd007

--- garnet/bin/kms/src/main.rs
+++ garnet/bin/kms/src/main.rs
@@ -13,10 +13,9 @@ mod kms_sealing_key;
 use crate::key_manager::KeyManager;
 
 use failure::{Error, ResultExt};
-use fidl::endpoints::{RequestStream, ServiceMarker};
-use fidl_fuchsia_kms::{KeyManagerMarker, KeyManagerRequestStream};
-use fuchsia_app::server::ServicesServer;
+use fidl_fuchsia_kms::KeyManagerRequestStream;
 use fuchsia_async as fasync;
+use fuchsia_component::server::ServiceFs;
 use futures::prelude::*;
 use log::error;
 use std::sync::Arc;
@@ -24,18 +23,14 @@ use std::sync::Arc;
 fn main() -> Result<(), Error> {
     let mut executor = fasync::Executor::new().context("Error creating executor")?;
     let key_manager = Arc::new(KeyManager::new());
-    let fut = ServicesServer::new()
-        .add_service((KeyManagerMarker::NAME, move |chan| spawn(chan, Arc::clone(&key_manager))))
-        .start()
-        .context("Error starting KMS services server")?;
-
-    executor.run_singlethreaded(fut).context("Error executing KMS future")?;
+    let mut fs = ServiceFs::new();
+    fs.dir("public").add_fidl_service(|stream| spawn(stream, Arc::clone(&key_manager)));
+    fs.take_and_serve_directory_handle()?;
+    let () = executor.run_singlethreaded(fs.collect());
     Ok(())
 }
 
-fn spawn(chan: fasync::Channel, key_manager: Arc<KeyManager>) {
-    let mut stream = KeyManagerRequestStream::from_channel(chan);
-
+fn spawn(mut stream: KeyManagerRequestStream, key_manager: Arc<KeyManager>) {
     fasync::spawn(
         async move {
             while let Some(r) = await!(stream.try_next())? {
