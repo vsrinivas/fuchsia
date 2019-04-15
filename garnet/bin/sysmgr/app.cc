vsrinivas/fuchsia

commit 8c3db8607c328c1970acd41b4d8f7a49e190690b

[sysmgr] Port to use libsys
Also remove already ported targets from libcomponent's visibility list.

DX-387 #comment

TEST=CQ

Change-Id: I0accee838c057d35d40f5e0e5eb60d69f82dac7e

garnet/bin/sysmgr/app.cc
@@ -4,15 +4,21 @@
 
 #include "garnet/bin/sysmgr/app.h"
 
-#include <fs/managed-vfs.h>
+#include <fuchsia/io/cpp/fidl.h>
+#include <fuchsia/sys/cpp/fidl.h>
 #include <lib/async/default.h>
+#include <lib/async/dispatcher.h>
 #include <lib/fdio/directory.h>
 #include <lib/fdio/fd.h>
 #include <lib/fdio/fdio.h>
+#include <lib/fidl/cpp/clone.h>
+#include <lib/fidl/cpp/interface_handle.h>
+#include <lib/sys/cpp/service_directory.h>
+#include <lib/vfs/cpp/node.h>
+#include <lib/vfs/cpp/service.h>
 #include <zircon/process.h>
 #include <zircon/processargs.h>
-#include "lib/component/cpp/connect.h"
-#include "lib/fidl/cpp/clone.h"
+
 #include "src/lib/fxl/logging.h"
 
 namespace sysmgr {
@@ -27,11 +33,9 @@ constexpr bool kAutoUpdatePackages = false;
 }  // namespace
 
 App::App(Config config)
-    : startup_context_(component::StartupContext::CreateFromStartupInfo()),
-      vfs_(async_get_default_dispatcher()),
-      svc_root_(fbl::AdoptRef(new fs::PseudoDir())),
+    : component_context_(sys::ComponentContext::Create()),
       auto_updates_enabled_(kAutoUpdatePackages) {
-  FXL_DCHECK(startup_context_);
+  FXL_DCHECK(component_context_);
 
   // The set of excluded services below are services that are the transitive
   // closure of dependencies required for auto-updates that must not be resolved
@@ -90,24 +94,25 @@ App::App(Config config)
         async_get_default_dispatcher());
   }
   static const char* const kLoaderName = fuchsia::sys::Loader::Name_;
-  auto child = fbl::AdoptRef(new fs::Service([this](zx::channel channel) {
-    if (auto_updates_enabled_) {
-      package_updating_loader_->Bind(
-          fidl::InterfaceRequest<fuchsia::sys::Loader>(std::move(channel)));
-    } else {
-      startup_context_->ConnectToEnvironmentService(kLoaderName,
-                                                    std::move(channel));
-    }
-    return ZX_OK;
-  }));
+  auto child = std::make_unique<vfs::Service>(
+      [this](zx::channel channel, async_dispatcher_t* dispatcher) {
+        if (auto_updates_enabled_) {
+          package_updating_loader_->Bind(
+              fidl::InterfaceRequest<fuchsia::sys::Loader>(std::move(channel)));
+        } else {
+          component_context_->svc()->Connect(kLoaderName, std::move(channel));
+        }
+      });
   svc_names_.push_back(kLoaderName);
-  svc_root_->AddEntry(kLoaderName, std::move(child));
+  svc_root_.AddEntry(kLoaderName, std::move(child));
 
   // Set up environment for the programs we will run.
   fuchsia::sys::ServiceListPtr service_list(new fuchsia::sys::ServiceList);
   service_list->names = std::move(svc_names_);
   service_list->host_directory = OpenAsDirectory();
-  startup_context_->environment()->CreateNestedEnvironment(
+  fuchsia::sys::EnvironmentPtr environment;
+  component_context_->svc()->Connect(environment.NewRequest());
+  environment->CreateNestedEnvironment(
       std::move(env_request), env_controller_.NewRequest(), kDefaultLabel,
       std::move(service_list), {});
 
@@ -128,42 +133,41 @@ App::App(Config config)
 App::~App() = default;
 
 zx::channel App::OpenAsDirectory() {
-  zx::channel h1, h2;
-  if (zx::channel::create(0, &h1, &h2) != ZX_OK)
-    return zx::channel();
-  if (vfs_.ServeDirectory(svc_root_, std::move(h1)) != ZX_OK)
-    return zx::channel();
-  return h2;
+  fidl::InterfaceHandle<fuchsia::io::Directory> dir;
+  svc_root_.Serve(
+      fuchsia::io::OPEN_RIGHT_READABLE | fuchsia::io::OPEN_RIGHT_WRITABLE,
+      dir.NewRequest().TakeChannel());
+  return dir.TakeChannel();
 }
 
 void App::ConnectToService(const std::string& service_name,
                            zx::channel channel) {
-  fbl::RefPtr<fs::Vnode> child;
-  svc_root_->Lookup(&child, service_name);
-  auto status = child->Serve(&vfs_, std::move(channel), 0);
-  if (status != ZX_OK) {
-    FXL_LOG(ERROR) << "Could not serve " << service_name << ": " << status;
+  vfs::Node* child;
+  auto status = svc_root_.Lookup(service_name, &child);
+  if (status == ZX_OK) {
+    status = child->Serve(fuchsia::io::OPEN_RIGHT_READABLE, std::move(channel));
   }
+  FXL_LOG(ERROR) << "Could not serve " << service_name << ": " << status;
 }
 
 void App::RegisterSingleton(std::string service_name,
                             fuchsia::sys::LaunchInfoPtr launch_info,
                             bool optional) {
-  auto child = fbl::AdoptRef(new fs::Service(
+  auto child = std::make_unique<vfs::Service>(
       [this, optional, service_name, launch_info = std::move(launch_info),
        controller = fuchsia::sys::ComponentControllerPtr()](
-          zx::channel client_handle) mutable {
+          zx::channel client_handle, async_dispatcher_t* dispatcher) mutable {
         FXL_VLOG(2) << "Servicing singleton service request for "
                     << service_name;
         auto it = services_.find(launch_info->url);
         if (it == services_.end()) {
           FXL_VLOG(1) << "Starting singleton " << launch_info->url
                       << " for service " << service_name;
-          component::Services services;
           fuchsia::sys::LaunchInfo dup_launch_info;
           dup_launch_info.url = launch_info->url;
           fidl::Clone(launch_info->arguments, &dup_launch_info.arguments);
-          dup_launch_info.directory_request = services.NewRequest();
+          auto services = sys::ServiceDirectory::CreateWithRequest(
+              &dup_launch_info.directory_request);
           controller.events().OnTerminated =
               [service_name, url = launch_info->url, optional](
                   int64_t return_code, fuchsia::sys::TerminationReason reason) {
@@ -189,11 +193,10 @@ void App::RegisterSingleton(std::string service_name,
               services_.emplace(launch_info->url, std::move(services));
         }
 
-        it->second.ConnectToService(std::move(client_handle), service_name);
-        return ZX_OK;
-      }));
+        it->second->Connect(service_name, std::move(client_handle));
+      });
   svc_names_.push_back(service_name);
-  svc_root_->AddEntry(service_name, std::move(child));
+  svc_root_.AddEntry(service_name, std::move(child));
 }
 
 void App::LaunchApplication(fuchsia::sys::LaunchInfo launch_info) {

garnet/bin/sysmgr/BUILD.gn
@@ -22,12 +22,12 @@ source_set("lib") {
 
   public_deps = [
     "//garnet/lib/loader",
-    "//garnet/public/lib/component/cpp",
     "//garnet/public/lib/fsl",
     "//garnet/public/lib/json",
     "//garnet/public/lib/svc/cpp",
     "//sdk/fidl/fuchsia.pkg",
     "//sdk/lib/fidl/cpp",
+    "//sdk/lib/sys/cpp",
     "//src/lib/fxl",
     "//src/lib/pkg_url",
     "//third_party/rapidjson",

garnet/bin/sysmgr/app.h
@@ -5,20 +5,20 @@
 #ifndef GARNET_BIN_SYSMGR_APP_H_
 #define GARNET_BIN_SYSMGR_APP_H_
 
+#include <fuchsia/pkg/cpp/fidl.h>
+#include <fuchsia/sys/cpp/fidl.h>
+#include <lib/sys/cpp/component_context.h>
+#include <lib/sys/cpp/service_directory.h>
+#include <lib/vfs/cpp/pseudo_dir.h>
+
 #include <map>
 #include <memory>
 #include <string>
 #include <unordered_set>
 
-#include <fs/managed-vfs.h>
-#include <fuchsia/pkg/cpp/fidl.h>
-#include <fuchsia/sys/cpp/fidl.h>
 #include "garnet/bin/sysmgr/config.h"
 #include "garnet/bin/sysmgr/package_updating_loader.h"
-#include "lib/component/cpp/startup_context.h"
 #include "src/lib/fxl/macros.h"
-#include "lib/svc/cpp/service_namespace.h"
-#include "lib/svc/cpp/services.h"
 
 namespace sysmgr {
 
@@ -44,18 +44,17 @@ class App {
   void RegisterDefaultServiceConnector();
   void LaunchApplication(fuchsia::sys::LaunchInfo launch_info);
 
-  std::unique_ptr<component::StartupContext> startup_context_;
+  std::unique_ptr<sys::ComponentContext> component_context_;
 
   // Keep track of all services, indexed by url.
-  std::map<std::string, component::Services> services_;
+  std::map<std::string, std::shared_ptr<sys::ServiceDirectory>> services_;
 
   // Nested environment within which the apps started by sysmgr will run.
   fuchsia::sys::EnvironmentPtr env_;
   fuchsia::sys::EnvironmentControllerPtr env_controller_;
   fuchsia::sys::LauncherPtr env_launcher_;
 
-  fs::ManagedVfs vfs_;
-  fbl::RefPtr<fs::PseudoDir> svc_root_;
+  vfs::PseudoDir svc_root_;
   fidl::VectorPtr<std::string> svc_names_;
 
   std::unique_ptr<PackageUpdatingLoader> package_updating_loader_;

garnet/bin/sysmgr/integration_tests/BUILD.gn
@@ -22,8 +22,8 @@ executable("test_sysmgr_service") {
 
   deps = [
     ":interface",
-    "//garnet/public/lib/component/cpp",
     "//sdk/lib/fidl/cpp",
+    "//sdk/lib/sys/cpp",
     "//zircon/public/lib/async-default",
     "//zircon/public/lib/async-loop-cpp",
     "//zircon/public/lib/zx",
@@ -38,9 +38,9 @@ executable("mock_resolver") {
   ]
 
   deps = [
-    "//garnet/public/lib/component/cpp",
     "//sdk/fidl/fuchsia.pkg",
     "//sdk/lib/fidl/cpp",
+    "//sdk/lib/sys/cpp",
     "//src/lib/fxl",
     "//zircon/public/lib/async-default",
     "//zircon/public/lib/async-loop-cpp",
@@ -58,8 +58,8 @@ executable("service_startup_test") {
   deps = [
     ":interface",
     "//garnet/bin/appmgr:appmgr_for_test",
-    "//garnet/public/lib/component/cpp",
     "//garnet/public/lib/gtest",
+    "//sdk/lib/sys/cpp",
     "//src/lib/fxl",
     "//src/lib/fxl/test:gtest_main",
   ]

garnet/bin/sysmgr/integration_tests/mock_resolver.cc
@@ -2,13 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <memory>
-#include <vector>
-
 #include <fuchsia/pkg/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
+#include <lib/fidl/cpp/binding_set.h>
+#include <lib/sys/cpp/component_context.h>
 #include <lib/zx/channel.h>
-#include "lib/component/cpp/startup_context.h"
+
+#include <memory>
+#include <vector>
 
 namespace {
 
@@ -17,9 +18,8 @@ namespace {
 // make for a non-hermetic test.
 class PackageResolverMock : public fuchsia::pkg::PackageResolver {
  public:
-  PackageResolverMock()
-      : context_(component::StartupContext::CreateFromStartupInfo()) {
-    context_->outgoing().AddPublicService(bindings_.GetHandler(this));
+  PackageResolverMock() : context_(sys::ComponentContext::Create()) {
+    context_->outgoing()->AddPublicService(bindings_.GetHandler(this));
   }
 
   void Resolve(::std::string package_uri,
@@ -32,7 +32,7 @@ class PackageResolverMock : public fuchsia::pkg::PackageResolver {
   }
 
  private:
-  std::unique_ptr<component::StartupContext> context_;
+  std::unique_ptr<sys::ComponentContext> context_;
   std::vector<zx::channel> dir_channels_;
   fidl::BindingSet<fuchsia::pkg::PackageResolver> bindings_;
 };

garnet/bin/sysmgr/integration_tests/test_sysmgr_service.cc
@@ -3,9 +3,10 @@
 // found in the LICENSE file.
 
 #include <lib/async-loop/cpp/loop.h>
+#include <lib/fidl/cpp/binding_set.h>
+#include <lib/sys/cpp/component_context.h>
 #include <lib/zx/channel.h>
 #include <test/sysmgr/cpp/fidl.h>
-#include "lib/component/cpp/startup_context.h"
 
 namespace sysmgr {
 namespace test {
@@ -13,8 +14,8 @@ namespace {
 
 class Service : public ::test::sysmgr::Interface {
  public:
-  Service() : context_(component::StartupContext::CreateFromStartupInfo()) {
-    context_->outgoing().AddPublicService(bindings_.GetHandler(this));
+  Service() : context_(sys::ComponentContext::Create()) {
+    context_->outgoing()->AddPublicService(bindings_.GetHandler(this));
   }
 
   ~Service() = default;
@@ -24,7 +25,7 @@ class Service : public ::test::sysmgr::Interface {
   }
 
  private:
-  std::unique_ptr<component::StartupContext> context_;
+  std::unique_ptr<sys::ComponentContext> context_;
   fidl::BindingSet<::test::sysmgr::Interface> bindings_;
 };
 

garnet/public/lib/component/cpp/BUILD.gn
@@ -47,12 +47,7 @@ source_set("cpp") {
     "//garnet/bin/netemul_runner/*",
     "//garnet/bin/media/*",
     "//garnet/bin/netconnector/*",
-    "//garnet/bin/sysmgr/*",
     "//garnet/bin/system_monitor/harvester/*",
-    "//garnet/bin/trace/*",
-    "//garnet/bin/trace/tests/*",
-    "//garnet/bin/trace_manager/*",
-    "//garnet/bin/tz-util/*",
     "//garnet/bin/ui/set_renderer_params_HACK:*",
     "//garnet/bin/ui/present_view:*",
     "//garnet/bin/ui/set_root_view:*",
@@ -130,9 +125,6 @@ source_set("cpp") {
     "//topaz/app/term:*",
     "//topaz/auth_providers/google:*",
     "//topaz/bin/ui/skottie_viewer:*",
-    "//topaz/runtime/dart_runner:*",
-    "//topaz/runtime/flutter_runner:*",
-    "//topaz/tests/web_runner_tests:*",
     "//vendor/*",
   ]
 }

commit 5b854e488d76e41f906a65d2e7c58f18e65773c0

[sysmgr] don't log when no error has occurred
This was previously broken in 8c3db86.

Change-Id: I16bfd0902fff284f0c11ac47dfc60715804c7807

garnet/bin/sysmgr/app.cc
@@ -147,7 +147,9 @@ void App::ConnectToService(const std::string& service_name,
   if (status == ZX_OK) {
     status = child->Serve(fuchsia::io::OPEN_RIGHT_READABLE, std::move(channel));
   }
-  FXL_LOG(ERROR) << "Could not serve " << service_name << ": " << status;
+  if (status != ZX_OK) {
+    FXL_LOG(ERROR) << "Could not serve " << service_name << ": " << status;
+  }
 }
 
 void App::RegisterSingleton(std::string service_name,