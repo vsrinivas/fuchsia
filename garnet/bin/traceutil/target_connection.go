10251de97dd52c3a620b0bbf7336d1a9122eed98
James Sullivan <jfsulliv@google.com>
2019-04-15 18:47:35 +0000

    [traceutil] Fix path to netaddr tool
    The `out/build-zircon` directory was replaced with `out/default.zircon`.
    
    Change-Id: I66659aa3b92979cb0dfc2e9d9f69e0da4135f185

--- garnet/bin/traceutil/target_connection.go
+++ garnet/bin/traceutil/target_connection.go
@@ -35,7 +35,7 @@ func newSigner(keyFile string) (ssh.Signer, error) {
 }
 
 func findDefaultTarget() (string, error) {
-	netaddr := path.Join(fuchsiaRoot, "out", "build-zircon", "tools", "netaddr")
+	netaddr := path.Join(fuchsiaRoot, "out", "default.zircon", "tools", "netaddr")
 	output, err := getCommandOutput(netaddr, "--fuchsia")
 	output = strings.TrimSpace(output)
 	if err != nil {
