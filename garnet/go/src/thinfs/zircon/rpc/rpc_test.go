ed21e860d2005be0bed6bf1a598488275e9a3198
Marco Vanotti <mvanotti@google.com>
2019-04-16 21:41:18 +0000

    Reland "[thinfs] fix nil pointer dereference."
    This reverts commit 6be4d7626cd2aa0f1903e6822e491816ddfaee2b.
    
    Reason for revert: Looks like the build errors were unrelated to this commit.
    
    Original change's description:
    > Revert "[thinfs] fix nil pointer dereference."
    > 
    > This reverts commit 36b9d349a5fea6c0aa211e3f234e18a83ed33674.
    > 
    > Reason for revert: I received a LUCI email saying that this breaks asan builds.
    > 
    > Original change's description:
    > > [thinfs] fix nil pointer dereference.
    > > 
    > > This CL fixes a nil pointer dereference that happens in the
    > > thinfs/zircon/rpc package.
    > > 
    > > The issue is that the `directoryWrapper is not correctly initialized,
    > > not setting the cookies map. Upon calls to GetToken(), the dictionary is
    > > dereferenced and that causes a panic.
    > > 
    > > The commit just makes sure that the cookies map is initialized
    > > correctly, and adds a test for it.
    > > 
    > > ZX-3928: Done
    > > 
    > > TEST=/pkfs/packages/go_thinfs_tests/0/test/go_thinfs_test
    > > 
    > > Change-Id: I4686714f547f8a759d11dcc2e9414ce60e37707d
    > 
    > TBR=kulakowski@google.com,smklein@google.com,mvanotti@google.com
    > 
    > Change-Id: I8bd13bf33f3ee744afed06d4e5d43e74b4349041
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    
    TBR=kulakowski@google.com,smklein@google.com,mvanotti@google.com
    
    Change-Id: I96f2afca116fac4a8f83dfbc15113234907cac17
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

---
+++ garnet/go/src/thinfs/zircon/rpc/rpc_test.go
@@ -0,0 +1,51 @@
+package rpc
+
+import (
+	"syscall/zx"
+	"testing"
+	"thinfs/fs"
+)
+
+type dummyFs struct{}
+
+func (d *dummyFs) Blockcount() int64           { return 0 }
+func (d *dummyFs) Blocksize() int64            { return 0 }
+func (d *dummyFs) Size() int64                 { return 0 }
+func (d *dummyFs) Close() error                { return nil }
+func (d *dummyFs) RootDirectory() fs.Directory { return nil }
+func (d *dummyFs) Type() string                { return "dummy" }
+func (d *dummyFs) FreeSize() int64             { return 0 }
+
+func TestCookies(t *testing.T) {
+	c1, c2, err := zx.NewChannel(0)
+	if err != nil {
+		t.Fatalf("failed to create channel: %v", err)
+	}
+	defer c1.Close()
+	defer c2.Close()
+
+	vfs, err := NewServer(&dummyFs{}, zx.Handle(c2))
+	if err != nil {
+		t.Fatalf("failed to create server: %v", err)
+	}
+	defer vfs.fs.Close()
+
+	if len(vfs.dirs) != 1 {
+		t.Fatalf("Unexpected number of directories. Want %d, got %d", 1, len(vfs.dirs))
+	}
+
+	for _, dir := range vfs.dirs {
+		res, h, err := dir.GetToken()
+		if err != nil {
+			t.Fatalf("GetToken() failed: %v", err)
+		}
+		if zx.Status(res) != zx.ErrOk {
+			t.Fatalf("GetToken() returned wrong value. Want %v, got %v", zx.ErrOk, zx.Status(res))
+		}
+
+		dir.setCookie(32)
+		if dir.getCookie(h) != 32 {
+			t.Fatalf("Wrong Cookie retrieved. Want %d, got %d", 32, dir.getCookie(h))
+		}
+	}
+}
