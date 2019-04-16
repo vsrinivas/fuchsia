33b6af7bb6aaf5c771c76d1150c6a7ead7b453e3
Taylor Cramer <cramertj@google.com>
2019-04-16 19:37:21 +0000

    [rust][http] http_client to fuchsia-component
    Change-Id: Ie7784bc3cc61f543db94265e48be178fb2e3b9d4

--- garnet/bin/http_client/src/main.rs
+++ garnet/bin/http_client/src/main.rs
@@ -4,11 +4,11 @@
 
 #![feature(async_await, await_macro, futures_api)]
 
-use failure::{Error, ResultExt};
-use fidl::endpoints::{RequestStream, ServerEnd, ServiceMarker};
+use failure::Error;
+use fidl::endpoints::ServerEnd;
 use fidl_fuchsia_net_oldhttp as oldhttp;
-use fuchsia_app::server::ServicesServer;
 use fuchsia_async as fasync;
+use fuchsia_component::server::ServiceFs;
 use fuchsia_hyper as hyper;
 use futures::compat::Future01CompatExt;
 use futures::prelude::*;
@@ -48,10 +48,9 @@ fn spawn_old_url_loader(server: ServerEnd<oldhttp::UrlLoaderMarker>) {
     );
 }
 
-fn spawn_old_server(chan: fasync::Channel) {
+fn spawn_old_server(stream: oldhttp::HttpServiceRequestStream) {
     fasync::spawn(
         async move {
-            let stream = oldhttp::HttpServiceRequestStream::from_channel(chan);
             await!(stream.err_into().try_for_each_concurrent(None, async move |message| {
                 let oldhttp::HttpServiceRequest::CreateUrlLoader { loader, .. } = message;
                 spawn_old_url_loader(loader);
@@ -64,11 +63,11 @@ fn spawn_old_server(chan: fasync::Channel) {
 
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), Error> {
-    let fut = ServicesServer::new()
-        .add_service((oldhttp::HttpServiceMarker::NAME, move |chan| spawn_old_server(chan)))
-        .start()
-        .context("Error starting http_client services server")?;
+    let mut fs = ServiceFs::new();
+    fs.dir("public")
+        .add_fidl_service(spawn_old_server);
+    fs.take_and_serve_directory_handle()?;
 
-    await!(fut).context("failed to execute http_client future")?;
+    let () = await!(fs.collect());
     Ok(())
 }
