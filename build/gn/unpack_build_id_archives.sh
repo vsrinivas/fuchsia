e52f9b4c0a84911e457b90c37fb94a246b660df2
Joshua Seaton <joshuaseaton@google.com>
2019-04-18 19:35:55 +0000

    [build-id] pass extended ids.txt to dump_breakpad_symbols
    This change creates a concatenation of the ids.txt produced by the build
    that that produced by the .build-id archives downloaded from CIPD - and
    passes that to dump_breakpad_symbols.
    
    Tested locally.
    
    Bug: INTK-884 #comment
    Bug: INTK-969 #comment
    Change-Id: I9d96f8098239ca6a1c828be0a44dbe36a0c3b14a

---
+++ build/gn/unpack_build_id_archives.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Copyright 2019 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+# Directory in which the prebuilt build-id archives are found.
+build_id_archive_dir=${1}
+# Directory at which to unpack all of the build-id archives.
+unpack_dir=${2}
+# The path at which to output the list of archives.
+output=${3}
+
+archives=()
+if [ -d $build_id_archive_dir ]
+then
+  archives=$(find "$build_id_archive_dir" -name "*.tar.bz2" -follow)
+fi
+
+if [ ! -d $unpack_dir ]
+then mkdir $unpack_dir
+fi
+
+if [ ! -z $array ]
+then echo $archives | xargs --max-procs 10 -n1 tar -xC $unpack_dir -jf
+fi
+
+printf "%s\n" "${archives[@]}" > $output
