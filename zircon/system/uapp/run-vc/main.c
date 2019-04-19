35868fd43ba0d2aa649e7dda5c823acbe60ea8e4
Ed Coyne <edcoyne@google.com>
2019-04-19 00:17:30 +0000

    [virtcon] Make virtcon host a fidl service for sessions.
    the virtcon (virtual-console) binary used to provide new sessions
    through a convoluted path through device coordinator. This will make it
    expose a fidl service directly for clients to request a new session.
    
    This removes device coordinator from the operational loop and will make
    it easy for the virtual-console binary to stand on it's own once we have
    something else to start it.
    
    It is a little messy due to the virtcon binary using the "port" library
    heavily and fidl being built around the async_loop.
    
    Test:
     * Booted on vim2, core.arm64. Virtual consoles work as expected.
     * Booted astro, looks normal, "k ut all" and "runtests -a" pass.
    
    ZX-3403 # Get virtcon out of the way.
    
    Change-Id: I09e66a7c5bc91fb41b6bfb99510b5be1aa609d57

--- zircon/system/uapp/run-vc/main.c
+++ zircon/system/uapp/run-vc/main.c
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 #include <fcntl.h>
-#include <fuchsia/device/manager/c/fidl.h>
+#include <fuchsia/virtualconsole/c/fidl.h>
 #include <lib/fdio/io.h>
 #include <lib/fdio/spawn.h>
 #include <lib/fdio/fd.h>
@@ -32,66 +32,50 @@ static zx_status_t dmctl_watch_func(int dirfd, int event, const char* fn, void* 
     return ZX_OK;
 };
 
-static zx_status_t open_dmctl(int* fd) {
-    int dirfd = open("/dev/misc", O_RDONLY);
-    if (dirfd < 0) {
-        return ZX_ERR_IO;
+static zx_status_t connect_to_service(const char* service, zx_handle_t* channel) {
+    zx_handle_t channel_local, channel_remote;
+    zx_status_t status = zx_channel_create(0, &channel_local, &channel_remote);
+    if (status != ZX_OK) {
+        printf("failed to create channel: %s\n", zx_status_get_string(status));
+        return false;
     }
-    zx_status_t status = fdio_watch_directory(dirfd, dmctl_watch_func, ZX_TIME_INFINITE, NULL);
-    if (status != ZX_ERR_STOP) {
-        if (status == ZX_OK) {
-            status = ZX_ERR_BAD_STATE;
-        }
-        printf("failed to watch /dev/misc: %s\n", zx_status_get_string(status));
-        close(dirfd);
-        return status;
+
+    status = fdio_service_connect(service, channel_remote);
+    if (status != ZX_OK) {
+        zx_handle_close(channel_local);
+        printf("failed to connect to service: %s\n", zx_status_get_string(status));
+        return false;
     }
-    *fd = openat(dirfd, "dmctl", O_RDWR);
-    close(dirfd);
-    if (*fd < 0) {
-        return ZX_ERR_IO;
-    }
-    return ZX_OK;
+
+    *channel = channel_local;
+    return true;
+
 }
 
 int main(int argc, const char** argv) {
-    int fd;
-    zx_status_t status = open_dmctl(&fd);
+    zx_handle_t session_manager = ZX_HANDLE_INVALID;
+    if (!connect_to_service("/svc/fuchsia.virtualconsole.SessionManager", &session_manager)) {
+        return -1;
+    }
+
+
+    zx_handle_t session, session_remote;
+    if (zx_channel_create(0, &session, &session_remote) != ZX_OK) {
+        return -1;
+    }
+
+    zx_status_t status = fuchsia_virtualconsole_SessionManagerCreateSession(session_manager,
+                                                                            session_remote);
     if (status != ZX_OK) {
-        fprintf(stderr, "failed to open dmctl: %s\n", zx_status_get_string(status));
-        return -1;
-    }
-    zx_handle_t dmctl;
-    status = fdio_get_service_handle(fd, &dmctl);
-    if (status != ZX_OK) {
-        fprintf(stderr, "error %s converting fd to handle\n", zx_status_get_string(status));
+        fprintf(stderr, "run-vc: failed to create session: %s\n", zx_status_get_string(status));
         return -1;
     }
 
-    zx_handle_t h0, h1;
-    if (zx_channel_create(0, &h0, &h1) < 0) {
-        return -1;
-    }
 
-    if (fuchsia_device_manager_ExternalControllerOpenVirtcon(dmctl, h1) < 0) {
+    if (session == ZX_HANDLE_INVALID) {
+        fprintf(stderr, "Received invalid handle from session manager!\n");
         return -1;
     }
-    h1 = ZX_HANDLE_INVALID;
-    zx_handle_close(dmctl);
-
-    zx_object_wait_one(h0, ZX_CHANNEL_READABLE | ZX_CHANNEL_PEER_CLOSED,
-                       ZX_TIME_INFINITE, NULL);
-
-    uint32_t type = 0;
-    zx_handle_t handle = ZX_HANDLE_INVALID;
-    uint32_t dcount = 0, hcount = 0;
-    if (zx_channel_read(h0, 0, &type, &handle, sizeof(type), 1, &dcount, &hcount) != ZX_OK) {
-        return -1;
-    }
-    if (dcount / sizeof(uint32_t) != hcount) {
-        return -1;
-    }
-    zx_handle_close(h0);
 
     // start shell if no arguments
     if (argc == 1) {
@@ -108,15 +92,16 @@ int main(int argc, const char** argv) {
     }
 
     uint32_t flags = FDIO_SPAWN_CLONE_ALL & ~FDIO_SPAWN_CLONE_STDIO;
+    uint32_t type = PA_HND(PA_FD, FDIO_FLAG_USE_FOR_STDIO);
 
     fdio_spawn_action_t actions[2] = {
         {.action = FDIO_SPAWN_ACTION_SET_NAME, .name = {.data = pname}},
-        {.action = FDIO_SPAWN_ACTION_ADD_HANDLE, .h = {.id = type, .handle = handle}},
+        {.action = FDIO_SPAWN_ACTION_ADD_HANDLE, .h = {.id = type, .handle = session}},
     };
 
     char err_msg[FDIO_SPAWN_ERR_MSG_MAX_LENGTH];
     status = fdio_spawn_etc(ZX_HANDLE_INVALID, flags, argv[0], argv,
-                            NULL, 1 + hcount, actions, NULL, err_msg);
+                            NULL, countof(actions), actions, NULL, err_msg);
     if (status != ZX_OK) {
         fprintf(stderr, "error %d (%s) launching: %s\n", status,
                 zx_status_get_string(status), err_msg);
