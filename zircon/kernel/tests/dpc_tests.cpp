103293b0df76c9d335b11d02ad7a4deb76d7a8bb
Venkatesh Srinivas <venkateshs@google.com>
2019-04-26 21:40:52 +0000

    [zircon][unit tests] Add unit tests of dpc_queue
    Add basic tests that exercise non-blocking and blocking calls from dpc_queue.
    Check that a DPC runs on the same CPU that it was queued on in the absence of
    hotplug. Also check queueing many DPCs and seeing that they all execute.
    
    Tested:
    * k ut dpc_tests on qemu x86, qemu-kvm x86, qemu ARM
    Ex (qemu ARM):
    $ k ut dpc_tests
    [00010.025] 00000:00000> dpc_tests : Running 2 tests...
    [00010.030] 00000:00000>   basic test of dpc_queue    : PASSED (3393392 nSec)
    [00010.037] 00000:00000>   test queueing lots of DPCs : PASSED (6933168 nSec)
    [00010.037] 00000:00000> dpc_tests : All tests passed (2/2) in 11158208 nSec
    [00010.041] 01188:01209>
    [00010.041] 01188:01209> SUMMARY: Ran 1 test case: 0 failed
    $
    
    Change-Id: I405c9eaf99e9f6050378293889d417e97a54ab81

---
+++ zircon/kernel/tests/dpc_tests.cpp
@@ -0,0 +1,75 @@
+// Copyright 2019 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include "tests.h"
+
+#include <arch/arch_ops.h>
+#include <arch/mp.h>
+#include <kernel/dpc.h>
+#include <kernel/event.h>
+#include <ktl/atomic.h>
+#include <lib/unittest/unittest.h>
+
+struct event_signal_from_dpc_context {
+   dpc_t dpc;
+   event_t event;
+   ktl::atomic<uint> expected_cpu;
+   ktl::atomic<bool> dpc_started;
+};
+static void event_signal_from_dpc_check_cpu(dpc_t* dpc) {
+    auto* const context = reinterpret_cast<struct event_signal_from_dpc_context*>(dpc->arg);
+    context->dpc_started = true;
+
+    // DPCs allow interrupts and blocking.
+    DEBUG_ASSERT(!arch_ints_disabled());
+    DEBUG_ASSERT(!arch_blocking_disallowed());
+    DEBUG_ASSERT(context->expected_cpu == arch_curr_cpu_num());
+
+    event_signal(&context->event, /*reschedule=*/false);
+}
+
+static bool test_dpc_queue() {
+    BEGIN_TEST;
+
+    static constexpr int kNumDPCs = 72;
+    struct event_signal_from_dpc_context context[kNumDPCs];
+
+    // Init all the DPCs and supporting context.
+    for (int i = 0; i < kNumDPCs; i++) {
+        context[i].dpc = DPC_INITIAL_VALUE;
+        event_init(&context[i].event, /*initial=*/false, /*flags=*/0);
+        context[i].dpc_started = false;
+    }
+
+    // Fire off DPCs.
+    for (int i = 0; i < kNumDPCs; i++) {
+        context[i].dpc.func = event_signal_from_dpc_check_cpu;
+        context[i].dpc.arg = reinterpret_cast<void*>(&context[i]);
+        arch_disable_ints();
+        context[i].expected_cpu = arch_curr_cpu_num();
+        dpc_queue(&context[i].dpc, /*reschedule=*/false);
+        arch_enable_ints();
+    }
+    for (int i = 0; i < kNumDPCs; i++) {
+        if (context[i].dpc_started) {
+            // Once the DPC has started executing, we can reclaim the submitted dpc_t. Zero it to
+            // try to check this.
+            context[i].dpc = DPC_INITIAL_VALUE;
+        }
+    }
+    for (int i = 0; i < kNumDPCs; i++) {
+        event_wait(&context[i].event);
+    }
+    for (int i = 0; i < kNumDPCs; i++) {
+        event_destroy(&context[i].event);
+    }
+
+    END_TEST;
+}
+
+UNITTEST_START_TESTCASE(dpc_tests)
+UNITTEST("basic test of dpc_queue", test_dpc_queue)
+UNITTEST_END_TESTCASE(dpc_tests, "dpc_tests", "Tests of DPCs");
