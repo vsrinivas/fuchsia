d263137cc1880ec5950e10cd1d06d333f2b5e618
Roland McGrath <mcgrathr@google.com>
2019-04-29 19:27:32 +0000

    [musl] Move Zircon-specific code out of third_party/ulib/musl
    This code was all written from scratch for Fuchsia.
    
    Test: pure source file moves, no effect on compiled code
    Change-Id: I9fc05d520884120560e4b28709f98acfa87f96cd

--- zircon/system/ulib/c/zircon/internal.c
+++ zircon/system/ulib/c/zircon/internal.c
@@ -1,3 +1,7 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "zircon_impl.h"
 
 #undef _zx_process_self
