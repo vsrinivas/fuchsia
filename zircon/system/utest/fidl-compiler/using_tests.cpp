80c1e6a41994603a62d2f9beb4add37b8592c672
Pascal Perez <pascallouis@google.com>
2019-04-29 17:33:20 +0000

    [fidlc] Minor test cleanup
    - Simpler library names in line with more recent tests;
    - Clearer names, and no comments (which duplicates what the test name
    conveys)
    - No _test suffix
    
    Also adding a test for unknown using, which was not covered before.
    
    Change-Id: I299acf93596f5b4d4d3907c7c0feac0228248d55

--- zircon/system/utest/fidl-compiler/using_tests.cpp
+++ zircon/system/utest/fidl-compiler/using_tests.cpp
@@ -13,13 +13,12 @@
 
 namespace {
 
-// Test that using properly allows referring to symbols in dependent library.
-bool valid_using_without_alias_test() {
+bool valid_using() {
     BEGIN_TEST;
 
     SharedAmongstLibraries shared;
     TestLibrary dependency("dependent.fidl", R"FIDL(
-library fidl.test.uzing.dependent;
+library dependent;
 
 struct Bar {
     int8 s;
@@ -28,13 +27,13 @@ struct Bar {
 )FIDL", &shared);
     ASSERT_TRUE(dependency.Compile());
 
-    TestLibrary library("uzing.fidl", R"FIDL(
-library fidl.test.uzing;
+    TestLibrary library("example.fidl", R"FIDL(
+library example;
 
-using fidl.test.uzing.dependent;
+using dependent;
 
 struct Foo {
-    fidl.test.uzing.dependent.Bar dep;
+    dependent.Bar dep;
 };
 
 )FIDL", &shared);
@@ -44,15 +43,12 @@ struct Foo {
     END_TEST;
 }
 
-// Test that using properly allows referring to symbols in dependent library,
-// using the aliased name of the dependent library, or the fully qualified name
-// of the dependent library.
-bool valid_using_with_alias_test() {
+bool valid_using_with_as() {
     BEGIN_TEST;
 
     SharedAmongstLibraries shared;
     TestLibrary dependency("dependent.fidl", R"FIDL(
-library fidl.test.uzing.dependent;
+library dependent;
 
 struct Bar {
     int8 s;
@@ -61,14 +57,14 @@ struct Bar {
 )FIDL", &shared);
     ASSERT_TRUE(dependency.Compile());
 
-    TestLibrary library("uzing.fidl", R"FIDL(
-library fidl.test.uzing;
+    TestLibrary library("example.fidl", R"FIDL(
+library example;
 
-using fidl.test.uzing.dependent as dependent_alias;
+using dependent as the_alias;
 
 struct Foo {
-    fidl.test.uzing.dependent.Bar dep1;
-    dependent_alias.Bar dep2;
+    dependent.Bar dep1;
+    the_alias.Bar dep2;
 };
 
 )FIDL", &shared);
@@ -78,17 +74,16 @@ struct Foo {
     END_TEST;
 }
 
-// Test that an unknown dependency is properly reported.
-bool invalid_did_not_declare_dependency_with_using() {
+bool invalid_missing_using() {
     BEGIN_TEST;
 
-    TestLibrary library("uzing.fidl", R"FIDL(
-library fidl.test.uzing;
+    TestLibrary library(R"FIDL(
+library example;
 
 // missing using.
 
 struct Foo {
-  fidl.test.uzing.dependent.Bar dep;
+    dependent.Bar dep;
 };
 
 )FIDL");
@@ -96,28 +91,48 @@ struct Foo {
     const auto& errors = library.errors();
     ASSERT_EQ(1, errors.size());
     ASSERT_STR_STR(errors[0].c_str(),
-        "Unknown dependent library fidl.test.uzing.dependent. Did you require it with `using`?");
+        "Unknown dependent library dependent. Did you require it with `using`?");
 
     END_TEST;
 }
 
-// Test that a duplicated using declaration is the same file is reported as an
-// error.
+bool invalid_unknown_using() {
+    BEGIN_TEST;
+
+    TestLibrary library(R"FIDL(
+library example;
+
+using dependent; // unknown using.
+
+struct Foo {
+    dependent.Bar dep;
+};
+
+)FIDL");
+    ASSERT_FALSE(library.Compile());
+    const auto& errors = library.errors();
+    ASSERT_EQ(1, errors.size());
+    ASSERT_STR_STR(errors[0].c_str(),
+        "Could not find library named dependent. Did you include its sources with --files?");
+
+    END_TEST;
+}
+
 bool invalid_duplicate_using() {
     BEGIN_TEST;
 
     SharedAmongstLibraries shared;
     TestLibrary dependency("dependent.fidl", R"FIDL(
-library fidl.test.uzing.dependent;
+library dependent;
 
 )FIDL", &shared);
     ASSERT_TRUE(dependency.Compile());
 
-    TestLibrary library("uzing.fidl", R"FIDL(
-library fidl.test.uzing;
+    TestLibrary library("example.fidl", R"FIDL(
+library example;
 
-using fidl.test.uzing.dependent;
-using fidl.test.uzing.dependent; // duplicated
+using dependent;
+using dependent; // duplicated
 
 )FIDL", &shared);
     ASSERT_TRUE(library.AddDependentLibrary(std::move(dependency)));
@@ -125,7 +140,7 @@ using fidl.test.uzing.dependent; // duplicated
     const auto& errors = library.errors();
     ASSERT_EQ(1, errors.size());
     ASSERT_STR_STR(errors[0].c_str(),
-        "Library fidl.test.uzing.dependent already imported. Did you require it twice?");
+        "Library dependent already imported. Did you require it twice?");
 
     END_TEST;
 }
@@ -133,8 +148,9 @@ using fidl.test.uzing.dependent; // duplicated
 } // namespace
 
 BEGIN_TEST_CASE(using_tests)
-RUN_TEST(valid_using_without_alias_test)
-RUN_TEST(valid_using_with_alias_test)
-RUN_TEST(invalid_did_not_declare_dependency_with_using)
+RUN_TEST(valid_using)
+RUN_TEST(valid_using_with_as)
+RUN_TEST(invalid_missing_using)
+RUN_TEST(invalid_unknown_using)
 RUN_TEST(invalid_duplicate_using)
 END_TEST_CASE(using_tests)
