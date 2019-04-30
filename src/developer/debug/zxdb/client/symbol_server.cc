1057d754f807aa5e0e85c8b26394ed14ed1bc83f
Casey Dahlin <sadmac@google.com>
2019-04-30 00:00:01 +0000

    [debugger] Remove use of tmpnam
    Bug: DX-1444
    Change-Id: I63d3a31d0352508da6a18ecabdecc076158b1ad2

--- src/developer/debug/zxdb/client/symbol_server.cc
+++ src/developer/debug/zxdb/client/symbol_server.cc
@@ -243,19 +243,11 @@ void CloudStorageSymbolServer::FetchWithCurl(const std::string& build_id,
       ClientSettings::System::kSymbolCache);
   std::string path;
 
-  if (cache_path.empty()) {
-    // We don't have a folder specified where downloaded symbols go. We'll just
-    // drop it in tmp and at least you'll be able to use them for this session.
-    path = std::tmpnam(nullptr);
-  } else {
+  if (!cache_path.empty()) {
     std::error_code ec;
     auto path_obj = std::filesystem::path(cache_path) / ".build-id";
 
-    if (!std::filesystem::is_directory(path_obj, ec)) {
-      // Something's wrong with the build ID folder we were provided. We'll
-      // just drop it in tmp.
-      path = std::tmpnam(nullptr);
-    } else {
+    if (std::filesystem::is_directory(path_obj, ec)) {
       // Download to a temporary file, so if we get cancelled (or we get sent
       // a 404 page instead of the real symbols) we don't pollute the build ID
       // folder.
@@ -264,9 +256,23 @@ void CloudStorageSymbolServer::FetchWithCurl(const std::string& build_id,
     }
   }
 
-  FXL_DCHECK(!path.empty());
+  FILE* file = nullptr;
 
-  FILE* file = std::fopen(path.c_str(), "wb");
+  // We don't have a folder specified where downloaded symbols go. We'll just
+  // drop it in tmp and at least you'll be able to use them for this session.
+  if (path.empty()) {
+    path = "/tmp/zxdb_downloaded_symbolsXXXXXX\0";
+    int fd = mkstemp(path.data());
+    path.pop_back();
+
+    if (fd >= 0) {
+      // Ownership of the fd is absorbed by fdopen.
+      file = fdopen(fd, "wb");
+    }
+  } else {
+    file = std::fopen(path.c_str(), "wb");
+  }
+
   if (!file) {
     debug_ipc::MessageLoop::Current()->PostTask(
         FROM_HERE, [cb]() { cb(Err("Error opening temporary file."), ""); });
