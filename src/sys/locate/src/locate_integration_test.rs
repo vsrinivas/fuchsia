370367edcd8eb36a9516d29e46be179521281a2e
Leo Lung <leolung@google.com>
2019-04-30 22:15:53 +0000

    [component_index][locate] Add locate tool
    Supports matching of exactly one component, or --list to match more than
    one component.
    
    CF-162 #comment
    
    TEST=
    fx run-test locate_integration_test
    
    locate tiles_ctl
    fuchsia-pkg://fuchsia.com/tiles_ctl#meta/tiles_ctl.cmx
    
    locate ctl
    fuchsia-pkg://fuchsia.com/stash_ctl#meta/stash_ctl.cmx
    fuchsia-pkg://fuchsia.com/tiles_ctl#meta/tiles_ctl.cmx
    ...
    Error: "ctl" matched more than one component. Try `locate --list` instead.
    
    locate --list ctl
    fuchsia-pkg://fuchsia.com/stash_ctl#meta/stash_ctl.cmx
    fuchsia-pkg://fuchsia.com/tiles_ctl#meta/tiles_ctl.cmx
    ...
    
    locate --list tiles_ctl
    fuchsia-pkg://fuchsia.com/tiles_ctl#meta/tiles_ctl.cmx
    
    locate leolung
    Error: "leolung" did not match any components.
    
    locate
    locate foo bar
    locate --list foo bar
    locate --list
    Usage: locate [--list] <search_keyword>
    
    Locates the fuchsia-pkg URL of <search_keyword>.
    
    Options:
      --list    Allows matching of more than one component.
    Error: Unable to parse args.
    
    locate rolldice
    fuchsia-pkg://fuchsia.com/rolldice#meta/rolldice.cmx
    
    run `locate rolldice`
    +---+
    |* *|
    |* *|
    |* *|
    +---+
    
    Change-Id: Iafa1cc048b2940967e08b3c4f244a1945b6bea00

---
+++ src/sys/locate/src/locate_integration_test.rs
@@ -0,0 +1,57 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![feature(async_await, await_macro, futures_api)]
+
+use failure::Error;
+use fidl_fuchsia_sys::FileDescriptor;
+use fuchsia_async as fasync;
+use fuchsia_component::client::{launch_with_options, launcher, LaunchOptions};
+use fuchsia_runtime::HandleType;
+use std::io::{BufRead, BufReader};
+
+/// Integration test for the locate shell tool.
+///
+/// Launches locate as locate.cmx, a component. Attempts to locate and assert
+/// the fuchsia-pkg URL of itself, the test component
+/// locate_integration_test.cmx.
+#[fasync::run_singlethreaded]
+async fn main() -> Result<(), Error> {
+    let (pipe, socket) = fdio::pipe_half()?;
+    let mut launch_options = LaunchOptions::new();
+    launch_options.set_out(FileDescriptor {
+        type0: HandleType::FileDescriptor as i32,
+        type1: 0,
+        type2: 0,
+        handle0: Some(socket.into()),
+        handle1: None,
+        handle2: None,
+    });
+
+    let launcher = launcher().expect("Failed to open launcher service");
+    let _app = launch_with_options(
+        &launcher,
+        "fuchsia-pkg://fuchsia.com/locate#meta/locate.cmx".to_string(),
+        Some(vec!["locate_integration_test".to_string()]),
+        launch_options,
+    )
+    .expect("Failed to launch locate");
+
+    let mut reader = BufReader::new(pipe);
+
+    // Assert locate.cmx was able to locate locate_integration_test.cmx.
+    let mut line = String::new();
+    reader.read_line(&mut line)?;
+    assert_eq!(
+        line,
+        "fuchsia-pkg://fuchsia.com/locate_integration_test#meta/locate_integration_test.cmx\n"
+    );
+
+    // Assert EOF.
+    line.clear();
+    reader.read_line(&mut line)?;
+    assert_eq!(line.is_empty(), true);
+
+    Ok(())
+}
---
+++ src/sys/locate/src/main.rs
@@ -0,0 +1,117 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![feature(async_await, await_macro, futures_api)]
+
+use failure::{err_msg, format_err, Error, ResultExt};
+use fidl_fuchsia_sys_index::{ComponentIndexMarker, FuzzySearchError};
+use fuchsia_async as fasync;
+use fuchsia_component::client::{launch, launcher};
+use std::convert::{TryFrom, TryInto};
+use std::env;
+
+#[fasync::run_singlethreaded]
+async fn main() {
+    match await!(run_locate()) {
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+        Ok(()) => {}
+    }
+}
+
+async fn run_locate() -> Result<(), Error> {
+    let cfg: LocateConfig = env::args().try_into()?;
+
+    let launcher = launcher().context("Failed to open launcher service.")?;
+    let app = launch(
+        &launcher,
+        "fuchsia-pkg://fuchsia.com/component_index#meta/component_index.cmx".to_string(),
+        None,
+    )
+    .context("Failed to launch component_index service.")?;
+
+    let component_index = app
+        .connect_to_service::<ComponentIndexMarker>()
+        .context("Failed to connect to component_index service.")?;
+
+    match await!(component_index.fuzzy_search(&cfg.search_keyword))? {
+        Ok(res_vec) => fuzzy_search_result(res_vec, cfg),
+        Err(e) => fuzzy_search_error(e, cfg),
+    }
+}
+
+fn fuzzy_search_result(res_vec: Vec<String>, cfg: LocateConfig) -> Result<(), Error> {
+    if res_vec.is_empty() {
+        return Err(format_err!("\"{}\" did not match any components.", cfg.search_keyword));
+    }
+    if cfg.list {
+        for res in &res_vec {
+            println!("{}", res);
+        }
+    } else {
+        if res_vec.len() == 1 {
+            println!("{}", res_vec[0]);
+        } else {
+            for res in &res_vec {
+                eprintln!("{}", res);
+            }
+            return Err(format_err!(
+                "\"{}\" matched more than one component. Try `locate --list` instead.",
+                cfg.search_keyword
+            ));
+        }
+    }
+    Ok(())
+}
+
+fn fuzzy_search_error(e: FuzzySearchError, cfg: LocateConfig) -> Result<(), Error> {
+    if e == FuzzySearchError::MalformedInput {
+        return Err(format_err!(
+            "\"{}\" contains unsupported characters. Valid characters are [A-Z a-z 0-9 / _ -].",
+            cfg.search_keyword
+        ));
+    }
+    Err(err_msg("fuchsia.sys.index.FuzzySearch could not serve the input query."))
+}
+
+struct LocateConfig {
+    list: bool,
+    search_keyword: String,
+}
+
+impl TryFrom<env::Args> for LocateConfig {
+    type Error = Error;
+    fn try_from(args: env::Args) -> Result<LocateConfig, Error> {
+        let mut args = args.peekable();
+        // Ignore arg[0]
+        let _ = args.next();
+
+        // Consume arg[1] if it is --list
+        let list = args.peek() == Some(&"--list".to_string());
+        if list {
+            let _ = args.next();
+        }
+
+        // Ensure nothing beyond current arg.
+        if let (Some(search_keyword), None) = (args.next(), args.next()) {
+            return Ok(LocateConfig { list, search_keyword });
+        } else {
+            help();
+            return Err(err_msg("Unable to parse args."));
+        }
+    }
+}
+
+fn help() {
+    println!(
+        r"Usage: locate [--list] <search_keyword>
+
+Locates the fuchsia-pkg URL of <search_keyword>.
+
+Options:
+  --list    Allows matching of more than one component."
+    )
+}
