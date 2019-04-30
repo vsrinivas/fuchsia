3418b8ee03b9da7c4cef0a48bb83366db479d607
Shac Ron <sron@google.com>
2019-04-30 01:44:39 +0000

    [musl] Add casts in byteswap to appease C++
    Added a few casts in byteswap to make it compatible with C++'s stricter
    implicit conversion rules. Can now be included from C++ files.
    
    Tests: built clang and gcc, ran QEMU.
    Change-Id: If85bf150c8afc3f58ef12a961f5e55ccef6010dc

--- zircon/third_party/ulib/musl/include/byteswap.h
+++ zircon/third_party/ulib/musl/include/byteswap.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 
 static __inline uint16_t __bswap_16(uint16_t __x) {
-    return __x << 8 | __x >> 8;
+    return (uint16_t)(__x << 8 | __x >> 8);
 }
 
 static __inline uint32_t __bswap_32(uint32_t __x) {
@@ -14,7 +14,7 @@ static __inline uint32_t __bswap_32(uint32_t __x) {
 }
 
 static __inline uint64_t __bswap_64(uint64_t __x) {
-    return ((uint64_t)__bswap_32(__x)) << 32 | __bswap_32(__x >> 32);
+    return ((uint64_t)__bswap_32((uint32_t)__x)) << 32 | __bswap_32((uint32_t)(__x >> 32));
 }
 
 #define bswap_16(x) __bswap_16(x)
