8b1cd61f5b5127cfab7fed501edeaa3839511d17
Dan Johnson <computerdruid@google.com>
2019-05-02 17:39:42 +0000

    [devshell] fix check for running `fx serve`
    Change-Id: I543f00f9b875e9e0e5cd929caaaf2e640d346372

--- tools/devshell/lib/updates.sh
+++ tools/devshell/lib/updates.sh
@@ -4,14 +4,14 @@
 
 function check-for-amber-server {
   # Make sure it is running.
-  if [[ -z "$(pgrep -f "amber-files/repository")" ]]; then
+  if [[ -z "$(pgrep -f 'pm serve .*/amber-files')" ]]; then
     fx-error "It looks like serve-updates is not running."
     fx-error "You probably need to start \"fx serve\""
     return -1
   fi
 
   # Warn if it is using the wrong repository.
-  if [[ -z "$(pgrep -f "${FUCHSIA_BUILD_DIR}/amber-files/repository")" ]]; then
+  if [[ -z "$(pgrep -f "pm serve .*${FUCHSIA_BUILD_DIR}/amber-files")" ]]; then
     fx-warn "WARNING: It looks like serve-updates is running in a different workspace."
     fx-warn "WARNING: You probably need to stop that one and start a new one here with \"fx serve\""
   fi
