e3f9407152a584ffb00555b02606167eadef8138
Vardhan Mudunuru <vardhan@google.com>
2019-05-02 22:02:05 +0000

    Revert "[run] Integrate with component_index"
    This reverts commit ee539081462f101f5fcc8a567631fdfcc8047441.
    
    Reason for revert: Fails to build in CI.
    
    Original change's description:
    > [run] Integrate with component_index
    > 
    > CF-162 #comment
    > 
    > Change-Id: Icb811b1073772923d1c471e7c1d68eac0b57281b
    
    TBR=jamesr@google.com,anmittal@google.com,shayba@google.com,leolung@google.com
    
    Change-Id: Ic7949fa65f76f3db8b8b713f274118d02436320f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- garnet/bin/run/main.cc
+++ garnet/bin/run/main.cc
@@ -3,25 +3,20 @@
 // found in the LICENSE file.
 
 #include <fuchsia/sys/cpp/fidl.h>
-#include <fuchsia/sys/index/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
-#include <lib/fdio/fd.h>
 #include <lib/fdio/limits.h>
+#include <lib/fdio/fd.h>
+#include <src/lib/fxl/strings/string_printf.h>
 #include <lib/sys/cpp/file_descriptor.h>
 #include <lib/sys/cpp/service_directory.h>
 #include <lib/sys/cpp/termination_reason.h>
-#include <src/lib/fxl/strings/string_printf.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <zircon/processargs.h>
-#include <zircon/syscalls.h>
-
 #include <utility>
+#include <zircon/syscalls.h>
+#include <zircon/processargs.h>
 
 using fuchsia::sys::TerminationReason;
-using fuchsia::sys::index::ComponentIndex_FuzzySearch_Response;
-using fuchsia::sys::index::ComponentIndex_FuzzySearch_Result;
-using fuchsia::sys::index::ComponentIndexPtr;
 using fxl::StringPrintf;
 
 static void consume_arg(int* argc, const char*** argv) {
@@ -29,36 +24,11 @@ static void consume_arg(int* argc, const char*** argv) {
   ++(*argv);
 }
 
-void launch(fuchsia::sys::LauncherSyncPtr launcher,
-            fuchsia::sys::ComponentControllerPtr controller,
-            fuchsia::sys::LaunchInfo launch_info, async::Loop* loop,
-            bool daemonize) {
-  if (daemonize) {
-    launcher->CreateComponent(std::move(launch_info), {});
-    return;
-  }
-
-  std::string url = launch_info.url;
-  launcher->CreateComponent(std::move(launch_info), controller.NewRequest());
-
-  controller.events().OnTerminated = [&url](
-                                         int64_t return_code,
-                                         TerminationReason termination_reason) {
-    if (termination_reason != TerminationReason::EXITED) {
-      fprintf(stderr, "%s: %s\n", url.c_str(),
-              sys::HumanReadableTerminationReason(termination_reason).c_str());
-    }
-    zx_process_exit(return_code);
-  };
-  loop->Run();
-}
-
 int main(int argc, const char** argv) {
   if (argc < 2) {
     fprintf(stderr, "Usage: run [-d] <program> <args>*\n");
     return 1;
   }
-
   // argv[0] is the program name;
   consume_arg(&argc, &argv);
 
@@ -67,93 +37,42 @@ int main(int argc, const char** argv) {
     daemonize = true;
     consume_arg(&argc, &argv);
   }
-
-  // Perform a fuzzy search on the program name if it is not in URI format.
-  bool fuzzy_search = false;
-  if (std::string(argv[0]).find("://") == std::string::npos) {
-    fuzzy_search = true;
-  }
-
+  fuchsia::sys::LaunchInfo launch_info;
+  launch_info.url = argv[0];
   std::string program_name = argv[0];
   consume_arg(&argc, &argv);
-
-  fuchsia::sys::LaunchInfo launch_info;
-  launch_info.url = program_name;
-  launch_info.out = sys::CloneFileDescriptor(STDOUT_FILENO);
-  launch_info.err = sys::CloneFileDescriptor(STDERR_FILENO);
   while (argc) {
     launch_info.arguments.push_back(*argv);
     consume_arg(&argc, &argv);
   }
-
   async::Loop loop(&kAsyncLoopConfigAttachToThread);
   auto services = sys::ServiceDirectory::CreateFromNamespace();
 
   // Connect to the Launcher service through our static environment.
   fuchsia::sys::LauncherSyncPtr launcher;
   services->Connect(launcher.NewRequest());
-  fuchsia::sys::ComponentControllerPtr controller;
 
-  if (fuzzy_search) {
-    fidl::InterfaceHandle<fuchsia::io::Directory> directory;
-    fuchsia::sys::LaunchInfo index_launch_info;
-    index_launch_info.url =
-        "fuchsia-pkg://fuchsia.com/component_index#meta/component_index.cmx";
-    index_launch_info.directory_request = directory.NewRequest().TakeChannel();
-    launcher->CreateComponent(std::move(index_launch_info),
-                              controller.NewRequest());
-
-    ComponentIndexPtr index;
-    sys::ServiceDirectory index_provider(std::move(directory));
-    index_provider.Connect(index.NewRequest());
-    index.set_error_handler([&program_name](zx_status_t status) {
-      fprintf(
-          stderr,
-          "Error: \"%s\" is not a valid URL. Attempted to match to a URL with "
-          "fuchsia.sys.index.FuzzySearch, but the service is not available\n",
-          program_name.c_str());
-      zx_process_exit(1);
-    });
-
-    index->FuzzySearch(
-        program_name,
-        [&launcher, &controller, &launch_info, &loop, &program_name,
-         &daemonize](ComponentIndex_FuzzySearch_Result result) {
-          if (result.is_err()) {
-            fprintf(stderr,
-                    "Error: \"%s\" contains unsupported characters for fuzzy "
-                    "matching. Valid characters are [A-Z a-z 0-9 / _ - .].\n",
-                    program_name.c_str());
-            zx_process_exit(1);
-          } else {
-            ComponentIndex_FuzzySearch_Response response = result.response();
-            std::vector<std::string> uris = response.uris;
-            if (uris.size() == 0) {
-              fprintf(stderr, "Error: \"%s\" did not match any components.\n",
-                      program_name.c_str());
-              zx_process_exit(1);
-            } else if (uris.size() != 1) {
-              for (auto& uri : uris) {
-                fprintf(stderr, "%s\n", uri.c_str());
-              }
-              fprintf(stderr, "Error: \"%s\" matched multiple components.\n",
-                      program_name.c_str());
-              zx_process_exit(1);
-            } else {
-              fprintf(stdout, "Found %s, executing.\n",
-                      launch_info.url.c_str());
-              std::string matched_name = uris[0];
-              launch_info.url = matched_name;
-              launch(std::move(launcher), std::move(controller),
-                     std::move(launch_info), &loop, daemonize);
-            }
-          }
-        });
-    loop.Run();
-  } else {
-    launch(std::move(launcher), std::move(controller), std::move(launch_info),
-           &loop, daemonize);
+  if (daemonize) {
+    launcher->CreateComponent(std::move(launch_info), {});
+    return 0;
   }
 
+  launch_info.out = sys::CloneFileDescriptor(STDOUT_FILENO);
+  launch_info.err = sys::CloneFileDescriptor(STDERR_FILENO);
+
+  fuchsia::sys::ComponentControllerPtr controller;
+  launcher->CreateComponent(std::move(launch_info), controller.NewRequest());
+
+  controller.events().OnTerminated = [&program_name](
+                                         int64_t return_code,
+                                         TerminationReason termination_reason) {
+    if (termination_reason != TerminationReason::EXITED) {
+      fprintf(stderr, "%s: %s\n", program_name.c_str(),
+              sys::HumanReadableTerminationReason(termination_reason).c_str());
+    }
+    zx_process_exit(return_code);
+  };
+
+  loop.Run();
   return 0;
 }
