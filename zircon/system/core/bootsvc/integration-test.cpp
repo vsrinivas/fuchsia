45874bdeeabcaf26d60bd2bff05025cc57ff5498
Bryan Henry <bryanhenry@google.com>
2019-05-02 19:31:16 -0700

    [bootsvc] Split tests into unit and integration tests
    Test: fx --dir out/x64 build && ./zircon/scripts/run-zircon-x64 -o
    out/x64.zircon -c
    "bootsvc.next=bin/bootsvc-integration-test,testargument"
    Test: run-zircon and then ran 'boot/test/sys/bootsvc-unit-test'
    
    Change-Id: Ica64d67bbd9bdaa95c6500a9c2dd099e4281b2e7

--- zircon/system/core/bootsvc/integration-test.cpp
+++ zircon/system/core/bootsvc/integration-test.cpp
@@ -13,6 +13,7 @@
 #include <fbl/string.h>
 #include <fbl/string_printf.h>
 #include <fbl/unique_fd.h>
+#include <fbl/vector.h>
 #include <fuchsia/boot/c/fidl.h>
 #include <lib/fdio/directory.h>
 #include <lib/fdio/namespace.h>
@@ -85,80 +86,8 @@ bool TestArguments() {
     BEGIN_TEST;
 
     ASSERT_EQ(arguments.size(), 2);
-    EXPECT_STR_EQ(arguments[0].c_str(), "bin/bootsvc-tests");
+    EXPECT_STR_EQ(arguments[0].c_str(), "bin/bootsvc-integration-test");
     EXPECT_STR_EQ(arguments[1].c_str(), "testargument");
-
-    END_TEST;
-}
-
-struct SplitStringTestCase {
-    const char* input;
-    char delimiter;
-};
-
-// Test the SplitString util individually.
-bool TestSplitString() {
-    BEGIN_TEST;
-
-    // Makeshift parametrized test.
-    struct {
-        const char* input;
-        char delimiter;
-    } cases[] = {
-        {.input = "", .delimiter = ','},
-        {.input = "abcd", .delimiter = ','},
-        {.input = "a,b c,d", .delimiter = ','},
-        {.input = "a,b c,d", .delimiter = ' '},
-        {.input = "::a:", .delimiter = ':'},
-    };
-    fbl::Vector<fbl::String> expected[] = {
-        {},
-        {"abcd"},
-        {"a", "b c", "d"},
-        {"a,b", "c,d"},
-        {"", "", "a", ""},
-    };
-    for (size_t n = 0; n < sizeof(cases) / sizeof(*cases); ++n) {
-        fbl::String case_msg = fbl::StringPrintf("Test Case %zu", n);
-        auto result = bootsvc::SplitString(cases[n].input, cases[n].delimiter);
-
-        // We use EXPECT_EQ rather than ASSERT_EQ so that we can continue with
-        // other test cases if one fails.
-        EXPECT_EQ(result.size(), expected[n].size(), case_msg.c_str());
-        if (result.size() == expected[n].size()) {
-            for (size_t i = 0; i < result.size(); ++i) {
-                EXPECT_STR_EQ(result[i].c_str(), expected[n][i].c_str(), case_msg.c_str());
-            }
-        }
-    }
-
-    END_TEST;
-}
-
-// Make sure that we can parse boot args from a configuration string
-bool TestParseBootArgs() {
-    BEGIN_TEST;
-
-    const char config1[] = R"(
-# comment
-key
-key=value
-=value
-)";
-
-    // Parse a valid config.
-    fbl::Vector<char> buf;
-    zx_status_t status = bootsvc::ParseBootArgs(config1, &buf);
-    ASSERT_EQ(ZX_OK, status);
-
-    const char expected[] = "key\0key=value";
-    auto actual = reinterpret_cast<const uint8_t*>(buf.get());
-    ASSERT_BYTES_EQ(reinterpret_cast<const uint8_t*>(expected), actual, buf.size(), "");
-
-    // Parse an invalid config.
-    const char config2[] = "k ey=value";
-    status = bootsvc::ParseBootArgs(config2, &buf);
-    ASSERT_EQ(ZX_ERR_INVALID_ARGS, status);
 
     END_TEST;
 }
@@ -291,8 +220,6 @@ BEGIN_TEST_CASE(bootsvc_integration_tests)
 RUN_TEST(TestLoader)
 RUN_TEST(TestNamespace)
 RUN_TEST(TestArguments)
-RUN_TEST(TestParseBootArgs)
-RUN_TEST(TestSplitString)
 RUN_TEST(TestBootArguments)
 RUN_TEST(TestBootItems)
 RUN_TEST(TestBootRootResource)
---
+++ zircon/system/core/bootsvc/unit-test.cpp
@@ -0,0 +1,92 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fbl/string.h>
+#include <fbl/string_printf.h>
+#include <fbl/vector.h>
+#include <unittest/unittest.h>
+#include <zircon/types.h>
+
+#include "util.h"
+
+namespace {
+
+struct SplitStringTestCase {
+    const char* input;
+    char delimiter;
+};
+
+// Test the SplitString util individually.
+bool TestSplitString() {
+    BEGIN_TEST;
+
+    // Makeshift parametrized test.
+    struct {
+        const char* input;
+        char delimiter;
+    } cases[] = {
+        {.input = "", .delimiter = ','},
+        {.input = "abcd", .delimiter = ','},
+        {.input = "a,b c,d", .delimiter = ','},
+        {.input = "a,b c,d", .delimiter = ' '},
+        {.input = "::a:", .delimiter = ':'},
+    };
+    fbl::Vector<fbl::String> expected[] = {
+        {},
+        {"abcd"},
+        {"a", "b c", "d"},
+        {"a,b", "c,d"},
+        {"", "", "a", ""},
+    };
+    for (size_t n = 0; n < sizeof(cases) / sizeof(*cases); ++n) {
+        fbl::String case_msg = fbl::StringPrintf("Test Case %zu", n);
+        auto result = bootsvc::SplitString(cases[n].input, cases[n].delimiter);
+
+        // We use EXPECT_EQ rather than ASSERT_EQ so that we can continue with
+        // other test cases if one fails.
+        EXPECT_EQ(result.size(), expected[n].size(), case_msg.c_str());
+        if (result.size() == expected[n].size()) {
+            for (size_t i = 0; i < result.size(); ++i) {
+                EXPECT_STR_EQ(result[i].c_str(), expected[n][i].c_str(), case_msg.c_str());
+            }
+        }
+    }
+
+    END_TEST;
+}
+
+// Make sure that we can parse boot args from a configuration string
+bool TestParseBootArgs() {
+    BEGIN_TEST;
+
+    const char config1[] = R"(
+# comment
+key
+key=value
+=value
+)";
+
+    // Parse a valid config.
+    fbl::Vector<char> buf;
+    zx_status_t status = bootsvc::ParseBootArgs(config1, &buf);
+    ASSERT_EQ(ZX_OK, status);
+
+    const char expected[] = "key\0key=value";
+    auto actual = reinterpret_cast<const uint8_t*>(buf.get());
+    ASSERT_BYTES_EQ(reinterpret_cast<const uint8_t*>(expected), actual, buf.size(), "");
+
+    // Parse an invalid config.
+    const char config2[] = "k ey=value";
+    status = bootsvc::ParseBootArgs(config2, &buf);
+    ASSERT_EQ(ZX_ERR_INVALID_ARGS, status);
+
+    END_TEST;
+}
+
+} // namespace
+
+BEGIN_TEST_CASE(bootsvc_unit_tests)
+RUN_TEST(TestParseBootArgs)
+RUN_TEST(TestSplitString)
+END_TEST_CASE(bootsvc_unit_tests)
