ff004c7d1f924541ac71655dcc10ac06fb661812
John Rosasco <rosasco@google.com>
2019-05-10 22:54:16 +0000

    [magma] Integrate IMG VCD into Fuchsia Build
    - Test icd_load verifies that the loader is loading the IMG VCD.
    
    Change-Id: I9ba6bbaa867df1a73860bae524f20e26d8983ec5

---
+++ garnet/lib/magma/tests/icd_load/icd_load.cc
@@ -0,0 +1,57 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "icd_load.h"
+
+#include <cstdint>
+#include <cstdio>
+#include <vector>
+
+#include "magma_util/dlog.h"
+#include "gtest/gtest.h"
+
+#if defined(MAGMA_USE_SHIM)
+#include "vulkan_shim.h"
+#else
+#include <vulkan/vulkan.h>
+#endif
+
+void IcdLoadTest::LoadIcd()
+{
+    // vkEnumerateInstanceExtensionProperties is the chosen entrypoint because
+    // it doesn't require an instance parameter.
+    uint32_t num_extensions = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &num_extensions, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(num_extensions);
+    vkEnumerateInstanceExtensionProperties(nullptr, &num_extensions, extensions.data());
+
+    // The VCD must be loaded for VK_KHR_get_physical_device_properties2
+    // to be available.
+    const char* kRequiredExtension = "VK_KHR_get_physical_device_properties2";
+    bool found_extension = false;
+    for (const auto& extension : extensions) {
+        DLOG("%s\n", extension.extensionName);
+        if (!strcmp(extension.extensionName, kRequiredExtension)) {
+            found_extension = true;
+            break;
+        }
+    }
+    EXPECT_TRUE(found_extension);
+}
+
+TEST(Vulkan, IcdLoad)
+{
+    IcdLoadTest test;
+    test.LoadIcd();
+}
+
+int main(int argc, char** argv)
+{
+#if defined(MAGMA_USE_SHIM)
+    VulkanShimInit();
+#endif
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ garnet/lib/magma/tests/icd_load/icd_load.h
@@ -0,0 +1,15 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GARNET_LIB_MAGMA_TESTS_ICD_LOAD_ICD_LOAD_H_
+#define GARNET_LIB_MAGMA_TESTS_ICD_LOAD_ICD_LOAD_H_
+
+#include "gtest/gtest.h"
+
+class IcdLoadTest {
+public:
+    static void LoadIcd();
+};
+
+#endif // GARNET_LIB_MAGMA_TESTS_ICD_LOAD_ICD_LOAD_H_
