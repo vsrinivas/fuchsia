a5c162275d712c63151cab8f25eecbcfb1719e2a
Aaron Green <aarongreen@google.com>
2019-05-13 21:23:42 +0000

    [fuzzing] Run scripts/fuzzing/test/* as host test.
    This CL extends the host test for host fuzzers to also run the python
    unit tests for 'fx fuzz'.
    
    Bugs: SEC-170
    Test: fx set core.x64 --with //src/fuzzing:tests
          fx run-host-test fuzzing_host_test
    Change-Id: Ic2568e5603b34d35a94ebe85161f476f7854c330

--- src/fuzzing/host_test.go
+++ src/fuzzing/host_test.go
@@ -21,6 +21,28 @@ type FuzzSpec struct {
 	Targets []string `json:"fuzz_targets"`
 }
 
+func TestFxFuzzLib(t *testing.T) {
+	fuchsiaDir, found := os.LookupEnv("FUCHSIA_DIR")
+	if !found {
+		t.SkipNow()
+	}
+	scriptsFuzzingTests := []string{
+		"cipd_test.py",
+		"device_test.py",
+		"fuzzer_test.py",
+		"host_test.py",
+	}
+	for _, test := range scriptsFuzzingTests {
+		pytest := path.Join(fuchsiaDir, "scripts", "fuzzing", "test", test)
+		cmd := exec.Command("python", pytest)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Errorf("%s failed:: %v", test, err)
+		}
+		t.Log(string(output[:]))
+	}
+}
+
 func TestHostFuzzers(t *testing.T) {
 	// Search the build directory structure for host variant fuzzer builds.
 	// This is necessarily sensitive to the layout of the build directory and
