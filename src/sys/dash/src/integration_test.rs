81f728220295eebf6608eef57e1e91da1941ff26
Leo Lung <leolung@google.com>
2019-05-14 20:46:32 +0000

    [shell] dash in a package
    1. Put the zircon dash binary in a fuchsia package.
    1a. Able to run hello_world from dash the binary.
    2. Create a v2 component that launches dash with the above args.
    3. Create an integration test with the same components.
    
    CF-745 #comment
    
    fx set core.chromebook-x64 --with //bundles/buildbot:core --with-base //src/sys/dash
    
    fx run-test dash_test
    
    fx shell /pkgfs/packages/dash/0/bin/sh -c /pkgfs/packages/dash/0/bin/hello_world
    Hippo: Hello World!
    
    fx shell run fuchsia-pkg://fuchsia.com/component_manager#meta/component_manager.cmx fuchsia-pkg://fuchsia.com/dash#meta/dash_hello.cm
    Hippo: Hello World!
    
    Change-Id: I6bb8ea615d96fd3dc6a5f157d91537c942e0348d

---
+++ src/sys/dash/src/integration_test.rs
@@ -0,0 +1,45 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![feature(async_await, await_macro)]
+
+use failure::Error;
+use fidl_fuchsia_sys::FileDescriptor;
+use fuchsia_async as fasync;
+use fuchsia_component::client::{launch_with_options, launcher, LaunchOptions};
+use fuchsia_runtime::HandleType;
+use std::io::{BufRead, BufReader};
+
+#[fasync::run_singlethreaded]
+async fn main() -> Result<(), Error> {
+    let (pipe, socket) = fdio::pipe_half()?;
+    let mut launch_options = LaunchOptions::new();
+    launch_options.set_out(FileDescriptor {
+        type0: HandleType::FileDescriptor as i32,
+        type1: 0,
+        type2: 0,
+        handle0: Some(socket.into()),
+        handle1: None,
+        handle2: None,
+    });
+
+    let launcher = launcher().expect("Failed to open launcher service");
+    let _app = launch_with_options(
+        &launcher,
+        "fuchsia-pkg://fuchsia.com/dash_test#meta/component_manager.cmx".to_string(),
+        Some(vec!["fuchsia-pkg://fuchsia.com/dash_test#meta/dash_hello.cm".to_string()]),
+        launch_options,
+    )
+    .expect("Failed to launch component_manager with dash_hello");
+
+    let mut reader = BufReader::new(pipe);
+
+    // Assert component manager as a v1 component was able to launch dash as a v2 component was
+    // able to launch hello world as a binary.
+    let mut line = String::new();
+    reader.read_line(&mut line)?;
+    assert_eq!(line, "Hippo: Hello World!\n");
+
+    Ok(())
+}
