cb26f7ce275ba1d0c9d6e8dbf1411ba9b9e05b67
Leo Lung <leolung@google.com>
2019-05-14 19:03:55 +0000

    [elf_runner] Test argv0 when there are no args
    Change-Id: I085bec02d482cdcfc17c10e790cd2f81cdb7a1f1

--- src/sys/component_manager/tests/elf_runner_test.rs
+++ src/sys/component_manager/tests/elf_runner_test.rs
@@ -12,6 +12,17 @@ use {
 };
 
 fn main() {
+    test(
+        "fuchsia-pkg://fuchsia.com/elf_runner_test#meta/echo_args.cm".to_string(),
+        "/pkg/bin/echo_args Hippos rule!\n".to_string(),
+    );
+    test(
+        "fuchsia-pkg://fuchsia.com/elf_runner_test#meta/echo_no_args.cm".to_string(),
+        "/pkg/bin/echo_args\n".to_string(),
+    );
+}
+
+fn test(url: String, expected_output: String) {
     let mut _executor = fasync::Executor::new().expect("error creating executor");
     let launcher = client::launcher().expect("failed to connect to launcher");
     let (pipe_handle, socket_handle) = fdio::pipe_half().expect("failed to create pipe");
@@ -28,13 +39,13 @@ fn main() {
     let _app = client::launch_with_options(
         &launcher,
         "fuchsia-pkg://fuchsia.com/elf_runner_test#meta/component_manager.cmx".to_string(),
-        Some(vec!["fuchsia-pkg://fuchsia.com/elf_runner_test#meta/echo_args.cm".to_string()]),
+        Some(vec![url]),
         options,
     )
     .expect("component manager failed to launch");
 
     let mut reader = BufReader::new(pipe_handle);
     let mut line = String::new();
-    reader.read_line(&mut line).expect("failed to read \"Hippos rule!\\n\"");
-    assert_eq!(line, "/pkg/bin/echo_args Hippos rule!\n");
+    reader.read_line(&mut line).expect("failed to read echo_args output");
+    assert_eq!(line, expected_output);
 }
