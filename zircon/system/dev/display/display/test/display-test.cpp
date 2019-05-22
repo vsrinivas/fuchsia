c4881444dbc9115ec29baea0ab48a13411a71b3d
Payam Moradshahi <payamm@google.com>
2019-05-22 03:50:04 +0000

    [display][test] Add support for display unit testing
    This CL simply adds the files needed for display unit testing without
    adding any actual tests.
    
    ZX-3810
    
    Test: compiled using buidall. Ran display-core-unittests on QEMU. Also
    tested on CLEO and ran display-test to make sure this change did not
    break existing functionality.
    
    Change-Id: I2a8a44059c492b271b05cf86d121583b2c9d5866

---
+++ zircon/system/dev/display/display/test/display-test.cpp
@@ -0,0 +1,14 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <zxtest/zxtest.h>
+
+namespace display {
+
+
+TEST(DispTest, NoOpTest) {
+    EXPECT_OK(ZX_OK);
+}
+
+} // namespace display
