28cd5858de4934784a90c4d65a65506af01f6746
Scott Graham <scottmg@google.com>
2019-05-22 17:07:51 +0000

    [logging] Use PLOG variant in gfx/resources/snapshot/snapshotter.cc
    This version logs the string version of the zx_status_t, rather than
    only the decimal equivalent.
    
    ZX-4110 #comment [logging] Use PLOG variant in gfx/resources/snapshot/snapshotter.cc
    
    Test: CQ
    Change-Id: Ic7f9186065d5edd6f5311e913f7193375c1d4aac

--- garnet/lib/ui/gfx/resources/snapshot/snapshotter.cc
+++ garnet/lib/ui/gfx/resources/snapshot/snapshotter.cc
@@ -51,24 +51,24 @@ bool VmoFromBytes(const uint8_t* bytes, size_t num_bytes, uint32_t type,
   size_t total_size = num_bytes + sizeof(type) + sizeof(version);
   zx_status_t status = zx::vmo::create(total_size, 0u, &vmo);
   if (status < 0) {
-    FXL_LOG(WARNING) << "zx::vmo::create failed: " << status;
+    FXL_PLOG(WARNING, status) << "zx::vmo::create failed";
     return false;
   }
 
   if (num_bytes > 0) {
     status = vmo.write(&type, 0, sizeof(uint32_t));
     if (status < 0) {
-      FXL_LOG(WARNING) << "zx::vmo::write snapshot type failed: " << status;
+      FXL_PLOG(WARNING, status) << "zx::vmo::write snapshot type failed";
       return false;
     }
     status = vmo.write(&version, sizeof(uint32_t), sizeof(uint32_t));
     if (status < 0) {
-      FXL_LOG(WARNING) << "zx::vmo::write snapshot version failed: " << status;
+      FXL_PLOG(WARNING, status) << "zx::vmo::write snapshot version failed";
       return false;
     }
     status = vmo.write(bytes, 2 * sizeof(uint32_t), num_bytes);
     if (status < 0) {
-      FXL_LOG(WARNING) << "zx::vmo::write bytes failed: " << status;
+      FXL_PLOG(WARNING, status) << "zx::vmo::write bytes failed";
       return false;
     }
   }
