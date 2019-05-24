2b8d306d70c335b98befe99e778d7f3f5c5d535c
Chris Fontas <cfontas@google.com>
2019-05-24 20:25:42 +0000

    Revert "[escher] Rename paper_renderer.cc to default_paper_renderer.cc"
    This reverts commit b596570e78a27269f9dc51321e54a4f5a4e4dc88.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [escher] Rename paper_renderer.cc to default_paper_renderer.cc
    > 
    > Upcoming CLs will refactor PaperRenderer to make a base-class
    > that is more amenable to testing.  Moving this now will allow
    > Gerrit diffs to be more intelligible.
    > 
    > Tests: CQ.  There are no behavioral changes.
    > 
    > Change-Id: I1d2e04509cdeecf18d63ec065754b4e0dec79064
    
    TBR=abarth@google.com,jjosh@google.com,jaeheon@google.com,cfontas@google.com
    
    Change-Id: I38af747eec601819130a581e9b8f92ff940dbdd6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- src/ui/lib/escher/paper/paper_renderer.cc
+++ src/ui/lib/escher/paper/paper_renderer.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/ui/lib/escher/paper/default_paper_renderer.h"
+#include "src/ui/lib/escher/paper/paper_renderer.h"
 
 #include <glm/gtc/matrix_access.hpp>
 
