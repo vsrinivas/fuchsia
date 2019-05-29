598d573b5331c7239bd99279a6f4726adb720c91
Zac Bowling <zbowling@google.com>
2019-05-29 20:31:26 +0000

    [bt][avrcp] Implement repl and avc key command
    Add the REPL to the test cmdline too and implement support for sending
    AVC commands.
    
    To use:
          start a2dp-sink service manually
          run bt-avrcp-controller to start avrcp service
          make discoverable with bt-cli
          pair phone
          run bt-avrcp-controller <device_id>
          avrcp> key play
    
    Test: fx run-test bt-avrcp-controller-tests
    
    Change-Id: I1db94dc96d3b1b8b5212635a6a51adc0ae28ff5e

---
+++ src/connectivity/bluetooth/tools/bt-avrcp-controller/src/commands.rs
@@ -0,0 +1,293 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    fidl_fuchsia_bluetooth_avrcp::AvcPanelCommand,
+    rustyline::{
+        completion::Completer, error::ReadlineError, highlight::Highlighter, hint::Hinter, Helper,
+    },
+    std::{
+        borrow::Cow::{self, Borrowed, Owned},
+        fmt,
+        str::FromStr,
+    },
+};
+
+macro_rules! gen_avc_commands {
+($name:ident {
+    $($variant:ident = ($long:expr, $short:expr)),*,
+}) => {
+
+        static AVC_COMMAND_LONG_VARIANTS: &[&str] = &[$($long,)*];
+        static AVC_COMMAND_SHORT_VARIANTS: &[&str] = &[$($short,)*];
+
+        pub fn avc_match_string(s: &str) -> Option<AvcPanelCommand> {
+            let lc = s.to_lowercase();
+            match lc.as_str() {
+                $($short => return Some(AvcPanelCommand::$variant)),*,
+                _ => {}
+            }
+            match lc.as_str() {
+                $($long => Some(AvcPanelCommand::$variant)),*,
+                _ => None
+            }
+        }
+    }
+}
+
+/// Macro to generate a command enum and its impl.
+macro_rules! gen_commands {
+    ($name:ident {
+        $($variant:ident = ($val:expr, [$($arg:expr),*], $help:expr)),*,
+    }) => {
+        /// Enum of all possible commands
+        #[derive(PartialEq)]
+        pub enum $name {
+            $($variant),*
+        }
+
+        impl $name {
+            /// Returns a list of the string representations of all variants
+            pub fn variants() -> Vec<String> {
+                let mut variants = Vec::new();
+                $(variants.push($val.to_string());)*
+                variants
+            }
+
+            pub fn arguments(&self) -> &'static str {
+                match self {
+                    $(
+                        $name::$variant => concat!($("<", $arg, "> ",)*)
+                    ),*
+                }
+            }
+
+            /// Help string for a given variant. The format is "command <arg>.. -- help message"
+            pub fn cmd_help(&self) -> &'static str {
+                match self {
+                    $(
+                        $name::$variant => concat!($val, " ", $("<", $arg, "> ",)* "-- ", $help)
+                    ),*
+                }
+            }
+
+            /// Multiline help string for `$name` including usage of all variants.
+            pub fn help_msg() -> String {
+                let command_help = concat!($(
+                    "\t", $val, " ", $("<", $arg, "> ",)* "-- ", $help, "\n"
+                ),*);
+
+                format!("Commands:\n{}\nAVC keys:\n\tkey {}\n", command_help, AVC_COMMAND_LONG_VARIANTS.join("\n\tkey "))
+            }
+
+        }
+
+        impl fmt::Display for $name {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match *self {
+                    $($name::$variant => write!(f, $val)),* ,
+                }
+            }
+        }
+
+        impl FromStr for $name {
+            type Err = ();
+
+            fn from_str(s: &str) -> Result<$name, ()> {
+                match s {
+                    $($val => Ok($name::$variant)),* ,
+                    _ => Err(()),
+                }
+            }
+        }
+
+    }
+}
+
+gen_avc_commands! {
+    AvcCommand {
+        Select = ("select", "sel"),
+        Up = ("up", "up"),
+        Down = ("down", "do"),
+        Left = ("left", "le"),
+        Right = ("right", "ri"),
+        RootMenu = ("rootmenu", "rm"),
+        ContentsMenu = ("contentsmenu", "cm"),
+        FavoriteMenu = ("favoritemenu", "fm"),
+        Exit = ("exit", "ex"),
+        OnDemandMenu = ("ondemandmenu", "om"),
+        AppsMenu = ("appsmenu", "am"),
+        Key0 = ("key0", "0"),
+        Key1 = ("key1", "1"),
+        Key2 = ("key2", "2"),
+        Key3 = ("key3", "3"),
+        Key4 = ("key4", "4"),
+        Key5 = ("key5", "5"),
+        Key6 = ("key6", "6"),
+        Key7 = ("key7", "7"),
+        Key8 = ("key8", "8"),
+        Key9 = ("key9", "9"),
+        Dot = ("dot", "."),
+        Enter = ("enter", "en"),
+        ChannelUp = ("channelup", "cu"),
+        ChannelDown = ("channeldown", "cd"),
+        ChannelPrevious = ("channelprevious", "cp"),
+        InputSelect = ("inputselect", "ip"),
+        Info = ("info", "in"),
+        Help = ("help", "he"),
+        PageUp = ("pageup", "pu"),
+        PageDown = ("pagedown", "pd"),
+        Lock = ("lock", "lo"),
+        Power = ("power", "po"),
+        VolumeUp = ("volumeup", "vu"),
+        VolumeDown = ("volumedown", "vd"),
+        Mute = ("mute", "m"),
+        Play = ("play", "pl"),
+        Stop = ("stop", "s"),
+        Pause = ("pause", "pa"),
+        Record = ("record", "rec"),
+        Rewind = ("rewind", "rew"),
+        FastForward = ("fastforward", "ff"),
+        Eject = ("eject", "ej"),
+        Forward = ("forward", "fw"),
+        Backward = ("backward", "bw"),
+        List = ("list", "l"),
+        F1 = ("f1","f1"),
+        F2 = ("f2","f2"),
+        F3 = ("f3","f3"),
+        F4 = ("f4","f4"),
+        F5 = ("f5","f5"),
+        F6 = ("f6","f6"),
+        F7 = ("f7","f7"),
+        F8 = ("f8","f8"),
+        F9 = ("f9","f9"),
+        Red = ("red", "re"),
+        Green = ("green", "gr"),
+        Blue = ("blue", "bl"),
+        Yellow = ("yellow", "ye"),
+    }
+}
+
+// `Cmd` is the declarative specification of all commands that bt-cli accepts.
+gen_commands! {
+    Cmd {
+        AvcCommand = ("key", ["command"], "send an AVC passthrough keypress event"),
+        GetMediaAttributes = ("get-media", [], "gets currently playing media attributes"),
+        Help = ("help", [], "This message"),
+        Exit = ("exit", [], "Close REPL"),
+        Quit = ("quit", [], "Close REPL"),
+    }
+}
+
+/// CmdHelper provides completion, hints, and highlighting for bt-cli
+pub struct CmdHelper {}
+
+impl CmdHelper {
+    pub fn new() -> CmdHelper {
+        CmdHelper {}
+    }
+}
+
+impl Completer for CmdHelper {
+    type Candidate = String;
+
+    fn complete(&self, line: &str, _pos: usize) -> Result<(usize, Vec<String>), ReadlineError> {
+        let components: Vec<_> = line.trim_start().split_whitespace().collect();
+
+        // Check whether we have entered a command and either whitespace or a partial argument.
+        // If yes, complete arguments; if no, complete commands
+        let mut variants = Vec::new();
+        // Check whether we have entered a command and either whitespace or a partial argument.
+        // If yes, complete arguments; if no, complete commands
+        let should_complete_arguments = (components.len() == 1 && line.ends_with(" "))
+            || (components.len() == 2 && !line.ends_with(" "));
+        if should_complete_arguments {
+            let command = components[0].trim();
+            let partial_argument = components.get(1).unwrap_or(&"");
+            let mut candidates = vec![];
+            if command == "key" {
+                // connect and device have 'id|addr' arguments
+                // can match against remote device identifier or address
+                for key in AVC_COMMAND_LONG_VARIANTS {
+                    if key.starts_with(partial_argument) {
+                        candidates.push(format!("{} {}", command, key));
+                    }
+                }
+                for key in AVC_COMMAND_SHORT_VARIANTS {
+                    if key.starts_with(partial_argument) {
+                        candidates.push(format!("{} {}", command, key));
+                    }
+                }
+            }
+            Ok((0, candidates))
+        } else {
+            for variant in Cmd::variants() {
+                if variant.starts_with(line) {
+                    variants.push(variant)
+                }
+            }
+            Ok((0, variants))
+        }
+    }
+}
+
+impl Hinter for CmdHelper {
+    /// CmdHelper provides hints for commands with arguments
+    fn hint(&self, line: &str, _pos: usize) -> Option<String> {
+        let needs_space = !line.ends_with(" ");
+        line.trim()
+            .parse::<Cmd>()
+            .map(|cmd| {
+                format!("{}{}", if needs_space { " " } else { "" }, cmd.arguments().to_string(),)
+            })
+            .ok()
+    }
+}
+
+impl Highlighter for CmdHelper {
+    /// CmdHelper provides highlights for commands with hints
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        if hint.trim().is_empty() {
+            Borrowed(hint)
+        } else {
+            Owned(format!("\x1b[90m{}\x1b[0m", hint))
+        }
+    }
+}
+
+/// CmdHelper can be used as an `Editor` helper for entering input commands
+impl Helper for CmdHelper {}
+
+/// Represents either continuation or breaking out of a read-evaluate-print loop.
+pub enum ReplControl {
+    Break,
+    Continue,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_avc_match_string() {
+        assert_eq!(Some(AvcPanelCommand::Up), avc_match_string("up"));
+        assert_eq!(Some(AvcPanelCommand::Up), avc_match_string("UP"));
+        assert_eq!(None, avc_match_string("UPS"));
+        assert_eq!(Some(AvcPanelCommand::Play), avc_match_string("play"));
+        assert_eq!(Some(AvcPanelCommand::Play), avc_match_string("pl"));
+        assert_eq!(None, avc_match_string("p"));
+        assert_eq!(None, avc_match_string(""));
+        assert_eq!(None, avc_match_string("12"));
+        assert_eq!(Some(AvcPanelCommand::Dot), avc_match_string("."));
+        assert_eq!(Some(AvcPanelCommand::ChannelUp), avc_match_string("channelup"));
+    }
+
+    #[test]
+    fn test_completer() {
+        let cmdhelper = CmdHelper::new();
+        assert!(cmdhelper.complete("ke", 0).unwrap().1.contains(&"key".to_string()));
+        assert!(cmdhelper.complete("get", 0).unwrap().1.contains(&"get-media".to_string()));
+        assert!(cmdhelper.complete("key ex", 0).unwrap().1.contains(&"key exit".to_string()));
+    }
+}
--- src/connectivity/bluetooth/tools/bt-avrcp-controller/src/main.rs
+++ src/connectivity/bluetooth/tools/bt-avrcp-controller/src/main.rs
@@ -2,20 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![feature(async_await, await_macro)]
+#![feature(async_await, await_macro, mpsc_select)]
 
 use {
     failure::{Error, ResultExt},
     fidl::endpoints::create_endpoints,
     fidl_fuchsia_bluetooth_avrcp::{
-        AvrcpMarker, ControllerEvent, ControllerEventStream, ControllerMarker,
+        AvrcpMarker, ControllerEvent, ControllerEventStream, ControllerMarker, ControllerProxy,
     },
     fuchsia_async as fasync,
+    fuchsia_bluetooth::types::Status,
     fuchsia_component::client::connect_to_service,
-    futures::{select, FutureExt, TryStreamExt},
+    futures::{
+        channel::mpsc::{channel, SendError},
+        select, FutureExt, Sink, SinkExt, Stream, StreamExt, TryStreamExt,
+    },
     pin_utils::pin_mut,
+    rustyline::{error::ReadlineError, CompletionType, Config, EditMode, Editor},
+    std::thread,
     structopt::StructOpt,
 };
+
+use crate::commands::{avc_match_string, Cmd, CmdHelper, ReplControl};
+
+mod commands;
+
+static PROMPT: &str = "\x1b[34mavrcp>\x1b[0m ";
+/// Escape code to clear the pty line on which the cursor is located.
+/// Used when evented output is intermingled with the REPL prompt.
+static CLEAR_LINE: &str = "\x1b[2K";
 
 /// Define the command line arguments that the tool accepts.
 #[derive(StructOpt)]
@@ -29,8 +44,114 @@ struct Opt {
     device: String,
 }
 
+async fn send_passthrough<'a>(
+    args: &'a [&'a str],
+    controller: &'a ControllerProxy,
+) -> Result<String, Error> {
+    if args.len() != 1 {
+        return Ok(format!("usage: {}", Cmd::AvcCommand.cmd_help()));
+    }
+    let cmd = avc_match_string(args[0]);
+    if cmd.is_none() {
+        return Ok(String::from("invalid avc command"));
+    }
+
+    // `args[0]` is the identifier of the remote device to connect to
+    let response = await!(controller.send_command(cmd.unwrap()))?;
+    if response.error.is_some() {
+        Ok(Status::from(response).to_string())
+    } else {
+        Ok(String::new())
+    }
+}
+
+async fn get_media<'a>(
+    _args: &'a [&'a str],
+    controller: &'a ControllerProxy,
+) -> Result<String, Error> {
+    let (response, media) = await!(controller.get_media_attributes())?;
+    if response.error.is_some() {
+        Ok(Status::from(response).to_string())
+    } else {
+        Ok(format!("Media attributes: {:#?}", media))
+    }
+}
+
+/// Handle a single raw input command from a user and indicate whether the command should
+/// result in continuation or breaking of the read evaluate print loop.
+async fn handle_cmd(controller: &ControllerProxy, line: String) -> Result<ReplControl, Error> {
+    let components: Vec<_> = line.trim().split_whitespace().collect();
+    if let Some((raw_cmd, args)) = components.split_first() {
+        let cmd = raw_cmd.parse();
+        let res = match cmd {
+            Ok(Cmd::AvcCommand) => await!(send_passthrough(args, &controller)),
+            Ok(Cmd::GetMediaAttributes) => await!(get_media(args, &controller)),
+            Ok(Cmd::Help) => Ok(Cmd::help_msg().to_string()),
+            Ok(Cmd::Exit) | Ok(Cmd::Quit) => return Ok(ReplControl::Break),
+            Err(_) => Ok(format!("\"{}\" is not a valid command", raw_cmd)),
+        }?;
+        if res != "" {
+            println!("{}", res);
+        }
+    }
+
+    Ok(ReplControl::Continue)
+}
+
+/// Generates a rustyline `Editor` in a separate thread to manage user input. This input is returned
+/// as a `Stream` of lines entered by the user.
+///
+/// The thread exits and the `Stream` is exhausted when an error occurs on stdin or the user
+/// sends a ctrl-c or ctrl-d sequence.
+///
+/// Because rustyline shares control over output to the screen with other parts of the system, a
+/// `Sink` is passed to the caller to send acknowledgements that a command has been processed and
+/// that rustyline should handle the next line of input.
+fn cmd_stream() -> (impl Stream<Item = String>, impl Sink<(), SinkError = SendError>) {
+    // Editor thread and command processing thread must be synchronized so that output
+    // is printed in the correct order.
+    let (mut cmd_sender, cmd_receiver) = channel(512);
+    let (ack_sender, mut ack_receiver) = channel(512);
+
+    thread::spawn(move || -> Result<(), Error> {
+        let mut exec = fasync::Executor::new().context("error creating readline event loop")?;
+
+        let fut = async {
+            let config = Config::builder()
+                .auto_add_history(true)
+                .history_ignore_space(true)
+                .completion_type(CompletionType::List)
+                .edit_mode(EditMode::Emacs)
+                .build();
+            let mut rl: Editor<CmdHelper> = Editor::with_config(config);
+            rl.set_helper(Some(CmdHelper::new()));
+            loop {
+                let readline = rl.readline(PROMPT);
+                match readline {
+                    Ok(line) => {
+                        cmd_sender.try_send(line)?;
+                    }
+                    Err(ReadlineError::Eof) | Err(ReadlineError::Interrupted) => {
+                        return Ok(());
+                    }
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                        return Err(e.into());
+                    }
+                }
+                // wait until processing thread is finished evaluating the last command
+                // before running the next loop in the repl
+                await!(ack_receiver.next());
+            }
+        };
+        exec.run_singlethreaded(fut)
+    });
+    (cmd_receiver, ack_sender)
+}
+
 async fn controller_listener(mut stream: ControllerEventStream) -> Result<(), Error> {
     while let Some(evt) = await!(stream.try_next())? {
+        print!("{}", CLEAR_LINE);
         match evt {
             ControllerEvent::PlaybackStatusChanged { status } => {
                 println!("Event: PlaybackStatusChanged(status = {:?})", status);
@@ -70,30 +191,67 @@ async fn controller_listener(mut stream: ControllerEventStream) -> Result<(), Er
     Ok(())
 }
 
+/// REPL execution
+async fn run_repl(controller: ControllerProxy) -> Result<(), Error> {
+    // `cmd_stream` blocks on input in a separate thread and passes commands and acks back to
+    // the main thread via async channels.
+    let (mut commands, mut acks) = cmd_stream();
+    loop {
+        if let Some(cmd) = await!(commands.next()) {
+            match await!(handle_cmd(&controller, cmd)) {
+                Ok(ReplControl::Continue) => {}
+                Ok(ReplControl::Break) => {
+                    println!("\n");
+                    break;
+                }
+                Err(e) => {
+                    println!("Error handling command: {}", e);
+                }
+            }
+        } else {
+            break;
+        }
+        await!(acks.send(()))?;
+    }
+    Ok(())
+}
+
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), Error> {
     let opt = Opt::from_args();
-    eprintln!("Connecting to \"{device}\" AVRCP remote target service.", device = opt.device);
+
+    let device_id = &opt.device.replace("-", "").to_lowercase();
 
     let avrcp_svc = connect_to_service::<AvrcpMarker>()
-        .context("failed to connect to Bluetooth AVRCP interface")?;
+        .context("Failed to connect to Bluetooth AVRCP interface")?;
 
     // Create a channel for our Request<Controller> to live
     let (c_client, c_server) =
-        create_endpoints::<ControllerMarker>().expect("error creating Controller endpoint");
+        create_endpoints::<ControllerMarker>().expect("Error creating Controller endpoint");
 
-    await!(avrcp_svc.get_controller_for_target(opt.device.as_str(), c_server))?;
+    let status = await!(avrcp_svc.get_controller_for_target(&device_id.as_str(), c_server))?;
+    eprintln!(
+        "Controller obtained to device \"{device}\" AVRCP remote target service {status:?}",
+        device = &device_id,
+        status = status
+    );
 
     let controller = c_client.into_proxy().expect("error obtaining controller client proxy");
 
     let evt_stream = controller.clone().take_event_stream();
     let event_fut = controller_listener(evt_stream).fuse();
-
+    let repl_fut = run_repl(controller).fuse();
     pin_mut!(event_fut);
+    pin_mut!(repl_fut);
+
     // These futures should only return when something fails.
     select! {
-        _ = event_fut => (),
-        // TODO: add repl future
+        result = event_fut => {
+            eprintln!(
+                "Service connection returned {status:?}", status = result);
+        },
+        _ = repl_fut => {}
     }
+
     Ok(())
 }
