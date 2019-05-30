cdc6d19d299937d0b40d84ae7ebbd828d85e18d6
Kevin Lindkvist <lindkvist@google.com>
2019-05-30 01:04:21 +0000

    [modular_test_harness] Add FakeAgent
    This FakeAgent contains all the boilerplate required to expose services
    to connecting clients, as well as exposing the Agent interface to
    modular. It also adds a convenience callback for when tasks are
    triggered.
    
    Change-Id: I51e4bea6e13419e5b69fb3c1051321059b8ab751

---
+++ peridot/public/lib/modular_test_harness/cpp/fake_agent.cc
@@ -0,0 +1,47 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/modular_test_harness/cpp/fake_agent.h>
+
+namespace modular {
+namespace testing {
+
+FakeAgent::FakeAgent() = default;
+FakeAgent::~FakeAgent() = default;
+
+// |modular::testing::FakeComponent|
+void FakeAgent::OnCreate(fuchsia::sys::StartupInfo startup_info) {
+  component_context()->svc()->Connect(component_context_.NewRequest());
+  component_context()->svc()->Connect(agent_context_.NewRequest());
+
+  component_context()->outgoing()->AddPublicService<fuchsia::modular::Agent>(
+      bindings_.GetHandler(this));
+}
+
+void FakeAgent::set_on_run_task(
+    fit::function<void(std::string task_id, RunTaskCallback callback)>
+        on_run_task) {
+  on_run_task_ = std::move(on_run_task);
+}
+
+// |fuchsia::modular::Agent|
+void FakeAgent::Connect(
+    std::string requestor_url,
+    fidl::InterfaceRequest<fuchsia::sys::ServiceProvider> services_request) {
+  services_.AddBinding(std::move(services_request));
+}
+
+// |fuchsia::modular::Agent|
+void FakeAgent::RunTask(std::string task_id, RunTaskCallback callback) {
+  if (on_run_task_) {
+    on_run_task_(task_id, std::move(callback));
+  }
+}
+
+std::vector<std::string> FakeAgent::GetSandboxServices() {
+  return {"fuchsia.modular.ComponentContext", "fuchsia.modular.AgentContext"};
+}
+
+}  // namespace testing
+}  // namespace modular
---
+++ peridot/public/lib/modular_test_harness/cpp/fake_agent.h
@@ -0,0 +1,79 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_MODULAR_TEST_HARNESS_CPP_FAKE_AGENT_H_
+#define LIB_MODULAR_TEST_HARNESS_CPP_FAKE_AGENT_H_
+
+#include <fuchsia/modular/cpp/fidl.h>
+#include <fuchsia/modular/testing/cpp/fidl.h>
+#include <lib/modular_test_harness/cpp/fake_component.h>
+#include <lib/svc/cpp/service_namespace.h>
+#include <src/lib/fxl/logging.h>
+
+namespace modular {
+namespace testing {
+
+// An Agent implementation which provides access to
+// |fuchsia::modular::AgentContext|, implements boilerplate for exposing
+// services, and exposes task running callback registration.
+class FakeAgent : public modular::testing::FakeComponent,
+                  fuchsia::modular::Agent {
+  // Returns a vector of services names which are expected to be provided to the
+  // agent.
+  static std::vector<std::string> GetSandboxServices();
+
+  FakeAgent();
+  ~FakeAgent();
+
+  // Returns the agent's |fuchsia::modular::ComponentContext|.
+  fuchsia::modular::ComponentContext* modular_component_context() {
+    return component_context_.get();
+  }
+
+  // Returns the agent's |fuchsia::modular::AgentContext|.
+  fuchsia::modular::AgentContext* agent_context() {
+    return agent_context_.get();
+  }
+
+  // Adds a service to the service namespace which is exposed to clients
+  // connecting to the agent.
+  template <typename ServiceType>
+  void AddService(
+      fidl::InterfaceRequestHandler<ServiceType> connection_handler) {
+    services_.AddService<ServiceType>(std::move(connection_handler));
+  }
+
+  // Sets a callback which will be called when a
+  // |fuchsia::modular::modular::Agent/RunTask| is received.
+  void set_on_run_task(
+      fit::function<void(std::string task_id, RunTaskCallback callback)>
+          on_run_task);
+
+ private:
+  // |FakeComponent|
+  void OnCreate(fuchsia::sys::StartupInfo startup_info) override;
+
+  // |fuchsia::modular::Agent|
+  void Connect(std::string requestor_url,
+               fidl::InterfaceRequest<fuchsia::sys::ServiceProvider>
+                   services_request) override;
+
+  // |fuchsia::modular::Agent|
+  void RunTask(std::string task_id, RunTaskCallback callback) override;
+
+  fit::function<void(std::string task_id, RunTaskCallback callback)>
+      on_run_task_;
+
+  fuchsia::modular::ComponentContextPtr component_context_;
+  fuchsia::modular::AgentContextPtr agent_context_;
+
+  fidl::BindingSet<fuchsia::modular::Agent> bindings_;
+
+  component::ServiceNamespace services_;
+};
+
+}  // namespace testing
+}  // namespace modular
+
+#endif  // LIB_MODULAR_TEST_HARNESS_CPP_FAKE_AGENT_H_
