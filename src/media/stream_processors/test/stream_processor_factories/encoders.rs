818d2f184798c4193281016e9fa66a9520a5059b
Payton Turnage <turnage@google.com>
2019-06-04 01:01:56 +0000

    [mediacodec] Define encoder factory for tests
    This will be used in encoder tests.
    
    TEST: no behaviour change
    Change-Id: I329bf72dcaa9b624b30767c9dbab87a9657efe7e

---
+++ src/media/stream_processors/test/stream_processor_factories/encoders.rs
@@ -0,0 +1,40 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![feature(async_await)]
+
+use fidl::endpoints::*;
+use fidl_fuchsia_media::*;
+use fidl_fuchsia_mediacodec::*;
+use fuchsia_component::client;
+use futures::{
+    future::{self, BoxFuture},
+    FutureExt,
+};
+use stream_processor_test::*;
+
+pub struct EncoderFactory;
+
+impl StreamProcessorFactory for EncoderFactory {
+    fn connect_to_stream_processor(
+        &self,
+        stream: &ElementaryStream,
+        format_details_version_ordinal: u64,
+    ) -> BoxFuture<Result<StreamProcessorProxy>> {
+        let get_encoder = || {
+            let factory = client::connect_to_service::<CodecFactoryMarker>()?;
+            let (encoder_client_end, encoder_request) = create_endpoints()?;
+            let encoder = encoder_client_end.into_proxy()?;
+            factory.create_encoder(
+                CreateEncoderParams {
+                    input_details: Some(stream.format_details(format_details_version_ordinal)),
+                    require_hw: Some(false),
+                },
+                encoder_request,
+            )?;
+            Ok(encoder)
+        };
+        future::ready(get_encoder()).boxed()
+    }
+}
