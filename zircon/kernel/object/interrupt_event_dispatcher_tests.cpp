7eddecad5b16847003be228e7fa9f7a5271e5f4c
Adrian Danis <adanis@google.com>
2019-06-06 03:42:53 +0000

    [kernel][object] Fix interrupt dispatcher test
    This fixes an underlying functional issue in the test where the created
    InterruptEventDispatcher was not being correctly shut down due to
    moving the dispatcher out of the KernelHandle, causing on_zero_handles
    to not get called. The underlying dispatcher is actually not needed so
    we simply avoid releasing it from the KernelHandle.
    
    Also fixes some stylistic issues of the thread signaling with an
    explicit shared atomic instead of abusing thread_kill.
    
    ZX-4305 #done
    
    Change-Id: I104f322c5365fdcad5d2ee556c0536f6c1ba98f4

--- zircon/kernel/object/interrupt_event_dispatcher_tests.cpp
+++ zircon/kernel/object/interrupt_event_dispatcher_tests.cpp
@@ -7,6 +7,7 @@
 #if ARCH_X86
 #include <arch/x86/apic.h>
 #endif
+#include <ktl/atomic.h>
 #include <lib/unittest/unittest.h>
 #include <object/interrupt_dispatcher.h>
 #include <object/interrupt_event_dispatcher.h>
@@ -23,13 +24,13 @@ static bool TestConcurrentIntEventDispatcherTeardown() {
     // Generating the interrupt events for this test is necessarily arch specific and is only
     // implemented for x86 here.
 #if ARCH_X86
-    KernelHandle<InterruptDispatcher> interrupt_handle;
+    KernelHandle<InterruptDispatcher> interrupt;
     zx_rights_t rights;
 
     uint32_t gsi;
     constexpr uint32_t gsi_search_max = 24;
     for (gsi = 0; gsi < gsi_search_max; gsi++) {
-        zx_status_t status = InterruptEventDispatcher::Create(&interrupt_handle, &rights, gsi,
+        zx_status_t status = InterruptEventDispatcher::Create(&interrupt, &rights, gsi,
                                                               ZX_INTERRUPT_MODE_DEFAULT);
         if (status == ZX_OK) {
             break;
@@ -37,32 +38,32 @@ static bool TestConcurrentIntEventDispatcherTeardown() {
     }
     ASSERT_NE(gsi, gsi_search_max, "Failed to find free global interrupt");
 
-    fbl::RefPtr<InterruptDispatcher> interrupt = interrupt_handle.release();
-
-    // Find the local vector
-    uint8_t vector = apic_io_fetch_irq_vector(gsi);
+    // Find the local vector, put it in the low byte of our shared state.
+    ktl::atomic<uint16_t> state = apic_io_fetch_irq_vector(gsi);
 
     // Spin up a thread to generate the interrupt. As IPIs cannot be masked this causes the
     // associated InterruptDispatcher handler to constantly get invoked, which is what we want.
     thread_t* int_thread = thread_create(
         "int",
         [](void* arg) -> int {
-            uint8_t* vec = static_cast<uint8_t*>(arg);
-            while (1) {
-                apic_send_self_ipi(*vec, DELIVERY_MODE_FIXED);
+            ktl::atomic<uint16_t>* state = static_cast<ktl::atomic<uint16_t>*>(arg);
+            uint8_t vec = state->load(ktl::memory_order_seq_cst) & 0xffu;
+            // Loop until anything is set in the high byte of the state.
+            while ((state->load(ktl::memory_order_seq_cst) & 0xff00) == 0) {
+                apic_send_self_ipi(vec, DELIVERY_MODE_FIXED);
                 thread_yield();
-                thread_process_pending_signals();
             }
             return -1;
         },
-        &vector, DEFAULT_PRIORITY);
+        &state, DEFAULT_PRIORITY);
     thread_resume(int_thread);
 
     // Remove the interrupt and if we don't deadlock and keep executing then all is well.
     interrupt.reset();
 
+    // Signal the thread by setting bits in the high byte.
+    state.fetch_or(0xff00, ktl::memory_order_seq_cst);
     // Shutdown the test.
-    thread_kill(int_thread);
     zx_status_t status = thread_join(int_thread, nullptr, current_time() + ZX_SEC(5));
     EXPECT_EQ(status, ZX_OK, "");
 #endif
