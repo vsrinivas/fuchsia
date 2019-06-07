a689f6682d4883cfc119522bcf95730316bbe539
Fady Samuel <fsamuel@google.com>
2019-06-07 23:00:05 +0000

    [componentmgr] Implement out directory in Hub
    This CL makes a number of changes to support the out directory in the Hub.
    
    1. It clones the client-side DirectoryProxy of the outgoing directory held
    by component manager to use in Hub.
    
    2. If the clone is successful (it isn't always available in tests), then
    it adds an out directory in the exec directory via a DirectoryBroker. DirectoryBroker
    did not support following paths into another directory tree, and so it was updated
    to support passing along a relative path.
    
    3. All calls to Controller::add_entry in Hub were replaced by Controller::add_entry_res
    to avoid races in tests.
    
    4. A unit test was added to verify that an out directory can now be hosted in hub.
    
    Bug: CF-541
    
    Change-Id: I41550d6eb2d59cd4b04f6a31ba44c097ec173650

--- src/sys/component_manager/src/directory_broker.rs
+++ src/sys/component_manager/src/directory_broker.rs
@@ -18,19 +18,22 @@ use {
 /// request for this directory entry is received the given ServerEnd is passed into the closure,
 /// which will presumably make an open request somewhere else and forward on the ServerEnd.
 pub struct DirectoryBroker {
-    route_service: Box<FnMut(ServerEnd<NodeMarker>) + Send>,
+    /// The parameters are as follows:
+    ///  flags: u32
+    ///  mode: u32
+    ///  relative_path: String
+    ///  server_end: ServerEnd<NodeMarker>
+    route_open: Box<FnMut(u32, u32, String, ServerEnd<NodeMarker>) + Send>,
     entry_info: fvfs::directory::entry::EntryInfo,
 }
 
 impl DirectoryBroker {
-    /// new_service_broker will create a new DirectoryBroker to proxy service requests. Whenever an
-    /// open call is received the given closure is called with the new ServerEnd. If the entry_info
-    /// of this node is inspected, it will tell the caller that it is a service node.
-    pub fn new_service_broker(
-        route_service: Box<FnMut(ServerEnd<NodeMarker>) + Send>,
+    /// new will create a new DirectoryBroker to forward directory open requests.
+    pub fn new(
+        route_open: Box<FnMut(u32, u32, String, ServerEnd<NodeMarker>) + Send>,
     ) -> DirectoryBroker {
         return DirectoryBroker {
-            route_service,
+            route_open,
             entry_info: fvfs::directory::entry::EntryInfo::new(INO_UNKNOWN, DIRENT_TYPE_SERVICE),
         };
     }
@@ -38,19 +41,15 @@ impl DirectoryBroker {
 impl DirectoryEntry for DirectoryBroker {
     fn open(
         &mut self,
-        _flags: u32,
-        _mode: u32,
+        flags: u32,
+        mode: u32,
         path: &mut Iterator<Item = &str>,
         server_end: ServerEnd<NodeMarker>,
     ) {
-        if let Some(_) = path.next() {
-            // Services do not support nested paths, so we just are going to close server_end.
-            // It will be closed by the destructor as it will go out of scope.
-            // TODO: we probably want to support nested paths
-            return;
-        }
-        (self.route_service)(server_end);
+        let relative_path = path.collect::<Vec<&str>>().join("/");
+        (self.route_open)(flags, mode, relative_path, server_end);
     }
+
     fn entry_info(&self) -> fvfs::directory::entry::EntryInfo {
         return fvfs::directory::entry::EntryInfo::new(
             self.entry_info.inode(),
