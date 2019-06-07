eec3b21ebaf3cd5c4d36df10d0e4e66453686572
David 'Digit' Turner <digit@google.com>
2019-06-07 22:52:00 +0000

    [graphics][compute]: Generate shaders for debugging.
    This CL refactors hotsort_target.gni and spinel_target.gni
    to use a common GN template to compile compute shaders to
    the SPIR-V format, as well as allow for the generation
    of human-readable versions of the generated shader sources.
    
    A new declare_args variable that can be set to true in
    your out/default/args.gn file to force the generation of
    pre-processed and auto-formatted compute shaders for hotsort
    and spinel.
    
    These are placed under the debug/ sub-directory, as in:
    
      out/default/gen/src/graphics/lib/compute/
         hotsort/targets/hs_amd_gcn3_u64/
           comp/
             hs_transpose.comp    -> original shader
           debug/
             hs_transpose.comp    -> same, but much easier to read.
    
    This helps understand the impact of hotsort_gen parameters
    and vendor-specific configurations, or simply what hotsort
    and spinel modifications end up doing to the generated code.
    
    These shaders are never used, and are only for manual review,
    though it would be technically possible to compile them.
    
    + Move some build settings to gn/build_settings.gni to prepare
      for SPN-35.
    
    TEST: Rebuild with fx build src/graphics/lib/compute:tests
    
    Change-Id: Iad1c3b408e5f4667facd5058ecf5084d48a43da8

---
+++ src/graphics/lib/compute/scripts/preprocess_and_format_glsl_shader.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# Copyright 2019 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Preprocess and clang-format a single GLSL shader script."""
+
+import argparse
+import os
+import subprocess
+import sys
+
+def _Panic(msg):
+    sys.stderr.write('ERROR: %s\n' % msg)
+    sys.exit(1)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '--glslangValidator-tool',
+        default='glslangValidator',
+        help='Use specific glslangValidator tool path.')
+
+    parser.add_argument('--clang-format-tool',
+                        default='clang-format',
+                        help='Use specific clang-format tool path.')
+
+    parser.add_argument(
+        '-I',
+        '--include-dir',
+        action='append',
+        default=[],
+        help='Include directory used during pre-processing')
+
+    parser.add_argument(
+        '-o', '--output', default='-', help='Output path, use - for stdout.')
+
+    parser.add_argument('input', help="Input GLSL shader script path.")
+
+    args = parser.parse_args()
+
+    cmd_args = [args.glslangValidator_tool, '-E', args.input]
+
+    # For some reason, this is defined when compiling, but not pre-processing!
+    cmd_args += [ '-DVULKAN' ]
+
+    for include_dir in args.include_dir:
+        cmd_args.append('-I%s' % include_dir)
+    try:
+        preprocessed_script = subprocess.check_output(cmd_args)
+    except subprocess.CalledProcessError as e:
+        _Panic('Preprocessing error: %s' % e)
+
+    try:
+        p = subprocess.Popen([args.clang_format_tool],
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        formatted_script, _ = p.communicate(preprocessed_script)
+    except subprocess.CalledProcessError as e:
+        _Panic('Formatting error: %s' % e)
+
+    out = sys.stdout if args.output == '-' else open(args.output, 'wb')
+    out.write(formatted_script)
+    out.close()
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
