c6b41820d758f85e1e391a9e596d702c47ed85f3
Cristian Donoso <donosoc@google.com>
2019-06-10 19:43:19 +0000

    [debugger] Multithreaded/Signaling test binaries.
    These are meant to be sandbox applications that can be run in the target
    for verifying/exploring behaviour.
    
    No code should depends on this and these should no be exported in a
    final build.
    
    Change-Id: Iae8311402c20f287c5e7a3bd582a325af496df94

--- src/developer/debug/debug_agent/test_data/multi_threaded.cc
+++ src/developer/debug/debug_agent/test_data/multi_threaded.cc
@@ -3,26 +3,79 @@
 // found in the LICENSE file.
 
 #define _ALL_SOURCE
-#include <zircon/threads.h>
+#include <zircon/status.h>
 #include <zircon/syscalls.h>
+#include <zircon/threads.h>
 
 #include <mutex>
 #include <string>
+#include <vector>
 
+#include "src/lib/fxl/logging.h"
 #include "src/lib/fxl/strings/string_printf.h"
 
-// Simple application that prints from several threads.
+// This binary is meant to be a playground for testing different multi-threading
+// behaviour/signaling/border cases.
+//
+// No code should depends on this, but rather is meant to be a sandox for
+// zxdb developers.
+
+namespace {
+
 constexpr int kThreadCount = 5;
+
 struct ThreadContext {
   int index = 0;
   std::string name;
+  thrd_t handle;
+  zx_handle_t zx_handle;
 };
+
+const char* ThreadStateToString(uint32_t state) {
+  switch (state) {
+    case ZX_THREAD_STATE_NEW:
+      return "ZX_THREAD_STATE_NEW";
+    case ZX_THREAD_STATE_RUNNING:
+      return "ZX_THREAD_STATE_RUNNING";
+    case ZX_THREAD_STATE_SUSPENDED:
+      return "ZX_THREAD_STATE_SUSPENDED";
+    case ZX_THREAD_STATE_BLOCKED:
+      return "ZX_THREAD_STATE_BLOCKED";
+    case ZX_THREAD_STATE_DYING:
+      return "ZX_THREAD_STATE_DYING";
+    case ZX_THREAD_STATE_DEAD:
+      return "ZX_THREAD_STATE_DEAD";
+    case ZX_THREAD_STATE_BLOCKED_EXCEPTION:
+      return "ZX_THREAD_STATE_BLOCKED_EXCEPTION";
+    case ZX_THREAD_STATE_BLOCKED_SLEEPING:
+      return "ZX_THREAD_STATE_BLOCKED_SLEEPING";
+    case ZX_THREAD_STATE_BLOCKED_FUTEX:
+      return "ZX_THREAD_STATE_BLOCKED_FUTEX";
+    case ZX_THREAD_STATE_BLOCKED_PORT:
+      return "ZX_THREAD_STATE_BLOCKED_PORT";
+    case ZX_THREAD_STATE_BLOCKED_CHANNEL:
+      return "ZX_THREAD_STATE_BLOCKED_CHANNEL";
+    case ZX_THREAD_STATE_BLOCKED_WAIT_ONE:
+      return "ZX_THREAD_STATE_BLOCKED_WAIT_ONE";
+    case ZX_THREAD_STATE_BLOCKED_WAIT_MANY:
+      return "ZX_THREAD_STATE_BLOCKED_WAIT_MANY";
+    case ZX_THREAD_STATE_BLOCKED_INTERRUPT:
+      return "ZX_THREAD_STATE_BLOCKED_INTERRUPT";
+    case ZX_THREAD_STATE_BLOCKED_PAGER:
+      return "ZX_THREAD_STATE_BLOCKED_PAGER";
+    default:
+      break;
+  }
+
+  return "<unknown>";
+}
 
 std::mutex kMutex;
 
+#define PRINT(...) printf(__VA_ARGS__); fflush(stdout)
+
 void __NO_INLINE PrintFunction(ThreadContext* ctx, int i) {
-  printf("%s: message %d\n", ctx->name.c_str(), i);
-  fflush(stdout);
+  PRINT("%s: message %d\n", ctx->name.c_str(), i);
 };
 
 int __NO_INLINE ThreadFunction(void* in) {
@@ -38,20 +91,195 @@ int __NO_INLINE ThreadFunction(void* in) {
   return 0;
 }
 
-int main() {
-  thrd_t threads[kThreadCount];
-  ThreadContext contexts[kThreadCount];
+std::unique_ptr<ThreadContext> CreateThread(const std::string& name,
+                                            thrd_start_t func,
+                                            void* ctx = nullptr) {
+  auto context = std::make_unique<ThreadContext>();
+  context->name = name;
+  thrd_create_with_name(&context->handle, func, ctx ? ctx : context.get(),
+                        context->name.c_str());
+  context->zx_handle = thrd_get_zx_handle(context->handle);
+  return context;
+}
+
+std::vector<std::unique_ptr<ThreadContext>> CreateThreads(int count) {
+  std::vector<std::unique_ptr<ThreadContext>> contexts;
+  contexts.reserve(count);
 
   for (int i = 0; i < kThreadCount; i++) {
-    contexts[i].index = i;
-    contexts[i].name = fxl::StringPrintf("thread-%d", i);
-    thrd_create_with_name(threads + i, ThreadFunction, contexts + i,
-                          contexts[i].name.c_str());
+    auto context = std::make_unique<ThreadContext>();
+
+    context->index = i;
+    context->name = fxl::StringPrintf("thread-%d", i);
+    thrd_create_with_name(&context->handle,
+                          ThreadFunction,
+                          context.get(),
+                          context->name.c_str());
+
+    context->zx_handle = thrd_get_zx_handle(context->handle);
+    contexts.push_back(std::move(context));
   }
 
+  return contexts;
+}
 
-  for (int i = 0; i < kThreadCount; i++) {
+// Printing --------------------------------------------------------------------
+
+// Simple application that prints from several threads.
+void MultithreadedPrinting() {
+  auto contexts = CreateThreads(kThreadCount);
+
+  for (auto& context : contexts) {
     int res = 0;
-    thrd_join(threads[i], &res);
+    thrd_join(context->handle, &res);
   }
+}
+
+// Suspending ------------------------------------------------------------------
+
+void Suspending() {
+  auto contexts = CreateThreads(kThreadCount);
+
+  PRINT("Suspending all the threads.\n");
+  for (int i = 0; i < kThreadCount; i++) {
+    zx_handle_t suspend_token;
+    zx_status_t status = zx_task_suspend(contexts[i]->zx_handle,
+                                         &suspend_token);
+    if (status != ZX_OK) {
+      PRINT("Could not suspend thread %d: %s\n", i,
+            zx_status_get_string(status));
+      exit(1);
+    }
+  }
+
+  PRINT("Waiting for suspend notifications.\n");
+  for (int i = 0; i < kThreadCount; i++) {
+    zx_signals_t signals;
+    zx_status_t status = zx_object_wait_one(contexts[i]->zx_handle,
+                                            ZX_THREAD_SUSPENDED,
+                                            zx_deadline_after(ZX_MSEC(100)),
+                                            &signals);
+    if (status != ZX_OK) {
+      PRINT("Could not wait for signal for thread %d: %s\n", i,
+            zx_status_get_string(status));
+      exit(1);
+    }
+
+    if ((signals & ZX_THREAD_SUSPENDED) == 0) {
+      PRINT("Did not get suspended signal for thread %d: %d\n", i, signals);
+      exit(1);
+    }
+
+    PRINT("Successfully suspended thread %i\n", i);
+  }
+
+}
+
+// Wait State ------------------------------------------------------------------
+
+std::atomic<bool> gEntered = false;
+std::atomic<bool> gExit = false;
+
+int InfiniteFunction(void*) {
+  while (!gExit) {
+    gEntered = true;
+    zx_nanosleep(zx_deadline_after(ZX_MSEC(500)));
+  }
+
+  return 0;
+}
+
+std::atomic<bool> gSecondStarted = false;
+
+int WaitOnThread(void* ctx) {
+  gSecondStarted = true;
+  ThreadContext* first_thread = reinterpret_cast<ThreadContext*>(ctx);
+  int res = 1;
+  thrd_join(first_thread->handle, &res);
+  FXL_DCHECK(res == 0);
+  return 0;
+}
+
+zx_info_thread GetThreadState(zx_handle_t thread_handle) {
+  zx_info_thread info;
+  zx_status_t status = zx_object_get_info(thread_handle, ZX_INFO_THREAD, &info,
+                                          sizeof(info), nullptr, nullptr);
+  FXL_DCHECK(status == ZX_OK) << "Got: " << zx_status_get_string(status);
+
+  return info;
+}
+
+
+void WaitState() {
+  auto first_thread = CreateThread("infinite", InfiniteFunction);
+
+  while (!gEntered)
+    zx_nanosleep(zx_deadline_after(ZX_MSEC(100)));
+
+  FXL_LOG(INFO) << "Thread entered infinite loop.";
+
+  auto second_thread = CreateThread("wait-on-first", WaitOnThread,
+                                    first_thread.get());
+  while (!gSecondStarted)
+    zx_nanosleep(zx_deadline_after(ZX_MSEC(100)));
+
+  FXL_LOG(INFO) << "Created second thread.";
+
+  // Print the state of the second thread.
+
+  for (int i = 0; i < 10; i++) {
+    auto info = GetThreadState(second_thread->zx_handle);
+    FXL_LOG(INFO) << "Got status: " << ThreadStateToString(info.state);
+    if (info.state == ZX_THREAD_STATE_BLOCKED_FUTEX)
+      break;
+
+    zx_nanosleep(zx_deadline_after(ZX_SEC(1)));
+  }
+
+  {
+    // Suspend the thread.
+    zx_handle_t suspend_token;
+    zx_status_t status =
+        zx_task_suspend(second_thread->zx_handle, &suspend_token);
+    FXL_DCHECK(status == ZX_OK) << "Got: " << zx_status_get_string(status);
+
+    // Wait for signal.
+    zx_signals_t observed;
+    status = zx_object_wait_one(second_thread->zx_handle, ZX_THREAD_SUSPENDED,
+                                zx_deadline_after(ZX_SEC(1)), &observed);
+    FXL_DCHECK(status == ZX_OK) << "Got: " << zx_status_get_string(status);
+    FXL_DCHECK((observed & ZX_THREAD_SUSPENDED) != 0);
+
+
+    auto info = GetThreadState(second_thread->zx_handle);
+    FXL_LOG(INFO) << "Got status: " << ThreadStateToString(info.state);
+
+    zx_handle_close(suspend_token);
+  }
+
+  FXL_LOG(INFO) << "Exiting.";
+  gExit = true;
+  int res = 2000;
+  FXL_DCHECK(thrd_join(second_thread->handle, &res) == thrd_success);
+  FXL_DCHECK(res == 0) << "Res: " << res;
+  FXL_DCHECK(thrd_join(first_thread->handle, &res) == thrd_success);
+  FXL_DCHECK(res == 0) << "Res: " << res;
+}
+
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  if (argc == 1 || strcmp(argv[1], "printing") == 0) {
+    MultithreadedPrinting();
+  } else if (strcmp(argv[1], "suspending") == 0) {
+    Suspending();
+  } else if (strcmp(argv[1], "wait_state") == 0) {
+    WaitState();
+  } else {
+    PRINT("Unknown option: %s\n", argv[1]);
+    return 1;
+  }
+
+  return 0;
 }
---
+++ src/developer/debug/debug_agent/test_data/thread_signals.cc
@@ -0,0 +1,71 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <unistd.h>
+#include <zircon/syscalls.h>
+#include <zircon/threads.h>
+
+#include "src/lib/fxl/logging.h"
+
+// This binary is meant to be a test ground for thread signaling.
+// The first iteration shows how ZX_THREAD_SUSPENDED and ZX_THREAD_RUNNING
+// signals are used.
+//
+// No code should depends on this, but rather is meant to be a sandox for
+// zxdb developers.
+
+namespace {
+
+static constexpr char kBeacon[] = "Counter: Thread running.\n";
+
+// This is the code that the new thread will run.
+// It's meant to be an eternal loop.
+int ThreadFunction(void*) {
+  while (true) {
+    // We use write to avoid deadlocking with the outside libc calls.
+    write(1, kBeacon, sizeof(kBeacon));
+    zx_nanosleep(zx_deadline_after(ZX_SEC(1)));
+  }
+
+  return 0;
+}
+
+}  // namespace
+
+int main() {
+  FXL_LOG(INFO) << "****** Creating thread.";
+
+  thrd_t thread;
+  thrd_create(&thread, ThreadFunction, nullptr);
+  zx_handle_t thread_handle = 0;
+  thread_handle = thrd_get_zx_handle(thread);
+
+  FXL_LOG(INFO) << "****** Suspending thread.";
+
+  zx_status_t status;
+  zx_handle_t suspend_token;
+  status = zx_task_suspend(thread_handle, &suspend_token);
+  FXL_DCHECK(status == ZX_OK) << "Could not suspend thread: " << status;
+
+  {
+    zx_signals_t observed;
+    status = zx_object_wait_one(thread_handle, ZX_THREAD_SUSPENDED,
+                                zx_deadline_after(ZX_SEC(1)), &observed);
+    FXL_DCHECK(status == ZX_OK) << "Could not get suspended signal: " << status;
+    FXL_DCHECK((observed & ZX_THREAD_SUSPENDED) != 0);
+  }
+
+  // Resuming the thread should get a signal.
+  FXL_LOG(INFO) << "****** Resuming thread.";
+  zx_handle_close(suspend_token);
+  {
+    zx_signals_t observed;
+    status = zx_object_wait_one(thread_handle, ZX_THREAD_RUNNING,
+                                zx_deadline_after(ZX_SEC(1)), &observed);
+    FXL_DCHECK(status == ZX_OK) << "Could not get running signal: " << status;
+    FXL_DCHECK((observed & ZX_THREAD_RUNNING) != 0);
+  }
+
+  FXL_LOG(INFO) << "****** Success.";
+}
