b9b4f13052e2a7c2f1425a40ad9911a7653d2faa
Aaron Green <aarongreen@google.com>
2019-06-12 17:02:28 +0000

    [build][fuzzing] Fix fuzzing unit tests
    This CL fixes the fuzzing unit tests introduced in CL 279988. With that
    CL, the unfortunate choice of a the variable 'package_name' meant the
    'generated_test_package' field was ignored and the tests were put a
    package with a name that conflicted with the fuzzers package. This
    passed CQ because it by default doesn't build the variant fuzzers
    package.
    
    Bug: SEC-224
    Test: 1) Things should build both with or without `--fuzz-with asan`:
        fx set core.x64 --with //bundles:tests [--fuzz-with asan]
        fx build
      2) For each $fuzzers_package, there should be a test package:
        fx run-tests <fuzzers_package_tests>
      3) For each fuzzer in a package with $fuzz_host == true, there should
         be a host fuzzer test:
         fx run-host-tests <fuzzer_test>
    Change-Id: I8bf81f029dfcff9ed6465d81199b56e4b32a2533

--- src/fuzzing/host/host_test.go
+++ src/fuzzing/host/host_test.go
@@ -5,21 +5,11 @@
 package fuzzing
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
-	"path/filepath"
 	"testing"
 )
-
-// Created by `fuzz_package` in //build/fuzzing/fuzzer.gni
-type FuzzSpec struct {
-	Host    bool     `json:"fuzz_host"`
-	Package string   `json:"fuzz_package"`
-	Targets []string `json:"fuzz_targets"`
-}
 
 func TestFxFuzzLib(t *testing.T) {
 	fuchsiaDir, found := os.LookupEnv("FUCHSIA_DIR")
@@ -40,74 +30,5 @@ func TestFxFuzzLib(t *testing.T) {
 			t.Errorf("%s failed:: %v", test, err)
 		}
 		t.Log(string(output[:]))
-	}
-}
-
-func TestHostFuzzers(t *testing.T) {
-	// Search the build directory structure for host variant fuzzer builds.
-	// This is necessarily sensitive to the layout of the build directory and
-	// will need to be updated if the host tests are moved or the host fuzzer
-	// builds' names change.
-	testBin, err := os.Executable()
-	if err != nil {
-		t.Fatalf("Failed to determine executable path: %v", err)
-	}
-	fxBuildDir := path.Dir(path.Dir(testBin))
-
-	// Find the fuzzers that should have been built for host.
-	jsonPath := path.Join(fxBuildDir, "fuzzers.json")
-	_, err = os.Stat(jsonPath)
-	if err != nil && os.IsNotExist(err) {
-		t.SkipNow()
-	}
-	jsonFile, err := os.Open(jsonPath)
-	if err != nil {
-		t.Fatalf("Failed to find fuzzer.json: %v", err)
-	}
-	defer jsonFile.Close()
-
-	jsonData, err := ioutil.ReadAll(jsonFile)
-	if err != nil {
-		t.Fatalf("Failed to read fuzzer.json: %v", err)
-	}
-
-	var specs []FuzzSpec
-	err = json.Unmarshal(jsonData, &specs)
-	if err != nil {
-		t.Fatalf("Failed to parse fuzzer.json: %v", err)
-	}
-	fuzzers := []string{}
-	for _, spec := range specs {
-		if spec.Host {
-			fuzzers = append(fuzzers, spec.Targets...)
-		}
-	}
-
-	// Create a empty file to act as a single input to each fuzzer.
-	variants, err := filepath.Glob(path.Join(fxBuildDir, "host_*-fuzzer"))
-	if err != nil {
-		t.Fatalf("Failed to glob host fuzzer build directories: %v", err)
-	}
-	if len(variants) == 0 {
-		t.SkipNow()
-	}
-	tempFile, err := ioutil.TempFile("", "empty.input")
-	if err != nil {
-		t.Fatalf("Failed to create empty input: %v", err)
-	}
-	tempName := tempFile.Name()
-	tempFile.Close()
-	defer os.Remove(tempName)
-
-	// Check that each fuzzer runs and exits cleanly.
-	for _, variant := range variants {
-		for _, fuzzer := range fuzzers {
-			cmd := exec.Command(path.Join(variant, fuzzer), tempName)
-			output, err := cmd.CombinedOutput()
-			if err != nil {
-				t.Errorf("Failed to run %s: %v", fuzzer, err)
-			}
-			t.Log(string(output[:]))
-		}
 	}
 }
