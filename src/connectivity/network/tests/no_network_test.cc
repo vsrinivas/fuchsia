a7d7243109b7deb589033f89c7a71abe2f5757c0
Tamir Duberstein <tamird@google.com>
2019-06-13 20:54:31 +0000

    [network/tests] simplify no_network_test
    This test just cares about not having an external interface; this is
    already the default netemul environment, so we can remove all the manual
    boilerplate.
    
    Change-Id: I3d17744400a4e5bf0430273446128f2d69e3b1f5

---
+++ src/connectivity/network/tests/no_network_test.cc
@@ -0,0 +1,82 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// These tests should run without any network interface (except loopback).
+
+#include <arpa/inet.h>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(NoNetworkTest, NonBlockingConnectHostV4) {
+  int connfd;
+  ASSERT_GE(connfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0), 0)
+      << strerror(errno);
+
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  ASSERT_EQ(inet_pton(AF_INET, "192.168.0.1", &addr.sin_addr), 1)
+      << strerror(errno);
+  addr.sin_port = htons(10000);
+
+  ASSERT_EQ(connect(connfd, (const struct sockaddr*)&addr, sizeof(addr)), -1);
+  ASSERT_EQ(errno, EHOSTUNREACH) << strerror(errno);
+
+  ASSERT_EQ(close(connfd), 0) << strerror(errno);
+}
+
+TEST(NoNetworkTest, NonBlockingConnectHostV6) {
+  int connfd;
+  ASSERT_GE(connfd = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0), 0)
+      << strerror(errno);
+
+  struct sockaddr_in6 addr;
+  addr.sin6_family = AF_INET6;
+  ASSERT_EQ(inet_pton(AF_INET6, "fc00::1", &addr.sin6_addr), 1)
+      << strerror(errno);
+  addr.sin6_port = htons(10000);
+
+  ASSERT_EQ(connect(connfd, (const struct sockaddr*)&addr, sizeof(addr)), -1);
+  ASSERT_EQ(errno, EHOSTUNREACH) << strerror(errno);
+
+  ASSERT_EQ(close(connfd), 0) << strerror(errno);
+}
+
+TEST(NoNetworkTest, NonBlockingConnectNetV4) {
+  int connfd;
+  ASSERT_GE(connfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0), 0)
+      << strerror(errno);
+
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  // multicast address
+  ASSERT_EQ(inet_pton(AF_INET, "224.0.0.0", &addr.sin_addr), 1)
+      << strerror(errno);
+  addr.sin_port = htons(10000);
+
+  ASSERT_EQ(connect(connfd, (const struct sockaddr*)&addr, sizeof(addr)), -1);
+  ASSERT_EQ(errno, ENETUNREACH) << strerror(errno);
+
+  ASSERT_EQ(close(connfd), 0) << strerror(errno);
+}
+
+TEST(NoNetworkTest, NonBlockingConnectNetV6) {
+  int connfd = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  ASSERT_GE(connfd, 0) << "socket failed: " << strerror(errno);
+
+  struct sockaddr_in6 addr;
+  addr.sin6_family = AF_INET6;
+  // linklocal address
+  ASSERT_EQ(inet_pton(AF_INET6, "fe80::1", &addr.sin6_addr), 1)
+      << strerror(errno);
+  addr.sin6_port = htons(10000);
+
+  ASSERT_EQ(connect(connfd, (const struct sockaddr*)&addr, sizeof(addr)), -1);
+  ASSERT_EQ(errno, ENETUNREACH) << strerror(errno);
+
+  ASSERT_EQ(close(connfd), 0) << strerror(errno);
+}
+
+}  // namespace
