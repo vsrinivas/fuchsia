f46bff9649de4228bfbce590705f0740917c341f
Andrew Wilson <anwilson@google.com>
2019-06-17 18:45:00 +0000

    Revert "[input] Rebased bin/ui/input on input-synthesis."
    This reverts commit 062d53aad73ef798791968c34b5c6edf77412bb8.
    
    Reason for revert: Breaks input swiping from shell which e2e screen navigation tests rely on. 
    
    Original change's description:
    > [input] Rebased bin/ui/input on input-synthesis.
    > 
    > Refactored bin/ui/input to make use of src/lib/ui/input-synthesis.
    > 
    > TEST=Build, CQ and FIDL tests.
    >      fx run-test input-synthesis-tests
    >      fx run-test e2e_input_tests
    > 
    > Change-Id: Iff14de9f87247d88dad7d729925c59cfc8af698e
    
    TBR=qsr@google.com,dtiselice@google.com,viktard@google.com,jaeheon@google.com
    
    Change-Id: I6bf9b8ac9ce6d1e2499c13703d0ce04e8ee3d4a0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- src/lib/ui/input-synthesis/src/lib.rs
+++ src/lib/ui/input-synthesis/src/lib.rs
@@ -11,7 +11,7 @@ use std::{
 
 use failure::Error;
 
-use fidl::endpoints::{self, ServerEnd};
+use fidl::endpoints;
 
 use fidl_fuchsia_ui_input::{
     self, Axis, AxisScale, DeviceDescriptor, InputDeviceMarker, InputDeviceProxy,
@@ -28,31 +28,8 @@ pub mod usages;
 
 use crate::{inverse_keymap::InverseKeymap, usages::Usages};
 
-trait ServerConsumer {
-    fn consume(
-        &self,
-        device: &mut DeviceDescriptor,
-        server: ServerEnd<InputDeviceMarker>,
-    ) -> Result<(), Error>;
-}
-
-struct RegistryServerConsumer;
-
-impl ServerConsumer for RegistryServerConsumer {
-    fn consume(
-        &self,
-        device: &mut DeviceDescriptor,
-        server: ServerEnd<InputDeviceMarker>,
-    ) -> Result<(), Error> {
-        let registry = app::client::connect_to_service::<InputDeviceRegistryMarker>()?;
-        registry.register_device(device, server)?;
-
-        Ok(())
-    }
-}
-
 macro_rules! register_device {
-    ( $consumer:expr , $field:ident : $value:expr ) => {{
+    ( $field:ident : $value:expr ) => {{
         let mut device = DeviceDescriptor {
             device_info: None,
             keyboard: None,
@@ -66,19 +43,15 @@ macro_rules! register_device {
 
         let (input_device_client, input_device_server) =
             endpoints::create_endpoints::<InputDeviceMarker>()?;
-        $consumer.consume(&mut device, input_device_server)?;
+        let registry = app::client::connect_to_service::<InputDeviceRegistryMarker>()?;
+        registry.register_device(&mut device, input_device_server)?;
 
         Ok(input_device_client.into_proxy()?)
     }};
 }
 
-fn register_touchsreen(
-    consumer: impl ServerConsumer,
-    width: u32,
-    height: u32,
-) -> Result<InputDeviceProxy, Error> {
+fn register_touchsreen(width: u32, height: u32) -> Result<InputDeviceProxy, Error> {
     register_device! {
-        consumer,
         touchscreen: TouchscreenDescriptor {
             x: Axis {
                 range: Range { min: 0, max: width as i32 },
@@ -95,18 +68,16 @@ fn register_touchsreen(
     }
 }
 
-fn register_keyboard(consumer: impl ServerConsumer) -> Result<InputDeviceProxy, Error> {
+fn register_keyboard() -> Result<InputDeviceProxy, Error> {
     register_device! {
-        consumer,
         keyboard: KeyboardDescriptor {
             keys: (Usages::HidUsageKeyA as u32..Usages::HidUsageKeyRightGui as u32).collect(),
         }
     }
 }
 
-fn register_media_buttons(consumer: impl ServerConsumer) -> Result<InputDeviceProxy, Error> {
+fn register_media_buttons() -> Result<InputDeviceProxy, Error> {
     register_device! {
-        consumer,
         media_buttons: MediaButtonsDescriptor {
             buttons: fidl_fuchsia_ui_input::MIC_MUTE
                 | fidl_fuchsia_ui_input::VOLUME_DOWN
@@ -161,14 +132,14 @@ fn media_buttons(
     }
 }
 
-fn media_button_event(
+/// Simulates a media button event.
+pub async fn media_button_event_command(
     volume_up: bool,
     volume_down: bool,
     mic_mute: bool,
     reset: bool,
-    consumer: impl ServerConsumer,
 ) -> Result<(), Error> {
-    let input_device = register_media_buttons(consumer)?;
+    let input_device = register_media_buttons()?;
 
     input_device
         .dispatch_report(&mut media_buttons(
@@ -181,14 +152,56 @@ fn media_button_event(
         .map_err(Into::into)
 }
 
-/// Simulates a media button event.
-pub async fn media_button_event_command(
-    volume_up: bool,
-    volume_down: bool,
-    mic_mute: bool,
-    reset: bool,
+fn tap(pos: Option<(u32, u32)>, time: u64) -> InputReport {
+    InputReport {
+        event_time: time,
+        keyboard: None,
+        media_buttons: None,
+        mouse: None,
+        stylus: None,
+        touchscreen: Some(Box::new(TouchscreenReport {
+            touches: match pos {
+                Some((x, y)) => {
+                    vec![Touch { finger_id: 1, x: x as i32, y: y as i32, width: 0, height: 0 }]
+                }
+                None => vec![],
+            },
+        })),
+        sensor: None,
+        trace_id: 0,
+    }
+}
+
+/// Simulates `tap_event_count` taps at coordinates `(x, y)` for a touchscreen of explicit
+/// `width` and `height`.
+///
+/// `duration` is divided equally between touch-down and touch-up event pairs, while the
+/// transition between these pairs is immediate.
+pub async fn tap_event_command(
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+    duration: Duration,
+    tap_event_count: usize,
 ) -> Result<(), Error> {
-    media_button_event(volume_up, volume_down, mic_mute, reset, RegistryServerConsumer)
+    let input_device = register_touchsreen(width, height)?;
+    let tap_duration = duration / tap_event_count as u32;
+
+    repeat_with_delay(
+        tap_event_count,
+        tap_duration,
+        |_| {
+            // Touch down.
+            input_device
+                .dispatch_report(&mut tap(Some((x, y)), nanos_from_epoch()?))
+                .map_err(Into::into)
+        },
+        |_| {
+            // Touch up.
+            input_device.dispatch_report(&mut tap(None, nanos_from_epoch()?)).map_err(Into::into)
+        },
+    )
 }
 
 fn key_press(keyboard: KeyboardReport, time: u64) -> InputReport {
@@ -216,8 +229,11 @@ fn key_press_usage(usage: Option<u32>, time: u64) -> InputReport {
     )
 }
 
-fn key_event(usage: u32, duration: Duration, consumer: impl ServerConsumer) -> Result<(), Error> {
-    let input_device = register_keyboard(consumer)?;
+/// Simulates a key press of specified `usage`.
+///
+/// `duration` is the time spent between key-press and key-release events.
+pub async fn key_event_command(usage: u32, duration: Duration) -> Result<(), Error> {
+    let input_device = register_keyboard()?;
 
     repeat_with_delay(
         1,
@@ -237,17 +253,15 @@ fn key_event(usage: u32, duration: Duration, consumer: impl ServerConsumer) -> R
     )
 }
 
-/// Simulates a key press of specified `usage`.
+/// Simulates `text` being typed on a [qwerty] keyboard by making use of [`InverseKeymap`].
 ///
-/// `duration` is the time spent between key-press and key-release events.
-pub async fn key_event_command(usage: u32, duration: Duration) -> Result<(), Error> {
-    key_event(usage, duration, RegistryServerConsumer)
-}
-
-fn text(input: String, duration: Duration, consumer: impl ServerConsumer) -> Result<(), Error> {
-    let input_device = register_keyboard(consumer)?;
+/// `duration` is divided equally between all keyboard events.
+///
+/// [qwerty]: keymaps/constant.QWERTY_MAP.html
+pub async fn text_command(text: String, duration: Duration) -> Result<(), Error> {
+    let input_device = register_keyboard()?;
     let key_sequence = InverseKeymap::new(keymaps::QWERTY_MAP)
-        .derive_key_sequence(&input)
+        .derive_key_sequence(&text)
         .ok_or_else(|| failure::err_msg("Cannot translate text to key sequence"))?;
 
     let stroke_duration = duration / (key_sequence.len() - 1) as u32;
@@ -267,80 +281,12 @@ fn text(input: String, duration: Duration, consumer: impl ServerConsumer) -> Res
     Ok(())
 }
 
-/// Simulates `input` being typed on a [qwerty] keyboard by making use of [`InverseKeymap`].
+/// Simulates swipe from coordinates `(x0, y0)` to `(x1, y1)` for a touchscreen of explicit
+/// `width` and `height`, with `move_event_count` touch-move events in between.
 ///
-/// `duration` is divided equally between all keyboard events.
-///
-/// [qwerty]: keymaps/constant.QWERTY_MAP.html
-pub async fn text_command(input: String, duration: Duration) -> Result<(), Error> {
-    text(input, duration, RegistryServerConsumer)
-}
-
-fn tap(pos: Option<(u32, u32)>, time: u64) -> InputReport {
-    InputReport {
-        event_time: time,
-        keyboard: None,
-        media_buttons: None,
-        mouse: None,
-        stylus: None,
-        touchscreen: Some(Box::new(TouchscreenReport {
-            touches: match pos {
-                Some((x, y)) => {
-                    vec![Touch { finger_id: 1, x: x as i32, y: y as i32, width: 0, height: 0 }]
-                }
-                None => vec![],
-            },
-        })),
-        sensor: None,
-        trace_id: 0,
-    }
-}
-
-fn tap_event(
-    x: u32,
-    y: u32,
-    width: u32,
-    height: u32,
-    tap_event_count: usize,
-    duration: Duration,
-    consumer: impl ServerConsumer,
-) -> Result<(), Error> {
-    let input_device = register_touchsreen(consumer, width, height)?;
-    let tap_duration = duration / tap_event_count as u32;
-
-    repeat_with_delay(
-        tap_event_count,
-        tap_duration,
-        |_| {
-            // Touch down.
-            input_device
-                .dispatch_report(&mut tap(Some((x, y)), nanos_from_epoch()?))
-                .map_err(Into::into)
-        },
-        |_| {
-            // Touch up.
-            input_device.dispatch_report(&mut tap(None, nanos_from_epoch()?)).map_err(Into::into)
-        },
-    )
-}
-
-/// Simulates `tap_event_count` taps at coordinates `(x, y)` for a touchscreen of explicit
-/// `width` and `height`.
-///
-/// `duration` is divided equally between touch-down and touch-up event pairs, while the
-/// transition between these pairs is immediate.
-pub async fn tap_event_command(
-    x: u32,
-    y: u32,
-    width: u32,
-    height: u32,
-    tap_event_count: usize,
-    duration: Duration,
-) -> Result<(), Error> {
-    tap_event(x, y, width, height, tap_event_count, duration, RegistryServerConsumer)
-}
-
-fn swipe(
+/// `duration` is the time spent between the touch-down and first touch-move events when
+/// `move_event_count > 0` or between the touch-down the touch-up events otherwise.
+pub async fn swipe_command(
     x0: u32,
     y0: u32,
     x1: u32,
@@ -349,9 +295,8 @@ fn swipe(
     height: u32,
     move_event_count: usize,
     duration: Duration,
-    consumer: impl ServerConsumer,
 ) -> Result<(), Error> {
-    let input_device = register_touchsreen(consumer, width, height)?;
+    let input_device = register_touchsreen(width, height)?;
 
     let mut delta_x = x1 as f64 - x0 as f64;
     let mut delta_y = y1 as f64 - y0 as f64;
@@ -386,222 +331,11 @@ fn swipe(
                     time,
                 ),
                 // UP
-                _ => tap(None, time),
+                _ => tap(Some((x1, y1)), time),
             };
 
             input_device.dispatch_report(&mut report).map_err(Into::into)
         },
         |_| Ok(()),
     )
-}
-
-/// Simulates swipe from coordinates `(x0, y0)` to `(x1, y1)` for a touchscreen of explicit
-/// `width` and `height`, with `move_event_count` touch-move events in between.
-///
-/// `duration` is the time spent between the touch-down and first touch-move events when
-/// `move_event_count > 0` or between the touch-down the touch-up events otherwise.
-pub async fn swipe_command(
-    x0: u32,
-    y0: u32,
-    x1: u32,
-    y1: u32,
-    width: u32,
-    height: u32,
-    move_event_count: usize,
-    duration: Duration,
-) -> Result<(), Error> {
-    swipe(x0, y0, x1, y1, width, height, move_event_count, duration, RegistryServerConsumer)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use fidl_fuchsia_ui_input::InputDeviceRequest;
-
-    use fuchsia_async as fasync;
-
-    use futures::TryStreamExt;
-
-    macro_rules! assert_reports_eq {
-        (
-            $report:ident ,
-            $event:expr ,
-            [ $( { $name:ident : $value:expr } ),* $( , )? ]
-            $( , )?
-        ) => {{
-            let mut executor = fasync::Executor::new().expect("failed to create executor");
-
-            struct $report;
-
-            impl ServerConsumer for $report {
-                fn consume(
-                    &self,
-                    _: &mut DeviceDescriptor,
-                    server: ServerEnd<InputDeviceMarker>,
-                ) -> Result<(), Error> {
-                    fasync::spawn(async move {
-                        let mut stream = server.into_stream().expect("failed to convert to stream");
-
-                        $(
-                            let option = await!(stream.try_next())
-                                .expect("failed to await on the next element");
-                            assert!(option.is_some(), "stream should not be empty");
-
-                            if let Some(request) = option {
-                                let InputDeviceRequest::DispatchReport { report, .. } = request;
-                                assert_eq!(
-                                    report.$name,
-                                    Some(Box::new($value)),
-                                );
-                            }
-                        )*
-
-                        assert!(
-                            await!(stream.try_next())
-                                .expect("failed to await on the next element")
-                                .is_none(),
-                            "stream should be empty"
-                        );
-                    });
-
-                    Ok(())
-                }
-            }
-
-            executor.run(async { $event }, 2).expect("failed to run input event");
-        }};
-    }
-
-    #[test]
-    fn media_event_report() {
-        assert_reports_eq!(TestConsumer,
-            media_button_event(true, false, true, false, TestConsumer),
-            [
-                {
-                    media_buttons: MediaButtonsReport {
-                        volume_up: true,
-                        volume_down: false,
-                        mic_mute: true,
-                        reset: false,
-                    }
-                },
-            ]
-        );
-    }
-
-    #[test]
-    fn key_event_report() {
-        assert_reports_eq!(TestConsumer,
-            key_event(40, Duration::from_millis(0), TestConsumer),
-            [
-                {
-                    keyboard: KeyboardReport {
-                        pressed_keys: vec![40]
-                    }
-                },
-                {
-                    keyboard: KeyboardReport {
-                        pressed_keys: vec![]
-                    }
-                },
-            ]
-        );
-    }
-
-    #[test]
-    fn text_event_report() {
-        assert_reports_eq!(TestConsumer,
-            text("A".to_string(), Duration::from_millis(0), TestConsumer),
-            [
-                {
-                    keyboard: KeyboardReport {
-                        pressed_keys: vec![225]
-                    }
-                },
-                {
-                    keyboard: KeyboardReport {
-                        pressed_keys: vec![4, 225]
-                    }
-                },
-                {
-                    keyboard: KeyboardReport {
-                        pressed_keys: vec![]
-                    }
-                },
-            ]
-        );
-    }
-
-    #[test]
-    fn tap_event_report() {
-        assert_reports_eq!(TestConsumer,
-            tap_event(10, 10, 1000, 1000, 1, Duration::from_millis(0), TestConsumer),
-            [
-                {
-                    touchscreen: TouchscreenReport {
-                        touches: vec![Touch {
-                            finger_id: 1,
-                            x: 10,
-                            y: 10,
-                            width: 0,
-                            height: 0,
-                        }],
-                    }
-                },
-                {
-                    touchscreen: TouchscreenReport {
-                        touches: vec![],
-                    }
-                },
-            ]
-        );
-    }
-
-    #[test]
-    fn swipe_event_report() {
-        assert_reports_eq!(TestConsumer,
-            swipe(10, 10, 100, 100, 1000, 1000, 2, Duration::from_millis(0), TestConsumer),
-            [
-                {
-                    touchscreen: TouchscreenReport {
-                        touches: vec![Touch {
-                            finger_id: 1,
-                            x: 10,
-                            y: 10,
-                            width: 0,
-                            height: 0,
-                        }],
-                    }
-                },
-                {
-                    touchscreen: TouchscreenReport {
-                        touches: vec![Touch {
-                            finger_id: 1,
-                            x: 55,
-                            y: 55,
-                            width: 0,
-                            height: 0,
-                        }],
-                    }
-                },
-                {
-                    touchscreen: TouchscreenReport {
-                        touches: vec![Touch {
-                            finger_id: 1,
-                            x: 100,
-                            y: 100,
-                            width: 0,
-                            height: 0,
-                        }],
-                    }
-                },
-                {
-                    touchscreen: TouchscreenReport {
-                        touches: vec![],
-                    }
-                },
-            ]
-        );
-    }
 }
