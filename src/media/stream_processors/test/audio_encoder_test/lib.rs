22006e576362e69a7d3a3736c989bc65016c6c43
Payton Turnage <turnage@google.com>
2019-06-20 17:29:37 +0000

    [mediacodec] Instructions for adding hash tests
    Adds some instructions for how hashes in hash tests
    are found and validated.
    
    TEST: no behavior change
    Change-Id: I301e1544754dd6a9f800dd856841ac30bff142ef

--- src/media/stream_processors/test/audio_encoder_test/lib.rs
+++ src/media/stream_processors/test/audio_encoder_test/lib.rs
@@ -14,9 +14,29 @@ use fidl_fuchsia_media::*;
 use std::rc::Rc;
 use stream_processor_test::*;
 
+// INSTRUCTIONS FOR ADDING HASH TESTS
+//
+// 1. If adding a new pcm input configuration, write the saw wave to file and check it in in the
+//    `test_data` directory. It should only be a few thousand PCM frames.
+// 2. Set the `output_file` field to write the encoded output into `/tmp/` so you can copy it to
+//    host.
+// 3. Create an encoded stream with the same settings using another encoder (for sbc, use sbcenc or
+//    ffmpeg; for aac use faac) on the reference saw wave.
+// 4. Verify the output
+//      a. If the codec should produce the exact same bytes for the same settings, `diff` the two
+//         files.
+//      b. If the codec is permitted to produce different encoded bytes for the same settings, do a
+//         similarity check:
+//           b1. Decode both the reference and our encoded stream (sbcdec, faad, etc)
+//           b2. Import both tracks into Audacity
+//           b3. Apply Effect > Invert to one track
+//           b4. Select both tracks and Tracks > Mix > Mix and Render to New Track
+//           b5. On the resulting track use Effect > Amplify and observe the new peak amplitude
+// 5. If all looks good, commit the hash.
+
 #[fuchsia_async::run_singlethreaded]
 #[test]
-async fn test_suite() -> std::result::Result<(), ::failure::Error> {
+async fn sbc_test_suite() -> Result<()> {
     let sub_bands = SbcSubBands::SubBands4;
     let block_count = SbcBlockCount::BlockCount8;
 
