47d32f128f996ab2c22662630e937003497a3e6b
Ricardo Vargas <rvargas@google.com>
2019-06-25 02:26:18 +0000

    [fs-test-utils] Remove unittest dependency for blobfs tests.
    These blobfs test utilities now are unaware of the testing framework.
    
    Change-Id: I40417e5caf4e3517aaaa014f2c811b1a18e774a7

--- zircon/system/ulib/fs-test-utils/blobfs/blobfs.cc
+++ zircon/system/ulib/fs-test-utils/blobfs/blobfs.cc
@@ -6,13 +6,11 @@
 
 #include <digest/digest.h>
 #include <digest/merkle-tree.h>
-#include <fbl/alloc_checker.h>
 #include <fbl/string.h>
 #include <fbl/unique_fd.h>
 #include <fbl/unique_ptr.h>
 #include <fs-test-utils/blobfs/blobfs.h>
 #include <lib/fdio/io.h>
-#include <unittest/unittest.h>
 #include <zircon/syscalls.h>
 
 namespace fs_test_utils {
@@ -24,7 +22,7 @@ using BlobSrcFunction = void (*)(char* data, size_t length);
 void RandomFill(char* data, size_t length) {
     static unsigned int seed = static_cast<unsigned int>(zx_ticks_get());
     // TODO(US-286): Make this easier to reproduce with reliably generated prng.
-    unittest_printf("RandomFill of %zu bytes with seed: %u\n", length, seed);
+    printf("RandomFill of %zu bytes with seed: %u\n", length, seed);
     for (size_t i = 0; i < length; i++) {
         data[i] = (char)rand_r(&seed);
     }
@@ -33,13 +31,9 @@ void RandomFill(char* data, size_t length) {
 // Creates, writes, reads (to verify) and operates on a blob.
 // Returns the result of the post-processing 'func' (true == success).
 bool GenerateBlob(BlobSrcFunction sourceCb, fbl::String mount_path,
-                  size_t size_data, fbl::unique_ptr<BlobInfo>* out) {
-    BEGIN_HELPER;
-    fbl::AllocChecker ac;
-    fbl::unique_ptr<BlobInfo> info(new (&ac) BlobInfo);
-    EXPECT_EQ(ac.check(), true);
-    info->data.reset(new (&ac) char[size_data]);
-    EXPECT_EQ(ac.check(), true);
+                  size_t size_data, std::unique_ptr<BlobInfo>* out) {
+    std::unique_ptr<BlobInfo> info(new BlobInfo);
+    info->data.reset(new char[size_data]);
     sourceCb(info->data.get(), size_data);
     info->size_data = size_data;
 
@@ -48,13 +42,15 @@ bool GenerateBlob(BlobSrcFunction sourceCb, fbl::String mount_path,
     if (info->size_merkle == 0) {
         info->merkle = nullptr;
     } else {
-        info->merkle.reset(new (&ac) char[info->size_merkle]);
-        ASSERT_EQ(ac.check(), true);
+        info->merkle.reset(new char[info->size_merkle]);
     }
     Digest digest;
-    ASSERT_EQ(MerkleTree::Create(&info->data[0], info->size_data, &info->merkle[0],
-                                 info->size_merkle, &digest),
-              ZX_OK, "Couldn't create Merkle Tree");
+    zx_status_t status = MerkleTree::Create(&info->data[0], info->size_data, &info->merkle[0],
+                                            info->size_merkle, &digest);
+    if (status != ZX_OK) {
+        printf("Couldn't create Merkle Tree\n");
+        return false;
+    }
     strcpy(info->path, mount_path.c_str());
     size_t mount_path_len = strlen(info->path);
     strcpy(info->path + mount_path_len, "/");
@@ -62,38 +58,43 @@ bool GenerateBlob(BlobSrcFunction sourceCb, fbl::String mount_path,
     digest.ToString(info->path + prefix_len, sizeof(info->path) - prefix_len);
 
     // Sanity-check the merkle tree
-    ASSERT_EQ(MerkleTree::Verify(&info->data[0], info->size_data, &info->merkle[0],
-                                 info->size_merkle, 0, info->size_data, digest),
-              ZX_OK, "Failed to validate Merkle Tree");
-
+    status = MerkleTree::Verify(&info->data[0], info->size_data, &info->merkle[0],
+                                 info->size_merkle, 0, info->size_data, digest);
+    if (status != ZX_OK) {
+        printf("Failed to validate Merkle Tree\n");
+        return false;
+    }
     *out = std::move(info);
-    END_HELPER;
+    return true;
 }
 
-bool GenerateRandomBlob(fbl::String mount_path, size_t size_data, fbl::unique_ptr<BlobInfo>* out) {
-    BEGIN_HELPER;
-    ASSERT_TRUE(GenerateBlob(RandomFill, mount_path, size_data, out));
-    END_HELPER;
+bool GenerateRandomBlob(fbl::String mount_path, size_t size_data, std::unique_ptr<BlobInfo>* out) {
+    return GenerateBlob(RandomFill, mount_path, size_data, out);
 }
 
 bool VerifyContents(int fd, const char* data, size_t size_data) {
-    BEGIN_HELPER;
-    // Verify the contents of the Blob
-    fbl::AllocChecker ac;
+    // Verify the contents of the Blob.
     constexpr size_t kReadSize = 8192;
-    fbl::unique_ptr<char[]> buffer(new (&ac) char[kReadSize]);
-    EXPECT_EQ(ac.check(), true);
-    ASSERT_EQ(lseek(fd, 0, SEEK_SET), 0);
+    std::unique_ptr<char[]> buffer(new char[kReadSize]);
+    if (lseek(fd, 0, SEEK_SET) != 0) {
+        printf("Failed to seek to start\n");
+        return false;
+    }
 
     size_t total_read = 0;
     while (total_read != size_data) {
         ssize_t result = read(fd, buffer.get(), kReadSize);
-        ASSERT_GT(result, 0);
-        ASSERT_EQ(memcmp(buffer.get(), &data[total_read], result), 0);
+        if (result <= 0) {
+            printf("Failed to read file. Result: %li\n", result);
+            return false;
+        }
+        if (memcmp(buffer.get(), &data[total_read], result) != 0) {
+            printf("Blob contents differ\n");
+            return false;
+        }
         total_read += result;
     }
-
-    END_HELPER;
+    return true;
 }
 
 } // namespace fs_test_utils
