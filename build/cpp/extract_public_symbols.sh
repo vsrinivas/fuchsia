85746644265edceed498cd2bc42af7fb316f9882
Yifei Teng <yifeit@google.com>
2019-06-25 00:07:31 +0000

    [sdk] Validate golden symbols for DSO prebuilts in the SDK
    Since the CL to add vulkan.symbols.abi to third_party/vulkan landed,
    we can also now start validating that the symbols do not change.
    
    Static libraries are more complex and likely requires a more complex
    policy on public symbols, and after syncing with the Toolchains team,
    we decided not to address them in this patch.
    
    DX-688 #comment
    
    Change-Id: I71e2bc0024fa6c5cda2bdec5077c42322357f9b4

---
+++ build/cpp/extract_public_symbols.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -euo pipefail
+
+function usage() {
+  echo "Usage: $0 [NM] [LIBRARY] [SYMBOLS]"
+  echo "Dump the list of exported symbols from a prebuilt library"
+  echo "[NM]:      path to the llvm-nm binary"
+  echo "[LIBRARY]: path to the prebuilt library"
+  echo "[SYMBOLS]: path to the output symbols file"
+  exit 1
+}
+
+if [[ $# -ne 3 ]]
+then
+    usage
+fi
+
+readonly NM=$1
+readonly LIBRARY=$2
+readonly SYMBOLS=$3
+
+if ! [[ -e "$NM" ]]; then
+  echo "Error: $NM not found" >&2
+  usage
+fi
+
+if ! [[ -e "$LIBRARY" ]]; then
+  echo "Error: $LIBRARY not found" >&2
+  usage
+fi
+
+mkdir -p $(dirname "${SYMBOLS}")
+
+if [[ ${LIBRARY} == *.a ]]
+then
+  echo "Error: $LIBRARY, support for static libraries is not ready yet."
+elif ! [[ ( ${LIBRARY} == *.so ) || ( ${LIBRARY} == *.so.debug ) ]]
+then
+  echo "Error: cannot handle $LIBRARY" >&2
+  exit 1
+fi
+
+${NM} --dynamic --defined-only ${LIBRARY} \
+  | awk '/ T /{ print $3; }' \
+  | env LC_COLLATE=C sort \
+  > ${SYMBOLS}
---
+++ build/cpp/verify_public_symbols.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -euo pipefail
+
+function usage() {
+  echo "Usage: $0 [CXXFILT] [LIBRARY_NAME] [CURRENT_SYMBOLS] [REFERENCE_SYMBOLS] [STAMP] [--warn]"
+  echo "Verifies the list of public symbols against a golden file."
+  echo "Additionally, checks that none of the symbols is a C++ symbol."
+  echo "[CXXFILT]:      path to the llvm-cxxfilt binary"
+  echo "[LIBRARY_NAME]: a human-readable name for the library"
+  echo "[CURRENT]:      path to the generated symbols file from the extract_public_symbols script"
+  echo "[REFERENCE]:    path to the checked in reference symbol file"
+  echo "[STAMP]:        touch this file when the script succeeds"
+  echo "[--warn]:       (optional) if is specified, only output warnings and do not fail the build"
+  exit 1
+}
+
+WARN_ON_CHANGES=0
+
+if [[ $# -ne 5 ]]
+then
+  if [[ $# -ne 6 ]]
+  then
+    usage
+  else
+    if ! [[ $6 == "--warn" ]]; then
+      usage
+    else
+      WARN_ON_CHANGES=1
+    fi
+  fi
+fi
+
+readonly CXXFILT=$1
+readonly LIBRARY_NAME=$2
+readonly CURRENT=$3
+readonly REFERENCE=$4
+readonly STAMP=$5
+
+if ! [[ -e "$CXXFILT" ]]; then
+  echo "Error: $CXXFILT not found" >&2
+  usage
+fi
+
+if ! [[ -e "$CURRENT" ]]; then
+  echo "Error: $CURRENT not found" >&2
+  usage
+fi
+
+# Detect presence of C++ symbols
+ORIGINAL_SYMBOLS=$(cat ${CURRENT})
+DEMANGLED_SYMBOLS=$(${CXXFILT} < ${CURRENT})
+
+if ! [[ "$ORIGINAL_SYMBOLS" == "$DEMANGLED_SYMBOLS" ]]; then
+  echo
+  echo "Error: Prebuilt libraries exported to the SDK should not have C++ symbols"
+  echo "In library $LIBRARY_NAME"
+  echo "NOTE: the following functions are exported with C++ linkage:"
+  while read original <&3 && read demangled <&4; do
+    if ! [[ "$original" == "$demangled" ]]; then
+      echo "$demangled"
+    fi
+  done 3<<<"$ORIGINAL_SYMBOLS" 4<<<"$DEMANGLED_SYMBOLS"
+  echo
+
+  if [[ "$WARN_ON_CHANGES" -eq 0 ]]; then
+    exit 1
+  fi
+fi
+
+if ! [[ -e "$REFERENCE" ]]; then
+  echo "Error: $REFERENCE not found" >&2
+  usage
+fi
+
+if ! diff ${CURRENT} ${REFERENCE} >/dev/null; then
+  echo
+  echo "Error: ABI has changed! In library $LIBRARY_NAME"
+  echo
+
+  ONLY_IN_CURRENT=$(comm -23 <(cat ${CURRENT}) <(cat ${REFERENCE}))
+  ONLY_IN_REFERENCE=$(comm -13 <(cat ${CURRENT}) <(cat ${REFERENCE}))
+
+  if ! [[ -z ${ONLY_IN_CURRENT} ]]; then
+    echo "NOTE: the following symbols were added:"
+    echo ${ONLY_IN_CURRENT}
+    echo
+  fi
+
+  if ! [[ -z ${ONLY_IN_REFERENCE} ]]; then
+    echo "NOTE: the following symbols were removed:"
+    echo ${ONLY_IN_REFERENCE}
+    echo
+  fi
+
+  echo "Please acknowledge this change by running:"
+  echo "cp $CURRENT $REFERENCE"
+  echo
+
+  if [[ "$WARN_ON_CHANGES" -eq 0 ]]; then
+    exit 1
+  fi
+fi
+
+touch ${STAMP}
