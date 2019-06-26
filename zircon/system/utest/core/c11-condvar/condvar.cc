9c04e0bee2102840b9dae9cafb3c56d49104ad34
Marty Faltesek <mfaltesek@google.com>
2019-06-26 22:09:52 +0000

    [zircon][core-test] condvar test enhancements
    Break up the main test, one for broadcast signal and the other for
    thread signal.
    
    ZX-3744 #done
    
    Test: /boot/test/core/c11-condvar-test
    
    Change-Id: Ie7a88d281bd1c5ea9d25e5f12e6833bdc777ab82

--- zircon/system/utest/core/c11-condvar/condvar.cc
+++ zircon/system/utest/core/c11-condvar/condvar.cc
@@ -10,25 +10,29 @@
 
 namespace {
 
-TEST(ConditionalVariableTest, BroadcastSignalWait) {
-    struct CondThreadArgs {
-        mtx_t mutex;
-        cnd_t cond;
-        int threads_woken;
-        int threads_started;
-        int threads_woke_first_barrier;
-    };
+constexpr int kNumThreads = 3;
+constexpr int* kIgnoreReturn = nullptr;
 
+struct CondThreadArgs {
+    mtx_t mutex;
+    cnd_t cond;
+    std::atomic<int> threads_started;
+    std::atomic<int> threads_woken;
+    std::atomic<bool> wait_condition;
+};
+
+TEST(ConditionalVariableTest, BroadcastSignalThreadWait) {
     auto CondWaitHelper = [](void* arg) -> int {
         CondThreadArgs* args = static_cast<CondThreadArgs*>(arg);
 
         mtx_lock(&args->mutex);
         args->threads_started++;
-        cnd_wait(&args->cond, &args->mutex);
-        args->threads_woke_first_barrier++;
-        cnd_wait(&args->cond, &args->mutex);
+        while (args->wait_condition == false) {
+            cnd_wait(&args->cond, &args->mutex);
+        }
         args->threads_woken++;
         mtx_unlock(&args->mutex);
+
         return 0;
     };
 
@@ -37,55 +41,100 @@ TEST(ConditionalVariableTest, BroadcastSignalWait) {
     ASSERT_EQ(mtx_init(&args.mutex, mtx_plain), thrd_success);
     ASSERT_EQ(cnd_init(&args.cond), thrd_success);
 
-    thrd_t thread1, thread2, thread3;
+    thrd_t threads[kNumThreads];
+    for (auto& thread : threads) {
+        ASSERT_EQ(thrd_create(&thread, CondWaitHelper, &args), thrd_success);
+    }
 
-    ASSERT_EQ(thrd_create(&thread1, CondWaitHelper, &args), thrd_success);
-    ASSERT_EQ(thrd_create(&thread2, CondWaitHelper, &args), thrd_success);
-    ASSERT_EQ(thrd_create(&thread3, CondWaitHelper, &args), thrd_success);
-
-    // Wait for all the threads to report that they've started.
+    // Wait for all the threads to report that they've started, and
+    // all have reached the wait.
     while (true) {
         mtx_lock(&args.mutex);
         int threads = args.threads_started;
         mtx_unlock(&args.mutex);
-        if (threads == 3) {
+        if (threads == kNumThreads) {
             break;
         }
         sched_yield();
     }
 
+    args.wait_condition = true;
     ASSERT_EQ(cnd_broadcast(&args.cond), thrd_success);
 
     // Wait for all the threads to report that they were woken.
     while (true) {
-        mtx_lock(&args.mutex);
-        int threads = args.threads_woke_first_barrier;
-        mtx_unlock(&args.mutex);
-        if (threads == 3) {
+        int threads_woken = args.threads_woken;
+        if (threads_woken == kNumThreads) {
             break;
         }
         sched_yield();
     }
 
-    for (int iteration = 0; iteration < 3; iteration++) {
+
+    for (auto& thread : threads) {
+        EXPECT_EQ(thrd_join(thread, kIgnoreReturn), thrd_success);
+    }
+}
+
+TEST(ConditionalVariableTest, SignalThreadWait) {
+    auto CondWaitHelper = [](void* arg) -> int {
+        CondThreadArgs* args = static_cast<CondThreadArgs*>(arg);
+
+        mtx_lock(&args->mutex);
+        args->threads_started++;
+        while (args->wait_condition == false) {
+            cnd_wait(&args->cond, &args->mutex);
+        }
+        args->wait_condition = false;
+        args->threads_woken++;
+        mtx_unlock(&args->mutex);
+
+        return 0;
+    };
+
+    CondThreadArgs args = {};
+
+    ASSERT_EQ(mtx_init(&args.mutex, mtx_plain), thrd_success);
+    ASSERT_EQ(cnd_init(&args.cond), thrd_success);
+
+    thrd_t threads[kNumThreads];
+    for (auto& thread : threads) {
+        ASSERT_EQ(thrd_create(&thread, CondWaitHelper, &args), thrd_success);
+    }
+
+    // Wait for all the threads to report that they've started,
+    // and all have reached the wait.
+    while (true) {
+        mtx_lock(&args.mutex);
+        int threads = args.threads_started;
+        mtx_unlock(&args.mutex);
+        if (threads == kNumThreads) {
+            break;
+        }
+        sched_yield();
+    }
+
+    for (int iteration = 0; iteration < kNumThreads; iteration++) {
+        mtx_lock(&args.mutex);
+        args.wait_condition = true;
         EXPECT_EQ(cnd_signal(&args.cond), thrd_success);
+        mtx_unlock(&args.mutex);
 
         // Wait for one thread to report that it was woken.
         while (true) {
             mtx_lock(&args.mutex);
-            int threads = args.threads_woken;
+            int threads_woken = args.threads_woken;
             mtx_unlock(&args.mutex);
-            if (threads == iteration + 1) {
+            if (threads_woken == iteration + 1) {
                 break;
             }
             sched_yield();
         }
     }
 
-    constexpr int* kIgnoreReturn = nullptr;
-    EXPECT_EQ(thrd_join(thread1, kIgnoreReturn), thrd_success);
-    EXPECT_EQ(thrd_join(thread2, kIgnoreReturn), thrd_success);
-    EXPECT_EQ(thrd_join(thread3, kIgnoreReturn), thrd_success);
+    for (auto& thread : threads) {
+        EXPECT_EQ(thrd_join(thread, kIgnoreReturn), thrd_success);
+    }
 }
 
 void TimeAddNsec(struct timespec* ts, int nsec) {
