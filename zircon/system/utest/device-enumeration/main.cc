07c8513e0af02c45fbc1a99d54edfbc999015429
Suraj Malhotra <surajmalhotra@google.com>
2019-06-26 18:09:30 +0000

    [device-enumeration-test] Migrate to use zxtest
    ZX-4426 #done
    
    TESTED: runtests -t device-enumeration-test on vim2, qemu, and astro
    Change-Id: I96121b20edfb7cd322b14bb5b76ed18c047dda45

--- zircon/system/utest/device-enumeration/main.cc
+++ zircon/system/utest/device-enumeration/main.cc
@@ -8,41 +8,29 @@
 #include <unistd.h>
 
 #include <fbl/algorithm.h>
+#include <fbl/string.h>
 #include <fbl/unique_fd.h>
 #include <fuchsia/sysinfo/c/fidl.h>
 #include <lib/devmgr-integration-test/fixture.h>
 #include <lib/fdio/directory.h>
 #include <lib/fdio/fd.h>
 #include <lib/fdio/fdio.h>
-#include <unittest/unittest.h>
 #include <zircon/status.h>
-
-using devmgr_integration_test::RecursiveWaitForFile;
+#include <zxtest/zxtest.h>
 
 namespace {
 
-enum class Board {
-    kQemu,
-    kVim2,
-    kAstro,
-    kCleo,
-    kSherlock,
-    kMt8167sRef,
-    kMsm8x53Som,
-    kAs370,
-    kVisalia,
-    kUnknown,
-};
+using devmgr_integration_test::RecursiveWaitForFile;
 
-Board GetBoardType() {
+fbl::String GetTestFilter() {
     constexpr char kSysInfoPath[] = "/dev/misc/sysinfo";
     fbl::unique_fd sysinfo(open(kSysInfoPath, O_RDWR));
     if (!sysinfo) {
-        return Board::kUnknown;
+        return "Unknown";
     }
     zx::channel channel;
     if (fdio_get_service_handle(sysinfo.release(), channel.reset_and_get_address()) != ZX_OK) {
-        return Board::kUnknown;
+        return "Unknown";
     }
 
     char board_name[fuchsia_sysinfo_SYSINFO_BOARD_NAME_LEN + 1];
@@ -51,65 +39,61 @@ Board GetBoardType() {
     zx_status_t fidl_status = fuchsia_sysinfo_DeviceGetBoardName(
         channel.get(), &status, board_name, sizeof(board_name), &actual_size);
     if (fidl_status != ZX_OK || status != ZX_OK) {
-        return Board::kUnknown;
+        return "Unknown";
     }
     board_name[actual_size] = '\0';
 
     printf("Found board %s\n", board_name);
 
     if (!strcmp(board_name, "qemu")) {
-        return Board::kQemu;
+        return "*Qemu*";
     } else if (!strcmp(board_name, "vim2")) {
-        return Board::kVim2;
+        return "*Vim2*";
     } else if (!strcmp(board_name, "astro")) {
-        return Board::kAstro;
+        return "*Astro*";
     } else if (!strcmp(board_name, "cleo")) {
-        return Board::kCleo;
+        return "*Cleo*";
     } else if (!strcmp(board_name, "sherlock")) {
-        return Board::kSherlock;
+        return "*Sherlock*";
     } else if (!strcmp(board_name, "mt8167s_ref")) {
-        return Board::kMt8167sRef;
+        return "*Mt8167sRef*";
     } else if (!strcmp(board_name, "msm8x53-som")) {
-        return Board::kMsm8x53Som;
+        return "*Msm8x53Som*";
     } else if (!strcmp(board_name, "as370")) {
-        return Board::kAs370;
+        return "*As370*";
     } else if (!strcmp(board_name, "visalia")) {
-        return Board::kVisalia;
+        return "*Visalia*";
     }
 
-    return Board::kUnknown;
+    return "Unknown";
 }
 
-bool TestRunner(const char** device_paths, size_t paths_num) {
-    BEGIN_HELPER;
+class DeviceEnumerationTest : public zxtest::Test {
+protected:
+    void TestRunner(const char** device_paths, size_t paths_num) {
+        fbl::unique_fd devfs_root(open("/dev", O_RDWR));
+        ASSERT_TRUE(devfs_root);
 
-    fbl::unique_fd devfs_root(open("/dev", O_RDWR));
-    ASSERT_TRUE(devfs_root);
-
-    fbl::unique_fd fd;
-    for (size_t i = 0; i < paths_num; ++i) {
-        printf("Checking %s\n", device_paths[i]);
-        EXPECT_EQ(RecursiveWaitForFile(devfs_root, device_paths[i], &fd),
-                  ZX_OK, device_paths[i]);
+        fbl::unique_fd fd;
+        for (size_t i = 0; i < paths_num; ++i) {
+            printf("Checking %s\n", device_paths[i]);
+            EXPECT_OK(RecursiveWaitForFile(devfs_root, device_paths[i], &fd),
+                      "%s", device_paths[i]);
+        }
     }
-    END_HELPER;
-}
+};
 
-bool qemu_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, QemuTest) {
     static const char* kDevicePaths[] = {
         "sys/platform/qemu-bus",
         "sys/platform/00:00:6/rtc",
         "sys/pci/00:00.0",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool vim2_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, Vim2Test) {
     static const char* kDevicePaths[] = {
         "sys/platform/vim",
         "sys/platform/00:00:1b/sysmem",
@@ -134,13 +118,10 @@ bool vim2_enumeration_test() {
         "wifi",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool astro_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, AstroTest) {
     static const char* kDevicePaths[] = {
         "sys/platform/aml-bus",
         "sys/platform/05:03:1/aml-axg-gpio",
@@ -182,13 +163,10 @@ bool astro_enumeration_test() {
         //"sys/platform/05:05:3/aml-uart/serial/bt-transport-uart/bcm-hci",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool cleo_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, CleoTest) {
     static const char* kDevicePaths[] = {
         "sys/platform/mt8167s_ref",
         "sys/platform/0d:00:1/mt8167-gpio",
@@ -231,13 +209,10 @@ bool cleo_enumeration_test() {
         "sys/platform/0d:00:6/mt8167-i2c/i2c/i2c-0-83/ltr-578als/hid-device-000",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool sherlock_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, SherlockTest) {
     static const char* kDevicePaths[] = {
         "sys/platform/sherlock",
         "sys/platform/05:04:1/aml-axg-gpio",
@@ -279,13 +254,10 @@ bool sherlock_enumeration_test() {
         "sys/platform/00:00:f/fallback-rtc",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool mt8167s_ref_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, Mt8167sRefTest) {
     static const char* kDevicePaths[] = {
         "sys/platform/mt8167s_ref",
         "sys/platform/0d:00:1/mt8167-gpio",
@@ -326,13 +298,10 @@ bool mt8167s_ref_enumeration_test() {
         "sys/platform/0d:00:9/mtk-thermal",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool msm8x53_som_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, Msm8x53SomTest) {
     static const char* kDevicePaths[] = {
         "sys/platform/msm8x53",
         "sys/platform/13:01:1",
@@ -341,13 +310,10 @@ bool msm8x53_som_enumeration_test() {
         "sys/platform/13:01:4/msm-clk",
         "sys/platform/13:01:5/msm8x53-power"};
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool as370_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, As370Test) {
     static const char* kDevicePaths[] = {
         "sys/platform/as370",
         "sys/platform/14:01:1",
@@ -360,13 +326,10 @@ bool as370_enumeration_test() {
         "as370-audio-out",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
 
-bool visalia_enumeration_test() {
-    BEGIN_TEST;
+TEST_F(DeviceEnumerationTest, VisaliaTest) {
     static const char* kDevicePaths[] = {
         "sys/platform/as370",
         "sys/platform/14:01:1",
@@ -377,52 +340,34 @@ bool visalia_enumeration_test() {
         "dwc2-usb",
     };
 
-    ASSERT_TRUE(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
-
-    END_TEST;
+    ASSERT_NO_FATAL_FAILURES(TestRunner(kDevicePaths, fbl::count_of(kDevicePaths)));
 }
-
-#define MAKE_TEST_CASE(name)          \
-    BEGIN_TEST_CASE(name)             \
-    RUN_TEST(name##_enumeration_test) \
-    END_TEST_CASE(name)               \
-    test_case_element* test_case_##name = TEST_CASE_ELEMENT(name)
-
-MAKE_TEST_CASE(qemu);
-MAKE_TEST_CASE(vim2);
-MAKE_TEST_CASE(astro);
-MAKE_TEST_CASE(cleo);
-MAKE_TEST_CASE(sherlock);
-MAKE_TEST_CASE(mt8167s_ref);
-MAKE_TEST_CASE(msm8x53_som);
-MAKE_TEST_CASE(as370);
-MAKE_TEST_CASE(visalia);
-
-#undef MAKE_TEST_CASE
 
 } // namespace
 
 int main(int argc, char** argv) {
-    switch (GetBoardType()) {
-    case Board::kQemu:
-        return unittest_run_one_test(test_case_qemu, TEST_ALL) ? 0 : -1;
-    case Board::kVim2:
-        return unittest_run_one_test(test_case_vim2, TEST_ALL) ? 0 : -1;
-    case Board::kAstro:
-        return unittest_run_one_test(test_case_astro, TEST_ALL) ? 0 : -1;
-    case Board::kCleo:
-        return unittest_run_one_test(test_case_cleo, TEST_ALL) ? 0 : -1;
-    case Board::kSherlock:
-        return unittest_run_one_test(test_case_sherlock, TEST_ALL) ? 0 : -1;
-    case Board::kMt8167sRef:
-        return unittest_run_one_test(test_case_mt8167s_ref, TEST_ALL) ? 0 : -1;
-    case Board::kMsm8x53Som:
-        return unittest_run_one_test(test_case_msm8x53_som, TEST_ALL) ? 0 : -1;
-    case Board::kAs370:
-        return unittest_run_one_test(test_case_as370, TEST_ALL) ? 0 : -1;
-    case Board::kVisalia:
-        return unittest_run_one_test(test_case_visalia, TEST_ALL) ? 0 : -1;
-    case Board::kUnknown:
+    fbl::Vector<fbl::String> errors;
+    auto options = zxtest::Runner::Options::FromArgs(argc, argv, &errors);
+
+    if (!errors.is_empty()) {
+        for (const auto& error : errors) {
+            fprintf(stderr, "%s\n", error.c_str());
+        }
+        options.help = true;
+    }
+
+    options.filter = fbl::StringPrintf("%s:%s", GetTestFilter().c_str(), options.filter.c_str());
+
+    // Errors will always set help to true.
+    if (options.help) {
+        zxtest::Runner::Options::Usage(argv[0], stdout);
+        return errors.is_empty();
+    }
+
+    if (options.list) {
+        zxtest::Runner::GetInstance()->List(options);
         return 0;
     }
+
+    return zxtest::Runner::GetInstance()->Run(options);
 }
