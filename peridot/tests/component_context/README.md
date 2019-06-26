777d44f0867bab55f59d730bc62af3564e7c2cce
Vickie Cheng <vickiecheng@google.com>
2019-06-26 18:39:11 +0000

    [modular][tests] Deprecate modular_tests
    This removes the last reminants of modular_tests.
    
    MF-386 #done
    
    Change-Id: Ic97e24a7037df817f0543ced5efee45444d8dc05

--- peridot/tests/component_context/README.md
+++
@@ -1,7 +0,0 @@
-# component_context integration test
-
-The purpose of `fuchsia::modular::ComponentContext` is mainly to expose APIs that lets clients
-interact with Agents.
-
-This test exercises the APIs used to connect with Agents, influence their
-life cycle, and use their services.
--- peridot/tests/run_modular_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/boot/bin/sh
-# Copyright 2017 The Fuchsia Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-set -e
-
-run_integration_tests --test_file=/pkgfs/packages/modular_tests/0/data/modular_tests.json "$@"
-
