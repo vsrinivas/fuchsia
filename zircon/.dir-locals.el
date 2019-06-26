61353241e56c86e88b354286b7a6430799005300
Roland McGrath <mcgrathr@google.com>
2019-06-26 03:09:12 +0000

    [zircon] Remove .dir-locals.el
    This file is provided for Emacs users to set the defaults for code
    style.  There's no longer any reason to have a separate file for
    Zircon, since the //.dir-locals.el file applies to the whole tree.
    
    Change-Id: Ifafa7681922956ba0d509c73c8180f68261127be

--- zircon/.dir-locals.el
+++
@@ -1,9 +0,0 @@
-;; Emacs settings.
-;; Copyright (C) 2016 Google, Inc.
-
-(
- (c-mode . ((c-file-style . "stroustrup")
-	    (indent-tabs-mode . nil)))
- (c++-mode . ((c-file-style . "stroustrup")
-	      (indent-tabs-mode . nil)))
-)
