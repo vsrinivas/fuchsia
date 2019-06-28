676bfc444d4fffbe5a53eac795a2c29953df155e
David Greenaway <dgreenaway@google.com>
2019-06-28 00:09:29 +0000

    [zircon][profile] Update unit tests to use C++ libraries and zxtest.
    Bug: ZX-4490
    
    Change-Id: I018d135bbe853e771d10213b8501452fe31136d5

--- zircon/system/utest/profile/profile.cc
+++ zircon/system/utest/profile/profile.cc
@@ -2,51 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <assert.h>
-#include <errno.h>
-#include <stdio.h>
-
-#include <lib/fdio/io.h>
+#include <fuchsia/scheduler/c/fidl.h>
+#include <lib/fdio/directory.h>
 #include <lib/fdio/fd.h>
 #include <lib/fdio/fdio.h>
-#include <lib/fdio/directory.h>
-
-#include <unittest/unittest.h>
-
+#include <lib/fdio/io.h>
+#include <lib/zx/channel.h>
+#include <lib/zx/object.h>
+#include <lib/zx/profile.h>
 #include <zircon/syscalls.h>
-#include <fuchsia/scheduler/c/fidl.h>
+#include <zxtest/zxtest.h>
 
-static bool get_profile(void) {
-    BEGIN_TEST;
+namespace {
 
-    zx_handle_t ch[2];
-    ASSERT_EQ(ZX_OK, zx_channel_create(0u, &ch[0], &ch[1]), "channel create");
-
-    ASSERT_EQ(ZX_OK, fdio_service_connect(
-        "/svc/" fuchsia_scheduler_ProfileProvider_Name, ch[0]), "connect");
-
-    zx_handle_t profile = ZX_HANDLE_INVALID;
-    zx_status_t status = ZX_ERR_NOT_SUPPORTED;
-    EXPECT_EQ(ZX_OK, fuchsia_scheduler_ProfileProviderGetProfile(
-        ch[1], 0u, "<test>", 6, &status, &profile), "");
-
-    EXPECT_EQ(ZX_OK, status, "profile create");
-
-    zx_info_handle_basic_t info;
-    ASSERT_EQ(
-        zx_object_get_info(profile, ZX_INFO_HANDLE_BASIC, &info, sizeof(info), nullptr, nullptr),
-        ZX_OK, "object_get_info");
-    EXPECT_NE(info.koid, 0, "no koid");
-    EXPECT_EQ(info.type, ZX_OBJ_TYPE_PROFILE, "incorrect type");
-
-    zx_handle_close(profile);
-    zx_handle_close(ch[0]);
-    zx_handle_close(ch[1]);
-
-    END_TEST;
+zx_status_t CreateProfile(const zx::channel& profile_provider, uint32_t priority,
+                          const std::string& name, zx_status_t* server_status,
+                          zx::profile* profile) {
+  zx_handle_t raw_profile_handle;
+  zx_status_t result = fuchsia_scheduler_ProfileProviderGetProfile(
+      profile_provider.get(), priority, name.c_str(), name.length(), server_status,
+      &raw_profile_handle);
+  if (result != ZX_OK) {
+    return ZX_OK;
+  }
+  *profile = zx::profile(raw_profile_handle);
+  return ZX_OK;
 }
 
+TEST(Profile, CreateDestroy) {
+  // Connect to ProfileProvider.
+  zx::channel channel1, channel2;
+  ASSERT_OK(zx::channel::create(/*flags=*/0u, &channel1, &channel2));
+  ASSERT_OK(fdio_service_connect("/svc/" fuchsia_scheduler_ProfileProvider_Name, channel1.get()),
+            "Could not connect to ProfileProvider.");
 
-BEGIN_TEST_CASE(sched_profiles)
-RUN_TEST(get_profile);
-END_TEST_CASE(sched_profiles)
+  // Create a profile.
+  zx::profile profile;
+  zx_status_t status;
+  auto name = std::string("<test>");
+  ASSERT_OK(CreateProfile(channel2, /*priority=*/0u, "<test>", &status, &profile),
+            "Error creating profile.");
+
+  // Ensure basic details are correct.
+  zx_info_handle_basic_t info;
+  ASSERT_OK(profile.get_info(ZX_INFO_HANDLE_BASIC, &info, sizeof(info), nullptr, nullptr),
+            "object_get_info");
+  EXPECT_NE(info.koid, 0, "no koid");
+  EXPECT_EQ(info.type, ZX_OBJ_TYPE_PROFILE, "incorrect type");
+}
+
+}  // namespace
