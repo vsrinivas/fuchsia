1e52ee920ffb57ac5bd7a6658359c52b8afeddf5
Todd Eisenberger <teisenbe@google.com>
2019-07-08 19:24:13 +0000

    [display-controller] Remove unused ioctl declarations
    Change-Id: Ibfa67e7c460135fe6bb9eafdafcb0e3a713bf9a1

--- zircon/system/public/zircon/device/display-controller.h
+++ zircon/system/public/zircon/device/display-controller.h
@@ -6,15 +6,7 @@
 #define SYSROOT_ZIRCON_DEVICE_DISPLAY_CONTROLLER_H_
 
 #include <stdint.h>
-#include <zircon/device/ioctl-wrapper.h>
-#include <zircon/device/ioctl.h>
 #include <zircon/types.h>
-
-#define IOCTL_DISPLAY_CONTROLLER_GET_HANDLE \
-    IOCTL(IOCTL_KIND_GET_HANDLE, IOCTL_FAMILY_DISPLAY_CONTROLLER, 1)
-
-IOCTL_WRAPPER_OUT(ioctl_display_controller_get_handle,
-                  IOCTL_DISPLAY_CONTROLLER_GET_HANDLE, zx_handle_t)
 
 #define IMAGE_TYPE_SIMPLE UINT32_C(0)
 #define INVALID_ID 0
