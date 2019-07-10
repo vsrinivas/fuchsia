f97a9cd15e20c7b57f469faf75ff05d5a715cc5c
Jimmy Wang <wangjimmy@google.com>
2019-07-10 00:23:57 +0000

    [discovermgr] Add function get_discover_cloud_http_headers() that connects to DeviceSettingManager to fetch the device_name.
    MI4-2533 #done
    MI4-2533 #comment Add function get_discover_cloud_http_headers() that connects to DeviceSettingManager to fetch the device_name.
    
    TESTED=fx run-test discovermgr_tests -- --nocapture
    
    Change-Id: I9895ac60e15c06debe6e58444ecdb596702aed2e

--- src/stories/bin/discovermgr/src/cloud_action_provider.rs
+++ src/stories/bin/discovermgr/src/cloud_action_provider.rs
@@ -1,19 +1,55 @@
 // Copyright 2019 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 use {
-    crate::models::Action, failure::Error, fidl_fuchsia_net_oldhttp as http,
-    fuchsia_async as fasync, fuchsia_component as component, fuchsia_syslog::macros::*,
-    fuchsia_zircon as zx, futures::io::AsyncReadExt, serde_derive::Deserialize,
+    crate::models::Action,
+    failure::{bail, Error},
+    fidl_fuchsia_net_oldhttp as http, fuchsia_async as fasync, fuchsia_component as component,
+    fuchsia_syslog::macros::*,
+    fuchsia_zircon as zx,
+    futures::io::AsyncReadExt,
+    serde_derive::Deserialize,
 };
+
+/// Returns a vec of HttpHeaders used for Discover Cloud API request.
+/// Returns error on failure.
+///
+/// Currently, discovermgr uses the device_name as Auth header.
+/// http_headers need "Authorization: ApiKey xxxxxx"
+/// for access authorization. Using the device_name as the apikey
+/// is a hack and should be removed when we decide to use the
+/// logged in user for access authorization.
+///
+async fn get_discover_cloud_http_headers() -> Result<Vec<http::HttpHeader>, Error> {
+    let mut http_headers = vec![http::HttpHeader {
+        name: "Accept".to_string(),
+        value: "application/json".to_string(),
+    }];
+
+    // Connect to device setting manager service
+    let device_settings_manager = component::client::connect_to_service::<
+        fidl_fuchsia_devicesettings::DeviceSettingsManagerMarker,
+    >()?;
+    let (device_name, status) = await!(device_settings_manager.get_string("DeviceName"))?;
+
+    if status != fidl_fuchsia_devicesettings::Status::Ok {
+        bail!("Could not get DeviceName from DeviceSettingsManagerMarker");
+    }
+
+    http_headers.push(http::HttpHeader {
+        name: "Authorization".to_string(),
+        value: format!("ApiKey {}", device_name).to_string(),
+    });
+
+    Ok(http_headers)
+}
 
 /// Fetch the contents of a URL as a string.
 ///
 /// Returns error on failure.
 ///
 /// Connects to the http service, sends a url request, and prints the response.
-async fn http_get(url: &str) -> Result<String, Error> {
+async fn http_get(url: &str, headers: Vec<http::HttpHeader>) -> Result<String, Error> {
     // Connect to the http service
     let net = component::client::connect_to_service::<http::HttpServiceMarker>()?;
 
@@ -25,10 +61,7 @@ async fn http_get(url: &str) -> Result<String, Error> {
     let mut req = http::UrlRequest {
         url: url.to_string(),
         method: "GET".to_string(),
-        headers: Some(vec![http::HttpHeader {
-            name: "Accept".to_string(),
-            value: "application/json".to_string(),
-        }]),
+        headers: Some(headers),
         body: None,
         response_body_buffer_size: 0,
         auto_follow_redirects: true,
@@ -62,8 +95,9 @@ fn serde_from_str(json: &str) -> Result<Vec<Action>, Error> {
 /// Fetch actions from cloud.
 ///
 async fn get_actions_http(url: &str) -> Result<Vec<Action>, Error> {
+    let http_headers = await!(get_discover_cloud_http_headers())?;
     // Fetch the body and parse, returning error messages on failure
-    await!(http_get(url))
+    await!(http_get(url, http_headers))
         .or_else(|_| {
             fx_log_err!("Unable to fetch actions from cloud - ({})", url);
             Err(failure::err_msg(format!("Unable to fetch actions from cloud ({})", url)))
@@ -100,6 +134,22 @@ mod test {
 
         // Check for bad url, expect an Err()
         assert!(await!(get_actions_http("http://example.com")).is_err());
+
+        Ok(())
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn test_get_discover_cloud_http_headers() -> Result<(), Error> {
+        // Check that we can connect to DeviceSettingsManagerMarker service
+        // in get_discover_cloud_http_headers().
+        // When running in test, "DeviceName" isn't set yet in
+        // DeviceSettingsManagerMarker.
+        // Check that we return an appropriate error.
+        let response = await!(get_discover_cloud_http_headers());
+        assert_eq!(
+            response.err().unwrap().to_string(),
+            "Could not get DeviceName from DeviceSettingsManagerMarker"
+        );
 
         Ok(())
     }
