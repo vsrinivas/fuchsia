feed4bb86abbd97a19522e5f8c4e7d99ace28cfc
Nick Maniscalco <maniscalco@google.com>
2019-07-11 14:48:43 -0700

    [zircon][launchpad] Convert launchpad test into a zxtest
    Convert launchpad test into a zxtest.
    
    Example output:
    
    $ /boot/test/sys/launchpad-test
    [==========] Flag Values:
                 --gtest_random_seed = 1562881701
    [==========]
    [==========] Running 3 tests from 2 test cases.
    [----------] Global test environment set-up.
    [----------] 2 tests from LaunchpadTest
    [ RUN      ] LaunchpadTest.Basic
    entry 0x8e61e2ad4360, base 0x8e61e2a7e000, header entry 0x56360
    [       OK ] LaunchpadTest.Basic (41 ms)
    [ RUN      ] LaunchpadTest.Limits
    [       OK ] LaunchpadTest.Limits (781 ms)
    [----------] 2 tests from LaunchpadTest (837 ms total)
    
    [----------] 1 test from LaunchPadTest
    [ RUN      ] LaunchPadTest.ArgumentSize
    [       OK ] LaunchPadTest.ArgumentSize (797 ms)
    [----------] 1 test from LaunchPadTest (805 ms total)
    
    [----------] Global test environment tear-down.
    [==========] 3 tests from 2 test cases ran (1658 ms total).
    [  PASSED  ] 3 tests
    
    Bug: ZX-4510 #done
    Test: /boot/test/sys/launchpad-test
    Change-Id: I794eb28ef7b7b17bbdbf851fb1ea5d314a2392ec

--- zircon/system/ulib/launchpad/test/launchpad.cc
+++ zircon/system/ulib/launchpad/test/launchpad.cc
@@ -7,27 +7,22 @@
 // normally used.
 
 #include <elfload/elfload.h>
-
+#include <fbl/algorithm.h>
+#include <fbl/array.h>
+#include <fbl/auto_call.h>
 #include <launchpad/launchpad.h>
 #include <launchpad/vmo.h>
-
+#include <lib/fdio/directory.h>
+#include <lib/fdio/fd.h>
+#include <lib/fdio/fdio.h>
+#include <lib/zx/handle.h>
+#include <lib/zx/vmo.h>
+#include <limits.h>
 #include <zircon/process.h>
 #include <zircon/processargs.h>
 #include <zircon/syscalls.h>
 #include <zircon/syscalls/object.h>
-#include <limits.h>
-
-#include <fbl/algorithm.h>
-#include <fbl/array.h>
-#include <fbl/auto_call.h>
-
-#include <lib/fdio/fd.h>
-#include <lib/fdio/fdio.h>
-#include <lib/fdio/directory.h>
-#include <lib/zx/handle.h>
-#include <lib/zx/vmo.h>
-
-#include <unittest/unittest.h>
+#include <zxtest/zxtest.h>
 
 // argv[0]
 static const char* program_path;
@@ -42,55 +37,48 @@ static const char dynld_path[] = LIBPREFIX "ld.so.1";
 
 static const char test_inferior_child_name[] = "inferior";
 
-static bool launchpad_test(void) {
-  BEGIN_TEST;
-
+TEST(LaunchpadTest, Basic) {
   launchpad_t* lp = NULL;
 
   zx_handle_t fdio_job = zx_job_default();
   ASSERT_NE(fdio_job, ZX_HANDLE_INVALID, "no fdio job object");
 
   zx_handle_t job_copy = ZX_HANDLE_INVALID;
-  ASSERT_EQ(zx_handle_duplicate(fdio_job, ZX_RIGHT_SAME_RIGHTS, &job_copy), ZX_OK,
+  ASSERT_OK(zx_handle_duplicate(fdio_job, ZX_RIGHT_SAME_RIGHTS, &job_copy),
             "zx_handle_duplicate failed");
 
   zx_status_t status = launchpad_create(job_copy, test_inferior_child_name, &lp);
-  ASSERT_EQ(status, ZX_OK, "launchpad_create");
+  ASSERT_OK(status, "launchpad_create");
 
   zx_handle_t vmo;
-  ASSERT_EQ(launchpad_vmo_from_file(program_path, &vmo), ZX_OK, "");
+  ASSERT_OK(launchpad_vmo_from_file(program_path, &vmo));
   status = launchpad_elf_load(lp, vmo);
-  ASSERT_EQ(status, ZX_OK, "launchpad_elf_load");
+  ASSERT_OK(status, "launchpad_elf_load");
 
   zx_vaddr_t base, entry;
   status = launchpad_get_base_address(lp, &base);
-  ASSERT_EQ(status, ZX_OK, "launchpad_get_base_address");
+  ASSERT_OK(status, "launchpad_get_base_address");
   status = launchpad_get_entry_address(lp, &entry);
-  ASSERT_EQ(status, ZX_OK, "launchpad_get_entry_address");
+  ASSERT_OK(status, "launchpad_get_entry_address");
   ASSERT_GT(base, 0u, "base > 0");
 
   zx_handle_t dynld_vmo = ZX_HANDLE_INVALID;
-  ASSERT_EQ(launchpad_vmo_from_file(dynld_path, &dynld_vmo), ZX_OK, "");
+  ASSERT_OK(launchpad_vmo_from_file(dynld_path, &dynld_vmo));
   ASSERT_NE(dynld_vmo, ZX_HANDLE_INVALID, "launchpad_vmo_from_file");
   elf_load_header_t header;
   uintptr_t phoff;
   status = elf_load_prepare(dynld_vmo, NULL, 0, &header, &phoff);
-  ASSERT_EQ(status, ZX_OK, "elf_load_prepare");
-  unittest_printf("entry %p, base %p, header entry %p\n", (void*)entry, (void*)base,
-                  (void*)header.e_entry);
+  ASSERT_OK(status, "elf_load_prepare");
+  printf("entry %p, base %p, header entry %p\n", (void*)entry, (void*)base, (void*)header.e_entry);
   ASSERT_EQ(entry, base + header.e_entry, "bad value for base or entry");
   zx_handle_close(dynld_vmo);
 
   launchpad_destroy(lp);
-
-  END_TEST;
 }
 
-static bool run_one_argument_size_test(size_t size) {
-  BEGIN_TEST;
-
+void RunOneArgumentSizeTest(size_t size) {
   launchpad_t* lp;
-  ASSERT_EQ(launchpad_create(ZX_HANDLE_INVALID, "argument size test", &lp), ZX_OK, "");
+  ASSERT_OK(launchpad_create(ZX_HANDLE_INVALID, "argument size test", &lp));
 
   char* big = static_cast<char*>(malloc(size + 3));
   big[0] = ':';
@@ -98,42 +86,32 @@ static bool run_one_argument_size_test(size_t size) {
   memset(&big[2], 'x', size);
   big[2 + size] = '\0';
   const char* const argv[] = {"/boot/bin/sh", "-c", big};
-  EXPECT_EQ(launchpad_set_args(lp, fbl::count_of(argv), argv), ZX_OK, "");
+  EXPECT_OK(launchpad_set_args(lp, fbl::count_of(argv), argv));
   free(big);
 
-  EXPECT_EQ(launchpad_load_from_file(lp, argv[0]), ZX_OK, "");
+  EXPECT_OK(launchpad_load_from_file(lp, argv[0]));
 
   zx_handle_t proc = ZX_HANDLE_INVALID;
   const char* errmsg = "???";
-  EXPECT_EQ(launchpad_go(lp, &proc, &errmsg), ZX_OK, errmsg);
+  EXPECT_OK(launchpad_go(lp, &proc, &errmsg), "%s", errmsg);
 
-  EXPECT_EQ(zx_object_wait_one(proc, ZX_PROCESS_TERMINATED, ZX_TIME_INFINITE, NULL), ZX_OK, "");
+  EXPECT_OK(zx_object_wait_one(proc, ZX_PROCESS_TERMINATED, ZX_TIME_INFINITE, NULL));
   zx_info_process_t info;
-  EXPECT_EQ(zx_object_get_info(proc, ZX_INFO_PROCESS, &info, sizeof(info), NULL, NULL), ZX_OK, "");
-  EXPECT_EQ(zx_handle_close(proc), ZX_OK, "");
+  EXPECT_OK(zx_object_get_info(proc, ZX_INFO_PROCESS, &info, sizeof(info), NULL, NULL));
+  EXPECT_OK(zx_handle_close(proc));
 
   EXPECT_EQ(info.return_code, 0, "shell exit status");
-
-  END_TEST;
 }
 
-static bool argument_size_test(void) {
-  bool ok = true;
+TEST(LaunchPadTest, ArgumentSize) {
   for (size_t size = 0; size < 2 * PAGE_SIZE; size += 1024) {
-    if (!run_one_argument_size_test(size)) {
-      ok = false;
-      unittest_printf_critical("    argument size test at %-29zu [FAILED]\n", size);
-    }
+    ASSERT_NO_FAILURES(RunOneArgumentSizeTest(size), "argument size is %-29zu", size);
   }
-  return ok;
 }
 
-static bool run_with_args_env_handles(unsigned int num_args, unsigned int num_env,
-                                      unsigned int num_handles) {
-  BEGIN_TEST;
-
+void RunWithArgsEnvHandles(unsigned int num_args, unsigned int num_env, unsigned int num_handles) {
   launchpad_t* lp;
-  ASSERT_EQ(launchpad_create(ZX_HANDLE_INVALID, "limits test", &lp), ZX_OK, "");
+  ASSERT_OK(launchpad_create(ZX_HANDLE_INVALID, "limits test", &lp));
   auto destroy_launchpad = fbl::MakeAutoCall([&]() { launchpad_destroy(lp); });
 
   // Set the args.
@@ -145,8 +123,8 @@ static bool run_with_args_env_handles(unsigned int num_args, unsigned int num_en
   for (unsigned int i = 3; i < argc; ++i) {
     argv[i] = "-v";
   }
-  ASSERT_EQ(launchpad_set_args(lp, argc, argv.get()), ZX_OK, launchpad_error_message(lp));
-  ASSERT_EQ(launchpad_load_from_file(lp, argv[0]), ZX_OK, launchpad_error_message(lp));
+  ASSERT_OK(launchpad_set_args(lp, argc, argv.get()), "%s", launchpad_error_message(lp));
+  ASSERT_OK(launchpad_load_from_file(lp, argv[0]), "%s", launchpad_error_message(lp));
 
   // Set the env.
   //
@@ -157,17 +135,17 @@ static bool run_with_args_env_handles(unsigned int num_args, unsigned int num_en
     envp[i] = "A=B";
   }
   envp[num_env - 1] = NULL;
-  ASSERT_EQ(launchpad_set_environ(lp, envp.get()), ZX_OK, launchpad_error_message(lp));
+  ASSERT_OK(launchpad_set_environ(lp, envp.get()), "%s", launchpad_error_message(lp));
 
   // Set some handles.
   zx::vmo vmo;
   zx_status_t status = zx::vmo::create(0, 0, &vmo);
-  ASSERT_EQ(status, ZX_OK, "");
+  ASSERT_OK(status);
   for (unsigned int i = 0; i < num_handles; ++i) {
     zx::vmo vmo_dup;
     zx_status_t status = vmo.duplicate(ZX_RIGHT_SAME_RIGHTS, &vmo_dup);
-    ASSERT_EQ(status, ZX_OK, "");
-    ASSERT_EQ(launchpad_add_handle(lp, vmo_dup.release(), PA_HND(PA_USER0, i)), ZX_OK,
+    ASSERT_OK(status);
+    ASSERT_OK(launchpad_add_handle(lp, vmo_dup.release(), PA_HND(PA_USER0, i)), "%s",
               launchpad_error_message(lp));
   }
 
@@ -175,41 +153,27 @@ static bool run_with_args_env_handles(unsigned int num_args, unsigned int num_en
   zx::handle proc;
   const char* err = "unknown error";
   destroy_launchpad.cancel();
-  ASSERT_EQ(launchpad_go(lp, proc.reset_and_get_address(), &err), ZX_OK, err);
+  ASSERT_OK(launchpad_go(lp, proc.reset_and_get_address(), &err), "%s", err);
 
   // See that it completed successfully.
-  ASSERT_EQ(zx_object_wait_one(proc.get(), ZX_PROCESS_TERMINATED, ZX_TIME_INFINITE, NULL), ZX_OK,
-            "");
+  ASSERT_OK(zx_object_wait_one(proc.get(), ZX_PROCESS_TERMINATED, ZX_TIME_INFINITE, NULL));
   zx_info_process_t info;
-  ASSERT_EQ(zx_object_get_info(proc.get(), ZX_INFO_PROCESS, &info, sizeof(info), NULL, NULL), ZX_OK,
-            "");
+  ASSERT_OK(zx_object_get_info(proc.get(), ZX_INFO_PROCESS, &info, sizeof(info), NULL, NULL));
   ASSERT_EQ(info.return_code, 0, "shell exit status");
-
-  END_TEST;
 }
 
-static bool launchpad_limits_test(void) {
-  BEGIN_TEST;
-  EXPECT_TRUE(run_with_args_env_handles(1, 1, 1), "");
-  EXPECT_TRUE(run_with_args_env_handles(10000, 1, 1), "");
-  EXPECT_TRUE(run_with_args_env_handles(1, 10000, 1), "");
-  EXPECT_TRUE(run_with_args_env_handles(58, 58, 58), "");
-  EXPECT_TRUE(run_with_args_env_handles(1, 1, 58), "");
-  EXPECT_TRUE(run_with_args_env_handles(5000, 10000, 0), "");
-  EXPECT_TRUE(run_with_args_env_handles(5000, 10000, 58), "");
-  END_TEST;
+TEST(LaunchpadTest, Limits) {
+  ASSERT_NO_FAILURES(RunWithArgsEnvHandles(1, 1, 1));
+  ASSERT_NO_FAILURES(RunWithArgsEnvHandles(10000, 1, 1));
+  ASSERT_NO_FAILURES(RunWithArgsEnvHandles(1, 10000, 1));
+  ASSERT_NO_FAILURES(RunWithArgsEnvHandles(58, 58, 58));
+  ASSERT_NO_FAILURES(RunWithArgsEnvHandles(1, 1, 58));
+  ASSERT_NO_FAILURES(RunWithArgsEnvHandles(5000, 10000, 0));
+  ASSERT_NO_FAILURES(RunWithArgsEnvHandles(5000, 10000, 58));
 }
 
-BEGIN_TEST_CASE(launchpad_tests)
-RUN_TEST(launchpad_test);
-RUN_TEST(argument_size_test);
-RUN_TEST(launchpad_limits_test);
-END_TEST_CASE(launchpad_tests)
-
+// Providing our own main() because we want to pass argv[0] to one of the tests via program_path.
 int main(int argc, char** argv) {
   program_path = argv[0];
-
-  bool success = unittest_run_all_tests(argc, argv);
-
-  return success ? 0 : -1;
+  return RUN_ALL_TESTS(argc, argv);
 }
