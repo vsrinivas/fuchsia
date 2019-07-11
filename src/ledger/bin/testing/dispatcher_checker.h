4008c48ec1a58168a5752d8c77e1aa8fa65e1155
Gabriel Kerneis <kerneis@google.com>
2019-07-11 08:42:13 +0000

    [ledger] Add DispatcherChecker class
    DispatcherChecker allows for a check similar to fxl::ThreadChecker in
    the specific context of Ledger, where multiple dispatchers are used,
    each attached to a given thread in production, but all bound to a single
    thread in tests for deterministic scheduling.
    
    Change-Id: Ia6c7be753a435b29d41796e5ebc576e987b9d8ec

---
+++ src/ledger/bin/testing/dispatcher_checker.h
@@ -0,0 +1,45 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_LEDGER_BIN_TESTING_DISPATCHER_CHECKER_H_
+#define SRC_LEDGER_BIN_TESTING_DISPATCHER_CHECKER_H_
+
+#include <lib/async/default.h>
+
+#include "src/lib/fxl/logging.h"
+
+namespace ledger {
+
+// A simple class that records the identity of the default dispatcher for the thread that it was
+// created on, and at later points can tell if the current thread's default dispatcher is the same
+// as its creation thread's. This class is thread-safe.
+//
+// Note that this class is not checking the "current dispatcher", because this information is not
+// available in general. It is still useful when the following conditions are met:
+// - each dispatcher is bound to at most one thread,
+// - every thread (except at most one) has a default dispatcher set,
+// - the default dispatcher for a thread does not change after creation.
+// Under those assumptions (which hold in Ledger), this class can be used to detect concurrency
+// issues in addition to ThreadChecker. It has the benefit of finding issues even in unit tests,
+// where all dispatchers are run on the same thread to emulate deterministic multithreading (thus
+// making ThreadChecker useless).
+class DispatcherChecker final {
+ public:
+  DispatcherChecker() : self_(async_get_default_dispatcher()) {}
+
+  // Not copyable or movable.
+  DispatcherChecker(const DispatcherChecker&) = delete;
+  DispatcherChecker& operator=(const DispatcherChecker&) = delete;
+
+  // Returns true if the current default dispatcher is the same as the default dispatcher when this
+  // object was created and false otherwise.
+  bool IsCreationDispatcherCurrent() const { return async_get_default_dispatcher() == self_; }
+
+ private:
+  async_dispatcher_t* const self_;
+};
+
+}  // namespace ledger
+
+#endif  // SRC_LEDGER_BIN_TESTING_DISPATCHER_CHECKER_H_
---
+++ src/ledger/bin/testing/dispatcher_checker_unittest.cc
@@ -0,0 +1,36 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/ledger/bin/testing/dispatcher_checker.h"
+
+#include <lib/async/cpp/task.h>
+
+#include "gtest/gtest.h"
+#include "src/ledger/bin/testing/test_with_environment.h"
+
+namespace ledger {
+
+using DispatcherCheckerTest = ledger::TestWithEnvironment;
+
+TEST_F(DispatcherCheckerTest, Trivial) {
+  DispatcherChecker checker;
+  EXPECT_TRUE(checker.IsCreationDispatcherCurrent());
+}
+
+TEST_F(DispatcherCheckerTest, MainLoopIsDefault) {
+  DispatcherChecker checker;
+  async::PostTask(environment_.dispatcher(),
+                  [&checker] { EXPECT_TRUE(checker.IsCreationDispatcherCurrent()); });
+  RunLoopUntilIdle();
+}
+
+// TODO(kerneis): enable once https://fuchsia-review.googlesource.com/c/fuchsia/+/299828 has rolled.
+TEST_F(DispatcherCheckerTest, DISABLED_IoLoopIsNotDefault) {
+  DispatcherChecker checker;
+  async::PostTask(environment_.io_dispatcher(),
+                  [&checker] { EXPECT_FALSE(checker.IsCreationDispatcherCurrent()); });
+  RunLoopUntilIdle();
+}
+
+}  // namespace ledger
