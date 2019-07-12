61934b600685a195feaa4a748ccd8ca91386898f
David Greenaway <dgreenaway@google.com>
2019-07-12 02:16:03 +0000

    [zircon][test] Add some basic thread affinity tests.
    We have scheduler stress tests in "zircon/tests/thread_tests.cc", but
    add some simple lightweight tests that can be added to the default suite
    of kernel tests.
    
    Change-Id: I398fb81584bcc2ce0398c0a7aa1f50a0b2f0a024

--- zircon/kernel/kernel/thread_test.cc
+++ zircon/kernel/kernel/thread_test.cc
@@ -7,12 +7,29 @@
 
 #include <assert.h>
 #include <debug.h>
+#include <kernel/atomic.h>
+#include <kernel/cpu.h>
+#include <kernel/mp.h>
 #include <kernel/mutex.h>
 #include <kernel/thread.h>
+#include <ktl/popcount.h>
 #include <lib/unittest/unittest.h>
+#include <zircon/errors.h>
 #include <zircon/types.h>
 
 namespace {
+
+// Wait for condition |cond| to become true, polling with slow exponential backoff
+// to avoid pegging the CPU.
+template <typename F>
+void wait_for_cond(F cond) {
+  zx_duration_t wait_duration = ZX_USEC(1);
+  while (!cond()) {
+    thread_sleep_relative(wait_duration);
+    // Increase wait_duration time by ~10%.
+    wait_duration += (wait_duration / 10u + 1u);
+  }
+}
 
 struct YieldData {
   volatile int* done;
@@ -99,8 +116,101 @@ static bool yield_deboost_test() {
   END_TEST;
 }
 
+bool set_affinity_self_test() {
+  BEGIN_TEST;
+
+  // Our worker thread will attempt to schedule itself onto each core, one at
+  // a time, and ensure it landed in the right location.
+  cpu_mask_t online_cpus = mp_get_online_mask();
+  ASSERT_NE(online_cpus, 0u, "Expected at least one CPU to be online.");
+  auto worker_body = +[](void* arg) -> int {
+    cpu_mask_t& online_cpus = *reinterpret_cast<cpu_mask_t*>(arg);
+    thread_t* const self = get_current_thread();
+
+    for (cpu_num_t c = 0u; c <= highest_cpu_set(online_cpus); c++) {
+      // Skip offline CPUs.
+      if ((cpu_num_to_mask(c) & online_cpus) == 0) {
+        continue;
+      }
+
+      // Set affinity to the given core.
+      thread_set_cpu_affinity(self, cpu_num_to_mask(c));
+
+      // Ensure we are on the correct CPU.
+      const cpu_num_t current_cpu = arch_curr_cpu_num();
+      if (current_cpu != c) {
+        UNITTEST_FAIL_TRACEF("Expected to be running on CPU %u, but actually running on %u.", c,
+                             current_cpu);
+        return ZX_ERR_INTERNAL;
+      }
+    }
+
+    return ZX_OK;
+  };
+  thread_t* worker =
+      thread_create("set_affinity_self_test_worker", worker_body, &online_cpus, DEFAULT_PRIORITY);
+  ASSERT_NONNULL(worker, "thread_create failed.");
+  thread_resume(worker);
+
+  // Wait for the worker thread to test itself.
+  int worker_retcode;
+  ASSERT_EQ(thread_join(worker, &worker_retcode, ZX_TIME_INFINITE), ZX_OK,
+            "Failed to join thread.");
+  EXPECT_EQ(worker_retcode, ZX_OK, "Worker thread failed.");
+
+  END_TEST;
+}
+
+bool set_affinity_other_test() {
+  BEGIN_TEST;
+
+  struct WorkerState {
+    volatile int current_cpu = -1;
+    volatile int should_stop = 0;
+  } state;
+
+  // Start a worker, which reports the CPU it is running on.
+  auto worker_body = [](void* arg) -> int {
+    WorkerState& state = *reinterpret_cast<WorkerState*>(arg);
+    while (atomic_load(&state.should_stop) != 1) {
+      atomic_store(&state.current_cpu, arch_curr_cpu_num());
+    }
+    return 0;
+  };
+  thread_t* worker =
+      thread_create("set_affinity_other_test_worker", worker_body, &state, LOW_PRIORITY);
+  thread_resume(worker);
+
+  // Migrate the worker task amongst different threads.
+  const cpu_mask_t online_cpus = mp_get_online_mask();
+  ASSERT_NE(online_cpus, 0u, "Expected at least one CPU to be online.");
+  for (cpu_num_t c = 0u; c <= highest_cpu_set(online_cpus); c++) {
+    // Skip offline CPUs.
+    if ((cpu_num_to_mask(c) & online_cpus) == 0) {
+      continue;
+    }
+
+    // Set affinity to the given core.
+    thread_set_cpu_affinity(worker, cpu_num_to_mask(c));
+
+    // Wait for it to land on the correct CPU.
+    wait_for_cond(
+        [c, &state]() { return static_cast<cpu_num_t>(atomic_load(&state.current_cpu)) == c; });
+  }
+
+  // Done.
+  atomic_store(&state.should_stop, 1);
+  int worker_retcode;
+  ASSERT_EQ(thread_join(worker, &worker_retcode, ZX_TIME_INFINITE), ZX_OK,
+            "Failed to join thread.");
+
+  END_TEST;
+}
+
 }  // namespace
 
 UNITTEST_START_TESTCASE(thread_tests)
 UNITTEST("yield_deboost_test", yield_deboost_test)
+UNITTEST("set_affinity_self_test", set_affinity_self_test)
+UNITTEST("set_affinity_other_test", set_affinity_other_test)
 UNITTEST_END_TESTCASE(thread_tests, "thread", "thread tests");
