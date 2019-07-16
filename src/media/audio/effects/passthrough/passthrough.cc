ecc90fafa67002f64d5d6370c0cff1338ee205db
Tim Detwiler <tjdetwiler@google.com>
2019-07-16 06:09:31 +0000

    [media][audio_effects] Move passthrough module.
    This moves the default passthrough audio effect from the current
    location at //src/media/audio/audio_core/audiofx to a new 'effects'
    director under //src/medial/audio and at the same time we rename this
    from 'audiofx' to 'passthrough' since the only effect this library
    implements is a simple passthrough effect (output == input for all
    channels).
    
    Test: CQ -- just a code move.
    Change-Id: I870b06e8c6b4ae44f5ca544c61d1990a721152d2

--- src/media/audio/effects/passthrough/passthrough.cc
+++ src/media/audio/effects/passthrough/passthrough.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <sdk/lib/media/audio_dfx/cpp/audio_device_fx.h>
+#include <lib/media/audio_dfx/cpp/audio_device_fx.h>
 #include <string.h>
 
 //
