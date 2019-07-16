39b4d6271c1653ed0c374e41779e73c3b7c7e260
Clayton Wilkinson <wilkinsonclay@google.com>
2019-07-16 00:01:43 +0000

    [docs] Adding snippet regions to hello_rust sample
    This adds [START <name>] ...[END <name>] regions that
    are extracted when publishing fuchsia.dev.
    
    This enables making sure the code snippets in the documentation
    are correct and up to date.
    
    Change-Id: Ib86a96b8b81518c0a79b20adcda8aab6b4b23ba2

--- examples/hello_world/rust/src/main.rs
+++ examples/hello_world/rust/src/main.rs
@@ -4,21 +4,48 @@
 
 #![feature(async_await, await_macro)]
 
-// NOTE: the contents of this file have been copied into documentation located at
-//       //docs/development/languages/rust/testing.md. Please update the snippets in that file if
-//       the contents below are changed.
+// NOTE: The comments that have [START/END ...] in them are used to identify code snippets that
+// appear in the documentation.  Please be aware that changes in these blocks will affect the
+// documentation on fuchsia.dev.
+// You can find the usages in the documenation by running:
+//  find docs zircon/docs -name "*.md" | xargs \
+//  grep "\{% includecode .*gerrit_path=\"examples/hello_world/rust/src/main.rs\""
+
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}, world!", greeting());
 }
 
+fn greeting() -> String {
+    return String::from("Hello");
+}
+
+// [START test_mod]
 #[cfg(test)]
 mod tests {
-    use fuchsia_async as fasync;
+    #[test]
+    fn it_works() {
+        assert_eq!(true, true);
+    }
+}
+// [END test_mod]
 
+#[cfg(test)]
+mod hello_tests {
+    use fuchsia_async as fasync;
+    use crate::greeting;
+
+    // [START async_test]
     #[fasync::run_until_stalled(test)]
     async fn my_test() {
         let some_future = async { 4 };
         assert_eq!(await!(some_future), 4);
+    }
+    // [END async_test]
+
+    #[test]
+    fn greeting_test() {
+        let expected = String::from("Hello");
+        assert_eq!(greeting(), expected)
     }
 }
