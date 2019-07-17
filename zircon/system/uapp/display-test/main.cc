b6218541b0d5de907838ca41c0f981799f7bf439
Pascal Perez <pascallouis@google.com>
2019-07-17 12:14:11 +0000

    [display-test] Fix handling of vsync
    Bug introduced
    https://fuchsia-review.googlesource.com/c/fuchsia/+/290615/8/zircon/system/uapp/display-test/main.cpp#280
    when preparing for ordinal migration.
    
    Fixed in
    https://fuchsia-review.googlesource.com/c/fuchsia/+/299010
    by payamm@ (thanks!).
    
    This CL is aesthetic, and will make it clearer to convert back to a
    switch statement when the ordinal migration is complete.
    
    FIDL-524 #comment
    
    Test: no behavior change
    Change-Id: Icfb1ac710a87157a82d1c884a49ae0a046a28e69

--- zircon/system/uapp/display-test/main.cc
+++ zircon/system/uapp/display-test/main.cc
@@ -277,8 +277,10 @@ zx_status_t wait_for_vsync(const fbl::Vector<fbl::unique_ptr<VirtualLayer>>& lay
             ((fuchsia_hardware_display_ControllerClientOwnershipChangeEvent*)msg.bytes().data())
                 ->has_ownership;
         return ZX_ERR_NEXT;
-    } else if (ordinal != fuchsia_hardware_display_ControllerVsyncOrdinal &&
-               ordinal != fuchsia_hardware_display_ControllerVsyncGenOrdinal) {
+    } else if (ordinal == fuchsia_hardware_display_ControllerVsyncOrdinal &&
+               ordinal == fuchsia_hardware_display_ControllerVsyncGenOrdinal) {
+        // Nothing.
+    } else {
         printf("Unknown ordinal %lu\n", ordinal);
         return ZX_ERR_STOP;
     }
