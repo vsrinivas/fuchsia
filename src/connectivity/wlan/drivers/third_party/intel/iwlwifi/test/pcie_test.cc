5c3666f97b41538a745ce1e47b9bd86a6806b423
Alex Legg <alexlegg@google.com>
2019-07-18 22:07:11 +0000

    [wlan][iwlwifi] Use mock-function library
    The way we used gmock previously is technically not allowed in Fuchsia.
    This CL swaps to mock-function, which requires that we use zxtest.
    
    TEST=fx run-test iwlwifi-test
    BUG=WLAN-895
    
    Change-Id: I7903f1f0413c4640f9507a80711c622d3e8ddf34

--- src/connectivity/wlan/drivers/third_party/intel/iwlwifi/test/pcie_test.cc
+++ src/connectivity/wlan/drivers/third_party/intel/iwlwifi/test/pcie_test.cc
@@ -14,9 +14,10 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
 #include <stdio.h>
+
+#include <lib/mock-function/mock-function.h>
+#include <zxtest/zxtest.h>
 
 extern "C" {
 #include "src/connectivity/wlan/drivers/third_party/intel/iwlwifi/pcie/internal.h"
@@ -31,18 +32,12 @@ struct iwl_trans_pcie_wrapper {
   PcieTest* test;
 };
 
-void write32_wrapper(struct iwl_trans* trans, uint32_t ofs, uint32_t val);
-
-class TransOps {
- public:
-  virtual void write32(uint32_t ofs, uint32_t val) = 0;
-};
-
-class PcieTest : public ::testing::Test, TransOps {
+class PcieTest : public zxtest::Test {
  public:
   PcieTest() {
     trans_ops_.write32 = write32_wrapper;
     trans_ = iwl_trans_alloc(sizeof(struct iwl_trans_pcie_wrapper), &cfg_, &trans_ops_);
+    ASSERT_NE(trans_, nullptr);
     auto wrapper = reinterpret_cast<iwl_trans_pcie_wrapper*>(IWL_TRANS_GET_PCIE_TRANS(trans_));
     wrapper->test = this;
     trans_pcie_ = &wrapper->trans_pcie;
@@ -50,7 +45,11 @@ class PcieTest : public ::testing::Test, TransOps {
 
   ~PcieTest() { iwl_trans_free(trans_); }
 
-  MOCK_METHOD2(write32, void(uint32_t ofs, uint32_t val));
+  mock_function::MockFunction<void, uint32_t, uint32_t> mock_write32_;
+  static void write32_wrapper(struct iwl_trans* trans, uint32_t ofs, uint32_t val) {
+    auto wrapper = reinterpret_cast<iwl_trans_pcie_wrapper*>(IWL_TRANS_GET_PCIE_TRANS(trans));
+    wrapper->test->mock_write32_.Call(ofs, val);
+  }
 
  protected:
   struct iwl_trans* trans_;
@@ -59,36 +58,31 @@ class PcieTest : public ::testing::Test, TransOps {
   struct iwl_trans_ops trans_ops_;
 };
 
-void write32_wrapper(struct iwl_trans* trans, uint32_t ofs, uint32_t val) {
-  auto wrapper = reinterpret_cast<iwl_trans_pcie_wrapper*>(IWL_TRANS_GET_PCIE_TRANS(trans));
-  wrapper->test->write32(ofs, val);
-}
-
 TEST_F(PcieTest, DisableInterrupts) {
-  ASSERT_NE(trans_, nullptr);
+  mock_write32_.ExpectCall(CSR_INT_MASK, 0x00000000);
+  mock_write32_.ExpectCall(CSR_INT, 0xffffffff);
+  mock_write32_.ExpectCall(CSR_FH_INT_STATUS, 0xffffffff);
 
   trans_pcie_->msix_enabled = false;
   set_bit(STATUS_INT_ENABLED, &trans_->status);
-
-  EXPECT_CALL(*this, write32(CSR_INT_MASK, 0x00000000));
-  EXPECT_CALL(*this, write32(CSR_INT, 0xffffffff));
-  EXPECT_CALL(*this, write32(CSR_FH_INT_STATUS, 0xffffffff));
   iwl_disable_interrupts(trans_);
+
   EXPECT_EQ(test_bit(STATUS_INT_ENABLED, &trans_->status), 0);
+  mock_write32_.VerifyAndClear();
 }
 
 TEST_F(PcieTest, DisableInterruptsMsix) {
-  ASSERT_NE(trans_, nullptr);
+  mock_write32_.ExpectCall(CSR_MSIX_FH_INT_MASK_AD, 0xabab);
+  mock_write32_.ExpectCall(CSR_MSIX_HW_INT_MASK_AD, 0x4242);
 
   trans_pcie_->msix_enabled = true;
   trans_pcie_->fh_init_mask = 0xabab;
   trans_pcie_->hw_init_mask = 0x4242;
   set_bit(STATUS_INT_ENABLED, &trans_->status);
-
-  EXPECT_CALL(*this, write32(CSR_MSIX_FH_INT_MASK_AD, 0xabab));
-  EXPECT_CALL(*this, write32(CSR_MSIX_HW_INT_MASK_AD, 0x4242));
   iwl_disable_interrupts(trans_);
+
   EXPECT_EQ(test_bit(STATUS_INT_ENABLED, &trans_->status), 0);
+  mock_write32_.VerifyAndClear();
 }
 
 }  // namespace
