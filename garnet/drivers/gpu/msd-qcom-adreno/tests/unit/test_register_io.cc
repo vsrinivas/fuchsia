52cd8e634d020d697180add9e2a3556f7b740ef3
Craig Stout <cstout@google.com>
2019-07-20 00:34:48 +0000

    [msd-qcom-adreno] Add RegisterIo test
    Change-Id: I386ff8ca27cd94060d19137e6fc8eeeb4595b98c

---
+++ garnet/drivers/gpu/msd-qcom-adreno/tests/unit/test_register_io.cc
@@ -0,0 +1,30 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+#include <helper/platform_device_helper.h>
+#include <magma_util/register_io.h>
+
+TEST(RegisterIo, Read) {
+  magma::PlatformDevice* platform_device = TestPlatformDevice::GetInstance();
+  ASSERT_TRUE(platform_device);
+
+  auto platform_mmio =
+      platform_device->CpuMapMmio(0, magma::PlatformMmio::CACHE_POLICY_UNCACHED_DEVICE);
+  ASSERT_TRUE(platform_mmio);
+
+  auto register_io = std::make_unique<magma::RegisterIo>(std::move(platform_mmio));
+  ASSERT_TRUE(register_io);
+
+  DLOG("reading timestamp...");
+  uint64_t timestamp = register_io->Read32(0x00000400 << 2);
+  timestamp |= static_cast<uint64_t>(register_io->Read32(0x00000401 << 2)) << 32;
+  EXPECT_NE(0u, timestamp);
+  DLOG("read timestamp %lu", timestamp);
+
+  DLOG("reading rbbm status...");
+  uint32_t rbbm_status = register_io->Read32(0x00000210 << 2);
+  DLOG("read rbbm_status 0x%x", rbbm_status);
+  EXPECT_NE(0u, rbbm_status);
+}
