ca907cbfe61ea58966c9d0e8807e0f4ac5119bf7
David Greenaway <dgreenaway@google.com>
2019-07-22 07:45:04 +0000

    [zircon] Trivial: ClangFormat "syscalls/profile.h".
    Change-Id: I6a207a03a7f4d93b1069e99b7122a4ae0d9aa9de

--- zircon/system/public/zircon/syscalls/profile.h
+++ zircon/system/public/zircon/syscalls/profile.h
@@ -9,31 +9,27 @@
 
 __BEGIN_CDECLS
 
-// ask clang format not to mess up the indentation:
-// clang-format off
-
-#define ZX_PROFILE_INFO_SCHEDULER   1
+#define ZX_PROFILE_INFO_SCHEDULER 1
 
 typedef struct zx_profile_scheduler {
-    int32_t priority;
-    uint32_t boost;
-    uint32_t deboost;
-    uint32_t quantum;
+  int32_t priority;
+  uint32_t boost;
+  uint32_t deboost;
+  uint32_t quantum;
 } zx_profile_scheduler_t;
 
-#define ZX_PRIORITY_LOWEST              0
-#define ZX_PRIORITY_LOW                 8
-#define ZX_PRIORITY_DEFAULT             16
-#define ZX_PRIORITY_HIGH                24
-#define ZX_PRIORITY_HIGHEST             31
+#define ZX_PRIORITY_LOWEST 0
+#define ZX_PRIORITY_LOW 8
+#define ZX_PRIORITY_DEFAULT 16
+#define ZX_PRIORITY_HIGH 24
+#define ZX_PRIORITY_HIGHEST 31
 
 typedef struct zx_profile_info {
-    uint32_t type;                  // one of ZX_PROFILE_INFO_
-    union {
-        zx_profile_scheduler_t scheduler;
-    };
+  uint32_t type;  // one of ZX_PROFILE_INFO_
+  union {
+    zx_profile_scheduler_t scheduler;
+  };
 } zx_profile_info_t;
-
 
 __END_CDECLS
 
