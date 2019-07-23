6c936c310349b596053ad2395ab5bd191db498dd
Isma Cortes Vazquez <isma@google.com>
2019-07-23 18:55:25 +0000

    [sl4f_client] Clean audio.dart
    TESTED=ran internal tests that use this
    
    Change-Id: Ib7483de132ddce27ba825958c4ff61e7a6212535

--- sdk/testing/sl4f/client/lib/src/audio.dart
+++ sdk/testing/sl4f/client/lib/src/audio.dart
@@ -15,46 +15,26 @@ class Audio {
   final Sl4f _sl4f;
   final Dump _dump;
 
-  Audio(this._sl4f, [Dump dump])
-      : _dump = dump ?? Dump();
+  Audio(this._sl4f, [Dump dump]) : _dump = dump ?? Dump();
 
   // TODO(perley): Drop this after migrating internal clients.
-  Future<void> putPlayback(File file) async {
-    return await putInputAudio(0, file);
-  }
+  Future<void> putPlayback(File file) => putInputAudio(0, file);
 
   Future<void> putInputAudio(int index, File file) async {
-    final audioBytes = file.readAsBytesSync();
+    final audioBytes = await file.readAsBytes();
     await _sl4f.request('audio_facade.PutInputAudio', {
       'index': index,
       'data': base64Encode(audioBytes),
     });
   }
 
-  Future<void> startInputInjection(int index) async {
-    await _sl4f.request('audio_facade.StartInputInjection', {
-      'index': index,
-    });
-  }
+  Future<void> startInputInjection(int index) =>
+      _sl4f.request('audio_facade.StartInputInjection', {'index': index});
 
-  // TODO(perley): Drop this after migrating internal clients.
-  Future<void> startCapture() async {
-    return await startOutputSave();
-  }
+  Future<void> startOutputSave() =>
+      _sl4f.request('audio_facade.StartOutputSave');
 
-  Future<void> startOutputSave() async {
-    await _sl4f.request('audio_facade.StartOutputSave');
-  }
-
-  // TODO(perley): Drop this after migrating internal clients.
-  Future<AudioTrack> getCapture() async {
-    await stopOutputSave();
-    return await getOutputAudio();
-  }
-
-  Future<void> stopOutputSave() async {
-    await _sl4f.request('audio_facade.StopOutputSave');
-  }
+  Future<void> stopOutputSave() => _sl4f.request('audio_facade.StopOutputSave');
 
   Future<AudioTrack> getOutputAudio({String dumpName}) async {
     // The response body is just base64encoded audio
