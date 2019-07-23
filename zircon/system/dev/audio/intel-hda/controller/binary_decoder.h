34835d7da17d9201c083e158f8a14c4821e770ad
David Greenaway <dgreenaway@google.com>
2019-07-24 09:05:03 +1000

    [intel-hda] Add BinaryDecoder to assist decoding packed data structures.
    The BinaryDecoder provides an interface to safely decode packed data
    structures without resorting to raw pointers, which has historically
    shown to be rather error prone (unaligned accesses, out of bounds memory
    accesses, etc).
    
    Used in the next CL to rework the NHLT parsing logic.
    
    Change-Id: I72e5c7072997ff4e7504e3f484f794b08badf7e9

---
+++ zircon/system/dev/audio/intel-hda/controller/binary_decoder.h
@@ -0,0 +1,130 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ZIRCON_SYSTEM_DEV_AUDIO_INTEL_HDA_CONTROLLER_BINARY_DECODER_H_
+#define ZIRCON_SYSTEM_DEV_AUDIO_INTEL_HDA_CONTROLLER_BINARY_DECODER_H_
+
+#include <cstdint>
+#include <type_traits>
+
+#include <fbl/span.h>
+#include <fbl/string_printf.h>
+#include <intel-hda/utils/status.h>
+#include <intel-hda/utils/status_or.h>
+
+namespace audio::intel_hda {
+
+// Light-weight class for decoding packed structs in a safe manner.
+//
+// Each operation returns either a specified struct or an error
+// indicating that the read would go out of bounds of the original input
+// buffer. Successful reads consume bytes from the buffer, while failed
+// reads don't modify internal state.
+//
+// The code avoids performing unaligned reads.
+//
+// A typical use will be as follows:
+//
+//   // Give the decoder a reference to binary data.
+//   BinaryDecoder decoder(fbl::Span<const uint8_t>(...));
+//
+//   // Read some structures.
+//   StatusOr<int32_t> a = decoder.Read<int32_t>();
+//   StatusOr<MyStruct> b = decoder.Read<MyStruct>();
+//   StatusOr<OtherStruct> c = decoder.Read<OtherStruct>();
+//
+//   // Read off a range of 16 bytes.
+//   StatusOr<fbl::Span<const uint8_t>> bytes = decoder.Read(16);
+//
+//   // Read off a struct that encodes its length as a field.
+//   //
+//   // The result will consist of a "MyStruct" and additional payload data
+//   // as a byte range.
+//   StatusOr<std::tuple<MyStruct, fbl::Span<const uint8_t>>> data
+//       = decoder.VariableLengthRead<MyStruct>(&MyStruct::length);
+//
+class BinaryDecoder {
+ public:
+  explicit BinaryDecoder(fbl::Span<const uint8_t> data) : buffer_(data) {}
+
+  // Read off the given number of bytes from the beginning of the buffer.
+  StatusOr<fbl::Span<const uint8_t>> Read(size_t size) {
+    if (size > buffer_.size_bytes()) {
+      return Status(
+          ZX_ERR_OUT_OF_RANGE,
+          fbl::StringPrintf(
+              "Input data has been truncated: expected %ld bytes, but only %ld available.", size,
+              buffer_.size_bytes()));
+    }
+    fbl::Span<const uint8_t> result = buffer_.subspan(0, size);
+    buffer_ = buffer_.subspan(size);
+    return result;
+  }
+
+  // Fetch a structure of type |T| from the buffer.
+  //
+  // |T| should be a POD type that can be initialized via memcpy().
+  template <typename T>
+  StatusOr<T> Read() {
+    static_assert(std::is_pod<T>::value, "Function only supports POD types.");
+    StatusOr<fbl::Span<const uint8_t>> maybe_bytes = Read(sizeof(T));
+    if (!maybe_bytes.ok()) {
+      return maybe_bytes.status();
+    }
+    fbl::Span<const uint8_t> bytes = maybe_bytes.ValueOrDie();
+    ZX_DEBUG_ASSERT(bytes.size_bytes() == sizeof(T));
+    T result;
+    memcpy(&result, bytes.data(), sizeof(T));
+    return result;
+  }
+
+  // Fetch a variable-length structure of type |T| which is followed by
+  // some number of bytes, specified by a field |F|.
+  //
+  // |T| should be a POD type that can be initialized via memcpy().
+  //
+  // |T::length_field| must be castable to size_t.
+  template <typename T, typename F>
+  StatusOr<std::tuple<T, fbl::Span<const uint8_t>>> VariableLengthRead(F T::*length_field) {
+    fbl::Span<const uint8_t> orig_buffer = buffer_;
+
+    // Read header.
+    auto maybe_header = Read<T>();
+    if (!maybe_header.ok()) {
+      return maybe_header.status();
+    }
+    const auto& header = maybe_header.ValueOrDie();
+
+    // Get the |length| field, and ensure that it covers at least the size
+    // of the header.
+    auto length = static_cast<size_t>(header.*length_field);
+    if (length < sizeof(T)) {
+      // Operations should be atomic: if we fail the second read, restore the
+      // original buffer.
+      buffer_ = orig_buffer;
+
+      return Status(
+          ZX_ERR_OUT_OF_RANGE,
+          fbl::StringPrintf(
+              "Length field shorter than structure type: length field specified as %lu bytes, "
+              "but structure is %lu bytes.",
+              length, sizeof(T)));
+    }
+
+    // Read the rest of the payload.
+    auto maybe_additional_bytes = Read(length - sizeof(T));
+    if (!maybe_additional_bytes.ok()) {
+      return maybe_additional_bytes.status();
+    }
+
+    return std::make_tuple(header, maybe_additional_bytes.ValueOrDie());
+  }
+
+ private:
+  fbl::Span<const uint8_t> buffer_;
+};
+
+}  // namespace audio::intel_hda
+
+#endif  // ZIRCON_SYSTEM_DEV_AUDIO_INTEL-HDA_CONTROLLER_BINARY_DECODE_H_
---
+++ zircon/system/dev/audio/intel-hda/controller/binary_decoder_test.cc
@@ -0,0 +1,137 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "binary_decoder.h"
+
+#include <zircon/errors.h>
+
+#include <array>
+#include <cstdint>
+#include <vector>
+
+#include <zxtest/zxtest.h>
+
+namespace audio::intel_hda {
+namespace {
+
+TEST(BinaryDecoder, Empty) {
+  BinaryDecoder decoder{fbl::Span<uint8_t>()};
+
+  // Empty read.
+  auto empty_read = decoder.Read(0);
+  ASSERT_TRUE(empty_read.ok());
+  EXPECT_EQ(empty_read.ValueOrDie().size(), 0);
+
+  // Non-empty read.
+  ASSERT_EQ(decoder.Read(1).status().code(), ZX_ERR_OUT_OF_RANGE);
+}
+
+TEST(BinaryDecoder, NonEmptyRead) {
+  std::array<uint8_t, 5> buffer;
+  BinaryDecoder decoder{fbl::Span<uint8_t>(buffer.begin(), buffer.size())};
+
+  // Successful read.
+  auto a = decoder.Read(1);
+  ASSERT_TRUE(a.ok());
+  EXPECT_EQ(a.ValueOrDie().size(), 1);
+  EXPECT_EQ(a.ValueOrDie().begin(), &buffer[0]);
+
+  // Another read.
+  auto b = decoder.Read(1);
+  ASSERT_TRUE(b.ok());
+  EXPECT_EQ(b.ValueOrDie().size(), 1);
+  EXPECT_EQ(b.ValueOrDie().begin(), &buffer[1]);
+
+  // Too big a read.
+  auto c = decoder.Read(4);
+  ASSERT_EQ(c.status().code(), ZX_ERR_OUT_OF_RANGE);
+
+  // But we should still be able to read the last three bytes.
+  auto d = decoder.Read(3);
+  ASSERT_TRUE(d.ok());
+  EXPECT_EQ(d.ValueOrDie().size(), 3);
+  EXPECT_EQ(d.ValueOrDie().begin(), &buffer[2]);
+}
+
+TEST(BinaryDecoder, ReadStruct) {
+  struct MyStruct {
+    char a, b;
+  };
+
+  // Can't read from too small a buffer.
+  {
+    std::array<uint8_t, 1> small_buffer = {1};
+    BinaryDecoder decoderA{fbl::Span<uint8_t>(small_buffer.begin(), small_buffer.size())};
+    ASSERT_EQ(decoderA.Read<MyStruct>().status().code(), ZX_ERR_OUT_OF_RANGE);
+  }
+
+  // We should be able to read from a precisely sized buffer.
+  {
+    std::array<uint8_t, 2> correct_buffer = {1, 2};
+
+    BinaryDecoder decoderB{fbl::Span<uint8_t>(correct_buffer.begin(), correct_buffer.size())};
+    auto value = decoderB.Read<MyStruct>();
+    ASSERT_TRUE(value.ok());
+    EXPECT_EQ(value.ValueOrDie().a, 1);
+    EXPECT_EQ(value.ValueOrDie().b, 2);
+    EXPECT_FALSE(decoderB.Read(1).ok());
+  }
+
+  // Reading from the beginning of a buffer is fine too.
+  {
+    std::array<uint8_t, 3> big_buffer = {1, 2, 3};
+
+    BinaryDecoder decoderC{fbl::Span<uint8_t>(big_buffer.begin(), big_buffer.size())};
+    auto value = decoderC.Read<MyStruct>();
+    ASSERT_TRUE(value.ok());
+    EXPECT_EQ(value.ValueOrDie().a, 1);
+    EXPECT_EQ(value.ValueOrDie().b, 2);
+    EXPECT_TRUE(decoderC.Read(1).ok());
+  }
+}
+
+TEST(BinaryDecoder, VarLengthRead) {
+  struct VarLength {
+    char size;
+    char data;
+  };
+
+  // Insufficient data available.
+  {
+    BinaryDecoder d{fbl::Span<uint8_t>()};
+    ASSERT_FALSE((d.VariableLengthRead<VarLength>(&VarLength::size).ok()));
+  }
+
+  // Length is smaller than the header structure.
+  {
+    std::array<uint8_t, 3> buffer = {/*size=*/1, /*data=*/2, /*payload=*/3};
+    BinaryDecoder d{fbl::Span<uint8_t>(buffer.begin(), buffer.size())};
+    EXPECT_EQ(d.VariableLengthRead<VarLength>(&VarLength::size).status().code(),
+              ZX_ERR_OUT_OF_RANGE);
+  }
+
+  // Length is larger than the buffer.
+  {
+    std::array<uint8_t, 3> buffer = {/*size=*/4, /*data=*/2, /*payload=*/3};
+    BinaryDecoder d{fbl::Span<uint8_t>(buffer.begin(), buffer.size())};
+    EXPECT_EQ(d.VariableLengthRead<VarLength>(&VarLength::size).status().code(),
+              ZX_ERR_OUT_OF_RANGE);
+  }
+
+  // Successful read.
+  {
+    std::array<uint8_t, 3> buffer = {/*size=*/3, /*data=*/2, /*payload=*/1};
+    BinaryDecoder d{fbl::Span<uint8_t>(buffer.begin(), buffer.size())};
+    auto maybe_val = d.VariableLengthRead<VarLength>(&VarLength::size);
+    ASSERT_TRUE(maybe_val.ok());
+    auto [val, payload] = maybe_val.ValueOrDie();
+    EXPECT_EQ(val.size, 3);
+    EXPECT_EQ(val.data, 2);
+    EXPECT_EQ(payload.size(), 1);
+    EXPECT_EQ(payload.begin(), &buffer[2]);
+  }
+}
+
+}  // namespace
+}  // namespace audio::intel_hda
