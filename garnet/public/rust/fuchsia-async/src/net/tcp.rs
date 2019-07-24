446d61a54cca8167d7570d256088b24b052da400
Kevin Wells <kevinwells@google.com>
2019-07-24 20:11:46 +0000

    [fuchsia-async] Fix hang in TCP connect
    When connect returns EINPROGRESS, clients are expected to wait for the
    socket to become writable and then query the socket's error state to
    determine if the connection attempt succeeded or failed. This change
    modifies TcpStream to check the error state after the socket becomes
    writable, and adds a test to verify connection attempts to nonexistent
    endpoints do not hang.
    
    Bug: PKG-873 #done
    Change-Id: Iba57c0cd5f3f80e65bf20aa02fd77b48b7539951

--- garnet/public/rust/fuchsia-async/src/net/tcp.rs
+++ garnet/public/rust/fuchsia-async/src/net/tcp.rs
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 use {
-    bytes::{Buf, BufMut},
     crate::net::{set_nonblock, EventedFd},
+    bytes::{Buf, BufMut},
     futures::{
         future::Future,
         io::{AsyncRead, AsyncWrite, Initializer},
@@ -67,7 +67,10 @@ impl TcpListener {
         AcceptStream(self)
     }
 
-    pub fn async_accept(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<(TcpStream, SocketAddr)>> {
+    pub fn async_accept(
+        &mut self,
+        cx: &mut Context<'_>,
+    ) -> Poll<io::Result<(TcpStream, SocketAddr)>> {
         try_ready!(EventedFd::poll_readable(&self.0, cx));
 
         match self.0.as_ref().accept() {
@@ -84,7 +87,8 @@ impl TcpListener {
     }
 
     pub fn from_listener(
-        listener: net::TcpListener, _addr: &SocketAddr,
+        listener: net::TcpListener,
+        _addr: &SocketAddr,
     ) -> io::Result<TcpListener> {
         TcpListener::new(listener)
     }
@@ -98,10 +102,7 @@ impl Future for Acceptor {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let (stream, addr);
         {
-            let listener = self
-                .0
-                .as_mut()
-                .expect("polled an Acceptor after completion");
+            let listener = self.0.as_mut().expect("polled an Acceptor after completion");
             let (s, a) = try_ready!(listener.async_accept(cx));
             stream = s;
             addr = a;
@@ -156,18 +157,16 @@ impl TcpStream {
         set_nonblock(stream.as_raw_fd())?;
         // This is safe because the file descriptor for stream will live as long as the TcpStream.
         let stream = unsafe { EventedFd::new(stream)? };
-        let stream = TcpStream {
-            state: ConnectState::New,
-            stream,
-        };
+        let stream = TcpStream { state: ConnectState::New, stream };
 
-        Ok(Connector {
-            addr,
-            stream: Some(stream),
-        })
+        Ok(Connector { addr, stream: Some(stream) })
     }
 
-    pub fn async_connect(&mut self, addr: &SocketAddr, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+    pub fn async_connect(
+        &mut self,
+        addr: &SocketAddr,
+        cx: &mut Context<'_>,
+    ) -> Poll<io::Result<()>> {
         match self.state {
             ConnectState::New => match self.stream.as_ref().connect(addr) {
                 Err(e) => {
@@ -184,16 +183,27 @@ impl TcpStream {
                     Poll::Ready(Ok(()))
                 }
             },
-            ConnectState::Connecting => {
-                self.stream.poll_writable(cx).map(|x| x.map_err(Into::into))
-            }
+            ConnectState::Connecting => self.stream.poll_writable(cx).map(|res| match res {
+                Err(e) => Err(e.into()),
+                Ok(()) => match self.stream.as_ref().take_error() {
+                    Err(err) | Ok(Some(err)) => Err(err),
+                    Ok(None) => {
+                        self.state = ConnectState::Connected;
+                        Ok(())
+                    }
+                },
+            }),
             ConnectState::Connected => {
                 Poll::Ready(Err(io::Error::from_raw_os_error(libc::EISCONN)))
             }
         }
     }
 
-    pub fn read_buf<B: BufMut>(&self, buf: &mut B, cx: &mut Context<'_>) -> Poll<io::Result<usize>> {
+    pub fn read_buf<B: BufMut>(
+        &self,
+        buf: &mut B,
+        cx: &mut Context<'_>,
+    ) -> Poll<io::Result<usize>> {
         match (&self.stream).as_ref().read(unsafe { buf.bytes_mut() }) {
             Ok(n) => {
                 unsafe {
@@ -229,10 +239,7 @@ impl TcpStream {
         // This is safe because the file descriptor for stream will live as long as the TcpStream.
         let stream = unsafe { EventedFd::new(stream)? };
 
-        Ok(TcpStream {
-            state: ConnectState::Connected,
-            stream,
-        })
+        Ok(TcpStream { state: ConnectState::Connected, stream })
     }
 }
 
@@ -285,13 +292,78 @@ impl Future for Connector {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = &mut *self;
         {
-            let stream = this
-                .stream
-                .as_mut()
-                .expect("polled a Connector after completion");
+            let stream = this.stream.as_mut().expect("polled a Connector after completion");
             try_ready!(stream.async_connect(&this.addr, cx));
         }
         let stream = this.stream.take().unwrap();
         Poll::Ready(Ok(stream))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::{TcpListener, TcpStream},
+        crate::Executor,
+        futures::{
+            io::{AsyncReadExt, AsyncWriteExt},
+            stream::StreamExt,
+        },
+        std::io::{Error, ErrorKind},
+    };
+
+    #[test]
+    fn connect_to_unbound_endpoint() {
+        let mut exec = Executor::new().expect("could not create executor");
+
+        let addr = "127.0.0.1:29996".parse().unwrap();
+        let connector = TcpStream::connect(addr).expect("could not create server");
+
+        let fut = async move {
+            let res = await!(connector);
+            assert!(res.is_err());
+            Ok::<(), Error>(())
+        };
+
+        exec.run_singlethreaded(fut).expect("failed to run tcp socket test");
+    }
+
+    #[test]
+    fn send_recv() {
+        let mut exec = Executor::new().expect("could not create executor");
+
+        let addr = "127.0.0.1:29997".parse().unwrap();
+        let query = b"ping";
+        let response = b"pong";
+        let mut listener =
+            TcpListener::bind(&addr).expect("could not create listener").accept_stream();
+
+        let server = async move {
+            let (mut socket, _clientaddr) =
+                await!(listener.next()).expect("stream to not be done").expect("client to connect");
+            drop(listener);
+
+            let mut buf = [0u8; 20];
+            let n = await!(socket.read(&mut buf[..])).expect("server read to succeed");
+            assert_eq!(query, &buf[..n]);
+
+            await!(socket.write_all(&response[..])).expect("server write to succeed");
+
+            let err = await!(socket.read_exact(&mut buf[..])).unwrap_err();
+            assert_eq!(err.kind(), ErrorKind::UnexpectedEof);
+        };
+
+        let client = async move {
+            let connector = TcpStream::connect(addr).expect("could not create server");
+            let mut socket = await!(connector).expect("client to connect to server");
+
+            await!(socket.write_all(&query[..])).expect("client write to succeed");
+
+            let mut buf = [0u8; 20];
+            let n = await!(socket.read(&mut buf[..])).expect("client read to succeed");
+            assert_eq!(response, &buf[..n]);
+        };
+
+        exec.run_singlethreaded(futures::future::join(server, client));
     }
 }
