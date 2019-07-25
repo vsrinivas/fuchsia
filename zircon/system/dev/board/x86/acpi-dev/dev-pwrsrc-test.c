079eb07babd7375714cba7acd0ddba151f200128
Todd Lee <leetodd@google.com>
2019-07-25 17:06:37 +0000

    [acpi-pwrsrc] fix inital state notification
    This fixes an issue where the initial state
    (online/offline) for the AC adapter doesn't
    get triggered.
    
    The first call_PSR was changing state, but
    suppressed notification. The second enabled
    notification, but with no state change, none
    was triggered.
    
    Note that there appears to still be a known
    issue where _PSR state is reported incorrectly
    (0 = online, 1 = offline) for all plug/unplug
    events (AC adapter state is initialized correctly).
    
    Change also includes test to validate signaling
    on PSR state change.
    
    Test: fx shell runtests -t x86-pwrsrc-test
    
    Change-Id: I9711eaa7f4aabc5f9626c90df8254d053fbce045

---
+++ zircon/system/dev/board/x86/acpi-dev/dev-pwrsrc-test.c
@@ -0,0 +1,102 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "dev-pwrsrc.c"
+
+#include <dirent.h>
+#include <zircon/syscalls/port.h>
+
+#include <zxtest/zxtest.h>
+
+#define SIGNAL_WAIT_TIMEOUT (5000u)
+
+acpi_pwrsrc_device_t* dev;
+
+ACPI_STATUS AcpiFakeEvaluatePsrOnline(ACPI_HANDLE handle, char* key, ACPI_OBJECT_LIST* args,
+                                      ACPI_BUFFER* buffer) {
+  if (strcmp("_PSR", key) == 0) {  // device power source state
+    ACPI_OBJECT* obj = buffer->Pointer;
+    EXPECT_NOT_NULL(obj);
+    obj->Integer.Value = POWER_STATE_ONLINE;
+  } else {
+    return AE_ERROR;
+  }
+  return AE_OK;
+}
+
+void create_pwrsrc_device(void) {
+  dev = calloc(1, sizeof(acpi_pwrsrc_device_t));
+  EXPECT_NOT_NULL(dev, "Failed to allocate memory for pwrsrc device");
+
+  dev->acpi_handle = NULL;
+  mtx_init(&dev->lock, mtx_plain);
+
+  zx_status_t status = zx_event_create(0, &dev->event);
+  EXPECT_OK(status);
+
+  dev->info.type = POWER_TYPE_AC;
+  dev->info.state = 0;
+  dev->acpi_eval = &AcpiFakeEvaluatePsrOnline;
+}
+
+void release_pwrsrc_device(void) {
+  if (dev->event != ZX_HANDLE_INVALID) {
+    zx_handle_close(dev->event);
+  }
+  free(dev);
+}
+
+void setup(void) { create_pwrsrc_device(); }
+
+void teardown(void) { release_pwrsrc_device(); }
+
+void verify_psr_state_signal(bool should_signal) {
+  zx_handle_t port;
+  zx_status_t status = zx_port_create(0, &port);
+  EXPECT_OK(status);
+
+  status = zx_object_wait_async(dev->event, port, 0, ZX_USER_SIGNAL_0, ZX_WAIT_ASYNC_ONCE);
+  EXPECT_OK(status);
+
+  call_PSR(dev);
+
+  zx_port_packet_t pkt;
+  status = zx_port_wait(port, zx_deadline_after(ZX_MSEC(SIGNAL_WAIT_TIMEOUT)), &pkt);
+
+  if (should_signal) {
+    ASSERT_OK(status);
+    ASSERT_EQ(ZX_PKT_TYPE_SIGNAL_ONE, pkt.type);
+  } else {
+    ASSERT_EQ(ZX_ERR_TIMED_OUT, status);
+  }
+}
+
+TEST(TestCase, TestSignalOnPsrStateChanged) {
+  setup();
+  // mock PSR eval will always return 'online'
+  // so make sure we start offline
+  dev->info.state &= ~POWER_STATE_ONLINE;
+  verify_psr_state_signal(true);
+  teardown();
+}
+
+TEST(TestCase, TestNoSignalOnPsrStateUnchanged) {
+  setup();
+  // mock PSR eval will always return 'online'
+  // so here we set the same.
+  dev->info.state |= POWER_STATE_ONLINE;
+  verify_psr_state_signal(false);
+  teardown();
+}
+
+// required stubs for faking ddk
+zx_driver_rec_t __zircon_driver_rec__ = {};
+
+void driver_printf(uint32_t flags, const char* fmt, ...) {}
+
+const char* device_get_name(zx_device_t* device) { return "fake-acpi-pwrsrc"; }
+
+zx_status_t device_add_from_driver(zx_driver_t* drv, zx_device_t* parent, device_add_args_t* args,
+                                   zx_device_t** out) {
+  return ZX_OK;
+}
--- zircon/system/dev/board/x86/acpi-dev/dev-pwrsrc.c
+++ zircon/system/dev/board/x86/acpi-dev/dev-pwrsrc.c
@@ -1,13 +1,7 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <ddk/binding.h>
-#include <ddk/debug.h>
-#include <ddk/device.h>
-#include <ddk/driver.h>
-
-#include <acpica/acpi.h>
 #include <fuchsia/hardware/power/c/fidl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,89 +9,101 @@
 #include <zircon/syscalls.h>
 #include <zircon/types.h>
 
+#include <acpica/acpi.h>
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/device.h>
+#include <ddk/driver.h>
+
 #include "dev.h"
 #include "errors.h"
 #include "power.h"
 
+// function pointer for testability, used to mock out AcpiEvaluateObject where necessary
+typedef ACPI_STATUS (*AcpiObjectEvalFunc)(ACPI_HANDLE, char*, ACPI_OBJECT_LIST*, ACPI_BUFFER*);
+
 typedef struct acpi_pwrsrc_device {
-    zx_device_t* zxdev;
+  zx_device_t* zxdev;
 
-    ACPI_HANDLE acpi_handle;
+  ACPI_HANDLE acpi_handle;
 
-    // event to notify on
-    zx_handle_t event;
+  // event to notify on
+  zx_handle_t event;
 
-    power_info_t info;
+  power_info_t info;
 
-    mtx_t lock;
+  mtx_t lock;
+
+  AcpiObjectEvalFunc acpi_eval;
+
 } acpi_pwrsrc_device_t;
 
-static zx_status_t call_PSR(acpi_pwrsrc_device_t* dev, bool notify) {
-    ACPI_OBJECT obj = {
-        .Type = ACPI_TYPE_INTEGER,
-    };
-    ACPI_BUFFER buffer = {
-        .Length = sizeof(obj),
-        .Pointer = &obj,
-    };
-    ACPI_STATUS acpi_status = AcpiEvaluateObject(dev->acpi_handle, (char*)"_PSR", NULL, &buffer);
-    if (acpi_status == AE_OK) {
-        mtx_lock(&dev->lock);
-        uint32_t state = dev->info.state;
-        if (obj.Integer.Value) {
-            dev->info.state |= POWER_STATE_ONLINE;
-        } else {
-            dev->info.state &= ~POWER_STATE_ONLINE;
-        }
-        zxlogf(DEBUG1, "acpi-pwrsrc: 0x%x -> 0x%x\n", state, dev->info.state);
-        if (notify && (state != dev->info.state)) {
-            zx_object_signal(dev->event, 0, ZX_USER_SIGNAL_0);
-        }
-        mtx_unlock(&dev->lock);
+static zx_status_t call_PSR(acpi_pwrsrc_device_t* dev) {
+  ACPI_OBJECT obj = {
+      .Type = ACPI_TYPE_INTEGER,
+  };
+  ACPI_BUFFER buffer = {
+      .Length = sizeof(obj),
+      .Pointer = &obj,
+  };
+  ACPI_STATUS acpi_status = dev->acpi_eval(dev->acpi_handle, (char*)"_PSR", NULL, &buffer);
+  if (acpi_status == AE_OK) {
+    mtx_lock(&dev->lock);
+    uint32_t state = dev->info.state;
+    if (obj.Integer.Value) {
+      dev->info.state |= POWER_STATE_ONLINE;
+    } else {
+      dev->info.state &= ~POWER_STATE_ONLINE;
     }
-    return acpi_to_zx_status(acpi_status);
+    zxlogf(TRACE, "acpi-pwrsrc: call_PSR state change 0x%x -> 0x%x\n", state, dev->info.state);
+    if (state != dev->info.state) {
+      zx_object_signal(dev->event, 0, ZX_USER_SIGNAL_0);
+    }
+    mtx_unlock(&dev->lock);
+  }
+  return acpi_to_zx_status(acpi_status);
 }
 
 static void acpi_pwrsrc_notify(ACPI_HANDLE handle, UINT32 value, void* ctx) {
-    acpi_pwrsrc_device_t* dev = ctx;
-    zxlogf(TRACE, "acpi-pwrsrc: got event 0x%x\n", value);
-    call_PSR(dev, true);
+  acpi_pwrsrc_device_t* dev = ctx;
+  zxlogf(TRACE, "acpi-pwrsrc: notify got event 0x%x\n", value);
+  call_PSR(dev);
 }
 
 static void acpi_pwrsrc_release(void* ctx) {
-    acpi_pwrsrc_device_t* dev = ctx;
-    AcpiRemoveNotifyHandler(dev->acpi_handle, ACPI_DEVICE_NOTIFY, acpi_pwrsrc_notify);
-    if (dev->event != ZX_HANDLE_INVALID) {
-        zx_handle_close(dev->event);
-    }
-    free(dev);
+  acpi_pwrsrc_device_t* dev = ctx;
+  AcpiRemoveNotifyHandler(dev->acpi_handle, ACPI_DEVICE_NOTIFY, acpi_pwrsrc_notify);
+  if (dev->event != ZX_HANDLE_INVALID) {
+    zx_handle_close(dev->event);
+  }
+  free(dev);
 }
 
 zx_status_t fidl_pwrsrc_get_power_info(void* ctx, fidl_txn_t* txn) {
-    acpi_pwrsrc_device_t* dev = ctx;
-    struct fuchsia_hardware_power_SourceInfo info;
+  acpi_pwrsrc_device_t* dev = ctx;
+  struct fuchsia_hardware_power_SourceInfo info;
 
-    mtx_lock(&dev->lock);
-    info.state = dev->info.state;
-    info.type = dev->info.type;
-    mtx_unlock(&dev->lock);
+  mtx_lock(&dev->lock);
+  info.state = dev->info.state;
+  info.type = dev->info.type;
+  mtx_unlock(&dev->lock);
 
-    // reading state clears the signal
-    zx_object_signal(dev->event, ZX_USER_SIGNAL_0, 0);
-    return fuchsia_hardware_power_SourceGetPowerInfo_reply(txn, ZX_OK, &info);
+  // reading state clears the signal
+  zx_object_signal(dev->event, ZX_USER_SIGNAL_0, 0);
+  return fuchsia_hardware_power_SourceGetPowerInfo_reply(txn, ZX_OK, &info);
 }
 
 zx_status_t fidl_pwrsrc_get_state_change_event(void* ctx, fidl_txn_t* txn) {
-    acpi_pwrsrc_device_t* dev = ctx;
-    zx_handle_t out_handle;
-    zx_rights_t rights = ZX_RIGHT_WAIT | ZX_RIGHT_TRANSFER;
-    zx_status_t status = zx_handle_duplicate(dev->event, rights, &out_handle);
+  acpi_pwrsrc_device_t* dev = ctx;
+  zx_handle_t out_handle;
+  zx_rights_t rights = ZX_RIGHT_WAIT | ZX_RIGHT_TRANSFER;
+  zx_status_t status = zx_handle_duplicate(dev->event, rights, &out_handle);
 
-    if (status == ZX_OK) {
-        // clear the signal before returning
-        zx_object_signal(dev->event, ZX_USER_SIGNAL_0, 0);
-    }
-    return fuchsia_hardware_power_SourceGetStateChangeEvent_reply(txn, status, out_handle);
+  if (status == ZX_OK) {
+    // clear the signal before returning
+    zx_object_signal(dev->event, ZX_USER_SIGNAL_0, 0);
+  }
+  return fuchsia_hardware_power_SourceGetStateChangeEvent_reply(txn, status, out_handle);
 }
 
 static fuchsia_hardware_power_Source_ops_t fidl_ops = {
@@ -107,7 +113,7 @@ static fuchsia_hardware_power_Source_ops_t fidl_ops = {
 
 static zx_status_t fuchsia_hardware_power_message_instance(void* ctx, fidl_msg_t* msg,
                                                            fidl_txn_t* txn) {
-    return fuchsia_hardware_power_Source_dispatch(ctx, txn, msg, &fidl_ops);
+  return fuchsia_hardware_power_Source_dispatch(ctx, txn, msg, &fidl_ops);
 }
 
 static zx_protocol_device_t acpi_pwrsrc_device_proto = {
@@ -117,50 +123,55 @@ static zx_protocol_device_t acpi_pwrsrc_device_proto = {
 };
 
 zx_status_t pwrsrc_init(zx_device_t* parent, ACPI_HANDLE acpi_handle) {
-    acpi_pwrsrc_device_t* dev = calloc(1, sizeof(acpi_pwrsrc_device_t));
-    if (!dev) {
-        return ZX_ERR_NO_MEMORY;
-    }
+  // driver trace logging can be enabled for debug as needed
+  // driver_set_log_flags(driver_get_log_flags() | DDK_LOG_TRACE);
 
-    dev->acpi_handle = acpi_handle;
-    mtx_init(&dev->lock, mtx_plain);
+  acpi_pwrsrc_device_t* dev = calloc(1, sizeof(acpi_pwrsrc_device_t));
+  if (!dev) {
+    return ZX_ERR_NO_MEMORY;
+  }
 
-    zx_status_t status = zx_event_create(0, &dev->event);
-    if (status != ZX_OK) {
-        free(dev);
-        return status;
-    }
+  dev->acpi_handle = acpi_handle;
+  mtx_init(&dev->lock, mtx_plain);
 
-    dev->info.type = POWER_TYPE_AC;
-    call_PSR(dev, false);
+  zx_status_t status = zx_event_create(0, &dev->event);
+  if (status != ZX_OK) {
+    free(dev);
+    return status;
+  }
 
-    ACPI_STATUS acpi_status = AcpiInstallNotifyHandler(acpi_handle, ACPI_DEVICE_NOTIFY,
-                                                       acpi_pwrsrc_notify, dev);
-    if (acpi_status != AE_OK) {
-        zxlogf(ERROR, "acpi-pwrsrc: could not install notify handler\n");
-        acpi_pwrsrc_release(dev);
-        return acpi_to_zx_status(acpi_status);
-    }
+  dev->info.type = POWER_TYPE_AC;
 
-    // read initial value
-    acpi_pwrsrc_notify(acpi_handle, 0, dev);
+  // use real AcpiEvaluateObject
+  dev->acpi_eval = &AcpiEvaluateObject;
 
-    device_add_args_t args = {
-        .version = DEVICE_ADD_ARGS_VERSION,
-        .name = "acpi-pwrsrc",
-        .ctx = dev,
-        .ops = &acpi_pwrsrc_device_proto,
-        .proto_id = ZX_PROTOCOL_POWER,
-    };
+  ACPI_STATUS acpi_status =
+      AcpiInstallNotifyHandler(acpi_handle, ACPI_DEVICE_NOTIFY, acpi_pwrsrc_notify, dev);
 
-    status = device_add(parent, &args, &dev->zxdev);
-    if (status != ZX_OK) {
-        zxlogf(ERROR, "acpi-pwrsrc: could not add device! err=%d\n", status);
-        acpi_pwrsrc_release(dev);
-        return status;
-    }
+  call_PSR(dev);
 
-    zxlogf(TRACE, "acpi-pwrsrc: initialized\n");
+  if (acpi_status != AE_OK) {
+    zxlogf(ERROR, "acpi-pwrsrc: could not install notify handler\n");
+    acpi_pwrsrc_release(dev);
+    return acpi_to_zx_status(acpi_status);
+  }
 
-    return ZX_OK;
+  device_add_args_t args = {
+      .version = DEVICE_ADD_ARGS_VERSION,
+      .name = "acpi-pwrsrc",
+      .ctx = dev,
+      .ops = &acpi_pwrsrc_device_proto,
+      .proto_id = ZX_PROTOCOL_POWER,
+  };
+
+  status = device_add(parent, &args, &dev->zxdev);
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "acpi-pwrsrc: could not add device! err=%d\n", status);
+    acpi_pwrsrc_release(dev);
+    return status;
+  }
+
+  zxlogf(TRACE, "acpi-pwrsrc: initialized\n");
+
+  return ZX_OK;
 }
