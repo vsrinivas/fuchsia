38325e561066f8e0c197235e434beeff85096214
Tim Detwiler <tjdetwiler@google.com>
2019-07-25 22:17:19 +0000

    [virtio_wl][tests] Add test fixture for virtio_wl tests.
    This is built as a secondary package from the rest of the guest
    integration tests until the termina_guest stabilizes in CQ. This new
    package will not run in CQ but will allow the tests to be developed an
    run manually for the time being.
    
    Test: $ fx set core.x64 --with src/virtualization/tests:termina
          $ fx run-test termina_integration_tests
    Change-Id: Ia35e18f486c5fc01c42d596e6147e82bc1f05018

---
+++ src/virtualization/tests/virtio_wl_tests.cc
@@ -0,0 +1,10 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "guest_test.h"
+
+using VirtioWlGuestTest = GuestTest<TerminaEnclosedGuest>;
+
+// Placeholder to demonstrate the fixture is functional. This should be replaced with a proper test.
+TEST_F(VirtioWlGuestTest, Sanity) {}
