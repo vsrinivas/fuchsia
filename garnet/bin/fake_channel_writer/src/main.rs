cde40822ba6d2a954a057283d330ff19287d583d
Sen Jiang <senj@google.com>
2019-07-25 21:59:26 +0000

    [fuchsia.update] Define ChannelControl API.
    Also a fake implementation that holds the data in memory.
    
    Bug: OMCL-58 #done
    Test: fx run-test omaha_client_service_bin_tests
    
    Change-Id: Iee732c7562bf8e3e439b0fece0f1b4566a5f4a0a

--- garnet/bin/fake_channel_writer/src/main.rs
+++ garnet/bin/fake_channel_writer/src/main.rs
@@ -5,43 +5,73 @@
 #![feature(async_await, await_macro)]
 
 use failure::{Error, ResultExt};
-use fidl_fuchsia_update::{ChannelWriterRequest, ChannelWriterRequestStream, Slot};
+use fidl_fuchsia_update::{
+    ChannelControlRequest, ChannelControlRequestStream, ChannelWriterRequest,
+    ChannelWriterRequestStream, Slot,
+};
 use fuchsia_async as fasync;
 use fuchsia_component::server::ServiceFs;
 use futures::prelude::*;
 
 enum IncomingServices {
     ChannelWriter(ChannelWriterRequestStream),
+    ChannelControl(ChannelControlRequestStream),
 }
 
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), Error> {
     let mut fs = ServiceFs::new_local();
-    fs.dir("svc").add_fidl_service(IncomingServices::ChannelWriter);
+    fs.dir("svc")
+        .add_fidl_service(IncomingServices::ChannelWriter)
+        .add_fidl_service(IncomingServices::ChannelControl);
     fs.take_and_serve_directory_handle()?;
 
     let mut current_slot = Slot::A;
     let mut current_data_blob: Option<Vec<u8>> = None;
-    while let Some(IncomingServices::ChannelWriter(mut stream)) = await!(fs.next()) {
-        while let Some(request) =
-            await!(stream.try_next()).context("error receiving ChannelWriter request")?
-        {
-            match request {
-                ChannelWriterRequest::GetChannelData { responder } => {
-                    let mut data_blob = current_data_blob.as_ref().map(|v| v.iter().cloned());
-                    responder.send(
-                        current_slot,
-                        data_blob
-                            .as_mut()
-                            .map(|i| i as &mut dyn std::iter::ExactSizeIterator<Item = u8>),
-                    )?;
+    let mut target_channel = String::new();
+    while let Some(service) = await!(fs.next()) {
+        match service {
+            IncomingServices::ChannelWriter(mut stream) => {
+                while let Some(request) =
+                    await!(stream.try_next()).context("error receiving ChannelWriter request")?
+                {
+                    match request {
+                        ChannelWriterRequest::GetChannelData { responder } => {
+                            let mut data_blob =
+                                current_data_blob.as_ref().map(|v| v.iter().cloned());
+                            responder.send(
+                                current_slot,
+                                data_blob
+                                    .as_mut()
+                                    .map(|i| i as &mut dyn std::iter::ExactSizeIterator<Item = u8>),
+                            )?;
+                        }
+                        ChannelWriterRequest::SetChannelData { slot, data_blob, responder } => {
+                            // In this fake implementation we also change the current slot for testing
+                            // purpose, but in production SetChannelData will not change current slot.
+                            current_slot = slot;
+                            current_data_blob = Some(data_blob);
+                            responder.send(&mut Ok(()))?;
+                        }
+                    }
                 }
-                ChannelWriterRequest::SetChannelData { slot, data_blob, responder } => {
-                    // In this fake implementation we also change the current slot for testing
-                    // purpose, but in production SetChannelData will not change current slot.
-                    current_slot = slot;
-                    current_data_blob = Some(data_blob);
-                    responder.send(&mut Ok(()))?;
+            }
+            IncomingServices::ChannelControl(mut stream) => {
+                while let Some(request) =
+                    await!(stream.try_next()).context("error receiving ChannelControl request")?
+                {
+                    match request {
+                        ChannelControlRequest::GetChannel { responder } => {
+                            responder.send("fake-current-channel")?;
+                        }
+                        ChannelControlRequest::GetTarget { responder } => {
+                            responder.send(&target_channel)?;
+                        }
+                        ChannelControlRequest::SetTarget { channel, responder } => {
+                            target_channel = channel;
+                            responder.send()?;
+                        }
+                    }
                 }
             }
         }
--- garnet/bin/omaha_client/src/channel.rs
+++ garnet/bin/omaha_client/src/channel.rs
@@ -4,13 +4,13 @@
 
 #[cfg(test)]
 mod tests {
-    use fidl_fuchsia_update::{ChannelWriterMarker, Slot};
+    use fidl_fuchsia_update::{ChannelControlMarker, ChannelWriterMarker, Slot};
     use fuchsia_async as fasync;
+    use fuchsia_component::client::connect_to_service;
 
     #[fasync::run_singlethreaded(test)]
     async fn test_fake_channel_writer() {
-        let writer =
-            fuchsia_component::client::connect_to_service::<ChannelWriterMarker>().unwrap();
+        let writer = connect_to_service::<ChannelWriterMarker>().unwrap();
 
         let data = vec![1, 2, 3];
         let result = await!(writer.set_channel_data(Slot::A, &mut data.iter().cloned())).unwrap();
@@ -25,6 +25,19 @@ mod tests {
         let (slot, actual_data) = await!(writer.get_channel_data()).unwrap();
         assert_eq!(Slot::B, slot);
         assert_eq!(Some(data_b), actual_data);
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn test_fake_channel_control() {
+        let control = connect_to_service::<ChannelControlMarker>().unwrap();
+
+        await!(control.set_target("test-target-channel")).unwrap();
+        assert_eq!("test-target-channel", await!(control.get_target()).unwrap());
+        assert_eq!("fake-current-channel", await!(control.get_channel()).unwrap());
+
+        await!(control.set_target("test-target-channel-2")).unwrap();
+        assert_eq!("test-target-channel-2", await!(control.get_target()).unwrap());
+        assert_eq!("fake-current-channel", await!(control.get_channel()).unwrap());
     }
 
 }
