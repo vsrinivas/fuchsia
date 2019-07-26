05be9fe2ee485f272fccbc3877dc529d58394ea2
Isma Cortes Vazquez <isma@google.com>
2019-07-26 00:35:51 +0000

    [sl4f] Add a logging service interface.
    Allows adding logs to the device's logs.
    
    TESTED=Ran unit test
    
    Change-Id: I61fe257c1ec8cf6aede41db5b30023421af44145

---
+++ sdk/testing/sl4f/client/lib/src/device_log.dart
@@ -0,0 +1,26 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+import 'sl4f_client.dart';
+
+/// Manipulate the device's logs.
+///
+/// For now it can only write to the device's logs.
+class DeviceLog {
+  final Sl4f _sl4f;
+
+  DeviceLog(this._sl4f);
+
+  /// Writes to the logs of the device at Error severity.
+  Future<void> error(String message) =>
+      _sl4f.request('logging.LogErr', {'message': message});
+
+  /// Writes to the logs of the device at Info severity.
+  Future<void> info(String message) =>
+      _sl4f.request('logging.LogInfo', {'message': message});
+
+  /// Writes to the logs of the device at Warning severity.
+  Future<void> warn(String message) =>
+      _sl4f.request('logging.LogWarn', {'message': message});
+}
---
+++ sdk/testing/sl4f/client/test/device_log_test.dart
@@ -0,0 +1,43 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:sl4f/sl4f.dart';
+import 'package:test/test.dart';
+
+void main(List<String> args) {
+  HttpServer fakeServer;
+  Sl4f sl4f;
+
+  setUp(() async {
+    fakeServer = await HttpServer.bind('127.0.0.1', 18081);
+    sl4f = Sl4f('127.0.0.1:18081', null);
+  });
+
+  tearDown(() async {
+    await fakeServer.close();
+    sl4f.close();
+  });
+
+  group(DeviceLog, () {
+    test('error', () {
+      void handler(HttpRequest req) async {
+        expect(req.contentLength, greaterThan(0));
+        final body = jsonDecode(await utf8.decodeStream(req));
+        expect(body['method'], 'logging.LogErr');
+        expect(body['params'].keys.single, 'message');
+        expect(body['params']['message'], 'hi');
+        req.response.write(
+            jsonEncode({'id': body['id'], 'result': 'Success', 'error': null}));
+        await req.response.close();
+      }
+
+      fakeServer.listen(handler);
+
+      expect(DeviceLog(sl4f).error('hi'), completes);
+    });
+  });
+}
