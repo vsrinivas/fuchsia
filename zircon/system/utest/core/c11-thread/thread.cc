c94e6b75a92f24abfdcc331809ee1250743992a5
Marty Faltesek <mfaltesek@google.com>
2019-07-28 23:15:29 +0000

    [zircon][core-test] Migrate c11-thread-test to zxtest
    Also add in cpp constructs.
    
    ZX-3714
    
    Test: runtests -t c11-thread-test -r 100
    
    Change-Id: I81b28ac64a4dc3d7058a4927b130dc0069e19597

--- zircon/system/utest/core/c11-thread/thread.cc
+++ zircon/system/utest/core/c11-thread/thread.cc
@@ -3,116 +3,174 @@
 // found in the LICENSE file.
 
 #include <errno.h>
-#include <inttypes.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sched.h>
 #include <threads.h>
-
 #include <zircon/syscalls.h>
 #include <zircon/threads.h>
-#include <unittest/unittest.h>
 
-static volatile int threads_done[7];
+#include <zxtest/zxtest.h>
 
-static int thread_entry(void* arg) {
-  int thread_number = (int)(intptr_t)arg;
-  errno = thread_number;
-  unittest_printf("thread %d sleeping for .1 seconds\n", thread_number);
-  zx_nanosleep(zx_deadline_after(ZX_MSEC(100)));
-  EXPECT_EQ(errno, thread_number, "errno changed by someone!");
-  threads_done[thread_number] = 1;
-  return thread_number;
+namespace {
+
+TEST(C11ThreadTest, ThreadLocalErrno) {
+  constexpr int kNumThreads = 4;
+  thrd_t thread[kNumThreads];
+
+  struct Args {
+    int thread_number;
+    int final_errno;
+  } args[kNumThreads];
+
+  auto ThreadLocalHelper = [](void* arg) -> int {
+    auto args = static_cast<Args*>(arg);
+    errno = args->thread_number;
+    zx::nanosleep(zx::deadline_after(zx::msec(100)));
+    args->final_errno = errno;
+
+    return args->thread_number;
+  };
+
+  for (int i = 0; i < kNumThreads; ++i) {
+    args[i].thread_number = i;
+    ASSERT_EQ(thrd_success,
+              thrd_create_with_name(&thread[i], ThreadLocalHelper, &args[i], "c11 thread test"));
+  }
+  for (int i = 0; i < kNumThreads; ++i) {
+    int return_value = 99;
+    ASSERT_EQ(thrd_join(thread[i], &return_value), thrd_success);
+    ASSERT_EQ(return_value, i);
+    ASSERT_EQ(args[i].final_errno, i);
+  }
 }
 
-bool c11_thread_test(void) {
-  BEGIN_TEST;
+TEST(C11ThreadTest, NullNameThreadShouldSucceed) {
+  auto NameHelper = [](void* arg) -> int { return 0; };
+
+  constexpr auto kNullArg = nullptr;
+  constexpr auto kNullName = nullptr;
+  constexpr int* kIgnoreRet = nullptr;
+  thrd_t thread;
+  ASSERT_EQ(thrd_create_with_name(&thread, NameHelper, kNullArg, kNullName), thrd_success);
+  ASSERT_EQ(thrd_join(thread, kIgnoreRet), thrd_success);
+}
+
+TEST(C11ThreadTest, CreateAndVerifyThreadHandle) {
+  constexpr int kRandomRet = 5;
+
+  auto ThreadHandleHelper = [](void* arg) -> int {
+    std::atomic<bool>* keep_running = static_cast<std::atomic<bool>*>(arg);
+    while (*keep_running) {
+      zx::nanosleep(zx::time::infinite_past());
+    }
+    return kRandomRet;
+  };
 
   thrd_t thread;
-  int return_value = 99;
-
-  unittest_printf("Welcome to thread test!\n");
-
-  memset((void*)threads_done, 0, sizeof(threads_done));
-  for (int i = 0; i != 4; ++i) {
-    int return_value = 99;
-    int ret = thrd_create_with_name(&thread, thread_entry, (void*)(intptr_t)i, "c11 thread test");
-    ASSERT_EQ(ret, thrd_success, "Error while creating thread");
-
-    ret = thrd_join(thread, &return_value);
-    ASSERT_EQ(ret, thrd_success, "Error while thread join");
-    ASSERT_EQ(return_value, i, "Incorrect return from thread");
-  }
-
-  unittest_printf("Attempting to create thread with a null name. This should succeed\n");
-  int ret = thrd_create_with_name(&thread, thread_entry, (void*)(intptr_t)4, NULL);
-  ASSERT_EQ(ret, thrd_success, "Error returned from thread creation");
+  std::atomic<bool> keep_running = true;
+  ASSERT_EQ(thrd_create(&thread, ThreadHandleHelper, &keep_running), thrd_success);
   zx_handle_t handle = thrd_get_zx_handle(thread);
   ASSERT_NE(handle, ZX_HANDLE_INVALID, "got invalid thread handle");
   // Prove this is a valid handle by duplicating it.
   zx_handle_t dup_handle;
-  zx_status_t status = zx_handle_duplicate(handle, ZX_RIGHT_SAME_RIGHTS, &dup_handle);
-  ASSERT_EQ(status, 0, "failed to duplicate thread handle");
-
-  ret = thrd_join(thread, &return_value);
-  ASSERT_EQ(ret, thrd_success, "Error while thread join");
-  ASSERT_EQ(zx_handle_close(dup_handle), ZX_OK, "failed to close duplicate handle");
-  ASSERT_EQ(return_value, 4, "Incorrect return from thread");
-
-  ret = thrd_create_with_name(&thread, thread_entry, (void*)(intptr_t)5, NULL);
-  ASSERT_EQ(ret, thrd_success, "Error returned from thread creation");
-  ret = thrd_detach(thread);
-  ASSERT_EQ(ret, thrd_success, "Error while thread detach");
-
-  while (!threads_done[5])
-    zx_nanosleep(zx_deadline_after(ZX_MSEC(100)));
-
-  thread_entry((void*)(intptr_t)6);
-  ASSERT_TRUE(threads_done[6], "All threads should have completed");
-
-  END_TEST;
+  ASSERT_OK(zx_handle_duplicate(handle, ZX_RIGHT_SAME_RIGHTS, &dup_handle));
+  ASSERT_OK(zx_handle_close(dup_handle), "failed to close duplicate handle");
+  keep_running = false;
+  int return_value;
+  ASSERT_EQ(thrd_join(thread, &return_value), thrd_success);
+  ASSERT_EQ(return_value, kRandomRet, "Incorrect return from thread");
 }
 
-bool long_name_succeeds(void) {
-  BEGIN_TEST;
+TEST(C11ThreadTest, DetachedThreadKeepsRunning) {
+  constexpr zx::duration kWaitEachIteration = zx::usec(10);
+  constexpr zx::duration kWaitMax = zx::usec(20000);
 
-  // Creating a thread with a super long name should succeed.
-  static const char long_name[] =
-      "0123456789012345678901234567890123456789"
-      "0123456789012345678901234567890123456789";
-  ASSERT_GT(strlen(long_name), (size_t)ZX_MAX_NAME_LEN - 1, "too short to truncate");
+  struct Args {
+    std::atomic<bool> keep_running;
+    std::atomic<bool> thread_done;
+    std::atomic<int> thread_iterations;
+    const zx::duration kWaitEachIteration;
+  } args = {true, false, 0, kWaitEachIteration};
+
+  auto HandleHelper = [](void* arg) -> int {
+    Args* args = static_cast<Args*>(arg);
+
+    while (args->keep_running.load()) {
+      zx::nanosleep(zx::deadline_after(args->kWaitEachIteration));
+      args->thread_iterations.fetch_add(1);
+    }
+    args->thread_done.store(true);
+    return 0;
+  };
 
   thrd_t thread;
-  int ret = thrd_create_with_name(&thread, thread_entry, (void*)(intptr_t)0, long_name);
-  ASSERT_EQ(ret, thrd_success, "long name should have succeeded");
+  ASSERT_EQ(thrd_create(&thread, HandleHelper, &args), thrd_success);
+
+  ASSERT_EQ(thrd_detach(thread), thrd_success);
+
+  // observe the thread is still operating
+  int recorded_thread_iterations = args.thread_iterations.load();
+  zx::duration time_waited;
+  while (recorded_thread_iterations == args.thread_iterations.load() && time_waited < kWaitMax) {
+    zx::nanosleep(zx::deadline_after(kWaitEachIteration));
+    time_waited += kWaitEachIteration;
+  }
+  ASSERT_EQ(args.thread_done.load(), false);
+
+  args.keep_running.store(false);
+
+  time_waited = zx::duration();
+  while (!args.thread_done.load() && time_waited < kWaitMax) {
+    zx::nanosleep(zx::deadline_after(kWaitEachIteration));
+    time_waited += kWaitEachIteration;
+  }
+  ASSERT_TRUE(args.thread_done.load());
+}
+
+TEST(C11ThreadTest, LongNameSucceeds) {
+  auto LongNameHelper = [](void* arg) { return 0; };
+
+  // Creating a thread with a super long name should succeed.
+  constexpr char kLongName[] =
+      "0123456789012345678901234567890123456789"
+      "0123456789012345678901234567890123456789";
+  ASSERT_GT(strlen(kLongName), static_cast<size_t>(ZX_MAX_NAME_LEN - 1), "too short to truncate");
+
+  thrd_t thread;
+  ASSERT_EQ(thrd_create_with_name(&thread, LongNameHelper, nullptr, kLongName), thrd_success);
 
   // Clean up.
-  int return_value;
-  EXPECT_EQ(thrd_join(thread, &return_value), thrd_success, "");
-  END_TEST;
+  EXPECT_EQ(thrd_join(thread, nullptr), thrd_success);
 }
 
-static int detach_thrd(void* arg) {
-  BEGIN_HELPER;
-  thrd_t* thrd = (thrd_t*)arg;
-  EXPECT_EQ(thrd_detach(*thrd), 0, "");
-  free(thrd);
-  END_HELPER;
-}
+TEST(C11ThreadTest, SelfDetachAndFree) {
+  constexpr int kNumThreads = 1000;
+  std::atomic<int> num_threads_completed = 0;
 
-bool detach_self_test(void) {
-  BEGIN_TEST;
+  struct Args {
+    thrd_t* thread;
+    std::atomic<int> detach_status;
+    std::atomic<int>* num_threads_completed;
+  } args[kNumThreads];
 
-  for (size_t i = 0; i < 1000; i++) {
-    thrd_t* thrd = static_cast<thrd_t*>(calloc(sizeof(thrd_t), 1));
-    ASSERT_NONNULL(thrd, "");
-    ASSERT_EQ(thrd_create(thrd, detach_thrd, thrd), 0, "");
+  auto DetachHelper = [](void* arg) -> int {
+    Args* args = static_cast<Args*>(arg);
+    args->detach_status.store(thrd_detach(*args->thread));
+    delete args->thread;
+    args->num_threads_completed->fetch_add(1);
+    return 0;
+  };
+
+  for (size_t i = 0; i < kNumThreads; i++) {
+    args[i].thread = new thrd_t;
+    args[i].num_threads_completed = &num_threads_completed;
+    ASSERT_EQ(thrd_create(args[i].thread, DetachHelper, &args[i]), thrd_success);
   }
-
-  END_TEST;
+  while (num_threads_completed.load() != kNumThreads) {
+    sched_yield();
+  }
+  for (size_t i = 0; i < kNumThreads; i++) {
+    ASSERT_EQ(args[i].detach_status.load(), thrd_success);
+  }
 }
 
-BEGIN_TEST_CASE(c11_thread_tests)
-RUN_TEST(c11_thread_test)
-RUN_TEST(long_name_succeeds)
-RUN_TEST(detach_self_test)
-END_TEST_CASE(c11_thread_tests)
+}  // namespace
