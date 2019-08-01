ecbe7bf2d3a2f0a3ef1cb93d4e7cb7b224578d84
Sen Jiang <senj@google.com>
2019-08-01 04:53:43 +0000

    [fuchsia.update] Remove ChannelWriter.
    Bug: PKG-889
    Test: fx build
    Change-Id: I6bb307870d65cade82881b869b8f8f6f5a2bf711

--- garnet/bin/fake_channel_writer/src/main.rs
+++ garnet/bin/fake_channel_writer/src/main.rs
@@ -5,57 +5,24 @@
 #![feature(async_await)]
 
 use failure::{Error, ResultExt};
-use fidl_fuchsia_update::{
-    ChannelControlRequest, ChannelControlRequestStream, ChannelWriterRequest,
-    ChannelWriterRequestStream, Slot,
-};
+use fidl_fuchsia_update::{ChannelControlRequest, ChannelControlRequestStream};
 use fuchsia_async as fasync;
 use fuchsia_component::server::ServiceFs;
 use futures::prelude::*;
 
 enum IncomingServices {
-    ChannelWriter(ChannelWriterRequestStream),
     ChannelControl(ChannelControlRequestStream),
 }
 
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), Error> {
     let mut fs = ServiceFs::new_local();
-    fs.dir("svc")
-        .add_fidl_service(IncomingServices::ChannelWriter)
-        .add_fidl_service(IncomingServices::ChannelControl);
+    fs.dir("svc").add_fidl_service(IncomingServices::ChannelControl);
     fs.take_and_serve_directory_handle()?;
 
-    let mut current_slot = Slot::A;
-    let mut current_data_blob: Option<Vec<u8>> = None;
     let mut target_channel = String::new();
     while let Some(service) = fs.next().await {
         match service {
-            IncomingServices::ChannelWriter(mut stream) => {
-                while let Some(request) =
-                    stream.try_next().await.context("error receiving ChannelWriter request")?
-                {
-                    match request {
-                        ChannelWriterRequest::GetChannelData { responder } => {
-                            let mut data_blob =
-                                current_data_blob.as_ref().map(|v| v.iter().cloned());
-                            responder.send(
-                                current_slot,
-                                data_blob
-                                    .as_mut()
-                                    .map(|i| i as &mut dyn std::iter::ExactSizeIterator<Item = u8>),
-                            )?;
-                        }
-                        ChannelWriterRequest::SetChannelData { slot, data_blob, responder } => {
-                            // In this fake implementation we also change the current slot for testing
-                            // purpose, but in production SetChannelData will not change current slot.
-                            current_slot = slot;
-                            current_data_blob = Some(data_blob);
-                            responder.send(&mut Ok(()))?;
-                        }
-                    }
-                }
-            }
             IncomingServices::ChannelControl(mut stream) => {
                 while let Some(request) =
                     stream.try_next().await.context("error receiving ChannelControl request")?
--- garnet/bin/omaha_client/src/channel.rs
+++ garnet/bin/omaha_client/src/channel.rs
@@ -4,28 +4,9 @@
 
 #[cfg(test)]
 mod tests {
-    use fidl_fuchsia_update::{ChannelControlMarker, ChannelWriterMarker, Slot};
+    use fidl_fuchsia_update::ChannelControlMarker;
     use fuchsia_async as fasync;
     use fuchsia_component::client::connect_to_service;
-
-    #[fasync::run_singlethreaded(test)]
-    async fn test_fake_channel_writer() {
-        let writer = connect_to_service::<ChannelWriterMarker>().unwrap();
-
-        let data = vec![1, 2, 3];
-        let result = writer.set_channel_data(Slot::A, &mut data.iter().cloned()).await.unwrap();
-        assert!(result.is_ok());
-        let (slot, actual_data) = writer.get_channel_data().await.unwrap();
-        assert_eq!(Slot::A, slot);
-        assert_eq!(Some(data), actual_data);
-
-        let data_b = vec![4, 5, 6, 7];
-        let result = writer.set_channel_data(Slot::B, &mut data_b.iter().cloned()).await.unwrap();
-        assert!(result.is_ok());
-        let (slot, actual_data) = writer.get_channel_data().await.unwrap();
-        assert_eq!(Slot::B, slot);
-        assert_eq!(Some(data_b), actual_data);
-    }
 
     #[fasync::run_singlethreaded(test)]
     async fn test_fake_channel_control() {
