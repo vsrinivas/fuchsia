fcc87f9c29257029cf7ce711f21e3c747f8b101e
Gary Miguel <garymm@google.com>
2019-08-01 16:30:29 +0000

    [build] Include {fuchsia,zedboot}.zbi in filesystem_sizes.json
    This will allow the infrastructure to enforce limits
    
    Bug: IN-1146
    Change-Id: I4cb7f568781878916cac77b206778315f92c1fe7

--- build/images/format_filesystem_sizes.py
+++ build/images/format_filesystem_sizes.py
@@ -4,55 +4,100 @@
 # found in the LICENSE file.
 
 import argparse
+import collections
 import json
+import os
 import subprocess
 import sys
 
 
+class CmdSize(object):
+    def __init__(self, name, cmd, limit):
+        self.name = name
+        self.size = subprocess.check_output(cmd).strip()
+        self.limit = limit
+
+
+class PathSize(object):
+    def __init__(self, name, path, limit):
+        self.name = name
+        self.size = os.path.getsize(path)
+        self.limit = limit
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Format FVM size')
+    parser = argparse.ArgumentParser(
+        description='Format filesystem image sizes')
     parser.add_argument('--fvm-blk', help='Path to fvm.sparse.blk file')
     parser.add_argument('--blob-blk', help='Path to blob.blk file')
     parser.add_argument('--data-blk', help='Path to data.blk file')
+    parser.add_argument('--fuchsia-zbi', help='Path to fuchsia.zbi file')
+    parser.add_argument('--zedboot-zbi', help='Path to zedboot.zbi file')
     parser.add_argument('--fvm-tool', help='Path to fvm tool')
     parser.add_argument('--blobfs-tool', help='Path to blobfs tool')
     parser.add_argument('--minfs-tool', help='Path to minfs tool')
     parser.add_argument('--max-fvm-contents-size',
                         help='Total size limit for FVM')
-    parser.add_argument('--max-blob-contents-size', default='0',
+    parser.add_argument('--max-blob-contents-size',
+                        default='0',
                         help='Maximum size for blob contents')
-    parser.add_argument('--max-blob-image-size',  default='0',
+    parser.add_argument('--max-blob-image-size',
+                        default='0',
                         help='Maximum size for blob image')
-    parser.add_argument('--max-data-contents-size', default='0',
+    parser.add_argument('--max-data-contents-size',
+                        default='0',
                         help='Maximum size for data contents')
-    parser.add_argument('--max-data-image-size', default='0',
+    parser.add_argument('--max-data-image-size',
+                        default='0',
                         help='Maximum size for data contents')
+    parser.add_argument('--max-fuchsia-zbi-size',
+                        default='0',
+                        help='Maximum size for fuchsia.zbi')
+    parser.add_argument('--max-zedboot-zbi-size',
+                        default='0',
+                        help='Maximum size for zedboot.zbi')
     parser.add_argument('--output', help='Path to output file')
     args = parser.parse_args()
 
-    blob_tool_prefix = [args.blobfs_tool, args.blob_blk]
-    minfs_tool_prefix = [args.minfs_tool, args.data_blk]
-
-    # 'name', 'tool invocation', 'limit'
-    data_points = [
-        ['fvm/contents_size', [args.fvm_tool, args.fvm_blk, 'size'],
-         args.max_fvm_contents_size],
-        ['blob/contents_size', blob_tool_prefix + ['used-data-size'],
-         args.max_blob_contents_size],
-        ['blob/image_size', blob_tool_prefix + ['used-size'],
-         args.max_blob_image_size],
-        ['data/contents_size', minfs_tool_prefix + ['used-data-size'],
-         args.max_data_contents_size],
-        ['data/image_size', minfs_tool_prefix + ['used-size'],
-         args.max_data_image_size]]
+    data_points = []
+    if args.fvm_blk:
+        data_points.append(
+            CmdSize('fvm/contents_size', [args.fvm_tool, args.fvm_blk, 'size'],
+                    args.max_fvm_contents_size))
+    if args.blob_blk:
+        blob_tool_prefix = [args.blobfs_tool, args.blob_blk]
+        data_points.extend([
+            CmdSize('blob/contents_size',
+                    blob_tool_prefix + ['used-data-size'],
+                    args.max_blob_contents_size),
+            CmdSize('blob/image_size', blob_tool_prefix + ['used-size'],
+                    args.max_blob_image_size)
+        ])
+    if args.data_blk:
+        minfs_tool_prefix = [args.minfs_tool, args.data_blk]
+        data_points.extend([
+            CmdSize('data/contents_size',
+                    minfs_tool_prefix + ['used-data-size'],
+                    args.max_data_contents_size),
+            CmdSize('data/image_size', minfs_tool_prefix + ['used-size'],
+                    args.max_data_image_size)
+        ])
+    if args.fuchsia_zbi:
+        data_points.append(
+            PathSize('fuchsia.zbi', args.fuchsia_zbi,
+                     args.max_fuchsia_zbi_size))
+    if args.zedboot_zbi:
+        data_points.append(
+            PathSize('zedboot.zbi', args.zedboot_zbi,
+                     args.max_zedboot_zbi_size))
 
     data = []
     for d in data_points:
-        value = subprocess.check_output(d[1]).strip()
         data.append({
-            'name': d[0],
-            'value': int(value),
-            'limit': int(d[2])})
+            'name': d.name,
+            'value': int(d.size),
+            'limit': int(d.limit)
+        })
 
     with open(args.output, 'w') as f:
         json.dump(data, f, indent=2)
