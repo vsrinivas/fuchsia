6693fa80f5737cabf58be3413a7e866d07fe44e4
Cristian Donoso <donosoc@google.com>
2019-08-02 21:53:27 +0000

    [fx][debugger][fidlcat] launch_debug_agent now checks for existing agent
    This permits for a user to start a debug agent with special
    configuration and have the fx client connect to it without any changes
    from its normal DX.
    
    TEST=Ran the following commands with no agent and a manual one:
         fx debug
         fx debug --port 5000
         fx fidlcat run fuchsia-pkg://fuchsia.com/echo_client_cpp#meta/echo_client_cpp.cmx
         fx fidlcat --port 5000 run fuchsia-pkg://fuchsia.com/echo_client_cpp#meta/echo_client_cpp.cmx
    
    Change-Id: I91679b50593037cad9dff8159e34b497bad7ab48

--- tools/devshell/contrib/lib/debug.sh
+++ tools/devshell/contrib/lib/debug.sh
@@ -14,32 +14,15 @@ get-fuchsia-device-addr-or-die() {
   echo ${got}
 }
 
-# Starts a debug agent on the target.  Requires $port to be set to the port you
-# want the debug agent to start, and $agent_out to be set to where you want the
-# agent's log output.
-launch_debug_agent() {
-  debug_agent_pkg="fuchsia-pkg://fuchsia.com/debug_agent#meta/debug_agent.cmx"
-
-  fx_ssh_pid=""
-  trap 'kill ${fx_ssh_pid} > /dev/null 2>&1' EXIT
-
-# Get the defaulted device address.
-  target=$(get-fuchsia-device-addr-or-die)
-
-# Leave the SSH connection open. Will be closed on script end.
-# We branch out on whether the user used the verbose-agent flag. If so, we
-# redirect the debug agent output to /dev/null.
-  echo -e "Attempting to start the Debug Agent."
-  fx-command-run "ssh" "${target}" "run ${debug_agent_pkg} --port=${port} ${debug_mode} ${unwind_flag}" > "${agent_out}" 2>&1 &
-  fx_ssh_pid="$!"
-
-# We wait until the debug agent is listening on the given port. We use NC to
-# attemp a tcp connection. This will actually go all the way with the handshake,
-# so the debug agent will think initially that NC is a client. But then it will
-# close the connection and receive the actual client's connection and work fine.
+# Checks to see if a debug agent is listening on ${target}:${port} (must be defined).
+# First argument is how many tries to look for.
+check_for_agent() {
+  # We wait until the debug agent is listening on the given port. We use NC to
+  # attemp a tcp connection. This will actually go all the way with the handshake,
+  # so the debug agent will think initially that NC is a client. But then it will
+  # close the connection and receive the actual client's connection and work fine.
   try_count=0
-  max_tries=10
-  echo "Waiting for the Debug Agent to start."
+  max_tries=$1
   while true; do
     # Use NC to test if the port is open and the debug agent is listening.
     if nc -w5 -6 -z "${target}" "${port}"; then
@@ -49,9 +32,45 @@ launch_debug_agent() {
     # Otherwise, we count and check if we need to exit.
     try_count=$(expr "${try_count}" + 1)
     if [[ "${try_count}" -gt "${max_tries}" ]]; then
-      fx-error "Timed out trying to find the Debug Agent. Exiting."
-      exit 1
+      return 1
     fi
     sleep 1
   done
+
+  return 0
+}
+
+# Starts a debug agent on the target if it's not there.
+# Requires $port to be set to the port you want the debug agent to be,
+# and $agent_out to be set to where you want the agent's log output.
+launch_debug_agent() {
+  debug_agent_pkg="fuchsia-pkg://fuchsia.com/debug_agent#meta/debug_agent.cmx"
+
+  fx_ssh_pid=""
+  trap 'kill ${fx_ssh_pid} > /dev/null 2>&1' EXIT
+
+  # Get the defaulted device address.
+  target=$(get-fuchsia-device-addr-or-die)
+
+  # See if the debug agent is already there.
+  echo "Checking for debug agent on [${target}]:${port}."
+  if check_for_agent 1; then
+    echo "Found agent already running."
+    return 0
+  fi
+
+  # Leave the SSH connection open. Will be closed on script end.
+  # We branch out on whether the user used the verbose-agent flag. If so, we
+  # redirect the debug agent output to /dev/null.
+  echo -e "Debug agent not found. Starting one."
+  fx-command-run "ssh" "${target}" "run ${debug_agent_pkg} --port=${port} ${debug_mode} ${unwind_flag}" > "${agent_out}" 2>&1 &
+  fx_ssh_pid="$!"
+
+  # Wait for the debug agent to start.
+  if check_for_agent 10; then
+    return 0
+  else
+    fx-error "Timed out trying to find the Debug Agent."
+    return 1
+  fi
 }
