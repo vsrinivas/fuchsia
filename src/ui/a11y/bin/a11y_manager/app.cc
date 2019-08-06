7928de5c5351a03ba4e27f19cda52914c7bf2e3b
Ankit Dave <ankitdave@google.com>
2019-08-06 17:29:09 +0000

    [a11y] (1/7)Adding Settings listener and Screen Reader.
    This is the CL-1 in the long chain(Total 7) of CLs for adding Explore action
    support on Screen Reader. This cl does not depend on any other cl to be
    submitted/reviewed.
    
    This cl adds following things:
    * Support for Settings listener in A11y Manager app.
    * Initial Support for ScreenReader class in A11y app.
    
    Testing:
      * Build: (Passed)
      * Unit Test: fx run-test a11y_tests (Passed)
      * A11y Manager app was missing unit tests. I have added unit tests for
      these app.* files along with tests for changes in this cl in
      https://fuchsia-review.googlesource.com/c/fuchsia/+/307653
    
    MI4-1567 #comment
    MI4-2536 #comment
    Change-Id: I73243ca861bc2663d5698689326730c845d8c3fc
    
    Change-Id: Ifc76272bf199fec53e5534e793050ebd55abbd41

--- src/ui/a11y/bin/a11y_manager/app.cc
+++ src/ui/a11y/bin/a11y_manager/app.cc
@@ -4,17 +4,22 @@
 
 #include "src/ui/a11y/bin/a11y_manager/app.h"
 
+#include <lib/syslog/cpp/logger.h>
+#include <zircon/status.h>
+
 namespace a11y_manager {
 
 App::App()
     : startup_context_(sys::ComponentContext::Create()),
       settings_manager_impl_(std::make_unique<SettingsManagerImpl>()),
       semantics_manager_impl_(std::make_unique<SemanticsManagerImpl>()) {
+  // Add Settings Manager service.
   startup_context_->outgoing()->AddPublicService<fuchsia::accessibility::SettingsManager>(
       [this](fidl::InterfaceRequest<fuchsia::accessibility::SettingsManager> request) {
         settings_manager_impl_->AddBinding(std::move(request));
       });
 
+  // Add Semantics Manager service.
   semantics_manager_impl_->SetDebugDirectory(startup_context_->outgoing()->debug_dir());
   startup_context_->outgoing()
       ->AddPublicService<fuchsia::accessibility::semantics::SemanticsManager>(
@@ -22,6 +27,47 @@ App::App()
               fidl::InterfaceRequest<fuchsia::accessibility::semantics::SemanticsManager> request) {
             semantics_manager_impl_->AddBinding(std::move(request));
           });
+
+  // Connect to Settings manager service and register a watcher.
+  settings_manager_impl_->AddBinding(settings_manager_.NewRequest());
+  settings_manager_.set_error_handler([](zx_status_t status) {
+    FX_LOGS(ERROR) << "Cannot connect to SettingsManager with status:"
+                   << zx_status_get_string(status);
+  });
+  fidl::InterfaceHandle<fuchsia::accessibility::SettingsWatcher> watcher_handle;
+  settings_watcher_bindings_.AddBinding(this, watcher_handle.NewRequest());
+  settings_manager_->Watch(std::move(watcher_handle));
+}
+
+void App::SetSettings(fuchsia::accessibility::Settings provided_settings) {
+  settings_.set_magnification_enabled(provided_settings.magnification_enabled());
+  if (provided_settings.has_magnification_zoom_factor()) {
+    settings_.set_magnification_zoom_factor(provided_settings.magnification_zoom_factor());
+  }
+  settings_.set_screen_reader_enabled(provided_settings.screen_reader_enabled());
+  settings_.set_color_inversion_enabled(provided_settings.color_inversion_enabled());
+  settings_.set_color_correction(provided_settings.color_correction());
+  if (provided_settings.has_color_adjustment_matrix()) {
+    settings_.set_color_adjustment_matrix(provided_settings.color_adjustment_matrix());
+  }
+}
+
+void App::OnScreenReaderEnabled(bool enabled) {
+  if (enabled) {
+    screen_reader_ = std::make_unique<a11y::ScreenReader>();
+  } else {
+    screen_reader_.reset();
+  }
+}
+
+void App::OnSettingsChange(fuchsia::accessibility::Settings provided_settings) {
+  // Check if screen reader settings have changed.
+  if (settings_.screen_reader_enabled() != provided_settings.screen_reader_enabled()) {
+    OnScreenReaderEnabled(provided_settings.screen_reader_enabled());
+  }
+
+  // Set A11y Settings.
+  SetSettings(std::move(provided_settings));
 }
 
 }  // namespace a11y_manager
--- src/ui/a11y/bin/a11y_manager/app.h
+++ src/ui/a11y/bin/a11y_manager/app.h
@@ -16,20 +16,42 @@
 #include "src/lib/fxl/macros.h"
 #include "src/ui/a11y/bin/a11y_manager/semantics/semantics_manager_impl.h"
 #include "src/ui/a11y/bin/a11y_manager/settings/settings_manager_impl.h"
+#include "src/ui/a11y/lib/screen_reader/screen_reader.h"
 
 namespace a11y_manager {
 
 // A11y manager application entry point.
-class App {
+class App : public fuchsia::accessibility::SettingsWatcher {
  public:
   App();
   ~App() = default;
 
+  // |fuchsia::accessibility::SettingsWatcher|
+  void OnSettingsChange(fuchsia::accessibility::Settings provided_settings) override;
+
  private:
+  // Helper function to copy given settings to member variable.
+  void SetSettings(fuchsia::accessibility::Settings provided_settings);
+
+  // Initializes Screen Reader pointer when screen reader is enabled, and destroys
+  // the pointer when Screen Reader is disabled.
+  void OnScreenReaderEnabled(bool enabled);
+
   std::unique_ptr<sys::ComponentContext> startup_context_;
 
+  // Pointer to Settings Manager Implementation.
   std::unique_ptr<SettingsManagerImpl> settings_manager_impl_;
+  // Pointer to Semantics Manager Implementation.
   std::unique_ptr<SemanticsManagerImpl> semantics_manager_impl_;
+
+  fidl::BindingSet<fuchsia::accessibility::SettingsWatcher> settings_watcher_bindings_;
+  // Private variable for storing A11y Settings.
+  fuchsia::accessibility::Settings settings_;
+  // Pointer to SettingsManager service, which will be used for connecting App
+  // to settings manager as a Watcher.
+  fuchsia::accessibility::SettingsManagerPtr settings_manager_;
+
+  std::unique_ptr<a11y::ScreenReader> screen_reader_;
 
   FXL_DISALLOW_COPY_AND_ASSIGN(App);
 };
