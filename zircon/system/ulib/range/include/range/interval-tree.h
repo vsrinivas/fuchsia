442cd4b776611cfdaa6612cc3ed2ad8f75a2de88
Sean Klein <smklein@google.com>
2019-08-14 01:14:25 +0000

    [range] Implement an IntervalTree: an associative container holding ranges
    Change-Id: I6edaffc0dc42a7ff8016c0cc87ac77596ea247dc

---
+++ zircon/system/ulib/range/include/range/interval-tree.h
@@ -0,0 +1,181 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RANGE_INTERVAL_TREE_H_
+#define RANGE_INTERVAL_TREE_H_
+
+#include <zircon/assert.h>
+
+#include <map>
+#include <utility>
+
+#include <range/range.h>
+
+namespace range {
+
+// An associative container which holds ranges of values.
+// IntervalTree is capable of holding these ranges of values, but indexing by individual
+// values, instead of by range.
+//
+// This class is thread-compatible.
+template <typename Range>
+class IntervalTree {
+ public:
+  using RangeType = Range;
+  using KeyType = typename Range::KeyType;
+  using MapType = std::map<KeyType, RangeType>;
+  using IterType = typename MapType::iterator;
+  using ConstIterType = typename MapType::const_iterator;
+
+  IntervalTree() = default;
+
+  // Inserts a range of values into the tree. If they overlap with existing ranges, they
+  // are combined with those existing ranges. If this range cannot be combined with
+  // existing ranges, an error is returned.
+  //
+  // Returns true if the range is inserted successfully.
+  // Returns false if the insertion was unsuccessful (This is only possible with an error
+  // propagated from |Range::Container::Update|).
+  //
+  // Runtime: O(log(number of ranges))
+  template <typename RangeType>
+  bool try_insert(RangeType&& range) {
+    // Special case: Set is empty, range is inserted as-is.
+    if (map_.empty()) {
+      map_.insert({range.Start(), std::forward<RangeType>(range)});
+      return true;
+    }
+    auto next = map_.upper_bound(range.Start());
+    auto prior = next;
+
+    // Merge with the subsequent elements, if possible.
+    while (next != map_.end()) {
+      zx_status_t status = range.Merge(next->second);
+      if (status != ZX_OK && range::Overlap(next->second, range)) {
+        // The ranges needed to merge (due to overlap), but could not.
+        return status == ZX_OK;
+      }
+      if (status == ZX_OK) {
+        // The ranges merged.
+        ++next;
+      } else {
+        // The ranges did not (and don't need to) merge.
+        break;
+      }
+    }
+
+    // Merge with the prior elements, if possible.
+    while (prior != map_.begin()) {
+      prior--;
+      zx_status_t status = range.Merge(prior->second);
+      if (status != ZX_OK && range::Overlap(prior->second, range)) {
+        // The ranges needed to merge (due to overlap), but could not.
+        return status == ZX_OK;
+      }
+      if (status == ZX_OK) {
+        // The ranges merged.
+        continue;
+      }
+      // The ranges did not (and don't need to) merge.
+      // Make sure prior only points to things we do need to erase.
+      prior++;
+      break;
+    }
+
+    if (prior != next) {
+      map_.erase(prior, next);
+    }
+    map_.insert({range.Start(), std::forward<RangeType>(range)});
+    return true;
+  }
+
+  // Inserts |range| of values into the tree.
+  //
+  // Precondition: |range| must be either mergeable with overlapping intervals in the tree, or must
+  // not overlap. Callers which cannot satisfy these preconditions should consider |try_insert|
+  // instead.
+  template <typename RangeType>
+  void insert(RangeType&& range) {
+    ZX_ASSERT(try_insert(std::forward<RangeType>(range)));
+  }
+
+  // Erases a single value from the tree. If this value is only part of a range, that
+  // range is split into multiple parts.
+  //
+  // Runtime: O(log(number of ranges))
+  void erase(const KeyType& value) {
+    auto iter = find(value);
+    if (iter == map_.end()) {
+      return;
+    }
+
+    // Remove the entire range containing the value.
+    RangeType range = iter->second;
+    map_.erase(iter);
+
+    // If we cut a range in pieces, put the remaining valid pieces back.
+    typename RangeType::Container prior_container = range.container();
+    ZX_ASSERT(RangeType::ContainerTraits::Update(nullptr, range.Start(), value, &prior_container) ==
+              ZX_OK);
+    RangeType prior(prior_container);
+    ZX_DEBUG_ASSERT(range.Start() == prior.Start());
+    ZX_DEBUG_ASSERT(value == prior.End());
+    if (prior.Length()) {
+      map_.insert({prior.Start(), prior});
+    }
+
+    typename RangeType::Container next_container = range.container();
+    ZX_ASSERT(RangeType::ContainerTraits::Update(nullptr, value + 1, range.End(),
+                                                 &next_container) == ZX_OK);
+    RangeType next(next_container);
+    ZX_DEBUG_ASSERT(value + 1 == next.Start());
+    ZX_DEBUG_ASSERT(range.End() == next.End());
+    if (next.Length()) {
+      map_.insert({next.Start(), next});
+    }
+  }
+
+  // Returns an iterator to a range which contains the value.
+  // If no such range exists, returns |end()|.
+  //
+  // Runtime: O(log(number of ranges))
+  IterType find(const KeyType& value) {
+    if (map_.empty()) {
+      return map_.end();
+    }
+    auto iter = map_.upper_bound(value);
+    if (iter == map_.begin()) {
+      // |range| is less than the first valid element.
+      return map_.end();
+    }
+    iter--;
+    if (iter == map_.end()) {
+      return map_.end();
+    }
+
+    if (iter->second.Start() <= value && value < iter->second.End()) {
+      return iter;
+    }
+    return map_.end();
+  }
+
+  [[nodiscard]] bool empty() const { return map_.empty(); }
+
+  [[nodiscard]] size_t size() const { return map_.size(); }
+
+  IterType begin() { return map_.begin(); }
+
+  ConstIterType begin() const { return map_.begin(); }
+
+  IterType end() { return map_.end(); }
+
+  ConstIterType end() const { return map_.end(); }
+
+ private:
+  MapType map_;
+};
+
+}  // namespace range
+
+#endif  // RANGE_INTERVAL_TREE_H_
---
+++ zircon/system/ulib/range/test/interval-tree-test.cc
@@ -0,0 +1,504 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "range/interval-tree.h"
+
+#include <zxtest/zxtest.h>
+
+namespace range {
+namespace {
+
+using TestRange = range::Range<uint64_t>;
+using TestIntervalTree = IntervalTree<TestRange>;
+
+TEST(IntervalTreeTest, EmptyTreeContainsNoRanges) {
+  TestIntervalTree tree;
+  ASSERT_TRUE(tree.empty());
+  ASSERT_EQ(0, tree.size());
+
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(tree.end(), tree.find(1));
+  EXPECT_EQ(tree.end(), tree.find(10000));
+}
+
+TEST(IntervalTreeTest, InsertOneRangeIncreasesTreeSizeByOne) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(1, 2));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, FindReturnsInsertedRange) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(1, 2));
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 2), tree.find(1)->second);
+  EXPECT_EQ(tree.end(), tree.find(2));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertAdjacentAfterPriorRangeCausesMerge) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(0, 1));
+  EXPECT_EQ(TestRange(0, 1), tree.find(0)->second);
+  tree.insert(TestRange(1, 2));
+  EXPECT_EQ(TestRange(0, 2), tree.find(0)->second);
+  tree.insert(TestRange(2, 3));
+  EXPECT_EQ(TestRange(0, 3), tree.find(0)->second);
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertAdjacentBeforePriorRangeCausesMerge) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(2, 3));
+  EXPECT_EQ(TestRange(2, 3), tree.find(2)->second);
+  tree.insert(TestRange(1, 2));
+  EXPECT_EQ(TestRange(1, 3), tree.find(1)->second);
+  tree.insert(TestRange(0, 1));
+  EXPECT_EQ(TestRange(0, 3), tree.find(0)->second);
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertAdjacentBetweenPriorRangesCausesThreeWayMerge) {
+  TestIntervalTree tree;
+  // Setup tree
+  tree.insert(TestRange(1, 3));
+  tree.insert(TestRange(5, 7));
+
+  // Verify preconditions
+  ASSERT_EQ(tree.end(), tree.find(0));
+  ASSERT_EQ(TestRange(1, 3), tree.find(1)->second);
+  ASSERT_EQ(TestRange(1, 3), tree.find(2)->second);
+  ASSERT_EQ(tree.end(), tree.find(3));
+  ASSERT_EQ(tree.end(), tree.find(4));
+  ASSERT_EQ(TestRange(5, 7), tree.find(5)->second);
+  ASSERT_EQ(TestRange(5, 7), tree.find(6)->second);
+  ASSERT_EQ(tree.end(), tree.find(7));
+  ASSERT_EQ(2, tree.size());
+
+  // Insert range that exactly fits between the two ranges
+  tree.insert(TestRange(3, 5));
+
+  // Verify postconditions
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 7), tree.find(1)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(2)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(3)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(4)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(5)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(6)->second);
+  EXPECT_EQ(tree.end(), tree.find(7));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertOverlapIntersectStartExtendsRange) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(1, 3));
+
+  // Verify preconditions
+  ASSERT_EQ(tree.end(), tree.find(0));
+  ASSERT_EQ(TestRange(1, 3), tree.find(1)->second);
+  ASSERT_EQ(TestRange(1, 3), tree.find(2)->second);
+  ASSERT_EQ(tree.end(), tree.find(3));
+  ASSERT_EQ(1, tree.size());
+
+  // Insert range that overlaps the current range.
+  tree.insert(TestRange(1, 4));
+
+  // Verify postconditions
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 4), tree.find(1)->second);
+  EXPECT_EQ(TestRange(1, 4), tree.find(2)->second);
+  EXPECT_EQ(TestRange(1, 4), tree.find(3)->second);
+  EXPECT_EQ(tree.end(), tree.find(4));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertOverlapWithEndOfPriorRangeExtendsRange) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(1, 3));
+
+  // Verify preconditions
+  ASSERT_EQ(tree.end(), tree.find(0));
+  ASSERT_EQ(TestRange(1, 3), tree.find(1)->second);
+  ASSERT_EQ(TestRange(1, 3), tree.find(2)->second);
+  ASSERT_EQ(tree.end(), tree.find(3));
+  ASSERT_EQ(1, tree.size());
+
+  // Insert range that overlaps the prior range
+  tree.insert(TestRange(2, 4));
+
+  // Verify postconditions
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 4), tree.find(1)->second);
+  EXPECT_EQ(TestRange(1, 4), tree.find(2)->second);
+  EXPECT_EQ(TestRange(1, 4), tree.find(3)->second);
+  EXPECT_EQ(tree.end(), tree.find(4));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertOverlapWithStartOfPriorRangePreExtendsRange) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(2, 4));
+
+  // Verify preconditions
+  ASSERT_EQ(tree.end(), tree.find(1));
+  ASSERT_EQ(TestRange(2, 4), tree.find(2)->second);
+  ASSERT_EQ(TestRange(2, 4), tree.find(3)->second);
+  ASSERT_EQ(tree.end(), tree.find(4));
+  ASSERT_EQ(1, tree.size());
+
+  // Insert range that overlaps the prior range
+  tree.insert(TestRange(1, 3));
+
+  // Verify postconditions
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 4), tree.find(1)->second);
+  EXPECT_EQ(TestRange(1, 4), tree.find(2)->second);
+  EXPECT_EQ(TestRange(1, 4), tree.find(3)->second);
+  EXPECT_EQ(tree.end(), tree.find(4));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertOverlapBetweenTwoPriorRangesCausesThreeWayMerge) {
+  TestIntervalTree tree;
+  // Setup tree
+  tree.insert(TestRange(1, 3));
+  tree.insert(TestRange(5, 7));
+
+  // Verify preconditions
+  ASSERT_EQ(tree.end(), tree.find(0));
+  ASSERT_EQ(TestRange(1, 3), tree.find(1)->second);
+  ASSERT_EQ(TestRange(1, 3), tree.find(2)->second);
+  ASSERT_EQ(tree.end(), tree.find(3));
+  ASSERT_EQ(tree.end(), tree.find(4));
+  ASSERT_EQ(TestRange(5, 7), tree.find(5)->second);
+  ASSERT_EQ(TestRange(5, 7), tree.find(6)->second);
+  ASSERT_EQ(tree.end(), tree.find(7));
+  ASSERT_EQ(2, tree.size());
+
+  // Insert range that exactly overlaps the two ranges.
+  tree.insert(TestRange(2, 6));
+
+  // Verify postconditions
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 7), tree.find(1)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(2)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(3)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(4)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(5)->second);
+  EXPECT_EQ(TestRange(1, 7), tree.find(6)->second);
+  EXPECT_EQ(tree.end(), tree.find(7));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertSubsumingTwoPriorRangesCausesThreeWayMerge) {
+  TestIntervalTree tree;
+  // Setup tree
+  tree.insert(TestRange(2, 4));
+  tree.insert(TestRange(5, 7));
+
+  // Verify preconditions
+  ASSERT_EQ(tree.end(), tree.find(0));
+  ASSERT_EQ(tree.end(), tree.find(1));
+  ASSERT_EQ(TestRange(2, 4), tree.find(2)->second);
+  ASSERT_EQ(TestRange(2, 4), tree.find(3)->second);
+  ASSERT_EQ(tree.end(), tree.find(4));
+  ASSERT_EQ(TestRange(5, 7), tree.find(5)->second);
+  ASSERT_EQ(TestRange(5, 7), tree.find(6)->second);
+  ASSERT_EQ(tree.end(), tree.find(7));
+  ASSERT_EQ(2, tree.size());
+
+  // Insert range that entirely overlaps the two prior requests.
+  tree.insert(TestRange(1, 8));
+
+  // Verify postconditions
+  EXPECT_EQ(1, tree.size());
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 8), tree.find(1)->second);
+  EXPECT_EQ(TestRange(1, 8), tree.find(2)->second);
+  EXPECT_EQ(TestRange(1, 8), tree.find(3)->second);
+  EXPECT_EQ(TestRange(1, 8), tree.find(4)->second);
+  EXPECT_EQ(TestRange(1, 8), tree.find(5)->second);
+  EXPECT_EQ(TestRange(1, 8), tree.find(6)->second);
+  EXPECT_EQ(TestRange(1, 8), tree.find(7)->second);
+  EXPECT_EQ(tree.end(), tree.find(8));
+}
+
+TEST(IntervalTreeTest, InsertSubsumingManyPriorRangesCausesManyWayMerge) {
+  TestIntervalTree tree;
+  // Setup tree
+  tree.insert(TestRange(2, 4));
+  tree.insert(TestRange(5, 7));
+  tree.insert(TestRange(9, 10));
+  tree.insert(TestRange(12, 14));
+
+  // Verify preconditions
+  ASSERT_EQ(4, tree.size());
+
+  // Insert range that entirely overlaps all prior requests.
+  tree.insert(TestRange(1, 15));
+
+  // Verify postconditions
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(TestRange(1, 15), tree.find(1)->second);
+  EXPECT_EQ(tree.end(), tree.find(15));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, InsertAlignedAtStartOfRangeSubsumingManyPriorRangesCausesManyWayMerge) {
+  TestIntervalTree tree;
+  // Setup tree
+  tree.insert(TestRange(2, 4));
+  tree.insert(TestRange(5, 7));
+  tree.insert(TestRange(9, 10));
+  tree.insert(TestRange(12, 14));
+
+  // Verify preconditions
+  ASSERT_EQ(4, tree.size());
+
+  // Insert range that entirely overlaps all prior requests.
+  tree.insert(TestRange(2, 15));
+
+  // Verify postconditions
+  EXPECT_EQ(1, tree.size());
+  EXPECT_EQ(tree.end(), tree.find(1));
+  EXPECT_EQ(TestRange(2, 15), tree.find(2)->second);
+  EXPECT_EQ(tree.end(), tree.find(15));
+}
+
+TEST(IntervalTreeTest, EraseEntireRangeDeletesItCompletely) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(2, 3));
+  ASSERT_EQ(TestRange(2, 3), tree.find(2)->second);
+
+  tree.erase(2);
+
+  EXPECT_EQ(tree.end(), tree.find(1));
+  EXPECT_EQ(tree.end(), tree.find(2));
+  EXPECT_EQ(tree.end(), tree.find(3));
+  EXPECT_EQ(0, tree.size());
+  EXPECT_TRUE(tree.empty());
+}
+
+TEST(IntervalTreeTest, EraseRangePrefixLeavesSuffix) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(2, 4));
+  ASSERT_EQ(TestRange(2, 4), tree.find(2)->second);
+
+  tree.erase(2);
+
+  EXPECT_EQ(tree.end(), tree.find(1));
+  EXPECT_EQ(tree.end(), tree.find(2));
+  EXPECT_EQ(TestRange(3, 4), tree.find(3)->second);
+  EXPECT_EQ(tree.end(), tree.find(4));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, EraseRangeSuffixLeavesPrefix) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(2, 4));
+  ASSERT_EQ(TestRange(2, 4), tree.find(2)->second);
+
+  tree.erase(3);
+
+  EXPECT_EQ(tree.end(), tree.find(1));
+  EXPECT_EQ(TestRange(2, 3), tree.find(2)->second);
+  EXPECT_EQ(tree.end(), tree.find(3));
+  EXPECT_EQ(tree.end(), tree.find(4));
+  EXPECT_EQ(1, tree.size());
+}
+
+TEST(IntervalTreeTest, EraseRangeMiddleLeavesPrefixAndSuffix) {
+  TestIntervalTree tree;
+  tree.insert(TestRange(2, 5));
+  ASSERT_EQ(TestRange(2, 5), tree.find(2)->second);
+
+  tree.erase(3);
+
+  EXPECT_EQ(tree.end(), tree.find(1));
+  EXPECT_EQ(TestRange(2, 3), tree.find(2)->second);
+  EXPECT_EQ(tree.end(), tree.find(3));
+  EXPECT_EQ(TestRange(4, 5), tree.find(4)->second);
+  EXPECT_EQ(tree.end(), tree.find(5));
+  EXPECT_EQ(2, tree.size());
+}
+
+struct RangeContainer {
+  RangeContainer(uint64_t start, uint64_t end, bool merge)
+      : start_(start), end_(end), allow_merge_(merge) {}
+
+  uint64_t start_ = 0;
+  uint64_t end_ = 0;
+  bool allow_merge_ = true;
+};
+
+struct RangeTraits {
+  static uint64_t Start(const RangeContainer& obj) { return obj.start_; }
+  static uint64_t End(const RangeContainer& obj) { return obj.end_; }
+
+  static zx_status_t Update(const RangeContainer* other, uint64_t start, uint64_t end,
+                            RangeContainer* obj) {
+    if (other) {
+      if (!obj->allow_merge_ || !other->allow_merge_) {
+        return ZX_ERR_INTERNAL;
+      }
+    }
+    obj->start_ = start;
+    obj->end_ = end;
+    return ZX_OK;
+  }
+};
+
+using CustomRange = range::Range<uint64_t, RangeContainer, RangeTraits>;
+using CustomTree = IntervalTree<CustomRange>;
+
+TEST(IntervalTreeCustomMergeTest, RejectedInsertSameStartDoesNotModifyTree) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(5, 10, true));
+  CustomRange range2(RangeContainer(5, 15, false));
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_FALSE(tree.try_insert(range2));
+
+  EXPECT_EQ(1, tree.size());
+  EXPECT_EQ(range1, tree.begin()->second);
+}
+
+TEST(IntervalTreeCustomMergeTest, RejectedInsertOverlapPriorDoesNotModifyTree) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(5, 10, true));
+  CustomRange range2(RangeContainer(3, 7, false));
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_FALSE(tree.try_insert(range2));
+
+  EXPECT_EQ(1, tree.size());
+  EXPECT_EQ(range1, tree.begin()->second);
+}
+
+TEST(IntervalTreeCustomMergeTest, RejectedInsertOverlapNextDoesNotModifyTree) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(5, 10, true));
+  CustomRange range2(RangeContainer(7, 12, false));
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_FALSE(tree.try_insert(range2));
+
+  EXPECT_EQ(1, tree.size());
+  EXPECT_EQ(range1, tree.begin()->second);
+}
+
+TEST(IntervalTreeCustomMergeTest, RejectedInsertAdjacentPriorAddsRange) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(5, 10, true));
+  CustomRange range2(RangeContainer(3, 5, false));
+
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_TRUE(tree.try_insert(range2));
+
+  EXPECT_EQ(2, tree.size());
+  auto iter = tree.begin();
+  EXPECT_EQ(range2, iter->second);
+  iter++;
+  EXPECT_EQ(range1, iter->second);
+  iter++;
+  EXPECT_EQ(tree.end(), iter);
+}
+
+TEST(IntervalTreeCustomMergeTest, RejectedInsertAdjacentNextAddsRange) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(5, 10, true));
+  CustomRange range2(RangeContainer(10, 15, false));
+
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_TRUE(tree.try_insert(range2));
+  ASSERT_EQ(2, tree.size());
+
+  auto iter = tree.begin();
+  EXPECT_EQ(range1, iter++->second);
+  EXPECT_EQ(range2, iter++->second);
+  EXPECT_EQ(tree.end(), iter);
+}
+
+TEST(IntervalTreeCustomMergeTest, UnmergedAdjacentRangesAreIndexableByFind) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(1, 3, true));
+  CustomRange range2(RangeContainer(3, 5, false));
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_TRUE(tree.try_insert(range2));
+  ASSERT_EQ(2, tree.size());
+
+  EXPECT_EQ(tree.end(), tree.find(0));
+  EXPECT_EQ(range1, tree.find(1)->second);
+  EXPECT_EQ(range1, tree.find(2)->second);
+  EXPECT_EQ(range2, tree.find(3)->second);
+  EXPECT_EQ(range2, tree.find(4)->second);
+  EXPECT_EQ(tree.end(), tree.find(5));
+}
+
+// [1, 3), [3, 5) + [0, 3) (merged) --> [0, 3), [3, 5)
+TEST(IntervalTreeCustomMergeTest, UnmergedAdjacentRangesCanStillMergeWithPrior) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(1, 3, true));
+  CustomRange range2(RangeContainer(3, 5, false));
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_TRUE(tree.try_insert(range2));
+  ASSERT_EQ(2, tree.size());
+
+  CustomRange range3(RangeContainer(0, 3, true));
+  ASSERT_TRUE(tree.try_insert(range3));
+  EXPECT_EQ(2, tree.size());
+  auto iter = tree.begin();
+  EXPECT_EQ(range3, iter++->second);
+  EXPECT_EQ(range2, iter++->second);
+  EXPECT_EQ(tree.end(), iter);
+}
+
+// [1, 3), [3, 5) + [3, 6) (merged) --> [1, 3), [3, 6)
+TEST(IntervalTreeCustomMergeTest, UnmergedAdjacentRangesCanStillMergeWithNext) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(1, 3, false));
+  CustomRange range2(RangeContainer(3, 5, true));
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_TRUE(tree.try_insert(range2));
+  ASSERT_EQ(2, tree.size());
+
+  CustomRange range3(RangeContainer(3, 6, true));
+  ASSERT_TRUE(tree.try_insert(range3));
+  EXPECT_EQ(2, tree.size());
+  auto iter = tree.begin();
+  EXPECT_EQ(range1, iter++->second);
+  EXPECT_EQ(range3, iter++->second);
+  EXPECT_EQ(tree.end(), iter);
+}
+
+// [1, 3), [3, 5) + [2, 4) (rejected) --> [1, 3), [3, 5)
+TEST(IntervalTreeCustomMergeTest, UnmergedAdjacentRangesCannotMergeOverGap) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(1, 3, true));
+  CustomRange range2(RangeContainer(3, 5, false));
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_TRUE(tree.try_insert(range2));
+  ASSERT_EQ(2, tree.size());
+
+  CustomRange range3(RangeContainer(2, 4, true));
+  ASSERT_FALSE(tree.try_insert(range3));
+  EXPECT_EQ(2, tree.size());
+  auto iter = tree.begin();
+  EXPECT_EQ(range1, iter++->second);
+  EXPECT_EQ(range2, iter++->second);
+  EXPECT_EQ(tree.end(), iter);
+}
+
+TEST(IntervalTreeCustomMergeTest, UnmergeableInsertIsFatal) {
+  CustomTree tree;
+  CustomRange range1(RangeContainer(1, 3, true));
+  CustomRange range2(RangeContainer(2, 5, false));
+
+  ASSERT_TRUE(tree.try_insert(range1));
+  ASSERT_FALSE(tree.try_insert(range2));
+
+  // If "try_insert" returned false, then "insert" will be fatal.
+  ASSERT_DEATH([&] { tree.insert(range2); });
+}
+
+}  // namespace
+}  // namespace range
