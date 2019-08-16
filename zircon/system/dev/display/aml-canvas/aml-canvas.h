f62db495efa2a7c278996d71bc3ac002ea642ca7
Taylor Cramer <cramertj@google.com>
2019-08-16 22:49:40 +0000

    Revert "[display][canvas] Port canvas driver from C to C++"
    This reverts commit f01fb5b4f3a95e0024bc50dfa1ef43b613dc314c.
    
    Reason for revert: causing device enumeration test failures
    
    Original change's description:
    > [display][canvas] Port canvas driver from C to C++
    > 
    > Ported the aml-canvas driver to use the C++ DDK template library.
    > 
    > Test: Added unit test 'aml-canvas-test' to verify device lifecycle and
    > various canvas config and free behaviors. Also tested locally on devices
    > that use the canvas driver. Brought up the virtual console and ran
    > tests: 'gfxtest' and 'display-test --amlogic'. Verified that scenic
    > comes up fine as well.
    > 
    > Change-Id: Iea588d2da97ecfa6a76b09b36fba40e197647f2a
    
    TBR=payamm@google.com,surajmalhotra@google.com,rashaeqbal@google.com
    
    Change-Id: I066590499e4a0923e13e7db493203b35f36f8373
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- zircon/system/dev/display/aml-canvas/aml-canvas.h
+++ zircon/system/dev/display/aml-canvas/aml-canvas.h
@@ -5,56 +5,59 @@
 #ifndef ZIRCON_SYSTEM_DEV_DISPLAY_AML_CANVAS_AML_CANVAS_H_
 #define ZIRCON_SYSTEM_DEV_DISPLAY_AML_CANVAS_AML_CANVAS_H_
 
-#include <lib/device-protocol/pdev.h>
-#include <lib/mmio/mmio.h>
-
-#include <array>
-
+#include <ddk/device.h>
 #include <ddk/mmio-buffer.h>
 #include <ddk/platform-defs.h>
 #include <ddk/protocol/platform/device.h>
-#include <ddktl/device.h>
-#include <ddktl/protocol/amlogiccanvas.h>
-#include <fbl/mutex.h>
+#include <hw/reg.h>
+#include <threads.h>
+
+#define NUM_CANVAS_ENTRIES 256
+#define CANVAS_BYTE_STRIDE 32
 
 #define IS_ALIGNED(a, b) (!(((uintptr_t)(a)) & (((uintptr_t)(b)) - 1)))
 
 #define CANVAS_ERROR(fmt, ...) zxlogf(ERROR, "[%s %d]" fmt, __func__, __LINE__, ##__VA_ARGS__)
 #define CANVAS_INFO(fmt, ...) zxlogf(INFO, "[%s %d]" fmt, __func__, __LINE__, ##__VA_ARGS__)
 
-namespace aml_canvas {
+#define READ32_DMC_REG(a) readl(canvas->dmc_regs.vaddr + a)
+#define WRITE32_DMC_REG(a, v) writel(v, canvas->dmc_regs.vaddr + a)
 
-constexpr size_t kNumCanvasEntries = 256;
+#define DMC_CAV_LUT_DATAL (0x12 << 2)
+#define DMC_CAV_LUT_DATAH (0x13 << 2)
+#define DMC_CAV_LUT_ADDR (0x14 << 2)
 
-class AmlCanvas;
-using DeviceType = ddk::Device<AmlCanvas, ddk::Unbindable>;
+#define DMC_CAV_ADDR_LMASK 0x1fffffff
+#define DMC_CAV_WIDTH_LMASK 0x7
+#define DMC_CAV_WIDTH_LWID 3
+#define DMC_CAV_WIDTH_LBIT 29
 
-class AmlCanvas : public DeviceType,
-                  public ddk::AmlogicCanvasProtocol<AmlCanvas, ddk::base_protocol> {
- public:
-  AmlCanvas(zx_device_t* parent, ddk::MmioBuffer mmio, zx::bti bti)
-      : DeviceType(parent), dmc_regs_(std::move(mmio)), bti_(std::move(bti)) {}
+#define DMC_CAV_WIDTH_HMASK 0x1ff
+#define DMC_CAV_WIDTH_HBIT 0
+#define DMC_CAV_HEIGHT_MASK 0x1fff
+#define DMC_CAV_HEIGHT_BIT 9
 
-  // This function is called from the c-bind function upon driver matching
-  static zx_status_t Setup(zx_device_t* parent);
+#define DMC_CAV_BLKMODE_MASK 3
+#define DMC_CAV_BLKMODE_BIT 24
 
-  // Required by ddk::AmlogicCanvasProtocol
-  zx_status_t AmlogicCanvasConfig(zx::vmo vmo, size_t offset, const canvas_info_t* info,
-                                  uint8_t* canvas_idx);
-  zx_status_t AmlogicCanvasFree(uint8_t canvas_idx);
+#define DMC_CAV_ENDIANNESS_BIT 26
+#define DMC_CAV_ENDIANNESS_MASK 0xf
 
-  // Required by ddk::Device
-  void DdkRelease();
-  void DdkUnbind();
+#define DMC_CAV_LUT_ADDR_INDEX_MASK 0x7
+#define DMC_CAV_LUT_ADDR_RD_EN (1 << 8)
+#define DMC_CAV_LUT_ADDR_WR_EN (2 << 8)
 
- private:
-  fbl::Mutex lock_;
-  ddk::MmioBuffer dmc_regs_ __TA_GUARDED(lock_);
-  zx::bti bti_ __TA_GUARDED(lock_);
-  std::array<zx::pmt, kNumCanvasEntries> pmts_ __TA_GUARDED(lock_);
+#define DMC_CAV_YWRAP (1 << 23)
+#define DMC_CAV_XWRAP (1 << 22)
 
-};  // class AmlCanvas
-
-}  // namespace aml_canvas
+// Context for driver implementation.
+typedef struct {
+  zx_device_t* zxdev;
+  pdev_protocol_t pdev;
+  mmio_buffer_t dmc_regs;
+  mtx_t lock;
+  zx_handle_t bti;
+  zx_handle_t pmt_handle[NUM_CANVAS_ENTRIES];
+} aml_canvas_t;
 
 #endif  // ZIRCON_SYSTEM_DEV_DISPLAY_AML_CANVAS_AML_CANVAS_H_
