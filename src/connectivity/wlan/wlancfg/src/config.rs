cb68e9a2074887dbe5ce90db9af4b994fc2b8107
Eric Wang <eyw@google.com>
2019-08-21 00:49:19 +0000

    [wlancfg] Fuzzy PHY matching for config
    Besides matching PHY paths with exact path string, also support partial
    matching iff the topological path in the config file ends with "/*". In
    the case where an exact match and a partial match with the same length
    both matches the path, the exact match is used.
    
    Bug: 34935
    Test:
    1. Added unit tests for name matching.
    2. All existing tests still pass.
    3. For good measure, verified data transfer with ath10k and brcm.
    4. New code is being tested in the follow up CLs that also passed CQ.
    
    Change-Id: I3a1f9588f6732126fd65d1db8dc01e03cadf8c82

--- src/connectivity/wlan/wlancfg/src/config.rs
+++ src/connectivity/wlan/wlancfg/src/config.rs
@@ -6,7 +6,7 @@ use failure::{Error, ResultExt};
 use fidl_fuchsia_wlan_device as wlan;
 use serde_derive::Deserialize;
 use std::{
-    collections::HashMap,
+    collections::BTreeMap,
     fs::File,
     io::{Read, Write},
 };
@@ -14,7 +14,7 @@ use std::{
 const CONFIG_FILE: &str = "/data/config.json";
 const DEFAULT_CONFIG_FILE: &str = "/config/data/default.json";
 
-#[derive(Copy, Clone, Debug, Deserialize)]
+#[derive(Copy, Clone, Debug, Deserialize, PartialEq)]
 pub enum Role {
     Client,
     Ap,
@@ -39,7 +39,8 @@ pub struct Device {
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    pub phy: HashMap<String, Vec<Role>>,
+    // Using BTreeMap as keys need to be sorted for the fuzzy path matching with fold() to work.
+    pub phy: BTreeMap<String, Vec<Role>>,
 }
 
 impl Config {
@@ -47,14 +48,24 @@ impl Config {
         Self::load_data_config().or_else(|_| Self::load_default_config())
     }
 
+    /// finds the longest prefix that matches the PHY's topological path
     pub fn roles_for_path(&self, path: &str) -> Option<&[Role]> {
-        if let Some(roles) = self.phy.get(path) {
-            println!("found wlan config entry for phy at {}", path);
-            Some(roles)
-        } else if let Some(roles) = self.phy.get("*") {
-            println!("using default wlan config entry for phy");
+        let mut key = "*";
+        for p in self.phy.keys() {
+            if (p.ends_with("/*") && path.starts_with(&p[..p.len() - 1])) || path == p {
+                key = p;
+            }
+        }
+
+        if let Some(roles) = self.phy.get(key) {
+            if key == "*" {
+                println!("using default wlan config entry for phy");
+            } else {
+                println!("using wlan config entry {} for phy {}", key, path);
+            }
             Some(roles)
         } else {
+            println!("no wlan config entry found for phy at {}", path);
             None
         }
     }
@@ -80,5 +91,44 @@ impl Config {
             data_file.write_all(contents.as_bytes()).context("could not write new config file")?;
         }
         Self::load_data_config()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::{Config, Role::*},
+        maplit::btreemap,
+    };
+
+    #[test]
+    fn get_role_from_path() {
+        let mut cfg = Config {
+            phy: btreemap! {
+                "/vendor/chip/1/good".to_string() => vec![Client],
+                "/vendor/chip/1/g".to_string() => vec![Ap, Ap],
+                "/vendor/chip/1/*".to_string() => vec![Ap],
+                "/vendor/chipmunk/good".to_string() => vec![Client, Ap],
+                "*".to_string() => vec![Mesh],
+            },
+        };
+
+        // exact match
+        assert_eq!(cfg.roles_for_path("/vendor/chip/1/good"), Some(&[Client][..]));
+        // exact match, same length as /*
+        assert_eq!(cfg.roles_for_path("/vendor/chip/1/g"), Some(&[Ap, Ap][..]));
+        // prefix matches
+        assert_eq!(cfg.roles_for_path("/vendor/chip/1/better"), Some(&[Ap][..]));
+        // exact match
+        assert_eq!(cfg.roles_for_path("/vendor/chipmunk/good"), Some(&[Client, Ap][..]));
+        // no match since no prefix defined
+        assert_eq!(cfg.roles_for_path("/vendor/chipmunk/good/child"), Some(&[Mesh][..]));
+        // nothing defined
+        assert_eq!(cfg.roles_for_path("/vendor/not-chip/1/mine"), Some(&[Mesh][..]));
+
+        // Now let's remove the default
+        cfg.phy.remove("*");
+        assert_eq!(cfg.roles_for_path("/vendor/chipmunk/good/child"), None);
+        assert_eq!(cfg.roles_for_path("/vendor/not-chip/1/mine"), None);
     }
 }
