d2116f267ac1dc222aeca79156e8c5a69d89fa3d
Craig Tiller <ctiller@google.com>
2019-08-21 16:16:48 +0000

    [rust] Add crate: tokio
    Change-Id: I3408c96c65222c0f7b2091a35aad0d44e54d2b68

---
+++ third_party/rust_crates/vendor/tokio-sync/src/lib.rs
@@ -0,0 +1,33 @@
+#![doc(html_root_url = "https://docs.rs/tokio-sync/0.1.6")]
+#![deny(missing_debug_implementations, missing_docs, unreachable_pub)]
+#![cfg_attr(test, deny(warnings))]
+
+//! Asynchronous synchronization primitives.
+//!
+//! This crate provides primitives for synchronizing asynchronous tasks.
+
+extern crate fnv;
+#[macro_use]
+extern crate futures;
+
+macro_rules! debug {
+    ($($t:tt)*) => {
+        if false {
+            println!($($t)*);
+        }
+    }
+}
+
+macro_rules! if_fuzz {
+    ($($t:tt)*) => {{
+        if false { $($t)* }
+    }}
+}
+
+pub mod lock;
+mod loom;
+pub mod mpsc;
+pub mod oneshot;
+pub mod semaphore;
+pub mod task;
+pub mod watch;
---
+++ third_party/rust_crates/vendor/tokio-tcp/README.md
@@ -0,0 +1,15 @@
+# tokio-tcp
+
+TCP bindings for `tokio`.
+
+[Documentation](https://docs.rs/tokio-tcp/0.1.3/tokio_tcp)
+
+## License
+
+This project is licensed under the [MIT license](./LICENSE).
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in Tokio by you, shall be licensed as MIT, without any additional
+terms or conditions.
