7f8cd34c84a3842151970bec26c7bee2864566c5
John Bauman <jbauman@google.com>
2019-08-22 07:12:35 +0000

    [mt8167-thermal] Always use 1.3V for CPU
    The GPU uses this rail as well, and needs it at 1.3V to run at 600MHz.
    For now always use 1.3V, since we're generally clocked to max anyway and
    the only side effect is poorer efficiency when downclocked.
    
    Change-Id: I7391b7669392332b547a6bfd5e733bb8789c0f63

--- zircon/system/dev/board/mt8167s_ref/mt8167-thermal.cc
+++ zircon/system/dev/board/mt8167s_ref/mt8167-thermal.cc
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fuchsia/hardware/thermal/c/fidl.h>
+
 #include <ddk/binding.h>
 #include <ddk/debug.h>
 #include <ddk/metadata.h>
 #include <ddk/platform-defs.h>
-#include <fuchsia/hardware/thermal/c/fidl.h>
 #include <soc/mt8167/mt8167-clk.h>
 #include <soc/mt8167/mt8167-hw.h>
 
@@ -35,36 +36,39 @@ constexpr fuchsia_hardware_thermal_ThermalTemperatureInfo TripPoint(float temp_c
           .gpu_clk_freq_source = 0};
 }
 
-constexpr fuchsia_hardware_thermal_ThermalDeviceInfo thermal_dev_info =
-    {
-        .active_cooling = false,
-        .passive_cooling = true,
-        .gpu_throttling = true,
-        .num_trip_points = 5,
-        .big_little = false,
-        .critical_temp_celsius = 120.0f,
-        .trip_point_info =
-            {
-                TripPoint(55.0f, 4),
-                TripPoint(65.0f, 3),
-                TripPoint(75.0f, 2),
-                TripPoint(85.0f, 1),
-                TripPoint(95.0f, 0),
-            },
-        .opps = {[fuchsia_hardware_thermal_PowerDomain_BIG_CLUSTER_POWER_DOMAIN] =
-                     {// See section 3.6 (MTCMOS Domains) of the functional specification document.
-                      .opp =
-                          {
-                              [0] = {.freq_hz = 598000000, .volt_uv = 1150000},
-                              [1] = {.freq_hz = 747500000, .volt_uv = 1150000},
-                              [2] = {.freq_hz = 1040000000, .volt_uv = 1200000},
-                              [3] = {.freq_hz = 1196000000, .volt_uv = 1250000},
-                              [4] = {.freq_hz = 1300000000, .volt_uv = 1300000},
-                          },
-                      .latency = 0,
-                      .count = 5},
-                 [fuchsia_hardware_thermal_PowerDomain_LITTLE_CLUSTER_POWER_DOMAIN] = {
-                     .opp = {}, .latency = 0, .count = 0}}};
+constexpr fuchsia_hardware_thermal_ThermalDeviceInfo
+    thermal_dev_info =
+        {.active_cooling = false,
+         .passive_cooling = true,
+         .gpu_throttling = true,
+         .num_trip_points = 5,
+         .big_little = false,
+         .critical_temp_celsius = 120.0f,
+         .trip_point_info =
+             {
+                 TripPoint(55.0f, 4),
+                 TripPoint(65.0f, 3),
+                 TripPoint(75.0f, 2),
+                 TripPoint(85.0f, 1),
+                 TripPoint(95.0f, 0),
+             },
+         .opps = {[fuchsia_hardware_thermal_PowerDomain_BIG_CLUSTER_POWER_DOMAIN] =
+                      {// See section 3.6 (MTCMOS Domains) of the functional specification document.
+                       // Use 1.3V because the GPU uses this voltage as well and needs that to clock
+                       // up to 600MHz.
+                       // TODO(fxb/35052) - Coordinate voltage between GPU and CPU.
+                       .opp =
+                           {
+                               [0] = {.freq_hz = 598000000, .volt_uv = 1'300'000},
+                               [1] = {.freq_hz = 747500000, .volt_uv = 1'300'000},
+                               [2] = {.freq_hz = 1040000000, .volt_uv = 1'300'000},
+                               [3] = {.freq_hz = 1196000000, .volt_uv = 1'300'000},
+                               [4] = {.freq_hz = 1300000000, .volt_uv = 1'300'000},
+                           },
+                       .latency = 0,
+                       .count = 5},
+                  [fuchsia_hardware_thermal_PowerDomain_LITTLE_CLUSTER_POWER_DOMAIN] = {
+                      .opp = {}, .latency = 0, .count = 0}}};
 
 constexpr pbus_metadata_t thermal_metadata[] = {
     {.type = DEVICE_METADATA_THERMAL_CONFIG,
