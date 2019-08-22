206b914b4e0566925c6727a54e06bc747433519a
Ankur Mittal <anmittal@google.com>
2019-08-22 20:50:47 +0000

    [sandbox-tests] Fix for --variant profile.
    BUG=35322
    BUG=35320
    BUG=35327
    
    Change-Id: I988ec35392640f67a9b2ec01bdc8dc1cd74992ba

--- garnet/bin/appmgr/integration_tests/sandbox/services/some_services.cc
+++ garnet/bin/appmgr/integration_tests/sandbox/services/some_services.cc
@@ -2,15 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "garnet/bin/appmgr/integration_tests/sandbox/namespace_test.h"
-
-#include <string>
-#include <vector>
-
+#include <fuchsia/debugdata/cpp/fidl.h>
 #include <fuchsia/ldsvc/cpp/fidl.h>
 #include <fuchsia/process/cpp/fidl.h>
 #include <fuchsia/sys/cpp/fidl.h>
 #include <zircon/errors.h>
+
+#include <string>
+#include <vector>
+
+#include "garnet/bin/appmgr/integration_tests/sandbox/namespace_test.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "src/lib/files/directory.h"
@@ -34,6 +35,11 @@ TEST_F(NamespaceTest, SomeServices) {
   // readdir should list services in sandbox.
   std::vector<std::string> files;
   ASSERT_TRUE(files::ReadDirContents("/svc", &files));
+
+  // Remove debug service if present due variant build.
+  files.erase(std::remove(files.begin(), files.end(), fuchsia::debugdata::DebugData::Name_),
+              files.end());
+
   EXPECT_THAT(files, ::testing::UnorderedElementsAre(".", "fuchsia.sys.Environment",
                                                      "fuchsia.process.Resolver"));
 }
