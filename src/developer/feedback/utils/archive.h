d58ba613b4983b148e771e45f923dc0bdd5bd055
Francois Rousseau <frousseau@google.com>
2019-08-23 20:31:36 +0000

    [feedback] util library to archive/unpack attachments
    * we need the ability to archive a set of feedback attachments
      into a single zip as it is more convenient to all existing clients
    * the ability to unpack is only for testing purposes today
    * we will use Archive() in the next CL
    
    Bug: 34773
    
    TESTED=`fx run-test feedback_util_tests`
    
    Change-Id: Idb0d6fd4e69ff41531218bc4c104fe0f98d3b1b2

---
+++ src/developer/feedback/utils/archive.h
@@ -0,0 +1,24 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#ifndef SRC_DEVELOPER_FEEDBACK_UTILS_ARCHIVE_H_
+#define SRC_DEVELOPER_FEEDBACK_UTILS_ARCHIVE_H_
+
+#include <fuchsia/feedback/cpp/fidl.h>
+#include <fuchsia/mem/cpp/fidl.h>
+
+#include <vector>
+
+namespace feedback {
+
+// Bundles a vector of attachments into a single ZIP archive with DEFLATE compression.
+bool Archive(const std::vector<fuchsia::feedback::Attachment>& attachments,
+             fuchsia::mem::Buffer* archive);
+
+// Unpack a ZIP archive into a vector of attachments.
+bool Unpack(const fuchsia::mem::Buffer& archive,
+            std::vector<fuchsia::feedback::Attachment>* attachments);
+
+}  // namespace feedback
+
+#endif  // SRC_DEVELOPER_FEEDBACK_UTILS_ARCHIVE_H_
---
+++ src/developer/feedback/utils/tests/archive_unittest.cc
@@ -0,0 +1,119 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#include "src/developer/feedback/utils/archive.h"
+
+#include <fuchsia/feedback/cpp/fidl.h>
+#include <fuchsia/mem/cpp/fidl.h>
+#include <lib/fsl/vmo/file.h>
+#include <lib/fsl/vmo/sized_vmo.h>
+#include <lib/fsl/vmo/strings.h>
+#include <lib/fsl/vmo/vector.h>
+#include <lib/syslog/cpp/logger.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "src/developer/feedback/testing/gmatchers.h"
+#include "src/developer/feedback/testing/gpretty_printers.h"
+#include "src/lib/fxl/logging.h"
+#include "src/lib/fxl/test/test_settings.h"
+#include "third_party/googletest/googlemock/include/gmock/gmock.h"
+#include "third_party/googletest/googletest/include/gtest/gtest.h"
+
+namespace feedback {
+namespace {
+
+using fuchsia::feedback::Attachment;
+using fuchsia::mem::Buffer;
+
+constexpr char kPlainTextFilename[] = "filename.txt";
+constexpr char kJsonFilename[] = "filename.json";
+constexpr char kXmlFilename[] = "filename.xml";
+constexpr char kPlainTextFileContent[] = "plain text content";
+constexpr char kJsonFileContent[] = R"({
+  "key": "json content"
+})";
+constexpr char kXmlFileContent[] = "<tag>xml content</tag>";
+
+// This corresponds to the content of resources/test_data.zip
+const std::map<std::string, std::string> kAttachments = {
+    {kPlainTextFilename, kPlainTextFileContent},
+    {kJsonFilename, kJsonFileContent},
+    {kXmlFilename, kXmlFileContent},
+};
+
+Attachment BuildAttachment(const std::string& key, const std::string& value) {
+  Attachment attachment;
+  attachment.key = key;
+  FXL_CHECK(fsl::VmoFromString(value, &attachment.value));
+  return attachment;
+}
+
+std::vector<Attachment> BuildAttachments(
+    const std::map<std::string, std::string>& str_attachments) {
+  std::vector<Attachment> attachments;
+  for (const auto& [key, value] : str_attachments) {
+    attachments.push_back(BuildAttachment(key, value));
+  }
+  return attachments;
+}
+
+TEST(ArchiveTest, Archive) {
+  Buffer archive;
+  ASSERT_TRUE(Archive(BuildAttachments(kAttachments), &archive));
+  ASSERT_TRUE(archive.vmo.is_valid());
+  ASSERT_GT(archive.size, 0u);
+
+  fsl::SizedVmo expected_vmo;
+  ASSERT_TRUE(fsl::VmoFromFilename("/pkg/data/test_data.zip", &expected_vmo));
+  std::vector<uint8_t> expected_bytes;
+  ASSERT_TRUE(fsl::VectorFromVmo(expected_vmo, &expected_bytes));
+  std::vector<uint8_t> actual_bytes;
+  ASSERT_TRUE(fsl::VectorFromVmo(archive, &actual_bytes));
+  EXPECT_EQ(actual_bytes, expected_bytes);
+}
+
+TEST(ArchiveTest, Unpack) {
+  fsl::SizedVmo vmo;
+  ASSERT_TRUE(fsl::VmoFromFilename("/pkg/data/test_data.zip", &vmo));
+  Buffer archive = std::move(vmo).ToTransport();
+
+  std::vector<Attachment> unpacked_attachments;
+  ASSERT_TRUE(Unpack(archive, &unpacked_attachments));
+  EXPECT_THAT(unpacked_attachments,
+              testing::UnorderedElementsAreArray({
+                  MatchesAttachment(kPlainTextFilename, kPlainTextFileContent),
+                  MatchesAttachment(kJsonFilename, kJsonFileContent),
+                  MatchesAttachment(kXmlFilename, kXmlFileContent),
+              }));
+}
+
+TEST(ArchiveTest, UnpackArchive) {
+  const std::vector<Attachment> original_attachments = BuildAttachments(kAttachments);
+  Buffer archive;
+  ASSERT_TRUE(Archive(original_attachments, &archive));
+
+  std::vector<Attachment> unpacked_attachments;
+  ASSERT_TRUE(Unpack(archive, &unpacked_attachments));
+  EXPECT_THAT(unpacked_attachments,
+              testing::UnorderedElementsAreArray({
+                  MatchesAttachment(kPlainTextFilename, kPlainTextFileContent),
+                  MatchesAttachment(kJsonFilename, kJsonFileContent),
+                  MatchesAttachment(kXmlFilename, kXmlFileContent),
+              }));
+}
+
+}  // namespace
+}  // namespace feedback
+
+int main(int argc, char** argv) {
+  if (!fxl::SetTestSettings(argc, argv)) {
+    return EXIT_FAILURE;
+  }
+
+  testing::InitGoogleTest(&argc, argv);
+  syslog::InitLogger({"feedback", "test"});
+  return RUN_ALL_TESTS();
+}
