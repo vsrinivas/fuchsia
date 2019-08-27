bb95a4450bb79bf5525260f1e28cfe4f074cff76
David Greenaway <dgreenaway@google.com>
2019-08-27 05:27:16 +0000

    [intel-hda] Update NHLT table definitions.
    Verify that the tables are still correct according to the 2018 revision
    of the NHLT specification (they are), and update header comments
    appropriately.
    
    Add a few comments describing details of some of the fields.
    
    Field definitions taken from:
    
       Intel® Smart Sound Technology NHLT Specification
       Architecture Guide/Overview
       Revision 1.0, 2018-06-06
       Intel Unique ID 595976
    
    Tests: N/A; changes only to structure definitions and comments.
    
    Change-Id: I2a819dacb85ef67c3d968d855fc3e88f48bcc5fc

--- zircon/system/ulib/intel-hda/include/intel-hda/utils/nhlt.h
+++ zircon/system/ulib/intel-hda/include/intel-hda/utils/nhlt.h
@@ -7,9 +7,10 @@
 
 // Non-HD Audio Link Table (NHLT) definitions taken from
 //
-// Intel Smart Sound Technology Audio DSP Non-HD Audio ACPI High Level Design
-// Revision 0.7
-// November 2015
+// Intel® Smart Sound Technology NHLT Specification
+// Architecture Guide/Overview
+// Revision 1.0, 2018-06-06
+// Intel Unique ID 595976
 
 #include <zircon/compiler.h>
 
@@ -39,21 +40,27 @@ struct acpi_table_header_t {
 } __PACKED;
 
 struct specific_config_t {
-  uint32_t capabilities_size;
+  uint32_t capabilities_size;  // In bytes, does not include size of this field.
   // followed by |capabilities_size| bytes.
 } __PACKED;
 
+struct nhlt_guid_t {
+  uint8_t guid[16];
+} __PACKED;
+
 struct format_config_t {
-  uint16_t format_tag;
-  uint16_t n_channels;
-  uint32_t n_samples_per_sec;
-  uint32_t n_avg_bytes_per_sec;
-  uint16_t n_block_align;
-  uint16_t bits_per_sample;
-  uint16_t cb_size;
-  uint16_t valid_bits_per_sample;
-  uint32_t channel_mask;
-  uint8_t subformat_guid[16];
+  uint16_t format_tag;             // Tag describing this field. Always 0xFFFE.
+  uint16_t n_channels;             // Number of channels.
+  uint32_t n_samples_per_sec;      // Samples per second.
+  uint32_t n_avg_bytes_per_sec;    // Average data transfer rate.
+  uint16_t n_block_align;          // Block alignment, in bytes.
+  uint16_t bits_per_sample;        // Bits per sample; always a multiple of 8. This represents
+                                   // the container size, and may be larger than the actual
+                                   // sample size.
+  uint16_t cb_size;                // Size of following three fields in bytes: always 22.
+  uint16_t valid_bits_per_sample;  // Number of bits of precision in the audio signal.
+  uint32_t channel_mask;           // Assignment of channels in the stream to speaker positions.
+  nhlt_guid_t subformat;           // Subformat of data.
   // followed by specific_config_t.
 } __PACKED;
 
@@ -62,28 +69,38 @@ struct formats_config_t {
   // followed by |format_config_count| format_config_t structures.
 } __PACKED;
 
+enum NhltLinkType : uint8_t {
+  NHLT_LINK_TYPE_HDA = 0,
+  NHLT_LINK_TYPE_PDM = 2,
+  NHLT_LINK_TYPE_SSP = 3,
+};
+
+enum NhltEndpointDirection : uint8_t {
+  NHLT_DIRECTION_RENDER = 0,
+  NHLT_DIRECTION_CAPTURE = 1,
+  NHLT_DIRECTION_BIDIR = 2,
+};
+
 struct nhlt_descriptor_t {
-  uint32_t length;
-  uint8_t link_type;
-  uint8_t instance_id;
+  uint32_t length;         // Size of the endpoint descriptor, including specific_config_t
+                           // and formats_config_t fields.
+  NhltLinkType link_type;  // Underlying link type.
+  uint8_t instance_id;     // Device instance, unique to a particular link type.
+                           // In the range [0, 7].
+
+  // Vendor / Device / Revision information for driver matching.
   uint16_t vendor_id;
   uint16_t device_id;
   uint16_t revision_id;
   uint32_t subsystem_id;
+
   uint8_t device_type;
-  uint8_t direction;
+  NhltEndpointDirection direction;
   uint8_t virtual_bus_id;
+
   // followed by specific_config_t
   // followed by formats_config_t
 } __PACKED;
-
-constexpr uint8_t NHLT_LINK_TYPE_HDA = 0;
-constexpr uint8_t NHLT_LINK_TYPE_PDM = 2;
-constexpr uint8_t NHLT_LINK_TYPE_SSP = 3;
-
-constexpr uint8_t NHLT_DIRECTION_RENDER = 0;
-constexpr uint8_t NHLT_DIRECTION_CAPTURE = 1;
-constexpr uint8_t NHLT_DIRECTION_BIDIR = 2;
 
 struct nhlt_table_t {
   acpi_table_header_t header;
