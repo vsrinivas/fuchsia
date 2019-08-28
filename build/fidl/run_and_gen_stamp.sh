245d2aa94bcdf292bc2a0540679474e772fd6d59
Rich Kadel <richkadel@google.com>
2019-08-28 12:59:36 +0000

    [build] add fidl-lint to fuchsia //build (non-zircon build)
    By default, fidl libraries will fail to build if there are any lint
    errors, unless the fidl() template has:
    
      excluded_checks = [ "check-name-that-fails", ... ]
    
    This CL does not support fidl libraries in zircon. Zircon's ninja
    templates also support FIDL but have a very different implementation, so
    these will be updated in a separate CL.
    
    Note, some zircon FIDL libraries are referenced (in deps) by non-zircon
    BUILD.gn files. There is a workaround in this CL that disables all
    currently-defined lint errors for these zircon FIDL libraries.
    
    Bug: 7821
    
    Test: fx build
    Change-Id: I3880f1c8cbed4a99728d023c442a8f22670b365d

---
+++ build/fidl/run_and_gen_stamp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script wraps a binary that does not generate any output file, but
+# can return a status (success or fail). Ninja actions need an output to
+# resolve dependencies, so this wrapper creates or updates a stamp file
+# with the date/time of the last success.
+
+# Stamp file to touch after the command runs successfully
+stamp="$1"
+shift
+
+# Execute the command and check return status
+"$@"
+status=$?
+# only update the stamp file if the command returned a successful status
+if (( $status == 0 )); then
+  touch "${stamp}"
+fi
+
+exit ${status}
