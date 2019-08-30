a38785ebee0f153ec6eedb89a642b080b83648a6
Nick Maniscalco <maniscalco@google.com>
2019-08-30 00:59:36 +0000

    [kernel] Implement outcalls for 16-byte atomics
    Clang reliably inlines 16-byte atomic intrinsics, but GCC does
    not. Implement outcalls for 16-byte atomics so that we can use things
    like ktl::atomic<__int128> in the kernel.
    
    Bug: 27334
    Test: Added kernel unit tests to verify basic behavior.
    Change-Id: Ie1b5bb1d296ae85e9beb370edac1b2cbf34b75f9

---
+++ zircon/kernel/lib/libc/atomic.cc
@@ -0,0 +1,130 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file provides callout implementations of 16-byte atomic operations. When the compiler fails
+// to provide inline intrinsics for a given atomic operation, it will generate a call to one of
+// these functions. In particular this is needed by GCC to support 16-byte operations.
+//
+// These implementations provide __ATOMIC_SEQ_CST semantics regardless of the requested "memory
+// model" argument.  As a result, they are not necessarily optimal (esp. on arm64), but they will be
+// correct because __ATOMIC_SEQ_CST has the strongest semantics.
+
+#include <cstdint>
+
+bool atomic_compare_exchange_16(volatile void* ptr, void* expected, unsigned __int128 desired, bool,
+                                int, int) __asm__("__atomic_compare_exchange_16");
+
+unsigned __int128 atomic_load_16(volatile void* ptr, int) __asm__("__atomic_load_16");
+
+void atomic_store_16(volatile void* ptr, unsigned __int128 value, int) __asm__("__atomic_store_16");
+
+#ifdef __aarch64__
+
+bool atomic_compare_exchange_16(volatile void* ptr_void, void* expected_void,
+                                unsigned __int128 desired, bool, int, int) {
+  auto ptr = static_cast<volatile unsigned __int128*>(ptr_void);
+  auto expected = static_cast<unsigned __int128*>(expected_void);
+  int result;
+  do {
+    uint64_t temp_lo;
+    uint64_t temp_hi;
+    __asm__ volatile("ldaxp %[lo], %[hi], %[src]"
+                     : [ lo ] "=r"(temp_lo), [ hi ] "=r"(temp_hi)
+                     : [ src ] "Q"(*ptr)
+                     : "memory");
+    unsigned __int128 temp = (static_cast<unsigned __int128>(temp_hi)) << 64 | temp_lo;
+
+    if (temp != *expected) {
+      // No reason to leave the monitor in Exclusive so clear it.
+      __asm__ volatile("clrex");
+      *expected = temp;
+      return false;
+    }
+
+    auto desired_lo = static_cast<uint64_t>(desired);
+    auto desired_hi = static_cast<uint64_t>(desired >> 64);
+    __asm__ volatile("stlxp %w[result], %[lo], %[hi], %[ptr]"
+                     : [ result ] "=&r"(result), [ ptr ] "=Q"(*ptr)
+                     : [ lo ] "r"(desired_lo), [ hi ] "r"(desired_hi)
+                     : "memory");
+  } while (result);
+  return true;
+}
+
+unsigned __int128 atomic_load_16(volatile void* ptr_void, int) {
+  auto ptr = static_cast<volatile unsigned __int128*>(ptr_void);
+  uint64_t value_lo;
+  uint64_t value_hi;
+  int result;
+  do {
+    __asm__ volatile("ldaxp %[lo], %[hi], %[ptr]"
+                     : [ lo ] "=r"(value_lo), [ hi ] "=r"(value_hi)
+                     : [ ptr ] "Q"(*ptr));
+    __asm__ volatile("stlxp %w[result], %[lo], %[hi], %[ptr]"
+                     : [ result ] "=&r"(result), [ ptr ] "=Q"(*ptr)
+                     : [ lo ] "r"(value_lo), [ hi ] "r"(value_hi)
+                     : "memory");
+  } while (result);
+  return (static_cast<unsigned __int128>(value_hi)) << 64 | value_lo;
+}
+
+void atomic_store_16(volatile void* ptr_void, unsigned __int128 value, int) {
+  auto ptr = static_cast<volatile unsigned __int128*>(ptr_void);
+  auto value_lo = static_cast<uint64_t>(value);
+  auto value_hi = static_cast<uint64_t>(value >> 64);
+  uint64_t temp_lo;
+  uint64_t temp_hi;
+  int result;
+  do {
+    __asm__ volatile("ldaxp %[temp_lo], %[temp_hi], %[ptr]"
+                     : [ temp_lo ] "=r"(temp_lo), [ temp_hi ] "=r"(temp_hi)
+                     : [ ptr ] "Q"(*ptr));
+    __asm__ volatile("stlxp %w[result], %[value_lo], %[value_hi], %[ptr]"
+                     : [ result ] "=&r"(result), [ ptr ] "=Q"(*ptr)
+                     : [ value_lo ] "r"(value_lo), [ value_hi ] "r"(value_hi)
+                     : "memory");
+  } while (result);
+}
+
+#elif defined(__x86_64__)
+
+bool atomic_compare_exchange_16(volatile void* ptr_void, void* expected_void,
+                                unsigned __int128 desired, bool, int, int) {
+  auto ptr = static_cast<volatile unsigned __int128*>(ptr_void);
+  auto expected = static_cast<unsigned __int128*>(expected_void);
+  auto desired_lo = static_cast<uint64_t>(desired);
+  auto desired_hi = static_cast<uint64_t>(desired >> 64);
+  bool result;
+  __asm__ volatile("lock cmpxchg16b %[dest]"
+                   : [ dest ] "+m"(*ptr), [ result ] "=@ccz"(result), "+A"(*expected)
+                   : "b"(desired_lo), "c"(desired_hi)
+                   : "memory");
+  return result;
+}
+
+unsigned __int128 atomic_load_16(volatile void* ptr_void, int) {
+  auto ptr = static_cast<volatile unsigned __int128*>(ptr_void);
+  unsigned __int128 result = 0;
+  __asm__ volatile("lock cmpxchg16b %[dest]"
+                   : [ dest ] "+m"(*ptr), "+A"(result)
+                   : "b"(0), "c"(0)
+                   : "cc", "memory");
+  return result;
+}
+
+void atomic_store_16(volatile void* ptr_void, unsigned __int128 value, int) {
+  auto ptr = static_cast<volatile unsigned __int128*>(ptr_void);
+  auto value_lo = static_cast<uint64_t>(value);
+  auto value_hi = static_cast<uint64_t>(value >> 64);
+  unsigned __int128 expected = 0;
+  bool matched;
+  do {
+    __asm__ volatile("lock cmpxchg16b %[dest]"
+                     : [ dest ] "+m"(*ptr), [ result ] "=@ccz"(matched), "+A"(expected)
+                     : "b"(value_lo), "c"(value_hi)
+                     : "memory");
+  } while (!matched);
+}
+
+#endif
---
+++ zircon/kernel/lib/libc/atomic_tests.cc
@@ -0,0 +1,74 @@
+// Copyright 2019 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <lib/unittest/unittest.h>
+
+#include <ktl/atomic.h>
+
+namespace {
+
+constexpr unsigned __int128 kValue =
+    (static_cast<unsigned __int128>(0x0123456789abcdef) << 64) + 0xfedcba9876543210;
+
+bool Load16Test() {
+  BEGIN_TEST;
+
+  {
+    ktl::atomic<unsigned __int128> v = 0u;
+    EXPECT_EQ(v.load(), 0u, "");
+  }
+
+  {
+    ktl::atomic<unsigned __int128> v = kValue;
+    EXPECT_EQ(v.load(), kValue, "");
+  }
+
+  END_TEST;
+}
+
+bool Store16Test() {
+  BEGIN_TEST;
+
+  ktl::atomic<unsigned __int128> v = 0u;
+  EXPECT_EQ(v.load(), 0u, "");
+  v.store(kValue);
+  EXPECT_EQ(v.load(), kValue, "");
+
+  END_TEST;
+}
+
+bool CompareExchange16Test() {
+  BEGIN_TEST;
+
+  {
+    // Comparison fails.
+    ktl::atomic<unsigned __int128> v = kValue;
+    unsigned __int128 expected = kValue - 1;
+    EXPECT_FALSE(v.compare_exchange_strong(expected, 0u), "");
+    EXPECT_EQ(expected, kValue, "");
+    EXPECT_EQ(v.load(), kValue, "");
+  }
+
+  {
+    // Comparison succeeds.
+    ktl::atomic<unsigned __int128> v = kValue;
+    unsigned __int128 expected = kValue;
+    constexpr unsigned __int128 kDesired = 0xaaaabbbbccccdddd;
+    EXPECT_TRUE(v.compare_exchange_strong(expected, kDesired), "");
+    EXPECT_EQ(expected, kValue, "");
+    EXPECT_EQ(v.load(), kDesired, "");
+  }
+
+  END_TEST;
+}
+
+}  // namespace
+
+UNITTEST_START_TESTCASE(libc_atomic_tests)
+UNITTEST("load_16", Load16Test)
+UNITTEST("store_16", Store16Test)
+UNITTEST("compare_exchange_16", CompareExchange16Test)
+UNITTEST_END_TESTCASE(libc_atomic_tests, "libc_atomic", "libc/atomic tests");
