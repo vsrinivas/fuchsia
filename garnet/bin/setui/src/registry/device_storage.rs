9a8edf51ee09706d68864b54ab651ec8243277c8
Bryce Lee <brycelee@google.com>
2019-08-30 14:27:03 +0000

    [settings] Reuse proxies in InMemoryStorageFactory
    This changelist maps proxies to types requested from
    InMemoryStorageFactory. This more closely mimicks the
    real behavior (where storage instances will all point to the
    same service) and allows the storage to be inspected
    and manipulated outside in tests.
    
    Fixes: fxb/35585
    Test: fx run-test setui_service_tests -- --test
    Change-Id: Ie633f145307c8a7110ca589a55a13181c9b5f53c

--- garnet/bin/setui/src/registry/device_storage.rs
+++ garnet/bin/setui/src/registry/device_storage.rs
@@ -15,6 +15,7 @@ const SETTINGS_PREFIX: &str = "settings";
 /// Stores device level settings in persistent storage.
 /// User level settings should not use this.
 pub struct DeviceStorage<T> {
+    caching_enabled: bool,
     current_data: Option<T>,
     stash_proxy: StoreAccessorProxy,
 }
@@ -34,6 +35,18 @@ pub trait DeviceStorageCompatible: Serialize + DeserializeOwned + Copy + Partial
 }
 
 impl<T: DeviceStorageCompatible> DeviceStorage<T> {
+    pub fn new(stash_proxy: StoreAccessorProxy, current_data: Option<T>) -> Self {
+        return DeviceStorage {
+            caching_enabled: true,
+            current_data: current_data,
+            stash_proxy: stash_proxy,
+        };
+    }
+
+    fn set_caching_enabled(&mut self, enabled: bool) {
+        self.caching_enabled = enabled;
+    }
+
     pub fn write(&mut self, new_value: T) -> Result<(), Error> {
         if self.current_data != Some(new_value) {
             self.current_data = Some(new_value);
@@ -47,7 +60,7 @@ impl<T: DeviceStorageCompatible> DeviceStorage<T> {
     /// Gets the latest value cached locally, or loads the value from storage.
     /// Doesn't support multiple concurrent callers of the same struct.
     pub async fn get(&mut self) -> T {
-        if let None = self.current_data {
+        if None == self.current_data || !self.caching_enabled {
             if let Some(stash_value) = self.stash_proxy.get_value(&prefixed(T::KEY)).await.unwrap()
             {
                 if let Value::Stringval(string_value) = &*stash_value {
@@ -68,7 +81,7 @@ impl<T: DeviceStorageCompatible> DeviceStorage<T> {
 }
 
 pub trait DeviceStorageFactory {
-    fn get_store<T: DeviceStorageCompatible>(&self) -> Arc<Mutex<DeviceStorage<T>>>;
+    fn get_store<T: DeviceStorageCompatible + 'static>(&self) -> Arc<Mutex<DeviceStorage<T>>>;
 }
 
 /// Factory that vends out storage for individual structs.
@@ -92,11 +105,11 @@ impl StashDeviceStorageFactory {
 
 impl DeviceStorageFactory for StashDeviceStorageFactory {
     /// Currently, this doesn't support more than one instance of the same struct.
-    fn get_store<T: DeviceStorageCompatible>(&self) -> Arc<Mutex<DeviceStorage<T>>> {
+    fn get_store<T: DeviceStorageCompatible + 'static>(&self) -> Arc<Mutex<DeviceStorage<T>>> {
         let (accessor_proxy, server_end) = create_proxy().unwrap();
         self.store.create_accessor(false, server_end).unwrap();
 
-        Arc::new(Mutex::new(DeviceStorage { stash_proxy: accessor_proxy, current_data: None }))
+        Arc::new(Mutex::new(DeviceStorage::new(accessor_proxy, None)))
     }
 }
 
@@ -110,23 +123,38 @@ pub mod testing {
     use fidl::encoding::OutOfLine;
     use fuchsia_async as fasync;
     use futures::prelude::*;
+    use std::any::TypeId;
+    use std::cell::RefCell;
+    use std::collections::HashMap;
 
     /// Storage that does not write to disk, for testing.
     /// Only supports a single key/value pair
-    pub struct InMemoryStorageFactory {}
+    pub struct InMemoryStorageFactory {
+        proxies: RefCell<HashMap<TypeId, StoreAccessorProxy>>,
+    }
 
     impl InMemoryStorageFactory {
         pub fn create() -> InMemoryStorageFactory {
-            InMemoryStorageFactory {}
+            InMemoryStorageFactory { proxies: RefCell::new(HashMap::new()) }
         }
     }
 
     impl DeviceStorageFactory for InMemoryStorageFactory {
-        fn get_store<T: DeviceStorageCompatible>(&self) -> Arc<Mutex<DeviceStorage<T>>> {
-            Arc::new(Mutex::new(DeviceStorage {
-                stash_proxy: spawn_stash_proxy(),
-                current_data: None,
-            }))
+        fn get_store<T: DeviceStorageCompatible + 'static>(&self) -> Arc<Mutex<DeviceStorage<T>>> {
+            let id = TypeId::of::<T>();
+            let mut proxies = self.proxies.borrow_mut();
+            if !proxies.contains_key(&id) {
+                proxies.insert(id, spawn_stash_proxy());
+            }
+
+            if let Some(proxy) = proxies.get(&id) {
+                let mut storage = DeviceStorage::new(proxy.clone(), None);
+                storage.set_caching_enabled(false);
+
+                Arc::new(Mutex::new(storage))
+            } else {
+                panic!("proxy should be present");
+            }
         }
     }
 
@@ -171,6 +199,7 @@ mod tests {
     use fuchsia_async as fasync;
     use futures::prelude::*;
     use serde_derive::{Deserialize, Serialize};
+    use testing::*;
 
     const VALUE0: i32 = 3;
     const VALUE1: i32 = 33;
@@ -209,8 +238,7 @@ mod tests {
             }
         });
 
-        let mut storage: DeviceStorage<TestStruct> =
-            DeviceStorage { stash_proxy: stash_proxy, current_data: None };
+        let mut storage: DeviceStorage<TestStruct> = DeviceStorage::new(stash_proxy, None);
 
         let result = storage.get().await;
 
@@ -234,8 +262,7 @@ mod tests {
             }
         });
 
-        let mut storage: DeviceStorage<TestStruct> =
-            DeviceStorage { stash_proxy: stash_proxy, current_data: None };
+        let mut storage: DeviceStorage<TestStruct> = DeviceStorage::new(stash_proxy, None);
 
         let result = storage.get().await;
 
@@ -247,8 +274,7 @@ mod tests {
         let (stash_proxy, mut stash_stream) =
             fidl::endpoints::create_proxy_and_stream::<StoreAccessorMarker>().unwrap();
 
-        let mut storage: DeviceStorage<TestStruct> =
-            DeviceStorage { stash_proxy: stash_proxy, current_data: None };
+        let mut storage: DeviceStorage<TestStruct> = DeviceStorage::new(stash_proxy, None);
 
         storage.write(TestStruct { value: VALUE2 }).expect("writing shouldn't fail");
 
@@ -268,6 +294,43 @@ mod tests {
         match stash_stream.next().await.unwrap() {
             Ok(StoreAccessorRequest::Commit { .. }) => {} // expected
             request => panic!("Unexpected request: {:?}", request),
+        }
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_in_memory_storage() {
+        let factory = InMemoryStorageFactory::create();
+
+        let store_1 = factory.get_store::<TestStruct>();
+        let store_2 = factory.get_store::<TestStruct>();
+
+        // Write initial data through first store.
+        let test_struct = TestStruct { value: VALUE0 };
+
+        // Ensure writing from store1 ends up in store2
+        test_write_propagation(store_1.clone(), store_2.clone(), test_struct).await;
+
+        let test_struct_2 = TestStruct { value: VALUE1 };
+        // Ensure writing from store2 ends up in store1
+        test_write_propagation(store_2.clone(), store_1.clone(), test_struct_2).await;
+    }
+
+    async fn test_write_propagation(
+        store_1: Arc<Mutex<DeviceStorage<TestStruct>>>,
+        store_2: Arc<Mutex<DeviceStorage<TestStruct>>>,
+        data: TestStruct,
+    ) {
+        {
+            let mut store_1_lock = store_1.lock().await;
+            assert!(store_1_lock.write(data).is_ok());
+        }
+
+        // Ensure it is read in from second store.
+        {
+            let mut store_2_lock = store_2.lock().await;
+            let retrieved_struct = store_2_lock.get().await;
+
+            assert_eq!(data, retrieved_struct);
         }
     }
 }
