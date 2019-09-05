390cc1b450b79aaeee51184a9153064f3d0860b2
Sean Klein <smklein@google.com>
2019-09-05 18:08:58 +0000

    [journal] Add in-flight operation tracker
    This enables the journal to identify when incoming data operations
    would intersect with in-flight metdata operations. This is the
    condition for emitting a revocation record, as described by go/revoke.
    
    Change-Id: Id7b1ed08745d5c34718db0d538ef1ae2ec41c6c7

---
+++ zircon/system/ulib/fs/journal/include/fs/journal/internal/operation-tracker.h
@@ -0,0 +1,51 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FS_JOURNAL_INTERNAL_OPERATION_TRACKER_H_
+#define FS_JOURNAL_INTERNAL_OPERATION_TRACKER_H_
+
+#include <algorithm>
+#include <vector>
+
+#include <range/interval-tree.h>
+
+namespace fs {
+namespace internal {
+
+// Tracks a collection of live operations.
+//
+// This class is thread-compatible.
+// This class is movable, but not copyable.
+class OperationTracker {
+ public:
+  using Range = range::Range<uint64_t>;
+  OperationTracker() = default;
+  OperationTracker(const OperationTracker&) = delete;
+  OperationTracker& operator=(const OperationTracker&) = delete;
+  OperationTracker(OperationTracker&& other) = default;
+  OperationTracker& operator=(OperationTracker&& other) = default;
+
+  // Tracks operations contained within the range.
+  void Insert(Range range) { operations_.insert(range); }
+
+  // Removes the overlapping portion of tracked operations which overlap with the input range.
+  // This method does not remove the non-overlapping portion.
+  // For example:
+  //   Insert([0, 100))
+  //   Remove([50, 150)
+  // Removes/returns [50, 100), but leaves [0, 50) in the tracker.
+  //
+  // Returns a vector of Ranges describing the overlapping portions of these operations.
+  std::vector<Range> Remove(Range range);
+
+  void Clear() { operations_.clear(); }
+
+ private:
+  range::IntervalTree<Range> operations_;
+};
+
+}  // namespace internal
+}  // namespace fs
+
+#endif  // FS_JOURNAL_INTERNAL_OPERATION_TRACKER_H_
