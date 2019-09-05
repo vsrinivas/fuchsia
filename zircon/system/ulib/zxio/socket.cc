250449469855cd5bbb55d67ddb9f0528bf1d3a6f
Tamir Duberstein <tamird@google.com>
2019-09-05 23:38:06 +0000

    [zxio] use placement new to avoid UB
    Change-Id: I725d061184f8b5377263663d1f180ccf95667073

--- zircon/system/ulib/zxio/socket.cc
+++ zircon/system/ulib/zxio/socket.cc
@@ -10,39 +10,42 @@
 namespace fio = ::llcpp::fuchsia::io;
 
 static zx_status_t zxio_socket_close(zxio_t* io) {
-  zxio_socket_t* zs = reinterpret_cast<zxio_socket_t*>(io);
-  return zxs_close(std::move(zs->socket));
+  auto zs = reinterpret_cast<zxio_socket_t*>(io);
+  zx_status_t status = zxs_close(std::move(zs->socket));
+  zs->~zxio_socket_t();
+  return status;
 }
 
 static zx_status_t zxio_socket_release(zxio_t* io, zx_handle_t* out_handle) {
-  zxs_socket_t socket = std::move(reinterpret_cast<zxio_socket_t*>(io)->socket);
-  *out_handle = socket.control.mutable_channel()->release();
+  auto zs = reinterpret_cast<zxio_socket_t*>(io);
+  *out_handle = zs->socket.control.mutable_channel()->release();
+  zs->~zxio_socket_t();
   return ZX_OK;
 }
 
 static zx_status_t zxio_socket_clone(zxio_t* io, zx_handle_t* out_handle) {
-  zxio_socket_t* socket = reinterpret_cast<zxio_socket_t*>(io);
+  auto zs = reinterpret_cast<zxio_socket_t*>(io);
   zx::channel local, remote;
   zx_status_t status = zx::channel::create(0, &local, &remote);
   if (status != ZX_OK) {
     return status;
   }
-  auto result = socket->socket.control.Clone(fio::CLONE_FLAG_SAME_RIGHTS, std::move(remote));
-  if (result.status() != ZX_OK) {
-    return result.status();
+  status = zs->socket.control.Clone(fio::CLONE_FLAG_SAME_RIGHTS, std::move(remote)).status();
+  if (status != ZX_OK) {
+    return status;
   }
   *out_handle = local.release();
   return ZX_OK;
 }
 
 static zx_status_t zxio_socket_read(zxio_t* io, void* buffer, size_t capacity, size_t* out_actual) {
-  zxio_socket_t* zs = reinterpret_cast<zxio_socket_t*>(io);
+  auto zs = reinterpret_cast<zxio_socket_t*>(io);
   return zxs_recv(&zs->socket, buffer, capacity, out_actual);
 }
 
 static zx_status_t zxio_socket_write(zxio_t* io, const void* buffer, size_t capacity,
                                      size_t* out_actual) {
-  zxio_socket_t* zs = reinterpret_cast<zxio_socket_t*>(io);
+  auto zs = reinterpret_cast<zxio_socket_t*>(io);
   return zxs_send(&zs->socket, buffer, capacity, out_actual);
 }
 
@@ -57,8 +60,10 @@ static constexpr zxio_ops_t zxio_socket_ops = []() {
 }();
 
 zx_status_t zxio_socket_init(zxio_storage_t* storage, zxs_socket_t socket) {
-  zxio_socket_t* zs = reinterpret_cast<zxio_socket_t*>(storage);
+  auto zs = new (storage) zxio_socket_t{
+      .io = storage->io,
+      .socket = std::move(socket),
+  };
   zxio_init(&zs->io, &zxio_socket_ops);
-  zs->socket = std::move(socket);
   return ZX_OK;
 }
