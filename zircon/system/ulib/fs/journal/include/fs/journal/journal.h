222e29152bdd98fa6d9e482be64959a25afe156e
Sean Klein <smklein@google.com>
2019-09-05 20:14:02 +0000

    [journal] Split JournalWriter into distinct file
    Change-Id: I0a799716d638f174a85b7a6cc33c48afcc26fad9

--- zircon/system/ulib/fs/journal/include/fs/journal/journal.h
+++ zircon/system/ulib/fs/journal/include/fs/journal/journal.h
@@ -11,139 +11,19 @@
 #include <zircon/status.h>
 #include <zircon/types.h>
 
+#include <algorithm>
+
 #include <fbl/vector.h>
 #include <fs/buffer/blocking_ring_buffer.h>
 #include <fs/buffer/ring_buffer.h>
 #include <fs/journal/background-executor.h>
 #include <fs/journal/format.h>
+#include <fs/journal/internal/journal-writer.h>
 #include <fs/journal/superblock.h>
 #include <fs/operation/buffered_operation.h>
 #include <fs/transaction/block_transaction.h>
 
 namespace fs {
-namespace internal {
-
-// A small container encapsulating a buffered request, along with the reservation that makes the
-// buffered request valid. These two pieces of data are coupled together because lifetime of the
-// operations must not exceed the lifetime of the reservation itself.
-//
-// This struct is used for both journaled metadata and unjournaled data.
-struct JournalWorkItem {
-  JournalWorkItem(fs::BlockingRingBufferReservation reservation,
-                  fbl::Vector<fs::BufferedOperation> operations)
-      : reservation(std::move(reservation)), operations(std::move(operations)) {}
-
-  fs::BlockingRingBufferReservation reservation;
-  fbl::Vector<fs::BufferedOperation> operations;
-};
-
-// The back-end of the journal. This class implements all the blocking operations which transmit
-// buffers to disk, without providing higher-level guarantees about asynchrony or execution
-// ordering.
-//
-// This class is thread-compatible.
-// This class is not movable or copyable.
-class JournalWriter {
- public:
-  JournalWriter(fs::TransactionHandler* transaction_handler, JournalSuperblock journal_superblock,
-                uint64_t journal_start_block, uint64_t entries_length);
-  explicit JournalWriter(fs::TransactionHandler* transaction_handler);
-  JournalWriter(const JournalWriter&) = delete;
-  JournalWriter& operator=(const JournalWriter&) = delete;
-  JournalWriter(JournalWriter&& other) = delete;
-  JournalWriter& operator=(JournalWriter&& other) = delete;
-
-  // Writes |work| to disk immediately.
-  fit::result<void, zx_status_t> WriteData(JournalWorkItem work);
-
-  // Writes |work| to disk immediately (possibly also to the journal)
-  //
-  // Updating metadata has three phases:
-  // 1) Updating the info block (if necessary to make space)
-  // 2) Write metadata to the journal itself.
-  // 3) Write metadata to the final on-disk location.
-  //
-  // This method currently blocks, completing all three phases before returning, but in the future,
-  // could be more fine grained, returning a promise that represents the completion of all phases.
-  fit::result<void, zx_status_t> WriteMetadata(JournalWorkItem work);
-
-  // Synchronizes the most up-to-date info block back to disk.
-  //
-  // Returns ZX_ERR_IO_REFUSED if writeback is disabled.
-  // Returns an error from the block device if the info block cannot be written.
-  // In all other cases, returns ZX_OK.
-  fit::result<void, zx_status_t> Sync();
-
-  // Returns true if journaling is "on", and metadata is treated differently
-  // from regular data.
-  //
-  // This method is thread-safe.
-  [[nodiscard]] bool IsJournalingEnabled() const { return entries_length_ != 0; }
-
- private:
-  // Returns true if all writeback is "off", and no further data will be written to the
-  // device.
-  [[nodiscard]] bool IsWritebackEnabled() const { return transaction_handler_; }
-
-  // Deactivates all writeback, calling all subsequent write operations to fail.
-  void DisableWriteback() { transaction_handler_ = nullptr; }
-
-  // Returns the start of the portion of the journal which stores metadata.
-  [[nodiscard]] uint64_t InfoStartBlock() const { return journal_start_block_; }
-
-  // Returns the length of the portion of the journal which stores metadata.
-  [[nodiscard]] static uint64_t InfoLength() { return kJournalMetadataBlocks; }
-
-  // Returns the start of the portion of the journal which stores entries.
-  [[nodiscard]] uint64_t EntriesStartBlock() const {
-    return journal_start_block_ + kJournalMetadataBlocks;
-  }
-
-  // Returns the length of the portion of the journal which stores entries.
-  [[nodiscard]] uint64_t EntriesLength() const { return entries_length_; }
-
-  // Writes |work| to the journal, and flushes it to the underlying device.
-  //
-  // Blocks the calling thread on I/O until the operation completes.
-  zx_status_t WriteMetadataToJournal(JournalWorkItem* work);
-
-  // Writes the info block if adding a |block_count| block entry to the journal
-  // will hit the start of the journal.
-  zx_status_t WriteInfoBlockIfIntersect(uint64_t block_count);
-
-  // Writes the info block to the underlying device.
-  // Asserts that the sequence number has increased, and that the info block has
-  // a meaningful update.
-  //
-  // Blocks the calling thread on I/O until the operation completes.
-  zx_status_t WriteInfoBlock();
-
-  // Writes an operation into the journal, creating a sequence of operations
-  // which deal with wraparound of the in-memory |reservation| buffer and the on-disk
-  // journal. Additionally, issues these operations to the underlying device and
-  // returns the result (see |WriteOperations|).
-  zx_status_t WriteOperationToJournal(const fs::BlockingRingBufferReservation& reservation);
-
-  // Writes operations directly through to disk.
-  //
-  // If any operations fail, this method will return the resulting error from the underlying
-  // block device. Afterwards, however, this function will exclusively return |ZX_ERR_IO_REFUSED|
-  // to prevent "partial operations" from being written to the underlying device.
-  zx_status_t WriteOperations(const fbl::Vector<fs::BufferedOperation>& operations);
-
-  fs::TransactionHandler* transaction_handler_ = nullptr;
-  JournalSuperblock journal_superblock_;
-  // Relative to the start of the filesystem. Points to the journal info block.
-  uint64_t journal_start_block_ = 0;
-  // The value of the sequence_number to be used in the next entry which is written to the
-  // journal.
-  uint64_t next_sequence_number_ = 0;
-  // Relative to |kJournalMetadataBlocks| (the start of entries).
-  uint64_t next_entry_start_block_ = 0;
-  const uint64_t entries_length_ = 0;
-};
-
-}  // namespace internal
 
 // This class implements an interface for filesystems to write back data to the underlying
 // device. It provides methods for the following functionality:
---
+++ zircon/system/ulib/fs/journal/journal-writer.cc
@@ -0,0 +1,244 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/sync/completion.h>
+#include <zircon/status.h>
+
+#include <fs/journal/internal/journal-writer.h>
+#include <fs/trace.h>
+#include <fs/transaction/writeback.h>
+
+#include "entry-view.h"
+
+namespace fs {
+namespace {
+
+using fs::FlushWriteRequests;
+using fs::OperationType;
+
+}  // namespace
+
+namespace internal {
+
+JournalWriter::JournalWriter(TransactionHandler* transaction_handler,
+                             JournalSuperblock journal_superblock, uint64_t journal_start_block,
+                             uint64_t entries_length)
+    : transaction_handler_(transaction_handler),
+      journal_superblock_(std::move(journal_superblock)),
+      journal_start_block_(journal_start_block),
+      next_sequence_number_(journal_superblock_.sequence_number()),
+      next_entry_start_block_(journal_superblock_.start()),
+      entries_length_(entries_length) {}
+
+JournalWriter::JournalWriter(TransactionHandler* transaction_handler)
+    : transaction_handler_(transaction_handler) {}
+
+fit::result<void, zx_status_t> JournalWriter::WriteData(JournalWorkItem work) {
+  zx_status_t status = WriteOperations(work.operations);
+  if (status != ZX_OK) {
+    FS_TRACE_ERROR("journal: Failed to write data: %s\n", zx_status_get_string(status));
+    return fit::error(status);
+  }
+  return fit::ok();
+}
+
+fit::result<void, zx_status_t> JournalWriter::WriteMetadata(JournalWorkItem work) {
+  const uint64_t block_count = work.reservation.length();
+  FS_TRACE_DEBUG("WriteMetadata: Writing %zu blocks (includes header, commit)\n", block_count);
+
+  // Ensure the info block is caught up, so it doesn't point to the middle of an invalid entry.
+  zx_status_t status = WriteInfoBlockIfIntersect(block_count);
+  if (status != ZX_OK) {
+    FS_TRACE_ERROR("WriteMetadata: Failed to write info block: %s\n", zx_status_get_string(status));
+    return fit::error(status);
+  }
+
+  // Write metadata to the journal itself.
+  status = WriteMetadataToJournal(&work);
+  if (status != ZX_OK) {
+    FS_TRACE_ERROR("WriteMetadata: Failed to write metadata to journal: %s\n",
+                   zx_status_get_string(status));
+    return fit::error(status);
+  }
+
+  // Write metadata to the final on-disk, non-journal location.
+  status = WriteOperations(work.operations);
+  if (status != ZX_OK) {
+    FS_TRACE_ERROR("WriteMetadata: Failed to write metadata to final location: %s\n",
+                   zx_status_get_string(status));
+    return fit::error(status);
+  }
+  return fit::ok();
+}
+
+zx_status_t JournalWriter::WriteOperationToJournal(const BlockBufferView& view) {
+  const uint64_t total_block_count = view.length();
+  const uint64_t max_reservation_size = EntriesLength();
+  uint64_t written_block_count = 0;
+  fbl::Vector<BufferedOperation> journal_operations;
+  BufferedOperation operation;
+  operation.vmoid = view.vmoid();
+  operation.op.type = OperationType::kWrite;
+
+  // Both the reservation and the on-disk location may wraparound.
+  while (written_block_count != total_block_count) {
+    operation.op.vmo_offset = (view.start() + written_block_count) % max_reservation_size;
+    operation.op.dev_offset = EntriesStartBlock() + next_entry_start_block_;
+
+    // The maximum number of blocks that can be written to the journal, on-disk, before needing to
+    // wrap around.
+    const uint64_t journal_block_max = EntriesLength() - next_entry_start_block_;
+    // The maximum number of blocks that can be written from the reservation, in-memory, before
+    // needing to wrap around.
+    const uint64_t reservation_block_max = max_reservation_size - operation.op.vmo_offset;
+    operation.op.length = std::min(total_block_count - written_block_count,
+                                   std::min(journal_block_max, reservation_block_max));
+    journal_operations.push_back(operation);
+    written_block_count += operation.op.length;
+    next_entry_start_block_ = (next_entry_start_block_ + operation.op.length) % EntriesLength();
+  }
+
+  zx_status_t status = WriteOperations(journal_operations);
+  if (status != ZX_OK) {
+    FS_TRACE_ERROR("JournalWriter::WriteOperationToJournal: Failed to write: %s\n",
+                   zx_status_get_string(status));
+    return status;
+  }
+  return status;
+}
+
+fit::result<void, zx_status_t> JournalWriter::Sync() {
+  if (!IsWritebackEnabled()) {
+    return fit::error(ZX_ERR_IO_REFUSED);
+  }
+  if (!IsJournalingEnabled()) {
+    return fit::ok();
+  }
+
+  if (next_sequence_number_ == journal_superblock_.sequence_number()) {
+    FS_TRACE_DEBUG("Sync: Skipping write to info block (no sequence update)\n");
+    return fit::ok();
+  }
+
+  zx_status_t status = WriteInfoBlock();
+  if (status != ZX_OK) {
+    return fit::error(status);
+  }
+  return fit::ok();
+}
+
+zx_status_t JournalWriter::WriteMetadataToJournal(JournalWorkItem* work) {
+  FS_TRACE_DEBUG("WriteMetadataToJournal: Writing %zu blocks with sequence_number %zu\n",
+                 work->reservation.length(), next_sequence_number_);
+
+  // Set the header and commit blocks within the journal.
+  JournalEntryView entry(work->reservation.buffer_view(), work->operations,
+                         next_sequence_number_++);
+
+  zx_status_t status = WriteOperationToJournal(work->reservation.buffer_view());
+  // Although the payload may be encoded while written to the journal, it should be decoded
+  // when written to the final on-disk location later.
+  entry.DecodePayloadBlocks();
+  return status;
+}
+
+zx_status_t JournalWriter::WriteInfoBlockIfIntersect(uint64_t block_count) {
+  // We need to write the info block now if [journal tail, journal tail + block_count)
+  // intersects with [journal head, journal tail).
+  //
+  // Logically, the journal is a circular buffer:
+  //
+  //   [ ____, ____, ____, ____, ____, ____ ]
+  //
+  // Within that buffer, the journal has some entries which will be replayed
+  //
+  //           Info Block        Next Entry Start Block
+  //           |                 |
+  //   [ ____, head, data, tail, ____, ____ ]
+  //
+  // In this diagram, it would be safe to write one, two, or three additional blocks:
+  // they would fit within the journal. However, if four blocks are written, the journal
+  // would "eat its own head":
+  //
+  //           Info Block
+  //           |
+  //   [ blk3, blk4, data, tail, blk1, blk2 ]
+  //           |
+  //           Collision!
+  //
+  // If power failure occurred, replay would be unable to parse prior entries, since the
+  // start block would point to an invalid entry. However, if we also wrote the info block
+  // repeatedly, the journaling code would incur a significant write amplification cost.
+  //
+  // To compromise, we write the info block before any writes that would trigger this collision.
+  const uint64_t head = journal_superblock_.start();
+  const uint64_t tail = next_entry_start_block_;
+  const uint64_t capacity = EntriesLength();
+
+  // It's a little tricky to distinguish between an "empty" and "full" journal, so we observe
+  // that case explicitly first, using the sequence number to make the distinction.
+  //
+  // We require an info block update if the journal is full, but not if it's empty.
+  bool write_info =
+      (head == tail) && (next_sequence_number_ != journal_superblock_.sequence_number());
+
+  if (!write_info) {
+    const uint64_t journal_used = (head <= tail) ? (tail - head) : ((capacity - head) + tail);
+    const uint64_t journal_free = capacity - journal_used;
+    if (journal_free < block_count) {
+      FS_TRACE_DEBUG("WriteInfoBlockIfIntersect: Writing info block (can't write %zu blocks)\n",
+                     block_count);
+      write_info = true;
+    } else {
+      FS_TRACE_DEBUG("WriteInfoBlockIfIntersect: Not writing info (have %zu, need %zu blocks)\n",
+                     journal_free, block_count);
+    }
+  }
+
+  if (write_info) {
+    zx_status_t status = WriteInfoBlock();
+    if (status != ZX_OK) {
+      FS_TRACE_ERROR("WriteInfoBlockIfIntersect: Failed to write info block\n");
+      return status;
+    }
+  }
+  return ZX_OK;
+}
+
+zx_status_t JournalWriter::WriteInfoBlock() {
+  ZX_DEBUG_ASSERT(next_sequence_number_ > journal_superblock_.sequence_number());
+  FS_TRACE_DEBUG("WriteInfoBlock: Updating sequence_number from %zu to %zu\n",
+                 journal_superblock_.sequence_number(), next_sequence_number_);
+
+  ZX_DEBUG_ASSERT(next_entry_start_block_ < EntriesLength());
+  journal_superblock_.Update(next_entry_start_block_, next_sequence_number_);
+  fbl::Vector<BufferedOperation> journal_operations;
+  BufferedOperation operation;
+  operation.vmoid = journal_superblock_.buffer().vmoid();
+  operation.op.type = OperationType::kWrite;
+  operation.op.vmo_offset = 0;
+  operation.op.dev_offset = InfoStartBlock();
+  operation.op.length = InfoLength();
+  journal_operations.push_back(operation);
+  return WriteOperations(journal_operations);
+}
+
+zx_status_t JournalWriter::WriteOperations(const fbl::Vector<BufferedOperation>& operations) {
+  if (!IsWritebackEnabled()) {
+    FS_TRACE_ERROR("WriteOperations: Not issuing writeback because writeback is disabled\n");
+    return ZX_ERR_IO_REFUSED;
+  }
+
+  zx_status_t status = FlushWriteRequests(transaction_handler_, operations);
+  if (status != ZX_OK) {
+    FS_TRACE_ERROR("WriteOperations: Failed to write requests: %s. Filesystem now read-only.\n",
+                   zx_status_get_string(status));
+    DisableWriteback();
+    return status;
+  }
+  return ZX_OK;
+}
+
+}  // namespace internal
+}  // namespace fs
