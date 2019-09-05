651bcc2b3a77415df0ee5f14a4f0e590a1cfa7f6
Justin Mattson <jmatt@google.com>
2019-09-05 00:01:39 +0000

    [component_manager] Make code more canonical
    Compact and make code more canonical. Also shorten over-length
    lines.
    
    Change-Id: Iaea140eb01751930386fbdb2dc64ffd78ef072aa

--- src/sys/component_manager/tests/shutdown_integration.rs
+++ src/sys/component_manager/tests/shutdown_integration.rs
@@ -4,34 +4,18 @@
 
 #![feature(async_await)]
 
-use {
-    failure::{Error},
-    fuchsia_async as fasync,
-    test_utils,
-};
+use {failure::Error, fuchsia_async as fasync, test_utils};
 
 #[fasync::run_singlethreaded(test)]
 async fn test() -> Result<(), Error> {
     let app = test_utils::launch_test_component(
-        "fuchsia-pkg://fuchsia.com/shutdown_integration_test#meta/component_manager.cmx".to_string(),
-        Some(vec![
-            "fuchsia-pkg://fuchsia.com/shutdown_integration_test#meta/shutdown_integration_root.cm".to_string(),
-        ])
-    ).expect("failed to launch component manager for test");
+        "fuchsia-pkg://fuchsia.com/shutdown_integration_test#meta/component_manager.cmx"
+            .to_string(),
+        Some(vec!["fuchsia-pkg://fuchsia.com/shutdown_integration_test\
+                   #meta/shutdown_integration_root.cm"
+            .to_string()]),
+    )
+    .expect("failed to launch component manager for test");
 
-    match app.wait_with_output().await {
-        Ok(output) => {
-            match output.exit_status.ok() {
-                Ok(()) => {
-                    Ok(())
-                },
-                Err(e) => {
-                    Err(e.into())
-                }
-            }
-        },
-        Err(e) => {
-            Err(e.into())
-        }
-    }
+    app.wait_with_output().await.and_then(|result| result.exit_status.ok().map_err(|e| e.into()))
 }
