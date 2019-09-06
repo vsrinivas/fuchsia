82b01f13f9a3ebba6434a5f1cc99d8d8610e7517
Payton Turnage <turnage@google.com>
2019-09-06 23:28:09 +0000

    [media.audio] EXPECT_FLOAT_EQ in GainCurve tests
    s/ASSERT_EQ/EXPECT_FLOAT_EQ in GainCurve tests.
    
    Replaces asserts with expects so more failures are printed.
    
    Replaces EQ asserts with a matcher that selects an fp epsilon to
    prevent flakes. None have yet occurred but they are possible.
    
    TEST: test only change
    Change-Id: I74546fefc09b76871e50cc673bbe293ac3ffbfff

--- src/media/audio/audio_core/gain_curve_unittest.cc
+++ src/media/audio/audio_core/gain_curve_unittest.cc
@@ -16,13 +16,13 @@ namespace {
 TEST(GainCurveTest, ValidationRejectsInsufficientMappings) {
   auto result1 = GainCurve::FromMappings({});
   ASSERT_TRUE(result1.is_error());
-  ASSERT_EQ(result1.error(), GainCurve::kLessThanTwoMappingsCannotMakeCurve);
+  EXPECT_EQ(result1.error(), GainCurve::kLessThanTwoMappingsCannotMakeCurve);
 
   auto result2 = GainCurve::FromMappings({
       GainCurve::VolumeMapping(0.0, 0.0),
   });
   ASSERT_TRUE(result2.is_error());
-  ASSERT_EQ(result2.error(), GainCurve::kLessThanTwoMappingsCannotMakeCurve);
+  EXPECT_EQ(result2.error(), GainCurve::kLessThanTwoMappingsCannotMakeCurve);
 }
 
 TEST(GainCurveTest, ValidationRejectsInsufficientDomain) {
@@ -31,14 +31,14 @@ TEST(GainCurveTest, ValidationRejectsInsufficientDomain) {
       GainCurve::VolumeMapping(0.5, 0.0),
   });
   ASSERT_TRUE(result1.is_error());
-  ASSERT_EQ(result1.error(), GainCurve::kDomain0to1NotCovered);
+  EXPECT_EQ(result1.error(), GainCurve::kDomain0to1NotCovered);
 
   auto result2 = GainCurve::FromMappings({
       GainCurve::VolumeMapping(0.2, -0.45),
       GainCurve::VolumeMapping(1.0, 0.0),
   });
   ASSERT_TRUE(result2.is_error());
-  ASSERT_EQ(result2.error(), GainCurve::kDomain0to1NotCovered);
+  EXPECT_EQ(result2.error(), GainCurve::kDomain0to1NotCovered);
 }
 
 TEST(GainCurveTest, ValidationRejectsInsufficientRange) {
@@ -47,7 +47,7 @@ TEST(GainCurveTest, ValidationRejectsInsufficientRange) {
       GainCurve::VolumeMapping(1.0, -1.0),
   });
   ASSERT_TRUE(result1.is_error());
-  ASSERT_EQ(result1.error(), GainCurve::kRange0NotCovered);
+  EXPECT_EQ(result1.error(), GainCurve::kRange0NotCovered);
 }
 
 TEST(GainCurveTest, ValidationRejectsNonIncreasingDomains) {
@@ -58,7 +58,7 @@ TEST(GainCurveTest, ValidationRejectsNonIncreasingDomains) {
       GainCurve::VolumeMapping(1.0, 0.0),
   });
   ASSERT_TRUE(result1.is_error());
-  ASSERT_EQ(result1.error(), GainCurve::kNonIncreasingDomainIllegal);
+  EXPECT_EQ(result1.error(), GainCurve::kNonIncreasingDomainIllegal);
 
   auto result2 = GainCurve::FromMappings({
       GainCurve::VolumeMapping(0.0, -100.0),
@@ -67,7 +67,7 @@ TEST(GainCurveTest, ValidationRejectsNonIncreasingDomains) {
       GainCurve::VolumeMapping(1.0, 0.0),
   });
   ASSERT_TRUE(result2.is_error());
-  ASSERT_EQ(result2.error(), GainCurve::kNonIncreasingDomainIllegal);
+  EXPECT_EQ(result2.error(), GainCurve::kNonIncreasingDomainIllegal);
 }
 
 TEST(GainCurveTest, ValidationRejectsNonIncreasingRanges) {
@@ -78,7 +78,7 @@ TEST(GainCurveTest, ValidationRejectsNonIncreasingRanges) {
       GainCurve::VolumeMapping(1.0, 0.0),
   });
   ASSERT_TRUE(result1.is_error());
-  ASSERT_EQ(result1.error(), GainCurve::kNonIncreasingRangeIllegal);
+  EXPECT_EQ(result1.error(), GainCurve::kNonIncreasingRangeIllegal);
 
   auto result2 = GainCurve::FromMappings({
       GainCurve::VolumeMapping(0.0, -2.0),
@@ -87,7 +87,7 @@ TEST(GainCurveTest, ValidationRejectsNonIncreasingRanges) {
       GainCurve::VolumeMapping(1.0, 0.0),
   });
   ASSERT_TRUE(result2.is_error());
-  ASSERT_EQ(result2.error(), GainCurve::kNonIncreasingRangeIllegal);
+  EXPECT_EQ(result2.error(), GainCurve::kNonIncreasingRangeIllegal);
 }
 
 TEST(GainCurveTest, VolumeToDbBasic) {
@@ -99,37 +99,37 @@ TEST(GainCurveTest, VolumeToDbBasic) {
   ASSERT_TRUE(curve_result.is_ok());
   auto curve = curve_result.take_value();
 
-  ASSERT_EQ(curve.VolumeToDb(0.0), -100.0);
-  ASSERT_EQ(curve.VolumeToDb(0.25), -75.0);
-  ASSERT_EQ(curve.VolumeToDb(0.5), -50.0);
-  ASSERT_EQ(curve.VolumeToDb(0.75), -25.0);
-  ASSERT_EQ(curve.VolumeToDb(1.0), 0.0);
+  EXPECT_FLOAT_EQ(curve.VolumeToDb(0.0), -100.0);
+  EXPECT_FLOAT_EQ(curve.VolumeToDb(0.25), -75.0);
+  EXPECT_FLOAT_EQ(curve.VolumeToDb(0.5), -50.0);
+  EXPECT_FLOAT_EQ(curve.VolumeToDb(0.75), -25.0);
+  EXPECT_FLOAT_EQ(curve.VolumeToDb(1.0), 0.0);
 }
 
 TEST(GainCurveTest, DefaultCurves) {
   auto curve = GainCurve::Default();
 
-  ASSERT_EQ(curve.VolumeToDb(0.0), fuchsia::media::audio::MUTED_GAIN_DB);
-  ASSERT_EQ(curve.VolumeToDb(1.0), 0.0);
+  EXPECT_FLOAT_EQ(curve.VolumeToDb(0.0), fuchsia::media::audio::MUTED_GAIN_DB);
+  EXPECT_FLOAT_EQ(curve.VolumeToDb(1.0), Gain::kUnityGainDb);
 
   const auto middle = curve.VolumeToDb(0.5);
-  ASSERT_GT(middle, fuchsia::media::audio::MUTED_GAIN_DB);
-  ASSERT_LT(middle, 0.0);
+  EXPECT_GT(middle, fuchsia::media::audio::MUTED_GAIN_DB);
+  EXPECT_LT(middle, 0.0);
 }
 
 TEST(GainCurveTest, DefaultCurveWithMinGainDb) {
   auto curve100 = GainCurve::DefaultForMinGain(-100.0);
   auto curve50 = GainCurve::DefaultForMinGain(-50.0);
 
-  ASSERT_EQ(curve100.VolumeToDb(0.0), fuchsia::media::audio::MUTED_GAIN_DB);
-  ASSERT_EQ(curve100.VolumeToDb(0.0), fuchsia::media::audio::MUTED_GAIN_DB);
-  ASSERT_EQ(curve50.VolumeToDb(1.0), Gain::kUnityGainDb);
-  ASSERT_EQ(curve50.VolumeToDb(1.0), Gain::kUnityGainDb);
+  EXPECT_FLOAT_EQ(curve100.VolumeToDb(0.0), fuchsia::media::audio::MUTED_GAIN_DB);
+  EXPECT_FLOAT_EQ(curve100.VolumeToDb(0.0), fuchsia::media::audio::MUTED_GAIN_DB);
+  EXPECT_FLOAT_EQ(curve50.VolumeToDb(1.0), Gain::kUnityGainDb);
+  EXPECT_FLOAT_EQ(curve50.VolumeToDb(1.0), Gain::kUnityGainDb);
 
   const auto middle100 = curve100.VolumeToDb(0.5);
   const auto middle50 = curve50.VolumeToDb(0.5);
 
-  ASSERT_LT(middle100, middle50);
+  EXPECT_LT(middle100, middle50);
 }
 
 }  // namespace
