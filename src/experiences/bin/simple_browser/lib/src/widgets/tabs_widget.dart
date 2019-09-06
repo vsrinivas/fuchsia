16bc6f5d47af111b0b7ea0e2413a51c4bed430e1
Alon Hetzroni <ahetzroni@google.com>
2019-09-06 13:46:08 +0000

    [simple_browser] Hide close on unfocused tabs
    Hide close button on unfocused tabs,
    but reveal it on mouse hover.
    
    BUG: 36162
    Change-Id: I68f4b37490832fdd54387614ee418dc599d27866

--- src/experiences/bin/simple_browser/lib/src/widgets/tabs_widget.dart
+++ src/experiences/bin/simple_browser/lib/src/widgets/tabs_widget.dart
@@ -31,12 +31,7 @@ class TabsWidget extends StatelessWidget {
   }
 
   Iterable<Widget> _buildPageTabs({BuildContext context}) => bloc.tabs
-      .map(
-        (tab) => _buildTab(
-          context: context,
-          tab: tab,
-        ),
-      )
+      .map(_buildTab)
       // add a 1pip separator before every tab,
       // divide the rest of the space between tabs
       .expand((item) => [
@@ -46,65 +41,93 @@ class TabsWidget extends StatelessWidget {
       // skip the first separator
       .skip(1);
 
-  Widget _buildTab({
-    BuildContext context,
+  Widget _buildTab(
     WebPageBloc tab,
-  }) {
-    final selected = tab == bloc.currentTab;
-    return AnimatedBuilder(
-      animation: tab.pageTitleNotifier,
-      builder: (_, __) => GestureDetector(
-        onTap: () {
+  ) =>
+      _TabWidget(
+        bloc: tab,
+        selected: tab == bloc.currentTab,
+        onSelect: () {
           bloc.request.add(FocusTabAction(tab: tab));
         },
+        onClose: () {
+          bloc.request.add(RemoveTabAction(tab: tab));
+        },
+      );
+}
+
+class _TabWidget extends StatefulWidget {
+  const _TabWidget({this.bloc, this.selected, this.onSelect, this.onClose});
+  final WebPageBloc bloc;
+  final bool selected;
+  final VoidCallback onSelect;
+  final VoidCallback onClose;
+
+  @override
+  _TabWidgetState createState() => _TabWidgetState();
+}
+
+class _TabWidgetState extends State<_TabWidget> {
+  final _hovering = ValueNotifier<bool>(false);
+  @override
+  Widget build(BuildContext context) {
+    final baseTheme = Theme.of(context);
+    return MouseRegion(
+      onEnter: (_) {
+        _hovering.value = true;
+      },
+      onExit: (_) {
+        _hovering.value = false;
+      },
+      child: GestureDetector(
+        onTap: widget.onSelect,
         child: Container(
-          color: selected
-              ? Theme.of(context).accentColor
-              : Theme.of(context).primaryColor,
-          child: Stack(
-            children: <Widget>[
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 4.0),
-                  child: Text(
-                    tab.pageTitle ?? 'NEW TAB',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: selected
-                          ? Theme.of(context).primaryColor
-                          : Theme.of(context).accentColor,
+          color:
+              widget.selected ? baseTheme.accentColor : baseTheme.primaryColor,
+          child: DefaultTextStyle(
+            style: baseTheme.textTheme.body1.copyWith(
+              color: widget.selected ? baseTheme.primaryColor : null,
+            ),
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: AnimatedBuilder(
+                      animation: widget.bloc.pageTitleNotifier,
+                      builder: (_, __) => Text(
+                        widget.bloc.pageTitle ?? 'NEW TAB',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Positioned(
-                top: 0.0,
-                right: 0.0,
-                bottom: 0.0,
-                child: AspectRatio(
-                  aspectRatio: 1.0,
-                  child: GestureDetector(
-                    onTap: () {
-                      bloc.request.add(RemoveTabAction(tab: tab));
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(1.0),
-                      child: Center(
-                        child: Text(
-                          '×',
-                          style: TextStyle(
-                            color: selected
-                                ? Theme.of(context).primaryColor
-                                : Theme.of(context).accentColor,
-                          ),
+                Positioned(
+                  top: 0.0,
+                  right: 0.0,
+                  bottom: 0.0,
+                  child: AnimatedBuilder(
+                    animation: _hovering,
+                    builder: (_, child) => Offstage(
+                      offstage: !(widget.selected || _hovering.value),
+                      child: child,
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: 1.0,
+                      child: GestureDetector(
+                        onTap: widget.onClose,
+                        child: Container(
+                          padding: const EdgeInsets.all(1.0),
+                          alignment: Alignment.center,
+                          child: Text('×'),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
