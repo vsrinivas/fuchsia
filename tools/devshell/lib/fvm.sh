b4dc71e8e295769b6970e8852dea33fe823c3f79
Renato Mangini Dias <mangini@google.com>
2019-09-06 19:49:18 +0000

    [fx] Add image size option to fx aemu
    Bug: 6618
    Change-Id: I3dbe5f112aac202227dd9ca02725412d2fdae851

--- tools/devshell/lib/fvm.sh
+++ tools/devshell/lib/fvm.sh
@@ -6,9 +6,11 @@
 # Arguments:
 #   - raw image file
 #   - extended image file
+#   - (optional) size of extended image file, defaults to original*2
 function fx-fvm-extend-image {
   fvmraw=$1
   fvmimg=$2
+
   stat_flags=()
   if [[ $(uname) == "Darwin" ]]; then
     stat_flags+=("-x")
@@ -16,7 +18,16 @@ function fx-fvm-extend-image {
   stat_output=$(stat "${stat_flags[@]}" "${fvmraw}")
   if [[ "$stat_output" =~ Size:\ ([0-9]+) ]]; then
     size="${BASH_REMATCH[1]}"
-    newsize=$(($size * 2))
+    recommended_size=$(($size * 2))
+    if [[ $# -gt 2 && -n "$3" ]]; then
+      newsize=$3
+      if [[ "${newsize}" -le "${size}" ]]; then
+        fx-error "Image size has to be greater than ${size} bytes.  Recommended value is ${recommended_size} bytes."
+        return 1
+      fi
+    else
+      newsize="${recommended_size}"
+    fi
     # We must take a copy of the build artifact, rather than re-use it, as we
     # need to modify it in order to extend it.
     echo -n "Creating disk image..."
