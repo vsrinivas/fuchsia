c5bda351c19089f0afe957a29ab74afa95b6e707
Benjamin Wright <benwright@google.com>
2019-09-09 21:17:35 +0000

    [rust][zircon] Add Resource::Create.
    Implements the abstract API surface for zx_resource_create.
    
    Bug: 35531
    Change-Id: I3eef49bb812aaf4b406792c74562952bcae37096

--- garnet/public/rust/fuchsia-zircon/src/resource.rs
+++ garnet/public/rust/fuchsia-zircon/src/resource.rs
@@ -4,7 +4,9 @@
 
 //! Type-safe bindings for Zircon resources.
 
-use crate::{AsHandleRef, Handle, HandleBased, HandleRef};
+use crate::ok;
+use crate::{AsHandleRef, Handle, HandleBased, HandleRef, Status};
+use fuchsia_zircon_sys as sys;
 
 /// An object representing a Zircon resource.
 ///
@@ -13,3 +15,49 @@ use crate::{AsHandleRef, Handle, HandleBased, HandleRef};
 #[repr(transparent)]
 pub struct Resource(Handle);
 impl_handle_based!(Resource);
+
+impl Resource {
+    /// Create a child resource object.
+    ///
+    /// Wraps the
+    /// [zx_resource_create](https://fuchsia.googlesource.com/fuchsia/+/master/docs/zircon/syscalls/resource_create.md)
+    /// syscall
+    pub fn create_child(
+        &self,
+        options: u32,
+        base: u64,
+        size: usize,
+        name: &[u8],
+    ) -> Result<Resource, Status> {
+        let mut resource_out = 0;
+        let name_ptr = name.as_ptr();
+        let name_len = name.len();
+        let status = unsafe {
+            sys::zx_resource_create(
+                self.raw_handle(),
+                options,
+                base,
+                size,
+                name_ptr,
+                name_len,
+                &mut resource_out,
+            )
+        };
+        ok(status)?;
+        unsafe { Ok(Resource::from(Handle::from_raw(resource_out))) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_child() {
+        let invalid_resource = Resource::from(Handle::invalid());
+        assert_eq!(
+            invalid_resource.create_child(sys::ZX_RSRC_KIND_VMEX, 0, 0, b"vmex"),
+            Err(Status::BAD_HANDLE)
+        );
+    }
+}
