bee3c3114837dbc208298eb38564791633715515
Michael Brunson <mbrunson@google.com>
2019-09-13 02:19:45 +0000

    [factory] Add fakes for factory store providers.
    Adds fakes for factory store providers for easier testing of client
    components.
    
    Test: fx set core.astro \
              --with src/factory/fake_factory_store_providers:tests
          fx build && fx ota
          fx run-test fake_factory_store_providers_tests
    
    Change-Id: I0a6b3dbc7dc2a4dc0f0ba13e24ca60daa237dad4

---
+++ src/factory/fake_factory_store_providers/src/main.rs
@@ -0,0 +1,175 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![feature(async_await)]
+
+use {
+    failure::{format_err, Error},
+    fidl_fuchsia_factory::{
+        CastCredentialsFactoryStoreProviderRequest,
+        CastCredentialsFactoryStoreProviderRequestStream, MiscFactoryStoreProviderRequest,
+        MiscFactoryStoreProviderRequestStream, PlayReadyFactoryStoreProviderRequest,
+        PlayReadyFactoryStoreProviderRequestStream, WidevineFactoryStoreProviderRequest,
+        WidevineFactoryStoreProviderRequestStream,
+    },
+    fidl_fuchsia_io::{DirectoryMarker, DirectoryProxy, MODE_TYPE_DIRECTORY, OPEN_RIGHT_READABLE},
+    fuchsia_async as fasync,
+    fuchsia_component::server::ServiceFs,
+    fuchsia_syslog::{self as syslog, macros::*},
+    fuchsia_vfs_pseudo_fs::{
+        directory::entry::DirectoryEntry, file::simple::read_only_str, tree_builder::TreeBuilder,
+    },
+    futures::{lock::Mutex, prelude::*},
+    serde_json::from_reader,
+    std::{collections::HashMap, fs::File, iter, str::FromStr, sync::Arc},
+    structopt::StructOpt,
+};
+
+type LockedDirectoryProxy = Arc<Mutex<DirectoryProxy>>;
+
+enum IncomingServices {
+    CastCredentialsFactoryStoreProvider(CastCredentialsFactoryStoreProviderRequestStream),
+    MiscFactoryStoreProvider(MiscFactoryStoreProviderRequestStream),
+    PlayReadyFactoryStoreProvider(PlayReadyFactoryStoreProviderRequestStream),
+    WidevineFactoryStoreProvider(WidevineFactoryStoreProviderRequestStream),
+}
+
+fn start_test_dir(config_path: &str) -> Result<DirectoryProxy, Error> {
+    let files: HashMap<String, String> = match File::open(&config_path) {
+        Ok(file) => from_reader(file)?,
+        Err(err) => {
+            fx_log_warn!("publishing empty directory for {} due to error: {:?}", &config_path, err);
+            HashMap::new()
+        }
+    };
+
+    fx_log_info!("Files from {}: {:?}", &config_path, files);
+
+    let mut tree = TreeBuilder::empty_dir();
+
+    for (name, contents) in files.into_iter() {
+        tree.add_entry(
+            &name.split("/").collect::<Vec<&str>>(),
+            read_only_str(move || Ok(contents.to_string())),
+        )
+        .unwrap();
+    }
+
+    let mut test_dir = tree.build();
+
+    let (test_dir_proxy, test_dir_service) = fidl::endpoints::create_proxy::<DirectoryMarker>()?;
+    test_dir.open(
+        OPEN_RIGHT_READABLE,
+        MODE_TYPE_DIRECTORY,
+        &mut iter::empty(),
+        test_dir_service.into_channel().into(),
+    );
+
+    fasync::spawn(async move {
+        let _ = test_dir.await;
+    });
+
+    Ok(test_dir_proxy)
+}
+
+async fn run_server(req: IncomingServices, dir_mtx: LockedDirectoryProxy) -> Result<(), Error> {
+    match req {
+        IncomingServices::CastCredentialsFactoryStoreProvider(mut stream) => {
+            while let Some(request) = stream.try_next().await? {
+                let CastCredentialsFactoryStoreProviderRequest::GetFactoryStore {
+                    dir,
+                    control_handle: _,
+                } = request;
+                dir_mtx.lock().await.clone(OPEN_RIGHT_READABLE, dir.into_channel().into())?;
+            }
+        }
+        IncomingServices::MiscFactoryStoreProvider(mut stream) => {
+            while let Some(request) = stream.try_next().await? {
+                let MiscFactoryStoreProviderRequest::GetFactoryStore { dir, control_handle: _ } =
+                    request;
+                dir_mtx.lock().await.clone(OPEN_RIGHT_READABLE, dir.into_channel().into())?;
+            }
+        }
+        IncomingServices::PlayReadyFactoryStoreProvider(mut stream) => {
+            while let Some(request) = stream.try_next().await? {
+                let PlayReadyFactoryStoreProviderRequest::GetFactoryStore {
+                    dir,
+                    control_handle: _,
+                } = request;
+                dir_mtx.lock().await.clone(OPEN_RIGHT_READABLE, dir.into_channel().into())?;
+            }
+        }
+        IncomingServices::WidevineFactoryStoreProvider(mut stream) => {
+            while let Some(request) = stream.try_next().await? {
+                let WidevineFactoryStoreProviderRequest::GetFactoryStore { dir, control_handle: _ } =
+                    request;
+                dir_mtx.lock().await.clone(OPEN_RIGHT_READABLE, dir.into_channel().into())?;
+            }
+        }
+    }
+    Ok(())
+}
+
+#[derive(Debug, StructOpt)]
+enum Provider {
+    Cast,
+    Misc,
+    Playready,
+    Widevine,
+}
+impl FromStr for Provider {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let formatted_str = s.trim().to_lowercase();
+        match formatted_str.as_ref() {
+            "cast" => Ok(Provider::Cast),
+            "misc" => Ok(Provider::Misc),
+            "playready" => Ok(Provider::Playready),
+            "widevine" => Ok(Provider::Widevine),
+            _ => Err(format_err!("Could not find '{}' provider", formatted_str)),
+        }
+    }
+}
+
+#[derive(Debug, StructOpt)]
+struct Flags {
+    /// The factory store provider to fake.
+    #[structopt(short, long)]
+    provider: Provider,
+
+    /// The path to the config file for the provider.
+    #[structopt(short, long)]
+    config: String,
+}
+
+#[fasync::run_singlethreaded]
+async fn main() -> Result<(), Error> {
+    syslog::init_with_tags(&["fake_factory_store_providers"])?;
+    let flags = Flags::from_args();
+    let dir = Arc::new(Mutex::new(start_test_dir(&flags.config)?));
+
+    let mut fs = ServiceFs::new_local();
+    let mut fs_dir = fs.dir("svc");
+
+    match flags.provider {
+        Provider::Cast => {
+            fs_dir.add_fidl_service(IncomingServices::CastCredentialsFactoryStoreProvider)
+        }
+        Provider::Misc => fs_dir.add_fidl_service(IncomingServices::MiscFactoryStoreProvider),
+        Provider::Playready => {
+            fs_dir.add_fidl_service(IncomingServices::PlayReadyFactoryStoreProvider)
+        }
+        Provider::Widevine => {
+            fs_dir.add_fidl_service(IncomingServices::WidevineFactoryStoreProvider)
+        }
+    };
+
+    fs.take_and_serve_directory_handle()?;
+    fs.for_each_concurrent(10, |req| {
+        run_server(req, dir.clone()).unwrap_or_else(|e| fx_log_err!("{:?}", e))
+    })
+    .await;
+    Ok(())
+}
---
+++ src/factory/fake_factory_store_providers/tests/fake_factory_store_providers_test.rs
@@ -0,0 +1,107 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![cfg(test)]
+#![feature(async_await)]
+
+use {
+    failure::Error,
+    fidl_fuchsia_factory::{
+        CastCredentialsFactoryStoreProviderMarker, MiscFactoryStoreProviderMarker,
+        PlayReadyFactoryStoreProviderMarker, WidevineFactoryStoreProviderMarker,
+    },
+    fidl_fuchsia_io::{DirectoryMarker, DirectoryProxy},
+    fuchsia_async as fasync,
+    std::path::PathBuf,
+};
+
+macro_rules! connect_to_factory_store_provider {
+    ($t:ty) => {{
+        let provider = fuchsia_component::client::connect_to_service::<$t>()
+            .expect("Failed to connect to service");
+
+        let (dir_proxy, dir_server) = fidl::endpoints::create_proxy::<DirectoryMarker>()?;
+        provider.get_factory_store(dir_server).expect("Failed to get factory store");
+        dir_proxy
+    }};
+}
+
+async fn read_file_from_proxy<'a>(
+    dir_proxy: &'a DirectoryProxy,
+    file_path: &'a str,
+) -> Result<Vec<u8>, Error> {
+    let file =
+        io_util::open_file(&dir_proxy, &PathBuf::from(file_path), io_util::OPEN_RIGHT_READABLE)?;
+    io_util::read_file_bytes(&file).await
+}
+
+async fn assert_file<'a>(
+    dir_proxy: &'a DirectoryProxy,
+    filename: &'a str,
+    expected_contents: &'a [u8],
+) -> Result<(), Error> {
+    let contents = read_file_from_proxy(&dir_proxy, filename).await?;
+    assert_eq!(expected_contents, &contents[..]);
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn read_factory_files_from_cast_credentials_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(CastCredentialsFactoryStoreProviderMarker);
+    assert_file(&dir_proxy, "txt/cast.txt", "a cast file".as_bytes()).await?;
+    assert_file(&dir_proxy, "cast2.bat", "another one (cast)".as_bytes()).await?;
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn missing_factory_files_from_cast_credentials_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(CastCredentialsFactoryStoreProviderMarker);
+    read_file_from_proxy(&dir_proxy, "missing.txt").await.unwrap_err();
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn read_factory_files_from_misc_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(MiscFactoryStoreProviderMarker);
+    assert_file(&dir_proxy, "misc.bin", "a misc file".as_bytes()).await?;
+    assert_file(&dir_proxy, "misc/misc.bin", "misc from another".as_bytes()).await?;
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn missing_factory_files_from_misc_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(MiscFactoryStoreProviderMarker);
+    read_file_from_proxy(&dir_proxy, "missing2").await.unwrap_err();
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn read_factory_files_from_playready_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(PlayReadyFactoryStoreProviderMarker);
+    assert_file(&dir_proxy, "txt/playready.txt", "a playready file".as_bytes()).await?;
+    assert_file(&dir_proxy, "playready.cfg", "another playready".as_bytes()).await?;
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn missing_factory_files_from_playready_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(PlayReadyFactoryStoreProviderMarker);
+    read_file_from_proxy(&dir_proxy, "abc").await.unwrap_err();
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn read_factory_files_from_widevine_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(WidevineFactoryStoreProviderMarker);
+    assert_file(&dir_proxy, "widevine.txt", "a widevine file".as_bytes()).await?;
+    assert_file(&dir_proxy, "widevine2.log", "and yet another wv".as_bytes()).await?;
+    Ok(())
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn missing_factory_files_from_widevine_store() -> Result<(), Error> {
+    let dir_proxy = connect_to_factory_store_provider!(WidevineFactoryStoreProviderMarker);
+    read_file_from_proxy(&dir_proxy, "defg").await.unwrap_err();
+    Ok(())
+}
