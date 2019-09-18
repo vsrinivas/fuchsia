15ac8bb20becdb1945d576335066ddb224519d92
Tyler Mandry <tmandry@gmail.com>
2019-09-18 03:57:56 +0000

    [rust][LSC] Remove remaining uses of async_await
    These all raced with fxr/319785
    
    Change-Id: I233a33708b6d34bb3532af77ff66ba1a06bf2b9e

--- garnet/bin/kms_test_client/src/main.rs
+++ garnet/bin/kms_test_client/src/main.rs
@@ -1,7 +1,6 @@
 // Copyright 2018 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#![feature(async_await)]
 #![allow(dead_code)]
 use rand::Rng;
 
