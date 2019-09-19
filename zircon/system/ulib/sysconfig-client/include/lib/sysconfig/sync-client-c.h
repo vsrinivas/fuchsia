2bff3cdce737bbfa50d1e2d9a03171d68b44e6cf
Suraj Malhotra <surajmalhotra@google.com>
2019-09-19 21:30:23 +0000

    [sysconfig-client] Add C wrapper
    Tested: fx build
    Change-Id: I7bfee4c46ae09124cfe8b1e303275f7e3e2d2b2f

---
+++ zircon/system/ulib/sysconfig-client/include/lib/sysconfig/sync-client-c.h
@@ -0,0 +1,52 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <zircon/types.h>
+
+__BEGIN_CDECLS
+
+typedef struct sysconfig_sync_client sysconfig_sync_client_t;
+
+typedef enum sysconfig_partition {
+  kSysconfigPartitionSysconfig,
+  // Used to determine which partition to boot into on boot.
+  kSysconfigPartitionABRMetadata,
+  // The follow are used to store verified boot metadata.
+  kSysconfigPartitionVerifiedBootMetadataA,
+  kSysconfigPartitionVerifiedBootMetadataB,
+  kSysconfigPartitionVerifiedBootMetadataR,
+} sysconfig_partition_t;
+
+// Allocates and initializes a `sysconfig_sync_client_t`. This object should be passed into other
+// APIs.
+//
+// Caller retains owernship of |devfs_root|.
+zx_status_t sysconfig_sync_client_create(int devfs_root, sysconfig_sync_client_t** out_client);
+
+// Frees the object created by `sysconfig_sync_client_create`.
+void sysconfig_sync_client_free(sysconfig_sync_client_t* client);
+
+// Provides write access for the partition specified. Always writes full partition.
+//
+// |vmo| must have a size greater than or equal to the partitions size + |vmo_offset|.
+// Callee retains ownership of |vmo|.
+zx_status_t sysconfig_write_partition(sysconfig_sync_client_t* client,
+                                      sysconfig_partition_t partition, zx_handle_t vmo,
+                                      zx_off_t vmo_offset);
+
+// Provides read access for the partition specified. Always reads full partition.
+//
+// |vmo| must have a size greater than or equal to the partitions size + |vmo_offset|.
+// Callee retains ownership of |vmo|.
+zx_status_t sysconfig_read_partition(sysconfig_sync_client_t* client,
+                                     sysconfig_partition_t partition, zx_handle_t vmo,
+                                     zx_off_t vmo_offset);
+
+// Returns the size of the partition specified.
+size_t sysconfig_get_partition_size(sysconfig_sync_client_t* client,
+                                    sysconfig_partition_t partition);
+
+__END_CDECLS
---
+++ zircon/system/ulib/sysconfig-client/sync-client-c.cc
@@ -0,0 +1,65 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/sysconfig/sync-client-c.h>
+#include <lib/sysconfig/sync-client.h>
+
+namespace {
+sysconfig::SyncClient::PartitionType Translate(sysconfig_partition_t partition) {
+  switch (partition) {
+    case kSysconfigPartitionSysconfig:
+      return sysconfig::SyncClient::PartitionType::kSysconfig;
+    case kSysconfigPartitionABRMetadata:
+      return sysconfig::SyncClient::PartitionType::kABRMetadata;
+    case kSysconfigPartitionVerifiedBootMetadataA:
+      return sysconfig::SyncClient::PartitionType::kVerifiedBootMetadataA;
+    case kSysconfigPartitionVerifiedBootMetadataB:
+      return sysconfig::SyncClient::PartitionType::kVerifiedBootMetadataB;
+    case kSysconfigPartitionVerifiedBootMetadataR:
+      return sysconfig::SyncClient::PartitionType::kVerifiedBootMetadataR;
+    default:
+      break;
+  }
+  ZX_ASSERT(false);
+}
+}  // namespace
+
+struct sysconfig_sync_client {
+  std::optional<sysconfig::SyncClient> cpp_client;
+};
+
+zx_status_t sysconfig_sync_client_create(int devfs_root, sysconfig_sync_client_t** out_client) {
+  auto client = std::make_unique<sysconfig_sync_client_t>();
+
+  fbl::unique_fd fd(devfs_root);
+  auto status = sysconfig::SyncClient::Create(fd, &client->cpp_client);
+  fd.release();
+  if (status != ZX_OK) {
+    return status;
+  }
+
+  *out_client = client.release();
+  return ZX_OK;
+}
+
+void sysconfig_sync_client_free(sysconfig_sync_client_t* client) { delete client; }
+
+zx_status_t sysconfig_write_partition(sysconfig_sync_client_t* client,
+                                      sysconfig_partition_t partition, zx_handle_t vmo,
+                                      zx_off_t vmo_offset) {
+  return client->cpp_client->WritePartition(Translate(partition), *zx::unowned_vmo(vmo),
+                                            vmo_offset);
+}
+
+zx_status_t sysconfig_read_partition(sysconfig_sync_client_t* client,
+                                     sysconfig_partition_t partition, zx_handle_t vmo,
+                                     zx_off_t vmo_offset) {
+  return client->cpp_client->ReadPartition(Translate(partition), *zx::unowned_vmo(vmo),
+                                            vmo_offset);
+}
+
+size_t sysconfig_get_partition_size(sysconfig_sync_client_t* client,
+                                    sysconfig_partition_t partition) {
+  return client->cpp_client->GetPartitionSize(Translate(partition));
+}
