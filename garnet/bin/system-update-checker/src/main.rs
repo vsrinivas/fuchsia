38271e99c6de87834229b764f5fabb1c6dc583d0
Ben Keller <galbanum@google.com>
2019-09-23 20:18:48 +0000

    [system-update-checker] Mark partition successful if healthy
    Bug: 22860
    Change-Id: I9f329a83f5defb330be01d1c04c6029dbbb56ce2

--- garnet/bin/system-update-checker/src/main.rs
+++ garnet/bin/system-update-checker/src/main.rs
@@ -16,22 +16,26 @@ mod update_manager;
 mod update_monitor;
 mod update_service;
 
-use crate::apply::Initiator;
-use crate::config::Config;
-use crate::poller::run_periodic_update_check;
-use crate::provider_handler::ProviderHandler;
-use crate::update_service::{RealUpdateManager, RealUpdateService};
-use failure::{Error, ResultExt};
-use fidl_fuchsia_update::ManagerRequestStream;
-use fidl_fuchsia_update_channel::ProviderRequestStream;
-use forced_fdr::perform_fdr_if_necessary;
-use fuchsia_async as fasync;
-use fuchsia_component::server::ServiceFs;
-use fuchsia_inspect as finspect;
-use fuchsia_syslog::{fx_log_err, fx_log_warn};
-use futures::prelude::*;
-use std::sync::Arc;
-use std::time::Duration;
+use {
+    crate::{
+        apply::Initiator,
+        config::Config,
+        poller::run_periodic_update_check,
+        provider_handler::ProviderHandler,
+        update_service::{RealUpdateManager, RealUpdateService},
+    },
+    failure::{Error, ResultExt},
+    fidl_fuchsia_update::ManagerRequestStream,
+    fidl_fuchsia_update_channel::ProviderRequestStream,
+    forced_fdr::perform_fdr_if_necessary,
+    fuchsia_async as fasync,
+    fuchsia_component::server::ServiceFs,
+    fuchsia_inspect as finspect,
+    fuchsia_syslog::{fx_log_err, fx_log_warn},
+    futures::{prelude::*, stream::FuturesUnordered},
+    std::sync::Arc,
+    std::time::Duration,
+};
 
 const MAX_CONCURRENT_CONNECTIONS: usize = 100;
 
@@ -52,12 +56,14 @@ async fn main() -> Result<(), Error> {
         fx_log_err!("while updating the target channel: {}", e);
     }
 
+    let mut futures = FuturesUnordered::new();
+
     let (current_channel_manager, current_channel_notifier) =
         channel::build_current_channel_manager_and_notifier(
             connect::ServiceConnector,
             "/misc/ota",
         )?;
-    let channel_fut = current_channel_notifier.run();
+    futures.push(current_channel_notifier.run().boxed());
 
     let update_manager = Arc::new(
         RealUpdateManager::new(
@@ -80,22 +86,31 @@ async fn main() -> Result<(), Error> {
     inspector.export(&mut fs);
 
     fs.take_and_serve_directory_handle().context("ServiceFs::take_and_serve_directory_handle")?;
-    let fidl_fut = fs.for_each_concurrent(MAX_CONCURRENT_CONNECTIONS, |incoming_service| {
-        handle_incoming_service(incoming_service)
-            .unwrap_or_else(|e| fx_log_err!("error handling client connection: {}", e))
-    });
+    futures.push(
+        fs.for_each_concurrent(MAX_CONCURRENT_CONNECTIONS, |incoming_service| {
+            handle_incoming_service(incoming_service)
+                .unwrap_or_else(|e| fx_log_err!("error handling client connection: {}", e))
+        })
+        .boxed(),
+    );
 
-    let cron_fut = run_periodic_update_check(update_manager.clone(), &config);
+    futures.push(run_periodic_update_check(update_manager.clone(), &config).boxed());
 
-    let first_update_fut = async move {
-        if config.poll_frequency().is_some() {
-            fasync::Timer::new(fasync::Time::after(Duration::from_secs(60).into())).await;
-            update_manager.try_start_update(Initiator::Automatic, None);
+    futures.push(
+        async move {
+            if config.poll_frequency().is_some() {
+                fasync::Timer::new(fasync::Time::after(Duration::from_secs(60).into())).await;
+                update_manager.try_start_update(Initiator::Automatic, None);
+            }
         }
-    };
+            .boxed(),
+    );
 
-    future::join5(channel_fut, fidl_fut, cron_fut, first_update_fut, perform_fdr_if_necessary())
-        .await;
+    futures.push(perform_fdr_if_necessary().boxed());
+
+    futures.push(check_and_mark_system_health().boxed());
+
+    futures.collect::<()>().await;
 
     Ok(())
 }
@@ -114,4 +129,15 @@ async fn handle_incoming_service(incoming_service: IncomingServices) -> Result<(
             handler.handle_request_stream(request_stream).await
         }
     }
+}
+
+async fn check_and_mark_system_health() {
+    if let Err(err) = check_and_mark_system_health_impl().await {
+        fx_log_err!("error during system health check: {}", err);
+    }
+}
+
+async fn check_and_mark_system_health_impl() -> Result<(), Error> {
+    system_health_check::check_system_health().await?;
+    Ok(system_health_check::mark_active_configuration_successful().await)
 }
---
+++ garnet/tests/system-update-checker/src/lib.rs
@@ -0,0 +1,155 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![cfg(test)]
+use {
+    failure::Error,
+    fidl_fuchsia_paver::{PaverRequest, PaverRequestStream},
+    fidl_fuchsia_update::{ManagerMarker, ManagerProxy, ManagerState},
+    fidl_fuchsia_update_channel::{ProviderMarker, ProviderProxy},
+    fuchsia_async as fasync,
+    fuchsia_component::{
+        client::{App, AppBuilder},
+        server::{NestedEnvironment, ServiceFs},
+    },
+    fuchsia_zircon::Status,
+    futures::{channel::mpsc, prelude::*},
+    parking_lot::Mutex,
+    std::{fs::File, sync::Arc},
+    tempfile::TempDir,
+};
+
+const SYSTEM_UPDATE_CHECKER_CMX: &str = "fuchsia-pkg://fuchsia.com/system-update-checker-integration-tests#meta/system-update-checker-for-integration-test.cmx";
+
+struct Mounts {
+    misc_ota: TempDir,
+}
+
+struct Proxies {
+    paver: Arc<MockPaver>,
+    channel_provider: ProviderProxy,
+    update_manager: ManagerProxy,
+}
+
+impl Mounts {
+    fn new() -> Self {
+        Self { misc_ota: tempfile::tempdir().expect("/tmp to exist") }
+    }
+}
+
+struct TestEnv {
+    _env: NestedEnvironment,
+    _mounts: Mounts,
+    proxies: Proxies,
+    _system_update_checker: App,
+}
+
+impl TestEnv {
+    fn new(paver: MockPaver) -> Self {
+        let mounts = Mounts::new();
+
+        let mut fs = ServiceFs::new();
+
+        let paver = Arc::new(paver);
+        let paver_clone = Arc::clone(&paver);
+        fs.add_fidl_service(move |stream: PaverRequestStream| {
+            let paver_clone = Arc::clone(&paver_clone);
+            fasync::spawn(
+                paver_clone
+                    .run_service(stream)
+                    .unwrap_or_else(|e| panic!("error running paver service: {:?}", e)),
+            )
+        });
+
+        let env = fs
+            .create_salted_nested_environment("system-update-checker_integration_test_env")
+            .expect("nested environment to create successfully");
+        fasync::spawn(fs.collect());
+
+        let system_update_checker = AppBuilder::new(SYSTEM_UPDATE_CHECKER_CMX)
+            .add_dir_to_namespace(
+                "/misc/ota".to_string(),
+                File::open(mounts.misc_ota.path()).expect("/misc/ota tempdir to open"),
+            )
+            .expect("/misc/ota to mount")
+            .spawn(env.launcher())
+            .expect("system_update_checker to launch");
+
+        Self {
+            _env: env,
+            _mounts: mounts,
+            proxies: Proxies {
+                paver,
+                channel_provider: system_update_checker
+                    .connect_to_service::<ProviderMarker>()
+                    .expect("connect to channel provider"),
+                update_manager: system_update_checker
+                    .connect_to_service::<ManagerMarker>()
+                    .expect("connect to update manager"),
+            },
+            _system_update_checker: system_update_checker,
+        }
+    }
+}
+
+struct MockPaver {
+    response: Status,
+    was_called_sender: mpsc::UnboundedSender<()>,
+    was_called: Mutex<mpsc::UnboundedReceiver<()>>,
+}
+
+impl MockPaver {
+    fn new(response: Status) -> Self {
+        let (was_called_sender, was_called) = mpsc::unbounded();
+        Self { response, was_called_sender, was_called: Mutex::new(was_called) }
+    }
+    async fn run_service(self: Arc<Self>, mut stream: PaverRequestStream) -> Result<(), Error> {
+        while let Some(req) = stream.try_next().await? {
+            match req {
+                PaverRequest::MarkActiveConfigurationSuccessful { responder } => {
+                    self.was_called_sender.unbounded_send(()).expect("mpsc send");
+                    responder.send(self.response.clone().into_raw()).expect("send ok");
+                }
+                req => panic!("unhandled paver request: {:?}", req),
+            }
+        }
+        Ok(())
+    }
+}
+
+#[fasync::run_singlethreaded(test)]
+// Test will hang if system-update-checker does not call paver service
+async fn test_calls_paver_service() {
+    let env = TestEnv::new(MockPaver::new(Status::OK));
+
+    let mut was_called = env.proxies.paver.was_called.lock();
+    assert_eq!(was_called.next().await, Some(()));
+}
+
+#[fasync::run_singlethreaded(test)]
+// Test will hang if system-update-checker does not call paver service
+async fn test_channel_provider_get_current_works_after_paver_service_fails() {
+    let env = TestEnv::new(MockPaver::new(Status::INTERNAL));
+
+    let mut was_called = env.proxies.paver.was_called.lock();
+    was_called.next().await;
+
+    assert_eq!(
+        env.proxies.channel_provider.get_current().await.expect("get_current"),
+        "".to_string()
+    );
+}
+
+#[fasync::run_singlethreaded(test)]
+// Test will hang if system-update-checker does not call paver service
+async fn test_update_manager_get_state_works_after_paver_service_fails() {
+    let env = TestEnv::new(MockPaver::new(Status::INTERNAL));
+
+    let mut was_called = env.proxies.paver.was_called.lock();
+    was_called.next().await;
+
+    let state = env.proxies.update_manager.get_state().await.expect("get_state");
+    assert_eq!(state.state, Some(ManagerState::Idle));
+    assert_eq!(state.version_available, None);
+}
