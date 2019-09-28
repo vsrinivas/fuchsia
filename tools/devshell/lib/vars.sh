2afb2cf86f2f364d18cc8f08f1785de0abc00349
Renato Mangini Dias <mangini@google.com>
2019-09-28 06:17:39 +0000

    [fx] Fix incorrect quoting
    Fix redundant quoting on ninja invocation.
    
    Change-Id: I1f4af16d7bc904da4e3a6b93a10bb12505297a5c

--- tools/devshell/lib/vars.sh
+++ tools/devshell/lib/vars.sh
@@ -426,7 +426,7 @@ function fx-run-ninja {
   # when TMPDIR="" - it is deliberately unquoted and using the ${+} expansion
   # expression). GOMA_DISABLED will forcefully disable Goma even if it's set to
   # empty.
-  full_cmdline=(env -i "TERM=\"${TERM}\"" "PATH=\"${PATH}\"" \
+  full_cmdline=(env -i "TERM=${TERM}" "PATH=${PATH}" \
     ${NINJA_STATUS+"NINJA_STATUS=${NINJA_STATUS}"} \
     ${GOMA_DISABLED+"GOMA_DISABLED=$GOMA_DISABLED"} \
     ${TMPDIR+"TMPDIR=$TMPDIR"} \
