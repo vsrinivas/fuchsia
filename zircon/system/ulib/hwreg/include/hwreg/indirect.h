f9477c0e5f70b63294e8d072b696fae7babc95c7
Randall Bosetti <rlb@google.com>
2019-10-01 02:46:43 +0000

    [zircon][ddk] Add a centralized indirect register template.
    Change-Id: I04bab28c4db0d4a6dda996abdb4aea8daf2d2017

---
+++ zircon/system/ulib/hwreg/include/hwreg/indirect.h
@@ -0,0 +1,69 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef HWREG_INDIRECT_H_
+#define HWREG_INDIRECT_H_
+
+#include <limits.h>
+#include <stdint.h>
+#include <zircon/assert.h>
+#include <zircon/threads.h>
+
+#include <mutex>
+#include <type_traits>
+
+#include <hwreg/mmio.h>
+
+namespace hwreg {
+
+// IndirectIo is a replacement for RegisterIo that sequences access to a common PCI device construct
+// where two registers, "index" and "data", are used to read and write a much larger store,
+// logically private_store[index] = data.
+//
+// Example usage from VGA:
+//
+// typedef IndirectIo<0x03D4, 0x03D5> CrtcIo;
+// class CrtcReg : public hwreg::RegisterBase<CrtcReg, uint8_t> {
+//  public:
+//   static auto Get(size_t offset) : { return hwreg::RegisterAddr<CrtcReg>(offset); }
+// };
+// ...
+// void SetMode3(volatile void* mmio) {
+//   auto io = CrtcIo(mmio);
+//   CrtcReg::Get(0x00).ReadFrom(&io).set_reg_value(0x5F).WriteTo(&io);
+//   CrtcReg::Get(0x01).ReadFrom(&io).set_reg_value(0x4F).WriteTo(&io);
+//   CrtcReg::Get(0x02).ReadFrom(&io).set_reg_value(0x50).WriteTo(&io);
+//   CrtcReg::Get(0x03).ReadFrom(&io).set_reg_value(0x82).WriteTo(&io);
+//   CrtcReg::Get(0x04).ReadFrom(&io).set_reg_value(0x55).WriteTo(&io);
+//   CrtcReg::Get(0x05).ReadFrom(&io).set_reg_value(0x81).WriteTo(&io);
+// }
+template <uint32_t index_offset, uint32_t data_offset, typename IndexType = uint8_t>
+class IndirectIo {
+ public:
+  explicit IndirectIo(RegisterIo io) : io_(io) {}
+
+  ~IndirectIo() = default;
+
+  template <class IntType>
+  void Write(IntType value, uint32_t offset) {
+    std::lock_guard<std::mutex> l(lock_);
+    io_.Write<IndexType>(static_cast<IndexType>(offset), index_offset);
+    io_.Write(value, data_offset);
+  }
+
+  template <class IntType>
+  IntType Read(uint32_t offset) {
+    std::lock_guard<std::mutex> l(lock_);
+    io_.Write<IndexType>(static_cast<IndexType>(offset), index_offset);
+    return io_.Read<IntType>(data_offset);
+  }
+
+ private:
+  std::mutex lock_;
+  RegisterIo io_ __TA_GUARDED(lock_);
+};
+
+}  // namespace hwreg
+
+#endif  // HWREG_INDIRECT_H_
---
+++ zircon/system/ulib/hwreg/test/indirect-test.cc
@@ -0,0 +1,115 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <assert.h>
+#include <stdio.h>
+#include <zircon/types.h>
+
+#include <climits>
+#include <limits>
+
+#include <hwreg/bitfields.h>
+#include <hwreg/indirect.h>
+#include <hwreg/mmio.h>
+#include <zxtest/zxtest.h>
+
+// This function exists so that the resulting code can be inspected easily in the
+// object file.
+static void compilation_test() {
+  typedef hwreg::IndirectIo<0x00, 0x04> Io;
+
+  volatile struct {
+    uint32_t index = 0xaa;
+    uint32_t reserved = 0xff;
+    uint32_t data = 0x11;
+  } fake_regs;
+  auto io = Io(static_cast<volatile void*>(&fake_regs));
+
+  io.Write<uint32_t>(1, 0);
+  io.Read<uint32_t>(0);
+}
+
+namespace {
+
+template <typename IntType, typename IndexType>
+void basic_access_test() {
+  typedef struct {
+    IndexType index;
+    // Test an indexed register with a reserved field in the middle.
+    IntType reserved;
+    IntType data;
+  } FakeRegs;
+  volatile FakeRegs fake_regs;
+  typedef hwreg::IndirectIo<
+      static_cast<uint32_t>(offsetof(FakeRegs, index)),
+      static_cast<uint32_t>(offsetof(FakeRegs, data)),
+      IndexType> Io;
+  fake_regs.index = 1;
+  fake_regs.reserved = 3;
+  fake_regs.data = 2;
+  Io io(hwreg::RegisterIo(static_cast<volatile void*>(&fake_regs)));
+
+  class Reg : public hwreg::RegisterBase<Reg, IntType> {
+   public:
+    static auto Get(uint32_t offset) { return hwreg::RegisterAddr<Reg>(offset); }
+  };
+
+  // Validate that reading from .data works
+  EXPECT_EQ(2, Reg::Get(0).ReadFrom(&io).reg_value());
+  EXPECT_EQ(0, fake_regs.index);
+  EXPECT_EQ(3, fake_regs.reserved);
+
+  // That reading from another register updates the index.
+  fake_regs.data = 6;
+  EXPECT_EQ(6, Reg::Get(2).ReadFrom(&io).reg_value());
+  EXPECT_EQ(2, fake_regs.index);
+  EXPECT_EQ(3, fake_regs.reserved);
+
+  // And writing also updates the index.
+  Reg::Get(0).ReadFrom(&io).set_reg_value(static_cast<IntType>(5)).WriteTo(&io);
+  EXPECT_EQ(0, fake_regs.index);
+  EXPECT_EQ(5, fake_regs.data);
+  EXPECT_EQ(3, fake_regs.reserved);
+}
+
+template <typename IntType>
+void single_thread_test() {
+  basic_access_test<IntType, IntType>();
+  basic_access_test<IntType, uint8_t>();
+}
+
+TEST(SingleThreadTestCase, Uint8) { ASSERT_NO_FAILURES(single_thread_test<uint8_t>()); }
+TEST(SingleThreadTestCase, Uint16) { ASSERT_NO_FAILURES(single_thread_test<uint16_t>()); }
+TEST(SingleThreadTestCase, Uint32) { ASSERT_NO_FAILURES(single_thread_test<uint32_t>()); }
+TEST(SingleThreadTestCase, Uint64) { ASSERT_NO_FAILURES(single_thread_test<uint64_t>()); }
+
+TEST(AlignedAccessTest, Uint32) {
+  typedef struct {
+    uint16_t index;
+    uint16_t reserved;
+    uint32_t data;
+  } FakeRegs;
+  volatile FakeRegs fake_regs;
+  typedef hwreg::IndirectIo<offsetof(FakeRegs, index), offsetof(FakeRegs, data), uint16_t> Io;
+  fake_regs.index = 0xaaaa;
+  fake_regs.reserved = 0xffff;
+  fake_regs.data = 0x12345678;
+  Io io(hwreg::RegisterIo(static_cast<volatile void*>(&fake_regs)));
+
+  class MatchingReg : public hwreg::RegisterBase<MatchingReg, uint32_t> {
+   public:
+    static auto Get(uint32_t offset) { return hwreg::RegisterAddr<MatchingReg>(offset); }
+  };
+
+  EXPECT_EQ(0x12345678, MatchingReg::Get(0).ReadFrom(&io).reg_value());
+
+  class SmallReg : public hwreg::RegisterBase<SmallReg, uint16_t> {
+   public:
+    static auto Get(uint32_t offset) { return hwreg::RegisterAddr<SmallReg>(offset); }
+  };
+
+  EXPECT_EQ(fake_regs.data & 0xffff, SmallReg::Get(0).ReadFrom(&io).reg_value());
+}
+
+}  // namespace
