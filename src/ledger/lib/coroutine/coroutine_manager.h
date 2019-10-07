5f9a4b8d4a52aed30e6c9b0362e31a721e42af24
Gabriel Kerneis <kerneis@google.com>
2019-10-07 19:51:34 +0000

    [ledger] Allow using CoroutineManager as a threadpool
    This commit introduces an optional maximum number of coroutines started
    by CoroutineManager. Additional tasks are enqueued until one of the
    running coroutines is available to resume them.
    
    Test: fx run-test ledger_tests -t ledger_lib_unittests
    Change-Id: Ia34a7a77ddda30509225b253d95288967488ba5b

--- src/ledger/lib/coroutine/coroutine_manager.h
+++ src/ledger/lib/coroutine/coroutine_manager.h
@@ -9,14 +9,30 @@
 
 #include <algorithm>
 #include <list>
+#include <queue>
+#include <stack>
 
 #include "src/ledger/lib/coroutine/coroutine.h"
+#include "src/lib/callback/destruction_sentinel.h"
 
 namespace coroutine {
 // CoroutineManager manages the lifetime of coroutines.
+//
+// CoroutineManager is parametrized by the maximum number of coroutines it will use:
+// - if |max_coroutines| is 0, the number of coroutines in unlimited, coroutines are created
+//   on-demand by |StartCoroutine|, and destroyed as soon as their task is over.
+// - otherwise, new coroutines are created on-demand until |max_coroutines| is reached, but
+//   coroutines are *never* destroyed. They stay in a pending state once their task is over, and are
+//   re-used to run further tasks scheduled with |StartCoroutine|. If no coroutine is available when
+//   |StartCoroutine| is called, the task is enqueued to be run later: no coroutine is created, but
+//   the call does not block.
+//
+// Note that this makes max_coroutines == 0 have a very different behavior from max_coroutines ==
+// SIZE_MAX (you should *not* use the latter).
 class CoroutineManager {
  public:
-  explicit CoroutineManager(CoroutineService* service) : service_(service) {}
+  explicit CoroutineManager(CoroutineService* service, size_t max_coroutines = 0)
+      : max_coroutines_(max_coroutines), service_(service) {}
 
   ~CoroutineManager() { Shutdown(); }
 
@@ -38,12 +54,12 @@ class CoroutineManager {
     if (disabled_) {
       return;
     }
-    service_->StartCoroutine([this, callback = std::move(callback),
-                              runnable = std::move(runnable)](CoroutineHandler* handler) mutable {
+    StartOrEnqueueCoroutine([this, callback = std::move(callback),
+                             runnable = std::move(runnable)](CoroutineHandler* handler) mutable {
       bool callback_called = false;
       auto iter = handlers_.insert(handlers_.cend(), handler);
       auto final_callback = [this, &callback_called, iter,
-                             callback = std::move(callback)](auto... args) {
+                             callback = std::move(callback)](auto... args) mutable {
         // Remove the handler before calling the final callback. Otherwise
         // the handler might be unnecessarily interrupted, if this object
         // destructor is called in the callback.
@@ -72,10 +88,12 @@ class CoroutineManager {
     if (disabled_) {
       return;
     }
-    service_->StartCoroutine(
+    StartOrEnqueueCoroutine(
         [this, runnable = std::move(runnable)](CoroutineHandler* handler) mutable {
           auto iter = handlers_.insert(handlers_.cend(), handler);
           runnable(handler);
+          // `runnable` is not allowed to delete the coroutine manager that executes it, so
+          // handlers_ is safe to access.
           handlers_.erase(iter);
         });
   }
@@ -90,9 +108,68 @@ class CoroutineManager {
     }
   }
 
+  // Enqueues |to_run|. Then either:
+  // - immediately executes it with an existing pending coroutine if available,
+  // - or starts a new coroutine to run it if there are fewer than |max_coroutines_| already.
+  // If |max_coroutines_| are already busy running tasks, simply leaves |to_run| enqueued for a
+  // coroutine to run it later.
+  void StartOrEnqueueCoroutine(fit::function<void(CoroutineHandler*)> to_run) {
+    pending_tasks_.push(std::move(to_run));
+    if (!pending_coroutines_.empty()) {
+      CoroutineHandler* handler = pending_coroutines_.top();
+      pending_coroutines_.pop();
+      handler->Resume(ContinuationStatus::OK);
+      return;
+    }
+    if (max_coroutines_ == 0 || handlers_.size() < max_coroutines_) {
+      service_->StartCoroutine([this](CoroutineHandler* handler) { RunPending(handler); });
+    }
+  }
+
+  // Runs pending tasks with the current |handler| coroutine until this coroutine manager is
+  // destructed.
+  void RunPending(CoroutineHandler* handler) {
+    auto it = sentinels_.emplace(sentinels_.end());
+    while (!disabled_) {
+      // Run the first available task.
+      auto to_run = std::move(pending_tasks_.front());
+      pending_tasks_.pop();
+      if (it->DestructedWhile([to_run = std::move(to_run), handler] { to_run(handler); }) ||
+          disabled_) {
+        // Return early if this manager has been or is being destructed.
+        return;
+      }
+      // Grab the next task.
+      if (!pending_tasks_.empty()) {
+        continue;
+      }
+      // No more task is available.
+      // 1. If we use an unbounded number of coroutines, free them once done.
+      if (max_coroutines_ == 0) {
+        sentinels_.erase(it);
+        return;
+      }
+      // 2. Otherwise, wait for a taks to be available.
+      pending_coroutines_.push(handler);
+      if (handler->Yield() == ContinuationStatus::INTERRUPTED) {
+        return;
+      }
+    }
+  }
+
  private:
+  // Maximum number of coroutines to start. If 0, unlimited.
+  const size_t max_coroutines_;
+  // Set to true when this manager is being destructed.
   bool disabled_ = false;
+  // Currently allocated coroutines.
   std::list<coroutine::CoroutineHandler*> handlers_;
+  // Currently pending coroutines (waiting for tasks to execute). This is a subset of |handlers_|.
+  std::stack<coroutine::CoroutineHandler*> pending_coroutines_;
+  // Each coroutine creates a sentinel to detect destruction of this coroutine manager.
+  std::list<callback::DestructionSentinel> sentinels_;
+  // Queue of pending tasks to execute when coroutines are available.
+  std::queue<fit::function<void(CoroutineHandler*)>> pending_tasks_;
   CoroutineService* const service_;
 };
 
--- src/ledger/lib/coroutine/coroutine_manager_unittest.cc
+++ src/ledger/lib/coroutine/coroutine_manager_unittest.cc
@@ -7,15 +7,19 @@
 #include <memory>
 
 #include "gtest/gtest.h"
+#include "src/ledger/lib/coroutine/coroutine.h"
 #include "src/ledger/lib/coroutine/coroutine_impl.h"
 #include "src/lib/callback/set_when_called.h"
 
 namespace coroutine {
 namespace {
 
-TEST(CoroutineManager, CallbackIsCalled) {
+// Parametrize tests with the maximum number of coroutines (0 = unlimited).
+using CoroutineManagerTest = ::testing::TestWithParam<size_t>;
+
+TEST_P(CoroutineManagerTest, CallbackIsCalled) {
   CoroutineServiceImpl coroutine_service;
-  CoroutineManager manager(&coroutine_service);
+  CoroutineManager manager(&coroutine_service, GetParam());
 
   bool called = false;
   CoroutineHandler* handler = nullptr;
@@ -33,10 +37,49 @@ TEST(CoroutineManager, CallbackIsCalled) {
   EXPECT_TRUE(called);
 }
 
-TEST(CoroutineManager, InterruptCoroutineOnDestruction) {
+TEST_P(CoroutineManagerTest, MultipleCoroutines) {
+  CoroutineServiceImpl coroutine_service;
+  CoroutineManager manager(&coroutine_service, GetParam());
+
+  bool called = false;
+  CoroutineHandler* handler = nullptr;
+  manager.StartCoroutine(
+      callback::SetWhenCalled(&called),
+      [&handler](CoroutineHandler* current_handler, fit::function<void()> callback) {
+        handler = current_handler;
+        EXPECT_EQ(ContinuationStatus::OK, handler->Yield());
+        callback();
+      });
+
+  bool called_2 = false;
+  CoroutineHandler* handler_2 = nullptr;
+  manager.StartCoroutine(
+      callback::SetWhenCalled(&called_2),
+      [&handler_2](CoroutineHandler* current_handler_2, fit::function<void()> callback) {
+        handler_2 = current_handler_2;
+        EXPECT_EQ(ContinuationStatus::OK, handler_2->Yield());
+        callback();
+      });
+
+  // Coroutine 1 has yielded before completion.
+  EXPECT_FALSE(called);
+  // Coroutine 2 has yielded before completion, or the task is pending (if GetParam() == 1).
+  EXPECT_FALSE(called_2);
+  // Resume first coroutine.
+  ASSERT_TRUE(handler);
+  handler->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called);
+  EXPECT_FALSE(called_2);
+  // Resume second coroutine (which may be the same as the first if GetParam() == 1).
+  ASSERT_TRUE(handler_2);
+  handler_2->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called_2);
+}
+
+TEST_P(CoroutineManagerTest, InterruptCoroutineOnDestruction) {
   CoroutineServiceImpl coroutine_service;
   std::unique_ptr<CoroutineManager> manager =
-      std::make_unique<CoroutineManager>(&coroutine_service);
+      std::make_unique<CoroutineManager>(&coroutine_service, GetParam());
 
   bool called = false;
   bool reached_callback = false;
@@ -61,9 +104,47 @@ TEST(CoroutineManager, InterruptCoroutineOnDestruction) {
   EXPECT_TRUE(executed_callback);
 }
 
-TEST(CoroutineManager, Shutdown) {
+TEST_P(CoroutineManagerTest, CoroutineCallbackStartsCoroutine) {
   CoroutineServiceImpl coroutine_service;
-  CoroutineManager manager(&coroutine_service);
+  CoroutineManager manager(&coroutine_service, GetParam());
+
+  bool called = false;
+  bool called_2 = false;
+  CoroutineHandler* handler = nullptr;
+  CoroutineHandler* handler_2 = nullptr;
+  // The callback of the first coroutine starts a second coroutine.
+  auto callback = [&called, &manager, &called_2, &handler_2] {
+    called = true;
+    manager.StartCoroutine(
+        callback::SetWhenCalled(&called_2),
+        [&handler_2](CoroutineHandler* current_handler_2, fit::function<void()> callback) {
+          handler_2 = current_handler_2;
+          EXPECT_EQ(ContinuationStatus::OK, handler_2->Yield());
+          callback();
+        });
+  };
+  manager.StartCoroutine(std::move(callback), [&handler](CoroutineHandler* current_handler,
+                                                         fit::function<void()> callback) {
+    handler = current_handler;
+    EXPECT_EQ(ContinuationStatus::OK, handler->Yield());
+    callback();
+  });
+
+  ASSERT_TRUE(handler);
+  EXPECT_FALSE(called);
+  EXPECT_FALSE(handler_2);
+  EXPECT_FALSE(called_2);
+  handler->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called);
+  ASSERT_TRUE(handler_2);
+  EXPECT_FALSE(called_2);
+  handler_2->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called_2);
+}
+
+TEST_P(CoroutineManagerTest, Shutdown) {
+  CoroutineServiceImpl coroutine_service;
+  CoroutineManager manager(&coroutine_service, GetParam());
 
   bool called = false;
   bool reached_callback = false;
@@ -97,9 +178,9 @@ TEST(CoroutineManager, Shutdown) {
   EXPECT_FALSE(coroutine_started);
 }
 
-TEST(CoroutineManager, NoCallback) {
+TEST_P(CoroutineManagerTest, NoCallback) {
   CoroutineServiceImpl coroutine_service;
-  CoroutineManager manager(&coroutine_service);
+  CoroutineManager manager(&coroutine_service, GetParam());
 
   CoroutineHandler* handler = nullptr;
   manager.StartCoroutine([&handler](CoroutineHandler* current_handler) {
@@ -111,14 +192,177 @@ TEST(CoroutineManager, NoCallback) {
   handler->Resume(ContinuationStatus::OK);
 }
 
-TEST(CoroutineManager, DeleteInCallback) {
+TEST_P(CoroutineManagerTest, DeleteInCallback) {
   CoroutineServiceImpl coroutine_service;
   std::unique_ptr<CoroutineManager> manager =
-      std::make_unique<CoroutineManager>(&coroutine_service);
+      std::make_unique<CoroutineManager>(&coroutine_service, GetParam());
 
   manager->StartCoroutine(
       [&manager] { manager.reset(); },
       [](CoroutineHandler* current_handler, fit::function<void()> callback) { callback(); });
+}
+
+TEST_P(CoroutineManagerTest, DeleteAfterCallback) {
+  CoroutineServiceImpl coroutine_service;
+  std::unique_ptr<CoroutineManager> manager =
+      std::make_unique<CoroutineManager>(&coroutine_service, GetParam());
+
+  manager->StartCoroutine(
+      [] {},
+      [&manager](CoroutineHandler* current_handler, fit::function<void()> callback) {
+        callback();
+        manager.reset();
+      });
+}
+
+INSTANTIATE_TEST_SUITE_P(CoroutineManagerTest, CoroutineManagerTest, testing::Values(0, 1, 2, 100));
+
+// Below, some tests with a controlled maximum number of coroutines checking pending tasks behavior.
+
+TEST(CoroutineManagerTest, DeleteInCallbackMultipleCoroutines) {
+  CoroutineServiceImpl coroutine_service;
+  std::unique_ptr<CoroutineManager> manager =
+      std::make_unique<CoroutineManager>(&coroutine_service, /*max_coroutines=*/2);
+
+  CoroutineHandler* handler = nullptr;
+  manager->StartCoroutine(
+      [&manager] { manager.reset(); },
+      [&handler](CoroutineHandler* current_handler, fit::function<void()> callback) {
+        handler = current_handler;
+        // This coroutine will be resumed.
+        EXPECT_EQ(ContinuationStatus::OK, handler->Yield());
+        callback();
+      });
+  bool called_2 = false;
+  CoroutineHandler* handler_2 = nullptr;
+  manager->StartCoroutine(
+      callback::SetWhenCalled(&called_2),
+      [&handler_2](CoroutineHandler* current_handler, fit::function<void()> callback) {
+        handler_2 = current_handler;
+        // This coroutine will be cancelled when the manager is deleted.
+        EXPECT_EQ(ContinuationStatus::INTERRUPTED, handler_2->Yield());
+        callback();
+      });
+  bool called_3 = false;
+  CoroutineHandler* handler_3 = nullptr;
+  manager->StartCoroutine(
+      callback::SetWhenCalled(&called_3),
+      [&handler_3](CoroutineHandler* current_handler, fit::function<void()> callback) {
+        handler_3 = current_handler;
+        // This point should never be reached, the coroutine will not even start.
+        EXPECT_EQ(ContinuationStatus::INTERRUPTED, handler_3->Yield());
+        callback();
+      });
+  // The first two tasks are started, the third one waits for a coroutine.
+  ASSERT_TRUE(handler);
+  EXPECT_TRUE(handler_2);
+  EXPECT_FALSE(handler_3);
+  // Resume first task, which deletes the manager. The second coroutine should be interrupted (its
+  // callback is not called), and the third one never started.
+  handler->Resume(ContinuationStatus::OK);
+  EXPECT_EQ(manager, nullptr);
+  EXPECT_FALSE(called_2);
+  EXPECT_EQ(handler_3, nullptr);
+  EXPECT_FALSE(called_3);
+}
+
+TEST(CoroutineManagerTest, MultipleConcurrentCoroutines) {
+  CoroutineServiceImpl coroutine_service;
+  CoroutineManager manager(&coroutine_service, 2);
+
+  bool called = false;
+  CoroutineHandler* handler = nullptr;
+  manager.StartCoroutine(
+      callback::SetWhenCalled(&called),
+      [&handler](CoroutineHandler* current_handler, fit::function<void()> callback) {
+        handler = current_handler;
+        EXPECT_EQ(ContinuationStatus::OK, handler->Yield());
+        callback();
+      });
+
+  bool called_2 = false;
+  CoroutineHandler* handler_2 = nullptr;
+  manager.StartCoroutine(
+      callback::SetWhenCalled(&called_2),
+      [&handler_2](CoroutineHandler* current_handler_2, fit::function<void()> callback) {
+        handler_2 = current_handler_2;
+        EXPECT_EQ(ContinuationStatus::OK, handler_2->Yield());
+        callback();
+      });
+
+  // Both coroutines are started.
+  ASSERT_TRUE(handler);
+  ASSERT_TRUE(handler_2);
+  // Both of them have yielded before completion.
+  EXPECT_FALSE(called);
+  EXPECT_FALSE(called_2);
+  // Resume first coroutine.
+  handler->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called);
+  EXPECT_FALSE(called_2);
+  // Resume second coroutine.
+  handler_2->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called_2);
+}
+
+TEST(CoroutineManagerTest, MultipleSerializedCoroutines) {
+  CoroutineServiceImpl coroutine_service;
+  // Limit the number of concurrent coroutines to 1 to force them running in sequence.
+  CoroutineManager manager(&coroutine_service, 1);
+
+  bool called = false;
+  CoroutineHandler* handler = nullptr;
+  manager.StartCoroutine(
+      callback::SetWhenCalled(&called),
+      [&handler](CoroutineHandler* current_handler, fit::function<void()> callback) {
+        handler = current_handler;
+        EXPECT_EQ(ContinuationStatus::OK, handler->Yield());
+        callback();
+      });
+
+  bool called_2 = false;
+  CoroutineHandler* handler_2 = nullptr;
+  manager.StartCoroutine(
+      callback::SetWhenCalled(&called_2),
+      [&handler_2](CoroutineHandler* current_handler_2, fit::function<void()> callback) {
+        handler_2 = current_handler_2;
+        EXPECT_EQ(ContinuationStatus::OK, handler_2->Yield());
+        callback();
+      });
+
+  // The first task is running in a coroutine, but has not completed yet.
+  ASSERT_TRUE(handler);
+  EXPECT_FALSE(called);
+  // The second task is enqueued, waiting for a coroutine to run.
+  EXPECT_EQ(handler_2, nullptr);
+  EXPECT_FALSE(called_2);
+  // Resume the coroutine, completing first task and starting the second one.
+  handler->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called);
+  ASSERT_TRUE(handler_2);
+  EXPECT_EQ(handler, handler_2);
+  EXPECT_FALSE(called_2);
+  // Complete the second task.
+  handler_2->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called_2);
+
+  // Start a third task. It should be executed by the same coroutine, which is kept around waiting
+  // for work to complete.
+  bool called_3 = false;
+  CoroutineHandler* handler_3 = nullptr;
+  manager.StartCoroutine(
+      callback::SetWhenCalled(&called_3),
+      [&handler_3](CoroutineHandler* current_handler_3, fit::function<void()> callback) {
+        handler_3 = current_handler_3;
+        EXPECT_EQ(ContinuationStatus::OK, handler_3->Yield());
+        callback();
+      });
+
+  ASSERT_TRUE(handler_3);
+  EXPECT_EQ(handler_3, handler_2);
+  EXPECT_FALSE(called_3);
+  handler_3->Resume(ContinuationStatus::OK);
+  EXPECT_TRUE(called_3);
 }
 
 }  // namespace
