efd204a9386ee68413ef7207a512aef856089648
Doug Evans <dje@google.com>
2019-10-08 18:42:02 +0000

    [trace/tests] Simplify argument vector building for common case
    ... of running components.
    
    Change-Id: I33a6f56372751947505850b7d94db66c9af33fc8

--- garnet/bin/trace/tests/run_test.cc
+++ garnet/bin/trace/tests/run_test.cc
@@ -78,40 +78,39 @@ static bool ReadTspec(const std::string& tspec_path, tracing::Spec* spec) {
   return true;
 }
 
-static void BuildTraceProgramArgv(const std::string& relative_tspec_path,
+static bool BuildTraceProgramArgs(const std::string& relative_tspec_path,
                                   const std::string& output_file_path,
-                                  std::vector<std::string>* argv) {
+                                  std::vector<std::string>* args) {
   tracing::Spec spec;
-  if (!ReadTspec(kPackageTestPrefix + relative_tspec_path, &spec))
-    return;
+  if (!ReadTspec(kPackageTestPrefix + relative_tspec_path, &spec)) {
+    return false;
+  }
 
-  argv->push_back(kTraceProgramUrl);
-  AppendLoggingArgs(argv, "");
-  argv->push_back("record");
-  argv->push_back(fxl::StringPrintf("--spec-file=%s",
+  AppendLoggingArgs(args, "");
+  args->push_back("record");
+  args->push_back(fxl::StringPrintf("--spec-file=%s",
                                     (kSystemPackageTestPrefix + relative_tspec_path).c_str()));
-  argv->push_back(fxl::StringPrintf("--output-file=%s", output_file_path.c_str()));
+  args->push_back(fxl::StringPrintf("--output-file=%s", output_file_path.c_str()));
 
-  AppendLoggingArgs(argv, "--append-args=");
+  AppendLoggingArgs(args, "--append-args=");
 
   // Note that |relative_tspec_path| cannot have a comma.
-  argv->push_back(fxl::StringPrintf(
+  args->push_back(fxl::StringPrintf(
       "--append-args=run,%s",
       ((spec.spawn ? kSystemPackageTestPrefix : kPackageTestPrefix) + relative_tspec_path)
           .c_str()));
+
+  return true;
 }
 
-static void BuildVerificationProgramArgv(const std::string& program_path,
-                                         const std::string& relative_tspec_path,
+static void BuildVerificationProgramArgs(const std::string& relative_tspec_path,
                                          const std::string& output_file_path,
-                                         std::vector<std::string>* argv) {
-  argv->push_back(program_path);
+                                         std::vector<std::string>* args) {
+  AppendLoggingArgs(args, "");
 
-  AppendLoggingArgs(argv, "");
-
-  argv->push_back("verify");
-  argv->push_back(relative_tspec_path);
-  argv->push_back(output_file_path);
+  args->push_back("verify");
+  args->push_back(relative_tspec_path);
+  args->push_back(output_file_path);
 }
 
 zx_status_t SpawnProgram(const zx::job& job, const std::vector<std::string>& argv,
@@ -196,11 +195,7 @@ static bool LaunchApp(sys::ComponentContext* context, const std::string& app,
   launch_info.url = std::string(app);
   launch_info.arguments = fidl::To<fidl::VectorPtr<std::string>>(args);
 
-  if (FXL_VLOG_IS_ON(1)) {
-    FXL_VLOG(1) << "Launching: " << launch_info.url << " " << fxl::JoinStrings(args, " ");
-  } else {
-    FXL_LOG(INFO) << "Launching: " << launch_info.url;
-  }
+  FXL_LOG(INFO) << "Launching: " << launch_info.url << " " << fxl::JoinStrings(args, " ");
 
   fuchsia::sys::ComponentControllerPtr component_controller;
   int64_t return_code = INT64_MIN;
@@ -250,12 +245,14 @@ static bool LaunchApp(sys::ComponentContext* context, const std::string& app,
 
 bool RunTspec(sys::ComponentContext* context, const std::string& relative_tspec_path,
               const std::string& output_file_path) {
-  std::vector<std::string> argv;
-  BuildTraceProgramArgv(relative_tspec_path, output_file_path, &argv);
+  std::vector<std::string> args;
+  if (!BuildTraceProgramArgs(relative_tspec_path, output_file_path, &args)) {
+    return false;
+  }
 
   FXL_LOG(INFO) << "Running tspec " << relative_tspec_path << ", output file " << output_file_path;
 
-  return LaunchApp(context, argv[0], std::vector<std::string>(argv.begin() + 1, argv.end()));
+  return LaunchApp(context, kTraceProgramUrl, args);
 }
 
 bool VerifyTspec(sys::ComponentContext* context, const std::string& relative_tspec_path,
@@ -268,11 +265,10 @@ bool VerifyTspec(sys::ComponentContext* context, const std::string& relative_tsp
   FXL_DCHECK(spec.app);
   const std::string& program_path = *spec.app;
 
-  std::vector<std::string> argv;
-  BuildVerificationProgramArgv(
-      program_path,
+  std::vector<std::string> args;
+  BuildVerificationProgramArgs(
       ((spec.spawn ? kSystemPackageTestPrefix : kPackageTestPrefix) + relative_tspec_path),
-      output_file_path, &argv);
+      output_file_path, &args);
 
   FXL_LOG(INFO) << "Verifying tspec " << relative_tspec_path << ", output file "
                 << output_file_path;
@@ -280,8 +276,12 @@ bool VerifyTspec(sys::ComponentContext* context, const std::string& relative_tsp
   // For consistency we do the exact same thing that the trace program does.
   // We also use the same function names for easier comparison.
   if (spec.spawn) {
+    std::vector<std::string> argv{program_path};
+    for (const auto& arg : args) {
+      argv.push_back(arg);
+    }
     return LaunchTool(argv);
   } else {
-    return LaunchApp(context, argv[0], std::vector<std::string>(argv.begin() + 1, argv.end()));
+    return LaunchApp(context, program_path, args);
   }
 }
