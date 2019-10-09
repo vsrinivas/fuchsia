37738dc2cb86b5cacb972b080ee26a05d8754d35
Christopher R. Johnson <crjohns@google.com>
2019-10-09 17:21:26 +0000

    [archivist] Include stats on total disk usage.
    This change uses the Admin capability we obtain on /data and /tmp to
    get FilesystemInfo containing stats on total space utilization.
    
    Change-Id: Ib38207026f09038ac11d8e0680702cb9f164bb2e

--- src/diagnostics/archivist/src/diagnostics.rs
+++ src/diagnostics/archivist/src/diagnostics.rs
@@ -5,7 +5,9 @@ use {
     fidl_fuchsia_io::{NodeMarker, OPEN_RIGHT_READABLE},
     files_async,
     fuchsia_component::server::{ServiceFs, ServiceObjTrait},
-    fuchsia_inspect::{self as inspect, component, Node, NumericProperty, Property, UintProperty},
+    fuchsia_inspect::{
+        self as inspect, component, Node, NumericProperty, Property, StringProperty, UintProperty,
+    },
     fuchsia_vfs_pseudo_fs::{
         directory::entry::DirectoryEntry, file::asynchronous::read_only, pseudo_directory,
     },
@@ -122,6 +124,33 @@ pub fn update_current_group(stats: &EventFileGroupStats) {
     }
 }
 
+async fn get_filesystem_info_from_admin(
+    proxy: fidl_fuchsia_io::DirectoryAdminProxy,
+) -> Result<fidl_fuchsia_io::FilesystemInfo, String> {
+    let (status, info) =
+        proxy.query_filesystem().await.or_else(|_| Err("Query failed".to_string()))?;
+    if status != 0 {
+        return Err("Query returned error".to_string());
+    }
+
+    info.ok_or_else(|| "FilesystemInfo wasn't present".to_string()).map(|v| *v)
+}
+
+struct TotalStats {
+    total_bytes: UintProperty,
+    used_bytes: UintProperty,
+}
+
+fn add_filesystem_stats(stats: &Node, info: fidl_fuchsia_io::FilesystemInfo) -> TotalStats {
+    // Total bytes includes the actual size of the filesystem and shared bytes that can be used between
+    // filesystems. We aren't out of space until we've exhausted both.
+    TotalStats {
+        total_bytes: stats
+            .create_uint("total_bytes", info.total_bytes + info.free_shared_pool_bytes),
+        used_bytes: stats.create_uint("used_bytes", info.used_bytes),
+    }
+}
+
 async fn get_data_directory_stats(name: String, path: PathBuf) -> Result<Vec<u8>, zx::Status> {
     let inspector = inspect::Inspector::new_with_size(1024 * 1024); // 1MB buffer.
 
@@ -185,6 +214,31 @@ async fn get_data_directory_stats(name: String, path: PathBuf) -> Result<Vec<u8>
         }
     }
 
+    let stats = inspector.root().create_child("stats");
+    let mut error_props: Vec<StringProperty> = vec![];
+
+    async fn get_info(path: String) -> Result<fidl_fuchsia_io::FilesystemInfo, String> {
+        let proxy = io_util::open_directory_in_namespace(
+            &path,
+            fidl_fuchsia_io::OPEN_RIGHT_READABLE | fidl_fuchsia_io::OPEN_RIGHT_ADMIN,
+        )
+        .or_else(|e| Err(format!("Could not get admin access {:?}", e)))?;
+
+        let proxy = fidl_fuchsia_io::DirectoryAdminProxy::new(
+            proxy.into_channel().or_else(|e| Err(format!("Could not extract channel {:?}", e)))?,
+        );
+
+        get_filesystem_info_from_admin(proxy).await
+    }
+
+    let _total_stats = match get_info(path.to_string_lossy().to_string()).await {
+        Ok(info) => Some(add_filesystem_stats(&stats, info)),
+        Err(val) => {
+            error_props.push(stats.create_string("error", val));
+            None
+        }
+    };
+
     inspector.copy_vmo_data().ok_or(zx::Status::INTERNAL)
 }
 
@@ -212,6 +266,7 @@ mod test {
     use fuchsia_inspect::assert_inspect_tree;
     use fuchsia_inspect::health::Reporter;
     use fuchsia_inspect::reader::NodeHierarchy;
+    use futures::TryStreamExt;
     use io_util;
     use std::convert::TryFrom;
     use std::fs::File;
@@ -361,6 +416,9 @@ mod test {
             .expect("get data");
         assert_inspect_tree!(NodeHierarchy::try_from(val).expect("data is not an inspect file"),
         root: {
+            stats: contains {
+                error: "Query failed"
+            },
             test_data: {
                 size: 68u64,
                 "top.txt": {
@@ -423,6 +481,9 @@ mod test {
         assert_inspect_tree!(
             NodeHierarchy::try_from(bytes).expect("file is not an inspect file"),
         root: {
+            stats: contains {
+                error: "Query failed"
+            },
             test_data: contains {
                 size:0u64,
                 "test.txt": {
@@ -433,5 +494,81 @@ mod test {
 
         assert!(proxy.describe().await.is_ok());
         assert!(f.describe().await.is_ok());
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn get_filesystem_info_from_admin_test() {
+        let (proxy, server) = create_proxy::<fidl_fuchsia_io::DirectoryAdminMarker>()
+            .expect("failed to create proxy");
+
+        fasync::spawn(async move {
+            let mut stream = server.into_stream().expect("failed to convert to stream");
+            while let Some(request) = stream.try_next().await.expect("failed to unwrap request") {
+                match request {
+                    fidl_fuchsia_io::DirectoryAdminRequest::QueryFilesystem { responder } => {
+                        responder
+                            .send(
+                                0,
+                                Some(fidl::encoding::OutOfLine(
+                                    &mut fidl_fuchsia_io::FilesystemInfo {
+                                        total_bytes: 800,
+                                        used_bytes: 200,
+                                        total_nodes: 0,
+                                        used_nodes: 0,
+                                        free_shared_pool_bytes: 200,
+                                        fs_id: 0,
+                                        block_size: 4096,
+                                        max_filename_size: 128,
+                                        fs_type: 0,
+                                        padding: 0,
+                                        name: ['a' as i8; 32],
+                                    },
+                                )),
+                            )
+                            .expect("failed to send response");
+                    }
+                    r => {
+                        eprintln!("Error: Unknown request {:?}", r);
+                        return;
+                    }
+                }
+            }
+        });
+
+        let info =
+            get_filesystem_info_from_admin(proxy).await.expect("failed to get filesystem info");
+        let inspector = inspect::Inspector::new();
+        let _props = add_filesystem_stats(inspector.root(), info);
+        assert_inspect_tree!(inspector,
+        root: {
+            total_bytes: 1000u64,
+            used_bytes: 200u64
+        });
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn get_filesystem_info_from_admin_failure_test() {
+        let (proxy, server) = create_proxy::<fidl_fuchsia_io::DirectoryAdminMarker>()
+            .expect("failed to create proxy");
+
+        fasync::spawn(async move {
+            let mut stream = server.into_stream().expect("failed to convert to stream");
+            while let Some(request) = stream.try_next().await.expect("failed to unwrap request") {
+                match request {
+                    fidl_fuchsia_io::DirectoryAdminRequest::QueryFilesystem { responder } => {
+                        responder.send(1, None).expect("failed to send response");
+                    }
+                    r => {
+                        eprintln!("Error: Unknown request {:?}", r);
+                        return;
+                    }
+                }
+            }
+        });
+
+        assert_eq!(
+            Err("Query returned error".to_string()),
+            get_filesystem_info_from_admin(proxy).await
+        );
     }
 }
