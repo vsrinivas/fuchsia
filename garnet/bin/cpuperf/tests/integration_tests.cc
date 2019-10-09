c0879d09856564834f7802529c77c31941e64a32
Doug Evans <dje@google.com>
2019-10-09 17:38:40 +0000

    [cpuperf_integration_tests] Disable ValueRecords test
    bug: 38183
    Change-Id: Ib09fd6037e7f27454ff85db9f12bfdc535fb37d9

--- garnet/bin/cpuperf/tests/integration_tests.cc
+++ garnet/bin/cpuperf/tests/integration_tests.cc
@@ -27,7 +27,8 @@ TEST(Cpuperf, ProgrammableCounters) { RunAndVerify("/pkg/data/programmable_count
 
 TEST(Cpuperf, UserFlag) { RunAndVerify("/pkg/data/user_flag.cpspec"); }
 
-TEST(Cpuperf, ValueRecords) { RunAndVerify("/pkg/data/value_records.cpspec"); }
+//TODO(38183): Re-enable.
+TEST(Cpuperf, DISABLED_ValueRecords) { RunAndVerify("/pkg/data/value_records.cpspec"); }
 
 TEST(Cpuperf, LastBranchRecord) {
   perfmon::Properties properties;
