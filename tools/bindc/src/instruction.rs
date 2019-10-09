2622db72e719da93e397a17602a04e92f30918f6
Alex Legg <alexlegg@google.com>
2019-10-09 00:37:51 +0000

    [bindc] Implement encode for instructions
    We will use this to write instructions to the output file.
    
    Bug: 34604
    
    Change-Id: Iac19eb6c73c18e080f99cc6a30e6b8fd461a9892

--- tools/bindc/src/instruction.rs
+++ tools/bindc/src/instruction.rs
@@ -6,7 +6,8 @@
 
 #![allow(dead_code)]
 
-use bitfield::bitfield;
+use bitfield::{bitfield, BitRange};
+use std::io::{self, Write};
 
 bitfield! {
     /// Each instruction is a pair of 32 bit unsigned integers divided as
@@ -35,6 +36,20 @@ pub enum Condition {
     LessThanEqual(u32, u32),
 }
 
+impl Condition {
+    fn to_raw(self) -> (u32, u32, u32) {
+        match self {
+            Condition::Always => (0, 0, 0),
+            Condition::Equal(b, v) => (1, b, v),
+            Condition::NotEqual(b, v) => (2, b, v),
+            Condition::GreaterThan(b, v) => (3, b, v),
+            Condition::LessThan(b, v) => (4, b, v),
+            Condition::GreaterThanEqual(b, v) => (5, b, v),
+            Condition::LessThanEqual(b, v) => (6, b, v),
+        }
+    }
+}
+
 pub enum Instruction {
     Abort(Condition),
     Match(Condition),
@@ -42,41 +57,38 @@ pub enum Instruction {
     Label(u32),
 }
 
-fn to_raw_condition(condition: Condition) -> (u32, u32, u32) {
-    match condition {
-        Condition::Always => (0, 0, 0),
-        Condition::Equal(b, v) => (1, b, v),
-        Condition::NotEqual(b, v) => (2, b, v),
-        Condition::GreaterThan(b, v) => (3, b, v),
-        Condition::LessThan(b, v) => (4, b, v),
-        Condition::GreaterThanEqual(b, v) => (5, b, v),
-        Condition::LessThanEqual(b, v) => (6, b, v),
+impl Instruction {
+    pub fn encode(self, output: &mut dyn Write) -> Result<(), io::Error> {
+        let raw = self.to_raw();
+        let bit_range: u64 = raw.bit_range(63, 0);
+        output.write(&bit_range.to_le_bytes())?;
+        Ok(())
     }
-}
 
-fn to_raw_instruction(instruction: Instruction) -> RawInstruction<[u32; 2]> {
-    let (c, o, a, b, v) = match instruction {
-        Instruction::Abort(condition) => {
-            let (c, b, v) = to_raw_condition(condition);
-            (c, 0, 0, b, v)
-        }
-        Instruction::Match(condition) => {
-            let (c, b, v) = to_raw_condition(condition);
-            (c, 1, 0, b, v)
-        }
-        Instruction::Goto(condition, a) => {
-            let (c, b, v) = to_raw_condition(condition);
-            (c, 2, a, b, v)
-        }
-        Instruction::Label(a) => (0, 5, a, 0, 0),
-    };
-    let mut raw_instruction = RawInstruction([0, 0]);
-    raw_instruction.set_condition(c);
-    raw_instruction.set_operation(o);
-    raw_instruction.set_parameter_a(a);
-    raw_instruction.set_parameter_b(b);
-    raw_instruction.set_value(v);
-    raw_instruction
+    fn to_raw(self) -> RawInstruction<[u32; 2]> {
+        let (c, o, a, b, v) = match self {
+            Instruction::Abort(condition) => {
+                let (c, b, v) = condition.to_raw();
+                (c, 0, 0, b, v)
+            }
+            Instruction::Match(condition) => {
+                let (c, b, v) = condition.to_raw();
+                (c, 1, 0, b, v)
+            }
+            Instruction::Goto(condition, a) => {
+                let (c, b, v) = condition.to_raw();
+                (c, 2, a, b, v)
+            }
+            Instruction::Label(a) => (0, 5, a, 0, 0),
+        };
+        let mut raw_instruction = RawInstruction([0, 0]);
+        raw_instruction.set_condition(c);
+        raw_instruction.set_operation(o);
+        raw_instruction.set_parameter_a(a);
+        raw_instruction.set_parameter_b(b);
+        raw_instruction.set_value(v);
+        raw_instruction
+    }
 }
 
 #[cfg(test)]
@@ -143,7 +155,7 @@ mod tests {
     #[test]
     fn test_abort_value() {
         let instruction = Instruction::Abort(Condition::Always);
-        let raw_instruction = to_raw_instruction(instruction);
+        let raw_instruction = instruction.to_raw();
         assert_eq!(raw_instruction.0[0], 0 << 4);
         assert_eq!(raw_instruction.0[1], 0);
         assert_eq!(raw_instruction.operation(), 0)
@@ -152,7 +164,7 @@ mod tests {
     #[test]
     fn test_match_value() {
         let instruction = Instruction::Match(Condition::Always);
-        let raw_instruction = to_raw_instruction(instruction);
+        let raw_instruction = instruction.to_raw();
         assert_eq!(raw_instruction.0[0], 1 << 4);
         assert_eq!(raw_instruction.0[1], 0);
         assert_eq!(raw_instruction.operation(), 1)
@@ -161,7 +173,7 @@ mod tests {
     #[test]
     fn test_goto_value() {
         let instruction = Instruction::Goto(Condition::Always, 0);
-        let raw_instruction = to_raw_instruction(instruction);
+        let raw_instruction = instruction.to_raw();
         assert_eq!(raw_instruction.0[0], 2 << 4);
         assert_eq!(raw_instruction.0[1], 0);
         assert_eq!(raw_instruction.operation(), 2)
@@ -170,7 +182,7 @@ mod tests {
     #[test]
     fn test_label_value() {
         let instruction = Instruction::Label(0);
-        let raw_instruction = to_raw_instruction(instruction);
+        let raw_instruction = instruction.to_raw();
         assert_eq!(raw_instruction.0[0], 5 << 4);
         assert_eq!(raw_instruction.0[1], 0);
         assert_eq!(raw_instruction.operation(), 5)
@@ -179,7 +191,7 @@ mod tests {
     #[test]
     fn test_complicated_value() {
         let instruction = Instruction::Goto(Condition::LessThan(23, 1234), 42);
-        let raw_instruction = to_raw_instruction(instruction);
+        let raw_instruction = instruction.to_raw();
         assert_eq!(raw_instruction.0[0], 4 | (2 << 4) | (42 << 8) | (23 << 16));
         assert_eq!(raw_instruction.0[1], 1234);
         assert_eq!(raw_instruction.condition(), 4);
@@ -187,5 +199,20 @@ mod tests {
         assert_eq!(raw_instruction.parameter_a(), 42);
         assert_eq!(raw_instruction.parameter_b(), 23);
         assert_eq!(raw_instruction.value(), 1234)
+    }
+
+    #[test]
+    fn test_encode() {
+        let instruction = Instruction::Goto(Condition::LessThan(23, 1234), 42);
+        let mut buf : Vec<u8> = vec![];
+        instruction.encode(&mut buf).unwrap();
+        assert_eq!(buf[0], 4 | (2 << 4));
+        assert_eq!(buf[1], 42);
+        assert_eq!(buf[2], 23);
+        assert_eq!(buf[3], 0);
+        assert_eq!(buf[4], 210);
+        assert_eq!(buf[5], 4);
+        assert_eq!(buf[6], 0);
+        assert_eq!(buf[7], 0);
     }
 }
