7b06737ef5511c357dedc7f70baf320eda485091
Joshua Seaton <joshuaseaton@google.com>
2019-10-10 19:11:38 +0000

    [artifactory] Move code
    This change is purely organizational: it moves the copy logic from the
    main package to an "artifactory" package. This will mitigate some
    possible clutter to be introduced per the attached bug.
    
    Bug: 38288
    
    Change-Id: I42d284d3ae09409b27c217cf66a2ea71e678c214

--- tools/artifactory/cmd/up.go
+++ tools/artifactory/cmd/up.go
@@ -15,9 +15,9 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
 	"sync"
 
+	"go.fuchsia.dev/fuchsia/tools/artifactory/lib"
 	"go.fuchsia.dev/fuchsia/tools/lib/logger"
 
 	"cloud.google.com/go/storage"
@@ -27,10 +27,6 @@ import (
 const (
 	// The size in bytes at which files will be read and written to GCS.
 	chunkSize = 8 * 1024 * 1024
-
-	// Err412Msg is substring of an error message returned that indicates a 412
-	// HTTP response.
-	err412Msg = "Error 412"
 
 	// Relative path within the build directory to the repo produced by a build.
 	repoSubpath = "amber-files"
@@ -251,45 +247,7 @@ func (s cloudSink) write(ctx context.Context, name string, path string, expected
 	}
 	defer fd.Close()
 
-	return copyToCloud(ctx, fullName, fd, w)
-}
-
-func copyToCloud(ctx context.Context, objName string, r io.Reader, w io.WriteCloser) error {
-	var err error
-	for {
-		if _, err = io.CopyN(w, r, chunkSize); err != nil {
-			break
-		}
-	}
-
-	// If the precondition of the object not existing is not met on write (i.e.,
-	// at the time of the write the object is there), then the server will respond
-	// with a 412. (See
-	// https://cloud.google.com/storage/docs/json_api/v1/status-codes and
-	// https://tools.ietf.org/html/rfc7232#section-4.2.)
-	// We do not report this as an error, however, as the associated object might
-	// have been created after having checked its non-existence - and we wish to
-	// be resilient in the event of such a race.
-	handleOkayError := func(err error) error {
-		preconditionNotMet := err != nil && strings.Contains(err.Error(), err412Msg)
-		if err == io.EOF || preconditionNotMet {
-			if preconditionNotMet {
-				logger.Warningf(ctx, "object %q: created after its non-existence check", objName)
-			}
-			return nil
-		}
-		return err
-	}
-
-	// Writes happen asynchronously, and so a nil may be returned while the write
-	// goes on to fail. It is recommended in
-	// https://godoc.org/cloud.google.com/go/storage#Writer.Write
-	// to return the value of Close() to detect the success of the write.
-	if handleOkayError(err) == nil {
-		return handleOkayError(w.Close())
-	}
-	w.Close()
-	return err
+	return artifactory.Copy(ctx, fullName, fd, w, chunkSize)
 }
 
 type checksumError struct {
