183c0902c6fa09694638d57f0e3726b34e23e475
John Grossman <johngro@google.com>
2019-10-10 05:08:43 +0000

    [fbl][test] Convert condition variable tests to zxtest
    Change-Id: I68bcf9dcc2f7ad7195e1021f4b1da7df31ee394e

--- zircon/system/ulib/fbl/test/condition_variable_tests.cc
+++ zircon/system/ulib/fbl/test/condition_variable_tests.cc
@@ -5,37 +5,30 @@
 #include <fbl/auto_lock.h>
 #include <fbl/condition_variable.h>
 #include <fbl/mutex.h>
-#include <unittest/unittest.h>
+#include <zxtest/zxtest.h>
 
-namespace fbl {
 namespace {
 
-bool EmptySignalTest() {
-  BEGIN_TEST;
-
-  ConditionVariable cvar;
+TEST(ConditionVariableTest, EmptySignal) {
+  fbl::ConditionVariable cvar;
   cvar.Signal();
   cvar.Broadcast();
-
-  END_TEST;
 }
 
-bool WaitTest() {
-  BEGIN_TEST;
-
+TEST(ConditionVariableTest, Wait) {
   struct State {
-    Mutex mutex;
-    ConditionVariable cvar;
+    fbl::Mutex mutex;
+    fbl::ConditionVariable cvar;
   } state;
 
   thrd_t thread;
-  AutoLock lock(&state.mutex);
+  fbl::AutoLock lock(&state.mutex);
 
   thrd_create(
       &thread,
       [](void* arg) {
         auto state = reinterpret_cast<State*>(arg);
-        AutoLock lock(&state->mutex);
+        fbl::AutoLock lock(&state->mutex);
         state->cvar.Signal();
         return 0;
       },
@@ -43,14 +36,6 @@ bool WaitTest() {
 
   state.cvar.Wait(&state.mutex);
   thrd_join(thread, NULL);
-
-  END_TEST;
 }
 
 }  // namespace
-}  // namespace fbl
-
-BEGIN_TEST_CASE(ConditionVariableTests)
-RUN_TEST(fbl::EmptySignalTest)
-RUN_TEST(fbl::WaitTest)
-END_TEST_CASE(ConditionVariableTests)
