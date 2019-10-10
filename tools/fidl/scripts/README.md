35be4c0f8bd4d0a65b6fcb8523be1e35eabecdb4
Mitchell Kember <mkember@google.com>
2019-10-10 22:46:33 +0000

    [fidl] Add script for finding nullable union fields
    This CL adds a new directory, tools/fidl/scripts, meant for small
    scripts we might want to re-use or reference later on. It also adds a
    first script: find_nullable_union_fields.py. This was used to find
    occurrences of nullable union fields. See fxb/38341 for details.
    
    Test: no code change
    Change-Id: Ic28c2e2806944e48561c2cf3a2a08d76a77dccc9

---
+++ tools/fidl/scripts/README.md
@@ -0,0 +1,11 @@
+# FIDL scripts
+
+This directory contains scripts used by FIDL contributors. When writing a
+script, consider saving it here if there is any chance it could be reused,
+including being used as a reference for future scripts. All scripts must be
+documented in this file.
+
+## find_nullable_union_fields.py
+
+This script analyzes all `.fidl` files in the fuchsia repository and reports
+occurrences of nullable fields in unions.
---
+++ tools/fidl/scripts/find_nullable_union_fields.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import os
+import sys
+
+
+def main():
+    for path in Path(os.environ["FUCHSIA_DIR"]).rglob("*.fidl"):
+        if not path.is_file():
+            continue
+        with open(path) as file:
+            try:
+                check_file(file)
+            except UnicodeDecodeError:
+                print(f"SKIPPING {path}: not unicode")
+
+
+def check_file(file):
+    line_parser = top_level
+    for index, line in enumerate(file):
+        if "//" in line:
+            line = line[:line.index("//")]
+        line = line.strip()
+        result = line_parser(line)
+        if result:
+            line_parser, output = result
+            if output:
+                info = "" if file.name.endswith(".test.fidl") else "*** "
+                info += f"{file.name}:{index + 1}"
+                print(f"{info}: {output}")
+
+
+def top_level(line):
+    if line.startswith("union "):
+        if "{" in line and "};" in line and "?" not in line:
+            return
+        if line.endswith("{"):
+            return in_union, None
+        return top_level, "unexpected formatting"
+
+    keywords = "struct", "xunion", "enum", "bits", "table", "protocol"
+    if any(line.startswith(k + " ") for k in keywords):
+        if "{" in line and "};" in line:
+            return
+        if not line.endswith("{"):
+            return top_level, "unexpected formatting"
+        return in_other, None
+
+
+def in_union(line):
+    if line.startswith("}"):
+        assert line == "};"
+        return top_level, None
+    if "?" in line:
+        return in_union, "nullable union field"
+
+
+def in_other(line):
+    if line.startswith("}"):
+        assert line == "};"
+        return top_level, None
+
+
+if __name__ == "__main__":
+    main()
