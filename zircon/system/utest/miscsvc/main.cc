ebf77b24806d7a555726826dcb55ced8ba6b4953
Ian McKellar <ianloic@google.com>
2019-10-15 03:18:50 +0000

    [fidl] Port miscsvc test to llcpp
    Bug: 38504
    Change-Id: I86d9f0c97ae1077857fe7a82d59df771bfbd636a

--- zircon/system/utest/miscsvc/main.cc
+++ zircon/system/utest/miscsvc/main.cc
@@ -4,7 +4,7 @@
 
 #include <fcntl.h>
 
-#include <fuchsia/paver/c/fidl.h>
+#include <fuchsia/paver/llcpp/fidl.h>
 #include <lib/fdio/fd.h>
 #include <lib/fdio/fdio.h>
 #include <lib/fdio/directory.h>
@@ -16,6 +16,8 @@
 
 namespace {
 
+using llcpp::fuchsia::paver::Paver;
+
 TEST(MiscSvcTest, PaverSvccEnumeratesSuccessfully) {
   zx::channel svc_local, svc_remote;
   ASSERT_OK(zx::channel::create(0, &svc_local, &svc_remote));
@@ -23,10 +25,11 @@ TEST(MiscSvcTest, PaverSvccEnumeratesSuccessfully) {
 
   zx::channel local, remote;
   ASSERT_OK(zx::channel::create(0, &local, &remote));
-  ASSERT_OK(fdio_service_connect_at(svc_local.get(), fuchsia_paver_Paver_Name, remote.release()));
+  ASSERT_OK(fdio_service_connect_at(svc_local.get(), Paver::Name, remote.release()));
 
-  fuchsia_paver_Paver_QueryActiveConfiguration_Result result;
-  ASSERT_OK(fuchsia_paver_PaverQueryActiveConfiguration(local.get(), &result));
+  Paver::SyncClient paver(std::move(local));
+  auto result = paver.QueryActiveConfiguration();
+  ASSERT_OK(result.status());
 }
 
 }  // namespace
