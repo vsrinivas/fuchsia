89818a380ac65b44e7f1a69a2a67e619162b7783
Ankur Mittal <anmittal@google.com>
2019-10-17 23:42:39 +0000

    [test_executor] Refactor out test running logic.
    Refactoring out test runnig logic so that we can use it to create on
    device binary which can be used by run_test_component to run these
    tests.
    
    BUG=37809
    
    TEST=current tests should cover it, also added new unit test.
    
    Change-Id: I444cd1cd409c59055a1e6b99589f978f0b730f35

--- garnet/bin/sl4f/src/test/facade.rs
+++ garnet/bin/sl4f/src/test/facade.rs
@@ -6,106 +6,34 @@ use {
     crate::test::types::{
         StepResult, StepResultItem, TestPlan, TestPlanTest, TestResult, TestResultItem, TestResults,
     },
-    failure::ResultExt,
-    fidl_fuchsia_sys,
-    fidl_fuchsia_test::{
-        Invocation,
-        RunListenerRequest::{OnTestCaseFinished, OnTestCaseStarted},
-    },
-    fuchsia_async as fasync,
+    fidl_fuchsia_sys, fuchsia_async as fasync,
     fuchsia_syslog::macros::*,
-    fuchsia_zircon as zx,
-    futures::{
-        future::BoxFuture,
-        io::{self, AsyncRead},
-        prelude::*,
-        ready,
-        task::{Context, Poll},
-    },
+    futures::{channel::mpsc, prelude::*},
     serde_json::Value,
-    std::{cell::RefCell, collections::HashMap, fmt, fs::File, io::Write, marker::Unpin, pin::Pin},
+    std::{collections::HashMap, fmt, fs::File, io::Write},
+    test_executor::TestEvent,
     uuid::Uuid,
-    zx::HandleBased,
 };
-
-#[must_use = "futures/streams"]
-pub struct LoggerStream {
-    socket: fasync::Socket,
-}
-impl Unpin for LoggerStream {}
-
-thread_local! {
-    pub static BUFFER:
-        RefCell<[u8; 256]> = RefCell::new([0; 256]);
-}
-
-impl LoggerStream {
-    /// Creates a new `LoggerStream` for given `socket`.
-    pub fn new(socket: zx::Socket) -> Result<LoggerStream, failure::Error> {
-        let l = LoggerStream {
-            socket: fasync::Socket::from_socket(socket).context("Invalid zircon socket")?,
-        };
-        Ok(l)
-    }
-}
-
-fn process_log_bytes(bytes: &[u8]) -> String {
-    // TODO(anmittal): Change this to consider break in logs and handle it.
-    let log = std::str::from_utf8(bytes).unwrap();
-    log.to_string()
-}
-
-impl Stream for LoggerStream {
-    type Item = io::Result<String>;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        BUFFER.with(|b| {
-            let mut b = b.borrow_mut();
-            let len = ready!(Pin::new(&mut self.socket).poll_read(cx, &mut *b)?);
-            if len == 0 {
-                return Poll::Ready(None);
-            }
-            Poll::Ready(Some(process_log_bytes(&b[0..len])).map(Ok))
-        })
-    }
-}
 
 struct Step {
     pub result: StepResult,
-    f: Option<BoxFuture<'static, io::Result<String>>>,
+    logs: Option<String>,
 }
 
 impl Step {
     pub fn default() -> Step {
-        Step { result: StepResult::default(), f: None }
+        Step { result: StepResult::default(), logs: None }
     }
 
-    pub fn start_log_collection(&mut self, logger_socket: zx::Socket) {
-        if logger_socket.is_invalid_handle() {
-            return;
+    pub fn add_log(&mut self, log: String) {
+        match &mut self.logs {
+            Some(s) => s.push_str(&log),
+            None => self.logs = Some(log),
         }
-
-        let ls = match LoggerStream::new(logger_socket) {
-            Err(e) => {
-                fx_log_err!("Logger: Failed to create fuchsia async socket: {:?}", e);
-                return;
-            }
-            Ok(ls) => ls,
-        };
-
-        let (remote, remote_handle) = ls.try_collect::<String>().remote_handle();
-
-        // spawn so that log collection can run in background.
-        fasync::spawn(remote);
-
-        self.f = Some(remote_handle.boxed());
     }
 
-    pub async fn await_logs(&mut self) -> Result<String, failure::Error> {
-        if let Some(ref mut f) = self.f.take().as_mut() {
-            return Ok(f.await?);
-        }
-        Ok("".to_string())
+    pub fn take_logs(&mut self) -> String {
+        self.logs.take().unwrap_or_else(|| "".to_string())
     }
 }
 
@@ -137,52 +65,19 @@ impl TestFacade {
     }
 
     async fn run_test_component(&self, url: String) -> Result<TestResult, failure::Error> {
-        let component_manager_for_test = "fuchsia-pkg://fuchsia.com/component_manager_for_test#\
-                                          meta/component_manager_for_test.cmx";
         let launcher = match fuchsia_component::client::connect_to_service::<
             fidl_fuchsia_sys::LauncherMarker,
         >() {
             Ok(l) => l,
             Err(e) => return Err(e),
         };
-        fx_log_info!("connecting to test component {}", url);
-        let is_v2_component = url.ends_with(".cm");
-        let mut app;
-        if is_v2_component {
-            app = fuchsia_component::client::launch(
-                &launcher,
-                component_manager_for_test.to_string(),
-                Some(vec![url.clone()]),
-            )
-            .map_err(|e| format_err!("Not able to launch v2 test:{}: {}", url, e))?;
-        } else {
-            app = fuchsia_component::client::launch(&launcher, url.clone(), None)
-                .map_err(|e| format_err!("Not able to launch v1 test:{}: {}", url, e))?;
-        }
 
-        fx_log_info!("connecting to test service");
-        let suite = app
-            .connect_to_service::<fidl_fuchsia_test::SuiteMarker>()
-            .map_err(|e| format_err!("Error connecting to test service: {}", e))?;
-        fx_log_info!("enumerating tests");
-        let cases =
-            suite.get_tests().await.map_err(|e| format_err!("Error getting test steps: {}", e))?;
-        fx_log_info!("got test list: {:#?}", cases);
-        let mut invocations = Vec::<Invocation>::new();
-        for case in cases {
-            invocations.push(Invocation { case: Some(case) });
-        }
-        let (run_listener_client, mut run_listener) =
-            fidl::endpoints::create_request_stream::<fidl_fuchsia_test::RunListenerMarker>()
-                .map_err(|e| format_err!("Error creating request stream: {}", e))?;
-        fx_log_info!("running tests");
-        suite
-            .run(
-                &mut invocations.into_iter().map(|i| i.into()),
-                fidl_fuchsia_test::RunOptions {},
-                run_listener_client,
-            )
-            .map_err(|e| format_err!("Error running tests in '{}': {}", url, e))?;
+        let (sender, mut recv) = mpsc::channel(1);
+
+        let (remote, test_fut) =
+            test_executor::run_test_component(launcher, url.clone(), sender).remote_handle();
+
+        fasync::spawn(remote);
 
         #[derive(PartialEq)]
         enum TestOutcome {
@@ -206,49 +101,56 @@ impl TestFacade {
         let mut test_outcome = TestOutcome::Passed;
 
         let mut current_step_map = HashMap::new();
-        while let Some(result_event) = run_listener
-            .try_next()
-            .await
-            .map_err(|e| format_err!("Error waiting for listener: {}", e))?
-        {
-            match result_event {
-                OnTestCaseStarted { name, primary_log, control_handle: _ } => {
-                    let mut step = Step {
-                        result: StepResult { name: name.clone(), ..StepResult::default() },
+
+        while let Some(test_event) = recv.next().await {
+            match test_event {
+                TestEvent::TestCaseStarted { test_case_name } => {
+                    let step = Step {
+                        result: StepResult {
+                            name: test_case_name.clone(),
+                            ..StepResult::default()
+                        },
                         ..Step::default()
                     };
 
-                    step.start_log_collection(primary_log);
-                    current_step_map.insert(name, step);
+                    current_step_map.insert(test_case_name, step);
                 }
-                OnTestCaseFinished { name, outcome, control_handle: _ } => {
-                    match current_step_map.get_mut(&name) {
+                TestEvent::TestCaseFinished { test_case_name, outcome } => {
+                    match current_step_map.get_mut(&test_case_name) {
                         Some(step) => {
-                            step.result.outcome = match outcome.status {
-                                Some(status) => match status {
-                                    fidl_fuchsia_test::Status::Passed => "passed".to_string(),
-                                    fidl_fuchsia_test::Status::Failed => {
-                                        if test_outcome == TestOutcome::Passed {
-                                            test_outcome = TestOutcome::Failed;
-                                        }
-                                        "failed".to_string()
+                            step.result.outcome = match outcome {
+                                test_executor::Outcome::Passed => "passed".to_string(),
+                                test_executor::Outcome::Failed => {
+                                    if test_outcome == TestOutcome::Passed {
+                                        test_outcome = TestOutcome::Failed;
                                     }
-                                },
-                                // This will happen when test protocol is not properly implemented
-                                // by the test and it forgets to set the outcome.
-                                None => {
+                                    "failed".to_string()
+                                }
+                                test_executor::Outcome::Error => {
                                     test_outcome = TestOutcome::Error;
                                     "error".to_string()
                                 }
                             };
                         }
                         None => {
-                            return Err(format_err!("test case: '{}' not found", name));
+                            return Err(format_err!("test case: '{}' not found", test_case_name));
+                        }
+                    }
+                }
+                TestEvent::LogMessage { test_case_name, msg } => {
+                    match current_step_map.get_mut(&test_case_name) {
+                        Some(step) => {
+                            step.add_log(msg);
+                        }
+                        None => {
+                            return Err(format_err!("test case: '{}' not found", test_case_name));
                         }
                     }
                 }
             }
         }
+
+        test_fut.await.map_err(|e| format_err!("Error running test: {}", e))?;
 
         let mut step_results = Vec::<StepResultItem>::new();
 
@@ -263,7 +165,7 @@ impl TestFacade {
                 }
                 step.result.outcome = "inconclusive".to_string();
             }
-            let logs = step.await_logs().await?;
+            let logs = step.take_logs();
 
             if logs.len() > 0 {
                 // TODO(anmittal): Display log until host can pull logs and display it.
@@ -277,8 +179,6 @@ impl TestFacade {
 
             step_results.push(StepResultItem::Result(step.result));
         }
-
-        app.kill().map_err(|e| format_err!("Error killing test '{}': {}", url, e))?;
 
         let mut test_result = TestResult::default();
         test_result.outcome = test_outcome.to_string();
---
+++ src/lib/test_executor/rust/src/lib.rs
@@ -0,0 +1,309 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! This crate runs and collects results from a test which implements fuchsia.test.Suite protocol.
+
+use {
+    failure::{format_err, ResultExt},
+    fidl_fuchsia_sys::LauncherProxy,
+    fidl_fuchsia_test::{
+        Invocation,
+        RunListenerRequest::{OnTestCaseFinished, OnTestCaseStarted},
+    },
+    fuchsia_async as fasync,
+    fuchsia_syslog::{fx_vlog, macros::*},
+    fuchsia_zircon as zx,
+    futures::{
+        channel::mpsc,
+        future::join_all,
+        future::BoxFuture,
+        io::{self, AsyncRead},
+        prelude::*,
+        ready,
+        task::{Context, Poll},
+    },
+    std::{cell::RefCell, marker::Unpin, pin::Pin},
+    zx::HandleBased,
+};
+
+/// Defines the outcome of a test case run.
+#[derive(PartialEq, Debug)]
+pub enum Outcome {
+    /// Test case passed.
+    Passed,
+    /// Test case failed.
+    Failed,
+    /// Test case did not communicate the outcome.
+    Error,
+}
+
+/// Event to send to caller of `run_test_component`
+/// TestCaseStarted: Whenever a new test case is started.
+/// TestCaseFinished: Whenever a test case finishes.
+/// LogMessage: Whenever a test case produces a log message.
+#[derive(PartialEq, Debug)]
+pub enum TestEvent {
+    TestCaseStarted { test_case_name: String },
+    TestCaseFinished { test_case_name: String, outcome: Outcome },
+    LogMessage { test_case_name: String, msg: String },
+}
+
+#[must_use = "futures/streams"]
+pub struct LoggerStream {
+    socket: fasync::Socket,
+}
+impl Unpin for LoggerStream {}
+
+thread_local! {
+    pub static BUFFER:
+        RefCell<[u8; 2048]> = RefCell::new([0; 2048]);
+}
+
+impl LoggerStream {
+    /// Creates a new `LoggerStream` for given `socket`.
+    pub fn new(socket: zx::Socket) -> Result<LoggerStream, failure::Error> {
+        let l = LoggerStream {
+            socket: fasync::Socket::from_socket(socket).context("Invalid zircon socket")?,
+        };
+        Ok(l)
+    }
+}
+
+fn process_log_bytes(bytes: &[u8]) -> String {
+    // TODO(anmittal): Change this to consider break in logs and handle it.
+    let log = std::str::from_utf8(bytes).unwrap();
+    log.to_string()
+}
+
+impl Stream for LoggerStream {
+    type Item = io::Result<String>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        BUFFER.with(|b| {
+            let mut b = b.borrow_mut();
+            let len = ready!(Pin::new(&mut self.socket).poll_read(cx, &mut *b)?);
+            if len == 0 {
+                return Poll::Ready(None);
+            }
+            Poll::Ready(Some(process_log_bytes(&b[0..len])).map(Ok))
+        })
+    }
+}
+
+struct LogProcessor {
+    name: String,
+    f: Option<BoxFuture<'static, Result<(), failure::Error>>>,
+}
+
+impl LogProcessor {
+    pub fn new(test_case_name: String) -> LogProcessor {
+        LogProcessor { name: test_case_name, f: None }
+    }
+
+    /// This will put a listener on `logger_socket`, process and send logs asynchronously in the
+    /// background.
+    /// Caller of this function must call `await_logs` to wait for all the logs to be collected.
+    pub fn collect_and_send_logs(
+        &mut self,
+        logger_socket: zx::Socket,
+        mut sender: mpsc::Sender<TestEvent>,
+    ) {
+        if logger_socket.is_invalid_handle() {
+            return;
+        }
+
+        let mut ls = match LoggerStream::new(logger_socket) {
+            Err(e) => {
+                fx_log_err!("Logger: Failed to create fuchsia async socket: {:?}", e);
+                return;
+            }
+            Ok(ls) => ls,
+        };
+
+        let name = self.name.clone();
+
+        let f = async move {
+            while let Some(log) = ls
+                .try_next()
+                .await
+                .map_err(|e| format_err!("Error while reading log msg: {}", e))?
+            {
+                sender
+                    .send(TestEvent::LogMessage { test_case_name: name.clone(), msg: log })
+                    .map_err(|e| format_err!("Error while sending log msg: {}", e))
+                    .await?
+            }
+            Ok(())
+        };
+
+        let (remote, remote_handle) = f.remote_handle();
+
+        fasync::spawn(remote);
+
+        self.f = Some(remote_handle.boxed());
+    }
+
+    /// This will wait for all the logs to be collected.
+    pub async fn await_logs(&mut self) -> Result<(), failure::Error> {
+        if let Some(ref mut f) = self.f.take().as_mut() {
+            return Ok(f.await?);
+        }
+        Ok(())
+    }
+}
+
+/// Runs the test component defined by `url` and reports `TestEvent` to sender for each test case.
+pub async fn run_test_component(
+    launcher: LauncherProxy,
+    url: String,
+    mut sender: mpsc::Sender<TestEvent>,
+) -> Result<(), failure::Error> {
+    let component_manager_for_test = "fuchsia-pkg://fuchsia.com/component_manager_for_test#\
+                                      meta/component_manager_for_test.cmx";
+
+    fx_vlog!(1, "connecting to test component {}", url);
+    let is_v2_component = url.ends_with(".cm");
+    let mut app;
+    if is_v2_component {
+        app = fuchsia_component::client::launch(
+            &launcher,
+            component_manager_for_test.to_string(),
+            Some(vec![url.clone()]),
+        )
+        .map_err(|e| format_err!("Not able to launch v2 test:{}: {}", url, e))?;
+    } else {
+        app = fuchsia_component::client::launch(&launcher, url.clone(), None)
+            .map_err(|e| format_err!("Not able to launch v1 test:{}: {}", url, e))?;
+    }
+
+    fx_vlog!(1, "connecting to test service");
+    let suite = app
+        .connect_to_service::<fidl_fuchsia_test::SuiteMarker>()
+        .map_err(|e| format_err!("Error connecting to test service: {}", e))?;
+    fx_vlog!(1, "enumerating tests");
+    let cases =
+        suite.get_tests().await.map_err(|e| format_err!("Error getting test steps: {}", e))?;
+    fx_vlog!(1, "got test list: {:#?}", cases);
+    let mut invocations = Vec::<Invocation>::new();
+    for case in cases {
+        invocations.push(Invocation { case: Some(case) });
+    }
+    let (run_listener_client, mut run_listener) =
+        fidl::endpoints::create_request_stream::<fidl_fuchsia_test::RunListenerMarker>()
+            .map_err(|e| format_err!("Error creating request stream: {}", e))?;
+    fx_vlog!(1, "running tests");
+    suite
+        .run(
+            &mut invocations.into_iter().map(|i| i.into()),
+            fidl_fuchsia_test::RunOptions {},
+            run_listener_client,
+        )
+        .map_err(|e| format_err!("Error running tests in '{}': {}", url, e))?;
+
+    let mut log_processors = Vec::<LogProcessor>::new();
+    while let Some(result_event) = run_listener
+        .try_next()
+        .await
+        .map_err(|e| format_err!("Error waiting for listener: {}", e))?
+    {
+        match result_event {
+            OnTestCaseStarted { name, primary_log, control_handle: _ } => {
+                sender.send(TestEvent::TestCaseStarted { test_case_name: name.clone() }).await?;
+
+                let mut log_processor = LogProcessor::new(name);
+                log_processor.collect_and_send_logs(primary_log, sender.clone());
+                log_processors.push(log_processor);
+            }
+            OnTestCaseFinished { name, outcome, control_handle: _ } => {
+                let outcome = match outcome.status {
+                    Some(status) => match status {
+                        fidl_fuchsia_test::Status::Passed => Outcome::Passed,
+                        fidl_fuchsia_test::Status::Failed => Outcome::Failed,
+                    },
+                    // This will happen when test protocol is not properly implemented
+                    // by the test and it forgets to set the outcome.
+                    None => Outcome::Error,
+                };
+                sender
+                    .send(TestEvent::TestCaseFinished { test_case_name: name, outcome: outcome })
+                    .await?;
+            }
+        }
+    }
+
+    join_all(log_processors.iter_mut().map(|l| l.await_logs()))
+        .await
+        .into_iter()
+        .fold(Ok(()), |acc, r| acc.and_then(|_| r))?;
+
+    app.kill().map_err(|e| format_err!("Error killing test '{}': {}", url, e))?;
+
+    Ok(())
+}
+
+/// The full test coverage of this library lives at //garnet/bin/sl4f/tests/test_framework
+/// They test that this library is able to handle various kind of tests launches and able to collect
+/// and pass back results.
+/// TODO(anmittal): move some of those tests here as unit tests.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn log_processor() {
+        let (sock_server, sock_client) =
+            zx::Socket::create(zx::SocketOpts::STREAM).expect("Failed while creating socket");
+
+        let name = "test_name";
+        let mut log_processor = LogProcessor::new(name.to_string());
+
+        let (sender, mut recv) = mpsc::channel(1);
+        log_processor.collect_and_send_logs(sock_client, sender);
+
+        sock_server.write(b"test message 1").expect("Can't write msg to socket");
+        sock_server.write(b"test message 2").expect("Can't write msg to socket");
+        sock_server.write(b"test message 3").expect("Can't write msg to socket");
+
+        let mut msg = recv.next().await;
+
+        assert_eq!(
+            msg,
+            Some(TestEvent::LogMessage {
+                test_case_name: name.to_string(),
+                msg: "test message 1test message 2test message 3".to_string()
+            })
+        );
+
+        // can receive messages multiple times
+        sock_server.write(b"test message 4").expect("Can't write msg to socket");
+        msg = recv.next().await;
+
+        assert_eq!(
+            msg,
+            Some(TestEvent::LogMessage {
+                test_case_name: name.to_string(),
+                msg: "test message 4".to_string()
+            })
+        );
+
+        // messages can be read after socket server is closed.
+        sock_server.write(b"test message 5").expect("Can't write msg to socket");
+        sock_server.into_handle(); // this will drop this handle and close it.
+        log_processor.await_logs().await.expect("await logs should not fail");
+
+        msg = recv.next().await;
+
+        assert_eq!(
+            msg,
+            Some(TestEvent::LogMessage {
+                test_case_name: name.to_string(),
+                msg: "test message 5".to_string()
+            })
+        );
+
+        // socket was closed, this should return None
+        msg = recv.next().await;
+        assert_eq!(msg, None);
+    }
+}
