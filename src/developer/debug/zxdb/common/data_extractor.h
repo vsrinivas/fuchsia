6e05604a49af2315752511db9b101a68ddae8773
Brett Wilson <brettw@google.com>
2019-10-17 19:05:35 +0000

    [debugger] Add generic data decoding helper.
    In a future patch we will need to be doing more manual DWARF decoding.
    This patch adds some infrastructure to help reading data from buffers.
    
    Change-Id: I2c619c24c17efba64c8239441853d6f6cc5f786f

---
+++ src/developer/debug/zxdb/common/data_extractor.h
@@ -0,0 +1,72 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_DEVELOPER_DEBUG_ZXDB_COMMON_DATA_EXTRACTOR_H_
+#define SRC_DEVELOPER_DEBUG_ZXDB_COMMON_DATA_EXTRACTOR_H_
+
+#include <string.h>
+
+#include <algorithm>
+#include <optional>
+
+#include "src/developer/debug/zxdb/common/array_view.h"
+
+namespace zxdb {
+
+class DataExtractor {
+ public:
+  DataExtractor() = default;
+  explicit DataExtractor(array_view<uint8_t> data) : data_(data) {}
+
+  // Returns the current position in the buffer.
+  size_t cur() const { return cur_; }
+
+  // Returns true is there is more data to read.
+  bool done() const { return cur_ >= data_.size(); }
+
+  // Reads the given value, returning it if there is room, and advancing the current location. If
+  // there is not enough bytes, the current position will remain unchanged and a nullopt will be
+  // returned.
+  //
+  // Normally one would read an explicitly sized value so the result doesn't depend on the current
+  // machine:
+  //
+  //   auto result = extractor.Read<uint32_t>();
+  template <typename T>
+  std::optional<T> Read() {
+    T result;
+    if (!ReadBytes(sizeof(T), &result))
+      return std::nullopt;
+    return result;
+  }
+
+  // Rturns true if there are at least the number of remaining bytes in the buffer.
+  bool CanRead(size_t bytes) const {
+    return bytes <= data_.size() &&  // Prevent overflow of subtraction below.
+           data_.size() - cur_ >= bytes;
+  }
+
+  // Advances the current location by the given number of bytes. If it advances past the end, it
+  // will stop there.
+  void Advance(size_t bytes) { cur_ = std::min(cur_ + bytes, data_.size()); }
+
+  // Copies the given number of bytes into the |dest| buffer and advances the current position.
+  // Returns true on success. False means there weren't enough bytes to read.
+  bool ReadBytes(size_t bytes, void* dest) {
+    if (!CanRead(bytes))
+      return false;
+
+    memcpy(dest, &data_[cur_], bytes);
+    cur_ += bytes;
+    return true;
+  }
+
+ private:
+  array_view<uint8_t> data_;
+  size_t cur_ = 0;  // Current index in data_.
+};
+
+}  // namespace zxdb
+
+#endif  // SRC_DEVELOPER_DEBUG_ZXDB_COMMON_DATA_EXTRACTOR_H_
---
+++ src/developer/debug/zxdb/common/data_extractor_unittest.cc
@@ -0,0 +1,80 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/developer/debug/zxdb/common/data_extractor.h"
+
+#include <limits>
+
+#include "gtest/gtest.h"
+
+namespace zxdb {
+
+TEST(DataExtractor, Numbers) {
+  // clang-format off
+  std::vector<uint8_t> buffer{
+      0x10,
+      0x20, 0x21,
+      0x30, 0x31, 0x32, 0x33,
+      0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47 };
+  // clang-format on
+
+  // Signed integers (little-endian).
+  DataExtractor ext(buffer);
+  EXPECT_FALSE(ext.done());
+  EXPECT_EQ(std::optional<int8_t>(0x10), ext.Read<int8_t>());
+  EXPECT_EQ(std::optional<int16_t>(0x2120), ext.Read<int16_t>());
+  EXPECT_EQ(std::optional<int32_t>(0x33323130), ext.Read<int32_t>());
+  EXPECT_EQ(std::optional<int64_t>(0x4746454443424140), ext.Read<int64_t>());
+  EXPECT_EQ(std::optional<int8_t>(), ext.Read<int8_t>());  // Read past end fails.
+  EXPECT_TRUE(ext.done());
+
+  // Unsigned integers (little-endian).
+  ext = DataExtractor(buffer);
+  EXPECT_EQ(std::optional<uint8_t>(0x10), ext.Read<uint8_t>());
+  EXPECT_EQ(std::optional<uint16_t>(0x2120), ext.Read<uint16_t>());
+  EXPECT_EQ(std::optional<uint32_t>(0x33323130), ext.Read<uint32_t>());
+  EXPECT_EQ(std::optional<uint64_t>(0x4746454443424140), ext.Read<uint64_t>());
+
+  // Reading past the end sould leave the position unchanged (buffer is 15 bytes long).
+  ext = DataExtractor(buffer);
+  ext.Read<uint64_t>();
+  EXPECT_EQ(8u, ext.cur());
+  EXPECT_FALSE(ext.Read<uint64_t>());  // Read fails.
+  EXPECT_EQ(8u, ext.cur());  // Position is unchanged.
+}
+
+TEST(DataExtractor, Manual) {
+  std::vector<uint8_t> buffer{0x01, 0x02, 0x03, 0x04};
+
+  DataExtractor ext(buffer);
+  EXPECT_EQ(0u, ext.cur());
+  EXPECT_TRUE(ext.CanRead(1));
+  EXPECT_TRUE(ext.CanRead(4));
+  EXPECT_FALSE(ext.CanRead(5));
+
+  EXPECT_EQ(0u, ext.cur());
+  ext.Advance(2);
+  EXPECT_EQ(2u, ext.cur());
+  ext.Advance(0);
+  EXPECT_EQ(2u, ext.cur());
+  EXPECT_TRUE(ext.CanRead(2));
+  EXPECT_FALSE(ext.CanRead(3));
+
+  // Advances one-past-the end. Should stop at the end.
+  ext.Advance(3);
+  EXPECT_EQ(4u, ext.cur());
+  EXPECT_TRUE(ext.CanRead(0));
+  EXPECT_FALSE(ext.CanRead(1));
+
+  // Test overflow of size variables. In this case size + read_size overflows.
+  size_t big_read = std::numeric_limits<size_t>::max() - 1;
+  ext = DataExtractor(buffer);
+  EXPECT_FALSE(ext.CanRead(big_read));
+
+  // Here cur + read_size overflows.
+  ext.Advance(2);
+  EXPECT_FALSE(ext.CanRead(big_read));
+}
+
+}  // namespace zxdb
