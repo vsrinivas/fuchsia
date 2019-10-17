a91905366fe9acbf17b0eb4d0eefa1e634cd8112
Dragoș Tiselice <dtiselice@google.com>
2019-10-17 18:16:17 +0000

    [carnelian] Temporarily removed tracing.
    It seems like tracing made one of the tests flaky. This patch
    temporarily removes tracing in Carnelian.
    
    flaky test: carnelian_lib_test exited with nonzero status: -1028
    
    Change-Id: I5286d4af254a8ab3d52465349c0973f2af7baa93

--- garnet/public/rust/carnelian/src/app.rs
+++ garnet/public/rust/carnelian/src/app.rs
@@ -17,7 +17,6 @@ use fuchsia_async::{self as fasync, DurationExt, Timer};
 use fuchsia_component::{self as component, client::connect_to_service};
 use fuchsia_framebuffer::{Frame, FrameBuffer, VSyncMessage};
 use fuchsia_scenic::{Session, SessionPtr, ViewTokenPair};
-use fuchsia_trace_provider;
 use fuchsia_zircon::{self as zx, DurationNum};
 use futures::{
     channel::mpsc::unbounded,
@@ -295,7 +294,6 @@ impl App {
         app.set_assistant(assistant);
         App::start_services(app)?;
         app.assistant.as_mut().unwrap().setup().context("app setup")?;
-        fuchsia_trace_provider::trace_provider_create_with_fdio();
         Ok(supports_scenic)
     }
 
