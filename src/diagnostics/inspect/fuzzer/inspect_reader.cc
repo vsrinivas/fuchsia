91b199009f5abf44ba11b2d226edc725d5ed9f17
Christopher R. Johnson <crjohns@google.com>
2019-10-18 17:34:05 +0000

    [inspect] Create a fuzzer for inspect reading.
    Change-Id: Id6207c91ba957cbec7f646a87560a5711f3185ae

---
+++ src/diagnostics/inspect/fuzzer/inspect_reader.cc
@@ -0,0 +1,19 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/inspect/cpp/reader.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <vector>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  std::vector<uint8_t> buf;
+  buf.resize(size);
+  memcpy(buf.data(), data, size);
+
+  inspect::ReadFromBuffer(std::move(buf));
+
+  return 0;
+}
