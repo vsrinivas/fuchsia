3306b0bd062c4be3a51042f813b3df7ce48f9e8b
Jeff Seibert <jseibert@google.com>
2019-10-18 19:21:11 +0000

    [bootfs] Fuzzer for bootfs parser.
    I figured something with 'parser' in the name would be a good target for
    fuzzing, so I wrote one. On a local machine it has not found anything
    yet.
    
    Change-Id: Id996aa4a49a4f9d918e2656c9ba03972607215ac

---
+++ zircon/system/ulib/bootfs/parser-fuzzer.cc
@@ -0,0 +1,21 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/bootfs/parser.h>
+#include <lib/zx/vmo.h>
+#include <zircon/boot/bootfs.h>
+
+namespace {
+
+extern "C" int LLVMFuzzerTestOneInput(void* Data, size_t Size) {
+  zx::vmo vmo;
+  zx::vmo::create(Size, 0, &vmo);
+  vmo.write(Data, 0, Size);
+  bootfs::Parser parser;
+  parser.Init(zx::unowned_vmo(vmo));
+  parser.Parse([](const zbi_bootfs_dirent_t* entry) { return ZX_OK; });
+  return 0;
+}
+
+}  // namespace
