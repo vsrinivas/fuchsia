13c120b49195020de3e4f65232056a9b3b0ab04e
David Worsham <dworsham@google.com>
2019-10-18 06:23:08 +0000

    Revert "[scenic][present_view] Use libsys; add tests"
    This reverts commit c562baf6e1c435fdddb0243a01775915190e21e3.
    
    Reason for revert: Breaking CI tests
    
    Original change's description:
    > [scenic][present_view] Use libsys; add tests
    > 
    > Extracted a "PresentView" class out of the monolithic main() function,
    > in order to facilitate writing unit tests.
    > 
    > Bug: 24598
    > Change-Id: Iaae600c2db153f4e918c793f2d55ee973f1eb473
    
    TBR=qsr@google.com,jjosh@google.com,jaeheon@google.com,dworsham@google.com,amott@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 24598
    Change-Id: Id48eddeb3d04e0466af39978d82c755e8324db6b

--- garnet/bin/ui/present_view/main.cc
+++ garnet/bin/ui/present_view/main.cc
@@ -2,48 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fuchsia/ui/app/cpp/fidl.h>
+#include <fuchsia/ui/policy/cpp/fidl.h>
+#include <fuchsia/ui/views/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
-#include <lib/sys/cpp/component_context.h>
-#include <lib/trace-provider/provider.h>
+#include <lib/async-loop/default.h>
+#include <lib/svc/cpp/services.h>
+#include <lib/ui/scenic/cpp/view_token_pair.h>
 
-#include "garnet/bin/ui/present_view/present_view.h"
-#include "src/lib/fxl/command_line.h"
-#include "src/lib/fxl/log_settings_command_line.h"
-#include "src/lib/fxl/logging.h"
+#include <src/lib/fxl/command_line.h>
+#include <src/lib/fxl/log_settings_command_line.h>
+#include <src/lib/fxl/logging.h>
+
+#include "src/lib/component/cpp/startup_context.h"
+
+using scenic::ViewTokenPair;
 
 namespace {
 
 constexpr char kKeyLocale[] = "locale";
 
-present_view::ViewInfo ParseCommandLine(const fxl::CommandLine& command_line) {
-  present_view::ViewInfo view_info;
-
-  const auto& positional_args = command_line.positional_args();
-  if (positional_args.empty()) {
-    return {};
-  }
-
-  view_info.url = positional_args[0];
-  for (size_t i = 1; i < positional_args.size(); ++i) {
-    view_info.arguments.push_back(positional_args[i]);
-  }
-  if (command_line.HasOption(kKeyLocale)) {
-    command_line.GetOptionValue(kKeyLocale, &view_info.locale);
-  }
-
-  return view_info;
-}
-
 }  // namespace
 
 int main(int argc, const char** argv) {
-  async::Loop loop(&kAsyncLoopConfigAttachToThread);
-  trace::TraceProviderWithFdio trace_provider(loop.dispatcher());
-
   auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
-  if (!fxl::SetLogSettingsFromCommandLine(command_line)) {
+  if (!fxl::SetLogSettingsFromCommandLine(command_line))
     return 1;
-  }
 
   FXL_LOG(WARNING) << "BE ADVISED: The present_view tool takes the URL to an "
                       "app that provided the ViewProvider interface and makes "
@@ -56,21 +40,56 @@ int main(int argc, const char** argv) {
   FXL_LOG(WARNING) << "If scenic is already running on your system you "
                       "will probably want to kill it before invoking this tool.";
 
-  present_view::ViewInfo view_info = ParseCommandLine(command_line);
-  present_view::PresentView present_view(sys::ComponentContext::Create());
-
-  int retval = 0;
-  bool present_success =
-      present_view.Present(std::move(view_info), [&loop, &retval](zx_status_t status) {
-        FXL_LOG(INFO) << "Launched component terminated; status: " << status;
-        retval = 1;
-        loop.Quit();
-      });
-  if (!present_success) {
+  const auto& positional_args = command_line.positional_args();
+  if (positional_args.empty()) {
+    FXL_LOG(ERROR) << "present_view requires the url of a view provider application "
+                      "to present_view.";
     return 1;
   }
 
-  loop.Run();
+  async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
+  auto startup_context_ = component::StartupContext::CreateFromStartupInfo();
 
-  return retval;
+  // Launch application.
+  component::Services services;
+  fuchsia::sys::LaunchInfo launch_info;
+  launch_info.url = positional_args[0];
+  launch_info.arguments = std::vector(++positional_args.begin(), positional_args.end());
+  launch_info.directory_request = services.NewRequest();
+  fuchsia::sys::ComponentControllerPtr controller;
+  startup_context_->launcher()->CreateComponent(std::move(launch_info), controller.NewRequest());
+  controller.set_error_handler([&loop](zx_status_t status) {
+    FXL_LOG(INFO) << "Launched application terminated.";
+    loop.Quit();
+  });
+
+  auto [view_token, view_holder_token] = scenic::ViewTokenPair::New();
+
+  // Note: This instance must be retained for the lifetime of the UI, so it has
+  // to be declared in the outer scope of |main| rather than inside the relevant
+  // |if| branch.
+  fidl::InterfacePtr<fuchsia::ui::views::View> view;
+  // For now, use the presence of a locale option as an indication to use the
+  // |fuchsia::ui::view::View| interface.
+  if (command_line.HasOption(kKeyLocale)) {
+    std::string locale_str;
+    command_line.GetOptionValue(kKeyLocale, &locale_str);
+    // Create a view using the |fuchsia::ui::views::View| interface.
+    services.ConnectToService<fuchsia::ui::views::View>(view.NewRequest());
+    view->Present(std::move(view_token));
+    // TODO(I18N-13): Provide fuchsia.intl.PropertyProvider instance.
+  } else {
+    // Create the view using the |fuchsia::ui::app::ViewProvider| interface.
+    fidl::InterfacePtr<::fuchsia::ui::app::ViewProvider> view_provider;
+    services.ConnectToService(view_provider.NewRequest());
+    view_provider->CreateView(std::move(view_token.value), nullptr, nullptr);
+  }
+
+  // Ask the presenter to display it.
+  auto presenter = startup_context_->ConnectToEnvironmentService<fuchsia::ui::policy::Presenter>();
+  presenter->PresentView(std::move(view_holder_token), nullptr);
+
+  // Done!
+  loop.Run();
+  return 0;
 }
