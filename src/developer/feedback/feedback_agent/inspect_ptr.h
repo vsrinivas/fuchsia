86598ab4296b184a2f16efc293905a786a795cf6
Francois Rousseau <frousseau@google.com>
2019-10-21 23:49:22 +0000

    [feedback] update CollectInspectData function comment
    * we no longer need "shell", just "hub" to access the hub
    
    Change-Id: Ib1e83078812958e2354a036a244656a93e91fc01

--- src/developer/feedback/feedback_agent/inspect_ptr.h
+++ src/developer/feedback/feedback_agent/inspect_ptr.h
@@ -14,7 +14,7 @@ namespace feedback {
 
 // Collects the Inspect data.
 //
-// Requires "shell" in the features of the calling component's sandbox to access the hub.
+// Requires "hub" in the features of the calling component's sandbox to access the hub.
 fit::promise<fuchsia::mem::Buffer> CollectInspectData(async_dispatcher_t* dispatcher,
                                                       zx::duration timeout);
 
