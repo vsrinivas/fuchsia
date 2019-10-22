49300566c2a8396f6031f5fa94a2b2f1c17ba5d6
Doug Evans <dje@google.com>
2019-10-22 23:34:35 +0000

    [trace_manager] Rename trace_registry_bindings_ -> provider_registry_bindings_
    ... and trace_controller_bindings_ -> controller_bindings. They read better.
    
    Change-Id: I4e2e7398a6eb236773bd086293a80e4d739f5ec3

--- garnet/bin/trace_manager/app.cc
+++ garnet/bin/trace_manager/app.cc
@@ -15,11 +15,11 @@ TraceManagerApp::TraceManagerApp(const Config& config)
   [[maybe_unused]] zx_status_t status;
 
   status =
-      context_->outgoing()->AddPublicService(trace_registry_bindings_.GetHandler(&trace_manager_));
+      context_->outgoing()->AddPublicService(provider_registry_bindings_.GetHandler(&trace_manager_));
   FXL_DCHECK(status == ZX_OK);
 
-  status = context_->outgoing()->AddPublicService(
-      trace_controller_bindings_.GetHandler(&trace_manager_));
+  status =
+      context_->outgoing()->AddPublicService(controller_bindings_.GetHandler(&trace_manager_));
   FXL_DCHECK(status == ZX_OK);
 
   FXL_VLOG(2) << "TraceManager services registered";
