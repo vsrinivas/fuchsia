63cbfd1e6649f79df283008efb1e01036042e012
Drew Fisher <zarvox@google.com>
2019-10-22 04:43:07 +0000

    [ldmsg] Add fuzzer
    This adds a simple fuzzer for the ldmsg encoder/decoder.
    
    Change-Id: I6d08a70ebaf2459b1a93cd182c335c9fb621f0e0

---
+++ zircon/system/ulib/ldmsg/test/ldmsg-fuzzer.cc
@@ -0,0 +1,35 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <ldmsg/ldmsg.h>
+#include <fuzzer/FuzzedDataProvider.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider fuzzed_data(data, size);
+  uint64_t ordinal = fuzzed_data.PickValueInArray({
+    LDMSG_OP_DONE,
+    LDMSG_OP_DONE_GEN,
+    LDMSG_OP_CLONE,
+    LDMSG_OP_CLONE_GEN,
+    LDMSG_OP_LOAD_OBJECT,
+    LDMSG_OP_CONFIG,
+    LDMSG_OP_LOAD_OBJECT_GEN,
+    LDMSG_OP_CONFIG_GEN,
+  });
+
+  size_t req_len;
+  ldmsg_req_t req;
+  req.header.ordinal = ordinal;
+  auto remaining_data = fuzzed_data.ConsumeRemainingBytes<uint8_t>();
+  zx_status_t status = ldmsg_req_encode(&req, &req_len, (const char*)remaining_data.data(),
+                                        remaining_data.size());
+  if (status != ZX_OK) {
+    return 0;
+  }
+
+  const char* out;
+  size_t len_out;
+  status = ldmsg_req_decode(&req, req_len, &out, &len_out);
+  return 0;
+}
