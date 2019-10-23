314dcaf1754edff3557d5eb406f29f2cc35d4afc
Benjamin Wright <benwright@google.com>
2019-10-23 23:11:35 +0000

    [cmdline][fuzzit] - Add a simple cmdline fuzzer.
    Change-Id: Ia6940323d143258144907605b3a3a26da46db152

---
+++ zircon/system/ulib/cmdline/test/cmdline_fuzzer.cc
@@ -0,0 +1,24 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+#include <vector>
+
+#include <cmdline/args_parser.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size < 2) {
+    return 0;
+  }
+
+  std::vector<std::string> params;
+  std::string arg0(data, data + size / 2);
+  std::string arg1(data + size / 2, data + size);
+
+  const char* argv[2] = {arg0.c_str(), arg1.c_str()};
+  cmdline::GeneralArgsParser parser;
+  parser.ParseGeneral(2, argv, &params);
+
+  return 0;
+}
