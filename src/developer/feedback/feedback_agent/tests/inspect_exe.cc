a52e4dd3cdd8df43773183471231b060a8428c4b
Francois Rousseau <frousseau@google.com>
2019-10-24 22:58:38 +0000

    [feedback] migrate test inspect_exe.cc to new Inspect
    TESTED=`fx run-test feedback_agent_tests -t data_provider_unittest -- --gtest_filter=DataProviderTestWithEnv.GetData_Inspect`
    
    Change-Id: Id61c198f3670e96791bc3320d03f5e637b8fd742

--- src/developer/feedback/feedback_agent/tests/inspect_exe.cc
+++ src/developer/feedback/feedback_agent/tests/inspect_exe.cc
@@ -4,36 +4,36 @@
 
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
+#include <lib/inspect/cpp/vmo/types.h>
 #include <lib/sys/cpp/component_context.h>
+#include <lib/sys/inspect/cpp/component.h>
 
 #include <cstdint>
-
-#include <fs/vmo_file.h>
-
-#include "src/lib/inspect_deprecated/component.h"
+#include <cstdlib>
 
 class Object {
  public:
-  Object(inspect_deprecated::Node node, uint64_t value) : node_(std::move(node)) {
-    version_ = node_.CreateStringProperty("version", "1.0");
-    metric_ = node_.CreateIntMetric("value", value);
+  Object(inspect::Node node, uint64_t value) : node_(std::move(node)) {
+    version_ = node_.CreateString("version", "1.0");
+    value_ = node_.CreateInt("value", value);
   }
 
  private:
-  inspect_deprecated::Node node_;
-  inspect_deprecated::StringProperty version_;
-  inspect_deprecated::IntMetric metric_;
+  inspect::Node node_;
+  inspect::StringProperty version_;
+  inspect::IntProperty value_;
 };
 
 int main(int argc, const char** argv) {
   async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
   auto context = sys::ComponentContext::Create();
-  auto inspector = inspect_deprecated::ComponentInspector::Initialize(context.get());
-  auto& root = inspector->root_tree()->GetRoot();
+  auto inspector = std::make_unique<sys::ComponentInspector>(context.get());
+  auto& root = inspector->root();
 
   Object o1(root.CreateChild("obj1"), 100);
   Object o2(root.CreateChild("obj2"), 200);
 
   loop.Run();
-  return 0;
+
+  return EXIT_SUCCESS;
 }
