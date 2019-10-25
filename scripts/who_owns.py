512d3eb210395e90765deef543e3fd3151d6c8a3
Shai Barack <shayba@google.com>
2019-10-25 15:16:14 +0000

    [scripts] who_owns.py
    Does exactly what you think it does.
    Automatically figure out for instance who you might need to send an
    announcement to for a medium-scale change so you could be cool like me.
    
    $ scripts/who_owns.py scripts/who_owns.py
    pylaligand@google.com,raggi@google.com
    
    $ scripts/who_owns.py scripts/who_owns.py src/sys/component_manager/src/main.rs
    adamlesinski@google.com,bryanhenry@google.com,dgonyeo@google.com,fsamuel@google.com,geb@google.com,jeffbrown@google.com,pylaligand@google.com,raggi@google.com,shayba@google.com
    
    Change-Id: I431568b5b4367caa8e36b831f3d02dda3273d9dd

---
+++ scripts/who_owns.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2.7
+# Copyright 2019 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import os
+import re
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+FUCHSIA_ROOT = os.path.dirname(  # $root
+    os.path.dirname(  # scripts
+        SCRIPT_DIR))  # unification
+
+# TODO this assumes all OWNERS are @google.com
+owner_exp = re.compile('^\s*([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)')
+perfile_exp = re.compile(
+    '^\s*per-file ([^\s=]*)=\s*([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.'
+    '[a-zA-Z0-9-.]+)')
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Finds all OWNERS of given paths')
+    parser.add_argument('paths', nargs='+')
+    args = parser.parse_args()
+    abspaths = [os.path.abspath(path) for path in args.paths]
+
+    # Find all OWNERS files
+    owners_paths = set()
+    for path in abspaths:
+        dir = path if os.path.isdir(path) else os.path.dirname(path)
+        dir = os.path.abspath(dir)
+        while (os.path.exists(dir) and
+               os.path.commonprefix([dir, FUCHSIA_ROOT]) == FUCHSIA_ROOT):
+            owners_path = os.path.join(dir, 'OWNERS')
+            if os.path.exists(owners_path):
+                owners_paths.add(owners_path)
+                break
+            dir = os.path.dirname(dir)
+
+    # Parse all OWNERS files
+    owners = set()
+    for path in owners_paths:
+        with open(path) as f:
+            for line in f.readlines():
+                match = owner_exp.match(line)
+                if match:
+                    owners.add(match.group(1))
+                    continue
+                match = perfile_exp.match(line)
+                if match:
+                    filename = os.path.abspath(
+                        os.path.join(os.path.dirname(path), match.group(1)))
+                    if filename in abspaths:
+                        owners.add(match.group(2))
+
+    print ','.join(sorted(owners))
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
