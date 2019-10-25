94b9223a7f3811b4f045eed15d451cc860f8026a
Marty Faltesek <mfaltesek@google.com>
2019-10-25 21:21:24 +0000

    [zircon][core-test] Migrate c11-mutex to zxtest.
    Test: runtests -t c11-mutex-test -r 100
    
    Change-Id: Ie731ec8e43358093ab47a258f7bda70989d8de69

--- zircon/system/utest/core/c11-mutex/mutex.cc
+++ zircon/system/utest/core/c11-mutex/mutex.cc
@@ -2,228 +2,239 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <inttypes.h>
 #include <lib/zircon-internal/thread_annotations.h>
+#include <lib/zx/clock.h>
+#include <lib/zx/event.h>
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <threads.h>
 #include <zircon/syscalls.h>
 #include <zircon/time.h>
 #include <zircon/types.h>
 
-#include <unittest/unittest.h>
+#include <array>
+#include <cinttypes>
+#include <cstdio>
+#include <cstdlib>
 
-static mtx_t g_mutex = MTX_INIT;
+#include <fbl/auto_call.h>
+#include <zxtest/zxtest.h>
 
-static void xlog(const char* str) {
-  zx_time_t now = 0;
-  zx_clock_get(ZX_CLOCK_UTC, &now);
-  unittest_printf("[%08" PRIu64 ".%08" PRIu64 "]: %s", now / 1000000000, now % 1000000000, str);
-}
+namespace {
 
-static int mutex_thread_1(void* arg) {
-  xlog("thread 1 started\n");
+constexpr int* kNoReturnValue = nullptr;
 
-  for (int times = 0; times < 300; times++) {
-    mtx_lock(&g_mutex);
-    zx_nanosleep(zx_deadline_after(ZX_USEC(1)));
-    mtx_unlock(&g_mutex);
+class TestThread {
+ public:
+  TestThread(size_t number_tries, zx::duration delay, mtx_t* lock)
+      : number_tries_(number_tries), delay_(delay), lock_(lock) {
+    ZX_ASSERT(lock != nullptr);
   }
 
-  xlog("thread 1 done\n");
-  return 0;
-}
+  void Start() {
+    int status = thrd_create(
+        &tid_,
+        [](void* ctx) -> int {
+          return reinterpret_cast<TestThread*>(ctx)->RepeatMutexLockAndUnlock();
+        },
+        this);
 
-static int mutex_thread_2(void* arg) {
-  xlog("thread 2 started\n");
-
-  for (int times = 0; times < 150; times++) {
-    mtx_lock(&g_mutex);
-    zx_nanosleep(zx_deadline_after(ZX_USEC(2)));
-    mtx_unlock(&g_mutex);
+    started_ = status == thrd_success;
+    ASSERT_TRUE(started_);
   }
 
-  xlog("thread 2 done\n");
-  return 0;
-}
-
-static int mutex_thread_3(void* arg) {
-  xlog("thread 3 started\n");
-
-  for (int times = 0; times < 100; times++) {
-    mtx_lock(&g_mutex);
-    zx_nanosleep(zx_deadline_after(ZX_USEC(3)));
-    mtx_unlock(&g_mutex);
-  }
-
-  xlog("thread 3 done\n");
-  return 0;
-}
-
-static bool got_lock_1 = false;
-static bool got_lock_2 = false;
-static bool got_lock_3 = false;
-
-// These tests all conditionally acquire the lock, by design. The
-// thread safety analysis is not up to this, so disable it.
-static int mutex_try_thread_1(void* arg) TA_NO_THREAD_SAFETY_ANALYSIS {
-  xlog("thread 1 started\n");
-
-  for (int times = 0; times < 300 || !got_lock_1; times++) {
-    int status = mtx_trylock(&g_mutex);
-    zx_nanosleep(zx_deadline_after(ZX_USEC(1)));
-    if (status == thrd_success) {
-      got_lock_1 = true;
-      mtx_unlock(&g_mutex);
+  void Join() {
+    if (started_) {
+      thrd_join(tid_, kNoReturnValue);
+      started_ = false;
     }
   }
 
-  xlog("thread 1 done\n");
-  return 0;
-}
-
-static int mutex_try_thread_2(void* arg) TA_NO_THREAD_SAFETY_ANALYSIS {
-  xlog("thread 2 started\n");
-
-  for (int times = 0; times < 150 || !got_lock_2; times++) {
-    int status = mtx_trylock(&g_mutex);
-    zx_nanosleep(zx_deadline_after(ZX_USEC(2)));
-    if (status == thrd_success) {
-      got_lock_2 = true;
-      mtx_unlock(&g_mutex);
+  int RepeatMutexLockAndUnlock() {
+    for (uint64_t tries = 0; tries < number_tries_; tries++) {
+      mtx_lock(lock_);
+      zx::nanosleep(zx::deadline_after(delay_));
+      mtx_unlock(lock_);
     }
+    return 0;
   }
 
-  xlog("thread 2 done\n");
-  return 0;
-}
+ private:
+  uint64_t number_tries_;
+  zx::duration delay_;
+  mtx_t* lock_;
+  thrd_t tid_;
+  bool started_ = false;
+};
 
-static int mutex_try_thread_3(void* arg) TA_NO_THREAD_SAFETY_ANALYSIS {
-  xlog("thread 3 started\n");
+TEST(C11MutexTest, MultiThreadedContention) {
+  // These tests all conditionally acquire the lock, by design. The
+  // thread safety analysis is not up to this, so disable it.
+  mtx_t lock;
+  ASSERT_EQ(thrd_success, mtx_init(&lock, mtx_timed));
 
-  for (int times = 0; times < 100 || !got_lock_3; times++) {
-    int status = mtx_trylock(&g_mutex);
-    zx_nanosleep(zx_deadline_after(ZX_USEC(3)));
-    if (status == thrd_success) {
-      got_lock_3 = true;
-      mtx_unlock(&g_mutex);
-    }
+  std::array threads = {
+      TestThread{300, zx::usec(100), &lock},
+      TestThread{150, zx::usec(200), &lock},
+      TestThread{100, zx::usec(300), &lock},
+  };
+
+  for (auto& thread : threads) {
+    thread.Start();
+    ASSERT_NO_FAILURES();
   }
-
-  xlog("thread 3 done\n");
-  return 0;
+  for (auto& thread : threads) {
+    thread.Join();
+    ASSERT_NO_FAILURES();
+  }
 }
 
-static bool test_initializer(void) {
-  BEGIN_TEST;
+// TODO(39408) Remove all TA_NO_THREAD_SAFETY_ANALYSIS annotations when we can
+TEST(C11MutexTest, TryMutexMultiThreadedContention) TA_NO_THREAD_SAFETY_ANALYSIS {
+  struct MutexThreadArgs {
+    std::atomic<int> lock_acquired;
+    std::atomic<int> lock_released = thrd_error;
+    mtx_t* lock;
+  } args;
 
-  int ret = mtx_init(&g_mutex, mtx_timed);
-  ASSERT_EQ(ret, thrd_success, "failed to initialize mtx_t");
+  // This test conditionally acquires the lock, by design. The
+  // thread safety analysis is not up to this, so disable it.
+  auto TryGrabLock = [](void* arg) TA_NO_THREAD_SAFETY_ANALYSIS -> int {
+    MutexThreadArgs* args = static_cast<MutexThreadArgs*>(arg);
 
-  END_TEST;
+    args->lock_acquired = mtx_trylock(args->lock);
+
+    if (args->lock_acquired == thrd_success) {
+      args->lock_released = mtx_unlock(args->lock);
+    }
+
+    return 0;
+  };
+
+  mtx_t lock;
+  bool lock_held = false;
+  ASSERT_EQ(thrd_success, mtx_init(&lock, mtx_plain));
+  args.lock = &lock;
+  auto cleanup_mutex = fbl::MakeAutoCall([&]() TA_NO_THREAD_SAFETY_ANALYSIS {
+    if (lock_held) {
+      mtx_unlock(&lock);
+    }
+    mtx_destroy(&lock);
+  });
+
+  thrd_t thread_id;
+
+  ASSERT_EQ(thrd_success, mtx_lock(&lock));
+  lock_held = true;
+  ASSERT_EQ(thrd_success, thrd_create(&thread_id, TryGrabLock, &args));
+  ASSERT_EQ(thrd_success, thrd_join(thread_id, kNoReturnValue));
+
+  ASSERT_EQ(thrd_success, mtx_unlock(&lock));
+  lock_held = false;
+
+  ASSERT_EQ(thrd_busy, args.lock_acquired);
+
+  args.lock_acquired = thrd_error;
+  args.lock_released = thrd_error;
+  ASSERT_EQ(thrd_success, thrd_create(&thread_id, TryGrabLock, &args));
+  ASSERT_EQ(thrd_success, thrd_join(thread_id, kNoReturnValue));
+  ASSERT_EQ(thrd_success, args.lock_acquired);
+  ASSERT_EQ(thrd_success, args.lock_released);
 }
 
-static bool test_mutexes(void) {
-  BEGIN_TEST;
-  thrd_t thread1, thread2, thread3;
-
-  thrd_create_with_name(&thread1, mutex_thread_1, NULL, "thread 1");
-  thrd_create_with_name(&thread2, mutex_thread_2, NULL, "thread 2");
-  thrd_create_with_name(&thread3, mutex_thread_3, NULL, "thread 3");
-
-  thrd_join(thread1, NULL);
-  thrd_join(thread2, NULL);
-  thrd_join(thread3, NULL);
-
-  END_TEST;
+TEST(C11MutexTest, InitalizeLocalMutex) {
+  mtx_t auto_mutex;
+  ASSERT_EQ(thrd_success, mtx_init(&auto_mutex, mtx_timed));
+  mtx_destroy(&auto_mutex);
 }
 
-static bool test_try_mutexes(void) {
-  BEGIN_TEST;
-  thrd_t thread1, thread2, thread3;
-
-  thrd_create_with_name(&thread1, mutex_try_thread_1, NULL, "thread 1");
-  thrd_create_with_name(&thread2, mutex_try_thread_2, NULL, "thread 2");
-  thrd_create_with_name(&thread3, mutex_try_thread_3, NULL, "thread 3");
-
-  thrd_join(thread1, NULL);
-  thrd_join(thread2, NULL);
-  thrd_join(thread3, NULL);
-
-  EXPECT_TRUE(got_lock_1, "failed to get lock 1");
-  EXPECT_TRUE(got_lock_2, "failed to get lock 2");
-  EXPECT_TRUE(got_lock_3, "failed to get lock 3");
-
-  END_TEST;
-}
-
-static bool test_static_initializer(void) {
-  BEGIN_TEST;
-
+TEST(C11MutexTest, StaticInitalizerSameBytesAsAuto) {
   static mtx_t static_mutex = MTX_INIT;
   mtx_t auto_mutex;
   memset(&auto_mutex, 0xae, sizeof(auto_mutex));
   mtx_init(&auto_mutex, mtx_plain);
+  auto cleanup_mutex = fbl::MakeAutoCall([&]() { mtx_destroy(&auto_mutex); });
 
-  EXPECT_BYTES_EQ((const uint8_t*)&static_mutex, (const uint8_t*)&auto_mutex, sizeof(mtx_t),
+  EXPECT_BYTES_EQ(reinterpret_cast<uint8_t*>(&static_mutex),
+                  reinterpret_cast<uint8_t*>(&auto_mutex), sizeof(mtx_t),
                   "MTX_INIT and mtx_init differ!");
-
-  END_TEST;
 }
 
-typedef struct {
-  mtx_t mutex;
-  zx_handle_t start_event;
-  zx_handle_t done_event;
-} timeout_args;
+TEST(C11MutexTest, TimeoutElapsed) {
+  struct ThreadTimeoutArgs {
+    mtx_t lock;
+    zx::event start_event;
+    zx::event done_event;
+    std::atomic<bool> mtx_lock_result = false;
+    std::atomic<bool> mtx_unlock_result = false;
+    std::atomic<zx_status_t> signal_result = ZX_ERR_INTERNAL;
+    std::atomic<zx_status_t> wait_one_result = ZX_ERR_INTERNAL;
+  };
 
-static int test_timeout_helper(void* ctx) TA_NO_THREAD_SAFETY_ANALYSIS {
-  timeout_args* args = static_cast<timeout_args*>(ctx);
-  ASSERT_EQ(mtx_lock(&args->mutex), thrd_success, "f to lock");
-  // Inform the main thread that we have acquired the lock.
-  ASSERT_EQ(zx_object_signal(args->start_event, 0, ZX_EVENT_SIGNALED), ZX_OK, "failed to signal");
-  // Wait until the main thread has completed its test.
-  ASSERT_EQ(zx_object_wait_one(args->done_event, ZX_EVENT_SIGNALED, ZX_TIME_INFINITE, NULL), ZX_OK,
-            "failed to wait");
-  ASSERT_EQ(mtx_unlock(&args->mutex), thrd_success, "failed to unlock");
-  return 0;
-}
+  auto TestTimeoutHelper = [](void* ctx) TA_NO_THREAD_SAFETY_ANALYSIS -> int {
+    ThreadTimeoutArgs* args = static_cast<ThreadTimeoutArgs*>(ctx);
 
-static bool test_timeout_elapsed(void) {
-  BEGIN_TEST;
+    if ((args->mtx_lock_result = mtx_lock(&args->lock)) != thrd_success) {
+      return -1;
+    }
 
-  const zx_duration_t kRelativeDeadline = ZX_MSEC(100);
+    // Inform the main thread that we have acquired the lock.
+    if ((args->signal_result = args->start_event.signal(0, ZX_EVENT_SIGNALED)) != ZX_OK) {
+      mtx_unlock(&args->lock);
+      return -1;
+    }
 
-  timeout_args args;
-  ASSERT_EQ(thrd_success, mtx_init(&args.mutex, mtx_plain), "could not create mutex");
-  ASSERT_EQ(zx_event_create(0, &args.start_event), ZX_OK, "could not create event");
-  ASSERT_EQ(zx_event_create(0, &args.done_event), ZX_OK, "could not create event");
+    // Wait until the main thread has completed its test.
+    if ((args->wait_one_result = args->done_event.wait_one(ZX_EVENT_SIGNALED, zx::time::infinite(),
+                                                           nullptr)) != ZX_OK) {
+      mtx_unlock(&args->lock);
+      return -1;
+    }
+
+    if ((args->mtx_unlock_result = mtx_unlock(&args->lock)) != thrd_success) {
+      return -1;
+    }
+    return 0;
+  };
+
+  constexpr zx::duration kRelativeDeadline = zx::msec(100);
+
+  ThreadTimeoutArgs args;
+  ASSERT_EQ(thrd_success, mtx_init(&args.lock, mtx_plain));
+  auto cleanup_mutex = fbl::MakeAutoCall([&]() { mtx_destroy(&args.lock); });
+  ASSERT_OK(zx::event::create(0, &args.start_event));
+  ASSERT_OK(zx::event::create(0, &args.done_event));
 
   thrd_t helper;
-  ASSERT_EQ(thrd_create(&helper, test_timeout_helper, &args), thrd_success, "");
+  ASSERT_EQ(thrd_create(&helper, TestTimeoutHelper, &args), thrd_success);
+
+  cleanup_mutex.cancel();
+  auto cleanup_thread = fbl::MakeAutoCall([&]() {
+    args.done_event.signal(0, ZX_EVENT_SIGNALED);
+    thrd_join(helper, kNoReturnValue);
+    mtx_destroy(&args.lock);
+  });
+
   // Wait for the helper thread to acquire the lock.
-  ASSERT_EQ(zx_object_wait_one(args.start_event, ZX_EVENT_SIGNALED, ZX_TIME_INFINITE, NULL), ZX_OK,
-            "failed to wait");
+  ASSERT_OK(args.start_event.wait_one(ZX_EVENT_SIGNALED, zx::time::infinite(), nullptr));
 
   for (int i = 0; i < 5; ++i) {
-    zx_time_t now = 0;
-    ASSERT_EQ(ZX_OK, zx_clock_get(ZX_CLOCK_UTC, &now), "could not read clock");
+    zx::time_utc now;
+    ASSERT_OK(zx::clock::get(&now));
     struct timespec then = {
-        .tv_sec = now / ZX_SEC(1),
-        .tv_nsec = now % ZX_SEC(1),
+        .tv_sec = now.get() / ZX_SEC(1),
+        .tv_nsec = now.get() % ZX_SEC(1),
     };
-    then.tv_nsec += kRelativeDeadline;
-    if (then.tv_nsec > (long)ZX_SEC(1)) {
+    then.tv_nsec += kRelativeDeadline.get();
+    if (then.tv_nsec > reinterpret_cast<long>(ZX_SEC(1))) {
       then.tv_nsec -= ZX_SEC(1);
       then.tv_sec += 1;
     }
-    int rc = mtx_timedlock(&args.mutex, &then);
+    int rc = mtx_timedlock(&args.lock, &then);
     ASSERT_EQ(rc, thrd_timedout, "wait should time out");
-    zx_time_t later = 0;
-    ASSERT_EQ(ZX_OK, zx_clock_get(ZX_CLOCK_UTC, &later), "could not read clock");
-    zx_duration_t elapsed = zx_time_sub_time(later, now);
+    zx::time_utc later;
+
+    ASSERT_OK(zx::clock::get(&later));
+    zx_duration_t elapsed = later.get() - now.get();
     // Since the wait is based on the UTC clock which can be adjusted while
     // the wait proceeds, it is important to check that the mutex does not
     // return early.
@@ -231,24 +242,20 @@ static bool test_timeout_elapsed(void) {
     // expose a way to do local modifications to the clock so we can properly
     // test this behavior. Currently the UTC offset is global and mutating it here
     // can create hard to diagnose flakes as seen with bug 34857.
-    EXPECT_GE(elapsed, kRelativeDeadline, "wait returned early");
+    EXPECT_GE(elapsed, kRelativeDeadline.get(), "wait returned early");
   }
 
   // Inform the helper thread that we are done.
-  ASSERT_EQ(zx_object_signal(args.done_event, 0, ZX_EVENT_SIGNALED), ZX_OK, "failed to signal");
-  ASSERT_EQ(thrd_join(helper, NULL), thrd_success, "failed to join");
+  ASSERT_OK(args.done_event.signal(0, ZX_EVENT_SIGNALED));
+  ASSERT_EQ(thrd_join(helper, kNoReturnValue), thrd_success);
+  cleanup_thread.cancel();
 
-  mtx_destroy(&args.mutex);
-  ASSERT_EQ(zx_handle_close(args.start_event), ZX_OK, "failed to close event");
-  ASSERT_EQ(zx_handle_close(args.done_event), ZX_OK, "failed to close event");
+  mtx_destroy(&args.lock);
 
-  END_TEST;
+  ASSERT_EQ(args.mtx_lock_result, thrd_success);
+  ASSERT_OK(args.signal_result);
+  ASSERT_OK(args.wait_one_result);
+  ASSERT_EQ(args.mtx_unlock_result, thrd_success);
 }
 
-BEGIN_TEST_CASE(mtx_tests)
-RUN_TEST(test_initializer)
-RUN_TEST(test_mutexes)
-RUN_TEST(test_try_mutexes)
-RUN_TEST(test_static_initializer)
-RUN_TEST(test_timeout_elapsed)
-END_TEST_CASE(mtx_tests)
+}  // namespace
