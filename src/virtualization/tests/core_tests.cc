21a11e29bfa66dc49afff4668133e161db9e0472
Alex Legg <alexlegg@google.com>
2019-10-30 04:02:07 +0000

    [virtualization] Move balloon, console, and rng tests to core suite
    Sharing a test suite means that these tests can all use the same guests,
    to reduce the number of guests the tests need to start.
    
    This can be taken further with some more refactoring: net, gpu, and
    input are still separate.
    
    Bug: 36558
    Change-Id: I239fddfbd4a7bcae99ea37012f87af58f12f6dd7

--- src/virtualization/tests/core_tests.cc
+++ src/virtualization/tests/core_tests.cc
@@ -4,15 +4,97 @@
 
 #include <gtest/gtest.h>
 
-#include "guest_test.h"
+#include "src/virtualization/tests/guest_test.h"
+
+using testing::HasSubstr;
+
+static constexpr size_t kVirtioBalloonPageCount = 256;
+static constexpr size_t kVirtioConsoleMessageCount = 100;
+static constexpr char kVirtioRngUtil[] = "virtio_rng_test_util";
 
 template <class T>
 using CoreGuestTest = GuestTest<T>;
 
+// This test suite contains all guest tests that don't require a specific configuration of devices.
+// They are grouped together so that they share guests and reduce the number of times guests are
+// started, which is time consuming. Note that this means that some tests need to dynamically check
+// the guest type in order to skip under certain conditions.
 TYPED_TEST_SUITE(CoreGuestTest, AllGuestTypes);
 
 TYPED_TEST(CoreGuestTest, LaunchGuest) {
   std::string result;
   EXPECT_EQ(this->Execute({"echo", "test"}, &result), ZX_OK);
   EXPECT_EQ(result, "test\n");
+}
+
+TYPED_TEST(CoreGuestTest, VirtioBalloon) {
+  // Zircon does not yet have a virtio balloon driver.
+  if (this->GetGuestKernel() == GuestKernel::ZIRCON) {
+    return;
+  }
+
+  std::string result;
+  EXPECT_EQ(this->Execute({"echo", "test"}, &result), ZX_OK);
+  EXPECT_EQ(result, "test\n");
+
+  fuchsia::virtualization::BalloonControllerSyncPtr balloon_controller;
+  this->ConnectToBalloon(balloon_controller.NewRequest());
+
+  uint32_t initial_num_pages;
+  zx_status_t status = balloon_controller->GetNumPages(&initial_num_pages);
+  ASSERT_EQ(status, ZX_OK);
+
+  // Request an increase to the number of pages in the balloon.
+  status = balloon_controller->RequestNumPages(initial_num_pages + kVirtioBalloonPageCount);
+  ASSERT_EQ(status, ZX_OK);
+
+  // Verify that the number of pages eventually equals the requested number. The
+  // guest may not respond to the request immediately so we call GetNumPages in
+  // a loop.
+  uint32_t num_pages;
+  while (true) {
+    status = balloon_controller->GetNumPages(&num_pages);
+    ASSERT_EQ(status, ZX_OK);
+    if (num_pages == initial_num_pages + kVirtioBalloonPageCount) {
+      break;
+    }
+  }
+
+  // Request a decrease to the number of pages in the balloon back to the
+  // initial value.
+  status = balloon_controller->RequestNumPages(initial_num_pages);
+  ASSERT_EQ(status, ZX_OK);
+
+  while (true) {
+    status = balloon_controller->GetNumPages(&num_pages);
+    ASSERT_EQ(status, ZX_OK);
+    if (num_pages == initial_num_pages) {
+      break;
+    }
+  }
+}
+
+TYPED_TEST(CoreGuestTest, VirtioConsole) {
+  // Test many small packets.
+  std::string result;
+  for (size_t i = 0; i != kVirtioConsoleMessageCount; ++i) {
+    EXPECT_EQ(this->Execute({"echo", "test"}, &result), ZX_OK);
+    EXPECT_EQ(result, "test\n");
+  }
+
+  // Test large packets. Note that we must keep the total length below 4096,
+  // which is the maximum line length for dash.
+  std::string test_data = "";
+  for (size_t i = 0; i != kVirtioConsoleMessageCount; ++i) {
+    test_data.append("Lorem ipsum dolor sit amet consectetur");
+  }
+  EXPECT_EQ(this->Execute({"echo", test_data.c_str()}, &result), ZX_OK);
+  test_data.append("\n");
+  EXPECT_EQ(result, test_data);
+}
+
+TYPED_TEST(CoreGuestTest, VirtioRng) {
+  std::string result;
+  EXPECT_EQ(this->RunUtil(kVirtioRngUtil, {}, &result), ZX_OK);
+  EXPECT_THAT(result, HasSubstr("PASS"));
 }
