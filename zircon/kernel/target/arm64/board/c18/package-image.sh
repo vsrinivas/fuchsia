7f3f393982e377b0068a53d5d287e5f5adc08dd9
Carl Norum <cjn@google.com>
2019-11-05 23:02:44 +0000

    [c18] c18/c19 bringup
    - boots and runs to a command line prompt
    - easy instructions:
    
        $ fx set c18.bringup
        $ fx build
        $ fx mkzedboot
    
    Change-Id: I982e7134921809538e8f7896819ddeebb68fe01b

---
+++ zircon/kernel/target/arm64/board/c18/package-image.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Fuchsia Authors
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+set -eo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZIRCON_DIR="${DIR}/../../../../.."
+PROJECT_ROOT="${ZIRCON_DIR}/.."
+
+# arguments
+BOARD=c18
+BUILD_DIR=
+BOOT_IMG=
+ZIRCON_BOOTIMAGE=
+
+# internal use
+FUTILITY=host-tools/futility
+KEYBLOCK="${PROJECT_ROOT}/third_party/vboot_reference/tests/devkeys/kernel.keyblock"
+PRIVATEKEY="${PROJECT_ROOT}/third_party/vboot_reference/tests/devkeys/kernel_data_key.vbprivk"
+ITSSCRIPT_TEMPLATE="${DIR}/its_script"
+DUMMY_DEVICE_TREE="${ZIRCON_DIR}/kernel/target/arm64/dtb/dummy-device-tree.dtb"
+
+function HELP {
+    echo "help:"
+    echo "-B <build-dir>  : path to zircon build directory"
+    echo "-o              : output boot.img file (defaults to <build-dir>/<board>-boot.img)"
+    echo "-z              : input zircon ZBI file (defaults to <build-dir>/<board>-boot.img)"
+    exit 1
+}
+
+while getopts "B:o:z:" FLAG; do
+    case $FLAG in
+        B) BUILD_DIR="${OPTARG}";;
+        o) BOOT_IMG="${OPTARG}";;
+        z) ZIRCON_BOOTIMAGE="${OPTARG}";;
+        \?)
+            echo unrecognized option
+            HELP
+            ;;
+    esac
+done
+
+if [[ -z "${BUILD_DIR}" ]]; then
+    echo must specify a Zircon build directory
+    HELP
+fi
+
+# zircon image built by the Zircon build system
+if [[ -z "${ZIRCON_BOOTIMAGE}" ]]; then
+    ZIRCON_BOOTIMAGE="${BUILD_DIR}/arm64.zbi"
+fi
+
+# Final packaged ChromeOS style boot image
+if [[ -z "${BOOT_IMG}" ]]; then
+    BOOT_IMG="${BUILD_DIR}/${BOARD}-boot.img"
+fi
+
+# boot shim for our board
+BOOT_SHIM="${BUILD_DIR}/${BOARD}-boot-shim.bin"
+
+# outputs
+SHIMMED_ZIRCON_BOOTIMAGE="${ZIRCON_BOOTIMAGE}.shim"
+COMPRESSED_BOOTIMAGE="${ZIRCON_BOOTIMAGE}.lz4"
+FIT_BOOTIMAGE="${ZIRCON_BOOTIMAGE}.fit"
+
+# shim and compress the ZBI
+cat "${BOOT_SHIM}" "${ZIRCON_BOOTIMAGE}" > "${SHIMMED_ZIRCON_BOOTIMAGE}"
+lz4 -q -B4 -f "${SHIMMED_ZIRCON_BOOTIMAGE}" "${COMPRESSED_BOOTIMAGE}"
+
+# wrap everything up the way depthcharge expects it to be
+mkimage -D "-q -I dts -O dtb"                                                \
+        -p 2048                                                              \
+        -f <(sed -e s@COMPRESSED_BOOTIMAGE@"${PWD}/${COMPRESSED_BOOTIMAGE}"@ \
+                 -e s@DUMMY_DEVICE_TREE@"${DUMMY_DEVICE_TREE}"@              \
+                 "${ITSSCRIPT_TEMPLATE}")                                    \
+        "${FIT_BOOTIMAGE}" >/dev/null
+
+# sign it
+"${FUTILITY}" vbutil_kernel                      \
+              --pack "${BOOT_IMG}"               \
+              --keyblock "${KEYBLOCK}"           \
+              --signprivate "${PRIVATEKEY}"      \
+              --bootloader "${ZIRCON_BOOTIMAGE}" \
+              --vmlinuz "${FIT_BOOTIMAGE}"       \
+              --version 1                        \
+              --arch arm
