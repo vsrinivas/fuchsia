a9e755f090a3d717c7c8a49cc472068341eaed3c
Ben Keller <galbanum@google.com>
2019-11-07 21:07:28 +0000

    [pkg-resolver-tests] Add comment on root.inspect glob
    Change-Id: I7ab85c8455fdb5910c93fad0c0abffe849b34098

--- garnet/tests/pkg-resolver/src/inspect.rs
+++ garnet/tests/pkg-resolver/src/inspect.rs
@@ -20,12 +20,33 @@ async fn test_initial_inspect_state() {
         .expect("fidl call succeeds")
         .expect("test apply result is ok");
 
-    // Use glob to find the path of the inspect VMO
-    // Note: the glob match only works if there is a pattern in the final path component.
-    // We are not sure why this is the case, but suspect it has something to do with
-    // how the vfs is implemented.
+    // When `glob` is matching a path component that is a string literal, it uses
+    // `std::fs::metadata()` to test the existence of the path instead of listing the parent dir.
+    // `metadata()` calls `stat`, which creates and destroys an fd in fdio.
+    // When the fd is for "root.inspect", which is a VMO, destroying the fd calls
+    // `zxio_vmofile_release`, which makes a fuchsia.io.File.Seek FIDL call.
+    // This FIDL call is received by `ServiceFs`, which, b/c "root.inspect" was opened
+    // by fdio with `OPEN_FLAG_NODE_REFERENCE`, is treating the zircon channel as a stream of
+    // Node requests.
+    // `ServiceFs` then closes the channel and logs a
+    // "ServiceFs failed to parse an incoming node request: UnknownOrdinal" error (with
+    // the File.Seek ordinal).
+    // `ServiceFs` closing the channel is seen by `metadata` as a `BrokenPipe` error, which
+    // `glob` interprets as there being nothing at "root.inspect", so the VMO is not found.
+    // To work around this, we use a trivial pattern in the "root.inspect" path component,
+    // which prevents the `metadata` shortcut.
+    //
+    // To fix this, `zxio_vmofile_release` probably shouldn't be unconditionally calling
+    // `fuchsia.io.File.Seek`, because, per a comment in `io.fidl`, that is not a valid
+    // method to be called on a `Node` opened with `OPEN_FLAG_NODE_REFERENCE`.
+    // `zxio_vmofile_release` could determine if the `Node` were opened with
+    // `OPEN_FLAG_NODE_REFERENCE` (by calling `Node.NodeGetFlags` or `File.GetFlags`).
+    // Note that if `zxio_vmofile_release` starts calling `File.GetFlags`, `ServiceFs`
+    // will need to stop unconditionally treating `Node`s opened with `OPEN_FLAG_NODE_REFERNCE`
+    // as `Node`s.
+    // TODO(fxb/40888)
     let pattern = format!(
-        "/hub/r/{}/*/c/pkg_resolver.cmx/*/out/objects/root[.]inspect",
+        "/hub/r/{}/*/c/pkg_resolver.cmx/*/out/objects/root.i[n]spect",
         glob::Pattern::escape(&env.nested_environment_label)
     );
     let paths = glob::glob_with(
