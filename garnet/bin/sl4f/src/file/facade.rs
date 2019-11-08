d16fadb9bdbdeb9f6bc872d9ab542068da37a42c
Kevin Wells <kevinwells@google.com>
2019-11-08 18:20:21 +0000

    [sl4f] Cleanup file facade semantics/add tests
    This change modifies the DeleteFile API to not stat the file first,
    allowing some VFS files that support deletion but not stat to be deleted
    with this API.
    
    Fixed: 40206
    Change-Id: I776320e06505d32cac73a8f001e9f5aedd581c40

--- garnet/bin/sl4f/src/file/facade.rs
+++ garnet/bin/sl4f/src/file/facade.rs
@@ -2,36 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use base64;
 use failure::Error;
 use serde_json::{to_value, Value};
-use std::fs::{create_dir, create_dir_all, remove_file, File};
-use std::io::prelude::*;
-use std::path::Path;
+use std::{fs, io, path::Path};
 
-use crate::file::types::*;
+use super::types::*;
 
 /// Facade providing access to session testing interfaces.
 #[derive(Debug)]
-pub struct FileFacade {}
+pub struct FileFacade;
 
 impl FileFacade {
-    pub fn new() -> FileFacade {
-        FileFacade {}
+    pub fn new() -> Self {
+        Self
     }
 
-    /// Deletes the given path, which must be a file. Returns OK(NotFound) if the file is alredy
-    /// inexistent.
+    /// Deletes the given path, which must be a file. Returns OK(NotFound) if the file does not
+    /// exist.
     pub async fn delete_file(&self, args: Value) -> Result<DeleteFileResult, Error> {
         let path = args.get("path").ok_or(format_err!("DeleteFile failed, no path"))?;
         let path = path.as_str().ok_or(format_err!("DeleteFile failed, path not string"))?;
 
-        if !Path::new(path).exists() {
-            return Ok(DeleteFileResult::NotFound);
+        match fs::remove_file(path) {
+            Ok(()) => Ok(DeleteFileResult::Success),
+            Err(e) if e.kind() == io::ErrorKind::NotFound => Ok(DeleteFileResult::NotFound),
+            Err(e) => Err(e.into()),
         }
-
-        remove_file(path)?;
-        Ok(DeleteFileResult::Success)
     }
 
     /// Creates a new directory. Returns OK(AlreadyExists) if the directory already exists.
@@ -47,9 +43,9 @@ impl FileFacade {
         }
 
         if recurse {
-            create_dir_all(path)?;
+            fs::create_dir_all(path)?;
         } else {
-            create_dir(path)?;
+            fs::create_dir(path)?;
         }
         Ok(MakeDirResult::Success)
     }
@@ -59,10 +55,7 @@ impl FileFacade {
         let path = args.get("path").ok_or(format_err!("ReadFile failed, no path"))?;
         let path = path.as_str().ok_or(format_err!("ReadFile failed, path not string"))?;
 
-        let mut file = File::open(path)?;
-        let mut contents = Vec::new();
-        file.read_to_end(&mut contents)?;
-
+        let contents = fs::read(path)?;
         let encoded_contents = base64::encode(&contents);
 
         Ok(to_value(encoded_contents)?)
@@ -81,8 +74,110 @@ impl FileFacade {
         let destination =
             destination.as_str().ok_or(format_err!("WriteFile failed, destination not string"))?;
 
-        let mut file = File::create(destination)?;
-        file.write_all(&contents)?;
+        fs::write(destination, &contents)?;
         Ok(WriteFileResult::Success)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use matches::assert_matches;
+    use serde_json::json;
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn delete_file_ok() {
+        let temp = tempfile::tempdir().unwrap();
+        let path = temp.path().join("test.txt");
+        fs::write(&path, "hello world!".as_bytes()).unwrap();
+        assert!(path.exists());
+
+        assert_matches!(
+            FileFacade.delete_file(json!({ "path": path })).await,
+            Ok(DeleteFileResult::Success)
+        );
+        assert!(!path.exists());
+
+        assert_matches!(
+            FileFacade.delete_file(json!({ "path": path })).await,
+            Ok(DeleteFileResult::NotFound)
+        );
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn make_dir_ok() {
+        let temp = tempfile::tempdir().unwrap();
+        let path = temp.path().join("a");
+        assert!(!path.exists());
+
+        assert_matches!(
+            FileFacade.make_dir(json!({ "path": path })).await,
+            Ok(MakeDirResult::Success)
+        );
+        assert!(path.is_dir());
+
+        assert_matches!(
+            FileFacade.make_dir(json!({ "path": path })).await,
+            Ok(MakeDirResult::AlreadyExists)
+        );
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn make_dir_recurse_ok() {
+        let temp = tempfile::tempdir().unwrap();
+        let path = temp.path().join("a/b/c");
+        assert!(!path.exists());
+
+        assert_matches!(FileFacade.make_dir(json!({ "path": path })).await, Err(_));
+        assert!(!path.exists());
+
+        assert_matches!(
+            FileFacade.make_dir(json!({ "path": path, "recurse": true })).await,
+            Ok(MakeDirResult::Success)
+        );
+        assert!(path.is_dir());
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn read_file_ok() {
+        const FILE_CONTENTS: &str = "hello world!";
+        const FILE_CONTENTS_AS_BASE64: &str = "aGVsbG8gd29ybGQh";
+
+        let temp = tempfile::tempdir().unwrap();
+        let path = temp.path().join("test.txt");
+        fs::write(&path, FILE_CONTENTS.as_bytes()).unwrap();
+
+        assert_matches!(
+            FileFacade.read_file(json!({ "path": path })).await,
+            Ok(value) if value == json!(FILE_CONTENTS_AS_BASE64)
+        );
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn write_file_ok() {
+        const FILE_CONTENTS: &str = "hello world!";
+        const FILE_CONTENTS_AS_BASE64: &str = "aGVsbG8gd29ybGQh";
+
+        let temp = tempfile::tempdir().unwrap();
+        let path = temp.path().join("test.txt");
+
+        assert_matches!(
+            FileFacade.write_file(json!({ "data": FILE_CONTENTS_AS_BASE64, "dst": path })).await,
+            Ok(WriteFileResult::Success)
+        );
+
+        assert_eq!(fs::read_to_string(&path).unwrap(), FILE_CONTENTS);
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn write_file_unwritable_path() {
+        const FILE_CONTENTS_AS_BASE64: &str = "aGVsbG8gd29ybGQh";
+
+        assert_matches!(
+            FileFacade
+                .write_file(json!({ "data": FILE_CONTENTS_AS_BASE64, "dst": "/pkg/is/readonly" }))
+                .await,
+            Err(_)
+        );
     }
 }
