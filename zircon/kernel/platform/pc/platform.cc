15de8155c2657aadbc62cecc8e9dd2a1bfc5d038
Marco Vanotti <mvanotti@google.com>
2019-11-08 10:38:28 +0000

    [kernel] clear cmdline entropy from zbi.
    This CL modifies the zbi parsing process by deleting the cmdline
    arguments after they are processed. One of the arguments might include
    entropy for the kernel cprng and we don't want it to be available to
    other processes with access to the ZBI.
    
    It looks like no other programs are using the CMDLINE zbi objects,
    so this change should be safe. I also used a modified version of change
    333450 to scan the memory and look for the entropy in any vmo, the only
    one that showed up was the zbi vmo.
    
    NOTE that this commit clears ALL the zbi objects of type CMDLINE and not
    just the parts related to the kernel.entropy-mixin, because that might
    appear in the middle of the payload, and appear multiple times with
    different lengths. Instead, it's easier to just clear everything if it
    is no longer needed.
    
    The whole process is less than ideal, because the ZBI shouldn't be used
    to pass secrets around, and there are no guarantees about what other
    copies are lying around in memory. Sadly, this is the best we can do to
    guarantee some entropy during early boot.
    
    I added a test to the global_prng to make sure that the zbi does not
    contain the entropy-mixin parameter. To run it, you can use: `k ut
    global_prng`.
    
    TEST=Tested in Astro, VIM2 and QEMU.
    
    Change-Id: Iba72eaa90740d42d0309735dd4c6925ff7f28755

--- zircon/kernel/platform/pc/platform.cc
+++ zircon/kernel/platform/pc/platform.cc
@@ -35,6 +35,7 @@
 #include <zircon/pixelformat.h>
 #include <zircon/types.h>
 
+#include <explicit-memory/bytes.h>
 #include <dev/uart.h>
 #include <fbl/alloc_checker.h>
 #include <fbl/vector.h>
@@ -120,6 +121,11 @@ zbi_result_t process_zbi_item(zbi_header_t* hdr, void* payload, void* cookie) {
       if (hdr->length > 0) {
         ((char*)payload)[hdr->length - 1] = 0;
         gCmdline.Append((char*)payload);
+
+        // The CMDLINE might include entropy for the zircon cprng.
+        // We don't want that information to be accesible after it has
+        // been added to the kernel cmdline.
+        mandatory_memset(payload, 0, hdr->length);
       }
       break;
     case ZBI_TYPE_EFI_MEMORY_MAP:
