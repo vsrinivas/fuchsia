f1cfe50007d9a40f297f2818a1baebf89341962f
shyam <shyamsundarp@google.com>
2019-11-09 02:49:46 +0000

    [audio_record] Fix e2e test.
    Looks like the sl4f test framework has evolved since the test was
    written and the test didn't adapt to it. Adding changes to address that.
    In addition, addressing logical concerns raised in the bug.
    
    To build with the test -
    $ fx set <target> --release --with=//src/tests/end_to_end/audio_record:test --with=//garnet/bin/sl4f:bin
    $ fx build
    
    To run the test locally -
    $ fx run-e2e-tests audio_record_test
    
    Bug: 40123
    Test: Verified that test works and passes locally.
    Change-Id: Ifd63bc648dc087945cf06de7d1007931fd38f73e

--- src/tests/end_to_end/audio_record/test/audio_record_test.dart
+++ src/tests/end_to_end/audio_record/test/audio_record_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'package:test/test.dart';
@@ -10,21 +9,47 @@ import 'package:sl4f/sl4f.dart' as sl4f;
 
 const _timeout = Duration(seconds: 60);
 const extension = 'wav';
+const int kWavHeaderLength = 44;
+const int kBitsPerSample = 16;
+int kBytesPerSample = (kBitsPerSample ~/ 8);
 
-double _averageSignalData(List<int> audioBytes) {
-  int sum = 0;
-  for (int i = 44; i < audioBytes.length; i++) {
-    sum += audioBytes[i];
+// Create an int16 value from two incoming uint8 values
+int _int16FromBytes(int lsbyte, int msbyte) {
+  int result = lsbyte + (msbyte << 8);
+  // After shifting to create an uint16, we must convert to int16
+  if (result >= 0x8000) {
+    result -= 0x10000;
   }
-  return sum / (audioBytes.length - 44);
+  return result;
 }
 
-double _varianceSignalData(List<int> audioBytes, double average) {
-  double variance = 0;
-  for (int i = 44; i < audioBytes.length; i++) {
-    variance += pow((audioBytes[i] - average), 2);
+bool _isDataAllZeros(List<int> audioBytes) {
+  for (int i = kWavHeaderLength; i < audioBytes.length; i += kBytesPerSample) {
+    int a = _int16FromBytes(audioBytes[i], audioBytes[i + 1]);
+    if (a != 0) {
+      return false;
+    }
   }
-  return variance / (audioBytes.length - 44);
+  return true;
+}
+
+double _averageSignalData(List<int> audioBytes, int numSamples) {
+  double sum = 0.0;
+  for (int i = kWavHeaderLength; i < audioBytes.length; i += kBytesPerSample) {
+    int a = _int16FromBytes(audioBytes[i], audioBytes[i + 1]);
+    sum += a;
+  }
+  return sum / numSamples;
+}
+
+double _varianceSignalData(
+    List<int> audioBytes, double average, int numSamples) {
+  double variance = 0.0;
+  for (int i = kWavHeaderLength; i < audioBytes.length; i += kBytesPerSample) {
+    int a = _int16FromBytes(audioBytes[i], audioBytes[i + 1]);
+    variance += pow((a - average), 2);
+  }
+  return variance / numSamples;
 }
 
 void _validateFile(File file) {
@@ -34,13 +59,14 @@ void _validateFile(File file) {
   if (file.lengthSync() != 288644) {
     fail('${file.path} is not of right size');
   }
+  int numSamples = ((288644 - kWavHeaderLength) ~/ kBytesPerSample);
   final audioBytes = file.readAsBytesSync();
-  double average = _averageSignalData(audioBytes);
-  if (average < 0.1) {
+  if (_isDataAllZeros(audioBytes)) {
     fail('${file.path} has only silence and no real data');
   }
-  double variance = _varianceSignalData(audioBytes, average);
-  if (variance < 0.1) {
+  double average = _averageSignalData(audioBytes, numSamples);
+  double variance = _varianceSignalData(audioBytes, average, numSamples);
+  if (variance < 0.001) {
     fail('${file.path} data has low variance and is not normal');
   }
 }
@@ -50,25 +76,26 @@ String _fileNameToTargetPath(String fileName) {
 }
 
 Future<File> _downloadAudioFile(
-    sl4f.Sl4f sl4f, sl4f.Dump dump, String fileName) async {
+    sl4f.Sl4f sl4fDriver, sl4f.Dump dump, String fileName) async {
   final filePath = _fileNameToTargetPath(fileName);
-  final String response =
-      await sl4f.request('traceutil_facade.GetTraceFile', {'path': filePath});
-  return dump.writeAsBytes('$fileName', extension, base64.decode(response));
+  final contents = await sl4f.Storage(sl4fDriver).readFile(filePath);
+  return dump.writeAsBytes('$fileName', extension, contents);
 }
 
 void main() {
   sl4f.Sl4f sl4fDriver;
+  Directory dumpDir;
   sl4f.Dump dumpDriver;
 
   setUp(() async {
     sl4fDriver = sl4f.Sl4f.fromEnvironment();
     await sl4fDriver.startServer();
-
-    dumpDriver = sl4f.Dump();
+    dumpDir = Directory.systemTemp.createTempSync('dump-test');
+    dumpDriver = sl4f.Dump(dumpDir.path);
   });
 
   tearDown(() async {
+    dumpDir.deleteSync(recursive: true);
     await sl4fDriver.stopServer();
     sl4fDriver.close();
   });
@@ -77,18 +104,26 @@ void main() {
     // The test will run twice to make sure that the driver is inited properly
     // for recording. And stopping recording does proper teardown so that the
     // driver is capable of starting recording the second time.
-    String fileNameOne = 't1';
-    final proc1 = await sl4fDriver.ssh.start(
-        'audio -r 48000 -c 2 -b 16 record ${_fileNameToTargetPath(fileNameOne)} 2');
-    await proc1.stdout.transform(utf8.decoder).join();
+
+    // audio commands require direct access to driver which will not work if
+    // audio_core is holding on to the resource. Hence explicitly killing it
+    // before trying the audio record command.
+    await sl4fDriver.ssh.run('killall audio_core.cmx');
+    String fileNameOne = 'test_1';
+    // 'audio' utility will capture 48kHz 2-chan 16-bit audio directly from
+    // the audio driver for 2 secs and save it to a WAV file.
+    final proc1 = await sl4fDriver.ssh.run(
+        'audio -r 48000 -c 2 -b $kBitsPerSample record ${_fileNameToTargetPath(fileNameOne)} 2');
+    expect(proc1.exitCode, equals(235)); // ZX_ERR_TIMED_OUT
+
     File fileOne =
         await _downloadAudioFile(sl4fDriver, dumpDriver, fileNameOne);
     _validateFile(fileOne);
 
-    String fileNameTwo = 't2';
-    final proc2 = await sl4fDriver.ssh.start(
-        'audio -r 48000 -c 2 -b 16 record ${_fileNameToTargetPath(fileNameTwo)} 2');
-    await proc2.stdout.transform(utf8.decoder).join();
+    String fileNameTwo = 'test_2';
+    final proc2 = await sl4fDriver.ssh.run(
+        'audio -r 48000 -c 2 -b $kBitsPerSample record ${_fileNameToTargetPath(fileNameTwo)} 2');
+    expect(proc2.exitCode, equals(235)); // ZX_ERR_TIMED_OUT
     File fileTwo =
         await _downloadAudioFile(sl4fDriver, dumpDriver, fileNameTwo);
     _validateFile(fileTwo);
