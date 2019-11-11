3090cbab8804cf02ba5834f4c75a6560f6b567fa
Chase Latta <chaselatta@google.com>
2019-11-11 19:46:28 +0000

    [terminal_settings_agent] add initial implementation.
    Creates the stub implementation for the terminal
    settings agent so we can start building out the
    settings panel.
    
    Change-Id: Iac288c717245141f37560c1f7133ad82099a8aa8

---
+++ src/experiences/bin/terminal_settings_agent/src/main.rs
@@ -0,0 +1,29 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    failure::Error, fuchsia_async as fasync, fuchsia_component::server::ServiceFs,
+    fuchsia_syslog as syslog, futures::prelude::*,
+};
+
+mod profiles_service;
+
+fn main() -> Result<(), Error> {
+    syslog::init_with_tags(&["terminal-settings-agent"]).expect("Can't init logger");
+    let mut executor = fasync::Executor::new()?;
+    let mut fs = ServiceFs::new();
+    let mut public = fs.dir("svc");
+
+    public.add_fidl_service(move |stream| {
+        fasync::spawn(
+            profiles_service::run_fidl_server(stream)
+                .unwrap_or_else(|e| panic!("Error while serving profiles service: {}", e)),
+        )
+    });
+
+    fs.take_and_serve_directory_handle()?;
+    let () = executor.run_singlethreaded(fs.collect());
+
+    Ok(())
+}
