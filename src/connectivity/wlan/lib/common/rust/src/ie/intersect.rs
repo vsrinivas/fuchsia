8db133eac3482d5db09b8bd06d4ce204a1b09945
Tony Young <tonyy@google.com>
2019-11-12 01:02:58 +0000

    [wlan] Make intersect_rates take refs
    This avoids having to allocate vectors for AP and client rates ahead of
    time, so we only construct the AP rates vector once.
    
    Test: Unit tests included
    Bug: 29663
    Change-Id: I2ef975569d0b55cbc1ff0630097361eb4be6d225

--- src/connectivity/wlan/lib/common/rust/src/ie/intersect.rs
+++ src/connectivity/wlan/lib/common/rust/src/ie/intersect.rs
@@ -8,27 +8,30 @@ use {
     std::collections::HashSet,
 };
 
-pub struct ApRates(pub Vec<SupportedRate>);
-pub struct ClientRates(pub Vec<SupportedRate>);
+pub struct ApRates<'a>(pub &'a [SupportedRate]);
+pub struct ClientRates<'a>(pub &'a [SupportedRate]);
 
 /// Returns the rates specified by the AP that are also supported by the client, with basic bits
 /// following their values in the AP.
 /// Returns Error if intersection fails.
 /// Note: The client MUST support ALL the basic rates specified by the AP or the intersection fails.
-pub fn intersect_rates(ap: ApRates, client: ClientRates) -> Result<Vec<SupportedRate>, Error> {
-    let mut ap = ap.0;
-    let client = client.0.into_iter().map(|r| r.rate()).collect::<HashSet<_>>();
+pub fn intersect_rates(
+    ap_rates: ApRates,
+    client_rates: ClientRates,
+) -> Result<Vec<SupportedRate>, Error> {
+    let mut rates = ap_rates.0.to_vec();
+    let client_rates = client_rates.0.iter().map(|r| r.rate()).collect::<HashSet<_>>();
     // The client MUST support ALL basic rates specified by the AP.
-    if ap.iter().any(|ra| ra.basic() && !client.contains(&ra.rate())) {
+    if rates.iter().any(|ra| ra.basic() && !client_rates.contains(&ra.rate())) {
         return Err(format_err!("At least one basic rate not supported."));
     }
 
     // Remove rates that are not supported by the client.
-    ap.retain(|ra| client.contains(&ra.rate()));
-    if ap.is_empty() {
+    rates.retain(|ra| client_rates.contains(&ra.rate()));
+    if rates.is_empty() {
         Err(format_err!("Client does not support any AP rates."))
     } else {
-        Ok(ap)
+        Ok(rates)
     }
 }
 
@@ -43,49 +46,56 @@ mod tests {
 
     #[test]
     fn some_basic_rate_missing() {
-        let ap = vec![SupportedRate::new_basic(120), SupportedRate::new_basic(111)];
-        let client = vec![SupportedRate(111)];
         // AP basic rate 120 is not supported, resulting in an Error
-        let error = intersect_rates(ApRates(ap), ClientRates(client)).unwrap_err();
+        let error = intersect_rates(
+            ApRates(&[SupportedRate::new_basic(120), SupportedRate::new_basic(111)][..]),
+            ClientRates(&[SupportedRate(111)][..]),
+        )
+        .unwrap_err();
         assert!(format!("{}", error).contains("At least one basic rate not supported."));
     }
 
     #[test]
     fn all_basic_rates_supported() {
-        let ap = vec![SupportedRate::new_basic(120), SupportedRate(111)];
-        let client = vec![SupportedRate(120)];
         assert_eq!(
             vec![SupportedRate::new_basic(120)],
-            intersect_rates(ApRates(ap), ClientRates(client)).unwrap()
+            intersect_rates(
+                ApRates(&[SupportedRate::new_basic(120), SupportedRate(111)][..]),
+                ClientRates(&[SupportedRate(120)][..])
+            )
+            .unwrap()
         );
     }
 
     #[test]
     fn all_basic_and_non_basic_rates_supported() {
-        let ap = vec![SupportedRate::new_basic(120), SupportedRate(111)];
-        let client = vec![SupportedRate(120)];
         assert_eq!(
             vec![SupportedRate::new_basic(120)],
-            intersect_rates(ApRates(ap), ClientRates(client)).unwrap()
+            intersect_rates(
+                ApRates(&[SupportedRate::new_basic(120), SupportedRate(111)][..]),
+                ClientRates(&[SupportedRate(120)][..])
+            )
+            .unwrap()
         );
     }
 
     #[test]
     fn no_rates_are_supported() {
-        let ap = vec![SupportedRate(120)];
-        let client = vec![];
-        let error = intersect_rates(ApRates(ap), ClientRates(client)).unwrap_err();
+        let error =
+            intersect_rates(ApRates(&[SupportedRate(120)][..]), ClientRates(&[][..])).unwrap_err();
         assert!(format!("{}", error).contains("Client does not support any AP rates."));
     }
 
     #[test]
     fn preserve_ap_rates_basicness() {
-        let ap = vec![SupportedRate(120), SupportedRate(111)];
-        let client = vec![SupportedRate::new_basic(120)];
         // AP side 120 is not basic so the result should be non-basic.
         assert_eq!(
             vec![SupportedRate(120)],
-            intersect_rates(ApRates(ap), ClientRates(client)).unwrap()
+            intersect_rates(
+                ApRates(&[SupportedRate(120), SupportedRate(111)][..]),
+                ClientRates(&[SupportedRate::new_basic(120)][..])
+            )
+            .unwrap()
         );
     }
 }
