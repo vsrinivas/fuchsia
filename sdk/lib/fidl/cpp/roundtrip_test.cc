4f208f00c2ebf0d2a49441653f48d61d9f1580db
Yifei Teng <yifeit@google.com>
2019-11-13 15:44:43 -0800

    [fidl] Make hlcpp roundtrip_test green under v1
    With some changes to test_util to add alt_type, we are able to keep most
    of the tests in roundtrip_test. Three test are disabled:
    
    The SimpleTable.SerializeAndDeserializeWithReserved test is failing with
    a transformer assert.
    
    The XUnion.SimpleUnion test is disabled because the encoded bytes would be
    different under v1.
    
    The XUnion.SerializeAndDeserializeSimpleUnion test is disabled because
    validation would not work with a v1 coding table.
    
    Bug: 41371
    Change-Id: Id6be7383f164606d94d070858f5b4cb715643d85

--- sdk/lib/fidl/cpp/roundtrip_test.cc
+++ sdk/lib/fidl/cpp/roundtrip_test.cc
@@ -76,6 +76,8 @@ TEST(SimpleTable, SerializeAndDeserialize) {
   EXPECT_EQ(1, RoundTrip<NewerSimpleTable>(input).x());
 }
 
+#ifndef FIDL_EXPERIMENTAL_WRITE_V1_WIREFORMAT_DISABLE_TEST
+// TODO(fxb/41371): Fix the transformer assert that occurs in this test.
 TEST(SimpleTable, SerializeAndDeserializeWithReserved) {
   SimpleTable input;
   input.set_y(1);
@@ -88,6 +90,8 @@ TEST(SimpleTable, SerializeAndDeserializeWithReserved) {
   // We should be able to decode to it.
   EXPECT_EQ(1, RoundTrip<NewerSimpleTable>(input).y());
 }
+
+#endif  // FIDL_EXPERIMENTAL_WRITE_V1_WIREFORMAT_DISABLE_TEST
 
 TEST(Empty, SerializeAndDeserialize) {
   Empty input{};
@@ -150,6 +154,8 @@ TEST(XUnion, Int32) {
   EXPECT_TRUE(ValueToBytes(input, expected));
 }
 
+#ifndef FIDL_EXPERIMENTAL_WRITE_V1_WIREFORMAT_DISABLE_TEST
+// TODO(39159): Enable this test when writing v1 by default.
 TEST(XUnion, SimpleUnion) {
   SimpleUnion su;
   su.set_str("hello");
@@ -171,6 +177,8 @@ TEST(XUnion, SimpleUnion) {
 
   EXPECT_TRUE(ValueToBytes(input, expected));
 }
+
+#endif  // FIDL_EXPERIMENTAL_WRITE_V1_WIREFORMAT_DISABLE_TEST
 
 TEST(XUnion, SimpleTable) {
   SimpleTable st;
@@ -198,6 +206,8 @@ TEST(XUnion, SerializeAndDeserializeInt32) {
   EXPECT_TRUE(fidl::Equals(input, RoundTrip<SampleXUnion>(input)));
 }
 
+#ifndef FIDL_EXPERIMENTAL_WRITE_V1_WIREFORMAT_DISABLE_TEST
+// TODO(39159): Enable this test when writing v1 by default.
 TEST(XUnion, SerializeAndDeserializeSimpleUnion) {
   SimpleUnion su;
   su.set_str("hello");
@@ -207,6 +217,8 @@ TEST(XUnion, SerializeAndDeserializeSimpleUnion) {
 
   EXPECT_TRUE(fidl::Equals(input, RoundTrip<SampleXUnion>(input)));
 }
+
+#endif  // FIDL_EXPERIMENTAL_WRITE_V1_WIREFORMAT_DISABLE_TEST
 
 TEST(XUnion, SerializeAndDeserializeSimpleTable) {
   SimpleTable st;
