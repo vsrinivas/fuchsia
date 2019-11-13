61e43285d2ff8af9073d80658ff52762fc649e1f
Adrian Danis <adanis@google.com>
2019-11-13 03:32:31 +0000

    [Reland] [acpi][thermal] Minimal C++ migration
    This relands CL 336470, which was reverted by CL 337969. The original
    was reverted due to causing chromebooks to not boot reliably.
    
    The issue was that the zx_protocol_device_t ops was not being
    initialized correctly and had garbage function pointers. This reland
    correctly zeroes the ops before filling in the methods it has.
    
    Bug: 41060
    
    Oriignal change's dexcription:
    
    > This does an intentionally minimal C++ conversion of the dev-thermal
    > ACPI device. Two changes not strictly required for conversion were
    >  * Test code no longer #include the implementation, but rather uses
    >    a shared header for definitions.
    >  * Whether a trip point has been set is explicitly recorded in a bool
    >    to prevent an implicit conversion from float->int in an if statement,
    >    which is invalid in C++.
    >
    > Motivation for this is general cleanup of the x86 board driver, which
    > will be easier if everything is C++.
    >
    > Test: runtests -t x86-thermal-test and used thermal-cli on a chromebook
    >
    > Change-Id: I3fc3fbf13203dd7d390fc28179ff9149a6d95a72
    
    Change-Id: I3c9ff691fa48975fcee54e0347e32ce25ae5ac20

--- zircon/system/dev/board/x86/acpi-dev/dev-thermal.cc
+++ zircon/system/dev/board/x86/acpi-dev/dev-thermal.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "dev-thermal.h"
+
 #include <fuchsia/hardware/thermal/c/fidl.h>
 #include <inttypes.h>
 #include <math.h>
@@ -25,19 +27,7 @@
 
 #define KELVIN_CELSIUS_OFFSET 273.15f
 
-typedef struct acpi_thermal_device {
-  zx_device_t* zxdev;
-  ACPI_HANDLE acpi_handle;
-
-  mtx_t lock;
-
-  // event to notify on
-  zx_handle_t event;
-
-  // programmable trip points
-  uint32_t trip_point_count;
-  float trip_points[fuchsia_hardware_thermal_MAX_TRIP_POINTS];
-} acpi_thermal_device_t;
+namespace acpi_thermal {
 
 static inline float decikelvin_to_celsius(uint64_t temp_decikelvin) {
   return ((float)temp_decikelvin / 10.0f) - KELVIN_CELSIUS_OFFSET;
@@ -72,7 +62,7 @@ static zx_status_t acpi_thermal_get_info(acpi_thermal_device_t* dev,
     goto out;
   }
   info->state = 0;
-  if (info->active_trip[0] && (decikelvin_to_celsius(temp) > info->active_trip[0])) {
+  if (dev->have_trip[0] && (decikelvin_to_celsius(temp) > info->active_trip[0])) {
     info->state |= fuchsia_hardware_thermal_THERMAL_STATE_TRIP_VIOLATION;
   }
 out:
@@ -81,7 +71,7 @@ out:
 }
 
 static zx_status_t fidl_GetInfo(void* ctx, fidl_txn_t* txn) {
-  acpi_thermal_device_t* dev = ctx;
+  acpi_thermal_device_t* dev = static_cast<acpi_thermal_device_t*>(ctx);
 
   // reading state clears the signal
   zx_object_signal(dev->event, ZX_USER_SIGNAL_0, 0);
@@ -105,7 +95,7 @@ static zx_status_t fidl_GetDvfsInfo(void* ctx, fuchsia_hardware_thermal_PowerDom
 }
 
 static zx_status_t fidl_GetTemperatureCelsius(void* ctx, fidl_txn_t* txn) {
-  acpi_thermal_device_t* dev = ctx;
+  acpi_thermal_device_t* dev = static_cast<acpi_thermal_device_t*>(ctx);
   uint64_t v;
 
   zx_status_t status = acpi_tmp_call(dev->acpi_handle, &v);
@@ -119,7 +109,7 @@ static zx_status_t fidl_GetTemperatureCelsius(void* ctx, fidl_txn_t* txn) {
 }
 
 static zx_status_t fidl_GetStateChangeEvent(void* ctx, fidl_txn_t* txn) {
-  acpi_thermal_device_t* dev = ctx;
+  acpi_thermal_device_t* dev = static_cast<acpi_thermal_device_t*>(ctx);
 
   zx_handle_t handle;
   zx_status_t status = zx_handle_duplicate(dev->event, ZX_RIGHT_SAME_RIGHTS, &handle);
@@ -138,7 +128,7 @@ static zx_status_t fidl_GetStateChangePort(void* ctx, fidl_txn_t* txn) {
 }
 
 static zx_status_t fidl_SetTripCelsius(void* ctx, uint32_t id, float temp, fidl_txn_t* txn) {
-  acpi_thermal_device_t* dev = ctx;
+  acpi_thermal_device_t* dev = static_cast<acpi_thermal_device_t*>(ctx);
 
   if (dev->trip_point_count < 1) {
     return ZX_ERR_NOT_SUPPORTED;
@@ -155,6 +145,7 @@ static zx_status_t fidl_SetTripCelsius(void* ctx, uint32_t id, float temp, fidl_
     return fuchsia_hardware_thermal_DeviceSetTripCelsius_reply(txn, acpi_to_zx_status(acpi_status));
   }
   mtx_lock(&dev->lock);
+  dev->have_trip[0] = true;
   dev->trip_points[0] = temp;
   mtx_unlock(&dev->lock);
   return fuchsia_hardware_thermal_DeviceSetTripCelsius_reply(txn, ZX_OK);
@@ -180,7 +171,8 @@ static zx_status_t fidl_SetFanLevel(void* ctx, uint32_t fan_level, fidl_txn_t* t
   return fuchsia_hardware_thermal_DeviceSetFanLevel_reply(txn, ZX_ERR_NOT_SUPPORTED);
 }
 
-static const fuchsia_hardware_thermal_Device_ops_t fidl_ops = {
+// Not static so the test harness can use them.
+fuchsia_hardware_thermal_Device_ops_t fidl_ops = {
     .GetInfo = fidl_GetInfo,
     .GetDeviceInfo = fidl_GetDeviceInfo,
     .GetDvfsInfo = fidl_GetDvfsInfo,
@@ -199,7 +191,7 @@ static zx_status_t acpi_thermal_message(void* ctx, fidl_msg_t* msg, fidl_txn_t* 
 }
 
 static void acpi_thermal_notify(ACPI_HANDLE handle, UINT32 value, void* ctx) {
-  acpi_thermal_device_t* dev = ctx;
+  acpi_thermal_device_t* dev = static_cast<acpi_thermal_device_t*>(ctx);
   zxlogf(TRACE, "acpi-thermal: got event 0x%x\n", value);
   switch (value) {
     case INT3403_THERMAL_EVENT:
@@ -209,17 +201,21 @@ static void acpi_thermal_notify(ACPI_HANDLE handle, UINT32 value, void* ctx) {
 }
 
 static void acpi_thermal_release(void* ctx) {
-  acpi_thermal_device_t* dev = ctx;
+  acpi_thermal_device_t* dev = static_cast<acpi_thermal_device_t*>(ctx);
   AcpiRemoveNotifyHandler(dev->acpi_handle, ACPI_DEVICE_NOTIFY, acpi_thermal_notify);
   zx_handle_close(dev->event);
   free(dev);
 }
 
-static zx_protocol_device_t acpi_thermal_device_proto = {
-    .version = DEVICE_OPS_VERSION,
-    .release = acpi_thermal_release,
-    .message = acpi_thermal_message,
-};
+static zx_protocol_device_t acpi_thermal_device_proto = []() {
+  zx_protocol_device_t ops = {};
+  ops.version = DEVICE_OPS_VERSION;
+  ops.release = acpi_thermal_release;
+  ops.message = acpi_thermal_message;
+  return ops;
+}();
+
+}  // namespace acpi_thermal
 
 zx_status_t thermal_init(zx_device_t* parent, ACPI_DEVICE_INFO* info, ACPI_HANDLE acpi_handle) {
   // only support sensors
@@ -233,7 +229,8 @@ zx_status_t thermal_init(zx_device_t* parent, ACPI_DEVICE_INFO* info, ACPI_HANDL
     return ZX_ERR_NOT_SUPPORTED;
   }
 
-  acpi_thermal_device_t* dev = calloc(1, sizeof(acpi_thermal_device_t));
+  acpi_thermal::acpi_thermal_device_t* dev = static_cast<acpi_thermal::acpi_thermal_device_t*>(
+      calloc(1, sizeof(acpi_thermal::acpi_thermal_device_t)));
   if (!dev) {
     return ZX_ERR_NO_MEMORY;
   }
@@ -248,7 +245,8 @@ zx_status_t thermal_init(zx_device_t* parent, ACPI_DEVICE_INFO* info, ACPI_HANDL
   }
 
   // install acpi event handler
-  acpi_status = AcpiInstallNotifyHandler(acpi_handle, ACPI_DEVICE_NOTIFY, acpi_thermal_notify, dev);
+  acpi_status = AcpiInstallNotifyHandler(acpi_handle, ACPI_DEVICE_NOTIFY,
+                                         acpi_thermal::acpi_thermal_notify, dev);
   if (acpi_status != AE_OK) {
     zxlogf(ERROR, "acpi-thermal: could not install notify handler\n");
     acpi_thermal_release(dev);
@@ -267,13 +265,13 @@ zx_status_t thermal_init(zx_device_t* parent, ACPI_DEVICE_INFO* info, ACPI_HANDL
   memcpy(name, &info->Name, sizeof(UINT32));
   name[4] = '\0';
 
-  device_add_args_t args = {
-      .version = DEVICE_ADD_ARGS_VERSION,
-      .name = name,
-      .ctx = dev,
-      .ops = &acpi_thermal_device_proto,
-      .proto_id = ZX_PROTOCOL_THERMAL,
-  };
+  device_add_args_t args = {};
+
+  args.version = DEVICE_ADD_ARGS_VERSION;
+  args.name = name;
+  args.ctx = dev;
+  args.ops = &acpi_thermal::acpi_thermal_device_proto;
+  args.proto_id = ZX_PROTOCOL_THERMAL;
 
   status = device_add(parent, &args, &dev->zxdev);
   if (status != ZX_OK) {
