a6896e4ea3666541a8a904d213796fbc76793e00
Craig Stout <cstout@google.com>
2019-11-18 20:32:56 +0000

    [ui][present_view] Migrate off deprecated async loop configs
    Change-Id: I69dc5d8d3b7b19d0fe1faccb9ddac083a90b6aab

--- src/ui/tools/present_view/main.cc
+++ src/ui/tools/present_view/main.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
 #include <lib/sys/cpp/component_context.h>
 #include <lib/trace-provider/provider.h>
 
@@ -37,7 +38,7 @@ present_view::ViewInfo ParseCommandLine(const fxl::CommandLine& command_line) {
 }  // namespace
 
 int main(int argc, const char** argv) {
-  async::Loop loop(&kAsyncLoopConfigAttachToThread);
+  async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
   trace::TraceProviderWithFdio trace_provider(loop.dispatcher());
 
   auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
