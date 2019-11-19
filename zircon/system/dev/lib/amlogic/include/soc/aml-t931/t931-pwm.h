89491ff10c91dcc7f53911734da56524ab5ec54a
Ruby Zhuang <rdzhuang@google.com>
2019-11-19 19:52:52 +0000

    [amlogic][pwm] Define register constants
    Bug:32409
    
    Change-Id: Ib5118499dd35a653603cb294ba10533c0f4a0f80

--- zircon/system/dev/lib/amlogic/include/soc/aml-t931/t931-pwm.h
+++ zircon/system/dev/lib/amlogic/include/soc/aml-t931/t931-pwm.h
@@ -5,6 +5,9 @@
 #ifndef ZIRCON_SYSTEM_DEV_LIB_AMLOGIC_INCLUDE_SOC_AML_T931_T931_PWM_H_
 #define ZIRCON_SYSTEM_DEV_LIB_AMLOGIC_INCLUDE_SOC_AML_T931_T931_PWM_H_
 
+#include <soc/aml-common/aml-pwm-regs.h>
+#include <soc/aml-t931/t931-hw.h>
+
 #define T931_PWM_A 0
 #define T931_PWM_B 1
 #define T931_PWM_C 2
@@ -16,5 +19,44 @@
 #define T931_PWM_AO_C 8
 #define T931_PWM_AO_D 9
 #define T931_PWM_COUNT 10
+
+namespace aml_pwm {
+
+// Offsets
+static_assert((T931_PWM_PWM_A == kAOffset) && (T931_PWM_PWM_C == kAOffset) &&
+                  (T931_PWM_PWM_E == kAOffset) && (T931_AO_PWM_PWM_A == kAOffset) &&
+                  (T931_AO_PWM_PWM_C == kAOffset),
+              "PWM_PWM_A offset incorrect!\n");
+static_assert((T931_PWM_PWM_B == kBOffset) && (T931_PWM_PWM_D == kBOffset) &&
+                  (T931_PWM_PWM_F == kBOffset) && (T931_AO_PWM_PWM_B == kBOffset) &&
+                  (T931_AO_PWM_PWM_D == kBOffset),
+              "PWM_PWM_B offset incorrect!\n");
+static_assert((T931_PWM_MISC_REG_AB == kMiscOffset) && (T931_PWM_MISC_REG_CD == kMiscOffset) &&
+                  (T931_PWM_MISC_REG_EF == kMiscOffset) &&
+                  (T931_AO_PWM_MISC_REG_AB == kMiscOffset) &&
+                  (T931_AO_PWM_MISC_REG_CD == kMiscOffset),
+              "MISC offset incorrect!\n");
+static_assert((T931_DS_A_B == kDSOffset) && (T931_DS_C_D == kDSOffset) &&
+                  (T931_DS_E_F == kDSOffset) && (T931_AO_DS_A_B == kDSOffset) &&
+                  (T931_AO_DS_C_D == kDSOffset),
+              "DS offset incorrect!\n");
+static_assert((T931_PWM_TIME_AB == kTimeOffset) && (T931_PWM_TIME_CD == kTimeOffset) &&
+                  (T931_PWM_TIME_EF == kTimeOffset) && (T931_AO_PWM_TIME_AB == kTimeOffset) &&
+                  (T931_AO_PWM_TIME_CD == kTimeOffset),
+              "Time offset incorrect!\n");
+static_assert((T931_PWM_A2 == kA2Offset) && (T931_PWM_C2 == kA2Offset) &&
+                  (T931_PWM_E2 == kA2Offset) && (T931_AO_PWM_A2 == kA2Offset) &&
+                  (T931_AO_PWM_C2 == kA2Offset),
+              "A2 offset incorrect!\n");
+static_assert((T931_PWM_B2 == kB2Offset) && (T931_PWM_D2 == kB2Offset) &&
+                  (T931_PWM_F2 == kB2Offset) && (T931_AO_PWM_B2 == kB2Offset) &&
+                  (T931_AO_PWM_D2 == kB2Offset),
+              "B2 offset incorrect!\n");
+static_assert((T931_PWM_BLINK_AB == kBlinkOffset) && (T931_PWM_BLINK_CD == kBlinkOffset) &&
+                  (T931_PWM_BLINK_EF == kBlinkOffset) && (T931_AO_PWM_BLINK_AB == kBlinkOffset) &&
+                  (T931_AO_PWM_BLINK_CD == kBlinkOffset),
+              "Blink offset incorrect!\n");
+
+}  // namespace aml_pwm
 
 #endif  // ZIRCON_SYSTEM_DEV_LIB_AMLOGIC_INCLUDE_SOC_AML_T931_T931_PWM_H_
