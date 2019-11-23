193abf71cf15384d9eb084deedf509ba1b8d0092
Leonard Chan <leonardchan@google.com>
2019-11-23 00:02:09 +0000

    [bma253] Fix reference binding to misaligned addresses
    UBSan complains about these in bma253-test.cc. Use memcpy() instead to
    safely access data from these misaligned addresses.
    
    Fixed: 41879
    Change-Id: Ib41a66518b29953ef10dc7c8c34fb6755c554a51

--- zircon/system/dev/input/bma253/bma253-test.cc
+++ zircon/system/dev/input/bma253/bma253-test.cc
@@ -46,9 +46,15 @@ TEST(AccelTest, InputReport) {
   EXPECT_EQ(sizeof(report), actual);
 
   EXPECT_EQ(BMA253_RPT_ID_INPUT, report.rpt_id);
-  EXPECT_EQ(0x65b, report.acceleration_x);
-  EXPECT_EQ(0xf73, report.acceleration_y);
-  EXPECT_EQ(0x390, report.acceleration_z);
+
+  // Use memcpy() to avoid a reference to a misaligned address.
+  uint16_t acceleration_x, acceleration_y, acceleration_z;
+  memcpy(&acceleration_x, &report.acceleration_x, sizeof(acceleration_x));
+  memcpy(&acceleration_y, &report.acceleration_y, sizeof(acceleration_y));
+  memcpy(&acceleration_z, &report.acceleration_z, sizeof(acceleration_z));
+  EXPECT_EQ(0x65b, acceleration_x);
+  EXPECT_EQ(0xf73, acceleration_y);
+  EXPECT_EQ(0x390, acceleration_z);
   EXPECT_EQ(0x3e, report.temperature);
 
   mock_i2c.VerifyAndClear();
@@ -70,7 +76,10 @@ TEST(AccelTest, FeatureReport) {
   EXPECT_EQ(sizeof(report), actual);
 
   EXPECT_EQ(BMA253_RPT_ID_FEATURE, report.rpt_id);
-  EXPECT_EQ(0, report.interval_ms);
+
+  uint32_t interval_ms;
+  memcpy(&interval_ms, &report.interval_ms, sizeof(interval_ms));
+  EXPECT_EQ(0, interval_ms);
 
   report.interval_ms = 1000;
 
@@ -82,7 +91,9 @@ TEST(AccelTest, FeatureReport) {
   EXPECT_EQ(sizeof(report), actual);
 
   EXPECT_EQ(BMA253_RPT_ID_FEATURE, report.rpt_id);
-  EXPECT_EQ(1000, report.interval_ms);
+
+  memcpy(&interval_ms, &report.interval_ms, sizeof(interval_ms));
+  EXPECT_EQ(1000, interval_ms);
 }
 
 TEST(AccelTest, Polling) {
@@ -120,22 +131,32 @@ TEST(AccelTest, Polling) {
 
   ASSERT_EQ(3, mock_ifc.reports().size());
 
+  uint16_t acceleration_x, acceleration_y, acceleration_z;
+  memcpy(&acceleration_x, &mock_ifc.reports()[0].acceleration_x, sizeof(acceleration_x));
+  memcpy(&acceleration_y, &mock_ifc.reports()[0].acceleration_y, sizeof(acceleration_y));
+  memcpy(&acceleration_z, &mock_ifc.reports()[0].acceleration_z, sizeof(acceleration_z));
   EXPECT_EQ(BMA253_RPT_ID_INPUT, mock_ifc.reports()[0].rpt_id);
-  EXPECT_EQ(0x65b, mock_ifc.reports()[0].acceleration_x);
-  EXPECT_EQ(0xf73, mock_ifc.reports()[0].acceleration_y);
-  EXPECT_EQ(0x390, mock_ifc.reports()[0].acceleration_z);
+  EXPECT_EQ(0x65b, acceleration_x);
+  EXPECT_EQ(0xf73, acceleration_y);
+  EXPECT_EQ(0x390, acceleration_z);
   EXPECT_EQ(0x3e, mock_ifc.reports()[0].temperature);
 
+  memcpy(&acceleration_x, &mock_ifc.reports()[1].acceleration_x, sizeof(acceleration_x));
+  memcpy(&acceleration_y, &mock_ifc.reports()[1].acceleration_y, sizeof(acceleration_y));
+  memcpy(&acceleration_z, &mock_ifc.reports()[1].acceleration_z, sizeof(acceleration_z));
   EXPECT_EQ(BMA253_RPT_ID_INPUT, mock_ifc.reports()[1].rpt_id);
-  EXPECT_EQ(0x519, mock_ifc.reports()[1].acceleration_x);
-  EXPECT_EQ(0x73c, mock_ifc.reports()[1].acceleration_y);
-  EXPECT_EQ(0xd6a, mock_ifc.reports()[1].acceleration_z);
+  EXPECT_EQ(0x519, acceleration_x);
+  EXPECT_EQ(0x73c, acceleration_y);
+  EXPECT_EQ(0xd6a, acceleration_z);
   EXPECT_EQ(0xb6, mock_ifc.reports()[1].temperature);
 
+  memcpy(&acceleration_x, &mock_ifc.reports()[2].acceleration_x, sizeof(acceleration_x));
+  memcpy(&acceleration_y, &mock_ifc.reports()[2].acceleration_y, sizeof(acceleration_y));
+  memcpy(&acceleration_z, &mock_ifc.reports()[2].acceleration_z, sizeof(acceleration_z));
   EXPECT_EQ(BMA253_RPT_ID_INPUT, mock_ifc.reports()[2].rpt_id);
-  EXPECT_EQ(0x246, mock_ifc.reports()[2].acceleration_x);
-  EXPECT_EQ(0xf26, mock_ifc.reports()[2].acceleration_y);
-  EXPECT_EQ(0x93e, mock_ifc.reports()[2].acceleration_z);
+  EXPECT_EQ(0x246, acceleration_x);
+  EXPECT_EQ(0xf26, acceleration_y);
+  EXPECT_EQ(0x93e, acceleration_z);
   EXPECT_EQ(0x72, mock_ifc.reports()[2].temperature);
 }
 
