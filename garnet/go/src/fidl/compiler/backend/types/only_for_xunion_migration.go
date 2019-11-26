f48f113e5dba8b8dbb062d4d2d66af8450c3ca3c
Pascal Perez <pascallouis@google.com>
2019-11-26 16:15:28 +0000

    [fidlgen] Conversion from union to xunion
    To work towards aligning the the union and xunion APIs, we will be
    converting unions to xunions selectively in targeted backends, before
    doing a global switch at the `fidlc` level.
    
    Test: stacked CLs, see goldens output
    Change-Id: Ia1511f153376a437d509a74bacee45079d934f11

---
+++ garnet/go/src/fidl/compiler/backend/types/only_for_xunion_migration.go
@@ -0,0 +1,49 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package types
+
+// TODO(fxb/39159): Remove post migration.
+
+// ConvertUnionToXUnion converts a union to a xunion. This should only be used
+// once we're already moved to the v1 format.
+func ConvertUnionToXUnion(union Union) XUnion {
+	var members []XUnionMember
+	for _, member := range union.Members {
+		if member.Reserved {
+			continue
+		}
+		members = append(members, XUnionMember{
+			Attributes:   member.Attributes,
+			Ordinal:      member.XUnionOrdinal,
+			Type:         member.Type,
+			Name:         member.Name,
+			Offset:       -1, // unused
+			MaxOutOfLine: -1, // unused
+		})
+	}
+	typeShape := TypeShape{
+		InlineSize:          24,
+		Alignment:           8,
+		Depth:               union.TypeShapeV1.Depth,
+		MaxHandles:          union.TypeShapeV1.MaxHandles,
+		MaxOutOfLine:        union.TypeShapeV1.MaxOutOfLine,
+		HasPadding:          union.TypeShapeV1.HasPadding,
+		HasFlexibleEnvelope: union.TypeShapeV1.HasFlexibleEnvelope,
+		ContainsUnion:       union.TypeShapeV1.ContainsUnion,
+	}
+	return XUnion{
+		Attributes:      union.Attributes,
+		Name:            union.Name,
+		Members:         members,
+		Size:            typeShape.InlineSize,
+		Alignment:       typeShape.Alignment,
+		MaxHandles:      typeShape.MaxHandles,
+		MaxOutOfLine:    typeShape.MaxOutOfLine,
+		TypeShapeOld:    typeShape,
+		TypeShapeV1:     typeShape,
+		TypeShapeV1NoEE: typeShape,
+		Strictness:      true,
+	}
+}
