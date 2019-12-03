dcaff9b532e098b43e102577524b5f72a5a5ac7c
Matt Sandy <msandy@google.com>
2019-12-03 05:06:09 +0000

    [camera] add buffer_leak_test
    This change adds a new test that repeatedly creates and destroys streams
    via the camera manager. This exposes cases where the platform or driver
    leaks buffers internally, since eventually sysmem will exhaust its
    contiguous memory budget.
    
    Test: ran the new buffer_leak_test
    Change-Id: I31c4f65f6db8080c5814cd3612f2338f73d34617

---
+++ src/camera/camera_manager2/test/buffer_leak_test.cc
@@ -0,0 +1,191 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuchsia/camera2/cpp/fidl.h>
+#include <fuchsia/sysmem/cpp/fidl.h>
+#include <lib/fzl/memory-probe.h>
+#include <lib/fzl/vmo-mapper.h>
+#include <lib/gtest/test_loop_fixture.h>
+#include <lib/sys/cpp/component_context.h>
+
+#include <gtest/gtest.h>
+#include <src/lib/syslog/cpp/logger.h>
+
+namespace camera {
+namespace {
+
+constexpr uint32_t kCampingBufferCount = 2;
+
+// If enabled, verify all mapped pages. If disabled, verifies first and last page only.
+// TODO(fxb/42252): support memory-probe range checks
+constexpr bool kEnableFullMemoryCheck = false;
+
+class BufferLeakTest : public gtest::TestLoopFixture {
+ protected:
+  void SetUp() override {
+    context_ = sys::ComponentContext::Create();
+    ASSERT_EQ(context_->svc()->Connect(allocator_.NewRequest()), ZX_OK);
+    allocator_.set_error_handler(MakeErrorHandler("Sysmem Allocator"));
+    RunLoopUntilIdle();
+  }
+
+  void TearDown() override {
+    allocator_ = nullptr;
+    context_ = nullptr;
+    RunLoopUntilIdle();
+  }
+
+  static fuchsia::camera2::StreamConstraints GetStreamConstraints() {
+    fuchsia::camera2::StreamConstraints constraints;
+    fuchsia::camera2::StreamProperties properties;
+    properties.set_stream_type(fuchsia::camera2::CameraStreamType::FULL_RESOLUTION);
+    constraints.set_properties(std::move(properties));
+    return constraints;
+  }
+
+  static fuchsia::sysmem::BufferCollectionConstraints GetCollectionConstraints() {
+    fuchsia::sysmem::BufferCollectionConstraints constraints;
+    constraints.min_buffer_count_for_camping = kCampingBufferCount;
+    constraints.image_format_constraints_count = 0;
+    constraints.usage.cpu = fuchsia::sysmem::cpuUsageRead | fuchsia::sysmem::cpuUsageWrite;
+    return constraints;
+  }
+
+  static fit::function<void(zx_status_t status)> MakeErrorHandler(std::string server) {
+    return [server](zx_status_t status) {
+      ADD_FAILURE() << server << " server disconnected - " << status;
+    };
+  }
+
+  // Verify whether the process has access to the memory reported by buffers consistent with format.
+  static void VerifyAccess(const fuchsia::sysmem::BufferCollectionInfo_2& buffers,
+                           const fuchsia::sysmem::ImageFormat_2& format) {
+    fzl::VmoMapper mapper;
+    for (uint32_t i = 0; i < buffers.buffer_count; ++i) {
+      uint32_t bad_page_reads = 0;
+      uint32_t bad_page_writes = 0;
+      ASSERT_EQ(mapper.Map(buffers.buffers[i].vmo, buffers.buffers[i].vmo_usable_start,
+                           buffers.settings.buffer_settings.size_bytes,
+                           ZX_VM_PERM_READ | ZX_VM_PERM_WRITE),
+                ZX_OK);
+      if (kEnableFullMemoryCheck) {
+        for (uint32_t row = 0; row < format.coded_height; ++row) {
+          for (uint32_t offset = 0; offset < format.bytes_per_row; offset += PAGE_SIZE) {
+            char* test_addr = static_cast<char*>(mapper.start()) + offset;
+            if (!probe_for_read(test_addr)) {
+              ++bad_page_reads;
+            }
+            if (!probe_for_write(test_addr)) {
+              ++bad_page_writes;
+            }
+          }
+        }
+      } else {
+        char* addr_first = static_cast<char*>(mapper.start());
+        char* addr_last = addr_first + format.bytes_per_row * format.coded_height - 1;
+        if (!probe_for_read(addr_first) || !probe_for_read(addr_last)) {
+          ++bad_page_reads;
+        }
+        if (!probe_for_write(addr_first) || !probe_for_write(addr_last)) {
+          ++bad_page_writes;
+        }
+      }
+      EXPECT_EQ(bad_page_reads, 0u) << "Buffer " << i << " memory not readable";
+      EXPECT_EQ(bad_page_writes, 0u) << "Buffer " << i << " memory not writable";
+      mapper.Unmap();
+    }
+  }
+
+  std::unique_ptr<sys::ComponentContext> context_;
+  fuchsia::sysmem::AllocatorPtr allocator_;
+};
+
+TEST_F(BufferLeakTest, RepeatConnections) {
+  // Repeatedly connect to streams until the total memory allocated is at least twice the size of
+  // all physical memory.
+  uint64_t allocation_sum = 0;
+  uint64_t allocation_sum_target = zx_system_get_physmem() * 2;
+  for (uint32_t i = 0; allocation_sum < allocation_sum_target; ++i) {
+    uint32_t percent_complete = (allocation_sum * 100) / allocation_sum_target;
+    std::cout << "\rAllocated " << allocation_sum << " of " << allocation_sum_target << " bytes ("
+              << percent_complete << "%)";
+    std::cout.flush();
+
+    fuchsia::camera2::ManagerPtr manager;
+    ASSERT_EQ(context_->svc()->Connect(manager.NewRequest()), ZX_OK);
+    manager.set_error_handler(MakeErrorHandler("Camera Manager"));
+
+    fuchsia::sysmem::BufferCollectionTokenPtr token;
+    allocator_->AllocateSharedCollection(token.NewRequest());
+    token.set_error_handler(MakeErrorHandler("Token"));
+
+    fidl::InterfaceHandle<fuchsia::sysmem::BufferCollectionToken> manager_token;
+    token->Duplicate(ZX_RIGHT_SAME_RIGHTS, manager_token.NewRequest());
+
+    fuchsia::sysmem::BufferCollectionPtr collection;
+    allocator_->BindSharedCollection(std::move(token), collection.NewRequest());
+    collection.set_error_handler(MakeErrorHandler("Collection"));
+    collection->SetConstraints(true, GetCollectionConstraints());
+    bool sync_returned = false;
+    collection->Sync([&]() { sync_returned = true; });
+    while (!sync_returned) {
+      RunLoopUntilIdle();
+    }
+
+    fuchsia::camera2::StreamPtr stream;
+    fuchsia::sysmem::ImageFormat_2 format_ret;
+    bool connect_returned = false;
+    manager->ConnectToStream(0, GetStreamConstraints(), std::move(manager_token),
+                             stream.NewRequest(), [&](fuchsia::sysmem::ImageFormat_2 format) {
+                               format_ret = std::move(format);
+                               connect_returned = true;
+                             });
+
+    fuchsia::sysmem::BufferCollectionInfo_2 buffers_ret;
+    bool wait_returned = false;
+    collection->WaitForBuffersAllocated(
+        [&](zx_status_t status, fuchsia::sysmem::BufferCollectionInfo_2 buffers) {
+          if (status == ZX_ERR_NO_MEMORY) {
+            ADD_FAILURE() << "SYSMEM OUT OF MEMORY at iteration " << i << " after allocating "
+                          << allocation_sum << " bytes";
+          } else {
+            ASSERT_EQ(status, ZX_OK) << "Failed to allocate buffers";
+          }
+          buffers_ret = std::move(buffers);
+          wait_returned = true;
+        });
+
+    while (!wait_returned || !connect_returned) {
+      RunLoopUntilIdle();
+    }
+
+    collection->Close();
+
+    RunLoopUntilIdle();
+
+    ASSERT_GE(buffers_ret.buffer_count, kCampingBufferCount);
+    VerifyAccess(buffers_ret, format_ret);
+
+    allocation_sum += buffers_ret.buffer_count * buffers_ret.settings.buffer_settings.size_bytes;
+
+    bool frame_received = false;
+    stream.events().OnFrameAvailable = [&](fuchsia::camera2::FrameAvailableInfo info) {
+      stream->ReleaseFrame(info.buffer_id);
+      frame_received = true;
+    };
+    stream->Start();
+    while (!frame_received) {
+      RunLoopUntilIdle();
+    }
+
+    collection = nullptr;
+    stream = nullptr;
+
+    RunLoopUntilIdle();
+  }
+  std::cout << std::endl;
+}
+
+}  // namespace
+}  // namespace camera
