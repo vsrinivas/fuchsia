27b878159bf71d47a33a7989e784e6041f271f9c
Adam Barth <abarth@google.com>
2019-12-03 05:38:25 +0000

    [peridot] Remove //peridot/web
    This code is not needed anymore.
    
    Change-Id: I5f28d69ff44899d3724c3b9f4ba864d082ad4bf8

--- peridot/web/cloud_dashboard/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import {firebase} from './firebase';
-
-export const environment = {
-  production: true,
-  firebase: firebase
-};
--- peridot/web/cloud_dashboard/src/environments/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebase = {
-  apiKey: 'AIzaSyDzzuJILOn6riFPTXC36HlH6CEdliLapDA',
-  authDomain: 'fuchsia-ledger.firebaseapp.com',
-  databaseURL: 'https://fuchsia-ledger.firebaseio.com',
-  projectId: 'fuchsia-ledger',
-  storageBucket: 'fuchsia-ledger.appspot.com',
-  messagingSenderId: '191622714118'
-};
