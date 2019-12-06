8cedb498701965107e3ce69a001a06ba84ee4cf5
Chase Latta <chaselatta@google.com>
2019-12-06 18:32:28 +0000

    [terminal] move binary logic into a library.
    Updates the BUILD file to build a standalone library which
    is used by the terminal binary.
    
    TEST=existing tests since this just introduces a standalone library.
    
    Change-Id: I36a7f4f6992c58a7e64a3196bb62b6ab27da3b7a

---
+++ garnet/bin/terminal/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![recursion_limit = "4096"]
+
+mod key_util;
+mod pty;
+mod terminal_view;
+
+pub mod app;
--- garnet/bin/terminal/src/main.rs
+++ garnet/bin/terminal/src/main.rs
@@ -2,14 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![recursion_limit = "4096"]
-
-mod app;
-mod key_util;
-mod pty;
-mod terminal_view;
-
-use {app::TerminalAssistant, carnelian::App, failure::Error, std::env};
+use {carnelian::App, failure::Error, std::env, terminal_lib::app::TerminalAssistant};
 
 fn main() -> Result<(), Error> {
     env::set_var("RUST_BACKTRACE", "full");
