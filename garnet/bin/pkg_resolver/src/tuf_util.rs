a2e585fa6dd95e74cb5ba22f7c9a26cf43da79b9
Erick Tryzelaar <etryzelaar@google.com>
2019-12-09 19:22:59 +0000

    [pkg-resolver] Temporarily allow deprecated functions
    rust-tuf has deprecated `tuf::Client::with_pinned_root`. Since that crate
    is external, we can't switch over to the new functions in a single commit.
    This patch temporarily allows the deprecated function to simplify
    migration.
    
    Change-Id: I172a8b93b1287e67d1731f0fc33607d55122a418

--- garnet/bin/pkg_resolver/src/tuf_util.rs
+++ garnet/bin/pkg_resolver/src/tuf_util.rs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Temporarily allow deprecated functions while rust-tuf is updated
+#![allow(deprecated)]
+
 use {
     crate::cache::MerkleForError,
     chrono::{DateTime, Utc},
