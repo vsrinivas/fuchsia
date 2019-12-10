edb41d80359f02774464fe1d4e9a5b80e993b838
Carlos Pizano <cpu@google.com>
2019-12-10 18:17:43 +0000

    [fzl] memory-probe: don't kill the thread
    Instead, manipulate the program counter and
    step over the offending instruction, the code
    then simply calls zx_thread_exit.
    
    In addition, the code now asserts for internal
    failures instet of returning false, which could
    mask issues.
    
    TEST = memory_probe_tests.cc are still adequate
    
    Change-Id: I4120b652f6526490f68c76cb93c28b1df0733cea

--- zircon/system/ulib/fzl/memory-probe.cc
+++ zircon/system/ulib/fzl/memory-probe.cc
@@ -10,6 +10,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <zircon/assert.h>
+#include <zircon/syscalls/debug.h>
 #include <zircon/syscalls/exception.h>
 
 namespace {
@@ -35,14 +36,17 @@ namespace {
 extern "C" void read_thread_func(uintptr_t address, uintptr_t);
 extern "C" void write_thread_func(uintptr_t address, uintptr_t);
 
-#define PROBE_FUNC(name, insn)                              \
-  __asm__(".pushsection .text." #name ",\"ax\",%progbits\n" \
-          ".balign 4\n"                                     \
-          ".type " #name ",%function\n"                     \
-          ".cfi_startproc\n"                                \
-          #name ":\n" insn "\n" CALL_INSN " zx_thread_exit\n"\
-          ".cfi_endproc\n"                                  \
-          ".size " #name ", . - " #name "\n"                \
+#define PROBE_FUNC(name, insn)                               \
+  __asm__(".pushsection .text." #name                        \
+          ",\"ax\",%progbits\n"                              \
+          ".balign 4\n"                                      \
+          ".type " #name                                     \
+          ",%function\n"                                     \
+          ".cfi_startproc\n" #name ":\n" insn "\n" CALL_INSN \
+          " zx_thread_exit\n"                                \
+          ".cfi_endproc\n"                                   \
+          ".size " #name ", . - " #name                      \
+          "\n"                                               \
           ".popsection");
 
 #ifdef __aarch64__
@@ -52,7 +56,7 @@ extern "C" void write_thread_func(uintptr_t address, uintptr_t);
 #elif defined(__x86_64__)
 #define CALL_INSN "call"
 #define READ_PROBE_INSN "movb (%rdi), %al"
-#define WRITE_PROBE_INSN "xor %eax, %eax; movb %al, (%rdi)"
+#define WRITE_PROBE_INSN "movb %al, (%rdi)"
 #else
 #error "what machine?"
 #endif
@@ -60,35 +64,52 @@ extern "C" void write_thread_func(uintptr_t address, uintptr_t);
 PROBE_FUNC(read_thread_func, READ_PROBE_INSN)
 PROBE_FUNC(write_thread_func, WRITE_PROBE_INSN)
 
+zx_status_t advance_program_counter(const zx::thread& thread) {
+  zx_thread_state_general_regs_t regs;
+  auto status = thread.read_state(ZX_THREAD_STATE_GENERAL_REGS, &regs, sizeof(regs));
+  if (status != ZX_OK) {
+    return status;
+  }
+
+#if defined(__aarch64__)
+  regs.pc += 4u;
+#elif defined(__x86_64__)
+  regs.rip += 2u;
+#else
+#error "what machine?"
+#endif
+
+  return thread.write_state(ZX_THREAD_STATE_GENERAL_REGS, &regs, sizeof(regs));
+}
+
 bool do_probe(void (*op)(uintptr_t address, uintptr_t), uintptr_t addr) {
   // This function starts a new thread to perform the read/write test, and catches any exceptions
   // in this thread to see if it failed or not.
   zx::thread thread;
-  zx_status_t status = zx::thread::create(*zx::process::self(), "memory_probe", 12u, 0u, &thread);
-  if (status != ZX_OK)
-    return false;
+  ZX_ASSERT(zx::thread::create(*zx::process::self(), "memory_probe", 12u, 0u, &thread) == ZX_OK);
 
   alignas(16) static uint8_t thread_stack[128];
   void* stack = thread_stack + sizeof(thread_stack);
 
   zx::channel exception_channel;
-  status = thread.create_exception_channel(0, &exception_channel);
-  if (status != ZX_OK)
-    return false;
+  ZX_ASSERT(thread.create_exception_channel(0, &exception_channel) == ZX_OK);
 
   thread.start(op, stack, addr, 0);
 
-  // Wait for crash or thread completion.
+  // Wait for exception or thread completion.
   zx_signals_t signals = 0;
-  status = exception_channel.wait_one(ZX_CHANNEL_READABLE | ZX_CHANNEL_PEER_CLOSED,
-                                      zx::time::infinite(), &signals);
-  if (status != ZX_OK)
-    return false;
+  ZX_ASSERT(exception_channel.wait_one(ZX_CHANNEL_READABLE | ZX_CHANNEL_PEER_CLOSED,
+                                       zx::time::infinite(), &signals) == ZX_OK);
 
   if (signals & ZX_CHANNEL_READABLE) {
-    // Thread crashed so the operation failed. The thread is now in a suspended state and
-    // needs to be explicitly terminated.
-    thread.kill();
+    zx_exception_info_t info;
+    zx::exception exception;
+    ZX_ASSERT(exception_channel.read(0, &info, exception.reset_and_get_address(), sizeof(info), 1,
+                                     nullptr, nullptr) == ZX_OK);
+    ZX_ASSERT(info.type == ZX_EXCP_FATAL_PAGE_FAULT);
+    ZX_ASSERT(advance_program_counter(thread) == ZX_OK);
+    uint32_t state = ZX_EXCEPTION_STATE_HANDLED;
+    exception.set_property(ZX_PROP_EXCEPTION_STATE, &state, sizeof(state));
     return false;
   }
 
