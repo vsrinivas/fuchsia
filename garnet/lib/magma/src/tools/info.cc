a576a1bdd7f273f8f9269e4378608d34470a196b
John Bauman <jbauman@google.com>
2019-12-11 03:39:01 +0000

    [magma] Allow non-000 other devices from magma-info
    Support parsing command-line options to get the device path, or by
    default pick the first one enumerated in the right directly.
    
    Change-Id: Ibf2d0c7729bdb3b06815b4cc154dbc323bcc22b1

--- garnet/lib/magma/src/tools/info.cc
+++ garnet/lib/magma/src/tools/info.cc
@@ -9,18 +9,39 @@
 #include <stdlib.h>
 #include <unistd.h>  // for close
 
-const char* kGpuDeviceName = "/dev/class/gpu/000";
+#include <filesystem>
+
+#include "src/lib/fxl/command_line.h"
+
+const char* kGpuClassPath = "/dev/class/gpu";
 
 int main(int argc, char** argv) {
-  int fd = open(kGpuDeviceName, O_RDONLY);
+  auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
+
+  static const char kGpuDeviceFlag[] = "gpu-device";
+  static const char kDumpTypeFlag[] = "dump-type";
+
+  std::string gpu_device_value;
+  if (!command_line.GetOptionValue(kGpuDeviceFlag, &gpu_device_value)) {
+    for (auto& p : std::filesystem::directory_iterator(kGpuClassPath)) {
+      gpu_device_value = p.path();
+    }
+    if (gpu_device_value.empty()) {
+      fprintf(stderr, "No magma device found\n");
+      return -1;
+    }
+  }
+  printf("Opening magma device: %s\n", gpu_device_value.c_str());
+  int fd = open(gpu_device_value.c_str(), O_RDONLY);
   if (fd < 0) {
-    printf("Failed to open magma device %s\n", kGpuDeviceName);
+    printf("Failed to open magma device %s\n", gpu_device_value.c_str());
     return -1;
   }
 
   uint32_t dump_type = 0;
-  if (argc >= 2) {
-    dump_type = atoi(argv[1]);
+  std::string dump_type_string;
+  if (command_line.GetOptionValue(kDumpTypeFlag, &dump_type_string)) {
+    dump_type = atoi(dump_type_string.c_str());
   }
 
   fdio_t* fdio = fdio_unsafe_fd_to_io(fd);
