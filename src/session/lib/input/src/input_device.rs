1b47c24d8ec2339d78a2f0771ccda1b31919fe4e
Kevin Lindkvist <lindkvist@google.com>
2019-12-13 04:06:11 +0000

    [session][input] Remove duplicate function
    Change-Id: I99b0828e84c6d5d2f61d8c878f68158ad9463f59

--- src/session/lib/input/src/input_device.rs
+++ src/session/lib/input/src/input_device.rs
@@ -114,6 +114,30 @@ pub trait InputDeviceBinding: Sized {
     }
 }
 
+/// Returns true if the device type of `input_device` matches `device_type`.
+///
+/// # Parameters
+/// - `input_device`: The InputDevice to check the type of.
+/// - `device_type`: The type of the device to compare to.
+pub async fn is_device_type(
+    input_device: &fidl_fuchsia_input_report::InputDeviceProxy,
+    device_type: InputDeviceType,
+) -> bool {
+    let device_descriptor = match input_device.get_descriptor().await {
+        Ok(descriptor) => descriptor,
+        Err(_) => {
+            return false;
+        }
+    };
+
+    // Return if the device type matches the desired `device_type`.
+    match device_type {
+        InputDeviceType::Mouse => device_descriptor.mouse.is_some(),
+        InputDeviceType::Touch => device_descriptor.touch.is_some(),
+        InputDeviceType::Keyboard => device_descriptor.keyboard.is_some(),
+    }
+}
+
 /// Returns all the devices of the given device type.
 ///
 /// # Parameters
@@ -151,30 +175,6 @@ pub enum InputDeviceType {
 /// The path to the input-report directory.
 static INPUT_REPORT_PATH: &str = "/dev/class/input-report";
 
-/// Returns true if the device type of `input_device` matches `device_type`.
-///
-/// # Parameters
-/// - `input_device`: The InputDevice to check the type of.
-/// - `device_type`: The type of the device to compare to.
-async fn is_device_type(
-    input_device: &fidl_fuchsia_input_report::InputDeviceProxy,
-    device_type: InputDeviceType,
-) -> bool {
-    let device_descriptor = match input_device.get_descriptor().await {
-        Ok(descriptor) => descriptor,
-        Err(_) => {
-            return false;
-        }
-    };
-
-    // Return if the device type matches the desired `device_type`.
-    match device_type {
-        InputDeviceType::Mouse => device_descriptor.mouse.is_some(),
-        InputDeviceType::Touch => device_descriptor.touch.is_some(),
-        InputDeviceType::Keyboard => device_descriptor.keyboard.is_some(),
-    }
-}
-
 /// Returns a proxy to the InputDevice in `entry` if it exists.
 ///
 /// # Parameters
@@ -193,4 +193,225 @@ fn get_device_from_dir_entry_path(
     let (input_device, server) = fidl::endpoints::create_proxy::<InputDeviceMarker>()?;
     fdio::service_connect(input_device_path.unwrap(), server.into_channel())?;
     Ok(input_device)
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*, fidl::endpoints::create_proxy_and_stream, fuchsia_async as fasync,
+        futures::prelude::*,
+    };
+
+    /// Spawns a local `fidl_fuchsia_input_report::InputDevice` server, and returns a proxy to the
+    /// spawned server.
+    /// The provided `request_handler` is notified when an incoming request is received.
+    ///
+    /// # Parameters
+    /// - `request_handler`: A function which is called with incoming requests to the spawned
+    ///                      `InputDevice` server.
+    /// # Returns
+    /// A `InputDeviceProxy` to the spawned server.
+    fn spawn_input_device_server<F: 'static>(
+        request_handler: F,
+    ) -> fidl_fuchsia_input_report::InputDeviceProxy
+    where
+        F: Fn(fidl_fuchsia_input_report::InputDeviceRequest) + Send,
+    {
+        let (input_device_proxy, mut input_device_server) =
+            create_proxy_and_stream::<fidl_fuchsia_input_report::InputDeviceMarker>()
+                .expect("Failed to create InputDevice proxy and server.");
+
+        fasync::spawn(async move {
+            while let Some(input_device_request) = input_device_server.try_next().await.unwrap() {
+                request_handler(input_device_request);
+            }
+        });
+
+        input_device_proxy
+    }
+
+    // Tests that is_device_type() returns true for InputDeviceType::Mouse when a mouse exists.
+    #[fasync::run_singlethreaded(test)]
+    async fn mouse_input_device_exists() {
+        let input_device_proxy =
+            spawn_input_device_server(move |input_device_request| match input_device_request {
+                fidl_fuchsia_input_report::InputDeviceRequest::GetDescriptor { responder } => {
+                    let _ = responder.send(fidl_fuchsia_input_report::DeviceDescriptor {
+                        device_info: None,
+                        mouse: Some(fidl_fuchsia_input_report::MouseDescriptor {
+                            movement_x: None,
+                            movement_y: None,
+                            scroll_v: None,
+                            scroll_h: None,
+                            buttons: None,
+                        }),
+                        sensor: None,
+                        touch: None,
+                        keyboard: None,
+                    });
+                }
+                _ => {
+                    assert!(false);
+                }
+            });
+
+        assert!(is_device_type(&input_device_proxy, InputDeviceType::Mouse).await);
+    }
+
+    // Tests that is_device_type() returns true for InputDeviceType::Mouse when a mouse doesn't
+    // exist.
+    #[fasync::run_singlethreaded(test)]
+    async fn mouse_input_device_doesnt_exist() {
+        let input_device_proxy =
+            spawn_input_device_server(move |input_device_request| match input_device_request {
+                fidl_fuchsia_input_report::InputDeviceRequest::GetDescriptor { responder } => {
+                    let _ = responder.send(fidl_fuchsia_input_report::DeviceDescriptor {
+                        device_info: None,
+                        mouse: None,
+                        sensor: None,
+                        touch: None,
+                        keyboard: None,
+                    });
+                }
+                _ => {
+                    assert!(false);
+                }
+            });
+
+        assert!(!is_device_type(&input_device_proxy, InputDeviceType::Mouse).await);
+    }
+
+    // Tests that is_device_type() returns true for InputDeviceType::Touch when a touchscreen
+    // exists.
+    #[fasync::run_singlethreaded(test)]
+    async fn touch_input_device_exists() {
+        let input_device_proxy =
+            spawn_input_device_server(move |input_device_request| match input_device_request {
+                fidl_fuchsia_input_report::InputDeviceRequest::GetDescriptor { responder } => {
+                    let _ = responder.send(fidl_fuchsia_input_report::DeviceDescriptor {
+                        device_info: None,
+                        mouse: None,
+                        sensor: None,
+                        touch: Some(fidl_fuchsia_input_report::TouchDescriptor {
+                            contacts: None,
+                            max_contacts: None,
+                            touch_type: None,
+                        }),
+                        keyboard: None,
+                    });
+                }
+                _ => {
+                    assert!(false);
+                }
+            });
+
+        assert!(is_device_type(&input_device_proxy, InputDeviceType::Touch).await);
+    }
+
+    // Tests that is_device_type() returns true for InputDeviceType::Touch when a touchscreen
+    // exists.
+    #[fasync::run_singlethreaded(test)]
+    async fn touch_input_device_doesnt_exist() {
+        let input_device_proxy =
+            spawn_input_device_server(move |input_device_request| match input_device_request {
+                fidl_fuchsia_input_report::InputDeviceRequest::GetDescriptor { responder } => {
+                    let _ = responder.send(fidl_fuchsia_input_report::DeviceDescriptor {
+                        device_info: None,
+                        mouse: None,
+                        sensor: None,
+                        touch: None,
+                        keyboard: None,
+                    });
+                }
+                _ => {
+                    assert!(false);
+                }
+            });
+
+        assert!(!is_device_type(&input_device_proxy, InputDeviceType::Touch).await);
+    }
+
+    // Tests that is_device_type() returns true for InputDeviceType::Keyboard when a keyboard
+    // exists.
+    #[fasync::run_singlethreaded(test)]
+    async fn keyboard_input_device_exists() {
+        let input_device_proxy =
+            spawn_input_device_server(move |input_device_request| match input_device_request {
+                fidl_fuchsia_input_report::InputDeviceRequest::GetDescriptor { responder } => {
+                    let _ = responder.send(fidl_fuchsia_input_report::DeviceDescriptor {
+                        device_info: None,
+                        mouse: None,
+                        sensor: None,
+                        touch: None,
+                        keyboard: Some(fidl_fuchsia_input_report::KeyboardDescriptor {
+                            keys: None,
+                        }),
+                    });
+                }
+                _ => {
+                    assert!(false);
+                }
+            });
+
+        assert!(is_device_type(&input_device_proxy, InputDeviceType::Keyboard).await);
+    }
+
+    // Tests that is_device_type() returns true for InputDeviceType::Keyboard when a keyboard
+    // exists.
+    #[fasync::run_singlethreaded(test)]
+    async fn keyboard_input_device_doesnt_exist() {
+        let input_device_proxy =
+            spawn_input_device_server(move |input_device_request| match input_device_request {
+                fidl_fuchsia_input_report::InputDeviceRequest::GetDescriptor { responder } => {
+                    let _ = responder.send(fidl_fuchsia_input_report::DeviceDescriptor {
+                        device_info: None,
+                        mouse: None,
+                        sensor: None,
+                        touch: None,
+                        keyboard: None,
+                    });
+                }
+                _ => {
+                    assert!(false);
+                }
+            });
+
+        assert!(!is_device_type(&input_device_proxy, InputDeviceType::Keyboard).await);
+    }
+
+    // Tests that is_device_type() returns true for every input device type that exists.
+    #[fasync::run_singlethreaded(test)]
+    async fn no_input_device_match() {
+        let input_device_proxy =
+            spawn_input_device_server(move |input_device_request| match input_device_request {
+                fidl_fuchsia_input_report::InputDeviceRequest::GetDescriptor { responder } => {
+                    let _ = responder.send(fidl_fuchsia_input_report::DeviceDescriptor {
+                        device_info: None,
+                        mouse: Some(fidl_fuchsia_input_report::MouseDescriptor {
+                            movement_x: None,
+                            movement_y: None,
+                            scroll_v: None,
+                            scroll_h: None,
+                            buttons: None,
+                        }),
+                        sensor: None,
+                        touch: Some(fidl_fuchsia_input_report::TouchDescriptor {
+                            contacts: None,
+                            max_contacts: None,
+                            touch_type: None,
+                        }),
+                        keyboard: Some(fidl_fuchsia_input_report::KeyboardDescriptor {
+                            keys: None,
+                        }),
+                    });
+                }
+                _ => {
+                    assert!(false);
+                }
+            });
+
+        assert!(is_device_type(&input_device_proxy, InputDeviceType::Mouse).await);
+        assert!(is_device_type(&input_device_proxy, InputDeviceType::Touch).await);
+        assert!(is_device_type(&input_device_proxy, InputDeviceType::Keyboard).await);
+    }
 }
