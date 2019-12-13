4c058f03e763cf4646fb39543640c4143609f9c1
Nathaniel Manista <nathaniel@google.com>
2019-12-13 15:57:31 +0000

    [ledger] Fix set_when_called.h includes
    This content should have been included in ab16513cdca68c682725d157d4.
    
    Change-Id: Ibeb388b974058a9cc5456acf6c3ad527ed40f9df

--- src/ledger/lib/callback/set_when_called_unittest.cc
+++ src/ledger/lib/callback/set_when_called_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "set_when_called.h"
+#include "src/ledger/lib/callback/set_when_called.h"
 
 #include <gtest/gtest.h>
 
