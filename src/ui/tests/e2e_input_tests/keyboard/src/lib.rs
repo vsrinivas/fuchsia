c4c6db4422b8792c6ad4924332b3440a35d9d42e
Victor Donich <viktard@google.com>
2019-12-13 02:08:19 +0000

    [input][keyboard] Mark keyboard_e2e_lib_test as non-flaky
    MULTIPLY: `[
      {
        "target": "keyboard_e2e_lib_test",
        "os": "fuchsia",
        "total_runs": 500
      }
    ]`
    
    Bug: 40902
    Change-Id: Ic100d6d0ea1a3e0912416aa1beac2f6efc01ccdb

--- src/ui/tests/e2e_input_tests/keyboard/src/lib.rs
+++ src/ui/tests/e2e_input_tests/keyboard/src/lib.rs
@@ -4,70 +4,57 @@
 
 #![cfg(test)]
 use failure::{Error, ResultExt};
+use fidl::endpoints::create_request_stream;
 use fidl_fuchsia_ui_input2 as ui_input;
-use fidl_fuchsia_ui_policy as ui_policy;
-use fidl_fuchsia_ui_scenic as ui_scenic;
 use fidl_fuchsia_ui_views as ui_views;
 use fuchsia_async;
 use fuchsia_component::client::connect_to_service;
-use fuchsia_scenic as scenic;
 use fuchsia_zircon as zx;
 use futures::StreamExt;
 use input_synthesis;
 use std::time::Duration;
+use ui_test_tools;
 
 static HID_KEY_A: u32 = 0x04;
 
-#[test]
-fn keyboard_events() -> Result<(), Error> {
+#[fuchsia_async::run_singlethreaded(test)]
+async fn keyboard_events() -> Result<(), Error> {
     fuchsia_syslog::init_with_tags(&["keyboard"])
         .expect("keyboard e2e syslog init should not fail");
 
-    let mut executor = fuchsia_async::Executor::new()?;
-
-    let presenter = connect_to_service::<ui_policy::PresenterMarker>()
-        .context("Failed to connect to Presentation service")?;
+    // When service goes out of scope, dependent services will be disconnected.
+    let _service = ui_test_tools::setup(ui_test_tools::EnvironmentType::ScenicWithRootPresenter)
+        .await
+        .context("ui_test_tools::setup")?;
 
     let keyboard = connect_to_service::<ui_input::KeyboardMarker>()
         .context("Failed to connect to Keyboard service")?;
 
     let (listener_client_end, mut listener_stream) =
-        fidl::endpoints::create_request_stream::<ui_input::KeyListenerMarker>()?;
+        create_request_stream::<ui_input::KeyListenerMarker>()?;
 
     let (raw_event_pair, _) = zx::EventPair::create()?;
     let view_ref = &mut ui_views::ViewRef { reference: raw_event_pair };
 
-    executor.run_singlethreaded(async move {
-        // Set listener and view ref.
-        keyboard.set_listener(view_ref, listener_client_end).await.expect("set_listener");
+    // Set listener and view ref.
+    keyboard.set_listener(view_ref, listener_client_end).await.expect("set_listener");
 
-        let mut token_pair = scenic::ViewTokenPair::new().expect("create ViewTokenPair");
+    input_synthesis::keyboard_event_command(HID_KEY_A, Duration::from_millis(0))
+        .await
+        .expect("key_event_command injects input");
 
-        presenter.present_view(&mut token_pair.view_holder_token, None).expect("present_view");
-
-        let scenic = connect_to_service::<ui_scenic::ScenicMarker>().expect("connect to Scenic");
-        scenic.get_display_info().await.expect("get_display_info");
-
-        // Scenic setup complete, start test.
-
-        input_synthesis::keyboard_event_command(HID_KEY_A, Duration::from_millis(0))
-            .await
-            .expect("key_event_command injects input");
-
-        // Wait for key listener callback to be activated.
-        match listener_stream.next().await {
-            Some(Ok(ui_input::KeyListenerRequest::OnKeyEvent { event, responder, .. })) => {
-                assert_eq!(event.key, Some(ui_input::Key::A));
-                responder.send(ui_input::Status::Handled).expect("responding from key listener")
-            }
-            Some(Err(e)) => {
-                panic!("Error from listener_stream.next(): {:}", e);
-            }
-            None => {
-                panic!("Error from listener_stream.next(): empty stream");
-            }
+    // Wait for key listener callback to be activated.
+    match listener_stream.next().await {
+        Some(Ok(ui_input::KeyListenerRequest::OnKeyEvent { event, responder, .. })) => {
+            assert_eq!(event.key, Some(ui_input::Key::A));
+            responder.send(ui_input::Status::Handled).expect("responding from key listener")
         }
-    });
-
+        Some(Err(e)) => {
+            panic!("Error from listener_stream.next(): {:}", e);
+        }
+        None => {
+            panic!("Error from listener_stream.next(): empty stream");
+        }
+    }
     Ok(())
 }
---
+++ src/ui/tests/e2e_input_tests/keyboard/ui-test-tools/lib.rs
@@ -0,0 +1,106 @@
+// 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use failure::{Error, ResultExt};
+use fidl::endpoints::{create_proxy, create_request_stream};
+use fidl_fuchsia_ui_gfx as ui_gfx;
+use fidl_fuchsia_ui_policy as ui_policy;
+use fidl_fuchsia_ui_scenic as ui_scenic;
+use fuchsia_component::client::connect_to_service;
+use fuchsia_scenic::{EntityNode, Session, SessionPtr, View, ViewTokenPair};
+use fuchsia_zircon::{ClockId, Time};
+use futures::TryStreamExt;
+
+/// Type of the e2e testing environment to setup.
+pub enum EnvironmentType {
+    /// Start Scenic and RootPresenter and wait for complete initialization.
+    ScenicWithRootPresenter,
+}
+
+/// Provides setup and utility methods for UI e2e tests that use `Scenic` and `RootPresenter`.
+pub struct Service {
+    session: SessionPtr,
+    presenter: ui_policy::PresenterProxy,
+    #[allow(dead_code)]
+    scenic: ui_scenic::ScenicProxy,
+    session_listener_stream: ui_scenic::SessionListenerRequestStream,
+}
+
+impl Service {
+    fn new(_environment: EnvironmentType) -> Result<Service, Error> {
+        let presenter = connect_to_service::<ui_policy::PresenterMarker>()
+            .context("connect to Presentation")?;
+
+        let scenic =
+            connect_to_service::<ui_scenic::ScenicMarker>().context("connect to Scenic")?;
+        let (listener_client_end, session_listener_stream) =
+            create_request_stream::<ui_scenic::SessionListenerMarker>()
+                .context("create_request_stream")?;
+
+        let (session_proxy, session_request) = create_proxy().context("create_proxy")?;
+        scenic
+            .create_session(session_request, Some(listener_client_end))
+            .context("create_session")?;
+
+        let session = Session::new(session_proxy);
+
+        Ok(Service { presenter, scenic, session, session_listener_stream })
+    }
+
+    async fn present(&self) -> Result<(), Error> {
+        let presentation_time = Time::get(ClockId::Monotonic).into_nanos() as u64;
+        self.session.lock().present(presentation_time).await.context("session present")?;
+        Ok(())
+    }
+
+    async fn view_holder_connected(&mut self) -> Result<(), Error> {
+        while let Some(msg) =
+            self.session_listener_stream.try_next().await.context("SessionListener event")?
+        {
+            match msg {
+                ui_scenic::SessionListenerRequest::OnScenicEvent { events, .. } => {
+                    for event in events.iter() {
+                        if let ui_scenic::Event::Gfx(ui_gfx::Event::ViewHolderConnected(
+                            ui_gfx::ViewHolderConnectedEvent { .. },
+                        )) = event
+                        {
+                            return Ok(())
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+        Ok(())
+    }
+}
+
+/// Setup UI e2e testing environment and wait for full setup of dependent services.
+/// Resolves when the setup is complete, returning an instance of `Service`.
+/// When instance of returned `Service` goes out of scope, dependent FIDL services will
+/// be disconnected.
+pub async fn setup(environment: EnvironmentType) -> Result<Service, Error> {
+    let mut service = Service::new(environment)?;
+
+    // Add a View and a root Entity as a child.
+    let mut token_pair = ViewTokenPair::new()?;
+    service
+        .presenter
+        .present_view(&mut token_pair.view_holder_token, None)
+        .context("present_view")?;
+
+    let root_node = EntityNode::new(service.session.clone());
+
+    let view =
+        View::new(service.session.clone(), token_pair.view_token, Some(String::from("root view")));
+    view.add_child(&root_node);
+
+    service.present().await?;
+    service.view_holder_connected().await.context("view_holder_connected")?;
+
+    // Second present is required because of http://fxb/42737
+    service.present().await?;
+
+    Ok(service)
+}
