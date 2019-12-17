0f9760c7f23aa653c2ef4391d02d6d11cb0bb2fd
David Greenaway <dgreenaway@google.com>
2019-12-17 22:26:05 +0000

    [psutils] Move more of 'ps' implementation into separate file.
    Move the majority of "ps" into "ps_internal", which will allow more
    of the implementation to be tested in unit tests.
    
    All that remains in "ps.c" is flag parsing. No real code changes.
    
    Bug: 42417
    Change-Id: Ic61b54d1fcdaba98d2c820bdef693f5c288bed7c

--- zircon/system/uapp/psutils/ps_internal.c
+++ zircon/system/uapp/psutils/ps_internal.c
@@ -4,10 +4,20 @@
 
 #include "ps_internal.h"
 
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <zircon/status.h>
+#include <zircon/syscalls/exception.h>
+#include <zircon/syscalls.h>
+#include <zircon/syscalls/object.h>
 
 #include <pretty/sizes.h>
+
+#include <task-utils/get.h>
+#include <task-utils/walker.h>
 
 void print_header(int id_w, const ps_options_t* options, FILE* out) {
   if (options->also_show_threads) {
@@ -75,3 +85,167 @@ void print_table(task_table_t* table, const ps_options_t* options, FILE* out) {
   print_header(id_w, options, out);
 }
 
+// Adds a task entry to the specified table. |*entry| is copied.
+// Returns a pointer to the new table entry.
+task_entry_t* add_entry(task_table_t* table, const task_entry_t* entry) {
+  if (table->num_entries + 1 >= table->capacity) {
+    size_t new_cap = table->capacity * 2;
+    if (new_cap < 128) {
+      new_cap = 128;
+    }
+    table->entries = realloc(table->entries, new_cap * sizeof(*entry));
+    table->capacity = new_cap;
+  }
+  table->entries[table->num_entries] = *entry;
+  return table->entries + table->num_entries++;
+}
+
+// The array of tasks built by the callbacks.
+static task_table_t tasks = {};
+
+// The current stack of ancestor jobs, indexed by depth.
+// process_callback may touch any entry whose depth is less that its own.
+#define JOB_STACK_SIZE 128
+static task_entry_t* job_stack[JOB_STACK_SIZE];
+
+// Adds a job's information to |tasks|.
+static zx_status_t job_callback(void* ctx, int depth, zx_handle_t job, zx_koid_t koid,
+                                zx_koid_t parent_koid) {
+  task_entry_t e = {.type = 'j', .depth = depth};
+  zx_status_t status = zx_object_get_property(job, ZX_PROP_NAME, e.name, sizeof(e.name));
+  if (status != ZX_OK) {
+    // This will abort walk_job_tree(), so we don't need to worry
+    // about job_stack.
+    return status;
+  }
+
+  zx_info_job_t info;
+  status = zx_object_get_info(job, ZX_INFO_JOB, &info, sizeof(info), NULL, NULL);
+  if (status != ZX_OK) {
+    return status;
+  }
+  if (info.kill_on_oom) {
+    snprintf(e.state_str, sizeof(e.state_str), "killoom");
+  }
+
+  snprintf(e.koid_str, sizeof(e.koid_str), "%" PRIu64, koid);
+  snprintf(e.parent_koid_str, sizeof(e.koid_str), "%" PRIu64, parent_koid);
+
+  // Put our entry pointer on the job stack so our descendants can find us.
+  assert(depth < JOB_STACK_SIZE);
+  job_stack[depth] = add_entry(&tasks, &e);
+  return ZX_OK;
+}
+
+// Adds a process's information to |tasks|.
+static zx_status_t process_callback(void* ctx, int depth, zx_handle_t process, zx_koid_t koid,
+                                    zx_koid_t parent_koid) {
+  task_entry_t e = {.type = 'p', .depth = depth};
+  zx_status_t status = zx_object_get_property(process, ZX_PROP_NAME, e.name, sizeof(e.name));
+  if (status != ZX_OK) {
+    return status;
+  }
+  zx_info_task_stats_t info;
+  status = zx_object_get_info(process, ZX_INFO_TASK_STATS, &info, sizeof(info), NULL, NULL);
+  if (status == ZX_ERR_BAD_STATE) {
+    // Process has exited, but has not been destroyed.
+    // Default to zero for all sizes.
+  } else if (status != ZX_OK) {
+    return status;
+  } else {
+    e.private_bytes = info.mem_private_bytes;
+    e.shared_bytes = info.mem_shared_bytes;
+    e.pss_bytes = info.mem_private_bytes + info.mem_scaled_shared_bytes;
+
+    // Update our ancestor jobs.
+    assert(depth > 0);
+    assert(depth < JOB_STACK_SIZE);
+    for (int i = 0; i < depth; i++) {
+      task_entry_t* job = job_stack[i];
+      job->pss_bytes += e.pss_bytes;
+      job->private_bytes += e.private_bytes;
+      // shared_bytes doesn't mean much as a sum, so leave it at zero.
+    }
+  }
+
+  if (((ps_options_t*)ctx)->only_show_jobs) {
+    return ZX_OK;
+  }
+
+  snprintf(e.koid_str, sizeof(e.koid_str), "%" PRIu64, koid);
+  snprintf(e.parent_koid_str, sizeof(e.koid_str), "%" PRIu64, parent_koid);
+  add_entry(&tasks, &e);
+
+  return ZX_OK;
+}
+
+// Return text representation of thread state.
+static const char* state_string(const zx_info_thread_t* info) {
+  if (info->wait_exception_channel_type != ZX_EXCEPTION_CHANNEL_TYPE_NONE) {
+    return "excp";
+  } else {
+    switch (ZX_THREAD_STATE_BASIC(info->state)) {
+      case ZX_THREAD_STATE_NEW:
+        return "new";
+      case ZX_THREAD_STATE_RUNNING:
+        return "running";
+      case ZX_THREAD_STATE_SUSPENDED:
+        return "susp";
+      case ZX_THREAD_STATE_BLOCKED:
+        return "blocked";
+      case ZX_THREAD_STATE_DYING:
+        return "dying";
+      case ZX_THREAD_STATE_DEAD:
+        return "dead";
+      default:
+        return "???";
+    }
+  }
+}
+
+// Adds a thread's information to |tasks|.
+static zx_status_t thread_callback(void* ctx, int depth, zx_handle_t thread, zx_koid_t koid,
+                                   zx_koid_t parent_koid) {
+  if (!((ps_options_t*)ctx)->also_show_threads) {
+    // TODO(cpu): Should update ancestor process with number of threads.
+    return ZX_OK;
+  }
+
+  task_entry_t e = {.type = 't', .depth = depth};
+  zx_status_t status = zx_object_get_property(thread, ZX_PROP_NAME, e.name, sizeof(e.name));
+  if (status != ZX_OK) {
+    return status;
+  }
+  zx_info_thread_t info;
+  status = zx_object_get_info(thread, ZX_INFO_THREAD, &info, sizeof(info), NULL, NULL);
+  if (status != ZX_OK) {
+    return status;
+  }
+  // TODO: Print thread stack size in one of the memory usage fields?
+  snprintf(e.koid_str, sizeof(e.koid_str), "%" PRIu64, koid);
+  snprintf(e.parent_koid_str, sizeof(e.koid_str), "%" PRIu64, parent_koid);
+  snprintf(e.state_str, sizeof(e.state_str), "%s", state_string(&info));
+  add_entry(&tasks, &e);
+  return ZX_OK;
+}
+
+zx_status_t show_all_jobs(ps_options_t* options) {
+  zx_status_t status = walk_root_job_tree(job_callback, process_callback, thread_callback, options);
+  if (status != ZX_OK) {
+    return status;
+  }
+  print_table(&tasks, options, stdout);
+  free(tasks.entries);
+  return ZX_OK;
+}
+
+zx_status_t show_job_tree(zx_handle_t target_job, ps_options_t* options) {
+  zx_status_t status =
+      walk_job_tree(target_job, job_callback, process_callback, thread_callback, &options);
+  if (status != ZX_OK) {
+    return status;
+  }
+  print_table(&tasks, options, stdout);
+  free(tasks.entries);
+  return ZX_OK;
+}
