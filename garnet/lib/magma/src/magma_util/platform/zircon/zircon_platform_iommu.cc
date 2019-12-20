6b0b5d1d8adc9b87c3a903ea7016e5667d4aaa7a
Craig Stout <cstout@google.com>
2019-12-20 02:50:05 +0000

    [magma] Add ZirconPlatformIommu
    Just stubs, to enable fuchsia build of msd-qcom-adreno.
    
    Change-Id: Ie3afecbf68e2d05c98344b8ac8e7c2d704e8d610

---
+++ garnet/lib/magma/src/magma_util/platform/zircon/zircon_platform_iommu.cc
@@ -0,0 +1,25 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform_iommu.h"
+
+namespace magma {
+
+class ZirconPlatformIommu : public PlatformIommu {
+ public:
+  bool Map(uint64_t gpu_addr, PlatformBusMapper::BusMapping* bus_mapping) const override {
+    return DRETF(false, "Map not implemented");
+  }
+
+  bool Unmap(uint64_t gpu_addr, PlatformBusMapper::BusMapping* bus_mapping) const override {
+    return DRETF(false, "Unmap not implemented");
+  }
+};
+
+std::unique_ptr<PlatformIommu> PlatformIommu::Create(
+    std::unique_ptr<PlatformHandle> iommu_connector) {
+  return std::make_unique<ZirconPlatformIommu>();
+}
+
+}  // namespace magma
