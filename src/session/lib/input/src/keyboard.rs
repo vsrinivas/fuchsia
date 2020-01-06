438c838e017f0e20b98f9fc3772c7d8708a2c476
Kevin Lindkvist <lindkvist@google.com>
2020-01-06 23:07:30 +0000

    [session][input] Add keyboard state tracking
    This adds the ability to track key presses accross keyboard reports.
    
    Change-Id: I5bceb14da03ff003e2d98ecf80447c322a56777c

--- src/session/lib/input/src/keyboard.rs
+++ src/session/lib/input/src/keyboard.rs
@@ -8,16 +8,43 @@ use {
     anyhow::{format_err, Error},
     async_trait::async_trait,
     fidl_fuchsia_input_report::{InputDeviceProxy, InputReport},
+    fidl_fuchsia_ui_input2::{Key, KeyEventPhase, Modifiers},
     fuchsia_async as fasync,
-    futures::channel::mpsc::{Receiver, Sender},
-    futures::StreamExt,
+    fuchsia_syslog::fx_log_err,
+    futures::{
+        channel::mpsc::{Receiver, Sender},
+        SinkExt, StreamExt,
+    },
+    maplit::hashmap,
+    std::collections::HashMap,
 };
 
-#[derive(Copy, Clone)]
-pub struct KeyboardInputMessage {}
+/// A [`KeyboardInputMessage`] represents an input event from a keyboard device.
+///
+/// The input message contains information about which keys are pressed, and which
+/// keys were released.
+///
+/// Clients can expect the following sequence of events for a given key:
+///
+/// 1. [`KeyEventPhase::Pressed`]
+/// 2. [`KeyEventPhase::Released`]
+///
+/// No duplicate [`KeyEventPhase::Pressed`] events will be sent for keys, even if the
+/// key is present in a subsequent [`InputReport`]. Clients can assume that
+/// a key is pressed for all received input messages until the key is present in
+/// the [`KeyEventPhase::Released`] entry of [`keys`].
+pub struct KeyboardInputMessage {
+    /// The keys associated with this input message, sorted by their `KeyEventPhase`
+    /// (e.g., whether they are pressed or released).
+    pub keys: HashMap<KeyEventPhase, Vec<Key>>,
+}
 
-#[derive(Copy, Clone)]
-pub struct KeyboardDescriptor {}
+/// A [`KeyboardDescriptor`] contains information about a specific keyboard device.
+#[derive(Clone)]
+pub struct KeyboardDescriptor {
+    /// All the keys available on the keyboard device.
+    pub keys: Vec<Key>,
+}
 
 /// A [`KeyboardBinding`] represents a connection to a keyboard input device.
 ///
@@ -55,15 +82,37 @@ impl input_device::InputDeviceBinding for KeyboardBinding {
     }
 
     fn get_descriptor(&self) -> input_device::InputDescriptor {
-        input_device::InputDescriptor::Keyboard(self.descriptor)
+        input_device::InputDescriptor::Keyboard(self.descriptor.clone())
     }
 
     fn process_reports(
         report: InputReport,
-        _previous_report: Option<InputReport>,
+        previous_report: Option<InputReport>,
         _device_descriptor: &mut input_device::InputDescriptor,
-        _input_message_sender: &mut Sender<input_device::InputMessage>,
+        input_message_sender: &mut Sender<input_device::InputMessage>,
     ) -> Option<InputReport> {
+        let new_keys = match KeyboardBinding::parse_pressed_keys(&report) {
+            Some(keys) => keys,
+            None => {
+                // It's OK for the report to contain an empty vector of keys, but it's not OK for
+                // the report to not have the appropriate fields set.
+                //
+                // In this case the report is treated as malformed, and the previous report is not
+                // updated.
+                fx_log_err!("Failed to parse keyboard keys: {:?}", report);
+                return previous_report;
+            }
+        };
+
+        // For the previous keys it's OK to not be able to parse the keys, since an empty vector is
+        // a sensible default (this is what happens when there is no previous report).
+        let previous_keys: Vec<Key> = previous_report
+            .as_ref()
+            .and_then(|unwrapped_report| KeyboardBinding::parse_pressed_keys(&unwrapped_report))
+            .unwrap_or_default();
+
+        KeyboardBinding::send_key_events(&new_keys, &previous_keys, input_message_sender.clone());
+
         Some(report)
     }
 
@@ -79,14 +128,14 @@ impl input_device::InputDeviceBinding for KeyboardBinding {
     async fn bind_device(device: &InputDeviceProxy) -> Result<Self, Error> {
         match device.get_descriptor().await?.keyboard {
             Some(fidl_fuchsia_input_report::KeyboardDescriptor {
-                input: Some(fidl_fuchsia_input_report::KeyboardInputDescriptor { keys: _ }),
+                input: Some(fidl_fuchsia_input_report::KeyboardInputDescriptor { keys }),
             }) => {
                 let (message_sender, message_receiver) =
                     futures::channel::mpsc::channel(input_device::INPUT_MESSAGE_BUFFER_SIZE);
                 Ok(KeyboardBinding {
                     message_sender,
                     message_receiver,
-                    descriptor: KeyboardDescriptor {},
+                    descriptor: KeyboardDescriptor { keys: keys.unwrap_or_default() },
                 })
             }
             descriptor => {
@@ -132,4 +181,294 @@ pub async fn all_keyboard_messages() -> Result<Receiver<input_device::InputMessa
     }
 
     Ok(message_receiver)
+}
+
+impl KeyboardBinding {
+    /// Converts a vector of keyboard keys to the appropriate [`Modifiers`] bitflags.
+    ///
+    /// For example, if `keys` contains `Key::LeftAlt`, the bitflags will contain
+    /// the corresponding flags for `LeftAlt`.
+    ///
+    /// # Parameters
+    /// - `keys`: The keys to check for modifiers.
+    ///
+    /// # Returns
+    /// Returns `None` if there are no modifier keys present.
+    pub fn to_modifiers(keys: &Vec<&Key>) -> Option<Modifiers> {
+        let mut modifiers = Modifiers::empty();
+        for key in keys {
+            let modifier = match key {
+                Key::LeftAlt => Some(Modifiers::Alt | Modifiers::LeftAlt),
+                Key::RightAlt => Some(Modifiers::Alt | Modifiers::RightAlt),
+                Key::LeftShift => Some(Modifiers::Shift | Modifiers::LeftShift),
+                Key::RightShift => Some(Modifiers::Shift | Modifiers::RightShift),
+                Key::LeftCtrl => Some(Modifiers::Control | Modifiers::LeftControl),
+                Key::RightCtrl => Some(Modifiers::Control | Modifiers::RightControl),
+                Key::LeftMeta => Some(Modifiers::Meta | Modifiers::LeftMeta),
+                Key::RightMeta => Some(Modifiers::Meta | Modifiers::RightMeta),
+                Key::CapsLock => Some(Modifiers::CapsLock),
+                Key::NumLock => Some(Modifiers::NumLock),
+                Key::ScrollLock => Some(Modifiers::ScrollLock),
+                _ => None,
+            };
+            if let Some(modifier) = modifier {
+                modifiers.insert(modifier);
+            };
+        }
+        if modifiers.is_empty() {
+            return None;
+        }
+        Some(modifiers)
+    }
+
+    /// Parses the currently pressed keys from an input report.
+    ///
+    /// # Parameters
+    /// - `input_report`: The input report to parse the keyboard keys from.
+    ///
+    /// # Returns
+    /// Returns `None` if any of the required input report fields are `None`. If all the
+    /// required report fields are present, but there are no pressed keys, an empty vector
+    /// is returned.
+    fn parse_pressed_keys(input_report: &InputReport) -> Option<Vec<Key>> {
+        input_report
+            .keyboard
+            .as_ref()
+            .and_then(|unwrapped_keyboard| unwrapped_keyboard.pressed_keys.as_ref())
+            .and_then(|unwrapped_keys| Some(unwrapped_keys.iter().cloned().collect()))
+    }
+
+    /// Sends key events to clients based on the new and previously pressed keys.
+    ///
+    /// # Parameters
+    /// - `new_keys`: The keys which are currently pressed, as reported by the bound device.
+    /// - `previous_keys`: The keys which were pressed in the previous input report.
+    fn send_key_events(
+        new_keys: &Vec<Key>,
+        previous_keys: &Vec<Key>,
+        mut input_message_sender: Sender<input_device::InputMessage>,
+    ) {
+        // Filter out the keys which were present in the previous keyboard report to avoid sending
+        // multiple `KeyEventPhase::Pressed` events for a key.
+        let pressed_keys: Vec<Key> =
+            new_keys.iter().cloned().filter(|key| !previous_keys.contains(key)).collect();
+
+        // Any key which is not present in the new keys, but was present in the previous report
+        // is considered to be released.
+        let released_keys: Vec<Key> =
+            previous_keys.iter().cloned().filter(|key| !new_keys.contains(key)).collect();
+
+        let keys_to_send = hashmap! {
+            KeyEventPhase::Pressed => pressed_keys,
+            KeyEventPhase::Released => released_keys,
+        };
+
+        fasync::spawn(async move {
+            match input_message_sender
+                .send(input_device::InputMessage::Keyboard(KeyboardInputMessage {
+                    keys: keys_to_send,
+                }))
+                .await
+            {
+                Err(error) => {
+                    fx_log_err!("Failed to send keyboard key events: {:?}", error);
+                }
+                _ => (),
+            }
+        });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_input_report(pressed_keys: Vec<Key>) -> InputReport {
+        InputReport {
+            event_time: None,
+            keyboard: Some(fidl_fuchsia_input_report::KeyboardInputReport {
+                pressed_keys: Some(pressed_keys),
+            }),
+            mouse: None,
+            touch: None,
+            sensor: None,
+            trace_id: None,
+        }
+    }
+
+    /// Tests that a key that is present in the new report, but was not present in the previous report
+    /// is propagated as pressed.
+    #[fasync::run_singlethreaded(test)]
+    async fn pressed_key() {
+        let previous_report = None;
+        let report = create_input_report(vec![Key::A]);
+
+        let mut descriptor =
+            input_device::InputDescriptor::Keyboard(KeyboardDescriptor { keys: vec![] });
+
+        let (message_sender, mut message_receiver) = futures::channel::mpsc::channel(1);
+        let _ = KeyboardBinding::process_reports(
+            report,
+            previous_report,
+            &mut descriptor,
+            &mut message_sender.clone(),
+        );
+
+        if let Some(input_message) = message_receiver.next().await {
+            match input_message {
+                input_device::InputMessage::Keyboard(keyboard_message) => {
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Pressed].len(), 1);
+                    assert_eq!(
+                        keyboard_message.keys[&KeyEventPhase::Pressed].first(),
+                        Some(&Key::A)
+                    );
+
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Released].len(), 0);
+                }
+                _ => assert!(false),
+            }
+        } else {
+            assert!(false);
+        }
+    }
+
+    /// Tests that a key that is not present in the new report, but was present in the previous report
+    /// is propagated as released.
+    #[fasync::run_singlethreaded(test)]
+    async fn released_key() {
+        let previous_report = create_input_report(vec![Key::A]);
+        let report = create_input_report(vec![]);
+
+        let mut descriptor =
+            input_device::InputDescriptor::Keyboard(KeyboardDescriptor { keys: vec![] });
+
+        let (message_sender, mut message_receiver) = futures::channel::mpsc::channel(1);
+        let _ = KeyboardBinding::process_reports(
+            report,
+            Some(previous_report),
+            &mut descriptor,
+            &mut message_sender.clone(),
+        );
+
+        if let Some(input_message) = message_receiver.next().await {
+            match input_message {
+                input_device::InputMessage::Keyboard(keyboard_message) => {
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Released].len(), 1);
+                    assert_eq!(
+                        keyboard_message.keys[&KeyEventPhase::Released].first(),
+                        Some(&Key::A)
+                    );
+
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Pressed].len(), 0);
+                }
+                _ => assert!(false),
+            }
+        } else {
+            assert!(false);
+        }
+    }
+
+    /// Tests that a key that is present in multiple consecutive input reports is not propagated
+    /// as a pressed event more than once.
+    #[fasync::run_singlethreaded(test)]
+    async fn multiple_pressed_event_filtering() {
+        let previous_report = create_input_report(vec![Key::A]);
+        let report = create_input_report(vec![Key::A]);
+
+        let mut descriptor =
+            input_device::InputDescriptor::Keyboard(KeyboardDescriptor { keys: vec![] });
+
+        let (message_sender, mut message_receiver) = futures::channel::mpsc::channel(1);
+        let _ = KeyboardBinding::process_reports(
+            report,
+            Some(previous_report),
+            &mut descriptor,
+            &mut message_sender.clone(),
+        );
+
+        if let Some(input_message) = message_receiver.next().await {
+            match input_message {
+                input_device::InputMessage::Keyboard(keyboard_message) => {
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Released].len(), 0);
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Pressed].len(), 0);
+                }
+                _ => assert!(false),
+            }
+        } else {
+            assert!(false);
+        }
+    }
+
+    /// Tests that both pressed and released keys are sent at once.
+    #[fasync::run_singlethreaded(test)]
+    async fn pressed_and_released_keys() {
+        let previous_report = create_input_report(vec![Key::A]);
+        let report = create_input_report(vec![Key::B]);
+
+        let mut descriptor =
+            input_device::InputDescriptor::Keyboard(KeyboardDescriptor { keys: vec![] });
+
+        let (message_sender, mut message_receiver) = futures::channel::mpsc::channel(1);
+        let _ = KeyboardBinding::process_reports(
+            report,
+            Some(previous_report),
+            &mut descriptor,
+            &mut message_sender.clone(),
+        );
+
+        if let Some(input_message) = message_receiver.next().await {
+            match input_message {
+                input_device::InputMessage::Keyboard(keyboard_message) => {
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Released].len(), 1);
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Pressed].len(), 1);
+
+                    assert_eq!(
+                        keyboard_message.keys[&KeyEventPhase::Released].first(),
+                        Some(&Key::A)
+                    );
+                    assert_eq!(
+                        keyboard_message.keys[&KeyEventPhase::Pressed].first(),
+                        Some(&Key::B)
+                    );
+                }
+                _ => assert!(false),
+            }
+        } else {
+            assert!(false);
+        }
+    }
+
+    /// Tests that modifier keys are propagated to the message receiver.
+    #[fasync::run_singlethreaded(test)]
+    async fn modifier_keys() {
+        let previous_report = None;
+        let report = create_input_report(vec![Key::LeftShift]);
+
+        let mut descriptor =
+            input_device::InputDescriptor::Keyboard(KeyboardDescriptor { keys: vec![] });
+
+        let (message_sender, mut message_receiver) = futures::channel::mpsc::channel(1);
+        let _ = KeyboardBinding::process_reports(
+            report,
+            previous_report,
+            &mut descriptor,
+            &mut message_sender.clone(),
+        );
+
+        if let Some(input_message) = message_receiver.next().await {
+            match input_message {
+                input_device::InputMessage::Keyboard(keyboard_message) => {
+                    assert_eq!(keyboard_message.keys[&KeyEventPhase::Pressed].len(), 1);
+
+                    assert_eq!(
+                        keyboard_message.keys[&KeyEventPhase::Pressed].first(),
+                        Some(&Key::LeftShift)
+                    );
+                }
+                _ => assert!(false),
+            }
+        } else {
+            assert!(false);
+        }
+    }
 }
