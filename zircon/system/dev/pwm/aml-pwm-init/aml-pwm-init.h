762234045da575e5afd49a036367a29c6a7185ba
Ruby Zhuang <rdzhuang@google.com>
2020-01-07 17:42:16 +0000

    [sherlock][pwm] pwm-init implementation
    Test: aml-pwm-init-test
    
    Change-Id: Ia2cd5e3d80bd1686e96de5b228e3f9be6a0da450

---
+++ zircon/system/dev/pwm/aml-pwm-init/aml-pwm-init.h
@@ -0,0 +1,50 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ZIRCON_SYSTEM_DEV_PWM_AML_PWM_INIT_AML_PWM_INIT_H_
+#define ZIRCON_SYSTEM_DEV_PWM_AML_PWM_INIT_AML_PWM_INIT_H_
+
+#include <ddk/debug.h>
+#include <ddk/platform-defs.h>
+#include <ddktl/device.h>
+#include <ddktl/protocol/gpio.h>
+#include <ddktl/protocol/pwm.h>
+#include <soc/aml-common/aml-pwm-regs.h>
+
+namespace pwm_init {
+
+class PwmInitDevice;
+using PwmInitDeviceType = ddk::Device<PwmInitDevice, ddk::UnbindableNew>;
+
+class PwmInitDevice : public PwmInitDeviceType {
+ public:
+  static zx_status_t Create(void* ctx, zx_device_t* parent);
+
+  void DdkUnbindNew(ddk::UnbindTxn txn) { txn.Reply(); }
+  void DdkRelease() { delete this; }
+
+ private:
+  friend class FakePwmInitDevice;
+
+  enum {
+    COMPONENT_PWM,
+    COMPONENT_WIFI_GPIO,
+    COMPONENT_BT_GPIO,
+    COMPONENT_COUNT,
+  };
+
+  explicit PwmInitDevice(zx_device_t* parent, ddk::PwmProtocolClient pwm,
+                         ddk::GpioProtocolClient wifi_gpio, ddk::GpioProtocolClient bt_gpio)
+      : PwmInitDeviceType(parent), pwm_(pwm), wifi_gpio_(wifi_gpio), bt_gpio_(bt_gpio) {}
+
+  zx_status_t Init();
+
+  ddk::PwmProtocolClient pwm_;
+  ddk::GpioProtocolClient wifi_gpio_;
+  ddk::GpioProtocolClient bt_gpio_;
+};
+
+}  // namespace pwm_init
+
+#endif  // ZIRCON_SYSTEM_DEV_PWM_AML_PWM_INIT_AML_PWM_INIT_H_
