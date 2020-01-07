08ab292ceab30d87b5d0cc3b6bf747b3bc66f31d
Ricardo Vargas <rvargas@google.com>
2020-01-07 03:28:41 +0000

    [nand-util] Generate wear histogram instead of just boundaries.
    Now, using --wear will print out a histogram of the wear count for all
    blocks, and log all values that are outside the expected range.
    
    Test: Run the tool against a real device and see a histogram.
    
    This CL basically increases the log verbosity of the tool (logging
    being generally exempt from dedicated tests). That said, the main reason
    for not having dedicated tests with this CL is that it would require
    breaking the normal flow of the code just to add something observable
    by a test, and that is likely to make the code harder to follow, for
    very little gain.
    
    Fixed: 39265
    
    Change-Id: I32585cc05f6c5895e2a1dec60be9f3f8b99185cf

--- zircon/system/uapp/nand-util/ftl.cc
+++ zircon/system/uapp/nand-util/ftl.cc
@@ -4,12 +4,15 @@
 
 #include "ftl.h"
 
-#include <algorithm>
-#include <new>
+#include <limits.h>
 #include <stdio.h>
+#include <zircon/assert.h>
+
+#include <algorithm>
+#include <array>
+#include <new>
 
 #include <fbl/vector.h>
-#include <zircon/assert.h>
 
 #include "ftl_internal.h"
 
@@ -79,10 +82,16 @@ bool WearCounts(const NandBroker& nand) {
     page_multiplier = ftl->ndm().page_multiplier();
   }
 
-  int min = 64 * 1024;
+  int min = INT_MAX;
   int max = 0;
   int sum = 0;
   int count = 0;
+
+  // Build a wear count histogram. The expected size is 255 so the first wear
+  // count can be either 255 below the max or 255 above the min; in other words,
+  // 512 buckets should be enough. 1000 provides a reasonable extra range.
+  std::array<int, 1000> histogram = {};
+  int offset = -1;
   for (uint32_t block = 0; block < num_blocks; block++) {
     int value = GetWearCount(nand, block, page_multiplier);
     if (value > 0) {
@@ -90,12 +99,30 @@ bool WearCounts(const NandBroker& nand) {
       max = std::max(max, value);
       sum += value;
       count++;
+
+      if (offset < 0) {
+        // Place the first found count at the center of the range.
+        offset = std::max(0, value - 500);
+      }
+      int bucket = value - offset;
+      if (bucket < 0 || bucket > 999) {
+        printf("Out of range for histogram: %d (block %u) vs start: %d\n", value, block, offset);
+        continue;
+      }
+      histogram[bucket] += 1;
     }
   }
 
   if (count) {
     sum /= count;
-    printf("Wear counts: min %d, max %d, average %d\n", min, max, sum);
+    printf("Wear counts: min %d, max %d, delta %d, average %d, count %d\n", min, max, max - min,
+           sum, count);
+    for (int i = min; i <= max; i++) {
+      int bucket = i - offset;
+      if (bucket >= 0 && bucket < 1000) {
+        printf("[%06d] %d\n", i, histogram[bucket]);
+      }
+    }
   } else {
     printf("No wear count found\n");
   }
