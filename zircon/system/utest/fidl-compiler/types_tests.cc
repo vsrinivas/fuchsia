2fe5edac87dc3b1a2a3000287e593246ef901635
Chris Tam <godtamit@google.com>
2020-01-08 17:55:31 +0000

    [fidlc][test] Use assert function instead of macro
    This change moves away from using a macro for checking primitive types
    in the types unit test.
    
    Bug: 8027
    
    Test: fidl-compiler-test
    Change-Id: I2c6860fc4a1b23a1a92fbde729061b79e1646b3e

--- zircon/system/utest/fidl-compiler/types_tests.cc
+++ zircon/system/utest/fidl-compiler/types_tests.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <fidl/flat_ast.h>
+#include <fidl/types.h>
 #include <unittest/unittest.h>
 
 #include "test_library.h"
@@ -10,17 +11,23 @@
 namespace fidl {
 namespace flat {
 
-#define CHECK_PRIMITIVE_TYPE(N, S)                                                                \
-  {                                                                                               \
-    auto the_type_name = Name(library_ptr, N);                                                    \
-    const Type* the_type;                                                                         \
-    ASSERT_TRUE(typespace.Create(the_type_name, nullptr /* maybe_arg_type */,                     \
-                                 std::optional<types::HandleSubtype>(), nullptr /* maybe_size */, \
-                                 types::Nullability::kNonnullable, &the_type, nullptr));          \
-    ASSERT_NE(the_type, nullptr, N);                                                              \
-    auto the_type_p = static_cast<const PrimitiveType*>(the_type);                                \
-    ASSERT_EQ(the_type_p->subtype, S, N);                                                         \
-  }
+bool CheckPrimitiveType(const Library* library, Typespace* typespace, const char* name,
+                        types::PrimitiveSubtype subtype) {
+  ASSERT_NONNULL(typespace);
+
+  std::string owned_name = std::string(name);
+
+  auto the_type_name = Name(library, owned_name);
+  const Type* the_type;
+  ASSERT_TRUE(typespace->Create(the_type_name, nullptr /* maybe_arg_type */,
+                                std::optional<types::HandleSubtype>(), nullptr /* maybe_size */,
+                                types::Nullability::kNonnullable, &the_type, nullptr));
+  ASSERT_NONNULL(the_type, name);
+  auto the_type_p = static_cast<const PrimitiveType*>(the_type);
+  ASSERT_EQ(the_type_p->subtype, subtype, name);
+
+  return true;
+}
 
 // Tests that we can look up root types with global names, i.e. those absent
 // of any libraries.
@@ -28,20 +35,19 @@ bool root_types_with_no_library_in_lookup() {
   BEGIN_TEST;
 
   Typespace typespace = Typespace::RootTypes(nullptr);
+  Library* library = nullptr;
 
-  Library* library_ptr = nullptr;
-
-  CHECK_PRIMITIVE_TYPE("bool", types::PrimitiveSubtype::kBool);
-  CHECK_PRIMITIVE_TYPE("int8", types::PrimitiveSubtype::kInt8);
-  CHECK_PRIMITIVE_TYPE("int16", types::PrimitiveSubtype::kInt16);
-  CHECK_PRIMITIVE_TYPE("int32", types::PrimitiveSubtype::kInt32);
-  CHECK_PRIMITIVE_TYPE("int64", types::PrimitiveSubtype::kInt64);
-  CHECK_PRIMITIVE_TYPE("uint8", types::PrimitiveSubtype::kUint8);
-  CHECK_PRIMITIVE_TYPE("uint16", types::PrimitiveSubtype::kUint16);
-  CHECK_PRIMITIVE_TYPE("uint32", types::PrimitiveSubtype::kUint32);
-  CHECK_PRIMITIVE_TYPE("uint64", types::PrimitiveSubtype::kUint64);
-  CHECK_PRIMITIVE_TYPE("float32", types::PrimitiveSubtype::kFloat32);
-  CHECK_PRIMITIVE_TYPE("float64", types::PrimitiveSubtype::kFloat64);
+  CheckPrimitiveType(library, &typespace, "bool", types::PrimitiveSubtype::kBool);
+  CheckPrimitiveType(library, &typespace, "int8", types::PrimitiveSubtype::kInt8);
+  CheckPrimitiveType(library, &typespace, "int16", types::PrimitiveSubtype::kInt16);
+  CheckPrimitiveType(library, &typespace, "int32", types::PrimitiveSubtype::kInt32);
+  CheckPrimitiveType(library, &typespace, "int64", types::PrimitiveSubtype::kInt64);
+  CheckPrimitiveType(library, &typespace, "uint8", types::PrimitiveSubtype::kUint8);
+  CheckPrimitiveType(library, &typespace, "uint16", types::PrimitiveSubtype::kUint16);
+  CheckPrimitiveType(library, &typespace, "uint32", types::PrimitiveSubtype::kUint32);
+  CheckPrimitiveType(library, &typespace, "uint64", types::PrimitiveSubtype::kUint64);
+  CheckPrimitiveType(library, &typespace, "float32", types::PrimitiveSubtype::kFloat32);
+  CheckPrimitiveType(library, &typespace, "float64", types::PrimitiveSubtype::kFloat64);
 
   END_TEST;
 }
@@ -57,17 +63,17 @@ bool root_types_with_some_library_in_lookup() {
   ASSERT_TRUE(library.Compile());
   auto library_ptr = library.library();
 
-  CHECK_PRIMITIVE_TYPE("bool", types::PrimitiveSubtype::kBool);
-  CHECK_PRIMITIVE_TYPE("int8", types::PrimitiveSubtype::kInt8);
-  CHECK_PRIMITIVE_TYPE("int16", types::PrimitiveSubtype::kInt16);
-  CHECK_PRIMITIVE_TYPE("int32", types::PrimitiveSubtype::kInt32);
-  CHECK_PRIMITIVE_TYPE("int64", types::PrimitiveSubtype::kInt64);
-  CHECK_PRIMITIVE_TYPE("uint8", types::PrimitiveSubtype::kUint8);
-  CHECK_PRIMITIVE_TYPE("uint16", types::PrimitiveSubtype::kUint16);
-  CHECK_PRIMITIVE_TYPE("uint32", types::PrimitiveSubtype::kUint32);
-  CHECK_PRIMITIVE_TYPE("uint64", types::PrimitiveSubtype::kUint64);
-  CHECK_PRIMITIVE_TYPE("float32", types::PrimitiveSubtype::kFloat32);
-  CHECK_PRIMITIVE_TYPE("float64", types::PrimitiveSubtype::kFloat64);
+  CheckPrimitiveType(library_ptr, &typespace, "bool", types::PrimitiveSubtype::kBool);
+  CheckPrimitiveType(library_ptr, &typespace, "int8", types::PrimitiveSubtype::kInt8);
+  CheckPrimitiveType(library_ptr, &typespace, "int16", types::PrimitiveSubtype::kInt16);
+  CheckPrimitiveType(library_ptr, &typespace, "int32", types::PrimitiveSubtype::kInt32);
+  CheckPrimitiveType(library_ptr, &typespace, "int64", types::PrimitiveSubtype::kInt64);
+  CheckPrimitiveType(library_ptr, &typespace, "uint8", types::PrimitiveSubtype::kUint8);
+  CheckPrimitiveType(library_ptr, &typespace, "uint16", types::PrimitiveSubtype::kUint16);
+  CheckPrimitiveType(library_ptr, &typespace, "uint32", types::PrimitiveSubtype::kUint32);
+  CheckPrimitiveType(library_ptr, &typespace, "uint64", types::PrimitiveSubtype::kUint64);
+  CheckPrimitiveType(library_ptr, &typespace, "float32", types::PrimitiveSubtype::kFloat32);
+  CheckPrimitiveType(library_ptr, &typespace, "float64", types::PrimitiveSubtype::kFloat64);
 
   END_TEST;
 }
