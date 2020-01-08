59afcf863138835d638d32dd83352696ad332ebb
Ankur Mittal <anmittal@google.com>
2020-01-08 19:15:44 +0000

    [gtest_runner] Figure out where test will write results.
    Creating a memfs where tests can write result.
    
    TEST= manual
    
    fx shell run fuchsia-pkg://fuchsia.com/component_manager_for_test#meta/component_manager_for_test.cmx fuchsia-pkg://fuchsia.com/gtest_runner#meta/root.cm
    
    Will write automatic tests once I start using this storage.
    
    Change-Id: I151b3b2a65dd69aa5f09077285f09281fdbb246d

--- src/sys/test_runners/gtest/src/main.rs
+++ src/sys/test_runners/gtest/src/main.rs
@@ -2,6 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use std::{fs, path::Path};
+
 fn main() {
-    panic!("Not implemented yet");
+    println!("runner starting");
+    // We will divide this directory up and pass to  tests as /test_result so that they can write
+    // their json output
+    let path = Path::new("/data/test_data");
+    fs::create_dir(&path).expect("should not fail");
+    println!("runner ending");
 }
