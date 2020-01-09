15e2138df6a93e5c8b3071b9332158d1ee54732d
Jeremy Manson <jeremymanson@google.com>
2020-01-09 07:26:46 +0000

    [shell] Add some pretty printing functions.
    Change-Id: I62c6ad713d5a64ad4777a0d6d10c601040fd3c47

--- src/developer/shell/lib/pp.js
+++ src/developer/shell/lib/pp.js
@@ -7,69 +7,202 @@
  */
 
 /**
+ * Returns a string containing a representation of the object
+ *
+ * @param {*} obj The object to print
+ * @param {*} options Optional boolean parameters.
+ *     If this contains a property called "whitespace", you will get pretty printing.
+ *     If this contains a property called "quotes", you will get quotation
+ *        marks around strings.
+ * @param {*} depth The function is recursive; this indicates the levels
+ *                  of recursive depth.
+ */
+function sprint_go(obj, options, depth) {
+  let whitespace = options.hasOwnProperty('whitespace');
+  let quote = options.hasOwnProperty('quotes') ? '"' : '';
+  let output = '';
+  let newline = '';
+  let indent = '';
+  let outdent = '';
+
+  let type = (typeof obj);
+  if (whitespace) {
+    newline = '\n';
+    for (let i = 0; i < depth - 1; i++) {
+      outdent += ' ';
+    }
+    indent = outdent + ' ';
+  }
+
+  // Implementation note: JSON.stringify dies with BigInt and friends, so we do it the hard way.
+  if (obj == null) {
+    output += 'null';
+    return output;
+  }
+  if (Array.isArray(obj)) {
+    output += '[' + newline;
+    for (let i = 0; i < obj.length; i++) {
+      if (i != 0) {
+        output += ',' + newline;
+      }
+      output += indent;
+      output += sprint_go(obj[i], options, depth + 1);
+    }
+    output += newline + outdent + ']';
+    return output;
+  }
+  switch (type) {
+    case 'number':
+      output += Number(obj).toString(10);
+      return output;
+    case 'string':
+      output += quote + obj + quote;
+      return output;
+    case 'boolean':
+      output += obj ? 'true' : 'false';
+      return output;
+    case 'object':
+      output += '{' + newline;
+      let first = true;
+      for (const property in obj) {
+        if (!first) {
+          output += ',' + newline;
+        }
+        output += indent;
+        output += sprint_go(property, options, depth + 1);
+        output += ' : ';
+        output += sprint_go(obj[property], options, depth + 1);
+        first = false;
+      }
+      output += newline + outdent + '}';
+      return output;
+  }
+  if (typeof obj.toString == 'function') {
+    output += obj.toString();
+    return output;
+  }
+  throw 'Unknown object type for ' + obj;
+}
+
+
+/**
+ * Returns a pretty printed representation of this object.
+ *
+ * @param {*} obj
+ * @param {*} options Optional boolean parameters.
+ *     If this contains a property called "whitespace", you will get pretty printing.
+ *     If this contains a property called "quotes", you will get quotation
+ *        marks around strings.
+ */
+function sprint(obj, options) {
+  if (!options) {
+    options = {quotes: true};
+  }
+  return sprint_go(obj, options, 1);
+}
+
+/**
  * Prints an object on the given fd.
  *
  * @param {*} fd Something like "std.out" or "std.err"
  * @param {*} obj The object to print.
+ * @param {*} options Optional boolean parameters (see sprint)
  */
-function fprint(fd, obj) {
-  // Implementation note: JSON.stringify dies with BigInt and friends, so we do it the hard way.
-  if (obj == null) {
-    fd.printf('null');
-    return;
+function fprint(fd, obj, options) {
+  if (!options) {
+    options = {quotes: true, whitespace: true};
   }
-  if (Array.isArray(obj)) {
-    fd.printf('[');
-    for (let i = 0; i < obj.length; i++) {
-      if (i != 0) {
-        fd.printf(',');
-      }
-      fprint(fd, obj[i]);
-    }
-    fd.printf(']');
-    return;
-  }
-  let type = (typeof obj);
-  switch (type) {
-    case 'number':
-      fd.printf(Number(obj).toString(10));
-      return;
-    case 'string':
-      fd.printf('"' + obj + '"');
-      return;
-    case 'boolean':
-      fd.printf(obj ? 'true' : 'false');
-      return;
-    case 'object':
-      fd.printf('{');
-      let first = true;
-      for (const property in obj) {
-        if (!first) {
-          fd.printf(', ')
-        }
-        fprint(fd, property);
-        fd.printf(':');
-        fprint(fd, obj[property]);
-        first = false;
-      }
-      fd.printf('}');
-      return;
-  }
-  if (typeof obj.toString == 'function') {
-    fd.printf(obj.toString());
-    return;
-  }
-  throw 'Unknown object type for ' + obj;
+  fd.printf('%s', sprint_go(obj, options, 1));
 }
 
 /**
  * Prints an object on stdout.
  *
  * @param {*} obj The object to print.
+ * @param {*} options Optional boolean parameters (see sprint)
  */
-function print(obj) {
-  fprint(std.out, obj)
+function print(obj, options) {
+  fprint(std.out, obj, options)
 }
 
+/**
+ * Returns a string containing a tabular representation of the given array.
+ * The given array is an array of objects.  Each property is one column in
+ * the table. The first element is assumed to contain exactly the properties
+ * to be printed. If subsequent elements are missing some of those properties,
+ * an exception will be thrown.  If subsequent elements have additional
+ * properties, they will not be printed.
+ *
+ * @param {*} arr The array to print in tabular form.
+ */
+function scols(arr) {
+  let schema = [];
+  let outArr = [];
+  let colWidths = [];
 
-export {print, fprint}
+  if (!Array.isArray(arr)) {
+    throw 'Attempt to print non-array value';
+  }
+
+  if (arr.length < 1) {
+    throw 'No columns for array detected'
+  }
+
+  for (let prop in arr[0]) {
+    if (arr[0].hasOwnProperty(prop)) {
+      schema.push(prop);
+      colWidths.push(prop.length >= 1 ? prop.length + 1 : 1);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let newElt = [];
+    for (let j = 0; j < schema.length; j++) {
+      let str = sprint_go(arr[i][schema[j]], {}, 1);
+      if (colWidths[j] < str.length + 1) {
+        colWidths[j] = str.length + 1;
+      }
+      newElt.push(str);
+    }
+    outArr.push(newElt);
+  }
+
+  let output = '\n';
+  for (let i = 0; i < schema.length; i++) {
+    output += schema[i].padStart(colWidths[i], ' ');
+  }
+  output += '\n';
+  for (let i = 0; i < outArr.length; i++) {
+    for (let j = 0; j < schema.length; j++) {
+      output += outArr[i][j].padStart(colWidths[j], ' ');
+    }
+    output += '\n';
+  }
+  return output;
+}
+
+/**
+ * Prints a tabular representation of the given array arr to file descriptor
+ * fd.  Example: pp,fpcols(std.err, []).
+ *
+ * For more on the tabular representation, see scols.
+ *
+ * @param {*} fd The file descriptor to which to write
+ * @param {*} arr The array to print to that file descriptor.
+ */
+function fpcols(fd, arr) {
+  fd.printf('%s', scols(arr));
+}
+
+/**
+ * Prints a tabular representation of the given array arr to stdout.
+ *
+ * For more on the tabular representation, see scols.
+ *
+ * @param {*} arr The array to print.
+ */
+function pcols(arr) {
+  fpcols(std.out, arr);
+}
+
+export {print, fprint, sprint, pcols, fpcols, scols}
---
+++ src/developer/shell/lib/pp_test.cc
@@ -0,0 +1,50 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gtest/gtest.h"
+#include "src/developer/shell/console/console.h"
+#include "src/developer/shell/lib/js_testing_utils.h"
+
+namespace shell {
+
+class PpTest : public JsTest {
+ protected:
+  void SetUp() override { JsTest::SetUp(); }
+};
+
+TEST_F(PpTest, BasicPrettyPrint) {
+  InitBuiltins("/pkg/data/fidling", "/pkg/data/lib");
+  ctx_->Export("util", "/pkg/data/lib");
+
+  std::string test_string = R"(
+    {
+      let data = { "a": 1, "b": 2.0, "c": "foo", "d" : true, "e" : [1,2,3] };
+      let actual = pp.sprint(data, {quotes:true, whitespace:true});
+      let expected =`{
+ "a" : 1,
+ "b" : 2,
+ "c" : "foo",
+ "d" : true,
+ "e" : [
+  1,
+  2,
+  3
+ ]
+}`;
+      if (actual != expected) {
+        throw "actual:\n" + actual + "\ndoes not match expected:\n" + expected;
+      }
+    }
+    {
+      let data = { "a": 1, "b": 2.0, "c": "foo", "d" : true };
+      let actualCols = pp.scols([data]);
+      let regex = /\s+a\s+b\s+c\s+c\s*\n\s+1\s+2\s+foo\s+true\s*/;
+      if (regex.test(actualCols)) {
+        throw "actual:\n" + actualCols + "\ndoes not match regex";
+      }
+    }
+  )";
+  ASSERT_TRUE(Eval(test_string));
+}
+}  // namespace shell
