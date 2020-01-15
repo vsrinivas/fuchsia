1fe6ec9b661c17c827b04ad8a178e7bd6a3b9a72
Amanda Tait <atait@google.com>
2020-01-15 19:14:54 +0000

    [dhcp] Run dhcpd-cli tests in hermetic envs
    This change modifies the test cases of dhcpd_cli_tests, such that each
    case runs in its own hermetic environment. When run in its own
    environment, each case will have its own instance of dhcpd.cmx, which in
    turn will prevent the set() tests from polluting the other cases.
    
    Change-Id: I44a758a3f941cfb350d9af1f487b9622a81790c5

--- src/connectivity/network/dhcpd-cli/src/lib.rs
+++ src/connectivity/network/dhcpd-cli/src/lib.rs
@@ -2,103 +2,70 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Error;
+use futures::{FutureExt, StreamExt};
 
-#[fuchsia_async::run_singlethreaded(test)]
-async fn test_get_option_subnet() -> Result<(), Error> {
-    let launcher = fuchsia_component::client::launcher()?;
-    let cli_url = fuchsia_component::fuchsia_single_component_package_url!("dhcpd-cli");
-    let cli_app = fuchsia_component::client::AppBuilder::new(cli_url).args(vec![
-        "get",
-        "option",
-        "subnet-mask",
-    ]);
-    let output = cli_app.output(&launcher)?.await?;
-    let stdout = std::str::from_utf8(&output.stdout)?;
-    let stderr = std::str::from_utf8(&output.stderr)?;
+#[cfg(test)]
+async fn test_cli(args: Vec<&str>, expected_stdout: &str) {
+    let mut fs = fuchsia_component::server::ServiceFs::new_local();
+    fs.add_proxy_service::<fidl_fuchsia_stash::StoreMarker, _>()
+        .add_component_proxy_service::<fidl_fuchsia_net_dhcp::Server_Marker>(
+        fuchsia_component::fuchsia_single_component_package_url!("dhcpd").to_string(),
+        Some(vec!["--test".to_string()]),
+    );
+    let env =
+        fs.create_salted_nested_environment("test_cli").expect("failed to create environment");
+    let output = fuchsia_component::client::AppBuilder::new(
+        fuchsia_component::fuchsia_single_component_package_url!("dhcpd-cli"),
+    )
+    .args(args)
+    .output(env.launcher())
+    .expect("failed to launch dhcpd-cli");
+    let fs = fs.for_each_concurrent(None, |_incoming| async {});
+
+    let output = futures::select! {
+        () = fs.fuse() => panic!("request stream terminated"),
+        output = output.fuse() => output.expect("dhcpd-cli terminated with error"),
+    };
+    let stdout = std::str::from_utf8(&output.stdout).expect("failed to get stdout");
+    let stderr = std::str::from_utf8(&output.stderr).expect("failed to get stderr");
     assert_eq!(stderr, "");
-    assert_eq!(stdout, "SubnetMask(Ipv4Address { addr: [0, 0, 0, 0] })\n");
-    Ok(())
+    assert_eq!(stdout, expected_stdout);
 }
 
 #[fuchsia_async::run_singlethreaded(test)]
-async fn test_get_parameter_lease() -> Result<(), Error> {
-    let launcher = fuchsia_component::client::launcher()?;
-    let cli_url = fuchsia_component::fuchsia_single_component_package_url!("dhcpd-cli");
-    let cli_app = fuchsia_component::client::AppBuilder::new(cli_url).args(vec![
-        "get",
-        "parameter",
-        "lease-length",
-    ]);
-    let output = cli_app.output(&launcher)?.await?;
-    let stdout = std::str::from_utf8(&output.stdout)?;
-    let stderr = std::str::from_utf8(&output.stderr)?;
-    assert_eq!(stderr, "");
-    assert_eq!(stdout, "Lease(LeaseLength { default: None, max: None })\n");
-    Ok(())
+async fn test_get_option_subnet() {
+    test_cli(
+        vec!["get", "option", "subnet-mask"],
+        "SubnetMask(Ipv4Address { addr: [0, 0, 0, 0] })\n",
+    )
+    .await
 }
 
 #[fuchsia_async::run_singlethreaded(test)]
-async fn test_set_option_subnet() -> Result<(), Error> {
-    let launcher = fuchsia_component::client::launcher()?;
-    let cli_url = fuchsia_component::fuchsia_single_component_package_url!("dhcpd-cli");
-    let cli_app = fuchsia_component::client::AppBuilder::new(cli_url).args(vec![
-        "set",
-        "option",
-        "subnet-mask",
-        "--mask",
-        "255.255.255.0",
-    ]);
-    let output = cli_app.output(&launcher)?.await?;
-    let stdout = std::str::from_utf8(&output.stdout)?;
-    let stderr = std::str::from_utf8(&output.stderr)?;
-    assert_eq!(stderr, "");
-    assert_eq!(stdout, "");
-    Ok(())
+async fn test_get_parameter_lease() {
+    test_cli(
+        vec!["get", "parameter", "lease-length"],
+        "Lease(LeaseLength { default: None, max: None })\n",
+    )
+    .await
 }
 
 #[fuchsia_async::run_singlethreaded(test)]
-async fn test_set_parameter_lease() -> Result<(), Error> {
-    let launcher = fuchsia_component::client::launcher()?;
-    let cli_url = fuchsia_component::fuchsia_single_component_package_url!("dhcpd-cli");
-    let cli_app = fuchsia_component::client::AppBuilder::new(cli_url).args(vec![
-        "set",
-        "parameter",
-        "lease-length",
-        "--default",
-        "42",
-    ]);
-    let output = cli_app.output(&launcher)?.await?;
-    let stdout = std::str::from_utf8(&output.stdout)?;
-    let stderr = std::str::from_utf8(&output.stderr)?;
-    assert_eq!(stderr, "");
-    assert_eq!(stdout, "");
-    Ok(())
+async fn test_set_option_subnet() {
+    test_cli(vec!["set", "option", "subnet-mask", "--mask", "255.255.255.0"], "").await
 }
 
 #[fuchsia_async::run_singlethreaded(test)]
-async fn test_list_option() -> Result<(), Error> {
-    let launcher = fuchsia_component::client::launcher()?;
-    let cli_url = fuchsia_component::fuchsia_single_component_package_url!("dhcpd-cli");
-    let cli_app = fuchsia_component::client::AppBuilder::new(cli_url).args(vec!["list", "option"]);
-    let output = cli_app.output(&launcher)?.await?;
-    let stdout = std::str::from_utf8(&output.stdout)?;
-    let stderr = std::str::from_utf8(&output.stderr)?;
-    assert_eq!(stderr, "");
-    assert_eq!(stdout, "[]\n");
-    Ok(())
+async fn test_set_parameter_lease() {
+    test_cli(vec!["set", "parameter", "lease-length", "--default", "42"], "").await
 }
 
 #[fuchsia_async::run_singlethreaded(test)]
-async fn test_list_parameter() -> Result<(), Error> {
-    let launcher = fuchsia_component::client::launcher()?;
-    let cli_url = fuchsia_component::fuchsia_single_component_package_url!("dhcpd-cli");
-    let cli_app =
-        fuchsia_component::client::AppBuilder::new(cli_url).args(vec!["list", "parameter"]);
-    let output = cli_app.output(&launcher)?.await?;
-    let stdout = std::str::from_utf8(&output.stdout)?;
-    let stderr = std::str::from_utf8(&output.stderr)?;
-    assert_eq!(stderr, "");
-    assert_eq!(stdout, "[]\n");
-    Ok(())
+async fn test_list_option() {
+    test_cli(vec!["list", "option"], "[]\n").await
+}
+
+#[fuchsia_async::run_singlethreaded(test)]
+async fn test_list_parameter() {
+    test_cli(vec!["list", "parameter"], "[]\n").await
 }
