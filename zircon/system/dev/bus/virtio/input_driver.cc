394b44ba98bbfdcfbf2d7b13fe116393ad5af764
Garratt Gallagher <garratt@google.com>
2020-01-16 21:57:38 +0000

    [virtio] splitting input into its own driver
    Step 1 in converting the input driver to ddktl.
    This CL just moves the matching logic for the input virtio driver
    to its own file, with its own target.
    
    Test: There are not functional changes.
    Change-Id: I380a95c79c71cebe2d0b80dca44125b883e8e88c

---
+++ zircon/system/dev/bus/virtio/input_driver.cc
@@ -0,0 +1,22 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <zircon/types.h>
+
+#include <ddk/driver.h>
+
+#include "driver_utils.h"
+#include "input.h"
+
+static const zx_driver_ops_t virtio_input_driver_ops = []() {
+  zx_driver_ops_t ops = {};
+  ops.version = DRIVER_OPS_VERSION;
+  ops.bind = CreateAndBind<virtio::InputDevice>;
+  return ops;
+}();
+
+ZIRCON_DRIVER_BEGIN(virtio_input, virtio_input_driver_ops, "zircon", "0.1", 5)
+BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PCI),
+    BI_ABORT_IF(NE, BIND_PCI_VID, VIRTIO_PCI_VENDOR_ID),
+    BI_MATCH_IF(EQ, BIND_PCI_DID, VIRTIO_DEV_TYPE_INPUT), ZIRCON_DRIVER_END(virtio_input)
--- zircon/system/dev/bus/virtio/virtio_driver.cc
+++ zircon/system/dev/bus/virtio/virtio_driver.cc
@@ -22,7 +22,6 @@
 #include "backends/pci.h"
 #include "device.h"
 #include "driver_utils.h"
-#include "input.h"
 #include "rng.h"
 
 static zx_status_t virtio_pci_bind(void* ctx, zx_device_t* bus_device) {
@@ -45,8 +44,6 @@ static zx_status_t virtio_pci_bind(void* ctx, zx_device_t* bus_device) {
     case VIRTIO_DEV_TYPE_ENTROPY:
     case VIRTIO_DEV_TYPE_T_ENTROPY:
       return CreateAndBind<virtio::RngDevice>(ctx, bus_device);
-    case VIRTIO_DEV_TYPE_INPUT:
-      return CreateAndBind<virtio::InputDevice>(ctx, bus_device);
     default:
       return ZX_ERR_NOT_SUPPORTED;
   }
@@ -64,5 +61,4 @@ ZIRCON_DRIVER_BEGIN(virtio, virtio_driver_ops, "zircon", "0.1", 16)
 BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PCI),
     BI_ABORT_IF(NE, BIND_PCI_VID, VIRTIO_PCI_VENDOR_ID),
     BI_MATCH_IF(EQ, BIND_PCI_DID, VIRTIO_DEV_TYPE_ENTROPY),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, VIRTIO_DEV_TYPE_T_ENTROPY),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, VIRTIO_DEV_TYPE_INPUT), BI_ABORT(), ZIRCON_DRIVER_END(virtio)
+    BI_MATCH_IF(EQ, BIND_PCI_DID, VIRTIO_DEV_TYPE_T_ENTROPY), BI_ABORT(), ZIRCON_DRIVER_END(virtio)
