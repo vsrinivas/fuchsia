5fdd5d6ceba208d1089cf52417c480afcdeee361
Aaron Green <aarongreen@google.com>
2020-01-17 00:01:13 +0000

    [zircon][fs][journal] Add replay_fuzzer
    This CL adds a fuzzer for ReplayJournal.
    
    Change-Id: I64c02d3b7e82eb9eff9354e1e43715cdf644ec39

---
+++ zircon/system/ulib/fs/journal/test/replay_fuzztest.cc
@@ -0,0 +1,29 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <fs/journal/replay.h>
+
+#include "fuzzer_utils.h"
+
+namespace fs {
+namespace {
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
+  FuzzerUtils fuzz_utils(data, size);
+  JournalSuperblock info;
+  if (fuzz_utils.FuzzSuperblock(&info) != ZX_OK) {
+    return 0;
+  }
+  auto journal_start = fuzz_utils.data_provider()->ConsumeIntegral<uint64_t>();
+  auto journal_length = fuzz_utils.data_provider()->ConsumeIntegral<uint64_t>();
+  fuzz_utils.handler()->SetJournalStart(journal_start);
+  ReplayJournal(fuzz_utils.handler(), fuzz_utils.registry(), journal_start, journal_length, &info);
+  return 0;
+}
+
+}  // namespace
+}  // namespace fs
