24ff0c854e38dd8ebca632324e6d1035cfb59804
Mark Seaborn <mseaborn@google.com>
2020-01-24 16:38:55 +0000

    [benchmarks] Add example test for testing regression detection and graphing
    Add an example test whose running time we can deliberately change in
    order to test regression detection.
    
    My particular motivation here is to check that we can preserve the
    history when we rename tests on the Catapult performance dashboard
    (chromeperf).
    
    Note that perfcompare indicates that this changes the performance
    results of some other tests, which is odd.
    
    Bug: 44174
    Test: zircon_benchmarks is run on the CQ
     * local testing: "fx shell zircon_benchmarks -p --filter Loop"
    Change-Id: I18bfbf210200bde95d0dad4b05ebf572eccfafba

---
+++ garnet/bin/zircon_benchmarks/example.cc
@@ -0,0 +1,35 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <perftest/perftest.h>
+
+namespace {
+
+// Execute an empty loop for the given number of iterations.
+bool NoOpLoop(perftest::RepeatState* state, int iteration_count) {
+  while (state->KeepRunning()) {
+    for (int i = 0; i < iteration_count; ++i) {
+      // No-op that the compiler should not optimize away.
+      __asm__ volatile("");
+    }
+  }
+  return true;
+}
+
+void RegisterTests() {
+  // This is intended as a simple way to test whether regression detection
+  // is working: We can land a change that increases the iteration count
+  // here and then manually check whether a regression gets reported, or
+  // check whether the increase appears on the performance dashboard's
+  // graph.
+  perftest::RegisterTest("ExampleNoOpLoop", NoOpLoop, 100);
+
+  // Run these so we have reference values to compare against.
+  perftest::RegisterTest("NoOpLoop/100", NoOpLoop, 100);
+  perftest::RegisterTest("NoOpLoop/1000", NoOpLoop, 1000);
+  perftest::RegisterTest("NoOpLoop/10000", NoOpLoop, 10000);
+}
+PERFTEST_CTOR(RegisterTests);
+
+}  // namespace
