820d7fdb2ff45612ae46de575b20056328811769
Tyler Mandry <tmandry@google.com>
2020-01-28 21:35:22 +0000

    [build][LSC] Enable -Werror everywhere
    The %lx format string fails on mac builders expecting %llx. The
    cross-platform way of doing this is to use the PRIu64 macro.
    
    Change-Id: I2d1e8ef700673edad7ae4e433039fe39a99ca85b

--- src/developer/debug/shared/arch_x86.cc
+++ src/developer/debug/shared/arch_x86.cc
@@ -4,6 +4,7 @@
 
 #include "src/developer/debug/shared/arch_x86.h"
 
+#include <cinttypes>
 #include <sstream>
 
 #include "src/lib/fxl/strings/string_printf.h"
@@ -14,14 +15,14 @@ namespace debug_ipc {
 
 std::string DR6ToString(uint64_t dr6) {
   return fxl::StringPrintf(
-      "0x%lx: B0=%d, B1=%d, B2=%d, B3=%d, BD=%d, BS=%d, BT=%d", dr6, X86_FLAG_VALUE(dr6, DR6B0),
+      "0x%" PRIx64 ": B0=%d, B1=%d, B2=%d, B3=%d, BD=%d, BS=%d, BT=%d", dr6, X86_FLAG_VALUE(dr6, DR6B0),
       X86_FLAG_VALUE(dr6, DR6B1), X86_FLAG_VALUE(dr6, DR6B2), X86_FLAG_VALUE(dr6, DR6B3),
       X86_FLAG_VALUE(dr6, DR6BD), X86_FLAG_VALUE(dr6, DR6BS), X86_FLAG_VALUE(dr6, DR6BT));
 }
 
 std::string DR7ToString(uint64_t dr7) {
   return fxl::StringPrintf(
-      "0x%lx: L0=%d, G0=%d, L1=%d, G1=%d, L2=%d, G2=%d, L3=%d, G4=%d, LE=%d, "
+      "0x%" PRIx64 ": L0=%d, G0=%d, L1=%d, G1=%d, L2=%d, G2=%d, L3=%d, G4=%d, LE=%d, "
       "GE=%d, GD=%d, R/W0=%d, LEN0=%d, R/W1=%d, LEN1=%d, R/W2=%d, LEN2=%d, "
       "R/W3=%d, LEN3=%d",
       dr7, X86_FLAG_VALUE(dr7, DR7L0), X86_FLAG_VALUE(dr7, DR7G0), X86_FLAG_VALUE(dr7, DR7L1),
