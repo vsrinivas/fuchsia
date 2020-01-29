522430b822f06bad45ede82a33cbc4bfe8b3c179
George Kulakowski <kulakowski@google.com>
2020-01-29 23:16:57 +0000

    [zircon][smbios] Modify some license headers
    Change-Id: I021400d2cf1f3031bcd137fab7491ad854cc0df0

--- zircon/system/ulib/smbios/smbios.cc
+++ zircon/system/ulib/smbios/smbios.cc
@@ -1,9 +1,6 @@
-// Copyright 2017 The Fuchsia Authors
-//
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT
-//
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 #include <lib/smbios/smbios.h>
 
 #include <fbl/algorithm.h>
