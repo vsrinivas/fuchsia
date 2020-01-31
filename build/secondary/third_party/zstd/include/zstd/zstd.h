beca061a9c4d4d2c1e1d9330c00fecdf35fa6b76
P.Y. Laligand <pylaligand@google.com>
2020-01-31 07:40:24 +0000

    [zstd] Add build file for the Fuchsia build.
    Use the new targets instead of the ZN-imported versions when compiling
    Zircon libraries.
    The new targets make it so that headers are properly located and
    compilation options are properly applied.
    
    Bug: 36548
    Change-Id: I184f90a26308381a1b0d63637060613f22e4e728

---
+++ build/secondary/third_party/zstd/include/zstd/zstd.h
@@ -0,0 +1,10 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_H_
+#define THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_H_
+
+#include "../../../../../../third_party/zstd/lib/zstd.h"
+
+#endif  // THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_H_
---
+++ build/secondary/third_party/zstd/include/zstd/zstd_seekable.h
@@ -0,0 +1,10 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_SEEKABLE_H_
+#define THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_SEEKABLE_H_
+
+#include "../../../../../../third_party/zstd/contrib/seekable_format/zstd_seekable.h"
+
+#endif  // THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_SEEKABLE_H_
