8c6ba1458f3b2f14d2f98f1ad0210b15ee05a5a9
Peng Hao <richard.peng@oppo.com>
2020-02-04 11:36:52 +0000

    [zircon][sched] Remove a type cast
    The input parameter to thread_is_idle is already const.
    
    Change-Id: I053ade564da0a02b5aa33848baa8b83fc414bb73

--- zircon/kernel/kernel/scheduler.cc
+++ zircon/kernel/kernel/scheduler.cc
@@ -161,8 +161,7 @@ inline bool IsDeadlineThread(const thread_t* thread) {
 inline bool IsThreadAdjustable(const thread_t* thread) {
   // Checking the thread state avoids unnecessary adjustments on a thread that
   // is no longer competing.
-  // TODO(eieio): Fix thread_is_idle to take a const pointer to the thread.
-  return !thread_is_idle(const_cast<thread_t*>(thread)) && IsFairThread(thread) &&
+  return !thread_is_idle(thread) && IsFairThread(thread) &&
          thread->state == THREAD_READY;
 }
 
