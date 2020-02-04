ab09e54687746b4ace82f6fe64d43aea9ecbfe1e
David Greenaway <dgreenaway@google.com>
2020-02-04 05:11:50 +0000

    [virtualization] Add error messages when guest launch commands fail.
    Add error messages when a connection to the manager fails, or the
    guest closes its channel with an error.
    
    Change-Id: I30c4528cc1022f9d6bcb31f138c655c9c1905559

--- src/virtualization/bin/guest/launch.cc
+++ src/virtualization/bin/guest/launch.cc
@@ -4,6 +4,8 @@
 
 #include "src/virtualization/bin/guest/launch.h"
 
+#include <zircon/status.h>
+
 #include "src/lib/fxl/logging.h"
 #include "src/lib/fxl/strings/string_printf.h"
 #include "src/virtualization/bin/guest/serial.h"
@@ -13,7 +15,11 @@ void handle_launch(int argc, const char** argv, async::Loop* loop,
                    sys::ComponentContext* context) {
   // Create environment.
   fuchsia::virtualization::ManagerPtr manager;
-  context->svc()->Connect(manager.NewRequest());
+  zx_status_t status = context->svc()->Connect(manager.NewRequest());
+  if (status != ZX_OK) {
+    printf("Failed to connect to guest manager: %s\n", zx_status_get_string(status));
+    return;
+  }
   fuchsia::virtualization::RealmPtr realm;
   manager->Create(argv[0], realm.NewRequest());
 
@@ -22,11 +28,14 @@ void handle_launch(int argc, const char** argv, async::Loop* loop,
   launch_info.url = fxl::StringPrintf("fuchsia-pkg://fuchsia.com/%s#meta/%s.cmx", argv[0], argv[0]);
   launch_info.guest_config = std::move(guest_config);
   fuchsia::virtualization::GuestPtr guest;
-  realm->LaunchInstance(std::move(launch_info), guest.NewRequest(), [](...) {});
+  realm->LaunchInstance(std::move(launch_info), guest.NewRequest(), [](uint32_t) {});
 
   // Setup serial console.
   SerialConsole console(loop);
   guest->GetSerial([&console](zx::socket socket) { console.Start(std::move(socket)); });
-  guest.set_error_handler([&loop](...) { loop->Quit(); });
+  guest.set_error_handler([&loop](zx_status_t status) {
+    printf("Connection to guest closed: %s\n", zx_status_get_string(status));
+    loop->Quit();
+  });
   loop->Run();
 }
