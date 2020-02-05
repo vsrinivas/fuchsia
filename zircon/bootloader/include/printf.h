936ef4cc37f17c507ddb60c14484d4685bd50c35
George Kulakowski <kulakowski@google.com>
2020-02-05 19:19:39 +0000

    [zircon][bootloader] Modify some license headers
    Change-Id: I2086ebde533f88361f55a909ff894b0cc83090a6

--- zircon/bootloader/include/printf.h
+++ zircon/bootloader/include/printf.h
@@ -1,9 +1,8 @@
-// Copyright 2016 The Fuchsia Authors
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
 // Copyright (c) 2008 Travis Geiselbrecht
-//
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT
+
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef ZIRCON_BOOTLOADER_INCLUDE_PRINTF_H_
 #define ZIRCON_BOOTLOADER_INCLUDE_PRINTF_H_
