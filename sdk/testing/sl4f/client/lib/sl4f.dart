4b04e587b710c77d56a26e7ed1ce51fddc4f0fa1
gracelaw <gracelaw@google.com>
2020-02-11 22:16:09 +0000

    [Longevity] Add Catapult dependencies to SL4F SDK
    Bug: 43829
    
    Change-Id: I7a5b5691d149658fe255b7c74025d607f9766a48

--- sdk/testing/sl4f/client/lib/sl4f.dart
+++ sdk/testing/sl4f/client/lib/sl4f.dart
@@ -21,6 +21,7 @@ export 'src/sl4f_client.dart';
 export 'src/ssh.dart';
 export 'src/storage.dart';
 export 'src/test.dart';
+export 'src/trace_processing/metrics_results.dart';
 export 'src/trace_processing/metrics_spec.dart';
 export 'src/update.dart';
 export 'src/webdriver.dart';
