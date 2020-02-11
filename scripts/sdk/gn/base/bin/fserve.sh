70a6843787136026acafbe15ead284467fdaf431
Wesley Moy <wesleymoy@google.com>
2020-02-11 04:02:48 +0000

    [GN SDK] Add script for running a package server
    This script starts a package server that can serve Fuchsia packages to a
    Fuchsia device. It also connects to a connected Fuchsia device to
    register this package server with that device.
    
    This script and its test are meant to be run from within a project that
    is managed by the GN build system and uses the GN SDK. Use generate.py
    to create a new project, then run bin/fserve.sh or bin/fserve-test.sh
    from that project.
    
    Change-Id: If70343f7cc1ce6fd1256057d871f1868f1877005

---
+++ scripts/sdk/gn/base/bin/fserve.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -u
+
+SCRIPT_SRC_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+
+# Fuchsia command common functions.
+source "${SCRIPT_SRC_DIR}/fuchsia-common.sh" || exit $?
+
+FUCHSIA_SDK_PATH="$(realpath "${SCRIPT_SRC_DIR}/../sdk")"
+FUCHSIA_IMAGE_WORK_DIR="$(realpath "${SCRIPT_SRC_DIR}/../images")"
+FUCHSIA_BUCKET="${DEFAULT_FUCHSIA_BUCKET}"
+
+FUCHSIA_SERVER_PORT="8083"
+IMAGE_NAME="generic-x64"
+usage () {
+  echo "Usage: $0"
+  echo "  [--work-dir <directory to store image assets>]"
+  echo "    Defaults to ${FUCHSIA_IMAGE_WORK_DIR}"
+  echo "  [--bucket <fuchsia gsutil bucket>]"
+  echo "    Defaults to ${FUCHSIA_BUCKET}"
+  echo "  [--image <image name>]"
+  echo "    Defaults to ${IMAGE_NAME}"
+  echo "  [--private-key <identity file>]"
+  echo "    Uses additional private key when using ssh to access the device."
+  echo "  [--server-port <port>]"
+  echo "    Port number to use when serving the packages.  Defaults to ${FUCHSIA_SERVER_PORT}."
+  echo "  [--device-name <device hostname>]"
+  echo "    Only serves packages to a device with the given device hostname."
+  echo "  [--kill]"
+  echo "    Kills any existing package manager server"
+  echo "  [--prepare]"
+  echo "    Downloads any dependencies but does not start the package server"
+}
+
+PRIVATE_KEY_FILE=""
+PREPARE_ONLY=""
+DEVICE_NAME_FILTER=""
+
+# Parse command line
+while (( "$#" )); do
+case $1 in
+    --work-dir)
+    shift
+    FUCHSIA_IMAGE_WORK_DIR="${1:-.}"
+    ;;
+    --bucket)
+    shift
+    FUCHSIA_BUCKET="${1}"
+    ;;
+    --image)
+    shift
+    IMAGE_NAME="${1}"
+    ;;
+    --private-key)
+    shift
+    PRIVATE_KEY_FILE="${1}"
+    ;;
+    --server-port)
+    shift
+    FUCHSIA_SERVER_PORT="${1}"
+    ;;
+    --device-name)
+    shift
+    DEVICE_NAME_FILTER="${1}"
+    ;;
+    --kill)
+    kill-running-pm
+    exit 0
+    ;;
+    --prepare)
+    PREPARE_ONLY="yes"
+    ;;
+    *)
+    # unknown option
+    usage
+    exit 1
+    ;;
+esac
+shift
+done
+
+# Check for core SDK being present
+if [[ ! -d "${FUCHSIA_SDK_PATH}" ]]; then
+  fx-error "Fuchsia Core SDK not found at ${FUCHSIA_SDK_PATH}."
+  exit 2
+fi
+
+SDK_ID=$(get-sdk-version "${FUCHSIA_SDK_PATH}")
+
+# The package tarball.  We add the SDK ID to the filename to make them
+# unique.
+#
+# Consider cleaning up old tarballs when getting a new one?
+#
+if [[ ! -v  IMAGE_NAME ]]; then
+  IMAGES=("$(get-available-images "${SDK_ID}")")
+  fx-error "IMAGE_NAME not set. Valid images for this SDK version are:" "${IMAGES[*]}."
+  exit 1
+fi
+
+FUCHSIA_TARGET_PACKAGES=$(get-package-src-path "${SDK_ID}" "${IMAGE_NAME}")
+IMAGE_FILENAME="${SDK_ID}_${IMAGE_NAME}.tar.gz"
+
+# Validate the image is found
+if [[ ! -f "${FUCHSIA_IMAGE_WORK_DIR}/${IMAGE_FILENAME}" ]] ; then
+  if ! run-gsutil ls "${FUCHSIA_TARGET_PACKAGES}"; then
+    echo "Packages for ${IMAGE_NAME} not found. Valid images for this SDK version are:"
+    IMAGES=("$(get-available-images "${SDK_ID}")")
+    echo "${IMAGES[*]}."
+    exit 2
+  fi
+
+  if ! run-gsutil cp "${FUCHSIA_TARGET_PACKAGES}" "${FUCHSIA_IMAGE_WORK_DIR}/${IMAGE_FILENAME}"; then
+    fx-error "Could not copy image from ${FUCHSIA_TARGET_PACKAGES} to ${FUCHSIA_IMAGE_WORK_DIR}/${IMAGE_FILENAME}."
+    exit 2
+  fi
+else
+  echo "Skipping download, packages tarball exists"
+fi
+
+# The checksum file contains the output from `md5`. This is used to detect content
+# changes in the packages file.
+CHECKSUM_FILE="${FUCHSIA_IMAGE_WORK_DIR}/packages/packages.md5"
+
+
+# check that any existing contents of the image directory match the intended target device
+if [[ -f "${CHECKSUM_FILE}" ]]; then
+  if [[ "$(md5sum "${FUCHSIA_IMAGE_WORK_DIR}/${IMAGE_FILENAME}")" != "$(cat "${CHECKSUM_FILE}")" ]]; then
+    fx-warn "Removing old package files."
+    if ! rm -f "$(cut -d ' '  -f3 "${CHECKSUM_FILE}")"; then
+      fx-error "Could not clean up old image archive."
+      exit 2
+    fi
+    if ! rm -rf "${FUCHSIA_IMAGE_WORK_DIR}/packages"; then
+      fx-error "Could not clean up old image."
+      exit 2
+    fi
+  fi
+else
+  # if the checksum file does not exist, something is inconsistent.
+  # so delete the entire directory to make sure we're starting clean.
+  # This also happens on a clean run, where the packages directory does not
+  # exist.
+  if ! rm -rf "${FUCHSIA_IMAGE_WORK_DIR}/packages"; then
+    fx-error "Could not clean up old packages."
+    exit 2
+  fi
+fi
+
+if ! mkdir -p "${FUCHSIA_IMAGE_WORK_DIR}/packages"; then
+  fx-error "Could not create packages directory."
+  exit 2
+fi
+
+# if the tarball is not untarred, do it.
+if [[ ! -d "${FUCHSIA_IMAGE_WORK_DIR}/packages/amber-files" ]]; then
+  if ! tar xzf "${FUCHSIA_IMAGE_WORK_DIR}/${IMAGE_FILENAME}" --directory "${FUCHSIA_IMAGE_WORK_DIR}/packages"; then
+    fx-error "Could not extract image from ${FUCHSIA_IMAGE_WORK_DIR}/${IMAGE_FILENAME}."
+    exit 1
+  fi
+  md5sum "${FUCHSIA_IMAGE_WORK_DIR}/${IMAGE_FILENAME}" > "${CHECKSUM_FILE}"
+fi
+
+# Exit out if we only need to prepare the downloads
+if [[ "${PREPARE_ONLY}" == "yes" ]]; then
+  exit 0
+fi
+
+HOST_IP=$(get-host-ip "${FUCHSIA_SDK_PATH}")
+DEVICE_IP=$(get-device-ip-by-name "$FUCHSIA_SDK_PATH" "$DEVICE_NAME_FILTER")
+if [[ ! "$?" || -z "$DEVICE_IP" ]]; then
+  fx-error "Could not get device IP address"
+  exit 2
+fi
+
+# kill existing pm if present
+kill-running-pm
+
+# Start the package server
+echo "** Starting package server in the background**"
+# `:port` syntax is valid for Go programs that intend to serve on every
+# interface on a given port. For example, if $FUCHSIA_SERVER_PORT is 54321,
+# this is similar to serving on [::]:54321 or 0.0.0.0:54321.
+"${FUCHSIA_SDK_PATH}/tools/pm" serve -repo "${FUCHSIA_IMAGE_WORK_DIR}/packages/amber-files" -l ":${FUCHSIA_SERVER_PORT}"&
+
+SSH_ARGS=()
+if [[ "${PRIVATE_KEY_FILE}" != "" ]]; then
+  SSH_ARGS+=( "-i" "${PRIVATE_KEY_FILE}" )
+fi
+SSH_ARGS+=( "${DEVICE_IP}" amber_ctl add_src -f "http://[${HOST_IP}]:$FUCHSIA_SERVER_PORT/config.json" )
+
+# Update the device to point to the server.
+# Because the URL to config.json contains an IPv6 address, the address needs
+# to be escaped in square brackets. This is not necessary for the ssh target,
+# since that's just an address and not a full URL.
+if ! ssh-cmd "${SSH_ARGS[@]}" ; then
+  echo "Error: could not update device"
+fi
---
+++ scripts/sdk/gn/bash_tests/fserve-test.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Test that verifies that fserve starts up a package server. This test also
+# verifies that fserve connects to a connected Fuchsia device and registers the
+# package server with the device.
+
+set -e
+
+# Sets up an ssh mock binary on the $PATH of any subshells and creates a stub
+# authorized_keys.
+set_up_ssh() {
+  PATH_DIR_FOR_TEST="$(mktemp -d)"
+  cp "${BT_TEMP_DIR}/ssh" "${PATH_DIR_FOR_TEST}"
+  export PATH="${PATH_DIR_FOR_TEST}:${PATH}"
+
+  # This authorized_keys file must not be empty, but its contents aren't used.
+  mkdir -p "${BT_TEMP_DIR}/scripts/sdk/gn/base/testdata"
+  echo ssh-ed25519 00000000000000000000000000000000000000000000000000000000000000000000 \
+    >"${BT_TEMP_DIR}/scripts/sdk/gn/base/testdata/authorized_keys"
+}
+
+# Sets up a device-finder mock. The implemented mock aims to produce minimal
+# output that parses correctly but is otherwise uninteresting.
+set_up_device_finder() {
+  cat >"${BT_TEMP_DIR}/scripts/sdk/gn/base/sdk/tools/device-finder.mock_side_effects" <<"SETVAR"
+while (("$#")); do
+  case "$1" in
+  --local)
+    # Emit a different address than the default so the device and the host can
+    # have different IP addresses.
+    echo fe80::1234%coffee
+    exit
+    ;;
+  --full)
+    echo fe80::c0ff:eec0:ffee%coffee coffee-coffee-coffee-coffee
+    exit
+    ;;
+  esac
+  shift
+done
+
+echo fe80::c0ff:eec0:ffee%coffee
+SETVAR
+}
+
+# Sets up a gsutil mock. The implemented mock creates an empty gzipped tarball
+# at the destination of the cp command.
+set_up_gsutil() {
+  cat >"${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/gsutil.mock_side_effects" <<"SETVAR"
+set -x
+
+if [[ "$1" != "cp" ]]; then
+  # Ignore any invocations other than cp.
+  exit 0
+fi
+
+outpath="$3"
+
+mkdir -p "$(dirname "${outpath}")"
+touch foo
+tar czf "${outpath}" foo
+SETVAR
+}
+
+# Creates a stub Core SDK hashes file. The filename is based on the SDK version
+# in manifest.json.
+set_up_sdk_stubs() {
+  # The filename is constructed from the Core SDK version ("id") in the
+  # manifest. See //scripts/sdk/gn/testdata/meta/manifest.json.
+  mkdir -p "${BT_TEMP_DIR}/scripts/sdk/gn/base/images/image"
+  local tarball="${BT_TEMP_DIR}/scripts/sdk/gn/base/images/8890373976687374912_generic-x64.tgz"
+
+  touch foo
+  tar czf "${tarball}" foo
+
+  local hash
+  hash=$(md5sum "${tarball}" | cut -d ' ' -f 1)
+  echo "${hash}  ${tarball}" >"${BT_TEMP_DIR}/scripts/sdk/gn/base/images/image/image.md5"
+}
+
+# Helpers.
+
+# Runs a bash script. The function provides these conveniences over calling the
+# script directly:
+#
+# * Rather than calling the bash script directly, this command explicitly
+#   invokes Bash and propagates some option flags.
+# * Rather than showing the bash output, this command only outputs output if a
+#   test fails.
+#
+# Args: the script to run and all args to pass.
+run_bash_script() {
+  local shell_flags
+  # propagate certain bash flags if present
+  shell_flags=()
+  local status
+  if [[ $- == *x* ]]; then
+    shell_flags+=(-x)
+    # Run command with inherited stdout/stderr.
+    bash "${shell_flags[@]}" "$@"
+    status=$?
+  else
+    # Run command, capturing stdout/stderr. Only output those if the test fails.
+    local output
+    output="$(bash "${shell_flags[@]}" "$@" 2>&1)"
+    status=$?
+    if [[ $status != 0 ]]; then
+      echo "${output}"
+    fi
+  fi
+  return $status
+}
+
+# Verifies that the given arguments appear in the command line invocation of the
+# most previously sourced mock state. Any arguments passed to this function will
+# be searched for in the actual arguments. This succeeds if the arguments are
+# found in adjacent positions in the correct order.
+#
+# This function only checks for presence. As a result, it will NOT verify any of
+# the following:
+#
+# * The arguments only appear once.
+# * The arguments don't appear with conflicting arguments.
+# * Any given argument --foo isn't overridden, say with a --no-foo flag later.
+#
+# Args: any number of arguments to check.
+# Returns: 0 if found; 1 if not found.
+check_mock_has_args() {
+  local expected=("$@")
+  for j in "${!BT_MOCK_ARGS[@]}"; do
+    local window=("${BT_MOCK_ARGS[@]:$j:${#expected}}")
+    local found=true
+    for k in "${!expected[@]}"; do
+      if [[ "${expected[$k]}" != "${window[$k]}" ]]; then
+        found=false
+        break
+      fi
+    done
+    if [[ "${found}" == "true" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
+# Verify that pm serve was run correctly.
+TEST_fserve_starts_package_server() {
+  set_up_ssh
+  set_up_device_finder
+  set_up_gsutil
+  set_up_sdk_stubs
+
+  # Run command.
+  BT_EXPECT run_bash_script "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fserve.sh"
+
+  source "${BT_TEMP_DIR}/scripts/sdk/gn/base/sdk/tools/pm.mock_state"
+
+  BT_EXPECT_EQ 6 ${#BT_MOCK_ARGS[@]}
+  check_mock_has_args serve
+  check_mock_has_args -repo "${BT_TEMP_DIR}/scripts/sdk/gn/base/images/packages/amber-files"
+  check_mock_has_args -l :8083
+
+  # Verify that pm was only run once.
+  BT_EXPECT_FILE_DOES_NOT_EXIST "${BT_TEMP_DIR}/scripts/sdk/gn/base/sdk/tools/pm.mock_state.1"
+}
+
+# Verifies that the package server has been correctly registered with the
+# Fuchsia device.
+TEST_fserve_registers_package_server() {
+  set_up_ssh
+  set_up_device_finder
+  set_up_gsutil
+  set_up_sdk_stubs
+
+  # Run command.
+  BT_EXPECT run_bash_script "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fserve.sh"
+
+  source "${PATH_DIR_FOR_TEST}/ssh.mock_state"
+
+  BT_EXPECT_EQ 8 "${#BT_MOCK_ARGS[@]}"
+  check_mock_has_args -F "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/sshconfig"
+  check_mock_has_args fe80::c0ff:eec0:ffee%coffee
+  check_mock_has_args amber_ctl add_src -f "http://[fe80::c0ff:eec0:ffee]:8083/config.json"
+
+  # Verify that ssh was only run once.
+  BT_EXPECT_FILE_DOES_NOT_EXIST "${PATH_DIR_FOR_TEST}/ssh.mock_state.1"
+}
+
+# Test initialization.
+BT_FILE_DEPS=(
+  scripts/sdk/gn/base/bin/fserve.sh
+  scripts/sdk/gn/base/bin/fuchsia-common.sh
+)
+BT_MOCKED_TOOLS=(
+  scripts/sdk/gn/base/bin/gsutil
+  scripts/sdk/gn/base/sdk/tools/device-finder
+  scripts/sdk/gn/base/sdk/tools/pm
+  ssh
+)
+
+BT_INIT_TEMP_DIR() {
+  mkdir -p "${BT_TEMP_DIR}/scripts/sdk/gn/base/sdk/meta"
+
+  # Create a stub SDK manifest.
+  cp "${BT_DEPS_ROOT}/scripts/sdk/gn/testdata/meta/manifest.json" \
+    "${BT_TEMP_DIR}/scripts/sdk/gn/base/sdk/meta/manifest.json"
+}
+
+BT_RUN_TESTS "$@"
