5d9bfef8d6dfe32762386bec52da904da57d46ab
Mark Seaborn <mseaborn@google.com>
2020-02-12 04:48:18 +0000

    [microbenchmarks] Remove unused RegisterTestFunc() function
    Bug: none
    Test: "fx set terminal.x64" + build
    Change-Id: Ibb56f99237e98c65efe384a6378a82f3319c12b1

--- src/tests/microbenchmarks/test_runner.h
+++ src/tests/microbenchmarks/test_runner.h
@@ -23,23 +23,4 @@ void RegisterTest(const char* test_name, Args... args) {
   });
 }
 
-typedef void TestFunc();
-
-// Convenience routine for registering a benchmark that is specified by a
-// function.  This is for benchmarks that don't set up any fixtures that
-// are shared across invocations of the function.
-//
-// This takes the function as a template parameter rather than as a value
-// parameter in order to avoid the potential cost of an indirect function
-// call and hence be consistent with RegisterTest() (which also avoids an
-// indirect function call).
-template <TestFunc test_func>
-void RegisterTestFunc(const char* test_name) {
-  class TestClass {
-   public:
-    void Run() { test_func(); }
-  };
-  RegisterTest<TestClass>(test_name);
-}
-
 }  // namespace fbenchmark
