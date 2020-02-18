4d76ea25b456c9e05a54055603965a34abf5e964
Rich Kadel <richkadel@google.com>
2020-02-18 19:04:20 +0000

    [test_util] Adding Counter for tests
    Adds a simple mutex Counter implementation that can simplify
    implementing runtime counts scoped to tests, to verify expected
    execution paths are taken.
    
    Test: cargo test (passes unit tests and even doc tests)
    Change-Id: Ie4099c99fad9fd2886323b7224a8d81dfbd847f3

--- src/lib/test_util/src/lib.rs
+++ src/lib/test_util/src/lib.rs
@@ -1,4 +1,10 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 //! This crate defines a collection of useful utilities for testing rust code.
+
+use std::sync::Mutex;
 
 /// Asserts that an expression matches some expected pattern. The pattern may
 /// be any valid rust pattern, which includes multiple patterns (`Foo | Bar`)
@@ -10,6 +16,8 @@
 /// # Examples
 ///
 /// ```
+/// use test_util::assert_matches;
+///
 /// ##[derive(Debug)]
 /// enum Foo {
 ///     Bar(String),
@@ -17,11 +25,11 @@
 /// }
 ///
 /// # fn main() {
-/// let a = Foo::Baz;
-/// assert_matches!(a, Foo::Baz);
+///     let a = Foo::Baz;
+///     assert_matches!(a, Foo::Baz);
 ///
-/// let b = Foo::Bar("foo".to_owned());
-/// assert_matches!(b, Foo::Bar(ref val) if val == "foo");
+///     let b = Foo::Bar("foo".to_owned());
+///     assert_matches!(b, Foo::Bar(ref val) if val == "foo");
 /// # }
 #[macro_export]
 macro_rules! assert_matches {
@@ -53,8 +61,67 @@ macro_rules! assert_matches {
     };
 }
 
+/// A mutually exclusive counter that is not shareable, but can be defined statically for the
+/// duration of a test. This simplifies the implementation of a simple test-global counter,
+/// avoiding the complexity of alternatives like std::sync::atomic objects that are typically
+/// wrapped in Arc()s, cloned, and require non-intuitive memory management options.
+///
+/// # Example
+///
+/// ```
+///    use test_util::Counter;
+///    use lazy_static::lazy_static;
+///
+///    #[test]
+///    async fn my_test() {
+///        lazy_static! {
+///            static ref CALL_COUNT: Counter = Counter::new(0);
+///        }
+///
+///        let handler = || {
+///            // some async callback
+///            // ...
+///            CALL_COUNT.inc();
+///        };
+///        handler();
+///        // ...
+///        CALL_COUNT.inc();
+///
+///        assert_eq!(CALL_COUNT.get(), 2);
+///    }
+/// ```
+pub struct Counter {
+    count: Mutex<usize>,
+}
+
+impl Counter {
+    /// Initializes a new counter to the given value.
+    pub fn new(initial: usize) -> Self {
+        Counter { count: Mutex::new(initial) }
+    }
+
+    /// Increments the counter by one.
+    pub fn inc(&self) {
+        *self.count.lock().unwrap() += 1;
+    }
+
+    /// Returns the current value of the counter.
+    pub fn get(&self) -> usize {
+        *self.count.lock().unwrap()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use {
+        super::*,
+        lazy_static::lazy_static,
+        std::collections::BTreeSet,
+        std::sync::mpsc,
+        std::sync::mpsc::{Receiver, Sender},
+        std::thread,
+    };
+
     #[derive(Debug)]
     enum Foo {
         Bar(String),
@@ -121,5 +188,69 @@ mod tests {
         if let Foo::Bar(ref val) = input {
             assert_eq!(val, "foo");
         }
+    }
+
+    #[test]
+    fn test_inc() {
+        lazy_static! {
+            static ref CALL_COUNT: Counter = Counter::new(0);
+        }
+
+        CALL_COUNT.inc();
+
+        assert_eq!(CALL_COUNT.get(), 1);
+    }
+
+    #[test]
+    fn test_incs_from_10() {
+        lazy_static! {
+            static ref CALL_COUNT: Counter = Counter::new(10);
+        }
+
+        CALL_COUNT.inc();
+        CALL_COUNT.inc();
+        CALL_COUNT.inc();
+
+        assert_eq!(CALL_COUNT.get(), 13);
+    }
+
+    #[test]
+    fn async_counts() {
+        lazy_static! {
+            static ref CALL_COUNT: Counter = Counter::new(0);
+        }
+
+        let (tx, rx): (Sender<usize>, Receiver<usize>) = mpsc::channel();
+        let mut children = Vec::new();
+
+        static NTHREADS: usize = 3;
+
+        for _ in 0..NTHREADS {
+            let thread_tx = tx.clone();
+            let child = thread::spawn(move || {
+                CALL_COUNT.inc();
+                thread_tx.send(CALL_COUNT.get()).unwrap();
+            });
+
+            children.push(child);
+        }
+
+        let mut ordered_ids = BTreeSet::new();
+        for _ in 0..NTHREADS {
+            ordered_ids.insert(rx.recv().unwrap());
+        }
+
+        // Wait for the threads to complete any remaining work
+        for child in children {
+            child.join().expect("child thread panicked");
+        }
+
+        let mut expected_id: usize = 1;
+        for id in ordered_ids.iter() {
+            assert!(*id == expected_id);
+            expected_id += 1;
+        }
+
+        assert_eq!(CALL_COUNT.get(), NTHREADS);
     }
 }
