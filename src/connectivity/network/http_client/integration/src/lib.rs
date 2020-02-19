66865a7ee60790a0f18146a696d378497b859f4d
Sarah Chan <spqchan@google.com>
2020-02-19 23:33:23 +0000

    Revert "[fuchsia.net.http] Implement http_client"
    This reverts commit 479ad2e3f2d3a43669df3d62267a159f67889d37.
    
    Reason for revert: Breaking the build
    
    Original change's description:
    > [fuchsia.net.http] Implement http_client
    > 
    > This implements all the methods in the fuchsia.net.http fidl service
    > using hyper in rust.
    > 
    > Change-Id: I980c634d6bf827211817c54bcb31ca6a078fce98
    
    TBR=abarth@google.com,zmbush@google.com
    
    Change-Id: I03c4a7960567838c76ff943eaec6a12cd9e45643
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- src/connectivity/network/http_client/integration/src/lib.rs
+++ src/connectivity/network/http_client/integration/src/lib.rs
@@ -6,14 +6,10 @@
 
 use {
     anyhow::{Context as _, Error},
-    fidl::{endpoints::RequestStream, handle::fuchsia_handles::Channel},
-    fidl_fuchsia_net_http as http, fuchsia_async as fasync,
-    fuchsia_component::{
-        client::{launch, launcher, App},
-        fuchsia_single_component_package_url,
-    },
-    fuchsia_zircon as zx,
-    futures::prelude::*,
+    fidl::endpoints::create_proxy,
+    fidl_fuchsia_net_oldhttp as oldhttp, fuchsia_async as fasync,
+    fuchsia_component::client::{launch, launcher},
+    fuchsia_component::fuchsia_single_component_package_url,
     rouille::{self, router, Request, Response},
     std::thread,
 };
@@ -26,257 +22,79 @@ pub fn serve_request(request: &Request) -> Response {
         (GET) (/) => {
             rouille::Response::text(ROOT_DOCUMENT)
         },
-        (GET) (/trigger_301) => {
-            rouille::Response::redirect_301("/")
-        },
-        (POST) (/see_other) => {
-            rouille::Response::redirect_303("/")
-        },
-        (GET) (/loop1) => {
-            rouille::Response::redirect_301("/loop2")
-        },
-        (GET) (/loop2) => {
-            rouille::Response::redirect_301("/loop1")
-        },
         _ => {
             rouille::Response::text("File not found\n").with_status_code(404)
         }
     )
 }
 
-fn setup() -> Result<(u16, App, http::LoaderProxy), Error> {
+pub fn start_test_server() -> Result<u16, Error> {
     let address = format!("{}:0", SERVER_IP);
     let server =
         rouille::Server::new(address, serve_request).expect("failed to create rouille server");
-    let server_port = server.server_addr().port();
+    let port = server.server_addr().port();
     thread::Builder::new().name("test-server".into()).spawn(move || {
         server.run();
     })?;
 
+    Ok(port)
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_new_client() -> Result<(), Error> {
+    test_oldhttp(fuchsia_single_component_package_url!("http_client"), false).await
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_old_client() -> Result<(), Error> {
+    test_oldhttp(fuchsia_single_component_package_url!("http"), true).await
+}
+
+async fn test_oldhttp(package_path: &str, check_body: bool) -> Result<(), Error> {
+    let server_port = start_test_server()?;
+
     let launcher = launcher().context("Failed to open launcher service")?;
-    let http_client =
-        launch(&launcher, fuchsia_single_component_package_url!("http_client").to_string(), None)
-            .context("Failed to launch http_client")?;
+    let http_client = launch(&launcher, package_path.to_string(), None)
+        .context("Failed to launch http_client")?;
 
-    let loader = http_client.connect_to_service::<http::LoaderMarker>()?;
+    let (loader, loader_server) = create_proxy::<oldhttp::UrlLoaderMarker>()?;
+    let service = http_client.connect_to_service::<oldhttp::HttpServiceMarker>()?;
+    service.create_url_loader(loader_server)?;
 
-    Ok((server_port, http_client, loader))
-}
-
-fn make_url(port: u16, path: &str) -> Vec<u8> {
-    format!("http://127.0.0.1:{}{}", port, path).as_bytes().to_vec()
-}
-
-fn make_request(port: u16, method: &str, path: &str) -> http::Request {
-    http::Request {
-        url: Some(make_url(port, path)),
-        method: Some(method.to_string()),
+    let mut request = oldhttp::UrlRequest {
+        url: format!("http://127.0.0.1:{}/", server_port).to_string(),
+        method: "GET".to_string(),
         headers: None,
         body: None,
-    }
-}
+        response_body_buffer_size: 0,
+        cache_mode: oldhttp::CacheMode::Default,
+        response_body_mode: oldhttp::ResponseBodyMode::Buffer,
+        auto_follow_redirects: false,
+    };
+    let response = loader.start(&mut request).await?;
 
-fn check_response(response: &http::Response) {
-    assert_eq!(response.status_code.unwrap(), 200);
+    assert_eq!(response.status_code, 200);
     let expected_header_names = ["server", "date", "content-type", "content-length"];
     // If the webserver started above ever returns different headers, or changes the order, this
     // assertion will fail. Note that case doesn't matter, and can vary across HTTP client
     // implementations, so we lowercase all the header keys before checking.
-    let response_headers: Vec<String> = response
-        .headers
-        .as_ref()
-        .unwrap()
-        .iter()
-        .map(|h| std::str::from_utf8(&h.name).unwrap().to_lowercase())
-        .collect();
+    let response_headers: Vec<String> =
+        response.headers.unwrap().iter().map(|h| h.name.to_lowercase()).collect();
     assert_eq!(&response_headers, &expected_header_names);
-}
 
-fn check_body(body: Option<zx::Socket>) {
-    let body = body.unwrap();
-
-    // Temporary check until we can use const fn when defining buf below.
-    assert_eq!(ROOT_DOCUMENT.len(), 14);
-    let mut buf = [0; 14];
-    loop {
-        match body.read(&mut buf) {
-            Ok(_) => break,
-            Err(zx::Status::SHOULD_WAIT) => {}
-            _ => panic!("This state should not be reachable"),
+    if check_body {
+        match *response.body.unwrap() {
+            oldhttp::UrlBody::Buffer(b) => {
+                // Temporary check until we can use const fn when defining buf below.
+                assert_eq!(ROOT_DOCUMENT.len(), 14);
+                assert_eq!(b.size, ROOT_DOCUMENT.len() as u64);
+                let mut buf = [0; 14];
+                b.vmo.read(&mut buf, 0).unwrap();
+                assert_eq!(&buf, ROOT_DOCUMENT.as_bytes());
+            }
+            oldhttp::UrlBody::Stream(_) => panic!("Unexpected stream for response body"),
         }
     }
-    assert_eq!(&buf, ROOT_DOCUMENT.as_bytes());
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_fetch_http() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let response = loader.fetch(make_request(server_port, "GET", "/")).await?;
-
-    check_response(&response);
-    check_body(response.body);
-
-    Ok(())
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_start_http() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let (tx, rx) = Channel::create()?;
-    let mut rs = http::LoaderClientRequestStream::from_channel(fasync::Channel::from_channel(rx)?);
-
-    loader.start(make_request(server_port, "GET", "/"), tx.into())?;
-
-    let (response, responder) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    check_response(&response);
-    check_body(response.body);
-
-    responder.send()?;
-
-    Ok(())
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_fetch_redirect() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let response = loader.fetch(make_request(server_port, "GET", "/trigger_301")).await?;
-
-    check_response(&response);
-
-    assert_eq!(response.final_url.unwrap(), make_url(server_port, "/"));
-
-    check_body(response.body);
-
-    Ok(())
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_start_redirect() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let (tx, rx) = Channel::create()?;
-    let mut rs = http::LoaderClientRequestStream::from_channel(fasync::Channel::from_channel(rx)?);
-
-    loader.start(make_request(server_port, "GET", "/trigger_301"), tx.into())?;
-
-    let (response, responder) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    assert_eq!(response.status_code.unwrap(), 301);
-
-    let redirect = response.redirect.unwrap();
-    assert_eq!(redirect.method.unwrap(), "GET");
-    assert_eq!(redirect.url.unwrap(), make_url(server_port, "/"));
-
-    responder.send()?;
-
-    let (response, _) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    check_response(&response);
-    assert_eq!(response.final_url.unwrap(), make_url(server_port, "/"));
-    check_body(response.body);
-
-    Ok(())
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_fetch_see_other() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let response = loader.fetch(make_request(server_port, "POST", "/see_other")).await?;
-
-    check_response(&response);
-
-    assert_eq!(response.final_url.unwrap(), make_url(server_port, "/"));
-
-    check_body(response.body);
-
-    Ok(())
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_start_see_other() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let (tx, rx) = Channel::create()?;
-    let mut rs = http::LoaderClientRequestStream::from_channel(fasync::Channel::from_channel(rx)?);
-
-    loader.start(make_request(server_port, "POST", "/see_other"), tx.into())?;
-
-    let (response, responder) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    assert_eq!(response.status_code.unwrap(), 303);
-
-    let redirect = response.redirect.unwrap();
-    assert_eq!(redirect.method.unwrap(), "GET");
-    assert_eq!(redirect.url.unwrap(), make_url(server_port, "/"));
-
-    responder.send()?;
-
-    let (response, _) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    check_response(&response);
-    assert_eq!(response.final_url.unwrap(), make_url(server_port, "/"));
-    check_body(response.body);
-
-    Ok(())
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_fetch_max_redirect() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let response = loader.fetch(make_request(server_port, "GET", "/loop1")).await?;
-
-    // The last request in the redirect loop will always return status code 301
-    assert_eq!(response.status_code.unwrap(), 301);
-
-    let redirect = response.redirect.unwrap();
-    assert_eq!(redirect.method.unwrap(), "GET");
-
-    Ok(())
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_start_redirect_loop() -> Result<(), Error> {
-    let (server_port, _http_client, loader) = setup()?;
-
-    let (tx, rx) = Channel::create()?;
-    let mut rs = http::LoaderClientRequestStream::from_channel(fasync::Channel::from_channel(rx)?);
-
-    loader.start(make_request(server_port, "GET", "/loop1"), tx.into())?;
-
-    let (response, responder) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    assert_eq!(response.status_code.unwrap(), 301);
-
-    let redirect = response.redirect.unwrap();
-    assert_eq!(redirect.method.unwrap(), "GET");
-    assert_eq!(redirect.url.unwrap(), make_url(server_port, "/loop2"));
-
-    responder.send()?;
-
-    let (response, responder) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    assert_eq!(response.status_code.unwrap(), 301);
-
-    let redirect = response.redirect.unwrap();
-    assert_eq!(redirect.method.unwrap(), "GET");
-    assert_eq!(redirect.url.unwrap(), make_url(server_port, "/loop1"));
-
-    responder.send()?;
-
-    let (response, responder) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    assert_eq!(response.status_code.unwrap(), 301);
-
-    let redirect = response.redirect.unwrap();
-    assert_eq!(redirect.method.unwrap(), "GET");
-    assert_eq!(redirect.url.unwrap(), make_url(server_port, "/loop2"));
-
-    responder.send()?;
-
-    let (response, _) = rs.next().await.unwrap()?.into_on_response().unwrap();
-    assert_eq!(response.status_code.unwrap(), 301);
-
-    let redirect = response.redirect.unwrap();
-    assert_eq!(redirect.method.unwrap(), "GET");
-    assert_eq!(redirect.url.unwrap(), make_url(server_port, "/loop1"));
 
     Ok(())
 }
