3c995ced63d5632dcda8d8e766692a5d664f82c3
Shai Barack <shayba@google.com>
2020-02-20 01:17:01 +0000

    [lsan] Fix fd leak
    Test: verified that lsan build no longer complains about the has_isolated_cache_storage test, which triggers the original leak.
    (lsan tryjobs still fail, just not on this test)
    
    Bug: 46201 #fixed
    Change-Id: I4bed327b2d19e156c8daebfe7e563387da191191

--- src/lib/files/directory.cc
+++ src/lib/files/directory.cc
@@ -78,6 +78,7 @@ bool ReadDirContentsAt(int root_fd, const std::string& path, std::vector<std::st
   }
   DIR* dir = fdopendir(fd);
   if (!dir) {
+    close(fd);
     return false;
   }
   struct dirent* de;
