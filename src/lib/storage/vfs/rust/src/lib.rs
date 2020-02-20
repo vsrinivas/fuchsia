c9fdf2fdb46ea753b0cb842837a3e8a028cee370
Stephen Demos <sdemos@google.com>
2020-02-20 00:41:35 +0000

    [vfs] remove unnecessary allows
    they don't seem to be doing anything anymore. one more TODO checked off
    the list!
    
    Test: fx run-test vfs_tests
    Change-Id: Ied21160ed0bc8189f2c7daf98decb741657d6be7

--- src/lib/storage/vfs/rust/src/lib.rs
+++ src/lib/storage/vfs/rust/src/lib.rs
@@ -6,9 +6,6 @@
 //! callbacks.  Examples are: component configuration, debug information or statistics.
 
 #![recursion_limit = "1024"]
-// TODO Follow 2018 idioms
-#![allow(elided_lifetimes_in_paths)]
-#![allow(unused_extern_crates)]
 
 use proc_macro_hack::proc_macro_hack;
 
