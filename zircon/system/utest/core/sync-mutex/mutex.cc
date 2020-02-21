ed4aab806ecc4780237b938943491acb6f6cdb54
Scott Graham <scottmg@google.com>
2020-02-21 17:27:54 +0000

    [zxtest][sync-mutex] Move utest/core/sync-mutex to zxtest
    Also make it .cc while here. No intended functionality change.
    
    Bug: 33529
    Change-Id: I70c417b131328a4fef80c9cd47b8fdeb6fbbf0b8

--- zircon/system/utest/core/sync-mutex/mutex.cc
+++ zircon/system/utest/core/sync-mutex/mutex.cc
@@ -14,13 +14,13 @@
 #include <zircon/time.h>
 #include <zircon/types.h>
 
-#include <unittest/unittest.h>
+#include <zxtest/zxtest.h>
 
-static sync_mutex_t g_mutex = SYNC_MUTEX_INIT;
+static sync_mutex_t g_mutex;
 
 static void xlog(const char* str) {
   zx_time_t now = zx_clock_get_monotonic();
-  unittest_printf("[%08" PRIu64 ".%08" PRIu64 "]: %s", now / 1000000000, now % 1000000000, str);
+  printf("[%08" PRIu64 ".%08" PRIu64 "]: %s", now / 1000000000, now % 1000000000, str);
 }
 
 static int mutex_thread_1(void* arg) {
@@ -116,38 +116,32 @@ static int mutex_try_thread_3(void* arg) TA_NO_THREAD_SAFETY_ANALYSIS {
   return 0;
 }
 
-static bool test_mutexes(void) {
-  BEGIN_TEST;
+TEST(SyncMutex, Mutexes) {
   thrd_t thread1, thread2, thread3;
 
-  thrd_create_with_name(&thread1, mutex_thread_1, NULL, "thread 1");
-  thrd_create_with_name(&thread2, mutex_thread_2, NULL, "thread 2");
-  thrd_create_with_name(&thread3, mutex_thread_3, NULL, "thread 3");
+  thrd_create_with_name(&thread1, mutex_thread_1, nullptr, "thread 1");
+  thrd_create_with_name(&thread2, mutex_thread_2, nullptr, "thread 2");
+  thrd_create_with_name(&thread3, mutex_thread_3, nullptr, "thread 3");
 
-  thrd_join(thread1, NULL);
-  thrd_join(thread2, NULL);
-  thrd_join(thread3, NULL);
-
-  END_TEST;
+  thrd_join(thread1, nullptr);
+  thrd_join(thread2, nullptr);
+  thrd_join(thread3, nullptr);
 }
 
-static bool test_try_mutexes(void) {
-  BEGIN_TEST;
+TEST(SyncMutex, TryMutexes) {
   thrd_t thread1, thread2, thread3;
 
-  thrd_create_with_name(&thread1, mutex_try_thread_1, NULL, "thread 1");
-  thrd_create_with_name(&thread2, mutex_try_thread_2, NULL, "thread 2");
-  thrd_create_with_name(&thread3, mutex_try_thread_3, NULL, "thread 3");
+  thrd_create_with_name(&thread1, mutex_try_thread_1, nullptr, "thread 1");
+  thrd_create_with_name(&thread2, mutex_try_thread_2, nullptr, "thread 2");
+  thrd_create_with_name(&thread3, mutex_try_thread_3, nullptr, "thread 3");
 
-  thrd_join(thread1, NULL);
-  thrd_join(thread2, NULL);
-  thrd_join(thread3, NULL);
+  thrd_join(thread1, nullptr);
+  thrd_join(thread2, nullptr);
+  thrd_join(thread3, nullptr);
 
   EXPECT_TRUE(got_lock_1, "failed to get lock 1");
   EXPECT_TRUE(got_lock_2, "failed to get lock 2");
   EXPECT_TRUE(got_lock_3, "failed to get lock 3");
-
-  END_TEST;
 }
 
 typedef struct {
@@ -157,32 +151,29 @@ typedef struct {
 } timeout_args;
 
 static int test_timeout_helper(void* ctx) TA_NO_THREAD_SAFETY_ANALYSIS {
-  timeout_args* args = ctx;
+  timeout_args* args = reinterpret_cast<timeout_args*>(ctx);
   sync_mutex_lock(&args->mutex);
   // Inform the main thread that we have acquired the lock.
-  ASSERT_EQ(zx_object_signal(args->start_event, 0, ZX_EVENT_SIGNALED), ZX_OK, "failed to signal");
+  ZX_ASSERT(zx_object_signal(args->start_event, 0, ZX_EVENT_SIGNALED) == ZX_OK);
   // Wait until the main thread has completed its test.
-  ASSERT_EQ(zx_object_wait_one(args->done_event, ZX_EVENT_SIGNALED, ZX_TIME_INFINITE, NULL), ZX_OK,
-            "failed to wait");
+  ZX_ASSERT(zx_object_wait_one(args->done_event, ZX_EVENT_SIGNALED, ZX_TIME_INFINITE, nullptr) ==
+            ZX_OK);
   sync_mutex_unlock(&args->mutex);
   return 0;
 }
 
-static bool test_timeout_elapsed(void) {
-  BEGIN_TEST;
-
+TEST(SyncMutex, TimeoutElapsed) {
   const zx_duration_t kRelativeDeadline = ZX_MSEC(100);
 
   timeout_args args;
-  args.mutex = SYNC_MUTEX_INIT;
   ASSERT_EQ(zx_event_create(0, &args.start_event), ZX_OK, "could not create event");
   ASSERT_EQ(zx_event_create(0, &args.done_event), ZX_OK, "could not create event");
 
   thrd_t helper;
   ASSERT_EQ(thrd_create(&helper, test_timeout_helper, &args), thrd_success, "");
   // Wait for the helper thread to acquire the lock.
-  ASSERT_EQ(zx_object_wait_one(args.start_event, ZX_EVENT_SIGNALED, ZX_TIME_INFINITE, NULL), ZX_OK,
-            "failed to wait");
+  ASSERT_EQ(zx_object_wait_one(args.start_event, ZX_EVENT_SIGNALED, ZX_TIME_INFINITE, nullptr),
+            ZX_OK, "failed to wait");
 
   for (int i = 0; i < 5; ++i) {
     zx_time_t now = zx_clock_get_monotonic();
@@ -194,16 +185,8 @@ static bool test_timeout_elapsed(void) {
 
   // Inform the helper thread that we are done.
   ASSERT_EQ(zx_object_signal(args.done_event, 0, ZX_EVENT_SIGNALED), ZX_OK, "failed to signal");
-  ASSERT_EQ(thrd_join(helper, NULL), thrd_success, "failed to join");
+  ASSERT_EQ(thrd_join(helper, nullptr), thrd_success, "failed to join");
 
   ASSERT_EQ(zx_handle_close(args.start_event), ZX_OK, "failed to close event");
   ASSERT_EQ(zx_handle_close(args.done_event), ZX_OK, "failed to close event");
-
-  END_TEST;
 }
-
-BEGIN_TEST_CASE(sync_mutex_tests)
-RUN_TEST(test_mutexes)
-RUN_TEST(test_try_mutexes)
-RUN_TEST(test_timeout_elapsed)
-END_TEST_CASE(sync_mutex_tests)
