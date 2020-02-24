4db51342c24955b1a3119e3b15b2fb9baddb0578
Filip Filmar <fmil@google.com>
2020-02-24 20:44:46 +0000

    [i18n] Handles test flakiness in intl_services
    This change should handle the test flakiness in intl_services_test.
    IIUC, the issue was that we set a test value, then read it
    asynchronously without receiving an OnChange signal, which acknowledges
    an update of the applied value.
    
    This could cause a subsequent read to read the old value of the profile,
    and not the one we have set.
    
    While I'm here, I added `FIDLError` and `Timeout` methods to the test
    fixture to make error checks easier, and to put an upper bound to the
    wait in the test case if something goes wrong in the event processing.
    
    Bug: 46196
    Change-Id: I46e2b9f37893dcf19ff756b2ea26d8d82b92dc30

--- src/intl/intl_services/tests/intl_services_test.cc
+++ src/intl/intl_services/tests/intl_services_test.cc
@@ -7,6 +7,7 @@
 #include <lib/sys/cpp/component_context.h>
 #include <lib/sys/cpp/service_directory.h>
 #include <lib/sys/cpp/testing/test_with_environment.h>
+#include <lib/zx/time.h>
 #include <zircon/status.h>
 
 using fuchsia::intl::LocaleId;
@@ -17,11 +18,14 @@ using fuchsia::settings::Error;
 using fuchsia::settings::HourCycle;
 using fuchsia::settings::IntlSettings;
 
+static const int32_t kTimeoutSec = 10;
+
 // See README.md for more detail about this test.
 class IntlServicesTest : public sys::testing::TestWithEnvironment {
  public:
   IntlServicesTest()
-      : ctx_(sys::ComponentContext::Create()),
+      : deadline_(zx::clock::get_monotonic() + zx::sec(kTimeoutSec)),
+        ctx_(sys::ComponentContext::Create()),
         settings_intl_(ctx_->svc()->Connect<fuchsia::settings::Intl>()),
         intl_property_provider_(ctx_->svc()->Connect<fuchsia::intl::PropertyProvider>()),
         settings_intl_status_(ZX_OK),
@@ -32,7 +36,20 @@ class IntlServicesTest : public sys::testing::TestWithEnvironment {
         [this](zx_status_t status) { intl_property_provider_status_ = status; });
   }
 
+  // Returns true if any error occurred in the FIDL roundtrip.
+  bool FIDLError() const {
+    return intl_property_provider_status_ != ZX_OK || settings_intl_status_ != ZX_OK;
+  }
+
+  // Returns true if timeout occurred.  Used so that the tests do not block.
+  bool Timeout() const {
+    zx::time now = zx::clock::get_monotonic();
+    return now > deadline_;
+  }
+
  protected:
+  zx::time deadline_;
+
   std::unique_ptr<sys::ComponentContext> ctx_;
   fuchsia::settings::IntlPtr settings_intl_;
   fuchsia::intl::PropertyProviderPtr intl_property_provider_;
@@ -41,67 +58,52 @@ class IntlServicesTest : public sys::testing::TestWithEnvironment {
   zx_status_t intl_property_provider_status_;
 };
 
-TEST_F(IntlServicesTest, Basic) {
-  {
-    IntlSettings settings;
-    settings.set_locales({LocaleId{.id = "ru-RU"}});
-    settings.set_time_zone_id(TimeZoneId{.id = "Europe/Moscow"});
-    settings.set_temperature_unit(TemperatureUnit::CELSIUS);
-    settings.set_hour_cycle(HourCycle::H23);
-    bool completed{};
-    fit::result<void, Error> result;
-    settings_intl_->Set(std::move(settings), [&](fit::result<void, Error> res) {
-      result = res;
-      completed = true;
-    });
-    RunLoopUntil([&] { return completed || settings_intl_status_ != ZX_OK; });
-    ASSERT_EQ(ZX_OK, settings_intl_status_) << zx_status_get_string(settings_intl_status_);
-    ASSERT_TRUE(result.is_ok());
-    ASSERT_TRUE(completed);
-  }
-  {
-    bool completed{};
-    Profile result;
-    intl_property_provider_->GetProfile([&](fuchsia::intl::Profile res) {
-      result = std::move(res);
-      completed = true;
-    });
-    RunLoopUntil([&] { return completed || intl_property_provider_status_ != ZX_OK; });
-    ASSERT_EQ(ZX_OK, intl_property_provider_status_)
-        << zx_status_get_string(intl_property_provider_status_);
-    ASSERT_TRUE(completed);
-    EXPECT_EQ(TemperatureUnit::CELSIUS, result.temperature_unit());
-    EXPECT_EQ("Europe/Moscow", result.time_zones()[0].id);
-    EXPECT_EQ("ru-RU-u-ca-gregory-fw-mon-hc-h23-ms-metric-nu-latn-tz-rumow", result.locales()[0].id)
-        << "Expected BCP-47 locale";
-  }
-  {
-    IntlSettings settings;
-    settings.set_locales({LocaleId{.id = "ru-RU"}});
-    settings.set_time_zone_id(TimeZoneId{.id = "Europe/Moscow"});
-    settings.set_temperature_unit(TemperatureUnit::FAHRENHEIT);
-    bool completed{};
-    fit::result<void, Error> result;
-    settings_intl_->Set(std::move(settings), [&](fit::result<void, Error> res) {
-      result = res;
-      completed = true;
-    });
-    RunLoopUntil([&] { return completed || settings_intl_status_ != ZX_OK; });
-    ASSERT_EQ(ZX_OK, settings_intl_status_) << zx_status_get_string(settings_intl_status_);
-    ASSERT_TRUE(result.is_ok());
-    ASSERT_TRUE(completed);
-  }
-  {
-    bool completed{};
-    Profile result;
-    intl_property_provider_->GetProfile([&](fuchsia::intl::Profile res) {
-      result = std::move(res);
-      completed = true;
-    });
-    RunLoopUntil([&] { return completed || intl_property_provider_status_ != ZX_OK; });
-    ASSERT_EQ(ZX_OK, intl_property_provider_status_)
-        << zx_status_get_string(intl_property_provider_status_);
-    ASSERT_TRUE(completed);
-    EXPECT_EQ(TemperatureUnit::FAHRENHEIT, result.temperature_unit());
-  }
+TEST_F(IntlServicesTest, AsyncSetThenGet) {
+  IntlSettings settings;
+  settings.set_locales({LocaleId{.id = "ru-RU"}});
+  settings.set_time_zone_id(TimeZoneId{.id = "Europe/Moscow"});
+  settings.set_temperature_unit(TemperatureUnit::CELSIUS);
+  settings.set_hour_cycle(HourCycle::H23);
+
+  Profile get_result;
+  bool get_completed{};
+  auto get_callback = [&](fuchsia::intl::Profile res) {
+    get_result = std::move(res);
+    get_completed = true;
+  };
+
+  bool on_change_completed{};
+  intl_property_provider_.events().OnChange = [&] {
+    // Reading the profile before OnChange arrives will cause a data race and
+    // make result comparison flaky in the test.
+    intl_property_provider_->GetProfile(get_callback);
+    on_change_completed = true;
+  };
+
+  fit::result<void, Error> set_result;
+  bool set_completed{};
+  auto set_callback = [&](fit::result<void, Error> res) {
+    set_result = std::move(res);
+    set_completed = true;
+  };
+  settings_intl_->Set(std::move(settings), set_callback);
+  RunLoopUntil([&] {
+    return (set_completed && get_completed && on_change_completed) || FIDLError() || Timeout();
+  });
+
+  ASSERT_EQ(ZX_OK, settings_intl_status_) << zx_status_get_string(settings_intl_status_);
+  ASSERT_EQ(ZX_OK, intl_property_provider_status_)
+      << zx_status_get_string(intl_property_provider_status_);
+
+  // The test should normally run for a fraction of a second, so even though this measures time
+  // *after* the test events completed, it should not matter for timeout checks.
+  ASSERT_FALSE(Timeout()) << "Test took too long to complete";
+
+  ASSERT_TRUE(set_result.is_ok());
+
+  EXPECT_EQ(TemperatureUnit::CELSIUS, get_result.temperature_unit());
+  EXPECT_EQ("Europe/Moscow", get_result.time_zones()[0].id);
+  EXPECT_EQ("ru-RU-u-ca-gregory-fw-mon-hc-h23-ms-metric-nu-latn-tz-rumow",
+            get_result.locales()[0].id)
+      << "Expected BCP-47 locale";
 }
