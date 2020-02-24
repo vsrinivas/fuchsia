72fe8cb1e053e4705aedd7e0acd06ea84ca41b58
Randall Bosetti <rlb@google.com>
2020-02-24 16:14:22 +0000

    Revert "[zircon][display] Always send vsyncs."
    This reverts commit c0a5f769183a3b3b00ba0b3547038f3fe265f5b8.
    
    Reason for revert: Tickles a shutdown bug in display-core.
    
    Original change's description:
    > [zircon][display] Always send vsyncs.
    > 
    > Test: Boot and display-tests on NUC, Pixelbook x150
    > Test: scenic_tests and zircon sysmem tests on Astro x150
    > Change-Id: I1ed641cf447d0e93bb25b6f41b66cd7e6574275b
    > 
    > MULTIPLY: `[
    >   {
    >     "name": "scenic_tests",
    >     "os": "fuchsia",
    >     "total_runs": 40
    >   },
    >   {
    >     "name": "sysmem-test",
    >     "os": "fuchsia",
    >     "total_runs": 40
    >   }
    > ]`
    > Change-Id: I1ed641cf447d0e93bb25b6f41b66cd7e6574275b
    
    TBR=jbauman@google.com,jeremymanson@google.com,nicoh@google.com,rlb@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Ia4451ef64dc7225e0843ce2c507800f483b08329

--- src/graphics/display/drivers/intel-i915/intel-i915.cc
+++ src/graphics/display/drivers/intel-i915/intel-i915.cc
@@ -285,7 +285,7 @@ void Controller::HandlePipeVsync(registers::Pipe pipe, zx_time_t timestamp) {
     }
   }
 
-  if (id != INVALID_DISPLAY_ID) {
+  if (id != INVALID_DISPLAY_ID && handle_count) {
     dc_intf_.OnDisplayVsync(id, timestamp, handles, handle_count);
   }
 }
