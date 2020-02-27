86d269879b31567eb0b9e390cf7ea4e7557bd7de
Filip Filmar <fmil@google.com>
2020-02-26 17:47:41 -0800

    [i18n][e2e] e2e tests for Ermine L10N
    Adds a simple basic test: switch locale with an external service, then
    figure out whether the on-screen display has actually changed as result.
    
    This is simplistic, but infinitely better than what we used to have
    (manual spot checks).
    
    Bug: 44741
    Change-Id: I0b3a62f9681249667b57d7a40f4a3b9f7a83c019

--- src/experiences/tests/e2e/test/ermine_intl_test.dart
+++ src/experiences/tests/e2e/test/ermine_intl_test.dart
@@ -1,8 +1,6 @@
-// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-import 'dart:math';
 
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_driver_sl4f/flutter_driver_sl4f.dart';
@@ -10,12 +8,21 @@ import 'package:sl4f/sl4f.dart';
 import 'package:test/test.dart';
 
 /// Tests that the DUT running ermine can do the following:
-///  - Connect to ermine using Flutter Driver.
-///  - Ensure its screenshot is not all black.
+///   - Change locale via setui_client and that change takes effect
 void main() {
   Sl4f sl4f;
   FlutterDriverConnector connector;
   FlutterDriver driver;
+
+  void setLocale(String localeId) async {
+    expect(
+        (await sl4f.ssh.run('run setui_client.cm intl -l $localeId')).exitCode,
+        0);
+  }
+
+  void findTextOnScreen(String text) {
+    expect(find.text(text).serialize()['text'], text);
+  }
 
   setUpAll(() async {
     sl4f = Sl4f.fromEnvironment();
@@ -38,6 +45,8 @@ void main() {
   });
 
   tearDownAll(() async {
+    setLocale('en-US');
+
     // Any of these may end up being null if the test fails in setup.
     await driver?.close();
     await connector?.tearDown();
@@ -45,27 +54,12 @@ void main() {
     sl4f?.close();
   });
 
-  test('Screen should not be black', () async {
-    await driver.waitUntilNoTransientCallbacks();
+  test('Locale can be switched and takes effect', () async {
+    setLocale('en-US');
+    findTextOnScreen('MEMORY');
 
-    // Now take a screen shot and make sure it is not all black.
-    final scenic = Scenic(sl4f);
-    final image = await scenic.takeScreenshot();
-    bool isAllBlack = image.data.every((pixel) => pixel & 0x00ffffff == 0);
-    expect(isAllBlack, false);
+    // The text on screen is equivalent to US English "MEMORY".
+    setLocale('sr');
+    findTextOnScreen('МЕМОРИЈА');
   });
-}
-
-Future<Point<int>> centerFromFinder(
-    FlutterDriver driver, SerializableFinder finder) async {
-  // Get the bottom right of the main screen.
-  final mainScreenFinder = find.byValueKey('main');
-  final bottomRight = await driver.getBottomRight(mainScreenFinder);
-
-  // The `input` utility expects screen coordinates to be scaled 0 - 1000.
-  final center = await driver.getCenter(finder);
-  int x = (center.dx / bottomRight.dx * 1000).toInt();
-  int y = (center.dy / bottomRight.dy * 1000).toInt();
-
-  return Point<int>(x, y);
 }
