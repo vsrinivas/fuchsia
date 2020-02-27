8b7694674d5d803f0fa1350f7ebdf9dbac293f9d
Craig Stout <cstout@google.com>
2020-02-27 18:42:01 +0000

    [magma] Add vk_icdGetPhysicalDeviceProcAddr to linker script
    Mesa 19.3 needs this for loader version 4.
    
    Testability:new test_icd_strip
    
    Change-Id: I4c1ded2b3eb5700a807ff22ba7552dc25a2878df

---
+++ src/graphics/lib/magma/tests/icd_strip/icd.c
@@ -0,0 +1,23 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vulkan/vulkan.h>
+
+#define __EXPORT __attribute__((__visibility__("default")))
+
+typedef VkResult(VKAPI_PTR* PFN_vkGetServiceAddr)(const char* pName, uint32_t handle);
+
+//
+// These are the four entry points that may be exported by a Vulkan ICD.
+//
+
+__EXPORT VkResult vk_icdNegotiateLoaderICDInterfaceVersion(uint32_t* pSupportedVersion) {
+  return VK_SUCCESS;
+}
+
+__EXPORT void vk_icdGetInstanceProcAddr(VkInstance instance, const char* name) {}
+
+__EXPORT void vk_icdGetPhysicalDeviceProcAddr(VkInstance instance, const char* name) {}
+
+__EXPORT void vk_icdInitializeConnectToServiceCallback(PFN_vkGetServiceAddr callback) {}
---
+++ src/graphics/lib/magma/tests/icd_strip/test_icd_strip.cc
@@ -0,0 +1,50 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <dlfcn.h>
+
+#include <filesystem>
+
+#include <gtest/gtest.h>
+
+// Checks that the magma_vulkan_icd template generates shared libries
+// that may contain the four necessary entry points.
+TEST(IcdStrip, LoadEntryPoints) {
+  void* handle = nullptr;
+
+  // The ICD is packaged under /pkg/lib/x64-shared or /pkg/lib/arm64-shared
+  for (auto& dir : std::filesystem::directory_iterator("/pkg/lib")) {
+    if (!dir.is_directory())
+      continue;
+    for (auto& file : std::filesystem::directory_iterator(dir.path())) {
+      ASSERT_FALSE(file.is_directory());
+      // dlopen takes path relative to /lib
+      auto lib = dir.path().stem() / file.path().filename();
+      handle = dlopen(lib.c_str(), RTLD_NOW);
+      ASSERT_TRUE(handle) << " dlerror: " << dlerror();
+      break;
+    }
+  }
+
+  ASSERT_TRUE(handle);
+
+  std::vector<const char*> entry_points = {
+      "vk_icdGetInstanceProcAddr",
+      "vk_icdGetPhysicalDeviceProcAddr",
+      "vk_icdNegotiateLoaderICDInterfaceVersion",
+      "vk_icdInitializeConnectToServiceCallback",
+  };
+
+  for (auto& entry_point : entry_points) {
+    void* ptr = dlsym(handle, entry_point);
+    EXPECT_TRUE(ptr) << " Couldn't find entry point: " << entry_point;
+  }
+
+  dlclose(handle);
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
