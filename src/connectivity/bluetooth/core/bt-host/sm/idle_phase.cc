311ecfc5c701bf600d34fdcba6966d910b349b0e
Lucas Jenkins <lucasjenkins@google.com>
2020-03-04 18:51:48 +0000

    [bt][sm] Add IdlePhase for SMP refactor
    Add IdlePhase, the non-active concrete PairingPhase. This phase exists
    when no pairing is occurring, and listens for peer Pairing/Security
    requests.
    
    Bug: 46278
    Test: Added idle_phase_unittest.cc class & associated tests. To run:
          `fx run-test bluetooth-tests -t bt-host-unittests -- \
           --gtest_filter="SMP_IdlePhaseTest*" `
    Change-Id: Ia6cb4f3e03e2db6d9aff02cb5fef9c402c1aee4c

---
+++ src/connectivity/bluetooth/core/bt-host/sm/idle_phase.cc
@@ -0,0 +1,71 @@
+// Copyright 2020 the Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "idle_phase.h"
+
+#include <zircon/assert.h>
+
+#include <type_traits>
+
+#include "src/connectivity/bluetooth/core/bt-host/common/log.h"
+#include "src/connectivity/bluetooth/core/bt-host/sm/packet.h"
+#include "src/connectivity/bluetooth/core/bt-host/sm/smp.h"
+#include "src/connectivity/bluetooth/core/bt-host/sm/types.h"
+
+namespace bt {
+
+namespace sm {
+
+IdlePhase::IdlePhase(fxl::WeakPtr<PairingChannel> chan, fxl::WeakPtr<Listener> listener,
+                     hci::Connection::Role role, PairingRequestCallback on_pairing_req,
+                     SecurityRequestCallback on_security_req)
+    : PairingPhase(std::move(chan), std::move(listener), role),
+      weak_ptr_factory_(this),
+      on_pairing_req_(std::move(on_pairing_req)),
+      on_security_req_(std::move(on_security_req)) {
+  sm_chan().SetChannelHandler(weak_ptr_factory_.GetWeakPtr());
+}
+
+void IdlePhase::OnPairingRequest(PairingRequestParams req_params) {
+  // Reject the command if we are the master.
+  if (role() == hci::Connection::Role::kMaster) {
+    bt_log(TRACE, "sm", "rejecting \"Pairing Request\" as master");
+    SendPairingFailed(ErrorCode::kCommandNotSupported);
+    return;
+  }
+  on_pairing_req_(req_params);
+}
+
+void IdlePhase::OnSecurityRequest(AuthReqField req) {
+  // Reject the command if we are the slave.
+  if (role() == hci::Connection::Role::kSlave) {
+    bt_log(TRACE, "sm", "rejecting \"Security Request\" as slave");
+    SendPairingFailed(ErrorCode::kCommandNotSupported);
+    return;
+  }
+  on_security_req_(req);
+}
+
+void IdlePhase::OnRxBFrame(ByteBufferPtr sdu) {
+  fit::result<ValidPacketReader, ErrorCode> maybe_reader = ValidPacketReader::ParseSdu(sdu, mtu());
+  if (maybe_reader.is_error()) {
+    bt_log(INFO, "sm", "dropped SMP packet: %s", bt_str(Status(maybe_reader.error())));
+    return;
+  }
+  ValidPacketReader reader = maybe_reader.value();
+  Code smp_code = reader.code();
+
+  if (smp_code == kPairingRequest) {
+    OnPairingRequest(reader.payload<PairingRequestParams>());
+  } else if (smp_code == kSecurityRequest) {
+    OnSecurityRequest(reader.payload<AuthReqField>());
+  } else {
+    bt_log(INFO, "sm", "dropped unexpected SMP code %#.2X when not pairing", smp_code);
+  }
+}
+
+void IdlePhase::OnChannelClosed() { bt_log(TRACE, "sm", "channel closed while not pairing"); }
+
+}  // namespace sm
+}  // namespace bt
---
+++ src/connectivity/bluetooth/core/bt-host/sm/idle_phase.h
@@ -0,0 +1,62 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_SM_IDLE_PHASE_H_
+#define SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_SM_IDLE_PHASE_H_
+
+#include <lib/fit/function.h>
+
+#include "src/connectivity/bluetooth/core/bt-host/sm/pairing_phase.h"
+#include "src/connectivity/bluetooth/core/bt-host/sm/smp.h"
+#include "src/lib/fxl/memory/weak_ptr.h"
+
+namespace bt {
+namespace sm {
+
+// Represents the state an SMP pairing not in progress. Its only responsibilites are accepting and
+// passing up Security Requests as master / Pairing Requests as slave.
+//
+// This class is not thread safe and is meant to be accessed on the thread it was created on. All
+// callbacks will be run by the default dispatcher of an IdlePhase's creation thread.
+using PairingRequestCallback = fit::function<void(PairingRequestParams)>;
+using SecurityRequestCallback = fit::function<void(AuthReqField)>;
+
+class IdlePhase final : public PairingPhase, public PairingChannelHandler {
+ public:
+  // Initializes this IdlePhase with the following parameters:
+  //   - |chan|: The L2CAP SMP fixed channel.
+  //   - |listener|: The Listener implementer
+  //   - |role|: The local connection role.
+  //   - |on_pairing_req|: callback which signals the receiption of an SMP Pairing Request.
+  //   - |on_security_req|: callback which handles the reception of an SMP Security Request.
+  IdlePhase(fxl::WeakPtr<PairingChannel> chan, fxl::WeakPtr<Listener> listener,
+            hci::Connection::Role role, PairingRequestCallback on_pairing_req,
+            SecurityRequestCallback on_security_req);
+
+  ~IdlePhase() override = default;
+
+ private:
+  // Perform basic HCI-level role validation
+  void OnPairingRequest(PairingRequestParams req_params);
+  void OnSecurityRequest(AuthReqField req);
+
+  // PairingChannelHandler overrides:
+  void OnRxBFrame(ByteBufferPtr sdu) override;
+  void OnChannelClosed() override;
+
+  // PairingPhase override
+  fxl::WeakPtr<PairingChannelHandler> AsChannelHandler() override {
+    return weak_ptr_factory_.GetWeakPtr();
+  }
+
+  fxl::WeakPtrFactory<IdlePhase> weak_ptr_factory_;
+  PairingRequestCallback on_pairing_req_;
+  SecurityRequestCallback on_security_req_;
+  DISALLOW_COPY_AND_ASSIGN_ALLOW_MOVE(IdlePhase);
+};
+
+}  // namespace sm
+}  // namespace bt
+
+#endif  // SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_SM_IDLE_PHASE_H_
