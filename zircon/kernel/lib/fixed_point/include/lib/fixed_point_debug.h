f1957042f21a8ce3439aace60ccb03a3b6bc45ce
Gabor Horvath <xazax@google.com>
2020-03-07 00:19:13 +0000

    [kernel][fixed_point] Cleaning up -Wunused-functions warnings
    Bug: 44879
    Change-Id: I0b5ac826dba439c73b186b810a29aa90cedb54f8

--- zircon/kernel/lib/fixed_point/include/lib/fixed_point_debug.h
+++ zircon/kernel/lib/fixed_point/include/lib/fixed_point_debug.h
@@ -8,7 +8,7 @@
 #ifndef ZIRCON_KERNEL_LIB_FIXED_POINT_INCLUDE_LIB_FIXED_POINT_DEBUG_H_
 #define ZIRCON_KERNEL_LIB_FIXED_POINT_INCLUDE_LIB_FIXED_POINT_DEBUG_H_
 
-static const char* fpd_shift_prefix_32(int shift) {
+static inline const char* fpd_shift_prefix_32(int shift) {
   switch (shift) {
     case 32:
       return "";
@@ -23,7 +23,7 @@ static const char* fpd_shift_prefix_32(int shift) {
   }
 }
 
-static const char* fpd_shift_prefix_64(int shift) {
+static inline const char* fpd_shift_prefix_64(int shift) {
   switch (shift) {
     case 32:
       return "";
@@ -38,7 +38,7 @@ static const char* fpd_shift_prefix_64(int shift) {
   }
 }
 
-static const char* fpd_shift_suffix(int shift) {
+static inline const char* fpd_shift_suffix(int shift) {
   switch (shift) {
     case 32:
       return " 00000000                  ";
@@ -53,7 +53,8 @@ static const char* fpd_shift_suffix(int shift) {
   }
 }
 
-static void debug_mul_u32_u32(uint32_t a, uint32_t b, int a_shift, int b_shift, uint64_t ret) {
+static inline void debug_mul_u32_u32(uint32_t a, uint32_t b, int a_shift, int b_shift,
+                                     uint64_t ret) {
 #if DEBUG_FIXED_POINT
   TRACEF("         %s%08x%s * %s%08x%s = %s%08x%s%08x%s\n", fpd_shift_prefix_32(a_shift), a,
          fpd_shift_suffix(a_shift), fpd_shift_prefix_32(b_shift), b, fpd_shift_suffix(b_shift),
@@ -63,8 +64,8 @@ static void debug_mul_u32_u32(uint32_t a, uint32_t b, int a_shift, int b_shift, 
 #endif
 }
 
-static void debug_u64_mul_u32_fp32_64(uint32_t a, struct fp_32_64 b, uint64_t res_0,
-                                      uint32_t res_l32_32, uint64_t ret) {
+static inline void debug_u64_mul_u32_fp32_64(uint32_t a, struct fp_32_64 b, uint64_t res_0,
+                                             uint32_t res_l32_32, uint64_t ret) {
 #if DEBUG_FIXED_POINT
   TRACEF(
       "          %08x                   *          %08x.%08x %08x"
@@ -78,8 +79,8 @@ static void debug_u64_mul_u32_fp32_64(uint32_t a, struct fp_32_64 b, uint64_t re
 #endif
 }
 
-static void debug_u32_mul_u64_fp32_64(uint64_t a, struct fp_32_64 b, uint64_t res_l32,
-                                      uint32_t ret) {
+static inline void debug_u32_mul_u64_fp32_64(uint64_t a, struct fp_32_64 b, uint64_t res_l32,
+                                             uint32_t ret) {
 #if DEBUG_FIXED_POINT
   TRACEF(
       "%08x %08x                   *          %08x.%08x %08x"
@@ -94,8 +95,8 @@ static void debug_u32_mul_u64_fp32_64(uint64_t a, struct fp_32_64 b, uint64_t re
 #endif
 }
 
-static void debug_u64_mul_u64_fp32_64(uint64_t a, struct fp_32_64 b, uint64_t res_0,
-                                      uint32_t res_l32_32, uint64_t ret) {
+static inline void debug_u64_mul_u64_fp32_64(uint64_t a, struct fp_32_64 b, uint64_t res_0,
+                                             uint32_t res_l32_32, uint64_t ret) {
 #if DEBUG_FIXED_POINT
   TRACEF(
       "%08x %08x                   *          %08x.%08x %08x"
