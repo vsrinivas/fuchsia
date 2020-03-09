e230e3559a87cb32cbe19530fe85517804bb3cf2
Mark Seaborn <mseaborn@google.com>
2020-03-09 23:29:45 +0000

    [profile][utest] Split the profile test into two test cases
    Minor cleanup to reduce duplication of the CheckBasicDetails() checks.
    This improves readability a little and will make it easier to test a
    third case.
    
    Bug: none
    Test: profile-test
     * run locally with:
         fx set bringup.x64 --with-base //garnet/packages/tests:zircon
         fx qemu -k -c zircon.autorun.boot=/boot/test/sys/profile-test
    Change-Id: I88d19b54d9031fefa8a6529b0579afec1e29d917

--- zircon/system/utest/profile/profile.cc
+++ zircon/system/utest/profile/profile.cc
@@ -45,37 +45,53 @@ zx_status_t CreateDeadlineProfile(const zx::channel& profile_provider, zx_durati
   return ZX_OK;
 }
 
-TEST(Profile, CreateDestroy) {
+void GetProfileProvider(zx::channel* channel) {
   // Connect to ProfileProvider.
-  zx::channel channel1, channel2;
-  ASSERT_OK(zx::channel::create(/*flags=*/0u, &channel1, &channel2));
-  ASSERT_OK(fdio_service_connect("/svc/" fuchsia_scheduler_ProfileProvider_Name, channel1.release()),
+  zx::channel server_endpoint;
+  ASSERT_OK(zx::channel::create(/*flags=*/0u, &server_endpoint, channel));
+  ASSERT_OK(fdio_service_connect("/svc/" fuchsia_scheduler_ProfileProvider_Name,
+                                 server_endpoint.release()),
             "Could not connect to ProfileProvider.");
-  ASSERT_EQ(channel1.get(), ZX_HANDLE_INVALID);
+  ASSERT_EQ(server_endpoint.get(), ZX_HANDLE_INVALID);
+}
 
-  // Create available profile types.
-  zx::profile profile;
-  zx_status_t status;
-  ASSERT_OK(CreateProfile(channel2, /*priority=*/0u, "<test>", &status, &profile),
-            "Error creating profile.");
-
-  zx::profile deadline_profile;
-  ASSERT_OK(
-      CreateDeadlineProfile(channel2, /*capacity=*/ZX_MSEC(2), /*relative_deadline=*/ZX_MSEC(10),
-                            /*period=*/ZX_MSEC(10), "<test>", &status, &deadline_profile),
-      "Error creating deadline profile.");
-
+void CheckBasicDetails(const zx::profile& profile) {
   // Ensure basic details are correct.
   zx_info_handle_basic_t info;
   ASSERT_OK(profile.get_info(ZX_INFO_HANDLE_BASIC, &info, sizeof(info), nullptr, nullptr),
             "object_get_info for profile");
   EXPECT_NE(info.koid, 0, "no koid for profile");
   EXPECT_EQ(info.type, ZX_OBJ_TYPE_PROFILE, "incorrect type for profile");
+}
 
-  ASSERT_OK(deadline_profile.get_info(ZX_INFO_HANDLE_BASIC, &info, sizeof(info), nullptr, nullptr),
-            "object_get_info for deadline profile");
-  EXPECT_NE(info.koid, 0, "no koid for deadline profile");
-  EXPECT_EQ(info.type, ZX_OBJ_TYPE_PROFILE, "incorrect type for deadline profile");
+// Test getting a profile via the GetProfile FIDL method.
+TEST(Profile, GetProfile) {
+  zx::channel provider;
+  GetProfileProvider(&provider);
+  ASSERT_FALSE(CURRENT_TEST_HAS_FAILURES());
+
+  zx::profile profile;
+  zx_status_t status;
+  ASSERT_OK(CreateProfile(provider, /*priority=*/0u, "<test>", &status, &profile),
+            "Error creating profile.");
+
+  CheckBasicDetails(profile);
+}
+
+// Test getting a profile via the GetDeadlineProfile FIDL method.
+TEST(Profile, GetDeadlineProfile) {
+  zx::channel provider;
+  GetProfileProvider(&provider);
+  ASSERT_FALSE(CURRENT_TEST_HAS_FAILURES());
+
+  zx::profile profile;
+  zx_status_t status;
+  ASSERT_OK(
+      CreateDeadlineProfile(provider, /*capacity=*/ZX_MSEC(2), /*relative_deadline=*/ZX_MSEC(10),
+                            /*period=*/ZX_MSEC(10), "<test>", &status, &profile),
+      "Error creating deadline profile.");
+
+  CheckBasicDetails(profile);
 }
 
 }  // namespace
