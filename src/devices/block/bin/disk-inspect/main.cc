79b425e5d690ae2ed6fce8fdbe248dcc4d8984f7
Tianrui Chen <tianruichen@google.com>
2020-03-10 00:41:50 +0000

    [fs][inspector] Update disk-inspect app to use new libaries.
    Update disk-inspect to become more interactive with basic
    input listening to be able to handle multiple commands.
    
    Bug: 37907
    Change-Id: Iaf9726a3b8f08cbfb49fece6263d603943988f33

--- src/devices/block/bin/disk-inspect/main.cc
+++ src/devices/block/bin/disk-inspect/main.cc
@@ -3,111 +3,120 @@
 // found in the LICENSE file.
 
 #include <fcntl.h>
+#include <fuchsia/hardware/block/partition/llcpp/fidl.h>
 #include <lib/fdio/fdio.h>
 #include <stdio.h>
 #include <zircon/status.h>
 
+#include <iostream>
+#include <sstream>
+
 #include <block-client/cpp/block-device.h>
 #include <block-client/cpp/remote-block-device.h>
+#include <disk_inspector/command.h>
+#include <disk_inspector/command_handler.h>
 #include <disk_inspector/disk_inspector.h>
 #include <fbl/unique_fd.h>
-#include <minfs/inspector.h>
+#include <minfs/command-handler.h>
 
 namespace {
 
-// Processes various disk objects recursively starting from root object
-// and prints values/elements of the objects.
-void ProcessDiskObjects(std::unique_ptr<disk_inspector::DiskObject> obj, uint32_t num_tabs) {
-  if (obj == nullptr)
-    return;
+namespace fuchsia_partition = llcpp::fuchsia::hardware::block::partition;
 
-  printf("\n");
-  for (uint32_t i = 0; i < num_tabs; i++) {
-    printf("\t");
-  }
-
-  printf("Name: %-25s", obj->GetName());
-  size_t num_elements = obj->GetNumElements();
-
-  // Non scalar types.
-  if (num_elements != 0) {
-    for (uint32_t i = 0; i < num_elements; i++) {
-      std::unique_ptr<disk_inspector::DiskObject> element = obj->GetElementAt(i);
-      ProcessDiskObjects(std::move(element), num_tabs + 1);
-    }
-    return;
-  }
-
-  const void *buffer;
-  size_t size;
-  obj->GetValue(&buffer, &size);
-
-  switch (size) {
-    case sizeof(uint64_t): {
-      const uint64_t *val = reinterpret_cast<const uint64_t *>(buffer);
-      printf(" Value:0x%lx", *val);
-    } break;
-
-    case sizeof(uint32_t): {
-      const uint32_t *val = reinterpret_cast<const uint32_t *>(buffer);
-      printf(" Value:0x%x", *val);
-    } break;
-
-    case sizeof(uint16_t): {
-      const uint16_t *val = reinterpret_cast<const uint16_t *>(buffer);
-      printf(" Value:0x%x", *val);
-    } break;
-
-    case sizeof(uint8_t): {
-      const char *val = reinterpret_cast<const char *>(buffer);
-      printf(" Value:%c", *val);
-    } break;
-
-    default:
-      ZX_ASSERT_MSG(false, "Unknown object size: %lu\n", size);
-  }
-}
-
-int Inspect(std::unique_ptr<block_client::BlockDevice> device) {
-  minfs::Inspector inspector = minfs::Inspector(std::move(device));
-  std::unique_ptr<disk_inspector::DiskObject> root;
-
-  if (inspector.GetRoot(&root) == ZX_OK) {
-    ProcessDiskObjects(std::move(root), 0);
-    printf("\n");
-    return 0;
-  }
-  fprintf(stderr, "ERROR: GetRoot failed\n");
-  return -1;
-}
-
-}  // namespace
-
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    fprintf(stderr, "usage: %s <device path>\n", argv[0]);
-    return -1;
-  }
-
-  fbl::unique_fd fd(open(argv[1], O_RDONLY));
-  if (fd.get() < 0) {
-    fprintf(stderr, "ERROR: Failed to open device: %d\n", fd.get());
-    return -1;
-  }
+std::unique_ptr<disk_inspector::CommandHandler> GetHandler(const char* path) {
+  fbl::unique_fd fd(open(path, O_RDONLY));
 
   zx::channel channel;
   zx_status_t status = fdio_get_service_handle(fd.release(), channel.reset_and_get_address());
   if (status != ZX_OK) {
-    fprintf(stderr, "ERROR: cannot acquire handle: %d\n", status);
-    return -1;
+    std::cerr << "Cannot acquire handle with error: " << status << "\n";
+    return nullptr;
   }
+
+  auto name_resp = fuchsia_partition::Partition::Call::GetName(channel.borrow());
+  if (!name_resp.ok()) {
+    std::cerr << "Get block device name failed.\n";
+    return nullptr;
+  }
+  if (name_resp->status != ZX_OK) {
+    std::cerr << "Could not get block device fs name: " << name_resp->status << "\n";
+    return nullptr;
+  }
+  if (name_resp->name.is_null()) {
+    std::cerr << "Block device name is null.\n";
+    return nullptr;
+  }
+  std::string name(name_resp->name.begin(), name_resp->name.size());
+  std::cout << "Found block partition label: " << name << "\n";
 
   std::unique_ptr<block_client::RemoteBlockDevice> device;
   status = block_client::RemoteBlockDevice::Create(std::move(channel), &device);
   if (status != ZX_OK) {
-    fprintf(stderr, "ERROR: cannot create remote device: %d\n", status);
+    std::cerr << "Cannot create remote device: " << status << "\n";
+    return nullptr;
+  }
+
+  std::unique_ptr<disk_inspector::CommandHandler> handler;
+
+  if (name == "minfs") {
+    status = minfs::CommandHandler::Create(std::move(device), &handler);
+  } else {
+    std::cerr << "FS with label " << name << " is not supported for inspection.\n";
+    return nullptr;
+  }
+
+  if (status != ZX_OK) {
+    std::cerr << "Could not create inspector for FS with error: " << status << "\n";
+    return nullptr;
+  }
+  return handler;
+}
+
+}  //  namespace
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "usage: " << argv[0] << " <device path>\n";
     return -1;
   }
 
-  return Inspect(std::move(device));
+  std::unique_ptr<disk_inspector::CommandHandler> handler = GetHandler(argv[1]);
+  if (handler == nullptr) {
+    std::cerr << "Could not get inspector at path. Closing.\n";
+    return -1;
+  }
+
+  std::cout << "Starting inspector. Type \"help\" to get available commands.\n";
+  std::cout << "Type \"exit\" to quit the application.\n";
+  while (true) {
+    std::string command_str;
+    getline(std::cin, command_str);
+    if (command_str.find_first_not_of(' ') == std::string::npos) {
+      continue;
+    }
+    std::stringstream ss(command_str);
+    std::istream_iterator<std::string> begin(ss);
+    std::istream_iterator<std::string> end;
+    std::vector<std::string> command_args(begin, end);
+    if (command_args[0] == "exit") {
+      return 0;
+    }
+    if (command_args[0] == "help") {
+      handler->PrintSupportedCommands();
+      continue;
+    }
+    zx_status_t status = handler->CallCommand(command_args);
+    if (status != ZX_OK) {
+      switch (status) {
+        case ZX_ERR_NOT_SUPPORTED: {
+          std::cerr << "Command not supported.\n";
+          break;
+        }
+        default: {
+          std::cerr << "Call command failed with error: " << status << "\n";
+        }
+      }
+    }
+  }
+  return 0;
 }
