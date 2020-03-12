4eec663b4da706c8ceaf542bbe125444cc6bca99
Jeff Belgum <belgum@google.com>
2020-03-12 19:12:55 +0000

    [bt][async-helpers] Replace Unknown with Generic HangingGetServerError variant.
    This variant contains an inner anyhow::Error that can be used to provide
    extra context and should be the default choice when creating
    a HangingGetServerError that is not described by other variants
    
    TEST: fx test async-helpers-tests
    
    Change-Id: I3e050e8c043f6c3db57311f9dc77d9e37f94ce72

--- src/connectivity/bluetooth/lib/async-helpers/src/hanging_get/error.rs
+++ src/connectivity/bluetooth/lib/async-helpers/src/hanging_get/error.rs
@@ -1,18 +1,31 @@
 use {
+    anyhow::format_err,
     futures::channel::{mpsc, oneshot},
     thiserror::Error,
 };
 
-#[derive(Error, Debug, PartialEq)]
+#[derive(Error, Debug)]
 pub enum HangingGetServerError {
+    #[error("Cannot have multiple concurrent observers for a single client")]
+    MultipleObservers,
     #[error("The HangingGetBroker associated with this handle has been dropped.")]
     NoBroker,
     #[error("This handle is sending messages faster than the broker can process them.")]
     RateLimit,
-    #[error("An unknown error condition was encountered.")]
-    Unknown,
-    #[error("Cannot have multiple concurrent observers for a single client")]
-    MultipleObservers,
+    #[error("Error: {}", .0)]
+    Generic(anyhow::Error),
+}
+
+impl PartialEq for HangingGetServerError {
+    fn eq(&self, other: &Self) -> bool {
+        use HangingGetServerError::*;
+        match (self, other) {
+            (MultipleObservers, MultipleObservers)
+            | (NoBroker, NoBroker)
+            | (RateLimit, RateLimit) => true,
+            _ => false,
+        }
+    }
 }
 
 impl From<mpsc::SendError> for HangingGetServerError {
@@ -22,13 +35,43 @@ impl From<mpsc::SendError> for HangingGetServerError {
         } else if error.is_full() {
             HangingGetServerError::RateLimit
         } else {
-            HangingGetServerError::Unknown
+            HangingGetServerError::Generic(format_err!(
+                "Unknown SendError error condition: {}",
+                error
+            ))
         }
     }
 }
 
 impl From<oneshot::Canceled> for HangingGetServerError {
-    fn from(_: oneshot::Canceled) -> Self {
-        HangingGetServerError::Unknown
+    fn from(e: oneshot::Canceled) -> Self {
+        HangingGetServerError::Generic(e.into())
+    }
+}
+
+impl From<anyhow::Error> for HangingGetServerError {
+    fn from(e: anyhow::Error) -> Self {
+        HangingGetServerError::Generic(e)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn error_partial_eq_impl() {
+        use HangingGetServerError::*;
+        let variants = [MultipleObservers, NoBroker, RateLimit, Generic(format_err!("err"))];
+        for (i, a) in variants.iter().enumerate() {
+            for (j, b) in variants.iter().enumerate() {
+                // variants with the same index are equal except `Generic` errors are _never_ equal.
+                if i == j && i != 3 && j != 3 {
+                    assert_eq!(a, b);
+                } else {
+                    assert_ne!(a, b);
+                }
+            }
+        }
     }
 }
