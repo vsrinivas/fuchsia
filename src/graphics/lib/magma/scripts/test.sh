659e209abb74cd98a264bd8e7847266aecb7ec01
Craig Stout <cstout@google.com>
2020-03-13 08:39:20 +0000

    [magma] Update test.sh to use fx test
    -f was removed from runtests
    
    Change-Id: I872b0c2baad06068bfec72f11f9e70acf4821942

--- src/graphics/lib/magma/scripts/test.sh
+++ src/graphics/lib/magma/scripts/test.sh
@@ -10,13 +10,41 @@ fx-config-read
 
 case "$1" in
 intel)
-  fx-command-run cp "${script_dir}/autorun_intel" /tmp/magma_autorun
+  fx-command-run test --no-build --simple -o \
+    magma_unit_tests \
+    msd_intel_gen_nonhardware_tests \
+    mesa_unit_tests \
+    state_pool \
+    state_pool_free_list_only \
+    state_pool_no_free \
+    block_pool_no_free \
+    magma_abi_conformance_tests \
+    msd_intel_gen_integration_tests \
+    icd_load \
+    vkreadback \
+    vkloop \
+    vkext_unprotected
   ;;
 mali)
-  fx-command-run cp "${script_dir}/autorun_mali" /tmp/magma_autorun
+  fx-command-run test --no-build --simple -o \
+    magma_unit_tests \
+    msd_arm_mali_nonhardware_tests \
+    magma_abi_conformance_tests \
+    msd_arm_mali_integration_tests \
+    icd_load \
+    vkreadback \
+    vkloop \
+    vkext_protected \
+    vkext_unprotected \
+    vk_timer_query_tests
   ;;
 pvr)
-  fx-command-run cp "${script_dir}/autorun_pvr" /tmp/magma_autorun
+  fx-command-run test --no-build --simple -o \
+    magma_unit_tests \
+    msd_img_rgx_nonhardware_tests \
+    magma_abi_conformance_tests \
+    msd_img_rgx_integration_tests \
+    msd_img_rgx_no_hardware_integration_tests
   ;;
 *)
   echo >&2 "unknown gpu: $1"
@@ -24,5 +52,3 @@ pvr)
   exit 1
   ;;
 esac
-
-fx-command-run shell -t runtests -f /tmp/magma_autorun
