07c3ef2a7016f0ba2987d057e2e118b9374dde98
Dale Sather <dalesat@google.com>
2020-03-17 18:12:53 +0000

    [soundplayer] support packed 24-bit samples in .wav files
    This CL allows soundplayer to play sounds from .wav files that
    contain packed 24-bit samples. The samples are expanded to the
    24-in-32 bit format that AudioRenderer supports.
    
    TEST: added unit test for the wav reader
    
    Fixed: 47577
    
    Change-Id: Ie9f32d6f3e32edf84e2e8b946cf28fd1a7fbc699

--- src/media/sounds/soundplayer/src/sound_player/wav_reader.rs
+++ src/media/sounds/soundplayer/src/sound_player/wav_reader.rs
@@ -11,6 +11,8 @@ use std::io::Read;
 const MIN_FMT_CHUNK_SIZE: u32 = 16;
 const PCM_ENCODING: u16 = 1;
 
+type Converter = fn(zx::Vmo, u64) -> Result<(zx::Vmo, u64)>;
+
 pub struct WavReader<'a> {
     slice: &'a [u8],
 }
@@ -36,13 +38,16 @@ impl WavReader<'_> {
         self.parse_fourcc("WAVE")?;
 
         let mut stream_type: Option<AudioStreamType> = None;
+        let mut converter: Option<Converter> = None;
         let mut size: u64 = 0;
         let mut vmo: Option<zx::Vmo> = None;
 
         while !self.slice.is_empty() {
             match self.parse_any_fourcc()?.as_ref() {
                 "fmt " => {
-                    stream_type = Some(self.parse_fmt()?);
+                    let (st, conv) = self.parse_fmt()?;
+                    stream_type = Some(st);
+                    converter = conv;
                 }
                 "data" => {
                     let (s, v) = self.parse_data()?;
@@ -55,8 +60,14 @@ impl WavReader<'_> {
             }
         }
 
-        let vmo = vmo.ok_or_else(|| anyhow::format_err!("No data chunk found"))?;
+        let mut vmo = vmo.ok_or_else(|| anyhow::format_err!("No data chunk found"))?;
         let stream_type = stream_type.ok_or_else(|| anyhow::format_err!("No fmt chunk found"))?;
+
+        if let Some(c) = converter {
+            let (v, s) = c(vmo, size)?;
+            vmo = v;
+            size = s;
+        }
 
         Ok(Wav { vmo, size, stream_type })
     }
@@ -92,12 +103,15 @@ impl WavReader<'_> {
         Ok(LittleEndian::read_u32(s))
     }
 
-    fn parse_fmt(&mut self) -> Result<AudioStreamType> {
-        fn sample_format_from_bits_per_sample(bits_per_sample: u16) -> Result<AudioSampleFormat> {
+    fn parse_fmt(&mut self) -> Result<(AudioStreamType, Option<Converter>)> {
+        fn sample_format_from_bits_per_sample(
+            bits_per_sample: u16,
+        ) -> Result<(AudioSampleFormat, Option<Converter>)> {
             match bits_per_sample {
-                8 => Ok(AudioSampleFormat::Unsigned8),
-                16 => Ok(AudioSampleFormat::Signed16),
-                32 => Ok(AudioSampleFormat::Signed24In32),
+                8 => Ok((AudioSampleFormat::Unsigned8, None)),
+                16 => Ok((AudioSampleFormat::Signed16, None)),
+                24 => Ok((AudioSampleFormat::Signed24In32, Some(convert_24_to_32))),
+                32 => Ok((AudioSampleFormat::Signed24In32, None)),
                 _ => Err(anyhow::format_err!("Invalid bits per sample {}", bits_per_sample)),
             }
         }
@@ -129,11 +143,16 @@ impl WavReader<'_> {
                 })?;
         }
 
-        return Ok(AudioStreamType {
-            sample_format: sample_format_from_bits_per_sample(bits_per_sample)?,
-            channels: channel_count as u32,
-            frames_per_second: frames_per_second,
-        });
+        let (sample_format, converter) = sample_format_from_bits_per_sample(bits_per_sample)?;
+
+        return Ok((
+            AudioStreamType {
+                sample_format: sample_format,
+                channels: channel_count as u32,
+                frames_per_second: frames_per_second,
+            },
+            converter,
+        ));
     }
 
     fn parse_data(&mut self) -> Result<(u64, zx::Vmo)> {
@@ -170,6 +189,31 @@ impl WavReader<'_> {
     }
 }
 
+fn convert_24_to_32(vmo: zx::Vmo, size: u64) -> Result<(zx::Vmo, u64)> {
+    assert!(size % 3 == 0);
+    let sample_count = size / 3;
+    let out_size = 4 * sample_count;
+
+    let mut source_buffer = vec![0u8; size as usize];
+    let mut dest_buffer = vec![0u8; out_size as usize];
+
+    vmo.read(&mut source_buffer[..], 0)?;
+
+    let mut source_index = 0;
+    let mut dest_index = 1;
+    for _ in 0..sample_count {
+        dest_buffer[dest_index..dest_index + 3]
+            .copy_from_slice(&source_buffer[source_index..source_index + 3]);
+        source_index += 3;
+        dest_index += 4;
+    }
+
+    let out_vmo = zx::Vmo::create(out_size)?;
+    out_vmo.write(&dest_buffer[..], 0)?;
+
+    Ok((out_vmo, out_size))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -194,7 +238,7 @@ mod test {
             0x10, 0x00, // bits/sample (16)
             0x64, 0x61, 0x74, 0x61, // 'data'
             0x04, 0x00, 0x00, 0x00, // data chunk size (4)
-            0x00, 0x00, 0x00, 0x00, // frames
+            0x01, 0x02, 0x03, 0x04, // frames
         ];
         let wav = WavReader::read(&t0[..])?;
         assert_matches!(wav.size, 4);
@@ -229,7 +273,7 @@ mod test {
             0x00, 0x00, 0x00, 0x00, // extra fmt stuff
             0x64, 0x61, 0x74, 0x61, // 'data'
             0x04, 0x00, 0x00, 0x00, // data chunk size (4)
-            0x00, 0x00, 0x00, 0x00, // frames
+            0x01, 0x02, 0x03, 0x04, // frames
         ];
         let wav = WavReader::read(&t0[..])?;
         assert_matches!(wav.size, 4);
@@ -464,10 +508,50 @@ mod test {
         Ok(())
     }
 
+    #[fasync::run_singlethreaded]
+    #[test]
+    async fn packed24() -> Result<()> {
+        let t0 = [
+            0x52, 0x49, 0x46, 0x46, // 'RIFF'
+            0x00, 0x00, 0x00, 0x00, // RIFF chunk size (ignored)
+            0x57, 0x41, 0x56, 0x45, // 'WAVE'
+            0x66, 0x6d, 0x74, 0x20, // 'fmt '
+            0x12, 0x00, 0x00, 0x00, // fmt chunk size (16 bytes)
+            0x01, 0x00, // encoding (1 = pcm)
+            0x02, 0x00, // channel count (2)
+            0x44, 0xac, 0x00, 0x00, // frames/second (44100)
+            0x98, 0x09, 0x04, 0x00, // byte rate (44100 * 6)
+            0x06, 0x00, // block alignment (6)
+            0x18, 0x00, // bits/sample (24)
+            0x00, 0x00, // extra param size (0)
+            0x64, 0x61, 0x74, 0x61, // 'data'
+            0x0c, 0x00, 0x00, 0x00, // data chunk size (12)
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // frame
+            0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, // frame
+        ];
+        let wav = WavReader::read(&t0[..])?;
+        assert_matches!(wav.size, 16);
+        assert_matches!(
+            wav.stream_type,
+            AudioStreamType {
+                sample_format: AudioSampleFormat::Signed24In32,
+                channels: 2,
+                frames_per_second: 44100,
+            }
+        );
+        let expected = [
+            0x00, 0x01, 0x02, 0x03, 0x00, 0x04, 0x05, 0x06, 0x00, 0x07, 0x08, 0x09, 0x00, 0x0a,
+            0x0b, 0x0c,
+        ];
+        verify_vmo(&wav.vmo, &expected)?;
+
+        Ok(())
+    }
+
     fn verify_vmo(vmo: &zx::Vmo, expected: &[u8]) -> Result<()> {
         let mut buffer = std::vec::Vec::with_capacity(expected.len());
         buffer.resize(expected.len(), 0);
-        vmo.read(&mut buffer[..], expected.len() as u64)?;
+        vmo.read(&mut buffer[..], 0)?;
         assert_matches!(buffer == expected, true);
         Ok(())
     }
