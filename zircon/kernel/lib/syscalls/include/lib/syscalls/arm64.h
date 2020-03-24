65c1df487a7654c8a8ae3ebdbd3efd47614b8e3b
Venkatesh Srinivas <venkateshs@google.com>
2020-03-24 18:27:49 +0000

    [vdso][arm64] Introduce speculation barrier after SVC in syscall
    Certain aarch64 processors may continue executing past an SVC
    (or ERET, BRK, HLT, ...) instruction in the pre-instruction
    privilege level. Insert a DSB NSH + ISB pair to prevent executing
    code after SVC speculatively at the original privilege level.
    
    Bug: 12540 Speculative Execution Mitigations.
    Bug: 33667 Spectre mitigations?
    
    Change-Id: I627743214178ca6944266d6f594df610cd3b2442

---
+++ zircon/kernel/lib/syscalls/include/lib/syscalls/arm64.h
@@ -0,0 +1,12 @@
+// Copyright 2020 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_LIB_SYSCALLS_ARM64_H_
+#define ZIRCON_KERNEL_LIB_SYSCALLS_ARM64_H_
+
+#define ARM64_SYSCALL_SPECULATION_BARRIER_SIZE (8)
+
+#endif  // ZIRCON_KERNEL_LIB_SYSCALLS_ARM64_H_
