vsrinivas/fuchsia

commit 1f521000daf81e6e71c93dc4cb83be099f117bb0

[hypervisor][x86] Flush RSB and indirect branch predictors on #VMEXIT
On #VMExit from a hypervisor vCPU to host usermode code, flush return
stack buffers and indirect branch predictors.

1. vCPU guest code is presumed-hostile; it may have trained the
   return stack buffer / return address stack with addresses that bring
   sensitive data into cache structures where it could be read by
   subsequent guest sequences.

   We'd need an unpaired RET instruction to consume entries from a
   poisoned RSB; the context switch code contains ones, so overwrite
   the RSB to protect contexts we're switching to.

2. vCPU guest code is presumed-hostile; it may have trained indirect
   branch prediction structures so that a subsequent CALL *(reg) /
   JMP *(reg) speculatively vector to attacker-controlled code, which
   may be used to leak sensitive data.

   The kernel is already protected against this family of attacks, by
   being built with a retpoline or enabling Enhanced IBRS. User threads
   in non-hypervisor processes are protected as Zircon unconditionally
   flushes indirect branch prediction structures on switching to a thread
   in a new process. However user threads in the hypervisor itself are
   not protected; execute an IBPB flush to protect them.

   This may be optimized later.

Bug: 33667 Spectre mitigations?
Bug: 12540 Speculative Execution Mitigations.
Change-Id: If3be25990e7dda97ab1f6fc951cc7071e594be72

zircon/kernel/arch/x86/hypervisor/vcpu.cc
@@ -12,6 +12,7 @@
 
 #include <arch/x86/descriptor.h>
 #include <arch/x86/feature.h>
+#include <arch/x86/platform_access.h>
 #include <arch/x86/pvclock.h>
 #include <fbl/auto_call.h>
 #include <hypervisor/cpu.h>
@@ -838,6 +839,19 @@ zx_status_t Vcpu::Resume(zx_port_packet_t* packet) {
     running_.store(false);
 
     SaveGuestExtendedRegisters(vmcs.Read(VmcsFieldXX::GUEST_CR4));
+
+    if (!x86_get_disable_spec_mitigations()) {
+      // Spectre V2: Ensure that code executed in the VM guest cannot influence either
+      // return address predictions or indirect branch prediction in the host.
+      //
+      // TODO(fxb/33667): We may be able to avoid the IBPB here; the kernel is either
+      // built with a retpoline or has Enhanced IBRS enabled. We currently execute an
+      // IBPB on context-switch to a new aspace. The IBPB is currently only here to
+      // protect hypervisor user threads.
+      MsrAccess msr;
+      x86_ras_fill();
+      x86_cpu_ibpb(&msr);
+    }
 
     if (status != ZX_OK) {
       ktrace_vcpu_exit(VCPU_FAILURE, vmcs.Read(VmcsFieldXX::GUEST_RIP));

commit 1d724c3b62f3fd954a07d3410381642927b34662

[kernel][x86][hypervisor] Check if IBPB is present before issuing
Commit 1f521000daf81 caused the hypervisor to issue an indirect branch
predictor barrier (flush) on every VMExit, so that hypervisor guests
can't attack VM host usermode threads.

Unfortunately processors without updated microcode may support VMX but
not IBPB; check that the feature is available before issuing the
barrier.

When we cannot issue the barrier, running VM guests is insecure.

Bug: 33667 Spectre mitigations?
Bug: 12540 Speculative Execution Mitigations.

Change-Id: I7455aab69bfbd914e72324b975791e88cbc97206

zircon/kernel/arch/x86/hypervisor/vcpu.cc
@@ -863,7 +863,9 @@ zx_status_t Vcpu::Resume(zx_port_packet_t* packet) {
       // protect hypervisor user threads.
       MsrAccess msr;
       x86_ras_fill();
-      x86_cpu_ibpb(&msr);
+      if (x86_cpu_has_ibpb()) {
+        x86_cpu_ibpb(&msr);
+      }
     }
 
     if (status != ZX_OK) {

zircon/kernel/arch/x86/include/arch/x86/feature.h
@@ -346,6 +346,11 @@ static inline bool x86_get_disable_spec_mitigations(void) {
   return g_disable_spec_mitigations;
 }
 
+static inline bool x86_cpu_has_ibpb(void) {
+  extern bool g_has_ibpb;
+  return g_has_ibpb;
+}
+
 static inline bool x86_cpu_should_ras_fill_on_ctxt_switch(void) {
   extern bool g_ras_fill_on_ctxt_switch;
   return g_ras_fill_on_ctxt_switch;