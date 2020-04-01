3f0da63fdc8090cd64e8d6dd85e17b44a566dbf3
Lucas Jenkins <lucasjenkins@google.com>
2020-04-01 03:06:37 +0000

    [bt][sm] ECDH wrappers for Secure Connections
    Add EcdhKey and derived LocalEcdhKey classes to enable ECDH key exchange
    for Secure Connections. LocalEcdhKey class ensures DH Key calculation
    will be done with a key pair that contains a private key.
    
    Bug: 769
    Test: `fx run-test bluetooth-tests -t bt-host-unittests --\
           --gtest_filter="SMP_EcdhKeyTest*"`, adds tests
           "ParseSerializedKey", "PointOffP256CurveXValueParsesToNullopt",
           "PointOffP256CurveYValueParsesToNullopt", "CreateGivesValidKey",
           "CalculateDhKeyWorks", "PublicKeyXAndYComparisonSameKey", &
           "PublicKeyXAndYComparisonDifferentKeys"
    Change-Id: I388e2802331f8d1a4e21da422f44cee4292de026
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/353415
    Commit-Queue: Lucas Jenkins <lucasjenkins@google.com>
    Reviewed-by: Arman Uguray <armansito@google.com>
    Testability-Review: Arman Uguray <armansito@google.com>

---
+++ src/connectivity/bluetooth/core/bt-host/sm/ecdh_key.cc
@@ -0,0 +1,138 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/connectivity/bluetooth/core/bt-host/sm/ecdh_key.h"
+
+#include <zircon/assert.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <memory>
+
+#include <openssl/ec_key.h>
+
+#include "openssl/base.h"
+#include "openssl/bn.h"
+#include "openssl/ec.h"
+#include "openssl/ecdh.h"
+#include "openssl/nid.h"
+#include "src/connectivity/bluetooth/core/bt-host/common/uint256.h"
+#include "src/connectivity/bluetooth/core/bt-host/sm/smp.h"
+
+namespace bt {
+
+namespace sm {
+
+std::optional<EcdhKey> EcdhKey::ParseFromPublicKey(sm::PairingPublicKeyParams pub_key) {
+  auto new_key = EcdhKey();
+  new_key.key_ = EC_KEY_new_by_curve_name(EC_curve_nist2nid("P-256"));
+  if (!new_key.key_) {
+    return std::nullopt;
+  }
+  BIGNUM x, y;
+  BN_init(&x);
+  BN_init(&y);
+  BN_le2bn(pub_key.x, sizeof(pub_key.x), &x);
+  BN_le2bn(pub_key.y, sizeof(pub_key.y), &y);
+  if (EC_KEY_set_public_key_affine_coordinates(new_key.key_, &x, &y) == 1) {
+    return new_key;
+  }
+  return std::nullopt;
+}
+
+EcdhKey& EcdhKey::operator=(EcdhKey&& other) noexcept {
+  this->key_ = other.key_;
+  other.key_ = nullptr;
+  return *this;
+}
+
+EcdhKey::EcdhKey(EcdhKey&& other) noexcept : key_(other.key_) { other.key_ = nullptr; }
+
+sm::PairingPublicKeyParams EcdhKey::GetSerializedPublicKey() const {
+  sm::PairingPublicKeyParams params;
+  BIGNUM x, y;
+  BN_init(&x);
+  BN_init(&y);
+  ZX_ASSERT(EC_POINT_get_affine_coordinates_GFp(
+                EC_KEY_get0_group(key_), EC_KEY_get0_public_key(key_), &x, &y, nullptr) == 1);
+  ZX_ASSERT(BN_bn2le_padded(params.x, sizeof(params.x), &x) == 1);
+  ZX_ASSERT(BN_bn2le_padded(params.y, sizeof(params.y), &y) == 1);
+  return params;
+}
+
+UInt256 EcdhKey::GetPublicKeyX() const {
+  BIGNUM x;
+  BN_init(&x);
+  bool success =
+      EC_POINT_get_affine_coordinates_GFp(EC_KEY_get0_group(key_), EC_KEY_get0_public_key(key_), &x,
+                                          nullptr, nullptr) == 1;
+  ZX_ASSERT(success);
+  UInt256 out{};
+  success = BN_bn2le_padded(out.data(), out.size(), &x) == 1;
+  ZX_ASSERT(success);
+  return out;
+}
+
+UInt256 EcdhKey::GetPublicKeyY() const {
+  BIGNUM y;
+  BN_init(&y);
+  bool success =
+      EC_POINT_get_affine_coordinates_GFp(EC_KEY_get0_group(key_), EC_KEY_get0_public_key(key_),
+                                          nullptr, &y, nullptr) == 1;
+  ZX_ASSERT(success);
+  UInt256 out{};
+  success = BN_bn2le_padded(out.data(), out.size(), &y) == 1;
+  ZX_ASSERT(success);
+  return out;
+}
+
+EcdhKey::EcdhKey() : key_(nullptr) {}
+
+EcdhKey::~EcdhKey() {
+  if (key_) {
+    EC_KEY_free(key_);
+  }
+}
+
+LocalEcdhKey::LocalEcdhKey() : EcdhKey(){};
+
+LocalEcdhKey::LocalEcdhKey(LocalEcdhKey&& other) noexcept : EcdhKey(std::move(other)) {}
+
+LocalEcdhKey& LocalEcdhKey::operator=(LocalEcdhKey&& other) noexcept {
+  EcdhKey::operator=(std::move(other));
+  return *this;
+}
+
+std::optional<LocalEcdhKey> LocalEcdhKey::Create() {
+  auto new_key = LocalEcdhKey();
+  new_key.set_boringssl_key(EC_KEY_new_by_curve_name(EC_curve_nist2nid("P-256")));
+  if (!new_key.boringssl_key()) {
+    return std::nullopt;
+  }
+  if (EC_KEY_generate_key(new_key.mut_boringssl_key()) != 1) {
+    return std::nullopt;
+  }
+  return new_key;
+}
+
+UInt256 LocalEcdhKey::CalculateDhKey(const EcdhKey& peer_public_key) {
+  UInt256 out{0};
+  bool success = ECDH_compute_key(out.data(), out.size(),
+                                  EC_KEY_get0_public_key(peer_public_key.boringssl_key()),
+                                  boringssl_key(), nullptr) == out.size();
+  ZX_ASSERT(success);
+  std::reverse(out.begin(), out.end());
+  return out;
+}
+
+void LocalEcdhKey::SetPrivateKeyForTesting(const UInt256& private_key) {
+  BIGNUM pkey;
+  BN_init(&pkey);
+  BN_le2bn(private_key.data(), private_key.size(), &pkey);
+  ZX_ASSERT_MSG(EC_KEY_set_private_key(mut_boringssl_key(), &pkey) == 1,
+                "Could not set private key in test");
+}
+
+}  // namespace sm
+}  // namespace bt
---
+++ src/connectivity/bluetooth/core/bt-host/sm/ecdh_key.h
@@ -0,0 +1,76 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_SM_ECDH_KEY_H_
+#define SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_SM_ECDH_KEY_H_
+
+#include <cstdint>
+#include <optional>
+
+#include "fbl/macros.h"
+#include "src/connectivity/bluetooth/core/bt-host/common/uint256.h"
+#include "src/connectivity/bluetooth/core/bt-host/sm/smp.h"
+#include "third_party/boringssl/src/include/openssl/base.h"
+
+namespace bt {
+namespace sm {
+
+// Class exposing operations on ECDH keys needed for Secure Connections pairing. The only valid
+// operation on a moved-from EcdhKey is to reassign it to an existing key.
+class EcdhKey {
+ public:
+  // Returns a new public key on the P-256 Elliptic Curve parsed from a peer public key, or
+  // std::nullopt if the peer key is not a valid point on the curve.
+  static std::optional<EcdhKey> ParseFromPublicKey(sm::PairingPublicKeyParams pub_key);
+
+  EcdhKey(EcdhKey&& other) noexcept;
+  EcdhKey& operator=(EcdhKey&& other) noexcept;
+  virtual ~EcdhKey();
+
+  // Returns a representation of the public key for SMP (Vol. 3 Part H Section 3.5.6).
+  sm::PairingPublicKeyParams GetSerializedPublicKey() const;
+
+  // Returns the X/Y value of the public key as std::array wrappers for e.g. comparisons, crypto.
+  UInt256 GetPublicKeyX() const;
+  UInt256 GetPublicKeyY() const;
+
+  const EC_KEY* boringssl_key() const { return key_; }
+
+ protected:
+  EcdhKey();
+
+  EC_KEY* mut_boringssl_key() { return key_; }
+  void set_boringssl_key(EC_KEY* k) { key_ = k; }
+
+ private:
+  EC_KEY* key_;
+  DISALLOW_COPY_AND_ASSIGN_ALLOW_MOVE(EcdhKey);
+};
+
+// Specialization of EcdhKey used to ensure that when calculating the shared DH Key between two
+// EcdhKeys, at least one has a private key.
+class LocalEcdhKey : public EcdhKey {
+ public:
+  // Returns a new random public-private key pair on the P-256 Elliptic Curve used for DH Key
+  // exchange in Secure Connections, or std::nullopt if allocation fails.
+  static std::optional<LocalEcdhKey> Create();
+
+  LocalEcdhKey(LocalEcdhKey&& other) noexcept;
+  LocalEcdhKey& operator=(LocalEcdhKey&& other) noexcept;
+
+  // Returns a 256-bit DHKey calculated from our private key and the peer public key.
+  UInt256 CalculateDhKey(const EcdhKey& peer_public_key);
+
+  // Used to verify correct DHKey calculation with known, non-random private and public keys.
+  void SetPrivateKeyForTesting(const UInt256& private_key);
+
+ private:
+  LocalEcdhKey();
+  DISALLOW_COPY_AND_ASSIGN_ALLOW_MOVE(LocalEcdhKey);
+};
+
+}  // namespace sm
+}  // namespace bt
+
+#endif  // SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_SM_ECDH_KEY_H_
