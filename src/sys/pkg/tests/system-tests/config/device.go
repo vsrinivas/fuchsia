eba26682ba57127647da3ce040a53896b38da074
Alex Legg <alexlegg@google.com>
2020-04-02 01:43:37 +0000

    [system-tests] Use device-finder
    Replace the use of netaddr with device-finder.
    
    BUG: 48134
    Change-Id: I0ef7a24c5e897780179f6bda896d9b0d0a5a96e2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/377194
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Testability-Review: Alex Legg <alexlegg@google.com>
    Commit-Queue: Alex Legg <alexlegg@google.com>

--- src/sys/pkg/tests/system-tests/config/device.go
+++ src/sys/pkg/tests/system-tests/config/device.go
@@ -20,11 +20,11 @@ import (
 )
 
 type DeviceConfig struct {
-	sshKeyFile     string
-	netaddrPath    string
-	DeviceName     string
-	deviceHostname string
-	sshPrivateKey  ssh.Signer
+	sshKeyFile       string
+	deviceFinderPath string
+	DeviceName       string
+	deviceHostname   string
+	sshPrivateKey    ssh.Signer
 }
 
 func NewDeviceConfig(fs *flag.FlagSet) *DeviceConfig {
@@ -35,30 +35,52 @@ func NewDeviceConfig(fs *flag.FlagSet) *DeviceConfig {
 	fs.StringVar(&c.sshKeyFile, "ssh-private-key", os.Getenv("FUCHSIA_SSH_KEY"), "SSH private key file that can access the device")
 	fs.StringVar(&c.DeviceName, "device", os.Getenv("FUCHSIA_NODENAME"), "device name")
 	fs.StringVar(&c.deviceHostname, "device-hostname", os.Getenv("FUCHSIA_IPV4_ADDR"), "device hostname or IPv4/IPv6 address")
-	fs.StringVar(&c.netaddrPath, "netaddr-path", filepath.Join(testDataPath, "netaddr"), "zircon netaddr tool path")
+	fs.StringVar(&c.deviceFinderPath, "device-finder-path", filepath.Join(testDataPath, "device-finder"), "device-finder tool path")
 
 	return c
 }
 
 func (c *DeviceConfig) DeviceHostname(ctx context.Context) (string, error) {
-	if c.deviceHostname == "" {
+	if c.deviceHostname != "" {
+		return c.deviceHostname, nil
+	}
+
+	// The logic here should match get-fuchsia-device-addr (//tools/devshell/lib/vars.sh).
+	if c.DeviceName == "" {
 		var err error
-		c.deviceHostname, err = c.netaddr(ctx, "--nowait", "--timeout=1000", "--fuchsia", c.DeviceName)
+		var deviceList string
+		deviceList, err = c.DeviceFinder(ctx, "list", "-netboot", "-ipv4=false", "-timeout=1s", "-full")
 		if err != nil {
-			return "", fmt.Errorf("ERROR: netaddr failed: %s", err)
+			return "", fmt.Errorf("ERROR: Failed to list devices: %s", err)
+		}
+		if strings.Contains(deviceList, "\n") {
+			return "", fmt.Errorf("ERROR: Found multiple devices. Use -device to specify one.")
+		}
+		// Device finder (with -full) outputs in the format "<address> <domain>".
+		var entry = strings.Split(deviceList, " ")
+		if len(entry) != 2 {
+			return "", fmt.Errorf("ERROR: device-finder return unexpected output: %s.", deviceList)
+		}
+		c.deviceHostname = entry[0]
+		c.DeviceName = entry[1]
+	} else {
+		var err error
+		c.deviceHostname, err = c.DeviceFinder(ctx, "resolve", "-netboot", "-ipv4=false", "-timeout=1s",
+			"-device-limit=1", c.DeviceName)
+		if err != nil {
+			return "", fmt.Errorf("ERROR: Failed to find device %s: %s", c.DeviceName, err)
 		}
 		if c.deviceHostname == "" {
 			return "", fmt.Errorf("unable to determine the device hostname")
 		}
 	}
-
 	return c.deviceHostname, nil
 }
 
-func (c *DeviceConfig) netaddr(ctx context.Context, arg ...string) (string, error) {
-	stdout, stderr, err := util.RunCommand(ctx, c.netaddrPath, arg...)
+func (c *DeviceConfig) DeviceFinder(ctx context.Context, arg ...string) (string, error) {
+	stdout, stderr, err := util.RunCommand(ctx, c.deviceFinderPath, arg...)
 	if err != nil {
-		return "", fmt.Errorf("netaddr failed: %s: %s", err, string(stderr))
+		return "", fmt.Errorf("device-finder failed: %s: %s", err, string(stderr))
 	}
 	return strings.TrimRight(string(stdout), "\n"), nil
 }
