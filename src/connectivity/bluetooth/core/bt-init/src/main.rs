ba8c3114743b22e9982d2a3dca9878e41071a3a0
Nick Pollard <nickpollard@gmail.com>
2020-04-02 22:26:33 +0000

    [bt][bt-init] Relay Bootstrap and HostWatcher protocols to bt-gap
    Add the fuchsia.bluetooth.sys.(Bootstrap|HostWatcher) protocols to the
    list of protocols proxied by bt-init to bt-gap. To ensure this behavior
    is tested, also update the bt-integration-tests to inject bt-init rather
    than bt-gap as the source for all bluetooth system protocols.
    
    Test: fx test fuchsia-pkg://fuchsia.com/bluetooth-tests#meta/bt-integration-tests.cmx
    
    Change-Id: I0a748f8f9a38bc969a6f0fdebbfd839fd8fd99b6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/377516
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Reviewed-by: Arman Uguray <armansito@google.com>
    Testability-Review: Ben Lawson <benlawson@google.com>
    Commit-Queue: Nick Pollard <nickpollard@google.com>

--- src/connectivity/bluetooth/core/bt-init/src/main.rs
+++ src/connectivity/bluetooth/core/bt-init/src/main.rs
@@ -10,7 +10,7 @@ use {
     fidl_fuchsia_bluetooth_gatt::Server_Marker,
     fidl_fuchsia_bluetooth_le::{CentralMarker, PeripheralMarker},
     fidl_fuchsia_bluetooth_snoop::SnoopMarker,
-    fidl_fuchsia_bluetooth_sys::AccessMarker,
+    fidl_fuchsia_bluetooth_sys::{AccessMarker, BootstrapMarker, HostWatcherMarker},
     fuchsia_async as fasync,
     fuchsia_component::{client, fuchsia_single_component_package_url, server},
     fuchsia_syslog::{self as syslog, fx_log_info, fx_log_warn},
@@ -48,8 +48,10 @@ fn main() -> Result<(), Error> {
     let mut fs = server::ServiceFs::new();
     fs.dir("svc")
         .add_service_at(AccessMarker::NAME, |chan| Some((AccessMarker::NAME, chan)))
+        .add_service_at(BootstrapMarker::NAME, |chan| Some((BootstrapMarker::NAME, chan)))
         .add_service_at(ControlMarker::NAME, |chan| Some((ControlMarker::NAME, chan)))
         .add_service_at(CentralMarker::NAME, |chan| Some((CentralMarker::NAME, chan)))
+        .add_service_at(HostWatcherMarker::NAME, |chan| Some((HostWatcherMarker::NAME, chan)))
         .add_service_at(PeripheralMarker::NAME, |chan| Some((PeripheralMarker::NAME, chan)))
         .add_service_at(ProfileMarker::NAME, |chan| Some((ProfileMarker::NAME, chan)))
         .add_service_at(Server_Marker::NAME, |chan| Some((Server_Marker::NAME, chan)));
