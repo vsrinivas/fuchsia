0111f1e3102dc2afecc7f9377ba172e917d5840e
shenmeng <shenmeng@oppo.com>
2020-04-07 02:59:40 +0000

    [kernel][arm64] Keep the value of PSTATE.PAN unchanged on taking an exception to EL1
    Set Privileged Access Never, on taking an exception to EL1.
    0b0 PSTATE.PAN is set to 1 on taking an exception to EL1.
    0b1 The value of PSTATE.PAN is left unchanged on taking an exception to EL1.
    In a system where the PE resets into EL1, this field resets to an
    architecturally UNKNOWN value.
    
    If run fuchsia on Qualcomm Snapdragon-710，the bit is not
    set for any processor other than processor 0.When processor 1
    runs usrboot and trap into the kernel accessing the userspace
    address, it gets stuck.If this bit is set for all processors,
    then fuchsia works correctly.Run Fuchsia on the development board
    vim2. The initial value of this bit is 1, so there is no problem.
    
    Change-Id: Ia204481ce255bbb4146f28885b1b43d5724cf64c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/353435
    Commit-Queue: Carlos Pizano <cpu@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Testability-Review: Corey Tabaka <eieio@google.com>

--- zircon/kernel/arch/arm64/arch.cc
+++ zircon/kernel/arch/arm64/arch.cc
@@ -51,6 +51,7 @@ static constexpr uint64_t PMUSERENR_EL0_ENABLE = 1 << 0;  // Enable EL0 access t
 
 // System Control Register, EL1.
 static constexpr uint64_t SCTLR_EL1_UCI = 1 << 26;  // Allow certain cache ops in EL0.
+static constexpr uint64_t SCTLR_EL1_SPAN = 1 << 23; // Keep the value of PSTATE.PAN unchanged on taking an exception to EL1.
 static constexpr uint64_t SCTLR_EL1_UCT = 1 << 15;  // Allow EL0 access to CTR register.
 static constexpr uint64_t SCTLR_EL1_DZE = 1 << 14;  // Allow EL0 to use DC ZVA.
 static constexpr uint64_t SCTLR_EL1_SA0 = 1 << 4;   // Enable Stack Alignment Check EL0.
@@ -159,7 +160,7 @@ static void arm64_cpu_early_init() {
 
   // Set some control bits in sctlr.
   uint64_t sctlr = __arm_rsr64("sctlr_el1");
-  sctlr |= SCTLR_EL1_UCI | SCTLR_EL1_UCT | SCTLR_EL1_DZE | SCTLR_EL1_SA0 | SCTLR_EL1_SA;
+  sctlr |= SCTLR_EL1_UCI | SCTLR_EL1_SPAN | SCTLR_EL1_UCT | SCTLR_EL1_DZE | SCTLR_EL1_SA0 | SCTLR_EL1_SA;
   sctlr &= ~SCTLR_EL1_AC;  // Disable alignment checking for EL1, EL0.
   __arm_wsr64("sctlr_el1", sctlr);
   __isb(ARM_MB_SY);
