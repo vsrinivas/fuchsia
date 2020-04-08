55ee7e46d0018f1c11e016c509ce0fb129e0a58d
Venkatesh Srinivas <venkateshs@google.com>
2020-04-08 20:27:19 +0000

    [virtio][ethernet] Basic unit test of virtio/ethernet driver
    virtio/ethernet is a simple driver for the virtio-net paravirtual
    ethernet adapter, used in QEMU/KVM and GCE.
    
    Add a basic unit test that starts / stops the device and checks for
    spurious ring kicks / packet transmissions. Unit tests for packet
    send/receive will be added soon, esp. to cover bugs found while
    working on fxb/49133
    
    Bug 49133: profile fyi builders timing out with lots of "dropping packet...
    
    Change-Id: I41c61e992df85c1477ed15ee2eb8bd3dfe51a7b2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/378394
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Testability-Review: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: Venkatesh Srinivas <venkateshs@google.com>

---
+++ src/connectivity/ethernet/drivers/virtio/ethernet_test.cc
@@ -0,0 +1,81 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ethernet.h"
+
+#include <atomic>
+#include <lib/fake-bti/bti.h>
+#include <zxtest/zxtest.h>
+
+#include "src/devices/bus/lib/virtio/backends/fake.h"
+
+namespace virtio {
+namespace {
+
+class FakeBackendForEthernetTest : public FakeBackend {
+ public:
+  enum Queue { RECEIVE = 0, TRANSMIT = 1, CONTROL = 2 };
+
+  typedef FakeBackend Base;
+
+  FakeBackendForEthernetTest()
+      : FakeBackend({{Queue::RECEIVE, 128}, {Queue::TRANSMIT, 128}, {Queue::CONTROL, 128}}) {
+    virtio_net_config_t config = {};
+    config.status = VIRTIO_NET_S_LINK_UP;
+    for (uint16_t i = 0; i < sizeof(config); ++i) {
+      AddClassRegister(i, reinterpret_cast<uint8_t*>(&config)[i]);
+    }
+  }
+
+ bool QueueKicked(uint16_t queue_index) { return Base::QueueKicked(queue_index); }
+};
+
+class EthernetDeviceTests : public zxtest::Test {
+ public:
+  void SetUp() override {
+    auto backend = std::make_unique<FakeBackendForEthernetTest>();
+    backend_ = backend.get();
+    zx::bti bti(ZX_HANDLE_INVALID);
+    fake_bti_create(bti.reset_and_get_address());
+    device_ =
+        std::make_unique<EthernetDevice>(/*parent=*/nullptr, std::move(bti), std::move(backend));
+    ASSERT_OK(device_->Init());
+
+    ops_.status = [](void* ctx, uint32_t status) {
+      reinterpret_cast<EthernetDeviceTests*>(ctx)->set_status(status);
+    };
+    ops_.recv = [](void* ctx, const void* data_buffer, size_t data_size, uint32_t flags) {
+      reinterpret_cast<EthernetDeviceTests*>(ctx)->receive(data_buffer, data_size, flags);
+    };
+    protocol_.ops = &ops_;
+    protocol_.ctx = this;
+  }
+
+  void set_status(uint32_t status) { last_status_ = status; }
+  void receive(const void* data_buffer, size_t data_size, uint32_t flags) {
+    received_++;
+  }
+
+  std::unique_ptr<EthernetDevice> device_;
+  FakeBackendForEthernetTest* backend_;
+  ethernet_ifc_protocol_ops_t ops_;
+  ethernet_ifc_protocol_t protocol_;
+
+  uint32_t last_status_;
+  std::atomic<int> received_;
+};
+
+TEST_F(EthernetDeviceTests, Start) {
+  EXPECT_OK(device_->EthernetImplStart(&protocol_));
+  EXPECT_EQ(last_status_, ETHERNET_STATUS_ONLINE);
+  device_->EthernetImplStop();
+
+  EXPECT_TRUE(backend_->QueueKicked(FakeBackendForEthernetTest::Queue::RECEIVE));
+  EXPECT_FALSE(backend_->QueueKicked(FakeBackendForEthernetTest::Queue::TRANSMIT));
+  EXPECT_FALSE(backend_->QueueKicked(FakeBackendForEthernetTest::Queue::CONTROL));
+  EXPECT_EQ(0, received_);  // No spurious packet reception.
+}
+
+}  // anonymous namespace
+}  // namespace virtio
