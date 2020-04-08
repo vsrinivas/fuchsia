51547492c4b4597f95305385f68a59ed90eba407
Peng Hao <richard.peng@oppo.com>
2020-04-08 20:38:16 +0000

    [zircon][top] Add output infomation thread's last_cpu
    Now that migration is supported, it is necessary to output
    the CPU that the thread last ran on.
    
         PID      TID      TIME% CPU STATE NAME
       14383    14389      96.44  3   run sysinfo.cm:initial-thread
        4850     4852      61.82  2   run /boot/bin/blobfs:initial-thread
       13551    13557      60.56  0   run top:initial-thread
       14261    14264      55.01  2 block wlanstack.cmx:initial-thread
        2066     2068      46.84  1 block console.cm:initial-thread
    
    Change-Id: Ic1622ebd923b9a817173b452c1472ae54f04f336
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/378993
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Testability-Review: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: Abdulla Kamar <abdulla@google.com>

--- zircon/system/uapp/psutils/top.c
+++ zircon/system/uapp/psutils/top.c
@@ -164,7 +164,8 @@ static void sort_threads(enum sort_order order) {
 
 static void print_threads(void) {
   thread_info_t* e;
-  printf("%8s %8s %10s %5s %s\n", "PID", "TID", raw_time ? "TIME_NS" : "TIME%", "STATE", "NAME");
+  printf("%8s %8s %10s %4s %5s %s\n", "PID", "TID", raw_time ? "TIME_NS" : "TIME%", "CPU", "STATE",
+         "NAME");
 
   int i = 0;
   list_for_every_entry (&thread_list, e, thread_info_t, node) {
@@ -177,11 +178,11 @@ static void print_threads(void) {
       if (e->delta_time > 0)
         percent = e->delta_time / (double)delay * 100;
 
-      printf("%8lu %8lu %10.2f %5s %s:%s\n", e->proc_koid, e->koid, percent, state_string(&e->info),
-             e->proc_name, e->name);
+      printf("%8lu %8lu %10.2f %4u %5s %s:%s\n", e->proc_koid, e->koid, percent,
+             e->stats.last_scheduled_cpu, state_string(&e->info), e->proc_name, e->name);
     } else {
-      printf("%8lu %8lu %10lu %5s %s:%s\n", e->proc_koid, e->koid, e->delta_time,
-             state_string(&e->info), e->proc_name, e->name);
+      printf("%8lu %8lu %10lu %4u %5s %s:%s\n", e->proc_koid, e->koid, e->delta_time,
+             e->stats.last_scheduled_cpu, state_string(&e->info), e->proc_name, e->name);
     }
 
     // only print the first count items (or all, if count < 0)
