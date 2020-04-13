47c592c25a5693b1b744646ccb171589578b4a17
Mark Seaborn <mseaborn@google.com>
2020-04-13 23:03:36 +0000

    [microbenchmarks] Do multiple process runs in SL4F test for parity with non-SL4F
    This is in preparation for switching the perfcompare trybot to using
    the SL4F-based wrapper
    (src/tests/end_to_end/perf/test/microbenchmarks_test.dart) instead of
    the older non-SL4F-based wrapper (src/tests/benchmarks/benchmarks.cc).
    
    Change microbenchmarks_test.dart to do multiple process runs of
    fuchsia_microbenchmarks similar to how benchmarks.cc does that.  The
    reasons are described in the comments, which are based on the comments
    in benchmarks.cc.
    
    A difference from benchmarks.cc is that we do the extra process runs
    unconditionally, even when the test is not running in the context of
    perfcompare.  This is for simplicity, and the extra running time is
    not much.  (There is no flag plumbed through to indicate whether the
    test is running on a perfcompare builder.  We could check the
    BUILDER_NAME env var, but we don't really want to conditionalise on
    infra builder names.)
    
    Bug: 49280
    Test: on CQ: fuchsia-x64-perfcompare-dawson_canyon-sl4f
     * Confidence intervals *should* get narrower, but this is currently
       confounded somewhat by issue 49784.
    Change-Id: I1b59fca7e10222df51924180388525ac5f0d6ff1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/380082
    Commit-Queue: Mark Seaborn <mseaborn@google.com>
    Reviewed-by: Nathan Rogers <nathanrogers@google.com>
    Testability-Review: Nathan Rogers <nathanrogers@google.com>

--- src/tests/end_to_end/perf/test/microbenchmarks_test.dart
+++ src/tests/end_to_end/perf/test/microbenchmarks_test.dart
@@ -12,9 +12,40 @@ void main() {
   test('fuchsia_microbenchmarks', () async {
     final helper = await PerfTestHelper.make();
     const resultsFile = '/tmp/perf_results.json';
+
+    // We run the fuchsia_microbenchmarks process multiple times.  That is
+    // useful for tests that exhibit between-process variation in results
+    // (e.g. due to memory layout chosen when a process starts) -- it
+    // reduces the variation in the average that we report.
+    //
+    // The perfcompare.py tool is able to use results from multiple process
+    // runs, and this helps reduce the widths of confidence intervals it
+    // reports for the microbenchmarks.
+    //
+    // However, catapult_converter does not yet support merging results
+    // from multiple process runs, so it only uses the results from the
+    // first process run.  (That limitation is partly because
+    // catapult_converter is run separately on the results from each
+    // process run.)
+    const int processRuns = 6;
+
     final result = await helper.sl4fDriver.ssh
         .run('/bin/fuchsia_microbenchmarks -p --quiet --out $resultsFile');
     expect(result.exitCode, equals(0));
+    // This makes the results visible to both perfcompare and Catapult.
     await helper.processResults(resultsFile);
+
+    for (var process = 0; process < processRuns - 1; ++process) {
+      // Pass "--runs" to reduce the number of within-process iterations of
+      // each test case.  This reduces the cost of these extra process
+      // runs.
+      final result = await helper.sl4fDriver.ssh
+          .run('/bin/fuchsia_microbenchmarks -p --quiet --runs 100'
+              ' --out $resultsFile');
+      expect(result.exitCode, equals(0));
+      // This makes the results visible to perfcompare but not Catapult.
+      await helper.storage.dumpFile(resultsFile,
+          'results_microbenchmarks_process$process', 'fuchsiaperf.json');
+    }
   }, timeout: Timeout.none);
 }
