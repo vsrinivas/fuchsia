b67aeb864fdeaccd336cefbfa439125b0a6d69f1
Mark Seaborn <mseaborn@google.com>
2020-04-13 21:28:46 +0000

    [microbenchmarks] Remove logging for debugging SSH flakiness issue
    When the flaky failures occurred, the "exit status" logging was *not*
    appearing in the output, suggesting a problem with SSH rather than
    with fuchsia_microbenchmarks.  (Also, this occurs with other tests
    too.)
    
    We've seen this occur enough times that the logging is no longer
    necessary.
    
    Note that this flake is still occurring, but at a much lower rate than
    originally (at least according to flake-fetcher).
    
    Bug: 39577
    Test: on CQ: terminal-x64-release
     * tested locally with: fx run-e2e-tests microbenchmarks_test
    Change-Id: Ib1be31d097714a4f577f82021557618ba73f7510
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/380008
    Reviewed-by: Nathan Rogers <nathanrogers@google.com>
    Testability-Review: Nathan Rogers <nathanrogers@google.com>
    Commit-Queue: Mark Seaborn <mseaborn@google.com>

--- src/tests/end_to_end/perf/test/microbenchmarks_test.dart
+++ src/tests/end_to_end/perf/test/microbenchmarks_test.dart
@@ -12,14 +12,8 @@ void main() {
   test('fuchsia_microbenchmarks', () async {
     final helper = await PerfTestHelper.make();
     const resultsFile = '/tmp/perf_results.json';
-    // Log the full 32-bit exit status value in order to debug the flaky failure
-    // in fxb/39577.  Do that on the Fuchsia side because the exit status we get
-    // back from SSH is apparently truncated to 8 bits.
-    const logStatus = r'status=$?; '
-        r'echo exit status: $status; '
-        r'if [ $status != 0 ]; then exit 1; fi';
-    final result = await helper.sl4fDriver.ssh.run(
-        '/bin/fuchsia_microbenchmarks -p --quiet --out $resultsFile; $logStatus');
+    final result = await helper.sl4fDriver.ssh
+        .run('/bin/fuchsia_microbenchmarks -p --quiet --out $resultsFile');
     expect(result.exitCode, equals(0));
     await helper.processResults(resultsFile);
   }, timeout: Timeout.none);
