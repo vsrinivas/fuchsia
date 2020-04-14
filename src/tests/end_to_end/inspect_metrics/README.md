1e58073feef3461e8971a3aeaa7e9d701f458d2b
Hai Bi <bihai@google.com>
2020-04-14 21:56:29 +0000

    [metrics][inspect] Add e2e test to read inspect
    DiskUsage may migrate, so this test will make sure reader side can be
    changed when this happens
    
    Bug: b/152262213
    Change-Id: I6f10a7736fa5ff700fc83d562044236fba96b398
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/378160
    Commit-Queue: Hai Bi <bihai@google.com>
    Reviewed-by: mesch <mesch@google.com>
    Reviewed-by: Isma Cortes <isma@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Testability-Review: Miguel Flores <miguelfrde@google.com>

---
+++ src/tests/end_to_end/inspect_metrics/README.md
@@ -0,0 +1,5 @@
+# Metrics to Inspect Test
+
+Test of the pipeline for writing metrics via Inspect.
+
+This test is for the workstation product. It can also be used for other products.
---
+++ src/tests/end_to_end/inspect_metrics/test/inspect_metrics_test.dart
@@ -0,0 +1,63 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:test/test.dart';
+import 'package:sl4f/sl4f.dart' as sl4f;
+
+const _timeout = Duration(seconds: 60);
+
+Future<void> _validateNode(sl4f.Inspect inspect, String selector) async {
+  final list = selector.split(':');
+  if (list.length != 3) {
+    fail('selector format should contain 2 colons');
+  }
+
+  final top = await inspect.snapshot(['$selector']);
+  if (top == null || top.isEmpty) {
+    fail('inspect selector $selector does not exist');
+  }
+
+  final paths = list[1].split('/');
+  var node = top[0]['contents'];
+  for (final name in paths) {
+    node = node[name];
+  }
+
+  final result = node[list[2]];
+  if (result == null) {
+    fail('inspect property ${list[2]} does not exist for selector $selector');
+  }
+}
+
+void main() {
+  sl4f.Sl4f sl4fDriver;
+
+  setUp(() async {
+    sl4fDriver = sl4f.Sl4f.fromEnvironment();
+    await sl4fDriver.startServer();
+  });
+
+  tearDown(() async {
+    await sl4fDriver.stopServer();
+    sl4fDriver.close();
+  });
+
+  test('inspect metrics tests inspection nodes written by metrics', () async {
+    // The test will read each of the node for disk usages
+    // fx set ... --release --with=//src/tests/end_to_end/inspect_metrics:test --with garnet/bin/sl4f:bin
+    // fx build
+    // fx run-e2e-tests inspect_metrics_test # which sets FUCHSIA_IPV4_ADDR automatically
+
+    final inspect = sl4f.Inspect(sl4fDriver);
+
+    // The following paths are used by reader code in b/152262213. Please notify if the paths are moved.
+    // There should be plan on how to move the path for the reader side to work before and after the move.
+    await _validateNode(inspect,
+        'archivist.cmx:root/data_stats/global_data/global_data/cache:size');
+    await _validateNode(inspect,
+        'archivist.cmx:root/data_stats/global_data/stats:used_bytes');
+  },
+      // This is a large test that waits for the DUT to come up.
+      timeout: Timeout(_timeout));
+}
