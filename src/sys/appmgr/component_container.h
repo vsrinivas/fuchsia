a6e03d7f9831427e5f14942843ed826447374955
Ben Wright <benwright@google.com>
2020-04-15 18:02:39 +0000

    [appmgr] Fix missing namespace.
    Small nit CL. Adds the component namespace to the component container.
    
    Change-Id: Ic3d7da505b0036d2275b92a7972e98ac4c6593a2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/380438
    Reviewed-by: Gary Bressler <geb@google.com>
    Testability-Review: Gary Bressler <geb@google.com>
    Commit-Queue: Benjamin Wright <benwright@google.com>

--- src/sys/appmgr/component_container.h
+++ src/sys/appmgr/component_container.h
@@ -5,11 +5,15 @@
 #ifndef SRC_SYS_APPMGR_COMPONENT_CONTAINER_H_
 #define SRC_SYS_APPMGR_COMPONENT_CONTAINER_H_
 
+namespace component {
+
 template <typename T>
 class ComponentContainer {
  public:
   virtual std::shared_ptr<T> ExtractComponent(T* controller) = 0;
   virtual ~ComponentContainer(){};
 };
+
+}  // namespace component
 
 #endif  // SRC_SYS_APPMGR_COMPONENT_CONTAINER_H_
