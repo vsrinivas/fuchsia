ca689bec574ac4d194012ac18f6c554a43e1e7e2
Martin Puryear <mpuryear@google.com>
2020-04-15 19:38:28 +0000

    [media][tests] Fix device test synchronization
    This CL updates the plug/unplug durability test case to wait on the
    signal indicating that the system has acted upon the device being
    plugged/unplugged.
    
    It also updates audio_device_tests to take into account that the
    default state of virtual audio devices is plugged (not unplugged).
    
    Test: test-code only
    
    Change-Id: If3c49a9f16221776c1daa15dc891f4ad03e2b44b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/380213
    Reviewed-by: Tim Detwiler <tjdetwiler@google.com>
    Testability-Review: Tim Detwiler <tjdetwiler@google.com>
    Commit-Queue: Martin Puryear <mpuryear@google.com>

--- src/media/audio/audio_core/test/device/src/virtual_devices.rs
+++ src/media/audio/audio_core/test/device/src/virtual_devices.rs
@@ -35,12 +35,12 @@ async fn input_device_remove() -> Result<()> {
 
 #[fasync::run_singlethreaded]
 #[test]
-async fn input_device_remove_plugged() -> Result<()> {
+async fn input_device_remove_unplugged() -> Result<()> {
     with_connected_device(|assets: DeviceTestAssets<InputProxy>| {
         async move {
             assets.device.change_plug_state(
                 zx::Time::get(zx::ClockId::Monotonic).into_nanos(),
-                /*plugged=*/ true,
+                /*plugged=*/ false,
             )?;
 
             assets.device.remove()?;
@@ -88,12 +88,12 @@ async fn output_device_remove() -> Result<()> {
 
 #[fasync::run_singlethreaded]
 #[test]
-async fn output_device_remove_plugged() -> Result<()> {
+async fn output_device_remove_unplugged() -> Result<()> {
     with_connected_device(|assets: DeviceTestAssets<OutputProxy>| {
         async move {
             assets.device.change_plug_state(
                 zx::Time::get(zx::ClockId::Monotonic).into_nanos(),
-                /*plugged=*/ true,
+                /*plugged=*/ false,
             )?;
 
             assets.device.remove()?;
@@ -118,40 +118,75 @@ async fn output_device_remove_plugged() -> Result<()> {
 async fn plug_unplug_durability() -> Result<()> {
     let env = Environment::new()?;
 
+    // Two enumerators, so we can create separate event streams for Adds and DefaultChanges
     let enumerator = env.connect_to_service::<AudioDeviceEnumeratorMarker>()?;
+    let enumerator2 = env.connect_to_service::<AudioDeviceEnumeratorMarker>()?;
 
     let mut device_adds = enumerator.take_event_stream().try_filter_map(move |e| {
         future::ready(Ok(AudioDeviceEnumeratorEvent::into_on_device_added(e)))
     });
+    let mut default_changes = enumerator2.take_event_stream().try_filter_map(move |e| {
+        future::ready(Ok(AudioDeviceEnumeratorEvent::into_on_default_device_changed(e)))
+    });
 
+    // Configure and add an input device that can notify audio_core when un/plugged
+    let mut expected_id1_bytes: [u8; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    let device1 = InputProxy::connect(&env)?;
+    device1.set_unique_id(&mut expected_id1_bytes)?;
+    device1.set_plug_properties(
+        /*plug_change_time=*/ 0, /*plugged=*/ true, /*hardwired=*/ false,
+        /*can_notify=*/ true,
+    )?;
+
+    // Wait for device1 to be added and signaled.
+    device1.add()?;
+    let device1_info = device_adds.try_next().await?.expect("");
+
+    // Wait for device1 to become the new default.
+    let (old_token, new_token) = default_changes.try_next().await?.expect("");
+    assert_eq!(old_token, 0);
+    assert_eq!(new_token, device1_info.token_id);
+
+    // Configure/add another (unique) input device that notifies audio_core when un/plugged
+    let mut expected_id2_bytes: [u8; 16] = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    let device2 = InputProxy::connect(&env)?;
+    device2.set_unique_id(&mut expected_id2_bytes)?;
+    device2.set_plug_properties(0, true, false, true)?;
+
+    // Wait for device2 to be added and signaled.
+    device2.add()?;
+    let device2_info = device_adds.try_next().await?.expect("");
+
+    // Wait for device2 to become the new default.
+    let (old_token, new_token) = default_changes.try_next().await?.expect("");
+    assert_eq!(old_token, device1_info.token_id);
+    assert_eq!(new_token, device2_info.token_id);
+
+    // Repeat the plug-unplug cycle 20 times. If no notification, hang. If disconnect, error.
     for _ in 0..20 {
-        let mut expected_id1_bytes: [u8; 16] =
-            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-        let device1 = InputProxy::connect(&env)?;
-        device1.set_unique_id(&mut expected_id1_bytes)?;
-        device1.add()?;
+        // After device2 unplug, wait for signal that default has moved device2 => device1.
+        device2.change_plug_state(zx::Time::get(zx::ClockId::Monotonic).into_nanos(), false)?;
+        let (old_token, new_token) = default_changes.try_next().await?.expect("");
+        assert_eq!(old_token, device2_info.token_id);
+        assert_eq!(new_token, device1_info.token_id);
 
-        let mut expected_id2_bytes: [u8; 16] =
-            [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-        let device2 = InputProxy::connect(&env)?;
-        device2.set_unique_id(&mut expected_id2_bytes)?;
-        device2.add()?;
+        // After device1 unplug, wait for signal that default has moved device1 => 0.
+        device1.change_plug_state(zx::Time::get(zx::ClockId::Monotonic).into_nanos(), false)?;
+        let (old_token, new_token) = default_changes.try_next().await?.expect("");
+        assert_eq!(old_token, device1_info.token_id);
+        assert_eq!(new_token, 0);
 
-        // Synchronize device adds with enumerator.
-        device_adds.try_next().await?;
-        device_adds.try_next().await?;
+        // After device1 plug, wait for signal that default has moved 0 => device1.
+        device1.change_plug_state(zx::Time::get(zx::ClockId::Monotonic).into_nanos(), true)?;
+        let (old_token, new_token) = default_changes.try_next().await?.expect("");
+        assert_eq!(old_token, 0);
+        assert_eq!(new_token, device1_info.token_id);
 
-        device1.change_plug_state(
-            zx::Time::get(zx::ClockId::Monotonic).into_nanos(),
-            /*plugged=*/ true,
-        )?;
-        drop(device1);
-
-        device2.change_plug_state(
-            zx::Time::get(zx::ClockId::Monotonic).into_nanos(),
-            /*plugged=*/ true,
-        )?;
-        drop(device2);
+        // After device2 plug, wait for signal that default has moved device1 => device2.
+        device2.change_plug_state(zx::Time::get(zx::ClockId::Monotonic).into_nanos(), true)?;
+        let (old_token, new_token) = default_changes.try_next().await?.expect("");
+        assert_eq!(old_token, device1_info.token_id);
+        assert_eq!(new_token, device2_info.token_id);
     }
 
     Ok(())
