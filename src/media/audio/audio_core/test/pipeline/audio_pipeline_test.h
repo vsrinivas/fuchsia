9afafc63bba456a92afd6a2feb791334c37e973a
Sarah Chan <spqchan@google.com>
2020-04-16 16:13:03 +0000

    Revert "[audio_core][tests] Fix flakes in pipeline tests"
    This reverts commit d61c420dc9361097d5f9340395896e73adcd0925.
    
    Reason for revert: Causing very frequent flakes. See fxb/50183
    
    Original change's description:
    > [audio_core][tests] Fix flakes in pipeline tests
    > 
    > This CL contains fixes for three issues that may be causing
    > the intermittent test failiures in audio_pipeline_tests.
    > 
    > Because the audio renderer may release packets before their
    > contents are entirely rendered to the output ring buffer, we
    > now write an additional packet of silence after our signal,
    > to guarantee a tight "right edge" on the signal we expect
    > in the virtual audio device's buffer.
    > 
    > Similarly, when interrupting an audio stream in-progress,
    > audio_pipeline_tests now waits for two packets (not just
    > one) to complete, before examining the contents of the
    > virtual ring buffer.  When subsequently restarting the
    > submittal of audio packets, the test waits for an entire
    > min_lead_time duration rather than the approximation of
    > 40 ms.
    > 
    > Finally, this test suite is now intolerant of other audio
    > devices arriving/departing during test execution, and will
    > assert if any unexpected device activity occurs. This
    > should never be the case, since audio_pipeline_tests is
    > run hermetically, although it is possible that a device
    > instance from a previous test case may have caused a problem
    > in this regard. This CL builds upon previous CL to fix
    > audio_core's state machine regarding device-plug/-unplug and
    > device-remove.
    > 
    > MULTIPLY: `[
    >   {
    >     "name": "audio_pipeline_tests",
    >     "total_runs": 200
    >   }
    > ]`
    > 
    > Test: test-only change
    > Fixed: 49469
    > 
    > Change-Id: I6c9f3d67bc2bf6802b9e924fb55bedd7ef43a042
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/379836
    > Commit-Queue: Martin Puryear <mpuryear@google.com>
    > Reviewed-by: Tim Detwiler <tjdetwiler@google.com>
    > Testability-Review: Tim Detwiler <tjdetwiler@google.com>
    
    TBR=tjdetwiler@google.com,mpuryear@google.com,tombergan@google.com,pbradsky@google.com
    
    Change-Id: If6defd064bc2fdf064644cf8847203b826bade06
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/380957
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Commit-Queue: Sarah Chan <spqchan@google.com>

--- src/media/audio/audio_core/test/pipeline/audio_pipeline_test.h
+++ src/media/audio/audio_core/test/pipeline/audio_pipeline_test.h
@@ -10,43 +10,38 @@
 #include <lib/fzl/vmo-mapper.h>
 #include <lib/zx/vmo.h>
 
-#include <set>
+#include <unordered_set>
 
 #include "src/media/audio/lib/test/hermetic_audio_test.h"
 
 namespace media::audio::test {
 
-// VAD format values
-constexpr uint32_t kFrameRate = 48000;
-constexpr uint16_t kRateFamilyFlags = 1;  // CONTINUOUS
-
-constexpr uint32_t kSampleFormat = 4;  // 16-bit LPCM
-constexpr auto kAudioFormat = fuchsia::media::AudioSampleFormat::SIGNED_16;
-constexpr uint32_t kSampleSize = 2;
-
-constexpr uint32_t kNumChannels = 2;
-constexpr uint32_t kFrameSize = kSampleSize * kNumChannels;
-
-constexpr uint32_t kFifoDepthBytes = 0;
-constexpr auto kExternalDelay = zx::msec(0);
+// VAD default format values
+constexpr uint32_t kDefaultFrameRate = 48000;
+constexpr uint32_t kDefaultSampleFormat = 4;  //  16-bit LPCM
+constexpr fuchsia::media::AudioSampleFormat kDefaultAudioFormat =
+    fuchsia::media::AudioSampleFormat::SIGNED_16;
+constexpr uint32_t kDefaultSampleSize = 2;
+constexpr uint32_t kDefaultNumChannels = 2;
+constexpr zx_duration_t kDefaultExternalDelayNs = 0;
+constexpr uint32_t kDefaultFrameSize = kDefaultSampleSize * kDefaultNumChannels;
 
 // Test-specific values
 // For our shared buffer to the renderer, use 50 pkts of 10 ms each
 constexpr uint32_t kPacketMs = 10;
 constexpr uint32_t kNumPayloads = 50;
-constexpr uint32_t kPacketFrames = kFrameRate / 1000 * kPacketMs;
-constexpr uint32_t kPacketSamples = kPacketFrames * kNumChannels;
-constexpr uint32_t kPacketBytes = kPacketSamples * kSampleSize;
+constexpr uint32_t kPacketFrames = kDefaultFrameRate / 1000 * kPacketMs;
+constexpr uint32_t kPacketBytes = kDefaultFrameSize * kPacketFrames;
 constexpr uint32_t kRendererFrames = kPacketFrames * kNumPayloads;
-constexpr uint32_t kRendererBytes = kFrameSize * kRendererFrames;
+constexpr uint32_t kRendererBytes = kDefaultFrameSize * kRendererFrames;
 
 // Set VAD ring buffer to 1000 ms, with notifs every 10ms
 constexpr uint32_t kSectionMs = 10;
 constexpr uint32_t kNumRingSections = 100;
-constexpr uint32_t kSectionFrames = kFrameRate / 1000 * kSectionMs;
-constexpr uint32_t kSectionBytes = kFrameSize * kSectionFrames;
+constexpr uint32_t kSectionFrames = kDefaultFrameRate / 1000 * kSectionMs;
+constexpr uint32_t kSectionBytes = kDefaultFrameSize * kSectionFrames;
 constexpr uint32_t kRingFrames = kNumRingSections * kSectionFrames;
-constexpr uint32_t kRingBytes = kFrameSize * kRingFrames;
+constexpr uint32_t kRingBytes = kDefaultFrameSize * kRingFrames;
 
 class AudioPipelineTest : public HermeticAudioCoreTest {
  protected:
@@ -69,7 +64,7 @@ class AudioPipelineTest : public HermeticAudioCoreTest {
 
   void SetUpBuffers();
 
-  uint64_t RingBufferSize() const { return kFrameSize * num_rb_frames_; }
+  uint64_t RingBufferSize() const { return kDefaultFrameSize * num_rb_frames_; }
   uint8_t* RingBufferStart() const { return reinterpret_cast<uint8_t*>(ring_buffer_.start()); }
 
   // Make a copy of the virtual audio device's ring buffer, into our "snapshot" buffer.
@@ -83,15 +78,13 @@ class AudioPipelineTest : public HermeticAudioCoreTest {
   void DisplaySnapshotSection(uint32_t section);
   void DisplaySnapshotSectionsForFrames(uint32_t first = 0, uint32_t second = 0, uint32_t third = 0,
                                         uint32_t fourth = 0, uint32_t fifth = 0);
-  void DisplaySnapshotBuffer();
 
   void MapAndAddRendererBuffer(uint32_t buffer_id);
 
-  // Send timestamped packets with values increasing by 1 per sample. By default, first sample is 1
-  // for packet PTS 0. By default we send an extra packet of silence, to work around fxb/49980.
-  // TODO(49981): Don't send an extra packet, once 49980 is fixed
+  // Submit timestamped packets (also with ID-tag starting with 0). Payload data vals increase by 1
+  // per sample. By default, val of first payload sample is 1, and first packet PTS is 0.
   void CreateAndSendPackets(uint32_t num_packets, int16_t initial_data_value = 1,
-                            int64_t initial_pts = 0, bool final_silent_packet = true);
+                            int64_t initial_pts = 0);
 
   // Wait for packet with the given ID to complete.
   void WaitForPacket(uint32_t packet_num);
@@ -103,9 +96,12 @@ class AudioPipelineTest : public HermeticAudioCoreTest {
   // virtualaudio-related members
   static fuchsia::virtualaudio::ControlSyncPtr control_sync_;
   fuchsia::virtualaudio::OutputPtr output_;
+  fuchsia::virtualaudio::InputPtr input_;
+  std::unordered_set<uint64_t> virtual_device_tokens_;
 
   bool received_set_format_ = false;
   bool received_set_gain_ = false;
+  float gain_db_ = fuchsia::media::audio::MUTED_GAIN_DB;
   bool received_ring_buffer_ = false;
   zx::vmo rb_vmo_;
   uint32_t num_rb_frames_ = 0;
@@ -127,14 +123,13 @@ class AudioPipelineTest : public HermeticAudioCoreTest {
   // AudioDeviceEnum-related members
   fuchsia::media::AudioDeviceEnumeratorPtr audio_dev_enum_;
   bool received_add_device_ = false;
+  uint64_t received_add_device_token_ = 0;
+  float received_gain_db_ = fuchsia::media::audio::MUTED_GAIN_DB;
+  bool received_mute_ = true;
   bool received_remove_device_ = false;
   bool received_gain_changed_ = false;
-  bool received_default_output_changed_ = false;
-
-  uint64_t device_token_ = 0;
-  bool device_is_default_ = false;
-  bool device_mute_ = true;
-  float device_gain_db_ = fuchsia::media::audio::MUTED_GAIN_DB;
+  bool received_default_device_changed_ = false;
+  uint64_t received_default_device_token_ = 0;
 
   //
   // AudioRenderer-related members
