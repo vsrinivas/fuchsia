4de2e40b8e598e1c92fb1f5483570ada0ab681f4
Tess Eisenberger <teisenbe@google.com>
2020-04-21 22:36:01 +0000

    [log-writer-logger] Switch to LLCPP FIDL instead of hand-rolled
    This also removes a bug in the error case where we were calling
    the POSIX close (which is for file descriptors) on a zx_handle_t.
    
    Change-Id: I4bd1d643255daec1cffa875884b168aec76865f3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/382317
    Reviewed-by: Derek Gonyeo <dgonyeo@google.com>
    Commit-Queue: Tess Eisenberger <teisenbe@google.com>
    Testability-Review: Tess Eisenberger <teisenbe@google.com>

--- zircon/system/ulib/log-writer-logger/log-writer-logger.cc
+++ zircon/system/ulib/log-writer-logger/log-writer-logger.cc
@@ -2,24 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <atomic>
-#include <cstdlib>
-#include <cstring>
-#include <fbl/algorithm.h>
+#include <fuchsia/logger/llcpp/fidl.h>
 #include <lib/fdio/directory.h>
-#include <lib/fidl/txn_header.h>
-#include <lib/zx/channel.h>
-#include <lib/zx/process.h>
-#include <lib/zx/socket.h>
-#include <utility>
-#include <zircon/assert.h>
-
-#include <fuchsia/logger/c/fidl.h>
+#include <lib/log-writer-logger/log-writer-logger.h>
 #include <lib/log-writer-logger/wire_format.h>
 #include <lib/log/log.h>
 #include <lib/log/log_writer.h>
+#include <lib/zx/channel.h>
+#include <lib/zx/process.h>
+#include <lib/zx/socket.h>
+#include <zircon/assert.h>
 
-#include <lib/log-writer-logger/log-writer-logger.h>
+#include <atomic>
+#include <cstdlib>
+#include <cstring>
+#include <utility>
+
+#include <fbl/algorithm.h>
 
 namespace {
 
@@ -53,6 +52,7 @@ bool connect_to_logger(zx::socket* socket) {
   if (zx::channel::create(0, &logger, &logger_request) != ZX_OK) {
     return false;
   }
+  ::llcpp::fuchsia::logger::LogSink::SyncClient logger_client(std::move(logger));
   if (fdio_service_connect("/svc/fuchsia.logger.LogSink", logger_request.release()) != ZX_OK) {
     return false;
   }
@@ -60,13 +60,8 @@ bool connect_to_logger(zx::socket* socket) {
   if (zx::socket::create(ZX_SOCKET_DATAGRAM, &local, &remote) != ZX_OK) {
     return false;
   }
-  fuchsia_logger_LogSinkConnectRequest req;
-  memset(&req, 0, sizeof(req));
-  fidl_init_txn_header(&req.hdr, 0, fuchsia_logger_LogSinkConnectGenOrdinal);
-  req.socket = FIDL_HANDLE_PRESENT;
-  zx_handle_t handles[1] = {remote.release()};
-  if (logger.write(0, &req, sizeof(req), handles, 1) != ZX_OK) {
-    close(handles[0]);
+  auto result = logger_client.Connect(std::move(remote));
+  if (result.status() != ZX_OK) {
     return false;
   }
   *socket = std::move(local);
