ba2b3b2d37562730aede9d9f2a4bbf23358c6251
Jeremy Manson <jeremymanson@google.com>
2020-04-22 22:52:06 +0000

    [fidlcat] Run the fidlcat e2e test in infra.
    This requires a small tweak: the build system has to list all files that the
    test depends upon explicitly.  Rather than enumerate all of the possible FIDL
    IR files in question, we restrict ourselves to only the ones that the test
    actually needs.
    
    Change-Id: I1cba9ac61836aac5752545fb91e6745e3b0843fd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/381554
    Commit-Queue: Jeremy Manson <jeremymanson@google.com>
    Testability-Review: Jeremy Manson <jeremymanson@google.com>
    Reviewed-by: Vincent Belliard <vbelliard@google.com>

--- src/tests/end_to_end/fidlcat/test/fidlcat_test.dart
+++ src/tests/end_to_end/fidlcat/test/fidlcat_test.dart
@@ -2,18 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:convert';
-import 'dart:io'
-    show
-        Directory,
-        File,
-        FileMode,
-        FileSystemException,
-        Platform,
-        Process,
-        ProcessResult;
+import 'dart:io' show Platform, Process, ProcessResult;
 
-import 'package:args/args.dart';
 import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 import 'package:sl4f/sl4f.dart' as sl4f;
@@ -31,61 +21,16 @@ void main(List<String> arguments) {
   final String fidlcatPath =
       Platform.script.resolve('runtime_deps/fidlcat').toFilePath();
 
-  /// Location of the IR for the core SDK APIs.
-  String fidlIrPath;
-
-  /// A convenient directory for temporary files
-  Directory tempDir;
-
-  /// The core.fidl_json.txt file passed to this program contains a list of
-  /// FIDL IR files relative to the root build directory.  This test program
-  /// is not run out of the root build directory, so we create a new file
-  /// containing absolute paths.
-  void setPath() {
-    final parser = ArgParser()..addOption('data-dir');
-    final argResults = parser.parse(arguments);
-    final String dataDir = argResults['data-dir'];
-
-    tempDir = Directory.systemTemp.createTempSync();
-
-    final int timestamp = (DateTime.now()).microsecondsSinceEpoch;
-    final String tempPath = tempDir.path;
-    fidlIrPath = '$tempPath/core.fidl_json_processed-$timestamp.txt';
-
-    final outFile = File(fidlIrPath)..createSync(recursive: true);
-
-    final String fidlIrPathInit =
-        Platform.script.resolve('runtime_deps/core.fidl_json.txt').toFilePath();
-    File(fidlIrPathInit)
-        .openRead()
-        .transform(utf8.decoder)
-        .transform(LineSplitter())
-        .forEach((line) => {
-              outFile.writeAsStringSync('$dataDir/$line\n',
-                  mode: FileMode.append)
-            });
-  }
-
-  void cleanup() {
-    try {
-      tempDir.deleteSync(recursive: true);
-    } on FileSystemException {
-      // Do nothing.
-    }
-  }
-
   sl4f.Sl4f sl4fDriver;
 
   setUp(() async {
     sl4fDriver = sl4f.Sl4f.fromEnvironment();
     await sl4fDriver.startServer();
-    setPath();
   });
 
   tearDown(() async {
     await sl4fDriver.stopServer();
     sl4fDriver.close();
-    cleanup();
   });
 
   /// Formats an IP address so that fidlcat can understand it (removes % part,
@@ -110,6 +55,9 @@ void main(List<String> arguments) {
     }
   }
 
+  /// Simple test to ensure that fidlcat can run the echo client, and that some of the expected
+  /// output is present.  It starts the agent on the target, and then launches fidlcat with the
+  /// correct parameters.
   group('fidlcat', () {
     test('Simple test of echo client output and shutdown', () async {
       int port = await sl4fDriver.ssh.pickUnusedPort();
@@ -122,25 +70,38 @@ void main(List<String> arguments) {
       final String symbolPath = Platform.script
           .resolve('runtime_deps/echo_client_cpp.debug')
           .toFilePath();
+      // We have to list all of the IR we need explicitly, here and in the BUILD.gn file. The
+      // two lists must be kept in sync: if you add an IR here, you must also add it to the
+      // BUILD.gn file.
       final String echoIr =
           Platform.script.resolve('runtime_deps/echo.fidl.json').toFilePath();
+      final String ioIr = Platform.script
+          .resolve('runtime_deps/fuchsia-io.fidl.json')
+          .toFilePath();
+      final String sysIr = Platform.script
+          .resolve('runtime_deps/fuchsia.sys.fidl.json')
+          .toFilePath();
       ProcessResult processResult;
       do {
         processResult = await Process.run(fidlcatPath, [
           '--connect=$target:$port',
           '--quit-agent-on-exit',
-          '--fidl-ir-path=@$fidlIrPath',
           '--fidl-ir-path=$echoIr',
+          '--fidl-ir-path=$ioIr',
+          '--fidl-ir-path=$sysIr',
           '-s',
           '$symbolPath',
           'run',
           'fuchsia-pkg://fuchsia.com/echo_client_cpp#meta/echo_client_cpp.cmx',
         ]);
       } while (processResult.exitCode == 2); // 2 means can't connect (yet).
+      String additionalResult =
+          'stderr ===\n${processResult.stderr.toString()}\nstdout ===\n${processResult.stdout.toString()}';
       expect(
           processResult.stdout.toString(),
           contains(
-              'sent request fidl.examples.echo/Echo.EchoString = {"value":"hello world"}'));
+              'sent request fidl.examples.echo/Echo.EchoString = {"value":"hello world"}'),
+          reason: additionalResult);
       await agentResult;
     });
   }, timeout: _timeout);
