2dc4dd26740abf6bc2ccda4afd6c514f3955cef0
Chris Suter <csuter@google.com>
2020-04-27 11:02:35 +0000

    [storage] Add a buffer abstraction that works for both host and target.
    Introduce a ResizeableArrayBuffer type (for host use) and a
    ResizeableVmoBuffer type (for target use) that offer the same interface
    and also track VmoId's registered with a device.
    
    Bug: 42096
    Change-Id: Iaa1f2f01b5ded99c06af9453413ad9d83a7a21a5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/382975
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Testability-Review: Gianfranco Valentino <gevalentino@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>

---
+++ zircon/system/ulib/minfs/resizeable_array_buffer.cc
@@ -0,0 +1,21 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "resizeable_array_buffer.h"
+
+namespace minfs {
+
+zx_status_t ResizeableArrayBuffer::Shrink(size_t block_count) {
+  ZX_ASSERT(block_count > 0 && block_count <= capacity());
+  buffer().resize(block_count * BlockSize());
+  return ZX_OK;
+}
+
+zx_status_t ResizeableArrayBuffer::Grow(size_t block_count) {
+  ZX_ASSERT(block_count >= capacity());
+  buffer().resize(block_count * BlockSize());
+  return ZX_OK;
+}
+
+}  // namespace minfs
---
+++ zircon/system/ulib/minfs/test/unit/resizeable_array_buffer_test.cc
@@ -0,0 +1,67 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "resizeable_array_buffer.h"
+
+#include <zxtest/zxtest.h>
+
+namespace minfs {
+namespace {
+
+const uint32_t kBlockSize = 8192;
+
+TEST(ResizeableArrayBufferTest, Grow) {
+  ResizeableArrayBuffer buffer(kBlockSize);
+  ASSERT_OK(buffer.Grow(2));
+  EXPECT_EQ(2, buffer.capacity());
+  char buf[kBlockSize];
+  memset(buf, 'a', sizeof(buf));
+  memcpy(buffer.Data(1), buf, kBlockSize);
+  ASSERT_OK(buffer.Grow(50));
+  // Check that after growing, the data is still there.
+  EXPECT_BYTES_EQ(buf, buffer.Data(1), kBlockSize);
+  EXPECT_EQ(50, buffer.capacity());
+}
+
+TEST(ResizeableArrayBufferTest, Shrink) {
+  ResizeableArrayBuffer buffer(5, kBlockSize);
+  char buf[kBlockSize];
+  memset(buf, 'a', sizeof(buf));
+  memcpy(buffer.Data(1), buf, kBlockSize);
+  ASSERT_OK(buffer.Shrink(2));
+  EXPECT_BYTES_EQ(buf, buffer.Data(1), kBlockSize);
+  EXPECT_EQ(2, buffer.capacity());
+}
+
+TEST(ResizeableArrayBufferTest, Zero) {
+  ResizeableArrayBuffer buffer(5, kBlockSize);
+  memset(buffer.Data(0), 'a', kBlockSize);
+  buffer.Zero(13, 21);
+  const char* p = reinterpret_cast<const char*>(buffer.Data(0));
+  for (unsigned i = 0; i < kBlockSize; ++i) {
+    if (i < 13 || i >= 13 + 21) {
+      EXPECT_EQ('a', p[i]);
+    } else {
+      EXPECT_EQ(0, p[i]);
+    }
+  }
+}
+
+TEST(ResizeableArrayBufferDeathTest, BadGrow) {
+  ResizeableArrayBuffer buffer(10, kBlockSize);
+  ASSERT_DEATH([&]() { [[maybe_unused]] zx_status_t status = buffer.Grow(4); });
+}
+
+TEST(ResizeableArrayBufferDeathTest, BadShrink) {
+  ResizeableArrayBuffer buffer(10, kBlockSize);
+  ASSERT_DEATH([&]() { [[maybe_unused]] zx_status_t status = buffer.Shrink(15); });
+}
+
+TEST(ResizeableArrayBufferDeathTest, BadShrink2) {
+  ResizeableArrayBuffer buffer(10, kBlockSize);
+  ASSERT_DEATH([&]() { [[maybe_unused]] zx_status_t status = buffer.Shrink(0); });
+}
+
+}  // namespace
+}  // namespace minfs
