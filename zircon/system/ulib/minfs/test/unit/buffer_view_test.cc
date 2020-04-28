7e69471fb7f309d9c53e2b0390b8d498032302b1
Chris Suter <csuter@google.com>
2020-04-28 02:46:01 +0000

    [storage] Add a BaseBufferView and BufferView: views of buffers.
    These classes make it a bit easier to manipulate buffers and ensure
    mutations are flushed.
    
    Change-Id: I3586085441c0ee7d18367b76d985286e6d288132
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/362342
    Commit-Queue: Chris Suter <csuter@google.com>
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Testability-Review: Gianfranco Valentino <gevalentino@google.com>

---
+++ zircon/system/ulib/minfs/test/unit/buffer_view_test.cc
@@ -0,0 +1,135 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "buffer_view.h"
+
+#include <zxtest/zxtest.h>
+
+namespace minfs {
+namespace {
+
+constexpr int kArraySize = 100;
+constexpr uint8_t kFill = 0x56;
+constexpr uint32_t kFill32 = 0x56565656;
+
+TEST(BufferViewTest, UpdatesOnBufferAreReflectedOnView) {
+  std::array<uint8_t, kArraySize> array;
+  array.fill(kFill);
+  constexpr int kIndex = 13;
+  constexpr int kLength = 3;
+  BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), kIndex, kLength);
+  EXPECT_EQ(kFill32, *view);
+  EXPECT_EQ(kFill32, view[2]);
+  const uint32_t kData = 0xf00dface;
+  reinterpret_cast<uint32_t&>(array[(kIndex + 2) * 4]) = kData;
+  EXPECT_EQ(kFill32, *view);
+  EXPECT_EQ(kData, view[2]);
+}
+
+TEST(BufferViewTest, FlushOnCleanViewIssuesNoFlush) {
+  std::array<uint8_t, kArraySize> array;
+  array.fill(kFill);
+  bool flushed = false;
+  BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), 13, 4, [&](BaseBufferView* view) {
+    flushed = true;
+    return ZX_OK;
+  });
+
+  EXPECT_OK(view.Flush());
+
+  EXPECT_FALSE(flushed);
+}
+
+TEST(BufferViewTest, FlushOnDirtyViewIssuesFlush) {
+  std::array<uint8_t, kArraySize> array;
+  array.fill(kFill);
+  bool flushed = false;
+  constexpr int kIndex = 13;
+  constexpr int kLength = 4;
+  BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), kIndex, kLength,
+                            [&](BaseBufferView* view) {
+                              flushed = true;
+                              return ZX_OK;
+                            });
+  static const uint32_t kData = 0xfacef00d;
+  view.mut_ref(2) = kData;
+  EXPECT_EQ(kData, reinterpret_cast<uint32_t&>(array[(kIndex + 2) * 4]));
+  EXPECT_TRUE(view.dirty());
+  EXPECT_EQ(kData, view[2]);
+
+  EXPECT_OK(view.Flush());
+
+  EXPECT_TRUE(flushed);
+}
+
+TEST(BufferViewTest, FlushOnDirtyViewSetsStateToClean) {
+  std::array<uint8_t, kArraySize> array;
+  array.fill(kFill);
+  bool flushed = false;
+  BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), 13, 4, [&](BaseBufferView* view) {
+    flushed = true;
+    return ZX_OK;
+  });
+  view.mut_ref(3) = 0x12345678;
+
+  view.set_dirty(false);
+  EXPECT_OK(view.Flush());
+
+  EXPECT_FALSE(flushed);
+}
+
+TEST(BufferViewTest, Move) {
+  std::array<uint8_t, kArraySize> array;
+  array.fill(kFill);
+  BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), 13, 3,
+                            [&](BaseBufferView* view) { return ZX_OK; });
+  static const uint32_t kData = 0xfacef00d;
+  view.mut_ref(2) = kData;
+  EXPECT_TRUE(view.dirty());
+
+  auto view2 = std::move(view);
+
+  EXPECT_TRUE(view2.dirty());
+  EXPECT_TRUE(view2.IsValid());
+  EXPECT_EQ(kData, view2[2]);
+  view2.set_dirty(false);
+}
+
+TEST(BufferViewDeathTest, OutOfRangeReadAsserts) {
+  ASSERT_DEATH(([]() {
+    std::array<uint8_t, kArraySize> array;
+    BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), 13, 3);
+    view[7];
+  }));
+}
+
+TEST(BufferViewDeathTest, OutOfRangeWriteAsserts) {
+  ASSERT_DEATH(([]() {
+    std::array<uint8_t, kArraySize> array;
+    BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), 13, 3,
+                              [&](BaseBufferView* view) { return ZX_OK; });
+    view.mut_ref(7) = 1;
+    view.set_dirty(false);
+  }));
+}
+
+TEST(BufferViewDeathTest, DestructorAssertNonNullFlusher) {
+  ASSERT_DEATH(([]() {
+    std::array<uint8_t, kArraySize> array;
+    BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), 13, 3);
+    view.mut_ref(0) = 10;
+  }));
+}
+
+TEST(BufferViewDeathTest, DestructorWithDirtyStateAssertsFlushed) {
+  ASSERT_DEATH(([]() {
+    std::array<uint8_t, kArraySize> array;
+    BufferView<uint32_t> view(BufferPtr::FromMemory(array.data()), 13, 3,
+                              [&](BaseBufferView* view) { return ZX_OK; });
+    view.mut_ref() = 10;
+  }));
+}
+
+}  // namespace
+}  // namespace minfs
