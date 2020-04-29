6817d2cde6a0ff377a5ad41c10065dab816f294a
Sen Jiang <senj@google.com>
2020-04-29 21:24:38 +0000

    [build] Generate packages.json in update package.
    Fixed: 50654
    Fixed: 50746
    Change-Id: I5331d1e5b845b4916dc386b60ea9644c9f2d7d3b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/383411
    Reviewed-by: James Tucker <raggi@google.com>
    Testability-Review: James Tucker <raggi@google.com>
    Commit-Queue: Sen Jiang <senj@google.com>

---
+++ build/images/generate_packages_json.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2.7
+#
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Generates the packages.json file in the update package.
+
+import json
+import sys
+
+
+def main(input_path, output_path):
+    with open(input_path, 'r') as input_file:
+        pkgurls = []
+        for line in input_file:
+            package, merkle_path = line.strip().split('=', 1)
+            merkle = open(merkle_path, 'r').read().strip()
+            pkgurls += [
+                'fuchsia-pkg://fuchsia.com/{}?hash={}'.format(package, merkle)
+            ]
+        with open(output_path, 'w') as output_file:
+            packages_json = {'version': 1, 'content': pkgurls}
+            output_file.write(json.dumps(packages_json))
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
