27524d36c5d6d1f66045c12225d3dbe07e1ff86f
Mukesh Agrawal <quiche@google.com>
2020-05-04 22:33:21 +0000

    [fx][create] add support for running in symlinked directory
    Canonicalize the path for the Fuchsia root, to resolve any symlinks.
    This resolves a problem where `fx create` would fail when run with
    `FUCHSIA_DIR` pointing to a symlink.
    
    Bug: 51137
    Test: manual -- ran in environment described in bug, and succeded
    Test: fx test //tools/create
    Test: fx test //tools/create/goldens
    Change-Id: I4ba3825eff92d716f9d9e7b31f0607fb66564eb7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/384763
    Commit-Queue: Mukesh Agrawal <quiche@google.com>
    Reviewed-by: Adam Lesinski <adamlesinski@google.com>
    Testability-Review: Adam Lesinski <adamlesinski@google.com>

--- tools/create/src/util.rs
+++ tools/create/src/util.rs
@@ -5,11 +5,12 @@
 use anyhow::anyhow;
 use std::ffi::OsStr;
 use std::path::PathBuf;
-use std::{env, io};
+use std::{env, fs, io};
 
 /// Return the fuchsia root directory.
-pub fn get_fuchsia_root() -> Result<PathBuf, env::VarError> {
-    Ok(PathBuf::from(env::var("FUCHSIA_DIR")?))
+pub fn get_fuchsia_root() -> Result<PathBuf, anyhow::Error> {
+    let dir = env::var("FUCHSIA_DIR")?;
+    Ok(fs::canonicalize(dir)?)
 }
 
 /// Returns the path to the template files.
@@ -32,4 +33,27 @@ pub fn filename_to_string(filename: impl AsRef<OsStr>) -> io::Result<String> {
             io::Error::new(io::ErrorKind::InvalidData, anyhow!("invalid filename {:?}", filename))
         })?
         .to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+    use std::os::unix;
+    use tempfile::tempdir;
+
+    #[test]
+    fn get_fuchsia_root_resolves_symlinks() {
+        let temp_dir = tempdir().expect("internal error: failed to create temp dir");
+        let subdir_path = temp_dir.path().join("subdir");
+        let link_path = temp_dir.path().join("symlink");
+        let _subdir =
+            std::fs::File::create(&subdir_path).expect("internal error: failed to create sub dir");
+        let _link = unix::fs::symlink(&subdir_path, &link_path);
+        env::set_var("FUCHSIA_DIR", link_path);
+
+        let fuchsia_root = get_fuchsia_root();
+        assert!(fuchsia_root.is_ok(), fuchsia_root);
+        assert_eq!(fuchsia_root.unwrap(), subdir_path);
+    }
 }
