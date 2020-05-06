5e177f45ad1474ada97fdc2421477a42559c7c04
Mukesh Agrawal <quiche@google.com>
2020-05-06 21:40:39 +0000

    [location] add skeleton for emergency location service
    Add the skeleton of the Emergency Location Service. This
    skeleton was created with `fx create`.
    
    This is broken out separately from any actual implementation
    to keep the CLs small and focused.
    
    Bug: 51514
    Test: fx format-code \
          && fx set core.astro --release --with //src/connectivity/location/emergency \
          && fx build \
          && fx test emergency-tests
    Change-Id: I03667194cdfadb7604962a948967347b65a01cec
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/386844
    Reviewed-by: Mukesh Agrawal <quiche@google.com>
    Reviewed-by: Sean Olson <seanolson@google.com>
    Testability-Review: Mukesh Agrawal <quiche@google.com>
    Commit-Queue: Mukesh Agrawal <quiche@google.com>

---
+++ src/connectivity/location/emergency/README.md
@@ -0,0 +1,34 @@
+# emergency
+
+This component provides an Emergency Location Service. The service passively
+waits for Wifi scan results (via the
+`fuchsia.location.sensor.WifiBaseStationWatcher` protocol), as well as Emergency
+Location requests (via the `fuchsia.location.position.EmergencyProvider`
+protocol). When an `EmergencyProvider` request arrives, this component uses the
+Google Maps API to resolve cached scan results to the current position.
+
+Note that access to the Google Maps API requires an API key. The key should be
+provided via the
+[config-data](https://fuchsia.dev/fuchsia-src/development/components/config_data)
+mechanism.
+
+## Building
+
+To add this component to your build, append
+`--with //src/connectivity/location/emergency`
+to the `fx set` invocation.
+
+## Running
+
+```
+$ fx shell run fuchsia-pkg://fuchsia.com/emergency#meta/emergency.cmx
+```
+
+## Testing
+
+Unit tests for emergency are available in the `emergency-tests`
+package.
+
+```
+$ fx test emergency-tests
+```
---
+++ src/connectivity/location/emergency/src/main.rs
@@ -0,0 +1,47 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::{self, Context};
+use fuchsia_async as fasync;
+use fuchsia_component::server::ServiceFs;
+use futures::prelude::*;
+
+/// Wraps all hosted protocols into a single type that can be matched against
+/// and dispatched.
+enum IncomingRequest {
+    // TODO: Add a variant for each protocol being served. E.g:
+// ```
+// MyProtocol(MyProtocolRequestStream),
+// ```
+}
+
+#[fasync::run_singlethreaded]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut service_fs = ServiceFs::new_local();
+
+    // TODO: Add services here. E.g:
+    // ```
+    // service_fs.dir("svc").add_fidl_service(IncomingRequest::MyProtocol);
+    // ```
+
+    service_fs.take_and_serve_directory_handle().context("failed to serve outgoing namespace")?;
+
+    service_fs
+        .for_each_concurrent(None, |_request: IncomingRequest| async move {
+            // TODO: match on `request` and handle each protocol.
+        })
+        .await;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[fasync::run_singlethreaded(test)]
+    async fn smoke_test() {
+        assert!(true);
+    }
+}
