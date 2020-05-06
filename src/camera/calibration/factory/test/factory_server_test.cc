2c1f451c42a02a3eb74c571ec69303e05601a273
Nic Zoghb <nzo@google.com>
2020-05-06 22:36:59 +0000

    [camera][calibrate] Rename Factory Dir and Files
    Tested: n/a
    
    Change-Id: I9e956cfe3e7be3caeec3f28485b80a15abfb4095
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/384358
    Commit-Queue: Nic Zoghb <nzo@google.com>
    Reviewed-by: Brijen Raval <braval@google.com>
    Testability-Review: Brijen Raval <braval@google.com>

--- src/camera/calibration/factory/test/factory_server_test.cc
+++ src/camera/calibration/factory/test/factory_server_test.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/camera/calibration/factory_protocol/factory_protocol.h"
+#include "src/camera/calibration/factory/factory_server.h"
 
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
