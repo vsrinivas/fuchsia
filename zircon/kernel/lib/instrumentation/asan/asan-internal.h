9a04d9ee7b799df855f3fc419474a2d512be5ee6
Venkatesh Srinivas <venkateshs@google.com>
2020-05-07 07:12:34 +0000

    [kernel][x86][kasan] Implement access checking (3/N)
    Full implementation of instrumentation to check validity of all data
    memory accesses. The checks currently are active, but since we do not
    poison any memory they will always pass.
    
    Pair-programmed-with: mvanotti@
    Bug: 30033 Kernelmode ASAN for Zircon
    Change-Id: Ifb9b13b1860dc309b5119b492487c4680a26fddf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/373081
    Commit-Queue: Venkatesh Srinivas <venkateshs@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Testability-Review: Nick Maniscalco <maniscalco@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/lib/instrumentation/asan/asan-internal.h
+++ zircon/kernel/lib/instrumentation/asan/asan-internal.h
@@ -8,17 +8,31 @@
 #define ZIRCON_KERNEL_LIB_INSTRUMENTATION_ASAN_ASAN_INTERNAL_H_
 
 #include <lib/instrumentation/asan.h>
-#include <zircon/types.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #include <arch/kernel_aspace.h>
 
 #ifdef __x86_64__
 
 inline constexpr size_t kAsanShift = 3;
-inline constexpr size_t kAsanShadowSize = 64UL * 1024UL * 1024UL * 1024UL;
+inline constexpr size_t kAsanShadowSize = KERNEL_ASPACE_SIZE >> kAsanShift;
+
+// Any value in the shadow above this value is poisoned.
+inline constexpr uint8_t kAsanSmallestPoisonedValue = 0x08;
 
 static_assert(X86_KERNEL_KASAN_PDP_ENTRIES * 1024ul * 1024ul * 1024ul == kAsanShadowSize);
-static_assert(KERNEL_ASPACE_SIZE >> kAsanShift == kAsanShadowSize);
+
+// Returns the address of the shadow byte corresponding to |address|.
+static inline uint8_t* addr2shadow(uintptr_t address) {
+  DEBUG_ASSERT(address >= KERNEL_ASPACE_BASE);
+  DEBUG_ASSERT(address <= KERNEL_ASPACE_BASE + KERNEL_ASPACE_SIZE - 1);
+
+  uint8_t* const kasan_shadow_map = reinterpret_cast<uint8_t*>(KASAN_SHADOW_OFFSET);
+  uint8_t* const shadow_byte_address =
+      kasan_shadow_map + ((address - KERNEL_ASPACE_BASE) >> kAsanShift);
+  return shadow_byte_address;
+}
 
 #endif  // __x86_64__
 
