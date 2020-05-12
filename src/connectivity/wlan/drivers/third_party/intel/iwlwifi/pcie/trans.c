abd8f916ffb56b7709e65bb787db5c3ca7c842e6
Louis Yung-chieh Lo <yjlou@google.com>
2020-05-12 02:49:49 +0000

    [wlan][iwlwifi] port iwl_trans_pcie_log_scd_error()
    This function is used later to dump the Tx queue info to debug
    in the error cases. Make it standalone so that easier for review.
    Most of changes are style.
    
    Bug: 45032
    Tested: Compiled only. Will be used in the following CL.
    
    Change-Id: I3a833f482638e57d6c97abdcfa207ab30925acd3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/385380
    Reviewed-by: Sakthi Vignesh Radhakrishnan <rsakthi@google.com>
    Reviewed-by: Alex Legg <alexlegg@google.com>
    Commit-Queue: Louis Yung-chieh Lo <yjlou@google.com>
    Testability-Review: Alex Legg <alexlegg@google.com>

--- src/connectivity/wlan/drivers/third_party/intel/iwlwifi/pcie/trans.c
+++ src/connectivity/wlan/drivers/third_party/intel/iwlwifi/pcie/trans.c
@@ -2146,35 +2146,33 @@ static void iwl_trans_pcie_block_txq_ptrs(struct iwl_trans* trans, bool block) {
 
 #define IWL_FLUSH_WAIT_MS 2000
 
-#if 0   // NEEDS_PORTING
 void iwl_trans_pcie_log_scd_error(struct iwl_trans* trans, struct iwl_txq* txq) {
-    uint32_t txq_id = txq->id;
-    uint32_t status;
-    bool active;
-    uint8_t fifo;
+  uint32_t txq_id = txq->id;
+  uint32_t status;
+  bool active;
+  uint8_t fifo;
 
-    if (trans->cfg->use_tfh) {
-        IWL_ERR(trans, "Queue %d is stuck %d %d\n", txq_id, txq->read_ptr, txq->write_ptr);
-        /* TODO: access new SCD registers and dump them */
-        return;
-    }
+  if (trans->cfg->use_tfh) {
+    IWL_ERR(trans, "Queue %d is stuck %d %d\n", txq_id, txq->read_ptr, txq->write_ptr);
+    /* TODO: access new SCD registers and dump them */
+    return;
+  }
 
-    status = iwl_read_prph(trans, SCD_QUEUE_STATUS_BITS(txq_id));
-    fifo = (status >> SCD_QUEUE_STTS_REG_POS_TXF) & 0x7;
-    active = !!(status & BIT(SCD_QUEUE_STTS_REG_POS_ACTIVE));
+  status = iwl_read_prph(trans, SCD_QUEUE_STATUS_BITS(txq_id));
+  fifo = (status >> SCD_QUEUE_STTS_REG_POS_TXF) & 0x7;
+  active = !!(status & BIT(SCD_QUEUE_STTS_REG_POS_ACTIVE));
 
-    IWL_ERR(trans,
-            "Queue %d is %sactive on fifo %d and stuck for %u ms. SW [%d, %d] HW [%d, %d] FH "
-            "TRB=0x0%x\n",
-            txq_id, active ? "" : "in", fifo, jiffies_to_msecs(txq->wd_timeout), txq->read_ptr,
-            txq->write_ptr,
-            iwl_read_prph(trans, SCD_QUEUE_RDPTR(txq_id)) &
-                (trans->cfg->base_params->max_tfd_queue_size - 1),
-            iwl_read_prph(trans, SCD_QUEUE_WRPTR(txq_id)) &
-                (trans->cfg->base_params->max_tfd_queue_size - 1),
-            iwl_read_direct32(trans, FH_TX_TRB_REG(fifo)));
+  IWL_ERR(trans,
+          "Queue %d is %sactive on fifo %d and stuck for %lu ms. SW [%d, %d] HW [%d, %d] FH "
+          "TRB=0x0%x\n",
+          txq_id, active ? "" : "in", fifo, txq->wd_timeout / ZX_MSEC(1), txq->read_ptr,
+          txq->write_ptr,
+          iwl_read_prph(trans, SCD_QUEUE_RDPTR(txq_id)) &
+              (trans->cfg->base_params->max_tfd_queue_size - 1),
+          iwl_read_prph(trans, SCD_QUEUE_WRPTR(txq_id)) &
+              (trans->cfg->base_params->max_tfd_queue_size - 1),
+          iwl_read_direct32(trans, FH_TX_TRB_REG(fifo)));
 }
-#endif  // NEEDS_PORTING
 
 #if 0   // NEEDS_PORTING
 static int iwl_trans_pcie_rxq_dma_data(struct iwl_trans* trans, int queue,
