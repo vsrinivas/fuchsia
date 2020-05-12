69a879acc72e382a97d0f98918888e7fb281c5ed
Shai Barack <shayba@google.com>
2020-05-12 06:18:36 +0000

    [scripts] Resolve URLs to GN labels through tests.json
    Change-Id: I7d817963a659d9578f42f6c9419a23391730686e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/388862
    Commit-Queue: Shai Barack <shayba@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Testability-Review: Shai Barack <shayba@google.com>

---
+++ scripts/tests/url_to_label.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+FUCHSIA_ROOT = os.path.dirname(  # $root
+    os.path.dirname(  # scripts
+        SCRIPT_DIR))  # tests
+
+
+# Stdin: lines of URLs of tests
+# Stdout: lines of corresponding GN labels (empty line for no match)
+# Assumes that you called `fx set ...`
+def main():
+    with open(os.path.join(FUCHSIA_ROOT,
+                           'out/default/tests.json')) as json_file:
+        tests_json = json.loads(json_file.read())
+        url_to_label = {
+            entry['test']['name']: entry['test']['label']
+            for entry in tests_json
+        }
+        for line in sys.stdin:
+            print url_to_label.get(line.strip(), '')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
