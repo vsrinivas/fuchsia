53b94da80b59be9d1ace4e27eb6f045aa021a4e5
Travis Geiselbrecht <travisg@google.com>
2020-05-13 05:42:20 +0000

    [kernel][syscalls] move some of the duplicated syscall code into separate routines
    Shaves off about 40-50KB from the kernel on both arches. Implemented to
    maximize the amount of extra work for the function calls, so should be
    cheap, if not faster due to reduced cache pressure.
    
    Tested: CQ and boot and run on both architectures. Validate assembly by
    hand to make sure it generates fairly optimal code.
    
    Change-Id: Iafecda239dc0fe4c076b8592c0e1e5cb930a9e50
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/380816
    Reviewed-by: David Greenaway <dgreenaway@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Testability-Review: David Greenaway <dgreenaway@google.com>
    Testability-Review: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Travis Geiselbrecht <travisg@google.com>

--- zircon/kernel/lib/syscalls/syscalls.cc
+++ zircon/kernel/lib/syscalls/syscalls.cc
@@ -23,19 +23,33 @@
 
 #define LOCAL_TRACE 0
 
-int sys_invalid_syscall(uint64_t num, uint64_t pc, uintptr_t vdso_code_address) {
+// Main syscall dispatch routine. For every syscall in the system stamp out a separate
+// wrapper_<name of syscall> routine using the do_syscall inline function instantiated
+// from an a header generated from an external tool.
+//
+// The end result is a wrapper_<syscall> that does per syscall argument validation and
+// argument marshalling to an inner routine called sys_<syscall>.
+
+namespace {
+
+__NO_INLINE int sys_invalid_syscall(uint64_t num, uint64_t pc, uintptr_t vdso_code_address) {
   LTRACEF("invalid syscall %lu from PC %#lx vDSO code %#lx\n", num, pc, vdso_code_address);
   Thread::Current::SignalPolicyException();
   return ZX_ERR_BAD_SYSCALL;
 }
 
+struct syscall_pre_out {
+  uintptr_t vdso_code_address;
+  ProcessDispatcher* current_process;
+};
+
 // N.B. Interrupts must be disabled on entry and they will be disabled on exit.
 // The reason is the two calls two arch_curr_cpu_num in the ktrace calls: we
 // don't want the cpu changing during the call.
 
-template <typename T>
-inline syscall_result do_syscall(uint64_t syscall_num, uint64_t pc, bool (*valid_pc)(uintptr_t),
-                                 T make_call) {
+// Try to do as much as possible in the shared preamble code to maximize code reuse
+// between syscalls.
+__NO_INLINE syscall_pre_out do_syscall_pre(uint64_t syscall_num, uint64_t pc) {
   ktrace_tiny(TAG_SYSCALL_ENTER, (static_cast<uint32_t>(syscall_num) << 8) | arch_curr_cpu_num());
 
   CPU_STATS_INC(syscalls);
@@ -49,15 +63,12 @@ inline syscall_result do_syscall(uint64_t syscall_num, uint64_t pc, bool (*valid
                 syscall_num, pc);
 
   ProcessDispatcher* current_process = ProcessDispatcher::GetCurrent();
-  const uintptr_t vdso_code_address = current_process->vdso_code_address();
+  uintptr_t vdso_code_address = current_process->vdso_code_address();
 
-  uint64_t ret;
-  if (unlikely(!valid_pc(pc - vdso_code_address))) {
-    ret = sys_invalid_syscall(syscall_num, pc, vdso_code_address);
-  } else {
-    ret = make_call(current_process);
-  }
+  return {vdso_code_address, current_process};
+}
 
+__NO_INLINE syscall_result do_syscall_post(uint64_t ret, uint64_t syscall_num) {
   LTRACEF_LEVEL(2, "t %p ret %#" PRIx64 "\n", Thread::Current::Get(), ret);
 
   /* re-disable interrupts on the way out
@@ -70,6 +81,33 @@ inline syscall_result do_syscall(uint64_t syscall_num, uint64_t pc, bool (*valid
   return {ret, Thread::Current::Get()->IsSignaled()};
 }
 
+}  // namespace
+
+// Stamped out syscall veneer routine for every syscall. Try to maximize shared code by forcing
+// most of the setup and teardown code into non-inlined preamble and postamble code.
+template <typename T>
+inline syscall_result do_syscall(uint64_t syscall_num, uint64_t pc, bool (*valid_pc)(uintptr_t),
+                                 T make_call) {
+  // Call the shared preamble code
+  auto pre_ret = do_syscall_pre(syscall_num, pc);
+  const uintptr_t vdso_code_address = pre_ret.vdso_code_address;
+  ProcessDispatcher* current_process = pre_ret.current_process;
+
+  // Validate the user space program counter originated from the vdso at the proper location,
+  // otherwise call through to the invalid syscall handler
+  uint64_t ret;
+  if (unlikely(!valid_pc(pc - vdso_code_address))) {
+    ret = sys_invalid_syscall(syscall_num, pc, vdso_code_address);
+  } else {
+    // Per syscall inlined routine to marshall args appropriately
+    ret = make_call(current_process);
+  }
+
+  // Call through to the shared postamble code
+  return do_syscall_post(ret, syscall_num);
+}
+
+// Called when an out of bounds syscall number is passed from user space
 syscall_result unknown_syscall(uint64_t syscall_num, uint64_t pc) {
   return do_syscall(
       syscall_num, pc, [](uintptr_t) { return false; },
