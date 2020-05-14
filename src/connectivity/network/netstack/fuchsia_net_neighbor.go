b18d0dabb132938adb61b0ce91989f8c06108e76
Sam Balana <sbalana@google.com>
2020-05-14 19:58:31 +0000

    [netstack2] Add skeleton for fuchsia.net.neighbor
    Add empty stubs for `fuchsia.net.neighbor` that returns "not
    implemented" errors. Later changes will remove these errors and add
    support for the API.
    
    Change-Id: I687ebec5e3194dffcf7ead1bff23bbb229ab6ad6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/384714
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
    Testability-Review: Ghanan Gowripalan <ghanan@google.com>
    Commit-Queue: Sam Balana <sbalana@google.com>

---
+++ src/connectivity/network/netstack/fuchsia_net_neighbor.go
@@ -0,0 +1,62 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package netstack
+
+import (
+	"errors"
+	"syscall/zx/fidl"
+
+	eth "fidl/fuchsia/hardware/ethernet"
+	net "fidl/fuchsia/net"
+	neigh "fidl/fuchsia/net/neighbor"
+)
+
+var ErrNotImplemented = errors.New("not implemented")
+
+type neighborImpl struct {
+	ns *Netstack
+}
+
+var _ neigh.ViewWithCtx = (*neighborImpl)(nil)
+
+func (n *neighborImpl) OpenEntryIterator(ctx fidl.Context, it neigh.EntryIteratorWithCtxInterfaceRequest, options neigh.EntryIteratorOptions) error {
+	// TODO(fxb/51775): Implement fuchsia.net.neighbor/View.OpenEntryIterator
+	return ErrNotImplemented
+}
+
+func (n *neighborImpl) GetUnreachabilityConfigs(ctx fidl.Context) ([]neigh.InterfaceUnreachabilityConfig, error) {
+	// TODO(fxb/51776): Implement fuchsia.net.neighbor/View.GetUnreachabilityConfigs
+	return nil, ErrNotImplemented
+}
+
+var _ neigh.ControllerWithCtx = (*neighborImpl)(nil)
+
+func (n *neighborImpl) AddEntry(ctx fidl.Context, interfaceID uint64, neighbor net.IpAddress, mac eth.MacAddress) (neigh.ControllerAddEntryResult, error) {
+	// TODO(fxb/51777): Implement fuchsia.net.neighbor/Controller.AddEntry
+	resp := neigh.ControllerAddEntryResponse{}
+	result := neigh.ControllerAddEntryResultWithResponse(resp)
+	return result, ErrNotImplemented
+}
+
+func (n *neighborImpl) RemoveEntry(ctx fidl.Context, interfaceID uint64, neighbor net.IpAddress) (neigh.ControllerRemoveEntryResult, error) {
+	// TODO(fxb/51778): Implement fuchsia.net.neighbor/Controller.RemoveEntry
+	resp := neigh.ControllerRemoveEntryResponse{}
+	result := neigh.ControllerRemoveEntryResultWithResponse(resp)
+	return result, ErrNotImplemented
+}
+
+func (n *neighborImpl) ClearEntries(ctx fidl.Context, interfaceID uint64) (neigh.ControllerClearEntriesResult, error) {
+	// TODO(fxb/51779): Implement fuchsia.net.neighbor/Controller.ClearEntries
+	resp := neigh.ControllerClearEntriesResponse{}
+	result := neigh.ControllerClearEntriesResultWithResponse(resp)
+	return result, ErrNotImplemented
+}
+
+func (n *neighborImpl) UpdateUnreachabilityConfig(ctx fidl.Context, config neigh.InterfaceUnreachabilityConfig) (neigh.ControllerUpdateUnreachabilityConfigResult, error) {
+	// TODO(fxb/51780): Implement fuchsia.net.neighbor/Controller.UpdateUnreachabilityConfig
+	resp := neigh.ControllerUpdateUnreachabilityConfigResponse{}
+	result := neigh.ControllerUpdateUnreachabilityConfigResultWithResponse(resp)
+	return result, ErrNotImplemented
+}
