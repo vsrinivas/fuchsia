8df46d95dd8986ad88e70c8bf1b9da236fa5de38
Ankur Mittal <anmittal@google.com>
2020-05-15 00:10:52 +0000

    [sysmgr] Make TestSysmgr work from a universe package
    Previously we were using appmgr to launch sysmgr which expects sysmgr to
    be in base, change that to launch sysmgr independnelty and test the
    environment it creates.
    
    Fixed: 50052
    Bug: 48818
    
    Change-Id: Idcc34434762a534767ca470e58c37f1348068b64
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/388266
    Reviewed-by: Shai Barack <shayba@google.com>
    Commit-Queue: Ankur Mittal <anmittal@google.com>
    Testability-Review: Shai Barack <shayba@google.com>

--- src/sys/sysmgr/integration_tests/service_startup_test.cc
+++ src/sys/sysmgr/integration_tests/service_startup_test.cc
@@ -2,22 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fuchsia/io/cpp/fidl.h>
 #include <fuchsia/logger/cpp/fidl.h>
+#include <fuchsia/sys/cpp/fidl.h>
 #include <lib/fdio/directory.h>
-#include <lib/fdio/fd.h>
-#include <lib/fdio/fdio.h>
 #include <lib/fidl/cpp/interface_ptr.h>
+#include <lib/fidl/cpp/string.h>
 #include <lib/gtest/real_loop_fixture.h>
 #include <lib/sys/cpp/component_context.h>
-
-#include <regex>
+#include <lib/sys/cpp/service_directory.h>
 
 #include <fidl/examples/echo/cpp/fidl.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <src/lib/files/glob.h>
 #include <test/sysmgr/cpp/fidl.h>
-
-#include "src/sys/appmgr/appmgr.h"
 
 namespace sysmgr {
 namespace test {
@@ -85,72 +84,62 @@ class TestSysmgr : public ::gtest::RealLoopFixture {
 };
 
 TEST_F(TestSysmgr, ServiceStartup) {
-  zx::channel h1, h2;
-  ASSERT_EQ(ZX_OK, zx::channel::create(0, &h1, &h2));
-
   auto environment_services = sys::ComponentContext::CreateAndServeOutgoingDirectory()->svc();
+  fuchsia::sys::LaunchInfo launch_info{
+      .url = "fuchsia-pkg://fuchsia.com/sysmgr_integration_tests#meta/sysmgr.cmx"};
+  fuchsia::sys::ServiceListPtr additional_services = std::make_unique<fuchsia::sys::ServiceList>();
+  fuchsia::sys::LauncherPtr launcher;
+  environment_services->Connect(launcher.NewRequest());
 
-  // Make fidl.examples.echo.Echo from our own environment available in appmgr's
-  // root realm.
-  fuchsia::sys::ServiceListPtr root_realm_services(new fuchsia::sys::ServiceList);
-  root_realm_services->names = std::vector<std::string>{fidl::examples::echo::Echo::Name_};
-  root_realm_services->host_directory = environment_services->CloneChannel().TakeChannel();
+  fuchsia::sys::ComponentControllerPtr contoller;
+  launcher->CreateComponent(std::move(launch_info), contoller.NewRequest());
 
-  std::vector<std::string> sysmgr_args;
-  component::AppmgrArgs args{
-      .pa_directory_request = h2.release(),
-      .root_realm_services = std::move(root_realm_services),
-      .environment_services = std::move(environment_services),
-      .sysmgr_url = "fuchsia-pkg://fuchsia.com/sysmgr_integration_tests#meta/sysmgr.cmx",
-      .sysmgr_args = std::move(sysmgr_args),
-      .run_virtual_console = false,
-      .retry_sysmgr_crash = false};
-  component::Appmgr appmgr(dispatcher(), std::move(args));
+  const std::string kGlob = "/hub/r/sys/*/svc";
+  std::string path;
 
-  // h1 is connected to h2, which is injected above as appmgr's
-  // PA_DIRECTORY_REQUEST handle. appmgr hosts a directory on that handle, which
-  // includes a svc/ subdirectory, which in turn connects to the first realm's
-  // services. That first realm is the sys realm created by sysmgr, so
-  // sysmgr_svc ends up being a directory with all services in the sys realm.
-  zx::channel svc_client, svc_server;
-  ASSERT_EQ(ZX_OK, zx::channel::create(0, &svc_client, &svc_server));
-  ASSERT_EQ(ZX_OK, fdio_service_connect_at(h1.get(), "svc", svc_server.release()));
-  sys::ServiceDirectory sysmgr_svc(std::move(svc_client));
+  // wait for sysmgr to create environment.
+  RunLoopUntil([&] {
+    files::Glob glob(kGlob);
+    if (glob.size() == 1u) {
+      path = std::string(*glob.begin());
+      return true;
+    }
+    return false;
+  });
+  // connect to nested environment's svc.
+  zx::channel directory;
+  auto sysmgr_svc = sys::ServiceDirectory::CreateWithRequest(&directory);
 
-  bool received_response = false;
+  ASSERT_EQ(ZX_OK, fdio_open(path.c_str(),
+                             fuchsia::io::OPEN_RIGHT_READABLE | fuchsia::io::OPEN_RIGHT_WRITABLE,
+                             directory.release()));
+
   std::string response;
-  ::test::sysmgr::InterfacePtr interface_ptr;
-  ASSERT_EQ(ZX_OK, sysmgr_svc.Connect(interface_ptr.NewRequest(dispatcher())));
+  ::test::sysmgr::InterfaceSyncPtr interface_ptr;
+  ASSERT_EQ(ZX_OK, sysmgr_svc->Connect(interface_ptr.NewRequest()));
 
   fuchsia::logger::LogPtr log_ptr;
-  ASSERT_EQ(ZX_OK, sysmgr_svc.Connect(log_ptr.NewRequest(dispatcher())));
+  ASSERT_EQ(ZX_OK, sysmgr_svc->Connect(log_ptr.NewRequest(dispatcher())));
+  ASSERT_EQ(ZX_OK, interface_ptr->Ping(&response));
 
-  interface_ptr->Ping([&](fidl::StringPtr r) {
-    received_response = true;
-    response = r.value_or("");
-  });
-  RunLoopUntil([&] { return received_response; });
   EXPECT_EQ("test_sysmgr_service_startup", response);
 
-  const std::string echo_msg = "test string for echo";
-  received_response = false;
-  fidl::examples::echo::EchoPtr echo_ptr;
-  ASSERT_EQ(ZX_OK, sysmgr_svc.Connect(echo_ptr.NewRequest(dispatcher())));
+  {
+    // sysmgr should create the environment with parent services inherited.
+    fidl::StringPtr echo_msg = "test string for echo";
+    fidl::examples::echo::EchoSyncPtr echo_ptr;
+    ASSERT_EQ(ZX_OK, sysmgr_svc->Connect(echo_ptr.NewRequest()));
 
-  echo_ptr->EchoString(echo_msg, [&](fidl::StringPtr r) {
-    received_response = true;
-    response = r.value_or("");
-  });
-  RunLoopUntil([&] { return received_response; });
-  EXPECT_EQ(echo_msg, response);
+    fidl::StringPtr response;
+    ASSERT_EQ(ZX_OK, echo_ptr->EchoString(echo_msg, &response));
+    EXPECT_EQ(echo_msg, response);
+  }
 
   VerifyLogs(log_ptr, {"test_sysmgr_service"},
              {
                  "\\[test_sysmgr_service.cc\\([0-9]{1,4}\\)\\] Entering loop.",
                  "\\[test_sysmgr_service.cc\\([0-9]{1,4}\\)\\] Received ping.",
              });
-  VerifyLogs(log_ptr, {"appmgr"},
-             {"\\[log_connector_impl.cc\\([0-9]{1,4}\\)\\] Successfully set up syslog"});
 }
 
 }  // namespace
