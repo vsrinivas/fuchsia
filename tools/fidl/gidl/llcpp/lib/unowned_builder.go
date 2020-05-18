be5cff2e37799e9fb72f61fbe0dbd7733a3b5b71
Benjamin Prosnitz <bprosnitz@google.com>
2020-05-18 16:39:04 +0000

    [llcpp][benchmarks][gidl] LLCPP unowned builder GIDL generator refactor
    - Strings are returned from each visitor step
    - This means more stuff is inlined in the output
    
    Bug: 52131
    
    Change-Id: Id4c6b834864d4980e9db44c406d2c4e862de7c70
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/390530
    Reviewed-by: Mitchell Kember <mkember@google.com>
    Testability-Review: Mitchell Kember <mkember@google.com>
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>

--- tools/fidl/gidl/llcpp/lib/unowned_builder.go
+++ tools/fidl/gidl/llcpp/lib/unowned_builder.go
@@ -14,29 +14,24 @@ import (
 )
 
 func BuildValueUnowned(value interface{}, decl gidlmixer.Declaration) (string, string) {
-	var builder llcppValueBuilder
-	gidlmixer.Visit(&builder, value, decl)
+	var builder unownedBuilder
+	valueVar := builder.visit(value, decl)
 	valueBuild := builder.String()
-	valueVar := builder.lastVar
 	return valueBuild, valueVar
 }
 
-type llcppValueBuilder struct {
+type unownedBuilder struct {
 	strings.Builder
-	varidx  int
-	lastVar string
+	varidx int
 }
 
-func (b *llcppValueBuilder) newVar() string {
+func (b *unownedBuilder) write(format string, vals ...interface{}) {
+	b.WriteString(fmt.Sprintf(format, vals...))
+}
+
+func (b *unownedBuilder) newVar() string {
 	b.varidx++
 	return fmt.Sprintf("v%d", b.varidx)
-}
-
-func (b *llcppValueBuilder) OnBool(value bool) {
-	newVar := b.newVar()
-	// FIDL_ALIGNDECL is needed to avoid tracking_ptrs that don't have an LSB of 0.
-	b.Builder.WriteString(fmt.Sprintf("FIDL_ALIGNDECL bool %s = %t;\n", newVar, value))
-	b.lastVar = newVar
 }
 
 func primitiveTypeName(subtype fidlir.PrimitiveSubtype) string {
@@ -55,98 +50,83 @@ func primitiveTypeName(subtype fidlir.PrimitiveSubtype) string {
 	}
 }
 
-func int64String(value int64) string {
-	if value == -9223372036854775808 {
-		return "(-9223372036854775807ll - 1)"
+func (b *unownedBuilder) visit(value interface{}, decl gidlmixer.Declaration) string {
+	switch value := value.(type) {
+	case bool:
+		return fmt.Sprintf("%t", value)
+	case uint64:
+		return fmt.Sprintf("%dll", value)
+	case int64:
+		if value == -9223372036854775808 {
+			return "(-9223372036854775807ll - 1)"
+		}
+		return fmt.Sprintf("%dll", value)
+	case float64:
+		return fmt.Sprintf("%g", value)
+	case string:
+		return fmt.Sprintf("fidl::StringView(%s, %d)", strconv.Quote(value), len(value))
+	case gidlir.Record:
+		switch decl := decl.(type) {
+		case *gidlmixer.StructDecl:
+			return b.visitStruct(value, decl)
+		case *gidlmixer.TableDecl:
+			return b.visitTable(value, decl)
+		case *gidlmixer.UnionDecl:
+			return b.visitUnion(value, decl)
+		default:
+			panic("unknown record decl type")
+		}
+	case []interface{}:
+		switch decl := decl.(type) {
+		case *gidlmixer.ArrayDecl:
+			return b.visitArray(value, decl)
+		case *gidlmixer.VectorDecl:
+			return b.visitVector(value, decl)
+		default:
+			panic("unknown list decl type")
+		}
+	case nil:
+		return fmt.Sprintf("%s{}", typeName(decl))
+	default:
+		panic(fmt.Sprintf("%T not implemented", value))
 	}
-	return fmt.Sprintf("%dll", value)
 }
 
-func uint64String(value uint64) string {
-	return fmt.Sprintf("%dll", value)
-}
-
-func float64String(value float64) string {
-	return fmt.Sprintf("%g", value)
-}
-
-func (b *llcppValueBuilder) OnInt64(value int64, typ fidlir.PrimitiveSubtype) {
-	newVar := b.newVar()
-	// FIDL_ALIGNDECL is needed to avoid tracking_ptrs that don't have an LSB of 0.
-	b.Builder.WriteString(fmt.Sprintf("FIDL_ALIGNDECL %s %s = %s;\n", primitiveTypeName(typ), newVar, int64String(value)))
-	b.lastVar = newVar
-}
-
-func (b *llcppValueBuilder) OnUint64(value uint64, subtype fidlir.PrimitiveSubtype) {
-	newVar := b.newVar()
-	// FIDL_ALIGNDECL is needed to avoid tracking_ptrs that don't have an LSB of 0.
-	b.Builder.WriteString(fmt.Sprintf("FIDL_ALIGNDECL %s %s = %s;\n", primitiveTypeName(subtype), newVar, uint64String(value)))
-	b.lastVar = newVar
-}
-
-func (b *llcppValueBuilder) OnFloat64(value float64, subtype fidlir.PrimitiveSubtype) {
-	newVar := b.newVar()
-	// FIDL_ALIGNDECL is needed to avoid tracking_ptrs that don't have an LSB of 0.
-	b.Builder.WriteString(fmt.Sprintf("FIDL_ALIGNDECL %s %s = %s;\n", primitiveTypeName(subtype), newVar, float64String(value)))
-	b.lastVar = newVar
-}
-
-func (b *llcppValueBuilder) OnString(value string, decl *gidlmixer.StringDecl) {
-	newVar := b.newVar()
-	// TODO(fxb/39686) Consider Go/C++ escape sequence differences
-	b.Builder.WriteString(fmt.Sprintf(
-		"fidl::StringView %s(%s, %d)\n;", newVar, strconv.Quote(value), len(value)))
-	b.lastVar = newVar
-}
-
-func (b *llcppValueBuilder) OnBits(value interface{}, decl *gidlmixer.BitsDecl) {
-	gidlmixer.Visit(b, value, &decl.Underlying)
-	newVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf("auto %s = %s(%s);\n", newVar, typeName(decl), b.lastVar))
-	b.lastVar = newVar
-}
-
-func (b *llcppValueBuilder) OnEnum(value interface{}, decl *gidlmixer.EnumDecl) {
-	gidlmixer.Visit(b, value, &decl.Underlying)
-	newVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf("auto %s = %s(%s);\n", newVar, typeName(decl), b.lastVar))
-	b.lastVar = newVar
-}
-
-func (b *llcppValueBuilder) OnStruct(value gidlir.Record, decl *gidlmixer.StructDecl) {
+func (b *unownedBuilder) visitStruct(value gidlir.Record, decl *gidlmixer.StructDecl) string {
 	containerVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf(
-		"%s %s{};\n", declName(decl), containerVar))
+	b.write(
+		"%s %s{};\n", declName(decl), containerVar)
 	for _, field := range value.Fields {
 		fieldDecl, ok := decl.Field(field.Key.Name)
 		if !ok {
 			panic(fmt.Sprintf("field %s not found", field.Key.Name))
 		}
-		gidlmixer.Visit(b, field.Value, fieldDecl)
-		b.Builder.WriteString(fmt.Sprintf(
-			"%s.%s = std::move(%s);\n", containerVar, field.Key.Name, b.lastVar))
+		b.write(
+			"%s.%s = %s;\n", containerVar, field.Key.Name, b.visit(field.Value, fieldDecl))
 	}
+	var result string
 	if decl.IsNullable() {
 		alignedVar := b.newVar()
-		b.Builder.WriteString(fmt.Sprintf("fidl::aligned<%s> %s = std::move(%s);\n", typeNameIgnoreNullable(decl), alignedVar, containerVar))
+		b.write("fidl::aligned<%s> %s = std::move(%s);\n", typeNameIgnoreNullable(decl), alignedVar, containerVar)
 		unownedVar := b.newVar()
-		b.Builder.WriteString(fmt.Sprintf("%s %s = fidl::unowned_ptr(&%s);\n", typeName(decl), unownedVar, alignedVar))
-		b.lastVar = unownedVar
+		b.write("%s %s = fidl::unowned_ptr(&%s);\n", typeName(decl), unownedVar, alignedVar)
+		result = unownedVar
 	} else {
-		b.lastVar = containerVar
+		result = containerVar
 	}
+	return fmt.Sprintf("std::move(%s)", result)
 }
 
-func (b *llcppValueBuilder) OnTable(value gidlir.Record, decl *gidlmixer.TableDecl) {
+func (b *unownedBuilder) visitTable(value gidlir.Record, decl *gidlmixer.TableDecl) string {
 	frameVar := b.newVar()
 
-	b.Builder.WriteString(fmt.Sprintf(
-		"auto %s = %s::Frame();\n", frameVar, declName(decl)))
+	b.write(
+		"auto %s = %s::Frame();\n", frameVar, declName(decl))
 
 	builderVar := b.newVar()
 
-	b.Builder.WriteString(fmt.Sprintf(
-		"auto %s = %s::Builder(fidl::unowned_ptr(&%s));\n", builderVar, declName(decl), frameVar))
+	b.write(
+		"auto %s = %s::Builder(fidl::unowned_ptr(&%s));\n", builderVar, declName(decl), frameVar)
 
 	for _, field := range value.Fields {
 		if field.Key.IsUnknown() {
@@ -156,26 +136,25 @@ func (b *llcppValueBuilder) OnTable(value gidlir.Record, decl *gidlmixer.TableDe
 		if !ok {
 			panic(fmt.Sprintf("field %s not found", field.Key.Name))
 		}
-		gidlmixer.Visit(b, field.Value, fieldDecl)
-		fieldVar := b.lastVar
+		fieldVar := b.visit(field.Value, fieldDecl)
 		alignedVar := b.newVar()
-		b.Builder.WriteString(fmt.Sprintf("fidl::aligned<%s> %s = std::move(%s);\n", typeName(fieldDecl), alignedVar, fieldVar))
-		b.Builder.WriteString(fmt.Sprintf(
-			"%s.set_%s(fidl::unowned_ptr(&%s));\n", builderVar, field.Key.Name, alignedVar))
+		b.write("fidl::aligned<%s> %s = std::move(%s);\n", typeName(fieldDecl), alignedVar, fieldVar)
+		b.write(
+			"%s.set_%s(fidl::unowned_ptr(&%s));\n", builderVar, field.Key.Name, alignedVar)
 
 	}
 	tableVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf(
-		"auto %s = %s.build();\n", tableVar, builderVar))
+	b.write(
+		"auto %s = %s.build();\n", tableVar, builderVar)
 
-	b.lastVar = tableVar
+	return fmt.Sprintf("std::move(%s)", tableVar)
 }
 
-func (b *llcppValueBuilder) OnUnion(value gidlir.Record, decl *gidlmixer.UnionDecl) {
+func (b *unownedBuilder) visitUnion(value gidlir.Record, decl *gidlmixer.UnionDecl) string {
 	containerVar := b.newVar()
 
-	b.Builder.WriteString(fmt.Sprintf(
-		"%s %s;\n", declName(decl), containerVar))
+	b.write(
+		"%s %s;\n", declName(decl), containerVar)
 
 	for _, field := range value.Fields {
 		if field.Key.IsUnknown() {
@@ -185,64 +164,45 @@ func (b *llcppValueBuilder) OnUnion(value gidlir.Record, decl *gidlmixer.UnionDe
 		if !ok {
 			panic(fmt.Sprintf("field %s not found", field.Key.Name))
 		}
-		gidlmixer.Visit(b, field.Value, fieldDecl)
-		fieldVar := b.lastVar
+		fieldVar := b.visit(field.Value, fieldDecl)
 		alignedVar := b.newVar()
-		b.Builder.WriteString(fmt.Sprintf("fidl::aligned<%s> %s = std::move(%s);\n", typeName(fieldDecl), alignedVar, fieldVar))
-		b.Builder.WriteString(fmt.Sprintf(
-			"%s.set_%s(fidl::unowned_ptr(&%s));\n", containerVar, field.Key.Name, alignedVar))
+		b.write("fidl::aligned<%s> %s = %s;\n", typeName(fieldDecl), alignedVar, fieldVar)
+		b.write(
+			"%s.set_%s(fidl::unowned_ptr(&%s));\n", containerVar, field.Key.Name, alignedVar)
 	}
-	b.lastVar = containerVar
+	return fmt.Sprintf("std::move(%s)", containerVar)
 }
 
-func (b *llcppValueBuilder) buildListItems(value []interface{}, decl gidlmixer.ListDeclaration) []string {
+func (b *unownedBuilder) buildListItems(value []interface{}, decl gidlmixer.ListDeclaration) []string {
 	var elements []string
 	elemDecl := decl.Elem()
 	for _, item := range value {
-		switch item := item.(type) {
-		case int64:
-			elements = append(elements, int64String(item))
-		case uint64:
-			elements = append(elements, uint64String(item))
-		case float64:
-			elements = append(elements, float64String(item))
-		default:
-			gidlmixer.Visit(b, item, elemDecl)
-			elements = append(elements, fmt.Sprintf("std::move(%s)", b.lastVar))
-		}
+		elements = append(elements, fmt.Sprintf("std::move(%s)", b.visit(item, elemDecl)))
 	}
 	return elements
 }
 
-func (b *llcppValueBuilder) OnArray(value []interface{}, decl *gidlmixer.ArrayDecl) {
+func (b *unownedBuilder) visitArray(value []interface{}, decl *gidlmixer.ArrayDecl) string {
 	elements := b.buildListItems(value, decl)
 	sliceVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf("FIDL_ALIGNDECL auto %s = %s{%s};\n",
-		sliceVar, typeName(decl), strings.Join(elements, ", ")))
-	b.lastVar = sliceVar
+	b.write("FIDL_ALIGNDECL auto %s = %s{%s};\n",
+		sliceVar, typeName(decl), strings.Join(elements, ", "))
+	return sliceVar
 }
 
-func (b *llcppValueBuilder) OnVector(value []interface{}, decl *gidlmixer.VectorDecl) {
+func (b *unownedBuilder) visitVector(value []interface{}, decl *gidlmixer.VectorDecl) string {
 	if len(value) == 0 {
 		sliceVar := b.newVar()
-		b.Builder.WriteString(fmt.Sprintf("auto %s = %s();\n",
-			sliceVar, typeName(decl)))
-		b.lastVar = sliceVar
-		return
-
+		b.write("auto %s = %s();\n",
+			sliceVar, typeName(decl))
+		return sliceVar
 	}
 	elements := b.buildListItems(value, decl)
 	arrayVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf("auto %s = fidl::Array<%s, %d>{%s};\n",
-		arrayVar, typeName(decl.Elem()), len(elements), strings.Join(elements, ", ")))
+	b.write("auto %s = fidl::Array<%s, %d>{%s};\n",
+		arrayVar, typeName(decl.Elem()), len(elements), strings.Join(elements, ", "))
 	sliceVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf("auto %s = %s(fidl::unowned_ptr(%s.data()), %d);\n",
-		sliceVar, typeName(decl), arrayVar, len(elements)))
-	b.lastVar = sliceVar
-}
-
-func (b *llcppValueBuilder) OnNull(decl gidlmixer.Declaration) {
-	newVar := b.newVar()
-	b.Builder.WriteString(fmt.Sprintf("%s %s{};\n", typeName(decl), newVar))
-	b.lastVar = newVar
+	b.write("auto %s = %s(fidl::unowned_ptr(%s.data()), %d);\n",
+		sliceVar, typeName(decl), arrayVar, len(elements))
+	return fmt.Sprintf("std::move(%s)", sliceVar)
 }
