608683a4947c115959caeaba1f4baa3e22285e6c
Yegor Pomortsev <ypomortsev@google.com>
2020-05-19 02:52:15 +0000

    [modular] Add a unit test for (de)serializing SessionmgrConfig
    This tests that ModularConfigXdr correctly serializes SessionmgrConfig
    to JSON and back from JSON, preserving non-default values.
    
    Change-Id: Ie0a35f785e99e0db2ec97c85eab76eaeb87fc30f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/390622
    Reviewed-by: Gabe Schine <thatguy@google.com>
    Reviewed-by: Vickie Cheng <vickiecheng@google.com>
    Testability-Review: Gabe Schine <thatguy@google.com>
    Testability-Review: Vickie Cheng <vickiecheng@google.com>
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>

--- src/modular/lib/modular_config/modular_config_xdr_unittest.cc
+++ src/modular/lib/modular_config/modular_config_xdr_unittest.cc
@@ -120,6 +120,89 @@ TEST(ModularConfigXdr, SessionmgrDefaultValues) {
   EXPECT_EQ(0u, read_config.session_agents().size());
 }
 
+// Tests that values are set correctly for SessionmgrConfig when reading JSON and
+// that values in the JSON document are equal to those in SessionmgrConfig when writing JSON.
+// All of the fields are set to a non-default value.
+TEST(ModularConfigXdr, SessionmgrReadWriteValues) {
+  static constexpr auto kStartupAgentUrl =
+      "fuchsia-pkg://fuchsia.com/startup_agent#meta/startup_agent.cmx";
+  static constexpr auto kSessionAgentUrl =
+      "fuchsia-pkg://fuchsia.com/session_agent#meta/session_agent.cmx";
+  static constexpr auto kAgentServiceName = "fuchsia.modular.ModularConfigXdrTest";
+  static constexpr auto kAgentUrl = "fuchsia-pkg://example.com/test_agent#meta/test_agent.cmx";
+  static constexpr auto kAgentComponentArg = "--test_agent_component_arg";
+
+  static constexpr auto kExpectedJson = R"(
+    {
+      "cloud_provider": "NONE",
+      "enable_cobalt": false,
+      "enable_story_shell_preload": false,
+      "use_memfs_for_ledger": true,
+      "startup_agents": [
+        "fuchsia-pkg://fuchsia.com/startup_agent#meta/startup_agent.cmx"
+      ],
+      "session_agents": [
+        "fuchsia-pkg://fuchsia.com/session_agent#meta/session_agent.cmx"
+      ],
+      "component_args": [
+        {
+          "uri": "fuchsia-pkg://example.com/test_agent#meta/test_agent.cmx",
+          "args": ["--test_agent_component_arg"]
+        }
+      ],
+      "agent_service_index": [
+        {
+          "service_name": "fuchsia.modular.ModularConfigXdrTest",
+          "agent_url": "fuchsia-pkg://example.com/test_agent#meta/test_agent.cmx"
+        }
+      ]
+    })";
+  rapidjson::Document expected_json_doc;
+  expected_json_doc.Parse(kExpectedJson);
+
+  // Create a SessionmgrConfig with non-default values.
+  fuchsia::modular::session::SessionmgrConfig write_config;
+  write_config.set_cloud_provider(fuchsia::modular::session::CloudProvider::NONE);
+  write_config.set_enable_cobalt(false);
+  write_config.set_enable_story_shell_preload(false);
+  write_config.set_use_memfs_for_ledger(true);
+  write_config.mutable_startup_agents()->push_back(kStartupAgentUrl);
+  write_config.mutable_session_agents()->push_back(kSessionAgentUrl);
+  fuchsia::modular::session::AppConfig component_arg;
+  component_arg.set_url(kAgentUrl);
+  component_arg.mutable_args()->push_back(kAgentComponentArg);
+  write_config.mutable_component_args()->push_back(std::move(component_arg));
+  fuchsia::modular::session::AgentServiceIndexEntry agent_entry;
+  agent_entry.set_service_name(kAgentServiceName);
+  agent_entry.set_agent_url(kAgentUrl);
+  write_config.mutable_agent_service_index()->push_back(std::move(agent_entry));
+
+  // Serialize the config to JSON.
+  rapidjson::Document write_config_json_doc;
+  XdrWrite(&write_config_json_doc, &write_config, XdrSessionmgrConfig);
+
+  EXPECT_EQ(expected_json_doc, write_config_json_doc);
+
+  // Deserialize it from the expected JSON to a SessionmgrConfig.
+  fuchsia::modular::session::SessionmgrConfig read_config;
+  EXPECT_TRUE(XdrRead(&expected_json_doc, &read_config, XdrSessionmgrConfig));
+
+  EXPECT_EQ(fuchsia::modular::session::CloudProvider::NONE, read_config.cloud_provider());
+  EXPECT_FALSE(read_config.enable_cobalt());
+  EXPECT_FALSE(read_config.enable_story_shell_preload());
+  EXPECT_TRUE(read_config.use_memfs_for_ledger());
+
+  EXPECT_EQ(1u, read_config.startup_agents().size());
+  EXPECT_EQ(1u, read_config.session_agents().size());
+  EXPECT_EQ(kStartupAgentUrl, read_config.startup_agents().at(0));
+  EXPECT_EQ(kSessionAgentUrl, read_config.session_agents().at(0));
+  EXPECT_EQ(kAgentUrl, read_config.component_args().at(0).url());
+  ASSERT_EQ(1u, read_config.component_args().at(0).args().size());
+  EXPECT_EQ(kAgentComponentArg, read_config.component_args().at(0).args().at(0));
+  EXPECT_EQ(kAgentServiceName, read_config.agent_service_index().at(0).service_name());
+  EXPECT_EQ(kAgentUrl, read_config.agent_service_index().at(0).agent_url());
+}
+
 // Tests that the cloud provider field is read and written correctly when it's not set to the
 // default field.
 TEST(ModularConfigXdr, CloudProvider) {
