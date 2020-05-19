8b34fa3184259a6343662165ec9647431bc680ed
Ankit Dave <ankitdave@google.com>
2020-05-19 22:19:41 +0000

    [a11y] Cleanup GestureListenerRegistry test.
    This cl cleans up MockGestureListener from GestureListenerRegistry test.
    
    Testing:
      * Build
      * Unit Test:
        * fx run-code a11y_lib_tests
    
    Change-Id: Icb611e89253a530cd694cd1ebe778a1adda4996e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/390678
    Testability-Review: Eric Jia <ejia@google.com>
    Reviewed-by: Ian Gonzalez <iangonzalez@google.com>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Commit-Queue: Ankit Dave  <ankitdave@google.com>

--- src/ui/a11y/lib/gesture_manager/tests/gesture_listener_registry_unittest.cc
+++ src/ui/a11y/lib/gesture_manager/tests/gesture_listener_registry_unittest.cc
@@ -12,33 +12,11 @@
 #include <vector>
 
 #include "src/ui/a11y/lib/gesture_manager/recognizers/one_finger_n_tap_recognizer.h"
+#include "src/ui/a11y/lib/gesture_manager/tests/mocks/mock_gesture_listener.h"
 #include "src/ui/a11y/lib/testing/input.h"
 
 namespace accessibility_test {
 namespace {
-
-class MockGestureListener : public fuchsia::accessibility::gesture::Listener {
- public:
-  MockGestureListener() : binding_(this) {
-    binding_.set_error_handler([this](zx_status_t) { is_registered_ = false; });
-  }
-
-  fidl::InterfaceHandle<fuchsia::accessibility::gesture::Listener> NewBinding() {
-    is_registered_ = true;
-    return binding_.NewBinding();
-  }
-
-  bool is_registered() const { return is_registered_; }
-
- private:
-  void OnGesture(fuchsia::accessibility::gesture::Type gesture_type,
-                 OnGestureCallback callback) override {
-    callback(true, nullptr);
-  }
-
-  fidl::Binding<fuchsia::accessibility::gesture::Listener> binding_;
-  bool is_registered_ = false;
-};
 
 class GestureListenerRegistryTest : public gtest::TestLoopFixture {
  public:
@@ -50,15 +28,15 @@ class GestureListenerRegistryTest : public gtest::TestLoopFixture {
 
 TEST_F(GestureListenerRegistryTest, RegistersSuccessfully) {
   EXPECT_FALSE(registry_.listener());
-  MockGestureListener listener;
+  accessiblity_test::MockGestureListener listener;
   registry_.Register(listener.NewBinding(), []() {});
   EXPECT_TRUE(registry_.listener());
 }
 
 TEST_F(GestureListenerRegistryTest, HonorsLastRegisteredListener) {
   EXPECT_FALSE(registry_.listener());
-  MockGestureListener listener;
-  MockGestureListener last_listener;
+  accessiblity_test::MockGestureListener listener;
+  accessiblity_test::MockGestureListener last_listener;
   registry_.Register(listener.NewBinding(), []() {});
   registry_.Register(last_listener.NewBinding(), []() {});
   RunLoopUntilIdle();
