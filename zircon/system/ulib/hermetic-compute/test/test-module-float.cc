27773d30344fdf17739f49fd9d18d8996be4bf77
Adam Barth <abarth@google.com>
2020-05-21 21:44:24 +0000

    [hermetic] Remove hermetic-compute and hermetic-decompressor
    These libraries are no longer used.
    
    Change-Id: I34455d775310a51f12ef52080c0f0af1628a26ce
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/392135
    Testability-Review: Adam Barth <abarth@google.com>
    Commit-Queue: Adam Barth <abarth@google.com>
    Reviewed-by: P.Y. Laligand <pylaligand@google.com>

--- zircon/system/ulib/hermetic-compute/test/test-module-float.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include <lib/hermetic-compute/hermetic-engine.h>
-
-struct TestEngine : public HermeticComputeEngine<TestEngine, float, double, long double> {
-  int64_t operator()(float x, double y, long double z) { return static_cast<int64_t>(x + y + z); }
-};
