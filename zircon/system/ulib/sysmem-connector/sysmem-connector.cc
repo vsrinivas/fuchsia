dcee50b2c3dc940fe101d7a521c2d7ae60a516b3
John Bauman <jbauman@google.com>
2020-05-22 02:21:28 +0000

    [sysmem-connector] Remove logging
    We've debugged the hang, so this logging isn't necessary anymore.
    
    Change-Id: Iaa75afc7c4a8c8ea783873566437d9bdc8e55c45
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/392148
    Reviewed-by: David Reveman <reveman@google.com>
    Testability-Review: David Reveman <reveman@google.com>
    Commit-Queue: John Bauman <jbauman@google.com>

--- zircon/system/ulib/sysmem-connector/sysmem-connector.cc
+++ zircon/system/ulib/sysmem-connector/sysmem-connector.cc
@@ -77,8 +77,6 @@ class SysmemConnector : public sysmem_connector {
   fbl::unique_fd sysmem_dir_fd_;
   zx::channel driver_connector_client_;
 
-  bool logged_connection_request_ = false;
-  bool logged_connected_ = false;
   //
   // Synchronized using lock_.
   //
@@ -111,12 +109,6 @@ zx_status_t SysmemConnector::Start() {
 
 void SysmemConnector::QueueRequest(zx::channel allocator_request) {
   ZX_DEBUG_ASSERT(thrd_current() != process_queue_thrd_);
-  if (!logged_connection_request_) {
-    // TODO(fxb/50876): Remove once hang has been debugged.
-    printf("sysmem-connector: Got first QueueRequest\n");
-    fflush(stdout);
-    logged_connection_request_ = true;
-  }
   bool trigger_needed;
   {  // scope lock
     fbl::AutoLock lock(&lock_);
@@ -245,11 +237,6 @@ void SysmemConnector::ProcessQueue() {
       connection_requests_.pop();
     }  // ~lock
     ZX_DEBUG_ASSERT(allocator_request);
-    if (!logged_connected_) {
-      // TODO(fxb/50876): Remove once hang has been debugged.
-      printf("sysmem-connector: Executing first QueueRequest\n");
-      fflush(stdout);
-    }
 
     // Poll for PEER_CLOSED just before we need the channel to be usable, to
     // avoid routing a request to a stale no-longer-usable sysmem device
@@ -312,12 +299,6 @@ void SysmemConnector::ProcessQueue() {
       // the next request will try ConnectToSysmemDriver() again.
       //
       // continue with next request
-    }
-    if (!logged_connected_) {
-      // TODO(fxb/50876): Remove once hang has been debugged.
-      printf("sysmem-connector: Finished first QueueRequest\n");
-      fflush(stdout);
-      logged_connected_ = true;
     }
   }
 }
