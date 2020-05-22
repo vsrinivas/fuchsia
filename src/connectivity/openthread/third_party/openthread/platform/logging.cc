0f581ea2de1efe01c2ab2885970dac87304fb0ae
Parag Dixit <paragdixit@google.com>
2020-05-22 05:30:54 +0000

    [connectivity][openthread] Changes in logging.cc to print to Fuchsia logs
      Bug: 52103
      Tests: (Logging changes exempt from testability)
      	Tested locally by calling otPlatLog at different ot log levels
    
    Change-Id: I0247466fe4db181beb43c6c1b240c5c5c1b82104
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/389455
    Reviewed-by: Jiaming (Charlie) Wang <jiamingw@google.com>
    Reviewed-by: Vaas Krishnamurthy <vaas@google.com>
    Testability-Review: Jiaming (Charlie) Wang <jiamingw@google.com>
    Commit-Queue: Parag Dixit <paragdixit@google.com>

--- src/connectivity/openthread/third_party/openthread/platform/logging.cc
+++ src/connectivity/openthread/third_party/openthread/platform/logging.cc
@@ -26,6 +26,59 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <openthread/platform/logging.h>
+#include <lib/syslog/cpp/macros.h>
 
-void otPlatLog(otLogLevel a_log_level, otLogRegion a_log_region, const char *a_format, ...) {}
+#include <openthread/platform/logging.h>
+#include <openthread/platform/toolchain.h>
+
+void otPlatLog(otLogLevel log_level, otLogRegion log_region, const char *format, ...) {
+  OT_UNUSED_VARIABLE(log_region);
+
+  va_list args;
+  va_start(args, format);
+
+  // Use vsnprintf to get size of buffer required to print formatted string
+  // (excluding null terminator)
+  size_t buffer_bytes = vsnprintf(nullptr, 0, format, args);
+  FX_CHECK(buffer_bytes >= 0) << "Error in vsnprintf: " << errno;
+
+  constexpr size_t null_terminator_bytes = 1;
+  buffer_bytes += null_terminator_bytes;
+
+  std::unique_ptr<char[]> buffer(new char[buffer_bytes]);
+
+  // Now create the string which we want to send to log
+  size_t buffer_bytes_2 = vsnprintf(buffer.get(), buffer_bytes, format, args) + 1;
+  // sanity check; should match so go ahead and assert that it does.
+  FX_CHECK(buffer_bytes == buffer_bytes_2)
+      << "Error in vsnprintf or size not equal: " << buffer_bytes << " != " << buffer_bytes_2
+      << ", errno = " << errno;
+  va_end(args);
+
+  // Now print the string to appropriate FX_LOG
+  switch (log_level) {
+    case OT_LOG_LEVEL_NONE:
+      FX_LOGS(FATAL) << buffer.get() << std::endl;
+      break;
+
+    case OT_LOG_LEVEL_CRIT:
+      FX_LOGS(ERROR) << buffer.get() << std::endl;
+      break;
+
+    case OT_LOG_LEVEL_WARN:
+      FX_LOGS(WARNING) << buffer.get() << std::endl;
+      break;
+
+    case OT_LOG_LEVEL_NOTE:
+    case OT_LOG_LEVEL_INFO:
+      FX_LOGS(INFO) << buffer.get() << std::endl;
+      break;
+
+    case OT_LOG_LEVEL_DEBG:
+      FX_LOGS(DEBUG) << buffer.get() << std::endl;
+      break;
+
+    default:
+      FX_CHECK(0) << "Unknown log_level";
+  }
+}
