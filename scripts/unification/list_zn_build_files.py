608619e53088987429aad91d7dab215aff37f863
P.Y. Laligand <pylaligand@google.com>
2020-05-22 00:23:30 +0000

    [unification] Add the ability to list file paths instead of labels.
    Bug: 52709
    Change-Id: If95b52b73b8afc986e42f028d78a744438daea82
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/392257
    Reviewed-by: Shai Barack <shayba@google.com>
    Testability-Review: Shai Barack <shayba@google.com>
    Commit-Queue: P.Y. Laligand <pylaligand@google.com>

--- scripts/unification/list_zn_build_files.py
+++ scripts/unification/list_zn_build_files.py
@@ -16,13 +16,19 @@ def main():
     parser.add_argument('--build-dir',
                         help='Path to the ZN build\'s output directory',
                         default=os.path.join(FUCHSIA_ROOT, 'out', 'default.zircon'))
+    parser.add_argument('--files',
+                        help='If set, print paths to build files instead of labels',
+                        action='store_true')
     args = parser.parse_args()
 
     with open(os.path.join(args.build_dir, 'build.ninja.d'), 'r') as ninja_file:
         content = ninja_file.readlines()[0]
     files = content.split()
-    files = [f.replace('../../zircon', '/').replace('/BUILD.gn', '')
-             for f in files if f.endswith('BUILD.gn')]
+    files = [f for f in files if f.endswith('BUILD.gn')]
+    if args.files:
+        files = [f.replace('../..', FUCHSIA_ROOT) for f in files]
+    else:
+        files = [f.replace('../../zircon', '/').replace('/BUILD.gn', '') for f in files]
     for file in sorted(files):
         print(file)
 
