e7b48b2e144a65be4049ed9254d41328291f5178
Chris Suter <csuter@google.com>
2020-05-25 22:49:12 +0000

    [storage] Pass through command-line for trampoline test runners.
    Change-Id: I08660b159a7d5456b19d84308d26c21eb3c1c718
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/392495
    Reviewed-by: Brett Wilson <brettw@google.com>
    Testability-Review: Brett Wilson <brettw@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>

--- zircon/system/ulib/blobfs/test/integration/blobfs_integration_chunk_compressed_paged_main.cc
+++ zircon/system/ulib/blobfs/test/integration/blobfs_integration_chunk_compressed_paged_main.cc
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <vector>
+
 #include "launch.h"
 
-int main() {
+int main(int argc, const char* argv[]) {
   // This is a temporary solution until we can support tests as packages on the bringup target,
   // which the blobfs-integration-test runs on. There is no good alternative currently to run
   // blobfs-integration-test with the additional --pager commandline arg.
@@ -12,8 +14,13 @@ int main() {
   // TODO(ZX-48961):
   // We might not need this test at all in the future, when we have paging enabled by default on all
   // platforms. The tests will then have the pager enabled by default as well.
-  const char* argv[] = {"/boot/test/fs/blobfs-integration-test", "--pager", "--compression",
-    "ZSTD_CHUNKED", nullptr};
 
-  return Execute(argv);
+  std::vector<const char*> args = {"/boot/test/fs/blobfs-integration-test", "--pager",
+                                   "--compression", "ZSTD_CHUNKED"};
+  for (int i = 1; i < argc; ++i) {
+    args.push_back(argv[i]);
+  }
+  args.push_back(nullptr);
+
+  return Execute(args.data());
 }
