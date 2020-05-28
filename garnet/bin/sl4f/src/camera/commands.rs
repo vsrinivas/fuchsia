5e81b4a574d3836176fff89a1b4f2c3c4e7ee37b
Nic Zoghb <nzo@google.com>
2020-05-28 04:57:43 +0000

    [camera][factory][sl4f] Impl facade method: detect
    * Adds relevant FIDL services
    * Implements the facade's detect() call
    * Adds a test to exercise the detect() hermetically (not end-to-end)
    * Adds test methods to enable checking against expected results from FIDL calls easily
    
    Bug: 41671
    Fix: 42847
    
    Test: fx test sl4f-service-tests for facade;
    https://android-review.googlesource.com/c/platform/tools/test/connectivity/+/1315494
    for end-to-end tests.
    
    Change-Id: I22a93cdaf9d9d8de794542bbbc8f6a39269893eb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/390914
    Commit-Queue: Nic Zoghb <nzo@google.com>
    Testability-Review: Tom Turney <tturney@google.com>
    Reviewed-by: Tom Turney <tturney@google.com>

--- garnet/bin/sl4f/src/camera/commands.rs
+++ garnet/bin/sl4f/src/camera/commands.rs
@@ -4,46 +4,55 @@
 
 use anyhow::Error;
 use async_trait::async_trait;
+use serde::Serialize;
 use serde_json::{from_value, to_value, Value};
 
 use crate::camera::facade::CameraFacade;
 use crate::camera::types::SetCfgRequest;
 use crate::server::Facade;
 
+fn wrap<T>(x: T) -> Result<Value, Error>
+where
+    T: Serialize,
+{
+    Ok(to_value(x)?)
+}
+
 #[async_trait(?Send)]
 impl Facade for CameraFacade {
     async fn handle_request(&self, method: String, args: Value) -> Result<Value, Error> {
-        let result = match method.as_ref() {
-            "DetectCamera" => self.detect().await?,
-            "GetSn" => self.get_sn().await?,
-            "GetCfg" => self.get_cfg().await?,
+        match method.as_ref() {
+            "Detect" => wrap(self.detect().await?),
+            "GetSn" => wrap(self.get_sn().await?),
+            "GetCfg" => wrap(self.get_cfg().await?),
             "SetCfg" => {
                 let req: SetCfgRequest = from_value(args)?;
-                self.set_cfg(req.mode, req.integration_time, req.analog_gain, req.digital_gain)
-                    .await?
+                wrap(
+                    self.set_cfg(req.mode, req.integration_time, req.analog_gain, req.digital_gain)
+                        .await?,
+                )
             }
             "Capture" => {
                 let req: String = from_value(args)?;
-                self.capture(req).await?
+                wrap(self.capture(req).await?)
             }
             "GetOtp" => {
                 let req: String = from_value(args)?;
-                self.get_otp(req).await?
+                wrap(self.get_otp(req).await?)
             }
             "ColorBars" => {
                 let req: bool = from_value(args)?;
-                self.color_bars(req).await?
+                wrap(self.color_bars(req).await?)
             }
             "Enable" => {
                 let req: bool = from_value(args)?;
-                self.enable(req).await?
+                wrap(self.enable(req).await?)
             }
             "Reset" => {
                 let req: bool = from_value(args)?;
-                self.reset(req).await?
+                wrap(self.reset(req).await?)
             }
             _ => return Err(format_err!("invalid method: {}", method)),
-        };
-        Ok(to_value(result)?)
+        }
     }
 }
--- garnet/bin/sl4f/src/camera/facade.rs
+++ garnet/bin/sl4f/src/camera/facade.rs
@@ -3,27 +3,80 @@
 // found in the LICENSE file.
 
 use anyhow::Error;
+use fidl_fuchsia_camera2_hal::{ControllerMarker, ControllerProxy};
+use fidl_fuchsia_hardware_camera::DeviceProxy;
+use fuchsia_async as fasync;
 use fuchsia_syslog::fx_log_err;
+use fuchsia_zircon as zx;
+use parking_lot::{RwLock, RwLockUpgradableReadGuard};
 use serde_json::{to_value, Value};
+
+use crate::camera::types::DetectResult;
+use crate::common_utils::common::macros::{fx_err_and_bail, with_line};
+
+// TODO(41671): Don't hardcode values; use glob for path. Especially for multiple cameras.
+const CAMERA_ID: i32 = 0;
+const CAMERA_PATH: &str = "/dev/class/camera/000";
 
 /// Facade that exposes various Camera interfaces.
 #[derive(Debug)]
-pub struct CameraFacade {}
+pub struct CameraFacade {
+    hal_svc: RwLock<Option<ControllerProxy>>,
+}
 
 impl CameraFacade {
-    /// Instantiate a Facade.
+    /// Instantiate the facade.
     pub fn new() -> Self {
-        CameraFacade {}
+        CameraFacade { hal_svc: RwLock::new(None) }
+    }
+
+    /// Test-only method to manually inject pre-defined services into the facade.
+    #[cfg(test)]
+    fn new_with_svc(hal_proxy: Option<ControllerProxy>) -> Self {
+        Self { hal_svc: RwLock::new(hal_proxy) }
+    }
+
+    /// Helper method to connect to the Camera HAL service.
+    fn get_hal_svc(&self, tag: &str) -> Result<ControllerProxy, Error> {
+        let lock = self.hal_svc.upgradable_read();
+        match lock.as_ref() {
+            Some(svc) => Ok(svc.clone()),
+            None => {
+                let (client, server) = zx::Channel::create()?;
+                fdio::service_connect(CAMERA_PATH, server)?;
+
+                let (controller_client, controller_server) =
+                    fidl::endpoints::create_proxy::<ControllerMarker>()?;
+
+                match DeviceProxy::new(fasync::Channel::from_channel(client)?)
+                    .get_channel2(controller_server.into_channel())
+                {
+                    Ok(_) => {
+                        *RwLockUpgradableReadGuard::upgrade(lock) = Some(controller_client.clone());
+                        Ok(controller_client)
+                    }
+                    Err(e) => fx_err_and_bail!(
+                        &with_line!(tag),
+                        format_err!("Couldn't call GetChannel2(); {}.", e)
+                    ),
+                }
+            }
+        }
     }
 
     /// Checks whether the device under test has a camera and provides information about
     /// it. If the device has multiple cameras, the first one listed is chosen.
     ///
     /// Takes no arguments. Outputs the serialized DeviceInfo in a DetectResult struct.
-    pub async fn detect(&self) -> Result<Value, Error> {
+    pub async fn detect(&self) -> Result<DetectResult, Error> {
         const TAG: &str = "CameraFacade::detect";
-        fx_log_err!("{} {}", TAG, "NOT IMEPLEMENTED");
-        Ok(to_value(())?)
+        let svc = self.get_hal_svc(TAG)?;
+        match svc.get_device_info().await {
+            Ok(r) => Ok(DetectResult { camera_id: CAMERA_ID, camera_info: r }),
+            Err(e) => {
+                fx_err_and_bail!(&with_line!(TAG), format_err!("Couldn't get DeviceInfo; {}.", e))
+            }
+        }
     }
 
     /// Retrieves the sensor's serial number.
@@ -131,13 +184,82 @@ impl CameraFacade {
 
 #[cfg(test)]
 mod tests {
-    // TODO(42847): end-to-end tests once FIDL service is linked up.
     use super::*;
-    use fuchsia_async as fasync;
+    use fidl_fuchsia_camera2::{DeviceInfo, DeviceType};
+    use fidl_fuchsia_camera2_hal::ControllerRequest;
+    use futures::{future::Future, join, stream::StreamExt};
+
+    struct MockCameraFactoryBuilder {
+        expected: Vec<Box<dyn FnOnce(ControllerRequest) + Send + 'static>>,
+    }
+
+    impl MockCameraFactoryBuilder {
+        fn new() -> Self {
+            Self { expected: vec![] }
+        }
+
+        fn build(self) -> (CameraFacade, impl Future<Output = ()>) {
+            let (proxy, mut stream) =
+                fidl::endpoints::create_proxy_and_stream::<ControllerMarker>().unwrap();
+            let fut = async move {
+                for expected in self.expected {
+                    expected(stream.next().await.unwrap().unwrap());
+                }
+                if matches!(stream.next().await, None) {
+                } else {
+                    panic!("Not what was expected.")
+                };
+            };
+
+            (CameraFacade::new_with_svc(Some(proxy)), fut)
+        }
+
+        fn push(mut self, request: impl FnOnce(ControllerRequest) + Send + 'static) -> Self {
+            self.expected.push(Box::new(request));
+            self
+        }
+
+        fn expect_get_device_info(self, device_info: DeviceInfo) -> Self {
+            self.push(move |req| match req {
+                ControllerRequest::GetDeviceInfo { responder } => {
+                    responder.send(device_info).unwrap()
+                }
+                _ => panic!("Unexpected request."),
+            })
+        }
+    }
 
     #[fasync::run_singlethreaded(test)]
-    async fn sanity() -> Result<(), Error> {
-        let _facade = CameraFacade::new();
-        Ok(())
+    async fn detect_calls_get_device_info_fidl() {
+        const TEST_UINT16: u16 = 256;
+        const TEST_STR: &str = "test_string";
+
+        let mock_device_info = DeviceInfo {
+            vendor_id: Some(TEST_UINT16),
+            vendor_name: Some(TEST_STR.to_string()),
+            product_id: Some(TEST_UINT16),
+            product_name: Some(TEST_STR.to_string()),
+            type_: Some(DeviceType::Virtual),
+        };
+
+        let (facade, fut) =
+            MockCameraFactoryBuilder::new().expect_get_device_info(mock_device_info).build();
+        let test = async move {
+            assert_eq!(
+                facade.detect().await.unwrap(),
+                DetectResult {
+                    camera_id: 0,
+                    camera_info: DeviceInfo {
+                        vendor_id: Some(TEST_UINT16),
+                        vendor_name: Some(TEST_STR.to_string()),
+                        product_id: Some(TEST_UINT16),
+                        product_name: Some(TEST_STR.to_string()),
+                        type_: Some(DeviceType::Virtual),
+                    },
+                }
+            );
+        };
+
+        join!(fut, test);
     }
 }
