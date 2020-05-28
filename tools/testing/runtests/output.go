3e4595246e8c68fcf42ac5cf0cf0fced21b15ee4
Joshua Seaton <joshuaseaton@google.com>
2020-05-28 06:26:59 +0000

    [tools][runtests] Add logic to derive test result from output
    This will be used to derive the result of a test run over serial.
    
    Bug: 41930
    Change-Id: I7ef3a9fea066eeae1d71d66187f9707f9a02bab6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/384013
    Testability-Review: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Gary Miguel <garymm@google.com>

---
+++ tools/testing/runtests/output.go
@@ -0,0 +1,35 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package runtests
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"regexp"
+
+	"go.fuchsia.dev/fuchsia/tools/lib/iomisc"
+)
+
+const (
+	runtestsSuccess = "[runtests][PASSED]"
+	runtestsFailure = "[runtests][FAILED]"
+)
+
+var (
+	resultSignatureRE = regexp.MustCompile(fmt.Sprintf("(%s|%s)", regexp.QuoteMeta(runtestsSuccess), regexp.QuoteMeta(runtestsFailure)))
+	resultMatchLength = len(runtestsSuccess) // Assumes len(runtestsSuccess) == len(runtestsFailure)
+)
+
+// TestPassed reads in the output from a runtests invocation of a single test
+// and returns whether that test succeeded.
+func TestPassed(ctx context.Context, testOutput io.Reader) (bool, error) {
+	m := iomisc.NewPatternMatchingReader(testOutput, resultSignatureRE, resultMatchLength)
+	match, err := iomisc.ReadUntilMatch(ctx, m, nil)
+	if err != nil {
+		return false, fmt.Errorf("unable to derive test result from runtests output: %w", err)
+	}
+	return string(match) == runtestsSuccess, nil
+}
