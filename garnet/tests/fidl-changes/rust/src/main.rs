dd2d9b68ae7000a4f0ded0d36b2747e7cdce420c
Mitchell Kember <mkember@google.com>
2020-05-29 18:53:29 +0000

    [fidl][rust][transitional] Update docs and test Rust ProxyInterface
    This CL updates the fidl-changes README to distinguish between two kinds
    of soft transition: FIDL-assisted and source-assisted. It details the
    states that must be tested for each kind of transition.
    
    It also reorganizes the Rust tests to supported testing both kinds
    (previously they only tested source-assisted transitions), and refactors
    the code to use modules rather than passing unique names into macros.
    
    Finally, it adds tests for Ifa1b10149e583401f20e1eefcda99443f554961b,
    which made the Rust bindings emit default implementations in
    ProxyInterface for methods marked [Transitional]. Testing this
    FIDL-assisted transition prompted all the other changes.
    
    Test: fx build garnet/tests/fidl-changes/rust
    Bug: 52607
    Change-Id: I3ba0b29564fb436ffb4c6f6bf7796e4e88d00964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/393194
    Commit-Queue: Mitchell Kember <mkember@google.com>
    Reviewed-by: Pascal Perez <pascallouis@google.com>
    Testability-Review: Mitchell Kember <mkember@google.com>

--- garnet/tests/fidl-changes/rust/src/main.rs
+++ garnet/tests/fidl-changes/rust/src/main.rs
@@ -2,31 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![allow(dead_code)]
+mod fidl_assisted;
+mod source_assisted;
 
-use fidl::endpoints::RequestStream;
-use fidl_fidl_test_after as after_fidl;
-use fidl_fidl_test_before as before_fidl;
-use fidl_fidl_test_during as during_fidl;
-use fuchsia_async as fasync;
-use futures::prelude::*;
-
-#[macro_use]
-mod before;
-#[macro_use]
-mod during;
-#[macro_use]
-mod after;
-
-// Test that the original code compiles against the original library.
-before_impl!(BeforeBefore, before_fidl);
-// Test that the pre-flighted code compiles against the original library.
-during_impl!(DuringBefore, before_fidl);
-// Test that the pre-flighted code compiles against the transitional library.
-during_impl!(DuringDuring, during_fidl);
-// Test that the pre-flighted code compiles against the final library.
-during_impl!(DuringAfter, after_fidl);
-// Test that the final code compiles against the final library.
-after_impl!(AfterAfter, after_fidl);
-
-fn main() {}
+fn main() {
+    panic!("This should only be compiled, not executed.")
+}
