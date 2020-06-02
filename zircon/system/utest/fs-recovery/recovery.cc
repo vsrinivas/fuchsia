ea003381b885e2666b8154af15ed116bbc31122d
Brett Wilson <brettw@google.com>
2020-06-02 16:41:34 +0000

    [storage] Convert fs-recovery to zxtest.
    Converts the fx-recovery test from the old Zircon test fixture to
    zxtest. Use std::string and avoid sprintf-ing into fixed buffers.
    
    Increases the test timeouts. The old timeouts were too short for running
    in an emulator so this also allows the test to pass in more cases.
    
    There should be no behavior change except some of the "failed" cases
    will be reported differently now because of the difference between how
    the test libraries handle subroutines.
    
    Change-Id: Ic6ef936e93ffe3921c1bcbfc1d904cadbaf761dc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/394395
    Testability-Review: Brett Wilson <brettw@google.com>
    Commit-Queue: Brett Wilson <brettw@google.com>
    Reviewed-by: Vikram Auradkar <auradkar@google.com>

--- zircon/system/utest/fs-recovery/recovery.cc
+++ zircon/system/utest/fs-recovery/recovery.cc
@@ -4,9 +4,9 @@
 
 #include <fcntl.h>
 #include <lib/devmgr-integration-test/fixture.h>
+#include <lib/fdio/cpp/caller.h>
 #include <lib/fdio/fd.h>
 #include <lib/fdio/fdio.h>
-#include <lib/fdio/cpp/caller.h>
 #include <lib/zx/vmo.h>
 
 #include <memory>
@@ -15,40 +15,34 @@
 #include <fs-management/mount.h>
 #include <fvm/format.h>
 #include <ramdevice-client/ramdisk.h>
-#include <unittest/unittest.h>
+#include <zxtest/zxtest.h>
 
 namespace {
 
 using devmgr_integration_test::IsolatedDevmgr;
 
-const uint32_t kBlockCount = 1024 * 256;
-const uint32_t kBlockSize = 512;
-const uint32_t kSliceSize = (1 << 20);
-const size_t kDeviceSize = kBlockCount * kBlockSize;
+constexpr uint32_t kBlockCount = 1024 * 256;
+constexpr uint32_t kBlockSize = 512;
+constexpr uint32_t kSliceSize = (1 << 20);
+constexpr size_t kDeviceSize = kBlockCount * kBlockSize;
 const char* kDataName = "fs-recovery-data";
 const char* kRamdiskPath = "misc/ramctl";
 
 // Test fixture that builds a ramdisk and destroys it when destructed.
-class FsRecoveryTest {
+class FsRecoveryTest : public zxtest::Test {
  public:
-  // Create an IsolatedDevmgr that can load device drivers such as fvm,
-  // zxcrypt, etc.
-  bool Initialize() {
-    BEGIN_HELPER;
+  // Create an IsolatedDevmgr that can load device drivers such as fvm, zxcrypt, etc.
+  zx_status_t Initialize() {
     auto args = IsolatedDevmgr::DefaultArgs();
     args.disable_block_watcher = false;
     args.sys_device_driver = devmgr_integration_test::IsolatedDevmgr::kSysdevDriver;
     args.load_drivers.push_back(devmgr_integration_test::IsolatedDevmgr::kSysdevDriver);
     args.driver_search_paths.push_back("/boot/driver");
-    ASSERT_EQ(IsolatedDevmgr::Create(std::move(args), &devmgr_), ZX_OK);
-    END_HELPER;
+    return IsolatedDevmgr::Create(std::move(args), &devmgr_);
   }
 
-  // Create a ram disk that is back by a VMO, which is formatted to look like
-  // an FVM volume.
-  bool CreateFvmRamdisk(size_t device_size, size_t block_size) {
-    BEGIN_HELPER;
-
+  // Create a ram disk that is back by a VMO, which is formatted to look like an FVM volume.
+  void CreateFvmRamdisk(size_t device_size, size_t block_size) {
     // Calculate total size of data + metadata.
     device_size = fbl::round_up(device_size, fvm::kBlockSize);
     size_t old_meta = fvm::MetadataSize(device_size, fvm::kBlockSize);
@@ -66,25 +60,20 @@ class FsRecoveryTest {
     ASSERT_GE(fd, 0);
     ASSERT_EQ(fvm_init_with_size(fd, device_size, kSliceSize), ZX_OK);
 
-    fbl::unique_fd ramdisk;
-    ASSERT_TRUE(WaitForDevice(kRamdiskPath, &ramdisk));
+    fbl::unique_fd ramdisk = WaitForDevice(kRamdiskPath);
+    ASSERT_TRUE(ramdisk);
 
-    ASSERT_EQ(ramdisk_create_at_from_vmo(devmgr_.devfs_root().get(), disk.get(), &ramdisk_client_),
-              ZX_OK);
-    END_HELPER;
+    ASSERT_OK(ramdisk_create_at_from_vmo(devmgr_.devfs_root().get(), disk.get(), &ramdisk_client_));
   }
 
-  // Create a partition in the FVM volume that has the data guid.
-  bool CreateFvmPartition(char* fvm_block_path) {
-    BEGIN_HELPER;
+  // Create a partition in the FVM volume that has the data guid. Returns the path to the FVM block
+  // device.
+  std::string CreateFvmPartition() {
+    std::string fvm_path = std::string(ramdisk_get_path(ramdisk_client_)) + "/fvm";
+    fbl::unique_fd fvm_fd = WaitForDevice(fvm_path);
+    EXPECT_TRUE(fvm_fd);
 
-    char fvm_path[PATH_MAX];
-    snprintf(fvm_path, PATH_MAX, "%s/fvm", ramdisk_get_path(ramdisk_client_));
-    fbl::unique_fd fvm_fd;
-    ASSERT_TRUE(WaitForDevice(fvm_path, &fvm_fd));
-
-    // Allocate a FVM partition with the data guid but don't actually format the
-    // partition.
+    // Allocate a FVM partition with the data guid but don't actually format the partition.
     alloc_req_t req;
     memset(&req, 0, sizeof(alloc_req_t));
     req.slice_count = 1;
@@ -99,30 +88,29 @@ class FsRecoveryTest {
 
     fdio_cpp::UnownedFdioCaller caller(fvm_fd.get());
     zx_status_t status;
-    ASSERT_EQ(fuchsia_hardware_block_volume_VolumeManagerAllocatePartition(
-                  caller.borrow_channel(), req.slice_count, &type_guid, &instance_guid, req.name,
-                  BLOCK_NAME_LEN, req.flags, &status),
-              ZX_OK);
-    ASSERT_EQ(status, ZX_OK);
+    EXPECT_OK(fuchsia_hardware_block_volume_VolumeManagerAllocatePartition(
+        caller.borrow_channel(), req.slice_count, &type_guid, &instance_guid, req.name,
+        BLOCK_NAME_LEN, req.flags, &status));
+    EXPECT_OK(status);
 
-    snprintf(fvm_block_path, PATH_MAX, "%s/%s-p-1/block", fvm_path, kDataName);
-    fbl::unique_fd fvm_block_fd;
-    ASSERT_TRUE(WaitForDevice(fvm_block_path, &fvm_block_fd));
+    std::string fvm_block_path = fvm_path + "/" + kDataName + "-p-1/block";
+    fbl::unique_fd fvm_block_fd = WaitForDevice(fvm_block_path);
+    EXPECT_TRUE(fvm_block_fd);
 
-    END_HELPER;
+    return fvm_block_path;
   }
 
-  // Wait for the device to be available and then check to make sure it is
-  // formatted of the passed in type. Since formatting can take some time
-  // after the device becomes available, we must recheck.
-  bool WaitForDiskFormat(const char* path, disk_format_t format, zx::duration deadline) {
-    fbl::unique_fd fd;
-    if (!WaitForDevice(path, &fd)) {
+  // Wait for the device to be available and then check to make sure it is formatted of the passed
+  // in type. Since formatting can take some time after the device becomes available, we must
+  // recheck.
+  bool WaitForDiskFormat(const std::string& path, disk_format_t format, zx::duration deadline) {
+    fbl::unique_fd fd = WaitForDevice(path);
+    if (!fd) {
       return false;
     }
     const zx::time absolute_deadline = zx::deadline_after(deadline);
     for (;;) {
-      fd.reset(openat(devmgr_.devfs_root().get(), path, O_RDONLY));
+      fd.reset(openat(devmgr_.devfs_root().get(), path.c_str(), O_RDONLY));
       if (detect_disk_format(fd.get()) == format) {
         return true;
       }
@@ -136,47 +124,34 @@ class FsRecoveryTest {
   }
 
  private:
-  bool WaitForDevice(const char* path, fbl::unique_fd* fd) {
-    BEGIN_HELPER;
-    fprintf(stderr, "Wait for device %s\n", path);
-    ASSERT_EQ(devmgr_integration_test::RecursiveWaitForFile(devmgr_.devfs_root(), path, fd), ZX_OK);
-
-    ASSERT_TRUE(*fd);
-    END_HELPER;
+  fbl::unique_fd WaitForDevice(const std::string& path) {
+    fbl::unique_fd result;
+    EXPECT_OK(
+        devmgr_integration_test::RecursiveWaitForFile(devmgr_.devfs_root(), path.c_str(), &result));
+    return result;
   }
 
-  ramdisk_client_t* ramdisk_client_;
+  ramdisk_client_t* ramdisk_client_ = nullptr;
   devmgr_integration_test::IsolatedDevmgr devmgr_;
 };
 
-bool EmptyPartitionRecoveryTest() {
-  BEGIN_TEST;
+TEST_F(FsRecoveryTest, EmptyPartitionRecoveryTest) {
+  ASSERT_OK(Initialize());
 
-  char fvm_block_path[PATH_MAX];
-  std::unique_ptr<FsRecoveryTest> recovery(new FsRecoveryTest());
-  ASSERT_TRUE(recovery->Initialize());
-  // Creates an FVM partition under an isolated devmgr. It creates, but does
-  // not properly format the data partition.
-  ASSERT_TRUE(recovery->CreateFvmRamdisk(kDeviceSize, kBlockSize));
-  ASSERT_TRUE(recovery->CreateFvmPartition(fvm_block_path));
+  // Creates an FVM partition under an isolated devmgr. It creates, but does not properly format the
+  // data partition.
+  CreateFvmRamdisk(kDeviceSize, kBlockSize);
+  std::string fvm_block_path = CreateFvmPartition();
 
-  // We then expect the devmgr to self-recover, i.e., format the zxcrypt/data
-  // partitions as expected from the FVM partition.
+  // We then expect the devmgr to self-recover, i.e., format the zxcrypt/data partitions as expected
+  // from the FVM partition.
 
   // First, wait for the zxcrypt partition to be formatted.
-  EXPECT_TRUE(
-      recovery->WaitForDiskFormat(fvm_block_path, DISK_FORMAT_ZXCRYPT, zx::duration(zx::sec(3))));
+  EXPECT_TRUE(WaitForDiskFormat(fvm_block_path, DISK_FORMAT_ZXCRYPT, zx::duration(zx::sec(100))));
 
   // Second, wait for the data partition to be formatted.
-  char data_path[PATH_MAX];
-  snprintf(data_path, sizeof(data_path), "%s/zxcrypt/unsealed/block", fvm_block_path);
-  EXPECT_TRUE(recovery->WaitForDiskFormat(data_path, DISK_FORMAT_MINFS, zx::duration(zx::sec(10))));
-
-  END_TEST;
+  std::string data_path = fvm_block_path + "/zxcrypt/unsealed/block";
+  EXPECT_TRUE(WaitForDiskFormat(data_path, DISK_FORMAT_MINFS, zx::duration(zx::sec(100))));
 }
-
-BEGIN_TEST_CASE(FsRecoveryTest)
-RUN_TEST(EmptyPartitionRecoveryTest)
-END_TEST_CASE(FsRecoveryTest)
 
 }  // namespace
