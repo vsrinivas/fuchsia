b536ffedf9ef1923b38afc9df06d0cc7f2b710fa
P.Y. Laligand <pylaligand@google.com>
2020-06-03 21:14:41 +0000

    [unification] Introduce a template that collects metadata about distributable objects.
    This should become the means through which build steps generating collections
    of runtime objects should acquire information about said objects.
    Convert fini_manifest to use the new dist_manifest under the hood.
    
    Switched these templates away from "output_dir" / "output_name" / "output_extension" and
    instead using an "outputs" singleton list.
    
    Bug: 45680
    Change-Id: I3ca1e0cec79035020c914088c22232afb0185871
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/394798
    Commit-Queue: P.Y. Laligand <pylaligand@google.com>
    Reviewed-by: David Turner <digit@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Testability-Review: Shai Barack <shayba@google.com>

---
+++ build/unification/future/data/convert_manifest_to_fini.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+'''Converts a distribution manifest to the Fuchsia INI format.'''
+
+import argparse
+import json
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--input', help='Path to original manifest', required=True)
+    parser.add_argument(
+        '--output', help='Path to the formatted manifest', required=True)
+    args = parser.parse_args()
+
+    with open(args.input, 'r') as input_file:
+        objects = json.load(input_file)
+    lines = sorted(o['destination'] + '=' + o['source'] + '\n' for o in objects)
+    with open(args.output, 'w') as output_file:
+        output_file.writelines(lines)
+
+
+if __name__ == '__main__':
+    main()
