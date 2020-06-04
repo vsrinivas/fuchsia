54099b9dc17cfed7221e204cfe6dd6ced8f84eb3
George Kulakowski <kulakowski@google.com>
2020-06-04 23:41:13 +0000

    [fxl] Begin replacing fxl's arraysize with std::size
    The two use a nearly-identical mechanism for computing the size of an
    array. This CL makes fxl's macro simply use std::size. Later, we will
    migrate clients directly to std::size.
    
    Bug: 33159
    Change-Id: I617b637e8b1bc0ce57dcb8e9d599712ff531d251
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/395655
    Reviewed-by: Adam Barth <abarth@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Testability-Review: Adam Barth <abarth@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: George Kulakowski <kulakowski@google.com>

--- src/lib/fxl/arraysize.h
+++ src/lib/fxl/arraysize.h
@@ -2,15 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef LIB_FXL_ARRAYSIZE_H_
-#define LIB_FXL_ARRAYSIZE_H_
+#ifndef SRC_LIB_FXL_ARRAYSIZE_H_
+#define SRC_LIB_FXL_ARRAYSIZE_H_
 
-#include <stddef.h>
+#include <iterator>
 
 #ifndef arraysize
-template <typename T, size_t N>
-char (&ArraySizeHelper(T (&array)[N]))[N];
-#define arraysize(array) (sizeof(ArraySizeHelper(array)))
+#define arraysize(array) ::std::size(array)
 #endif
 
-#endif  // LIB_FXL_ARRAYSIZE_H_
+#endif  // SRC_LIB_FXL_ARRAYSIZE_H_
