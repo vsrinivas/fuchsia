5467ce4e806dbbfb97ea5b4209fa36eabc2abe9f
Matthew Dempsky <mdempsky@google.com>
2020-06-05 23:24:12 +0000

    [syzkaller] run syz-sysgen during build
    Follows new upstream build pattern.
    
    Bug: 53194
    Change-Id: I9288f468e3efc0d7358164394b30c126a94a0085
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/394465
    Testability-Review: Drew Fisher <zarvox@google.com>
    Commit-Queue: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Marco Vanotti <mvanotti@google.com>

---
+++ src/testing/fuzzing/syzkaller/run-sysgen.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+exec "$@" >/dev/null
