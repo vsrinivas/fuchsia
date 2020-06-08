d2332fe4c97a3a39f75d5f288bfdd4feb7492f8f
Corey Tabaka <eieio@google.com>
2020-06-08 23:06:07 +0000

    [ktl] Add std::pair and related utility types.
    - Add std::pair and the utility type tags to ktl.
    
    Test: No behavior change involved.
    Change-Id: Ic9a2129f165e716d9216c5c0f0852edc1b63487f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/396313
    Reviewed-by: George Kulakowski <kulakowski@google.com>
    Testability-Review: Corey Tabaka <eieio@google.com>
    Commit-Queue: Corey Tabaka <eieio@google.com>

---
+++ zircon/kernel/lib/ktl/include/ktl/pair.h
@@ -0,0 +1,20 @@
+// Copyright 2020 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_INCLUDE_KTL_PAIR_H_
+#define ZIRCON_KERNEL_INCLUDE_KTL_PAIR_H_
+
+#include <utility>
+
+namespace ktl {
+
+using std::pair;
+using std::piecewise_construct_t;
+using std::piecewise_construct;
+
+}  // namespace ktl
+
+#endif  // ZIRCON_KERNEL_INCLUDE_KTL_PAIR_H_
