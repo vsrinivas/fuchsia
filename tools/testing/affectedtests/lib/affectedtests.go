dfd71582c8d92c0929622d05b59cce45fb3aa630
Shai Barack <shayba@google.com>
2020-06-09 23:05:04 +0000

    [affectedtests] Reland affectedtests speedup
    Now with 100% less flakes.
    This was previously flaky because the logic to shard the
    Ninja graph in its dotfile representation wasn't stable.
    As a result we would fail to acknowledge some of the nodes
    or edges, and then miss some possible paths in the graph.
    
    Multiply: affectedtests_tests
    
    Change-Id: I16702dabce1aab2c929edc1ccc6dec0beff670b7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/397044
    Testability-Review: Shai Barack <shayba@google.com>
    Commit-Queue: Shai Barack <shayba@google.com>
    Reviewed-by: Gary Miguel <garymm@google.com>

--- tools/testing/affectedtests/lib/affectedtests.go
+++ tools/testing/affectedtests/lib/affectedtests.go
@@ -11,10 +11,9 @@ import (
 
 var (
 	// Example: "0x12345678" [label="../../path/to/myfile.cc"]
-	nodePattern = regexp.MustCompile(`(?m)^"0x([^"]*)" \[label="([^"]*)"`)
-
+	nodePattern = regexp.MustCompile(`^"0x([0-9a-f"]+)" \[label="([^"]+)"`)
 	// Example: "0x12345678" -> "0xdeadbeef"
-	edgePattern = regexp.MustCompile(`(?m)^"0x([^"]*)" -> "0x([^"]*)"`)
+	edgePattern = regexp.MustCompile(`^"0x([0-9a-f]+)" -> "0x([0-9a-f]+)"`)
 )
 
 const buildDirRelativePrefix = "../../"
@@ -27,6 +26,81 @@ var member void
 
 // AffectedTests finds tests out of `testSpecs` that are affected by changes to `srcs` according to the build graph `dotBytes`.
 func AffectedTests(srcs []string, testSpecs []build.TestSpec, dotBytes []byte) []string {
+	// Parsing the dotfile is the slowest part.
+	// Do this first, and parse nodes and edges in parallel.
+	dotLines := strings.Split(string(dotBytes), "\n")
+	const chunkSize = 10_000
+	chunksCount := len(dotLines) / chunkSize
+
+	// Parse nodes
+	nodeToOutputCh := make(chan map[int64]string)
+	nodeToOutputChunkCh := make(chan map[int64]string, chunksCount)
+	// Make chunks of nodes
+	for i := 0; i < chunksCount; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+		if end > len(dotLines) {
+			end = len(dotLines)
+		}
+		go func(chunkLines []string) {
+			chunk := make(map[int64]string)
+			for _, line := range chunkLines {
+				if match := nodePattern.FindStringSubmatch(line); match != nil {
+					// Safe to ignore error because the regex only matches hex digits
+					node, _ := strconv.ParseInt(match[1], 16, 0)
+					chunk[node] = match[2]
+				}
+			}
+			nodeToOutputChunkCh <- chunk
+		}(dotLines[start:end])
+	}
+	// Join all node chunks
+	go func() {
+		nodeToOutput := make(map[int64]string)
+		for i := 0; i < chunksCount; i++ {
+			chunk := <-nodeToOutputChunkCh
+			for k, v := range chunk {
+				nodeToOutput[k] = v
+			}
+		}
+		nodeToOutputCh <- nodeToOutput
+	}()
+
+	// Parse edges
+	edgesCh := make(chan map[int64][]int64)
+	edgesChunkCh := make(chan map[int64][]int64, chunksCount)
+	// Make chunks of edges
+	for i := 0; i < chunksCount; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+		if end > len(dotLines) {
+			end = len(dotLines)
+		}
+		go func(chunkLines []string) {
+			chunk := make(map[int64][]int64)
+			for _, line := range chunkLines {
+				if match := edgePattern.FindStringSubmatch(line); match != nil {
+					// Safe to ignore error because the regex only matches hex digits
+					src, _ := strconv.ParseInt(match[1], 16, 0)
+					dst, _ := strconv.ParseInt(match[2], 16, 0)
+					chunk[src] = append(chunk[src], dst)
+				}
+			}
+			edgesChunkCh <- chunk
+		}(dotLines[start:end])
+	}
+	// Join all chunks
+	go func() {
+		edges := make(map[int64][]int64)
+		for i := 0; i < chunksCount; i++ {
+			chunk := <-edgesChunkCh
+			for k, v := range chunk {
+				edges[k] = append(edges[k], v...)
+			}
+		}
+		edgesCh <- edges
+	}()
+
 	testLabelToName := make(map[string][]string)
 	for _, testSpec := range testSpecs {
 		test := testSpec.Test
@@ -41,26 +115,13 @@ func AffectedTests(srcs []string, testSpecs []build.TestSpec, dotBytes []byte) [
 		testStampToNames[stamp] = names
 	}
 
-	dotStr := string(dotBytes)
-	nodeToOutput := make(map[int64]string)
-	for _, match := range nodePattern.FindAllStringSubmatch(dotStr, -1) {
-		node, _ := strconv.ParseInt(match[1], 16, 0)
-		nodeToOutput[node] = match[2]
-	}
-
-	edges := make(map[int64][]int64)
-	for _, match := range edgePattern.FindAllStringSubmatch(dotStr, -1) {
-		src, _ := strconv.ParseInt(match[1], 16, 0)
-		dst, _ := strconv.ParseInt(match[2], 16, 0)
-		edges[src] = append(edges[src], dst)
-	}
-
 	srcsSet := make(stringSet)
 	for _, src := range srcs {
 		srcsSet[src] = member
 	}
 
 	nodesToVisit := list.New()
+	nodeToOutput := <-nodeToOutputCh
 	for node, output := range nodeToOutput {
 		// Cut leading "../../" to rebase output to root build dir
 		if !strings.HasPrefix(output, buildDirRelativePrefix) {
@@ -73,6 +134,7 @@ func AffectedTests(srcs []string, testSpecs []build.TestSpec, dotBytes []byte) [
 	}
 
 	nodesVisited := make(intSet)
+	edges := <-edgesCh
 	for e := nodesToVisit.Front(); e != nil; e = e.Next() {
 		var node int64 = e.Value.(int64)
 		nodesVisited[node] = member
