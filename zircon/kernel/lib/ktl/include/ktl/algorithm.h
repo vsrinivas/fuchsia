d31d791481034869c37563a4675de748da486c63
Roland McGrath <mcgrathr@google.com>
2020-06-10 21:26:28 +0000

    [kernel][ktl] Fill out ktl/algorithm.h a bit
    Add the full suite of "Non-modifying sequence operations".
    Add a subset of the straightforward "Modifying sequence operations".
    Fill out the "Sorting operations" subset a bit.
    Add the full suite of "Binary search operations (on sorted ranges)".
    
    These are mostly the straightforward linear and binary search
    iterator applicators and the most straightforward mutators.
    
    std::sort is generally less desirable than std::stable_sort, but C++
    20 makes std::sort available to constexpr while std::stable_sort is
    not.  libc++ doesn't yet provide constexpr std::sort, but include it
    in the ktl subset on that expectation.
    
    Change-Id: I1e4df04f3e716bca60ad949ab347f48867df2671
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/396533
    Reviewed-by: George Kulakowski <kulakowski@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/lib/ktl/include/ktl/algorithm.h
+++ zircon/kernel/lib/ktl/include/ktl/algorithm.h
@@ -11,8 +11,42 @@
 
 namespace ktl {
 
+// "Non-modifying sequence operations"
+using std::adjacent_find;
+using std::all_of;
+using std::any_of;
+using std::count;
+using std::count_if;
+using std::find;
+using std::find_end;
+using std::find_first_of;
+using std::find_if;
+using std::find_if_not;
+using std::for_each;
+using std::for_each_n;
+using std::none_of;
+using std::search;
+using std::search_n;
+
+// "Modifying sequence operations" (subset)
+using std::copy;
+using std::copy_if;
+using std::fill;
+using std::fill_n;
+using std::swap;
+using std::transform;
+
 // "Sorting operations" (subset)
+using std::is_sorted;
+using std::is_sorted_until;
+using std::sort;
 using std::stable_sort;
+
+// "Binary search operations (on sorted ranges)"
+using std::binary_search;
+using std::equal_range;
+using std::lower_bound;
+using std::upper_bound;
 
 // "Minimum/maximum operations"
 using std::clamp;
