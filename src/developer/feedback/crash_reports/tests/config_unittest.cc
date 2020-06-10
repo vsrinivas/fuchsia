5ae61e9d5e9f998e232fe8cf8d7fd60aef9143c4
Francois Rousseau <frousseau@google.com>
2020-06-10 21:49:52 +0000

    [crash] switch to fuchsia_test_package()
    * we overwrite output_dir in executable() to circumvent binary name
      collisions under root_out_dir
    
    Bug: 53656
    Change-Id: Id47405be516d7fff503b1ca57a0e6c6e5822d41c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/397623
    Testability-Review: Francois Rousseau <frousseau@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Francois Rousseau <frousseau@google.com>
    Commit-Queue: Francois Rousseau <frousseau@google.com>

--- src/developer/feedback/crash_reports/tests/config_unittest.cc
+++ src/developer/feedback/crash_reports/tests/config_unittest.cc
@@ -23,21 +23,21 @@ void CheckEmptyConfig(const Config& config) {
 
 TEST(ConfigTest, ParseConfig_ValidConfig_UploadDisabled) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/valid_config_upload_disabled.json", &config), ZX_OK);
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/valid_upload_disabled.json", &config), ZX_OK);
   EXPECT_EQ(config.crash_server.upload_policy, kDisabled);
   EXPECT_EQ(config.crash_server.url, nullptr);
 }
 
 TEST(ConfigTest, ParseConfig_ValidConfig_UploadEnabled) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/valid_config_upload_enabled.json", &config), ZX_OK);
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/valid_upload_enabled.json", &config), ZX_OK);
   EXPECT_EQ(config.crash_server.upload_policy, kEnabled);
   EXPECT_EQ(*config.crash_server.url, "http://localhost:1234");
 }
 
 TEST(ConfigTest, ParseConfig_ValidConfig_UploadReadFromPrivacySettings) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/valid_config_upload_read_from_privacy_settings.json", &config),
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/valid_upload_read_from_privacy_settings.json", &config),
             ZX_OK);
   EXPECT_EQ(config.crash_server.upload_policy, kReadFromPrivacySettings);
   EXPECT_EQ(*config.crash_server.url, "http://localhost:1234");
@@ -45,7 +45,7 @@ TEST(ConfigTest, ParseConfig_ValidConfig_UploadReadFromPrivacySettings) {
 
 TEST(ConfigTest, ParseConfig_ValidConfig_UploadDisabledServerUrlIgnored) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/valid_config_upload_disabled_spurious_server.json", &config),
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/valid_upload_disabled_spurious_server.json", &config),
             ZX_OK);
   EXPECT_EQ(config.crash_server.upload_policy, kDisabled);
   // Even though a URL is set in the config file, we check that it is not set in the struct.
@@ -60,37 +60,38 @@ TEST(ConfigTest, ParseConfig_MissingConfig) {
 
 TEST(ConfigTest, ParseConfig_BadConfig_SpuriousField) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/bad_schema_spurious_field_config.json", &config),
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/bad_schema_spurious_field.json", &config),
             ZX_ERR_INTERNAL);
   CheckEmptyConfig(config);
 }
 
 TEST(ConfigTest, ParseConfig_BadConfig_MissingRequiredField) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/bad_schema_missing_required_field_config.json", &config),
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/bad_schema_missing_required_field.json", &config),
             ZX_ERR_INTERNAL);
   CheckEmptyConfig(config);
 }
 
 TEST(ConfigTest, ParseConfig_BadConfig_MissingServerUrlWithUploadEnabled) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/bad_schema_missing_server_upload_enabled_config.json", &config),
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/bad_schema_missing_server_upload_enabled.json", &config),
             ZX_ERR_INTERNAL);
   CheckEmptyConfig(config);
 }
 
 TEST(ConfigTest, ParseConfig_BadConfig_MissingServerUrlWithUploadReadFromPrivacySettings) {
   Config config;
-  ASSERT_EQ(ParseConfig(
-                "/pkg/data/bad_schema_missing_server_upload_read_from_privacy_settings_config.json",
-                &config),
-            ZX_ERR_INTERNAL);
+  ASSERT_EQ(
+      ParseConfig(
+          "/pkg/data/configs/bad_schema_missing_server_upload_read_from_privacy_settings.json",
+          &config),
+      ZX_ERR_INTERNAL);
   CheckEmptyConfig(config);
 }
 
 TEST(ConfigTest, ParseConfig_BadConfig_InvalidUploadPolicy) {
   Config config;
-  ASSERT_EQ(ParseConfig("/pkg/data/bad_schema_invalid_upload_policy_config.json", &config),
+  ASSERT_EQ(ParseConfig("/pkg/data/configs/bad_schema_invalid_upload_policy.json", &config),
             ZX_ERR_INTERNAL);
   CheckEmptyConfig(config);
 }
