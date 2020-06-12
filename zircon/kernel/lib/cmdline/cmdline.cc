2eb7ccc91d25cb22533f2f03edd65bb4c3678847
John Grossman <johngro@google.com>
2020-06-12 18:06:01 +0000

    [kernel][cmdline] Add parser support for kernel.ram.reserve
    Extend the kernel command line parser to allow for a new argument
    which allows users to request blocks of contiguous memory to be
    reserved and the physical address published via the kernel command
    line arguments to the rest of the system.
    
    Reserved memory will be allocated and pinned, and then held for the
    duration of the system's uptime.  It will not be available for the PMM
    to share with anyone else.  Additionally, the region allocated may be used
    to create VMOs via zx_vmo_create_physical.  It will not be subject to
    the root resource filter (which lands in the CL after this one).
    
    Also note that this is just the parser change.  The actual reservation
    behavior comes in with the CL which introduces the root resource
    filter.
    
    This is all about supporting core testing.  See bug 34029 for details.
    
    Test: new parser unit tests
    Change-Id: I86c63f930cce7ad46af79771b28c7545edb8d2a7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/395566
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Adrian Danis <adanis@google.com>
    Commit-Queue: John Grossman <johngro@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/lib/cmdline/cmdline.cc
+++ zircon/kernel/lib/cmdline/cmdline.cc
@@ -7,9 +7,12 @@
 #include "lib/cmdline.h"
 
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <zircon/assert.h>
+
+#include <fbl/auto_call.h>
 
 Cmdline gCmdline;
 
@@ -75,6 +78,158 @@ const char* Cmdline::GetString(const char* key) const {
     return nullptr;
   }
 
+  const char* ptr = FindKey(key, sz);
+
+  if (ptr != nullptr) {
+    // Return a pointer to the data after the '='
+    const char* data = ptr + sz + 1;
+    ZX_DEBUG_ASSERT(data < (data_ + length_));
+    return data;
+  }
+
+  return nullptr;
+}
+
+bool Cmdline::GetBool(const char* key, bool default_value) const {
+  const char* value = GetString(key);
+  if (value == nullptr) {
+    return default_value;
+  }
+  if ((strcmp(value, "0") == 0) || (strcmp(value, "false") == 0) || (strcmp(value, "off") == 0)) {
+    return false;
+  }
+  return true;
+}
+
+uint64_t Cmdline::GetUInt64(const char* key, uint64_t default_value) const {
+  const char* value_str = GetString(key);
+  if (value_str == nullptr || *value_str == '\0') {
+    return default_value;
+  }
+
+  char* end;
+  static_assert(sizeof(unsigned long int) == sizeof(uint64_t));
+  uint64_t value = strtol(value_str, &end, 0);
+  if (*end != '\0') {
+    return default_value;
+  }
+  return value;
+}
+
+uint32_t Cmdline::GetUInt32(const char* key, uint32_t default_value) const {
+  return static_cast<uint32_t>(Cmdline::GetUInt64(key, default_value));
+}
+
+void Cmdline::ProcessRamReservations(const ProcessRamReservationsCbk& cbk) {
+  constexpr const char kHeader[] = "kernel.ram.reserve.";
+  constexpr size_t kHeaderLen = sizeof(kHeader) - 1;
+  constexpr char kErasedArgFillChar = 'x';
+
+  // Our internal length_ (which does not include the final \0 termination) must
+  // be strictly smaller than our internal storage.
+  ZX_DEBUG_ASSERT(length_ < sizeof(data_));
+
+  size_t offset = 0;
+  while (offset < length_) {
+    char* arg = data_ + offset;
+    size_t arg_len = strlen(arg);
+
+    // If this is the final zero-length argument, then we are at the end of the
+    // command line argument list.  Otherwise, make sure that we always
+    // increment offset to point to the next argument.
+    if (!arg_len) {
+      break;
+    }
+    offset += arg_len + 1;
+
+    // Does our argument start with our header?  If not, then just move on to
+    // the next argument.
+    if (strncmp(kHeader, arg, kHeaderLen)) {
+      continue;
+    }
+
+    // If something goes wrong from here on out, be sure to log a warning and
+    // erase the entry.
+    auto cleanup = fbl::MakeAutoCall([arg, arg_len]() {
+      printf("WARN - Reservation was rejected or encountered a parsing error.  \"%s\"\n", arg);
+      memset(arg, kErasedArgFillChar, arg_len);
+    });
+
+    // Find the '=' in our argument.  If we cannot find one, then reservation is
+    // malformed and should be skipped.
+    char* equal = strchr(arg, '=');
+    if (equal == nullptr) {
+      continue;
+    }
+
+    // If the '=' comes right after the header, then the user failed to supply a
+    // unique name to the reservation.  Warn and skip.
+    const char* reservation_name = arg + kHeaderLen;
+    if (reservation_name == equal) {
+      continue;
+    }
+
+    // If this is not the final instance of the region key in our command line
+    // arguments, then someone must have overridden the argument while building
+    // the kernel command line.  Erase this entry and move on.
+    if (arg != FindKey(arg, equal - arg)) {
+      cleanup.cancel();
+      memset(arg, kErasedArgFillChar, arg_len);
+      continue;
+    }
+
+    // reservations are always of the form "size,placeholder".  If we fail to
+    // find the "," separator, then this is an invalid region reservation and we
+    // should skip it.
+    const char* comma = strchr(equal + 1, ',');
+    if (comma == nullptr) {
+      continue;
+    }
+
+    // Parse our size
+    const char* value = equal + 1;
+    char* end;
+    size_t size = strtoul(value, &end, 0);
+    if (end != comma) {
+      continue;
+    }
+
+    // Sanity check to make sure that the user passed the placeholder for the
+    // allocated address.
+    constexpr const char* kDynamicToken = "0xXXXXXXXXXXXXXXXX";
+    if (strcmp(kDynamicToken, comma + 1)) {
+      continue;
+    }
+
+    // Great, we have all of our details ready to go.  Invoke the user-supplied
+    // callback.
+    std::string_view name_view(reservation_name, equal - reservation_name);
+    auto result = cbk(size, name_view);
+
+    // If the user accepted the reservation, record the base address and we are
+    // done.
+    if (result.has_value()) {
+      char* dst = const_cast<char*>(comma + 1);
+      size_t space = length_ - (dst - data_);
+      snprintf(dst, space, "0x%016lx", result.value());
+      cleanup.cancel();
+    }
+  }
+}
+
+size_t Cmdline::size() const { return length_ + 1; }
+
+void Cmdline::AddOrAbort(char c) {
+  if (length_ < kCmdlineMax - 1) {
+    data_[length_++] = c;
+  } else {
+    ZX_PANIC("cmdline overflow");
+  }
+}
+
+const char* Cmdline::FindKey(const char* key, size_t key_len) const {
+  ZX_DEBUG_ASSERT(key && key_len);
+
   if (length_ == 0) {
     return nullptr;
   }
@@ -108,54 +263,10 @@ const char* Cmdline::GetString(const char* key) const {
     if (ptr != data_) {
       ++ptr;
     }
-    if (!strncmp(ptr, key, sz) && ptr[sz] == '=') {
+    if (!strncmp(ptr, key, key_len) && ptr[key_len] == '=') {
       break;
     }
   }
-  ptr += sz;
-  ptr++;  // Increment past the '='.
+
   return ptr;
-}
-
-bool Cmdline::GetBool(const char* key, bool default_value) const {
-  const char* value = GetString(key);
-  if (value == nullptr) {
-    return default_value;
-  }
-  if ((strcmp(value, "0") == 0) || (strcmp(value, "false") == 0) || (strcmp(value, "off") == 0)) {
-    return false;
-  }
-  return true;
-}
-
-uint64_t Cmdline::GetUInt64(const char* key, uint64_t default_value) const {
-  const char* value_str = GetString(key);
-  if (value_str == nullptr || *value_str == '\0') {
-    return default_value;
-  }
-
-  char* end;
-  static_assert(sizeof(unsigned long int) == sizeof(uint64_t));
-  uint64_t value = strtol(value_str, &end, 0);
-  if (*end != '\0') {
-    return default_value;
-  }
-  return value;
-}
-
-uint32_t Cmdline::GetUInt32(const char* key, uint32_t default_value) const {
-  return static_cast<uint32_t>(Cmdline::GetUInt64(key, default_value));
-}
-
-size_t Cmdline::size() const {
-  return length_ + 1;
-}
-
-
-void Cmdline::AddOrAbort(char c) {
-  if (length_ < kCmdlineMax - 1) {
-    data_[length_++] = c;
-  } else {
-    ZX_PANIC("cmdline overflow");
-  }
 }
--- zircon/kernel/lib/cmdline/include/lib/cmdline.h
+++ zircon/kernel/lib/cmdline/include/lib/cmdline.h
@@ -10,8 +10,12 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <sys/types.h>
-
 #include <zircon/compiler.h>
+
+#include <optional>
+#include <string_view>
+
+#include <fbl/function.h>
 
 // Cmdline is used to build and access the kernel command line.
 //
@@ -59,8 +63,41 @@ class Cmdline {
   // Return the last value for |key| or |default_value| if not found.
   uint64_t GetUInt64(const char* key, uint64_t default_value) const;
 
-  // Returns a pointer to the command line.  This is a sequence of zero or more \0-terminated
-  // strings followed by a \0.
+  // Process and issue callbacks for the reserved RAM entries of the kernel
+  // command line, fixing up the entries in response to the results of the
+  // callback.
+  //
+  // A kernel command line may include commands to reserve sections of
+  // contiguous physical RAM, usually for testing purposes.  Reserved sections
+  // will be contiguous in physical RAM, off limits to the PMM allocator, and
+  // accessible by usermode software with access to the root resource or an MMIO
+  // resource with appropriate range.  The commands take the following form.
+  //
+  // kernel.ram.reserve.<name>=<size>,0xXXXXXXXXXXXXXXXX
+  //
+  // Note the "0xXXXXXXXXXXXXXXXX".  This is a placeholder for a dynamically
+  // allocated address and needs to be replicated exactly so that the kernel has
+  // a place to publish the physical address of the reservation to usermode.
+  //
+  // To assist in processing these regions, the Cmdline class provides the
+  // ProcessRamReservions method.  This method will attempt to find all of the
+  // requested reservation pairs in the system and call the user supplied
+  // callback for each.  If the reservation fails for any reason, the method
+  // will erase the entry in the cmd line image, replacing it with
+  // "." characters instead.  If the reservation is successful, the method will
+  // update the base address placeholder with physical address which was reserved.
+  //
+  // Users must supply a callback function/lambda to the ProcessRamReservations
+  // call.  The size and name of each valid reservation will be supplied to the
+  // callback, which must return the physical address of the successful
+  // reservation, or std::nullopt in the case that the reservation fails for any
+  // reason.
+  using ProcessRamReservationsCbk =
+      fbl::InlineFunction<std::optional<uintptr_t>(size_t size, std::string_view name),
+                          sizeof(void*)>;
+  void ProcessRamReservations(const ProcessRamReservationsCbk& cbk);
+
+  // read-only  access to the underlying data
   const char* data() const { return data_; }
 
   // Return the size of data() including the final \0.
@@ -68,10 +105,14 @@ class Cmdline {
   // Guaranteed to be >= 1;
   size_t size() const;
 
- private:
+ protected:
   // Adds the given character to data_ and updates length_. If the character would cause the buffer
   // to exceed kCmdlineMax, panic.
   void AddOrAbort(char c);
+
+  // Find the key of the explicitly specified length in our list of command line
+  // arguments.
+  const char* FindKey(const char* key, size_t key_len) const;
 
   // Zero-initialize to ensure the |gCmdline| instance of this class lives in the BSS rather than
   // DATA segment so we don't bloat the kernel.
