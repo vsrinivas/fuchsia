a6dc9823fbd905d39693f85f015e61030a30ba86
Vincent Belliard <vbelliard@google.com>
2020-06-13 00:43:16 +0000

    [fidlcat] create fidlcat_printer.cc
    and fidlcat_printer.h
    
    Puts FidlcatPrinter class in its own file.
    
    Change-Id: I68e011f28209e94973267ec5c3d571fdca677849
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/398255
    Commit-Queue: Vincent Belliard <vbelliard@google.com>
    Reviewed-by: Jeremy Manson <jeremymanson@google.com>
    Testability-Review: Jeremy Manson <jeremymanson@google.com>

--- tools/fidlcat/lib/event.h
+++ tools/fidlcat/lib/event.h
@@ -11,43 +11,13 @@
 #include "src/lib/fidl_codec/wire_object.h"
 #include "src/lib/fidl_codec/wire_types.h"
 #include "src/lib/fxl/memory/weak_ptr.h"
-#include "tools/fidlcat/lib/inference.h"
-#include "tools/fidlcat/lib/type_decoder.h"
+#include "tools/fidlcat/lib/fidlcat_printer.h"
 
 namespace fidlcat {
 
 class Location;
 class Syscall;
 class SyscallDecoder;
-class SyscallDisplayDispatcher;
-
-// Printer which allows us to print the infered data for handles.
-class FidlcatPrinter : public fidl_codec::PrettyPrinter {
- public:
-  FidlcatPrinter(SyscallDisplayDispatcher* dispatcher, uint64_t process_id, std::ostream& os,
-                 std::string_view line_header, int tabulations = 0);
-
-  bool display_stack_frame() const { return display_stack_frame_; }
-
-  bool DumpMessages() const override { return dump_messages_; }
-
-  void DisplayHandle(const zx_handle_info_t& handle) override;
-  void DisplayStatus(zx_status_t status);
-  bool DisplayReturnedValue(SyscallReturnType type, int64_t returned_value);
-  void DisplayInline(
-      const std::vector<std::unique_ptr<fidl_codec::StructMember>>& members,
-      const std::map<const fidl_codec::StructMember*, std::unique_ptr<fidl_codec::Value>>& values);
-  void DisplayOutline(
-      const std::vector<std::unique_ptr<fidl_codec::StructMember>>& members,
-      const std::map<const fidl_codec::StructMember*, std::unique_ptr<fidl_codec::Value>>& values);
-  void DisplayStackFrame(const std::vector<Location>& stack_frame);
-
- private:
-  const Inference& inference_;
-  const uint64_t process_id_;
-  const bool display_stack_frame_;
-  const bool dump_messages_;
-};
 
 class Process {
  public:
---
+++ tools/fidlcat/lib/fidlcat_printer.h
@@ -0,0 +1,51 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_FIDLCAT_LIB_FIDLCAT_PRINTER_H_
+#define TOOLS_FIDLCAT_LIB_FIDLCAT_PRINTER_H_
+
+#include <zircon/system/public/zircon/types.h>
+
+#include "src/developer/debug/zxdb/client/process.h"
+#include "src/lib/fidl_codec/wire_object.h"
+#include "src/lib/fidl_codec/wire_types.h"
+#include "tools/fidlcat/lib/inference.h"
+#include "tools/fidlcat/lib/type_decoder.h"
+
+namespace fidlcat {
+
+class Location;
+class SyscallDisplayDispatcher;
+
+// Printer which allows us to print the infered data for handles.
+class FidlcatPrinter : public fidl_codec::PrettyPrinter {
+ public:
+  FidlcatPrinter(SyscallDisplayDispatcher* dispatcher, uint64_t process_id, std::ostream& os,
+                 std::string_view line_header, int tabulations = 0);
+
+  bool display_stack_frame() const { return display_stack_frame_; }
+
+  bool DumpMessages() const override { return dump_messages_; }
+
+  void DisplayHandle(const zx_handle_info_t& handle) override;
+  void DisplayStatus(zx_status_t status);
+  bool DisplayReturnedValue(SyscallReturnType type, int64_t returned_value);
+  void DisplayInline(
+      const std::vector<std::unique_ptr<fidl_codec::StructMember>>& members,
+      const std::map<const fidl_codec::StructMember*, std::unique_ptr<fidl_codec::Value>>& values);
+  void DisplayOutline(
+      const std::vector<std::unique_ptr<fidl_codec::StructMember>>& members,
+      const std::map<const fidl_codec::StructMember*, std::unique_ptr<fidl_codec::Value>>& values);
+  void DisplayStackFrame(const std::vector<Location>& stack_frame);
+
+ private:
+  const Inference& inference_;
+  const uint64_t process_id_;
+  const bool display_stack_frame_;
+  const bool dump_messages_;
+};
+
+}  // namespace fidlcat
+
+#endif  // TOOLS_FIDLCAT_LIB_FIDLCAT_PRINTER_H_
