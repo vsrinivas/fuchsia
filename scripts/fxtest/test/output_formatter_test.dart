1d824742e101e00fa380aa2899fdc256dfed3675
Wesley Moy <wesleymoy@google.com>
2020-06-16 23:01:59 +0000

    [fx][test] Ensure test output uses wrapWith
    Previously, the OutputFormatter was constructed without the wrapWith
    function having either @required or a sensible fallback. This has been
    fixed in I0bfdb64442e6 and will raise an error at compile time.
    
    This change bolsters that improvement by verifying that the output is
    correct given realistic implementations of wrapWith.
    
    These tests were verified via fx test and pub run test at JIRI_HEAD, and
    verified broken before commit 84912b5446 in fuchsia.git via pub run
    test.
    
    Fixed: 53877
    Tested: pub run test at JIRI_HEAD and 84912b5446^
    Change-Id: I1c6581716fc528b207967d189c04a60e3daee22a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/398213
    Commit-Queue: Renato Mangini Dias <mangini@google.com>
    Reviewed-by: Craig Labenz <craiglabenz@google.com>
    Testability-Review: Craig Labenz <craiglabenz@google.com>

---
+++ scripts/fxtest/test/output_formatter_test.dart
@@ -0,0 +1,20 @@
+import 'package:fxtest/fxtest.dart';
+import 'package:fxtest/output/output_formatter.dart';
+import 'package:io/ansi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  var config = TestsConfig.fromRawArgs(rawArgs: []);
+
+  test('standard output formatter displays ratio correctly', () {
+    var formatter = StandardOutputFormatter(
+        hasRealTimeOutput: false, wrapWith: config.wrapWith);
+    expect(formatter.ratioDisplay,
+        '${green.escape}PASS: 0${resetAll.escape} ${red.escape}FAIL: 0${resetAll.escape}');
+  });
+
+  test('info formatter displays ratio correctly', () {
+    var formatter = InfoFormatter();
+    expect(formatter.ratioDisplay, 'PASS: 0 FAIL: 0');
+  });
+}
