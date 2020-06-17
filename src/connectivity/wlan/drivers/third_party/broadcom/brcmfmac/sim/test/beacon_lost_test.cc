9378438607af73f8fe56298fce489b81aea52819
Joshua Conner <joshconner@google.com>
2020-06-17 16:37:22 +0000

    [brcmfmac] Refactor beacon_lost_test
    Take advantage of new SimTest infrastructure for automating the
    association process and using an interface class with virtual
    functions.
    
    Bug: 53992
    Test: Nothing new, refactor of code.
    Change-Id: I31d468716333fc1deb560959e187c579093f9378
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/399605
    Reviewed-by: Zhiyi Chen <zhiyichen@google.com>
    Commit-Queue: Joshua Conner <joshconner@google.com>
    Testability-Review: Zhiyi Chen <zhiyichen@google.com>

--- src/connectivity/wlan/drivers/third_party/broadcom/brcmfmac/sim/test/beacon_lost_test.cc
+++ src/connectivity/wlan/drivers/third_party/broadcom/brcmfmac/sim/test/beacon_lost_test.cc
@@ -18,6 +18,13 @@ constexpr wlan_ssid_t kDefaultSsid = {.len = 15, .ssid = "Fuchsia Fake AP"};
 const common::MacAddr kDefaultBssid({0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc});
 const common::MacAddr kMadeupClient({0xde, 0xad, 0xbe, 0xef, 0x00, 0x01});
 
+struct ClientIfc : public SimInterface {
+  void OnDeauthInd(const wlanif_deauth_indication_t* ind) override;
+
+  // Once test is finished, associations and disassociations from teardowns are ignored
+  bool test_complete_ = false;
+};
+
 class BeaconLostTest : public SimTest {
  public:
   // How long an individual test will run for. We need an end time because tests run until no more
@@ -27,122 +34,29 @@ class BeaconLostTest : public SimTest {
   void Init();
   void Finish();
 
-  // Run through the join => auth => assoc flow
-  void StartAssoc();
-
   // Move client's location in environment
   void MoveClient(int32_t x, int32_t y);
 
  protected:
-  struct AssocContext {
-    // Information about the BSS we are attempting to associate with. Used to generate the
-    // appropriate MLME calls (Join => Auth => Assoc).
-    simulation::WlanTxInfo tx_info = {.channel = kDefaultChannel};
-    common::MacAddr bssid = kDefaultBssid;
-    wlan_ssid_t ssid = kDefaultSsid;
-
-    // There should be one result for each association response received
-    std::list<wlan_assoc_result_t> expected_results;
-
-    // An optional function to call when we see the association request go out
-    std::optional<std::function<void()>> on_assoc_req_callback;
-
-    // Track number of association responses
-    size_t assoc_resp_count;
-    // Track number of deauth indications, triggered by firmware
-    size_t deauth_ind_count;
-  };
-
-  struct AssocRespInfo {
-    wlan_channel_t channel;
-    common::MacAddr src;
-    common::MacAddr dst;
-    uint16_t status;
-  };
-
   // This is the interface we will use for our single client interface
-  SimInterface client_ifc_;
-
-  AssocContext context_;
+  ClientIfc client_ifc_;
 
   // Keep track of the APs that are in operation so we can easily disable beaconing on all of them
   // at the end of each test.
   std::list<simulation::FakeAp*> aps_;
-
-  // Once test is finished, associations and disassociations from teardowns are ignored
-  bool test_complete_ = false;
-
- private:
-  // StationIfc overrides
-  void Rx(std::shared_ptr<const simulation::SimFrame> frame,
-          std::shared_ptr<const simulation::WlanRxInfo> info) override;
-
-  // SME callbacks
-  static wlanif_impl_ifc_protocol_ops_t sme_ops_;
-  wlanif_impl_ifc_protocol sme_protocol_ = {.ops = &sme_ops_, .ctx = this};
-
-  // Event handlers
-  void OnJoinConf(const wlanif_join_confirm_t* resp);
-  void OnAuthConf(const wlanif_auth_confirm_t* resp);
-  void OnAssocConf(const wlanif_assoc_confirm_t* resp);
-  void OnDeauthInd(const wlanif_deauth_indication_t* ind);
 };
 
-// Since we're acting as wlanif, we need handlers for any protocol calls we may receive
-wlanif_impl_ifc_protocol_ops_t BeaconLostTest::sme_ops_ = {
-    .on_scan_result =
-        [](void* cookie, const wlanif_scan_result_t* result) {
-          // Ignore
-        },
-    .on_scan_end =
-        [](void* cookie, const wlanif_scan_end_t* end) {
-          // Ignore
-        },
-    .join_conf =
-        [](void* cookie, const wlanif_join_confirm_t* resp) {
-          static_cast<BeaconLostTest*>(cookie)->OnJoinConf(resp);
-        },
-    .auth_conf =
-        [](void* cookie, const wlanif_auth_confirm_t* resp) {
-          static_cast<BeaconLostTest*>(cookie)->OnAuthConf(resp);
-        },
-    .deauth_ind =
-        [](void* cookie, const wlanif_deauth_indication_t* ind) {
-          static_cast<BeaconLostTest*>(cookie)->OnDeauthInd(ind);
-        },
-    .assoc_conf =
-        [](void* cookie, const wlanif_assoc_confirm_t* resp) {
-          static_cast<BeaconLostTest*>(cookie)->OnAssocConf(resp);
-        },
-    .disassoc_conf =
-        [](void* cookie, const wlanif_disassoc_confirm_t* resp) {
-          // ignore
-        },
-    .signal_report =
-        [](void* cookie, const wlanif_signal_report_indication* ind) {
-          // ignore
-        },
-};
-
-void BeaconLostTest::Rx(std::shared_ptr<const simulation::SimFrame> frame,
-                        std::shared_ptr<const simulation::WlanRxInfo> info) {
-  ASSERT_EQ(frame->FrameType(), simulation::SimFrame::FRAME_TYPE_MGMT);
-
-  auto mgmt_frame = std::static_pointer_cast<const simulation::SimManagementFrame>(frame);
-  // If a handler has been installed, call it
-  if (mgmt_frame->MgmtFrameType() == simulation::SimManagementFrame::FRAME_TYPE_ASSOC_REQ) {
-    if (context_.on_assoc_req_callback) {
-      (*context_.on_assoc_req_callback)();
-    }
+// Ignore any deauth indications that are received during teardown
+void ClientIfc::OnDeauthInd(const wlanif_deauth_indication_t* ind) {
+  if (!test_complete_) {
+    SimInterface::OnDeauthInd(ind);
   }
 }
 
 // Create our device instance and hook up the callbacks
 void BeaconLostTest::Init() {
   ASSERT_EQ(SimTest::Init(), ZX_OK);
-  ASSERT_EQ(StartInterface(WLAN_INFO_MAC_ROLE_CLIENT, &client_ifc_, &sme_protocol_), ZX_OK);
-  context_.assoc_resp_count = 0;
-  context_.deauth_ind_count = 0;
+  ASSERT_EQ(StartInterface(WLAN_INFO_MAC_ROLE_CLIENT, &client_ifc_), ZX_OK);
   SCHEDULE_CALL(&BeaconLostTest::Finish, kTestDuration);
 }
 
@@ -151,44 +65,7 @@ void BeaconLostTest::Finish() {
     ap->DisableBeacon();
   }
   aps_.clear();
-  test_complete_ = true;
-}
-
-void BeaconLostTest::OnJoinConf(const wlanif_join_confirm_t* resp) {
-  // Send auth request
-  wlanif_auth_req_t auth_req;
-  std::memcpy(auth_req.peer_sta_address, context_.bssid.byte, ETH_ALEN);
-  auth_req.auth_type = WLAN_AUTH_TYPE_OPEN_SYSTEM;
-  auth_req.auth_failure_timeout = 1000;  // ~1s (although value is ignored for now)
-  client_ifc_.if_impl_ops_->auth_req(client_ifc_.if_impl_ctx_, &auth_req);
-}
-
-void BeaconLostTest::OnAuthConf(const wlanif_auth_confirm_t* resp) {
-  // Send assoc request
-  wlanif_assoc_req_t assoc_req = {.rsne_len = 0, .vendor_ie_len = 0};
-  memcpy(assoc_req.peer_sta_address, context_.bssid.byte, ETH_ALEN);
-  client_ifc_.if_impl_ops_->assoc_req(client_ifc_.if_impl_ctx_, &assoc_req);
-}
-
-void BeaconLostTest::OnAssocConf(const wlanif_assoc_confirm_t* resp) {
-  context_.assoc_resp_count++;
-  EXPECT_EQ(resp->result_code, context_.expected_results.front());
-  context_.expected_results.pop_front();
-}
-
-void BeaconLostTest::OnDeauthInd(const wlanif_deauth_indication_t* ind) {
-  if (!test_complete_)
-    context_.deauth_ind_count++;
-}
-
-void BeaconLostTest::StartAssoc() {
-  // Send join request
-  wlanif_join_req join_req = {};
-  std::memcpy(join_req.selected_bss.bssid, context_.bssid.byte, ETH_ALEN);
-  join_req.selected_bss.ssid.len = context_.ssid.len;
-  memcpy(join_req.selected_bss.ssid.data, context_.ssid.ssid, WLAN_MAX_SSID_LEN);
-  join_req.selected_bss.chan = context_.tx_info.channel;
-  client_ifc_.if_impl_ops_->join_req(client_ifc_.if_impl_ctx_, &join_req);
+  client_ifc_.test_complete_ = true;
 }
 
 // Move the client (not the test)
@@ -204,12 +81,9 @@ TEST_F(BeaconLostTest, NoBeaconDisassocTest) {
   // Start up fake AP
   simulation::FakeAp ap(env_.get(), kDefaultBssid, kDefaultSsid, kDefaultChannel);
   ap.EnableBeacon(zx::msec(100));
-  // Do not add ap to list of ap's that need to have their beacon disabled, since we
-  // will disable it in test
-  context_.expected_results.push_front(WLAN_ASSOC_RESULT_SUCCESS);
 
   // Associate with fake AP
-  SCHEDULE_CALL(&BeaconLostTest::StartAssoc, zx::msec(10));
+  client_ifc_.AssociateWith(ap, zx::msec(10));
 
   // disable the beacon after association
   auto cb_fn = std::make_unique<std::function<void()>>();
@@ -219,9 +93,10 @@ TEST_F(BeaconLostTest, NoBeaconDisassocTest) {
   env_->Run();
 
   // Association with fake AP should be successful
-  EXPECT_EQ(context_.assoc_resp_count, 1U);
+  EXPECT_EQ(client_ifc_.stats_.assoc_successes_, 1U);
+
   // A disassociation should have occured due a beacon timeout
-  EXPECT_EQ(context_.deauth_ind_count, 1U);
+  EXPECT_EQ(client_ifc_.stats_.deauth_indications_.size(), 1U);
 }
 
 // Verify that deauthorization occurs after moving away from associated AP
@@ -242,10 +117,10 @@ TEST_F(BeaconLostTest, BeaconTooFarDisassocTest) {
   simulation::FakeAp ap(env_.get(), kDefaultBssid, kDefaultSsid, kDefaultChannel);
   ap.EnableBeacon(zx::msec(100));
   aps_.push_back(&ap);
-  context_.expected_results.push_front(WLAN_ASSOC_RESULT_SUCCESS);
 
   // Associate with fake AP
-  SCHEDULE_CALL(&BeaconLostTest::StartAssoc, zx::msec(10));
+  client_ifc_.AssociateWith(ap, zx::msec(10));
+
   // Move away from the AP
   auto cb_fn = std::make_unique<std::function<void()>>();
   *cb_fn = std::bind(&BeaconLostTest::MoveClient, this, 150, 0);
@@ -254,9 +129,10 @@ TEST_F(BeaconLostTest, BeaconTooFarDisassocTest) {
   env_->Run();
 
   // Association with fake AP should be successful
-  EXPECT_EQ(context_.assoc_resp_count, 1U);
+  EXPECT_EQ(client_ifc_.stats_.assoc_successes_, 1U);
+
   // A disassociation should have occured due to moving away from the AP
-  EXPECT_EQ(context_.deauth_ind_count, 1U);
+  EXPECT_EQ(client_ifc_.stats_.deauth_indications_.size(), 1U);
 }
 
 // Verify that losing a beacon from an unassociated ap does not cause any disassociation
@@ -284,10 +160,9 @@ TEST_F(BeaconLostTest, WrongBeaconLossTest) {
   env_->MoveStation(&ap2, 50, 0);
   aps_.push_back(&ap2);
 
-  context_.expected_results.push_front(WLAN_ASSOC_RESULT_SUCCESS);
-
   // Will associate with one AP
-  SCHEDULE_CALL(&BeaconLostTest::StartAssoc, zx::msec(10));
+  client_ifc_.AssociateWith(ap1, zx::msec(10));
+
   // Move away closer to the AP we are associated to. Should not impact connection.
   auto cb_fn = std::make_unique<std::function<void()>>();
   *cb_fn = std::bind(&BeaconLostTest::MoveClient, this, -75, 0);
@@ -296,9 +171,10 @@ TEST_F(BeaconLostTest, WrongBeaconLossTest) {
   env_->Run();
 
   // Association with fake AP should be successful
-  EXPECT_EQ(context_.assoc_resp_count, 1U);
+  EXPECT_EQ(client_ifc_.stats_.assoc_successes_, 1U);
+
   // No disassociation should occur
-  EXPECT_EQ(context_.deauth_ind_count, 0U);
+  EXPECT_EQ(client_ifc_.stats_.deauth_indications_.size(), 0U);
 }
 
 // Verify that moving out of range of the beacon for a small duration does not cause a deauth
@@ -326,10 +202,9 @@ TEST_F(BeaconLostTest, TempBeaconLossTest) {
   env_->MoveStation(&ap1, 0, 0);
   aps_.push_back(&ap1);
 
-  context_.expected_results.push_front(WLAN_ASSOC_RESULT_SUCCESS);
-
   // Will associate with one AP
-  SCHEDULE_CALL(&BeaconLostTest::StartAssoc, zx::msec(10));
+  client_ifc_.AssociateWith(ap1, zx::msec(10));
+
   // Move away from the AP we are associated to.
   auto cb_fn = std::make_unique<std::function<void()>>();
   *cb_fn = std::bind(&BeaconLostTest::MoveClient, this, 100, 0);
@@ -343,9 +218,10 @@ TEST_F(BeaconLostTest, TempBeaconLossTest) {
   env_->Run();
 
   // Association with fake AP should be successful
-  EXPECT_EQ(context_.assoc_resp_count, 1U);
+  EXPECT_EQ(client_ifc_.stats_.assoc_successes_, 1U);
+
   // No disassociation should occur
-  EXPECT_EQ(context_.deauth_ind_count, 0U);
+  EXPECT_EQ(client_ifc_.stats_.deauth_indications_.size(), 0U);
 }
 
 }  // namespace wlan::brcmfmac
