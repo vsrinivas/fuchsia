b356a0bd345e7d1e926b913f5913fe8f3130ea17
Paul Daniel Faria <paulfaria@google.com>
2020-06-18 23:37:53 +0000

    [SetUi] Fix setui_client, add integration test
    Fix an issue with setui_client's cmx file that was introduced
    with a recent change. Add an integration test to validate that
    setui_client can actually by run and is not broken in the future.
    
    Tests: fx test -o setui-client-integration-test
    Change-Id: Icfc2994cd7e9053ee6f39329ade9c0a75f7cb466
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/399874
    Commit-Queue: Paul Faria <paulfaria@google.com>
    Reviewed-by: Francois Rousseau <frousseau@google.com>
    Testability-Review: Francois Rousseau <frousseau@google.com>

---
+++ garnet/bin/setui_client/src/integration_test.rs
@@ -0,0 +1,24 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::Error,
+    fuchsia_async as fasync,
+    fuchsia_component::client::{launcher, AppBuilder},
+};
+
+const SETUI_CLIENT_PATH: &str = "fuchsia-pkg://fuchsia.com/setui_client#meta/setui_client.cmx";
+const EXPECTED_OUT_NAME: &str = "setui_client";
+const EXPECTED_OUT_HELP: &str = "Prints this message or the help of the given subcommand(s)";
+
+#[fasync::run_singlethreaded(test)]
+async fn run() -> Result<(), Error> {
+    let output = AppBuilder::new(SETUI_CLIENT_PATH).arg("--help").output(&launcher()?)?.await?;
+    assert!(output.exit_status.success());
+
+    let output = String::from_utf8_lossy(&output.stdout);
+    assert!(output.contains(EXPECTED_OUT_NAME));
+    assert!(output.contains(EXPECTED_OUT_HELP));
+    Ok(())
+}
