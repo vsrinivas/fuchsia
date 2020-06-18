592f66f34367946eee6fcca79cf6fd5704b30448
Tamir Duberstein <tamird@google.com>
2020-06-18 23:27:00 +0000

    [tcp-stream-ext] Move stub_netstack in-process
    There's no need for IPC here, and the code is simpler without it.
    
    Change-Id: I7533b683328e734167928bc9e06a6b4081aacf39
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/400365
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Ben Keller <galbanum@google.com>
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
    Testability-Review: Bruno Dal Bo <brunodalbo@google.com>
    Testability-Review: Ben Keller <galbanum@google.com>
    Testability-Review: Ghanan Gowripalan <ghanan@google.com>
    Commit-Queue: Tamir Duberstein <tamird@google.com>

--- src/lib/tcp-stream-ext/src/stub_netstack_test.rs
+++ src/lib/tcp-stream-ext/src/stub_netstack_test.rs
@@ -3,12 +3,15 @@
 // found in the LICENSE file.
 
 #![cfg(test)]
+
 use {
+    fidl_fuchsia_io::{NodeInfo, StreamSocket},
+    fidl_fuchsia_posix_socket::{StreamSocketMarker, StreamSocketRequest},
+    fuchsia_async as fasync,
+    fuchsia_zircon::{self as zx, HandleBased as _},
+    futures::{future, stream::StreamExt as _},
     matches::assert_matches,
-    std::{
-        net::{Ipv4Addr, SocketAddr, TcpListener, TcpStream},
-        os::unix::io::{FromRawFd, IntoRawFd as _},
-    },
+    std::{net::TcpStream, thread},
     tcp_stream_ext::TcpStreamExt as _,
 };
 
@@ -16,35 +19,66 @@ const TCP_KEEPCNT_OPTION_VALUE: i32 = -11;
 const TCP_KEEPINTVL_OPTION_VALUE: i32 = -12;
 const TCP_USER_TIMEOUT_OPTION_VALUE: i32 = -13;
 
-fn make_tcp_stream() -> TcpStream {
-    let listener = TcpListener::bind(&SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 0)).unwrap();
-    let fd = listener.into_raw_fd();
-    // Making a `TcpStream` by converting the listener instead of `TcpStream::connect`ing to it
-    // allows implementing less of fuchsia.posix.socket in the stub (notably, `GetSockName`).
-    // Safe because the fd is owned.
-    unsafe { TcpStream::from_raw_fd(fd) }
+fn with_tcp_stream(f: impl FnOnce(TcpStream) -> ()) {
+    let (client, server) = fidl::endpoints::create_endpoints::<StreamSocketMarker>()
+        .expect("create stream socket endpoints");
+
+    // fdio::create_fd isn't async, so we need a dedicated thread for FIDL dispatch.
+    let handle = thread::spawn(|| {
+        fasync::Executor::new().expect("new executor").run_singlethreaded(
+            server.into_stream().expect("endpoint into stream").for_each(move |request| {
+                future::ready(match request.expect("stream socket request stream") {
+                    StreamSocketRequest::Describe { responder } => {
+                        let (s0, _s1) =
+                            zx::Socket::create(zx::SocketOpts::STREAM).expect("create zx socket");
+                        let () = responder
+                            .send(&mut NodeInfo::StreamSocket(StreamSocket { socket: s0 }))
+                            .expect("send Describe response");
+                    }
+                    StreamSocketRequest::GetSockOpt { level, optname, responder } => {
+                        let () = assert_eq!(i32::from(level), libc::IPPROTO_TCP);
+                        let option_value = match i32::from(optname) {
+                            libc::TCP_KEEPCNT => TCP_KEEPCNT_OPTION_VALUE,
+                            libc::TCP_KEEPINTVL => TCP_KEEPINTVL_OPTION_VALUE,
+                            libc::TCP_USER_TIMEOUT => TCP_USER_TIMEOUT_OPTION_VALUE,
+                            optname => panic!("unhandled GetSockOpt option name: {}", optname),
+                        };
+                        let () = responder
+                            .send(&mut Ok(option_value.to_le_bytes().to_vec()))
+                            .expect("send GetSockOpt response");
+                    }
+                    request => panic!("unhandled StreamSocketRequest: {:?}", request),
+                })
+            }),
+        )
+    });
+    let () = f(fdio::create_fd(client.into_handle()).expect("endpoint into handle"));
+    handle.join().expect("thread join")
 }
 
 #[test]
 fn keepalive_interval_errors_on_negative_duration() {
-    let stream = make_tcp_stream();
-    assert_matches!(
-        stream.keepalive_interval(),
-        Err(tcp_stream_ext::Error::NegativeDuration(TCP_KEEPINTVL_OPTION_VALUE))
-    );
+    with_tcp_stream(|stream| {
+        assert_matches!(
+            stream.keepalive_interval(),
+            Err(tcp_stream_ext::Error::NegativeDuration(TCP_KEEPINTVL_OPTION_VALUE))
+        )
+    })
 }
 
 #[test]
 fn user_timeout_errors_on_negative_duration() {
-    let stream = make_tcp_stream();
-    assert_matches!(
-        stream.user_timeout(),
-        Err(tcp_stream_ext::Error::NegativeDuration(TCP_USER_TIMEOUT_OPTION_VALUE))
-    );
+    with_tcp_stream(|stream| {
+        assert_matches!(
+            stream.user_timeout(),
+            Err(tcp_stream_ext::Error::NegativeDuration(TCP_USER_TIMEOUT_OPTION_VALUE))
+        )
+    })
 }
 
 #[test]
 fn keepalive_count() {
-    let stream = make_tcp_stream();
-    assert_matches!(stream.keepalive_count(), Ok(TCP_KEEPCNT_OPTION_VALUE));
+    with_tcp_stream(|stream| {
+        assert_matches!(stream.keepalive_count(), Ok(TCP_KEEPCNT_OPTION_VALUE))
+    })
 }
