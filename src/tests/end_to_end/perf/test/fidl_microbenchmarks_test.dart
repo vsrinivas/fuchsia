c426e38f7132046350647c618036b0beaec472c8
Mark Seaborn <mseaborn@google.com>
2020-06-18 19:06:01 +0000

    [benchmarks] Shard fidl_microbenchmarks_test
    This executable currently takes ~7.5 minutes to run in infrastructure.
    We want each test executable that runs in CQ to run reliably in <5
    minutes.  This makes it much less likely that we'll hit sharding task
    level timeouts, which are hard to debug.
    
    Based on CL by garymm@:
    https://fuchsia-review.googlesource.com/c/fuchsia/+/399541
    
    Bug: 54144
    Test: fidl_microbenchmarks_test_shard_{0,1} on terminal-x64-release on CQ
    Change-Id: I3f2d29db7131db67552fb86d540218b25bf917d1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/400014
    Reviewed-by: Gary Miguel <garymm@google.com>
    Reviewed-by: Benjamin Prosnitz <bprosnitz@google.com>
    Commit-Queue: Gary Miguel <garymm@google.com>
    Testability-Review: Benjamin Prosnitz <bprosnitz@google.com>

--- src/tests/end_to_end/perf/test/fidl_microbenchmarks_test.dart
+++ src/tests/end_to_end/perf/test/fidl_microbenchmarks_test.dart
@@ -2,9 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:args/args.dart';
 import 'package:test/test.dart';
 
 import 'helpers.dart';
+
+var _tests = [];
 
 String tmpPerfResultsJson(String benchmarkBinary) {
   return '/tmp/perf_results_$benchmarkBinary.json';
@@ -15,15 +18,17 @@ void runFidlBenchmark(String benchmarkBinary, [String optArgs]) {
   final path = (optArgs == null)
       ? '/bin/$benchmarkBinary -p --quiet --out $resultsFile'
       : '/bin/$benchmarkBinary $optArgs';
-  test(benchmarkBinary, () async {
-    final helper = await PerfTestHelper.make();
-    final result = await helper.sl4fDriver.ssh.run(path);
-    expect(result.exitCode, equals(0));
-    await helper.processResults(resultsFile);
-  }, timeout: Timeout.none);
+  _tests.add(() {
+    test(benchmarkBinary, () async {
+      final helper = await PerfTestHelper.make();
+      final result = await helper.sl4fDriver.ssh.run(path);
+      expect(result.exitCode, equals(0));
+      await helper.processResults(resultsFile);
+    }, timeout: Timeout.none);
+  });
 }
 
-void main() {
+void main(List<String> args) {
   enableLoggingOutput();
 
   runFidlBenchmark('go_fidl_microbenchmarks',
@@ -38,15 +43,36 @@ void main() {
   runFidlBenchmark('walker_fidl_microbenchmarks');
   runFidlBenchmark('cpp_allocation_strategy_fidl_microbenchmarks');
 
-  test('dart_fidl_microbenchmarks', () async {
-    final helper = await PerfTestHelper.make();
-    const resultsFile =
-        '/data/r/sys/r/fidl_benchmarks/fuchsia.com:dart_fidl_benchmarks:0#meta:dart_fidl_benchmarks.cmx/results.json';
-    const command = 'run-test-component --realm-label=fidl_benchmarks '
-        'fuchsia-pkg://fuchsia.com/dart_fidl_benchmarks#meta/dart_fidl_benchmarks.cmx '
-        '--out_file /data/results.json';
-    final result = await helper.sl4fDriver.ssh.run(command);
-    expect(result.exitCode, equals(0));
-    await helper.processResults(resultsFile);
-  }, timeout: Timeout.none);
+  _tests.add(() {
+    test('dart_fidl_microbenchmarks', () async {
+      final helper = await PerfTestHelper.make();
+      const resultsFile =
+          '/data/r/sys/r/fidl_benchmarks/fuchsia.com:dart_fidl_benchmarks:0#meta:dart_fidl_benchmarks.cmx/results.json';
+      const command = 'run-test-component --realm-label=fidl_benchmarks '
+          'fuchsia-pkg://fuchsia.com/dart_fidl_benchmarks#meta/dart_fidl_benchmarks.cmx '
+          '--out_file /data/results.json';
+      final result = await helper.sl4fDriver.ssh.run(command);
+      expect(result.exitCode, equals(0));
+      await helper.processResults(resultsFile);
+    }, timeout: Timeout.none);
+  });
+
+  // The Dart test library is supposed to support sharding, but its
+  // sharding options do not seem to be accessible when running Dart tests
+  // on Fuchsia, so we reimplement the same options here.
+  final parser = ArgParser()
+    ..addOption('total-shards',
+        help: 'Number of total shards to split test suites into.',
+        defaultsTo: '1')
+    ..addOption('shard-index',
+        help: 'Which shard of test suites to run.', defaultsTo: '0');
+  final argResults = parser.parse(args);
+
+  int totalShards = int.parse(argResults['total-shards']);
+  int shardIndex = int.parse(argResults['shard-index']);
+  for (var i = 0; i < _tests.length; i++) {
+    if (i % totalShards == shardIndex) {
+      _tests[i]();
+    }
+  }
 }
