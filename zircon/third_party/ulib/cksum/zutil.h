e00ab7fbe20c482152158fa49708dbaeb3abb8d1
Joshua Seaton <joshuaseaton@google.com>
2020-06-19 22:11:55 +0000

    [zircon][cksum] Update cksum library for kernel compatibility
    Also, fix header guards.
    
    Change-Id: I3d0c0b32b043c34b6ec0a035a17573b715d24dcb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/400843
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>

--- zircon/third_party/ulib/cksum/zutil.h
+++ zircon/third_party/ulib/cksum/zutil.h
@@ -1,14 +1,13 @@
-#ifndef __ZUTIL_H
-#define __ZUTIL_H
+
+#ifndef ZIRCON_THIRD_PARTY_ULIB_CKSUM_ZUTIL_H_
+#define ZIRCON_THIRD_PARTY_ULIB_CKSUM_ZUTIL_H_
 
 /* necessary stuff to transplant crc32 and adler32 from zlib */
 #include <inttypes.h>
 #include <stddef.h>
-#include <sys/types.h>
 
 #define Z_NULL NULL
 #define OF(args) args
 #define ZEXPORT
 
-#endif
-
+#endif  // ZIRCON_THIRD_PARTY_ULIB_CKSUM_ZUTIL_H_
