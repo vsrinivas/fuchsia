67a6d40d5b51d303e2f9f158d94959fde572a995
Adam Lesinski <adamlesinski@google.com>
2020-06-20 01:16:46 +0000

    [tools][affected_targets] Add support for more file types
    Added support for 'c', 'cpp', 'BUILD.gn', 'gni', and 'fidl'
    
    Rust file type parsing support was added, but `affected_targets`
    will treat it as an unsupported file type.
    
    Change-Id: I7815414107432ae6ddbf29744feccbedb48de608
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/400936
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Testability-Review: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Kevin Lindkvist <lindkvist@google.com>

--- tools/affected_targets/src/files.rs
+++ tools/affected_targets/src/files.rs
@@ -2,50 +2,51 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use {
-    anyhow::Error,
-    std::{ffi::OsStr, path::Path, str::FromStr},
-};
+use std::{convert::Infallible, ffi::OsStr, path::Path, str::FromStr};
 
 /// A `FileType` represents a type of file. The type is determined based on
 /// the file extension of the string with which the `FileType` is constructed.
 #[derive(PartialEq, Debug)]
 pub enum FileType {
     Cpp,
+    Fidl,
+    Gn,
+    Rust,
     Unknown(String),
 }
 
+impl FileType {
+    /// Returns whether the file type is supported by `affected_targets`.
+    pub fn supported(&self) -> bool {
+        match self {
+            FileType::Cpp | FileType::Fidl | FileType::Gn => true,
+            _ => false,
+        }
+    }
+}
+
 impl FromStr for FileType {
-    type Err = Error;
+    type Err = Infallible;
 
     fn from_str(file_string: &str) -> Result<Self, Self::Err> {
-        let file_type = match &Path::new(file_string).extension().and_then(OsStr::to_str) {
-            Some("h") | Some("cc") => FileType::Cpp,
-            Some(extension) => FileType::Unknown(extension.to_string()),
+        let path = Path::new(file_string);
+        if let Some(filename) = path.file_name() {
+            if filename == "BUILD.gn" {
+                return Ok(FileType::Gn);
+            }
+        }
+        let file_type = match &path.extension().and_then(OsStr::to_str) {
+            Some(extension) => match *extension {
+                "h" | "c" | "cc" | "cpp" => FileType::Cpp,
+                "fidl" => FileType::Fidl,
+                "gni" => FileType::Gn,
+                "rs" => FileType::Rust,
+                ext => FileType::Unknown(ext.to_string()),
+            },
             None => FileType::Unknown("".to_string()),
         };
         Ok(file_type)
     }
-}
-
-/// Returns `true` iff all `files` are of the same type, as determined by their extension.
-///
-/// For example, if all files have either a ".h" or ".cc" file extension, they are all
-/// considered to be `FileType::Cpp` and `are_all_files_of_type` will return `true`.alloc
-///
-/// For `FileType::Unknown`, the associated string will be used to check for equality. That
-/// means that for unsupported file types, all files will be considered the same if they share
-/// the same file extension, or if none of the files have an extension.
-pub fn are_all_files_of_type<I, S>(files: I, file_type: FileType) -> bool
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<str>,
-{
-    !files
-        .into_iter()
-        .map(|file| FileType::from_str(file.as_ref()))
-        .filter_map(Result::ok) // FileType::from_str only returns Ok, so this filter is fine.
-        .any(|parsed_file_type| parsed_file_type != file_type)
 }
 
 /// Returns true if all `files` are of a type supported by `affected_targets`.
@@ -58,36 +59,61 @@ where
     I: IntoIterator<Item = S>,
     S: AsRef<str>,
 {
-    are_all_files_of_type(files, FileType::Cpp)
+    files.into_iter().map(|file| FileType::from_str(file.as_ref()).unwrap()).all(|f| f.supported())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use {super::*, matches::assert_matches};
 
     #[test]
-    fn test_all_files_same_type_cpp() {
-        assert_eq!(are_all_files_of_type(&["test.h", "test.cc"], FileType::Cpp), true);
+    fn cpp_files_parsed_correctly() {
+        assert_matches!(FileType::from_str("this/test.h"), Ok(FileType::Cpp));
+        assert_matches!(FileType::from_str("test.c"), Ok(FileType::Cpp));
+        assert_matches!(FileType::from_str("test.cc"), Ok(FileType::Cpp));
+        assert_matches!(FileType::from_str("test.cpp"), Ok(FileType::Cpp));
     }
 
     #[test]
-    fn test_different_types_cpp() {
-        assert_eq!(are_all_files_of_type(&["test.foo", "test.cc"], FileType::Cpp), false);
-    }
-
-    #[test]
-    fn test_all_files_same_type_unknown() {
-        assert_eq!(
-            are_all_files_of_type(&["test.foo", "test.foo"], FileType::Unknown("foo".to_string())),
-            true
+    fn fidl_files_parsed_correctly() {
+        assert_matches!(
+            FileType::from_str("//sdk/fidl/fuchsia.sample/protocol.fidl"),
+            Ok(FileType::Fidl)
         );
     }
 
     #[test]
-    fn test_different_types_unknown() {
-        assert_eq!(
-            are_all_files_of_type(&["test.foo", "test.bar"], FileType::Unknown("foo".to_string())),
-            false
-        );
+    fn gn_files_parsed_correctly() {
+        assert_matches!(FileType::from_str("//foo/bar/BUILD.gn"), Ok(FileType::Gn));
+        assert_matches!(FileType::from_str("//build/rust/rustc_binary.gni"), Ok(FileType::Gn));
+        assert_matches!(FileType::from_str("//foo/barBUILD.gn"), Ok(FileType::Unknown(_)));
+        assert_matches!(FileType::from_str("//foo/bar.gn"), Ok(FileType::Unknown(_)));
+    }
+
+    #[test]
+    fn rust_files_parsed_correctly() {
+        assert_matches!(FileType::from_str("//this/test.rs"), Ok(FileType::Rust));
+    }
+
+    #[test]
+    fn no_extension_parsed_correctly() {
+        assert_matches!(FileType::from_str("//this/test"), Ok(FileType::Unknown(ext)) if ext == "");
+    }
+
+    #[test]
+    fn unrecognized_extension_parsed_correctly() {
+        assert_matches!(FileType::from_str("//test.foo"), Ok(FileType::Unknown(ext)) if ext == "foo");
+    }
+
+    #[test]
+    fn test_file_types_are_supported() {
+        assert!(file_types_are_supported(&["test.h", "BUILD.gn", "test.fidl"]));
+        assert!(!file_types_are_supported(&[
+            "test.h",
+            "BUILD.gn",
+            "test.fidl",
+            "test.unsupported"
+        ]));
+        assert!(!file_types_are_supported(&["test.h", "BUILD.gn", "test.fidl", "no_extension"]));
     }
 }
