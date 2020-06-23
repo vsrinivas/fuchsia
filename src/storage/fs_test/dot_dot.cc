ae38485d1644a72b7206af72873fe48fdd2b22e5
Chris Suter <csuter@google.com>
2020-06-23 01:42:18 +0000

    [storage] Move dot_dot tests to new fs_test framework.
    Bug: 52611
    Change-Id: If43b4e3bcc2c058f164b7be9ff0bec4ec9d8816d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/397861
    Testability-Review: Chris Suter <csuter@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>
    Reviewed-by: Stephen Demos <sdemos@google.com>

--- src/storage/fs_test/dot_dot.cc
+++ src/storage/fs_test/dot_dot.cc
@@ -10,133 +10,133 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <zircon/compiler.h>
 
-#include "filesystems.h"
-#include "misc.h"
+#include "src/storage/fs_test/fs_test_fixture.h"
+#include "src/storage/fs_test/misc.h"
+
+namespace fs_test {
+namespace {
+
+using DotDotTest = FileSystemTest;
 
 // Test cases of '..' where the path can be canonicalized on the client.
-bool test_dot_dot_client(void) {
-  BEGIN_TEST;
-  ASSERT_EQ(mkdir("::foo", 0755), 0, "");
-  ASSERT_EQ(mkdir("::foo/bit", 0755), 0, "");
-  ASSERT_EQ(mkdir("::foo/bar", 0755), 0, "");
-  ASSERT_EQ(mkdir("::foo/bar/baz", 0755), 0, "");
+TEST_P(DotDotTest, DotDotClient) {
+  ASSERT_EQ(mkdir(GetPath("foo").c_str(), 0755), 0);
+  ASSERT_EQ(mkdir(GetPath("foo/bit").c_str(), 0755), 0);
+  ASSERT_EQ(mkdir(GetPath("foo/bar").c_str(), 0755), 0);
+  ASSERT_EQ(mkdir(GetPath("foo/bar/baz").c_str(), 0755), 0);
 
-  expected_dirent_t foo_dir[] = {
-      {false, ".", DT_DIR},
-      {false, "bar", DT_DIR},
-      {false, "bit", DT_DIR},
+  ExpectedDirectoryEntry foo_dir[] = {
+      {".", DT_DIR},
+      {"bar", DT_DIR},
+      {"bit", DT_DIR},
   };
 
-  expected_dirent_t bar_dir[] = {
-      {false, ".", DT_DIR},
-      {false, "baz", DT_DIR},
+  ExpectedDirectoryEntry bar_dir[] = {
+      {".", DT_DIR},
+      {"baz", DT_DIR},
   };
 
   // Test cases of client-side dot-dot when moving between directories.
-  DIR* dir = opendir("::foo/bar/..");
-  ASSERT_NONNULL(dir, "");
-  ASSERT_TRUE(fcheck_dir_contents(dir, foo_dir, countof(foo_dir)), "");
-  ASSERT_EQ(closedir(dir), 0, "");
+  DIR* dir = opendir(GetPath("foo/bar/..").c_str());
+  ASSERT_NE(dir, nullptr);
+  ASSERT_NO_FATAL_FAILURE(CheckDirectoryContents(dir, foo_dir));
+  ASSERT_EQ(closedir(dir), 0);
 
-  dir = opendir("::foo/bar/../bit/..//././//");
-  ASSERT_NONNULL(dir, "");
-  ASSERT_TRUE(fcheck_dir_contents(dir, foo_dir, countof(foo_dir)), "");
-  ASSERT_EQ(closedir(dir), 0, "");
+  dir = opendir(GetPath("foo/bar/../bit/..//././//").c_str());
+  ASSERT_NE(dir, nullptr);
+  ASSERT_NO_FATAL_FAILURE(CheckDirectoryContents(dir, foo_dir));
+  ASSERT_EQ(closedir(dir), 0);
 
-  dir = opendir("::foo/bar/baz/../../../foo/bar/baz/..");
-  ASSERT_NONNULL(dir, "");
-  ASSERT_TRUE(fcheck_dir_contents(dir, bar_dir, countof(bar_dir)), "");
-  ASSERT_EQ(closedir(dir), 0, "");
+  dir = opendir(GetPath("foo/bar/baz/../../../foo/bar/baz/..").c_str());
+  ASSERT_NE(dir, nullptr);
+  ASSERT_NO_FATAL_FAILURE(CheckDirectoryContents(dir, bar_dir));
+  ASSERT_EQ(closedir(dir), 0);
 
   // Clean up
-  ASSERT_EQ(unlink("::foo/bar/baz"), 0, "");
-  ASSERT_EQ(unlink("::foo/bar"), 0, "");
-  ASSERT_EQ(unlink("::foo/bit"), 0, "");
-  ASSERT_EQ(unlink("::foo"), 0, "");
-  END_TEST;
+  ASSERT_EQ(unlink(GetPath("foo/bar/baz").c_str()), 0);
+  ASSERT_EQ(unlink(GetPath("foo/bar").c_str()), 0);
+  ASSERT_EQ(unlink(GetPath("foo/bit").c_str()), 0);
+  ASSERT_EQ(unlink(GetPath("foo").c_str()), 0);
 }
 
 // Test cases of '..' where the path cannot be canonicalized on the client.
-bool test_dot_dot_server(void) {
-  BEGIN_TEST;
-  ASSERT_EQ(mkdir("::foo", 0755), 0, "");
-  ASSERT_EQ(mkdir("::foo/bar", 0755), 0, "");
+TEST_P(DotDotTest, DotDotServer) {
+  ASSERT_EQ(mkdir(GetPath("foo").c_str(), 0755), 0);
+  ASSERT_EQ(mkdir(GetPath("foo/bar").c_str(), 0755), 0);
 
-  int foo_fd = open("::foo", O_RDONLY | O_DIRECTORY);
-  ASSERT_GT(foo_fd, 0, "");
+  int foo_fd = open(GetPath("foo").c_str(), O_RDONLY | O_DIRECTORY);
+  ASSERT_GT(foo_fd, 0);
 
   // ".." from foo --> Not Supported
-  ASSERT_LT(openat(foo_fd, "..", O_RDONLY | O_DIRECTORY), 0, "");
+  ASSERT_LT(openat(foo_fd, "..", O_RDONLY | O_DIRECTORY), 0);
 
   // "bar/../.." from foo --> Not supported
-  ASSERT_LT(openat(foo_fd, "bar/../..", O_RDONLY | O_DIRECTORY), 0, "");
+  ASSERT_LT(openat(foo_fd, "bar/../..", O_RDONLY | O_DIRECTORY), 0);
 
   // "../../../../../bar" -->  Not supported
-  ASSERT_LT(openat(foo_fd, "../../../../../bar", O_RDONLY | O_DIRECTORY), 0, "");
+  ASSERT_LT(openat(foo_fd, "../../../../../bar", O_RDONLY | O_DIRECTORY), 0);
 
   // Try to create a file named '..'
-  ASSERT_LT(openat(foo_fd, "..", O_RDWR | O_CREAT), 0, "");
-  ASSERT_LT(openat(foo_fd, ".", O_RDWR | O_CREAT), 0, "");
+  ASSERT_LT(openat(foo_fd, "..", O_RDWR | O_CREAT), 0);
+  ASSERT_LT(openat(foo_fd, ".", O_RDWR | O_CREAT), 0);
 
   // Try to create a directory named '..'
-  ASSERT_LT(mkdirat(foo_fd, "..", 0666), 0, "");
-  ASSERT_LT(mkdirat(foo_fd, ".", 0666), 0, "");
+  ASSERT_LT(mkdirat(foo_fd, "..", 0666), 0);
+  ASSERT_LT(mkdirat(foo_fd, ".", 0666), 0);
 
   // Clean up
-  ASSERT_EQ(close(foo_fd), 0, "");
-  ASSERT_EQ(unlink("::foo/bar"), 0, "");
-  ASSERT_EQ(unlink("::foo"), 0, "");
-  END_TEST;
+  ASSERT_EQ(close(foo_fd), 0);
+  ASSERT_EQ(unlink(GetPath("foo/bar").c_str()), 0);
+  ASSERT_EQ(unlink(GetPath("foo").c_str()), 0);
 }
 
 // Test cases of '..' which operate on multiple paths.
 // This is mostly intended to test other pathways for client-side
 // cleaning operations.
-bool test_dot_dot_rename(void) {
-  BEGIN_TEST;
-  ASSERT_EQ(mkdir("::foo", 0755), 0, "");
-  ASSERT_EQ(mkdir("::foo/bit", 0755), 0, "");
-  ASSERT_EQ(mkdir("::foo/bar", 0755), 0, "");
-  ASSERT_EQ(mkdir("::foo/bar/baz", 0755), 0, "");
+TEST_P(DotDotTest, DotDotRename) {
+  ASSERT_EQ(mkdir(GetPath("foo").c_str(), 0755), 0);
+  ASSERT_EQ(mkdir(GetPath("foo/bit").c_str(), 0755), 0);
+  ASSERT_EQ(mkdir(GetPath("foo/bar").c_str(), 0755), 0);
+  ASSERT_EQ(mkdir(GetPath("foo/bar/baz").c_str(), 0755), 0);
 
-  expected_dirent_t foo_dir_bit[] = {
-      {false, ".", DT_DIR},
-      {false, "bar", DT_DIR},
-      {false, "bit", DT_DIR},
+  ExpectedDirectoryEntry foo_dir_bit[] = {
+      {".", DT_DIR},
+      {"bar", DT_DIR},
+      {"bit", DT_DIR},
   };
 
-  expected_dirent_t foo_dir_bits[] = {
-      {false, ".", DT_DIR},
-      {false, "bar", DT_DIR},
-      {false, "bits", DT_DIR},
+  ExpectedDirectoryEntry foo_dir_bits[] = {
+      {".", DT_DIR},
+      {"bar", DT_DIR},
+      {"bits", DT_DIR},
   };
 
   // Check that the source is cleaned
-  ASSERT_EQ(rename("::foo/bar/./../bit/./../bit", "::foo/bits"), 0, "");
-  ASSERT_TRUE(check_dir_contents("::foo", foo_dir_bits, countof(foo_dir_bits)), "");
+  ASSERT_EQ(rename(GetPath("foo/bar/./../bit/./../bit").c_str(), GetPath("foo/bits").c_str()), 0);
+  ASSERT_NO_FATAL_FAILURE(CheckDirectoryContents(GetPath("foo").c_str(), foo_dir_bits));
 
   // Check that the destination is cleaned
-  ASSERT_EQ(rename("::foo/bits", "::foo/bar/baz/../../././bit"), 0, "");
-  ASSERT_TRUE(check_dir_contents("::foo", foo_dir_bit, countof(foo_dir_bit)), "");
+  ASSERT_EQ(rename(GetPath("foo/bits").c_str(), GetPath("foo/bar/baz/../../././bit").c_str()), 0);
+  ASSERT_NO_FATAL_FAILURE(CheckDirectoryContents(GetPath("foo").c_str(), foo_dir_bit));
 
   // Check that both are cleaned
-  ASSERT_EQ(rename("::foo/bar/../bit/.", "::foo/bar/baz/../../././bits"), 0, "");
-  ASSERT_TRUE(check_dir_contents("::foo", foo_dir_bits, countof(foo_dir_bits)), "");
+  ASSERT_EQ(rename(GetPath("foo/bar/../bit/.").c_str(), GetPath("foo/bar/baz/../../././bits").c_str()), 0);
+  ASSERT_NO_FATAL_FAILURE(CheckDirectoryContents(GetPath("foo").c_str(), foo_dir_bits));
 
   // Check that both are cleaned (including trailing '/')
-  ASSERT_EQ(rename("::foo/./bar/../bits/", "::foo/bar/baz/../../././bit/.//"), 0, "");
-  ASSERT_TRUE(check_dir_contents("::foo", foo_dir_bit, countof(foo_dir_bit)), "");
+  ASSERT_EQ(rename(GetPath("foo/./bar/../bits/").c_str(), GetPath("foo/bar/baz/../../././bit/.//").c_str()), 0);
+  ASSERT_NO_FATAL_FAILURE(CheckDirectoryContents(GetPath("foo").c_str(), foo_dir_bit));
 
   // Clean up
-  ASSERT_EQ(unlink("::foo/bar/baz"), 0, "");
-  ASSERT_EQ(unlink("::foo/bar"), 0, "");
-  ASSERT_EQ(unlink("::foo/bit"), 0, "");
-  ASSERT_EQ(unlink("::foo"), 0, "");
-  END_TEST;
+  ASSERT_EQ(unlink(GetPath("foo/bar/baz").c_str()), 0);
+  ASSERT_EQ(unlink(GetPath("foo/bar").c_str()), 0);
+  ASSERT_EQ(unlink(GetPath("foo/bit").c_str()), 0);
+  ASSERT_EQ(unlink(GetPath("foo").c_str()), 0);
 }
 
-RUN_FOR_ALL_FILESYSTEMS(dot_dot_tests,
-                        RUN_TEST_MEDIUM(test_dot_dot_client) RUN_TEST_MEDIUM(test_dot_dot_server)
-                            RUN_TEST_MEDIUM(test_dot_dot_rename))
+INSTANTIATE_TEST_SUITE_P(/*no prefix*/, DotDotTest, testing::ValuesIn(AllTestFileSystems()),
+                         testing::PrintToStringParamName());
+
+}  // namespace
+}  // namespace fs_test
