88794b9af99bf7206ad31bf3bfaad1c2623a1ca1
Mark Seaborn <mseaborn@google.com>
2020-06-24 05:53:40 +0000

    [utest][errno] Convert test to use zxtest instead of unittest
    The code changes were automated.
    
    Bug: 51652
    Test: runs on CQ, but tested locally with:
      fx set bringup.x64 --with-base //garnet/packages/tests:zircon --with-base //bundles/bringup:tests
      fx build
      fx qemu -k -c zircon.autorun.boot=/boot/test/sys/errno-test
    Change-Id: I76d6c8d3fe9db4910f67b1f0b3206a8177e3c9e5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/398561
    Reviewed-by: George Kulakowski <kulakowski@google.com>
    Testability-Review: George Kulakowski <kulakowski@google.com>
    Commit-Queue: Mark Seaborn <mseaborn@google.com>

--- zircon/system/utest/errno/errno.c
+++ zircon/system/utest/errno/errno.c
@@ -8,41 +8,34 @@
 #include <stdio.h>
 #include <zircon/syscalls.h>
 
-#include <unittest/unittest.h>
+#include <zxtest/zxtest.h>
 
 static void* do_test(void* arg) {
   int thread_no = *(int*)arg;
-  unittest_printf("do_test for thread: %d\n", thread_no);
+  printf("do_test for thread: %d\n", thread_no);
   errno = -thread_no;
   zx_nanosleep(zx_deadline_after(ZX_MSEC(300)));
-  unittest_printf("comparing result for: %d\n", thread_no);
+  printf("comparing result for: %d\n", thread_no);
   EXPECT_EQ(errno, -thread_no, "Incorrect errno for this thread");
   return NULL;
 }
 
-bool errno_test(void) {
-  BEGIN_TEST;
+TEST(ErrnoTests, errno_test) {
   int main_thread = 1, thread_1 = 2, thread_2 = 3;
 
   pthread_t thread2, thread3;
 
-  unittest_printf("creating thread: %d\n", thread_1);
+  printf("creating thread: %d\n", thread_1);
   pthread_create(&thread2, NULL, do_test, &thread_1);
 
-  unittest_printf("creating thread: %d\n", thread_2);
+  printf("creating thread: %d\n", thread_2);
   pthread_create(&thread3, NULL, do_test, &thread_2);
 
   do_test(&main_thread);
 
-  unittest_printf("joining thread: %d\n", thread_1);
+  printf("joining thread: %d\n", thread_1);
   pthread_join(thread2, NULL);
 
-  unittest_printf("joining thread: %d\n", thread_2);
+  printf("joining thread: %d\n", thread_2);
   pthread_join(thread3, NULL);
-
-  END_TEST;
 }
-
-BEGIN_TEST_CASE(errno_tests)
-RUN_TEST(errno_test)
-END_TEST_CASE(errno_tests)
