017665f8af9806a0e201ef1da707f8a0ecfa880b
Craig Tiller <craig.tiller@gmail.com>
2020-06-25 17:37:15 +0000

    [rust][fasync] Make this crate compile on host
    Change-Id: If2a5a3ed94be335bd0ed13b83a4d4f401bc0a860
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/402014
    Reviewed-by: Tyler Mandry <tmandry@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Rich Kadel <richkadel@google.com>
    Commit-Queue: Craig Tiller <ctiller@google.com>
    Testability-Review: Adam Perry <adamperry@google.com>
    Testability-Review: Rich Kadel <richkadel@google.com>

--- src/lib/fuchsia-async/src/lib.rs
+++ src/lib/fuchsia-async/src/lib.rs
@@ -9,25 +9,42 @@
 /// A future which can be used by multiple threads at once.
 pub mod atomic_future;
 
+#[cfg(target_os = "fuchsia")]
 mod channel;
+#[cfg(target_os = "fuchsia")]
 pub use self::channel::{Channel, RecvMsg};
+#[cfg(target_os = "fuchsia")]
 mod on_signals;
+#[cfg(target_os = "fuchsia")]
 pub use self::on_signals::OnSignals;
+#[cfg(target_os = "fuchsia")]
 mod rwhandle;
+#[cfg(target_os = "fuchsia")]
 pub use self::rwhandle::RWHandle;
+#[cfg(target_os = "fuchsia")]
 mod socket;
+#[cfg(target_os = "fuchsia")]
 pub use self::socket::Socket;
+#[cfg(target_os = "fuchsia")]
 mod timer;
+#[cfg(target_os = "fuchsia")]
 pub use self::timer::{Interval, OnTimeout, TimeoutExt, Timer, WakeupTime};
+#[cfg(target_os = "fuchsia")]
 mod executor;
+#[cfg(target_os = "fuchsia")]
 pub use self::executor::{
     spawn, spawn_local, DurationExt, EHandle, Executor, PacketReceiver, ReceiverRegistration, Time,
     WaitState,
 };
+#[cfg(target_os = "fuchsia")]
 mod fifo;
+#[cfg(target_os = "fuchsia")]
 pub use self::fifo::{Fifo, FifoEntry, FifoReadable, FifoWritable, ReadEntry, WriteEntry};
+#[cfg(target_os = "fuchsia")]
 pub mod net;
+#[cfg(target_os = "fuchsia")]
 mod task;
+#[cfg(target_os = "fuchsia")]
 pub use self::task::Task;
 
 // Re-export pin_mut as its used by the async proc macros
