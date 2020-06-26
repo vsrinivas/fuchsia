083335fa2eea1d10611460898b16991ee4ceed52
Francois Rousseau <frousseau@google.com>
2020-06-26 01:23:17 +0000

    [bugreport] remove command line option parsing
    * the command doesn't actually take any parameters so might as well not
      have a help and save us all that parsing
    
    Change-Id: I94ea6c789b16c929385389d37ec0f484a87b79cc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/402711
    Testability-Review: Francois Rousseau <frousseau@google.com>
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>
    Commit-Queue: Francois Rousseau <frousseau@google.com>

--- src/developer/forensics/bugreport/main.cc
+++ src/developer/forensics/bugreport/main.cc
@@ -4,27 +4,12 @@
 
 #include <lib/sys/cpp/component_context.h>
 
-#include <cstdio>
 #include <cstdlib>
-#include <set>
-#include <string>
 
 #include "src/developer/forensics/bugreport/bug_reporter.h"
-#include "src/developer/forensics/bugreport/command_line_options.h"
 
 int main(int argc, char** argv) {
-  using namespace ::forensics::bugreport;
-
-  const Mode mode = ParseModeFromArgcArgv(argc, argv);
-  std::set<std::string> attachment_allowlist;
-  switch (mode) {
-    case Mode::FAILURE:
-      return EXIT_FAILURE;
-    case Mode::HELP:
-      return EXIT_SUCCESS;
-    case Mode::DEFAULT:
-      auto environment_services = sys::ServiceDirectory::CreateFromNamespace();
-
-      return MakeBugReport(environment_services) ? EXIT_SUCCESS : EXIT_FAILURE;
-  }
+  return ::forensics::bugreport::MakeBugReport(sys::ServiceDirectory::CreateFromNamespace())
+             ? EXIT_SUCCESS
+             : EXIT_FAILURE;
 }
