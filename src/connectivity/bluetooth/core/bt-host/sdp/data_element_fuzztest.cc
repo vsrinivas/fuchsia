823f180db8ced2a34c014e606f324792277462cd
Yu Shan <shanyu@google.com>
2020-06-27 00:09:44 +0000

    [bluetooth] Add a fuzzer for data element parsing.
    Tested: Run the fuzzer manually.
    Change-Id: I9279cc64fd23f34bc990e8240b54378d89ec94ab
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/401307
    Commit-Queue: Yu Shan <shanyu@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Testability-Review: Ben Lawson <benlawson@google.com>

---
+++ src/connectivity/bluetooth/core/bt-host/sdp/data_element_fuzztest.cc
@@ -0,0 +1,29 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "src/connectivity/bluetooth/core/bt-host/common/log.h"
+#include "src/connectivity/bluetooth/core/bt-host/sdp/data_element.h"
+
+// Prevent "undefined symbol: __zircon_driver_rec__" error.
+BT_DECLARE_FAKE_DRIVER();
+
+namespace bt {
+namespace sdp {
+
+void fuzz(const uint8_t* data, size_t size) {
+  DynamicByteBuffer buf(size);
+  memcpy(buf.mutable_data(), data, size);
+  DataElement elem;
+  DataElement::Read(&elem, buf);
+}
+
+}  // namespace sdp
+}  // namespace bt
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  bt::sdp::fuzz(data, size);
+  return 0;
+}
