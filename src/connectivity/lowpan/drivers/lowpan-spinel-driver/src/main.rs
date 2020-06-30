cd9a716daeed66fdd79a50f720f0ba6bf8360ffc
Robert Quattlebaum <rquattle@google.com>
2020-06-30 19:59:50 +0000

    [lowpan][lowpan-spinel-driver] Fix for opening Spinel radio device
    This change updates how the spinel driver protocol is obtained
    to be in line with <fxb/44998>.
    
    Change-Id: Ifc580d695827364c9bf882d2ce28990ec92b714e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/401677
    Commit-Queue: Robert Quattlebaum <rquattle@google.com>
    Reviewed-by: Jiaming (Charlie) Wang <jiamingw@google.com>
    Testability-Review: Jiaming (Charlie) Wang <jiamingw@google.com>

--- src/connectivity/lowpan/drivers/lowpan-spinel-driver/src/main.rs
+++ src/connectivity/lowpan/drivers/lowpan-spinel-driver/src/main.rs
@@ -43,6 +43,7 @@ use anyhow::Error;
 use fidl_fuchsia_lowpan_device::{RegisterMarker, RegisterProxyInterface};
 use fidl_fuchsia_lowpan_spinel::{
     DeviceMarker as SpinelDeviceMarker, DeviceProxy as SpinelDeviceProxy,
+    DeviceSetupProxy as SpinelDeviceSetupProxy,
 };
 use fuchsia_component::client::connect_to_service_at;
 use lowpan_driver_common::register_and_serve_driver;
@@ -59,13 +60,8 @@ struct DriverArgs {
     )]
     pub service_prefix: String,
 
-    #[argh(
-        option,
-        long = "spinelprefix",
-        description = "spinel namespace prefix (default: '/svc')",
-        default = "\"/svc\".to_string()"
-    )]
-    pub spinel_prefix: String,
+    #[argh(option, long = "spinelprefix", description = "spinel namespace prefix (ex: '/svc')")]
+    pub spinel_prefix: Option<String>,
 
     #[argh(
         option,
@@ -111,18 +107,66 @@ where
     Ok(())
 }
 
+async fn connect_to_spinel_device_proxy_hack() -> Result<SpinelDeviceProxy, Error> {
+    use {std::fs::File, std::path::Path};
+    const OT_PROTOCOL_PATH: &str = "/dev/class/ot-radio";
+
+    let ot_radio_dir = File::open(OT_PROTOCOL_PATH).context("opening dir in devmgr")?;
+    let directory_proxy = fidl_fuchsia_io::DirectoryProxy::new(
+        fuchsia_async::Channel::from_channel(fdio::clone_channel(&ot_radio_dir)?)?,
+    );
+
+    let ot_radio_devices = files_async::readdir(&directory_proxy).await?;
+
+    // Should have 1 device that implements OT_RADIO
+    if ot_radio_devices.len() != 1 {
+        return Err(format_err!("incorrect device number {}, shuold be 1", ot_radio_devices.len()));
+    }
+
+    let last_device: &files_async::DirEntry = ot_radio_devices.last().unwrap();
+
+    let found_device_path = Path::new(OT_PROTOCOL_PATH).join(last_device.name.clone());
+
+    fx_log_info!("device path {} got", found_device_path.to_str().unwrap());
+
+    let file = File::open(found_device_path).context("err opening ot radio device")?;
+
+    let spinel_device_setup_channel = fasync::Channel::from_channel(fdio::clone_channel(&file)?)?;
+    let spinel_device_setup_proxy = SpinelDeviceSetupProxy::new(spinel_device_setup_channel);
+
+    let (client_side, server_side) = fidl::endpoints::create_endpoints::<SpinelDeviceMarker>()?;
+
+    spinel_device_setup_proxy
+        .set_channel(server_side.into_channel())
+        .await?
+        .map_err(|x| format_err!("spinel_device_setup.set_channel() returned error {}", x))?;
+
+    Ok(client_side.into_proxy()?)
+}
+
+#[allow(unused)]
+fn connect_to_spinel_device_proxy(_prefix: &str) -> Result<SpinelDeviceProxy, Error> {
+    connect_to_service_at::<SpinelDeviceMarker>(_prefix)
+        .context("Failed to connect to Spinel device")
+}
+
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), Error> {
     let args: DriverArgs = argh::from_env();
 
     fuchsia_syslog::init_with_tags(&["lowpan-spinel-driver"]).context("initialize logging")?;
 
+    let spinel_device = if let Some(spinel_prefix) = args.spinel_prefix.as_ref() {
+        connect_to_spinel_device_proxy(spinel_prefix.as_str())?
+    } else {
+        connect_to_spinel_device_proxy_hack().await?
+    };
+
     run_driver(
         args.name,
         connect_to_service_at::<RegisterMarker>(args.service_prefix.as_str())
             .context("Failed to connect to Lowpan Registry service")?,
-        connect_to_service_at::<SpinelDeviceMarker>(args.spinel_prefix.as_str())
-            .context("Failed to connect to Spinel device")?,
+        spinel_device,
     )
     .await
 }
