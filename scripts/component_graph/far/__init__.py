18ccb551ff2104a9296ab662353137612c4db968
Daniel Hwang <arkay@google.com>
2020-07-01 20:36:27 +0000

    [scrutiny] Remove old fx component-graph.
    This is being done in favor of the newer fx scrutiny, which currently
    has the same functionality as the old fx component-graph but aims to be
    grown into a more full-fledged system in the future.
    
    Change-Id: Id1a5b3eb5a73e75a182a31aab27e1dd3e471c554
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/403780
    Reviewed-by: Benjamin Wright <benwright@google.com>
    Reviewed-by: Gary Bressler <geb@google.com>
    Testability-Review: Benjamin Wright <benwright@google.com>
    Commit-Queue: Daniel Hwang <arkay@google.com>

--- scripts/component_graph/far/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright 2019 The Fuchsia Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-"""The FAR module provides support for parsing FAR files."""
-
-import far.far_reader
