6eecc7d875faf30a30cfae70e0876f02498c1bff
Chris Suter <csuter@google.com>
2020-07-02 21:52:40 +0000

    [storage] [storage] Move persist tests to new fs_test framework.
    Bug: 52611
    Change-Id: I50f61545bdde37871c6b48d8b8f5bcf4f4aa438c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/403638
    Testability-Review: Chris Suter <csuter@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>
    Reviewed-by: Martin Lindsay <mlindsay@google.com>

--- src/storage/fs_test/persist.cc
+++ src/storage/fs_test/persist.cc
@@ -14,213 +14,221 @@
 #include <zircon/compiler.h>
 #include <zircon/syscalls.h>
 
+#include <iostream>
 #include <iterator>
 #include <memory>
 
-#include <fbl/algorithm.h>
 #include <fbl/alloc_checker.h>
-#include <fbl/array.h>
 #include <fbl/string_piece.h>
 #include <fbl/unique_fd.h>
 
-#include "filesystems.h"
-#include "misc.h"
+#include "src/storage/fs_test/fs_test_fixture.h"
+#include "src/storage/fs_test/misc.h"
 
-constexpr bool is_directory(const char* const path) {
+namespace fs_test {
+namespace {
+
+using PersistTest = FilesystemTest;
+
+constexpr bool IsDirectory(const char* const path) {
   return path[fbl::constexpr_strlen(path) - 1] == '/';
 }
 
-bool TestPersistSimple(void) {
-  BEGIN_TEST;
-
-  if (!test_info->can_be_mounted) {
-    fprintf(stderr, "Filesystem cannot be mounted; cannot test persistence\n");
-    return true;
-  }
-
-  const char* const paths[] = {
-      "::abc", "::def/", "::def/def_subdir/", "::def/def_subdir/def_subfile",
-      "::ghi", "::jkl",  "::mnopqrstuvxyz"};
+TEST_P(PersistTest, Simple) {
+  const char* const relative_paths[] = {
+      "abc", "def/", "def/def_subdir/", "def/def_subdir/def_subfile",
+      "ghi", "jkl",  "mnopqrstuvxyz"};
+  std::string paths[std::size(relative_paths)];
   for (size_t i = 0; i < std::size(paths); i++) {
-    if (is_directory(paths[i])) {
-      ASSERT_EQ(mkdir(paths[i], 0644), 0);
+    paths[i] = GetPath(relative_paths[i]);
+    if (IsDirectory(paths[i].c_str())) {
+      ASSERT_EQ(mkdir(paths[i].c_str(), 0644), 0);
     } else {
-      fbl::unique_fd fd(open(paths[i], O_RDWR | O_CREAT | O_EXCL, 0644));
+      fbl::unique_fd fd(open(paths[i].c_str(), O_RDWR | O_CREAT | O_EXCL, 0644));
       ASSERT_TRUE(fd);
     }
   }
 
-  ASSERT_TRUE(check_remount(), "Could not remount filesystem");
+  EXPECT_EQ(fs().Unmount().status_value(), 0);
+  EXPECT_EQ(fs().Fsck().status_value(), 0);
+  EXPECT_EQ(fs().Mount().status_value(), 0);
 
   // The files should still exist when we remount
   for (ssize_t i = std::size(paths) - 1; i >= 0; i--) {
-    if (is_directory(paths[i])) {
-      ASSERT_EQ(rmdir(paths[i]), 0);
+    if (IsDirectory(paths[i].c_str())) {
+      ASSERT_EQ(rmdir(paths[i].c_str()), 0);
     } else {
-      ASSERT_EQ(unlink(paths[i]), 0);
+      ASSERT_EQ(unlink(paths[i].c_str()), 0);
     }
   }
 
-  ASSERT_TRUE(check_remount(), "Could not remount filesystem");
+  EXPECT_EQ(fs().Unmount().status_value(), 0);
+  EXPECT_EQ(fs().Fsck().status_value(), 0);
+  EXPECT_EQ(fs().Mount().status_value(), 0);
 
   // But they should stay deleted!
   for (ssize_t i = std::size(paths) - 1; i >= 0; i--) {
-    if (is_directory(paths[i])) {
-      ASSERT_EQ(rmdir(paths[i]), -1);
+    if (IsDirectory(paths[i].c_str())) {
+      ASSERT_EQ(rmdir(paths[i].c_str()), -1);
     } else {
-      ASSERT_EQ(unlink(paths[i]), -1);
+      ASSERT_EQ(unlink(paths[i].c_str()), -1);
     }
   }
-
-  END_TEST;
 }
 
-bool TestPersistRapidRemount(void) {
-  BEGIN_TEST;
-
-  if (!test_info->can_be_mounted) {
-    fprintf(stderr, "Filesystem cannot be mounted; cannot test persistence\n");
-    return true;
+TEST_P(PersistTest, RapidRemount) {
+  for (size_t i = 0; i < 10; i++) {
+    ASSERT_EQ(fs().Unmount().status_value(), 0);
+    ASSERT_EQ(fs().Fsck().status_value(), 0);
+    ASSERT_EQ(fs().Mount().status_value(), 0);
   }
-
-  for (size_t i = 0; i < 100; i++) {
-    ASSERT_TRUE(check_remount(), "Could not remount filesystem");
-  }
-
-  END_TEST;
 }
 
-template <size_t BufferSize>
-bool TestPersistWithData(void) {
-  BEGIN_TEST;
+using WithBufferSize = std::tuple<TestFilesystemOptions, /*buffer_size=*/size_t>;
 
-  if (!test_info->can_be_mounted) {
-    fprintf(stderr, "Filesystem cannot be mounted; cannot test persistence\n");
-    return true;
-  }
+class PersistWithDataTest : public BaseFilesystemTest,
+                            public testing::WithParamInterface<WithBufferSize> {
+ public:
+  PersistWithDataTest() : BaseFilesystemTest(std::get<0>(GetParam())) {}
 
-  const char* const files[] = {
-      "::abc",
-      "::def",
-      "::and-another-file-filled-with-data",
+  size_t buffer_size() const { return std::get<1>(GetParam()); }
+};
+
+TEST_P(PersistWithDataTest, ReadsReturnWrittenDataAfterRemount) {
+  const std::string files[] = {
+      GetPath("abc"),
+      GetPath("def"),
+      GetPath("and-another-file-filled-with-data"),
   };
   std::unique_ptr<uint8_t[]> buffers[std::size(files)];
   unsigned int seed = static_cast<unsigned int>(zx_ticks_get());
-  unittest_printf("Persistent data test using seed: %u\n", seed);
+  std::cerr << "Persistent data test using seed: " << seed;
   fbl::AllocChecker ac;
   for (size_t i = 0; i < std::size(files); i++) {
-    buffers[i].reset(new (&ac) uint8_t[BufferSize]);
+    buffers[i].reset(new (&ac) uint8_t[buffer_size()]);
     ASSERT_TRUE(ac.check());
 
-    for (size_t j = 0; j < BufferSize; j++) {
+    for (size_t j = 0; j < buffer_size(); j++) {
       buffers[i][j] = (uint8_t)rand_r(&seed);
     }
-    fbl::unique_fd fd(open(files[i], O_RDWR | O_CREAT, 0644));
+    fbl::unique_fd fd(open(files[i].c_str(), O_RDWR | O_CREAT, 0644));
     ASSERT_TRUE(fd);
-    ASSERT_EQ(write(fd.get(), &buffers[i][0], BufferSize), BufferSize);
+    ASSERT_EQ(write(fd.get(), &buffers[i][0], buffer_size()), static_cast<ssize_t>(buffer_size()));
     ASSERT_EQ(fsync(fd.get()), 0);
   }
 
-  ASSERT_TRUE(check_remount(), "Could not remount filesystem");
+  EXPECT_EQ(fs().Unmount().status_value(), 0);
+  EXPECT_EQ(fs().Fsck().status_value(), 0);
+  EXPECT_EQ(fs().Mount().status_value(), 0);
 
   // Read files
   for (size_t i = 0; i < std::size(files); i++) {
-    std::unique_ptr<uint8_t[]> rbuf(new (&ac) uint8_t[BufferSize]);
+    std::unique_ptr<uint8_t[]> rbuf(new (&ac) uint8_t[buffer_size()]);
     ASSERT_TRUE(ac.check());
-    fbl::unique_fd fd(open(files[i], O_RDONLY, 0644));
+    fbl::unique_fd fd(open(files[i].c_str(), O_RDONLY, 0644));
     ASSERT_TRUE(fd);
 
     struct stat buf;
     ASSERT_EQ(fstat(fd.get(), &buf), 0);
-    ASSERT_EQ(buf.st_nlink, 1);
-    ASSERT_EQ(buf.st_size, BufferSize);
+    ASSERT_EQ(buf.st_nlink, 1ul);
+    ASSERT_EQ(buf.st_size, static_cast<off_t>(buffer_size()));
 
-    ASSERT_EQ(read(fd.get(), &rbuf[0], BufferSize), BufferSize);
-    for (size_t j = 0; j < BufferSize; j++) {
+    ASSERT_EQ(read(fd.get(), &rbuf[0], buffer_size()), static_cast<ssize_t>(buffer_size()));
+    for (size_t j = 0; j < buffer_size(); j++) {
       ASSERT_EQ(rbuf[j], buffers[i][j]);
     }
   }
 
-  ASSERT_TRUE(check_remount(), "Could not remount filesystem");
+  EXPECT_EQ(fs().Unmount().status_value(), 0);
+  EXPECT_EQ(fs().Fsck().status_value(), 0);
+  EXPECT_EQ(fs().Mount().status_value(), 0);
 
   // Delete all files
   for (size_t i = 0; i < std::size(files); i++) {
-    ASSERT_EQ(unlink(files[i]), 0);
+    ASSERT_EQ(unlink(files[i].c_str()), 0);
   }
 
-  ASSERT_TRUE(check_remount(), "Could not remount filesystem");
+  EXPECT_EQ(fs().Unmount().status_value(), 0);
+  EXPECT_EQ(fs().Fsck().status_value(), 0);
+  EXPECT_EQ(fs().Mount().status_value(), 0);
 
   // Files should stay deleted
 
-  DIR* dirp = opendir("::.");
-  ASSERT_NONNULL(dirp);
+  DIR* dirp = opendir(GetPath("").c_str());
+  ASSERT_NE(dirp, nullptr);
   struct dirent* de;
   de = readdir(dirp);
-  ASSERT_NONNULL(de);
+  ASSERT_NE(de, nullptr);
   ASSERT_EQ(strncmp(de->d_name, ".", 1), 0);
-  ASSERT_NULL(readdir(dirp));
+  ASSERT_EQ(readdir(dirp), nullptr);
   ASSERT_EQ(closedir(dirp), 0);
-
-  END_TEST;
 }
 
-constexpr size_t kMaxLoopLength = 26;
+using PersistRenameLoopTestParam = std::tuple<TestFilesystemOptions, /*directory=*/bool,
+                                              std::tuple</*loop_length=*/int, /*moves=*/int>>;
 
-template <bool MoveDirectory, size_t LoopLength, size_t Moves>
-bool TestRenameLoop(void) {
-  BEGIN_TEST;
+const TestFilesystemOptions& FilesystemOptions(const PersistRenameLoopTestParam& param) {
+  return std::get<0>(param);
+}
 
-  if (!test_info->can_be_mounted) {
-    fprintf(stderr, "Filesystem cannot be mounted; cannot test persistence\n");
-    return true;
+bool IsDirectory(const PersistRenameLoopTestParam& param) { return std::get<1>(param); }
+
+int LoopLength(const PersistRenameLoopTestParam& param) { return std::get<0>(std::get<2>(param)); }
+
+int Moves(const PersistRenameLoopTestParam& param) { return std::get<1>(std::get<2>(param)); }
+
+class PersistRenameLoopTest : public BaseFilesystemTest,
+                              public testing::WithParamInterface<PersistRenameLoopTestParam> {
+ public:
+  PersistRenameLoopTest() : BaseFilesystemTest(FilesystemOptions(GetParam())) {}
+};
+
+TEST_P(PersistRenameLoopTest, MultipleRenamesCorrectAfterRemount) {
+  // Create loop_length() directories
+  for (int i = 0; i < LoopLength(GetParam()); i++) {
+    const std::string src = GetPath(std::string(1, 'a' + i));
+    ASSERT_EQ(mkdir(src.c_str(), 0644), 0);
   }
 
-  static_assert(LoopLength <= kMaxLoopLength, "Loop length too long");
-
-  char src[128];
-  // Create "LoopLength" directories
-  for (size_t i = 0; i < LoopLength; i++) {
-    ASSERT_GT(sprintf(src, "::%c", static_cast<char>('a' + i)), 0);
-    ASSERT_EQ(mkdir(src, 0644), 0);
-  }
-
+  const std::string target_name = "target";
+  std::string src = "a/" + target_name;
   // Create a 'target'
-  if (MoveDirectory) {
-    ASSERT_EQ(mkdir("::a/target", 0644), 0);
+  if (IsDirectory(GetParam())) {
+    ASSERT_EQ(mkdir(GetPath(src).c_str(), 0644), 0);
   } else {
-    fbl::unique_fd fd(open("::a/target", O_RDWR | O_CREAT));
+    fbl::unique_fd fd(open(GetPath(src).c_str(), O_RDWR | O_CREAT));
     ASSERT_TRUE(fd);
   }
 
   // Move the target through the loop a bunch of times
-  size_t moves = Moves;
-  strcpy(src, "::a/target");
+  int to_do = Moves(GetParam());
   size_t char_index = 0;
-  while (moves--) {
-    char dst[128];
-    strcpy(dst, src);
-    char_index = (char_index + 1) % LoopLength;
-    dst[2] = static_cast<char>('a' + char_index);
-    ASSERT_EQ(rename(src, dst), 0);
-    strcpy(src, dst);
+  std::string dst = src;
+  while (to_do--) {
+    char_index = (char_index + 1) % LoopLength(GetParam());
+    dst[0] = 'a' + char_index;
+    ASSERT_EQ(rename(GetPath(src).c_str(), GetPath(dst).c_str()), 0);
+    src[0] = 'a' + char_index;
   }
 
-  ASSERT_TRUE(check_remount(), "Could not remount filesystem");
+  EXPECT_EQ(fs().Unmount().status_value(), 0);
+  EXPECT_EQ(fs().Fsck().status_value(), 0);
+  EXPECT_EQ(fs().Mount().status_value(), 0);
 
   // Check that the target only exists in ONE directory
   bool target_found = false;
-  for (size_t i = 0; i < LoopLength; i++) {
-    ASSERT_GT(sprintf(src, "::%c", static_cast<char>('a' + i)), 0);
-    DIR* dirp = opendir(src);
-    ASSERT_NONNULL(dirp);
+  for (int i = 0; i < LoopLength(GetParam()); i++) {
+    std::string src(1, 'a' + i);
+    DIR* dirp = opendir(GetPath(src).c_str());
+    ASSERT_NE(dirp, nullptr);
     struct dirent* de;
     de = readdir(dirp);
-    ASSERT_NONNULL(de);
+    ASSERT_NE(de, nullptr);
     ASSERT_EQ(strcmp(de->d_name, "."), 0);
     de = readdir(dirp);
     if (de != nullptr) {
-      ASSERT_FALSE(target_found, "Target found twice!");
-      ASSERT_EQ(strcmp(de->d_name, "target"), 0, "Non-target found");
+      ASSERT_FALSE(target_found) << "Target found twice!";
+      ASSERT_EQ(de->d_name, target_name) << "Non-target found";
       target_found = true;
     }
 
@@ -228,41 +236,69 @@ bool TestRenameLoop(void) {
   }
   ASSERT_TRUE(target_found);
 
-  ASSERT_TRUE(check_remount(), "Could not remount filesystem");
+  EXPECT_EQ(fs().Unmount().status_value(), 0);
+  EXPECT_EQ(fs().Fsck().status_value(), 0);
+  EXPECT_EQ(fs().Mount().status_value(), 0);
 
   // Clean up
 
   target_found = false;
-  for (size_t i = 0; i < LoopLength; i++) {
-    ASSERT_GT(sprintf(src, "::%c", static_cast<char>('a' + i)), 0);
-    int ret = unlink(src);
+  for (int i = 0; i < LoopLength(GetParam()); i++) {
+    std::string src(1, 'a' + i);
+    int ret = unlink(GetPath(src).c_str());
     if (ret != 0) {
       ASSERT_FALSE(target_found);
-      ASSERT_GT(sprintf(src, "::%c/target", static_cast<char>('a' + i)), 0);
-      ASSERT_EQ(unlink(src), 0);
-      ASSERT_GT(sprintf(src, "::%c", static_cast<char>('a' + i)), 0);
-      ASSERT_EQ(unlink(src), 0);
+      ASSERT_EQ(unlink(GetPath(src + "/" + target_name).c_str()), 0);
+      ASSERT_EQ(unlink(GetPath(src).c_str()), 0);
       target_found = true;
     }
   }
-  ASSERT_TRUE(target_found, "Target was never unlinked");
-
-  END_TEST;
+  ASSERT_TRUE(target_found) << "Target was never unlinked";
 }
 
-RUN_FOR_ALL_FILESYSTEMS(persistence_tests,
-                        RUN_TEST_MEDIUM(TestPersistSimple) RUN_TEST_LARGE(TestPersistRapidRemount)
-                            RUN_TEST_MEDIUM((TestPersistWithData<1>))
-                                RUN_TEST_MEDIUM((TestPersistWithData<100>))
-                                    RUN_TEST_LARGE((TestPersistWithData<8192 - 1>))
-                                        RUN_TEST_LARGE((TestPersistWithData<8192>))
-                                            RUN_TEST_LARGE((TestPersistWithData<8192 + 1>))
-                                                RUN_TEST_LARGE((TestPersistWithData<8192 * 128>))
-                                                    RUN_TEST_MEDIUM((TestRenameLoop<false, 2, 2>));
-                        RUN_TEST_LARGE((TestRenameLoop<false, 2, 100>));
-                        RUN_TEST_LARGE((TestRenameLoop<false, 15, 100>));
-                        RUN_TEST_LARGE((TestRenameLoop<false, 25, 500>));
-                        RUN_TEST_MEDIUM((TestRenameLoop<true, 2, 2>));
-                        RUN_TEST_LARGE((TestRenameLoop<true, 2, 100>));
-                        RUN_TEST_LARGE((TestRenameLoop<true, 15, 100>));
-                        RUN_TEST_LARGE((TestRenameLoop<true, 25, 500>));)
+// These tests only work on filesystems that can be unmounted.
+std::vector<TestFilesystemOptions> GetTestCombinations() {
+  std::vector<TestFilesystemOptions> test_combinations;
+  for (TestFilesystemOptions options : AllTestFilesystems()) {
+    if (options.filesystem->GetTraits().can_unmount) {
+      test_combinations.push_back(options);
+    }
+  }
+  return test_combinations;
+}
+
+INSTANTIATE_TEST_SUITE_P(/*no prefix*/, PersistTest, testing::ValuesIn(GetTestCombinations()),
+                         testing::PrintToStringParamName());
+
+std::string PersistWithDataTestParamDescription(
+    const testing::TestParamInfo<WithBufferSize>& param) {
+  std::stringstream s;
+  s << std::get<0>(param.param) << "WithBufferSize" << std::get<1>(param.param);
+  return s.str();
+}
+
+INSTANTIATE_TEST_SUITE_P(/*no prefix*/, PersistWithDataTest,
+                         testing::Combine(testing::ValuesIn(GetTestCombinations()),
+                                          testing::Values(1, 100, 8192 - 1, 8192, 8192 + 1,
+                                                          8192 * 128)),
+                         PersistWithDataTestParamDescription);
+
+std::string PersistRenameLoopTestParamDescription(
+    const testing::TestParamInfo<PersistRenameLoopTestParam>& param) {
+  std::stringstream s;
+  s << FilesystemOptions(param.param)
+    << (IsDirectory(param.param) ? "RenameDirectory" : "RenameFile") << Moves(param.param)
+    << "Times"
+    << "Through" << LoopLength(param.param) << "Directories";
+  return s.str();
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    /*no prefix*/, PersistRenameLoopTest,
+    testing::Combine(testing::ValuesIn(GetTestCombinations()), testing::Bool(),
+                     testing::Values(std::make_tuple(2, 2), std::make_tuple(2, 100),
+                                     std::make_tuple(15, 100), std::make_tuple(25, 500))),
+    PersistRenameLoopTestParamDescription);
+
+}  // namespace
+}  // namespace fs_test
