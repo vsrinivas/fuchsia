592d09680844fdd0b23e2c2752bd774bb2196069
Marie Janssen 💖 <jamuraa@google.com>
2020-07-07 20:36:17 +0000

    [appmgr] Increase test timeouts
    Increase the appmgr_cpu_metrics_test timeout since they have been
    failing for timeout reasons.
    
    Bug: 53844
    
    Multiply: inspect_metrics_emulator_test
    Change-Id: Ia9f7e378ef504ffe98f78db4978b4cfb28c11ed6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/404917
    Testability-Review: Marie Janssen 💖 <jamuraa@google.com>
    Testability-Review: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Luke Nicholson <lukenicholson@google.com>
    Commit-Queue: Marie Janssen 💖 <jamuraa@google.com>

--- src/tests/end_to_end/inspect_metrics/test/appmgr_cpu_metrics_test.dart
+++ src/tests/end_to_end/inspect_metrics/test/appmgr_cpu_metrics_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@Timeout(Duration(seconds: 60))
+
 import 'package:test/test.dart';
 import 'package:sl4f/sl4f.dart' as sl4f;
 import 'util.dart';
