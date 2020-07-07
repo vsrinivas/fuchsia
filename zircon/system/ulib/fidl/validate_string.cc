af3e132d23fac784607331c8a6e70eaf5e4409ce
Benjamin Prosnitz <bprosnitz@google.com>
2020-07-07 11:21:31 +0000

    [fidl] Minor fidl_validate_string performance improvement
    It is slightly more efficient to use < than bitwise & and =.
    Currently strings in LLCPP are significantly slower than in
    Rust.
    
    LibFIDL/fidl_validate_string/1025/WallTime                                                            faster            0.851-0.995    927 +/- 37 ns         853 +/- 33 ns
    LibFIDL/fidl_validate_string/258/WallTime                                                             faster            0.909-0.914    236.94 +/- 0.17 ns    215.91 +/- 0.43 ns
    LibFIDL/fidl_validate_string/4098/WallTime                                                            faster            0.837-0.915    3944 +/- 71 ns        3452 +/- 91 ns
    LibFIDL/fidl_validate_string/65536/WallTime                                                           faster            0.982-0.995    142124 +/- 614 ns     140535 +/- 312 ns
    
    Change-Id: Ib190f7c0f3af0e058d35d5f98bdbad81584230b1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/403653
    Reviewed-by: Pascal Perez <pascallouis@google.com>
    Testability-Review: Pascal Perez <pascallouis@google.com>
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>

--- zircon/system/ulib/fidl/validate_string.cc
+++ zircon/system/ulib/fidl/validate_string.cc
@@ -22,10 +22,12 @@ zx_status_t fidl_validate_string(const char* data, uint64_t size) {
     // We therefore force the byte to be treated as unsigned, since we cannot
     // rely on the default.
     unsigned char byte = data[pos];
-    if (byte < 0b10000000) {
+    if (byte < 0b10000000) {  // Matches 0b1xxx xxxx
       pos++;
       continue;
-    } else if ((byte & 0b11100000) == 0b11000000) {
+    }
+
+    if (byte < 0b11100000) {  // Matches 0b110x xxxx
       next_pos = pos + 2;
       if (next_pos > size) {
         return ZX_ERR_INVALID_ARGS;
@@ -34,12 +36,11 @@ zx_status_t fidl_validate_string(const char* data, uint64_t size) {
         return ZX_ERR_INVALID_ARGS;
       }
       // range check
-      code_point = (byte & 0b00011111) << 6 |
-                   (data[pos + 1] & 0b00111111);
+      code_point = (byte & 0b00011111) << 6 | (data[pos + 1] & 0b00111111);
       if (code_point < 0x80 || 0x7ff < code_point) {
         return ZX_ERR_INVALID_ARGS;
       }
-    } else if ((byte & 0b11110000) == 0b11100000) {
+    } else if (byte < 0b11110000) {  // Matches 0b1110 xxxx
       next_pos = pos + 3;
       if (next_pos > size) {
         return ZX_ERR_INVALID_ARGS;
@@ -51,14 +52,13 @@ zx_status_t fidl_validate_string(const char* data, uint64_t size) {
         return ZX_ERR_INVALID_ARGS;
       }
       // range check
-      code_point = (byte & 0b00001111) << 12 |
-                   (data[pos + 1] & 0b00111111) << 6 |
+      code_point = (byte & 0b00001111) << 12 | (data[pos + 1] & 0b00111111) << 6 |
                    (data[pos + 2] & 0b00111111);
       if (code_point < 0x800 || 0xffff < code_point ||
           (0xd7ff < code_point && code_point < 0xe000)) {
         return ZX_ERR_INVALID_ARGS;
       }
-    } else { // 0b11110000
+    } else {  // Matches 0b1111 xxxx
       next_pos = pos + 4;
       if (next_pos > size) {
         return ZX_ERR_INVALID_ARGS;
@@ -73,10 +73,8 @@ zx_status_t fidl_validate_string(const char* data, uint64_t size) {
         return ZX_ERR_INVALID_ARGS;
       }
       // range check
-      code_point = (byte & 0b00000111) << 18 |
-                   (data[pos + 1] & 0b00111111) << 12 |
-                   (data[pos + 2] & 0b00111111) << 6 |
-                   (data[pos + 3] & 0b00111111);
+      code_point = (byte & 0b00000111) << 18 | (data[pos + 1] & 0b00111111) << 12 |
+                   (data[pos + 2] & 0b00111111) << 6 | (data[pos + 3] & 0b00111111);
       if (code_point < 0xffff || 0x10ffff < code_point) {
         return ZX_ERR_INVALID_ARGS;
       }
