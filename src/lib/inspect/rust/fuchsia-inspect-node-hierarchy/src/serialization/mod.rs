565fcae48f9cd9f70af45e9a67e7d6b570d1dbc6
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2020-07-08 02:58:58 +0000

    [cleanup][inspect-node-hierarchy] Remove no longer used deprecated formatter
    Tested: existing tests
    
    Change-Id: Iae8e29f670022f28933b9c41c2861db1e24b9687
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/405080
    Testability-Review: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Luke Nicholson <lukenicholson@google.com>
    Commit-Queue: Miguel Flores <miguelfrde@google.com>

--- src/lib/inspect/rust/fuchsia-inspect-node-hierarchy/src/serialization/mod.rs
+++ src/lib/inspect/rust/fuchsia-inspect-node-hierarchy/src/serialization/mod.rs
@@ -33,51 +33,14 @@
 
 use {crate::NodeHierarchy, anyhow::Error};
 
-pub use crate::serialization::deprecated_json::*;
 pub use crate::serialization::json::*;
 
-pub mod deprecated_json;
 pub mod json;
 mod serialize;
-mod utils;
 
 /// Implementers of this trait will be able to convert an `Object` type data format that
 /// is encoding a diagnostics data hierarchy into a NodeHierarchy.
 pub trait HierarchyDeserializer<Key = String> {
     type Object;
     fn deserialize(data_format: Self::Object) -> Result<NodeHierarchy<Key>, Error>;
-}
-
-/// DEPRECATED:
-/// Node hierarchies to be formatted including information about the path.
-/// Example:
-///
-/// ```
-/// DeprecatedHierarchyData {
-///     hierarchy: SOME_HIERARCHY,
-///     file_path: "/some/path",
-///     fields: vec!["root", "node1", "node2"],
-/// }
-/// ```
-///
-/// Means that the hierarchy `SOME_HIERARCHY` points to the inspect node named
-/// `node2` that is child of the node `node1` under `root` in an inspect file
-/// located at `/some/path`.
-///
-pub struct DeprecatedHierarchyData {
-    /// The node hierarchy to be formatted.
-    pub hierarchy: NodeHierarchy,
-
-    /// The path where the inspect file is.
-    pub file_path: String,
-
-    /// The path to the node within the inspect tree.
-    pub fields: Vec<String>,
-}
-
-/// Implementers of this trait will provide different ways of formatting an
-/// inspect hierarchy.
-pub trait DeprecatedHierarchyFormatter {
-    fn format(hierarchy: DeprecatedHierarchyData) -> Result<String, Error>;
-    fn format_multiple(hierarchies: Vec<DeprecatedHierarchyData>) -> Result<String, Error>;
 }
