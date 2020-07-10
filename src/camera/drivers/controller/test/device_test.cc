061969f1579e0e2b8c3e72a8bac59b67b49db174
Jocelyn Dang <jocelyndang@google.com>
2020-07-10 17:53:18 +0000

    [camera][controller] Migrate off DdkRemoveDeprecated.
    DdkAsyncRemove() will additionally request calling the unbind hook.
    Since the current unbind implementation only shuts down the async loop,
    this doesn't affect the test behavior.
    
    BUG=37831
    
    Change-Id: I17aa08964b251d6215148754b7ab1c16d5fe96a0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/405481
    Reviewed-by: Brijen Raval <braval@google.com>
    Testability-Review: Brijen Raval <braval@google.com>
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>

--- src/camera/drivers/controller/test/device_test.cc
+++ src/camera/drivers/controller/test/device_test.cc
@@ -37,7 +37,7 @@ class ControllerDeviceTest : public gtest::TestLoopFixture {
 
   void TearDown() override {
     if (controller_device_) {
-      ASSERT_EQ(controller_device_->DdkRemoveDeprecated(), ZX_OK);
+      controller_device_->DdkAsyncRemove();
     }
     ASSERT_EQ(ddk_->WaitUntilRemove(), ZX_OK);
     ASSERT_TRUE(ddk_->Ok());
