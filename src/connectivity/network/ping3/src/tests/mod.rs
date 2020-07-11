224dbc8025c9e4c90b710c20c0d86aab19d83742
Ghanan Gowripalan <ghanan@google.com>
2020-07-11 08:27:07 +0000

    [ping3] Use netemul rust library for tests
    Fixed: 55483
    
    Change-Id: I75131905af475e16334f501c4e815985cab755a0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/405788
    Reviewed-by: Sam Balana <sbalana@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Ghanan Gowripalan <ghanan@google.com>
    Testability-Review: Tamir Duberstein <tamird@google.com>

--- src/connectivity/network/ping3/src/tests/mod.rs
+++ src/connectivity/network/ping3/src/tests/mod.rs
@@ -4,246 +4,125 @@
 
 mod integration_tests;
 
-use anyhow::{Context as _, Error};
-use fidl::endpoints::DiscoverableService;
-use fidl_fuchsia_net::{IpAddress, Subnet};
-use fidl_fuchsia_net_icmp::ProviderMarker;
-use fidl_fuchsia_net_stack::{ForwardingDestination, ForwardingEntry, PhysicalStatus, StackMarker};
-use fidl_fuchsia_net_stack_ext::FidlReturn;
-use fidl_fuchsia_netemul_environment::{
-    EnvironmentOptions, LaunchService, LoggerOptions, ManagedEnvironmentMarker,
-    ManagedEnvironmentProxy, VirtualDevice,
-};
-use fidl_fuchsia_netemul_network::{
-    DeviceConnection, DeviceProxy_Marker, EndpointBacking, EndpointConfig, EndpointManagerMarker,
-    EndpointManagerProxy, EndpointProxy, NetworkConfig, NetworkContextMarker, NetworkManagerMarker,
-    NetworkProxy,
-};
-use fidl_fuchsia_netemul_sandbox::{SandboxMarker, SandboxProxy};
-use fidl_fuchsia_sys::{
-    ComponentControllerEvent, ComponentControllerMarker, LaunchInfo, LauncherMarker,
-    TerminationReason,
-};
-use fuchsia_component::client::connect_to_service;
+use fidl::endpoints::DiscoverableService as _;
+use fidl_fuchsia_net as fnet;
+use fidl_fuchsia_net_icmp as fnet_icmp;
+use fidl_fuchsia_net_stack as fnet_stack;
+use fidl_fuchsia_net_stack_ext::FidlReturn as _;
+use fidl_fuchsia_netemul_environment as netemul_environment;
 use fuchsia_component::fuchsia_single_component_package_url;
 use fuchsia_zircon as zx;
-use futures::StreamExt;
-use std::vec::Vec;
+
+use anyhow::Context as _;
 
 const NETSTACK_URL: &'static str = fuchsia_single_component_package_url!("netstack3");
 const PING_URL: &'static str = fuchsia_single_component_package_url!("ping3");
 
-fn create_netstack_env(
-    sandbox: &SandboxProxy,
+/// Configuration for an environment.
+struct EnvConfig {
+    /// Name for the environment and endpoint.
     name: String,
-    device: fidl::endpoints::ClientEnd<DeviceProxy_Marker>,
-) -> Result<ManagedEnvironmentProxy, Error> {
-    let (env, env_server_end) = fidl::endpoints::create_proxy::<ManagedEnvironmentMarker>()?;
-    let services = vec![
-        LaunchService {
-            name: StackMarker::SERVICE_NAME.to_string(),
-            url: NETSTACK_URL.to_string(),
-            arguments: None,
-        },
-        LaunchService {
-            name: ProviderMarker::SERVICE_NAME.to_string(),
-            url: NETSTACK_URL.to_string(),
-            arguments: None,
-        },
-    ];
-    let devices = vec![VirtualDevice { path: "eth001".to_string(), device }];
-    sandbox
-        .create_environment(
-            env_server_end,
-            EnvironmentOptions {
-                name: Some(name),
-                services: Some(services),
-                devices: Some(devices),
-                inherit_parent_launch_services: None,
-                logger_options: Some(LoggerOptions {
-                    enabled: Some(true),
-                    klogs_enabled: None,
-                    filter_options: None,
-                    syslog_output: Some(true),
-                }),
-            },
-        )
-        .context("Failed to create environment")?;
 
-    Ok(env)
+    /// The static IP to add to the created interface.
+    static_ip: fnet::Subnet,
 }
 
-pub struct TestSetup {
-    sandbox: SandboxProxy,
-    network: NetworkProxy,
-    endpoint_manager: EndpointManagerProxy,
-    endpoints: Vec<EndpointProxy>,
-    subnet_addr: IpAddress,
-    subnet_prefix: u8,
-}
+/// Creates an environment for each configuration in `configs`.
+///
+/// The environments will each join a network named `test_name` with a single interface.
+/// `on_link_subnet` is the subnet that is considered on-link; the netstack in each
+/// environment will be configured with an on-link route to the subnet out the interface.
+async fn create_environments<'a>(
+    sandbox: &'a netemul::TestSandbox,
+    test_name: String,
+    on_link_subnet: fnet::Subnet,
+    configs: Vec<EnvConfig>,
+) -> Result<
+    (Vec<(netemul::TestEnvironment<'a>, netemul::TestInterface<'a>)>, netemul::TestNetwork<'a>),
+    anyhow::Error,
+> {
+    let net = sandbox.create_network(test_name.clone()).await.context("create network")?;
 
-impl TestSetup {
-    pub async fn new(subnet_addr: IpAddress, subnet_prefix: u8) -> Result<Self, Error> {
-        let sandbox =
-            connect_to_service::<SandboxMarker>().context("Failed to connect to sandbox")?;
+    let mut envs = Vec::with_capacity(configs.len());
+    for config in configs.into_iter() {
+        let name = format!("{}_{}", test_name, config.name);
+        let env = sandbox
+            .create_environment(
+                name.clone(),
+                vec![
+                    netemul_environment::LaunchService {
+                        name: fnet_stack::StackMarker::SERVICE_NAME.to_string(),
+                        url: NETSTACK_URL.to_string(),
+                        arguments: None,
+                    },
+                    netemul_environment::LaunchService {
+                        name: fnet_icmp::ProviderMarker::SERVICE_NAME.to_string(),
+                        url: NETSTACK_URL.to_string(),
+                        arguments: None,
+                    },
+                ],
+            )
+            .with_context(|| format!("create {} environment", name))?;
 
-        let (netctx, netctx_server_end) = fidl::endpoints::create_proxy::<NetworkContextMarker>()?;
-        sandbox.get_network_context(netctx_server_end).context("Failed to get network context")?;
-
-        let (network_manager, network_manager_server_end) =
-            fidl::endpoints::create_proxy::<NetworkManagerMarker>()?;
-        netctx
-            .get_network_manager(network_manager_server_end)
-            .context("Failed to get network manager")?;
-
-        let config = NetworkConfig { latency: None, packet_loss: None, reorder: None };
-        let (status, network) = network_manager
-            .create_network("test", config)
+        let ep = net
+            .create_endpoint::<netemul::Ethernet, _>(config.name.clone())
             .await
-            .context("Failed to create network")?;
-        assert_eq!(status, zx::sys::ZX_OK);
-        let network = network.unwrap().into_proxy()?;
+            .with_context(|| format!("create {} endpoint", config.name))?;
+        let iface =
+            ep.into_interface_in_environment(&env).await.context("into interface in env")?;
+        let () = iface.set_link_up(true).await.context("set link up")?;
+        let () = iface.enable_interface().await.context("enable interface")?;
 
-        let (endpoint_manager, endpoint_manager_server_end) =
-            fidl::endpoints::create_proxy::<EndpointManagerMarker>()?;
-        netctx
-            .get_endpoint_manager(endpoint_manager_server_end)
-            .context("Failed to get endpoint manager")?;
-
-        Ok(Self {
-            sandbox,
-            network,
-            endpoint_manager,
-            endpoints: Vec::new(),
-            subnet_addr,
-            subnet_prefix,
-        })
-    }
-
-    async fn add_env(
-        &mut self,
-        name: &str,
-        addr: IpAddress,
-    ) -> Result<ManagedEnvironmentProxy, Error> {
-        let mut config =
-            EndpointConfig { mtu: 1500, mac: None, backing: EndpointBacking::Ethertap };
-        let (status, endpoint) = self
-            .endpoint_manager
-            .create_endpoint(name, &mut config)
-            .await
-            .context("Failed to create endpoint")?;
-        assert_eq!(status, zx::sys::ZX_OK);
-        let endpoint = endpoint.unwrap().into_proxy()?;
-        endpoint.set_link_up(true).await.context("Failed to set link up")?;
-        let status =
-            self.network.attach_endpoint(name).await.context("Failed to attach endpoint")?;
-        assert_eq!(status, zx::sys::ZX_OK);
-
-        // Create device
-        let (device, device_server_end) =
-            fidl::endpoints::create_endpoints::<DeviceProxy_Marker>()?;
-        endpoint.get_proxy_(device_server_end)?;
-
-        // Create environment
-        let env = create_netstack_env(&self.sandbox, name.to_string(), device)
-            .context("Failed to create environment")?;
-
-        // Add the ethernet interface
-        let (stack, stack_server_end) = fidl::endpoints::create_proxy::<StackMarker>()?;
-        env.connect_to_service("fuchsia.net.stack.Stack", stack_server_end.into_channel())
-            .context("Can't connect to fuchsia.net.stack")?;
-
-        let interface_id = match endpoint
-            .get_device()
-            .await
-            .context("Failed to get ethernet device")?
-        {
-            DeviceConnection::Ethernet(device) => stack
-                .add_ethernet_interface("fake_topo_path", device)
-                .await
-                .squash_result()
-                .context("Failed to get ethernet interface")?,
-            DeviceConnection::NetworkDevice(device) => {
-                todo!("(48860) Support and test NetworkDevice connections. Got unexpected NetworkDevice {:?}", device);
-            }
-        };
-
-        // Wait for interface to become online
+        // Wait for the interface to become online
         //
-        // TODO(fxb/38311): Replace this loop with an event loop waiting for fuchsia.net.stack's
-        // Stack.OnInterfaceStatusChange to return an ONLINE InterfaceStatusChange.
+        // TODO(21154): Replace this loop with a hanging get on `WatchInterfaces`.
+        //
+        // Note, netstack3 does not implement `fuchsia.netstack.Netstack/OnInterfacesChanged`
+        // or `fuchsia.net.stack.Stack/OnInterfaceStatusChange`.
         loop {
-            let info = stack
-                .get_interface_info(interface_id)
-                .await
-                .squash_result()
-                .context("Failed to get interface info")?;
-            if info.properties.physical_status == PhysicalStatus::Up {
+            let info = iface.get_info().await.context("get interface info")?;
+            if info.properties.physical_status == fnet_stack::PhysicalStatus::Up
+                && info.properties.administrative_status
+                    == fnet_stack::AdministrativeStatus::Enabled
+            {
                 break;
             }
-            eprintln!("Interface not ready, waiting 10ms...");
-            zx::Duration::from_millis(100).sleep();
+            let sleep_ms = 100;
+            eprintln!("interface not ready, waiting {}ms...", sleep_ms);
+            let () = zx::Duration::from_millis(sleep_ms).sleep();
         }
 
-        // Assign IP address and add routes
-        stack
-            .add_interface_address(
-                interface_id,
-                &mut Subnet { addr, prefix_len: self.subnet_prefix },
-            )
-            .await
-            .squash_result()
-            .context("Failed to add interface address")?;
+        let () = iface.add_ip_addr(config.static_ip).await.context("add ip addr")?;
 
-        stack
-            .add_forwarding_entry(&mut ForwardingEntry {
-                subnet: Subnet { addr: self.subnet_addr, prefix_len: self.subnet_prefix },
-                destination: ForwardingDestination::DeviceId(interface_id),
+        let stack =
+            env.connect_to_service::<fnet_stack::StackMarker>().context("connect to net stack")?;
+        let () = stack
+            .add_forwarding_entry(&mut fnet_stack::ForwardingEntry {
+                subnet: on_link_subnet,
+                destination: fnet_stack::ForwardingDestination::DeviceId(iface.id()),
             })
             .await
             .squash_result()
             .context("Failed to add forwarding entry")?;
 
-        self.endpoints.push(endpoint);
-        Ok(env)
+        let () = envs.push((env, iface));
     }
+
+    Ok((envs, net))
 }
 
-async fn assert_ping(env: &ManagedEnvironmentProxy, args: &str, expected_return_code: i64) {
-    let (launcher, launcher_server_end) =
-        fidl::endpoints::create_proxy::<LauncherMarker>().unwrap();
-    env.get_launcher(launcher_server_end).context("Failed to get launcher").unwrap();
-
-    let (controller, controller_server_end) =
-        fidl::endpoints::create_proxy::<ComponentControllerMarker>().unwrap();
-    let mut ping_launch = LaunchInfo {
-        url: PING_URL.to_string(),
-        arguments: Some(args.split(" ").map(String::from).collect()),
-        out: None,
-        err: None,
-        directory_request: None,
-        flat_namespace: None,
-        additional_services: None,
-    };
-    launcher
-        .create_component(&mut ping_launch, Some(controller_server_end))
-        .context("Failed to create component")
-        .unwrap();
-
-    let mut event_stream = controller.take_event_stream();
-    while let Some(evt) = event_stream.next().await {
-        match evt.unwrap() {
-            ComponentControllerEvent::OnTerminated { return_code, termination_reason } => {
-                assert_eq!(termination_reason, TerminationReason::Exited);
-                assert!(
-                    return_code == expected_return_code,
-                    format!(
-                        "Expected return code {}, but got {} instead",
-                        expected_return_code, return_code
-                    )
-                );
-            }
-            _ => {}
-        }
-    }
+/// Launch `ping3` and wait for it to terminate.
+async fn ping3(
+    env: &netemul::TestEnvironment<'_>,
+    args: &str,
+) -> Result<fuchsia_component::client::ExitStatus, anyhow::Error> {
+    let launcher = env.get_launcher().context("get launcher").context("get launcher")?;
+    fuchsia_component::client::launch(
+        &launcher,
+        PING_URL.to_string(),
+        Some(args.split(" ").map(String::from).collect()),
+    )
+    .context("launch ping3 component")?
+    .wait()
+    .await
 }
