8c904fc45b7a0b8248e0ad9256bf59a2ed5bbafc
Austin Foxley <afoxley@google.com>
2020-07-11 05:25:46 +0000

    [codec] Add test for hardware h265 encoder presence on sherlock
    Test: fx test codec_detect_hw_encode_test
    Change-Id: I946c8f8ff35f28b97e9b36d13a95e1aa500eabdf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/403279
    Commit-Queue: Austin Foxley <afoxley@google.com>
    Reviewed-by: Dale Sather <dalesat@google.com>
    Testability-Review: Dale Sather <dalesat@google.com>

--- src/media/codec/factory/test/codec_detect_hw_encode_test.cc
+++ src/media/codec/factory/test/codec_detect_hw_encode_test.cc
@@ -19,3 +19,18 @@ TEST_F(CodecFactoryHwDetectTest, H264EncoderPresent) {
     return factory != nullptr;
   });
 }
+
+TEST_F(CodecFactoryHwDetectTest, H265EncoderPresent) {
+  codec_factory::CodecFactoryApp app(dispatcher());
+
+  // Loop needs to run till hw codec are fully discovered, so run test till that happens.
+  RunLoopUntil([&app]() {
+    auto factory = app.FindHwCodec(
+        [](const fuchsia::mediacodec::CodecDescription& hw_codec_description) -> bool {
+          std::string mime_type = "video/h265";
+          return (fuchsia::mediacodec::CodecType::ENCODER == hw_codec_description.codec_type) &&
+                 (mime_type == hw_codec_description.mime_type);
+        });
+    return factory != nullptr;
+  });
+}
