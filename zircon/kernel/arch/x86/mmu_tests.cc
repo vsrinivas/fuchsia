bcd2a02d0330aac5e1177cf452fc476f06cbc7a1
Venkatesh Srinivas <venkateshs@google.com>
2020-07-11 03:53:31 +0000

    [kernel][x86] Speculation: L1TF unit test
    Add a unit test that spot-checks page tables to ensure Fuchsia is safe
    against the L1TF vulnerability.
    
    L1TF is a speculative-execution information leak bug that allows hostile
    code to infer the contents of page frames that are pointed to by page
    tables but are not marked present. To mitigate L1TF, operating systems
    must ensure not-present PTEs always point at a safe page frame.
    
    Fuchsia always marks not-present PTEs as 0; to ensure that this stays
    the case, examine physmap page tables and user address space page tables
    for the thread running 'k ut' to ensure that a PTE w/ the present bit
    clear points at the 0 page. This is not a complete test / proof of
    security, only a spot check, but it does offer some confidence in the
    mitigation.
    
    Bug: 12540 Speculative Execution Mitigations.
    Bug: 33667 Spectre mitigations?
    
    Change-Id: I4eb32cb33e022ab858100a962e360aa0639e835e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/372564
    Commit-Queue: Venkatesh Srinivas <venkateshs@google.com>
    Reviewed-by: Adrian Danis <adanis@google.com>
    Testability-Review: Adrian Danis <adanis@google.com>

--- zircon/kernel/arch/x86/mmu_tests.cc
+++ zircon/kernel/arch/x86/mmu_tests.cc
@@ -13,7 +13,9 @@
 #include <arch/mmu.h>
 #include <arch/x86/mmu.h>
 #include <vm/arch_vm_aspace.h>
+#include <vm/physmap.h>
 #include <vm/pmm.h>
+#include <vm/vm_aspace.h>
 
 static bool check_virtual_address_mapped(uint64_t* pml4, vaddr_t va) {
   constexpr uint kPageTableLevels = 4;
@@ -84,6 +86,91 @@ static bool x86_arch_vmaspace_usermmu_tests() {
   END_TEST;
 }
 
+// Returns true if there was a valid translation or if the page was not present and the translation
+// path had a safe phys_addr.
+//
+// Returns the terminal page table entry in |ptep| and amount to increment address to get the next
+// page table entry in |step|.
+static bool check_virtual_address_l1tf_invariant(uint64_t* pml4, vaddr_t va, uint64_t* ptep,
+  size_t* step) {
+  constexpr uint kPageTableLevels = 4;
+
+  // Virtual Address is split at [47:39], [38:30], [29:21], [20:12]
+  uint64_t offsets[kPageTableLevels] = {BITS_SHIFT(va, 47, 39), BITS_SHIFT(va, 38, 30),
+                                        BITS_SHIFT(va, 29, 21), BITS_SHIFT(va, 20, 12)};
+  uint64_t* current_level = pml4;
+  size_t next_step = 512 * GB;
+  // Walk the page tables down for address |va| until we hit a leaf PTE or a not-present PTE.
+  for (uint i = 0; i < kPageTableLevels; i++) {
+    uint64_t index = offsets[i];
+    uint64_t pte = current_level[index];
+
+    *ptep = pte;
+    *step = next_step;
+    // L1TF Invariant: If P == 0, then the address must be zero or some safe address.
+    if ((pte & X86_MMU_PG_P) == 0) {
+      return (pte & X86_PG_FRAME) == 0;
+    }
+    if (i == 0) {
+      DEBUG_ASSERT((pte & X86_MMU_PG_PS) == 0);  // No 512 GB pages.
+    }
+    // On finding a large page, terminate the walk early.
+    if ((pte & X86_MMU_PG_PS)) {
+      return true;
+    }
+
+    uint64_t next_level_va = X86_PHYS_TO_VIRT(pte & X86_PG_FRAME);
+    current_level = reinterpret_cast<uint64_t*>(next_level_va);
+    next_step = next_step >> ADDR_OFFSET;  // next_step steps by the address space under this PTE
+  }
+  return true;
+}
+
+static bool x86_test_l1tf_invariant() {
+  BEGIN_TEST;
+
+  // Mitigating L1TF requires that no PTE with the present bit set to false points to a page frame.
+  // Check the page tables for the kernel physmap and for the bottom 512 GB of the user address space
+  // of the current address space.
+  //
+  // A complete check would check every valid address of every address space, which could take too
+  // long to be included in a kernel unit test; based on examination of code and this unit test,
+  // we have some confidence the kernel is not breaking this invariant.
+  constexpr size_t kUserMemoryToCheck = 512 * GB;
+
+  // Check all page tables for the physmap, to make sure there are no page table enties with a valid
+  // address but the present bit not set.
+  uint64_t* const pml4 = reinterpret_cast<uint64_t*>(X86_PHYS_TO_VIRT(
+    VmAspace::kernel_aspace()->arch_aspace().pt_phys()));
+  for (uintptr_t addr = PHYSMAP_BASE; addr < (PHYSMAP_BASE + PHYSMAP_SIZE);) {
+    uint64_t pte = 0;
+    size_t step = 0;
+    bool ok = check_virtual_address_l1tf_invariant(pml4, addr, &pte, &step);
+    DEBUG_ASSERT(step > 0);
+    char error_address[32];
+    if (!ok) {
+      snprintf(error_address, sizeof(error_address), "0x%lx pte=%lx ", addr, pte);
+    }
+    EXPECT_TRUE(ok, error_address);
+    addr += step;
+  }
+
+  // Check kUserMemoryToCheck addresses, to make sure there are no page tables with a valid address
+  // but with the present bit set.
+  for (uintptr_t addr = USER_ASPACE_BASE; addr < (USER_ASPACE_BASE + kUserMemoryToCheck);) {
+    uint64_t pte = 0;
+    size_t step = 0;
+    bool ok = check_virtual_address_l1tf_invariant(pml4, addr, &pte, &step);
+    char error_address[32];
+    snprintf(error_address, sizeof(error_address), "0x%lx pte=%lx ", addr, pte);
+    EXPECT_TRUE(ok, error_address);
+    addr += step;
+  }
+
+  END_TEST;
+}
+
 UNITTEST_START_TESTCASE(x86_mmu_tests)
 UNITTEST("user-aspace page table tests", x86_arch_vmaspace_usermmu_tests)
+UNITTEST("l1tf test", x86_test_l1tf_invariant)
 UNITTEST_END_TESTCASE(x86_mmu_tests, "x86_mmu", "x86 mmu tests")
