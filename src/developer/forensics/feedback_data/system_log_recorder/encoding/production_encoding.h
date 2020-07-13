800b7e47f1ec1e552b7e3de41de41d1a7874c47e
Daniel Salazar Villarreal <villarreald@google.com>
2020-07-13 23:32:50 +0000

    [feedback] Switch encoding for previous boot logs
    * Switch from IdentityEncoding to Lz4Encoding.
    * The package size increases by 16kB.
    
    TESTED=`crasher` (report ID c8beebf47759322a)
    TESTED=`fx test feedback-data-tests`
    
    Bug: 51563
    
    Change-Id: I1535f55b84c7484439242312409e581b7079798d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/406393
    Commit-Queue: Daniel Salazar Villarreal <villarreald@google.com>
    Reviewed-by: Francois Rousseau <frousseau@google.com>
    Testability-Review: Francois Rousseau <frousseau@google.com>

--- src/developer/forensics/feedback_data/system_log_recorder/encoding/production_encoding.h
+++ src/developer/forensics/feedback_data/system_log_recorder/encoding/production_encoding.h
@@ -5,16 +5,16 @@
 #ifndef SRC_DEVELOPER_FORENSICS_FEEDBACK_DATA_SYSTEM_LOG_RECORDER_ENCODING_PRODUCTION_ENCODING_H_
 #define SRC_DEVELOPER_FORENSICS_FEEDBACK_DATA_SYSTEM_LOG_RECORDER_ENCODING_PRODUCTION_ENCODING_H_
 
-#include "src/developer/forensics/feedback_data/system_log_recorder/encoding/identity_decoder.h"
-#include "src/developer/forensics/feedback_data/system_log_recorder/encoding/identity_encoder.h"
+#include "src/developer/forensics/feedback_data/system_log_recorder/encoding/lz4_decoder.h"
+#include "src/developer/forensics/feedback_data/system_log_recorder/encoding/lz4_encoder.h"
 
 namespace forensics {
 namespace feedback_data {
 namespace system_log_recorder {
 
 // Set the encoder - decoder pair for encoding log messages in production.
-using ProductionDecoder = IdentityDecoder;
-using ProductionEncoder = IdentityEncoder;
+using ProductionDecoder = Lz4Decoder;
+using ProductionEncoder = Lz4Encoder;
 
 }  // namespace system_log_recorder
 }  // namespace feedback_data
