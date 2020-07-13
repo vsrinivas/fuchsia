f58e41cb95a2a859c785935a632c3bd47be526b9
Joshua Conner <joshconner@google.com>
2020-07-13 14:25:15 +0000

    [brcmfamc] Refactor passive_scan_test
    Take advantage of recently-added features in SimInterface to
    simplify passive_scan_test.
    
    Bug: 53992
    Test: This is a refactor of an existing test.
    Change-Id: Ic6314682bdc4992ae3cf60a7e73762a46c8dee72
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/406114
    Commit-Queue: Joshua Conner <joshconner@google.com>
    Reviewed-by: Zhiyi Chen <zhiyichen@google.com>
    Testability-Review: Zhiyi Chen <zhiyichen@google.com>

--- src/connectivity/wlan/drivers/third_party/broadcom/brcmfmac/sim/test/passive_scan_test.cc
+++ src/connectivity/wlan/drivers/third_party/broadcom/brcmfmac/sim/test/passive_scan_test.cc
@@ -14,6 +14,8 @@
 
 namespace wlan::brcmfmac {
 
+class PassiveScanTest;
+
 struct ApInfo {
   explicit ApInfo(simulation::Environment* env, const common::MacAddr& bssid,
                   const wlan_ssid_t& ssid, const wlan_channel_t& chan)
@@ -23,57 +25,38 @@ struct ApInfo {
   size_t beacons_seen_count_ = 0;
 };
 
+class PassiveScanTestInterface : public SimInterface {
+ public:
+  void OnScanResult(const wlanif_scan_result_t* result) override;
+  void OnScanEnd(const wlanif_scan_end_t* end) override;
+
+  PassiveScanTest* test_ = nullptr;
+};
+
 class PassiveScanTest : public SimTest {
  public:
-  static constexpr zx::duration kBeaconInterval = zx::msec(100);
-  static constexpr uint32_t kDwellTimeMs = 120;
+  // Set our beacon interval to 80% of the passive scan dwell time
+  static constexpr zx::duration kBeaconInterval =
+      zx::msec((SimInterface::kDefaultPassiveScanDwellTimeMs / 5) * 4);
 
-  PassiveScanTest() = default;
   void Init();
   void StartFakeAp(const common::MacAddr& bssid, const wlan_ssid_t& ssid,
                    const wlan_channel_t& chan, zx::duration beacon_interval = kBeaconInterval);
 
-  // Event handlers
-  void EndSimulation();
-  void StartScan();
-
-  enum { NOT_STARTED, RUNNING, COMPLETE } scan_state_ = NOT_STARTED;
-  bool all_aps_seen_ = false;
-
-  // SME standin functions
-  void OnScanResult(const wlanif_scan_result_t* result);
-  void OnScanEnd(const wlanif_scan_end_t* end);
-
- private:
-  // This is the interface we will use for our single client interface
-  SimInterface client_ifc_;
-
   // All simulated APs
   std::list<std::unique_ptr<ApInfo>> aps_;
 
-  // Txn ID for the current scan
-  uint64_t scan_txn_id_ = 0;
+  bool all_aps_seen_ = false;
 
-  // SME callbacks
-  static wlanif_impl_ifc_protocol_ops_t sme_ops_;
-  wlanif_impl_ifc_protocol sme_protocol_ = {.ops = &sme_ops_, .ctx = this};
-};
-
-// Since we're acting as wlanif, we need handlers for any protocol calls we may receive
-wlanif_impl_ifc_protocol_ops_t PassiveScanTest::sme_ops_ = {
-    .on_scan_result =
-        [](void* cookie, const wlanif_scan_result_t* result) {
-          static_cast<PassiveScanTest*>(cookie)->OnScanResult(result);
-        },
-    .on_scan_end =
-        [](void* cookie, const wlanif_scan_end_t* end) {
-          static_cast<PassiveScanTest*>(cookie)->OnScanEnd(end);
-        },
+ protected:
+  // This is the interface we will use for our single client interface
+  PassiveScanTestInterface client_ifc_;
 };
 
 void PassiveScanTest::Init() {
   ASSERT_EQ(SimTest::Init(), ZX_OK);
-  ASSERT_EQ(StartInterface(WLAN_INFO_MAC_ROLE_CLIENT, &client_ifc_, &sme_protocol_), ZX_OK);
+  ASSERT_EQ(StartInterface(WLAN_INFO_MAC_ROLE_CLIENT, &client_ifc_), ZX_OK);
+  client_ifc_.test_ = this;
 }
 
 // Create a new AP with the specified parameters, and tell it to start beaconing
@@ -84,37 +67,13 @@ void PassiveScanTest::StartFakeAp(const common::MacAddr& bssid, const wlan_ssid_
   aps_.push_back(std::move(ap_info));
 }
 
-// Called when simulation time has run out. Takes down all fake APs and the simulated DUT.
-void PassiveScanTest::EndSimulation() {
-  for (auto ap_info = aps_.begin(); ap_info != aps_.end(); ap_info++) {
-    (*ap_info)->ap_.DisableBeacon();
-  }
-}
-
-// Tell the DUT to run a scan
-void PassiveScanTest::StartScan() {
-  wlanif_scan_req_t req = {
-      .txn_id = ++scan_txn_id_,
-      .bss_type = WLAN_BSS_TYPE_INFRASTRUCTURE,
-      .scan_type = WLAN_SCAN_TYPE_PASSIVE,
-      .num_channels = 11,
-      .channel_list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
-      .min_channel_time = kDwellTimeMs,
-      .max_channel_time = kDwellTimeMs,
-      .num_ssids = 0,
-  };
-  client_ifc_.if_impl_ops_->start_scan(client_ifc_.if_impl_ctx_, &req);
-  scan_state_ = RUNNING;
-}
-
 // Keep track of which AP we received the scan result for, using the BSSID as a unique identifier.
-void PassiveScanTest::OnScanResult(const wlanif_scan_result_t* result) {
+void PassiveScanTestInterface::OnScanResult(const wlanif_scan_result_t* result) {
+  SimInterface::OnScanResult(result);
+
   int matches_seen = 0;
 
-  ASSERT_NE(result, nullptr);
-  EXPECT_EQ(scan_txn_id_, result->txn_id);
-
-  for (auto ap_info = aps_.begin(); ap_info != aps_.end(); ap_info++) {
+  for (auto ap_info = test_->aps_.begin(); ap_info != test_->aps_.end(); ap_info++) {
     common::MacAddr mac_addr = (*ap_info)->ap_.GetBssid();
     ASSERT_EQ(sizeof(result->bss.bssid), sizeof(mac_addr.byte));
     if (!std::memcmp(result->bss.bssid, mac_addr.byte, sizeof(mac_addr.byte))) {
@@ -127,6 +86,7 @@ void PassiveScanTest::OnScanResult(const wlanif_scan_result_t* result) {
       ASSERT_LE(ssid_info.len, sizeof(ssid_info.ssid));
       EXPECT_EQ(memcmp(result->bss.ssid.data, ssid_info.ssid, ssid_info.len), 0);
 
+      // Verify channel
       wlan_channel_t channel = (*ap_info)->ap_.GetChannel();
       EXPECT_EQ(result->bss.chan.primary, channel.primary);
       EXPECT_EQ(result->bss.chan.cbw, channel.cbw);
@@ -138,10 +98,10 @@ void PassiveScanTest::OnScanResult(const wlanif_scan_result_t* result) {
   EXPECT_EQ(matches_seen, 1);
 }
 
-void PassiveScanTest::OnScanEnd(const wlanif_scan_end_t* end) {
-  scan_state_ = COMPLETE;
+void PassiveScanTestInterface::OnScanEnd(const wlanif_scan_end_t* end) {
+  SimInterface::OnScanEnd(end);
 
-  for (auto ap_info = aps_.begin(); ap_info != aps_.end(); ap_info++) {
+  for (auto ap_info = test_->aps_.begin(); ap_info != test_->aps_.end(); ap_info++) {
     if ((*ap_info)->beacons_seen_count_ == 0) {
       // Failure
       return;
@@ -149,7 +109,7 @@ void PassiveScanTest::OnScanEnd(const wlanif_scan_end_t* end) {
   }
 
   // The beacons from all APs were seen
-  all_aps_seen_ = true;
+  test_->all_aps_seen_ = true;
 }
 
 constexpr wlan_channel_t kDefaultChannel = {
@@ -160,6 +120,7 @@ const common::MacAddr kDefaultBssid({0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc});
 TEST_F(PassiveScanTest, BasicFunctionality) {
   constexpr zx::duration kScanStartTime = zx::sec(1);
   constexpr zx::duration kDefaultTestDuration = zx::sec(100);
+  constexpr uint64_t kScanId = 0x1248;
 
   // Create our simulated device
   Init();
@@ -168,12 +129,9 @@ TEST_F(PassiveScanTest, BasicFunctionality) {
   StartFakeAp(kDefaultBssid, kDefaultSsid, kDefaultChannel);
 
   // Request a future scan
-  SCHEDULE_CALL(kScanStartTime, &PassiveScanTest::StartScan, this);
+  SCHEDULE_CALL(kScanStartTime, &PassiveScanTestInterface::StartScan, &client_ifc_, kScanId, false);
 
-  // Request a future notification so we can shut down the test
-  SCHEDULE_CALL(kDefaultTestDuration, &PassiveScanTest::EndSimulation, this);
-
-  env_->Run();
+  env_->Run(kDefaultTestDuration);
 
   EXPECT_EQ(all_aps_seen_, true);
 }
