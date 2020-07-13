7e018d451f4ef081b890ceb50f3efe5b3de7a167
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2020-07-13 20:40:57 +0000

    [fuchsia-inspect-contrib] Prepare for soft-migration of InspectDataFetcher to ArchiveReader
    To match location of ArchiveReader in the C++ contrib library.
    Moving it out of core fuchsia-inspect library.
    
    This first step is purely to enable a soft-migration.
    
    Tested: no new functionality
    Bug: 55138
    
    Change-Id: I3928c171ef3d437eb8c87a07eceb922cc790ffbf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/406329
    Commit-Queue: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Luke Nicholson <lukenicholson@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Testability-Review: Christopher Johnson <crjohns@google.com>

--- src/lib/inspect/rust/fuchsia-inspect-contrib/src/lib.rs
+++ src/lib/inspect/rust/fuchsia-inspect-contrib/src/lib.rs
@@ -6,3 +6,4 @@ pub mod inspectable;
 #[macro_use]
 pub mod log;
 pub mod nodes;
+pub mod reader;
---
+++ src/lib/inspect/rust/fuchsia-inspect-contrib/src/reader/mod.rs
@@ -0,0 +1,6 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(55138): move implementation here. This is place holder for a soft migration
+pub use fuchsia_inspect::testing::InspectDataFetcher as ArchiveReader;
