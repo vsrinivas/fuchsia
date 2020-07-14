c7d4bd60a78313f94cc2e36c46e3efa088b18104
Travis Geiselbrecht <travisg@google.com>
2020-07-14 05:22:14 +0000

    [kernel][init] Always info print the INIT hooks as they're run
    Now that we can print from very early in the kernel or the serial
    drivers will properly drop data if sent too early, no reason to filter
    out init hook traces if they're too early. The existing logic of
    filtering the trace was a leftover of a more fragile time.
    
    Tested: Validate the early printfs work on arm and x86 on qemu.
    Change-Id: I11624ae3602f566567d2882e4c0571a7c0761c74
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/406356
    Commit-Queue: Travis Geiselbrecht <travisg@google.com>
    Reviewed-by: Adrian Danis <adanis@google.com>
    Reviewed-by: Venkatesh Srinivas <venkateshs@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Testability-Review: Adrian Danis <adanis@google.com>
    Testability-Review: Nick Maniscalco <maniscalco@google.com>

--- zircon/kernel/top/init.cc
+++ zircon/kernel/top/init.cc
@@ -20,10 +20,6 @@
 #include <arch/ops.h>
 
 #define LOCAL_TRACE 0
-#define TRACE_INIT (LK_DEBUGLEVEL >= 2)
-#ifndef EARLIEST_TRACE_LEVEL
-#define EARLIEST_TRACE_LEVEL (LK_INIT_LEVEL_PLATFORM_EARLY + 1)
-#endif
 
 extern const struct lk_init_struct __start_lk_init[];
 extern const struct lk_init_struct __stop_lk_init[];
@@ -77,12 +73,8 @@ void lk_init_level(enum lk_init_flags required_flag, uint start_level, uint stop
     if (!found)
       break;
 
-    if (TRACE_INIT) {
-      if (found->level >= EARLIEST_TRACE_LEVEL) {
-        printf("INIT: cpu %u, calling hook %p (%s) at level %#x, flags %#x\n", arch_curr_cpu_num(),
-               found->hook, found->name, found->level, found->flags);
-      }
-    }
+    dprintf(INFO, "INIT: cpu %u, calling hook %p (%s) at level %#x, flags %#x\n", arch_curr_cpu_num(),
+           found->hook, found->name, found->level, found->flags);
 
     found->hook(found->level);
     last_called_level = found->level;
