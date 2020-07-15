d43bf3b41de33f1da53e79af1370ae87f19aa04f
Mark Seaborn <mseaborn@google.com>
2020-07-15 06:21:19 +0000

    [blktest] Cleanup: Remove test case that was never enabled
    This test case was #if'd out when it was originally added in 2017 in
    this change:
    https://fuchsia.googlesource.com/fuchsia/+/4ef35f3b8366d64cccb0fe6e240fb101238d7dfb%5E%21/
    
    It wouldn't have compiled then if it were enabled because the
    arguments passed to get_testdev() didn't match the function's type.
    
    Bug: 51652
    Test: blktest is run by ftl-integration-test on bringup builders on CQ
    Change-Id: Ic81d8ac33c26fef381cffa0bd11f2259aab4ac90
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/406923
    Reviewed-by: Manali Bhutiyani <manalib@google.com>
    Testability-Review: Manali Bhutiyani <manalib@google.com>
    Commit-Queue: Mark Seaborn <mseaborn@google.com>

--- src/devices/block/bin/blktest/blktest.cc
+++ src/devices/block/bin/blktest/blktest.cc
@@ -116,35 +116,6 @@ TEST(BlkdevTests, blkdev_test_bad_requests) {
   ASSERT_EQ(read(fd.get(), buf.get(), blk_size), -1);
 }
 
-#if 0
-TEST(BlkdevTests, DISABLED_blkdev_test_multiple) {
-  uint8_t buf[PAGE_SIZE];
-  uint8_t out[PAGE_SIZE];
-
-  int fd1 = get_testdev("blkdev-test-A", PAGE_SIZE, 512);
-  int fd2 = get_testdev("blkdev-test-B", PAGE_SIZE, 512);
-
-  // Write 'a' to fd1, write 'b', to fd2
-  memset(buf, 'a', sizeof(buf));
-  ASSERT_EQ(write(fd1, buf, sizeof(buf)), (ssize_t)sizeof(buf), "");
-  memset(buf, 'b', sizeof(buf));
-  ASSERT_EQ(write(fd2, buf, sizeof(buf)), (ssize_t)sizeof(buf), "");
-
-  ASSERT_EQ(lseek(fd1, 0, SEEK_SET), 0, "");
-  ASSERT_EQ(lseek(fd2, 0, SEEK_SET), 0, "");
-
-  // Read 'b' from fd2, read 'a' from fd1
-  ASSERT_EQ(read(fd2, out, sizeof(buf)), (ssize_t)sizeof(buf), "");
-  ASSERT_EQ(memcmp(out, buf, sizeof(out)), 0, "");
-  close(fd2);
-
-  memset(buf, 'a', sizeof(buf));
-  ASSERT_EQ(read(fd1, out, sizeof(buf)), (ssize_t)sizeof(buf), "");
-  ASSERT_EQ(memcmp(out, buf, sizeof(out)), 0, "");
-  close(fd1);
-}
-#endif
-
 TEST(BlkdevTests, blkdev_test_fifo_no_op) {
   // Get a FIFO connection to a blkdev and immediately close it
   uint64_t blk_size, blk_count;
