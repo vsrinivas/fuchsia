1b67cf5f0736ccb07bb455cf7f986d30a0e0d0fb
Chris Suter <csuter@google.com>
2020-07-16 04:03:38 +0000

    [storage] Move sync tests to new fs_test framework.
    Bug: 52611
    Change-Id: I81173e343dee95542347c092c28cb3a5b566d257
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/406758
    Commit-Queue: Chris Suter <csuter@google.com>
    Reviewed-by: Vikram Auradkar <auradkar@google.com>
    Testability-Review: Chris Suter <csuter@google.com>

--- src/storage/fs_test/sync.cc
+++ src/storage/fs_test/sync.cc
@@ -8,35 +8,43 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include "filesystems.h"
-#include "misc.h"
+#include "src/storage/fs_test/fs_test_fixture.h"
+
+namespace fs_test {
+namespace {
+
+using SyncTest = FilesystemTest;
 
 // TODO(smklein): Create a more complex test, capable of mocking a block device
 // and ensuring that data is actually being flushed to a block device.
 // For now, test that 'fsync' and 'fdatasync' don't throw errors for file and
 // directories.
-bool test_sync(void) {
-  BEGIN_TEST;
-
-  int fd = open("::alpha", O_RDWR | O_CREAT | O_EXCL, 0644);
-  ASSERT_GT(fd, 0, "");
-  ASSERT_STREAM_ALL(write, fd, "Hello, World!\n", 14);
-  ASSERT_EQ(fsync(fd), 0, "");
-  ASSERT_EQ(lseek(fd, 0, SEEK_SET), 0, "");
-  ASSERT_STREAM_ALL(write, fd, "Adios, World!\n", 14);
-  ASSERT_EQ(fdatasync(fd), 0, "");
-  ASSERT_EQ(close(fd), 0, "");
-  ASSERT_EQ(unlink("::alpha"), 0, "");
-
-  ASSERT_EQ(mkdir("::dirname", 0755), 0, "");
-  fd = open("::dirname", O_RDONLY | O_DIRECTORY, 0644);
-  ASSERT_GT(fd, 0, "");
-  ASSERT_EQ(fsync(fd), 0, "");
-  ASSERT_EQ(fdatasync(fd), 0, "");
-  ASSERT_EQ(close(fd), 0, "");
-  ASSERT_EQ(unlink("::dirname"), 0, "");
-
-  END_TEST;
+TEST_P(SyncTest, VerifyNoErrorsForFsync) {
+  const std::string alpha = GetPath("alpha");
+  int fd = open(alpha.c_str(), O_RDWR | O_CREAT | O_EXCL, 0644);
+  ASSERT_GT(fd, 0);
+  ASSERT_EQ(write(fd, "Hello, World!\n", 14), 14);
+  ASSERT_EQ(fsync(fd), 0);
+  ASSERT_EQ(lseek(fd, 0, SEEK_SET), 0);
+  ASSERT_EQ(write(fd, "Adios, World!\n", 14), 14);
+  ASSERT_EQ(fdatasync(fd), 0);
+  ASSERT_EQ(close(fd), 0);
+  ASSERT_EQ(unlink(alpha.c_str()), 0);
 }
 
-RUN_FOR_ALL_FILESYSTEMS(sync_tests, RUN_TEST_MEDIUM(test_sync))
+TEST_P(SyncTest, VerifyNoErrorsForFdatasync) {
+  const std::string dirname = GetPath("dirname");
+  ASSERT_EQ(mkdir(dirname.c_str(), 0755), 0);
+  int fd = open(dirname.c_str(), O_RDONLY | O_DIRECTORY, 0644);
+  ASSERT_GT(fd, 0);
+  ASSERT_EQ(fsync(fd), 0);
+  ASSERT_EQ(fdatasync(fd), 0);
+  ASSERT_EQ(close(fd), 0);
+  ASSERT_EQ(unlink(dirname.c_str()), 0);
+}
+
+INSTANTIATE_TEST_SUITE_P(/*no prefix*/, SyncTest, testing::ValuesIn(AllTestFilesystems()),
+                         testing::PrintToStringParamName());
+
+}  // namespace
+}  // namespace fs_test
