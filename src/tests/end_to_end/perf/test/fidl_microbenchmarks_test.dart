c6b9e71f031ba4fedb62d78034c79a5b163ba906
Benjamin Prosnitz <bprosnitz@google.com>
2020-07-17 19:56:42 +0000

    [fidl][benchmarks] Run FIDL perftest benchmark processes multiple times
    This is similar to what was done for fuchsia_microbenchmarks. The
    benchmarks tend to converge to different means across process runs so
    running a process multiple times will give more stable results for
    perfcompare.
    
    This is only done for C++ FIDL benchmarks that use perftest because
    these benchmarks tend to run faster than the other benchmarks. In the
    future, we might consider increasing the speed of the other benchmarks
    and also running their process multiple times.
    
    Change-Id: I75f77104551a81af2bffdbb3234169a8d413e993
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/407555
    Reviewed-by: Mark Seaborn <mseaborn@google.com>
    Testability-Review: Mark Seaborn <mseaborn@google.com>
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>

--- src/tests/end_to_end/perf/test/fidl_microbenchmarks_test.dart
+++ src/tests/end_to_end/perf/test/fidl_microbenchmarks_test.dart
@@ -9,15 +9,15 @@ import 'helpers.dart';
 
 var _tests = [];
 
+const int perftestProcessRuns = 6;
+
 String tmpPerfResultsJson(String benchmarkBinary) {
   return '/tmp/perf_results_$benchmarkBinary.json';
 }
 
-void runFidlBenchmark(String benchmarkBinary, [String optArgs]) {
+void runFidlBenchmark(String benchmarkBinary, String args) {
   final resultsFile = tmpPerfResultsJson(benchmarkBinary);
-  final path = (optArgs == null)
-      ? '/bin/$benchmarkBinary -p --quiet --out $resultsFile'
-      : '/bin/$benchmarkBinary $optArgs';
+  final path = '/bin/$benchmarkBinary $args';
   _tests.add(() {
     test(benchmarkBinary, () async {
       final helper = await PerfTestHelper.make();
@@ -28,18 +28,48 @@ void runFidlBenchmark(String benchmarkBinary, [String optArgs]) {
   });
 }
 
+// Runs a benchmark that uses the C++ perftest runner.
+// It is believed that benchmarks converge to different means in different
+// process runs (and reboots). Since each of these benchmarks are currently
+// fast to run (a few secs), run the binary several times for more stability
+// in perfcompare results.
+// However, this is currently not possible with catapult_converter, so only
+// report the first result to catapult.
+void runPerftestFidlBenchmark(String benchmarkBinary) {
+  final resultsFile = tmpPerfResultsJson(benchmarkBinary);
+  _tests.add(() {
+    test(benchmarkBinary, () async {
+      final helper = await PerfTestHelper.make();
+      final result = await helper.sl4fDriver.ssh
+          .run('/bin/$benchmarkBinary -p --quiet --out $resultsFile');
+      expect(result.exitCode, equals(0));
+      // This makes the results visible to both perfcompare and Catapult.
+      await helper.processResults(resultsFile);
+
+      for (var process = 0; process < perftestProcessRuns - 1; ++process) {
+        final result = await helper.sl4fDriver.ssh
+            .run('/bin/$benchmarkBinary -p --quiet --out $resultsFile');
+        expect(result.exitCode, equals(0));
+        // This makes the results visible to perfcompare but not Catapult.
+        await helper.storage.dumpFile(resultsFile,
+            'results_fidl_microbenchmarks_process$process', 'fuchsiaperf.json');
+      }
+    }, timeout: Timeout.none);
+  });
+}
+
 void main(List<String> args) {
   enableLoggingOutput();
 
   runFidlBenchmark('go_fidl_microbenchmarks',
       '--encode_counts --out_file ${tmpPerfResultsJson('go_fidl_microbenchmarks')}');
-  runFidlBenchmark('hlcpp_fidl_microbenchmarks');
-  runFidlBenchmark('lib_fidl_microbenchmarks');
-  runFidlBenchmark('llcpp_fidl_microbenchmarks');
-  runFidlBenchmark('walker_fidl_microbenchmarks');
+  runPerftestFidlBenchmark('hlcpp_fidl_microbenchmarks');
+  runPerftestFidlBenchmark('lib_fidl_microbenchmarks');
+  runPerftestFidlBenchmark('llcpp_fidl_microbenchmarks');
+  runPerftestFidlBenchmark('walker_fidl_microbenchmarks');
   runFidlBenchmark('rust_fidl_microbenchmarks',
       tmpPerfResultsJson('rust_fidl_microbenchmarks'));
-  runFidlBenchmark('reference_fidl_microbenchmarks');
+  runPerftestFidlBenchmark('reference_fidl_microbenchmarks');
 
   _tests.add(() {
     test('dart_fidl_microbenchmarks', () async {
