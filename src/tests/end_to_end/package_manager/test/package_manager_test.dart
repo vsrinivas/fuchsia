8e3809adfa7b274b11be3ac2342e6a0bc3f574c7
Victor Chang <vfcc@google.com>
2020-07-19 22:23:18 +0000

    [e2e] Test amberctl's default name when no name is given.
    Test: fx run-e2e-tests package_manager_test
    Change-Id: I7af1202b49aa16aa886f496f4f99195a6c7bb6f6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/398916
    Commit-Queue: Victor Chang <vfcc@google.com>
    Reviewed-by: Jeremy Manson <jeremymanson@google.com>
    Reviewed-by: John Shamoon <johnshamoon@google.com>
    Testability-Review: Jeremy Manson <jeremymanson@google.com>

--- src/tests/end_to_end/package_manager/test/package_manager_test.dart
+++ src/tests/end_to_end/package_manager/test/package_manager_test.dart
@@ -264,5 +264,52 @@ void main() {
       // The `-q` flag was given to `pm serve`, so there should be no serve output.
       expect(serveOutput.contains('[pm serve]'), isFalse);
     });
+    test('Test amberctl default name behavior when no name is given.',
+        () async {
+      // Covers these commands (success cases only):
+      //
+      // Newly covered:
+      // amberctl add_src -f http://<host>:<port>/config.json
+      //
+      // Previously covered:
+      // pm serve -repo=<path> -l :<port>
+      // pkgctl repo
+      final repoPath = tempDir.path;
+      final processInfo = await setupServe(
+          log, pmPath, repoPath, 'component_hello_world-0.far', []);
+      serveProcess = processInfo[0];
+      int port = processInfo[1];
+
+      log.info('Getting Host Address');
+      final hostAddress = await formattedHostAddress(sl4fDriver);
+
+      log.info(
+          'Adding the new repository as an update source with http://$hostAddress:$port');
+      final addRepoCfgResponse = await sl4fDriver.ssh
+          .run('amberctl add_src -f http://$hostAddress:$port/config.json');
+      expect(addRepoCfgResponse.exitCode, 0);
+
+      log.info('Running pkgctl repo to list sources');
+      var listSrcsResponse = await sl4fDriver.ssh.run('pkgctl repo');
+      expect(listSrcsResponse.exitCode, 0);
+      String repoName = 'http://$hostAddress:$port';
+      // Remove the `%25ethp0003` from
+      // http://[fe80::9813:f1ff:fe9b:c411%25ethp0003]:38729
+      // (for example)
+      RegExp(r'(%\w+)]').allMatches(repoName).forEach((match) {
+        String str = match.group(1);
+        repoName = repoName.replaceAll(str, '');
+      });
+      // Clean up name to match what amberctl will do.
+      repoName = repoName
+          .replaceAll('/', '_')
+          .replaceAll(':', '_')
+          .replaceAll('[', '_')
+          .replaceAll(']', '_');
+
+      log.info('Checking repo name is $repoName');
+      var listSrcsResponseOutput = listSrcsResponse.stdout.toString();
+      expect(listSrcsResponseOutput.contains(repoName), isTrue);
+    });
   }, timeout: _timeout);
 }
