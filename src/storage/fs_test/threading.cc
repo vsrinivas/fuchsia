aaf16a33a9c35d3cc4b29309061ae657b613f9c7
Chris Suter <csuter@google.com>
2020-07-20 00:30:40 +0000

    [storage] Move threading tests to new fs_test framework.
    Bug: 52611
    Change-Id: I7168d698fbc0f7f59357c4b30c00bfa10ece0db7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/407094
    Testability-Review: Chris Suter <csuter@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>
    Reviewed-by: Chris Drouillard <cdrllrd@google.com>

--- src/storage/fs_test/threading.cc
+++ src/storage/fs_test/threading.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <atomic>
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -12,35 +11,43 @@
 #include <sys/stat.h>
 #include <threads.h>
 #include <unistd.h>
-
-#include <fbl/unique_fd.h>
-#include <unittest/unittest.h>
 #include <zircon/syscalls.h>
 
-#include "filesystems.h"
-#include "misc.h"
+#include <atomic>
 
-// Try repeatedly creating and removing a file within a directory,
-// as fast as possible, in an attempt to trigger filesystem-internal
-// threading races between creation and deletion of a file.
-template <bool reuse_subdirectory>
-bool TestInodeReuse(void) {
-  BEGIN_TEST;
+#include <fbl/unique_fd.h>
 
-  ASSERT_EQ(mkdir("::reuse", 0755), 0);
-  DIR* d = opendir("::reuse");
-  ASSERT_NONNULL(d);
+#include "src/storage/fs_test/fs_test_fixture.h"
+
+namespace fs_test {
+namespace {
+
+using ParamType = std::tuple<TestFilesystemOptions, /*reuse_subdirectory=*/bool>;
+
+class InodeReuseTest : public BaseFilesystemTest, public testing::WithParamInterface<ParamType> {
+ public:
+  InodeReuseTest() : BaseFilesystemTest(std::get<0>(GetParam())) {}
+
+  bool reuse_subdirectory() const { return std::get<1>(GetParam()); }
+};
+
+// Try repeatedly creating and removing a file within a directory, as fast as possible, in an
+// attempt to trigger filesystem-internal threading races between creation and deletion of a file.
+TEST_P(InodeReuseTest, InodeReuse) {
+  const std::string reuse = GetPath("reuse");
+  ASSERT_EQ(mkdir(reuse.c_str(), 0755), 0);
+  DIR* d = opendir(reuse.c_str());
+  ASSERT_NE(d, nullptr);
   for (size_t i = 0; i < 1000; i++) {
     ASSERT_EQ(mkdirat(dirfd(d), "foo", 0666), 0);
-    if (reuse_subdirectory) {
+    if (reuse_subdirectory()) {
       ASSERT_EQ(mkdirat(dirfd(d), "foo/bar", 0666), 0);
       ASSERT_EQ(unlinkat(dirfd(d), "foo/bar", 0), 0);
     }
     ASSERT_EQ(unlinkat(dirfd(d), "foo", 0), 0);
   }
   ASSERT_EQ(closedir(d), 0);
-  ASSERT_EQ(rmdir("::reuse"), 0);
-  END_TEST;
+  ASSERT_EQ(rmdir(reuse.c_str()), 0);
 }
 
 // Return codes from helper threads
@@ -50,56 +57,57 @@ constexpr int kUnexpectedFailure = -2;
 
 using thrd_cb_t = int(void*);
 
-// Launch some threads, and have them all execute callback
-// 'cb'.
-//
-// It is expected that:
-//  - kSuccessCount threads will return "kSuccess"
-//  - ALL OTHER threads will return "kFailure"
-//
-// In any other condition, this helper fails.
-// For example, returning "kUnexpectedFailure" from cb
-// is an easy way to fail the entire test from a background thread.
-template <size_t kNumThreads, size_t kSuccessCount>
-bool thread_action_test(thrd_cb_t cb, void* arg = nullptr) {
-  BEGIN_HELPER;
-  static_assert(kNumThreads >= kSuccessCount, "Need more threads or less successes");
+class ThreadingTest : public FilesystemTest {
+ protected:
+  // Launch some threads, and have them all execute callback 'cb'.
+  //
+  // It is expected that:
+  //  - kSuccessCount threads will return "kSuccess"
+  //  - ALL OTHER threads will return "kFailure"
+  //
+  // In any other condition, this helper fails.  For example, returning "kUnexpectedFailure" from cb
+  // is an easy way to fail the entire test from a background thread.
+  template <size_t kNumThreads, size_t kSuccessCount>
+  void ThreadActionTest(std::function<int()> cb) {
+    static_assert(kNumThreads >= kSuccessCount, "Need more threads or less successes");
 
-  thrd_t threads[kNumThreads];
-  for (size_t i = 0; i < kNumThreads; i++) {
-    ASSERT_EQ(thrd_create(&threads[i], cb, arg), thrd_success);
-  }
-
-  // Join all threads first before checking whether they were successful. This way all threads
-  // will be cleaned up even if we encounter a failure.
-  int success[kNumThreads];
-  int result[kNumThreads];
-  for (size_t i = 0; i < kNumThreads; i++) {
-    success[i] = thrd_join(threads[i], &result[i]);
-  };
-
-  size_t success_count = 0;
-  for (size_t i = 0; i < kNumThreads; i++) {
-    ASSERT_EQ(success[i], thrd_success);
-    if (result[i] == kSuccess) {
-      success_count++;
-      ASSERT_LE(success_count, kSuccessCount, "Too many succeeding threads");
-    } else {
-      ASSERT_EQ(result[i], kFailure, "Unexpected return code from worker thread");
+    thrd_t threads[kNumThreads];
+    auto callback_wrapper = +[](void* arg) {
+      auto cb = reinterpret_cast<std::function<int()>*>(arg);
+      return (*cb)();
+    };
+    for (size_t i = 0; i < kNumThreads; i++) {
+      ASSERT_EQ(thrd_create(&threads[i], callback_wrapper, &cb), thrd_success);
     }
-  }
-  ASSERT_EQ(success_count, kSuccessCount, "Not enough succeeding threads");
 
-  END_HELPER;
-}
+    // Join all threads first before checking whether they were successful. This way all threads
+    // will be cleaned up even if we encounter a failure.
+    int success[kNumThreads];
+    int result[kNumThreads];
+    for (size_t i = 0; i < kNumThreads; i++) {
+      success[i] = thrd_join(threads[i], &result[i]);
+    };
+
+    size_t success_count = 0;
+    for (size_t i = 0; i < kNumThreads; i++) {
+      ASSERT_EQ(success[i], thrd_success);
+      if (result[i] == kSuccess) {
+        success_count++;
+        ASSERT_LE(success_count, kSuccessCount) << "Too many succeeding threads";
+      } else {
+        ASSERT_EQ(result[i], kFailure) << "Unexpected return code from worker thread";
+      }
+    }
+    ASSERT_EQ(success_count, kSuccessCount) << "Not enough succeeding threads";
+  }
+};
 
 constexpr size_t kIterCount = 10;
 
-bool TestCreateUnlinkExclusive(void) {
-  BEGIN_TEST;
+TEST_P(ThreadingTest, CreateUnlinkExclusive) {
   for (size_t i = 0; i < kIterCount; i++) {
-    ASSERT_TRUE((thread_action_test<10, 1>([](void* arg) {
-      fbl::unique_fd fd(open("::exclusive", O_RDWR | O_CREAT | O_EXCL));
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 1>([this]() {
+      fbl::unique_fd fd(open(GetPath("exclusive").c_str(), O_RDWR | O_CREAT | O_EXCL));
       if (fd) {
         return close(fd.release()) == 0 ? kSuccess : kUnexpectedFailure;
       } else if (errno == EEXIST) {
@@ -108,8 +116,8 @@ bool TestCreateUnlinkExclusive(void) {
       return kUnexpectedFailure;
     })));
 
-    ASSERT_TRUE((thread_action_test<10, 1>([](void* arg) {
-      if (unlink("::exclusive") == 0) {
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 1>([this]() {
+      if (unlink(GetPath("exclusive").c_str()) == 0) {
         return kSuccess;
       } else if (errno == ENOENT) {
         return kFailure;
@@ -117,14 +125,12 @@ bool TestCreateUnlinkExclusive(void) {
       return kUnexpectedFailure;
     })));
   }
-  END_TEST;
 }
 
-bool TestMkdirRmdirExclusive(void) {
-  BEGIN_TEST;
+TEST_P(ThreadingTest, MkdirRmdirExclusive) {
   for (size_t i = 0; i < kIterCount; i++) {
-    ASSERT_TRUE((thread_action_test<10, 1>([](void* arg) {
-      if (mkdir("::exclusive", 0666) == 0) {
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 1>([this]() {
+      if (mkdir(GetPath("exclusive").c_str(), 0666) == 0) {
         return kSuccess;
       } else if (errno == EEXIST) {
         return kFailure;
@@ -132,8 +138,8 @@ bool TestMkdirRmdirExclusive(void) {
       return kUnexpectedFailure;
     })));
 
-    ASSERT_TRUE((thread_action_test<10, 1>([](void* arg) {
-      if (rmdir("::exclusive") == 0) {
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 1>([this]() {
+      if (rmdir(GetPath("exclusive").c_str()) == 0) {
         return kSuccess;
       } else if (errno == ENOENT) {
         return kFailure;
@@ -141,103 +147,86 @@ bool TestMkdirRmdirExclusive(void) {
       return kUnexpectedFailure;
     })));
   }
-  END_TEST;
 }
 
-bool TestRenameExclusive(void) {
-  BEGIN_TEST;
+TEST_P(ThreadingTest, RenameExclusive) {
   for (size_t i = 0; i < kIterCount; i++) {
     // Test case of renaming from a single source.
-    ASSERT_EQ(mkdir("::rename_start", 0666), 0);
-    ASSERT_TRUE((thread_action_test<10, 1>([](void* arg) {
-      if (rename("::rename_start", "::rename_end") == 0) {
+    ASSERT_EQ(mkdir(GetPath("rename_start").c_str(), 0666), 0);
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 1>([this]() {
+      if (rename(GetPath("rename_start").c_str(), GetPath("rename_end").c_str()) == 0) {
         return kSuccess;
       } else if (errno == ENOENT) {
         return kFailure;
       }
       return kUnexpectedFailure;
     })));
-    ASSERT_EQ(rmdir("::rename_end"), 0);
+    ASSERT_EQ(rmdir(GetPath("rename_end").c_str()), 0);
 
-    // Test case of renaming from multiple sources at once,
-    // to a single destination
+    // Test case of renaming from multiple sources at once, to a single destination
     std::atomic<uint32_t> ctr{0};
-    ASSERT_TRUE((thread_action_test<10, 1>(
-        [](void* arg) {
-          auto ctr = reinterpret_cast<std::atomic<uint32_t>*>(arg);
-          char start[128];
-          snprintf(start, sizeof(start) - 1, "::rename_start_%u", ctr->fetch_add(1));
-          if (mkdir(start, 0666)) {
-            return kUnexpectedFailure;
-          }
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 1>([this, &ctr]() {
+      char start[128];
+      snprintf(start, sizeof(start) - 1, GetPath("rename_start_%u").c_str(), ctr.fetch_add(1));
+      if (mkdir(start, 0666)) {
+        return kUnexpectedFailure;
+      }
 
-          // Give the target a child, so it cannot be overwritten as a target
-          char child[256];
-          snprintf(child, sizeof(child) - 1, "%s/child", start);
-          if (mkdir(child, 0666)) {
-            return kUnexpectedFailure;
-          }
+      // Give the target a child, so it cannot be overwritten as a target
+      char child[256];
+      snprintf(child, sizeof(child) - 1, "%s/child", start);
+      if (mkdir(child, 0666)) {
+        return kUnexpectedFailure;
+      }
 
-          if (rename(start, "::rename_end") == 0) {
-            return kSuccess;
-          } else if (errno == ENOTEMPTY || errno == EEXIST) {
-            return rmdir(child) == 0 && rmdir(start) == 0 ? kFailure : kUnexpectedFailure;
-          }
-          return kUnexpectedFailure;
-        },
-        &ctr)));
+      if (rename(start, GetPath("rename_end").c_str()) == 0) {
+        return kSuccess;
+      } else if (errno == ENOTEMPTY || errno == EEXIST) {
+        return rmdir(child) == 0 && rmdir(start) == 0 ? kFailure : kUnexpectedFailure;
+      }
+      return kUnexpectedFailure;
+    })));
 
-    DIR* dir = opendir("::rename_end");
-    ASSERT_NONNULL(dir);
+    DIR* dir = opendir(GetPath("rename_end").c_str());
+    ASSERT_NE(dir, nullptr);
     struct dirent* de;
     while ((de = readdir(dir)) && de != nullptr) {
       unlinkat(dirfd(dir), de->d_name, AT_REMOVEDIR);
     }
     ASSERT_EQ(closedir(dir), 0);
-    ASSERT_EQ(rmdir("::rename_end"), 0);
+    ASSERT_EQ(rmdir(GetPath("rename_end").c_str()), 0);
   }
-  END_TEST;
 }
 
-bool TestRenameOverwrite(void) {
-  BEGIN_TEST;
+TEST_P(ThreadingTest, RenameOverwrite) {
   for (size_t i = 0; i < kIterCount; i++) {
-    // Test case of renaming from multiple sources at once,
-    // to a single destination
+    // Test case of renaming from multiple sources at once, to a single destination
     std::atomic<uint32_t> ctr{0};
-    ASSERT_TRUE((thread_action_test<10, 10>(
-        [](void* arg) {
-          auto ctr = reinterpret_cast<std::atomic<uint32_t>*>(arg);
-          char start[128];
-          snprintf(start, sizeof(start) - 1, "::rename_start_%u", ctr->fetch_add(1));
-          if (mkdir(start, 0666)) {
-            return kUnexpectedFailure;
-          }
-          if (rename(start, "::rename_end") == 0) {
-            return kSuccess;
-          }
-          return kUnexpectedFailure;
-        },
-        &ctr)));
-    ASSERT_EQ(rmdir("::rename_end"), 0);
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 10>([this, &ctr]() {
+      char start[128];
+      snprintf(start, sizeof(start) - 1, GetPath("rename_start_%u").c_str(), ctr.fetch_add(1));
+      if (mkdir(start, 0666)) {
+        return kUnexpectedFailure;
+      }
+      if (rename(start, GetPath("rename_end").c_str()) == 0) {
+        return kSuccess;
+      }
+      return kUnexpectedFailure;
+    })));
+    ASSERT_EQ(rmdir(GetPath("rename_end").c_str()), 0);
   }
-  END_TEST;
 }
 
-bool TestLinkExclusive(void) {
-  BEGIN_TEST;
+using ThreadingLinkTest = ThreadingTest;
 
-  if (!test_info->supports_hardlinks) {
-    return true;
-  }
-
+TEST_P(ThreadingLinkTest, LinkExclusive) {
   for (size_t i = 0; i < kIterCount; i++) {
-    fbl::unique_fd fd(open("::link_start", O_RDWR | O_CREAT | O_EXCL));
+    fbl::unique_fd fd(open(GetPath("link_start").c_str(), O_RDWR | O_CREAT | O_EXCL));
     ASSERT_TRUE(fd);
     ASSERT_EQ(close(fd.release()), 0);
 
-    ASSERT_TRUE((thread_action_test<10, 1>([](void* arg) {
-      if (link("::link_start", "::link_end") == 0) {
+    ASSERT_NO_FATAL_FAILURE((ThreadActionTest<10, 1>([this]() {
+      if (link(GetPath("link_start").c_str(), GetPath("link_end").c_str()) == 0) {
         return kSuccess;
       } else if (errno == EEXIST) {
         return kFailure;
@@ -245,17 +234,43 @@ bool TestLinkExclusive(void) {
       return kUnexpectedFailure;
     })));
 
-    ASSERT_EQ(unlink("::link_start"), 0);
-    ASSERT_EQ(unlink("::link_end"), 0);
-    ASSERT_TRUE(check_remount());
+    ASSERT_EQ(unlink(GetPath("link_start").c_str()), 0);
+    ASSERT_EQ(unlink(GetPath("link_end").c_str()), 0);
+
+    if (fs().GetTraits().can_unmount) {
+      EXPECT_EQ(fs().Unmount().status_value(), 0);
+      EXPECT_EQ(fs().Fsck().status_value(), 0);
+      EXPECT_EQ(fs().Mount().status_value(), 0);
+    }
   }
-  END_TEST;
 }
 
-RUN_FOR_ALL_FILESYSTEMS(threading_tests, RUN_TEST_LARGE((TestInodeReuse<false>))
-                                             RUN_TEST_LARGE((TestInodeReuse<true>))
-                                                 RUN_TEST_MEDIUM(TestCreateUnlinkExclusive)
-                                                     RUN_TEST_MEDIUM(TestMkdirRmdirExclusive)
-                                                         RUN_TEST_LARGE(TestRenameExclusive)
-                                                             RUN_TEST_LARGE(TestRenameOverwrite)
-                                                                 RUN_TEST_LARGE(TestLinkExclusive))
+std::vector<TestFilesystemOptions> GetThreadingLinkTestCombinations() {
+  std::vector<TestFilesystemOptions> test_combinations;
+  for (TestFilesystemOptions options : AllTestFilesystems()) {
+    if (options.filesystem->GetTraits().supports_hard_links) {
+      test_combinations.push_back(options);
+    }
+  }
+  return test_combinations;
+}
+
+std::string GetParamDescription(const testing::TestParamInfo<ParamType>& param) {
+  std::stringstream s;
+  s << std::get<0>(param.param) << (std::get<1>(param.param) ? "ReusingSubdir" : "");
+  return s.str();
+}
+
+INSTANTIATE_TEST_SUITE_P(/*no prefix*/, InodeReuseTest,
+                         testing::Combine(testing::ValuesIn(AllTestFilesystems()), testing::Bool()),
+                         GetParamDescription);
+
+INSTANTIATE_TEST_SUITE_P(/*no prefix*/, ThreadingTest, testing::ValuesIn(AllTestFilesystems()),
+                         testing::PrintToStringParamName());
+
+INSTANTIATE_TEST_SUITE_P(/*no prefix*/, ThreadingLinkTest,
+                         testing::ValuesIn(GetThreadingLinkTestCombinations()),
+                         testing::PrintToStringParamName());
+
+}  // namespace
+}  // namespace fs_test
