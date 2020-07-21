d1ac8ee6b4e06cfa991ffebe4a339a6bb0dd052f
Arman Uguray <armansito@google.com>
2020-07-21 18:57:31 +0000

    [bt] Add asserting version of UUID::FromBytes
    Added an assertion constructor version of UUID::FromBytes. This removes
    some boilerplate as most production code that relies on FromBytes often
    asserts success afterwards.
    
    Test: bt-host-unittests (new UUIDTest and existing tests)
    Change-Id: I5f67c1e546fa5882f2b4e7204f2173f7f7885fc5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/408874
    Commit-Queue: Arman Uguray <armansito@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Testability-Review: Ben Lawson <benlawson@google.com>

--- src/connectivity/bluetooth/core/bt-host/gatt/client.cc
+++ src/connectivity/bluetooth/core/bt-host/gatt/client.cc
@@ -257,10 +257,8 @@ class Impl final : public Client {
             }
 
             // This must succeed as we have performed the appropriate checks above.
-            UUID uuid;
             BufferView uuid_bytes(entry.value, entry_length - (2 * sizeof(att::Handle)));
-            __UNUSED bool result = UUID::FromBytes(uuid_bytes, &uuid);
-            ZX_DEBUG_ASSERT(result);
+            UUID uuid(uuid_bytes);
 
             ServiceData service(kind, start, end, uuid);
             last_handle = service.range_end;
@@ -500,9 +498,7 @@ class Impl final : public Client {
 
         // This must succeed as we have performed the necessary checks
         // above.
-        UUID type;
-        __UNUSED bool result = UUID::FromBytes(value.view(3), &type);
-        ZX_DEBUG_ASSERT(result);
+        UUID type(value.view(3));
 
         // Notify the handler. By default, there are no extended properties to report.
         chrc_cb(CharacteristicData(properties, /*extended_properties=*/std::nullopt, chrc_handle,
