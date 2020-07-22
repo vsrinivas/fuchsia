77d8ce9ea7e562a2eea61c1a8b89263befca7798
Tamir Duberstein <tamird@google.com>
2020-07-22 11:05:06 +0000

    [zxio] Use smart types in debuglog
    - Use an iterator instead of raw index
    - Check `write` return value
    - Use fbl::AutoLock
    
    Change-Id: I6880ce259c8f1a5e4ee6bb0e38c688fa707ca4aa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/408975
    Commit-Queue: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Testability-Review: Yifei Teng <yifeit@google.com>

--- zircon/system/ulib/zxio/debuglog.cc
+++ zircon/system/ulib/zxio/debuglog.cc
@@ -2,13 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <lib/sync/mutex.h>
 #include <lib/zxio/inception.h>
 #include <lib/zxio/null.h>
 #include <lib/zxio/ops.h>
 #include <zircon/syscalls/log.h>
 
 #include <array>
+
+#include <fbl/auto_lock.h>
+#include <fbl/mutex.h>
 
 #include "private.h"
 
@@ -32,12 +34,14 @@ class Debuglog : public HasIo {
 
   static const zxio_ops_t kOps;
 
+  struct Buffer {
+    std::array<char, LOGBUF_MAX> pending;
+    decltype(pending)::iterator it;
+  };
+
   zx::debuglog handle_;
-  sync_mutex_t lock_;
-  struct {
-    unsigned next = 0;
-    std::unique_ptr<std::array<char, LOGBUF_MAX>> pending;
-  } buffer_ __TA_GUARDED(lock_);
+  fbl::Mutex lock_;
+  std::unique_ptr<Buffer> buffer_ __TA_GUARDED(lock_);
 };
 
 constexpr zxio_ops_t Debuglog::kOps = ([]() {
@@ -68,49 +72,58 @@ zx_status_t Debuglog::Writev(const zx_iovec_t* vector, size_t vector_count, zxio
     return ZX_ERR_NOT_SUPPORTED;
   }
 
-  auto& outgoing = buffer_;
-  sync_mutex_lock(&lock_);
-  if (outgoing.pending == nullptr) {
-    outgoing.pending = std::make_unique<std::array<char, LOGBUF_MAX>>();
+  fbl::AutoLock lock(&lock_);
+  if (buffer_ == nullptr) {
+    buffer_ = std::make_unique<Buffer>();
+    buffer_->it = buffer_->pending.begin();
   }
+  Buffer& outgoing = *buffer_;
 
-  zx_status_t status = zxio_do_vector(
-      vector, vector_count, out_actual, [&](void* buffer, size_t capacity, size_t* out_actual) {
-        // Convince the compiler that the lock is held here. This is safe
-        // because the lock is held over the call to zxio_do_vector and
-        // zxio_do_vector is synchronous, so it cannot extend the life of this
-        // lambda.
-        //
-        // This is required because the compiler does its analysis
-        // function-by-function. In this function, it can't see that the lock is
-        // held by the calling scope. If We annotate this function with
-        // TA_REQUIRES, then this function compiles, but its call in
-        // zxio_do_vector doesn't, because the compiler can't tell that the lock
-        // is held in that function.
-        []() __TA_ASSERT(lock_) {}();
-        const char* data = static_cast<const char*>(buffer);
-        for (size_t i = 0; i < capacity; ++i) {
-          char c = *data++;
-          if (c == '\n') {
-            handle_.write(0, outgoing.pending->data(), outgoing.next);
-            outgoing.next = 0;
-            continue;
-          }
-          if (c < ' ') {
-            continue;
-          }
-          (*outgoing.pending)[outgoing.next++] = c;
-          if (outgoing.next == LOGBUF_MAX) {
-            handle_.write(0, outgoing.pending->data(), outgoing.next);
-            outgoing.next = 0;
-            continue;
-          }
+  auto flush = [&]() __TA_REQUIRES(lock_) {
+    zx_status_t status =
+        handle_.write(0, outgoing.pending.data(), outgoing.it - outgoing.pending.begin());
+    outgoing.it = outgoing.pending.begin();
+    return status;
+  };
+
+  auto write = [&](void* buffer, size_t capacity, size_t* out_actual) {
+    // Convince the compiler that the lock is held here. This is safe because the lock is held over
+    // the call to zxio_do_vector and zxio_do_vector is synchronous, so it cannot extend the life of
+    // this lambda.
+    //
+    // This is required because the compiler does its analysis function-by-function. In this
+    // function, it can't see that the lock is held by the calling scope. If We annotate this
+    // function with TA_REQUIRES, then this function compiles, but its call in zxio_do_vector
+    // doesn't, because the compiler can't tell that the lock is held in that function.
+    []() __TA_ASSERT(lock_) {}();
+    const char* data = static_cast<const char*>(buffer);
+    for (size_t i = 0; i < capacity; ++i) {
+      char c = data[i];
+      if (c == '\n') {
+        zx_status_t status = flush();
+        if (status != ZX_OK) {
+          return status;
         }
-        *out_actual = capacity;
-        return ZX_OK;
-      });
-  sync_mutex_unlock(&lock_);
-  return status;
+        continue;
+      }
+      if (c < ' ') {
+        continue;
+      }
+      *outgoing.it = c;
+      ++outgoing.it;
+      if (outgoing.it == outgoing.pending.end()) {
+        zx_status_t status = flush();
+        if (status != ZX_OK) {
+          return status;
+        }
+        continue;
+      }
+    }
+    *out_actual = capacity;
+    return ZX_OK;
+  };
+
+  return zxio_do_vector(vector, vector_count, out_actual, write);
 }
 
 zx_status_t Debuglog::IsATty(bool* tty) {
