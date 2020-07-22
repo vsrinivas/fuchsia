dc058ede35c7af2b6398210d4652336b8b3e8620
Roland McGrath <mcgrathr@google.com>
2020-07-22 19:53:19 +0000

    [build] Enable leak detection in asan builds
    This reworks the (still temporary!) suppression machinery so
    that it does not interfere with asan_default_options GN build
    argument settings.
    
    Bug: 45047
    Change-Id: I9a30bf5c8e05576f30e14e7897ecdb56f13c053c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/372841
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Petr Hosek <phosek@google.com>

--- build/config/sanitizers/asan_default_options.c
+++ build/config/sanitizers/asan_default_options.c
@@ -4,10 +4,21 @@
 
 #include <sanitizer/asan_interface.h>
 
-// This exists to be built into every executable selected to use the
-// asan_no_detect_leaks variant.  ASan applies the options here before
-// looking at the ASAN_OPTIONS environment variable.
+// TODO(45047): BUILD.gn machinery passes the suppress-lsan.ld input linker
+// script to define this symbol as some nonzero address (never to be
+// dereferenced, just observed as nonzero).
+__attribute__((weak)) extern struct {
+} _FUCHSIA_SUPPRESS_LSAN;
+
+// ASan applies the options here before looking at the ASAN_OPTIONS
+// environment variable.
 const char* __asan_default_options(void) {
+  // TODO(45047): Remove this later.  If the magic cookie was linked in,
+  // add LSan suppression to the compiled-in options list.
+  if (&_FUCHSIA_SUPPRESS_LSAN) {
+    return ASAN_DEFAULT_OPTIONS ":detect_leaks=0";
+  }
+
   // This macro is defined by BUILD.gn from the `asan_default_options` GN
   // build argument.
   return ASAN_DEFAULT_OPTIONS;
