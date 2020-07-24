85d71da8fda3f75ae9544c8afac344479b6dbeca
Nick Maniscalco <maniscalco@google.com>
2020-07-24 19:56:26 +0000

    [kernel][msi] Remove most  __WEAK msi implementations
    Remove weak symbol implementations of several MSI functions.  Relying
    on weak symbols can be risky because a change to or removal of a
    cooresponding strong symbol can result in unexpected behavior change.
    
    Note, two weak implementations remain as they are used on x64.
    
    No functional change.
    
    Change-Id: Ie8430d3827a23fd0244b9e0e87f6c67786ba5dce
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/408436
    Reviewed-by: Christopher Anderson <cja@google.com>
    Reviewed-by: George Kulakowski <kulakowski@google.com>
    Commit-Queue: Nick Maniscalco <maniscalco@google.com>
    Testability-Review: George Kulakowski <kulakowski@google.com>

--- zircon/kernel/dev/interrupt/msi.cc
+++ zircon/kernel/dev/interrupt/msi.cc
@@ -14,23 +14,8 @@
 
 #include <dev/interrupt.h>
 
-__WEAK bool msi_is_supported() { return false; }
-
 __WEAK bool msi_supports_masking() { return false; }
 
 __WEAK void msi_mask_unmask(const msi_block_t* block, uint msi_id, bool mask) {
-  PANIC_UNIMPLEMENTED;
-}
-
-__WEAK zx_status_t msi_alloc_block(uint requested_irqs, bool can_target_64bit, bool is_msix,
-                                   msi_block_t* out_block) {
-  PANIC_UNIMPLEMENTED;
-  __UNREACHABLE;
-}
-
-__WEAK void msi_free_block(msi_block_t* block) { PANIC_UNIMPLEMENTED; }
-
-__WEAK void msi_register_handler(const msi_block_t* block, uint msi_id, int_handler handler,
-                                 void* ctx) {
   PANIC_UNIMPLEMENTED;
 }
