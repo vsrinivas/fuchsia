af5a73bc3a59f265c54b3d9332c89bc9840c7e3c
Sean Gillen <sdgillen@google.com>
2020-07-24 19:08:08 +0000

    [perf] Add scroll_flutter benchmark
    Add flutter_driver support to flutter/helpers.dart. Add a runner for the
    scroll_flutter benchmark using flutter_driver.
    
    Bug: 55832
    
    Change-Id: Ic0c339da2fc70b26f5f326b09e7843484314a705
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/407356
    Reviewed-by: Brian Hamrick <bhamrick@google.com>
    Reviewed-by: Nathan Rogers <nathanrogers@google.com>
    Commit-Queue: Sean Gillen <sdgillen@google.com>
    Testability-Review: Nathan Rogers <nathanrogers@google.com>

--- src/tests/end_to_end/perf/test/flutter/helpers.dart
+++ src/tests/end_to_end/perf/test/flutter/helpers.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:flutter_driver_sl4f/flutter_driver_sl4f.dart';
 import 'package:logging/logging.dart';
 import 'package:sl4f/sl4f.dart';
 import 'package:sl4f/trace_processing.dart';
@@ -16,10 +18,10 @@ class FlutterTestHelper {
   Sl4f sl4f;
   Logger log;
 
+  FlutterDriverConnector connector;
+  FlutterDriver driver;
   Performance perf;
   Tiles tiles;
-
-  // TODO(55832): Add flutter_driver support.
 
   /// The tile key added by [launchApp]; stored so the tile can be removed by
   /// [_tearDown].
@@ -55,6 +57,9 @@ class FlutterTestHelper {
   }
 
   Future<void> _tearDown() async {
+    await driver?.close();
+    await connector?.tearDown();
+
     if (_tileKey != null) {
       await tiles.remove(_tileKey);
       log.fine('Removed tile with key $_tileKey');
@@ -65,10 +70,52 @@ class FlutterTestHelper {
   }
 
   /// Launch a given app using tiles_ctl.
-  Future<void> addTile(String appName) async {
-    _tileKey = await tiles
-        .addFromUrl('fuchsia-pkg://fuchsia.com/$appName#meta/$appName.cmx');
+  Future<void> addTile(String appName, {List<String> args}) async {
+    _tileKey = await tiles.addFromUrl(
+      'fuchsia-pkg://fuchsia.com/$appName#meta/$appName.cmx',
+      args: args,
+    );
     log.info('Launched $appName with tile key $_tileKey.');
+  }
+
+  /// Connect [driver] to the given [appName].
+  ///
+  /// Using flutter_driver requires that the app is built with flutter_driver
+  /// support and run in an environment with flutter_driver. There are two ways
+  /// to do this:
+  /// 1) Require flutter_driver in the app. Note that according to the
+  ///    documentation for |enableFlutterDriverExtension|, "applications
+  ///    intended for release should never include this method." This call will
+  ///    not work in release builds of the Flutter app.
+  ///      import 'package:flutter_driver/driver_extension.dart';
+  ///      void main() { enableFlutterDriverExtension(); }
+  /// 2) Use |flutter_driver_extendable| in the app's BUILD file.
+  ///    This adds a wrapper around main to call enableFlutterDriverExtension()
+  ///    when flutter_driver is enabled in debug builds, so release builds will
+  ///    not include flutter_driver.
+  ///      import("//build/config.gni")
+  ///      import("//build/testing/flutter_driver.gni")
+  ///      import("//topaz/runtime/flutter_runner/flutter_app.gni")
+  ///      flutter_app("example") {
+  ///        flutter_driver_extendable = flutter_driver_enabled
+  ///      }
+  ///    Then use --args=is_debug=true --args=flutter_driver_enabled=true in
+  ///    fx set to enable flutter_driver.
+  Future<void> connectFlutterDriver(String appName) async {
+    connector = FlutterDriverConnector(sl4f);
+    await connector.initialize();
+
+    final isolate = await connector.isolate(appName);
+    if (isolate == null) {
+      fail('Flutter driver could not find $appName.');
+    }
+    log.fine('Flutter driver found $appName.');
+
+    driver = await connector.driverForIsolate(appName);
+    if (driver == null) {
+      fail('Unable to connect to $appName.');
+    }
+    log.info('Flutter driver connected to $appName.');
   }
 
   /// A [MetricsProcessor] that finds the startup time of a Flutter app.
