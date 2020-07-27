8faf746c4319d2de97582cb7f13f6f85ee1bef55
Christopher R. Johnson <crjohns@google.com>
2020-07-27 20:29:49 +0000

    [e2e] Remove check for archivist CPU usage
    This causes some test flakes, and it is getting removed soon anyway.
    
    Multiply: host_x64/inspect_metrics_emulator_test
    
    Fixed: 53844
    Change-Id: I25b11a5db92b098401fdc4129573eb49f782e0fb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/410834
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Testability-Review: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Christopher Johnson <crjohns@google.com>

--- src/tests/end_to_end/inspect_metrics/test/appmgr_cpu_metrics_test.dart
+++ src/tests/end_to_end/inspect_metrics/test/appmgr_cpu_metrics_test.dart
@@ -6,9 +6,6 @@ import 'package:test/test.dart';
 import 'package:sl4f/sl4f.dart' as sl4f;
 import 'util.dart';
 
-const String archivistPath =
-    'core/appmgr:root/cpu_stats/measurements/root/app/sys/archivist.cmx';
-
 const String appmgrPath =
     'core/appmgr:root/cpu_stats/measurements/root/appmgr.cm';
 
@@ -21,35 +18,11 @@ void main() {
       sl4fDriver = sl4f.Sl4f.fromEnvironment();
       await sl4fDriver.startServer();
       inspect = sl4f.Inspect(sl4fDriver);
-
-      // Wait until some data shows up for archivist.
-      // In E2E tests sometimes the system just started up, and appmgr did not have a chance to expose data on Archivist yet.
-      // Appmgr will expose the needed information once it processes the start event for Archvist.
-      var values = [];
-      while (values.isEmpty) {
-        values = await getInspectValues(
-            inspect, '$archivistPath/*/@samples/*:cpu_time');
-      }
     });
 
     tearDown(() async {
       await sl4fDriver.stopServer();
       sl4fDriver.close();
-    });
-
-    test('appmgr exposes cpu metrics for archivist', () async {
-      expect(
-          await getInspectValues(
-              inspect, '$archivistPath/*/@samples/*:cpu_time'),
-          multiValue(greaterThan(0), length: greaterThan(0)));
-      expect(
-          await getInspectValues(
-              inspect, '$archivistPath/*/@samples/*:queue_time'),
-          multiValue(greaterThan(0), length: greaterThan(0)));
-      expect(
-          await getInspectValues(
-              inspect, '$archivistPath/*/@samples/*:timestamp'),
-          multiValue(greaterThan(0), length: greaterThan(0)));
     });
 
     test('appmgr exposes cpu metrics for itself', () async {
