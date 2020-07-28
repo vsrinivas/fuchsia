7f785b0e5a69eea01862a0cdce8cc03bf404ae10
James Tucker <raggi@google.com>
2020-07-28 18:45:02 +0000

    [ffx] Reland, daemon start starts a daemon
    The pid must be waited on unix hosts, otherwise the child process
    becomes a zombie. This implementation will perform a daemonize
    downstream of the fork, so it will succeed if the second fork works,
    but will wait for the first fork to complete, avoiding the zombie
    condition. This also prevents the hosting session leader from
    terminating the daemon at some later point. The allowed deprecation
    is for libc::daemonize that is deprecated as of macOS 10.5. We'll
    need to bring in a 3p lib or write more ffi code to replace this
    eventually.
    
    The daemon does not chdir("/"), as it does not yet have context
    information passed from the frontend sufficient to otherwise discover
    the correct environment and configuration.
    
    Bug: 57054
    Change-Id: I586743b8bf0e14a2c99832161c3bcd046a830756
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/410452
    Commit-Queue: James Tucker <raggi@google.com>
    Testability-Review: James Tucker <raggi@google.com>
    Reviewed-by: Matthew Boetger <boetger@google.com>

--- src/developer/ffx/daemon/src/lib.rs
+++ src/developer/ffx/daemon/src/lib.rs
@@ -14,7 +14,9 @@ use {
     fidl_fuchsia_overnet_protocol::NodeId,
     fuchsia_async::Task,
     futures::prelude::*,
+    libc,
     std::env,
+    std::os::unix::process::CommandExt,
     std::process::Command,
 };
 
@@ -67,13 +69,18 @@ pub async fn find_and_connect() -> Result<DaemonProxy> {
 }
 
 pub async fn spawn_daemon() -> Result<()> {
-    Command::new(env::current_exe().unwrap()).arg(DAEMON).arg("start").spawn().map(
-        // XXX(raggi): this is leaking a process wait handle, which
-        // leads to zombies. A CL in the stack below this commit will
-        // soon address this problem.
-        |_| ()
-    ).context("spawning background daemon")
+    let mut ffx_path = env::current_exe()?;
+    // when we daemonize, our path will change to /, so get the canonical path before that occurs.
+    ffx_path = std::fs::canonicalize(ffx_path)?;
+    log::info!("Starting new ffx background daemon from {:?}", &ffx_path);
+    daemonize(Command::new(ffx_path).arg(DAEMON).arg("start"))
+        .spawn()
+        .context("spawning daemon start")?
+        .wait()
+        .map(|_| ())
+        .context("waiting for daemon start")
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 // Overnet Server implementation
 
@@ -127,4 +134,55 @@ pub async fn start() -> Result<()> {
     }
     futures::try_join!(onet::run_ascendd(), exec_server(Daemon::new().await?))?;
     Ok(())
+}
+
+// daemonize adds a pre_exec to call daemon(3) causing the spawned
+// process to be forked again and detached from the controlling
+// terminal.
+fn daemonize(c: &mut Command) -> &mut Command {
+    unsafe {
+        c.pre_exec(|| {
+            // daemonize(3) is deprecated on macOS 10.15. The replacement is not
+            // yet clear, we may want to replace this with a manual double fork
+            // setsid, etc.
+            #[allow(deprecated)]
+            // XXX(57054): we currently avoid the chdir because we have
+            // not plumbed a way to tell the daemon to open it's log
+            // file (or discover the correct environment) via another
+            // means. Probably the spawn_daemon invocation should be
+            // passing this information down based on the configuration
+            // of the frontend.
+            match libc::daemon(1, 0) {
+                0 => Ok(()),
+                x => Err(std::io::Error::from_raw_os_error(x)),
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_daemonize() -> Result<(), anyhow::Error> {
+        let started = std::time::Instant::now();
+        // TODO(raggi): this technically leaks a sleep process, which is
+        // not ideal, but the much better approach would be a
+        // significant amount of work, as we'd really want a program
+        // that will wait for a signal on some other channel (such as a
+        // unix socket) and otherwise linger as a daemon. If we had
+        // that, we could then check the ppid and assert that daemon(3)
+        // really did the work we're expecting it to. As that would
+        // involve specific subprograms, finding those, and so on, it is
+        // likely beyond ROI for this test coverage, which aims to just
+        // prove that the immediate spawn() succeeded was detached from
+        // the program in question. There is a risk that this
+        // implementation passes if sleep(1) is not found, which is also
+        // not ideal.
+        let mut child = daemonize(Command::new("sleep").arg("10")).spawn()?;
+        child.wait()?;
+        assert!(started.elapsed() < std::time::Duration::from_secs(10));
+        Ok(())
+    }
 }
