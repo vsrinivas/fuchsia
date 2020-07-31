b31c44169c0d02c2185531b107f2c6fb377ae970
Ben Wright <benwright@google.com>
2020-07-31 04:45:20 +0000

    [scrutiny] Integrate ZSTD decompressing.
    ZSTD is required to decompress bootfs images this change adds a utility
    library to the scrutiny project which holds common use code that isn't
    part of the code framework. This library isn't designed to have a stable
    API like the framework.
    
    Change-Id: Idba0d73b7c0a656658d9ee6b811acbe3c57a48fe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/412223
    Reviewed-by: Daniel Hwang <arkay@google.com>
    Testability-Review: Benjamin Wright <benwright@google.com>
    Commit-Queue: Benjamin Wright <benwright@google.com>

---
+++ src/security/tools/scrutiny/utils/src/zstd.rs
@@ -0,0 +1,85 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{anyhow, Result},
+    libc::{c_char, c_int, c_void, size_t},
+    std::ffi::CStr,
+};
+
+#[link(name = "zstd")]
+extern "C" {
+    fn ZSTD_compress(
+        dst: *mut c_void,
+        dst_capacity: size_t,
+        src: *const c_void,
+        src_size: size_t,
+        compression_level: c_int,
+    ) -> size_t;
+    fn ZSTD_decompress(
+        dst: *mut c_void,
+        dst_capacity: size_t,
+        src: *const c_void,
+        compressed_size: size_t,
+    ) -> size_t;
+    fn ZSTD_isError(code: size_t) -> u32;
+    fn ZSTD_getErrorName(code: size_t) -> *const c_char;
+}
+
+pub fn compress(src: &[u8], dst_capacity: u32, level: i32) -> Result<Vec<u8>> {
+    unsafe {
+        let src_len = src.len() as size_t;
+        let src_ptr = src.as_ptr() as *const c_void;
+        let dst_len = dst_capacity as size_t;
+        let mut dst = Vec::with_capacity(dst_capacity as usize);
+        let dst_ptr = dst.as_mut_ptr() as *mut c_void;
+        let compression_level = level as c_int;
+
+        let code = ZSTD_compress(dst_ptr, dst_len, src_ptr, src_len, compression_level);
+        if ZSTD_isError(code) != 0 {
+            let error: *const c_char = ZSTD_getErrorName(code);
+            let error_str = CStr::from_ptr(error).to_str().unwrap().to_owned();
+            Err(anyhow!(error_str))
+        } else {
+            dst.set_len(code as size_t);
+            Ok(dst)
+        }
+    }
+}
+
+/// Attempts to decompress the payload and returns the result as a vector.
+pub fn decompress(src: &[u8], dst_capacity: u32) -> Result<Vec<u8>> {
+    unsafe {
+        let src_len = src.len() as size_t;
+        let src_ptr = src.as_ptr() as *const c_void;
+        let dst_len = dst_capacity as size_t;
+        let mut dst = Vec::with_capacity(dst_capacity as usize);
+        let dst_ptr = dst.as_mut_ptr() as *mut c_void;
+
+        let code = ZSTD_decompress(dst_ptr, dst_len, src_ptr, src_len);
+
+        if ZSTD_isError(code) != 0 {
+            let error: *const c_char = ZSTD_getErrorName(code);
+            let error_str = CStr::from_ptr(error).to_str().unwrap().to_owned();
+            Err(anyhow!(error_str))
+        } else {
+            dst.set_len(code as size_t);
+            Ok(dst)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, hex};
+
+    #[test]
+    fn test_compress_decompress() {
+        let dst_capacity = 100;
+        let to_compress = hex::decode("aaaabbbbcccc").unwrap();
+        let compressed = compress(&to_compress, dst_capacity, 3).unwrap();
+        let decompressed = decompress(&compressed, dst_capacity).unwrap();
+        assert_eq!(decompressed, to_compress);
+    }
+}
