30ddafb8de36ad95958e6df0f5fd926e48844737
Tim Detwiler <tjdetwiler@google.com>
2020-08-04 02:09:49 +0000

    [audio_core] Remove audio_core_trace_enabled arg
    This was added to hedge against potential performance impacts of the
    trace markers. This has been default true and unused for a while so
    we'll just remove the option to disable tracing.
    
    Test: fx build
    Change-Id: I1778977df2c044cfc1744e903c2dcbfbad91e715
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/413019
    Reviewed-by: Tom Bergan <tombergan@google.com>
    Testability-Review: Tim Detwiler <tjdetwiler@google.com>
    Commit-Queue: Tim Detwiler <tjdetwiler@google.com>

--- src/media/audio/audio_core/main.cc
+++ src/media/audio/audio_core/main.cc
@@ -3,12 +3,8 @@
 // found in the LICENSE file.
 
 #include <lib/sys/cpp/component_context.h>
-
-#include "lib/syslog/logger.h"
-
-#ifndef NTRACE
+#include <lib/syslog/logger.h>
 #include <lib/trace-provider/provider.h>
-#endif
 
 #include "src/lib/fxl/command_line.h"
 #include "src/media/audio/audio_core/audio_core_impl.h"
@@ -28,9 +24,7 @@ constexpr char kProcessConfigPath[] = "/config/data/audio_core_config.json";
 
 static int StartAudioCore() {
   auto threading_model = ThreadingModel::CreateWithMixStrategy(MixStrategy::kThreadPerMix);
-#ifndef NTRACE
   trace::TraceProviderWithFdio trace_provider(threading_model->FidlDomain().dispatcher());
-#endif
 
   syslog::SetLogSettings({.min_log_level = FX_LOG_INFO}, {"audio_core"});
 
