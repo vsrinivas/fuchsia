vsrinivas/fuchsia

commit 94c35a115460295ee6a197d4e99b88705108fd34

[build] Offer golden verification for sysmgr configs.
The validate_sysmgr_config script already sees a full snapshot of all files,
and this change uses that to create a merged config we can
test against checked-in goldens.

Allows assemble_system.gni to pass `sysmgr_golden` to config_package.gni
since this is most usefully defined at the level of the image.

Bug: 53646
Change-Id: I88b5b5039f5355f5ee1a695ee5be41dcc3c29765
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/402814
Commit-Queue: Adam Perry <adamperry@google.com>
Reviewed-by: Bryan Henry <bryanhenry@google.com>
Reviewed-by: Shai Barack <shayba@google.com>
Testability-Review: Shai Barack <shayba@google.com>

build/tools/validate_sysmgr_config.py
@@ -54,6 +54,11 @@ def main():
         metavar='FILE',
         help='Write a GN depfile to the given path',
         required=True)
+    parser.add_argument(
+        '--merged',
+        metavar='FILE',
+        help='Write a snapshot of this sysmgr config to the given path',
+        required=True)
     parser.add_argument('manifest', help='config-data package manifest')
     args = parser.parse_args()
 
@@ -64,7 +69,13 @@ def main():
     sysmgr_lines = [line for line in lines if line.startswith('data/sysmgr/')]
     sysmgr_config_files = [line.split('=')[1] for line in sysmgr_lines]
 
-    # Parse all config files and build a list of all conflicts, rather than
+    # Parse all config files
+    configs = []
+    for config_file in sysmgr_config_files:
+        with open(config_file, 'r') as f:
+            configs.append(json.load(f))
+
+    # Build a list of all conflicts, rather than
     # failing immediately on the first conflict. This allows us to print a more
     # useful build failure so that developers don't need to play whack-a-mole with
     # multiple conflicts.
@@ -72,10 +83,7 @@ def main():
     service_conflicts = {}
     seen_diag = None
     diag_conflicts = []
-    for config_file in sysmgr_config_files:
-        with open(config_file, 'r') as f:
-            config = json.load(f)
-
+    for config in configs:
         diag = config.get('diagnostics')
         if diag is not None:
             if seen_diag is None:
@@ -109,6 +117,22 @@ def main():
                 'Duplicate diagnostics config in files: {}'.format(
                     ', '.join(diag_conflicts)))
         return 1
+
+    # Create a single merged configuration analogous to sysmgr's init itself.
+    merged_config = {}
+    for config in configs:
+        for category, values in config.items():
+            existing = merged_config.get(category)
+            if type(existing) is dict:
+                merged_config[category].update(values)
+            elif type(existing) is list:
+                merged_config[category] += values
+            else:
+                merged_config[category] = values
+
+    with open(args.merged, 'w') as f:
+        json.dump(
+            merged_config, f, indent=2, separators=(',', ': '), sort_keys=True)
 
     # Write the depfile, which is a Makefile format file that has a single output
     # (the stamp file) and lists all input files as dependencies.

build/config.gni
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 import("//build/package.gni")
+import("//build/testing/golden_file.gni")
 
 # Define configuration data that can be aggregated into other packages. This is
 # primarily used to aggregate configuration files into the config-data package
@@ -66,6 +67,15 @@ template("config_data") {
 #
 # Parameters
 #
+#   sysmgr_golden (optional)
+#     [path] File to treat as the golden file for this image's sysmgr configuration.
+#     Fails the build if the configuration generated by this build is different than
+#     the golden file's contents. To downgrade to a warning, set `sysmgr_golden_warn`.
+#
+#   sysmgr_golden_warn (default: false)
+#     [boolean] If true, mismatches in the sysmgr golden config are treated as warnings
+#     rather than errors.
+#
 #   deps (required)
 #   public_deps (optional)
 #   data_deps (optional)
@@ -91,6 +101,7 @@ template("config_package") {
     outputs = [ config_package_manifest_path ]
   }
 
+  merged_sysmgr_config = "$target_gen_dir/$target_name.sysmgr"
   action(validate_sysmgr_config_target_name) {
     forward_variables_from(invoker,
                            [
@@ -102,11 +113,15 @@ template("config_package") {
     deps += [ ":${config_package_manifest}" ]
 
     script = "//build/tools/validate_sysmgr_config.py"
+
     inputs = [ config_package_manifest_path ]
 
     depfile = "$target_out_dir/$target_name.d"
     stamp_file = "$target_out_dir/$target_name.script.stamp"
-    outputs = [ stamp_file ]
+    outputs = [
+      merged_sysmgr_config,
+      stamp_file,
+    ]
 
     args = [
       rebase_path(config_package_manifest_path, root_build_dir),
@@ -114,7 +129,27 @@ template("config_package") {
       rebase_path(stamp_file, root_build_dir),
       "--depfile",
       rebase_path(depfile, root_build_dir),
+      "--merged",
+      rebase_path(merged_sysmgr_config, root_build_dir),
     ]
+  }
+
+  if (defined(invoker.sysmgr_golden)) {
+    golden_file("${target_name}_sysmgr_golden") {
+      forward_variables_from(invoker,
+                             [
+                               "deps",
+                               "public_deps",
+                               "data_deps",
+                               "testonly",
+                             ])
+      golden = invoker.sysmgr_golden
+      current = merged_sysmgr_config
+      warn_on_changes =
+          defined(invoker.sysmgr_golden_warn) && invoker.sysmgr_golden_warn
+
+      deps += [ ":$validate_sysmgr_config_target_name" ]
+    }
   }
 
   package(target_name) {
@@ -131,6 +166,10 @@ template("config_package") {
       ":${config_package_manifest}",
       ":${validate_sysmgr_config_target_name}",
     ]
+
+    if (defined(invoker.sysmgr_golden)) {
+      deps += [ ":${target_name}_sysmgr_golden" ]
+    }
 
     extra = [ config_package_manifest_path ]
     metadata = {

build/images/assemble_system.gni
@@ -45,6 +45,9 @@ import("//src/sys/pkg/bin/pm/pm.gni")
 #     These arguments come after synthesized arguments to configure blobfs and
 #     pkgfs.
 #
+#   sysmgr_golden, sysmgr_golden_warn (optional, default: false)
+#     [path, boolean] Passed directly to the config_package template, see //build/config.gni.
+#
 #   ta_uuids (default: [])
 #     [list of strings] UUIDs of Trusted Applications to include in the system
 #     image.
@@ -110,6 +113,12 @@ template("assemble_system") {
     testonly = true
     visibility = [ ":*" ]
     deps = base_packages + cache_packages + universe_packages
+    if (defined(invoker.sysmgr_golden)) {
+      sysmgr_golden = invoker.sysmgr_golden
+    }
+    if (defined(invoker.sysmgr_golden_warn)) {
+      sysmgr_golden_warn = invoker.sysmgr_golden_warn
+    }
   }
 
   group("${image_name}_base_packages") {

build/images/guest/BUILD.gn
@@ -16,4 +16,5 @@ assemble_system("guest") {
     "//src/virtualization/packages/zircon_guest:services_config",
     "//src/virtualization/tests:virtualization_test_utils_pkg",
   ]
+  sysmgr_golden = "sysmgr-golden.json"
 }

build/images/recovery/BUILD.gn
@@ -83,6 +83,7 @@ assemble_system("recovery-eng") {
   # unification is in progress
   bootfs_allowlist = []
   ta_uuids = recovery_ta_uuids
+  sysmgr_golden = "sysmgr-eng-golden.json"
 }
 
 assemble_system("recovery-fdr") {
@@ -99,6 +100,7 @@ assemble_system("recovery-fdr") {
   # unification is in progress
   bootfs_allowlist = []
   ta_uuids = recovery_ta_uuids
+  sysmgr_golden = "sysmgr-fdr-golden.json"
 }
 
 if (is_host) {

commit 184db87182aee8578233623679095b06594b2f7b

[validate_sysmgr] Print the correct conflict
This code previously referred to the `config_file` *loop* variable,
which always contained the path to the last file loaded, producing
errors like:

  Duplicate configuration for service fuchsia.net.http.Loader in files:
  ../../src/sys/timekeeper/service.config,
  ../../src/sys/timekeeper/service.config

which didn't make much sense. This change fixes that and uses
defaultdict to simplify the implementation.

It appears this bug was introduced in 94c35a1.

Change-Id: I2a0e1e6eed8f17d55c04332c79fc2d5bb9ae931a
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/413903
Reviewed-by: Adam Perry <adamperry@google.com>
Reviewed-by: Bryan Henry <bryanhenry@google.com>
Testability-Review: Bryan Henry <bryanhenry@google.com>
Commit-Queue: Nathan Mulcahey <nmulcahey@google.com>

build/tools/validate_sysmgr_config.py
@@ -37,6 +37,7 @@ where the path before the '=' is the destination path in the package, and the
 path after the '=' is the source file (rebased to the root build directory).
 """
 
+from collections import defaultdict
 import argparse
 import json
 import os
@@ -67,38 +68,35 @@ def main():
     sysmgr_lines = [line for line in lines if line.startswith('data/sysmgr/')]
     sysmgr_config_files = [line.split('=')[1] for line in sysmgr_lines]
 
-    # Parse all config files
-    configs = []
-    for config_file in sysmgr_config_files:
-        with open(config_file, 'r') as f:
-            configs.append(json.load(f))
-
+    # Parse all config files.
+    #
     # Build a list of all conflicts, rather than
     # failing immediately on the first conflict. This allows us to print a more
     # useful build failure so that developers don't need to play whack-a-mole with
     # multiple conflicts.
-    seen_services = {}
-    service_conflicts = {}
-    for config in configs:
-        services = config.get('services')
-        if not services:
-            continue
-        for service in services.keys():
-            if service in seen_services:
-                if not service in service_conflicts:
-                    service_conflicts[service] = [seen_services[service]]
-                service_conflicts[service].append(config_file)
-            else:
-                seen_services[service] = config_file
+    configs = []
+    files_by_service = defaultdict(list)
+    for config_file in sysmgr_config_files:
+        with open(config_file, 'r') as f:
+            config = json.load(f)
+            configs.append(config)
+
+            services = config.get('services')
+            if services:
+                for service in services.keys():
+                    files_by_service[service].append(config_file)
 
     # If any conflicts were detected, print a useful error message and then
     # exit.
-    if service_conflicts:
-        print('Error: conflicts detected in sysmgr configuration')
-        for service, config_files in list(service_conflicts.items()):
+    service_conflicts = False
+    for service, config_files in files_by_service.items():
+        if len(config_files) > 1:
             print(
-                'Duplicate configuration for service {} in files: {}'.format(
-                    service, ', '.join(config_files)))
+                'Duplicate sysmgr configuration for service {} in files: {}'.
+                format(service, ', '.join(config_files)))
+            service_conflicts = True
+
+    if service_conflicts:
         return 1
 
     # Create a single merged configuration analogous to sysmgr's init itself.