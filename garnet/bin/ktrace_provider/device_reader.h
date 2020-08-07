1fdd68144b5fe2f905b3178915008a5eeb6f7285
Puneetha Ramachandra <puneetha@google.com>
2020-08-07 19:03:01 +0000

    [ktrace] Convert driver to service
    Remove the ktrace driver from dev/misc and make it a service.
    
    Bug: 55284
    Bug: 33645
    Bug: 33828
    Test: ktrace start 0, ktrace stop, ktrace written, ktrace save /tmp/xyz
          fx traceutil record --categories=kernel,kernel:retain,kernel:sched --buffer-size=64 --duration=10s --stream
          insntrace --control init start stop dump
          fx test appmgr_integration_tests -- --gtest_filter="*HubTest*"
          fx test --no-build -o return_child_result_tests
    Change-Id: I3552df09c5933c2ad3be0436411d06856076335a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/412705
    Commit-Queue: Puneetha Ramachandra <puneetha@google.com>
    Testability-Review: Alex Legg <alexlegg@google.com>
    Testability-Review: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Alex Legg <alexlegg@google.com>
    API-Review: Abdulla Kamar <abdulla@google.com>

--- garnet/bin/ktrace_provider/device_reader.h
+++ garnet/bin/ktrace_provider/device_reader.h
@@ -5,7 +5,7 @@
 #ifndef GARNET_BIN_KTRACE_PROVIDER_DEVICE_READER_H_
 #define GARNET_BIN_KTRACE_PROVIDER_DEVICE_READER_H_
 
-#include <src/lib/files/unique_fd.h>
+#include <fuchsia/tracing/kernel/cpp/fidl.h>
 
 #include "garnet/bin/ktrace_provider/reader.h"
 
@@ -15,13 +15,17 @@ class DeviceReader : public Reader {
  public:
   DeviceReader();
 
+  zx_status_t Init();
+
  private:
+  static constexpr char kKtraceReaderSvc[] = "/svc/fuchsia.tracing.kernel.Reader";
   static constexpr size_t kChunkSize{16 * 4 * 1024};
 
+  std::tuple<zx_status_t, zx::channel> OpenKtraceReader();
   void ReadMoreData() override;
 
-  fbl::unique_fd fd_;
-
+  fuchsia::tracing::kernel::ReaderSyncPtr ktrace_reader_;
+  size_t offset_ = 0;
   char buffer_[kChunkSize];
 
   DeviceReader(const DeviceReader&) = delete;
