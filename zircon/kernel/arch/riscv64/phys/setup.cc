aece35c4bc653311add59d2d9abd4a222c795e75
Florent Revest <revest@google.com>
2020-08-07 10:41:57 +0200

    [zircon] Add a placeholder kernel/arch/riscv64/phys/
    This is necessary to go past
    ERROR at //kernel/phys/BUILD.zircon.gn:62:7: Unable to load "/usr/local/google/home/revest/fuchsia/zircon/kernel/arch/riscv64/phys/phys.gni".
          import("$zx/kernel/arch/$kernel_cpu/phys/phys.gni")
          ^-------------------------------------------------
    See //public/gn/toolchain/environment_redirect.gni:266:26: which caused the file to be included.
            public_deps += [ "$label($toolchain_name)" ]
                             ^------------------------
    
    See //BUILD.gn:203:3: whence it was called.
      _run_zircon_gn("zircon") {
      ^-------------------------
    
    Change-Id: I2d1cfee68cae8b6d6215aeba81601162a22bdb1b

---
+++ zircon/kernel/arch/riscv64/phys/setup.cc
@@ -0,0 +1,12 @@
+// Copyright 2021 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <phys/main.h>
+
+void ArchSetUp() {
+  // Nothing.
+}
+
