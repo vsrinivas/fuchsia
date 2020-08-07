f9b195829546b846f5f551f33eefda5e4dc5ecec
omer <omerlevran@google.com>
2020-08-07 14:05:19 +0000

    [CTS] Add license to hello_world_util.h
    Change-Id: I82f695688ebf6a5057f104ec4c89ae7331bac468
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/414516
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Reviewed-by: Jeremy Manson <jeremymanson@google.com>
    Testability-Review: Jeremy Manson <jeremymanson@google.com>
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>

--- cts/tests/hello_world/hello_world_util.h
+++ cts/tests/hello_world/hello_world_util.h
@@ -1,3 +1,7 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef CTS_TESTS_HELLO_WORLD_HELLO_WORLD_UTIL_H_
 #define CTS_TESTS_HELLO_WORLD_HELLO_WORLD_UTIL_H_
 #include <string>
