8d96c05e86fc98a677f45cd8ffb2b72e1559fe1b
Bryan Henry <bryanhenry@google.com>
2020-08-08 16:36:04 +0000

    [ldsvc] Refactor loader service test fixture for reuse
    Enables reuse of this test fixture in LoaderServiceBase subclasses that
    live elsewhere in the codebase.
    
    Bug: 55959
    Change-Id: I3f6ac555311cfd1b582de3f3ab47a831ec8cccf8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/414174
    Commit-Queue: Bryan Henry <bryanhenry@google.com>
    Reviewed-by: Drew Fisher <zarvox@google.com>
    Testability-Review: Drew Fisher <zarvox@google.com>

--- src/lib/loader_service/loader_service_test.cc
+++ src/lib/loader_service/loader_service_test.cc
@@ -5,216 +5,24 @@
 #include "src/lib/loader_service/loader_service.h"
 
 #include <fuchsia/ldsvc/llcpp/fidl.h>
-#include <fuchsia/security/resource/llcpp/fidl.h>
-#include <lib/async-loop/cpp/loop.h>
-#include <lib/async-loop/default.h>
-#include <lib/fdio/directory.h>
-#include <lib/fdio/fd.h>
 #include <lib/fdio/unsafe.h>
-#include <lib/fidl/llcpp/memory.h>
-#include <lib/fidl/llcpp/string_view.h>
-#include <lib/memfs/cpp/vnode.h>
 #include <lib/zx/channel.h>
-#include <lib/zx/object.h>
 #include <lib/zx/status.h>
 #include <zircon/errors.h>
 #include <zircon/fidl.h>
-#include <zircon/limits.h>
 
-#include <cstdint>
-#include <memory>
-#include <optional>
-#include <string_view>
-#include <vector>
-
-#include <fbl/ref_ptr.h>
-#include <fbl/unique_fd.h>
-#include <gtest/gtest.h>
 #include <ldmsg/ldmsg.h>
 
-#include "src/lib/testing/loop_fixture/real_loop_fixture.h"
-
-namespace loader {
-namespace {
-
-namespace fldsvc = ::llcpp::fuchsia::ldsvc;
-namespace fsec = ::llcpp::fuchsia::security::resource;
+#include "src/lib/loader_service/loader_service_test_fixture.h"
 
 #define ASSERT_OK(expr) ASSERT_EQ(ZX_OK, expr)
 #define EXPECT_OK(expr) EXPECT_EQ(ZX_OK, expr)
 
-struct TestDirectoryEntry {
-  std::string path;
-  std::string file_contents;
-  bool executable;
+namespace loader {
+namespace test {
+namespace {
 
-  TestDirectoryEntry(std::string path, std::string file_contents, bool executable)
-      : path(std::move(path)), file_contents(std::move(file_contents)), executable(executable) {}
-};
-
-zx::status<zx::unowned_resource> GetVmexResource() {
-  static const std::string kVmexResourcePath = "/svc/" + std::string(fsec::Vmex::Name);
-
-  static zx::resource vmex_resource;
-  if (!vmex_resource.is_valid()) {
-    zx::channel client, server;
-    auto status = zx::make_status(zx::channel::create(0, &client, &server));
-    if (status.is_error()) {
-      return status.take_error();
-    }
-    status = zx::make_status(fdio_service_connect(kVmexResourcePath.c_str(), server.release()));
-    if (status.is_error()) {
-      return status.take_error();
-    }
-
-    auto result = fsec::Vmex::Call::Get(client.borrow());
-    if (!result.ok()) {
-      return zx::error(result.status());
-    }
-    vmex_resource = std::move(result.Unwrap()->vmex);
-  }
-  return zx::ok(vmex_resource.borrow());
-}
-
-void AddDirectoryEntry(const fbl::RefPtr<memfs::VnodeDir>& root, TestDirectoryEntry entry) {
-  ASSERT_FALSE(entry.path.empty() || entry.path.front() == '/' || entry.path.back() == '/');
-
-  zx::vmo vmo;
-  ASSERT_OK(zx::vmo::create(ZX_PAGE_SIZE, 0, &vmo));
-  ASSERT_OK(vmo.write(entry.file_contents.data(), 0, entry.file_contents.size()));
-  if (entry.executable) {
-    auto vmex_rsrc = GetVmexResource();
-    ASSERT_OK(vmex_rsrc.status_value());
-    ASSERT_TRUE(vmex_rsrc.value()->is_valid());
-    ASSERT_OK(vmo.replace_as_executable(*vmex_rsrc.value(), &vmo));
-  }
-
-  fbl::RefPtr<memfs::VnodeDir> dir(root);
-  std::string_view view(entry.path);
-  while (true) {
-    size_t next = view.find('/');
-    if (next == std::string_view::npos) {
-      // No more subdirectories; create vnode for vmo, then done!
-      ASSERT_FALSE(view.empty());
-      ASSERT_OK(dir->CreateFromVmo(view, vmo.release(), 0, entry.file_contents.size()));
-      return;
-    } else {
-      // Create subdirectory if it doesn't already exist.
-      std::string_view subdir(view.substr(0, next));
-      ASSERT_FALSE(subdir.empty());
-
-      fbl::RefPtr<fs::Vnode> out;
-      zx_status_t status = dir->Lookup(&out, subdir);
-      if (status == ZX_ERR_NOT_FOUND) {
-        status = dir->Create(&out, subdir, S_IFDIR);
-      }
-      ASSERT_OK(status);
-
-      dir = fbl::RefPtr<memfs::VnodeDir>::Downcast(std::move(out));
-      view.remove_prefix(next + 1);
-    }
-  }
-}
-
-template <typename T>
-class LoaderServiceTestTemplate : public gtest::RealLoopFixture {
- public:
-  LoaderServiceTestTemplate()
-      : fs_loop_(&kAsyncLoopConfigNoAttachToCurrentThread),
-        loader_loop_(&kAsyncLoopConfigNoAttachToCurrentThread) {}
-
-  // This should only be called once per test case. This would be in SetUp but we want to allow test
-  // defined directory contents.
-  void CreateTestLoader(std::vector<TestDirectoryEntry> config, std::shared_ptr<T>* loader) {
-    fbl::unique_fd root_fd;
-    ASSERT_NO_FATAL_FAILURE(CreateTestDirectory(std::move(config), &root_fd));
-    const ::testing::TestInfo* const test_info =
-        ::testing::UnitTest::GetInstance()->current_test_info();
-    *loader = T::Create(loader_loop_.dispatcher(), std::move(root_fd), test_info->name());
-  }
-
-  // This should only be called once per test case. This would be in SetUp but we want to allow test
-  // defined directory contents.
-  void CreateTestDirectory(std::vector<TestDirectoryEntry> config, fbl::unique_fd* root_fd) {
-    ASSERT_FALSE(vfs_);
-    ASSERT_FALSE(root_dir_);
-
-    ASSERT_OK(memfs::Vfs::Create("<tmp>", &vfs_, &root_dir_));
-    vfs_->SetDispatcher(fs_loop_.dispatcher());
-
-    for (auto entry : config) {
-      ASSERT_NO_FATAL_FAILURE(AddDirectoryEntry(root_dir_, entry));
-    }
-
-    zx::channel client, server;
-    ASSERT_OK(zx::channel::create(0, &client, &server));
-    ASSERT_OK(vfs_->ServeDirectory(fbl::RefPtr(root_dir_), std::move(server)));
-
-    // Must start fs_loop before fdio_fd_create, since that will attempt to Describe the directory.
-    ASSERT_OK(fs_loop_.StartThread("fs_loop"));
-    ASSERT_OK(fdio_fd_create(client.release(), root_fd->reset_and_get_address()));
-
-    // The loader needs a separate thread from the FS because it uses synchronous fd-based I/O.
-    ASSERT_OK(loader_loop_.StartThread("loader_loop"));
-  }
-
-  virtual void TearDown() override {
-    if (vfs_) {
-      std::optional<zx_status_t> shutdown_status;
-      vfs_->Shutdown([&](zx_status_t status) { shutdown_status = status; });
-      RunLoopUntil([&]() { return shutdown_status.has_value(); });
-      ASSERT_OK(shutdown_status.value());
-    }
-  }
-
-  async::Loop& loader_loop() { return loader_loop_; }
-  fbl::RefPtr<memfs::VnodeDir>& root_dir() { return root_dir_; }
-
- private:
-  async::Loop fs_loop_;
-  async::Loop loader_loop_;
-  std::unique_ptr<memfs::Vfs> vfs_;
-  fbl::RefPtr<memfs::VnodeDir> root_dir_;
-};
-
-using LoaderServiceTest = LoaderServiceTestTemplate<LoaderService>;
-
-zx_rights_t get_rights(const zx::object_base& handle) {
-  zx_info_handle_basic_t info;
-  zx_status_t status = handle.get_info(ZX_INFO_HANDLE_BASIC, &info, sizeof(info), nullptr, nullptr);
-  return status == ZX_OK ? info.rights : ZX_RIGHT_NONE;
-}
-
-// This takes a non-const reference because LLCPP SyncClient's generated methods are non-const.
-void LoadObject(fldsvc::Loader::SyncClient& client, std::string name,
-                zx::status<std::string> contents) {
-  auto result = client.LoadObject(fidl::unowned_str(name));
-  ASSERT_TRUE(result.ok());
-  auto response = result.Unwrap();
-  ASSERT_EQ(contents.status_value(), response->rv);
-
-  zx::vmo vmo = std::move(response->object);
-  if (contents.is_error()) {
-    ASSERT_FALSE(vmo.is_valid());
-  } else {
-    ASSERT_TRUE(vmo.is_valid());
-    ASSERT_EQ(get_rights(vmo) & ZX_RIGHT_EXECUTE, ZX_RIGHT_EXECUTE);
-
-    char data[ZX_PAGE_SIZE] = {};
-    ASSERT_OK(vmo.read(data, 0, ZX_PAGE_SIZE));
-    ASSERT_EQ(std::string(data), contents.value());
-  }
-}
-
-// This takes a non-const reference because LLCPP SyncClient's generated methods are non-const.
-void Config(fldsvc::Loader::SyncClient& client, std::string config,
-            zx::status<zx_status_t> expected) {
-  auto result = client.Config(fidl::StringView(fidl::unowned_ptr(config.data()), config.size()));
-  ASSERT_EQ(result.status(), expected.status_value());
-  if (expected.is_ok()) {
-    ASSERT_EQ(result.Unwrap()->rv, expected.value());
-  }
-}
+namespace fldsvc = ::llcpp::fuchsia::ldsvc;
 
 TEST_F(LoaderServiceTest, ConnectBindDone) {
   std::shared_ptr<LoaderService> loader;
@@ -488,4 +296,5 @@ TEST_F(LoaderServiceTest, InteropWithLdmsg_LoadObject) {
 }
 
 }  // namespace
+}  // namespace test
 }  // namespace loader
---
+++ src/lib/loader_service/loader_service_test_fixture.h
@@ -0,0 +1,87 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_LIB_LOADER_SERVICE_LOADER_SERVICE_TEST_FIXTURE_H_
+#define SRC_LIB_LOADER_SERVICE_LOADER_SERVICE_TEST_FIXTURE_H_
+
+#include <fuchsia/ldsvc/llcpp/fidl.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
+#include <lib/memfs/cpp/vnode.h>
+
+#include <memory>
+#include <vector>
+
+#include <fbl/ref_ptr.h>
+#include <fbl/unique_fd.h>
+#include <gtest/gtest.h>
+
+#include "src/lib/loader_service/loader_service.h"
+#include "src/lib/testing/loop_fixture/real_loop_fixture.h"
+
+namespace loader {
+namespace test {
+
+struct TestDirectoryEntry {
+  std::string path;
+  std::string file_contents;
+  bool executable;
+
+  TestDirectoryEntry(std::string path, std::string file_contents, bool executable)
+      : path(std::move(path)), file_contents(std::move(file_contents)), executable(executable) {}
+};
+
+class LoaderServiceTest : public gtest::RealLoopFixture {
+ public:
+  LoaderServiceTest()
+      : fs_loop_(&kAsyncLoopConfigNoAttachToCurrentThread),
+        loader_loop_(&kAsyncLoopConfigNoAttachToCurrentThread) {}
+
+  virtual void TearDown() override;
+
+  // Either this or CreateTestDirectory should only be called once per test case. This would be in
+  // SetUp but we want to allow test defined directory contents.
+  template <typename T>
+  void CreateTestLoader(std::vector<TestDirectoryEntry> config, std::shared_ptr<T>* loader) {
+    fbl::unique_fd root_fd;
+    ASSERT_NO_FATAL_FAILURE(CreateTestDirectory(std::move(config), &root_fd));
+    const ::testing::TestInfo* const test_info =
+        ::testing::UnitTest::GetInstance()->current_test_info();
+    *loader = T::Create(loader_loop_.dispatcher(), std::move(root_fd), test_info->name());
+  }
+
+  // Either this or CreateTestLoader should only be called once per test case. This would be in
+  // SetUp but we want to allow test defined directory contents.
+  void CreateTestDirectory(std::vector<TestDirectoryEntry> config, fbl::unique_fd* root_fd);
+
+  // Add a directory entry to the given VnodeDir. Can be used to add entries mid-test case using
+  // root_dir() below.
+  void AddDirectoryEntry(const fbl::RefPtr<memfs::VnodeDir>& root, TestDirectoryEntry entry);
+
+  // Exercise a LoadObject call and assert that the result matches `expected`.
+  //
+  // This takes a non-const reference because LLCPP SyncClient's generated methods are non-const.
+  void LoadObject(llcpp::fuchsia::ldsvc::Loader::SyncClient& client, std::string name,
+                  zx::status<std::string> expected);
+
+  // Exercise a Config call and assert that the result matches `expected`.
+  //
+  // This takes a non-const reference because LLCPP SyncClient's generated methods are non-const.
+  void Config(llcpp::fuchsia::ldsvc::Loader::SyncClient& client, std::string config,
+              zx::status<zx_status_t> expected);
+
+  async::Loop& loader_loop() { return loader_loop_; }
+  fbl::RefPtr<memfs::VnodeDir>& root_dir() { return root_dir_; }
+
+ private:
+  async::Loop fs_loop_;
+  async::Loop loader_loop_;
+  std::unique_ptr<memfs::Vfs> vfs_;
+  fbl::RefPtr<memfs::VnodeDir> root_dir_;
+};
+
+}  // namespace test
+}  // namespace loader
+
+#endif  // SRC_LIB_LOADER_SERVICE_LOADER_SERVICE_TEST_FIXTURE_H_
