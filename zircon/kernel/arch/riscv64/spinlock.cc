f4078156b39a76acaddcc7a4f12c3b06eb955702
Florent Revest <revest@google.com>
2020-08-09 13:25:32 +0200

    [zircon] Add spinlock support on riscv64
    Change-Id: Id1ca6c8881e5c1d94c8c40f82952420b1df575be

--- zircon/kernel/arch/riscv64/spinlock.cc
+++ zircon/kernel/arch/riscv64/spinlock.cc
@@ -13,11 +13,40 @@
 // annotations cause Clang to detect violations.
 
 void arch_spin_lock(arch_spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
+  uint64_t new_value = arch_curr_cpu_num() + 1, existing_value, sc_failed;
+
+  __asm__ volatile (
+    "0: lr.d.aq  %0, %2;"      // Load the current lock value
+    "   bne      %0, x0, 0b;"  // Spin if it is already locked (non-zero value)
+    "   sc.d.rl  %1, %z3, %2;" // Try to store new_value (cpu_num+1) in the lock
+    "   bnez     %1, 0b"       // If the lock had changed behind our back, retry
+    : "=&r"(existing_value), "=&r" (sc_failed), "+A"(lock->value)
+    : "r"(new_value)
+    : "memory");
+
+  WRITE_PERCPU_FIELD32(num_spinlocks, READ_PERCPU_FIELD32(num_spinlocks) + 1);
 }
 
 bool arch_spin_trylock(arch_spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
-  return 0;
+  uint64_t new_value = arch_curr_cpu_num() + 1, existing_value, sc_failed;
+
+  __asm__ volatile (
+    "0: lr.d.aq  %0, %2;"      // Load the current lock value
+    "   bne      %0, x0, 1f;"  // Bail out if already locked (non-zero value)
+    "   sc.d.rl  %1, %z3, %2;" // Try to store new_value (cpu_num+1) in the lock
+    "   bnez     %1, 0b;"      // If the lock had changed behind our back, retry
+    "1:"
+    : "=&r"(existing_value), "=&r" (sc_failed), "+A"(lock->value)
+    : "r"(new_value)
+    : "memory");
+
+  if (existing_value == 0) {
+    WRITE_PERCPU_FIELD32(num_spinlocks, READ_PERCPU_FIELD32(num_spinlocks) + 1);
+  }
+  return existing_value;
 }
 
 void arch_spin_unlock(arch_spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
+  WRITE_PERCPU_FIELD32(num_spinlocks, READ_PERCPU_FIELD32(num_spinlocks) - 1);
+  __atomic_store_n(&lock->value, 0UL, __ATOMIC_RELEASE);
 }
