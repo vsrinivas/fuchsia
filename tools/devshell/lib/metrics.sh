def3acc7a3dd566afa0efadfa4f96b3ced1e96ad
Dave Schuyler <dschuyler@chromium.org>
2020-08-10 22:41:25 +0000

    [tools] Track all args for `fx emu`
    This CL adds the emulator (emu) to the list of subcommands that have all args
    tracked by metrics.
    
    Change-Id: Ie7e8986cce2dbf4161f2ad22b87194ab1f4541b8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/413205
    Reviewed-by: Renato Mangini Dias <mangini@google.com>
    Testability-Review: Renato Mangini Dias <mangini@google.com>
    Commit-Queue: Dave Schuyler <dschuyler@google.com>

--- tools/devshell/lib/metrics.sh
+++ tools/devshell/lib/metrics.sh
@@ -8,7 +8,7 @@
 ### depends on FUCHSIA_DIR being defined correctly.
 
 _METRICS_GA_PROPERTY_ID="UA-127897021-6"
-_METRICS_TRACK_ALL_ARGS=( "set" "fidlcat" "run-test" "run-test-component" "run-host-tests" )
+_METRICS_TRACK_ALL_ARGS=( "emu" "set" "fidlcat" "run-test" "run-test-component" "run-host-tests" )
 _METRICS_TRACK_RESULTS=( "set" "build" )
 _METRICS_ALLOWS_CUSTOM_REPORTING=( "test" )
 
