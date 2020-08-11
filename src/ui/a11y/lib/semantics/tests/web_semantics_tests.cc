7e37f66dc0dfa16f18cd6817cd927ec91e6b2124
Tess Eisenberger <teisenbe@google.com>
2020-08-11 00:16:05 +0000

    [a11y] Web semantics hit testing integration test
    Verify that hit tests return the correct node when targeting buttons
    and labels.
    
    Fixed: 57732
    Change-Id: I2a6291161e2ef19822c18ee648137cf477ff6cee
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/414660
    Commit-Queue: Tess Eisenberger <teisenbe@google.com>
    Reviewed-by: Alice Neels <neelsa@google.com>
    Testability-Review: Alice Neels <neelsa@google.com>

--- src/ui/a11y/lib/semantics/tests/web_semantics_tests.cc
+++ src/ui/a11y/lib/semantics/tests/web_semantics_tests.cc
@@ -28,65 +28,104 @@ class WebSemanticsTest : public SemanticsIntegrationTest {
         {.url = "fuchsia-pkg://fuchsia.com/web_engine#meta/context_provider.cmx"},
         "fuchsia.web.ContextProvider");
   }
+
+  void SetUp() override {
+    ASSERT_NO_FATAL_FAILURE(SemanticsIntegrationTest::SetUp());
+
+    web_runner_tests::TestServer server;
+    FX_CHECK(server.FindAndBindPort());
+
+    auto serve = server.ServeAsync([&server] {
+      while (server.Accept()) {
+        web_runner_tests::MockHttpGetResponse(&server, "static.html");
+      }
+    });
+
+    view_manager()->SetSemanticsEnabled(true);
+
+    scenic::EmbeddedViewInfo web_runner = scenic::LaunchComponentAndCreateView(
+        environment()->launcher_ptr(),
+        fxl::StringPrintf("http://localhost:%d/static.html", server.port()), {});
+
+    web_runner.controller.events().OnTerminated = [](auto...) { FAIL(); };
+
+    view_ref_koid_ = fsl::GetKoid(web_runner.view_ref.reference.get());
+
+    // Present the view.
+    embedder_view_.emplace(scenic::ViewContext{
+        .session_and_listener_request = scenic::CreateScenicSessionPtrAndListenerRequest(scenic()),
+        .view_token = CreatePresentationViewToken(),
+    });
+
+    // Embed the view.
+    bool is_rendering = false;
+    embedder_view_->EmbedView(std::move(web_runner),
+                              [&is_rendering](fuchsia::ui::gfx::ViewState view_state) {
+                                is_rendering = view_state.is_rendering;
+                              });
+    ASSERT_TRUE(RunLoopWithTimeoutOrUntil([&is_rendering] { return is_rendering; }, kTimeout));
+
+    EXPECT_TRUE(RunLoopWithTimeoutOrUntil(
+        [this] {
+          auto node = view_manager()->GetSemanticNode(view_ref_koid_, 0u);
+          return node != nullptr && node->has_attributes() && node->attributes().has_label();
+        },
+        kTimeout))
+        << "No root node found.";
+
+    /* The semantic tree for static.html:
+     * ID: 0 Label:Say something. Anything.
+     *     ID: 5 Label:no label
+     *         ID: 7 Label:Test 1 2 3...
+     *             ID: 13 Label:Test 1 2 3...
+     *         ID: 11 Label:Click here
+     *             ID: 14 Label:Click here
+     *                 ID: 15 Label:Click here
+     */
+  }
+
+  zx_koid_t view_ref_koid() const { return view_ref_koid_; }
+
+ private:
+  // Wrapped in optional since the view is not created until the middle of SetUp
+  std::optional<scenic::EmbedderView> embedder_view_;
+
+  zx_koid_t view_ref_koid_;
 };
 
 // Loads a static page via the component framework and verifies its semantic tree.
 TEST_F(WebSemanticsTest, StaticSemantics) {
-  web_runner_tests::TestServer server;
-  FX_CHECK(server.FindAndBindPort());
-
-  auto serve = server.ServeAsync([&server] {
-    while (server.Accept()) {
-      web_runner_tests::MockHttpGetResponse(&server, "static.html");
-    }
-  });
-
-  view_manager()->SetSemanticsEnabled(true);
-
-  scenic::EmbeddedViewInfo web_runner = scenic::LaunchComponentAndCreateView(
-      environment()->launcher_ptr(),
-      fxl::StringPrintf("http://localhost:%d/static.html", server.port()), {});
-
-  web_runner.controller.events().OnTerminated = [](auto...) { FAIL(); };
-
-  // Get the viewref koid.
-  const zx_koid_t view_ref_koid = fsl::GetKoid(web_runner.view_ref.reference.get());
-
-  // Present the view.
-  scenic::EmbedderView embedder_view({
-      .session_and_listener_request = scenic::CreateScenicSessionPtrAndListenerRequest(scenic()),
-      .view_token = CreatePresentationViewToken(),
-  });
-
-  // Embed the view.
-  bool is_rendering = false;
-  embedder_view.EmbedView(std::move(web_runner),
-                          [&is_rendering](fuchsia::ui::gfx::ViewState view_state) {
-                            is_rendering = view_state.is_rendering;
-                          });
-  ASSERT_TRUE(RunLoopWithTimeoutOrUntil([&is_rendering] { return is_rendering; }, kTimeout));
-  EXPECT_TRUE(RunLoopWithTimeoutOrUntil(
-      [&] {
-        auto node = view_manager()->GetSemanticNode(view_ref_koid, 0u);
-        return node != nullptr && node->has_attributes() && node->attributes().has_label();
-      },
-      kTimeout))
-      << "No root node found.";
-
-  // Verify semantic tree for static.html:
-  /*
-  0 Label:Say something. Anything.
-    ID: 5 Label:no label
-        ID: 6 Label:Test 1 2 3...
-            ID: 10 Label:Test 1 2 3...
-  */
-
-  auto root = view_manager()->GetSemanticNode(view_ref_koid, 0u);
-  auto node = FindNodeWithLabel(root, view_ref_koid, "Say something. Anything.");
+  auto root = view_manager()->GetSemanticNode(view_ref_koid(), 0u);
+  auto node = FindNodeWithLabel(root, view_ref_koid(), "Say something. Anything.");
   ASSERT_TRUE(node);
 
-  node = FindNodeWithLabel(root, view_ref_koid, "Test 1 2 3...");
+  node = FindNodeWithLabel(root, view_ref_koid(), "Test 1 2 3... ");
   ASSERT_TRUE(node);
+
+  node = FindNodeWithLabel(root, view_ref_koid(), "Click here");
+  ASSERT_TRUE(node);
+}
+
+TEST_F(WebSemanticsTest, HitTesting) {
+  auto root = view_manager()->GetSemanticNode(view_ref_koid(), 0u);
+
+  // When performing hit tests, aim for just inside the node's bounding box.  Note
+  // that for nodes from Chrome, the min corner has a larger y value than the max.
+  fuchsia::math::PointF offset = {1., -1.};
+
+  // Hit test the plain text
+  auto node = FindNodeWithLabel(root, view_ref_koid(), "Test 1 2 3... ");
+  ASSERT_TRUE(node);
+  auto hit_node = HitTest(view_ref_koid(), CalculateViewTargetPoint(view_ref_koid(), node, offset));
+  ASSERT_TRUE(hit_node.has_value());
+  ASSERT_EQ(*hit_node, node->node_id());
+
+  // Hit test the button
+  node = FindNodeWithLabel(root, view_ref_koid(), "Click here");
+  ASSERT_TRUE(node);
+  hit_node = HitTest(view_ref_koid(), CalculateViewTargetPoint(view_ref_koid(), node, offset));
+  ASSERT_TRUE(hit_node.has_value());
+  ASSERT_EQ(*hit_node, node->node_id());
 }
 
 }  // namespace
