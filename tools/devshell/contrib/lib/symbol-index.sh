cc990f36cc09ea3a4881aa0c7bef2879cc95c113
Dangyi Liu <dangyi@google.com>
2020-08-11 20:22:35 +0000

    [debugger/fidlcat] Use symbol-index in fx commands
    Migrate to use "symbol-index" file for symbol lookup. `fx debug` and
    `fx fidlcat` will now check for the registration in symbol-index before
    running the commands. Hard-coded symbol paths are dropped.
    
    Bug: 57458
    Change-Id: I75c26fb9403b9da243bcc590044e0361fa667ea8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/414515
    Reviewed-by: James Tucker <raggi@google.com>
    Reviewed-by: Renato Mangini Dias <mangini@google.com>
    Testability-Review: James Tucker <raggi@google.com>
    Commit-Queue: Dangyi Liu <dangyi@google.com>

---
+++ tools/devshell/contrib/lib/symbol-index.sh
@@ -0,0 +1,31 @@
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This library is used by:
+# * symbol-index
+# * debug
+# * fidlcat
+#
+# This file is not self-contained! ../../lib/vars.sh must be sourced before this file.
+
+
+function symbol-index {
+  local symbol_index="${FUCHSIA_BUILD_DIR}/host-tools/symbol-index"
+
+  if [[ ! -e ${symbol_index} ]]; then
+    fx-error "${symbol_index} is not found, make sure you have //bundles:tools (or //tools) in your
+    'fx set' command and 'fx build' was executed successfully."
+    exit 1
+  fi
+
+  "${symbol_index}" "$@"
+  return $?
+}
+
+function ensure-symbol-index-registered {
+  symbol-index add "${FUCHSIA_BUILD_DIR}/.build-id" "${FUCHSIA_BUILD_DIR}" || return $?
+  symbol-index add "${ZIRCON_BUILDROOT}/.build-id" "${ZIRCON_BUILDROOT}" || return $?
+  symbol-index add "${FUCHSIA_DIR}/prebuilt/.build-id" || return $?
+  symbol-index add "${PREBUILT_CLANG_DIR}/lib/debug/.build-id" || return $?
+}
