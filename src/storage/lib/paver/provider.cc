0b63c73ba6d423b6011eec397334402f8c7d8457
Yecheng Zhao <zyecheng@google.com>
2020-08-12 22:29:39 +0000

    [paver] Register luis device partitioner
    Change-Id: Iaf8e0fad79b8b5d5df26dffd4b8bb03ed97fb0d2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/415817
    Testability-Review: David Pursell <dpursell@google.com>
    Reviewed-by: David Pursell <dpursell@google.com>
    Commit-Queue: Yecheng Zhao <zyecheng@google.com>

--- src/storage/lib/paver/provider.cc
+++ src/storage/lib/paver/provider.cc
@@ -15,6 +15,7 @@
 #include "src/storage/lib/paver/device-partitioner.h"
 #include "src/storage/lib/paver/paver.h"
 #include "src/storage/lib/paver/sherlock.h"
+#include "src/storage/lib/paver/luis.h"
 #include "src/storage/lib/paver/x64.h"
 
 namespace {
@@ -37,12 +38,14 @@ zx_status_t Init(void** out_ctx) {
   paver::DevicePartitionerFactory::Register(std::make_unique<paver::AstroPartitionerFactory>());
   paver::DevicePartitionerFactory::Register(std::make_unique<paver::As370PartitionerFactory>());
   paver::DevicePartitionerFactory::Register(std::make_unique<paver::SherlockPartitionerFactory>());
+  paver::DevicePartitionerFactory::Register(std::make_unique<paver::LuisPartitionerFactory>());
   paver::DevicePartitionerFactory::Register(
       std::make_unique<paver::ChromebookX64PartitionerFactory>());
   paver::DevicePartitionerFactory::Register(std::make_unique<paver::X64PartitionerFactory>());
   paver::DevicePartitionerFactory::Register(std::make_unique<paver::DefaultPartitionerFactory>());
   abr::ClientFactory::Register(std::make_unique<paver::AstroAbrClientFactory>());
   abr::ClientFactory::Register(std::make_unique<paver::SherlockAbrClientFactory>());
+  abr::ClientFactory::Register(std::make_unique<paver::LuisAbrClientFactory>());
   abr::ClientFactory::Register(std::make_unique<paver::X64AbrClientFactory>());
   return ZX_OK;
 }
