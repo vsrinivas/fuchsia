8c5f6fce5cf537cb860de46e815d8cd76a8ab448
Corey Tabaka <eieio@google.com>
2020-08-17 20:38:24 +0000

    [kernel][arm64] Fix clobbered register hazard in EL1-to-El1.
    Zircon kernel reserves registers x15 and x18 for the fixed registers
    percpu_ptr and shadow_call_sp, respectively. However, the SMCCC only
    preserves x18 across SMC/HVC calls. The SMC/HVC call veneers attempt
    to address this by saving both registers to the stack before a call
    and restoring the registers after the call, however, there is a
    window where the unpredictable value in x15 after the SMC/HVC call
    can be used by a coincident EL1 exception (e.g. preemption timer).
    
    There are two problems that this patch addresses:
    1. The EL1->EL1 exception paths do not restore the value of x15 from
       TPIDR_EL1, the way the EL0->EL1 paths do. If an EL1 exception
       pends during an SMC/HVC call, then upon return to EL1 from the
       call, the EL1 exception will immediately execute with an
       unpredictable value in x15.
    2. In some instances, SMC/HVC calls are made with preemption
       enabled (e.g. zx_smc_call). If a preemption pends during an SMC/
       HVC call, the thread may be migrated to a differnt CPU between
       the save and restore of x15. While the context swithc logic
       is able to fix up x15 values in exception frames, it does not
       know about the veneer stack frame, resulting in the veneer
       potentially restoring a stale value to x15.
    
    This patch addresses these issues by adding restoration of x15
    from TPIDR_EL1 to the EL1->EL1 exception paths AND disabling
    preemption around SMC/HVC calls. IRQs are not explicitly disabled
    by the SMC/HVC call veneers so that yielding SMC/HVC calls may be
    preemptable in contexts where IRQs are otherwise permitted.
    
    Fixed: 58334
    Test: Automated tests + ~24 hour burnin with video and synth load.
    Change-Id: I75eca2c24df49cee6bcb79bebf69be9e45f45027
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/417895
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Testability-Review: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Corey Tabaka <eieio@google.com>

--- zircon/kernel/arch/arm64/include/arch/arm64/smccc.h
+++ zircon/kernel/arch/arm64/include/arch/arm64/smccc.h
@@ -9,6 +9,8 @@
 
 #include <zircon/types.h>
 
+#include <kernel/auto_preempt_disabler.h>
+
 __BEGIN_CDECLS
 
 // ARM Secure Monitor Call Calling Convention (SMCCC)
@@ -23,17 +25,31 @@ typedef struct arm_smccc_result {
   uint64_t x6;  // at least one implementation uses it as a way to return session_id.
 } arm_smccc_result_t;
 
-arm_smccc_result_t arm_smccc_smc(uint32_t w0,               // Function Identifier
-                                 uint64_t x1, uint64_t x2,  // Parameters
-                                 uint64_t x3, uint64_t x4,  // Parameters
-                                 uint64_t x5, uint64_t x6,  // Parameters
-                                 uint32_t w7);              // Client ID[15:0], Secure OS ID[31:16]
+// Calls the low-level SMC function with preemption disabled.
+inline arm_smccc_result_t arm_smccc_smc(uint32_t w0,               // Function Identifier
+                                        uint64_t x1, uint64_t x2,  // Parameters
+                                        uint64_t x3, uint64_t x4,  // Parameters
+                                        uint64_t x5, uint64_t x6,  // Parameters
+                                        uint32_t w7) {  // Client ID[15:0], Secure OS ID[31:16]
+  extern arm_smccc_result_t arm_smccc_smc_internal(uint32_t w0, uint64_t x1, uint64_t x2,
+                                                   uint64_t x3, uint64_t x4, uint64_t x5,
+                                                   uint64_t x6, uint32_t w7);
+  AutoPreemptDisabler<APDInitialState::PREEMPT_DISABLED> disabler;
+  return arm_smccc_smc_internal(w0, x1, x2, x3, x4, x5, x6, w7);
+}
 
-arm_smccc_result_t arm_smccc_hvc(uint32_t w0,               // Function Identifier
-                                 uint64_t x1, uint64_t x2,  // Parameters
-                                 uint64_t x3, uint64_t x4,  // Parameters
-                                 uint64_t x5, uint64_t x6,  // Parameters
-                                 uint32_t w7);              // Secure OS ID[31:16]
+// Calls the low-level HVC function with preemption disabled.
+inline arm_smccc_result_t arm_smccc_hvc(uint32_t w0,               // Function Identifier
+                                        uint64_t x1, uint64_t x2,  // Parameters
+                                        uint64_t x3, uint64_t x4,  // Parameters
+                                        uint64_t x5, uint64_t x6,  // Parameters
+                                        uint32_t w7) {             // Secure OS ID[31:16]
+  extern arm_smccc_result_t arm_smccc_hvc_internal(uint32_t w0, uint64_t x1, uint64_t x2,
+                                                   uint64_t x3, uint64_t x4, uint64_t x5,
+                                                   uint64_t x6, uint32_t w7);
+  AutoPreemptDisabler<APDInitialState::PREEMPT_DISABLED> disabler;
+  return arm_smccc_hvc_internal(w0, x1, x2, x3, x4, x5, x6, w7);
+}
 
 __END_CDECLS
 
