e38d914421c819738afbba0377d59fba981b7e36
John Bauman <jbauman@google.com>
2020-08-18 19:50:33 +0000

    [amlogic_decoder] Remove H264MultiDecoderV1
    It's been completely replaced by a newer version.
    
    Change-Id: I6d0e7734aa14e4de33d285b94661053e0066535c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/417356
    Reviewed-by: Dustin Green <dustingreen@google.com>
    Testability-Review: John Bauman <jbauman@google.com>
    Commit-Queue: John Bauman <jbauman@google.com>

--- src/media/drivers/amlogic_decoder/local_codec_factory.cc
+++ src/media/drivers/amlogic_decoder/local_codec_factory.cc
@@ -12,7 +12,6 @@
 
 #include "codec_adapter_h264.h"
 #include "codec_adapter_h264_multi.h"
-#include "codec_adapter_h264_multi_v1.h"
 #include "codec_adapter_mpeg2.h"
 #include "codec_adapter_vp9.h"
 #include "device_ctx.h"
@@ -166,32 +165,6 @@ const CodecAdapterFactory kCodecFactories[] = {
         true,  // multi_instance
         [](std::mutex& lock, CodecAdapterEvents* events, DeviceCtx* device) {
           return std::make_unique<CodecAdapterH264Multi>(lock, events, device);
-        },
-    },
-    {
-        true,  // is_enabled
-        fuchsia::mediacodec::CodecDescription{
-            .codec_type = fuchsia::mediacodec::CodecType::DECODER,
-            // TODO(dustingreen): See TODO comments on this field in
-            // codec_common.fidl.
-            .mime_type = "video/h264-multi-v1",
-
-            // TODO(dustingreen): Determine which of these can safely indicate
-            // more capability.
-            .can_stream_bytes_input = false,
-            .can_find_start = false,
-            .can_re_sync = false,
-            .will_report_all_detected_errors = false,
-
-            .is_hw = true,
-
-            // TODO(dustingreen): Determine if this claim of "true" is actually
-            // the case.
-            .split_header_handling = true,
-        },
-        true,  // multi_instance
-        [](std::mutex& lock, CodecAdapterEvents* events, DeviceCtx* device) {
-          return std::make_unique<CodecAdapterH264MultiV1>(lock, events, device);
         },
     },
     {
