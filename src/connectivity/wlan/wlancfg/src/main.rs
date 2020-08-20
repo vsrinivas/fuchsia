536a455dd6e75d9bac248ea0880b4428f6ffdd02
Kevin Sakuma <sakuma@google.com>
2020-08-20 01:43:26 +0000

    [wlan][policy] Do not exit when location cannot be determined.
    Provides policy service in the absence of
    RegulatoryRegionWatcher.
    
    Bug: 58526
    Test: fx test run-without-regulatory-manager
    Multiply: run-without-regulatory-manager
    Change-Id: I3d59da2c8a81523b6ddbe255e2265edb8090b1b5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/418789
    Reviewed-by: Marc Khouri <mnck@google.com>
    Testability-Review: Marc Khouri <mnck@google.com>
    Commit-Queue: Kevin Sakuma <sakuma@google.com>

--- src/connectivity/wlan/wlancfg/src/main.rs
+++ src/connectivity/wlan/wlancfg/src/main.rs
@@ -29,13 +29,18 @@ use {
     },
     anyhow::{format_err, Context as _, Error},
     fidl_fuchsia_location_namedplace::RegulatoryRegionWatcherMarker,
-    fidl_fuchsia_wlan_device_service::DeviceServiceMarker,
+    fidl_fuchsia_wlan_device_service::{DeviceServiceMarker, DeviceServiceProxy},
     fidl_fuchsia_wlan_policy as fidl_policy, fuchsia_async as fasync,
     fuchsia_async::DurationExt,
     fuchsia_component::server::ServiceFs,
     fuchsia_zircon::prelude::*,
     futures::{
-        self, channel::mpsc, future::try_join3, lock::Mutex, prelude::*, select, TryFutureExt,
+        self,
+        channel::mpsc,
+        future::{try_join3, BoxFuture},
+        lock::Mutex,
+        prelude::*,
+        select, TryFutureExt,
     },
     log::{error, info},
     pin_utils::pin_mut,
@@ -199,16 +204,40 @@ async fn serve_fidl(
     }
 }
 
+// Some builds will not include the RegulatoryRegionWatcher.  In such cases, wlancfg can continue
+// to run, though it will not be able to set its regulatory region and will fallback to world wide.
+fn run_regulatory_manager(
+    wlan_svc: DeviceServiceProxy,
+    phy_manager: Arc<Mutex<PhyManager>>,
+    iface_manager: Arc<Mutex<IfaceManager>>,
+) -> BoxFuture<'static, Result<(), Error>> {
+    match fuchsia_component::client::connect_to_service::<RegulatoryRegionWatcherMarker>() {
+        Ok(regulatory_svc) => {
+            let regulatory_manager =
+                RegulatoryManager::new(regulatory_svc, wlan_svc, phy_manager, iface_manager);
+            let regulatory_fut = async move {
+                regulatory_manager.run().await.unwrap_or_else(|e| {
+                    error!("regulatory manager failed: {:?}", e);
+                });
+                Ok(())
+            };
+
+            Box::pin(regulatory_fut)
+        }
+        Err(e) => {
+            error!("could not connect to regulatory manager: {:?}", e);
+            let regulatory_fut = async move { Ok(()) };
+            Box::pin(regulatory_fut)
+        }
+    }
+}
+
 fn main() -> Result<(), Error> {
     util::logger::init();
 
     let mut executor = fasync::Executor::new().context("error create event loop")?;
     let wlan_svc = fuchsia_component::client::connect_to_service::<DeviceServiceMarker>()
         .context("failed to connect to device service")?;
-    let regulatory_svc =
-        fuchsia_component::client::connect_to_service::<RegulatoryRegionWatcherMarker>()
-            .context("failed to connect to regulatory region service")?;
-
     let saved_networks = Arc::new(executor.run_singlethreaded(SavedNetworksManager::new())?);
     let network_selector = Arc::new(NetworkSelector::new(Arc::clone(&saved_networks)));
     let phy_manager = Arc::new(Mutex::new(PhyManager::new(wlan_svc.clone())));
@@ -229,12 +258,6 @@ fn main() -> Result<(), Error> {
         saved_networks.clone(),
         client_event_sender,
     )));
-    let regulatory_manager = RegulatoryManager::new(
-        regulatory_svc,
-        wlan_svc.clone(),
-        phy_manager.clone(),
-        iface_manager.clone(),
-    );
 
     let legacy_client = shim::IfaceRef::new();
     let listener = device::Listener::new(
@@ -264,7 +287,9 @@ fn main() -> Result<(), Error> {
         .err_into()
         .and_then(|_| future::ready(Err(format_err!("Device watcher future exited unexpectedly"))));
 
-    let regulatory_fut = regulatory_manager.run();
+    let regulatory_fut =
+        run_regulatory_manager(wlan_svc.clone(), phy_manager.clone(), iface_manager.clone());
+
     executor
         .run_singlethreaded(try_join3(fidl_fut, regulatory_fut, dev_watcher_fut))
         .map(|_: (Void, (), Void)| ())
