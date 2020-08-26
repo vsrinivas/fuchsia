d6f557502617d774e3c4d0dbb4e0958fce19cd92
Tamir Duberstein <tamird@google.com>
2020-08-26 14:40:28 +0000

    [netstack] Update bug reference
    Fixed: 49547
    Change-Id: I01f11b634654b6b67c970bb3c00267c5e3e8301e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/421035
    Reviewed-by: Devon H. O'Dell <dhobsd@google.com>
    Testability-Review: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Tamir Duberstein <tamird@google.com>

--- src/connectivity/network/netstack/bench/bench_test.go
+++ src/connectivity/network/netstack/bench/bench_test.go
@@ -63,7 +63,7 @@ func (*nullChecksumOffloadEndpoint) Capabilities() stack.LinkEndpointCapabilitie
 var _ stack.LinkEndpoint = (*nullEndpoint)(nil)
 
 func BenchmarkWritePacket(b *testing.B) {
-	b.Skip("TODO(fxbug.dev/49547): re-enable once pprof crash is fixed in third_party/go")
+	b.Skip("TODO(https://github.com/golang/go/issues/40823): re-enable")
 	const nicid = 1
 	for _, test := range []struct {
 		name string
