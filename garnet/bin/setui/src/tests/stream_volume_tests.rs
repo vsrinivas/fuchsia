ba17a37b6499ed4fdc8adfa05cb644e703156a81
Bryce Lee <brycelee@google.com>
2020-08-26 20:51:11 +0000

    [SetUI][Volume] Simplify test_drop_thread.
    This changelist reduces the logic in test_drop_thread. The
    test is only using the environment to supply the service
    registry. Other parts, including the event factory/publisher
    can be handled outside. The current implementation's
    publisher capture logic and event listening is susceptible
    to timing issues.
    
    Fixed: 58968
    MULTIPLY: setui_service_tests
    Test: fx test -o setui_service_tests
    Change-Id: I620686e5eb0886537704e8c470ec22d78bc4576f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/420875
    Reviewed-by: William Xiao <wxyz@google.com>
    Reviewed-by: Chip Fukuhara <cfukuhara@google.com>
    Reviewed-by: Paul Faria <paulfaria@google.com>
    Testability-Review: William Xiao <wxyz@google.com>
    Commit-Queue: Bryce Lee <brycelee@google.com>

--- garnet/bin/setui/src/tests/stream_volume_tests.rs
+++ garnet/bin/setui/src/tests/stream_volume_tests.rs
@@ -8,12 +8,10 @@ use crate::handler::device_storage::testing::*;
 use crate::internal::event;
 use crate::message::base::{MessageEvent, MessengerType};
 use crate::service_context::ExternalServiceProxy;
-use crate::switchboard::base::{AudioStreamType, SettingType};
+use crate::switchboard::base::AudioStreamType;
 use crate::tests::fakes::audio_core_service::AudioCoreService;
 use crate::tests::fakes::service_registry::ServiceRegistry;
-use crate::tests::scaffold;
 use crate::EnvironmentBuilder;
-use futures::future::BoxFuture;
 use futures::lock::Mutex;
 use futures::StreamExt;
 use std::sync::Arc;
@@ -31,37 +29,22 @@ async fn create_service() -> Arc<Mutex<ServiceRegistry>> {
 // Tests that the volume event stream thread exits when the StreamVolumeControl is deleted.
 #[fuchsia_async::run_until_stalled(test)]
 async fn test_drop_thread() {
-    let event_factory: Arc<Mutex<Option<event::message::Factory>>> = Arc::new(Mutex::new(None));
-    let event_factory_capture = event_factory.clone();
-
-    let create_subscriber =
-        Arc::new(move |factory: event::message::Factory| -> BoxFuture<'static, ()> {
-            let event_factory = event_factory_capture.clone();
-            Box::pin(async move {
-                *event_factory.lock().await = Some(factory);
-            })
-        });
-
-    let service_registry = create_service().await;
-
     let env = EnvironmentBuilder::new(InMemoryStorageFactory::create())
-        .service(ServiceRegistry::serve(service_registry))
-        .settings(&[SettingType::Audio])
-        .event_subscribers(&[scaffold::event::subscriber::Blueprint::create(create_subscriber)])
+        .service(ServiceRegistry::serve(create_service().await))
         .spawn_and_get_nested_environment(ENV_NAME)
         .await
         .unwrap();
 
-    let factory =
-        event_factory.clone().lock().await.take().expect("Should have captured event factory");
-    let (_, mut receptor) = factory
-        .create(MessengerType::Unbound)
-        .await
-        .expect("Should be able to retrieve messenger for publisher");
+    let factory = event::message::create_hub();
+
+    let (_, mut receptor) =
+        factory.create(MessengerType::Unbound).await.expect("Should be able to retrieve receptor");
 
     let publisher = event::Publisher::create(&factory, MessengerType::Unbound).await;
 
     let audio_proxy = env.connect_to_service::<fidl_fuchsia_media::AudioCoreMarker>().unwrap();
+
+    // Scoped to cause the object to be dropped.
     {
         StreamVolumeControl::create(
             &ExternalServiceProxy::new(audio_proxy, None),
