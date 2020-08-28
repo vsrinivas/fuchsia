3daf40d7643208463bf398da6980cb38cfc43706
Matt Boetger <boetger@google.com>
2020-08-28 00:57:06 +0000

    [ffx] Move configs to .local/share
    Change-Id: I15aa8629bc6b201c415c48564e7cf0a9d92456de
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/421674
    Reviewed-by: James Tucker <raggi@google.com>
    Testability-Review: James Tucker <raggi@google.com>
    Commit-Queue: Matthew Boetger <boetger@google.com>

--- src/developer/ffx/core/src/lib.rs
+++ src/developer/ffx/core/src/lib.rs
@@ -7,9 +7,12 @@ use std::{env::var, fs::create_dir_all, path::PathBuf};
 pub use core_macros::{ffx_command, ffx_plugin};
 
 pub fn get_base_path() -> PathBuf {
-    let mut path = var("XDG_CONFIG_HOME")
-        .map(PathBuf::from)
-        .unwrap_or_else(|_| home::home_dir().expect("unknown home directory"));
+    let mut path = var("XDG_CONFIG_HOME").map(PathBuf::from).unwrap_or_else(|_| {
+        let mut home = home::home_dir().expect("unknown home directory");
+        home.push(".local");
+        home.push("share");
+        home
+    });
     path.push("Fuchsia");
     path.push("ffx");
     create_dir_all(&path).expect("unable to create ffx directory");
