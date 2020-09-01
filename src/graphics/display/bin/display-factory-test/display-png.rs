7b772dcf654303262618671342ba57deec2f1417
Rob Tsuk <rob@tsuk.com>
2020-09-01 17:22:00 +0000

    [factory] Support different gamma file format
    Support an already-in-use format for calibration.
    
    Also added a Fargo.toml file for easier testing.
    
    Testing: manual plus new unit tests for parsing the new format.
    Change-Id: Ie2d3289be8b4f3128b92f7fc51b0284225e9a3c9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/422934
    Reviewed-by: David Reveman <reveman@google.com>
    Reviewed-by: Payam Moradshahi <payamm@google.com>
    Testability-Review: David Reveman <reveman@google.com>
    Commit-Queue: Rob Tsuk <robtsuk@google.com>

--- src/graphics/display/bin/display-factory-test/display-png.rs
+++ src/graphics/display/bin/display-factory-test/display-png.rs
@@ -15,8 +15,7 @@ use {
     euclid::default::{Point2D, Rect},
     fuchsia_async as fasync,
     fuchsia_zircon::{AsHandleRef, Duration, Event, Signals},
-    std::fs::File,
-    std::process,
+    std::{fs::File, process},
 };
 
 const WHITE_COLOR: Color = Color { r: 255, g: 255, b: 255, a: 255 };
@@ -29,20 +28,16 @@ struct Args {
     #[argh(option)]
     file: Option<String>,
 
-    /// path to file containing gamma values to apply to the red channel
-    /// If this option is provided the other channels must also be provided.
+    /// path to file containing gamma values. The file format is
+    /// a header line plus three lines of 256 three-digit hexadecimal
+    /// value groups.
     #[argh(option)]
-    gamma_red_file: Option<String>,
+    gamma_file: Option<String>,
 
-    /// path to file containing gamma values to apply to the green channel
-    /// If this option is provided the other channels must also be provided.
-    #[argh(option)]
-    gamma_green_file: Option<String>,
-
-    /// path to file containing gamma values to apply to the blue channel
-    /// If this option is provided the other channels must also be provided.
-    #[argh(option)]
-    gamma_blue_file: Option<String>,
+    /// integer value with which to divide the gamma values found in gamma
+    /// values file. Does nothing if no gamma file is specified.
+    #[argh(option, default = "1023")]
+    gamma_divisor: i64,
 
     /// background color (default is white)
     #[argh(option, from_str_fn(parse_color))]
@@ -85,6 +80,59 @@ struct GammaValues {
     blue: Vec<f32>,
 }
 
+const VALUE_GROUP_LENGTH: usize = 3;
+const VALUE_COUNT: usize = 256;
+const VALUE_LINE_LENGTH: usize = VALUE_COUNT * VALUE_GROUP_LENGTH;
+
+impl GammaValues {
+    fn parse_values_line(line: &str, divisor: f32) -> Result<Vec<f32>, Error> {
+        ensure!(
+            line.len() == VALUE_LINE_LENGTH,
+            "Expected value line to be length {} but saw {}",
+            VALUE_LINE_LENGTH,
+            line.len()
+        );
+        let values: Vec<f32> = (0..VALUE_LINE_LENGTH)
+            .step_by(3)
+            .filter_map(|index| {
+                usize::from_str_radix(&line[index..index + 3], 16)
+                    .ok()
+                    .and_then(|value| Some(value as f32 / divisor))
+            })
+            .collect();
+        ensure!(
+            values.len() == VALUE_COUNT,
+            "Expected {} values, saw {}",
+            VALUE_COUNT,
+            values.len()
+        );
+        Ok(values)
+    }
+
+    fn parse(source_text: &str, divisor: f32) -> Result<Self, Error> {
+        let parts: Vec<&str> = source_text.split("\n").collect();
+        ensure!(parts.len() >= 4, "Expected four lines, got {}", parts.len());
+        let version_line_parts: Vec<&str> = parts[0].split(" ").collect();
+        ensure!(
+            version_line_parts == vec!["Gamma", "Calibration", "1.0"],
+            "Expected version on first line but saw '{}'",
+            parts[0]
+        );
+        let version = version_line_parts[2].parse::<f32>().context("parsing version number")?;
+        ensure!(version == 1.0, "expected version 1.0, got {}", version);
+        let red = Self::parse_values_line(parts[1], divisor)?;
+        let green = Self::parse_values_line(parts[2], divisor)?;
+        let blue = Self::parse_values_line(parts[3], divisor)?;
+        Ok(Self { red, green, blue })
+    }
+
+    fn parse_file(path: &str, divisor: f32) -> Result<Self, Error> {
+        let source_text = std::fs::read_to_string(path)
+            .context(format!("Error trying to parse gamma values file: {}", path))?;
+        Self::parse(&source_text, divisor)
+    }
+}
+
 #[derive(Default)]
 struct DisplayPngAppAssistant {
     png_source: Option<PngSourceInfo>,
@@ -93,33 +141,7 @@ struct DisplayPngAppAssistant {
     position: Option<Point2D<f32>>,
 }
 
-impl DisplayPngAppAssistant {
-    fn read_gamma_channel_file(src: &String) -> Result<Vec<f32>, Error> {
-        let file =
-            File::open(format!("{}", src)).context(format!("failed to open file {}", src))?;
-        let mut rdr = csv::ReaderBuilder::new().has_headers(false).from_reader(file);
-        let gamma_values: Vec<f32> = rdr
-            .records()
-            // map records, dropping any that don't parse correctly.
-            .filter_map(|record| {
-                // Get the first field, and if it is there, try to parse it as
-                // a f32 (inferred from the type of the field). Use .ok() to turn
-                // a parse failure into a None value which will get filtered out by
-                // filter_map. Finally scale the value.
-                // TODO(55351): Remove hard-coded 1023 scaling value.
-                record.ok().and_then(|record| {
-                    record
-                        .get(0)
-                        .and_then(|value_str| value_str.parse().ok())
-                        .and_then(|v: f32| Some(v / 1023.0))
-                })
-            })
-            .collect();
-
-        ensure!(gamma_values.len() == 256, "The gamma values file must have exactly 256 lines");
-        Ok(gamma_values)
-    }
-}
+impl DisplayPngAppAssistant {}
 
 impl AppAssistant for DisplayPngAppAssistant {
     fn setup(&mut self) -> Result<(), Error> {
@@ -146,21 +168,10 @@ impl AppAssistant for DisplayPngAppAssistant {
             });
         }
 
-        if args.gamma_red_file.is_some()
-            || args.gamma_green_file.is_some()
-            || args.gamma_blue_file.is_some()
-        {
-            ensure!(
-                args.gamma_red_file.is_some()
-                    && args.gamma_green_file.is_some()
-                    && args.gamma_blue_file.is_some(),
-                "When providing a gamma channel file one must provide all three"
-            );
-
-            let red = Self::read_gamma_channel_file(args.gamma_red_file.as_ref().unwrap())?;
-            let green = Self::read_gamma_channel_file(args.gamma_green_file.as_ref().unwrap())?;
-            let blue = Self::read_gamma_channel_file(args.gamma_blue_file.as_ref().unwrap())?;
-            self.gamma_values = Some(GammaValues { red, green, blue });
+        if let Some(gamma_file) = args.gamma_file {
+            ensure!(args.gamma_divisor != 0, "gamma_divisor value must be non-zero");
+            self.gamma_values =
+                Some(GammaValues::parse_file(&gamma_file, args.gamma_divisor as f32)?);
         }
 
         Ok(())
@@ -307,6 +318,44 @@ impl ViewAssistant for DisplayPngViewAssistant {
 
         ready_event.as_handle_ref().signal(Signals::NONE, Signals::EVENT_SIGNALED)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const LEGAL_SAMPLE_DATA: &'static str = r#"Gamma Calibration 1.0
+00000300700a00e01101401801b01f02202602902d03003403703b03e04204504904c05005305705b05e06206506906d07007407807b07f08308608a08e09209509909d0a10a50a80ac0b00b40b80bc0bf0c30c70cb0ce0d20d60da0de0e10e50e90ec0f00f40f70fb0ff10210610910d11111411811b11f12312612a12d13113413813c13f14314614a14e15115515815c16016316716b16e17217617917d18018418818b18f19319619a19e1a11a51a91ac1b01b41b71bb1bf1c21c61ca1cd1d11d51d81dc1e01e31e71eb1ee1f21f61f91fd20120420820c20f21321721a21e22222522922d23123423823c23f24324724b24e25225625a25d26126526926c27027427827b27f28328728b28e29229629a29e2a12a52a92ad2b12b52b82bc2c02c42c82cc2d02d32d72db2df2e32e72eb2ef2f32f72fa2fe30230630a30e31231631a31e32232632932d33133533933d34134534934c35035435835c36036336736b36f37337737a37e38238638a38e39139539939d3a13a53a83ac3b03b4
+00000400700b00f01201601a01d02102502902c03003403803b03f04304704b04e05205605a05e06206506906d07107507907d08108508908d09109509909d0a10a50a90ad0b10b50b90bd0c10c50c90cd0d20d60da0de0e20e60ea0ee0f20f60fa0fe10210610a10d11111511911d12112512912c13013413813c14014414714b14f15315715b15f16216616a16e17217617a17e18218618918d19119519919d1a11a51a91ad1b11b51b91bd1c11c51c81cc1d01d41d81dc1e01e41e81ec1f01f41f81fc20020420820b20f21321721b21f22322722b22f23323723b23f24224624a24e25225625a25e26226626a26e27227627a27e28228628a28e29329729b29f2a32a72ab2af2b32b72bb2bf2c42c82cc2d02d42d82dc2e02e52e92ed2f12f52f92fe30230630a30e31331731b31f32332832c33033433933d34134534a34e35235635b35f36336736b37037437837c38038438938d39139539939d3a23a63aa3ae3b23b63ba3be3c33c73cb3cf3d33d73db3df3e33e83ec3f03f43f83fc
+00000300700a00e01101501801c01f02202602902d03003403803b03f04204604904d05005405805b05f06206606a06d07107507807c08008408708b08f09309609a09e0a20a60a90ad0b10b50b90bd0c00c40c80cc0d00d40d70db0df0e30e60ea0ee0f10f50f90fc10010410710b10f11211611911d12112412812c12f13313613a13e14114514814c15015315715b15e16216516916d17017417817b17f18318718a18e19219519919d1a01a41a81ab1af1b31b61ba1be1c21c51c91cd1d01d41d81db1df1e31e61ea1ee1f11f51f91fd20020420820b20f21321621a21e22122522922d23023423823b23f24324724a24e25225625925d26126526926c27027427827c27f28328728b28f29229629a29e2a22a62a92ad2b12b52b92bd2c12c52c82cc2d02d42d82dc2e02e42e82ec2f02f32f72fb2ff30330730b30f31331731b31f32332732b32f33333633a33e34234634a34e35235635935d36136536936d37137537837c38038438838c39039339739b39f3a33a73aa3ae3b23b63ba"#;
+
+    #[test]
+    fn legal_calibration_data() {
+        let gamma_values = GammaValues::parse(LEGAL_SAMPLE_DATA, 1023.0).expect("parse");
+        assert_eq!(gamma_values.red.len(), 256);
+        assert_eq!(gamma_values.red[0], 0.0);
+        assert_eq!(gamma_values.green.len(), 256);
+        assert_eq!((gamma_values.green[1] * 1023.0) as usize, 4);
+        assert_eq!(gamma_values.blue.len(), 256);
+        assert_eq!((gamma_values.blue[255] * 1023.0) as usize, 0x3ba);
+    }
+
+    const TRUNCATED_SAMPLE_DATA: &'static str =
+        "Gamma Calibration 1.0\n00000300700a00e01101401801b01f02202602902d0300340\n\n\n";
+
+    #[test]
+    fn empty_calibration_data() {
+        let gamma_values = GammaValues::parse(TRUNCATED_SAMPLE_DATA, 1023.0);
+        assert!(gamma_values.is_err());
+    }
+
+    const BAD_VERSION_SAMPLE_DATA: &'static str = "Gamma Calibration 2.0\n\n\n\n";
+
+    #[test]
+    fn bad_version_calibration_data() {
+        let gamma_values = GammaValues::parse(BAD_VERSION_SAMPLE_DATA, 1023.0);
+        assert!(gamma_values.is_err());
     }
 }
 
