3dcbc43e2b785b9c273d719906dd217c93c2f515
Dangyi Liu <dangyi@google.com>
2020-09-02 18:36:27 +0000

    [symbol-index] Add Rust's prebuilt symbols
    This resolves missing symbols from libstd-xxxxxx.so when "-r" option is
    not enabled for `fx symbolize`.
    
    Change-Id: I60c1e66b98526699d79576bfcb43ffd7e70e9525
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/423194
    Reviewed-by: Francois Rousseau <frousseau@google.com>
    Testability-Review: Francois Rousseau <frousseau@google.com>
    Commit-Queue: Dangyi Liu <dangyi@google.com>

--- tools/devshell/contrib/lib/symbol-index.sh
+++ tools/devshell/contrib/lib/symbol-index.sh
@@ -29,6 +29,7 @@ ${FUCHSIA_BUILD_DIR}/.build-id     ${FUCHSIA_BUILD_DIR}
 ${ZIRCON_BUILDROOT}/.build-id      ${ZIRCON_BUILDROOT}
 ${FUCHSIA_DIR}/prebuilt/.build-id
 ${PREBUILT_CLANG_DIR}/lib/debug/.build-id
+${PREBUILT_RUST_DIR}/lib/debug/.build-id
 EOF
   return $?
 }
