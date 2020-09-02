22b9822addf9c0e1ea9ba8091cd2f383df47fde0
Drew Fisher <zarvox@google.com>
2020-09-02 02:21:06 +0000

    [block-verity] use zxlogf in config
    `//src/devices/testing/fake_ddk` exists to (among other things) wire
    up zxlogf so your code that wants to log things in the driver
    environment can still run in a unit test.  Use it.
    
    This change will allow us to see in the logs why a config was rejected
    by the block-verity driver.
    
    Change-Id: Icc92c57d5ef374b0d77fa593b91ac030882f4999
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/423245
    Reviewed-by: Allison Pearce <ampearce@google.com>
    Testability-Review: Drew Fisher <zarvox@google.com>
    Commit-Queue: Drew Fisher <zarvox@google.com>

--- src/devices/block/drivers/block-verity/config.cc
+++ src/devices/block/drivers/block-verity/config.cc
@@ -12,34 +12,34 @@ zx_status_t CheckConfig(const ::llcpp::fuchsia::hardware::block::verified::Confi
                         const block_info_t& blk) {
   // Check that the config specifies a supported hash function
   if (!config.has_hash_function()) {
-    printf("Config did not specify a hash function");
+    zxlogf(WARNING, "Config did not specify a hash function");
     return ZX_ERR_INVALID_ARGS;
   }
   switch (config.hash_function()) {
     case ::llcpp::fuchsia::hardware::block::verified::HashFunction::SHA256:
       break;
     default:
-      printf("Unknown hash function enum value %hhu\n", config.hash_function());
+      zxlogf(WARNING, "Unknown hash function enum value %hhu\n", config.hash_function());
       return ZX_ERR_INVALID_ARGS;
   }
 
   // Check that the config specifies a supported block size, and that the block
   // size matches that of the underlying block device
   if (!config.has_block_size()) {
-    printf("Config did not specify a block size\n");
+    zxlogf(WARNING, "Config did not specify a block size\n");
     return ZX_ERR_INVALID_ARGS;
   }
   switch (config.block_size()) {
     case ::llcpp::fuchsia::hardware::block::verified::BlockSize::SIZE_4096:
       // Verify that the block size from the device matches the value requested.
       if (blk.block_size != 4096) {
-        printf("Config specified block size 4096 but underlying block size is %d\n",
+        zxlogf(WARNING, "Config specified block size 4096 but underlying block size is %d\n",
                blk.block_size);
         return ZX_ERR_INVALID_ARGS;
       }
       break;
     default:
-      printf("Unknown block size enum value %hhu\n", config.block_size());
+      zxlogf(WARNING, "Unknown block size enum value %hhu\n", config.block_size());
       return ZX_ERR_INVALID_ARGS;
   }
 
