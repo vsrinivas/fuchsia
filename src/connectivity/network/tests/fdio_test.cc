66d40f4505dc76f3b6879f0a5e6b245b657d629a
Tamir Duberstein <tamird@google.com>
2020-09-10 15:28:26 +0000

    [fdio_test] Tidy up
    - Simplify NoClose test (remove I/O, remove looping)
    - Add precondition check in NoClose test
    - Avoid double-close through fdio_fd_transfer(<fd>.get(), ...)
    - Use RAII types throughout
    - Use INADDR_LOOPBACK instead of LOOPADDR_ANY
    - Use designated initialization
    - Use std::array over std::vector where possible
    
    Change-Id: I8168a81c70be1835f5b10562907c232b1a7ce254
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/425596
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Testability-Review: Bruno Dal Bo <brunodalbo@google.com>
    Commit-Queue: Tamir Duberstein <tamird@google.com>

--- src/connectivity/network/tests/fdio_test.cc
+++ src/connectivity/network/tests/fdio_test.cc
@@ -9,6 +9,7 @@
 #include <lib/fdio/fd.h>
 #include <lib/fdio/spawn.h>
 #include <lib/sync/completion.h>
+#include <lib/zx/process.h>
 #include <netinet/in.h>
 #include <poll.h>
 #include <sys/socket.h>
@@ -24,88 +25,63 @@
 #include "src/lib/testing/predicates/status.h"
 #include "util.h"
 
-TEST(NetStreamTest, BlockingAcceptWriteNoClose) {
-  short port = 0;  // will be assigned by the first bind.
+TEST(NetStreamTest, ReleasePortNoClose) {
+  fbl::unique_fd first;
+  ASSERT_TRUE(first = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
 
-  for (int j = 0; j < 2; j++) {
-    int acptfd;
-    ASSERT_GE(acptfd = socket(AF_INET, SOCK_STREAM, 0), 0) << strerror(errno);
+  struct sockaddr_in addr = {
+      .sin_family = AF_INET,
+      .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+  };
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = port;
-    addr.sin_addr.s_addr = INADDR_ANY;
+  ASSERT_EQ(bind(first.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr)), 0)
+      << strerror(errno);
 
-    int ret = 0;
-    int backoff_msec = 10;
-    for (;;) {
-      ret = bind(acptfd, (const struct sockaddr*)&addr, sizeof(addr));
-      if (j > 0 && ret < 0 && errno == EADDRINUSE) {
-        // Wait until netstack detects the peer handle is closed and
-        // tears down the port.
-        zx_nanosleep(zx_deadline_after(ZX_MSEC(backoff_msec)));
-        backoff_msec *= 2;
-      } else {
-        break;
-      }
+  socklen_t addrlen = sizeof(addr);
+  ASSERT_EQ(getsockname(first.get(), reinterpret_cast<struct sockaddr*>(&addr), &addrlen), 0)
+      << strerror(errno);
+  ASSERT_EQ(addrlen, sizeof(addr));
+
+  fbl::unique_fd second;
+  ASSERT_TRUE(second = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
+
+  // Confirm bind fails while address is in use.
+  ASSERT_EQ(bind(second.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr)), -1);
+  ASSERT_EQ(errno, EADDRINUSE) << strerror(errno);
+
+  // Simulate unexpected process exit by closing the handle without calling close.
+  zx::handle handle;
+  zx_status_t status = fdio_fd_transfer(first.release(), handle.reset_and_get_address());
+  ASSERT_EQ(status, ZX_OK) << zx_status_get_string(status);
+  status = zx_handle_close(handle.release());
+  ASSERT_EQ(status, ZX_OK) << zx_status_get_string(status);
+
+  while (true) {
+    int ret = bind(second.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr));
+    if (ret == 0) {
+      break;
     }
-    ASSERT_EQ(ret, 0) << "bind failed: " << strerror(errno) << " port: " << port;
+    ASSERT_EQ(ret, -1);
+    ASSERT_EQ(errno, EADDRINUSE) << strerror(errno);
 
-    socklen_t addrlen = sizeof(addr);
-    ASSERT_EQ(getsockname(acptfd, (struct sockaddr*)&addr, &addrlen), 0) << strerror(errno);
-    ASSERT_EQ(addrlen, sizeof(addr));
-
-    // remember the assigned port and use it for the next bind.
-    port = addr.sin_port;
-
-    ASSERT_EQ(listen(acptfd, 1), 0) << strerror(errno);
-
-    int clientfd;
-    ASSERT_GE(clientfd = socket(AF_INET, SOCK_STREAM, 0), 0) << strerror(errno);
-    ASSERT_EQ(connect(clientfd, (const struct sockaddr*)&addr, sizeof(addr)), 0) << strerror(errno);
-
-    int connfd;
-    ASSERT_GE(connfd = accept(acptfd, nullptr, nullptr), 0) << strerror(errno);
-
-    const char msg[] = "hello";
-    ASSERT_EQ(write(connfd, msg, sizeof(msg)), (ssize_t)sizeof(msg)) << strerror(errno);
-    ASSERT_EQ(close(connfd), 0) << strerror(errno);
-
-    struct pollfd pfd = {
-        .fd = clientfd,
-        .events = POLLIN,
-    };
-    ASSERT_EQ(poll(&pfd, 1, kTimeout), 1) << strerror(errno);
-    char buf[sizeof(msg) + 1] = {};
-    ASSERT_EQ(read(clientfd, buf, sizeof(buf)), (ssize_t)sizeof(msg)) << strerror(errno);
-    ASSERT_STREQ(buf, msg);
-    ASSERT_EQ(close(clientfd), 0) << strerror(errno);
-
-    // Simulate unexpected process exit by closing the handle
-    // without sending a Close op to netstack.
-    zx_handle_t handle;
-    zx_status_t status = fdio_fd_transfer(acptfd, &handle);
-    ASSERT_EQ(status, ZX_OK) << zx_status_get_string(status);
-    status = zx_handle_close(handle);
-    ASSERT_EQ(status, ZX_OK) << zx_status_get_string(status);
+    zx::nanosleep(zx::deadline_after(zx::msec(20)));
   }
 }
 
 TEST(NetStreamTest, RaceClose) {
-  int fd;
-  ASSERT_GE(fd = socket(AF_INET, SOCK_STREAM, 0), 0) << strerror(errno);
+  fbl::unique_fd fd;
+  ASSERT_TRUE(fd = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
 
-  zx_handle_t handle;
-  zx_status_t status = fdio_fd_transfer(fd, &handle);
+  ::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient client;
+  zx_status_t status =
+      fdio_fd_transfer(fd.release(), client.mutable_channel()->reset_and_get_address());
   ASSERT_EQ(status, ZX_OK) << zx_status_get_string(status);
 
   sync_completion_t completion;
 
-  ::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient client((zx::channel(handle)));
-
-  std::vector<std::thread> workers;
-  for (int i = 0; i < 10; i++) {
-    workers.push_back(std::thread([&client, &completion]() {
+  std::array<std::thread, 10> workers;
+  for (auto& worker : workers) {
+    worker = std::thread([&client, &completion]() {
       zx_status_t status = sync_completion_wait(&completion, ZX_TIME_INFINITE);
       ASSERT_EQ(status, ZX_OK) << zx_status_get_string(status);
 
@@ -115,7 +91,7 @@ TEST(NetStreamTest, RaceClose) {
       } else {
         EXPECT_EQ(status = response.Unwrap()->s, ZX_OK) << zx_status_get_string(status);
       }
-    }));
+    });
   }
 
   sync_completion_signal(&completion);
@@ -127,12 +103,12 @@ TEST(SocketTest, ZXSocketSignalNotPermitted) {
   fbl::unique_fd fd;
   ASSERT_TRUE(fd = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
 
-  zx::channel channel;
+  ::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient client;
   zx_status_t status;
-  ASSERT_EQ(status = fdio_fd_transfer(fd.get(), channel.reset_and_get_address()), ZX_OK)
+  ASSERT_EQ(
+      status = fdio_fd_transfer(fd.release(), client.mutable_channel()->reset_and_get_address()),
+      ZX_OK)
       << zx_status_get_string(status);
-
-  ::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient client(std::move(channel));
 
   auto response = client.Describe();
   ASSERT_EQ(status = response.status(), ZX_OK) << zx_status_get_string(status);
@@ -226,11 +202,11 @@ TYPED_TEST(SocketTest, CloseResourcesOnClose) {
   ASSERT_EQ(status = fdio_fd_clone(this->fd_.get(), clone.reset_and_get_address()), ZX_OK)
       << zx_status_get_string(status);
 
-  zx::channel channel;
-  ASSERT_EQ(status = fdio_fd_transfer(this->fd_.release(), channel.reset_and_get_address()), ZX_OK)
+  typename TypeParam::Client client;
+  ASSERT_EQ(status = fdio_fd_transfer(this->fd_.release(),
+                                      client.mutable_channel()->reset_and_get_address()),
+            ZX_OK)
       << zx_status_get_string(status);
-
-  typename TypeParam::Client client(std::move(channel));
 
   auto describe_response = client.Describe();
   ASSERT_EQ(status = describe_response.status(), ZX_OK) << zx_status_get_string(status);
@@ -273,9 +249,10 @@ TEST(SocketTest, AcceptedSocketIsConnected) {
   fbl::unique_fd serverfd;
   ASSERT_TRUE(serverfd = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
 
-  struct sockaddr_in addr = {};
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  struct sockaddr_in addr = {
+      .sin_family = AF_INET,
+      .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+  };
   ASSERT_EQ(bind(serverfd.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr)), 0)
       << strerror(errno);
   ASSERT_EQ(listen(serverfd.get(), 1), 0) << strerror(errno);
@@ -298,12 +275,12 @@ TEST(SocketTest, AcceptedSocketIsConnected) {
   ASSERT_TRUE(connfd = fbl::unique_fd(accept(serverfd.get(), nullptr, nullptr))) << strerror(errno);
   ASSERT_EQ(close(serverfd.release()), 0) << strerror(errno);
 
-  zx::channel channel;
+  ::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient client;
   zx_status_t status;
-  ASSERT_EQ(status = fdio_fd_transfer(connfd.get(), channel.reset_and_get_address()), ZX_OK)
+  ASSERT_EQ(status = fdio_fd_transfer(connfd.release(),
+                                      client.mutable_channel()->reset_and_get_address()),
+            ZX_OK)
       << zx_status_get_string(status);
-
-  ::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient client(std::move(channel));
 
   auto response = client.Describe();
   ASSERT_EQ(status = response.status(), ZX_OK) << zx_status_get_string(status);
@@ -326,9 +303,10 @@ TEST(SocketTest, CloseClonedSocketAfterTcpRst) {
   fbl::unique_fd serverfd;
   ASSERT_TRUE(serverfd = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
 
-  struct sockaddr_in addr = {};
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  struct sockaddr_in addr = {
+      .sin_family = AF_INET,
+      .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+  };
   ASSERT_EQ(bind(serverfd.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr)), 0)
       << strerror(errno);
   ASSERT_EQ(listen(serverfd.get(), 1), 0) << strerror(errno);
@@ -352,9 +330,8 @@ TEST(SocketTest, CloseClonedSocketAfterTcpRst) {
   ASSERT_EQ(close(serverfd.release()), 0) << strerror(errno);
 
   // Clone the file descriptor a bunch of times to increase the refcount.
-  std::vector<fbl::unique_fd> connfds;
-  for (int i = 0; i < 10; i++) {
-    fbl::unique_fd clonefd;
+  std::array<fbl::unique_fd, 10> connfds;
+  for (auto& clonefd : connfds) {
     {
       zx_status_t status;
       zx::channel channel;
@@ -364,7 +341,6 @@ TEST(SocketTest, CloseClonedSocketAfterTcpRst) {
       ASSERT_EQ(status = fdio_fd_create(channel.release(), clonefd.reset_and_get_address()), ZX_OK)
           << zx_status_get_string(status);
     }
-    connfds.push_back(std::move(clonefd));
   }
 
   // Fill up the rcvbuf (client-side).
@@ -390,13 +366,13 @@ TEST(SocketTest, CloseClonedSocketAfterTcpRst) {
 
   // Now that the socket's endpoint has been closed, clone the socket again to increase the
   // endpoint's reference count, then close all copies of the socket.
-  std::vector<::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient> clients;
-  for (int i = 0; i < 10; i++) {
-    zx::channel channel;
+  std::array<::llcpp::fuchsia::posix::socket::StreamSocket::SyncClient, 10> clients;
+  for (auto& client : clients) {
     zx_status_t status;
-    ASSERT_EQ(status = fdio_fd_clone(connfd.get(), channel.reset_and_get_address()), ZX_OK)
+    ASSERT_EQ(
+        status = fdio_fd_clone(connfd.get(), client.mutable_channel()->reset_and_get_address()),
+        ZX_OK)
         << zx_status_get_string(status);
-    clients.emplace_back(std::move(channel));
   }
 
   for (auto& client : clients) {
@@ -431,7 +407,7 @@ TEST(SocketTest, PassFD) {
   }
   ASSERT_EQ(listen(listener.get(), 1), 0) << strerror(errno);
 
-  zx::handle proc;
+  zx::process proc;
   {
     fbl::unique_fd client;
     ASSERT_TRUE(client = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
