3b0a68866c710cac91fb6fad8a0043dae55eb802
Ben Lawson <benlawson@google.com>
2020-09-11 17:31:38 +0000

    [bt][ddk] Create ddk.protocol.bt.vendor protocol
    Create ddk.protocol.bt.vendor DDK protocol for Bluetooth vendor drivers
    to implement. The protocol supports getting supported features and
    encoding vendor HCI commands.
    
    Bug: 57163
    
    Change-Id: Id9baa0011d575bf5b01f562f24544ae1267d5a05
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/421819
    Commit-Queue: Ben Lawson <benlawson@google.com>
    Reviewed-by: Xo Wang <xow@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Testability-Review: Suraj Malhotra <surajmalhotra@google.com>

--- src/lib/ddk/include/ddk/protodefs.h
+++ src/lib/ddk/include/ddk/protodefs.h
@@ -169,5 +169,6 @@ DDK_PROTOCOL_DEF(TEMPERATURE,             145, "temperature", 0)
 DDK_PROTOCOL_DEF(VREG,                    146, "vreg", 0)
 DDK_PROTOCOL_DEF(ADC,                     147, "adc", 0)
 DDK_PROTOCOL_DEF(DSI,                     148, "dsi", PF_NOPUB)
+DDK_PROTOCOL_DEF(BT_VENDOR,               149, "bt-vendor", 0)
 #undef DDK_PROTOCOL_DEF
 #endif
