5087d0acba49d69d8ac3cfb6e80f6fdd63a5b5cc
Corey Tabaka <eieio@google.com>
2020-09-11 00:31:37 +0000

    [kernel][lib] Clean up / update relaxed atomic.
    - Remove forward semantics.
    - Add fetch_add and operator+=().
    - Bring signatures closer in line with std::atomic.
    - Add note about the oddness of the std/ktl::atomic signatures.
    
    Change-Id: I3da9c4fc8f91c8f5196959f565e03945acb48dcc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/425532
    Testability-Review: Corey Tabaka <eieio@google.com>
    Commit-Queue: Corey Tabaka <eieio@google.com>
    Reviewed-by: John Grossman <johngro@google.com>

--- zircon/kernel/include/lib/relaxed_atomic.h
+++ zircon/kernel/include/lib/relaxed_atomic.h
@@ -8,41 +8,32 @@
 #define ZIRCON_KERNEL_INCLUDE_LIB_RELAXED_ATOMIC_H_
 
 #include <ktl/atomic.h>
-#include <ktl/forward.h>
 
-// A wrapper of std/ktl atomic that defaults to relaxed operations.
-// Currently doesn't expose all functionality, feel free to expand as needed.
-// Be careful adding read/modify/write, think carefully if they make sense as
-// relaxed operations.
-template<typename T>
+// Wrapper around ktl::atomic that assumes ktl::memory_order_relaxed for all
+// operations to simplify pure relaxed use cases. Only a subset of operations
+// are supported as needed.
+//
+// NOTE: ktl::atomic has specific defintions for implicit constructors,
+// coversion operators, and compound assignment operators. This utility attempts
+// to mirror the signatures and beahvior of the underlying atomic as closely as
+// possible.
+template <typename T>
 class RelaxedAtomic {
  public:
   RelaxedAtomic() noexcept = default;
-  RelaxedAtomic(T&& desired) : wrapped_(ktl::forward<T>(desired)) {}
+  constexpr RelaxedAtomic(T desired) : wrapped_(desired) {}
 
-  template<typename U>
-  T operator=(U&& desired) noexcept {
-    wrapped_.store(ktl::forward<U>(desired), ktl::memory_order_relaxed);
-    return desired;
-  }
+  T load() const noexcept { return wrapped_.load(ktl::memory_order_relaxed); }
+  void store(T desired) noexcept { wrapped_.store(desired, ktl::memory_order_relaxed); }
+  T fetch_add(T value) noexcept { return wrapped_.fetch_add(value, ktl::memory_order_relaxed); }
 
-  operator T() const noexcept {
-    return load();
-  }
+  operator T() const noexcept { return load(); }
 
-  T load() const {
-    return wrapped_.load(ktl::memory_order_relaxed);
-  }
-
-  template<typename U>
-  void store(U&& value) {
-    wrapped_.store(ktl::forward<U>(value), ktl::memory_order_relaxed);
-  }
+  T operator=(T desired) noexcept { return store(desired), desired; }
+  T operator+=(T value) noexcept { return fetch_add(value) + value; }
 
  private:
   ktl::atomic<T> wrapped_;
 };
 
-
 #endif  // ZIRCON_KERNEL_INCLUDE_LIB_RELAXED_ATOMIC_H_
-
