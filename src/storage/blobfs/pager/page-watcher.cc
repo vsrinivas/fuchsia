9e936e78595ad19bc89f8823560cd64beb6aee05
Rasha Eqbal <rashaeqbal@google.com>
2020-09-15 07:03:08 +0000

    [blobfs][logs] Remove PageWatcher log on async loop shutdown
    When the pager async loop is shutting down, each blob's PageWatcher
    receives a ZX_ERR_CANCELED packet, so it can mark itself safe for
    destruction. This was not a commonly exercised code path, so we logged
    an info message when this happened - most blobs would have already been
    destroyed before the async loop shut down. With blob eviction disabled,
    this is now the typical path, resulting in a lot of log spew when blobfs
    is shutting down.
    
    Test: Purely a change in logging. No change in behavior.
    Change-Id: I3864d5f14029c5ee01779b12a026770e897c1af1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/427139
    Commit-Queue: James Sullivan <jfsulliv@google.com>
    Reviewed-by: James Sullivan <jfsulliv@google.com>
    Testability-Review: James Sullivan <jfsulliv@google.com>

--- src/storage/blobfs/pager/page-watcher.cc
+++ src/storage/blobfs/pager/page-watcher.cc
@@ -63,8 +63,6 @@ void PageWatcher::HandlePageRequest(async_dispatcher_t* dispatcher, async::Paged
     // Signal here without waiting for a ZX_PAGER_VMO_COMPLETE packet, to prevent holding up
     // destruction indefinitely. The pager async loop is shutting down, so we won't receive any more
     // packets on its port.
-    FS_TRACE_INFO("blobfs: Pager async loop shutting down. Signaling detach for vmo %u\n",
-                  vmo_->get());
     SignalPagerDetach();
     return;
   }
