c114833913dc040e2529c1747dddc638dcc1fb64
Clayton Wilkinson <wilkinsonclay@google.com>
2020-09-15 21:15:31 +0000

    [GN SDK] Fixing fssh.sh default option processing
    This fixes the situation when both the device-name and
    the device-ip have been set using `fconfig.sh`. Now, the
    device-ip value takes precedence.
    
    As normal, the command line options override the default values.
    
    Bug: 58596
    Change-Id: I4f273bb634def39a6d6978b8ad7a285c86bd6272
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/427337
    Reviewed-by: Jonathan Koren <jdkoren@google.com>
    Testability-Review: Jonathan Koren <jdkoren@google.com>
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>

--- scripts/sdk/gn/base/bin/fssh.sh
+++ scripts/sdk/gn/base/bin/fssh.sh
@@ -14,18 +14,26 @@ readonly MY_SCRIPT_SRC_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2
 source "${MY_SCRIPT_SRC_DIR}/fuchsia-common.sh" || exit $?
 
 function usage {
-  echo "Usage: $0"
+  echo "Usage: $(basename "$0") [args]"
+  echo "  [--help]"
+  echo "    This message."
   echo "  [--device-name <device hostname>]"
-  echo "    Connects to a device by looking up the given device hostname."
-  echo "  [--device-ip <device ipaddr>]"
-  echo "    Connects to a device by using the provided IP address, cannot use with --device-name"
+  echo "    Connects to the device with the given device hostname. Cannot be used with --device-ip."
+  echo "    Defaults to the value from \`fconfig.sh get device-name\`."
+  echo "  [--device-ip <device ip>]"
+  echo "    Connects to the device with the given device ip address. Cannot be used with --device-name."
+  echo "    Defaults to the value from \`fconfig.sh get device-ip\`."
+  echo "    Note: If defaults are configured for both device-name and device-ip, then device-ip is used."
+  echo "          If the device is specified at all, then the first device discovered is used."
   echo "  [--private-key <identity file>]"
   echo "    Uses additional private key when using ssh to access the device."
+  echo
+  echo "All positional arguments are passed through to SSH to be executed on the device."
 }
 
 PRIVATE_KEY_FILE=""
-DEVICE_NAME_FILTER="$(get-fuchsia-property device-name)"
-DEVICE_IP="$(get-fuchsia-property device-ip)"
+DEVICE_NAME_FILTER=""
+DEVICE_IP_ADDR=""
 POSITIONAL=()
 
 
@@ -38,20 +46,24 @@ case $1 in
     ;;
     --device-ip)
       shift
-      DEVICE_IP="${1}"
+      DEVICE_IP_ADDR="${1}"
     ;;
     --private-key)
-    shift
-    PRIVATE_KEY_FILE="${1}"
+      shift
+      PRIVATE_KEY_FILE="${1}"
     ;;
-    -*)
-    if [[ "${#POSITIONAL[@]}" -eq 0 ]]; then
-      echo "Unknown option ${1}"
+    --help)
       usage
       exit 1
-    else
-      POSITIONAL+=("${1}")
-    fi
+    ;;
+    -*)
+      if [[ "${#POSITIONAL[@]}" -eq 0 ]]; then
+        echo "Unknown option ${1}"
+        usage
+        exit 1
+      else
+        POSITIONAL+=("${1}")
+      fi
     ;;
     *)
       POSITIONAL+=("${1}")
@@ -60,24 +72,36 @@ esac
 shift
 done
 
+if [[ "${DEVICE_IP_ADDR}" == "" && "${DEVICE_NAME_FILTER}" == "" ]]; then
+  # No device specified on the command line, so use the default IP, then the
+  # default name if configured.
+  DEVICE_IP_ADDR="$(get-fuchsia-property device-ip)"
+  if [[ -z "${DEVICE_IP_ADDR}" ]]; then
+    DEVICE_NAME_FILTER="$(get-fuchsia-property device-name)"
+    if [[ -n "${DEVICE_NAME_FILTER}" ]]; then
+      echo "Using device name ${DEVICE_NAME_FILTER}. Use --device-name or fconfig.sh to use another device."
+    fi
+  else
+    echo "Using device address ${DEVICE_IP_ADDR}. Use --device-ip or fconfig.sh to use another device."
+  fi
+elif [[ "${DEVICE_IP_ADDR}" != "" && "${DEVICE_NAME_FILTER}" != "" ]]; then
+  fx-error "Cannot use both --device-name and --device-ip".
+  exit 1
+fi
+
 readonly PRIVATE_KEY_FILE
 readonly DEVICE_NAME_FILTER
-readonly DEVICE_IP
+readonly DEVICE_IP_ADDR
 readonly POSITIONAL
 
-target_device_ip="${DEVICE_IP}"
+target_device_ip="${DEVICE_IP_ADDR}"
 
 # Get the device IP address.
-if [[ "${DEVICE_IP}" == "" ]]; then
-  target_device_ip=$(get-device-ip-by-name "$DEVICE_NAME_FILTER")
+if [[ "${target_device_ip}" == "" ]]; then
+  target_device_ip=$(get-device-ip-by-name "${DEVICE_NAME_FILTER}")
   if [[ ! "$?" || -z "$target_device_ip" ]]; then
     fx-error "Error finding device"
     exit 2
-  fi
-else
-  if [[ "${DEVICE_NAME_FILTER}" != "" ]]; then
-    fx-error "Cannot specify both --device-name and --device-ip"
-    exit 3
   fi
 fi
 
--- scripts/sdk/gn/bash_tests/fssh-test.sh
+++ scripts/sdk/gn/bash_tests/fssh-test.sh
@@ -25,6 +25,11 @@ BT_SET_UP() {
   FUCHSIA_WORK_DIR="${HOME}/.fuchsia"
 
   MOCKED_DEVICE_FINDER="${BT_TEMP_DIR}/scripts/sdk/gn/base/$(gn-test-tools-subdir)/device-finder"
+  FCONFIG_CMD="${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fconfig.sh"
+
+  # Add the ssh mock to the path so fssh uses it vs. the real ssh.
+  SSH_MOCK_PATH="${BT_TEMP_DIR}/isolated_path_for"
+  export PATH="${SSH_MOCK_PATH}:${PATH}"
 }
 
 # Sets up a device-finder mock. The implemented mock aims to produce minimal
@@ -54,14 +59,22 @@ SETVAR
 TEST_fssh_help() {
   BT_EXPECT_FAIL  "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh" --help > "${BT_TEMP_DIR}/usage.txt"
 
-readonly EXPECTED_HELP="Unknown option --help
-Usage: ${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh
+readonly EXPECTED_HELP="Usage: fssh.sh [args]
+  [--help]
+    This message.
   [--device-name <device hostname>]
-    Connects to a device by looking up the given device hostname.
-  [--device-ip <device ipaddr>]
-    Connects to a device by using the provided IP address, cannot use with --device-name
+    Connects to the device with the given device hostname. Cannot be used with --device-ip.
+    Defaults to the value from \`fconfig.sh get device-name\`.
+  [--device-ip <device ip>]
+    Connects to the device with the given device ip address. Cannot be used with --device-name.
+    Defaults to the value from \`fconfig.sh get device-ip\`.
+    Note: If defaults are configured for both device-name and device-ip, then device-ip is used.
+          If the device is specified at all, then the first device discovered is used.
   [--private-key <identity file>]
-    Uses additional private key when using ssh to access the device."
+    Uses additional private key when using ssh to access the device.
+
+All positional arguments are passed through to SSH to be executed on the device."
+
   BT_EXPECT_FILE_CONTAINS "${BT_TEMP_DIR}/usage.txt" "${EXPECTED_HELP}"
 }
 
@@ -69,15 +82,12 @@ Usage: ${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh
 TEST_fssh() {
   set_up_device_finder
 
-  # Add the ssh mock to the path so fssh uses it vs. the real ssh.
-  export PATH="${BT_TEMP_DIR}/isolated_path_for:${PATH}"
-
   # Run command.
-  BT_EXPECT gn-test-run-bash-script "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh"
+  BT_EXPECT "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh"
 
   # Verify that ssh was run correctly.
   # shellcheck disable=SC1090
-  source "${BT_TEMP_DIR}/isolated_path_for/ssh.mock_state"
+  source "${SSH_MOCK_PATH}/ssh.mock_state"
 
   gn-test-check-mock-args _ANY_ -F "${FUCHSIA_WORK_DIR}/sshconfig" fe80::c0ff:eec0:ffee%coffee
 
@@ -87,15 +97,12 @@ TEST_fssh() {
 TEST_fssh_by_ip() {
    set_up_device_finder
 
-  # Add the ssh mock to the path so fssh uses it vs. the real ssh.
-  export PATH="${BT_TEMP_DIR}/isolated_path_for:${PATH}"
-
   # Run command.
-  BT_EXPECT gn-test-run-bash-script "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh" --device-ip fe80::d098:513f:9cfb:eb53%hardcoded
+  BT_EXPECT "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh" --device-ip fe80::d098:513f:9cfb:eb53%hardcoded
 
   # Verify that ssh was run correctly.
   # shellcheck disable=SC1090
-  source "${BT_TEMP_DIR}/isolated_path_for/ssh.mock_state"
+  source "${SSH_MOCK_PATH}/ssh.mock_state"
 
   gn-test-check-mock-args _ANY_ -F "${FUCHSIA_WORK_DIR}/sshconfig" fe80::d098:513f:9cfb:eb53%hardcoded
 }
@@ -103,15 +110,12 @@ TEST_fssh_by_ip() {
 TEST_fssh_by_name() {
    set_up_device_finder
 
-  # Add the ssh mock to the path so fssh uses it vs. the real ssh.
-  export PATH="${BT_TEMP_DIR}/isolated_path_for:${PATH}"
-
   # Run command.
-  BT_EXPECT gn-test-run-bash-script "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh" --device-name coffee-coffee-coffee-coffee
+  BT_EXPECT "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh" --device-name coffee-coffee-coffee-coffee
 
   # Verify that ssh was run correctly.
   # shellcheck disable=SC1090
-  source "${BT_TEMP_DIR}/isolated_path_for/ssh.mock_state"
+  source "${SSH_MOCK_PATH}/ssh.mock_state"
 
   gn-test-check-mock-args _ANY_ -F "${FUCHSIA_WORK_DIR}/sshconfig" fe80::c0ff:eec0:ffee%coffee
 }
@@ -120,16 +124,56 @@ TEST_fssh_name_not_found() {
   echo 2 > "${MOCKED_DEVICE_FINDER}.mock_status"
   echo "2020/02/25 07:42:59 no devices with domain matching 'name-not-found'" >  "${MOCKED_DEVICE_FINDER}.stderr"
 
-  # Add the ssh mock to the path so fssh uses it vs. the real ssh.
-  export PATH="${BT_TEMP_DIR}/isolated_path_for:${PATH}"
-
   # Run command.
   BT_EXPECT_FAIL  "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh" --device-name name-not-found
 }
 
+
+TEST_fssh_with_ip_prop() {
+  set_up_device_finder
+
+  BT_EXPECT "${FCONFIG_CMD}" set device-ip "192.1.1.2"
+
+  BT_EXPECT "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh"
+
+  # shellcheck disable=SC1090
+  source "${SSH_MOCK_PATH}/ssh.mock_state"
+  expected_args=("${SSH_MOCK_PATH}/ssh" -F "${FUCHSIA_WORK_DIR}/sshconfig" 192.1.1.2)
+  gn-test-check-mock-args "${expected_args[@]}"
+}
+
+TEST_fssh_with_name_prop() {
+  set_up_device_finder
+
+  BT_EXPECT "${FCONFIG_CMD}" set device-name "coffee-coffee-coffee-coffee"
+
+  BT_EXPECT "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh"
+
+  # shellcheck disable=SC1090
+  source "${SSH_MOCK_PATH}/ssh.mock_state"
+  expected_args=("${SSH_MOCK_PATH}/ssh" -F "${FUCHSIA_WORK_DIR}/sshconfig" "fe80::c0ff:eec0:ffee%coffee")
+  gn-test-check-mock-args "${expected_args[@]}"
+}
+
+TEST_fssh_with_all_props() {
+  set_up_device_finder
+
+  BT_EXPECT "${FCONFIG_CMD}" set device-ip "192.1.1.2"
+  BT_EXPECT "${FCONFIG_CMD}" set device-name "coffee-coffee-coffee-coffee"
+
+  BT_EXPECT "${BT_TEMP_DIR}/scripts/sdk/gn/base/bin/fssh.sh"
+
+  # shellcheck disable=SC1090
+  source "${SSH_MOCK_PATH}/ssh.mock_state"
+  expected_args=("${SSH_MOCK_PATH}/ssh" -F "${FUCHSIA_WORK_DIR}/sshconfig" 192.1.1.2)
+  gn-test-check-mock-args "${expected_args[@]}"
+}
+
+
 # Test initialization.
 # shellcheck disable=SC2034
 BT_FILE_DEPS=(
+  scripts/sdk/gn/base/bin/fconfig.sh
   scripts/sdk/gn/base/bin/fssh.sh
   scripts/sdk/gn/base/bin/fuchsia-common.sh
   scripts/sdk/gn/bash_tests/gn-bash-test-lib.sh
