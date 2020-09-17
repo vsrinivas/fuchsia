1d6f41fd00bebe5f37ca7e907f225640dfb1e72b
Simon Shields <simonshields@google.com>
2020-09-17 06:13:47 +0000

    [usb-update-checker] Move all tests to integration tests.
    This simplifies unit tests in the usb-update-checker, and lets us test
    other things like that the monitor works.
    
    Bug: 59172
    Fixed: 59376
    Change-Id: I2ff236ab9cb4ea6f23ec753f4d5e707a34ebb5cf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/426814
    Commit-Queue: Simon Shields <simonshields@google.com>
    Reviewed-by: Ben Keller <galbanum@google.com>
    Testability-Review: Ben Keller <galbanum@google.com>

--- src/sys/pkg/tests/usb-system-update-checker/src/lib.rs
+++ src/sys/pkg/tests/usb-system-update-checker/src/lib.rs
@@ -5,13 +5,16 @@
 use {
     fidl_fuchsia_pkg::{PackageResolverMarker, PackageUrl},
     fidl_fuchsia_update_installer::{InstallerRequestStream, State, *},
-    fidl_fuchsia_update_usb::{CheckSuccess, CheckerMarker, CheckerProxy},
+    fidl_fuchsia_update_usb::{
+        CheckError, CheckSuccess, CheckerMarker, CheckerProxy, MonitorMarker, MonitorRequest,
+    },
     fuchsia_async::{self as fasync, Task},
     fuchsia_component::{
         client::{App, AppBuilder},
         server::{NestedEnvironment, ServiceFs},
     },
     fuchsia_pkg_testing::{PackageBuilder, RepositoryBuilder},
+    fuchsia_url::pkg_url::PkgUrl,
     futures::{lock::Mutex, prelude::*},
     isolated_swd::{
         resolver::for_tests::ResolverForTest, updater::for_tests::generate_packages_json,
@@ -31,7 +34,7 @@ const UPDATE_PACKAGE_URL: &str = "fuchsia-pkg://fuchsia.com/update";
 
 struct TestEnvBuilder {
     update_version: SystemVersion,
-    system_version: SystemVersion,
+    system_version: Option<SystemVersion>,
     states: Vec<State>,
 }
 
@@ -39,7 +42,7 @@ impl TestEnvBuilder {
     pub fn new() -> Self {
         TestEnvBuilder {
             update_version: SystemVersion::Opaque("".to_string()),
-            system_version: SystemVersion::Opaque("".to_string()),
+            system_version: None,
             states: vec![],
         }
     }
@@ -52,7 +55,7 @@ impl TestEnvBuilder {
 
     /// Set the value of /config/build-info/version.
     pub fn system_version(mut self, version: SystemVersion) -> Self {
-        self.system_version = version;
+        self.system_version = Some(version);
         self
     }
 
@@ -149,13 +152,15 @@ struct TestEnv {
     checker: App,
     _env: NestedEnvironment,
     _svcfs_task: Task<()>,
+    monitor_task: Task<bool>,
+    monitor_client: Option<fidl::endpoints::ClientEnd<MonitorMarker>>,
     _installer: Arc<MockInstaller>,
 }
 
 impl TestEnv {
     pub async fn new(
         resolver: ResolverForTest,
-        system_version: SystemVersion,
+        system_version: Option<SystemVersion>,
         installer_states: Vec<State>,
     ) -> Self {
         let installer = MockInstaller::new(installer_states);
@@ -171,11 +176,13 @@ impl TestEnv {
 
         let build_info_dir = tempfile::tempdir().expect("Creating temporary directory succeeds");
 
-        std::fs::write(
-            format!("{}/version", build_info_dir.path().to_string_lossy()),
-            system_version.to_string(),
-        )
-        .expect("Writing system version succeeds");
+        if let Some(version) = system_version {
+            std::fs::write(
+                format!("{}/version", build_info_dir.path().to_string_lossy()),
+                version.to_string(),
+            )
+            .expect("Writing system version succeeds");
+        }
 
         let env = svcfs
             .create_salted_nested_environment("usb-update-checker-test")
@@ -191,11 +198,27 @@ impl TestEnv {
         let svcfs_task = fasync::Task::spawn(svcfs.collect());
         let checker = checker.spawn(env.launcher()).expect("Launching update checker succeeds");
 
+        let (monitor_client, mut stream) =
+            fidl::endpoints::create_request_stream::<MonitorMarker>()
+                .expect("Creating monitor endpoints succeeds");
+        let monitor_task = fasync::Task::spawn(async move {
+            while let Some(request) = stream.try_next().await.expect("try_next succeeds") {
+                match request {
+                    MonitorRequest::OnUpdateStarted { .. } => {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        });
+
         TestEnv {
             _resolver: resolver,
             checker,
             _env: env,
             _svcfs_task: svcfs_task,
+            monitor_task,
+            monitor_client: Some(monitor_client),
             _installer: installer,
         }
     }
@@ -203,11 +226,23 @@ impl TestEnv {
     pub fn connect(&self) -> CheckerProxy {
         self.checker.connect_to_service::<CheckerMarker>().expect("Connect to checker succeeds")
     }
+
+    /// Get the monitor client end that can be passed to fuchsia.update.usb/Check.
+    /// Should only be called once per TestEnv.
+    pub fn get_monitor_client(&mut self) -> fidl::endpoints::ClientEnd<MonitorMarker> {
+        self.monitor_client.take().expect("get_monitor_client is only called once")
+    }
+
+    /// Returns true if the monitor received an OnUpdateStarted event.
+    /// Blocks until the monitor either receives an event or is closed.
+    pub async fn was_update_started(&mut self) -> bool {
+        (&mut self.monitor_task).await
+    }
 }
 
 #[fasync::run_singlethreaded(test)]
 async fn test_install_update_succeeds() {
-    let env = TestEnvBuilder::new()
+    let mut env = TestEnvBuilder::new()
         .update_version(SystemVersion::Semantic(SemanticVersion::from([1])))
         .system_version(SystemVersion::Semantic(SemanticVersion::from([0, 1])))
         .state(State::Prepare(PrepareData::empty()))
@@ -220,9 +255,179 @@ async fn test_install_update_succeeds() {
 
     let proxy = env.connect();
     let result = proxy
+        .check(
+            &mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() },
+            None,
+            Some(env.get_monitor_client()),
+        )
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Ok(CheckSuccess::UpdatePerformed));
+    assert!(env.was_update_started().await);
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_invalid_url_fails() {
+    let env = TestEnvBuilder::new().build().await;
+
+    let proxy = env.connect();
+    let result = proxy
+        .check(&mut PackageUrl { url: "wow:// this isn't even a URL!!".to_string() }, None, None)
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Err(CheckError::InvalidUpdateUrl));
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_pinned_url_fails() {
+    let env = TestEnvBuilder::new().build().await;
+
+    let proxy = env.connect();
+    // A valid hash is 64 bytes.
+    let hash = "d00dfeed".repeat(8);
+    let evil_url = PkgUrl::parse(&format!("fuchsia-pkg://fuchsia.com/update?hash={}", hash))
+        .expect("Parsing URL succeeds");
+    let result = proxy
+        .check(&mut PackageUrl { url: evil_url.to_string() }, None, None)
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Err(CheckError::InvalidUpdateUrl));
+}
+
+fn semantic_version(a: u32, b: u32, c: u32, d: u32) -> SystemVersion {
+    SystemVersion::Semantic(SemanticVersion::from([a, b, c, d]))
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_version_comparison_update_older_ignored() {
+    let mut env = TestEnvBuilder::new()
+        .system_version(semantic_version(0, 2020_01_01, 2, 4))
+        .update_version(semantic_version(0, 2019_01_01, 1, 1))
+        .build()
+        .await;
+
+    let result = env
+        .connect()
+        .check(
+            &mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() },
+            None,
+            Some(env.get_monitor_client()),
+        )
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Ok(CheckSuccess::UpdateNotNeeded));
+    assert_eq!(env.was_update_started().await, false);
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_version_comparison_update_newer_installed() {
+    let env = TestEnvBuilder::new()
+        .system_version(semantic_version(0, 2020_01_01, 2, 4))
+        .update_version(semantic_version(0, 2021_01_01, 1, 1))
+        .state(State::Prepare(PrepareData::empty()))
+        .state(State::Fetch(FetchData::empty()))
+        .state(State::Stage(StageData::empty()))
+        .state(State::WaitToReboot(WaitToRebootData::empty()))
+        .state(State::DeferReboot(DeferRebootData::empty()))
+        .build()
+        .await;
+
+    let result = env
+        .connect()
         .check(&mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() }, None, None)
         .await
         .expect("Send request OK");
 
     assert_eq!(result, Ok(CheckSuccess::UpdatePerformed));
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_version_comparison_update_same_ignored() {
+    let version = semantic_version(0, 2020_01_01, 2, 4);
+    let env =
+        TestEnvBuilder::new().system_version(version.clone()).update_version(version).build().await;
+
+    let result = env
+        .connect()
+        .check(&mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() }, None, None)
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Ok(CheckSuccess::UpdateNotNeeded));
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_version_comparison_opaque_system_version() {
+    let env = TestEnvBuilder::new()
+        .system_version(SystemVersion::Opaque("2020-08-13T10:27:00+00:00".to_string()))
+        .update_version(semantic_version(0, 2020_01_01, 2, 4))
+        .build()
+        .await;
+
+    let result = env
+        .connect()
+        .check(&mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() }, None, None)
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Err(CheckError::UpdateFailed));
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_version_comparison_opaque_update_version() {
+    let env = TestEnvBuilder::new()
+        .system_version(semantic_version(0, 2020_01_01, 2, 4))
+        .update_version(SystemVersion::Opaque("2020-08-13T10:27:00+00:00".to_string()))
+        .build()
+        .await;
+
+    let result = env
+        .connect()
+        .check(&mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() }, None, None)
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Err(CheckError::UpdateFailed));
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_version_comparison_no_system_version() {
+    let env =
+        TestEnvBuilder::new().update_version(semantic_version(0, 2021_01_01, 1, 1)).build().await;
+
+    let result = env
+        .connect()
+        .check(&mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() }, None, None)
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Err(CheckError::UpdateFailed));
+}
+
+#[fasync::run_singlethreaded(test)]
+async fn test_install_update_fails() {
+    let mut env = TestEnvBuilder::new()
+        .system_version(semantic_version(0, 1970_01_01, 0, 0))
+        .update_version(semantic_version(0, 2020_01_01, 1, 1))
+        .state(State::Prepare(PrepareData::empty()))
+        .state(State::FailPrepare(FailPrepareData::empty()))
+        .build()
+        .await;
+
+    let result = env
+        .connect()
+        .check(
+            &mut PackageUrl { url: UPDATE_PACKAGE_URL.to_string() },
+            None,
+            Some(env.get_monitor_client()),
+        )
+        .await
+        .expect("Send request OK");
+
+    assert_eq!(result, Err(CheckError::UpdateFailed));
+    assert!(env.was_update_started().await);
 }
