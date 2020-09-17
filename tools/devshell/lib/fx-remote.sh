7377ba6739278af7fa22c65f594fc0b20e51457d
Joshua Seaton <joshuaseaton@google.com>
2020-09-17 21:49:06 +0000

    [fx] Use get-build-artifacts for pave-remote; extend to `netboot` and `pave-remote`
    Right now, we rely on build-archive.tar having everything needed to
    pave; that gets rsync'ed and unpacked. This change updates the command
    to instead use fx get-build-artifacts (which uses `jq` on images.json)
    to find precisely the set of images needed.
    
    This change is beneficial for four reasons:
    (1) it is more efficient, only building and rsyncing precisely what is
    needed - and avoiding unpacking
    (2) it readily extends to the similar use-cases of remote netbooting and
    running a ZBI test (see follow-up CLs)
    (3) it removes a dependency on the build archive (which is in the
    process of being deleted/deprecated).
    (4) in the cases where (host OS, host arch) == (remote OS, remote arch),
    we now no longer need to build bootserver locally.
    
    We introduce `fx netboot-remote` and `fx pave-zedboot-remote`
    along the same lines.
    
    Test: tested locally on my glinux laptop - and enlisted someone with a
    mac to test locally as well.
    
    Fixed: 58096
    Change-Id: I41f7347c320be55c807455300acfb2bb26e2e20c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/416964
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Renato Mangini Dias <mangini@google.com>
    Reviewed-by: Aaron Knobloch <aknobloch@google.com>
    Reviewed-by: James Tucker <raggi@google.com>
    Testability-Review: Renato Mangini Dias <mangini@google.com>
    Testability-Review: James Tucker <raggi@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>

--- tools/devshell/lib/fx-remote.sh
+++ tools/devshell/lib/fx-remote.sh
@@ -56,3 +56,51 @@ function fetch_remote_build_archive {
   fi
   echo >&2 "Build archive expanded into out/fetched"
 }
+
+function get_remote_build_dir {
+  local host=$1
+  local remote_checkout=$2
+  echo $(ssh "${host}" "cd ${remote_checkout} && .jiri_root/bin/fx get-build-dir")
+}
+
+function fetch_remote_build_artifacts {
+  local host="$1"
+  local remote_checkout="$2"
+  local local_dir="$3"
+  local mode="$4"  # See fx get-build-artifacts for possible values.
+  local build="$5"
+
+  local artifacts=($(ssh "${host}" "cd ${remote_checkout} && .jiri_root/bin/fx get-build-artifacts --no-build ${mode}")) || exit $?
+
+  if $build; then
+    ssh "${host}" "cd ${remote_checkout} && .jiri_root/bin/fx build ${artifacts[@]}"
+  fi
+
+  local remote_build_dir="$(get_remote_build_dir "${host}" "${remote_checkout}")" || exit $?
+  local joined=$(printf "%s," "${artifacts[@]}")
+  mkdir -p "${local_dir}"
+  # --relative ensures that the artifacts are copied to out/fetched relative to
+  # the '/./' (i.e., the build directory).
+  rsync --compress --partial --progress --relative "${host}:${remote_build_dir}/./{${joined}}" "${local_dir}" || exit $?
+}
+
+# Attempts to fetch a remote tool able to run on the host platform, and
+# default to building one locally if unable to find one.
+function fetch_or_build_tool {
+  local host="$1"
+  local remote_checkout="$2"
+  local local_dir="$3"
+  local tool_name="$4"
+
+  local tool="$(ssh "${host}" "cd ${remote_checkout} && .jiri_root/bin/fx get-build-artifacts --allow-empty --os ${HOST_OS} --cpu ${HOST_CPU}" --name ${tool_name} tools)"
+  if [[ -n "${tool}" ]] ; then
+    local remote_build_dir="$(get_remote_build_dir "${host}" "${remote_checkout}")" || exit $?
+    rsync --compress --partial --progress --relative "${host}:${remote_build_dir}/./${tool}" "${local_dir}" >&2 || exit $?
+  else
+    tool="$(fx-command-run get-build-artifacts tools --expect-one --name ${tool_name})" || exit $?
+    rm -f "${local_dir}/${tool}"
+    mkdir -p "${local_dir}/$(dirname "$tool")"
+    cp "${FUCHSIA_BUILD_DIR}/${tool}" "${local_dir}/${tool}"
+  fi
+  echo "${tool}"
+}
