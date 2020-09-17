8ca55271285a89e6df0e1692be4d06a29ec03306
omer <omerlevran@google.com>
2020-09-17 12:37:06 +0000

    [licenses] - adding missing license for generate-intellij-config.py
    Change-Id: I024977e8190f26685e26ce9eaac23f714cab0993
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/427922
    Reviewed-by: Adam Barth <abarth@google.com>
    Testability-Review: Adam Barth <abarth@google.com>
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>

--- scripts/generate-intellij-config.py
+++ scripts/generate-intellij-config.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2.7
 
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 from lxml import etree
 import os
 import re
