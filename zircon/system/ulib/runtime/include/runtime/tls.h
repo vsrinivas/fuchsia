289a1d747dbc5b550436c3bbfad7286c227f6669
Roland McGrath <mcgrathr@google.com>
2020-09-20 01:55:33 +0000

    [libc] Force inlining of <runtime/tls.h> functions
    Force the inlining that was always expected to happen.  This
    increases code size slightly.  However, with the new Scudo
    changes that use libc's internal inlines based on these more
    frequently for its TLS slot, it actually reduces code size
    slightly on ARM (while still increasing it very slightly on x86).
    
    Bug: 60123
    Change-Id: I2ea7f35756f081582e587345fbd92494cc811f48
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/428523
    Reviewed-by: Petr Hosek <phosek@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>

--- zircon/system/ulib/runtime/include/runtime/tls.h
+++ zircon/system/ulib/runtime/include/runtime/tls.h
@@ -22,9 +22,16 @@ static inline void zxr_tp_set(zx_handle_t self, void* tp);
 #define ZXR_NO_SANITIZERS
 #endif
 
+// These are tiny functions meant to be inlined, where a call would often
+// actually take more instruction bytes than just inlining it.  However,
+// Clang avoids inlining across functions with differing attributes and the
+// necessary `no_sanitize` attribute trips that logic.  So tell the
+// compiler to force inlining here.
+#define ZXR_TLS_INLINE ZXR_NO_SANITIZERS __attribute__((always_inline))
+
 #if defined(__aarch64__)
 
-ZXR_NO_SANITIZERS static inline void* zxr_tp_get(void) {
+ZXR_TLS_INLINE static inline void* zxr_tp_get(void) {
   // This just emits "mrs %[reg], tpidr_el0", but the compiler
   // knows what exactly it's doing (unlike an asm).  So it can
   // e.g. CSE it with another implicit thread-pointer fetch it
@@ -32,13 +39,13 @@ ZXR_NO_SANITIZERS static inline void* zxr_tp_get(void) {
   return __builtin_thread_pointer();
 }
 
-ZXR_NO_SANITIZERS static inline void zxr_tp_set(zx_handle_t self, void* tp) {
+ZXR_TLS_INLINE static inline void zxr_tp_set(zx_handle_t self, void* tp) {
   __asm__ volatile("msr tpidr_el0, %0" : : "r"(tp));
 }
 
 #elif defined(__x86_64__)
 
-ZXR_NO_SANITIZERS static inline void* zxr_tp_get(void) {
+ZXR_TLS_INLINE static inline void* zxr_tp_get(void) {
 // This fetches %fs:0, but the compiler knows what it's doing.
 // LLVM knows that in the Fuchsia ABI %fs:0 always stores the
 // %fs.base address, and its optimizer will see through this
@@ -65,7 +72,7 @@ ZXR_NO_SANITIZERS static inline void* zxr_tp_get(void) {
 #endif
 }
 
-ZXR_NO_SANITIZERS static inline void zxr_tp_set(zx_handle_t self, void* tp) {
+ZXR_TLS_INLINE static inline void zxr_tp_set(zx_handle_t self, void* tp) {
   zx_status_t status =
       _zx_object_set_property(self, ZX_PROP_REGISTER_FS, (uintptr_t*)&tp, sizeof(uintptr_t));
   if (status != ZX_OK)
