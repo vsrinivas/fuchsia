09eded6213e6041f4d32affc846506635f5adfd8
Alex Legg <alexlegg@google.com>
2020-09-23 02:34:43 +0000

    [fdf] Migrate imx227 driver to bindc
    Change-Id: I5df6b647687f5c6565fbecfa9d8fd207b8d44d19
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/352141
    Commit-Queue: Alex Legg <alexlegg@google.com>
    Reviewed-by: John Sasinowski <jsasinowski@google.com>
    Testability-Review: Alex Legg <alexlegg@google.com>

--- src/camera/drivers/sensors/imx227/imx227.cc
+++ src/camera/drivers/sensors/imx227/imx227.cc
@@ -22,6 +22,7 @@
 #include <hw/reg.h>
 
 #include "src/camera/drivers/sensors/imx227/constants.h"
+#include "src/camera/drivers/sensors/imx227/imx227-bind.h"
 #include "src/camera/drivers/sensors/imx227/imx227_modes.h"
 #include "src/camera/drivers/sensors/imx227/imx227_seq.h"
 #include "src/camera/drivers/sensors/imx227/mipi_ccs_regs.h"
@@ -502,9 +503,4 @@ static constexpr zx_driver_ops_t driver_ops = []() {
 }  // namespace camera
 
 // clang-format off
-ZIRCON_DRIVER_BEGIN(imx227, camera::driver_ops, "imx227", "0.1", 4)
-    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_COMPOSITE),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_SONY),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_PID, PDEV_PID_SONY_IMX227),
-    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, PDEV_DID_CAMERA_SENSOR),
-ZIRCON_DRIVER_END(imx227)
+ZIRCON_DRIVER(imx227, camera::driver_ops, "imx227", "0.1");
