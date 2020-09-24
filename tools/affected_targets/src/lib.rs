278eed75e5710524ab70e346a8b941c16f1c2bc8
Ina Huh <ihuh@google.com>
2020-09-24 19:05:09 +0000

    [tools][affected_targets] Remove tool since it is no longer used.
    It has been rewritten into a python script in the recipes code:
    https://fuchsia.googlesource.com/infra/recipes/+/refs/heads/master/recipe_modules/build/resources/should_build.py
    
    Bug: 57120
    Change-Id: I87445325d9026bab4dba23b52ccd8aeade19b782
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/429517
    Commit-Queue: Ina Huh <ihuh@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Testability-Review: Kevin Lindkvist <lindkvist@google.com>

--- tools/affected_targets/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-pub mod analysis;
-pub mod argument_parsing;
-pub mod files;
-pub mod gn;
