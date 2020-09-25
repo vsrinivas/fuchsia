613013d6a1c986b0d7e33b82e5ddfb569a8e23ea
Oliver Newman <olivernewman@google.com>
2020-09-25 18:55:09 +0000

    [fint] Add Context.proto
    This proto will be used to pass dynamic/context-dependent information
    from recipes to fint.
    
    Design: https://docs.google.com/document/d/1y9yQ3-mjxAgM43gBhjT1PQZsdLKRzRp-xVR8e-KC3zQ/edit#
    
    I also added a couple fields to static.proto to keep it up-to-date with
    infra's fuchsia.proto, to which the corresponding fields were added in
    http://fxr/430019.
    
    Bug: 59040
    Change-Id: I5135d84d6565a2b607acb214adee30b0a07b6590
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/430815
    Reviewed-by: Gary Miguel <garymm@google.com>
    Testability-Review: Oliver Newman <olivernewman@google.com>
    Commit-Queue: Oliver Newman <olivernewman@google.com>

--- tools/integration/fint/proto/gen.go
+++ tools/integration/fint/proto/gen.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//go:generate protoc -I . --go_out=. static.proto
+//go:generate protoc -I . --go_out=. context.proto static.proto
 
 package proto
