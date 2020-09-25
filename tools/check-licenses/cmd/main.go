a91c0b2674a39737992ed151eba312f7a5bc07e5
Jerry Belton <jcecil@google.com>
2020-09-25 20:53:26 +0000

    [licenses] Adding several flags to make check-licenses more configurable from the command-line
    Change-Id: If58e9524090f447ce8b0cea5a25caf9c254ba842
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/430174
    Commit-Queue: Jerry Belton <jcecil@google.com>
    Testability-Review: Francois Rousseau <frousseau@google.com>
    Reviewed-by: Omer Lev-ran <omerlevran@google.com>

--- tools/check-licenses/cmd/main.go
+++ tools/check-licenses/cmd/main.go
@@ -8,32 +8,72 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 
 	checklicenses "go.fuchsia.dev/fuchsia/tools/check-licenses"
 )
 
-var (
-	config checklicenses.Config
+var config checklicenses.Config
 
+var (
 	configFile = flag.String("config_file", "tools/check-licenses/config/config.json", "Location of config.json.")
-	target     = flag.String("target", "Options: {all, <target>}", "Analyze the dependency tree of a specific GN build target.")
+
+	skipDirs          = flag.String("skip_dirs", "", "Comma separated list of directory names to skip when traversing a directory tree. This arg is added to the list of skipdirs in the config file.")
+	skipFiles         = flag.String("skip_files", "", "Comma separated list of file names to skip when traversing a directory tree. This arg is added to the list of skipfiles in the config file.")
+	licensePatternDir = flag.String("license_pattern_dir", "", "Location of directory containing pattern files for all licenses that may exist in the given code base.")
+	baseDir           = flag.String("base_dir", "", "Root location to begin directory traversal.")
+	target            = flag.String("target", "", "Analyze the dependency tree of a specific GN build target.")
+	logLevel          = flag.Int("log_level", 0, "Log level, see https://godoc.org/github.com/golang/glog for more info.")
 )
 
 func validateArgs() {
-	_, err := os.Stat(*configFile)
-	if os.IsNotExist(err) {
+	if _, err := os.Stat(*configFile); os.IsNotExist(err) {
 		log.Fatalf("Config file \"%v\" does not exist!", *configFile)
 	}
-
-	if *target != "Options: {all, <target>}" {
-		log.Printf("WARNING: Flag \"target\" was set to \"%v\", but that flag is currently unsupported. check-licenses will parse the full directory tree.\n", *target)
-		*target = "Options: {all, <target>}"
-	}
-	config.Target = *target
-
 	if err := config.Init(configFile); err != nil {
 		log.Fatalf("Failed to initialize config: %v", err)
 	}
+
+	if *skipDirs != "" {
+		split := strings.Split(*skipDirs, ",")
+		for _, s := range split {
+			if s != "" {
+				config.SkipDirs = append(config.SkipDirs, s)
+			}
+		}
+	}
+
+	if *skipFiles != "" {
+		split := strings.Split(*skipFiles, ",")
+		for _, s := range split {
+			if s != "" {
+				config.SkipFiles = append(config.SkipFiles, s)
+			}
+		}
+	}
+
+	if *licensePatternDir != "" {
+		if info, err := os.Stat(*licensePatternDir); os.IsNotExist(err) && info.IsDir() {
+			log.Fatalf("License pattern directory path \"%v\" does not exist!", *licensePatternDir)
+		}
+		config.LicensePatternDir = *licensePatternDir
+	}
+
+	if *baseDir != "" {
+		if info, err := os.Stat(*baseDir); os.IsNotExist(err) && info.IsDir() {
+			log.Fatalf("Base directory path \"%v\" does not exist!", *baseDir)
+		}
+		config.BaseDir = *baseDir
+	}
+
+	if *target != "" {
+		log.Printf("WARNING: Flag \"target\" was set to \"%v\", but that flag is currently unsupported. check-licenses will parse the full directory tree.\n", *target)
+		//TODO: enable target-based dependency traversal
+		//config.Target = *target
+	}
+
+	//TODO: incorporate https://godoc.org/github.com/golang/glog
+	//and configure it using the logLevel argument
 }
 
 func main() {
