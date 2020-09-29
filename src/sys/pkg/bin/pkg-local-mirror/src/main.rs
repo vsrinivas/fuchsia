3327e2b374e13f242a12e58a59d0598c1c9abcb4
Simon Shields <simonshields@google.com>
2020-09-29 13:58:33 +1000

    [HACK] get pkg-local-mirror working
    Change-Id: Ifda8cd286a5a2ce0150af3c6e1b3f52d0020a573

--- src/sys/pkg/bin/pkg-local-mirror/src/main.rs
+++ src/sys/pkg/bin/pkg-local-mirror/src/main.rs
@@ -3,31 +3,43 @@
 // found in the LICENSE file.
 
 use {
-    anyhow::{anyhow, Context as _},
+    anyhow::{anyhow, Context as _, Error},
+    fidl_fuchsia_fs::{AdminMarker, AdminProxy},
+    fidl_fuchsia_io::{DirectoryMarker, OPEN_RIGHT_READABLE},
     fidl_fuchsia_pkg::LocalMirrorRequestStream,
     fuchsia_async as fasync,
     fuchsia_component::server::ServiceFs,
     fuchsia_syslog::{fx_log_info, fx_log_warn},
-    futures::{future::TryFutureExt as _, StreamExt as _},
+    futures::StreamExt as _,
     pkg_local_mirror::PkgLocalMirror,
+    std::path::Path,
 };
 
-const USB_DIR_PATH: &str = "/usb/0/fuchsia_pkg";
+const USB_DIR_PATH: &str = "fuchsia_pkg";
+
+async fn make_local_mirror(
+    admin: AdminProxy,
+    stream: LocalMirrorRequestStream,
+) -> Result<(), Error> {
+    let (proxy, remote) =
+        fidl::endpoints::create_proxy::<DirectoryMarker>().context("creating root proxy")?;
+    admin.get_root(remote).context("Getting root")?;
+    let dir_proxy = io_util::open_directory(&proxy, Path::new(USB_DIR_PATH), OPEN_RIGHT_READABLE)
+        .context("opening fuchsia_pkg dir")?;
+    let pkg_local_mirror =
+        PkgLocalMirror::new(&dir_proxy).await.context("creating local mirror")?;
+    pkg_local_mirror.handle_request_stream(stream).await
+}
 
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), anyhow::Error> {
     fuchsia_syslog::init_with_tags(&["pkg-local-mirror"]).expect("can't init logger");
     fx_log_info!("starting pkg-local-mirror");
 
-    // TODO(fxbug.dev/59830): Get handle to USB directory using fuchsia.fs/Admin.GetRoot.
-    let pkg_local_mirror = {
-        let usb_dir = io_util::directory::open_in_namespace(
-            USB_DIR_PATH,
-            fidl_fuchsia_io::OPEN_RIGHT_READABLE,
-        )
-        .with_context(|| format!("while opening usb dir: {}", USB_DIR_PATH))?;
-        PkgLocalMirror::new(&usb_dir).await.context("creating PkgLocalMirror")?
-    };
+    let fat_proxy = fuchsia_component::client::connect_to_service_at_path::<AdminMarker>(
+        "/svc/fuchsia.fat.Admin",
+    )
+    .context("connecting to fatfs")?;
 
     let mut fs = ServiceFs::new_local();
     let _ = fs.take_and_serve_directory_handle().context("serving directory handle")?;
@@ -37,12 +49,15 @@ async fn main() -> Result<(), anyhow::Error> {
     let () = fs
         .for_each_concurrent(None, |incoming_service| match incoming_service {
             IncomingService::LocalMirror(stream) => {
-                pkg_local_mirror.handle_request_stream(stream).unwrap_or_else(|e| {
-                    fx_log_warn!(
-                        "error handling fuchsia.pkg/LocalMirror request stream {:#}",
-                        anyhow!(e)
-                    )
-                })
+                let clone = fat_proxy.clone();
+                async move {
+                    make_local_mirror(clone, stream).await.unwrap_or_else(|e| {
+                        fx_log_warn!(
+                            "error handling fuchsia.pkg/LocalMirror request stream {:#}",
+                            anyhow!(e)
+                        )
+                    })
+                }
             }
         })
         .await;
