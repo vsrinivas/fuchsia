9a37f7d348df08632e5ead65e733ff0c59c56565
David 'Digit' Turner <digit@google.com>
2020-09-29 21:24:14 +0000

    [build][alln] Migrate arm64 boot shims to the Fuchsia build.
    This CL ensures that the arm64 boot shims can be built with the
    Fuchsia build as well, and comparing them with the Zircon-built
    binaries, to verify that they are byte-identical.
    
    NOTE: Because GN was complaining about invalid includes, the
          visalia-uart.c and cleo-uart.c are replaced by simple
          symlinks to the C source file they included.
    
    A future CL will move their build entirely to the Fuchsia build
    and remove the corresponding Zircon build files.
    
    For testing. comparing the md5sum of the generated binaries
    is enough:
    
      $ (cd out/default/physmem_arm64/ && md5sum *-boot-shim.bin)
      1469d858f38359c1f4dddb0e38472166  as370-boot-shim.bin
      bf7e99eb62a062a5a8a2a60c5ccbbae6  c18-boot-shim.bin
      f7131e6bf0e7388897f1ac7fe6ded286  cleo-boot-shim.bin
      40fcb6e149c0b00c2d27b2b7d48d4bca  msm8998-boot-shim.bin
      7c3d6bfa27a81983064048258fa72dad  msm8x53-som-boot-shim.bin
      c47f5497eceece696da4a86866ea4dec  mt8167s_ref-boot-shim.bin
      ae1e047439e24290172c48889e5bbd2a  qemu-boot-shim.bin
      81835432c25455eb535812217e68af4a  visalia-boot-shim.bin
    
      $ (cd out/default.zircon && md5sum *-boot-shim.bin)
      1469d858f38359c1f4dddb0e38472166  as370-boot-shim.bin
      bf7e99eb62a062a5a8a2a60c5ccbbae6  c18-boot-shim.bin
      f7131e6bf0e7388897f1ac7fe6ded286  cleo-boot-shim.bin
      40fcb6e149c0b00c2d27b2b7d48d4bca  msm8998-boot-shim.bin
      7c3d6bfa27a81983064048258fa72dad  msm8x53-som-boot-shim.bin
      c47f5497eceece696da4a86866ea4dec  mt8167s_ref-boot-shim.bin
      ae1e047439e24290172c48889e5bbd2a  qemu-boot-shim.bin
      81835432c25455eb535812217e68af4a  visalia-boot-shim.bin
    
    Bug: 59918
    Testing: See above
    
    Change-Id: Ib3cf2593f04e62b90ff0ac1a5d8ef2aae7f7c52b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/431299
    Reviewed-by: P.Y. Laligand <pylaligand@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Testability-Review: Shai Barack <shayba@google.com>
    Commit-Queue: David Turner <digit@google.com>

--- zircon/kernel/target/arm64/boot-shim/cleo-uart.c
+++ zircon/kernel/target/arm64/boot-shim/cleo-uart.c
@@ -1,8 +1,1 @@
-// Copyright 2018 The Fuchsia Authors
-//
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT
-
-// Cleo is also mt8167s.
-#include "mt8167s_ref-uart.c"
+mt8167s_ref-uart.c
--- zircon/kernel/target/arm64/boot-shim/visalia-uart.c
+++ zircon/kernel/target/arm64/boot-shim/visalia-uart.c
@@ -1,8 +1,1 @@
-// Copyright 2019 The Fuchsia Authors
-//
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT
-
-// Visalia is also AS370.
-#include "as370-uart.c"
+as370-uart.c
