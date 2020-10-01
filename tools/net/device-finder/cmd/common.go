vsrinivas/fuchsia

commit 5665528bb9ebc04fbae6c924b7407772945ea864

[device-finder] Allow -netboot without -mdns=false
Also default to using both protocols when local resolution is requested.

Change-Id: I58c235658f4fa5f10bcad5992c218544c81980b1
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/433329
Reviewed-by: James Tucker <raggi@google.com>
Reviewed-by: Andrew Davies <awdavies@google.com>
Commit-Queue: Tamir Duberstein <tamird@google.com>
Testability-Review: Andrew Davies <awdavies@google.com>

tools/net/device-finder/cmd/common.go
@@ -105,6 +105,9 @@ type devFinderCmd struct {
 	ipv4      bool
 	ipv6      bool
 
+	// Used to visit flags after parsing, necessary for complex if-set logic.
+	flagSet *flag.FlagSet
+
 	mdnsHandler mDNSHandler
 	finders     []deviceFinder
 
@@ -151,7 +154,13 @@ func (f *fuchsiaDevice) outbound() (*fuchsiaDevice, error) {
 	}, nil
 }
 
+const (
+	mdnsFlag    = "mdns"
+	netbootFlag = "netboot"
+)
+
 func (cmd *devFinderCmd) SetCommonFlags(f *flag.FlagSet) {
+	cmd.flagSet = f
 	f.BoolVar(&cmd.json, "json", false, "Outputs in JSON format.")
 	f.StringVar(&cmd.mdnsAddrs, "addr", "224.0.0.251,ff02::fb", "[linux only] Comma separated list of addresses to issue mDNS queries to.")
 	f.StringVar(&cmd.mdnsPorts, "port", "5353", "[linux only] Comma separated list of ports to issue mDNS queries to.")
@@ -160,8 +169,8 @@ func (cmd *devFinderCmd) SetCommonFlags(f *flag.FlagSet) {
 	f.BoolVar(&cmd.acceptUnicast, "accept-unicast", true, "[linux only] Accepts unicast responses. For if the receiving device responds from a different subnet or behind port forwarding.")
 	f.IntVar(&cmd.deviceLimit, "device-limit", 0, "Exits before the timeout at this many devices per resolution (zero means no limit).")
 	f.IntVar(&cmd.ttl, "ttl", -1, "[linux only] Sets the TTL for outgoing mcast messages. Primarily for debugging and testing. Setting this to zero limits messages to the localhost.")
-	f.BoolVar(&cmd.netboot, "netboot", false, "Determines whether to use netboot protocol")
-	f.BoolVar(&cmd.mdns, "mdns", true, "Determines whether to use mDNS protocol")
+	f.BoolVar(&cmd.mdns, mdnsFlag, true, "Determines whether to use mDNS protocol")
+	f.BoolVar(&cmd.netboot, netbootFlag, false, "Determines whether to use netboot protocol")
 	f.BoolVar(&cmd.ignoreNAT, "ignore-nat", false, "[linux only] Determines whether to ignore possible NAT. Returns the target's address as it sees itself behind a NAT.")
 	f.BoolVar(&cmd.ipv6, "ipv6", true, "Set whether to query using IPv6. Disabling IPv6 will also disable netboot.")
 	f.BoolVar(&cmd.ipv4, "ipv4", true, "Set whether to query using IPv4")
@@ -324,6 +333,28 @@ func (cmd *devFinderCmd) close() {
 
 func (cmd *devFinderCmd) deviceFinders() ([]deviceFinder, error) {
 	if len(cmd.finders) == 0 {
+		{
+			mdnsSet := false
+			netbootSet := false
+			cmd.flagSet.Visit(func(f *flag.Flag) {
+				switch f.Name {
+				case mdnsFlag:
+					mdnsSet = true
+				case netbootFlag:
+					netbootSet = true
+				}
+			})
+			if !mdnsSet {
+				if netbootSet {
+					// Turn off the mDNS default if only netboot is specified.
+					cmd.mdns = false
+				} else if cmd.localResolve {
+					// Local resolution produces the same result regardless of protocol; default to enabling
+					// both if neither is specified.
+					cmd.netboot = true
+				}
+			}
+		}
 		if !cmd.localResolve && cmd.netboot && cmd.mdns {
 			return nil, errors.New("only one of mdns and netboot may be specified")
 		}
@@ -333,7 +364,8 @@ func (cmd *devFinderCmd) deviceFinders() ([]deviceFinder, error) {
 			} else {
 				cmd.finders = append(cmd.finders, &mdnsFinder{deviceFinderBase{cmd: cmd}})
 			}
-		} else if cmd.netboot && cmd.ipv6 {
+		}
+		if cmd.netboot && cmd.ipv6 {
 			cmd.finders = append(cmd.finders, &netbootFinder{deviceFinderBase{cmd: cmd}})
 		} else {
 			return nil, errors.New("either mdns or netboot and ipv6 must be specified")

commit 548a37bbf432614f29f4e51a52799435ca9bad96

[device-finder] Don't error when netboot is off
Accidentally introduced in 5665528bb9ebc04fbae6c924b7407772945ea864.

Tested manually:
  $ fx device-finder list
  fe80::5054:ff:fe63:5e7a%qemu
  $ fx device-finder list -netboot
  fe80::5054:4d:fe63:5e7a%qemu
  $ fx device-finder list -mdns
  fe80::5054:ff:fe63:5e7a%qemu
  $ fx device-finder list -netboot -mdns
  list.go:73: only one of mdns and netboot may be specified
  $ fx device-finder list -netboot=false -mdns=false
  list.go:73: either mdns or netboot and ipv6 must be specified
  $ fx device-finder list -mdns=false
  list.go:73: either mdns or netboot and ipv6 must be specified
  $ fx device-finder list -netboot=false
  list.go:73: either mdns or netboot and ipv6 must be specified
  $ fx device-finder list -local-resolve
  fe80::5054:ff:fe63:5e7a%qemu
  $ fx device-finder list -local -mdns -netboot
  fe80::447f:e5ff:fe4f:fa92%qemu

Before this change, the bare `fx device-finder list` invocation would fail.

Change-Id: I10b5ef5d495ad826ec913f10eb07d069daee6221
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/433679
Commit-Queue: Tamir Duberstein <tamird@google.com>
Reviewed-by: Andrew Davies <awdavies@google.com>
Reviewed-by: Yecheng Zhao <zyecheng@google.com>
Reviewed-by: Adam Barth <abarth@google.com>
Testability-Review: Andrew Davies <awdavies@google.com>
Testability-Review: Adam Barth <abarth@google.com>

tools/net/device-finder/cmd/common.go
@@ -367,7 +367,7 @@ func (cmd *devFinderCmd) deviceFinders() ([]deviceFinder, error) {
 		}
 		if cmd.netboot && cmd.ipv6 {
 			cmd.finders = append(cmd.finders, &netbootFinder{deviceFinderBase{cmd: cmd}})
-		} else {
+		} else if !cmd.mdns {
 			return nil, errors.New("either mdns or netboot and ipv6 must be specified")
 		}
 	}