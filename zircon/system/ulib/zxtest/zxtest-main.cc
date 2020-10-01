70e2a50995fce0579cd49b06bbf092b38fedeff1
Suraj Malhotra <surajmalhotra@google.com>
2020-10-01 06:35:15 +0000

    [zxtest] Set stdout to line buffering by default
    This lowers the risk of losing logs due to buffering when test process
    crashes occur.
    
    Change-Id: I4b75d0fca36712632a669578dee03b06e07a44e8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/433286
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Testability-Review: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Suraj Malhotra <surajmalhotra@google.com>

--- zircon/system/ulib/zxtest/zxtest-main.cc
+++ zircon/system/ulib/zxtest/zxtest-main.cc
@@ -2,9 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stdio.h>
 #include <zircon/compiler.h>
+
 #include <zxtest/zxtest.h>
 
 // Provides a default entry point to run all registered tests. If the main program provides its own
 // main, the library's main will be ignored.
-__WEAK int main(int argc, char** argv) { return RUN_ALL_TESTS(argc, argv); }
+__WEAK int main(int argc, char** argv) {
+  setlinebuf(stdout);
+  return RUN_ALL_TESTS(argc, argv);
+}
