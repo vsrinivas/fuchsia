28cc333ed580a3f941985a0a7b4bd98906b7cc03
omer <omerlevran@google.com>
2020-10-02 23:00:39 +0000

    [setui] add missing license
    Change-Id: Ie4586919990509bd0b9943c1adb263a73e3e3404
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/434400
    Reviewed-by: Chip Fukuhara <cfukuhara@google.com>
    Reviewed-by: Bryce Lee <brycelee@google.com>
    Testability-Review: Chip Fukuhara <cfukuhara@google.com>
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>

--- garnet/bin/setui/src/agent/media_buttons.rs
+++ garnet/bin/setui/src/agent/media_buttons.rs
@@ -1,3 +1,7 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 use crate::agent::base::{Context as AgentContext, Descriptor};
 use crate::blueprint_definition;
 
