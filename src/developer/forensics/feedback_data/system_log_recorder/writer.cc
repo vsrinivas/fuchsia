56bf65b19e5717dffe75c1fb404c67a2c9b0f9c1
omer <omerlevran@google.com>
2020-10-05 23:20:23 +0000

    [forensics] - fix license with extra spaces
    Change-Id: I5de0023d350e15df3ef3e8b6e55d0bb674cba073
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/435116
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>
    Testability-Review: Alex Pankhurst <pankhurst@google.com>

--- src/developer/forensics/feedback_data/system_log_recorder/writer.cc
+++ src/developer/forensics/feedback_data/system_log_recorder/writer.cc
@@ -1,5 +1,5 @@
 // Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD - style license that can be
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "src/developer/forensics/feedback_data/system_log_recorder/writer.h"
