vsrinivas/fuchsia

commit 0267c3eb9e00b9d17a2af8c4c7bab504b76802b7

[dispatcher-pool] Move away from custom fbl container traits.
Update code to stop using custom fbl container traits in order to
implement lists used to manage EventSources in their ExecutionDomains.

This refactor removes a use of custom container traits from the system
which helps to prepare for an up-and-coming refactor of the fbl::
container template arguments which will require special handling of
users of custom node traits.  The fewer of these that we have, the
easier the refactor will be.

Change-Id: I68a20fe38a4b76c5b0bb9223bcb372384fffaf83
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/386814
Commit-Queue: John Grossman <johngro@google.com>
Reviewed-by: Nick Maniscalco <maniscalco@google.com>
Testability-Review: Nick Maniscalco <maniscalco@google.com>

zircon/system/ulib/dispatcher-pool/dispatcher-execution-domain.cc
@@ -4,10 +4,10 @@
 
 #include <lib/zx/event.h>
 
+#include <utility>
+
 #include <dispatcher-pool/dispatcher-execution-domain.h>
 #include <dispatcher-pool/dispatcher-thread-pool.h>
-
-#include <utility>
 
 namespace dispatcher {
 
@@ -51,7 +51,7 @@ ExecutionDomain::~ExecutionDomain() {
   // before destructing.
   ZX_DEBUG_ASSERT(deactivated());
   ZX_DEBUG_ASSERT(sources_.is_empty());
-  ZX_DEBUG_ASSERT(!thread_pool_node_state_.InContainer());
+  ZX_DEBUG_ASSERT(InContainer());
 }
 
 void ExecutionDomain::Deactivate(bool sync_dispatch) {
@@ -59,7 +59,7 @@ void ExecutionDomain::Deactivate(bool sync_dispatch) {
   // from being added to the sources_ list.  We can then swap the contents of
   // the sources_ list with a temp list, leave the lock and deactivate all of
   // the sources at our leisure.
-  fbl::DoublyLinkedList<fbl::RefPtr<EventSource>, EventSource::SourcesListTraits> to_deactivate;
+  SourceList to_deactivate;
   bool sync_needed = false;
 
   {

zircon/system/ulib/dispatcher-pool/include/dispatcher-pool/dispatcher-event-source.h
@@ -2,28 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#pragma once
+#ifndef DISPATCHER_POOL_DISPATCHER_EVENT_SOURCE_H_
+#define DISPATCHER_POOL_DISPATCHER_EVENT_SOURCE_H_
 
+#include <lib/zx/event.h>
+#include <lib/zx/handle.h>
+#include <lib/zx/port.h>
+#include <unistd.h>
 #include <zircon/assert.h>
 #include <zircon/compiler.h>
 #include <zircon/syscalls/port.h>
 #include <zircon/types.h>
-#include <lib/zx/event.h>
-#include <lib/zx/handle.h>
-#include <lib/zx/port.h>
+
+#include <fbl/auto_lock.h>
 #include <fbl/function.h>
 #include <fbl/intrusive_double_list.h>
-#include <fbl/auto_lock.h>
 #include <fbl/mutex.h>
 #include <fbl/ref_counted.h>
 #include <fbl/ref_ptr.h>
-#include <unistd.h>
 
 namespace dispatcher {
 
 class Channel;
 class ExecutionDomain;
 class ThreadPool;
+
+namespace internal {
+struct EventSourceSourceListTag {};       // Tag used for our ExecutionDomain's Sources List
+struct EventSourcePendingWorkListTag {};  // Tag used for our ExecutionDomain's Pending Work List
+}  // namespace internal
 
 // class EventSource
 //
@@ -40,11 +47,19 @@ class ThreadPool;
 // needed, it may be Deactivated and finally destroyed.  An event source may not
 // be re-activated once it has been deactivated.
 //
-class EventSource : public fbl::RefCounted<EventSource> {
+class EventSource : public fbl::RefCounted<EventSource>,
+                    public fbl::ContainableBaseClasses<
+                        fbl::TaggedDoublyLinkedListable<fbl::RefPtr<EventSource>,
+                                                        internal::EventSourceSourceListTag>,
+                        fbl::TaggedDoublyLinkedListable<fbl::RefPtr<EventSource>,
+                                                        internal::EventSourcePendingWorkListTag>> {
  public:
+  using SourceListTag = internal::EventSourceSourceListTag;
+  using PendingWorkListTag = internal::EventSourcePendingWorkListTag;
+
   zx_signals_t process_signal_mask() const { return process_signal_mask_; }
-  bool InExecutionDomain() const { return sources_node_state_.InContainer(); }
-  bool InPendingList() const { return pending_work_node_state_.InContainer(); }
+  bool InExecutionDomain() const { return fbl::InContainer<SourceListTag>(*this); }
+  bool InPendingList() const { return fbl::InContainer<PendingWorkListTag>(*this); }
 
   virtual void Deactivate() __TA_EXCLUDES(obj_lock_) = 0;
 
@@ -110,28 +125,9 @@ class EventSource : public fbl::RefCounted<EventSource> {
  private:
   friend class fbl::RefPtr<EventSource>;
   friend class ExecutionDomain;
-
-  struct SourcesListTraits {
-    static fbl::DoublyLinkedListNodeState<fbl::RefPtr<EventSource>>& node_state(
-        EventSource& event_source) {
-      return event_source.sources_node_state_;
-    }
-  };
-
-  struct PendingWorkListTraits {
-    static fbl::DoublyLinkedListNodeState<fbl::RefPtr<EventSource>>& node_state(
-        EventSource& event_source) {
-      return event_source.pending_work_node_state_;
-    }
-  };
-
   const zx_signals_t process_signal_mask_;
-
-  // Node state for existing on the domain's sources_ list.
-  fbl::DoublyLinkedListNodeState<fbl::RefPtr<EventSource>> sources_node_state_;
-
-  // Node state for existing on the domain's pending_work_ list.
-  fbl::DoublyLinkedListNodeState<fbl::RefPtr<EventSource>> pending_work_node_state_;
 };
 
 }  // namespace dispatcher
+
+#endif  // DISPATCHER_POOL_DISPATCHER_EVENT_SOURCE_H_

zircon/system/ulib/dispatcher-pool/include/dispatcher-pool/dispatcher-execution-domain.h
@@ -2,21 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#pragma once
+#ifndef DISPATCHER_POOL_DISPATCHER_EXECUTION_DOMAIN_H_
+#define DISPATCHER_POOL_DISPATCHER_EXECUTION_DOMAIN_H_
 
+#include <lib/zx/event.h>
+#include <lib/zx/profile.h>
 #include <zircon/assert.h>
 #include <zircon/compiler.h>
 #include <zircon/types.h>
-#include <lib/zx/event.h>
-#include <lib/zx/profile.h>
+
+#include <atomic>
+
+#include <dispatcher-pool/dispatcher-event-source.h>
 #include <fbl/intrusive_double_list.h>
 #include <fbl/mutex.h>
 #include <fbl/ref_counted.h>
 #include <fbl/ref_ptr.h>
-
-#include <dispatcher-pool/dispatcher-event-source.h>
-
-#include <atomic>
 
 namespace dispatcher {
 
@@ -47,7 +48,8 @@ class ThreadPool;
 // dispatch operations will be started, and the system will be completely
 // deactivated when the current in-flight dispatch operation unwinds.
 //
-class ExecutionDomain : public fbl::RefCounted<ExecutionDomain> {
+class ExecutionDomain : public fbl::RefCounted<ExecutionDomain>,
+                        public fbl::DoublyLinkedListable<fbl::RefPtr<ExecutionDomain>> {
  public:
   // Token and ScopedToken are small (empty) objects which are intended to be
   // used with the clang static thread analysis framework in order to express
@@ -117,13 +119,6 @@ class ExecutionDomain : public fbl::RefCounted<ExecutionDomain> {
   friend class WakeupEvent;
   using DispatchState = EventSource::DispatchState;
 
-  struct ThreadPoolListTraits {
-    static fbl::DoublyLinkedListNodeState<fbl::RefPtr<ExecutionDomain>>& node_state(
-        ExecutionDomain& domain) {
-      return domain.thread_pool_node_state_;
-    }
-  };
-
   ExecutionDomain(fbl::RefPtr<ThreadPool> thread_pool, zx::event dispatch_idle_evt);
   virtual ~ExecutionDomain();
 
@@ -159,13 +154,12 @@ class ExecutionDomain : public fbl::RefCounted<ExecutionDomain> {
 
   // The list of all sources bound to us, as well as the sources which are
   // currently waiting to be dispatched.
-  fbl::DoublyLinkedList<fbl::RefPtr<EventSource>, EventSource::SourcesListTraits> sources_
-      __TA_GUARDED(sources_lock_);
-  fbl::DoublyLinkedList<fbl::RefPtr<EventSource>, EventSource::PendingWorkListTraits> pending_work_
-      __TA_GUARDED(sources_lock_);
-
-  // Node state for existing in our thread pool's execution domain list.
-  fbl::DoublyLinkedListNodeState<fbl::RefPtr<ExecutionDomain>> thread_pool_node_state_;
+  using SourceList =
+      fbl::TaggedDoublyLinkedList<fbl::RefPtr<EventSource>, EventSource::SourceListTag>;
+  using PendingWorkList =
+      fbl::TaggedDoublyLinkedList<fbl::RefPtr<EventSource>, EventSource::PendingWorkListTag>;
+  SourceList sources_ __TA_GUARDED(sources_lock_);
+  PendingWorkList pending_work_ __TA_GUARDED(sources_lock_);
 };
 
 // A helper macro which can ease some of the namespace pain of establishing the
@@ -182,3 +176,5 @@ class ExecutionDomain : public fbl::RefCounted<ExecutionDomain> {
   ::dispatcher::ExecutionDomain::ScopedToken _sym_name((_exe_domain)->token())
 
 }  // namespace dispatcher
+
+#endif  // DISPATCHER_POOL_DISPATCHER_EXECUTION_DOMAIN_H_

zircon/system/ulib/dispatcher-pool/include/dispatcher-pool/dispatcher-thread-pool.h
@@ -90,9 +90,7 @@ class ThreadPool : public fbl::RefCounted<ThreadPool>,
   uint32_t active_thread_count_ __TA_GUARDED(pool_lock_) = 0;
   bool pool_shutting_down_ __TA_GUARDED(pool_lock_) = false;
 
-  fbl::DoublyLinkedList<fbl::RefPtr<ExecutionDomain>, ExecutionDomain::ThreadPoolListTraits>
-      active_domains_ __TA_GUARDED(pool_lock_);
-
+  fbl::DoublyLinkedList<fbl::RefPtr<ExecutionDomain>> active_domains_ __TA_GUARDED(pool_lock_);
   fbl::DoublyLinkedList<std::unique_ptr<Thread>> active_threads_ __TA_GUARDED(pool_lock_);
 };
 

commit 7aec4e6d8aa824ef16b5258aa48a7e54185befdd

[dispatcher-pool] Correct an assert on removal
A refactor in 0267c3eb9e00 mistakenly inverted this dtor assert intended
to ensure the object was no longer contained in another structure.
Furthermore, all fbl::Containable* derived objects already have this
check. The easiest fix is to just delete the assert.

Change-Id: I27dd943405181b31bb8ad90409cb212005953669
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/436218
Reviewed-by: John Grossman <johngro@google.com>
Testability-Review: Christopher Anderson <cja@google.com>
Commit-Queue: Christopher Anderson <cja@google.com>

zircon/system/ulib/dispatcher-pool/dispatcher-execution-domain.cc
@@ -51,7 +51,6 @@ ExecutionDomain::~ExecutionDomain() {
   // before destructing.
   ZX_DEBUG_ASSERT(deactivated());
   ZX_DEBUG_ASSERT(sources_.is_empty());
-  ZX_DEBUG_ASSERT(InContainer());
 }
 
 void ExecutionDomain::Deactivate(bool sync_dispatch) {