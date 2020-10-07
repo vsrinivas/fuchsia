fea2371568c4a004934f9f5d4a8b342607cee61b
Cameron Finucane <eep@google.com>
2020-10-07 22:58:50 +0000

    [scripts][fuzzing] Enable CQ testing of `fx fuzz`
    Now that the `fx fuzz` unit tests are hermetic, we should make sure it
    is tested in CQ. This updates the existing host test wrapper to work
    even in environments without `$FUCHSIA_DIR` set, and to run all
    `*_test.py` tests instead of a specific list.
    
    Test: fx test host_fuzzers_test
    Change-Id: Ic45058674b1c710889620841a29fa396021e5fe3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/434480
    Commit-Queue: Cameron Finucane <eep@google.com>
    Reviewed-by: Aaron Green <aarongreen@google.com>
    Testability-Review: Aaron Green <aarongreen@google.com>

--- src/lib/fuzzing/host/host_test.go
+++ src/lib/fuzzing/host/host_test.go
@@ -1,34 +1,27 @@
-// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package fuzzing
 
 import (
-	"os"
+	"flag"
 	"os/exec"
-	"path"
+	"path/filepath"
 	"testing"
 )
 
+// This is only used when run via `go test`
+const relativePath = "../../../../scripts/fuzzing"
+
+var testDataDir = flag.String("test-data-dir", relativePath, "Path to `fx fuzz` files; only used in GN build")
+
 func TestFxFuzzLib(t *testing.T) {
-	fuchsiaDir, found := os.LookupEnv("FUCHSIA_DIR")
-	if !found {
-		t.SkipNow()
+	pyTestDir := filepath.Join(*testDataDir, "test")
+	cmd := exec.Command("python", "-m", "unittest", "discover", "-s", pyTestDir, "-p", "*_test.py", "-b")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("test failed: %v", err)
 	}
-	scriptsFuzzingTests := []string{
-		"cipd_test.py",
-		"device_test.py",
-		"fuzzer_test.py",
-		"host_test.py",
-	}
-	for _, test := range scriptsFuzzingTests {
-		pytest := path.Join(fuchsiaDir, "scripts", "fuzzing", "test", test)
-		cmd := exec.Command("python", pytest, "-b")
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			t.Errorf("%s failed:: %v", test, err)
-		}
-		t.Log(string(output[:]))
-	}
+	t.Log(string(output[:]))
 }
