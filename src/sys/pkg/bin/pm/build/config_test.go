9d620db7ae54076219d99f6549f1fd696637dc28
Kevin Wells <kevinwells@google.com>
2020-10-08 20:23:19 +0000

    [pm] Backfill a test for BlobInfo()
    It currently doesn't produce blobs in a deterministic order, which a
    future CL will fix.
    
    Bug: 61368
    Change-Id: I17811df61513ec023e743f29b7867c81ea6683f3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/436619
    Testability-Review: Kevin Wells <kevinwells@google.com>
    Commit-Queue: Kevin Wells <kevinwells@google.com>
    Reviewed-by: Devon H. O'Dell <dhobsd@google.com>

---
+++ src/sys/pkg/bin/pm/build/config_test.go
@@ -0,0 +1,40 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package build
+
+import (
+	"os"
+	"path/filepath"
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func TestOrderedBlobInfo(t *testing.T) {
+	cfg := TestConfig()
+	defer os.RemoveAll(filepath.Dir(cfg.TempDir))
+	BuildTestPackage(cfg)
+
+	blobs, err := cfg.BlobInfo()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// The meta far is always first, and all following content entries are
+	// sorted by the path in the package.
+	expectedPaths := []string{"meta/", "a", "b", "dir/c", "rand1", "rand2"}
+
+	actualPaths := make([]string, 0, len(blobs))
+	for _, blob := range blobs {
+		actualPaths = append(actualPaths, blob.Path)
+	}
+
+	// FIXME(61368) Remove this sort once the paths are actually sorted by BlobInfo().
+	sort.Strings(actualPaths[1:])
+
+	if !reflect.DeepEqual(actualPaths, expectedPaths) {
+		t.Errorf("got %v, want %v", actualPaths, expectedPaths)
+	}
+}
