9153a9220039e19c47b4c5cfb523a0d47f99d6b1
Mukesh Agrawal <quiche@google.com>
2020-10-08 00:59:32 +0000

    [ui-input] input-synthesis: add unit tests for legacy backend
    Now that the backend has been separated from the front-end, the
    legacy backend has no test coverage of its own. Fix that by adding
    unit tests.
    
    Bug: 60789
    Test: fx test input-synthesis-tests
    Change-Id: I6896529197eadeef77ce60b42da77736265eabe5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/434299
    Reviewed-by: Viktar Donich <viktard@google.com>
    Testability-Review: Mukesh Agrawal <quiche@google.com>
    Commit-Queue: Mukesh Agrawal <quiche@google.com>

--- src/lib/ui/input-synthesis/src/legacy_backend.rs
+++ src/lib/ui/input-synthesis/src/legacy_backend.rs
@@ -84,3 +84,172 @@ pub(crate) fn multi_finger_tap(fingers: Option<Vec<Touch>>, time: u64) -> InputR
         trace_id: 0,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use {super::*, proptest::prelude::*};
+
+    proptest! {
+        #[test]
+        fn media_buttons_populates_report_correctly(
+            volume_up: bool,
+            volume_down: bool,
+            mic_mute: bool,
+            reset: bool,
+            pause: bool,
+            event_time: u64
+        ) {
+            prop_assert_eq!(
+                media_buttons(volume_up, volume_down, mic_mute, reset, pause, event_time),
+                InputReport {
+                    event_time,
+                    keyboard: None,
+                    media_buttons: Some(Box::new(MediaButtonsReport {
+                        volume_up,
+                        volume_down,
+                        mic_mute,
+                        reset,
+                        pause
+                    })),
+                    mouse: None,
+                    stylus: None,
+                    touchscreen: None,
+                    sensor: None,
+                    trace_id: 0
+                }
+            );
+        }
+    }
+
+    #[test]
+    fn key_press_populates_report_correctly() {
+        assert_eq!(
+            key_press(KeyboardReport { pressed_keys: vec![1, 2, 3] }, 200),
+            InputReport {
+                event_time: 200,
+                keyboard: Some(Box::new(KeyboardReport { pressed_keys: vec![1, 2, 3] })),
+                media_buttons: None,
+                mouse: None,
+                stylus: None,
+                touchscreen: None,
+                sensor: None,
+                trace_id: 0
+            }
+        );
+    }
+
+    #[test]
+    fn key_press_usage_populates_report_correctly_when_a_key_is_pressed() {
+        assert_eq!(
+            key_press_usage(Some(1), 300),
+            InputReport {
+                event_time: 300,
+                keyboard: Some(Box::new(KeyboardReport { pressed_keys: vec![1] })),
+                media_buttons: None,
+                mouse: None,
+                stylus: None,
+                touchscreen: None,
+                sensor: None,
+                trace_id: 0
+            }
+        );
+    }
+
+    #[test]
+    fn key_press_usage_populates_report_correctly_when_no_key_is_pressed() {
+        assert_eq!(
+            key_press_usage(None, 400),
+            InputReport {
+                event_time: 400,
+                keyboard: Some(Box::new(KeyboardReport { pressed_keys: vec![] })),
+                media_buttons: None,
+                mouse: None,
+                stylus: None,
+                touchscreen: None,
+                sensor: None,
+                trace_id: 0
+            }
+        );
+    }
+
+    #[test]
+    fn tap_populates_report_correctly_when_finger_is_present() {
+        assert_eq!(
+            tap(Some((10, 20)), 500),
+            InputReport {
+                event_time: 500,
+                keyboard: None,
+                media_buttons: None,
+                mouse: None,
+                stylus: None,
+                touchscreen: Some(Box::new(TouchscreenReport {
+                    touches: vec![Touch { finger_id: 1, x: 10, y: 20, width: 0, height: 0 },]
+                })),
+                sensor: None,
+                trace_id: 0
+            }
+        );
+    }
+
+    #[test]
+    fn tap_populates_report_correctly_when_finger_is_absent() {
+        assert_eq!(
+            tap(None, 600),
+            InputReport {
+                event_time: 600,
+                keyboard: None,
+                media_buttons: None,
+                mouse: None,
+                stylus: None,
+                touchscreen: Some(Box::new(TouchscreenReport { touches: vec![] })),
+                sensor: None,
+                trace_id: 0
+            }
+        );
+    }
+
+    #[test]
+    fn multi_finger_tap_populates_report_correctly_when_fingers_are_present() {
+        assert_eq!(
+            multi_finger_tap(
+                Some(vec![
+                    Touch { finger_id: 1, x: 99, y: 100, width: 10, height: 20 },
+                    Touch { finger_id: 2, x: 199, y: 201, width: 30, height: 40 }
+                ]),
+                700
+            ),
+            InputReport {
+                event_time: 700,
+                keyboard: None,
+                media_buttons: None,
+                mouse: None,
+                stylus: None,
+                touchscreen: Some(Box::new(TouchscreenReport {
+                    touches: vec![
+                        Touch { finger_id: 1, x: 99, y: 100, width: 10, height: 20 },
+                        Touch { finger_id: 2, x: 199, y: 201, width: 30, height: 40 }
+                    ]
+                })),
+                sensor: None,
+                trace_id: 0
+            }
+        );
+    }
+
+    #[test]
+    fn multi_finger_tap_populates_report_correctly_when_no_fingers_are_present() {
+        assert_eq!(
+            multi_finger_tap(None, 800),
+            InputReport {
+                event_time: 800,
+                keyboard: None,
+                media_buttons: None,
+                mouse: None,
+                stylus: None,
+                touchscreen: Some(Box::new(TouchscreenReport { touches: vec![] })),
+                sensor: None,
+                trace_id: 0
+            }
+        );
+    }
+}
