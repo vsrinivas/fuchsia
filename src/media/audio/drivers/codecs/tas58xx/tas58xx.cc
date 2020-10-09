ac64d8134fb33d005ee2c000329c492cd15b108e
Andres Oportus <andresoportus@google.com>
2020-10-09 17:06:26 +0000

    [fdf] Migrate tas58xx to bindc
    Test: ti_tas58xx-bind_test
    Change-Id: I7622fd69dd13da4045392ef7447dd6701c2fddb9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/436623
    Reviewed-by: Alex Legg <alexlegg@google.com>
    Testability-Review: Alex Legg <alexlegg@google.com>
    Commit-Queue: Andres Oportus <andresoportus@google.com>

--- src/media/audio/drivers/codecs/tas58xx/tas58xx.cc
+++ src/media/audio/drivers/codecs/tas58xx/tas58xx.cc
@@ -10,13 +10,14 @@
 #include <algorithm>
 #include <memory>
 
-#include <ddk/binding.h>
 #include <ddk/metadata.h>
 #include <ddk/platform-defs.h>
 #include <ddk/protocol/composite.h>
 #include <ddk/protocol/i2c.h>
 #include <fbl/algorithm.h>
 #include <fbl/alloc_checker.h>
+
+#include "src/media/audio/drivers/codecs/tas58xx/ti_tas58xx-bind.h"
 
 namespace {
 // clang-format off
@@ -319,10 +320,4 @@ static constexpr zx_driver_ops_t driver_ops = []() {
 
 }  // namespace audio
 
-// clang-format off
-ZIRCON_DRIVER_BEGIN(ti_tas58xx, audio::driver_ops, "zircon", "0.1", 3)
-    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_COMPOSITE),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_TI),
-    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, PDEV_DID_TI_TAS58xx),
-ZIRCON_DRIVER_END(ti_tas58xx)
-    // clang-format on
+ZIRCON_DRIVER(ti_tas58xx, audio::driver_ops, "zircon", "0.1")
