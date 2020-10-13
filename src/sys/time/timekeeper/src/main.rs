7c6d2bb07bbb50896aafac3e34fa957c429a7afc
Jody Sankey <jsankey@google.com>
2020-10-13 19:52:54 +0000

    [timekeeper] Actually enable HTTPSdate in monitor mode.
    Bug: 56858
    Change-Id: I50fe0950b5ec61988f7e91928fe5e518435b60f3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/436629
    Commit-Queue: Jody Sankey <jsankey@google.com>
    Reviewed-by: Satsuki Ueno <satsukiu@google.com>
    Testability-Review: Satsuki Ueno <satsukiu@google.com>

--- src/sys/time/timekeeper/src/main.rs
+++ src/sys/time/timekeeper/src/main.rs
@@ -53,9 +53,7 @@ struct TimeSourceUrls {
 /// belong in a config file.
 const DEFAULT_SOURCES: TimeSourceUrls = TimeSourceUrls {
     primary: "fuchsia-pkg://fuchsia.com/network-time-service#meta/network_time_service.cmx",
-    // TODO(jsankey): Replace with Some("fuchsia-pkg://fuchsia.com/httpsdate-time-source#
-    //                meta/httpsdate_time_source.cmx") once HTTPSdate is stable.
-    monitor: None,
+    monitor: Some("fuchsia-pkg://fuchsia.com/httpsdate-time-source#meta/httpsdate_time_source.cmx"),
 };
 
 /// The time sources to install when the dev_time_sources flag is set. In the future, these values
