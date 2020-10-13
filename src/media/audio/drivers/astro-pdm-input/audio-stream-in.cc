d484a0eb54c7fb32ac393eeb3bdd6c51158b93de
Andres Oportus <andresoportus@google.com>
2020-10-13 03:02:45 +0000

    [fdf] Migrate astro pdm to bindc
    Test: aml_pdm-bind_test
    Change-Id: I441b5def52cf4a19e31a1413727caeaf8b8723ed
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/437976
    Commit-Queue: Andres Oportus <andresoportus@google.com>
    Reviewed-by: Alex Legg <alexlegg@google.com>
    Testability-Review: Alex Legg <alexlegg@google.com>

--- src/media/audio/drivers/astro-pdm-input/audio-stream-in.cc
+++ src/media/audio/drivers/astro-pdm-input/audio-stream-in.cc
@@ -9,10 +9,11 @@
 #include <optional>
 #include <utility>
 
-#include <ddk/binding.h>
 #include <ddk/debug.h>
 #include <ddk/driver.h>
 #include <ddk/platform-defs.h>
+
+#include "src/media/audio/drivers/astro-pdm-input/aml_pdm-bind.h"
 
 namespace audio {
 namespace astro {
@@ -277,12 +278,6 @@ static constexpr zx_driver_ops_t aml_pdm_driver_ops = []() {
   ops.bind = pdm_audio_bind;
   return ops;
 }();
+__END_CDECLS
 
-// clang-format off
-ZIRCON_DRIVER_BEGIN(aml_pdm, aml_pdm_driver_ops, "aml-pdm-in", "0.1", 3)
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_AMLOGIC),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_PID, PDEV_PID_AMLOGIC_S905D2),
-    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, PDEV_DID_AMLOGIC_PDM),
-ZIRCON_DRIVER_END(aml_pdm)
-    // clang-format on
-    __END_CDECLS
+ZIRCON_DRIVER(aml_pdm, aml_pdm_driver_ops, "aml-pdm-in", "0.1")
