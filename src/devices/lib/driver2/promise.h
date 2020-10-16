2e6f116bd494e5634c4f17d678e3606edb2cbe88
Abdulla Kamar <abdulla@google.com>
2020-10-16 03:23:46 +0000

    [devices] Add fit::promise helpers to driver2
    This is part of the drivers-as-components work. These helper functions
    aid in working with asynchronous code in drivers. You can then
    succinctly chain together a sequence events like so:
    
      executor.schedule_task(
        Connect<Protocol>(ns, dispatcher, "/svc/protocol")
          .and_then(Continue(fit::bind_member(this, &Driver::Setup)))
          .or_else(fit::bind_member(this, &Driver::Stop))
          .wrap_with(scope));
    
    Bug: 33183
    Change-Id: I68c60851b9c1a388a933e03b1d0a9f9e359e270a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/438620
    Commit-Queue: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Testability-Review: Suraj Malhotra <surajmalhotra@google.com>
    Testability-Review: Abdulla Kamar <abdulla@google.com>

---
+++ src/devices/lib/driver2/promise.h
@@ -0,0 +1,123 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_DEVICES_LIB_DRIVER2_PROMISE_H_
+#define SRC_DEVICES_LIB_DRIVER2_PROMISE_H_
+
+#include <lib/fit/promise.h>
+
+#include "src/devices/lib/driver2/namespace.h"
+
+namespace promise {
+
+template <typename T>
+class ContinueWith;
+
+namespace internal {
+
+// Connects to the given |path| in |ns|, and returns a fit::result containing a
+// fidl::Client on success.
+template <typename T>
+fit::result<fidl::Client<T>, zx_status_t> ConnectWithResult(const Namespace& ns,
+                                                            async_dispatcher_t* dispatcher,
+                                                            std::string_view path) {
+  auto result = ns.Connect(path);
+  if (result.is_error()) {
+    return fit::error(result.status_value());
+  }
+  return fit::ok(fidl::Client<T>(std::move(result.value()), dispatcher));
+}
+
+// Helps to call a fit::promise lambda function.
+template <typename Func, size_t size>
+struct ContinueCall {
+  static_assert(size == 2, "Unexpected number of arguments");
+
+  using return_type = typename fit::callable_traits<Func>::return_type;
+  using value_type = typename fit::callable_traits<Func>::args::template at<1>;
+
+  static auto Call(Func func) {
+    return [func = std::move(func), done = false, with = ContinueWith<return_type>()](
+               fit::context& context, value_type value) mutable -> return_type {
+      if (done) {
+        return with.Result();
+      }
+      done = true;
+      with.Suspend(context);
+      return func(with, std::forward<value_type>(value));
+    };
+  }
+};
+
+template <typename Func>
+struct ContinueCall<Func, 1> {
+  using return_type = typename fit::callable_traits<Func>::return_type;
+
+  static auto Call(Func func) {
+    return [func = std::move(func), done = false,
+            with = ContinueWith<return_type>()](fit::context& context) mutable -> return_type {
+      if (done) {
+        return with.Result();
+      }
+      done = true;
+      with.Suspend(context);
+      return func(with);
+    };
+  }
+};
+
+}  // namespace internal
+
+// Connects to the given |path| in |ns|, and returns a fit::promise containing a
+// fidl::Client on success.
+template <typename T>
+fit::promise<fidl::Client<T>, zx_status_t> Connect(const Namespace& ns,
+                                                   async_dispatcher_t* dispatcher,
+                                                   std::string_view path) {
+  return fit::make_result_promise(internal::ConnectWithResult<T>(ns, dispatcher, path));
+}
+
+// Wraps a fit::suspended_task in order to provide an ergonomic way to suspend
+// and resume when using a FIDL callback, without the need for a fit::bridge.
+//
+// TODO(fxbug.dev/62049): Consider moving this into libfit.
+template <typename T>
+class ContinueWith {
+ public:
+  // Returns |result| when the promise is resumed.
+  void Return(T result) {
+    task_.resume_task();
+    result_.swap(result);
+  }
+
+ private:
+  fit::suspended_task task_;
+  T result_;
+
+  void Suspend(fit::context& context) {
+    auto task = context.suspend_task();
+    task_.swap(task);
+  }
+
+  T Result() { return std::move(result_); }
+
+  template <typename Func, size_t size>
+  friend struct internal::ContinueCall;
+};
+
+// Allows a fit::promise compatible lambda function to be easily suspended and
+// resumed. This is achieved by replacing the first argument with a ContinueWith
+// object that can capture the result of a callback and resume execution of a
+// promise.
+//
+// TODO(fxbug.dev/62049): Consider moving this into libfit.
+template <typename Func>
+auto Continue(Func func) {
+  constexpr size_t size = fit::callable_traits<Func>::args::size;
+  return internal::ContinueCall<Func, size>::Call(std::move(func));
+}
+
+}  // namespace promise
+
+#endif  // SRC_DEVICES_LIB_DRIVER2_PROMISE_H_
---
+++ src/devices/lib/driver2/promise_test.cc
@@ -0,0 +1,40 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/devices/lib/driver2/promise.h"
+
+#include <gtest/gtest.h>
+
+struct fake_context : fit::context {
+  fit::executor* executor() const override {
+    EXPECT_TRUE(false);
+    return nullptr;
+  }
+
+  fit::suspended_task suspend_task() override { return fit::suspended_task(); }
+};
+
+// Test that promise::Continue() correctly returns fit::results, and can be
+// resumed using promise::ContinueWith.
+TEST(PromiseTest, Continue) {
+  fake_context context;
+
+  auto success = fit::make_promise(promise::Continue(
+      [](promise::ContinueWith<fit::result<>>& with) -> fit::result<> { return fit::ok(); }));
+  EXPECT_TRUE(success(context).is_ok());
+
+  auto failure = fit::make_promise(promise::Continue(
+      [](promise::ContinueWith<fit::result<>>& with) -> fit::result<> { return fit::error(); }));
+  EXPECT_TRUE(failure(context).is_error());
+
+  fit::function<void()> callback;
+  auto pending = fit::make_promise(
+      promise::Continue([&callback](promise::ContinueWith<fit::result<>>& with) -> fit::result<> {
+        callback = [&with] { with.Return(fit::ok()); };
+        return fit::pending();
+      }));
+  EXPECT_TRUE(pending(context).is_pending());
+  callback();
+  EXPECT_TRUE(pending(context).is_ok());
+}
