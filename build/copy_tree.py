634e147cdf735e29c5fdd78e375ad0c7e74e8736
Gary Miguel <garymm@google.com>
2020-10-23 20:17:47 +0000

    [build] copy every file in the python lib, except .pyc files
    copy() doesn't work properly with directories, and this may be
    contributing to bug 62337.
    
    pyc files are generated while actions are running, which may be racing
    with the copy itself trying to read them. Host tests most likely don't
    benefit from them anyways, so we omit them.
    
    Bug: 62337
    Bug: 62640
    Change-Id: I9edccdf03292a8f785bb0f30345e72a56f2a467b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/441614
    Reviewed-by: Tyler Mandry <tmandry@google.com>
    Commit-Queue: Tyler Mandry <tmandry@google.com>
    Commit-Queue: Gary Miguel <garymm@google.com>
    Testability-Review: Gary Miguel <garymm@google.com>

--- build/copy_tree.py
+++ build/copy_tree.py
@@ -13,13 +13,19 @@ params = argparse.ArgumentParser(
 params.add_argument("source", type=Path)
 params.add_argument("target", type=Path)
 params.add_argument("stamp", type=Path)
+params.add_argument("--ignore_pattern", action="append")
 args = params.parse_args()
 
 if args.target.is_file():
     args.target.unlink()
 if args.target.is_dir():
     shutil.rmtree(args.target, ignore_errors=True)
-shutil.copytree(args.source, args.target, symlinks=True)
+
+ignore = None
+if args.ignore_pattern:
+    ignore = shutil.ignore_patterns(*args.ignore_pattern)
+
+shutil.copytree(args.source, args.target, symlinks=True, ignore=ignore)
 
 stamp = Path(str(args.stamp))
 stamp.touch()
