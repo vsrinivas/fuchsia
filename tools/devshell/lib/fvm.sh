04e39583e9d70b2b0b15b39dfadcebcfbfa04efd
David Pursell <dpursell@google.com>
2020-10-23 17:22:17 +0000

    [fvm] clean up raw FVM logic
    Stop building raw FVMs by default for NAND devices, and extract common
    raw NAND generation logic to a helper function with tests.
    
    We now attempt to generate the raw FVM from the sparse image before
    falling back to the fastboot image, since that should be widely
    available, and prevents us from having to write a bunch more code to
    convert from NAND FVM images. We could probably even stop using the
    fastboot FVM altogether, but it doesn't hurt to keep it in for
    flexibility in case future builds need it.
    
    Bug: 58896
    Change-Id: Ia2aa2b6cbd988da2fdbd86e88c7ad60608eb5430
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/441609
    Reviewed-by: Yecheng Zhao <zyecheng@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Testability-Review: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: David Pursell <dpursell@google.com>

--- tools/devshell/lib/fvm.sh
+++ tools/devshell/lib/fvm.sh
@@ -3,13 +3,22 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Creates an extended raw FVM image from a source FVM.
+#
 # Arguments:
-#   - Input fvm. The format can be raw or compressed.
+#   - Input FVM. The format can be raw or compressed.
 #   - output image file
-#   - (optional) desired minimum disk-size of the output fvm image, defaults to twice the
-#     uncompressed size of the given image. If the requested size is already smaller than the
-#     disk-size specified in the metadata, the disk-size will remain the same. It is guaranteed
-#     that the file size of the output image is the same as the disk-size in the metadata.
+#   - (optional) desired minimum disk-size of the output FVM image, defaults to
+#     twice the uncompressed size of the given image. If the requested size is
+#     already smaller than the disk-size specified in the metadata, the
+#     disk-size will remain the same. It is guaranteed that the file size of the
+#     output image is the same as the disk-size in the metadata.
+#
+# Output:
+#   (stderr) error logs on failure.
+#
+# Returns:
+#   0 on success, 1 on failure.
 function fx-fvm-extend-image {
   fvm_in=$1
   fvmimg=$2
@@ -36,12 +45,41 @@ function fx-fvm-extend-image {
     else
       newsize="${recommended_size}"
     fi
-    echo -n "Creating disk image..."
     "${HOST_OUT_DIR}/fvm" "${fvmimg}" extend --length "${newsize}" --length-is-lowerbound
-    echo "done"
   else
-    fx-error "Could not extend fvm, unable to stat fvm image"
+    fx-error "Could not extend FVM, unable to stat FVM image ${fvm_in}"
     return 1
   fi
   return 0
+}
+
+# Finds a source FVM to generate a raw FVM image from.
+#
+# The raw FVM is primarily used by the emulator, and is coverted from other FVM
+# formats on-demand. This locate the best source FVM to create the raw FVM from.
+#
+# The resulting path will generally be passed to fx-fvm-extend-image, but it's
+# useful to separate the functions since failing to find an FVM isn't usually
+# an error whereas failing to extend one is.
+#
+# Arguments:
+#   None
+#
+# Output:
+#   (stdout) path to a source FVM image if one was found, nothing otherwise.
+function fx-fvm-find-raw-source {
+  # Look for source FVM formats in this order. Every build that uses an FVM
+  # should produce at least one of these.
+  source_fvms=(
+    "${IMAGE_FVM_RAW}"
+    "${IMAGE_FVM_SPARSE}"
+    "${IMAGE_FVM_FASTBOOT}"
+  )
+
+  for source_fvm in "${source_fvms[@]}"; do
+    if [[ -n "${source_fvm}" && -f "${FUCHSIA_BUILD_DIR}/${source_fvm}" ]]; then
+      echo "${FUCHSIA_BUILD_DIR}/${source_fvm}"
+      return
+    fi
+  done
 }
