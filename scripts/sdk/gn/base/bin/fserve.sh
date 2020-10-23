df87cea134cc656d72e1c5c49a8f68199e4be179
Wayne Piekarski <waynepie@google.com>
2020-10-23 23:53:54 +0000

    [gn sdk] Remove bash -i from fserve.sh script to fix unexpected side effects
    The #!/bin/bash -i was added in fxr/410994 and resulted in some
    unexpected side effects. For example, femu.sh on OSX would suspend
    itself on startup, somehow due to the -i creating a login shell that
    changed the terminal properties. There were other problems with error
    messages appearing in the output, although this was fixed in fxr/414742.
    I suspect there were other bugs introduced by this as well.
    
    However, the bash -i was originally introduced to fix a problem where
    gsutils was not being found if in a non-system PATH location. Users will
    need to add gsutils to a system PATH directory, and cannot rely on a custom
    .bashrc or .bash_profile setting.
    
    Change-Id: I96d6921f83b99a10010dc596860c46861e3c0ef2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/440181
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    Testability-Review: Clayton Wilkinson <wilkinsonclay@google.com>
    Commit-Queue: Wayne Piekarski <waynepie@google.com>

--- scripts/sdk/gn/base/bin/fserve.sh
+++ scripts/sdk/gn/base/bin/fserve.sh
@@ -1,9 +1,4 @@
-#!/bin/bash -i
-# WARNING: THIS SCRIPT NEEDS -i
-# This causes this script to use an interactive shell, which is initialized
-# with the user's profile environment which is needed to find tools on
-# $PATH.
-
+#!/bin/bash
 # Copyright 2020 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
