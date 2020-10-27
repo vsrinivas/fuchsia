713e13d2fb365bbdb00f33c2e09e2599d87fbb4c
omer <omerlevran@google.com>
2020-10-27 16:10:16 +0000

    [licenses] - add missing licenses
    Change-Id: I0140d7690d4e4ef393570b60c7ca677a72df493a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/443263
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Testability-Review: Francois Rousseau <frousseau@google.com>
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>

--- scripts/status/lib/status.dart
+++ scripts/status/lib/status.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 export 'args_gn_collector.dart';
 export 'collector.dart';
 export 'env_reader.dart';
