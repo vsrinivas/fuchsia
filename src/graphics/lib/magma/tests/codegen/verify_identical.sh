a85fe96163529cc1bc6a5447e231dc443b6680d5
John Bauman <jbauman@google.com>
2020-10-27 21:15:08 +0000

    [magma] Print better error message when generated header doesn't match
    Print a cp command to update the file.
    
    Change-Id: I00f3f39da628a6c12a885d875c2365115c32464e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/443856
    Reviewed-by: Craig Stout <cstout@google.com>
    Testability-Review: Craig Stout <cstout@google.com>
    Commit-Queue: John Bauman <jbauman@google.com>

--- src/graphics/lib/magma/tests/codegen/verify_identical.sh
+++ src/graphics/lib/magma/tests/codegen/verify_identical.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-set -eo pipefail
+set -o pipefail
 
 usage() {
     echo "usage: ${0} {file} {reference} {output}"
@@ -13,7 +13,10 @@ usage() {
 
 diff ${1} ${2} > ${3}
 if [ "$?" -eq "1" ]; then
+    file_path=`realpath ${2}`
+    reference_path=`realpath ${1}`
     1>&2 echo "Error: file \"${1}\" does not match reference \"${2}\"."
-    1>&2 echo "\"${1}\" may need to be regenerated."
+    1>&2 echo "\"${2}\" may need to be regenerated, perhaps by running"
+    1>&2 echo "  cp ${reference_path} ${file_path}"
     exit -1
 fi
