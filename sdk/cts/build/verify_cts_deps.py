eabb3e3070f45a5a62b4a32f152e82d378cc0024
omer <omerlevran@google.com>
2020-10-27 00:34:49 +0000

    [cts] add missing license
    Change-Id: I4c2b03e21eec0189020fd043dfed8110429265f0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/443254
    Testability-Review: Francois Rousseau <frousseau@google.com>
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Reviewed-by: Jeremy Manson <jeremymanson@google.com>

--- sdk/cts/build/verify_cts_deps.py
+++ sdk/cts/build/verify_cts_deps.py
@@ -2,6 +2,7 @@
 # Copyright 2020 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 """A helper script for CTS dependency verification.
 
 The Compatibility Test Suite (CTS) has strict rules on GN dependencies. A CTS target may only depend
