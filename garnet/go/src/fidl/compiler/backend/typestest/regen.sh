9648fcaf73af3bba1920302e0ecb418efa9c8bf4
Yifei Teng <yifeit@google.com>
2020-10-29 18:52:03 +0000

    [fidlgen] Split goldens into subdirectories by backends
    ...in preparataion for creating two more hlcpp backend variations
    (domain objects, proxies and stubs).
    
    In the mean time, polish the shell script just a little bit:
    
    - Apply lint fixes suggested by the ShellCheck linter
    - Use `LC_ALL=C` to ensure stable sort ordering
    
    More fundamental improvements are punted since we have plans to replace
    this by GN completely and fidlgen_dart blocks some breaking changes.
    
    TEST: deleted goldens and manually re-ran the script from a few
    locations. Verified that the goldens output remains stable.
    
    Change-Id: Icbbf16b46f77b9cadb661dcc348bd18b627e7b84
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/443456
    Reviewed-by: Felix Zhu <fcz@google.com>
    Reviewed-by: Pascal Perez <pascallouis@google.com>
    Testability-Review: Felix Zhu <fcz@google.com>
    Testability-Review: Pascal Perez <pascallouis@google.com>
    Commit-Queue: Yifei Teng <yifeit@google.com>

--- garnet/go/src/fidl/compiler/backend/typestest/regen.sh
+++ garnet/go/src/fidl/compiler/backend/typestest/regen.sh
@@ -44,18 +44,21 @@ if [ ! -x "${FIDLGEN_SYZ}" ]; then
 fi
 
 EXAMPLE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-EXAMPLE_DIR="$( echo $EXAMPLE_DIR | sed -e "s+${FUCHSIA_DIR}/++" )"
-GOLDENS_DIR="${EXAMPLE_DIR}/../goldens"
+EXAMPLE_DIR="$( realpath "$EXAMPLE_DIR" )"
+GOLDENS_DIR="$( realpath "${EXAMPLE_DIR}/../goldens" )"
 FIDLC_IR_DIR="${FUCHSIA_DIR}/zircon/tools/fidl/goldens"
 GOLDENS=()
 
-# base all paths in FUCHSIA_DIR 
-cd "${FUCHSIA_DIR}"
-
 # fresh regen
 find "${GOLDENS_DIR}" -type f -not -name 'BUILD.gn' -exec rm {} \;
+find "${GOLDENS_DIR}"/* -maxdepth 0 -type d -exec rm -r {} \;
 
-for src_path in `find "${FIDLC_IR_DIR}" -name '*.test.json.golden'`; do
+# base all paths in GOLDENS_DIR
+mkdir -p "${GOLDENS_DIR}"
+cd "${GOLDENS_DIR}"
+
+while IFS= read -r -d '' src_path
+do
     src_name="$( basename "${src_path}" | sed -e 's/\.json\.golden$//g' )"
 
     # TODO(fxbug.dev/45006): Skipping due to issue with representation of binary
@@ -64,89 +67,112 @@ for src_path in `find "${FIDLC_IR_DIR}" -name '*.test.json.golden'`; do
         continue
     fi
 
-    json_name="${src_name}.json"
-    cpp_header_name=${json_name}.h
-    cpp_test_header_name=${json_name}_test_base.h
-    cpp_source_name=${json_name}.cc
-    llcpp_header_name=${json_name}.llcpp.h
-    llcpp_source_name=${json_name}.llcpp.cc
-    libfuzzer_header_name=${json_name}.libfuzzer.h
-    libfuzzer_source_name=${json_name}.libfuzzer.cc
-    go_impl_name=${json_name}.go
-    rust_name=${json_name}.rs
-    syzkaller_name=${json_name}.syz.txt
-
-    GOLDENS+=(
-      "$json_name",
-      "${cpp_header_name}.golden",
-      "${cpp_test_header_name}.golden",
-      "${cpp_source_name}.golden",
-      "${llcpp_header_name}.golden",
-      "${llcpp_source_name}.golden",
-      "${go_impl_name}.golden",
-      "${rust_name}.golden",
-    )
+    ensure_goldens_dir () {
+        local goldens_sub_dir="${GOLDENS_DIR}/$1"
+        mkdir -p "$goldens_sub_dir"
+        realpath "$goldens_sub_dir" --relative-to="$GOLDENS_DIR"
+    }
 
     echo -e "\033[1mexample: ${src_name}\033[0m"
 
+    # Note that each backend needs to have a unique suffix in the generated
+    # file names, because the generated files are collated into one flat
+    # directory and filtered by various fidlgen golden tests.
+    # See //garnet/go/src/fidl/compiler/backend/goldens/BUILD.gn
+
+    json_name="${src_name}.json"
     echo "  json ir: ${src_name} > ${json_name}"
-    cp "${src_path}" "${GOLDENS_DIR}/${json_name}"
+    GOLDENS_SUB_DIR="$(ensure_goldens_dir 'json')"
+    json_path="${GOLDENS_SUB_DIR}/$json_name"
+    cp "${src_path}" "$json_path"
+    GOLDENS+=(
+      "$json_path"
+    )
 
-    echo "  cpp: ${json_name} > ${cpp_header_name}, ${cpp_source_name}, and ${cpp_test_header_name}"
+    hlcpp_header_name=${json_name}.h
+    hlcpp_test_header_name=${json_name}_test_base.h
+    hlcpp_source_name=${json_name}.cc
+    echo "  hlcpp: ${json_name} > ${hlcpp_header_name}, ${hlcpp_source_name}, and ${hlcpp_test_header_name}"
+    GOLDENS_SUB_DIR="$(ensure_goldens_dir 'hlcpp')"
+    GOLDENS+=(
+      "${GOLDENS_SUB_DIR}/${hlcpp_header_name}.golden"
+      "${GOLDENS_SUB_DIR}/${hlcpp_test_header_name}.golden"
+      "${GOLDENS_SUB_DIR}/${hlcpp_source_name}.golden"
+    )
     ${FIDLGEN_HLCPP} \
-        -json "${GOLDENS_DIR}/${json_name}" \
-        -output-base "${GOLDENS_DIR}/${json_name}" \
-        -include-base "${GOLDENS_DIR}" \
+        -json "$json_path" \
+        -output-base "${GOLDENS_SUB_DIR}/${json_name}" \
+        -include-base "${GOLDENS_SUB_DIR}" \
         -clang-format-path "${PREBUILT_CLANG_DIR}/bin/clang-format"
-    mv "${GOLDENS_DIR}/${cpp_header_name}" "${GOLDENS_DIR}/${cpp_header_name}.golden"
-    mv "${GOLDENS_DIR}/${cpp_source_name}" "${GOLDENS_DIR}/${cpp_source_name}.golden"
-    mv "${GOLDENS_DIR}/${cpp_test_header_name}" "${GOLDENS_DIR}/${cpp_test_header_name}.golden"
+    mv "${GOLDENS_SUB_DIR}/${hlcpp_header_name}" "${GOLDENS_SUB_DIR}/${hlcpp_header_name}.golden"
+    mv "${GOLDENS_SUB_DIR}/${hlcpp_source_name}" "${GOLDENS_SUB_DIR}/${hlcpp_source_name}.golden"
+    mv "${GOLDENS_SUB_DIR}/${hlcpp_test_header_name}" "${GOLDENS_SUB_DIR}/${hlcpp_test_header_name}.golden"
 
+    llcpp_header_name=${json_name}.llcpp.h
+    llcpp_source_name=${json_name}.llcpp.cc
     echo "  llcpp: ${json_name} > ${llcpp_header_name} and ${llcpp_source_name}"
+    GOLDENS_SUB_DIR="$(ensure_goldens_dir 'llcpp')"
+    GOLDENS+=(
+      "${GOLDENS_SUB_DIR}/${llcpp_header_name}.golden"
+      "${GOLDENS_SUB_DIR}/${llcpp_source_name}.golden"
+    )
     ${FIDLGEN_LLCPP} \
-        -json "${GOLDENS_DIR}/${json_name}" \
-        -header "${GOLDENS_DIR}/${llcpp_header_name}" \
-        -source "${GOLDENS_DIR}/${llcpp_source_name}" \
-        -include-base "${GOLDENS_DIR}" \
+        -json "$json_path" \
+        -header "${GOLDENS_SUB_DIR}/${llcpp_header_name}" \
+        -source "${GOLDENS_SUB_DIR}/${llcpp_source_name}" \
+        -include-base "${GOLDENS_SUB_DIR}" \
         -clang-format-path "${PREBUILT_CLANG_DIR}/bin/clang-format"
-    mv "${GOLDENS_DIR}/${llcpp_header_name}" "${GOLDENS_DIR}/${llcpp_header_name}.golden"
-    mv "${GOLDENS_DIR}/${llcpp_source_name}" "${GOLDENS_DIR}/${llcpp_source_name}.golden"
+    mv "${GOLDENS_SUB_DIR}/${llcpp_header_name}" "${GOLDENS_SUB_DIR}/${llcpp_header_name}.golden"
+    mv "${GOLDENS_SUB_DIR}/${llcpp_source_name}" "${GOLDENS_SUB_DIR}/${llcpp_source_name}.golden"
 
     # libfuzzer expects at least one nonempty protocol definition or it will fail.
     # Add sources that contain a protocol with at least one method.
     # Remove libfuzzer golden files from a source that (no longer) contains a golden file.
     # The regex \[[^]] means a literal [ followed by anything except ].
+    libfuzzer_header_name=${json_name}.libfuzzer.h
+    libfuzzer_source_name=${json_name}.libfuzzer.cc
+    GOLDENS_SUB_DIR="$(ensure_goldens_dir 'libfuzzer')"
     if tr -d '[:space:]' < "${src_path}" | grep -q '"methods":\[[^]]' ; then
-        GOLDENS+=(
-            "${libfuzzer_header_name}.golden",
-            "${libfuzzer_source_name}.golden",
-        )
         echo "  libfuzzer: ${json_name} > ${libfuzzer_header_name}, and ${libfuzzer_source_name}"
+        GOLDENS+=(
+            "${GOLDENS_SUB_DIR}/${libfuzzer_header_name}.golden"
+            "${GOLDENS_SUB_DIR}/${libfuzzer_source_name}.golden"
+        )
         ${FIDLGEN_LIBFUZZER} \
-            -json "${GOLDENS_DIR}/${json_name}" \
-            -output-base "${GOLDENS_DIR}/${json_name}" \
-            -include-base "${GOLDENS_DIR}" \
+            -json "$json_path" \
+            -output-base "${GOLDENS_SUB_DIR}/${json_name}" \
+            -include-base "${GOLDENS_SUB_DIR}" \
             -clang-format-path "${PREBUILT_CLANG_DIR}/bin/clang-format"
-        mv "${GOLDENS_DIR}/${cpp_header_name}" "${GOLDENS_DIR}/${libfuzzer_header_name}.golden"
-        mv "${GOLDENS_DIR}/${cpp_source_name}" "${GOLDENS_DIR}/${libfuzzer_source_name}.golden"
+        mv "${GOLDENS_SUB_DIR}/${json_name}.h" "${GOLDENS_SUB_DIR}/${libfuzzer_header_name}.golden"
+        mv "${GOLDENS_SUB_DIR}/${json_name}.cc" "${GOLDENS_SUB_DIR}/${libfuzzer_source_name}.golden"
     else
-        if [[ -f "${GOLDENS_DIR}/${libfuzzer_header_name}.golden" ]]; then
-            rm "${GOLDENS_DIR}/${libfuzzer_header_name}.golden"
+        if [[ -f "${GOLDENS_SUB_DIR}/${libfuzzer_header_name}.golden" ]]; then
+            rm "${GOLDENS_SUB_DIR}/${libfuzzer_header_name}.golden"
         fi
-        if [[ -f "${GOLDENS_DIR}/${libfuzzer_source_name}.golden" ]]; then
-            rm "${GOLDENS_DIR}/${libfuzzer_source_name}.golden"
+        if [[ -f "${GOLDENS_SUB_DIR}/${libfuzzer_source_name}.golden" ]]; then
+            rm "${GOLDENS_SUB_DIR}/${libfuzzer_source_name}.golden"
         fi
     fi
 
+    go_impl_name=${json_name}.go
     echo "  go: ${json_name} > ${go_impl_name}"
+    GOLDENS_SUB_DIR="$(ensure_goldens_dir 'go')"
+    GOLDENS+=(
+      "${GOLDENS_SUB_DIR}/${go_impl_name}.golden"
+    )
     ${FIDLGEN_GO} \
-        -json "${GOLDENS_DIR}/${json_name}" \
-        -output-impl "${GOLDENS_DIR}/${go_impl_name}.golden"
+        -json "$json_path" \
+        -output-impl "${GOLDENS_SUB_DIR}/${go_impl_name}.golden"
 
+    rust_name=${json_name}.rs
     echo "  rust: ${json_name} > ${rust_name}"
+    GOLDENS_SUB_DIR="$(ensure_goldens_dir 'rust')"
+    GOLDENS+=(
+      "${GOLDENS_SUB_DIR}/${rust_name}.golden"
+    )
     ${FIDLGEN_RUST} \
-        -json "${GOLDENS_DIR}/${json_name}" \
-        -output-filename "${GOLDENS_DIR}/${rust_name}.golden" \
+        -json "$json_path" \
+        -output-filename "${GOLDENS_SUB_DIR}/${rust_name}.golden" \
         -rustfmt "${PREBUILT_RUST_TOOLS_DIR}/bin/rustfmt"
 
     # TODO(fxbug.dev/45007): Syzkaller does not support enum member references in struct
@@ -154,22 +180,25 @@ for src_path in `find "${FIDLC_IR_DIR}" -name '*.test.json.golden'`; do
     if [ "struct_default_value_enum_library_reference.test" = "${src_name}" ]; then
         continue
     fi
-    GOLDENS+=(
-      "${syzkaller_name}.golden",
-    )
+    syzkaller_name=${json_name}.syz.txt
     echo "  syzkaller: ${json_name} > ${syzkaller_name}"
+    GOLDENS_SUB_DIR="$(ensure_goldens_dir 'syzkaller')"
+    GOLDENS+=(
+      "${GOLDENS_SUB_DIR}/${syzkaller_name}.golden"
+    )
     ${FIDLGEN_SYZ} \
-        -json "${GOLDENS_DIR}/${json_name}" \
-        -output-syz "${GOLDENS_DIR}/${syzkaller_name}.golden"
-done
+        -json "$json_path" \
+        -output-syz "${GOLDENS_SUB_DIR}/${syzkaller_name}.golden"
+done <   <(find "${FIDLC_IR_DIR}" -name '*.test.json.golden' -print0)
 
-> "${GOLDENS_DIR}/goldens.txt"
-printf "%s\n" "${GOLDENS[@]//,}" | sort >> "${GOLDENS_DIR}/goldens.txt"
+# Set `LC_ALL=C` during sorting to make it locale-independent.
+true > "${GOLDENS_DIR}/goldens.txt"
+printf "%s\n" "${GOLDENS[@]//,}" | LC_ALL=C sort >> "${GOLDENS_DIR}/goldens.txt"
 
-> "${GOLDENS_DIR}/OWNERS"
+true > "${GOLDENS_DIR}/OWNERS"
 find "${EXAMPLE_DIR}/../.." \
-    "tools/fidl/fidlgen_"{go,hlcpp,rust,libfuzzer,syzkaller} \
+    "${FUCHSIA_DIR}/tools/fidl/fidlgen_"{go,hlcpp,rust,libfuzzer,syzkaller} \
     -name 'OWNERS' -exec cat {} \; \
-    | grep -v -e "^#" | awk 'NF' | sort -u > "${GOLDENS_DIR}/OWNERS"
+    | grep -v -e "^#" | awk 'NF' | LC_ALL=C sort -u > "${GOLDENS_DIR}/OWNERS"
 echo "" >> "${GOLDENS_DIR}/OWNERS"
 echo "# COMPONENT: FIDL>Testing" >> "${GOLDENS_DIR}/OWNERS"
