2a1f02de512d97a2141886451912f57325154286
James Sullivan <jfsulliv@google.com>
2020-10-30 20:34:15 +0000

    [blobfs][test] Fix BlobLoaderTest
    The test had a few problems:
    - The test was never exercising the actual fetching of blobs from disk,
      since the filesystem was not remounted after writing the test files so
      everything was in cache.
    - The test wasn't respecting compression settings since the generated
      files were random.
    
    Fixed both of these problems by remounting the filesystem and by
    generating "realistic" (i.e. compressible) data. Also refactored the
    tests to be clearer, simpler, and more defensive (e.g. making sure that
    compression settings were actually respected, where appropriate.)
    
    Bug: 63288
    Change-Id: I19cb2dd553c82cf48f3635c75a0b5d250d95cdc6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/445518
    Reviewed-by: Chris Suter <csuter@google.com>
    Testability-Review: Chris Suter <csuter@google.com>
    Commit-Queue: James Sullivan <jfsulliv@google.com>
    Fuchsia-Auto-Submit: James Sullivan <jfsulliv@google.com>

--- src/storage/blobfs/test/unit/blob-loader-test.cc
+++ src/storage/blobfs/test/unit/blob-loader-test.cc
@@ -36,59 +36,6 @@ using block_client::FakeBlockDevice;
 constexpr uint32_t kBlockSize = 512;
 constexpr uint32_t kNumBlocks = 400 * kBlobfsBlockSize / kBlockSize;
 
-// Returns a set of page-aligned addresses in [start, start+len)
-std::set<uint64_t> AddressRange(uint64_t start, uint64_t len) {
-  std::set<uint64_t> addresses;
-  for (uint64_t address = start; address < start + len; address += ZX_PAGE_SIZE) {
-    addresses.insert(address);
-  }
-  return addresses;
-}
-
-// FakeTransferBuffer is an implementation of TransferBuffer that uses a static backing buffer as
-// its data source (rather than a block device).
-class FakeTransferBuffer : public pager::TransferBuffer {
- public:
-  FakeTransferBuffer(const char* data, size_t len) : data_(new uint8_t[len], len) {
-    EXPECT_EQ(zx::vmo::create(len, 0, &vmo_), ZX_OK);
-    memcpy(data_.get(), data, len);
-  }
-
-  void AssertHasNoAddressesMapped() { ASSERT_EQ(mapped_addresses_.size(), 0ul); }
-
-  void AssertHasAddressesMapped(std::set<uint64_t> addresses) {
-    for (const auto& address : addresses) {
-      if (mapped_addresses_.find(address) == mapped_addresses_.end()) {
-        EXPECT_TRUE(false) << "Address " << address << " not mapped";
-      }
-    }
-  }
-
-  zx::status<> Populate(uint64_t offset, uint64_t length, const pager::UserPagerInfo& info) final {
-    if (offset % kBlobfsBlockSize != 0) {
-      return zx::error(ZX_ERR_INVALID_ARGS);
-    }
-    if (offset + length > data_.size()) {
-      return zx::error(ZX_ERR_OUT_OF_RANGE);
-    }
-    zx_status_t status;
-    if ((status = vmo_.write(data_.get() + offset, 0, length)) != ZX_OK) {
-      return zx::error(status);
-    }
-    for (const auto& address : AddressRange(offset, length)) {
-      mapped_addresses_.insert(address);
-    }
-    return zx::ok();
-  }
-
-  const zx::vmo& vmo() const final { return vmo_; }
-
- private:
-  zx::vmo vmo_;
-  fbl::Array<uint8_t> data_;
-  std::set<uint64_t> mapped_addresses_;
-};
-
 class BlobLoaderTest : public TestWithParam<CompressionAlgorithm> {
  public:
   void SetUp() override {
@@ -99,59 +46,40 @@ class BlobLoaderTest : public TestWithParam<CompressionAlgorithm> {
     ASSERT_EQ(FormatFilesystem(device.get(), FilesystemOptions{}), ZX_OK);
     loop_.StartThread();
 
-    MountOptions options = {.compression_settings = {
-                                .compression_algorithm = GetParam(),
-                            }};
-    ASSERT_EQ(Blobfs::Create(loop_.dispatcher(), std::move(device), options, zx::resource(), &fs_),
+    options_ = {.compression_settings = {
+                    .compression_algorithm = GetParam(),
+                }};
+    ASSERT_EQ(Blobfs::Create(loop_.dispatcher(), std::move(device), options_, zx::resource(), &fs_),
               ZX_OK);
 
     // Pre-seed with some random blobs.
     for (unsigned i = 0; i < 3; i++) {
-      AddRandomBlob(1024, nullptr);
+      AddBlob(1024);
     }
-    ASSERT_EQ(Sync(), ZX_OK);
+    ASSERT_EQ(Remount(), ZX_OK);
   }
 
-  FakeTransferBuffer& InitPager(std::unique_ptr<FakeTransferBuffer> buffer) {
-    FakeTransferBuffer& buffer_ref = *buffer;
-    auto status_or_pager = pager::UserPager::Create(std::move(buffer), fs_->Metrics());
-    EXPECT_TRUE(status_or_pager.is_ok());
-    pager_ = std::move(status_or_pager).value();
-    return buffer_ref;
+  // Remounts the filesystem, which ensures writes are flushed and caches are wiped.
+  zx_status_t Remount() {
+    return Blobfs::Create(loop_.dispatcher(), Blobfs::Destroy(std::move(fs_)), options_,
+                          zx::resource(), &fs_);
   }
 
-  BlobLoader CreateLoader() const {
-    auto* fs_ptr = fs_.get();
-
-    zx::status<BlobLoader> loader =
-        BlobLoader::Create(fs_ptr, fs_ptr, fs_->GetNodeFinder(), pager_.get(), fs_->Metrics());
-    EXPECT_EQ(loader.status_value(), ZX_OK);
-    // TODO(jfsulliv): Pessimizing move seems to be necessary, since otherwise fitx::result::value
-    // selects the const-ref variant and the (deleted) copy constructor of BlobLoader is invoked
-    // instead. Remove this pessimising move if possible.
-    return std::move(loader.value());
-  }
-
-  // Sync waits for blobfs to sync with the underlying block device.
-  zx_status_t Sync() {
-    sync_completion_t completion;
-    fs_->Sync([&completion](zx_status_t status) { sync_completion_signal(&completion); });
-    return sync_completion_wait(&completion, zx::duration::infinite().get());
-  }
-
-  // AddRandomBlob creates and writes a random blob to the file system.
-  // |out_info| is optional and is used to retrieve the created file information.
-  void AddRandomBlob(size_t sz, std::unique_ptr<BlobInfo>* out_info) {
+  // AddBlob creates and writes a blob of a specified size to the file system.
+  // The contents of the blob are compressible at a realistic level for a typical ELF binary.
+  // The returned BlobInfo describes the created blob, but its lifetime is unrelated to the lifetime
+  // of the on-disk blob.
+  [[maybe_unused]] std::unique_ptr<BlobInfo> AddBlob(size_t sz) {
     fbl::RefPtr<fs::Vnode> root;
-    ASSERT_EQ(fs_->OpenRootNode(&root), ZX_OK);
+    EXPECT_EQ(fs_->OpenRootNode(&root), ZX_OK);
     fs::Vnode* root_node = root.get();
 
     std::unique_ptr<BlobInfo> info;
-    GenerateRandomBlob("", sz, &info);
+    GenerateRealisticBlob("", sz, &info);
     memmove(info->path, info->path + 1, strlen(info->path));  // Remove leading slash.
 
     fbl::RefPtr<fs::Vnode> file;
-    ASSERT_EQ(root_node->Create(info->path, 0, &file), ZX_OK);
+    EXPECT_EQ(root_node->Create(info->path, 0, &file), ZX_OK);
 
     size_t actual;
     EXPECT_EQ(file->Truncate(info->size_data), ZX_OK);
@@ -159,12 +87,16 @@ class BlobLoaderTest : public TestWithParam<CompressionAlgorithm> {
     EXPECT_EQ(actual, info->size_data);
     EXPECT_EQ(file->Close(), ZX_OK);
 
-    if (out_info != nullptr) {
-      *out_info = std::move(info);
-    }
+    return info;
   }
 
+  BlobLoader& loader() { return fs_->loader(); }
+
   Blobfs* Fs() const { return fs_.get(); }
+
+  CompressionAlgorithm ExpectedAlgorithm() const {
+    return options_.compression_settings.compression_algorithm;
+  }
 
   uint32_t LookupInode(const BlobInfo& info) {
     Digest digest;
@@ -175,12 +107,21 @@ class BlobLoaderTest : public TestWithParam<CompressionAlgorithm> {
     return vnode->Ino();
   }
 
+  CompressionAlgorithm LookupCompression(const BlobInfo& info) {
+    Digest digest;
+    fbl::RefPtr<CacheNode> node;
+    EXPECT_EQ(digest.Parse(info.path), ZX_OK);
+    EXPECT_EQ(fs_->Cache().Lookup(digest, &node), ZX_OK);
+    auto vnode = fbl::RefPtr<Blob>::Downcast(std::move(node));
+    auto algorithm_or = AlgorithmForInode(vnode->GetNode());
+    EXPECT_TRUE(algorithm_or.is_ok());
+    return algorithm_or.value();
+  }
+
  protected:
   std::unique_ptr<Blobfs> fs_;
-  std::unique_ptr<FakeTransferBuffer> buffer_;
-  std::unique_ptr<pager::UserPager> pager_;
-  BlobLoader loader_;
   async::Loop loop_{&kAsyncLoopConfigNoAttachToCurrentThread};
+  MountOptions options_;
 };
 
 // A seperate parameterized test fixture that will only be run with compression algorithms that
@@ -189,14 +130,11 @@ using BlobLoaderPagedTest = BlobLoaderTest;
 
 TEST_P(BlobLoaderTest, NullBlob) {
   size_t blob_len = 0;
-  std::unique_ptr<BlobInfo> info;
-  AddRandomBlob(blob_len, &info);
-  ASSERT_EQ(Sync(), ZX_OK);
-
-  BlobLoader loader = CreateLoader();
+  std::unique_ptr<BlobInfo> info = AddBlob(blob_len);
+  ASSERT_EQ(Remount(), ZX_OK);
 
   fzl::OwnedVmoMapper data, merkle;
-  ASSERT_EQ(loader.LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
+  ASSERT_EQ(loader().LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
 
   EXPECT_FALSE(data.vmo().is_valid());
   EXPECT_EQ(data.size(), 0ul);
@@ -207,14 +145,13 @@ TEST_P(BlobLoaderTest, NullBlob) {
 
 TEST_P(BlobLoaderTest, SmallBlob) {
   size_t blob_len = 1024;
-  std::unique_ptr<BlobInfo> info;
-  AddRandomBlob(blob_len, &info);
-  ASSERT_EQ(Sync(), ZX_OK);
-
-  BlobLoader loader = CreateLoader();
+  std::unique_ptr<BlobInfo> info = AddBlob(blob_len);
+  ASSERT_EQ(Remount(), ZX_OK);
+  // We explicitly don't check the compression algorithm was respected here, since files this small
+  // don't need to be compressed.
 
   fzl::OwnedVmoMapper data, merkle;
-  ASSERT_EQ(loader.LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
+  ASSERT_EQ(loader().LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
 
   ASSERT_TRUE(data.vmo().is_valid());
   ASSERT_GE(data.size(), info->size_data);
@@ -226,27 +163,22 @@ TEST_P(BlobLoaderTest, SmallBlob) {
 
 TEST_P(BlobLoaderPagedTest, SmallBlob) {
   size_t blob_len = 1024;
-  std::unique_ptr<BlobInfo> info;
-  AddRandomBlob(blob_len, &info);
-  ASSERT_EQ(Sync(), ZX_OK);
-
-  auto buffer_ptr = std::make_unique<FakeTransferBuffer>(info->data.get(), info->size_data);
-  FakeTransferBuffer& buffer = InitPager(std::move(buffer_ptr));
-  BlobLoader loader = CreateLoader();
+  std::unique_ptr<BlobInfo> info = AddBlob(blob_len);
+  ASSERT_EQ(Remount(), ZX_OK);
+  // We explicitly don't check the compression algorithm was respected here, since files this small
+  // don't need to be compressed.
 
   fzl::OwnedVmoMapper data, merkle;
   std::unique_ptr<pager::PageWatcher> page_watcher;
-  ASSERT_EQ(loader.LoadBlobPaged(LookupInode(*info), nullptr, &page_watcher, &data, &merkle),
+  ASSERT_EQ(loader().LoadBlobPaged(LookupInode(*info), nullptr, &page_watcher, &data, &merkle),
             ZX_OK);
 
   ASSERT_TRUE(data.vmo().is_valid());
   ASSERT_GE(data.size(), info->size_data);
-  buffer.AssertHasNoAddressesMapped();
   // Use vmo::read instead of direct read so that we can synchronously fail if the pager fails.
   fbl::Array<uint8_t> buf(new uint8_t[blob_len], blob_len);
   ASSERT_EQ(data.vmo().read(buf.get(), 0, blob_len), ZX_OK);
   EXPECT_EQ(memcmp(buf.get(), info->data.get(), info->size_data), 0);
-  buffer.AssertHasAddressesMapped({0ul});
 
   EXPECT_FALSE(merkle.vmo().is_valid());
   EXPECT_EQ(info->size_merkle, 0ul);
@@ -254,14 +186,12 @@ TEST_P(BlobLoaderPagedTest, SmallBlob) {
 
 TEST_P(BlobLoaderTest, LargeBlob) {
   size_t blob_len = 1 << 18;
-  std::unique_ptr<BlobInfo> info;
-  AddRandomBlob(blob_len, &info);
-  ASSERT_EQ(Sync(), ZX_OK);
-
-  BlobLoader loader = CreateLoader();
+  std::unique_ptr<BlobInfo> info = AddBlob(blob_len);
+  ASSERT_EQ(Remount(), ZX_OK);
+  ASSERT_EQ(LookupCompression(*info), ExpectedAlgorithm());
 
   fzl::OwnedVmoMapper data, merkle;
-  ASSERT_EQ(loader.LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
+  ASSERT_EQ(loader().LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
 
   ASSERT_TRUE(data.vmo().is_valid());
   ASSERT_GE(data.size(), info->size_data);
@@ -274,14 +204,12 @@ TEST_P(BlobLoaderTest, LargeBlob) {
 
 TEST_P(BlobLoaderTest, LargeBlobWithNonAlignedLength) {
   size_t blob_len = (1 << 18) - 1;
-  std::unique_ptr<BlobInfo> info;
-  AddRandomBlob(blob_len, &info);
-  ASSERT_EQ(Sync(), ZX_OK);
-
-  BlobLoader loader = CreateLoader();
+  std::unique_ptr<BlobInfo> info = AddBlob(blob_len);
+  ASSERT_EQ(Remount(), ZX_OK);
+  ASSERT_EQ(LookupCompression(*info), ExpectedAlgorithm());
 
   fzl::OwnedVmoMapper data, merkle;
-  ASSERT_EQ(loader.LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
+  ASSERT_EQ(loader().LoadBlob(LookupInode(*info), nullptr, &data, &merkle), ZX_OK);
 
   ASSERT_TRUE(data.vmo().is_valid());
   ASSERT_GE(data.size(), info->size_data);
@@ -294,27 +222,21 @@ TEST_P(BlobLoaderTest, LargeBlobWithNonAlignedLength) {
 
 TEST_P(BlobLoaderPagedTest, LargeBlob) {
   size_t blob_len = 1 << 18;
-  std::unique_ptr<BlobInfo> info;
-  AddRandomBlob(blob_len, &info);
-  ASSERT_EQ(Sync(), ZX_OK);
-
-  auto buffer_ptr = std::make_unique<FakeTransferBuffer>(info->data.get(), info->size_data);
-  FakeTransferBuffer& buffer = InitPager(std::move(buffer_ptr));
-  BlobLoader loader = CreateLoader();
+  std::unique_ptr<BlobInfo> info = AddBlob(blob_len);
+  ASSERT_EQ(Remount(), ZX_OK);
+  ASSERT_EQ(LookupCompression(*info), ExpectedAlgorithm());
 
   fzl::OwnedVmoMapper data, merkle;
   std::unique_ptr<pager::PageWatcher> page_watcher;
-  ASSERT_EQ(loader.LoadBlobPaged(LookupInode(*info), nullptr, &page_watcher, &data, &merkle),
+  ASSERT_EQ(loader().LoadBlobPaged(LookupInode(*info), nullptr, &page_watcher, &data, &merkle),
             ZX_OK);
 
   ASSERT_TRUE(data.vmo().is_valid());
   ASSERT_GE(data.size(), info->size_data);
-  buffer.AssertHasNoAddressesMapped();
   // Use vmo::read instead of direct read so that we can synchronously fail if the pager fails.
   fbl::Array<uint8_t> buf(new uint8_t[blob_len], blob_len);
   ASSERT_EQ(data.vmo().read(buf.get(), 0, blob_len), ZX_OK);
   EXPECT_EQ(memcmp(buf.get(), info->data.get(), info->size_data), 0);
-  buffer.AssertHasAddressesMapped(AddressRange(0, 1 << 18));
 
   ASSERT_TRUE(merkle.vmo().is_valid());
   ASSERT_GE(merkle.size(), info->size_merkle);
@@ -323,27 +245,21 @@ TEST_P(BlobLoaderPagedTest, LargeBlob) {
 
 TEST_P(BlobLoaderPagedTest, LargeBlobWithNonAlignedLength) {
   size_t blob_len = (1 << 18) - 1;
-  std::unique_ptr<BlobInfo> info;
-  AddRandomBlob(blob_len, &info);
-  ASSERT_EQ(Sync(), ZX_OK);
-
-  auto buffer_ptr = std::make_unique<FakeTransferBuffer>(info->data.get(), info->size_data);
-  FakeTransferBuffer& buffer = InitPager(std::move(buffer_ptr));
-  BlobLoader loader = CreateLoader();
+  std::unique_ptr<BlobInfo> info = AddBlob(blob_len);
+  ASSERT_EQ(Remount(), ZX_OK);
+  ASSERT_EQ(LookupCompression(*info), ExpectedAlgorithm());
 
   fzl::OwnedVmoMapper data, merkle;
   std::unique_ptr<pager::PageWatcher> page_watcher;
-  ASSERT_EQ(loader.LoadBlobPaged(LookupInode(*info), nullptr, &page_watcher, &data, &merkle),
+  ASSERT_EQ(loader().LoadBlobPaged(LookupInode(*info), nullptr, &page_watcher, &data, &merkle),
             ZX_OK);
 
   ASSERT_TRUE(data.vmo().is_valid());
   ASSERT_GE(data.size(), info->size_data);
-  buffer.AssertHasNoAddressesMapped();
   // Use vmo::read instead of direct read so that we can synchronously fail if the pager fails.
   fbl::Array<uint8_t> buf(new uint8_t[blob_len], blob_len);
   ASSERT_EQ(data.vmo().read(buf.get(), 0, blob_len), ZX_OK);
   EXPECT_EQ(memcmp(buf.get(), info->data.get(), info->size_data), 0);
-  buffer.AssertHasAddressesMapped(AddressRange(0, blob_len));
 
   ASSERT_TRUE(merkle.vmo().is_valid());
   ASSERT_GE(merkle.size(), info->size_merkle);
@@ -378,9 +294,8 @@ constexpr std::array<CompressionAlgorithm, 4> kCompressionAlgorithms = {
     CompressionAlgorithm::CHUNKED,
 };
 
-constexpr std::array<CompressionAlgorithm, 3> kPagingCompressionAlgorithms = {
+constexpr std::array<CompressionAlgorithm, 2> kPagingCompressionAlgorithms = {
     CompressionAlgorithm::UNCOMPRESSED,
-    CompressionAlgorithm::ZSTD_SEEKABLE,
     CompressionAlgorithm::CHUNKED,
 };
 
