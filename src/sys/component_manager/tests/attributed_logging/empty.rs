1c263dfc1f0b548c39d831c0eb2006483397b9e8
Justin Mattson <jmatt@google.com>
2020-10-31 07:05:27 +0000

    [components] Add logging integration test
    Verify that when there is a component which uses a LogSink
    we see a capability route for it. Since the component never tries
    to use the LogSink itself we assume this is because component
    manager requested the capability on behalf of the component.
    
    Bug: 63000
    
    Change-Id: Ieca62a125d127948f78f3394b72b79271222420d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/445635
    Reviewed-by: Adam Lesinski <adamlesinski@google.com>
    Testability-Review: Adam Lesinski <adamlesinski@google.com>
    Commit-Queue: Justin Mattson <jmatt@google.com>

---
+++ src/sys/component_manager/tests/attributed_logging/empty.rs
@@ -0,0 +1,6 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is goverened by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Program for a completely empty component
+fn main() {}
---
+++ src/sys/component_manager/tests/attributed_logging/integration_test.rs
@@ -0,0 +1,51 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    fuchsia_async as fasync,
+    test_utils_lib::{
+        events::{self, Event},
+        matcher::EventMatcher,
+        opaque_test::OpaqueTestBuilder,
+        sequence::{self, EventSequence},
+    },
+};
+
+#[fasync::run_singlethreaded(test)]
+/// Verifies that when a component has a LogSink in its namespace that the
+/// component manager tries to connect to this.
+async fn check_logsink_requested() {
+    let test_env =
+        OpaqueTestBuilder::new("fuchsia-pkg://fuchsia.com/attributed-logging-test#meta/root.cm")
+            .component_manager_url(
+                "fuchsia-pkg://fuchsia.com/attributed-logging-test#meta/component-manager.cmx",
+            )
+            .config("/pkg/data/cm_config")
+            .build()
+            .await
+            .expect("failed to construct OpaqueTest");
+
+    let mut event_source = test_env
+        .connect_to_event_source()
+        .await
+        .expect("could not connect to event source for opaque test");
+
+    let expected = EventSequence::new()
+        .all_of(
+            vec![
+                EventMatcher::ok()
+                    .r#type(events::CapabilityRouted::TYPE)
+                    .capability_id("fuchsia.logger.LogSink")
+                    .moniker("/empty_child:0"),
+                EventMatcher::ok().r#type(events::Stopped::TYPE).moniker("/empty_child:0"),
+            ],
+            sequence::Ordering::Ordered,
+        )
+        .subscribe_and_expect(&mut event_source)
+        .await
+        .unwrap();
+
+    event_source.start_component_tree().await;
+    expected.await.unwrap();
+}
