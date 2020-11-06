882295ed88c7a3512906bb1dcafe50dad566a202
John Bauman <jbauman@google.com>
2020-11-06 19:31:42 +0000

    [sysmem] Set metadata for sysmem
    Otherwise sysmem asserts because it doesn't have a way to rank different
    formats against each other.
    
    Change-Id: Id1e38206b9b2ae50d8f7d161224b4d447ae181e7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/447524
    Reviewed-by: John Rosasco <rosasco@google.com>
    Testability-Review: John Rosasco <rosasco@google.com>
    Testability-Review: John Bauman <jbauman@google.com>
    Commit-Queue: John Bauman <jbauman@google.com>

--- src/devices/sysmem/tests/sysmem/fuzz/sysmem_fuzz_common.cc
+++ src/devices/sysmem/tests/sysmem/fuzz/sysmem_fuzz_common.cc
@@ -3,6 +3,10 @@
 // found in the LICENSE file.
 #include "sysmem_fuzz_common.h"
 
+#include <zircon/device/sysmem.h>
+
+#include <ddk/platform-defs.h>
+
 #include "log_rtn.h"
 
 FakeDdkSysmem::~FakeDdkSysmem() {
@@ -22,6 +26,15 @@ bool FakeDdkSysmem::Init() {
     fflush(stderr);
     return false;
   }
+  // Pick a platform where AFBC textures will be used.
+  static const sysmem_metadata_t metadata{
+      .vid = PDEV_VID_AMLOGIC,
+      .pid = PDEV_PID_AMLOGIC_S905D2,
+      .protected_memory_size = 0,
+      .contiguous_memory_size = 0,
+  };
+  ddk_.SetMetadata(&metadata, sizeof(metadata));
+
   fbl::Array<fake_ddk::ProtocolEntry> protocols(new fake_ddk::ProtocolEntry[2], 2);
   protocols[0] = {ZX_PROTOCOL_PBUS, *reinterpret_cast<const fake_ddk::Protocol*>(pbus_.proto())};
   protocols[1] = {ZX_PROTOCOL_PDEV, *reinterpret_cast<const fake_ddk::Protocol*>(pdev_.proto())};
