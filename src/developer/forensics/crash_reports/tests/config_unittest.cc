fb41cf6a8c3523f6c2c4d703f40c21b7d65d7306
Alex Pankhurst <pankhurst@google.com>
2020-11-11 18:37:09 +0000

    [crash_reports] Clean up config tests
    The tests were opaque becuase the config files being tested were defined
    outside of the tests in a separate directory.
    
    Bug: 62362
    
    TESTED=`fx test crash-reports-tests`
    
    Change-Id: I0368d3c96129ea0595b6c9e70e7ad7fbcdbaf304
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/449015
    Commit-Queue: Alex Pankhurst <pankhurst@google.com>
    Reviewed-by: Francois Rousseau <frousseau@google.com>
    Testability-Review: Francois Rousseau <frousseau@google.com>
    Testability-Review: Alex Pankhurst <pankhurst@google.com>

--- src/developer/forensics/crash_reports/tests/config_unittest.cc
+++ src/developer/forensics/crash_reports/tests/config_unittest.cc
@@ -4,88 +4,152 @@
 
 #include "src/developer/forensics/crash_reports/config.h"
 
+#include <lib/syslog/cpp/macros.h>
 #include <zircon/errors.h>
 
 #include <gtest/gtest.h>
 
+#include "src/lib/files/scoped_temp_dir.h"
+
 namespace forensics {
 namespace crash_reports {
 namespace {
-
-#define UNWRAP_OR_ASSERT(var, expr)   \
-  auto tmp_##var = expr;              \
-  ASSERT_TRUE(tmp_##var.has_value()); \
-  auto var = std::move(tmp_##var.value());
 
 constexpr CrashServerConfig::UploadPolicy kDisabled = CrashServerConfig::UploadPolicy::DISABLED;
 constexpr CrashServerConfig::UploadPolicy kEnabled = CrashServerConfig::UploadPolicy::ENABLED;
 constexpr CrashServerConfig::UploadPolicy kReadFromPrivacySettings =
     CrashServerConfig::UploadPolicy::READ_FROM_PRIVACY_SETTINGS;
 
-TEST(ConfigTest, ParseConfig_ValidConfig_UploadDisabled) {
-  UNWRAP_OR_ASSERT(config, ParseConfig("/pkg/data/configs/valid_upload_disabled.json"));
+class ConfigTest : public testing::Test {
+ protected:
+  std::string WriteConfig(const std::string& config) {
+    std::string path;
+    FX_CHECK(tmp_dir_.NewTempFileWithData(config, &path));
+    return path;
+  }
+
+ private:
+  files::ScopedTempDir tmp_dir_;
+};
+
+// Parse |config_str| into |var| or assert if the operation fails.
+#define PARSE_OR_ASSERT(var, config_str)                 \
+  auto tmp_##var = ParseConfig(WriteConfig(config_str)); \
+  ASSERT_TRUE(tmp_##var.has_value());                    \
+  auto var = std::move(tmp_##var.value());
+
+// Parse |config_str| and assert it is malformed.
+#define ASSERT_IS_BAD_CONFIG(config_str) \
+  ASSERT_FALSE(ParseConfig(WriteConfig(config_str)).has_value());
+
+TEST_F(ConfigTest, ParseConfig_ValidConfig_UploadDisabled) {
+  PARSE_OR_ASSERT(config, R"({
+    "crash_server" : {
+        "upload_policy": "disabled"
+    }
+})");
   EXPECT_EQ(config.crash_server.upload_policy, kDisabled);
   EXPECT_EQ(config.crash_server.url, nullptr);
 }
 
-TEST(ConfigTest, ParseConfig_ValidConfig_UploadEnabled) {
-  UNWRAP_OR_ASSERT(config, ParseConfig("/pkg/data/configs/valid_upload_enabled.json"));
+TEST_F(ConfigTest, ParseConfig_ValidConfig_UploadEnabled) {
+  PARSE_OR_ASSERT(config, R"({
+    "crash_server" : {
+        "upload_policy": "enabled",
+        "url": "http://localhost:1234"
+    }
+})");
   EXPECT_EQ(config.crash_server.upload_policy, kEnabled);
   EXPECT_EQ(*config.crash_server.url, "http://localhost:1234");
 }
 
-TEST(ConfigTest, ParseConfig_ValidConfig_UploadReadFromPrivacySettings) {
-  UNWRAP_OR_ASSERT(config,
-                   ParseConfig("/pkg/data/configs/valid_upload_read_from_privacy_settings.json"));
+TEST_F(ConfigTest, ParseConfig_ValidConfig_UploadReadFromPrivacySettings) {
+  PARSE_OR_ASSERT(config, R"({
+    "crash_server" : {
+        "upload_policy": "read_from_privacy_settings",
+        "url": "http://localhost:1234"
+    }
+})");
   EXPECT_EQ(config.crash_server.upload_policy, kReadFromPrivacySettings);
   EXPECT_EQ(*config.crash_server.url, "http://localhost:1234");
 }
 
-TEST(ConfigTest, ParseConfig_ValidConfig_UploadDisabledServerUrlIgnored) {
-  UNWRAP_OR_ASSERT(config,
-                   ParseConfig("/pkg/data/configs/valid_upload_disabled_spurious_server.json"));
+TEST_F(ConfigTest, ParseConfig_ValidConfig_UploadDisabledServerUrlIgnored) {
+  PARSE_OR_ASSERT(config, R"({
+    "crash_server" : {
+        "upload_policy": "disabled",
+        "url": "http://localhost:1234"
+    }
+})");
   EXPECT_EQ(config.crash_server.upload_policy, kDisabled);
   // Even though a URL is set in the config file, we check that it is not set in the struct.
   EXPECT_EQ(config.crash_server.url, nullptr);
 }
 
-TEST(ConfigTest, ParseConfig_HasDailyPerProductQuota) {
-  UNWRAP_OR_ASSERT(config, ParseConfig("/pkg/data/configs/has_quota.json"));
+TEST_F(ConfigTest, ParseConfig_HasDailyPerProductQuota) {
+  PARSE_OR_ASSERT(config, R"({
+    "crash_reporter": {
+        "daily_per_product_quota": 100
+    },
+    "crash_server" : {
+        "upload_policy": "enabled",
+        "url": "http://localhost:1234"
+    }
+})");
   ASSERT_TRUE(config.daily_per_product_quota.has_value());
   EXPECT_EQ(config.daily_per_product_quota.value(), 100u);
 }
 
-TEST(ConfigTest, ParseConfig_MissingDailyPerProductQuota) {
-  UNWRAP_OR_ASSERT(config, ParseConfig("/pkg/data/configs/missing_quota.json"));
+TEST_F(ConfigTest, ParseConfig_MissingDailyPerProductQuota) {
+  PARSE_OR_ASSERT(config, R"({
+    "crash_server" : {
+        "upload_policy": "enabled",
+        "url": "http://localhost:1234"
+    }
+})");
   EXPECT_FALSE(config.daily_per_product_quota.has_value());
 }
 
-TEST(ConfigTest, ParseConfig_MissingConfig) {
+TEST_F(ConfigTest, ParseConfig_MissingConfig) {
   ASSERT_FALSE(ParseConfig("undefined file").has_value());
 }
 
-TEST(ConfigTest, ParseConfig_BadConfig_SpuriousField) {
-  ASSERT_FALSE(ParseConfig("/pkg/data/configs/bad_schema_spurious_field.json").has_value());
+TEST_F(ConfigTest, ParseConfig_BadConfig_SpuriousField) {
+  ASSERT_IS_BAD_CONFIG(R"({
+    "crash_server" : {
+        "upload_policy": "disabled"
+    },
+    "spurious field": []
+})");
 }
 
-TEST(ConfigTest, ParseConfig_BadConfig_MissingRequiredField) {
-  ASSERT_FALSE(ParseConfig("/pkg/data/configs/bad_schema_missing_required_field.json").has_value());
+TEST_F(ConfigTest, ParseConfig_BadConfig_MissingRequiredField) {
+  ASSERT_IS_BAD_CONFIG(R"({
+})");
 }
 
-TEST(ConfigTest, ParseConfig_BadConfig_MissingServerUrlWithUploadEnabled) {
-  ASSERT_FALSE(
-      ParseConfig("/pkg/data/configs/bad_schema_missing_server_upload_enabled.json").has_value());
+TEST_F(ConfigTest, ParseConfig_BadConfig_MissingServerUrlWithUploadEnabled) {
+  ASSERT_IS_BAD_CONFIG(R"({
+    "crash_server" : {
+        "upload_policy": "enabled"
+    }
+})");
 }
 
-TEST(ConfigTest, ParseConfig_BadConfig_MissingServerUrlWithUploadReadFromPrivacySettings) {
-  ASSERT_FALSE(
-      ParseConfig(
-          "/pkg/data/configs/bad_schema_missing_server_upload_read_from_privacy_settings.json")
-          .has_value());
+TEST_F(ConfigTest, ParseConfig_BadConfig_MissingServerUrlWithUploadReadFromPrivacySettings) {
+  ASSERT_IS_BAD_CONFIG(R"({
+    "crash_server" : {
+        "upload_policy": "read_from_privacy_settings"
+    }
+})");
 }
 
-TEST(ConfigTest, ParseConfig_BadConfig_InvalidUploadPolicy) {
-  ASSERT_FALSE(ParseConfig("/pkg/data/configs/bad_schema_invalid_upload_policy.json").has_value());
+TEST_F(ConfigTest, ParseConfig_BadConfig_InvalidUploadPolicy) {
+  ASSERT_IS_BAD_CONFIG(R"({
+    "crash_server" : {
+        "upload_policy": "not_in_enum"
+    }
+})");
 }
 
 }  // namespace
@@ -95,6 +159,9 @@ TEST(ConfigTest, ParseConfig_BadConfig_InvalidUploadPolicy) {
 void PrintTo(const CrashServerConfig::UploadPolicy& upload_policy, std::ostream* os) {
   *os << ToString(upload_policy);
 }
+
+#undef ASSERT_IS_BAD_CONFIG
+#undef PARSE_OR_ASSERT
 
 }  // namespace crash_reports
 }  // namespace forensics
