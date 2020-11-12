c27a634dd979e480f5216aea8da6d7c11fe54348
Joshua Seaton <joshuaseaton@google.com>
2020-11-12 21:20:23 +0000

    [kernel] Work around lack of guard in <cpuid.h>
    The compiler-supplied header currently lacks a multiple inclusion guard
    for both Clang and GCC. Until the issue is resolved, we override its
    inclusion with a simple, guarded wrapper.
    
    Bug: 64109
    Change-Id: I5d3ffdc63066f2e5cf42853c35b88b5a994601b2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/449617
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Venkatesh Srinivas <venkateshs@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>

---
+++ zircon/kernel/arch/x86/include/cpuid.h
@@ -0,0 +1,13 @@
+// Copyright 2020 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_ARCH_X86_INCLUDE_CPUID_H_
+#define ZIRCON_KERNEL_ARCH_X86_INCLUDE_CPUID_H_
+
+// TODO(fxbug.dev/64109): <cpuid.h> lacks a proper multiple inclusion guard!
+#include_next <cpuid.h>
+
+#endif  // ZIRCON_KERNEL_ARCH_X86_INCLUDE_CPUID_H_
