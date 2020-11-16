vsrinivas/fuchsia

commit 779c5284a33b9f397b1f93e746054e71c683379f

[netstack] Include link header in packetbuffer to stack
Before this change, the link layer headers were not visible to the
`stack.Stack`. The stack may want to look at the link headers so
this change makes sure to not strip them away.

Change-Id: I5aa3232a0f0387af59f36baa0d127dd1f2500878
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/447646
Reviewed-by: Tamir Duberstein <tamird@google.com>
Testability-Review: Tamir Duberstein <tamird@google.com>
Commit-Queue: Ghanan Gowripalan <ghanan@google.com>

src/connectivity/network/netstack/filter/endpoint.go
@@ -8,6 +8,7 @@
 package filter
 
 import (
+	"fmt"
 	"sync/atomic"
 
 	"go.fuchsia.dev/fuchsia/src/connectivity/network/netstack/packetbuffer"
@@ -57,21 +58,40 @@ func (e *Endpoint) DeliverNetworkPacket(dstLinkAddr, srcLinkAddr tcpip.LinkAddre
 		return
 	}
 
-	// The filter expects the packet's header to be in the packet buffer's header.
+	// The filter expects the packet's header to be in a single view.
 	//
-	// Since we are delivering the packet to a NetworkDispatcher, we do not need
-	// to allocate bytes for a LinkEndpoint's header.
+	// Since we are delivering the packet to a NetworkDispatcher, we know only the
+	// link header may be set - the rest of the packet will be in Data.
+	//
+	// Note, the ethernet and netdevice clients always provide a packet buffer
+	// that was created with a single view, so we will not incur any allocations
+	// or copies below when a packet is received from those clients.
 	//
 	// TODO(fxbug.dev/50424): Support using a buffer.VectorisedView when parsing packets
 	// so we don't need to create a single view here.
-	hdr := packetbuffer.ToView(pkt)
-	if e.filter.Run(Incoming, protocol, hdr, buffer.VectorisedView{}) != Pass {
+	if len(pkt.Data.Views()) == 0 {
+		panic("expected to receive a packet buffer with at least 1 remaining view in Data")
+	}
+
+	if len(pkt.Data.Views()) != 1 {
+		// If we have more than 1 view in Data, combine them into a single view.
+		// Note, the stack may be interested in the link headers so make sure to
+		// not throw it away.
+		linkHdrLen := len(pkt.LinkHeader().View())
+		pkt = stack.NewPacketBuffer(stack.PacketBufferOptions{
+			Data: packetbuffer.ToView(pkt).ToVectorisedView(),
+		})
+		_, ok := pkt.LinkHeader().Consume(linkHdrLen)
+		if !ok {
+			panic(fmt.Sprintf("failed to consume %d bytes for the link header", linkHdrLen))
+		}
+	}
+
+	if e.filter.Run(Incoming, protocol, pkt.Data.Views()[0], buffer.VectorisedView{}) != Pass {
 		return
 	}
 
-	e.Endpoint.DeliverNetworkPacket(dstLinkAddr, srcLinkAddr, protocol, stack.NewPacketBuffer(stack.PacketBufferOptions{
-		Data: hdr.ToVectorisedView(),
-	}))
+	e.Endpoint.DeliverNetworkPacket(dstLinkAddr, srcLinkAddr, protocol, pkt)
 }
 
 // WritePacket implements stack.LinkEndpoint.

src/connectivity/network/netstack/link/eth/endpoint.go
@@ -43,12 +43,11 @@ func (e *endpoint) WritePackets(r *stack.Route, gso *stack.GSO, pkts stack.Packe
 }
 
 func (e *endpoint) DeliverNetworkPacket(dstLinkAddr, srcLinkAddr tcpip.LinkAddress, protocol tcpip.NetworkProtocolNumber, pkt *stack.PacketBuffer) {
-	ethBytes, ok := pkt.Data.PullUp(header.EthernetMinimumSize)
+	ethBytes, ok := pkt.LinkHeader().Consume(header.EthernetMinimumSize)
 	if !ok {
 		// TODO(fxbug.dev/42949): record this in statistics.
 		return
 	}
-	pkt.Data.TrimFront(header.EthernetMinimumSize)
 	eth := header.Ethernet(ethBytes)
 
 	if len(dstLinkAddr) == 0 {

src/connectivity/network/netstack/link/eth/endpoint_test.go
@@ -390,15 +390,19 @@ func TestEndpoint(t *testing.T) {
 			t.Run("ReceivePacket", func(t *testing.T) {
 				const payload = "foobarbaz"
 
+				ethFields := header.EthernetFields{
+					SrcAddr: localLinkAddress,
+					DstAddr: remoteLinkAddress,
+					Type:    protocol,
+				}
+				wantLinkHdr := make(buffer.View, header.EthernetMinimumSize)
+				header.Ethernet(wantLinkHdr).Encode(&ethFields)
+
 				// Send the first sendSize bytes of a frame.
 				send := func(sendSize int) {
 					entry := &device.rxEntries[0]
 					buf := device.iob.BufferFromEntry(*entry)
-					header.Ethernet(buf).Encode(&header.EthernetFields{
-						SrcAddr: localLinkAddress,
-						DstAddr: remoteLinkAddress,
-						Type:    protocol,
-					})
+					header.Ethernet(buf).Encode(&ethFields)
 					if got, want := copy(buf[header.EthernetMinimumSize:], payload), len(payload); got != want {
 						t.Fatalf("got copy() = %d, want %d", got, want)
 					}
@@ -454,9 +458,18 @@ func TestEndpoint(t *testing.T) {
 							SrcLinkAddr: localLinkAddress,
 							DstLinkAddr: remoteLinkAddress,
 							Protocol:    protocol,
-							Pkt: &stack.PacketBuffer{
-								Data: buffer.View(payload[:extra]).ToVectorisedView(),
-							},
+							Pkt: func() *stack.PacketBuffer {
+								vv := wantLinkHdr.ToVectorisedView()
+								vv.AppendView(buffer.View(payload[:extra]))
+								pkt := stack.NewPacketBuffer(stack.PacketBufferOptions{
+									Data: vv,
+								})
+								_, ok := pkt.LinkHeader().Consume(header.EthernetMinimumSize)
+								if !ok {
+									t.Fatalf("failed to consume %d bytes for link header", header.EthernetMinimumSize)
+								}
+								return pkt
+							}(),
 						}, args, testutil.PacketBufferCmpTransformer); diff != "" {
 							t.Fatalf("delivered network packet mismatch (-want +got):\n%s", diff)
 						}

src/connectivity/network/netstack/link/netdevice/client_test.go
@@ -414,6 +414,14 @@ func TestReceivePacket(t *testing.T) {
 		t.Fatalf("unexpected packet received: %v", args)
 	}
 
+	ethFields := header.EthernetFields{
+		SrcAddr: tcpip.LinkAddress(otherMac.Octets[:]),
+		DstAddr: tcpip.LinkAddress(tunMac.Octets[:]),
+		Type:    protocol,
+	}
+	wantLinkHdr := make(buffer.View, header.EthernetMinimumSize)
+	header.Ethernet(wantLinkHdr).Encode(&ethFields)
+
 	// Then test some valid configurations.
 	for _, extra := range []int{
 		// Test receiving a frame that is equal to the minimum frame size.
@@ -429,9 +437,18 @@ func TestReceivePacket(t *testing.T) {
 			SrcLinkAddr: tcpip.LinkAddress(otherMac.Octets[:]),
 			DstLinkAddr: tcpip.LinkAddress(tunMac.Octets[:]),
 			Protocol:    protocol,
-			Pkt: &stack.PacketBuffer{
-				Data: buffer.View(pktPayload[:extra]).ToVectorisedView(),
-			},
+			Pkt: func() *stack.PacketBuffer {
+				vv := wantLinkHdr.ToVectorisedView()
+				vv.AppendView(buffer.View(pktPayload[:extra]))
+				pkt := stack.NewPacketBuffer(stack.PacketBufferOptions{
+					Data: vv,
+				})
+				_, ok := pkt.LinkHeader().Consume(header.EthernetMinimumSize)
+				if !ok {
+					t.Fatalf("failed to consume %d bytes for link header", header.EthernetMinimumSize)
+				}
+				return pkt
+			}(),
 		}, testutil.PacketBufferCmpTransformer); diff != "" {
 			t.Fatalf("delivered network packet mismatch (-want +got):\n%s", diff)
 		}

commit ce29c0e0642ec05e3ef1c1b5093fe60b695536d7

[netstack] Do not panic on empty ethernet frames
Fixes a regression introduced in 779c5284a3.

Change-Id: Ieca61f3f1c498ffa686a21d9d06261ffe8895c68
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/451375
Reviewed-by: Tamir Duberstein <tamird@google.com>
Testability-Review: Tamir Duberstein <tamird@google.com>
Commit-Queue: Ghanan Gowripalan <ghanan@google.com>

src/connectivity/network/netstack/filter/endpoint.go
@@ -70,7 +70,9 @@ func (e *Endpoint) DeliverNetworkPacket(dstLinkAddr, srcLinkAddr tcpip.LinkAddre
 	// TODO(fxbug.dev/50424): Support using a buffer.VectorisedView when parsing packets
 	// so we don't need to create a single view here.
 	if len(pkt.Data.Views()) == 0 {
-		panic("expected to receive a packet buffer with at least 1 remaining view in Data")
+		// Expected to receive a packet buffer with at least 1 remaining view in
+		// Data.
+		return
 	}
 
 	if len(pkt.Data.Views()) != 1 {