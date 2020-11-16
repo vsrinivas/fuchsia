12955832118b212400f6ed423c3f2ad02a34d5d0
Shai Barack <shayba@google.com>
2020-11-16 21:36:01 +0000

    [build] markdown_goldens should be an action, not exec_script
    exec_script is executed inline with GN gen, which slows down the build.
    
    Also, the build was incorrect before this change because the testdata
    wasn't an input to this invocation. Fixed now.
    
    Also, this helps us ensure that we use hermetic Python throughout the
    build. Previously exec_script was invoking Python from PATH, without
    guarantees as to what Python tool is actually invoked.
    
    Also I don't understand how this script worked before because it had
    invalid Python code in it (was invoking `exit()` instead of
    `sys.exit()`). I guess exec_script silently swallowed the error? /shrug
    Either way, fixed.
    
    Bug: 64359
    
    Change-Id: I4aacc31fb1bfa71af1b39420c38ff10e7b9d07a3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/450607
    Fuchsia-Auto-Submit: Shai Barack <shayba@google.com>
    Reviewed-by: Claudio Cherubino <ccherubino@google.com>
    Testability-Review: Shai Barack <shayba@google.com>
    Commit-Queue: Shai Barack <shayba@google.com>

--- tools/fidl/fidldoc/markdown_goldens.py
+++ tools/fidl/fidldoc/markdown_goldens.py
@@ -17,25 +17,31 @@ from os import path
 import sys
 
 
-def main(name, argv):
+def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         '--input', metavar='FILE', help='path to jsonir_goldens.txt')
     parser.add_argument(
         '--goldens', metavar='FOLDER', help='path to markdown goldens')
+    parser.add_argument(
+        '--stamp', help="Path to stamp file to write after completion")
+    parser.add_argument(
+        '--depfile', help="Path to depfile to write if also stamping")
+    args = parser.parse_args()
 
-    args = parser.parse_args(argv)
+    deps = []
 
     with open(args.input, 'r') as f:
         # Each line of this file points to a JSON IR golden
         lines = f.readlines()
         for line in lines:
-            line = line.rstrip() # strip the trailing \n
+            line = line.rstrip()  # strip the trailing \n
 
             # Markdown goldens have the same filename as JSON IR goldens
             # with a ".md" extension
             golden_name = line + '.md'
             golden_file = path.join(args.goldens, golden_name)
+            deps.append(golden_file)
 
             # No corresponding Markdown golden, let's stop the build and
             # provide instructions to fix the issue
@@ -43,12 +49,23 @@ def main(name, argv):
                 print('No Markdown golden file for {}'.format(line))
                 print('To continue building, create the following file:')
                 print(golden_file)
-                print('Once the build is complete, follow instructions to regenerate goldens:')
-                print('https://fuchsia.googlesource.com/fuchsia/+/HEAD/tools/fidl/fidldoc/README.md')
-                exit(1)
+                print(
+                    'Once the build is complete, follow instructions to regenerate goldens:'
+                )
+                print(
+                    'https://fuchsia.googlesource.com/fuchsia/+/HEAD/tools/fidl/fidldoc/README.md'
+                )
+                return 1
 
-    exit(0)
+    if args.stamp:
+        with open(args.stamp, 'w') as stamp:
+            stamp.truncate()
+        if args.depfile:
+            with open(args.depfile, 'wt') as depfile:
+                depfile.write(args.stamp + ": " + " ".join(deps) + "\n")
+
+    return 0
 
 
 if __name__ == "__main__":
-    main(sys.argv[0], sys.argv[1:])
+    sys.exit(main())
