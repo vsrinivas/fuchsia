4ae353a64fda0c8b80de31043aec63e0aefff3df
Jerry Belton <jcecil@google.com>
2020-11-16 21:03:48 +0000

    [licenses] Fixing license texts across the Fuchsia tree.
    The remaining files in additionalSkipFiles require:
    * New template patterns, or
    * Updating generator scripts to include license info (they're generated files)
    
    Change-Id: I6772495fa889c95892eb880e94c3f330ae40f258
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/450237
    Commit-Queue: Jerry Belton <jcecil@google.com>
    Reviewed-by: Omer Lev-ran <omerlevran@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Testability-Review: Adam Barth <abarth@google.com>

--- tools/integration/bootstrap.sh
+++ tools/integration/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright 2019 The Fuchsia Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can
+# Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 set -o errexit    # exit when a command fails
