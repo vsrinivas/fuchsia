39c498dce5ae5f8230d7a3523c876de121627faf
Ruby Zhuang <rdzhuang@google.com>
2020-11-17 02:18:44 +0000

    [registers] mock registers for testing
    Test: fake-registers-test
    
    Bug: 34041
    
    Change-Id: Iec562bdb58f0d4084c0fd1d4af849076cc3572ca
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/450914
    Commit-Queue: Ruby Zhuang <rdzhuang@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Testability-Review: Suraj Malhotra <surajmalhotra@google.com>

---
+++ src/devices/registers/testing/mock-registers/mock-registers-test.cc
@@ -0,0 +1,150 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mock-registers.h"
+
+#include <lib/async-loop/cpp/loop.h>
+
+#include <zxtest/zxtest.h>
+
+namespace mock_registers {
+
+class MockRegistersTest : public zxtest::Test {
+ public:
+  void SetUp() override {
+    zx_status_t status;
+
+    loop_.StartThread();
+
+    device_ = std::make_unique<MockRegistersDevice>(loop_.dispatcher());
+
+    zx::channel client_end, server_end;
+    if ((status = zx::channel::create(0, &client_end, &server_end)) != ZX_OK) {
+      printf("Could not create channel %d\n", status);
+      return;
+    }
+    device_->RegistersConnect(std::move(server_end));
+    client_ = ::llcpp::fuchsia::hardware::registers::Device::SyncClient(std::move(client_end));
+  }
+
+  void TearDown() override {
+    EXPECT_OK(registers()->VerifyAll());
+    loop_.Shutdown();
+  }
+
+  MockRegisters* registers() { return device_->fidl_service(); }
+
+ protected:
+  std::unique_ptr<MockRegistersDevice> device_;
+
+  ::llcpp::fuchsia::hardware::registers::Device::SyncClient client_;
+  async::Loop loop_{&kAsyncLoopConfigNeverAttachToThread};
+};
+
+TEST_F(MockRegistersTest, ReadTest) {
+  // 8-bit
+  registers()->ExpectRead<uint8_t>(0, 1, 2);
+  auto read8_res = client_.ReadRegister8(0, 1);
+  EXPECT_TRUE(read8_res.ok());
+  EXPECT_TRUE(read8_res->result.is_response());
+  EXPECT_EQ(read8_res->result.response().value, 2);
+
+  // 16-bit
+  registers()->ExpectRead<uint16_t>(5, 15, 3);
+  auto read16_res = client_.ReadRegister16(5, 15);
+  EXPECT_TRUE(read16_res.ok());
+  EXPECT_TRUE(read16_res->result.is_response());
+  EXPECT_EQ(read16_res->result.response().value, 3);
+
+  // 32-bit
+  registers()->ExpectRead<uint32_t>(145, 127, 25);
+  auto read32_res = client_.ReadRegister32(145, 127);
+  EXPECT_TRUE(read32_res.ok());
+  EXPECT_TRUE(read32_res->result.is_response());
+  EXPECT_EQ(read32_res->result.response().value, 25);
+
+  // 64-bit
+  registers()->ExpectRead<uint64_t>(325, 54, 136);
+  auto read64_res = client_.ReadRegister64(325, 54);
+  EXPECT_TRUE(read64_res.ok());
+  EXPECT_TRUE(read64_res->result.is_response());
+  EXPECT_EQ(read64_res->result.response().value, 136);
+
+  // Multiple Reads
+  registers()->ExpectRead<uint32_t>(25, 63, 46);
+  registers()->ExpectRead<uint32_t>(25, 84, 53);
+  registers()->ExpectRead<uint32_t>(102, 57, 7);
+  registers()->ExpectRead<uint32_t>(3, 24, 299);
+  registers()->ExpectRead<uint32_t>(102, 67, 38);
+  auto res1 = client_.ReadRegister32(25, 63);
+  EXPECT_TRUE(res1.ok());
+  EXPECT_TRUE(res1->result.is_response());
+  EXPECT_EQ(res1->result.response().value, 46);
+  auto res2 = client_.ReadRegister32(25, 84);
+  EXPECT_TRUE(res2.ok());
+  EXPECT_TRUE(res2->result.is_response());
+  EXPECT_EQ(res2->result.response().value, 53);
+  auto res3 = client_.ReadRegister32(102, 57);
+  EXPECT_TRUE(res3.ok());
+  EXPECT_TRUE(res3->result.is_response());
+  EXPECT_EQ(res3->result.response().value, 7);
+  auto res4 = client_.ReadRegister32(3, 24);
+  EXPECT_TRUE(res4.ok());
+  EXPECT_TRUE(res4->result.is_response());
+  EXPECT_EQ(res4->result.response().value, 299);
+  auto res5 = client_.ReadRegister32(102, 67);
+  EXPECT_TRUE(res5.ok());
+  EXPECT_TRUE(res5->result.is_response());
+  EXPECT_EQ(res5->result.response().value, 38);
+}
+
+TEST_F(MockRegistersTest, WriteTest) {
+  // 8-bit
+  registers()->ExpectWrite<uint8_t>(0, 1, 2);
+  auto write8_res = client_.WriteRegister8(0, 1, 2);
+  EXPECT_TRUE(write8_res.ok());
+  EXPECT_TRUE(write8_res->result.is_response());
+
+  // 16-bit
+  registers()->ExpectWrite<uint16_t>(5, 15, 3);
+  auto write16_res = client_.WriteRegister16(5, 15, 3);
+  EXPECT_TRUE(write16_res.ok());
+  EXPECT_TRUE(write16_res->result.is_response());
+
+  // 32-bit
+  registers()->ExpectWrite<uint32_t>(145, 127, 25);
+  auto write32_res = client_.WriteRegister32(145, 127, 25);
+  EXPECT_TRUE(write32_res.ok());
+  EXPECT_TRUE(write32_res->result.is_response());
+
+  // 64-bit
+  registers()->ExpectWrite<uint64_t>(325, 54, 136);
+  auto write64_res = client_.WriteRegister64(325, 54, 136);
+  EXPECT_TRUE(write64_res.ok());
+  EXPECT_TRUE(write64_res->result.is_response());
+
+  // Multiple Writes
+  registers()->ExpectWrite<uint32_t>(25, 63, 46);
+  registers()->ExpectWrite<uint32_t>(25, 84, 53);
+  registers()->ExpectWrite<uint32_t>(102, 57, 7);
+  registers()->ExpectWrite<uint32_t>(3, 24, 299);
+  registers()->ExpectWrite<uint32_t>(102, 67, 38);
+  auto res1 = client_.WriteRegister32(25, 63, 46);
+  EXPECT_TRUE(res1.ok());
+  EXPECT_TRUE(res1->result.is_response());
+  auto res2 = client_.WriteRegister32(25, 84, 53);
+  EXPECT_TRUE(res2.ok());
+  EXPECT_TRUE(res2->result.is_response());
+  auto res3 = client_.WriteRegister32(102, 57, 7);
+  EXPECT_TRUE(res3.ok());
+  EXPECT_TRUE(res3->result.is_response());
+  auto res4 = client_.WriteRegister32(3, 24, 299);
+  EXPECT_TRUE(res4.ok());
+  EXPECT_TRUE(res4->result.is_response());
+  auto res5 = client_.WriteRegister32(102, 67, 38);
+  EXPECT_TRUE(res5.ok());
+  EXPECT_TRUE(res5->result.is_response());
+}
+
+}  // namespace mock_registers
---
+++ src/devices/registers/testing/mock-registers/mock-registers.h
@@ -0,0 +1,209 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuchsia/hardware/registers/llcpp/fidl.h>
+
+#include <map>
+#include <queue>
+
+#include <ddktl/protocol/registers.h>
+
+namespace mock_registers {
+
+// Mock Registers. FIDL implementation.
+class MockRegisters : public ::llcpp::fuchsia::hardware::registers::Device::Interface {
+ public:
+  explicit MockRegisters(async_dispatcher_t* dispatcher) : dispatcher_(dispatcher) {}
+  ~MockRegisters() {}
+
+  // Manage the Fake FIDL Message Loop
+  zx_status_t Init(zx::channel remote) {
+    auto result = fidl::BindServer(dispatcher_, std::move(remote), this);
+    return result.is_ok() ? ZX_OK : result.error();
+  }
+
+  template <typename T>
+  void ExpectRead(uint64_t offset, T mask, T value) {
+    static_assert(std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
+                  std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t>);
+    GetExpectRead<T>()[offset].push(std::pair<T, T>(mask, value));
+  }
+
+  template <typename T>
+  void ExpectWrite(uint64_t offset, T mask, T value) {
+    static_assert(std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
+                  std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t>);
+    GetExpectWrite<T>()[offset].push(std::pair<T, T>(mask, value));
+  }
+
+  zx_status_t VerifyAll();
+
+ private:
+  // Implement Registers FIDL Protocol.
+  void ReadRegister8(uint64_t offset, uint8_t mask, ReadRegister8Completer::Sync& completer) {
+    ReadRegister(offset, mask, completer);
+  }
+  void ReadRegister16(uint64_t offset, uint16_t mask, ReadRegister16Completer::Sync& completer) {
+    ReadRegister(offset, mask, completer);
+  }
+  void ReadRegister32(uint64_t offset, uint32_t mask, ReadRegister32Completer::Sync& completer) {
+    ReadRegister(offset, mask, completer);
+  }
+  void ReadRegister64(uint64_t offset, uint64_t mask, ReadRegister64Completer::Sync& completer) {
+    ReadRegister(offset, mask, completer);
+  }
+  void WriteRegister8(uint64_t offset, uint8_t mask, uint8_t value,
+                      WriteRegister8Completer::Sync& completer) {
+    WriteRegister(offset, mask, value, completer);
+  }
+  void WriteRegister16(uint64_t offset, uint16_t mask, uint16_t value,
+                       WriteRegister16Completer::Sync& completer) {
+    WriteRegister(offset, mask, value, completer);
+  }
+  void WriteRegister32(uint64_t offset, uint32_t mask, uint32_t value,
+                       WriteRegister32Completer::Sync& completer) {
+    WriteRegister(offset, mask, value, completer);
+  }
+  void WriteRegister64(uint64_t offset, uint64_t mask, uint64_t value,
+                       WriteRegister64Completer::Sync& completer) {
+    WriteRegister(offset, mask, value, completer);
+  }
+
+  // Helper functions for FIDL
+  template <typename T, typename Completer>
+  void ReadRegister(uint64_t offset, T mask, Completer& completer) {
+    auto& expect_read = GetExpectRead<T>()[offset];
+    if (expect_read.empty()) {
+      completer.ReplyError(ZX_ERR_NOT_SUPPORTED);
+      return;
+    }
+    auto value = expect_read.front();
+    expect_read.pop();
+    if (value.first == mask) {
+      printf("Read %u\n", static_cast<uint32_t>(value.second));
+      completer.ReplySuccess(static_cast<T>(value.second));
+    } else {
+      completer.ReplyError(ZX_ERR_INVALID_ARGS);
+    }
+  }
+
+  template <typename T, typename Completer>
+  void WriteRegister(uint64_t offset, T mask, T value, Completer& completer) {
+    printf("Write %u\n", static_cast<uint32_t>(value));
+    auto& expect_write = GetExpectWrite<T>()[offset];
+    if (expect_write.empty()) {
+      completer.ReplyError(ZX_ERR_NOT_SUPPORTED);
+      return;
+    }
+    auto expected_value = expect_write.front();
+    expect_write.pop();
+    if ((expected_value.first == mask) && (expected_value.second == value)) {
+      completer.ReplySuccess();
+    } else {
+      completer.ReplyError(ZX_ERR_INVALID_ARGS);
+    }
+  }
+
+  // Helper functions to get queues.
+  template <typename T>
+  auto& GetExpectRead() {
+    if constexpr (!std::is_same_v<T, uint64_t>) {
+      return expect_read64;
+    } else if constexpr (!std::is_same_v<T, uint32_t>) {
+      return expect_read32;
+    } else if constexpr (!std::is_same_v<T, uint16_t>) {
+      return expect_read16;
+    } else if constexpr (!std::is_same_v<T, uint8_t>) {
+      return expect_read8;
+    }
+  }
+
+  template <typename T>
+  auto& GetExpectWrite() {
+    if constexpr (!std::is_same_v<T, uint64_t>) {
+      return expect_write64;
+    } else if constexpr (!std::is_same_v<T, uint32_t>) {
+      return expect_write32;
+    } else if constexpr (!std::is_same_v<T, uint16_t>) {
+      return expect_write16;
+    } else if constexpr (!std::is_same_v<T, uint8_t>) {
+      return expect_write8;
+    }
+  }
+
+  async_dispatcher_t* dispatcher_;
+
+  std::map<uint64_t, std::queue<std::pair<uint8_t, uint8_t>>> expect_read8;
+  std::map<uint64_t, std::queue<std::pair<uint16_t, uint16_t>>> expect_read16;
+  std::map<uint64_t, std::queue<std::pair<uint32_t, uint32_t>>> expect_read32;
+  std::map<uint64_t, std::queue<std::pair<uint64_t, uint64_t>>> expect_read64;
+  std::map<uint64_t, std::queue<std::pair<uint8_t, uint8_t>>> expect_write8;
+  std::map<uint64_t, std::queue<std::pair<uint16_t, uint16_t>>> expect_write16;
+  std::map<uint64_t, std::queue<std::pair<uint32_t, uint32_t>>> expect_write32;
+  std::map<uint64_t, std::queue<std::pair<uint64_t, uint64_t>>> expect_write64;
+};
+
+// Mock Registers Device implementing Banjo protocol to connect to FIDL implementation.
+class MockRegistersDevice : public ddk::RegistersProtocol<MockRegistersDevice> {
+ public:
+  MockRegistersDevice(async_dispatcher_t* dispatcher)
+      : proto_({&registers_protocol_ops_, this}), fidl_service_(dispatcher) {}
+
+  void RegistersConnect(zx::channel chan) { fidl_service_.Init(std::move(chan)); }
+
+  const registers_protocol_t* proto() const { return &proto_; }
+  MockRegisters* fidl_service() { return &fidl_service_; }
+
+ private:
+  registers_protocol_t proto_;
+  MockRegisters fidl_service_;
+};
+
+zx_status_t MockRegisters::VerifyAll() {
+  for (const auto& expect_read : GetExpectRead<uint8_t>()) {
+    if (!expect_read.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+  for (const auto& expect_read : GetExpectRead<uint16_t>()) {
+    if (!expect_read.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+  for (const auto& expect_read : GetExpectRead<uint32_t>()) {
+    if (!expect_read.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+  for (const auto& expect_read : GetExpectRead<uint64_t>()) {
+    if (!expect_read.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+
+  for (const auto& expect_write : GetExpectWrite<uint8_t>()) {
+    if (!expect_write.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+  for (const auto& expect_write : GetExpectWrite<uint16_t>()) {
+    if (!expect_write.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+  for (const auto& expect_write : GetExpectWrite<uint32_t>()) {
+    if (!expect_write.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+  for (const auto& expect_write : GetExpectWrite<uint64_t>()) {
+    if (!expect_write.second.empty()) {
+      return ZX_ERR_INTERNAL;
+    }
+  }
+
+  return ZX_OK;
+}
+
+}  // namespace mock_registers
