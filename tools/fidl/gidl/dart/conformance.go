7c0de56ccaf4df308a5c6ffb7578f6ac1056d910
Chase Latta <chaselatta@google.com>
2020-11-17 05:07:42 +0000

    [gidl][dart] opt out of null safety
    We need to opt the gidl generated mains out of null safety until the
    gidl library has been migrated to null safety.
    
    Change-Id: Id1a183a41b2d49174aa6c01d27d51db04c87f51c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/451699
    Fuchsia-Auto-Submit: Chase Latta <chaselatta@google.com>
    Reviewed-by: Ian McKellar <ianloic@google.com>
    Testability-Review: Ian McKellar <ianloic@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- tools/fidl/gidl/dart/conformance.go
+++ tools/fidl/gidl/dart/conformance.go
@@ -17,7 +17,7 @@ import (
 )
 
 var tmpl = template.Must(template.New("tmpls").Parse(`
-// @dart = 2.10
+// @dart = 2.8
 
 // Ignore unused imports so that GIDL tests can be commented out without error.
 // ignore_for_file: unused_import
