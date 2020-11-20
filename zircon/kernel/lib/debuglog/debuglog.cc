vsrinivas/fuchsia

commit 7458d199e028f954e9377be740ccf118cdf19abb

[kernel][debuglog] Use Signal instead of SignalNoResched
In debuglog, when signaling events, use the signal variant that
triggers an immediate reschedule to eliminate the potential for
"dropping" reschedules when called from IRQ context and the current
CPU is chosen for the newly scheduled thread.

Change-Id: I46fd4921e6c19b4777c65e52ec2f51bee527e69e
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/447240
Commit-Queue: Nick Maniscalco <maniscalco@google.com>
Reviewed-by: Corey Tabaka <eieio@google.com>
Testability-Review: Corey Tabaka <eieio@google.com>

zircon/kernel/lib/debuglog/debuglog.cc
@@ -199,7 +199,7 @@ zx_status_t DLog::write(uint32_t severity, uint32_t flags, ktl::string_view str)
     if (holding_thread_lock) {
       this->event.SignalThreadLocked();
     } else {
-      this->event.SignalNoResched();
+      this->event.Signal();
     }
   }();
 
@@ -378,7 +378,7 @@ void dlog_serial_write(ktl::string_view str) {
 // and kernel serial console.
 static void debuglog_dumper_notify(void* cookie) {
   Event* event = reinterpret_cast<Event*>(cookie);
-  event->SignalNoResched();
+  event->Signal();
 }
 
 static AutounsignalEvent dumper_event;
@@ -451,7 +451,7 @@ static zx_status_t dlog_shutdown_thread(Thread* thread, const char* name,
                                         ktl::atomic<bool>* shutdown_requested, Event* event,
                                         zx_time_t deadline) {
   shutdown_requested->store(true);
-  event->SignalNoResched();
+  event->Signal();
   if (thread != nullptr) {
     zx_status_t status = thread->Join(nullptr, deadline);
     if (status != ZX_OK) {

commit 932e5368ddced0a972543d28b51cdb3dc8354032

[kernel][debuglog] Don't call Signal if holding any spinlocks
Fixes a bug introduced by 7458d199e028f954e9377be740ccf118cdf19abb.

When signaling the debuglog thread, do not trigger an immediate
reschedule if holding a non-thread-lock spinlock.  Calling Signal when
preemption/rescheduling are enabled can trigger a reschedule and it is
an error to reschedule while holding a non-thread-lock spinlock.

Re-enable RegisterInterruptHandlerTooMany test.

See fxbug.dev/64871 to understand why this workaround is needed.

See fxbug.dev/64884 for a proposed solution that will replace this
temporary workaround.

Bug: 64871
Bug: 64884
Test: k ut -r 10000 pc_interrupt
Change-Id: Ic3890334a6ad383e655ae66df503e042310fb0e0
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/453340
Reviewed-by: Corey Tabaka <eieio@google.com>
Reviewed-by: Travis Geiselbrecht <travisg@google.com>
Testability-Review: Corey Tabaka <eieio@google.com>
Commit-Queue: Nick Maniscalco <maniscalco@google.com>

zircon/kernel/lib/debuglog/debuglog.cc
@@ -193,15 +193,34 @@ zx_status_t DLog::write(uint32_t severity, uint32_t flags, ktl::string_view str)
     holding_thread_lock = thread_lock.HolderCpu() == arch_curr_cpu_num();
   }
 
-  [this, holding_thread_lock]() TA_NO_THREAD_SAFETY_ANALYSIS {
-    // if we happen to be called from within the global thread lock, use a
-    // special version of event signal
-    if (holding_thread_lock) {
-      this->event.SignalThreadLocked();
+  // Signal the event.
+  if (holding_thread_lock) {
+    // If we happen to be holding the global thread lock, use a special version of event signal.
+    AssertHeld<ThreadLock, IrqSave>(*ThreadLock::Get());
+    event.SignalThreadLocked();
+  } else {
+    // TODO(fxbug.dev/64884): Once fxbug.dev/64884 is fixed, we can replace the following
+    // conditional statement with a call to Signal.
+    //
+    // We're not holding the thread lock.
+    if (arch_num_spinlocks_held() == 0 ||
+        Thread::Current::Get()->preemption_state().PreemptOrReschedDisabled()) {
+      // If we're not holding any spinlocks, then we can call Signal.  And if we are holding a
+      // spinlock, but we're running in a preempt/reschedule disabled context, we can still call
+      // Signal because it will defer the reschedule until preempt/reschedule are re-enabled.
+      event.Signal();
     } else {
-      this->event.Signal();
+      // We are holding at least one (non thread lock) spinlock and we aren't running in an preempt
+      // or reschedule disabled context, which means it's unsafe to reschedule this CPU until after
+      // we have released the held spinlock(s).  We can't call Singal here.  The best we can do at
+      // this point is call SignalNoResched and hope that something triggers a reschedule soon.
+      //
+      // TODO(fxbug.dev/64884): There is a bug here.  Calling SignalNoResched will not trigger an
+      // immediate reschedule and will not set up a deferred reschedule.  This code path may result
+      // in "lost reschedules".
+      event.SignalNoResched();
     }
-  }();
+  }
 
   return ZX_OK;
 }

zircon/kernel/platform/pc/interrupts_test.cc
@@ -163,8 +163,7 @@ bool TestUnregisterInterruptHandlerNotRegistered() {
   END_TEST;
 }
 
-// TODO(fxbug.dev/64871): Remove the [[maybe_unused]] annotation.
-[[maybe_unused]] bool TestRegisterInterruptHandlerTooMany() {
+bool TestRegisterInterruptHandlerTooMany() {
   BEGIN_TEST;
 
   FakeIoApic::Reset();
@@ -306,8 +305,7 @@ UNITTEST_START_TESTCASE(pc_interrupt_tests)
 UNITTEST("RegisterInterruptHandler", TestRegisterInterruptHandler)
 UNITTEST("RegisterInterruptHandlerTwice", TestRegisterInterruptHandlerTwice)
 UNITTEST("UnregisterInterruptHandlerNotRegistered", TestUnregisterInterruptHandlerNotRegistered)
-// TODO(fxbug.dev/64871): RegisterInterruptHandlerTooMany is disabled because it triggers a bug.
-// UNITTEST("RegisterInterruptHandlerTooMany", TestRegisterInterruptHandlerTooMany)
+UNITTEST("RegisterInterruptHandlerTooMany", TestRegisterInterruptHandlerTooMany)
 UNITTEST("HandlerAllocationAlignment", TestHandlerAllocationAlignment)
 UNITTEST("PvEoi", TestPvEoi)
 UNITTEST_END_TESTCASE(pc_interrupt_tests, "pc_interrupt", "Tests for external interrupts")