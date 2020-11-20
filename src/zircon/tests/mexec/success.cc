f8933b3b74658682ec9ca6fc78bd2c35c12b4f23
Joshua Seaton <joshuaseaton@google.com>
2020-11-20 01:51:43 +0000

    [mexec] Consolidate mexec testing logic
    This change moves
    //build/mexec_zbi_test -> //src/bringup/lib/mexec/testing/zbi_test.gni
    and
    //src/zircon/tests/mexec -> //src/bringup/lib/mexec/tests
    
    Bug: 57244
    Change-Id: Id581c8cf618ee3ca7de2a1f50010797c2e3fa78f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/453054
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: David Greenaway <dgreenaway@google.com>
    Testability-Review: David Greenaway <dgreenaway@google.com>

--- src/zircon/tests/mexec/success.cc
+++
@@ -1,8 +0,0 @@
-// Copyright 2019 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This test just succeeds unconditionally.
-// The logic is that since this test is intended to test mexec, the fact that
-// this test runs _at all_ is proof positive that the system was able to mexec.
-int main() { return 0; }
