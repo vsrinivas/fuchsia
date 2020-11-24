d89fce3d59074c1f9155c1f19e37b388a06f7c78
Lucas Jenkins <lucasjenkins@google.com>
2020-11-24 05:19:04 +0000

    [bt][sm] Fuzz ValidPacketReader::ParseSdu
    This function parses untrusted input received over an L2CAP channel
    from the peer into SMP messages used in our stack. As such, it should
    be fuzzed to ensure external code can't break it.
    
    Fixed: 53655
    Test: Improves test coverage.
    Change-Id: Ifa2e960d28cec73237783ea0a20a53e823f1fce4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/454457
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Testability-Review: Ben Lawson <benlawson@google.com>
    Commit-Queue: Lucas Jenkins <lucasjenkins@google.com>

---
+++ src/connectivity/bluetooth/core/bt-host/sm/valid_packet_reader_parse_sdu_fuzztest.cc
@@ -0,0 +1,31 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+
+#include "src/connectivity/bluetooth/core/bt-host/common/byte_buffer.h"
+#include "src/connectivity/bluetooth/core/bt-host/common/log.h"
+#include "src/connectivity/bluetooth/core/bt-host/sm/packet.h"
+
+// Prevent "undefined symbol: __zircon_driver_rec__" error.
+BT_DECLARE_FAKE_DRIVER();
+
+namespace bt::sm {
+
+void fuzz(const uint8_t* data, size_t size) {
+  DynamicByteBuffer buf(size);
+  memcpy(buf.mutable_data(), data, size);
+  ByteBufferPtr buf_ptr = std::make_unique<DynamicByteBuffer>(buf);
+  fit::result<ValidPacketReader, ErrorCode> result = ValidPacketReader::ParseSdu(buf_ptr);
+  if (result.is_ok()) {
+    ZX_ASSERT(result.value().is_valid());
+  }
+}
+
+}  // namespace bt::sm
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  bt::sm::fuzz(data, size);
+  return 0;
+}
