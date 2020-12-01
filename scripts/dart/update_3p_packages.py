5c1c05ba52ab243fc14f9988849f184e0acabaea
Chase Latta <chaselatta@google.com>
2020-12-01 19:52:59 +0000

    Revert "[dart] ignore deps for prebuilts"
    This reverts commit 9346a15cce93c6176c25f2f21843cdfc80cad9ee.
    
    Reason for revert: This was not the reason for the roller breakage.
    
    Original change's description:
    > [dart] ignore deps for prebuilts
    >
    > Change-Id: I86a5a9734a63435657f6955aa952d8974f9ddbb4
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/456629
    > Reviewed-by: Naud Ghebre <naudzghebre@google.com>
    > Reviewed-by: Ian McKellar <ianloic@google.com>
    > Reviewed-by: Darren Chan <chandarren@google.com>
    > Testability-Review: Chase Latta <chaselatta@google.com>
    > Commit-Queue: Chase Latta <chaselatta@google.com>
    
    TBR=ianloic@google.com,chaselatta@google.com,chandarren@google.com,naudzghebre@google.com
    
    Change-Id: I5ab283344c1e858660820fe4867fac6b1c2f80fb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/456866
    Reviewed-by: Chase Latta <chaselatta@google.com>
    Commit-Queue: Chase Latta <chaselatta@google.com>

--- scripts/dart/update_3p_packages.py
+++ scripts/dart/update_3p_packages.py
@@ -13,7 +13,9 @@ import sys
 # graph.  If they contain conflicting requirements for a package 'pub get' will
 # error out and the conflicts will have to be resolved before the packages can
 # be updated.
-ROOT_PUBSPECS = []
+ROOT_PUBSPECS = [
+    'prebuilt/third_party/sky_engine',
+]
 
 FLUTTER_GIT = 'https://github.com/flutter/flutter.git'
 FLUTTER_ROOT = 'third_party/dart-pkg/git/flutter'
