6a298e70cf6efe083a67f0f84524ad9c0ea89aa5
Joshua Seaton <joshuaseaton@google.com>
2020-12-02 23:00:56 +0000

    [zbitl] Cosmetically update fuzzers
    This change updates the iteration fuzzer to have a simpler and more
    idiomatically-spelled control flow - and updates the names of the fuzzer
    to include a -fuzzer suffix. The reason for the suffix is that host-side
    tests are created automatically by the fuzzing framework that could
    otherwise pollute the host artifact build directory namespace with names
    like "iteration-test".
    
    Bug: 62638
    Change-Id: Ieecc9a466e7cbbcdbaca7e7c515d385623d734d4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/457265
    Testability-Review: Joshua Seaton <joshuaseaton@google.com>
    Testability-Review: David Greenaway <dgreenaway@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: David Greenaway <dgreenaway@google.com>

--- zircon/system/ulib/zbitl/testing/iteration-fuzzer.cc
+++ zircon/system/ulib/zbitl/testing/iteration-fuzzer.cc
@@ -2,42 +2,48 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <lib/zbitl/memory.h>
 #include <lib/zbitl/view.h>
 
-constexpr size_t kMaxSize = 1024;
+#include <string>
 
-template <zbitl::Checking Check>
-using MemoryView = zbitl::View<fbl::Span<const uint8_t>, Check>;
+#include <fuzzer/FuzzedDataProvider.h>
+
+// If FuzzedDataProvider::ConsumeEnum() did not require a terminal kMaxValue
+// enum value, we would just use zbitl::Checking.
+enum class Checking { kPermissive, kStrict, kCrc, kMaxValue };
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  if (size > kMaxSize) {
-    return 0;
-  }
-  fbl::Span<const uint8_t> span{data, size};
-  {
-    MemoryView<zbitl::Checking::kPermissive> view(span);
+  FuzzedDataProvider provider(data, size);
+
+  auto checking = provider.ConsumeEnum<Checking>();
+  std::string zbi = provider.ConsumeRemainingBytesAsString();
+
+  auto iterate = [](auto view) {
     for (auto [header, payload] : view) {
       static_cast<void>(header);
       static_cast<void>(payload);
     }
     view.ignore_error();
-  }
-  {
-    MemoryView<zbitl::Checking::kStrict> view(span);
-    for (auto [header, payload] : view) {
-      static_cast<void>(header);
-      static_cast<void>(payload);
+  };
+
+  switch (checking) {
+    case Checking::kPermissive: {
+      zbitl::View<std::string_view, zbitl::Checking::kPermissive> view(zbi);
+      iterate(view);
+      break;
     }
-    view.ignore_error();
-  }
-  {
-    MemoryView<zbitl::Checking::kCrc> view(span);
-    for (auto [header, payload] : view) {
-      static_cast<void>(header);
-      static_cast<void>(payload);
+    case Checking::kStrict: {
+      zbitl::View<std::string_view, zbitl::Checking::kStrict> view(zbi);
+      iterate(view);
+      break;
     }
-    view.ignore_error();
-  }
+    case Checking::kCrc: {
+      zbitl::View<std::string_view, zbitl::Checking::kCrc> view(zbi);
+      iterate(view);
+      break;
+    }
+    case Checking::kMaxValue:  // Just a placeholder.
+      break;
+  };
   return 0;
 }
