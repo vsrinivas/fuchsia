f85c4d69399dedbd947e4648a4f629b0eab42463
Erik Gilling <konkers@google.com>
2020-12-04 01:08:44 +0000

    [ffx]: Add `ffx trace record`.
    Initial implementation supports the following options:
    * --buffer-size
    * --categories
    * --duration
    * --output
    
    Bug: 65345
    Change-Id: I9cccfaa9e25affc90729f9d36ef45525a6ae5a78
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/455015
    Commit-Queue: Erik Gilling <konkers@google.com>
    Reviewed-by: Fadi Meawad <fmeawad@google.com>
    Reviewed-by: Matthew Boetger <boetger@google.com>
    Testability-Review: Matthew Boetger <boetger@google.com>

--- src/developer/ffx/plugins/trace/src/args.rs
+++ src/developer/ffx/plugins/trace/src/args.rs
@@ -17,6 +17,7 @@ pub struct TraceCommand {
 #[argh(subcommand)]
 pub enum TraceSubCommand {
     ListProviders(ListProviders),
+    Record(Record),
     // More commands including `record` and `convert` to follow.
 }
 
@@ -24,3 +25,76 @@ pub enum TraceSubCommand {
 /// List the target's trace providers
 #[argh(subcommand, name = "list-providers")]
 pub struct ListProviders {}
+
+// Work around argh's handling of Vec.  Listing categories as a comma
+// separated list of values rather than a repeated keyed option
+// is much more concise when dealing with a large set of categories.
+pub type TraceCategories = Vec<String>;
+
+// This list should be kept in sync with DEFAULT_CATEGORIES in
+// //src/testing/sl4f/src/tracing/facade.rs as well as the help text below
+static DEFAULT_CATEGORIES: &str = "app,audio,benchmark,blobfs,gfx,input,kernel:meta,kernel:sched,ledger,magma,minfs,modular,view,flutter,dart,dart:compiler,dart:dart,dart:debugger,dart:embedder,dart:gc,dart:isolate,dart:profiler,dart:vm";
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Record a trace
+#[argh(subcommand, name = "record")]
+pub struct Record {
+    /// size of per-provider trace buffer in MB.  Defaults to 4.
+    #[argh(option, default = "4")]
+    pub buffer_size: u32,
+
+    /// comma-separated list of categories to enable.  Defaults
+    /// to "app,audio,benchmark,blobfs,gfx,input,kernel:meta,
+    /// kernel:sched,ledger,magma,minfs,modular,view,flutter,
+    /// dart,dart:compiler,dart:dart,dart:debugger,dart:embedder,
+    /// dart:gc,dart:isolate,dart:profiler,dart:vm"
+    #[argh(
+        option,
+        default = "parse_categories(DEFAULT_CATEGORIES).unwrap()",
+        from_str_fn(parse_categories)
+    )]
+    pub categories: TraceCategories,
+
+    /// duration of trace capture in seconds. Defaults to 10 seconds.
+    #[argh(option, default = "10.0")]
+    pub duration: f64,
+
+    /// name of output trace file.  Defaults to trace.fxt.
+    #[argh(option, default = "String::from(\"trace.fxt\")")]
+    pub output: String,
+}
+
+fn parse_categories(value: &str) -> Result<TraceCategories, String> {
+    let mut cats = Vec::new();
+
+    if value.is_empty() {
+        return Err("no categories specified".to_string());
+    }
+
+    for cat in value.split(",") {
+        if cat.is_empty() {
+            return Err("empty category specified".to_string());
+        }
+        cats.push(String::from(cat));
+    }
+
+    Ok(cats)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_categories() {
+        assert_eq!(parse_categories(&"a"), Ok(vec!["a".to_string()]));
+
+        assert_eq!(
+            parse_categories(&"a,b,c:d"),
+            Ok(vec!["a".to_string(), "b".to_string(), "c:d".to_string()])
+        );
+
+        assert_eq!(parse_categories(&""), Err("no categories specified".to_string()));
+        assert_eq!(parse_categories(&"a,,b"), Err("empty category specified".to_string()));
+    }
+}
--- src/developer/ffx/plugins/trace/src/lib.rs
+++ src/developer/ffx/plugins/trace/src/lib.rs
@@ -3,11 +3,15 @@
 // found in the LICENSE file.
 
 use {
-    anyhow::Result,
+    anyhow::{anyhow, Context, Result},
+    async_std::fs::File,
     ffx_core::ffx_plugin,
-    ffx_trace_args::{TraceCommand, TraceSubCommand},
-    fidl_fuchsia_tracing_controller::ControllerProxy,
+    ffx_trace_args::{Record, TraceCommand, TraceSubCommand},
+    fidl_fuchsia_tracing_controller::{
+        ControllerProxy, StartOptions, StopOptions, TerminateOptions, TraceConfig,
+    },
     std::io::{stdout, Write},
+    std::time::Duration,
 };
 
 #[ffx_plugin("tracing", ControllerProxy = "core/appmgr:out:fuchsia.tracing.controller.Controller")]
@@ -15,6 +19,9 @@ pub async fn trace(controller_proxy: ControllerProxy, cmd: TraceCommand) -> Resu
     match cmd.sub_cmd {
         TraceSubCommand::ListProviders(_sub_cmd) => {
             list_providers_cmd_impl(controller_proxy, Box::new(stdout())).await?
+        }
+        TraceSubCommand::Record(sub_cmd) => {
+            record_cmd_impl(sub_cmd, controller_proxy, Box::new(stdout())).await?
         }
     }
 
@@ -27,10 +34,77 @@ async fn list_providers_cmd_impl<W: Write>(
 ) -> Result<()> {
     let providers = controller_proxy.get_providers().await?;
 
-    write!(&mut writer, "Trace providers:\n")?;
+    writeln!(&mut writer, "Trace providers:")?;
     for provider in &providers {
-        write!(&mut writer, "- {}\n", provider.name.as_ref().unwrap_or(&"<unknown>".to_string()))?;
+        writeln!(&mut writer, "- {}", provider.name.as_ref().unwrap_or(&"<unknown>".to_string()))?;
     }
+
+    Ok(())
+}
+
+async fn record_trace<W: Write>(
+    opts: Record,
+    controller_proxy: ControllerProxy,
+    server_socket: fidl::Socket,
+    mut writer: W,
+) -> Result<()> {
+    let trace_config = TraceConfig {
+        buffer_size_megabytes_hint: Some(opts.buffer_size),
+        categories: Some(opts.categories),
+        ..TraceConfig::empty()
+    };
+
+    write!(writer, "initialize tracing\n")?;
+
+    controller_proxy.initialize_tracing(trace_config, server_socket)?;
+
+    write!(writer, "starting trace\n")?;
+
+    controller_proxy
+        .start_tracing(StartOptions::empty())
+        .await?
+        .map_err(|e| anyhow!("Failed to start trace: {:?}", e))?;
+
+    write!(writer, "waiting for {} seconds\n", &opts.duration)?;
+    fuchsia_async::Timer::new(Duration::from_secs_f64(opts.duration)).await;
+
+    write!(writer, "stopping trace\n")?;
+
+    controller_proxy
+        .stop_tracing(StopOptions { write_results: Some(true), ..StopOptions::empty() })
+        .await?;
+
+    write!(writer, "terminating tracing\n")?;
+
+    controller_proxy
+        .terminate_tracing(TerminateOptions {
+            write_results: Some(true),
+            ..TerminateOptions::empty()
+        })
+        .await?;
+
+    Ok(())
+}
+
+async fn record_cmd_impl<W: Write>(
+    opts: Record,
+    controller_proxy: ControllerProxy,
+    writer: W,
+) -> Result<()> {
+    let (client_socket, server_socket) =
+        fidl::Socket::create(fidl::SocketOpts::STREAM).context("failed ot create socket")?;
+    let client_socket = fidl::AsyncSocket::from_socket(client_socket)?;
+
+    let f = File::create(&opts.output).await?;
+
+    let t = fuchsia_async::Task::spawn(async move {
+        let mut f = f;
+        futures::io::copy(client_socket, &mut f).await
+    });
+
+    record_trace(opts, controller_proxy, server_socket, writer).await?;
+
+    t.cancel().await;
 
     Ok(())
 }
@@ -38,34 +112,70 @@ async fn list_providers_cmd_impl<W: Write>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use fidl_fuchsia_tracing_controller::{ControllerRequest, ProviderInfo};
+    use fidl_fuchsia_tracing_controller::{ControllerRequest, ProviderInfo, TerminateResult};
+    use std::sync::{Arc, Mutex};
 
-    fn setup_fake_controller_server() -> ControllerProxy {
-        setup_fake_controller_proxy(move |req| match req {
-            ControllerRequest::InitializeTracing { config: _, output: _, control_handle: _ } => (),
-            ControllerRequest::TerminateTracing { options: _, responder: _ } => (),
-            ControllerRequest::StartTracing { options: _, responder: _ } => (),
-            ControllerRequest::StopTracing { options: _, responder: _ } => (),
-            ControllerRequest::GetProviders { responder } => {
-                let a = ProviderInfo {
-                    id: Some(1),
-                    pid: Some(10),
-                    name: Some("provider_a".to_string()),
-                    ..ProviderInfo::EMPTY
-                };
+    const TEST_TRACE_OUTPUT: &'static [u8] = &[0x00, 0x55, 0xaa, 0xff];
 
-                let b = ProviderInfo {
-                    id: Some(2),
-                    pid: Some(20),
-                    name: Some("provider_b".to_string()),
-                    ..ProviderInfo::EMPTY
-                };
+    #[derive(Debug)]
+    struct FakeControllerState {
+        buffer_size: u32,
+        categories: Vec<String>,
+        output: Option<fidl::Socket>,
+    }
 
-                responder.send(&mut vec![a, b].into_iter()).unwrap();
-            }
-            ControllerRequest::GetKnownCategories { responder: _ } => (),
-            ControllerRequest::WatchAlert { responder: _ } => (),
-        })
+    fn setup_fake_controller_server() -> (Arc<Mutex<FakeControllerState>>, ControllerProxy) {
+        let state = Arc::new(Mutex::new(FakeControllerState {
+            buffer_size: 0,
+            categories: vec![],
+            output: None,
+        }));
+        (
+            state.clone(),
+            setup_fake_controller_proxy(move |req| match req {
+                ControllerRequest::InitializeTracing { config, output, control_handle: _ } => {
+                    let mut state = state.lock().unwrap();
+                    if let Some(buffer_size) = config.buffer_size_megabytes_hint {
+                        state.buffer_size = buffer_size;
+                    }
+                    if let Some(categories) = config.categories {
+                        state.categories = categories;
+                    }
+                    state.output = Some(output);
+                }
+                ControllerRequest::TerminateTracing { options: _, responder } => {
+                    responder.send(TerminateResult::empty()).unwrap();
+                }
+                ControllerRequest::StartTracing { options: _, responder } => {
+                    responder.send(&mut Ok(())).unwrap();
+                }
+                ControllerRequest::StopTracing { options: _, responder } => {
+                    let state = state.lock().unwrap();
+                    state.output.as_ref().unwrap().write(TEST_TRACE_OUTPUT).unwrap();
+
+                    responder.send().unwrap();
+                }
+                ControllerRequest::GetProviders { responder } => {
+                    let a = ProviderInfo {
+                        id: Some(1),
+                        pid: Some(10),
+                        name: Some("provider_a".to_string()),
+                        ..ProviderInfo::EMPTY
+                    };
+
+                    let b = ProviderInfo {
+                        id: Some(2),
+                        pid: Some(20),
+                        name: Some("provider_b".to_string()),
+                        ..ProviderInfo::EMPTY
+                    };
+
+                    responder.send(&mut vec![a, b].into_iter()).unwrap();
+                }
+                ControllerRequest::GetKnownCategories { responder: _ } => (),
+                ControllerRequest::WatchAlert { responder: _ } => (),
+            }),
+        )
     }
 
     #[fuchsia_async::run_singlethreaded(test)]
@@ -77,10 +187,45 @@ mod tests {
           \n";
 
         let mut output = Vec::new();
-        list_providers_cmd_impl(setup_fake_controller_server(), &mut output)
-            .await
-            .expect("list_providers_cmd_impl");
+        let (_state, proxy) = setup_fake_controller_server();
+        list_providers_cmd_impl(proxy, &mut output).await.expect("list_providers_cmd_impl");
 
         assert_eq!(output, TEST_OUTPUT);
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_record() {
+        let trace_file = tempfile::NamedTempFile::new().unwrap();
+        let trace_path = trace_file.into_temp_path();
+
+        let mut output = Vec::new();
+        let (state, proxy) = setup_fake_controller_server();
+        record_cmd_impl(
+            Record {
+                buffer_size: 1,
+                categories: vec!["a".to_string(), "b".to_string(), "c".to_string()],
+                duration: 0.5,
+                output: trace_path.to_str().unwrap().to_string(),
+            },
+            proxy,
+            &mut output,
+        )
+        .await
+        .expect("record_cmd_impl");
+
+        let out_string = String::from_utf8(output).unwrap();
+        let state = state.lock().unwrap();
+
+        // Verify that record handles its arguments correctly.
+        assert_eq!(state.categories, vec!["a".to_string(), "b".to_string(), "c".to_string(),]);
+        assert_eq!(state.buffer_size, 1);
+        assert!(out_string.contains("waiting for 0.5 seconds"));
+
+        // Verify that the trace was written correctly.
+        let trace_output = std::fs::read(&trace_path).unwrap();
+        assert_eq!(trace_output, TEST_TRACE_OUTPUT);
+
+        // Verify that we've cleaned up the temp file.
+        trace_path.close().unwrap();
     }
 }
