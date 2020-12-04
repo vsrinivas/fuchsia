69648876129d1a3e0656a90b61a17f2949c913c4
Victor Chang <vfcc@google.com>
2020-12-04 00:34:20 +0000

    Reland "[cts] fdio-spawn-tests rules are now `cts_` variants."
    This reverts commit 9311753bda691a0f25017c2ab5b4f15b6e3a7d71.
    
    Reason for revert: Creating revert CL and applying a fix.
    
    Original change's description:
    > Revert "[cts] fdio-spawn-tests rules are now `cts_` variants."
    >
    > This reverts commit da9d84a6dd501a72200796ef60160ff7d5340193.
    >
    > Reason for revert: causing build flakes: https://ci.chromium.org/b/8863052722722929680
    >
    > Original change's description:
    > > [cts] fdio-spawn-tests rules are now `cts_` variants.
    > >
    > > Change-Id: Ifa55914eeaca7b86be9c16d293dc40bd616398fb
    > > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/432998
    > > Fuchsia-Auto-Submit: Victor Chang <vfcc@google.com>
    > > Commit-Queue: Victor Chang <vfcc@google.com>
    > > Reviewed-by: John Shamoon <johnshamoon@google.com>
    > > Reviewed-by: Jeremy Manson <jeremymanson@google.com>
    > > Testability-Review: Jeremy Manson <jeremymanson@google.com>
    >
    > TBR=jeremymanson@google.com,vfcc@google.com,johnshamoon@google.com
    >
    > Change-Id: I0cda8551df88f6ad7fed0076ee545196bdee68fb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/453898
    > Reviewed-by: Oliver Newman <olivernewman@google.com>
    > Commit-Queue: Oliver Newman <olivernewman@google.com>
    
    TBR=jeremymanson@google.com,vfcc@google.com,olivernewman@google.com,johnshamoon@google.com
    
    Change-Id: Ic4dba41a4eabe9fca1d3b4b202a4a8020da3a066
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/457657
    Reviewed-by: John Shamoon <johnshamoon@google.com>
    Reviewed-by: Jeremy Manson <jeremymanson@google.com>
    Testability-Review: Jeremy Manson <jeremymanson@google.com>
    Commit-Queue: Victor Chang <vfcc@google.com>
    Fuchsia-Auto-Submit: Victor Chang <vfcc@google.com>

--- garnet/tests/fdio_spawn/return_arg_util.cc
+++ garnet/tests/fdio_spawn/return_arg_util.cc
@@ -2,17 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/lib/fxl/strings/string_number_conversions.h"
+#include <string>
 
 int main(int argc, const char* argv[]) {
   if (argc < 2) {
     return -1;
   }
-
-  const char* arg = argv[1];
-  int number;
-  if (!fxl::StringToNumberWithError<int>(arg, &number)) {
-    return -1;
-  }
-  return number;
+  // Won't handle stoi's exception because:
+  // 1) Exceptions are disabled.
+  // 2) This is just a basic echo program to be used by a test.
+  // 3) The test that calls this code does not feed any invalid inputs.
+  return std::stoi(std::string(argv[1]), nullptr, 10);
 }
