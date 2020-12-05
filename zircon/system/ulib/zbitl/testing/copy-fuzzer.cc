f9c29808acd6a72e2c6683c4145871dc3eac5595
Joshua Seaton <joshuaseaton@google.com>
2020-12-05 20:56:29 +0000

    [zbitl] Exercise VMO codepaths in copy fuzzer
    More coverage is always better, and this allows us to exercise the
    clone codepaths. No bugs were found.
    
    Bug: 62638
    Change-Id: I5e8b4f18c9459653e4247c2600b0f95659d6cb20
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/458156
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>

--- zircon/system/ulib/zbitl/testing/copy-fuzzer.cc
+++ zircon/system/ulib/zbitl/testing/copy-fuzzer.cc
@@ -8,6 +8,11 @@
 #include <zircon/assert.h>
 #include <zircon/boot/image.h>
 
+#ifdef __Fuchsia__
+#include <lib/zbitl/vmo.h>
+#include <lib/zx/vmo.h>
+#endif
+
 #include <cstddef>
 #include <iterator>
 #include <string>
@@ -15,10 +20,15 @@
 #include <fbl/algorithm.h>
 #include <fuzzer/FuzzedDataProvider.h>
 
-// The maximal allowed `to_offset` parameter to a byte-range copy method,
-// which will result of an allocation - inherently restricted by the fuzzing
-// framework - of that size.
-constexpr uint32_t kToOffsetMax = 0x100000;  // 1MiB.
+namespace {
+
+enum class StorageType {
+  kFblByteArray,
+#ifdef __Fuchsia__
+  kVmo,
+#endif
+  kMaxValue,  // Required by FuzzedDataProvider::ConsumeEnum().
+};
 
 // Corresponds to a particular `Copy` `View` method. See usage below for the
 // exact mapping.
@@ -34,26 +44,63 @@ enum class CopyCodepath {
   kMaxValue,  // Required by FuzzedDataProvider::ConsumeEnum().
 };
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  FuzzedDataProvider provider(data, size);
+// We abstract the main fuzzing logic below to allow for different storage
+// types, templating on a fuzz trait struct.expected to have the following
+// static members and methods:
+// * `static constexpr uint32_t kToOffsetMax`, giving the maximum allowed
+//   value of `to_offset` to be passed to the byte-range Copy signatures. The
+//   reason to restrict it - with copy-creation - is that it will result in a
+//   created storage object of size >= `to_offset`, which can easily exceed
+//   libFuzzer's memory cap, in certain cases. The cap is controlled by
+//   -rss_limit_mb flag, which limits the peak RSS of the whole process with
+//    all other overheads, and defaults to 2048MB.
+// * `static auto Convert(const std::string& zbi)`, converting a string
+//   representation of a ZBI to that of the associated storage type.
 
+struct FblByteArrayFuzzTraits {
+  // To stay within libFuzzer's allowed allocation budget (see above), which
+  // defaults to 2000MiB. Our choice of 1MiB stays well within this range.
+  static constexpr uint32_t kToOffsetMax = 0x100000;
+
+  static fbl::Array<std::byte> Convert(const std::string& zbi) {
+    fbl::Array<std::byte> array{new std::byte[zbi.size()], zbi.size()};
+    memcpy(array.data(), zbi.data(), zbi.size());
+    return array;
+  }
+};
+
+#ifdef __Fuchsia__
+struct VmoFuzzTraits {
+  // A VMO of "size" UINT32_MAX will not actually necessarily occupy that
+  // amount of space, but only that comprised by the number of pages written
+  // to, which should not be particularly large here.
+  static constexpr uint32_t kToOffsetMax = std::numeric_limits<uint32_t>::max();
+
+  static zx::vmo Convert(const std::string& zbi) {
+    zx::vmo vmo;
+    ZX_ASSERT(ZX_OK == zx::vmo::create(zbi.size(), 0, &vmo));
+    ZX_ASSERT(ZX_OK == vmo.write(zbi.data(), 0, zbi.size()));
+    return vmo;
+  }
+};
+#endif  // __Fuchsia__
+
+template <typename FuzzTraits>
+int Fuzz(FuzzedDataProvider& provider) {
   // Compute some parameters up front (not necessarily used), so that we can
   // consume the rest of the data to create the ZBI.
   const auto codepath = provider.ConsumeEnum<CopyCodepath>();
   const auto from_offset = provider.ConsumeIntegral<uint32_t>();
   const auto from_length = provider.ConsumeIntegral<uint32_t>();
-  const auto to_offset = provider.ConsumeIntegralInRange<uint32_t>(
-      0, kToOffsetMax - std::min(kToOffsetMax, from_length));
+  const auto to_offset = provider.ConsumeIntegralInRange<uint32_t>(0, FuzzTraits::kToOffsetMax);
 
-  // Use a fbl::Array-backed view to exercise the copy-creation codepaths.
   std::string zbi = provider.ConsumeRemainingBytesAsString();
-  fbl::Array<std::byte> array{new std::byte[zbi.size()], zbi.size()};
-  memcpy(array.data(), zbi.data(), zbi.size());
-  zbitl::View view(std::move(array));
+  auto storage = FuzzTraits::Convert(zbi);
+  zbitl::View view(std::move(storage));
 
   // Storage destination (only used in the kDirect* codepaths).
-  std::unique_ptr<std::byte[]> buff(new std::byte[size]);
-  fbl::Span<std::byte> to{buff.get(), size};
+  std::unique_ptr<std::byte[]> buff(new std::byte[zbi.size()]);
+  fbl::Span<std::byte> to{buff.get(), zbi.size()};
 
   // These two codepaths are per-view (and not per-iterator) and should not
   // affect the view's internal error state.
@@ -97,4 +144,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   view.ignore_error();
   return 0;
+}
+
+}  // namespace
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider provider(data, size);
+
+  switch (provider.ConsumeEnum<StorageType>()) {
+    case StorageType::kFblByteArray:
+      return Fuzz<FblByteArrayFuzzTraits>(provider);
+#ifdef __Fuchsia__
+    case StorageType::kVmo:
+      return Fuzz<VmoFuzzTraits>(provider);
+#endif
+    case StorageType::kMaxValue:  // Placeholder.
+      return 0;
+  };
 }
