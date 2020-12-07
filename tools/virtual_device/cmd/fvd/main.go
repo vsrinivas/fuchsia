e21c8f6e151c110e42f5b6b2477aec2fa42dabea
Kendal Harland <kjharland@google.com>
2020-12-07 16:45:34 +0000

    [tools][fvd] Add and validate the HardwareProfile
    * Adds the HardwareProfile portion of the FVD proto.
    * Updates the fvd binary and tests to validate portions
      of the profile.
    
    Bug: 62150
    Test:
     * fx set core.x64 --with=//tools/virtual_device:default
     * fx build
     * cat out/default/default.fvd prints the contents:
    
      {
        "name": "default",
        "kernel": "qemu-kernel",
        "initrd": "zircon-a",
        "drive": {
          "id": "maindisk",
          "image": "storage-full"
        },
        "hw": {
          "arch": "x64",
          "cpuCount": 4,
          "ram": "1M",
          "mac": "52:54:00:63:5e:7a"
        },
        "nodename": "default-virtual-device"
      }
    
    Change-Id: Ieafae493d368078b4427558d6c4ba31a99a063af
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/457144
    Commit-Queue: Kendal Harland <kjharland@google.com>
    Reviewed-by: Marc-Antoine Ruel <maruel@google.com>
    Reviewed-by: Ina Huh <ihuh@google.com>
    Testability-Review: Ina Huh <ihuh@google.com>

--- tools/virtual_device/cmd/fvd/main.go
+++ tools/virtual_device/cmd/fvd/main.go
@@ -2,33 +2,41 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// fvd generates a Fuchsia Virtual Device (.fvd) file for the current
-// product x arch image being built. It is meant to be run during the build.
+// fvd generates a Fuchsia Virtual Device (.fvd) file during the build.
+//
+// The FVD is generated from a default struct representing the final output,
+// build metadata, and the build's image.json manifest.
+//
+// This binary also validates the properties of the generated FVD and sets
+// default values where appropriate, even though launchers will often do so at
+// runtime.
+//
+// For the FVD schema, see //tools/virtual_device/proto/virtual_device.proto.
 package main
 
-// TODO(kjharland): Set QEMU metadata.
-
 import (
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
+	"regexp"
 
 	"github.com/golang/protobuf/jsonpb"
+	"go.fuchsia.dev/fuchsia/tools/build"
 	fvdpb "go.fuchsia.dev/fuchsia/tools/virtual_device/proto"
 )
 
-// The names of images to write to the FVD.
-//
-// In the future these might be configurable but for now we use the same images
-// for all emulator product x arch combinations.
-const (
-	imageNameKernel         = "qemu-kernel"
-	imageNameFVM            = "storage-full"
-	imageNameInitialRamdisk = "zircon-a"
+// Regular expressions for validating FVD properties.
+var (
+	// ramRe matches a system RAM size description, like '50G'.
+	//
+	// See //tools/virtual_device/proto/virtual_device.proto for a full description of the
+	// format.
+	ramRe = regexp.MustCompile(`^[0-9]+[bBkKmMgG]$`)
+
+	// macRe matches a MAC address.
+	macRe = regexp.MustCompile(`^([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}$`)
 )
 
 func main() {
@@ -39,24 +47,43 @@ func main() {
 }
 
 func mainImpl() error {
-	imagesJSONPath := flag.String("images_json", "", "The path to images.json")
+	imageManifestPath := flag.String("images_json", "", "The path to images.json")
 	outputPath := flag.String("output", "", "Where to write the .fvd file (Default: stdout)")
-	fvdName := flag.String("name", "default", "The virtual device name")
+	targetCPU := flag.String("buildinfo_cpu", "", "The target cpu architecture")
+
 	flag.Parse()
 
-	if *imagesJSONPath == "" {
-		return errors.New("missing --images_json")
+	if *imageManifestPath == "" {
+		return errors.New("missing -images_json")
 	}
-	if *fvdName == "" {
-		return errors.New("missing --name")
+	if *targetCPU == "" {
+		return errors.New("missing -buildinfo_cpu")
 	}
 
-	images, err := readImagesJSON(*imagesJSONPath)
+	images, err := build.LoadImages(*imageManifestPath)
 	if err != nil {
 		return err
 	}
 
-	fvd, err := generate(*fvdName, images)
+	fvd := &fvdpb.VirtualDevice{
+		Name:     "default",
+		Nodename: "fuchsia-virtual-device",
+		Kernel:   "qemu-kernel",
+		Initrd:   "zircon-a",
+		Drive: &fvdpb.Drive{
+			Id:    "maindisk",
+			Image: "storage-full",
+		},
+		Hw: &fvdpb.HardwareProfile{
+			Arch:     *targetCPU,
+			CpuCount: 1,
+			Ram:      "1M",
+			Mac:      "52:54:00:63:5e:7a",
+		},
+	}
+	if err := validateFVD(fvd, images); err != nil {
+		return fmt.Errorf("invalid FVD: %w", err)
+	}
 
 	var out io.Writer = os.Stdout
 	if *outputPath != "" {
@@ -72,54 +99,54 @@ func mainImpl() error {
 	return m.Marshal(out, fvd)
 }
 
-func generate(name string, images imagesJSON) (*fvdpb.VirtualDevice, error) {
-	fvd := &fvdpb.VirtualDevice{Name: name}
+// TODO(kjharland): Move this to a common pkg so launchers can call it at runtime.
+func validateFVD(fvd *fvdpb.VirtualDevice, images build.ImageManifest) error {
+	// Ensure the images referenced in the FVD exist in the image manifest.
+	var exists = struct{}{}
+	found := map[string]struct{}{}
 
-	// Ensure the image names written into the FVD are present in images.json.
-	for _, entry := range images {
-		switch entry.Name {
-		case imageNameKernel:
-			fvd.Kernel = entry.Name
-		case imageNameFVM:
-			fvd.Fvm = entry.Name
-		case imageNameInitialRamdisk:
-			fvd.Initrd = entry.Name
+	for _, image := range images {
+		found[image.Name] = exists
+	}
+	if _, ok := found[fvd.Kernel]; !ok {
+		return fmt.Errorf("kernel image %q not found", fvd.Kernel)
+	}
+	if _, ok := found[fvd.Initrd]; !ok {
+		return fmt.Errorf("initial ramdisk image %q not found", fvd.Initrd)
+	}
+
+	// If drive points to a file instead of an entry in the image manifest, the
+	// filepath will be checked at runtime instead, since it may not exist at
+	// the moment, or it may be a relative path that only makes sense at runtime
+	// (e.g. a MinFS image that is created during a test run).
+	if !fvd.Drive.IsFilename {
+		if _, ok := found[fvd.Drive.Image]; !ok {
+			return fmt.Errorf("drive image %q not found", fvd.Drive.Image)
 		}
 	}
 
-	if fvd.Kernel == "" {
-		return nil, errors.New("kernel image not found")
-	}
-	if fvd.Fvm == "" {
-		return nil, errors.New("fvm image not found")
-	}
-	if fvd.Initrd == "" {
-		return nil, errors.New("initial ramdisk not found")
+	if !isValidRAM(fvd.Hw.Ram) {
+		return fmt.Errorf("invalid ram: %q", fvd.Hw.Ram)
 	}
 
-	return fvd, nil
+	if !isValidArch(fvd.Hw.Arch) {
+		return fmt.Errorf("invalid arch: %q", fvd.Hw.Arch)
+	}
+
+	if !isValidMAC(fvd.Hw.Mac) {
+		return fmt.Errorf("invalid MAC address: %q", fvd.Hw.Mac)
+	}
+	return nil
 }
 
-// scope represents a JSON object entry in images.json.
-//
-// See `build_api_module("images")` in /BUILD.gn for documentation on the full
-// set of fields that may appear in the scope. This is only a subset needed to
-// generate the FVD proto.
-type scope struct {
-	Name string `json:"name"`
-	Path string `json:"path"`
+func isValidRAM(ram string) bool {
+	return ram == "" || ramRe.MatchString(ram)
 }
 
-type imagesJSON []scope
+func isValidArch(arch string) bool {
+	return arch == "" || arch == "x64" || arch == "arm64"
+}
 
-func readImagesJSON(filename string) (imagesJSON, error) {
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	var i imagesJSON
-	if err := json.Unmarshal(bytes, &i); err != nil {
-		return nil, err
-	}
-	return i, nil
+func isValidMAC(mac string) bool {
+	return mac == "" || macRe.MatchString(mac)
 }
--- tools/virtual_device/cmd/fvd/main_test.go
+++ tools/virtual_device/cmd/fvd/main_test.go
@@ -6,68 +6,151 @@ package main
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"go.fuchsia.dev/fuchsia/tools/build"
 	fvdpb "go.fuchsia.dev/fuchsia/tools/virtual_device/proto"
-	"google.golang.org/protobuf/testing/protocmp"
 )
 
-func TestGenerate(t *testing.T) {
+func TestValidateFVD(t *testing.T) {
+	// An ImageManifest shared by all test cases. This is not modified during testing.
+	testImageManifest := build.ImageManifest{
+		{Name: "qemu-kernel", Path: "/kernel"},
+		{Name: "storage-full", Path: "/fvm"},
+		{Name: "zircon-a", Path: "/ramdisk"},
+	}
+
+	// Test cases.
+	//
+	// setupFVD is used to initialize the FVD to pass to validateFVD. It is called with a
+	// known-good FVD as input, during test setup.
 	tests := []struct {
-		// Name of the test case, also conveniently used as the name of the FVD.
-		name    string
-		images  imagesJSON
-		want    *fvdpb.VirtualDevice
-		wantErr bool
+		name     string
+		setupFVD func(*fvdpb.VirtualDevice)
+		wantErr  bool
 	}{{
-		name: "ok",
-		images: imagesJSON{
-			{"qemu-kernel", "/kernel"},
-			{"storage-full", "/fvm"},
-			{"zircon-a", "/ramdisk"},
-		},
-		want: &fvdpb.VirtualDevice{
-			Name:   "ok",
-			Kernel: "qemu-kernel",
-			Fvm:    "storage-full",
-			Initrd: "zircon-a",
-		},
+		name:     "ok",
+		setupFVD: func(fvd *fvdpb.VirtualDevice) { return },
 	}, {
-		name: "missing kernel",
-		images: imagesJSON{
-			{"storage-full", "/fvm"},
-			{"zircon-a", "/ramdisk"},
-		},
-		wantErr: true,
+		name:     "missing kernel",
+		setupFVD: func(fvd *fvdpb.VirtualDevice) { fvd.Kernel = "" },
+		wantErr:  true,
 	}, {
-		name: "missing fvm",
-		images: imagesJSON{
-			{"qemu-kernel", "/kernel"},
-			{"zircon-a", "/ramdisk"},
-		},
-		wantErr: true,
+		name:     "missing drive",
+		setupFVD: func(fvd *fvdpb.VirtualDevice) { fvd.Drive.Image = "" },
+		wantErr:  true,
 	}, {
-		name: "missing initial ramdisk",
-		images: imagesJSON{
-			{"qemu-kernel", "/kernel"},
-			{"storage-full", "/fvm"},
-		},
-		wantErr: true,
+		name:     "missing initial ramdisk",
+		setupFVD: func(fvd *fvdpb.VirtualDevice) { fvd.Initrd = "" },
+		wantErr:  true,
+	}, {
+		name:     "invalid ram",
+		setupFVD: func(fvd *fvdpb.VirtualDevice) { fvd.Hw.Ram = "-1G" },
+		wantErr:  true,
+	}, {
+		name:     "invalid arch",
+		setupFVD: func(fvd *fvdpb.VirtualDevice) { fvd.Hw.Arch = "MIPS" },
+		wantErr:  true,
+	}, {
+		name:     "invalid mac",
+		setupFVD: func(fvd *fvdpb.VirtualDevice) { fvd.Hw.Mac = "0::12345" },
+		wantErr:  true,
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := generate(tt.name, tt.images)
-			if err != nil != tt.wantErr {
+			inputFVD := knownGoodFVD()
+			tt.setupFVD(inputFVD)
+
+			if err := validateFVD(inputFVD, testImageManifest); err != nil != tt.wantErr {
 				if tt.wantErr {
-					t.Fatalf("got error %v", err)
+					t.Fatalf("wanted an error but got nil")
 				}
-				t.Fatalf("wanted an error but got %v", got)
-			}
-			if tt.wantErr {
-				return // handled above.
-			}
-			if diff := cmp.Diff(got, tt.want, protocmp.Transform()); diff != "" {
-				t.Fatalf("got diff (+got,-want):\n%s\n", diff)
+				t.Fatalf("got error %v", err)
 			}
 		})
+	}
+}
+
+func TestIsValidRAM(t *testing.T) {
+	tests := []struct {
+		ram  string
+		want bool
+	}{
+		{"0", false},
+		{"1", false},
+		{"1Z", false},
+		{"b", false},
+		{"-22G", false},
+
+		{"0b", true},
+		{"0B", true},
+		{"1k", true},
+		{"1K", true},
+		{"16m", true},
+		{"16M", true},
+		{"256g", true},
+		{"256G", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.ram, func(t *testing.T) {
+			if isValidRAM(tt.ram) != tt.want {
+				t.Fatalf("isValidRAM(%q) got %v but wanted %v", tt.ram, !tt.want, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidArch(t *testing.T) {
+	tests := []struct {
+		arch string
+		want bool
+	}{
+		{"MIPS", false},
+
+		{"x64", true},
+		{"arm64", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.arch, func(t *testing.T) {
+			if isValidArch(tt.arch) != tt.want {
+				t.Fatalf("isValidArch(%q) got %v but wanted %v", tt.arch, !tt.want, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidMAC(t *testing.T) {
+	tests := []struct {
+		mac  string
+		want bool
+	}{
+		{"00", false},
+		{"00:11", false},
+		{"00:11:22", false},
+		{"00:11:22:33", false},
+		{"00:11:22:33:44", false},
+		{"00:11:22:33:44:55:", false},
+		{":00:11:22:33:44:55:", false},
+		{"00:11:22:33:44:55:66", false},
+		{"00:11:22:33:44:55:66", false},
+
+		{"00:11:22:33:44:55", true},
+		{"7d:af:25:e3:dd:0b", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.mac, func(t *testing.T) {
+			if isValidMAC(tt.mac) != tt.want {
+				t.Fatalf("isValidMAC(%q) got %v but wanted %v", tt.mac, !tt.want, tt.want)
+			}
+		})
+	}
+}
+
+func knownGoodFVD() *fvdpb.VirtualDevice {
+	return &fvdpb.VirtualDevice{
+		Kernel: "qemu-kernel",
+		Drive:  &fvdpb.Drive{Image: "storage-full"},
+		Initrd: "zircon-a",
+		Hw: &fvdpb.HardwareProfile{
+			Ram: "5G",
+		},
 	}
 }
