a4c684d93feaf967eda36e11e90e873faaa2617b
Vincent Belliard <vbelliard@google.com>
2020-12-09 14:45:59 +0000

    [fidl][llcpp] use read_etc for sync event handler
    Change-Id: I5f63a7b0efc2f6e7084253ebcaadca8ba21c1c9d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/457477
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Reviewed-by: Benjamin Prosnitz <bprosnitz@google.com>
    Testability-Review: Yifei Teng <yifeit@google.com>
    Commit-Queue: Nicolas Sylvain <nsylvain@google.com>

--- tools/fidl/fidlgen_llcpp/codegen/fragment_sync_event_handler.tmpl.go
+++ tools/fidl/fidlgen_llcpp/codegen/fragment_sync_event_handler.tmpl.go
@@ -36,13 +36,13 @@ const fragmentSyncEventHandlerTmpl = `
   })();
   ::fidl::internal::ByteStorage<kReadAllocSize> read_storage;
   uint8_t* read_bytes = read_storage.data();
-  zx_handle_t read_handles[kHandleAllocSize];
+  zx_handle_info_t read_handles[kHandleAllocSize];
   uint32_t actual_bytes;
   uint32_t actual_handles;
-  status = client_end->read(ZX_CHANNEL_READ_MAY_DISCARD,
-                            read_bytes, read_handles,
-                            kReadAllocSize, kHandleAllocSize,
-                            &actual_bytes, &actual_handles);
+  status = client_end->read_etc(ZX_CHANNEL_READ_MAY_DISCARD,
+                                read_bytes, read_handles,
+                                kReadAllocSize, kHandleAllocSize,
+                                &actual_bytes, &actual_handles);
   if (status == ZX_ERR_BUFFER_TOO_SMALL) {
     // Message size is unexpectedly larger than calculated.
     // This can only be due to a newer version of the protocol defining a new event,
@@ -53,13 +53,13 @@ const fragmentSyncEventHandlerTmpl = `
     return ::fidl::Result(status, ::fidl::kErrorReadFailed);
   }
   if (actual_bytes < sizeof(fidl_message_header_t)) {
-    zx_handle_close_many(read_handles, actual_handles);
+    FidlHandleInfoCloseMany(read_handles, actual_handles);
     return ::fidl::Result(ZX_ERR_INVALID_ARGS, ::fidl::kErrorInvalidHeader);
   }
   fidl_message_header_t* hdr = reinterpret_cast<fidl_message_header_t*>(read_bytes);
   status = fidl_validate_txn_header(hdr);
   if (status != ZX_OK) {
-    zx_handle_close_many(read_handles, actual_handles);
+    FidlHandleInfoCloseMany(read_handles, actual_handles);
     return ::fidl::Result(status, ::fidl::kErrorInvalidHeader);
   }
   switch (hdr->ordinal) {
@@ -67,8 +67,8 @@ const fragmentSyncEventHandlerTmpl = `
     {{- if not .HasRequest }}
     case {{ .OrdinalName }}: {
       const char* error_message;
-      zx_status_t status = fidl_decode({{ .Name }}Response::Type, read_bytes, actual_bytes,
-                                       read_handles, actual_handles, &error_message);
+      zx_status_t status = fidl_decode_etc({{ .Name }}Response::Type, read_bytes, actual_bytes,
+                                           read_handles, actual_handles, &error_message);
       if (status != ZX_OK) {
         return ::fidl::Result(status, error_message);
       }
@@ -78,7 +78,7 @@ const fragmentSyncEventHandlerTmpl = `
     {{- end }}
   {{- end }}
     default: {
-      FidlHandleCloseMany(read_handles, actual_handles);
+      FidlHandleInfoCloseMany(read_handles, actual_handles);
       return ::fidl::Result(Unknown(), nullptr);
     }
   }
