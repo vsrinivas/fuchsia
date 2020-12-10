52b16b923674cd908cb2aa7724cc53d8b2e43529
Roland McGrath <mcgrathr@google.com>
2020-12-10 03:47:02 +0000

    [kernel] Don't use mapping to mutate vDSO VMOs
    Use direct VMO Read/Write operations instead of mapping.
    This avoids the need to allow kernel page faults.
    
    Bug: 66141
    Change-Id: I63979b29988e82ca87e779b6efddee804593cddd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/460159
    Reviewed-by: Adrian Danis <adanis@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Testability-Review: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/lib/userabi/vdso.cc
+++ zircon/kernel/lib/userabi/vdso.cc
@@ -30,163 +30,123 @@ extern "C" const char vdso_image[];
 
 namespace {
 
-// Each KernelVmoWindow object represents a mapping in the kernel address
-// space of a T object found inside a VM object.  The kernel mapping exists
-// for the lifetime of the KernelVmoWindow object.
-template <typename T>
-class KernelVmoWindow {
+class VDsoMutator {
  public:
-  static_assert(__is_pod(T), "this is for C-compatible types only!");
+  explicit VDsoMutator(const fbl::RefPtr<VmObject>& vmo) : vmo_(vmo) {}
 
-  KernelVmoWindow(const char* name, fbl::RefPtr<VmObject> vmo, uint64_t offset)
-      : mapping_(nullptr) {
-    uint64_t page_offset = ROUNDDOWN(offset, PAGE_SIZE);
-    size_t offset_in_page = static_cast<size_t>(offset % PAGE_SIZE);
-    ASSERT(offset % alignof(T) == 0);
+  void RedirectSymbol(size_t idx1, size_t idx2, uintptr_t value) {
+    auto [sym1, sym2] = ReadSymbol(idx1, idx2);
 
-    const size_t size = offset_in_page + sizeof(T);
-    const uint arch_mmu_flags = ARCH_MMU_FLAG_PERM_READ | ARCH_MMU_FLAG_PERM_WRITE;
-    zx_status_t status = VmAspace::kernel_aspace()->RootVmar()->CreateVmMapping(
-        0 /* ignored */, size, 0 /* align pow2 */, 0 /* vmar flags */, ktl::move(vmo), page_offset,
-        arch_mmu_flags, name, &mapping_);
-    ASSERT(status == ZX_OK);
-    data_ = reinterpret_cast<T*>(mapping_->base() + offset_in_page);
+    // Just change the st_value of the symbol.
+    sym1.value = sym2.value = value;
+    WriteSymbol(idx1, sym1);
+    WriteSymbol(idx2, sym2);
   }
 
-  ~KernelVmoWindow() {
-    if (mapping_) {
-      zx_status_t status = mapping_->Destroy();
-      ASSERT(status == ZX_OK);
-    }
-  }
+  void BlockSymbol(size_t idx1, size_t idx2) {
+    auto [sym1, sym2] = ReadSymbol(idx1, idx2);
 
-  T* data() const { return data_; }
+    // First change the symbol to have local binding so it can't be resolved.
+    // The high nybble is the STB_* bits; STB_LOCAL is 0.
+    sym1.info &= 0xf;
+    sym2.info &= 0xf;
+    WriteSymbol(idx1, sym1);
+    WriteSymbol(idx2, sym2);
+
+    // Now fill the code region (a whole function) with safely invalid code.
+    // This code should never be run, and any attempt to use it should crash.
+    ASSERT(sym1.value >= VDSO_CODE_START);
+    ASSERT(sym1.value + sym1.size < VDSO_CODE_END);
+    zx_status_t status = vmo_->Write(GetTrapFill(sym1.size), sym1.value, sym1.size);
+    ASSERT_MSG(status == ZX_OK, "vDSO VMO Write failed: %d", status);
+  }
 
  private:
-  fbl::RefPtr<VmMapping> mapping_;
-  T* data_;
-};
-
-// The .dynsym section of the vDSO, an array of ELF symbol table entries.
-struct VDsoDynSym {
-  struct {
+  struct ElfSym {
     uintptr_t info, value, size;
-  } table[VDSO_DYNSYM_COUNT];
+  };
+
+#if ARCH_X86
+  // Fill with the single-byte HLT instruction, so any place
+  // user-mode jumps into this code, it gets a trap.
+  using Insn = uint8_t;
+  static constexpr Insn kTrapFill = 0xf4;  // hlt
+#elif ARCH_ARM64
+  // Fixed-size instructions.  Use 'brk #1' (what __builtin_trap() emits).
+  using Insn = uint32_t;
+  static constexpr Insn kTrapFill = 0xd4200020;
+#else
+#error what architecture?
+#endif
+
+  void* GetTrapFill(size_t fill_size) {
+    ASSERT(fill_size % sizeof(Insn) == 0);
+    fill_size /= sizeof(Insn);
+    if (fill_size > trap_fill_size_) {
+      fbl::AllocChecker ac;
+      trap_fill_.reset(new (&ac) Insn[fill_size]);
+      ASSERT(ac.check());
+      trap_fill_size_ = fill_size;
+      for (size_t i = 0; i < fill_size; ++i) {
+        trap_fill_[i] = kTrapFill;
+      }
+    }
+    return trap_fill_.get();
+  }
+
+  uintptr_t SymtabAddress(size_t idx) {
+    ASSERT(idx < VDSO_DYNSYM_COUNT);
+    return VDSO_DATA_START_dynsym + (idx * sizeof(ElfSym));
+  }
+
+  ElfSym ReadSymbol(size_t idx) {
+    ElfSym sym;
+    zx_status_t status = vmo_->Read(&sym, SymtabAddress(idx), sizeof(sym));
+    ASSERT_MSG(status == ZX_OK, "vDSO VMO Read failed: %d", status);
+    return sym;
+  }
+
+  ktl::array<ElfSym, 2> ReadSymbol(size_t idx1, size_t idx2) {
+    ElfSym sym1 = ReadSymbol(idx1);
+    ElfSym sym2 = ReadSymbol(idx2);
+    ASSERT_MSG(sym1.value == sym2.value, "dynsym %zu vs %zu value %#lx vs %#lx", idx2, idx2,
+               sym1.value, sym2.value);
+    ASSERT_MSG(sym1.size == sym2.size, "dynsym %zu vs %zu size %#lx vs %#lx", idx2, idx2, sym1.size,
+               sym2.size);
+    return {sym1, sym2};
+  }
+
+  void WriteSymbol(size_t idx, const ElfSym& sym) {
+    zx_status_t status = vmo_->Write(&sym, SymtabAddress(idx), sizeof(sym));
+    ASSERT_MSG(status == ZX_OK, "vDSO VMO Write failed: %d", status);
+  }
+
+  const fbl::RefPtr<VmObject>& vmo_;
+  ktl::unique_ptr<Insn[]> trap_fill_;
+  size_t trap_fill_size_ = 0;
 };
 
 #define PASTE(a, b, c) PASTE_1(a, b, c)
 #define PASTE_1(a, b, c) a##b##c
 
-class VDsoDynSymWindow {
- public:
-  DISALLOW_COPY_ASSIGN_AND_MOVE(VDsoDynSymWindow);
-
-  static_assert(sizeof(VDsoDynSym) == VDSO_DATA_END_dynsym - VDSO_DATA_START_dynsym,
-                "either VDsoDynsym or gen-rodso-code.sh is suspect");
-
-  explicit VDsoDynSymWindow(fbl::RefPtr<VmObject> vmo)
-      : window_("vDSO .dynsym", ktl::move(vmo), VDSO_DATA_START_dynsym) {}
-
-  void get_symbol_entry(size_t i, uintptr_t* value, size_t* size) {
-    *value = window_.data()->table[i].value;
-    *size = window_.data()->table[i].size;
-  }
-
-  void set_symbol_entry(size_t i, uintptr_t value, size_t size) {
-    window_.data()->table[i].value = value;
-    window_.data()->table[i].size = size;
-  }
-
-  void localize_symbol_entry(size_t i) {
-    // The high nybble is the STB_* bits; STB_LOCAL is 0.
-    window_.data()->table[i].info &= 0xf;
-  }
-
-#define get_symbol(symbol, value, size) get_symbol_entry(PASTE(VDSO_DYNSYM_, symbol, ), value, size)
-
-#define set_symbol(symbol, target)                                             \
-  set_symbol_entry(PASTE(VDSO_DYNSYM_, symbol, ), PASTE(VDSO_CODE_, target, ), \
-                   PASTE(VDSO_CODE_, target, _SIZE))
-
-#define localize_symbol(symbol) localize_symbol_entry(PASTE(VDSO_DYNSYM_, symbol, ))
-
- private:
-  KernelVmoWindow<VDsoDynSym> window_;
-};
-
-class VDsoCodeWindow {
- public:
-  DISALLOW_COPY_ASSIGN_AND_MOVE(VDsoCodeWindow);
-
-  using CodeBuffer = uint8_t[VDSO_CODE_END - VDSO_CODE_START];
-
-  explicit VDsoCodeWindow(fbl::RefPtr<VmObject> vmo)
-      : window_("vDSO code segment", ktl::move(vmo), VDSO_CODE_START) {}
-
-  // Fill the given code region (a whole function) with safely invalid code.
-  // This code should never be run, and any attempt to use it should crash.
-  void block_execution(uintptr_t address, size_t size) {
-    ASSERT(address >= VDSO_CODE_START);
-    ASSERT(address + size < VDSO_CODE_END);
-    address -= VDSO_CODE_START;
-
-#if ARCH_X86
-
-    // Fill with the single-byte HLT instruction, so any place
-    // user-mode jumps into this code, it gets a trap.
-    memset(&Code()[address], 0xf4, size);
-
-#elif ARCH_ARM64
-
-    // Fixed-size instructions.
-    ASSERT(address % 4 == 0);
-    ASSERT(size % 4 == 0);
-    uint32_t* code = reinterpret_cast<uint32_t*>(&Code()[address]);
-    for (size_t i = 0; i < size / 4; ++i)
-      code[i] = 0xd4200020;  // 'brk #1' (what __builtin_trap() emits)
-
-#else
-#error what architecture?
-#endif
-  }
-
- private:
-  CodeBuffer& Code() { return *window_.data(); }
-
-  KernelVmoWindow<CodeBuffer> window_;
-};
-
-#define REDIRECT_SYSCALL(dynsym_window, symbol, target) \
-  do {                                                  \
-    dynsym_window.set_symbol(symbol, target);           \
-    dynsym_window.set_symbol(_##symbol, target);        \
-  } while (0)
+#define REDIRECT_SYSCALL(mutator, symbol, target)                                       \
+  mutator.RedirectSymbol(PASTE(VDSO_DYNSYM_, symbol, ), PASTE(VDSO_DYNSYM__, symbol, ), \
+                         PASTE(VDSO_CODE_, target, ))
 
 // Block the named zx_* function.  The symbol table entry will
 // become invisible to runtime symbol resolution, and the code of
 // the function will be clobbered with trapping instructions.
-#define BLOCK_SYSCALL(dynsym_window, code_window, symbol)   \
-  do {                                                      \
-    dynsym_window.localize_symbol(symbol);                  \
-    dynsym_window.localize_symbol(_##symbol);               \
-    uintptr_t address, _address;                            \
-    size_t size, _size;                                     \
-    dynsym_window.get_symbol(symbol, &address, &size);      \
-    dynsym_window.get_symbol(_##symbol, &_address, &_size); \
-    ASSERT(address == _address);                            \
-    ASSERT(size == _size);                                  \
-    code_window.block_execution(address, size);             \
-  } while (0)
+#define BLOCK_SYSCALL(mutator, symbol) \
+  mutator.BlockSymbol(PASTE(VDSO_DYNSYM_, symbol, ), PASTE(VDSO_DYNSYM__, symbol, ))
 
 // Random attributes in kazoo fidl files become "categories" of syscalls.
 // For each category, define a function block_<category> to block all the
 // syscalls in that category.  These functions can be used in
 // VDso::CreateVariant (below) to block a category of syscalls for a particular
 // variant vDSO.
-#define SYSCALL_CATEGORY_BEGIN(category)                                             \
-  [[maybe_unused]] void block_##category##_syscalls(VDsoDynSymWindow& dynsym_window, \
-                                                    VDsoCodeWindow& code_window) {
-#define SYSCALL_IN_CATEGORY(syscall) BLOCK_SYSCALL(dynsym_window, code_window, zx_##syscall);
+#define SYSCALL_CATEGORY_BEGIN(category) \
+  [[maybe_unused]] void block_##category##_syscalls(VDsoMutator& mutator) {
+#define SYSCALL_IN_CATEGORY(syscall) BLOCK_SYSCALL(mutator, zx_##syscall);
 #define SYSCALL_CATEGORY_END(category) }
 #include <lib/syscalls/category.inc>
 #undef SYSCALL_CATEGORY_BEGIN
@@ -198,33 +158,15 @@ using VdsoBuildIdNote = ktl::array<uint8_t, VDSO_BUILD_ID_NOTE_SIZE>;
 constexpr VdsoBuildIdNote kVdsoBuildIdNote = VDSO_BUILD_ID_NOTE_BYTES;
 
 // That should exactly match the note read from the vDSO image at runtime.
-KernelVmoWindow<VdsoBuildIdNote> VdsoBuildIdNoteWindow(VDso* vdso) {
-  return {"vDSO build ID", vdso->vmo()->vmo(), VDSO_BUILD_ID_NOTE_ADDRESS};
+void CheckBuildId(const fbl::RefPtr<VmObject>& vmo) {
+  VdsoBuildIdNote note;
+  zx_status_t status = vmo->Read(&note, VDSO_BUILD_ID_NOTE_ADDRESS, sizeof(note));
+  ASSERT_MSG(status == ZX_OK, "vDSO VMO Read failed: %d", status);
+  ASSERT(note == kVdsoBuildIdNote);
 }
 
-}  // anonymous namespace
-
-const VDso* VDso::instance_ = NULL;
-
-// Private constructor, can only be called by Create (below).
-VDso::VDso(KernelHandle<VmObjectDispatcher>* vmo_kernel_handle)
-    : RoDso("vdso/full", vdso_image, VDSO_CODE_END, VDSO_CODE_START, vmo_kernel_handle) {}
-
-// This is called exactly once, at boot time.
-const VDso* VDso::Create(KernelHandle<VmObjectDispatcher>* vmo_kernel_handles) {
-  ASSERT(!instance_);
-
-  fbl::AllocChecker ac;
-  VDso* vdso = new (&ac) VDso(&vmo_kernel_handles[0]);
-  ASSERT(ac.check());
-
-  // Sanity-check that it's the exact vDSO image the kernel was compiled for.
-  ASSERT(*VdsoBuildIdNoteWindow(vdso).data() == kVdsoBuildIdNote);
-
-  // Map a window into the VMO to write the vdso_constants struct.
-  static_assert(sizeof(vdso_constants) == VDSO_DATA_CONSTANTS_SIZE, "gen-rodso-code.sh is suspect");
-  KernelVmoWindow<vdso_constants> constants_window("vDSO constants", vdso->vmo()->vmo(),
-                                                   VDSO_DATA_CONSTANTS);
+// Fill out the contents of the vdso_constants struct.
+void SetConstants(const fbl::RefPtr<VmObject>& vmo) {
   zx_ticks_t per_second = ticks_per_second();
 
   // Grab a copy of the ticks to mono ratio; we need this to initialize the
@@ -242,8 +184,7 @@ const VDso* VDso::Create(KernelHandle<VmObjectDispatcher>* vmo_kernel_handles) {
   // Rather than assigning each member individually, do this with
   // struct assignment and a compound literal so that the compiler
   // can warn if the initializer list omits any member.
-  auto constants = constants_window.data();
-  *constants = vdso_constants{
+  vdso_constants constants = {
       arch_max_num_cpus(),
       {
           arch_cpu_features(),
@@ -259,13 +200,17 @@ const VDso* VDso::Create(KernelHandle<VmObjectDispatcher>* vmo_kernel_handles) {
       strlen(version_string()),
       "",
   };
-  ASSERT(constants->version_string_len < sizeof(constants->version_string));
-  memcpy(constants->version_string, version_string(), constants->version_string_len);
+  ASSERT(constants.version_string_len < sizeof(constants.version_string));
+  memcpy(constants.version_string, version_string(), constants.version_string_len);
 
-  // Conditionally patch some of the entry points related to time based on
-  // platform details which get determined at runtime.
-  VDsoDynSymWindow dynsym_window(vdso->vmo()->vmo());
+  static_assert(sizeof(constants) == VDSO_DATA_CONSTANTS_SIZE, "gen-rodso-code.sh is suspect");
+  zx_status_t status = vmo->Write(&constants, VDSO_DATA_CONSTANTS, sizeof(constants));
+  ASSERT_MSG(status == ZX_OK, "vDSO VMO Write failed: %d", status);
+}
 
+// Conditionally patch some of the entry points related to time based on
+// platform details which get determined at runtime.
+void PatchTimeSyscalls(VDsoMutator mutator) {
   // If user mode cannot access the tick counter registers, or kernel command
   // line arguments demand that we access the tick counter via a syscall
   // instead of direct observation, then we need to make sure to redirect
@@ -279,7 +224,7 @@ const VDso* VDso::Create(KernelHandle<VmObjectDispatcher>* vmo_kernel_handles) {
       gCmdline.GetBool("vdso.clock_get_monotonic_force_syscall", false);
 
   if (need_syscall_for_ticks) {
-    REDIRECT_SYSCALL(dynsym_window, zx_ticks_get, SYSCALL_zx_ticks_get_via_kernel);
+    REDIRECT_SYSCALL(mutator, zx_ticks_get, SYSCALL_zx_ticks_get_via_kernel);
   }
 #if ARCH_ARM64
   else {
@@ -299,7 +244,7 @@ const VDso* VDso::Create(KernelHandle<VmObjectDispatcher>* vmo_kernel_handles) {
       } else {
         dprintf(INFO, "Installing A73 quirks for zx_ticks_get in VDSO\n");
       }
-      REDIRECT_SYSCALL(dynsym_window, zx_ticks_get, ticks_get_arm_a73);
+      REDIRECT_SYSCALL(mutator, zx_ticks_get, ticks_get_arm_a73);
     }
   }
 #endif
@@ -308,16 +253,40 @@ const VDso* VDso::Create(KernelHandle<VmObjectDispatcher>* vmo_kernel_handles) {
     // Force a syscall for zx_clock_get_monotonic if instructed to do so by the
     // kernel command line arguments.  Make sure to swap out the implementation
     // of zx_deadline_after as well.
-    REDIRECT_SYSCALL(dynsym_window, zx_clock_get_monotonic,
-                     SYSCALL_zx_clock_get_monotonic_via_kernel);
-    REDIRECT_SYSCALL(dynsym_window, zx_deadline_after, deadline_after_via_kernel_mono);
+    REDIRECT_SYSCALL(mutator, zx_clock_get_monotonic, SYSCALL_zx_clock_get_monotonic_via_kernel);
+    REDIRECT_SYSCALL(mutator, zx_deadline_after, deadline_after_via_kernel_mono);
   } else if (need_syscall_for_ticks) {
     // If ticks must be accessed via syscall, then choose the alternate form
     // for clock_get_monotonic which performs the scaling in user mode, but
     // thunks into the kernel to read the ticks register.
-    REDIRECT_SYSCALL(dynsym_window, zx_clock_get_monotonic, clock_get_monotonic_via_kernel_ticks);
-    REDIRECT_SYSCALL(dynsym_window, zx_deadline_after, deadline_after_via_kernel_ticks);
+    REDIRECT_SYSCALL(mutator, zx_clock_get_monotonic, clock_get_monotonic_via_kernel_ticks);
+    REDIRECT_SYSCALL(mutator, zx_deadline_after, deadline_after_via_kernel_ticks);
   }
+}
+
+}  // anonymous namespace
+
+const VDso* VDso::instance_ = NULL;
+
+// Private constructor, can only be called by Create (below).
+VDso::VDso(KernelHandle<VmObjectDispatcher>* vmo_kernel_handle)
+    : RoDso("vdso/full", vdso_image, VDSO_CODE_END, VDSO_CODE_START, vmo_kernel_handle) {}
+
+// This is called exactly once, at boot time.
+const VDso* VDso::Create(KernelHandle<VmObjectDispatcher>* vmo_kernel_handles) {
+  ASSERT(!instance_);
+
+  fbl::AllocChecker ac;
+  VDso* vdso = new (&ac) VDso(&vmo_kernel_handles[0]);
+  ASSERT(ac.check());
+
+  // Sanity-check that it's the exact vDSO image the kernel was compiled for.
+  CheckBuildId(vdso->vmo()->vmo());
+
+  // Fill out the contents of the vdso_constants struct.
+  SetConstants(vdso->vmo()->vmo());
+
+  PatchTimeSyscalls(VDsoMutator{vdso->vmo()->vmo()});
 
   DEBUG_ASSERT(!(vdso->vmo_rights() & ZX_RIGHT_WRITE));
   for (size_t v = static_cast<size_t>(Variant::FULL) + 1; v < static_cast<size_t>(Variant::COUNT);
@@ -352,19 +321,18 @@ void VDso::CreateVariant(Variant variant, KernelHandle<VmObjectDispatcher>* vmo_
   zx_status_t status = vmo()->CreateChild(ZX_VMO_CHILD_COPY_ON_WRITE, 0, size(), false, &new_vmo);
   ASSERT(status == ZX_OK);
 
-  VDsoDynSymWindow dynsym_window(new_vmo);
-  VDsoCodeWindow code_window(new_vmo);
+  VDsoMutator mutator{new_vmo};
 
   const char* name = nullptr;
   switch (variant) {
     case Variant::TEST1:
       name = "vdso/test1";
-      block_test_category1_syscalls(dynsym_window, code_window);
+      block_test_category1_syscalls(mutator);
       break;
 
     case Variant::TEST2:
       name = "vdso/test2";
-      block_test_category2_syscalls(dynsym_window, code_window);
+      block_test_category2_syscalls(mutator);
       break;
 
     // No default case so the compiler will warn about new enum entries.
