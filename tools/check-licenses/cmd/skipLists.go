8de0bd4c58b0717d60c10e54113fa6ee6c6c5b75
omer <omerlevran@google.com>
2020-12-11 19:44:33 +0000

    [licenses] - add more license patterns
    Bug: 66052, b/173235782, b/172069467, b/173236669, b/173234622, b/173234530, b/173234811, b/173233340, b/173235429
    Change-Id: Idae253726ff4ddb5e41dfbbd9269f78099d3930b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/458456
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Testability-Review: Adam Barth <abarth@google.com>

--- tools/check-licenses/cmd/skipLists.go
+++ tools/check-licenses/cmd/skipLists.go
@@ -6,45 +6,36 @@ package main
 
 var additionalSkipDirs = []string{
 	"build/secondary/third_party/llvm",
-	"prebuilt",                                                           // TODO(b/172076124): Remove once completed.
-	"prebuilt/third_party/flutter",                                       // TODO(b/169676435): Remove once completed.
-	"prebuilt/third_party/llvm",                                          // TODO(b/172076113): Remove once completed.
-	"prebuilt/third_party/ovmf",                                          // TODO(fxb/59350): Remove once completed.
-	"prebuilt/third_party/zlib",                                          // TODO(b/172066115): Remove once completed.
-	"prebuilt/virtualization/packages/termina_guest",                     // TODO(b/171975485): Remove once completed.
-	"src/connectivity/wlan/drivers/third_party/mediatek",                 // TODO(b/172586985): Remove once completed.
-	"third_party/catapult",                                               // TODO(b/171586646): Remove once completed.
-	"third_party/cobalt_config",                                          // TODO(b/172586985): Remove once completed.
-	"third_party/crashpad/third_party/apple_cf",                          // TODO(b/172586985): Remove once completed.
-	"third_party/crashpad/third_party/getopt",                            // TODO(b/172586985): Remove once completed.
-	"third_party/crashpad/third_party/lss",                               // TODO(b/172586985): Remove once completed.
-	"third_party/crashpad/third_party/mini_chromium",                     // TODO(b/172586985): Remove once completed.
-	"third_party/crashpad/third_party/xnu",                               // TODO(b/172586985): Remove once completed.
-	"third_party/dart-pkg/pub/characters/third_party/Unicode_Consortium", // TODO(b/172586985): Remove once completed.
-	"third_party/dart-pkg/pub/characters/third_party/Wikipedia",          // TODO(b/172586985): Remove once completed.
-	"third_party/gperftools/src/third_party",                             // TODO(b/172586985): Remove once completed.
-	"third_party/grpc/third_party/cares",                                 // TODO(b/172586985): Remove once completed.
-	"third_party/libc-tests/third_party/nacl-tests",                      // TODO(b/172586985): Remove once completed.
-	"third_party/markupsafe",                                             // TODO(b/172586985): Remove once completed.
-	"third_party/mesa",                                                   // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/build_gn",                        // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/jlink",                           // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/mbedtls",                         // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/microchip",                       // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/NordicSemiconductor",             // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/nxp",                             // TODO(b/172066115): Remove once completed.
-	"third_party/openthread/third_party/openthread-test-driver",          // TODO(b/171816602): Remove once completed.
-	"third_party/openthread/third_party/Qorvo",                           // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/silabs",                          // TODO(b/172586985): Remove once completed.
-	"third_party/openthread/third_party/ti",                              // TODO(b/172066853): Remove once completed.
-	"third_party/rust_crates/compat",                                     // TODO(b/172586985): Remove once completed.
-	"third_party/rust_crates/tiny_mirrors",                               // TODO(b/172586985): Remove once completed.
-	"third_party/tink/third_party/wycheproof",                            // TODO(b/172586985): Remove once completed.
-	"third_party/vim",                                                    // TODO(b/172066343): Remove once completed.
-	"third_party/zlib",                                                   // TODO(b/172069467): Remove once completed.
-	"zircon/third_party/scudo",                                           // TODO(b/172586985): Remove once completed.
-	"zircon/third_party/zstd",                                            // TODO(b/171584331): Remove once completed.
-	"src/devices/tools/banjo/src/backends/templates",                     // Skip template directories
+	"prebuilt",                                                  // TODO(b/172076124): Remove once completed.
+	"prebuilt/third_party/flutter",                              // TODO(b/169676435): Remove once completed.
+	"prebuilt/third_party/llvm",                                 // TODO(b/172076113): Remove once completed.
+	"prebuilt/third_party/ovmf",                                 // TODO(fxb/59350): Remove once completed.
+	"prebuilt/third_party/zlib",                                 // TODO(b/172066115): Remove once completed.
+	"prebuilt/virtualization/packages/termina_guest",            // TODO(b/171975485): Remove once completed.
+	"src/connectivity/wlan/drivers/third_party/mediatek",        // TODO(b/172586985): Remove once completed.
+	"third_party/crashpad/third_party/apple_cf",                 // TODO(b/173233942): Remove once completed.
+	"third_party/crashpad/third_party/getopt",                   // TODO(b/173234393): Remove once completed.
+	"third_party/crashpad/third_party/xnu",                      // TODO(b/173234731): Remove once completed.
+	"third_party/catapult",                                      // TODO(b/171586646): Remove once completed.
+	"third_party/cobalt_config",                                 // TODO(b/172586985): Remove once completed.
+	"third_party/dart-pkg/pub/characters/third_party/Wikipedia", // TODO(b/172586985): Remove once completed.
+	"third_party/grpc/third_party/cares",                        // TODO(b/172586985): Remove once completed.
+	"third_party/libc-tests/third_party/nacl-tests",             // TODO(b/172586985): Remove once completed.
+	"third_party/openthread/third_party/build_gn",               // TODO(b/172586985): Remove once completed.
+	"third_party/openthread/third_party/jlink",                  // TODO(b/172586985): Remove once completed.
+	"third_party/openthread/third_party/microchip",              // TODO(b/173236215): Remove once completed.
+	"third_party/openthread/third_party/NordicSemiconductor",    // TODO(b/172586985): Remove once completed.
+	"third_party/openthread/third_party/nxp",                    // TODO(b/172066115): Remove once completed.
+	"third_party/openthread/third_party/openthread-test-driver", // TODO(b/171816602): Remove once completed.
+	"third_party/openthread/third_party/Qorvo",                  // TODO(b/172586985): Remove once completed.
+	"third_party/openthread/third_party/silabs",                 // TODO(b/172586985): Remove once completed.
+	"third_party/openthread/third_party/ti",                     // TODO(b/172066853): Remove once completed.
+	"third_party/rust_crates/compat",                            // TODO(b/172586985): Remove once completed.
+	"third_party/rust_crates/tiny_mirrors",                      // TODO(b/172586985): Remove once completed.
+	"third_party/tink/third_party/wycheproof",                   // TODO(b/172586985): Remove once completed.
+	"third_party/vim",                                           // TODO(b/172066343): Remove once completed.
+	"zircon/third_party/zstd",                                   // TODO(b/171584331): Remove once completed.
+	"src/devices/tools/banjo/src/backends/templates",            // Skip template directories
 }
 
 var additionalSkipFiles = []string{
