d527d4ec1c27c11bd268e13923d4832fe2f64737
Yecheng Zhao <zyecheng@google.com>
2020-12-11 20:13:54 +0000

    [firmware][sdk]Fixed comment style.
    Fixed comment style for zircon_boot library in firmware sdk.
    
    Change-Id: Ib7092ae4b1ddb507479c8dba3362cdec6f4f8add
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/460581
    Reviewed-by: David Pursell <dpursell@google.com>
    Testability-Review: David Pursell <dpursell@google.com>
    Commit-Queue: Yecheng Zhao <zyecheng@google.com>

--- src/firmware/lib/zircon_boot/include/lib/zircon_boot/zircon_boot.h
+++ src/firmware/lib/zircon_boot/include/lib/zircon_boot/zircon_boot.h
@@ -54,76 +54,66 @@ struct ZirconBootOps {
   // Device-specific data.
   void* context;
 
-  /**
-   * Reads from a partition.
-   *
-   * @ops: Pointer to the host |ZirconBootOps|.
-   * @part: Name of the partition.
-   * @offset: Offset in the partition to read from.
-   * @size: Number of bytes to read.
-   * @dst: Output buffer.
-   * @read_size: Output pointer for storing the actual number of bytes read. The libary uses
-   * this value to verify the integrity of read data. It expects it to always equal the
-   * requested number of bytes.
-   *
-   * Returns true on success.
-   */
+  // Reads from a partition.
+  //
+  // @ops: Pointer to the host |ZirconBootOps|.
+  // @part: Name of the partition.
+  // @offset: Offset in the partition to read from.
+  // @size: Number of bytes to read.
+  // @dst: Output buffer.
+  // @read_size: Output pointer for storing the actual number of bytes read. The libary uses
+  // this value to verify the integrity of read data. It expects it to always equal the
+  // requested number of bytes.
+  //
+  // Returns true on success.
   bool (*read_from_partition)(ZirconBootOps* ops, const char* part, size_t offset, size_t size,
                               void* dst, size_t* read_size);
 
-  /**
-   * Writes data to partition.
-   *
-   * @ops: Pointer to the host |ZirconBootOps|.
-   * @part: Name of the partition.
-   * @offset: Offset in the partition to write to.
-   * @size: Number of bytes to write.
-   * @src: Payload buffer.
-   * @write_size: Output pointer for storing the actual number of bytes written. The library
-   * uses this value to verify the integrity of write data. It expects it to always equal the
-   * number of bytes requested to be written.
-   *
-   * Returns true on success.
-   */
+  // Writes data to partition.
+  //
+  // @ops: Pointer to the host |ZirconBootOps|.
+  // @part: Name of the partition.
+  // @offset: Offset in the partition to write to.
+  // @size: Number of bytes to write.
+  // @src: Payload buffer.
+  // @write_size: Output pointer for storing the actual number of bytes written. The library
+  // uses this value to verify the integrity of write data. It expects it to always equal the
+  // number of bytes requested to be written.
+  //
+  // Returns true on success.
   bool (*write_to_partition)(ZirconBootOps* ops, const char* part, size_t offset, size_t size,
                              const void* src, size_t* write_size);
 
-  /**
-   * Boots image in memory.
-   *
-   * @ops: Pointer to the host |ZirconBootOps|.
-   * @image: Pointer to the zircon kernel image in memory. It is expected to be a zbi
-   *         container.
-   * @capacity: Capacity of the container.
-   * @slot: The A/B/R slot index of the image.
-   *
-   * The function is not expected to return if boot is successful.
-   */
+  // Boots image in memory.
+  //
+  // @ops: Pointer to the host |ZirconBootOps|.
+  // @image: Pointer to the zircon kernel image in memory. It is expected to be a zbi
+  //        container.
+  // @capacity: Capacity of the container.
+  // @slot: The A/B/R slot index of the image.
+  //
+  // The function is not expected to return if boot is successful.
   void (*boot)(ZirconBootOps* ops, zbi_header_t* image, size_t capacity, AbrSlotIndex slot);
 
-  /**
-   * Gets the A/B/R slot of the currrently running firmware. The method needs to be implemented
-   * to support firmware A/B/R logic. Specifically, device needs to find a way to store and pass
-   * this information from non-A/B/R bootloader, i.e. stashing in registers.
-   *
-   * LoadAndBoot() will check whether this pointer is NULL to decide whether to boot according to
-   * OS ABR or firmware ABR.
-   *
-   * @ops: Pointer to the host |ZirconBootOps|
-   * @out_slot: An output pointer for returning firmware slot
-   *
-   * Returns true on success.
-   */
+  // Gets the A/B/R slot of the currrently running firmware. The method needs to be implemented
+  // to support firmware A/B/R logic. Specifically, device needs to find a way to store and pass
+  // this information from non-A/B/R bootloader, i.e. stashing in registers.
+  //
+  // LoadAndBoot() will check whether this pointer is NULL to decide whether to boot according to
+  // OS ABR or firmware ABR.
+  //
+  // @ops: Pointer to the host |ZirconBootOps|
+  // @out_slot: An output pointer for returning firmware slot
+  //
+  // Returns true on success.
   bool (*get_firmware_slot)(ZirconBootOps* ops, AbrSlotIndex* out_slot);
 
-  /**
-   * Reboots the device.
-   *
-   * @ops: Pointer to the host |ZirconBootOps|
-   * @force_recovery: Enable/Disable force recovery.
-   *
-   * The function is not expected to return if reboot is successful.
-   */
+  // Reboots the device.
+  //
+  // @ops: Pointer to the host |ZirconBootOps|
+  // @force_recovery: Enable/Disable force recovery.
+  //
+  // The function is not expected to return if reboot is successful.
   void (*reboot)(ZirconBootOps* ops, bool force_recovery);
 
   // Adds device-specific ZBI items based on available boot information. The method is optional and
@@ -144,17 +134,15 @@ typedef enum ForceRecovery {
   kForceRecoveryOff,
 } ForceRecovery;
 
-/**
- * Loads kernel image into memory and boots it. if ops.get_firmware_slot is set, the function
- * boots according to firwmare ABR. Otherwise it boots according to OS ABR.
- *
- * @ops: Required operations.
- * @load_address: Pointer to the target memory to load image.
- * @load_address_size: Maximum size of the memory for loading the image.
- * @force_recovery: Enable/Disable force recovery.
- *
- * The function is not expected to return if boot is successful.
- */
+// Loads kernel image into memory and boots it. if ops.get_firmware_slot is set, the function
+// boots according to firwmare ABR. Otherwise it boots according to OS ABR.
+//
+// @ops: Required operations.
+// @load_address: Pointer to the target memory to load image.
+// @load_address_size: Maximum size of the memory for loading the image.
+// @force_recovery: Enable/Disable force recovery.
+//
+// The function is not expected to return if boot is successful.
 ZirconBootResult LoadAndBoot(ZirconBootOps* ops, void* load_address, size_t load_address_size,
                              ForceRecovery force_recovery);
 
