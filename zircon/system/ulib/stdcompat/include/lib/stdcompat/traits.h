2955a841ccbb6e30032d5bc6cab72ed15857e51c
Gianfranco Valentino <gevalentino@google.com>
2020-12-15 23:07:21 +0000

    [stdcompat]: Branch traits and tests.
    Copy fit/traits.h and its test into std compat.
    Files not yet added into the build. The goal of this patch is merely to
    keep track of the source of the file.
    
    Bug: 57349
    TEST=NONE
    
    Change-Id: I545b55bf126a258b82b352ba3c6a4eea26cc9d81
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/461526
    Reviewed-by: John Grossman <johngro@google.com>
    Testability-Review: Gianfranco Valentino <gevalentino@google.com>
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>

---
+++ zircon/system/ulib/stdcompat/include/lib/stdcompat/traits.h
@@ -0,0 +1,156 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_FIT_TRAITS_H_
+#define LIB_FIT_TRAITS_H_
+
+#include <tuple>
+#include <type_traits>
+
+namespace fit {
+
+// C++14 compatible polyfill for C++17 traits.
+
+#if defined(__cplusplus) && __cplusplus >= 201703L
+
+template <typename... T>
+using void_t = std::void_t<T...>;
+
+template <typename... Ts>
+using conjunction = std::conjunction<Ts...>;
+template <typename... Ts>
+inline constexpr bool conjunction_v = std::conjunction_v<Ts...>;
+
+template <typename... Ts>
+using disjunction = std::disjunction<Ts...>;
+template <typename... Ts>
+inline constexpr bool disjunction_v = std::disjunction_v<Ts...>;
+
+template <typename... Ts>
+using negation = std::negation<Ts...>;
+template <typename... Ts>
+inline constexpr bool negation_v = std::negation_v<Ts...>;
+
+#else
+
+template <typename... T>
+struct make_void {
+  typedef void type;
+};
+template <typename... T>
+using void_t = typename make_void<T...>::type;
+
+template <typename... Ts>
+struct conjunction : std::true_type {};
+template <typename T>
+struct conjunction<T> : T {};
+template <typename First, typename... Rest>
+struct conjunction<First, Rest...>
+    : std::conditional_t<bool(First::value), conjunction<Rest...>, First> {};
+
+template <typename... Ts>
+constexpr bool conjunction_v = conjunction<Ts...>::value;
+
+template <typename... Ts>
+struct disjunction : std::false_type {};
+template <typename T>
+struct disjunction<T> : T {};
+template <typename First, typename... Rest>
+struct disjunction<First, Rest...>
+    : std::conditional_t<bool(First::value), First, disjunction<Rest...>> {};
+
+template <typename... Ts>
+constexpr bool disjunction_v = disjunction<Ts...>::value;
+
+// Utility type that negates its truth-like parameter type.
+template <typename T>
+struct negation : std::integral_constant<bool, !bool(T::value)> {};
+
+template <typename T>
+constexpr bool negation_v = negation<T>::value;
+
+#endif
+
+// Encapsulates capture of a parameter pack. Typical use is to use instances of this empty struct
+// for type dispatch in function template deduction/overload resolution.
+//
+// Example:
+//  template <typename Callable, typename... Args>
+//  auto inspect_args(Callable c, parameter_pack<Args...>) {
+//      // do something with Args...
+//  }
+//
+//  template <typename Callable>
+//  auto inspect_args(Callable c) {
+//      return inspect_args(std::move(c), typename callable_traits<Callable>::args{});
+//  }
+template <typename... T>
+struct parameter_pack {
+  static constexpr size_t size = sizeof...(T);
+
+  template <size_t i>
+  using at = typename std::tuple_element_t<i, std::tuple<T...>>;
+};
+
+// |callable_traits| captures elements of interest from function-like types (functions, function
+// pointers, and functors, including lambdas). Due to common usage patterns, const and non-const
+// functors are treated identically.
+//
+// Member types:
+//  |args|        - a |parameter_pack| that captures the parameter types of the function. See
+//                  |parameter_pack| for usage and details.
+//  |return_type| - the return type of the function.
+//  |type|        - the underlying functor or function pointer type. This member is absent if
+//                  |callable_traits| are requested for a raw function signature (as opposed to a
+//                  function pointer or functor; e.g. |callable_traits<void()>|).
+//  |signature|   - the type of the equivalent function.
+
+template <typename T>
+struct callable_traits : public callable_traits<decltype(&T::operator())> {};
+
+// Treat mutable call operators the same as const call operators.
+//
+// It would be equivalent to erase the const instead, but the common case is lambdas, which are
+// const, so prefer to nest less deeply for the common const case.
+template <typename FunctorType, typename ReturnType, typename... ArgTypes>
+struct callable_traits<ReturnType (FunctorType::*)(ArgTypes...)>
+    : public callable_traits<ReturnType (FunctorType::*)(ArgTypes...) const> {};
+
+// Common functor specialization.
+template <typename FunctorType, typename ReturnType, typename... ArgTypes>
+struct callable_traits<ReturnType (FunctorType::*)(ArgTypes...) const>
+    : public callable_traits<ReturnType (*)(ArgTypes...)> {
+  using type = FunctorType;
+};
+
+// Function pointer specialization.
+template <typename ReturnType, typename... ArgTypes>
+struct callable_traits<ReturnType (*)(ArgTypes...)>
+    : public callable_traits<ReturnType(ArgTypes...)> {
+  using type = ReturnType (*)(ArgTypes...);
+};
+
+// Base specialization.
+template <typename ReturnType, typename... ArgTypes>
+struct callable_traits<ReturnType(ArgTypes...)> {
+  using signature = ReturnType(ArgTypes...);
+  using return_type = ReturnType;
+  using args = parameter_pack<ArgTypes...>;
+
+  callable_traits() = delete;
+};
+
+// Determines whether a type has an operator() that can be invoked.
+template <typename T, typename = void_t<>>
+struct is_callable : public std::false_type {};
+template <typename ReturnType, typename... ArgTypes>
+struct is_callable<ReturnType (*)(ArgTypes...)> : public std::true_type {};
+template <typename FunctorType, typename ReturnType, typename... ArgTypes>
+struct is_callable<ReturnType (FunctorType::*)(ArgTypes...)> : public std::true_type {};
+template <typename T>
+struct is_callable<T, void_t<decltype(&T::operator())>> : public std::true_type {};
+
+}  // namespace fit
+
+#endif  // LIB_FIT_TRAITS_H_
---
+++ zircon/system/ulib/stdcompat/test/traits_test.cc
@@ -0,0 +1,137 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/fit/function.h>
+#include <lib/fit/traits.h>
+
+#include <functional>
+
+#include <zxtest/zxtest.h>
+
+namespace {
+namespace test_void_t {
+static_assert(std::is_same<fit::void_t<>, void>::value, "");
+static_assert(std::is_same<fit::void_t<int>, void>::value, "");
+static_assert(std::is_same<fit::void_t<int, int>, void>::value, "");
+}  // namespace test_void_t
+
+namespace test_logical_traits {
+static_assert(fit::conjunction_v<> == true, "");
+static_assert(fit::conjunction_v<std::false_type> == false, "");
+static_assert(fit::conjunction_v<std::true_type> == true, "");
+static_assert(fit::conjunction_v<std::false_type, std::false_type> == false, "");
+static_assert(fit::conjunction_v<std::false_type, std::true_type> == false, "");
+static_assert(fit::conjunction_v<std::true_type, std::false_type> == false, "");
+static_assert(fit::conjunction_v<std::true_type, std::true_type> == true, "");
+
+static_assert(fit::disjunction_v<> == false, "");
+static_assert(fit::disjunction_v<std::false_type> == false, "");
+static_assert(fit::disjunction_v<std::true_type> == true, "");
+static_assert(fit::disjunction_v<std::false_type, std::false_type> == false, "");
+static_assert(fit::disjunction_v<std::false_type, std::true_type> == true, "");
+static_assert(fit::disjunction_v<std::true_type, std::false_type> == true, "");
+static_assert(fit::disjunction_v<std::true_type, std::true_type> == true, "");
+
+static_assert(fit::negation_v<std::false_type> == true, "");
+static_assert(fit::negation_v<std::true_type> == false, "");
+}  // namespace test_logical_traits
+
+namespace test_callables {
+template <typename Callable, typename... Args>
+void invoke_with_defaults(Callable c, fit::parameter_pack<Args...>) {
+  c(Args()...);
+}
+
+template <typename Callable>
+void invoke_with_defaults(Callable c) {
+  invoke_with_defaults(std::move(c), typename fit::callable_traits<Callable>::args{});
+}
+
+TEST(TraitsTest, arg_capture) {
+  int i = 0;
+  invoke_with_defaults([&] { i = 42; });
+  EXPECT_EQ(42, i);
+  invoke_with_defaults([&](int, float) { i = 54; });
+  EXPECT_EQ(54, i);
+}
+
+// Performs static assertions against a function-like type of signature int(float, bool).
+template <typename Callable>
+struct CallableTraitsTest {
+  using Traits = fit::callable_traits<Callable>;
+
+  static_assert(std::is_same_v<int(float, bool), typename Traits::signature>, "");
+  static_assert(std::is_same_v<int, typename Traits::return_type>, "");
+  static_assert(2 == Traits::args::size, "");
+  static_assert(std::is_same_v<float, typename Traits::args::template at<0>>, "");
+  static_assert(std::is_same_v<bool, typename Traits::args::template at<1>>, "");
+};
+
+// compile-time test
+namespace signature_traits {
+using Traits = CallableTraitsTest<int(float, bool)>::Traits;
+}  // namespace signature_traits
+
+// compile-time test
+namespace function_pointer_traits {
+using Traits = CallableTraitsTest<int (*)(float, bool)>::Traits;
+static_assert(std::is_same_v<int (*)(float, bool), Traits::type>, "");
+}  // namespace function_pointer_traits
+
+// compile-time test
+namespace member_function_pointer_traits {
+struct Object {
+  int method(float, bool);
+};
+using Traits = CallableTraitsTest<int (Object::*)(float, bool)>::Traits;
+static_assert(std::is_same_v<Object, Traits::type>, "");
+}  // namespace member_function_pointer_traits
+
+// compile-time test
+namespace lambda_traits {
+[[maybe_unused]] auto lambda = [](float, bool) { return 0; };
+using Traits = CallableTraitsTest<decltype(lambda)>::Traits;
+}  // namespace lambda_traits
+
+template <typename Functor>
+struct FunctorTraitsTest {
+  using Traits = typename CallableTraitsTest<Functor>::Traits;
+  static_assert(std::is_same_v<Functor, typename Traits::type>, "");
+};
+
+// compile-time test
+namespace mutable_functor_traits {
+struct MutableFunctor {
+  int operator()(float, bool) { return 0; }
+};
+using Traits = FunctorTraitsTest<MutableFunctor>::Traits;
+}  // namespace mutable_functor_traits
+
+// compile-time test
+namespace fit_callable_traits {
+using Traits = FunctorTraitsTest<fit::function<int(float, bool)>>;
+}  // namespace fit_callable_traits
+
+// compile-time test
+namespace std_callable_traits {
+using Traits = FunctorTraitsTest<std::function<int(float, bool)>>;
+}  // namespace std_callable_traits
+
+static_assert(!fit::is_callable<void>::value, "");
+static_assert(!fit::is_callable<int>::value, "");
+static_assert(!fit::is_callable<int(float, bool)>::value, "");
+static_assert(fit::is_callable<int (*)(float, bool)>::value, "");
+static_assert(fit::is_callable<int (member_function_pointer_traits::Object::*)(float, bool)>::value,
+              "");
+static_assert(fit::is_callable<decltype(lambda_traits::lambda)>::value, "");
+static_assert(fit::is_callable<mutable_functor_traits::MutableFunctor>::value, "");
+
+static_assert(fit::is_callable<fit::function<int(float, bool)>>::value, "");
+static_assert(fit::is_callable<fit::callback<int(float, bool)>>::value, "");
+
+static_assert(fit::is_callable<std::function<int(float, bool)>>::value, "");
+
+}  // namespace test_callables
+
+}  // namespace
