f2506b570a19696210b9a3ba90a54416300008c0
John Bauman <jbauman@google.com>
2020-12-15 23:53:38 +0000

    [intel-i915] Migrate to using bindc
    Change-Id: Id17320d8534b6198cd61dd0f8810e529ec9fe115
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/462578
    Reviewed-by: Randall Bosetti <rlb@google.com>
    Testability-Review: Randall Bosetti <rlb@google.com>
    Commit-Queue: John Bauman <jbauman@google.com>

--- src/graphics/display/drivers/intel-i915/intel-i915.cc
+++ src/graphics/display/drivers/intel-i915/intel-i915.cc
@@ -23,7 +23,6 @@
 #include <memory>
 #include <utility>
 
-#include <ddk/binding.h>
 #include <ddk/device.h>
 #include <ddk/driver.h>
 #include <ddk/protocol/intelgpucore.h>
@@ -42,6 +41,7 @@
 #include "registers-transcoder.h"
 #include "registers.h"
 #include "tiling.h"
+#include "src/graphics/display/drivers/intel-i915/intel-i915-bind.h"
 
 #define INTEL_I915_BROADWELL_DID (0x1616)
 
@@ -2281,35 +2281,4 @@ static constexpr zx_driver_ops_t intel_i915_driver_ops = []() {
 }();
 
 // clang-format off
-ZIRCON_DRIVER_BEGIN(intel_i915, intel_i915_driver_ops, "zircon", "0.1", 28)
-    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PCI),
-    BI_ABORT_IF(NE, BIND_PCI_VID, INTEL_I915_VID),
-    // Skylake DIDs
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x191b),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1912),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x191d),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1902),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1916),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x191e),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1906),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x190b),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1926),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1927),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x1923),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x193b),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x192d),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x193d),
-    // Kaby lake DIDs
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x5916),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x591c),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x591e),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x591b),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x5912),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x5926),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x5906),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x5927),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x5902),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x591a),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x591d),
-    BI_MATCH_IF(EQ, BIND_PCI_DID, 0x3ea5),
-ZIRCON_DRIVER_END(intel_i915)
+ZIRCON_DRIVER(intel_i915, intel_i915_driver_ops, "zircon", "0.1");
