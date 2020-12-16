7da471afba7003f21a3f555ddb0baed128552ff9
Tyler Mandry <tmandry@google.com>
2020-12-16 06:12:23 +0000

    [third_party][rust] Add missing license headers, files
    We use tiny_mirrors in two ways, as both null/stub implementations and
    to import the actual crate code and patch it in some way.
    
    In the first case, make sure we have all license headers. In the second,
    make sure there are LICENSE files.
    
    Bug: b/173235604
    Change-Id: I845712fe828270fb08d9f4e076150b302826b39f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/461698
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>
    Testability-Review: Adam Perry <adamperry@google.com>
    Commit-Queue: Tyler Mandry <tmandry@google.com>

--- third_party/rust_crates/tiny_mirrors/libm/src/lib.rs
+++ third_party/rust_crates/tiny_mirrors/libm/src/lib.rs
@@ -1,5 +1,6 @@
 // Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 //! A shim impelementation of the libm crate, binding directly to the in-tree libc's versions of
 //! these functions.
