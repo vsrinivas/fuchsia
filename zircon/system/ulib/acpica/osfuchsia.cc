8638d03e38d40f614303b68a2104fad65e400089
Pedro Falcato <pedro.falcato@gmail.com>
2020-12-16 22:07:55 +0000

    [acpi] Don't allow access to the whole I/O port space
    This makes sure that the I/O port permissions are restricted to what really is needed by
    ACPICA, fixing an old TODO and a possible security problem.
    
    Test: Built and ran QEMU x64 to make sure everything still ran
    correctly.
    
    Change-Id: I7434b8d9bf156e4097c07b5ba9ca4a98691e5f99
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/460815
    Commit-Queue: Adrian Danis <adanis@google.com>
    Reviewed-by: Adrian Danis <adanis@google.com>
    Reviewed-by: Christopher Anderson <cja@google.com>
    Reviewed-by: Venkatesh Srinivas <venkateshs@google.com>

--- zircon/system/ulib/acpica/osfuchsia.cc
+++ zircon/system/ulib/acpica/osfuchsia.cc
@@ -22,6 +22,8 @@
 #include <new>
 #include <utility>
 
+#include <bitmap/raw-bitmap.h>
+#include <bitmap/storage.h>
 #include <fbl/alloc_checker.h>
 #include <fbl/auto_lock.h>
 #include <fbl/intrusive_double_list.h>
@@ -196,6 +198,8 @@ void acpica_disable_noncontested_mode() {
   acpi_spinlocks_held--;
 }
 
+static void initialize_port_bitmap();
+
 /**
  * @brief Initialize the OSL subsystem.
  *
@@ -221,8 +225,8 @@ ACPI_STATUS AcpiOsInitialize() {
     return status;
   }
 
-  /* TODO(teisenbe): be less permissive */
-  zx_ioports_request(root_resource_handle, 0, 65536);
+  initialize_port_bitmap();
+
   return AE_OK;
 }
 
@@ -953,6 +957,86 @@ ACPI_STATUS AcpiOsWriteMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 Value, UINT3
   return AE_OK;
 }
 
+// Essentially, we're using a bitmap here to represent each individual I/O port, so that we can
+// keep track of which I/O ports are allowed and which are not by the kernel.
+
+static constexpr size_t max_io_port = UINT16_MAX;
+static constexpr size_t io_port_bitmap_size = max_io_port + 1;
+static fbl::Mutex bitmap_lock;
+static bitmap::RawBitmapGeneric<bitmap::FixedStorage<io_port_bitmap_size>> port_bitmap;
+
+static void initialize_port_bitmap() {
+  // This cannot fail given that we're using fixed storage
+  port_bitmap.Reset(io_port_bitmap_size);
+}
+
+static bool check_port_permissions(uint16_t address, uint8_t width_bytes) {
+  LTRACEF("Testing %#x until %#x, in bitmap of size %#zx\n", address, address + width_bytes,
+          port_bitmap.size());
+
+  return port_bitmap.Scan(address, address + width_bytes, true);
+}
+
+/**
+ * @brief Make the I/O ports accessible and set them in the bitmap, so that we don't call
+ * the kernel again.
+ *
+ * @param address The I/O address.
+ * @param width_bytes The width of the access, in bytes.
+ *
+ * @return Status code that indicates success or reason for error.
+ */
+static zx_status_t add_port_permissions(uint16_t address, uint8_t width_bytes) {
+  assert(port_bitmap.Set(address, address + width_bytes) == ZX_OK);
+
+  LTRACEF("Adding permissions to [%#x, %#x]\n", address, address + width_bytes);
+
+  return zx_ioports_request(root_resource_handle, address, address + width_bytes);
+}
+
+/**
+ * @brief Handle all matters of I/O port permissions with the kernel.
+ *
+ * @param address The I/O address.
+ * @param width_bits The wideness of the access, in bits.
+ *
+ * @return Status code that indicates success or reason for error.
+ */
+static zx_status_t handle_port_permissions(uint16_t address, UINT32 width_bits) {
+  // It's a good idea to convert bits to bytes here, considering each
+  // I/O port "byte" has its own bit in the bitmap
+  uint8_t width_bytes = static_cast<uint8_t>(width_bits / 8);
+
+  fbl::AutoLock g{&bitmap_lock};
+
+  if (!check_port_permissions(address, width_bytes)) {
+    // If the port is disallowed at the moment, call the kernel so it isn't
+    return add_port_permissions(address, width_bytes);
+  } else {
+    LTRACEF("port %#x(width %#x) was already set.\n", address, width_bytes);
+  }
+
+  return ZX_OK;
+}
+
+static ACPI_STATUS zx_status_to_acpi_status(zx_status_t st) {
+  // Note: This function was written with regard to zx_ioports_request(),
+  // but it may be a good idea to fill this out with more ZX_ statuses
+  // if needed in the future.
+  switch (st) {
+    case ZX_ERR_NO_MEMORY:
+      return AE_NO_MEMORY;
+    case ZX_ERR_ACCESS_DENIED:
+      return AE_ACCESS;
+    case ZX_ERR_INVALID_ARGS:
+      return AE_BAD_PARAMETER;
+    case ZX_OK:
+      return AE_OK;
+    default:
+      return AE_ERROR;
+  }
+}
+
 /**
  * @brief Read a value from an input port.
  *
@@ -963,19 +1047,25 @@ ACPI_STATUS AcpiOsWriteMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 Value, UINT3
  * @return Exception code that indicates success or reason for failure.
  */
 ACPI_STATUS AcpiOsReadPort(ACPI_IO_ADDRESS Address, UINT32* Value, UINT32 Width) {
-  if (Address > 0xffff) {
+  if (Address > max_io_port) {
     return AE_BAD_PARAMETER;
+  }
+
+  uint16_t io_port = (uint16_t)Address;
+
+  if (zx_status_t st = handle_port_permissions(io_port, Width); st != ZX_OK) {
+    return zx_status_to_acpi_status(st);
   }
 
   switch (Width) {
     case 8:
-      *Value = inp((uint16_t)Address);
+      *Value = inp(io_port);
       break;
     case 16:
-      *Value = inpw((uint16_t)Address);
+      *Value = inpw(io_port);
       break;
     case 32:
-      *Value = inpd((uint16_t)Address);
+      *Value = inpd(io_port);
       break;
     default:
       return AE_BAD_PARAMETER;
@@ -993,19 +1083,25 @@ ACPI_STATUS AcpiOsReadPort(ACPI_IO_ADDRESS Address, UINT32* Value, UINT32 Width)
  * @return Exception code that indicates success or reason for failure.
  */
 ACPI_STATUS AcpiOsWritePort(ACPI_IO_ADDRESS Address, UINT32 Value, UINT32 Width) {
-  if (Address > 0xffff) {
+  if (Address > max_io_port) {
     return AE_BAD_PARAMETER;
+  }
+
+  uint16_t io_port = (uint16_t)Address;
+
+  if (zx_status_t st = handle_port_permissions(io_port, Width); st != ZX_OK) {
+    return zx_status_to_acpi_status(st);
   }
 
   switch (Width) {
     case 8:
-      outp((uint16_t)Address, (uint8_t)Value);
+      outp(io_port, (uint8_t)Value);
       break;
     case 16:
-      outpw((uint16_t)Address, (uint16_t)Value);
+      outpw(io_port, (uint16_t)Value);
       break;
     case 32:
-      outpd((uint16_t)Address, (uint32_t)Value);
+      outpd(io_port, (uint32_t)Value);
       break;
     default:
       return AE_BAD_PARAMETER;
