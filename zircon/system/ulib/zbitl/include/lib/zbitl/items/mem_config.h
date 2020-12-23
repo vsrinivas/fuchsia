c8b7f9462c051e73fba7ed07ddfea70208876cb6
David Greenaway <dgreenaway@google.com>
2020-12-23 02:34:25 +0000

    [zbitl] Add class `MemRangeMerger` to merge contiguous memory ranges.
    Some platforms, such as the Acer Switch, give hundreds of small ranges
    in the EFI memory map. This overflows fixed-sized buffers in the kernel
    that track reserved ranges, causing a panic on boot (c.f.
    http://fxbug.dev/66742).
    
    Prior to moving the kernel boot code over to zbitl, the old code used to
    merge contiguous ranges.
    
    This CL adds a new class that takes an iterator over zbi_mem_range_t
    objects and gives a new iterator over merged ranges.
    
    Bug: 66742
    Change-Id: Icf707cbdcc16e88a87d7b6b89ddedd799e3fa9eb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/464341
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: David Greenaway <dgreenaway@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

--- zircon/system/ulib/zbitl/include/lib/zbitl/items/mem_config.h
+++ zircon/system/ulib/zbitl/include/lib/zbitl/items/mem_config.h
@@ -109,6 +109,103 @@ class MemRangeTable {
   View<ByteView> view_;
 };
 
+// Takes an iterator yielding a sorted list of zbi_mem_range_t items, and merges
+// together contiguous ranges of the same type.
+template <typename Iterator>
+class MemRangeMerger {
+ public:
+  MemRangeMerger() = default;
+  MemRangeMerger(Iterator begin, Iterator end) : begin_(begin), end_(end) {}
+
+  class iterator;
+  iterator begin() const { return iterator(begin_, end_); }
+  iterator end() const { return iterator(end_, end_); }
+
+  class iterator {
+   public:
+    iterator(Iterator begin, Iterator end) : it_(begin), next_(begin), end_(end) { Next(); }
+
+    // Iterator traits.
+    using iterator_category = std::input_iterator_tag;
+    using reference = const zbi_mem_range_t&;
+    using value_type = zbi_mem_range_t;
+    using pointer = const zbi_mem_range_t*;
+    using difference_type = size_t;
+
+    // Equality / inequality.
+    bool operator==(const iterator& other) const { return it_ == other.it_; }
+    bool operator!=(const iterator& other) const { return !(*this == other); }
+
+    // Return the current element.
+    const zbi_mem_range_t& operator*() const {
+      ZX_DEBUG_ASSERT_MSG(it_ != end_, "Attempted to dereference 'end' iterator.");
+      return current_;
+    }
+    const zbi_mem_range_t* operator->() {
+      ZX_DEBUG_ASSERT_MSG(it_ != end_, "Attempted to dereference 'end' iterator.");
+      return &current_;
+    }
+
+    // Increment operators: move iterator to next element.
+    iterator& operator++() {  // prefix
+      Next();
+      return *this;
+    }
+    iterator operator++(int) {  // postfix
+      iterator result = *this;
+      ++this;
+      return result;
+    }
+
+   private:
+    // Ensure iterator type is valid.
+    static_assert(std::is_same_v<zbi_mem_range_t, typename Iterator::value_type>,
+                  "Expected an iterator of type zbi_mem_range_t.");
+
+    // Fill `current_` with the next merged range.
+    void Next() {
+      // If we are at the end, do nothing.
+      it_ = next_;
+      if (it_ == end_) {
+        return;
+      }
+
+      // Keep merging entries together until we hit the end of our input
+      // or hit a discontinuity.
+      current_ = *next_;
+      ++next_;
+      while (next_ != end_) {
+        zbi_mem_range_t next = *next_;
+        // Ensure the end of this region is the start of the next.
+        if (current_.paddr + current_.length != next.paddr) {
+          break;
+        }
+        // Ensure the type of this region matches the next.
+        if (current_.type != next.type) {
+          break;
+        }
+        // Increase the size of this region.
+        current_.length += next.length;
+        ++next_;
+      }
+    }
+
+    // The merged memory range.
+    zbi_mem_range_t current_;
+
+    // The pair [it_, next_) represent the range of items merged into current_.
+    Iterator it_;
+    Iterator next_;
+
+    // The end_ iterator of the underlying container.
+    Iterator end_;
+  };
+
+ private:
+  Iterator begin_;
+  Iterator end_;
+};
+
 }  // namespace zbitl
 
 #endif  // LIB_ZBITL_ITEMS_MEM_CONFIG_H_
--- zircon/system/ulib/zbitl/test/mem_config_test.cc
+++ zircon/system/ulib/zbitl/test/mem_config_test.cc
@@ -528,4 +528,124 @@ TEST(MemRangeIterator, EfiRealData) {
       ranges.back()));
 }
 
+TEST(MemRangeMerger, Empty) {
+  std::vector<zbi_mem_range_t> input{};
+  zbitl::MemRangeMerger merger(input.begin(), input.end());
+  EXPECT_EQ(merger.begin(), merger.end());
+}
+
+TEST(MemRangeMerger, SingleItem) {
+  // Create an input with just a single range.
+  std::vector<zbi_mem_range_t> input = {
+      zbi_mem_range_t{
+          .paddr = 1,
+          .length = 2,
+          .type = 3,
+      },
+  };
+  zbitl::MemRangeMerger merger(input.begin(), input.end());
+  EXPECT_NE(merger.begin(), merger.end());
+
+  // Check first element.
+  auto it = merger.begin();
+  EXPECT_EQ(it->paddr, 1u);
+  EXPECT_EQ(it->length, 2u);
+  EXPECT_EQ(it->type, 3u);
+
+  // Should be no more elements.
+  ++it;
+  EXPECT_EQ(it, merger.end());
+}
+
+TEST(MemRangeMerger, MergeItems) {
+  // Create an input with multiple ranges to be merged.
+  std::vector<zbi_mem_range_t> input = {
+      zbi_mem_range_t{
+          .paddr = 0,
+          .length = 100,
+          .type = 1,
+      },
+      zbi_mem_range_t{
+          .paddr = 100,
+          .length = 200,
+          .type = 1,
+      },
+      zbi_mem_range_t{
+          .paddr = 300,
+          .length = 100,
+          .type = 1,
+      },
+  };
+  zbitl::MemRangeMerger merger(input.begin(), input.end());
+
+  // Merge the items.
+  std::vector<zbi_mem_range_t> result(merger.begin(), merger.end());
+
+  // Ensure we got the correctly merged results.
+  ASSERT_EQ(result.size(), 1u);
+  EXPECT_EQ(result[0].paddr, 0u);
+  EXPECT_EQ(result[0].length, 400u);
+  EXPECT_EQ(result[0].type, 1u);
+}
+
+TEST(MemRangeMerger, ShouldNotCombineNonContiguousItems) {
+  // Ensure we don't merge non-contiguous items.
+  std::vector<zbi_mem_range_t> input = {
+      zbi_mem_range_t{
+          .paddr = 0,
+          .length = 1,
+          .type = 1,
+      },
+      zbi_mem_range_t{
+          .paddr = 2,  // skips byte 1; should not be merged.
+          .length = 1,
+          .type = 1,
+      },
+      zbi_mem_range_t{
+          .paddr = 3,  // not the same type; should not be merged.
+          .length = 1,
+          .type = 2,
+      },
+  };
+  zbitl::MemRangeMerger merger(input.begin(), input.end());
+
+  // Merge the items.
+  std::vector<zbi_mem_range_t> result(merger.begin(), merger.end());
+
+  // Ensure the input matches the output.
+  ASSERT_EQ(result.size(), input.size());
+  for (size_t i = 0; i < input.size(); i++) {
+    EXPECT_EQ(input[i].paddr, result[i].paddr);
+    EXPECT_EQ(input[i].length, result[i].length);
+    EXPECT_EQ(input[i].type, result[i].type);
+  }
+}
+
+TEST(MemRangeMerger, MemRangeTableIterator) {
+  // Create a MemRangeTable, and use it as our input.
+  ZbiMemoryImage zbi = CreateImage();
+  AppendPayload(zbi, ZBI_TYPE_EFI_MEMORY_MAP,
+                JoinBytes(uint64_t{sizeof(efi_memory_descriptor)},
+                          efi_memory_descriptor{
+                              .Type = 1,
+                              .PhysicalStart = 0x1000,
+                              .NumberOfPages = 1,
+                          },
+                          efi_memory_descriptor{
+                              .Type = 1,
+                              .PhysicalStart = 0x2000,
+                              .NumberOfPages = 2,
+                          }));
+
+  // Merge elements together.
+  zbitl::MemRangeTable container{AsView(zbi)};
+  zbitl::MemRangeMerger merger(container.begin(), container.end());
+  std::vector<zbi_mem_range_t> ranges(merger.begin(), merger.end());
+  ASSERT_TRUE(container.take_error().is_ok());
+  ASSERT_EQ(ranges.size(), 1u);
+  EXPECT_EQ(ranges[0].paddr, 0x1000u);
+  EXPECT_EQ(ranges[0].length, 0x3000u);
+  EXPECT_EQ(ranges[0].type, 1u);
+}
+
 }  // namespace
