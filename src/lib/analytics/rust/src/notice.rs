5bebfe2761ab2e1a723f2ff589a574ba051d600a
Amit Uttamchandani <amituttam@google.com>
2020-12-28 20:33:00 +0000

    [ffx] Fix long lines in analytics notice
    Change-Id: I7cf9fe083b94fa45dd0ee76c71545ec11d93ce18
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/465355
    Reviewed-by: Jordon Wing <jwing@google.com>
    Reviewed-by: James Tucker <raggi@google.com>
    Fuchsia-Auto-Submit: Amit Uttamchandani <amituttam@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/lib/analytics/rust/src/notice.rs
+++ src/lib/analytics/rust/src/notice.rs
@@ -10,9 +10,10 @@ use std::io::Write;
 pub const FULL_NOTICE: &str =
     "Welcome to Fuchsia! - https://fuchsia.dev
 
-Fuchsia core developer tools use Google Analytics to report feature usage statistics and basic crash reports.
-Google may examine the collected data in aggregate to help improve these tools,
-other Fuchsia tools, and the Fuchsia SDK.
+Fuchsia core developer tools use Google Analytics to report feature usage
+statistics and basic crash reports. Google may examine the collected data
+in aggregate to help improve these tools, other Fuchsia tools, and the
+Fuchsia SDK.
 
 Analytics are not sent on this very first run. To disable reporting, type
 
@@ -22,8 +23,8 @@ To display the current setting and what is collected, type
 
     ffx config analytics
 
-If you opt out of analytics, an opt-out event will be sent, and then no further
-information will be sent by the core developer tools to Google.
+If you opt out of analytics, an opt-out event will be sent, and then no
+further information will be sent by the core developer tools to Google.
 
 By using Fuchsia core developer tools, you agree to the Google Terms of Service.
 Note: The Google Privacy Policy describes how data is handled in your use of
