7b983669bfb397a358ba95abee8c5d9600aa2804
Shai Barack <shayba@google.com>
2020-12-29 19:34:56 +0000

    [build] Also validate cml files against packaged files
    While we're at it, for the new templates validate only against
    dependencies of the component (rather than collecting all packaged
    manifests and validating them against all packaged files). Ensures that
    the build graph is more correct.
    
    Also fix a couple of errors here and there that were caught by the more
    stringent checks.
    
    Change-Id: I77248ceca44d3fc79a52ddc0797bed34de25d9a3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/465714
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Xyan Bhatnagar <xbhatnag@google.com>
    Commit-Queue: Shai Barack <shayba@google.com>
    Fuchsia-Auto-Submit: Shai Barack <shayba@google.com>

--- tools/cmc/build/validate_component_manifest_references.py
+++ tools/cmc/build/validate_component_manifest_references.py
@@ -5,113 +5,130 @@
 # Lint as: python3
 
 import argparse
-import json
+import re
 import sys
 
+
+# TODO(shayba): Make this more robust.
+# * Actually parse cmx JSON / cml JSON5,
+#   rather than match against formatted files with regular expressions.
+# * Introduce more analysis that's aware of runners,
+#   rather than bail out when any runners are involved.
+#   Or maybe specialize the analysis,
+#   for instance for Dart look for *.dlib files under program.data.
+# * Validate more paths (data files/directories?)
+#   rather than just binary paths.
 def main():
-  parser = argparse.ArgumentParser(
-      'Validate component manifests against package manifests',
-      fromfile_prefix_chars='@')
-  parser.add_argument(
-      '--component_manifests',
-      required=True,
-      type=argparse.FileType('r'),
-      nargs='*',
-      help='Path to a component manifest to validate (cmx/cml file)')
-  parser.add_argument(
-      '--package_manifest',
-      required=True,
-      type=argparse.FileType('r'),
-      help='Path to a package manifest to validate against')
-  parser.add_argument(
-      '--stamp',
-      required=True,
-      type=argparse.FileType('w'),
-      help='Stamp file')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        'Validate component manifests against package manifests')
+    parser.add_argument(
+        '--component_manifest',
+        required=True,
+        type=argparse.FileType('r'),
+        help='Path to a component manifest to validate (cmx/cml file)')
+    parser.add_argument(
+        '--package_manifest',
+        required=True,
+        type=argparse.FileType('r'),
+        help='Path to a package manifest to validate against')
+    parser.add_argument(
+        '--gn-label',
+        required=True,
+        help='GN label to include in error messages')
+    parser.add_argument(
+        '--stamp',
+        required=True,
+        type=argparse.FileType('w'),
+        help='Stamp file')
+    args = parser.parse_args()
 
-  dsts = [dst for dst, src in
-          (line.split('=') for line in args.package_manifest.readlines())]
+    dsts = [
+        dst for dst, _, src in (
+            line.partition('=') for line in args.package_manifest.readlines())
+    ]
 
-  for component_manifest in args.component_manifests:
-    if not component_manifest.name.endswith('.cmx'):
-      # Known issue: `json` can't parse JSON5 (.cml files).
-      # Need to address this in the future, or after this script is
-      # productionized into ffx.
-      continue
+    # Matches program.binary in any formatted cmx or cml file
+    binary_re = re.compile('        "?binary"?: "([^"]*)",?')
+    binary_value = None
+    for line in args.component_manifest.readlines():
+        if "runner" in line:
+            # Runners can modify the component's incoming namespace,
+            # so don't bother trying to validate.
+            return stamp(args)
+        m = binary_re.match(line)
+        if m:
+            binary_value = m.group(1)
 
-    cm = json.load(component_manifest)
+    if not binary_value:
+        # Nothing to validate
+        return stamp(args)
 
-    if "runner" in cm:
-      # Runners can change the namespace (for instance guest_runner), and can
-      # generally reinterpret this part of the manifest, so we won't judge.
-      continue
-
-    try:
-      binary = cm["program"]["binary"]
-    except:
-      # Nothing to validate
-      continue
-
-    if binary in dsts:
-      continue
+    if binary_value in dsts:
+        return stamp(args)
 
     # Legacy package.gni supports the "disabled test" feature that intentionally
     # breaks component manifests. /shrug
-    if binary.startswith("test/") and "test/disabled/" + binary[5:] in dsts:
-      continue
+    if binary_value.startswith(
+            "test/") and "test/disabled/" + binary_value[5:] in dsts:
+        return stamp(args)
 
-    print(f'Error found in {component_manifest.name}')
-    print(f'program.binary="{binary}" but {binary} is not in the package!')
+    print(f'Error found in: {args.gn_label}')
+    print(f'Failed to validate manifest: {args.component_manifest.name}')
+    print(f'program.binary="{binary_value}" but {binary_value} is not in deps!')
     print()
-    nearest = nearest_match(binary, dsts)
+    nearest = nearest_match(binary_value, dsts)
     if nearest:
-      print(f'Did you mean "{nearest}"?')
-      print()
+        print(f'Did you mean "{nearest}"?')
+        print()
     print('Try any of the following:')
     print('\n'.join(sorted(dsts)))
     return 1
 
-  args.stamp.write('Success!')
-  return 0
+
+def stamp(args):
+    args.stamp.write('Success!')
+    return 0
+
 
 def nearest_match(start, candidates):
-  """Finds the nearest match to `start` out of `candidates`."""
-  nearest = None
-  min_distance = sys.maxsize
-  for candidate in candidates:
-    distance = minimum_edit_distance(start, candidate)
-    if distance < min_distance:
-      min_distance = distance
-      nearest = candidate
-  return nearest
+    """Finds the nearest match to `start` out of `candidates`."""
+    nearest = None
+    min_distance = sys.maxsize
+    for candidate in candidates:
+        distance = minimum_edit_distance(start, candidate)
+        if distance < min_distance:
+            min_distance = distance
+            nearest = candidate
+    return nearest
+
 
 def minimum_edit_distance(s, t):
-  """Finds the Levenshtein distance between `s` and `t`."""
-  # Dynamic programming table
-  rows = len(s) + 1
-  cols = len(t) + 1
-  dist = [[0 for x in range(cols)] for x in range(rows)]
+    """Finds the Levenshtein distance between `s` and `t`."""
+    # Dynamic programming table
+    rows = len(s) + 1
+    cols = len(t) + 1
+    dist = [[0 for x in range(cols)] for x in range(rows)]
 
-  # Fastest way to transform to empty string is N deletions
-  for i in range(1, rows):
-      dist[i][0] = i
-  # Fastest way to transform from empty string is N insertions
-  for i in range(1, cols):
-    dist[0][i] = i
+    # Fastest way to transform to empty string is N deletions
+    for i in range(1, rows):
+        dist[i][0] = i
+    # Fastest way to transform from empty string is N insertions
+    for i in range(1, cols):
+        dist[0][i] = i
 
-  for col in range(1, cols):
-      for row in range(1, rows):
-          if s[row-1] == t[col-1]:
-              cost = 0
-          else:
-              cost = 1
-          dist[row][col] = min(dist[row-1][col] + 1,      # Deletion
-                               dist[row][col-1] + 1,      # Insertion
-                               dist[row-1][col-1] + cost) # Substitution
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row - 1] == t[col - 1]:
+                cost = 0
+            else:
+                cost = 1
+            dist[row][col] = min(
+                dist[row - 1][col] + 1,  # Deletion
+                dist[row][col - 1] + 1,  # Insertion
+                dist[row - 1][col - 1] + cost)  # Substitution
 
-  return dist[row][col]
+    return dist[row][col]
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
