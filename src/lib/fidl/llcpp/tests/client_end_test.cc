96516b2b5c72c13aa35531ef0d23e0b0e33ae067
Yifei Teng <yifeit@google.com>
2020-12-29 23:12:39 +0000

    [fidl][llcpp] Introduce fidl::UnownedClientEnd<Protocol>
    During the soft migration to typed channels, we discovered that using a
    const reference to a fidl::ClientEnd<Protocol> would not cover all the
    use cases. This CL adds an unowned version of ClientEnd, similar to a
    typed zx::unowned_channel, to be used by upcoming migrations.
    
    Test: added some
    Bug: 65212
    Change-Id: Ifb40260baa1638ce786d3e8232dba43b11e4782c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/466344
    Commit-Queue: Yifei Teng <yifeit@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>
    Reviewed-by: Vincent Belliard <vbelliard@google.com>

--- src/lib/fidl/llcpp/tests/client_end_test.cc
+++ src/lib/fidl/llcpp/tests/client_end_test.cc
@@ -10,6 +10,8 @@
 #include <zircon/status.h>
 #include <zircon/syscalls/object.h>
 
+#include <type_traits>
+
 #include <gtest/gtest.h>
 #include <llcpptest/protocol/test/llcpp/fidl.h>
 
@@ -49,4 +51,55 @@ TEST(ClientEnd, Control) {
   client_end = {};
   EXPECT_FALSE(client_end.is_valid());
   EXPECT_EQ(ZX_ERR_PEER_CLOSED, h1.write(0, "a", 1, nullptr, 0));
+}
+
+TEST(UnownedClientEnd, Constructors) {
+  auto endpoints = fidl::CreateEndpoints<llcpp_test::Frobinator>();
+  ASSERT_EQ(ZX_OK, endpoints.status_value()) << endpoints.status_string();
+  fidl::ClientEnd<llcpp_test::Frobinator> client_end = std::move(endpoints->client);
+
+  {
+    // Construct from a |fidl::ClientEnd|.
+    fidl::UnownedClientEnd<llcpp_test::Frobinator> unowned_client_end(client_end);
+    ASSERT_EQ(unowned_client_end.channel(), client_end.channel().get());
+
+    // Implicit construction during parameter passing.
+    auto id = [](fidl::UnownedClientEnd<llcpp_test::Frobinator> unowned) { return unowned; };
+    auto roundtrip = id(client_end);
+    ASSERT_EQ(roundtrip.channel(), client_end.channel().get());
+  }
+
+  {
+    // Construct from a |zx_handle_t|.
+    fidl::UnownedClientEnd<llcpp_test::Frobinator> unowned_client_end(client_end.channel().get());
+    ASSERT_EQ(unowned_client_end.channel(), client_end.channel().get());
+  }
+
+  {
+    // Copy construction.
+    fidl::UnownedClientEnd<llcpp_test::Frobinator> unowned_client_end(client_end);
+    fidl::UnownedClientEnd<llcpp_test::Frobinator> unowned_client_end2(unowned_client_end);
+    ASSERT_EQ(unowned_client_end.channel(), unowned_client_end2.channel());
+  }
+}
+
+TEST(UnownedClientEnd, IsValid) {
+  fidl::ClientEnd<llcpp_test::Frobinator> invalid{};
+  fidl::UnownedClientEnd<llcpp_test::Frobinator> unowned_client_end(invalid);
+  ASSERT_FALSE(unowned_client_end.is_valid());
+
+  auto endpoints = fidl::CreateEndpoints<llcpp_test::Frobinator>();
+  ASSERT_EQ(ZX_OK, endpoints.status_value()) << endpoints.status_string();
+  fidl::UnownedClientEnd<llcpp_test::Frobinator> unowned_client_end_valid(endpoints->client);
+  ASSERT_TRUE(unowned_client_end_valid.is_valid());
+}
+
+TEST(UnownedClientEnd, BorrowFromClientEnd) {
+  auto endpoints = fidl::CreateEndpoints<llcpp_test::Frobinator>();
+  ASSERT_EQ(ZX_OK, endpoints.status_value()) << endpoints.status_string();
+
+  auto unowned_client_end = endpoints->client.borrow();
+  static_assert(std::is_same_v<decltype(unowned_client_end),
+                               decltype(fidl::UnownedClientEnd<llcpp_test::Frobinator>(0))>);
+  ASSERT_EQ(unowned_client_end.channel(), endpoints->client.channel().get());
 }
--- zircon/system/ulib/fidl/include/lib/fidl/llcpp/client_end.h
+++ zircon/system/ulib/fidl/include/lib/fidl/llcpp/client_end.h
@@ -12,6 +12,9 @@
 
 namespace fidl {
 
+template <typename Protocol>
+class UnownedClientEnd;
+
 // The client endpoint of a FIDL channel.
 //
 // The remote (server) counterpart of the channel expects this end of the
@@ -50,8 +53,67 @@ class ClientEnd final {
   // Transfers ownership of the underlying channel to the caller.
   zx::channel TakeChannel() { return std::move(channel_); }
 
+  // Returns a type-safe copy of the underlying channel in this |ClientEnd|
+  // that does not claim ownership.
+  fidl::UnownedClientEnd<Protocol> borrow() const {
+    return UnownedClientEnd<Protocol>(channel_.borrow());
+  }
+
  private:
   zx::channel channel_;
+};
+
+// A typed client endpoint that does not claim ownership. It is typically
+// created from an owning |fidl::ClientEnd<Protocol>|.
+// These types are used by generated FIDL APIs that do not take ownership.
+//
+// The remote (server) counterpart of the channel expects this end of the
+// channel to speak the protocol represented by |Protocol|.
+//
+// Compared to a |const fidl::ClientEnd<Protocol>&|,
+// |fidl::UnownedClientEnd<Protocol>| has the additional flexibility of being
+// able to be stored in a member variable or field, while still remembering
+// the associated FIDL protocol.
+template <typename Protocol>
+class UnownedClientEnd final {
+ public:
+  // An unowned client end can only be constructed from an existing channel.
+  //
+  // This constructor defines an implicit conversion to facilitate invoking
+  // generated FIDL APIs with either an unowned client end, or a const
+  // reference to a |fidl::ClientEnd<Protocol>|.
+  // NOLINTNEXTLINE
+  UnownedClientEnd(const ClientEnd<Protocol>& owner) : UnownedClientEnd(owner.channel().get()) {}
+
+  // Creates an |UnownedClientEnd| from a raw zircon handle.
+  // Prefer only using this constructor when interfacing with C APIs.
+  explicit UnownedClientEnd(zx_handle_t h) : channel_(h) {}
+
+  // Creates an |UnownedClientEnd| from a |zx::unowned_channel|.
+  //
+  // Using this constructor is discouraged since it tends to erase the actual
+  // type of the underlying protocol.
+  // Consider declaring the type of the input variable as a
+  // |fidl::UnownedClientEnd<Protocol>| instead.
+  //
+  // TODO(fxbug.dev/65212): Make the conversion explicit as users migrate to
+  // typed channels.
+  // NOLINTNEXTLINE
+  UnownedClientEnd(const zx::unowned_channel& h) : channel_(h->get()) {}
+
+  // The unowned client end is copyable - it simply copies the handle value.
+  UnownedClientEnd(const UnownedClientEnd& other) = default;
+  UnownedClientEnd& operator=(const UnownedClientEnd& other) = default;
+
+  // Whether the underlying channel is valid.
+  bool is_valid() const { return channel_ != ZX_HANDLE_INVALID; }
+  explicit operator bool() const { return is_valid(); }
+
+  // The underlying channel.
+  zx_handle_t channel() const { return channel_; }
+
+ private:
+  zx_handle_t channel_;
 };
 
 }  // namespace fidl
