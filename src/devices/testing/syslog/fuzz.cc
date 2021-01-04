3fec60c6eb3b08d164c548157a7317cf72bb9341
Brian Bosak <bbosak@google.com>
2021-01-04 15:44:50 +0000

    [logs] Add fuzzer for structured logging backend
    This change adds a fuzzer to test the structured logging backend.
    Due to limitations of libFuzzer, testing inputs past the
    bounds of LogBuffer doesn't currently work. This limitation will
    be resolved in a future CL.
    
    Test: fx fuzz syslog-fuzzers/syslog-fuzzer
    Change-Id: Ic08bcc654bac55194e6043be1c7061ffda22fc90
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/461876
    Reviewed-by: Aaron Green <aarongreen@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>
    Testability-Review: Christopher Johnson <crjohns@google.com>

---
+++ src/devices/testing/syslog/fuzz.cc
@@ -0,0 +1,77 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/syslog/cpp/log_settings.h>
+#include <lib/syslog/cpp/logging_backend.h>
+#include <lib/syslog/cpp/logging_backend_fuchsia_private.h>
+#include <lib/syslog/cpp/macros.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <sstream>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+// use -f to get printf output from this test.
+
+// Parses an input stream from libFuzzer and executes arbitrary
+// logging commands to fuzz the structured logging backend.
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider provider(data, size);
+  enum class OP : uint8_t {
+    kStringField,
+    kSignedIntField,
+    kUnsignedIntField,
+    kDoubleField,
+    kMaxValue = kDoubleField,
+  };
+  syslog_backend::LogBuffer buffer;
+  auto severity = provider.ConsumeIntegral<syslog::LogSeverity>();
+  // Fatal crashes...
+  if (severity == syslog::LOG_FATAL) {
+    severity = syslog::LOG_ERROR;
+  }
+  auto file = provider.ConsumeRandomLengthString();
+  auto line = provider.ConsumeIntegral<unsigned int>();
+  auto msg = provider.ConsumeRandomLengthString();
+  auto condition = provider.ConsumeRandomLengthString();
+  syslog_backend::BeginRecord(&buffer, severity, file.data(), line, msg.data(), condition.data());
+  while (provider.remaining_bytes()) {
+    auto op = provider.ConsumeEnum<OP>();
+    auto key = provider.ConsumeRandomLengthString();
+    switch (op) {
+      case OP::kDoubleField:
+        syslog_backend::WriteKeyValue(&buffer, key.data(),
+                                      provider.ConsumeFloatingPoint<double>());
+        break;
+      case OP::kSignedIntField: {
+        int64_t value;
+        if (provider.remaining_bytes() < sizeof(value)) {
+          return 0;
+        }
+        value = provider.ConsumeIntegral<int64_t>();
+        syslog_backend::WriteKeyValue(&buffer, key.data(), value);
+      } break;
+      case OP::kUnsignedIntField: {
+        uint64_t value;
+        if (provider.remaining_bytes() < sizeof(value)) {
+          return 0;
+        }
+        value = provider.ConsumeIntegral<uint64_t>();
+        syslog_backend::WriteKeyValue(&buffer, key.data(), value);
+      } break;
+      case OP::kStringField: {
+        auto value = provider.ConsumeRandomLengthString();
+        syslog_backend::WriteKeyValue(&buffer, key.data(), value.data());
+      } break;
+    }
+  }
+  syslog_backend::EndRecord(&buffer);
+  syslog_backend::FlushRecord(&buffer);
+  return 0;
+}
