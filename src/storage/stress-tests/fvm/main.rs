a721ba89fc525be093f7565be6d66a837a8e036a
Xyan Bhatnagar <xbhatnag@google.com>
2021-01-12 14:53:03 +0000

    [storage][stress_tests] Combine libraries with binaries
    Storage tests were split into binary + library. This is unnecessary
    and makes the future migration to the actor model difficult.
    Unify the code into a single rustc_binary rule.
    
    Also remove the concept of Environment. The actor model has its own
    concept of Environment that is incompatible with this one.
    
    Test: fx test fvm-crash
          fx test minfs-nocrash
    Change-Id: I2e80ef8422817cdd6ea10aeee07d2763a7c4668a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/469800
    Commit-Queue: Xyan Bhatnagar <xbhatnag@google.com>
    Reviewed-by: James Sullivan <jfsulliv@google.com>

--- src/storage/stress-tests/fvm/main.rs
+++ src/storage/stress-tests/fvm/main.rs
@@ -2,14 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+pub mod fvm;
+pub mod operator;
+
 use {
     anyhow::Error,
     argh::FromArgs,
+    fasync::{Task, TimeoutExt},
+    fidl_fuchsia_hardware_block_partition::Guid,
     fuchsia_async as fasync,
-    fvm_stress_test_lib::run_test,
-    log::info,
-    log::LevelFilter,
-    stress_test_utils::{Environment, StdoutLogger},
+    fuchsia_zircon::Vmo,
+    futures::{future::join_all, SinkExt},
+    fvm::Volume,
+    log::{debug, info, LevelFilter},
+    operator::VolumeOperator,
+    rand::{rngs::SmallRng, Rng, SeedableRng},
+    std::{thread::sleep, time::Duration},
+    stress_test_utils::{fvm::FvmInstance, random_seed, StdoutLogger},
 };
 
 #[derive(Clone, Debug, FromArgs)]
@@ -72,22 +81,134 @@ struct Args {
     rebind_probability: f64,
 }
 
-impl Environment for Args {
-    fn init_logger(&self) {
-        let filter = self.log_filter.unwrap_or(LevelFilter::Debug);
-        StdoutLogger::init(filter);
+// All partitions in this test have their type set to this arbitrary GUID.
+const TYPE_GUID: Guid = Guid {
+    value: [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf],
+};
+
+pub async fn run_test(
+    mut rng: SmallRng,
+    ramdisk_block_count: u64,
+    fvm_slice_size: u64,
+    ramdisk_block_size: u64,
+    num_volumes: u64,
+    max_slices_in_extend: u64,
+    max_vslice_count: u64,
+    disconnect_secs: u64,
+    rebind_probability: f64,
+    time_limit_secs: Option<u64>,
+    num_operations: Option<u64>,
+) {
+    let vmo_size = ramdisk_block_count * ramdisk_block_size;
+
+    // Create the VMO that the ramdisk is backed by
+    let vmo = Vmo::create(vmo_size).unwrap();
+
+    // Initialize the ramdisk and setup FVM.
+    let mut instance = FvmInstance::new(true, &vmo, fvm_slice_size, ramdisk_block_size).await;
+
+    let mut tasks = vec![];
+    let mut senders = vec![];
+
+    for i in 0..num_volumes {
+        // Make a new RNG for this volume
+        let volume_rng_seed: u128 = rng.gen();
+        let volume_rng = SmallRng::from_seed(volume_rng_seed.to_le_bytes());
+
+        // Create the new volume
+        let volume_name = format!("testpart-{}", i);
+        let instance_guid = instance.new_volume(&volume_name, TYPE_GUID).await;
+
+        // Connect to the volume
+        let (volume, sender) = Volume::new(instance_guid, fvm_slice_size).await;
+
+        // Create the operator
+        let operator =
+            VolumeOperator::new(volume, volume_rng, max_slices_in_extend, max_vslice_count);
+
+        // Start the operator
+        let task = operator.run(num_operations.unwrap_or(u64::MAX));
+
+        tasks.push(task);
+        senders.push(sender);
     }
 
-    fn seed(&self) -> Option<u128> {
-        self.seed
+    // Send the initial block path to all operators
+    for sender in senders.iter_mut() {
+        let _ = sender.send(instance.block_path()).await;
     }
+
+    // Create the disconnection task in a new thread
+    if disconnect_secs > 0 {
+        Task::blocking(async move {
+            loop {
+                sleep(Duration::from_secs(disconnect_secs));
+
+                if rng.gen_bool(rebind_probability) {
+                    debug!("Rebinding FVM driver");
+                    instance.rebind_fvm_driver().await;
+                } else {
+                    // Crash the old instance and replace it with a new instance.
+                    // This will cause the component tree to be taken down abruptly.
+                    debug!("Killing component manager");
+                    instance.kill_component_manager();
+                    instance =
+                        FvmInstance::new(false, &vmo, fvm_slice_size, ramdisk_block_size).await;
+                }
+
+                // Give the new block path to the operators.
+                // Ignore the result because some operators may have completed.
+                let path = instance.block_path();
+                for sender in senders.iter_mut() {
+                    let _ = sender.send(path.clone()).await;
+                }
+            }
+        })
+        .detach();
+    }
+
+    let operator_tasks = join_all(tasks);
+
+    if let Some(time_limit_secs) = time_limit_secs {
+        operator_tasks.on_timeout(Duration::from_secs(time_limit_secs), || vec![]).await;
+    } else {
+        operator_tasks.await;
+    };
 }
 
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), Error> {
     // Get arguments from command line
     let args: Args = argh::from_env();
-    let rng = args.setup_env();
+
+    let filter = args.log_filter.unwrap_or(LevelFilter::Info);
+    StdoutLogger::init(filter);
+
+    // Initialize seed
+    let seed = match args.seed {
+        Some(seed) => seed,
+        None => random_seed(),
+    };
+    let rng = SmallRng::from_seed(seed.to_le_bytes());
+
+    info!("--------------------- stressor is starting -----------------------");
+    info!("ENVIRONMENT = {:#?}", args);
+    info!("SEED FOR THIS INVOCATION = {}", seed);
+    info!("------------------------------------------------------------------");
+
+    // Setup a panic handler that prints out details of this invocation
+    let args_clone = args.clone();
+    let seed = seed.clone();
+    let default_panic_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        eprintln!("");
+        eprintln!("--------------------- stressor has crashed -----------------------");
+        eprintln!("ENVIRONMENT = {:#?}", args_clone);
+        eprintln!("SEED FOR THIS INVOCATION = {}", seed);
+        eprintln!("------------------------------------------------------------------");
+        eprintln!("");
+        default_panic_hook(panic_info);
+    }));
 
     run_test(
         rng,
--- src/storage/stress-tests/utils/mod.rs
+++ src/storage/stress-tests/utils/mod.rs
@@ -7,12 +7,9 @@ pub mod fvm;
 pub mod io;
 
 use {
-    log::{info, set_logger, set_max_level, LevelFilter},
-    rand::{rngs::SmallRng, FromEntropy, Rng, SeedableRng},
-    std::{
-        fmt::Debug,
-        io::{stdout, Write},
-    },
+    log::{set_logger, set_max_level, LevelFilter},
+    rand::{rngs::SmallRng, FromEntropy, Rng},
+    std::io::{stdout, Write},
 };
 
 // A simple logger that prints to stdout
@@ -51,52 +48,8 @@ impl log::Log for StdoutLogger {
     }
 }
 
-/// This trait helps configure the stress test environment by setting up logging,
-/// crash handling and random number generation.
-pub trait Environment: 'static + Clone + Debug + Send + Sync {
-    /// Initialize a logger, if necessary.
-    fn init_logger(&self);
-
-    /// The seed to be used for this stress test environment.
-    /// If not set, a random seed will be used.
-    fn seed(&self) -> Option<u128>;
-
-    /// Returns the RNG to be used for this test.
-    fn setup_env(&self) -> SmallRng {
-        self.init_logger();
-
-        // Initialize seed
-        let seed = match self.seed() {
-            Some(seed) => seed,
-            None => random_seed(),
-        };
-        let rng = SmallRng::from_seed(seed.to_le_bytes());
-
-        info!("--------------------- stressor is starting -----------------------");
-        info!("ENVIRONMENT = {:#?}", self);
-        info!("SEED FOR THIS INVOCATION = {}", seed);
-        info!("------------------------------------------------------------------");
-
-        // Setup a panic handler that prints out details of this invocation
-        let self_clone = self.clone();
-        let seed = seed.clone();
-        let default_panic_hook = std::panic::take_hook();
-        std::panic::set_hook(Box::new(move |panic_info| {
-            eprintln!("");
-            eprintln!("--------------------- stressor has crashed -----------------------");
-            eprintln!("ENVIRONMENT = {:#?}", self_clone);
-            eprintln!("SEED FOR THIS INVOCATION = {}", seed);
-            eprintln!("------------------------------------------------------------------");
-            eprintln!("");
-            default_panic_hook(panic_info);
-        }));
-
-        rng
-    }
-}
-
 /// Use entropy to generate a random seed
-fn random_seed() -> u128 {
+pub fn random_seed() -> u128 {
     let mut temp_rng = SmallRng::from_entropy();
     temp_rng.gen()
 }
