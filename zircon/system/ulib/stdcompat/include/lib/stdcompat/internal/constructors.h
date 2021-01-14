175e8d70e5cd10b3224bef6758c7b0de9a114664
Gianfranco Valentino <gevalentino@google.com>
2021-01-14 05:28:01 +0000

    [stdcompat][internal]: Constructors Internal.
    Update the constructors_internal.h to match cpp17 namespace.
    Updated default constructor,  move constructor, and move assignemnt
    operators to be noexcept, since the spec requires optional and variant
    to be noxecept.
    
    Added test to verify that the generated constructors are as expected.
    
    Bug: 57349
    Test: stdcompat-tests
    Change-Id: I1725b62d04ee7b141d37887adf0272292ebcd154
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/466161
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/system/ulib/stdcompat/include/lib/stdcompat/internal/constructors.h
+++ zircon/system/ulib/stdcompat/include/lib/stdcompat/internal/constructors.h
@@ -2,15 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef LIB_STDCOMPAT_INTERNAL_CONSTRUCTORS_INTERNAL_H_
-#define LIB_STDCOMPAT_INTERNAL_CONSTRUCTORS_INTERNAL_H_
+#ifndef LIB_STDCOMPAT_INTERNAL_CONSTRUCTORS_H_
+#define LIB_STDCOMPAT_INTERNAL_CONSTRUCTORS_H_
 
 #include <type_traits>
 #include <utility>
 
-#include "utility_internal.h"
-
-namespace fit {
+namespace cpp17 {
 namespace internal {
 
 // Mixin that implicitly deletes the subclass default constructor when type T
@@ -96,6 +94,6 @@ struct modulate_copy_and_move
     : modulate_copy_and_move_index<std::index_sequence_for<Ts...>, Ts...> {};
 
 }  // namespace internal
-}  // namespace fit
+}  // namespace cpp17
 
-#endif  // LIB_STDCOMPAT_INTERNAL_CONSTRUCTORS_INTERNAL_H_
+#endif  // LIB_STDCOMPAT_INTERNAL_CONSTRUCTORS_H_
---
+++ zircon/system/ulib/stdcompat/test/constructors_internal_test.cc
@@ -0,0 +1,118 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/stdcompat/internal/constructors.h>
+
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+struct DeletedCopyConstructor {
+  DeletedCopyConstructor(const DeletedCopyConstructor&) noexcept = delete;
+  DeletedCopyConstructor& operator=(const DeletedCopyConstructor&) noexcept = default;
+};
+
+struct DeletedCopyAssignment {
+  DeletedCopyAssignment(const DeletedCopyAssignment&) noexcept = default;
+  DeletedCopyAssignment& operator=(const DeletedCopyAssignment&) noexcept = delete;
+};
+
+struct DeletedMoveConstructor {
+  DeletedMoveConstructor(DeletedMoveConstructor&&) noexcept = delete;
+  DeletedMoveConstructor& operator=(DeletedMoveConstructor&&) noexcept = default;
+};
+
+struct DeletedMoveAssignament {
+  DeletedMoveAssignament(DeletedMoveAssignament&&) noexcept = default;
+  DeletedMoveAssignament& operator=(DeletedMoveAssignament&&) noexcept = delete;
+};
+
+TEST(ModulateCopyAndMoveTest, SingleTypeIsModulatedCorrectly) {
+  static_assert(!std::is_copy_constructible<
+                cpp17::internal::modulate_copy_and_move<DeletedCopyConstructor>>::value);
+  static_assert(std::is_copy_assignable<
+                cpp17::internal::modulate_copy_and_move<DeletedCopyConstructor>>::value);
+
+  static_assert(std::is_copy_constructible<
+                cpp17::internal::modulate_copy_and_move<DeletedCopyAssignment>>::value);
+  static_assert(!std::is_copy_assignable<
+                cpp17::internal::modulate_copy_and_move<DeletedCopyAssignment>>::value);
+
+  static_assert(!std::is_move_constructible<
+                cpp17::internal::modulate_copy_and_move<DeletedMoveConstructor>>::value);
+  static_assert(std::is_move_assignable<
+                cpp17::internal::modulate_copy_and_move<DeletedMoveConstructor>>::value);
+
+  static_assert(std::is_move_constructible<
+                cpp17::internal::modulate_copy_and_move<DeletedMoveAssignament>>::value);
+  static_assert(!std::is_move_assignable<
+                cpp17::internal::modulate_copy_and_move<DeletedMoveAssignament>>::value);
+}
+
+TEST(ModulateCopyAndMoveTest, MultipleTypesAreModulatedCorrectly) {
+  {
+    using MultipleTypes = cpp17::internal::modulate_copy_and_move<DeletedCopyConstructor>;
+
+    static_assert(!std::is_copy_constructible<MultipleTypes>::value);
+  }
+
+  {
+    using MultipleTypes =
+        cpp17::internal::modulate_copy_and_move<DeletedCopyConstructor, DeletedCopyAssignment>;
+
+    static_assert(!std::is_copy_constructible<MultipleTypes>::value);
+    static_assert(!std::is_copy_assignable<MultipleTypes>::value);
+  }
+
+  {
+    using MultipleTypes =
+        cpp17::internal::modulate_copy_and_move<DeletedCopyConstructor, DeletedMoveAssignament>;
+
+    static_assert(!std::is_copy_constructible<MultipleTypes>::value);
+    static_assert(!std::is_move_assignable<MultipleTypes>::value);
+  }
+
+  {
+    using MultipleTypes =
+        cpp17::internal::modulate_copy_and_move<DeletedCopyConstructor, DeletedMoveAssignament,
+                                                DeletedMoveConstructor>;
+
+    static_assert(!std::is_copy_constructible<MultipleTypes>::value);
+    static_assert(!std::is_move_constructible<MultipleTypes>::value);
+    static_assert(!std::is_move_assignable<MultipleTypes>::value);
+  }
+
+  {
+    using MultipleTypes =
+        cpp17::internal::modulate_copy_and_move<DeletedCopyConstructor, DeletedCopyAssignment,
+                                                DeletedMoveAssignament, DeletedMoveConstructor>;
+
+    static_assert(!std::is_copy_constructible<MultipleTypes>::value);
+    static_assert(!std::is_copy_assignable<MultipleTypes>::value);
+    static_assert(!std::is_move_constructible<MultipleTypes>::value);
+    static_assert(!std::is_move_assignable<MultipleTypes>::value);
+  }
+}
+
+TEST(ModulateDefaultConstructorTest, SingleTypeModulatedCorrectly) {
+  {
+    struct NonDefaultConstructible {
+      NonDefaultConstructible() = delete;
+    };
+
+    using ModulatedType = cpp17::internal::modulate_default_constructor<NonDefaultConstructible>;
+    static_assert(!std::is_default_constructible<ModulatedType>::value);
+  }
+
+  {
+    struct DefaultConstructible {};
+
+    using ModulatedType = cpp17::internal::modulate_default_constructor<DefaultConstructible>;
+    static_assert(std::is_default_constructible<ModulatedType>::value);
+  }
+}
+
+}  // namespace
