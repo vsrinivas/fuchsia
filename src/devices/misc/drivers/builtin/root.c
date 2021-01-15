73dd79cab7b5225ba05db975e7e60eeb14ce9841
Garratt Gallagher <garratt@google.com>
2021-01-15 22:21:48 +0000

    [fdf] Migrate root_drivers driver to bindc
    Test: Bind tests included.
    Change-Id: I9bdd762e0799cd0342aa43de18043b11170f63b4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/470982
    Fuchsia-Auto-Submit: Garratt Gallagher <garratt@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/misc/drivers/builtin/root.c
+++ src/devices/misc/drivers/builtin/root.c
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <ddk/binding.h>
 #include <ddk/device.h>
 #include <ddk/driver.h>
+
+#include "src/devices/misc/drivers/builtin/root_drivers_bind.h"
 
 zx_status_t null_bind(void* ctx, zx_device_t* parent);
 zx_status_t zero_bind(void* ctx, zx_device_t* parent);
@@ -20,5 +21,4 @@ static zx_driver_ops_t root_driver_ops = {
     .bind = root_bind,
 };
 
-ZIRCON_DRIVER_BEGIN(root_drivers, root_driver_ops, "zircon", "0.1", 1)
-BI_MATCH_IF(EQ, BIND_PROTOCOL, ZX_PROTOCOL_ROOT), ZIRCON_DRIVER_END(root_drivers)
+ZIRCON_DRIVER(root_drivers, root_driver_ops, "zircon", "0.1");
--- src/devices/misc/drivers/builtin/zero.c
+++ src/devices/misc/drivers/builtin/zero.c
@@ -7,7 +7,6 @@
 #include <string.h>
 #include <zircon/types.h>
 
-#include <ddk/binding.h>
 #include <ddk/device.h>
 #include <ddk/driver.h>
 
