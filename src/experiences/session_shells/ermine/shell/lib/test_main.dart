e5258e6374a70710c8e7dbb6b92d8a994266ec64
Darren Chan <chandarren@google.com>
2021-01-17 21:13:17 +0000

    [build][flutter] Remove flutter_driver_extendable.
    Remove `flutter_driver_extendable`:
     - Remove from relevant build rules.
     - Create separate dart entrypoints that use enableFlutterDriverExtension().
    
    Bug: 66605
    Change-Id: Idfea636b4c9dd05f0d5b6e46d34fa47dbb127621
    Reviewed-on: https://fuchsia-review.googlesource.com/c/experiences/+/471460
    Commit-Queue: Darren Chan <chandarren@google.com>
    Reviewed-by: Sanjay Chouksey <sanjayc@google.com>
    Reviewed-by: Naud Ghebre <naudzghebre@google.com>

---
+++ src/experiences/session_shells/ermine/shell/lib/test_main.dart
@@ -0,0 +1,16 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:flutter/services.dart';
+
+import 'main.dart' as entrypoint;
+
+Future<void> main() async {
+  final handler = OptionalMethodChannel('flutter_driver/handler');
+  enableFlutterDriverExtension(handler: (String data) async {
+    return handler.invokeMethod(data);
+  });
+  return entrypoint.main();
+}
