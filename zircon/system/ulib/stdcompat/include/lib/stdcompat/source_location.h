f31f3816a30b740e3b2a4ed3046ad0a9ad26deb9
Gianfranco Valentino <gevalentino@google.com>
2021-01-21 23:08:57 +0000

    [stdcompat]: Port fit::source_location.
    Made cpp20::source_location available. Added test for alias
    verification on cpp20(or as long as the cpp_lib_source_location is at
    least the right draft).
    
    Bug: 57356
    Test: stdcompat-tests
    Change-Id: I10a731917a852ac3eb9a4ab8d11e0f20da0f7855
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/471918
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/system/ulib/stdcompat/include/lib/stdcompat/source_location.h
+++ zircon/system/ulib/stdcompat/include/lib/stdcompat/source_location.h
@@ -1,33 +1,27 @@
 // Copyright 2020 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-#ifndef LIB_FIT_SOURCE_LOCATION_H_
-#define LIB_FIT_SOURCE_LOCATION_H_
+#ifndef LIB_STDCOMPAT_SOURCE_LOCATION_H_
+#define LIB_STDCOMPAT_SOURCE_LOCATION_H_
 
-#if __cplusplus > 201703L
-#include <version>
-#endif
+#include "version.h"
 
-#if defined(__cpp_lib_source_location) && __cpp_lib_source_location && \
-    !defined(FORCE_FIT_SOURCE_LOCATION)
-
-// In C++20 fit::source_location should simply be an alias for std::source_location.
+#if __cpp_lib_source_location >= 201907L && !defined(LIB_STDCOMPAT_USE_POLYFILLS)
 
 #include <source_location>
 
-namespace fit {
+namespace cpp20 {
 
 using std::source_location;
 
-}  // namespace fit
+}  // namespace cpp20
 
 #else
 
 #include <cstdint>
 
-namespace fit {
+namespace cpp20 {
 
-// Polyfill implementation of std::source_location.
 class source_location {
   struct values {
     constexpr explicit values(std::uint_least32_t line, std::uint_least32_t column,
@@ -46,40 +40,42 @@ class source_location {
   source_location(source_location&& other) noexcept = default;
   ~source_location() = default;
 
-#define LIB_FIT_SOURCE_LOCATION_LINE (0)
-#define LIB_FIT_SOURCE_LOCATION_COLUMN (0)
-#define LIB_FIT_SOURCE_LOCATION_FILE ("")
-#define LIB_FIT_SOURCE_LOCATION_FUNCTION ("")
+#define LIB_STDCOMPAT_SOURCE_LOCATION_LINE (0)
+#define LIB_STDCOMPAT_SOURCE_LOCATION_COLUMN (0)
+#define LIB_STDCOMPAT_SOURCE_LOCATION_FILE ("")
+#define LIB_STDCOMPAT_SOURCE_LOCATION_FUNCTION ("")
 
 #if defined(__has_builtin)
 #if __has_builtin(__builtin_LINE)
-#undef LIB_FIT_SOURCE_LOCATION_LINE
-#define LIB_FIT_SOURCE_LOCATION_LINE (__builtin_LINE())
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_LINE
+#define LIB_STDCOMPAT_SOURCE_LOCATION_LINE (__builtin_LINE())
 #endif
 #if __has_builtin(__builtin_COLUMN)
-#undef LIB_FIT_SOURCE_LOCATION_COLUMN
-#define LIB_FIT_SOURCE_LOCATION_COLUMN (__builtin_COLUMN())
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_COLUMN
+#define LIB_STDCOMPAT_SOURCE_LOCATION_COLUMN (__builtin_COLUMN())
 #endif
 #if __has_builtin(__builtin_FILE)
-#undef LIB_FIT_SOURCE_LOCATION_FILE
-#define LIB_FIT_SOURCE_LOCATION_FILE (__builtin_FILE())
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_FILE
+#define LIB_STDCOMPAT_SOURCE_LOCATION_FILE (__builtin_FILE())
 #endif
 #if __has_builtin(__builtin_FUNCTION)
-#undef LIB_FIT_SOURCE_LOCATION_FUNCTION
-#define LIB_FIT_SOURCE_LOCATION_FUNCTION (__builtin_FUNCTION())
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_FUNCTION
+#define LIB_STDCOMPAT_SOURCE_LOCATION_FUNCTION (__builtin_FUNCTION())
 #endif
 #endif
 
-  static constexpr source_location current(
-      values value = values{LIB_FIT_SOURCE_LOCATION_LINE, LIB_FIT_SOURCE_LOCATION_COLUMN,
-                            LIB_FIT_SOURCE_LOCATION_FILE, LIB_FIT_SOURCE_LOCATION_FUNCTION}) {
+  static constexpr source_location current(values value = values{
+                                               LIB_STDCOMPAT_SOURCE_LOCATION_LINE,
+                                               LIB_STDCOMPAT_SOURCE_LOCATION_COLUMN,
+                                               LIB_STDCOMPAT_SOURCE_LOCATION_FILE,
+                                               LIB_STDCOMPAT_SOURCE_LOCATION_FUNCTION}) {
     return source_location{value};
   }
 
-#undef LIB_FIT_SOURCE_LOCATION_LINE
-#undef LIB_FIT_SOURCE_LOCATION_COLUMN
-#undef LIB_FIT_SOURCE_LOCATION_FILE
-#undef LIB_FIT_SOURCE_LOCATION_FUNCTION
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_LINE
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_COLUMN
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_FILE
+#undef LIB_STDCOMPAT_SOURCE_LOCATION_FUNCTION
 
   constexpr std::uint_least32_t line() const noexcept { return values_.line; }
 
@@ -95,8 +91,8 @@ class source_location {
   values values_{0, 0, "", ""};
 };
 
-}  // namespace fit
+}  // namespace cpp20
 
-#endif
+#endif  // __cpp_lib_source_location >= 201907L && !defined(LIB_STDCOMPAT_USE_POLYFILLS)
 
-#endif  // LIB_FIT_SOURCE_LOCATION_H_
+#endif  // LIB_STDCOMPAT_SOURCE_LOCATION_H_
--- zircon/system/ulib/stdcompat/test/source_location_test.cc
+++ zircon/system/ulib/stdcompat/test/source_location_test.cc
@@ -1,28 +1,27 @@
 // Copyright 2020 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-#define FORCE_FIT_SOURCE_LOCATION
-#include <lib/fit/source_location.h>
+#include <lib/stdcompat/source_location.h>
 
-#include <zxtest/zxtest.h>
+#include <gtest/gtest.h>
 
 namespace {
 
-std::tuple<fit::source_location, uint32_t, uint32_t> FooBar() {
+std::tuple<cpp20::source_location, uint32_t, uint32_t> FooBar() {
   // clang-format off
   uint32_t line = __LINE__ + 1;
-  auto source_location = fit::source_location::current();
+  auto source_location = cpp20::source_location::current();
   uint32_t column =      26;
   // clang-format on
   return {source_location, line, column};
 }
 
-fit::source_location ExampleLoggingFunction(
-    fit::source_location location = fit::source_location::current()) {
+cpp20::source_location ExampleLoggingFunction(
+    cpp20::source_location location = cpp20::source_location::current()) {
   return location;
 }
 
-std::tuple<fit::source_location, uint32_t, uint32_t> BizBaz() {
+std::tuple<cpp20::source_location, uint32_t, uint32_t> BizBaz() {
   // clang-format off
   uint32_t line = __LINE__ + 1;
   auto source_location = ExampleLoggingFunction();
@@ -31,7 +30,7 @@ std::tuple<fit::source_location, uint32_t, uint32_t> BizBaz() {
   return {source_location, line, column};
 }
 
-TEST(SourceLocationTests, direct_call_values) {
+TEST(SourceLocationTest, DirectCallValue) {
   auto [location, line, column] = FooBar();
   EXPECT_EQ(location.file_name(), __FILE__);
   EXPECT_EQ(location.function_name(), "FooBar");
@@ -39,12 +38,18 @@ TEST(SourceLocationTests, direct_call_values) {
   EXPECT_EQ(location.column(), column);
 }
 
-TEST(SourceLocationTests, default_parameter_values) {
+TEST(SourceLocationTest, DefaultParameterValue) {
   auto [location, line, column] = BizBaz();
   EXPECT_EQ(location.file_name(), __FILE__);
   EXPECT_EQ(location.function_name(), "BizBaz");
   EXPECT_EQ(location.line(), line);
   EXPECT_EQ(location.column(), column);
 }
+
+#if __cpp_lib_source_location >= 201907L && !defined(LIB_STDCOMPAT_USE_POLYFILLS)
+TEST(SourceLocationTest, IsAliasOfStdInStd20) {
+  static_assert(std::is_same_v<cpp20::source_location, std::source_location>);
+}
+#endif  // __cpp_lib_source_location >= 201907L && !defined(LIB_STDCOMPAT_USE_POLYFILLS)
 
 }  // namespace
