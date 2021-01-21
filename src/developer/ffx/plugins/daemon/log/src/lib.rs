05a8c7266b71b6b8541974c93ebdccf699b3e2f3
Matt Boetger <boetger@google.com>
2021-01-21 19:22:56 +0000

    [ffx] Add `ffx daemon log` to dump the daemon log file.
    Change-Id: I725786f41cd3b34bb37d526a518b758e238324ef
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/470599
    Commit-Queue: Matthew Boetger <boetger@google.com>
    Reviewed-by: James Tucker <raggi@google.com>
    Reviewed-by: Andrew Davies <awdavies@google.com>

---
+++ src/developer/ffx/plugins/daemon/log/src/lib.rs
@@ -0,0 +1,28 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::Result,
+    ffx_config::get,
+    ffx_core::{ffx_bail, ffx_plugin},
+    ffx_daemon_log_args::LogCommand,
+    std::fs::File,
+    std::io::{BufRead, BufReader},
+    std::path::PathBuf,
+};
+
+#[ffx_plugin()]
+pub async fn daemon(_cmd: LogCommand) -> Result<()> {
+    if !get("log.enabled").await? {
+        ffx_bail!("Logging is not enabled.");
+    }
+    let mut log_path: PathBuf = get("log.dir").await?;
+    log_path.push("ffx.daemon.log");
+    let file = File::open(log_path).or_else(|_| ffx_bail!("Daemon log not found."))?;
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+    Ok(())
+}
