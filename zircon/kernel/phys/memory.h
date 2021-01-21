7ee117b110014e23bb774dd94df10b84bf566fa8
David Greenaway <dgreenaway@google.com>
2021-01-21 03:38:36 +0000

    [kernel][phys] Update decompress-test to use dynamic memory.
    Currently, decompress-test uses fixed-size memory buffers for scratch
    memory and the decompressed output.
    
    We move the decompression routines into a new file and update the
    existing test to call into this library.
    
    Change-Id: Id7ee9046ce05ece3635b7256cc9ef3006dd395e7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/465101
    Commit-Queue: David Greenaway <dgreenaway@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/phys/memory.h
+++ zircon/kernel/phys/memory.h
@@ -12,6 +12,9 @@
 
 #include <cstddef>
 
+#include <ktl/span.h>
+#include <ktl/unique_ptr.h>
+
 // Parse the given ZBI to initialise the memory allocator with free ranges of memory.
 //
 // Panics on failure.
@@ -24,5 +27,33 @@ void* AllocateMemory(size_t size, size_t alignment = __STDCPP_DEFAULT_NEW_ALIGNM
 
 // Return the given range of memory back to the allocator.
 void FreeMemory(void* ptr, size_t size);
+
+// A deleter for ktl::unique_ptr that maintains the allocation size. See
+// the function `MakeUnique` below for a convenient way to use it.
+struct AllocationDeleter {
+  explicit AllocationDeleter(size_t size) : size(size) {}
+
+  void operator()(void* ptr) const {
+    if (ptr != nullptr) {
+      FreeMemory(ptr, size);
+    }
+  }
+
+  size_t size;
+};
+
+// Short-hand for a ktl::unique_ptr with a custom deleter that releases
+// back into the physboot memory pool.
+template <typename T>
+using UniquePtr = ktl::unique_ptr<T, AllocationDeleter>;
+
+// Create a ktl::unique_ptr for an allocation made via AllocateMemory.
+//
+// We different from a plain ktl::unique_unique pointer because we have a custom
+// deleter (`FreeMemory`) and also need to track the size of the allocation.
+template <typename T>
+UniquePtr<T> AdoptAllocation(T* ptr, size_t size) {
+  return ktl::unique_ptr<T, AllocationDeleter>(ptr, AllocationDeleter{size});
+}
 
 #endif  // ZIRCON_KERNEL_PHYS_MEMORY_H_
--- zircon/kernel/phys/test/decompress-test.cc
+++ zircon/kernel/phys/test/decompress-test.cc
@@ -4,6 +4,9 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT
 
+#include "../decompress.h"
+
+#include <inttypes.h>
 #include <lib/fitx/result.h>
 #include <lib/zbitl/error_stdio.h>
 #include <lib/zbitl/view.h>
@@ -26,61 +29,57 @@ constexpr char kTestPayload[] =
     "abcdefghijklmnopqrstuvwxyz";
 constexpr uint32_t kTestPayloadSize = sizeof(kTestPayload) - 1;
 
-// This test uses a fixed buffer so it doesn't need any real memory handling.
-constexpr size_t kScratchSize = 256 << 10;
-
-struct ScratchAllocator {
-  fitx::result<ktl::string_view, ScratchAllocator> operator()(size_t size) const {
-    ZX_ASSERT(size <= sizeof(buffer_));
-    return fitx::ok(ScratchAllocator{});
-  }
-
-  void* get() const { return buffer_; }
-
-  inline static ktl::byte buffer_ alignas(std::max_align_t)[kScratchSize];
-};
-
 }  // namespace
 
 int TestMain(void* zbi_ptr, arch::EarlyTicks) {
-  zbitl::PermissiveView<ktl::span<ktl::byte>> zbi({static_cast<ktl::byte*>(zbi_ptr), SIZE_MAX});
+  // Initialize memory for allocation/free.
+  InitMemory(static_cast<const zbi_header_t*>(zbi_ptr));
+
+  // Fetch ZBI.
+  zbitl::View<zbitl::ByteView> zbi(
+      zbitl::StorageFromRawHeader(static_cast<const zbi_header_t*>(zbi_ptr)));
 
   printf("Scanning ZBI of %zu bytes...\n", zbi.size_bytes());
 
-  for (auto it = zbi.begin(); it != zbi.end(); ++it) {
-    if ((*it).header->type == ZBI_TYPE_STORAGE_RAMDISK) {
-      uint32_t size = zbitl::UncompressedLength(*(*it).header);
-      printf("Found RAMDISK item of %u bytes (%u)...\n", (*it).header->length, size);
-
-      // Ignore the iteration state now that we've found the item.
-      zbi.ignore_error();
-
-      char payload[kTestPayloadSize];
-      ZX_ASSERT_MSG(size == kTestPayloadSize, "decompressed size %u != test size %u\n", size,
-                    kTestPayloadSize);
-
-      if (auto result =
-              zbi.CopyStorageItem(ktl::span(payload, sizeof(payload)), it, ScratchAllocator{});
-          result.is_error()) {
-        zbitl::PrintViewCopyError(result.error_value());
-        return 1;
-      }
-
-      printf("Copied payload: `%.*s`\n", static_cast<int>(sizeof(payload)), payload);
-      if (!memcmp(payload, kTestPayload, sizeof(payload))) {
-        return 0;
-      }
-
-      printf("FAILED!  Expected payload: `%s`\n", kTestPayload);
-      return 1;
-    }
+  // Search for a payload of type ZBI_TYPE_STORAGE_RAMDISK.
+  auto it = zbi.begin();
+  while (it != zbi.end() && (*it).header->type != ZBI_TYPE_STORAGE_RAMDISK) {
+    ++it;
   }
 
+  // Ensure there was no error during iteration.
   if (auto result = zbi.take_error(); result.is_error()) {
+    printf("FAILED: Error while enumerating ZBI payload: ");
     zbitl::PrintViewError(result.error_value());
     return 1;
   }
 
-  printf("No RAMDISK item found!\n");
-  return 1;
+  // Fail if we didn't find anything.
+  if (it == zbi.end()) {
+    printf("FAILED: No payload found.\n");
+    return 1;
+  }
+
+  // Attempt to decompress the payload.
+  auto result = CopyAndDecompressItem(zbi, it);
+  if (result.is_error()) {
+    printf("FAILED: Could not decompress payload.\n");
+    return 1;
+  }
+
+  // Ensure the payload matched our expected value.
+  printf("Copied payload: `%.*s`\n", static_cast<int>(result->size),
+         reinterpret_cast<uint8_t*>(result->ptr.get()));
+  if (result->size != kTestPayloadSize) {
+    printf("FAILED: Payload size incorrect: wanted %" PRIu32 ", got %" PRIu64 "\n",
+           kTestPayloadSize, result->size);
+    return 1;
+  }
+  if (memcmp(result->ptr.get(), kTestPayload, kTestPayloadSize) != 0) {
+    printf("FAILED! Incorrect payload value. Expected payload: `%s`\n", kTestPayload);
+    return 1;
+  }
+
+  printf("Success.\n");
+  return 0;
 }
