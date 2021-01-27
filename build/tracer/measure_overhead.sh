0c37443cef81f23a0d43ca4fbd471158bcbad740
David Fang <fangism@google.com>
2021-01-27 21:20:50 +0000

    [tracer] Benchmark action-traced builds.
    Script for anyone to measure tracing overhead.
    Tests configuations:
      * no tracing (baseline)
      * tracing enabled, but no checks (testing Python wrapping overhead)
      * tracing enabled, all checks enabled (tests trace check overhead)
    
    Test:
      build/tracer/measure_overhead.sh 3
    
    Change-Id: Id24591fc6621877fde645231afca733dc37917a7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/475280
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Fuchsia-Auto-Submit: Shai Barack <shayba@google.com>

---
+++ build/tracer/measure_overhead.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+# Copyright 2020 The Fuchsia Authors. All Rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script for measuring action tracing build overhead.
+# See usage() below.
+
+set -e
+
+# Constants:
+
+# These files will be locally modified for this experiment:
+# Assume default output dir.
+args_gn=out/default/args.gn
+bcfg_gn=build/config/BUILDCONFIG.gn
+
+# The `time` binary has more options than the shell built-in.
+time=/usr/bin/time
+
+# Where locally patched files and logs will be saved.
+exp_dir="$(mktemp -t -d tmp.action_trace_overhead.XXXXXXXX)"
+
+function usage() {
+  cat <<EOF
+$0 N
+
+This script measures the overhead of action-tracing.
+Run this from \$FUCHSIA_DIR, your source checkout root.
+
+N: (positional) run the experiment this many times.
+
+This makes temporary changes to the following files,
+but will restore them after all is done:
+  $args_gn
+  $bcfg_gn
+
+This leaves all others settings in args.gn, e.g. goma, untouched.
+Temporary and log files will be kept in $exp_dir (example).
+EOF
+}
+
+# Expect one positional argument, print usage() otherwise.
+test "$#" = 1 || { usage; exit 1;}
+N="$1"
+
+# Disable tracing by patching the args.gn file.
+function disable_trace() {
+  local args_gn="$1"
+  if grep -q -w build_should_trace_actions "$args_gn"
+  then
+    sed -e '/build_should_trace_actions/s|true|false|' "$args_gn"
+  else
+    cat "$args_gn"
+    echo "build_should_trace_actions = false"
+  fi
+}
+
+# Enable tracing by patching the args.gn file.
+function enable_trace() {
+  local args_gn="$1"
+  if grep -q -w build_should_trace_actions "$args_gn"
+  then
+    sed -e '/build_should_trace_actions/s|false|true|' "$args_gn"
+  else
+    cat "$args_gn"
+    echo "build_should_trace_actions = true"
+  fi
+}
+
+function disable_checks() {
+  local bcfg_gn="$1"
+  # Ignore trace errors, to let the build continue past trace errors.
+  # Force --no-check-access-permissions and --no-check-output-freshness.
+  sed -e 's|--failed-check-status=1|--failed-check-status=0|' \
+    -e '/if (!defined(hermetic_deps))/,/}/s|hermetic_deps = true|hermetic_deps = false|' \
+    -e '/if (!defined(all_outputs_fresh))/,/}/s|all_outputs_fresh = true|hermetic_deps = false|' \
+    "$bcfg_gn"
+}
+
+function enable_checks() {
+  local bcfg_gn="$1"
+  # Ignore trace errors, to let the build continue past trace errors.
+  # Let --check-access-permissions and --check-output-freshness run by default,
+  # disregarding suppressions.
+  sed -e 's|--failed-check-status=1|--failed-check-status=0|' \
+    -e '/if (!defined(hermetic_deps))/,/}/s|hermetic_deps = false|hermetic_deps = true|' \
+    -e '/if (!defined(all_outputs_fresh))/,/}/s|all_outputs_fresh = false|all_outputs_fresh = true|' \
+    "$bcfg_gn"
+}
+
+# 1. Generate experiment configurations.
+# Backup temporarily modified files
+echo "Using temporary directory for local changes and logs: $exp_dir"
+mkdir -p "$exp_dir"/"$(dirname "$args_gn")"
+mkdir -p "$exp_dir"/"$(dirname "$bcfg_gn")"
+cp "$args_gn" "$exp_dir"/"$args_gn".orig
+cp "$bcfg_gn" "$exp_dir"/"$bcfg_gn".orig
+
+# Keep these around so we can diff them easily.
+disable_trace "$exp_dir"/"$args_gn".orig > "$exp_dir"/"$args_gn.notrace"
+enable_trace "$exp_dir"/"$args_gn".orig > "$exp_dir"/"$args_gn.trace"
+
+# Configure with checks enabled/disabled, ignoring suppressions.
+disable_checks "$exp_dir"/"$bcfg_gn".orig > "$exp_dir"/"$bcfg_gn".nochecks
+enable_checks "$exp_dir"/"$bcfg_gn".orig > "$exp_dir"/"$bcfg_gn".allchecks
+
+function vdiff() {
+  echo "diff -u $1 $2"
+  diff -u "$1" "$2" || :
+}
+
+function vcat() {
+  echo "### $1 ###"
+  cat "$1"
+}
+
+vdiff "$exp_dir"/"$args_gn".{orig,notrace}
+vdiff "$exp_dir"/"$args_gn".{orig,trace}
+vdiff "$exp_dir"/"$bcfg_gn".{orig,nochecks}
+vdiff "$exp_dir"/"$bcfg_gn".{orig,allchecks}
+
+# Benchmark build one time.
+function run_once() {
+  local log="$1"
+  # do `fx clean-build`, but only time the build part.
+  fx clean
+  "$time" -a --output="$log" fx build
+}
+
+### Run benchmarks.
+function run_all() {
+  local N="$1"
+  echo "=== Running all configurations ==="
+
+  echo "===== Benchmarking baseline, with no tracing ====="
+  cp "$exp_dir"/"$args_gn".notrace "$args_gn"
+  cp "$exp_dir"/"$bcfg_gn".nochecks "$bcfg_gn"
+  fx gen
+  rm -f build.time.notrace
+  for i in $(seq $N); do
+    echo "======= run $i ======="
+    run_once "$exp_dir"/build.time.notrace
+  done
+
+  echo "===== Benchmarking tracing, all checks disabled ====="
+  # 2. Measure with tracing, but no checks
+  cp "$exp_dir"/"$args_gn".trace "$args_gn"
+  cp "$exp_dir"/"$bcfg_gn".nochecks "$bcfg_gn"
+  fx gen
+  rm -f build.time.trace.nochecks
+  for i in $(seq $N); do
+    echo "======= run $i ======="
+    run_once "$exp_dir"/build.time.trace.nochecks
+  done
+
+  echo "===== Benchmarking tracing, all checks enabled ====="
+  cp "$exp_dir"/"$args_gn".trace "$args_gn"
+  cp "$exp_dir"/"$bcfg_gn".allchecks "$bcfg_gn"
+  fx gen
+  rm -f build.time.trace.allchecks
+  for i in $(seq $N); do
+    echo "======= run $i ======="
+    run_once "$exp_dir"/build.time.trace.allchecks
+  done
+
+  cat <<EOF
+=== All done. ===
+Files are in: $exp_dir
+Test configurations are saved in:
+
+  $exp_dir/$args_gn.notrace
+  $exp_dir/$args_gn.trace
+
+  $exp_dir/$bcfg_gn.nochecks
+  $exp_dir/$bcfg_gn.allchecks
+
+Raw time logs are in:
+  $exp_dir/build.time.notrace (baseline, no tracing)
+  $exp_dir/build.time.trace.nochecks (traced, no checks)
+  $exp_dir/build.time.trace.allchecks (traced, all checks)
+
+EOF
+  vcat $exp_dir/build.time.notrace
+  vcat $exp_dir/build.time.trace.nochecks
+  vcat $exp_dir/build.time.trace.allchecks
+}
+
+function restore_files() {
+echo "=== Restoring original configuration. ==="
+# Temporary files will still be left around for examination.
+cp experiment/"$args_gn".orig "$args_gn"
+cp experiment/"$bcfg_gn".orig "$bcfg_gn"
+}
+
+trap restores_files EXIT
+
+run_all "$N"
+
