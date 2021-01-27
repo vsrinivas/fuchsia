9fdfc6da8ee31f5d30e80c63543aba76bcdfb63b
Jocelyn Dang <jocelyndang@google.com>
2021-01-27 18:53:50 +0000

    [driver-registrar] Basic commandline tool for calling DriverRegistrar.
    Also update cml files with DriverRegistrar protocol.
    
    This is for short term use in testing loading out-of-tree drivers,
    and should be replaced with something better in future.
    
    Bug: 66669
    
    Change-Id: I8d9ba452a57c98862dc9851da1f571fcda7a2fec
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/464998
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

---
+++ src/devices/bin/driver-registrar/driver-registrar.cc
@@ -0,0 +1,48 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuchsia/driver/registrar/llcpp/fidl.h>
+#include <fuchsia/pkg/cpp/fidl.h>
+#include <lib/fdio/directory.h>
+#include <lib/zx/channel.h>
+#include <stdio.h>
+
+#include <fbl/string_printf.h>
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <driver package url>\n", argv[0]);
+    return -1;
+  }
+
+  zx::channel local, remote;
+  zx_status_t status = zx::channel::create(0u, &local, &remote);
+  if (status != ZX_OK) {
+    fprintf(stderr, "Failed to create channel, status %d\n", status);
+    return status;
+  }
+
+  const auto svc_path =
+      fbl::StringPrintf("/svc/%s", llcpp::fuchsia::driver::registrar::DriverRegistrar::Name);
+  status = fdio_service_connect(svc_path.c_str(), remote.release());
+  if (status != ZX_OK) {
+    fprintf(stderr, "fdio_service_connect failed, pathc %s, status %d\n", svc_path.c_str(), status);
+    return status;
+  }
+  llcpp::fuchsia::driver::registrar::DriverRegistrar::SyncClient client(std::move(local));
+
+  auto resp =
+      client.Register(llcpp::fuchsia::pkg::PackageUrl{fidl::unowned_str(argv[1], strlen(argv[1]))});
+  if (!resp.ok()) {
+    fprintf(stderr, "Failed to call DriverRegistrar::Register for driver package %s\n", argv[1]);
+    return -1;
+  }
+  if (resp->result.is_err()) {
+    fprintf(stderr, "DriverRegistrar::Register returned err %d for driver package %s\n",
+            resp->result.err(), argv[1]);
+    return -1;
+  }
+  printf("DriverRegistrar::Register successfully registered driver package %s\n", argv[1]);
+  return 0;
+}
