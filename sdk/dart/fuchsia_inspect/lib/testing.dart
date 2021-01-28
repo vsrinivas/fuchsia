11c8a869b44b77076741011d345f14befa4c0c60
naudzghebre <naudzghebre@google.com>
2021-01-28 22:01:04 +0000

    [topaz][fuchsia][migration] Remove //sdk/dart/fuchsia_scenic_flutter and //sdk/dart/fuchsia_inspect temporarily.
    Change-Id: I2defdcc821164a3f039740e1717be73e44726d84
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/477557
    Commit-Queue: Naud Ghebre <naudzghebre@google.com>
    Reviewed-by: Chase Latta <chaselatta@google.com>

--- sdk/dart/fuchsia_inspect/lib/testing.dart
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Testing matchers for Inspect Dart.
-export 'src/testing/matcher.dart';
-
-// Utility classes, like FakeVmoHolder.
-export 'src/testing/util.dart';
