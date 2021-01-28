b4483da88f6dc5acd21a1924c988d36e65534950
William Xiao <wxyz@google.com>
2021-01-28 00:42:12 +0000

    Reland "[settings] Add additional E2E tests for volume policy transforms"
    The initial version of the E2E tests didn't specify a fake audio core
    service, so the audio controller was connecting directly to the real
    audio core service, causing flakiness in the test. This CL adds a fake
    audio core service for these tests. This test also switches from the
    deprecated watch2 to watch for the audio setting, though this change is
    benign.
    
    Fixed: 60367
    Test: fx test setui_service_tests
    MULTIPLY: setui_service_tests
    
    Original change's description:
    > Revert "[settings] Add additional E2E tests for volume policy transforms"
    >
    > This reverts commit 0c10e706c82f3353658c867a981ad41bf0557f62.
    >
    > Reason for revert: tests are flaky
    > Bug: 68411
    >
    > Original change's description:
    > > [settings] Add additional E2E tests for volume policy transforms
    > >
    > > These E2E tests verify that fuchsia.settings.policy.volume transforms
    > > affect the input and output of the fuchsia.settings.audio API's volume
    > > values.
    > >
    > > Bug: 60367
    > > Test: fx test setui_service_tests
    > > Change-Id: Ia90a74d178174d1e1bb8ca251c5bf0d2c6f4a978
    > > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/474318
    > > Reviewed-by: Paul Faria <paulfaria@google.com>
    > > Reviewed-by: Bryce Lee <brycelee@google.com>
    > > Commit-Queue: William Xiao <wxyz@google.com>
    >
    > TBR=brycelee@google.com,wxyz@google.com,paulfaria@google.com,setui-review@google.com
    >
    > Change-Id: I3f6c4a3c351853201fc8fdd21794312a314d6c7e
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 60367
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/475812
    > Reviewed-by: William Xiao <wxyz@google.com>
    > Commit-Queue: William Xiao <wxyz@google.com>
    
    TBR=brycelee@google.com,wxyz@google.com,paulfaria@google.com,setui-review@google.com
    
    Bug: 68411
    Bug: 60367
    Change-Id: I102705e46aea18b763b2a6e327bccc4e4790408a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/475814
    Reviewed-by: Chip Fukuhara <cfukuhara@google.com>
    Commit-Queue: William Xiao <wxyz@google.com>

--- garnet/bin/setui/src/tests/audio_policy_tests.rs
+++ garnet/bin/setui/src/tests/audio_policy_tests.rs
@@ -4,32 +4,117 @@
 
 use crate::audio::default_audio_info;
 use crate::audio::policy::{
-    Request as AudioRequest, Response as AudioResponse, StateBuilder, TransformFlags,
+    PropertyTarget, Request as AudioRequest, Response as AudioResponse, StateBuilder, Transform,
+    TransformFlags,
 };
-use crate::audio::types::AudioStreamType;
+use crate::audio::types::{AudioSettingSource, AudioStream, AudioStreamType};
 use crate::base::SettingType;
 use crate::handler::device_storage::testing::InMemoryStorageFactory;
 use crate::message::base::{Audience, MessengerType};
 use crate::policy::base::{response::Payload, Request};
+use crate::tests::fakes::audio_core_service;
+use crate::tests::fakes::service_registry::ServiceRegistry;
 use crate::{internal, EnvironmentBuilder};
+use fidl_fuchsia_settings::{self as audio_fidl, AudioMarker, AudioProxy};
 use fidl_fuchsia_settings_policy::{
-    self as fidl, VolumePolicyControllerMarker, VolumePolicyControllerProxy,
+    self as policy_fidl, VolumePolicyControllerMarker, VolumePolicyControllerProxy,
 };
+use fuchsia_component::server::NestedEnvironment;
 use std::collections::HashSet;
 
 const ENV_NAME: &str = "settings_service_privacy_test_environment";
 
+struct TestEnvironment {
+    /// The nested environment itself.
+    nested_environment: NestedEnvironment,
+
+    /// Handle to the volume policy service.
+    policy_service: VolumePolicyControllerProxy,
+
+    /// Handle to the setui audio service.
+    setui_audio_service: AudioProxy,
+}
+
 /// Creates an environment for audio policy.
-async fn create_test_environment() -> VolumePolicyControllerProxy {
+async fn create_test_environment() -> TestEnvironment {
     let storage_factory = InMemoryStorageFactory::create();
 
+    let service_registry = ServiceRegistry::create();
+
+    let audio_core_service_handle = audio_core_service::Builder::new().build();
+    service_registry.lock().await.register_service(audio_core_service_handle.clone());
+
     let env = EnvironmentBuilder::new(storage_factory)
+        .service(ServiceRegistry::serve(service_registry))
         .settings(&[SettingType::Audio])
         .spawn_and_get_nested_environment(ENV_NAME)
         .await
         .unwrap();
 
-    env.connect_to_service::<VolumePolicyControllerMarker>().unwrap()
+    let policy_service = env.connect_to_service::<VolumePolicyControllerMarker>().unwrap();
+    let setui_audio_service = env.connect_to_service::<AudioMarker>().unwrap();
+
+    TestEnvironment { nested_environment: env, policy_service, setui_audio_service }
+}
+
+/// Sets the stream volume to the given volume level using the audio service.
+async fn set_stream_volume(env: &TestEnvironment, stream: AudioStreamType, volume_level: f32) {
+    let mut audio_settings = audio_fidl::AudioSettings::EMPTY;
+    audio_settings.streams = Some(vec![AudioStream {
+        stream_type: stream,
+        source: AudioSettingSource::User,
+        user_volume_level: volume_level,
+        user_volume_muted: false,
+    }
+    .into()]);
+    env.setui_audio_service
+        .set(audio_settings)
+        .await
+        .expect("set call succeeded")
+        .expect("set succeeded");
+}
+
+/// Get the volume of the given stream from the audio service in the given test environment.
+///
+/// Panics for an unexpected result, such as the stream or volume level not being present.
+async fn get_stream_volume(env: &TestEnvironment, stream: AudioStreamType) -> f32 {
+    get_stream_volume_from_proxy(&env.setui_audio_service, stream).await
+}
+
+/// Get the volume of the given stream from the given audio service proxy.
+///
+/// Panics for an unexpected result, such as the stream or volume level not being present.
+async fn get_stream_volume_from_proxy(
+    setui_audio_service: &AudioProxy,
+    stream: AudioStreamType,
+) -> f32 {
+    let audio_settings = setui_audio_service.watch().await.expect("failed to watch audio settings");
+
+    let stream = audio_settings
+        .streams
+        .expect("no streams in audio settings")
+        .iter()
+        .find(|stream_settings| stream_settings.stream == Some(stream.into()))
+        .expect("failed to find audio stream")
+        .clone();
+
+    stream.user_volume.as_ref().expect("no user volume").level.expect("no volume level")
+}
+
+async fn add_policy(env: &TestEnvironment, target: PropertyTarget, transform: Transform) -> u32 {
+    env.policy_service
+        .add_policy(&mut policy_fidl::Target::Stream(target.into()), &mut transform.into())
+        .await
+        .expect("failed to add policy")
+        .expect("no policy ID found")
+}
+
+async fn remove_policy(env: &TestEnvironment, policy_id: u32) {
+    env.policy_service
+        .remove_policy(policy_id)
+        .await
+        .expect("failed to remove policy")
+        .expect("removal error");
 }
 
 // A simple validation test to ensure the policy message hub propagates messages
@@ -87,10 +172,10 @@ async fn test_policy_get_default() {
         .map(|stream| stream.stream_type)
         .collect::<HashSet<_>>();
 
-    let policy_service = create_test_environment().await;
+    let env = create_test_environment().await;
 
     // Attempt to read properties.
-    let properties = policy_service.get_properties().await.expect("failed to get properties");
+    let properties = env.policy_service.get_properties().await.expect("failed to get properties");
 
     // Verify that each expected stream type is contained in the property targets.
     assert_eq!(properties.len(), expected_stream_types.len());
@@ -98,7 +183,7 @@ async fn test_policy_get_default() {
         // Allow unreachable patterns so this test will still build if more Target enums are added.
         #[allow(unreachable_patterns)]
         match property.target.expect("no target found for property") {
-            fidl::Target::Stream(stream) => {
+            policy_fidl::Target::Stream(stream) => {
                 assert!(expected_stream_types.contains(&stream.into()));
             }
             _ => panic!("unexpected target type"),
@@ -111,40 +196,29 @@ async fn test_policy_get_default() {
 #[fuchsia_async::run_until_stalled(test)]
 async fn test_policy_add_policy() {
     let expected_policy_target = AudioStreamType::Media;
-    let expected_policy_parameters =
-        fidl::PolicyParameters::Disable(fidl::Disable { ..fidl::Disable::EMPTY });
+    let policy_transform = Transform::Disable;
 
-    let policy_service = create_test_environment().await;
+    let env = create_test_environment().await;
 
     // Add a policy property and save the returned policy ID.
-    let added_policy_id = policy_service
-        .add_policy(
-            &mut fidl::Target::Stream(expected_policy_target.into()),
-            &mut expected_policy_parameters.clone(),
-        )
-        .await
-        .expect("failed to add policy")
-        .unwrap();
+    let added_policy_id = add_policy(&env, expected_policy_target, policy_transform).await;
 
     // Read the policies.
-    let properties = policy_service.get_properties().await.expect("failed to get properties");
+    let properties = env.policy_service.get_properties().await.expect("failed to get properties");
 
     // Verify that the expected target has the given policy and that others are unchanged.
     for property in properties {
         // Allow unreachable patterns so this test will still build if more Target enums are added.
         #[allow(unreachable_patterns)]
         match property.target.expect("no target found for property") {
-            fidl::Target::Stream(stream) => {
+            policy_fidl::Target::Stream(stream) => {
                 if stream == expected_policy_target.into() {
                     // Chosen property should have a policy added.
                     let added_policies = property.active_policies.unwrap();
                     assert_eq!(added_policies.len(), 1);
                     let added_policy = added_policies.first().unwrap();
                     assert_eq!(added_policy.policy_id.unwrap(), added_policy_id);
-                    assert_eq!(
-                        *added_policy.parameters.as_ref().unwrap(),
-                        expected_policy_parameters
-                    );
+                    assert_eq!(*added_policy.parameters.as_ref().unwrap(), policy_transform.into());
                 } else {
                     // Other properties shouldn't have any policies.
                     assert!(property.active_policies.unwrap().is_empty());
@@ -160,34 +234,135 @@ async fn test_policy_add_policy() {
 #[fuchsia_async::run_until_stalled(test)]
 async fn test_policy_remove_policy() {
     let expected_policy_target = AudioStreamType::Media;
-    let expected_policy_parameters =
-        fidl::PolicyParameters::Disable(fidl::Disable { ..fidl::Disable::EMPTY });
 
-    let policy_service = create_test_environment().await;
+    let env = create_test_environment().await;
 
     // Add a policy property and save the returned policy ID.
-    let added_policy_id = policy_service
-        .add_policy(
-            &mut fidl::Target::Stream(expected_policy_target.into()),
-            &mut expected_policy_parameters.clone(),
-        )
-        .await
-        .expect("failed to add policy")
-        .unwrap();
+    let added_policy_id = add_policy(&env, expected_policy_target, Transform::Disable).await;
 
     // Remove the same policy using the returned ID.
-    policy_service
-        .remove_policy(added_policy_id)
-        .await
-        .expect("failed to remove policy")
-        .expect("removal error");
+    remove_policy(&env, added_policy_id).await;
 
     // Fetch the properties.
-    let properties = policy_service.get_properties().await.expect("failed to get properties");
+    let properties = env.policy_service.get_properties().await.expect("failed to get properties");
 
     // Verify that the expected target has the given policy and that others are unchanged.
     for property in properties {
         // No policies are active.
         assert!(property.active_policies.unwrap().is_empty());
     }
+}
+
+// Tests that adding a new min policy transform affects the audio output from the audio setting.
+#[fuchsia_async::run_until_stalled(test)]
+async fn test_policy_add_min_policy_adjusts_volume() {
+    let expected_policy_target = AudioStreamType::Media;
+    let expected_user_volume: f32 = 0.8;
+
+    let env = create_test_environment().await;
+
+    // Set media volume level to 0 to start with.
+    set_stream_volume(&env, expected_policy_target, 0.0).await;
+
+    // Add a min policy transform.
+    add_policy(&env, expected_policy_target, Transform::Min(expected_user_volume)).await;
+
+    assert_eq!(expected_user_volume, get_stream_volume(&env, expected_policy_target).await);
+}
+
+// Tests that adding a new max policy transform does not initially affect the audio output from the
+// audio setting when at max, but does take effect after removal.
+#[fuchsia_async::run_until_stalled(test)]
+async fn test_policy_add_max_policy_adjusts_volume_after_removal() {
+    let expected_policy_target = AudioStreamType::Media;
+    let initial_volume: f32 = 1.0;
+    let expected_user_volume: f32 = 0.8;
+
+    let env = create_test_environment().await;
+
+    // Set media volume level to max to start with.
+    set_stream_volume(&env, expected_policy_target, initial_volume).await;
+
+    // Add a max policy transform.
+    let added_policy_id =
+        add_policy(&env, expected_policy_target, Transform::Max(expected_user_volume)).await;
+
+    // Volume should stay at max from the point of view of the client.
+    assert_eq!(initial_volume, get_stream_volume(&env, expected_policy_target).await);
+
+    // Remove the added policy.
+    remove_policy(&env, added_policy_id).await;
+
+    // After the policy is removed, the volume now reflects its clamped value.
+    assert_eq!(expected_user_volume, get_stream_volume(&env, expected_policy_target).await);
+}
+
+// Tests that incoming sets are clamped when a min policy is present.
+#[fuchsia_async::run_until_stalled(test)]
+async fn test_policy_min_policy_clamps_sets() {
+    let expected_policy_target = AudioStreamType::Media;
+    let expected_user_volume: f32 = 0.2;
+
+    let env = create_test_environment().await;
+
+    // Set media volume level to 50% to start with.
+    set_stream_volume(&env, expected_policy_target, 0.5).await;
+
+    // Add a min policy transform.
+    let policy_id =
+        add_policy(&env, expected_policy_target, Transform::Min(expected_user_volume)).await;
+
+    // Attempt to set the volume to 0.
+    set_stream_volume(&env, expected_policy_target, 0.0).await;
+
+    // The volume remains at 20%, the minimum set by policy.
+    assert_eq!(expected_user_volume, get_stream_volume(&env, expected_policy_target).await);
+
+    // Remove the min volume policy.
+    remove_policy(&env, policy_id).await;
+
+    let audio_connection = env.nested_environment.connect_to_service::<AudioMarker>().unwrap();
+
+    // The volume remains at 20% after the policy is removed.
+    assert_eq!(
+        expected_user_volume,
+        get_stream_volume_from_proxy(&audio_connection, expected_policy_target).await
+    );
+}
+
+// Tests that incoming sets are scaled when a max policy is present.
+#[fuchsia_async::run_until_stalled(test)]
+async fn test_policy_max_policy_scales_sets() {
+    let expected_policy_target = AudioStreamType::Media;
+    let initial_volume: f32 = 0.4;
+    let max_volume_limit: f32 = 0.8;
+    let max_volume: f32 = 1.0;
+
+    let env = create_test_environment().await;
+
+    // Set media volume level to 40% to start with.
+    set_stream_volume(&env, expected_policy_target, initial_volume).await;
+
+    // Add a max policy transform.
+    let policy_id =
+        add_policy(&env, expected_policy_target, Transform::Max(max_volume_limit)).await;
+
+    // The volume is 50% when retrieved, since the initial 40% is half of the max of 80%.
+    assert_eq!(
+        initial_volume / max_volume_limit,
+        get_stream_volume(&env, expected_policy_target).await
+    );
+
+    // Attempt to set the volume to max.
+    set_stream_volume(&env, expected_policy_target, max_volume).await;
+
+    // The volume is 100% when retrieved since the max policy limit is transparent to clients.
+    assert_eq!(max_volume, get_stream_volume(&env, expected_policy_target).await);
+
+    // Remove the max volume policy.
+    remove_policy(&env, policy_id).await;
+
+    // The volume is 80% when retrieved after the policy is removed, since the internal volume was
+    // 80% due to the policy.
+    assert_eq!(max_volume_limit, get_stream_volume(&env, expected_policy_target).await);
 }
