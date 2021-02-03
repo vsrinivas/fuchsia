801358d7372a22a28120a33ce0b9aca43925973a
Brian Bosak <bbosak@google.com>
2021-02-03 04:42:28 +0000

    [logs] Add a fuzzer for the legacy logging library
    This patch adds a fuzzer for the legacy fx-logger library.
    
    Change-Id: I4429f36587587f040cea4312d5efe5ca1805c8ed
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/480081
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>

---
+++ src/devices/testing/fx-logger/fuzz.cc
@@ -0,0 +1,33 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/syslog/cpp/log_settings.h>
+#include <lib/syslog/cpp/logging_backend.h>
+#include <lib/syslog/cpp/logging_backend_fuchsia_private.h>
+#include <lib/syslog/cpp/macros.h>
+#include <lib/syslog/global.h>
+#include <lib/syslog/logger.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <sstream>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+// use -f to get printf output from this test.
+
+// Parses an input stream from libFuzzer and executes arbitrary
+// logging commands to fuzz the structured logging backend.
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider provider(data, size);
+  std::string str = provider.ConsumeRandomLengthString();
+  std::string tag = provider.ConsumeRandomLengthString();
+  FX_LOGF(DEBUG, tag.c_str(), "%s%d%d%lu", str.c_str(), provider.ConsumeIntegral<int>(),
+          provider.ConsumeIntegral<int>(), provider.ConsumeIntegral<uint64_t>());
+  return 0;
+}
