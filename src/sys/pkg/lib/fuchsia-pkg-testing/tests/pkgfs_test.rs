de59a2f4ac03cd0e16a14a9b65a46fd264d45c32
Olivia <nolivia@google.com>
2021-02-03 19:53:22 +0000

    [pkgfs] Reorganizes pkgfs_test to make the garbage collection tests easier to find.
    Fixed: 66887
    
    Change-Id: I624e95445f5c9ae3b1859880b350376c35e08fb3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/477278
    Commit-Queue: Olivia Nordquist <nolivia@google.com>
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>

--- src/sys/pkg/lib/fuchsia-pkg-testing/tests/pkgfs_test.rs
+++ src/sys/pkg/lib/fuchsia-pkg-testing/tests/pkgfs_test.rs
@@ -4,6 +4,7 @@
 
 #![cfg(test)]
 use {
+    crate::*,
     blobfs_ramdisk::BlobfsRamdisk,
     fuchsia_async as fasync,
     fuchsia_merkle::Hash,
@@ -13,113 +14,13 @@ use {
     pkgfs::package::OpenRights,
     pkgfs_ramdisk::PkgfsRamdisk,
     std::{
-        collections::HashSet,
         fmt::Debug,
         fs,
-        future::Future,
         io::{self, Read, Write},
     },
 };
 
-mod get_buffer;
-
-fn ls_simple(d: openat::DirIter) -> Result<Vec<String>, io::Error> {
-    Ok(d.map(|i| i.map(|entry| entry.file_name().to_string_lossy().into()))
-        .collect::<Result<Vec<_>, _>>()?)
-}
-
-trait AsRootDir {
-    fn as_root_dir(&self) -> openat::Dir;
-}
-
-impl AsRootDir for PkgfsRamdisk {
-    fn as_root_dir(&self) -> openat::Dir {
-        self.root_dir().expect("getting pkgfs root dir")
-    }
-}
-
-fn ls(root: &dyn AsRootDir, path: impl openat::AsPath) -> Result<Vec<String>, io::Error> {
-    let d = root.as_root_dir();
-    ls_simple(d.list_dir(path)?)
-}
-
-/// Allows us to call sort inline
-fn sorted<T: Ord>(mut vec: Vec<T>) -> Vec<T> {
-    vec.sort();
-    vec
-}
-
-fn verify_contents<'a>(
-    pkg: &'a Package,
-    dir: fidl_fuchsia_io::DirectoryProxy,
-) -> impl Future<Output = Result<(), VerificationError>> + 'a {
-    async move { pkg.verify_contents(&dir).await }
-}
-
-fn subdir_proxy(d: &openat::Dir, path: &str) -> fidl_fuchsia_io::DirectoryProxy {
-    let handle = fdio::transfer_fd(
-        d.open_file(path).unwrap_or_else(|e| panic!("opening {}: {:?}", path, e)),
-    )
-    .unwrap();
-    fidl_fuchsia_io::DirectoryProxy::new(
-        fuchsia_async::Channel::from_channel(handle.into()).unwrap(),
-    )
-}
-
-fn copy_file_with_len(
-    d: &openat::Dir,
-    path: &std::path::Path,
-    source: &mut std::fs::File,
-) -> Result<(), anyhow::Error> {
-    use std::convert::TryInto;
-    let mut bytes = vec![];
-    source.read_to_end(&mut bytes)?;
-    let mut file = d.write_file(path, 0777)?;
-    file.set_len(bytes.len().try_into().unwrap())?;
-    file.write_all(&bytes)?;
-    Ok(())
-}
-
-fn install(pkgfs: &PkgfsRamdisk, pkg: &Package) {
-    let d = pkgfs.root_dir().expect("getting pkgfs root dir");
-
-    copy_file_with_len(
-        &d,
-        &std::path::Path::new(&format!("install/pkg/{}", pkg.meta_far_merkle_root())),
-        &mut pkg.meta_far().unwrap(),
-    )
-    .unwrap();
-
-    let mut needs: HashSet<String> =
-        ls_simple(d.list_dir(format!("needs/packages/{}", pkg.meta_far_merkle_root())).unwrap())
-            .unwrap()
-            .into_iter()
-            .collect();
-
-    for mut content in pkg.content_blob_files() {
-        let merkle = content.merkle.to_string();
-        if needs.contains(&merkle) {
-            copy_file_with_len(
-                &d,
-                &std::path::Path::new("install/blob").join(&merkle),
-                &mut content.file,
-            )
-            .unwrap_or_else(|e| panic!("error writing {}: {:?}", merkle, e));
-            needs.remove(&merkle);
-        }
-    }
-
-    // Quick sanity check that we actually installed the package.
-    let mut file_contents = String::new();
-    d.open_file(format!("versions/{}/meta", pkg.meta_far_merkle_root()))
-        .unwrap()
-        .read_to_string(&mut file_contents)
-        .unwrap();
-
-    assert_eq!(file_contents, format!("{}", pkg.meta_far_merkle_root()))
-}
-
-/// Helper function implementing the logic for the asser_error_kind! macro
+/// Helper function implementing the logic for the assert_error_kind! macro
 ///
 /// Returns Ok(io:Error) if the kind matches, otherwise returns Err(String) with the panic message.
 fn assert_error_kind_helper<T: Debug>(
@@ -130,16 +31,16 @@ fn assert_error_kind_helper<T: Debug>(
     match result {
         Ok(val) => Err(format!(
             r"assertion failed: `{}.is_err()`
-   result: `Ok({:?})`,
- expected: `Err({{ kind: {:?}, .. }})`",
+            result: `Ok({:?})`,
+            expected: `Err({{ kind: {:?}, .. }})`",
             result_expr, val, expected
         )),
         Err(err) if err.kind() == expected => Ok(err),
         Err(err) => Err(format!(
             r"assertion failed: `{expr}.unwrap_err().kind() == {expected:?}`
- err.kind(): `{kind:?}`,
-   expected: `{expected:?}`
-   full err: `{full:?}`",
+            err.kind(): `{kind:?}`,
+            expected: `{expected:?}`
+            full err: `{full:?}`",
             expr = result_expr,
             expected = expected,
             kind = err.kind(),
@@ -156,14 +57,6 @@ macro_rules! assert_error_kind {
     ($result:expr, $expected:expr,) => {{
         assert_error_kind!($result, $expected)
     }};
-}
-
-async fn example_package() -> Package {
-    PackageBuilder::new("example")
-        .add_resource_at("a/b", "Hello world!\n".as_bytes())
-        .build()
-        .await
-        .expect("build package")
 }
 
 #[fasync::run_singlethreaded(test)]
@@ -374,7 +267,7 @@ async fn test_pkgfs_restart_install() {
         io::ErrorKind::NotFound
     );
 
-    // Retry installing meta.far (fails with EEXIST)
+    // Retry installing meta.far (fails with AlreadyExists)
     assert_error_kind!(
         d.new_file(
             "install/pkg/b5690901cd8664a742eb0a7d2a068eb0d4ff49c10a615cfa4c0044dd2eaccd93",
@@ -856,94 +749,6 @@ async fn test_pkgfs_packages_doesnt_show_base_package_updates() {
 }
 
 #[fasync::run_singlethreaded(test)]
-async fn test_pkgfs_install_update() {
-    // GC doesn't work without a working system image
-    let system_image_package =
-        SystemImageBuilder::new().pkgfs_non_static_packages_allowlist(&["example"]).build().await;
-
-    let blobfs = BlobfsRamdisk::start().unwrap();
-    system_image_package.write_to_blobfs_dir(&blobfs.root_dir().unwrap());
-    let pkgfs = PkgfsRamdisk::builder()
-        .blobfs(blobfs)
-        .system_image_merkle(system_image_package.meta_far_merkle_root())
-        .start()
-        .unwrap();
-
-    let d = pkgfs.root_dir().expect("getting pkgfs root dir");
-
-    let pkg = example_package().await;
-    install(&pkgfs, &pkg);
-
-    assert_eq!(ls_simple(d.list_dir("packages/example").unwrap()).unwrap(), ["0"]);
-    verify_contents(&pkg, subdir_proxy(&d, "packages/example/0"))
-        .await
-        .expect("valid example package");
-
-    let pkg2 = PackageBuilder::new("example")
-        .add_resource_at("a/b", "Hello world 2!\n".as_bytes())
-        .build()
-        .await
-        .expect("build package");
-    install(&pkgfs, &pkg2);
-
-    assert_eq!(sorted(ls(&pkgfs, "packages").unwrap()), ["example", "system_image"]);
-    assert_eq!(ls_simple(d.list_dir("packages/example").unwrap()).unwrap(), ["0"]);
-    verify_contents(&pkg2, subdir_proxy(&d, "packages/example/0"))
-        .await
-        .expect("pkg2 replaced pkg");
-
-    assert_eq!(
-        sorted(ls(&pkgfs, "versions").unwrap()),
-        sorted(vec![
-            pkg2.meta_far_merkle_root().to_string(),
-            system_image_package.meta_far_merkle_root().to_string()
-        ])
-    );
-
-    // old version is no longer accesible.
-    assert_error_kind!(
-        d.metadata(&format!("versions/{}", pkg.meta_far_merkle_root())).map(|m| m.is_dir()),
-        io::ErrorKind::NotFound
-    );
-
-    {
-        let blobfs_dir = pkgfs.blobfs().root_dir().unwrap();
-
-        // Old blobs still in blobfs.
-        let expected_blobs = sorted(
-            pkg.list_blobs()
-                .unwrap()
-                .into_iter()
-                .chain(pkg2.list_blobs().unwrap())
-                .chain(system_image_package.list_blobs().unwrap())
-                .map(|m| m.to_string())
-                .collect(),
-        );
-        assert_eq!(sorted(ls_simple(blobfs_dir.list_dir(".").unwrap()).unwrap()), expected_blobs);
-
-        // Trigger GC
-        d.remove_dir("ctl/garbage").unwrap();
-
-        // pkg blobs are in blobfs no longer
-        let expected_blobs = sorted(
-            pkg2.list_blobs()
-                .unwrap()
-                .into_iter()
-                .chain(system_image_package.list_blobs().unwrap())
-                .map(|m| m.to_string())
-                .collect(),
-        );
-        let got_blobs = sorted(ls_simple(blobfs_dir.list_dir(".").unwrap()).unwrap());
-
-        assert_eq!(got_blobs, expected_blobs);
-    }
-
-    drop(d);
-
-    pkgfs.stop().await.expect("stopping pkgfs");
-}
-
-#[fasync::run_singlethreaded(test)]
 async fn test_pkgfs_restart_deactivates_ephemeral_packages() {
     let pkgfs = PkgfsRamdisk::builder()
         .enforce_packages_non_static_allowlist(false) // turn off allowlist enforcement
@@ -1083,95 +888,6 @@ async fn test_pkgfs_with_cache_index_missing_cache_content_blob() {
     );
 
     assert_eq!(ls_simple(d.list_dir("needs/packages").unwrap()).unwrap(), Vec::<&str>::new());
-
-    drop(d);
-
-    pkgfs.stop().await.expect("stopping pkgfs");
-}
-
-#[fasync::run_singlethreaded(test)]
-async fn test_pkgfs_shadowed_cache_package() {
-    let pkg = example_package().await;
-    let system_image_package = SystemImageBuilder::new()
-        .cache_packages(&[&pkg])
-        .pkgfs_non_static_packages_allowlist(&["example"])
-        .build()
-        .await;
-
-    let blobfs = BlobfsRamdisk::start().unwrap();
-    system_image_package.write_to_blobfs_dir(&blobfs.root_dir().unwrap());
-    pkg.write_to_blobfs_dir(&blobfs.root_dir().unwrap());
-    let pkgfs = PkgfsRamdisk::builder()
-        .blobfs(blobfs)
-        .system_image_merkle(system_image_package.meta_far_merkle_root())
-        .start()
-        .unwrap();
-
-    let d = pkgfs.root_dir().expect("getting pkgfs root dir");
-
-    assert_eq!(ls_simple(d.list_dir("packages/example").unwrap()).unwrap(), ["0"]);
-    verify_contents(&pkg, subdir_proxy(&d, "packages/example/0"))
-        .await
-        .expect("valid example package");
-
-    let pkg2 = PackageBuilder::new("example")
-        .add_resource_at("a/b", "Hello world 2!\n".as_bytes())
-        .build()
-        .await
-        .expect("build package");
-    install(&pkgfs, &pkg2);
-
-    assert_eq!(sorted(ls(&pkgfs, "packages").unwrap()), ["example", "system_image"]);
-    assert_eq!(ls_simple(d.list_dir("packages/example").unwrap()).unwrap(), ["0"]);
-    verify_contents(&pkg2, subdir_proxy(&d, "packages/example/0"))
-        .await
-        .expect("pkg2 replaced pkg");
-
-    assert_eq!(
-        sorted(ls(&pkgfs, "versions").unwrap()),
-        sorted(vec![
-            pkg2.meta_far_merkle_root().to_string(),
-            system_image_package.meta_far_merkle_root().to_string()
-        ])
-    );
-
-    // cached version is no longer accesible.
-    assert_error_kind!(
-        d.metadata(&format!("versions/{}", pkg.meta_far_merkle_root())).map(|m| m.is_dir()),
-        io::ErrorKind::NotFound
-    );
-
-    {
-        let blobfs_dir = pkgfs.blobfs().root_dir().unwrap();
-
-        // Old blobs still in blobfs.
-        let expected_blobs = sorted(
-            pkg.list_blobs()
-                .unwrap()
-                .into_iter()
-                .chain(pkg2.list_blobs().unwrap())
-                .chain(system_image_package.list_blobs().unwrap())
-                .map(|m| m.to_string())
-                .collect(),
-        );
-        assert_eq!(sorted(ls_simple(blobfs_dir.list_dir(".").unwrap()).unwrap()), expected_blobs);
-
-        // Trigger GC
-        d.remove_dir("ctl/garbage").unwrap();
-
-        // cached pkg blobs are in blobfs no longer
-        let expected_blobs = sorted(
-            pkg2.list_blobs()
-                .unwrap()
-                .into_iter()
-                .chain(system_image_package.list_blobs().unwrap())
-                .map(|m| m.to_string())
-                .collect(),
-        );
-        let got_blobs = sorted(ls_simple(blobfs_dir.list_dir(".").unwrap()).unwrap());
-
-        assert_eq!(got_blobs, expected_blobs);
-    }
 
     drop(d);
 
