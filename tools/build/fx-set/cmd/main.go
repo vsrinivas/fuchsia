9221647f63a35ed9e99f71a0437e1709f40946da
Oliver Newman <olivernewman@google.com>
2021-02-03 22:52:09 +0000

    [fx] Add basic flags to fx-set command
    These are the flags necessary for running GN gen with all supported
    args. They should be fully backwards-compatible with the corresponding
    flags in the old version of `fx set`.
    
    Don't do anything with the flags yet to keep this CL relatively small.
    I'll start actually using them in a follow-up.
    
    Bug: 68465
    Change-Id: I6cedc84ee5fdc517913f96a15f288e51cdacef5c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/480079
    Commit-Queue: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Gary Boone <gboone@google.com>

--- tools/build/fx-set/cmd/main.go
+++ tools/build/fx-set/cmd/main.go
@@ -5,9 +5,110 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	flag "github.com/spf13/pflag"
+	"go.fuchsia.dev/fuchsia/tools/lib/color"
+	"go.fuchsia.dev/fuchsia/tools/lib/command"
+	"go.fuchsia.dev/fuchsia/tools/lib/logger"
+)
+
+const (
+	// fx ensures that this env var is set.
+	checkoutDirEnvVar = "FUCHSIA_DIR"
 )
 
 func main() {
-	fmt.Println("fint_set experiment enabled")
+	ctx := command.CancelOnSignals(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+
+	l := logger.NewLogger(logger.ErrorLevel, color.NewColor(color.ColorAuto), os.Stdout, os.Stderr, "")
+	// Don't include timestamps or other metadata in logs, since this tool is
+	// only intended to be run on developer workstations.
+	l.SetFlags(0)
+	ctx = logger.WithLogger(ctx, l)
+
+	if err := mainImpl(ctx); err != nil {
+		if ctx.Err() == nil {
+			logger.Errorf(ctx, err.Error())
+		}
+		os.Exit(1)
+	}
+}
+
+func mainImpl(ctx context.Context) error {
+	checkoutDir := os.Getenv(checkoutDirEnvVar)
+	if checkoutDir == "" {
+		return fmt.Errorf("%s env var must be set", checkoutDirEnvVar)
+	}
+
+	args, err := parseArgs(os.Args[1:])
+	if err != nil {
+		return err
+	}
+
+	if args.verbose {
+		if l := logger.LoggerFromContext(ctx); l != nil {
+			l.LoggerLevel = logger.DebugLevel
+		}
+	}
+
+	return nil
+}
+
+type setArgs struct {
+	verbose bool
+
+	// Flags passed to GN.
+	board            string
+	product          string
+	isRelease        bool
+	universePackages []string
+	basePackages     []string
+	cachePackages    []string
+	hostLabels       []string
+	variants         []string
+	gnArgs           []string
+}
+
+func parseArgs(args []string) (*setArgs, error) {
+	cmd := &setArgs{}
+
+	flagSet := flag.NewFlagSet("fx set", flag.ExitOnError)
+
+	// Help strings don't matter because `fx set -h` uses the help text from
+	// //tools/devshell/set, which should be kept up to date with these flags.
+	flagSet.BoolVar(&cmd.verbose, "verbose", false, "")
+	flagSet.BoolVar(&cmd.isRelease, "release", false, "")
+	flagSet.StringSliceVar(&cmd.universePackages, "with", []string{}, "")
+	flagSet.StringSliceVar(&cmd.basePackages, "with-base", []string{}, "")
+	flagSet.StringSliceVar(&cmd.cachePackages, "with-cache", []string{}, "")
+	flagSet.StringSliceVar(&cmd.hostLabels, "with-host", []string{}, "")
+	flagSet.StringSliceVar(&cmd.variants, "variant", []string{}, "")
+	// Unlike StringSliceVar, StringArrayVar doesn't split flag values at
+	// commas. Commas are syntactically significant in GN, so they should be
+	// preserved rather than interpreting them as value separators.
+	flagSet.StringArrayVar(&cmd.gnArgs, "args", []string{}, "")
+
+	if err := flagSet.Parse(args); err != nil {
+		return nil, err
+	}
+
+	if flagSet.NArg() == 0 {
+		return nil, fmt.Errorf("missing a PRODUCT.BOARD argument")
+	} else if flagSet.NArg() > 1 {
+		return nil, fmt.Errorf("only one positional PRODUCT.BOARD argument allowed")
+	}
+
+	productDotBoard := flagSet.Arg(0)
+	productAndBoard := strings.Split(productDotBoard, ".")
+	if len(productAndBoard) != 2 {
+		return nil, fmt.Errorf("unable to parse PRODUCT.BOARD: %q", productDotBoard)
+	}
+	cmd.product, cmd.board = productAndBoard[0], productAndBoard[1]
+
+	return cmd, nil
 }
--- tools/build/fx-set/cmd/main_test.go
+++ tools/build/fx-set/cmd/main_test.go
@@ -3,3 +3,136 @@
 // found in the LICENSE file.
 
 package main
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func TestParseArgs(t *testing.T) {
+	testCases := []struct {
+		name      string
+		args      []string
+		expected  setArgs
+		expectErr bool
+	}{
+		{
+			name:      "missing PRODUCT.BOARD",
+			args:      []string{},
+			expectErr: true,
+		},
+		{
+			name: "valid PRODUCT.BOARD",
+			args: []string{"core.x64"},
+			expected: setArgs{
+				product: "core",
+				board:   "x64",
+			},
+		},
+		{
+			name:      "invalid PRODUCT.BOARD",
+			args:      []string{"corex64"},
+			expectErr: true,
+		},
+		{
+			name:      "multiple PRODUCT.BOARD args",
+			args:      []string{"core.x64", "bringup.arm64"},
+			expectErr: true,
+		},
+		{
+			name: "verbose",
+			args: []string{"--verbose", "core.x64"},
+			expected: setArgs{
+				product: "core",
+				board:   "x64",
+				verbose: true,
+			},
+		},
+		{
+			name: "universe packages",
+			args: []string{"core.x64", "--with", "u1,u2", "--with", "u3,u4"},
+			expected: setArgs{
+				product:          "core",
+				board:            "x64",
+				universePackages: []string{"u1", "u2", "u3", "u4"},
+			},
+		},
+		{
+			name: "base packages",
+			args: []string{"core.x64", "--with-base", "b1,b2", "--with-base", "b3,b4"},
+			expected: setArgs{
+				product:      "core",
+				board:        "x64",
+				basePackages: []string{"b1", "b2", "b3", "b4"},
+			},
+		},
+		{
+			name: "cache packages",
+			args: []string{"core.x64", "--with-cache", "c1,c2", "--with-cache", "c3,c4"},
+			expected: setArgs{
+				product:       "core",
+				board:         "x64",
+				cachePackages: []string{"c1", "c2", "c3", "c4"},
+			},
+		},
+		{
+			name: "host labels",
+			args: []string{"core.x64", "--with-host", "h1,h2", "--with-host", "h3,h4"},
+			expected: setArgs{
+				product:    "core",
+				board:      "x64",
+				hostLabels: []string{"h1", "h2", "h3", "h4"},
+			},
+		},
+		{
+			name: "variants",
+			args: []string{"core.x64", "--variant", "kasan,profile", "--variant", "ubsan"},
+			expected: setArgs{
+				product:  "core",
+				board:    "x64",
+				variants: []string{"kasan", "profile", "ubsan"},
+			},
+		},
+		{
+			name: "gn args",
+			args: []string{"core.x64", "--args", `foo=["bar", "baz"]`, "--args", "x=5"},
+			expected: setArgs{
+				product: "core",
+				board:   "x64",
+				// --args values shouldn't be split at commas, since commas can
+				// be part of the args themselves.
+				gnArgs: []string{`foo=["bar", "baz"]`, "x=5"},
+			},
+		},
+		{
+			name: "release",
+			args: []string{"core.x64", "--release"},
+			expected: setArgs{
+				product:   "core",
+				board:     "x64",
+				isRelease: true,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cmd, err := parseArgs(tc.args)
+			if err != nil {
+				if !tc.expectErr {
+					t.Fatalf("Parse args error: %s", err)
+				}
+				return
+			} else if tc.expectErr {
+				t.Fatalf("Expected parse args error but parsing succeeded")
+			}
+
+			opts := []cmp.Option{cmpopts.EquateEmpty(), cmp.AllowUnexported(setArgs{})}
+			if diff := cmp.Diff(&tc.expected, cmd, opts...); diff != "" {
+				t.Fatalf("Unexpected arg parse result (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
