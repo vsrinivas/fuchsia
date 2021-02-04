850c8d35cd8b8b1bf75e910f24417ccb75b4471d
Jay Zhuang <jayzhuang@google.com>
2021-02-04 17:39:56 +0000

    [build][images] Address non-hermetic actions
    The main fix is making manifest_content_expand.sh write a depfile.
    Also marked a few suppressions with bugs while I'm here.
    
    Change-Id: I00858c6a9926fa7d81f09e653f7364daeb489860
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/480880
    Reviewed-by: David Fang <fangism@google.com>
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>

--- build/images/manifest_content_expand.sh
+++ build/images/manifest_content_expand.sh
@@ -3,27 +3,43 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# manifest_content_expand.sh input-file output-file
+# manifest_content_expand.sh input-file output-file depfile
 #
 # Split manifest lines, replacing the source path with the contents of the file
 # at source path. This is used to replace .merkle files with their merkleroot
 # values to produce merkleroot indices, such as for pkgfs.
 #
-# example input:
+# example input line:
 # sshd-host/0=obj/garnet/bin/sshd-host/sshd-host.meta/meta.far.merkle
-# example output:
+# example output line:
 # sshd-host/0=cdd425612a8e968e849b9906335db74927bdfbfa3ecaafdcd2bf6585c2dfe284
+# merkle path will be added to depfile:
+# output-file: obj/garnet/bin/sshd-host/sshd-host.meta/meta.far.merkle
 
-set -e
+set -eu
 
 readonly input="$1"
 readonly output="$2"
+readonly depfile="$3"
 
-while read line
-do
+merkles="$(cat "${input}")"
+readonly merkles
+
+for line in $merkles; do
   if [ -n "${line}" ]; then
-    left="${line%%=*}"
-    right=$(cat "${line#*=}")
-    printf "%s=%s\n" "$left" "$right"
+    pkg="${line%%=*}"
+    merkle_path="${line#*=}"
+    merkle_root="$(cat "${merkle_path}")"
+    printf "%s=%s\n" "${pkg}" "${merkle_root}"
   fi
-done < "${input}" > "${output}"
+done > "${output}"
+
+{
+  printf "%s:" "${output}"
+  for line in $merkles; do
+    if [ -n "${line}" ]; then
+      merkle_path="${line#*=}"
+      printf " %s" "${merkle_path}"
+    fi
+  done
+} > "${depfile}"
