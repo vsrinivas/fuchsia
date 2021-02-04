38b29c1085ae54597c721c262ea6bd95d9a00360
Marie Janssen <jamuraa@google.com>
2021-02-04 18:39:14 +0000

    [bt][a2dp] Fix peers racing discovery to streaming
    When we attempt to start streaming, the discovery and capability
    collection process can take long enough that the peer starts
    their own connection process after we have started collecting.
    
    Previously we would attempt to start streaming and fail in this case,
    after this change we will not attempt to start.
    
    Bug: 67408
    Test: added streaming_start_configure_while_discovery
    
    Change-Id: If1cc4d42ef5b49d9635c270337e4e220874235f7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/480219
    Commit-Queue: Marie Janssen 💖 <jamuraa@google.com>
    Fuchsia-Auto-Submit: Marie Janssen 💖 <jamuraa@google.com>
    Reviewed-by: Jeff Belgum <belgum@google.com>

--- src/connectivity/bluetooth/profiles/bt-a2dp/src/sink_task.rs
+++ src/connectivity/bluetooth/profiles/bt-a2dp/src/sink_task.rs
@@ -153,7 +153,9 @@ impl MediaTaskRunner for ConfiguredSinkTask {
             let session_id = session_id_fut.await;
             media_stream_task(
                 stream,
-                Box::new(move || player::Player::new(session_id, codec_config.clone(), audio_factory.clone())),
+                Box::new(move || {
+                    player::Player::new(session_id, codec_config.clone(), audio_factory.clone())
+                }),
                 stream_inspect,
             )
             .await
@@ -405,7 +407,9 @@ mod tests {
 
         // Should try to start the player
         match exec.run_until_stalled(&mut audio_factory_requests.next()) {
-            Poll::Ready(Some(Ok(media::SessionAudioConsumerFactoryRequest::CreateAudioConsumer{ .. }))) => {},
+            Poll::Ready(Some(Ok(
+                media::SessionAudioConsumerFactoryRequest::CreateAudioConsumer { .. },
+            ))) => {}
             x => panic!("Expected a audio consumer request, got {:?}", x),
         };
     }
