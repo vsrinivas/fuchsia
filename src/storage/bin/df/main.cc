19f8761d79a853bbb45fd4f7157026deca3ed528
Scott Graham <scottmg@google.com>
2021-02-04 23:28:44 +0000

    [llcpp][df] Update df to use llcpp for io bindings
    Remove use of deprecated C bindings. No intended change in
    functionality. The "Device" retrieval doesn't currently work well (due
    to fxbug.dev/45697), but isn't further regressed.
    
    Test: `df /` before and after
    Test: `df /data` before and after
    Test: `df /blob` before and after
    Bug: 68648
    Bug: 45697
    Change-Id: Ice7f39eb0054cdeebe230fef5e60139e31716900
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/480677
    Commit-Queue: Scott Graham <scottmg@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>

--- src/storage/bin/df/main.cc
+++ src/storage/bin/df/main.cc
@@ -5,7 +5,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <fuchsia/io/c/fidl.h>
+#include <fuchsia/io/llcpp/fidl.h>
 #include <lib/fdio/cpp/caller.h>
 #include <stdalign.h>
 #include <stdarg.h>
@@ -17,9 +17,12 @@
 #include <unistd.h>
 #include <zircon/device/vfs.h>
 
+#include <string>
 #include <utility>
 
 #include <fbl/unique_fd.h>
+
+namespace fio = llcpp::fuchsia::io;
 
 int usage(void) {
   fprintf(stderr, "usage: df [ <option>* ] [paths]\n");
@@ -102,15 +105,15 @@ void print_human_readable(int padding, size_t size) {
   }
 }
 
-void print_fs_type(const char* name, const df_options_t* options,
-                   const fuchsia_io_FilesystemInfo* info, const char* device_path) {
+void print_fs_type(const char* name, const df_options_t* options, const fio::FilesystemInfo* info,
+                   const char* device_path) {
   if (options->node_usage) {
     size_t nodes_total = info ? info->total_nodes : 0;
     size_t nodes_used = info ? info->used_nodes : 0;
     size_t nodes_available = nodes_total - nodes_used;
     size_t use_percentage = nodes_total ? nodes_used * 100 / nodes_total : 0;
-    printf(ffmt, info != nullptr ? reinterpret_cast<const char*>(info->name) : "?", nodes_total,
-           nodes_used, nodes_available, use_percentage, name, device_path ? device_path : "none");
+    printf(ffmt, info != nullptr ? reinterpret_cast<const char*>(info->name.data()) : "?",
+           nodes_total, nodes_used, nodes_available, use_percentage, name, device_path);
   } else {
     // Block Usage
     if (options->human_readable) {
@@ -118,21 +121,20 @@ void print_fs_type(const char* name, const df_options_t* options,
       size_t bytes_used = info ? info->used_bytes : 0;
       size_t bytes_available = bytes_total - bytes_used;
       size_t use_percentage = bytes_total ? bytes_used * 100 / bytes_total : 0;
-      printf("%-10s ", info != nullptr ? reinterpret_cast<const char*>(info->name) : "?");
+      printf("%-10s ", info != nullptr ? reinterpret_cast<const char*>(info->name.data()) : "?");
       print_human_readable(5, bytes_total);
       print_human_readable(5, bytes_used);
       print_human_readable(5, bytes_available);
       printf("%5zu%%  ", use_percentage);
       printf("%-10s  ", name);
-      printf("%-10s\n", device_path ? device_path : "none");
+      printf("%-10s\n", device_path);
     } else {
       size_t blocks_total = info ? info->total_bytes >> 10 : 0;
       size_t blocks_used = info ? info->used_bytes >> 10 : 0;
       size_t blocks_available = blocks_total - blocks_used;
       size_t use_percentage = blocks_total ? blocks_used * 100 / blocks_total : 0;
-      printf(ffmt, info != nullptr ? reinterpret_cast<const char*>(info->name) : "?", blocks_total,
-             blocks_used, blocks_available, use_percentage, name,
-             device_path ? device_path : "none");
+      printf(ffmt, info != nullptr ? reinterpret_cast<const char*>(info->name.data()) : "?",
+             blocks_total, blocks_used, blocks_available, use_percentage, name, device_path);
     }
   }
 }
@@ -172,33 +174,29 @@ int main(int argc, const char** argv) {
       admin = false;
     }
 
-    fuchsia_io_FilesystemInfo info;
-    zx_status_t status;
+    fio::FilesystemInfo info;
     fdio_cpp::FdioCaller caller(std::move(fd));
-    zx_status_t io_status =
-        fuchsia_io_DirectoryAdminQueryFilesystem(caller.borrow_channel(), &status, &info);
-    if (io_status != ZX_OK || status != ZX_OK) {
+    auto result = fio::DirectoryAdmin::Call::QueryFilesystem(
+        fidl::UnownedClientEnd<fio::DirectoryAdmin>(caller.borrow_channel()));
+    if (!result.ok() || result->s != ZX_OK) {
       print_fs_type(dirs[i], &options, nullptr, "Unknown; cannot query filesystem");
       continue;
     }
-    info.name[fuchsia_io_MAX_FS_NAME_BUFFER - 1] = '\0';
+    info = *result->info;
+    info.name[fio::MAX_FS_NAME_BUFFER - 1] = '\0';
 
-    char device_buffer[1024];
-    char* device_path = static_cast<char*>(device_buffer);
-    size_t path_len;
-    io_status = fuchsia_io_DirectoryAdminGetDevicePath(
-        caller.borrow_channel(), &status, device_path, sizeof(device_buffer) - 1, &path_len);
-    const char* path = nullptr;
-    if (io_status == ZX_OK) {
-      if (status == ZX_OK) {
-        device_buffer[path_len] = '\0';
-        path = device_path;
-      } else if (!admin && status == ZX_ERR_ACCESS_DENIED) {
+    auto result2 = fio::DirectoryAdmin::Call::GetDevicePath(
+        fidl::UnownedClientEnd<fio::DirectoryAdmin>(caller.borrow_channel()));
+    std::string path(std::string("I/O failure: ") + result2.status_string());
+    if (result2.ok()) {
+      if (result2->s == ZX_OK) {
+        path = result2->path.get();
+      } else if (!admin && result2->s == ZX_ERR_ACCESS_DENIED) {
         path = "Unknown; missing O_ADMIN";
       }
     }
 
-    print_fs_type(dirs[i], &options, &info, path);
+    print_fs_type(dirs[i], &options, &info, path.c_str());
   }
 
   return 0;
