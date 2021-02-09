d6ecd08bf922cb0ba9e0e899213060bbc53c07f6
Simon Shields <simonshields@google.com>
2021-02-09 23:02:21 +0000

    [make-fuchsia-vol] Big refactor to improve testability.
    This CL splits "main" into several functions: initArguments() for
    parsing arguments, prepareDisk() for resizing and determining
    information about the disk, createPartitionTable() for figuring out the
    sizes of the partitions and the structure of the GPT, and writeDisk() for
    writing out the disk image.
    
    The intent behind this refactor is to improve the testability of
    make-fuchsia-vol, particularly createPartitionTable(). For simplicity,
    the majority of functions are minimally altered (they still reference
    the global argument variables), but createPartitionTable() is modified
    to be hermetic and not rely on the global variables. It takes all
    configuration as arguments to the function.
    
    No functional change is expected from this CL - it is a pure refactor
    that adds tests for createPartitionTable().
    
    Change-Id: I51af963021a9b963cc7a396e7fd1e84f7f4ce77e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/481823
    Reviewed-by: James Tucker <raggi@google.com>
    Commit-Queue: Simon Shields <simonshields@google.com>

--- tools/make-fuchsia-vol/make-fuchsia-vol.go
+++ tools/make-fuchsia-vol/make-fuchsia-vol.go
@@ -12,20 +12,14 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"runtime"
 	"strconv"
 	"strings"
 
-	"go.fuchsia.dev/fuchsia/src/lib/thinfs/block/file"
 	"go.fuchsia.dev/fuchsia/src/lib/thinfs/fs"
-	"go.fuchsia.dev/fuchsia/src/lib/thinfs/fs/msdosfs"
-	"go.fuchsia.dev/fuchsia/src/lib/thinfs/gpt"
-	"go.fuchsia.dev/fuchsia/src/lib/thinfs/mbr"
 )
 
 var (
@@ -118,6 +112,24 @@ func needFuchsiaBuildDir() {
 }
 
 func main() {
+	bootMode := initArguments()
+	disk, err := filepath.Abs(flag.Args()[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	diskInfo := prepareDisk(disk)
+
+	sizes := partitionSizes{
+		efiSize:    uint64(*efiSize),
+		abrSize:    uint64(*abrSize),
+		vbmetaSize: uint64(*vbmetaSize),
+		fvmSize:    uint64(*fvmSize),
+	}
+	partitions := createPartitionTable(&diskInfo, &sizes, *abr, *verbose, *ramdiskOnly)
+	writeDisk(disk, partitions, diskInfo, sizes, bootMode)
+}
+
+func initArguments() BootPartition {
 	flag.Parse()
 	tryLoadManifests()
 
@@ -214,378 +226,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	disk, err := filepath.Abs(flag.Args()[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if *resize != 0 {
-		s, err := os.Stat(disk)
-		if err == nil {
-			if s.Size() != *resize {
-				if *verbose {
-					log.Printf("Resizing %q from %d to %d", disk, s.Size(), *resize)
-				}
-				if err := os.Truncate(disk, *resize); err != nil {
-					log.Fatalf("failed to truncate %q to %d: %s", disk, *resize, err)
-				}
-			}
-		} else if os.IsNotExist(err) {
-			if *verbose {
-				log.Printf("Creating %q", disk)
-			}
-			f, err := os.Create(disk)
-			if err != nil {
-				log.Fatalf("failed to create %q: %s", disk, err)
-			}
-			if err := f.Truncate(*resize); err != nil {
-				log.Fatalf("failed to truncate %q to %d: %s", disk, *resize, err)
-			}
-			f.Close()
-		} else {
-			log.Fatal(err)
-		}
-	} else {
-		if _, err := os.Stat(disk); err != nil {
-			log.Fatalf("cannot read %q: %s\n", disk, err)
-		}
-	}
-
-	f, err := os.Open(disk)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if *blockSize == 0 {
-		lbs, err := gpt.GetLogicalBlockSize(f)
-		if err != nil {
-			log.Printf("WARNING: could not detect logical block size: %s. Assuming %d\n", err, lbs)
-		}
-		*blockSize = int64(lbs)
-	}
-
-	if *physicalBlockSize == 0 {
-		pbs, err := gpt.GetPhysicalBlockSize(f)
-		if err != nil {
-			log.Printf("WARNING: could not detect physical block size: %s. Assuming %d\n", err, pbs)
-		}
-		*physicalBlockSize = int64(pbs)
-	}
-	if *physicalBlockSize < 4096 && runtime.GOOS == "darwin" {
-		// OSX is not reliably returning correct values for USB sticks, unclear why
-		*physicalBlockSize = 4096
-	}
-
-	var (
-		logical  = uint64(*blockSize)
-		physical = uint64(*physicalBlockSize)
-		optimal  = uint64(*optimalTransferSize)
-
-		diskSize uint64
-	)
-
-	// ignore the error here as it may be an image file...
-	diskSize, _ = gpt.GetDiskSize(f)
-
-	if diskSize == 0 {
-		s, err := os.Stat(disk)
-		if err != nil {
-			log.Fatalf("could not stat %q: %s\n", disk, err)
-		}
-		diskSize = uint64(s.Size())
-	}
-	if diskSize == 0 {
-		log.Fatalf("could not determine size of %q", disk)
-	}
-
-	// Note: this isn't entirely correct, as it doesn't take into account padding.
-	// Consider adding a real API for this in the GPT lib.
-	minGPTSize := int64((gpt.MinPartitionEntryArraySize + gpt.HeaderSize) * 2)
-	if uint64(*efiSize+minGPTSize) > diskSize {
-		log.Fatalf("%q is not large enough for the partition layout\n", disk)
-	}
-
-	if *verbose {
-		log.Printf("Disk: %s", disk)
-		log.Printf("Disk size: %d", diskSize)
-		log.Printf("Block Size: %d", logical)
-		log.Printf("Physical block size: %d", physical)
-		log.Printf("Optimal transfer size: %d", optimal)
-	}
-
-	g, err := gpt.ReadGPT(f, logical, diskSize)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lbaSize := diskSize / logical
-	g.MBR = mbr.NewProtectiveMBR(lbaSize)
-	g.Primary.Partitions = []gpt.PartitionEntry{}
-
-	g.Update(logical, physical, optimal, diskSize) // for the firstusablelba
-	end := g.Primary.FirstUsableLBA
-
-	var efiStart uint64
-	efiStart, end = optimalBlockAlign(end, uint64(*efiSize), logical, physical, optimal)
-	// compute the size of the fat geometry that fits within the well-aligned GPT
-	// partition that was computed above.
-	*efiSize = fitFAT(int64((end)-efiStart) * int64(logical))
-	// efiEnd is the last sector of viable fat geometry, which may be different
-	// from end, which is the last sector of the gpt partition.
-	efiEnd := efiStart + (uint64(*efiSize) / logical) - 1
-
-	if *verbose {
-		log.Printf("EFI START: %d", efiStart)
-		log.Printf("EFI END: %d", efiEnd)
-		log.Printf("EFI LB SIZE: %d", efiEnd-efiStart+1)
-	}
-
-	g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-		PartitionTypeGUID:   gpt.GUIDEFI,
-		UniquePartitionGUID: gpt.NewRandomGUID(),
-		PartitionName:       gpt.NewPartitionName("efi-system"),
-		StartingLBA:         efiStart,
-		EndingLBA:           end,
-	})
-
-	var startingCHS [3]byte
-	startingCHS[0] = byte(efiStart / (16 * 63))
-	startingCHS[1] = byte((efiStart / 63) % 16)
-	startingCHS[2] = byte((efiStart % 63) + 1)
-
-	var endingCHS [3]byte
-	endingCHS[0] = byte(efiEnd / (16 * 63))
-	endingCHS[1] = byte((efiEnd / 63) % 16)
-	endingCHS[2] = byte((efiEnd % 63) + 1)
-
-	// Install a "hybrid MBR" hack for the case of bios bootloaders that might
-	// need it (e.g. rpi's binary blob that's stuck in MBR land).
-	g.MBR.PartitionRecord[2] = mbr.PartitionRecord{
-		BootIndicator: 0x80,
-		StartingCHS:   startingCHS,
-		EndingCHS:     endingCHS,
-		OSType:        mbr.FAT32,
-		StartingLBA:   uint32(efiStart),
-		SizeInLBA:     uint32(efiEnd),
-	}
-
-	var aStart, bStart, rStart uint64
-	var vbmetaAStart, vbmetaBStart, vbmetaRStart, miscStart uint64
-	if *abr {
-		aStart, end = optimalBlockAlign(end+1, uint64(*abrSize), logical, physical, optimal)
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaZirconA,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("ZIRCON-A"),
-			StartingLBA:         aStart,
-			EndingLBA:           end,
-		})
-
-		vbmetaAStart, end = optimalBlockAlign(end+1, uint64(*vbmetaSize), logical, physical, optimal)
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaVbmetaA,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("VBMETA_A"),
-			StartingLBA:         vbmetaAStart,
-			EndingLBA:           end,
-		})
-
-		bStart, end = optimalBlockAlign(end+1, uint64(*abrSize), logical, physical, optimal)
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaZirconB,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("ZIRCON-B"),
-			StartingLBA:         bStart,
-			EndingLBA:           end,
-		})
-
-		vbmetaBStart, end = optimalBlockAlign(end+1, uint64(*vbmetaSize), logical, physical, optimal)
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaVbmetaB,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("VBMETA_B"),
-			StartingLBA:         vbmetaBStart,
-			EndingLBA:           end,
-		})
-
-		rStart, end = optimalBlockAlign(end+1, uint64(*abrSize), logical, physical, optimal)
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaZirconR,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("ZIRCON-R"),
-			StartingLBA:         rStart,
-			EndingLBA:           end,
-		})
-
-		vbmetaRStart, end = optimalBlockAlign(end+1, uint64(*vbmetaSize), logical, physical, optimal)
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaVbmetaR,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("VBMETA_R"),
-			StartingLBA:         vbmetaRStart,
-			EndingLBA:           end,
-		})
-
-		miscStart, end = optimalBlockAlign(end+1, uint64(*vbmetaSize), logical, physical, optimal)
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaMisc,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("MISC"),
-			StartingLBA:         miscStart,
-			EndingLBA:           end,
-		})
-	}
-
-	var fvmStart uint64
-
-	fvmStart, end = optimalBlockAlign(end+1, uint64(*fvmSize), logical, physical, optimal)
-	if !*ramdiskOnly {
-		if *fvmSize == 0 {
-			end = g.Primary.LastUsableLBA
-		}
-		*fvmSize = int64((end + 1 - fvmStart) * logical)
-
-		g.Primary.Partitions = append(g.Primary.Partitions, gpt.PartitionEntry{
-			PartitionTypeGUID:   gpt.GUIDFuchsiaFVM,
-			UniquePartitionGUID: gpt.NewRandomGUID(),
-			PartitionName:       gpt.NewPartitionName("FVM"),
-			StartingLBA:         fvmStart,
-			EndingLBA:           end,
-		})
-	}
-
-	g.Update(logical, physical, optimal, diskSize)
-
-	if err := g.Validate(); err != nil {
-		log.Fatal(err)
-	}
-
-	if *verbose {
-		log.Printf("EFI size: %d", *efiSize)
-		if !*ramdiskOnly {
-			log.Printf("FVM size: %d", *fvmSize)
-		}
-
-		log.Printf("Writing GPT")
-	}
-
-	f, err = os.OpenFile(disk, os.O_RDWR, 0750)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := g.WriteTo(f); err != nil {
-		log.Fatalf("error writing partition table: %s", err)
-	}
-
-	f.Sync()
-
-	aStart = aStart * logical
-	vbmetaAStart = vbmetaAStart * logical
-	bStart = bStart * logical
-	vbmetaBStart = vbmetaBStart * logical
-	rStart = rStart * logical
-	vbmetaRStart = vbmetaRStart * logical
-	miscStart = miscStart * logical
-	efiStart = efiStart * logical
-	fvmStart = fvmStart * logical
-
-	if *verbose {
-		log.Printf("Writing EFI partition and files")
-	}
-
-	cmd := exec.Command(fuchsiaTool("mkfs-msdosfs"),
-		"-@", strconv.FormatUint(efiStart, 10),
-		// XXX(raggi): mkfs-msdosfs offset gets subtracted by the tool for available
-		// size, so we have to add the offset back on to get the correct geometry.
-		"-S", strconv.FormatUint(uint64(*efiSize)+efiStart, 10),
-		"-F", "32",
-		"-L", "ESP",
-		"-O", "Fuchsia",
-		"-b", fmt.Sprintf("%d", logical),
-		disk,
-	)
-
-	if out, err := cmd.CombinedOutput(); err != nil {
-		log.Printf("mkfs-msdosfs failed:\n%s", out)
-		log.Fatal(err)
-	}
-
-	dev, err := file.NewRange(f, int64(logical), int64(efiStart), *efiSize)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fatfs, err := msdosfs.New(disk, dev, fs.ReadWrite|fs.Force)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	root := fatfs.RootDirectory()
-
-	tf, err := ioutil.TempFile("", "gsetup-boot")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tf.WriteString("efi\\boot\\bootx64.efi")
-	tf.Close()
-	defer os.Remove(tf.Name())
-
-	msCopyIn(root, tf.Name(), "EFI/Google/GSetup/Boot")
-	msCopyIn(root, *bootloader, "EFI/BOOT/bootx64.efi")
-	if !*abr {
-		msCopyIn(root, *zbi, "zircon.bin")
-		msCopyIn(root, *zedboot, "zedboot.bin")
-	}
-	if *cmdline != "" {
-		msCopyIn(root, *cmdline, "cmdline")
-	}
-
-	root.Sync()
-	if err := root.Close(); err != nil {
-		log.Fatal(err)
-	}
-	if err := fatfs.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	f.Sync()
-
-	if *abr {
-		if *verbose {
-			log.Print("Populating A/B/R and vbmeta partitions")
-		}
-		partitionCopy(f, int64(aStart), *abrSize, *zirconA)
-		partitionCopy(f, int64(vbmetaAStart), *vbmetaSize, *vbmetaA)
-		partitionCopy(f, int64(bStart), *abrSize, *zirconB)
-		partitionCopy(f, int64(vbmetaBStart), *vbmetaSize, *vbmetaB)
-		partitionCopy(f, int64(rStart), *abrSize, *zirconR)
-		partitionCopy(f, int64(vbmetaRStart), *vbmetaSize, *vbmetaR)
-		if _, err := f.Seek(int64(miscStart), os.SEEK_SET); err != nil {
-			log.Fatal(err)
-		}
-		if err := WriteAbr(bootMode, f); err != nil {
-			log.Fatal(err)
-		}
-
-	}
-
-	f.Sync()
-
-	if !*ramdiskOnly {
-		if *verbose {
-			log.Print("Populating FVM in GPT image")
-		}
-		fvm(disk, int64(fvmStart), *fvmSize, "create", "--blob", *blob, "--data", *data)
-	}
-
-	// Keep the file open so that OSX doesn't try to remount the disk while tools are working on it.
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	if *verbose {
-		log.Printf("Done")
-	}
+	return bootMode
 }
 
 func partitionCopy(f *os.File, start, size int64, path string) {
