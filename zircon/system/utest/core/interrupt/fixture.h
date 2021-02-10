d5d3b8739ebe6c58b36ca5c902931df5598d4dce
Christopher Anderson <cja@google.com>
2021-02-10 00:20:53 +0000

    [core][interrupt-test] Move ThreadEntry out of fixture
    This method only needed to be used by the interrupt test in
    the end so keep it local to the .cc.
    
    Change-Id: I90e3f412e2765416802e2b7fee08f049318eba7c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/484324
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Christopher Anderson <cja@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/utest/core/interrupt/fixture.h
+++ zircon/system/utest/core/interrupt/fixture.h
@@ -36,48 +36,4 @@ class RootResourceFixture : public zxtest::Test {
   zx::bti bti_;
 };
 
-// This is not really a function, but an entry point for a thread that has
-// a tiny stack and no other setup. It's not really entered with the C ABI
-// as such.  Rather, it's entered with the first argument register set to
-// zx_handle_t and with the SP at the very top of the allocated stack.
-// It's defined in pure assembly so that there are no issues with
-// compiler-generated code's assumptions about the proper ABI setup,
-// instrumentation, etc.
-extern "C" void ThreadEntry(uintptr_t arg1, uintptr_t arg2);
-// if (zx_interrupt_wait(static_cast<zx_handle_t>(arg1), nullptr) == ZX_OK) {
-//   zx_thread_exit();
-// }
-// ASSERT(false);
-__asm__(
-    ".pushsection .text.ThreadEntry,\"ax\",%progbits\n"
-    ".balign 4\n"
-    ".type ThreadEntry,%function\n"
-    "ThreadEntry:\n"
-#ifdef __aarch64__
-    "  mov w20, w0\n"           // Save handle in callee-saves register.
-    "  mov w0, w20\n"           // Load saved handle into argument register.
-    "  mov x1, xzr\n"           // Load nullptr into argument register.
-    "  bl zx_interrupt_wait\n"  // Call.
-    "  cbz w0, exit\n"          // Exit if returned ZX_OK.
-    "  brk #0\n"                // Else crash.
-    "exit:\n"
-    "  bl zx_thread_exit\n"
-    "  brk #0\n"  // Crash if we didn't exit.
-#elif defined(__x86_64__)
-    "  mov %edi, %ebx\n"          // Save handle in callee-saves register.
-    "  mov %ebx, %edi\n"          // Load saved handle into argument register.
-    "  xor %edx, %edx\n"          // Load nullptr into argument register.
-    "  call zx_interrupt_wait\n"  // Call.
-    "  testl %eax, %eax\n"        // If returned ZX_OK...
-    "  jz exit\n"                 // ...exit.
-    "  ud2\n"                     // Else crash.
-    "exit:\n"
-    "  call zx_thread_exit\n"
-    "  ud2\n"  // Crash if we didn't exit.
-#else
-#error "what machine?"
-#endif
-    ".size ThreadEntry, . - ThreadEntry\n"
-    ".popsection");
-
 }  // namespace
