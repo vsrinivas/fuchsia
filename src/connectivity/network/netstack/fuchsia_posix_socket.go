vsrinivas/fuchsia

commit 8dc677fae295c4b2c13384b29581f05ed7c6ba35

Reland "[netstack][fdio] TCP connection reset handling fixes"
This is a reland of c99fe9c1284265fe5eb0cd3ce7c3ba431b2f3f4a
Fix a bug where netstack could panic because of a race in starting poll
on the socket handle while closing the same because of close or hangup
event.

Fixed: 69513
Test: fx --dir out/qemu test -od netstack_bsdsocket_c_api_test -- \
--gtest_filter=NetStreamTest.ConnectCloseRace

Original change's description:
> [netstack][fdio] TCP connection reset handling fixes
>
> Fix fdio and netstack2 layers to propagate reset error state and return
> appropriate errors for socket read/write calls, while keeping the
> behavior in parity with Linux. Address few edge cases for connecting
> vs connected states and blocking vs non-blocking socket calls.
>
> - Define new fdio states to record reset error state for connected
>   and connecting sockets. Apply that for subsequent read/write socket
>   calls to return appropriate errors. This change ensures that the
>   behavior is in parity with Linux.
> - Fix netstack2 to account for connection handshake failures while
>   signalling an incoming event to the client.
> - Define 2 new signals to convey connection reset and refused cases
>   to the fdio layer.
> - Refactor part of gVisor endpoint error handling from within loopXX
>   routines and offload the actions to the EventHUp callback that is
>   currently registered with gVisor stack.
> - Fix EventHUp callback registration to check for prior endpoint
>   error state and if so, intitiate closing of the endpoint.
> - Enable TCP shutdown for a listening socket.
>
> Bug: 35594
> Test: fx test -od netstack-bsdsocket-c-api-test -- --gtest_filter=NetStreamTest/StopListenWhileConnect.*
> Change-Id: Ibd1320279d33fb44739a19e95fa0b4b81223f58f
> Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/437660
> Commit-Queue: Mithun Iyer <iyerm@google.com>
> Reviewed-by: Tamir Duberstein <tamird@google.com>
> Reviewed-by: Ghanan Gowripalan <ghanan@google.com>

src/connectivity/network/netstack/fuchsia_posix_socket.go
@@ -138,6 +138,8 @@ type endpoint struct {
 
 	ns *Netstack
 
+	// gVisor stack clears the hard error on the endpoint on a read, so,
+	// save the error when returned by gVisor endpoint calls.
 	hardError hardError
 }
 
@@ -175,6 +177,13 @@ func (he *hardError) storeAndRetrieveLocked(err tcpip.Error) tcpip.Error {
 		}
 	}
 	return he.mu.err
+}
+
+func (he *hardError) get() tcpip.Error {
+	he.mu.Lock()
+	err := he.mu.err
+	he.mu.Unlock()
+	return err
 }
 
 func (ep *endpoint) Sync(fidl.Context) (int32, error) {
@@ -432,10 +441,38 @@ func newEndpointWithSocket(ep tcpip.Endpoint, wq *waiter.Queue, transProto tcpip
 		linger:  make(chan struct{}),
 	}
 
-	// Handle the case where the endpoint has already seen an error state.
-	// For ex: Accepted endpoints that get a RST by the time we come here.
-	// In such case, we would skip registering for EventHUp as we are
-	// looking at the endpoint post HUp.
+	onHUp := func() {
+		eps.onHUpOnce.Do(func() {
+			if !eps.endpoint.ns.onRemoveEndpoint(eps.endpoint.key) {
+				_ = syslog.Errorf("endpoint map delete error, endpoint with key %d does not exist", eps.endpoint.key)
+			}
+			// Run this in a separate goroutine to avoid deadlock.
+			//
+			// The waiter.Queue lock is held by the caller of this callback.
+			// close() blocks on completions of `loop*`, which
+			// depend on acquiring waiter.Queue lock to unregister events.
+			go func() {
+				eps.wq.EventUnregister(&eps.onHUp)
+				eps.close()
+			}()
+		})
+	}
+
+	eps.onHUp.Callback = callback(func(*waiter.Entry, waiter.EventMask) {
+		onHUp()
+	})
+	eps.wq.EventRegister(&eps.onHUp, waiter.EventHUp)
+
+	ns.onAddEndpoint(&eps.endpoint)
+
+	// Accepted endpoints which are already reset would not notify hangup event.
+	// Check for the hard error state and handle any cleanup.
+	//
+	// Note that we register a callback for hangup event and add the endpoint
+	// to the internal map before checking for the error state. This is to avoid
+	// losing notification of hangup event with a race between checking for hard
+	// error state in gVisor endpoint and the same endpoint's error state being
+	// updated because of processing of an incoming RST.
 	//
 	// Acquire hard error lock across ep calls to avoid races and store the
 	// hard error deterministically.
@@ -443,38 +480,8 @@ func newEndpointWithSocket(ep tcpip.Endpoint, wq *waiter.Queue, transProto tcpip
 	hardError := eps.endpoint.hardError.storeAndRetrieveLocked(eps.ep.LastError())
 	eps.endpoint.hardError.mu.Unlock()
 	if hardError != nil {
-		// Run this in a separate goroutine to avoid deadlock.
-		//
-		// close() blocks on completions of `loop*` routines.
-		go eps.close()
-	} else {
-		// Add the endpoint before registering for an EventHUp callback and starting
-		// the loop{read,Write} go-routines. We remove the endpoint from the map on
-		// EventHUp which can be trigerred soon-after the callback registration or
-		// starting of the loop{Read,Write}.
-		ns.onAddEndpoint(&eps.endpoint)
-
-		// Register a callback for error and closing events from gVisor to
-		// trigger a close of the endpoint.
-		eps.onHUp.Callback = callback(func(*waiter.Entry, waiter.EventMask) {
-			eps.onHUpOnce.Do(func() {
-				if !eps.endpoint.ns.onRemoveEndpoint(eps.endpoint.key) {
-					_ = syslog.Errorf("endpoint map delete error, endpoint with key %d does not exist", eps.endpoint.key)
-				}
-				// Run this in a separate goroutine to avoid deadlock.
-				//
-				// The waiter.Queue lock is held by the caller of this callback.
-				// close() blocks on completions of `loop*`, which
-				// depend on acquiring waiter.Queue lock to unregister events.
-				go func() {
-					eps.wq.EventUnregister(&eps.onHUp)
-					eps.close()
-				}()
-			})
-		})
-		eps.wq.EventRegister(&eps.onHUp, waiter.EventHUp)
+		onHUp()
 	}
-
 	return eps, nil
 }
 
@@ -596,6 +603,25 @@ func (eps *endpointWithSocket) close() {
 			}
 		}
 
+		// Signal the client about hard errors that require special errno
+		// handling by the client for read/write calls.
+		err := eps.endpoint.hardError.get()
+		if err == nil {
+			// The gVisor endpoint could have got a hard error after the
+			// read/write loops have ended, check for that here.
+			err = eps.ep.LastError()
+		}
+		switch err.(type) {
+		case *tcpip.ErrConnectionRefused:
+			if err := eps.local.Handle().SignalPeer(0, zxsocket.SignalConnectionRefused); err != nil {
+				panic(fmt.Sprintf("Handle().SignalPeer(0, zxsocket.SignalConnectionRefused) = %s", err))
+			}
+		case *tcpip.ErrConnectionReset:
+			if err := eps.local.Handle().SignalPeer(0, zxsocket.SignalConnectionReset); err != nil {
+				panic(fmt.Sprintf("Handle().SignalPeer(0, zxsocket.SignalConnectionReset) = %s", err))
+			}
+		}
+
 		if err := eps.local.Close(); err != nil {
 			panic(err)
 		}
@@ -619,6 +645,9 @@ func (eps *endpointWithSocket) Listen(_ fidl.Context, backlog int16) (socket.Str
 	// fail above, so we register the callback only in the success case to avoid
 	// incorrectly handling events on connected sockets.
 	eps.onListen.Do(func() {
+		// Start polling for any shutdown events from the client as shutdown is
+		// allowed on a listening stream socket.
+		eps.startPollLoop()
 		var entry waiter.Entry
 		cb := func() {
 			var err error
@@ -656,7 +685,19 @@ func (eps *endpointWithSocket) Listen(_ fidl.Context, backlog int16) (socket.Str
 	return socket.StreamSocketListenResultWithResponse(socket.StreamSocketListenResponse{}), nil
 }
 
-func (eps *endpointWithSocket) startLoops(signals zx.Signals) {
+func (eps *endpointWithSocket) startPollLoop() {
+	eps.mu.Lock()
+	defer eps.mu.Unlock()
+	select {
+	case <-eps.closing:
+	default:
+		ch := make(chan struct{})
+		eps.mu.loopPollDone = ch
+		go eps.loopPoll(ch)
+	}
+}
+
+func (eps *endpointWithSocket) startReadWriteLoops(signals zx.Signals) {
 	eps.mu.Lock()
 	defer eps.mu.Unlock()
 	select {
@@ -669,7 +710,6 @@ func (eps *endpointWithSocket) startLoops(signals zx.Signals) {
 			done *<-chan struct{}
 			fn   func(chan<- struct{})
 		}{
-			{&eps.mu.loopPollDone, eps.loopPoll},
 			{&eps.mu.loopReadDone, eps.loopRead},
 			{&eps.mu.loopWriteDone, eps.loopWrite},
 		} {
@@ -707,7 +747,8 @@ func (eps *endpointWithSocket) Connect(ctx fidl.Context, address fidlnet.SocketA
 						signals |= zxsocket.SignalConnected
 					}
 					go eps.wq.EventUnregister(&entry)
-					eps.startLoops(signals)
+					eps.startPollLoop()
+					eps.startReadWriteLoops(signals)
 				})
 			}
 			entry.Callback = callback(func(_ *waiter.Entry, m waiter.EventMask) {
@@ -783,7 +824,7 @@ func (eps *endpointWithSocket) Accept(wantAddr bool) (posix.Errno, *tcpip.FullAd
 			return 0, nil, nil, err
 		}
 
-		eps.onConnect.Do(func() { eps.startLoops(zxsocket.SignalOutgoing | zxsocket.SignalConnected) })
+		eps.onConnect.Do(func() { eps.startReadWriteLoops(zxsocket.SignalOutgoing | zxsocket.SignalConnected) })
 
 		return 0, addr, eps, nil
 	}
@@ -791,13 +832,7 @@ func (eps *endpointWithSocket) Accept(wantAddr bool) (posix.Errno, *tcpip.FullAd
 
 // loopWrite shuttles signals and data from the zircon socket to the tcpip.Endpoint.
 func (eps *endpointWithSocket) loopWrite(ch chan<- struct{}) {
-	triggerClose := false
-	defer func() {
-		close(ch)
-		if triggerClose {
-			eps.close()
-		}
-	}()
+	defer close(ch)
 
 	const sigs = zx.SignalSocketReadable | zx.SignalSocketPeerWriteDisabled | localSignalClosing
 
@@ -902,12 +937,10 @@ func (eps *endpointWithSocket) loopWrite(ch chan<- struct{}) {
 			}
 			return
 		case *tcpip.ErrConnectionAborted, *tcpip.ErrConnectionReset, *tcpip.ErrNetworkUnreachable, *tcpip.ErrNoRoute:
-			triggerClose = true
 			return
 		case *tcpip.ErrTimeout:
 			// The maximum duration of missing ACKs was reached, or the maximum
 			// number of unacknowledged keepalives was reached.
-			triggerClose = true
 			return
 		default:
 			_ = syslog.Errorf("TCP Endpoint.Write(): %s", err)
@@ -917,13 +950,7 @@ func (eps *endpointWithSocket) loopWrite(ch chan<- struct{}) {
 
 // loopRead shuttles signals and data from the tcpip.Endpoint to the zircon socket.
 func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
-	triggerClose := false
-	defer func() {
-		close(ch)
-		if triggerClose {
-			eps.close()
-		}
-	}()
+	defer close(ch)
 
 	inEntry, inCh := waiter.NewChannelEntry(nil)
 	eps.wq.EventRegister(&inEntry, waiter.EventIn)
@@ -956,7 +983,6 @@ func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
 			//
 			// The connection was alive but now is dead - this is equivalent to
 			// having received a TCP RST.
-			triggerClose = true
 			return
 		case *tcpip.ErrConnectionRefused:
 			// Connection refused is a "hard error" that may be observed on either the
@@ -986,7 +1012,6 @@ func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
 			}
 			return
 		case *tcpip.ErrConnectionAborted, *tcpip.ErrConnectionReset, *tcpip.ErrNetworkUnreachable, *tcpip.ErrNoRoute:
-			triggerClose = true
 			return
 		case nil, *tcpip.ErrBadBuffer:
 			if err == nil {

src/connectivity/network/tests/bsdsocket_test.cc
@@ -1407,6 +1407,55 @@ TEST(NetStreamTest, ConnectTwice) {
   ASSERT_EQ(close(client.release()), 0) << strerror(errno);
 }
 
+TEST(NetStreamTest, ConnectCloseRace) {
+  struct sockaddr_in addr = {
+      .sin_family = AF_INET,
+      .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+  };
+
+  // Use the ephemeral port allocated by the stack as destination address for connect.
+  {
+    fbl::unique_fd tmp;
+    ASSERT_TRUE(tmp = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
+
+    ASSERT_EQ(bind(tmp.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr)), 0)
+        << strerror(errno);
+    socklen_t addrlen = sizeof(addr);
+    ASSERT_EQ(getsockname(tmp.get(), reinterpret_cast<struct sockaddr*>(&addr), &addrlen), 0)
+        << strerror(errno);
+    ASSERT_EQ(addrlen, sizeof(addr));
+  }
+
+  std::array<std::thread, 50> threads;
+  for (auto& t : threads) {
+    t = std::thread([&] {
+      for (int i = 0; i < 5; i++) {
+        fbl::unique_fd client;
+        ASSERT_TRUE(client = fbl::unique_fd(socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)))
+            << strerror(errno);
+
+        ASSERT_EQ(
+            connect(client.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr)),
+            -1);
+        ASSERT_TRUE(errno == EINPROGRESS
+#if !defined(__Fuchsia__)
+                    // Linux could return ECONNREFUSED if it processes the incoming RST before
+                    // connect system
+                    // call returns.
+                    || errno == ECONNREFUSED
+#endif
+                    )
+            << strerror(errno);
+        ASSERT_EQ(close(client.release()), 0) << strerror(errno);
+      }
+    });
+  }
+
+  for (auto& t : threads) {
+    t.join();
+  }
+}
+
 void TestHangupDuringConnect(void (*hangup)(fbl::unique_fd*)) {
   fbl::unique_fd client, listener;
   ASSERT_TRUE(client = fbl::unique_fd(socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)))
@@ -2699,6 +2748,116 @@ INSTANTIATE_TEST_SUITE_P(NetStreamTest, IOMethodTest,
                            return info.param.IOMethodToString();
                          });
 
+namespace {
+// Test close/shutdown of listening socket with multiple non-blocking connects.
+// This tests client sockets in connected and connecting states.
+void TestListenWhileConnect(const IOMethod& ioMethod, void (*stopListen)(fbl::unique_fd&)) {
+  fbl::unique_fd client, listener;
+  ASSERT_TRUE(listener = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
+  constexpr int kBacklog = 2;
+  // Linux completes one more connection than the listen backlog argument.
+  // To ensure that there is at least one client connection that stays in
+  // connecting state, keep 2 more client connections than the listen backlog.
+  // gVisor differs in this behavior though, gvisor.dev/issue/3153.
+  constexpr int kClients = kBacklog + 2;
+
+  struct sockaddr_in addr = {
+      .sin_family = AF_INET,
+      .sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+  };
+
+  ASSERT_EQ(bind(listener.get(), reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr)), 0)
+      << strerror(errno);
+  ASSERT_EQ(listen(listener.get(), kBacklog), 0) << strerror(errno);
+
+  socklen_t addrlen = sizeof(addr);
+  ASSERT_EQ(getsockname(listener.get(), reinterpret_cast<struct sockaddr*>(&addr), &addrlen), 0)
+      << strerror(errno);
+  ASSERT_EQ(addrlen, sizeof(addr));
+
+  std::array<fbl::unique_fd, kClients> clients;
+  for (fbl::unique_fd& client : clients) {
+    ASSERT_TRUE(client = fbl::unique_fd(socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)))
+        << strerror(errno);
+    int ret = connect(client.get(), reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+    // Linux manpage for connect, for EINPROGRESS error:
+    // "The socket is nonblocking and the connection cannot be completed immediately."
+    // Which means that the non-blocking connect may succeed (ie. ret == 0) in the unlikely case
+    // where the connection does complete immediately before the system call returns.
+    //
+    // On Fuchsia, a non-blocking connect would always fail with EINPROGRESS.
+#if !defined(__Fuchsia__)
+    if (ret != 0)
+#endif
+    {
+      EXPECT_EQ(ret, -1);
+      EXPECT_EQ(errno, EINPROGRESS) << strerror(errno);
+    }
+  }
+
+  ASSERT_NO_FATAL_FAILURE(stopListen(listener));
+
+  for (auto& client : clients) {
+    struct pollfd pfd = {
+        .fd = client.get(),
+        .events = POLLIN,
+    };
+    // When the listening socket is stopped, then we expect the remote to reset
+    // the connection.
+    int n = poll(&pfd, 1, kTimeout);
+    ASSERT_GE(n, 0) << strerror(errno);
+    ASSERT_EQ(n, 1);
+    ASSERT_EQ(pfd.revents, POLLIN | POLLHUP | POLLERR);
+    char c;
+    ASSERT_EQ(ioMethod.executeIO(client.get(), &c, sizeof(c)), -1);
+    // Subsequent read can fail with:
+    // ECONNRESET: If the client connection was established and was reset by the
+    // remote.
+    // ECONNREFUSED: If the client connection failed to be established.
+    ASSERT_TRUE(errno == ECONNRESET || errno == ECONNREFUSED) << strerror(errno);
+    // The last client connection would be in connecting (SYN_SENT) state.
+    if (client == clients[kClients - 1]) {
+      ASSERT_EQ(errno, ECONNREFUSED) << strerror(errno);
+    }
+
+    bool isWrite = ioMethod.isWrite();
+#if !defined(__Fuchsia__)
+    auto undo = disableSIGPIPE(isWrite);
+#endif
+
+    if (isWrite) {
+      ASSERT_EQ(ioMethod.executeIO(client.get(), &c, sizeof(c)), -1);
+      EXPECT_EQ(errno, EPIPE) << strerror(errno);
+    } else {
+      ASSERT_EQ(ioMethod.executeIO(client.get(), &c, sizeof(c)), 0) << strerror(errno);
+    }
+  }
+}
+
+class StopListenWhileConnect : public ::testing::TestWithParam<IOMethod> {};
+
+TEST_P(StopListenWhileConnect, Close) {
+  TestListenWhileConnect(
+      GetParam(), [](fbl::unique_fd& f) { ASSERT_EQ(close(f.release()), 0) << strerror(errno); });
+}
+
+TEST_P(StopListenWhileConnect, Shutdown) {
+  TestListenWhileConnect(GetParam(), [](fbl::unique_fd& f) {
+    ASSERT_EQ(shutdown(f.get(), SHUT_RD), 0) << strerror(errno);
+  });
+}
+
+INSTANTIATE_TEST_SUITE_P(NetStreamTest, StopListenWhileConnect,
+                         ::testing::Values(IOMethod::Op::READ, IOMethod::Op::READV,
+                                           IOMethod::Op::RECV, IOMethod::Op::RECVFROM,
+                                           IOMethod::Op::RECVMSG, IOMethod::Op::WRITE,
+                                           IOMethod::Op::WRITEV, IOMethod::Op::SEND,
+                                           IOMethod::Op::SENDTO, IOMethod::Op::SENDMSG),
+                         [](const ::testing::TestParamInfo<IOMethod>& info) {
+                           return info.param.IOMethodToString();
+                         });
+}  // namespace
+
 TEST(NetStreamTest, NonBlockingConnectRefused) {
   fbl::unique_fd acptfd;
   ASSERT_TRUE(acptfd = fbl::unique_fd(socket(AF_INET, SOCK_STREAM, 0))) << strerror(errno);
@@ -2790,12 +2949,9 @@ TEST(NetStreamTest, GetSocketAcceptConn) {
   EXPECT_EQ(got_len, sizeof(got));
   EXPECT_EQ(got, 1);
 
-  // TODO(fxbug.dev/61714): fix shutdown and remove the guard.
-#if defined(__Fuchsia__)
-  EXPECT_EQ(shutdown(fd.get(), SHUT_WR), -1);
-  EXPECT_EQ(errno, EPIPE);
-#else
   ASSERT_EQ(shutdown(fd.get(), SHUT_WR), 0) << strerror(errno);
+  // TODO(https://fxbug.dev/61714): Fix the race with shutdown and getsockopt.
+#if !defined(__Fuchsia__)
   got = -1;
   got_len = sizeof(got);
   ASSERT_EQ(getsockopt(fd.get(), SOL_SOCKET, SO_ACCEPTCONN, &got, &got_len), 0) << strerror(errno);
@@ -2803,12 +2959,9 @@ TEST(NetStreamTest, GetSocketAcceptConn) {
   EXPECT_EQ(got, 1);
 #endif
 
-  // TODO(fxbug.dev/61714): fix shutdown and remove the guard.
-#if defined(__Fuchsia__)
-  EXPECT_EQ(shutdown(fd.get(), SHUT_RD), -1);
-  EXPECT_EQ(errno, EPIPE);
-#else
   ASSERT_EQ(shutdown(fd.get(), SHUT_RD), 0) << strerror(errno);
+  // TODO(https://fxbug.dev/61714): Fix the race with shutdown and getsockopt.
+#if !defined(__Fuchsia__)
   got = -1;
   got_len = sizeof(got);
   ASSERT_EQ(getsockopt(fd.get(), SOL_SOCKET, SO_ACCEPTCONN, &got, &got_len), 0) << strerror(errno);

sdk/lib/fdio/internal.h
@@ -116,9 +116,15 @@ using fdio_ops_t = struct fdio_ops {
 #define IOFLAG_CLOEXEC (1 << 0)
 #define IOFLAG_EPOLL (1 << 2)
 #define IOFLAG_WAITABLE (1 << 3)
+
+// Socket is connecting to the peer.
 #define IOFLAG_SOCKET_CONNECTING (1 << 4)
+// Socket is connected to the peer.
 #define IOFLAG_SOCKET_CONNECTED (1 << 5)
+// Socket is operating in non-blocking mode.
 #define IOFLAG_NONBLOCK (1 << 6)
+// Socket has an error signal asserted.
+#define IOFLAG_SOCKET_HAS_ERROR (1 << 7)
 
 // The subset of fdio_t per-fd flags queryable via fcntl.
 // Static assertions in unistd.cc ensure we aren't colliding.

sdk/lib/fdio/private-socket.h
@@ -13,6 +13,8 @@
 #define ZXSIO_SIGNAL_CONNECTED ZX_USER_SIGNAL_3
 #define ZXSIO_SIGNAL_SHUTDOWN_READ ZX_USER_SIGNAL_4
 #define ZXSIO_SIGNAL_SHUTDOWN_WRITE ZX_USER_SIGNAL_5
+#define ZXSIO_SIGNAL_CONNECTION_REFUSED ZX_USER_SIGNAL_6
+#define ZXSIO_SIGNAL_CONNECTION_RESET ZX_USER_SIGNAL_7
 
 bool fdio_is_socket(fdio_t* io);
 

commit 7f07287fd84f0c0c59f6087e3ced3b6b9629c33c

[netstack] Fixes to updating the saved hard error state
Netstack saves the hard error returned by gVisor calls and uses the
saved value for subsequent socket calls. This is required as gVisor
keeps the hard error state as clear-on-read.

Fix a bug introduced by 8dc677fae295c4b2c13384b29581f05ed7c6ba35 where
the hard error read+save is racy in Netstack with a failing connect and
processing of hangup event due to the connect failure.

The bug is exposed when the following sequence of events occur:
(1) call gVisor Connect() which is expected to fail
(2) gVisor endpoint receives RST and notifies hangup event
(3) While processing the hangup event, we call LastError(), but fail
to save the error (ErrConnectionRefused).
(4) call gVisor Connect() on the same endpoint. This fails with a
different error than (3) which is incorrectly returned to the socket
layer, as the actual hard error was not saved in (3).

Fix updation of hard error state on getsocktopt SO_ERROR which is buggy
due to a similar race as above.

Bug: 35594
Bug: 67142
Test: No new test added
fx test -od netstack_bsdsocket_c_api_test -- \
--gtest_filter=NetStreamTest.ConnectTwice --gtest_repeat=10000
fx test test -od netstack_bsdsocket_c_api_test -- \
--gtest_filter=LocalhostTest.AcceptAfterReset

Change-Id: Icf6e4377df71f844c96b66557fb6fc4de9ac00d8
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/485186
Commit-Queue: Mithun Iyer <iyerm@google.com>
Reviewed-by: Tamir Duberstein <tamird@google.com>

src/connectivity/network/netstack/fuchsia_posix_socket.go
@@ -179,13 +179,6 @@ func (he *hardError) storeAndRetrieveLocked(err tcpip.Error) tcpip.Error {
 	return he.mu.err
 }
 
-func (he *hardError) get() tcpip.Error {
-	he.mu.Lock()
-	err := he.mu.err
-	he.mu.Unlock()
-	return err
-}
-
 func (ep *endpoint) Sync(fidl.Context) (int32, error) {
 	_ = syslog.DebugTf("Sync", "%p", ep)
 
@@ -349,7 +342,7 @@ func (ep *endpoint) GetSockOpt(_ fidl.Context, level, optName int16) (socket.Bas
 		ep.mu.Unlock()
 	} else {
 		var err tcpip.Error
-		val, err = GetSockOpt(ep.ep, ep.ns, ep.netProto, ep.transProto, level, optName)
+		val, err = GetSockOpt(ep.ep, ep.ns, &ep.hardError, ep.netProto, ep.transProto, level, optName)
 		if err != nil {
 			return socket.BaseSocketGetSockOptResultWithErr(tcpipErrorToCode(err)), nil
 		}
@@ -603,14 +596,13 @@ func (eps *endpointWithSocket) close() {
 			}
 		}
 
+		// The gVisor endpoint could have got a hard error after the
+		// read/write loops have ended, check for that here.
+		eps.endpoint.hardError.mu.Lock()
+		err := eps.endpoint.hardError.storeAndRetrieveLocked(eps.ep.LastError())
+		eps.endpoint.hardError.mu.Unlock()
 		// Signal the client about hard errors that require special errno
 		// handling by the client for read/write calls.
-		err := eps.endpoint.hardError.get()
-		if err == nil {
-			// The gVisor endpoint could have got a hard error after the
-			// read/write loops have ended, check for that here.
-			err = eps.ep.LastError()
-		}
 		switch err.(type) {
 		case *tcpip.ErrConnectionRefused:
 			if err := eps.local.Handle().SignalPeer(0, zxsocket.SignalConnectionRefused); err != nil {

src/connectivity/network/netstack/socket_conv.go
@@ -74,10 +74,10 @@ func boolToInt32(v bool) int32 {
 	return 0
 }
 
-func GetSockOpt(ep tcpip.Endpoint, ns *Netstack, netProto tcpip.NetworkProtocolNumber, transProto tcpip.TransportProtocolNumber, level, name int16) (interface{}, tcpip.Error) {
+func GetSockOpt(ep tcpip.Endpoint, ns *Netstack, he *hardError, netProto tcpip.NetworkProtocolNumber, transProto tcpip.TransportProtocolNumber, level, name int16) (interface{}, tcpip.Error) {
 	switch level {
 	case C.SOL_SOCKET:
-		return getSockOptSocket(ep, ns, netProto, transProto, name)
+		return getSockOptSocket(ep, ns, he, netProto, transProto, name)
 
 	case C.SOL_TCP:
 		return getSockOptTCP(ep, name)
@@ -101,7 +101,7 @@ func GetSockOpt(ep tcpip.Endpoint, ns *Netstack, netProto tcpip.NetworkProtocolN
 	return nil, &tcpip.ErrUnknownProtocol{}
 }
 
-func getSockOptSocket(ep tcpip.Endpoint, ns *Netstack, netProto tcpip.NetworkProtocolNumber, transProto tcpip.TransportProtocolNumber, name int16) (interface{}, tcpip.Error) {
+func getSockOptSocket(ep tcpip.Endpoint, ns *Netstack, he *hardError, netProto tcpip.NetworkProtocolNumber, transProto tcpip.TransportProtocolNumber, name int16) (interface{}, tcpip.Error) {
 	switch name {
 	case C.SO_TYPE:
 		switch transProto {
@@ -138,8 +138,9 @@ func getSockOptSocket(ep tcpip.Endpoint, ns *Netstack, netProto tcpip.NetworkPro
 		}
 
 	case C.SO_ERROR:
-		// Get the last error and convert it.
-		err := ep.LastError()
+		he.mu.Lock()
+		err := he.storeAndRetrieveLocked(ep.LastError())
+		he.mu.Unlock()
 		if err == nil {
 			return int32(0), nil
 		}

src/connectivity/network/tests/bsdsocket_test.cc
@@ -1327,17 +1327,7 @@ TEST(LocalhostTest, AcceptAfterReset) {
   int err;
   socklen_t optlen = sizeof(err);
   ASSERT_EQ(getsockopt(conn.get(), SOL_SOCKET, SO_ERROR, &err, &optlen), 0) << strerror(errno);
-  ASSERT_EQ(err,
-  // TODO(gvisor.dev/issue/1509): loopRead is consuming the error, so we don't observe it here. This
-  // is yet another symptom of the poor integration between gvisor/netstack and fuchsia/netstack.
-  // Resolving this will require refactoring gvisor/netstack to allow "bring-your-own-buffer", so
-  // that it can read from and write directly into the zircon socket provided by fuchsia/netstack.
-#if defined(__Fuchsia__)
-            0)
-#else
-            ECONNRESET)
-#endif
-      << strerror(errno);
+  ASSERT_EQ(err, ECONNRESET) << strerror(errno);
   ASSERT_EQ(optlen, sizeof(err));
 }
 