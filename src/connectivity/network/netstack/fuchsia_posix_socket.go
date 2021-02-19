vsrinivas/fuchsia

commit 69db912e2aaeaa0f5de8e8a067fb71a5b78015d1

Reland "[netstack] Start loops after connection"
This is a reland of c844f4c04cf433b532501ee90c6bf9362f1f2fd1

The reland corrects a problem where accepted sockets were not
immediately marked connected, resulting in test flakes.

Original change's description:
> [netstack] Start loops after connection
>
> - Listening sockets no longer require `loop{Poll,Read,Write}`
> - `loop{Read,Write}` no longer need to detect connectedness
>
> Change-Id: Id82f68872710f8455fea7e8103ee4d3c3832feca
> Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/470477
> Commit-Queue: Tamir Duberstein <tamird@google.com>
> Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
> Reviewed-by: Mithun Iyer <iyerm@google.com>
> Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
> Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>

Multiply: ipv4-forward-test: 30
Multiply: netstack_socket_integration_test: 30
Multiply: isolated-swd-tests: 30
Multiply: mock-omaha-server-lib-test: 30
Multiply: resolve-propagates-pkgfs-failure: 30
Multiply: fuchsia-pkg://fuchsia.com/pkg-resolver-integration-tests#meta/resolve-succeeds.cmx: 30
Multiply: fuchsia-pkg://fuchsia.com/pkg-resolver-integration-tests#meta/resolve-succeeds-with-broken-minfs.cmx: 30
Fixed: 67799, 67800, 67797, 67798, 67801
Fixed: 67802, 67804
Change-Id: I7bb9d5d0b79a94af1ebdc39568aa7d4f1168577f
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/471757
Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>

src/connectivity/network/netstack/fuchsia_posix_socket.go
@@ -347,16 +347,16 @@ type endpointWithSocket struct {
 
 	// These channels enable coordination of orderly shutdown of loops, handles,
 	// and endpoints. See the comment on `close` for more information.
-	//
-	// Notes:
-	//
-	//  - closing is signaled iff close has been called.
-	//
-	//  - loop{Read,Write}Done are signaled iff loop{Read,Write} have
-	//    exited, respectively.
-	//
-	//  - loopPollDone is signaled when the poller goroutine exits.
-	closing, loopReadDone, loopWriteDone, loopPollDone chan struct{}
+	mu struct {
+		sync.Mutex
+
+		// loop{Read,Write,Poll}Done are signaled iff loop{Read,Write,Poll} have
+		// exited, respectively.
+		loopReadDone, loopWriteDone, loopPollDone <-chan struct{}
+	}
+
+	// closing is signaled iff close has been called.
+	closing chan struct{}
 
 	// This is used to make sure that endpoint.close only cleans up its
 	// resources once - the first time it was closed.
@@ -369,6 +369,12 @@ type endpointWithSocket struct {
 
 	// onHUp is used to register callback for closing events.
 	onHUp waiter.Entry
+
+	// onListen is used to register callbacks for listening sockets.
+	onListen sync.Once
+
+	// onConnect is used to register callbacks for connected sockets.
+	onConnect sync.Once
 }
 
 func newEndpointWithSocket(ep tcpip.Endpoint, wq *waiter.Queue, transProto tcpip.TransportProtocolNumber, netProto tcpip.NetworkProtocolNumber, ns *Netstack) (*endpointWithSocket, error) {
@@ -385,13 +391,10 @@ func newEndpointWithSocket(ep tcpip.Endpoint, wq *waiter.Queue, transProto tcpip
 			netProto:   netProto,
 			ns:         ns,
 		},
-		local:         localS,
-		peer:          peerS,
-		loopReadDone:  make(chan struct{}),
-		loopWriteDone: make(chan struct{}),
-		loopPollDone:  make(chan struct{}),
-		closing:       make(chan struct{}),
-		linger:        make(chan struct{}),
+		local:   localS,
+		peer:    peerS,
+		closing: make(chan struct{}),
+		linger:  make(chan struct{}),
 	}
 
 	// Handle the case where the endpoint has already seen an error state.
@@ -437,55 +440,36 @@ func newEndpointWithSocket(ep tcpip.Endpoint, wq *waiter.Queue, transProto tcpip
 		eps.wq.EventRegister(&eps.onHUp, waiter.EventHUp)
 	}
 
-	go func() {
-		defer close(eps.loopPollDone)
+	return eps, nil
+}
 
-		sigs := zx.Signals(zx.SignalSocketWriteDisabled | localSignalClosing)
+func (eps *endpointWithSocket) loopPoll(ch chan<- struct{}) {
+	defer close(ch)
 
-		for {
-			obs, err := zxwait.Wait(zx.Handle(eps.local), sigs, zx.TimensecInfinite)
-			if err != nil {
-				panic(err)
-			}
+	sigs := zx.Signals(zx.SignalSocketWriteDisabled | localSignalClosing)
 
-			if obs&sigs&zx.SignalSocketWriteDisabled != 0 {
-				sigs ^= zx.SignalSocketWriteDisabled
-				if err := eps.ep.Shutdown(tcpip.ShutdownRead); err != nil && err != tcpip.ErrNotConnected {
+	for {
+		obs, err := zxwait.Wait(zx.Handle(eps.local), sigs, zx.TimensecInfinite)
+		if err != nil {
+			panic(err)
+		}
+
+		if obs&sigs&zx.SignalSocketWriteDisabled != 0 {
+			sigs ^= zx.SignalSocketWriteDisabled
+			if err := eps.ep.Shutdown(tcpip.ShutdownRead); err != nil {
+				// Shutdown can return ErrNotConnected if the endpoint was connected
+				// but no longer is.
+				if err != tcpip.ErrNotConnected {
 					panic(err)
 				}
 			}
-
-			if obs&localSignalClosing != 0 {
-				// We're shutting down.
-				return
-			}
 		}
-	}()
 
-	{
-		// Synchronize with loopRead to ensure that:
-		//
-		// - we observe all incoming event notifications; such notifications can
-		// begin to arrive as soon as this function returns, so we must register
-		// before returning.
-		//
-		// - we correctly initialize the connected state before allowing it to be
-		// observed by the peer; the state is observable as soon as this function
-		// returns, so we must wait to be notified before returning.
-		initCh := make(chan struct{})
-		go func() {
-			inEntry, inCh := waiter.NewChannelEntry(nil)
-			eps.wq.EventRegister(&inEntry, waiter.EventIn)
-			defer eps.wq.EventUnregister(&inEntry)
-
-			eps.loopRead(inCh, initCh)
-		}()
-		<-initCh
+		if obs&localSignalClosing != 0 {
+			// We're shutting down.
+			return
+		}
 	}
-
-	go eps.loopWrite()
-
-	return eps, nil
 }
 
 type endpointWithEvent struct {
@@ -558,14 +542,23 @@ func (eps *endpointWithSocket) close() {
 			panic(err)
 		}
 
+		// Grab the loop channels _after_ having closed `eps.closing` to avoid a
+		// race in which the loops are allowed to start without guaranteeing that
+		// this routine will wait for them to return.
+		eps.mu.Lock()
+		channels := []<-chan struct{}{
+			eps.mu.loopReadDone,
+			eps.mu.loopWriteDone,
+			eps.mu.loopPollDone,
+		}
+		eps.mu.Unlock()
+
 		// The interruptions above cause our loops to exit. Wait until
 		// they do before releasing resources they may be using.
-		for _, ch := range []<-chan struct{}{
-			eps.loopReadDone,
-			eps.loopWriteDone,
-			eps.loopPollDone,
-		} {
-			<-ch
+		for _, ch := range channels {
+			if ch != nil {
+				<-ch
+			}
 		}
 
 		if err := eps.local.Close(); err != nil {
@@ -591,13 +584,120 @@ func (eps *endpointWithSocket) Listen(_ fidl.Context, backlog int16) (socket.Str
 	if backlog < 0 {
 		backlog = 0
 	}
+
 	if err := eps.ep.Listen(int(backlog)); err != nil {
 		return socket.StreamSocketListenResultWithErr(tcpipErrorToCode(err)), nil
 	}
 
+	// It is possible to call `listen` on a connected socket - such a call would
+	// fail above, so we register the callback only in the success case to avoid
+	// incorrectly handling events on connected sockets.
+	eps.onListen.Do(func() {
+		var entry waiter.Entry
+		cb := func() {
+			var err error
+			eps.incoming.mu.Lock()
+			if !eps.incoming.mu.asserted && eps.endpoint.ep.Readiness(waiter.EventIn) != 0 {
+				err = eps.local.Handle().SignalPeer(0, zxsocket.SignalIncoming)
+				eps.incoming.mu.asserted = true
+			}
+			eps.incoming.mu.Unlock()
+			if err != nil {
+				if err, ok := err.(*zx.Error); ok && err.Status == zx.ErrBadHandle {
+					// The endpoint is closing -- this is possible when an incoming
+					// connection races with the listening endpoint being closed.
+					go eps.wq.EventUnregister(&entry)
+				} else {
+					panic(err)
+				}
+			}
+		}
+		entry.Callback = callback(func(_ *waiter.Entry, m waiter.EventMask) {
+			if m&waiter.EventErr == 0 {
+				cb()
+			}
+		})
+		eps.wq.EventRegister(&entry, waiter.EventIn|waiter.EventErr)
+
+		// We're registering after calling Listen, so we might've missed an event.
+		// Call the callback once to check for already-present incoming
+		// connections.
+		cb()
+	})
+
 	syslog.VLogTf(syslog.DebugVerbosity, "listen", "%p: backlog=%d", eps, backlog)
 
 	return socket.StreamSocketListenResultWithResponse(socket.StreamSocketListenResponse{}), nil
+}
+
+func (eps *endpointWithSocket) startLoops(signals zx.Signals) {
+	eps.mu.Lock()
+	defer eps.mu.Unlock()
+	select {
+	case <-eps.closing:
+	default:
+		if err := eps.local.Handle().SignalPeer(0, signals); err != nil {
+			panic(err)
+		}
+		for _, m := range []struct {
+			done *<-chan struct{}
+			fn   func(chan<- struct{})
+		}{
+			{&eps.mu.loopPollDone, eps.loopPoll},
+			{&eps.mu.loopReadDone, eps.loopRead},
+			{&eps.mu.loopWriteDone, eps.loopWrite},
+		} {
+			ch := make(chan struct{})
+			*m.done = ch
+			go m.fn(ch)
+		}
+	}
+}
+
+func (eps *endpointWithSocket) Connect(ctx fidl.Context, address fidlnet.SocketAddress) (socket.BaseSocketConnectResult, error) {
+	result, err := eps.endpoint.Connect(ctx, address)
+	if err != nil {
+		return socket.BaseSocketConnectResult{}, err
+	}
+	switch result.Which() {
+	case socket.BaseSocketConnectResultErr:
+		if result.Err != posix.ErrnoEinprogress {
+			break
+		}
+		fallthrough
+	case socket.BaseSocketConnectResultResponse:
+		// It is possible to call `connect` on a listening socket - such a call
+		// would fail above, so we register the callback only in the success case
+		// to avoid incorrectly handling events on connected sockets.
+		eps.onConnect.Do(func() {
+			var (
+				once  sync.Once
+				entry waiter.Entry
+			)
+			cb := func(m waiter.EventMask) {
+				once.Do(func() {
+					var signals zx.Signals = zxsocket.SignalOutgoing
+					if m&waiter.EventErr == 0 {
+						signals |= zxsocket.SignalConnected
+					}
+					go eps.wq.EventUnregister(&entry)
+					eps.startLoops(signals)
+				})
+			}
+			entry.Callback = callback(func(_ *waiter.Entry, m waiter.EventMask) {
+				cb(m)
+			})
+			eps.wq.EventRegister(&entry, waiter.EventOut|waiter.EventErr)
+
+			// We're registering after calling Connect, so we might've missed an
+			// event. Call the callback once to check for an already-complete (even
+			// with error) handshake.
+			if m := eps.ep.Readiness(waiter.EventOut | waiter.EventErr); m != 0 {
+				cb(m)
+			}
+		})
+	}
+	return result, nil
 }
 
 func (eps *endpointWithSocket) Accept(wantAddr bool) (posix.Errno, *tcpip.FullAddress, *endpointWithSocket, error) {
@@ -649,16 +749,22 @@ func (eps *endpointWithSocket) Accept(wantAddr bool) (posix.Errno, *tcpip.FullAd
 		}
 	}
 	{
-		ep, err := newEndpointWithSocket(ep, wq, eps.transProto, eps.netProto, eps.endpoint.ns)
-		return 0, addr, ep, err
+		eps, err := newEndpointWithSocket(ep, wq, eps.transProto, eps.netProto, eps.endpoint.ns)
+		if err != nil {
+			return 0, nil, nil, err
+		}
+
+		eps.onConnect.Do(func() { eps.startLoops(zxsocket.SignalOutgoing | zxsocket.SignalConnected) })
+
+		return 0, addr, eps, nil
 	}
 }
 
 // loopWrite shuttles signals and data from the zircon socket to the tcpip.Endpoint.
-func (eps *endpointWithSocket) loopWrite() {
+func (eps *endpointWithSocket) loopWrite(ch chan<- struct{}) {
 	triggerClose := false
 	defer func() {
-		close(eps.loopWriteDone)
+		close(ch)
 		if triggerClose {
 			eps.close()
 		}
@@ -681,8 +787,12 @@ func (eps *endpointWithSocket) loopWrite() {
 				switch err.Status {
 				case zx.ErrBadState:
 					// Reading has been disabled for this socket endpoint.
-					if err := eps.ep.Shutdown(tcpip.ShutdownWrite); err != nil && err != tcpip.ErrNotConnected {
-						panic(err)
+					if err := eps.ep.Shutdown(tcpip.ShutdownWrite); err != nil {
+						// Shutdown can return ErrNotConnected if the endpoint was
+						// connected but no longer is.
+						if err != tcpip.ErrNotConnected {
+							panic(err)
+						}
 					}
 					return
 				case zx.ErrShouldWait:
@@ -722,6 +832,10 @@ func (eps *endpointWithSocket) loopWrite() {
 			}
 			// TODO(https://fxbug.dev/35006): Handle all transport write errors.
 			switch err {
+			case tcpip.ErrNotConnected:
+				// Write never returns ErrNotConnected except for endpoints that were
+				// never connected. Such endpoints should never reach this loop.
+				panic(fmt.Sprintf("connected endpoint returned %s", err))
 			case nil:
 				v = v[n:]
 				if len(v) != 0 {
@@ -768,37 +882,20 @@ func (eps *endpointWithSocket) loopWrite() {
 }
 
 // loopRead shuttles signals and data from the tcpip.Endpoint to the zircon socket.
-func (eps *endpointWithSocket) loopRead(inCh <-chan struct{}, initCh chan<- struct{}) {
+func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
 	triggerClose := false
 	defer func() {
-		close(eps.loopReadDone)
+		close(ch)
 		if triggerClose {
 			eps.close()
 		}
 	}()
 
-	initDone := func() {
-		if initCh != nil {
-			close(initCh)
-			initCh = nil
-		}
-	}
+	inEntry, inCh := waiter.NewChannelEntry(nil)
+	eps.wq.EventRegister(&inEntry, waiter.EventIn)
+	defer eps.wq.EventUnregister(&inEntry)
 
 	const sigs = zx.SignalSocketWritable | zx.SignalSocketWriteDisabled | localSignalClosing
-
-	outEntry, outCh := waiter.NewChannelEntry(nil)
-	connected := false
-	if !connected {
-		eps.wq.EventRegister(&outEntry, waiter.EventOut)
-		defer func() {
-			if !connected {
-				// If connected became true then we must have already unregistered
-				// below. We must never unregister the same entry twice because that
-				// can corrupt the waiter queue.
-				eps.wq.EventUnregister(&outEntry)
-			}
-		}()
-	}
 
 	writer := socketWriter{
 		socket: eps.local,
@@ -814,59 +911,14 @@ func (eps *endpointWithSocket) loopRead(inCh <-chan struct{}, initCh chan<- stru
 			hardError = eps.hardError.storeAndRetrieveLocked(err)
 		}
 		eps.hardError.mu.Unlock()
-		if err == tcpip.ErrNotConnected {
-			if connected {
-				panic(fmt.Sprintf("connected endpoint returned %s", err))
-			}
-			// We're not connected; unblock the caller before waiting for incoming packets.
-			initDone()
-			select {
-			case <-eps.closing:
-				// We're shutting down.
-				return
-			case <-inCh:
-				// We got an incoming connection; we must be a listening socket.
-				// Because we are a listening socket, we don't expect anymore
-				// outbound events so there's no harm in letting outEntry remain
-				// registered until the end of the function.
-				var err error
-				eps.incoming.mu.Lock()
-				if !eps.incoming.mu.asserted && eps.endpoint.ep.Readiness(waiter.EventIn) != 0 {
-					err = eps.local.Handle().SignalPeer(0, zxsocket.SignalIncoming)
-					eps.incoming.mu.asserted = true
-				}
-				eps.incoming.mu.Unlock()
-				if err != nil {
-					panic(err)
-				}
-				continue
-			case <-outCh:
-				// We became connected; the next Read will reflect this.
-				continue
-			}
-		} else if !connected {
-			var signals zx.Signals = zxsocket.SignalOutgoing
-			switch err {
-			case nil, tcpip.ErrWouldBlock, tcpip.ErrClosedForReceive:
-				connected = true
-				eps.wq.EventUnregister(&outEntry)
-
-				signals |= zxsocket.SignalConnected
-			}
-
-			if err := eps.local.Handle().SignalPeer(0, signals); err != nil {
-				panic(err)
-			}
-		}
-		// Either we're connected or not; unblock the caller.
-		initDone()
 		// TODO(https://fxbug.dev/35006): Handle all transport read errors.
 		switch err {
+		case tcpip.ErrNotConnected:
+			// Read never returns ErrNotConnected except for endpoints that were
+			// never connected. Such endpoints should never reach this loop.
+			panic(fmt.Sprintf("connected endpoint returned %s", err))
 		case tcpip.ErrNoLinkAddress:
-			// TODO(https://github.com/google/gvisor/issues/751): revisit this assertion.
-			if connected {
-				panic(fmt.Sprintf("Endpoint.Read() = %s on a connected socket should never happen", err))
-			}
+			// TODO(https://github.com/google/gvisor/issues/751): revisit this comment.
 			// At the time of writing, this error is only possible when link
 			// address resolution fails during an outbound TCP connection attempt.
 			// This happens via the following call hierarchy:
@@ -886,26 +938,15 @@ func (eps *endpointWithSocket) loopRead(inCh <-chan struct{}, initCh chan<- stru
 			// fails to respond to a SYN within 60 seconds, or if the retransmit
 			// logic gives up after 60 seconds of missing ACKs from the peer, or if
 			// the maximum number of unacknowledged keepalives is reached.
-			if connected {
-				// The connection was alive but now is dead - this is equivalent to
-				// having received a TCP RST.
-				triggerClose = true
-				return
-			}
-			// The connection was never created. This is equivalent to the
-			// connection having been refused.
-			fallthrough
+			//
+			// The connection was alive but now is dead - this is equivalent to
+			// having received a TCP RST.
+			triggerClose = true
+			return
 		case tcpip.ErrConnectionRefused:
-			// Linux allows sockets with connection errors to be reused. If the
-			// client calls connect() again (and the underlying Endpoint correctly
-			// permits the attempt), we need to wait for an outbound event again.
-			select {
-			case <-outCh:
-				continue
-			case <-eps.closing:
-				// We're shutting down.
-				return
-			}
+			// TODO(https://fxbug.dev/61594): Allow the socket to be reused for
+			// another connection attempt to match Linux.
+			return
 		case tcpip.ErrWouldBlock:
 			select {
 			case <-inCh:
@@ -949,7 +990,7 @@ func (eps *endpointWithSocket) loopRead(inCh <-chan struct{}, initCh chan<- stru
 						// allow the pending data to be read without error but will
 						// return ErrNotConnected if Shutdown is called. Otherwise
 						// this is unexpected, panic.
-						if !(connected && err == tcpip.ErrNotConnected) {
+						if err != tcpip.ErrNotConnected {
 							panic(err)
 						}
 						syslog.InfoTf("loopRead", "%p: client shutdown a closed endpoint; ep info: %#v", eps, eps.endpoint.ep.Info())

src/connectivity/network/netstack/netstack_test.go
@@ -259,15 +259,17 @@ func TestEndpoint_Close(t *testing.T) {
 	}
 	defer eps.close()
 
+	eps.mu.Lock()
 	channels := []struct {
 		ch   <-chan struct{}
 		name string
 	}{
 		{ch: eps.closing, name: "closing"},
-		{ch: eps.loopReadDone, name: "loopReadDone"},
-		{ch: eps.loopWriteDone, name: "loopWriteDone"},
-		{ch: eps.loopPollDone, name: "loopPollDone"},
+		{ch: eps.mu.loopReadDone, name: "loopReadDone"},
+		{ch: eps.mu.loopWriteDone, name: "loopWriteDone"},
+		{ch: eps.mu.loopPollDone, name: "loopPollDone"},
 	}
+	eps.mu.Unlock()
 
 	// Check starting conditions.
 	for _, ch := range channels {
@@ -370,10 +372,12 @@ func TestEndpoint_Close(t *testing.T) {
 	timeout := make(chan struct{})
 	time.AfterFunc(5*time.Second, func() { close(timeout) })
 	for _, ch := range channels {
-		select {
-		case <-ch.ch:
-		case <-timeout:
-			t.Errorf("%s not cleaned up", ch.name)
+		if ch.ch != nil {
+			select {
+			case <-ch.ch:
+			case <-timeout:
+				t.Errorf("%s not cleaned up", ch.name)
+			}
 		}
 	}
 
@@ -472,24 +476,28 @@ func TestTCPEndpointMapAcceptAfterReset(t *testing.T) {
 		}
 		defer eps.close()
 
+		eps.mu.Lock()
 		channels := []struct {
 			ch   <-chan struct{}
 			name string
 		}{
 			{ch: eps.closing, name: "closing"},
-			{ch: eps.loopReadDone, name: "loopReadDone"},
-			{ch: eps.loopWriteDone, name: "loopWriteDone"},
-			{ch: eps.loopPollDone, name: "loopPollDone"},
+			{ch: eps.mu.loopReadDone, name: "loopReadDone"},
+			{ch: eps.mu.loopWriteDone, name: "loopWriteDone"},
+			{ch: eps.mu.loopPollDone, name: "loopPollDone"},
 		}
+		eps.mu.Unlock()
 
 		// Give a generous timeout for the closed channel to be detected.
 		timeout := make(chan struct{})
 		time.AfterFunc(5*time.Second, func() { close(timeout) })
 		for _, ch := range channels {
-			select {
-			case <-ch.ch:
-			case <-timeout:
-				t.Errorf("%s not cleaned up", ch.name)
+			if ch.ch != nil {
+				select {
+				case <-ch.ch:
+				case <-timeout:
+					t.Errorf("%s not cleaned up", ch.name)
+				}
 			}
 		}
 

commit 29cad579f25cfe959f95ce75fb07387569efbf61

[netstack] Handle listen/close race
Fix a bug introduced by 69db912e2aae that can cause a netstack panic
when a listening socket is closed while a passive connection handshake
gets completed. Handle zx.ErrPeerClosed error along with zx.ErrBadHandle
while processing the incoming connection event notification on the
listening endpoint.

Fixed: 70626
Test: fx test -od netstack_bsdsocket_c_api_test -- \
--gtest_filter=NetStreamTest.CloseDuringConnect --gtest_repeat=1000

Change-Id: Ibdc12ffa2accd75caefb9782ed268b8062e83cc2
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/488777
Commit-Queue: Mithun Iyer <iyerm@google.com>
Reviewed-by: Tamir Duberstein <tamird@google.com>

src/connectivity/network/netstack/fuchsia_posix_socket.go
@@ -645,7 +645,7 @@ func (eps *endpointWithSocket) Listen(_ fidl.Context, backlog int16) (socket.Str
 			}
 			eps.incoming.mu.Unlock()
 			if err != nil {
-				if err, ok := err.(*zx.Error); ok && err.Status == zx.ErrBadHandle {
+				if err, ok := err.(*zx.Error); ok && (err.Status == zx.ErrBadHandle || err.Status == zx.ErrPeerClosed) {
 					// The endpoint is closing -- this is possible when an incoming
 					// connection races with the listening endpoint being closed.
 					go eps.wq.EventUnregister(&entry)