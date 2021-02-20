dfbe68f31f3b8c5016076ad2e958c1394548ee87
Casey Borders <CaseyBorders@google.com>
2021-02-20 01:09:24 +0000

    [session][ffx] Add ffx session launch command
    Add a command to the ffx session subcommand that can be used to launch a
    new session
    
    Bug: 70551
    Change-Id: Ieac89d138912dab5cd56218ffb97c49d713133a9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/488533
    Reviewed-by: Matthew Boetger <boetger@google.com>
    Reviewed-by: Rich Kadel <richkadel@google.com>
    Commit-Queue: Casey Borders <casbor@google.com>

---
+++ src/developer/ffx/plugins/session/launch/src/args.rs
@@ -0,0 +1,13 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {argh::FromArgs, ffx_core::ffx_command};
+
+#[ffx_command()]
+#[derive(FromArgs, Debug, PartialEq)]
+#[argh(subcommand, name = "launch", description = "Launch a session")]
+pub struct SessionLaunchCommand {
+    #[argh(positional)]
+    pub url: String,
+}
---
+++ src/developer/ffx/plugins/session/launch/src/lib.rs
@@ -0,0 +1,40 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{format_err, Result},
+    ffx_core::ffx_plugin,
+    ffx_session_launch_args::SessionLaunchCommand,
+    fidl_fuchsia_session::{LaunchConfiguration, LauncherProxy},
+};
+
+#[ffx_plugin(LauncherProxy = "core/session-manager:expose:fuchsia.session.Launcher")]
+pub async fn launch(launcher_proxy: LauncherProxy, cmd: SessionLaunchCommand) -> Result<()> {
+    println!("Launching session: {}", cmd.url);
+    let config = LaunchConfiguration { session_url: Some(cmd.url), ..LaunchConfiguration::EMPTY };
+    launcher_proxy.launch(config).await?.map_err(|err| format_err!("{:?}", err))
+}
+
+#[cfg(test)]
+mod test {
+    use {super::*, fidl_fuchsia_session::LauncherRequest};
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_launch_session() {
+        const SESSION_URL: &str = "Session URL";
+
+        let proxy = setup_fake_launcher_proxy(|req| match req {
+            LauncherRequest::Launch { configuration, responder } => {
+                assert!(configuration.session_url.is_some());
+                let session_url = configuration.session_url.unwrap();
+                assert!(session_url == SESSION_URL.to_string());
+                let _ = responder.send(&mut Ok(()));
+            }
+        });
+
+        let launch_cmd = SessionLaunchCommand { url: SESSION_URL.to_string() };
+        let result = launch(proxy, launch_cmd).await;
+        assert!(result.is_ok());
+    }
+}
