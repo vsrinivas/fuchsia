1097c10f27b733e9642f4da6bf0c03f8b33c9580
Roland McGrath <mcgrathr@google.com>
2021-02-23 04:41:00 +0000

    [kernel][boot-options] Add `k showopt` and `k setopt` commands
    Bug: 53593
    Change-Id: Ic91f477b1950104626c3b5047c0c130800bf076b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/396534
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

---
+++ zircon/kernel/lib/boot-options/console.cc
@@ -0,0 +1,45 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/boot-options/boot-options.h>
+#include <lib/console.h>
+
+#include <ktl/span.h>
+#include <ktl/string_view.h>
+
+namespace {
+
+// Note that using this can introduce data races on the member variables.
+int Set(int argc, const cmd_args* argv, uint32_t flags) {
+  if (argc < 2) {
+    printf("Usage: %s <key>[=<value>]...\n", argv[0].str);
+    return -1;
+  }
+
+  for (const auto& arg : ktl::span(argv, argc).subspan(1)) {
+    gBootOptions->SetMany(arg.str, stdout);
+  }
+
+  return 0;
+}
+
+int Show(int argc, const cmd_args* argv, uint32_t flags) {
+  if (argc > 1) {
+    int result = 0;
+    for (const auto& arg : ktl::span(argv, argc).subspan(1)) {
+      result |= gBootOptions->Show(std::string_view{arg.str});
+    }
+    return result;
+  }
+
+  gBootOptions->Show();
+  return 0;
+}
+
+}  // namespace
+
+STATIC_COMMAND_START
+STATIC_COMMAND("setopt", "Set boot options (as from kernel cmdline)", Set)
+STATIC_COMMAND("showopt", "Show boot options (from kernel cmdline)", Show)
+STATIC_COMMAND_END(options)
