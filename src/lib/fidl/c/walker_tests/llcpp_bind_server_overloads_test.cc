5583e1a145803c7a05da5ff52e28a3b546c77789
Yifei Teng <yifeit@google.com>
2021-02-23 20:07:03 +0000

    [fidl][llcpp] Flexible BindServer method signature
    Previously, there were three ways to invoke BindServer:
    
    - Call it with a raw pointer to the server, and no unbound hook.
    - Call it with a unique_ptr to the server, and no unbound hook.
    - Call it with a raw pointer to the server, with unbound hook.
    
    This is not a scalable design: we either cannot support the unbound hook
    in the unique_ptr overload, or have to add one more dedicated overload.
    
    We have also seen code in the wild that uses a shared_ptr, but
    could only borrow a raw pointer into BindServer.
    
    To address these pain points and the matrix of pointer type vs
    with/without unbound hook, we can employ a two staged BindServer call.
    The first stage disambiguates the pointer type, and the second stage
    selects with/without unbound hook.
    
    This CL previously also split up `server.h` into a public and internal
    header, as well as did another pass over the documentation. Those are
    now split into the next two CLs for ease of review.
    
    TEST: added dedicated tests for each case in the matrix.
    Change-Id: Id93326fd1e48f846d19bb8c9b0bda745774f1c2b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/488737
    Reviewed-by: Ian McKellar <ianloic@google.com>
    Reviewed-by: Vincent Belliard <vbelliard@google.com>
    Commit-Queue: Yifei Teng <yifeit@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>

---
+++ src/lib/fidl/c/walker_tests/llcpp_bind_server_overloads_test.cc
@@ -0,0 +1,195 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
+#include <lib/fidl/llcpp/server.h>
+#include <lib/sync/completion.h>
+
+#include <fidl/test/coding/fuchsia/llcpp/fidl.h>
+#include <zxtest/zxtest.h>
+
+namespace {
+
+using ::llcpp::fidl::test::coding::fuchsia::Simple;
+
+class Server : public Simple::Interface {
+ public:
+  explicit Server(sync_completion_t* destroyed) : destroyed_(destroyed) {}
+  Server(Server&& other) = delete;
+  Server(const Server& other) = delete;
+  Server& operator=(Server&& other) = delete;
+  Server& operator=(const Server& other) = delete;
+
+  ~Server() override { sync_completion_signal(destroyed_); }
+
+  void Echo(int32_t request, EchoCompleter::Sync& completer) override { ZX_PANIC("Never used"); }
+  void Close(CloseCompleter::Sync& completer) override { ZX_PANIC("Never used"); }
+
+ private:
+  sync_completion_t* destroyed_;
+};
+
+class BindServerOverloads : public zxtest::Test {
+ public:
+  BindServerOverloads()
+      : loop_(&kAsyncLoopConfigNoAttachToCurrentThread),
+        endpoints_(fidl::CreateEndpoints<Simple>()) {}
+
+  void SetUp() override {
+    ASSERT_OK(loop_.StartThread());
+    ASSERT_OK(endpoints_.status_value());
+  }
+
+  zx::status<fidl::Endpoints<Simple>>& endpoints() { return endpoints_; }
+
+  async_dispatcher_t* dispatcher() { return loop_.dispatcher(); }
+
+  async::Loop& loop() { return loop_; }
+
+ private:
+  async::Loop loop_;
+  zx::status<fidl::Endpoints<Simple>> endpoints_;
+};
+
+// Test that |BindServer| correctly destroys a server it uniquely owns.
+TEST_F(BindServerOverloads, UniquePtrWithoutUnboundHook) {
+  sync_completion_t destroyed;
+  sync_completion_t unbound;
+  auto result = fidl::BindServer(dispatcher(), std::move(endpoints()->server),
+                                 std::make_unique<Server>(&destroyed));
+
+  // Trigger binding destruction before loop's destruction.
+  endpoints()->client.reset();
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+// Test that |BindServer| correctly destroys a server it uniquely owns,
+// and that the |on_unbound| hook is executed before server destruction.
+TEST_F(BindServerOverloads, UniquePtrWithUnboundHook) {
+  sync_completion_t destroyed;
+  sync_completion_t unbound;
+  auto result = fidl::BindServer(
+      dispatcher(), std::move(endpoints()->server), std::make_unique<Server>(&destroyed),
+      [&unbound, &destroyed](Server*, fidl::UnbindInfo info, fidl::ServerEnd<Simple> server_end) {
+        // Server is held alive by the runtime until we leave this lambda.
+        ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+        sync_completion_signal(&unbound);
+      });
+
+  // Trigger binding destruction before loop's destruction.
+  endpoints()->client.reset();
+  ASSERT_OK(sync_completion_wait(&unbound, ZX_TIME_INFINITE));
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+// Test that |BindServer| correctly destroys a server it uniquely owns
+// via a |shared_ptr|.
+TEST_F(BindServerOverloads, SharedPtrWithoutUnboundHook) {
+  sync_completion_t destroyed;
+  sync_completion_t unbound;
+  auto result = fidl::BindServer(dispatcher(), std::move(endpoints()->server),
+                                 std::make_shared<Server>(&destroyed));
+
+  // Trigger binding destruction before loop's destruction.
+  endpoints()->client.reset();
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+// Test that |BindServer| correctly destroys a server it uniquely owns
+// via a |shared_ptr|, and that the |on_unbound| hook is executed before
+// server destruction.
+TEST_F(BindServerOverloads, SharedPtrWithUnboundHook) {
+  sync_completion_t destroyed;
+  sync_completion_t unbound;
+  auto result = fidl::BindServer(
+      dispatcher(), std::move(endpoints()->server), std::make_shared<Server>(&destroyed),
+      [&unbound, &destroyed](Server*, fidl::UnbindInfo info, fidl::ServerEnd<Simple> server_end) {
+        // Server is held alive by the runtime until we leave this lambda.
+        ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+        sync_completion_signal(&unbound);
+      });
+
+  // Trigger binding destruction before loop's destruction.
+  endpoints()->client.reset();
+  ASSERT_OK(sync_completion_wait(&unbound, ZX_TIME_INFINITE));
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+// Test that |BindServer| does not prematurely destroy a server managed by
+// |shared_ptr| when there are still outstanding references.
+TEST_F(BindServerOverloads, SharedPtrWithUnboundHookAndSharedOwnership) {
+  sync_completion_t destroyed;
+  sync_completion_t unbound;
+  auto shared_server = std::make_shared<Server>(&destroyed);
+  auto result = fidl::BindServer(
+      dispatcher(), std::move(endpoints()->server), shared_server,
+      [&unbound, &destroyed](Server*, fidl::UnbindInfo info, fidl::ServerEnd<Simple> server_end) {
+        // Server is held alive by the runtime until we leave this lambda.
+        ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+        sync_completion_signal(&unbound);
+      });
+
+  // Trigger binding destruction before loop's destruction.
+  endpoints()->client.reset();
+  ASSERT_OK(sync_completion_wait(&unbound, ZX_TIME_INFINITE));
+
+  ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+  shared_server.reset();
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+// Test borrowing a server implementation with a raw pointer.
+TEST_F(BindServerOverloads, RawPtrWithoutUnboundHook) {
+  sync_completion_t destroyed;
+  sync_completion_t unbound;
+
+  fit::optional<Server> server{&destroyed};
+  auto result = fidl::BindServer(dispatcher(), std::move(endpoints()->server), &server.value());
+
+  // Trigger binding destruction before loop's destruction.
+  endpoints()->client.reset();
+  ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+  loop().Shutdown();
+  ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+  // Now it's safe to destroy |server|.
+  server.reset();
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+// Test borrowing a server implementation with a raw pointer, and supplying
+// an |on_unbound| hook.
+TEST_F(BindServerOverloads, RawPtrWithUnboundHook) {
+  sync_completion_t destroyed;
+  sync_completion_t unbound;
+
+  fit::optional<Server> server{&destroyed};
+  auto result = fidl::BindServer(
+      dispatcher(), std::move(endpoints()->server), &server.value(),
+      [&unbound, &destroyed](Server*, fidl::UnbindInfo info, fidl::ServerEnd<Simple> server_end) {
+        // Server is held alive by the local variable.
+        ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+        sync_completion_signal(&unbound);
+      });
+
+  // Trigger binding destruction before loop's destruction.
+  endpoints()->client.reset();
+  ASSERT_OK(sync_completion_wait(&unbound, ZX_TIME_INFINITE));
+  ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+  loop().Shutdown();
+  ASSERT_EQ(ZX_ERR_TIMED_OUT, sync_completion_wait(&destroyed, ZX_TIME_INFINITE_PAST));
+
+  // Now it's safe to destroy |server|.
+  server.reset();
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+}  // namespace
--- zircon/system/ulib/fidl/include/lib/fidl/llcpp/server.h
+++ zircon/system/ulib/fidl/include/lib/fidl/llcpp/server.h
@@ -36,9 +36,9 @@ class IncomingMessageDispatcher {
 };
 
 template <typename Protocol>
-fit::result<ServerBindingRef<Protocol>, zx_status_t> BindServerImpl(
+fit::result<ServerBindingRef<Protocol>, zx_status_t> BindServerTypeErased(
     async_dispatcher_t* dispatcher, fidl::ServerEnd<Protocol> server_end,
-    IncomingMessageDispatcher* interface, AnyOnUnboundFn on_unbound);
+    internal::IncomingMessageDispatcher* interface, internal::AnyOnUnboundFn on_unbound);
 
 // Defines an incoming method entry. Used by a server to dispatch an incoming message.
 struct MethodEntry {
@@ -103,15 +103,125 @@ class ServerBindingRef {
   const typename Protocol::WeakEventSender& operator*() const { return event_sender_; }
 
  private:
-  friend fit::result<ServerBindingRef<Protocol>, zx_status_t> internal::BindServerImpl<Protocol>(
-      async_dispatcher_t* dispatcher, fidl::ServerEnd<Protocol> server_end,
-      internal::IncomingMessageDispatcher* interface, internal::AnyOnUnboundFn on_unbound);
+  // This is so that only |BindServerTypeErased| will be able to construct a
+  // new instance of |ServerBindingRef|.
+  friend fit::result<ServerBindingRef<Protocol>, zx_status_t>
+  internal::BindServerTypeErased<Protocol>(async_dispatcher_t* dispatcher,
+                                           fidl::ServerEnd<Protocol> server_end,
+                                           internal::IncomingMessageDispatcher* interface,
+                                           internal::AnyOnUnboundFn on_unbound);
 
   explicit ServerBindingRef(std::weak_ptr<internal::AsyncServerBinding<Protocol>> internal_binding)
       : event_sender_(std::move(internal_binding)) {}
 
   typename Protocol::WeakEventSender event_sender_;
 };
+
+namespace internal {
+
+//
+// Definitions related to binding a connection to a dispatcher
+//
+
+// Binds an implementation of some FIDL server protocol |interface| and
+// |server_end| to the |dispatcher|.
+//
+// |interface| should be a pointer to some |Protocol::Interface| class.
+//
+// |IncomingMessageDispatcher::dispatch_message| looks up an incoming FIDL
+// message in the associated protocol and possibly invokes a handler on
+// |interface|, which will be provided as the first argument.
+//
+// |on_unbound| will be called with |interface| if |on_unbound| is specified.
+// The public |fidl::BindServer| functions should translate |interface| back to
+// the user pointer type, possibly at an offset, before invoking the
+// user-provided on-unbound handler.
+template <typename Protocol>
+fit::result<ServerBindingRef<Protocol>, zx_status_t> BindServerTypeErased(
+    async_dispatcher_t* dispatcher, fidl::ServerEnd<Protocol> server_end,
+    IncomingMessageDispatcher* interface, internal::AnyOnUnboundFn on_unbound) {
+  auto internal_binding = internal::AsyncServerBinding<Protocol>::Create(
+      dispatcher, std::move(server_end), interface, std::move(on_unbound));
+  auto status = internal_binding->BeginWait();
+  if (status != ZX_OK) {
+    return fit::error(status);
+  }
+  return fit::ok(fidl::ServerBindingRef<Protocol>(std::move(internal_binding)));
+}
+
+// All overloads of |BindServer| calls into this function.
+// This function exists to support deducing the |OnUnbound| type,
+// and type-erasing the interface and the |on_unbound| handlers, before
+// calling into |BindServerTypeErased|.
+//
+// Note: if you see a compiler error that ends up in this function, that is
+// probably because you passed in an incompatible |on_unbound| handler.
+template <typename ServerImpl, typename OnUnbound>
+fit::result<ServerBindingRef<typename ServerImpl::_EnclosingProtocol>, zx_status_t> BindServerImpl(
+    async_dispatcher_t* dispatcher,
+    fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol> server_end, ServerImpl* impl,
+    OnUnbound&& on_unbound) {
+  using ProtocolType = typename ServerImpl::_EnclosingProtocol;
+  return BindServerTypeErased<ProtocolType>(
+      dispatcher, std::move(server_end), impl,
+      [on_unbound = std::forward<OnUnbound>(on_unbound)](
+          internal::IncomingMessageDispatcher* any_interface, UnbindInfo info,
+          zx::channel channel) mutable {
+        // Note: this cast may change the value of the pointer, due to how C++
+        // implements classes with virtual tables.
+        auto* impl = static_cast<ServerImpl*>(any_interface);
+        std::invoke(on_unbound, impl, info, fidl::ServerEnd<ProtocolType>(std::move(channel)));
+      });
+}
+
+template <typename OnUnbound>
+using OnUnboundIsNull = std::is_same<std::remove_reference_t<OnUnbound>, std::nullptr_t>;
+
+// This base class provides either a functioning `operator()` or a no-op,
+// depending on whether the |OnUnbound| type is a nullptr.
+template <typename Derived, typename OnUnbound, typename Enable = void>
+struct UnboundThunkCallOperator;
+
+template <typename Derived, typename OnUnbound>
+struct UnboundThunkCallOperator<Derived, OnUnbound,
+                                std::enable_if_t<!OnUnboundIsNull<OnUnbound>::value>> {
+  template <typename ServerImpl>
+  void operator()(ServerImpl* impl_ptr, UnbindInfo info,
+                  fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol>&& server_end) {
+    static_assert(std::is_convertible_v<OnUnbound, OnUnboundFn<ServerImpl>>,
+                  "|on_unbound| must have the same signature as fidl::OnUnboundFn<ServerImpl>.");
+    auto* self = static_cast<Derived*>(this);
+    std::invoke(self->on_unbound_, impl_ptr, info, std::move(server_end));
+  }
+};
+
+template <typename Derived, typename OnUnbound>
+struct UnboundThunkCallOperator<Derived, OnUnbound,
+                                std::enable_if_t<OnUnboundIsNull<OnUnbound>::value>> {
+  template <typename ServerImpl>
+  void operator()(ServerImpl* impl_ptr, UnbindInfo info,
+                  fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol>&& server_end) {
+    // |fn_| is a nullptr, meaning the user did not provide an |on_unbound| callback.
+    static_assert(std::is_same_v<OnUnbound, std::nullptr_t>, "|on_unbound| is no-op here");
+  }
+};
+
+// An |UnboundThunk| is a functor that delegates to an |OnUnbound| callable,
+// and which ensures that the server implementation is only destroyed after
+// the invocation and destruction of the |OnUnbound| callable, when the server
+// is managed in a |shared_ptr| or |unique_ptr|.
+template <typename ServerImplMaybeOwned, typename OnUnbound>
+struct UnboundThunk
+    : public UnboundThunkCallOperator<UnboundThunk<ServerImplMaybeOwned, OnUnbound>, OnUnbound> {
+  UnboundThunk(ServerImplMaybeOwned&& impl, OnUnbound&& on_unbound)
+      : impl_(std::forward<ServerImplMaybeOwned>(impl)),
+        on_unbound_(std::forward<OnUnbound>(on_unbound)) {}
+
+  std::remove_reference_t<ServerImplMaybeOwned> impl_;
+  std::remove_reference_t<OnUnbound> on_unbound_;
+};
+
+}  // namespace internal
 
 // Binds an implementation of a low-level C++ server interface to |server_end| using a potentially
 // multi-threaded |dispatcher|. This implementation allows for multiple in-flight synchronously or
@@ -192,115 +302,43 @@ class ServerBindingRef {
 //     fidl::BindServer(dispatcher, std::move(server_end), this);
 //
 // TODO(fxbug.dev/67062): |fidl::BindServer| and associated API should return a |zx::status|.
-template <typename ServerImpl>
-fit::result<ServerBindingRef<typename ServerImpl::_EnclosingProtocol>, zx_status_t> BindServer(
-    async_dispatcher_t* dispatcher,
-    fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol> server_end, ServerImpl* impl) {
-  return internal::BindServerImpl<typename ServerImpl::_EnclosingProtocol>(
-      dispatcher, std::move(server_end), impl, nullptr);
-}
-
-// As above, but will invoke |on_unbound| on |impl| when the channel is being unbound, either due to
-// error or an explicit |Close| or |Unbind|.
-//
-// |on_unbound| should be a callable of the following signature:
-//
-//     template <typename T, typename ProtocolType = typename T::_EnclosingProtocol>
-//     void(T*, fidl::UnbindInfo, fidl::ServerEnd<ProtocolType>)
-//
-// where |T| is the type of the server implementation and |ProtocolType| is the
-// type of the FIDL protocol.
-//
-// NOTE: |on_unbound| will generally be executed from a |dispatcher| thread.
-// However, on |dispatcher| shutdown, any active bindings will be unbound, thus
-// it may also be executed on the thread invoking shutdown. The user must ensure
-// that shutdown is never invoked while holding locks which |on_unbound| may
-// also take.
-//
-// This function is able to infer the type of |ServerImpl| and |OnUnbound| in
-// most cases. The following code would compile without explicitly specializing
-// |BindServer|:
-//
-//     // Suppose |this| is a server implementation class |Foo|, that
-//     // implements the |Bar| FIDL protocol.
-//     fidl:ServerEnd<Bar> server_end = ...;
-//     fidl::BindServer(dispatcher, std::move(server_end), this,
-//                      [](Foo*, fidl::UnbindInfo, fidl::ServerEnd<Bar>) { ... });
-//
-template <typename ServerImpl, typename OnUnbound,
-          // Here are some SFINAE machinery to help check if a |Callable| type has the
-          // signature of |OnUnboundFn<ServerImpl>|.
-          //
-          // Because C++ does not consider implicit conversions when performing template
-          // parameter deduction, using SFINAE is our next best option.
-          typename = std::enable_if_t<
-              std::is_invocable_v<OnUnbound, ServerImpl*, UnbindInfo,
-                                  fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol>>>>
+template <typename ServerImpl, typename OnUnbound = std::nullptr_t>
 fit::result<ServerBindingRef<typename ServerImpl::_EnclosingProtocol>, zx_status_t> BindServer(
     async_dispatcher_t* dispatcher,
     fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol> server_end, ServerImpl* impl,
-    OnUnbound&& on_unbound) {
-  using ProtocolType = typename ServerImpl::_EnclosingProtocol;
-  return internal::BindServerImpl<ProtocolType>(
+    OnUnbound&& on_unbound = nullptr) {
+  return internal::BindServerImpl<ServerImpl>(
       dispatcher, std::move(server_end), impl,
-      [fn = std::forward<OnUnbound>(on_unbound)](internal::IncomingMessageDispatcher* any_interface,
-                                                 UnbindInfo info, zx::channel channel) mutable {
-        // Note: this cast may change the value of the pointer, due to how C++
-        // implements classes with virtual tables.
-        auto* impl = static_cast<ServerImpl*>(any_interface);
-        static_assert(
-            std::is_convertible_v<OnUnbound, OnUnboundFn<ServerImpl>>,
-            "|on_unbound| must have the same signature as fidl::OnUnboundFn<ServerImpl>.");
-        std::invoke(fn, impl, info, fidl::ServerEnd<ProtocolType>(std::move(channel)));
-      });
+      internal::UnboundThunk(std::move(impl), std::forward<OnUnbound>(on_unbound)));
 }
 
-// Similar to the first variant, however, the user gives the binding ownership of the server
-// implementation. In order to destroy the implementation on unbind, the unique_ptr is passed to a
-// hook which will be automatically invoked during unbinding.
-//
-// NOTE: The same restriction on |on_unbound| in the previous variant applies to ~Interface().
-template <typename ServerImpl>
+// Overload of |BindServer| that takes ownership of the server as a |unique_ptr|.
+// The pointer is destroyed on the same thread as the one calling |on_unbound|,
+// and happens right after |on_unbound|. See |BindServer| for details.
+template <typename ServerImpl, typename OnUnbound = std::nullptr_t>
 fit::result<ServerBindingRef<typename ServerImpl::_EnclosingProtocol>, zx_status_t> BindServer(
     async_dispatcher_t* dispatcher,
     fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol> server_end,
-    std::unique_ptr<ServerImpl> impl) {
+    std::unique_ptr<ServerImpl>&& impl, OnUnbound&& on_unbound = nullptr) {
   ServerImpl* impl_raw = impl.get();
-  return internal::BindServerImpl<typename ServerImpl::_EnclosingProtocol>(
+  return internal::BindServerImpl<ServerImpl>(
       dispatcher, std::move(server_end), impl_raw,
-      [impl = std::move(impl)](internal::IncomingMessageDispatcher* interface, UnbindInfo info,
-                               zx::channel channel) {});
+      internal::UnboundThunk(std::move(impl), std::forward<OnUnbound>(on_unbound)));
 }
 
-namespace internal {
-
-// Binds an implementation of some FIDL server protocol |interface| to |channel|.
-//
-// |interface| should be a pointer to some |FidlProtocol::Interface| class.
-//
-// |dispatch_fn| looks up an incoming FIDL message in the associated protocol
-// and possibly invokes a handler on |interface|, which will be provided as
-// the first argument.
-//
-// |on_unbound| will be called with |interface| if |on_unbound| is specified.
-// The public |fidl::BindServer| functions should translate |interface| back to
-// the user pointer type, possibly at an offset, before invoking the
-// user-provided on-unbound handler.
-template <typename Protocol>
-fit::result<ServerBindingRef<Protocol>, zx_status_t> BindServerImpl(
-    async_dispatcher_t* dispatcher, fidl::ServerEnd<Protocol> server_end,
-    IncomingMessageDispatcher* interface, internal::AnyOnUnboundFn on_unbound) {
-  auto internal_binding = internal::AsyncServerBinding<Protocol>::Create(
-      dispatcher, std::move(server_end), interface, std::move(on_unbound));
-  auto status = internal_binding->BeginWait();
-  if (status == ZX_OK) {
-    return fit::ok(fidl::ServerBindingRef<Protocol>(std::move(internal_binding)));
-  } else {
-    return fit::error(status);
-  }
+// Overload of |BindServer| that shares ownership of the server via a |shared_ptr|.
+// The pointer is destroyed on the same thread as the one calling |on_unbound|,
+// and happens right after |on_unbound|. See |BindServer| for details.
+template <typename ServerImpl, typename OnUnbound = std::nullptr_t>
+fit::result<ServerBindingRef<typename ServerImpl::_EnclosingProtocol>, zx_status_t> BindServer(
+    async_dispatcher_t* dispatcher,
+    fidl::ServerEnd<typename ServerImpl::_EnclosingProtocol> server_end,
+    std::shared_ptr<ServerImpl> impl, OnUnbound&& on_unbound = nullptr) {
+  ServerImpl* impl_raw = impl.get();
+  return internal::BindServerImpl<ServerImpl>(
+      dispatcher, std::move(server_end), impl_raw,
+      internal::UnboundThunk(std::move(impl), std::forward<OnUnbound>(on_unbound)));
 }
-
-}  // namespace internal
 
 }  // namespace fidl
 
