af510b848c70ab8e3ea0cee21780196bf0c228da
Xo Wang <xow@google.com>
2021-02-23 05:53:43 +0000

    [bt][fidl] Add Host/WatchPeers fuzzer
    Add a fuzzer that creates and mutates a peer in an Adapter's peer cache
    then checks that it is succesfully received as a response to WatchPeers.
    
    Bug: fxbug.dev/66355, fxbug.dev/59064
    Test: on a non-fuzz build, fx test host_server_watch_peers_fuzzer_test
    on a fuzz build, "fx fuzz start bt-host-fuzzers" then select
    host_server_watch_peers_fuzzer
    
    Change-Id: Ic6003be774ccf4954d24d8ea8bd10cf0a55cf366
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/490184
    Commit-Queue: Xo Wang <xow@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>

---
+++ src/connectivity/bluetooth/core/bt-host/fidl/host_server_watch_peers_fuzztest.cc
@@ -0,0 +1,100 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "src/connectivity/bluetooth/core/bt-host/fidl/adapter_test_fixture.h"
+#include "src/connectivity/bluetooth/core/bt-host/fidl/host_server.h"
+#include "src/connectivity/bluetooth/core/bt-host/gatt_host.h"
+#include "src/connectivity/bluetooth/core/bt-host/testing/peer_fuzzer.h"
+
+namespace bthost {
+namespace {
+
+class HostServerFuzzTest final : public bthost::testing::AdapterTestFixture {
+ public:
+  HostServerFuzzTest() { SetUp(); }
+
+  ~HostServerFuzzTest() override {
+    host_ = nullptr;
+    host_server_ = nullptr;
+    gatt_host_ = nullptr;
+    AdapterTestFixture::TearDown();
+  }
+
+  // Creates a peer with fuzzer-generated mutations that will be encoded as a
+  // fuchsia.bluetooth.sys.Peer and sent as a response to WatchPeers immediately (see
+  // FIDL_HostServerTest.WatchPeersRepliesOnFirstCallWithExistingPeers).
+  void FuzzWatchPeers(FuzzedDataProvider &fuzzed_data_provider) {
+    // WatchPeers only tracks connectable peers by design
+    bt::gap::Peer *const peer =
+        adapter()->peer_cache()->NewPeer(bt::testing::MakePublicDeviceAddress(fuzzed_data_provider),
+                                         /*connectable=*/true);
+    ZX_ASSERT(peer);
+    bt::gap::testing::PeerFuzzer peer_fuzzer(fuzzed_data_provider, *peer);
+    while (fuzzed_data_provider.remaining_bytes() != 0) {
+      peer_fuzzer.FuzzOneField();
+    }
+
+    // TODO(fxbug.dev/65461): WatchPeers will trigger this test as a failure if we try to encode a
+    // lot of peers, even though fuzzing multiple peers would be helpful.
+    int watch_peers_responses = 0;
+    host_client()->WatchPeers([this, peer, &watch_peers_responses](auto updated, auto removed) {
+      ZX_ASSERT_MSG(updated.size() == 1, "peer %s: peers updated = %zu", bt_str(*peer),
+                    updated.size());
+      ZX_ASSERT_MSG(removed.size() == 0, "peer %s: peers removed = %zu", bt_str(*peer),
+                    removed.size());
+      HandleWatchPeersResponse(*host_client(), watch_peers_responses, /*max_call_depth=*/1,
+                               std::move(updated), std::move(removed));
+    });
+    RunLoopUntilIdle();
+    ZX_ASSERT_MSG(watch_peers_responses == 1, "peer %s: WatchPeers returned %d times",
+                  bt_str(*peer), watch_peers_responses);
+  }
+
+ private:
+  HostServer *host_server() const { return host_server_.get(); }
+
+  fuchsia::bluetooth::host::Host *host_client() const { return host_.get(); }
+
+  void SetUp() override {
+    AdapterTestFixture::SetUp();
+    gatt_host_ = std::make_unique<GattHost>(take_gatt());
+    fidl::InterfaceHandle<fuchsia::bluetooth::host::Host> host_handle;
+    host_server_ = std::make_unique<HostServer>(host_handle.NewRequest().TakeChannel(),
+                                                adapter()->AsWeakPtr(), gatt_host_->AsWeakPtr());
+    host_.Bind(std::move(host_handle));
+  }
+
+  // WatchPeers response handler that can re-initiate the call per the "hanging get" pattern up to
+  // |max_call_depth| times, like a normal client might.
+  template <typename Host>
+  void HandleWatchPeersResponse(Host &host, int &call_counter, int max_call_depth,
+                                std::vector<fuchsia::bluetooth::sys::Peer> updated,
+                                std::vector<fuchsia::bluetooth::PeerId> removed) {
+    call_counter++;
+    ZX_ASSERT_MSG(call_counter <= max_call_depth, "max depth (%d) exceeded", call_counter);
+    host.WatchPeers([this, &host, &call_counter, max_call_depth](auto updated, auto removed) {
+      this->HandleWatchPeersResponse(host, call_counter, max_call_depth, std::move(updated),
+                                     std::move(removed));
+    });
+  }
+
+  // No-op impl for the GoogleTest base class
+  void TestBody() override {}
+
+  std::unique_ptr<GattHost> gatt_host_;
+  std::unique_ptr<HostServer> host_server_;
+  fuchsia::bluetooth::host::HostPtr host_;
+};
+
+}  // namespace
+}  // namespace bthost
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  FuzzedDataProvider fuzzed_data_provider(data, size);
+  bthost::HostServerFuzzTest host_server_fuzz_test;
+  host_server_fuzz_test.FuzzWatchPeers(fuzzed_data_provider);
+  return 0;
+}
