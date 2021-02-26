8c7b824af33638c51c6bc7db52a158dc91c8711a
Sam Hansen <hansens@google.com>
2021-02-26 18:39:51 +0000

    [usb][xhci] Migrate xhci to new bind mechanism
    Change-Id: I313eed4dd6dd6580cb8d790e6c429f733fa66135
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/489306
    Commit-Queue: Sam Hansen <hansens@google.com>
    Reviewed-by: Alex Legg <alexlegg@google.com>

--- src/devices/usb/drivers/xhci/usb-xhci.cc
+++ src/devices/usb/drivers/xhci/usb-xhci.cc
@@ -17,7 +17,6 @@
 #include <algorithm>
 #include <memory>
 
-#include <ddk/binding.h>
 #include <ddk/debug.h>
 #include <ddk/platform-defs.h>
 #include <fbl/alloc_checker.h>
@@ -25,6 +24,7 @@
 #include <hw/arch_ops.h>
 #include <hw/reg.h>
 
+#include "src/devices/usb/drivers/xhci/usb_xhci_bind.h"
 #include "xhci-device-manager.h"
 #include "xhci-root-hub.h"
 #include "xhci-util.h"
@@ -455,30 +455,4 @@ static constexpr zx_driver_ops_t driver_ops = []() {
 
 }  // namespace usb_xhci
 
-// clang-format off
-ZIRCON_DRIVER_BEGIN(usb_xhci, usb_xhci::driver_ops, "zircon", "0.1", 17)
-    BI_GOTO_IF(EQ, BIND_PROTOCOL, ZX_PROTOCOL_PDEV, 0),
-    BI_GOTO_IF(EQ, BIND_COMPOSITE, 1, 1),
-
-    // PCI binding support
-    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_PCI),
-    BI_ABORT_IF(NE, BIND_PCI_CLASS, 0x0C),
-    BI_ABORT_IF(NE, BIND_PCI_SUBCLASS, 0x03),
-    BI_MATCH_IF(EQ, BIND_PCI_INTERFACE, 0x30),
-    BI_ABORT(),
-
-    // platform bus support
-    BI_LABEL(0),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_GENERIC),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_PID, PDEV_PID_GENERIC),
-    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, PDEV_DID_USB_XHCI),
-    BI_ABORT(),
-
-    // composite binding support
-    BI_LABEL(1),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, PDEV_VID_GENERIC),
-    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_PID, PDEV_PID_GENERIC),
-    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, PDEV_DID_USB_XHCI_COMPOSITE),
-
-    BI_ABORT(),
-ZIRCON_DRIVER_END(usb_xhci)
+ZIRCON_DRIVER(usb_xhci, usb_xhci::driver_ops, "zircon", "0.1");
