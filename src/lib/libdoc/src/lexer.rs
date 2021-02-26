1b150b88d3e12570ca4bd9ea2d353e828f195f53
Vincent Belliard <vbelliard@google.com>
2021-02-26 06:40:15 +0000

    [libdoc] more lexer
    Adds numbers and references.
    
    Renames Text into Name.
    
    Reorder spaces to have a consistent ordering for the whole file.
    
    Bug: 71029
    Change-Id: I07159788cbf291eacdb06c9fe6795f1130174f40
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/491122
    Commit-Queue: Vincent Belliard <vbelliard@google.com>
    Reviewed-by: Dave Schuyler <dschuyler@google.com>

--- src/lib/libdoc/src/lexer.rs
+++ src/lib/libdoc/src/lexer.rs
@@ -13,16 +13,31 @@ use std::str::CharIndices;
 
 /// Defines all the lexical items we can parse.
 pub enum LexicalContent {
-    /// Some text. It can be any text the lexer can isolate.
-    /// For example: "hello", "+", "++", "&", ...
-    Text(String),
+    /// A number. For example:
+    /// - 1234
+    /// - 0xabcd
+    Number(String),
+
+    /// A name from an English text point of view. For example:
+    /// - Hello
+    /// - heap-specific
+    /// - doesn't
+    Name(String),
+
+    /// A reference to an existing applicative concept like a method name, a field name, ...
+    /// A reference is held between back quotes.
+    Reference(String),
+
     /// The end of an english sentence.
     /// For example ".", ":", "!", "?".
     EndOfSentence(char),
+
     /// Some consecutive blank spaces.
     Spaces(u32),
+
     /// Some consecutive new lines.
     NewLines(u32),
+
     /// The end of the documentation. The parser generates one and only one EndOfInput.
     /// This is always the last item in the list.
     EndOfInput,
@@ -44,11 +59,9 @@ pub fn reduce_lexems(compiler: &mut DocCompiler, source: &Rc<Source>) -> Option<
     let mut current = iter.next();
     while let Some((index, character)) = current {
         current = match character {
-            // Reduces a name.
+            '0'..='9' => reduce_number_or_name(&mut items, &source, index, character, &mut iter),
             'a'..='z' | 'A'..='Z' | '_' => reduce_name(&mut items, &source, index, &mut iter),
-            // Reduces spaces.
-            ' ' => reduce_spaces(&mut items, &source, index, &mut iter),
-            // Reduces a sentence end character.
+            '`' => reduce_reference(compiler, &mut items, &source, &mut iter),
             '.' | ':' | '!' | '?' => {
                 items.push(LexicalItem {
                     location: Location { source: Rc::clone(&source), start: index, end: index },
@@ -56,7 +69,7 @@ pub fn reduce_lexems(compiler: &mut DocCompiler, source: &Rc<Source>) -> Option<
                 });
                 iter.next()
             }
-            // Reduces new lines.
+            ' ' => reduce_spaces(&mut items, &source, index, &mut iter),
             '\n' => reduce_new_lines(&mut items, &source, index, &mut iter),
             // Unknown character.
             _ => {
@@ -86,9 +99,76 @@ pub fn reduce_lexems(compiler: &mut DocCompiler, source: &Rc<Source>) -> Option<
     }
 }
 
+/// Reduces a number.
+///
+/// For example:
+/// - 1234
+/// - 0xabcd
+///
+/// If we find characters which are not valid for a number but valid for a name then, the whole
+/// sequence of characters (including the leading digits) are used to return a Name.
+///
+/// For example:
+/// - 123abc
+fn reduce_number_or_name(
+    items: &mut Vec<LexicalItem>,
+    source: &Rc<Source>,
+    start: usize,
+    first_character: char,
+    iter: &mut CharIndices<'_>,
+) -> Option<(usize, char)> {
+    let mut hexadecimal = false;
+    let mut number = true;
+    let mut current: Option<(usize, char)> = iter.next();
+    if first_character == '0' {
+        // Checks for an hexadecimal number.
+        if let Some((_, character)) = current {
+            if character == 'x' || character == 'X' {
+                hexadecimal = true;
+                current = iter.next();
+            }
+        }
+    }
+    let end = loop {
+        match current {
+            Some((index, character)) => match character {
+                '0'..='9' => {}
+                'a'..='f' | 'A'..='F' => {
+                    if !hexadecimal {
+                        number = false;
+                    }
+                }
+                'g'..='z' | 'G'..='Z' | '_' | '-' | '\'' => number = false,
+                _ => {
+                    break index;
+                }
+            },
+            None => {
+                break source.text.len();
+            }
+        }
+        current = iter.next();
+    };
+    if number {
+        // Only digits (potentially hexadecimal) have been found => Number.
+        items.push(LexicalItem {
+            location: Location { source: Rc::clone(&source), start, end },
+            content: LexicalContent::Number(source.text[start..end].to_string()),
+        });
+    } else {
+        // Not a valid number => Name.
+        items.push(LexicalItem {
+            location: Location { source: Rc::clone(&source), start, end },
+            content: LexicalContent::Name(source.text[start..end].to_string()),
+        });
+    }
+    current
+}
+
 /// Reduces a name.
 ///
 /// A name is a valid name from the English language point of view.
+///
 /// Examples of valid names:
 /// - Hello
 /// - heap-specific
@@ -116,8 +196,43 @@ fn reduce_name(
     };
     items.push(LexicalItem {
         location: Location { source: Rc::clone(&source), start, end },
-        content: LexicalContent::Text(source.text[start..end].to_string()),
+        content: LexicalContent::Name(source.text[start..end].to_string()),
     });
+    current
+}
+
+/// Reduces a reference (text between back quotes).
+fn reduce_reference(
+    compiler: &mut DocCompiler,
+    items: &mut Vec<LexicalItem>,
+    source: &Rc<Source>,
+    iter: &mut CharIndices<'_>,
+) -> Option<(usize, char)> {
+    let mut current = iter.next();
+    let start = if let Some((index, _)) = current { index } else { 0 };
+    loop {
+        match current {
+            Some((index, character)) => match character {
+                '`' => {
+                    items.push(LexicalItem {
+                        location: Location { source: Rc::clone(&source), start, end: index },
+                        content: LexicalContent::Reference(source.text[start..index].to_string()),
+                    });
+                    current = iter.next();
+                    break;
+                }
+                _ => {}
+            },
+            None => {
+                compiler.add_error(
+                    &Location { source: Rc::clone(&source), start, end: source.text.len() },
+                    "Unterminated reference".to_owned(),
+                );
+                break;
+            }
+        }
+        current = iter.next();
+    }
     current
 }
 
@@ -195,22 +310,41 @@ mod test {
     use crate::DocCompiler;
     use std::rc::Rc;
 
-    fn lexical_items_to_errors(compiler: &mut DocCompiler, items: &Vec<LexicalItem>) {
+    /// Convert the lexical items to text to be able to check them.
+    fn lexical_items_to_errors(
+        compiler: &mut DocCompiler,
+        items: &Vec<LexicalItem>,
+        with_spaces: bool,
+    ) {
         for item in items.iter() {
             match &item.content {
-                LexicalContent::Text(text) => {
-                    compiler.add_error(&item.location, format!("Text <{}>", text))
+                LexicalContent::Number(text) => {
+                    compiler.add_error(&item.location, format!("Number <{}>", text))
+                }
+                LexicalContent::Name(text) => {
+                    compiler.add_error(&item.location, format!("Name <{}>", text))
+                }
+                LexicalContent::Reference(text) => {
+                    compiler.add_error(&item.location, format!("Reference <{}>", text))
                 }
                 LexicalContent::EndOfSentence(character) => {
                     compiler.add_error(&item.location, format!("EndOfSentence <{}>", character))
                 }
                 LexicalContent::Spaces(count) => {
-                    compiler.add_error(&item.location, format!("Spaces ({})", count))
+                    if with_spaces {
+                        compiler.add_error(&item.location, format!("Spaces ({})", count));
+                    }
                 }
                 LexicalContent::NewLines(count) => {
-                    compiler.add_error(&item.location, format!("NewLines ({})", count))
+                    if with_spaces {
+                        compiler.add_error(&item.location, format!("NewLines ({})", count));
+                    }
                 }
-                LexicalContent::EndOfInput => compiler.add_error(&item.location, "End".to_owned()),
+                LexicalContent::EndOfInput => {
+                    if with_spaces {
+                        compiler.add_error(&item.location, "End".to_owned());
+                    }
+                }
             }
         }
     }
@@ -230,38 +364,32 @@ mod test {
     }
 
     #[test]
-    fn lexer_items() {
+    fn lexer_numbers() {
         let mut compiler = DocCompiler::new();
         let source = Rc::new(Source::new(
             "sdk/foo/foo.fidl".to_owned(),
             10,
             4,
-            "Some documentation.\n".to_owned(),
+            "1234 0x789abc 123abc 0x78abg".to_owned(),
         ));
         let items = reduce_lexems(&mut compiler, &source);
         assert!(!items.is_none());
-        lexical_items_to_errors(&mut compiler, &items.unwrap());
+        lexical_items_to_errors(&mut compiler, &items.unwrap(), /*with_spaces=*/ false);
         assert_eq!(
             compiler.errors,
             "\
-Some documentation.
+1234 0x789abc 123abc 0x78abg
 ^^^^
-sdk/foo/foo.fidl: 10:4: Text <Some>
-Some documentation.
-    ^
-sdk/foo/foo.fidl: 10:8: Spaces (1)
-Some documentation.
-     ^^^^^^^^^^^^^
-sdk/foo/foo.fidl: 10:9: Text <documentation>
-Some documentation.
-                  ^
-sdk/foo/foo.fidl: 10:22: EndOfSentence <.>
-Some documentation.
-                   ^
-sdk/foo/foo.fidl: 10:23: NewLines (1)
-
-^
-sdk/foo/foo.fidl: 11:4: End
+sdk/foo/foo.fidl: 10:4: Number <1234>
+1234 0x789abc 123abc 0x78abg
+     ^^^^^^^^
+sdk/foo/foo.fidl: 10:9: Number <0x789abc>
+1234 0x789abc 123abc 0x78abg
+              ^^^^^^
+sdk/foo/foo.fidl: 10:18: Name <123abc>
+1234 0x789abc 123abc 0x78abg
+                     ^^^^^^^
+sdk/foo/foo.fidl: 10:25: Name <0x78abg>
 "
         );
     }
@@ -277,46 +405,59 @@ sdk/foo/foo.fidl: 11:4: End
         ));
         let items = reduce_lexems(&mut compiler, &source);
         assert!(!items.is_none());
-        lexical_items_to_errors(&mut compiler, &items.unwrap());
+        lexical_items_to_errors(&mut compiler, &items.unwrap(), /*with_spaces=*/ false);
         assert_eq!(
             compiler.errors,
             "\
 It's correct to use heap-specific.
 ^^^^
-sdk/foo/foo.fidl: 10:4: Text <It's>
-It's correct to use heap-specific.
-    ^
-sdk/foo/foo.fidl: 10:8: Spaces (1)
+sdk/foo/foo.fidl: 10:4: Name <It's>
 It's correct to use heap-specific.
      ^^^^^^^
-sdk/foo/foo.fidl: 10:9: Text <correct>
-It's correct to use heap-specific.
-            ^
-sdk/foo/foo.fidl: 10:16: Spaces (1)
+sdk/foo/foo.fidl: 10:9: Name <correct>
 It's correct to use heap-specific.
              ^^
-sdk/foo/foo.fidl: 10:17: Text <to>
-It's correct to use heap-specific.
-               ^
-sdk/foo/foo.fidl: 10:19: Spaces (1)
+sdk/foo/foo.fidl: 10:17: Name <to>
 It's correct to use heap-specific.
                 ^^^
-sdk/foo/foo.fidl: 10:20: Text <use>
-It's correct to use heap-specific.
-                   ^
-sdk/foo/foo.fidl: 10:23: Spaces (1)
+sdk/foo/foo.fidl: 10:20: Name <use>
 It's correct to use heap-specific.
                     ^^^^^^^^^^^^^
-sdk/foo/foo.fidl: 10:24: Text <heap-specific>
+sdk/foo/foo.fidl: 10:24: Name <heap-specific>
 It's correct to use heap-specific.
                                  ^
 sdk/foo/foo.fidl: 10:37: EndOfSentence <.>
-It's correct to use heap-specific.
-                                  ^
-sdk/foo/foo.fidl: 10:38: NewLines (1)
+"
+        );
+    }
 
-^
-sdk/foo/foo.fidl: 11:4: End
+    #[test]
+    fn lexer_reference() {
+        let mut compiler = DocCompiler::new();
+        let source = Rc::new(Source::new(
+            "sdk/foo/foo.fidl".to_owned(),
+            10,
+            4,
+            "`xyz` isn't `abc`.".to_owned(),
+        ));
+        let items = reduce_lexems(&mut compiler, &source);
+        assert!(!items.is_none());
+        lexical_items_to_errors(&mut compiler, &items.unwrap(), /*with_spaces=*/ false);
+        assert_eq!(
+            compiler.errors,
+            "\
+`xyz` isn't `abc`.
+ ^^^
+sdk/foo/foo.fidl: 10:5: Reference <xyz>
+`xyz` isn't `abc`.
+      ^^^^^
+sdk/foo/foo.fidl: 10:10: Name <isn't>
+`xyz` isn't `abc`.
+             ^^^
+sdk/foo/foo.fidl: 10:17: Reference <abc>
+`xyz` isn't `abc`.
+                 ^
+sdk/foo/foo.fidl: 10:21: EndOfSentence <.>
 "
         );
     }
@@ -332,19 +473,19 @@ sdk/foo/foo.fidl: 11:4: End
         ));
         let items = reduce_lexems(&mut compiler, &source);
         assert!(!items.is_none());
-        lexical_items_to_errors(&mut compiler, &items.unwrap());
+        lexical_items_to_errors(&mut compiler, &items.unwrap(), /*with_spaces=*/ true);
         assert_eq!(
             compiler.errors,
             "\
 Some     documentation.
 ^^^^
-sdk/foo/foo.fidl: 10:4: Text <Some>
+sdk/foo/foo.fidl: 10:4: Name <Some>
 Some     documentation.
     ^^^^^
 sdk/foo/foo.fidl: 10:8: Spaces (5)
 Some     documentation.
          ^^^^^^^^^^^^^
-sdk/foo/foo.fidl: 10:13: Text <documentation>
+sdk/foo/foo.fidl: 10:13: Name <documentation>
 Some     documentation.
                       ^
 sdk/foo/foo.fidl: 10:26: EndOfSentence <.>
@@ -353,13 +494,13 @@ Some     documentation.
 sdk/foo/foo.fidl: 10:27: NewLines (3)
 And   spaces.
 ^^^
-sdk/foo/foo.fidl: 13:4: Text <And>
+sdk/foo/foo.fidl: 13:4: Name <And>
 And   spaces.
    ^^^
 sdk/foo/foo.fidl: 13:7: Spaces (3)
 And   spaces.
       ^^^^^^
-sdk/foo/foo.fidl: 13:10: Text <spaces>
+sdk/foo/foo.fidl: 13:10: Name <spaces>
 And   spaces.
             ^
 sdk/foo/foo.fidl: 13:16: EndOfSentence <.>
