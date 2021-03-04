00582436da95cbe4bc018baed83ab85d51876772
Zach Kirschenbaum <zkbaum@google.com>
2021-03-04 03:05:59 +0000

    [system-updater] arg parsing for epoch generator
    This CL adds a CLI parser to the system-updater. In the future, we'll
    enhance this to read epoch history and write epoch.json to the out
    directory.
    
    Test: fx test -o epoch-generator-tests
    Bug: 68403
    Change-Id: I3ef55c0ae89941193df9ab37a0bb2bbc6ed65251
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/493938
    Reviewed-by: Dan Johnson <computerdruid@google.com>
    Commit-Queue: Zach Kirschenbaum <zkbaum@google.com>

---
+++ src/sys/pkg/bin/system-updater/epoch/src/args.rs
@@ -0,0 +1,70 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {argh::FromArgs, std::path::Path};
+
+#[derive(FromArgs)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
+/// Program arguments.
+pub struct Args {
+    /// the path to the history file to read from.
+    #[argh(option)]
+    history: String,
+
+    /// the path to write to.
+    #[argh(option)]
+    output: String,
+}
+
+impl Args {
+    // TODO(zkbaum) use this in a future CL.
+    #[allow(dead_code)]
+    pub fn history(&self) -> &Path {
+        Path::new(&self.history)
+    }
+
+    // TODO(zkbaum) use this in a future CL.
+    #[allow(dead_code)]
+    pub fn output(&self) -> &Path {
+        Path::new(&self.output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, matches::assert_matches};
+
+    #[test]
+    fn fail_unknown_option() {
+        assert_matches!(Args::from_args(&["generate"], &["--unknown"]), Err(_));
+    }
+
+    #[test]
+    fn fail_unknown_subcommand() {
+        assert_matches!(Args::from_args(&["generate"], &["unknown"]), Err(_));
+    }
+
+    #[test]
+    fn fail_missing_output_dir() {
+        assert_matches!(Args::from_args(&["generate"], &["--history", "history_path"]), Err(_));
+    }
+
+    #[test]
+    fn fail_missing_history() {
+        assert_matches!(Args::from_args(&["generate"], &["--output", "output_path"]), Err(_));
+    }
+
+    #[test]
+    fn success() {
+        let args = Args::from_args(
+            &["generate"],
+            &["--history", "history_path", "--output", "output_path"],
+        )
+        .unwrap();
+        assert_eq!(
+            args,
+            Args { history: "history_path".to_owned(), output: "output_path".to_owned() }
+        );
+    }
+}
