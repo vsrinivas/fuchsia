102820b4be596f24edfd79b1b751422a0a57df21
Sundarajan Srinivasan <sundarsrini@google.com>
2021-03-06 00:16:11 +0000

    [weavestack] Enable Tunnel Telemetry
    *Enable Tunnel Telemetry.
    *Increase the size of debug event buffer to 512.
    
    Bug: 71076
    Test: Manually verified the telemetry data.
    
    Change-Id: I02a5d60fb95db3166d39888eb21a406fa51675b4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/490278
    Commit-Queue: Sundarajan Srinivasan <sundarsrini@google.com>
    Fuchsia-Auto-Submit: Sundarajan Srinivasan <sundarsrini@google.com>
    Reviewed-by: Neethu Joseph <neethujoseph@google.com>
    Reviewed-by: Jason Graffius <jgraff@google.com>

--- src/connectivity/weave/adaptation/weave_device_platform_config.h
+++ src/connectivity/weave/adaptation/weave_device_platform_config.h
@@ -13,6 +13,16 @@
 // Enable Trait Manager.
 #define WEAVE_DEVICE_CONFIG_ENABLE_TRAIT_MANAGER 1
 
+// Enable Tunnel Telemetry.
+#define WEAVE_DEVICE_CONFIG_ENABLE_TUNNEL_TELEMETRY 1
+
+// The default size of individual debug event buffer is 256 bytes. Using the
+// default size causes openweave to die, as openweave can't reserve enough space
+// for some of the debug events which have a base size of 196 bytes and a
+// few more bytes used for other headers which exceed 256.
+// So increase the size of debug event buffer.
+#define WEAVE_DEVICE_CONFIG_EVENT_LOGGING_DEBUG_BUFFER_SIZE 512
+
 // Disable Weave-based time sync services, as these are handled in Fuchsia by
 // other component(s).
 #define WEAVE_DEVICE_CONFIG_ENABLE_WEAVE_TIME_SERVICE_TIME_SYNC 0
