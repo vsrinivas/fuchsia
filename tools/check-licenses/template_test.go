7eea85519130add747eb3dd89ba2aac2e3590d97
Sean Cuff <seancuff@google.com>
2021-03-08 22:18:02 +0000

    [check-licenses] Replace <> before newlines
    Before this, the <br /> tags were being escaped, so there were literal
    <br />s appearing in license text.
    
    Change-Id: I2c37df787da47068db1e6b9511f5707462a2ff3b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/497818
    Commit-Queue: Sean Cuff <seancuff@google.com>
    Reviewed-by: James Robinson <jamesr@google.com>

--- tools/check-licenses/template_test.go
+++ tools/check-licenses/template_test.go
@@ -3,3 +3,40 @@
 // found in the LICENSE file.
 
 package checklicenses
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetHTMLTextDoesNotEscapeBR(t *testing.T) {
+	tests := []struct {
+		text string
+		want string
+	}{
+		{
+			text: "nothing to do",
+			want: "nothing to do",
+		},
+		{
+			text: "text with a\nlinebreak",
+			want: "text with a<br />linebreak",
+		},
+		{
+			text: "text with a <br /> tag to escape",
+			want: "text with a &lt;br /&gt; tag to escape",
+		},
+		{
+			text: "text\nwith <br /> both",
+			want: "text<br />with &lt;br /&gt; both",
+		},
+	}
+	for _, tc := range tests {
+		m := &Match{Text: tc.text}
+		fmt.Printf("%v", m)
+		getHTMLText := getFuncMap(nil)["getHTMLText"].(func(*Match) string)
+		if got := getHTMLText(m); got != tc.want {
+			t.Errorf("getHTMLText(%q) = %q; want %q", m.Text, got, tc.want)
+		}
+	}
+}
