e6f4d1075872399515dc2fd38524ded67b2d3a43
Anirudh Mathukumilli <rudymathu@google.com>
2021-03-09 03:28:31 +0000

    [e2e] Add a flash test
    Implements an end to end test that validates that fastboot flashing
    works correctly.
    
    Change-Id: I4f24640866b905514bb3511e143cc07fd68fbbc0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/497879
    Commit-Queue: Anirudh Mathukumilli <rudymathu@google.com>
    Reviewed-by: Yuan Zhi <yuanzhi@google.com>

---
+++ src/tests/end_to_end/flash/test/flash_test.dart
@@ -0,0 +1,48 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+import 'package:sl4f/sl4f.dart' as sl4f;
+import 'package:test/test.dart';
+
+void main() {
+  final sl4fDriver = sl4f.Sl4f.fromEnvironment();
+  sl4f.Device deviceController;
+
+  setUp(() async {
+    await sl4fDriver.startServer();
+    deviceController = sl4f.Device(sl4fDriver);
+  });
+
+  tearDown(() async {
+    await sl4fDriver.stopServer();
+    sl4fDriver.close();
+  });
+
+  test('test flashing', () async {
+    // Get env info necessary to run test.
+    String fbSernum = Platform.environment['FUCHSIA_FASTBOOT_SERNUM'];
+    if (fbSernum.isEmpty) {
+      fail('FUCHSIA_FASTBOOT_SERNUM environment variable was empty.');
+    }
+    String nodename = Platform.environment['FUCHSIA_NODENAME'];
+    if (nodename.isEmpty) {
+      fail('FUCHSIA_NODENAME environment variable was empty.');
+    }
+
+    // Get the device into fastboot.
+    await sl4fDriver.ssh.run('dm reboot-bootloader');
+
+    // Run flash.sh.
+    var process = Process.runSync('./flash.sh', ['-s', fbSernum]);
+    stdout.write(process.stdout);
+    stderr.write(process.stderr);
+    expect(process.exitCode, equals(0),
+        reason: 'flash.sh exited with nonzero exit code');
+
+    // Verify that the device came back by checking the nodename.
+    expect(deviceController.getDeviceName, equals(nodename),
+        reason: 'could not get nodename via sl4f');
+  }, timeout: Timeout(Duration(minutes: 2)));
+}
