ce8e966faf71c3ead054d99afb3d21b11b8a7299
Joshua Seaton <joshuaseaton@google.com>
2021-03-11 23:55:39 +0000

    [fit-promise] Introduce an empty fit-promse to an SDK
    This is eventually where the promise-related content of libfit will be
    moved to. Once all users of that content are expressly depending on
    fit-promise, we will make fit-promise depend on fit and start migrating
    things over.
    
    Bug: 57348
    Change-Id: I3d94532272f1d899c3e4d3089ba0562e3b96004c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/496555
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Reviewed-by: Mark Schott <schottm@google.com>
    API-Review: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>

---
+++ sdk/lib/fit-promise/include/lib/fit/do-not-include.h
@@ -0,0 +1,12 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_FIT_PROMISE_INCLUDE_LIB_FIT_DO_NOT_INCLUDE_H_
+#define LIB_FIT_PROMISE_INCLUDE_LIB_FIT_DO_NOT_INCLUDE_H_
+
+// TODO(fxbug.dev/57348): SDK machinery does not allow for an empty library;
+// this header is merely a placeholder to keep that machinery happy until we
+// are ready to populate fit-promise.
+
+#endif  // LIB_FIT_PROMISE_INCLUDE_LIB_FIT_DO_NOT_INCLUDE_H_
