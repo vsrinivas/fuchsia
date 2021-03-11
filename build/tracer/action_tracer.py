25b6064042b70eed013c93fcccde236c8c26ca12
David Fang <fangism@google.com>
2021-03-11 02:06:17 +0000

    [build][tracer] Treat stat() operations like reads
    Some actions/tools are known to operate depending on
    the success of stat() operations.
    
    Increased fsatrace buffer size to accomodate trace volume.
    
    Change-Id: I8d3612e79e06ed72867fcc91e3b7a82771c48538
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/499389
    Commit-Queue: David Fang <fangism@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>

--- build/tracer/action_tracer.py
+++ build/tracer/action_tracer.py
@@ -200,10 +200,12 @@ def _parse_fsatrace_line(fsatrace_line: str) -> Iterable[FSAccess]:
     if sep != "|":
         return
 
-    # op: operation code in [rwdtm]
-    if op == "r":
+    # op: operation code in [rwdtmq]
+    if op in {"r", "q"}:
+        # Treat "q" stat() like a read operation.
         yield Read(path)
     elif op in {"w", "t"}:
+        # Treat "t" touch like a write operation.
         yield Write(path)
     elif op == "d":
         yield Delete(path)
@@ -762,11 +764,11 @@ def main():
     trace_output_dir = os.path.dirname(args.trace_output)
     os.makedirs(trace_output_dir, exist_ok=True)
 
-    os.environ["FSAT_BUF_SIZE"] = "5000000"
+    os.environ["FSAT_BUF_SIZE"] = "8000000"
     retval = subprocess.call(
         [
             args.fsatrace_path,
-            "erwmdt",
+            "erwmdtq",
             args.trace_output,
             "--",
         ] + command.tokens)
