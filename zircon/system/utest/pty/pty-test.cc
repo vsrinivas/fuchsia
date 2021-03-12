6b1d5c7c5beb50bd87181714838105c8428fb574
Tamir Duberstein <tamird@google.com>
2021-03-12 21:25:39 +0000

    [utest] Package pty test
    Migrate it to typed channel APIs while I'm here and improve error
    messages.
    
    Bug: 69585
    Change-Id: I5555facd52571256de49dbabfbf2aeb0fab7b619
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/500858
    Commit-Queue: Tamir Duberstein <tamird@google.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Tess Eisenberger <teisenbe@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>

--- zircon/system/utest/pty/pty-test.cc
+++ zircon/system/utest/pty/pty-test.cc
@@ -6,12 +6,11 @@
 #include <fcntl.h>
 #include <fuchsia/hardware/pty/llcpp/fidl.h>
 #include <lib/fdio/cpp/caller.h>
-#include <lib/fdio/directory.h>
 #include <lib/fdio/fd.h>
 #include <lib/fdio/io.h>
+#include <lib/service/llcpp/service.h>
 #include <lib/zx/time.h>
 #include <poll.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <zircon/status.h>
 
@@ -19,31 +18,31 @@
 
 namespace fpty = ::fuchsia_hardware_pty;
 
-// returns an int to avoid sign errors from ASSERT_*()
-static int fd_signals(const fbl::unique_fd& fd, uint32_t wait_for_any, zx::time deadline) {
+zx::status<uint32_t> fd_signals(const fbl::unique_fd& fd, uint32_t wait_for_any,
+                                zx::time deadline) {
   uint32_t signals = 0;
-  fdio_wait_fd(fd.get(), wait_for_any, &signals, deadline.get());
-  if (deadline != zx::time{}) {
-    // If we waited for non-zero time, re-read with 0 time.  This call bottoms
-    // out in zx_object_wait_one, which will return signals that were
-    // transiently asserted during the wait.  The second call will allow us to
-    // ignore signals that aren't currently asserted.
-    fdio_wait_fd(fd.get(), wait_for_any, &signals, 0);
+  zx_status_t status = fdio_wait_fd(fd.get(), wait_for_any, &signals, deadline.get());
+  if (status != ZX_OK && status != ZX_ERR_TIMED_OUT) {
+    return zx::error(status);
   }
-  return signals;
+  return zx::ok(signals);
 }
 
-static ssize_t write_full(int fd) {
+#define ASSERT_SIGNALS(val1, val2) \
+  ASSERT_OK(val1);                 \
+  ASSERT_EQ(val1.value(), val2)
+
+static ssize_t write_full(const fbl::unique_fd& fd) {
   char tmp[300];
   memset(tmp, 0x33, sizeof(tmp));
   ssize_t total = 0;
   for (;;) {
-    ssize_t r = write(fd, tmp, sizeof(tmp));
+    ssize_t r = write(fd.get(), tmp, sizeof(tmp));
     if (r < 0) {
       if (errno == EAGAIN) {
         break;
       }
-      return -errno;
+      return r;
     }
     if (r == 0) {
       break;
@@ -53,21 +52,21 @@ static ssize_t write_full(int fd) {
   return total;
 }
 
-static ssize_t read_all(int fd) {
+static ssize_t read_all(const fbl::unique_fd& fd) {
   char tmp[700];
   ssize_t total = 0;
   for (;;) {
-    ssize_t r = read(fd, tmp, sizeof(tmp));
+    ssize_t r = read(fd.get(), tmp, sizeof(tmp));
     if (r < 0) {
       if (errno == EAGAIN) {
         break;
       }
-      return -errno;
+      return r;
     }
     if (r == 0) {
       break;
     }
-    for (int n = 0; n < r; n++) {
+    for (ssize_t n = 0; n < r; n++) {
       if (tmp[n] != 0x33) {
         return -EFAULT;
       }
@@ -77,27 +76,20 @@ static ssize_t read_all(int fd) {
   return total;
 }
 
-static zx_status_t open_client(int fd, uint32_t client_id, int* out_fd) {
+static zx_status_t open_client(const fbl::unique_fd& fd, uint32_t client_id, int* out_fd) {
   if (!out_fd) {
     return ZX_ERR_INVALID_ARGS;
   }
 
-  fdio_t* io = fdio_unsafe_fd_to_io(fd);
-  if (!io) {
-    return ZX_ERR_INTERNAL;
+  fdio_cpp::UnownedFdioCaller io(fd.get());
+
+  zx::status endpoints = fidl::CreateEndpoints<fpty::Device>();
+  if (endpoints.is_error()) {
+    return endpoints.status_value();
   }
 
-  zx::channel device_channel, client_channel;
-  zx_status_t status = zx::channel::create(0, &device_channel, &client_channel);
-  if (status != ZX_OK) {
-    fdio_unsafe_release(io);
-    return status;
-  }
-
-  auto result = fpty::Device::Call::OpenClient(zx::unowned_channel(fdio_unsafe_borrow_channel(io)),
-                                               client_id, std::move(device_channel));
-
-  fdio_unsafe_release(io);
+  auto result = fpty::Device::Call::OpenClient(io.borrow_as<fpty::Device>(), client_id,
+                                               std::move(endpoints->server));
 
   if (result.status() != ZX_OK) {
     return result.status();
@@ -106,7 +98,7 @@ static zx_status_t open_client(int fd, uint32_t client_id, int* out_fd) {
     return result->s;
   }
 
-  status = fdio_fd_create(client_channel.release(), out_fd);
+  zx_status_t status = fdio_fd_create(endpoints->client.channel().release(), out_fd);
   if (status != ZX_OK) {
     return status;
   }
@@ -121,200 +113,193 @@ TEST(PtyTests, pty_test) {
   // This manifests as a loss of fd signals.
   fbl::unique_fd ps;
   {
-    zx::channel local, remote;
-    ASSERT_EQ(zx::channel::create(0, &local, &remote), ZX_OK);
-    ASSERT_EQ(fdio_service_connect("/svc/fuchsia.hardware.pty.Device", remote.release()), ZX_OK);
-    int fd;
-    ASSERT_EQ(fdio_fd_create(local.release(), &fd), ZX_OK);
-    ps.reset(fd);
+    zx::status client_end = service::Connect<fpty::Device>();
+    ASSERT_OK(client_end.status_value());
+
+    ASSERT_OK(fdio_fd_create(client_end->channel().release(), ps.reset_and_get_address()));
     ASSERT_TRUE(ps.is_valid());
-    int flags = fcntl(ps.get(), F_GETFL);
-    ASSERT_GE(flags, 0);
-    ASSERT_EQ(fcntl(ps.get(), F_SETFL, flags | O_NONBLOCK), 0);
+    int flags;
+    ASSERT_GE(flags = fcntl(ps.get(), F_GETFL), 0, "%s", strerror(errno));
+    ASSERT_EQ(fcntl(ps.get(), F_SETFL, flags | O_NONBLOCK), 0, "%s", strerror(errno));
   }
 
   fdio_cpp::UnownedFdioCaller ps_io(ps.get());
 
-  int pc_fd;
-  ASSERT_EQ(open_client(ps.get(), 0, &pc_fd), ZX_OK);
-  ASSERT_GE(pc_fd, 0, "");
-
-  fbl::unique_fd pc(pc_fd);
-  ASSERT_EQ(bool(pc), true, "");
+  fbl::unique_fd pc;
+  ASSERT_OK(open_client(ps, 0, pc.reset_and_get_address()));
+  ASSERT_TRUE(pc.is_valid());
 
   fdio_cpp::UnownedFdioCaller pc_io(pc.get());
 
   char tmp[32];
 
-  ASSERT_EQ(fd_signals(ps, POLLOUT, zx::time{}), POLLOUT, "");
-  ASSERT_EQ(fd_signals(pc, POLLOUT, zx::time{}), POLLOUT, "");
+  ASSERT_SIGNALS(fd_signals(ps, POLLOUT, zx::time{}), POLLOUT);
+  ASSERT_SIGNALS(fd_signals(pc, POLLOUT, zx::time{}), POLLOUT);
 
   // nothing to read
-  ASSERT_EQ(read(ps.get(), tmp, 32), -1, "");
-  ASSERT_EQ(errno, EAGAIN, "");
-  ASSERT_EQ(read(pc.get(), tmp, 32), -1, "");
-  ASSERT_EQ(errno, EAGAIN, "");
+  ASSERT_EQ(read(ps.get(), tmp, 32), -1);
+  ASSERT_EQ(errno, EAGAIN, "%s", strerror(errno));
+  ASSERT_EQ(read(pc.get(), tmp, 32), -1);
+  ASSERT_EQ(errno, EAGAIN, "%s", strerror(errno));
 
   // write server, read client
-  ASSERT_EQ(write(ps.get(), "xyzzy", 5), 5, "");
-  ASSERT_EQ(fd_signals(pc, POLLIN | POLLOUT, zx::time{}), POLLIN | POLLOUT, "");
+  ASSERT_EQ(write(ps.get(), "xyzzy", 5), 5, "%s", strerror(errno));
+  ASSERT_SIGNALS(fd_signals(pc, POLLIN | POLLOUT, zx::time{}), POLLIN | POLLOUT);
 
-  memset(tmp, 0xee, 5);
-  ASSERT_EQ(read(pc.get(), tmp, 5), 5, "");
-  ASSERT_EQ(memcmp(tmp, "xyzzy", 5), 0, "");
-  ASSERT_EQ(fd_signals(pc, POLLOUT, zx::time{}), POLLOUT, "");
+  memset(tmp, 0, 6);
+  ASSERT_EQ(read(pc.get(), tmp, 5), 5, "%s", strerror(errno));
+  ASSERT_SUBSTR(tmp, "xyzzy");
+  ASSERT_SIGNALS(fd_signals(pc, POLLOUT, zx::time{}), POLLOUT);
 
   // write client, read server
-  ASSERT_EQ(write(pc.get(), "xyzzy", 5), 5, "");
-  ASSERT_EQ(fd_signals(ps, POLLIN | POLLOUT, zx::time{}), POLLIN | POLLOUT, "");
+  ASSERT_EQ(write(pc.get(), "xyzzy", 5), 5, "%s", strerror(errno));
+  ASSERT_SIGNALS(fd_signals(ps, POLLIN | POLLOUT, zx::time{}), POLLIN | POLLOUT);
 
-  memset(tmp, 0xee, 5);
-  ASSERT_EQ(read(ps.get(), tmp, 5), 5, "");
-  ASSERT_EQ(memcmp(tmp, "xyzzy", 5), 0, "");
-  ASSERT_EQ(fd_signals(ps, POLLOUT, zx::time{}), POLLOUT, "");
+  memset(tmp, 0, 6);
+  ASSERT_EQ(read(ps.get(), tmp, 5), 5, "%s", strerror(errno));
+  ASSERT_SUBSTR(tmp, "xyzzy");
+  ASSERT_SIGNALS(fd_signals(ps, POLLOUT, zx::time{}), POLLOUT);
 
   // write server until full, then drain
-  ASSERT_EQ(write_full(ps.get()), 4096, "");
-  ASSERT_EQ(fd_signals(ps, 0, zx::time{}), 0, "");
-  ASSERT_EQ(read_all(pc.get()), 4096, "");
-  ASSERT_EQ(fd_signals(ps, POLLOUT, zx::time{}), POLLOUT, "");
+  ASSERT_EQ(write_full(ps), 4096, "%s", strerror(errno));
+  ASSERT_SIGNALS(fd_signals(ps, 0, zx::time{}), 0);
+  ASSERT_EQ(read_all(pc), 4096, "%s", strerror(errno));
+  ASSERT_SIGNALS(fd_signals(ps, POLLOUT, zx::time{}), POLLOUT);
 
   // write client until full, then drain
-  ASSERT_EQ(write_full(pc.get()), 4096, "");
-  ASSERT_EQ(fd_signals(pc, 0, zx::time{}), 0, "");
-  ASSERT_EQ(read_all(ps.get()), 4096, "");
-  ASSERT_EQ(fd_signals(pc, POLLOUT, zx::time{}), POLLOUT, "");
+  ASSERT_EQ(write_full(pc), 4096, "%s", strerror(errno));
+  ASSERT_SIGNALS(fd_signals(pc, 0, zx::time{}), 0);
+  ASSERT_EQ(read_all(ps), 4096, "%s", strerror(errno));
+  ASSERT_SIGNALS(fd_signals(pc, POLLOUT, zx::time{}), POLLOUT);
 
   // verify no events pending
-  auto result1 = fpty::Device::Call::ReadEvents(pc_io.channel());
+  auto result1 = fpty::Device::Call::ReadEvents(pc_io.borrow_as<fpty::Device>());
 
-  ASSERT_EQ(result1.status(), ZX_OK, "");
-  ASSERT_EQ(result1->status, ZX_OK, "");
-  ASSERT_EQ(result1->events, 0u, "");
+  ASSERT_OK(result1.status());
+  ASSERT_OK(result1->status);
+  ASSERT_EQ(result1->events, 0u);
 
   // write a ctrl-c
-  ASSERT_EQ(write(ps.get(), "\x03", 1), 1, "");
+  ASSERT_EQ(write(ps.get(), "\x03", 1), 1, "%s", strerror(errno));
 
   // should be an event now
-  auto result2 = fpty::Device::Call::ReadEvents(pc_io.channel());
-  ASSERT_EQ(result2.status(), ZX_OK, "");
-  ASSERT_EQ(result2->status, ZX_OK, "");
-  ASSERT_EQ(result2->events, fpty::wire::EVENT_INTERRUPT, "");
+  auto result2 = fpty::Device::Call::ReadEvents(pc_io.borrow_as<fpty::Device>());
+  ASSERT_OK(result2.status());
+  ASSERT_OK(result2->status);
+  ASSERT_EQ(result2->events, fpty::wire::EVENT_INTERRUPT);
 
   // should vanish once we read it
-  auto result3 = fpty::Device::Call::ReadEvents(pc_io.channel());
-  ASSERT_EQ(result3.status(), ZX_OK, "");
-  ASSERT_EQ(result3->status, ZX_OK, "");
-  ASSERT_EQ(result3->events, 0u, "");
+  auto result3 = fpty::Device::Call::ReadEvents(pc_io.borrow_as<fpty::Device>());
+  ASSERT_OK(result3.status());
+  ASSERT_OK(result3->status);
+  ASSERT_EQ(result3->events, 0u);
 
   // write something containing a special char
   // should write up to and including the special char
   // converting the special char to a signal
-  ASSERT_EQ(write(ps.get(), "hello\x03world", 11), 6, "");
-  ASSERT_EQ(read(pc.get(), tmp, 6), 5, "");
-  ASSERT_EQ(memcmp(tmp, "hello", 5), 0, "");
-  auto result4 = fpty::Device::Call::ReadEvents(pc_io.channel());
-  ASSERT_EQ(result4.status(), ZX_OK, "");
-  ASSERT_EQ(result4->status, ZX_OK, "");
-  ASSERT_EQ(result4->events, fpty::wire::EVENT_INTERRUPT, "");
+  ASSERT_EQ(write(ps.get(), "hello\x03world", 11), 6, "%s", strerror(errno));
+  ASSERT_EQ(read(pc.get(), tmp, 6), 5, "%s", strerror(errno));
+  ASSERT_SUBSTR(tmp, "hello");
+  auto result4 = fpty::Device::Call::ReadEvents(pc_io.borrow_as<fpty::Device>());
+  ASSERT_OK(result4.status());
+  ASSERT_OK(result4->status);
+  ASSERT_EQ(result4->events, fpty::wire::EVENT_INTERRUPT);
 
-  auto ws_result1 = fpty::Device::Call::GetWindowSize(pc_io.channel());
-  ASSERT_EQ(ws_result1.status(), ZX_OK, "");
-  ASSERT_EQ(ws_result1->status, ZX_OK, "");
-  ASSERT_EQ(ws_result1->size.width, 0u, "");
-  ASSERT_EQ(ws_result1->size.height, 0u, "");
+  auto ws_result1 = fpty::Device::Call::GetWindowSize(pc_io.borrow_as<fpty::Device>());
+  ASSERT_OK(ws_result1.status());
+  ASSERT_OK(ws_result1->status);
+  ASSERT_EQ(ws_result1->size.width, 0u, "%s", strerror(errno));
+  ASSERT_EQ(ws_result1->size.height, 0u, "%s", strerror(errno));
 
   fpty::wire::WindowSize ws;
   ws.width = 80;
   ws.height = 25;
-  auto result5 = fpty::Device::Call::SetWindowSize(pc_io.channel(), ws);
-  ASSERT_EQ(result5.status(), ZX_OK, "");
-  ASSERT_EQ(result5->status, ZX_OK, "");
-  auto ws_result2 = fpty::Device::Call::GetWindowSize(pc_io.channel());
-  ASSERT_EQ(ws_result2.status(), ZX_OK, "");
-  ASSERT_EQ(ws_result2->status, ZX_OK, "");
-  ASSERT_EQ(ws_result2->size.width, 80u, "");
-  ASSERT_EQ(ws_result2->size.height, 25u, "");
+  auto result5 = fpty::Device::Call::SetWindowSize(pc_io.borrow_as<fpty::Device>(), ws);
+  ASSERT_OK(result5.status());
+  ASSERT_OK(result5->status);
+  auto ws_result2 = fpty::Device::Call::GetWindowSize(pc_io.borrow_as<fpty::Device>());
+  ASSERT_OK(ws_result2.status());
+  ASSERT_OK(ws_result2->status);
+  ASSERT_EQ(ws_result2->size.width, 80u, "%s", strerror(errno));
+  ASSERT_EQ(ws_result2->size.height, 25u, "%s", strerror(errno));
 
   // verify that we don't get events for special chars in raw mode
-  auto result6 = fpty::Device::Call::ClrSetFeature(pc_io.channel(), 0, fpty::wire::FEATURE_RAW);
-  ASSERT_EQ(result6.status(), ZX_OK, "");
-  ASSERT_EQ(result6->status, ZX_OK, "");
-  ASSERT_EQ(result6->features & fpty::wire::FEATURE_RAW, fpty::wire::FEATURE_RAW, "");
-  ASSERT_EQ(write(ps.get(), "\x03", 1), 1, "");
-  ASSERT_EQ(read(pc.get(), tmp, 1), 1, "");
-  ASSERT_EQ(tmp[0], '\x03', "");
-  auto result7 = fpty::Device::Call::ReadEvents(pc_io.channel());
-  ASSERT_EQ(result7.status(), ZX_OK, "");
-  ASSERT_EQ(result7->status, ZX_OK, "");
-  ASSERT_EQ(result7->events, 0u, "");
+  auto result6 = fpty::Device::Call::ClrSetFeature(pc_io.borrow_as<fpty::Device>(), 0,
+                                                   fpty::wire::FEATURE_RAW);
+  ASSERT_OK(result6.status());
+  ASSERT_OK(result6->status);
+  ASSERT_EQ(result6->features & fpty::wire::FEATURE_RAW, fpty::wire::FEATURE_RAW);
+  ASSERT_EQ(write(ps.get(), "\x03", 1), 1, "%s", strerror(errno));
+  ASSERT_EQ(read(pc.get(), tmp, 1), 1, "%s", strerror(errno));
+  ASSERT_EQ(tmp[0], '\x03', "%s", strerror(errno));
+  auto result7 = fpty::Device::Call::ReadEvents(pc_io.borrow_as<fpty::Device>());
+  ASSERT_OK(result7.status());
+  ASSERT_OK(result7->status);
+  ASSERT_EQ(result7->events, 0u);
 
   // create a second client
-  int pc1_fd;
-  ASSERT_EQ(open_client(pc.get(), 1, &pc1_fd), ZX_OK);
-  ASSERT_GE(pc1_fd, 0, "");
-
-  fbl::unique_fd pc1(pc1_fd);
-  ASSERT_EQ(bool(pc1), true, "");
+  fbl::unique_fd pc1;
+  ASSERT_OK(open_client(pc, 1, pc1.reset_and_get_address()));
+  ASSERT_TRUE(pc1.is_valid());
 
   fdio_cpp::UnownedFdioCaller pc1_io(pc1.get());
 
   // reads/writes to non-active client should block
-  ASSERT_EQ(fd_signals(pc1, 0, zx::time{}), 0, "");
-  ASSERT_EQ(write(pc1.get(), "test", 4), -1, "");
-  ASSERT_EQ(errno, EAGAIN, "");
-  ASSERT_EQ(read(pc1.get(), tmp, 4), -1, "");
-  ASSERT_EQ(errno, EAGAIN, "");
+  ASSERT_SIGNALS(fd_signals(pc1, 0, zx::time{}), 0);
+  ASSERT_EQ(write(pc1.get(), "test", 4), -1);
+  ASSERT_EQ(errno, EAGAIN, "%s", strerror(errno));
+  ASSERT_EQ(read(pc1.get(), tmp, 4), -1);
+  ASSERT_EQ(errno, EAGAIN, "%s", strerror(errno));
 
   uint32_t n = 2;
-  auto result8 = fpty::Device::Call::MakeActive(pc_io.channel(), n);
-  ASSERT_EQ(result8.status(), ZX_OK, "");
-  ASSERT_EQ(result8->status, ZX_ERR_NOT_FOUND, "");
+  auto result8 = fpty::Device::Call::MakeActive(pc_io.borrow_as<fpty::Device>(), n);
+  ASSERT_EQ(result8.status(), ZX_OK);
+  ASSERT_STATUS(result8->status, ZX_ERR_NOT_FOUND);
 
   // non-controlling client cannot change active client
-  auto result9 = fpty::Device::Call::MakeActive(pc1_io.channel(), n);
-  ASSERT_EQ(result9.status(), ZX_OK, "");
-  ASSERT_EQ(result9->status, ZX_ERR_ACCESS_DENIED, "");
+  auto result9 = fpty::Device::Call::MakeActive(pc1_io.borrow_as<fpty::Device>(), n);
+  ASSERT_EQ(result9.status(), ZX_OK);
+  ASSERT_STATUS(result9->status, ZX_ERR_ACCESS_DENIED);
 
   // but controlling client can
   n = 1;
-  auto result10 = fpty::Device::Call::MakeActive(pc_io.channel(), n);
-  ASSERT_EQ(result10.status(), ZX_OK, "");
-  ASSERT_EQ(result10->status, ZX_OK, "");
-  ASSERT_EQ(fd_signals(pc, 0, zx::time{}), 0, "");
-  ASSERT_EQ(fd_signals(pc1, POLLOUT, zx::time{}), POLLOUT, "");
-  ASSERT_EQ(write(pc1.get(), "test", 4), 4, "");
-  ASSERT_EQ(read(ps.get(), tmp, 4), 4, "");
-  ASSERT_EQ(memcmp(tmp, "test", 4), 0, "");
+  auto result10 = fpty::Device::Call::MakeActive(pc_io.borrow_as<fpty::Device>(), n);
+  ASSERT_OK(result10.status());
+  ASSERT_OK(result10->status);
+  ASSERT_SIGNALS(fd_signals(pc, 0, zx::time{}), 0);
+  ASSERT_SIGNALS(fd_signals(pc1, POLLOUT, zx::time{}), POLLOUT);
+  ASSERT_EQ(write(pc1.get(), "test", 4), 4, "%s", strerror(errno));
+  ASSERT_EQ(read(ps.get(), tmp, 4), 4, "%s", strerror(errno));
+  ASSERT_SUBSTR(tmp, "test");
 
   // make sure controlling client observes departing active client
   pc1_io.reset();
   pc1.reset();
-  ASSERT_EQ(fd_signals(pc, POLLHUP | POLLPRI, zx::time::infinite()), POLLHUP | POLLPRI, "");
-  auto result11 = fpty::Device::Call::ReadEvents(pc_io.channel());
-  ASSERT_EQ(result11.status(), ZX_OK, "");
-  ASSERT_EQ(result11->status, ZX_OK, "");
-  ASSERT_EQ(result11->events, fpty::wire::EVENT_HANGUP, "");
+  ASSERT_SIGNALS(fd_signals(pc, POLLHUP | POLLPRI, zx::time::infinite()), POLLHUP | POLLPRI);
+  auto result11 = fpty::Device::Call::ReadEvents(pc_io.borrow_as<fpty::Device>());
+  ASSERT_OK(result11.status());
+  ASSERT_OK(result11->status);
+  ASSERT_EQ(result11->events, fpty::wire::EVENT_HANGUP);
 
   // verify that server observes departure of last client
   pc_io.reset();
   pc.reset();
-  ASSERT_EQ(fd_signals(ps, POLLHUP | POLLIN, zx::time::infinite()), POLLHUP | POLLIN, "");
+  ASSERT_SIGNALS(fd_signals(ps, POLLHUP | POLLIN, zx::time::infinite()), POLLHUP | POLLIN);
 
   ps_io.reset();
   ps.reset();
 }
 
 TEST(PtyTests, not_a_pty_test) {
-  fbl::unique_fd root_dir(open("/", O_DIRECTORY | O_RDONLY));
-  ASSERT_EQ(bool(root_dir), true, "");
+  fbl::unique_fd root_dir;
+  ASSERT_TRUE(root_dir = fbl::unique_fd(open("/", O_DIRECTORY | O_RDONLY)), "%s", strerror(errno));
 
   fdio_cpp::UnownedFdioCaller io(root_dir.get());
 
   // Sending pty messages such as 'get window size' should fail
   // properly on things that are not ptys.
-  auto result = fpty::Device::Call::GetWindowSize(io.channel());
-  ASSERT_EQ(result.status(), ZX_ERR_BAD_HANDLE, "");
+  auto result = fpty::Device::Call::GetWindowSize(io.borrow_as<fpty::Device>());
+  ASSERT_STATUS(result.status(), ZX_ERR_BAD_HANDLE);
 
   io.reset();
 }
