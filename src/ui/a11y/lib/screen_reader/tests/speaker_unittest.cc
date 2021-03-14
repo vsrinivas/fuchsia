5c7c1d635c036787d601597e0fcf928819b888df
Lucas Radaelli <lucasradaelli@google.com>
2021-03-14 20:01:05 +0000

    [a11y] Fixes flaky a11y test.
    Fixed: 69327
    Change-Id: Iec0faab68c2cd0baecead250a2d2a434d286933a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/500399
    Fuchsia-Auto-Submit: Lucas Radaelli <lucasradaelli@google.com>
    Reviewed-by: Alexander Brusher <abrusher@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/ui/a11y/lib/screen_reader/tests/speaker_unittest.cc
+++ src/ui/a11y/lib/screen_reader/tests/speaker_unittest.cc
@@ -155,13 +155,12 @@ TEST_F(SpeakerTest, TaskWaitsInQueueWhenNotInterrupting) {
   auto task2 = speaker_->SpeakNodePromise(&node, {.interrupt = false});
   executor_.schedule_task(std::move(task));
   executor_.schedule_task(std::move(task2));
-  RunLoopWithTimeout(zx::duration(zx::msec(50)));
+  RunLoopUntil([this]() { return mock_tts_engine_.ExamineUtterances().size() == 3u; });
+
+  // If the second task waited in the queue before speaking, all three messages will be presente,
+  // meaning that the third one did not trump the second.
   EXPECT_TRUE(mock_tts_engine_.ReceivedSpeak());
-  ASSERT_EQ(mock_tts_engine_.ExamineUtterances().size(), 1u);
   EXPECT_EQ(mock_tts_engine_.ExamineUtterances()[0].message(), "foo");
-  RunLoopWithTimeout(zx::duration(zx::msec(300)));
-  // Now, the first task ran and notified the second it can start running. Check if utterances were
-  // received in order.
   ASSERT_EQ(mock_tts_engine_.ExamineUtterances().size(), 3u);
   EXPECT_EQ(mock_tts_engine_.ExamineUtterances()[1].message(), "button");
   EXPECT_EQ(mock_tts_engine_.ExamineUtterances()[2].message(), "foo");
