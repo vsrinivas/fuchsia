a3b976d6741ba7603f0a0aaa33f3c2a491b517d6
Shai Barack <shayba@google.com>
2021-03-15 19:41:02 +0000

    [appmgr] "system-temp" is gone
    Fixed: 25041
    
    Change-Id: If48c31fe42ed43898086db3387e7899fbc5e93f6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/501259
    Fuchsia-Auto-Submit: Shai Barack <shayba@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Gary Bressler <geb@google.com>

--- src/sys/appmgr/integration_tests/sandbox/features/system-temp/no_system_temp.cc
+++
@@ -1,7 +0,0 @@
-// Copyright 2018 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "src/sys/appmgr/integration_tests/sandbox/namespace_test.h"
-
-TEST_F(NamespaceTest, NoSystemTemp) { ExpectDoesNotExist("/tmp/"); }
