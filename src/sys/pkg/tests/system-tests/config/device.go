e562102f71b5dd8c8b2b65312361d2efca11885d
Erick Tryzelaar <etryzelaar@google.com>
2021-03-15 16:41:08 -0700

    [sshutil][swd-e2e-tests] Re-land Introduce Resolver interface
    This reverts commit 4637ca02610e28c432439a565a8658b976f3ae31.
    
    Change-Id: Ia1b6bfc8e036fe6987dce1635811b80e48b46334

--- src/sys/pkg/tests/system-tests/config/device.go
+++ src/sys/pkg/tests/system-tests/config/device.go
@@ -11,19 +11,17 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"golang.org/x/crypto/ssh"
 
 	"go.fuchsia.dev/fuchsia/src/sys/pkg/testing/host-target-testing/device"
-	"go.fuchsia.dev/fuchsia/src/sys/pkg/testing/host-target-testing/util"
 	"go.fuchsia.dev/fuchsia/tools/botanist/constants"
 )
 
 type DeviceConfig struct {
 	sshKeyFile       string
 	deviceFinderPath string
-	DeviceName       string
+	deviceName       string
 	deviceHostname   string
 	sshPrivateKey    ssh.Signer
 	SerialSocketPath string
@@ -35,7 +33,7 @@ func NewDeviceConfig(fs *flag.FlagSet) *DeviceConfig {
 	testDataPath := filepath.Join(filepath.Dir(os.Args[0]), "test_data", "system-tests")
 
 	fs.StringVar(&c.sshKeyFile, "ssh-private-key", os.Getenv(constants.SSHKeyEnvKey), "SSH private key file that can access the device")
-	fs.StringVar(&c.DeviceName, "device", os.Getenv(constants.NodenameEnvKey), "device name")
+	fs.StringVar(&c.deviceName, "device", os.Getenv(constants.NodenameEnvKey), "device name")
 	fs.StringVar(&c.deviceHostname, "device-hostname", os.Getenv(constants.DeviceAddrEnvKey), "device hostname or IPv4/IPv6 address")
 	fs.StringVar(&c.deviceFinderPath, "device-finder-path", filepath.Join(testDataPath, "device-finder"), "device-finder tool path")
 	fs.StringVar(&c.SerialSocketPath, "device-serial", os.Getenv(constants.SerialSocketEnvKey), "device serial path")
@@ -43,48 +41,12 @@ func NewDeviceConfig(fs *flag.FlagSet) *DeviceConfig {
 	return c
 }
 
-func (c *DeviceConfig) DeviceHostname(ctx context.Context) (string, error) {
+func (c *DeviceConfig) DeviceResolver(ctx context.Context) (device.DeviceResolver, error) {
 	if c.deviceHostname != "" {
-		return c.deviceHostname, nil
+		return device.NewConstantHostResolver(c.deviceName, c.deviceHostname), nil
 	}
 
-	// The logic here should match get-fuchsia-device-addr (//tools/devshell/lib/vars.sh).
-	if c.DeviceName == "" {
-		var err error
-		var deviceList string
-		deviceList, err = c.DeviceFinder(ctx, "list", "-ipv4=false", "-timeout=1s", "-full")
-		if err != nil {
-			return "", fmt.Errorf("ERROR: Failed to list devices: %w", err)
-		}
-		if strings.Contains(deviceList, "\n") {
-			return "", fmt.Errorf("ERROR: Found multiple devices. Use -device to specify one.")
-		}
-		// Device finder (with -full) outputs in the format "<address> <domain>".
-		var entry = strings.Split(deviceList, " ")
-		if len(entry) != 2 {
-			return "", fmt.Errorf("ERROR: device-finder return unexpected output: %s.", deviceList)
-		}
-		c.deviceHostname = entry[0]
-		c.DeviceName = entry[1]
-	} else {
-		var err error
-		c.deviceHostname, err = c.DeviceFinder(ctx, "resolve", "-ipv4=false", "-timeout=1s", "-device-limit=1", c.DeviceName)
-		if err != nil {
-			return "", fmt.Errorf("ERROR: Failed to find device %s: %w", c.DeviceName, err)
-		}
-		if c.deviceHostname == "" {
-			return "", fmt.Errorf("unable to determine the device hostname")
-		}
-	}
-	return c.deviceHostname, nil
-}
-
-func (c *DeviceConfig) DeviceFinder(ctx context.Context, arg ...string) (string, error) {
-	stdout, stderr, err := util.RunCommand(ctx, c.deviceFinderPath, arg...)
-	if err != nil {
-		return "", fmt.Errorf("device-finder failed: %w: %s", err, string(stderr))
-	}
-	return strings.TrimRight(string(stdout), "\n"), nil
+	return device.NewDeviceFinderResolver(ctx, c.deviceFinderPath, c.deviceName)
 }
 
 func (c *DeviceConfig) SSHPrivateKey() (ssh.Signer, error) {
@@ -109,7 +71,7 @@ func (c *DeviceConfig) SSHPrivateKey() (ssh.Signer, error) {
 }
 
 func (c *DeviceConfig) NewDeviceClient(ctx context.Context) (*device.Client, error) {
-	deviceHostname, err := c.DeviceHostname(ctx)
+	deviceResolver, err := c.DeviceResolver(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -119,5 +81,5 @@ func (c *DeviceConfig) NewDeviceClient(ctx context.Context) (*device.Client, err
 		return nil, err
 	}
 
-	return device.NewClient(ctx, deviceHostname, c.DeviceName, sshPrivateKey)
+	return device.NewClient(ctx, deviceResolver, sshPrivateKey)
 }
