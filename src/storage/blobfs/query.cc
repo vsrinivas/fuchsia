35cb4ae3874ee3fde5bf7961c0cf734d35e1468b
Vincent Belliard <vbelliard@google.com>
2021-03-16 16:44:27 +0000

    [storage] remove UnownedBuilder usage
    Change-Id: I9f624a3f1006e4232f1f1734baf832cc8a1e6383
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/499570
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Vincent Belliard <vbelliard@google.com>

--- src/storage/blobfs/query.cc
+++ src/storage/blobfs/query.cc
@@ -27,70 +27,57 @@ QueryService::QueryService(async_dispatcher_t* dispatcher, Blobfs* blobfs, Runne
 void QueryService::GetInfo(FilesystemInfoQuery query, GetInfoCompleter::Sync& completer) {
   static_assert(sizeof(kFsName) < fuchsia_fs::wire::MAX_FS_NAME_LENGTH, "Blobfs name too long");
 
-  fuchsia_fs::wire::FilesystemInfo::UnownedBuilder builder;
+  fidl::FidlAllocator allocator;
+  fuchsia_fs::wire::FilesystemInfo filesystem_info(allocator);
 
-  uint64_t total_bytes;
   if (query & FilesystemInfoQuery::TOTAL_BYTES) {
-    total_bytes = blobfs_->Info().data_block_count * blobfs_->Info().block_size;
-    builder.set_total_bytes(fidl::unowned_ptr(&total_bytes));
+    filesystem_info.set_total_bytes(allocator,
+                                    blobfs_->Info().data_block_count * blobfs_->Info().block_size);
   }
 
-  uint64_t used_bytes;
   if (query & FilesystemInfoQuery::USED_BYTES) {
-    used_bytes = blobfs_->Info().alloc_block_count * blobfs_->Info().block_size;
-    builder.set_used_bytes(fidl::unowned_ptr(&used_bytes));
+    filesystem_info.set_used_bytes(allocator,
+                                   blobfs_->Info().alloc_block_count * blobfs_->Info().block_size);
   }
 
-  uint64_t total_nodes;
   if (query & FilesystemInfoQuery::TOTAL_NODES) {
-    total_nodes = blobfs_->Info().inode_count;
-    builder.set_total_nodes(fidl::unowned_ptr(&total_nodes));
+    filesystem_info.set_total_nodes(allocator, blobfs_->Info().inode_count);
   }
 
-  uint64_t used_nodes;
   if (query & FilesystemInfoQuery::USED_NODES) {
-    used_nodes = blobfs_->Info().alloc_inode_count;
-    builder.set_used_nodes(fidl::unowned_ptr(&used_nodes));
+    filesystem_info.set_used_nodes(allocator, blobfs_->Info().alloc_inode_count);
   }
 
-  zx::event fs_id;
   if (query & FilesystemInfoQuery::FS_ID) {
+    zx::event fs_id;
     zx_status_t status = blobfs_->GetFsId(&fs_id);
     if (status != ZX_OK) {
       completer.ReplyError(status);
       return;
     }
-    builder.set_fs_id(fidl::unowned_ptr(&fs_id));
+    filesystem_info.set_fs_id(allocator, std::move(fs_id));
   }
 
-  uint32_t block_size;
   if (query & FilesystemInfoQuery::BLOCK_SIZE) {
-    block_size = kBlobfsBlockSize;
-    builder.set_block_size(fidl::unowned_ptr(&block_size));
+    filesystem_info.set_block_size(allocator, kBlobfsBlockSize);
   }
 
-  uint32_t max_node_name_size;
   if (query & FilesystemInfoQuery::MAX_NODE_NAME_SIZE) {
-    max_node_name_size = digest::kSha256HexLength;
-    builder.set_max_node_name_size(fidl::unowned_ptr(&max_node_name_size));
+    filesystem_info.set_max_node_name_size(allocator, digest::kSha256HexLength);
   }
 
-  fuchsia_fs::wire::FsType fs_type;
   if (query & FilesystemInfoQuery::FS_TYPE) {
-    fs_type = fuchsia_fs::wire::FsType::BLOBFS;
-    builder.set_fs_type(fidl::unowned_ptr(&fs_type));
+    filesystem_info.set_fs_type(allocator, fuchsia_fs::wire::FsType::BLOBFS);
   }
 
-  fidl::StringView name;
   if (query & FilesystemInfoQuery::NAME) {
-    name = fidl::StringView(kFsName);
-    builder.set_name(fidl::unowned_ptr(&name));
+    fidl::StringView name(kFsName);
+    filesystem_info.set_name(allocator, std::move(name));
   }
 
-  fidl::StringView device_path;
   char name_buf[fuchsia_io2::wire::MAX_PATH_LENGTH];
-  size_t name_len;
   if (query & FilesystemInfoQuery::DEVICE_PATH) {
+    size_t name_len;
     zx_status_t status =
         blobfs_->Device()->GetDevicePath(fuchsia_io2::wire::MAX_PATH_LENGTH, name_buf, &name_len);
     if (status != ZX_OK) {
@@ -99,11 +86,11 @@ void QueryService::GetInfo(FilesystemInfoQuery query, GetInfoCompleter::Sync& co
     }
     // It appears that the |name_len| returned by |GetDevicePath| includes a trailing NUL.
     ZX_ASSERT(name_buf[name_len - 1] == '\0');
-    device_path = fidl::StringView(name_buf, name_len - 1);
-    builder.set_device_path(fidl::unowned_ptr(&device_path));
+    fidl::StringView device_path(name_buf, name_len - 1);
+    filesystem_info.set_device_path(allocator, std::move(device_path));
   }
 
-  completer.ReplySuccess(builder.build());
+  completer.ReplySuccess(std::move(filesystem_info));
 }
 
 void QueryService::IsNodeInFilesystem(zx::event token,
