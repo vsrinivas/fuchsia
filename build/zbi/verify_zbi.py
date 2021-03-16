a130cdd05dc0f42e56a1cbdccab42cf4a8644d67
Yu Shan <shanyu@google.com>
2021-03-16 23:08:01 +0000

    [Security] Add bootfs filelist check.
    Add logic to verify bootfs filelist against a golden file in
    verify_zbi.py. Add rules to specify a bootfs fileslist golden
    file to verify against.
    
    This would make sure every bootfs filelist change would be
    reviewed.
    
    Tests: Local build.
    Bug: 72195
    Change-Id: Ib550fe6ce210c33dc998f794c41b523aecebf5e8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/500277
    Reviewed-by: David Turner <digit@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Yu Shan <shanyu@google.com>

---
+++ build/zbi/verify_zbi.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Use scrutiny to verify various ZBI items."""
+
+import argparse
+import difflib
+import os
+import subprocess
+import sys
+import shlex
+import tempfile
+
+SUPPORTED_TYPES = ['kernel_cmdline', 'bootfs_filelist']
+
+
+def print_error(msg):
+    print(msg, file=sys.stderr)
+
+
+def main(input_args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--zbi-file', help='Path to the zbi to verify', required=True)
+    parser.add_argument(
+        '--scrutiny',
+        help='Path to the scrutiny tool used for verifying kernel cmdline',
+        required=True)
+    parser.add_argument(
+        '--fuchsia-dir',
+        help='Path to fuchsia root directory, required for scrutiny to work',
+        required=True)
+    parser.add_argument(
+        '--golden-file', help='Path to the golden file', required=True)
+    parser.add_argument(
+        '--stamp', help='Path to the victory file', required=True)
+    parser.add_argument(
+        '--type',
+        help=('The type of the ZBI item to verify'),
+        choices=SUPPORTED_TYPES,
+        required=True)
+    args = parser.parse_args(input_args)
+
+    try:
+        if not verify_zbi(args):
+            return 1
+    except (AssertionError, IOError) as e:
+        print_error(str(e))
+        return 1
+    except CmdlineFormatError as e:
+        print_error('kernel cmdline is not formatted correctly: {}'.format(e))
+        return 1
+    except subprocess.CalledProcessError as e:
+        print_error('failed to run scrutiny: {}'.format(e))
+        return 1
+    with open(args.stamp, 'w') as stamp_file:
+        stamp_file.write('Golden!\n')
+    return 0
+
+
+def verify_zbi(args):
+    """verify_zbi verifies the ZBI image and return True if verified.
+
+    Raises:
+      AssertionError: If verification fails.
+      IOError: If verification fails.
+      subprocess.CalledProcessError: If failed to run scrutiny.
+      CmdlineFormatError: If the kernel cmdline is not formatted correctly.
+    """
+    # Check for some necessary files/dirs exist first.
+    for file in [args.scrutiny, args.zbi_file, args.fuchsia_dir]:
+        assert os.path.exists(file)
+
+    with tempfile.TemporaryDirectory() as tmp:
+        subprocess.check_call(
+            [
+                args.scrutiny, '-c', 'tool.zbi.extract --input ' +
+                shlex.quote(args.zbi_file) + ' --output ' + shlex.quote(tmp)
+            ],
+            env={'FUCHSIA_DIR': args.fuchsia_dir})
+
+        if args.type == 'kernel_cmdline':
+            return verify_kernel_cmdline(args.golden_file, tmp)
+        elif args.type == 'bootfs_filelist':
+            return verify_bootfs_filelist(args.golden_file, tmp)
+
+        return True
+
+
+def verify_kernel_cmdline(kernel_cmdline_golden_file, scrutiny_out):
+    """verify_zbi verifies the kernel cmdline in ZBI image.
+
+    Raises:
+      IOError: If verification fails.
+      CmdlineFormatError: If the kernel cmdline is not formatted correctly.
+    """
+    with open(kernel_cmdline_golden_file, 'r') as f:
+        golden_file_content = f.read().strip()
+    if not os.path.exists(os.path.join(scrutiny_out, 'sections',
+                                       'cmdline.blk')):
+        # We find no kernel cmdline. Check whether the golden file is empty.
+        if not golden_file_content:
+            # Golden file is empty. Pass the check.
+            return True
+        else:
+            print_error('Found no kernel cmdline in ZBI')
+            print_error(
+                'Please update kernel cmdline golden file at ' +
+                golden_file_content + ' to be an empty file')
+            return False
+    with open(os.path.join(scrutiny_out, 'sections', 'cmdline.blk'), 'r') as f:
+        # The cmdline.blk contains a trailing \x00.
+        cmdline = f.read().strip().rstrip('\x00')
+    return compare_cmdline(
+        cmdline, golden_file_content, kernel_cmdline_golden_file)
+
+
+def verify_bootfs_filelist(bootfs_filelist_golden_file, scrutiny_out):
+    """verify_zbi verifies the bootFS filelist in ZBI image.
+
+    Raises:
+      IOError: If verification fails.
+    """
+    with open(bootfs_filelist_golden_file, 'r') as f:
+        golden_file_content = f.read().strip()
+    bootfs_folder = os.path.join(scrutiny_out, 'bootfs')
+    bootfs_files = []
+    for root, _, files in os.walk(bootfs_folder):
+        for file in files:
+            bootfs_files.append(
+                os.path.relpath(os.path.join(root, file), bootfs_folder))
+    got_content = '\n'.join(sorted(bootfs_files))
+
+    if golden_file_content != got_content:
+        print_error('BootFS file list mismatch!')
+        print_error(
+            'Please update bootFS file list golden file at ' +
+            bootfs_filelist_golden_file + ' to:')
+        print_error('```')
+        print_error(got_content)
+        print_error('```')
+        print_error('')
+        print_error('Diff:')
+        print_error(difflib.context_diff(golden_file_content, got_content))
+        return False
+    return True
+
+
+class CmdlineFormatError(Exception):
+    """Exception thrown when kernel cmdline is in invalid format."""
+
+    def __init__(self, msg):
+        Exception.__init__(self)
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+def compare_cmdline(actual_cmdline, golden_cmdline, golden_file):
+    """compare_cmdline compares the actual cmdline with the golden cmdline.
+
+    Raises:
+      CmdlineFormatError: If the kernel cmdline is not formatted correctly.
+    """
+    golden_cmd = generate_sorted_cmdline(golden_cmdline, '\n')
+    actual_cmd = generate_sorted_cmdline(actual_cmdline, ' ')
+    if golden_cmd != actual_cmd:
+        print_error('Kernel cmdline mismatch!')
+        print_error(
+            'Please update kernel cmdline golden file at ' + golden_file +
+            ' to:')
+        print_error('```')
+        print_error(actual_cmd)
+        print_error('```')
+        print_error('')
+        print_error('Diff:')
+        print_error(difflib.context_diff(golden_cmd, actual_cmd))
+        return False
+    return True
+
+
+def generate_sorted_cmdline(cmdline, splitter):
+    """generate_sorted_cmdline generates a kernel cmdline sorted by entry keys.
+
+    Raises:
+      CmdlineFormatError: If the kernel cmdline is not formatted correctly.
+    """
+    cmdline_entries = {}
+    entries = cmdline.split(splitter)
+    for entry in entries:
+        if len(entry.split('=')) > 2:
+            raise CmdlineFormatError(
+                'invalid kernel cmdline, key value pair: ' + entry)
+        key, _, value = entry.partition('=')
+        if key in cmdline_entries:
+            raise CmdlineFormatError('duplicate kernel cmdline key: ' + key)
+        cmdline_entries[key] = value
+
+    return '\n'.join(
+        ('%s=%s' % (key, value)) if value else key
+        for key, value in sorted(cmdline_entries.items()))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
---
+++ build/zbi/verify_zbi_test.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+# Copyright 2020 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+r"""Unit test for verify_zbi.py.
+
+Need to have SCRUTINY and ZBI environmental variables set.
+
+To manually run this test:
+
+  SCRUTINY=~/fuchsia/out/default/host_x64/scrutiny \
+  ZBI=~/fuchsia/out/default/host_x64/zbi python3 \
+  verify_zbi_test.py
+"""
+import os
+import subprocess
+import sys
+import tempfile
+import unittest
+import unittest.mock as mock
+
+import verify_zbi
+
+SUBPROCESS_RUN = subprocess.run
+
+
+def verify_kernel_cmdline(golden, actual):
+    with tempfile.TemporaryDirectory() as test_folder:
+        golden_file = os.path.join(test_folder, 'golden')
+        stamp_file = os.path.join(test_folder, 'stamp')
+        fuchsia_folder = os.path.join(test_folder, 'fuchsia')
+        test_zbi = os.path.join(test_folder, 'test.zbi')
+        cmdline_file = os.path.join(test_folder, 'cmdline')
+        scrutiny = os.environ['SCRUTINY']
+        with open(golden_file, 'w+') as f:
+            f.write(golden)
+        with open(cmdline_file, 'wb+') as f:
+            f.write(actual)
+
+        # Use ZBI to create a test.zbi that only contains cmdline.
+        subprocess.check_call(
+            [os.environ['ZBI'], '-o', test_zbi, '-T', 'CMDLINE', cmdline_file])
+        os.mkdir(fuchsia_folder)
+
+        args = [
+            '--type', 'kernel_cmdline', '--zbi-file', test_zbi, '--scrutiny',
+            scrutiny, '--fuchsia-dir', fuchsia_folder, '--golden-file',
+            golden_file, '--stamp', stamp_file
+        ]
+        # Verify the cmdline in the generated ZBI.
+        return verify_zbi.main(args)
+
+
+def verify_bootfs_filelist(want_filelist, got_files):
+    with tempfile.TemporaryDirectory() as test_folder:
+        golden_file = os.path.join(test_folder, 'golden')
+        stamp_file = os.path.join(test_folder, 'stamp')
+        fuchsia_folder = os.path.join(test_folder, 'fuchsia')
+        os.mkdir(fuchsia_folder)
+        test_zbi = os.path.join(test_folder, 'test.zbi')
+        with open(golden_file, 'w+') as f:
+            f.write(want_filelist)
+
+        fake_scrutiny = os.path.join(test_folder, 'fake_scrutiny')
+        with open(fake_scrutiny, 'w+') as f:
+            f.write('fake scrutiny')
+
+        # Create a dummy test.zbi. We are not going to use the real scrutiny to
+        # parse it so its content doesn't matter.
+        with open(test_zbi, 'w+') as f:
+            f.write('test ZBI')
+
+        fake_subprocess = FakeSubprocess(got_files, [])
+        with mock.patch('subprocess.check_call') as mock_run:
+            mock_run.side_effect = fake_subprocess.run
+
+            args = [
+                '--type', 'bootfs_filelist', '--zbi-file', test_zbi,
+                '--scrutiny', fake_scrutiny, '--fuchsia-dir', fuchsia_folder,
+                '--golden-file', golden_file, '--stamp', stamp_file
+            ]
+            return verify_zbi.main(args)
+
+
+class RunVerifyZbiKernelCmdlineTest(unittest.TestCase):
+
+    def test_verify_kernel_cmdline_sucess_normal_case(self):
+        self.assertEqual(
+            0,
+            verify_kernel_cmdline(
+                'key1=v1\nkey2=v2\nkey3=v3', b'key1=v1 key2=v2 key3=v3'))
+
+    def test_verify_kernel_cmdline_success_order_diff(self):
+        self.assertEqual(
+            0,
+            verify_kernel_cmdline(
+                'key1=v1\nkey2=v2\nkey3=v3', b'key2=v2 key1=v1 key3=v3'))
+
+    def test_verify_kernel_cmdline_success_no_value_option(self):
+        self.assertEqual(
+            0, verify_kernel_cmdline('option1\noption2', b'option1 option2'))
+
+    def test_verify_kernel_cmdline_fail_golden_empty(self):
+        self.assertEqual(
+            1, verify_kernel_cmdline('', b'key2=v2 key1=v1 key3=v3'))
+
+    def test_verify_kernel_cmdline_fail_missing_key2(self):
+        self.assertEqual(
+            1, verify_kernel_cmdline('key1=v1\nkey2=v2', b'key1=v1'))
+
+    def test_verify_kernel_cmdline_fail_key1_mismatch(self):
+        self.assertEqual(
+            1, verify_kernel_cmdline('key1=v1\nkey2=v2', b'key1=v2 key2=v2'))
+
+    def test_verify_kernel_cmdline_fail_key2_mismatch(self):
+        self.assertEqual(
+            1, verify_kernel_cmdline('key1=v1\nkey2=v2', b'key1=v1 key2=v1'))
+
+    def test_verify_kernel_cmdline_fail_additional_key3(self):
+        self.assertEqual(
+            1,
+            verify_kernel_cmdline(
+                'key1=v1\nkey2=v2', b'key1=v1 key2=v2 key3=v3'))
+
+    def test_verify_kernel_cmdline_fail_invalid_format(self):
+        self.assertEqual(
+            1, verify_kernel_cmdline('key1=v1\nkey2=v2', b'invalid=format=1'))
+
+    def test_verify_kernel_cmdline_fail_option1_missing(self):
+        self.assertEqual(
+            1, verify_kernel_cmdline('option1\noption2', b'option2'))
+
+    def test_verify_kernel_cmdline_fail_additional_option3(self):
+        self.assertEqual(
+            1,
+            verify_kernel_cmdline(
+                'option1\noption2', b'option1 option2 option3'))
+
+    def test_verify_kernel_cmdline_zbi_not_found(self):
+        with tempfile.TemporaryDirectory() as test_folder:
+            golden_file = os.path.join(test_folder, 'golden')
+            stamp_file = os.path.join(test_folder, 'stamp')
+            fuchsia_folder = os.path.join(test_folder, 'fuchsia')
+            test_zbi = os.path.join(test_folder, 'test.zbi')
+            scrutiny = os.environ['SCRUTINY']
+            with open(golden_file, 'w+') as f:
+                f.write('option1')
+
+            # Do not create test_zbi
+
+            os.mkdir(fuchsia_folder)
+
+            args = [
+                '--type', 'kernel_cmdline', '--zbi-file', test_zbi,
+                '--scrutiny', scrutiny, '--fuchsia-dir', fuchsia_folder,
+                '--golden-file', golden_file, '--stamp', stamp_file
+            ]
+            self.assertEqual(1, verify_zbi.main(args))
+
+    def test_verify_kernel_cmdline_success_no_cmdline_found(self):
+        with tempfile.TemporaryDirectory() as test_folder:
+            golden_file = os.path.join(test_folder, 'golden')
+            stamp_file = os.path.join(test_folder, 'stamp')
+            fuchsia_folder = os.path.join(test_folder, 'fuchsia')
+            test_zbi = os.path.join(test_folder, 'test.zbi')
+            scrutiny = os.environ['SCRUTINY']
+
+            # Create an empty golden file
+            with open(golden_file, 'w+') as f:
+                f.write('')
+
+            # Use ZBI to create a test.zbi with no cmdline.
+            subprocess.check_call([os.environ['ZBI'], '-o', test_zbi])
+
+            os.mkdir(fuchsia_folder)
+
+            args = [
+                '--type', 'kernel_cmdline', '--zbi-file', test_zbi,
+                '--scrutiny', scrutiny, '--fuchsia-dir', fuchsia_folder,
+                '--golden-file', golden_file, '--stamp', stamp_file
+            ]
+            self.assertEqual(0, verify_zbi.main(args))
+
+    def test_verify_kernel_cmdline_fail_golden_empty_cmdline_found(self):
+        self.assertEqual(1, verify_kernel_cmdline('', b'option2'))
+
+    def test_verify_kernel_cmdline_fail_golden_not_empty_cmdline_not_found(
+            self):
+        with tempfile.TemporaryDirectory() as test_folder:
+            golden_file = os.path.join(test_folder, 'golden')
+            stamp_file = os.path.join(test_folder, 'stamp')
+            fuchsia_folder = os.path.join(test_folder, 'fuchsia')
+            test_zbi = os.path.join(test_folder, 'test.zbi')
+            scrutiny = os.environ['SCRUTINY']
+
+            # Create an empty golden file
+            with open(golden_file, 'w+') as f:
+                f.write('option1')
+
+            # Use ZBI to create a test.zbi with no cmdline.
+            subprocess.check_call([os.environ['ZBI'], '-o', test_zbi])
+
+            os.mkdir(fuchsia_folder)
+
+            args = [
+                '--type', 'kernel_cmdline', '--zbi-file', test_zbi,
+                '--scrutiny', scrutiny, '--fuchsia-dir', fuchsia_folder,
+                '--golden-file', golden_file, '--stamp', stamp_file
+            ]
+            self.assertEqual(1, verify_zbi.main(args))
+
+    def test_verify_bootfs_filelist_normal_case(self):
+        self.assertEqual(
+            0,
+            verify_bootfs_filelist(
+                'fileA\nfileB\nfileC', ['fileA', 'fileC', 'fileB']))
+
+    def test_verify_bootfs_filelist_sub_dir(self):
+        self.assertEqual(
+            0,
+            verify_bootfs_filelist(
+                'dir/fileA\ndir/fileC\nfileB',
+                ['dir/fileA', 'dir/fileC', 'fileB']))
+
+    def test_verify_bootfs_filelist_mismatch(self):
+        self.assertEqual(
+            1,
+            verify_bootfs_filelist('fileA\nfileB\nfileC', ['fileA', 'fileC']))
+
+    def test_verify_bootfs_filelist_sub_dir_mismatch(self):
+        self.assertEqual(
+            1,
+            verify_bootfs_filelist(
+                'dir/fileA\ndir/fileC\nfileB',
+                ['dir1/fileA', 'dir/fileC', 'fileB']))
+
+
+class FakeSubprocess(object):
+
+    def __init__(self, bootfs_filelist, static_pkgs_list):
+        self.bootfs_filelist = bootfs_filelist
+        self.static_pkgs_list = static_pkgs_list
+
+    def run(self, *argv, **kwargs):
+        del kwargs
+        command = argv[0]
+        print(command[0])
+        if command[0].endswith('fake_scrutiny'):
+            output = ''
+            scrutiny_commands = command[2].split(' ')
+            for i in range(0, len(scrutiny_commands) - 1):
+                if scrutiny_commands[i] == '--output':
+                    output = scrutiny_commands[i + 1]
+            os.mkdir(os.path.join(output, 'bootfs'))
+            for file in self.bootfs_filelist:
+                dirpath = os.path.dirname(os.path.join(output, 'bootfs', file))
+                if not os.path.exists(dirpath):
+                    os.mkdir(dirpath)
+                with open(os.path.join(output, 'bootfs', file), 'w+') as f:
+                    f.write('bootfs file')
+
+        return subprocess.CompletedProcess(args=[], returncode=0, stdout=b'')
+
+
+if __name__ == '__main__':
+    if 'SCRUTINY' not in os.environ or 'ZBI' not in os.environ:
+        print('Please set SCRUTINY and ZBI environmental path')
+        sys.exit(1)
+    unittest.main()
