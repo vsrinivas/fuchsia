fa2b10e8f9d279bd964506aa73cbce080530075f
Tamir Duberstein <tamird@google.com>
2021-03-16 19:41:03 +0000

    [fdio] Simplify watcher, avoid allocation
    It turns out that fuchsia.io/DirectoryWatcher isn't a proper FIDL
    protocol; messages sent on the channel contain raw events without a FIDL
    header. Thus it isn't possible to implement this as a subclass of
    `fuchsia_io::DirectoryWatcher::Interface`.
    
    Instead, this simplifies the implementation and removes the use of heap
    allocated structures.
    
    Fixed: 72249
    Change-Id: Iafc9c304b3f1cd2c47bf7c2fe0c9aa6e43e1e86c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/501780
    Commit-Queue: Tamir Duberstein <tamird@google.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>

--- sdk/lib/fdio/tests/fdio_watcher.cc
+++ sdk/lib/fdio/tests/fdio_watcher.cc
@@ -2,10 +2,97 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fuchsia/io/llcpp/fidl.h>
+#include <fuchsia/io/llcpp/fidl_test_base.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
+#include <lib/fdio/fd.h>
 #include <lib/fdio/watcher.h>
+#include <lib/fidl-async/cpp/bind.h>
+#include <lib/zx/time.h>
 
+#include <vector>
+
+#include <fbl/unique_fd.h>
 #include <zxtest/zxtest.h>
 
 TEST(WatcherTest, WatchInvalidDirFD) {
-  ASSERT_EQ(ZX_ERR_INVALID_ARGS, fdio_watch_directory(-1, nullptr, ZX_TIME_INFINITE, nullptr));
+  ASSERT_STATUS(fdio_watch_directory(-1, nullptr, ZX_TIME_INFINITE, nullptr), ZX_ERR_INVALID_ARGS);
+}
+
+template <typename F>
+class Server final : public fuchsia_io::testing::Directory_TestBase {
+ public:
+  explicit Server(F onWatch) : onWatch_(onWatch) {}
+
+  void NotImplemented_(const std::string& name, fidl::CompleterBase& completer) override {
+    ADD_FAILURE("%s should not be called", name.c_str());
+    completer.Close(ZX_ERR_NOT_SUPPORTED);
+  }
+
+  void Close(Interface::CloseCompleter::Sync& completer) override { completer.Reply(ZX_OK); }
+
+  void Describe(Interface::DescribeCompleter::Sync& completer) override {
+    fidl::aligned<fuchsia_io::wire::DirectoryObject> directory;
+    completer.Reply(fuchsia_io::wire::NodeInfo::WithDirectory(fidl::unowned_ptr(&directory)));
+  }
+
+  void Watch(uint32_t mask, uint32_t options, zx::channel watcher,
+             Interface::WatchCompleter::Sync& completer) override {
+    onWatch_(mask, options, std::move(watcher), completer);
+  }
+
+ private:
+  F onWatch_;
+};
+
+TEST(WatcherTest, Smoke) {
+  auto endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+  ASSERT_OK(endpoints.status_value());
+
+  Server server([](uint32_t mask, uint32_t options, zx::channel watcher,
+                   fuchsia_io::Directory::Interface::WatchCompleter::Sync& completer) {
+    uint8_t bytes[fuchsia_io::wire::MAX_BUF];
+    auto it = std::begin(bytes);
+
+    {
+      constexpr char name[] = "unsupported";
+      *it++ = fuchsia_io::wire::WATCH_EVENT_IDLE + 1;
+      *it++ = sizeof(name);
+      it = std::copy(std::cbegin(name), std::cend(name), it);
+    }
+    {
+      constexpr char name[] = "valid";
+      *it++ = fuchsia_io::wire::WATCH_EVENT_ADDED;
+      *it++ = sizeof(name);
+      it = std::copy(std::cbegin(name), std::cend(name), it);
+    }
+    {
+      // Incomplete; event without name.
+      *it++ = fuchsia_io::wire::WATCH_EVENT_ADDED;
+      *it++ = 1;
+    }
+
+    completer.Reply(watcher.write(0, bytes, std::distance(std::begin(bytes), it), nullptr, 0));
+  });
+  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
+  ASSERT_OK(fidl::BindSingleInFlightOnly(loop.dispatcher(), std::move(endpoints->server), &server));
+  ASSERT_OK(loop.StartThread("fake-directory-server"));
+
+  fbl::unique_fd directory;
+  ASSERT_OK(
+      fdio_fd_create(endpoints->client.channel().release(), directory.reset_and_get_address()));
+
+  std::vector<std::pair<int, std::string>> events;
+  ASSERT_STATUS(fdio_watch_directory(
+                    directory.get(),
+                    [](int dirfd, int event, const char* name, void* cookie) {
+                      auto events_cookie = reinterpret_cast<decltype(events)*>(cookie);
+                      events_cookie->emplace_back(event, std::string(name));
+                      return ZX_OK;
+                    },
+                    zx::time::infinite().get(), &events),
+                ZX_ERR_PEER_CLOSED);
+  decltype(events) expected_events = {{WATCH_EVENT_ADD_FILE, "valid"}};
+  ASSERT_EQ(events, expected_events);
 }
--- sdk/lib/fdio/watcher.cc
+++ sdk/lib/fdio/watcher.cc
@@ -4,34 +4,26 @@
 
 #include <fuchsia/io/llcpp/fidl.h>
 #include <lib/fdio/watcher.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <zircon/syscalls.h>
 #include <zircon/types.h>
+
+#include <fbl/span.h>
 
 #include "fdio_unistd.h"
 
 namespace fio = fuchsia_io;
 
-using fdio_watcher_t = struct fdio_watcher {
-  zx_handle_t h;
-  watchdir_func_t func;
-  void* cookie;
-  int fd;
-};
-
-static zx_status_t fdio_watcher_create(int dirfd, fdio_watcher_t** out) {
+__EXPORT
+zx_status_t fdio_watch_directory(int dirfd, watchdir_func_t cb, zx_time_t deadline, void* cookie) {
   fdio_ptr io = fd_to_io(dirfd);
   if (io == nullptr) {
     return ZX_ERR_INVALID_ARGS;
   }
 
   zx_handle_t handle;
-  zx_status_t status = io->borrow_channel(&handle);
-  if (status != ZX_OK) {
+  if (zx_status_t status = io->borrow_channel(&handle); status != ZX_OK) {
     return status;
   }
-  auto directory = fidl::UnownedClientEnd<fio::Directory>(handle);
+  fidl::UnownedClientEnd<fio::Directory> directory(handle);
   if (!directory.is_valid()) {
     return ZX_ERR_NOT_SUPPORTED;
   }
@@ -42,108 +34,71 @@ static zx_status_t fdio_watcher_create(int dirfd, fdio_watcher_t** out) {
   }
 
   auto result = fio::Directory::Call::Watch(directory, fio::wire::WATCH_MASK_ALL, 0,
-                                            endpoints->server.TakeChannel());
-  status = result.status();
-  if (status != ZX_OK) {
+                                            endpoints->client.TakeChannel());
+  if (zx_status_t status = result.status(); status != ZX_OK) {
     return status;
   }
-  fio::Directory::WatchResponse* response = result.Unwrap();
-  status = response->s;
-  if (status != ZX_OK) {
+  if (zx_status_t status = result->s; status != ZX_OK) {
     return status;
   }
 
-  auto watcher = static_cast<fdio_watcher_t*>(malloc(sizeof(fdio_watcher_t)));
-  watcher->h = endpoints->client.channel().release();
-  *out = watcher;
-  return ZX_OK;
-}
-
-// watcher process expects the msg buffer to be len + 1 in length
-// as it drops temporary nuls in it while dispatching
-static zx_status_t fdio_watcher_process(fdio_watcher_t* w, uint8_t* msg, size_t len) {
-  // Message Format: { OP, LEN, DATA[LEN] }
-  while (len >= 2) {
-    unsigned event = *msg++;
-    unsigned namelen = *msg++;
-
-    if (len < (namelen + 2u)) {
-      break;
-    }
-
-    switch (event) {
-      case fio::wire::WATCH_EVENT_ADDED:
-      case fio::wire::WATCH_EVENT_EXISTING:
-        event = WATCH_EVENT_ADD_FILE;
-        break;
-      case fio::wire::WATCH_EVENT_REMOVED:
-        event = WATCH_EVENT_REMOVE_FILE;
-        break;
-      case fio::wire::WATCH_EVENT_IDLE:
-        event = WATCH_EVENT_WAITING;
-        break;
-      default:
-        // unsupported event
-        continue;
-    }
-
-    uint8_t tmp = msg[namelen];
-    msg[namelen] = 0;
-
-    zx_status_t status;
-    if ((status = w->func(w->fd, event, (char*)msg, w->cookie)) != ZX_OK) {
-      return status;
-    }
-    msg[namelen] = tmp;
-    len -= (namelen + 2);
-    msg += namelen;
-  }
-
-  return ZX_OK;
-}
-
-static zx_status_t fdio_watcher_loop(fdio_watcher_t* w, zx_time_t deadline) {
   for (;;) {
-    // extra byte for watcher process use
-    uint8_t msg[fio::wire::MAX_BUF + 1];
-    uint32_t sz = fio::wire::MAX_BUF;
-    zx_status_t status;
-    if ((status = zx_channel_read(w->h, 0, msg, nullptr, sz, 0, &sz, nullptr)) < 0) {
-      if (status != ZX_ERR_SHOULD_WAIT) {
-        return status;
+    uint8_t bytes[fio::wire::MAX_BUF + 1];  // Extra byte for temporary null terminator.
+    uint32_t num_bytes;
+    zx_status_t status = endpoints->server.channel().read_etc(0, &bytes, nullptr, sizeof(bytes), 0,
+                                                              &num_bytes, nullptr);
+    if (status != ZX_OK) {
+      if (status == ZX_ERR_SHOULD_WAIT) {
+        status = endpoints->server.channel().wait_one(ZX_CHANNEL_READABLE | ZX_CHANNEL_PEER_CLOSED,
+                                                      zx::time(deadline), nullptr);
+        if (status != ZX_OK) {
+          return status;
+        }
+        continue;
       }
-      if ((status = zx_object_wait_one(w->h, ZX_CHANNEL_READABLE | ZX_CHANNEL_PEER_CLOSED, deadline,
-                                       nullptr)) < 0) {
-        return status;
-      }
-      continue;
-    }
-
-    if ((status = fdio_watcher_process(w, msg, sz)) != ZX_OK) {
       return status;
     }
+
+    // Message Format: { OP, LEN, DATA[LEN] }
+    fbl::Span span(bytes, num_bytes);
+    auto it = span.begin();
+    for (;;) {
+      if (std::distance(it, span.end()) < 2) {
+        break;
+      }
+      uint8_t event = *it++;
+      uint8_t len = *it++;
+      uint8_t* name = it;
+
+      if (std::distance(it, span.end()) < len) {
+        break;
+      }
+      it += len;
+
+      switch (event) {
+        case fio::wire::WATCH_EVENT_ADDED:
+        case fio::wire::WATCH_EVENT_EXISTING:
+          event = WATCH_EVENT_ADD_FILE;
+          break;
+        case fio::wire::WATCH_EVENT_REMOVED:
+          event = WATCH_EVENT_REMOVE_FILE;
+          break;
+        case fio::wire::WATCH_EVENT_IDLE:
+          event = WATCH_EVENT_WAITING;
+          break;
+        default:
+          // unsupported event
+          continue;
+      }
+
+      // The callback expects a null-terminated string.
+      uint8_t tmp = *it;
+      *it = 0;
+      status = cb(dirfd, event, reinterpret_cast<const char*>(name), cookie);
+      *it = tmp;
+      if (status != ZX_OK) {
+        return status;
+      }
+    }
   }
-}
-
-static void fdio_watcher_destroy(fdio_watcher_t* watcher) {
-  zx_handle_close(watcher->h);
-  free(watcher);
-}
-
-__EXPORT
-zx_status_t fdio_watch_directory(int dirfd, watchdir_func_t cb, zx_time_t deadline, void* cookie) {
-  fdio_watcher_t* watcher = nullptr;
-
-  zx_status_t status;
-  if ((status = fdio_watcher_create(dirfd, &watcher)) < 0) {
-    return status;
-  }
-
-  watcher->func = cb;
-  watcher->cookie = cookie;
-  watcher->fd = dirfd;
-  status = fdio_watcher_loop(watcher, deadline);
-
-  fdio_watcher_destroy(watcher);
-  return status;
 }
