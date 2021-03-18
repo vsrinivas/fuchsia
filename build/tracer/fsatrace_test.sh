7762b071668eb0fdc9802a4c3a53e5bb6c93a93c
David Fang <fangism@google.com>
2021-03-18 20:27:50 +0000

    [fsatrace] Test trace behavior of removing symlink.
    Confirm what fsatrace sees on different platforms.
    See https://github.com/jacereda/fsatrace/issues/41
    
    Test: ./build/tracer/fsatrace_test.sh
    
    Change-Id: I40931d2d0dac1a5edef469f73f7a9b9e0473f2bb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/502239
    Fuchsia-Auto-Submit: David Fang <fangism@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: David Turner <digit@google.com>

--- build/tracer/fsatrace_test.sh
+++ build/tracer/fsatrace_test.sh
@@ -17,6 +17,9 @@ then fsatrace="$1"
 else fsatrace=./prebuilt/fsatrace/fsatrace
 fi
 
+# Absolute path.
+fsatrace="$(readlink -f "$fsatrace")"
+
 # If a stamp file is named, touch it at the end of this script.
 stamp=
 if test "$#" -ge 2
@@ -28,19 +31,38 @@ fi
 # Note: To work on BSD and Linux mktemp, -t must be attached to the template.
 tmpd="$(mktemp -d -t tmp.fsatrace_test.XXXXX)"
 
+### Test: file copy
 echo "hello, world" > "$tmpd"/src.txt
-
 cp="$(which cp)"
-"$fsatrace" erwmdtq "$tmpd/trace.txt" -- cp "$tmpd"/src.txt "$tmpd"/dest.txt
+"$fsatrace" erwmdtq "$tmpd/cp_trace.txt" -- cp "$tmpd"/src.txt "$tmpd"/dest.txt
 
 # Use exit code of diff.
-diff -u "$tmpd/trace.txt" - <<EOF
+diff -u "$tmpd/cp_trace.txt" - <<EOF
 r|$cp
 q|$tmpd/src.txt
 r|$tmpd/src.txt
 q|$tmpd/src.txt
 w|$tmpd/dest.txt
 q|$tmpd/dest.txt
+EOF
+# If there is a difference, -e will exit here.
+
+### Test: remove symlink
+# See https://github.com/jacereda/fsatrace/issues/41
+# On Linux, removing a symlink *looks* like removing the link's target.
+
+rm="$(which rm)"
+pushd "$tmpd" > /dev/null 2>&1
+touch __temp__.txt
+ln -s __temp__.txt __link__.txt
+"$fsatrace" erwmdtq "$tmpd/rm_trace.txt" -- rm -f __link__.txt
+rm -f __temp__.txt
+popd > /dev/null 2>&1
+
+diff -u "$tmpd/rm_trace.txt" - <<EOF
+r|$rm
+q|$tmpd/__temp__.txt
+d|$tmpd/__temp__.txt
 EOF
 # If there is a difference, -e will exit here.
 
