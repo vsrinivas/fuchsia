abef16570a9e4cb515518439ccdd1ee9d54d0da2
Paul Daniel Faria <paulfaria@google.com>
2021-03-18 21:48:09 +0000

    [setui][handler] Wait for startup to complete
    Fix a bug where the SettingHandlerFactoryImpl did not properly
    wait for startup to complete before continuing on.
    
    Fixed: 72165
    Test: fx test -o setui_service_tests
    MULTIPLY: setui_service_tests
    Change-Id: I43e9968b34830f03b2a758e46b9ad0162b78e9fa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/500899
    Reviewed-by: Chip Fukuhara <cfukuhara@google.com>
    Reviewed-by: Sijie Chen <sijiec@google.com>
    Commit-Queue: Paul Faria <paulfaria@google.com>

--- garnet/bin/setui/src/handler/setting_handler_factory_impl.rs
+++ garnet/bin/setui/src/handler/setting_handler_factory_impl.rs
@@ -7,10 +7,12 @@ use crate::handler::base::{
 };
 use crate::handler::device_storage::DeviceStorageFactory;
 use crate::handler::setting_handler::{Command, Payload, State};
-use crate::message::base::{Audience, MessageEvent, MessengerType, Status};
+use crate::message::base::{Audience, MessageEvent, MessengerType};
+use crate::service;
 use crate::service::message::{Factory, Signature};
 use crate::service_context::ServiceContextHandle;
 use async_trait::async_trait;
+use fuchsia_syslog::fx_log_err;
 use futures::StreamExt;
 use std::collections::HashMap;
 use std::collections::HashSet;
@@ -76,14 +78,29 @@ impl<T: DeviceStorageFactory + Send + Sync> SettingHandlerFactory for SettingHan
             .send();
 
         // Wait for the startup phase to be over before continuing.
-        if let Some(MessageEvent::Status(Status::Received)) = controller_receptor.next().await {
-            // Startup phase is complete and had no errors. The proxy can assume it
-            // has an active controller with create() and startup() already run on it
-            // before handling its request.
-            return Ok(signature);
-        } else {
-            return Err(SettingHandlerFactoryError::HandlerStartupError(setting_type));
+        while let Some(message_event) = controller_receptor.next().await {
+            match message_event {
+                MessageEvent::Status(_) => {} // no-op
+                MessageEvent::Message(service::Payload::Controller(Payload::Result(result)), _) => {
+                    // Startup phase is complete. If it had no errors the proxy can assume it
+                    // has an active controller with create() and startup() already run on it
+                    // before handling its request.
+                    return result.map(|_| signature).map_err(|_| {
+                        SettingHandlerFactoryError::HandlerStartupError(setting_type)
+                    });
+                }
+                _ => {
+                    fx_log_err!(
+                        "Unexpected message response {:?} for {:?} controller startup request",
+                        message_event,
+                        setting_type
+                    );
+                    return Err(SettingHandlerFactoryError::HandlerStartupError(setting_type));
+                }
+            }
         }
+
+        panic!("Did not get any responses from {:?} controller startup", setting_type);
     }
 }
 
@@ -103,5 +120,131 @@ impl<T: DeviceStorageFactory + Send + Sync> SettingHandlerFactoryImpl<T> {
 
     pub fn register(&mut self, setting_type: SettingType, generate_function: GenerateHandler<T>) {
         self.generators.insert(setting_type, generate_function);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::handler::base::Request;
+    use crate::handler::device_storage::testing::InMemoryStorageFactory;
+    use crate::handler::setting_handler::controller::{Create, Handle};
+    use crate::handler::setting_handler::{
+        BoxedController, ClientImpl, ClientProxy, ControllerError, ControllerStateResult,
+        SettingHandlerResult,
+    };
+    use crate::message::base::{filter, Message, MessageType};
+    use crate::service;
+    use crate::service_context::ServiceContext;
+    use fuchsia_async as fasync;
+    use futures::future::FutureExt;
+    use futures::select;
+    use std::convert::TryFrom;
+
+    /// Test controller used to test startup waiting behavior in SettingHandlerFactoryImpl.
+    struct TestController;
+
+    #[async_trait]
+    impl Create for TestController {
+        async fn create(_client: ClientProxy) -> Result<Self, ControllerError> {
+            Ok(Self)
+        }
+    }
+
+    #[async_trait]
+    impl Handle for TestController {
+        // Not relevant.
+        async fn handle(&self, _request: Request) -> Option<SettingHandlerResult> {
+            None
+        }
+
+        // When we see a startup message, send a signal to signify we're done.
+        async fn change_state(&mut self, state: State) -> Option<ControllerStateResult> {
+            match state {
+                State::Startup => Some(Ok(())),
+                _ => None,
+            }
+        }
+    }
+
+    #[fasync::run_until_stalled(test)]
+    async fn ensure_startup_is_awaited() {
+        let messenger_factory = service::message::create_hub();
+        let mut factory_impl = SettingHandlerFactoryImpl::new(
+            {
+                let mut settings = HashSet::new();
+                settings.insert(SettingType::Unknown);
+                settings
+            },
+            ServiceContext::create(None, Some(messenger_factory.clone())),
+            Arc::new(InMemoryStorageFactory::new()),
+            Arc::new(AtomicU64::new(0)),
+        );
+
+        // Register generation of controller with factory_impl.
+        let generate_handler: GenerateHandler<InMemoryStorageFactory> =
+            Box::new(move |context: Context<InMemoryStorageFactory>| {
+                Box::pin(ClientImpl::create(
+                    context,
+                    Box::new(move |proxy| {
+                        Box::pin(async move {
+                            Ok(Box::new(TestController::create(proxy).await.unwrap())
+                                as BoxedController)
+                        })
+                    }),
+                ))
+            });
+        factory_impl.register(SettingType::Unknown, generate_handler);
+
+        // Create a broker that only listens to replies.
+        let (_, broker_receptor) = messenger_factory
+            .create(MessengerType::Broker(Some(filter::Builder::single(
+                filter::Condition::Custom(Arc::new(move |message: &Message<_, _, _>| {
+                    // Only filter for reply's that contain results.
+                    matches!(message.get_type(), MessageType::Reply(_))
+                        && Payload::try_from(message.payload())
+                            .map_or(false, |payload| matches!(payload, Payload::Result(_)))
+                })),
+            ))))
+            .await
+            .expect("could not create broker receptor");
+
+        let (_, receptor) = messenger_factory
+            .create(MessengerType::Unbound)
+            .await
+            .expect("messenger should be created");
+        // Generate the controller, but don't await it yet so we can time it with the response
+        // from the broker.
+        let mut generate_future = factory_impl
+            .generate(SettingType::Unknown, messenger_factory, receptor.get_signature())
+            .into_stream()
+            .fuse();
+        let mut broker_receptor = broker_receptor.fuse();
+
+        // We need to validate that the generate_done always completes after the startup_done
+        // signal. If this ever fails, it implies that the generator does not properly wait for
+        // startup to complete.
+        let mut generate_done = None;
+        let mut startup_done = None;
+        let mut idx: u8 = 0;
+        while generate_done.is_none() || startup_done.is_none() {
+            select! {
+                result = generate_future.select_next_some() => {
+                    result.expect("should have received a signature");
+                    generate_done = Some(idx);
+                    idx += 1;
+                }
+                maybe = broker_receptor.next() => {
+                    maybe.expect("should have gotten a reply");
+                    startup_done = Some(idx);
+                    idx += 1;
+                }
+                complete => break,
+            }
+        }
+
+        // Validate that the id for generate is larger than the one for startup,
+        // implying that generate finished after startup was done.
+        assert!(generate_done.unwrap() > startup_done.unwrap());
     }
 }
