eabb430a7bc0ae64d1b2b9e9f77b827497ce354a
Clayton Wilkinson <wilkinsonclay@google.com>
2021-03-19 18:58:29 +0000

    [clidoc] Adding subcommand keyword processing
    Several changes to handle generating reference docs for an unlisted
    command.
    
    1. Added positional arguments to command line to specify commands to
    process. If none are given, the existing executables are filtered
    against the allow list.
    
    2. Added setting the logger. This allows the log output on stdout.
    
    3. Refactored the test to use a standalone shell script and expected
    markdown output. This makes enhancing the test coverage easier.
    
    4.  This enables finding subcommands under the "subcommands:" heading
    which is common for golang.
    
    Change-Id: Ic3c4101479b118c014ba8b9ea739146aef7a56f7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/502458
    Reviewed-by: Kasia Hayden <kasiahayden@google.com>
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>

---
+++ tools/clidoc/src/testdata/tool_with_subcommands.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+# Top level help
+if [[ $1 == "--help" ]]; then
+    cat <<EOF
+Usage: host-tool-cmd
+Top level tool description
+
+Options:
+    --help   Display usage information
+    --verbose Print progress details
+
+Commands:
+    subcommand1  Starts subcommand1
+    subcommand2  Starts subcommand2
+    command      Should not be recursed
+    subcommand   Should not be recursed
+EOF
+elif [[ $1 == "subcommand1" ]] && [[ $2 == "--help" ]]; then
+    cat <<EOF
+Usage: host-tool-cmd subcommand1 <args>
+Description goes here subcommand1 with <args>
+
+Options:
+    --sc1   sc1 option for subcommand1
+EOF
+elif [[ $1 == "subcommand2" ]] && [[ $2 == "--help" ]]; then
+    cat <<EOF
+Usage: host-tool-cmd subcommand2
+Description goes here subcommand2
+
+EOF
+fi
