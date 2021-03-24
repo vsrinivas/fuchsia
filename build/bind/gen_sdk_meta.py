09d08bfa709450a6bd590e632f49fffcc2ee4449
Jocelyn Dang <jocelyndang@google.com>
2021-03-24 00:35:35 +0000

    [bind] Changes required for SDK inclusion.
    This will allow bind libraries and bindc to be included
    in the driver SDK. The changes are basically to add the same
    for bind as the existing banjo_library.gni, gen_sdk_meta.py
    and banjo_library.json.
    
    https://fuchsia.googlesource.com/fuchsia/+/refs/heads/master/build/banjo/banjo_library.gni
    https://fuchsia.googlesource.com/fuchsia/+/refs/heads/master/build/banjo/gen_sdk_meta.py
    https://fuchsia.googlesource.com/fuchsia/+/master/build/sdk/meta/banjo_library.json
    
    $ diff build/banjo/gen_sdk_meta.py build/bind/gen_sdk_meta.py
    2c2
    < # Copyright 2018 The Fuchsia Authors. All rights reserved.
    
    Change-Id: I816c1b73c35a47da4c3adcf5b18cb243827daf78
    ---
    > # Copyright 2021 The Fuchsia Authors. All rights reserved.
    24c24
    <         'type': 'banjo_library',
    ---
    >         'type': 'bind_library',
    36c36
    <         if type == 'banjo_library' or type == 'banjo_dummy_library':
    ---
    >         if type == 'bind_library':
    
    $ diff build/sdk/meta/banjo_library.json build/sdk/meta/bind_library.json
    3,4c3,4
    <   "id": "http://fuchsia.com/schemas/sdk/banjo_library.json",
    <   "description": "A BANJO library",
    ---
    >   "id": "http://fuchsia.com/schemas/sdk/bind_library.json",
    >   "description": "A BIND library",
    19c19
    <                 "banjo_library"
    ---
    >                 "bind_library"
    26c26
    <           "$ref": "common.json#/definitions/banjo_library_name"
    ---
    >           "$ref": "common.json#/definitions/bind_library_name"
    44c44
    <             "$ref": "common.json#/definitions/banjo_library_name"
    ---
    >             "$ref": "common.json#/definitions/bind_library_name"
    
    $ diff <(head -n 107 build/banjo/banjo_library.gni) <(head -n 113 build/bind/bind.gni)
    
    12,13c16
    <
    < template("_banjo_library_sdk") {
    ---
    > template("_bind_library_sdk") {
    20c23
    <   file_base = "banjo/$library_name"
    ---
    >   file_base = "bind/$library_name"
    21a25
    >
    24,40c28,43
    <   foreach(source, invoker.sources) {
    <     relative_source = rebase_path(source, ".")
    <     if (string_replace(relative_source, "..", "bogus") != relative_source) {
    <       # If the source file is not within the same directory, just use the file
    <       # name.
    <       relative_source = get_path_info(source, "file")
    <     }
    <     destination = "$file_base/$relative_source"
    <     sdk_sources += [ destination ]
    <     sdk_unrebased_sources += [ relative_source ]
    <     all_files += [
    <       {
    <         source = rebase_path(source)
    <         dest = destination
    <       },
    <     ]
    <   }
    ---
    >   source = invoker.source
    >   sdk_unrebased_sources += [ source ]
    >   relative_source = rebase_path(source, ".")
    >   if (string_replace(relative_source, "..", "bogus") != relative_source) {
    >     # If the source file is not within the same directory, just use the file
    >     # name.
    >     relative_source = get_path_info(source, "file")
    >   }
    >   destination = "$file_base/$relative_source"
    >   sdk_sources += [ destination ]
    >   all_files += [
    >     {
    >       source = rebase_path(source)
    >       dest = destination
    >     },
    >   ]
    63c66
    <     script = "//build/banjo/gen_sdk_meta.py"
    ---
    >     script = "//build/bind/gen_sdk_meta.py"
    87c90
    <     id = "sdk://banjo/$library_name"
    ---
    >     id = "sdk://bind/$library_name"
    94c97
    <       schema = "banjo_library"
    ---
    >       schema = "bind_library"
    
    Bug: 71473
    Change-Id: I53ad5514619ac83bd7e6618b74f7a273e455f8ef
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/505339
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: Jocelyn Dang <jocelyndang@google.com>

---
+++ build/bind/gen_sdk_meta.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3.8
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import json
+import os
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser('Builds a metadata file')
+    parser.add_argument('--out', help='Path to the output file', required=True)
+    parser.add_argument('--name', help='Name of the library', required=True)
+    parser.add_argument(
+        '--root', help='Root of the library in the SDK', required=True)
+    parser.add_argument(
+        '--specs', help='Path to spec files of dependencies', nargs='*')
+    parser.add_argument('--sources', help='List of library sources', nargs='+')
+    args = parser.parse_args()
+
+    metadata = {
+        'type': 'bind_library',
+        'name': args.name,
+        'root': args.root,
+        'sources': args.sources,
+    }
+
+    deps = []
+    for spec in args.specs:
+        with open(spec, 'r') as spec_file:
+            data = json.load(spec_file)
+        type = data['type']
+        name = data['name']
+        if type == 'bind_library':
+            deps.append(name)
+        else:
+            raise Exception('Unsupported dependency type: %s' % type)
+    metadata['deps'] = deps
+
+    with open(args.out, 'w') as out_file:
+        json.dump(
+            metadata,
+            out_file,
+            indent=2,
+            sort_keys=True,
+            separators=(',', ': '))
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
