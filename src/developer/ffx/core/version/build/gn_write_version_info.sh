9d2cd2ae542cf809c0d4d88686e5691fe6606e94
James Tucker <raggi@google.com>
2021-03-25 22:42:03 +0000

    [ffx] ensure version information is generated
    Previously the script did not fail when the version information could
    not be gathered, and a recent change to the integration repository
    resulted in HEAD being ambiguous to git show in some user scenarios.
    We now rev-parse and then show that commit to generate our string,
    whcih is more in-line wiht the behavior that the Zircon git hash
    collection uses.
    
    The dependencies are changed to only depend on HEAD as well, as a
    small speculative change to try to somewhat reduce the frequency of
    needless incremental rebuilds in infrastructure. This again shares
    behavior with the Zircon git hash computation. A more complete
    solution should follow as described in the associated bug.
    
    Bug: 73152
    Change-Id: Ic5ead447ea4b9d1ce69de24d0a8f0feb053de93e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/507325
    Fuchsia-Auto-Submit: James Tucker <raggi@google.com>
    Reviewed-by: Jordon Wing <jwing@google.com>
    Commit-Queue: James Tucker <raggi@google.com>

--- src/developer/ffx/core/version/build/gn_write_version_info.sh
+++ src/developer/ffx/core/version/build/gn_write_version_info.sh
@@ -3,10 +3,20 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-OUTFILE=$1
-GIT_DIR=$2
+set -e
+OUTFILE="$1"
+GIT_DIR="$2"
 
-VERSION_INFO="$(TZ=UTC git --git-dir=$GIT_DIR show --no-patch --no-notes --pretty='%H-%ct' HEAD 2> /dev/null)"
+if [[ ! -d "$GIT_DIR" ]]; then
+  echo >&2 "Invalid GIT_DIR provided: $GIT_DIR"
+fi
+
+GIT_REV="$(git --git-dir=${GIT_DIR} rev-parse HEAD 2>/dev/null)"
+VERSION_INFO="$(TZ=UTC git --git-dir=${GIT_DIR} show --no-patch --no-notes --pretty='%H-%ct' ${GIT_REV} 2> /dev/null)"
+if [[ -z "$VERSION_INFO" ]]; then
+  echo >&2 "Failed to gather version information from ${GIT_DIR}"
+  exit 1
+fi
 
 # Update the existing file only if it's changed.
 if [ ! -r "$OUTFILE" ] || [ "$(<"$OUTFILE")" != "$VERSION_INFO" ]; then
