aeb9261e5e4841820e8141bd8e764667ef2c91f0
Charles Whitten <cwhitten@google.com>
2021-03-25 23:16:31 +0000

    [memfs][cts] Migrate memfsnull & memfsbasic tests to cts
    go/memfs-to-cts
    Migrates existing memfsnull and memfsbasic tests from //zircon/system/utest to //sdk/cts/tests.
    
    Tested:
    fx set core.x64 --with //sdk/cts/tests/pkg/memfs:tests
    fx test //sdk/cts/tests/pkg/memfs
    
    fx set bringup.x64 --with-base //bundles/bringup:tests
    fx build
    fx qemu -k -c zircon.autorun.boot=/boot/test/memfs
    
    Bug: 66792
    
    Change-Id: Id1bb98aefb3ad4f9faaf961ab6db044a78dc15d9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/505538
    Reviewed-by: Jeremy Manson <jeremymanson@google.com>
    Reviewed-by: John Shamoon <johnshamoon@google.com>
    Commit-Queue: Charles Whitten <cwhitten@google.com>

--- zircon/system/utest/memfs/memfs-tests.cc
+++ zircon/system/utest/memfs/memfs-tests.cc
@@ -28,60 +28,6 @@
 
 namespace {
 
-TEST(MemfsTests, TestMemfsNull) {
-  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
-  ASSERT_EQ(loop.StartThread(), ZX_OK);
-  memfs_filesystem_t* vfs;
-  zx_handle_t root;
-
-  ASSERT_EQ(memfs_create_filesystem(loop.dispatcher(), &vfs, &root), ZX_OK);
-  ASSERT_EQ(zx_handle_close(root), ZX_OK);
-  sync_completion_t unmounted;
-  memfs_free_filesystem(vfs, &unmounted);
-  ASSERT_EQ(sync_completion_wait(&unmounted, zx::duration::infinite().get()), ZX_OK);
-}
-
-TEST(MemfsTests, TestMemfsBasic) {
-  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
-  ASSERT_EQ(loop.StartThread(), ZX_OK);
-
-  // Create a memfs filesystem, acquire a file descriptor
-  memfs_filesystem_t* vfs;
-  zx_handle_t root;
-  ASSERT_EQ(memfs_create_filesystem(loop.dispatcher(), &vfs, &root), ZX_OK);
-  int fd;
-  ASSERT_EQ(fdio_fd_create(root, &fd), ZX_OK);
-
-  // Access files within the filesystem.
-  DIR* d = fdopendir(fd);
-
-  // Create a file
-  const char* filename = "file-a";
-  fd = openat(dirfd(d), filename, O_CREAT | O_RDWR);
-  ASSERT_GE(fd, 0);
-  const char* data = "hello";
-  ssize_t datalen = strlen(data);
-  ASSERT_EQ(write(fd, data, datalen), datalen);
-  ASSERT_EQ(lseek(fd, 0, SEEK_SET), 0);
-  char buf[32];
-  ASSERT_EQ(read(fd, buf, sizeof(buf)), datalen);
-  ASSERT_EQ(memcmp(buf, data, datalen), 0);
-  close(fd);
-
-  // Readdir the file
-  struct dirent* de;
-  ASSERT_NOT_NULL((de = readdir(d)));
-  ASSERT_EQ(strcmp(de->d_name, "."), 0);
-  ASSERT_NOT_NULL((de = readdir(d)));
-  ASSERT_EQ(strcmp(de->d_name, filename), 0);
-  ASSERT_NULL(readdir(d));
-
-  ASSERT_EQ(closedir(d), 0);
-  sync_completion_t unmounted;
-  memfs_free_filesystem(vfs, &unmounted);
-  ASSERT_EQ(sync_completion_wait(&unmounted, zx::duration::infinite().get()), ZX_OK);
-}
-
 TEST(MemfsTests, TestMemfsAppend) {
   async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
   ASSERT_EQ(loop.StartThread(), ZX_OK);
