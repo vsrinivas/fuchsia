8fbd5d2e8fe6d913b6c55bc264ff8b1edde0ac0d
Chris Phoenix <cphoenix@google.com>
2021-03-26 02:16:14 +0000

    [elephant] Refactor integration test
    Refactor integration.rs.
     - Split the big test function
     - Simplify a couple of static declarations
     - Make the app launch boilerplate more consistent
    
    Test: $ fx test //src/diagnostics/elephant
    
    Bug: 65024, 71350
    Change-Id: Ia593c1fe8e03e05c5a3fe57275b341360dfa70df
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/506880
    Reviewed-by: Chris Phoenix <cphoenix@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Chris Phoenix <cphoenix@google.com>

--- src/diagnostics/elephant/tests/integration.rs
+++ src/diagnostics/elephant/tests/integration.rs
@@ -5,9 +5,9 @@ use diagnostics_data::LifecycleType;
 use diagnostics_reader::{ArchiveReader, Lifecycle};
 use fidl_fuchsia_diagnostics_persist::DataPersistenceMarker;
 use fidl_fuchsia_sys::ComponentControllerEvent;
-use fuchsia_async::{self as fasync, futures::StreamExt};
-use fuchsia_component::client::{launch, launcher, App, AppBuilder};
-use lazy_static::lazy_static;
+use fuchsia_async::futures::StreamExt;
+use fuchsia_component::client::{launcher, App, AppBuilder};
+use log::*;
 use serde_json::{self, Value};
 use std::fs::{create_dir, File};
 use std::io::Read;
@@ -15,11 +15,8 @@ use std::{thread, time};
 
 static METADATA_KEY: &str = "metadata";
 static TIMESTAMP_KEY: &str = "timestamp";
-
-lazy_static! {
-    static ref INJECTED_STORAGE_DIR: &'static str = "/tmp/injected_storage";
-    static ref ELEPHANT_INJECTED_PATH: &'static str = "/cache";
-}
+static ELEPHANT_INJECTED_PATH: &str = "/cache";
+static INJECTED_STORAGE_DIR: &str = "/tmp/injected_storage";
 
 const ELEPHANT_URL: &str = "fuchsia-pkg://fuchsia.com/elephant-integration-tests#meta/elephant.cmx";
 const INSPECT_PROVIDER_URL: &str =
@@ -27,21 +24,50 @@ const INSPECT_PROVIDER_URL: &str =
 
 const TEST_ELEPHANT_SERVICE_NAME: &str = "fuchsia.diagnostics.persist.DataPersistence_test-service";
 
-async fn setup() -> (App, App) {
-    create_dir(*INJECTED_STORAGE_DIR).unwrap();
-    let elephant_app = AppBuilder::new(ELEPHANT_URL)
+/// Runs the elephant persistor and a test component that can have its inspect properties
+/// manipulated by the test via fidl. then just trigger persistence on elephant.
+#[fuchsia::test]
+async fn event_count_sampler_test() {
+    let _example_app = setup().await;
+
+    wait_for_inspect_ready().await;
+
+    let elephant_app = elephant().await;
+
+    let elephant_service = elephant_app
+        .connect_to_named_service::<DataPersistenceMarker>(TEST_ELEPHANT_SERVICE_NAME)
+        .unwrap();
+
+    elephant_service.persist("test-component-metric").await.unwrap();
+
+    verify_elephant_files();
+
+    // TODO(cphoenix)
+    // kill the elephant service, and then restart it with an AppBuilder using the same dir you originally used, check to see that
+    // the cache looks correct, re-persist, check again etc.
+}
+
+// Starts and returns an Elephant app. Assumes:
+//  - The INJECTED_STORAGE_DIR has been created.
+//  - No other Elephant app is running in that directory.
+async fn elephant() -> App {
+    AppBuilder::new(ELEPHANT_URL)
         .add_dir_to_namespace(
-            (*ELEPHANT_INJECTED_PATH).into(),
-            File::open(*INJECTED_STORAGE_DIR).unwrap(),
+            ELEPHANT_INJECTED_PATH.into(),
+            File::open(INJECTED_STORAGE_DIR).unwrap(),
         )
         .unwrap()
         .spawn(&launcher().unwrap())
-        .unwrap();
+        .unwrap()
+}
+
+async fn setup() -> App {
+    create_dir(INJECTED_STORAGE_DIR).unwrap();
 
     let arguments = vec!["--rows=5".to_string(), "--columns=3".to_string()];
 
     let example_app =
-        launch(&launcher().unwrap(), INSPECT_PROVIDER_URL.to_string(), Some(arguments)).unwrap();
+        AppBuilder::new(INSPECT_PROVIDER_URL).args(arguments).spawn(&launcher().unwrap()).unwrap();
 
     let mut component_stream = example_app.controller().take_event_stream();
 
@@ -60,15 +86,10 @@ async fn setup() -> (App, App) {
         }
     }
 
-    (elephant_app, example_app)
+    example_app
 }
 
-/// Runs the elephant persistor and a test component that can have its inspect properties
-/// manipulated by the test via fidl. then just trigger persistence on elephant.
-#[fasync::run_singlethreaded(test)]
-async fn event_count_sampler_test() {
-    let (elephant_app, _example_app) = setup().await;
-
+async fn wait_for_inspect_ready() {
     loop {
         let results = ArchiveReader::new()
             .snapshot::<Lifecycle>()
@@ -85,18 +106,15 @@ async fn event_count_sampler_test() {
             break;
         }
     }
+}
 
-    let elephant_service = elephant_app
-        .connect_to_named_service::<DataPersistenceMarker>(TEST_ELEPHANT_SERVICE_NAME)
-        .unwrap();
-
-    elephant_service.persist("test-component-metric").await.unwrap();
-
+fn verify_elephant_files() {
     loop {
         let persisted_data_path =
             "/tmp/injected_storage/current/test-service/test-component-metric";
         let file = File::open(persisted_data_path);
         if file.is_err() {
+            warn!("Persisted data file can't be opened (yet)");
             thread::sleep(time::Duration::from_secs(1));
             continue;
         }
@@ -123,6 +141,8 @@ async fn event_count_sampler_test() {
         // Just because the file was present doesn't mean we persisted. Creation and
         // writing isn't atomic, and we sometimes flake as we race between the creation and write.
         if string_result_array.is_empty() {
+            warn!("No data has been written to the persisted file (yet)");
+            thread::sleep(time::Duration::from_secs(1));
             continue;
         }
 
@@ -138,10 +158,6 @@ async fn event_count_sampler_test() {
         assert_eq!(sorted_results_json_struct, expected_result);
         break;
     }
-
-    // TODO(cphoenix)
-    // kill the elephant service, and then restart it with an AppBuilder using the same dir you originally used, check to see that
-    // the cache looks correct, re-persist, check again etc.
 }
 
 fn expected_text() -> String {
