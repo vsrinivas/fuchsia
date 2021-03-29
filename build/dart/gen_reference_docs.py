0fc88a8356cda2c382ea6b4891fc196c08a297c5
Jonathan Koren <jdkoren@google.com>
2021-03-29 17:27:44 +0000

    [dart][docs] script to generate dart documentation
    This allows building all the dart reference docs by running
      fx set ... --with //sdk/dart:dart_sdk_docs
      fx build sdk/dart:dart_sdk_docs
    
    Bug: 72283
    Change-Id: If2dd9151e00aecf6b0419829b7a60c64b5c0521e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/502039
    Reviewed-by: Renato Mangini Dias <mangini@google.com>
    Reviewed-by: Chase Latta <chaselatta@google.com>
    Reviewed-by: Naud Ghebre <naudzghebre@google.com>
    Commit-Queue: Jonathan Koren <jdkoren@google.com>

---
+++ build/dart/gen_reference_docs.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3.8
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Generate Dart reference docs for one or more Fuchsia packages.
+
+This script uses Dartdoc, which documents a single package. If called with more
+than one package, an intermediary package is generated in order to document all
+the given packages at once; in that case, the --gen-dir argument is required. If
+this behavior is not desired, do not pass more than one package argument.
+"""
+
+import argparse
+import json
+import os
+import shutil
+import subprocess
+import sys
+
+FUCHSIA_ROOT = os.path.dirname(  # $root
+    os.path.dirname(             # build
+    os.path.dirname(             # dart
+    os.path.abspath(__file__))))
+if sys.version_info[0] >= 3:
+  sys.path += [os.path.join(FUCHSIA_ROOT, 'third_party', 'pyyaml', 'lib3')]
+else:
+  sys.path += [os.path.join(FUCHSIA_ROOT, 'third_party', 'pyyaml', 'lib')]
+
+import yaml
+
+_PUBSPEC_CONTENT = """name: Fuchsia
+homepage: https://fuchsia.dev/reference/dart
+description: API documentation for fuchsia
+environment:
+  sdk: '>=2.10.0 <3.0.0'
+dependencies:
+"""
+
+
+def is_dart_package_dir(package_dir):
+    """Returns whether or not a directory resembles a Dart package dir."""
+    if not os.path.isdir(package_dir):
+        print('%s is not a directory' % (package_dir))
+        return False
+
+    if not os.path.isdir(os.path.join(package_dir, 'lib')):
+        print('%s is missing a lib subdirectory' % (package_dir))
+        return False
+
+    pubspec_file = os.path.join(package_dir, 'pubspec.yaml')
+    if not os.path.exists(pubspec_file):
+        print('%s is missing a pubspec.yaml' % (package_dir))
+        return False
+
+    with open(pubspec_file) as f:
+        pubspec = yaml.load(f, Loader=yaml.Loader)
+        if not pubspec or pubspec['name'] != os.path.basename(package_dir):
+            print('%s has an invalid pubspec.yaml' % (package_dir))
+            return False
+
+    return True
+
+
+
+def collect_top_level_files(package_dir):
+    """Return a list of dart filenames under the package's lib directory."""
+    return sorted([
+        os.path.basename(p)
+        for p in os.listdir(
+            os.path.join(package_dir, 'lib'))
+        if os.path.basename(p).endswith('.dart')
+    ])
+
+
+def compose_pubspec_content(package_dict):
+    """Compose suitable contents for a pubspec file.
+
+    The pubspec will have dependencies on the packages in package_dict.
+
+    Args:
+        package_dict: Dictionary mapping package name to path.
+    Returns:
+        String with the pubspec content.
+    """
+    pubspec_content = _PUBSPEC_CONTENT
+    for pkg in package_dict:
+        pubspec_content += '  %s:\n    path: %s/\n' % (pkg, package_dict[pkg])
+    return pubspec_content
+
+
+def compose_imports_content(imports_dict):
+    """Compose suitable contents for an imports file.
+
+    The contents will include import statements for all items in imports_dict.
+
+    Args:
+        imports_dict: Dictionary mapping package name to a list of file names
+            belonging to that package.
+    Returns:
+        String with the imports content.
+    """
+    lines = []
+    for pkg in imports_dict:
+        for i in imports_dict[pkg]:
+            lines.append("import 'package:%s/%s';\n" % (pkg, i))
+    lines.sort()
+    return 'library Fuchsia;\n' + ''.join(lines)
+
+
+def fabricate_package(gen_dir, pubspec_content, imports_content):
+    """Write given pubspec and imports content in the given directory.
+
+    Args:
+        packages: A list of package directories to use as dependencies.
+        gen_dir: The directory for generated files.
+    """
+    if not os.path.exists(os.path.join(gen_dir, 'lib')):
+        os.makedirs(os.path.join(gen_dir, 'lib'))
+
+    # Fabricate pubspec.yaml.
+    pubspec_file = os.path.join(gen_dir, 'pubspec.yaml')
+    if os.path.exists(pubspec_file):
+        os.remove(pubspec_file)
+    with open(pubspec_file, 'w') as f:
+        f.write(pubspec_content)
+
+    # Fabricate a dart file with all the imports collected.
+    lib_file = os.path.join(gen_dir, 'lib', 'lib.dart')
+    if os.path.exists(lib_file):
+        os.remove(lib_file)
+    with open(lib_file, 'w') as f:
+        f.write(imports_content)
+
+
+def generate_docs(package_dir, out_dir, dart_prebuilt_dir):
+    """Generate dart reference docs.
+
+    Args:
+        package_dir: The directory of the package to document.
+        out_dir: The output directory for documentation.
+        dart_prebuilt_dir: The directory with dart executables (dartdoc, pub).
+
+    Returns:
+        0 if documentation was generated successfully, non-zero otherwise.
+    """
+    # Run pub over this package to fetch deps.
+    process = subprocess.run(
+        [os.path.join(dart_prebuilt_dir, 'pub'), 'get'],
+        cwd=package_dir,
+        capture_output=True,
+        universal_newlines=True)
+    if process.returncode:
+        print(process.stderr)
+        return 1
+
+    # Clear the outdir first.
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+
+    # Run dartdoc.
+    excludedPackages = ['Dart', 'logging']
+    process = subprocess.run(
+        [
+            os.path.join(dart_prebuilt_dir, 'dartdoc'),
+            '--auto-include-dependencies',
+            '--exclude-packages',
+            ','.join(excludedPackages),
+            '--output',
+            out_dir,
+            '--format',
+            'md'
+        ],
+        cwd=package_dir,
+        capture_output=True,
+        universal_newlines=True)
+    if process.returncode:
+        print(process.stderr)
+        return 1
+
+    return 0
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__,  # Prepend help doc with this file's docstring.
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        '-g',
+        '--gen-dir',
+        type=str,
+        required=False,
+        help='Location where intermediate files can be generated (should be '
+             'different from the output directory). This is required if more '
+             'than one package argument is given.')
+    parser.add_argument(
+        '-o',
+        '--out-dir',
+        type=str,
+        required=True,
+        help='Output location where generated docs should go')
+    parser.add_argument(
+        '-p',
+        '--prebuilts-dir',
+        type=str,
+        required=False,
+        default='',
+        help="Location of dart prebuilts, usually a Dart SDK's bin directory")
+    parser.add_argument(
+        'packages',
+        type=str,
+        nargs='+',
+        help='Paths of packages to document')
+
+    args = parser.parse_args()
+    if len(args.packages) == 1:
+        package_dir = args.packages[0]
+    else:
+        # Dartdoc runs over a single package only. Fabricate a package that
+        # depends on all the other packages and document that one.
+        if not args.gen_dir:
+            print('ERROR: --gen-dir is required to document multiple packages.')
+            parser.print_help()
+            return 1
+
+        package_dir = args.gen_dir
+
+        packages_dict = {}
+        imports_dict = {}
+        for pkg in args.packages:
+            if not is_dart_package_dir(pkg):
+                return 1
+
+            package_basename = os.path.basename(pkg)
+            packages_dict[package_basename] = os.path.abspath(pkg)
+            imports_dict[package_basename] = collect_top_level_files(pkg)
+
+        pubspec_content = compose_pubspec_content(packages_dict)
+        imports_content = compose_imports_content(imports_dict)
+        fabricate_package(package_dir, pubspec_content, imports_content)
+
+    return generate_docs(package_dir, args.out_dir, args.prebuilts_dir)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ build/dart/tests/gen_reference_docs_test.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3.8
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import shutil
+import subprocess
+import tempfile
+import unittest
+from unittest import mock
+
+import gen_reference_docs
+
+
+class GenReferenceDocsTest(unittest.TestCase):
+
+    def setUp(self):
+        # Create a temporary directory
+        self.temp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.temp_dir)
+
+    def test_is_dart_package_dir_does_not_exist(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        self.assertFalse(gen_reference_docs.is_dart_package_dir(package_dir))
+
+    def test_is_dart_package_dir_missing_lib(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        os.mkdir(package_dir)
+        self.assertFalse(gen_reference_docs.is_dart_package_dir(package_dir))
+
+    def test_is_dart_package_dir_missing_pubspec(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        lib_dir = os.path.join(package_dir, 'lib')
+        os.makedirs(lib_dir)
+        self.assertFalse(gen_reference_docs.is_dart_package_dir(package_dir))
+
+    def test_is_dart_package_dir_invalid_pubspec(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        lib_dir = os.path.join(package_dir, 'lib')
+        os.makedirs(lib_dir)
+        with open(os.path.join(package_dir, 'pubspec.yaml'), 'w') as f:
+            f.write('name: none\n')
+        self.assertFalse(gen_reference_docs.is_dart_package_dir(package_dir))
+
+    def test_is_dart_package_dir(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        lib_dir = os.path.join(package_dir, 'lib')
+        os.makedirs(lib_dir)
+        with open(os.path.join(package_dir, 'pubspec.yaml'), 'w') as f:
+            f.write('name: test_package\n')
+        self.assertTrue(gen_reference_docs.is_dart_package_dir(package_dir))
+
+    def test_collect_top_level_files(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        lib_dir = os.path.join(package_dir, 'lib')
+        os.makedirs(lib_dir)
+        for name in ('foo.dart', 'bar.dart', 'baz.dart', 'foo.java'):
+            with open(os.path.join(lib_dir, name), 'w') as f:
+                f.write('// comment')
+
+        result = gen_reference_docs.collect_top_level_files(package_dir)
+        self.assertEqual(result, ['bar.dart', 'baz.dart', 'foo.dart'])
+
+    def test_collect_top_level_files_empty(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        lib_dir = os.path.join(package_dir, 'lib')
+        os.makedirs(lib_dir)
+
+        result = gen_reference_docs.collect_top_level_files(package_dir)
+        self.assertEqual(result, [])
+
+    def test_collect_top_level_files_no_dart_files(self):
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        lib_dir = os.path.join(package_dir, 'lib')
+        os.makedirs(lib_dir)
+        for name in ('foo.py', 'foo.yaml'):
+            with open(os.path.join(lib_dir, name), 'w') as f:
+                f.write('# comment')
+
+        result = gen_reference_docs.collect_top_level_files(package_dir)
+        self.assertEqual(result, [])
+
+    def test_compose_pubspec_content(self):
+        packages = {
+            'foo': '/path/to/foo',
+            'bar': '/path/to/bar',
+        }
+        expected = expected_pubspec_content = """name: Fuchsia
+homepage: https://fuchsia.dev/reference/dart
+description: API documentation for fuchsia
+environment:
+  sdk: '>=2.10.0 <3.0.0'
+dependencies:
+  foo:
+    path: /path/to/foo/
+  bar:
+    path: /path/to/bar/
+"""
+        result = gen_reference_docs.compose_pubspec_content(packages)
+        self.assertEqual(result, expected)
+
+    def test_compose_imports_content(self):
+        imports = {
+            'foo': ['foo.dart', 'another_foo.dart'],
+            'bar': ['bar.dart'],
+        }
+        expected = """library Fuchsia;
+import 'package:bar/bar.dart';
+import 'package:foo/another_foo.dart';
+import 'package:foo/foo.dart';
+"""
+        result = gen_reference_docs.compose_imports_content(imports)
+        self.assertEqual(result, expected)
+
+    def test_fabricate_package(self):
+        gen_dir = os.path.join(self.temp_dir, 'gen')
+        pubspec = 'pubspec file'
+        imports = 'imports file'
+
+        gen_reference_docs.fabricate_package(gen_dir, pubspec, imports)
+
+        with open(os.path.join(gen_dir, 'pubspec.yaml'), 'r') as f:
+            self.assertEqual(f.read(), pubspec)
+        with open(os.path.join(gen_dir, 'lib', 'lib.dart'), 'r') as f:
+            self.assertEqual(f.read(), imports)
+
+    @mock.patch.object(subprocess, 'run')
+    def test_generate_docs(self, mock_run):
+        mock_run.return_value = subprocess.CompletedProcess(args='',
+                                                            returncode=0)
+
+        package_dir = os.path.join(self.temp_dir, 'test_package')
+        out_dir = os.path.join(self.temp_dir, 'out')
+        prebuilts = os.path.join(self.temp_dir, 'prebuilts')
+        result = gen_reference_docs.generate_docs(package_dir=package_dir,
+                                                  out_dir=out_dir,
+                                                  dart_prebuilt_dir=prebuilts)
+        self.assertEqual(result, 0)
+        mock_run.assert_has_calls([
+            mock.call.run(
+                [os.path.join(prebuilts, 'pub'), 'get'],
+                cwd=package_dir,
+                capture_output=True,
+                universal_newlines=True),
+            mock.call.run(
+                [
+                    os.path.join(prebuilts, 'dartdoc'),
+                    '--auto-include-dependencies',
+                    '--exclude-packages',
+                    'Dart,logging',
+                    '--output',
+                    out_dir,
+                    '--format',
+                    'md'
+                ],
+                cwd=package_dir,
+                capture_output=True,
+                universal_newlines=True),
+            ])
+
+
+if __name__ == '__main__':
+    unittest.main()
