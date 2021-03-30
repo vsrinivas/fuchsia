c196b8cc436d49205fb4d07b15bb5c0c0816f157
Charles Whitten <cwhitten@google.com>
2021-03-30 02:15:07 +0000

    [memfs][cts] Migrate memfsoverflow & memfsdetachlinkedfilesystem tests to cts
    go/memfs-to-cts
    Migrates remaining memfs tests (memfsoverflow & memfsdetachlinkedfilesystem) from //zircon/system/utest to //sdk/cts/tests.
    
    Tested:
    fx set core.x64 --with //sdk/cts/tests/pkg/memfs:tests
    fx test //sdk/cts/tests/pkg/memfs
    
    fx set bringup.x64 --with-base //bundles/bringup:tests
    fx build
    fx qemu -k -c zircon.autorun.boot=/boot/test/memfs
    
    Bug: 66792
    
    Change-Id: I558f5d67eb877e345cdeea7b041857dd38a49dc6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/505548
    Commit-Queue: Charles Whitten <cwhitten@google.com>
    Reviewed-by: Jeremy Manson <jeremymanson@google.com>
    Reviewed-by: John Shamoon <johnshamoon@google.com>

--- sdk/cts/tests/pkg/memfs/memfs_tests.cc
+++ sdk/cts/tests/pkg/memfs/memfs_tests.cc
@@ -10,8 +10,10 @@
 #include <lib/fdio/fd.h>
 #include <lib/fdio/fdio.h>
 #include <lib/memfs/memfs.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
+#include <fbl/unique_fd.h>
 #include <zxtest/zxtest.h>
 
 namespace {
@@ -223,6 +225,72 @@ TEST(MemfsTests, TestMemfsCloseDuringAccess) {
     // only close the client side of the connection.
     ASSERT_EQ(closedir(d), 0);
   }
+}
+
+TEST(MemfsTests, TestMemfsOverflow) {
+  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
+  ASSERT_EQ(loop.StartThread(), ZX_OK);
+
+  // Create a memfs filesystem, acquire a file descriptor
+  memfs_filesystem_t* vfs;
+  zx_handle_t root;
+  ASSERT_EQ(memfs_create_filesystem(loop.dispatcher(), &vfs, &root), ZX_OK);
+  int root_fd;
+  ASSERT_EQ(fdio_fd_create(root, &root_fd), ZX_OK);
+
+  // Access files within the filesystem.
+  DIR* d = fdopendir(root_fd);
+  ASSERT_NOT_NULL(d);
+
+  // Issue writes to the file in an order that previously would have triggered
+  // an overflow in the memfs write path.
+  //
+  // Values provided mimic the bug reported by syzkaller (fxbug.dev/33581).
+  uint8_t buf[4096];
+  memset(buf, 'a', sizeof(buf));
+  fbl::unique_fd fd(openat(dirfd(d), "file", O_CREAT | O_RDWR));
+  ASSERT_TRUE(fd);
+  ASSERT_EQ(pwrite(fd.get(), buf, 199, 0), 199);
+  ASSERT_EQ(pwrite(fd.get(), buf, 226, 0xfffffffffffff801), -1);
+  ASSERT_EQ(errno, EINVAL);
+
+  ASSERT_EQ(closedir(d), 0);
+  sync_completion_t unmounted;
+  memfs_free_filesystem(vfs, &unmounted);
+  ASSERT_EQ(sync_completion_wait(&unmounted, zx::duration::infinite().get()), ZX_OK);
+}
+
+TEST(MemfsTests, TestMemfsDetachLinkedFilesystem) {
+  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
+  ASSERT_EQ(loop.StartThread(), ZX_OK);
+
+  // Create a memfs filesystem, acquire a file descriptor
+  memfs_filesystem_t* vfs;
+  zx_handle_t root;
+  ASSERT_EQ(memfs_create_filesystem(loop.dispatcher(), &vfs, &root), ZX_OK);
+  int root_fd;
+  ASSERT_EQ(fdio_fd_create(root, &root_fd), ZX_OK);
+
+  // Access files within the filesystem.
+  DIR* d = fdopendir(root_fd);
+  ASSERT_NOT_NULL(d);
+
+  // Leave a regular file.
+  fbl::unique_fd fd(openat(dirfd(d), "file", O_CREAT | O_RDWR));
+  ASSERT_TRUE(fd);
+
+  // Leave an empty subdirectory.
+  ASSERT_EQ(0, mkdirat(dirfd(d), "empty-subdirectory", 0));
+
+  // Leave a subdirectory with children.
+  ASSERT_EQ(0, mkdirat(dirfd(d), "subdirectory", 0));
+  ASSERT_EQ(0, mkdirat(dirfd(d), "subdirectory/child", 0));
+
+  ASSERT_EQ(closedir(d), 0);
+
+  sync_completion_t unmounted;
+  memfs_free_filesystem(vfs, &unmounted);
+  ASSERT_EQ(sync_completion_wait(&unmounted, zx::duration::infinite().get()), ZX_OK);
 }
 
 }  // namespace
