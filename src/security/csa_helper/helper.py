b6729422490a64e179fcf03a600b0bc3a02dd2d3
Daniel Hwang <arkay@google.com>
2021-03-31 04:31:05 +0000

    [security] Add helper script for determing static analyzer changed files
    Add a script that takes a list of TUs as generated by `fx gn gen
    --export-compile-commands=default <OUT_DIR>`, runs `fx ninja build`
    against the files, and outputs an `analyze-build` compatible file of the
    TU targets that changed.
    
    Change-Id: If74b7c9b3f4ccf59f15bf3d7a3969de682b78a62
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/505900
    Reviewed-by: Yu Shan <shanyu@google.com>
    Commit-Queue: Daniel Hwang <arkay@google.com>

---
+++ src/security/csa_helper/helper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Provides some basic helper functions for printing."""
+
+
+def yellow(message):
+  return '\033[1;33;40m%s\033[0m'%(message)
+
+
+def white(message):
+  return '\033[37;1;40m%s\033[0m'%(message)
+
+
+def red(message):
+  return '\033[1;31;40m%s\033[0m'%(message)
---
+++ src/security/csa_helper/main_test.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Unit test for main.py.
+
+To manually run this test:
+  python3 main_test.py
+"""
+import json
+import os
+import subprocess
+import tempfile
+import time
+import unittest
+import unittest.mock as mock
+
+import main
+
+_TARGET_1 = 'foo.cc.o'
+_TARGET_2 = 'baz/bar.cc.o'
+_TARGET_3 = 'biz/fuzz.cc.o'
+_TARGETS = [_TARGET_1, _TARGET_2, _TARGET_3]
+
+
+def setup_compdb_and_fs(directory, targets, changed):
+  compdb = []
+  changed_comp_db = []
+  # Setup targets
+  for target in targets:
+    if not target.endswith('.o'):
+      raise Exception('Expected all targets to end with \'.o\', '
+                      'but received %s.'%(target))
+
+    # Add the target to the compdb
+    target_def = {
+        'file': target[:-2],
+        'directory': directory,
+        'command': 'clang++ -o %s'%(target)
+    }
+    compdb.append(target_def)
+
+    # If it is expected to change, add the target to the changed compdb
+    if target in changed:
+      changed_comp_db.append(target_def)
+
+    # Make the placeholder file to set the last modified time before fx ninja
+    target_path = os.path.join(directory, target)
+    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+    with open(target_path, 'w') as f:
+      f.write('test placeholder')
+
+  return (compdb, changed_comp_db)
+
+
+class RunCsaHelperTest(unittest.TestCase):
+
+  def helper(self, temp_dir, targets, changed):
+    out_dir = os.path.join(temp_dir, 'foo/bar')
+
+    # Set up the output directory and compdbs
+    compdb, expected_changed_compdb = \
+      setup_compdb_and_fs(out_dir, targets, changed)
+
+    # Write the compdb
+    input_file = os.path.join(temp_dir, 'compile_commands.json')
+    with open(input_file, 'w') as in_file:
+      json.dump(compdb, in_file)
+
+    # Mock out the xargs fx ninja call
+    fake_ninja = FakeNinja(out_dir, changed)
+    with mock.patch('subprocess.run') as mock_run:
+      mock_run.side_effect = fake_ninja.run
+
+      output_file = os.path.join(temp_dir, 'modified_compile_commands.json')
+      args = ['--input', input_file, '--output', output_file]
+
+      # Ensure the script succeeded
+      self.assertEqual(0, main.main(args))
+
+      # Check that the output compdb contains the files that were modified
+      with open(output_file, 'r') as out_file:
+        self.assertEqual(
+            json.dumps(expected_changed_compdb, indent=2), out_file.read())
+
+  def test_no_changed_files(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      self.helper(temp_dir, _TARGETS, [])
+
+  def test_some_changed_files(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      self.helper(temp_dir, _TARGETS, [_TARGET_1, _TARGET_3])
+
+  def test_all_changed_files(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      self.helper(temp_dir, _TARGETS, [_TARGET_1, _TARGET_2, _TARGET_3])
+
+
+class FakeNinja(object):
+
+  def __init__(self, directory, files_to_update):
+    self.directory = directory
+    self.files_to_update = files_to_update
+    return
+
+  def run(self, *argv, **kwargs):
+    del argv
+    del kwargs
+
+    # Sleep for a second so that the modified time check changes
+    time.sleep(1)
+
+    for file_to_update in self.files_to_update:
+      target_path = os.path.join(self.directory, file_to_update)
+      if not os.path.exists(target_path):
+        raise Exception('Expected path %s to already exist.'%(target_path))
+      with open(target_path, 'w') as f:
+        f.write('updated')
+
+    return subprocess.CompletedProcess(args=[], returncode=0, stdout=b'')
+
+
+if __name__ == '__main__':
+  unittest.main()
