d0095cff5bf6bc5a59fbe3c051b4e10ada7e598a
Dustin Green <dustingreen@google.com>
2021-03-31 02:29:02 +0000

    [codec] Compare output to SW decoder.
    Do pixel-by-pixel comparison with output from SW
    decode when we detect a bad output frame hash.
    
    On sherlock we still get incorrect decode output
    data sometimes at 285.7 MHz.
    
    A local repro of the bad output data on sherlock
    can be seen by increasing kTryCount to > 5000 or
    so.
    
    The flakes seen when the clock is set to 800 MHz
    on sherlock appear to be the same incorrect data
    (same set of bad hash values), just at a higher
    repro rate than when running at 285.7 MHz on
    sherlock.
    
    I've seen zero repros so far on sherlock running
    at 24 MHz.
    
    BUG: fxbug.dev/67247
    Change-Id: I75430316e6516846e58c2ea77dc9cda58013a9a6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/509040
    Reviewed-by: Austin Foxley <afoxley@google.com>
    Commit-Queue: Dustin Green <dustingreen@google.com>

--- src/media/codec/examples/use_media_decoder/test/use_h264_multi_decoder_concurrent_stream_switching_test.cc
+++ src/media/codec/examples/use_media_decoder/test/use_h264_multi_decoder_concurrent_stream_switching_test.cc
@@ -40,7 +40,7 @@ int main(int argc, char* argv[]) {
   // TODO(fxbug.dev/13483): The retries should not be necessary here.  These are presently needed to
   // de-flake due to a decode correctness bug that results in a few slightly incorrect pixels
   // sometimes.
-  constexpr uint32_t kMaxRetryCount = 100;
+  constexpr uint32_t kMaxRetryCount = 2;
   for (uint32_t try_ordinal = 0; try_ordinal < kMaxRetryCount; ++try_ordinal) {
     const UseVideoDecoderTestParams test_params{
         .keep_stream_modulo = 4,
