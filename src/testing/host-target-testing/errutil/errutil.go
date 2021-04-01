734905af8055f9c5818987deb1329c9f7a5e57fd
John Wittrock <wittrock@google.com>
2021-04-01 17:14:02 +0000

    [system-tests] print backtraces on device initialization errs
    Bug: 73229
    Change-Id: I04b0cb64be795eb13042a41af2bd9f531bc099a5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/509978
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: John Wittrock <wittrock@google.com>

--- src/testing/host-target-testing/errutil/errutil.go
+++ src/testing/host-target-testing/errutil/errutil.go
@@ -156,5 +156,6 @@ func run(ctx context.Context, serial net.Conn, cmd string) error {
 
 func shouldPrintThreads(err error) bool {
 	return errors.Is(err, context.DeadlineExceeded) ||
-		strings.Contains(err.Error(), "use of closed network connection")
+		strings.Contains(err.Error(), "use of closed network connection") ||
+		strings.Contains(err.Error(), "device failed to initialize")
 }
