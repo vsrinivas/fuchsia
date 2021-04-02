8cfe845da941d5c9a0698f5176b8b0bb27a62130
Aidan Wolter <awolter@google.com>
2021-04-02 18:47:42 +0000

    [assembly] Add integration test for vbmeta library
    This integration test generates a vbmeta file using avbtool.py and
    compares it to the output of the vbmeta library, ensuring exact
    byte-for-byte accuracy.
    
    Bug: 72017
    Change-Id: I059c4aa2e0f45cab965156abde94410409b8912d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/507140
    Commit-Queue: Aidan Wolter <awolter@google.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Kevin Wells <kevinwells@google.com>

---
+++ src/lib/assembly/vbmeta/tests/integration_test.rs
@@ -0,0 +1,28 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use vbmeta::Key;
+use vbmeta::VBMeta;
+use vbmeta::{HashDescriptor, Salt};
+
+use std::convert::TryFrom;
+
+const PEM: &str = include_str!(env!("AVB_KEY"));
+const METADATA: &[u8] = include_bytes!(env!("AVB_METADATA"));
+const SALT: &str = env!("SALT");
+const IMAGE: &[u8] = include_bytes!(env!("IMAGE"));
+const EXPECTED_VBMETA: &[u8] = include_bytes!(env!("EXPECTED_VBMETA"));
+
+#[test]
+fn avbtool_comparison() {
+    let key = Key::try_new(PEM, METADATA).unwrap();
+
+    let salt_bytes: &[u8] = &hex::decode(SALT).unwrap();
+    let salt = Salt::try_from(salt_bytes).unwrap();
+    let descriptor = HashDescriptor::new("zircon", IMAGE, salt);
+    let descriptors = vec![descriptor];
+
+    let vbmeta = VBMeta::try_new(descriptors, key).unwrap();
+    assert_eq!(vbmeta.bytes, EXPECTED_VBMETA);
+}
