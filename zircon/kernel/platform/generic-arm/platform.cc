b4e53f95bb0559fb244ddd9559a2b4384e38b1a7
Marco Vanotti <mvanotti@google.com>
2021-04-02 23:06:48 +0000

    [kernel][asan][arm64][mexec] Mark platform_mexec as NO_ASAN
    This CL removes ASAN instrumentation in the platform_mexec function as
    it acceses memory outside of the kernel address space.
    
    Change-Id: I7fe5871ea067daacc9014ef1b14f040e610d1e94
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/504520
    Commit-Queue: Marco Vanotti <mvanotti@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>

--- zircon/kernel/platform/generic-arm/platform.cc
+++ zircon/kernel/platform/generic-arm/platform.cc
@@ -12,6 +12,7 @@
 #include <lib/cmdline.h>
 #include <lib/console.h>
 #include <lib/debuglog.h>
+#include <lib/instrumentation/asan.h>
 #include <lib/memory_limit.h>
 #include <lib/system-topology.h>
 #include <mexec.h>
@@ -696,8 +697,11 @@ void platform_mexec_prep(uintptr_t new_bootimage_addr, size_t new_bootimage_len)
   DEBUG_ASSERT(mp_get_online_mask() == cpu_num_to_mask(BOOT_CPU_ID));
 }
 
-void platform_mexec(mexec_asm_func mexec_assembly, memmov_ops_t* ops, uintptr_t new_bootimage_addr,
-                    size_t new_bootimage_len, uintptr_t entry64_addr) {
+// This function requires NO_ASAN because it accesses ops, which is memory
+// that lives outside of the kernel address space (comes from IdAllocator).
+NO_ASAN void platform_mexec(mexec_asm_func mexec_assembly, memmov_ops_t* ops,
+                            uintptr_t new_bootimage_addr, size_t new_bootimage_len,
+                            uintptr_t entry64_addr) {
   DEBUG_ASSERT(arch_ints_disabled());
   DEBUG_ASSERT(mp_get_online_mask() == cpu_num_to_mask(BOOT_CPU_ID));
 
