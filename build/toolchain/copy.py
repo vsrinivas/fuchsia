8ac8c21f4741b21d191b9603529b16928bc6ad4b
Jay Zhuang <jayzhuang@google.com>
2021-04-06 18:10:01 +0000

    [build] Disable directory copy in default_tools
    Bug: 73250
    Change-Id: I9fe6ed32af12a0be900269855e241ae31c62ca81
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/511298
    Reviewed-by: Shai Barack <shayba@google.com>
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>

--- build/toolchain/copy.py
+++ build/toolchain/copy.py
@@ -2,8 +2,7 @@
 # Copyright 2020 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-"""Emulation of rm -rf out && cp -af in out. This is necessary on Mac in order
+"""Emulation of `rm -f out && cp -af` in out. This is necessary on Mac in order
 to preserve nanoseconds of mtime. See https://fxbug.dev/56376#c5."""
 
 import os
@@ -11,14 +10,18 @@ import shutil
 import sys
 
 
-# This implementation is mostly from:
-# https://source.chromium.org/chromium/chromium/src/+/master:build/toolchain/win/tool_wrapper.py
 def main():
     if len(sys.argv) != 3:
-        print('usage: recursive_copy.py source dest', file=sys.stderr)
+        print('usage: copy.py source dest', file=sys.stderr)
         return 1
     source = sys.argv[1]
     dest = sys.argv[2]
+
+    if os.path.isdir(source):
+        print(
+            f'{source} is a directory, tool "copy" does not support directory copies'
+        )
+        return 1
 
     if os.path.exists(dest):
         if os.path.isdir(dest):
@@ -38,10 +41,7 @@ def main():
                 os.chmod(dest, stat.S_IWRITE)
             os.unlink(dest)
 
-    if os.path.isdir(source):
-        shutil.copytree(source, dest)
-    else:
-        shutil.copy2(source, dest)
+    shutil.copy2(source, dest)
 
 
 if __name__ == '__main__':
