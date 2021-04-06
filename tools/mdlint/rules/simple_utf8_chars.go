9857c6d3408a8c7fc753840f7f95e78c5ebcd639
Pascal Perez <pascallouis@google.com>
2021-04-06 11:33:41 +0000

    [mdlint] Rule to prevent fancy utf8 chars
    We often see open quotes, close quotes, single quotes, or ellipsis when
    their ASCII counterpart are much better since they can be properly
    rendered later.
    
    Bug: 62964
    Change-Id: Ie21a15c1a18e41bdb2dfa97ea6780f893e5e37ac
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/511541
    Reviewed-by: Adam Barth <abarth@google.com>
    Fuchsia-Auto-Submit: Pascal Perez <pascallouis@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ tools/mdlint/rules/simple_utf8_chars.go
@@ -0,0 +1,42 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package rules
+
+import (
+	"go.fuchsia.dev/fuchsia/tools/mdlint/core"
+)
+
+func init() {
+	core.RegisterLintRuleOverTokens(simpleUtf8CharsName, newSimpleUtf8Chars)
+}
+
+const simpleUtf8CharsName = "simple-utf8-chars"
+
+type simpleUtf8Chars struct {
+	core.DefaultLintRuleOverTokens
+	core.Reporter
+}
+
+var _ core.LintRuleOverTokens = (*simpleUtf8Chars)(nil)
+
+func newSimpleUtf8Chars(reporter core.Reporter) core.LintRuleOverTokens {
+	return &simpleUtf8Chars{Reporter: reporter}
+}
+
+var dontButPrefer = map[rune]string{
+	'“': `"`,
+	'”': `"`,
+	'’': `'`,
+	'…': `...`,
+}
+
+func (rule *simpleUtf8Chars) OnNext(tok core.Token) {
+	for _, r := range tok.Content {
+		if sub, ok := dontButPrefer[r]; ok {
+			rule.Warnf(tok, "avoid %c, prefer %s", r, sub)
+			return // avoid double reporting on same token
+		}
+	}
+}
---
+++ tools/mdlint/rules/simple_utf8_chars_test.go
@@ -0,0 +1,25 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package rules
+
+import (
+	"testing"
+)
+
+func TestSimpleUtf8Chars(t *testing.T) {
+	ruleTestCase{
+		files: map[string]string{
+			"example.md": `
+Copy-pasting nicely formatted documents can lead to «issues…»
+
+For instance,
+
+- fancy open quotes like «“so»
+- fancy close quotes like «so”»
+- or fancy single quotes like «d’Auzac» de Lamartinie
+- or all together «“d’Et-bugge”»`,
+		},
+	}.runOverTokens(t, newSimpleUtf8Chars)
+}
