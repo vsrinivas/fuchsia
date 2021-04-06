d35e8c10bc2c7b85fb40d19a2dcebb25aaf6c883
Andres Oportus <andresoportus@google.com>
2021-04-06 17:51:24 +0000

    [audio][nelson] Switch audio streaming interface to mono
    Test: audio-driver-ctl -c 1 on Nelson
    Test: Nelson E2E audio input and output check
    Change-Id: Ic4e0da1af1f4b319b5726a017540c16811ddb463
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/511696
    Fuchsia-Auto-Submit: Andres Oportus <andresoportus@google.com>
    Reviewed-by: Eric Holland <hollande@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/board/drivers/nelson/nelson-audio.cc
+++ src/devices/board/drivers/nelson/nelson-audio.cc
@@ -199,11 +199,14 @@ zx_status_t Nelson::AudioInit() {
   metadata.bus = metadata::AmlBus::TDM_B;
   metadata.version = metadata::AmlVersion::kS905D3G;
   metadata.dai.type = metadata::DaiType::I2s;
+  metadata.ring_buffer.number_of_channels = 1;
+
+  // Get the one ring buffer channel to output in the *left* I2S slot.
+  metadata.swaps = 0x10;
+  metadata.lanes_enable_mask[0] = 2;
+
   metadata.codecs.number_of_codecs = 1;
   metadata.codecs.types[0] = metadata::CodecType::Tas58xx;
-  metadata.ring_buffer.number_of_channels = 2;
-  metadata.swaps = 0x10;
-  metadata.lanes_enable_mask[0] = 3;
   metadata.codecs.channels_to_use_bitmask[0] = 3;
   pbus_metadata_t tdm_metadata[] = {
       {
