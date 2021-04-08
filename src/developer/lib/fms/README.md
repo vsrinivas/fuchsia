535f00fb73645bedaa45121ed39c67dec3468587
Dave Schuyler <dschuyler@chromium.org>
2021-04-08 19:18:51 +0000

    [fms] add ensure api
    WIP. Adds a Modules object with an ensure API.
    
    Change-Id: I2c6caad4a552979b1b7f4ad14f59c2409f953baa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/512773
    Commit-Queue: Dave Schuyler <dschuyler@google.com>
    Reviewed-by: Matthew Boetger <boetger@google.com>

--- src/developer/lib/fms/README.md
+++ src/developer/lib/fms/README.md
@@ -4,3 +4,19 @@ The FMS library (will) support the lookup of a manifest entry based on an entry
 name and downloading the artifact(s) referenced in the manifest entry.
 
 [This lib will be a work in progress for Q2, 2021.]
+
+## Development of FMS Lib
+
+When working on FMS lib, consider using:
+
+```
+$ fx set [...] --with //src/developer/lib/fms:tests
+```
+
+### Unit Tests
+
+Unit tests can be run with:
+
+```
+$ fx test fms_lib_test
+```
--- src/developer/lib/fms/src/lib.rs
+++ src/developer/lib/fms/src/lib.rs
@@ -2,4 +2,72 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Nothing to see here yet.
+//! Encapsulates the software distribution to host systems.
+//!
+//! Uses a unique identifier (FMS Name) to refer to SDK Modules which allows
+//! users of FMS lib to not be concerned about where modules are being
+//! downloaded from (be it CIPD, MOS-TUF, GCS, or something else).
+
+use {
+    anyhow::{bail, Result},
+    std::path::{Path, PathBuf},
+};
+
+/// Manager for finding available SDK Modules for download or ensuring that an
+/// SDK Module (artifact) is downloaded.
+struct Modules {
+    sdk_path: PathBuf,
+    // Contents TBD.
+}
+
+impl Modules {
+    pub fn new<P: AsRef<Path>>(sdk_path: P) -> Self {
+        Self { sdk_path: sdk_path.as_ref().to_path_buf() }
+    }
+
+    /// Make a module available for use.
+    ///
+    /// If the module identified by 'module_name' is already downloaded and up
+    /// to date the path to the module will be returned immediately.
+    /// If module is missing, it will be downloaded made ready before returning
+    /// the path.
+    pub fn ensure(&self, module_name: &str) -> Result<PathBuf> {
+        // Temporary while API is fleshed out.
+        if module_name == "not-an-sdk-module" {
+            bail!("Error TBD");
+        }
+        Ok(self.sdk_path.join("fake").join("path"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lib() {
+        let sdk_dir = Path::new("~/fuchsia-sdk");
+        let modules = Modules::new(&sdk_dir);
+        assert_eq!(modules.sdk_path, sdk_dir);
+        let modules = Modules::new(sdk_dir.to_path_buf());
+        assert_eq!(modules.sdk_path, sdk_dir);
+    }
+
+    #[test]
+    fn test_ensure() {
+        let sdk_dir = Path::new("~/fuchsia-sdk");
+        let modules = Modules::new(&sdk_dir);
+        let path = modules.ensure("fake-sdk-module").unwrap();
+        assert_eq!(path, sdk_dir.join("fake").join("path"));
+    }
+
+    #[test]
+    fn test_ensure_not_found() {
+        let sdk_dir = Path::new("~/fuchsia-sdk");
+        let modules = Modules::new(&sdk_dir);
+        match modules.ensure("not-an-sdk-module") {
+            Err(e) => assert_eq!(e.to_string(), "Error TBD"),
+            Ok(_) => assert!(false),
+        }
+    }
+}
