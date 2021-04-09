b0b8998531e2831c017c2d677abde283f03c855b
Adam Barth <abarth@google.com>
2021-04-09 16:46:23 +0000

    [ffx][starnix] Implement client for ffx starnix shell
    This CL implements the client side of the ffx starnix shell command. The
    server side will follow in a later CL.
    
    Bug: 74060
    Change-Id: Id9cf8ca172dfe3bd385071d6bca140bf7c657b65
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/511627
    Reviewed-by: Matthew Boetger <boetger@google.com>
    Reviewed-by: James Tucker <raggi@google.com>
    API-Review: Adam Barth <abarth@google.com>
    Commit-Queue: Adam Barth <abarth@google.com>

--- src/developer/ffx/plugins/starnix/shell/src/lib.rs
+++ src/developer/ffx/plugins/starnix/shell/src/lib.rs
@@ -1,10 +1,79 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use {anyhow::Result, ffx_core::ffx_plugin, ffx_starnix_shell_args::ShellStarnixCommand};
+use {
+    anyhow::{anyhow, Context, Result},
+    blocking::Unblock,
+    ffx_core::{ffx_error, ffx_plugin},
+    ffx_starnix_shell_args::ShellStarnixCommand,
+    fidl::endpoints::create_proxy,
+    fidl_fuchsia_starnix_developer::{
+        ManagerProxy, ShellControllerEvent, ShellControllerMarker, ShellParams,
+    },
+    futures::StreamExt,
+};
 
-#[ffx_plugin("starnix_enabled")]
-pub async fn shell_starnix(_shell: ShellStarnixCommand) -> Result<()> {
+#[ffx_plugin(
+    "starnix_enabled",
+    ManagerProxy = "core/starnix:expose:fuchsia.starnix.developer.Manager"
+)]
+pub async fn shell_starnix(manager_proxy: ManagerProxy, _shell: ShellStarnixCommand) -> Result<()> {
+    let (controller_proxy, controller_server_end) = create_proxy::<ShellControllerMarker>()?;
+    let (sin, cin) =
+        fidl::Socket::create(fidl::SocketOpts::STREAM).context("failed to create stdin socket")?;
+    let (sout, cout) =
+        fidl::Socket::create(fidl::SocketOpts::STREAM).context("failed to create stdout socket")?;
+    let (serr, cerr) =
+        fidl::Socket::create(fidl::SocketOpts::STREAM).context("failed to create stderr socket")?;
+
+    let mut stdin = fidl::AsyncSocket::from_socket(cin)?;
+    let mut stdout = Unblock::new(std::io::stdout());
+    let mut stderr = Unblock::new(std::io::stderr());
+    let copy_futures = futures::future::try_join3(
+        // We may need to swap out for a latency sensitive copy that calls "flush" regularly.
+        // If you see what feels like stalling behavior at odd buffer biases, it may down to this
+        // copy not pumping sufficient to flush.
+        futures::io::copy(Unblock::new(std::io::stdin()), &mut stdin),
+        // This approach does not support "ffx starnix shell cat largefile | head -n 1" because
+        // closing stdout is not propagated back to cout.
+        futures::io::copy(fidl::AsyncSocket::from_socket(cout)?, &mut stdout),
+        futures::io::copy(fidl::AsyncSocket::from_socket(cerr)?, &mut stderr),
+    );
+
+    let mut event_stream = controller_proxy.take_event_stream();
+    let term_event_future = async move {
+        while let Some(result) = event_stream.next().await {
+            match result? {
+                ShellControllerEvent::OnTerminated { return_code } => {
+                    return Ok(return_code);
+                }
+            }
+        }
+        Err(anyhow!(ffx_error!("Shell terminated abnormally")))
+    };
+
+    unsafe {
+        signal_hook::register(signal_hook::SIGINT, move || {
+            eprintln!("Caught interrupt. Forcing exit...");
+            std::process::exit(0);
+        })?;
+    }
+
+    let params = ShellParams {
+        stdin: Some(sin.into()),
+        stdout: Some(sout.into()),
+        stderr: Some(serr.into()),
+        ..ShellParams::EMPTY
+    };
+
+    manager_proxy
+        .start_shell(params, controller_server_end)
+        .map_err(|_| anyhow!("Error starting shell: {:?}"))?;
+
+    let (copy_result, return_code) = futures::join!(copy_futures, term_event_future);
+    copy_result?;
+
+    println!("(Shell exited with code: {})", return_code?);
     Ok(())
 }
