1793be78a3e047bb806eec4568b7f39738a9a946
Yeonhee Lee <yhlee@google.com>
2021-04-09 14:19:30 +0000

    [e2e][simple_browser] Deflakes simple browser e2e tests
    - Confirmed on Femu that it deflaked the flakiness at clicking web links.
    
    Change-Id: I65495bd7613140f8b31dd33c818a391bd0600dec
    Reviewed-on: https://fuchsia-review.googlesource.com/c/experiences/+/513181
    Commit-Queue: Yeonhee Lee <yhlee@google.com>
    Reviewed-by: Sanjay Chouksey <sanjayc@google.com>
    Reviewed-by: Charles Whitten <cwhitten@google.com>

--- src/experiences/tests/e2e/test/ermine_session_shell_simple_browser_test.dart
+++ src/experiences/tests/e2e/test/ermine_session_shell_simple_browser_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:math';
 
 import 'package:ermine_driver/ermine_driver.dart';
@@ -10,7 +11,8 @@ import 'package:sl4f/sl4f.dart';
 import 'package:test/test.dart';
 import 'package:webdriver/sync_io.dart';
 
-const _timeout = Duration(seconds: 2);
+const _timeoutOneSec = Duration(seconds: 1);
+const _timeoutTenSec = Duration(seconds: 10);
 const _sampleViewRect = Rectangle(100, 200, 100, 100);
 const testserverUrl =
     'fuchsia-pkg://fuchsia.com/ermine_testserver#meta/ermine_testserver.cmx';
@@ -60,14 +62,14 @@ void main() {
     FlutterDriver browser = await ermine.launchAndWaitForSimpleBrowser();
     const stopUrl = 'http://127.0.0.1:8080/stop';
     await browser.requestData(stopUrl);
-    await browser.waitUntilNoTransientCallbacks(timeout: _timeout);
-    await browser.waitFor(find.text(stopUrl), timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitFor(find.text(stopUrl), timeout: _timeoutTenSec);
     expect(await ermine.isStopped(testserverUrl), isTrue);
     print('Stopped the test server');
     await browser.close();
 
     await ermine.driver.requestData('close');
-    await ermine.driver.waitUntilNoTransientCallbacks(timeout: _timeout);
+    await ermine.driver.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
     await ermine.driver.waitForAbsent(find.text('simple-browser.cmx'));
     expect(await ermine.isStopped(simpleBrowserUrl), isTrue);
     print('Closed the browser');
@@ -88,6 +90,56 @@ void main() {
     }, timeout: timeout);
   }
 
+  /// Keeps finding a web element that satisfies the given [by] condition until
+  /// the timeout expires. [NoSuchElementException] thrown by [findElement]
+  /// in the meantime is ignored.
+  /// Returns the [WebElement] if it finds one. Otherwise, returns null.
+  Future<WebElement> _waitForWebElement(WebDriver web, By by) async {
+    return await ermine.waitFor<WebElement>(() async {
+      try {
+        final element = web.findElement(by);
+        return element;
+      } on NoSuchElementException {
+        return null;
+      }
+    }, timeout: _timeoutTenSec);
+  }
+
+  /// Keeps calling the given action until it gets the expected result within
+  /// a fixed time. Errors thrown by the action in the meantime is ignored.
+  /// e.g. [DriverError], thrown in case the driver fails to locate a [Finder].
+  /// Returns true if it gets the expected result. Otherwise, returns false.
+  Future<bool> _repeatActionUntilGetResult(
+      void action(), Future<void> result()) async {
+    return await ermine.waitFor(() async {
+      action.call();
+      try {
+        await result.call();
+        return true;
+        // ignore: avoid_catches_without_on_clauses
+      } catch (e) {
+        print('$e. Keep repeating the action until timeout expires...');
+        return false;
+      }
+    }, timeout: _timeoutTenSec);
+  }
+
+  /// Keeps calling `waitFor(finder)` until the timeout expires. Returns true
+  /// if it locates one. Otherwise, returns false.
+  Future<bool> _repeatActionWaitingFor(
+      FlutterDriver browser, void action(), SerializableFinder finder) async {
+    return await _repeatActionUntilGetResult(
+        action, () => browser.waitFor(finder, timeout: _timeoutOneSec));
+  }
+
+  /// Keeps calling `waitForAbsent(finder)` until the timeout expires. Returns
+  /// true if it locates one. Otherwise, returns false.
+  Future<bool> _repeatActionWaitingForAbsent(
+      FlutterDriver browser, void action(), SerializableFinder finder) async {
+    return await _repeatActionUntilGetResult(
+        action, () => browser.waitForAbsent(finder, timeout: _timeoutOneSec));
+  }
+
   // TODO(fxb/68689): Transition pointer interactions to Sl4f.Input once it is
   // ready.
   test('Should be able to do page and history navigation.', () async {
@@ -97,7 +149,8 @@ void main() {
     // Access to the website.
     await input.text('http://127.0.0.1:8080/index.html');
     await input.keyPress(kEnterKey);
-    await browser.waitFor(indexTabFinder, timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitFor(indexTabFinder, timeout: _timeoutTenSec);
 
     final webdriver =
         (await webDriverConnector.webDriversForHost('127.0.0.1')).single;
@@ -106,22 +159,29 @@ void main() {
     expect(await browser.getText(indexTabFinder), isNotNull);
     print('Opened http://127.0.0.1:8080/index.html');
 
-    final nextLink = webdriver.findElement(By.linkText('Next'));
+    final nextLink = await _waitForWebElement(webdriver, By.linkText('Next'));
     expect(nextLink, isNotNull);
 
     // Clicks the text link that opens next.html (page navigation)
-    nextLink.click();
-    await browser.waitForAbsent(indexTabFinder, timeout: _timeout);
+    expect(
+        await _repeatActionWaitingForAbsent(
+            browser, nextLink.click, indexTabFinder),
+        isTrue,
+        reason: 'Failed to click the Next link.');
     expect(await browser.getText(newTabFinder), isNotNull);
     expect(await browser.getText(nextTabFinder), isNotNull);
     print('Clicked the next.html link');
 
-    final prevLink = webdriver.findElement(By.linkText('Prev'));
+    final prevLink = await _waitForWebElement(webdriver, By.linkText('Prev'));
     expect(prevLink, isNotNull);
 
     // Clicks the text link that opens index.html (page navigation)
-    prevLink.click();
-    await browser.waitForAbsent(nextTabFinder, timeout: _timeout);
+    expect(
+        await _repeatActionWaitingForAbsent(
+            browser, prevLink.click, nextTabFinder),
+        isTrue,
+        reason: 'Failed to click the Prev link.');
+
     expect(await browser.getText(newTabFinder), isNotNull);
     expect(await browser.getText(indexTabFinder), isNotNull);
     print('Clicked the index.html link');
@@ -129,7 +189,8 @@ void main() {
     // Goes back to next.html by tapping the BCK button (history navigation)
     final back = find.byValueKey('back');
     await browser.tap(back);
-    await browser.waitForAbsent(indexTabFinder, timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitForAbsent(indexTabFinder, timeout: _timeoutTenSec);
     expect(await browser.getText(newTabFinder), isNotNull);
     expect(await browser.getText(nextTabFinder), isNotNull);
     print('Hit BCK');
@@ -137,14 +198,15 @@ void main() {
     // Goes forward to index.html by tapping the FWD button (history navigation)
     final forward = find.byValueKey('forward');
     await browser.tap(forward);
-    await browser.waitForAbsent(nextTabFinder, timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitForAbsent(nextTabFinder, timeout: _timeoutTenSec);
     expect(await browser.getText(newTabFinder), isNotNull);
     expect(await browser.getText(indexTabFinder), isNotNull);
     print('Hit FWD');
 
     // Clicks + button to increase the number
-    var digitLink = webdriver.findElement(By.id('target'));
-    final addButton = webdriver.findElement(By.id('increase'));
+    var digitLink = await _waitForWebElement(webdriver, By.id('target'));
+    final addButton = await _waitForWebElement(webdriver, By.id('increase'));
     expect(digitLink.text, '0');
     addButton.click();
     await ermine.waitFor(() async {
@@ -159,19 +221,21 @@ void main() {
     // Refreshes the page
     final refresh = find.byValueKey('refresh');
     await browser.tap(refresh);
-    digitLink = webdriver.findElement(By.id('target'));
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    digitLink = await _waitForWebElement(webdriver, By.id('target'));
     await ermine.waitFor(() async {
       return digitLink.text == '0';
     });
     print('Hit RFRSH');
 
-    final popupLink = webdriver.findElement(By.linkText('Popup'));
+    final popupLink = await _waitForWebElement(webdriver, By.linkText('Popup'));
     expect(popupLink, isNotNull);
 
     // Clicks the text link that opens popup.html (popup page navigation)
-    popupLink.click();
-    await browser.waitUntilNoTransientCallbacks(timeout: _timeout);
-    await browser.waitFor(popupTabFinder, timeout: _timeout);
+    expect(
+        await _repeatActionWaitingFor(browser, popupLink.click, popupTabFinder),
+        isTrue,
+        reason: 'Failed to click the Popup link.');
     expect(await browser.getText(newTabFinder), isNotNull);
     expect(await browser.getText(indexTabFinder), isNotNull);
     expect(await browser.getText(popupTabFinder), isNotNull);
@@ -179,11 +243,11 @@ void main() {
 
     await browser.close();
     await ermine.driver.requestData('close');
-    await ermine.driver.waitUntilNoTransientCallbacks(timeout: _timeout);
+    await ermine.driver.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
     await ermine.driver.waitForAbsent(find.text('simple-browser.cmx'));
     expect(await ermine.isStopped(simpleBrowserUrl), isTrue);
     print('Closed the browser');
-  }, skip: true);
+  });
 
   test('Should be able to play videos on web pages.', () async {
     FlutterDriver browser;
@@ -195,7 +259,7 @@ void main() {
     // the fuchsia-colored background for another 3 seconds.
     await input.text('http://127.0.0.1:8080/video.html');
     await input.keyPress(kEnterKey);
-    await browser.waitFor(videoTabFinder, timeout: _timeout);
+    await browser.waitFor(videoTabFinder, timeout: _timeoutTenSec);
 
     expect(await browser.getText(videoTabFinder), isNotNull);
     print('Opened http://127.0.0.1:8080/video.html');
@@ -234,28 +298,28 @@ void main() {
     // Opens red.html in the second tab leaving the first tab as an empty tab.
     await input.text(redUrl);
     await input.keyPress(kEnterKey);
-    await browser.waitUntilNoTransientCallbacks(timeout: _timeout);
-    await browser.waitFor(redTabFinder, timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitFor(redTabFinder, timeout: _timeoutTenSec);
     print('Opened red.html');
 
     // Opens green.html in the third tab.
     await browser.tap(find.byValueKey('new_tab'));
-    await browser.waitFor(find.text(newTabHintText), timeout: _timeout);
+    await browser.waitFor(find.text(newTabHintText), timeout: _timeoutTenSec);
 
     await input.text(greenUrl);
     await input.keyPress(kEnterKey);
-    await browser.waitUntilNoTransientCallbacks(timeout: _timeout);
-    await browser.waitFor(greenTabFinder, timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitFor(greenTabFinder, timeout: _timeoutTenSec);
     print('Opened green.html');
 
     // Opens blue.html in the forth tab.
     await browser.tap(find.byValueKey('new_tab'));
-    await browser.waitFor(find.text(newTabHintText), timeout: _timeout);
+    await browser.waitFor(find.text(newTabHintText), timeout: _timeoutTenSec);
 
     await input.text(blueUrl);
     await input.keyPress(kEnterKey);
-    await browser.waitUntilNoTransientCallbacks(timeout: _timeout);
-    await browser.waitFor(blueTabFinder, timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitFor(blueTabFinder, timeout: _timeoutTenSec);
     print('Opened blue.html');
 
     // Should have 4 tabs and the forth tab should be focused.
@@ -340,23 +404,14 @@ void main() {
     // Access to the website.
     await input.text(testInputPage);
     await input.keyPress(kEnterKey);
-    await browser.waitUntilNoTransientCallbacks(timeout: _timeout);
-    await browser.waitFor(textInputTabFinder, timeout: _timeout);
+    await browser.waitUntilNoTransientCallbacks(timeout: _timeoutTenSec);
+    await browser.waitFor(textInputTabFinder, timeout: _timeoutTenSec);
     print('Opened $testInputPage');
 
     final webdriver =
         (await webDriverConnector.webDriversForHost('127.0.0.1')).single;
 
-    WebElement textField;
-
-    await ermine.waitFor(() async {
-      try {
-        textField = webdriver.findElement(By.id('text-input'));
-        return true;
-      } on NoSuchElementException {
-        return false;
-      }
-    }, timeout: _timeout);
+    final textField = await _waitForWebElement(webdriver, By.id('text-input'));
     print('The textfield is found.');
 
     expect(textField, isNotNull);
@@ -364,7 +419,7 @@ void main() {
     textField.click();
     await ermine.waitFor(() async {
       return webdriver.activeElement.equals(textField);
-    }, timeout: _timeout);
+    }, timeout: _timeoutTenSec);
     print('The textfield is now focused.');
 
     // TODO(fxb/74070): Sl4f.Input currently does not work for web elements.
@@ -373,7 +428,7 @@ void main() {
     textField.sendKeys(testText);
     await ermine.waitFor(() async {
       return textField.properties['value'] == testText;
-    }, timeout: _timeout);
+    }, timeout: _timeoutTenSec);
     print('Text is entered into the textfield.');
 
     await browser.close();
