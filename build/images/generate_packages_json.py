7dd0e876b68aa0ee614450c3479d89c648c1e4f5
Jay Zhuang <jayzhuang@google.com>
2021-04-09 21:41:24 +0000

    [build] Fix hermeticity of generate_packages_json
    ... by writing depfiles including all merkle files it reads
    
    Change-Id: I069341bcc49765bc477c2cb0c7621c171f475527
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/513724
    Reviewed-by: Anirudh Mathukumilli <rudymathu@google.com>
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>

--- build/images/generate_packages_json.py
+++ build/images/generate_packages_json.py
@@ -6,23 +6,50 @@
 
 # Generates the packages.json file in the update package.
 
+import argparse
 import json
 import sys
 
 
-def main(input_path, output_path):
-    with open(input_path, 'r') as input_file:
-        pkgurls = []
-        for line in input_file:
-            package, merkle_path = line.strip().split('=', 1)
-            merkle = open(merkle_path, 'r').read().strip()
-            pkgurls += [
-                'fuchsia-pkg://fuchsia.com/{}?hash={}'.format(package, merkle)
-            ]
-        with open(output_path, 'w') as output_file:
-            packages_json = {'version': 1, 'content': pkgurls}
-            output_file.write(json.dumps(packages_json))
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generates the packages.json file in the update package',
+    )
+    parser.add_argument(
+        '--input_file',
+        type=argparse.FileType('r'),
+        required=True,
+    )
+    parser.add_argument(
+        '--output_file',
+        type=argparse.FileType('w'),
+        required=True,
+    )
+    parser.add_argument(
+        '--depfile',
+        type=argparse.FileType('w'),
+        required=True,
+    )
+    args = parser.parse_args()
+
+    pkgurls = []
+    deps = []
+    for line in args.input_file:
+        package, merkle_path = line.strip().split('=', 1)
+        deps.append(merkle_path)
+        with open(merkle_path, 'r') as merkle_file:
+            merkle = merkle_file.read().strip()
+            pkgurls.append(
+                'fuchsia-pkg://fuchsia.com/{}?hash={}'.format(package, merkle),
+            )
+
+    args.depfile.write('{}: {}\n'.format(args.output_file.name, ' '.join(deps)))
+
+    args.output_file.write(json.dumps({
+        'version': 1,
+        'content': pkgurls,
+    }))
 
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    sys.exit(main())
