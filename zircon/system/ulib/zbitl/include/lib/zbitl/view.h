088ddac3f78f0adc9b85a18c0ae9a79a41ebcccb
David Greenaway <dgreenaway@google.com>
2021-04-09 01:03:50 +0000

    [zbitl] Add payload size overflow check.
    When calculating an aligned payload size, the addition may overflow
    causing us to incorrectly believe that a container's size is large
    enough to contain a too-large payload.
    
    Perform an additional overflow check and add some tests.
    
    Bug: 72113
    Change-Id: I57c10315c3ad3c17edb6327e18beef318e296934
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/511720
    Fuchsia-Auto-Submit: David Greenaway <dgreenaway@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

--- zircon/system/ulib/zbitl/include/lib/zbitl/view.h
+++ zircon/system/ulib/zbitl/include/lib/zbitl/view.h
@@ -485,7 +485,9 @@ class View {
       const uint32_t payload_size = value_.header.length();
       const uint32_t aligned_payload_size =
           (payload_size + kPayloadSizeAlignment - 1) & -kPayloadSizeAlignment;
-      if (payload_offset > view_->limit_ || aligned_payload_size > view_->limit_ - payload_offset) {
+      if (payload_offset > view_->limit_ ||
+          aligned_payload_size < payload_size ||  // ensure aligned size didn't overflow
+          aligned_payload_size > view_->limit_ - payload_offset) {
         Fail("container too short for next item payload");
         return;
       }
--- zircon/system/ulib/zbitl/test/view-tests.cc
+++ zircon/system/ulib/zbitl/test/view-tests.cc
@@ -70,11 +70,11 @@ TEST(ZbitlViewStringTests, TooSmallForNextHeader) {
   }
 }
 
-TEST(ZbitlViewStringTests, TooSmallForNextPayload) {
-  constexpr uint32_t kItemType = ZBI_TYPE_IMAGE_ARGS;
-
-  constexpr std::string_view kExpectedError = "container too short for next item payload";
-
+// Construct and iterate over a ZBI with a payload of `actual_size`
+// which has a header size claiming `claimed_size`.
+//
+// We check that an error was correctly reported.
+void CheckInvalidPayloadSizeDetected(uint32_t claimed_size, uint32_t actual_size) {
   // Construct a ZBI of reported size 64, but whose last header reports that
   // the last item extends beyond that. Iteration should result in
   // `kExpectedError`.
@@ -86,23 +86,47 @@ TEST(ZbitlViewStringTests, TooSmallForNextPayload) {
   }
   {
     const zbi_header_t header = {
-        .type = kItemType,
-        .length = 8,
+        .type = ZBI_TYPE_IMAGE_ARGS,
+        .length = claimed_size,
         .flags = ZBI_FLAG_VERSION,
         .magic = ZBI_ITEM_MAGIC,
         .crc32 = ZBI_ITEM_NO_CRC32,
     };
     zbi.append(reinterpret_cast<const char*>(&header), sizeof(header));
+    zbi.append(actual_size, 'X');
   }
+
+  // Iterate over the ZBI, and ensure no items were found.
   zbitl::View<std::string_view> view(zbi);
+  EXPECT_TRUE(view.begin() == view.end());
 
-  for (auto [header, payload] : view) {
-    EXPECT_EQ(header->type, header->type);
-  }
-
+  // Ensure an error was produced.
   auto result = view.take_error();
   ASSERT_TRUE(result.is_error());
-  EXPECT_EQ(kExpectedError, result.error_value().zbi_error);
+  EXPECT_EQ(result.error_value().zbi_error, "container too short for next item payload");
+}
+
+TEST(ZbitlViewStringTests, TooSmallForNextPayload) {
+  // Try a variety of actual/claimed sizes.
+  struct TestCase {
+    uint32_t claimed_size;
+    uint32_t actual_size;
+  } sizes[] = {
+      {1, 0},
+      {ZBI_ALIGNMENT - 1, 0},
+      {ZBI_ALIGNMENT, 0},
+      {ZBI_ALIGNMENT, ZBI_ALIGNMENT - 1},
+      {ZBI_ALIGNMENT + 1, ZBI_ALIGNMENT},
+      {1024, 1023},
+      {1024, 1024 - ZBI_ALIGNMENT},
+      {UINT_MAX, 0},
+      {UINT_MAX, 1024},
+  };
+  for (const TestCase& size : sizes) {
+    SCOPED_TRACE("claimed_size = " + std::to_string(size.claimed_size) +
+                 ", actual_size = " + std::to_string(size.actual_size));
+    CheckInvalidPayloadSizeDetected(size.claimed_size, size.actual_size);
+  }
 }
 
 }  // namespace
