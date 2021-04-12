e2f2491fbf7573120d4f06a08392fee0ef3c3604
Jay Zhuang <jayzhuang@google.com>
2021-04-12 18:05:32 +0000

    [merge_sysmgr_config] Seperate out unittest
    ... using python_host_test, also including these tests in CQ.
    
    Test: fx test --host merge_sysmgr_config_test
    Change-Id: Idc99315ae67c4168f026b607bff57f83b5898b88
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/513730
    Reviewed-by: Kevin Wells <kevinwells@google.com>
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>

--- src/sys/pkg/scripts/merge_sysmgr_config.py
+++ src/sys/pkg/scripts/merge_sysmgr_config.py
@@ -14,7 +14,6 @@ operation is not recursive.
 import argparse
 import copy
 import json
-import unittest
 
 
 def parseargs():
@@ -100,73 +99,6 @@ class DuplicateEntryError(Exception):
     def __init__(self, item, target):
         super(DuplicateEntryError, self).__init__(
             "{item!r} already in {target!r}".format(item=item, target=target))
-
-
-class TestMerge(unittest.TestCase):
-    """$ python2.7 -m unittest merge_sysmgr_config"""
-
-    def test_merge_list(self):
-        ls = ["foo"]
-
-        merge_list(ls, ["bar"])
-        self.assertEqual(ls, ["foo", "bar"])
-
-        with self.assertRaises(DuplicateEntryError):
-            merge_list(ls, ["bar"])
-
-    def test_merge_dict(self):
-        d = {"foo": 1}
-
-        merge_dict(d, {"bar": 2})
-        self.assertEqual(d, {"foo": 1, "bar": 2})
-
-        with self.assertRaises(DuplicateEntryError):
-            merge_dict(d, {"bar": 2})
-
-    def test_nonrecursive(self):
-        d = {"outer": {"inner": "value"}}
-
-        with self.assertRaises(DuplicateEntryError):
-            merge_dict(d, d)
-
-    def test_merge_config(self):
-        a = {
-            "services": {
-                "fuchsia.foo": "foo"
-            },
-            "update_dependencies": ["fuchsia.foo"],
-            "exclusive": ["pass-through"]
-        }
-
-        b = {
-            "services": {
-                "fuchsia.bar": "bar"
-            },
-            "update_dependencies": ["fuchsia.bar"]
-        }
-
-        merged = copy.deepcopy(a)
-        merge_config(merged, b)
-        self.assertEqual(
-            merged, {
-                "services": {
-                    "fuchsia.bar": "bar",
-                    "fuchsia.foo": "foo"
-                },
-                "update_dependencies": ["fuchsia.foo", "fuchsia.bar"],
-                "exclusive": ["pass-through"]
-            })
-
-        merge_config(b, a)
-        self.assertEqual(
-            b, {
-                "services": {
-                    "fuchsia.bar": "bar",
-                    "fuchsia.foo": "foo"
-                },
-                "update_dependencies": ["fuchsia.bar", "fuchsia.foo"],
-                "exclusive": ["pass-through"]
-            })
 
 
 if __name__ == "__main__":
