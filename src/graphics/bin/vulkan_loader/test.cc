99c8fc380798d1d964eb552ade9e8c1b139840d5
John Bauman <jbauman@google.com>
2021-04-13 20:24:41 +0000

    [vulkan_loader] Add integration test
    Route /pkg from a component as system-lib, and use that to test whether
    vulkan-loader can load executable VMOs.
    
    Change-Id: I7cfade3e989e56b0629b79db5ef47da1c1d9555d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/490017
    Fuchsia-Auto-Submit: John Bauman <jbauman@google.com>
    Reviewed-by: Bryan Henry <bryanhenry@google.com>
    Reviewed-by: Craig Stout <cstout@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/graphics/bin/vulkan_loader/test.cc
@@ -0,0 +1,26 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuchsia/vulkan/loader/cpp/fidl.h>
+#include <lib/fdio/directory.h>
+#include <lib/zx/vmo.h>
+
+#include <gtest/gtest.h>
+
+TEST(VulkanLoader, SystemLoad) {
+  fuchsia::vulkan::loader::LoaderSyncPtr loader;
+  EXPECT_EQ(ZX_OK, fdio_service_connect("/svc/fuchsia.vulkan.loader.Loader",
+                                        loader.NewRequest().TakeChannel().release()));
+
+  zx::vmo vmo_out;
+  // The test instance reads from /pkg/bin, and this executable is guaranteed to be there.
+  EXPECT_EQ(ZX_OK, loader->Get("pkg-server", &vmo_out));
+  EXPECT_TRUE(vmo_out.is_valid());
+  zx_info_handle_basic_t handle_info;
+  EXPECT_EQ(ZX_OK, vmo_out.get_info(ZX_INFO_HANDLE_BASIC, &handle_info, sizeof(handle_info),
+                                    nullptr, nullptr));
+  EXPECT_TRUE(handle_info.rights & ZX_RIGHT_EXECUTE);
+  EXPECT_EQ(ZX_OK, loader->Get("not-present", &vmo_out));
+  EXPECT_FALSE(vmo_out.is_valid());
+}
