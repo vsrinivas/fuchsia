c5e8d381a75a781494ad7e4cd8b5f9531431d94e
Drew Fisher <zarvox@google.com>
2021-04-14 16:49:11 +0000

    Revert "[ulib][c] Migrate to zx_system_get_page_size"
    This reverts commit 822750a183cf6b77fbbca62f552db3095089d18a.
    
    Reason for revert: in chain with https://fuchsia-review.googlesource.com/c/fuchsia/+/509164 which is suspected of consistently breaking certain tests on the coverage bot
    
    Original change's description:
    > [ulib][c] Migrate to zx_system_get_page_size
    >
    > Migrates usages of page size constants to zx_system_get_page_size.
    >
    > Bug: 67899
    > Change-Id: Ic62c29d2ffb45877cea8b3feebe6e716521b8a68
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/509165
    > Commit-Queue: Adrian Danis <adanis@google.com>
    > Reviewed-by: Roland McGrath <mcgrathr@google.com>
    
    TBR=mcgrathr@google.com,adanis@google.com
    
    Change-Id: Id35e367b8b8a9aada186339ebe7a31caa7f36b19
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 67899
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/515603
    Reviewed-by: Drew Fisher <zarvox@google.com>
    Commit-Queue: Drew Fisher <zarvox@google.com>

--- zircon/system/ulib/c/test/debugdata-test-helper.cc
+++ zircon/system/ulib/c/test/debugdata-test-helper.cc
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "debugdata.h"
+
 #include <assert.h>
 #include <lib/zx/vmo.h>
 #include <zircon/assert.h>
 #include <zircon/sanitizer.h>
-
-#include "debugdata.h"
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
 
   if (!strcmp(argv[1], "publish_data")) {
     zx::vmo vmo;
-    ZX_ASSERT(zx::vmo::create(zx_system_get_page_size(), 0, &vmo) == ZX_OK);
+    ZX_ASSERT(zx::vmo::create(ZX_PAGE_SIZE, 0, &vmo) == ZX_OK);
     ZX_ASSERT(vmo.write(kTestData, 0, sizeof(kTestData)) == ZX_OK);
     ZX_ASSERT(vmo.set_property(ZX_PROP_NAME, kTestName, sizeof(kTestName)) == ZX_OK);
     __sanitizer_publish_data(kTestName, vmo.release());
