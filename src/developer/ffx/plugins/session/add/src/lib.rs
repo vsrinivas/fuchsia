9d449ae294514cb760735c49541a1e948880679c
Casey Dahlin <sadmac@google.com>
2021-04-14 21:06:23 +0000

    [ffx] Use fuchsia.element.Manager where possible
    Fixed: 73591
    Change-Id: I223f4939c3ae98a863aa9598338265e6ee1fbc35
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/512760
    Reviewed-by: Matthew Boetger <boetger@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/plugins/session/add/src/lib.rs
+++ src/developer/ffx/plugins/session/add/src/lib.rs
@@ -4,30 +4,48 @@
 
 use {
     anyhow::{format_err, Result},
-    ffx_core::ffx_plugin,
+    ffx_core::{ffx_bail, ffx_plugin},
     ffx_session_add_args::SessionAddCommand,
-    fidl::encoding::Decodable,
+    fidl_fuchsia_element::{ManagerProxy, Spec},
     fidl_fuchsia_session::{ElementManagerProxy, ElementSpec},
 };
 
-#[ffx_plugin(ElementManagerProxy = "core/session-manager:expose:fuchsia.session.ElementManager")]
-pub async fn add(element_manager_proxy: ElementManagerProxy, cmd: SessionAddCommand) -> Result<()> {
+#[ffx_plugin(
+    ElementManagerProxy = "core/session-manager:expose:fuchsia.session.ElementManager",
+    ManagerProxy = "core/session-manager:expose:fuchsia.element.Manager"
+)]
+pub async fn add(
+    element_manager_proxy: Option<ElementManagerProxy>,
+    manager_proxy: Option<ManagerProxy>,
+    cmd: SessionAddCommand,
+) -> Result<()> {
     println!("Add {} to the current session", &cmd.url);
-    element_manager_proxy
-        .propose_element(
-            ElementSpec { component_url: Some(cmd.url.to_string()), ..ElementSpec::new_empty() },
-            None,
-        )
-        .await?
-        .map_err(|err| format_err!("{:?}", err))
+    if let Some(manager_proxy) = manager_proxy {
+        manager_proxy
+            .propose_element(Spec { component_url: Some(cmd.url.to_string()), ..Spec::EMPTY }, None)
+            .await?
+            .map_err(|err| format_err!("{:?}", err))
+    } else if let Some(element_manager_proxy) = element_manager_proxy {
+        element_manager_proxy
+            .propose_element(
+                ElementSpec { component_url: Some(cmd.url.to_string()), ..ElementSpec::EMPTY },
+                None,
+            )
+            .await?
+            .map_err(|err| format_err!("{:?}", err))
+    } else {
+        ffx_bail!("Session does not expose an element manager");
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use {super::*, fidl_fuchsia_session::ElementManagerRequest};
+    use {
+        super::*, fidl_fuchsia_element::ManagerRequest, fidl_fuchsia_session::ElementManagerRequest,
+    };
 
     #[fuchsia_async::run_singlethreaded(test)]
-    async fn test_add_element() {
+    async fn test_add_element_legacy() {
         const TEST_ELEMENT_URL: &str = "Test Element Url";
 
         let proxy = setup_fake_element_manager_proxy(|req| match req {
@@ -38,7 +56,23 @@ mod test {
         });
 
         let add_cmd = SessionAddCommand { url: TEST_ELEMENT_URL.to_string() };
-        let response = add(proxy, add_cmd).await;
+        let response = add(Some(proxy), None, add_cmd).await;
+        assert!(response.is_ok());
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_add_element() {
+        const TEST_ELEMENT_URL: &str = "Test Element Url";
+
+        let proxy = setup_fake_manager_proxy(|req| match req {
+            ManagerRequest::ProposeElement { spec, responder, .. } => {
+                assert_eq!(spec.component_url.unwrap(), TEST_ELEMENT_URL.to_string());
+                let _ = responder.send(&mut Ok(()));
+            }
+        });
+
+        let add_cmd = SessionAddCommand { url: TEST_ELEMENT_URL.to_string() };
+        let response = add(None, Some(proxy), add_cmd).await;
         assert!(response.is_ok());
     }
 }
