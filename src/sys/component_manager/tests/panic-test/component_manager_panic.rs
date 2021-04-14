f0e38b567e0ce8fe64ec6302c65893cbf06a2a95
Xyan Bhatnagar <xbhatnag@google.com>
2021-04-14 17:36:02 +0000

    [component_manager] Migrate panic_test from OpaqueTest to test_manager
    This CL migrates `component_manager_panic_test` from OpaqueTest
    to use test_manager directly.
    
    We do this by starting a component_manager instance as a v2
    component under the test and using the events system to wait
    for it to crash.
    
    Multiply: component_manager_panic_test: 100
    Test: fx test component_manager_panic_test
    Change-Id: Icf085950a231439ef2c5b721f19f5cebd19d7842
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/515100
    Commit-Queue: Xyan Bhatnagar <xbhatnag@google.com>
    Reviewed-by: Gary Bressler <geb@google.com>
    Reviewed-by: Justin Mattson <jmatt@google.com>

--- src/sys/component_manager/tests/panic-test/component_manager_panic.rs
+++ src/sys/component_manager/tests/panic-test/component_manager_panic.rs
@@ -2,27 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use {fuchsia_async as fasync, test_utils_lib::opaque_test::OpaqueTest};
+use {
+    component_events::events::*, component_events::matcher::*, fidl_fuchsia_io as fio,
+    fidl_fuchsia_sys2 as fsys, fuchsia_async as fasync, fuchsia_component as fcomponent,
+};
 
 #[fasync::run_singlethreaded(test)]
 async fn test() {
-    // For the root component manifest, pass in the path the component manager
-    // itself, which should be completely invalid.
-    let mut test =
-        OpaqueTest::default("fuchsia-pkg://fuchsia.com/component_manager#bin/component_manager")
-            .await
-            .unwrap();
+    // Bind to the component manager component, causing it to start
+    let realm_svc = fcomponent::client::connect_to_service::<fsys::RealmMarker>()
+        .expect("Could not connect to Realm service");
+    let mut child = fsys::ChildRef { name: "component_manager".to_string(), collection: None };
 
-    let event_source = test.connect_to_event_source().await.unwrap();
+    // Create endpoints for the fuchsia.io.Directory protocol.
+    // Component manager will connect us to the exposed directory of the component we bound to.
+    // This isn't needed for this test, but we must do it anyway.
+    let (_, exposed_dir) = fidl::endpoints::create_endpoints::<fio::DirectoryMarker>().unwrap();
+    realm_svc
+        .bind_child(&mut child, exposed_dir)
+        .await
+        .expect("Could not send bind_child command")
+        .expect("bind_child command did not succeed");
 
-    // Errors during manifest resolution also block progress. `start_component_tree` resumes the
-    // task attempting to start the root component.
-    event_source.start_component_tree().await;
-
-    // We expect that component manager will crash since the root component
-    // manifest is invalid.
-    let exit_status = test.component_manager_app.wait().await.unwrap();
-    if exit_status.success() {
-        panic!("component manager should have exited with error, but exited normally");
-    }
+    // Wait for the component manager to stop because of a panic
+    let source = EventSource::new().expect("Could not connect to fuchsia.sys2.EventSource");
+    let mut stream = source
+        .take_static_event_stream("panic_test_event_stream")
+        .await
+        .expect("Could not take static event stream");
+    let mut matcher = EventMatcher::ok().stop(Some(ExitStatusMatcher::Crash(11)));
+    matcher.expect_match::<Stopped>(&mut stream).await;
 }
