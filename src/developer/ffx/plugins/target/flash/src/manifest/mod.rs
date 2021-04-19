6901dd1fbc3e6d138f6bf6f58bea5485cbc4af34
Matt Boetger <boetger@google.com>
2021-04-19 22:16:34 +0000

    [ffx] Move V1 flash manifest code.
    This will make it easier to add a v2 later on.
    
    Bug: 74508
    MULTIPLY: ffx_flash_lib_test
    
    Change-Id: Iab2438a7d8085d618a9b6c67b748fb8f7e467f61
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/516173
    Commit-Queue: Matthew Boetger <boetger@google.com>
    Reviewed-by: Andrew Davies <awdavies@google.com>

---
+++ src/developer/ffx/plugins/target/flash/src/manifest/mod.rs
@@ -0,0 +1,308 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    crate::manifest::v1::FlashManifest as FlashManifestV1,
+    anyhow::{anyhow, bail, Context, Result},
+    async_trait::async_trait,
+    chrono::{DateTime, Duration, Utc},
+    ffx_core::ffx_bail,
+    ffx_flash_args::FlashCommand,
+    fidl::endpoints::{create_endpoints, ServerEnd},
+    fidl_fuchsia_developer_bridge::{
+        FastbootProxy, UploadProgressListenerMarker, UploadProgressListenerRequest,
+    },
+    futures::prelude::*,
+    futures::try_join,
+    serde::{Deserialize, Serialize},
+    serde_json::{from_value, Value},
+    std::io::{Read, Write},
+    std::path::{Path, PathBuf},
+    termion::{color, style},
+};
+
+pub(crate) mod v1;
+
+pub(crate) const UNKNOWN_VERSION: &str = "Unknown flash manifest version";
+const LARGE_FILE: &str = "large file, please wait... ";
+
+#[async_trait]
+pub(crate) trait Flash {
+    async fn flash<W>(
+        &self,
+        writer: &mut W,
+        fastboot_proxy: FastbootProxy,
+        cmd: FlashCommand,
+    ) -> Result<()>
+    where
+        W: Write + Send;
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub(crate) struct ManifestFile {
+    version: u64,
+    manifest: Value,
+}
+
+pub(crate) enum FlashManifest {
+    V1(FlashManifestV1),
+}
+
+impl FlashManifest {
+    pub(crate) fn load<R: Read>(reader: R) -> Result<Self> {
+        let value: Value = serde_json::from_reader::<R, Value>(reader)
+            .context("reading flash manifest from disk")?;
+        // GN generated JSON always comes from a list
+        let manifest: ManifestFile = match value {
+            Value::Array(v) => from_value(v[0].clone())?,
+            Value::Object(_) => from_value(value)?,
+            _ => ffx_bail!("Could not parse flash manifest."),
+        };
+        match manifest.version {
+            1 => Ok(Self::V1(from_value(manifest.manifest.clone())?)),
+            _ => ffx_bail!("{}", UNKNOWN_VERSION),
+        }
+    }
+}
+
+#[async_trait]
+impl Flash for FlashManifest {
+    async fn flash<W>(
+        &self,
+        writer: &mut W,
+        fastboot_proxy: FastbootProxy,
+        cmd: FlashCommand,
+    ) -> Result<()>
+    where
+        W: Write + Send,
+    {
+        match self {
+            Self::V1(v) => v.flash(writer, fastboot_proxy, cmd).await,
+        }
+    }
+}
+
+pub(crate) fn done_time<W: Write + Send>(
+    writer: &mut W,
+    duration: Duration,
+) -> std::io::Result<()> {
+    writeln!(
+        writer,
+        "{}Done{} [{}{:.2}s{}]",
+        color::Fg(color::Green),
+        style::Reset,
+        color::Fg(color::Blue),
+        (duration.num_milliseconds() as f32) / (1000 as f32),
+        style::Reset
+    )?;
+    writer.flush()
+}
+
+async fn handle_upload_progress_for_staging<W: Write + Send>(
+    writer: &mut W,
+    prog_server: ServerEnd<UploadProgressListenerMarker>,
+) -> Result<Option<DateTime<Utc>>> {
+    let mut stream = prog_server.into_stream()?;
+    let mut start_time: Option<DateTime<Utc>> = None;
+    let mut finish_time: Option<DateTime<Utc>> = None;
+    loop {
+        match stream.try_next().await? {
+            Some(UploadProgressListenerRequest::OnStarted { size, .. }) => {
+                start_time.replace(Utc::now());
+                log::debug!("Upload started: {}", size);
+                write!(writer, "Uploading... ")?;
+                if size > (1 << 24) {
+                    write!(writer, "{}", LARGE_FILE)?;
+                }
+                writer.flush()?;
+            }
+            Some(UploadProgressListenerRequest::OnFinished { .. }) => {
+                if let Some(st) = start_time {
+                    let d = Utc::now().signed_duration_since(st);
+                    log::debug!("Upload duration: {:.2}s", (d.num_milliseconds() / 1000));
+                    done_time(writer, d)?;
+                } else {
+                    // Write done without the time .
+                    writeln!(writer, "{}Done{}", color::Fg(color::Green), style::Reset)?;
+                    writer.flush()?;
+                }
+                finish_time.replace(Utc::now());
+                log::debug!("Upload finished");
+            }
+            Some(UploadProgressListenerRequest::OnError { error, .. }) => {
+                log::error!("{}", error);
+                bail!(error)
+            }
+            Some(UploadProgressListenerRequest::OnProgress { bytes_written, .. }) => {
+                log::debug!("Upload progress: {}", bytes_written);
+            }
+            None => return Ok(finish_time),
+        }
+    }
+}
+
+async fn handle_upload_progress_for_flashing<W: Write + Send>(
+    name: &str,
+    writer: &mut W,
+    prog_server: ServerEnd<UploadProgressListenerMarker>,
+) -> Result<Option<DateTime<Utc>>> {
+    let mut stream = prog_server.into_stream()?;
+    let mut start_time: Option<DateTime<Utc>> = None;
+    let mut finish_time: Option<DateTime<Utc>> = None;
+    let mut is_large: bool = false;
+    loop {
+        match stream.try_next().await? {
+            Some(UploadProgressListenerRequest::OnStarted { size, .. }) => {
+                start_time.replace(Utc::now());
+                log::debug!("Upload started: {}", size);
+                write!(writer, "Uploading... ")?;
+                if size > (1 << 24) {
+                    is_large = true;
+                    write!(writer, "{}", LARGE_FILE)?;
+                }
+                writer.flush()?;
+            }
+            Some(UploadProgressListenerRequest::OnFinished { .. }) => {
+                if let Some(st) = start_time {
+                    let d = Utc::now().signed_duration_since(st);
+                    log::debug!("Upload duration: {:.2}s", (d.num_milliseconds() / 1000));
+                    done_time(writer, d)?;
+                } else {
+                    // Write done without the time .
+                    writeln!(writer, "{}Done{}", color::Fg(color::Green), style::Reset)?;
+                    writer.flush()?;
+                }
+                write!(writer, "Partitioning {}... ", name)?;
+                if is_large {
+                    write!(writer, "{}", LARGE_FILE)?;
+                }
+                writer.flush()?;
+                finish_time.replace(Utc::now());
+                log::debug!("Upload finished");
+            }
+            Some(UploadProgressListenerRequest::OnError { error, .. }) => {
+                log::error!("{}", error);
+                ffx_bail!("{}", error)
+            }
+            Some(UploadProgressListenerRequest::OnProgress { bytes_written, .. }) => {
+                log::debug!("Upload progress: {}", bytes_written);
+            }
+            None => return Ok(finish_time),
+        }
+    }
+}
+
+pub(crate) async fn stage_file<W: Write + Send>(
+    writer: &mut W,
+    path: &Path,
+    file: &str,
+    fastboot_proxy: &FastbootProxy,
+) -> Result<()> {
+    let (prog_client, prog_server) = create_endpoints::<UploadProgressListenerMarker>()?;
+    let file_to_upload = get_file(path, file).context("reconciling file for upload")?;
+    writeln!(writer, "Preparing to stage {}", file_to_upload)?;
+    try_join!(
+        fastboot_proxy.stage(&file_to_upload, prog_client).map_err(|e| anyhow!(e)),
+        handle_upload_progress_for_staging(writer, prog_server),
+    )
+    .and_then(|(stage, _)| {
+        stage.map_err(|e| anyhow!("There was an error staging {}: {:?}", file_to_upload, e))
+    })
+}
+
+fn get_file(path: &Path, file: &str) -> Result<String> {
+    if PathBuf::from(file).is_absolute() {
+        Ok(file.to_string())
+    } else if let Some(p) = path.parent() {
+        let mut parent = p.to_path_buf();
+        parent.push(file);
+        if let Some(f) = parent.to_str() {
+            Ok(f.to_string())
+        } else {
+            bail!("Only UTF-8 strings are currently supported")
+        }
+    } else {
+        bail!("Could not get file to upload");
+    }
+}
+
+pub(crate) async fn flash_partition<W: Write + Send>(
+    writer: &mut W,
+    path: &Path,
+    name: &str,
+    file: &str,
+    fastboot_proxy: &FastbootProxy,
+) -> Result<()> {
+    let (prog_client, prog_server) = create_endpoints::<UploadProgressListenerMarker>()?;
+    let file_to_upload = get_file(path, file).context("reconciling file for upload")?;
+    writeln!(writer, "Preparing to upload {}", file_to_upload)?;
+    try_join!(
+        fastboot_proxy.flash(name, &file_to_upload, prog_client).map_err(|e| anyhow!(e)),
+        handle_upload_progress_for_flashing(name, writer, prog_server),
+    )
+    .and_then(|(flash, prog)| {
+        if let Some(p) = prog {
+            let d = Utc::now().signed_duration_since(p);
+            log::debug!("Partition duration: {:.2}s", (d.num_milliseconds() / 1000));
+            done_time(writer, d)?;
+        } else {
+            // Write a line break otherwise
+            writeln!(writer, "{}Done{}", color::Fg(color::Green), style::Reset)?;
+            writer.flush()?;
+        }
+        flash.map_err(|e| {
+            anyhow!("There was an error flashing \"{}\" - {}: {:?}", name, file_to_upload, e)
+        })
+    })
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// tests
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json::from_str;
+    use std::io::BufReader;
+
+    const UNKNOWN_VERSION: &'static str = r#"{
+        "version": 99999,
+        "manifest": "test"
+    }"#;
+
+    const MANIFEST: &'static str = r#"{
+        "version": 1,
+        "manifest": []
+    }"#;
+
+    const ARRAY_MANIFEST: &'static str = r#"[{
+        "version": 1,
+        "manifest": []
+    }]"#;
+
+    #[test]
+    fn test_deserialization() -> Result<()> {
+        let _manifest: ManifestFile = from_str(MANIFEST)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_loading_unknown_version() {
+        let manifest_contents = UNKNOWN_VERSION.to_string();
+        let result = FlashManifest::load(BufReader::new(manifest_contents.as_bytes()));
+        assert!(result.is_err());
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_loading_version_1() -> Result<()> {
+        let manifest_contents = MANIFEST.to_string();
+        FlashManifest::load(BufReader::new(manifest_contents.as_bytes())).map(|_| ())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_loading_version_1_from_array() -> Result<()> {
+        let manifest_contents = ARRAY_MANIFEST.to_string();
+        FlashManifest::load(BufReader::new(manifest_contents.as_bytes())).map(|_| ())
+    }
+}
---
+++ src/developer/ffx/plugins/target/flash/src/manifest/v1.rs
@@ -0,0 +1,344 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    crate::manifest::{done_time, flash_partition, stage_file, Flash},
+    anyhow::{anyhow, bail, Context as _, Result},
+    async_trait::async_trait,
+    chrono::Utc,
+    ffx_core::ffx_bail,
+    ffx_flash_args::{FlashCommand, OemFile},
+    fidl::endpoints::create_endpoints,
+    fidl_fuchsia_developer_bridge::{FastbootProxy, RebootListenerMarker, RebootListenerRequest},
+    futures::prelude::*,
+    futures::try_join,
+    serde::{Deserialize, Serialize},
+    std::{io::Write, path::Path},
+};
+
+pub(crate) const MISSING_PRODUCT: &str = "Manifest does not contain product";
+pub(crate) const MULTIPLE_PRODUCT: &str =
+    "Multiple products found in manifest. Please specify a product";
+pub(crate) const DEFAULT_PRODUCT: &str = "fuchsia";
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub(crate) struct Product {
+    pub(crate) name: String,
+    pub(crate) bootloader_partitions: Vec<Partition>,
+    pub(crate) partitions: Vec<Partition>,
+    pub(crate) oem_files: Vec<OemFile>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub(crate) struct Partition(String, String);
+
+impl Partition {
+    pub(crate) fn name(&self) -> &str {
+        self.0.as_str()
+    }
+
+    pub(crate) fn file(&self) -> &str {
+        self.1.as_str()
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub(crate) struct FlashManifest(pub(crate) Vec<Product>);
+
+#[async_trait]
+impl Flash for FlashManifest {
+    async fn flash<W>(
+        &self,
+        writer: &mut W,
+        fastboot_proxy: FastbootProxy,
+        cmd: FlashCommand,
+    ) -> Result<()>
+    where
+        W: Write + Send,
+    {
+        let path = Path::new(&cmd.manifest)
+            .canonicalize()
+            .context("Getting absolute path of flashing manifest")?;
+        let product = match cmd.product {
+            Some(p) => match self.0.iter().find(|product| product.name == p) {
+                Some(res) => res,
+                None => ffx_bail!("{} {}", MISSING_PRODUCT, p),
+            },
+            None => {
+                if self.0.len() == 1 {
+                    &self.0[0]
+                } else {
+                    match self.0.iter().find(|product| product.name == DEFAULT_PRODUCT) {
+                        Some(res) => res,
+                        None => ffx_bail!("{}", MULTIPLE_PRODUCT),
+                    }
+                }
+            }
+        };
+
+        for partition in &product.bootloader_partitions {
+            flash_partition(writer, &path, partition.name(), partition.file(), &fastboot_proxy)
+                .await?;
+        }
+        if product.bootloader_partitions.len() > 0 {
+            write!(writer, "Rebooting to bootloader... ")?;
+            writer.flush()?;
+            let (reboot_client, reboot_server) = create_endpoints::<RebootListenerMarker>()?;
+            let mut stream = reboot_server.into_stream()?;
+            let start_time = Utc::now();
+            try_join!(
+                fastboot_proxy
+                    .reboot_bootloader(reboot_client)
+                    .map_err(|e| anyhow!("fidl error when rebooting to bootloader: {:?}", e)),
+                async move {
+                    if let Some(RebootListenerRequest::OnReboot { control_handle: _ }) =
+                        stream.try_next().await?
+                    {
+                        Ok(())
+                    } else {
+                        bail!("Did not receive reboot signal");
+                    }
+                }
+            )
+            .and_then(|(reboot, _)| {
+                let d = Utc::now().signed_duration_since(start_time);
+                log::debug!("Reboot duration: {:.2}s", (d.num_milliseconds() / 1000));
+                done_time(writer, d)?;
+                reboot.map_err(|e| anyhow!("failed booting to bootloader: {:?}", e))
+            })?;
+        }
+        for partition in &product.partitions {
+            flash_partition(writer, &path, partition.name(), partition.file(), &fastboot_proxy)
+                .await?;
+        }
+        for oem_file in &product.oem_files {
+            stage_file(writer, &path, oem_file.file(), &fastboot_proxy).await?;
+            writeln!(writer, "Sending command \"{}\"", oem_file.command())?;
+            fastboot_proxy.oem(oem_file.command()).await?.map_err(|_| {
+                anyhow!("There was an error sending oem command \"{}\"", oem_file.command())
+            })?;
+        }
+        for oem_file in &cmd.oem_stage {
+            stage_file(writer, &path, oem_file.file(), &fastboot_proxy).await?;
+            writeln!(writer, "Sending command \"{}\"", oem_file.command())?;
+            fastboot_proxy.oem(oem_file.command()).await?.map_err(|_| {
+                anyhow!("There was an error sending oem command \"{}\"", oem_file.command())
+            })?;
+        }
+        fastboot_proxy
+            .erase("misc")
+            .await?
+            .map_err(|_| anyhow!("Could not erase misc partition"))?;
+        fastboot_proxy.set_active("a").await?.map_err(|_| anyhow!("Could not set active slot"))?;
+        fastboot_proxy.continue_boot().await?.map_err(|_| anyhow!("Could not reboot device"))?;
+        writeln!(writer, "Continuing to boot - this could take awhile")?;
+        Ok(())
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// tests
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::setup;
+    use regex::Regex;
+    use serde_json::from_str;
+    use tempfile::NamedTempFile;
+
+    const SIMPLE_MANIFEST: &'static str = r#"[
+            {
+                "name": "zedboot",
+                "bootloader_partitions": [],
+                "partitions": [
+                    ["test1", "path1"],
+                    ["test2", "path2"],
+                    ["test3", "path3"],
+                    ["test4", "path4"],
+                    ["test5", "path5"]
+                ],
+                "oem_files": []
+            }
+    ]"#;
+
+    const MANIFEST: &'static str = r#"[
+        {
+            "name": "zedboot",
+            "bootloader_partitions": [
+                ["test1", "path1"],
+                ["test2", "path2"]
+            ],
+            "partitions": [
+                ["test1", "path1"],
+                ["test2", "path2"],
+                ["test3", "path3"],
+                ["test4", "path4"],
+                ["test5", "path5"]
+            ],
+            "oem_files": [
+                ["test1", "path1"],
+                ["test2", "path2"]
+            ]
+        },
+        {
+            "name": "product",
+            "bootloader_partitions": [],
+            "partitions": [
+                ["test10", "path10"],
+                ["test20", "path20"],
+                ["test30", "path30"]
+            ],
+            "oem_files": []
+        }
+    ]"#;
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_deserializing_should_work() -> Result<()> {
+        let v: FlashManifest = from_str(MANIFEST)?;
+        let zedboot: &Product = &v.0[0];
+        assert_eq!("zedboot", zedboot.name);
+        assert_eq!(2, zedboot.bootloader_partitions.len());
+        let bootloader_expected = [["test1", "path1"], ["test2", "path2"]];
+        for x in 0..bootloader_expected.len() {
+            assert_eq!(zedboot.bootloader_partitions[x].name(), bootloader_expected[x][0]);
+            assert_eq!(zedboot.bootloader_partitions[x].file(), bootloader_expected[x][1]);
+        }
+        assert_eq!(5, zedboot.partitions.len());
+        let expected = [
+            ["test1", "path1"],
+            ["test2", "path2"],
+            ["test3", "path3"],
+            ["test4", "path4"],
+            ["test5", "path5"],
+        ];
+        for x in 0..expected.len() {
+            assert_eq!(zedboot.partitions[x].name(), expected[x][0]);
+            assert_eq!(zedboot.partitions[x].file(), expected[x][1]);
+        }
+        assert_eq!(2, zedboot.oem_files.len());
+        let oem_files_expected = [["test1", "path1"], ["test2", "path2"]];
+        for x in 0..oem_files_expected.len() {
+            assert_eq!(zedboot.oem_files[x].command(), oem_files_expected[x][0]);
+            assert_eq!(zedboot.oem_files[x].file(), oem_files_expected[x][1]);
+        }
+        let product: &Product = &v.0[1];
+        assert_eq!("product", product.name);
+        assert_eq!(0, product.bootloader_partitions.len());
+        assert_eq!(3, product.partitions.len());
+        let expected2 = [["test10", "path10"], ["test20", "path20"], ["test30", "path30"]];
+        for x in 0..expected2.len() {
+            assert_eq!(product.partitions[x].name(), expected2[x][0]);
+            assert_eq!(product.partitions[x].file(), expected2[x][1]);
+        }
+        assert_eq!(0, product.oem_files.len());
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_no_product_should_succeed_if_one_product() -> Result<()> {
+        let v: FlashManifest = from_str(SIMPLE_MANIFEST)?;
+        let tmp_file = NamedTempFile::new().expect("tmp access failed");
+        let tmp_file_name = tmp_file.path().to_string_lossy().to_string();
+        let (_, proxy) = setup();
+        let mut writer = Vec::<u8>::new();
+        v.flash(&mut writer, proxy, FlashCommand { manifest: tmp_file_name, ..Default::default() })
+            .await
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_no_product_should_fail_if_multiple_products() -> Result<()> {
+        let v: FlashManifest = from_str(MANIFEST)?;
+        let tmp_file = NamedTempFile::new().expect("tmp access failed");
+        let tmp_file_name = tmp_file.path().to_string_lossy().to_string();
+        let (_, proxy) = setup();
+        let mut writer = Vec::<u8>::new();
+        assert!(v
+            .flash(
+                &mut writer,
+                proxy,
+                FlashCommand { manifest: tmp_file_name, ..Default::default() },
+            )
+            .await
+            .is_err());
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_no_product_should_fail_if_product_missing() -> Result<()> {
+        let tmp_file = NamedTempFile::new().expect("tmp access failed");
+        let tmp_file_name = tmp_file.path().to_string_lossy().to_string();
+        let v: FlashManifest = from_str(MANIFEST)?;
+        let (_, proxy) = setup();
+        let mut writer = Vec::<u8>::new();
+        assert!(v
+            .flash(
+                &mut writer,
+                proxy,
+                FlashCommand {
+                    manifest: tmp_file_name,
+                    product: Some("Unknown".to_string()),
+                    ..Default::default()
+                }
+            )
+            .await
+            .is_err());
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_should_succeed_if_product_found() -> Result<()> {
+        let v: FlashManifest = from_str(MANIFEST)?;
+        let tmp_file = NamedTempFile::new().expect("tmp access failed");
+        let tmp_file_name = tmp_file.path().to_string_lossy().to_string();
+        let (_, proxy) = setup();
+        let mut writer = Vec::<u8>::new();
+        v.flash(
+            &mut writer,
+            proxy,
+            FlashCommand {
+                manifest: tmp_file_name,
+                product: Some("product".to_string()),
+                ..Default::default()
+            },
+        )
+        .await?;
+        let output = String::from_utf8(writer).expect("utf-8 string");
+        for partition in &v.0[1].partitions {
+            let name_listing = Regex::new(&partition.name()).expect("test regex");
+            let path_listing = Regex::new(&partition.file()).expect("test regex");
+            assert_eq!(name_listing.find_iter(&output).count(), 1);
+            assert_eq!(path_listing.find_iter(&output).count(), 1);
+        }
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_oem_file_should_be_staged_from_command() -> Result<()> {
+        let v: FlashManifest = from_str(SIMPLE_MANIFEST)?;
+        let (state, proxy) = setup();
+        let test_oem_cmd = "test-oem-cmd";
+        let tmp_file = NamedTempFile::new().expect("tmp access failed");
+        let tmp_file_name = tmp_file.path().to_string_lossy().to_string();
+        let test_staged_file = format!("{},{}", test_oem_cmd, tmp_file_name).parse::<OemFile>()?;
+        let manifest_file = NamedTempFile::new().expect("tmp access failed");
+        let manifest_file_name = manifest_file.path().to_string_lossy().to_string();
+        let mut writer = Vec::<u8>::new();
+        v.flash(
+            &mut writer,
+            proxy,
+            FlashCommand {
+                manifest: manifest_file_name,
+                oem_stage: vec![test_staged_file],
+                ..Default::default()
+            },
+        )
+        .await?;
+        let state = state.lock().unwrap();
+        assert_eq!(1, state.staged_files.len());
+        assert_eq!(1, state.oem_commands.len());
+        assert_eq!(test_oem_cmd, state.oem_commands[0]);
+        Ok(())
+    }
+}
