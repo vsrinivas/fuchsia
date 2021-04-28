0d876757bb67c1179711ee31e00352dcc47beb8e
Roland McGrath <mcgrathr@google.com>
2021-04-28 02:40:53 +0000

    [libc] Fix seed48 definition prototype to match declaration
    GCC 11 likes to warn about this mismatch.
    
    Change-Id: Ice206e2b8bd32ab6466b2939b6cd2a8bd2408cf5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/521818
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Petr Hosek <phosek@google.com>

--- zircon/third_party/ulib/musl/src/prng/seed48.c
+++ zircon/third_party/ulib/musl/src/prng/seed48.c
@@ -3,7 +3,7 @@
 
 #include "rand48_impl.h"
 
-unsigned short* seed48(unsigned short* s) {
+unsigned short* seed48(unsigned short s[3]) {
   static unsigned short p[3];
   memcpy(p, __seed48, sizeof p);
   memcpy(__seed48, s, sizeof p);
