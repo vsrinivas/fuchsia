acc269778c389c03a5d83170bf2961636abdf33a
Adam Barth <abarth@google.com>
2021-04-29 00:02:08 +0000

    [starnix] Add Fuchsia pipe file object
    This kind of file object will let us connect stdin/stdout/stderr from
    inside starnix to the rest of the Fuchsia system.
    
    Bug: 74060
    Change-Id: I74f48d85d887066db7db0c8a2da440295011e8bf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/522125
    Reviewed-by: Theodore Dubois <tbodt@google.com>
    Commit-Queue: Adam Barth <abarth@google.com>

--- src/proc/bin/starnix/fs/fuchsia/mod.rs
+++ src/proc/bin/starnix/fs/fuchsia/mod.rs
@@ -2,8 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use fuchsia_zircon::{self as zx, AsHandleRef, HandleBased};
+
+use crate::fs::FileHandle;
+
+mod pipe;
 mod remote;
 mod syslog;
 
+pub use pipe::*;
 pub use remote::*;
 pub use syslog::*;
+
+/// Create a FileHandle from a zx::Handle.
+#[allow(dead_code)] // Will be used by process initialization later.
+pub fn create_file_from_handle(handle: zx::Handle) -> Result<FileHandle, zx::Status> {
+    let info = handle.basic_info()?;
+    match info.object_type {
+        zx::ObjectType::SOCKET => FuchsiaPipe::from_socket(zx::Socket::from_handle(handle)),
+        _ => Err(zx::Status::NOT_SUPPORTED),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_create_from_invalid_handle() {
+        assert!(create_file_from_handle(zx::Handle::invalid()).is_err());
+    }
+
+    #[test]
+    fn test_create_pipe_from_handle() {
+        let (left_handle, right_handle) =
+            zx::Socket::create(zx::SocketOpts::STREAM).expect("failed to create socket");
+        create_file_from_handle(left_handle.into_handle())
+            .expect("failed to create left FileHandle");
+        create_file_from_handle(right_handle.into_handle())
+            .expect("failed to create right FileHandle");
+    }
+}
---
+++ src/proc/bin/starnix/fs/fuchsia/pipe.rs
@@ -0,0 +1,60 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use fuchsia_zircon as zx;
+use std::sync::Arc;
+
+use crate::fd_impl_nonseekable;
+use crate::fs::*;
+use crate::task::*;
+use crate::uapi::*;
+
+#[derive(FileObject)]
+pub struct FuchsiaPipe {
+    common: FileCommon,
+    socket: zx::Socket,
+}
+
+impl FuchsiaPipe {
+    pub fn from_socket(socket: zx::Socket) -> Result<FileHandle, zx::Status> {
+        // TODO: Distinguish between stream and datagram sockets.
+        Ok(Arc::new(FuchsiaPipe { common: FileCommon::default(), socket }))
+    }
+}
+
+impl FileObject for FuchsiaPipe {
+    fd_impl_nonseekable!();
+
+    fn write(&self, task: &Task, data: &[iovec_t]) -> Result<usize, Errno> {
+        let mut size = 0;
+        for vec in data {
+            let mut local = vec![0; vec.iov_len];
+            task.mm.read_memory(vec.iov_base, &mut local)?;
+            let actual = self.socket.write(&local).map_err(Errno::from_status_like_fdio)?;
+            size += actual;
+            if actual != vec.iov_len {
+                break;
+            }
+        }
+        Ok(size)
+    }
+
+    fn read(&self, task: &Task, data: &[iovec_t]) -> Result<usize, Errno> {
+        let mut size = 0;
+        for vec in data {
+            let mut local = vec![0; vec.iov_len];
+            let actual = self.socket.read(&mut local).map_err(Errno::from_status_like_fdio)?;
+            task.mm.write_memory(vec.iov_base, &local[0..actual])?;
+            size += actual;
+            if actual != vec.iov_len {
+                break;
+            }
+        }
+        Ok(size)
+    }
+
+    fn fstat(&self, _task: &Task) -> Result<stat_t, Errno> {
+        Err(ENOSYS)
+    }
+}
