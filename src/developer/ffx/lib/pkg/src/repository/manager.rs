3afb558ff01a9b9a818afbb3572bba43ae3eaf0d
Casey Dahlin <sadmac@google.com>
2021-04-29 00:32:16 +0000

    [ffx][repository] Allow manager to load repos from config
    Change-Id: Ie869ea2236a26f41c4efb25ab848f3f927ede7d1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/519965
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/lib/pkg/src/repository/manager.rs
+++ src/developer/ffx/lib/pkg/src/repository/manager.rs
@@ -3,12 +3,22 @@
 // found in the LICENSE file.
 
 use {
-    super::Repository,
+    super::{file_system::FileSystemRepository, Repository},
+    anyhow::Result,
     parking_lot::RwLock,
+    serde::{Deserialize, Serialize},
+    serde_json::Value,
     std::{collections::HashMap, sync::Arc},
 };
 
 type ArcRepository = Arc<dyn Repository + Send + Sync>;
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum RepositorySpec {
+    #[serde(rename = "fs")]
+    FileSystem { path: std::path::PathBuf },
+}
 
 /// RepositoryManager is responsible for managing all the repositories in use by ffx.
 pub struct RepositoryManager {
@@ -26,6 +36,17 @@ impl RepositoryManager {
         self.repositories.write().insert(repo.name().to_string(), repo);
     }
 
+    /// Use a configuration value to instantiate a [Repository] and add it to the [RepositoryManager].
+    pub fn add_from_config(&self, name: String, spec: Value) -> Result<()> {
+        let spec = serde_json::from_value(spec)?;
+        match spec {
+            RepositorySpec::FileSystem { path } => {
+                self.add(Arc::new(FileSystemRepository::new(name, path)));
+                Ok(())
+            }
+        }
+    }
+
     /// Get a [Repository].
     pub fn get(&self, repo_name: &str) -> Option<ArcRepository> {
         self.repositories.read().get(repo_name).map(|repo| Arc::clone(repo))
@@ -36,5 +57,23 @@ impl RepositoryManager {
         let mut ret = self.repositories.read().values().map(Arc::clone).collect::<Vec<_>>();
         ret.sort_unstable_by_key(|x| x.name().to_owned());
         ret.into_iter()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use {fuchsia_async as fasync, serde_json::json};
+
+    #[fasync::run_singlethreaded(test)]
+    async fn fs_from_config() {
+        let value = json!({ "type": "fs", "path": "/nowhere" });
+        let manager = RepositoryManager::new();
+        manager.add_from_config("my_repo".to_owned(), value).expect("Error adding value");
+
+        assert_eq!(
+            manager.repositories().map(|x| x.name().to_owned()).collect::<Vec<_>>(),
+            vec!["my_repo".to_owned()]
+        );
     }
 }
