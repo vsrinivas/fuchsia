668361c15c458fca181e91c32adadd3c14b32417
Marie Janssen <jamuraa@google.com>
2021-05-04 15:23:33 +0000

    [bt][lib] Correct generation bug in DetachableMap
    In DetachableMap, you can get a DetachableWeak reference to an item, but
    then that item could be replaced later.  If you then detached the original
    item, it would remove the new item from the Map, even though you didn't
    detach a reference to that item.
    
    Add a bool tracking whether the item is in the parent map, and check it
    was true before removing it.
    
    Bug: 75375
    Test: added cant_detach_replaced_key
    Test: fx test bluetooth-crate-tests
    Change-Id: I024bfead7fdfd72273ba034257b420dfe163c3d3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/520371
    Commit-Queue: Marie Janssen 💖 <jamuraa@google.com>
    Fuchsia-Auto-Submit: Marie Janssen 💖 <jamuraa@google.com>
    Reviewed-by: Jeff Belgum <belgum@google.com>

--- src/connectivity/bluetooth/lib/fuchsia-bluetooth/src/detachable_map.rs
+++ src/connectivity/bluetooth/lib/fuchsia-bluetooth/src/detachable_map.rs
@@ -7,26 +7,46 @@ use std::{
     borrow::Borrow,
     collections::{hash_map::Entry, HashMap},
     hash::Hash,
+    sync::atomic::{AtomicBool, Ordering},
     sync::{Arc, Weak},
 };
+
+// TODO(fxbug.dev/74427): This would be so so much easier with Arc::new_cyclic.
 
 /// A weak reference to an entry in a DetachableMap. This is a weak reference, if the entry is
 /// detached before it is upgraded, the entry can be gone.
 pub struct DetachableWeak<K, V> {
+    /// Weak reference to the value, upgrading this doesn't guarantee it's in the map, but usually
+    /// it is.
     inner: Weak<V>,
-    parent: Arc<RwLock<HashMap<K, Arc<V>>>>,
+    parent: Arc<RwLock<HashMap<K, (Arc<V>, Arc<AtomicBool>)>>>,
+    /// Key reference, shared with all DetachableWeaks that reference the same entry in `parent`.
+    /// false if anyone detached the entry or the key was replaced in `parent`, to prevent removing
+    /// an entry at the same key.
+    key_ref: Arc<AtomicBool>,
+    /// A copy of `key` that can be used whenever.
     key: K,
 }
 
 impl<K: Clone, V> Clone for DetachableWeak<K, V> {
     fn clone(&self) -> Self {
-        Self { inner: self.inner.clone(), parent: self.parent.clone(), key: self.key.clone() }
+        Self {
+            inner: self.inner.clone(),
+            parent: self.parent.clone(),
+            key_ref: self.key_ref.clone(),
+            key: self.key.clone(),
+        }
     }
 }
 
 impl<K: Hash + Eq, V> DetachableWeak<K, V> {
-    fn new(item: &Arc<V>, parent: Arc<RwLock<HashMap<K, Arc<V>>>>, key: K) -> Self {
-        Self { inner: Arc::downgrade(item), parent, key }
+    fn new(
+        item: &Arc<V>,
+        parent: Arc<RwLock<HashMap<K, (Arc<V>, Arc<AtomicBool>)>>>,
+        key: K,
+        key_ref: Arc<AtomicBool>,
+    ) -> Self {
+        Self { inner: Arc::downgrade(item), parent, key, key_ref }
     }
 
     /// Attempt to upgrade the weak pointer to an Arc, extending the lifetime of the value if
@@ -39,7 +59,12 @@ impl<K: Hash + Eq, V> DetachableWeak<K, V> {
     /// If other references to the item exist (from `upgrade`), it will not be dropped until those
     /// references are dropped.
     pub fn detach(self) {
-        self.parent.write().remove(&self.key);
+        let mut lock = self.parent.write();
+        if self.key_ref.swap(false, Ordering::Relaxed) {
+            // Dropping this reference only frees the key_ref and value if no other references are
+            // held.
+            let _ = lock.remove(&self.key);
+        }
     }
 
     /// Get a reference to the key for this entry.
@@ -49,7 +74,7 @@ impl<K: Hash + Eq, V> DetachableWeak<K, V> {
 }
 
 pub struct LazyEntry<K, V> {
-    parent: Arc<RwLock<HashMap<K, Arc<V>>>>,
+    parent: Arc<RwLock<HashMap<K, (Arc<V>, Arc<AtomicBool>)>>>,
     key: K,
 }
 
@@ -60,7 +85,7 @@ impl<K: Clone, V> Clone for LazyEntry<K, V> {
 }
 
 impl<K: Hash + Eq + Clone, V> LazyEntry<K, V> {
-    fn new(parent: Arc<RwLock<HashMap<K, Arc<V>>>>, key: K) -> Self {
+    fn new(parent: Arc<RwLock<HashMap<K, (Arc<V>, Arc<AtomicBool>)>>>, key: K) -> Self {
         Self { parent, key }
     }
 
@@ -75,7 +100,9 @@ impl<K: Hash + Eq + Clone, V> LazyEntry<K, V> {
     pub fn try_insert(&self, value: V) -> Result<DetachableWeak<K, V>, V> {
         match self.parent.write().entry(self.key.clone()) {
             Entry::Occupied(_) => return Err(value),
-            Entry::Vacant(entry) => entry.insert(Arc::new(value)),
+            Entry::Vacant(entry) => {
+                let _ = entry.insert((Arc::new(value), Arc::new(AtomicBool::new(true))));
+            }
         };
         Ok(self.get().unwrap())
     }
@@ -83,10 +110,11 @@ impl<K: Hash + Eq + Clone, V> LazyEntry<K, V> {
     /// Attempt to resolve the entry to a weak reference, as returned by `DetachableMap::get`.
     /// Returns None if the key does not exist.
     pub fn get(&self) -> Option<DetachableWeak<K, V>> {
-        self.parent.read().get(&self.key).and_then(|v| {
+        self.parent.read().get(&self.key).and_then(|(v, key_ref)| {
             let map = self.parent.clone();
+            let key_ref = key_ref.clone();
             let key = self.key.clone();
-            Some(DetachableWeak::new(&v, map, key))
+            Some(DetachableWeak::new(&v, map, key, key_ref))
         })
     }
 }
@@ -95,7 +123,9 @@ impl<K: Hash + Eq + Clone, V> LazyEntry<K, V> {
 /// the map, allowing any client to expire the key in the map.  They are weak, and can be upgraded
 /// to a strong reference to the stored object.
 pub struct DetachableMap<K, V> {
-    map: Arc<RwLock<HashMap<K, Arc<V>>>>,
+    /// The map. For each key, it holds a reference to the value, and a shared bool indicating if
+    /// the paired value is still in the map.
+    map: Arc<RwLock<HashMap<K, (Arc<V>, Arc<AtomicBool>)>>>,
 }
 
 impl<K: Hash + Eq + Clone, V> Default for DetachableMap<K, V> {
@@ -113,7 +143,14 @@ impl<K: Hash + Eq + Clone, V> DetachableMap<K, V> {
     /// Inserts a new item into the map at `key`
     /// Returns a reference to the old item at `key` if one existed or None otherwise.
     pub fn insert(&mut self, key: K, value: V) -> Option<Arc<V>> {
-        self.map.write().insert(key, Arc::new(value))
+        let mut lock = self.map.write();
+        let new_pair = (Arc::new(value), Arc::new(AtomicBool::new(true)));
+        if let Some((prev_val, old_key_ref)) = lock.insert(key, new_pair) {
+            old_key_ref.store(false, Ordering::Relaxed);
+            Some(prev_val)
+        } else {
+            None
+        }
     }
 
     /// True if the map contains a value for the specified key  The key may be any borrowed form of
@@ -128,10 +165,11 @@ impl<K: Hash + Eq + Clone, V> DetachableMap<K, V> {
 
     /// Returns a detachable reference to the value at the given key, if it exists.
     pub fn get(&self, key: &K) -> Option<DetachableWeak<K, V>> {
-        self.map.read().get(key).and_then(|v| {
+        self.map.read().get(key).and_then(|(v, key_ref)| {
             let map = self.map.clone();
+            let key_ref = key_ref.clone();
             let key = key.clone();
-            Some(DetachableWeak::new(&v, map, key))
+            Some(DetachableWeak::new(&v, map, key, key_ref))
         })
     }
 
@@ -148,7 +186,7 @@ impl<K: Hash + Eq + Clone, V> DetachableMap<K, V> {
 mod test {
     use super::*;
 
-    #[derive(Default, Debug)]
+    #[derive(Default, Debug, PartialEq)]
     struct TestStruct {
         data: u32,
     }
@@ -157,7 +195,7 @@ mod test {
     fn contains_keys() {
         let mut map = DetachableMap::default();
 
-        map.insert(0, TestStruct { data: 45 });
+        assert!(map.insert(0, TestStruct { data: 45 }).is_none());
 
         assert!(map.contains_key(&0));
 
@@ -173,7 +211,7 @@ mod test {
     fn upgrade_detached() {
         let mut map = DetachableMap::default();
 
-        map.insert(0, TestStruct { data: 45 });
+        assert!(map.insert(0, TestStruct { data: 45 }).is_none());
 
         let detached = map.get(&0);
         assert!(detached.is_some());
@@ -204,6 +242,52 @@ mod test {
 
         // Detaching twice doesn't do anything (and doesn't panic)
         detached.detach();
+    }
+
+    #[test]
+    fn cant_detach_replaced_key() {
+        let mut map = DetachableMap::default();
+
+        assert!(map.insert(0, TestStruct { data: 12 }).is_none());
+
+        let detached = map.get(&0).expect("key is in");
+
+        // Replace the struct in the original map, keeping the data around.
+        let replaced = map.insert(0, TestStruct { data: 34 });
+
+        // Can still upgrade to the old data, since it's still alive.
+        assert_eq!(TestStruct { data: 12 }, *detached.upgrade().expect("data is still alive"));
+
+        // dropping the old data means upgrade fails though.
+        drop(replaced);
+
+        assert!(detached.upgrade().is_none());
+
+        // Trying to detach from an earlier generation will not remove the new data.
+        detached.detach();
+
+        let new_detached = map.get(&0).expect("key was replaced");
+
+        assert_eq!(TestStruct { data: 34 }, *new_detached.upgrade().expect("should be there"));
+
+        // Detaching from a cloned one has no effect, even if the key was re-added before we try.
+        let cloned = new_detached.clone();
+        let another = map.get(&0).expect("still there");
+
+        new_detached.detach();
+
+        assert!(map.get(&0).is_none());
+
+        // We don't replace any data, it got detached above.
+        let replaced = map.insert(0, TestStruct { data: 45 });
+        assert!(replaced.is_none());
+
+        // This doesn't remove the new data either though.
+        cloned.detach();
+        assert!(map.get(&0).is_some());
+        // Even if we didn't clone it from the same DetachedWeak.
+        another.detach();
+        assert!(map.get(&0).is_some());
     }
 
     #[test]
