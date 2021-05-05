a1005a4a6cbba5b01fb557a9e6e3b7d03b01f493
Xyan Bhatnagar <xbhatnag@google.com>
2021-05-05 19:29:17 +0000

    [component_manager] Fix work_scheduler_integration_test
    The work scheduler test assumed that there would be exactly one
    connection established to the Worker protocol. This assumption is
    incorrect.
    
    Depending on how tasks get scheduled in component manager, it is
    possible for the CapabilityReadyNotifier to attempt to open the Worker
    protocol (since it is exposed to framework).
    
    Furthermore, the Notifier uses the DESCRIBE flag to open the service
    and waits for an OnOpen event. This is a bug.
    
    Also, ServiceFS silently ignores the DESCRIBE flag when serving
    a protocol. Hence the Notifier is waiting for an OnOpen event that
    will never arrive. This keeps the channel open while no DoWork
    requests are sent on it.
    
    This is why the integration test can sometimes hang.
    
    The proposed fix is three-fold:
    1. In the integration test, spawn a task for each client connecting
       to the Worker protocol and use mpsc channels to notify the main
       task when work has been scheduled.
    2. CapabilityReadyNotifier should not use the DESCRIBE flag when
       opening exposed protocols.
    3. Add a guardrail when ServiceFS attempts to serve protocols
       with the DESCRIBE flag. This should never be supported.
    
    This CL is part 1 of the three-fold fix.
    
    Fixed: 74497
    Multiply: work_scheduler_integration_test: 1000
    Test: fx test work_scheduler_integration_test
    Change-Id: I001305ff2e4d1e193e3b9d2d7edf6175f2071520
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/525640
    Commit-Queue: Xyan Bhatnagar <xbhatnag@google.com>
    Reviewed-by: Fady Samuel <fsamuel@google.com>

--- src/sys/component_manager/tests/work_scheduler/components/bound/worker.rs
+++ src/sys/component_manager/tests/work_scheduler/components/bound/worker.rs
@@ -5,15 +5,13 @@
 use {
     fidl_fuchsia_sys2 as fsys, fuchsia_async as fasync,
     fuchsia_component::client::connect_to_protocol, fuchsia_component::server::ServiceFs,
-    futures::StreamExt,
+    futures::channel::mpsc, futures::StreamExt,
 };
-
-enum ExposedServices {
-    Worker(fsys::WorkerRequestStream),
-}
 
 #[fasync::run_singlethreaded]
 async fn main() {
+    let (tx, mut rx) = mpsc::unbounded();
+
     // Schedule some work (against ourselves)
     let work_scheduler_control = connect_to_protocol::<fsys::WorkSchedulerControlMarker>().unwrap();
     let work_scheduler = connect_to_protocol::<fsys::WorkSchedulerMarker>().unwrap();
@@ -36,15 +34,32 @@ async fn main() {
         .expect("connection error scheduling work item")
         .expect("error scheduling work item");
 
-    // Serve the worker protocol and receive the scheduled work
-    let mut fs = ServiceFs::new_local();
-    fs.dir("svc").add_fidl_service(ExposedServices::Worker);
+    let mut fs = ServiceFs::new();
+    fs.dir("svc").add_fidl_service(move |mut stream: fsys::WorkerRequestStream| {
+        let tx = tx.clone();
+        fasync::Task::spawn(async move {
+            // Get the next work request
+            while let Some(Ok(fsys::WorkerRequest::DoWork { work_id, responder })) =
+                stream.next().await
+            {
+                // Send the work request to the main task
+                let _ = tx.unbounded_send(work_id);
+
+                // Respond gracefully to the client
+                responder.send(&mut Ok(())).unwrap();
+            }
+        })
+        .detach();
+    });
     fs.take_and_serve_directory_handle().expect("failed to serve outgoing directory");
 
-    let ExposedServices::Worker(mut stream) = fs.next().await.unwrap();
+    // Serve the outgoing directory
+    fasync::Task::spawn(async move {
+        fs.collect::<()>().await;
+    })
+    .detach();
 
-    let fsys::WorkerRequest::DoWork { work_id, responder } = stream.next().await.unwrap().unwrap();
-
+    // Wait for the first work request
+    let work_id = rx.next().await.unwrap();
     assert_eq!(work_id, "TEST");
-    responder.send(&mut Ok(())).unwrap();
 }
