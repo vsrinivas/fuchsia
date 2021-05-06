00547ef86b6cb87bae2aad831d4fe51a23e70681
Benjamin Wright <benwright@google.com>
2021-05-06 01:41:00 +0000

    [scrutiny] Clean up comments in json file.
    This simple change removes unused commented structures in the json file
    to keep the code base a bit cleaner.
    
    Change-Id: Ibcd1c8cea25408017fcc259bd31ecdca246d9dd4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/525781
    Fuchsia-Auto-Submit: Benjamin Wright <benwright@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Laura Peskin <pesk@google.com>

--- src/security/scrutiny/lib/plugins/src/core/util/jsons.rs
+++ src/security/scrutiny/lib/plugins/src/core/util/jsons.rs
@@ -8,32 +8,14 @@ use {
     std::collections::HashMap,
 };
 
-/// This file contains types that encapsulate JSON objects to be parsed by serde_json.
-/// Currently unused fields have been commented out, but should be able to be uncommented
-/// as they come into use.
-
-/* targets.json */
-
 /// JSON structure of the content returned by hitting the package server for targets.json
 /// ie: http://0.0.0.0:8083/targets.json
 ///
 /// Path: /targets.json
 #[derive(Deserialize)]
 pub struct TargetsJson {
-    //pub signatures: Vec<Signatures>,
     pub signed: Signed,
 }
-
-/// JSON structure of the `signatures` field in targets.json.
-/// Contains the signature for verification.
-///
-/// Path: /targets.json -> signatures[]
-// #[derive(Deserialize)]
-// pub struct Signatures {
-//     pub keyid: String,
-//     pub method: String,
-//     pub sig: String,
-// }
 
 /// JSON structure of the `signed` field in targets.json.
 /// Contains information about target packages.
@@ -41,11 +23,7 @@ pub struct TargetsJson {
 /// Path: /targets.json -> signed
 #[derive(Deserialize)]
 pub struct Signed {
-    //pub typ: String,
-    //pub expires: String,
-    //pub spec_version: String,
     pub targets: HashMap<String, FarPackageDefinition>,
-    //pub version: usize,
 }
 
 /// JSON structure containing information about the FAR packages provided by the package server.
@@ -54,8 +32,6 @@ pub struct Signed {
 #[derive(Deserialize)]
 pub struct FarPackageDefinition {
     pub custom: Custom,
-    //pub hashes: HashMap<String, String>,
-    //pub length: usize,
 }
 
 /// JSON structure of the custom field in a FAR package definition.
@@ -64,10 +40,7 @@ pub struct FarPackageDefinition {
 #[derive(Deserialize)]
 pub struct Custom {
     pub merkle: String,
-    //pub size: usize,
 }
-
-/* service package definition retrieved via package server */
 
 /// JSON structure of a blob retrieved from package server via merkle hash that
 /// defines a service package.
@@ -92,30 +65,13 @@ pub struct Manifest {
     pub sandbox: Sandbox,
 }
 
-/* cmx file format read via far reader */
-
 /// JSON structure of a cmx file contained in a FAR archive, read via a FAR reader.
 ///
 /// Path: //far_file
 #[derive(Deserialize, Serialize)]
 pub struct CmxJson {
-    //pub program: Option<Program>,
     pub sandbox: Option<Sandbox>,
-    //pub runner: Option<String>,
 }
-
-/// JSON structure of the program block in a cmx file.
-/// In the case where //far_file -> runner is not present, this will be a string pointing to
-/// the binary to run the program.
-/// In the case where //far_file -> runner is present, this will be a string-string map
-/// interpreted as args to be passed to the runner.
-/// https://fuchsia.googlesource.com/docs/+/7291604af051af6c62259df2ceca2a6f3d958ba1/the-book/package_metadata.md#program
-///
-/// Path: //far_file -> program
-// #[derive(Deserialize)]
-// pub struct Program {
-//     pub binary: Option<String>,
-// }
 
 /// JSON structure of the sandbox defined by the cmx file defining a component.
 ///
