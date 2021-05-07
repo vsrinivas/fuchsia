76a9a5f599d434c477606fce6942d9e3a2a12dff
Peter Johnston <peterjohnston@google.com>
2021-05-07 19:38:55 +0000

    [testing] Add proc macro to help with test fixtures
    The #[fixture] proc macro takes one argument, which is the identifier of
    a wrapper function.
    
    The wrapper function takes two arguments: the name of the test function,
    to be used or ignored however it wants, and a Fn of a signature matching
    the input to the proc macro (the test body). The helper is responsible
    for doing any necessary setup, and passing inputs to the test body
    function. The macro essentially stuffs the test body into a closure,
    which is passed to the test wrapper function along with the name of the
    test.
    
    For example, this test case:
    ```
    async fn my_test() {
        let (proxy, service_fut) = setup_my_service("my_test");
        let ((), ()) = futures::future::join(service_fut, || async move {
            // test body using `proxy`
        }).await;
    }
    ```
    
    can be rewritten as:
    ```
    async fn my_test(proxy: MyServiceProxy) {
        // test body using `proxy`
    }
    ```
    
    with this helper:
    ```
    async fn setup_my_service<F, Fut>(test_name: &str, test: F)
    where
        F: FnOnce(MyServiceProxy) -> impl futures::Future<Output = ()>,
    {
        let (proxy, fut) = setup_my_service(test_name);
        let ((), ()) = futures::future::join(fut, test(proxy)).await;
    }
    ```
    
    Bug: 75547
    Test: fx test -d fixture-tests
    Change-Id: Ib657adc5a70ab37b3dfafc90f204e1ca06e7a388
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/521042
    Reviewed-by: James Robinson <jamesr@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Peter Johnston <peterjohnston@google.com>

---
+++ src/lib/testing/fixture/src/lib.rs
@@ -0,0 +1,126 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! This crate provides an attribute macro to help instrument test functions
+//! with setup logic.
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::spanned::Spanned as _;
+
+fn fixture_inner(wrapper_fn: syn::Ident, input: syn::ItemFn) -> Result<TokenStream, TokenStream> {
+    let syn::ItemFn { attrs, sig, block, vis: _ } = input;
+    let syn::Signature {
+        ident: test_name,
+        inputs,
+        output,
+        asyncness,
+        constness,
+        generics,
+        unsafety: _,
+        abi: _,
+        fn_token: _,
+        paren_token: _,
+        variadic: _,
+    } = sig;
+    if let Some(constness) = constness {
+        return Err(syn::Error::new(constness.span(), "test function cannot be const")
+            .to_compile_error()
+            .into());
+    }
+    let syn::Generics { lt_token: _, params, gt_token: _, where_clause: _ } = &generics;
+    if !params.is_empty() {
+        return Err(syn::Error::new(generics.span(), "test function cannot be generic")
+            .to_compile_error()
+            .into());
+    }
+    let (maybe_move, maybe_await) =
+        if asyncness.is_some() { (Some(quote!(move)), Some(quote!(.await))) } else { (None, None) };
+
+    let args = inputs
+        .into_iter()
+        .map(|input| match input {
+            syn::FnArg::Receiver(receiver) => {
+                Err(syn::Error::new(receiver.span(), "test function signature cannot contain self")
+                    .to_compile_error()
+                    .into())
+            }
+            syn::FnArg::Typed(arg) => Ok(arg),
+        })
+        .collect::<Result<Vec<_>, TokenStream>>()?;
+
+    // TODO(https://fxbug.dev/76111): make passing `test_name` to the wrapper function an optional
+    // parameter on the #[fixture] macro.
+    let result = quote! {
+        #(#attrs)*
+        #asyncness fn #test_name () #output {
+            #wrapper_fn (stringify!(#test_name), |#( #args ),*| #asyncness #maybe_move
+                #block
+            ) #maybe_await
+        }
+    };
+    Ok(result.into())
+}
+
+/// Wraps the body of a test `fn` in a closure, and passes it to a specified
+/// helper `fn` to be run by the helper with any relevant setup and inputs that
+/// it requires.
+///
+/// Example:
+///
+/// ```
+/// # struct Foo;
+///
+/// async fn setup<F, Fut>(test_name: &str, test: F)
+/// where
+///     F: FnOnce(Foo) -> Fut,
+///     Fut: std::future::Future<Output = ()>,
+/// {
+///     // setup foo
+/// #   let foo = Foo{};
+///     test(foo).await
+/// }
+///
+/// #[fixture::fixture(setup)]
+/// async fn test_foo(input: Foo) {
+///     // test body using `input`
+/// }
+/// ```
+///
+/// Expands to:
+///
+/// ```
+/// # struct Foo;
+///
+/// async fn setup<F, Fut>(test_name: &str, test: F)
+/// # where
+/// #     F: FnOnce(Foo) -> Fut,
+/// #     Fut: std::future::Future<Output = ()>,
+/// # {
+/// #     // setup foo
+/// #     let foo = Foo{};
+/// #     test(foo).await
+/// # }
+///
+/// async fn test_foo() {
+///     setup("test_foo", |input| async move {
+///         // test body using `input`
+///     }).await
+/// }
+/// ```
+///
+/// This macro supports async as well as non-async functions, and any number of
+/// inputs to the test body. For a non-async test, the wrapper function could
+/// have a signature like this:
+///
+/// fn setup(test_name: &str, test: impl FnOnce()) { /* ... */}
+#[proc_macro_attribute]
+pub fn fixture(attrs: TokenStream, input: TokenStream) -> TokenStream {
+    let wrapper_fn = syn::parse_macro_input!(attrs as syn::Ident);
+    let input = syn::parse_macro_input!(input as syn::ItemFn);
+    match fixture_inner(wrapper_fn, input) {
+        Ok(token_stream) => token_stream,
+        Err(token_stream) => token_stream,
+    }
+}
---
+++ src/lib/testing/fixture/src/tests.rs
@@ -0,0 +1,70 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[cfg(test)]
+mod tests {
+    use fixture::fixture;
+    use futures::{SinkExt as _, StreamExt as _};
+
+    fn non_async_setup(test_name: &str, test: impl FnOnce(&str)) {
+        test(test_name)
+    }
+
+    #[fixture(non_async_setup)]
+    #[test]
+    fn non_async_test(test_name: &str) {
+        assert_eq!(test_name, "non_async_test");
+    }
+
+    fn no_args_setup(_test_name: &str, test: impl FnOnce()) {
+        test()
+    }
+
+    #[fixture(no_args_setup)]
+    #[test]
+    fn no_args_test() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    async fn async_setup<F, Fut>(_test_name: &str, test: F)
+    where
+        F: FnOnce(futures::channel::mpsc::Sender<()>) -> Fut,
+        Fut: futures::Future<Output = ()>,
+    {
+        let (tx, mut rx) = futures::channel::mpsc::channel(0);
+        let rx_fut = async {
+            assert_eq!(rx.next().await.unwrap(), ());
+        };
+        let ((), ()) = futures::future::join(rx_fut, test(tx)).await;
+    }
+
+    #[fixture(async_setup)]
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn async_test(mut tx: futures::channel::mpsc::Sender<()>) {
+        let () = tx.send(()).await.unwrap();
+    }
+
+    #[derive(Default, Debug, PartialEq)]
+    struct Foo;
+
+    fn setup_ref_input(_test_name: &str, test: impl FnOnce(Foo)) {
+        test(Foo::default())
+    }
+
+    #[fixture(setup_ref_input)]
+    #[test]
+    fn test_ref_input(ref foo: Foo) {
+        assert_eq!(foo, &Foo::default());
+    }
+
+    fn generic_setup<T: Default>(_test_name: &str, test: impl FnOnce(T)) {
+        test(T::default())
+    }
+
+    #[fixture(generic_setup)]
+    #[test]
+    fn generic_test(foo: Foo) {
+        assert_eq!(foo, Foo::default());
+    }
+}
