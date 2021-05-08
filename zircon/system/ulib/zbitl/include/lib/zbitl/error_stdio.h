5c101dc19b645c57a419f5c3de067cdbbc684a59
Roland McGrath <mcgrathr@google.com>
2021-05-08 20:28:38 +0000

    [zbitl] Use <inttypes.h> macros
    Don't assume uint32_t matches %u.  Use PRIu32 as the spec requires.
    
    Change-Id: I9cc5ba635cd2080f158d87aabe71e0d21f0ea5e3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/527062
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

--- zircon/system/ulib/zbitl/include/lib/zbitl/error_stdio.h
+++ zircon/system/ulib/zbitl/include/lib/zbitl/error_stdio.h
@@ -5,6 +5,7 @@
 #ifndef LIB_ZBITL_ERROR_STDIO_H_
 #define LIB_ZBITL_ERROR_STDIO_H_
 
+#include <inttypes.h>
 #include <stdio.h>
 
 #include <string_view>
@@ -17,8 +18,8 @@ namespace zbitl {
 // Prints an error message from a View `Error` value.
 template <typename ViewError>
 void PrintViewError(const ViewError& error, FILE* f = stdout) {
-  fprintf(f, "%.*s at offset %u", static_cast<int>(error.zbi_error.size()), error.zbi_error.data(),
-          error.item_offset);
+  fprintf(f, "%.*s at offset %" PRIu32, static_cast<int>(error.zbi_error.size()),
+          error.zbi_error.data(), error.item_offset);
   if (error.storage_error) {
     auto storage_error = ViewError::storage_error_string(error.storage_error.value());
     std::string_view storage_error_sv = storage_error;
@@ -34,12 +35,12 @@ void PrintViewCopyError(const ViewCopyError& error, FILE* f = stdout) {
   if (error.read_error) {
     auto read_error = ViewCopyError::read_error_string(error.read_error.value());
     std::string_view read_error_sv = read_error;
-    fprintf(f, ": read error at source offset %u: %.*s", error.read_offset,
+    fprintf(f, ": read error at source offset %" PRIu32 ": %.*s", error.read_offset,
             static_cast<int>(read_error_sv.size()), read_error_sv.data());
   } else if (error.write_error) {
     auto write_error = ViewCopyError::write_error_string(error.write_error.value());
     std::string_view write_error_sv = write_error;
-    fprintf(f, ": write error at destination offset %u: %.*s", error.write_offset,
+    fprintf(f, ": write error at destination offset %" PRIu32 ": %.*s", error.write_offset,
             static_cast<int>(write_error_sv.size()), write_error_sv.data());
   }
   fprintf(f, "\n");  // To flush the buffer.
