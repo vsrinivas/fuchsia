82c4a2a1f5d2619068885484e7b40a9a4e60cc4c
Joshua Seaton <joshuaseaton@google.com>
2021-05-13 01:36:18 +0000

    [stdcompat] Introduce cpp20::(starts|ends)_with()
    Change-Id: If0fc37a2f10e2a32cd8504adce2e8a6d66e148a5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/525741
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    API-Review: Roland McGrath <mcgrathr@google.com>

--- sdk/lib/stdcompat/include/lib/stdcompat/string_view.h
+++ sdk/lib/stdcompat/include/lib/stdcompat/string_view.h
@@ -528,6 +528,64 @@ std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>&
 }  // namespace std
 #endif  // __cpp_lib_string_view >= 201606L && !defined(LIB_STDCOMPAT_USE_POLYFILLS)
 
+// Per the README, we define standalone cpp20::starts_with() and
+// cpp20::ends_with() functions. These correspond to std::basic_string_view
+// methods introduced in C++20. For parity's sake, in the C++20 context we also
+// define the same functions, though as thin wrappers around these methods.
+namespace cpp20 {
+
+#if __cpp_lib_string_view >= 202002L && !defined(LIB_STDCOMPAT_USE_POLYFILLS)
+
+template <class CharT, class Traits = std::char_traits<CharT>, typename PrefixType>
+constexpr bool starts_with(cpp17::basic_string_view<CharT, Traits> s,
+                           std::decay_t<PrefixType> prefix) {
+  return s.starts_with(prefix);
+}
+
+template <class CharT, class Traits = std::char_traits<CharT>, typename SuffixType>
+constexpr bool ends_with(cpp17::basic_string_view<CharT, Traits> s,
+                         std::decay_t<SuffixType> suffix) {
+  return s.ends_with(suffix);
+}
+
+#else  // Polyfills for C++20 std::basic_string_view methods.
+
+template <class CharT, class Traits = std::char_traits<CharT>>
+constexpr bool starts_with(cpp17::basic_string_view<CharT, Traits> s,
+                           cpp17::basic_string_view<CharT, Traits> prefix) {
+  return s.substr(0, prefix.size()) == prefix;
+}
+
+template <class CharT, class Traits = std::char_traits<CharT>>
+constexpr bool starts_with(cpp17::basic_string_view<CharT, Traits> s, const CharT* prefix) {
+  return starts_with(s, decltype(s){prefix});
+}
+
+template <class CharT, class Traits = std::char_traits<CharT>>
+constexpr bool starts_with(cpp17::basic_string_view<CharT, Traits> s, CharT c) {
+  return !s.empty() && Traits::eq(s.front(), c);
+}
+
+template <class CharT, class Traits = std::char_traits<CharT>>
+constexpr bool ends_with(cpp17::basic_string_view<CharT, Traits> s,
+                         cpp17::basic_string_view<CharT, Traits> suffix) {
+  return s.size() >= suffix.size() && s.substr(s.size() - suffix.size(), suffix.size()) == suffix;
+}
+
+template <class CharT, class Traits = std::char_traits<CharT>>
+constexpr bool ends_with(cpp17::basic_string_view<CharT, Traits> s, const CharT* suffix) {
+  return ends_with(s, decltype(s){suffix});
+}
+
+template <class CharT, class Traits = std::char_traits<CharT>>
+constexpr bool ends_with(cpp17::basic_string_view<CharT, Traits> s, CharT c) {
+  return !s.empty() && Traits::eq(s.back(), c);
+}
+
+#endif  // if __cpp_lib_string_view >= 202002L && !defined(LIB_STDCOMPAT_USE_POLYFILLS)
+
+}  // namespace cpp20
+
 namespace cpp17 {
 // Constructs a string_view from ""_sv literal.
 // Literals with no leading underscore are reserved for the standard library.
--- sdk/lib/stdcompat/test/string_view_test.cc
+++ sdk/lib/stdcompat/test/string_view_test.cc
@@ -813,6 +813,76 @@ TEST(StringViewTest, FindLastNotOfReturnsNposWhenNoMatch) {
   EXPECT_EQ(cpp17::string_view::npos, kString.find_last_not_of('G'));
 }
 
+TEST(StringViewTest, StartsWith) {
+  constexpr cpp17::string_view kString = "ABCdef";
+
+  // By convention, a string view always "starts" with an empty or NUL string.
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{}));
+  EXPECT_TRUE(cpp20::starts_with(kString, ""));
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{""}));
+
+  EXPECT_TRUE(cpp20::starts_with(kString, 'A'));
+  EXPECT_FALSE(cpp20::starts_with(kString, 'B'));
+  EXPECT_FALSE(cpp20::starts_with(kString, 'f'));
+
+  EXPECT_TRUE(cpp20::starts_with(kString, "A"));
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{"A"}));
+  EXPECT_TRUE(cpp20::starts_with(kString, "AB"));
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{"AB"}));
+  EXPECT_TRUE(cpp20::starts_with(kString, "ABC"));
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{"ABC"}));
+  EXPECT_TRUE(cpp20::starts_with(kString, "ABCd"));
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{"ABCd"}));
+  EXPECT_TRUE(cpp20::starts_with(kString, "ABCde"));
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{"ABCde"}));
+
+  // A string view should start with itself.
+  EXPECT_TRUE(cpp20::starts_with(kString, "ABCdef"));
+  EXPECT_TRUE(cpp20::starts_with(kString, kString));
+  EXPECT_TRUE(cpp20::starts_with(kString, "ABCdef\0"));
+  EXPECT_TRUE(cpp20::starts_with(kString, cpp17::string_view{"ABCdef\0"}));
+
+  EXPECT_FALSE(cpp20::starts_with(kString, "rAnDoM"));
+  EXPECT_FALSE(cpp20::starts_with(kString, cpp17::string_view{"rAnDoM"}));
+  EXPECT_FALSE(cpp20::starts_with(kString, "longer than kString"));
+  EXPECT_FALSE(cpp20::starts_with(kString, cpp17::string_view{"longer than kString"}));
+}
+
+TEST(StringViewTest, EndsWith) {
+  constexpr cpp17::string_view kString = "ABCdef";
+
+  // By convention, a string view always "ends" with an empty or NUL string.
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{}));
+  EXPECT_TRUE(cpp20::ends_with(kString, ""));
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{""}));
+
+  EXPECT_TRUE(cpp20::ends_with(kString, 'f'));
+  EXPECT_FALSE(cpp20::ends_with(kString, 'e'));
+  EXPECT_FALSE(cpp20::ends_with(kString, 'A'));
+
+  EXPECT_TRUE(cpp20::ends_with(kString, "f"));
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{"f"}));
+  EXPECT_TRUE(cpp20::ends_with(kString, "ef"));
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{"ef"}));
+  EXPECT_TRUE(cpp20::ends_with(kString, "def"));
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{"def"}));
+  EXPECT_TRUE(cpp20::ends_with(kString, "Cdef"));
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{"Cdef"}));
+  EXPECT_TRUE(cpp20::ends_with(kString, "BCdef"));
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{"BCdef"}));
+
+  // A string view should end with itself.
+  EXPECT_TRUE(cpp20::ends_with(kString, "ABCdef"));
+  EXPECT_TRUE(cpp20::ends_with(kString, kString));
+  EXPECT_TRUE(cpp20::ends_with(kString, "ABCdef\0"));
+  EXPECT_TRUE(cpp20::ends_with(kString, cpp17::string_view{"ABCdef\0"}));
+
+  EXPECT_FALSE(cpp20::ends_with(kString, "rAnDoM"));
+  EXPECT_FALSE(cpp20::ends_with(kString, cpp17::string_view{"rAnDoM"}));
+  EXPECT_FALSE(cpp20::ends_with(kString, "longer than kString"));
+  EXPECT_FALSE(cpp20::ends_with(kString, cpp17::string_view{"longer than kString"}));
+}
+
 TEST(StringViewTest, HashVaidation) {
   constexpr cpp17::string_view kStringView1 = "1234";
   constexpr cpp17::string_view kStringView2 = "123456";
