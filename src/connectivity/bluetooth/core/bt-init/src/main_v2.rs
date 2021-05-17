7c73af8077017d2c56d23dc8bd53359714ff686c
Lucas Jenkins <lucasjenkins@google.com>
2021-05-17 20:49:53 +0000

    [bt-init] Define temporary v2 bt-init component.
    Define a manifest and separate binary for a v2 bt-init component. To
    support the optional presence of bt-rfcomm, use CF APIs to attempt to
    open the Profile service of the bt-rfcomm child component, falling back
    to bt-gap's Profile service if opening bt-rfcomm fails.
    
    The manifest's child URLs are absolute paths to production components,
    which will not allow them to be packaged with bt-init for hermetic
    integration testing. As such, this manifest will never be used as-is.
    We will convert it to a CML shard which parameterizes its children (see
    fxbug.dev/74547). It exists in this CL solely for making the review
    process simpler.
    
    Bug: 72282
    Test: `fx test bt-init-unittests-v2` to verify logic handling optional
          RFCOMM presence and maintain unit test coverage from v1. TODO -
          this is a step towards unblocking integration test coverage in
          CQ (fxbug.dev/53915), so this component will be thoroughly
          integration-tested before it is ever used in production. Also, we
          will write a manifest smoke test for this manifest once converted
          to a CML shard.
    Change-Id: Id6f08ef192c0e0cf75668fef29ad9c4ddb415449
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/504726
    Commit-Queue: Lucas Jenkins <lucasjenkins@google.com>
    Reviewed-by: Ani Ramakrishnan <aniramakri@google.com>
    Reviewed-by: Justin Mattson <jmatt@google.com>

---
+++ src/connectivity/bluetooth/core/bt-init/src/main_v2.rs
@@ -0,0 +1,206 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{Context as _, Error},
+    async_trait::async_trait,
+    fdio,
+    fidl::endpoints::{DiscoverableService, Proxy},
+    fidl_fuchsia_bluetooth_bredr::ProfileMarker,
+    fidl_fuchsia_bluetooth_snoop::SnoopMarker,
+    fidl_fuchsia_io::DirectoryProxy,
+    fuchsia_async as fasync,
+    fuchsia_component::{client, server},
+    futures::{future, StreamExt},
+    log::{error, info, warn},
+};
+
+mod config;
+
+const BT_GAP_CHILD_NAME: &str = "bt-gap";
+const BT_RFCOMM_CHILD_NAME: &str = "bt-rfcomm";
+
+#[async_trait]
+trait ComponentClientAdapter {
+    async fn open_childs_exposed_directory(
+        &mut self,
+        child_name: String,
+    ) -> Result<DirectoryProxy, Error>;
+}
+
+struct ComponentClient;
+
+#[async_trait]
+impl ComponentClientAdapter for ComponentClient {
+    async fn open_childs_exposed_directory(
+        &mut self,
+        child_name: String,
+    ) -> Result<DirectoryProxy, Error> {
+        client::open_childs_exposed_directory(child_name, None).await
+    }
+}
+
+/// Open the directory of the child which will underlie the Profile service. bt-rfcomm is the
+/// preferred Profile service provider if present, but if it is not we fall back to bt-gap.
+//
+// TODO(fxbug.dev/71315): A single bt-rfcomm instance won't function correctly in the presence
+// of multiple bt-host devices during its lifetime. When handling this is a priority, we will
+// likely need to either launch an instance of bt-rfcomm per-bt-host (e.g. inside bt-gap), or
+// modify bt-rfcomm to accommodate this issue.
+async fn open_childs_service_directory<C: ComponentClientAdapter>(
+    component_client: &mut C,
+) -> Result<DirectoryProxy, Error> {
+    let underlying_profile_svc =
+        component_client.open_childs_exposed_directory(BT_RFCOMM_CHILD_NAME.to_owned()).await;
+    match underlying_profile_svc {
+        Err(e) => {
+            warn!(
+                "failed to bind bt-rfcomm child ({:?}), falling back to bt-gap's Profile service",
+                e
+            );
+            component_client.open_childs_exposed_directory(BT_GAP_CHILD_NAME.to_owned()).await
+        }
+        dir => {
+            info!("successfully opened bt-rfcomm svc directory");
+            dir
+        }
+    }
+}
+
+#[fuchsia::component(logging = true)]
+fn main() -> Result<(), Error> {
+    info!("starting bt-init...");
+
+    let mut executor = fasync::LocalExecutor::new().context("error creating executor")?;
+    let cfg = config::Config::load().context("error loading config")?;
+
+    // Start bt-snoop service before anything else and hold onto the connection until bt-init exits.
+    let snoop_connection;
+    if cfg.autostart_snoop() {
+        info!("starting snoop service...");
+        snoop_connection = client::connect_to_protocol::<SnoopMarker>();
+        if let Err(e) = snoop_connection {
+            warn!("failed to start snoop service: {}", e);
+        } else {
+            info!("snoop service started successfully");
+        }
+    }
+
+    let run_bluetooth = async move {
+        let underlying_profile_svc = open_childs_service_directory(&mut ComponentClient).await?;
+        let mut fs = server::ServiceFs::new();
+        fs.dir("svc").add_service_at(ProfileMarker::SERVICE_NAME, |chan| {
+            Some((ProfileMarker::SERVICE_NAME, chan))
+        });
+        fs.take_and_serve_directory_handle()?;
+
+        info!("initialization complete, begin serving {}", ProfileMarker::SERVICE_NAME);
+        let outer_fs = fs.for_each(move |(name, chan)| {
+            if name == ProfileMarker::SERVICE_NAME {
+                error!(
+                    "Received unexpected service {} when we only expect to serve {}",
+                    name,
+                    ProfileMarker::SERVICE_NAME
+                );
+                return future::ready(());
+            }
+            let _ =
+                fdio::service_connect_at(underlying_profile_svc.as_channel().as_ref(), name, chan)
+                    .map_err(|e| warn!("error passing {} handle to service: {:?}", name, e));
+            future::ready(())
+        });
+        Ok::<(), Error>(outer_fs.await)
+    };
+
+    executor
+        .run_singlethreaded(run_bluetooth)
+        .context("bt-init encountered an error during execution")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use {
+        anyhow::format_err, fuchsia_async::Channel as AsyncChannel, fuchsia_zircon as zx,
+        std::collections::HashSet,
+    };
+    struct MockComponentClient {
+        pub children_to_fail_for: HashSet<String>,
+        pub bt_gap_channel: Option<zx::Channel>,
+        pub bt_rfcomm_channel: Option<zx::Channel>,
+    }
+
+    impl MockComponentClient {
+        fn new() -> Self {
+            Self {
+                children_to_fail_for: HashSet::new(),
+                bt_gap_channel: None,
+                bt_rfcomm_channel: None,
+            }
+        }
+    }
+
+    #[async_trait]
+    impl ComponentClientAdapter for MockComponentClient {
+        async fn open_childs_exposed_directory(
+            &mut self,
+            child_name: String,
+        ) -> Result<DirectoryProxy, Error> {
+            if self.children_to_fail_for.contains(&child_name) {
+                return Err(format_err!("couldn't open {}'s directory :/", &child_name));
+            }
+            let (local, client) = zx::Channel::create()?;
+            match child_name.as_str() {
+                BT_RFCOMM_CHILD_NAME => self.bt_rfcomm_channel = Some(local),
+                BT_GAP_CHILD_NAME => self.bt_gap_channel = Some(local),
+                _ => panic!("MockComponentClient received unexpected child name: {}", child_name),
+            }
+            Ok(DirectoryProxy::from_channel(AsyncChannel::from_channel(client).unwrap()))
+        }
+    }
+
+    fn assert_channels_connected(writer: &zx::Channel, reader: &zx::Channel) {
+        let expected_bytes = [1, 2, 3, 4, 5];
+        writer.write(&expected_bytes, &mut []).unwrap();
+        let mut bytes = zx::MessageBuf::new();
+        reader.read(&mut bytes).unwrap();
+        assert_eq!(&expected_bytes, bytes.bytes());
+    }
+
+    #[fuchsia::test]
+    async fn test_open_rfcomm_works() {
+        let mut mock_client = MockComponentClient::new();
+
+        // If opening bt-rfcomm's directory works, the directory should be connected to bt-rfcomm.
+        let profile_svc = open_childs_service_directory(&mut mock_client).await.unwrap();
+        assert!(mock_client.bt_rfcomm_channel.is_some());
+        assert_channels_connected(
+            mock_client.bt_rfcomm_channel.unwrap().as_ref(),
+            profile_svc.as_channel().as_ref(),
+        );
+    }
+
+    #[fuchsia::test]
+    async fn test_open_rfcomm_fails() {
+        // If opening bt-rfcomm's directory fails, the directory should be connected to bt-gap.
+        let mut mock_client = MockComponentClient::new();
+        mock_client.children_to_fail_for.insert(BT_RFCOMM_CHILD_NAME.to_owned());
+        let profile_svc = open_childs_service_directory(&mut mock_client).await.unwrap();
+        assert!(mock_client.bt_rfcomm_channel.is_none());
+        assert!(mock_client.bt_gap_channel.is_some());
+        assert_channels_connected(
+            mock_client.bt_gap_channel.unwrap().as_ref(),
+            profile_svc.as_channel().as_ref(),
+        );
+    }
+
+    #[fuchsia::test]
+    async fn test_open_rfcomm_and_gap_fail() {
+        // If opening both bt-gap and bt-rfcomm's directory fail, opening the profile service should fail.
+        let mut mock_client = MockComponentClient::new();
+        mock_client.children_to_fail_for.insert(BT_RFCOMM_CHILD_NAME.to_owned());
+        mock_client.children_to_fail_for.insert(BT_GAP_CHILD_NAME.to_owned());
+        assert!(open_childs_service_directory(&mut mock_client).await.is_err());
+    }
+}
