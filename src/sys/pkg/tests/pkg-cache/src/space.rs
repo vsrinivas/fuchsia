bb677926a53295803bf2ffdcf5b7ae1be050c2e9
Devon H. O'Dell <dhobsd@google.com>
2021-05-17 23:03:39 +0000

    [pkg-cache-integration-tests] Remove pkgfs tests
    The pkgfs tests existed to demonstrate parity between GC implementations
    and are otherwise not useful. However, fxrev.dev/514646 implemented and
    replaced pkgfs-for-gc in the same CL, making it important to have these
    tests in git history somewhere. Here, we remove these tests since they
    do not exercise an active system.
    
    Bug: 66888
    Change-Id: Iaf5e8e57e553569c7a2544eb08eacd04815f0aa5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/530883
    Reviewed-by: Dan Johnson <computerdruid@google.com>
    Commit-Queue: Devon H. O'Dell <dhobsd@google.com>
    Fuchsia-Auto-Submit: Devon H. O'Dell <dhobsd@google.com>

--- src/sys/pkg/tests/pkg-cache/src/space.rs
+++ src/sys/pkg/tests/pkg-cache/src/space.rs
@@ -218,25 +218,7 @@ async fn setup_test_env(
 /// Assert that performing a GC does nothing on a blobfs that only includes the system image and
 /// static packages.
 #[fasync::run_singlethreaded(test)]
-async fn gc_noop_system_image_pkgfs() {
-    let static_package = PackageBuilder::new("static-package")
-        .add_resource_at("resource", &[][..])
-        .build()
-        .await
-        .unwrap();
-    let (env, _) = setup_test_env(None, &[&static_package]).await;
-    let original_blobs = env.blobfs().list_blobs().expect("to get an initial list of blobs");
-    let proxy = env.pkgfs.root_dir_proxy().expect("to get a handle to pkgfs");
-    let client =
-        pkgfs::control::Client::open_from_pkgfs_root(&proxy).expect("to get a control client");
-    client.gc().await.expect("to perform a gc without error");
-    assert_eq!(env.blobfs().list_blobs().expect("to get new blobs"), original_blobs);
-}
-
-/// Assert that performing a GC does nothing on a blobfs that only includes the system image and
-/// static packages.
-#[fasync::run_singlethreaded(test)]
-async fn gc_noop_system_image_pkg_cache() {
+async fn gc_noop_system_image() {
     let static_package = PackageBuilder::new("static-package")
         .add_resource_at("resource", &[][..])
         .build()
@@ -253,114 +235,7 @@ async fn gc_noop_system_image_pkg_cache() {
 /// Furthermore, ensure that an incomplete package does not lose blobs, and that the previous
 /// packages' blobs survive until the new package is entirely written.
 #[fasync::run_singlethreaded(test)]
-async fn gc_dynamic_index_protected_pkgfs() {
-    let (env, sysimg_pkg) = setup_test_env(None, &[]).await;
-
-    let pkg = PackageBuilder::new("gc_dynamic_index_protected_pkgfs")
-        .add_resource_at("bin/x", "bin-x-version-1".as_bytes())
-        .add_resource_at("data/unchanging", "unchanging-content".as_bytes())
-        .build()
-        .await
-        .unwrap();
-    do_fetch(&env.proxies.package_cache, &pkg).await;
-
-    // Ensure that the just-fetched blobs are not reaped by a GC cycle.
-    let mut test_blobs = env.blobfs().list_blobs().expect("to get an initial list of blobs");
-
-    let proxy = env.pkgfs.root_dir_proxy().expect("to get a handle to pkgfs");
-    let client =
-        pkgfs::control::Client::open_from_pkgfs_root(&proxy).expect("to get a control client");
-    client.gc().await.expect("to perform a gc without error");
-    assert_eq!(env.blobfs().list_blobs().expect("to get new blobs"), test_blobs);
-
-    // Fetch an updated package, skipping both its content blobs to guarantee that there are
-    // missing blobs. This helps us ensure that the meta.far is not lost.
-    let pkgprime = PackageBuilder::new("gc_dynamic_index_protected_pkgfs")
-        .add_resource_at("bin/x", "bin-x-version-2".as_bytes())
-        .add_resource_at("bin/y", "bin-y-version-1".as_bytes())
-        .add_resource_at("data/unchanging", "unchanging-content".as_bytes())
-        .build()
-        .await
-        .unwrap();
-    // pkgfs would let us call do_fetch here, but pkg-cache differs from pkgfs in that the
-    // NeededBlobs protocol can be "canceled". This means that if the channel is closed before the
-    // protocol is completed, the blobs mentioned in the meta far are no longer protected by the
-    // dynamic index. That's WAI, but complicating that interface further isn't worth it.
-    //
-    // Here, we persist the meta.far
-    let mut meta_blob_info =
-        BlobInfo { blob_id: BlobId::from(*pkgprime.meta_far_merkle_root()).into(), length: 0 };
-    let package_cache = &env.proxies.package_cache;
-
-    let (needed_blobs, needed_blobs_server_end) =
-        fidl::endpoints::create_proxy::<NeededBlobsMarker>().unwrap();
-    let (dir, dir_server_end) = fidl::endpoints::create_proxy::<DirectoryMarker>().unwrap();
-    let get_fut = package_cache
-        .get(
-            &mut meta_blob_info,
-            &mut std::iter::empty(),
-            needed_blobs_server_end,
-            Some(dir_server_end),
-        )
-        .map_ok(|res| res.map_err(Status::from_raw));
-
-    let (meta_far, contents) = pkgprime.contents();
-    let mut contents = contents
-        .into_iter()
-        .map(|blob| (BlobId::from(blob.merkle), blob.contents))
-        .collect::<HashMap<_, Vec<u8>>>();
-
-    let (meta_blob, meta_blob_server_end) = fidl::endpoints::create_proxy::<FileMarker>().unwrap();
-    let res = needed_blobs.open_meta_blob(meta_blob_server_end).await.unwrap().unwrap();
-    assert!(res);
-    write_blob(&meta_far.contents, meta_blob).await;
-
-    // Ensure that the new meta.far is persisted despite having missing blobs, and the "old" blobs
-    // are not removed.
-    client.gc().await.expect("to perform a gc without error");
-    test_blobs.insert(*pkgprime.meta_far_merkle_root());
-    assert_eq!(env.blobfs().list_blobs().expect("to get new blobs"), test_blobs);
-
-    // Fetch the remaining blobs, and run a GC to kick out the old ones.
-    let missing_blobs = get_missing_blobs(&needed_blobs).await;
-    for mut blob in missing_blobs {
-        let buf = contents.remove(&blob.blob_id.into()).unwrap();
-
-        let (content_blob, content_blob_server_end) =
-            fidl::endpoints::create_proxy::<FileMarker>().unwrap();
-        assert!(needed_blobs
-            .open_blob(&mut blob.blob_id, content_blob_server_end)
-            .await
-            .unwrap()
-            .unwrap());
-
-        let () = write_blob(&buf, content_blob).await;
-
-        // Run a GC to try to reap blobs protected by meta far.
-        client.gc().await.expect("to perform a gc without error");
-    }
-
-    let () = get_fut.await.unwrap().unwrap();
-    let () = pkgprime.verify_contents(&dir).await.unwrap();
-    client.gc().await.expect("to perform a gc without error");
-
-    // At this point, we expect blobfs to only contain the blobs from the system image package and
-    // from pkgprime.
-    let expected_blobs = sysimg_pkg
-        .list_blobs()
-        .unwrap()
-        .union(&pkgprime.list_blobs().unwrap())
-        .cloned()
-        .collect::<BTreeSet<_>>();
-
-    assert_eq!(env.blobfs().list_blobs().expect("all blobs"), expected_blobs);
-}
-
-/// Assert that any blobs protected by the dynamic index are ineligible for garbage collection.
-/// Furthermore, ensure that an incomplete package does not lose blobs, and that the previous
-/// packages' blobs survive until the new package is entirely written.
-#[fasync::run_singlethreaded(test)]
-async fn gc_dynamic_index_protected_pkg_cache() {
+async fn gc_dynamic_index_protected() {
     let (env, sysimg_pkg) = setup_test_env(None, &[]).await;
 
     let pkg = PackageBuilder::new("gc_dynamic_index_protected_pkg_cache")
@@ -463,35 +338,7 @@ async fn gc_dynamic_index_protected_pkg_cache() {
 
 /// Test that a blobfs with blobs not belonging to a known package will lose those blobs on GC.
 #[fasync::run_singlethreaded(test)]
-async fn gc_random_blobs_pkgfs() {
-    let static_package = PackageBuilder::new("static-package")
-        .add_resource_at("resource", &[][..])
-        .build()
-        .await
-        .unwrap();
-    let blobfs = BlobfsRamdisk::builder()
-        .with_blob(b"blobby mcblobberson".to_vec())
-        .start()
-        .expect("blobfs creation to succeed with stray blob");
-    let gced_blob = blobfs
-        .list_blobs()
-        .expect("to find initial blob")
-        .into_iter()
-        .next()
-        .expect("to get initial blob");
-    let (env, _) = setup_test_env(Some(blobfs), &[&static_package]).await;
-    let mut original_blobs = env.blobfs().list_blobs().expect("to get an initial list of blobs");
-    let proxy = env.pkgfs.root_dir_proxy().expect("to get a handle to pkgfs");
-    let client =
-        pkgfs::control::Client::open_from_pkgfs_root(&proxy).expect("to get a control client");
-    client.gc().await.expect("to perform a gc without error");
-    assert!(original_blobs.remove(&gced_blob));
-    assert_eq!(env.blobfs().list_blobs().expect("to read current blobfs state"), original_blobs);
-}
-
-/// Test that a blobfs with blobs not belonging to a known package will lose those blobs on GC.
-#[fasync::run_singlethreaded(test)]
-async fn gc_random_blobs_pkg_cache() {
+async fn gc_random_blobs() {
     let static_package = PackageBuilder::new("static-package")
         .add_resource_at("resource", &[][..])
         .build()
@@ -515,118 +362,10 @@ async fn gc_random_blobs_pkg_cache() {
     assert_eq!(env.blobfs().list_blobs().expect("to read current blobfs state"), original_blobs);
 }
 
-/// Effectively the same as gc_dynamic_index_protected_pkgfs, except that the updated package also
+/// Effectively the same as gc_dynamic_index_protected, except that the updated package also
 /// existed as a static package as well.
 #[fasync::run_singlethreaded(test)]
-async fn gc_updated_static_package_pkgfs() {
-    let static_package = PackageBuilder::new("gc_updated_static_package_pkgfs")
-        .add_resource_at("bin/x", "bin-x-version-0".as_bytes())
-        .add_resource_at("data/unchanging", "unchanging-content".as_bytes())
-        .build()
-        .await
-        .unwrap();
-
-    let (env, _) = setup_test_env(None, &[&static_package]).await;
-    let initial_blobs = env.blobfs().list_blobs().expect("to get initial blob list");
-
-    let pkg = PackageBuilder::new("gc_updated_static_package_pkgfs")
-        .add_resource_at("bin/x", "bin-x-version-1".as_bytes())
-        .add_resource_at("data/unchanging", "unchanging-content".as_bytes())
-        .build()
-        .await
-        .unwrap();
-    do_fetch(&env.proxies.package_cache, &pkg).await;
-
-    // Ensure that the just-fetched blobs are not reaped by a GC cycle.
-    let mut test_blobs = env.blobfs().list_blobs().expect("to get an initial list of blobs");
-
-    let proxy = env.pkgfs.root_dir_proxy().expect("to get a handle to pkgfs");
-    let client =
-        pkgfs::control::Client::open_from_pkgfs_root(&proxy).expect("to get a control client");
-    client.gc().await.expect("to perform a gc without error");
-    assert_eq!(env.blobfs().list_blobs().expect("to get new blobs"), test_blobs);
-
-    let pkgprime = PackageBuilder::new("gc_updated_static_package_pkgfs")
-        .add_resource_at("bin/x", "bin-x-version-2".as_bytes())
-        .add_resource_at("data/unchanging", "unchanging-content".as_bytes())
-        .build()
-        .await
-        .unwrap();
-
-    // pkgfs would let us call do_fetch here, but pkg-cache differs from pkgfs in that the
-    // NeededBlobs protocol can be "canceled". This means that if the channel is closed before the
-    // protocol is completed, the blobs mentioned in the meta far are no longer protected by the
-    // dynamic index. That's WAI, but complicating that interface further isn't worth it.
-    //
-    // Here, we persist the meta.far
-    let mut meta_blob_info =
-        BlobInfo { blob_id: BlobId::from(*pkgprime.meta_far_merkle_root()).into(), length: 0 };
-    let package_cache = &env.proxies.package_cache;
-
-    let (needed_blobs, needed_blobs_server_end) =
-        fidl::endpoints::create_proxy::<NeededBlobsMarker>().unwrap();
-    let (dir, dir_server_end) = fidl::endpoints::create_proxy::<DirectoryMarker>().unwrap();
-    let get_fut = package_cache
-        .get(
-            &mut meta_blob_info,
-            &mut std::iter::empty(),
-            needed_blobs_server_end,
-            Some(dir_server_end),
-        )
-        .map_ok(|res| res.map_err(Status::from_raw));
-
-    let (meta_far, contents) = pkgprime.contents();
-    let mut contents = contents
-        .into_iter()
-        .map(|blob| (BlobId::from(blob.merkle), blob.contents))
-        .collect::<HashMap<_, Vec<u8>>>();
-
-    let (meta_blob, meta_blob_server_end) = fidl::endpoints::create_proxy::<FileMarker>().unwrap();
-    let res = needed_blobs.open_meta_blob(meta_blob_server_end).await.unwrap().unwrap();
-    assert!(res);
-    write_blob(&meta_far.contents, meta_blob).await;
-
-    // Ensure that the new meta.far is persisted despite having missing blobs, and the "old" blobs
-    // are not removed.
-    client.gc().await.expect("to perform a gc without error");
-    test_blobs.insert(*pkgprime.meta_far_merkle_root());
-    assert_eq!(env.blobfs().list_blobs().expect("to get new blobs"), test_blobs);
-
-    // Fetch the remaining blobs, and run a GC to kick out the old ones.
-    let missing_blobs = get_missing_blobs(&needed_blobs).await;
-    for mut blob in missing_blobs {
-        let buf = contents.remove(&blob.blob_id.into()).unwrap();
-
-        let (content_blob, content_blob_server_end) =
-            fidl::endpoints::create_proxy::<FileMarker>().unwrap();
-        assert!(needed_blobs
-            .open_blob(&mut blob.blob_id, content_blob_server_end)
-            .await
-            .unwrap()
-            .unwrap());
-
-        let () = write_blob(&buf, content_blob).await;
-
-        // Run a GC to try to reap blobs protected by meta far.
-        client.gc().await.expect("to perform a gc without error");
-    }
-
-    let () = get_fut.await.unwrap().unwrap();
-    let () = pkgprime.verify_contents(&dir).await.unwrap();
-    client.gc().await.expect("to perform a gc without error");
-
-    // At this point, we expect blobfs to only contain the blobs from the system image package and
-    // from pkgprime.
-    let expected_blobs =
-        initial_blobs.union(&pkgprime.list_blobs().unwrap()).cloned().collect::<BTreeSet<_>>();
-
-    assert_eq!(env.blobfs().list_blobs().expect("all blobs"), expected_blobs);
-}
-
-/// Effectively the same as gc_dynamic_index_protected_pkgfs, except that the updated package also
-/// existed as a static package as well.
-#[fasync::run_singlethreaded(test)]
-async fn gc_updated_static_package_pkg_cache() {
+async fn gc_updated_static_package() {
     let static_package = PackageBuilder::new("gc_updated_static_package_pkg_cache")
         .add_resource_at("bin/x", "bin-x-version-0".as_bytes())
         .add_resource_at("data/unchanging", "unchanging-content".as_bytes())
