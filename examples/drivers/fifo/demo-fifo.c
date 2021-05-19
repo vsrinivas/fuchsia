8b5470f2a3078ab67883a18631edd431aafd5133
Suraj Malhotra <surajmalhotra@google.com>
2021-05-19 17:41:24 +0000

    [fdf][demo-fifo] Fix build
    Bug: 77082
    Change-Id: Iaca3599cc62cf4735c82cd33f495fada89d6ea42
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/531760
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- examples/drivers/fifo/demo-fifo.c
+++ examples/drivers/fifo/demo-fifo.c
@@ -2,15 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/ddk/device.h>
+#include <lib/ddk/driver.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <threads.h>
 #include <zircon/syscalls.h>
 #include <zircon/types.h>
-
-#include <ddk/device.h>
-#include <ddk/driver.h>
 
 #include "examples/drivers/fifo/demo-fifo-bind.h"
 
