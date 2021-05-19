0c5e65ad704c52ea7adc2c7258816747fb4344e7
Roland McGrath <mcgrathr@google.com>
2021-05-19 00:38:28 +0000

    [phys][x86] Use InitMemoryFromRanges() for Multiboot
    Reuse the common legacy-boot support code for Multiboot.
    
    Bug: 68762
    Change-Id: I5e12a831a2488ba1155170088752024fe807084e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/528514
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/arch/x86/phys/multiboot-init.cc
+++ zircon/kernel/arch/x86/phys/multiboot-init.cc
@@ -11,8 +11,11 @@
 #include <zircon/boot/e820.h>
 #include <zircon/boot/multiboot.h>
 
+#include <ktl/atomic.h>
 #include <phys/allocation.h>
 #include <phys/main.h>
+#include <phys/symbolize.h>
+#include <pretty/sizes.h>
 
 #include "legacy-boot.h"
 
@@ -22,107 +25,94 @@ LegacyBoot gLegacyBoot;
 void InitMemory(void* bootloader_data) {
   const auto& info = *static_cast<const multiboot_info_t*>(bootloader_data);
 
-  auto& allocator = Allocation::GetAllocator();
-  auto add_range = [&allocator](uint64_t base, uint64_t size, auto what) {
-    ZX_ASSERT_MSG(allocator.AddRange(base, size).is_ok(),
-                  "Cannot add %s range [%#" PRIx64 ", %#" PRIx64 ")\n", what, base, size);
-    printf("  paddr: [0x%016" PRIx64 " -- 0x%016" PRIx64 ") : size %10" PRIu64 " kiB : %s added\n",
-           base, base + size, size >> 10, what);
-  };
-  auto remove_range = [&allocator](uint64_t base, uint64_t size, auto what) {
-    ZX_ASSERT_MSG(allocator.RemoveRange(base, size).is_ok(),
-                  "Cannot remove %s range [%#" PRIx64 ", %#" PRIx64 ")\n", what, base, size);
-    printf("  paddr: [0x%016" PRIx64 " -- 0x%016" PRIx64 ") : size %10" PRIu64
-           " kiB : %s removed\n",
-           base, base + size, size >> 10, what);
-  };
-
-  // Handle the fixed ranges, if present.
-  if (info.flags & MB_INFO_MEM_SIZE) {
-    // Lower memory is from 0 to 640K.
-    add_range(0, info.mem_lower << 10, "mem_lower");
-
-    // Upper memory is above 1M.
-    add_range(1 << 20, (info.mem_upper << 10) - (1 << 20), "mem_upper");
+  if ((info.flags & MB_INFO_BOOT_LOADER) && info.boot_loader_name != 0) {
+    gLegacyBoot.bootloader = reinterpret_cast<const char*>(info.boot_loader_name);
   }
 
-  // Iterate over the discontiguous ranges, if present.
-  auto for_each_mmap = [&info](auto&& f) {
-    if (info.mmap_addr != 0 && info.mmap_length >= sizeof(memory_map_t) &&
-        (info.flags & MB_INFO_MMAP)) {
-      auto first = reinterpret_cast<const memory_map_t*>(info.mmap_addr);
-      auto last = reinterpret_cast<const memory_map_t*>(info.mmap_addr + info.mmap_length);
-      constexpr auto next = [](const memory_map_t* m) {
-        return reinterpret_cast<const memory_map_t*>(  //
-            reinterpret_cast<const char*>(&m->base_addr_low) + m->size);
+  if ((info.flags & MB_INFO_CMD_LINE) && info.cmdline != 0) {
+    gLegacyBoot.cmdline = reinterpret_cast<const char*>(info.cmdline);
+  }
+
+  if ((info.flags & MB_INFO_MMAP) && info.mmap_addr != 0 &&
+      info.mmap_length >= sizeof(memory_map_t)) {
+    // If the map of discontiguous ranges is present, it covers everything.
+    // The entry size fits the ZBI format, so convert each entry in place.
+    static_assert(sizeof(memory_map_t) >= sizeof(zbi_mem_range_t));
+
+    auto mem_ranges = reinterpret_cast<zbi_mem_range_t*>(info.mmap_addr);
+    size_t count = 0;
+
+    auto m = reinterpret_cast<const memory_map_t*>(info.mmap_addr);
+    const auto end = reinterpret_cast<const memory_map_t*>(info.mmap_addr + info.mmap_length);
+    while (m < end) {
+      zbi_mem_range_t entry = {
+          .paddr = (uint64_t{m->base_addr_high} << 32) | m->base_addr_low,
+          .length = (uint64_t{m->length_high} << 32) | m->length_low,
       };
-      for (auto m = first; m < last; m = next(m)) {
-        uint64_t base = (uint64_t{m->base_addr_high} << 32) | m->base_addr_low;
-        uint64_t size = (uint64_t{m->length_high} << 32) | m->length_low;
-        f(m->type, base, size);
+
+      switch (m->type) {
+        case MB_MMAP_TYPE_AVAILABLE:
+          entry.type = ZBI_MEM_RANGE_RAM;
+          break;
+
+        case MB_MMAP_TYPE_RESERVED:
+        default:
+          // There are other MB_MMAP_TYPE_* types but none indicates usable RAM
+          // and none corresponds to ZBI_MEM_RANGE_PERIPHERAL.
+          entry.type = ZBI_MEM_RANGE_RESERVED;
+          break;
       }
-    }
-  };
 
-  // Add normal memory first.
-  for_each_mmap([&](auto type, auto base, auto size) {
-    if (type == MB_MMAP_TYPE_AVAILABLE) {
-      add_range(base, size, "mmap");
-    }
-  });
+      m = reinterpret_cast<const memory_map_t*>(  //
+          reinterpret_cast<const char*>(&m->size + 1) + m->size);
 
-  // Now remove everything else, in case it overlapped.
-  for_each_mmap([&](auto type, auto base, auto size) {
-    if (type != MB_MMAP_TYPE_AVAILABLE) {
-      remove_range(base, size, "mmap");
-    }
-  });
+      // Tell the compiler not to move any of the memory accesses after the
+      // store below, since we're violating the TBAA rules and the compiler
+      // could otherwise presume that they don't overlap.
+      ktl::atomic_signal_fence(ktl::memory_order_seq_cst);
 
-  // Remove the memory occupied by modules (i.e. the ZBI).
+      mem_ranges[count++] = entry;
+    }
+
+    gLegacyBoot.mem_config = {mem_ranges, count};
+  } else if (info.flags & MB_INFO_MEM_SIZE) {
+    // Without the full map, there are only the fixed low areas of memory.
+    static zbi_mem_range_t low_ranges[] = {
+        // Lower memory is from 0 to 640K.
+        {
+            .paddr = 0,
+            .length = info.mem_lower << 10,
+            .type = ZBI_MEM_RANGE_RAM,
+        },
+
+        // Upper memory is above 1M.
+        {
+            .paddr = 1 << 20,
+            .length = info.mem_upper << 10,
+            .type = ZBI_MEM_RANGE_RAM,
+        },
+    };
+    gLegacyBoot.mem_config = low_ranges;
+  }
+
   if ((info.flags & MB_INFO_MODS) && info.mods_addr != 0) {
     ktl::span mods{
         reinterpret_cast<const module_t*>(info.mods_addr),
         info.mods_count,
     };
-    for (const auto& m : mods) {
-      uintptr_t base = m.mod_start, size = m.mod_end - m.mod_start;
-      remove_range(base, size, "mod");
-      gLegacyBoot.ramdisk = {reinterpret_cast<ktl::byte*>(base), size};
-    }
-    if (mods.size() != 1) {
-      printf("Multiboot mods @ %p count %zu != expected 1.\n", mods.data(), mods.size());
-    }
-  }
-
-  // Remove the memory occupied by the Multiboot command line, if present.
-  // It will be copied into the data ZBI, but that requires allocation.
-  // The same goes for the boot loader name, if present.
-
-  auto collect_string = [&](uintptr_t paddr, auto what) {
-    ktl::string_view result;
-    if (paddr != 0) {
-      result = reinterpret_cast<const char*>(paddr);
-      if (!result.empty()) {
-        remove_range(paddr, result.size() + 1, what);
+    if (!mods.empty()) {
+      gLegacyBoot.ramdisk = {
+          reinterpret_cast<ktl::byte*>(mods.front().mod_start),
+          mods.front().mod_end,
+      };
+      if (mods.size() > 1) {
+        printf("%s: Multiboot mods @ %p count %zu != expected 1.\n", Symbolize::kProgramName_,
+               mods.data(), mods.size());
       }
     }
-    return result;
-  };
-
-  if (info.flags & MB_INFO_CMD_LINE) {
-    gLegacyBoot.cmdline = collect_string(info.cmdline, "cmdline");
   }
 
-  if (info.flags & MB_INFO_BOOT_LOADER) {
-    gLegacyBoot.bootloader = collect_string(info.boot_loader_name, "boot_loader_name");
-  }
-
-  // Remove space occupied by the program itself.
-  Allocation::InitReservedRanges();
-
-  // Remove everything above the 32-bit barrier since we can't use it directly.
-  constexpr uint64_t k4GiB = uint64_t{1} << 32;
-  remove_range(k4GiB, -k4GiB, "high 32 bits");
+  InitMemoryFromRanges();
 
   // Note this doesn't remove the memory covering the Multiboot info itself or
   // the memory map or module list data just examined.  We assume those have
