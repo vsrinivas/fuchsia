365602d4630a15119e96da7ace45fcd615dd65a9
Venkatesh Srinivas <venkateshs@chromium.org>
2021-05-20 23:17:36 +0000

    [mtrace][perf_mon][x86] Test Instructions Retired fixed-function counters
    All architectural Intel PMUs support a small number of fixed-function
    counters, for example, instructions retired or unhalted reference
    cycles. perf_mon provides access to these fixed-function counters via
    the global_ctrl and fixed_ctrl fields of ArchPmuConfig, which directly
    program architectural PMU MSRs IA32_PERF_GLOBAL_CTRL and IA32_FIXED_CTRL
    respectively.
    
    Add a unit test of the mtrace syscall, enabling counting of the fixed-
    function instructions retired counter. Validates that the PMU can be
    configured, that tracing can be started/stopped, and that counter
    records are written to perf-cpu VMOs.
    
    * Adds test coverage to:
      MTRACE_PERFMON_STAGE_CONFIG
      MTRACE_PERFMON_START
      MTRACE_PERFMON_STOP
    
      with non-zero fixed_ctl
    
    The tests are meant to be minimal functionality tests, not tests of
    error handling. So they only test one valid configuration of the PMU; do
    not test PMIs (performance interrupts), timebase handling, etc.
    
    Tested:
    * On Intel Apollo Lake / Goldmont:
    $ /boot/test/mtrace
    [==========] Flag Values:
                 --gtest_random_seed = 1621459700
    [==========]
    [==========] Running 4 tests from 1 test case.
    [----------] Global test environment set-up.
    [----------] 4 tests from X86MtraceTestCase
    [ RUN      ] X86MtraceTestCase.GetProperties
    [       OK ] X86MtraceTestCase.GetProperties (2 ms)
    [ RUN      ] X86MtraceTestCase.InitFini
    [       OK ] X86MtraceTestCase.InitFini (2 ms)
    [ RUN      ] X86MtraceTestCase.AssignBuffer
    [       OK ] X86MtraceTestCase.AssignBuffer (2 ms)
    [ RUN      ] X86MtraceTestCase.InstructionsRetiredFixedCounterTest
    [00006.894] 06684:06686> arch_perfmon_start:1484: Enabling perfmon, 1 fixed, 0 programmable, 0 misc
    [00006.907] 06684:06686> arch_perfmon_stop_locked:1631: Disabling perfmon
    [       OK ] X86MtraceTestCase.InstructionsRetiredFixedCounterTest (23 ms)
    [----------] 4 tests from X86MtraceTestCase (40 ms total)
    
    [----------] Global test environment tear-down.
    [==========] 4 tests from 1 test case ran (47 ms total).
    [  PASSED  ] 4 tests
    $
    
    Change-Id: I241858aa38a6432abf581d956dc1ba316efa597b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/524545
    Commit-Queue: Theodore Dubois <tbodt@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>

--- src/zircon/tests/mtrace/main.cc
+++ src/zircon/tests/mtrace/main.cc
@@ -144,4 +144,78 @@ TEST(X86MtraceTestCase, AssignBuffer) {
   EXPECT_EQ(status, ZX_OK);
 }
 
+// Test a full mtrace MTRACE_KIND_PERFMON cycle - initialize, allocate buffers, configure,
+// start/stop tracing, and validate the returned buffers. The test uses a real hardware performance
+// counter - the Intel Architectural PMU Fixed-Function Counter 0, 'Instructions Retired'.
+TEST(X86MtraceTestCase, InstructionsRetiredFixedCounterTest) {
+  perfmon::X86PmuProperties properties;
+  zx::status<zx::resource> root_resource_or_error = GetRootResource();
+  zx::resource root_resource = std::move(root_resource_or_error.value());
+  ASSERT_TRUE(root_resource_or_error.is_ok(), "failed to get root resource: %s",
+              zx_status_get_string(root_resource_or_error.error_value()));
+  zx_status_t status =
+      zx_mtrace_control(root_resource.get(), MTRACE_KIND_PERFMON, MTRACE_PERFMON_GET_PROPERTIES, 0,
+                        &properties, sizeof(properties));
+  if (!IsIntelPMUSupported()) {
+    EXPECT_EQ(status, ZX_ERR_NOT_SUPPORTED);
+    printf("Skipping test, Intel Architectual PMU not supported\n");
+    return;
+  }
+  ASSERT_EQ(status, ZX_OK);
+  status = zx_mtrace_control(root_resource.get(), MTRACE_KIND_PERFMON, MTRACE_PERFMON_INIT, 0,
+                             nullptr, 0);
+  EXPECT_EQ(status, ZX_OK);
+  uint32_t num_cpus = zx_system_get_num_cpus();
+  std::vector<zx_handle_t> vmos(num_cpus);
+  for (uint i = 0; i < num_cpus; i++) {
+    ASSERT_EQ(zx_vmo_create(PAGE_SIZE, 0, &vmos[i]), ZX_OK);
+    int cpu = i;
+    zx_pmu_buffer_t buffer = {.vmo = vmos[i]};
+    status = zx_mtrace_control(root_resource.get(), MTRACE_KIND_PERFMON,
+                               MTRACE_PERFMON_ASSIGN_BUFFER, cpu, &buffer, sizeof(buffer));
+    EXPECT_EQ(status, ZX_OK);
+  }
+
+  // Stage a configuation to enable the instructions retired fixed-function counter.
+  perfmon::X86PmuConfig config = {};
+  config.global_ctrl = (1ul << 32);          // Enable fixed counter 0.
+  config.fixed_ctrl = 1;                     // Enabled fixed counter 0 at CPL=0.
+  config.fixed_events[0] = (2ul << 11) | 1;  // kGroupFixed | Instructions retired.
+  status = zx_mtrace_control(root_resource.get(), MTRACE_KIND_PERFMON, MTRACE_PERFMON_STAGE_CONFIG,
+                             0, &config, sizeof(config));
+  EXPECT_EQ(status, ZX_OK);
+
+  // Start and stop tracing. Each will execute some code at CPL=0.
+  status = zx_mtrace_control(root_resource.get(), MTRACE_KIND_PERFMON, MTRACE_PERFMON_START, 0,
+                             nullptr, 0);
+  EXPECT_EQ(status, ZX_OK);
+  status = zx_mtrace_control(root_resource.get(), MTRACE_KIND_PERFMON, MTRACE_PERFMON_STOP, 0,
+                             nullptr, 0);
+  EXPECT_EQ(status, ZX_OK);
+
+  status = zx_mtrace_control(root_resource.get(), MTRACE_KIND_PERFMON, MTRACE_PERFMON_FINI, 0,
+                             nullptr, 0);
+  EXPECT_EQ(status, ZX_OK);
+  // Examine the buffers; each buffer should have a fixed length BufferHeader followed by one or
+  // more variable-length Records. Each Record has a common header.
+  struct Buffer {
+    perfmon::BufferHeader header;
+    char data[PAGE_SIZE - sizeof(perfmon::BufferHeader)];
+  };
+  static_assert(sizeof(Buffer) == PAGE_SIZE);
+  for (uint i = 0; i < num_cpus; i++) {
+    Buffer buffer = {};
+    status = zx_vmo_read(vmos[i], &buffer, 0, sizeof(Buffer));
+    EXPECT_EQ(status, ZX_OK);
+    EXPECT_EQ(buffer.header.version, 0);
+    EXPECT_EQ(buffer.header.arch, 1);  // Arch X86
+    // Expect at least one record.
+    EXPECT_GT(buffer.header.capture_end, sizeof(buffer.header));
+    // First record must be a time record.
+    perfmon::RecordHeader* const record =
+        reinterpret_cast<perfmon::RecordHeader*>(buffer.data);
+    EXPECT_EQ(record->type, perfmon::kRecordTypeTime);
+  }
+}
+
 }  // namespace
