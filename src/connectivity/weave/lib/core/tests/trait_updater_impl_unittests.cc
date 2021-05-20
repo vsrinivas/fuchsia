0ae6cb81dd5bf4c9e52eee41b9180cab5abb4175
Prashanth Swaminathan <prashanthsw@google.com>
2021-05-20 00:03:51 +0000

    [weave][lint] Fix lib/core linter complaints
    Test: fx test weave-trait-applet-tests
    Change-Id: If683906ecffe934d3920ee93e3664daa973362cc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/532068
    Fuchsia-Auto-Submit: Prashanth Swaminathan <prashanthsw@google.com>
    Reviewed-by: Sundarajan Srinivasan <sundarsrini@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/weave/lib/core/tests/trait_updater_impl_unittests.cc
+++ src/connectivity/weave/lib/core/tests/trait_updater_impl_unittests.cc
@@ -2,68 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/gtest/test_loop_fixture.h>
+
 #include "src/connectivity/weave/lib/core/trait_updater.h"
 #include "src/connectivity/weave/lib/core/trait_updater_delegate_impl.h"
 
-#include "src/connectivity/weave/adaptation/configuration_manager_delegate_impl.h"
-#include "src/connectivity/weave/adaptation/tests/weave_test_fixture.h"
-
 namespace nl::Weave::DeviceLayer::Internal::testing {
-using nl::Weave::DeviceLayer::TraitUpdaterImpl;
+namespace {
 using nl::Weave::DeviceLayer::WeaveDeviceEvent;
-using weavestack::applets::Applet;
 
 std::vector<std::string> test_list = {"test_applets.so"};
 std::vector<std::string> empty_list;
+}  // namespace
 
-class ConfigurationManagerTestDelegateImpl : public ConfigurationManagerDelegateImpl {
+class TraitUpdaterImplTest : public ::gtest::TestLoopFixture {
  public:
-    bool IsMemberOfFabric() override {
-      return member_of_fabric;
-    }
-    void SetMemberOfFabric(bool value) {
-        member_of_fabric = value;
-    }
- protected:
-    bool member_of_fabric = false;
-};
-
-class TraitUpdaterImplTest : public WeaveTestFixture<> {
- public:
-  void SetUp() {
-    WeaveTestFixture<>::SetUp();
-    WeaveTestFixture<>::RunFixtureLoop();
-    ConfigurationMgrImpl().SetDelegate(std::make_unique<ConfigurationManagerTestDelegateImpl>());
+  void SetUp() override {
     TraitUpdater().SetDelegate(std::make_unique<TraitUpdaterDelegateImpl>());
   }
 
-  void TearDown() {
-    WeaveTestFixture<>::StopFixtureLoop();
-    WeaveTestFixture<>::TearDown();
-    ConfigurationMgrImpl().SetDelegate(nullptr);
-    TraitUpdater().SetDelegate(nullptr);
-  }
+  void TearDown() override { TraitUpdater().SetDelegate(nullptr); }
 };
 
 TEST_F(TraitUpdaterImplTest, InitNoApplets) {
-  auto delegate = (TraitUpdaterDelegateImpl*)TraitUpdater().GetDelegate();
+  auto delegate = reinterpret_cast<TraitUpdaterDelegateImpl*>(TraitUpdater().GetDelegate());
   EXPECT_EQ(ZX_ERR_INVALID_ARGS, delegate->InitApplets(empty_list));
 }
 
 TEST_F(TraitUpdaterImplTest, Init) {
-  auto delegate = (TraitUpdaterDelegateImpl*)TraitUpdater().GetDelegate();
+  auto delegate = reinterpret_cast<TraitUpdaterDelegateImpl*>(TraitUpdater().GetDelegate());
   EXPECT_EQ(ZX_OK, delegate->InitApplets(test_list));
 }
 
 TEST_F(TraitUpdaterImplTest, OnPlatformEvent) {
   constexpr char kTestString[] = "DEADBEEF";
-  auto trait_updater_delegate = (TraitUpdaterDelegateImpl*)TraitUpdater().GetDelegate();
-  EXPECT_EQ(ZX_OK, trait_updater_delegate->InitApplets(test_list));
-  WeaveDeviceEvent fabric_event{.Type = DeviceEventType::kFabricMembershipChange};
-  auto delegate = ConfigurationMgrImpl().GetDelegate();
-  reinterpret_cast<ConfigurationManagerTestDelegateImpl*>(delegate)->SetMemberOfFabric(true);
-  trait_updater_delegate->HandleWeaveDeviceEvent(&fabric_event);
-  EXPECT_STREQ((char*)fabric_event.Platform.arg, kTestString);
+  constexpr WeaveDeviceEvent test_event = {};
+
+  auto delegate = reinterpret_cast<TraitUpdaterDelegateImpl*>(TraitUpdater().GetDelegate());
+  EXPECT_EQ(ZX_OK, delegate->InitApplets(test_list));
+
+  delegate->HandleWeaveDeviceEvent(&test_event);
+  EXPECT_STREQ((char*)test_event.Platform.arg, kTestString);
 }
 
-}  // nl::Weave::DeviceLayer
+}  // namespace nl::Weave::DeviceLayer::Internal::testing
