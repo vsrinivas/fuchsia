5a30ae6dfe33cf96bd09b2e34e7d40d2770391ff
Faraaz Sareshwala <fsareshwala@google.com>
2021-05-23 22:50:16 +0000

    [bt][cleanup] reformat comments
    This change simply reformats comments to adjust to the line length of
    the file. There are no other logic changes within this change. This
    change is introduced mainly as a precursor to larger changes coming up
    shortly. The change is intended to avoid noise in the larger changes.
    
    Change-Id: Ie4efeb095e5c5c6643791801bec0aab3c1a73a73
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/533531
    Commit-Queue: Faraaz Sareshwala <fsareshwala@google.com>
    Reviewed-by: Ani Ramakrishnan <aniramakri@google.com>

--- src/connectivity/bluetooth/core/bt-host/hci/low_energy_advertiser.h
+++ src/connectivity/bluetooth/core/bt-host/hci/low_energy_advertiser.h
@@ -24,8 +24,8 @@ class Transport;
 
 class AdvertisingIntervalRange final {
  public:
-  // Constructs an advertising interval range, capping the values based on the allowed range
-  // (Vol 2, Part E, 7.8.5).
+  // Constructs an advertising interval range, capping the values based on the allowed range (Vol 2,
+  // Part E, 7.8.5).
   constexpr AdvertisingIntervalRange(uint16_t min, uint16_t max)
       : min_(std::max(min, kLEAdvertisingIntervalMin)),
         max_(std::min(max, kLEAdvertisingIntervalMax)) {
@@ -46,54 +46,48 @@ class LowEnergyAdvertiser : public LocalAddressClient {
   // Get the current limit in bytes of the advertisement data supported.
   virtual size_t GetSizeLimit() = 0;
 
-  // Get the current limit of number of advertising sets supported, including
-  // currently enabled ones.  This can change when the state of advertising
-  // changes.  It should be checked before trying to add an advertisement.
+  // Get the current limit of number of advertising sets supported, including currently enabled
+  // ones.  This can change when the state of advertising changes.  It should be checked before
+  // trying to add an advertisement.
   virtual size_t GetMaxAdvertisements() const = 0;
 
-  // TODO(armansito): The |address| parameter of this function doesn't always
-  // correspond to the advertised device address as the local address for an
-  // advertisement cannot always be configured by the advertiser. This is the
-  // case especially in the following conditions:
+  // TODO(armansito): The |address| parameter of this function doesn't always correspond to the
+  // advertised device address as the local address for an advertisement cannot always be configured
+  // by the advertiser. This is the case especially in the following conditions:
   //
-  //   1. The type of |address| is "LE Public". The advertised address always
-  //   corresponds to the controller's BD_ADDR. This is the case in both legacy
-  //   and extended advertising.
+  //   1. The type of |address| is "LE Public". The advertised address always corresponds to the
+  //      controller's BD_ADDR. This is the case in both legacy and extended advertising.
   //
-  //   2. The type of |address| is "LE Random" and the advertiser implements
-  //   legacy advertising. Since the controller local address is shared between
-  //   scan, initiation, and advertising procedures, the advertiser cannot
-  //   configure this address without interfering with the state of other
-  //   ongoing procedures.
+  //   2. The type of |address| is "LE Random" and the advertiser implements legacy advertising.
+  //      Since the controller local address is shared between scan, initiation, and advertising
+  //      procedures, the advertiser cannot configure this address without interfering with the
+  //      state of other ongoing procedures.
   //
-  // We should either revisit this interface or update the documentation to
-  // reflect the fact that the |address| is sometimes a hint and may or may not
-  // end up being advertised. Currently the GAP layer decides which address to
-  // pass to this call but the layering should be revisited when we add support
-  // for extended advertising.
+  // We should either revisit this interface or update the documentation to reflect the fact that
+  // the |address| is sometimes a hint and may or may not end up being advertised. Currently the GAP
+  // layer decides which address to pass to this call but the layering should be revisited when we
+  // add support for extended advertising.
   //
   // -----
   //
-  // Attempt to start advertising |data| with |adv_options.flags| and scan response |scan_rsp|
-  // using advertising address |address|.
-  // If |adv_options.anonymous| is set, |address| is ignored.
+  // Attempt to start advertising |data| with |adv_options.flags| and scan response |scan_rsp| using
+  // advertising address |address|. If |adv_options.anonymous| is set, |address| is ignored.
   //
   // If |address| is currently advertised, the advertisement is updated.
   //
-  // If |connect_callback| is provided, the advertisement will be connectable,
-  // and the provided |status_callback| will be called with a connection reference
-  // when this advertisement is connected to and the advertisement has been
-  // stopped.
+  // If |connect_callback| is provided, the advertisement will be connectable, and the provided
+  // |status_callback| will be called with a connection reference when this advertisement is
+  // connected to and the advertisement has been stopped.
   //
-  // |adv_options.interval| must be a value in "controller timeslices". See
-  // hci-spec/hci_constants.h for the valid range.
+  // |adv_options.interval| must be a value in "controller timeslices". See hci-spec/hci_constants.h
+  // for the valid range.
   //
-  // Provides results in |status_callback|. If advertising is setup, the final
-  // interval of advertising is provided in |interval| and |status|
-  // is kSuccess. Otherwise, |status| indicates the type of error and |interval| has no meaning.
+  // Provides results in |status_callback|. If advertising is setup, the final interval of
+  // advertising is provided in |interval| and |status| is kSuccess. Otherwise, |status| indicates
+  // the type of error and |interval| has no meaning.
   //
-  // |status_callback| may be called before this function returns, but will
-  // be called before any calls to |connect_callback|.
+  // |status_callback| may be called before this function returns, but will be called before any
+  // calls to |connect_callback|.
   //
   // The maximum advertising and scan response data sizes are determined by the Bluetooth controller
   // (4.x supports up to 31 bytes while 5.x is extended up to 251). If |data| and |scan_rsp| exceed
@@ -118,14 +112,13 @@ class LowEnergyAdvertiser : public LocalAddressClient {
                                 ConnectionCallback connect_callback,
                                 StatusCallback status_callback) = 0;
 
-  // Stops any advertisement currently active on |address|. Idempotent and
-  // asynchronous. Returns true if advertising will be stopped, false otherwise.
+  // Stops any advertisement currently active on |address|. Idempotent and asynchronous. Returns
+  // true if advertising will be stopped, false otherwise.
   virtual bool StopAdvertising(const DeviceAddress& address) = 0;
 
-  // Callback for an incoming LE connection. This function should be called
-  // in reaction to any connection that was not initiated locally. This object
-  // will determine if it was a result of an active advertisement and route the
-  // connection accordingly.
+  // Callback for an incoming LE connection. This function should be called in reaction to any
+  // connection that was not initiated locally. This object will determine if it was a result of an
+  // active advertisement and route the connection accordingly.
   // TODO(armansito): Require advertising handle.
   virtual void OnIncomingConnection(ConnectionHandle handle, Connection::Role role,
                                     const DeviceAddress& peer_address,
