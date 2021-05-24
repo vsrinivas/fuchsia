a2564682e2ef8405a9a828fcb293844635d537c1
Anthony Fandrianto <atyfto@google.com>
2021-05-24 19:55:01 +0000

    Revert "[unpack_debug_symbols] Fix hermeticity"
    This reverts commit cfea9966ce047bdb42ea3ef67faa34b748004bb9.
    
    Reason for revert: Maybe broke ninja convergence here: go/bbid/8846567755095722880. Testing out a revert
    
    Original change's description:
    > [unpack_debug_symbols] Fix hermeticity
    >
    > ... by writing all files read to depfile
    >
    > Before this fix, unpack_debug_symbols only writes the build_id
    > directory or archive, not the files it actually reads, to depfile.
    >
    > Also removed reference to ioutil since it's being deprecated.
    >
    > Change-Id: I91271ed20ab5c26e1ad1a0bc672f5510f8c42d96
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/513720
    > Commit-Queue: Jay Zhuang <jayzhuang@google.com>
    > Reviewed-by: Shai Barack <shayba@google.com>
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I5ff72541b9c027e795a4069dafed086b7736c3aa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/534142
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>

--- tools/debug/unpack_debug_symbols/cmd/main.go
+++ tools/debug/unpack_debug_symbols/cmd/main.go
@@ -12,6 +12,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -29,6 +30,7 @@ import (
 
 var (
 	debugArchive   string
+	buildDir       string
 	buildIDDirIn   string
 	buildIDDirOut  string
 	outputManifest string
@@ -51,6 +53,7 @@ func init() {
 	level = logger.WarningLevel
 
 	flag.StringVar(&debugArchive, "debug-archive", "", "path to archive of debug binaries")
+	flag.StringVar(&buildDir, "build-dir", "", "path to the build directory")
 	flag.StringVar(&buildIDDirIn, "build-id-dir-in", "", "path to an input .build-id directory")
 	flag.StringVar(&buildIDDirOut, "build-id-dir-out", "", "path to an output .build-id directory to populate")
 	flag.StringVar(&outputManifest, "output-manifest", "", "path to output a json manifest of debug binaries to")
@@ -84,7 +87,7 @@ type binaryRef struct {
 // layout.
 var buildIDFileNoExtRE = regexp.MustCompile("^([0-9a-f][0-9a-f])/([0-9a-f]+)$")
 
-func isBuildIDDir(ctx context.Context, dir string, contents []os.DirEntry) bool {
+func isBuildIDDir(ctx context.Context, dir string, contents []os.FileInfo) bool {
 	for _, info := range contents {
 		// Some special files are allowed and expected.
 		if info.Name() == "LICENSE" || info.Name()[0] == '.' {
@@ -107,7 +110,7 @@ func isBuildIDDir(ctx context.Context, dir string, contents []os.DirEntry) bool 
 		// Now that we know the directory name was a 2 digit hex value, it's safe
 		// to assume its most likely a .build-id directory and we can check to see
 		// if it contains the sorts of files we expect
-		files, err := os.ReadDir(filepath.Join(dir, info.Name()))
+		files, err := ioutil.ReadDir(filepath.Join(dir, info.Name()))
 		if err != nil {
 			logger.Tracef(ctx, "%s couldn't read directory: %v", info.Name(), err)
 			return false
@@ -174,7 +177,7 @@ func getStartDir() (string, error) {
 // the first .build-id subdirectory.
 func findBuildIDDir(ctx context.Context, startDir string) (string, error) {
 	logger.Tracef(ctx, "determining if %s is a .build-id directory", startDir)
-	infos, err := os.ReadDir(startDir)
+	infos, err := ioutil.ReadDir(startDir)
 	if err != nil {
 		return "", err
 	}
@@ -303,13 +306,23 @@ func unpack(ctx context.Context, br *BatchRunner) ([]binaryRef, error) {
 }
 
 func writeManifest(bfrs []binaryRef) error {
-	var out []binary
+	out := []binary{}
 	for _, bfr := range bfrs {
+		// Even though these files live outside of the build directory,
+		// relativize them as is conventional for build API metadata.
+		relDebug, err := filepath.Rel(buildDir, bfr.ref.Filepath)
+		if err != nil {
+			return err
+		}
+		relBreakpad, err := filepath.Rel(buildDir, bfr.breakpad)
+		if err != nil {
+			return err
+		}
 		out = append(out, binary{
 			CPU:      cpu,
-			Debug:    bfr.ref.Filepath,
+			Debug:    relDebug,
 			BuildID:  bfr.ref.BuildID,
-			Breakpad: bfr.breakpad,
+			Breakpad: relBreakpad,
 			OS:       osName,
 		})
 	}
@@ -332,6 +345,9 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	ctx = logger.WithLogger(ctx, log)
+	if buildDir == "" {
+		log.Fatalf("-build-dir is required.")
+	}
 	if buildIDDirOut == "" {
 		log.Fatalf("-build-id-dir-out is required.")
 	}
@@ -346,7 +362,7 @@ func main() {
 	}
 
 	// This action should rerun if the input .build-id directory or debug archive changes.
-	var deps []string
+	var dep string
 	if buildIDDirIn != "" {
 		empty, err := osmisc.DirIsEmpty(buildIDDirIn)
 		if err != nil {
@@ -359,9 +375,17 @@ func main() {
 			log.Tracef("%s does not exist, no work needed", buildIDDirIn)
 			return
 		}
-		deps = []string{buildIDDirIn}
+		dep = buildIDDirIn
 	} else {
-		deps = []string{debugArchive}
+		dep = debugArchive
+	}
+	relDep, err := filepath.Rel(buildDir, dep)
+	if err != nil {
+		log.Fatalf("failed to relativize %s: %v", dep, err)
+	}
+	depfileContents := fmt.Sprintf("%s: %s", outputManifest, relDep)
+	if err := ioutil.WriteFile(depfile, []byte(depfileContents), os.ModePerm); err != nil {
+		log.Fatalf("failed to write depfile: %v", err)
 	}
 
 	// If the input .build-id directory is empty and no debug archive is
@@ -387,7 +411,7 @@ func main() {
 		log.Fatalf("while checking if archive existed: %v", err)
 	}
 
-	var bfrs []binaryRef
+	bfrs := []binaryRef{}
 
 	log.Tracef("checking!")
 	if exists {
@@ -407,14 +431,6 @@ func main() {
 			log.Fatalf("while finding .build-id directory: %v", err)
 		}
 		bfrs, err = produceSymbols(ctx, dir, br)
-	}
-
-	for _, bfr := range bfrs {
-		deps = append(deps, bfr.ref.Filepath)
-	}
-	depfileContents := fmt.Sprintf("%s: %s", outputManifest, strings.Join(deps, " "))
-	if err := os.WriteFile(depfile, []byte(depfileContents), os.ModePerm); err != nil {
-		log.Fatalf("failed to write depfile: %v", err)
 	}
 
 	// TODO: write the manifest to a tmp file and rename it into place.
