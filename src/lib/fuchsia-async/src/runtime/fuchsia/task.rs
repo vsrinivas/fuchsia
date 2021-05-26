d03831a73c464cd22c4fe4fd51b11d0677a5e8cf
Xyan Bhatnagar <xbhatnag@google.com>
2021-05-26 21:11:46 +0000

    [fuchsia-async] Fuse future in Task creation calls
    The future passed into Task creation calls should be fused before
    getting a remote handle to it.
    
    This ensures that the future is destroyed and all moved objects
    within it are cleaned up before the result is sent via the remote
    handle.
    
    If packet receivers registered on one executor have their have
    their registration objects moved to another executor, this can
    cause race conditions.
    
    Imagine the following scenario:
    1. Thread `main` registers receiver against its executor.
    2. `main` moves the ReceiverRegistration object to new thread `foo`
       using Task::blocking()
    3. `main` waits on the Task for thread `foo`.
    4. The future completes but is not destroyed yet.
    5. `main` is woken from await.
    6. `main` attempts to exit and shutdown the executor.
    7. `main` executor panics because receiver is still registered.
    
    What should have happened:
    1. Thread `main` registers receiver against its executor.
    2. `main` moves the ReceiverRegistration object to new thread `foo`
       using Task::blocking()
    3. `main` waits on the Task for thread `foo`.
    4. The future completes and is destroyed. The ReceiverRegistration
       moved into `foo` is destroyed. The receiver is deregistered from
       `main` executor.
    5. `main` is woken from await.
    6. `main` attempts to exit and shutdown the executor.
    7. `main` exits gracefully.
    
    Multiply: blobfs-short: 1000
    Multiply: minfs-short: 1000
    Multiply: fuchsia-async-tests: 1000
    Fixed: 75370, 76387
    Test: fx test blobfs-crash
          fx test minfs-crash
          fx test fuchsia-async-tests
    Change-Id: Id6bce3b4a19f7be336e1ba346d3786923d91d56a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/532164
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Commit-Queue: Xyan Bhatnagar <xbhatnag@google.com>

--- src/lib/fuchsia-async/src/runtime/fuchsia/task.rs
+++ src/lib/fuchsia-async/src/runtime/fuchsia/task.rs
@@ -48,6 +48,13 @@ impl<T: Send> Task<T> {
     /// it completes, the returned `Task` is dropped while the executor is running,
     /// or the executor is destroyed, whichever comes first.
     pub fn spawn(future: impl Future<Output = T> + Send + 'static) -> Task<T> {
+        // Fuse is a combinator that will drop the underlying future as soon as it has been
+        // completed to ensure resources are reclaimed as soon as possible. That gives callers that
+        // await on the Task the guarantee that the future has been dropped.
+        //
+        // Note that it is safe to pass in a future that has already been fused. Double fusing
+        // a future does not change the expected behavior.
+        let future = future.fuse();
         let (future, remote_handle) = future.remote_handle();
         super::executor::spawn(future);
         Task { remote_handle }
@@ -57,6 +64,22 @@ impl<T: Send> Task<T> {
     /// TODO: Consider using a backing thread pool to alleviate the cost of spawning new threads
     /// if this proves to be a bottleneck.
     pub fn blocking(future: impl Future<Output = T> + Send + 'static) -> Task<T> {
+        // Fuse is a combinator that will drop the underlying future as soon as it has been
+        // completed to ensure resources are reclaimed as soon as possible. That gives callers that
+        // await on the Task the guarantee that the future has been dropped.
+        //
+        // This is especially important for the `blocking` call which starts up an executor in
+        // another thread.
+        //
+        // For example, if a receiver was registered on the main executor and its
+        // ReceiverRegistration object is moved into a Task that runs on a different executor,
+        // then the result of that Task should only be sent *after* the ReceiverRegistration object
+        // has been destroyed. Otherwise there can be a race between the main executor shutting
+        // down on one thread and the receiver being deregistered on another.
+        //
+        // Note that it is safe to pass in a future that has already been fused. Double fusing
+        // a future does not change the expected behavior.
+        let future = future.fuse();
         let (future, remote_handle) = future.remote_handle();
         std::thread::spawn(move || {
             super::executor::LocalExecutor::new().unwrap().run_singlethreaded(future)
@@ -68,6 +91,13 @@ impl<T: Send> Task<T> {
 impl<T> Task<T> {
     /// Spawn a new task on the thread local executor.
     pub fn local(future: impl Future<Output = T> + 'static) -> Task<T> {
+        // Fuse is a combinator that will drop the underlying future as soon as it has been
+        // completed to ensure resources are reclaimed as soon as possible. That gives callers that
+        // await on the Task the guarantee that the future has been dropped.
+        //
+        // Note that it is safe to pass in a future that has already been fused. Double fusing
+        // a future does not change the expected behavior.
+        let future = future.fuse();
         let (future, remote_handle) = future.remote_handle();
         super::executor::spawn_local(future);
         Task { remote_handle }
@@ -86,5 +116,77 @@ impl<T: 'static> Future for Task<T> {
     type Output = T;
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         self.remote_handle.poll_unpin(cx)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::executor::LocalExecutor;
+    use super::*;
+    use std::sync::{Arc, Mutex};
+
+    /// This struct holds a thread-safe mutable boolean and
+    /// sets its value to true when dropped.
+    #[derive(Clone)]
+    struct SetsBoolTrueOnDrop {
+        value: Arc<Mutex<bool>>,
+    }
+
+    impl SetsBoolTrueOnDrop {
+        fn new() -> (Self, Arc<Mutex<bool>>) {
+            let value = Arc::new(Mutex::new(false));
+            let sets_bool_true_on_drop = Self { value: value.clone() };
+            (sets_bool_true_on_drop, value)
+        }
+    }
+
+    impl Drop for SetsBoolTrueOnDrop {
+        fn drop(&mut self) {
+            let mut lock = self.value.lock().unwrap();
+            *lock = true;
+        }
+    }
+
+    #[test]
+    fn future_destroyed_before_await_returns() {
+        LocalExecutor::new().unwrap().run_singlethreaded(async {
+            let (sets_bool_true_on_drop, value) = SetsBoolTrueOnDrop::new();
+
+            // Move the switch into a future that runs on a different thread.
+            // Once we return from this await, that switch should have been dropped.
+            Task::blocking(async move {
+                let lock = sets_bool_true_on_drop.value.lock().unwrap();
+                assert_eq!(*lock, false);
+            })
+            .await;
+
+            // Switch moved into the future should have been dropped at this point.
+            // The value of the boolean should now be true.
+            let lock = value.lock().unwrap();
+            assert_eq!(*lock, true);
+        });
+    }
+
+    #[test]
+    fn fused_future_passed_into_task() {
+        LocalExecutor::new().unwrap().run_singlethreaded(async {
+            let (sets_bool_true_on_drop, value) = SetsBoolTrueOnDrop::new();
+
+            // The fused future passed in here gets double fused. This should not
+            // change the expected behavior.
+            Task::blocking(
+                async move {
+                    let lock = sets_bool_true_on_drop.value.lock().unwrap();
+                    assert_eq!(*lock, false);
+                }
+                .fuse(),
+            )
+            .await;
+
+            // Switch moved into the future should have been dropped at this point.
+            // The value of the boolean should now be true.
+            let lock = value.lock().unwrap();
+            assert_eq!(*lock, true);
+        });
     }
 }
