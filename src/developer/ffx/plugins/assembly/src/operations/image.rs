2b9c7dc20f031fdf3e3f6c442ba671a4b8d3db72
Aaron Wood <aaronwood@google.com>
2021-05-27 04:02:48 +0000

    [assembly] Add update package to build
    Hook up the creation of an update package to the build
    for all situations that contain a zbi.  If they aren't
    bootfs-only, then also validate that the created
    update package matches the one in the existing build.
    
    Bug: 76129
    Change-Id: I15a9c7457a50ed603cc560c2061efdf5e22dacbe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/535021
    Reviewed-by: Aidan Wolter <awolter@google.com>
    Commit-Queue: Aaron Wood <aaronwood@google.com>

--- src/developer/ffx/plugins/assembly/src/operations/image.rs
+++ src/developer/ffx/plugins/assembly/src/operations/image.rs
@@ -19,7 +19,7 @@ use vbmeta::Salt;
 use zbi::ZbiBuilder;
 
 pub fn assemble(args: ImageArgs) -> Result<()> {
-    let ImageArgs { product, board, outdir, gendir, full } = args;
+    let ImageArgs { product, board, outdir, gendir, full: _ } = args;
 
     info!("Loading configuration files.");
     info!("  product:  {}", product.display());
@@ -50,11 +50,6 @@ pub fn assemble(args: ImageArgs) -> Result<()> {
     } else {
         None
     };
-
-    // Bail out here for now, unless asked to do otherwise.
-    if !full {
-        return Ok(());
-    }
 
     info!("Creating the update package");
     let _update_pkg_path =
@@ -251,7 +246,10 @@ fn construct_update(
 
     if let Some(recovery_config) = &board.recovery {
         update_pkg_builder.add_file(&recovery_config.zbi, "zedboot")?;
-        update_pkg_builder.add_file(&recovery_config.vbmeta, "recovery.vbmeta")?;
+
+        if let Some(recovery_vbmeta) = &recovery_config.vbmeta {
+            update_pkg_builder.add_file(recovery_vbmeta, "recovery.vbmeta")?;
+        }
     }
 
     // Add the bootloaders.
