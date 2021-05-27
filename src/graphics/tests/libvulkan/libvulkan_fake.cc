bb5227ae577c0dc6f51347ec74fd3afe86d13163
John Bauman <jbauman@google.com>
2021-05-27 18:21:00 +0000

    [graphics] Re-enable loader-gpu-devices testing
    fake_vulkan_loader exposes the CONNECT_TO_DEVICE_FS feature, so the
    Vulkan loader can now support loader-gpu-devices in the libvulkan.so
    tests.
    
    Bug: 77112
    Change-Id: Ice9bb12a6bcea5dcb6e3aec331a6bb9ab7ba5646
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/535822
    Fuchsia-Auto-Submit: John Bauman <jbauman@google.com>
    Reviewed-by: John Rosasco <rosasco@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/graphics/tests/libvulkan/libvulkan_fake.cc
+++ src/graphics/tests/libvulkan/libvulkan_fake.cc
@@ -143,9 +143,6 @@ VKAPI_ATTR void VKAPI_CALL vk_icdInitializeOpenInNamespaceCallback(PFN_vkOpenInN
 
 VKAPI_ATTR __attribute__((visibility("default"))) void VKAPI_CALL
 vk_icdInitializeOpenInNamespaceCallback(PFN_vkOpenInNamespaceAddr open_in_namespace_addr) {
-  // Disable until loader with ConnectDeviceFs support has rolled.
-  // TODO(fxbug.dev/77112): Re-enable.
-#if defined(ENABLE_DEVICE_FS_TEST)
   zx::channel server_end, client_end;
   zx::channel::create(0, &server_end, &client_end);
 
@@ -169,7 +166,6 @@ vk_icdInitializeOpenInNamespaceCallback(PFN_vkOpenInNamespaceAddr open_in_namesp
     fprintf(stderr, "fdio_bind_to_fd failed\n");
     return;
   }
-#endif
 
   open_in_namespace_callback_initialized = true;
 }
