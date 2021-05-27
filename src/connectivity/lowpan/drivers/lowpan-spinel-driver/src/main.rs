86cec83387145b13453d377f0574af91b34c82bc
Parag Dixit <paragdixit@google.com>
2021-05-27 21:01:02 +0000

    [lowpan] Updated spinel-driver to use config file
    A default config file from /config/data/device_config.json is read
    to figure out config variables. Different config file can be used
    by command-line arguments.
    
    If config file doesn't exist, default values are loaded.
    If config file exists but doesn't have some config variables, default
    values are used.
    If command-line argument is specified, that overrides the config value.
    
    Tests: tested default and updated values through config file and
    command-line modification.
    
    Bug: 77542
    Change-Id: Iba7d14d948cb85e5d687d9496818ccc365cd45f3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/533103
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Robert Quattlebaum <rquattle@google.com>
    Reviewed-by: Jiaming (Charlie) Wang <jiamingw@google.com>
    Fuchsia-Auto-Submit: Robert Quattlebaum <rquattle@google.com>

--- src/connectivity/lowpan/drivers/lowpan-spinel-driver/src/main.rs
+++ src/connectivity/lowpan/drivers/lowpan-spinel-driver/src/main.rs
@@ -13,6 +13,10 @@ use spinel_pack::{self as spinel_pack};
 
 use std::fmt::Debug;
 
+use serde::Deserialize;
+use std::io;
+use std::path;
+
 mod driver;
 mod flow_window;
 mod spinel;
@@ -61,16 +65,22 @@ use futures::future::LocalBoxFuture;
 use lowpan_driver_common::{register_and_serve_driver, register_and_serve_driver_factory};
 
 /// This struct contains the arguments decoded from the command
-/// line invocation of the driver.
+/// line invocation of the driver. All values are optional. If an
+/// argument is provided, the value takes precedence over value in
+/// config file. The config file to be used can be passed through
+/// command-line or default config file is used.
 #[derive(argh::FromArgs, PartialEq, Debug)]
 struct DriverArgs {
     #[argh(
         option,
-        long = "serviceprefix",
-        description = "service namespace prefix (default: '/svc')",
-        default = "\"/svc\".to_string()"
+        long = "config-path",
+        description = "path to json config file which defines configuration options.",
+        default = "\"/config/data/device_config.json\".to_string()"
     )]
-    pub service_prefix: String,
+    pub config_path: String,
+
+    #[argh(option, long = "serviceprefix", description = "service namespace prefix")]
+    pub service_prefix: Option<String>,
 
     #[argh(switch, long = "otstack", description = "launch and connect to ot-stack")]
     pub use_ot_stack: bool,
@@ -78,21 +88,148 @@ struct DriverArgs {
     #[argh(
         option,
         long = "max-auto-restarts",
-        description = "maximum number of automatic restarts",
-        default = "10"
+        description = "maximum number of automatic restarts"
     )]
-    pub max_auto_restarts: u32,
+    pub max_auto_restarts: Option<u32>,
 
     #[argh(switch, long = "integration", description = "enable integration test mode")]
     pub is_integration_test: bool,
 
+    #[argh(option, long = "name", description = "name of interface")]
+    pub name: Option<String>,
+
+    #[argh(option, long = "ot-stack-url", description = "ot_stack_url to launch")]
+    pub ot_stack_url: Option<String>,
+
     #[argh(
         option,
-        long = "name",
-        description = "name of interface",
-        default = "\"lowpan0\".to_string()"
+        long = "ot-radio-path",
+        description = "path of ot-radio protocol device to connect to"
     )]
+    pub ot_radio_path: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    #[serde(default = "Config::default_service_prefix")]
+    pub service_prefix: String,
+
+    #[serde(default = "Config::default_use_ot_stack")]
+    pub use_ot_stack: bool,
+
+    #[serde(default = "Config::default_max_auto_restarts")]
+    pub max_auto_restarts: u32,
+
+    #[serde(default = "Config::default_is_integration_test")]
+    pub is_integration_test: bool,
+
+    #[serde(default = "Config::default_name")]
     pub name: String,
+
+    #[serde(default = "Config::default_ot_stack_url")]
+    pub ot_stack_url: String,
+
+    #[serde(default = "Config::default_ot_radio_path")]
+    pub ot_radio_path: String,
+}
+
+impl Config {
+    // Default values of config if not contained in the config file
+    fn default_service_prefix() -> String {
+        "/svc".to_string()
+    }
+    fn default_use_ot_stack() -> bool {
+        false
+    }
+    fn default_max_auto_restarts() -> u32 {
+        10
+    }
+    fn default_is_integration_test() -> bool {
+        false
+    }
+    fn default_name() -> String {
+        "lowpan0".to_string()
+    }
+    fn default_ot_radio_path() -> String {
+        "/dev/class/ot-radio/000".to_string()
+    }
+    fn default_ot_stack_url() -> String {
+        "fuchsia-pkg://fuchsia.com/ot-stack#meta/ot-stack.cmx".to_string()
+    }
+
+    // Returns default config struct:
+    pub fn default() -> Config {
+        Config {
+            service_prefix: Config::default_service_prefix(),
+            use_ot_stack: Config::default_use_ot_stack(),
+            max_auto_restarts: Config::default_max_auto_restarts(),
+            is_integration_test: Config::default_is_integration_test(),
+            name: Config::default_name(),
+            ot_radio_path: Config::default_ot_radio_path(),
+            ot_stack_url: Config::default_ot_stack_url(),
+        }
+    }
+
+    // Loads config variables with the deserialized contents of config file
+    pub fn load<P: AsRef<path::Path>>(path: P) -> Result<Self, Error> {
+        let path = path.as_ref();
+        let file = std::fs::File::open(path)
+            .with_context(|| format!("lowpan could not open the config file {}", path.display()))?;
+        let config = serde_json::from_reader(io::BufReader::new(file)).with_context(|| {
+            format!("lowpan could not deserialize the config file {}", path.display())
+        })?;
+        Ok(config)
+    }
+
+    // Override the config value with command-line option if it is passed
+    pub fn cmd_line_override(&mut self, args: DriverArgs) {
+        if let Some(tmp) = args.service_prefix {
+            fx_log_info!(
+                "cmdline overriding service_prefix from {} to {}",
+                self.service_prefix,
+                tmp
+            );
+            self.service_prefix = tmp;
+        }
+
+        if let Some(tmp) = args.max_auto_restarts {
+            fx_log_info!(
+                "cmdline overriding max_auto_restarts from {} to {}",
+                self.max_auto_restarts,
+                tmp
+            );
+            self.max_auto_restarts = tmp;
+        }
+
+        if let Some(tmp) = args.name {
+            fx_log_info!("cmdline overriding name from {} to {}", self.name, tmp);
+            self.name = tmp;
+        }
+
+        if let Some(tmp) = args.ot_stack_url {
+            fx_log_info!("cmdline overriding ot_stack_url from {} to {}", self.ot_stack_url, tmp);
+            self.ot_stack_url = tmp;
+        }
+
+        if let Some(tmp) = args.ot_radio_path {
+            fx_log_info!("cmdline overriding ot_radio_path from {} to {}", self.ot_radio_path, tmp);
+            self.ot_radio_path = tmp;
+        }
+
+        if args.use_ot_stack {
+            fx_log_info!("cmdline overriding use_ot_stack from {} to {}", self.use_ot_stack, true);
+            self.use_ot_stack = true;
+        }
+
+        if args.is_integration_test {
+            fx_log_info!(
+                "cmdline overriding is_integration_test from {} to {}",
+                self.is_integration_test,
+                true
+            );
+            self.is_integration_test = true;
+        }
+    }
 }
 
 async fn run_driver<N, RP, RFP, NI>(
@@ -191,9 +328,11 @@ async fn connect_to_spinel_device_proxy_hack() -> Result<(Option<App>, SpinelDev
     Ok((None, client_side.into_proxy()?))
 }
 
-fn connect_to_spinel_device_proxy() -> Result<(Option<App>, SpinelDeviceProxy), Error> {
-    let server_url = "fuchsia-pkg://fuchsia.com/ot-stack#meta/ot-stack.cmx".to_string();
-    let arg = Some(vec!["/dev/class/ot-radio/000".to_string()]);
+fn connect_to_spinel_device_proxy(
+    config: &Config,
+) -> Result<(Option<App>, SpinelDeviceProxy), Error> {
+    let server_url = config.ot_stack_url.clone();
+    let arg = Some(vec![config.ot_radio_path.clone()]);
     let launcher = launcher().expect("Failed to open launcher service");
     let app = launch(&launcher, server_url, arg).expect("Failed to launch ot-stack service");
     let ot_stack_proxy = app
@@ -210,32 +349,56 @@ fn connect_to_spinel_device_proxy_test() -> Result<(Option<App>, SpinelDevicePro
 
 #[allow(unused)]
 async fn prepare_to_run(
-    args: &DriverArgs,
+    config: &Config,
 ) -> Result<(Option<App>, LocalBoxFuture<'_, Result<(), Error>>), Error> {
-    let (app, spinel_device) = if args.is_integration_test {
+    let (app, spinel_device) = if config.is_integration_test {
         connect_to_spinel_device_proxy_test().context("connect_to_spinel_device_proxy_test")?
-    } else if args.use_ot_stack {
-        connect_to_spinel_device_proxy().context("connect_to_spinel_device_proxy")?
+    } else if config.use_ot_stack {
+        connect_to_spinel_device_proxy(config).context("connect_to_spinel_device_proxy")?
     } else {
         connect_to_spinel_device_proxy_hack()
             .await
             .context("connect_to_spinel_device_proxy_hack")?
     };
 
-    let network_device_interface = TunNetworkInterface::try_new(Some(args.name.clone()))
+    let network_device_interface = TunNetworkInterface::try_new(Some(config.name.clone()))
         .await
         .context("Unable to start TUN driver")?;
 
     let driver_future = run_driver(
-        args.name.clone(),
-        connect_to_protocol_at::<RegisterMarker>(args.service_prefix.as_str())
+        config.name.clone(),
+        connect_to_protocol_at::<RegisterMarker>(config.service_prefix.as_str())
             .context("Failed to connect to Lowpan Registry service")?,
-        connect_to_protocol_at::<FactoryRegisterMarker>(args.service_prefix.as_str()).ok(),
+        connect_to_protocol_at::<FactoryRegisterMarker>(config.service_prefix.as_str()).ok(),
         spinel_device,
         network_device_interface,
     );
 
     Ok((app, driver_future.boxed_local()))
+}
+
+fn process_config_files_and_args() -> Result<Config, Error> {
+    use std::path::Path;
+
+    let args: DriverArgs = argh::from_env();
+
+    let config_path = args.config_path.clone();
+
+    let mut config = if Path::new(&config_path).exists() {
+        fx_log_info!("Config file {} exists, will load config data from it", config_path);
+        Config::load(config_path)?
+    } else {
+        fx_log_info!("Config file {} doesn't exist, will use default values", config_path);
+        Config::default()
+    };
+
+    fx_log_debug!("Config values before cmd-line-override {:?} ", config);
+
+    config.cmd_line_override(args);
+
+    fx_log_debug!("Final config values are: {:?} ", config);
+
+    Ok(config)
 }
 
 #[fasync::run_singlethreaded]
@@ -249,11 +412,12 @@ async fn main() -> Result<(), Error> {
         return Ok(());
     }
 
-    let args: DriverArgs = argh::from_env();
+    let config = process_config_files_and_args()?;
+
     let mut attempt_count = 0;
 
     loop {
-        let (app, driver_future) = prepare_to_run(&args).await?;
+        let (app, driver_future) = prepare_to_run(&config).await?;
 
         let start_timestamp = fasync::Time::now();
 
@@ -279,7 +443,7 @@ async fn main() -> Result<(), Error> {
             attempt_count = 0;
         }
 
-        if args.max_auto_restarts <= attempt_count {
+        if config.max_auto_restarts <= attempt_count {
             break ret;
         }
 
@@ -299,7 +463,7 @@ async fn main() -> Result<(), Error> {
         fx_log_info!(
             "lowpan-spinel-driver restart attempt {} ({} max)",
             attempt_count,
-            args.max_auto_restarts
+            config.max_auto_restarts
         );
     }
 }
