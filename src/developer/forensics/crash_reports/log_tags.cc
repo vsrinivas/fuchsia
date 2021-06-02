4b640087fee45c83ab75cdf059e8c51dac0bac3f
Alex Pankhurst <pankhurst@google.com>
2021-06-02 02:26:22 +0000

    [forensics] Crash reports logs tags handle missing tags
    A nullptr was returned, leading to a potentially bad dereference. Now
    a pointer to an empty string is returned.
    
    Change-Id: I7fe41276abfa66c0968671f053e0fba37564feb2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/537006
    Fuchsia-Auto-Submit: Alex Pankhurst <pankhurst@google.com>
    Reviewed-by: Francois Rousseau <frousseau@google.com>
    Commit-Queue: Alex Pankhurst <pankhurst@google.com>

--- src/developer/forensics/crash_reports/log_tags.cc
+++ src/developer/forensics/crash_reports/log_tags.cc
@@ -9,6 +9,11 @@
 
 namespace forensics {
 namespace crash_reports {
+namespace {
+
+static constexpr const char* kEmptyTags = "";
+
+}
 
 void LogTags::Register(const ReportId report_id, const std::vector<std::string>& tags) {
   tags_[report_id] = fxl::StringPrintf("%s, %zu", fxl::JoinStrings(tags, ", ").c_str(), report_id);
@@ -17,7 +22,7 @@ void LogTags::Register(const ReportId report_id, const std::vector<std::string>&
 void LogTags::Unregister(const ReportId report_id) { tags_.erase(report_id); }
 
 const char* LogTags::Get(const ReportId report_id) {
-  return (tags_.find(report_id) != tags_.end()) ? tags_[report_id].c_str() : nullptr;
+  return (tags_.find(report_id) != tags_.end()) ? tags_[report_id].c_str() : kEmptyTags;
 }
 
 }  // namespace crash_reports
