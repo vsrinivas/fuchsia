f7ef949ad9a6359e55542a5be857253a0bc79170
Ben Lawson <benlawson@google.com>
2021-06-04 22:09:40 +0000

    [bt][bt-host] Replace ZX_DEBUG_ASSERTs in SocketChannelRelay with ZX_ASSERT
    To aid in debugging crashes in release mode, use ZX_ASSERT in SocketChannelRelay.
    
    Bug: 77407
    Test: fx test bt-host-socket-tests
    Change-Id: Ib697aba89bcc7a53b60e56133a8b068801de14c4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/536035
    Reviewed-by: Xo Wang <xow@google.com>
    Commit-Queue: Ben Lawson <benlawson@google.com>

--- src/connectivity/bluetooth/core/bt-host/socket/socket_channel_relay.h
+++ src/connectivity/bluetooth/core/bt-host/socket/socket_channel_relay.h
@@ -191,9 +191,9 @@ SocketChannelRelay<ChannelT>::SocketChannelRelay(zx::socket socket, fbl::RefPtr<
       socket_packet_sent_count_(0u),
       socket_packet_recv_count_(0u),
       weak_ptr_factory_(this) {
-  ZX_DEBUG_ASSERT(dispatcher_);
-  ZX_DEBUG_ASSERT(socket_);
-  ZX_DEBUG_ASSERT(channel_);
+  ZX_ASSERT(dispatcher_);
+  ZX_ASSERT(socket_);
+  ZX_ASSERT(channel_);
 
   // Note: binding |this| is safe, as BindWait() wraps the bound method inside
   // of a lambda which verifies that |this| hasn't been destroyed.
@@ -207,7 +207,7 @@ SocketChannelRelay<ChannelT>::SocketChannelRelay(zx::socket socket, fbl::RefPtr<
 
 template <typename ChannelT>
 SocketChannelRelay<ChannelT>::~SocketChannelRelay() {
-  ZX_DEBUG_ASSERT(thread_checker_.is_thread_valid());
+  ZX_ASSERT(thread_checker_.is_thread_valid());
 
   if (state_ != RelayState::kDeactivated) {
     bt_log(TRACE, "l2cap",
@@ -220,7 +220,7 @@ SocketChannelRelay<ChannelT>::~SocketChannelRelay() {
 
 template <typename ChannelT>
 bool SocketChannelRelay<ChannelT>::Activate() {
-  ZX_DEBUG_ASSERT(state_ == RelayState::kActivating);
+  ZX_ASSERT(state_ == RelayState::kActivating);
 
   // Note: we assume that BeginWait() does not synchronously dispatch any
   // events. The wait handler will assert otherwise.
@@ -264,7 +264,7 @@ bool SocketChannelRelay<ChannelT>::Activate() {
 
 template <typename ChannelT>
 void SocketChannelRelay<ChannelT>::Deactivate() {
-  ZX_DEBUG_ASSERT(state_ != RelayState::kDeactivated);
+  ZX_ASSERT(state_ != RelayState::kDeactivated);
 
   state_ = RelayState::kDeactivating;
   if (!socket_write_queue_.empty()) {
@@ -277,7 +277,7 @@ void SocketChannelRelay<ChannelT>::Deactivate() {
   // We assume that UnbindAndCancelWait() will not trigger a re-entrant call
   // into Deactivate(). And the RelayIsDestroyedWhenDispatcherIsShutDown test
   // verifies that to be the case. (If we had re-entrant calls, a
-  // ZX_DEBUG_ASSERT() in the lambda bound by BindWait() would cause an abort.)
+  // ZX_ASSERT() in the lambda bound by BindWait() would cause an abort.)
   UnbindAndCancelWait(&sock_read_waiter_);
   UnbindAndCancelWait(&sock_write_waiter_);
   UnbindAndCancelWait(&sock_close_waiter_);
@@ -302,7 +302,7 @@ void SocketChannelRelay<ChannelT>::DeactivateAndRequestDestruction() {
 
 template <typename ChannelT>
 void SocketChannelRelay<ChannelT>::OnSocketReadable(zx_status_t status) {
-  ZX_DEBUG_ASSERT(state_ == RelayState::kActivated);
+  ZX_ASSERT(state_ == RelayState::kActivated);
   if (!CopyFromSocketToChannel() || !BeginWait("socket read waiter", &sock_read_waiter_)) {
     DeactivateAndRequestDestruction();
   }
@@ -310,23 +310,23 @@ void SocketChannelRelay<ChannelT>::OnSocketReadable(zx_status_t status) {
 
 template <typename ChannelT>
 void SocketChannelRelay<ChannelT>::OnSocketWritable(zx_status_t status) {
-  ZX_DEBUG_ASSERT(state_ == RelayState::kActivated);
-  ZX_DEBUG_ASSERT(!socket_write_queue_.empty());
+  ZX_ASSERT(state_ == RelayState::kActivated);
+  ZX_ASSERT(!socket_write_queue_.empty());
   ServiceSocketWriteQueue();
 }
 
 template <typename ChannelT>
 void SocketChannelRelay<ChannelT>::OnSocketClosed(zx_status_t status) {
-  ZX_DEBUG_ASSERT(state_ == RelayState::kActivated);
+  ZX_ASSERT(state_ == RelayState::kActivated);
   DeactivateAndRequestDestruction();
 }
 
 template <typename ChannelT>
 void SocketChannelRelay<ChannelT>::OnChannelDataReceived(ByteBufferPtr rx_data) {
-  ZX_DEBUG_ASSERT(thread_checker_.is_thread_valid());
+  ZX_ASSERT(thread_checker_.is_thread_valid());
   // Note: kActivating is deliberately permitted, as ChannelImpl::Activate()
   // will synchronously deliver any queued frames.
-  ZX_DEBUG_ASSERT(state_ != RelayState::kDeactivated);
+  ZX_ASSERT(state_ != RelayState::kDeactivated);
   TRACE_DURATION("bluetooth", "SocketChannelRelay::OnChannelDataReceived", "channel id",
                  channel_->id());
 
@@ -336,13 +336,13 @@ void SocketChannelRelay<ChannelT>::OnChannelDataReceived(ByteBufferPtr rx_data) 
     return;
   }
 
-  ZX_DEBUG_ASSERT(rx_data);
+  ZX_ASSERT(rx_data);
   if (rx_data->size() == 0) {
     bt_log(DEBUG, "l2cap", "Ignoring empty %s on socket on channel %u", __func__, channel_->id());
     return;
   }
 
-  ZX_DEBUG_ASSERT(socket_write_queue_.size() <= socket_write_queue_max_frames_);
+  ZX_ASSERT(socket_write_queue_.size() <= socket_write_queue_max_frames_);
   // On a full queue, we drop the oldest element, on the theory that newer data
   // is more useful. This should be true, e.g., for real-time applications such
   // as voice calls. In the future, we may want to make the drop-head vs.
@@ -361,9 +361,9 @@ void SocketChannelRelay<ChannelT>::OnChannelDataReceived(ByteBufferPtr rx_data) 
 
 template <typename ChannelT>
 void SocketChannelRelay<ChannelT>::OnChannelClosed() {
-  ZX_DEBUG_ASSERT(thread_checker_.is_thread_valid());
-  ZX_DEBUG_ASSERT(state_ != RelayState::kActivating);
-  ZX_DEBUG_ASSERT(state_ != RelayState::kDeactivated);
+  ZX_ASSERT(thread_checker_.is_thread_valid());
+  ZX_ASSERT(state_ != RelayState::kActivating);
+  ZX_ASSERT(state_ != RelayState::kDeactivated);
 
   if (state_ == RelayState::kDeactivating) {
     bt_log(DEBUG, "l2cap", "Ignorning %s on socket for channel %u while deactivating", __func__,
@@ -371,7 +371,7 @@ void SocketChannelRelay<ChannelT>::OnChannelClosed() {
     return;
   }
 
-  ZX_DEBUG_ASSERT(state_ == RelayState::kActivated);
+  ZX_ASSERT(state_ == RelayState::kActivated);
   if (!socket_write_queue_.empty()) {
     ServiceSocketWriteQueue();
   }
@@ -393,11 +393,11 @@ bool SocketChannelRelay<ChannelT>::CopyFromSocketToChannel() {
   do {
     size_t n_bytes_read = 0;
     read_res = socket_.read(0, read_buf, read_buf_size, &n_bytes_read);
-    ZX_DEBUG_ASSERT_MSG(
+    ZX_ASSERT_MSG(
         read_res == ZX_OK || read_res == ZX_ERR_SHOULD_WAIT || read_res == ZX_ERR_PEER_CLOSED, "%s",
         zx_status_get_string(read_res));
-    ZX_DEBUG_ASSERT_MSG(n_bytes_read <= read_buf_size, "(n_bytes_read=%zu, read_buf_size=%zu)",
-                        n_bytes_read, read_buf_size);
+    ZX_ASSERT_MSG(n_bytes_read <= read_buf_size, "(n_bytes_read=%zu, read_buf_size=%zu)",
+                  n_bytes_read, read_buf_size);
     if (read_res == ZX_ERR_SHOULD_WAIT) {
       return true;
     }
@@ -408,7 +408,7 @@ bool SocketChannelRelay<ChannelT>::CopyFromSocketToChannel() {
       return false;
     }
 
-    ZX_DEBUG_ASSERT(n_bytes_read > 0);
+    ZX_ASSERT(n_bytes_read > 0);
     socket_packet_recv_count_++;
     if (n_bytes_read > channel_->max_tx_sdu_size()) {
       bt_log(TRACE, "l2cap", "Dropping %zu bytes for channel %u as max TX SDU is %u ", n_bytes_read,
@@ -441,13 +441,13 @@ void SocketChannelRelay<ChannelT>::ServiceSocketWriteQueue() {
   TRACE_DURATION("bluetooth", "SocketChannelRelay::ServiceSocketWriteQueue", "channel_id",
                  channel_->id());
   do {
-    ZX_DEBUG_ASSERT(!socket_write_queue_.empty());
-    ZX_DEBUG_ASSERT(socket_write_queue_.front());
+    ZX_ASSERT(!socket_write_queue_.empty());
+    ZX_ASSERT(socket_write_queue_.front());
     TRACE_DURATION("bluetooth", "SocketChannelRelay::ServiceSocketWriteQueue write", "channel_id",
                    channel_->id());
 
     const ByteBuffer& rx_data = *socket_write_queue_.front();
-    ZX_DEBUG_ASSERT_MSG(rx_data.size(), "Zero-length message on write queue");
+    ZX_ASSERT_MSG(rx_data.size(), "Zero-length message on write queue");
 
     socket_packet_sent_count_++;
     TRACE_FLOW_END("bluetooth", "SocketChannelRelay::OnChannelDataReceived queued",
@@ -458,18 +458,17 @@ void SocketChannelRelay<ChannelT>::ServiceSocketWriteQueue() {
 
     size_t n_bytes_written = 0;
     write_res = socket_.write(0, rx_data.data(), rx_data.size(), &n_bytes_written);
-    ZX_DEBUG_ASSERT_MSG(
+    ZX_ASSERT_MSG(
         write_res == ZX_OK || write_res == ZX_ERR_SHOULD_WAIT || write_res == ZX_ERR_PEER_CLOSED,
         "%s", zx_status_get_string(write_res));
     if (write_res != ZX_OK) {
-      ZX_DEBUG_ASSERT(n_bytes_written == 0);
+      ZX_ASSERT(n_bytes_written == 0);
       bt_log(TRACE, "l2cap", "Failed to write %zu bytes to socket for channel %u: %s",
              rx_data.size(), channel_->id(), zx_status_get_string(write_res));
       break;
     }
-    ZX_DEBUG_ASSERT_MSG(n_bytes_written == rx_data.size(),
-                        "(n_bytes_written=%zu, rx_data.size()=%zu)", n_bytes_written,
-                        rx_data.size());
+    ZX_ASSERT_MSG(n_bytes_written == rx_data.size(), "(n_bytes_written=%zu, rx_data.size()=%zu)",
+                  n_bytes_written, rx_data.size());
     socket_write_queue_.pop_front();
   } while (write_res == ZX_OK && !socket_write_queue_.empty());
 
@@ -512,12 +511,12 @@ void SocketChannelRelay<ChannelT>::BindWait(zx_signals_t trigger, const char* wa
                      expected_wait = wait, handler = std::move(handler)](
                         async_dispatcher_t* actual_dispatcher, async::WaitBase* actual_wait,
                         zx_status_t status, const zx_packet_signal_t* signal) {
-    ZX_DEBUG_ASSERT_MSG(self, "(%s, channel_id=%u)", wait_name, channel_id);
-    ZX_DEBUG_ASSERT_MSG(actual_dispatcher == self->dispatcher_, "(%s, channel_id=%u)", wait_name,
-                        channel_id);
-    ZX_DEBUG_ASSERT_MSG(actual_wait == expected_wait, "(%s, channel_id=%u)", wait_name, channel_id);
-    ZX_DEBUG_ASSERT_MSG(status == ZX_OK || status == ZX_ERR_CANCELED, "(%s, channel_id=%u)",
-                        wait_name, channel_id);
+    ZX_ASSERT_MSG(self, "(%s, channel_id=%u)", wait_name, channel_id);
+    ZX_ASSERT_MSG(actual_dispatcher == self->dispatcher_, "(%s, channel_id=%u)", wait_name,
+                  channel_id);
+    ZX_ASSERT_MSG(actual_wait == expected_wait, "(%s, channel_id=%u)", wait_name, channel_id);
+    ZX_ASSERT_MSG(status == ZX_OK || status == ZX_ERR_CANCELED, "(%s, channel_id=%u)", wait_name,
+                  channel_id);
 
     if (status == ZX_ERR_CANCELED) {  // Dispatcher is shutting down.
       bt_log(DEBUG, "l2cap", "%s canceled on socket for channel %u", wait_name, channel_id);
@@ -525,15 +524,15 @@ void SocketChannelRelay<ChannelT>::BindWait(zx_signals_t trigger, const char* wa
       return;
     }
 
-    ZX_DEBUG_ASSERT_MSG(signal, "(%s, channel_id=%u)", wait_name, channel_id);
-    ZX_DEBUG_ASSERT_MSG(signal->trigger == expected_wait->trigger(), "(%s, channel_id=%u)",
-                        wait_name, channel_id);
-    ZX_DEBUG_ASSERT_MSG(self->thread_checker_.is_thread_valid(), "(%s, channel_id=%u)", wait_name,
-                        channel_id);
-    ZX_DEBUG_ASSERT_MSG(self->state_ != RelayState::kActivating, "(%s, channel_id=%u)", wait_name,
-                        channel_id);
-    ZX_DEBUG_ASSERT_MSG(self->state_ != RelayState::kDeactivated, "(%s, channel_id=%u)", wait_name,
-                        channel_id);
+    ZX_ASSERT_MSG(signal, "(%s, channel_id=%u)", wait_name, channel_id);
+    ZX_ASSERT_MSG(signal->trigger == expected_wait->trigger(), "(%s, channel_id=%u)", wait_name,
+                  channel_id);
+    ZX_ASSERT_MSG(self->thread_checker_.is_thread_valid(), "(%s, channel_id=%u)", wait_name,
+                  channel_id);
+    ZX_ASSERT_MSG(self->state_ != RelayState::kActivating, "(%s, channel_id=%u)", wait_name,
+                  channel_id);
+    ZX_ASSERT_MSG(self->state_ != RelayState::kDeactivated, "(%s, channel_id=%u)", wait_name,
+                  channel_id);
 
     if (self->state_ == RelayState::kDeactivating) {
       bt_log(DEBUG, "l2cap", "Ignorning %s on socket for channel %u while deactivating", wait_name,
@@ -546,15 +545,15 @@ void SocketChannelRelay<ChannelT>::BindWait(zx_signals_t trigger, const char* wa
 
 template <typename ChannelT>
 bool SocketChannelRelay<ChannelT>::BeginWait(const char* wait_name, async::Wait* wait) {
-  ZX_DEBUG_ASSERT(state_ != RelayState::kDeactivating);
-  ZX_DEBUG_ASSERT(state_ != RelayState::kDeactivated);
+  ZX_ASSERT(state_ != RelayState::kDeactivating);
+  ZX_ASSERT(state_ != RelayState::kDeactivated);
 
   if (wait->is_pending()) {
     return true;
   }
 
   zx_status_t wait_res = wait->Begin(dispatcher_);
-  ZX_DEBUG_ASSERT(wait_res == ZX_OK || wait_res == ZX_ERR_BAD_STATE);
+  ZX_ASSERT(wait_res == ZX_OK || wait_res == ZX_ERR_BAD_STATE);
 
   if (wait_res != ZX_OK) {
     bt_log(ERROR, "l2cap", "Failed to enable waiting on %s: %s", wait_name,
@@ -567,13 +566,13 @@ bool SocketChannelRelay<ChannelT>::BeginWait(const char* wait_name, async::Wait*
 
 template <typename ChannelT>
 void SocketChannelRelay<ChannelT>::UnbindAndCancelWait(async::Wait* wait) {
-  ZX_DEBUG_ASSERT(state_ != RelayState::kActivating);
-  ZX_DEBUG_ASSERT(state_ != RelayState::kDeactivated);
+  ZX_ASSERT(state_ != RelayState::kActivating);
+  ZX_ASSERT(state_ != RelayState::kDeactivated);
   zx_status_t cancel_res;
   wait->set_handler(nullptr);
   cancel_res = wait->Cancel();
-  ZX_DEBUG_ASSERT_MSG(cancel_res == ZX_OK || cancel_res == ZX_ERR_NOT_FOUND, "Cancel failed: %s",
-                      zx_status_get_string(cancel_res));
+  ZX_ASSERT_MSG(cancel_res == ZX_OK || cancel_res == ZX_ERR_NOT_FOUND, "Cancel failed: %s",
+                zx_status_get_string(cancel_res));
 }
 
 template <typename ChannelT>
