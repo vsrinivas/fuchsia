d6cac20cb5294105270440cc74bae1d67ed20bdc
Kevin Wells <kevinwells@google.com>
2021-06-07 21:13:29 +0000

    [pkg-cache] Refactor enumerate_package_blobs
    This change moves the dynamic_index's enumerate_package_blobs into
    index.rs to make it more clear that it is shared between the dynamic and
    retained indices. No functional changes expected.
    
    Bug: 77361
    Change-Id: Ic79cfaa8b66aca94baf2b1e89b769346a8d3c904
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/539723
    Commit-Queue: Kevin Wells <kevinwells@google.com>
    Fuchsia-Auto-Submit: Kevin Wells <kevinwells@google.com>
    Reviewed-by: Zach Kirschenbaum <zkbaum@google.com>

--- src/sys/pkg/bin/pkg-cache/src/index.rs
+++ src/sys/pkg/bin/pkg-cache/src/index.rs
@@ -4,13 +4,77 @@
 
 //! Types to track and manage indices of packages.
 
+use {
+    fuchsia_merkle::Hash,
+    fuchsia_pkg::{MetaContents, MetaPackage, PackagePath},
+    std::collections::HashSet,
+};
+
 // TODO(fxbug.dev/77361) use this module, remove this allow.
 mod dynamic;
 mod package;
 #[allow(dead_code)]
 mod retained;
 
-pub use dynamic::{
-    enumerate_package_blobs, fulfill_meta_far_blob, load_cache_packages, DynamicIndex,
-    DynamicIndexError,
-};
+pub use dynamic::{fulfill_meta_far_blob, load_cache_packages, DynamicIndex, DynamicIndexError};
+
+/// Parses the meta far blob, if it exists in blobfs, returning the package path in meta/package and
+/// the set of all content blobs specified in meta/contents.
+pub async fn enumerate_package_blobs(
+    blobfs: &blobfs::Client,
+    meta_hash: &Hash,
+) -> Result<Option<(PackagePath, HashSet<Hash>)>, DynamicIndexError> {
+    let file = match blobfs.open_blob_for_read(&meta_hash).await {
+        Ok(file) => file,
+        Err(io_util::node::OpenError::OpenError(fuchsia_zircon::Status::NOT_FOUND)) => {
+            return Ok(None)
+        }
+        Err(e) => return Err(DynamicIndexError::OpenBlob(e)),
+    };
+
+    let mut meta_far =
+        fuchsia_archive::AsyncReader::new(io_util::file::AsyncFile::from_proxy(file)).await?;
+    let meta_package = MetaPackage::deserialize(&meta_far.read_file("meta/package").await?[..])?;
+    let meta_contents = MetaContents::deserialize(&meta_far.read_file("meta/contents").await?[..])?;
+
+    Ok(Some((meta_package.into_path(), meta_contents.into_hashes().collect::<HashSet<_>>())))
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*, crate::test_utils::add_meta_far_to_blobfs, fuchsia_async as fasync,
+        maplit::hashset,
+    };
+
+    #[fasync::run_singlethreaded(test)]
+    async fn enumerate_package_blobs_and_meta_far_exists() {
+        let meta_far_hash = Hash::from([2; 32]);
+        let path = PackagePath::from_name_and_variant("fake-package", "0").unwrap();
+
+        let (blobfs_fake, blobfs) = fuchsia_pkg_testing::blobfs::Fake::new();
+
+        let blob_hash0 = Hash::from([3; 32]);
+        let blob_hash1 = Hash::from([4; 32]);
+        add_meta_far_to_blobfs(
+            &blobfs_fake,
+            meta_far_hash,
+            "fake-package",
+            vec![blob_hash0, blob_hash1],
+        );
+
+        let res = enumerate_package_blobs(&blobfs, &meta_far_hash).await.unwrap();
+
+        assert_eq!(res, Some((path, hashset! {blob_hash0, blob_hash1})));
+    }
+
+    #[fasync::run_singlethreaded(test)]
+    async fn enumerate_package_blobs_and_missing_meta_far() {
+        let meta_far_hash = Hash::from([2; 32]);
+        let (_blobfs_fake, blobfs) = fuchsia_pkg_testing::blobfs::Fake::new();
+
+        let res = enumerate_package_blobs(&blobfs, &meta_far_hash).await.unwrap();
+
+        assert_eq!(res, None);
+    }
+}
--- src/sys/pkg/bin/pkg-cache/src/index/dynamic.rs
+++ src/sys/pkg/bin/pkg-cache/src/index/dynamic.rs
@@ -6,7 +6,7 @@ use {
     anyhow::{Context as _, Error},
     fuchsia_inspect as finspect,
     fuchsia_merkle::Hash,
-    fuchsia_pkg::{MetaContents, MetaPackage, PackagePath},
+    fuchsia_pkg::PackagePath,
     fuchsia_syslog::{fx_log_err, fx_log_warn},
     fuchsia_zircon as zx,
     futures::lock::Mutex,
@@ -254,35 +254,13 @@ pub async fn fulfill_meta_far_blob(
         });
     }
 
-    let (path, required_blobs) = enumerate_package_blobs(blobfs, &blob_hash)
+    let (path, required_blobs) = crate::index::enumerate_package_blobs(blobfs, &blob_hash)
         .await?
         .ok_or_else(|| DynamicIndexError::BlobNotFound(blob_hash))?;
 
     index.lock().await.add_package(blob_hash, Package::WithMetaFar { path, required_blobs });
 
     Ok(())
-}
-
-/// Parses the meta far blob, if it exists in blobfs, returning the package path in meta/package and
-/// the set of all content blobs specified in meta/contents.
-pub async fn enumerate_package_blobs(
-    blobfs: &blobfs::Client,
-    meta_hash: &Hash,
-) -> Result<Option<(PackagePath, HashSet<Hash>)>, DynamicIndexError> {
-    let file = match blobfs.open_blob_for_read(&meta_hash).await {
-        Ok(file) => file,
-        Err(io_util::node::OpenError::OpenError(fuchsia_zircon::Status::NOT_FOUND)) => {
-            return Ok(None)
-        }
-        Err(e) => return Err(DynamicIndexError::OpenBlob(e)),
-    };
-
-    let mut meta_far =
-        fuchsia_archive::AsyncReader::new(io_util::file::AsyncFile::from_proxy(file)).await?;
-    let meta_package = MetaPackage::deserialize(&meta_far.read_file("meta/package").await?[..])?;
-    let meta_contents = MetaContents::deserialize(&meta_far.read_file("meta/contents").await?[..])?;
-
-    Ok(Some((meta_package.into_path(), meta_contents.into_hashes().collect::<HashSet<_>>())))
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -727,36 +705,5 @@ mod tests {
             fulfill_meta_far_blob(&dynamic_index, &blobfs, meta_far_hash).await,
             Err(DynamicIndexError::BlobNotFound(hash)) if hash == meta_far_hash
         );
-    }
-
-    #[fasync::run_singlethreaded(test)]
-    async fn enumerate_package_blobs_and_meta_far_exists() {
-        let meta_far_hash = Hash::from([2; 32]);
-        let path = PackagePath::from_name_and_variant("fake-package", "0").unwrap();
-
-        let (blobfs_fake, blobfs) = fuchsia_pkg_testing::blobfs::Fake::new();
-
-        let blob_hash0 = Hash::from([3; 32]);
-        let blob_hash1 = Hash::from([4; 32]);
-        add_meta_far_to_blobfs(
-            &blobfs_fake,
-            meta_far_hash,
-            "fake-package",
-            vec![blob_hash0, blob_hash1],
-        );
-
-        let res = enumerate_package_blobs(&blobfs, &meta_far_hash).await.unwrap();
-
-        assert_eq!(res, Some((path, hashset! {blob_hash0, blob_hash1})));
-    }
-
-    #[fasync::run_singlethreaded(test)]
-    async fn enumerate_package_blobs_and_missing_meta_far() {
-        let meta_far_hash = Hash::from([2; 32]);
-        let (_blobfs_fake, blobfs) = fuchsia_pkg_testing::blobfs::Fake::new();
-
-        let res = enumerate_package_blobs(&blobfs, &meta_far_hash).await.unwrap();
-
-        assert_eq!(res, None);
     }
 }
