259720587a18d4f99dc7644e4d4b36112f94340b
John Sheu <sheu@google.com>
2021-06-07 18:50:44 +0000

    [ycm] Update paths.py script to python3
    Change-Id: Ie0d44573697838f7bfd7024ae3d6f3310b215c60
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/539582
    Reviewed-by: Francois Rousseau <frousseau@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: John Sheu <sheu@google.com>

--- scripts/youcompleteme/paths.py
+++ scripts/youcompleteme/paths.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 # vim: set expandtab:ts=2:sw=2
 # Copyright 2016 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import os
 import platform
