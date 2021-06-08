a732f25f8840d3d25bf79ad26df07a605dba097f
Austin Foxley <afoxley@google.com>
2021-06-08 23:01:03 +0000

    [bt][a2dp] Add some logging to a2dp-loopback util
    Also convert to fuchsia_shell_package build template. This is for flake
    debugging in CQ as a local repro isn't happening.
    
    Bug: 74461
    Multiply: bt_a2dp_e2e_test
    Change-Id: Ideb5e008bce9caf799103d0aeadc337ebb9f2612
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/539669
    Fuchsia-Auto-Submit: Austin Foxley <afoxley@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Ani Ramakrishnan <aniramakri@google.com>

--- src/connectivity/bluetooth/profiles/tests/bt-a2dp-loopback/src/main.rs
+++ src/connectivity/bluetooth/profiles/tests/bt-a2dp-loopback/src/main.rs
@@ -5,7 +5,7 @@
 use {
     anyhow::Error, argh::FromArgs, bt_profile_test_server_client::ProfileTestHarness,
     fidl_fuchsia_bluetooth_bredr::LaunchInfo, fuchsia_async as fasync,
-    fuchsia_bluetooth::types::PeerId, fuchsia_zircon as zx, futures::future,
+    fuchsia_bluetooth::types::PeerId, fuchsia_zircon as zx, futures::future, log::info,
     matches::assert_matches, std::convert::TryInto,
 };
 
@@ -25,14 +25,18 @@ struct Opt {
 async fn main() -> Result<(), Error> {
     let opts: Opt = argh::from_env();
     fuchsia_syslog::init_with_tags(&["a2dp-loopback"])?;
+    info!("Starting a2dp-loopback");
+
     let test_harness = ProfileTestHarness::new().expect("Failed to create profile test harness");
 
+    info!("Create mock peer #1");
     // Create MockPeer #1 for source.
     let id1 = PeerId(1);
     let mock_peer1 = test_harness.register_peer(id1).await?;
     let launch_info = LaunchInfo { component_url: Some(A2DP_URL.to_string()), ..LaunchInfo::EMPTY };
     assert_matches!(mock_peer1.launch_profile(launch_info).await, Ok(()));
 
+    info!("Create mock peer #2");
     // Create MockPeer #2 for sink, but disable the auto connect so they don't collide
     let id2 = PeerId(8);
     let mock_peer2 = test_harness.register_peer(id2).await?;
@@ -44,11 +48,13 @@ async fn main() -> Result<(), Error> {
     assert_matches!(mock_peer2.launch_profile(launch_info).await, Ok(()));
 
     if opts.duration > 0 {
+        info!("Streaming for duration {}", opts.duration);
         fasync::Timer::new(fasync::Time::after(zx::Duration::from_seconds(
             opts.duration.try_into()?,
         )))
         .await;
     } else {
+        info!("Streaming forever...");
         future::pending::<()>().await;
     }
     Ok(())
