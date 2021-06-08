37e59c5f88ef3b76b9d58d4eea2b7d550657ff31
Aaron Wood <aaronwood@google.com>
2021-06-08 21:56:09 +0000

    [build][ffx] disable ffx metrics for build actions
    This change disables the collection and reporting of metrics by ffx
    for all build actions.
    
    Fixes: 78295
    Change-Id: I5caba6bc3cfe4d0bc9f61871eb4a74c6d5c993ac
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/539871
    Fuchsia-Auto-Submit: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Matthew Boetger <boetger@google.com>
    Reviewed-by: Bob Evans <bobevans@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/ffx/build/make_ffx_env.py
+++ src/developer/ffx/build/make_ffx_env.py
@@ -7,14 +7,24 @@ import argparse
 import json
 import sys
 
+
 def main():
     parser = argparse.ArgumentParser(description='Make empty ffx environment')
-    parser.add_argument("file", type=argparse.FileType('w'))
+    parser.add_argument("env_file", type=argparse.FileType('w'))
+    parser.add_argument("build_config", type=argparse.FileType('w'))
     args = parser.parse_args()
 
-    ffx_env = {}
-    json.dump(ffx_env, args.file)
+    ffx_env = {
+        "user": "",
+        "build": "",
+        "global": args.build_config.name,
+    }
+
+    ffx_build_config = {"ffx": {"analytics": {"disabled": True}}}
+
+    json.dump(ffx_build_config, args.build_config, indent=2)
+    json.dump(ffx_env, args.env_file, indent=2)
+
 
 if __name__ == '__main__':
     sys.exit(main())
-
