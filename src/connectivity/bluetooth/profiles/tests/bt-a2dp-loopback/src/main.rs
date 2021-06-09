28e030a68ba6f6f5b9340ac54c2e50b19a31f060
Austin Foxley <afoxley@google.com>
2021-06-09 22:51:15 +0000

    [bt][a2dp-loopback] Wait for mock peers to connect before streaming wait
    Since it can take some time for the profiles and all their dependencies
    to launch (especially in CQ), we shouldn't start waiting for streaming
    until the mock peers actually connect to each other.
    
    Test: bt_a2dp_e2e_test
    Multiply: bt_a2dp_e2e_test
    Bug: 74461
    Change-Id: I8569b699b7b230a470c2a2ba11c07956e056b106
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/540784
    Fuchsia-Auto-Submit: Austin Foxley <afoxley@google.com>
    Reviewed-by: Ani Ramakrishnan <aniramakri@google.com>
    Commit-Queue: Austin Foxley <afoxley@google.com>

--- src/connectivity/bluetooth/profiles/tests/bt-a2dp-loopback/src/main.rs
+++ src/connectivity/bluetooth/profiles/tests/bt-a2dp-loopback/src/main.rs
@@ -3,10 +3,17 @@
 // found in the LICENSE file.
 
 use {
-    anyhow::Error, argh::FromArgs, bt_profile_test_server_client::ProfileTestHarness,
-    fidl_fuchsia_bluetooth_bredr::LaunchInfo, fuchsia_async as fasync,
-    fuchsia_bluetooth::types::PeerId, fuchsia_zircon as zx, futures::future, log::info,
-    matches::assert_matches, std::convert::TryInto,
+    anyhow::{format_err, Error},
+    argh::FromArgs,
+    bt_profile_test_server_client::ProfileTestHarness,
+    fidl_fuchsia_bluetooth_bredr::*,
+    fuchsia_async as fasync,
+    fuchsia_bluetooth::types::PeerId,
+    fuchsia_zircon as zx,
+    futures::future,
+    log::info,
+    matches::assert_matches,
+    std::convert::TryInto,
 };
 
 /// A2DP component URL.
@@ -39,13 +46,32 @@ async fn main() -> Result<(), Error> {
     info!("Create mock peer #2");
     // Create MockPeer #2 for sink, but disable the auto connect so they don't collide
     let id2 = PeerId(8);
-    let mock_peer2 = test_harness.register_peer(id2).await?;
+    let mut mock_peer2 = test_harness.register_peer(id2).await?;
     let launch_info = LaunchInfo {
         component_url: Some(A2DP_URL.to_string()),
         arguments: Some(vec!["--initiator-delay".to_string(), "0".to_string()]),
         ..LaunchInfo::EMPTY
     };
     assert_matches!(mock_peer2.launch_profile(launch_info).await, Ok(()));
+
+    // Loop till peer connects
+    loop {
+        match mock_peer2.expect_observer_request().await? {
+            PeerObserverRequest::PeerConnected { peer_id, responder, .. } => {
+                info!("Peer #1 connected to Peer #2");
+                assert_eq!(id1, peer_id.into());
+                responder.send()?;
+                break;
+            }
+            PeerObserverRequest::ServiceFound { peer_id, responder, .. } => {
+                assert_eq!(id1, peer_id.into());
+                responder.send()?;
+            }
+            x => {
+                return Err(format_err!("Expected PeerConnected or ServiceFound but got: {:?}", x))
+            }
+        }
+    }
 
     if opts.duration > 0 {
         info!("Streaming for duration {}", opts.duration);
