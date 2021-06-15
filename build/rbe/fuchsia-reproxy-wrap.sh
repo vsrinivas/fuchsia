cba85836652512444f185859e27c6dde33789e61
David Fang <fangism@google.com>
2021-06-15 17:58:00 +0000

    [rbe] auto start/stop reproxy around any command
    This can be used to test RBE, while having tool commands
    use a simplified wrapper that doesn't repeatedly start and stop the
    reproxy.
    
    usage:
      [modify build/toolchain/clang_toolchain.gni: ]
    
      build/rbe/fuchsia-reproxy-wrap.sh -- fx build
    
    Eventually, this will be integrated into fx itself.
    
    Tested:
     ./build/rbe/fuchsia-reproxy-wrap.sh -- sleep 15s &
     kill -{INT,HUP,TERM} [pid]
    
    Change-Id: Ic683e6bbe4abce5e7d2ed0433b768f5410933cd4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/542684
    Commit-Queue: David Fang <fangism@google.com>
    Reviewed-by: David Turner <digit@google.com>

--- build/rbe/fuchsia-reproxy-wrap.sh
+++ build/rbe/fuchsia-reproxy-wrap.sh
@@ -13,34 +13,26 @@ script_dir="$(dirname "$script")"
 # defaults
 config="$script_dir"/fuchsia-re-client.cfg
 # location of reclient binaries relative to output directory where build is run
-# TODO(fangism): this could also be set relative to --exec_root.
 reclient_bindir="$script_dir"/../../prebuilt/proprietary/third_party/reclient/linux-x64
-auto_reproxy="$script_dir"/fuchsia-reproxy-wrap.sh
 
 usage() {
   cat <<EOF
-$script [rewrapper options] -- command [args...]
+$script [reproxy options] -- command [args...]
 
-This script runs a command remotely (RBE) using rewrapper.
+This script runs reproxy around another command(s).
+reproxy is a proxy process between reclient and a remote back-end (RBE).
+It needs to be running to host rewrapper commands, which are invoked
+by a build system like 'make' or 'ninja'.
 
-example: $script -- echo Hello
+example:
+  $script -- ninja
 
 options:
-  --cfg FILE: reclient config for both reproxy and rewrapper tools
-      [default: $config]
+  --cfg FILE: reclient config for reproxy and rewrapper tools
   --bindir DIR: location of reproxy and rewrapper tools
-      [default: $reclient_bindir]
-
-  --auto-reproxy: startup and shutdown reproxy around the command.
-  --no-reproxy: assume reproxy is already running, and only use rewrapper.
-      [This is the default.]
-
-  All other options are forwarded to rewrapper until -- is encountered.
 EOF
 }
 
-rewrapper_options=()
-want_auto_reproxy=0
 # Extract script options before --
 for opt
 do
@@ -62,12 +54,8 @@ do
     --cfg) prev_opt=config ;;
     --bindir=*) reclient_bindir="$optarg" ;;
     --bindir) prev_opt=reclient_bindir ;;
-    --auto-reproxy) want_auto_reproxy=1 ;;
-    --no-reproxy) want_auto_reproxy=0 ;;
     # stop option processing
     --) shift; break ;;
-    # Forward all other options to rewrapper
-    *) rewrapper_options=("${rewrapper_options[@]}" "$opt")
   esac
   shift
 done
@@ -76,17 +64,19 @@ test -z "$prev_out" || { echo "Option is missing argument to set $prev_opt." ; e
 reproxy_cfg="$config"
 rewrapper_cfg="$config"
 
-rewrapper="$reclient_bindir"/rewrapper
+bootstrap="$reclient_bindir"/bootstrap
+reproxy="$reclient_bindir"/reproxy
 
-# command is in "$@"
-rewrapped_command=("$rewrapper" --cfg="$rewrapper_cfg" "${rewrapper_options[@]}" "$@")
+# Use the same config for bootstrap as for reproxy
+"$bootstrap" --re_proxy="$reproxy" --cfg="$reproxy_cfg"
+# b/188923283 -- added --cfg to shut down properly
+shutdown() {
+  "$bootstrap" --shutdown --cfg="$reproxy_cfg"
+}
 
-if test "$want_auto_reproxy" = 1
-then
-  # startup and stop reproxy around this single command
-  "$auto_reproxy" --cfg="$reproxy_cfg" -- "${rewrapped_command[@]}"
-else
-  # reproxy is already running
-  "${rewrapped_command[@]}"
-fi
+# EXIT also covers INT
+trap shutdown EXIT
 
+# original command is in "$@"
+# Do not 'exec' this, so that trap takes effect.
+"$@"
