460ad247190d8579017c9fc7a5a3ecd901bb399f
Roland McGrath <mcgrathr@google.com>
2021-06-17 19:31:11 +0000

    [build] Consolidate vboot details
    This consolidates all the knowledge of what goes into ChromeOS
    "vboot" image signing into variables in vboot.gni.  The vboot()
    template there and the assembly configuration will now both use
    the same wrapper script with the same parameters.
    
    A new `vboot_verbose` build option is added to allow printing out
    the exact `futility` command the wrapper script is running.
    
    Bug: 78896
    Change-Id: I8047919ced126f5a8e62f95a87fe5d04d66ba5e3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/544261
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

--- build/images/vboot/vboot_sign.py
+++ build/images/vboot/vboot_sign.py
@@ -3,59 +3,61 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""Perform a ChromeOS-style vboot signing.
+
+This converts a ZBI into a "kernel partition" for ChromeOS targets.
+See vboot.gni, which supplies most of the arguments used here.
+
+The -z switch for the input ZBI and the -o switch for the output image
+are a fixed protocol with the assembly tools.  The rest of the arguments
+are just piped through from the settings found in vboot.gni:vboot_action.
+
+This wrapper script only needs to exist because the assembly tools always use
+-z and -o for the input and output files.  The assembly configuration adds
+arbitrary additional arguments to the script, but those two switches are always
+passed implicitly.  If those tools were changed to accept switches to say what
+switches to use for those, vboot.gni could give sufficient details for the
+//build/images/assembly/BUILD.gn code to parameterize assembly configuration to
+run the `futility` tool directly without Python.
+
+"""
+
 import argparse
+import shlex
 import subprocess
 import sys
 
-#####
-#  Perform a ChromeOS style vboot signing
-#
-#  This converts a zbi into a "kernel partition" for ChromeOS targets.
-#
-#  Params:
-#  -z  => path to the zbi
-#  -o  => output path to write to
-#  -B  => build working directory
-
-VBOOT_DIR = "../../third_party/vboot_reference"
-KERNEL_KEYBLOCK = VBOOT_DIR + "/tests/devkeys/kernel.keyblock"
-PRIVATE_KEYBLOCK = VBOOT_DIR + "/tests/devkeys/kernel_data_key.vbprivk"
-
 
 def main():
-    parser = argparse.ArgumentParser(description='vboot sign a ZBI')
-    parser.add_argument("-z", required=True, help="path the ZBI to sign.")
-    parser.add_argument(
-        "-o", required=True, help="path to write the signed kernel image to.")
-    parser.add_argument(
-        "-B", help="path to the build root, which all paths are relative to.")
-    parser.add_argument("--host-tool", required=True)
-    parser.add_argument("--kernel-keyblock", required=True)
-    parser.add_argument("--private-keyblock", required=True)
-    parser.add_argument("--multiboot", required=True)
+    parser = argparse.ArgumentParser(description="Sign a ZBI as a vboot image")
+    parser.add_argument("--zbi", "-z", required=True, metavar="FILE.zbi",
+                        help="path the ZBI to sign.")
+    parser.add_argument("--output", "-o", required=True, metavar="FILE",
+                        help="path to write the signed kernel image to.")
+    parser.add_argument("--tool", required=True, metavar="FILE",
+                        help="Path to signing tool (futility)")
+    parser.add_argument("--args", required=True, action="append",
+                        metavar="ARG", help="Initial arguments to the tool")
+    parser.add_argument("--input-args", required=True, action="append",
+                        metavar="ARG", help="Arguments preceding input file")
+    parser.add_argument("--output-args", required=True, action="append",
+                        metavar="ARG", help="Arguments preceding output file")
+    parser.add_argument("--verbose", action="store_true",
+                        help="Display the command being run")
+    parser.add_argument("--quiet", dest="verbose", action="store_false",
+                        help="Don't display the command being run")
     args = parser.parse_args()
 
-    subprocess.run(
-        [
-            args.host_tool,
-            "vbutil_kernel",
-            "--pack",
-            args.o,
-            "--keyblock",
-            args.kernel_keyblock,
-            "--signprivate",
-            args.private_keyblock,
-            "--bootloader",
-            args.z,
-            "--vmlinuz",
-            args.multiboot,
-            "--version",
-            "1",
-            "--flags",
-            "0x2",
-        ],
-        check=True)
+    cmd = ([ args.tool ] + args.args +
+           args.output_args + [ args.output ] +
+           args.input_args + [ args.zbi ])
+
+    if args.verbose:
+        sys.stderr.write("+ {cmd}\n".format(
+            cmd=" ".join([shlex.quote(arg) for arg in cmd])))
+
+    subprocess.run(cmd, check=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
