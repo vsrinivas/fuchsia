1639cf72e24a11db27596d9a8e37d2084ad8603f
Yifei Teng <yifeit@google.com>
2021-06-18 09:46:58 +0000

    [fidl][llcpp] Reformat WireUnownedResult template
    The template used tabs instead of spaces which renders it differently
    from all the rest of the templates. C++ coding style also prefers
    spaces.
    
    TEST: no change in goldens
    Change-Id: I33a6733069c4cf1e7aa37fe8f7c24c392e946197
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/544987
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Benjamin Prosnitz <bprosnitz@google.com>

--- tools/fidl/fidlgen_llcpp/codegen/fragment_method_unownedresult.tmpl.go
+++ tools/fidl/fidlgen_llcpp/codegen/fragment_method_unownedresult.tmpl.go
@@ -11,53 +11,53 @@ const fragmentMethodUnownedResultTmpl = `
 {{- EnsureNamespace "" }}
 template<>
 class {{ .WireUnownedResult }} final : public ::fidl::Result {
-	public:
-    {{- $args := (printf "::fidl::UnownedClientEnd<%s> _client" .Protocol) }}
-    {{- if .RequestArgs }}
-      {{ $args = (List $args "uint8_t* _request_bytes" "uint32_t _request_byte_capacity") }}
-    {{- end }}
-    {{- $args = (List $args .RequestArgs) }}
-    {{- if .HasResponse }}
-        {{- $args = (List $args "uint8_t* _response_bytes" "uint32_t _response_byte_capacity") }}
-    {{- end }}
-     explicit {{ .WireUnownedResult.Self }}({{ RenderParams $args}});
-	 explicit {{ .WireUnownedResult.Self }}(const ::fidl::Result& result) : ::fidl::Result(result) {}
-	 {{ .WireUnownedResult.Self }}({{ .WireUnownedResult.Self }}&&) = delete;
-	 {{ .WireUnownedResult.Self }}(const {{ .WireUnownedResult.Self }}&) = delete;
-	 {{ .WireUnownedResult.Self }}* operator=({{ .WireUnownedResult.Self }}&&) = delete;
-	 {{ .WireUnownedResult.Self }}* operator=(const {{ .WireUnownedResult.Self }}&) = delete;
-	 {{- if and .HasResponse .Response.IsResource }}
-	 ~{{ .WireUnownedResult.Self }}() {
-	   if (ok()) {
-		 Unwrap()->_CloseHandles();
-	   }
-	 }
-	 {{- else }}
-	 ~{{ .WireUnownedResult.Self }}() = default;
-	 {{- end }}
-	 {{- if .HasResponse }}
+ public:
+  {{- $args := (printf "::fidl::UnownedClientEnd<%s> _client" .Protocol) }}
+  {{- if .RequestArgs -}}
+    {{ $args = (List $args "uint8_t* _request_bytes" "uint32_t _request_byte_capacity") }}
+  {{- end }}
+  {{- $args = (List $args .RequestArgs) }}
+  {{- if .HasResponse -}}
+    {{ $args = (List $args "uint8_t* _response_bytes" "uint32_t _response_byte_capacity") }}
+  {{- end }}
+  explicit {{ .WireUnownedResult.Self }}({{ RenderParams $args }});
+  explicit {{ .WireUnownedResult.Self }}(const ::fidl::Result& result) : ::fidl::Result(result) {}
+  {{ .WireUnownedResult.Self }}({{ .WireUnownedResult.Self }}&&) = delete;
+  {{ .WireUnownedResult.Self }}(const {{ .WireUnownedResult.Self }}&) = delete;
+  {{ .WireUnownedResult.Self }}* operator=({{ .WireUnownedResult.Self }}&&) = delete;
+  {{ .WireUnownedResult.Self }}* operator=(const {{ .WireUnownedResult.Self }}&) = delete;
+  {{- if and .HasResponse .Response.IsResource }}
+  ~{{ .WireUnownedResult.Self }}() {
+    if (ok()) {
+      Unwrap()->_CloseHandles();
+    }
+  }
+  {{- else }}
+  ~{{ .WireUnownedResult.Self }}() = default;
+  {{- end }}
+  {{- if .HasResponse }}
 
-	 {{ .WireResponse }}* Unwrap() {
-	   ZX_DEBUG_ASSERT(ok());
-	   return reinterpret_cast<{{ .WireResponse }}*>(bytes_);
-	 }
-	 const {{ .WireResponse }}* Unwrap() const {
-	   ZX_DEBUG_ASSERT(ok());
-	   return reinterpret_cast<const {{ .WireResponse }}*>(bytes_);
-	 }
+  {{ .WireResponse }}* Unwrap() {
+    ZX_DEBUG_ASSERT(ok());
+    return reinterpret_cast<{{ .WireResponse }}*>(bytes_);
+  }
+  const {{ .WireResponse }}* Unwrap() const {
+    ZX_DEBUG_ASSERT(ok());
+    return reinterpret_cast<const {{ .WireResponse }}*>(bytes_);
+  }
 
-	 {{ .WireResponse }}& value() { return *Unwrap(); }
-	 const {{ .WireResponse }}& value() const { return *Unwrap(); }
+  {{ .WireResponse }}& value() { return *Unwrap(); }
+  const {{ .WireResponse }}& value() const { return *Unwrap(); }
 
-	 {{ .WireResponse }}* operator->() { return &value(); }
-	 const {{ .WireResponse }}* operator->() const { return &value(); }
+  {{ .WireResponse }}* operator->() { return &value(); }
+  const {{ .WireResponse }}* operator->() const { return &value(); }
 
-	 {{ .WireResponse }}& operator*() { return value(); }
-	 const {{ .WireResponse }}& operator*() const { return value(); }
+  {{ .WireResponse }}& operator*() { return value(); }
+  const {{ .WireResponse }}& operator*() const { return value(); }
 
-	private:
-	 uint8_t* bytes_;
-	 {{- end }}
+ private:
+  uint8_t* bytes_;
+  {{- end }}
 };
 {{- end }}
 
