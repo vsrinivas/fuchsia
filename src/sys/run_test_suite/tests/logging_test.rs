e27c7b10006308449a04ba666c6efa5d255054e7
Satsuki Ueno <satsukiu@google.com>
2021-06-21 21:47:55 +0000

    [test] Sleep to mitigate missing logs flake
    Archivist may not process logs in case it is shut down before
    it receives the CapabilityRequested event. This adds a sleep
    so that component manager has more time to route
    CapabilityRequested. This should be reverted once the events
    are ordered by component manager.
    
    Bug: 79121
    Bug: 74305
    
    Multiply: run_test_suite_integration_tests
    
    Change-Id: I1fe1527e87955f5c1d688336b99807cec69aa37a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/545694
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Satsuki Ueno <satsukiu@google.com>

--- src/sys/run_test_suite/tests/logging_test.rs
+++ src/sys/run_test_suite/tests/logging_test.rs
@@ -11,4 +11,10 @@ fn log_and_exit() {
     tracing::debug!("my debug message");
     tracing::info!("my info message");
     tracing::warn!("my warn message");
+
+    // TODO(fxbug.dev/79121): Component manager may send the Stop event to Archivist before it
+    // sends CapabilityRequested. In this case the logs may be lost. This sleep delays
+    // terminating the test to give component manager time to send the CapabilityRequested
+    // event. This sleep should be removed once component manager orders the events.
+    std::thread::sleep(std::time::Duration::from_secs(2));
 }
