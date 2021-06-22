c69be8c2b4ae6423f936bd1e1b59cd98d304f8d0
Simon Shields <simonshields@google.com>
2021-06-22 04:42:13 +0000

    [x86][acpi] Add a zx_status_value() method to acpi::status.
    This uses the existing acpi_to_zx_status code.
    
    Bug: 78200
    Change-Id: I271ffd893819f1af4a36172296f7d4a350e96289
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/545146
    Reviewed-by: Alex Legg <alexlegg@google.com>
    Commit-Queue: Simon Shields <simonshields@google.com>

--- src/devices/board/drivers/x86/acpi/status.h
+++ src/devices/board/drivers/x86/acpi/status.h
@@ -9,6 +9,65 @@
 
 #include <acpica/acpi.h>
 
+static inline zx_status_t acpi_to_zx_status(ACPI_STATUS acpi_status) {
+  switch (acpi_status) {
+    case AE_ERROR:
+    case AE_NO_ACPI_TABLES:
+      return ZX_ERR_INTERNAL;
+    case AE_NO_NAMESPACE:
+      return ZX_ERR_NOT_FOUND;
+    case AE_NO_MEMORY:
+      return ZX_ERR_NO_MEMORY;
+    case AE_NOT_FOUND:
+    case AE_NOT_EXIST:
+      return ZX_ERR_NOT_FOUND;
+    case AE_ALREADY_EXISTS:
+      return ZX_ERR_ALREADY_EXISTS;
+    case AE_TYPE:
+      return ZX_ERR_WRONG_TYPE;
+    case AE_NULL_OBJECT:
+    case AE_NULL_ENTRY:
+      return ZX_ERR_NOT_FOUND;
+    case AE_BUFFER_OVERFLOW:
+      return ZX_ERR_BUFFER_TOO_SMALL;
+    case AE_STACK_OVERFLOW:
+    case AE_STACK_UNDERFLOW:
+      return ZX_ERR_INTERNAL;
+    case AE_NOT_IMPLEMENTED:
+    case AE_SUPPORT:
+      return ZX_ERR_NOT_SUPPORTED;
+    case AE_LIMIT:
+      return ZX_ERR_INTERNAL;
+    case AE_TIME:
+      return ZX_ERR_TIMED_OUT;
+    case AE_ACQUIRE_DEADLOCK:
+    case AE_RELEASE_DEADLOCK:
+    case AE_NOT_ACQUIRED:
+    case AE_ALREADY_ACQUIRED:
+      return ZX_ERR_INTERNAL;
+    case AE_NO_HARDWARE_RESPONSE:
+      return ZX_ERR_TIMED_OUT;
+    case AE_NO_GLOBAL_LOCK:
+      return ZX_ERR_INTERNAL;
+    case AE_ABORT_METHOD:
+      return ZX_ERR_INTERNAL;
+    case AE_SAME_HANDLER:
+      return ZX_ERR_ALREADY_EXISTS;
+    case AE_OWNER_ID_LIMIT:
+      return ZX_ERR_NO_RESOURCES;
+    case AE_NOT_CONFIGURED:
+      return ZX_ERR_NOT_FOUND;
+    case AE_ACCESS:
+      return ZX_ERR_ACCESS_DENIED;
+    case AE_IO_ERROR:
+      return ZX_ERR_IO;
+    case AE_OK:
+      return ZX_OK;
+    default:
+      return ZX_ERR_INTERNAL;
+  }
+}
+
 namespace acpi {
 
 // This is almost a direct copy of zx::status, but wrapping ACPI_STATUS instead of zx_status_t.
@@ -45,6 +104,8 @@ class LIB_FITX_NODISCARD status<T> : public ::fitx::result<ACPI_STATUS, T> {
   constexpr ACPI_STATUS status_value() const {
     return this->is_error() ? base::error_value() : AE_OK;
   }
+
+  constexpr zx_status_t zx_status_value() const { return acpi_to_zx_status(status_value()); }
 };
 
 // Specialization of status for empty value type.
@@ -70,6 +131,8 @@ class LIB_FITX_NODISCARD status<> : public ::fitx::result<ACPI_STATUS> {
   constexpr ACPI_STATUS status_value() const {
     return this->is_error() ? base::error_value() : AE_OK;
   }
+
+  constexpr zx_status_t zx_status_value() const { return acpi_to_zx_status(status_value()); }
 };
 
 // Simplified alias of acpi::error<ACPI_STATUS>.
