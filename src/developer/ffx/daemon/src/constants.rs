d2300d92d75b9f9c5209ba461d6399009b232b5c
Matt Boetger <boetger@google.com>
2021-06-22 00:54:12 +0000

    [ffx] Update fastboot check interval time
    Since fastboot over network is only broadcast every 10 secs, fastboot
    over network targets will be timed out before rediscovered.  Bumping
    this value up should fix it.
    
    Change-Id: Ia89f89c930ef82cfc060826bb1c0fcfcaa0b3831
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/545953
    Fuchsia-Auto-Submit: Matthew Boetger <boetger@google.com>
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/ffx/daemon/src/constants.rs
+++ src/developer/ffx/daemon/src/constants.rs
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 use std::time::Duration;
 
-pub(crate) const FASTBOOT_CHECK_INTERVAL: Duration = Duration::from_secs(3);
+pub(crate) const FASTBOOT_CHECK_INTERVAL: Duration = Duration::from_secs(10);
 pub(crate) const MDNS_BROADCAST_INTERVAL: Duration = Duration::from_secs(20);
 pub(crate) const ZEDBOOT_BROADCAST_INTERVAL: Duration = Duration::from_secs(2);
 
