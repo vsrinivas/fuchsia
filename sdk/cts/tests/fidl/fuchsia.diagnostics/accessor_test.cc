753be7bf9fbf8bfbc2730980f2e3ac4d5f5aa0cc
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2021-06-24 18:31:12 +0000

    [cts][diagnostics] Test for reading inspect data from the ArchiveAccessor
    CTS test case for fuchsia.diagnostics.ArchiveAccessor/StreamDiagnostics
    with parameters DataType=INSPECT and StreamMode=SNAPSHOT.
    
    Fixed: 78480
    
    Change-Id: Ia7964bc27b6584ec650a7b018b91158e3a99cd37
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/546630
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>

---
+++ sdk/cts/tests/fidl/fuchsia.diagnostics/accessor_test.cc
@@ -0,0 +1,294 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuchsia/diagnostics/cpp/fidl.h>
+#include <fuchsia/sys/cpp/fidl.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
+#include <lib/fit/bridge.h>
+#include <lib/fit/result.h>
+#include <lib/fit/single_threaded_executor.h>
+#include <lib/inspect/contrib/cpp/archive_reader.h>
+#include <lib/sys/cpp/component_context.h>
+
+#include <regex>
+
+#include <rapidjson/document.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/stringbuffer.h>
+#include <zxtest/zxtest.h>
+
+namespace {
+
+constexpr auto kInspectPublisherUrl =
+    "fuchsia-pkg://fuchsia.com/fuchsia-diagnostics-tests#meta/inspect-publisher.cmx";
+
+const char EXPECTED_DATA[] = R"JSON({
+    "data_source": "Inspect",
+    "metadata": {
+        "component_url": "fuchsia-pkg://fuchsia.com/fuchsia-diagnostics-tests#meta/inspect-publisher.cmx",
+        "errors": null,
+        "filename": "fuchsia.inspect.Tree",
+        "timestamp": TIMESTAMP
+    },
+    "moniker": "inspect-publisher.cmx",
+    "payload": {
+        "root": {
+            "arrays": {
+                "doubles": [
+                    0.0,
+                    0.0,
+                    3.5,
+                    0.0
+                ],
+                "ints": [
+                    -1,
+                    0
+                ],
+                "uints": [
+                    0,
+                    2,
+                    0
+                ]
+            },
+            "buffers": {
+                "bytes": "b64:AQID",
+                "string": "foo"
+            },
+            "exponential_histograms": {
+                "double": {
+                    "buckets": [
+                        {
+                            "count": 0.0,
+                            "floor": -1.7976931348623157e308,
+                            "ceiling": 1.5
+                        },
+                        {
+                            "count": 0.0,
+                            "floor": 1.5,
+                            "ceiling": 3.5
+                        },
+                        {
+                            "count": 1.0,
+                            "floor": 3.5,
+                            "ceiling": 8.5
+                        },
+                        {
+                            "count": 0.0,
+                            "floor": 8.5,
+                            "ceiling": 26.0
+                        },
+                        {
+                            "count": 0.0,
+                            "floor": 26.0,
+                            "ceiling": 1.7976931348623157e308
+                        }
+                    ]
+                },
+                "int": {
+                    "buckets": [
+                        {
+                            "count": 0,
+                            "floor": -9223372036854775808,
+                            "ceiling": -10
+                        },
+                        {
+                            "count": 0,
+                            "floor": -10,
+                            "ceiling": -8
+                        },
+                        {
+                            "count": 1,
+                            "floor": -8,
+                            "ceiling": -4
+                        },
+                        {
+                            "count": 0,
+                            "floor": -4,
+                            "ceiling": 8
+                        },
+                        {
+                            "count": 0,
+                            "floor": 8,
+                            "ceiling": 9223372036854775807
+                        }
+                    ]
+                },
+                "uint": {
+                    "buckets": [
+                        {
+                            "count": 0,
+                            "floor": 0,
+                            "ceiling": 1
+                        },
+                        {
+                            "count": 0,
+                            "floor": 1,
+                            "ceiling": 3
+                        },
+                        {
+                            "count": 1,
+                            "floor": 3,
+                            "ceiling": 7
+                        },
+                        {
+                            "count": 0,
+                            "floor": 7,
+                            "ceiling": 19
+                        },
+                        {
+                            "count": 0,
+                            "floor": 19,
+                            "ceiling": 18446744073709551615
+                        }
+                    ]
+                }
+            },
+            "linear_histgorams": {
+                "double": {
+                    "buckets": [
+                        {
+                            "count": 0.0,
+                            "floor": -1.7976931348623157e308,
+                            "ceiling": 1.5
+                        },
+                        {
+                            "count": 0.0,
+                            "floor": 1.5,
+                            "ceiling": 4.0
+                        },
+                        {
+                            "count": 1.0,
+                            "floor": 4.0,
+                            "ceiling": 6.5
+                        },
+                        {
+                            "count": 0.0,
+                            "floor": 6.5,
+                            "ceiling": 9.0
+                        },
+                        {
+                            "count": 0.0,
+                            "floor": 9.0,
+                            "ceiling": 1.7976931348623157e308
+                        }
+                    ]
+                },
+                "int": {
+                    "buckets": [
+                        {
+                            "count": 0,
+                            "floor": -9223372036854775808,
+                            "ceiling": -10
+                        },
+                        {
+                            "count": 0,
+                            "floor": -10,
+                            "ceiling": -8
+                        },
+                        {
+                            "count": 0,
+                            "floor": -8,
+                            "ceiling": -6
+                        },
+                        {
+                            "count": 1,
+                            "floor": -6,
+                            "ceiling": -4
+                        },
+                        {
+                            "count": 0,
+                            "floor": -4,
+                            "ceiling": 9223372036854775807
+                        }
+                    ]
+                },
+                "uint": {
+                    "buckets": [
+                        {
+                            "count": 0,
+                            "floor": 0,
+                            "ceiling": 1
+                        },
+                        {
+                            "count": 0,
+                            "floor": 1,
+                            "ceiling": 3
+                        },
+                        {
+                            "count": 1,
+                            "floor": 3,
+                            "ceiling": 5
+                        },
+                        {
+                            "count": 0,
+                            "floor": 5,
+                            "ceiling": 7
+                        },
+                        {
+                            "count": 0,
+                            "floor": 7,
+                            "ceiling": 18446744073709551615
+                        }
+                    ]
+                }
+            },
+            "numeric": {
+                "bool": true,
+                "double": 1.5,
+                "int": -1,
+                "uint": 1
+            }
+        }
+    },
+    "version": 1
+})JSON";
+
+class AccessorTest : public zxtest::Test {};
+
+// Tests that reading inspect data returns expected data from the archive accessor.
+TEST_F(AccessorTest, StreamDiagnosticsInspect) {
+  async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
+  ASSERT_EQ(loop.StartThread(), ZX_OK);
+
+  auto context = sys::ComponentContext::Create();
+
+  fuchsia::sys::EnvironmentSyncPtr environment;
+  EXPECT_EQ(ZX_OK, context->svc()->Connect(environment.NewRequest()));
+
+  fuchsia::sys::LauncherSyncPtr launcher;
+  EXPECT_EQ(ZX_OK, environment->GetLauncher(launcher.NewRequest()));
+
+  fuchsia::sys::LaunchInfo launch_info;
+  launch_info.url = kInspectPublisherUrl;
+
+  fuchsia::sys::ComponentControllerPtr controller;
+  EXPECT_EQ(ZX_OK, launcher->CreateComponent(std::move(launch_info), controller.NewRequest()));
+
+  fuchsia::diagnostics::ArchiveAccessorPtr accessor;
+  EXPECT_EQ(ZX_OK, context->svc()->Connect(accessor.NewRequest()));
+
+  inspect::contrib::ArchiveReader reader(
+      context->svc()->Connect<fuchsia::diagnostics::ArchiveAccessor>(),
+      {"inspect-publisher.cmx:root"});
+
+  fit::result<std::vector<inspect::contrib::DiagnosticsData>, std::string> actual_result;
+  fit::single_threaded_executor executor;
+  executor.schedule_task(
+      reader.SnapshotInspectUntilPresent({"inspect-publisher.cmx"})
+          .then([&](fit::result<std::vector<inspect::contrib::DiagnosticsData>, std::string>&
+                        result) mutable { actual_result = std::move(result); }));
+  executor.run();
+
+  EXPECT_TRUE(actual_result.is_ok());
+  EXPECT_EQ(1, actual_result.value().size());
+
+  auto& data = actual_result.value()[0];
+  data.Sort();
+
+  std::regex reg("\"timestamp\": \\d+");
+  EXPECT_EQ(EXPECTED_DATA, regex_replace(data.PrettyJson(), reg, "\"timestamp\": TIMESTAMP"));
+}
+
+}  // namespace
---
+++ sdk/cts/tests/fidl/fuchsia.diagnostics/inspect_publisher.cc
@@ -0,0 +1,83 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
+#include <lib/sys/cpp/component_context.h>
+#include <lib/sys/inspect/cpp/component.h>
+
+// This trivial component writes a single property and node of every possible type in inspect for
+// the sake of verifying that the accessor API is stable in CTS.
+int main() {
+  async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
+  auto context = sys::ComponentContext::CreateAndServeOutgoingDirectory();
+  auto inspector = std::make_unique<sys::ComponentInspector>(context.get());
+
+  auto numeric_properties = inspector->root().CreateChild("numeric");
+  numeric_properties.CreateInt("int", -1, inspector.get());
+  numeric_properties.CreateUint("uint", 1, inspector.get());
+  numeric_properties.CreateDouble("double", 1.5, inspector.get());
+  numeric_properties.CreateBool("bool", true, inspector.get());
+
+  inspector->root().CreateLazyValues(
+      "buffers_lazy_values",
+      []() {
+        inspect::Inspector inspector;
+        auto buffer_properties = inspector.GetRoot().CreateChild("buffers");
+        buffer_properties.CreateString("string", "foo", &inspector);
+        buffer_properties.CreateByteVector("bytes", std::vector<uint8_t>({1, 2, 3}), &inspector);
+        inspector.emplace(std::move(buffer_properties));
+        return fit::make_ok_promise(std::move(inspector));
+      },
+      inspector.get());
+
+  inspector->root().CreateLazyNode(
+      "arrays",
+      []() {
+        inspect::Inspector inspector;
+        auto ints = inspector.GetRoot().CreateIntArray("ints", 2);
+        ints.Set(0, -1);
+        inspector.emplace(std::move(ints));
+
+        auto uints = inspector.GetRoot().CreateUintArray("uints", 3);
+        uints.Set(1, 2);
+        inspector.emplace(std::move(uints));
+
+        auto doubles = inspector.GetRoot().CreateDoubleArray("doubles", 4);
+        doubles.Set(2, 3.5);
+        inspector.emplace(std::move(doubles));
+
+        return fit::make_ok_promise(std::move(inspector));
+      },
+      inspector.get());
+
+  auto linear_histograms = inspector->root().CreateChild("linear_histgorams");
+  auto int_linear_hist = linear_histograms.CreateLinearIntHistogram("int", /*floor*/ -10,
+                                                                    /*step_size*/ 2, /*buckets*/ 3);
+  int_linear_hist.Insert(-5);
+
+  auto uint_linear_hist = linear_histograms.CreateLinearUintHistogram(
+      "uint", /*floor*/ 1, /*step_size*/ 2, /*buckets*/ 3);
+  uint_linear_hist.Insert(3);
+
+  auto double_linear_hist = linear_histograms.CreateLinearDoubleHistogram(
+      "double", /*floor*/ 1.5, /*step_size*/ 2.5, /*buckets*/ 3);
+  double_linear_hist.Insert(4.5);
+
+  auto exp_histograms = inspector->root().CreateChild("exponential_histograms");
+  auto int_exp_hist = exp_histograms.CreateExponentialIntHistogram(
+      "int", /*floor*/ -10, /*initial_step*/ 2, /*step_multiplier*/ 3, /*buckets*/ 3);
+  int_exp_hist.Insert(-5);
+
+  auto uint_exp_hist = exp_histograms.CreateExponentialUintHistogram(
+      "uint", /*floor*/ 1, /*initial_step*/ 2, /*step_multiplier*/ 3, /*buckets*/ 3);
+  uint_exp_hist.Insert(3);
+
+  auto double_exp_hist = exp_histograms.CreateExponentialDoubleHistogram(
+      "double", /*floor*/ 1.5, /*initial_step*/ 2, /*step_multiplier*/ 3.5, /*buckets*/ 3);
+  double_exp_hist.Insert(4.5);
+
+  loop.Run();
+  return 0;
+}
