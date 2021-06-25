dbb25d489cde6ebc361148c008587536171564bc
David 'Digit' Turner <digit@google.com>
2021-06-25 18:51:01 +0000

    [build] extract_imported_symbols(): Add library_target parameter
    This CL improves the extract_imported_symbols() template to accept
    a new `library_target` parameter, which is a GN label pointing to
    another shared_library() or loadable_module() target.
    
    This computes the real location of the library output file
    automatically, instead of requiring the caller to guess it (which
    is generally error-prone, and will become impossible once shared
    library variant redirection is implemented, see associated bug).
    
    Also modify the magma_vulkan_icd() template to use the new
    parameter.
    
    Bug: 79665
    Change-Id: I6badc60902d875440cd4778133eccef5a87115f3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/548283
    Commit-Queue: David Turner <digit@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: David Fang <fangism@google.com>

--- build/cpp/extract_imported_symbols.sh
+++ build/cpp/extract_imported_symbols.sh
@@ -7,22 +7,30 @@
 set -euo pipefail
 
 function usage() {
-  echo "Usage: $0 [NM] [LIBRARY] [SYMBOLS]"
+  echo "Usage: $0 [NM] [LIBRARY] [SYMBOLS] [DEPFILE]"
   echo "Dump the list of imported symbols from a prebuilt library"
   echo "[NM]:      path to the llvm-nm binary"
   echo "[LIBRARY]: path to the prebuilt library"
   echo "[SYMBOLS]: path to the output symbols file"
+  echo "[DEPFILE]: path to the output Ninja depfile (optional)"
   exit 1
 }
 
-if [[ $# -ne 3 ]]
+if [[ $# -ne 3 && $# -ne 4 ]]
 then
     usage
 fi
 
 readonly NM=$1
-readonly LIBRARY=$2
+LIBRARY=$2
 readonly SYMBOLS=$3
+readonly DEPFILE=${4-}
+
+# If LIBRARY begins with an @, it is the path of a response
+# file containing the real library file path.
+if [[ "${LIBRARY#@}" != "${LIBRARY}" ]]; then
+  LIBRARY=$(cat "${LIBRARY#@}")
+fi
 
 if ! [[ -e "$NM" ]]; then
   echo "Error: $NM not found" >&2
@@ -49,3 +57,7 @@ ${NM} --dynamic --undefined-only --no-weak ${LIBRARY} \
   | awk '{ print $2; }' \
   | env LC_ALL=C sort \
   > ${SYMBOLS}
+
+if [[ -n "${DEPFILE}" ]]; then
+  printf "%s: %s\n" "${SYMBOLS}" "${LIBRARY}" > "${DEPFILE}"
+fi
