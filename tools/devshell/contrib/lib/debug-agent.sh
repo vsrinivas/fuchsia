d2fbfe1a3ed46d29f1e87513bb002af66fa6fa8a
Vincent Belliard <vbelliard@google.com>
2021-06-25 01:34:24 +0000

    [zxdb] modify check_for_agent
    With a forwarded port, the fact that the debug agent port is open
    doesn't mean that the debug agent is running (because the port is always
    open).
    
    This CL modifies check_for_agent to be able to handle two case:
    - only one debug agent on a local or remote device.
    - one or several debug agents on a local device. In that case, if one
    debug agent uses the port 2345, it must be launched first.
    
    Bug: 78902
    Change-Id: Id184bb5954768de6f873204c726060d8992d39fa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/547901
    Commit-Queue: Vincent Belliard <vbelliard@google.com>
    Reviewed-by: Brett Wilson <brettw@google.com>
    Reviewed-by: Puneetha Ramachandra <puneetha@google.com>

--- tools/devshell/contrib/lib/debug-agent.sh
+++ tools/devshell/contrib/lib/debug-agent.sh
@@ -10,13 +10,35 @@
 # port with nc(1).
 # 1: agent port
 # 2: number of tries
+#
+# The only reliable way to check that a debug agent is listening on a given port is to try a
+# connection.
+# Currently we can't do that.
+#
+# The current solution handle two cases.
+# The first case is when we only need one debug agent. This is the usual case. The default port
+# (2345) is always used. In that case, we only check that the debug agent is running on the device
+# without knowing if it listens on the given port or another one.
+#
+# The second case is when we need several debug agents. The use case is to be able to have several
+# zxdb connected to several debug agents. In that case, each debug agent needs a different port.
+# To check that the debug agent associated with a given port is running, we check that the port is
+# open on the device. This only works is the ports are not forwarded by the local computer.
 check_for_agent() {
   local port="$1"
   local num_tries="$2"
 
   while ((num_tries--)); do
-    if nc -w5 -z "$(get-fuchsia-device-addr)" "${port}"; then
-      return 0
+    if [[ ${port} -eq "2345" ]]; then
+      # This test works even when ports are forwarded but it only checks that one agent is running.
+      if fx-command-run "shell" "ps" | grep "debug_agent.cmx" > /dev/null; then
+        return 0
+      fi
+    else
+      # This test only works when the device is local. It checks that the port is listening.
+      if nc -w5 -z "$(get-fuchsia-device-addr)" "${port}"; then
+        return 0
+      fi
     fi
     sleep 1
   done
