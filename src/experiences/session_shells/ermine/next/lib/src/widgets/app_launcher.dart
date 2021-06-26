1f4bff64fd7aa82464549a8d01315defbe5ad2c5
Yeonhee Lee <yhlee@google.com>
2021-06-26 03:23:35 +0000

    [Ermine.Next] Added a loading indicator to the app launcher
    - Displays an indeterminate circular loading indicator while loading the view.
    - Disables the entry while loading the view.
    - When the same app has been launched several times, the loading checks the loading status of lastly launched one.
    
    Videos:
    - Dark: https://screencast.googleplex.com/cast/NTIwMDczMTE4NTM0ODYwOHw5Yjg0ZDkyYS1jYQ
    - Light: https://screencast.googleplex.com/cast/NjIyNjA1MTU1NjQ0MjExMnw5YTI0OTcxMy1hMg
    
    Bug: 79236
    
    Change-Id: I64ab1a2329d51e7d6aa7cf82575bba473adac93a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/experiences/+/548288
    Commit-Queue: Yeonhee Lee <yhlee@google.com>
    Reviewed-by: Sanjay Chouksey <sanjayc@google.com>

--- src/experiences/session_shells/ermine/next/lib/src/widgets/app_launcher.dart
+++ src/experiences/session_shells/ermine/next/lib/src/widgets/app_launcher.dart
@@ -3,39 +3,63 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:next/src/states/app_state.dart';
 
 /// Defines a widget to list all launchable application entries.
 class AppLauncher extends StatelessWidget {
-  final AppState appState;
+  final AppState app;
 
-  const AppLauncher(this.appState);
+  const AppLauncher(this.app);
 
   @override
   Widget build(BuildContext context) {
     return RepaintBoundary(
       child: ListView.builder(
         padding: EdgeInsets.symmetric(vertical: 8),
-        itemCount: appState.appLaunchEntries.length,
+        itemCount: app.appLaunchEntries.length,
         itemBuilder: (context, index) {
-          final item = appState.appLaunchEntries[index];
-          return ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 24),
-            autofocus: index == 0,
-            leading: Image(
-              image: AssetImage(item['icon'] ?? 'images/Default-icon-2x.png'),
-              color: (item['url'] != null)
-                  ? Theme.of(context).colorScheme.secondary
-                  : Theme.of(context).disabledColor,
-              width: 32,
-              height: 32,
-            ),
-            title: Text(item['title']!),
-            enabled: item['url'] != null,
-            onTap: () => appState.launch([item['title']!, item['url']!]),
-          );
+          final item = app.appLaunchEntries[index];
+          return Observer(builder: (context) {
+            return ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 24),
+              autofocus: index == 0,
+              leading: Image(
+                image: AssetImage(item['icon'] ?? 'images/Default-icon-2x.png'),
+                color: _isEnabled(item)
+                    ? Theme.of(context).colorScheme.secondary
+                    : Theme.of(context).disabledColor,
+                width: 32,
+                height: 32,
+              ),
+              title: Text(item['title']!),
+              trailing: _isLoading(item)
+                  ? SizedBox(
+                      width: 24,
+                      height: 24,
+                      child: CircularProgressIndicator(),
+                    )
+                  : null,
+              enabled: _isEnabled(item),
+              onTap: () => app.launch([item['title']!, item['url']!]),
+            );
+          });
         },
       ),
     );
+  }
+
+  bool _isLoading(Map<String, String> item) {
+    final reversedViews = app.views.reversed;
+    for (final view in reversedViews) {
+      if (view.title == item['title']!) {
+        return !view.ready.value;
+      }
+    }
+    return false;
+  }
+
+  bool _isEnabled(Map<String, String> item) {
+    return item['url'] != null && !_isLoading(item);
   }
 }
