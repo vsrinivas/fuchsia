2fbdb090447df4b2f87509b24361eb6bec8b2ab7
C Stout <cstout@google.com>
2021-06-28 22:13:13 +0000

    [graphics][tests] Add image export test for linux
    Change-Id: I8387d2b2e9c909bf10b53011ca29da24e9db6e69
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/548683
    Commit-Queue: Craig Stout <cstout@google.com>
    Reviewed-by: John Bauman <jbauman@google.com>

--- src/graphics/tests/vkext/test_vk_ext_fd.cc
+++ src/graphics/tests/vkext/test_vk_ext_fd.cc
@@ -2,38 +2,71 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <array>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 #include "src/graphics/tests/common/vulkan_context.h"
 
-// Test the vulkan semaphore external fd extension.
-class TestVkExtFd : public testing::Test {
+class TestBase : public testing::Test {
  public:
+  TestBase(std::vector<const char*> desired_device_extensions)
+      : desired_device_extensions_(std::move(desired_device_extensions)) {}
+
   void SetUp() override {
     auto app_info =
-        vk::ApplicationInfo().setPApplicationName("test").setApplicationVersion(VK_API_VERSION_1_1);
+        vk::ApplicationInfo().setPApplicationName("test").setApiVersion(VK_API_VERSION_1_1);
     auto instance_info = vk::InstanceCreateInfo().setPApplicationInfo(&app_info);
 
-    std::array<const char*, 2> device_extensions{VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME,
-                                                 VK_KHR_EXTERNAL_FENCE_FD_EXTENSION_NAME};
-
-    auto builder = VulkanContext::Builder();
-    builder.set_instance_info(instance_info).set_validation_layers_enabled(false);
-    builder.set_device_info(builder.DeviceInfo().setPEnabledExtensionNames(device_extensions));
-
-    context_ = builder.Unique();
-    ASSERT_TRUE(context_);
+    constexpr uint32_t kPhysicalDeviceIndex = 0;
+    context_ = std::make_unique<VulkanContext>(kPhysicalDeviceIndex);
+    context_->set_instance_info(instance_info);
+    context_->set_validation_layers_enabled(false);
+    ASSERT_TRUE(context_->InitInstance());
 
     loader_.init(*context_->instance(), vkGetInstanceProcAddr);
+    ASSERT_TRUE(context_->InitQueueFamily());
+
+    {
+      uint32_t ext_count = 0;
+      auto [result, extensions] = context_->physical_device().enumerateDeviceExtensionProperties();
+      EXPECT_EQ(result, vk::Result::eSuccess);
+
+      for (auto& extension : extensions) {
+        for (auto& desired : desired_device_extensions_) {
+          if (strcmp(extension.extensionName, desired) == 0) {
+            ext_count++;
+          }
+        }
+      }
+      if (ext_count != desired_device_extensions_.size()) {
+        fprintf(stderr, "Missing extension(s)\n");
+        GTEST_SKIP();
+      }
+    }
+
+    auto device_create_info = vk::DeviceCreateInfo()
+                                  .setQueueCreateInfoCount(1)
+                                  .setPQueueCreateInfos(&context_->queue_info())
+                                  .setPEnabledExtensionNames(desired_device_extensions_);
+    context_->set_device_info(std::move(device_create_info));
+    ASSERT_TRUE(context_->InitDevice());
   }
 
+  std::vector<const char*> desired_device_extensions_;
   std::unique_ptr<VulkanContext> context_;
   vk::DispatchLoaderDynamic loader_;
 };
 
-TEST_F(TestVkExtFd, SemaphoreExportThenImport) {
+// Test the vulkan semaphore external fd extension.
+class TestVkExtSemFd : public TestBase {
+ public:
+  TestVkExtSemFd()
+      : TestBase({VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME,
+                  VK_KHR_EXTERNAL_FENCE_FD_EXTENSION_NAME}) {}
+};
+
+TEST_F(TestVkExtSemFd, SemaphoreExportThenImport) {
   auto export_create_info = vk::ExportSemaphoreCreateInfo().setHandleTypes(
       vk::ExternalSemaphoreHandleTypeFlagBits::eOpaqueFd);
   auto create_info = vk::SemaphoreCreateInfo().setPNext(&export_create_info);
@@ -71,7 +104,7 @@ TEST_F(TestVkExtFd, SemaphoreExportThenImport) {
   }
 }
 
-TEST_F(TestVkExtFd, FenceExportThenImport) {
+TEST_F(TestVkExtSemFd, FenceExportThenImport) {
   auto export_create_info =
       vk::ExportFenceCreateInfo().setHandleTypes(vk::ExternalFenceHandleTypeFlagBits::eOpaqueFd);
   auto create_info = vk::FenceCreateInfo().setPNext(&export_create_info);
@@ -104,5 +137,81 @@ TEST_F(TestVkExtFd, FenceExportThenImport) {
                                  .setFd(fd);
     auto import_ret = context_->device()->importFenceFdKHR(fence_import_info, loader_);
     ASSERT_EQ(vk::Result::eSuccess, import_ret);
+  }
+}
+
+// Test the vulkan memory external fd extension.
+class TestVkExtMemFd : public TestBase {
+ public:
+  TestVkExtMemFd() : TestBase({VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME}) {}
+};
+
+TEST_F(TestVkExtMemFd, ImageExport) {
+  constexpr uint32_t kDefaultWidth = 64;
+  constexpr uint32_t kDefaultHeight = 64;
+  constexpr vk::Format kDefaultVkFormat = vk::Format::eB8G8R8A8Unorm;
+
+  vk::UniqueImage image;
+  vk::UniqueDeviceMemory memory;
+
+  {
+    auto external_create_info = vk::ExternalMemoryImageCreateInfo().setHandleTypes(
+        vk::ExternalMemoryHandleTypeFlagBitsKHR::eOpaqueFd);
+
+    auto create_info = vk::ImageCreateInfo()
+                           .setImageType(vk::ImageType::e2D)
+                           .setFormat(kDefaultVkFormat)
+                           .setExtent(vk::Extent3D(kDefaultWidth, kDefaultHeight, 1))
+                           .setMipLevels(1)
+                           .setArrayLayers(1)
+                           .setSamples(vk::SampleCountFlagBits::e1)
+                           .setTiling(vk::ImageTiling::eOptimal)
+                           .setUsage(vk::ImageUsageFlagBits::eTransferSrc)
+                           .setSharingMode(vk::SharingMode::eExclusive)
+                           .setInitialLayout(vk::ImageLayout::ePreinitialized)
+                           .setPNext(&external_create_info);
+
+    auto result = context_->device()->createImageUnique(create_info);
+    ASSERT_EQ(vk::Result::eSuccess, result.result);
+    image = std::move(result.value);
+  }
+
+  {
+    auto memory_reqs_chain =
+        context_->device()
+            ->getImageMemoryRequirements2<vk::MemoryRequirements2, vk::MemoryDedicatedRequirements>(
+                image.get());
+    EXPECT_TRUE(
+        memory_reqs_chain.get<vk::MemoryDedicatedRequirements>().requiresDedicatedAllocation);
+
+    auto& mem_reqs = memory_reqs_chain.get<vk::MemoryRequirements2>().memoryRequirements;
+    uint32_t memory_type_index = __builtin_ctz(mem_reqs.memoryTypeBits);
+
+    auto dedicated_create_info = vk::MemoryDedicatedAllocateInfo().setImage(image.get());
+
+    auto export_create_info =
+        vk::ExportMemoryAllocateInfo()
+            .setHandleTypes(vk::ExternalMemoryHandleTypeFlagBitsKHR::eOpaqueFd)
+            .setPNext(&dedicated_create_info);
+
+    auto alloc_info = vk::MemoryAllocateInfo()
+                          .setAllocationSize(mem_reqs.size)
+                          .setMemoryTypeIndex(memory_type_index)
+                          .setPNext(&export_create_info);
+
+    auto result = context_->device()->allocateMemoryUnique(alloc_info);
+    ASSERT_EQ(result.result, vk::Result::eSuccess);
+    memory = std::move(result.value);
+
+    ASSERT_EQ(vk::Result::eSuccess,
+              context_->device()->bindImageMemory(image.get(), memory.get(), 0u));
+  }
+
+  {
+    auto get_fd_info = vk::MemoryGetFdInfoKHR()
+                           .setMemory(memory.get())
+                           .setHandleType(vk::ExternalMemoryHandleTypeFlagBitsKHR::eOpaqueFd);
+    auto result = context_->device()->getMemoryFdKHR(get_fd_info, loader_);
+    ASSERT_EQ(result.result, vk::Result::eSuccess);
   }
 }
