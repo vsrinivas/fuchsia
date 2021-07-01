d92275974db301a4e71b5a316e243ef86f91b733
Suraj Malhotra <surajmalhotra@google.com>
2021-07-01 07:29:02 +0000

    [ffx][driver][bind] Add debug-bind subcommand to ffx
    This is a more intuitive way to use the debugger than running a v1
    component.
    
    Tested: ffx driver debug-bind /boot/driver/ahci.so pci-00:01.0 -i -p
    
    Change-Id: I8bd3ac5d3907ca1d3d077cd781b98863aaecee34
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/550825
    Commit-Queue: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Alex Legg <alexlegg@google.com>

---
+++ src/developer/ffx/plugins/driver/debug-bind/src/lib.rs
@@ -0,0 +1,87 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{format_err, Result},
+    bind::debugger,
+    bind::encode_bind_program_v1::RawInstruction,
+    bind::instruction::DeviceProperty,
+    ffx_core::ffx_plugin,
+    ffx_driver_debug_bind_args::DriverDebugBindCommand,
+    fidl_fuchsia_device_manager::{BindDebuggerProxy, BindRulesBytecode},
+    fuchsia_zircon_status as zx,
+};
+
+#[ffx_plugin(
+    "driver_enabled",
+    BindDebuggerProxy = "bootstrap/driver_manager:expose:fuchsia.device.manager.BindDebugger"
+)]
+pub async fn debug_bind(service: BindDebuggerProxy, cmd: DriverDebugBindCommand) -> Result<()> {
+    let bind_rules_result = service
+        .get_bind_rules(&cmd.driver_path)
+        .await
+        .map_err(|err| format_err!("FIDL call to get bind rules failed: {}", err))?
+        .map_err(|err| {
+            format_err!(
+                "FIDL call to get bind rules returned an error: {}",
+                zx::Status::from_raw(err)
+            )
+        })?;
+
+    let bind_rules = match bind_rules_result {
+        BindRulesBytecode::BytecodeV1(rules) => rules,
+        BindRulesBytecode::BytecodeV2(_) => {
+            return Err(format_err!("Currently the debugger only supports the old bytecode"));
+        }
+    };
+
+    let device_properties = service
+        .get_device_properties(&cmd.device_path)
+        .await
+        .map_err(|err| format_err!("FIDL call to get device properties failed: {}", err))?
+        .map_err(|err| {
+            format_err!(
+                "FIDL call to get device properties returned an error: {}",
+                zx::Status::from_raw(err)
+            )
+        })?;
+
+    let raw_instructions = bind_rules
+        .into_iter()
+        .map(|instruction| RawInstruction([instruction.op, instruction.arg, instruction.debug]))
+        .collect::<Vec<RawInstruction<[u32; 3]>>>();
+
+    let device_properties = device_properties
+        .props
+        .into_iter()
+        .map(DeviceProperty::from)
+        .collect::<Vec<DeviceProperty>>();
+
+    if cmd.print_instructions {
+        println!("Bind program:");
+        for instruction in &raw_instructions {
+            println!("{}", instruction);
+        }
+        println!();
+    }
+
+    if cmd.print_properties {
+        println!("Device properties:");
+        for property in &device_properties {
+            println!("{}", property);
+        }
+        println!();
+    }
+
+    let binds = debugger::debug(&raw_instructions, &device_properties)
+        .map_err(|err| format_err!("{}", err))?
+        .is_some();
+
+    if binds {
+        println!("Driver binds to the device.");
+    } else {
+        println!("Driver doesn't bind to the device.");
+    }
+    Ok(())
+}
