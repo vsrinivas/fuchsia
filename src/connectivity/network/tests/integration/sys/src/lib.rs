6483bae0578005c2555599f49090760e82cd02f2
Peter Johnston <peterjohnston@google.com>
2021-07-03 02:12:57 +0000

    [netstack/tests] Migrate sys test to v2
    Bug: 77202
    Fixed: 77514
    Test: fx test -d netstack-integration-tests-v2
    Multiply: netstack-sys-integration-test
    Change-Id: I2135b84611ae8e19ecdf9957b2b6bf8f2905a494
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/550602
    Commit-Queue: Peter Johnston <peterjohnston@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Amanda Tait <atait@google.com>

--- src/connectivity/network/tests/integration/sys/src/lib.rs
+++ src/connectivity/network/tests/integration/sys/src/lib.rs
@@ -4,14 +4,27 @@
 
 #![cfg(test)]
 
-use fuchsia_zircon as zx;
+use {
+    fidl::endpoints::DiscoverableService as _,
+    fidl::endpoints::Proxy as _,
+    fidl_fuchsia_io as fio, fidl_fuchsia_io2 as fio2, fidl_fuchsia_logger as flogger,
+    fidl_fuchsia_netstack as fnetstack,
+    fuchsia_component::server::ServiceFs,
+    fuchsia_component_test::{
+        builder::{Capability, CapabilityRoute, ComponentSource, RealmBuilder, RouteEndpoint},
+        mock::MockHandles,
+    },
+    fuchsia_zircon as zx,
+    futures::{FutureExt as _, StreamExt as _},
+    netstack_testing_common::realms::{Netstack, Netstack2},
+    vfs::directory::entry::DirectoryEntry as _,
+};
 
 #[fuchsia_async::run_singlethreaded(test)]
 async fn start_with_cache_no_space() {
-    use {futures::FutureExt as _, vfs::directory::entry::DirectoryEntry as _};
-
     struct NoSpaceEntryConstructor {
         paths: std::sync::Mutex<Vec<String>>,
+        requests: std::sync::atomic::AtomicUsize,
     }
 
     impl vfs::directory::mutable::entry_constructor::EntryConstructor for NoSpaceEntryConstructor {
@@ -22,73 +35,167 @@ async fn start_with_cache_no_space() {
             name: &str,
             _path: &vfs::path::Path,
         ) -> Result<std::sync::Arc<dyn vfs::directory::entry::DirectoryEntry>, zx::Status> {
-            let Self { paths } = &*self;
+            let Self { paths, requests } = &*self;
             let () = paths.lock().unwrap().push(name.into());
+            let _: usize = requests.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
             Err(zx::Status::NO_SPACE)
         }
     }
 
-    let constructor =
-        std::sync::Arc::new(NoSpaceEntryConstructor { paths: std::sync::Mutex::new(Vec::new()) });
+    let constructor = std::sync::Arc::new(NoSpaceEntryConstructor {
+        paths: std::sync::Mutex::new(Vec::new()),
+        requests: std::sync::atomic::AtomicUsize::new(0),
+    });
 
     let root = vfs::mut_pseudo_directory! {};
-    let (client, server) =
-        fidl::endpoints::create_endpoints().expect("failed to create FIDL endpoints");
+    let (client, server) = fidl::endpoints::create_proxy::<fio::DirectoryMarker>()
+        .expect("failed to create FIDL endpoints");
     let () = root.open(
         vfs::execution_scope::ExecutionScope::build().entry_constructor(constructor.clone()).new(),
         fidl_fuchsia_io::OPEN_RIGHT_READABLE | fidl_fuchsia_io::OPEN_RIGHT_WRITABLE,
         0,
         vfs::path::Path::empty(),
-        server,
+        server.into_channel().into(),
     );
 
-    let launcher = fuchsia_component::client::launcher().expect("failed to get launcher");
-    let mut options = fuchsia_component::client::LaunchOptions::new();
-    let _: &mut fuchsia_component::client::LaunchOptions =
-        options.add_handle_to_namespace("/cache".into(), client.into());
-    let app = fuchsia_component::client::launch_with_options(
-        &launcher,
-        "fuchsia-pkg://fuchsia.com/netstack-integration-tests#meta/netstack-debug.cmx".into(),
-        None,
-        options,
-    )
-    .expect("failed to launch netstack");
+    const NETSTACK_MONIKER: &str = "netstack";
+    const MOCK_CACHE_MONIKER: &str = "mock-cache";
+    const CACHE_DIR_NAME: &str = "cache";
+    const CACHE_DIR_PATH: &str = "/cache";
+    const DIAGNOSTICS_DIR_NAME: &str = "diagnostics";
+    const DIAGNOSTICS_DIR_PATH: &str = "/diagnostics";
+
+    let mut builder = RealmBuilder::new().await.expect("failed to create realm builder");
+    let _: &mut RealmBuilder = builder
+        .add_eager_component(NETSTACK_MONIKER, ComponentSource::url(Netstack2::VERSION.get_url()))
+        .await
+        .expect("failed to add netstack component")
+        .add_route(CapabilityRoute {
+            capability: Capability::protocol(fnetstack::NetstackMarker::SERVICE_NAME),
+            source: RouteEndpoint::component(NETSTACK_MONIKER),
+            targets: vec![RouteEndpoint::AboveRoot],
+        })
+        .unwrap_or_else(|e| {
+            panic!(
+                "failed to expose {} from netstack: {}",
+                fnetstack::NetstackMarker::SERVICE_NAME,
+                e,
+            )
+        })
+        .add_route(CapabilityRoute {
+            capability: Capability::protocol(flogger::LogSinkMarker::SERVICE_NAME),
+            source: RouteEndpoint::AboveRoot,
+            targets: vec![RouteEndpoint::component(NETSTACK_MONIKER)],
+        })
+        .unwrap_or_else(|e| {
+            panic!("failed to offer {} to netstack: {}", flogger::LogSinkMarker::SERVICE_NAME, e)
+        })
+        .add_component(
+            MOCK_CACHE_MONIKER,
+            ComponentSource::mock(move |mock_handles: MockHandles| {
+                let cache = Clone::clone(&client);
+                Box::pin(async {
+                    let mut fs = ServiceFs::new();
+                    let () = fs.add_remote(CACHE_DIR_NAME, cache);
+                    let _: &mut ServiceFs<_> =
+                        fs.serve_connection(mock_handles.outgoing_dir.into_channel())?;
+                    let () = fs.collect::<()>().await;
+                    Ok(())
+                })
+            }),
+        )
+        .await
+        .expect("failed to add mock cache component")
+        .add_route(CapabilityRoute {
+            capability: Capability::directory(CACHE_DIR_NAME, CACHE_DIR_PATH, fio2::RW_STAR_DIR),
+            source: RouteEndpoint::component(MOCK_CACHE_MONIKER),
+            targets: vec![RouteEndpoint::component(NETSTACK_MONIKER)],
+        })
+        .expect("failed to route cache to netstack");
+    let mut realm = builder.build();
+    let mut netstack_decl = realm
+        .get_decl(&NETSTACK_MONIKER.into())
+        .await
+        .expect("failed to find netstack component decl");
+    let cm_rust::ComponentDecl { exposes, capabilities, .. } = &mut netstack_decl;
+    // The netstack component exposes `/diagnostics` to `framework` with `connect` rights, in order
+    // to serve inspect data. For this test, expose `/diagnostics` to `parent` instead, with `r*`
+    // rights, so that it is accessible from the root of the realm and we can introspect it.
+    let target = exposes
+        .iter_mut()
+        .find_map(|expose| match expose {
+            cm_rust::ExposeDecl::Directory(cm_rust::ExposeDirectoryDecl {
+                source_name,
+                target,
+                ..
+            }) if source_name.str() == DIAGNOSTICS_DIR_NAME => Some(target),
+            _ => None,
+        })
+        .expect("failed to find diagnostics expose");
+    *target = cm_rust::ExposeTarget::Parent;
+    let rights =
+        capabilities
+            .iter_mut()
+            .find_map(|capability| match capability {
+                cm_rust::CapabilityDecl::Directory(cm_rust::DirectoryDecl {
+                    name, rights, ..
+                }) if name.str() == DIAGNOSTICS_DIR_NAME => Some(rights),
+                _ => None,
+            })
+            .expect("failed to find diagnostics capability");
+    *rights = fio2::R_STAR_DIR;
+    let () = realm
+        .set_component(&NETSTACK_MONIKER.into(), netstack_decl)
+        .await
+        .expect("failed to modify netstack component decl");
+    let () = realm
+        .add_route(CapabilityRoute {
+            capability: Capability::directory(
+                DIAGNOSTICS_DIR_NAME,
+                DIAGNOSTICS_DIR_PATH,
+                fio2::R_STAR_DIR,
+            ),
+            source: RouteEndpoint::component(NETSTACK_MONIKER),
+            targets: vec![RouteEndpoint::AboveRoot],
+        })
+        .await
+        .expect("failed to expose diagnostics directory from netstack");
+    let realm_instance = realm.create().await.expect("error creating realm");
+
+    let netstack = realm_instance
+        .root
+        .connect_to_protocol_at_exposed_dir::<fnetstack::NetstackMarker>()
+        .expect("failed to connect to netstack")
+        .into_channel()
+        .expect("failed to convert netstack proxy into async channel");
+    let mut netstack_fut =
+        fuchsia_async::OnSignals::new(&netstack, zx::Signals::CHANNEL_PEER_CLOSED).fuse();
 
     let (client_channel, server_channel) =
         zx::Channel::create().expect("failed to create zircon channel");
-    let () =
-        app.pass_to_named_protocol(".", server_channel).expect("failed to connect to svc directory");
+    let () = realm_instance
+        .root
+        .connect_request_to_named_service_at_exposed_dir(DIAGNOSTICS_DIR_NAME, server_channel)
+        .expect("failed to open diagnostics directory exposed from netstack");
     let client_channel = fuchsia_async::Channel::from_channel(client_channel)
         .expect("failed to create async channel");
     let proxy = fidl_fuchsia_io::DirectoryProxy::new(client_channel);
 
     let () = futures::select! {
-        res = app.wait_with_output().fuse() => {
-            let fuchsia_component::client::Output {
-                exit_status,
-                stdout,
-                stderr,
-            } = res.expect("failed to wait for netstack output");
-            panic!(
-                "netstack unexpectedly exited; exit_status={} stdout={} stderr={}",
-                exit_status,
-                String::from_utf8_lossy(&stdout),
-                String::from_utf8_lossy(&stderr)
-            );
-        },
+        res = netstack_fut => panic!("netstack unexpectedly exited; got signals: {:?}", res),
         res = proxy.describe() => {
-            let info = res.expect("failed to describe svc directory");
+            let info = res.expect("failed to describe diagnostics directory");
             assert_eq!(
                 info,
-                fidl_fuchsia_io::NodeInfo::Directory(fidl_fuchsia_io::DirectoryObject,)
+                fidl_fuchsia_io::NodeInfo::Directory(fidl_fuchsia_io::DirectoryObject)
             );
         }
     };
 
-    let paths = {
-        let NoSpaceEntryConstructor { paths } = &*constructor;
-        paths.lock().unwrap().clone()
+    let (paths, requests) = {
+        let NoSpaceEntryConstructor { paths, requests } = &*constructor;
+        (paths.lock().unwrap().clone(), requests.load(std::sync::atomic::Ordering::SeqCst))
     };
-
     assert_eq!(paths[..], ["pprof"]);
+    assert_eq!(requests, 1);
 }
