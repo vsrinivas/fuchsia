e0801e46faeb3d253d73a6431a0e5d7da96ee5c8
Erick Tryzelaar <etryzelaar@google.com>
2021-07-12 20:04:29 +0000

    [fx] Fix `legacy_serve` usability issues
    This makes the following changes to fx as prep for launching `fx
    --disable=legacy_serve serve-updates`:
    
    * Aliases the repository as `fuchsia.com`
    * Renames `check-for-amber-server` to `check-for-package-server`, and
      update it to work with the ffx server on port 8085.
    * Changes `add-update-source` to opt into the `ffx_repository` feature
      flag (this should be okay because we're still behind the fx feature
      flag.
    * Add support for the `--persist` flag.
    
    Change-Id: Ibe6e0f83454768f57e465837c6055b12ede78622
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/553096
    Reviewed-by: James Tucker <raggi@google.com>
    Reviewed-by: Dan Johnson <computerdruid@google.com>
    Commit-Queue: Erick Tryzelaar <etryzelaar@google.com>

--- tools/devshell/lib/updates.sh
+++ tools/devshell/lib/updates.sh
@@ -2,18 +2,38 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-function check-for-amber-server {
-  # Make sure it is running.
-  if [[ -z "$(pgrep -f 'pm serve .*/amber-files')" ]]; then
-    fx-error "It looks like serve-updates is not running."
-    fx-error "You probably need to start \"fx serve\""
-    return 1
+function check-for-package-server {
+  if is_feature_enabled "legacy_serve"; then
+    # Make sure it is running.
+    if [[ -z "$(pgrep -f 'pm serve .*/amber-files')" ]]; then
+      fx-error "It looks like serve-updates is not running."
+      fx-error "You probably need to start \"fx serve\""
+      return 1
+    fi
+
+    # Warn if it is using the wrong repository.
+    if [[ -z "$(pgrep -f "pm serve .*${FUCHSIA_BUILD_DIR}/amber-files")" ]]; then
+      fx-warn "WARNING: It looks like serve-updates is running in a different workspace."
+      fx-warn "WARNING: You probably need to stop that one and start a new one here with \"fx serve\""
+    fi
+  else
+    if is_macos; then
+      if ! netstat -anp tcp | awk '{print $4}' | grep "\.8085$" > /dev/null; then
+        fx-error "It looks like the ffx package server is not running."
+        fx-error "You probably need to run \"fx add-update-source\""
+        return 1
+      fi
+    else
+      if ! ss -f inet -f inet6 -an | awk '{print $5}' | grep ":8085$" > /dev/null; then
+        fx-error "It looks like the ffx package server is not running."
+        fx-error "You probably need to run \"fx add-update-source\""
+        return 1
+      fi
+    fi
+
+    # FIXME(http://fxbug.dev/80431): Check if the current `devhost` points at
+    # '${FUCHSIA_BUILD_DIR}/amber-files'.
   fi
 
-  # Warn if it is using the wrong repository.
-  if [[ -z "$(pgrep -f "pm serve .*${FUCHSIA_BUILD_DIR}/amber-files")" ]]; then
-    fx-warn "WARNING: It looks like serve-updates is running in a different workspace."
-    fx-warn "WARNING: You probably need to stop that one and start a new one here with \"fx serve\""
-  fi
   return 0
 }
