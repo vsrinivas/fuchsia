2aac697c13719650d8d273002d06b4da00365a41
C Stout <cstout@google.com>
2021-07-13 23:01:39 +0000

    [msd-intel-gen] Move GpuProgress implementation to .cc
    Change-Id: I6ed60151c6b50ecb0b649ca00043142ec7884cc9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/554130
    Commit-Queue: Craig Stout <cstout@google.com>
    Reviewed-by: John Bauman <jbauman@google.com>

---
+++ src/graphics/drivers/msd-intel-gen/src/gpu_progress.cc
@@ -0,0 +1,50 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu_progress.h"
+
+#include "magma_util/dlog.h"
+
+void GpuProgress::Submitted(uint32_t sequence_number, std::chrono::steady_clock::time_point time) {
+  DASSERT(sequence_number != Sequencer::kInvalidSequenceNumber);
+  if (sequence_number != last_submitted_sequence_number_) {
+    DLOG("Submitted 0x%x", sequence_number);
+    DASSERT(sequence_number > last_submitted_sequence_number_);
+    if (last_submitted_sequence_number_ == last_completed_sequence_number_) {
+      // Starting from idle.
+      hangcheck_start_time_ = time;
+    }
+    last_submitted_sequence_number_ = sequence_number;
+  }
+}
+
+void GpuProgress::Completed(uint32_t sequence_number, std::chrono::steady_clock::time_point time) {
+  DASSERT(sequence_number != Sequencer::kInvalidSequenceNumber);
+  if (sequence_number != last_completed_sequence_number_) {
+    DLOG("Completed 0x%x", sequence_number);
+    DASSERT(sequence_number > last_completed_sequence_number_);
+    last_completed_sequence_number_ = sequence_number;
+  } else {
+    DLOG("completed 0x%x AGAIN", sequence_number);
+  }
+
+  // Handle initial condition - init batch isn't submitted as a command buffer.
+  if (last_submitted_sequence_number_ == Sequencer::kInvalidSequenceNumber)
+    last_submitted_sequence_number_ = last_completed_sequence_number_;
+
+  if (last_completed_sequence_number_ == last_submitted_sequence_number_) {
+    // Going idle.
+    hangcheck_start_time_ = std::chrono::steady_clock::time_point::max();
+  } else {
+    // Starting more work.
+    hangcheck_start_time_ = time;
+  }
+}
+
+std::chrono::steady_clock::duration GpuProgress::GetHangcheckTimeout(
+    uint64_t max_completion_time_ms, std::chrono::steady_clock::time_point now) {
+  if (hangcheck_start_time_ == std::chrono::steady_clock::time_point::max())
+    return std::chrono::steady_clock::duration::max();
+  return hangcheck_start_time_ + std::chrono::milliseconds(max_completion_time_ms) - now;
+}
--- src/graphics/drivers/msd-intel-gen/src/gpu_progress.h
+++ src/graphics/drivers/msd-intel-gen/src/gpu_progress.h
@@ -7,54 +7,16 @@
 
 #include <chrono>
 
-#include "magma_util/dlog.h"
-#include "magma_util/macros.h"
 #include "sequencer.h"
 
 class GpuProgress {
  public:
-  void Submitted(uint32_t sequence_number, std::chrono::steady_clock::time_point time) {
-    DASSERT(sequence_number != Sequencer::kInvalidSequenceNumber);
-    if (sequence_number != last_submitted_sequence_number_) {
-      DLOG("Submitted 0x%x", sequence_number);
-      DASSERT(sequence_number > last_submitted_sequence_number_);
-      if (last_submitted_sequence_number_ == last_completed_sequence_number_) {
-        // Starting from idle.
-        hangcheck_start_time_ = time;
-      }
-      last_submitted_sequence_number_ = sequence_number;
-    }
-  }
+  void Submitted(uint32_t sequence_number, std::chrono::steady_clock::time_point time);
 
-  void Completed(uint32_t sequence_number, std::chrono::steady_clock::time_point time) {
-    DASSERT(sequence_number != Sequencer::kInvalidSequenceNumber);
-    if (sequence_number != last_completed_sequence_number_) {
-      DLOG("Completed 0x%x", sequence_number);
-      DASSERT(sequence_number > last_completed_sequence_number_);
-      last_completed_sequence_number_ = sequence_number;
-    } else {
-      DLOG("completed 0x%x AGAIN", sequence_number);
-    }
-
-    // Handle initial condition - init batch isn't submitted as a command buffer.
-    if (last_submitted_sequence_number_ == Sequencer::kInvalidSequenceNumber)
-      last_submitted_sequence_number_ = last_completed_sequence_number_;
-
-    if (last_completed_sequence_number_ == last_submitted_sequence_number_) {
-      // Going idle.
-      hangcheck_start_time_ = std::chrono::steady_clock::time_point::max();
-    } else {
-      // Starting more work.
-      hangcheck_start_time_ = time;
-    }
-  }
+  void Completed(uint32_t sequence_number, std::chrono::steady_clock::time_point time);
 
   std::chrono::steady_clock::duration GetHangcheckTimeout(
-      uint64_t max_completion_time_ms, std::chrono::steady_clock::time_point now) {
-    if (hangcheck_start_time_ == std::chrono::steady_clock::time_point::max())
-      return std::chrono::steady_clock::duration::max();
-    return hangcheck_start_time_ + std::chrono::milliseconds(max_completion_time_ms) - now;
-  }
+      uint64_t max_completion_time_ms, std::chrono::steady_clock::time_point now);
 
   uint32_t last_submitted_sequence_number() { return last_submitted_sequence_number_; }
 
