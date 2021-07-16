db938aec52c4138e5493036626f7e7c950841b8a
Daniel Sauve <danielsauve@google.com>
2021-07-16 18:54:12 +0000

    [ffx] Fix CPU virtualization check for Linux
    Change-Id: I2834e889176bf036bed08e25125db69c3c6b0806
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/555542
    Reviewed-by: Gabe Schine <thatguy@google.com>
    Commit-Queue: Daniel Sauve <danielsauve@google.com>

--- src/developer/ffx/plugins/platform/preflight/src/check/emu_acceleration.rs
+++ src/developer/ffx/plugins/platform/preflight/src/check/emu_acceleration.rs
@@ -6,9 +6,16 @@ use {
     crate::check::{PreflightCheck, PreflightCheckResult, PreflightCheckResult::*},
     crate::command_runner::CommandRunner,
     crate::config::*,
-    anyhow::Result,
+    anyhow::{anyhow, Result},
     async_trait::async_trait,
+    lazy_static::lazy_static,
+    regex::Regex,
 };
+
+lazy_static! {
+    // Regex to check whether the output of `lscpu` contains either VT-x or AMD-V to confirm that the CPU supports virtualization.
+    static ref CPU_VIRTUALIZATION_RE: Regex = Regex::new(r"Virtualization:(\s)*(VT-x|AMD-V).*").unwrap();
+}
 
 static NO_CPU_VIRT_MESSAGE: &str = "CPU does not support virtualization. This \
 will prevent emulator acceleration from working with the Fuchsia emulator.";
@@ -27,15 +34,28 @@ pub struct EmuAcceleration<'a> {
     command_runner: &'a CommandRunner,
 }
 
+pub fn linux_check_cpu_virtualization(command_runner: &CommandRunner) -> Result<bool> {
+    let (status, stdout, stderr) = (command_runner)(&vec!["lscpu"])?;
+    if !status.success() {
+        return Err(anyhow!(
+            "Could not exec `lscpu`: exited with code {}, stdout: {}, stderr: {}",
+            status.code(),
+            stdout,
+            stderr
+        ));
+    }
+
+    Ok(CPU_VIRTUALIZATION_RE.is_match(stdout.as_str()))
+}
+
 impl<'a> EmuAcceleration<'a> {
     pub fn new(command_runner: &'a CommandRunner) -> Self {
         EmuAcceleration { command_runner }
     }
 
     async fn run_linux(&self) -> Result<PreflightCheckResult> {
-        let (status, _, _) =
-            (self.command_runner)(&vec!["grep", "-qE", "'vmx|svm'", "/proc/cpuinfo"])?;
-        if !status.success() {
+        let supports_virtualization = linux_check_cpu_virtualization(self.command_runner)?;
+        if !supports_virtualization {
             return Ok(Warning(NO_CPU_VIRT_MESSAGE.to_string()));
         }
         let (status, _, _) = (self.command_runner)(&vec!["test", "-r", "/dev/kvm"])?;
@@ -71,14 +91,89 @@ impl PreflightCheck for EmuAcceleration<'_> {
 mod test {
     use {super::*, crate::command_runner::ExitStatus};
 
+    static LSCPU_OUTPUT_GOOD_INTEL: &str = "Architecture:                    x86_64
+    CPU op-mode(s):                  32-bit, 64-bit
+    Byte Order:                      Little Endian
+    Address sizes:                   46 bits physical, 48 bits virtual
+    CPU(s):                          12
+    On-line CPU(s) list:             0-11
+    Thread(s) per core:              2
+    Core(s) per socket:              6
+    Socket(s):                       1
+    NUMA node(s):                    1
+    Vendor ID:                       GenuineIntel
+    CPU family:                      6
+    Model:                           85
+    Model name:                      Intel(R) Xeon(R) W-2135 CPU @ 3.70GHz
+    Stepping:                        4
+    CPU MHz:                         1223.575
+    CPU max MHz:                     4500.0000
+    CPU min MHz:                     1200.0000
+    BogoMIPS:                        7399.70
+    Virtualization:                  VT-x
+    L1d cache:                       192 KiB
+    L1i cache:                       192 KiB
+    L2 cache:                        6 MiB
+    L3 cache:                        8.3 MiB
+    NUMA node0 CPU(s):               0-11";
+    static LSCPU_OUTPUT_GOOD_AMD: &str = "Architecture:                    x86_64
+    CPU op-mode(s):                  32-bit, 64-bit
+    Byte Order:                      Little Endian
+    Address sizes:                   43 bits physical, 48 bits virtual
+    CPU(s):                          128
+    On-line CPU(s) list:             0-127
+    Thread(s) per core:              2
+    Core(s) per socket:              64
+    Socket(s):                       1
+    NUMA node(s):                    1
+    Vendor ID:                       AuthenticAMD
+    CPU family:                      6
+    Model:                           85
+    Model name:                      AMD EPYC 7V12 64-Core Processor
+    Stepping:                        4
+    CPU MHz:                         1500.061
+    CPU max MHz:                     2450.0000
+    CPU min MHz:                     1500.0000
+    BogoMIPS:                        4900.44
+    Virtualization:                  AMD-V
+    L1d cache:                       2 MiB
+    L1i cache:                       2 MiB
+    L2 cache:                        32 MiB
+    L3 cache:                        256 MiB
+    NUMA node0 CPU(s):               0-127";
+    static LSCPU_OUTPUT_GOOD_BAD: &str = "Architecture:                    x86_64
+    CPU op-mode(s):                  32-bit, 64-bit
+    Byte Order:                      Little Endian
+    Address sizes:                   43 bits physical, 48 bits virtual
+    CPU(s):                          128
+    On-line CPU(s) list:             0-127
+    Thread(s) per core:              2
+    Core(s) per socket:              64
+    Socket(s):                       1
+    NUMA node(s):                    1
+    Vendor ID:                       AuthenticAMD
+    CPU family:                      6
+    Model:                           85
+    Model name:                      AMD EPYC 7V12 64-Core Processor
+    Stepping:                        4
+    CPU MHz:                         1500.061
+    CPU max MHz:                     2450.0000
+    CPU min MHz:                     1500.0000
+    BogoMIPS:                        4900.44
+    Virtualization:                  none
+    L1d cache:                       2 MiB
+    L1i cache:                       2 MiB
+    L2 cache:                        32 MiB
+    L3 cache:                        256 MiB
+    NUMA node0 CPU(s):               0-127";
     static KERN_HV_SUPPORTED: &str = "\nkern.hv_support: 1\n";
     static KERN_HV_UNSUPPORTED: &str = "kern.hv_support: 0";
 
     #[fuchsia_async::run_singlethreaded(test)]
     async fn test_linux_cpu_unsupported() -> Result<()> {
         let run_command: CommandRunner = |args| {
-            assert_eq!(args.to_vec(), vec!["grep", "-qE", "'vmx|svm'", "/proc/cpuinfo"]);
-            return Ok((ExitStatus(1), "".to_string(), "".to_string()));
+            assert_eq!(args.to_vec(), vec!["lscpu"]);
+            return Ok((ExitStatus(0), LSCPU_OUTPUT_GOOD_BAD.to_string(), "".to_string()));
         };
 
         let check = EmuAcceleration::new(&run_command);
@@ -90,8 +185,8 @@ mod test {
     #[fuchsia_async::run_singlethreaded(test)]
     async fn test_linux_kvm_not_enabled() -> Result<()> {
         let run_command: CommandRunner = |args| {
-            if args.to_vec() == vec!["grep", "-qE", "'vmx|svm'", "/proc/cpuinfo"] {
-                return Ok((ExitStatus(0), "vmx\n".to_string(), "".to_string()));
+            if args.to_vec() == vec!["lscpu"] {
+                return Ok((ExitStatus(0), LSCPU_OUTPUT_GOOD_INTEL.to_string(), "".to_string()));
             }
             assert_eq!(args.to_vec(), vec!["test", "-r", "/dev/kvm"]);
             Ok((ExitStatus(1), "".to_string(), "".to_string()))
@@ -104,10 +199,26 @@ mod test {
     }
 
     #[fuchsia_async::run_singlethreaded(test)]
-    async fn test_linux_success() -> Result<()> {
+    async fn test_linux_success_intel() -> Result<()> {
         let run_command: CommandRunner = |args| {
-            if args.to_vec() == vec!["grep", "-qE", "'vmx|svm'", "/proc/cpuinfo"] {
-                return Ok((ExitStatus(0), "vmx\n".to_string(), "".to_string()));
+            if args.to_vec() == vec!["lscpu"] {
+                return Ok((ExitStatus(0), LSCPU_OUTPUT_GOOD_INTEL.to_string(), "".to_string()));
+            }
+            assert_eq!(args.to_vec(), vec!["test", "-r", "/dev/kvm"]);
+            Ok((ExitStatus(0), "".to_string(), "".to_string()))
+        };
+
+        let check = EmuAcceleration::new(&run_command);
+        let response = check.run(&PreflightConfig { system: OperatingSystem::Linux }).await;
+        assert!(matches!(response?, PreflightCheckResult::Success(..)));
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_linux_success_amd() -> Result<()> {
+        let run_command: CommandRunner = |args| {
+            if args.to_vec() == vec!["lscpu"] {
+                return Ok((ExitStatus(0), LSCPU_OUTPUT_GOOD_AMD.to_string(), "".to_string()));
             }
             assert_eq!(args.to_vec(), vec!["test", "-r", "/dev/kvm"]);
             Ok((ExitStatus(0), "".to_string(), "".to_string()))
