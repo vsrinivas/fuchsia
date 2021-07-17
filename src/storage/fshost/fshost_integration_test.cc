756cacd7fe4c6cc863ceda7b3a8b579d925a13a8
Brett Wilson <brettw@google.com>
2021-07-17 15:59:02 +0000

    [fshost] Increase mount timeout.
    Waiting for mounts is occasionally timing out on asan. Increase the
    timeout.
    
    Bug: 80789
    Change-Id: I2108dc6ab5a892ba547c09124ad024f3be33c1cc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/556665
    Commit-Queue: Brett Wilson <brettw@google.com>
    Reviewed-by: Brandon Castellano <bcastell@google.com>

--- src/storage/fshost/fshost_integration_test.cc
+++ src/storage/fshost/fshost_integration_test.cc
@@ -67,9 +67,12 @@ void FshostIntegrationTest::ResumeWatcher() const {
 fbl::unique_fd FshostIntegrationTest::WaitForMount(const std::string& name,
                                                    uint64_t expected_fs_type) {
   // The mount point will always exist so we expect open() to work regardless of whether the device
-  // is actually mounted. We retry until the mount point has the expected filesystem type. Retry 20
-  // times and then give up.
-  for (int i = 0; i < 20; i++) {
+  // is actually mounted. We retry until the mount point has the expected filesystem type.
+  //
+  // This can be relatively slow on some bots (especially with asan) because it can involve lots of
+  // complex process launching so use a high retry limit.
+  constexpr int kMaxRetries = 30;
+  for (int i = 0; i < kMaxRetries; i++) {
     fidl::SynchronousInterfacePtr<fuchsia::io::Node> root;
     zx_status_t status =
         exposed_dir()->Open(fuchsia::io::OPEN_RIGHT_READABLE, 0, name, root.NewRequest());
