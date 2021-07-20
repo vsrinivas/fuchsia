8c27954f0aa8ff2a85c18a67becb9eb53a4978b7
Arthur Sfez <asfez@google.com>
2021-07-20 17:52:50 +0000

    [network/tests] Increase send timeout...
    ...when filling up the peer's stream socket receive buffer. It is
    currently just too short and can cause flakes in
    NetStreamSocketsTest.PeerClosedPOLLOUT. Increasing it made the flakes go
    away when running the tests locally in a loop.
    
    Multiply: fuchsia-pkg://fuchsia.com/netstack-c-api-tests#meta/netstack-bsdsocket-c-api-test.cmx
    
    Fixed: 80924
    Change-Id: I6c5177e5ba9342a14c17ce8a80e045bc3e6b2e71
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/557194
    Fuchsia-Auto-Submit: Arthur Sfez <asfez@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/network/tests/util.cc
+++ src/connectivity/network/tests/util.cc
@@ -62,7 +62,7 @@ ssize_t fill_stream_send_buf(int fd, int peer_fd) {
   EXPECT_EQ(tv_len, sizeof(original_tv));
   const struct timeval tv = {
       .tv_sec = 0,
-      .tv_usec = 1 << 13,  // ~8ms
+      .tv_usec = 1 << 14,  // ~16ms
   };
   EXPECT_EQ(setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)), 0) << strerror(errno);
 #endif
