a3bb150a49017061ad3a2a5ee1b0e09666926fd4
Tamir Duberstein <tamird@google.com>
2021-07-21 18:46:06 +0000

    [enclosed_runner] Deny unused results
    Avoid detached tasks and unnecessary abstraction while I'm here.
    
    Deny unused results where trivial.
    
    Bug: 73468
    Change-Id: If2b3f3e6b5b5f84e0eb18d625e0ae49fd0495dcf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/558145
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/network/netstack3/tools/enclosed_runner/src/main.rs
+++ src/connectivity/network/netstack3/tools/enclosed_runner/src/main.rs
@@ -6,14 +6,11 @@ use anyhow::{format_err, Context as _, Error};
 use fidl_fuchsia_hardware_ethernet as zx_eth;
 use fidl_fuchsia_net as net;
 use fidl_fuchsia_net_icmp;
-use fidl_fuchsia_net_stack::{self as netstack, StackMarker, StackProxy};
+use fidl_fuchsia_net_stack::{self as netstack, StackMarker};
 use fidl_fuchsia_net_stack_ext::FidlReturn;
 use fidl_fuchsia_posix_socket;
 use fuchsia_async as fasync;
-use fuchsia_component::{
-    client::AppBuilder,
-    server::{NestedEnvironment, ServiceFs},
-};
+use fuchsia_component::{client::AppBuilder, server::ServiceFs};
 use fuchsia_zircon as zx;
 use futures::prelude::*;
 use std::fs::File;
@@ -33,10 +30,6 @@ struct Opt {
     /// IP address and subnet prefix  MUST be in the form [IP]/[prefix].
     #[structopt(short = "i")]
     ip_prefix: Option<String>,
-}
-
-struct Netstack {
-    stack: StackProxy,
 }
 
 fn parse_ip_addr_and_prefix(addr: &str) -> Result<(net::IpAddress, u8), Error> {
@@ -78,89 +71,69 @@ fn mask_with_prefix(mut ip: net::IpAddress, prefix: u8) -> net::IpAddress {
     ip
 }
 
-impl Netstack {
-    fn new(env: &NestedEnvironment) -> Result<Self, Error> {
-        Ok(Self { stack: env.connect_to_protocol::<StackMarker>()? })
-    }
+#[fasync::run_singlethreaded]
+async fn main() -> Result<(), Error> {
+    let Opt { ethernet, ip_prefix } = Opt::from_args();
 
-    async fn add_ethernet(&self, path: String) -> Result<u64, Error> {
-        let dev = File::open(&path).context("failed to open device")?;
-        let topological_path =
-            fdio::device_get_topo_path(&dev).context("failed to get topological path")?;
+    let mut ns_builder = AppBuilder::new(NETSTACK_URL.to_string());
+    let directory_request = ns_builder.directory_request().context("directory request")?;
+
+    let mut services = ServiceFs::new_local();
+    let _: &mut ServiceFs<_> = services
+        .add_proxy_service_to::<StackMarker, _>(directory_request.clone())
+        .add_proxy_service_to::<fidl_fuchsia_posix_socket::ProviderMarker, _>(
+            directory_request.clone(),
+        )
+        .add_proxy_service_to::<fidl_fuchsia_net_icmp::ProviderMarker, _>(
+            directory_request.clone(),
+        );
+    let env = services.create_nested_environment("netstack3-env").context("create environment")?;
+    let services = services.for_each_concurrent(None, futures::future::ready);
+
+    let netstack = ns_builder.spawn(env.launcher()).context("spawn")?;
+
+    if let Some(path) = ethernet {
+        let stack = netstack.connect_to_protocol::<StackMarker>().context("connect to Stack")?;
+
+        let dev = File::open(&path).context("open device")?;
+        let topological_path = fdio::device_get_topo_path(&dev).context("get topological path")?;
         let fd = dev.as_raw_fd();
         let mut client = 0;
         zx::Status::ok(unsafe { fdio::fdio_sys::fdio_get_service_handle(fd, &mut client) })
-            .context("failed to get fdio service handle")?;
+            .context("get fdio service handle")?;
         let dev = fidl::endpoints::ClientEnd::<zx_eth::DeviceMarker>::new(
             // Safe because we checked the return status above.
             zx::Channel::from(unsafe { zx::Handle::from_raw(client) }),
         );
-        let id = self
-            .stack
+        let id = stack
             .add_ethernet_interface(&topological_path, dev)
             .await
             .squash_result()
-            .context("error adding device")?;
-        Ok(id)
-    }
-
-    async fn set_ip(
-        &self,
-        id: u64,
-        ip_address: net::IpAddress,
-        prefix_len: u8,
-    ) -> Result<(), Error> {
-        let mut fidl_addr = net::Subnet { addr: ip_address.clone(), prefix_len };
-        let () = self
-            .stack
-            .add_interface_address(id, &mut fidl_addr)
-            .await
-            .squash_result()
-            .context(format!("error adding interface address {}", id))?;
-        let () = self
-            .stack
-            .add_forwarding_entry(&mut netstack::ForwardingEntry {
-                subnet: net::Subnet { addr: mask_with_prefix(ip_address, prefix_len), prefix_len },
-                destination: netstack::ForwardingDestination::DeviceId(id),
-            })
-            .await
-            .squash_result()
-            .context("error adding forwarding entry")?;
-        Ok(())
-    }
-}
-
-#[fasync::run_singlethreaded]
-async fn main() -> Result<(), Error> {
-    let options = Opt::from_args();
-    // create nested environment for netstack:
-
-    let mut ns_builder = AppBuilder::new(NETSTACK_URL.to_string());
-
-    let mut services = ServiceFs::new_local();
-    services
-        .add_proxy_service_to::<StackMarker, _>(ns_builder.directory_request().unwrap().clone())
-        .add_proxy_service_to::<fidl_fuchsia_posix_socket::ProviderMarker, _>(
-            ns_builder.directory_request().unwrap().clone(),
-        )
-        .add_proxy_service_to::<fidl_fuchsia_net_icmp::ProviderMarker, _>(
-            ns_builder.directory_request().unwrap().clone(),
-        );
-
-    let env = services.create_nested_environment("netstack3-env")?;
-
-    let _netstack = ns_builder.spawn(env.launcher())?;
-    fasync::Task::local(services.collect()).detach();
-
-    let stack = Netstack::new(&env)?;
-    if let Some(eth_path) = options.ethernet {
-        // open ethernet device and send to stack.
-        let id = stack.add_ethernet(eth_path).await?;
+            .context("add ethernet device")?;
         println!("Created interface with id {}", id);
 
-        if let Some(ip_prefix) = options.ip_prefix {
-            let (addr, prefix) = parse_ip_addr_and_prefix(&ip_prefix)?;
-            stack.set_ip(id, addr, prefix).await?;
+        if let Some(ip_prefix) = ip_prefix {
+            let (ip_address, prefix_len) =
+                parse_ip_addr_and_prefix(&ip_prefix).context("parse IP")?;
+            let () = stack
+                .add_interface_address(
+                    id,
+                    &mut net::Subnet { addr: ip_address.clone(), prefix_len },
+                )
+                .await
+                .squash_result()
+                .context("add interface address")?;
+            let () = stack
+                .add_forwarding_entry(&mut netstack::ForwardingEntry {
+                    subnet: net::Subnet {
+                        addr: mask_with_prefix(ip_address, prefix_len),
+                        prefix_len,
+                    },
+                    destination: netstack::ForwardingDestination::DeviceId(id),
+                })
+                .await
+                .squash_result()
+                .context("add forwarding entry")?;
             println!("Configured interface {} for address: {}", id, ip_prefix);
         }
     }
@@ -173,11 +146,5 @@ async fn main() -> Result<(), Error> {
     println!("killall enclosed-runner.cmx");
     println!("or CTRL^C out.");
 
-    // Await forever on an empty future.
-    // This will cause enclosed-runner to never exit, unless it's killed
-    // from outside. We want enclosed-runner to keep alive so we will have
-    // the test environment alive as well so we can chrealm into it, or just
-    // observe netstack3.
-    let () = futures::future::pending().await;
-    Ok(())
+    Ok(services.await)
 }
