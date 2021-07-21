5bd40fa9fbf490cb3d41f29ee6ef247fca5d90f4
Charles Whitten <cwhitten@google.com>
2021-07-21 18:17:02 +0000

    [Ermine][Quicksettings] Add channel service
    This CL adds basic channel service for quicksettings. It provides optedIntoUpdates value which will be used for the ‘About Fuchsia’ quicksetting. Channel service does not provide any channel control functionality; this will be added in subsequent CLs.
    
    Bug: 79588, 80893
    
    Change-Id: Ib374547d8dcce7ca460f01de29e3cb64f526b883
    Reviewed-on: https://fuchsia-review.googlesource.com/c/experiences/+/557905
    Reviewed-by: Yeonhee Lee <yhlee@google.com>
    Commit-Queue: Charles Whitten <cwhitten@google.com>

---
+++ src/experiences/session_shells/ermine/shell/lib/src/services/settings/channel_service.dart
@@ -0,0 +1,67 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui';
+
+import 'package:ermine/src/services/settings/task_service.dart';
+import 'package:fidl_fuchsia_update_channelcontrol/fidl_async.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fuchsia_services/services.dart';
+
+/// Defines a [TaskService] to control channel in QuickSettings.
+class ChannelService extends TaskService {
+  late final VoidCallback onChanged;
+  late final ValueChanged<bool> onConnected;
+
+  final _control = ChannelControlProxy();
+
+  late String _currentChannel;
+  late List<String> _availableChannels;
+
+  @override
+  Future<void> start() async {
+    Incoming.fromSvcPath().connectToService(_control);
+
+    await _control.getTarget().then((name) {
+      _currentChannel = _shortNames[name] ?? name;
+      onChanged();
+    });
+
+    await _control.getTargetList().then((channels) {
+      _availableChannels = channels;
+    });
+  }
+
+  @override
+  Future<void> stop() async {
+    dispose();
+  }
+
+  String get channel => _currentChannel;
+  set channel(String name) {
+    _currentChannel = name;
+    _control.setTarget(name);
+    onChanged();
+  }
+
+  bool get optedIntoUpdates =>
+      ((_shortNames[_currentChannel] ?? _currentChannel) != 'devhost') &&
+      ((_shortNames[_currentChannel] ?? _currentChannel) != 'fuchsia.com');
+
+  List<String> get channels => _availableChannels;
+
+  /// Returns the mapping of internal channel name to it's short name.
+  static final _shortNames = <String, String>{
+    '2gmrtg05aspff9bisjxsu46no.fuchsia-updates.googleusercontent.com': 'test',
+    '4igty6t46noanfx782kp9ywyc.fuchsia-updates.googleusercontent.com':
+        'dogfood',
+    'b5cvjayvpm75pukjav4d4hurk.fuchsia-updates.googleusercontent.com': 'beta',
+    '4x15snlqjzlsgunidd0q1hj8n.fuchsia-updates.googleusercontent.com': 'stable',
+  };
+
+  @override
+  void dispose() {
+    _control.ctrl.close();
+  }
+}
