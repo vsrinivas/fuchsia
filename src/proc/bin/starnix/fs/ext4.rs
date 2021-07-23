345bccd0999bee06e31e432e1d33f4bdd9dc7faf
Theodore Dubois <tbodt@google.com>
2021-07-23 18:32:32 +0000

    [starnix] Support the Android sparse image format for ext4
    We can now directly read the images produced by the Android tree.
    
    Change-Id: I7c31b756a66b6824e2ef1c739c98ebcf22223f76
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/558967
    Commit-Queue: Theodore Dubois <tbodt@google.com>
    Fuchsia-Auto-Submit: Theodore Dubois <tbodt@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>

--- src/proc/bin/starnix/fs/ext4.rs
+++ src/proc/bin/starnix/fs/ext4.rs
@@ -3,11 +3,14 @@
 // found in the LICENSE file.
 
 use ext4_read_only::parser::Parser as ExtParser;
-use ext4_read_only::readers::VmoReader as ExtVmoReader;
+use ext4_read_only::readers::{self as ext4_readers, VmoReader as ExtVmoReader};
 use ext4_read_only::structs as ext_structs;
 use fuchsia_zircon::{self as zx};
 use once_cell::sync::OnceCell;
+use std::collections::BTreeMap;
+use std::mem::size_of_val;
 use std::sync::{Arc, Weak};
+use zerocopy::{AsBytes, FromBytes};
 
 use super::*;
 use crate::devices::AnonNodeDevice;
@@ -15,7 +18,7 @@ use crate::logging::impossible_error;
 use crate::types::*;
 
 pub struct ExtFilesystem {
-    parser: ExtParser<ExtVmoReader>,
+    parser: ExtParser<AndroidSparseReader<ExtVmoReader>>,
 }
 impl FileSystemOps for Arc<ExtFilesystem> {}
 
@@ -29,12 +32,9 @@ impl ExtFilesystem {
     #[allow(dead_code)]
     pub fn new(vmo: zx::Vmo) -> Result<FileSystemHandle, Errno> {
         let size = vmo.get_size().map_err(|_| EIO)?;
-        let fs = Arc::new(Self {
-            parser: ExtParser::new(ExtVmoReader::new(Arc::new(fidl_fuchsia_mem::Buffer {
-                size,
-                vmo,
-            }))),
-        });
+        let vmo_reader = ExtVmoReader::new(Arc::new(fidl_fuchsia_mem::Buffer { vmo, size }));
+        let parser = ExtParser::new(AndroidSparseReader::new(vmo_reader).map_err(|_| EIO)?);
+        let fs = Arc::new(Self { parser });
         let root = FsNode::new_root(
             ExtDirectory { inner: ExtNode::new(fs.clone(), ext_structs::ROOT_INODE_NUM)? },
             AnonNodeDevice::new(0),
@@ -128,5 +128,137 @@ fn vmo_error(err: zx::Status) -> Errno {
     match err {
         zx::Status::NO_MEMORY => ENOMEM,
         _ => impossible_error(err),
+    }
+}
+
+struct AndroidSparseReader<R: ext4_readers::Reader> {
+    inner: R,
+    header: SparseHeader,
+    chunks: BTreeMap<usize, SparseChunk>,
+}
+
+/// Copied from system/core/libsparse/sparse_format.h
+#[derive(AsBytes, FromBytes, Default, Debug)]
+#[repr(C)]
+struct SparseHeader {
+    /// 0xed26ff3a
+    magic: u32,
+    /// (0x1) - reject images with higher major versions
+    major_version: u16,
+    /// (0x0) - allow images with higer minor versions
+    minor_version: u16,
+    /// 28 bytes for first revision of the file format
+    file_hdr_sz: u16,
+    /// 12 bytes for first revision of the file format
+    chunk_hdr_sz: u16,
+    /// block size in bytes, must be a multiple of 4 (4096)
+    blk_sz: u32,
+    /// total blocks in the non-sparse output image
+    total_blks: u32,
+    /// total chunks in the sparse input image
+    total_chunks: u32,
+    /// CRC32 checksum of the original data, counting "don't care"
+    /// as 0. Standard 802.3 polynomial, use a Public Domain
+    /// table implementation
+    image_checksum: u32,
+}
+
+const SPARSE_HEADER_MAGIC: u32 = 0xed26ff3a;
+
+/// Copied from system/core/libsparse/sparse_format.h
+#[derive(AsBytes, FromBytes, Default)]
+#[repr(C)]
+struct RawChunkHeader {
+    /// 0xCAC1 -> raw; 0xCAC2 -> fill; 0xCAC3 -> don't care
+    chunk_type: u16,
+    _reserved: u16,
+    /// in blocks in output image
+    chunk_sz: u32,
+    /// in bytes of chunk input file including chunk header and data
+    total_sz: u32,
+}
+
+#[derive(Debug)]
+enum SparseChunk {
+    Raw { in_offset: usize, in_size: usize },
+    Fill { fill: [u8; 4] },
+    DontCare,
+}
+
+const CHUNK_TYPE_RAW: u16 = 0xCAC1;
+const CHUNK_TYPE_FILL: u16 = 0xCAC2;
+const CHUNK_TYPE_DONT_CARE: u16 = 0xCAC3;
+
+impl<R: ext4_readers::Reader> AndroidSparseReader<R> {
+    fn new(inner: R) -> Result<Self, anyhow::Error> {
+        let mut header = SparseHeader::default();
+        inner.read(0, header.as_bytes_mut())?;
+        let mut chunks = BTreeMap::new();
+        if header.magic == SPARSE_HEADER_MAGIC {
+            if header.major_version != 1 {
+                anyhow::bail!("unknown sparse image major version {}", header.major_version);
+            }
+            let mut in_offset = size_of_val(&header);
+            let mut out_offset = 0;
+            for _ in 0..header.total_chunks {
+                let mut chunk_header = RawChunkHeader::default();
+                inner.read(in_offset, chunk_header.as_bytes_mut())?;
+                let data_offset = in_offset + size_of_val(&chunk_header);
+                let data_size = chunk_header.total_sz as usize - size_of_val(&chunk_header);
+                in_offset += chunk_header.total_sz as usize;
+                let chunk_out_offset = out_offset;
+                out_offset += chunk_header.chunk_sz as usize * header.blk_sz as usize;
+                let chunk = match chunk_header.chunk_type {
+                    CHUNK_TYPE_RAW => {
+                        SparseChunk::Raw { in_offset: data_offset, in_size: data_size }
+                    }
+                    CHUNK_TYPE_FILL => {
+                        let mut fill = [0u8; 4];
+                        if data_size != size_of_val(&fill) {
+                            anyhow::bail!(
+                                "fill chunk of sparse image is the wrong size: {}, should be {}",
+                                data_size,
+                                size_of_val(&fill),
+                            );
+                        }
+                        inner.read(data_offset, fill.as_bytes_mut())?;
+                        SparseChunk::Fill { fill }
+                    }
+                    CHUNK_TYPE_DONT_CARE | _ => SparseChunk::DontCare,
+                };
+                chunks.insert(chunk_out_offset, chunk);
+            }
+        }
+        Ok(Self { inner, header, chunks })
+    }
+}
+
+impl<R: ext4_readers::Reader> ext4_readers::Reader for AndroidSparseReader<R> {
+    fn read(&self, offset: usize, data: &mut [u8]) -> Result<(), ext4_readers::ReaderError> {
+        if self.header.magic != SPARSE_HEADER_MAGIC {
+            return self.inner.read(offset, data);
+        }
+        let total_size = self.header.total_blks as usize * self.header.blk_sz as usize;
+
+        let (chunk_start, chunk) = match self.chunks.range(..offset + 1).next_back() {
+            Some(x) => x,
+            _ => return Err(ext4_readers::ReaderError::OutOfBounds(offset, total_size)),
+        };
+        match chunk {
+            SparseChunk::Raw { in_offset, in_size } => {
+                let chunk_offset = offset - chunk_start;
+                if chunk_offset > *in_size {
+                    return Err(ext4_readers::ReaderError::OutOfBounds(chunk_offset, total_size));
+                }
+                self.inner.read(in_offset + chunk_offset, data)?;
+            }
+            SparseChunk::Fill { fill } => {
+                for i in offset..offset + data.len() {
+                    data[i - offset] = fill[offset % fill.len()];
+                }
+            }
+            SparseChunk::DontCare => {}
+        }
+        Ok(())
     }
 }
