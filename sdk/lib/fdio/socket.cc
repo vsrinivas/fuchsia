vsrinivas/fuchsia

commit a31257b11c3ff92523d776498541165ddc3d0160

[netstack] Use SetDisposition instead of Shutdown.
Bug: 78128
Change-Id: I2973da660c41d3d009510a9082dd459d47689591
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/557118
Fuchsia-Auto-Submit: Arthur Sfez <asfez@google.com>
Commit-Queue: Arthur Sfez <asfez@google.com>
Reviewed-by: Tamir Duberstein <tamird@google.com>

src/connectivity/network/netstack/fuchsia_posix_socket.go
@@ -1489,7 +1489,7 @@ func (eps *endpointWithSocket) loopWrite(ch chan<- struct{}) {
 			// and shutting it down here would cause signals to be asserted twice,
 			// which can produce races in the client.
 			if eps.ep.Readiness(waiter.EventErr)&waiter.EventErr == 0 {
-				if err := eps.local.Shutdown(zx.SocketShutdownRead); err != nil {
+				if err := eps.local.SetDisposition(0, zx.SocketDispositionWriteDisabled); err != nil {
 					panic(err)
 				}
 				_ = syslog.DebugTf("zx_socket_shutdown", "%p: ZX_SOCKET_SHUTDOWN_READ", eps)
@@ -1559,7 +1559,7 @@ func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
 			// and shutting it down here would cause signals to be asserted twice,
 			// which can produce races in the client.
 			if eps.ep.Readiness(waiter.EventErr)&waiter.EventErr == 0 {
-				if err := eps.local.Shutdown(zx.SocketShutdownWrite); err != nil {
+				if err := eps.local.SetDisposition(zx.SocketDispositionWriteDisabled, 0); err != nil {
 					panic(err)
 				}
 				_ = syslog.DebugTf("zx_socket_shutdown", "%p: ZX_SOCKET_SHUTDOWN_WRITE", eps)
@@ -1627,52 +1627,42 @@ func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
 // TODO(https://fxbug.dev/78129): After ABI transition, create a copy of this
 // Shutdown2 method but name it Shutdown.
 func (eps *endpointWithSocket) Shutdown2(_ fidl.Context, how socket.ShutdownMode) (socket.BaseSocketShutdown2Result, error) {
-	{
-		how, err := func() (uint32, posix.Errno) {
-			var shutdownFlags uint32
+	var disposition, dispositionPeer uint32
 
-			if how&socket.ShutdownModeRead != 0 {
-				shutdownFlags |= zx.SocketShutdownRead
-				how ^= socket.ShutdownModeRead
-			}
-			if how&socket.ShutdownModeWrite != 0 {
-				shutdownFlags |= zx.SocketShutdownWrite
-				how ^= socket.ShutdownModeWrite
-			}
-			if shutdownFlags == 0 || how != 0 {
-				return 0, posix.ErrnoEinval
-			}
-
-			return shutdownFlags, 0
-		}()
-
-		if err != 0 {
-			return socket.BaseSocketShutdown2ResultWithErr(err), nil
-		}
-
-		if h := eps.local.Handle(); !h.IsValid() {
-			return socket.BaseSocketShutdown2ResultWithErr(tcpipErrorToCode(&tcpip.ErrNotConnected{})), nil
-		}
-
-		if err := eps.peer.Shutdown(how); err != nil {
-			return socket.BaseSocketShutdown2Result{}, err
-		}
-
-		// Only handle a shutdown read on the endpoint here. Shutdown write is
-		// performed by the loopWrite goroutine: it ensures that it happens *after*
-		// all the buffered data in the zircon socket was read.
-		if how&zx.SocketShutdownRead != 0 {
-			switch err := eps.ep.Shutdown(tcpip.ShutdownRead); err.(type) {
-			case nil, *tcpip.ErrNotConnected:
-				// Shutdown can return ErrNotConnected if the endpoint was connected
-				// but no longer is.
-			default:
-				panic(err)
-			}
-		}
-
-		return socket.BaseSocketShutdown2ResultWithResponse(socket.BaseSocketShutdown2Response{}), nil
+	if how&socket.ShutdownModeWrite != 0 {
+		disposition = zx.SocketDispositionWriteDisabled
+		how ^= socket.ShutdownModeWrite
 	}
+	if how&socket.ShutdownModeRead != 0 {
+		dispositionPeer = zx.SocketDispositionWriteDisabled
+		how ^= socket.ShutdownModeRead
+	}
+	if how != 0 || (disposition == 0 && dispositionPeer == 0) {
+		return socket.BaseSocketShutdown2ResultWithErr(posix.ErrnoEinval), nil
+	}
+
+	if h := eps.local.Handle(); !h.IsValid() {
+		return socket.BaseSocketShutdown2ResultWithErr(tcpipErrorToCode(&tcpip.ErrNotConnected{})), nil
+	}
+
+	if err := eps.peer.SetDisposition(disposition, dispositionPeer); err != nil {
+		return socket.BaseSocketShutdown2Result{}, err
+	}
+
+	// Only handle a shutdown read on the endpoint here. Shutdown write is
+	// performed by the loopWrite goroutine: it ensures that it happens *after*
+	// all the buffered data in the zircon socket was read.
+	if how&socket.ShutdownModeRead != 0 {
+		switch err := eps.ep.Shutdown(tcpip.ShutdownRead); err.(type) {
+		case nil, *tcpip.ErrNotConnected:
+			// Shutdown can return ErrNotConnected if the endpoint was connected
+			// but no longer is.
+		default:
+			panic(err)
+		}
+	}
+
+	return socket.BaseSocketShutdown2ResultWithResponse(socket.BaseSocketShutdown2Response{}), nil
 }
 
 type datagramSocketImpl struct {

commit f546709dbe76ba57f229912d8ce5533a6693c3d4

[netstack] Add BaseSocket.Shutdown fidl method
The deprecated DatagramSocket.Shutdown fidl method is not used anywhere
anymore. This commit removes it. But until the next ABI transition, we
still need to keep the now deprecated BaseSocket.Shutdown2 method.

Remove the legacy test target where shutdown(2) implementation called
zx_socket_shutdown on the zircon socket, and instead add a new legacy
test target in which fdio uses the now deprecated Shutdown2 fidl method.

Now that no clients shutdown the zircon sockets themselves, the loopPoll
can be removed from netstack, because netstack calls shutdown on the
endpoint directly when the BaseSocket.Shutdown fidl method is called.

Note that this commit also fixes a dormant bug introduced in
commit a31257b11c3ff92523d776498541165ddc3d0160, where the
implementation of the fidl method Shutdown for StreamSocket would never
call Shutdown on the endpoint. It revealed itself when loopPoll was
removed.

Bug: 78129
Change-Id: I0c7193bcb0905121a08f03898c1c29f469c74f71
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/558885
Reviewed-by: Tamir Duberstein <tamird@google.com>
Reviewed-by: Roland McGrath <mcgrathr@google.com>
Commit-Queue: Arthur Sfez <asfez@google.com>
API-Review: Bruno Dal Bo <brunodalbo@google.com>

sdk/fidl/fuchsia.posix.socket/socket.fidl
@@ -50,8 +50,11 @@ protocol BaseSocket {
         addr fuchsia.net.SocketAddress;
     }) error fuchsia.posix.Errno;
     /// Shuts down part of the socket.
-    // TODO(https://fxbug.dev/78129): This method is required during the ABI transition. Once
-    // BaseSocket provides the Shutdown method and that every client uses it, remove Shutdown2.
+    Shutdown(struct {
+        mode ShutdownMode;
+    }) -> (struct {}) error fuchsia.posix.Errno;
+    // TODO(https://fxbug.dev/78129): Remove after ABI transition.
+    @deprecated("Use Shutdown instead")
     Shutdown2(struct {
         mode ShutdownMode;
     }) -> (struct {}) error fuchsia.posix.Errno;
@@ -384,13 +387,6 @@ type SendMsgFlags = strict bits : uint16 {
 /// All methods on this type are nonblocking; their exact behaviors match their Linux counterparts.
 protocol DatagramSocket {
     compose BaseSocket;
-
-    // TODO(https://fxbug.dev/78129): After ABI transition, move this method into BaseSocket.
-    /// Shuts down part of the socket.
-    @deprecated("Use Shutdown2 instead")
-    Shutdown(struct {
-        mode ShutdownMode;
-    }) -> (struct {}) error fuchsia.posix.Errno;
 
     /// Receives a message from the socket.
     RecvMsg(struct {

sdk/lib/fdio/socket.cc
@@ -27,6 +27,21 @@ namespace fnet = fuchsia_net;
 
 namespace {
 
+// TODO(https://fxbug.dev/78129): Remove after ABI transition.
+bool use_legacy_shutdown2_fidl() {
+  static std::once_flag once;
+  static bool legacy;
+
+  std::call_once(once, [&]() {
+    legacy = []() {
+      constexpr char kLegacyShutdown2Fidl[] = "LEGACY_SHUTDOWN2_FIDL";
+      const char* const legacy_env = getenv(kLegacyShutdown2Fidl);
+      return legacy_env && strcmp(legacy_env, "1") == 0;
+    }();
+  });
+  return legacy;
+}
+
 // A helper structure to keep a socket address and the variants allocations in stack.
 struct SocketAddress {
   fnet::wire::SocketAddress address;
@@ -1133,8 +1148,23 @@ struct BaseSocket {
       default:
         return ZX_ERR_INVALID_ARGS;
     }
-    // TODO(https://fxbug.dev/78129): Use Shutdown instead of Shutdown2 after ABI transition.
-    auto response = client().Shutdown2(mode);
+
+    if (use_legacy_shutdown2_fidl()) {
+      auto response = client().Shutdown2(mode);
+      zx_status_t status = response.status();
+      if (status != ZX_OK) {
+        return status;
+      }
+      auto const& result = response.Unwrap()->result;
+      if (result.is_err()) {
+        *out_code = static_cast<int16_t>(result.err());
+        return ZX_OK;
+      }
+      *out_code = 0;
+      return ZX_OK;
+    }
+
+    auto response = client().Shutdown(mode);
     zx_status_t status = response.status();
     if (status != ZX_OK) {
       return status;
@@ -1342,21 +1372,6 @@ Errno zxsio_posix_ioctl(int req, va_list va, F fallback) {
     default:
       return fallback(req, va);
   }
-}
-
-// TODO(https://fxbug.dev/78129): Remove after ABI transition.
-bool use_legacy_stream_socket_shutdown() {
-  static std::once_flag once;
-  static bool legacy;
-
-  std::call_once(once, [&]() {
-    legacy = []() {
-      constexpr char kLegacyStreamSocketShutdown[] = "LEGACY_STREAM_SOCKET_SHUTDOWN";
-      const char* const legacy_env = getenv(kLegacyStreamSocketShutdown);
-      return legacy_env && strcmp(legacy_env, "1") == 0;
-    }();
-  });
-  return legacy;
 }
 
 }  // namespace
@@ -1899,19 +1914,6 @@ struct stream_socket : public zxio {
   }
 
   zx_status_t shutdown(int how, int16_t* out_code) override {
-    if (use_legacy_stream_socket_shutdown()) {
-      *out_code = 0;
-      zx_signals_t observed;
-      zx_status_t status = zxio_stream_socket().pipe.socket.wait_one(
-          ZX_SOCKET_PEER_CLOSED, zx::time::infinite_past(), &observed);
-      if (status == ZX_OK || status == ZX_ERR_TIMED_OUT) {
-        if (observed & ZX_SOCKET_PEER_CLOSED) {
-          return ZX_ERR_NOT_CONNECTED;
-        }
-        return zxio::shutdown(how, out_code);
-      }
-      return status;
-    }
     return BaseSocket(zxio_stream_socket().client).shutdown(how, out_code);
   }
 

sdk/lib/zxio/pipe.cc
@@ -113,14 +113,18 @@ static constexpr zxio_ops_t zxio_pipe_ops = []() {
   };
 
   ops.shutdown = [](zxio_t* io, zxio_shutdown_options_t options) {
-    // TODO(https://fxbug.dev/78129): Update to zx::socket::set_disposition() once stream sockets
-    // in fdio stop using this zxio shutdown operation.
-    static_assert(ZX_SOCKET_SHUTDOWN_READ == ZXIO_SHUTDOWN_OPTIONS_READ);
-    static_assert(ZX_SOCKET_SHUTDOWN_WRITE == ZXIO_SHUTDOWN_OPTIONS_WRITE);
-    if ((options & ZX_SOCKET_SHUTDOWN_MASK) != options) {
+    if ((options & ZXIO_SHUTDOWN_OPTIONS_MASK) != options) {
       return ZX_ERR_INVALID_ARGS;
     }
-    return zxio_get_pipe(io).socket.shutdown(options);
+    uint32_t disposition = 0;
+    if (options & ZXIO_SHUTDOWN_OPTIONS_WRITE) {
+      disposition = ZX_SOCKET_DISPOSITION_WRITE_DISABLED;
+    }
+    uint32_t disposition_peer = 0;
+    if (options & ZXIO_SHUTDOWN_OPTIONS_READ) {
+      disposition_peer = ZX_SOCKET_DISPOSITION_WRITE_DISABLED;
+    }
+    return zxio_get_pipe(io).socket.set_disposition(disposition, disposition_peer);
   };
 
   return ops;

src/connectivity/network/netstack/fuchsia_posix_socket.go
@@ -962,7 +962,7 @@ type endpointWithSocket struct {
 
 		// loop{Read,Write,Poll}Done are signaled iff loop{Read,Write,Poll} have
 		// exited, respectively.
-		loopPollDone, loopReadDone, loopWriteDone <-chan struct{}
+		loopReadDone, loopWriteDone <-chan struct{}
 	}
 
 	// closing is signaled iff close has been called.
@@ -1042,36 +1042,6 @@ func (eps *endpointWithSocket) HUp() {
 	})
 }
 
-// TODO(https://fxbug.dev/78129): Remove loopPoll after ABI transition.
-func (eps *endpointWithSocket) loopPoll(ch chan<- struct{}) {
-	defer close(ch)
-
-	sigs := zx.Signals(zx.SignalSocketWriteDisabled | localSignalClosing)
-
-	for {
-		obs, err := zxwait.WaitContext(context.Background(), zx.Handle(eps.local), sigs)
-		if err != nil {
-			panic(err)
-		}
-
-		if obs&sigs&zx.SignalSocketWriteDisabled != 0 {
-			sigs ^= zx.SignalSocketWriteDisabled
-			switch err := eps.ep.Shutdown(tcpip.ShutdownRead); err.(type) {
-			case nil, *tcpip.ErrNotConnected:
-				// Shutdown can return ErrNotConnected if the endpoint was connected
-				// but no longer is.
-			default:
-				panic(err)
-			}
-		}
-
-		if obs&localSignalClosing != 0 {
-			// We're shutting down.
-			return
-		}
-	}
-}
-
 type endpointWithEvent struct {
 	endpoint
 
@@ -1119,8 +1089,16 @@ func (epe *endpointWithEvent) shutdown(how socket.ShutdownMode) (posix.Errno, er
 	return 0, nil
 }
 
-// TODO(https://fxbug.dev/78129): After ABI transition, create a copy of this
-// Shutdown2 method but name it Shutdown.
+func (epe *endpointWithEvent) Shutdown(_ fidl.Context, how socket.ShutdownMode) (socket.BaseSocketShutdownResult, error) {
+	if errno, err := epe.shutdown(how); err != nil {
+		return socket.BaseSocketShutdownResult{}, err
+	} else if errno != 0 {
+		return socket.BaseSocketShutdownResultWithErr(errno), nil
+	}
+	return socket.BaseSocketShutdownResultWithResponse(socket.BaseSocketShutdownResponse{}), nil
+}
+
+// TODO(https://fxbug.dev/78129): Remove after ABI transition.
 func (epe *endpointWithEvent) Shutdown2(_ fidl.Context, how socket.ShutdownMode) (socket.BaseSocketShutdown2Result, error) {
 	if errno, err := epe.shutdown(how); err != nil {
 		return socket.BaseSocketShutdown2Result{}, err
@@ -1128,16 +1106,6 @@ func (epe *endpointWithEvent) Shutdown2(_ fidl.Context, how socket.ShutdownMode)
 		return socket.BaseSocketShutdown2ResultWithErr(errno), nil
 	}
 	return socket.BaseSocketShutdown2ResultWithResponse(socket.BaseSocketShutdown2Response{}), nil
-}
-
-// TODO(https://fxbug.dev/78129): After ABI transition, delete this function.
-func (epe *endpointWithEvent) Shutdown(_ fidl.Context, how socket.ShutdownMode) (socket.DatagramSocketShutdownResult, error) {
-	if errno, err := epe.shutdown(how); err != nil {
-		return socket.DatagramSocketShutdownResult{}, err
-	} else if errno != 0 {
-		return socket.DatagramSocketShutdownResultWithErr(errno), nil
-	}
-	return socket.DatagramSocketShutdownResultWithResponse(socket.DatagramSocketShutdownResponse{}), nil
 }
 
 const localSignalClosing = zx.SignalUser1
@@ -1167,7 +1135,6 @@ func (eps *endpointWithSocket) close() {
 		// this routine will wait for them to return.
 		eps.mu.Lock()
 		channels := []<-chan struct{}{
-			eps.mu.loopPollDone,
 			eps.mu.loopReadDone,
 			eps.mu.loopWriteDone,
 		}
@@ -1211,9 +1178,6 @@ func (eps *endpointWithSocket) Listen(_ fidl.Context, backlog int16) (socket.Str
 	// incorrectly handling events on connected sockets.
 	eps.onListen.Do(func() {
 		eps.pending.supported = waiter.EventIn
-		// Start polling for any shutdown events from the client as shutdown is
-		// allowed on a listening stream socket.
-		eps.startPollLoop()
 		var entry waiter.Entry
 		cb := func() {
 			err := eps.pending.update()
@@ -1245,18 +1209,6 @@ func (eps *endpointWithSocket) Listen(_ fidl.Context, backlog int16) (socket.Str
 	_ = syslog.DebugTf("listen", "%p: backlog=%d", eps, backlog)
 
 	return socket.StreamSocketListenResultWithResponse(socket.StreamSocketListenResponse{}), nil
-}
-
-func (eps *endpointWithSocket) startPollLoop() {
-	eps.mu.Lock()
-	defer eps.mu.Unlock()
-	select {
-	case <-eps.closing:
-	default:
-		ch := make(chan struct{})
-		eps.mu.loopPollDone = ch
-		go eps.loopPoll(ch)
-	}
 }
 
 func (eps *endpointWithSocket) startReadWriteLoops() {
@@ -1306,7 +1258,6 @@ func (eps *endpointWithSocket) Connect(ctx fidl.Context, address fidlnet.SocketA
 				once.Do(func() {
 					go eps.wq.EventUnregister(&entry)
 					if m&waiter.EventErr == 0 {
-						eps.startPollLoop()
 						eps.startReadWriteLoops()
 					} else {
 						eps.HUp()
@@ -1599,8 +1550,7 @@ func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
 				case zx.ErrBadState:
 					// Writing has been disabled for this socket endpoint.
 					// TODO(https://fxbug.dev/78892): we might not need to shutdown the
-					// endpoint here, since it is done by the loopPoll goroutine and by
-					// the Shutdown fidl method.
+					// endpoint here, since it is done by the Shutdown fidl method.
 					switch err := eps.ep.Shutdown(tcpip.ShutdownRead); err.(type) {
 					case nil:
 					case *tcpip.ErrNotConnected:
@@ -1624,9 +1574,7 @@ func (eps *endpointWithSocket) loopRead(ch chan<- struct{}) {
 	}
 }
 
-// TODO(https://fxbug.dev/78129): After ABI transition, create a copy of this
-// Shutdown2 method but name it Shutdown.
-func (eps *endpointWithSocket) Shutdown2(_ fidl.Context, how socket.ShutdownMode) (socket.BaseSocketShutdown2Result, error) {
+func (eps *endpointWithSocket) shutdown(how socket.ShutdownMode) (posix.Errno, error) {
 	var disposition, dispositionPeer uint32
 
 	if how&socket.ShutdownModeWrite != 0 {
@@ -1638,21 +1586,21 @@ func (eps *endpointWithSocket) Shutdown2(_ fidl.Context, how socket.ShutdownMode
 		how ^= socket.ShutdownModeRead
 	}
 	if how != 0 || (disposition == 0 && dispositionPeer == 0) {
-		return socket.BaseSocketShutdown2ResultWithErr(posix.ErrnoEinval), nil
+		return posix.ErrnoEinval, nil
 	}
 
 	if h := eps.local.Handle(); !h.IsValid() {
-		return socket.BaseSocketShutdown2ResultWithErr(tcpipErrorToCode(&tcpip.ErrNotConnected{})), nil
+		return tcpipErrorToCode(&tcpip.ErrNotConnected{}), nil
 	}
 
 	if err := eps.peer.SetDisposition(disposition, dispositionPeer); err != nil {
-		return socket.BaseSocketShutdown2Result{}, err
+		return 0, err
 	}
 
 	// Only handle a shutdown read on the endpoint here. Shutdown write is
 	// performed by the loopWrite goroutine: it ensures that it happens *after*
 	// all the buffered data in the zircon socket was read.
-	if how&socket.ShutdownModeRead != 0 {
+	if dispositionPeer&zx.SocketDispositionWriteDisabled != 0 {
 		switch err := eps.ep.Shutdown(tcpip.ShutdownRead); err.(type) {
 		case nil, *tcpip.ErrNotConnected:
 			// Shutdown can return ErrNotConnected if the endpoint was connected
@@ -1662,6 +1610,25 @@ func (eps *endpointWithSocket) Shutdown2(_ fidl.Context, how socket.ShutdownMode
 		}
 	}
 
+	return 0, nil
+}
+
+func (eps *endpointWithSocket) Shutdown(_ fidl.Context, how socket.ShutdownMode) (socket.BaseSocketShutdownResult, error) {
+	if errno, err := eps.shutdown(how); err != nil {
+		return socket.BaseSocketShutdownResult{}, err
+	} else if errno != 0 {
+		return socket.BaseSocketShutdownResultWithErr(errno), nil
+	}
+	return socket.BaseSocketShutdownResultWithResponse(socket.BaseSocketShutdownResponse{}), nil
+}
+
+// TODO(https://fxbug.dev/78129): Remove after ABI transition.
+func (eps *endpointWithSocket) Shutdown2(_ fidl.Context, how socket.ShutdownMode) (socket.BaseSocketShutdown2Result, error) {
+	if errno, err := eps.shutdown(how); err != nil {
+		return socket.BaseSocketShutdown2Result{}, err
+	} else if errno != 0 {
+		return socket.BaseSocketShutdown2ResultWithErr(errno), nil
+	}
 	return socket.BaseSocketShutdown2ResultWithResponse(socket.BaseSocketShutdown2Response{}), nil
 }
 

sdk/lib/fdio/tests/fdio_socket.cc
@@ -46,10 +46,10 @@ class Server final : public fuchsia_posix_socket::testing::StreamSocket_TestBase
     completer.Close(ZX_OK);
   }
 
-  void Shutdown2(Shutdown2RequestView request, Shutdown2Completer::Sync& completer) override {
-    auto response = fuchsia_posix_socket::wire::BaseSocketShutdown2Response();
-    auto result = fuchsia_posix_socket::wire::BaseSocketShutdown2Result::WithResponse(
-        fidl::ObjectView<fuchsia_posix_socket::wire::BaseSocketShutdown2Response>::FromExternal(
+  void Shutdown(ShutdownRequestView request, ShutdownCompleter::Sync& completer) override {
+    auto response = fuchsia_posix_socket::wire::BaseSocketShutdownResponse();
+    auto result = fuchsia_posix_socket::wire::BaseSocketShutdownResult::WithResponse(
+        fidl::ObjectView<fuchsia_posix_socket::wire::BaseSocketShutdownResponse>::FromExternal(
             &response));
 
     shutdown_count_++;

src/connectivity/network/netstack/netstack_test.go
@@ -281,7 +281,6 @@ func TestEndpoint_Close(t *testing.T) {
 		name string
 	}{
 		{ch: eps.closing, name: "closing"},
-		{ch: eps.mu.loopPollDone, name: "loopPollDone"},
 		{ch: eps.mu.loopReadDone, name: "loopReadDone"},
 		{ch: eps.mu.loopWriteDone, name: "loopWriteDone"},
 	}
@@ -485,7 +484,6 @@ func TestTCPEndpointMapAcceptAfterReset(t *testing.T) {
 		name string
 	}{
 		{ch: eps.closing, name: "closing"},
-		{ch: eps.mu.loopPollDone, name: "loopPollDone"},
 		{ch: eps.mu.loopReadDone, name: "loopReadDone"},
 		{ch: eps.mu.loopWriteDone, name: "loopWriteDone"},
 	}

src/connectivity/network/tests/BUILD.gn
@@ -152,15 +152,14 @@ foreach(test, tests) {
 }
 
 # Same as netstack_bsdsocket_c_api_test but with its environment set to use the
-# legacy stream socket shutdown implementation.
+# deprecated Shutdown2 fidl method.
 #
 # TODO(https://fxbug.dev/78129): Remove after ABI transition.
 fuchsia_test_component(
-    "netstack_bsdsocket_c_api_legacy_stream_socket_shutdown_test_component") {
-  component_name = "netstack_bsdsocket_c_api_legacy_stream_socket_shutdown_test"
+    "netstack_bsdsocket_c_api_legacy_shutdown2_fidl_test_component") {
+  component_name = "netstack_bsdsocket_c_api_legacy_shutdown2_fidl_test"
   deps = [ ":netstack_bsdsocket_c_api_test" ]
-  manifest =
-      "meta/netstack_bsdsocket_c_api_legacy_stream_socket_shutdown_test.cmx"
+  manifest = "meta/netstack_bsdsocket_c_api_legacy_shutdown2_fidl_test.cmx"
 }
 
 fuchsia_test_package("netstack-c-api-tests-inner") {
@@ -168,7 +167,7 @@ fuchsia_test_package("netstack-c-api-tests-inner") {
 
   test_components = [
     ":connect_test",
-    ":netstack_bsdsocket_c_api_legacy_stream_socket_shutdown_test_component",
+    ":netstack_bsdsocket_c_api_legacy_shutdown2_fidl_test_component",
     ":netstack_external_network_test",
   ]
   foreach(test, tests) {

src/connectivity/network/tests/meta/netstack_bsdsocket_c_api_legacy_shutdown2_fidl_test.cmx
@@ -13,7 +13,7 @@
     "program": {
         "binary": "test/netstack_bsdsocket_c_api_test",
         "env_vars": [
-            "LEGACY_STREAM_SOCKET_SHUTDOWN=1"
+            "LEGACY_SHUTDOWN2_FIDL=1"
         ]
     },
     "sandbox": {