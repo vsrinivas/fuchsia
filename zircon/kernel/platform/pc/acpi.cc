093fe5ef1e0323c07ad4b354a868449c8076c63c
Nick Maniscalco <maniscalco@google.com>
2021-07-26 18:29:31 +0000

    [kernel][acpi] Eliminate function scoped static from AcpiParser
    This change is part of a series that eliminates function scoped static
    variables from the kernel.  In the kernel environment function scoped
    statics cannot be both safely and efficiently initialized so we are
    replacing them with alternatives.
    
    Bug: 79612
    Change-Id: Ife9f23e669fb5ce54523db920f06ff9b8ab50965
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/558322
    Commit-Queue: Nick Maniscalco <maniscalco@google.com>
    Reviewed-by: John Grossman <johngro@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>

--- zircon/kernel/platform/pc/acpi.cc
+++ zircon/kernel/platform/pc/acpi.cc
@@ -7,6 +7,7 @@
 #include <lib/acpi_lite.h>
 #include <lib/acpi_lite/zircon.h>
 #include <lib/console.h>
+#include <lib/lazy_init/lazy_init.h>
 #include <lib/zx/status.h>
 #include <zircon/types.h>
 
@@ -17,6 +18,10 @@ namespace {
 
 // System-wide ACPI parser.
 acpi_lite::AcpiParser* global_acpi_parser;
+
+lazy_init::LazyInit<acpi_lite::AcpiParser, lazy_init::CheckType::None,
+                    lazy_init::Destructor::Disabled>
+    g_parser;
 
 int ConsoleAcpiDump(int argc, const cmd_args* argv, uint32_t flags) {
   if (global_acpi_parser == nullptr) {
@@ -36,15 +41,16 @@ acpi_lite::AcpiParser& GlobalAcpiLiteParser() {
 }
 
 void PlatformInitAcpi(zx_paddr_t acpi_rsdp) {
+  ASSERT(global_acpi_parser == nullptr);
+
   // Create AcpiParser.
-  static acpi_lite::AcpiParser parser = [acpi_rsdp]() {
-    zx::status<acpi_lite::AcpiParser> result = acpi_lite::AcpiParserInit(acpi_rsdp);
-    if (result.is_error()) {
-      panic("Could not initialize ACPI. Error code: %d.", result.error_value());
-    }
-    return result.value();
-  }();
-  global_acpi_parser = &parser;
+  zx::status<acpi_lite::AcpiParser> result = acpi_lite::AcpiParserInit(acpi_rsdp);
+  if (result.is_error()) {
+    panic("Could not initialize ACPI. Error code: %d.", result.error_value());
+  }
+
+  g_parser.Initialize(result.value());
+  global_acpi_parser = &g_parser.Get();
 }
 
 STATIC_COMMAND_START
