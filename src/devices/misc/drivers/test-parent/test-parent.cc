fc16eee1e306b2d3ed7d4a371cb6530ac2623d7f
Suraj Malhotra <surajmalhotra@google.com>
2021-07-27 02:21:14 +0000

    [fdf][test] Add a new test parent driver
    This driver will provide an alternative test device which other drivers
    may autobind to. It will allow us to remote the test device auto-created
    by driver manager.
    
    Bug: 77388
    Change-Id: I9272c52b3e760c60e090d026289838dbc91296a8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/555049
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/devices/misc/drivers/test-parent/test-parent.cc
@@ -0,0 +1,90 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/ddk/device.h>
+#include <lib/ddk/driver.h>
+#include <lib/ddk/platform-defs.h>
+#include <zircon/assert.h>
+#include <zircon/types.h>
+
+#include <memory>
+
+#include <ddktl/device.h>
+
+#include "src/devices/misc/drivers/test-parent/test-parent-bind.h"
+
+namespace {
+
+class TestParent;
+using TestParentType = ddk::Device<TestParent>;
+
+class TestParent : public TestParentType {
+ public:
+  explicit TestParent(zx_device_t* device) : TestParentType(device) {}
+
+  static zx_status_t Create(zx_device_t* parent);
+
+  // Device protocol implementation.
+  void DdkRelease() {
+    // test-parent should never have its release called.
+    ZX_ASSERT_MSG(false, "TestParent::DdkRelease() invoked!\n");
+  }
+};
+
+zx_status_t TestParent::Create(zx_device_t* parent) {
+  auto test_parent = std::make_unique<TestParent>(parent);
+  zx_status_t status = test_parent->DdkAdd(ddk::DeviceAddArgs("test")
+                                               .set_proto_id(ZX_PROTOCOL_TEST_PARENT)
+                                               .set_flags(DEVICE_ADD_ALLOW_MULTI_COMPOSITE));
+  if (status != ZX_OK) {
+    return status;
+  }
+
+  // Now owned by the driver framework.
+  __UNUSED auto ptr = test_parent.release();
+
+  return ZX_OK;
+}
+
+class SysDevice;
+using SysDeviceType = ddk::Device<SysDevice>;
+
+class SysDevice : public SysDeviceType {
+ public:
+  explicit SysDevice(zx_device_t* device) : SysDeviceType(device) {}
+
+  static zx_status_t Create(void* ctx, zx_device_t* parent, const char* name, const char* args,
+                            zx_handle_t items_svc_handle);
+
+  // Device protocol implementation.
+  void DdkRelease() {
+    // sysdev should never have its release called.
+    ZX_ASSERT_MSG(false, "SysDevice::DdkRelease() invoked!\n");
+  }
+};
+
+zx_status_t SysDevice::Create(void* ctx, zx_device_t* parent, const char* name, const char* args,
+                              zx_handle_t items_svc_handle) {
+  auto sys_device = std::make_unique<SysDevice>(parent);
+  zx_status_t status =
+      sys_device->DdkAdd(ddk::DeviceAddArgs("sys").set_flags(DEVICE_ADD_NON_BINDABLE));
+  if (status != ZX_OK) {
+    return status;
+  }
+
+  // Now owned by the driver framework.
+  auto ptr = sys_device.release();
+  return TestParent::Create(ptr->zxdev());
+}
+
+static constexpr zx_driver_ops_t driver_ops = []() {
+  zx_driver_ops_t ops = {};
+  ops.version = DRIVER_OPS_VERSION;
+  ops.create = SysDevice::Create;
+  return ops;
+}();
+
+}  // namespace
+
+ZIRCON_DRIVER(test - parent, driver_ops, "zircon", "0.1");
