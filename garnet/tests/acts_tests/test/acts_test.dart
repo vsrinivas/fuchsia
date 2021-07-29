0a6a1037a352da59fe394876788b120b74c5d2f8
Yuping Zhai <yupingz@google.com>
2021-07-29 21:41:27 +0000

    [acts] Update acts to run on virtualenv
    Change-Id: I247042db2a1658cb7a1f3e3a887dfcdcf2fef726
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/510518
    Commit-Queue: Yuping Zhai <yupingz@google.com>
    Reviewed-by: Anirudh Mathukumilli <rudymathu@google.com>

--- garnet/tests/acts_tests/test/acts_test.dart
+++ garnet/tests/acts_tests/test/acts_test.dart
@@ -3,11 +3,10 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:archive/archive.dart';
 import 'package:args/args.dart';
+import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
 const String ipv4Env = 'FUCHSIA_IPV4_ADDR';
@@ -15,13 +14,14 @@ const String outputEnv = 'FUCHSIA_TEST_OUTDIR';
 
 const int deviceReadyTimeoutSec = 30;
 
-const String defaultActsZipAbsPath = '/etc/connectivity/acts.zip';
+// Configuration file used by ACTS. See format here:
+// https://g3doc.corp.google.com/third_party/py/acts/README.md#configuration-files.
 const String actsConfigAbsPath = '/etc/connectivity/acts_config.json';
-const String actsBinRelPath =
-    'tools/test/connectivity/acts/framework/acts/bin/act.py';
-const String actsFrameworkRelPath = 'tools/test/connectivity/acts/framework';
-const String actsOutputPath = '/tmp/logs/tb/latest';
-const String actsSummaryFileName = 'test_run_summary.json';
+const String actsRelativePath =
+    'host_x64/gen/garnet/tests/acts_tests/acts/acts.zip';
+const String pythonRelativePath =
+    '../../prebuilt/third_party/python3/linux-x64/bin/python3.8';
+const String virtualEnvName = 'my_virtualenv';
 
 Future<void> waitDeviceReady(final String ip) async {
   for (var i = 0; i < deviceReadyTimeoutSec; i++) {
@@ -35,49 +35,53 @@ Future<void> waitDeviceReady(final String ip) async {
   throw Exception('Device [$ip] is not ping-able.');
 }
 
-String resolveActsPath() {
-  final List entities = Directory.current.listSync(recursive: true);
-  for (var entity in entities) {
-    if (entity.path.endsWith('acts.zip')) {
-      return entity.path;
-    }
-  }
-  // Use pre-provisioned 'acts.zip' if it's not found under cur directory
-  return defaultActsZipAbsPath;
+void setupActs(final String tmpDirPath, final String tests) {
+  final actsPath = path.join(Directory.current.path, actsRelativePath);
+
+  // Install virtualenv
+  var installVirtualEnv = Process.runSync(
+      '$pythonRelativePath', ['-m', 'pip', 'install', 'virtualenv']);
+  stdout.write(installVirtualEnv.stdout);
+  stderr.write(installVirtualEnv.stderr);
+
+  // Create the virtual environment
+  var virtualEnv = Process.runSync('$pythonRelativePath',
+      ['-m', 'virtualenv', '$tmpDirPath/$virtualEnvName']);
+  stdout.write(virtualEnv.stdout);
+  stderr.write(virtualEnv.stderr);
+
+  // Unzip ACTS under the virtual environment.
+  Process.runSync(
+      'unzip', [actsPath, '-d', '$tmpDirPath/$virtualEnvName/acts']);
+
+  // Run virtualenv using python interpreter and install ACTS
+  var setupActsEnv = Process.runSync('$tmpDirPath/$virtualEnvName/bin/python3',
+      ['$tmpDirPath/$virtualEnvName/acts/setup.py', 'install']);
+  stdout.write(setupActsEnv.stdout);
+  stderr.write(setupActsEnv.stderr);
 }
 
-void extractActsZip(final String tmpDirPath) {
-  final actsPath = resolveActsPath();
-  print('Found "acts.zip" @ [$actsPath].');
+Future<Process> runActs(
+    final String tmpDirPath, final String tests, final String outputDir) {
+  final pythonPath = '$tmpDirPath/$virtualEnvName/bin/python3';
+  final actsBinAbsPath =
+      '$tmpDirPath/$virtualEnvName/acts/acts_framework/acts/bin/act.py';
 
-  final List<int> bytes = File(actsPath).readAsBytesSync();
-  final Archive archive = ZipDecoder().decodeBytes(bytes);
-
-  // Extract the contents of the Zip archive to disk.
-  for (ArchiveFile file in archive) {
-    String filename = file.name;
-    var filepath = '$tmpDirPath/$filename';
-    if (file.isFile) {
-      List<int> data = file.content;
-      File(filepath)
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(data);
-    } else {
-      Directory(filepath).create(recursive: true);
-    }
-  }
-}
-
-Future<Process> runActs(final String tmpDirPath, final String tests) {
-  final actsBinAbsPath = '$tmpDirPath/$actsBinRelPath';
-
-  final actsArgs = ['-c', actsConfigAbsPath, '-tc']..addAll(tests.split(' '));
+  final actsArgs = [
+    actsBinAbsPath,
+    '-c',
+    actsConfigAbsPath,
+    '-lp',
+    '$outputDir/acts_logpath',
+    '-tc'
+  ]..addAll(tests.split(' '));
 
   // Set ACTS binary as executable
   Process.runSync('chmod', ['+x', actsBinAbsPath]);
 
-  return Process.start(actsBinAbsPath, actsArgs, environment: {
-    'PYTHONPATH': '$tmpDirPath/$actsFrameworkRelPath',
+  return Process.start(pythonPath, actsArgs, environment: {
+    'PYTHONPATH': '$tmpDirPath/$virtualEnvName/bin',
+    'ACTS_LOGPATH': outputDir,
     // Required for some Wifi testcases
     'LANG': 'C.UTF-8',
     'LC_ALL': 'C.UTF-8'
@@ -97,15 +101,11 @@ void main(final List<String> args) {
   final String ipStr = envVars[ipv4Env];
   final String outputDir = envVars[outputEnv];
 
-  tearDown(() {
-    // Copy ACTS output to designated dir for GCS upload
-    Process.runSync('/bin/cp', ['-r', actsOutputPath, outputDir]);
-  });
-
   test('ActsSuite', () async {
-    // Extract ACTS zip into tmp dir
     final tmpDir = Directory.systemTemp.createTempSync('acts_');
-    extractActsZip(tmpDir.path);
+
+    // Extract ACTS and set up virtual environment.
+    setupActs(tmpDir.path, argResults['tests']);
 
     // Wait for device to be online before running tests
     for (var ip in ipStr.split(',')) {
@@ -113,16 +113,9 @@ void main(final List<String> args) {
     }
 
     // Run ACTS tests
-    final proc = await runActs(tmpDir.path, argResults['tests']);
+    final proc = await runActs(tmpDir.path, argResults['tests'], outputDir);
     await Future.wait(
         [stdout.addStream(proc.stdout), stderr.addStream(proc.stderr)]);
     expect(await proc.exitCode, equals(0));
-
-    // Assert that ACTS actually ran tests
-    final json =
-        File('$actsOutputPath/$actsSummaryFileName').readAsStringSync();
-    final data = jsonDecode(json);
-    expect(data, contains('Results'));
-    expect(data['Summary']['Executed'], isNonZero);
   }, timeout: Timeout(Duration(minutes: int.parse(argResults['timeout']))));
 }
