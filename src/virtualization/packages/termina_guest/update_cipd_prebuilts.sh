dcfc0d43526a113017bbebdfb52a5c3b5ce5cc66
David Greenaway <dgreenaway@google.com>
2021-08-03 06:51:25 +0000

    [virtualization][termina] Use prebuilt Termina images from ChromeOS team
    We currently build Termina guest images by downloading a prebuilt
    ChromeOS release from public servers, and then running scripts from
    a ChromeOS checkout to convert the ChromeOS image into a Termina guest
    image. ChromeOS, however, publicly publishes termina guest images
    directly, avoiding the need for us to perform the conversion step
    ourselves.
    
    This CL updates the "update_cipd_prebuilds.sh" script to directly pull
    the prebuilt Termina guest images, extract them, and upload to CIPD
    without needing any further modification.
    
    Change-Id: I49f284fc6ccc778003940ebb9396d0415ab66984
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/563782
    Reviewed-by: David Reveman <reveman@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: David Greenaway <dgreenaway@google.com>

--- src/virtualization/packages/termina_guest/update_cipd_prebuilts.sh
+++ src/virtualization/packages/termina_guest/update_cipd_prebuilts.sh
@@ -21,43 +21,16 @@ print_usage_and_exit() {
   echo "Update prebuilt termina images from published Chrome images."
   echo ""
   echo "This script assists in updating the CIPD prebuilt images of the termina"
-  echo "VM by downloading prebuilts from the chrome image archive and converting"
-  echo "them into a format that is compatible with Fuchsia."
-  echo ""
-  echo "Note: This script requires a valid ChromeOS source tree. Instructions"
-  echo "for setting that up can be found at:"
-  echo "    https://sites.google.com/a/chromium.org/dev/chromium-os/quick-start-guide"
-  echo "and it's recommended to run the following inside cros_sdk prior to"
-  echo "using this script:"
-  echo "    ./setup_board --board=tatl"
-  echo "    ./build_packages --board=tatl"
-  echo "    ./build_image --board=tatl test"
+  echo "VM by downloading prebuilts from the chrome image archive, extracting"
+  echo "them, then uploading them to CIPD."
   echo ""
   echo "Usage:"
-  echo "  update_cipd_prebuilts.sh -c [CROS_WORKSPACE_PATH] -r [TERMINA_REVISION]"
-  echo "      -k [KERNEL_OVERLAY_DIR] [-n] [-f]"
+  echo "  update_cipd_prebuilts.sh [-r <TERMINA_REVISION>] ( -n | -f )"
   echo ""
   echo "Where:"
-  echo "   -c [CROS_WORKSPACE_PATH] - path to a ChromeOS work tree. Instructions"
-  echo "      for setting that up can be found at:"
-  echo "      https://sites.google.com/a/chromium.org/dev/chromium-os/quick-start-guide"
-  echo ""
   echo "   -r [TERMINA_REVISION] - Version of termina to publish to CIPD. This will"
   echo "      be a string that looks something like 'R76-12182.0.0-rc1'. If omitted,"
   echo "      the most recent version will be used."
-  echo ""
-  echo "   -k [KERNEL_OVERLAY_DIR] - Directory to use for kernel overlays. This should"
-  echo "      be a directory with the following layout:"
-  echo ""
-  echo "      KERNEL_OVERLAY_DIR/"
-  echo "         |--x64"
-  echo "         |    |-vm_kernel"
-  echo "         |--arm64"
-  echo "         |    |-vm_kernel"
-  echo ""
-  echo "      These kernels will be used in lieu of the prebuilt kernels from the"
-  echo "      Chrome image archive. If the kernel does not exist in the overlay, "
-  echo "      then the Chrome prebuilt will still be used."
   echo ""
   echo "   -n - Dry run. Don't actually upload anything, just show what would be"
   echo "      uploaded."
@@ -65,16 +38,6 @@ print_usage_and_exit() {
   echo "   -f - Force. Script will refuse to upload unless this flag is specified."
 
   exit $1
-}
-
-check_depot_tools() {
-  type -P cros_sdk &>/dev/null && return 0
-  echo "ChromeOS depot_tools not found"
-  echo ""
-  echo "This script requires a ChromeOS workspace to run. For setup instructions,"
-  echo "see:"
-  echo "  https://sites.google.com/a/chromium.org/dev/chromium-os/quick-start-guide"
-  exit 1
 }
 
 board_for_arch() {
@@ -97,47 +60,17 @@ board_for_arch() {
 #      or
 #      https://storage.googleapis.com/chromeos-image-archive/tael-full/LATEST-master
 # $3 - Directory to decompress the prebuilt image into.
-#
-# The images distributed by Chrome are full disk images that contains several
-# partitions. These images are not directly bootable on Fuchsia and will require
-# some post-processing to be usable.
 fetch_and_decompress() {
   local -r arch="$1"
   local -r revision="$2"
   local -r outdir="$3"
 
   local -r board="`board_for_arch ${arch}`"
-  local -r artifact_dir="${outdir}/${board}"
 
-  mkdir -p "${artifact_dir}"
-  wget -O "${artifact_dir}/chromiumos_base_image.tar.xz" \
-    "https://storage.googleapis.com/chromeos-image-archive/${board}/${revision}/chromiumos_base_image.tar.xz"
-  tar xvf "${artifact_dir}/chromiumos_base_image.tar.xz" -C "${artifact_dir}"
-  rm "${artifact_dir}/chromiumos_base_image.tar.xz"
-}
-
-# Extracts a kernel image and rootfs filesystem image from the bootable disk
-# image provided by ChromeOS.
-#
-# $1 - Architecture (x64 or arm64).
-# $2 - Path to a ChromeOS workspace. This is needed as we'll reuse the scripts
-#      provided by Chrome to extract the kernel and rootfs from the full disk
-#      image.
-termina_build_image() {
-  local -r arch="$1"
-  local -r cros_dir="$2"
-
-  local -r board="`board_for_arch ${arch}`"
-  # Note that the references to $HOME here will be resoved inside the ChromeOS
-  # chroot and not the current users $HOME. This is because the ChromeOS build
-  # system relies on doing a chroot into a sysroot to support the build. This
-  # is handled by the 'cros_sdk' command below.
-  local -r chroot_outdir="~/${board}/output"
-  local -r input_image="~/${board}/chromiumos_base_image.bin"
-
-  pushd "${cros_dir}"
-  cros_sdk bash -c "rm -rf ${chroot_outdir} && ~/trunk/src/platform/container-guest-tools/termina/termina_build_image.py ${input_image} ${chroot_outdir}"
-  popd
+  curl -o "${outdir}/guest-vm-base.tbz" \
+    "https://storage.googleapis.com/chromeos-image-archive/${board}/${revision}/guest-vm-base.tbz"
+  tar xvf "${outdir}/guest-vm-base.tbz" --strip-components=1 -C "${outdir}"
+  rm "${outdir}/guest-vm-base.tbz"
 }
 
 # Returns the most recent revision for a given board.
@@ -149,11 +82,9 @@ latest_revision_for_board() {
 }
 
 main() {
-  while getopts "c:r:k:nfh" FLAG; do
+  while getopts "r:nfh" FLAG; do
     case "${FLAG}" in
-    c) cros_dir="${OPTARG}" ;;
     r) termina_revision_requested="${OPTARG}" ;;
-    k) kernel_overlay_dir="${OPTARG}" ;;
     n) dry_run=true ;;
     f) force=true ;;
     h) print_usage_and_exit 0 ;;
@@ -163,38 +94,29 @@ main() {
   shift $((OPTIND - 1))
 
   declare -r cipd="${FUCHSIA_DIR}/.jiri_root/bin/cipd"
-  declare -r cros_dir=${cros_dir}
   declare -r termina_revision_requested=${termina_revision_requested}
-  declare -r kernel_overlay_dir=${kernel_overlay_dir}
   declare -r dry_run=${dry_run}
   declare -r force=${force}
   declare jiri_entries="    <!-- termina guest images -->"
 
-  if [[ -z "${cros_dir}" ]];
-  then
-    print_usage_and_exit 1
-  fi
-
+  # Ensure one of "dry-run" or "force" is given.
   if [ "$dry_run" == "$force" ];
   then
     print_usage_and_exit 1
   fi
 
-  check_depot_tools
+  # Create a temporary directory to work in.
+  local output_dir=$(mktemp -d)
+  trap "rm -rf ${output_dir}" EXIT
 
   for arch in "x64" "arm64"
   do
     board=`board_for_arch ${arch}`
     termina_revision=${termina_revision_requested:-`latest_revision_for_board ${board}`}
-    kernel_overlay_path="${kernel_overlay_dir}/${arch}/vm_kernel"
-    if [[ ! -z "${kernel_overlay_dir}" ]] && [[ -f "${kernel_overlay_path}" ]]; then
-      echo "Using kernel from overlay ${kernel_overlay_path}"
-      kernel_override="${kernel_overlay_path}"
-    fi
 
     jiri_entries="${jiri_entries}
     <package name=\"fuchsia_internal/linux/termina-${arch}\"
-             version=\"termina-rev:${termina_revision}\"
+             version=\"termina-upstream:${termina_revision}\"
              path=\"prebuilt/virtualization/packages/termina_guest/images/${arch}\"
              internal=\"true\"/>"
 
@@ -202,30 +124,27 @@ main() {
     # CIPD instances with the same tag.
     ${cipd} \
       search fuchsia_internal/linux/termina-${arch} \
-      -tag "termina-rev:${termina_revision}" | grep "fuchsia_internal/linux/termina-${arch}"
+      -tag "termina-upstream:${termina_revision}" | grep "fuchsia_internal/linux/termina-${arch}"
     if [[ $? -eq 0 ]];
     then
       echo "CIPD artifact already exists for ${termina_revision} on arch ${arch}; skipping upload."
       continue
     fi
 
-    fetch_and_decompress "${arch}" "${termina_revision}" "${cros_dir}/chroot/home/${USER}"
-    termina_build_image "${arch}" "${cros_dir}"
+    # Download and decompress the image.
+    mkdir -p "${output_dir}/${arch}"
+    fetch_and_decompress "${arch}" "${termina_revision}" "${output_dir}/${arch}"
 
-    if [[ ! -z "${kernel_override}" ]]; then
-      cp "${kernel_override}" "${cros_dir}/chroot/home/${USER}/${board}/output/vm_kernel"
-    fi
-
+    # Upload to CIPD.
     options=()
     options+=("create")
-    options+=("-in" "${cros_dir}/chroot/home/${USER}/${board}/output")
+    options+=("-in" "${output_dir}/${arch}")
     options+=("-name" "fuchsia_internal/linux/termina-${arch}")
     options+=("-install-mode" "copy")
-    options+=("-tag" "termina-rev:${termina_revision}")
-
-    echo cipd ${options[*]}
+    options+=("-tag" "termina-upstream:${termina_revision}")
+    echo "cipd ${options[*]}"
     if [ "$dry_run" != true ] ; then
-      ${cipd} ${options[*]}
+      "${cipd}" ${options[*]}
     fi
   done
 
