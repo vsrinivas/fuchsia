2059b201771992d161eefaaf8503cd64eba346ef
Prashanth Swaminathan <prashanthsw@google.com>
2021-08-04 19:09:19 +0000

    [weave] Rebind lowpan device on device operation
    If the lowpan service restarts or otherwise closes the connection to
    weavestack, the connection must be re-established before any further
    provisioning or status operations. Before these operations, rebind to
    the lowpan service, giving us an opportunity to establish the connection
    or fail if the lowpan service is down.
    
    Bug: b/195447349
    Test: fx test weavestack-unittests
    Test: fx test weavestack-adaptation-unittests
    Change-Id: I6711025fca1abfd60790bf3cd52735c57c48ecce
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/564341
    Reviewed-by: Jason Graffius <jgraff@google.com>
    Commit-Queue: Prashanth Swaminathan <prashanthsw@google.com>

--- src/connectivity/weave/adaptation/thread_stack_manager_delegate_impl.h
+++ src/connectivity/weave/adaptation/thread_stack_manager_delegate_impl.h
@@ -51,11 +51,11 @@ class NL_DLL_EXPORT ThreadStackManagerDelegateImpl : public ThreadStackManagerIm
 
  private:
   std::string interface_name_;
-  fuchsia::lowpan::device::DeviceSyncPtr device_;
   bool is_thread_supported_ = false;
 
   zx_status_t GetProtocols(fuchsia::lowpan::device::Protocols protocols);
-  zx_status_t GetDeviceState(fuchsia::lowpan::device::DeviceState* state);
+  zx_status_t GetDevice(fuchsia::lowpan::device::DeviceSyncPtr& device);
+  zx_status_t GetDeviceState(fuchsia::lowpan::device::DeviceState& device_state);
 };
 
 }  // namespace DeviceLayer
