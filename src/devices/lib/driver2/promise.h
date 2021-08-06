aedab6ffba5eb190601cf653b1100544c390766c
Abdulla Kamar <abdulla@google.com>
2021-08-06 17:51:53 +0000

    [driver2] Remove Continue and ContinueWith
    Going forward, we'll lean on fpromise::bridge instead, as that's widely
    used and known to developers.
    
    Fixed: 62049
    Bug: 33183
    Change-Id: I078a4225ec8e67be412c784bc3ba2bf32b985c40
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/565724
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Fuchsia-Auto-Submit: Abdulla Kamar <abdulla@google.com>

--- src/devices/lib/driver2/promise.h
+++ src/devices/lib/driver2/promise.h
@@ -11,9 +11,6 @@
 
 namespace driver {
 
-template <typename T>
-class ContinueWith;
-
 namespace internal {
 
 // Connects to the given |path| in |ns|, and returns a fpromise::result containing a
@@ -30,44 +27,6 @@ fpromise::result<fidl::WireSharedClient<T>, zx_status_t> ConnectWithResult(
   return fpromise::ok(std::move(client));
 }
 
-// Helps to call a fpromise::promise lambda function.
-template <typename Func, size_t size>
-struct ContinueCall {
-  static_assert(size == 2, "Unexpected number of arguments");
-
-  using return_type = typename fit::callable_traits<Func>::return_type;
-  using value_type = typename fit::callable_traits<Func>::args::template at<1>;
-
-  static auto Call(Func func) {
-    return [func = std::move(func), done = false, with = ContinueWith<return_type>()](
-               fpromise::context& context, value_type value) mutable -> return_type {
-      if (done) {
-        return with.Result();
-      }
-      done = true;
-      with.Suspend(context);
-      return func(with, std::forward<value_type>(value));
-    };
-  }
-};
-
-template <typename Func>
-struct ContinueCall<Func, 1> {
-  using return_type = typename fit::callable_traits<Func>::return_type;
-
-  static auto Call(Func func) {
-    return [func = std::move(func), done = false,
-            with = ContinueWith<return_type>()](fpromise::context& context) mutable -> return_type {
-      if (done) {
-        return with.Result();
-      }
-      done = true;
-      with.Suspend(context);
-      return func(with);
-    };
-  }
-};
-
 }  // namespace internal
 
 // Connects to the given |path| in |ns|, and returns a fpromise::promise containing a
@@ -78,46 +37,6 @@ fpromise::promise<fidl::WireSharedClient<T>, zx_status_t> Connect(
     std::string_view path = fidl::DiscoverableProtocolDefaultPath<T>,
     uint32_t flags = ZX_FS_RIGHT_READABLE) {
   return fpromise::make_result_promise(internal::ConnectWithResult<T>(ns, dispatcher, path, flags));
-}
-
-// Wraps a fpromise::suspended_task in order to provide an ergonomic way to suspend
-// and resume when using a FIDL callback, without the need for a fpromise::bridge.
-//
-// TODO(fxbug.dev/62049): Consider moving this into libfit.
-template <typename T>
-class ContinueWith {
- public:
-  // Returns |result| when the promise is resumed.
-  void Return(T result) {
-    task_.resume_task();
-    result_.swap(result);
-  }
-
- private:
-  fpromise::suspended_task task_;
-  T result_;
-
-  void Suspend(fpromise::context& context) {
-    auto task = context.suspend_task();
-    task_.swap(task);
-  }
-
-  T Result() { return std::move(result_); }
-
-  template <typename Func, size_t size>
-  friend struct internal::ContinueCall;
-};
-
-// Allows a fpromise::promise compatible lambda function to be easily suspended and
-// resumed. This is achieved by replacing the first argument with a ContinueWith
-// object that can capture the result of a callback and resume execution of a
-// promise.
-//
-// TODO(fxbug.dev/62049): Consider moving this into libfit.
-template <typename Func>
-auto Continue(Func func) {
-  constexpr size_t size = fit::callable_traits<Func>::args::size;
-  return internal::ContinueCall<Func, size>::Call(std::move(func));
 }
 
 }  // namespace driver
