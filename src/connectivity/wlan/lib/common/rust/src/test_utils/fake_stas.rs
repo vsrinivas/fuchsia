a1d85c04d2e909ace72995dbebedcb8397c1e4eb
Charles Celerier <chcl@google.com>
2021-08-10 18:18:21 +0000

    [wlan] Add random_fidl_bss_description testing macro
    The random_fidl_bss_description enables a refactor of wlancfg testing
    code to reuse fidl_internal::BssDescription struct generation code
    already in wlan-common.
    
    This change also adds the `protection => ...` syntax to
    fake_bss_description as well and adds unit testing of the syntax for
    all macros.
    
    Test: fx test wlan-common-tests
    Change-Id: Ib97eb0aeed782894f3adadd378d7b773f91255c4

--- src/connectivity/wlan/lib/common/rust/src/test_utils/fake_stas.rs
+++ src/connectivity/wlan/lib/common/rust/src/test_utils/fake_stas.rs
@@ -13,9 +13,16 @@ use {
         },
     },
     anyhow::Context,
-    fidl_fuchsia_wlan_common as fidl_common, fidl_fuchsia_wlan_internal as fidl_internal,
-    fidl_fuchsia_wlan_sme as fidl_sme,
+    fidl_fuchsia_wlan_common as fidl_common, fidl_fuchsia_wlan_ieee80211 as fidl_ieee80211,
+    fidl_fuchsia_wlan_internal as fidl_internal, fidl_fuchsia_wlan_sme as fidl_sme,
     ieee80211::Ssid,
+    num_derive::FromPrimitive,
+    num_traits::FromPrimitive,
+    rand::{
+        distributions::{Distribution, Standard},
+        Rng,
+    },
+    std::convert::TryInto,
 };
 
 #[rustfmt::skip]
@@ -211,9 +218,11 @@ enum IeOverride {
     SetRaw(Vec<u8>),
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+const LAST_FAKE_PROTECTION_CFG_VALUE: isize = 14;
+
+#[derive(Debug, FromPrimitive, Copy, Clone, PartialEq)]
 pub enum FakeProtectionCfg {
-    Open,
+    Open = 0,
     Wep,
     Wpa1,
     Wpa1Enhanced,
@@ -227,7 +236,15 @@ pub enum FakeProtectionCfg {
     Wpa3Transition,
     Wpa3,
     Wpa3Enterprise,
-    Eap,
+    Eap = LAST_FAKE_PROTECTION_CFG_VALUE,
+}
+
+impl Distribution<FakeProtectionCfg> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> FakeProtectionCfg {
+        let r = rng.gen_range(0, LAST_FAKE_PROTECTION_CFG_VALUE + 1);
+        FromPrimitive::from_isize(r)
+            .unwrap_or_else(|| panic!("Out of range random value for FakeProtectionCfg: {:?}", r))
+    }
 }
 
 impl From<fidl_sme::Protection> for FakeProtectionCfg {
@@ -265,8 +282,9 @@ pub fn build_fake_bss_description_creator__(
         },
         rssi_dbm: 0,
         snr_db: 0,
+
         protection_cfg,
-        ssid: (*b"fake-ssid").into(),
+        ssid: Ssid::from("fake-ssid"),
         rates: vec![0x82, 0x84, 0x8b, 0x96, 0x0c, 0x12, 0x18, 0x24, 0x30, 0x48, 0x60, 0x6c],
         wmm_param: Some(fake_wmm_param()),
 
@@ -282,6 +300,113 @@ pub fn build_fake_bss_description_creator__(
         immediate_block_ack: false,
 
         ies_overrides: IesOverrides::new(),
+    }
+}
+
+fn random_ecw_min_max(rng: &mut rand::prelude::ThreadRng) -> ie::EcwMinMax {
+    let min = rng.gen_range(0x0, 0xf);
+    let max = rng.gen_range(min, 0xf);
+    ie::EcwMinMax((max << 4) | min)
+}
+
+pub fn build_random_bss_description_creator__(
+    protection_cfg: FakeProtectionCfg,
+) -> BssDescriptionCreator {
+    // Only the Infrastructure BSS type is supported.
+    let bss_type = fidl_internal::BssType::Infrastructure;
+
+    let mut rng = rand::thread_rng();
+
+    // Random rates
+    let mut rates: Vec<u8> = vec![];
+    for _ in 0..rng.gen_range(1, ie::SUPPORTED_RATES_MAX_LEN + ie::EXTENDED_SUPPORTED_RATES_MAX_LEN)
+    {
+        rates.push(rng.gen());
+    }
+    let rates = rates; // shadow to make rates immutable
+
+    // Random IE bytes
+    let mut giant_vendor_ies = vec![];
+    for _j in 0..8 {
+        giant_vendor_ies.extend_from_slice(&[221, 250]);
+        giant_vendor_ies.extend((0..250).map(|_| rng.gen::<u8>()))
+    }
+    let ies_overrides = IesOverrides::new().set_raw(giant_vendor_ies);
+
+    let qos: bool = rng.gen();
+    let apsd: bool = rng.gen();
+
+    BssDescriptionCreator {
+        bssid: (0..6).map(|_| rng.gen::<u8>()).collect::<Vec<u8>>().try_into().unwrap(),
+        bss_type,
+        beacon_period: rng.gen::<u16>(),
+        timestamp: rng.gen::<u64>(),
+        local_time: rng.gen::<u64>(),
+        // TODO(fxbug.dev/81978): Purely random valid channel values is not implemented.
+        channel: fidl_common::WlanChannel {
+            primary: rng.gen_range(1, 255),
+            cbw: fidl_common::ChannelBandwidth::Cbw20,
+            secondary80: 0,
+        },
+        rssi_dbm: rng.gen::<i8>(),
+        snr_db: rng.gen::<i8>(),
+
+        protection_cfg,
+        ssid: (0..fidl_ieee80211::MAX_SSID_BYTE_LEN)
+            .map(|_| rng.gen::<u8>())
+            .collect::<Vec<u8>>()
+            .into(),
+        rates,
+
+        // TODO(fxbug.dev/82572): random_fidl_bss_description!(Open, qos: true) will not necessarily
+        // generate a random wmm_param as one might expect.
+        wmm_param: if qos {
+            Some(ie::WmmParam {
+                wmm_info: ie::WmmInfo(0).with_ap_wmm_info(
+                    ie::ApWmmInfo(0)
+                        .with_parameter_set_count(rng.gen_range(0x00, 0xf))
+                        .with_uapsd(if apsd { rng.gen() } else { false }),
+                ),
+                _reserved: rng.gen(),
+                ac_be_params: ie::WmmAcParams {
+                    aci_aifsn: ie::WmmAciAifsn(0).with_aifsn(rng.gen_range(2, 0xf)).with_aci(0),
+                    ecw_min_max: random_ecw_min_max(&mut rng),
+                    txop_limit: rng.gen(),
+                },
+                ac_bk_params: ie::WmmAcParams {
+                    aci_aifsn: ie::WmmAciAifsn(0).with_aifsn(rng.gen_range(2, 0xf)).with_aci(1),
+                    ecw_min_max: random_ecw_min_max(&mut rng),
+                    txop_limit: rng.gen(),
+                },
+                ac_vi_params: ie::WmmAcParams {
+                    aci_aifsn: ie::WmmAciAifsn(0).with_aifsn(rng.gen_range(2, 0xf)).with_aci(2),
+                    ecw_min_max: random_ecw_min_max(&mut rng),
+                    txop_limit: rng.gen(),
+                },
+                ac_vo_params: ie::WmmAcParams {
+                    aci_aifsn: ie::WmmAciAifsn(0).with_aifsn(rng.gen_range(2, 0xf)).with_aci(3),
+                    ecw_min_max: random_ecw_min_max(&mut rng),
+                    txop_limit: rng.gen(),
+                },
+            })
+        } else {
+            None
+        },
+
+        cf_pollable: rng.gen(),
+        cf_poll_req: rng.gen(),
+        short_preamble: rng.gen(),
+        spectrum_mgmt: rng.gen(),
+        qos,
+        short_slot_time: rng.gen(),
+        apsd,
+        radio_measurement: rng.gen(),
+        delayed_block_ack: rng.gen(),
+        immediate_block_ack: rng.gen(),
+
+        // Generating completely random IEs would be chaotic at best, so we
+        // generate some random vendor ies instead.
+        ies_overrides,
     }
 }
 
@@ -313,30 +438,100 @@ fn derive_wpa1_vendor_ies(protection_cfg: FakeProtectionCfg) -> Option<Vec<u8>> 
 }
 
 #[macro_export]
-macro_rules! fake_fidl_bss_description {
-    ($protection_type:ident $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
-        let protection = $crate::test_utils::fake_stas::FakeProtectionCfg::$protection_type;
-        $crate::fake_fidl_bss_description!(protection => protection $(, $bss_key: $bss_value)*)
-    }};
-    (protection => $protection_type:expr $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+macro_rules! __fake_fidl_bss_description {
+    ($build_fake_bss_description_creator__:path, $fake_protection_cfg:expr $(, $bss_key:ident: $bss_value:expr)*) => {{
         let bss_description_creator = $crate::test_utils::fake_stas::BssDescriptionCreator {
             $(
                 $bss_key: $bss_value,
             )*
-            ..$crate::test_utils::fake_stas::build_fake_bss_description_creator__($protection_type.into())
+            ..$build_fake_bss_description_creator__($fake_protection_cfg.into())
         };
         bss_description_creator.create_bss_description().expect("expect creating BSS to succeed")
     }};
 }
 
 #[macro_export]
-macro_rules! fake_bss_description {
-    ($protection_type:ident $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
-        let fidl_bss = $crate::fake_fidl_bss_description!($protection_type $(, $bss_key: $bss_value)*);
+macro_rules! fake_fidl_bss_description {
+    ($protection_name:ident $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_fidl_bss_description!(
+            $crate::test_utils::fake_stas::build_fake_bss_description_creator__,
+            $crate::test_utils::fake_stas::FakeProtectionCfg::$protection_name
+                $(, $bss_key: $bss_value)*)
+    }};
+    (protection => $fake_protection_cfg:expr $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_fidl_bss_description!(
+            $crate::test_utils::fake_stas::build_fake_bss_description_creator__,
+            $fake_protection_cfg
+                $(, $bss_key: $bss_value)*)
+    }};
+}
+
+#[macro_export]
+macro_rules! random_fidl_bss_description {
+    ($($bss_key:ident: $bss_value:expr),* $(,)?) => {{
+        let mut rng = rand::thread_rng();
+        $crate::__fake_fidl_bss_description!(
+            $crate::test_utils::fake_stas::build_random_bss_description_creator__,
+            rng.gen::<$crate::test_utils::fake_stas::FakeProtectionCfg>()
+                $(, $bss_key: $bss_value)*)
+    }};
+    ($protection_name:ident $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_fidl_bss_description!(
+            $crate::test_utils::fake_stas::build_random_bss_description_creator__,
+            $crate::test_utils::fake_stas::FakeProtectionCfg::$protection_name
+                $(, $bss_key: $bss_value)*)
+    }};
+    (protection => $fake_protection_cfg:expr $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_fidl_bss_description!(
+            $crate::test_utils::fake_stas::build_random_bss_description_creator__,
+            $fake_protection_cfg
+                $(, $bss_key: $bss_value)*)
+    }};
+}
+
+#[macro_export]
+macro_rules! __fake_bss_description {
+    ($fidl_bss_description_macro:ident, $fake_protection_cfg:expr $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        let fidl_bss = $crate::$fidl_bss_description_macro!(protection => $fake_protection_cfg $(, $bss_key: $bss_value)*);
         let bss_description = $crate::bss::BssDescription::from_fidl(fidl_bss)
             .expect("expect BSS conversion to succeed");
         bss_description
     }}
+}
+
+#[macro_export]
+macro_rules! fake_bss_description {
+    ($protection_name:ident $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_bss_description!(
+            fake_fidl_bss_description,
+            $crate::test_utils::fake_stas::FakeProtectionCfg::$protection_name $(, $bss_key: $bss_value)*)
+    }};
+    (protection => $fake_protection_cfg:expr $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_bss_description!(
+            fake_fidl_bss_description,
+            $fake_protection_cfg $(, $bss_key: $bss_value)*)
+    }};
+}
+
+#[macro_export]
+macro_rules! random_bss_description {
+    ($($bss_key:ident: $bss_value:expr),* $(,)?) => {{
+        let mut rng = rand::thread_rng();
+        $crate::__fake_bss_description!(
+            random_fidl_bss_description,
+            rng.gen::<$crate::test_utils::fake_stas::FakeProtectionCfg>()
+                $(, $bss_key: $bss_value)*)
+    }};
+    ($protection_name:ident $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_bss_description!(
+            random_fidl_bss_description,
+            $crate::test_utils::fake_stas::FakeProtectionCfg::$protection_name $(, $bss_key: $bss_value)*)
+    }};
+    (protection => $fake_protection_cfg:expr $(, $bss_key:ident: $bss_value:expr)* $(,)?) => {{
+        $crate::__fake_bss_description!(
+            random_fidl_bss_description,
+            $fake_protection_cfg $(, $bss_key: $bss_value)*)
+    }};
 }
 
 #[cfg(test)]
@@ -354,11 +549,78 @@ mod tests {
     fn macro_sanity_test() {
         let fidl_bss_description = fake_fidl_bss_description!(Open);
         let bss_description = fake_bss_description!(Open);
+        let random_fidl_bss_description = random_fidl_bss_description!(Open);
+        let random_bss_description = random_bss_description!(Open);
         assert_eq!(
             BssDescription::from_fidl(fidl_bss_description)
                 .expect("Failed to convert fake_fidl_bss_description value"),
             bss_description
         );
+        assert_ne!(
+            BssDescription::from_fidl(random_fidl_bss_description.clone())
+                .expect("Failed to convert random_fidl_bss_description value"),
+            bss_description
+        );
+        assert_ne!(
+            BssDescription::from_fidl(random_fidl_bss_description)
+                .expect("Failed to convert random_fidl_bss_description value"),
+            random_bss_description
+        );
+    }
+
+    #[test]
+    fn fake_protection_cfg_from_primitive() {
+        assert!(
+            0 <= LAST_FAKE_PROTECTION_CFG_VALUE,
+            "LAST_FAKE_PROTECTION_CFG_VALUE is not positive: {}",
+            LAST_FAKE_PROTECTION_CFG_VALUE,
+        );
+
+        let too_low: Option<FakeProtectionCfg> = FromPrimitive::from_isize(-1);
+        assert_eq!(
+            too_low, None::<FakeProtectionCfg>,
+            "Successfully converted low out of range FakeProtectionCfg value"
+        );
+
+        for i in 0..(LAST_FAKE_PROTECTION_CFG_VALUE + 1) {
+            let _: FakeProtectionCfg = FromPrimitive::from_isize(i).unwrap_or_else(|| {
+                panic!("Failed to convert {:?} to a FakeProtectionCfg value", i)
+            });
+        }
+
+        let too_high: Option<FakeProtectionCfg> =
+            FromPrimitive::from_isize(LAST_FAKE_PROTECTION_CFG_VALUE + 1);
+        assert_eq!(
+            too_high, None::<FakeProtectionCfg>,
+            "Successfully converted high out of range FakeProtectionCfg value"
+        );
+    }
+
+    #[test]
+    fn fake_protection_cfg_expr_syntax() {
+        let fidl_bss_description =
+            fake_fidl_bss_description!(protection => FakeProtectionCfg::Open);
+        assert_eq!(
+            BssDescription::from_fidl(fidl_bss_description)
+                .expect("Failed to convert fake_fidl_bss_description value")
+                .protection(),
+            Protection::Open
+        );
+
+        let fidl_bss_description =
+            random_fidl_bss_description!(protection => FakeProtectionCfg::Open);
+        assert_eq!(
+            BssDescription::from_fidl(fidl_bss_description)
+                .expect("Failed to convert random_fidl_bss_description value")
+                .protection(),
+            Protection::Open
+        );
+
+        let bss_description = fake_bss_description!(protection => FakeProtectionCfg::Open);
+        assert_eq!(bss_description.protection(), Protection::Open);
+
+        let bss_description = random_bss_description!(protection => FakeProtectionCfg::Open);
+        assert_eq!(bss_description.protection(), Protection::Open);
     }
 
     #[test]
@@ -376,6 +638,25 @@ mod tests {
         assert_eq!(bss.protection(), Protection::Wpa1);
 
         let bss = fake_bss_description!(Wpa2);
+        assert!(mac::CapabilityInfo(bss.capability_info).privacy());
+        assert_eq!(bss.protection(), Protection::Wpa2Personal);
+    }
+
+    #[test]
+    fn fake_protection_cfg_privacy_bit_and_protection_in_random_bss() {
+        let bss = random_bss_description!(Open);
+        assert!(!mac::CapabilityInfo(bss.capability_info).privacy());
+        assert_eq!(bss.protection(), Protection::Open);
+
+        let bss = random_bss_description!(Wep);
+        assert!(mac::CapabilityInfo(bss.capability_info).privacy());
+        assert_eq!(bss.protection(), Protection::Wep);
+
+        let bss = random_bss_description!(Wpa1);
+        assert!(mac::CapabilityInfo(bss.capability_info).privacy());
+        assert_eq!(bss.protection(), Protection::Wpa1);
+
+        let bss = random_bss_description!(Wpa2);
         assert!(mac::CapabilityInfo(bss.capability_info).privacy());
         assert_eq!(bss.protection(), Protection::Wpa2Personal);
     }
@@ -421,6 +702,100 @@ mod tests {
     #[should_panic(expected = "Personal is not supported")]
     fn unsupported_bss_type() {
         fake_fidl_bss_description!(Open, bss_type: fidl_internal::BssType::Personal);
+    }
+
+    #[test]
+    fn any_protection_syntax() {
+        let _ = random_fidl_bss_description!();
+        let _ = random_bss_description!();
+    }
+
+    #[test]
+    fn random_fidl_bss_decription_override() {
+        let random_bss = random_bss_description!(ssid: Ssid::from("foo"));
+        assert_eq!(random_bss.ssid, Ssid::from("foo"));
+    }
+
+    #[test]
+    fn valid_random_ecw_min_max() {
+        let mut rng = rand::thread_rng();
+        for _ in 0..100 {
+            let ecw_min_max = random_ecw_min_max(&mut rng);
+            assert!(ecw_min_max.ecw_max() >= ecw_min_max.ecw_min());
+        }
+    }
+
+    #[test]
+    fn random_bss_is_not_constant() {
+        for _ in 0..5 {
+            let random_bss_1 = BssDescription::from_fidl(random_fidl_bss_description!())
+                .expect("Failed to convert random_bss_description value");
+            let random_bss_2 = BssDescription::from_fidl(random_fidl_bss_description!())
+                .expect("Failed to convert random_bss_description value");
+            if random_bss_1 != random_bss_2 {
+                return;
+            }
+        }
+        panic!("random bss is always the same");
+    }
+
+    #[test]
+    fn random_bss_protection_is_not_constant() {
+        for _ in 0..5 {
+            let random_bss_1 = BssDescription::from_fidl(random_fidl_bss_description!())
+                .expect("Failed to convert random_bss_description value");
+            let random_bss_2 = BssDescription::from_fidl(random_fidl_bss_description!())
+                .expect("Failed to convert random_bss_description value");
+            if random_bss_1.protection() != random_bss_2.protection() {
+                return;
+            }
+        }
+        panic!("random bss protection is always the same");
+    }
+
+    #[test]
+    fn some_random_bss_bits_are_fixed() {
+        for _ in 0..5 {
+            let random_bss = random_fidl_bss_description!(Open);
+            assert_eq!(random_bss.bss_type, fidl_internal::BssType::Infrastructure);
+            assert!(mac::CapabilityInfo(random_bss.capability_info).ess());
+            assert!(!mac::CapabilityInfo(random_bss.capability_info).ibss());
+            assert!(!mac::CapabilityInfo(random_bss.capability_info).privacy());
+        }
+    }
+
+    #[test]
+    fn random_bss_wmm_param_consistent_with_qos_and_apsd() {
+        for _ in 0..5 {
+            let random_bss = BssDescription::from_fidl(random_fidl_bss_description!())
+                .expect("Failed to convert random_bss_description value");
+            let capability_info = mac::CapabilityInfo(random_bss.capability_info);
+            if capability_info.qos() {
+                let wmm_param = random_bss.wmm_param().expect("Unable to find WMM Param");
+                if !capability_info.apsd() {
+                    assert!(!wmm_param.wmm_info.ap_wmm_info().uapsd());
+                }
+            } else {
+                assert!(random_bss.find_wmm_param().is_none());
+            }
+        }
+    }
+
+    #[test]
+    fn random_bss_decription_randomness() {
+        // Test random_bss_description generation of random protection since
+        // it doesn't rely on random_fidl_bss_description for it.
+        let random_bss_1 = random_bss_description!();
+        let random_bss_2 = random_bss_description!();
+        let random_bss_3 = random_bss_description!();
+        let random_bss_4 = random_bss_description!();
+        let random_bss_5 = random_bss_description!();
+        assert!(
+            random_bss_1.protection() != random_bss_2.protection()
+                || random_bss_1.protection() != random_bss_3.protection()
+                || random_bss_1.protection() != random_bss_4.protection()
+                || random_bss_1.protection() != random_bss_5.protection()
+        );
     }
 
     #[test]
