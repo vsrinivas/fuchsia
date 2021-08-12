94811407772e225c985f40f2356f8d1245a3ba12
Marie Janssen <jamuraa@google.com>
2021-08-12 17:10:36 +0000

    [bt][gap] Add cooldown after local disconnects
    Add a 30 second cooldown for incoming connections from peers that we
    have just received an API disconnect from.  This rejects all incoming
    connections from the peer until the timeout expires, but doesn't
    prevent locally initiated connections to the peers.
    
    Bug: 82269
    Test: Added - fx test bt-host-gap-tests bt-host-common-tests
    Change-Id: I86eb95251699cb37f3272f2d106198f578df264e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/566761
    Commit-Queue: Marie Janssen 💖 <jamuraa@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>

---
+++ src/connectivity/bluetooth/core/bt-host/common/expiring_set_unittest.cc
@@ -0,0 +1,61 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "expiring_set.h"
+
+#include <gtest/gtest.h>
+
+#include "lib/gtest/test_loop_fixture.h"
+#include "src/connectivity/bluetooth/core/bt-host/common/test_helpers.h"
+
+namespace bt {
+
+namespace {
+
+zx::time now() { return async::Now(async_get_default_dispatcher()); }
+
+using ExpiringSetTest = ::gtest::TestLoopFixture;
+
+TEST_F(ExpiringSetTest, Expiration) {
+  ExpiringSet<std::string> set;
+
+  set.add_until("Expired", now() - zx::msec(1));
+  EXPECT_FALSE(set.contains("Expired"));
+
+  set.add_until("Just a minute", now() + zx::min(1));
+  set.add_until("Two minutes", now() + zx::min(2));
+  EXPECT_TRUE(set.contains("Just a minute"));
+  EXPECT_TRUE(set.contains("Two minutes"));
+
+  RunLoopFor(zx::sec(1));
+  EXPECT_TRUE(set.contains("Just a minute"));
+  EXPECT_TRUE(set.contains("Two minutes"));
+
+  RunLoopFor(zx::min(1));
+  EXPECT_FALSE(set.contains("Just a minute"));
+  EXPECT_TRUE(set.contains("Two minutes"));
+
+  RunLoopFor(zx::min(1));
+  EXPECT_FALSE(set.contains("Just a minute"));
+  EXPECT_FALSE(set.contains("Two minutes"));
+}
+
+TEST_F(ExpiringSetTest, Remove) {
+  ExpiringSet<std::string> set;
+
+  set.add_until("Expired", now() - zx::msec(1));
+  EXPECT_FALSE(set.contains("Expired"));
+  set.remove("Expired");
+
+  set.add_until("Temporary", now() + zx::sec(1000));
+  EXPECT_TRUE(set.contains("Temporary"));
+
+  set.remove("Temporary");
+
+  EXPECT_FALSE(set.contains("Temporary"));
+}
+
+}  // namespace
+
+}  // namespace bt
