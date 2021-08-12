b0c4ec4a0c201bc3c687e184aa45366abcecafc4
Ani Ramakrishnan <aniramakri@google.com>
2021-08-12 18:50:04 +0000

    [bt][hfp] Re-enable integration tests
    Update HFP-AG test setup to include a mock dev/ directory with DAI
    input and output devices. Also include the AudioDeviceEnumerator
    capability with a stub implementation.
    
    Specify the RFCOMM component as an intermediary for the HFP profile and
    the test driven peer.
    
    Bug: 79954
    Test: fx test bt-hfp-audio-gateway-integration-tests
    Multiply: bt-hfp-audio-gateway-integration-tests
    
    Change-Id: I0606bf5e9e44cf2bbafdd7767db2c56817678bfe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/555944
    Commit-Queue: Ani Ramakrishnan <aniramakri@google.com>
    Reviewed-by: Jeff Belgum <belgum@google.com>

--- src/connectivity/bluetooth/profiles/tests/bt-hfp-audio-gateway-integration-tests/src/main.rs
+++ src/connectivity/bluetooth/profiles/tests/bt-hfp-audio-gateway-integration-tests/src/main.rs
@@ -6,15 +6,23 @@ use {
     anyhow::format_err,
     at_commands::{self as at, SerDe},
     bitflags::bitflags,
+    bt_manifest_integration_lib::{mock_component, mock_dev},
     fidl::{encoding::Decodable, endpoints::DiscoverableProtocolMarker},
     fidl_fuchsia_bluetooth_bredr as bredr,
     fidl_fuchsia_bluetooth_hfp::HfpMarker,
-    fuchsia_async::{DurationExt, TimeoutExt},
+    fidl_fuchsia_io2 as fio2,
+    fidl_fuchsia_media::{AudioDeviceEnumeratorMarker, AudioDeviceEnumeratorRequest},
+    fixture::fixture,
+    fuchsia_async::{self as fasync, DurationExt, TimeoutExt},
+    fuchsia_audio_dai::test::mock_dai_dev_with_io_devices,
     fuchsia_bluetooth::types::{Channel, PeerId, Uuid},
-    fuchsia_component_test::{builder::Capability, RealmInstance},
+    fuchsia_component_test::{
+        builder::{Capability, CapabilityRoute, ComponentSource, RealmBuilder, RouteEndpoint},
+        mock::{Mock, MockHandles},
+        RealmInstance,
+    },
     fuchsia_zircon::Duration,
-    futures::{stream::StreamExt, TryFutureExt},
-    matches::assert_matches,
+    futures::{channel::mpsc, stream::StreamExt, TryFutureExt},
     mock_piconet_client::v2::{BtProfileComponent, PiconetHarness, PiconetMember},
     std::convert::TryInto,
     test_call_manager::TestCallManager,
@@ -23,10 +31,14 @@ use {
 /// HFP-AG component V2 URL.
 const HFP_AG_URL_V2: &str =
     "fuchsia-pkg://fuchsia.com/bt-hfp-audio-gateway-integration-tests#meta/bt-hfp-audio-gateway.cm";
+/// RFCOMM component v2 URL.
+const RFCOMM_URL_V2: &str =
+    "fuchsia-pkg://fuchsia.com/bt-hfp-audio-gateway-integration-tests#meta/bt-rfcomm.cm";
 
-/// The moniker for the HFP-AG component under test.
+/// The moniker for the Hands Free Profile Audio Gateway component under test.
 const HFP_AG_MONIKER: &str = "bt-hfp-ag-profile";
-
+/// The moniker for the mock dev/ directory provider.
+const MOCK_DEV_MONIKER: &str = "mock-dev";
 /// The moniker for a mock piconet member.
 const MOCK_PICONET_MEMBER_MONIKER: &str = "mock-peer";
 
@@ -90,40 +102,170 @@ fn hfp_hf_service_definition() -> bredr::ServiceDefinition {
     }
 }
 
+struct HfpAgIntegrationTest {
+    /// The collection of components used for the integration test. Must be kept alive.
+    test_realm: RealmInstance,
+    /// The observer for the HFP component - used to assert on behavior.
+    hfp_under_test: BtProfileComponent,
+    test_driven_peer: PiconetMember,
+    test_call_manager: TestCallManager,
+}
+
+impl HfpAgIntegrationTest {
+    /// Builds the test topology used by the integration test.
+    async fn setup_piconet_with_hfp_and_mock_peer() -> Self {
+        let mut test_harness = PiconetHarness::new().await;
+
+        // Mock piconet member with an RFCOMM intermediary.
+        let spec = test_harness
+            .add_mock_piconet_member(
+                MOCK_PICONET_MEMBER_MONIKER.to_string(),
+                Some(RFCOMM_URL_V2.to_string()),
+            )
+            .await
+            .expect("failed to add mock piconet member");
+
+        // The HFP profile under test.
+        // - Add a fake `dev/` directory with DAI devices to be used by HFP.
+        // - Add a provider for the `AudioDeviceEnumerator` capability used by HFP.
+        // - Add the HFP component with a hermetic RFCOMM intermediary component. Expose the
+        //   `fuchsia.bluetooth.hfp.Hfp` capability for the `test_call_manager`.
+        add_mock_dai_devices(&mut test_harness.builder).await;
+        add_mock_audio_device_enumerator_provider(&mut test_harness.builder).await;
+        let hfp_under_test = test_harness
+            .add_profile_with_capabilities(
+                HFP_AG_MONIKER.to_string(),
+                HFP_AG_URL_V2.to_string(),
+                Some(RFCOMM_URL_V2.to_string()),
+                vec![],
+                vec![Capability::protocol(HfpMarker::PROTOCOL_NAME)],
+            )
+            .await
+            .expect("failed to add HFP profile");
+
+        let test_realm = test_harness.build().await.unwrap();
+
+        // To be driven by the test.
+        let test_driven_peer = PiconetMember::new_from_spec(spec, &test_realm)
+            .expect("failed to get piconet member from spec");
+        // Set up the test call manager to interact with the `Hfp` protocol.
+        let test_call_manager = setup_test_call_manager(&test_realm, &hfp_under_test).await;
+
+        Self { test_realm, hfp_under_test, test_driven_peer, test_call_manager }
+    }
+}
+
+/// Adds a mock component to the `builder` that provides the `AudioDeviceEnumerator` capability.
+/// Note: This implementation consumes but does not handle the FIDL requests. This is sufficient
+/// for the current integration tests. We should consider injecting a v2 component that implements
+/// this capability once it has been migrated to v2.
+async fn add_mock_audio_device_enumerator_provider(builder: &mut RealmBuilder) {
+    const MOCK_AUDIO_DEVICE_MONIKER: &str = "mock-audio-device-provider";
+    let (sender, mut receiver) = mpsc::channel(0);
+    fasync::Task::spawn(async move {
+        while let Some(req) = receiver.next().await {
+            log::info!("Received AudioDeviceEnumerator request: {:?}", req);
+        }
+    })
+    .detach();
+
+    let _ = builder
+        .add_eager_component(
+            MOCK_AUDIO_DEVICE_MONIKER,
+            ComponentSource::Mock(Mock::new({
+                let sender = sender.clone();
+                move |mock_handles: MockHandles| {
+                    Box::pin(mock_component::<
+                        AudioDeviceEnumeratorMarker,
+                        AudioDeviceEnumeratorRequest,
+                    >(sender.clone(), mock_handles))
+                }
+            })),
+        )
+        .await
+        .expect("Failed adding AudioDevice mock to topology");
+
+    let _ = builder
+        .add_protocol_route::<AudioDeviceEnumeratorMarker>(
+            RouteEndpoint::component(MOCK_AUDIO_DEVICE_MONIKER),
+            vec![RouteEndpoint::component(HFP_AG_MONIKER)],
+        )
+        .expect("Failed adding route for AudioDeviceEnumerator capability");
+}
+
+/// Adds a mock dev/ directory provider for the DAI devices used by HFP.
+/// Routes the directory capability from the provider to the HFP component.
+async fn add_mock_dai_devices(builder: &mut RealmBuilder) {
+    let _ = builder
+        .add_eager_component(
+            MOCK_DEV_MONIKER,
+            ComponentSource::Mock(Mock::new({
+                move |mock_handles: MockHandles| {
+                    Box::pin(mock_dev(
+                        mock_handles,
+                        mock_dai_dev_with_io_devices("input1".to_string(), "output1".to_string()),
+                    ))
+                }
+            })),
+        )
+        .await
+        .expect("Failed adding mock /dev provider to topology");
+
+    let _ = builder
+        .add_route(CapabilityRoute {
+            capability: Capability::directory("dev-dai", "/dev/class/dai", fio2::RW_STAR_DIR),
+            source: RouteEndpoint::component(MOCK_DEV_MONIKER),
+            targets: vec![RouteEndpoint::component(HFP_AG_MONIKER)],
+        })
+        .expect("Failed adding route for DAI device directory");
+}
+
+/// Returns a TestCallManager that uses the `Hfp` protocol provided by the `hfp_component` in the
+/// test `topology`.
+async fn setup_test_call_manager(
+    topology: &RealmInstance,
+    hfp_component: &BtProfileComponent,
+) -> TestCallManager {
+    let hfp_protocol = hfp_component
+        .connect_to_protocol::<HfpMarker>(topology)
+        .expect("`Hfp` protocol should be available");
+    let facade = TestCallManager::new();
+    facade.register_manager(hfp_protocol).await.unwrap();
+    facade
+}
+
+async fn setup_piconet<F, Fut>(_name: &str, test: F)
+where
+    F: FnOnce(HfpAgIntegrationTest) -> Fut,
+    Fut: futures::Future<Output = ()>,
+{
+    let test_fixture = HfpAgIntegrationTest::setup_piconet_with_hfp_and_mock_peer().await;
+    test(test_fixture).await
+}
+
 /// Tests that HFP correctly advertises it's services and can be
 /// discovered by another peer in the mock piconet.
+#[fixture(setup_piconet)]
 #[fuchsia::test]
-async fn test_hfp_ag_service_advertisement() {
-    let mut test_harness = PiconetHarness::new().await;
+async fn test_hfp_ag_service_advertisement(tf: HfpAgIntegrationTest) {
+    let HfpAgIntegrationTest {
+        test_realm: _tr,
+        hfp_under_test,
+        test_driven_peer,
+        test_call_manager: _tcm,
+    } = tf;
 
-    // Add a mock piconet member.
-    let spec = test_harness
-        .add_mock_piconet_member(MOCK_PICONET_MEMBER_MONIKER.to_string())
-        .await
-        .expect("failed to add mock piconet member");
-    // Add the HFP profile under test.
-    let hfp_under_test = test_harness
-        .add_profile(HFP_AG_MONIKER.to_string(), HFP_AG_URL_V2.to_string())
-        .await
-        .expect("failed to add HFP profile");
-
-    let test_topology = test_harness.build().await.unwrap();
-
-    // Get the piconet member to be driven by this test and register a search for AGs in
-    // the piconet.
-    let remote_peer = PiconetMember::new_from_spec(spec, &test_topology)
-        .expect("failed to create piconet member from spec");
-    let mut results_requests = remote_peer
+    let mut results_requests = test_driven_peer
         .register_service_search(
             bredr::ServiceClassProfileIdentifier::HandsfreeAudioGateway,
             vec![],
         )
-        .unwrap();
+        .expect("can register service search");
 
     // Expect the test-driven piconet member to discover the HFP component.
     let service_found_fut = results_requests.select_next_some().map_err(|e| format_err!("{:?}", e));
     let bredr::SearchResultsRequest::ServiceFound { peer_id, responder, .. } =
-        service_found_fut.await.expect("Error in search request");
+        service_found_fut.await.expect("should receive search results");
     assert_eq!(hfp_under_test.peer_id(), peer_id.into());
     responder.send().unwrap();
 }
@@ -135,7 +277,7 @@ async fn expect_connection(
     connect_requests: &mut bredr::ConnectionReceiverRequestStream,
     other: PeerId,
 ) -> Channel {
-    match connect_requests.select_next_some().await.unwrap() {
+    match connect_requests.select_next_some().await.expect("should receive connection request") {
         bredr::ConnectionReceiverRequest::Connected { peer_id, channel, .. } => {
             assert_eq!(other, peer_id.into());
             channel.try_into().unwrap()
@@ -143,59 +285,29 @@ async fn expect_connection(
     }
 }
 
-/// Returns a TestCallManager that uses the `Hfp` protocol provided by the `bt-hfp-audio-gateway`
-/// component in the test `topology`.
-async fn setup_test_call_manager(
-    topology: &RealmInstance,
-    hfp_component: &BtProfileComponent,
-) -> TestCallManager {
-    let hfp_protocol = hfp_component
-        .connect_to_protocol::<HfpMarker>(topology)
-        .expect("HFP protocol should be available");
-    let facade = TestCallManager::new();
-    facade.register_manager(hfp_protocol).await.unwrap();
-    facade
-}
-
-/// Tests that HFP correctly searches for Handsfree, discovers a mock peer
-/// providing it, and attempts to connect to the mock peer.
+/// Tests that HFP correctly searches for Handsfree, discovers a peer providing it, and attempts to
+/// connect to the peer.
+#[fixture(setup_piconet)]
 #[fuchsia::test]
-async fn test_hfp_search_and_connect() {
-    let mut test_harness = PiconetHarness::new().await;
-
-    // Add a mock piconet member.
-    let spec = test_harness
-        .add_mock_piconet_member(MOCK_PICONET_MEMBER_MONIKER.to_string())
-        .await
-        .expect("failed to add mock piconet member");
-    // Add the HFP profile under test. Specify the `f.b.hfp.Hfp` protocol as an additional
-    // capability so that the TestCallManager can access it from the test realm service directory.
-    let additional_capabilities = vec![Capability::protocol(HfpMarker::PROTOCOL_NAME)];
-    let mut hfp_under_test = test_harness
-        .add_profile_with_capabilities(
-            HFP_AG_MONIKER.to_string(),
-            HFP_AG_URL_V2.to_string(),
-            vec![],
-            additional_capabilities,
-        )
-        .await
-        .expect("failed to add HFP profile");
-
-    let test_topology = test_harness.build().await.unwrap();
-    // Once the test realm has been built, we can grab the piconet member to be driven
-    // by this test.
-    let remote_peer = PiconetMember::new_from_spec(spec, &test_topology)
-        .expect("failed to create piconet member from spec");
+async fn test_hfp_search_and_connect(tf: HfpAgIntegrationTest) {
+    let HfpAgIntegrationTest {
+        test_realm: _tr,
+        mut hfp_under_test,
+        test_driven_peer,
+        test_call_manager: _tcm,
+    } = tf;
 
     // Remote peer advertises an HFP HF service.
     let service_defs = vec![hfp_hf_service_definition()];
-    let mut connect_requests = remote_peer.register_service_advertisement(service_defs).unwrap();
-
-    // Set up the test call manager to interact with the HFP protocol.
-    let _facade = setup_test_call_manager(&test_topology, &hfp_under_test).await;
+    let mut connect_requests = test_driven_peer
+        .register_service_advertisement(service_defs)
+        .expect("can register service search");
 
     // We expect HFP to discover Peer #1's service advertisement.
-    hfp_under_test.expect_observer_service_found_request(remote_peer.peer_id()).await.unwrap();
+    hfp_under_test
+        .expect_observer_service_found_request(test_driven_peer.peer_id())
+        .await
+        .expect("should observe search results");
 
     // We then expect HFP to attempt to connect to Peer #1.
     let _channel = expect_connection(&mut connect_requests, hfp_under_test.peer_id()).await;
@@ -216,13 +328,18 @@ async fn expect_data(channel: &mut Channel, expected: Vec<at::Response>) {
         .flatten()
         .collect();
 
-    let mut actual_bytes = Vec::new();
-    let read_result = channel
-        .read_datagram(&mut actual_bytes)
-        .on_timeout(CHANNEL_TIMEOUT.after_now(), move || Err(fidl::Status::TIMED_OUT))
-        .await;
-    assert_matches!(read_result, Ok(_));
-    assert_eq!(actual_bytes, expected_bytes);
+    loop {
+        let mut actual_bytes = Vec::new();
+        let read_result = channel
+            .read_datagram(&mut actual_bytes)
+            .on_timeout(CHANNEL_TIMEOUT.after_now(), move || Err(fidl::Status::TIMED_OUT))
+            .await
+            .expect("reading from channel is ok");
+        if read_result != 0 {
+            assert_eq!(actual_bytes, expected_bytes);
+            break;
+        }
+    }
 }
 
 /// Serializes and sends the provided AT `command` using the `channel` and then
@@ -259,45 +376,30 @@ const CIND_TEST_RESPONSE_BYTES: &[u8] = b"+CIND: \
 /// Note: This integration test validates that the expected responses are received, but
 /// does not validate individual field values (e.g the exact features or exact indicators) as
 /// this is implementation specific.
+#[fixture(setup_piconet)]
 #[fuchsia::test]
-async fn test_hfp_full_slc_init_procedure() {
-    let mut test_harness = PiconetHarness::new().await;
-
-    // Add a mock piconet member.
-    let spec = test_harness
-        .add_mock_piconet_member(MOCK_PICONET_MEMBER_MONIKER.to_string())
-        .await
-        .expect("failed to add mock piconet member");
-    // Add the HFP profile under test. Specify the `f.b.hfp.Hfp` protocol as an additional
-    // capability so that the TestCallManager can access it from the test realm service directory.
-    let additional_capabilities = vec![Capability::protocol(HfpMarker::PROTOCOL_NAME)];
-    let mut hfp_under_test = test_harness
-        .add_profile_with_capabilities(
-            HFP_AG_MONIKER.to_string(),
-            HFP_AG_URL_V2.to_string(),
-            vec![],
-            additional_capabilities,
-        )
-        .await
-        .expect("failed to add HFP profile");
-    let hfp_peer_id = hfp_under_test.peer_id();
-
-    let test_topology = test_harness.build().await.unwrap();
-    let remote_peer = PiconetMember::new_from_spec(spec, &test_topology)
-        .expect("failed to create piconet member from spec");
+async fn test_hfp_full_slc_init_procedure(tf: HfpAgIntegrationTest) {
+    let HfpAgIntegrationTest {
+        test_realm: _tr,
+        mut hfp_under_test,
+        test_driven_peer,
+        test_call_manager: _tcm,
+    } = tf;
 
     // Peer #1 advertises an HFP HF service.
     let service_defs = vec![hfp_hf_service_definition()];
-    let mut connect_requests = remote_peer.register_service_advertisement(service_defs).unwrap();
-
-    // Set up the test call manager to interact with the HFP protocol.
-    let _facade = setup_test_call_manager(&test_topology, &hfp_under_test).await;
+    let mut connect_requests = test_driven_peer
+        .register_service_advertisement(service_defs)
+        .expect("should register advertisement");
 
     // We expect HFP to discover Peer #1's service advertisement.
-    hfp_under_test.expect_observer_service_found_request(remote_peer.peer_id()).await.unwrap();
+    hfp_under_test
+        .expect_observer_service_found_request(test_driven_peer.peer_id())
+        .await
+        .expect("should observe search results");
 
     // We then expect HFP to attempt to connect to Peer #1.
-    let mut remote = expect_connection(&mut connect_requests, hfp_peer_id).await;
+    let mut remote = expect_connection(&mut connect_requests, hfp_under_test.peer_id()).await;
 
     // Peer sends its HF features to the HFP component (AG) - we expect HFP to send
     // its AG features back.
