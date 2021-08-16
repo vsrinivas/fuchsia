8848445523d45dd57fadaa517d63179228415484
Clayton Wilkinson <wilkinsonclay@google.com>
2021-08-16 21:06:10 +0000

    [gn-sdk] Enabling auto indexing of package symbols
    This adds a global flag `fuchsia_auto_index_symbols`.
    When true, the ids.txt file generated from a `fuchsia_package`
    is added to the symbolizer index using the `symbol-index` SDK tool.
    The Fuchsia symbol index used to symbolize logs and to find source
    when using zxdb.
    
    The default value is false, in order to be backwards compatibility
    with existing builds, and only makes sense to set to true when building
    as a developer that will need to symbolize logs or use the debugger with
    locally build code.
    
    To enable it for a build add `fuchsia_auto_index_symbols=true` to
    the gn gen args.
    
    Bugs: b/195019412
    Change-Id: I459a152703c8808c4c4a8bdd5a831a33af3b2c5b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/567504
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>
    Reviewed-by: Dangyi Liu <dangyi@google.com>
    Reviewed-by: Omer Lev-ran <omerlevran@google.com>

--- scripts/sdk/gn/base/build/gn_run_binary.py
+++ scripts/sdk/gn/base/build/gn_run_binary.py
@@ -5,21 +5,41 @@
 """Helper script for GN to run an arbitrary binary.
 
 Run with:
-  python3.8 gn_run_binary.py <binary_name> [args ...]
+  python3.8 gn_run_binary.py [--stamp <stamp_file>] <binary_name> [args ...]
 """
 
 import os
 import subprocess
 import sys
 
-# This script is designed to run binaries produced by the current build. We
-# may prefix it with "./" to avoid picking up system versions that might
-# also be on the path.
-path = sys.argv[1]
-if not os.path.isabs(path):
-    path = './' + path
 
-# The rest of the arguments are passed directly to the executable.
-args = [path] + sys.argv[2:]
+def main(argv) -> int:
 
-sys.exit(subprocess.call(args))
+    # can't use argparse here since it gets confused if the binary being called
+    # has its own --stamp option.
+    if argv and argv[0] == '--stamp':
+        stamp_file = argv[1]
+        parameters = argv[2:]
+    else:
+        stamp_file = None
+        parameters = argv
+
+    # This script is designed to run binaries produced by the current build. We
+    # may prefix it with "./" to avoid picking up system versions that might
+    # also be on the path.
+    path = parameters[0]
+    if not os.path.isabs(path):
+        path = './' + path
+
+    # The rest of the arguments are passed directly to the executable.
+    exec_args = [path] + parameters[1:]
+
+    rc = subprocess.call(exec_args)
+    if not rc and stamp_file:
+        with open(stamp_file, 'w') as f:
+            os.utime(f.name, None)
+    return rc
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
