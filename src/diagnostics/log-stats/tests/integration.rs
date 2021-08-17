f20d49ed157c44e1eebf4b53fd213f2785e143d7
Yaneury Fermin <yaneury@google.com>
2021-08-17 02:26:16 +0000

    [lsc] Migrate BindChild to OpenExposedDir
    This CL is part of a migration of usages of
    fuchsia.sys2.Realm/BindChild to fuchsia.sys2.Realm/OpenExposedDir.
    The former has been deprecated.
    
    For this CL in particular, the log-stats integration test
    has been changed to explicitly bind to the
    `fuchsia.component.Binder` so as to start the `log-stats`
    component.
    
    Change-Id: I4f59e3c5a97604f5896d25f0a11c1fd955b529bd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/563930
    Reviewed-by: Fady Samuel <fsamuel@google.com>
    Commit-Queue: Yaneury Fermin <yaneury@google.com>

--- src/diagnostics/log-stats/tests/integration.rs
+++ src/diagnostics/log-stats/tests/integration.rs
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 use diagnostics_reader::{tree_assertion, ArchiveReader, Inspect};
+use fidl_fuchsia_component as fcomponent;
 use fuchsia_async as fasync;
 use fuchsia_component::client;
 use fuchsia_zircon::DurationNum;
@@ -11,8 +12,13 @@ const RETRY_DELAY_MS: i64 = 300;
 
 #[fuchsia::test]
 async fn log_attribution() {
-    let _dir =
-        client::open_childs_exposed_directory("log-stats", None).await.expect("child is running");
+    // Connecting to `fuchsia.component.Binder` will start the `log-stats` component.
+    let _ = client::connect_to_childs_protocol::<fcomponent::BinderMarker>(
+        "log-stats".to_owned(),
+        None,
+    )
+    .await
+    .expect("child is running");
 
     // We expect two logs from log-stats itself:
     // - INFO: Maintaining.
