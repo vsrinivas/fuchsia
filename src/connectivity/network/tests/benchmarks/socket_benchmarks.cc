7de7fbe13b928e9a605cf28e801aeb5b99877d8c
Bruno Dal Bo <brunodalbo@google.com>
2021-08-17 21:45:22 +0000

    [network] Introduce localhost UDP & TCP benchmarks
    Remove existing UDP trace-based benchmarks in favor of something that
    can run entirely on the host.
    
    Fixed: 82467
    Change-Id: If3ce436ea7c6fdc2ef9e7caf46e884f9258384fc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/567528
    Commit-Queue: Bruno Dal Bo <brunodalbo@google.com>
    Fuchsia-Auto-Submit: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Nick Brown <nickbrow@google.com>

---
+++ src/connectivity/network/tests/benchmarks/socket_benchmarks.cc
@@ -0,0 +1,224 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <arpa/inet.h>
+#include <lib/syslog/cpp/macros.h>
+#include <sys/socket.h>
+
+#include <fbl/unique_fd.h>
+#include <perftest/perftest.h>
+
+#include "src/lib/fxl/strings/string_printf.h"
+
+namespace {
+
+#define CHECK_TRUE_ERRNO(true_condition) FX_CHECK(true_condition) << strerror(errno)
+#define CHECK_ZERO_ERRNO(zero_condition) CHECK_TRUE_ERRNO((zero_condition) == 0)
+
+template <typename T>
+class AddrStorage {
+ public:
+  static_assert(std::is_same_v<T, sockaddr_in> || std::is_same_v<T, sockaddr_in6>);
+  sockaddr* as_sockaddr() { return reinterpret_cast<sockaddr*>(&addr); }
+  socklen_t socklen() const { return sizeof(addr); }
+  T addr;
+};
+
+class Ipv6 {
+ public:
+  using SockAddr = AddrStorage<sockaddr_in6>;
+  static constexpr int kFamily = AF_INET6;
+
+  static SockAddr loopback() {
+    return {
+        .addr =
+            {
+                .sin6_family = kFamily,
+                .sin6_addr = IN6ADDR_LOOPBACK_INIT,
+            },
+    };
+  }
+};
+
+class Ipv4 {
+ public:
+  using SockAddr = AddrStorage<sockaddr_in>;
+  static constexpr int kFamily = AF_INET;
+
+  static SockAddr loopback() {
+    return {
+        .addr =
+            {
+                .sin_family = kFamily,
+                .sin_addr =
+                    {
+                        .s_addr = htonl(INADDR_LOOPBACK),
+                    },
+            },
+    };
+  }
+};
+
+// Helper no-op function to assert functions abstracted over IP version are properly parameterized.
+template <typename Ip>
+void TemplateIsIpVersion() {
+  static_assert(std::is_same_v<Ip, Ipv4> || std::is_same_v<Ip, Ipv6>);
+}
+
+// Tests the unidirectional throughput of streaming `transfer` bytes on a TCP loopback socket.
+//
+// Measures the time to write `transfer` bytes on one end of the socket and read them on the other
+// end on the same thread and calculates the throughput.
+template <typename Ip>
+bool TcpWriteRead(perftest::RepeatState* state, size_t transfer) {
+  TemplateIsIpVersion<Ip>();
+  using Addr = typename Ip::SockAddr;
+  fbl::unique_fd listen_sock;
+  CHECK_TRUE_ERRNO(listen_sock = fbl::unique_fd(socket(Ip::kFamily, SOCK_STREAM, 0)));
+  Addr sockaddr = Ip::loopback();
+  CHECK_ZERO_ERRNO(bind(listen_sock.get(), sockaddr.as_sockaddr(), sockaddr.socklen()));
+  CHECK_ZERO_ERRNO(listen(listen_sock.get(), 0));
+
+  socklen_t socklen = sockaddr.socklen();
+  CHECK_ZERO_ERRNO(getsockname(listen_sock.get(), sockaddr.as_sockaddr(), &socklen));
+
+  fbl::unique_fd client_sock;
+  CHECK_TRUE_ERRNO(client_sock = fbl::unique_fd(socket(Ip::kFamily, SOCK_STREAM, 0)));
+
+  // Set send buffer to transfer size to ensure we can write `transfer` bytes before reading it on
+  // the other end.
+  FX_CHECK(transfer < std::numeric_limits<int32_t>::max());
+  int32_t sndbuf = static_cast<int32_t>(transfer);
+  CHECK_ZERO_ERRNO(setsockopt(client_sock.get(), SOL_SOCKET, SO_SNDBUF, &sndbuf, sizeof(sndbuf)));
+
+  CHECK_ZERO_ERRNO(connect(client_sock.get(), sockaddr.as_sockaddr(), sockaddr.socklen()));
+
+  fbl::unique_fd server_sock;
+  CHECK_TRUE_ERRNO(server_sock = fbl::unique_fd(accept(listen_sock.get(), nullptr, nullptr)));
+
+  std::vector<uint8_t> send_bytes, recv_bytes;
+  // Avoid large memory regions with zeroes that can cause the system to try and reclaim pages from
+  // us. For more information see Zircon page scanner and eviction strategies.
+  send_bytes.resize(transfer, 0xAA);
+  recv_bytes.resize(transfer, 0xBB);
+
+  state->SetBytesProcessedPerRun(transfer);
+  while (state->KeepRunning()) {
+    for (size_t sent = 0; sent < transfer;) {
+      ssize_t wr = write(client_sock.get(), send_bytes.data() + sent, transfer - sent);
+      CHECK_TRUE_ERRNO(wr > 0);
+      sent += wr;
+    }
+    for (size_t recv = 0; recv < transfer;) {
+      ssize_t rd = read(server_sock.get(), recv_bytes.data() + recv, transfer - recv);
+      CHECK_TRUE_ERRNO(rd > 0);
+      recv += rd;
+    }
+  }
+
+  return true;
+}
+
+// Tests the unidirectional throughput of transmitting a message of `size` bytes over a UDP
+// loopback socket.
+//
+// Measures the time to write a message with `message_size` bytes on one end of the socket and read
+// it on the other on the same thread and calculates the throughput.
+template <typename Ip>
+bool UdpWriteRead(perftest::RepeatState* state, size_t message_size) {
+  TemplateIsIpVersion<Ip>();
+  using Addr = typename Ip::SockAddr;
+  fbl::unique_fd server_sock;
+  CHECK_TRUE_ERRNO(server_sock = fbl::unique_fd(socket(Ip::kFamily, SOCK_DGRAM, 0)));
+  Addr sockaddr = Ip::loopback();
+  CHECK_ZERO_ERRNO(bind(server_sock.get(), sockaddr.as_sockaddr(), sockaddr.socklen()));
+
+  socklen_t socklen = sockaddr.socklen();
+  CHECK_ZERO_ERRNO(getsockname(server_sock.get(), sockaddr.as_sockaddr(), &socklen));
+
+  fbl::unique_fd client_sock;
+  CHECK_TRUE_ERRNO(client_sock = fbl::unique_fd(socket(Ip::kFamily, SOCK_DGRAM, 0)));
+  CHECK_ZERO_ERRNO(connect(client_sock.get(), sockaddr.as_sockaddr(), sockaddr.socklen()));
+
+  std::vector<uint8_t> send_bytes, recv_bytes;
+  // Avoid large memory regions with zeroes that can cause the system to try and reclaim pages from
+  // us. For more information see Zircon page scanner and eviction strategies.
+  send_bytes.resize(message_size, 0xAA);
+  recv_bytes.resize(message_size, 0xBB);
+
+  state->SetBytesProcessedPerRun(message_size);
+  while (state->KeepRunning()) {
+    ssize_t wr = write(client_sock.get(), send_bytes.data(), send_bytes.size());
+    CHECK_TRUE_ERRNO(wr > 0);
+    FX_CHECK(static_cast<size_t>(wr) == send_bytes.size())
+        << "wrote " << wr << " expected " << send_bytes.size();
+
+    ssize_t rd = read(server_sock.get(), recv_bytes.data(), recv_bytes.size());
+    CHECK_TRUE_ERRNO(rd > 0);
+    FX_CHECK(static_cast<size_t>(rd) == recv_bytes.size())
+        << "read " << rd << " expected " << send_bytes.size();
+  }
+
+  return true;
+}
+
+void RegisterTests() {
+  constexpr char kTestNameFmt[] = "WriteRead/%s/%s/%ld%s";
+  enum class Transport { kUdp, kTcp };
+  enum class Network { kIpv4, kIpv6 };
+
+  auto get_test_name = [&kTestNameFmt](Transport transport, Network network,
+                                       size_t bytes) -> std::string {
+    const char* unit = "B";
+    if (bytes >= 1024) {
+      bytes /= 1024;
+      unit = "kB";
+    }
+    const char* transport_name = [transport]() {
+      switch (transport) {
+        case Transport::kUdp:
+          return "UDP";
+        case Transport::kTcp:
+          return "TCP";
+      }
+    }();
+    const char* network_name = [network]() {
+      switch (network) {
+        case Network::kIpv4:
+          return "IPv4";
+        case Network::kIpv6:
+          return "IPv6";
+      }
+    }();
+
+    return fxl::StringPrintf(kTestNameFmt, transport_name, network_name, bytes, unit);
+  };
+
+  constexpr size_t kTransferSizesForTcp[] = {
+      1 << 10, 10 << 10, 100 << 10, 500 << 10, 1000 << 10,
+  };
+  for (size_t transfer : kTransferSizesForTcp) {
+    perftest::RegisterTest(get_test_name(Transport::kTcp, Network::kIpv4, transfer).c_str(),
+                           TcpWriteRead<Ipv4>, transfer);
+    perftest::RegisterTest(get_test_name(Transport::kTcp, Network::kIpv6, transfer).c_str(),
+                           TcpWriteRead<Ipv6>, transfer);
+  }
+
+  // NB: Knowledge encoded at a distance here. This should not be hitting IP fragmentation but
+  // Netstack does not support the IP_DONTFRAG flag or equivalent.
+  constexpr size_t kMessageSizesForUdp[] = {1, 100, 1 << 10, 10 << 10, 60 << 10};
+  for (size_t message_size : kMessageSizesForUdp) {
+    perftest::RegisterTest(get_test_name(Transport::kUdp, Network::kIpv4, message_size).c_str(),
+                           UdpWriteRead<Ipv4>, message_size);
+    perftest::RegisterTest(get_test_name(Transport::kUdp, Network::kIpv6, message_size).c_str(),
+                           UdpWriteRead<Ipv6>, message_size);
+  }
+}
+PERFTEST_CTOR(RegisterTests);
+}  // namespace
+
+int main(int argc, char** argv) {
+  constexpr char kTestSuiteName[] = "fuchsia.network.socket.loopback";
+  return perftest::PerfTestMain(argc, argv, kTestSuiteName);
+}
--- src/tests/end_to_end/perf/test/netstack_benchmarks_test.dart
+++ src/tests/end_to_end/perf/test/netstack_benchmarks_test.dart
@@ -2,71 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:sl4f/sl4f.dart';
-import 'package:sl4f/trace_processing.dart';
 import 'package:test/test.dart';
 
 import 'helpers.dart';
 
-const _testName = 'fuchsia.netstack.udp_micro_benchmarks';
-const _appPath =
-    'fuchsia-pkg://fuchsia.com/netstack-benchmarks#meta/udp-benchmark.cmx';
-const _catapultConverterPath = 'runtime_deps/catapult_converter';
-const _trace2jsonPath = 'runtime_deps/trace2json';
-
-// This mirrors kIterationCount in udp_benchmarks.cc.
-const _expectedIterations = 1000;
-
-List<TestCaseResults> _netstackBenchmarksMetricsProcessor(
-    Model model, Map<String, dynamic> extraArgs) {
-  final durations = filterEventsTyped<DurationEvent>(
-    getAllEvents(model),
-    category: 'benchmark',
-    name: extraArgs['eventName'],
-  ).map((event) => event.duration.toMillisecondsF()).toList();
-
-  expect(durations.length, _expectedIterations);
-
-  return [
-    TestCaseResults(extraArgs['outputTestName'], Unit.milliseconds, durations)
-  ];
-}
-
 void main() {
   enableLoggingOutput();
 
-  test(_testName, () async {
+  test('socket_benchmarks', () async {
     final helper = await PerfTestHelper.make();
-
-    final traceSession =
-        await helper.performance.initializeTracing(categories: ['benchmark']);
-    await traceSession.start();
-
-    // TODO(fxbug.dev/53552): Use launch facade instead of ssh.
-    await helper.sl4fDriver.ssh.run('/bin/run $_appPath');
-
-    await traceSession.stop();
-
-    final fxtTraceFile = await traceSession.terminateAndDownload(_testName);
-    final jsonTraceFile = await helper.performance
-        .convertTraceFileToJson(_trace2jsonPath, fxtTraceFile);
-
-    final List<MetricsSpec> metricsSpecs = [];
-
-    for (final mode in ['send', 'recv']) {
-      for (final size in ['64', '1024', '2048', '4096']) {
-        metricsSpecs.add(MetricsSpec(name: 'netstack', extraArgs: {
-          'eventName': '${mode}_${size}bytes',
-          'outputTestName': 'udp/$mode/${size}bytes',
-        }));
-      }
-    }
-
-    await helper.performance.processTrace(
-      MetricsSpecSet(metricsSpecs: metricsSpecs, testName: _testName),
-      jsonTraceFile,
-      converterPath: _catapultConverterPath,
-      registry: {'netstack': _netstackBenchmarksMetricsProcessor},
-    );
+    await helper.runTestComponent(
+        packageName: 'socket-benchmarks',
+        componentName: 'socket-benchmarks.cmx',
+        commandArgs: '-p --quiet --out ${PerfTestHelper.componentOutputPath}');
   }, timeout: Timeout.none);
 }
