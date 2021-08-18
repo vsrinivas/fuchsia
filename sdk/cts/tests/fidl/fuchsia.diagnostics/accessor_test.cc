0e29249e6a7179ec905996b77ad3fe6fb66852ab
Kendal Harland <kjharland@google.com>
2021-08-18 15:29:28 +0000

    [sdk/cts] Migrate fuchsia-diagnostics-tests to CFv2
    Migrating to CFv2 allows this test to launch its child components
    using relative URLs, which are not supported in CFv1. absolute URLs
    are problematic because the CTS release process rewrites package names
    to workaround failures when identical CTS fuchsia packages are included
    in the same build.
    
    Tests:
    - ToT:
      fx set core.x64 --with //sdk/cts/tests/fidl/fuchsia.diagnostics:fuchsia-diagnostics-test
      fx test
    
    - Canary CTS Release
      fx set core.x64 --with //sdk/cts --args cts_version=\"canary\"
      fx build
      sudo cp -r out/default/cts/  prebuilt/cts/canary/linux-x64/
      fx set core.x64 --with //sdk/cts/canary:tests
      fx test //sdk/cts/canary:fuchsia-diagnostics-tests_canary -o -v
    
    Change-Id: I6105ceff388ff7e5e0789d0a2a7f32219f078cc1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/568911
    Commit-Queue: Kendal Harland <kjharland@google.com>
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>

--- sdk/cts/tests/fidl/fuchsia.diagnostics/accessor_test.cc
+++ sdk/cts/tests/fidl/fuchsia.diagnostics/accessor_test.cc
@@ -2,8 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fuchsia/component/cpp/fidl.h>
 #include <fuchsia/diagnostics/cpp/fidl.h>
-#include <fuchsia/sys/cpp/fidl.h>
+#include <fuchsia/sys2/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
 #include <lib/fpromise/bridge.h>
@@ -11,6 +12,8 @@
 #include <lib/fpromise/single_threaded_executor.h>
 #include <lib/inspect/contrib/cpp/archive_reader.h>
 #include <lib/sys/cpp/component_context.h>
+#include <lib/sys/cpp/testing/realm_builder.h>
+#include <lib/sys/cpp/testing/realm_builder_types.h>
 
 #include <regex>
 
@@ -21,18 +24,18 @@
 
 namespace {
 
-constexpr auto kInspectPublisherUrl =
-    "fuchsia-pkg://fuchsia.com/fuchsia-diagnostics-tests#meta/inspect-publisher.cmx";
+class AccessorTest : public zxtest::Test {};
+using namespace sys::testing;
 
 const char EXPECTED_DATA[] = R"JSON({
     "data_source": "Inspect",
     "metadata": {
-        "component_url": "fuchsia-pkg://fuchsia.com/fuchsia-diagnostics-tests#meta/inspect-publisher.cmx",
+        "component_url": "realm-builder://0",
         "errors": null,
         "filename": "fuchsia.inspect.Tree",
         "timestamp": TIMESTAMP
     },
-    "moniker": "inspect-publisher.cmx",
+    "moniker": "fuchsia_component_test_collection\\:CHILD_NAME/inspect-publisher",
     "payload": {
         "root": {
             "arrays": {
@@ -60,87 +63,87 @@ const char EXPECTED_DATA[] = R"JSON({
                 "double": {
                     "buckets": [
                         {
+                            "ceiling": 1.5,
                             "count": 0.0,
-                            "floor": -1.7976931348623157e308,
-                            "ceiling": 1.5
+                            "floor": -1.7976931348623157e308
                         },
                         {
+                            "ceiling": 3.5,
                             "count": 0.0,
-                            "floor": 1.5,
-                            "ceiling": 3.5
+                            "floor": 1.5
                         },
                         {
+                            "ceiling": 8.5,
                             "count": 1.0,
-                            "floor": 3.5,
-                            "ceiling": 8.5
+                            "floor": 3.5
                         },
                         {
+                            "ceiling": 26.0,
                             "count": 0.0,
-                            "floor": 8.5,
-                            "ceiling": 26.0
+                            "floor": 8.5
                         },
                         {
+                            "ceiling": 1.7976931348623157e308,
                             "count": 0.0,
-                            "floor": 26.0,
-                            "ceiling": 1.7976931348623157e308
+                            "floor": 26.0
                         }
                     ]
                 },
                 "int": {
                     "buckets": [
                         {
+                            "ceiling": -10,
                             "count": 0,
-                            "floor": -9223372036854775808,
-                            "ceiling": -10
+                            "floor": -9223372036854775808
                         },
                         {
+                            "ceiling": -8,
                             "count": 0,
-                            "floor": -10,
-                            "ceiling": -8
+                            "floor": -10
                         },
                         {
+                            "ceiling": -4,
                             "count": 1,
-                            "floor": -8,
-                            "ceiling": -4
+                            "floor": -8
                         },
                         {
+                            "ceiling": 8,
                             "count": 0,
-                            "floor": -4,
-                            "ceiling": 8
+                            "floor": -4
                         },
                         {
+                            "ceiling": 9223372036854775807,
                             "count": 0,
-                            "floor": 8,
-                            "ceiling": 9223372036854775807
+                            "floor": 8
                         }
                     ]
                 },
                 "uint": {
                     "buckets": [
                         {
+                            "ceiling": 1,
                             "count": 0,
-                            "floor": 0,
-                            "ceiling": 1
+                            "floor": 0
                         },
                         {
+                            "ceiling": 3,
                             "count": 0,
-                            "floor": 1,
-                            "ceiling": 3
+                            "floor": 1
                         },
                         {
+                            "ceiling": 7,
                             "count": 1,
-                            "floor": 3,
-                            "ceiling": 7
+                            "floor": 3
                         },
                         {
+                            "ceiling": 19,
                             "count": 0,
-                            "floor": 7,
-                            "ceiling": 19
+                            "floor": 7
                         },
                         {
+                            "ceiling": 18446744073709551615,
                             "count": 0,
-                            "floor": 19,
-                            "ceiling": 18446744073709551615
+                            "floor": 19
                         }
                     ]
                 }
@@ -149,87 +152,87 @@ const char EXPECTED_DATA[] = R"JSON({
                 "double": {
                     "buckets": [
                         {
+                            "ceiling": 1.5,
                             "count": 0.0,
-                            "floor": -1.7976931348623157e308,
-                            "ceiling": 1.5
+                            "floor": -1.7976931348623157e308
                         },
                         {
+                            "ceiling": 4.0,
                             "count": 0.0,
-                            "floor": 1.5,
-                            "ceiling": 4.0
+                            "floor": 1.5
                         },
                         {
+                            "ceiling": 6.5,
                             "count": 1.0,
-                            "floor": 4.0,
-                            "ceiling": 6.5
+                            "floor": 4.0
                         },
                         {
+                            "ceiling": 9.0,
                             "count": 0.0,
-                            "floor": 6.5,
-                            "ceiling": 9.0
+                            "floor": 6.5
                         },
                         {
+                            "ceiling": 1.7976931348623157e308,
                             "count": 0.0,
-                            "floor": 9.0,
-                            "ceiling": 1.7976931348623157e308
+                            "floor": 9.0
                         }
                     ]
                 },
                 "int": {
                     "buckets": [
                         {
+                            "ceiling": -10,
                             "count": 0,
-                            "floor": -9223372036854775808,
-                            "ceiling": -10
+                            "floor": -9223372036854775808
                         },
                         {
+                            "ceiling": -8,
                             "count": 0,
-                            "floor": -10,
-                            "ceiling": -8
+                            "floor": -10
                         },
                         {
+                            "ceiling": -6,
                             "count": 0,
-                            "floor": -8,
-                            "ceiling": -6
+                            "floor": -8
                         },
                         {
+                            "ceiling": -4,
                             "count": 1,
-                            "floor": -6,
-                            "ceiling": -4
+                            "floor": -6
                         },
                         {
+                            "ceiling": 9223372036854775807,
                             "count": 0,
-                            "floor": -4,
-                            "ceiling": 9223372036854775807
+                            "floor": -4
                         }
                     ]
                 },
                 "uint": {
                     "buckets": [
                         {
+                            "ceiling": 1,
                             "count": 0,
-                            "floor": 0,
-                            "ceiling": 1
+                            "floor": 0
                         },
                         {
+                            "ceiling": 3,
                             "count": 0,
-                            "floor": 1,
-                            "ceiling": 3
+                            "floor": 1
                         },
                         {
+                            "ceiling": 5,
                             "count": 1,
-                            "floor": 3,
-                            "ceiling": 5
+                            "floor": 3
                         },
                         {
+                            "ceiling": 7,
                             "count": 0,
-                            "floor": 5,
-                            "ceiling": 7
+                            "floor": 5
                         },
                         {
+                            "ceiling": 18446744073709551615,
                             "count": 0,
-                            "floor": 7,
-                            "ceiling": 18446744073709551615
+                            "floor": 7
                         }
                     ]
                 }
@@ -245,50 +248,64 @@ const char EXPECTED_DATA[] = R"JSON({
     "version": 1
 })JSON";
 
-class AccessorTest : public zxtest::Test {};
-
 // Tests that reading inspect data returns expected data from the archive accessor.
 TEST_F(AccessorTest, StreamDiagnosticsInspect) {
   async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
   ASSERT_EQ(loop.StartThread(), ZX_OK);
 
+  static constexpr auto kInspectPublisher = Moniker{"inspect-publisher"};
+  static constexpr auto kInspectPublisherUrl = "#meta/inspect-publisher.cm";
+
   auto context = sys::ComponentContext::Create();
 
-  fuchsia::sys::EnvironmentSyncPtr environment;
-  EXPECT_EQ(ZX_OK, context->svc()->Connect(environment.NewRequest()));
-
-  fuchsia::sys::LauncherSyncPtr launcher;
-  EXPECT_EQ(ZX_OK, environment->GetLauncher(launcher.NewRequest()));
-
-  fuchsia::sys::LaunchInfo launch_info;
-  launch_info.url = kInspectPublisherUrl;
-
-  fuchsia::sys::ComponentControllerPtr controller;
-  EXPECT_EQ(ZX_OK, launcher->CreateComponent(std::move(launch_info), controller.NewRequest()));
-
   fuchsia::diagnostics::ArchiveAccessorPtr accessor;
-  EXPECT_EQ(ZX_OK, context->svc()->Connect(accessor.NewRequest()));
+  ASSERT_EQ(ZX_OK, context->svc()->Connect(accessor.NewRequest()));
 
-  inspect::contrib::ArchiveReader reader(
-      context->svc()->Connect<fuchsia::diagnostics::ArchiveAccessor>(),
-      {"inspect-publisher.cmx:root"});
+  auto realm = Realm::Builder::New(context.get())
+                   .AddComponent(kInspectPublisher,
+                                 Component{
+                                     .source = ComponentUrl{kInspectPublisherUrl},
+                                     .eager = true,
+                                 })
+                   .AddRoute(CapabilityRoute{
+                       .capability = Protocol{"fuchsia.logger.LogSink"},
+                       .source = AboveRoot(),
+                       .targets = {kInspectPublisher},
+                   })
+                   .Build(loop.dispatcher());
+
+  auto _binder = realm.ConnectSync<fuchsia::component::Binder>();
+
+  auto selector =
+      "fuchsia_component_test_collection\\:" + realm.GetChildName() + "/inspect-publisher:root";
+  inspect::contrib::ArchiveReader reader(std::move(accessor), {selector});
 
   fpromise::result<std::vector<inspect::contrib::DiagnosticsData>, std::string> actual_result;
   fpromise::single_threaded_executor executor;
+
   executor.schedule_task(
-      reader.SnapshotInspectUntilPresent({"inspect-publisher.cmx"})
+      reader.SnapshotInspectUntilPresent({"inspect-publisher"})
           .then([&](fpromise::result<std::vector<inspect::contrib::DiagnosticsData>, std::string>&
                         result) mutable { actual_result = std::move(result); }));
+
   executor.run();
 
   EXPECT_TRUE(actual_result.is_ok());
-  EXPECT_EQ(1, actual_result.value().size());
 
   auto& data = actual_result.value()[0];
   data.Sort();
+  std::string actual = data.PrettyJson();
 
-  std::regex reg("\"timestamp\": \\d+");
-  EXPECT_EQ(EXPECTED_DATA, regex_replace(data.PrettyJson(), reg, "\"timestamp\": TIMESTAMP"));
+  std::smatch timestamp_m;
+  EXPECT_TRUE(std::regex_search(actual, timestamp_m, std::regex("\"timestamp\": (\\d+)")));
+
+  std::string expected = EXPECTED_DATA;
+
+  // Replace non-deterministic expected values.
+  expected = std::regex_replace(expected, std::regex("CHILD_NAME"), realm.GetChildName());
+  expected = std::regex_replace(expected, std::regex("TIMESTAMP"), timestamp_m.str(1));
+
+  EXPECT_EQ(expected, actual);
 }
 
 }  // namespace
