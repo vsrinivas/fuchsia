02b612378a11204ca52d31e0ba480820e3100e79
Yifei Teng <yifeit@google.com>
2021-08-21 01:24:06 +0000

    [exception_handler] Migrate to LLCPP result callbacks
    See https://groups.google.com/a/fuchsia.dev/g/fidl-dev/c/peLGiIKrufQ
    
    LLCPP now has the ability to notify send-time errors together with any
    other error during the call in the two-way async callback. The result
    callback avoids racy errors that happen while the call is in-flight.
    
    Bug: 75324
    Change-Id: I95956b3832d0dc8afe01429cc49b7c0b24cccba9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/571858
    Commit-Queue: Yifei Teng <yifeit@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>

--- src/bringup/bin/svchost/exception_handler.cc
+++ src/bringup/bin/svchost/exception_handler.cc
@@ -85,12 +85,13 @@ void ExceptionHandler::Handle(zx::exception exception, const zx_exception_info_t
   exception_info.thread_koid = info.tid;
   exception_info.type = static_cast<fuchsia_exception::wire::ExceptionType>(info.type);
 
-  if (const auto result = connection_->OnException(
-          std::move(exception), exception_info,
-          [](fidl::WireResponse<fuchsia_exception::Handler::OnException>* response) {});
-      result.status() != ZX_OK) {
-    LogError("Failed to pass exception to handler", info, result.status());
-  }
+  connection_->OnException(
+      std::move(exception), exception_info,
+      [info](fidl::WireUnownedResult<fuchsia_exception::Handler::OnException>&& result) {
+        if (!result.ok()) {
+          LogError("Failed to pass exception to handler", info, result.status());
+        }
+      });
 }
 
 bool ExceptionHandler::ConnectedToServer() const { return !server_endpoint_.is_valid(); }
