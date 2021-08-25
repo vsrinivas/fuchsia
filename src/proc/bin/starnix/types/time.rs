bfe85e0bb5785faab058613141b80e5f5539288c
Kevin Lindkvist <lindkvist@google.com>
2021-08-25 16:26:26 +0000

    [starnix] Add additional time conversion functions
    These will be useful for implementing timers.
    
    Bug: 83235
    Change-Id: I8ab7c02cf6b028aef8c55c9f62aa2556fddc94f3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/573303
    Reviewed-by: Adam Barth <abarth@google.com>
    Commit-Queue: Kevin Lindkvist <lindkvist@google.com>

--- src/proc/bin/starnix/types/time.rs
+++ src/proc/bin/starnix/types/time.rs
@@ -30,3 +30,48 @@ pub fn duration_from_timespec(ts: timespec) -> Result<zx::Duration, Errno> {
     }
     return Ok(zx::Duration::from_seconds(ts.tv_sec) + zx::Duration::from_nanos(ts.tv_nsec));
 }
+
+/// Returns a `zx::Time` for the given `timespec`, treating the `timespec` as an absolute point in
+/// time (i.e., not relative to "now").
+#[cfg(test)]
+pub fn time_from_timespec(ts: timespec) -> Result<zx::Time, Errno> {
+    let duration = duration_from_timespec(ts)?;
+    Ok(zx::Time::ZERO + duration)
+}
+
+/// Returns an `itimerspec` with `it_value` set to `deadline` and `it_interval` set to `interval`.
+#[cfg(test)]
+pub fn itimerspec_from_deadline_interval(deadline: zx::Time, interval: zx::Duration) -> itimerspec {
+    itimerspec {
+        it_interval: timespec_from_duration(interval),
+        it_value: timespec_from_time(deadline),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_itimerspec() {
+        let deadline = zx::Time::from_nanos(2 * NANOS_PER_SECOND + 50);
+        let interval = zx::Duration::from_nanos(1000);
+        let time_spec = itimerspec_from_deadline_interval(deadline, interval);
+        assert_eq!(time_spec.it_value.tv_sec, 2);
+        assert_eq!(time_spec.it_value.tv_nsec, 50);
+        assert_eq!(time_spec.it_interval.tv_nsec, 1000);
+    }
+
+    #[test]
+    fn test_time_from_timespec() {
+        let time_spec = timespec { tv_sec: 100, tv_nsec: 100 };
+        let time = time_from_timespec(time_spec).expect("failed to create time from time spec");
+        assert_eq!(time.into_nanos(), 100 * NANOS_PER_SECOND + 100);
+    }
+
+    #[test]
+    fn test_invalid_time_from_timespec() {
+        let time_spec = timespec { tv_sec: 100, tv_nsec: NANOS_PER_SECOND * 2 };
+        assert_eq!(time_from_timespec(time_spec), Err(EINVAL));
+    }
+}
