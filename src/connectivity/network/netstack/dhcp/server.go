76f53ec6272c474d1a19fc4925468e92644fa296
Zeling Feng <zeling@google.com>
2021-08-27 01:49:04 +0000

    [netstack] Run `fx format-code`
    Run `fx format-code` on all go files under
    //src/connectivity/network/netstack to add //go:build lines as go 1.17
    prefers //go:build lines over // +build lines.
    
    Test: No behavior change; result code builds.
    
    Change-Id: Ife4b1452337a6f440ab67d2a28e35f37decd3283
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/573579
    Commit-Queue: Zeling Feng <zeling@google.com>
    Fuchsia-Auto-Submit: Zeling Feng <zeling@google.com>
    Reviewed-by: Tamir Duberstein <tamird@google.com>

--- src/connectivity/network/netstack/dhcp/server.go
+++ src/connectivity/network/netstack/dhcp/server.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !build_with_native_toolchain
 // +build !build_with_native_toolchain
 
 package dhcp
--- src/connectivity/network/netstack/filter/matchers.go
+++ src/connectivity/network/netstack/filter/matchers.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//go:build !build_with_native_toolchain
 // +build !build_with_native_toolchain
 
 package filter
