vsrinivas/fuchsia

commit e7765fa80c24cbcc9c3fe867004d045c770fd53d

[vmm] Avoid undefined behavior
Before this change UBSAN caught a misaligned pointer:

```
../../src/virtualization/bin/vmm/device/virtio_magma.cc:430:7: runtime error: reference binding to misaligned address 0x200b5fbb5114 for type 'const std::unordered_map<unsigned long, ImageInfoWithToken>::key_type' (aka 'const unsigned long'), which requires 8 byte alignment
0x200b5fbb5114: note: pointer points here
  00 00 00 00 00 39 70 06  a9 26 00 00 00 00 00 00  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
              ^
   #0    0x0000230cbdba2eeb in VirtioMagma::Handle_virt_create_image(VirtioMagma*, virtio_magma_virt_create_image_ctrl_t const*, virtio_magma_virt_create_image_resp_t*) ../../src/virtualization/bin/vmm/device/virtio_magma.cc:430 <<application>>+0xa6eeb
   #1.2  0x000021fbe7871e37 in ubsan_GetStackTrace() compiler-rt/lib/ubsan/ubsan_diag.cpp:55 <libclang_rt.asan.so>+0x3be37
   #1.1  0x000021fbe7871e37 in MaybePrintStackTrace() compiler-rt/lib/ubsan/ubsan_diag.cpp:53 <libclang_rt.asan.so>+0x3be37
   #1    0x000021fbe7871e37 in ~ScopedReport() compiler-rt/lib/ubsan/ubsan_diag.cpp:389 <libclang_rt.asan.so>+0x3be37
   #2    0x000021fbe7872acb in handleTypeMismatchImpl() compiler-rt/lib/ubsan/ubsan_handlers.cpp:137 <libclang_rt.asan.so>+0x3cacb
   #3    0x000021fbe78725dd in compiler-rt/lib/ubsan/ubsan_handlers.cpp:142 <libclang_rt.asan.so>+0x3c5dd
   #4    0x0000230cbdba2eeb in VirtioMagma::Handle_virt_create_image(VirtioMagma*, virtio_magma_virt_create_image_ctrl_t const*, virtio_magma_virt_create_image_resp_t*) ../../src/virtualization/bin/vmm/device/virtio_magma.cc:430 <<application>>+0xa6eeb
   #5    0x0000230cbdb949ed in VirtioMagmaGeneric::HandleCommand(VirtioMagmaGeneric*, VirtioChain) x64-asan-ubsan/gen/src/virtualization/bin/vmm/device/virtio_magma_generic.h:2144 <<application>>+0x989ed
   #6    0x0000230cbdb89ba5 in VirtioMagma::NotifyQueue(VirtioMagma*, uint16_t) ../../src/virtualization/bin/vmm/device/virtio_magma.cc:131 <<application>>+0x8dba5

src/virtualization/bin/vmm/device/virtio_magma.cc
@@ -12,13 +12,10 @@
 #include <lib/fit/defer.h>
 #include <lib/syslog/cpp/log_settings.h>
 #include <lib/syslog/cpp/macros.h>
-#include <lib/syslog/global.h>
 #include <lib/trace-provider/provider.h>
 #include <lib/trace/event.h>
 #include <lib/zx/channel.h>
 #include <lib/zx/vmar.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <zircon/status.h>
 
 #include "src/virtualization/bin/vmm/device/magma_image.h"
@@ -171,8 +168,8 @@ zx_status_t VirtioMagma::Handle_release_buffer(const virtio_magma_release_buffer
 
   auto connection = reinterpret_cast<magma_connection_t>(request->connection);
 
-  auto& image_map = connection_image_map_[connection];
-  image_map.erase(request->buffer);
+  const uint64_t buffer = request->buffer;
+  connection_image_map_[connection].erase(buffer);
 
   return ZX_OK;
 }
@@ -193,17 +190,18 @@ zx_status_t VirtioMagma::Handle_internal_map(const virtio_magma_internal_map_ctr
 
   zx::vmo vmo(handle);
 
+  const uint64_t length = request->length;
   zx_vaddr_t zx_vaddr;
   zx_status_t zx_status = vmar_.map(ZX_VM_PERM_READ | ZX_VM_PERM_WRITE, 0 /*vmar_offset*/, vmo,
-                                    0 /* vmo_offset */, request->length, &zx_vaddr);
+                                    0 /* vmo_offset */, length, &zx_vaddr);
   if (zx_status != ZX_OK) {
-    FX_LOGS(ERROR) << "vmar map (length " << request->length << ") failed: " << zx_status;
+    FX_LOGS(ERROR) << "vmar map (length " << length << ") failed: " << zx_status;
     response->result_return = MAGMA_STATUS_INVALID_ARGS;
     return zx_status;
   }
 
   const uint64_t buffer_id = magma_get_buffer_id(reinterpret_cast<magma_buffer_t>(request->buffer));
-  buffer_maps_.emplace(buffer_id, std::pair<zx_vaddr_t, size_t>(zx_vaddr, request->length));
+  buffer_maps_.emplace(buffer_id, std::pair<zx_vaddr_t, size_t>(zx_vaddr, length));
 
   response->address_out = zx_vaddr;
 
@@ -216,7 +214,7 @@ zx_status_t VirtioMagma::Handle_internal_unmap(const virtio_magma_internal_unmap
 
   response->hdr.type = VIRTIO_MAGMA_RESP_INTERNAL_UNMAP;
 
-  uint64_t buffer_id = magma_get_buffer_id(reinterpret_cast<magma_buffer_t>(request->buffer));
+  const uint64_t buffer_id = magma_get_buffer_id(reinterpret_cast<magma_buffer_t>(request->buffer));
 
   for (auto iter = buffer_maps_.find(buffer_id); iter != buffer_maps_.end(); iter++) {
     const auto& mapping = iter->second;
@@ -276,7 +274,8 @@ zx_status_t VirtioMagma::Handle_export(const virtio_magma_export_ctrl_t* request
     auto& image_map =
         connection_image_map_[reinterpret_cast<magma_connection_t>(request->connection)];
 
-    auto iter = image_map.find(request->buffer);
+    const uint64_t buffer = request->buffer;
+    auto iter = image_map.find(buffer);
     if (iter == image_map.end()) {
       response->hdr.type = VIRTIO_MAGMA_RESP_EXPORT;
       response->buffer_handle_out = 0;
@@ -366,8 +365,7 @@ zx_status_t VirtioMagma::Handle_import(const virtio_magma_import_ctrl_t* request
     auto& image_map =
         connection_image_map_[reinterpret_cast<magma_connection_t>(request->connection)];
 
-    magma_buffer_t image = response->buffer_out;
-    image_map[image] = std::move(info);
+    image_map[response->buffer_out] = std::move(info);
   }
 
   return ZX_OK;

src/virtualization/bin/vmm/device/virtio_magma_generic_h_gen.py
@@ -160,7 +160,7 @@ def generate_handle_command(magma):
         ret += '      case VIRTIO_MAGMA_CMD_' + name.upper() + ': {\n'
         ret += '        TRACE_DURATION("magma", "' + name + '");\n'
         ret += '        auto request = reinterpret_cast<const virtio_magma_' + name + '_ctrl_t*>(request_desc.addr);\n'
-        ret += '        auto response = reinterpret_cast<virtio_magma_' + name + '_resp_t*>(response_desc.addr);\n'
+        ret += '        virtio_magma_' + name + '_resp_t response{};\n'
         ret += '#ifdef VIRTMAGMA_DEBUG\n'
         ret += '        FX_LOGS(INFO) << "Received MAGMA command (" << command_type << "):\\n"\\\n'
         ret += '          "  hdr = { " << virtio_magma_ctrl_type_string((virtio_magma_ctrl_type)request->hdr.type) << ", " << request->hdr.flags << " }"\\\n'
@@ -171,12 +171,12 @@ def generate_handle_command(magma):
                         'name'] + ') << ""\\\n'
         ret += '          "";\n'
         ret += '#endif // VIRTMAGMA_DEBUG\n'
-        ret += '        if (response_desc.len < sizeof(*response)) {\n'
+        ret += '        if (response_desc.len < sizeof(response)) {\n'
         ret += '          FX_LOGS(ERROR) << "MAGMA command (" << command_type << ") response descriptor too small";\n'
         ret += '          chain.Return();\n'
         ret += '          return ZX_ERR_INVALID_ARGS;\n'
         ret += '        }\n'
-        ret += '        zx_status_t status = Handle_' + name + '(request, response);\n'
+        ret += '        zx_status_t status = Handle_' + name + '(request, &response);\n'
         ret += '        if (status != ZX_OK) {\n'
         ret += '          FX_LOGS(ERROR) << "Handle_' + name + ' failed (" << zx_status_get_string(status) << ")";\n'
         ret += '          chain.Return();\n'
@@ -184,17 +184,18 @@ def generate_handle_command(magma):
         ret += '        }\n'
         ret += '#ifdef VIRTMAGMA_DEBUG\n'
         ret += '        FX_LOGS(INFO) << "Sending MAGMA response:\\n"\\\n'
-        ret += '          "  hdr = { " << virtio_magma_ctrl_type_string((virtio_magma_ctrl_type)response->hdr.type) << ", " << response->hdr.flags << " }"\\\n'
+        ret += '          "  hdr = { " << virtio_magma_ctrl_type_string((virtio_magma_ctrl_type)response.hdr.type) << ", " << response.hdr.flags << " }"\\\n'
         for argument in export['arguments']:
             if is_response_argument(argument):
                 ret += '          "\\n  ' + argument[
-                    'name'] + ' = " << static_cast<uint64_t>(response->' + argument[
+                    'name'] + ' = " << static_cast<uint64_t>(response.' + argument[
                         'name'] + ') << ""\\\n'
         if export['type'] != 'void':
-            ret += '          "\\n  result_return = " << static_cast<int64_t>(response->result_return) << ""\\\n'
+            ret += '          "\\n  result_return = " << static_cast<int64_t>(response.result_return) << ""\\\n'
         ret += '          "";\n'
         ret += '#endif // VIRTMAGMA_DEBUG\n'
-        ret += '        *chain.Used() = sizeof(*response);\n'
+        ret += '        memcpy(response_desc.addr, &response, sizeof(response));\n'
+        ret += '        *chain.Used() = sizeof(response);\n'
         ret += '      } break;\n'
     ret += '''      default: {
         FX_LOGS(ERROR) << "Unsupported MAGMA command (" << command_type << ")";

src/virtualization/bin/vmm/device/BUILD.gn
@@ -214,9 +214,6 @@ source_set("virtio_magma_lib") {
     defines = [ "VIRTMAGMA_DEBUG=1" ]
   }
 
-  # TODO(fxbug.dev/66436): Fix instances of undefined behavior.
-  configs += [ "//build/config:temporarily_disable_ubsan_do_not_use" ]
-
   # TODO(https://fxbug.dev/58162): delete the below and fix compiler warnings
   configs += [ "//build/config:Wno-conversion" ]
 }

commit a5fbcc3568b673df7fa902dcb0b85b94b54a5edd

[virtualization][virtio_magma] Fix magma_read_notification_channel2
The ubsan fixes in e7765fa80c24cbcc9c3fe867004d045c770fd53d broke
this interface because it works in a very nonobvious way: it writes
the notification bytes into the response buffer starting just after
the response struct.  It also failed to set the used amount in the
response descriptor correctly.

Fix this by giving a higher level command intercept and add a test.

Change-Id: I60dc110309b2e595a8be4872b7391161b8a29c7e
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/574436
Commit-Queue: Craig Stout <cstout@google.com>
Reviewed-by: David Reveman <reveman@google.com>

src/virtualization/bin/vmm/device/virtio_magma.cc
@@ -129,6 +129,57 @@ void VirtioMagma::NotifyQueue(uint16_t queue) {
   }
 }
 
+// Handle special commands here - notably, those that send back bytes beyond
+// what is contained in the response struct.
+zx_status_t VirtioMagma::HandleCommandDescriptors(VirtioDescriptor* request_desc,
+                                                  VirtioDescriptor* response_desc,
+                                                  uint32_t* used_out) {
+  auto header = reinterpret_cast<virtio_magma_ctrl_hdr*>(request_desc->addr);
+
+  switch (header->type) {
+    case VIRTIO_MAGMA_CMD_READ_NOTIFICATION_CHANNEL2: {
+      auto request_copy =
+          *reinterpret_cast<virtio_magma_read_notification_channel2_ctrl_t*>(request_desc->addr);
+      virtio_magma_read_notification_channel2_resp_t response{};
+
+      // TODO(fxbug.dev/83668) - check for sizeof(response) + request_copy.buffer_size
+      if (response_desc->len < sizeof(response)) {
+        FX_LOGS(ERROR)
+            << "VIRTIO_MAGMA_CMD_READ_NOTIFICATION_CHANNEL2: response descriptor too small";
+        return ZX_ERR_INVALID_ARGS;
+      }
+
+      // The notification data immediately follows the response struct.
+      auto notification_buffer =
+          reinterpret_cast<virtio_magma_read_notification_channel2_resp_t*>(response_desc->addr) +
+          1;
+      request_copy.buffer = reinterpret_cast<uint64_t>(notification_buffer);
+
+      zx_status_t status =
+          VirtioMagmaGeneric::Handle_read_notification_channel2(&request_copy, &response);
+      if (status != ZX_OK)
+        return status;
+
+      if (response.result_return == MAGMA_STATUS_OK) {
+        // For testing
+        constexpr uint32_t kMagicFlags = 0xabcd1234;
+        if (header->flags == kMagicFlags && request_copy.buffer_size >= sizeof(kMagicFlags) &&
+            response.buffer_size_out == 0) {
+          memcpy(notification_buffer, &kMagicFlags, sizeof(kMagicFlags));
+          response.buffer_size_out = sizeof(kMagicFlags);
+        }
+      }
+
+      memcpy(response_desc->addr, &response, sizeof(response));
+      *used_out = sizeof(response) + response.buffer_size_out;
+
+      return ZX_OK;
+    }
+  }
+
+  return ZX_ERR_NOT_SUPPORTED;
+}
+
 zx_status_t VirtioMagma::Handle_device_import(const virtio_magma_device_import_ctrl_t* request,
                                               virtio_magma_device_import_resp_t* response) {
   zx::channel server_handle, client_handle;
@@ -246,15 +297,6 @@ zx_status_t VirtioMagma::Handle_poll(const virtio_magma_poll_ctrl_t* request,
   // Transform byte count back to item count
   request_mod.count /= sizeof(magma_poll_item_t);
   return VirtioMagmaGeneric::Handle_poll(&request_mod, response);
-}
-
-zx_status_t VirtioMagma::Handle_read_notification_channel2(
-    const virtio_magma_read_notification_channel2_ctrl_t* request,
-    virtio_magma_read_notification_channel2_resp_t* response) {
-  auto request_mod = *request;
-  // The notification data immediately follows the response struct.
-  request_mod.buffer = reinterpret_cast<uint64_t>(&response[1]);
-  return VirtioMagmaGeneric::Handle_read_notification_channel2(&request_mod, response);
 }
 
 zx_status_t VirtioMagma::Handle_export(const virtio_magma_export_ctrl_t* request,

src/virtualization/bin/vmm/device/virtio_magma.h
@@ -46,6 +46,10 @@ class VirtioMagma : public VirtioMagmaGeneric,
              StartCallback callback) override;
 
  private:
+  zx_status_t HandleCommandDescriptors(VirtioDescriptor* request_desc,
+                                       VirtioDescriptor* response_desc,
+                                       uint32_t* used_out) override;
+
   zx_status_t Handle_device_import(const virtio_magma_device_import_ctrl_t* request,
                                    virtio_magma_device_import_resp_t* response) override;
   zx_status_t Handle_release_connection(const virtio_magma_release_connection_ctrl_t* request,
@@ -58,9 +62,6 @@ class VirtioMagma : public VirtioMagmaGeneric,
                                     virtio_magma_internal_unmap_resp_t* response) override;
   zx_status_t Handle_poll(const virtio_magma_poll_ctrl_t* request,
                           virtio_magma_poll_resp_t* response) override;
-  zx_status_t Handle_read_notification_channel2(
-      const virtio_magma_read_notification_channel2_ctrl_t* request,
-      virtio_magma_read_notification_channel2_resp_t* response) override;
   zx_status_t Handle_export(const virtio_magma_export_ctrl_t* request,
                             virtio_magma_export_resp_t* response) override;
   zx_status_t Handle_import(const virtio_magma_import_ctrl_t* request,

src/virtualization/bin/vmm/device/virtio_magma_generic_h_gen.py
@@ -125,7 +125,11 @@ def generate_generic_method(method, is_internal):
 
 # Generate the main command switch method
 def generate_handle_command(magma):
-    ret = '''  zx_status_t HandleCommand(VirtioChain chain) {
+    ret = '''  virtual zx_status_t HandleCommandDescriptors(VirtioDescriptor* request, VirtioDescriptor* response, uint32_t* used_out) {
+    return ZX_ERR_NOT_SUPPORTED;
+  }
+
+  virtual zx_status_t HandleCommand(VirtioChain chain) {
     TRACE_DURATION("machina", "VirtioMagma::HandleCommand");
     VirtioDescriptor request_desc{};
     if (!chain.NextDescriptor(&request_desc)) {
@@ -176,7 +180,14 @@ def generate_handle_command(magma):
         ret += '          chain.Return();\n'
         ret += '          return ZX_ERR_INVALID_ARGS;\n'
         ret += '        }\n'
-        ret += '        zx_status_t status = Handle_' + name + '(request, &response);\n'
+        ret += '        uint32_t used;\n'
+        ret += '        zx_status_t status = HandleCommandDescriptors(&request_desc, &response_desc, &used);\n'
+        ret += '        if (status != ZX_ERR_NOT_SUPPORTED) {\n'
+        ret += '          *chain.Used() = used;\n'
+        ret += '          chain.Return();\n'
+        ret += '          return status;\n'
+        ret += '        }\n'
+        ret += '        status = Handle_' + name + '(request, &response);\n'
         ret += '        if (status != ZX_OK) {\n'
         ret += '          FX_LOGS(ERROR) << "Handle_' + name + ' failed (" << zx_status_get_string(status) << ")";\n'
         ret += '          chain.Return();\n'

src/virtualization/bin/vmm/device/virtio_magma_test.cc
@@ -454,6 +454,54 @@ TEST_F(VirtioMagmaTest, HandleConnectionMethod) {
   ASSERT_NO_FATAL_FAILURE(ReleaseDevice(device));
 }
 
+TEST_F(VirtioMagmaTest, HandleReadNotificationChannel2) {
+  magma_device_t device{};
+  ASSERT_NO_FATAL_FAILURE(ImportDevice(&device));
+  uint64_t connection{};
+  ASSERT_NO_FATAL_FAILURE(CreateConnection(device, &connection));
+
+  {
+    virtio_magma_read_notification_channel2_ctrl_t request{};
+    request.hdr.type = VIRTIO_MAGMA_CMD_READ_NOTIFICATION_CHANNEL2;
+    constexpr uint32_t kMagicFlags = 0xabcd1234;
+    request.hdr.flags =
+        kMagicFlags;  // VirtioMagma will put these magic flags in the returned buffer
+    request.connection = connection;
+    request.buffer_size = sizeof(uint32_t);
+    request.buffer = 0;  // not used
+    virtio_magma_read_notification_channel2_resp_t response{};
+    uint16_t descriptor_id{};
+    void* response_ptr;
+    ASSERT_EQ(DescriptorChainBuilder(out_queue_)
+                  .AppendReadableDescriptor(&request, sizeof(request))
+                  .AppendWritableDescriptor(&response_ptr, sizeof(response) + request.buffer_size)
+                  .Build(&descriptor_id),
+              ZX_OK);
+    magma_->NotifyQueue(0);
+
+    auto used_elem = NextUsed(&out_queue_);
+    EXPECT_TRUE(used_elem);
+    EXPECT_EQ(used_elem->id, descriptor_id);
+    EXPECT_EQ(used_elem->len, sizeof(response) + request.buffer_size);
+
+    memcpy(&response, response_ptr, sizeof(response));
+    EXPECT_EQ(response.hdr.type, VIRTIO_MAGMA_RESP_READ_NOTIFICATION_CHANNEL2);
+    EXPECT_EQ(response.hdr.flags, 0u);
+    ASSERT_EQ(static_cast<magma_status_t>(response.result_return), MAGMA_STATUS_OK);
+    EXPECT_EQ(response.buffer_size_out, sizeof(uint32_t));
+    EXPECT_EQ(response.more_data_out, 0u);
+
+    uint32_t buffer;
+    memcpy(&buffer,
+           reinterpret_cast<virtio_magma_read_notification_channel2_resp_t*>(response_ptr) + 1,
+           sizeof(buffer));
+    EXPECT_EQ(buffer, kMagicFlags);
+  }
+
+  ASSERT_NO_FATAL_FAILURE(ReleaseConnection(connection));
+  ASSERT_NO_FATAL_FAILURE(ReleaseDevice(device));
+}
+
 TEST_F(VirtioMagmaTest, HandleImportExport) {
   magma_device_t device{};
   ASSERT_NO_FATAL_FAILURE(ImportDevice(&device));