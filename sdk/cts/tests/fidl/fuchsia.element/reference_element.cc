1d3a33618db9d2862ce0eab1e6ed74b9173185f9
Yegor Pomortsev <ypomortsev@google.com>
2021-08-30 23:41:34 +0000

    [session][cts] Serve ViewProvider from reference-element
    Fix flake due to element_manager failing to connect to the element's
    ViewProvider protocol.
    
    The protocol doesn't need to be fully implemented because
    neither element-test nor reference-graphical-presenter use the view
    token eventpair and ViewRef.
    
    Fixed: 83663
    Test: fx test -vo fuchsia-element-tests -- --gtest_repeat=1000 --gtest_break_on_failure=1
    Change-Id: Ie53f834c4ce0341e5a0d088ca4bd55db51dd7e88
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/575422
    Fuchsia-Auto-Submit: Yegor Pomortsev <ypomortsev@google.com>
    Reviewed-by: John Shamoon <johnshamoon@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- sdk/cts/tests/fidl/fuchsia.element/reference_element.cc
+++ sdk/cts/tests/fidl/fuchsia.element/reference_element.cc
@@ -2,14 +2,56 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fuchsia/sys/cpp/fidl.h>
+#include <fuchsia/ui/app/cpp/fidl.h>
+#include <fuchsia/ui/views/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
+#include <lib/fidl/cpp/binding_set.h>
+#include <lib/sys/cpp/component_context.h>
 #include <stdlib.h>
+
+#include <iostream>
+#include <utility>
+
+class ViewProviderImpl : fuchsia::ui::app::ViewProvider {
+ public:
+  fidl::InterfaceRequestHandler<fuchsia::ui::app::ViewProvider> GetHandler() {
+    return bindings_.GetHandler(this);
+  }
+
+ private:
+  // |fuchsia.ui.app.ViewProvider|
+  void CreateView(
+      zx::eventpair view_handle,
+      fidl::InterfaceRequest<fuchsia::sys::ServiceProvider> /*incoming_services*/,
+      fidl::InterfaceHandle<fuchsia::sys::ServiceProvider> /*outgoing_services*/) override {
+    std::cout << "WARNING: ViewControllerImpl.CreateView is not implemented" << std::endl;
+  }
+
+  // |fuchsia.ui.app.ViewProvider|
+  void CreateViewWithViewRef(zx::eventpair view_handle,
+                             fuchsia::ui::views::ViewRefControl view_ref_control,
+                             fuchsia::ui::views::ViewRef view_ref) override {
+    std::cout << "WARNING: ViewControllerImpl.CreateViewWithViewRef is not implemented"
+              << std::endl;
+  }
+
+  // |fuchsia.ui.app.ViewProvider|
+  void CreateView2(fuchsia::ui::app::CreateView2Args args) override {
+    std::cout << "WARNING: ViewControllerImpl.CreateView2 is not implemented" << std::endl;
+  }
+
+  fidl::BindingSet<fuchsia::ui::app::ViewProvider> bindings_;
+};
 
 int main(int argc, const char** argv) {
   async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
+  auto context = sys::ComponentContext::CreateAndServeOutgoingDirectory();
+
+  auto view_provider = std::make_unique<ViewProviderImpl>();
+  context->outgoing()->AddPublicService(view_provider->GetHandler());
 
   loop.Run();
-
   return EXIT_SUCCESS;
 }
