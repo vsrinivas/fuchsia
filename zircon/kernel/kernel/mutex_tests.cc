2bfb579f72abe8ee8baa7247e04a273f97660767
David Greenaway <dgreenaway@google.com>
2021-08-31 06:53:56 +0000

    [kernel][test] Add test exercising singleton mutexes.
    The test ensures that acquiring a singleton mutex is thread-safe the
    first time it is acquired.
    
    At time of writing, we have a bug where singleton mutexes are lazily
    initialised, but not initialised in a thread-safe manner. This means
    that the first time a mutex it is possible that two threads may hold it
    simultaneously.
    
    This test sets up N threads and races them acquiring a singleton mutex
    While the test is safe to run multiple times, it can only exercise the
    static initialisation code path once per boot.
    
    Bug: 78920
    Change-Id: I28d6eb90bb46e681297f59391207b78792ee5626
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/544202
    Reviewed-by: Adrian Danis <adanis@google.com>
    Commit-Queue: David Greenaway <dgreenaway@google.com>

--- zircon/kernel/kernel/mutex_tests.cc
+++ zircon/kernel/kernel/mutex_tests.cc
@@ -83,6 +83,106 @@ bool mutex_assert_held_compile_test() {
   END_TEST;
 }
 
+DECLARE_SINGLETON_MUTEX(TestSingletonMutex);
+
+// Ensure that acquiring a singleton mutex is thread-safe the first time it is
+// acquired.
+//
+// We've previously had bugs where singleton mutexes were defined static and
+// lazily initialised. While in general C++ guarantees that static variables
+// are initialised in a thread-safe manner, the kernel turns off those
+// mechanisms with the compiler flag "-fno-threadsafe-statics". This led to
+// a bug where the first time a mutex was acquired, it could be help by
+// multiple threads simultaneously.
+//
+// This test sets up N threads and races them acquiring the singleton mutex
+// "TestSingletonMutex". While the test is safe to run multiple times, it
+// can only exercise the static initialisation code path once per boot.
+bool singleton_mutex_threadsafe() {
+  BEGIN_TEST;
+
+  // If we have already run print a warning that this test is unlikely to exercise anything.
+  static bool already_run = false;
+  if (already_run) {
+    dprintf(INFO,
+            "Test has already run this boot. "
+            "Subsequent runs will not exercise the mutex init code path again.\n");
+  }
+  already_run = true;
+
+  // Start multiple threads, all attempting to race to acquire the singleton mutex.
+  struct ThreadState {
+    ktl::atomic<bool> ready;
+    ktl::atomic<bool>* should_start;
+    ktl::atomic<bool>* in_critical_section;
+    Thread* thread;
+  };
+  auto worker_body = +[](void* arg) -> int {
+    ThreadState* state = static_cast<ThreadState*>(arg);
+
+    // Tell parent we are ready.
+    state->ready = true;
+
+    // Spin until all threads are ready to start.
+    //
+    // We busy-wait here without yielding to try and synchronise threads on
+    // different CPUs as much as possible, so that they all race to acquire
+    // the mutex below.
+    while (!state->should_start->load(ktl::memory_order_relaxed)) {
+    }
+
+    {
+      // Acquire the mutex.
+      Guard<Mutex> guard(TestSingletonMutex::Get());
+
+      // Ensure no other thread already has the mutex.
+      bool other_thread_in_critical_section = state->in_critical_section->exchange(true);
+      ZX_ASSERT_MSG(!other_thread_in_critical_section,
+                    "Another thread was already in the critical section.");
+
+      // Delay before releasing the mutex, to give other threads a chance to
+      // notice we are holding it.
+      Thread::Current::SleepRelative(ZX_MSEC(1));
+
+      bool still_hold_critical_section = state->in_critical_section->exchange(false);
+      ZX_ASSERT_MSG(still_hold_critical_section, "Another thread released our critical section.");
+    }
+
+    return 0;
+  };
+
+  // Create worker theads and start them up.
+  constexpr int kNumThreads = 4;
+  std::array<ThreadState, kNumThreads> threads;
+  ktl::atomic<bool> should_start = false;
+  ktl::atomic<bool> in_critical_section = false;
+  for (ThreadState& state : threads) {
+    state.ready = false, state.should_start = &should_start,
+    state.in_critical_section = &in_critical_section,
+    state.thread = Thread::Create("test_singleton_mutex", worker_body, &state, DEFAULT_PRIORITY);
+    ASSERT_NONNULL(state.thread, "Thread::Create failed.");
+    state.thread->Resume();
+  }
+
+  // Wait for all the threads to start.
+  for (ThreadState& state : threads) {
+    while (!state.ready) {
+      Thread::Current::Yield();
+    }
+  }
+
+  // Let all the threads race.
+  should_start = true;
+
+  // Wait for all the threads to finish.
+  for (ThreadState& state : threads) {
+    int ret;
+    state.thread->Join(&ret, ZX_TIME_INFINITE);
+  }
+
+  END_TEST;
+}
+
 }  // namespace
 
 UNITTEST_START_TESTCASE(mutex_tests)
@@ -90,4 +190,5 @@ UNITTEST("mutex_lock_unlock", mutex_lock_unlock)
 UNITTEST("mutex_is_held", mutex_is_held)
 UNITTEST("mutex_assert_held", mutex_assert_held)
 UNITTEST("mutex_assert_held_compile_test", mutex_assert_held_compile_test)
+UNITTEST("singleton mutex has thread-safe init", singleton_mutex_threadsafe)
 UNITTEST_END_TESTCASE(mutex_tests, "mutex", "Mutex tests")
