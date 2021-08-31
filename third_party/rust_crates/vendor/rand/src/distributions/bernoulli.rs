8ce6c741c1af3dbacff90cfcab3b269462706958
Peter Johnston <peterjohnston@google.com>
2021-08-31 17:14:07 +0000

    Reland "[tempfile] Update to v3.2"
    This is a reland of a00958d0e03d8284d4fcb42b8e213ed21865aea1
    
    The original CL introduced a dependency on a version of libc that was no
    longer in tree by the time the CL was merged, causing build failures.
    
    Original change's description:
    > [tempfile] Update to v3.2
    >
    > rand_chacha v0.2.1 contains unsound code, and tempfile v3.1 transitively
    > depends on it. Update tempfile to the latest version which bumps its
    > dependency on rand to v0.8 (and therefore rand_chacha to v0.3.1) so that
    > we can (eventually) excise rand_chacha v0.2.1 from the tree.
    >
    > Change-Id: I388049c8b297ed1e91e32497afceb79ac917591f
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/569847
    > Commit-Queue: Peter Johnston <peterjohnston@google.com>
    > Reviewed-by: Joshua Liebow-Feeser <joshlf@google.com>
    > Reviewed-by: Adam Perry <adamperry@google.com>
    
    Change-Id: I0d9970abf34e4b1c12fca5abfde6ca5448c2716e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/575581
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Joshua Liebow-Feeser <joshlf@google.com>
    Commit-Queue: Peter Johnston <peterjohnston@google.com>

--- third_party/rust_crates/vendor/rand/src/distributions/bernoulli.rs
+++ third_party/rust_crates/vendor/rand/src/distributions/bernoulli.rs
@@ -8,9 +8,12 @@
 
 //! The Bernoulli distribution.
 
-use crate::Rng;
 use crate::distributions::Distribution;
+use crate::Rng;
+use core::{fmt, u64};
 
+#[cfg(feature = "serde1")]
+use serde::{Serialize, Deserialize};
 /// The Bernoulli distribution.
 ///
 /// This is a special case of the Binomial distribution where `n = 1`.
@@ -31,6 +34,7 @@ use crate::distributions::Distribution;
 /// so only probabilities that are multiples of 2<sup>-64</sup> can be
 /// represented.
 #[derive(Clone, Copy, Debug)]
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
 pub struct Bernoulli {
     /// Probability of success, relative to the maximal integer.
     p_int: u64,
@@ -55,7 +59,7 @@ pub struct Bernoulli {
 // the RNG, and pay the performance price for all uses that *are* reasonable.
 // Luckily, if `new()` and `sample` are close, the compiler can optimize out the
 // extra check.
-const ALWAYS_TRUE: u64 = ::core::u64::MAX;
+const ALWAYS_TRUE: u64 = u64::MAX;
 
 // This is just `2.0.powi(64)`, but written this way because it is not available
 // in `no_std` mode.
@@ -67,6 +71,17 @@ pub enum BernoulliError {
     /// `p < 0` or `p > 1`.
     InvalidProbability,
 }
+
+impl fmt::Display for BernoulliError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(match self {
+            BernoulliError::InvalidProbability => "p is outside [0, 1] in Bernoulli distribution",
+        })
+    }
+}
+
+#[cfg(feature = "std")]
+impl ::std::error::Error for BernoulliError {}
 
 impl Bernoulli {
     /// Construct a new `Bernoulli` with the given probability of success `p`.
@@ -81,26 +96,32 @@ impl Bernoulli {
     /// 2<sup>-64</sup> in `[0, 1]` can be represented as a `f64`.)
     #[inline]
     pub fn new(p: f64) -> Result<Bernoulli, BernoulliError> {
-        if p < 0.0 || p >= 1.0 {
-            if p == 1.0 { return Ok(Bernoulli { p_int: ALWAYS_TRUE }) }
+        if !(0.0..1.0).contains(&p) {
+            if p == 1.0 {
+                return Ok(Bernoulli { p_int: ALWAYS_TRUE });
+            }
             return Err(BernoulliError::InvalidProbability);
         }
-        Ok(Bernoulli { p_int: (p * SCALE) as u64 })
+        Ok(Bernoulli {
+            p_int: (p * SCALE) as u64,
+        })
     }
 
     /// Construct a new `Bernoulli` with the probability of success of
     /// `numerator`-in-`denominator`. I.e. `new_ratio(2, 3)` will return
     /// a `Bernoulli` with a 2-in-3 chance, or about 67%, of returning `true`.
     ///
-    /// If `numerator == denominator` then the returned `Bernoulli` will always
     /// return `true`. If `numerator == 0` it will always return `false`.
+    /// For `numerator > denominator` and `denominator == 0`, this returns an
+    /// error. Otherwise, for `numerator == denominator`, samples are always
+    /// true; for `numerator == 0` samples are always false.
     #[inline]
     pub fn from_ratio(numerator: u32, denominator: u32) -> Result<Bernoulli, BernoulliError> {
-        if numerator > denominator {
+        if numerator > denominator || denominator == 0 {
             return Err(BernoulliError::InvalidProbability);
         }
         if numerator == denominator {
-            return Ok(Bernoulli { p_int: ALWAYS_TRUE })
+            return Ok(Bernoulli { p_int: ALWAYS_TRUE });
         }
         let p_int = ((f64::from(numerator) / f64::from(denominator)) * SCALE) as u64;
         Ok(Bernoulli { p_int })
@@ -111,7 +132,9 @@ impl Distribution<bool> for Bernoulli {
     #[inline]
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> bool {
         // Make sure to always return true for p = 1.0.
-        if self.p_int == ALWAYS_TRUE { return true; }
+        if self.p_int == ALWAYS_TRUE {
+            return true;
+        }
         let v: u64 = rng.gen();
         v < self.p_int
     }
@@ -119,12 +142,24 @@ impl Distribution<bool> for Bernoulli {
 
 #[cfg(test)]
 mod test {
-    use crate::Rng;
-    use crate::distributions::Distribution;
     use super::Bernoulli;
+    use crate::distributions::Distribution;
+    use crate::Rng;
+
+    #[test]
+    #[cfg(feature="serde1")]
+    fn test_serializing_deserializing_bernoulli() {
+        let coin_flip = Bernoulli::new(0.5).unwrap();
+        let de_coin_flip : Bernoulli = bincode::deserialize(&bincode::serialize(&coin_flip).unwrap()).unwrap();
+
+        assert_eq!(coin_flip.p_int, de_coin_flip.p_int);
+    }
 
     #[test]
     fn test_trivial() {
+        // We prefer to be explicit here.
+        #![allow(clippy::bool_assert_comparison)]
+
         let mut r = crate::test::rng(1);
         let always_false = Bernoulli::new(0.0).unwrap();
         let always_true = Bernoulli::new(1.0).unwrap();
@@ -137,7 +172,7 @@ mod test {
     }
 
     #[test]
-    #[cfg(not(miri))] // Miri is too slow
+    #[cfg_attr(miri, ignore)] // Miri is too slow
     fn test_average() {
         const P: f64 = 0.3;
         const NUM: u32 = 3;
@@ -161,9 +196,9 @@ mod test {
         assert!((avg1 - P).abs() < 5e-3);
 
         let avg2 = (sum2 as f64) / (N as f64);
-        assert!((avg2 - (NUM as f64)/(DENOM as f64)).abs() < 5e-3);
+        assert!((avg2 - (NUM as f64) / (DENOM as f64)).abs() < 5e-3);
     }
-    
+
     #[test]
     fn value_stability() {
         let mut rng = crate::test::rng(3);
@@ -172,6 +207,8 @@ mod test {
         for x in &mut buf {
             *x = rng.sample(&distr);
         }
-        assert_eq!(buf, [true, false, false, true, false, false, true, true, true, true]);
+        assert_eq!(buf, [
+            true, false, false, true, false, false, true, true, true, true
+        ]);
     }
 }
--- third_party/rust_crates/vendor/rand/src/rngs/mock.rs
+++ third_party/rust_crates/vendor/rand/src/rngs/mock.rs
@@ -8,23 +8,27 @@
 
 //! Mock random number generator
 
-use rand_core::{RngCore, Error, impls};
+use rand_core::{impls, Error, RngCore};
+
+#[cfg(feature = "serde1")]
+use serde::{Serialize, Deserialize};
 
 /// A simple implementation of `RngCore` for testing purposes.
-/// 
+///
 /// This generates an arithmetic sequence (i.e. adds a constant each step)
 /// over a `u64` number, using wrapping arithmetic. If the increment is 0
 /// the generator yields a constant.
-/// 
+///
 /// ```
 /// use rand::Rng;
 /// use rand::rngs::mock::StepRng;
-/// 
+///
 /// let mut my_rng = StepRng::new(2, 1);
 /// let sample: [u64; 3] = my_rng.gen();
 /// assert_eq!(sample, [2, 3, 4]);
 /// ```
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
 pub struct StepRng {
     v: u64,
     a: u64,
@@ -34,7 +38,10 @@ impl StepRng {
     /// Create a `StepRng`, yielding an arithmetic sequence starting with
     /// `initial` and incremented by `increment` each time.
     pub fn new(initial: u64, increment: u64) -> Self {
-        StepRng { v: initial, a: increment }
+        StepRng {
+            v: initial,
+            a: increment,
+        }
     }
 }
 
@@ -60,5 +67,21 @@ impl RngCore for StepRng {
     fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
         self.fill_bytes(dest);
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[cfg(feature = "serde1")]
+    fn test_serialization_step_rng() {
+        use super::StepRng;
+
+        let some_rng = StepRng::new(42, 7);
+        let de_some_rng: StepRng =
+            bincode::deserialize(&bincode::serialize(&some_rng).unwrap()).unwrap();
+        assert_eq!(some_rng.v, de_some_rng.v);
+        assert_eq!(some_rng.a, de_some_rng.a);
+
     }
 }
