ddce40231b1f2e3aee3ef09defcaeadb9cada42a
Joshua Liebow-Feeser <joshlf@google.com>
2021-09-03 17:25:21 +0000

    [packet-formats] Add NonZeroDuration constructors
    Add constructors to `NonZeroDuration` which take `NonZeroU32` or
    `NonZeroU64` values, and are thus infallible. Most of these are `const`,
    and all of them will eventually be `const` once `Duration::new` is
    `const` in stable Rust.
    
    Change-Id: I678c04428641526da2022938964571cde8011e46
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/571830
    Commit-Queue: Joshua Liebow-Feeser <joshlf@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Zeling Feng <zeling@google.com>

--- src/connectivity/lib/packet-formats/src/utils.rs
+++ src/connectivity/lib/packet-formats/src/utils.rs
@@ -4,7 +4,7 @@
 
 //! Utilities useful when parsing and serializing wire formats.
 
-use core::num::NonZeroU64;
+use core::num::{NonZeroU32, NonZeroU64};
 use core::time::Duration;
 
 /// A zero-valued `Duration`.
@@ -32,6 +32,40 @@ impl NonZeroDuration {
         NonZeroDuration(Duration::from_secs(secs.get()))
     }
 
+    /// Creates a new `NonZeroDuration` from the specified non-zero number of
+    /// whole seconds and additional nanoseconds.
+    ///
+    /// If the number of nanoseconds is greater than 1 billion (the number of
+    /// nanoseconds in a second), then it will carry over into the seconds
+    /// provided.
+    ///
+    /// # Panics
+    ///
+    /// This constructor will panic if the carry from the nanoseconds overflows
+    /// the seconds counter.
+    // TODO(https://github.com/rust-lang/rust/issues/72440): Make this const
+    pub fn from_nonzero_secs_nanos(secs: NonZeroU64, nanos: NonZeroU32) -> NonZeroDuration {
+        NonZeroDuration(Duration::new(secs.get(), nanos.get()))
+    }
+
+    /// Creates a new `NonZeroDuration` from the specified non-zero number of
+    /// milliseconds.
+    pub const fn from_nonzero_millis(millis: NonZeroU64) -> NonZeroDuration {
+        NonZeroDuration(Duration::from_millis(millis.get()))
+    }
+
+    /// Creates a new `NonZeroDuration` from the specified non-zero number of
+    /// microseconds.
+    pub const fn from_nonzero_micros(micros: NonZeroU64) -> NonZeroDuration {
+        NonZeroDuration(Duration::from_micros(micros.get()))
+    }
+
+    /// Creates a new `NonZeroDuration` from the specified non-zero number of
+    /// nanoseconds.
+    pub const fn from_nonzero_nanos(nanos: NonZeroU64) -> NonZeroDuration {
+        NonZeroDuration(Duration::from_nanos(nanos.get()))
+    }
+
     /// Creates a non-zero if the given value is not zero.
     pub fn new(d: Duration) -> Option<NonZeroDuration> {
         if d == ZERO_DURATION {
@@ -42,7 +76,7 @@ impl NonZeroDuration {
     }
 
     /// Returns the value as a [`Duration`].
-    pub const fn get(&self) -> Duration {
+    pub const fn get(self) -> Duration {
         self.0
     }
 }
@@ -61,14 +95,36 @@ mod tests {
     fn non_zero_duration() {
         // `NonZeroDuration` should not hold a zero duration.
         assert_eq!(NonZeroDuration::new(Duration::from_secs(0)), None);
-        let d = Duration::from_secs(1);
 
         // `new_unchecked` should hold the duration as is.
+        let d = Duration::from_secs(1);
         assert_eq!(unsafe { NonZeroDuration::new_unchecked(d) }, NonZeroDuration(d));
 
         // `NonZeroDuration` should hold a non-zero duration.
         let non_zero = NonZeroDuration::new(d);
         assert_eq!(non_zero, Some(NonZeroDuration(d)));
+
+        let one_u64 = NonZeroU64::new(1).unwrap();
+        assert_eq!(
+            NonZeroDuration::from_nonzero_secs(one_u64),
+            NonZeroDuration(Duration::from_secs(1))
+        );
+        assert_eq!(
+            NonZeroDuration::from_nonzero_secs_nanos(one_u64, NonZeroU32::new(1).unwrap()),
+            NonZeroDuration(Duration::new(1, 1))
+        );
+        assert_eq!(
+            NonZeroDuration::from_nonzero_millis(one_u64),
+            NonZeroDuration(Duration::from_millis(1))
+        );
+        assert_eq!(
+            NonZeroDuration::from_nonzero_micros(one_u64),
+            NonZeroDuration(Duration::from_micros(1))
+        );
+        assert_eq!(
+            NonZeroDuration::from_nonzero_nanos(one_u64),
+            NonZeroDuration(Duration::from_nanos(1))
+        );
 
         // `get` and `Into::into` should return the underlying duration.
         let non_zero = non_zero.unwrap();
