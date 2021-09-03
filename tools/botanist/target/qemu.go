86f882728883757e65e4206e06657f3d2d1c2a89
Yilong Li <liyl@google.com>
2021-09-03 20:19:20 +0000

    [qemu] Only disable fatal thresholds for kernel lockup detector.
    fxrev.dev/496819 introduced "fatal thresholds" for kernel lockup
    detector and system will only reboot when the fatal thresholds
    are exceeded.
    
    This change reenables the non-fatal kernel lockup detection so
    that it won't cause a kernel oops but could still generate error
    messages for debug purpose. This will match the current behavior
    in FEMU virtual device launcher (vdl).
    
    Change-Id: I21d6dab7b9351de34308957c3914a6cd010f2691
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/577383
    Reviewed-by: Ina Huh <ihuh@google.com>
    Commit-Queue: Yilong Li <liyl@google.com>

--- tools/botanist/target/qemu.go
+++ tools/botanist/target/qemu.go
@@ -310,12 +310,9 @@ func (t *QEMUTarget) Start(ctx context.Context, images []bootserver.Image, args 
 	qemuCmd.AddKernelArg("kernel.halt-on-panic=true")
 	// Print a message if `dm poweroff` times out.
 	qemuCmd.AddKernelArg("devmgr.suspend-timeout-debug=true")
-	// Disable kernel lockup detector in emulated environments to prevent false alarms from
-	// potentially oversubscribed hosts.
-	qemuCmd.AddKernelArg("kernel.lockup-detector.critical-section-threshold-ms=0")
+	// Disable kernel lockup detector fatal thresholds in emulated environments to
+	// prevent false alarms from potentially oversubscribed hosts.
 	qemuCmd.AddKernelArg("kernel.lockup-detector.critical-section-fatal-threshold-ms=0")
-	qemuCmd.AddKernelArg("kernel.lockup-detector.heartbeat-period-ms=0")
-	qemuCmd.AddKernelArg("kernel.lockup-detector.heartbeat-age-threshold-ms=0")
 	qemuCmd.AddKernelArg("kernel.lockup-detector.heartbeat-age-fatal-threshold-ms=0")
 	// Do not print colors.
 	qemuCmd.AddKernelArg("TERM=dumb")
