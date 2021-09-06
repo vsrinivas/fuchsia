519301735829ff334064be491ac3bbcbf86ec45d
David Greenaway <dgreenaway@google.com>
2021-09-06 00:48:23 +0000

    [virtualization] Improve usability of mklinux.sh
    A few minor improvements:
    
    * Improve the usage message to explain the "-l" and "-b" options more
      clearly, and show that at least one is required.
    
    * Give an error message if neither "-l" or "-b" is specified.
    
    * Improve error messages if no architecture or an unknown architecture
      is specified.
    
    * Avoid clobbering changes in a Linux directory with changes.
    
    * Add some progress messages prior to silent, long-running operations
      such as clones and fetches.
    
    Change-Id: I8ade77def9e0e769b92d8e47d8d1b6341549582e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/577242
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: David Greenaway <dgreenaway@google.com>

--- src/virtualization/packages/linux_guest/mklinux.sh
+++ src/virtualization/packages/linux_guest/mklinux.sh
@@ -9,13 +9,15 @@
 set -eo pipefail
 
 usage() {
-  echo "usage: ${0} [options] {arm64, x64}"
+  echo "usage: ${0} (-l <linux-dir> | -b <branch>) [options] (arm64 | x64 )"
   echo
-  echo "  -b [branch]     Remote branch of zircon-guest linux repository to"
-  echo "                  pull before building (default: machina)."
-  echo "  -d [defconfig]  Linux config (default: machina_defconfig)."
-  echo "  -l [linux-dir]  Linux source dir."
-  echo "  -o [image]      Output location for the built kernel."
+  echo "  -l <linux-dir>  Build Linux from an existing checkout from this"
+  echo "                  directory. (default: /tmp/linux)"
+  echo "  -b <branch>     Clone or fetch this branch of the Linux kernel from "
+  echo "                  http://zircon-guest.googlesource.com/."
+  echo "                  Linux will be cloned into <linux-dir> if specified."
+  echo "  -d <defconfig>  Linux config to use (default: machina_defconfig)."
+  echo "  -o <image>      Output location for the built kernel."
   echo
   exit 1
 }
@@ -31,12 +33,17 @@ while getopts "b:d:l:o:" OPT; do
 done
 shift $((OPTIND - 1))
 
+# Ensure either "-b <branch>" or "-l <linux-dir>" were provided.
+if [[ -z "${LINUX_BRANCH}" && -z ${LINUX_DIR} ]]; then
+  echo "Error: Either one of '-b <branch>' or '-l <linux dir>' must be provided."
+  echo ""
+  usage
+fi
+
 declare -r LINUX_DIR=${LINUX_DIR:-/tmp/linux}
 declare -r LINUX_DEFCONFIG=${LINUX_DEFCONFIG:-machina_defconfig}
 
-declare -rx KBUILD_BUILD_USER="machina"
-declare -rx KBUILD_BUILD_HOST="fuchsia.com"
-
+# Configure environment for the specified architecture.
 case "${1}" in
 arm64)
   type aarch64-linux-gnu-gcc-9 ||
@@ -50,20 +57,39 @@ x64)
   declare -rx ARCH=x86_64
   declare -x CROSS_COMPILE=x86_64-linux-gnu-
   declare -r LINUX_IMAGE="${LINUX_DIR}/arch/x86/boot/bzImage";;
+"")
+  echo "Error: No architecture specified."
+  echo ""
+  usage;;
 *)
+  echo "Error: Unknown architecture specified."
+  echo ""
   usage;;
 esac
 
-# Current default branch isn't compatible with gcc10
+# Set compiler to use for Linux compile.
+#
+# Linux 4.18 is not compatible with gcc-10. Linux 5.4 is compatible with both
+# gcc-9 and gcc-10. We default to using gcc-9 while we still need to support
+# Linux 4.18.
 declare -x CC=${CROSS_COMPILE}gcc-9
 
+# Clone and checkout the given branch.
 if [ -n "${LINUX_BRANCH}" ]; then
-  # Shallow clone the repository.
+  # Shallow clone the repository if it doesn't exist.
   if [ ! -d "${LINUX_DIR}/.git" ]; then
+    echo "Cloning Linux into ${LINUX_DIR}..."
     git clone --depth 1 --branch "${LINUX_BRANCH}" https://zircon-guest.googlesource.com/third_party/linux "${LINUX_DIR}"
   fi
 
+  # Ensure the repository is clean.
+  if ! git -C "${LINUX_DIR}" diff --quiet; then
+    echo "Git repository in ${LINUX_DIR} is unclean. Aborting."
+    exit 1
+  fi
+
   # Update the repository.
+  echo "Updating and checking out branch ${LINUX_BRANCH}..."
   pushd "${LINUX_DIR}"
   if [[ $(git branch --list "${LINUX_BRANCH}") ]]; then
     git checkout "${LINUX_BRANCH}"
@@ -76,12 +102,19 @@ if [ -n "${LINUX_BRANCH}" ]; then
 fi
 
 # Build Linux.
-pushd "${LINUX_DIR}"
-# CC must be specified on the command line to override the setting in Makefile
-make CC=${CC} "${LINUX_DEFCONFIG}"
-make CC=${CC} -j "$(getconf _NPROCESSORS_ONLN)"
-popd
+(
+    cd "${LINUX_DIR}"
 
+    # Avoid putting the current user's username/hostname in the output image.
+    declare -rx KBUILD_BUILD_USER="machina"
+    declare -rx KBUILD_BUILD_HOST="fuchsia.com"
+
+    # CC must be specified on the command line to override the setting in Makefile
+    make CC=${CC} "${LINUX_DEFCONFIG}"
+    make CC=${CC} -j "$(getconf _NPROCESSORS_ONLN)"
+)
+
+# Move output image to destination.
 if [ -n "${LINUX_OUT}" ]; then
   mkdir -p "$(dirname "${LINUX_OUT}")"
   mv "${LINUX_IMAGE}" "${LINUX_OUT}"
