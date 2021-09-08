5080a433ce24f36b42fa57d03084fe30099c44c4
Yaneury Fermin <yaneury@google.com>
2021-09-08 22:10:36 +0000

    [archivist] Change test to use exposed dir
    This CL changes the `log_unattributed_stream` test
    to connect to LogSink via the exposed directory of
    the root of the test Realm instead of its hub path.
    This is done because the latter path was causing flaky
    behavior for reasons that have yet to be understood.
    
    Bug: 81401
    Bug: 81400
    
    Change-Id: Idbd75dfc17abeaa39609cfc0d2262b07d5e0c421
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/578861
    Commit-Queue: Yaneury Fermin <yaneury@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>

--- src/diagnostics/archivist/tests/v2/src/logs/attribution.rs
+++ src/diagnostics/archivist/tests/v2/src/logs/attribution.rs
@@ -14,7 +14,6 @@ use fidl::{Socket, SocketOpts};
 use fidl_fuchsia_diagnostics::ArchiveAccessorMarker;
 use fidl_fuchsia_logger::{LogFilterOptions, LogLevelFilter, LogMarker, LogSinkMarker};
 use fuchsia_async as fasync;
-use fuchsia_component::client;
 use fuchsia_syslog::levels::INFO;
 use fuchsia_syslog_listener::run_log_listener_with_proxy;
 use futures::{channel::mpsc, StreamExt};
@@ -79,15 +78,10 @@ async fn log_unattributed_stream() {
     let _ =
         EventMatcher::ok().moniker("archivist").wait::<Started>(&mut event_stream).await.unwrap();
 
-    let path = format!(
-         "/hub/children/fuchsia_component_test_collection:{}/children/test/children/archivist/exec/expose/fuchsia.logger.LogSink",
-         instance.root.child_name()
-    );
-
     // connect multiple identical log sinks
     for _ in 0..50 {
         let (message_client, message_server) = Socket::create(SocketOpts::DATAGRAM).unwrap();
-        let log_sink = client::connect_to_protocol_at_path::<LogSinkMarker>(&path).unwrap();
+        let log_sink = instance.root.connect_to_protocol_at_exposed_dir::<LogSinkMarker>().unwrap();
         log_sink.connect(message_server).unwrap();
 
         // each with the same message repeated multiple times
