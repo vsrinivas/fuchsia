fdc23c82bd358c3dbd7854a5417e427a5e9c9499
C Stout <cstout@google.com>
2021-09-08 02:24:50 +0000

    [virtio] Switch VIRTIO_ID_WL to 63
    This is the value used upstream.
    Requires an updated Linux kernel.
    
    Change-Id: I6d67bb7e1802756c005593c54c2d2bfb576f6955
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/578381
    Reviewed-by: David Greenaway <dgreenaway@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: David Reveman <reveman@google.com>
    Commit-Queue: Craig Stout <cstout@google.com>

--- zircon/system/ulib/virtio/include/virtio/virtio_ids.h
+++ zircon/system/ulib/virtio/include/virtio/virtio_ids.h
@@ -42,7 +42,7 @@
 #define VIRTIO_ID_GPU 16          /* virtio GPU */
 #define VIRTIO_ID_INPUT 18        /* virtio input */
 #define VIRTIO_ID_VSOCK 19        /* virtio vsock transport */
-#define VIRTIO_ID_WL 30           /* virtio wayland */
+#define VIRTIO_ID_WL 63           /* virtio wayland */
 #define VIRTIO_ID_MAGMA 50        /* virtio magma */
 
 #endif /* _LINUX_VIRTIO_IDS_H */
