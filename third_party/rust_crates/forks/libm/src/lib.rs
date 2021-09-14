bb772cd092ca3c8ccd8382ae9a9c9c7a3df80fb8
Onath Claridge <claridge@google.com>
2021-09-14 01:04:08 +0000

    [libm] Add ldexp and modf wrappers.
    Change-Id: Ia2d3b10cc21c31d6bc1c1ab414085799a4f41b75
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/580901
    Reviewed-by: Adam Perry <adamperry@google.com>
    Commit-Queue: Onath Claridge <claridge@google.com>

--- third_party/rust_crates/forks/libm/src/lib.rs
+++ third_party/rust_crates/forks/libm/src/lib.rs
@@ -5,6 +5,13 @@
 //! A shim impelementation of the libm crate, binding directly to the in-tree libc's versions of
 //! these functions.
 
+#[macro_use]
+extern crate static_assertions;
+
+// Make sure we aren't building for one of the "esoteric systems" on which c_int is not identical
+// to i32 (https://doc.rust-lang.org/std/os/raw/type.c_int.html).
+assert_type_eq_all!(std::os::raw::c_int, i32);
+
 extern "C" {
     #[link_name = "cbrt"]
     fn cbrt_raw(x: f64) -> f64;
@@ -12,8 +19,14 @@ extern "C" {
     #[link_name = "frexpf"]
     fn frexpf_raw(x: f32, exp: *mut i32) -> f32;
 
+    #[link_name = "ldexp"]
+    fn ldexp_raw(x: f64, n: i32) -> f64;
+
     #[link_name = "ldexpf"]
     fn ldexpf_raw(x: f32, n: i32) -> f32;
+
+    #[link_name = "modf"]
+    fn modf_raw(x: f64, integer_part: *mut f64) -> f64;
 }
 
 /// Cube root
@@ -30,8 +43,23 @@ pub fn frexpf(x: f32) -> (f32, i32) {
     (v, exp)
 }
 
-/// Multiplies a floating point value arg by the number 2 raised to the exp power.
+/// Multiplies an f64 arg by the number 2 raised to the exp power.
+#[inline]
+pub fn ldexp(x: f64, n: i32) -> f64 {
+    unsafe { ldexp_raw(x, n) }
+}
+
+/// Multiplies an f32 arg by the number 2 raised to the exp power.
 #[inline]
 pub fn ldexpf(x: f32, n: i32) -> f32 {
     unsafe { ldexpf_raw(x, n) }
+}
+
+/// Returns the fractional and integral parts of an f64. The return ordering `(fractional_part,
+/// integral_part)` is based on the libm crate from crates.io.
+#[inline]
+pub fn modf(x: f64) -> (f64, f64) {
+    let mut integral_part = 0.0;
+    let fractional_part = unsafe { modf_raw(x, &mut integral_part) };
+    (fractional_part, integral_part)
 }
