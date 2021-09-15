2c0003685c75b1130943e090436048062f3509dc
Ani Ramakrishnan <aniramakri@google.com>
2021-09-15 20:09:38 +0000

    [bt-a2dp-source-integration-tests] Migrate to CFv2
    Define a manifest for A2DP source which specifies source-only mode
    and disables avrcp-target support.
    
    Update integration tests to use v2 Mock Piconet Server library
    functionality to set up the test topology.
    
    Include the v2 Codec Factory package to provide relevant capabilities to
    the A2DP component.
    
    Bug: 74812
    Test: fx test bt-a2dp-source-integration-tests
    
    Change-Id: I56c9f4e6d4e8c32130dff569f84e4baeccfa5a0f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/566675
    Reviewed-by: Marie Janssen 💖 <jamuraa@google.com>
    Fuchsia-Auto-Submit: Ani Ramakrishnan <aniramakri@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/bluetooth/profiles/tests/bt-a2dp-source-integration-tests/src/main.rs
+++ src/connectivity/bluetooth/profiles/tests/bt-a2dp-source-integration-tests/src/main.rs
@@ -2,17 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use {
-    anyhow::{format_err, Error},
-    fidl::encoding::Decodable,
-    fidl_fuchsia_bluetooth_bredr::*,
-    fuchsia_async as fasync,
-    fuchsia_bluetooth::types::{PeerId, Uuid},
-    futures::{stream::StreamExt, TryFutureExt},
-    mock_piconet_client::ProfileTestHarness,
-};
+use fidl::encoding::Decodable;
+use fidl::endpoints::DiscoverableProtocolMarker;
+use fidl_fuchsia_bluetooth_bredr::*;
+use fidl_fuchsia_cobalt::LoggerFactoryMarker;
+use fidl_fuchsia_mediacodec::CodecFactoryMarker;
+use fidl_fuchsia_sysmem::AllocatorMarker;
+use fidl_fuchsia_tracing_provider::RegistryMarker;
+use fixture::fixture;
+use fuchsia_bluetooth::types::Uuid;
+use fuchsia_component_test::{builder::Capability, RealmInstance};
+use futures::stream::StreamExt;
+use mock_piconet_client::v2::{BtProfileComponent, PiconetHarness, PiconetMember};
 
-const A2DP_URL: &str = fuchsia_component::fuchsia_single_component_package_url!("bt-a2dp");
+/// A2DP Source component V2 URL.
+const A2DP_URL_V2: &str =
+    "fuchsia-pkg://fuchsia.com/bt-a2dp-source-integration-tests#meta/bt-a2dp-source.cm";
+const A2DP_SOURCE_MONIKER: &str = "bt-a2dp-source-profile";
+/// Name prefix for mock peers
+const MOCK_PEER_NAME: &str = "mock-peer";
 
 /// Make the SDP definition for an A2DP sink service.
 fn a2dp_sink_service_definition() -> ServiceDefinition {
@@ -37,96 +45,102 @@ fn a2dp_sink_service_definition() -> ServiceDefinition {
     }
 }
 
-/// The launch arguments required to launch A2DP Source.
-fn a2dp_source_launch_info() -> LaunchInfo {
-    LaunchInfo {
-        component_url: Some(A2DP_URL.to_string()),
-        arguments: Some(vec![
-            "--enable-sink".to_string(),
-            "false".to_string(),
-            "--enable-avrcp-target".to_string(),
-            "false".to_string(),
-        ]),
-        ..LaunchInfo::EMPTY
-    }
+struct A2dpSourceIntegrationTest {
+    test_realm: RealmInstance,
+    a2dp_src_under_test: BtProfileComponent,
+    test_driven_peer: PiconetMember,
 }
 
-/// Tests that A2DP source correctly advertises it's services and can be
-/// discovered by another peer in the mock piconet.
-#[fasync::run_singlethreaded(test)]
-async fn test_a2dp_source_service_advertisement() -> Result<(), Error> {
-    let test_harness = ProfileTestHarness::new().expect("Failed to create profile test harness");
+async fn setup_piconet_with_a2dp_source_and_mock_peer() -> A2dpSourceIntegrationTest {
+    let mut test_harness = PiconetHarness::new().await;
 
-    // Create MockPeer #1 to be driven by the test.
-    let id1 = PeerId(1);
-    let mock_peer1 = test_harness.register_peer(id1).await?;
-
-    // Peer #1 adds a search for Audio Sources in the piconet.
-    let mut results_requests = mock_peer1
-        .register_service_search(ServiceClassProfileIdentifier::AudioSource, vec![])
-        .await?;
-
-    // MockPeer #2 is the profile-under-test: A2DP Source.
-    let id2 = PeerId(8);
-    let mock_peer2 = test_harness.register_peer(id2).await?;
-    mock_peer2
-        .launch_profile(a2dp_source_launch_info())
+    // Piconet member to be driven by the test.
+    let spec = test_harness
+        .add_mock_piconet_member(MOCK_PEER_NAME.to_string(), None)
         .await
-        .expect("launch profile should be ok");
+        .expect("can add member");
 
-    // We expect Peer #1 to discover A2DP Source's service advertisement.
-    let service_found_fut = results_requests.select_next_some().map_err(|e| format_err!("{:?}", e));
-    let SearchResultsRequest::ServiceFound { peer_id, responder, .. } = service_found_fut.await?;
-    assert_eq!(id2, peer_id.into());
-    responder.send()?;
+    // Piconet member representing the profile-under-test: A2DP Source.
+    // Note: Only a subset of capabilities are provided to A2DP Source; this is the minimum set of
+    // capabilities sufficient for the scope of these integration tests. This list can be expanded
+    // when the test code becomes more complex.
+    let use_capabilities = vec![
+        Capability::protocol(CodecFactoryMarker::PROTOCOL_NAME),
+        Capability::protocol(LoggerFactoryMarker::PROTOCOL_NAME),
+        Capability::protocol(AllocatorMarker::PROTOCOL_NAME),
+        Capability::protocol(RegistryMarker::PROTOCOL_NAME),
+    ];
+    let a2dp_src_under_test = test_harness
+        .add_profile_with_capabilities(
+            A2DP_SOURCE_MONIKER.to_string(),
+            A2DP_URL_V2.to_string(),
+            None,
+            use_capabilities,
+            vec![],
+        )
+        .await
+        .expect("can add profile");
 
-    Ok(())
+    let test_realm = test_harness.build().await.expect("test topology should build");
+    let test_driven_peer =
+        PiconetMember::new_from_spec(spec, &test_realm).expect("can build member");
+
+    A2dpSourceIntegrationTest { test_realm, a2dp_src_under_test, test_driven_peer }
+}
+
+async fn setup_piconet<F, Fut>(_name: &str, test: F)
+where
+    F: FnOnce(A2dpSourceIntegrationTest) -> Fut,
+    Fut: futures::Future<Output = ()>,
+{
+    let test_fixture = setup_piconet_with_a2dp_source_and_mock_peer().await;
+    test(test_fixture).await
+}
+
+/// Tests that A2DP source correctly advertises it's services and can be discovered by another peer
+/// in the mock piconet.
+#[fixture(setup_piconet)]
+#[fuchsia::test]
+async fn a2dp_source_service_advertisement_discovered_by_peer(tf: A2dpSourceIntegrationTest) {
+    let A2dpSourceIntegrationTest { test_realm: _tr, a2dp_src_under_test, test_driven_peer } = tf;
+
+    // Test driven peer adds a search for Audio Sources in the piconet.
+    let mut results_requests = test_driven_peer
+        .register_service_search(ServiceClassProfileIdentifier::AudioSource, vec![])
+        .expect("can register service search");
+
+    // We expect it to discover A2DP Source component's service advertisement.
+    let service_found_fut = results_requests.select_next_some();
+    let SearchResultsRequest::ServiceFound { peer_id, responder, .. } =
+        service_found_fut.await.expect("should discover service");
+    assert_eq!(peer_id, a2dp_src_under_test.peer_id().into());
+    let _ = responder.send().unwrap();
 }
 
 /// Tests that A2DP source correctly searches for Audio Sinks, discovers a mock peer providing
 /// Audio Sink, and attempts to connect to it.
-#[fasync::run_singlethreaded(test)]
-async fn test_a2dp_source_search_and_connect() -> Result<(), Error> {
-    let test_harness = ProfileTestHarness::new().expect("Failed to create profile test harness");
+#[fixture(setup_piconet)]
+#[fuchsia::test]
+async fn a2dp_source_search_and_connect(tf: A2dpSourceIntegrationTest) {
+    let A2dpSourceIntegrationTest { test_realm: _tr, mut a2dp_src_under_test, test_driven_peer } =
+        tf;
 
-    // MockPeer #1 is driven by the test.
-    let id1 = PeerId(10);
-    let mut mock_peer1 = test_harness.register_peer(id1).await?;
-
-    // Peer #1 advertises an Audio Sink service.
+    // Test-driven peer advertises an AudioSink service.
     let service_defs = vec![a2dp_sink_service_definition()];
-    let mut connect_requests = mock_peer1.register_service_advertisement(service_defs).await?;
+    let mut connect_requests = test_driven_peer
+        .register_service_advertisement(service_defs)
+        .expect("can register advertisement");
 
-    // MockPeer #2 is the profile-under-test: A2DP Source.
-    let id2 = PeerId(20);
-    let mut mock_peer2 = test_harness.register_peer(id2).await?;
-    mock_peer2
-        .launch_profile(a2dp_source_launch_info())
+    // We expect A2DP Source to discover the service advertisement.
+    a2dp_src_under_test
+        .expect_observer_service_found_request(test_driven_peer.peer_id())
         .await
-        .expect("launch profile should be ok");
+        .expect("should observe service found");
 
-    // We expect A2DP Source to discover Peer #1's service advertisement.
-    if let PeerObserverRequest::ServiceFound { peer_id, responder, .. } =
-        mock_peer2.expect_observer_request().await?
-    {
-        assert_eq!(id1, peer_id.into());
-        responder.send()?;
-    }
-
-    // We then expect A2DP Source to attempt to connect to Peer #1.
-    match connect_requests.select_next_some().await? {
+    // We then expect A2DP Source to attempt to connect to the peer.
+    match connect_requests.select_next_some().await.expect("connection request") {
         ConnectionReceiverRequest::Connected { peer_id, .. } => {
-            assert_eq!(id2, peer_id.into());
+            assert_eq!(peer_id, a2dp_src_under_test.peer_id().into());
         }
     }
-
-    // The observer of Peer #1 should be relayed of the connection attempt.
-    match mock_peer1.expect_observer_request().await? {
-        PeerObserverRequest::PeerConnected { peer_id, responder, .. } => {
-            assert_eq!(id2, peer_id.into());
-            responder.send()?;
-        }
-        x => return Err(format_err!("Expected PeerConnected but got: {:?}", x)),
-    }
-    Ok(())
 }
