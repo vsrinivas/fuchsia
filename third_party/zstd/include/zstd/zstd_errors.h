bf926a27febce146ad582245a7729c37fb3905f1
Roland McGrath <mcgrathr@google.com>
2021-09-16 13:11:44 +0000

    [zstd] Prepare to roll to v1.5.0
    This prepares the build integration for a soft transition to roll
    //third_party/zstd/src from the v1.4.8 tag to the v1.5.0 tag.
    
    Bug: 83607
    Change-Id: Ie7f2cee54f63dfe25f9b8faabda69fb07cc3a903
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/581871
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Chris Drouillard <cdrllrd@google.com>

--- third_party/zstd/include/zstd/zstd_errors.h
+++ third_party/zstd/include/zstd/zstd_errors.h
@@ -5,6 +5,11 @@
 #ifndef THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_ERRORS_H_
 #define THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_ERRORS_H_
 
+// TODO(fxbug.dev/83607): temporary during soft transition
+#ifdef _TEMPORARY_ZSTD_148
 #include "../../src/lib/common/zstd_errors.h"
+#else
+#include "../../src/lib/zstd_errors.h"
+#endif
 
 #endif  // THIRD_PARTY_ZSTD_INCLUDE_ZSTD_ZSTD_ERRORS_H_
