8199b540257dc361e52ade4f5d6f0db48768923f
Benjamin Prosnitz <bprosnitz@google.com>
2021-09-16 18:30:08 +0000

    [netsvc] Temporarily disable netsvc stress tests on ASAN builder
    FIDL is undergoing a wire format migration. As part of this, a
    transformer is used to transform between the old and new wire
    format before/after decode/encode. This slows down the netsvc
    stress test so that it keeps timing out.
    
    This CL disables the test on ASAN builds until the migration is
    complete.
    
    Bug: 84697
    Change-Id: I18069a58ddb240fd734f292cb0a04ba3c6b1168e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/581902
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>

--- src/bringup/bin/netsvc/test/paver-stress-test.cc
+++ src/bringup/bin/netsvc/test/paver-stress-test.cc
@@ -20,6 +20,9 @@ void netifc_recv(void* data, size_t len) {}
 bool netifc_send_pending() { return false; }
 }
 
+// TODO(fxbug.dev/84697) Re-enable once FIDL wire format migration is complete.
+#if !__has_feature(address_sanitizer)
+
 // We attempt to write more data than we have memory to ensure we are not keeping the file in memory
 // the entire time.
 TEST_F(PaverTest, WriteFvmManyLargeWrites) {
@@ -48,3 +51,5 @@ TEST_F(PaverTest, WriteFvmManyLargeWrites) {
   ASSERT_OK(paver_.exit_code());
   ASSERT_EQ(fake_svc_.fake_paver().GetCommandTrace(), std::vector<Command>{Command::kWriteVolumes});
 }
+
+#endif // !__has_feature(address_sanitizer)
