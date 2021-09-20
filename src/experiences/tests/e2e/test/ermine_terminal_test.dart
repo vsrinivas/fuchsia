00911034859710f00b689357dde010b9ce08030b
Sanjay Chouksey <sanjayc@google.com>
2021-09-20 15:01:40 +0000

    [e2e] Deflake text injection in terminal tests
    This is to address text injection flakyness found in this build failure:
    https://luci-milo.appspot.com/ui/p/turquoise/builders/global.ci/workstation.x64-debug-e2e/b8835872260351440401/overview
    
    Change-Id: I655f57768eee77f62bbf4ac9a2339f7465813691
    Reviewed-on: https://fuchsia-review.googlesource.com/c/experiences/+/583193
    Fuchsia-Auto-Submit: Sanjay Chouksey <sanjayc@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Yeonhee Lee <yhlee@google.com>

--- src/experiences/tests/e2e/test/ermine_terminal_test.dart
+++ src/experiences/tests/e2e/test/ermine_terminal_test.dart
@@ -79,22 +79,24 @@ void main() {
     });
   }
 
-  // Injects a command into terminal. This assumes only one terminal
-  // instance is running.
+  // Injects a command into terminal. This assumes the terminal view has focus.
   Future<void> inject(String cmd,
       {Duration delay = const Duration(seconds: 1)}) async {
-    await input.text(cmd);
-    await Future.delayed(delay);
-
-    // Wait for buffer to contain the injected command.
     await ermine.waitFor(() async {
-      final buffer = await waitForBuffer();
-      return buffer.contains(cmd);
-    });
+      // Inject the command.
+      await input.text(cmd);
+      await Future.delayed(delay);
 
-    // Commit the command.
-    await input.keyPress(kEnterKey);
-    await Future.delayed(delay);
+      final buffer = await waitForBuffer();
+      if (buffer.contains(cmd)) {
+        // Commit the command by injecting the ENTER key.
+        await input.keyPress(kEnterKey);
+        await Future.delayed(delay);
+        return true;
+      } else {
+        return false;
+      }
+    });
   }
 
   test('Launch and close two terminal instances', () async {
