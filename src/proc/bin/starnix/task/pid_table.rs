45bb20a86d02cae3e57ae75c74b2d3218b21ccf2
Theodore Dubois <tbodt@google.com>
2021-09-22 18:51:47 +0000

    [starnix] Use one hashtable for all things in the PidTable
    Change-Id: I26ae1ba267aed7c6ff2d14765ae193811a4e805f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/583622
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Theodore Dubois <tbodt@google.com>
    Fuchsia-Auto-Submit: Theodore Dubois <tbodt@google.com>

--- src/proc/bin/starnix/task/pid_table.rs
+++ src/proc/bin/starnix/task/pid_table.rs
@@ -8,57 +8,76 @@ use std::sync::{Arc, Weak};
 use crate::task::*;
 use crate::types::*;
 
+/// Entities identified by a pid. In the future this will include process groups and sessions.
+#[derive(Default)]
+struct PidEntry {
+    task: Option<Weak<Task>>,
+    group: Option<Weak<ThreadGroup>>,
+}
+
 pub struct PidTable {
     /// The most-recently allocated pid in this table.
     last_pid: pid_t,
 
     /// The tasks in this table, organized by pid_t.
-    ///
-    /// This reference is the primary reference keeping the tasks alive.
-    tasks: HashMap<pid_t, Weak<Task>>,
-
-    /// The thread groups that are present in this table.
-    thread_groups: HashMap<pid_t, Weak<ThreadGroup>>,
+    table: HashMap<pid_t, PidEntry>,
 }
 
 impl PidTable {
     pub fn new() -> PidTable {
-        PidTable { last_pid: 0, tasks: HashMap::new(), thread_groups: HashMap::new() }
+        PidTable { last_pid: 0, table: HashMap::new() }
+    }
+
+    fn get_entry(&self, pid: pid_t) -> Option<&PidEntry> {
+        self.table.get(&pid)
+    }
+    fn get_entry_mut(&mut self, pid: pid_t) -> &mut PidEntry {
+        self.table.entry(pid).or_insert(Default::default())
     }
 
     pub fn get_task(&self, pid: pid_t) -> Option<Arc<Task>> {
-        self.tasks.get(&pid).and_then(|task| task.upgrade())
+        self.get_entry(pid).and_then(|entry| entry.task.as_ref()).and_then(|task| task.upgrade())
     }
 
     pub fn get_thread_groups(&self) -> Vec<Arc<ThreadGroup>> {
-        self.thread_groups.iter().flat_map(|(_pid, thread_group)| thread_group.upgrade()).collect()
+        self.table
+            .iter()
+            .flat_map(|(_pid, entry)| entry.group.as_ref().and_then(|g| g.upgrade()))
+            .collect()
     }
 
     pub fn allocate_pid(&mut self) -> pid_t {
+        // TODO: wrap the pid number and check for collisions
         self.last_pid += 1;
         return self.last_pid;
     }
 
     pub fn add_task(&mut self, task: &Arc<Task>) {
-        assert!(!self.tasks.contains_key(&task.id));
-        self.tasks.insert(task.id, Arc::downgrade(task));
+        let entry = self.get_entry_mut(task.id);
+        assert!(entry.task.is_none());
+        self.get_entry_mut(task.id).task = Some(Arc::downgrade(task));
     }
 
     pub fn add_thread_group(&mut self, thread_group: &Arc<ThreadGroup>) {
-        assert!(!self.thread_groups.contains_key(&thread_group.leader));
-        self.thread_groups.insert(thread_group.leader, Arc::downgrade(thread_group));
+        let entry = self.get_entry_mut(thread_group.leader);
+        assert!(entry.group.is_none());
+        entry.group = Some(Arc::downgrade(thread_group));
     }
 
     pub fn remove_task(&mut self, pid: pid_t) {
-        self.tasks.remove(&pid);
+        let entry = self.get_entry_mut(pid);
+        assert!(entry.task.is_some());
+        entry.task = None;
     }
 
     pub fn remove_thread_group(&mut self, pid: pid_t) {
-        self.thread_groups.remove(&pid);
+        let entry = self.get_entry_mut(pid);
+        assert!(entry.group.is_some());
+        entry.group = None;
     }
 
     /// Returns the task ids for all the currently running tasks.
     pub fn task_ids(&self) -> Vec<pid_t> {
-        self.tasks.keys().cloned().collect()
+        self.table.iter().flat_map(|(pid, entry)| entry.task.as_ref().and(Some(*pid))).collect()
     }
 }
