0e174438d61e3002f2090ab7367eb23fcf249782
David Gilhooley <djgilhooley@gmail.com>
2021-09-23 16:57:37 +0000

    [ddk-fallback-test] Use DriverTestRealm
    Change-Id: Ib32296c042768b35bcb14ab021766a92b49e1c69
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/582825
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/tests/ddk-fallback-test-v2/test.cc
+++ src/devices/tests/ddk-fallback-test-v2/test.cc
@@ -1,6 +1,7 @@
 // Copyright 2021 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#include <fidl/fuchsia.driver.test/cpp/wire.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
 #include <lib/devmgr-integration-test/fixture.h>
@@ -15,4 +16,26 @@ TEST(DdkFirmwaretest, DriverWasLoaded) {
   fbl::unique_fd out;
   ASSERT_EQ(ZX_OK,
             devmgr_integration_test::RecursiveWaitForFile(dev, "sys/test/ddk-fallback-test", &out));
+}
+
+int main(int argc, char **argv) {
+  // Setup DriverTestRealm.
+  auto client_end = service::Connect<fuchsia_driver_test::Realm>();
+  if (!client_end.is_ok()) {
+    return 1;
+  }
+  auto client = fidl::BindSyncClient(std::move(*client_end));
+
+  fidl::Arena allocator;
+  auto response = client.Start(fuchsia_driver_test::wire::RealmArgs(allocator));
+  if (response.status() != ZX_OK) {
+    return 1;
+  }
+  if (response->result.is_err()) {
+    return 1;
+  }
+
+  // Run the tests.
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
