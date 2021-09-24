b71fe77b334c14d21cd94c5aea73c8715fffd804
Abdulla Kamar <abdulla@google.com>
2021-09-24 07:07:03 +0000

    [syscalls][hypervisor] Format hypervisor syscalls
    Minor change to correctly format the syscall glue.
    
    Change-Id: Idba8e23b3bd65391d7c67dbf6d2477a5458a4611
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/585268
    Fuchsia-Auto-Submit: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Alex Legg <alexlegg@google.com>
    Commit-Queue: Abdulla Kamar <abdulla@google.com>

--- zircon/kernel/lib/syscalls/hypervisor.cc
+++ zircon/kernel/lib/syscalls/hypervisor.cc
@@ -18,108 +18,115 @@
 
 #include "priv.h"
 
-// zx_status_t zx_guest_create
 zx_status_t sys_guest_create(zx_handle_t resource, uint32_t options, user_out_handle* guest_handle,
                              user_out_handle* vmar_handle) {
-  if (options != 0u)
+  if (options != 0u) {
     return ZX_ERR_INVALID_ARGS;
+  }
 
   zx_status_t status =
       validate_ranged_resource(resource, ZX_RSRC_KIND_SYSTEM, ZX_RSRC_SYSTEM_HYPERVISOR_BASE, 1);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   KernelHandle<GuestDispatcher> new_guest_handle;
   KernelHandle<VmAddressRegionDispatcher> new_vmar_handle;
   zx_rights_t guest_rights, vmar_rights;
   status =
       GuestDispatcher::Create(&new_guest_handle, &guest_rights, &new_vmar_handle, &vmar_rights);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   status = guest_handle->make(ktl::move(new_guest_handle), guest_rights);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
   return vmar_handle->make(ktl::move(new_vmar_handle), vmar_rights);
 }
 
-// zx_status_t zx_guest_set_trap
 zx_status_t sys_guest_set_trap(zx_handle_t handle, uint32_t kind, zx_vaddr_t addr, size_t size,
                                zx_handle_t port_handle, uint64_t key) {
   auto up = ProcessDispatcher::GetCurrent();
 
   fbl::RefPtr<GuestDispatcher> guest;
   zx_status_t status = up->handle_table().GetDispatcherWithRights(handle, ZX_RIGHT_WRITE, &guest);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   fbl::RefPtr<PortDispatcher> port;
   if (port_handle != ZX_HANDLE_INVALID) {
     status = up->handle_table().GetDispatcherWithRights(port_handle, ZX_RIGHT_WRITE, &port);
-    if (status != ZX_OK)
+    if (status != ZX_OK) {
       return status;
+    }
   }
 
   return guest->SetTrap(kind, addr, size, ktl::move(port), key);
 }
 
-// zx_status_t zx_vcpu_create
 zx_status_t sys_vcpu_create(zx_handle_t guest_handle, uint32_t options, zx_vaddr_t entry,
                             user_out_handle* out) {
-  if (options != 0u)
+  if (options != 0u) {
     return ZX_ERR_INVALID_ARGS;
+  }
 
   auto up = ProcessDispatcher::GetCurrent();
   fbl::RefPtr<GuestDispatcher> guest;
   zx_status_t status =
       up->handle_table().GetDispatcherWithRights(guest_handle, ZX_RIGHT_MANAGE_PROCESS, &guest);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   KernelHandle<VcpuDispatcher> handle;
   zx_rights_t rights;
   status = VcpuDispatcher::Create(guest, entry, &handle, &rights);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   return out->make(ktl::move(handle), rights);
 }
 
-// zx_status_t zx_vcpu_resume
 zx_status_t sys_vcpu_resume(zx_handle_t handle, user_out_ptr<zx_port_packet_t> user_packet) {
   auto up = ProcessDispatcher::GetCurrent();
 
   fbl::RefPtr<VcpuDispatcher> vcpu;
   zx_status_t status = up->handle_table().GetDispatcherWithRights(handle, ZX_RIGHT_EXECUTE, &vcpu);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
-  zx_port_packet packet;
+  zx_port_packet packet{};
   status = vcpu->Resume(&packet);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   status = user_packet.copy_to_user(packet);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   return ZX_OK;
 }
 
-// zx_status_t zx_vcpu_interrupt
 zx_status_t sys_vcpu_interrupt(zx_handle_t handle, uint32_t vector) {
   auto up = ProcessDispatcher::GetCurrent();
 
   fbl::RefPtr<VcpuDispatcher> vcpu;
   zx_status_t status = up->handle_table().GetDispatcherWithRights(handle, ZX_RIGHT_SIGNAL, &vcpu);
-  if (status != ZX_OK)
+  if (status != ZX_OK) {
     return status;
+  }
 
   vcpu->VirtualInterrupt(vector);
   return ZX_OK;
 }
 
-// zx_status_t zx_vcpu_read_state
 zx_status_t sys_vcpu_read_state(zx_handle_t handle, uint32_t kind, user_out_ptr<void> user_buffer,
                                 size_t buffer_size) {
   auto up = ProcessDispatcher::GetCurrent();
@@ -157,7 +164,6 @@ static zx_status_t WriteState(VcpuDispatcher* vcpu, user_in_ptr<const void> user
   return vcpu->WriteState(state);
 }
 
-// zx_status_t zx_vcpu_write_state
 zx_status_t sys_vcpu_write_state(zx_handle_t handle, uint32_t kind,
                                  user_in_ptr<const void> user_buffer, size_t buffer_size) {
   auto up = ProcessDispatcher::GetCurrent();
