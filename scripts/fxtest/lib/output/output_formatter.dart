578b2e6c9d6863bbeed819414f567976006791e7
Mahesh Saripalli <maheshsr@google.com>
2021-09-28 01:18:21 +0000

    [fx test] Display failed tests at end.
    While running a lot of tests, it is hard to figure out which tests got failed, so we'll add the list of failed tests at the end.
    
    Bug: 66778
    Change-Id: Ia885eefe85dab14dace130ddab921ccaec218747
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/586626
    Commit-Queue: Mahesh Saripalli <maheshsr@google.com>
    Reviewed-by: Gary Boone <gboone@google.com>

--- scripts/fxtest/lib/output/output_formatter.dart
+++ scripts/fxtest/lib/output/output_formatter.dart
@@ -28,6 +28,7 @@ abstract class OutputFormatter {
   final List<TestInfo> _infoEvents = [];
   final List<TestStarted> _testStartedEvents = [];
   final List<TestResult> _testResultEvents = [];
+  final List<TestResult> _testFailedEvents = [];
   final List<UnrunnableTestEvent> _unrunnableTestEvents = [];
   final OutputBuffer buffer;
   final Stylizer wrapWith;
@@ -128,7 +129,12 @@ abstract class OutputFormatter {
       _lastTestStartTime = _now;
       _handleTestStarted(event);
     } else if (event is TestResult) {
-      (event.isSuccess) ? _numPassed += 1 : _numFailed += 1;
+      if (event.isSuccess) {
+        _numPassed += 1;
+      } else {
+        _numFailed += 1;
+        _testFailedEvents.add(event);
+      }
       _handleTestResult(event);
     } else if (event is TestOutputEvent) {
       // This must always come before `TestInfo`, because it is a subclass
@@ -228,6 +234,19 @@ abstract class OutputFormatter {
         'Skipped ${_unrunnableTestEvents.length} unrunnable legacy tests. All device tests must be component-tests, but these are binaries.',
         ..._unrunnableTestEvents.map((e) => '  ${e.testName}')
       ]);
+    }
+    if (_testFailedEvents.isNotEmpty) {
+      String tests = _testFailedEvents.length != 1 ? 'tests' : 'test';
+      buffer.addLine(wrapWith('Failed $tests:', [red]));
+      for (var event in _testFailedEvents) {
+        // testName is a friendly string which contains the whole invocation cmd,
+        // Extract the test name from invocation cmd, on error display whole cmd.
+        var invocationCmd = event.testName.split(' ');
+        var name = wrapWith(
+            invocationCmd.length > 7 ? invocationCmd[7] : event.testName,
+            [cyan]);
+        buffer.addLine('${addEmoji(Emoji.x)}  $name');
+      }
     }
   }
 
