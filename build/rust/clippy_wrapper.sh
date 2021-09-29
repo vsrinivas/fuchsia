bfdd2afd1e5c125473cbee44fff77e52847ccbb4
Joseph Ryan <josephryan3.14@gmail.com>
2021-09-29 19:47:39 +0000

    [build] Add clippy gn targets
    Change-Id: I6b0b1fb51c1841a8eeb09643f261f0652a640fdf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/586003
    Commit-Queue: Joseph Ryan <josephry@google.com>
    Reviewed-by: Tyler Mandry <tmandry@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

---
+++ build/rust/clippy_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -eo pipefail
+
+# the first arg is the rebased path to `target_name.clippy` in the generated
+# output directory, which is used to form all other output paths.
+output="$1"
+# after that the positional args are the clippy-driver command and args set
+# in the clippy GN template
+
+sort -u "$output.transdeps" | cat - "$output.deps" | xargs -d '\n' \
+    "${@:2}" -Cpanic=abort -Zpanic_abort_tests -Zno_codegen \
+    --emit metadata="$output.rmeta" --emit dep-info="$output.d" \
+    --error-format=json --json=diagnostic-rendered-ansi 2>"$output"
