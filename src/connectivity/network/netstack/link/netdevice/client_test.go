vsrinivas/fuchsia

commit f5cd5a5efeab146933f985f258e9477aaef460d8

[netstack] Introduce multi-port client
Refactor netdevice client into Client and Port, so it supports
multi-port network devices.

fuchsia.net.stack/Stack.AddInterface is updated to work with new client
structure, but still assumes that the provided device contains a port 0.

Bug: 82468
Test: `fx test netstack-link-netdevice-gotests
       netstack-integration-tests`
Change-Id: Ied6f2a57403c7316418cb29178617de0931b277d
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/585235
Commit-Queue: Bruno Dal Bo <brunodalbo@google.com>
Reviewed-by: Tamir Duberstein <tamird@google.com>

src/connectivity/network/netstack/fuchsia_net_stack.go
@@ -114,50 +114,87 @@ func (ns *Netstack) getNetInterfaces() []stack.InterfaceInfo {
 	return out
 }
 
-func (ns *Netstack) addInterface(config stack.InterfaceConfig, device stack.DeviceDefinition) stack.StackAddInterfaceResult {
+var _ link.Observer = (*cancellingObserver)(nil)
 
+// TODO(https://fxbug.dev/85061): Remove this type once AddInterface is removed. It exists to
+// support the legacy API only.
+type cancellingObserver struct {
+	link.Observer
+	cancel context.CancelFunc
+}
+
+func (c *cancellingObserver) SetOnLinkClosed(f func()) {
+	c.Observer.SetOnLinkClosed(func() {
+		c.cancel()
+		f()
+	})
+}
+
+// TODO(https://fxbug.dev/85061): Delete this API once fuchsia.net.interfaces.admin can install
+// netdevices.
+func (ns *Netstack) addInterface(config stack.InterfaceConfig, device stack.DeviceDefinition) stack.StackAddInterfaceResult {
 	var (
 		namePrefix string
-		ep         tcpipstack.LinkEndpoint
-		controller link.Controller
-		observer   link.Observer
+		dev        *network.DeviceWithCtxInterface
 	)
 
 	switch device.Which() {
 	case stack.DeviceDefinitionEthernet:
-		client, err := netdevice.NewMacAddressingClient(context.Background(), &device.Ethernet.NetworkDevice, &device.Ethernet.Mac, &netdevice.SimpleSessionConfigFactory{
-			FrameTypes: []network.FrameType{network.FrameTypeEthernet},
-		})
-		if err != nil {
-			_ = syslog.Warnf("failed to create network device client for Ethernet interface: %s", err)
-			return stack.StackAddInterfaceResultWithErr(stack.ErrorInternal)
-		}
-		ep = ethernet.New(client)
-		controller = client
-		observer = client
 		namePrefix = "eth"
+		dev = &device.Ethernet.NetworkDevice
+		// NB: Users are no longer required to provide a connection to MacAddressing, it's retrievable
+		// through the netdevice API. We can just dispose of it here, netdevice.Client will take care
+		// of connecting to it if needed.
+		_ = device.Ethernet.Mac.Close()
 	case stack.DeviceDefinitionIp:
-		client, err := netdevice.NewClient(context.Background(), &device.Ip, &netdevice.SimpleSessionConfigFactory{
-			FrameTypes: []network.FrameType{network.FrameTypeIpv4, network.FrameTypeIpv6},
-		})
-		if err != nil {
-			_ = syslog.Warnf("failed to create network device client for IP interface: %s", err)
-			return stack.StackAddInterfaceResultWithErr(stack.ErrorInternal)
-		}
-		ep = client
-		controller = client
-		observer = client
 		namePrefix = "ip"
+		dev = &device.Ip
 	default:
 		_ = syslog.Errorf("unsupported device definition: %d", device.Which())
 		return stack.StackAddInterfaceResultWithErr(stack.ErrorInvalidArgs)
 	}
 
+	client, err := netdevice.NewClient(context.Background(), dev, &netdevice.SimpleSessionConfigFactory{})
+	if err != nil {
+		_ = syslog.Warnf("failed to create network device client: %s", err)
+		return stack.StackAddInterfaceResultWithErr(stack.ErrorInternal)
+	}
+
+	defer func() {
+		if client != nil {
+			_ = client.Close()
+		}
+	}()
+
+	// Always connect to port zero here to fulfill the deprecated API.
+	port, err := client.NewPort(context.Background(), netdevice.PortId(0))
+	if err != nil {
+		_ = syslog.Warnf("failed to create network device port: %s", err)
+		return stack.StackAddInterfaceResultWithErr(stack.ErrorInternal)
+	}
+
+	defer func() {
+		if port != nil {
+			_ = port.Close()
+		}
+	}()
+
+	var ep tcpipstack.LinkEndpoint
+	switch mode := port.Mode(); mode {
+	case netdevice.PortModeEthernet:
+		ep = ethernet.New(port)
+	case netdevice.PortModeIp:
+		ep = port
+	default:
+		panic(fmt.Sprintf("unknown port mode: %d", mode))
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
 	ifs, err := ns.addEndpoint(
 		makeEndpointName(namePrefix, config.GetNameWithDefault("")),
 		ep,
-		controller,
-		observer,
+		port,
+		&cancellingObserver{Observer: port, cancel: cancel},
 		routes.Metric(config.GetMetricWithDefault(0)),
 	)
 	if err != nil {
@@ -168,6 +205,15 @@ func (ns *Netstack) addInterface(config stack.InterfaceConfig, device stack.Devi
 			return stack.StackAddInterfaceResultWithErr(stack.ErrorInternal)
 		}
 	}
+
+	// Run the device in a goroutine, cancellingObserver will cancel the context
+	// when the port reports a link down.
+	go client.Run(ctx)
+
+	// Prevent deferred functions from cleaning up.
+	client = nil
+	port = nil
+
 	return stack.StackAddInterfaceResultWithResponse(stack.StackAddInterfaceResponse{Id: uint64(ifs.nicid)})
 }
 

src/connectivity/network/netstack/fuchsia_inspect_inspect.go
@@ -528,7 +528,7 @@ func (impl *nicInfoInspectImpl) ListChildren() []string {
 	switch impl.value.controller.(type) {
 	case *eth.Client:
 		children = append(children, ethInfo)
-	case netdevice.InfoProvider:
+	case *netdevice.Port:
 		children = append(children, netdeviceInfo)
 	}
 
@@ -567,7 +567,7 @@ func (impl *nicInfoInspectImpl) GetChild(childName string) inspectInner {
 	case netdeviceInfo:
 		return &netdevInspectImpl{
 			name:  childName,
-			value: impl.value.controller.(netdevice.InfoProvider),
+			value: impl.value.controller.(*netdevice.Port),
 		}
 	default:
 		return nil
@@ -773,7 +773,7 @@ var _ inspectInner = (*netdevInspectImpl)(nil)
 
 type netdevInspectImpl struct {
 	name  string
-	value netdevice.InfoProvider
+	value *netdevice.Port
 }
 
 func (impl *netdevInspectImpl) ReadData() inspect.Object {

src/connectivity/network/netstack/link/netdevice/BUILD.gn
@@ -28,7 +28,6 @@ go_library("netdevice") {
   sources = [
     "client.go",
     "client_test.go",
-    "mac_client.go",
     "session_config.go",
     "session_config_test.go",
   ]

src/connectivity/network/netstack/link/netdevice/mac_client.go
@@ -1,90 +1,0 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-//go:build !build_with_native_toolchain
-// +build !build_with_native_toolchain
-
-package netdevice
-
-import (
-	"context"
-	"fmt"
-	"syscall/zx"
-
-	"go.fuchsia.dev/fuchsia/src/connectivity/network/netstack/link"
-
-	"fidl/fuchsia/hardware/network"
-
-	"gvisor.dev/gvisor/pkg/tcpip"
-	"gvisor.dev/gvisor/pkg/tcpip/stack"
-)
-
-var _ link.Controller = (*MacAddressingClient)(nil)
-var _ stack.GSOEndpoint = (*MacAddressingClient)(nil)
-var _ stack.LinkEndpoint = (*MacAddressingClient)(nil)
-
-// A fuchsia.hardware.network/Device client with an auxiliary
-// fuchsia.hardware.network/MacAddressing proxy that provides the link layer
-// information.
-type MacAddressingClient struct {
-	*Client
-	linkAddress tcpip.LinkAddress
-	mac         *network.MacAddressingWithCtxInterface
-}
-
-func (c *MacAddressingClient) SetPromiscuousMode(b bool) error {
-	var mode network.MacFilterMode
-	if b {
-		mode = network.MacFilterModePromiscuous
-	} else {
-		// NOTE: Netstack currently is not capable of handling multicast
-		// filters, promiscuous mode = false means receive all multicasts still.
-		mode = network.MacFilterModeMulticastPromiscuous
-	}
-	if status, err := c.mac.SetMode(context.Background(), mode); err != nil {
-		return err
-	} else if zx.Status(status) != zx.ErrOk {
-		return &zx.Error{
-			Status: zx.Status(status),
-			Text:   "fuchsia.hardware.network/MacAddressing.SetMode",
-		}
-	}
-
-	return nil
-}
-
-func (c *MacAddressingClient) LinkAddress() tcpip.LinkAddress {
-	return c.linkAddress
-}
-
-// NewMacAddressingClient creates a new Network Device client with attached
-// link-layer MAC information.
-func NewMacAddressingClient(ctx context.Context, dev *network.DeviceWithCtxInterface, mac *network.MacAddressingWithCtxInterface, sessionConfigFactory SessionConfigFactory) (*MacAddressingClient, error) {
-	addr, err := mac.GetUnicastAddress(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get mac address: %w", err)
-	}
-	linkAddress := tcpip.LinkAddress(addr.Octets[:])
-	client, err := NewClient(ctx, dev, sessionConfigFactory)
-	if err != nil {
-		return nil, err
-	}
-
-	// Set device to multicast promiscuous to match current behavior. When
-	// Netstack controls multicast filters this can be removed.
-	if status, err := mac.SetMode(ctx, network.MacFilterModeMulticastPromiscuous); err != nil {
-		return nil, fmt.Errorf("failed to set multicast promiscuous: %w", err)
-	} else if zx.Status(status) != zx.ErrOk {
-		return nil, &zx.Error{
-			Status: zx.Status(status),
-			Text:   "fuchsia.hardware.network/MacAddressing.SetMode",
-		}
-	}
-
-	return &MacAddressingClient{
-		Client:      client,
-		linkAddress: linkAddress,
-		mac:         mac,
-	}, nil
-}

src/connectivity/network/netstack/link/netdevice/session_config.go
@@ -13,15 +13,13 @@ import (
 	"fidl/fuchsia/hardware/network"
 )
 
-// A factory of session configurations from device information.
-// A default implementation is provided by SimpleSessionConfigFactory.
+// SessionConfigFactory creates session configurations from device information.
 type SessionConfigFactory interface {
-	// Creates a SessionConfig for a given network device based on the provided
-	// deviceInfo and portStatus.
-	MakeSessionConfig(deviceInfo network.DeviceInfo, portStatus network.PortStatus) (SessionConfig, error)
+	MakeSessionConfig(deviceInfo network.DeviceInfo) (SessionConfig, error)
 }
 
-// Configuration used to open a session with a network device.
+// SessionConfig holds configuration used to open a session with a network
+// device.
 type SessionConfig struct {
 	// Length of each buffer.
 	BufferLength uint32
@@ -39,48 +37,22 @@ type SessionConfig struct {
 	TxDescriptorCount uint16
 	// Session flags.
 	Options network.SessionFlags
-	// Types of rx frames to subscribe to.
-	RxFrames []network.FrameType
 }
 
-// The buffer length used by SimpleSessionConfigFactory.
+// DefaultBufferLength is the buffer length used by SimpleSessionConfigFactory.
 const DefaultBufferLength uint32 = 2048
 
-// A simple session configuration factory.
-type SimpleSessionConfigFactory struct {
-	// The frame types to subscribe to. Will subscribe to all frame types if
-	// empty.
-	FrameTypes []network.FrameType
-}
-
-type InsufficientBufferLengthError struct {
-	BufferLength uint32
-	BufferHeader uint16
-	BufferTail   uint16
-	MTU          uint32
-}
-
-func (e *InsufficientBufferLengthError) Error() string {
-	return fmt.Sprintf("buffer length=%d < header=%d + tail=%d + mtu=%d", e.BufferLength, e.BufferHeader, e.BufferTail, e.MTU)
-}
+// SimpleSessionConfigFactory is the default configuration factory.
+type SimpleSessionConfigFactory struct{}
 
 // MakeSessionConfig implements SessionConfigFactory.
-func (c *SimpleSessionConfigFactory) MakeSessionConfig(deviceInfo network.DeviceInfo, portStatus network.PortStatus) (SessionConfig, error) {
+func (c *SimpleSessionConfigFactory) MakeSessionConfig(deviceInfo network.DeviceInfo) (SessionConfig, error) {
 	bufferLength := DefaultBufferLength
 	if bufferLength > deviceInfo.MaxBufferLength {
 		bufferLength = deviceInfo.MaxBufferLength
 	}
 	if bufferLength < deviceInfo.MinRxBufferLength {
 		bufferLength = deviceInfo.MinRxBufferLength
-	}
-
-	if bufferLength < uint32(deviceInfo.MinTxBufferHead)+uint32(deviceInfo.MinTxBufferTail)+portStatus.GetMtu() {
-		return SessionConfig{}, &InsufficientBufferLengthError{
-			BufferLength: bufferLength,
-			BufferHeader: deviceInfo.MinTxBufferHead,
-			BufferTail:   deviceInfo.MinTxBufferTail,
-			MTU:          portStatus.GetMtu(),
-		}
 	}
 
 	config := SessionConfig{
@@ -92,7 +64,6 @@ func (c *SimpleSessionConfigFactory) MakeSessionConfig(deviceInfo network.Device
 		RxDescriptorCount: deviceInfo.RxDepth,
 		TxDescriptorCount: deviceInfo.TxDepth,
 		Options:           network.SessionFlagsPrimary,
-		RxFrames:          c.FrameTypes,
 	}
 	align := deviceInfo.BufferAlignment
 	if config.BufferStride%align != 0 {
@@ -104,4 +75,28 @@ func (c *SimpleSessionConfigFactory) MakeSessionConfig(deviceInfo network.Device
 		}
 	}
 	return config, nil
+}
+
+type insufficientBufferLengthError struct {
+	bufferLength uint32
+	bufferHeader uint16
+	bufferTail   uint16
+	mtu          uint32
+}
+
+func (e *insufficientBufferLengthError) Error() string {
+	return fmt.Sprintf("buffer=%d < header=%d + tail=%d + mtu=%d", e.bufferLength, e.bufferHeader, e.bufferTail, e.mtu)
+}
+
+func (c *SessionConfig) checkValidityForPort(portStatus network.PortStatus) error {
+	mtu := portStatus.GetMtu()
+	if c.BufferLength < uint32(c.TxHeaderLength)+uint32(c.TxTailLength)+mtu {
+		return &insufficientBufferLengthError{
+			bufferLength: c.BufferLength,
+			bufferHeader: c.TxHeaderLength,
+			bufferTail:   c.TxTailLength,
+			mtu:          mtu,
+		}
+	}
+	return nil
 }

src/connectivity/network/netstack/link/netdevice/session_config_test.go
@@ -8,6 +8,7 @@
 package netdevice
 
 import (
+	"errors"
 	"testing"
 
 	"fidl/fuchsia/hardware/network"
@@ -27,16 +28,12 @@ func TestMakeSessionConfig(t *testing.T) {
 	baseInfo.SetMinTxBufferHead(txBufferHead)
 	baseInfo.SetMinTxBufferTail(txBufferTail)
 
-	factory := SimpleSessionConfigFactory{
-		FrameTypes: []network.FrameType{network.FrameTypeEthernet},
-	}
+	factory := SimpleSessionConfigFactory{}
 
 	tests := []struct {
-		name             string
-		updateInfo       func(*network.DeviceInfo)
-		updatePortStatus func(*network.PortStatus)
-		expectedConfig   SessionConfig
-		expectedErr      *InsufficientBufferLengthError
+		name           string
+		updateInfo     func(*network.DeviceInfo)
+		expectedConfig SessionConfig
 	}{
 		{
 			name: "defaults",
@@ -49,7 +46,6 @@ func TestMakeSessionConfig(t *testing.T) {
 				RxDescriptorCount: baseInfo.TxDepth,
 				TxDescriptorCount: baseInfo.RxDepth,
 				Options:           network.SessionFlagsPrimary,
-				RxFrames:          factory.FrameTypes,
 			},
 		},
 		{
@@ -66,7 +62,6 @@ func TestMakeSessionConfig(t *testing.T) {
 				RxDescriptorCount: baseInfo.TxDepth,
 				TxDescriptorCount: baseInfo.RxDepth,
 				Options:           network.SessionFlagsPrimary,
-				RxFrames:          factory.FrameTypes,
 			},
 		},
 		{
@@ -83,7 +78,6 @@ func TestMakeSessionConfig(t *testing.T) {
 				RxDescriptorCount: baseInfo.TxDepth,
 				TxDescriptorCount: baseInfo.RxDepth,
 				Options:           network.SessionFlagsPrimary,
-				RxFrames:          factory.FrameTypes,
 			},
 		},
 		{
@@ -101,19 +95,6 @@ func TestMakeSessionConfig(t *testing.T) {
 				RxDescriptorCount: baseInfo.TxDepth,
 				TxDescriptorCount: baseInfo.RxDepth,
 				Options:           network.SessionFlagsPrimary,
-				RxFrames:          factory.FrameTypes,
-			},
-		},
-		{
-			name: "insufficient buffer length",
-			updatePortStatus: func(portStatus *network.PortStatus) {
-				portStatus.SetMtu(DefaultBufferLength)
-			},
-			expectedErr: &InsufficientBufferLengthError{
-				BufferLength: DefaultBufferLength,
-				BufferHeader: txBufferHead,
-				BufferTail:   txBufferTail,
-				MTU:          DefaultBufferLength,
 			},
 		},
 	}
@@ -124,27 +105,63 @@ func TestMakeSessionConfig(t *testing.T) {
 			if test.updateInfo != nil {
 				test.updateInfo(&info)
 			}
-			var portStatus network.PortStatus
-			if test.updatePortStatus != nil {
-				test.updatePortStatus(&portStatus)
+			sessionConfig, err := factory.MakeSessionConfig(info)
+			if err != nil {
+				t.Fatalf("MakeSessionConfig(%+v): %s", info, err)
 			}
-			sessionConfig, err := factory.MakeSessionConfig(info, portStatus)
-			if test.expectedErr != nil {
-				if concreteErr, ok := err.(*InsufficientBufferLengthError); ok {
-					if diff := cmp.Diff(concreteErr, test.expectedErr); diff != "" {
-						t.Errorf("MakeSessionConfig(%+v, %+v) error diff: (-want +got)\n%s", info, portStatus, diff)
-					}
-				} else {
-					t.Fatalf("got MakeSessionConfig(%+v, %+v) error = %+v, want %s", info, portStatus, err, test.expectedErr)
-				}
-			} else {
+			if diff := cmp.Diff(test.expectedConfig, sessionConfig); diff != "" {
+				t.Errorf("MakeSessionConfig(%+v): (-want +got)\n%s", info, diff)
+			}
+		})
+	}
+}
+
+func TestCheckSessionConfig(t *testing.T) {
+	config := SessionConfig{
+		BufferLength:   DefaultBufferLength,
+		TxHeaderLength: 16,
+		TxTailLength:   8,
+	}
+
+	tests := []struct {
+		name  string
+		mtu   uint32
+		check func(t *testing.T, portStatus network.PortStatus, err error)
+	}{
+		{
+			name: "success",
+			mtu:  config.BufferLength - uint32(config.TxHeaderLength+config.TxTailLength),
+			check: func(t *testing.T, portStatus network.PortStatus, err error) {
 				if err != nil {
-					t.Fatalf("MakeSessionConfig(%+v, %+v): %s", info, portStatus, err)
+					t.Fatalf("config.checkValidityForPort(%+v) = %s", portStatus, err)
 				}
-				if diff := cmp.Diff(test.expectedConfig, sessionConfig); diff != "" {
-					t.Errorf("MakeSessionConfig(%+v, %+v): (-want +got)\n%s", info, portStatus, diff)
+			},
+		},
+		{
+			name: "failure",
+			mtu:  config.BufferLength,
+			check: func(t *testing.T, portStatus network.PortStatus, err error) {
+				var got *insufficientBufferLengthError
+				if !errors.As(err, &got) {
+					t.Fatalf("checkValidityForPort(%+v) = %s, expected %T", portStatus, err, got)
 				}
-			}
+				if diff := cmp.Diff(got, &insufficientBufferLengthError{
+					bufferLength: config.BufferLength,
+					bufferHeader: config.TxHeaderLength,
+					bufferTail:   config.TxTailLength,
+					mtu:          config.BufferLength,
+				}, cmp.AllowUnexported(*got)); diff != "" {
+					t.Errorf("checkValidityForPort(%+v) error diff: (-want +got)\n%s", portStatus, diff)
+				}
+			},
+		},
+	}
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			var portStatus network.PortStatus
+			portStatus.SetMtu(testCase.mtu)
+			err := config.checkValidityForPort(portStatus)
+			testCase.check(t, portStatus, err)
 		})
 	}
 }

commit 177b6f24660c4b99d4beb88b82aafd829c18faad

[netstack] Avoid referencing closed VMOs...
... in netdevice.Client. If netdevice.Client.Close was called while any
of the goroutines was operating on either the descriptor or the data
VMOs it could cause a runtime panic.

Fix by only closing the VMOs after Run completes, which is similar to
what previous netdevice code did prior to multi-port support.

Regression was introduced in f5cd5a5efeab146933f985f258e9477aaef460d8.

Multiply: netstack-interfaces-fidl-test: 30
Fixed: 85607
Change-Id: I1929b10b8e999fc6130a7451b01ee93903f45066
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/587903
Commit-Queue: Bruno Dal Bo <brunodalbo@google.com>
Fuchsia-Auto-Submit: Bruno Dal Bo <brunodalbo@google.com>
Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
Reviewed-by: Tamir Duberstein <tamird@google.com>

src/connectivity/network/netstack/fuchsia_net_stack.go
@@ -208,7 +208,11 @@ func (ns *Netstack) addInterface(config stack.InterfaceConfig, device stack.Devi
 
 	// Run the device in a goroutine, cancellingObserver will cancel the context
 	// when the port reports a link down.
-	go client.Run(ctx)
+	go func(client *netdevice.Client) {
+		if err := client.Run(ctx); err != nil {
+			_ = syslog.Errorf("netdevice client run error client.Run(_) = %s", err)
+		}
+	}(client)
 
 	// Prevent deferred functions from cleaning up.
 	client = nil

src/connectivity/network/netstack/link/netdevice/client.go
@@ -64,8 +64,9 @@ type Client struct {
 
 	mu struct {
 		sync.RWMutex
-		closed bool
-		ports  map[PortId]*Port
+		closed     bool
+		didCallRun bool
+		ports      map[PortId]*Port
 	}
 }
 
@@ -265,7 +266,15 @@ func (p *Port) Close() error {
 	return multierr.Combine(p.port.Close(), p.watcher.Close(), err)
 }
 
-func (c *Client) Run(ctx context.Context) {
+func (c *Client) Run(ctx context.Context) error {
+	c.mu.Lock()
+	hadCalledRun := c.mu.didCallRun
+	c.mu.didCallRun = true
+	c.mu.Unlock()
+	if hadCalledRun {
+		panic("can't call Run twice on the same client")
+	}
+
 	ctx, cancel := context.WithCancel(ctx)
 	detachWithError := func(reason error) {
 		cancel()
@@ -351,6 +360,10 @@ func (c *Client) Run(ctx context.Context) {
 	}()
 
 	wg.Wait()
+
+	// We can only close the VMOs after all the goroutines here have ended to
+	// prevent any references into the VMO after it's unmapped.
+	return multierr.Combine(c.data.Close(), c.descriptors.Close())
 }
 
 func (p *Port) IsAttached() bool {
@@ -470,15 +483,23 @@ func (c *Client) Close() error {
 		c.mu.closed = true
 		ports := c.mu.ports
 		c.mu.ports = nil
+
+		// NB: descriptors and data VMOs are closed only after all the goroutines
+		// in Run are closed to prevent data races.
+		// If Run was not called, close them here.
+		var err error
+		if !c.mu.didCallRun {
+			err = multierr.Combine(c.data.Close(), c.descriptors.Close())
+		}
+
 		return ports, multierr.Combine(
+			err,
 			c.device.Close(),
 			// Session also has a Close method, make sure we're calling the ChannelProxy
 			// one.
 			((*fidl.ChannelProxy)(c.session)).Close(),
 			c.handler.RxFifo.Close(),
 			c.handler.TxFifo.Close(),
-			c.data.Close(),
-			c.descriptors.Close(),
 		)
 	}()
 

src/connectivity/network/netstack/link/netdevice/client_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 	"time"
 
+	"go.fuchsia.dev/fuchsia/src/connectivity/network/netstack/link/fifo"
 	"go.fuchsia.dev/fuchsia/src/connectivity/network/netstack/testutil"
 	"go.fuchsia.dev/fuchsia/src/lib/component"
 	syslog "go.fuchsia.dev/fuchsia/src/lib/syslog/go"
@@ -324,7 +325,9 @@ func runClient(t *testing.T, client *Client) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
-		client.Run(ctx)
+		if err := client.Run(ctx); err != nil {
+			t.Errorf("client.Run(_) = %s", err)
+		}
 		wg.Done()
 	}()
 	t.Cleanup(func() {
@@ -811,7 +814,9 @@ func TestShutdown(t *testing.T) {
 			ctx, cancel := context.WithCancel(ctx)
 			clientClosed := make(chan struct{})
 			go func() {
-				client.Run(ctx)
+				if err := client.Run(ctx); err != nil {
+					t.Errorf("client.Run(_) = %s", err)
+				}
 				close(clientClosed)
 			}()
 			t.Cleanup(func() {
@@ -1064,4 +1069,43 @@ func TestPairExchangePackets(t *testing.T) {
 	}
 	lPort.Wait()
 	rPort.Wait()
+}
+
+func TestClosesVMOsIfDidntRun(t *testing.T) {
+	ctx := context.Background()
+	_, _, client, _ := createTunClientPair(t, ctx)
+
+	vmos := []struct {
+		name string
+		vmo  fifo.MappedVMO
+	}{
+		{
+			name: "data",
+			vmo:  client.data,
+		},
+		{
+			name: "descriptors",
+			vmo:  client.descriptors,
+		},
+	}
+
+	// Close the client and expect already closed errors on the VMOs.
+	if err := client.Close(); err != nil {
+		t.Fatalf("client.Close() = %s", err)
+	}
+
+	for _, vmo := range vmos {
+		t.Run(vmo.name, func(t *testing.T) {
+			data := vmo.vmo.GetData(0, 1)
+			// Try to write into the unmapped region and expect a panic.
+			defer func() {
+				if r := recover(); r == nil {
+					t.Errorf("expected to have observed a panic")
+				}
+			}()
+
+			t.Errorf("data = %d, should've panicked", data[0])
+		})
+	}
+
 }