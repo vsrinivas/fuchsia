661804d220270a754b91ac3645f831718b8741d9
Jerry Belton <jcecil@google.com>
2021-10-04 20:03:13 +0000

    [cts] Removing obsolete testdriver code.
    We have decided to integrate the CTS out-of-tree test driver into ffx.
    See  go/ffx-cts-plugin-doc for more info.
    
    Change-Id: I0474e04ac3a4115111ccd6baa3e3769a5b2a3dc8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/589483
    Reviewed-by: John Shamoon <johnshamoon@google.com>
    Fuchsia-Auto-Submit: Jerry Belton <jcecil@google.com>
    Commit-Queue: Jerry Belton <jcecil@google.com>

--- sdk/cts/tools/testdriver/common_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-package testdriver
-
-import "flag"
-
-var testDataDir = flag.String("test_data_dir", "", "Path to test data; only used in GN build")
