a71334d4976dcea90f49916642f814c35ebb63cc
Simon Shields <simonshields@google.com>
2021-10-05 23:55:26 +0000

    [fx-create] Fixes for the driver template.
    Stop relying on fake-ddk, which new drivers can't use.
    Also fix the naming of the "unit-test" target, which wasn't globally
    unique.
    
    Change-Id: I8b8a03491b629e0faeb22766a93e51309b85d5ef
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/589785
    Commit-Queue: Simon Shields <simonshields@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>

--- tools/create/goldens/my-driver-cpp/unit-tests.cc
+++ tools/create/goldens/my-driver-cpp/unit-tests.cc
@@ -2,39 +2,40 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <lib/fake_ddk/fake_ddk.h>
+#include <memory>
 
 #include <sdk/lib/inspect/testing/cpp/zxtest/inspect.h>
 #include <zxtest/zxtest.h>
 
 #include "tools/create/goldens/my-driver-cpp/my_driver_cpp.h"
+#include "src/devices/testing/mock-ddk/mock-device.h"
 
 namespace my_driver_cpp {
 
 using inspect::InspectTestHelper;
 
 class MyDriverCppTest : public InspectTestHelper, public zxtest::Test {
-  void SetUp() override {}
+  void SetUp() override { fake_root_ = MockDevice::FakeRootParent(); }
 
   void TearDown() override {}
 
  protected:
-  fake_ddk::Bind ddk_;
+  std::shared_ptr<zx_device> fake_root_;
 };
 
 TEST_F(MyDriverCppTest, LifetimeTest) {
-  auto device = new MyDriverCpp(fake_ddk::kFakeParent);
+  auto device = new MyDriverCpp(fake_root_.get());
   ASSERT_OK(device->Bind());
-  ASSERT_OK(ddk_.WaitUntilInitComplete());
+  device->zxdev()->InitOp();
+  ASSERT_OK(device->zxdev()->WaitUntilInitReplyCalled(zx::time::infinite()));
   device->DdkAsyncRemove();
-  EXPECT_TRUE(ddk_.Ok());
-  device->DdkRelease();
+  ASSERT_OK(mock_ddk::ReleaseFlaggedDevices(fake_root_.get()));
 }
 
 // TODO: `is_bound` is an example inspect property. Replace this test with inspect properties
 // if any are added to the driver. Remove this test if no new inspect nodes/properties were added.
 TEST_F(MyDriverCppTest, InspectTest) {
-  auto device = new MyDriverCpp(fake_ddk::kFakeParent);
+  auto device = new MyDriverCpp(fake_root_.get());
   // Verify is_bound = false.
   ASSERT_NO_FATAL_FAILURES(ReadInspect(device->inspect_vmo()));
   ASSERT_NO_FATAL_FAILURES(
@@ -47,10 +48,10 @@ TEST_F(MyDriverCppTest, InspectTest) {
   ASSERT_NO_FATAL_FAILURES(
       CheckProperty(hierarchy().node(), "is_bound", inspect::BoolPropertyValue(true)));
 
-  ASSERT_OK(ddk_.WaitUntilInitComplete());
+  device->zxdev()->InitOp();
+  ASSERT_OK(device->zxdev()->WaitUntilInitReplyCalled(zx::time::infinite()));
   device->DdkAsyncRemove();
-  EXPECT_TRUE(ddk_.Ok());
-  device->DdkRelease();
+  ASSERT_OK(mock_ddk::ReleaseFlaggedDevices(fake_root_.get()));
 }
 
 }  // namespace my_driver_cpp
