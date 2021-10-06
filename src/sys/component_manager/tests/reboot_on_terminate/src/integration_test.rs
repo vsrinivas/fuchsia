a841f458b9803f84f378d15d5c2b31b7050bb3c1
Satsuki Ueno <satsukiu@google.com>
2021-10-06 22:37:47 +0000

    Revert "[cts] Add prebuilt reboot_on_terminate_test to CTS"
    This reverts commit 60b1e42696dcc62a8c325ff3a4fbcfe60e18763d.
    
    Reason for revert: Possibly breaking e2e tests
    
    Original change's description:
    > [cts] Add prebuilt reboot_on_terminate_test to CTS
    >
    > This CL adds reboot_on_terminate_test to the CTS. It uses the CTS build
    > rules to verify that FIDL dependencies are released in an SDK and that
    > non-FIDL dependencies are permitted for use in the CTS.
    >
    > To run this test as a prebuilt:
    >
    > 1. fx set core.x64 --with //sdk/cts --args cts_version=\"canary\"
    > 2. fx build
    > 3. sudo cp out/default/cts/*.far out/default/cts/test_manifest.json prebuilt/cts/canary/linux-x64/cts
    > 4. fx set core.x64 --with //sdk/cts/canary:tests
    > 5. fx test reboot_on_terminate_test_canary
    >
    > Change-Id: I025be285cb53aff9803116fb6b24cd5d624b0ec9
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/568008
    > Reviewed-by: Gary Bressler <geb@google.com>
    > Commit-Queue: John Shamoon <johnshamoon@google.com>
    
    TBR=geb@google.com,pshickel@google.com,johnshamoon@google.com
    
    Change-Id: I4ddfb6186e3670d577ed90c68a38bd2aac3e8a4d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/590905
    Reviewed-by: Satsuki Ueno <satsukiu@google.com>
    Commit-Queue: Satsuki Ueno <satsukiu@google.com>

--- src/sys/component_manager/tests/reboot_on_terminate/src/integration_test.rs
+++ src/sys/component_manager/tests/reboot_on_terminate/src/integration_test.rs
@@ -13,50 +13,11 @@ use {
     futures::channel::mpsc,
     futures::prelude::*,
     log::*,
-    std::env,
 };
 
 #[fuchsia::test]
 async fn reboot_on_terminate_success() {
     let (send_trigger_called, mut receive_trigger_called) = mpsc::unbounded();
-    let mut realm = build_reboot_on_terminate_realm(send_trigger_called).await;
-
-    // TODO(fxbug.dev/86057): Use a relative URL when the component manager can load the manifest
-    // from its own package.
-    set_component_manager_url(
-        &mut realm,
-        &(env!("REBOOT_ON_TERMINATE_PACKAGE").to_owned() + "#meta/reboot_on_terminate_success.cm"),
-    )
-    .await;
-    let _realm = realm.create().await.unwrap();
-
-    // Wait for the test to signal that it received the shutdown request.
-    info!("waiting for shutdown request");
-    let _ = receive_trigger_called.next().await.expect("failed to receive results");
-}
-
-#[fuchsia::test]
-async fn reboot_on_terminate_policy() {
-    let (send_trigger_called, mut receive_trigger_called) = mpsc::unbounded();
-    let mut realm = build_reboot_on_terminate_realm(send_trigger_called).await;
-
-    // TODO(fxbug.dev/86057): Use a relative URL when the component manager can load the manifest
-    // from its own package.
-    set_component_manager_url(
-        &mut realm,
-        &(env!("REBOOT_ON_TERMINATE_PACKAGE").to_owned() + "#meta/reboot_on_terminate_policy.cm"),
-    )
-    .await;
-    let _realm = realm.create().await.unwrap();
-
-    // Wait for the test to signal that the security policy was correctly applied.
-    info!("waiting for policy error");
-    let _ = receive_trigger_called.next().await.expect("failed to receive results");
-}
-
-async fn build_reboot_on_terminate_realm(
-    send_trigger_called: mpsc::UnboundedSender<()>,
-) -> fuchsia_component_test::Realm {
     let mut builder = RealmBuilder::new().await.unwrap();
 
     // The actual test runs in a nested component manager that is configured with
@@ -121,7 +82,97 @@ async fn build_reboot_on_terminate_realm(
         })
         .unwrap();
 
-    return builder.build();
+    let mut realm = builder.build();
+    set_component_manager_url(
+        &mut realm,
+        "fuchsia-pkg://fuchsia.com/reboot_on_terminate_test#meta/reboot_on_terminate_success.cm",
+    )
+    .await;
+    let _realm = realm.create().await.unwrap();
+
+    // Wait for the test to signal that it received the shutdown request.
+    info!("waiting for shutdown request");
+    let _ = receive_trigger_called.next().await.expect("failed to receive results");
+}
+
+#[fuchsia::test]
+async fn reboot_on_terminate_policy() {
+    let (send_trigger_called, mut receive_trigger_called) = mpsc::unbounded();
+    let mut builder = RealmBuilder::new().await.unwrap();
+
+    // The actual test runs in a nested component manager that is configured with
+    // reboot_on_terminate_enabled.
+    builder
+        .add_eager_component(
+            "component_manager",
+            ComponentSource::url("#meta/component_manager.cm"),
+        )
+        .await
+        .unwrap();
+
+    // The root component will use Trigger to report its shutdown.
+    builder
+        .add_route(CapabilityRoute {
+            capability: Capability::protocol("fidl.test.components.Trigger"),
+            source: RouteEndpoint::component("trigger"),
+            targets: vec![RouteEndpoint::component("component_manager")],
+        })
+        .unwrap();
+    builder
+        .add_component(
+            "trigger",
+            ComponentSource::mock(move |mock_handles| {
+                Box::pin(trigger_mock(send_trigger_called.clone(), mock_handles))
+            }),
+        )
+        .await
+        .unwrap();
+
+    // Forward logging to debug test breakages.
+    builder
+        .add_route(CapabilityRoute {
+            capability: Capability::protocol("fuchsia.logger.LogSink"),
+            source: RouteEndpoint::AboveRoot,
+            targets: vec![RouteEndpoint::component("component_manager")],
+        })
+        .unwrap();
+    builder
+        .add_route(CapabilityRoute {
+            capability: Capability::protocol("fuchsia.boot.WriteOnlyLog"),
+            source: RouteEndpoint::AboveRoot,
+            targets: vec![RouteEndpoint::component("component_manager")],
+        })
+        .unwrap();
+
+    // Forward loader so that nested component_manager can load packages.
+    builder
+        .add_route(CapabilityRoute {
+            capability: Capability::protocol("fuchsia.sys.Loader"),
+            source: RouteEndpoint::AboveRoot,
+            targets: vec![RouteEndpoint::component("component_manager")],
+        })
+        .unwrap();
+
+    // Component manager needs fuchsia.process.Launcher to spawn new processes.
+    builder
+        .add_route(CapabilityRoute {
+            capability: Capability::protocol("fuchsia.process.Launcher"),
+            source: RouteEndpoint::AboveRoot,
+            targets: vec![RouteEndpoint::component("component_manager")],
+        })
+        .unwrap();
+
+    let mut realm = builder.build();
+    set_component_manager_url(
+        &mut realm,
+        "fuchsia-pkg://fuchsia.com/reboot_on_terminate_test#meta/reboot_on_terminate_policy.cm",
+    )
+    .await;
+    let _realm = realm.create().await.unwrap();
+
+    // Wait for the test to signal that the security policy was correctly applied.
+    info!("waiting for policy error");
+    let _ = receive_trigger_called.next().await.expect("failed to receive results");
 }
 
 async fn trigger_mock(
