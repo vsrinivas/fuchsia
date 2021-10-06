02c472b59b4c2c08d689c25d563e8d67a69220bc
Sam Hansen <hansens@google.com>
2021-10-06 00:44:58 +0000

    [scripts] fuchsia-vendored-python update hook
    This adds a hook to create //.jiri_root/bin/fuchsia-vendored-python upon
    jiri update (per RFC-0129). Verified ops using the following script:
    
      #!/usr/bin/env fuchsia-vendored-python
      import sys
    
      if __name__ == '__main__':
        print(sys.executable)
    
    Change-Id: I97c0f381263633a23aa3000d02ddee203a1f7b87
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/590172
    Commit-Queue: Sam Hansen <hansens@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>

--- scripts/devshell/lib/add_symlink_to_bin.sh
+++ scripts/devshell/lib/add_symlink_to_bin.sh
@@ -15,4 +15,7 @@ if [[ -d "${FUCHSIA_DIR}/.jiri_root/bin" ]]; then
 
   rm -f "${FUCHSIA_DIR}/.jiri_root/bin/hermetic-env"
   ln -s "../../scripts/hermetic-env" "${FUCHSIA_DIR}/.jiri_root/bin/hermetic-env"
+
+  rm -f "${FUCHSIA_DIR}/.jiri_root/bin/fuchsia-vendored-python"
+  ln -s "../../scripts/fuchsia-vendored-python" "${FUCHSIA_DIR}/.jiri_root/bin/fuchsia-vendored-python"
 fi
