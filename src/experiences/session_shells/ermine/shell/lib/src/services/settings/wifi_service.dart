6ab9233ae0bed81f33c06a81d74cb1d9f3487a52
Charles Whitten <cwhitten@google.com>
2021-10-08 18:15:50 +0000

    [Ermine][Quicksettings] Add initial network scanning
    This CL introduces network scanning logic to WiFi service. Note that the WiFi service is not added to settings state since the service will need fxr/589051 landed to properly work.
    
    Tested on Atlas ToT with fxr/589051 (sync to patchset 12 to add service to settings state).
    
    Bug: 79885
    
    Change-Id: I4616a2f825efb99e77f9a42cdcce002e5d8d7595
    Reviewed-on: https://fuchsia-review.googlesource.com/c/experiences/+/590423
    Reviewed-by: Yeonhee Lee <yhlee@google.com>
    Reviewed-by: Sanjay Chouksey <sanjayc@google.com>
    Commit-Queue: Charles Whitten <cwhitten@google.com>

--- src/experiences/session_shells/ermine/shell/lib/src/services/settings/wifi_service.dart
+++ src/experiences/session_shells/ermine/shell/lib/src/services/settings/wifi_service.dart
@@ -6,8 +6,10 @@ import 'dart:async';
 
 import 'package:ermine/src/services/settings/task_service.dart';
 import 'package:fidl/fidl.dart' show InterfaceHandle, InterfaceRequest;
+import 'package:fidl_fuchsia_wlan_common/fidl_async.dart';
 import 'package:fidl_fuchsia_wlan_policy/fidl_async.dart';
 import 'package:flutter/foundation.dart';
+import 'package:fuchsia_logger/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:fuchsia_services/services.dart';
 
@@ -18,6 +20,12 @@ class WiFiService implements TaskService {
   ClientProviderProxy? _clientProvider;
   ClientControllerProxy? _clientController;
   late ClientStateUpdatesMonitor _monitor;
+  StreamSubscription? _scanForNetworksSubscription;
+  ScanResultIteratorProxy? _scanResultIteratorProvider;
+
+  Timer? _timer;
+  int scanIntervalInSeconds = 20;
+  late List<ScanResult> _scannedNetworks;
 
   WiFiService();
 
@@ -28,15 +36,25 @@ class WiFiService implements TaskService {
     _monitor = ClientStateUpdatesMonitor();
 
     Incoming.fromSvcPath().connectToService(_clientProvider);
-    Incoming.fromSvcPath().connectToService(_clientController);
 
     await _clientProvider?.getController(
         InterfaceRequest(_clientController?.ctrl.request().passChannel()),
         _monitor.getInterfaceHandle());
+
+    final requestStatus = await _clientController?.startClientConnections();
+    if (requestStatus != RequestStatus.acknowledged) {
+      log.warning(
+          'Failed to start wlan client connection. Request status: $requestStatus');
+    }
+
+    _timer = Timer.periodic(
+        Duration(seconds: scanIntervalInSeconds), (_) => scanForNetworks());
   }
 
   @override
   Future<void> stop() async {
+    _timer?.cancel();
+    await _scanForNetworksSubscription?.cancel();
     dispose();
   }
 
@@ -46,16 +64,46 @@ class WiFiService implements TaskService {
     _clientProvider = ClientProviderProxy();
     _clientController?.ctrl.close();
     _clientController = ClientControllerProxy();
+    _scanResultIteratorProvider?.ctrl.close();
+    _scanResultIteratorProvider = ScanResultIteratorProxy();
   }
+
+  Future<void> scanForNetworks() async {
+    _scanForNetworksSubscription = () async {
+      _scanResultIteratorProvider = ScanResultIteratorProxy();
+      await _clientController?.scanForNetworks(InterfaceRequest(
+          _scanResultIteratorProvider?.ctrl.request().passChannel()));
+      List<ScanResult> aggregateScanResults = [];
+      List<ScanResult>? scanResults;
+      try {
+        scanResults = await _scanResultIteratorProvider?.getNext();
+        while (scanResults != null && scanResults.isNotEmpty) {
+          aggregateScanResults = aggregateScanResults + scanResults;
+          scanResults = await _scanResultIteratorProvider?.getNext();
+        }
+      } on Exception catch (e) {
+        log.warning('Error encountered during scan: $e');
+      }
+      _scannedNetworks = aggregateScanResults.toSet().toList();
+    }()
+        .asStream()
+        .listen((_) {});
+  }
+
+  List<String?> get scannedNetworks =>
+      _scannedNetworks.map((network) => network.id?.ssid.toString()).toList();
 }
 
 class ClientStateUpdatesMonitor extends ClientStateUpdates {
   final _binding = ClientStateUpdatesBinding();
+  ClientStateSummary? _summary;
 
   ClientStateUpdatesMonitor();
 
   InterfaceHandle<ClientStateUpdates> getInterfaceHandle() =>
       _binding.wrap(this);
+
+  ClientStateSummary? getState() => _summary;
 
   @override
   Future<void> onClientStateUpdate(ClientStateSummary summary) async {}
