vsrinivas/fuchsia

commit b6148d6435623cd6c16697bf693e31c2c3e76071

[fxfs] Document why the key ID must currently be 0
Also some cleanup.

Change-Id: Ia6e1967ace58226025cce56bf65ee653938e4ac2
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/592680
Reviewed-by: Chris Suter <csuter@google.com>
Reviewed-by: James Sullivan <jfsulliv@google.com>
API-Review: Chris Suter <csuter@google.com>
Commit-Queue: Chris Palmer <palmer@google.com>

src/storage/fxfs-crypt/src/main.rs
@@ -12,7 +12,7 @@ use {
         stream::{StreamExt, TryStreamExt},
         TryFutureExt,
     },
-    rand::RngCore,
+    rand::thread_rng,
 };
 
 enum Services {
@@ -27,8 +27,11 @@ async fn handle_request(stream: Services) -> Result<(), Error> {
             while let Some(request) = stream.try_next().await.context("Reading request")? {
                 match request {
                     CryptRequest::CreateKey { wrapping_key_id, owner, responder } => {
-                        assert_eq!(wrapping_key_id, 0);
-                        let mut rng = rand::thread_rng();
+                        assert_eq!(
+                            wrapping_key_id, 0,
+                            "Support for multiple key IDs not implemented yet. Key ID must be 0."
+                        );
+                        let mut rng = thread_rng();
                         let mut key = [0; 32];
                         rng.fill_bytes(&mut key);
                         let mut wrapped = [0; 32];
@@ -43,7 +46,10 @@ async fn handle_request(stream: Services) -> Result<(), Error> {
                         });
                     }
                     CryptRequest::UnwrapKeys { wrapping_key_id, owner, keys, responder } => {
-                        assert_eq!(wrapping_key_id, 0);
+                        assert_eq!(
+                            wrapping_key_id, 0,
+                            "Support for multiple key IDs not implemented yet. Key ID must be 0."
+                        );
                         let mut unwrapped_keys = Vec::new();
                         for key in keys {
                             let mut unwrapped = [0; 32];

src/storage/fxfs/fidl/fxfs.fidl
@@ -10,7 +10,7 @@ using zx;
 @discoverable
 protocol Crypt {
     /// Creates a new key wrapped with the key identified by `wrapping_key_id`.  `owner` identifies
-    /// the owner of the key and must be suppled to UnwrapKeys.
+    /// the owner of the key and must be supplied to `UnwrapKeys`.
     CreateKey(struct {
         wrapping_key_id uint64;
         owner uint64;
@@ -20,7 +20,7 @@ protocol Crypt {
     }) error zx.status;
 
     /// Unwraps keys that are wrapped by the key identified by `wrapping_key_id`.  `owner` must be
-    /// the same as that passed to CreateKey.  This can fail due to permission reasons, but an
+    /// the same as that passed to `CreateKey`.  This can fail due to permission reasons, but an
     /// incorrect key or owner will not fail; it will just return an unwrapped key that won't
     /// actually decrpyt the data.
     UnwrapKeys(struct {

commit 0e0f97e6eff68a4a4211bf2903437823343ce40c

[Fxfs] Fix use statement.
Previous commit b6148d6435623cd6c16697bf693e31c2c3e76071 broke the build.

Change-Id: Ie8219249bf9f86ed557f00582576b14fe43799e1
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/593661
Reviewed-by: Chris Suter <csuter@google.com>
Commit-Queue: Chris Palmer <palmer@google.com>

src/storage/fxfs-crypt/src/main.rs
@@ -12,7 +12,7 @@ use {
         stream::{StreamExt, TryStreamExt},
         TryFutureExt,
     },
-    rand::thread_rng,
+    rand::RngCore,
 };
 
 enum Services {
@@ -31,7 +31,7 @@ async fn handle_request(stream: Services) -> Result<(), Error> {
                             wrapping_key_id, 0,
                             "Support for multiple key IDs not implemented yet. Key ID must be 0."
                         );
-                        let mut rng = thread_rng();
+                        let mut rng = rand::thread_rng();
                         let mut key = [0; 32];
                         rng.fill_bytes(&mut key);
                         let mut wrapped = [0; 32];