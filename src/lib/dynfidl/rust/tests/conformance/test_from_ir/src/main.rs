c0fb036c74a7f8206362332cface08555c73cc65
Adam Perry <adamperry@google.com>
2021-10-15 23:02:01 +0000

    [dynfidl] Temporarily allow dead code.
    Unblocks a Rust toolchain roll. Tested that it builds locally in case
    we want to land it quickly.
    
    Bug: 77068
    Change-Id: I13a8ef02dd797320b271d126824a423416ba0a12
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/594993
    Commit-Queue: Adam Perry <adamperry@google.com>
    Commit-Queue: Tyler Mandry <tmandry@google.com>
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Reviewed-by: Tyler Mandry <tmandry@google.com>

--- src/lib/dynfidl/rust/tests/conformance/test_from_ir/src/main.rs
+++ src/lib/dynfidl/rust/tests/conformance/test_from_ir/src/main.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![allow(dead_code)] // TODO(https://fxbug.dev/77068) remove after toolchain rolls again
+
 use argh::FromArgs;
 use heck::SnakeCase;
 use proc_macro2::TokenStream;
