eee4a983a1b16ab3b4ff5d46dca7b216d1fb5d57
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2021-10-15 04:22:53 +0000

    Reland "[selectors] Parse inclusion operators in metadata selectors"
    This is a reland of 14fc44e60b0ed77a613bfb79271e61164c4efb4f
    
    Fixed: in the random mutations that the test does, `HAS any`, `has ANY`, `HAS all` and `has ALL`
    were being accepted and they shouldn't have been.
    
    Multiply: selectors-test
    
    Original change's description:
    > [selectors] Parse inclusion operators in metadata selectors
    >
    > Bug: 55118
    > Tested: added tests
    >
    > Change-Id: I7fbba200a8d6837a9a1e14ce02120a0e8976d100
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/592709
    > Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    > Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    > Reviewed-by: Christopher Johnson <crjohns@google.com>
    
    Bug: 55118
    Change-Id: I76b78111711ea9ecc486f8b937716fede5970e0f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/593783
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>

--- src/lib/diagnostics/selectors/src/parser.rs
+++ src/lib/diagnostics/selectors/src/parser.rs
@@ -8,47 +8,87 @@
 use nom::{
     branch::alt,
     bytes::complete::{is_not, tag},
-    character::complete::{char, digit1, hex_digit1, multispace0},
+    character::complete::{char, digit1, hex_digit1, multispace0, multispace1},
     combinator::{map, recognize},
     error::{make_error, ErrorKind, ParseError},
     multi::{many0, separated_nonempty_list},
-    sequence::{delimited, preceded},
+    sequence::{delimited, preceded, tuple},
     IResult,
 };
 
-/// This macro generates:
-/// - The `enum Operator` containing the given `$variant_name`s as the variants.
-/// - A parser function `$name` that consumes an input of value `$symbol` and returns an
-///   `Operator::$variant_name`.
-/// - A function `operator` that parses an input into an `Operator`.
-macro_rules! operator {
-    ($($symbol:literal => $variant_name:ident => $name:ident),+) => {
-        #[derive(Debug, Eq, PartialEq)]
-        pub enum Operator {
-            $($variant_name),+
-        }
+#[derive(Debug, Eq, PartialEq)]
+pub enum ComparisonOperator {
+    Equal,
+    GreaterEq,
+    Greater,
+    LessEq,
+    Less,
+    NotEq,
+}
 
-        $(
-            fn $name(input: &str) -> IResult<&str, Operator> {
-                map(tag($symbol), move |_| Operator::$variant_name)(input)
-            }
-        )+
-
-        fn operator(input: &str) -> IResult<&str, Operator> {
-            alt(($( $name ),+))(input)
-        }
+macro_rules! comparison {
+    ($tag:literal, $variant:ident) => {
+        map(tag($tag), move |_| ComparisonOperator::$variant)
     };
 }
 
-operator!(
-   "=" => Equal => equal,
-   ">=" => GreaterEq => greater_eq,
-   ">" => Greater => greater,
-   "<=" => LessEq => less_eq,
-   "<" => Less => less,
-   "!=" => NotEq => not_eq
-);
-//Maybe include IN, HAS above.
+/// Parses comparison operators.
+fn comparison(input: &str) -> IResult<&str, ComparisonOperator> {
+    alt((
+        comparison!("=", Equal),
+        comparison!(">=", GreaterEq),
+        comparison!(">", Greater),
+        comparison!("<=", LessEq),
+        comparison!("<", Less),
+        comparison!("!=", NotEq),
+    ))(input)
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum InclusionOperator {
+    HasAny,
+    HasAll,
+    In,
+}
+
+fn has_any(input: &str) -> IResult<&str, (&str, &str)> {
+    let (rest, (has, _, any)) = alt((
+        tuple((tag("has"), multispace1, tag("any"))),
+        tuple((tag("HAS"), multispace1, tag("ANY"))),
+    ))(input)?;
+    Ok((rest, (has, any)))
+}
+
+fn has_all(input: &str) -> IResult<&str, (&str, &str)> {
+    let (rest, (has, _, all)) = alt((
+        tuple((tag("has"), multispace1, tag("all"))),
+        tuple((tag("HAS"), multispace1, tag("ALL"))),
+    ))(input)?;
+    Ok((rest, (has, all)))
+}
+
+/// Parses inclusion operators.
+fn inclusion(input: &str) -> IResult<&str, InclusionOperator> {
+    alt((
+        map(has_any, move |_| InclusionOperator::HasAny),
+        map(has_all, move |_| InclusionOperator::HasAll),
+        map(alt((tag("in"), tag("IN"))), move |_| InclusionOperator::In),
+    ))(input)
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Operator {
+    Inclusion(InclusionOperator),
+    Comparison(ComparisonOperator),
+}
+
+/// Parses any operator.
+fn operator(input: &str) -> IResult<&str, Operator> {
+    alt((
+        map(inclusion, move |o| Operator::Inclusion(o)),
+        map(comparison, move |o| Operator::Comparison(o)),
+    ))(input)
+}
 
 /// This macro generates:
 /// - The `enum $ty` containing the given `$variant_name`s as the variants.
@@ -244,10 +284,8 @@ fn list_of_values(input: &str) -> IResult<&str, Vec<Value<'_>>> {
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Operation<'a> {
-    Comparison(Operator, Value<'a>),
-    // TODO(fxbug.dev/55118): HasOne(Value),
-    // TODO(fxbug.dev/55118): HasAny(Vec<Value>),
-    // TODO(fxbug.dev/55118): In(Vec<Value>),
+    Comparison(ComparisonOperator, Value<'a>),
+    Inclusion(InclusionOperator, Vec<Value<'a>>),
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -256,18 +294,42 @@ pub struct FilterExpression<'a> {
     pub op: Operation<'a>,
 }
 
+enum OneOrMany<T> {
+    One(T),
+    Many(Vec<T>),
+}
+
 fn filter_expression(input: &str) -> IResult<&str, FilterExpression<'_>> {
     // TODO(fxbug.dev/55118): validate that identifiers are used with valid operators. Consider if
     // this has to be done at parse time or we can defer it for a follow-up validation step.
     let (rest, identifier) = spaced(identifier)(input)?;
-    let (rest, operator) = spaced(operator)(rest)?;
-    // TODO(fxbug.dev/55118): support the rest of the operations (In, Has).
+    let (rest_after_op, op) = spaced(operator)(rest)?;
     let (rest, value) = spaced(alt((
-        map(number, Value::Number),
-        map(severity_sym, |s| Value::Severity(s)),
-        map(string_literal, |s| Value::StringLiteral(s)),
-    )))(rest)?;
-    Ok((rest, FilterExpression { identifier, op: Operation::Comparison(operator, value) }))
+        map(number, move |n| OneOrMany::One(Value::Number(n))),
+        map(severity_sym, move |s| OneOrMany::One(Value::Severity(s))),
+        map(string_literal, move |s| OneOrMany::One(Value::StringLiteral(s))),
+        map(list_of_values, OneOrMany::Many),
+    )))(rest_after_op)?;
+    match (op, value) {
+        (Operator::Inclusion(op), OneOrMany::Many(values)) => {
+            Ok((rest, FilterExpression { identifier, op: Operation::Inclusion(op, values) }))
+        }
+        (Operator::Inclusion(o @ InclusionOperator::HasAny), OneOrMany::One(value)) => {
+            Ok((rest, FilterExpression { identifier, op: Operation::Inclusion(o, vec![value]) }))
+        }
+        (Operator::Inclusion(o @ InclusionOperator::HasAll), OneOrMany::One(value)) => {
+            Ok((rest, FilterExpression { identifier, op: Operation::Inclusion(o, vec![value]) }))
+        }
+        (Operator::Comparison(op), OneOrMany::One(value)) => {
+            Ok((rest, FilterExpression { identifier, op: Operation::Comparison(op, value) }))
+        }
+        (Operator::Inclusion(InclusionOperator::In), OneOrMany::One(_))
+        | (Operator::Comparison(_), OneOrMany::Many(_)) => {
+            // TODO(fxbug.dev/55118): better custom errors. This error should be explicit that a
+            // comparison operator can't be used together with a list.
+            Err(nom::Err::Failure(make_error(rest_after_op, ErrorKind::Not)))
+        }
+    }
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -304,13 +366,11 @@ mod tests {
     macro_rules! test_sym_parser {
         (
             parser: $parser:ident,
-            accepts: [$([$($accepted:literal),+] => $expected:expr),+],
-            rejects: [$($rejected:literal),*]
+            accepts: [$([$($accepted:literal),+] => $expected:expr),+]
         ) => {{
             // Verify we can parse all accepted values.
             $($(assert_eq!(Ok(("", $expected)), $parser($accepted));)+)+
             // Verify we reject all rejected values.
-            $(assert!($parser($rejected).is_err());)*
             assert!($parser("").is_err());
 
             // Verify we fail to parse any of the accepted values with random case swaps.
@@ -339,7 +399,9 @@ mod tests {
                     if accepted_values.contains(&&*rejected_value) {
                         continue;
                     }
-                    assert!($parser(&rejected_value).is_err());
+                    assert!(
+                        $parser(&rejected_value).is_err(),
+                        "{} should be rejected by {}", rejected_value, stringify!($operator));
                     break;
                 }
             }
@@ -353,16 +415,18 @@ mod tests {
             accepts: [
                 [ "filename", "Filename", "FILENAME" ] => Identifier::Filename,
                 [
-                    "lifecycle_event_type", "LifecycleEventType", "LIFECYCLE_EVENT_TYPE"
+                    "lifecycle_event_type", "LifecycleEventType", "LIFECYCLE_EVENT_TYPE",
+                    "lifecycleEventType"
                 ] => Identifier::LifecycleEventType,
-                [ "line_number", "LineNumber", "LINE_NUMBER" ] => Identifier::LineNumber,
+                [
+                    "line_number", "LineNumber", "LINE_NUMBER", "lineNumber"
+                ] => Identifier::LineNumber,
                 [ "pid", "Pid", "PID" ] => Identifier::Pid,
                 [ "severity", "Severity", "SEVERITY" ] => Identifier::Severity,
                 [ "tags", "Tags", "TAGS" ] => Identifier::Tags,
                 [ "tid", "Tid", "TID" ] => Identifier::Tid,
                 [ "timestamp", "Timestamp", "TIMESTAMP" ] => Identifier::Timestamp
-            ],
-            rejects: []
+            ]
         }
     }
 
@@ -371,14 +435,16 @@ mod tests {
         test_sym_parser! {
             parser: operator,
             accepts: [
-                [ "=" ] => Operator::Equal,
-                [ ">" ] => Operator::Greater,
-                [ ">=" ] => Operator::GreaterEq,
-                [ "<" ] => Operator::Less,
-                [ "<=" ] => Operator::LessEq,
-                [ "!=" ] => Operator::NotEq
-            ],
-            rejects: [ "in", "IN", "In", "has", "Has", "HAS" ]
+                [ "=" ] => Operator::Comparison(ComparisonOperator::Equal),
+                [ ">" ] => Operator::Comparison(ComparisonOperator::Greater),
+                [ ">=" ] => Operator::Comparison(ComparisonOperator::GreaterEq),
+                [ "<" ] => Operator::Comparison(ComparisonOperator::Less),
+                [ "<=" ] => Operator::Comparison(ComparisonOperator::LessEq),
+                [ "!=" ] => Operator::Comparison(ComparisonOperator::NotEq),
+                [ "in", "IN" ] => Operator::Inclusion(InclusionOperator::In),
+                [ "has any", "HAS ANY" ] => Operator::Inclusion(InclusionOperator::HasAny),
+                [ "has all", "HAS ALL" ] => Operator::Inclusion(InclusionOperator::HasAll)
+            ]
         }
     }
 
@@ -392,8 +458,7 @@ mod tests {
                 [ "info", "INFO", "Info" ] => Severity::Info,
                 [ "warn", "WARN", "Warn" ] => Severity::Warn,
                 [ "error", "ERROR", "Error" ] => Severity::Error
-            ],
-            rejects: []
+            ]
         }
     }
 
@@ -465,19 +530,52 @@ mod tests {
     fn parse_filter_expression() {
         let expected = FilterExpression {
             identifier: Identifier::Pid,
-            op: Operation::Comparison(Operator::Equal, Value::Number(123)),
+            op: Operation::Comparison(ComparisonOperator::Equal, Value::Number(123)),
         };
         assert_eq!(Ok(("", expected)), filter_expression("pid = 123"));
 
         let expected = FilterExpression {
             identifier: Identifier::Severity,
-            op: Operation::Comparison(Operator::GreaterEq, Value::Severity(Severity::Info)),
+            op: Operation::Comparison(
+                ComparisonOperator::GreaterEq,
+                Value::Severity(Severity::Info),
+            ),
         };
         assert_eq!(Ok(("", expected)), filter_expression("severity>=info"));
 
         // All three operands are required
         assert!(filter_expression("tid >").is_err());
         assert!(filter_expression("!= 3").is_err());
+
+        // The inclusion operator HAS can be used with lists and single values.
+        let expected = FilterExpression {
+            identifier: Identifier::Tags,
+            op: Operation::Inclusion(
+                InclusionOperator::HasAny,
+                vec![Value::StringLiteral("foo"), Value::StringLiteral("bar")],
+            ),
+        };
+        assert_eq!(Ok(("", expected)), filter_expression("tags HAS ANY [\"foo\", \"bar\"]"));
+
+        let expected = FilterExpression {
+            identifier: Identifier::Tags,
+            op: Operation::Inclusion(InclusionOperator::HasAny, vec![Value::StringLiteral("foo")]),
+        };
+        assert_eq!(Ok(("", expected)), filter_expression("tags has any \"foo\""));
+
+        // The inclusion operator IN can only be used with lists.
+        let expected = FilterExpression {
+            identifier: Identifier::LifecycleEventType,
+            op: Operation::Inclusion(
+                InclusionOperator::In,
+                vec![Value::StringLiteral("started"), Value::StringLiteral("stopped")],
+            ),
+        };
+        assert_eq!(
+            Ok(("", expected)),
+            filter_expression("lifecycle_event_type in [\"started\", \"stopped\"]")
+        );
+        assert!(filter_expression("pid in 123").is_err());
     }
 
     #[test]
@@ -485,26 +583,42 @@ mod tests {
         let expected = MetadataSelector(vec![
             FilterExpression {
                 identifier: Identifier::LineNumber,
-                op: Operation::Comparison(Operator::Equal, Value::Number(10)),
+                op: Operation::Comparison(ComparisonOperator::Equal, Value::Number(10)),
             },
             FilterExpression {
                 identifier: Identifier::Filename,
-                op: Operation::Comparison(Operator::Equal, Value::StringLiteral("foo.rs")),
+                op: Operation::Inclusion(
+                    InclusionOperator::In,
+                    vec![Value::StringLiteral("foo.rs")],
+                ),
             },
             FilterExpression {
                 identifier: Identifier::Severity,
-                op: Operation::Comparison(Operator::LessEq, Value::Severity(Severity::Error)),
+                op: Operation::Comparison(
+                    ComparisonOperator::LessEq,
+                    Value::Severity(Severity::Error),
+                ),
+            },
+            FilterExpression {
+                identifier: Identifier::Tags,
+                op: Operation::Inclusion(
+                    InclusionOperator::HasAll,
+                    vec![Value::StringLiteral("foo"), Value::StringLiteral("bar")],
+                ),
             },
         ]);
         assert_eq!(
             Ok(("", expected)),
-            metadata_selector("where line_number = 10, filename = \"foo.rs\", severity <= ERROR")
+            metadata_selector(
+                "where line_number = 10, filename in [\"foo.rs\"],
+                 severity <= ERROR, tags HAS ALL [\"foo\", \"bar\"]"
+            )
         );
 
         // Requires >= 1 filters.
         let expected = MetadataSelector(vec![FilterExpression {
             identifier: Identifier::Timestamp,
-            op: Operation::Comparison(Operator::Greater, Value::Number(123)),
+            op: Operation::Comparison(ComparisonOperator::Greater, Value::Number(123)),
         }]);
         assert_eq!(Ok(("", expected)), metadata_selector("WHERE timestamp > 123"));
         assert!(metadata_selector("where").is_err());
