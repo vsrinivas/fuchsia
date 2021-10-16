a95de0ee567d22651c70fd976d1decb8f3017a70
Jody Sankey <jsankey@google.com>
2021-10-16 02:36:27 +0000

    [examples] Add a new example component illustrating storage.
    This component maintains a single file whose contents increment each
    time the component is launched. The component logs helpful messages
    to explain any failures that are encountered.
    
    This component will be useful for verifying that dynamically
    initialized storage capabilities are working correctly during the
    early stages of the user data protection program.
    
    Bug: 85555
    Change-Id: I403036539accdbe8ec737e35c81988484108b8e3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/594969
    Commit-Queue: Jody Sankey <jsankey@google.com>
    Reviewed-by: Yaneury Fermin <yaneury@google.com>

---
+++ examples/components/storage/README.md
@@ -0,0 +1,10 @@
+# Storage Example
+
+This directory contains a simple example component that uses isolated persistent
+storage to increment a count of the number of times it has been initialized.
+
+The counter value read from the file and written to file are logged and exposed
+via inspect.
+
+This component may be used to verify that a storage capability is provisioned
+correctly.
---
+++ examples/components/storage/src/main.rs
@@ -0,0 +1,120 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{anyhow, Error},
+    byteorder::{BigEndian, ByteOrder, WriteBytesExt},
+    lazy_static::lazy_static,
+    std::{
+        fs,
+        io::Read,
+        path::{Path, PathBuf},
+    },
+    tracing::{info, warn},
+};
+
+const U64_SIZE: usize = (u64::BITS / 8) as usize;
+const DEFAULT_VALUE: u64 = 1;
+
+lazy_static! {
+    static ref FILE_PATH: PathBuf = PathBuf::from("/data/counter");
+}
+
+#[fuchsia::component(logging = true)]
+async fn main() -> Result<(), Error> {
+    process_file(&FILE_PATH);
+    Ok(())
+}
+
+/// Attempts to read a single u64 from the supplied path then write an incremented value back into
+/// the same path. If any errors are encountered during reading the write will use a default
+/// value.
+fn process_file(path: &Path) {
+    // TODO(jsankey): Record these outcomes in inspect.
+    let updated_value = match read_file(path) {
+        Ok(value) => {
+            info!("Successfully read a value of: {}", value);
+            value.wrapping_add(1)
+        }
+        Err(err) => {
+            warn!("Error reading previous value: {:?}", err);
+            DEFAULT_VALUE
+        }
+    };
+
+    match write_file(path, updated_value) {
+        Ok(()) => info!("Successfully wrote a value of: {}", updated_value),
+        Err(err) => warn!("Error writing updated value: {}", err),
+    }
+}
+
+/// Reads a single u64 from the supplied path, returning an error if the file does not exists or is
+/// not exactly the right length.
+fn read_file(path: &Path) -> Result<u64, Error> {
+    let mut buf = Vec::<u8>::new();
+    let mut file = fs::File::open(path).map_err(|err| anyhow!("Error opening file: {:?}", err))?;
+    let size =
+        file.read_to_end(&mut buf).map_err(|err| anyhow!("Error reading file: {:?}", err))?;
+    match size {
+        U64_SIZE => Ok(BigEndian::read_u64(&buf)),
+        _ => Err(anyhow!("File length invalid: {}", size)),
+    }
+}
+
+/// Writes the supplied u64 into the supplied path, returning any errors that are encountered.
+fn write_file(path: &Path, value: u64) -> Result<(), Error> {
+    let mut file =
+        fs::File::create(path).map_err(|err| anyhow!("Error creating file: {:?}", err))?;
+    file.write_u64::<BigEndian>(value).map_err(|err| anyhow!("Error writing value: {:?}", err))
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, tempfile::TempDir, test_case::test_case};
+
+    fn make_path() -> (TempDir, PathBuf) {
+        let dir = TempDir::new().expect("error creating tempdir");
+        let path = dir.path().join("testfile");
+        (dir, path)
+    }
+
+    fn make_file(data: Vec<u8>) -> (TempDir, PathBuf) {
+        let (dir, path) = make_path();
+        fs::write(&path, &data).expect("error writing file");
+        (dir, path)
+    }
+
+    #[test_case(vec![0, 0, 0, 0, 1, 2, 3, 4], Some(0x01020304); "valid")]
+    #[test_case(vec![0, 0, 0, 0, 1, 2, 3], None; "too short")]
+    #[test_case(vec![0, 0, 0, 0, 1, 2, 3, 4, 5], None; "too long")]
+    fn test_read_existing_file(content: Vec<u8>, expected: Option<u64>) {
+        let (_tempdir, path) = make_file(content);
+        match expected {
+            Some(value) => assert_eq!(read_file(&path).unwrap(), value),
+            None => assert!(read_file(&path).is_err(), "read should fail"),
+        }
+    }
+
+    #[test]
+    fn test_read_missing_file() {
+        let (_tempdir, path) = make_path();
+        assert!(read_file(&path).is_err(), "read should fail");
+    }
+
+    #[test_case(vec![0, 0, 0, 0, 1, 2, 3, 4], 0x01020305; "valid")]
+    #[test_case(vec![255, 255, 255, 255, 255, 255, 255, 255], 0x0; "rollover")]
+    #[test_case(vec![0, 0, 0, 0, 1, 2], DEFAULT_VALUE; "content too short")]
+    fn test_process_existing_file(content: Vec<u8>, expected: u64) {
+        let (_tempdir, path) = make_file(content);
+        process_file(&path);
+        assert_eq!(read_file(&path).unwrap(), expected);
+    }
+
+    #[test]
+    fn test_process_missing_file() {
+        let (_tempdir, path) = make_path();
+        process_file(&path);
+        assert_eq!(read_file(&path).unwrap(), DEFAULT_VALUE);
+    }
+}
