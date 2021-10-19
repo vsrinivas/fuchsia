f74453dcd07fd6166f9226e5ce3c3eeb85485092
Ina Huh <ihuh@google.com>
2021-10-19 18:54:07 +0000

    Revert "[metrics_buffer] Move CodecMetrics to MetricsBuffer"
    This reverts commit 837acacd67ba5de7fa68ddb0e97db78f35862b29.
    
    Reason for revert: metrics_buffer_test is failing in CI
    
    Original change's description:
    > [metrics_buffer] Move CodecMetrics to MetricsBuffer
    >
    > Generalize CodecMetrics and make it a lib called MetricsBuffer.
    > This way MetricsBuffer can be used by both the HW video decoder
    > and sysmem.
    >
    > Bug: fxbug.dev/86491
    > Tested: MetricsBufferTest (many iterations)
    > Change-Id: I1d855eb6e0b696a0749918625a6ffd01c00ba015
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/595121
    > Commit-Queue: Dustin Green <dustingreen@google.com>
    > Reviewed-by: Abdulla Kamar <abdulla@google.com>
    > Reviewed-by: Austin Foxley <afoxley@google.com>
    
    TBR=abdulla@google.com,dustingreen@google.com,afoxley@google.com
    
    Change-Id: I38a87e1d5779ba136e1837b70c11bba7afd2b007
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: fxbug.dev/86491
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/595809
    Reviewed-by: Ina Huh <ihuh@google.com>
    Commit-Queue: Ina Huh <ihuh@google.com>

--- src/media/lib/codec_impl/include/lib/media/codec_impl/codec_metrics.h
+++ src/media/lib/codec_impl/include/lib/media/codec_impl/codec_metrics.h
@@ -14,30 +14,27 @@
 
 #include <src/lib/cobalt/cpp/cobalt_event_builder.h>
 #include <src/lib/cobalt/cpp/cobalt_logger.h>
-#include <src/lib/metrics_buffer/metrics_buffer.h>
 
 #include <src/media/lib/metrics/metrics.cb.h>
 
 // Methods of this class can be called on any thread.
-//
-// TODO(fxb/86491): This wrapper is temporary to minimize files with diffs in a CL for fxb/86491.
-// We can switch to using MetricsBuffer/MetricBuffer directly in a later CL.
 class CodecMetrics final {
  public:
   // A nop instance so unit tests don't need to wire up cobalt.
-  CodecMetrics();
+  CodecMetrics() __TA_EXCLUDES(lock_);
 
   // !service_directory is ok.  If !service_directory, the instance will be a nop instance until
   // SetServiceDirectory() is called.
-  CodecMetrics(std::shared_ptr<sys::ServiceDirectory> service_directory);
+  CodecMetrics(std::shared_ptr<sys::ServiceDirectory> service_directory) __TA_EXCLUDES(lock_);
 
-  ~CodecMetrics();
+  ~CodecMetrics() __TA_EXCLUDES(lock_);
 
   // Set the ServiceDirectory from which to get fuchsia.cobalt.LoggerFactory.  This can be nullptr.
   // This can be called again, regardless of whether there was already a previous ServiceDirectory.
   // Previously-queued events may be lost (especially recently-queued events) when switching to a
   // new ServiceDirectory.
-  void SetServiceDirectory(std::shared_ptr<sys::ServiceDirectory> service_directory);
+  void SetServiceDirectory(std::shared_ptr<sys::ServiceDirectory> service_directory)
+      __TA_EXCLUDES(lock_);
 
   // Log the event as EVENT_COUNT, with period_duration_micros 0, possibly aggregating with any
   // other calls to this method with the same component and event wihtin a short duration to limit
@@ -47,11 +44,53 @@ class CodecMetrics final {
   // isn't expected to unbind very often, if ever, and if we're suspending already then it's
   // unlikely that the pending cobalt events would be persisted anyway.
   void LogEvent(media_metrics::StreamProcessorEvents2MetricDimensionImplementation implementation,
-                media_metrics::StreamProcessorEvents2MetricDimensionEvent event);
+                media_metrics::StreamProcessorEvents2MetricDimensionEvent event)
+      __TA_EXCLUDES(lock_);
 
  private:
-  std::shared_ptr<cobalt::MetricsBuffer> metrics_buffer_;
-  cobalt::MetricBuffer metric_buffer_;
+  class PendingCountsKey {
+   public:
+    PendingCountsKey(
+        media_metrics::StreamProcessorEvents2MetricDimensionImplementation implementation,
+        media_metrics::StreamProcessorEvents2MetricDimensionEvent event);
+
+    media_metrics::StreamProcessorEvents2MetricDimensionImplementation implementation() const;
+    media_metrics::StreamProcessorEvents2MetricDimensionEvent event() const;
+
+   private:
+    media_metrics::StreamProcessorEvents2MetricDimensionImplementation implementation_;
+    media_metrics::StreamProcessorEvents2MetricDimensionEvent event_;
+  };
+  struct PendingCountsKeyHash {
+    size_t operator()(const PendingCountsKey& key) const noexcept;
+  };
+  struct PendingCountsKeyEqual {
+    bool operator()(const PendingCountsKey& lhs, const PendingCountsKey& rhs) const noexcept;
+  };
+
+  void TryPostFlushCountsLocked() __TA_REQUIRES(lock_);
+  void FlushPendingEventCounts() __TA_EXCLUDES(lock_);
+
+  static constexpr zx::duration kMinLoggingPeriod = zx::sec(5);
+
+  std::mutex lock_;
+
+  // We have a separate async::Loop for each instance of cobalt::CobaltLogger, because CobaltLogger
+  // requires that no async tasks posted by CobaltLogger out-live the CobaltLogger.  The easiest way
+  // to achieve that is to give CobaltLogger its own async::Loop and Quit(), JoinThreads(),
+  // Shutdown() that async::Loop before destroying the CobaltLogger.
+  std::unique_ptr<async::Loop> loop_ = nullptr;
+
+  std::unique_ptr<cobalt::CobaltLogger> cobalt_logger_ __TA_GUARDED(lock_);
+
+  // Don't bother with std::optional<> here - instead subtract kMinLoggingPeriod so we'll log
+  // immediately the first time.
+  zx::time last_flushed_ __TA_GUARDED(lock_) = zx::clock::get_monotonic() - kMinLoggingPeriod;
+
+  // From component and event to event count.
+  using PendingCounts =
+      std::unordered_map<PendingCountsKey, int64_t, PendingCountsKeyHash, PendingCountsKeyEqual>;
+  PendingCounts pending_counts_ __TA_GUARDED(lock_);
 };
 
 #endif  // SRC_MEDIA_LIB_CODEC_IMPL_INCLUDE_LIB_MEDIA_CODEC_IMPL_CODEC_METRICS_H_
