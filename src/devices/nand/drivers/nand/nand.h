c6195876d1c07bd7da0e5f51cfdee8c37107db0f
Martin Lindsay <mlindsay@google.com>
2021-10-20 21:31:26 +0000

    Revert "[nand] Add a read cache for poorly performing reads."
    This reverts commit b2aab12c76f7d2141449b91796b43c752de880e0.
    
    Reason for revert: Failures seen in CQ.
    EQ: https://logs.chromium.org/logs/turquoise/buildbucket/cr-buildbucket/8832824905772017457/+/u/failures/Astro-_2_/attempt_0__fail_/serial_log.txt
    
    Original change's description:
    > [nand] Add a read cache for poorly performing reads.
    >
    > Adds a small simple FIFO-evicting read cache. The cache should not need
    > to be very large as it really is intended to re-serve entries until they
    > can be copied into a newer place for more reliable readback. The small
    > size allows for a simple implementation without need to optimize
    > retrieval.
    >
    > Pages are added to the cache after a successful read that saw either
    > previously failed reads or reads with >= half of the correctable bits
    > flipped. Pages are evicted from the cache whenever their block is
    > erased.
    >
    > Bug: 85915
    >
    > Change-Id: Iec7cff8ac6440e9ac5548bf8529f47a9ce536934
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/595904
    > Commit-Queue: Martin Lindsay <mlindsay@google.com>
    > Reviewed-by: Chris Suter <csuter@google.com>
    
    TBR=csuter@google.com,mlindsay@google.com
    
    Change-Id: Ica1f2429c4eed4ca45590b197b7fd91383009dcb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 85915
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/596603
    Reviewed-by: Martin Lindsay <mlindsay@google.com>
    Commit-Queue: Martin Lindsay <mlindsay@google.com>

--- src/devices/nand/drivers/nand/nand.h
+++ src/devices/nand/drivers/nand/nand.h
@@ -21,7 +21,6 @@
 #include <fbl/mutex.h>
 
 #include "lib/inspect/cpp/vmo/types.h"
-#include "src/devices/nand/drivers/nand/read_cache.h"
 
 namespace nand {
 
@@ -99,7 +98,6 @@ class NandDevice : public DeviceType, public ddk::NandProtocol<NandDevice, ddk::
 
   // Count read failures where all retries are exhausted.
   inspect::UintProperty read_failure_;
-  std::unique_ptr<ReadCache> dangerous_reads_cache_ = nullptr;
 
   thrd_t worker_thread_;
 
