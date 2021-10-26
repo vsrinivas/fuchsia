82127d266d9e5069241df1f546a776a2ad86d0b2
David Gilhooley <dgilhooley@google.com>
2021-10-26 20:24:13 +0000

    [driver_test_realm] Add platform
    The platform version of DriverTestRealm is like the Simple
    realm but it launches the platform bus.
    
    Change-Id: Ice62acefed76b8ff3cbdbd436ed45ec67a86f137
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/597702
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ sdk/lib/driver_test_realm/platform/README.md
@@ -0,0 +1,6 @@
+# Overview
+
+This directory contains a Simple DriverTestRealm where the root driver is
+the platform bus driver.
+For more information on using a SimpleDriverTestRealm see
+[this documentation](/sdk/lib/driver_test_realm/simple/README.md)
---
+++ sdk/lib/driver_test_realm/platform/platform.cc
@@ -0,0 +1,34 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/fuchsia.driver.test/cpp/wire.h>
+#include <lib/service/llcpp/service.h>
+#include <lib/syslog/global.h>
+
+int main() {
+  auto client_end = service::Connect<fuchsia_driver_test::Realm>();
+  if (!client_end.is_ok()) {
+    FX_LOGF(ERROR, "platform_driver_test_realm", "Failed to connect to Realm FIDL: %d",
+            client_end.error_value());
+    return 1;
+  }
+  auto client = fidl::BindSyncClient(std::move(*client_end));
+
+  fidl::Arena arena;
+  fuchsia_driver_test::wire::RealmArgs args(arena);
+  args.set_root_driver(arena, fidl::StringView("fuchsia-boot:///#driver/platform-bus.so"));
+  auto wire_result = client.Start(std::move(args));
+  if (wire_result.status() != ZX_OK) {
+    FX_LOGF(ERROR, "platform_driver_test_realm", "Failed to call to Realm:Start: %d",
+            wire_result.status());
+    return 1;
+  }
+  if (wire_result->result.is_err()) {
+    FX_LOGF(ERROR, "platform_driver_test_realm", "Realm:Start failed: %d",
+            wire_result->result.err());
+    return 1;
+  }
+
+  return 0;
+}
