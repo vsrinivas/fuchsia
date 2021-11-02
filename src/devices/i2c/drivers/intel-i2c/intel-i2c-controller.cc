c7772588a4574a5d77340856d18ce4d8216dd7a7
Yifei Teng <yifeit@google.com>
2021-11-02 20:57:09 +0000

    [intel-i2c] Migrate intel-i2c-controller.cc to new WireSyncClient API
    This is part of a migration to stabilize the LLCPP API. In this step, we
    are aligning the interface of WireSyncClient and WireClient. The main
    changes to WireSyncClient are:
    
    - Similar to WireClient, use the "->" operator to make FIDL calls with
    managed memory allocation.
    
    - Similar to an earlier change in `fidl::WireCall(...)`, use the
    `client.buffer(arena_or_buffer_span)` accessor to make FIDL calls with
    caller-controlled allocation.
    
    - Removing accessors that expose a raw pointer/reference to the
    channel, instead providing a `Bind` function to initialize a default
    constructed client, and a `TakeClientEnd` function to de-initialize it.
    
    - When binding a client endpoint, the client endpoint must be valid.
    
    See https://groups.google.com/a/fuchsia.dev/g/fidl-dev/c/nF72xpPL4gU
    
    Bug: 87683
    Change-Id: Ic8b733c3567e8f223511ac3a37febd530edda2eb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/600479
    Reviewed-by: Sam Hansen <hansens@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/i2c/drivers/intel-i2c/intel-i2c-controller.cc
+++ src/devices/i2c/drivers/intel-i2c/intel-i2c-controller.cc
@@ -265,7 +265,7 @@ zx_status_t IntelI2cController::I2cImplTransact(const uint32_t bus_id, const i2c
 }
 
 uint32_t IntelI2cController::I2cImplGetBusBase() {
-  auto result = acpi_.GetBusId();
+  auto result = acpi_->GetBusId();
   if (result.ok() && result->result.is_response()) {
     return result->result.response().bus_id;
   }
