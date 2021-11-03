df039201d333c63091ac2f08082c70a0cb03d8db
Brian Bosak <bbosak@google.com>
2021-11-03 04:40:16 +0000

    Revert "Reland "[logs] LogSinkConnected event""
    This reverts commit 3921dac377d4c492f31d84a0a22a460f7dcf6585.
    
    Reason for revert: https://fxbug.dev/87715
    
    Original change's description:
    > Reland "[logs] LogSinkConnected event"
    >
    > This reverts commit 46ca63a99d4142378bd7d7cfa85c1235381d5538.
    >
    > Multiply: archivist-integration-tests-v2
    >
    > Reason for revert: Doing this properly this time and adding the wait for Stopped event.
    >
    > Original change's description:
    > > Revert "[logs] LogSinkConnected event"
    > >
    > > This reverts commit 1579a675814c860045f8e96f02c4c1c0a0a74e77.
    > >
    > > Reason for revert: Flakes as per my comments -- this isn't at all surprising.
    > >
    > > Original change's description:
    > > > [logs] LogSinkConnected event
    > > >
    > > > This patch adds a new event that is triggered when
    > > > Connect or ConnectStructured is called.
    > > >
    > > > Change-Id: Iada2b3d96d6f5249e107d34749e9cb2390c5d673
    > > > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/589445
    > > > Reviewed-by: Miguel Flores <miguelfrde@google.com>
    > > > Commit-Queue: Brian Bosak <bbosak@google.com>
    > >
    > > TBR=miguelfrde@google.com,crjohns@google.com,bbosak@google.com
    > >
    > > Change-Id: Ic0b1a54721e2e9fbb131135e72faa110056a2c6f
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/590625
    > > Reviewed-by: Brian Bosak <bbosak@google.com>
    > > Commit-Queue: Brian Bosak <bbosak@google.com>
    >
    > # Not skipping CQ checks because this is a reland.
    >
    > Change-Id: Ibe5bf1efdf36be3dc880e070ae94ea0e62d8b2b0
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/590626
    > Reviewed-by: Miguel Flores <miguelfrde@google.com>
    > Commit-Queue: Brian Bosak <bbosak@google.com>
    
    Change-Id: I179f037e036a84d2dcf96dbd8e67994584541eb5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/600882
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>

--- src/diagnostics/archivist/src/lifecycle/container.rs
+++ src/diagnostics/archivist/src/lifecycle/container.rs
@@ -3,10 +3,7 @@
 // found in the LICENSE file.
 
 use {
-    crate::{
-        container::ComponentIdentity, inspect::container::InspectArtifactsContainer,
-        logs::container::LogsArtifactsContainer,
-    },
+    crate::{container::ComponentIdentity, inspect::container::InspectArtifactsContainer},
     diagnostics_data::{self as schema, LifecycleType},
     diagnostics_hierarchy::{hierarchy, DiagnosticsHierarchy},
     fuchsia_zircon as zx,
@@ -44,18 +41,6 @@ impl LifecycleDataContainer {
             payload: None,
             event_timestamp: artifact.event_timestamp,
             lifecycle_type: LifecycleType::DiagnosticsReady,
-        }
-    }
-
-    pub fn from_logs_sink_connected_artifact(
-        artifact: &LogsArtifactsContainer,
-        identity: Arc<ComponentIdentity>,
-    ) -> Self {
-        LifecycleDataContainer {
-            identity,
-            payload: None,
-            event_timestamp: artifact.event_timestamp,
-            lifecycle_type: LifecycleType::LogSinkConnected,
         }
     }
 
