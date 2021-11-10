d77750f43939c1ff334f77ca1bbb5620e3150ac0
David Gilhooley <dgilhooley@google.com>
2021-11-10 18:08:20 +0000

    [storage] Refactor storage_driver_test_realm
    Write a new storage_driver_test_realm so that it will
    wait for ramctl to appear before the test is shut down.
    
    This temporarily fixes an issue with DriverTestRealm shutting down.
    This issue will be permanently fixed in fxb/88341
    
    Also update all storage tests to use this specific storage_driver_test_realm
    which removes a lot of boilerplate CML.
    
    Bug: 76509
    Change-Id: If0f91220d4e43f31033c1f6d95f1355f839a197e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/604781
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: David Gilhooley <dgilhooley@google.com>

---
+++ src/storage/testing/driver_test_realm/README.md
@@ -0,0 +1,9 @@
+# Overview
+
+This directory contains the Storage DriverTestRealm.
+This component starts a DriverTestRealm with all the storage drivers
+and it will ensure that the storage drivers are enumerated before the
+test is shut down.
+
+For more information on using a DriverTestRealm see
+[this documentation](/sdk/lib/driver_test_realm)
---
+++ src/storage/testing/driver_test_realm/storage_driver_test_realm.cc
@@ -0,0 +1,42 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/fuchsia.driver.test/cpp/wire.h>
+#include <lib/service/llcpp/service.h>
+#include <lib/syslog/global.h>
+
+#include "src/devices/lib/device-watcher/cpp/device-watcher.h"
+
+int main() {
+  auto client_end = service::Connect<fuchsia_driver_test::Realm>();
+  if (!client_end.is_ok()) {
+    FX_LOGF(ERROR, "platform_driver_test_realm", "Failed to connect to Realm FIDL: %d",
+            client_end.error_value());
+    return 1;
+  }
+  auto client = fidl::BindSyncClient(std::move(*client_end));
+
+  fidl::Arena arena;
+  fuchsia_driver_test::wire::RealmArgs args(arena);
+  args.set_root_driver(arena, fidl::StringView("fuchsia-boot:///#driver/platform-bus.so"));
+  auto wire_result = client->Start(std::move(args));
+  if (wire_result.status() != ZX_OK) {
+    FX_LOGF(ERROR, "platform_driver_test_realm", "Failed to call to Realm:Start: %d",
+            wire_result.status());
+    return 1;
+  }
+  if (wire_result->result.is_err()) {
+    FX_LOGF(ERROR, "platform_driver_test_realm", "Realm:Start failed: %d",
+            wire_result->result.err());
+    return 1;
+  }
+
+  fbl::unique_fd out;
+  zx_status_t status =
+      device_watcher::RecursiveWaitForFile("/dev/sys/platform/00:00:2d/ramctl", &out);
+  if (status != ZX_OK) {
+    FX_LOGF(ERROR, "platform_driver_test_realm", "Failed to wait for ramctl: %d", status);
+  }
+  return 0;
+}
