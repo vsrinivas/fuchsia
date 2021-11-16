e0810ef44d253b099c0b929265a99a05d4b1670e
Benjamin Prosnitz <bprosnitz@google.com>
2021-11-16 14:30:33 +0000

    [gidl] GIDL test for bytes/handles exceeding payload size
    This replaces an existing Go test with a GIDL test. This test fails in
    Rust and Dart but no attempt was made to fix the issues in this CL
    because they require separate fixes to each of the bindings.
    
    Change-Id: I0910a0db4e5c0cdfc8c9866648579c124ee6bbfb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/606701
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>
    Reviewed-by: Mitchell Kember <mkember@google.com>

--- tools/fidl/gidl/ir/error.go
+++ tools/fidl/gidl/ir/error.go
@@ -7,49 +7,53 @@ package ir
 type ErrorCode string
 
 const (
-	CountExceedsLimit            ErrorCode = "COUNT_EXCEEDS_LIMIT"
-	ExceededMaxOutOfLineDepth    ErrorCode = "EXCEEDED_MAX_OUT_OF_LINE_DEPTH"
-	ExtraHandles                 ErrorCode = "EXTRA_HANDLES"
-	IncorrectHandleType          ErrorCode = "INCORRECT_HANDLE_TYPE"
-	InvalidInlineBitInEnvelope   ErrorCode = "INVALID_INLINE_BIT_IN_ENVELOPE"
-	InvalidNumBytesInEnvelope    ErrorCode = "INVALID_NUM_BYTES_IN_ENVELOPE"
-	InvalidNumHandlesInEnvelope  ErrorCode = "INVALID_NUM_HANDLES_IN_ENVELOPE"
-	InvalidPaddingByte           ErrorCode = "INVALID_PADDING_BYTE"
-	InvalidPresenceIndicator     ErrorCode = "INVALID_PRESENCE_INDICATOR"
-	MissingRequiredHandleRights  ErrorCode = "MISSING_REQUIRED_HANDLE_RIGHTS"
-	NonEmptyStringWithNullBody   ErrorCode = "NON_EMPTY_STRING_WITH_NULL_BODY"
-	NonNullableTypeWithNullValue ErrorCode = "NON_NULLABLE_TYPE_WITH_NULL_VALUE"
-	NonResourceUnknownHandles    ErrorCode = "NON_RESOURCE_UNKNOWN_HANDLES"
-	StrictBitsUnknownBit         ErrorCode = "STRICT_BITS_UNKNOWN_BIT"
-	StrictEnumUnknownValue       ErrorCode = "STRICT_ENUM_UNKNOWN_VALUE"
-	StrictUnionUnknownField      ErrorCode = "STRICT_UNION_UNKNOWN_FIELD"
-	StringNotUtf8                ErrorCode = "STRING_NOT_UTF8"
-	StringTooLong                ErrorCode = "STRING_TOO_LONG"
-	TooFewBytes                  ErrorCode = "TOO_FEW_BYTES"
-	TooFewHandles                ErrorCode = "TOO_FEW_HANDLES"
-	UnionFieldNotSet             ErrorCode = "UNION_FIELD_NOT_SET"
+	CountExceedsLimit                  ErrorCode = "COUNT_EXCEEDS_LIMIT"
+	ExceededMaxOutOfLineDepth          ErrorCode = "EXCEEDED_MAX_OUT_OF_LINE_DEPTH"
+	ExtraHandles                       ErrorCode = "EXTRA_HANDLES"
+	IncorrectHandleType                ErrorCode = "INCORRECT_HANDLE_TYPE"
+	InvalidInlineBitInEnvelope         ErrorCode = "INVALID_INLINE_BIT_IN_ENVELOPE"
+	InvalidNumBytesInEnvelope          ErrorCode = "INVALID_NUM_BYTES_IN_ENVELOPE"
+	InvalidNumHandlesInEnvelope        ErrorCode = "INVALID_NUM_HANDLES_IN_ENVELOPE"
+	InvalidPaddingByte                 ErrorCode = "INVALID_PADDING_BYTE"
+	InvalidPresenceIndicator           ErrorCode = "INVALID_PRESENCE_INDICATOR"
+	MissingRequiredHandleRights        ErrorCode = "MISSING_REQUIRED_HANDLE_RIGHTS"
+	NonEmptyStringWithNullBody         ErrorCode = "NON_EMPTY_STRING_WITH_NULL_BODY"
+	NonNullableTypeWithNullValue       ErrorCode = "NON_NULLABLE_TYPE_WITH_NULL_VALUE"
+	NonResourceUnknownHandles          ErrorCode = "NON_RESOURCE_UNKNOWN_HANDLES"
+	StrictBitsUnknownBit               ErrorCode = "STRICT_BITS_UNKNOWN_BIT"
+	StrictEnumUnknownValue             ErrorCode = "STRICT_ENUM_UNKNOWN_VALUE"
+	StrictUnionUnknownField            ErrorCode = "STRICT_UNION_UNKNOWN_FIELD"
+	StringNotUtf8                      ErrorCode = "STRING_NOT_UTF8"
+	StringTooLong                      ErrorCode = "STRING_TOO_LONG"
+	TooFewBytes                        ErrorCode = "TOO_FEW_BYTES"
+	TooFewHandles                      ErrorCode = "TOO_FEW_HANDLES"
+	UnionFieldNotSet                   ErrorCode = "UNION_FIELD_NOT_SET"
+	EnvelopeBytesExceedMessageLength   ErrorCode = "ENVELOPE_BYTES_EXCEED_MESSAGE_LENGTH"
+	EnvelopeHandlesExceedMessageLength ErrorCode = "ENVELOPE_HANDLES_EXCEED_MESSAGE_LENGTH"
 )
 
 var AllErrorCodes = map[ErrorCode]struct{}{
-	CountExceedsLimit:            {},
-	ExceededMaxOutOfLineDepth:    {},
-	ExtraHandles:                 {},
-	IncorrectHandleType:          {},
-	InvalidInlineBitInEnvelope:   {},
-	InvalidNumBytesInEnvelope:    {},
-	InvalidNumHandlesInEnvelope:  {},
-	InvalidPaddingByte:           {},
-	InvalidPresenceIndicator:     {},
-	MissingRequiredHandleRights:  {},
-	NonEmptyStringWithNullBody:   {},
-	NonNullableTypeWithNullValue: {},
-	NonResourceUnknownHandles:    {},
-	StrictBitsUnknownBit:         {},
-	StrictEnumUnknownValue:       {},
-	StrictUnionUnknownField:      {},
-	StringNotUtf8:                {},
-	StringTooLong:                {},
-	TooFewBytes:                  {},
-	TooFewHandles:                {},
-	UnionFieldNotSet:             {},
+	CountExceedsLimit:                  {},
+	ExceededMaxOutOfLineDepth:          {},
+	ExtraHandles:                       {},
+	IncorrectHandleType:                {},
+	InvalidInlineBitInEnvelope:         {},
+	InvalidNumBytesInEnvelope:          {},
+	InvalidNumHandlesInEnvelope:        {},
+	InvalidPaddingByte:                 {},
+	InvalidPresenceIndicator:           {},
+	MissingRequiredHandleRights:        {},
+	NonEmptyStringWithNullBody:         {},
+	NonNullableTypeWithNullValue:       {},
+	NonResourceUnknownHandles:          {},
+	StrictBitsUnknownBit:               {},
+	StrictEnumUnknownValue:             {},
+	StrictUnionUnknownField:            {},
+	StringNotUtf8:                      {},
+	StringTooLong:                      {},
+	TooFewBytes:                        {},
+	TooFewHandles:                      {},
+	UnionFieldNotSet:                   {},
+	EnvelopeBytesExceedMessageLength:   {},
+	EnvelopeHandlesExceedMessageLength: {},
 }
