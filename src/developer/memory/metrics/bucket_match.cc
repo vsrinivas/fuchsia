vsrinivas/fuchsia

commit 6033e5f0669ca7bea3c1539ef611c80714b7a07e

[memory] Replace std::regex with re2::RE2
See RFC-0105 for background:
https://fuchsia.dev/fuchsia-src/contribute/governance/rfcs/0105_regular_expression_libraries

For the most part this is purely mechanical, with a few exceptions:

- By default, RE2 does not match against invalid UTF8. If the source
  strings may be invalid UTF8 (e.g., if they can contain '\0'), the RE2
  object must be created in Latin1 mode.

- RE2 is not copyable or movable.

Bug: 77811
Change-Id: I3749bec506ce5f693e031d403b689cafcfc3bba0
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/603043
Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
Fuchsia-Auto-Submit: Tom Bergan <tombergan@google.com>
Reviewed-by: David Moore <davemoore@google.com>

src/developer/memory/metrics/BUILD.gn
@@ -18,7 +18,10 @@ source_set("metrics") {
     "watcher.h",
   ]
 
-  public_deps = [ "//sdk/fidl/fuchsia.kernel:fuchsia.kernel_llcpp" ]
+  public_deps = [
+    "//sdk/fidl/fuchsia.kernel:fuchsia.kernel_llcpp",
+    "//third_party/re2",
+  ]
 
   deps = [
     "//sdk/fidl/fuchsia.kernel:fuchsia.kernel_c",

src/developer/memory/metrics/bucket_match.cc
@@ -9,7 +9,8 @@
 
 #include <filesystem>
 #include <optional>
-#include <regex>
+
+#include <re2/re2.h>
 
 #include "third_party/rapidjson/include/rapidjson/document.h"
 #include "third_party/rapidjson/include/rapidjson/ostreamwrapper.h"
@@ -22,11 +23,10 @@ BucketMatch::BucketMatch(const std::string& name, const std::string& process,
                          const std::string& vmo, std::optional<int64_t> event_code)
     : name_(name),
       match_all_processes_(process.empty() || process == ".*"),
-      process_(process),
+      process_(std::make_shared<re2::RE2>(process)),
       match_all_vmos_(vmo.empty() || vmo == ".*"),
-      vmo_(vmo),
-      event_code_(event_code) {
-}
+      vmo_(std::make_shared<re2::RE2>(vmo)),
+      event_code_(event_code) {}
 
 bool BucketMatch::ProcessMatch(const Process& process) {
   if (match_all_processes_) {
@@ -36,7 +36,7 @@ bool BucketMatch::ProcessMatch(const Process& process) {
   if (pi != process_match_.end()) {
     return pi->second;
   }
-  bool match = std::regex_match(process.name, process_);
+  bool match = re2::RE2::FullMatch(process.name, *process_);
   process_match_.emplace(process.koid, match);
   return match;
 }
@@ -49,7 +49,7 @@ bool BucketMatch::VmoMatch(const std::string& vmo) {
   if (vi != vmo_match_.end()) {
     return vi->second;
   }
-  bool match = std::regex_match(vmo, vmo_);
+  bool match = re2::RE2::FullMatch(vmo, *vmo_);
   vmo_match_.emplace(vmo, match);
   return match;
 }

src/developer/memory/metrics/bucket_match.h
@@ -9,10 +9,11 @@
 
 #include <functional>
 #include <optional>
-#include <regex>
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+#include <re2/re2.h>
 
 #include "src/developer/memory/metrics/capture.h"
 #include "src/lib/files/file.h"
@@ -43,9 +44,9 @@ class BucketMatch {
  private:
   const std::string name_;
   bool match_all_processes_;
-  const std::regex process_;
+  const std::shared_ptr<re2::RE2> process_;  // shared_ptr because RE2 is not movable or copyable
   bool match_all_vmos_;
-  const std::regex vmo_;
+  const std::shared_ptr<re2::RE2> vmo_;
   const std::optional<int64_t> event_code_;
 
   // Cache of the matching results against the |process_| regexp.

src/developer/memory/metrics/digest.cc
@@ -6,8 +6,6 @@
 
 #include <lib/trace/event.h>
 
-#include <regex>
-
 #include "src/developer/memory/metrics/bucket_match.h"
 
 namespace memory {

src/developer/memory/metrics/digest.h
@@ -7,7 +7,6 @@
 
 #include <zircon/types.h>
 
-#include <regex>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>

src/developer/memory/metrics/summary.cc
@@ -21,10 +21,11 @@ const std::vector<const NameMatch> Summary::kNameMatches = {
     {"scudo:.*", "[scudo]"},
     {".*\\.so.*", "[libraries]"}};
 
-Namer::Namer(const std::vector<const NameMatch>& name_matches)
-    : regex_matches_(name_matches.size()) {
+Namer::Namer(const std::vector<const NameMatch>& name_matches) {
+  regex_matches_.reserve(name_matches.size());
   for (const auto& name_match : name_matches) {
-    regex_matches_.push_back(RegexMatch{std::regex(name_match.regex), name_match.name});
+    regex_matches_.push_back(
+        RegexMatch{std::make_unique<re2::RE2>(name_match.regex), name_match.name});
   }
 }
 
@@ -34,7 +35,7 @@ const std::string& Namer::NameForName(const std::string& name) {
     return found_name->second;
   }
   for (const auto& regex_match : regex_matches_) {
-    if (std::regex_match(name, regex_match.regex)) {
+    if (re2::RE2::FullMatch(name, *regex_match.regex)) {
       name_to_name_.emplace(name, regex_match.name);
       return regex_match.name;
     }

src/developer/memory/metrics/summary.h
@@ -7,11 +7,12 @@
 
 #include <zircon/types.h>
 
-#include <regex>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+
+#include <re2/re2.h>
 
 #include "src/developer/memory/metrics/capture.h"
 
@@ -61,7 +62,7 @@ class Namer {
 
  private:
   struct RegexMatch {
-    std::regex regex;
+    std::unique_ptr<re2::RE2> regex;  // unique_ptr because RE2 is not movable
     std::string name;
   };
 

commit b3bed85241bf00f18433222ec9b3e320bdc5e062

Reland "[memory] Replace std::regex with re2::RE2"
This is a reland of 6033e5f0669ca7bea3c1539ef611c80714b7a07e
which caused an unaccounted-for increase in astro binary size.
The astro size limits have been updated to reflect this change,
so this should be safe to land.

Original change's description:
> [memory] Replace std::regex with re2::RE2
>
> See RFC-0105 for background:
> https://fuchsia.dev/fuchsia-src/contribute/governance/rfcs/0105_regular_expression_libraries
>
> For the most part this is purely mechanical, with a few exceptions:
>
> - By default, RE2 does not match against invalid UTF8. If the source
>   strings may be invalid UTF8 (e.g., if they can contain '\0'), the RE2
>   object must be created in Latin1 mode.
>
> - RE2 is not copyable or movable.
>
> Bug: 77811
> Change-Id: I3749bec506ce5f693e031d403b689cafcfc3bba0
> Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/603043
> Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
> Fuchsia-Auto-Submit: Tom Bergan <tombergan@google.com>
> Reviewed-by: David Moore <davemoore@google.com>

Bug: 77811
Change-Id: Ia914d748bea11b0ce975fbdb5087c2a5f5f9b688
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/605998
Reviewed-by: David Moore <davemoore@google.com>
Commit-Queue: Tom Bergan <tombergan@google.com>

src/developer/memory/metrics/BUILD.gn
@@ -18,7 +18,10 @@ source_set("metrics") {
     "watcher.h",
   ]
 
-  public_deps = [ "//sdk/fidl/fuchsia.kernel:fuchsia.kernel_llcpp" ]
+  public_deps = [
+    "//sdk/fidl/fuchsia.kernel:fuchsia.kernel_llcpp",
+    "//third_party/re2",
+  ]
 
   deps = [
     "//sdk/fidl/fuchsia.kernel:fuchsia.kernel_c",

src/developer/memory/metrics/bucket_match.cc
@@ -9,7 +9,8 @@
 
 #include <filesystem>
 #include <optional>
-#include <regex>
+
+#include <re2/re2.h>
 
 #include "third_party/rapidjson/include/rapidjson/document.h"
 #include "third_party/rapidjson/include/rapidjson/ostreamwrapper.h"
@@ -22,11 +23,10 @@ BucketMatch::BucketMatch(const std::string& name, const std::string& process,
                          const std::string& vmo, std::optional<int64_t> event_code)
     : name_(name),
       match_all_processes_(process.empty() || process == ".*"),
-      process_(process),
+      process_(std::make_shared<re2::RE2>(process)),
       match_all_vmos_(vmo.empty() || vmo == ".*"),
-      vmo_(vmo),
-      event_code_(event_code) {
-}
+      vmo_(std::make_shared<re2::RE2>(vmo)),
+      event_code_(event_code) {}
 
 bool BucketMatch::ProcessMatch(const Process& process) {
   if (match_all_processes_) {
@@ -36,7 +36,7 @@ bool BucketMatch::ProcessMatch(const Process& process) {
   if (pi != process_match_.end()) {
     return pi->second;
   }
-  bool match = std::regex_match(process.name, process_);
+  bool match = re2::RE2::FullMatch(process.name, *process_);
   process_match_.emplace(process.koid, match);
   return match;
 }
@@ -49,7 +49,7 @@ bool BucketMatch::VmoMatch(const std::string& vmo) {
   if (vi != vmo_match_.end()) {
     return vi->second;
   }
-  bool match = std::regex_match(vmo, vmo_);
+  bool match = re2::RE2::FullMatch(vmo, *vmo_);
   vmo_match_.emplace(vmo, match);
   return match;
 }

src/developer/memory/metrics/bucket_match.h
@@ -9,10 +9,11 @@
 
 #include <functional>
 #include <optional>
-#include <regex>
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+#include <re2/re2.h>
 
 #include "src/developer/memory/metrics/capture.h"
 #include "src/lib/files/file.h"
@@ -43,9 +44,9 @@ class BucketMatch {
  private:
   const std::string name_;
   bool match_all_processes_;
-  const std::regex process_;
+  const std::shared_ptr<re2::RE2> process_;  // shared_ptr because RE2 is not movable or copyable
   bool match_all_vmos_;
-  const std::regex vmo_;
+  const std::shared_ptr<re2::RE2> vmo_;
   const std::optional<int64_t> event_code_;
 
   // Cache of the matching results against the |process_| regexp.

src/developer/memory/metrics/digest.cc
@@ -6,8 +6,6 @@
 
 #include <lib/trace/event.h>
 
-#include <regex>
-
 #include "src/developer/memory/metrics/bucket_match.h"
 
 namespace memory {

src/developer/memory/metrics/digest.h
@@ -7,7 +7,6 @@
 
 #include <zircon/types.h>
 
-#include <regex>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>

src/developer/memory/metrics/summary.cc
@@ -21,10 +21,11 @@ const std::vector<const NameMatch> Summary::kNameMatches = {
     {"scudo:.*", "[scudo]"},
     {".*\\.so.*", "[libraries]"}};
 
-Namer::Namer(const std::vector<const NameMatch>& name_matches)
-    : regex_matches_(name_matches.size()) {
+Namer::Namer(const std::vector<const NameMatch>& name_matches) {
+  regex_matches_.reserve(name_matches.size());
   for (const auto& name_match : name_matches) {
-    regex_matches_.push_back(RegexMatch{std::regex(name_match.regex), name_match.name});
+    regex_matches_.push_back(
+        RegexMatch{std::make_unique<re2::RE2>(name_match.regex), name_match.name});
   }
 }
 
@@ -34,7 +35,7 @@ const std::string& Namer::NameForName(const std::string& name) {
     return found_name->second;
   }
   for (const auto& regex_match : regex_matches_) {
-    if (std::regex_match(name, regex_match.regex)) {
+    if (re2::RE2::FullMatch(name, *regex_match.regex)) {
       name_to_name_.emplace(name, regex_match.name);
       return regex_match.name;
     }

src/developer/memory/metrics/summary.h
@@ -7,11 +7,12 @@
 
 #include <zircon/types.h>
 
-#include <regex>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+
+#include <re2/re2.h>
 
 #include "src/developer/memory/metrics/capture.h"
 
@@ -61,7 +62,7 @@ class Namer {
 
  private:
   struct RegexMatch {
-    std::regex regex;
+    std::unique_ptr<re2::RE2> regex;  // unique_ptr because RE2 is not movable
     std::string name;
   };
 