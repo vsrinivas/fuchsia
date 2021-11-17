Tiggerlaboratoriet/pigweed

[bt][gap] Delay Connected signal until after pair
Redefine the semantics of BR/EDR connection states, extending the
kInitializing state until the peer has completed pairing, either
by recalling a previously-negotiated key or from a new pair procedure.

This has the effect of not marking peers connected for Access clients
until a profile (local or peer) has attempted a connection, which
matches expectations of clients more closely.

As part of this change, BR/EDR peers that connect without pairing may
become temporary (and expire) if they do not pair.

Bug: b/199745280
Test: updated tests for new semantics
Test: fx test bt-host-gap-tests
Change-Id: Idc097b7a4785ffbf1c7f21adc9adda62a85da282
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/582823
Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
Fuchsia-Auto-Submit: Marie Janssen 💖 <jamuraa@google.com>
Reviewed-by: Ben Lawson <benlawson@google.com>
Reviewed-by: Xo Wang <xow@google.com>

pw_bluetooth_sapphire/host/gap/peer.cc
@@ -251,13 +251,19 @@ void Peer::BrEdrData::SetConnectionState(ConnectionState state) {
     peer_->peer_metrics_->LogBrEdrDisconnection();
   }
 
+  ConnectionState from_state = connection_state();
   conn_state_.Set(state);
   peer_->UpdateExpiry();
-  peer_->UpdatePeerAndNotifyListeners(NotifyListenersChange::kBondNotUpdated);
+  // Transition to or from Connected state is a notifyable change.
+  if ((from_state == ConnectionState::kConnected) || (state == ConnectionState::kConnected)) {
+    peer_->UpdatePeerAndNotifyListeners(NotifyListenersChange::kBondNotUpdated);
+  }
 
-  // Become non-temporary if we became connected. BR/EDR device remain
-  // non-temporary afterwards.
-  if (state == ConnectionState::kConnected) {
+  // Become non-temporary if we successfully connect or are initializing. BR/EDR device remain
+  // non-temporary afterwards if bonded, and temporary again if disconnect without bonding.
+  if (state == ConnectionState::kNotConnected) {
+    peer_->TryMakeTemporary();
+  } else {
     peer_->TryMakeNonTemporary();
   }
 }
@@ -512,7 +518,12 @@ bool Peer::TryMakeNonTemporary() {
 
 bool Peer::TryMakeTemporary() {
   if (le() && le()->connection_state() == ConnectionState::kNotConnected && !identity_known()) {
-    bt_log(DEBUG, "gap", "became temporary: %s:", bt_str(*this));
+    bt_log(DEBUG, "gap", "LE became temporary: %s:", bt_str(*this));
+    temporary_.Set(true);
+    return true;
+  }
+  if (bredr() && !bredr()->bonded()) {
+    bt_log(DEBUG, "gap", "BR/EDR became temporary: %s:", bt_str(*this));
     temporary_.Set(true);
     return true;
   }

pw_bluetooth_sapphire/host/gap/peer_unittest.cc
@@ -371,6 +371,36 @@ TEST_F(PeerTest, SettingBrEdrConnectionStateUpdatesLastUpdated) {
   EXPECT_GE(notify_count, 1);
 }
 
+TEST_F(PeerTest, SettingBrEdrConnectionStateUpdatesTemporary) {
+  int notify_count = 0;
+  set_notify_listeners_cb([&](const Peer&, Peer::NotifyListenersChange) { notify_count++; });
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kInitializing);
+  ASSERT_FALSE(peer().temporary());
+  // Notifications: one for non-temporary.
+  EXPECT_EQ(notify_count, 1);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kNotConnected);
+  ASSERT_TRUE(peer().temporary());
+  EXPECT_EQ(notify_count, 1);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kInitializing);
+  ASSERT_FALSE(peer().temporary());
+  // +1 notification (non-temporary)
+  EXPECT_EQ(notify_count, 2);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kConnected);
+  peer().MutBrEdr().SetBondData(kSecureBrEdrKey);
+  ASSERT_FALSE(peer().temporary());
+  // +2 notification (connected, bonded)
+  EXPECT_EQ(notify_count, 4);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kNotConnected);
+  ASSERT_FALSE(peer().temporary());
+  // +1 notification (connection state)
+  EXPECT_EQ(notify_count, 5);
+}
+
 TEST_F(PeerTest, SettingInquiryDataUpdatesLastUpdated) {
   SetUpPeer(/*address=*/kAddrLeAlias, /*connectable=*/true);
   EXPECT_EQ(peer().last_updated(), zx::time(0));

vsrinivas/fuchsia

src/connectivity/bluetooth/core/bt-host/gap/peer.cc
@@ -220,13 +220,19 @@ void Peer::BrEdrData::SetConnectionState(ConnectionState state) {
     peer_->peer_metrics_->LogBrEdrDisconnection();
   }
 
+  ConnectionState from_state = connection_state();
   conn_state_.Set(state);
   peer_->UpdateExpiry();
-  peer_->NotifyListeners(NotifyListenersChange::kBondNotUpdated);
+  // Transition to or from Connected state is a notifyable change.
+  if ((from_state == ConnectionState::kConnected) || (state == ConnectionState::kConnected)) {
+    peer_->NotifyListeners(NotifyListenersChange::kBondNotUpdated);
+  }
 
-  // Become non-temporary if we became connected. BR/EDR device remain
-  // non-temporary afterwards.
-  if (state == ConnectionState::kConnected) {
+  // Become non-temporary if we successfully connect or are initializing. BR/EDR device remain
+  // non-temporary afterwards if bonded, and temporary again if disconnect without bonding.
+  if (state == ConnectionState::kNotConnected) {
+    peer_->TryMakeTemporary();
+  } else {
     peer_->TryMakeNonTemporary();
   }
 }
@@ -475,6 +481,20 @@ bool Peer::TryMakeNonTemporary() {
   }
 
   return true;
+}
+
+bool Peer::TryMakeTemporary() {
+  if (le() && le()->connection_state() == ConnectionState::kNotConnected && !identity_known()) {
+    bt_log(DEBUG, "gap", "LE became temporary: %s:", bt_str(*this));
+    temporary_.Set(true);
+    return true;
+  }
+  if (bredr() && !bredr()->bonded()) {
+    bt_log(DEBUG, "gap", "BR/EDR became temporary: %s:", bt_str(*this));
+    temporary_.Set(true);
+    return true;
+  }
+  return false;
 }
 
 void Peer::UpdateExpiry() {

src/connectivity/bluetooth/core/bt-host/gap/peer_unittest.cc
@@ -13,6 +13,7 @@
 #include "lib/gtest/test_loop_fixture.h"
 #include "src/connectivity/bluetooth/core/bt-host/common/manufacturer_names.h"
 #include "src/connectivity/bluetooth/core/bt-host/hci-spec/util.h"
+#include "src/connectivity/bluetooth/core/bt-host/hci-spec/link_key.h"
 
 namespace bt::gap {
 namespace {
@@ -21,6 +22,11 @@ using namespace inspect::testing;
 
 constexpr uint16_t kManufacturer = 0x0001;
 constexpr uint16_t kSubversion = 0x0002;
+
+const bt::sm::LTK kSecureBrEdrKey(sm::SecurityProperties(true /*encrypted*/, true /*authenticated*/,
+                                                         true /*secure_connections*/,
+                                                         sm::kMaxEncryptionKeySize),
+                                  hci::LinkKey(UInt128{4}, 5, 6));
 
 class GAP_PeerTest : public ::gtest::TestLoopFixture {
  public:
@@ -220,6 +226,36 @@ TEST_F(GAP_PeerTest, SetNameWithInvalidUtf8NameDoesNotUpdatePeerName) {
   peer().SetName(kName);
   EXPECT_FALSE(listener_notified);
   EXPECT_FALSE(peer().name().has_value());
+}
+
+TEST_F(GAP_PeerTest, SettingBrEdrConnectionStateUpdatesTemporary) {
+  int notify_count = 0;
+  set_notify_listeners_cb([&](const Peer&, Peer::NotifyListenersChange) { notify_count++; });
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kInitializing);
+  ASSERT_FALSE(peer().temporary());
+  // Notifications: one for non-temporary.
+  EXPECT_EQ(notify_count, 1);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kNotConnected);
+  ASSERT_TRUE(peer().temporary());
+  EXPECT_EQ(notify_count, 1);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kInitializing);
+  ASSERT_FALSE(peer().temporary());
+  // +1 notification (non-temporary)
+  EXPECT_EQ(notify_count, 2);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kConnected);
+  peer().MutBrEdr().SetBondData(kSecureBrEdrKey);
+  ASSERT_FALSE(peer().temporary());
+  // +2 notification (connected, bonded)
+  EXPECT_EQ(notify_count, 4);
+
+  peer().MutBrEdr().SetConnectionState(Peer::ConnectionState::kNotConnected);
+  ASSERT_FALSE(peer().temporary());
+  // +1 notification (connection state)
+  EXPECT_EQ(notify_count, 5);
 }
 
 }  // namespace