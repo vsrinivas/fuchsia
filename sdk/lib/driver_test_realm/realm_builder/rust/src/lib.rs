d9d3763fc536b9a19e88df7c5103aa4a5ec56355
Abdulla Kamar <abdulla@google.com>
2021-11-18 00:37:00 +0000

    [fdf] Add driver_test_realm_manifest_setup
    This allows a test to specify its own manifest for the
    driver_test_realm, thereby allowing it to add more capability routes, or
    provide any other modifications.
    
    Bug: 86778
    Bug: 33183
    Change-Id: Ib72eb96951878e4e175f706e94859eeb3a72f9e6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/608105
    Commit-Queue: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Fuchsia-Auto-Submit: Abdulla Kamar <abdulla@google.com>

--- sdk/lib/driver_test_realm/realm_builder/rust/src/lib.rs
+++ sdk/lib/driver_test_realm/realm_builder/rust/src/lib.rs
@@ -17,53 +17,61 @@ pub const DRIVER_TEST_REALM_URL: &str = "#meta/driver_test_realm.cm";
 pub trait DriverTestRealmBuilder {
     /// Set up the DriverTestRealm component in the RealmBuilder realm.
     /// This configures proper input/output routing of capabilities.
+    /// This takes a `manifest_url` to use, which is used by tests that need to
+    /// specify a custom driver test realm.
+    async fn driver_test_realm_manifest_setup(&self, manifest_url: &str) -> Result<&Self>;
+    /// Set up the DriverTestRealm component in the RealmBuilder realm.
+    /// This configures proper input/output routing of capabilities.
     async fn driver_test_realm_setup(&self) -> Result<&Self>;
 }
 
 #[async_trait::async_trait]
 impl DriverTestRealmBuilder for RealmBuilder {
-    async fn driver_test_realm_setup(&self) -> Result<&Self> {
-        self.add_child(COMPONENT_NAME, DRIVER_TEST_REALM_URL, ChildProperties::new().eager())
-            .await?;
-
+    async fn driver_test_realm_manifest_setup(&self, manifest_url: &str) -> Result<&Self> {
         let driver_realm = RouteEndpoint::component(COMPONENT_NAME);
-        self.add_route(
-            RouteBuilder::protocol("fuchsia.logger.LogSink")
-                .source(RouteEndpoint::AboveRoot)
-                .targets(vec![driver_realm.clone()]),
-        )
-        .await?;
-        self.add_route(
-            RouteBuilder::protocol("fuchsia.process.Launcher")
-                .source(RouteEndpoint::AboveRoot)
-                .targets(vec![driver_realm.clone()]),
-        )
-        .await?;
-        self.add_route(
-            RouteBuilder::protocol("fuchsia.sys.Launcher")
-                .source(RouteEndpoint::AboveRoot)
-                .targets(vec![driver_realm.clone()]),
-        )
-        .await?;
-        self.add_route(
-            RouteBuilder::protocol("fuchsia.driver.development.DriverDevelopment")
-                .source(driver_realm.clone())
-                .targets(vec![RouteEndpoint::AboveRoot]),
-        )
-        .await?;
-        self.add_route(
-            RouteBuilder::protocol("fuchsia.driver.test.Realm")
-                .source(driver_realm.clone())
-                .targets(vec![RouteEndpoint::AboveRoot]),
-        )
-        .await?;
-        self.add_route(
-            RouteBuilder::directory("dev", "dev", fio2::RW_STAR_DIR)
-                .source(driver_realm.clone())
-                .targets(vec![RouteEndpoint::AboveRoot]),
-        )
-        .await?;
+        self.add_child(COMPONENT_NAME, manifest_url, ChildProperties::new().eager())
+            .await?
+            .add_route(
+                RouteBuilder::protocol("fuchsia.logger.LogSink")
+                    .source(RouteEndpoint::AboveRoot)
+                    .targets(vec![driver_realm.clone()]),
+            )
+            .await?
+            .add_route(
+                RouteBuilder::protocol("fuchsia.process.Launcher")
+                    .source(RouteEndpoint::AboveRoot)
+                    .targets(vec![driver_realm.clone()]),
+            )
+            .await?
+            .add_route(
+                RouteBuilder::protocol("fuchsia.sys.Launcher")
+                    .source(RouteEndpoint::AboveRoot)
+                    .targets(vec![driver_realm.clone()]),
+            )
+            .await?
+            .add_route(
+                RouteBuilder::protocol("fuchsia.driver.development.DriverDevelopment")
+                    .source(driver_realm.clone())
+                    .targets(vec![RouteEndpoint::AboveRoot]),
+            )
+            .await?
+            .add_route(
+                RouteBuilder::protocol("fuchsia.driver.test.Realm")
+                    .source(driver_realm.clone())
+                    .targets(vec![RouteEndpoint::AboveRoot]),
+            )
+            .await?
+            .add_route(
+                RouteBuilder::directory("dev", "dev", fio2::RW_STAR_DIR)
+                    .source(driver_realm.clone())
+                    .targets(vec![RouteEndpoint::AboveRoot]),
+            )
+            .await?;
         Ok(&self)
+    }
+
+    async fn driver_test_realm_setup(&self) -> Result<&Self> {
+        self.driver_test_realm_manifest_setup(DRIVER_TEST_REALM_URL).await
     }
 }
 
