63a14f7cfbf52342009b562e6289d3e6c9113862
Florent Revest <revest@chromium.org>
2021-11-21 12:50:09 +0100

    [cpuperf] Define dummy cpu perf specs for riscv64
    Change-Id: I68d55da2d84e8ab73f15bca5e1e9c99d86333b13

---
+++ garnet/bin/cpuperf/tests/riscv64/tally.cc
@@ -0,0 +1,51 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/syslog/cpp/macros.h>
+
+#include "riscv64_tests.h"
+#include "garnet/lib/perfmon/events.h"
+
+class TallyVerifier : public Verifier {
+ public:
+  static std::unique_ptr<Verifier> Create(const cpuperf::SessionResultSpec* spec) {
+    return std::make_unique<TallyVerifier>(spec);
+  }
+
+  TallyVerifier(const cpuperf::SessionResultSpec* spec) : Verifier(spec) {
+    const perfmon::EventDetails* details;
+
+    bool rc __UNUSED = LookupEventByName("arch", "inst_retired", &details);
+    FX_DCHECK(rc);
+    instructions_retired_id_ = details->id;
+  }
+
+ private:
+  bool VerifyRecord(const perfmon::SampleRecord& record) override {
+    if (record.header->event == instructions_retired_id_) {
+      ++instructions_retired_count_;
+    }
+    return true;
+  }
+
+  bool VerifyTrace(const RecordCounts& counts) override {
+    bool pass = true;
+    if (instructions_retired_count_ == 0) {
+      FX_LOGS(ERROR) << "Missing inst_retired events";
+      pass = false;
+    }
+    return pass;
+  }
+
+  // Ids of the events we should see.
+  perfmon::EventId instructions_retired_id_;
+
+  // Counts of the events we should see;
+  size_t instructions_retired_count_ = 0;
+};
+
+const TestSpec kTallySpec = {
+    "tally",
+    &TallyVerifier::Create,
+};
