8208726268e1755a89f031237454fd743dd5e622
Martin Puryear <mpuryear@google.com>
2021-11-23 04:04:25 +0000

    [audio][test] Adjust fidelity frequencies
    In our fidelity testing, we use sinusoid input signals to characterize
    a pipeline's frequency response and signal/noise. We measure frequency
    response and signal/noise at specific frequencies, enforcing that the
    results meet our expectations. Our set of test frequencies span from
    subsonic (including 0hz) through the audible spectrum into the
    ultrasonic range.
    
    We get more accurate measurements because we avoid "windowing" the
    output. This is possible because we use specific test frequencies that
    lead to an integral number of output wavelengths in our power-of-two
    buffer. We have discovered a side-effect of this test optimization.
    
    Test frequencies that share many common factors with 48000 are prone
    to slightly inaccurate results (+/- a few dB) compared to the rest of
    the spectrum. Changing the frequency by even 1hz in either direction
    leads to results that are much more consistent.
    
    This CL changes our test frequencies to avoid frequencies that are
    multiples of 375 (the non-power-of-two factor of 48000), specifically
    21khz and 31.5 khz.
    
    We still believe this no-windowing optimization is the best approach,
    this slight workaround notwithstanding.
    
    Change-Id: Iebec83ff5c74d3d63d81e9bb49af23db7ad3d20b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/609979
    Commit-Queue: Martin Puryear <mpuryear@google.com>
    Reviewed-by: Tom Bergan <tombergan@google.com>

--- src/media/audio/lib/test/hermetic_fidelity_frequencies.cc
+++ src/media/audio/lib/test/hermetic_fidelity_frequencies.cc
@@ -11,14 +11,25 @@ namespace media::audio::test {
 // Frequencies at which we do fidelity testing. We test a broad spectrum of 3 freqs/octave
 // (10 freqs/decade), omitting some subsonic frequencies and adding others near the top of the
 // audible range. The last five frequencies expressly frame the ultrasound range.
+//
+// We adjust these frequencies so that an integral number of wavelengths fit into our power-of-two-
+// sized analysis buffer. This eliminates our need to window the output before analysis, which in
+// turn leads to crisper results. The limitations of float32 "soften" most measurements, except
+// where positions/values align so cleanly in the buffer that minimal information is lost to float-
+// epsilon-imprecision. In those cases (frequencies that translate into a high-factor number of
+// wavelengths in the analysis buffer), we are susceptible to "harmonic spike" measurements. Ideally
+// each frequency translates to a PRIME number of periods in the analysis buffer, but since
+// "periods" equals "floor-or-ceiling of frequency / frame_rate * analysis_buffer_size", we must at
+// least avoid frequencies containing every non-2 factor of our frame rate (for 48khz and 96khz this
+// is ** 375 **). Thus in our frequency list we avoid 375, 3000, 7500, 21000, 31500, etc.
 const std::array<int32_t, HermeticFidelityTest::kNumReferenceFreqs>
     HermeticFidelityTest::kReferenceFrequencies = {
         // clang-format off
   0,      12,     20,     25,     31,     40,     50,     62,     80,     100,
   125,    160,    200,    250,    315,    400,    500,    625,    800,    1000,
   1250,   1600,   2000,   2500,   3150,   4000,   5000,   6300,   8000,   10000,
-  12500,  16000,  20000,  21000,  22000,  23000,  23500,  24500,  25000,  26000,
-  28000,  31500,
+  12500,  16000,  20000,  20900,  22000,  23000,  23500,  24500,  25000,  26000,
+  28000,  31400,
         // clang-format on
 };
 
