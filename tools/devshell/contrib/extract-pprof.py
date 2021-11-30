21839286a20c839bbedf2e30a62b690316c07fc5
Tamir Duberstein <tamird@google.com>
2021-11-30 19:40:37 +0000

    [extract-pprof] format-code
    - Apply python formatting
    - Remove unused import
    
    Change-Id: I6027fb2b250fa4bb93576f2d42337c5d5608fc6c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/610801
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Peter Johnston <peterjohnston@google.com>

--- tools/devshell/contrib/extract-pprof.py
+++ tools/devshell/contrib/extract-pprof.py
@@ -6,53 +6,54 @@ import argparse
 import base64
 import json
 import os
-import sys
 import tempfile
 
 
 def main():
-  parser = argparse.ArgumentParser(
-      description="Extract pprof data from inspect.json")
-  parser.add_argument(
-      "--inspect", required=True, help="Path to inspect.json file")
-  parser.add_argument(
-      "--component",
-      required=True,
-      help="Component to extract pprof data for (i.e. netstack)")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Extract pprof data from inspect.json")
+    parser.add_argument(
+        "--inspect", required=True, help="Path to inspect.json file")
+    parser.add_argument(
+        "--component",
+        required=True,
+        help="Component to extract pprof data for (e.g. core/network/netstack)")
+    args = parser.parse_args()
 
-  tempdir = tempfile.mkdtemp()
+    tempdir = tempfile.mkdtemp()
 
-  print("inspect.json @ %s" % (args.inspect))
-  print("component = %s" % (args.component))
-  print("output @ %s" % (tempdir))
+    print("inspect.json @ %s" % args.inspect)
+    print("component = %s" % args.component)
+    print("output @ %s" % tempdir)
 
-  with open(args.inspect, "r") as f:
-    inspect = json.loads(f.read())
+    with open(args.inspect, "r") as f:
+        inspect = json.loads(f.read())
 
-  for entry in inspect:
-    if entry["data_source"] != "Inspect" or entry["moniker"] != args.component:
-      continue
+    for entry in inspect:
+        if entry["data_source"] != "Inspect":
+            continue
+        if entry["moniker"] != args.component:
+            continue
 
-    filepath = entry["metadata"]["filename"]
-    # We only care about pprof data.
-    if not filepath.startswith("pprof"):
-      continue
+        filepath = entry["metadata"]["filename"]
+        # We only care about pprof data.
+        if not filepath.startswith("pprof"):
+            continue
 
-    filepath = os.path.join(tempdir, filepath)
-    os.makedirs(filepath, exist_ok=True)
+        filepath = os.path.join(tempdir, filepath)
+        os.makedirs(filepath, exist_ok=True)
 
-    data = entry["payload"]["root"]["pprof"]
-    for k, v in data.items():
-      # The first 4 characters are used to indicate that the file is a
-      # base64 encoded file.
-      prefix, v = v[:4], v[4:]
-      if prefix != "b64:":
-        print("%s at %s is not base64 encoded" % (k, filepath))
+        data = entry["payload"]["root"]["pprof"]
+        for k, v in data.items():
+            # The first 4 characters are used to indicate that the file is a
+            # base64 encoded file.
+            prefix, v = v[:4], v[4:]
+            if prefix != "b64:":
+                print("%s at %s is not base64 encoded" % (k, filepath))
 
-      with open(os.path.join(filepath, k), "wb") as f:
-        f.write(base64.b64decode(v))
+            with open(os.path.join(filepath, k), "wb") as f:
+                f.write(base64.b64decode(v))
 
 
 if __name__ == "__main__":
-  main()
+    main()
