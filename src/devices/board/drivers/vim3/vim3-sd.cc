274f2ae791f45e6801c14eb833611835e2b1e12d
Braden Kell <bradenkell@google.com>
2021-12-02 18:11:34 +0000

    DO NOT SUBMIT: Bind WLAN driver on vim3 SD and switch to new tuning
    Change-Id: I1c9f4dd8047b92c7ce500ffbbef636dc977b54ad

--- src/devices/board/drivers/vim3/vim3-sd.cc
+++ src/devices/board/drivers/vim3/vim3-sd.cc
@@ -42,7 +42,8 @@ static aml_sdmmc_config_t config = {
     .min_freq = 400'000,
     .max_freq = 50'000'000,
     .version_3 = true,
-    .prefs = 0,
+    .prefs = 0x1000'0000,  // Magic number to detect the SD slot.
+    .use_new_tuning = true,
 };
 
 static const pbus_metadata_t sd_metadata[] = {
@@ -51,6 +52,37 @@ static const pbus_metadata_t sd_metadata[] = {
         .data_buffer = reinterpret_cast<const uint8_t*>(&config),
         .data_size = sizeof(config),
     },
+};
+
+static const zx_bind_inst_t sdio_fn1_match[] = {
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_SDIO),
+    BI_ABORT_IF(NE, BIND_SDIO_VID, 0x02d0),
+    BI_ABORT_IF(NE, BIND_SDIO_FUNCTION, 1),
+    BI_MATCH_IF(EQ, BIND_SDIO_PID, 0x4345),
+};
+static const zx_bind_inst_t sdio_fn2_match[] = {
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_SDIO),
+    BI_ABORT_IF(NE, BIND_SDIO_VID, 0x02d0),
+    BI_ABORT_IF(NE, BIND_SDIO_FUNCTION, 2),
+    BI_MATCH_IF(EQ, BIND_SDIO_PID, 0x4345),
+};
+static const zx_bind_inst_t oob_gpio_match[] = {
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_GPIO),
+    BI_MATCH_IF(EQ, BIND_GPIO_PIN, A311D_GPIOC(6)),  // CD pin
+};
+static const device_fragment_part_t sdio_fn1_fragment[] = {
+    {std::size(sdio_fn1_match), sdio_fn1_match},
+};
+static const device_fragment_part_t sdio_fn2_fragment[] = {
+    {std::size(sdio_fn2_match), sdio_fn2_match},
+};
+static const device_fragment_part_t oob_gpio_fragment[] = {
+    {std::size(oob_gpio_match), oob_gpio_match},
+};
+static const device_fragment_t wifi_fragments[] = {
+    {"sdio-function-1", std::size(sdio_fn1_fragment), sdio_fn1_fragment},
+    {"sdio-function-2", std::size(sdio_fn2_fragment), sdio_fn2_fragment},
+    {"gpio-oob", std::size(oob_gpio_fragment), oob_gpio_fragment},
 };
 
 zx_status_t Vim3::SdInit() {
@@ -76,9 +108,38 @@ zx_status_t Vim3::SdInit() {
   gpio_impl_.SetAltFunction(A311D_GPIOC(3), A311D_GPIOC_3_SDCARD_D3_FN);
   gpio_impl_.SetAltFunction(A311D_GPIOC(4), A311D_GPIOC_4_SDCARD_CLK_FN);
   gpio_impl_.SetAltFunction(A311D_GPIOC(5), A311D_GPIOC_5_SDCARD_CMD_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(6), 0);
+
+  gpio_impl_.SetDriveStrength(A311D_GPIOC(0), 4000, nullptr);
+  gpio_impl_.SetDriveStrength(A311D_GPIOC(1), 4000, nullptr);
+  gpio_impl_.SetDriveStrength(A311D_GPIOC(2), 4000, nullptr);
+  gpio_impl_.SetDriveStrength(A311D_GPIOC(3), 4000, nullptr);
+  gpio_impl_.SetDriveStrength(A311D_GPIOC(4), 4000, nullptr);
+  gpio_impl_.SetDriveStrength(A311D_GPIOC(5), 4000, nullptr);
 
   if ((status = pbus_.CompositeDeviceAdd(&sd_dev, /* nullptr */ 0, 0, nullptr)) != ZX_OK) {
     zxlogf(ERROR, "SdInit could not add sd_dev: %d", status);
+    return status;
+  }
+  constexpr zx_device_prop_t props[] = {
+      {BIND_PLATFORM_DEV_VID, 0, PDEV_VID_BROADCOM},
+      {BIND_PLATFORM_DEV_PID, 0, PDEV_PID_BCM4356},
+      {BIND_PLATFORM_DEV_DID, 0, PDEV_DID_BCM_WIFI},
+  };
+
+  const composite_device_desc_t comp_desc = {
+      .props = props,
+      .props_count = countof(props),
+      .fragments = wifi_fragments,
+      .fragments_count = countof(wifi_fragments),
+      .primary_fragment = "sdio-function-1",
+      .spawn_colocated = true,
+      .metadata_list = nullptr,
+      .metadata_count = 0,
+  };
+
+  if ((status = DdkAddComposite("wifi", &comp_desc)) != ZX_OK) {
+    zxlogf(ERROR, "%s: device_add_composite failed: %d", __func__, status);
     return status;
   }
 
