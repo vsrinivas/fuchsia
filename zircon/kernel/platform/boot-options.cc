f59ca9d7e6c959ee81282103c440717908d6f0d2
Joshua Seaton <joshuaseaton@google.com>
2021-12-04 01:11:10 +0000

    [kernel] Re-enable boot option warnings
    Since the beginning of the boot option migration, we have been
    suppressing warnings like
    ```
    WARN: Kernel ignored unrecognized option 'kernel.foo.bar'
    ```
    This was spurious then, as BootOptions recognized very few of the
    options. Now it does, however - and we only have a single option left to
    add support for: "kernel.ram.reserve.test". Since this option is only
    set in core-tests.zbi, it seems beneficial to re-enable now-relevant
    warnings, at the expense of adding one spurious one in a niche setting.
    
    Bug: 53594
    Change-Id: Id2c3924ce883a91db6be55d1b55e2d5de83b8492
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/614101
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/platform/boot-options.cc
+++ zircon/kernel/platform/boot-options.cc
@@ -7,28 +7,15 @@
 #include <lib/boot-options/boot-options.h>
 #include <lib/cmdline.h>
 #include <platform.h>
-#include <stdio.h>
 #include <zircon/assert.h>
 
 namespace {
-
-// TODO(53594): A no-op destination for spurious warnings for the duration of
-// the migration.
-struct NullFile {
-  int Write(ktl::string_view s) { return static_cast<int>(s.size()); }
-};
 
 BootOptions gBootOptionsInstance;
 
 }  // namespace
 
 void ParseBootOptions(ktl::string_view cmdline) {
-  // TODO(53594): Set `complain` to `stdout` once we have migrated all
-  // cmdline arguments to BootOptions; until then, we suppress spurious
-  // complaints about unrecognized options.
-  NullFile null;
-  FILE complain{&null};
-
   // Because we don't know if we have looked at the zbi entries yet, the best thing is
   // to recheck the zbi if no entry in the command line is found, whenever the serial is being
   // parsed.
@@ -36,7 +23,7 @@ void ParseBootOptions(ktl::string_view cmdline) {
     gBootOptionsInstance.serial_source = OptionSource::kZbi;
   }
 
-  gBootOptionsInstance.SetMany(cmdline, &complain);
+  gBootOptionsInstance.SetMany(cmdline);
 
   // Note: it is intentional that we build up `gBootOptions` before
   // `gCmdline`, as the former can redact information of which we do not
