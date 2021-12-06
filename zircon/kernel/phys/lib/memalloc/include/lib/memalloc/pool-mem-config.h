2b92efc8be0d83398cda5a9a1cabee539ad74503
Roland McGrath <mcgrathr@google.com>
2021-12-06 21:11:27 +0000

    [kernel][phys] Move PoolMemConfig converter into memalloc
    The boot_shim::PoolMemConfigItem is now based on a generic
    general memalloc::PoolMemConfig conversion pseudo-container.
    
    Bug: 88119
    Change-Id: I298a6311c43a4475a8c98c6fad308c79df780c99
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/614205
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

---
+++ zircon/kernel/phys/lib/memalloc/include/lib/memalloc/pool-mem-config.h
@@ -0,0 +1,110 @@
+// Copyright 2021 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_PHYS_LIB_MEMALLOC_INCLUDE_LIB_MEMALLOC_POOL_MEM_CONFIG_H_
+#define ZIRCON_KERNEL_PHYS_LIB_MEMALLOC_INCLUDE_LIB_MEMALLOC_POOL_MEM_CONFIG_H_
+
+#include <zircon/boot/image.h>
+
+#include <algorithm>
+
+#include "pool.h"
+
+namespace memalloc {
+
+// memalloc::PoolMemConfig wraps reference to memalloc::Pool in a
+// forward-iterable container-like type that yields zbi_mem_range_t.
+// The resulting table coalesces adjacent ranges whose types reduce to
+// the same basic type.
+
+class PoolMemConfig {
+ public:
+  PoolMemConfig(const PoolMemConfig& other) = default;
+
+  explicit PoolMemConfig(const Pool& pool) : pool_(pool) {}
+
+  class iterator {
+   public:
+    using value_type = zbi_mem_range_t;
+    using reference = value_type&;
+    using pointer = value_type*;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::forward_iterator_tag;
+
+    value_type operator*() const {
+      return {
+          .paddr = first_->addr,
+          .length = size_,
+          .type = ReduceType(*first_),
+      };
+    }
+
+    bool operator==(const iterator& other) const { return first_ == other.first_; }
+    bool operator!=(const iterator& other) const { return first_ != other.first_; }
+
+    iterator& operator++() {  // prefix
+      first_ = ++last_;
+      size_ = 0;
+      Coalesce();
+      return *this;
+    }
+
+    iterator operator++(int) {  // postfix
+      iterator old = *this;
+      ++*this;
+      return old;
+    }
+
+   private:
+    friend PoolMemConfig;
+
+    static constexpr uint32_t ReduceType(const Range& range) {
+      return memalloc::IsExtendedType(range.type)  // Reduce to basic types.
+                 ? ZBI_MEM_RANGE_RAM
+                 : static_cast<uint32_t>(range.type);
+    }
+
+    void Coalesce() {
+      while (last_ != end_) {
+        size_ += last_->size;
+        Pool::iterator next = std::next(last_);
+        if (next == end_ || next->addr != last_->addr + last_->size ||
+            ReduceType(*next) != ReduceType(*last_)) {
+          break;
+        }
+        last_ = next;
+      }
+    }
+
+    Pool::iterator first_, last_, end_;
+    uint64_t size_ = 0;
+  };
+
+  // No size() method is provided because it's O(n).  Use std::distance.
+
+  bool empty() const { return begin() == end(); }
+
+  iterator begin() const {
+    iterator it;
+    it.first_ = it.last_ = pool_.begin();
+    it.end_ = pool_.end();
+    it.Coalesce();
+    return it;
+  }
+
+  iterator end() const {
+    iterator it;
+    it.first_ = it.last_ = it.end_ = pool_.end();
+    return it;
+  }
+
+ private:
+  const Pool& pool_;
+};
+
+}  // namespace memalloc
+
+#endif  // ZIRCON_KERNEL_PHYS_LIB_MEMALLOC_INCLUDE_LIB_MEMALLOC_POOL_MEM_CONFIG_H_
---
+++ zircon/kernel/phys/lib/memalloc/pool-mem-config-test.cc
@@ -0,0 +1,96 @@
+// Copyright 2021 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <lib/memalloc/pool-mem-config.h>
+#include <lib/memalloc/pool.h>
+
+#include <forward_list>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+constexpr uint64_t kChunkSize = memalloc::Pool::kBookkeepingChunkSize;
+
+// The pool uses a callback to convert "physical" address regions allocated for
+// bookkeeping space to accessible pointers.  The callback for testing purposes
+// just ignores that "physical" address and allocates a new chunk for the Pool
+// code to store its bookkeeping data in.
+memalloc::Pool::BookkeepingAddressToPointer ForTestPool() {
+  // The list in the (move-only) lambda just keeps the pointers all live for
+  // the lifetime of the Pool (which owns the wrapped and returned lambda).
+  using Bookkeeping = std::forward_list<std::unique_ptr<std::byte[]>>;
+  return [bookkeeping = Bookkeeping()](uint64_t addr, uint64_t size) mutable {
+    std::byte* chunk = new std::byte[size];  // Uninitialized space.
+    bookkeeping.emplace_front(chunk);
+    return chunk;
+  };
+}
+
+TEST(MemallocPoolMemConfigTests, Empty) {
+  memalloc::Pool pool(ForTestPool());
+
+  // Initialize with an empty pool.
+  memalloc::PoolMemConfig mem_config(pool);
+  EXPECT_TRUE(mem_config.empty());
+  EXPECT_EQ(mem_config.begin(), mem_config.end());
+}
+
+TEST(MemallocPoolMemConfigTests, Ranges) {
+  memalloc::Pool pool(ForTestPool());
+
+  memalloc::Range test_pool_ranges[] = {
+      {
+          .addr = 0,
+          .size = kChunkSize * 1000,
+          .type = memalloc::Type::kFreeRam,
+      },
+      {
+          .addr = kChunkSize * 50,
+          .size = kChunkSize * 2,
+          .type = memalloc::Type::kReserved,
+      },
+  };
+  EXPECT_TRUE(pool.Init(std::array{cpp20::span<memalloc::Range>(test_pool_ranges)}).is_ok());
+  auto alloc_result = pool.Allocate(memalloc::Type::kPoolTestPayload, kChunkSize * 100);
+  ASSERT_TRUE(alloc_result.is_ok());
+  EXPECT_EQ(kChunkSize * 52, alloc_result.value());
+
+  constexpr zbi_mem_range_t kExpectedZbiRanges[] = {
+      {
+          .paddr = 0,
+          .length = kChunkSize * 50,
+          .type = ZBI_MEM_RANGE_RAM,
+      },
+      {
+          .paddr = kChunkSize * 50,
+          .length = kChunkSize * 2,
+          .type = ZBI_MEM_RANGE_RESERVED,
+      },
+      {
+          .paddr = kChunkSize * 52,
+          .length = kChunkSize * 948,
+          .type = ZBI_MEM_RANGE_RAM,
+      },
+  };
+
+  memalloc::PoolMemConfig mem_config(pool);
+
+  EXPECT_EQ(std::size(kExpectedZbiRanges),
+            static_cast<size_t>(std::distance(mem_config.begin(), mem_config.end())));
+
+  auto it = mem_config.begin();
+  for (const zbi_mem_range_t& expected : kExpectedZbiRanges) {
+    ASSERT_NE(it, mem_config.end());
+    const zbi_mem_range_t range = *it++;
+    EXPECT_EQ(expected.paddr, range.paddr);
+    EXPECT_EQ(expected.length, range.length);
+    EXPECT_EQ(expected.type, range.type);
+  }
+  EXPECT_EQ(it, mem_config.end());
+}
+
+}  // namespace
