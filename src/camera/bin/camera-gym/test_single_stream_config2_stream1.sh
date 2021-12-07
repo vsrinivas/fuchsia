8c7118645ef5648323b7e43b999c768a60d514de
Ernest Hua <ernesthua@google.com>
2021-12-07 05:41:45 +0000

    [camera][camera-gym] Sample single config single stream scripts
    Bug: b/196411805
    Test: Run each script.
    Change-Id: I348c3246577ccaefd135c648170ee77a476e764f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/568401
    Fuchsia-Auto-Submit: Ernest Hua <ernesthua@google.com>
    Reviewed-by: John Sasinowski <jsasinowski@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/camera/bin/camera-gym/test_single_stream_config2_stream1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Testing the camera-gym-ctl:
+#
+# This sequence assumes:
+#
+# 1. "fx serve-remote ..." or equivalent is running.
+# 2. "fx vendor google camera-gym-manual" is started.
+
+count=`fx shell ps | grep camera-gym-manual | wc -l`
+if [ ${count} != 1 ]
+then
+  echo 'ERROR: Manual instance of camera-gym not detected'
+  exit 1
+fi
+
+fx shell camera-gym-ctl --set-config=2 --add-stream=1
