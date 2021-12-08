a8464313ffdf7b81ecc56214878ed094421e988f
Kevin Lindkvist <lindkvist@google.com>
2021-12-08 12:18:38 +0000

    [starnix] Add support for VIRTIO_MAGMA_CMD_DEVICE_IMPORT
    Change-Id: I2f238a9920ec55e257bf5b933f01875d585914cd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/613372
    Reviewed-by: Craig Stout <cstout@google.com>
    Commit-Queue: Kevin Lindkvist <lindkvist@google.com>

--- src/proc/bin/starnix/device/magma_file.rs
+++ src/proc/bin/starnix/device/magma_file.rs
@@ -4,9 +4,14 @@
 
 #![allow(non_upper_case_globals)]
 
+use fuchsia_zircon as zx;
+use fuchsia_zircon::AsHandleRef;
+use magma::*;
+use parking_lot::Mutex;
 use std::sync::Arc;
 
 use super::magma::*;
+use crate::errno;
 use crate::error;
 use crate::fd_impl_nonblocking;
 use crate::fd_impl_nonseekable;
@@ -22,11 +27,13 @@ impl FsNodeOps for MagmaNode {
     }
 }
 
-pub struct MagmaFile {}
+pub struct MagmaFile {
+    channel: Arc<Mutex<Option<zx::Channel>>>,
+}
 
 impl MagmaFile {
     pub fn new() -> Result<Box<dyn FileOps>, Errno> {
-        Ok(Box::new(Self {}))
+        Ok(Box::new(Self { channel: Arc::new(Mutex::new(None)) }))
     }
 }
 
@@ -43,9 +50,32 @@ impl FileOps for MagmaFile {
         _out_addr: UserAddress,
     ) -> Result<SyscallResult, Errno> {
         let (command, command_type) = read_magma_command_and_type(task, in_addr)?;
-        let _response_address = UserAddress::from(command.response_address);
+        let response_address = UserAddress::from(command.response_address);
 
         match command_type {
+            virtio_magma_ctrl_type_VIRTIO_MAGMA_CMD_DEVICE_IMPORT => {
+                let (_control, mut response): (
+                    virtio_magma_device_import_ctrl_t,
+                    virtio_magma_device_import_resp_t,
+                ) = read_control_and_response(task, &command)?;
+
+                let (client_channel, server_channel) =
+                    zx::Channel::create().map_err(|_| errno!(EINVAL))?;
+                fdio::service_connect(&"/dev/class/gpu/000", server_channel)
+                    .map_err(|_| errno!(EINVAL))?;
+
+                let device_channel = client_channel.raw_handle();
+                *self.channel.lock() = Some(client_channel);
+
+                let mut device_out: usize = 0;
+                response.result_return = unsafe {
+                    magma_device_import(device_channel, &mut device_out as *mut usize) as u64
+                };
+
+                response.device_out = device_out;
+                response.hdr.type_ = virtio_magma_ctrl_type_VIRTIO_MAGMA_RESP_DEVICE_IMPORT as u32;
+                task.mm.write_object(UserRef::new(response_address), &response)
+            }
             t => {
                 log::warn!("Got unknown request: {:?}", t);
                 error!(ENOSYS)
