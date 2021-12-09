8a3273b2f4e717c4eb5894e44b8be3bff878142a
Tamir Duberstein <tamird@google.com>
2021-12-09 01:15:35 +0000

    [rust_crates] Unfork combine
    Fixed: 81590
    Change-Id: I7f14aaf5b03c5ead333185551bd670f294ac73a0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/616043
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ third_party/rust_crates/vendor/combine/travis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+set -ex
+
+if [[ "$TRAVIS_RUST_VERSION" == "1.40.0" ]]; then
+    cargo "$@" check
+    cargo "$@" check --no-default-features
+else
+    cargo "$@" build
+    cargo "$@" test --all-features
+    cargo "$@" test --all-features --examples
+
+    cargo "$@" test --bench json --bench http -- --test
+    cargo "$@" check --bench mp4 --features mp4
+    
+    cargo "$@" build --no-default-features --features alloc
+    cargo "$@" test --no-default-features --features alloc --examples
+
+    cargo "$@" build --no-default-features
+    cargo "$@" test --no-default-features --examples
+
+    cargo "$@" check --no-default-features --features tokio-02
+    cargo "$@" check --no-default-features --features tokio-03
+fi
+
+if [[ "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+    cargo doc
+fi
