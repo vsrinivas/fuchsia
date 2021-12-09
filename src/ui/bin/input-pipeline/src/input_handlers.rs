79badea9bdcb4b155ca56c8bf7b9427921c77336
Filip Filmar <fmil@google.com>
2021-12-09 21:40:01 +0000

    [input] Removes the modifier handler from the input-pipeline component
    The component is not used yet, so this removals should not be consequential.
    However, it is worth noting that any potential users of this component at this time
    will *not* have modifiers or lock states reported *at all*.
    
    Per request from quiche@ at fxr/613624
    
    Change-Id: I39728ae4e45ed9786d9083e9680dbf888ee35309
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/615926
    Reviewed-by: Mukesh Agrawal <quiche@google.com>
    Commit-Queue: Filip Filmar <fmil@google.com>
    Fuchsia-Auto-Submit: Filip Filmar <fmil@google.com>

--- src/ui/bin/input-pipeline/src/input_handlers.rs
+++ src/ui/bin/input-pipeline/src/input_handlers.rs
@@ -7,7 +7,7 @@ use {
     input_pipeline as input_pipeline_lib,
     input_pipeline_lib::{
         factory_reset_handler::FactoryResetHandler, media_buttons_handler::MediaButtonsHandler,
-        modifier_handler::ModifierHandler, touch_injector_handler::TouchInjectorHandler, Size,
+        touch_injector_handler::TouchInjectorHandler, Size,
     },
     std::rc::Rc,
 };
@@ -18,13 +18,7 @@ pub async fn create(
 ) -> Vec<Rc<dyn input_pipeline_lib::input_handler::InputHandler>> {
     let scenic = connect_to_protocol::<fidl_fuchsia_ui_scenic::ScenicMarker>()
         .expect("Failed to connect to Scenic.");
-    let modifier_handler = ModifierHandler::new();
-    vec![
-        modifier_handler,
-        factory_reset_handler,
-        media_buttons_handler,
-        make_touch_injector_handler(&scenic).await,
-    ]
+    vec![factory_reset_handler, media_buttons_handler, make_touch_injector_handler(&scenic).await]
 }
 
 async fn make_touch_injector_handler(
