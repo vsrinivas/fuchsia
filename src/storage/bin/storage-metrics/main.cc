fd61cd76fab6b26b4cf95767a0bc3bddc70d0f1d
Brandon Castellano <bcastell@google.com>
2021-12-10 03:13:03 +0000

    [storage-metrics] Typed channel migration
    Bug: 69585
    Bug: 89814
    Test: None: No functional change.
    Change-Id: Ief9b3a86e8abf3af2cc672676ef90bb9be95118c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/616651
    Reviewed-by: Stephen Demos <sdemos@google.com>
    Fuchsia-Auto-Submit: Brandon Castellano <bcastell@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/storage/bin/storage-metrics/main.cc
+++ src/storage/bin/storage-metrics/main.cc
@@ -4,13 +4,12 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <fidl/fuchsia.hardware.block/cpp/wire.h>
 #include <fidl/fuchsia.io.admin/cpp/wire.h>
 #include <fidl/fuchsia.minfs/cpp/wire.h>
-#include <fuchsia/hardware/block/c/fidl.h>
-#include <fuchsia/minfs/c/fidl.h>
-#include <fuchsia/storage/metrics/c/fidl.h>
 #include <getopt.h>
 #include <lib/fdio/cpp/caller.h>
+#include <lib/service/llcpp/service.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -66,79 +65,66 @@ void PrintFsMetrics(const MinfsFidlMetrics& metrics, const char* path) {
 }
 
 // Sends a FIDL call to enable or disable filesystem metrics for path
-zx_status_t EnableFsMetrics(const char* path, bool enable) {
-  fbl::unique_fd fd(open(path, O_RDONLY | O_DIRECTORY));
-  if (!fd) {
-    fprintf(stderr, "Error opening %s, errno %d (%s)\n", path, errno, strerror(errno));
-    return ZX_ERR_IO;
+zx::status<> EnableFsMetrics(const char* path, bool enable) {
+  auto client_end = service::Connect<fuchsia_minfs::Minfs>(path);
+  if (!client_end.is_ok()) {
+    return client_end.take_error();
   }
+  fidl::WireSyncClient client(std::move(client_end.value()));
 
-  zx_status_t status;
-  fdio_cpp::FdioCaller caller(std::move(fd));
-  zx_status_t rc = fuchsia_minfs_MinfsToggleMetrics(caller.borrow_channel(), enable, &status);
-  if (rc != ZX_OK || status != ZX_OK) {
-    fprintf(stderr, "Error toggling metrics for %s, status %d\n", path,
-            (rc != ZX_OK) ? rc : status);
-    return (rc != ZX_OK) ? rc : status;
+  auto result = client->ToggleMetrics(enable);
+  zx_status_t status = !result.ok() ? result.status() : result.value().status;
+  if (status != ZX_OK) {
+    fprintf(stderr, "Error toggling metrics for %s: %s\n", path, zx_status_get_string(status));
+    return zx::error(status);
   }
-  return status;
+  return zx::ok();
 }
 
 // Retrieves the Filesystem metrics for path. Only supports Minfs.
-zx_status_t GetFsMetrics(const char* path, MinfsFidlMetrics* out_metrics) {
-  fbl::unique_fd fd(open(path, O_RDONLY | O_DIRECTORY));
-  if (!fd) {
-    fprintf(stderr, "Error opening %s, errno %d (%s)\n", path, errno, strerror(errno));
-    return ZX_ERR_IO;
+zx::status<MinfsFidlMetrics> GetFsMetrics(const char* path) {
+  auto client_end = service::Connect<fuchsia_minfs::Minfs>(path);
+  if (!client_end.is_ok()) {
+    return client_end.take_error();
   }
+  fidl::WireSyncClient client(std::move(client_end.value()));
 
-  fdio_cpp::FdioCaller caller(std::move(fd));
-  auto result = fidl::WireCall<fuchsia_minfs::Minfs>(caller.channel())->GetMetrics();
-  if (!result.ok()) {
-    fprintf(stderr, "Error getting metrics for %s, status %d\n", path, result.status());
-    return result.status();
-  }
-  if (result.value().status == ZX_ERR_UNAVAILABLE) {
-    fprintf(stderr, "Metrics Unavailable for %s\n", path);
-    return result.status();
-  }
-  if (result.value().status != ZX_OK) {
-    fprintf(stderr, "Error getting metrics for %s, status %d\n", path, result.value().status);
-    return result.value().status;
+  auto result = client->GetMetrics();
+  zx_status_t status = !result.ok() ? result.status() : result.value().status;
+  if (status != ZX_OK) {
+    fprintf(stderr, "Error getting metrics for %s: %s\n", path, zx_status_get_string(status));
+    return zx::error(status);
   }
   if (!result.value().metrics) {
     fprintf(stderr, "Error getting metrics for %s, returned metrics was null\n", path);
-    return ZX_ERR_INTERNAL;
+    return zx::error(ZX_ERR_INTERNAL);
   }
-  *out_metrics = *result.value().metrics;
-  return ZX_OK;
+  return zx::ok(*result.value().metrics);
 }
 
-void PrintBlockMetrics(const char* dev, const fuchsia_hardware_block_BlockStats& stats) {
+void PrintBlockMetrics(const char* dev, const fuchsia_hardware_block::wire::BlockStats& stats) {
   printf("Block Metrics for device path: %s\n", dev);
   storage_metrics::BlockDeviceMetrics metrics(&stats);
   metrics.Dump(stdout);
 }
 
 // Retrieves metrics for the block device at dev. Clears metrics if clear is true.
-zx_status_t GetBlockMetrics(const char* dev, bool clear, fuchsia_hardware_block_BlockStats* stats) {
-  fbl::unique_fd fd(open(dev, O_RDONLY));
-  if (!fd) {
-    fprintf(stderr, "Error opening %s, errno %d (%s)\n", dev, errno, strerror(errno));
-    return ZX_ERR_IO;
+zx::status<fuchsia_hardware_block::wire::BlockStats> GetBlockStats(const char* dev, bool clear) {
+  auto client_end = service::Connect<fuchsia_hardware_block::Block>(dev);
+  if (!client_end.is_ok()) {
+    return client_end.take_error();
   }
-  fdio_cpp::FdioCaller caller(std::move(fd));
-  zx_status_t status;
-  zx_status_t io_status =
-      fuchsia_hardware_block_BlockGetStats(caller.borrow_channel(), clear, &status, stats);
-  if (io_status != ZX_OK) {
-    status = io_status;
-  }
+  fidl::WireSyncClient client(std::move(client_end.value()));
+
+  auto result = client->GetStats(clear);
+  zx_status_t status = !result.ok() ? result.error().status() : result.value().status;
+
   if (status != ZX_OK) {
-    fprintf(stderr, "Error getting stats for %s\n", dev);
-    return status;
+    fprintf(stderr, "Error getting stats for %s: %s\n", dev, zx_status_get_string(status));
+    return zx::error(status);
   }
-  return ZX_OK;
+
+  return zx::ok(*result.value().stats);
 }
 
 void ParseCommandLineArguments(int argc, char** argv, StorageMetricOptions* options) {
@@ -168,33 +154,31 @@ void ParseCommandLineArguments(int argc, char** argv, StorageMetricOptions* opti
 
 // Retrieves filesystem metrics for the filesystem at path and prints them.
 void RunFsMetrics(const char* path, const StorageMetricOptions options) {
-  zx_status_t rc;
   // The order of these conditionals allows for stats to be output regardless of the
   // value of enable.
   if (options.enable_fs_metrics == BooleanFlagState::kEnable) {
-    rc = EnableFsMetrics(path, true);
-    if (rc != ZX_OK) {
-      fprintf(stderr, "storage-metrics could not enable filesystem metrics for %s, status %d\n",
-              path, rc);
+    zx::status<> rc = EnableFsMetrics(path, true);
+    if (!rc.is_ok()) {
+      fprintf(stderr, "storage-metrics could not enable filesystem metrics for %s: %s\n", path,
+              rc.status_string());
       return;
     }
   }
-  MinfsFidlMetrics metrics;
-  rc = GetFsMetrics(path, &metrics);
-  if (rc == ZX_OK) {
-    PrintFsMetrics(metrics, path);
+  zx::status<MinfsFidlMetrics> metrics = GetFsMetrics(path);
+  if (metrics.is_ok()) {
+    PrintFsMetrics(*metrics, path);
   } else {
     fprintf(stderr,
-            "storage-metrics could not get filesystem metrics for %s, status %d. This may mean "
+            "storage-metrics could not get filesystem metrics for %s: %s. This may mean "
             "that it is not a minfs file system.\n",
-            path, rc);
+            path, metrics.status_string());
     return;
   }
   if (options.enable_fs_metrics == BooleanFlagState::kDisable) {
-    rc = EnableFsMetrics(path, false);
-    if (rc != ZX_OK) {
-      fprintf(stderr, "storage-metrics could not disable filesystem metrics for %s, status %d\n",
-              path, rc);
+    zx::status<> rc = EnableFsMetrics(path, false);
+    if (!rc.is_ok()) {
+      fprintf(stderr, "storage-metrics could not disable filesystem metrics for %s: %s\n", path,
+              rc.status_string());
     }
   }
 }
@@ -217,31 +201,27 @@ void RunBlockMetrics(const char* path, const StorageMetricOptions options) {
                                    caller.borrow_channel()))
                     ->GetDevicePath();
 
-  zx_status_t rc;
-  fuchsia_hardware_block_BlockStats stats;
   if (result.ok()) {
     std::string device_path(result.value().path.get());
-    rc = GetBlockMetrics(device_path.data(), options.clear_block, &stats);
-    if (rc == ZX_OK) {
-      PrintBlockMetrics(device_path.data(), stats);
+    zx::status<fuchsia_hardware_block::wire::BlockStats> block_stats =
+        GetBlockStats(device_path.data(), options.clear_block);
+    if (block_stats.is_ok()) {
+      PrintBlockMetrics(device_path.data(), *block_stats);
     } else {
-      fprintf(stderr,
-              "storage-metrics could not retrieve block metrics for %s,"
-              " status %d\n",
-              path, rc);
+      fprintf(stderr, "storage-metrics could not retrieve block metrics for %s: %s\n", path,
+              block_stats.status_string());
     }
   } else {
     // Maybe this is not a filesystem. See if this happens to be a block device.
     // TODO(auradkar): We need better args parsing to consider fs and block
     // device seperately.
-    rc = GetBlockMetrics(path, options.clear_block, &stats);
-    if (rc != ZX_OK) {
-      fprintf(stderr,
-              "storage-metrics could not retrieve block metrics for %s,"
-              " status %d\n",
-              path, rc);
+    zx::status<fuchsia_hardware_block::wire::BlockStats> block_stats =
+        GetBlockStats(path, options.clear_block);
+    if (!block_stats.is_ok()) {
+      fprintf(stderr, "storage-metrics could not retrieve block metrics for %s: %s\n", path,
+              block_stats.status_string());
     }
-    PrintBlockMetrics(path, stats);
+    PrintBlockMetrics(path, *block_stats);
   }
 }
 
