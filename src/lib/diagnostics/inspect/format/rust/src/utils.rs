182935127245bfcb7baa8d3bf6deab6d9a2f52ed
Christopher R. Johnson <crjohns@google.com>
2021-12-10 19:34:42 +0000

    [inspect] Add some tests for util functions.
    Change-Id: Ie4f5030b2c0a76d60673e4e05e17d26195055be5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/617343
    Fuchsia-Auto-Submit: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Clayton McCray <claytonmccray@google.com>
    Commit-Queue: Christopher Johnson <crjohns@google.com>

--- src/lib/diagnostics/inspect/format/rust/src/utils.rs
+++ src/lib/diagnostics/inspect/format/rust/src/utils.rs
@@ -10,9 +10,9 @@ use std::cmp::{max, min};
 /// Returns the smallest order such that (MIN_ORDER_SHIFT << order) >= size.
 /// Size must be non zero.
 pub fn fit_order(size: usize) -> usize {
-    std::mem::size_of::<usize>() * 8
-        - (size - 1).leading_zeros().to_usize().unwrap()
-        - constants::MIN_ORDER_SHIFT
+    (std::mem::size_of::<usize>() * 8 - (size - 1).leading_zeros().to_usize().unwrap())
+        .checked_sub(constants::MIN_ORDER_SHIFT)
+        .unwrap_or(0)
 }
 
 /// Get index in the VMO for a given |offset|.
@@ -52,4 +52,87 @@ pub fn array_capacity(order: usize, entry_type: BlockType) -> Option<usize> {
             - constants::ARRAY_PAYLOAD_METADATA_SIZE_BYTES)
             / size
     })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn fit_order_test() {
+        assert_eq!(0, fit_order(1));
+        assert_eq!(0, fit_order(16));
+        assert_eq!(1, fit_order(17));
+        assert_eq!(2, fit_order(33));
+        assert_eq!(7, fit_order(2048));
+    }
+
+    #[test]
+    fn order_to_size_test() {
+        assert_eq!(16, order_to_size(0));
+        assert_eq!(32, order_to_size(1));
+        assert_eq!(64, order_to_size(2));
+        assert_eq!(128, order_to_size(3));
+        assert_eq!(256, order_to_size(4));
+        assert_eq!(512, order_to_size(5));
+        assert_eq!(1024, order_to_size(6));
+        assert_eq!(2048, order_to_size(7));
+    }
+
+    #[test]
+    fn fit_payload_test() {
+        for payload_size in 0..500 {
+            let block_size = block_size_for_payload(payload_size);
+            let order = fit_order(block_size);
+            let payload_max = payload_size_for_order(order);
+            assert!(
+                payload_size <= payload_max,
+                "Needed {} bytes for a payload, but only got {}; block size {}, order {}",
+                payload_size,
+                payload_max,
+                block_size,
+                order
+            );
+        }
+    }
+
+    #[test]
+    fn array_capacity_numeric() {
+        assert_eq!(2, array_capacity(1, BlockType::IntValue).expect("get size"));
+        assert_eq!(2 + 4, array_capacity(2, BlockType::IntValue).expect("get size"));
+        assert_eq!(2 + 4 + 8, array_capacity(3, BlockType::IntValue).expect("get size"));
+        assert_eq!(2 + 4 + 8 + 16, array_capacity(4, BlockType::IntValue).expect("get size"));
+        assert_eq!(2 + 4 + 8 + 16 + 32, array_capacity(5, BlockType::IntValue).expect("get size"));
+        assert_eq!(
+            2 + 4 + 8 + 16 + 32 + 64,
+            array_capacity(6, BlockType::IntValue).expect("get size")
+        );
+        assert_eq!(
+            2 + 4 + 8 + 16 + 32 + 64 + 128,
+            array_capacity(7, BlockType::IntValue).expect("get size")
+        );
+    }
+
+    #[test]
+    fn array_capacity_string_reference() {
+        assert_eq!(4, array_capacity(1, BlockType::StringReference).expect("get size"));
+        assert_eq!(4 + 8, array_capacity(2, BlockType::StringReference).expect("get size"));
+        assert_eq!(4 + 8 + 16, array_capacity(3, BlockType::StringReference).expect("get size"));
+        assert_eq!(
+            4 + 8 + 16 + 32,
+            array_capacity(4, BlockType::StringReference).expect("get size")
+        );
+        assert_eq!(
+            4 + 8 + 16 + 32 + 64,
+            array_capacity(5, BlockType::StringReference).expect("get size")
+        );
+        assert_eq!(
+            4 + 8 + 16 + 32 + 64 + 128,
+            array_capacity(6, BlockType::StringReference).expect("get size")
+        );
+        assert_eq!(
+            4 + 8 + 16 + 32 + 64 + 128 + 256,
+            array_capacity(7, BlockType::StringReference).expect("get size")
+        );
+    }
 }
