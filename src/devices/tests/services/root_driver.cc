61e0a4ee0623b058a5bd71156479966dc9342d1c
Abdulla Kamar <abdulla@google.com>
2021-12-10 22:01:40 +0000

    [fdf] Add DFv2 unified services integration test
    This integration test verifies that a driver can expose a unified
    service and it can be used by another component.
    
    Fixed: 89409
    Bug: 33183
    Change-Id: I6d1641301fed747a1335fc272ae859e2084b1a35
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/610246
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Abdulla Kamar <abdulla@google.com>
    Fuchsia-Auto-Submit: Abdulla Kamar <abdulla@google.com>

---
+++ src/devices/tests/services/root_driver.cc
@@ -0,0 +1,100 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/fuchsia.driver.framework/cpp/wire.h>
+#include <fidl/fuchsia.services.test/cpp/wire.h>
+#include <lib/service/llcpp/outgoing_directory.h>
+
+#include "src/devices/lib/driver2/logger.h"
+#include "src/devices/lib/driver2/namespace.h"
+#include "src/devices/lib/driver2/record_cpp.h"
+
+namespace fdf = fuchsia_driver_framework;
+namespace fio = fuchsia_io;
+namespace ft = fuchsia_services_test;
+
+namespace {
+
+class RootDriver : public fidl::WireServer<ft::ControlPlane>,
+                   public fidl::WireServer<ft::DataPlane> {
+ public:
+  RootDriver(async_dispatcher_t* dispatcher, fidl::WireSharedClient<fdf::Node> node,
+             driver::Namespace ns, driver::Logger logger)
+      : dispatcher_(dispatcher),
+        outgoing_(dispatcher),
+        node_(std::move(node)),
+        ns_(std::move(ns)),
+        logger_(std::move(logger)) {}
+
+  static constexpr const char* Name() { return "root"; }
+
+  static zx::status<std::unique_ptr<RootDriver>> Start(fdf::wire::DriverStartArgs& start_args,
+                                                       async_dispatcher_t* dispatcher,
+                                                       fidl::WireSharedClient<fdf::Node> node,
+                                                       driver::Namespace ns,
+                                                       driver::Logger logger) {
+    auto driver =
+        std::make_unique<RootDriver>(dispatcher, std::move(node), std::move(ns), std::move(logger));
+    auto result = driver->Run(std::move(start_args.outgoing_dir()));
+    if (result.is_error()) {
+      return result.take_error();
+    }
+    return zx::ok(std::move(driver));
+  }
+
+ private:
+  zx::status<> Run(fidl::ServerEnd<fio::Directory> outgoing_dir) {
+    service::ServiceHandler handler;
+    ft::Device::Handler device(&handler);
+
+    auto control = [this](fidl::ServerEnd<ft::ControlPlane> server_end) -> zx::status<> {
+      fidl::BindServer<fidl::WireServer<ft::ControlPlane>>(dispatcher_, std::move(server_end),
+                                                           this);
+      return zx::ok();
+    };
+    auto result = device.add_control(control);
+    if (result.is_error()) {
+      FDF_LOG(ERROR, "Failed to add control handler to Device service: %s", result.status_string());
+      return result.take_error();
+    }
+
+    auto data = [this](fidl::ServerEnd<ft::DataPlane> server_end) -> zx::status<> {
+      fidl::BindServer<fidl::WireServer<ft::DataPlane>>(dispatcher_, std::move(server_end), this);
+      return zx::ok();
+    };
+    result = device.add_data(data);
+    if (result.is_error()) {
+      FDF_LOG(ERROR, "Failed to add data handler to Device service: %s", result.status_string());
+      return result.take_error();
+    }
+
+    result = outgoing_.AddService<ft::Device>(std::move(handler));
+    if (result.is_error()) {
+      FDF_LOG(ERROR, "Failed to add Device service: %s", result.status_string());
+      return result.take_error();
+    }
+    return outgoing_.Serve(std::move(outgoing_dir));
+  }
+
+  // fidl::WireServer<ft::ControlPlane>
+  void ControlDo(ControlDoRequestView request, ControlDoCompleter::Sync& completer) override {
+    completer.Reply();
+  }
+
+  // fidl::WireServer<ft::DataPlane>
+  void DataDo(DataDoRequestView request, DataDoCompleter::Sync& completer) override {
+    completer.Reply();
+  }
+
+  async_dispatcher_t* dispatcher_;
+  service::OutgoingDirectory outgoing_;
+
+  fidl::WireSharedClient<fdf::Node> node_;
+  driver::Namespace ns_;
+  driver::Logger logger_;
+};
+
+}  // namespace
+
+FUCHSIA_DRIVER_RECORD_CPP_V1(RootDriver);
---
+++ src/devices/tests/services/test.rs
@@ -0,0 +1,69 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{Context, Result},
+    fidl::endpoints::ServiceMarker,
+    fidl_fuchsia_driver_test as fdt, fidl_fuchsia_services_test as ft,
+    fuchsia_async::{self as fasync, DurationExt, Timer},
+    fuchsia_component::client,
+    fuchsia_component_test::{RealmBuilder, RouteBuilder, RouteEndpoint},
+    fuchsia_driver_test::{DriverTestRealmBuilder, DriverTestRealmInstance},
+    fuchsia_zircon::DurationNum,
+};
+
+#[fasync::run_singlethreaded(test)]
+async fn test_services() -> Result<()> {
+    // Create the RealmBuilder.
+    let builder = RealmBuilder::new().await?;
+    builder
+        .driver_test_realm_manifest_setup("#meta/realm.cm")
+        .await?
+        .add_route(
+            RouteBuilder::service(ft::DeviceMarker::SERVICE_NAME)
+                .source(RouteEndpoint::component("driver_test_realm"))
+                .targets(vec![RouteEndpoint::AboveRoot]),
+        )
+        .await?;
+    // Build the Realm.
+    let realm = builder.build().await?;
+    // Start the DriverTestRealm.
+    let args = fdt::RealmArgs {
+        root_driver: Some("#meta/root.cm".to_string()),
+        use_driver_framework_v2: Some(true),
+        ..fdt::RealmArgs::EMPTY
+    };
+    realm.driver_test_realm_start(args).await?;
+
+    // Find an instance of the `Device` service.
+    let instance;
+    let service = client::open_service_at_dir::<ft::DeviceMarker>(realm.root.get_exposed_dir())
+        .context("Failed to open service")?;
+    loop {
+        // TODO(fxbug.dev/4776): Once component manager supports watching for
+        // service instances, this loop shousld be replaced by a watcher.
+        let entries =
+            files_async::readdir(&service).await.context("Failed to read service instances")?;
+        if let Some(entry) = entries.iter().next() {
+            instance = entry.name.clone();
+            break;
+        }
+        Timer::new(100.millis().after_now()).await;
+    }
+
+    // Connect to the `Device` service.
+    let device = client::connect_to_service_instance_at_dir::<ft::DeviceMarker>(
+        realm.root.get_exposed_dir(),
+        &instance,
+    )
+    .context("Failed to open service")?;
+    // Use the `ControlPlane` protocol from the `Device` service.
+    let control = device.control()?;
+    control.control_do().await?;
+    // Use the `DataPlane` protocol from the `Device` service.
+    let data = device.data()?;
+    data.data_do().await?;
+
+    Ok(())
+}
