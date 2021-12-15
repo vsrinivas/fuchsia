c78a1936548b3325f5d91fa7a96baf91fd0fe446
Benjamin Prosnitz <bprosnitz@google.com>
2021-12-15 16:09:34 +0000

    [fidl_driver] Create fidl_driver library and use it
    This creates a shell for a fidl_driver library which contains code at
    the intersection of the driver runtime and LLCPP. It then makes a basic
    change to use a new wire_messaging.h header in the fidl_driver runtime
    library for drivers, ratehr than the LLCPP wire_messaging.h header.
    
    Change-Id: I86c24c6ff750a5c813d7e025e9ddcfe18908aa93
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/619827
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Reviewed-by: Pascal Perez <pascallouis@google.com>
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>

---
+++ sdk/lib/fidl_driver/include/lib/fidl/driver/wire_messaging.h
@@ -0,0 +1,10 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_FIDL_DRIVER_INCLUDE_LIB_FIDL_DRIVER_WIRE_MESSAGING_H_
+#define LIB_FIDL_DRIVER_INCLUDE_LIB_FIDL_DRIVER_WIRE_MESSAGING_H_
+
+#include <lib/fidl/llcpp/wire_messaging.h>
+
+#endif  // LIB_FIDL_DRIVER_INCLUDE_LIB_FIDL_DRIVER_WIRE_MESSAGING_H_
