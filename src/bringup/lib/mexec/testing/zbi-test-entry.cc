c0a88da2a3cdc4ed7548f0bb0e99cc4db267beca
Joshua Seaton <joshuaseaton@google.com>
2021-12-15 01:27:25 +0000

    [zbi_test] Do not pass userboot.shutdown by default
    Such options should be explicit - and only a minority of ZBI tests
    require this particular one (i.e., those that depend on userboot to
    print the success string).
    
    This option silently introduces a race condition into the
    bootsvc-root-job-test ZBI test. The point of the test is to assert on
    bootsvc's role as a "critical" process, causing the root job to exit
    when it does. The test logic runs as follows:
    (1) the kernel's RootJobObserver is configured to print the ZBI test
    success string on root job exit via `kernel.root-job.notice`;
    (2) bootsvc is purposefully crashed;
    (3) the root-job should then exit as a result;
    (4) the RootJobObserver should then print the success string.
    
    However, `userboot.shutdown` introduces a race between (3)-(4) and
    userboot itself reacting to bootsvc's non-zero exit by powering off the
    system (in which case no success string would be printed).
    
    Moreover, passing this option by default covered up a bug in
    `mexec_zbi_test()`'s child-ZBI-loading code: it assumed that the child
    ZBI always had more than one item. (For phys ZBI tests, we only intend
    to pass a single kernel item, but the presence of `userboot.shutdown`
    resulted in a second CMDLINE item.)
    
    Bug: 90078
    Change-Id: I061bbf0a9fa7bc78bba41977805829308afa77a6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/618023
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>

--- src/bringup/lib/mexec/testing/zbi-test-entry.cc
+++ src/bringup/lib/mexec/testing/zbi-test-entry.cc
@@ -5,7 +5,6 @@
 #include <fcntl.h>
 #include <lib/fdio/io.h>
 #include <lib/zbitl/error-stdio.h>
-#include <lib/zbitl/image.h>
 #include <lib/zbitl/view.h>
 #include <lib/zbitl/vmo.h>
 #include <lib/zx/vmo.h>
@@ -24,7 +23,7 @@ constexpr const char* kMexecZbi = "/boot/testdata/mexec-child.zbi";
 int main() {
   fbl::unique_fd fd{open(kMexecZbi, O_RDONLY)};
   if (!fd) {
-    printf("failed to open %s: %s\n", kMexecZbi, strerror(errno));
+    printf("zbi-mexec-test-entry: failed to open child ZBI %s: %s\n", kMexecZbi, strerror(errno));
     return ZX_ERR_INTERNAL;
   }
 
@@ -33,12 +32,26 @@ int main() {
     zx::vmo vmo;
     if (zx_status_t status = fdio_get_vmo_exact(fd.get(), vmo.reset_and_get_address());
         status != ZX_OK) {
-      printf("failed get child ZBI's VMO: %s\n", zx_status_get_string(status));
+      printf("zbi-mexec-test-entry: failed get child ZBI's VMO: %s\n",
+             zx_status_get_string(status));
       return status;
     }
-    zbitl::View view(std::move(vmo));
 
-    if (auto result = view.Copy(view.begin(), ++view.begin()); result.is_error()) {
+    zbitl::View view(std::move(vmo));
+    if (view.begin() == view.end()) {
+      if (auto result = view.take_error(); result.is_error()) {
+        printf("zbi-mexec-test-entry: invalid child ZBI: ");
+        zbitl::PrintViewError(result.error_value());
+      } else {
+        printf("zbi-mexec-test-entry: empty child ZBI\n");
+      }
+      return ZX_ERR_INTERNAL;
+    }
+
+    auto first = view.begin();
+    auto second = std::next(first);
+    if (auto result = view.Copy(first, second); result.is_error()) {
+      printf("zbi-mexec-test-entry: failed to copy out kernel payload: ");
       zbitl::PrintViewCopyError(result.error_value());
       view.ignore_error();
       return ZX_ERR_INTERNAL;
@@ -46,7 +59,8 @@ int main() {
       kernel_zbi = std::move(result).value();
     }
 
-    if (auto result = view.Copy(++view.begin(), view.end()); result.is_error()) {
+    if (auto result = view.Copy(second, view.end()); result.is_error()) {
+      printf("zbi-mexec-test-entry: failed to copy out data ZBI: ");
       zbitl::PrintViewCopyError(result.error_value());
       view.ignore_error();
       return ZX_ERR_INTERNAL;
@@ -55,6 +69,7 @@ int main() {
     }
 
     if (auto result = view.take_error(); result.is_error()) {
+      printf("zbi-mexec-test-entry: ZBI iteration failure: ");
       zbitl::PrintViewError(result.error_value());
       return ZX_ERR_INTERNAL;
     }
@@ -62,19 +77,20 @@ int main() {
 
   zx::resource resource{zx_take_startup_handle(PA_HND(PA_RESOURCE, 0))};
   if (!resource.is_valid()) {
-    printf("unable to get a hold of the root resource\n");
+    printf("zbi-mexec-test-entry: unable to get a hold of the root resource\n");
     return ZX_ERR_INTERNAL;
   }
 
   if (zx_status_t status = mexec::PrepareDataZbi(resource.borrow(), data_zbi.borrow());
       status != ZX_OK) {
-    return status;
+    printf("zbi-mexec-test-entry: failed to prepare data ZBI: %s\n", zx_status_get_string(status));
+    return ZX_ERR_INTERNAL;
   }
 
   if (zx_status_t status =
           zx_system_mexec(resource.release(), kernel_zbi.release(), data_zbi.release());
       status != ZX_OK) {
-    printf("zx_system_mexec(): %s\n", zx_status_get_string(status));
+    printf("zbi-mexec-test-entry: zx_system_mexec(): %s\n", zx_status_get_string(status));
     return ZX_ERR_INTERNAL;
   }
   return ZX_OK;
