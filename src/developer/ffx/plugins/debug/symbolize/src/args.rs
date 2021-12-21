cef3bd9a5913ca80c778e1fb2f28ab0a667a6d7b
Dangyi Liu <dangyi@google.com>
2021-12-21 20:38:45 +0000

    [ffx][debug] New subcommand "symbolize"
    It should function the same as "fx symbolize".
    
    Fixed: 88848
    Change-Id: I3167c739cc52de4fdfa09ccf7bd8106bb57cc21b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/619086
    Reviewed-by: Matt Boetger <boetger@google.com>
    Commit-Queue: Dangyi Liu <dangyi@google.com>

---
+++ src/developer/ffx/plugins/debug/symbolize/src/args.rs
@@ -0,0 +1,15 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {argh::FromArgs, ffx_core::ffx_command};
+
+/// Options for "ffx debug symbolize".
+#[ffx_command()]
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "symbolize", description = "symbolize a log")]
+pub struct SymbolizeCommand {
+    /// extra arguments passed to the symbolizer. Any arguments starting with "-" must be after a "--" separator.
+    #[argh(positional)]
+    pub symbolizer_args: Vec<String>,
+}
---
+++ src/developer/ffx/plugins/debug/symbolize/src/lib.rs
@@ -0,0 +1,26 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {
+    anyhow::{anyhow, Result},
+    fuchsia_async::unblock,
+    std::process::Command,
+};
+
+#[ffx_core::ffx_plugin()]
+pub async fn symbolize(cmd: ffx_debug_symbolize_args::SymbolizeCommand) -> Result<i32> {
+    if let Err(e) = symbol_index::ensure_symbol_index_registered().await {
+        eprintln!("ensure_symbol_index_registered failed, error was: {:#?}", e);
+    }
+
+    let symbolizer_path = ffx_config::get_sdk().await?.get_host_tool("symbolizer")?;
+
+    let mut cmd = Command::new(symbolizer_path).args(cmd.symbolizer_args).spawn()?;
+
+    if let Some(exit_code) = unblock(move || cmd.wait()).await?.code() {
+        Ok(exit_code)
+    } else {
+        Err(anyhow!("symbolizer terminated by signal"))
+    }
+}
