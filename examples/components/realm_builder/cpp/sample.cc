e35fe10391f3d5f6b2d9ffb4e7fbe3478c42cb52
Yaneury Fermin <yaneury@google.com>
2022-01-05 18:22:06 +0000

    [lsc][examples] Migrate to new Realm Builder API
    This CL migrates usages of Realm Builder to the newer API
    as proposed in RFC 140.
    
    The current strategy is to first house this new library
    under the `sys::testing::experimental` namespace.
    Then, all current clients will be migrated to from the old API
    to this new one. Afterwards, the old API will be deleted. Finally,
    all usages will be upgrated to the `sys::testing` namespace.
    
    Bug: 90794
    Change-Id: I25d2a51d57d9eb8c8aa3adafca3dfbb567cb0c57
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/624686
    Reviewed-by: Derek Gonyeo <dgonyeo@google.com>
    Commit-Queue: Yaneury Fermin <yaneury@google.com>

--- examples/components/realm_builder/cpp/sample.cc
+++ examples/components/realm_builder/cpp/sample.cc
@@ -23,9 +23,9 @@ using namespace sys::testing;
 // [END use_namespace_cpp]
 
 // [START mock_component_impl_cpp]
-class MockEchoServerImpl : public fidl::examples::routing::echo::Echo, public MockComponent {
+class LocalEchoServerImpl : public fidl::examples::routing::echo::Echo, public LocalComponent {
  public:
-  explicit MockEchoServerImpl(async::Loop& loop) : loop_(loop) {}
+  explicit LocalEchoServerImpl(async::Loop& loop) : loop_(loop) {}
 
   // Override `EchoString` from `Echo` protocol.
   void EchoString(::fidl::StringPtr value, EchoStringCallback callback) override {
@@ -33,22 +33,22 @@ class MockEchoServerImpl : public fidl::examples::routing::echo::Echo, public Mo
     loop_.Quit();
   }
 
-  // Override `Start` from `MockComponent` class.
-  void Start(std::unique_ptr<MockHandles> mock_handles) override {
+  // Override `Start` from `LocalComponent` class.
+  void Start(std::unique_ptr<LocalComponentHandles> handles) override {
     // Keep reference to `mock_handles` in member variable.
     // This class contains handles to the component's incoming
     // and outgoing capabilities.
-    mock_handles_ = std::move(mock_handles);
+    handles_ = std::move(handles);
 
     ASSERT_EQ(
-        mock_handles_->outgoing()->AddPublicService(bindings_.GetHandler(this, loop_.dispatcher())),
+        handles_->outgoing()->AddPublicService(bindings_.GetHandler(this, loop_.dispatcher())),
         ZX_OK);
   }
 
  private:
   async::Loop& loop_;
   fidl::BindingSet<fidl::examples::routing::echo::Echo> bindings_;
-  std::unique_ptr<MockHandles> mock_handles_;
+  std::unique_ptr<LocalComponentHandles> handles_;
 };
 // [END mock_component_impl_cpp]
 
@@ -56,56 +56,50 @@ TEST(SampleTest, CallsEcho) {
   async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
 
   // [START init_realm_builder_cpp]
-  auto builder = Realm::Builder::Create();
+  auto builder = experimental::RealmBuilder::Create();
   // [END init_realm_builder_cpp]
 
   // [START add_component_cpp]
   // Add component `a` to the realm, which is fetched using a URL.
-  builder.AddComponent(
-      Moniker{"a"},
-      Component{.source = ComponentUrl{
-                    "fuchsia-pkg://fuchsia.com/realm-builder-examples#meta/echo_client.cm"}});
+  builder.AddChild("a", "fuchsia-pkg://fuchsia.com/realm-builder-examples#meta/echo_client.cm");
   // Add component `b` to the realm, which is fetched using a relative URL.
-  builder.AddComponent(Moniker{"b"}, Component{.source = ComponentUrl{"#meta/echo_client.cm"}});
+  builder.AddChild("b", "#meta/echo_client.cm");
   // [END add_component_cpp]
 
   // [START add_legacy_component_cpp]
   // Add component `c` to the realm, which is fetched using a legacy URL.
-  builder.AddComponent(
-      Moniker{"c"},
-      Component{.source = LegacyComponentUrl{
-                    "fuchsia-pkg://fuchsia.com/realm-builder-examples#meta/echo_client.cmx"}});
+  builder.AddLegacyChild("c",
+                         "fuchsia-pkg://fuchsia.com/realm-builder-examples#meta/echo_client.cmx");
   // [END add_legacy_component_cpp]
 
   // [START add_mock_component_cpp]
-  auto mock_echo_server = MockEchoServerImpl(loop);
+  auto mock_echo_server = LocalEchoServerImpl(loop);
 
-  builder.AddComponent(Moniker{"d"}, Component{.source = Mock{&mock_echo_server}});
+  builder.AddLocalChild("d", &mock_echo_server);
   // [END add_mock_component_cpp]
 
   // [START route_between_children_cpp]
-  builder.AddRoute(CapabilityRoute{.capability = Protocol{"fidl.examples.routing.echo.Echo"},
-                                   .source = Moniker{"d"},
-                                   .targets = {Moniker{"a"}, Moniker{"b"}, Moniker{"c"}}});
+  builder.AddRoute(Route{.capabilities = {Protocol{"fidl.examples.routing.echo.Echo"}},
+                         .source = ChildRef{"d"},
+                         .targets = {ChildRef{"a"}, ChildRef{"b"}, ChildRef{"c"}}});
   // [END route_between_children_cpp]
 
   // [START route_to_test_cpp]
-  builder.AddRoute(CapabilityRoute{.capability = Protocol{"fidl.examples.routing.echo.Echo"},
-                                   .source = Moniker{"d"},
-                                   .targets = {AboveRoot()}});
+  builder.AddRoute(Route{.capabilities = {Protocol{"fidl.examples.routing.echo.Echo"}},
+                         .source = ChildRef{"d"},
+                         .targets = {ParentRef()}});
   // [END route_to_test_cpp]
 
   // [START route_from_test_cpp]
-  builder.AddRoute(
-      CapabilityRoute{.capability = Protocol{"fuchsia.logger.LogSink"},
-                      .source = AboveRoot(),
-                      .targets = {Moniker{"a"}, Moniker{"b"}, Moniker{"c"}, Moniker{"d"}}});
+  builder.AddRoute(Route{.capabilities = {Protocol{"fuchsia.logger.LogSink"}},
+                         .source = ParentRef(),
+                         .targets = {ChildRef{"a"}, ChildRef{"b"}, ChildRef{"c"}, ChildRef{"d"}}});
   // [END route_from_test_cpp]
 
   // [START route_from_test_sibling_cpp]
-  builder.AddRoute(CapabilityRoute{.capability = Protocol{"fuchsia.example.Foo"},
-                                   .source = AboveRoot(),
-                                   .targets = {Moniker{"a"}}});
+  builder.AddRoute(Route{.capabilities = {Protocol{"fuchsia.example.Foo"}},
+                         .source = ParentRef(),
+                         .targets = {ChildRef{"a"}}});
   // [END route_from_test_sibling_cpp]
 
   // [START build_realm_cpp]
