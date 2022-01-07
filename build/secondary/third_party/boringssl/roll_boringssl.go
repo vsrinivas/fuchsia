b3a5840bf7bdb99563d5f01eb1594ac85000771c
Nathan Mulcahey <nmulcahey@google.com>
2022-01-07 23:40:53 +0000

    [boringssl] Unnest boringssl [1/3]
    1) Check the Fuchsia specific files for boringssl into fuchsia.git under
    //build/secondary. This allows us to remove the repository that
    containts them from the checkout in step #2.
    
    TODO:
    2) Remove //third_party/boringssl and //third_party/boringssl/src from
    the checkout. The latter will immediately be superseded by the same
    content at the same location in the checkout (`jiri` will do a delete +
    create)
    3) Move the fuchsia specific files for boringssl into
    //third_party/boringssl as part of fuchsia.git
    
    This CL also removes a number of unnecessary files (LICENSE, AUTHORS,
    PATENTS) and changes the path in GN files for the source files moved to
    //build/secondary to be absolute as GN does not search in
    //build/secondary for the relative paths.
    
    Bug: b/189325238
    Bug: 59863
    Change-Id: Icefcabaf415fa004504bfbaea5275b909209b9fa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/628243
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Aaron Green <aarongreen@google.com>
    Commit-Queue: Nathan Mulcahey <nmulcahey@google.com>

---
+++ build/secondary/third_party/boringssl/roll_boringssl.go
@@ -0,0 +1,133 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// This script updates //third_party/boringssl/src to point to the current revision at:
+//   https://boringssl.googlesource.com/boringssl/+/master
+//
+// It also updates the generated build files, Rust bindings, and subset of code used in Zircon.
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+// Returns the path to the boringssl directory.
+func configure() string {
+	log.Println("Configuring...")
+	_, file, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatal("failed to find current executable")
+	}
+	return filepath.Dir(file)
+}
+
+// Updates BoringSSL sources and returns the revision.
+func updateSources(dir, commit string) []byte {
+	log.Println("Updating BoringSSL sources...")
+	dir = filepath.Join(dir, "src")
+	{
+		cmd := exec.Command("git", "-C", dir, "fetch", "--all", "--prune")
+		if err := cmd.Run(); err != nil {
+			log.Fatalf("%s failed: %s", cmd.Args, err)
+		}
+	}
+	{
+		cmd := exec.Command("git", "-C", dir, "checkout", commit)
+		if err := cmd.Run(); err != nil {
+			log.Fatalf("%s failed: %s", cmd.Args, err)
+		}
+	}
+	{
+		cmd := exec.Command("git", "-C", dir, "rev-parse", commit)
+		out, err := cmd.Output()
+		if err != nil {
+			log.Fatalf("%s failed: %s", cmd.Args, err)
+		}
+		return bytes.TrimSpace(out)
+	}
+}
+
+// Create the GN build files for the current sources.
+func generateGN(dir string) {
+	log.Printf("Generating build files...")
+	cmd := exec.Command("python", filepath.Join("src", "util", "generate_build_files.py"), "gn")
+	cmd.Dir = dir
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("%s failed: %s", cmd.Args, err)
+	}
+}
+
+// Regenerates the Rust bindings.
+func generateRustBindings(dir string) {
+	log.Printf("Generating Rust bindings...")
+	cmd := exec.Command(filepath.Join("rust", "boringssl-sys", "bindgen.sh"))
+	cmd.Dir = dir
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("%s failed: %s", cmd.Args, err)
+	}
+}
+
+// Updates the README file that ends with the current upstream git revision.
+func updateReadMe(dir string, sha1 []byte) {
+	const readmeName = "README.fuchsia"
+	log.Printf("Updating %s...", readmeName)
+	readme, err := os.OpenFile(filepath.Join(dir, readmeName), os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatalf("failed to open %s: %s", readmeName, err)
+	}
+	defer func() {
+		if err := readme.Close(); err != nil {
+			log.Fatalf("failed to close %s: %s", readmeName, err)
+		}
+	}()
+
+	// Assume that the file ends with a git URL.
+	info, err := readme.Stat()
+	if err != nil {
+		log.Fatalf("failed to stat %s: %s", readmeName, err)
+	}
+	offset := info.Size() - int64(len(sha1)+2 /* trailing newlines */)
+	if _, err = readme.WriteAt(sha1, offset); err != nil {
+		log.Fatalf("failed to write to %s: %s", readmeName, err)
+	}
+}
+
+func main() {
+	commit := flag.String("commit", "origin/upstream/master", "Upstream commit-ish to check out")
+
+	flag.Parse()
+
+	log.SetFlags(log.Lmicroseconds)
+
+	dir := configure()
+	sha1 := updateSources(dir, *commit)
+
+	log.Printf("Commit resolved to %s", sha1)
+
+	generateGN(dir)
+	generateRustBindings(dir)
+	updateReadMe(dir, sha1)
+
+	log.Println()
+	log.Println("To test, run:")
+	log.Println("  $ fx set ... --with //third_party/boringssl:tests --args=run_slow_bssl_tests=true")
+	log.Println("  $ fx build")
+	log.Println("  $ fx serve")
+	log.Println("  $ fx test boringssl_tests")
+	log.Println()
+	log.Println("To verify Zircon linkage (see zircon-unused.c):")
+	log.Println("  $ fx --dir out/bringup.x64.no_opt set bringup.x64 --args=optimize='\"none\"'")
+	log.Println("  $ fx --dir out/bringup.x64.no_opt build")
+	log.Println("  $ fx use default")
+
+	log.Println("If tests pass; commit the changes in //third_party/boringssl (you may need to bypass the CQ)")
+	log.Println("Then, update the BoringSSL revision in the internal integration repository")
+}
---
+++ build/secondary/third_party/boringssl/rust/boringssl-sys/bindgen.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+# Hard-coded paths
+readonly LIBC="$FUCHSIA_DIR/zircon/third_party/ulib/musl"
+readonly BSSL="../../src"
+if [[ -z "$FUCHSIA_DIR" ]] ; then
+  echo "FUCHSIA_DIR not set."
+  exit 1
+elif [[ ! -d $LIBC ]] ; then
+  echo "Can't find libc."
+  exit 1
+elif [[ ! -d $BSSL ]] ; then
+  echo "Can't find BoringSSL."
+  exit 1
+fi
+
+# NOTE(joshlf): We pin to a particular version of bindgen since updates
+# sometimes change the semantics of the generated bindings (for example, by
+# changing the Rust types that are generated for particular C types). If a more
+# recent version of bindgen is available, "roll" bindgen by updating the
+# `BINDGEN_EXPECTED_VERSION` variable here.
+BINDGEN_EXPECTED_VERSION="bindgen 0.58.1"
+BINDGEN_GOT_VERSION="$(bindgen --version)"
+if [ "$BINDGEN_GOT_VERSION" != "$BINDGEN_EXPECTED_VERSION" ]; then
+    echo "Unexpected version of bindgen: got $BINDGEN_GOT_VERSION; wanted $BINDGEN_EXPECTED_VERSION.
+If a newer version is available, edit this script to pin to that version." >&2
+    exit 1
+fi
+
+# Go to the directory this script lives in
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+# Construct a header file which imports every BoringSSL header.
+for header in $(ls $BSSL/include/openssl/); do
+    # Skip certain headers which contain platform-specific logic, and will not
+    # compile on all platforms.
+    if [[ "$header" != "arm_arch.h" && \
+          "$header" != "lhash_macros.h" ]]; then
+        echo "#include <openssl/${header}>" >> bindgen.h
+    fi
+done
+
+# TODO(joshlf):
+# - Use the --use-core flag once std isn't required (see
+#   https://github.com/rust-lang-nursery/rust-bindgen/issues/1015)
+
+# Whitelist BoringSSL-related symbols so we don't get non-BoringSSL symbols such
+# as platform-specific symbols (specific to the platform that is running
+# 'bindgen') and other C standard library symbols. The whitelist is broken into
+# two sections: symbols with a prefix followed by an underscore and symbols with
+# no following characters, such as the `RC4` function.
+WHITELIST="((AES|ERR|BIO|CRYPTO|RAND|V_ASN1|ASN1|B_ASN1|CBS_ASN1|CAST|EVP|CBS|CBB|CIPHER|OPENSSL|SSLEAY|DH|DES|DIGEST|DSA|NID|EC|ECDSA|ECDH|ED25519|X25519|PKCS5_PBKDF2|SHA|SHA1|SHA224|SHA256|SHA384|SHA512|HMAC|RSA|BN|RC4|MD5)_.*)|(RC4)$"
+# NOTE(joshlf) on --target: Currently, we just pass x86_64 since none of the
+# symbols we're linking against are architecture-specific (they may be
+# word-size-specific, but Fuchsia only targets 64-bit platforms). If this ever
+# becomes a problem, then the thing to do is probably to generate different
+# files for different platforms (bindgen_x86_64.rs, bindgen_arm64.rs, etc) and
+# conditionally compile them depending on target.
+bindgen bindgen.h \
+    --whitelist-function "$WHITELIST" \
+    --whitelist-type "$WHITELIST" \
+    --whitelist-var "$WHITELIST" \
+    -o src/lib.rs -- \
+    -I $BSSL/include \
+    -I $LIBC/include \
+    --target=x86_64-fuchsia
+
+TMP="$(mktemp)"
+
+# Prepend copyright comment, #[allow] for various warnings we don't care about,
+# and a line telling Rust to link against libcrypto.
+cat >> "$TMP" <<EOF
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+#[link(name = "crypto")] extern {}
+
+EOF
+
+cat src/lib.rs >> "$TMP"
+mv "$TMP" src/lib.rs
+rustfmt src/lib.rs
+
+rm bindgen.h
