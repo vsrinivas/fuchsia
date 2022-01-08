6f830eae24e1095659dce01faa55f295cecae455
C Stout <cstout@google.com>
2022-01-08 00:38:43 +0000

    [virtualization] Remove minigbm from termina VM build script
    Now using mesa gbm instead.
    
    Change-Id: I980319b6fb57bae76fc420766a64801def18d226
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/627925
    Reviewed-by: David Reveman <reveman@google.com>
    Commit-Queue: Craig Stout <cstout@google.com>

--- src/virtualization/packages/termina_guest/update_cipd_prebuilts.sh
+++ src/virtualization/packages/termina_guest/update_cipd_prebuilts.sh
@@ -72,15 +72,9 @@ create_cros_tree() {
   (cd src/overlays; git checkout cros/test-magma)
   (cd src/third_party/chromiumos-overlay; git checkout cros/test-magma)
   (cd src/platform2; git checkout cros/test-magma)
-  (cd src/platform/minigbm; git checkout cros/test-magma)
 
   # Apply patches to termina temporarily
   (cd src/platform/tremplin; git fetch https://chromium.googlesource.com/chromiumos/platform/tremplin refs/changes/34/3302234/1 && git cherry-pick FETCH_HEAD)
-
-  # Apply patches to minigbm temporarily
-  (cd src/platform/minigbm; git fetch https://chromium.googlesource.com/chromiumos/platform/minigbm refs/changes/72/3276672/13 && git cherry-pick FETCH_HEAD)
-  (cd src/platform/minigbm; git fetch https://chromium.googlesource.com/chromiumos/platform/minigbm refs/changes/36/3276636/4 && git cherry-pick FETCH_HEAD)
-  (cd src/platform/minigbm; git fetch https://chromium.googlesource.com/chromiumos/platform/minigbm refs/changes/88/3276688/4 && git cherry-pick FETCH_HEAD)
 
   # Switch to Fuchsia's mesa branch
   (cd src/third_party/mesa && \
@@ -164,9 +158,6 @@ build_termina_image() {
 
   # Switch to source build of magma
   cros_sdk bash -c "cros_workon --board=${board} start magma"
-
-  # Switch to source build of minigbm
-  cros_sdk bash -c "cros_workon --board=${board} start minigbm"
 
   # Switch to source build of tremplin
   cros_sdk bash -c "cros_workon --board=${board} start tremplin"
