edcd3cc47db9df371885bb72833891d82d2caacb
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2022-01-09 02:11:04 +0000

    [archivist] Asynchronously drain task containing finished log/batch iterator connections
    Previously this stream wouldn't be drained until a few other futures
    were completed which wouldn't happen until a archivist shutdown.
    
    We'd store Task<()> objects indefinitely even after the BatchIterator,
    Log and ArchiveAccessor servers had stopped serving.
    
    This CL demonstrates how this approach was broken:
    https://fuchsia-review.googlesource.com/c/fuchsia/+/628594
    
    Bug: 90973
    Multiply: archivist-unittests
    Multiply: archivist-integration-tests-v2
    
    Change-Id: I22ee4b2a7eb01cb0f3b054e129fd190505a04449
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/628595
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/diagnostics/archivist/src/archivist.rs
+++ src/diagnostics/archivist/src/archivist.rs
@@ -60,9 +60,6 @@ pub struct Archivist {
     /// receiver must close for `Archivist::run` to return gracefully.
     log_sender: mpsc::UnboundedSender<Task<()>>,
 
-    /// Receiver for stream which will process Log connections.
-    listen_receiver: mpsc::UnboundedReceiver<Task<()>>,
-
     /// Sender which is used to close the stream of Log connections after log_sender
     /// completes.
     ///
@@ -86,6 +83,9 @@ pub struct Archivist {
 
     /// Tasks that drains klog.
     _drain_klog_task: Option<fasync::Task<()>>,
+
+    /// Task draining the receiver for the `listen_sender`s.
+    drain_listeners_task: fasync::Task<()>,
 }
 
 impl Archivist {
@@ -133,13 +133,15 @@ impl Archivist {
             archivist_state,
             log_receiver,
             log_sender,
-            listen_receiver,
             listen_sender,
             event_source_registry: EventSourceRegistry::new(events_node),
             stop_recv: None,
             _consume_own_logs_task: None,
             _lifecycle_task: None,
             _drain_klog_task: None,
+            drain_listeners_task: fasync::Task::spawn(async move {
+                listen_receiver.for_each_concurrent(None, |rx| async move { rx.await }).await;
+            }),
         })
     }
 
@@ -297,14 +299,14 @@ impl Archivist {
 
         // Process messages from log sink.
         let log_receiver = self.log_receiver;
-        let listen_receiver = self.listen_receiver;
+        let drain_listeners_task = self.drain_listeners_task;
         let all_msg = async {
             log_receiver.for_each_concurrent(None, |rx| async move { rx.await }).await;
             debug!("Log ingestion stopped.");
             data_repo.terminate_logs();
             logs_budget.terminate();
             debug!("Flushing to listeners.");
-            listen_receiver.for_each_concurrent(None, |rx| async move { rx.await }).await;
+            drain_listeners_task.await;
             debug!("Log listeners and batch iterators stopped.");
             component_removal_task.cancel().await;
             debug!("Not processing more component removal requests.");
