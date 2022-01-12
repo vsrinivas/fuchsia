8b75cb58472a1d8a848d10d345f12c4260a1d173
Yifei Teng <yifeit@google.com>
2022-01-12 20:31:56 +0000

    [fidl][cpp] Split out unified_messaging_declarations.h
    Over in the wire C++ bindings, the pure forward declaration parts of
    wire_messaging.h was split out to wire_messaging_declarations.h, to
    avoid some dependency cycles. Making a similar change in the unified
    bindings could untangle a future-to-be dependency cycle too.
    
    Change-Id: Ic16467164b141b4a09a7f9cc2fc58ad34779ac1d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/630424
    Reviewed-by: Benjamin Prosnitz <bprosnitz@google.com>
    Commit-Queue: Yifei Teng <yifeit@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>

--- src/lib/fidl/cpp/include/lib/fidl/cpp/unified_messaging.h
+++ src/lib/fidl/cpp/include/lib/fidl/cpp/unified_messaging.h
@@ -9,55 +9,16 @@
 #include <lib/fidl/cpp/internal/message_extensions.h>
 #include <lib/fidl/cpp/internal/natural_client_base.h>
 #include <lib/fidl/cpp/internal/natural_types.h>
+#include <lib/fidl/cpp/unified_messaging_declarations.h>
 #include <lib/fidl/llcpp/message.h>
 #include <lib/fidl/llcpp/wire_messaging.h>
 #include <lib/fitx/result.h>
 
 #include <cstdint>
 
-// This header centralizes the forward declarations for the various types in the
-// unified messaging layer. The C++ FIDL code generator populates the concrete
-// definitions from FIDL protocols in the schema.
 namespace fidl {
 
-// |Response| represents the response of a FIDL method call, using natural
-// types. See |WireResponse| for the equivalent using wire types.
-//
-// When |Method| response has a payload, |Response| will expose the following
-// operators for the user to access the payload:
-//
-//     MethodResponse& operator*();
-//     MethodResponse* operator->();
-//
-// When |Method| response has no payload, those operators will be absent.
-//
-// When |Method| has no response (one-way), this class will be undefined.
-template <typename Method>
-class Response;
-
-// |Event| represents an incoming FIDL event using natural types. See
-// |WireEvent| for the equivalent using wire types.
-//
-// When |Method| has a payload, |Event| will expose the following operators for
-// the user to access the payload:
-//
-//     EventPayload& operator*();
-//     EventPayload* operator->();
-//
-// When |Method| has no payload, those operators will be absent.
-template <typename Method>
-class Event;
-
 namespace internal {
-
-// |MessageTraits| contains information about a request or response message:
-// |Message| must be either a |fidl::Request<Foo>| or |fidl::Response<Foo>|.
-//
-// - bool kHasPayload: whether the message has a payload object. For example, a
-//                     `Foo(struct {})` has a payload that is an empty struct.
-// - Payload:          if |kHasPayload| is true, a type alias to the payload.
-template <typename Message>
-struct MessageTraits;
 
 // |MessageBase| is a mixin with common functionalities for transactional
 // message wrappers.
@@ -126,31 +87,6 @@ inline ::fitx::result<::fidl::Error> ToFitxResult(::fidl::Result result) {
   return ::fitx::error<::fidl::Error>(result);
 }
 
-// |NaturalClientImpl| implements methods for making synchronous and
-// asynchronous FIDL calls with natural types.
-//
-// All specializations of |NaturalClientImpl| should inherit from
-// |fidl::internal::NaturalClientBase|.
-template <typename Protocol>
-class NaturalClientImpl;
-
-// |ClientCallbackTraits| contains two nested definitions, that describe the
-// async callback types used in the |fidl::Client| for the FIDL method |Method|,
-// that works with natural domain objects:
-//
-// - |ResultCallback|: the callback taking a |fitx::result| type.
-// - |ResponseCallback|: the callback taking a |fidl::Response| type.
-template <typename Method>
-class ClientCallbackTraits;
-
-// |NaturalEventHandlerInterface| contains handlers for each event inside
-// the protocol |FidlProtocol|.
-template <typename FidlProtocol>
-class NaturalEventHandlerInterface;
-
-template <typename FidlProtocol>
-class NaturalEventDispatcher;
-
 }  // namespace internal
 
 // |ClientCallback| is the async callback type used in the |fidl::Client| for
@@ -174,11 +110,6 @@ using ClientCallback = typename internal::ClientCallbackTraits<Method>::ResultCa
 //
 template <typename Method>
 using ClientResponseCallback = typename internal::ClientCallbackTraits<Method>::ResponseCallback;
-
-// |AsyncEventHandler| is used by asynchronous clients to handle events using
-// natural types. It also adds a callback for handling errors.
-template <typename Protocol>
-class AsyncEventHandler;
 
 }  // namespace fidl
 
---
+++ src/lib/fidl/cpp/include/lib/fidl/cpp/unified_messaging_declarations.h
@@ -0,0 +1,90 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_LIB_FIDL_CPP_INCLUDE_LIB_FIDL_CPP_UNIFIED_MESSAGING_DECLARATIONS_H_
+#define SRC_LIB_FIDL_CPP_INCLUDE_LIB_FIDL_CPP_UNIFIED_MESSAGING_DECLARATIONS_H_
+
+#include <lib/fidl/llcpp/wire_messaging_declarations.h>
+
+#include <cstdint>
+
+// This header centralizes the forward declarations for the various types in the
+// unified messaging layer. The C++ FIDL code generator populates the concrete
+// definitions from FIDL protocols in the schema.
+namespace fidl {
+
+// |Response| represents the response of a FIDL method call, using natural
+// types. See |WireResponse| for the equivalent using wire types.
+//
+// When |Method| response has a payload, |Response| will expose the following
+// operators for the user to access the payload:
+//
+//     MethodResponse& operator*();
+//     MethodResponse* operator->();
+//
+// When |Method| response has no payload, those operators will be absent.
+//
+// When |Method| has no response (one-way), this class will be undefined.
+template <typename Method>
+class Response;
+
+// |Event| represents an incoming FIDL event using natural types. See
+// |WireEvent| for the equivalent using wire types.
+//
+// When |Method| has a payload, |Event| will expose the following operators for
+// the user to access the payload:
+//
+//     EventPayload& operator*();
+//     EventPayload* operator->();
+//
+// When |Method| has no payload, those operators will be absent.
+template <typename Method>
+class Event;
+
+namespace internal {
+
+// |MessageTraits| contains information about a request or response message:
+// |Message| must be either a |fidl::Request<Foo>| or |fidl::Response<Foo>|.
+//
+// - bool kHasPayload: whether the message has a payload object. For example, a
+//                     `Foo(struct {})` has a payload that is an empty struct.
+// - Payload:          if |kHasPayload| is true, a type alias to the payload.
+template <typename Message>
+struct MessageTraits;
+
+// |NaturalClientImpl| implements methods for making synchronous and
+// asynchronous FIDL calls with natural types.
+//
+// All specializations of |NaturalClientImpl| should inherit from
+// |fidl::internal::NaturalClientBase|.
+template <typename Protocol>
+class NaturalClientImpl;
+
+// |ClientCallbackTraits| contains two nested definitions, that describe the
+// async callback types used in the |fidl::Client| for the FIDL method |Method|,
+// that works with natural domain objects:
+//
+// - |ResultCallback|: the callback taking a |fitx::result| type.
+// - |ResponseCallback|: the callback taking a |fidl::Response| type.
+template <typename Method>
+class ClientCallbackTraits;
+
+// |NaturalEventHandlerInterface| contains handlers for each event inside
+// the protocol |FidlProtocol|.
+template <typename FidlProtocol>
+class NaturalEventHandlerInterface;
+
+template <typename FidlProtocol>
+class NaturalEventDispatcher;
+
+}  // namespace internal
+
+// |AsyncEventHandler| is used by asynchronous clients to handle events using
+// natural types. It also adds a callback for handling errors.
+template <typename Protocol>
+class AsyncEventHandler;
+
+}  // namespace fidl
+
+#endif  // SRC_LIB_FIDL_CPP_INCLUDE_LIB_FIDL_CPP_UNIFIED_MESSAGING_DECLARATIONS_H_
