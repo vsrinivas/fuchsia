28a29b1b01742c2d5bbff4b221eef2de740c41c2
Xo Wang <xow@google.com>
2022-01-12 00:22:52 +0000

    [bt][l2cap] Explicitly memoize FCS computation
    Add code to explicitly memoize the "per-octet" subroutine of ComputeFcs
    by filling a look-up table at compile time using values from
    ComputeRemainderForOctet.
    
    Update some documentation to remove references to logical LSb in the C++
    computation model since that is the opposite significance to the modulo
    math. Instead just refer to "left" or "right" and only mention LSb and
    MSb in reference to the Bluetooth spec.
    
    Fix bug in the way that unused_remainder is computed (the result is
    correct but if the previous expression were ported to uint32_t for a
    32-bit CRC then it'd produce wrong results, which is counter to the
    point of documenting the code).
    
    Bug: 91277
    Test: Refactor only. Ran bt-host tests
    Change-Id: Id3b30f4e9c0ab3247a93105c90eb6866af437806
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/629721
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Commit-Queue: Xo Wang <xow@google.com>

--- src/connectivity/bluetooth/core/bt-host/l2cap/fcs.cc
+++ src/connectivity/bluetooth/core/bt-host/l2cap/fcs.cc
@@ -6,36 +6,54 @@
 namespace bt::l2cap {
 namespace {
 
-// Subtrahend for each iteration of the linear feedback shift register (LFSR) representing the
-// polynomial D**16 + D**15 + D**2 + D**0, written in LSb-left form with the (implicit) D**16
-// coefficient omitted.
-constexpr uint16_t kPolynomial = 0b1010'0000'0000'0001;
-
-// Compute the remainder of |b| divided by |kPolynomial| in which each bit position is a mod 2
-// coefficient of a polynomial, with the least significant bit as coefficient of the greatest power.
-// This performs the same function as loading LFSR bits [8:15] (v5.0, Vol 3, Part A, Section 3.3.5,
-// Figure 3.4) with |b| bits [7:0], then operating it for eight cycles, and returns the value in the
-// register (i.e. as if the Figure 3.4 switch were set to position 2).
-//
-// Note: polynomial mod 2 arithmetic implies that addition and subtraction do not carry. Hence both
-// are equivalent to XOR, and the use of "add" or "subtract" are analogies to regular long division.
-//
-// Memoization is left up to the compiler, not explicitly declared as a lookup table.
-[[gnu::const]] constexpr uint16_t RemainderForOctet(const uint8_t b) {
-  uint16_t accumulator = b;
-  for (size_t i = 0; i < 8; i++) {
-    // Subtract the divisor if accumulator is greater than it. Polynomial mod 2 comparison only
-    // looks at the most powerful coefficient (which is the LSb).
-    const bool subtract = accumulator & 0b1;
-
-    // Because data shifts in LSb-first (Figure 3.4), this shifts in the MSb-to-LSb direction.
-    accumulator >>= 1;
-    if (subtract) {
-      accumulator ^= kPolynomial;
+// When constructed, memoizes the remainders yielded by running RemainderForOctet on each of the
+// possible octet values.
+class RemainderTable {
+ public:
+  constexpr RemainderTable() {
+    for (unsigned i = 0; i < sizeof(remainders_) / sizeof(remainders_[0]); i++) {
+      remainders_[i] = ComputeRemainderForOctet(i);
     }
   }
-  return accumulator;
-}
+
+  constexpr uint16_t GetRemainderForOctet(uint8_t b) const { return remainders_[b]; }
+
+ private:
+  // Subtrahend for each iteration of the linear feedback shift register (LFSR) representing the
+  // polynomial D**16 + D**15 + D**2 + D**0, written in LSb-left form with the (implicit) D**16
+  // coefficient omitted.
+  static constexpr uint16_t kPolynomial = 0b1010'0000'0000'0001;
+
+  // Compute the remainder of |b| divided by |kPolynomial| in which each bit position is a mod 2
+  // coefficient of a polynomial, with the rightmost bit as the coefficient of the greatest power.
+  // This performs the same function as loading LFSR bits [8:15] (v5.0, Vol 3, Part A,
+  // Section 3.3.5, Figure 3.4) with |b| bits [7:0], then operating it for eight cycles, and returns
+  // the value in the register (i.e. as if the Figure 3.4 switch were set to position 2).
+  //
+  // Note: polynomial mod 2 arithmetic implies that addition and subtraction do not carry. Hence
+  // both are equivalent to XOR, and the use of "add" or "subtract" are analogies to regular long
+  // division.
+  static constexpr uint16_t ComputeRemainderForOctet(const uint8_t b) {
+    uint16_t accumulator = b;
+    for (size_t i = 0; i < 8; i++) {
+      // Subtract the divisor if accumulator is greater than it. Polynomial mod 2 comparison only
+      // looks at the most powerful coefficient (which is the rightmost bit).
+      const bool subtract = accumulator & 0b1;
+
+      // Because data shifts in LSb-first (Figure 3.4), this shifts in the MSb-to-LSb direction,
+      // which is towards the right.
+      accumulator >>= 1;
+      if (subtract) {
+        accumulator ^= kPolynomial;
+      }
+    }
+    return accumulator;
+  }
+
+  uint16_t remainders_[1 << 8] = {};
+};
+
+constexpr RemainderTable gRemainderTable;
 
 }  // namespace
 
@@ -53,11 +71,11 @@ FrameCheckSequence ComputeFcs(BufferView view, FrameCheckSequence initial_value)
 
     // Because only the least significant eight bits are fed back into the LFSR, the other bits can
     // be shifted within the accumulation register without modification.
-    const uint8_t unused_remainder = remainder >> (8 * (sizeof(uint16_t) - 1));
+    const uint16_t unused_remainder = remainder >> 8;
 
     // Perform eight rounds of division. Add the remainder produced to the bits of the remainder
     // that we haven't used (the above shifting is associative wrt this addition).
-    remainder = RemainderForOctet(dividend) ^ unused_remainder;
+    remainder = gRemainderTable.GetRemainderForOctet(dividend) ^ unused_remainder;
   }
 
   return FrameCheckSequence{remainder};
