2081ced5182a8af2af01123839d5ba25b87c5487
Aaron Drew <ripper@google.com>
2022-01-17 04:50:43 +0000

    [fxfs] A minimal trait system for versioned struct/enum.
    This introduces two traits - Version and VersionLatest.
    These provide provide version numbering, upgrading from older
    versions to newer versions and (de)serialization support.
    
    The intention is that these will form the basis of FxFS's stable
    on-disk format strategy.
    
    Change-Id: I87d9c33f8d0f32e6260b5292b8d4c509050bc776
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/620442
    Reviewed-by: Chris Suter <csuter@google.com>
    Commit-Queue: Aaron Drew <ripper@google.com>

---
+++ src/storage/fxfs/src/serialized_types/test_traits.rs
@@ -0,0 +1,32 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file saves some repetition in tests where integers are used in place of keys and values
+// by just implementing the traits for some basic types here.
+use crate::serialized_types::{versioned_type, Version, VersionLatest};
+
+// The traits for u64 are implemented by hand to illustrate what the proc_macro is doing.
+impl Version for u64 {
+    fn version() -> u32 {
+        1
+    }
+}
+impl VersionLatest for u64 {
+    fn deserialize_from_version<R: ?Sized>(reader: &mut R, version: u32) -> anyhow::Result<Self>
+    where
+        R: std::io::Read,
+        Self: Sized,
+    {
+        match version {
+            1 => Ok(u64::deserialize_from(reader)?.into()),
+            _ => anyhow::bail!(format!("Invalid version {} for u64.", version)),
+        }
+    }
+}
+
+versioned_type! { 1 => i64 }
+versioned_type! { 1 => u32 }
+versioned_type! { 1 => i32 }
+versioned_type! { 1 => u8 }
+versioned_type! { 1 => i8 }
