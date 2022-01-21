3b38a217d140a43af4c5bf25ffe2f3f003d9cec5
Derek Gonyeo <dgonyeo@google.com>
2022-01-21 15:21:02 +0000

    [driver_test_realm][rust] remove impl for old Realm Builder
    All users of the driver_test_realm traits have been moved over to
    fuchsia_component_test::new::RealmBuilder, so delete the implementations
    for the old Realm Builder struct.
    
    Bug: 91733
    Change-Id: Idc4032075bd647fefecdddff4a88d99845ee3be7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/625563
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Derek Gonyeo <dgonyeo@google.com>

--- sdk/lib/driver_test_realm/realm_builder/rust/src/lib.rs
+++ sdk/lib/driver_test_realm/realm_builder/rust/src/lib.rs
@@ -4,11 +4,9 @@
 
 use {
     anyhow::{anyhow, Result},
-    fidl_fuchsia_driver_test as fdt, fidl_fuchsia_io2 as fio2,
-    fuchsia_component_test::{
-        new::{Capability, RealmBuilder, RealmInstance, Ref, Route},
-        ChildOptions, RealmBuilder as OldRealmBuilder, RealmInstance as OldRealmInstance,
-        RouteBuilder, RouteEndpoint,
+    fidl_fuchsia_driver_test as fdt,
+    fuchsia_component_test::new::{
+        Capability, ChildOptions, RealmBuilder, RealmInstance, Ref, Route,
     },
 };
 
@@ -60,57 +58,6 @@ impl DriverTestRealmBuilder for RealmBuilder {
     }
 }
 
-// TODO(91733): To be deleted once all clients are moved to new::RealmBuilder
-#[async_trait::async_trait]
-impl DriverTestRealmBuilder for OldRealmBuilder {
-    async fn driver_test_realm_manifest_setup(&self, manifest_url: &str) -> Result<&Self> {
-        let driver_realm = RouteEndpoint::component(COMPONENT_NAME);
-        self.add_child(COMPONENT_NAME, manifest_url, ChildOptions::new().eager())
-            .await?
-            .add_route(
-                RouteBuilder::protocol("fuchsia.logger.LogSink")
-                    .source(RouteEndpoint::AboveRoot)
-                    .targets(vec![driver_realm.clone()]),
-            )
-            .await?
-            .add_route(
-                RouteBuilder::protocol("fuchsia.process.Launcher")
-                    .source(RouteEndpoint::AboveRoot)
-                    .targets(vec![driver_realm.clone()]),
-            )
-            .await?
-            .add_route(
-                RouteBuilder::protocol("fuchsia.sys.Launcher")
-                    .source(RouteEndpoint::AboveRoot)
-                    .targets(vec![driver_realm.clone()]),
-            )
-            .await?
-            .add_route(
-                RouteBuilder::protocol("fuchsia.driver.development.DriverDevelopment")
-                    .source(driver_realm.clone())
-                    .targets(vec![RouteEndpoint::AboveRoot]),
-            )
-            .await?
-            .add_route(
-                RouteBuilder::protocol("fuchsia.driver.test.Realm")
-                    .source(driver_realm.clone())
-                    .targets(vec![RouteEndpoint::AboveRoot]),
-            )
-            .await?
-            .add_route(
-                RouteBuilder::directory("dev", "dev", fio2::RW_STAR_DIR)
-                    .source(driver_realm.clone())
-                    .targets(vec![RouteEndpoint::AboveRoot]),
-            )
-            .await?;
-        Ok(&self)
-    }
-
-    async fn driver_test_realm_setup(&self) -> Result<&Self> {
-        self.driver_test_realm_manifest_setup(DRIVER_TEST_REALM_URL).await
-    }
-}
-
 #[async_trait::async_trait]
 pub trait DriverTestRealmInstance {
     /// Connect to the DriverTestRealm in this Instance and call Start with `args`.
@@ -122,29 +69,6 @@ pub trait DriverTestRealmInstance {
 
 #[async_trait::async_trait]
 impl DriverTestRealmInstance for RealmInstance {
-    async fn driver_test_realm_start(&self, args: fdt::RealmArgs) -> Result<()> {
-        let config = self.root.connect_to_protocol_at_exposed_dir::<fdt::RealmMarker>()?;
-        config
-            .start(args)
-            .await?
-            .map_err(|e| anyhow!("DriverTestRealm Start failed with: {}", e))?;
-        Ok(())
-    }
-
-    fn driver_test_realm_connect_to_dev(&self) -> Result<fidl_fuchsia_io::DirectoryProxy> {
-        let (dev, dev_server) =
-            fidl::endpoints::create_endpoints::<fidl_fuchsia_io::DirectoryMarker>()?;
-        self.root
-            .connect_request_to_named_protocol_at_exposed_dir("dev", dev_server.into_channel())?;
-        Ok(fidl_fuchsia_io::DirectoryProxy::new(fidl::handle::AsyncChannel::from_channel(
-            dev.into_channel(),
-        )?))
-    }
-}
-
-// TODO(91733): To be deleted once all clients are moved to new::RealmInstance
-#[async_trait::async_trait]
-impl DriverTestRealmInstance for OldRealmInstance {
     async fn driver_test_realm_start(&self, args: fdt::RealmArgs) -> Result<()> {
         let config = self.root.connect_to_protocol_at_exposed_dir::<fdt::RealmMarker>()?;
         config
