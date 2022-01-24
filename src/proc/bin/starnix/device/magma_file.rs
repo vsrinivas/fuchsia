44ffd05d5580159bb3456fc39cf869cc20d0a187
Kevin Lindkvist <lindkvist@google.com>
2022-01-24 21:46:30 +0000

    [starnix] Implement MAGMA_CMD_CREATE_SEMAPHORE
    Change-Id: Ia7fb1233389bd436669c52e4643eb13f2a2efcd0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/636803
    Fuchsia-Auto-Submit: Kevin Lindkvist <lindkvist@google.com>
    Reviewed-by: Craig Stout <cstout@google.com>
    Commit-Queue: Kevin Lindkvist <lindkvist@google.com>
    Reviewed-by: Adam Lesinski <adamlesinski@google.com>

--- src/proc/bin/starnix/device/magma_file.rs
+++ src/proc/bin/starnix/device/magma_file.rs
@@ -375,6 +375,25 @@ impl FileOps for MagmaFile {
                 response.hdr.type_ = virtio_magma_ctrl_type_VIRTIO_MAGMA_RESP_GET_BUFFER_ID as u32;
                 current_task.mm.write_object(UserRef::new(response_address), &response)
             }
+            virtio_magma_ctrl_type_VIRTIO_MAGMA_CMD_CREATE_SEMAPHORE => {
+                let (control, mut response): (
+                    virtio_magma_create_semaphore_ctrl_t,
+                    virtio_magma_create_semaphore_resp_t,
+                ) = read_control_and_response(current_task, &command)?;
+
+                let mut semaphore_out = 0;
+                response.result_return = unsafe {
+                    magma_create_semaphore(
+                        control.connection as magma_connection_t,
+                        &mut semaphore_out,
+                    ) as u64
+                };
+
+                response.semaphore_out = semaphore_out;
+                response.hdr.type_ =
+                    virtio_magma_ctrl_type_VIRTIO_MAGMA_RESP_CREATE_SEMAPHORE as u32;
+                current_task.mm.write_object(UserRef::new(response_address), &response)
+            }
             t => {
                 log::warn!("Got unknown request: {:?}", t);
                 error!(ENOSYS)
