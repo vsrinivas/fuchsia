0acb4ed7642c7ad3eb9a601b6251581b677c0c11
Sean Cuff <seancuff@google.com>
2022-01-26 19:05:49 +0000

    [sdk] Create host_tool_sdk_element
    Also adds sdk_element_adapter support for host tools
    
    Output: go/paste/6222919131725824
    
    Bug: b/198192634
    Change-Id: I04343d2733216180f1a7d644eeac94a1b3626e18
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/585666
    Reviewed-by: Misha Gridnev <gridman@google.com>
    Commit-Queue: Sean Cuff <seancuff@google.com>

--- build/sdk/sdk_element_adapter.py
+++ build/sdk/sdk_element_adapter.py
@@ -8,6 +8,8 @@ import json
 import sys
 import os
 
+SUPPORTED_TYPES = ['host_tool', 'version_history']
+
 
 def main():
     """Writes SDK atom-format metadata based on SDK element metadata.
@@ -38,49 +40,83 @@ def main():
 
     with open(args.meta_in, 'r') as meta_in:
         element_meta = json.load(meta_in)
-    element_type = element_meta['data']['element_type']
-    category = element_meta['data']['category'] if 'category' in element_meta[
-        'data'] else 'partner'
 
     with open(args.manifest_in, 'r') as manifest_in:
         element_manifest = json.load(manifest_in)
 
-    if element_type == 'version_history':
-        # Version history is pure metadata, so meta_out is the same as meta_in.
-        atom_meta = element_meta
-        atom_id = "sdk://version_history"
-    else:
-        return 'Unsupported element type ' + element_meta['data']['element_type']
-
-    files = [
-        {
-            'destination': file['dst'],
-            'source': file['src'],
-        } for file in element_manifest
-    ]
-
-    atom_manifest = {
-        'atoms':
-            [
-                {
-                    'category': category,
-                    'deps': [],
-                    'files': files,
-                    'gn-label': args.gn_label,
-                    'id': atom_id,
-                    'meta': files[-1]['destination'],
-                    'plasa': [],
-                    'type': element_type,
-                }
-            ],
-        'ids': [atom_id]
-    }
+    adapter = Adapter(
+        element_meta, element_manifest, args.gn_label, args.meta_out)
+    if adapter.element_type not in SUPPORTED_TYPES:
+        return 'Unsupported element type "{}"'.format(adapter.type)
 
     with open(args.meta_out, 'w') as meta_out:
-        json.dump(atom_meta, meta_out, indent=2, sort_keys=True)
+        json.dump(adapter.atom_meta(), meta_out, indent=2, sort_keys=True)
 
     with open(args.manifest_out, 'w') as manifest_out:
-        json.dump(atom_manifest, manifest_out, indent=2, sort_keys=True)
+        json.dump(
+            adapter.atom_manifest(), manifest_out, indent=2, sort_keys=True)
+
+
+class Adapter(object):
+
+    def __init__(self, element_meta, element_manifest, gn_label, meta_out):
+        self.element_meta = element_meta
+        self.gn_label = gn_label
+        self.name = element_meta['data']['name']
+        self.element_type = element_meta['data']['element_type']
+        # Type-dependent fields
+        if self.element_type == 'version_history':
+            self.file_base = ''
+            self.id = 'sdk://version_history'
+        elif self.element_type == 'host_tool':
+            self.file_base = os.path.join(
+                'tools', element_meta['data']['host_arch'])
+            self.id = 'sdk://{}/{}'.format(self.file_base, self.name)
+            self.root = 'tools'
+
+        self.files = [
+            {
+                'destination': os.path.join(self.file_base, file['dst']),
+                'source': file['src'],
+            } for file in element_manifest[:-1]
+        ] + [
+            {
+                'destination':
+                    os.path.join(self.file_base, element_manifest[-1]['dst']),
+                'source':
+                    meta_out,
+            }
+        ]
+
+    def atom_meta(self):
+        if self.element_type == 'version_history':
+            # Version history is pure metadata, so meta_out is the same as meta_in.
+            return self.element_meta
+
+        return {
+            'files': [file['destination'] for file in self.files[:-1]],
+            'name': self.name,
+            'root': self.root,
+            'type': self.element_type
+        }
+
+    def atom_manifest(self):
+        return {
+            'atoms':
+                [
+                    {
+                        'category': 'partner',
+                        'deps': [],
+                        'files': self.files,
+                        'gn-label': self.gn_label,
+                        'id': self.id,
+                        'meta': self.files[-1]['destination'],
+                        'plasa': [],
+                        'type': self.element_type,
+                    }
+                ],
+            'ids': [self.id]
+        }
 
 
 if __name__ == '__main__':
