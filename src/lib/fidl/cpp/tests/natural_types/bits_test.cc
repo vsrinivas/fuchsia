4c10e7ed1e71b1c81a4186e254c25af99df6a7da
Ian McKellar <ianloic@google.com>
2022-01-27 21:34:48 +0000

    [fidl][cpp] Initial natural API test
    This is the start of tests for the natural API bindings. So far it just
    contains a copy of the wire API tests for enums and bits with the symbol
    names changed - since those are the same types but in a different
    namespace.
    
    This also moves the fidl.llcpp.types.test FIDL library definition out of
    the wire_types subdirectory since it's used for more.
    
    Change-Id: I8563c835ab8abe6c1bf5bc4066661de82a97fe6a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/635537
    Reviewed-by: Benjamin Prosnitz <bprosnitz@google.com>
    Fuchsia-Auto-Submit: Ian McKellar <ianloic@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Ian McKellar <ianloic@google.com>

---
+++ src/lib/fidl/cpp/tests/natural_types/bits_test.cc
@@ -0,0 +1,120 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/fidl.llcpp.types.test/cpp/natural_types.h>
+#include <lib/stdcompat/optional.h>
+
+#include <gtest/gtest.h>
+
+template <typename T>
+class Bits : public ::testing::Test {};
+
+TYPED_TEST_SUITE_P(Bits);
+
+TYPED_TEST_P(Bits, BitwiseOperators) {
+  TypeParam b_or_d = TypeParam::kB | TypeParam::kD;
+  EXPECT_EQ(static_cast<uint8_t>(b_or_d), 6u /* 2 | 4*/);
+
+  TypeParam b_or_e = TypeParam::kB | TypeParam::kE;
+  EXPECT_EQ(static_cast<uint8_t>(b_or_e), 10u /* 2 | 8*/);
+
+  TypeParam not_b = ~TypeParam::kB;
+  EXPECT_EQ(static_cast<uint8_t>(not_b), 12u /* ~2 & (2 | 4 | 8)*/);
+
+  TypeParam not_d = ~TypeParam::kD;
+  EXPECT_EQ(static_cast<uint8_t>(not_d), 10u /* ~4 & (2 | 4 | 8)*/);
+
+  TypeParam not_e = ~TypeParam::kE;
+  EXPECT_EQ(static_cast<uint8_t>(not_e), 6u /* ~8 & (2 | 4 | 8)*/);
+
+  TypeParam b_and_not_e = TypeParam::kB & ~TypeParam::kE;
+  EXPECT_EQ(static_cast<uint8_t>(b_and_not_e), 2u /* 2 & 6*/);
+
+  TypeParam b_or_d_and_b_or_e = (TypeParam::kB | TypeParam::kD) & (TypeParam::kB | TypeParam::kE);
+  EXPECT_EQ(static_cast<uint8_t>(b_or_d_and_b_or_e), 2u /* 6 & 10*/);
+
+  TypeParam b_xor_not_e = TypeParam::kB ^ ~TypeParam::kE;
+  EXPECT_EQ(static_cast<uint8_t>(b_xor_not_e), 4u /* 4 ^ 6*/);
+
+  TypeParam b_or_d_xor_b_or_e = (TypeParam::kB | TypeParam::kD) ^ (TypeParam::kB | TypeParam::kE);
+  EXPECT_EQ(static_cast<uint8_t>(b_or_d_xor_b_or_e), 12u /* 6 ^ 10*/);
+}
+
+TYPED_TEST_P(Bits, BitwiseAssignOperators) {
+  TypeParam b_or_d = TypeParam::kB;
+  b_or_d |= TypeParam::kD;
+  EXPECT_EQ(static_cast<uint8_t>(b_or_d), 6u /* 2 | 4*/);
+
+  TypeParam b_and_not_e = TypeParam::kB;
+  b_and_not_e &= ~TypeParam::kE;
+  EXPECT_EQ(static_cast<uint8_t>(b_and_not_e), 2u /* 2 & 6*/);
+
+  TypeParam b_xor_not_e = TypeParam::kB;
+  b_xor_not_e ^= ~TypeParam::kE;
+  EXPECT_EQ(static_cast<uint8_t>(b_xor_not_e), 4u /* 4 ^ 6*/);
+
+  EXPECT_EQ(static_cast<uint8_t>(TypeParam::kB), 2u);
+  EXPECT_EQ(static_cast<uint8_t>(TypeParam::kD), 4u);
+  EXPECT_EQ(static_cast<uint8_t>(TypeParam::kE), 8u);
+}
+
+TYPED_TEST_P(Bits, IsConstexpr) {
+  static constexpr auto this_should_compile = TypeParam::kB | TypeParam::kD | TypeParam::kE;
+  EXPECT_EQ(this_should_compile, TypeParam::kMask);
+}
+
+TYPED_TEST_P(Bits, CanConvertToNumberButMustBeExplicit) {
+  uint8_t r8 = static_cast<uint8_t>(TypeParam::kB);
+  EXPECT_EQ(r8, 2u);
+  uint16_t r16 = static_cast<uint8_t>(TypeParam::kB);
+  EXPECT_EQ(r16, 2u);
+}
+
+TYPED_TEST_P(Bits, CanConvertToBool) {
+  bool result = static_cast<bool>(TypeParam::kB);
+  EXPECT_TRUE(result);
+}
+
+TYPED_TEST_P(Bits, TruncatingUnknown) {
+  // The bits type only has 2, 4, and 8 defined.
+  auto bits = TypeParam::TruncatingUnknown(1);
+  EXPECT_EQ(static_cast<uint8_t>(bits), 0);
+}
+
+TYPED_TEST_P(Bits, TryFrom) {
+  // The bits type only has 2, 4, and 8 defined.
+  auto result = TypeParam::TryFrom(1);
+  EXPECT_EQ(result, cpp17::nullopt);
+
+  auto result_ok = TypeParam::TryFrom(2);
+  EXPECT_EQ(result_ok, cpp17::optional<TypeParam>(TypeParam::kB));
+}
+
+TYPED_TEST_P(Bits, AllowingUnknownThroughStaticCast) {
+  // The bits type only has 2, 4, and 8 defined.
+  auto bits = static_cast<TypeParam>(1);
+  EXPECT_EQ(static_cast<uint8_t>(bits), 1);
+}
+
+REGISTER_TYPED_TEST_SUITE_P(Bits, BitwiseOperators, BitwiseAssignOperators, IsConstexpr,
+                            CanConvertToNumberButMustBeExplicit, CanConvertToBool,
+                            TruncatingUnknown, TryFrom, AllowingUnknownThroughStaticCast);
+
+using BitsTypesToTest =
+    ::testing::Types<fidl_llcpp_types_test::StrictBits, fidl_llcpp_types_test::FlexibleBits>;
+INSTANTIATE_TYPED_TEST_SUITE_P(BitsTests, Bits, BitsTypesToTest);
+
+// The following APIs tested are only available on flexible bits.
+
+TEST(Bits, QueryingUnknown) {
+  using BitsType = fidl_llcpp_types_test::FlexibleBits;
+  // The bits type only has 2, 4, and 8 defined.
+  auto bits = static_cast<BitsType>(2 | 1);
+  EXPECT_TRUE(bits.has_unknown_bits());
+  EXPECT_EQ(static_cast<uint8_t>(bits.unknown_bits()), 1);
+
+  bits = BitsType::TruncatingUnknown(2 | 1);
+  EXPECT_FALSE(bits.has_unknown_bits());
+  EXPECT_EQ(static_cast<uint8_t>(bits.unknown_bits()), 0);
+}
---
+++ src/lib/fidl/cpp/tests/natural_types/enum_test.cc
@@ -0,0 +1,94 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/fidl.llcpp.types.test/cpp/natural_types.h>
+
+#include <gtest/gtest.h>
+
+using fidl_llcpp_types_test::EmptyFlexibleEnum;
+using fidl_llcpp_types_test::FlexibleEnum;
+using fidl_llcpp_types_test::StrictEnum;
+
+TEST(Enum, Defaults) {
+  using StrictType = StrictEnum;
+  using FlexibleType = FlexibleEnum;
+
+  StrictType strict_enum_default = {};
+  FlexibleType flexible_enum_default = {};
+
+  EXPECT_EQ(0u, static_cast<uint32_t>(strict_enum_default));
+  EXPECT_EQ(0u, static_cast<uint32_t>(flexible_enum_default));
+}
+
+TEST(FlexibleEnum, IsUnknown) {
+  using EnumType = FlexibleEnum;
+
+  EXPECT_FALSE(EnumType::kB.IsUnknown());
+  EXPECT_FALSE(EnumType::kD.IsUnknown());
+  EXPECT_FALSE(EnumType::kE.IsUnknown());
+
+  EXPECT_TRUE(EnumType::kCustom.IsUnknown());
+  EXPECT_TRUE(EnumType::Unknown().IsUnknown());
+}
+
+TEST(EmptyFlexibleEnum, IsUnknown) {
+  using EnumType = EmptyFlexibleEnum;
+
+  EXPECT_TRUE(EnumType::Unknown().IsUnknown());
+}
+
+TEST(FlexibleEnum, Equality) {
+  using EnumType = FlexibleEnum;
+
+  EXPECT_TRUE(EnumType::kB == EnumType(2));
+  EXPECT_TRUE(EnumType::kD == EnumType(4));
+  EXPECT_TRUE(EnumType::Unknown() == EnumType::kCustom);
+
+  EXPECT_FALSE(EnumType::kB != EnumType(2));
+  EXPECT_FALSE(EnumType::kD != EnumType(4));
+  EXPECT_FALSE(EnumType::Unknown() != EnumType::kCustom);
+}
+
+TEST(Enum, Switch) {
+  using StrictType = StrictEnum;
+  using FlexibleType = FlexibleEnum;
+
+  // This is a compilation test ensuring that we can use strict and flexible
+  // enums in switch statements.
+  auto switch_on_strict = [](StrictType value) -> uint32_t {
+    switch (value) {
+      case StrictType::kB:
+        return 4000u;
+      case StrictType::kD:
+        return 5000u;
+      case StrictType::kE:
+        return 6000u;
+    }
+  };
+  EXPECT_EQ(6000u, switch_on_strict(StrictType::kE));
+
+  auto switch_on_flexible = [](FlexibleType value) -> uint32_t {
+    switch (value) {
+      case FlexibleType::kB:
+        return 4000u;
+      case FlexibleType::kD:
+        return 5000u;
+      default:
+        return 6000u;
+    }
+  };
+  EXPECT_EQ(6000u, switch_on_flexible(FlexibleType::kE));
+}
+
+TEST(Enum, Clone) {
+  StrictEnum se1 = StrictEnum::kB;
+  EXPECT_EQ(se1, StrictEnum::kB);
+  StrictEnum se2 = se1;
+  EXPECT_EQ(se1, se2);
+
+  FlexibleEnum fe1 = FlexibleEnum::kB;
+  EXPECT_EQ(fe1, FlexibleEnum::kB);
+  FlexibleEnum fe2 = fe1;
+  EXPECT_EQ(fe1, fe2);
+}
