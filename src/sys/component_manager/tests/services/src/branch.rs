2218283889ee98b2cb823dac742bde8344496e4d
Yegor Pomortsev <ypomortsev@google.com>
2022-01-28 02:10:00 +0000

    [services] Refactor test to start provider components outside branch
    Start the provider components from the test, instead of from the
    branch component.
    
    This allows the test to verify that service instances are added and
    removed when components are started and stopped, in follow-up changes.
    
    Bug: 79715
    Change-Id: I5f4359af4e13ef7546da8372083da191509945d4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/624421
    Reviewed-by: Gary Bressler <geb@google.com>
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>
    Fuchsia-Auto-Submit: Yegor Pomortsev <ypomortsev@google.com>

--- src/sys/component_manager/tests/services/src/branch.rs
+++ src/sys/component_manager/tests/services/src/branch.rs
@@ -5,7 +5,6 @@
 use {
     anyhow::{format_err, Context, Error},
     fidl_fuchsia_component as fcomponent, fidl_fuchsia_component_decl as fdecl,
-    fidl_fuchsia_io as fio,
     fuchsia_component::client,
     futures::future,
     tracing::*,
@@ -16,11 +15,12 @@ const ACCOUNT_PROVIDERS_COLLECTION: &str = "account_providers";
 
 #[fuchsia::component]
 async fn main() -> Result<(), Error> {
-    // Launch two BankAccount providers into the `account_providers` collection.
+    // Create two BankAccount providers into the `account_providers` collection.
+    // The providers are not eagerly started. The test should start them as needed.
     let realm = client::connect_to_protocol::<fcomponent::RealmMarker>()
         .context("connect to Realm service")?;
-    start_provider(&realm, "a", "#meta/provider-a.cm").await?;
-    start_provider(&realm, "b", "#meta/provider-b.cm").await?;
+    create_provider(&realm, "a", "#meta/provider-a.cm").await?;
+    create_provider(&realm, "b", "#meta/provider-b.cm").await?;
 
     // Wait indefinitely to keep this component running.
     future::pending::<()>().await;
@@ -28,17 +28,15 @@ async fn main() -> Result<(), Error> {
     unreachable!();
 }
 
-/// Starts a BankAccount provider component in `ACCOUNT_PROVIDERS_COLLECTION`.
-async fn start_provider(
+/// Creates a BankAccount provider component in `ACCOUNT_PROVIDERS_COLLECTION`.
+///
+/// This does not start the component.
+async fn create_provider(
     realm: &fcomponent::RealmProxy,
     name: &str,
     url: &str,
 ) -> Result<(), Error> {
     info!("creating BankAccount provider \"{}\" with url={}", name, url);
-    let child_args = fcomponent::CreateChildArgs {
-        numbered_handles: None,
-        ..fcomponent::CreateChildArgs::EMPTY
-    };
     realm
         .create_child(
             &mut fdecl::CollectionRef { name: ACCOUNT_PROVIDERS_COLLECTION.to_string() },
@@ -49,31 +47,11 @@ async fn start_provider(
                 environment: None,
                 ..fdecl::Child::EMPTY
             },
-            child_args,
+            fcomponent::CreateChildArgs::EMPTY,
         )
         .await
         .context("failed to call CreateChild")?
         .map_err(|e| format_err!("Failed to create child: {:?}", e))?;
-
-    let (exposed_dir, exposed_dir_server_end) =
-        fidl::endpoints::create_proxy::<fio::DirectoryMarker>()
-            .context("failed to create Directory endpoints")?;
-    info!("open exposed dir of BankAccount provider \"{}\" with url={}", name, url);
-    realm
-        .open_exposed_dir(
-            &mut fdecl::ChildRef {
-                name: name.to_string(),
-                collection: Some(ACCOUNT_PROVIDERS_COLLECTION.to_string()),
-            },
-            exposed_dir_server_end,
-        )
-        .await
-        .context("failed to call OpenExposedDir")?
-        .map_err(|e| format_err!("Failed to open exposed dir: {:?}", e))?;
-
-    // Connect to Binder to launch the component.
-    let _ = client::connect_to_protocol_at_dir_root::<fcomponent::BinderMarker>(&exposed_dir)
-        .context("failed to connect to fuchsia.component.Binder")?;
 
     Ok(())
 }
--- src/sys/component_manager/tests/services/src/service_routing_test.rs
+++ src/sys/component_manager/tests/services/src/service_routing_test.rs
@@ -3,14 +3,14 @@
 // found in the LICENSE file.
 
 use {
-    anyhow::{Context, Error},
+    anyhow::{format_err, Context, Error},
     component_events::{
-        events::{Event, EventMode, EventSource, EventSubscription, Started},
+        events::{Discovered, Event, EventMode, EventSource, EventSubscription, Started},
         matcher::EventMatcher,
         sequence::*,
     },
     fidl::endpoints::ServiceMarker,
-    fidl_fuchsia_examples_services as fexamples,
+    fidl_fuchsia_examples_services as fexamples, fidl_fuchsia_sys2 as fsys2,
     fuchsia_component::client,
     fuchsia_component_test::ScopedInstance,
     tracing::*,
@@ -25,9 +25,20 @@ const BRANCH_COMPONENT_URL: &str = "#meta/service-routing-branch.cm";
 /// Name of the collection in the branch component that contains BankAccount service providers.
 const ACCOUNT_PROVIDERS_COLLECTION: &str = "account_providers";
 
+/// Name of the provider-a.cm child component in the branch.
+const PROVIDER_A_NAME: &str = "a";
+
+/// Name of the provider-b.cm child component in the branch.
+const PROVIDER_B_NAME: &str = "b";
+
+/// Path to the LifecycleController protocol in the hub.
+const LIFECYCLE_CONTROLLER_HUB_PATH: &str = "/hub/debug/fuchsia.sys2.LifecycleController";
+
 #[fuchsia::test]
 async fn list_instances_test() {
     let branch = start_branch().await.expect("failed to start branch component");
+    start_provider(&branch, PROVIDER_A_NAME).await.expect("failed to start provider a");
+    start_provider(&branch, PROVIDER_B_NAME).await.expect("failed to start provider b");
 
     // List the instances in the BankAccount service.
     let service_dir = io_util::directory::open_directory(
@@ -50,6 +61,8 @@ async fn list_instances_test() {
 #[fuchsia::test]
 async fn connect_to_instances_test() {
     let branch = start_branch().await.expect("failed to start branch component");
+    start_provider(&branch, PROVIDER_A_NAME).await.expect("failed to start provider a");
+    start_provider(&branch, PROVIDER_B_NAME).await.expect("failed to start provider b");
 
     // List the instances in the BankAccount service.
     let service_dir = io_util::directory::open_directory(
@@ -90,41 +103,34 @@ async fn connect_to_instances_test() {
 async fn start_branch() -> Result<ScopedInstance, Error> {
     let event_source = EventSource::new()?;
     let event_stream = event_source
-        .subscribe(vec![EventSubscription::new(vec![Started::NAME], EventMode::Async)])
+        .subscribe(vec![EventSubscription::new(vec![Discovered::NAME], EventMode::Async)])
         .await
         .context("failed to subscribe to EventSource")?;
-    event_source.start_component_tree().await;
 
-    // Create a branch component instance.
     let branch =
         ScopedInstance::new(BRANCHES_COLLECTION.to_string(), BRANCH_COMPONENT_URL.to_string())
             .await
             .context("failed to create branch component instance")?;
     branch.start_with_binder_sync().await?;
 
-    // Wait for the account providers in the branch to start.
-    //
-    // This ensures that the aggregated BankAccount service contains service instances from
-    // all account providers started by the branch.
+    // Wait for the providers to be discovered (created) to ensure that
+    // subsequent calls to `start_provider` can start them.
     EventSequence::new()
         .has_subset(
             vec![
-                EventMatcher::ok().r#type(Started::TYPE).moniker_regex(format!(
-                    "./{}:{}",
-                    BRANCHES_COLLECTION,
-                    branch.child_name()
-                )),
-                EventMatcher::ok().r#type(Started::TYPE).moniker_regex(format!(
-                    "./{}:{}/{}:a",
+                EventMatcher::ok().r#type(Discovered::TYPE).moniker_regex(format!(
+                    "./{}:{}/{}:{}",
                     BRANCHES_COLLECTION,
                     branch.child_name(),
-                    ACCOUNT_PROVIDERS_COLLECTION
+                    ACCOUNT_PROVIDERS_COLLECTION,
+                    PROVIDER_A_NAME,
                 )),
-                EventMatcher::ok().r#type(Started::TYPE).moniker_regex(format!(
-                    "./{}:{}/{}:b",
+                EventMatcher::ok().r#type(Discovered::TYPE).moniker_regex(format!(
+                    "./{}:{}/{}:{}",
                     BRANCHES_COLLECTION,
                     branch.child_name(),
-                    ACCOUNT_PROVIDERS_COLLECTION
+                    ACCOUNT_PROVIDERS_COLLECTION,
+                    PROVIDER_B_NAME,
                 )),
             ],
             Ordering::Unordered,
@@ -134,4 +140,44 @@ async fn start_branch() -> Result<ScopedInstance, Error> {
         .context("event sequence did not match expected")?;
 
     Ok(branch)
+}
+
+/// Starts the provider with the name `child_name` in the branch component.
+async fn start_provider(branch: &ScopedInstance, child_name: &str) -> Result<(), Error> {
+    let lifecycle_controller_proxy = client::connect_to_protocol_at_path::<
+        fsys2::LifecycleControllerMarker,
+    >(LIFECYCLE_CONTROLLER_HUB_PATH)
+    .context("failed to connect to LifecycleController")?;
+
+    let event_source = EventSource::new()?;
+    let event_stream = event_source
+        .subscribe(vec![EventSubscription::new(vec![Started::NAME], EventMode::Async)])
+        .await
+        .context("failed to subscribe to EventSource")?;
+
+    let provider_moniker = format!(
+        "./{}:{}/{}:{}",
+        BRANCHES_COLLECTION,
+        branch.child_name(),
+        ACCOUNT_PROVIDERS_COLLECTION,
+        child_name,
+    );
+
+    // Start the provider child.
+    lifecycle_controller_proxy
+        .start(&provider_moniker)
+        .await?
+        .map_err(|err| format_err!("failed to start provider component: {:?}", err))?;
+
+    // Wait for the provider to start.
+    EventSequence::new()
+        .has_subset(
+            vec![EventMatcher::ok().r#type(Started::TYPE).moniker_regex(provider_moniker)],
+            Ordering::Unordered,
+        )
+        .expect(event_stream)
+        .await
+        .context("event sequence did not match expected")?;
+
+    Ok(())
 }
