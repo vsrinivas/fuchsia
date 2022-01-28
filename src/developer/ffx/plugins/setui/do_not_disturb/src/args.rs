b1d30f925abada4e989e9b93147ed6f2b986c764
Sijie Chen <sijiec@google.com>
2022-01-28 18:26:37 +0000

    [ffx][setui] Migrate Do Not Disturb
    This CL adds a subcommand, do not disturb of ffx_setui. Compared to
    codes in setui_client, the `command` method takes in DoNotDisturb
    instead of individual variants.
    
    Bug: 76259
    Test:
    fx build ffx
    fx ffx setui do_not_disturb -> watch
    fx ffx setui do_not_disturb -n true -u false -> set
    fx test ffx_setui_factory_reset_test -o
    
    Change-Id: Id836faccce66146cad637db28ba04cab5bd0c0b4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/638262
    Reviewed-by: Paul Faria <paulfaria@google.com>
    Commit-Queue: Sijie Chen <sijiec@google.com>

---
+++ src/developer/ffx/plugins/setui/do_not_disturb/src/args.rs
@@ -0,0 +1,31 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use argh::FromArgs;
+use ffx_core::ffx_command;
+use fidl_fuchsia_settings::DoNotDisturbSettings;
+
+#[ffx_command()]
+#[derive(FromArgs, Debug, PartialEq, Clone, Copy)]
+#[argh(subcommand, name = "do_not_disturb")]
+/// get or set DnD settings
+pub struct DoNotDisturb {
+    /// when set to 'true', allows the device to enter do not disturb mode
+    #[argh(option, short = 'u')]
+    pub user_dnd: Option<bool>,
+
+    /// when set to 'true', forces the device into do not disturb mode
+    #[argh(option, short = 'n')]
+    pub night_mode_dnd: Option<bool>,
+}
+
+impl From<DoNotDisturb> for DoNotDisturbSettings {
+    fn from(src: DoNotDisturb) -> DoNotDisturbSettings {
+        DoNotDisturbSettings {
+            user_initiated_do_not_disturb: src.user_dnd,
+            night_mode_initiated_do_not_disturb: src.night_mode_dnd,
+            ..DoNotDisturbSettings::EMPTY
+        }
+    }
+}
---
+++ src/developer/ffx/plugins/setui/do_not_disturb/src/lib.rs
@@ -0,0 +1,123 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::Result;
+use ffx_core::ffx_plugin;
+use ffx_setui_do_not_disturb_args::DoNotDisturb;
+use fidl_fuchsia_settings::{DoNotDisturbProxy, DoNotDisturbSettings};
+use utils::handle_mixed_result;
+use utils::{self, Either, WatchOrSetResult};
+
+#[ffx_plugin(
+    "setui",
+    DoNotDisturbProxy = "core/setui_service:expose:fuchsia.settings.DoNotDisturb"
+)]
+pub async fn run_command(
+    do_not_disturb_proxy: DoNotDisturbProxy,
+    do_not_disturb: DoNotDisturb,
+) -> Result<()> {
+    handle_mixed_result("DoNotDisturb", command(do_not_disturb_proxy, do_not_disturb).await)
+        .await?;
+
+    Ok(())
+}
+
+async fn command(proxy: DoNotDisturbProxy, do_not_disturb: DoNotDisturb) -> WatchOrSetResult {
+    let settings = DoNotDisturbSettings::from(do_not_disturb);
+
+    if settings == DoNotDisturbSettings::EMPTY {
+        Ok(Either::Watch(utils::watch_to_stream(proxy, |p| p.watch())))
+    } else {
+        Ok(Either::Set(if let Err(err) = proxy.set(settings.clone()).await? {
+            format!("{:?}", err)
+        } else {
+            format!("Successfully set DoNotDisturb to {:?}", do_not_disturb)
+        }))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use fidl_fuchsia_settings::{DoNotDisturbRequest, DoNotDisturbSettings};
+    use futures::prelude::*;
+    use test_case::test_case;
+
+    #[test_case(
+        DoNotDisturb {
+            user_dnd: Some(false),
+            night_mode_dnd: Some(false),
+        };
+        "Test do not disturb set() output with both user_dnd and night_mode_dnd as false."
+    )]
+    #[test_case(
+        DoNotDisturb {
+            user_dnd: Some(true),
+            night_mode_dnd: Some(false),
+        };
+        "Test do not disturb set() output with user_dnd as true and night_mode_dnd as false."
+    )]
+    #[test_case(
+        DoNotDisturb {
+            user_dnd: Some(false),
+            night_mode_dnd: Some(true),
+        };
+        "Test do not disturb set() output with user_dnd and night_mode_dnd as true."
+    )]
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn validate_do_not_disturb_set_output(
+        expected_do_not_disturb: DoNotDisturb,
+    ) -> Result<()> {
+        let proxy = setup_fake_do_not_disturb_proxy(move |req| match req {
+            DoNotDisturbRequest::Set { settings: _, responder } => {
+                let _ = responder.send(&mut Ok(()));
+            }
+            DoNotDisturbRequest::Watch { responder: _ } => {
+                panic!("Unexpected call to watch");
+            }
+        });
+
+        let output = utils::assert_set!(command(proxy, expected_do_not_disturb));
+        assert_eq!(
+            output,
+            format!("Successfully set DoNotDisturb to {:?}", expected_do_not_disturb)
+        );
+        Ok(())
+    }
+
+    #[test_case(
+        DoNotDisturb {
+            user_dnd: None,
+            night_mode_dnd: None,
+        };
+        "Test do not disturb watch() output with empty DoNotDisturb."
+    )]
+    #[test_case(
+        DoNotDisturb {
+            user_dnd: None,
+            night_mode_dnd: Some(false),
+        };
+        "Test do not disturb watch() output with non-empty DoNotDisturb."
+    )]
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn validate_do_not_disturb_watch_output(
+        expected_do_not_disturb: DoNotDisturb,
+    ) -> Result<()> {
+        let proxy = setup_fake_do_not_disturb_proxy(move |req| match req {
+            DoNotDisturbRequest::Set { settings: _, responder: _ } => {
+                panic!("Unexpected call to set");
+            }
+            DoNotDisturbRequest::Watch { responder } => {
+                let _ = responder.send(DoNotDisturbSettings::from(expected_do_not_disturb));
+            }
+        });
+
+        let output = utils::assert_watch!(command(
+            proxy,
+            DoNotDisturb { user_dnd: None, night_mode_dnd: None }
+        ));
+        assert_eq!(output, format!("{:#?}", DoNotDisturbSettings::from(expected_do_not_disturb)));
+        Ok(())
+    }
+}
