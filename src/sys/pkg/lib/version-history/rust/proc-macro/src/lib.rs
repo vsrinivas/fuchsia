fe583e12632ac4dfd753d7931edbad10d0c2c7f2
Aaron Wood <aaronwood@google.com>
2022-01-29 16:20:53 +0000

    [version-history] Provide latest version and a validation fn
    This hardens the proc_macro against an empty version history list,
    adds a LATEST_VERSION macro, and a fn for validating that the
    given abi_revision is in the VERSION_HISTORY list.
    
    Change-Id: Ib6552742e2506c75098fc764fbe0075427e159a1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/639475
    Fuchsia-Auto-Submit: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/sys/pkg/lib/version-history/rust/proc-macro/src/lib.rs
+++ src/sys/pkg/lib/version-history/rust/proc-macro/src/lib.rs
@@ -7,6 +7,9 @@ use {proc_macro::TokenStream, version_history_shared::version_history};
 #[proc_macro]
 pub fn declare_version_history(_tokens: TokenStream) -> TokenStream {
     let versions = version_history().expect("version-history.json to be parsed");
+    if versions.is_empty() {
+        panic!("version-history.json did not contain any versions");
+    }
 
     let mut tokens = String::from("[");
     for version in versions {
@@ -18,4 +21,17 @@ pub fn declare_version_history(_tokens: TokenStream) -> TokenStream {
     tokens.push_str("]");
 
     tokens.parse().unwrap()
+}
+
+#[proc_macro]
+pub fn latest_sdk_version(_tokens: TokenStream) -> TokenStream {
+    let versions = version_history().expect("version-history.json to be parsed");
+    let latest_version =
+        versions.last().expect("version-history.json did not contain any versions");
+    format!(
+        "Version {{ api_level: {}, abi_revision: {} }}",
+        latest_version.api_level, latest_version.abi_revision
+    )
+    .parse()
+    .unwrap()
 }
--- src/sys/pkg/lib/version-history/rust/src/lib.rs
+++ src/sys/pkg/lib/version-history/rust/src/lib.rs
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// VERSION_HISTORY is an array of all the known SDK versions.
+/// VERSION_HISTORY is an array of all the known SDK versions.  It is guaranteed
+/// (at compile-time) by the proc_macro to be non-empty.
 pub const VERSION_HISTORY: &[Version] = &version_history_macro::declare_version_history!();
+
+/// LATEST_VERSION is the latest known SDK version.
+pub const LATEST_VERSION: &Version = &version_history_macro::latest_sdk_version!();
 
 /// Version is a mapping between the supported API level and the ABI revisions.
 ///
@@ -20,21 +24,57 @@ pub struct Version {
     pub abi_revision: u64,
 }
 
+/// Returns true if the given abi_revision is listed in the VERSION_HISTORY of
+/// known SDK versions.
+pub fn is_valid_abi_revision(abi_revision: u64) -> bool {
+    VERSION_HISTORY.iter().any(|v| v.abi_revision == abi_revision)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use proptest::prelude::*;
+
+    // Helper to convert from the shared crate's Version struct to this crate's
+    // struct.
+    fn version_history_from_shared() -> Vec<Version> {
+        version_history_shared::version_history()
+            .unwrap()
+            .into_iter()
+            .map(|v| Version { api_level: v.api_level, abi_revision: v.abi_revision })
+            .collect::<Vec<_>>()
+    }
 
     #[test]
     fn test_proc_macro_worked() {
-        let expected = version_history_shared::version_history()
-            .unwrap()
-            .into_iter()
-            .map(|version| Version {
-                api_level: version.api_level,
-                abi_revision: version.abi_revision,
-            })
-            .collect::<Vec<_>>();
-
+        let expected = version_history_from_shared();
         assert_eq!(expected, VERSION_HISTORY);
+    }
+
+    #[test]
+    fn test_latest_version_proc_macro() {
+        let shared_versions = version_history_from_shared();
+        let expected = shared_versions.last().expect("version_history_shared was empty");
+        assert_eq!(expected, &version_history_macro::latest_sdk_version!());
+    }
+
+    #[test]
+    fn test_valid_abi_revision() {
+        for version in VERSION_HISTORY {
+            assert!(is_valid_abi_revision(version.abi_revision));
+        }
+    }
+
+    // To ensure this test doesn't flake, proptest is used to generate u64
+    // values which are not in the current VERSION_HISTORY list.
+    proptest! {
+        #[test]
+        fn test_invalid_abi_revision(abi_revision in any::<u64>().prop_filter("using u64 that isn't in VERSION_HISTORY", |abi_revision|
+            // The randomly chosen 'abi_revision' must not equal any of the
+            // abi_revisions in the VERSION_HISTORY list.
+            VERSION_HISTORY.iter().all(|v| &v.abi_revision != abi_revision)
+        )) {
+            assert!(!is_valid_abi_revision(abi_revision))
+        }
     }
 }
