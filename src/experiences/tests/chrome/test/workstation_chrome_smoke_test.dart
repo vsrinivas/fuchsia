994a91692db80b5afa7bc928bdd5bd086b4685ba
Yeonhee Lee <yhlee@google.com>
2022-02-01 01:26:35 +0000

    [workstation_pro][chrome_smoke] Added screenshot test
    Improving workstation_chrome_smoke_test by making it take a screenshot to see if a blank white page of the Chromium view is rendered.
    The original test failed to catch a crash because Ermine seemingly succeeded to present a Chromium view but just failed to render it due to the crash. Screencapturing is to prevent false positive cases like this one.
    Ideally, we should enable workstation_chrome_advanced_smoke_test, which runs a local test server and checks if a certain page served by the server is loaded, but it needs to be deflaked before being moved to CI.
    In the meantime, enhancing this basic smoke test so that it can avoid false passing as much as it can.
    
    Tested on a local machine 10 times and all passed.
    
    Bug: 92206
    
    Change-Id: I90881bb8e1c0d472f63240ba87575ccd46339007
    Reviewed-on: https://fuchsia-review.googlesource.com/c/experiences/+/639343
    Reviewed-by: Sanjay Chouksey <sanjayc@google.com>
    Commit-Queue: Yeonhee Lee <yhlee@google.com>

--- src/experiences/tests/chrome/test/workstation_chrome_smoke_test.dart
+++ src/experiences/tests/chrome/test/workstation_chrome_smoke_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 // ignore_for_file: import_of_legacy_library_into_null_safe
+import 'dart:math';
 
 import 'package:ermine_driver/ermine_driver.dart';
 import 'package:fidl_fuchsia_input/fidl_async.dart';
@@ -41,6 +42,25 @@ void main() {
     expect(snapshot.focused, true);
     expect(snapshot.url, chromeUrl);
     print('A Chrome view is presented');
+
+    // Takes a screenshot and checks the color
+    const white = 0xffffffff; // (0xAABBGGRR)
+    Map<int, int> histogram;
+
+    await Future.delayed(Duration(seconds: 3));
+    final isWhite = await ermine.waitFor(() async {
+      print('Take a screenshot...');
+      final screenshot = await ermine.screenshot(Rectangle(500, 500, 100, 100));
+      histogram = ermine.histogram(screenshot);
+      print('Color key: ${histogram.keys.first}');
+      print('Color value: ${histogram.values.first}');
+      if (histogram.keys.length == 1 && histogram[white] == 10000) {
+        return true;
+      }
+      return false;
+    }, timeout: Duration(minutes: 2));
+
+    expect(isWhite, isTrue);
 
     // Close the Chrome view.
     await ermine.threeKeyShortcut(Key.leftCtrl, Key.leftShift, Key.w);
