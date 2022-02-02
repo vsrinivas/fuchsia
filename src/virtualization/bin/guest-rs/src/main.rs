4d556ee8c001bbe2e593e4ac71a2a636d3a874f8
Jenna Macdonald <polyrain@google.com>
2022-02-02 00:03:11 +0000

    [virtualization] Add argument parsing for `guest-rs`
    This CL adds argument parsing support for the various functions of
    'guest-rs'.
    
    Branched from fxr/639003, which had some review comments (since
    resolved).
    
    Change-Id: Ia24a04631ade1bdaa2314b7cb99eb5c586f00563
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/639008
    Reviewed-by: David Hastings <dahastin@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Reviewed-by: Alexander Radchenko <alerad@google.com>
    Commit-Queue: Jenna Macdonald <polyrain@google.com>

--- src/virtualization/bin/guest-rs/src/main.rs
+++ src/virtualization/bin/guest-rs/src/main.rs
@@ -2,6 +2,132 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-fn main() {
-    println!("Hello, world!");
+use {anyhow::Error, argh::FromArgs, fuchsia_async as fasync};
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Top-level command.
+struct GuestOptions {
+    #[argh(subcommand)]
+    nested: SubCommands,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum SubCommands {
+    One(Launch),
+    Two(Balloon),
+    Three(BalloonStats),
+    Four(Serial),
+    Five(List),
+    Six(Socat),
+    Seven(SocatListen),
+    Eight(Vsh),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Launch a guest image. Usage: guest-rs launch --package <package> [--vmm_args <args>]
+#[argh(subcommand, name = "launch")]
+struct Launch {
+    #[argh(option)]
+    /// package name to launch e.g. 'zircon_guest'.
+    package: String,
+    /// arguments to configure guest image with.
+    #[argh(option)]
+    vmm_args: Vec<String>,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Modify the size of a memory balloon. Usage: guest-rs balloon --env_id <id> --cid <id> --num_pages <num>
+#[argh(subcommand, name = "balloon")]
+struct Balloon {
+    #[argh(option)]
+    /// environment id where guest lives.
+    env_id: u32,
+    #[argh(option)]
+    /// context id of guest.
+    cid: u32,
+    #[argh(option)]
+    /// number of pages guest balloon will have after use.
+    num_pages: u32,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// See the stats of a guest's memory balloon. Usage: guest-rs balloon-stats --env_id <id> --cid <id>
+#[argh(subcommand, name = "balloon-stats")]
+struct BalloonStats {
+    #[argh(option)]
+    /// environment id where guest lives.
+    env_id: u32,
+    #[argh(option)]
+    /// context id of guest.
+    cid: u32,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Access the serial output for a guest. Usage: guest-rs serial --env_id <id> --cid <id>
+#[argh(subcommand, name = "serial")]
+struct Serial {
+    #[argh(option)]
+    /// environment id where guest lives.
+    env_id: u32,
+    #[argh(option)]
+    /// context id of guest.
+    cid: u32,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// List existing guest environments. Usage: guest-rs list
+#[argh(subcommand, name = "list")]
+struct List {}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Create a socat connection on the specified port. Usage: guest-rs socat --env_id <id> --cid <id> --port <num>
+#[argh(subcommand, name = "socat")]
+struct Socat {
+    #[argh(option)]
+    /// environment id where guest lives.
+    env_id: u32,
+    #[argh(option)]
+    /// context id of guest.
+    cid: u32,
+    #[argh(option)]
+    /// port for listeners to connect on.
+    port: u32,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Listen through socat on the specified port. Usage: guest-rs socat-listen --env_id <id> --host_port <num>
+#[argh(subcommand, name = "socat-listen")]
+struct SocatListen {
+    #[argh(option)]
+    /// environment id of host.
+    env_id: u32,
+    #[argh(option)]
+    /// port number of host (see `guest-rs socat`)
+    host_port: u32,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Create virtual shell for a guest or connect via virtual shell. Usage: guest-rs [--env_id <id> [--cid <id> [--port [num]]]] [--args <args>]
+#[argh(subcommand, name = "vsh")]
+struct Vsh {
+    #[argh(option)]
+    /// optional environment id of host.
+    env_id: Option<u32>,
+    #[argh(option)]
+    /// optional context id of vsh to connect to.
+    cid: Option<u32>,
+    #[argh(option)]
+    /// optional port of a vsh socket to connect to.
+    port: Option<u32>,
+    #[argh(option)]
+    /// list of arguments to run non-interactively on launch.
+    args: Vec<String>,
+}
+
+#[fasync::run_singlethreaded]
+async fn main() -> Result<(), Error> {
+    let options: GuestOptions = argh::from_env();
+    println!("{:#?}", options);
+    Ok(())
 }
