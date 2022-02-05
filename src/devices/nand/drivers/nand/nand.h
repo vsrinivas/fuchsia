8d815ce7602890c5ea3db745790e7ea5043a2a96
Braden Kell <bradenkell@google.com>
2022-02-05 00:19:37 +0000

    [nand][core] Stop handling requests after driver suspend hook
    Error out all current and future requests after the suspend hook is
    called. The rawnand driver doesn't necessarily have to also handle this
    hook because the core driver will be suspended first.
    
    Test: fx test nand-unittest
    Test: Rebooted Astro, didn't see messages from core NAND driver about
          reads being retried
    Change-Id: I2a82a985a343a5d588f1fdb26bfc72a5e92efa0e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/640213
    Reviewed-by: Martin Lindsay <mlindsay@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Braden Kell <bradenkell@google.com>
    (cherry picked from commit a3f1a53d02f0b910983c72171defe8dec7adf13e)

--- src/devices/nand/drivers/nand/nand.h
+++ src/devices/nand/drivers/nand/nand.h
@@ -28,7 +28,7 @@ namespace nand {
 using Transaction = nand::BorrowedOperation<>;
 
 class NandDevice;
-using DeviceType = ddk::Device<NandDevice, ddk::GetSizable>;
+using DeviceType = ddk::Device<NandDevice, ddk::GetSizable, ddk::Suspendable>;
 
 class NandDevice : public DeviceType, public ddk::NandProtocol<NandDevice, ddk::base_protocol> {
  public:
@@ -48,6 +48,7 @@ class NandDevice : public DeviceType, public ddk::NandProtocol<NandDevice, ddk::
 
   // Device protocol implementation.
   zx_off_t DdkGetSize() { return device_get_size(parent()); }
+  void DdkSuspend(ddk::SuspendTxn txn);
   void DdkRelease();
 
   // Nand protocol implementation.
@@ -78,6 +79,8 @@ class NandDevice : public DeviceType, public ddk::NandProtocol<NandDevice, ddk::
 
   void DoIo(Transaction txn);
   zx_status_t WorkerThread();
+
+  void Shutdown();
 
   ddk::RawNandProtocolClient raw_nand_;
 
