91498281e7cfcfd2315ffb0fd58f8805d0a92267
Paul Daniel Faria <paulfaria@google.com>
2022-02-10 22:06:52 +0000

    [setui] Enable clippy lints
    This will enable clippy lints for CL's in setui.
    
    Also add back in deny for unused_results, which was accidentally removed
    in an earlier CL.
    
    Test: N/A
    Change-Id: If7383d40fa7d2157d458ecddface17be9fe10f2c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/644843
    Reviewed-by: William Xiao <wxyz@google.com>
    Fuchsia-Auto-Submit: Paul Faria <paulfaria@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- garnet/bin/setui/src/lib.rs
+++ garnet/bin/setui/src/lib.rs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![deny(unused_results)]
+#![warn(clippy::all)]
+
 use crate::accessibility::accessibility_controller::AccessibilityController;
 use crate::agent::authority::Authority;
 use crate::agent::{BlueprintHandle as AgentBlueprintHandle, Lifespan};
--- garnet/bin/setui/src/main.rs
+++ garnet/bin/setui/src/main.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![warn(clippy::all)]
+
 use anyhow::{Context, Error};
 use fuchsia_async as fasync;
 use fuchsia_component::client::connect_to_protocol;
