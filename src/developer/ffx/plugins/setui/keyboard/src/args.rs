cdb08bf02809ef35c4ea49306136624b96ec24f6
Sijie Chen <sijiec@google.com>
2022-02-15 18:54:45 +0000

    [ffx][setui] Migrate Keyboard
    This CL adds a subcommand, keyboard of ffx_setui.
    
    Bug: 76259
    Test:
    fx set workstation.qemu-x64 --auto-dir --with src/developer/ffx:tests
    fx build ffx
    fx ffx setui keyboard -> watch
    fx ffx setui keyboard -p 123ms -> set
    fx test ffx_setui_keyboard_test -o
    fx test ffx_setui_keyboard_args_test -o
    
    Change-Id: I74133223b0dde711f2b33871b254ee19dceccb54
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/645114
    Reviewed-by: William Xiao <wxyz@google.com>
    Commit-Queue: Sijie Chen <sijiec@google.com>

---
+++ src/developer/ffx/plugins/setui/keyboard/src/args.rs
@@ -0,0 +1,115 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use argh::FromArgs;
+use ffx_core::ffx_command;
+use fidl_fuchsia_settings::KeyboardSettings;
+use regex::Regex;
+use std::num::ParseIntError;
+
+#[ffx_command()]
+#[derive(FromArgs, Debug, PartialEq, Clone, Copy)]
+#[argh(subcommand, name = "keyboard")]
+/// get or set keyboard settings
+pub struct Keyboard {
+    /// keymap selection for the keyboard. Valid options are UsQwerty, FrAzerty, and UsDvorak.
+    #[argh(option, short = 'k', from_str_fn(str_to_keymap))]
+    pub keymap: Option<fidl_fuchsia_input::KeymapId>,
+
+    /// delay value of autorepeat values for the keyboard. Values should be a positive integer plus
+    /// an SI time unit. Valid units are s, ms. If this value and autorepeat_period are zero, the
+    /// autorepeat field of KeyboardSettings will be cleaned as None.
+    #[argh(option, short = 'd', from_str_fn(str_to_duration))]
+    pub autorepeat_delay: Option<i64>,
+
+    /// period value of autorepeat values for the keyboard. Values should be a positive integer plus
+    /// an SI time unit. Valid units are s, ms. If this value and autorepeat_delay are zero, the
+    /// autorepeat field of KeyboardSettings will be cleaned as None.
+    #[argh(option, short = 'p', from_str_fn(str_to_duration))]
+    pub autorepeat_period: Option<i64>,
+}
+
+impl From<KeyboardSettings> for Keyboard {
+    fn from(src: KeyboardSettings) -> Self {
+        Keyboard {
+            keymap: src.keymap,
+            autorepeat_delay: src.autorepeat.map(|a| a.delay),
+            autorepeat_period: src.autorepeat.map(|a| a.period),
+        }
+    }
+}
+
+impl From<Keyboard> for KeyboardSettings {
+    fn from(src: Keyboard) -> KeyboardSettings {
+        KeyboardSettings {
+            keymap: src.keymap,
+            autorepeat: if src.autorepeat_delay.is_none() && src.autorepeat_period.is_none() {
+                None
+            } else {
+                Some(fidl_fuchsia_settings::Autorepeat {
+                    delay: src.autorepeat_delay.unwrap_or(0),
+                    period: src.autorepeat_period.unwrap_or(0),
+                })
+            },
+            ..KeyboardSettings::EMPTY
+        }
+    }
+}
+
+/// Converts a single string of keymap id value into a fidl_fuchsia_input::KeymapId.
+fn str_to_keymap(src: &str) -> Result<fidl_fuchsia_input::KeymapId, String> {
+    Ok(match src.to_lowercase().as_str() {
+        "usqwerty" => fidl_fuchsia_input::KeymapId::UsQwerty,
+        "frazerty" => fidl_fuchsia_input::KeymapId::FrAzerty,
+        "usdvorak" => fidl_fuchsia_input::KeymapId::UsDvorak,
+        _ => return Err(String::from("Couldn't parse keymap id.")),
+    })
+}
+
+/// Converts a single string of number and unit into a number interpreting as nanoseconds.
+fn str_to_duration(src: &str) -> Result<i64, String> {
+    // This regex matches a string that starts with at least one digit, follows by zero or more
+    // whitespace, and ends with zero or more letters. Digits and letters are captured.
+    let re = Regex::new(r"^(\d+)\s*([A-Za-z]*)$").unwrap();
+    let captures = re.captures(src).ok_or_else(|| {
+        String::from("Invalid input, please pass in number and units as <123ms> or <0>.")
+    })?;
+
+    let num: i64 = captures[1].parse().map_err(|e: ParseIntError| e.to_string())?;
+    let unit = captures[2].to_string();
+
+    Ok(match unit.to_lowercase().as_str() {
+        "s" | "second" | "seconds" => num * 1_000_000_000,
+        "ms" | "millisecond" | "milliseconds" => num * 1_000_000,
+        _ => {
+            if unit.is_empty() && num == 0 {
+                0
+            } else {
+                return Err(String::from("Couldn't parse duration, please specify a valid unit."));
+            }
+        }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const CMD_NAME: &'static [&'static str] = &["keyboard"];
+
+    #[test]
+    fn test_keyboard_cmd() {
+        // Test input arguments are generated to according struct.
+        let keymap = "usqwerty";
+        let num = "123s";
+        let args = &["-k", keymap, "-d", num];
+        assert_eq!(
+            Keyboard::from_args(CMD_NAME, args),
+            Ok(Keyboard {
+                keymap: Some(str_to_keymap(keymap).unwrap()),
+                autorepeat_delay: Some(str_to_duration(num).unwrap()),
+                autorepeat_period: None
+            })
+        )
+    }
+}
---
+++ src/developer/ffx/plugins/setui/keyboard/src/lib.rs
@@ -0,0 +1,154 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::format_err;
+use anyhow::Result;
+use ffx_core::ffx_plugin;
+use ffx_setui_keyboard_args::Keyboard;
+use fidl_fuchsia_settings::{KeyboardProxy, KeyboardSettings};
+use utils::handle_mixed_result;
+use utils::{self, Either, WatchOrSetResult};
+
+#[ffx_plugin("setui", KeyboardProxy = "core/setui_service:expose:fuchsia.settings.Keyboard")]
+pub async fn run_command(keyboard_proxy: KeyboardProxy, keyboard: Keyboard) -> Result<()> {
+    handle_mixed_result("Keyboard", command(keyboard_proxy, keyboard).await).await?;
+
+    Ok(())
+}
+
+async fn command(proxy: KeyboardProxy, keyboard: Keyboard) -> WatchOrSetResult {
+    if keyboard.autorepeat_delay.unwrap_or(0) < 0 || keyboard.autorepeat_period.unwrap_or(0) < 0 {
+        return Err(format_err!("Negative values are invalid for autorepeat values."));
+    }
+    let settings = KeyboardSettings::from(keyboard);
+
+    if settings == KeyboardSettings::EMPTY {
+        Ok(Either::Watch(utils::watch_to_stream(proxy, |p| p.watch())))
+    } else {
+        Ok(Either::Set(if let Err(err) = proxy.set(settings).await? {
+            format!("{:?}", err)
+        } else {
+            format!("Successfully set Keyboard to {:?}", keyboard)
+        }))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use fidl_fuchsia_settings::{KeyboardRequest, KeyboardSettings};
+    use futures::prelude::*;
+    use test_case::test_case;
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn test_run_command() {
+        const NUM: i64 = 7;
+
+        let proxy = setup_fake_keyboard_proxy(move |req| match req {
+            KeyboardRequest::Set { settings: _, responder } => {
+                let _ = responder.send(&mut Ok(()));
+            }
+            KeyboardRequest::Watch { responder: _ } => {
+                panic!("Unexpected call to watch");
+            }
+        });
+
+        let keyboard =
+            Keyboard { keymap: None, autorepeat_delay: Some(NUM), autorepeat_period: Some(NUM) };
+        let response = run_command(proxy, keyboard).await;
+        assert!(response.is_ok());
+    }
+
+    #[test_case(
+        Keyboard {
+            keymap: Some(fidl_fuchsia_input::KeymapId::FrAzerty),
+            autorepeat_delay: Some(-1),
+            autorepeat_period: Some(-2),
+        }; "Test keyboard invalid autorepeat inputs."
+    )]
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn validate_keyboard_failure(expected_keyboard: Keyboard) -> Result<()> {
+        let proxy = setup_fake_keyboard_proxy(move |req| match req {
+            KeyboardRequest::Set { settings: _, responder } => {
+                let _ = responder.send(&mut Ok(()));
+            }
+            KeyboardRequest::Watch { responder } => {
+                let _ = responder.send(KeyboardSettings::from(expected_keyboard));
+            }
+        });
+
+        let result = command(proxy, expected_keyboard).await;
+        match result {
+            Err(e) => {
+                assert!(format!("{:?}", e)
+                    .contains("Negative values are invalid for autorepeat values."))
+            }
+            _ => panic!("Should return errors."),
+        }
+        Ok(())
+    }
+
+    #[test_case(
+        Keyboard {
+            keymap: Some(fidl_fuchsia_input::KeymapId::UsQwerty),
+            autorepeat_delay: Some(2),
+            autorepeat_period: Some(3),
+        }; "Test keyboard set() output."
+    )]
+    #[test_case(
+        Keyboard {
+            keymap: Some(fidl_fuchsia_input::KeymapId::UsDvorak),
+            autorepeat_delay: Some(3),
+            autorepeat_period: Some(4),
+        }; "Test keyboard set() output with different values."
+    )]
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn validate_keyboard_set_output(expected_keyboard: Keyboard) -> Result<()> {
+        let proxy = setup_fake_keyboard_proxy(move |req| match req {
+            KeyboardRequest::Set { settings: _, responder } => {
+                let _ = responder.send(&mut Ok(()));
+            }
+            KeyboardRequest::Watch { responder: _ } => {
+                panic!("Unexpected call to watch");
+            }
+        });
+
+        let output = utils::assert_set!(command(proxy, expected_keyboard));
+        assert_eq!(output, format!("Successfully set Keyboard to {:?}", expected_keyboard));
+        Ok(())
+    }
+
+    #[test_case(
+        Keyboard {
+            keymap: None,
+            autorepeat_delay: Some(0),
+            autorepeat_period: Some(0),
+        }; "Test keyboard watch() output with empty Keyboard."
+    )]
+    #[test_case(
+        Keyboard {
+            keymap: Some(fidl_fuchsia_input::KeymapId::UsDvorak),
+            autorepeat_delay: Some(7),
+            autorepeat_period: Some(8),
+        }; "Test keyboard watch() output with non-empty Keyboard."
+    )]
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn validate_keyboard_watch_output(expected_keyboard: Keyboard) -> Result<()> {
+        let proxy = setup_fake_keyboard_proxy(move |req| match req {
+            KeyboardRequest::Set { settings: _, responder: _ } => {
+                panic!("Unexpected call to set");
+            }
+            KeyboardRequest::Watch { responder } => {
+                let _ = responder.send(KeyboardSettings::from(expected_keyboard));
+            }
+        });
+
+        let output = utils::assert_watch!(command(
+            proxy,
+            Keyboard { keymap: None, autorepeat_delay: None, autorepeat_period: None }
+        ));
+        assert_eq!(output, format!("{:#?}", KeyboardSettings::from(expected_keyboard)));
+        Ok(())
+    }
+}
