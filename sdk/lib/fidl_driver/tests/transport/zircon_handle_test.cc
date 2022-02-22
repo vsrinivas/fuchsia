4fcb7c17b7396a4196a0e313750ec3e4db1b315d
Benjamin Prosnitz <bprosnitz@google.com>
2022-02-22 23:01:07 +0000

    [fidl_driver] Enable zircon handle sending test
    This test tests sending a zircon handle over the driver
    transport.
    
    Multiply: fidl_driver_transport_tests (fuchsia): 100
    Change-Id: Iaa376bf726309bb38b0197322822926b8ff499e6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/649185
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>

--- sdk/lib/fidl_driver/tests/transport/zircon_handle_test.cc
+++ sdk/lib/fidl_driver/tests/transport/zircon_handle_test.cc
@@ -23,8 +23,7 @@ class TestServer : public fdf::WireServer<test_transport::SendZirconHandleTest> 
   }
 };
 
-// TODO(fxbug.dev/92488): Investigate use-after-free in |driver_runtime::Dispatcher|.
-TEST(DriverTransport, DISABLED_SendZirconHandle) {
+TEST(DriverTransport, SendZirconHandle) {
   void* driver = reinterpret_cast<void*>(uintptr_t(1));
   fdf_internal_push_driver(driver);
   auto deferred = fit::defer([]() { fdf_internal_pop_driver(); });
