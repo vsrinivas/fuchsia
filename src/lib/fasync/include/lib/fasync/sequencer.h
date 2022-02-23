11cab2f97fb15e99634cefbc6b2b5545909a5af0
Mark Schott <schottm@google.com>
2022-02-23 02:12:31 +0000

    [fasync] Add fasync::sequencer
    fasync::sequencer is another wrapper type like fasync::scope that
    modifies the behavior of the wrapped futures, in this case imposing a
    FIFO ordering that can be useful for e.g. thread safety.
    
    Test: fasync-tests
    Change-Id: I28346fafe736c7af85ed03a912f475686ffacd13
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/627374
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Commit-Queue: Mark Schott <schottm@google.com>

---
+++ src/lib/fasync/include/lib/fasync/sequencer.h
@@ -0,0 +1,100 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_LIB_FASYNC_INCLUDE_LIB_FASYNC_SEQUENCER_H_
+#define SRC_LIB_FASYNC_INCLUDE_LIB_FASYNC_SEQUENCER_H_
+
+#include <lib/fasync/internal/compiler.h>
+
+LIB_FASYNC_CPP_VERSION_COMPAT_BEGIN
+
+#include <assert.h>
+#include <lib/fasync/bridge.h>
+#include <lib/fit/thread_safety.h>
+
+#include <mutex>
+
+namespace fasync {
+
+// |fasync::sequencer|
+//
+// A sequencer imposes a first-in-first-out sequential execution order onto a sequence of futures.
+// Each successively enqueued future remains suspended until all previously enqueued futures
+// complete or are abandoned.
+//
+// |fasync::sequencer| is designed to be used either on its own or chained onto a future using
+// |fasync::future::wrap_with()|.
+//
+// EXAMPLE
+//
+//     // This wrapper type is intended to be applied to a sequence of futures so we store it in a
+//     // variable.
+//     fasync::sequencer seq;
+//
+//     // This task consists of some amount of work that must be completed sequentially followed by
+//     // other work that can happen in any order. We use |wrap_with()| to wrap the sequential work
+//     // with the sequencer.
+//     fasync::future<> perform_complex_task() {
+//         return fasync::make_future([] { /* Do sequential work. */ }) |
+//             fasync::then([] (fitx::result<fitx::failed>& result) {
+//                 /* This will also be wrapped. */
+//             }) |
+//             fasync::wrap_with(seq) |
+//             fasync::then([] (fitx::result<fitx::failed>& result) { /* Do more work. */ });
+//     }
+//
+class sequencer final {
+ public:
+  sequencer() {
+    // Capture a new consumer and intentionally abandon its associated completer so that a future
+    // chained onto the consumer using |future_or()| will become immediately runnable.
+    fasync::bridge<fitx::failed> bridge;
+    prior_ = std::move(bridge.consumer);
+  }
+
+  ~sequencer() = default;
+
+  // Returns a new future which will invoke |future| after all previously enqueued futures on this
+  // sequencer have completed or been abandoned.
+  //
+  // This method is thread-safe.
+  template <typename F>
+  decltype(auto) wrap(F&& future) {
+    fasync::bridge<fitx::failed> bridge;
+    fasync::consumer<fitx::failed> prior = swap_prior(std::move(bridge.consumer));
+    return prior.future_or(fitx::ok()) |
+           fasync::then([future = std::move(future), completer = std::move(bridge.completer)](
+                            fasync::context& context) mutable {
+             //  This handler will run once the completer associated with the |prior| future is
+             //  abandoned. Once the future has finished, both the future and completer will be
+             //  destroyed thereby causing the next future chained onto the |bridge|'s associated
+             //  consumer to become runnable.
+             return cpp20::invoke(future, context);
+           });
+  }
+
+  constexpr sequencer(const sequencer&) = delete;
+  constexpr sequencer(sequencer&&) = delete;
+  constexpr sequencer& operator=(const sequencer&) = delete;
+  constexpr sequencer& operator=(sequencer&&) = delete;
+
+ private:
+  fasync::consumer<fitx::failed> swap_prior(fasync::consumer<fitx::failed> new_prior) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    fasync::consumer<fitx::failed> old_prior = std::move(prior_);
+    prior_ = std::move(new_prior);
+    return old_prior;
+  }
+
+  std::mutex mutex_;
+
+  // Holds the consumption capability of the most recently wrapped future.
+  fasync::consumer<fitx::failed> prior_ FIT_GUARDED(mutex_);
+};
+
+}  // namespace fasync
+
+LIB_FASYNC_CPP_VERSION_COMPAT_END
+
+#endif  // SRC_LIB_FASYNC_INCLUDE_LIB_FASYNC_SEQUENCER_H_
---
+++ src/lib/fasync/tests/sequencer_tests.cc
@@ -0,0 +1,115 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/fasync/bridge.h>
+#include <lib/fasync/sequencer.h>
+#include <lib/fasync/single_threaded_executor.h>
+#include <unistd.h>
+
+#include <string>
+#include <thread>
+
+#include <zxtest/zxtest.h>
+
+#undef assert
+#define assert(expr)                                                             \
+  do {                                                                           \
+    if (!(expr)) {                                                               \
+      std::cout << "Assert " << #expr << " failed at:" << __LINE__ << std::endl; \
+      abort();                                                                   \
+    }                                                                            \
+  } while (false)
+
+namespace {
+
+TEST(SequencerTests, sequencing_tasks) {
+  fasync::sequencer seq;
+  std::string str;
+
+  // This future writes ":a" sequentially then writes ":a2" later.
+  auto a = fasync::make_future([&] { str += ":a"; }) | fasync::wrap_with(seq) |
+           fasync::then([&] { str += ":a2"; });
+
+  // This future writes ":b" sequentially then writes ":b2" and ":b3" later.
+  // Also schedules another sequential task that writes ":e".
+  auto b = fasync::make_future([&](fasync::context& context) {
+             str += ":b";
+             context.executor().schedule(fasync::make_future([&] { str += ":e"; }) |
+                                         fasync::wrap_with(seq));
+           }) |
+           fasync::wrap_with(seq) |
+           fasync::then(
+               [&, count = 0](fasync::context& context) mutable -> fasync::try_poll<fitx::failed> {
+                 if (++count == 5) {
+                   str += ":b3";
+                   return fasync::ready(fitx::failed());
+                 }
+                 str += ":b2";
+                 context.suspend_task().resume();  // immediately resume
+                 return fasync::pending();
+               });
+
+  static_assert(fasync::is_try_future_v<decltype(b)>);
+
+  // This future writes ":c" sequentially then abandons itself.
+  auto c = fasync::make_future([&](fasync::context& context) {
+             str += ":c";
+             context.suspend_task();  // abandon result
+             return fasync::pending();
+           }) |
+           fasync::wrap_with(seq) | fasync::then([&] { str += ":c2"; });
+
+  // This future writes ":d" sequentially.
+  auto d = fasync::make_future([&] { str += ":d"; }) | fasync::wrap_with(seq);
+
+  // These futures just write ":z1" and ":z2" whenever they happen to run.
+  auto z1 = fasync::make_future([&] { str += ":z1"; });
+  auto z2 = fasync::make_future([&] { str += ":z2"; });
+
+  // Schedule the futures in an order which intentionally does not match the sequencing.
+  fasync::single_threaded_executor executor;
+  executor.schedule(std::move(z1));
+  executor.schedule(std::move(b));
+  executor.schedule(std::move(c));
+  executor.schedule(std::move(a));
+  executor.schedule(std::move(d));
+  executor.schedule(std::move(z2));
+  executor.run();
+
+  // Evaluate the futures and check the execution order.
+  EXPECT_STREQ(":z1:a:a2:z2:b:b2:c:b2:d:b2:e:b2:b3", str.c_str());
+}
+
+TEST(SequencerTests, thread_safety) {
+  fasync::sequencer seq;
+  fasync::single_threaded_executor executor;
+  uint64_t run_count = 0;
+
+  // Schedule work from a few threads, just to show that we can.
+  constexpr int num_threads = 4;
+  constexpr int num_tasks_per_thread = 100;
+  std::thread threads[num_threads];
+  for (int i = 0; i < num_threads; i++) {
+    fasync::bridge<fitx::failed> bridge;
+    threads[i] = std::thread([&, completer = std::move(bridge.completer)]() mutable {
+      for (int j = 0; j < num_tasks_per_thread; j++) {
+        executor.schedule(fasync::make_future([&] { run_count++; }) | fasync::wrap_with(seq));
+        usleep(1);
+      }
+      completer.complete_ok();
+    });
+    executor.schedule(bridge.consumer.future());
+  }
+
+  // Run the tasks.
+  executor.run();
+  for (int i = 0; i < num_threads; i++) {
+    threads[i].join();
+  }
+
+  // We expect all tasks to have run.
+  EXPECT_EQ(num_threads * num_tasks_per_thread, run_count);
+}
+
+}  // namespace
