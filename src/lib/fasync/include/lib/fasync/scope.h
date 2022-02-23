d37b2c7d6defccb88a86fcf0e21576ab87f2da75
Mark Schott <schottm@google.com>
2022-02-23 00:48:41 +0000

    [fasync] Add fasync::scope
    fasync::scope is an important mechanism for lifetime management so that
    futures don't use (for example) dangling references after the referents
    have died. One places an fasync::scope in the same scope as such
    references or other resources and the relevant futures (once wrapped)
    will automatically be canceled when the fasync::scope is destroyed.
    
    Test: fasync-tests
    Change-Id: I9c4427cd727e7b7e9b55cd381dc4784672a1f11b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/627373
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Commit-Queue: Mark Schott <schottm@google.com>

---
+++ src/lib/fasync/include/lib/fasync/scope.h
@@ -0,0 +1,375 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_LIB_FASYNC_INCLUDE_LIB_FASYNC_SCOPE_H_
+#define SRC_LIB_FASYNC_INCLUDE_LIB_FASYNC_SCOPE_H_
+
+#include <lib/fasync/internal/compiler.h>
+
+LIB_FASYNC_CPP_VERSION_COMPAT_BEGIN
+
+#include <assert.h>
+#include <lib/fasync/future.h>
+#include <lib/fit/thread_safety.h>
+
+#include <atomic>
+#include <mutex>
+
+namespace fasync {
+
+// |fasync::scope|
+//
+// Provides a mechanism for binding futures to the lifetime of another object such that they are
+// destroyed before that object goes out of scope. It is particularly useful for ensuring that the
+// lifetime of a future does not exceed the lifetime of any variables that it has captured by
+// reference.
+//
+// A scope is thread-safe but non-reentrant: it must not be destroyed while any of its associated
+// futures are running.
+//
+// EXAMPLE
+//
+// Define a |fasync::scope| as a member of the object to whose lifetime the futures should be bound.
+//
+//     // We mark this class final because its destructor has side-effects that rely on the order of
+//     // destruction. If this object were subclassed there would be a possibility for futures bound
+//     // to its scope to inadvertently access the subclass's state while the object was being
+//     // destroyed.
+//     class accumulator final {
+//     public:
+//         accumulator() = default;
+//         ~accumulator() = default;
+//
+//         fasync::future<int> accumulate(int value);
+//
+//     private:
+//         int prior_total_ = 0;
+//
+//         // This member is last so that the scope is exited before all other members of the object
+//         // are destroyed. Alternately, we could enforce this ordering by explicitly invoking
+//         // |fasync::scope::exit()| where appropriate.
+//         fasync::scope scope_;
+//     };
+//
+// Use |fasync::wrap_with()| to wrap up futures that capture pointers to the object. In this
+// example, the captured pointer is "this".
+//
+//     fasync::future<int> accumulator::accumulate(int value) {
+//         return fasync::make_future([this, value] {
+//             prior_total_ += value;
+//             return fitx::ok(prior_total_);
+//         }) | fasync::wrap_with(scope_); // Binding to scope happens here.
+//     }
+//
+class scope final {
+ public:
+  // Creates a new scope.
+  scope() : state_(new state()) {}
+
+  // Exits the scope and destroys all of its wrapped futures.
+  // Asserts that no futures are currently running.
+  ~scope() { state_->exit(true /*scope_was_destroyed*/); }
+
+  constexpr scope(const scope&) = delete;
+  constexpr scope& operator=(const scope&) = delete;
+  constexpr scope(scope&&) = delete;
+  constexpr scope& operator=(scope&&) = delete;
+
+  // Returns true if the scope has been exited.
+  //
+  // This method is thread-safe.
+  bool exited() const { return state_->exited(); }
+
+  // Exits the scope and destroys all of its wrapped futures.
+  // Assets that no futures are currently running.
+  //
+  // This method is thread-safe.
+  void exit() { return state_->exit(false /*scope_was_destroyed*/); }
+
+  // Returns a future which wraps the specified |future| and binds the future to this scope.
+  //
+  // The specified future will automatically be destroyed when its wrapper is destroyed or when the
+  // scope is exited. If the scope has already exited then the wrapped future will be immediately
+  // destroyed.
+  //
+  // When the returned future is invoked before the scope is exited, the future that it wraps will
+  // be invoked as usual. However, when the returned future is invoked after the scope is exited, it
+  // immediately returns a pending result (since the future that it previously wrapped has already
+  // been destroyed). By returning a pending result, the return future effectively indicates to the
+  // executor that the task has been "abandoned" due to the scope being exited.
+  //
+  // This method is thread-safe.
+  template <typename F>
+  auto wrap(F&& future) {
+    return fasync::make_future(
+        scoped_future<F>(state_->adopt_future(new future_holder<F>(std::move(future)))));
+  }
+
+ private:
+  class state;
+  class future_holder_base;
+
+  // Holds a reference to a future that is owned by the state.
+  class future_handle final {
+   public:
+    constexpr future_handle() = default;
+
+   private:
+    // |state| and |future| belong to the state object.
+    // Invariant: If |future_holder| is non-null then |state| is also non-null.
+    friend state;
+    constexpr future_handle(state* state, future_holder_base* future_holder)
+        : state_(state), future_holder_(future_holder) {}
+
+    state* state_ = nullptr;
+    future_holder_base* future_holder_ = nullptr;
+  };
+
+  // Holds the shared state of the scope.
+  // This object is destroyed once the scope and all of its futures have been destroyed.
+  class state final {
+   public:
+    constexpr state() = default;
+    ~state() {
+      assert(acquired_future_count_.load(std::memory_order_relaxed) == scope_exited);
+      assert(scope_was_destroyed_);
+      assert(future_handle_count_ == 0);
+      assert(head_future_holder_ == nullptr);
+    }
+
+    constexpr state(const state&) = delete;
+    constexpr state& operator=(const state&) = delete;
+    constexpr state(state&&) = delete;
+    constexpr state& operator=(state&&) = delete;
+
+    // The following methods are called from the |scope|.
+
+    bool exited() const {
+      return acquired_future_count_.load(std::memory_order_relaxed) & scope_exited;
+    }
+
+    void exit(bool scope_was_destroyed) {
+      future_holder_base* release_head = nullptr;
+      bool delete_self = false;
+      {
+        std::lock_guard<std::mutex> lock(mutex_);
+        assert(!scope_was_destroyed_);
+        scope_was_destroyed_ = scope_was_destroyed;
+
+        // Atomically exit the scope. We cannot do this safely if there are any running futures
+        // since they might still be accessing state which is guarded by the scope. Worse, if a
+        // future re-entrantly destroys the scope during its execution then as a side-effect the
+        // future itself will be destroyed. So assert!
+        uint64_t prior_count =
+            acquired_future_count_.exchange(scope_exited, std::memory_order_relaxed);
+        if (!(prior_count & scope_exited)) {
+          // Cannot exit fasync::scope while any of its futures are running!
+          assert(prior_count == 0);
+
+          // Take the futures so they can be deleted outside of the lock.
+          release_head = head_future_holder_;
+          head_future_holder_ = nullptr;
+        }
+
+        // If there are no more handles then we can delete the state now.
+        delete_self = should_delete_self();
+      }
+
+      // Delete aborted futures and self outside of the lock.
+      while (release_head) {
+        future_holder_base* release_next = release_head->next;
+        delete release_head;
+        release_head = release_next;
+      }
+      if (delete_self) {
+        delete this;
+      }
+    }
+
+    // The following methods are called from the |scoped_future|.
+
+    // Links a future to the scope's lifecycle such that it will be destroyed when the scope is
+    // exited. Returns a handle that may be used to access the future later.
+    // The state takes ownership of the future.
+    future_handle adopt_future(future_holder_base* future_holder) {
+      {
+        std::lock_guard<std::mutex> lock(mutex_);
+        assert(!scope_was_destroyed_);  // Otherwise how did we get here?
+
+        // If the scope hasn't been exited yet, link the future and mint a new handle. Otherwise we
+        // will abort the future.
+        if (!exited()) {
+          if (head_future_holder_) {
+            head_future_holder_->prev = future_holder;
+            future_holder->next = head_future_holder_;
+          }
+          head_future_holder_ = future_holder;
+          future_handle_count_++;
+          return future_handle(this, future_holder);
+        }
+      }
+
+      // Delete aborted future outside of the lock.
+      delete future_holder;
+      return future_handle{};
+    }
+
+    // Unlinks a future from the scope's lifecycle given its handle and causes the underlying future
+    // to be destroyed if it hasn't already been destroyed due to the scope exiting.
+    // Does nothing if the handle was default-initialized.
+    static void drop_future(future_handle future_handle) {
+      if (!future_handle.future_holder_) {
+        return;  // Invalid handle, nothing to do.
+      }
+
+      {
+        std::lock_guard<std::mutex> lock(future_handle.state_->mutex_);
+
+        // If the scope hasn't been exited yet, unlink the future and prepare to destroy it.
+        // Otherwise, it's already been unlinked and destroyed so release the handle but don't touch
+        // the pointer!
+        assert(future_handle.state_->future_handle_count_ > 0);
+        future_handle.state_->future_handle_count_--;
+        if (!future_handle.state_->exited()) {
+          if (future_handle.future_holder_->next) {
+            future_handle.future_holder_->next->prev = future_handle.future_holder_->prev;
+          }
+          if (future_handle.future_holder_->prev) {
+            future_handle.future_holder_->prev->next = future_handle.future_holder_->next;
+          } else {
+            future_handle.state_->head_future_holder_ = future_handle.future_holder_->next;
+          }
+          // Fallthrough to delete the future.
+        } else if (!future_handle.state_->should_delete_self()) {
+          return;
+        } else {
+          // Fallthrough to delete self.
+          future_handle.future_holder_ = nullptr;
+        }
+      }
+
+      // Delete the future or scope outside of the lock.
+      if (future_handle.future_holder_) {
+        delete future_handle.future_holder_;
+      } else {
+        delete future_handle.state_;
+      }
+    }
+
+    // Acquires a future given its handle.
+    // Returns nullptr if the handle was default-initialized or if the scope exited, meaning that
+    // the future was not acquired. The future must be released before it can be acquired again.
+    static future_holder_base* try_acquire_future(future_handle future_handle) {
+      if (future_handle.future_holder_) {
+        uint64_t prior_count =
+            future_handle.state_->acquired_future_count_.fetch_add(1u, std::memory_order_relaxed);
+        if (!(prior_count & scope_exited)) {
+          return future_handle.future_holder_;
+        }
+        future_handle.state_->acquired_future_count_.fetch_sub(1u, std::memory_order_relaxed);
+      }
+      return nullptr;
+    }
+
+    // Releases a future that was successfully acquired.
+    static void release_future(future_handle future_handle) {
+      future_handle.state_->acquired_future_count_.fetch_sub(1u, std::memory_order_relaxed);
+    }
+
+   private:
+    constexpr bool should_delete_self() const FIT_REQUIRES(mutex_) {
+      return scope_was_destroyed_ && future_handle_count_ == 0;
+    }
+
+    static constexpr uint64_t scope_exited = static_cast<uint64_t>(1u) << 63;
+
+    // Tracks of the number of futures currently running ("acquired").
+    // The top bit is set when the scope is exited, at which point no new futures can be acquired.
+    // After exiting, the count can be incremented transiently but is immediately decremented again
+    // until all future handles have been released. Once no future handles remain, the count will
+    // equal |scope_exited| and will not change again.
+    std::atomic_uint64_t acquired_future_count_{0};
+
+    mutable std::mutex mutex_;
+    bool scope_was_destroyed_ FIT_GUARDED(mutex_) = false;
+    uint64_t future_handle_count_ FIT_GUARDED(mutex_) = 0;
+    future_holder_base* head_future_holder_ FIT_GUARDED(mutex_) = nullptr;
+  };
+
+  // Base type for managing the lifetime of a future of any type.
+  // It is owned by the state and retained indirectly by the continuation using a |future_handle|.
+  class future_holder_base {
+   public:
+    constexpr future_holder_base() = default;
+    virtual ~future_holder_base() = default;
+
+    constexpr future_holder_base(const future_holder_base&) = delete;
+    constexpr future_holder_base& operator=(const future_holder_base&) = delete;
+    constexpr future_holder_base(future_holder_base&&) = delete;
+    constexpr future_holder_base& operator=(future_holder_base&&) = delete;
+
+   private:
+    // |next| and |prev| belong to the state object.
+    friend class state;
+    future_holder_base* next = nullptr;
+    future_holder_base* prev = nullptr;
+  };
+
+  // Holder for a future of a particular type.
+  template <typename F>
+  class future_holder final : public future_holder_base {
+   public:
+    explicit constexpr future_holder(F&& future) : future(std::move(future)) {}
+    ~future_holder() override = default;
+
+    F future;
+  };
+
+  // Wraps a future whose lifetime is managed by the scope.
+  template <typename F>
+  class scoped_future final {
+   public:
+    explicit constexpr scoped_future(future_handle future_handle) : future_handle_(future_handle) {}
+
+    ~scoped_future() { state::drop_future(future_handle_); }
+
+    constexpr scoped_future(const scoped_future&) = delete;
+    constexpr scoped_future& operator=(const scoped_future&) = delete;
+
+    constexpr scoped_future(scoped_future&& other) : future_handle_(other.future_handle_) {
+      other.future_handle_ = future_handle{};
+    }
+
+    scoped_future& operator=(scoped_future&& other) {
+      if (this != &other) {
+        state::drop_future(future_handle_);
+        future_handle_ = other.future_handle_;
+        other.future_handle_ = future_handle{};
+      }
+      return *this;
+    }
+
+    future_poll_t<F> operator()(context& context) {
+      future_poll_t<F> poll = fasync::pending();
+      auto holder = static_cast<future_holder<F>*>(state::try_acquire_future(future_handle_));
+      if (holder) {
+        poll = holder->future(context);
+        state::release_future(future_handle_);
+      }
+      return poll;
+    }
+
+   private:
+    future_handle future_handle_;
+  };
+
+  // The scope's shared state.
+  state* const state_;
+};
+
+}  // namespace fasync
+
+LIB_FASYNC_CPP_VERSION_COMPAT_END
+
+#endif  // SRC_LIB_FASYNC_INCLUDE_LIB_FASYNC_SCOPE_H_
---
+++ src/lib/fasync/tests/scope_tests.cc
@@ -0,0 +1,211 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <lib/fasync/bridge.h>
+#include <lib/fasync/future.h>
+#include <lib/fasync/scope.h>
+#include <lib/fasync/single_threaded_executor.h>
+#include <lib/fit/defer.h>
+#include <unistd.h>
+
+#include <thread>
+
+#include <zxtest/zxtest.h>
+
+#include "test_utils.h"
+
+namespace {
+
+// Asynchronously accumulates a sum.
+// This is an example of an object that offers futures that captures the "this" pointer, thereby
+// needing a scope to prevent dangling pointers in case it is destroyed before the futures complete.
+class accumulator {
+ public:
+  // Adds a value to the counter then returns it.
+  // Takes time proportional to the value being added.
+  fasync::future<uint32_t> add(uint32_t value) {
+    return fasync::make_future(
+               [this, cycles = value](fasync::context& context) mutable -> fasync::poll<uint32_t> {
+                 if (cycles == 0) {
+                   return fasync::ready(counter_);
+                 }
+                 counter_++;
+                 cycles--;
+                 context.suspend_task().resume();
+                 return fasync::pending();
+               }) |
+           fasync::wrap_with(scope_);
+  }
+
+  // Gets the current count, immediately.
+  uint32_t count() const { return counter_; }
+
+ private:
+  fasync::scope scope_;
+  uint32_t counter_ = 0;
+};
+
+TEST(ScopeTests, scoping_tasks) {
+  auto acc = std::make_unique<accumulator>();
+  fasync::single_threaded_executor executor;
+  uint32_t sums[4] = {};
+
+  // Schedule some tasks which accumulate values asynchronously.
+  executor.schedule(acc->add(2) | fasync::then([&](const uint32_t& value) { sums[0] = value; }));
+  executor.schedule(acc->add(1) | fasync::then([&](const uint32_t& value) { sums[1] = value; }));
+  executor.schedule(acc->add(5) | fasync::then([&](const uint32_t& value) { sums[2] = value; }));
+
+  // Schedule a task which accumulates and then destroys the accumulator so that the scope is
+  // exited. Any remaining futures will be aborted.
+  uint32_t last_count = 0;
+  executor.schedule(acc->add(3) | fasync::then([&](const uint32_t& value) {
+                      sums[3] = value;
+                      // Schedule destruction in another task to avoid re-entrance.
+                      executor.schedule(fasync::make_future([&] {
+                        last_count = acc->count();
+                        acc.reset();
+                      }));
+                    }));
+
+  // Run the tasks.
+  executor.run();
+
+  // The counts reflect the fact that the scope is exited part-way through the cycle. For example,
+  // the sums[2] task doesn't get to run since it only runs after 5 cycles and the scope is exited
+  // on the third.
+  EXPECT_EQ(11, last_count);
+  EXPECT_EQ(7, sums[0]);
+  EXPECT_EQ(5, sums[1]);
+  EXPECT_EQ(0, sums[2]);
+  EXPECT_EQ(10, sums[3]);
+}
+
+TEST(ScopeTests, exit_destroys_wrapped_futures) {
+  fasync::scope scope;
+  EXPECT_FALSE(scope.exited());
+
+  // Set up three wrapped futures.
+  bool destroyed[4] = {};
+  auto p0 =
+      scope.wrap(fasync::make_future([d = fit::defer([&] {
+                                        destroyed[0] = true;
+                                      })]() -> fitx::result<fitx::failed> { return fitx::ok(); }));
+  auto p1 =
+      scope.wrap(fasync::make_future([d = fit::defer([&] {
+                                        destroyed[1] = true;
+                                      })]() -> fitx::result<fitx::failed> { return fitx::ok(); }));
+  auto p2 =
+      scope.wrap(fasync::make_future([d = fit::defer([&] {
+                                        destroyed[2] = true;
+                                      })]() -> fitx::result<fitx::failed> { return fitx::ok(); }));
+  EXPECT_FALSE(destroyed[0]);
+  EXPECT_FALSE(destroyed[1]);
+  EXPECT_FALSE(destroyed[2]);
+
+  // Execute one of them to completion, causing it to be destroyed.
+  EXPECT_TRUE(fasync::block(std::move(p1)).value().is_ok());
+  EXPECT_FALSE(destroyed[0]);
+  EXPECT_TRUE(destroyed[1]);
+  EXPECT_FALSE(destroyed[2]);
+
+  // Exit the scope, causing the wrapped future to be destroyed while still leaving the wrapper
+  // alive (but aborted).
+  scope.exit();
+  EXPECT_TRUE(scope.exited());
+  EXPECT_TRUE(destroyed[0]);
+  EXPECT_TRUE(destroyed[1]);
+  EXPECT_TRUE(destroyed[2]);
+
+  // Wrapping another future causes the wrapped future to be immediately destroyed.
+  auto p3 =
+      scope.wrap(fasync::make_future([d = fit::defer([&] {
+                                        destroyed[3] = true;
+                                      })]() -> fitx::result<fitx::failed> { return fitx::ok(); }));
+  EXPECT_TRUE(destroyed[3]);
+
+  // Executing the wrapped futures returns pending.
+  EXPECT_TRUE(fasync::testing::poll(std::move(p0)).is_pending());
+  EXPECT_TRUE(fasync::testing::poll(std::move(p2)).is_pending());
+  EXPECT_TRUE(fasync::testing::poll(std::move(p3)).is_pending());
+
+  // Exiting again has no effect.
+  scope.exit();
+  EXPECT_TRUE(scope.exited());
+}
+
+TEST(ScopeTests, double_wrap) {
+  fasync::scope scope;
+
+  // Here we wrap a task that's already been wrapped to see what happens when the scope is exited.
+  // This is interesting because it means that the destruction of one wrapped future will cause the
+  // destruction of another wrapped future and could uncover re-entrance issues.
+  uint32_t run_count = 0;
+  bool destroyed = false;
+  auto future =
+      fasync::make_future([&, d = fit::defer([&] { destroyed = true; })](fasync::context& context) {
+        run_count++;
+        return fasync::pending();
+      }) |
+      fasync::wrap_with(scope) | fasync::wrap_with(scope);  // wrap again!
+
+  // Run the future once to show that we can.
+  EXPECT_TRUE(fasync::testing::poll(future).is_pending());
+  EXPECT_EQ(1, run_count);
+  EXPECT_FALSE(destroyed);
+
+  // Now exit the scope, which should cause the future to be destroyed.
+  scope.exit();
+  EXPECT_EQ(1, run_count);
+  EXPECT_TRUE(destroyed);
+
+  // Running the future again should do nothing.
+  EXPECT_TRUE(fasync::testing::poll(std::move(future)).is_pending());
+  EXPECT_EQ(1, run_count);
+  EXPECT_TRUE(destroyed);
+}
+
+TEST(ScopeTests, thread_safety) {
+  fasync::scope scope;
+  fasync::single_threaded_executor executor;
+  uint64_t run_count = 0;
+
+  // Schedule work from a few threads, just to show that we can.
+  // Part way through, exit the scope.
+  constexpr int num_threads = 4;
+  constexpr int num_tasks_per_thread = 100;
+  constexpr int exit_threshold = 75;
+  std::thread threads[num_threads];
+  for (int i = 0; i < num_threads; i++) {
+    fasync::bridge<fitx::failed> bridge;
+    threads[i] = std::thread([&, completer = std::move(bridge.completer)]() mutable {
+      for (int j = 0; j < num_tasks_per_thread; j++) {
+        if (j == exit_threshold) {
+          executor.schedule(fasync::make_future([&] { scope.exit(); }));
+        }
+
+        executor.schedule(fasync::make_future([&] { run_count++; }) | fasync::wrap_with(scope));
+      }
+      completer.complete_ok();
+    });
+    executor.schedule(bridge.consumer.future());
+  }
+
+  // Run the tasks.
+  executor.run();
+  for (int i = 0; i < num_threads; i++) {
+    threads[i].join();
+  }
+
+  // We expect some non-deterministic number of tasks to have run related to the exit threshold.
+  // We scheduled num_threads * num_tasks_per_thread tasks, but on each thread we exited the
+  // (common) scope after scheduling its first exit_threshold tasks. Once one of those threads
+  // exits the scope, no more tasks (scheduled by any thread) will run within the scope, so the
+  // number of executed tasks cannot increase any further. Therefore we know that at least
+  // |exit_threshold| tasks have run but we could have run as many as |num_threads * exit_threshold|
+  // in a perfect world where all of the threads called |scope.exit()| at the same time.
+  EXPECT_GE(run_count, exit_threshold);
+  EXPECT_LE(run_count, num_threads * exit_threshold);
+}
+
+}  // namespace
