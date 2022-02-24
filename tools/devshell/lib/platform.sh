8f9f4c1f679c29c48e760bdf4ef0defd39300b13
Roland McGrath <mcgrathr@google.com>
2022-02-24 07:21:07 +0000

    [fx dis] Use objdump from binutils-gdb prebuilt
    The binutils-gdb prebuilt has an objdump that handles all formats.
    Current GNU versions are no longer confused by .relr.dyn sections
    and so objdump works again without specifying the architecture.
    
    Change-Id: I8272355d4b377b3c4f440ee973c5f5063162d0af
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/649795
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Petr Hosek <phosek@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- tools/devshell/lib/platform.sh
+++ tools/devshell/lib/platform.sh
@@ -44,6 +44,7 @@ readonly PREBUILT_3P_DIR="${FUCHSIA_DIR}/prebuilt/third_party"
 readonly PREBUILT_TOOLS_DIR="${FUCHSIA_DIR}/prebuilt/tools"
 
 readonly PREBUILT_AEMU_DIR="${PREBUILT_3P_DIR}/android/aemu/release/${HOST_PLATFORM}"
+readonly PREBUILT_BINUTILS_DIR="${PREBUILT_3P_DIR}/binutils-gdb/${HOST_PLATFORM}"
 readonly PREBUILT_CLANG_DIR="${PREBUILT_3P_DIR}/clang/${HOST_PLATFORM}"
 readonly PREBUILT_CMAKE_DIR="${PREBUILT_3P_DIR}/cmake/${HOST_PLATFORM}"
 readonly PREBUILT_DART_DIR="${PREBUILT_3P_DIR}/dart/${HOST_PLATFORM}"
