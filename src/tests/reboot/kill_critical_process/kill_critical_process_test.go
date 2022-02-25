6b3547c78b3f00db36793ddbbef77d6b95d72351
David Hastings <dahastin@google.com>
2022-02-25 05:20:21 +0000

    [reboot] Change kill critical process test target
    Both bootsvc and component manger are marked as critical processes.
    Since bootsvc is going away soon, it will ease the transition to just
    change this test to target the other remaining critical process.
    
    See fxr/650682 for an example where we boot into component manager
    directly.
    
    Bug: 44784
    Change-Id: I2568f2d6debe147c17d3223bedf45c2b8a31ee9d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/650685
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: David Hastings <dahastin@google.com>

--- src/tests/reboot/kill_critical_process/kill_critical_process_test.go
+++ src/tests/reboot/kill_critical_process/kill_critical_process_test.go
@@ -10,10 +10,10 @@ import (
 	"go.fuchsia.dev/fuchsia/src/tests/reboot/reboottest"
 )
 
-// Test that "killall bootsvc" will reboot the system (because bootsvc is
+// Test that "killall component_manager" will reboot the system (because component_manager is
 // marked as a critical process; see also |zx_job_set_critical|).
 func TestKillCriticalProcess(t *testing.T) {
 	// Killing a critical process will result in an "unclean reboot" because,
 	// among other things, the filesystem won't be shutdown cleanly.
-	reboottest.RebootWithCommand(t, "killall bootsvc", reboottest.UncleanReboot, "fuchsia.zbi")
+	reboottest.RebootWithCommand(t, "killall component_manager", reboottest.UncleanReboot, "fuchsia.zbi")
 }
