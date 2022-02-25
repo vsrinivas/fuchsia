19d280ef8c3ee8c726b81a3ab4a069a8ae12be53
David Hastings <dahastin@google.com>
2022-02-25 08:06:01 +0000

    [component_manager] Free loader service when starting
    Userboot exists until its children close the loader service. When
    loading component manager directly (and not through bootsvc), component
    manager must free the loader service.
    
    Bug: 44784
    
    Tested:
    Local QEMU
    
    Change-Id: I2c705f5ccb3854a730fb8968ecdd375d5a1eadf5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/650865
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: David Hastings <dahastin@google.com>

--- src/sys/component_manager/src/main.rs
+++ src/sys/component_manager/src/main.rs
@@ -23,6 +23,12 @@ use {
     std::{panic, process, thread, time::Duration},
 };
 
+extern "C" {
+    fn dl_set_loader_service(
+        handle: fuchsia_zircon::sys::zx_handle_t,
+    ) -> fuchsia_zircon::sys::zx_handle_t;
+}
+
 fn main() {
     // Make sure we exit if there is a panic. Add this hook before we init the
     // KernelLogger because it installs its own hook and then calls any
@@ -53,6 +59,17 @@ fn main() {
     {
         panic!("Component manager failed to set itself as critical: {}", err);
     }
+
+    // Close any loader service passed to component manager so that the service session can be
+    // freed, as component manager won't make use of a loader service such as by calling dlopen.
+    // If userboot invoked component manager directly, this service was the only reason userboot
+    // continued to run and closing it will let userboot terminate.
+    let ldsvc = unsafe {
+        fuchsia_zircon::Handle::from_raw(dl_set_loader_service(
+            fuchsia_zircon::sys::ZX_HANDLE_INVALID,
+        ))
+    };
+    drop(ldsvc);
 
     // Enable tracing in Component Manager
     trace_provider::trace_provider_create_with_fdio();
