7cdcee3180c15db13ca3657dda1743b3407efeb9
Erick Tryzelaar <etryzelaar@google.com>
2022-03-02 20:40:57 +0000

    [rust-3p] Update pathdiff to 0.2.1
    This also enables pathdiff to work with camino, and to be usable on
    target. We need this for the fuchsia-pkg crate, which works on both host
    and target.
    
    Change-Id: I3ebc2adabc011b0be58afa51d541e14ab1b8817b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/652466
    Reviewed-by: Adam Perry <adamperry@google.com>
    Commit-Queue: Erick Tryzelaar <etryzelaar@google.com>
    Fuchsia-Auto-Submit: Erick Tryzelaar <etryzelaar@google.com>

--- third_party/rust_crates/vendor/pathdiff/src/lib.rs
+++ third_party/rust_crates/vendor/pathdiff/src/lib.rs
@@ -75,3 +75,148 @@ where
         Some(comps.iter().map(|c| c.as_os_str()).collect())
     }
 }
+
+#[cfg(feature = "camino")]
+mod utf8_paths {
+    use camino::{Utf8Component, Utf8Path, Utf8PathBuf};
+
+    /// Construct a relative UTF-8 path from a provided base directory path to the provided path.
+    ///
+    /// ```rust
+    /// # extern crate camino;
+    /// use camino::*;
+    /// use pathdiff::diff_utf8_paths;
+    ///
+    /// let baz = "/foo/bar/baz";
+    /// let bar = "/foo/bar";
+    /// let quux = "/foo/bar/quux";
+    /// assert_eq!(diff_utf8_paths(bar, baz), Some("../".into()));
+    /// assert_eq!(diff_utf8_paths(baz, bar), Some("baz".into()));
+    /// assert_eq!(diff_utf8_paths(quux, baz), Some("../quux".into()));
+    /// assert_eq!(diff_utf8_paths(baz, quux), Some("../baz".into()));
+    /// assert_eq!(diff_utf8_paths(bar, quux), Some("../".into()));
+    ///
+    /// assert_eq!(diff_utf8_paths(&baz, &bar.to_string()), Some("baz".into()));
+    /// assert_eq!(diff_utf8_paths(Utf8Path::new(baz), Utf8Path::new(bar).to_path_buf()), Some("baz".into()));
+    /// ```
+    #[cfg_attr(docsrs, doc(cfg(feature = "camino")))]
+    pub fn diff_utf8_paths<P, B>(path: P, base: B) -> Option<Utf8PathBuf>
+    where
+        P: AsRef<Utf8Path>,
+        B: AsRef<Utf8Path>,
+    {
+        let path = path.as_ref();
+        let base = base.as_ref();
+
+        if path.is_absolute() != base.is_absolute() {
+            if path.is_absolute() {
+                Some(Utf8PathBuf::from(path))
+            } else {
+                None
+            }
+        } else {
+            let mut ita = path.components();
+            let mut itb = base.components();
+            let mut comps: Vec<Utf8Component> = vec![];
+            loop {
+                match (ita.next(), itb.next()) {
+                    (None, None) => break,
+                    (Some(a), None) => {
+                        comps.push(a);
+                        comps.extend(ita.by_ref());
+                        break;
+                    }
+                    (None, _) => comps.push(Utf8Component::ParentDir),
+                    (Some(a), Some(b)) if comps.is_empty() && a == b => (),
+                    (Some(a), Some(b)) if b == Utf8Component::CurDir => comps.push(a),
+                    (Some(_), Some(b)) if b == Utf8Component::ParentDir => return None,
+                    (Some(a), Some(_)) => {
+                        comps.push(Utf8Component::ParentDir);
+                        for _ in itb {
+                            comps.push(Utf8Component::ParentDir);
+                        }
+                        comps.push(a);
+                        comps.extend(ita.by_ref());
+                        break;
+                    }
+                }
+            }
+            Some(comps.iter().map(|c| c.as_str()).collect())
+        }
+    }
+}
+
+#[cfg(feature = "camino")]
+pub use crate::utf8_paths::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_absolute() {
+        assert_diff_paths("/foo", "/bar", Some("../foo"));
+        assert_diff_paths("/foo", "bar", Some("/foo"));
+        assert_diff_paths("foo", "/bar", None);
+        assert_diff_paths("foo", "bar", Some("../foo"));
+    }
+
+    #[test]
+    fn test_identity() {
+        assert_diff_paths(".", ".", Some(""));
+        assert_diff_paths("../foo", "../foo", Some(""));
+        assert_diff_paths("./foo", "./foo", Some(""));
+        assert_diff_paths("/foo", "/foo", Some(""));
+        assert_diff_paths("foo", "foo", Some(""));
+
+        assert_diff_paths("../foo/bar/baz", "../foo/bar/baz", Some("".into()));
+        assert_diff_paths("foo/bar/baz", "foo/bar/baz", Some(""));
+    }
+
+    #[test]
+    fn test_subset() {
+        assert_diff_paths("foo", "fo", Some("../foo"));
+        assert_diff_paths("fo", "foo", Some("../fo"));
+    }
+
+    #[test]
+    fn test_empty() {
+        assert_diff_paths("", "", Some(""));
+        assert_diff_paths("foo", "", Some("foo"));
+        assert_diff_paths("", "foo", Some(".."));
+    }
+
+    #[test]
+    fn test_relative() {
+        assert_diff_paths("../foo", "../bar", Some("../foo"));
+        assert_diff_paths("../foo", "../foo/bar/baz", Some("../.."));
+        assert_diff_paths("../foo/bar/baz", "../foo", Some("bar/baz"));
+
+        assert_diff_paths("foo/bar/baz", "foo", Some("bar/baz"));
+        assert_diff_paths("foo/bar/baz", "foo/bar", Some("baz"));
+        assert_diff_paths("foo/bar/baz", "foo/bar/baz", Some(""));
+        assert_diff_paths("foo/bar/baz", "foo/bar/baz/", Some(""));
+
+        assert_diff_paths("foo/bar/baz/", "foo", Some("bar/baz"));
+        assert_diff_paths("foo/bar/baz/", "foo/bar", Some("baz"));
+        assert_diff_paths("foo/bar/baz/", "foo/bar/baz", Some(""));
+        assert_diff_paths("foo/bar/baz/", "foo/bar/baz/", Some(""));
+
+        assert_diff_paths("foo/bar/baz", "foo/", Some("bar/baz"));
+        assert_diff_paths("foo/bar/baz", "foo/bar/", Some("baz"));
+        assert_diff_paths("foo/bar/baz", "foo/bar/baz", Some(""));
+    }
+
+    #[test]
+    fn test_current_directory() {
+        assert_diff_paths(".", "foo", Some("../."));
+        assert_diff_paths("foo", ".", Some("foo"));
+        assert_diff_paths("/foo", "/.", Some("foo"));
+    }
+
+    fn assert_diff_paths(path: &str, base: &str, expected: Option<&str>) {
+        assert_eq!(diff_paths(path, base), expected.map(|s| s.into()));
+        #[cfg(feature = "camino")]
+        assert_eq!(diff_utf8_paths(path, base), expected.map(|s| s.into()));
+    }
+}
