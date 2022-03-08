002865891795955a44307e7ea56b65be3e5ece21
Benjamin Prosnitz <bprosnitz@google.com>
2022-03-08 01:03:52 +0000

    [fidl_driver] Test for fdf::WireClient
    Previous tests involved fdf::WireSyncClient or fdf::WireSharedClient
    only. This adds a test for fdf::WireClient with identical behavior to
    the existing two way test.
    
    Multiply: fidl_driver_transport_tests (fuchsia): 1000
    
    Change-Id: Ib81b64d1457c6be7a78c7f31be3bd2466b9f5a0a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/650362
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Benjamin Prosnitz <bprosnitz@google.com>

--- sdk/lib/fidl_driver/tests/transport/async_two_way_test.cc
+++ sdk/lib/fidl_driver/tests/transport/async_two_way_test.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <fidl/test.transport/cpp/driver/wire.h>
+#include <lib/async/cpp/task.h>
 #include <lib/fdf/cpp/dispatcher.h>
 #include <lib/fdf/internal.h>
 #include <lib/fit/defer.h>
@@ -37,6 +38,49 @@ struct TestServer : public fdf::WireServer<test_transport::TwoWayTest> {
 };
 
 TEST(DriverTransport, TwoWayAsync) {
+  fidl_driver_testing::ScopedFakeDriver driver;
+
+  auto dispatcher = fdf::Dispatcher::Create(0);
+  ASSERT_OK(dispatcher.status_value());
+
+  auto channels = fdf::ChannelPair::Create(0);
+  ASSERT_OK(channels.status_value());
+
+  fdf::ServerEnd<test_transport::TwoWayTest> server_end(std::move(channels->end0));
+  fdf::ClientEnd<test_transport::TwoWayTest> client_end(std::move(channels->end1));
+
+  auto server = std::make_shared<TestServer>();
+  fdf::BindServer(dispatcher->get(), std::move(server_end), server,
+                  fidl_driver_testing::FailTestOnServerError<::test_transport::TwoWayTest>());
+  auto arena = fdf::Arena::Create(0, "");
+  ASSERT_OK(arena.status_value());
+  server->fdf_request_arena = arena->get();
+
+  fdf::WireClient<test_transport::TwoWayTest> client;
+  sync_completion_t called;
+  auto bind_and_run_on_dispatcher_thread = [&] {
+    client.Bind(std::move(client_end), dispatcher->get());
+
+    client.buffer(*arena)->TwoWay(
+        kRequestPayload, [&](fdf::WireUnownedResult<::test_transport::TwoWayTest::TwoWay>& result) {
+          ASSERT_OK(result.status());
+          ASSERT_EQ(kResponsePayload, result->payload);
+          ASSERT_EQ(server->fdf_response_arena, result.arena().get());
+          sync_completion_signal(&called);
+        });
+  };
+  async::PostTask(dispatcher->async_dispatcher(), bind_and_run_on_dispatcher_thread);
+  ASSERT_OK(sync_completion_wait(&called, ZX_TIME_INFINITE));
+
+  sync_completion_t destroyed;
+  auto destroy_on_dispatcher_thread = [client = std::move(client), &destroyed] {
+    sync_completion_signal(&destroyed);
+  };
+  async::PostTask(dispatcher->async_dispatcher(), std::move(destroy_on_dispatcher_thread));
+  ASSERT_OK(sync_completion_wait(&destroyed, ZX_TIME_INFINITE));
+}
+
+TEST(DriverTransport, TwoWayAsyncShared) {
   fidl_driver_testing::ScopedFakeDriver driver;
 
   auto dispatcher = fdf::Dispatcher::Create(FDF_DISPATCHER_OPTION_UNSYNCHRONIZED);
