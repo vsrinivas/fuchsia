fd95467a3b4601b66e1f9ca667590aa9597943a2
Tom Bergan <tombergan@google.com>
2022-03-16 23:41:43 +0000

    [mixer_service] Add a sketch of mixer objects
    This CL adds a sketch of two kinds of mixer objects, Thread and
    PipelineStage. There are two implementations of Thread, one for the
    "detached" thread and one for the mixer threads. See here for more
    discussion:
    //src/media/audio/mixer_service/docs/execution_model.md
    
    This is just a sketch with not much functionality. Future CLs will flesh
    out this directory.
    
    Bug: 87651
    Change-Id: Ifeb9ebda8d118cfd2e2d05b995521bde610d026b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/649810
    Reviewed-by: Alper Gungormusler <alperg@google.com>
    Commit-Queue: Tom Bergan <tombergan@google.com>

---
+++ src/media/audio/mixer_service/mix/pipeline_stage.h
@@ -0,0 +1,80 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_MEDIA_AUDIO_MIXER_SERVICE_MIX_PIPELINE_STAGE_H_
+#define SRC_MEDIA_AUDIO_MIXER_SERVICE_MIX_PIPELINE_STAGE_H_
+
+#include <fidl/fuchsia.audio.mixer/cpp/wire.h>
+#include <lib/fpromise/result.h>
+
+#include <atomic>
+#include <memory>
+#include <optional>
+#include <string>
+
+#include "src/media/audio/mixer_service/common/global_work_queue.h"
+#include "src/media/audio/mixer_service/mix/thread.h"
+
+namespace media_audio_mixer_service {
+
+class PipelineStage;
+using PipelineStagePtr = std::shared_ptr<PipelineStage>;
+
+// A stage in a pipeline tree.
+//
+// Each PipelineStage consumes zero or more source streams and produces at most one destination
+// stream. This abstract class provides functionality common to all pipeline stages.
+class PipelineStage {
+ public:
+  virtual ~PipelineStage() = default;
+
+  // Returns the PipelineStage's name. This is used for diagnostics only.
+  // The name may not be a unique identifier.
+  const std::string& name() const { return name_; }
+
+  // Returns the thread which currently controls this PipelineStage.
+  // It is safe to call this method on any thread, but if not called from thread(),
+  // the returned value may change concurrently.
+  ThreadPtr thread() const { return std::atomic_load(&thread_); }
+
+  // Adds a source stream.
+  // REQUIRED: caller must verify that src produces a stream with a compatible format
+  virtual void AddSource(PipelineStagePtr src) TA_REQ(thread()->checker()) = 0;
+
+  // Removes a source stream.
+  // REQUIRED: caller must verify that src is currently a source for this PipelineStage.
+  virtual void RemoveSource(PipelineStagePtr src) TA_REQ(thread()->checker()) = 0;
+
+  // TODO(fxbug.dev/87651): bring in stuff from the old ReadableStream:
+  // - format
+  // - timeline transform
+  // - clocks
+  // - delay aka lead time
+  // - reading and trimming the destination stream, perhaps as:
+  //
+  //   class Buffer {};
+  //   std::optional<Buffer> ReadDestStream(MixJobContext& ctx, Fixed frame_start,
+  //                                        int64_t frame_count);
+  //   void TrimDestStream(Fixed frame);
+
+ protected:
+  explicit PipelineStage(std::string_view name) : name_(name) {}
+
+  PipelineStage(const PipelineStage&) = delete;
+  PipelineStage& operator=(const PipelineStage&) = delete;
+
+  PipelineStage(PipelineStage&&) = delete;
+  PipelineStage& operator=(PipelineStage&&) = delete;
+
+ private:
+  const std::string name_;
+
+  // This is accessed with atomic instructions (std::atomic_load and std::atomic_store) so that any
+  // thread can call thread()->checker(). This can't be a std::atomic<ThreadPtr> until C++20.
+  ThreadPtr thread_;
+};
+
+}  // namespace media_audio_mixer_service
+
+#endif  // SRC_MEDIA_AUDIO_MIXER_SERVICE_MIX_PIPELINE_STAGE_H_
