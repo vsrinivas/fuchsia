55d27a0be1e22c758255af51ecf02a4847f389a2
Jerry Belton <jcecil@google.com>
2022-03-16 21:07:55 +0000

    [check-licenses] Updates to license package
    Deletes the old copy of the notice processing functions.
    
    Gets rid of several fields that are only used for the final step
    (aggregating a NOTICE file). We can do that work in the result package,
    and prevent these data classes from becoming too complicated.
    
    Change-Id: Iaf4460d71c789c85ed1f5b097ed2811f2a527f99
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/659035
    Reviewed-by: Sean Cuff <seancuff@google.com>
    Commit-Queue: Jerry Belton <jcecil@google.com>

--- tools/check-licenses/license/search.go
+++ tools/check-licenses/license/search.go
@@ -5,129 +5,88 @@
 package license
 
 import (
+	"context"
 	"fmt"
-	"sync"
+
+	"golang.org/x/sync/errgroup"
 
 	"go.fuchsia.dev/fuchsia/tools/check-licenses/file"
-	"go.fuchsia.dev/fuchsia/tools/check-licenses/license/notice"
 )
 
 type SearchResult struct {
-	Patterns []*Pattern
+	LicenseData *file.FileData
+	Patterns    []*Pattern
 }
 
-var AllPatterns []*Pattern
-var AllSearchResults map[string]*SearchResult
+// Search each data slice in the given file for license texts.
+// f (file) is assumed to be a single or multi license file, where all content
+// in the file is license information.
+func Search(f *file.File) ([]*SearchResult, error) {
+	return search(f, AllPatterns)
+}
 
-// Search the given data slice for text that matches this License pattern.
-func Search(f *file.File) (*SearchResult, error) {
+// SearchHeaders searches the beginning portion of the given file for
+// copyright text.
+// f (file) is assumed to be some source artifact, which may contain
+// a small section of licensing or copyright information at the top of the file.
+func SearchHeaders(f *file.File) ([]*SearchResult, error) {
+	return search(f, AllCopyrightPatterns)
+}
 
-	// First, make sure we haven't already seen this license text before.
-	hash, err := f.Hash()
-	if err != nil {
-		return nil, err
-	}
-	if result, ok := AllSearchResults[hash]; ok {
-		// We've seen this before. Return the previous SearchResult.
-		plusVal(fmt.Sprintf("%v [%v]", DuplicateLicenseText, hash), f.Path)
-		return result, nil
-	}
+// search the file using the given list of patterns.
+func search(f *file.File, patterns []*Pattern) ([]*SearchResult, error) {
+	searchResults := make([]*SearchResult, 0)
 
-	// Parse the files into a structured data format.
-	data := make([]*notice.Data, 0)
-
-	// The "LicenseFormat" field of each file is set at the project level
-	// (in README.fuchsia files) and it affects how they are analyzed here.
-	switch f.LicenseFormat {
-
-	// Default: File.LicenseFormat == Any
-	// This is most likely a regular source file in the repository.
-	// May or may not have copyright information.
-	//
-	// TODO(jcecil): Consider using a smaller set of patterns on these files
-	// to help it run faster.
-	case file.Any:
-		text, err := f.ReadAll()
-		if err != nil {
-			return nil, err
-		}
-		data = append(data, &notice.Data{
-			LicenseText: text,
-		})
-
-	// File.LicenseFormat == CopyrightHeader
-	// All source files belonging to "The Fuchsia Authors" (fuchsia.git)
-	// must contain Copyright header information.
-	case file.CopyrightHeader:
-		text, err := f.ReadAll()
-		if err != nil {
-			return nil, err
-		}
-		data = append(data, &notice.Data{
-			LicenseText: text,
-		})
-
-	// File.LicenseFormat == SingleLicense
-	// Regular LICENSE files that contain text for a single license.
-	case file.SingleLicense:
-		text, err := f.ReadAll()
-		if err != nil {
-			return nil, err
-		}
-		data = append(data, &notice.Data{
-			LicenseText: text,
-		})
-
-	// File.LicenseFormat == MultiLicense*
-	// NOTICE files that contain text for multiple licenses.
-	// See the files in the /notice subdirectory for more info.
-	case file.MultiLicenseChromium:
-		data, err = notice.ParseChromium(f)
-		if err != nil {
-			return nil, err
-		}
-	case file.MultiLicenseFlutter:
-		data, err = notice.ParseFlutter(f)
-		if err != nil {
-			return nil, err
-		}
-	case file.MultiLicenseGoogle:
-		data, err = notice.ParseGoogle(f)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	result := SearchResult{
-		Patterns: make([]*Pattern, 0),
-	}
+	eg, _ := errgroup.WithContext(context.Background())
 
 	// Run the license patterns on the parsed license texts.
 	// Save the match results to a result object.
-	var wg sync.WaitGroup
-	for _, d := range data {
-		matches := make([]bool, len(AllPatterns))
-		for i, p := range AllPatterns {
-			wg.Add(1)
-			go func(i int, p *Pattern, text []byte) {
-				defer wg.Done()
-				matches[i] = p.Search([]byte(d.LicenseText))
-			}(i, p, d.LicenseText)
+	for _, d := range f.Data {
+
+		result := &SearchResult{
+			LicenseData: d,
+			Patterns:    make([]*Pattern, 0),
 		}
-		wg.Wait()
+
+		matches := make([]bool, len(patterns))
+		for i, p := range patterns {
+			// https://golang.org/doc/faq#closures_and_goroutines
+			i := i
+			p := p
+			eg.Go(func() error {
+				matches[i] = p.Search(d)
+				return nil
+			})
+
+			// The license patterns have a lot of overlap right now.
+			// A single license segment may match to 5 or more different patterns.
+			// This results in really large output files, since some of the texts
+			// are duplicated (when grouping by patterns).
+			//
+			// TODO: Clean up the license texts to make this not happen.
+			// In the mean time, break after the first match.
+			if err := eg.Wait(); err != nil {
+				return nil, err
+			}
+
+			if matches[i] {
+				break
+			}
+		}
 
 		matchFound := false
 		for i := range matches {
 			if matches[i] {
-				result.Patterns = append(result.Patterns, AllPatterns[i])
+				result.Patterns = append(result.Patterns, patterns[i])
 				matchFound = true
 			}
 		}
+		searchResults = append(searchResults, result)
 		if !matchFound {
 			plusVal(UnrecognizedLicenses, fmt.Sprintf("%v - %v", f.Path, d.LibraryName))
 		}
 	}
 
-	AllSearchResults[hash] = &result
-	return &result, nil
+	return searchResults, nil
+
 }
