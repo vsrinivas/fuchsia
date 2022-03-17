63ca751e2a3735f27335d307db2c71c4a71e8bc3
Abdulla Kamar <abdulla@google.com>
2022-03-17 22:28:37 +0000

    [hypervisor][x86] More formatting of vmx_cpu_state.cc
    Use anonymous namespace instead of static functions.
    
    Bug: 95763
    Change-Id: Ib2efcfb021d51b4320dfaefdd4ace2c8f0cd2afb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/659605
    Reviewed-by: Tim Detwiler <tjdetwiler@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: Abdulla Kamar <abdulla@google.com>
    Fuchsia-Auto-Submit: Abdulla Kamar <abdulla@google.com>

--- zircon/kernel/arch/x86/hypervisor/vmx_cpu_state.cc
+++ zircon/kernel/arch/x86/hypervisor/vmx_cpu_state.cc
@@ -16,12 +16,12 @@
 #include "vmx_cpu_state_priv.h"
 
 namespace {
-DECLARE_SINGLETON_MUTEX(GuestMutex);
-}  // namespace
-static size_t num_guests TA_GUARDED(GuestMutex::Get()) = 0;
-static fbl::Array<VmxPage> vmxon_pages TA_GUARDED(GuestMutex::Get());
 
-static zx_status_t vmxon(paddr_t pa) {
+DECLARE_SINGLETON_MUTEX(GuestMutex);
+size_t num_guests TA_GUARDED(GuestMutex::Get()) = 0;
+fbl::Array<VmxPage> vmxon_pages TA_GUARDED(GuestMutex::Get());
+
+zx_status_t vmxon(paddr_t pa) {
   uint8_t err;
 
   __asm__ __volatile__("vmxon %[pa]"
@@ -32,7 +32,7 @@ static zx_status_t vmxon(paddr_t pa) {
   return err ? ZX_ERR_INTERNAL : ZX_OK;
 }
 
-static zx_status_t vmxoff() {
+zx_status_t vmxoff() {
   uint8_t err;
 
   __asm__ __volatile__("vmxoff"
@@ -43,62 +43,7 @@ static zx_status_t vmxoff() {
   return err ? ZX_ERR_INTERNAL : ZX_OK;
 }
 
-zx_status_t invept(InvEpt invalidation, uint64_t eptp) {
-  uint8_t err;
-  uint64_t descriptor[] = {eptp, 0};
-
-  __asm__ __volatile__("invept %[descriptor], %[invalidation]"
-                       : "=@ccna"(err)  // Set `err` on error (C or Z flag set)
-                       : [descriptor] "m"(descriptor), [invalidation] "r"(invalidation)
-                       : "cc");
-
-  return err ? ZX_ERR_INTERNAL : ZX_OK;
-}
-
-VmxInfo::VmxInfo() {
-  // From Volume 3, Appendix A.1.
-  uint64_t basic_info = read_msr(X86_MSR_IA32_VMX_BASIC);
-  revision_id = static_cast<uint32_t>(BITS(basic_info, 30, 0));
-  region_size = static_cast<uint16_t>(BITS_SHIFT(basic_info, 44, 32));
-  write_back = BITS_SHIFT(basic_info, 53, 50) == VMX_MEMORY_TYPE_WRITE_BACK;
-  io_exit_info = BIT_SHIFT(basic_info, 54);
-  vmx_controls = BIT_SHIFT(basic_info, 55);
-}
-
-EptInfo::EptInfo() {
-  // From Volume 3, Appendix A.10.
-  uint64_t ept_info = read_msr(X86_MSR_IA32_VMX_EPT_VPID_CAP);
-  page_walk_4 = BIT_SHIFT(ept_info, 6);
-  write_back = BIT_SHIFT(ept_info, 14);
-  invept =
-      // INVEPT instruction is supported.
-      BIT_SHIFT(ept_info, 20) &&
-      // Single-context INVEPT type is supported.
-      BIT_SHIFT(ept_info, 25) &&
-      // All-context INVEPT type is supported.
-      BIT_SHIFT(ept_info, 26);
-}
-
-zx_status_t VmxPage::Alloc(const VmxInfo& vmx_info, uint8_t fill) {
-  // From Volume 3, Appendix A.1: Bits 44:32 report the number of bytes that
-  // software should allocate for the VMXON region and any VMCS region. It is
-  // a value greater than 0 and at most 4096 (bit 44 is set if and only if
-  // bits 43:32 are clear).
-  if (vmx_info.region_size > PAGE_SIZE) {
-    return ZX_ERR_NOT_SUPPORTED;
-  }
-
-  // Check use of write-back memory for VMX regions is supported.
-  if (!vmx_info.write_back) {
-    return ZX_ERR_NOT_SUPPORTED;
-  }
-
-  // The maximum size for a VMXON or VMCS region is 4096, therefore
-  // unconditionally allocating a page is adequate.
-  return hypervisor::Page::Alloc(fill);
-}
-
-static zx_status_t vmxon_task(void* context, cpu_num_t cpu_num) {
+zx_status_t vmxon_task(void* context, cpu_num_t cpu_num) {
   auto pages = static_cast<fbl::Array<VmxPage>*>(context);
   VmxPage& page = (*pages)[cpu_num];
 
@@ -180,7 +125,7 @@ static zx_status_t vmxon_task(void* context, cpu_num_t cpu_num) {
   return ZX_OK;
 }
 
-static void vmxoff_task(void* arg) {
+void vmxoff_task(void* arg) {
   // Execute VMXOFF.
   zx_status_t status = vmxoff();
   if (status != ZX_OK) {
@@ -190,6 +135,63 @@ static void vmxoff_task(void* arg) {
 
   // Disable VMX.
   x86_set_cr4(x86_get_cr4() & ~X86_CR4_VMXE);
+}
+
+}  // namespace
+
+zx_status_t invept(InvEpt invalidation, uint64_t eptp) {
+  uint8_t err;
+  uint64_t descriptor[] = {eptp, 0};
+
+  __asm__ __volatile__("invept %[descriptor], %[invalidation]"
+                       : "=@ccna"(err)  // Set `err` on error (C or Z flag set)
+                       : [descriptor] "m"(descriptor), [invalidation] "r"(invalidation)
+                       : "cc");
+
+  return err ? ZX_ERR_INTERNAL : ZX_OK;
+}
+
+VmxInfo::VmxInfo() {
+  // From Volume 3, Appendix A.1.
+  uint64_t basic_info = read_msr(X86_MSR_IA32_VMX_BASIC);
+  revision_id = static_cast<uint32_t>(BITS(basic_info, 30, 0));
+  region_size = static_cast<uint16_t>(BITS_SHIFT(basic_info, 44, 32));
+  write_back = BITS_SHIFT(basic_info, 53, 50) == VMX_MEMORY_TYPE_WRITE_BACK;
+  io_exit_info = BIT_SHIFT(basic_info, 54);
+  vmx_controls = BIT_SHIFT(basic_info, 55);
+}
+
+EptInfo::EptInfo() {
+  // From Volume 3, Appendix A.10.
+  uint64_t ept_info = read_msr(X86_MSR_IA32_VMX_EPT_VPID_CAP);
+  page_walk_4 = BIT_SHIFT(ept_info, 6);
+  write_back = BIT_SHIFT(ept_info, 14);
+  invept =
+      // INVEPT instruction is supported.
+      BIT_SHIFT(ept_info, 20) &&
+      // Single-context INVEPT type is supported.
+      BIT_SHIFT(ept_info, 25) &&
+      // All-context INVEPT type is supported.
+      BIT_SHIFT(ept_info, 26);
+}
+
+zx_status_t VmxPage::Alloc(const VmxInfo& vmx_info, uint8_t fill) {
+  // From Volume 3, Appendix A.1: Bits 44:32 report the number of bytes that
+  // software should allocate for the VMXON region and any VMCS region. It is
+  // a value greater than 0 and at most 4096 (bit 44 is set if and only if
+  // bits 43:32 are clear).
+  if (vmx_info.region_size > PAGE_SIZE) {
+    return ZX_ERR_NOT_SUPPORTED;
+  }
+
+  // Check use of write-back memory for VMX regions is supported.
+  if (!vmx_info.write_back) {
+    return ZX_ERR_NOT_SUPPORTED;
+  }
+
+  // The maximum size for a VMXON or VMCS region is 4096, therefore
+  // unconditionally allocating a page is adequate.
+  return hypervisor::Page::Alloc(fill);
 }
 
 zx_status_t alloc_vmx_state() {
