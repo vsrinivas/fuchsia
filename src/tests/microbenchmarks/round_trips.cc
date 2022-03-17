2552734bd3fe7887026e8480864c4739cb17ce73
Mark Seaborn <mseaborn@google.com>
2022-03-17 01:23:19 +0000

    [microbenchmarks] Enable the condvar round-trip test on Linux
    Move this test into a separate file so that we can enable it on Linux
    without enclosing most of round_trips.cc in #ifdefs.
    
    Bug: 93071
    Change-Id: I8cc7aada3547ff712dcf47c74202169e5ec2cefc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/657324
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Mark Seaborn <mseaborn@google.com>

--- src/tests/microbenchmarks/round_trips.cc
+++ src/tests/microbenchmarks/round_trips.cc
@@ -583,66 +583,6 @@ class FutexTest {
   volatile int futex2_ = 0;  // Signals from server to client.
 };
 
-// Test the round trip time for waking up threads using pthread condition
-// variables (condvars).  Condvars are implemented using futexes, so we
-// expect this to be a bit slower than FutexTest due to the overhead that
-// pthread's condvar implementation adds.
-class PthreadCondvarTest {
- public:
-  PthreadCondvarTest() {
-    FX_CHECK(pthread_mutex_init(&mutex_, nullptr) == 0);
-    FX_CHECK(pthread_cond_init(&condvar1_, nullptr) == 0);
-    FX_CHECK(pthread_cond_init(&condvar2_, nullptr) == 0);
-    thread_ = std::thread([this]() { ThreadFunc(); });
-  }
-
-  ~PthreadCondvarTest() {
-    // Tell the thread to shut down.
-    FX_CHECK(pthread_mutex_lock(&mutex_) == 0);
-    state_ = EXIT;
-    FX_CHECK(pthread_cond_signal(&condvar1_) == 0);
-    FX_CHECK(pthread_mutex_unlock(&mutex_) == 0);
-
-    thread_.join();
-
-    FX_CHECK(pthread_cond_destroy(&condvar1_) == 0);
-    FX_CHECK(pthread_cond_destroy(&condvar2_) == 0);
-    FX_CHECK(pthread_mutex_destroy(&mutex_) == 0);
-  }
-
-  void Run() {
-    FX_CHECK(pthread_mutex_lock(&mutex_) == 0);
-    // Wake the child.
-    state_ = WAKE_CHILD;
-    FX_CHECK(pthread_cond_signal(&condvar1_) == 0);
-    // Wait for the reply.
-    while (state_ != REPLY_TO_PARENT)
-      FX_CHECK(pthread_cond_wait(&condvar2_, &mutex_) == 0);
-    FX_CHECK(pthread_mutex_unlock(&mutex_) == 0);
-  }
-
- private:
-  void ThreadFunc() {
-    FX_CHECK(pthread_mutex_lock(&mutex_) == 0);
-    for (;;) {
-      if (state_ == EXIT)
-        break;
-      if (state_ == WAKE_CHILD) {
-        state_ = REPLY_TO_PARENT;
-        FX_CHECK(pthread_cond_signal(&condvar2_) == 0);
-      }
-      FX_CHECK(pthread_cond_wait(&condvar1_, &mutex_) == 0);
-    }
-    FX_CHECK(pthread_mutex_unlock(&mutex_) == 0);
-  }
-
-  std::thread thread_;
-  pthread_mutex_t mutex_;
-  pthread_cond_t condvar1_;  // Signals from parent to child.
-  pthread_cond_t condvar2_;  // Signals from child to parent.
-  enum { INITIAL, WAKE_CHILD, REPLY_TO_PARENT, EXIT } state_ = INITIAL;
-};
-
 struct ThreadFuncEntry {
   const char* name;
   ThreadFunc func;
@@ -701,7 +641,6 @@ __attribute__((constructor)) void RegisterTests() {
   RegisterTestMultiProc<SocketPortTest>("RoundTrip_SocketPort");
   RegisterTestMultiProc<FidlTest>("RoundTrip_Fidl");
   fbenchmark::RegisterTest<FutexTest>("RoundTrip_Futex_SingleProcess");
-  fbenchmark::RegisterTest<PthreadCondvarTest>("RoundTrip_PthreadCondvar_SingleProcess");
 }
 
 }  // namespace
