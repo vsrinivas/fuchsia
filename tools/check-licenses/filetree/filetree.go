7ac0beb08846ab98cda4386e2dbacac091d38a38
Jerry Belton <jcecil@google.com>
2022-03-18 01:50:41 +0000

    [check-licenses] Updates to filetree package.
    Added config file for the filetree package, and the SkipAnywhere
    field to the filetree config, to allow us to skip a given directory
    (e.g. .git folders) from anywhere in the tree.
    
    By default, skip directories that we use to hold custom readme files
    and license pattern directories.
    
    Other small changes to make this package consistent with the other ones.
    
    Change-Id: I73e96973939e3bf09b8e3c1d106b85db110ee354
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/659397
    Reviewed-by: Sean Cuff <seancuff@google.com>
    Commit-Queue: Jerry Belton <jcecil@google.com>

--- tools/check-licenses/filetree/filetree.go
+++ tools/check-licenses/filetree/filetree.go
@@ -5,11 +5,9 @@
 package filetree
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime/trace"
 	"sort"
 	"sync"
 
@@ -18,33 +16,32 @@ import (
 
 // FileTree is an in memory representation of the state of the repository.
 type FileTree struct {
-	Name     string             `json:"name,omitempty"`
-	Path     string             `json:"path,omitempty"`
-	Files    []string           `json:"files,omitempty"`
-	Children []*FileTree        `json:"children,omitempty"`
-	Parent   *FileTree          `json:"-"`
-	Projects []*project.Project `json:"project,omitempty"`
+	Name      string             `json:"name,omitempty"`
+	Path      string             `json:"path,omitempty"`
+	FilePaths []string           `json:"files,omitempty"`
+	Children  []*FileTree        `json:"children,omitempty"`
+	Parent    *FileTree          `json:"-"`
+	Projects  []*project.Project `json:"project,omitempty"`
 
 	sync.RWMutex
 }
 
-// filetreeByPath implements sort.Interface for []*FileTree based on the Path field.
-type filetreeByPath []*FileTree
+// Order implements sort.Interface for []*FileTree based on the Path field.
+type Order []*FileTree
 
-func (a filetreeByPath) Len() int           { return len(a) }
-func (a filetreeByPath) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a filetreeByPath) Less(i, j int) bool { return a[i].Path < a[j].Path }
+func (a Order) Len() int           { return len(a) }
+func (a Order) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Order) Less(i, j int) bool { return a[i].Path < a[j].Path }
 
 // NewFileTree returns an instance of FileTree.
-func NewFileTree(ctx context.Context, root string, parent *FileTree) (*FileTree, error) {
-	defer trace.StartRegion(ctx, "NewFileTree").End()
+func NewFileTree(root string, parent *FileTree) (*FileTree, error) {
 	ft := FileTree{}
 
 	ft.Name = filepath.Base(root)
 	ft.Path = root
 	ft.Parent = parent
 
-	// If we are not at a "barrier" directory (e.g. prebuilt, thirdparty),
+	// If we are not at a "barrier" directory (e.g. prebuilt, third_party),
 	// then the license info of the parent directory also applies to this directory.
 	// Propagate that information down here.
 	if !project.IsBarrier(root) && parent != nil {
@@ -56,30 +53,32 @@ func NewFileTree(ctx context.Context, root string, parent *FileTree) (*FileTree,
 		return nil, err
 	}
 
-	// First, find all README.fuchsia files in the current directory,
-	// and create Project structs for them.
-	for _, item := range directoryContents {
-		path := filepath.Join(root, item.Name())
-		if item.Name() == "README.fuchsia" {
-			p, err := project.NewProject(path)
-			if err != nil {
-				return nil, err
-			}
-			ft.Projects = append(ft.Projects, p)
+	// First, find the README.fuchsia file in the current directory (if it exists),
+	// or retrieve the project instance from any predefined "readmes" directory.
+	p, err := project.NewProject(filepath.Join(root, "README.fuchsia"), root)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// There is no README.fuchsia file in the current directory.
+			// That's OK. Continue.
+		} else {
+			return nil, err
 		}
+	} else {
+		ft.Projects = append(ft.Projects, p)
 	}
 
 	// Then traverse the rest of the contents of this directory.
 	for _, item := range directoryContents {
 		path := filepath.Join(root, item.Name())
 		if Config.shouldSkip(path) {
+			plusVal(Skipped, path)
 			continue
 		}
 
 		// Directories
 		if item.IsDir() {
 			plus1(NumFolders)
-			child, err := NewFileTree(ctx, path, &ft)
+			child, err := NewFileTree(path, &ft)
 			if err != nil {
 				return nil, err
 			}
@@ -89,21 +88,28 @@ func NewFileTree(ctx context.Context, root string, parent *FileTree) (*FileTree,
 
 		// Files
 		plus1(NumFiles)
-		ft.Files = append(ft.Files, path)
+		ft.FilePaths = append(ft.FilePaths, path)
 	}
 
-	sort.Sort(filetreeByPath(ft.Children))
+	sort.Sort(Order(ft.Children))
+	sort.Sort(project.Order(ft.Projects))
 
 	// Verify that all files in the current directory belong to a project.
-	if len(ft.Projects) == 0 && len(ft.Files) > 0 {
-		plusVal(MissingProject, ft.Path)
-		if Config.ExitOnMissingProject {
-			return nil, fmt.Errorf("Filetree %v has no associated projects", root)
+	if len(ft.Projects) == 0 {
+		for _, f := range ft.FilePaths {
+			plusVal(FileMissingProject, f)
+		}
+		if len(ft.FilePaths) > 0 {
+			plusVal(FolderMissingProject, ft.Path)
+			if Config.ExitOnMissingProject {
+				return nil, fmt.Errorf("Filetree %v has no associated projects", root)
+			}
 		}
 	}
 	for _, p := range ft.Projects {
-		p.AddFiles(ft.Files)
+		p.AddFiles(ft.FilePaths)
 	}
 
+	AllFileTrees[ft.Path] = &ft
 	return &ft, nil
 }
