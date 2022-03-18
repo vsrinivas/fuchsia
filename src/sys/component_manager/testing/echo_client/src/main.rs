e50f7ad7ef27390320345fa8d81f19105a4c28fc
Dave Smith <smithdave@google.com>
2022-03-18 22:17:43 +0000

    [sys][component_manager] Reduce example dependency
    Provide stable test artifact for echo_client and migrate additional
    test dependencies to use //src/sys/component_manager/testing
    
    Bug: 82774
    Bug: 93065
    Change-Id: Ie03d6736c5aef3a0c638bfd137e393525a3e3715
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/660284
    Reviewed-by: Gabe Schine <thatguy@google.com>
    Commit-Queue: Dave Smith <smithdave@google.com>

---
+++ src/sys/component_manager/testing/echo_client/src/main.rs
@@ -0,0 +1,26 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow;
+use fidl_fidl_examples_routing_echo::EchoMarker;
+use fuchsia_component::client::connect_to_protocol;
+use tracing;
+
+#[fuchsia::component]
+async fn main() -> Result<(), anyhow::Error> {
+    // Parse arguments, removing binary name
+    let mut args: Vec<String> = std::env::args().collect();
+    args.remove(0);
+
+    // Connect to FIDL protocol
+    let echo = connect_to_protocol::<EchoMarker>().expect("error connecting to echo");
+
+    // Send messages over FIDL interface
+    for message in args {
+        let out = echo.echo_string(Some(&message)).await.expect("echo_string failed");
+        tracing::info!("Server response: {}", out.as_ref().expect("echo_string got empty result"));
+    }
+
+    Ok(())
+}
