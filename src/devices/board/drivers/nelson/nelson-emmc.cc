f5515525ba0cdf30eda1492566f631e1cc9d1127
Braden Kell <bradenkell@google.com>
2022-03-22 21:54:55 +0000

    [nelson][emmc] Increase frequency to 166 MHz
    Test: Ran iocheck on Nelson for one hour, no eMMC errors occurred
    Change-Id: I892a2d0d470908906a776b6c181fd1ad9a938ebe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/596563
    Reviewed-by: Gurjant Kalsi <gkalsi@google.com>
    Commit-Queue: Braden Kell <bradenkell@google.com>

--- src/devices/board/drivers/nelson/nelson-emmc.cc
+++ src/devices/board/drivers/nelson/nelson-emmc.cc
@@ -48,9 +48,8 @@ constexpr pbus_bti_t emmc_btis[] = {
 
 static aml_sdmmc_config_t config = {
     .supports_dma = true,
-    // As per AMlogic, on S912 chipset, HS400 mode can be operated at 125MHZ or low.
-    .min_freq = 400000,
-    .max_freq = 120000000,
+    .min_freq = 400'000,
+    .max_freq = 166'666'667,
     .version_3 = true,
     .prefs = SDMMC_HOST_PREFS_DISABLE_HS400,
     .use_new_tuning = true,
