4312261e9ba95cbd391f77dacfcbf73943b3075f
Benjamin Lerman <qsr@chromium.org>
2022-03-25 13:11:24 +0000

    [starnix][pty] Use a shared Terminal object between main and replicas
    This ensures the replicas device nodes are removed once both sides are
    closed
    
    Bug: 95707
    Change-Id: I7a42c5d1e7ebb0c3769df5d31a0dfb0f77479250
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/662142
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/proc/bin/starnix/fs/devpts.rs
+++ src/proc/bin/starnix/fs/devpts.rs
@@ -2,8 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use parking_lot::Mutex;
-use std::sync::Arc;
+use parking_lot::{Mutex, RwLock};
+use std::collections::{BTreeSet, HashMap};
+use std::sync::{Arc, Weak};
 
 use crate::device::DeviceOps;
 use crate::device::WithStaticDeviceId;
@@ -31,10 +32,10 @@ pub fn dev_pts_fs(kernel: &Kernel) -> &FileSystemHandle {
 
 fn init_devpts(kernel: &Kernel) -> FileSystemHandle {
     let fs = TmpFs::new();
-    let root = fs.root();
 
     // Create ptmx
-    root.create_node(b"ptmx", FileMode::IFCHR | FileMode::from_bits(0o666), DeviceType::PTMX)
+    fs.root()
+        .create_node(b"ptmx", FileMode::IFCHR | FileMode::from_bits(0o666), DeviceType::PTMX)
         .unwrap();
 
     {
@@ -55,19 +56,67 @@ fn init_devpts(kernel: &Kernel) -> FileSystemHandle {
 
 struct TTYState {
     fs: FileSystemHandle,
-    next_id: Mutex<u32>,
+    terminals: RwLock<HashMap<u32, Weak<Terminal>>>,
+    pts_ids_set: Mutex<PtsIdsSet>,
 }
 
 impl TTYState {
     pub fn new(fs: FileSystemHandle) -> Self {
-        Self { fs, next_id: Mutex::new(0) }
+        Self {
+            fs,
+            terminals: RwLock::new(HashMap::new()),
+            pts_ids_set: Mutex::new(PtsIdsSet::new(DEVPTS_COUNT)),
+        }
     }
 
-    pub fn get_next_id(&self) -> u32 {
-        let mut next_id = self.next_id.lock();
-        let id = *next_id;
-        *next_id = (*next_id + 1) % DEVPTS_COUNT;
-        id
+    pub fn get_next_terminal(self: &Arc<Self>) -> Result<Arc<Terminal>, Errno> {
+        let id = self.pts_ids_set.lock().get()?;
+        let device_type = get_device_type_for_pts(id);
+        let terminal = Arc::new(Terminal::new(self.clone(), id));
+
+        self.fs.root().create_node(
+            id.to_string().as_bytes(),
+            FileMode::IFCHR | FileMode::from_bits(0o666),
+            device_type,
+        )?;
+        self.terminals.write().insert(id, Arc::downgrade(&terminal));
+        Ok(terminal)
+    }
+
+    pub fn release_terminal(&self, id: u32) -> Result<(), Errno> {
+        let pts_filename = id.to_string();
+        match self.fs.root().unlink(pts_filename.as_bytes(), UnlinkKind::NonDirectory) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                if e.value() == ENOENT.value() {
+                    log::warn!("Unable to delete pts id {}.", id);
+                    Ok(())
+                } else {
+                    Err(e)
+                }
+            }
+        }?;
+
+        self.pts_ids_set.lock().release(id);
+        self.terminals.write().remove(&id);
+        Ok(())
+    }
+}
+
+struct Terminal {
+    state: Arc<TTYState>,
+    id: u32,
+}
+
+impl Terminal {
+    pub fn new(state: Arc<TTYState>, id: u32) -> Self {
+        Self { state, id }
+    }
+}
+
+impl Drop for Terminal {
+    fn drop(&mut self) {
+        self.state.release_terminal(self.id).unwrap()
     }
 }
 
@@ -92,27 +141,19 @@ impl DeviceOps for DevPtmx {
         _node: &FsNode,
         _flags: OpenFlags,
     ) -> Result<Box<dyn FileOps>, Errno> {
-        let id = self.state.get_next_id();
-        let device_type = get_device_type_for_pts(id);
-        self.state
-            .fs
-            .root()
-            .create_node(
-                id.to_string().as_bytes(),
-                FileMode::IFCHR | FileMode::from_bits(0o666),
-                device_type,
-            )
-            .unwrap();
+        let terminal = self.state.get_next_terminal()?;
 
-        Ok(Box::new(DevPtmxFile::new(self.state.clone(), id)))
+        Ok(Box::new(DevPtmxFile::new(terminal)))
     }
 }
 
-struct DevPtmxFile {}
+struct DevPtmxFile {
+    _terminal: Arc<Terminal>,
+}
 
 impl DevPtmxFile {
-    pub fn new(_state: Arc<TTYState>, _id: u32) -> Self {
-        Self {}
+    pub fn new(terminal: Arc<Terminal>) -> Self {
+        Self { _terminal: terminal }
     }
 }
 
@@ -202,14 +243,15 @@ impl DeviceOps for DevPts {
         _flags: OpenFlags,
     ) -> Result<Box<dyn FileOps>, Errno> {
         let pts_id = (id.major() - DEVPTS_FIRST_MAJOR) * 256 + id.minor();
-        Ok(Box::new(DevPtsFile::new(self.state.clone(), pts_id)))
+        let terminal = self.state.terminals.read().get(&pts_id).ok_or(EIO)?.upgrade().ok_or(EIO)?;
+        Ok(Box::new(DevPtsFile::new(terminal)))
     }
 }
 
 struct DevPtsFile {}
 
 impl DevPtsFile {
-    pub fn new(_state: Arc<TTYState>, _id: u32) -> Self {
+    pub fn new(_terminal: Arc<Terminal>) -> Self {
         Self {}
     }
 }
@@ -282,6 +324,41 @@ impl FileOps for DevPtsFile {
     }
 }
 
+struct PtsIdsSet {
+    pts_count: u32,
+    next_id: u32,
+    reclaimed_ids: BTreeSet<u32>,
+}
+
+impl PtsIdsSet {
+    pub fn new(pts_count: u32) -> Self {
+        Self { pts_count, next_id: 0, reclaimed_ids: BTreeSet::new() }
+    }
+
+    pub fn release(&mut self, id: u32) {
+        assert!(self.reclaimed_ids.insert(id))
+    }
+
+    pub fn get(&mut self) -> Result<u32, Errno> {
+        match self.reclaimed_ids.iter().next() {
+            Some(e) => {
+                let value = e.clone();
+                self.reclaimed_ids.remove(&value);
+                Ok(value)
+            }
+            None => {
+                if self.next_id < self.pts_count {
+                    let id = self.next_id;
+                    self.next_id += 1;
+                    Ok(id)
+                } else {
+                    error!(ENOSPC)
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -294,7 +371,65 @@ mod tests {
         let root = fs.root();
         assert!(root.component_lookup(b"0").is_err());
         let ptmx = root.component_lookup(b"ptmx").unwrap();
-        ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
+        let _opened_ptmx = ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
         assert!(root.component_lookup(b"0").is_ok());
+    }
+
+    #[test]
+    fn closing_ptmx_closes_pts() {
+        let (kernel, _task) = create_kernel_and_task();
+        let fs = dev_pts_fs(&kernel);
+        let root = fs.root();
+        assert!(root.component_lookup(b"0").is_err());
+        let ptmx = root.component_lookup(b"ptmx").unwrap();
+        ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
+        assert!(root.component_lookup(b"0").is_err());
+    }
+
+    #[test]
+    fn pts_are_reused() {
+        let (kernel, _task) = create_kernel_and_task();
+        let fs = dev_pts_fs(&kernel);
+        let root = fs.root();
+        assert!(root.component_lookup(b"0").is_err());
+        let ptmx = root.component_lookup(b"ptmx").unwrap();
+        let _opened_ptmx0 = ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
+        let mut _opened_ptmx1 = ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
+        let _opened_ptmx2 = ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
+        assert!(root.component_lookup(b"0").is_ok());
+        assert!(root.component_lookup(b"1").is_ok());
+        assert!(root.component_lookup(b"2").is_ok());
+        std::mem::drop(_opened_ptmx1);
+        assert!(root.component_lookup(b"1").is_err());
+        _opened_ptmx1 = ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
+        assert!(root.component_lookup(b"1").is_ok());
+    }
+
+    #[test]
+    fn opening_inexistant_replica_fails() {
+        let (kernel, _task) = create_kernel_and_task();
+        let fs = dev_pts_fs(&kernel);
+        let pts = fs
+            .root()
+            .create_node(
+                b"custom_pts",
+                FileMode::IFCHR | FileMode::from_bits(0o666),
+                DeviceType::new(DEVPTS_FIRST_MAJOR, 0),
+            )
+            .unwrap();
+        assert!(pts.node.open(&kernel, OpenFlags::RDONLY).is_err());
+    }
+
+    #[test]
+    fn deleting_pts_nodes_do_not_crash() {
+        let (kernel, _task) = create_kernel_and_task();
+        let fs = dev_pts_fs(&kernel);
+        let root = fs.root();
+        let ptmx = root.component_lookup(b"ptmx").unwrap();
+        let opened_ptmx0 = ptmx.node.open(&kernel, OpenFlags::RDONLY).unwrap();
+        assert!(root.component_lookup(b"0").is_ok());
+        root.unlink(b"0", UnlinkKind::NonDirectory).unwrap();
+        assert!(root.component_lookup(b"0").is_err());
+        std::mem::drop(opened_ptmx0);
     }
 }
