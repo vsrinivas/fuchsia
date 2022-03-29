5d306755de6e2d050c79fb120d80724411f957df
Braden Kell <bradenkell@google.com>
2022-03-29 17:44:21 +0000

    [i2c][lib] Add FIDL support to device-protocol-i2c-channel
    Split the existing I2cChannel class into a base and implementation, and
    add a wrapper for the FIDL protocol. Almost all I2C clients use this
    library, so supporting FIDL is a prerequisite for migration.
    
    Bug: 96293
    MULTIPLY: i2c-channel-test
    Change-Id: If7e85c455bd9f53f15c85d57e633474d030b2d96
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/663524
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Commit-Queue: Braden Kell <bradenkell@google.com>

--- src/devices/i2c/lib/device-protocol-i2c-channel/include/lib/device-protocol/i2c-channel.h
+++ src/devices/i2c/lib/device-protocol-i2c-channel/include/lib/device-protocol/i2c-channel.h
@@ -5,6 +5,7 @@
 #ifndef SRC_DEVICES_I2C_LIB_DEVICE_PROTOCOL_I2C_CHANNEL_INCLUDE_LIB_DEVICE_PROTOCOL_I2C_CHANNEL_H_
 #define SRC_DEVICES_I2C_LIB_DEVICE_PROTOCOL_I2C_CHANNEL_INCLUDE_LIB_DEVICE_PROTOCOL_I2C_CHANNEL_H_
 
+#include <fidl/fuchsia.hardware.i2c/cpp/wire.h>
 #include <fuchsia/hardware/i2c/cpp/banjo.h>
 #include <lib/device-protocol/i2c.h>
 #include <lib/sync/completion.h>
@@ -12,23 +13,16 @@
 
 namespace ddk {
 
-class I2cChannel : public I2cProtocolClient {
+// TODO(fxbug.dev/96293): Merge I2cFidlChannel back into I2cChannel and delete I2cChannelBase once
+// all clients are using FIDL.
+class I2cChannelBase {
  public:
   struct StatusRetries {
     zx_status_t status;
     uint8_t retries;
   };
 
-  I2cChannel() {}
-
-  I2cChannel(const i2c_protocol_t* proto) : I2cProtocolClient(proto) {}
-
-  I2cChannel(zx_device_t* parent) : I2cProtocolClient(parent) {}
-
-  I2cChannel(zx_device_t* parent, const char* fragment_name)
-      : I2cProtocolClient(parent, fragment_name) {}
-
-  ~I2cChannel() = default;
+  virtual ~I2cChannelBase() = default;
 
   // Performs typical i2c Read: writes device register address (1 byte) followed
   // by len reads into buf.
@@ -41,11 +35,8 @@ class I2cChannel : public I2cProtocolClient {
     return WriteReadSync(buf, len, nullptr, 0);
   }
 
-  zx_status_t WriteReadSync(const uint8_t* tx_buf, size_t tx_len, uint8_t* rx_buf, size_t rx_len) {
-    i2c_protocol_t proto;
-    GetProto(&proto);
-    return i2c_write_read_sync(&proto, tx_buf, tx_len, rx_buf, rx_len);
-  }
+  virtual zx_status_t WriteReadSync(const uint8_t* tx_buf, size_t tx_len, uint8_t* rx_buf,
+                                    size_t rx_len) = 0;
 
   // ReadSync with retries, returns status and retry attempts.
   StatusRetries ReadSyncRetries(uint8_t addr, uint8_t* buf, size_t len, uint8_t retries,
@@ -62,17 +53,112 @@ class I2cChannel : public I2cProtocolClient {
   // WriteReadSync with retries, returns status and retry attempts.
   StatusRetries WriteReadSyncRetries(const uint8_t* tx_buf, size_t tx_len, uint8_t* rx_buf,
                                      size_t rx_len, uint8_t retries, zx::duration delay) {
-    i2c_protocol_t proto;
-    GetProto(&proto);
     uint8_t attempt = 0;
-    auto status = i2c_write_read_sync(&proto, tx_buf, tx_len, rx_buf, rx_len);
+    auto status = WriteReadSync(tx_buf, tx_len, rx_buf, rx_len);
     while (status != ZX_OK && attempt < retries) {
       zx::nanosleep(zx::deadline_after(delay));
       attempt++;
-      status = i2c_write_read_sync(&proto, tx_buf, tx_len, rx_buf, rx_len);
+      status = WriteReadSync(tx_buf, tx_len, rx_buf, rx_len);
     }
     return {status, attempt};
   }
+};
+
+class I2cChannel : public I2cChannelBase, public I2cProtocolClient {
+ public:
+  I2cChannel() = default;
+
+  I2cChannel(const i2c_protocol_t* proto) : I2cProtocolClient(proto) {}
+
+  I2cChannel(zx_device_t* parent) : I2cProtocolClient(parent) {}
+
+  I2cChannel(zx_device_t* parent, const char* fragment_name)
+      : I2cProtocolClient(parent, fragment_name) {}
+
+  ~I2cChannel() override = default;
+
+  zx_status_t WriteReadSync(const uint8_t* tx_buf, size_t tx_len, uint8_t* rx_buf,
+                            size_t rx_len) override {
+    i2c_protocol_t proto;
+    GetProto(&proto);
+    return i2c_write_read_sync(&proto, tx_buf, tx_len, rx_buf, rx_len);
+  }
+};
+
+class I2cFidlChannel : public I2cChannelBase {
+ public:
+  explicit I2cFidlChannel(fidl::ClientEnd<fuchsia_hardware_i2c::Device2> client_end)
+      : fidl_client_(std::move(client_end)) {}
+
+  ~I2cFidlChannel() override = default;
+
+  fidl::WireResult<fuchsia_hardware_i2c::Device2::Transfer> Transfer(
+      fidl::VectorView<bool> segments_is_write,
+      fidl::VectorView<fidl::VectorView<uint8_t>> write_segments_data,
+      fidl::VectorView<uint8_t> read_segments_length) {
+    return fidl_client_->Transfer(segments_is_write, write_segments_data, read_segments_length);
+  }
+
+  zx_status_t WriteReadSync(const uint8_t* tx_buf, size_t tx_len, uint8_t* rx_buf,
+                            size_t rx_len) override {
+    if (tx_len > fuchsia_hardware_i2c::wire::kMaxTransferSize || rx_len > UINT8_MAX) {
+      return ZX_ERR_OUT_OF_RANGE;
+    }
+
+    fidl::Arena arena;
+
+    fidl::VectorView<bool> segments_is_write;
+    fidl::VectorView<fidl::VectorView<uint8_t>> write_segments;
+    fidl::VectorView<uint8_t> read_segments_length;
+
+    if (tx_len > 0 && rx_len > 0) {
+      segments_is_write = fidl::VectorView<bool>(arena, 2);
+      segments_is_write[0] = true;
+      segments_is_write[1] = false;
+    } else if (tx_len > 0) {
+      segments_is_write = fidl::VectorView<bool>(arena, 1);
+      segments_is_write[0] = true;
+    } else if (rx_len > 0) {
+      segments_is_write = fidl::VectorView<bool>(arena, 1);
+      segments_is_write[0] = false;
+    } else {
+      return ZX_OK;
+    }
+
+    if (tx_len > 0) {
+      write_segments = fidl::VectorView<fidl::VectorView<uint8_t>>(arena, 1);
+      write_segments[0] = fidl::VectorView<uint8_t>(arena, tx_len);
+      memcpy(write_segments[0].mutable_data(), tx_buf, tx_len);
+    }
+
+    if (rx_len > 0) {
+      read_segments_length = fidl::VectorView<uint8_t>(arena, 1);
+      read_segments_length[0] = static_cast<uint8_t>(rx_len);
+    }
+
+    const auto reply =
+        fidl_client_->Transfer(segments_is_write, write_segments, read_segments_length);
+    if (!reply.ok()) {
+      return reply.status();
+    }
+    if (reply->result.is_err()) {
+      return reply->result.err();
+    }
+
+    if (rx_len > 0) {
+      const auto& read_segments_data = reply->result.response().read_segments_data;
+      if (read_segments_data.count() != 1 || read_segments_data[0].count() != rx_len) {
+        return ZX_ERR_IO;
+      }
+
+      memcpy(rx_buf, read_segments_data[0].data(), rx_len);
+    }
+
+    return ZX_OK;
+  }
+
+ private:
+  fidl::WireSyncClient<fuchsia_hardware_i2c::Device2> fidl_client_;
 };
 
 }  // namespace ddk
--- src/devices/i2c/lib/device-protocol-i2c-channel/test.cc
+++ src/devices/i2c/lib/device-protocol-i2c-channel/test.cc
@@ -2,13 +2,72 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
 #include <lib/device-protocol/i2c-channel.h>
 #include <lib/fake-i2c/fake-i2c.h>
 #include <zircon/errors.h>
 
+#include <vector>
+
 #include <zxtest/zxtest.h>
 
-class I2cDevice : public fake_i2c::FakeI2c {
+class I2cDevice : public fake_i2c::FakeI2c, public fidl::WireServer<fuchsia_hardware_i2c::Device2> {
+ public:
+  void Transfer(TransferRequestView request, TransferCompleter::Sync& completer) override {
+    if (request->segments_is_write.count() <= 0 || request->segments_is_write.count() > 2) {
+      completer.ReplyError(ZX_ERR_OUT_OF_RANGE);
+      return;
+    }
+
+    const bool is_write = request->segments_is_write[0];
+    const bool is_read = !request->segments_is_write[request->segments_is_write.count() - 1];
+
+    if (request->segments_is_write.count() == 2 && (!is_write || !is_read)) {
+      completer.ReplyError(ZX_ERR_INVALID_ARGS);
+      return;
+    }
+    if (is_write && request->write_segments_data.count() != 1) {
+      completer.ReplyError(ZX_ERR_INVALID_ARGS);
+      return;
+    }
+    if (is_read && request->read_segments_length.count() != 1) {
+      completer.ReplyError(ZX_ERR_INVALID_ARGS);
+      return;
+    }
+
+    if (is_write &&
+        request->write_segments_data[0].count() > fuchsia_hardware_i2c::wire::kMaxTransferSize) {
+      completer.ReplyError(ZX_ERR_OUT_OF_RANGE);
+      return;
+    }
+    if (is_read && request->read_segments_length[0] != rx_data_.size()) {
+      completer.ReplyError(ZX_ERR_IO);
+      return;
+    }
+
+    if (is_write) {
+      tx_data_ = std::vector<uint8_t>(request->write_segments_data[0].cbegin(),
+                                      request->write_segments_data[0].cend());
+    } else {
+      tx_data_.clear();
+    }
+
+    fidl::Arena arena;
+
+    fidl::ObjectView<fuchsia_hardware_i2c::wire::Device2TransferResponse> response(arena);
+    if (is_read) {
+      response->read_segments_data = fidl::VectorView<fidl::VectorView<uint8_t>>(arena, 1);
+      response->read_segments_data[0] = fidl::VectorView<uint8_t>(arena, rx_data_.size());
+      memcpy(response->read_segments_data[0].mutable_data(), rx_data_.data(), rx_data_.size());
+    }
+
+    completer.Reply(fuchsia_hardware_i2c::wire::Device2TransferResult::WithResponse(response));
+  }
+
+  const std::vector<uint8_t>& tx_data() const { return tx_data_; }
+  void set_rx_data(std::vector<uint8_t> rx_data) { rx_data_ = std::move(rx_data); }
+
  protected:
   zx_status_t Transact(const uint8_t* write_buffer, size_t write_buffer_size, uint8_t* read_buffer,
                        size_t* read_buffer_size) override {
@@ -29,6 +88,18 @@ class I2cDevice : public fake_i2c::FakeI2c {
 
  private:
   size_t count = 0;
+  std::vector<uint8_t> tx_data_;
+  std::vector<uint8_t> rx_data_;
+};
+
+class I2cChannelTest : public zxtest::Test {
+ public:
+  I2cChannelTest() : loop_(&kAsyncLoopConfigNoAttachToCurrentThread) {}
+
+  void SetUp() override { loop_.StartThread(); }
+
+ protected:
+  async::Loop loop_;
 };
 
 TEST(I2cChannelTest, NoRetries) {
@@ -66,4 +137,69 @@ TEST(I2cChannelTest, RetriesOk) {
                                           sizeof(rx_buffer), kNumberOfRetries, zx::usec(1));
   EXPECT_EQ(ret.status, ZX_OK);
   EXPECT_EQ(ret.retries, 4);
+}
+
+TEST_F(I2cChannelTest, FidlRead) {
+  auto endpoints = fidl::CreateEndpoints<fuchsia_hardware_i2c::Device2>();
+  ASSERT_TRUE(endpoints.is_ok());
+
+  I2cDevice i2c_dev;
+  auto binding = fidl::BindServer(loop_.dispatcher(), std::move(endpoints->server), &i2c_dev);
+
+  const std::array<uint8_t, 4> expected_rx_data{0x12, 0x34, 0xab, 0xcd};
+
+  ddk::I2cFidlChannel client(std::move(endpoints->client));
+  i2c_dev.set_rx_data({expected_rx_data.data(), expected_rx_data.data() + expected_rx_data.size()});
+
+  uint8_t buf[4];
+  EXPECT_OK(client.ReadSync(0x89, buf, expected_rx_data.size()));
+  ASSERT_EQ(i2c_dev.tx_data().size(), 1);
+  EXPECT_EQ(i2c_dev.tx_data()[0], 0x89);
+  EXPECT_BYTES_EQ(buf, expected_rx_data.data(), expected_rx_data.size());
+
+  // Make sure the library propagates errors.
+  EXPECT_NOT_OK(client.ReadSync(0x89, buf, 3));
+}
+
+TEST_F(I2cChannelTest, FidlWrite) {
+  auto endpoints = fidl::CreateEndpoints<fuchsia_hardware_i2c::Device2>();
+  ASSERT_TRUE(endpoints.is_ok());
+
+  I2cDevice i2c_dev;
+  auto binding = fidl::BindServer(loop_.dispatcher(), std::move(endpoints->server), &i2c_dev);
+
+  const std::array<uint8_t, 4> expected_tx_data{0x0f, 0x1e, 0x2d, 0x3c};
+
+  ddk::I2cFidlChannel client(std::move(endpoints->client));
+
+  EXPECT_OK(client.WriteSync(expected_tx_data.data(), expected_tx_data.size()));
+  ASSERT_EQ(i2c_dev.tx_data().size(), expected_tx_data.size());
+  EXPECT_BYTES_EQ(i2c_dev.tx_data().data(), expected_tx_data.data(), expected_tx_data.size());
+}
+
+TEST_F(I2cChannelTest, FidlWriteRead) {
+  auto endpoints = fidl::CreateEndpoints<fuchsia_hardware_i2c::Device2>();
+  ASSERT_TRUE(endpoints.is_ok());
+
+  I2cDevice i2c_dev;
+  auto binding = fidl::BindServer(loop_.dispatcher(), std::move(endpoints->server), &i2c_dev);
+
+  const std::array<uint8_t, 4> expected_rx_data{0x12, 0x34, 0xab, 0xcd};
+  const std::array<uint8_t, 4> expected_tx_data{0x0f, 0x1e, 0x2d, 0x3c};
+
+  ddk::I2cFidlChannel client(std::move(endpoints->client));
+  i2c_dev.set_rx_data({expected_rx_data.data(), expected_rx_data.data() + expected_rx_data.size()});
+
+  uint8_t buf[4];
+  EXPECT_OK(client.WriteReadSync(expected_tx_data.data(), expected_tx_data.size(), buf,
+                                 expected_rx_data.size()));
+  ASSERT_EQ(i2c_dev.tx_data().size(), expected_tx_data.size());
+  EXPECT_BYTES_EQ(i2c_dev.tx_data().data(), expected_tx_data.data(), expected_tx_data.size());
+  EXPECT_BYTES_EQ(buf, expected_rx_data.data(), expected_rx_data.size());
+
+  EXPECT_NOT_OK(client.WriteReadSync(expected_tx_data.data(), expected_tx_data.size(), buf, 3));
+
+  EXPECT_OK(client.WriteReadSync(nullptr, 0, buf, expected_rx_data.size()));
+  EXPECT_EQ(i2c_dev.tx_data().size(), 0);
+  EXPECT_BYTES_EQ(buf, expected_rx_data.data(), expected_rx_data.size());
 }
