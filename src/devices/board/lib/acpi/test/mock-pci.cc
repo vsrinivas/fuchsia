056c63e55d1a4508cab5ef8adf5aacbb0b18e7c2
Simon Shields <simonshields@google.com>
2022-03-31 18:00:44 +1100

    [x86][acpi] Store ACPI_DEVICE_INFO in UniquePtr.
    In a newer version of ACPICA this will become variable-length, so we
    can't just copy it around like we used to.
    
    Change-Id: I57f0d01366195bde9752769a07373c8f7a4a7194

--- src/devices/board/lib/acpi/test/mock-pci.cc
+++ src/devices/board/lib/acpi/test/mock-pci.cc
@@ -13,8 +13,9 @@ std::vector<pci_bdf_t> stored_acpi_bdfs;
 std::vector<pci_bdf_t> GetAcpiBdfs() { return std::move(stored_acpi_bdfs); }
 }  // namespace acpi::test
 
-zx_status_t pci_init(zx_device_t* platform_bus, ACPI_HANDLE object, ACPI_DEVICE_INFO* info,
-                     acpi::Manager* acpi, std::vector<pci_bdf_t> acpi_bdfs) {
+zx_status_t pci_init(zx_device_t* platform_bus, ACPI_HANDLE object,
+                     acpi::UniquePtr<ACPI_DEVICE_INFO> info, acpi::Manager* acpi,
+                     std::vector<pci_bdf_t> acpi_bdfs) {
   acpi::test::stored_acpi_bdfs = std::move(acpi_bdfs);
   return ZX_OK;
 }
