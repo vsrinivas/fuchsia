da7b539d87dcf86fd30399e43582675da891d229
Tamir Duberstein <tamird@google.com>
2022-04-01 17:00:54 +0000

    [poison_tuf_signature] Optimize
    - Use buffered readers and writers.
    - Treat most of the JSON payload as opaque `Value`.
    - Avoid intermediate vectors.
    - Don't compress the result.
    
    Before this change:
      fx build host_x64/poison_tuf_signature && time ./host_x64/poison_tuf_signature \
        --input obj/src/security/pkg_test/tests/core_bad_signature_update_v1_pm_publish_archive.zip \
        --output obj/src/security/pkg_test/tests/poisoned_v1_tuf_repo.zip
      ninja: Entering directory `/usr/local/google/home/tamird/src/fuchsia/out/qemu-x64'
      [2/2] COPY host_x64-asan-ubsan/poison_tuf_signature host_x64/poison_tuf_signature
    
      real    6m41.866s
      user    6m40.525s
      sys     0m1.128s
    
    After this change:
      fx build host_x64/poison_tuf_signature && time ./host_x64/poison_tuf_signature \
        --input obj/src/security/pkg_test/tests/core_bad_signature_update_v1_pm_publish_archive.zip \
        --output obj/src/security/pkg_test/tests/poisoned_v1_tuf_repo.zip
      ninja: Entering directory `/usr/local/google/home/tamird/src/fuchsia/out/qemu-x64'
      [0/1] Regenerating ninja files
      [2/2] COPY host_x64-asan-ubsan/poison_tuf_signature host_x64/poison_tuf_signature
    
      real    0m53.149s
      user    0m52.351s
      sys     0m0.727s
    
    Bug: 88453
    Change-Id: I4377111c3a0bbc99e7711f949f0386618f67db2b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/664989
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Mark Dittmer <markdittmer@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/security/pkg_test/poison_tuf_signature/src/main.rs
+++ src/security/pkg_test/poison_tuf_signature/src/main.rs
@@ -3,23 +3,21 @@
 // found in the LICENSE file.
 
 use {
-    anyhow::{bail, Context, Result},
-    argh::from_env,
+    anyhow::Context as _,
     serde::{Deserialize, Serialize},
-    serde_json::{from_reader, to_vec},
     std::{
-        collections::BTreeMap,
         fs::File,
-        io::{Read, Write},
+        io::{copy, BufReader, BufWriter, Write as _},
     },
     zip::{
         read::ZipArchive,
         write::{FileOptions, ZipWriter},
+        CompressionMethod,
     },
 };
 
 /// Flags for poison_tuf_signature.
-#[derive(argh::FromArgs, Debug, PartialEq)]
+#[derive(argh::FromArgs)]
 pub struct Args {
     /// absolute path to input zip file containing TUF repository.
     #[argh(option)]
@@ -32,81 +30,63 @@ pub struct Args {
 }
 
 #[derive(Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 struct TargetsJson {
     signatures: Vec<Signature>,
-    signed: Targets,
+    signed: serde_json::Value,
 }
 
 #[derive(Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 struct Signature {
-    keyid: String,
     sig: String,
+    keyid: serde_json::Value,
 }
 
-#[derive(Deserialize, Serialize)]
-struct Targets {
-    targets: BTreeMap<String, Target>,
-}
-
-#[derive(Deserialize, Serialize)]
-struct Target {
-    custom: TargetCustom,
-}
-
-#[derive(Deserialize, Serialize)]
-struct TargetCustom {
-    merkle: String,
-    size: usize,
-}
-
-fn poison_signature(targets_json_contents: &[u8]) -> Result<Vec<u8>> {
-    let mut targets_json: TargetsJson =
-        from_reader(targets_json_contents).context("Failed to deserialize targets.json data")?;
-    if targets_json.signatures.len() == 0 {
-        bail!("targets.json contains no signatures to poison");
-    }
-    match targets_json.signatures[0].sig.chars().next() {
-        Some(first_char) => {
-            targets_json.signatures[0]
-                .sig
-                .replace_range(0..1, if first_char == '0' { "1" } else { "0" });
-            to_vec(&targets_json)
-                .context("Failed to serialize targets.json with poisoned signature")
-        }
-        None => {
-            bail!("targets.json contains empty signature hex string");
-        }
-    }
-}
-
-fn main() -> Result<()> {
-    let _args @ Args { input, output } = &from_env();
+fn main() -> anyhow::Result<()> {
+    let Args { input, output } = argh::from_env();
 
     let input_file = File::open(input).context("Failed to open input zip archive")?;
+    let input_file = BufReader::new(input_file);
     let mut input_zip = ZipArchive::new(input_file).context("Failed open file as zip archive")?;
     let output_file = File::create(output).context("Failed to create output zip archive")?;
+    let output_file = BufWriter::new(output_file);
     let mut output_zip = ZipWriter::new(output_file);
 
     for i in 0..input_zip.len() {
         let mut in_file =
             input_zip.by_index(i).context("Failed to open file inside input zip archive")?;
 
+        let name = in_file.name();
+
+        // Do not compress; we are producing test-only output here and size doesn't matter.
+        let options = FileOptions::default().compression_method(CompressionMethod::Stored);
+
         output_zip
-            .start_file(in_file.name(), FileOptions::default())
+            .start_file(name, options)
             .context("Failed to create file in output zip archive")?;
-        let mut contents = vec![];
-        in_file
-            .read_to_end(&mut contents)
-            .context("Failed to read file inside input zip archive")?;
 
-        if in_file.name().ends_with("targets.json") {
-            contents =
-                poison_signature(contents.as_slice()).context("Failed to poison signature")?;
-        }
-
-        output_zip
-            .write_all(contents.as_slice())
-            .context("Failed to write to file inside output zip archive")?;
+        if name.ends_with("targets.json") {
+            let mut json = serde_json::from_reader(in_file)
+                .context("Failed to deserialize targets.json data")?;
+            let TargetsJson { signatures, signed: _ } = &mut json;
+            match signatures.as_mut_slice() {
+                [] => anyhow::bail!("targets.json contains no signatures to poison"),
+                [Signature { keyid: _, sig }, ..] => match sig.chars().next() {
+                    None => {
+                        anyhow::bail!("targets.json contains no signatures to poison");
+                    }
+                    Some(first_char) => {
+                        sig.replace_range(..1, if first_char == '0' { "1" } else { "0" });
+                    }
+                },
+            }
+            let () = serde_json::to_writer(&mut output_zip, &json)
+                .context("Failed to serialize targets.json with poisoned signature")?;
+        } else {
+            let _: u64 = copy(&mut in_file, &mut output_zip)
+                .context("Failed to write to file inside output zip archive")?;
+        };
     }
 
     let mut output_file = output_zip.finish().context("Failed to finalize output zip archive")?;
