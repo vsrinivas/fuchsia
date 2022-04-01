97bba80f5eb3bb5d6d77dbfc21a4ac5789368764
Jerry Belton <jcecil@google.com>
2022-04-01 16:20:50 +0000

    [licenses] Fixing copyright headers
    Change-Id: I13d74d6dd68e8ee273548227176bfa0c80331986
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/664985
    Fuchsia-Auto-Submit: Jerry Belton <jcecil@google.com>
    Reviewed-by: Marie Janssen <jamuraa@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/forensics/utils/sized_data.h
+++ src/developer/forensics/utils/sized_data.h
@@ -1,5 +1,5 @@
 // Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source is governed by a BSD-style license that can be
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef SRC_DEVELOPER_FORENSICS_UTILS_SIZED_DATA_H_
