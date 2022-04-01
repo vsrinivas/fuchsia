5d3009fa8c208f267e98e8757fe5273350f90c3d
Chip Fukuhara <cfukuhara@google.com>
2022-04-01 01:21:40 +0000

    [settings] Add inspect queue
    Adds a utility queue of specified length that holds inspect-writeable
    items.
    
    Bug: 94356
    Test: fx test setui_service_tests
    
    Change-Id: Ibe6eaf5ab366b166f03f01f56b542db14f4432e7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/663791
    Reviewed-by: William Xiao <wxyz@google.com>
    Commit-Queue: Chip Fukuhara <cfukuhara@google.com>

--- garnet/bin/setui/src/inspect/utils.rs
+++ garnet/bin/setui/src/inspect/utils.rs
@@ -2,5 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// A queue of fixed length that holds inspect-writeable items that is
+/// inspect-writeable itself.
+pub mod inspect_queue;
+
 /// A map from String to inspect-writeable item.
 pub mod inspect_map;
---
+++ garnet/bin/setui/src/inspect/utils/inspect_queue.rs
@@ -0,0 +1,151 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![cfg_attr(not(test), allow(dead_code))]
+
+use fuchsia_inspect::{self as inspect, Node};
+use fuchsia_inspect_derive::{AttachError, Inspect};
+use std::collections::VecDeque;
+
+/// A vector of items to be written to inspect. It will only keep the last [size_limit]
+/// items. It is compatible with inspect_derive.
+#[derive(Default)]
+pub(crate) struct InspectQueue<T> {
+    items: VecDeque<T>,
+    // Required because VecDeque::with_capacity doesn't necessarily give the
+    // exact capacity specified.
+    size_limit: usize,
+    pub(crate) inspect_node: inspect::Node,
+}
+
+impl<T> InspectQueue<T> {
+    pub(crate) fn new(size: usize) -> Self {
+        Self {
+            items: VecDeque::with_capacity(size),
+            size_limit: size,
+            inspect_node: inspect::Node::default(),
+        }
+    }
+
+    pub(crate) fn push(&mut self, item: T) {
+        // If the queue is over capacity, remove one.
+        if self.items.len() == self.size_limit {
+            let _ = self.items.pop_front();
+        }
+        self.items.push_back(item);
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    // Required by clippy as a counterpart to len().
+    #[allow(dead_code)]
+    pub(crate) fn is_empty(&self) -> bool {
+        self.items.len() == 0
+    }
+}
+
+impl<T> Inspect for &mut InspectQueue<T>
+where
+    for<'a> &'a mut T: Inspect,
+{
+    fn iattach(self, parent: &Node, name: impl AsRef<str>) -> Result<(), AttachError> {
+        let items = &mut self.items;
+        self.inspect_node = parent.create_child(name.as_ref());
+        for inspect_info in items.iter_mut() {
+            let _ = inspect_info.iattach(&self.inspect_node, inspect::unique_name(""));
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::inspect::utils::inspect_queue::InspectQueue;
+    use fuchsia_inspect::{self as inspect, assert_data_tree, Node};
+    use fuchsia_inspect_derive::{IValue, Inspect, WithInspect};
+
+    #[derive(Inspect)]
+    struct TestInspectWrapper {
+        inspect_node: Node,
+        queue: InspectQueue<TestInspectItem>,
+    }
+
+    impl TestInspectWrapper {
+        fn new(queue: InspectQueue<TestInspectItem>) -> Self {
+            Self { inspect_node: Node::default(), queue: queue }
+        }
+    }
+
+    #[derive(Inspect)]
+    struct TestInspectItem {
+        inspect_node: Node,
+        id: IValue<u64>,
+    }
+
+    impl TestInspectItem {
+        fn new(id: u64) -> Self {
+            Self { inspect_node: Node::default(), id: id.into() }
+        }
+    }
+
+    // Test that a queue with less items than its capacity has the correct
+    // inspect and queue content.
+    #[test]
+    fn test_queue_under_capacity() {
+        let inspector = inspect::Inspector::new();
+        let mut inspect_queue = InspectQueue::<TestInspectItem>::new(3);
+        let test_val_1 = TestInspectItem::new(6);
+        let test_val_2 = TestInspectItem::new(5);
+        inspect_queue.push(test_val_1);
+        inspect_queue.push(test_val_2);
+        let _wrapper = TestInspectWrapper::new(inspect_queue)
+            .with_inspect(inspector.root(), "inspect_wrapper")
+            .expect("failed to create TestInspectWrapper inspect node");
+        assert_data_tree!(inspector, root: {
+            inspect_wrapper: {
+                queue: {
+                    "0": {
+                        "id": 6 as u64,
+                    },
+                    "1": {
+                        "id": 5 as u64,
+                    },
+                },
+            }
+        });
+        assert_eq!(_wrapper.queue.len(), 2);
+    }
+
+    // Test that a queue with more items than its capacity has the correct
+    // inspect and queue content.
+    #[test]
+    fn test_queue_over_capacity() {
+        let inspector = inspect::Inspector::new();
+        let mut inspect_queue = InspectQueue::<TestInspectItem>::new(2);
+        let test_val_1 = TestInspectItem::new(6);
+        let test_val_2 = TestInspectItem::new(5);
+        let test_val_3 = TestInspectItem::new(4);
+        inspect_queue.push(test_val_1);
+        inspect_queue.push(test_val_2);
+        inspect_queue.push(test_val_3);
+        let _wrapper = TestInspectWrapper::new(inspect_queue)
+            .with_inspect(inspector.root(), "inspect_wrapper")
+            .expect("failed to create TestInspectWrapper inspect node");
+        assert_data_tree!(inspector, root: {
+            inspect_wrapper: {
+                queue: {
+                    "0": {
+                        "id": 5 as u64,
+                    },
+                    "1": {
+                        "id": 4 as u64,
+                    },
+                },
+            }
+        });
+        assert_eq!(_wrapper.queue.len(), 2);
+    }
+}
