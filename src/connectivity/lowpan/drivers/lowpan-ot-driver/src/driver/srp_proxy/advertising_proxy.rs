caed2bdd10d3742e066feeda5af0bbdfa5c79f27
Robert Quattlebaum <rquattle@google.com>
2022-04-04 22:41:35 +0000

    [lowpan-ot-driver] SRP Advertising Proxy mDNS Integration.
    Bug: 80592
    Test: Manual verification
    Change-Id: Ifca719a7c9ee5fc5fc6490894a1aa907a8da6924
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/651142
    Reviewed-by: Jiaming (Charlie) Wang <jiamingw@google.com>
    Fuchsia-Auto-Submit: Robert Quattlebaum <rquattle@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/connectivity/lowpan/drivers/lowpan-ot-driver/src/driver/srp_proxy/advertising_proxy.rs
+++ src/connectivity/lowpan/drivers/lowpan-ot-driver/src/driver/srp_proxy/advertising_proxy.rs
@@ -4,6 +4,10 @@
 
 use crate::driver::srp_proxy::LOCAL_DOMAIN;
 use crate::prelude::*;
+use fidl::endpoints::create_endpoints;
+use fidl_fuchsia_net_mdns::*;
+use fuchsia_async::Task;
+use fuchsia_component::client::connect_to_protocol;
 use parking_lot::Mutex;
 use std::collections::HashMap;
 use std::ffi::{CStr, CString};
@@ -25,12 +29,15 @@ impl Drop for AdvertisingProxy {
 
 #[derive(Debug)]
 struct AdvertisingProxyInner {
+    srp_domain: String,
     hosts: HashMap<CString, AdvertisingProxyHost>,
+    mdns_proxy_host_publisher: ProxyHostPublisherProxy,
 }
 
 #[derive(Debug)]
 pub struct AdvertisingProxyHost {
     services: HashMap<CString, AdvertisingProxyService>,
+    service_publisher: ServiceInstancePublisherProxy,
 }
 
 #[derive(Debug)]
@@ -39,6 +46,9 @@ pub struct AdvertisingProxyService {
     port: u16,
     priority: u16,
     weight: u16,
+
+    #[allow(dead_code)]
+    task: Task<Result>,
 }
 
 impl AdvertisingProxyService {
@@ -51,9 +61,20 @@ impl AdvertisingProxyService {
     }
 }
 
+// Splits the TXT record into individual values.
+fn split_txt(txt: &[u8]) -> Result<Vec<Vec<u8>>, anyhow::Error> {
+    Ok(ot::dnssd_split_txt(std::str::from_utf8(txt)?)
+        .map(|x| x.as_bytes().to_vec())
+        .collect::<Vec<_>>())
+}
+
 impl AdvertisingProxy {
     pub fn new(instance: &ot::Instance) -> Result<AdvertisingProxy, anyhow::Error> {
-        let inner = Arc::new(Mutex::new(AdvertisingProxyInner { hosts: Default::default() }));
+        let inner = Arc::new(Mutex::new(AdvertisingProxyInner {
+            srp_domain: instance.srp_server_get_domain().to_str()?.to_string(),
+            hosts: Default::default(),
+            mdns_proxy_host_publisher: connect_to_protocol::<ProxyHostPublisherMarker>()?,
+        }));
         let ret = AdvertisingProxy { inner: inner.clone() };
 
         ret.inner.lock().publish_srp_all(instance)?;
@@ -76,6 +97,8 @@ impl AdvertisingProxy {
                 );
             },
         ));
+
+        info!("AdvertisingProxy Started");
 
         Ok(ret)
     }
@@ -104,32 +127,96 @@ impl AdvertisingProxyInner {
     ) -> Result<(), anyhow::Error> {
         if srp_host.is_deleted() {
             // Delete the host.
-            info!("No longer advertising {:?} on {:?}", srp_host.full_name_cstr(), LOCAL_DOMAIN);
+            info!(
+                "No longer advertising host {:?} on {:?}",
+                srp_host.full_name_cstr(),
+                LOCAL_DOMAIN
+            );
 
             self.hosts.remove(srp_host.full_name_cstr());
             return Ok(());
         }
 
-        let host: &mut AdvertisingProxyHost =
-            if let Some(host) = self.hosts.get_mut(srp_host.full_name_cstr()) {
-                // Use the existing host.
-                info!(
-                    "Updating advertisement of {:?} on {:?}",
-                    srp_host.full_name_cstr(),
-                    LOCAL_DOMAIN
-                );
+        let host: &mut AdvertisingProxyHost = if let Some(host) =
+            self.hosts.get_mut(srp_host.full_name_cstr())
+        {
+            // Use the existing host.
+            info!(
+                "Updating advertisement of {:?} on {:?}",
+                srp_host.full_name_cstr(),
+                LOCAL_DOMAIN
+            );
 
-                host
-            } else {
-                // Add the host.
-                info!("Advertising {:?} on {:?}", srp_host.full_name_cstr(), LOCAL_DOMAIN);
+            host
+        } else {
+            // Add the host.
+            let local_name = srp_host
+                .full_name_cstr()
+                .as_ref()
+                .to_str()?
+                .trim_end_matches(&self.srp_domain)
+                .trim_end_matches('.');
 
-                self.hosts.insert(
-                    srp_host.full_name_cstr().to_owned(),
-                    AdvertisingProxyHost { services: Default::default() },
-                );
-                self.hosts.get_mut(srp_host.full_name_cstr()).unwrap()
-            };
+            info!(
+                "Advertising host {:?} on {:?} as {:?}",
+                srp_host.full_name_cstr(),
+                LOCAL_DOMAIN,
+                local_name
+            );
+
+            let (client, server) = create_endpoints::<ServiceInstancePublisherMarker>()
+                .context("Failed to create FIDL endpoints")?;
+
+            let mut addrs = srp_host
+                .addresses()
+                .iter()
+                .filter_map(|x| {
+                    if !net_types::ip::Ipv6Addr::from_bytes(x.octets()).is_unicast_link_local() {
+                        Some(fidl_fuchsia_net::IpAddress::Ipv6(fidl_fuchsia_net::Ipv6Address {
+                            addr: x.octets(),
+                        }))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            let publish_proxy_host_future = self
+                .mdns_proxy_host_publisher
+                .publish_proxy_host(
+                    &local_name,
+                    &mut addrs.iter_mut(),
+                    ProxyHostPublicationOptions {
+                        media: Some(Media::all()),
+                        ip_versions: Some(IpVersions::V6),
+                        perform_probe: Some(true),
+                        ..ProxyHostPublicationOptions::EMPTY
+                    },
+                    server,
+                )
+                .map(|x| match x {
+                    Ok(Ok(())) => {
+                        debug!("publish_proxy_host: success");
+                    }
+                    Ok(Err(err)) => {
+                        error!("publish_proxy_host: {:?}", err);
+                    }
+                    Err(err) => {
+                        error!("publish_proxy_host: {:?}", err);
+                    }
+                });
+
+            fuchsia_async::Task::spawn(publish_proxy_host_future).detach();
+
+            self.hosts.insert(
+                srp_host.full_name_cstr().to_owned(),
+                AdvertisingProxyHost {
+                    services: Default::default(),
+                    service_publisher: client.into_proxy()?,
+                },
+            );
+            self.hosts.get_mut(srp_host.full_name_cstr()).unwrap()
+        };
 
         let services = &mut host.services;
 
@@ -138,10 +225,25 @@ impl AdvertisingProxyInner {
             None,
             None,
         ) {
+            // The service name as a Rust string slice from the SRP service.
+            let service_name = srp_service.service_name_cstr().as_ref().to_str()?;
+
+            // The service name without the domain, with a trailing period, like "_trel.udp_.".
+            let local_service_name = service_name.trim_end_matches(&self.srp_domain);
+
+            // The instance name without the service name or domain,
+            // without any trailing period, like "My-Service".
+            let local_instance_name = srp_service
+                .full_name_cstr()
+                .as_ref()
+                .to_str()?
+                .trim_end_matches(service_name)
+                .trim_end_matches('.');
+
             if srp_service.is_deleted() {
                 // Delete the service.
                 info!(
-                    "No longer advertising {:?} on {:?}",
+                    "No longer advertising service {:?} on {:?}",
                     srp_service.full_name_cstr(),
                     LOCAL_DOMAIN
                 );
@@ -159,9 +261,81 @@ impl AdvertisingProxyInner {
                 continue;
             }
 
-            info!("Advertising {:?} on {:?}", srp_service.full_name_cstr(), LOCAL_DOMAIN);
+            info!(
+                "Advertising service {:?} on {:?} as {:?}",
+                srp_service.full_name_cstr(),
+                LOCAL_DOMAIN,
+                local_instance_name
+            );
 
             // (Re-)Add the service.
+
+            let (client, server) = create_endpoints::<ServiceInstancePublicationResponder_Marker>()
+                .context("Failed to create FIDL endpoints")?;
+
+            let publish_init_future = host
+                .service_publisher
+                .publish_service_instance(
+                    &local_service_name,
+                    &local_instance_name,
+                    ServiceInstancePublicationOptions {
+                        media: Some(Media::all()),
+                        ip_versions: Some(IpVersions::V6),
+                        perform_probe: Some(true),
+                        ..ServiceInstancePublicationOptions::EMPTY
+                    },
+                    client,
+                )
+                .map(|x| match x {
+                    Ok(Ok(())) => {
+                        debug!("publish_service_instance: success");
+                        Ok(())
+                    }
+                    Ok(Err(err)) => {
+                        error!("publish_service_instance: {:?}", err);
+                        Err(format_err!("publish_service_instance: {:?}", err))
+                    }
+                    Err(err) => {
+                        error!("publish_service_instance: {:?}", err);
+                        Err(format_err!("publish_service_instance: {:?}", err))
+                    }
+                });
+
+            // Make copies of all of the pertinent data for the publication responder.
+            let txt = match split_txt(srp_service.txt_data()) {
+                Ok(x) => x,
+                Err(err) => {
+                    error!("publish_service_instance: split_txt failed: {:?}", err);
+                    continue;
+                }
+            };
+            let port = srp_service.port();
+            let weight = srp_service.weight();
+            let priority = srp_service.priority();
+
+            let publish_responder_future =
+                server.into_stream().unwrap().map_err(Into::into).try_for_each(
+                    move |ServiceInstancePublicationResponder_Request::OnPublication {
+                              responder,
+                              ..
+                          }| {
+                        let txt = txt.clone();
+                        async move {
+                            responder
+                                .send(&mut Ok(ServiceInstancePublication {
+                                    port: Some(port),
+                                    text: Some(txt),
+                                    srv_priority: Some(priority),
+                                    srv_weight: Some(weight),
+                                    ..ServiceInstancePublication::EMPTY
+                                }))
+                                .map_err(Into::into)
+                        }
+                    },
+                );
+
+            let future = futures::future::try_join(publish_init_future, publish_responder_future)
+                .map_ok(|_| ());
 
             services.insert(
                 srp_service.full_name_cstr().to_owned(),
@@ -170,6 +344,7 @@ impl AdvertisingProxyInner {
                     port: srp_service.port(),
                     priority: srp_service.priority(),
                     weight: srp_service.weight(),
+                    task: fuchsia_async::Task::spawn(future),
                 },
             );
         }
