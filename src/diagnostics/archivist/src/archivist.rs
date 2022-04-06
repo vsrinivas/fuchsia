cad5fafdf8e83dfea1ee79243f361d0ecaf03118
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2022-04-06 17:53:09 +0000

    Reland "[archivist] Use structured config"
    This is a reland of commit e808049662e3f25dc523d0563be301e12548adf6
    
    Previous attempt raced in CQ with https://fuchsia-review.googlesource.com/c/fuchsia/+/662308/
    
    Original change's description:
    > [archivist] Use structured config
    >
    > Merges archivist CLI, archivist_config.json and service config
    > files into the same.
    >
    > V1 components continue passing flags for a few configurations.
    >
    > Fixed: 96483
    > Tested: existing tests
    >
    > Change-Id: I8b4ff152ca85c9028951b57fd0ae99ce4a407190
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/664351
    > Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    > Reviewed-by: Adam Perry <adamperry@google.com>
    > Reviewed-by: Christopher Johnson <crjohns@google.com>
    > Reviewed-by: Shai Barack <shayba@google.com>
    > Commit-Queue: Miguel Flores <miguelfrde@google.com>
    
    Change-Id: I908ef0f27d2e98a65b176dac340892bc0a1c1412
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/665786
    Reviewed-by: Adam Perry <adamperry@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/diagnostics/archivist/src/archivist.rs
+++ src/diagnostics/archivist/src/archivist.rs
@@ -4,7 +4,7 @@
 
 use {
     crate::{
-        component_lifecycle, configs, diagnostics,
+        component_lifecycle, diagnostics,
         error::Error,
         events::{
             router::{ConsumerConfig, EventConsumer, EventRouter, ProducerConfig, ProducerType},
@@ -18,6 +18,7 @@ use {
         pipeline::Pipeline,
         repository::DataRepo,
     },
+    archivist_config::Config,
     async_trait::async_trait,
     fidl_fuchsia_io as fio, fidl_fuchsia_logger as flogger,
     fidl_fuchsia_sys2::EventSourceMarker,
@@ -35,7 +36,7 @@ use {
         prelude::*,
     },
     parking_lot::RwLock,
-    std::sync::Arc,
+    std::{path::Path, sync::Arc},
     tracing::{debug, error, warn},
 };
 
@@ -92,7 +93,7 @@ impl Archivist {
     /// Creates new instance, sets up inspect and adds 'archive' directory to output folder.
     /// Also installs `fuchsia.diagnostics.Archive` service.
     /// Call `install_log_services`
-    pub fn new(archivist_configuration: configs::Config) -> Result<Self, Error> {
+    pub fn new(archivist_configuration: &Config) -> Result<Self, Error> {
         let mut fs = ServiceFs::new();
         diagnostics::serve(&mut fs)?;
 
@@ -100,11 +101,11 @@ impl Archivist {
         let (listen_sender, listen_receiver) = mpsc::unbounded();
 
         let logs_budget =
-            BudgetManager::new(archivist_configuration.logs.max_cached_original_bytes);
+            BudgetManager::new(archivist_configuration.logs_max_cached_original_bytes as usize);
         let diagnostics_repo = DataRepo::new(&logs_budget, component::inspector().root());
 
         let pipelines_node = component::inspector().root().create_child("pipelines");
-        let pipelines_path = archivist_configuration.pipelines_path;
+        let pipelines_path = Path::new(&archivist_configuration.pipelines_path);
         let pipelines = vec![
             Pipeline::feedback(diagnostics_repo.clone(), &pipelines_path, &pipelines_node),
             Pipeline::legacy_metrics(diagnostics_repo.clone(), &pipelines_path, &pipelines_node),
@@ -552,15 +553,22 @@ mod tests {
     use futures::channel::oneshot;
 
     fn init_archivist() -> Archivist {
-        let config = configs::Config {
+        let config = Config {
+            consume_own_logs: false,
+            enable_component_event_provider: false,
+            enable_klog: false,
+            enable_event_source: false,
+            enable_log_connector: false,
+            install_controller: false,
+            listen_to_lifecycle: false,
+            log_to_debuglog: false,
+            logs_max_cached_original_bytes: LEGACY_DEFAULT_MAXIMUM_CACHED_LOGS_BYTES as u64,
             num_threads: 1,
-            logs: configs::LogsConfig {
-                max_cached_original_bytes: LEGACY_DEFAULT_MAXIMUM_CACHED_LOGS_BYTES,
-            },
             pipelines_path: DEFAULT_PIPELINES_PATH.into(),
+            bind_services: vec![],
         };
 
-        let mut archivist = Archivist::new(config).unwrap();
+        let mut archivist = Archivist::new(&config).unwrap();
         // Install a fake producer that allows all incoming events. This allows skipping
         // validation for the purposes of the tests here.
         let mut fake_producer = FakeProducer {};
