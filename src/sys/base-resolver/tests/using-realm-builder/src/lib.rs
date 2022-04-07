37b4bd87f191810b21a9ee69795e7889280ec96a
Ben Keller <galbanum@google.com>
2022-04-07 18:09:28 +0000

    [base-resolver] Stop waiting for minfs before resolving pkg-cache
    Post fxrev.dev/659354 all non-shell clients of minfs are routed
    the delayed version.
    
    Fixed: 92415
    Change-Id: I24812d3fa03b8f41252209ce0ca60029afc4d2b0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/666902
    Reviewed-by: Sen Jiang <senj@google.com>
    Reviewed-by: Allison Pearce <ampearce@google.com>
    Reviewed-by: Gary Bressler <geb@google.com>
    Commit-Queue: Ben Keller <galbanum@google.com>

--- src/sys/base-resolver/tests/using-realm-builder/src/lib.rs
+++ src/sys/base-resolver/tests/using-realm-builder/src/lib.rs
@@ -4,22 +4,16 @@
 
 use {
     assert_matches::assert_matches,
-    async_trait::async_trait,
     blobfs_ramdisk::BlobfsRamdisk,
     fidl_fuchsia_io as fio,
     fidl_fuchsia_sys2::*,
     fuchsia_component_test::{Capability, ChildOptions, RealmBuilder, RealmInstance, Ref, Route},
-    fuchsia_zircon as zx,
     futures::{
-        channel::oneshot,
-        future::{BoxFuture, FutureExt as _, Shared},
+        future::{BoxFuture, FutureExt as _},
         stream::TryStreamExt as _,
     },
     std::sync::Arc,
-    vfs::{
-        directory::{connection::io1::DerivedConnection as _, entry::DirectoryEntry},
-        execution_scope::ExecutionScope,
-    },
+    vfs::{directory::entry::DirectoryEntry, execution_scope::ExecutionScope},
 };
 
 static BASE_RESOLVER_URL: &str =
@@ -38,7 +32,6 @@ trait BootArgumentsStreamHandler: Send + Sync {
 struct TestEnvBuilder {
     blobfs: Arc<dyn DirectoryEntry>,
     boot_args: Arc<dyn BootArgumentsStreamHandler>,
-    minfs: Option<Arc<dyn DirectoryEntry>>,
 }
 
 impl TestEnvBuilder {
@@ -46,19 +39,12 @@ impl TestEnvBuilder {
         blobfs: Arc<dyn DirectoryEntry>,
         boot_args: Arc<dyn BootArgumentsStreamHandler>,
     ) -> Self {
-        Self { blobfs, boot_args, minfs: None }
-    }
-
-    fn minfs(mut self, minfs: Arc<dyn DirectoryEntry>) -> Self {
-        assert!(self.minfs.is_none());
-        self.minfs = Some(minfs);
-        self
+        Self { blobfs, boot_args }
     }
 
     async fn build(self) -> TestEnv {
         let blobfs = self.blobfs;
         let boot_args = self.boot_args;
-        let minfs = self.minfs.unwrap_or_else(|| vfs::pseudo_directory! {});
 
         let builder = RealmBuilder::new().await.unwrap();
 
@@ -73,10 +59,8 @@ impl TestEnvBuilder {
                 move |handles| {
                     let blobfs_clone = blobfs.clone();
                     let boot_args_clone = boot_args.clone();
-                    let minfs_clone = minfs.clone();
                     let out_dir = vfs::pseudo_directory! {
                         "blob" => blobfs_clone,
-                        "minfs" => minfs_clone,
                         "svc" => vfs::pseudo_directory! {
                             "fuchsia.boot.Arguments" =>
                                 vfs::service::host(move |stream|
@@ -111,9 +95,6 @@ impl TestEnvBuilder {
                         Capability::directory("pkgfs-packages")
                             .path("/pkgfs-packages")
                             .rights(fio::RX_STAR_DIR),
-                    )
-                    .capability(
-                        Capability::directory("minfs").path("/minfs").rights(fio::R_STAR_DIR),
                     )
                     .capability(Capability::protocol_by_name("fuchsia.boot.Arguments"))
                     .from(&local_mocks)
@@ -226,182 +207,19 @@ async fn create_blobfs_and_boot_args_from_this_package() -> (BlobfsRamdisk, Boot
     (blobfs, BootArgsFixedHash::new(*system_image.meta_far_merkle_root()))
 }
 
-// Signals when Self is opened, then blocks until signalled.
-#[derive(Clone)]
-struct MinfsSyncOnOpenSelf {
-    sync_point: SyncPoint,
-}
-
-impl MinfsSyncOnOpenSelf {
-    fn new() -> (Self, SyncPoint) {
-        let sync_point = SyncPoint::new();
-        (Self { sync_point: sync_point.clone() }, sync_point)
-    }
-
-    fn directory_entry(&self) -> Arc<dyn DirectoryEntry> {
-        Arc::new(self.clone())
-    }
-}
-
-impl vfs::directory::entry::DirectoryEntry for MinfsSyncOnOpenSelf {
-    fn open(
-        self: Arc<Self>,
-        scope: ExecutionScope,
-        flags: fio::OpenFlags,
-        _mode: u32,
-        path: vfs::path::Path,
-        server_end: fidl::endpoints::ServerEnd<fio::NodeMarker>,
-    ) {
-        if path.is_empty() {
-            let sync_point = self.sync_point.clone();
-            let () = scope.clone().spawn(async move {
-                let () = sync_point.signal_reached_then_wait_until_can_continue().await;
-                let () =
-                vfs::directory::immutable::connection::io1::ImmutableConnection::create_connection(
-                    scope, self, flags, server_end,
-                );
-            });
-            return;
-        }
-        let () = vfs::common::send_on_open_with_error(flags, server_end, zx::Status::NOT_FOUND);
-    }
-
-    fn entry_info(&self) -> vfs::directory::entry::EntryInfo {
-        vfs::directory::entry::EntryInfo::new(fio::INO_UNKNOWN, fio::DirentType::Directory)
-    }
-}
-
-#[async_trait]
-impl vfs::directory::entry_container::Directory for MinfsSyncOnOpenSelf {
-    async fn read_dirents<'a>(
-        &'a self,
-        pos: &'a vfs::directory::traversal_position::TraversalPosition,
-        sink: Box<(dyn vfs::directory::dirents_sink::Sink + 'static)>,
-    ) -> Result<
-        (
-            vfs::directory::traversal_position::TraversalPosition,
-            Box<(dyn vfs::directory::dirents_sink::Sealed + 'static)>,
-        ),
-        zx::Status,
-    > {
-        let sink = match pos {
-            vfs::directory::traversal_position::TraversalPosition::Start => {
-                match sink.append(
-                    &vfs::directory::entry::EntryInfo::new(0, fio::DirentType::Directory),
-                    ".",
-                ) {
-                    vfs::directory::dirents_sink::AppendResult::Ok(sink) => (sink),
-                    vfs::directory::dirents_sink::AppendResult::Sealed(_) => {
-                        panic!("BlockingMinfs has no contents, writing '.' should succeed")
-                    }
-                }
-            }
-            _ => panic!("BlockingMinfs has no contents, readdir shouldn't be paging"),
-        };
-        Ok((vfs::directory::traversal_position::TraversalPosition::End, sink.seal().into()))
-    }
-
-    fn register_watcher(
-        self: Arc<Self>,
-        _: ExecutionScope,
-        _: fio::WatchMask,
-        _: vfs::directory::entry_container::DirectoryWatcher,
-    ) -> Result<(), zx::Status> {
-        Err(zx::Status::NOT_SUPPORTED)
-    }
-
-    fn unregister_watcher(self: Arc<Self>, _: usize) {}
-
-    async fn get_attrs(&self) -> Result<fio::NodeAttributes, zx::Status> {
-        Ok(fio::NodeAttributes {
-            mode: fio::MODE_TYPE_DIRECTORY,
-            id: 1,
-            content_size: 0,
-            storage_size: 0,
-            link_count: 1,
-            creation_time: 0,
-            modification_time: 0,
-        })
-    }
-
-    fn close(&self) -> Result<(), zx::Status> {
-        Ok(())
-    }
-}
-
-// Allows synchronizing two async processes, P0 and P1, around the first time event E occurs in P1:
-//   * P0 performs `sync_point.wait_until_reached().await` to block until E occurs
-//   * P1 performs `sync_point.signal_reached_then_wait_until_can_continue().await` to:
-//       1. signal that E has occurred
-//       2. block until P0 is ready
-//   * P0 performs `sync_point.signal_can_continue()` to signal that it is aware E has occurred and
-//     P1 can continue
-#[derive(Clone)]
-struct SyncPoint {
-    reached: Shared<oneshot::Receiver<()>>,
-    reached_signaller: Arc<futures::lock::Mutex<Option<oneshot::Sender<()>>>>,
-    can_continue: Shared<oneshot::Receiver<()>>,
-    can_continue_signaller: Arc<futures::lock::Mutex<Option<oneshot::Sender<()>>>>,
-}
-
-impl SyncPoint {
-    fn new() -> Self {
-        let (reached_signaller, reached) = oneshot::channel();
-        let reached_signaller = Arc::new(futures::lock::Mutex::new(Some(reached_signaller)));
-        let reached = reached.shared();
-
-        let (can_continue_signaller, can_continue) = oneshot::channel();
-        let can_continue_signaller =
-            Arc::new(futures::lock::Mutex::new(Some(can_continue_signaller)));
-        let can_continue = can_continue.shared();
-
-        Self { reached, reached_signaller, can_continue, can_continue_signaller }
-    }
-
-    fn wait_until_reached(&self) -> impl futures::Future<Output = ()> {
-        self.reached.clone().map(|res| res.expect("never cancelled"))
-    }
-
-    async fn signal_reached_then_wait_until_can_continue(&self) {
-        if let Some(sender) = self.reached_signaller.lock().await.take() {
-            sender.send(()).unwrap()
-        }
-        let () = self.can_continue.clone().await.unwrap();
-    }
-
-    async fn signal_can_continue(&self) {
-        if let Some(sender) = self.can_continue_signaller.lock().await.take() {
-            sender.send(()).unwrap()
-        }
-    }
-}
-
 #[fuchsia::test]
-async fn pkg_cache_resolver_waits_for_minfs() {
+async fn pkg_cache_resolver_succeeds() {
     let (blobfs, boot_args) = create_blobfs_and_boot_args_from_this_package().await;
-    let (minfs, minfs_sync_point) = MinfsSyncOnOpenSelf::new();
 
     let env = TestEnvBuilder::new(
         vfs::remote::remote_dir(blobfs.root_dir_proxy().unwrap()),
         Arc::new(boot_args),
     )
-    .minfs(minfs.directory_entry())
     .build()
     .await;
 
-    let mut minfs_opened = minfs_sync_point.wait_until_reached().fuse();
-    let mut resolve = env.pkg_cache_resolver().resolve(PKG_CACHE_COMPONENT_URL).fuse();
-    let () = futures::select! {
-        () = minfs_opened => (),
-        _ = resolve => panic!("resolve should not succeed before minfs open attempt")
-    };
-
-    let mut timeout = fuchsia_async::Timer::new(std::time::Duration::from_millis(100)).fuse();
-    let () = futures::select! {
-        () = timeout => (),
-        _ = resolve => panic!("resolve should not succeed before minfs unblocked")
-    };
-
-    let () = minfs_sync_point.signal_can_continue().await;
-    assert_matches!(resolve.await.unwrap(), Ok(Component { .. }));
+    assert_matches!(
+        env.pkg_cache_resolver().resolve(PKG_CACHE_COMPONENT_URL).await.unwrap(),
+        Ok(Component { .. })
+    );
 }
