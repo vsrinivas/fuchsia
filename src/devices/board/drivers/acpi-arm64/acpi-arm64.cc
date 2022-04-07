68ee47c94e6ba8f095388fd4839fbe8c5e572964
Simon Shields <simonshields@google.com>
2022-04-07 18:31:35 +1000

    [acpi][arm64] Initial implementation of the ARM64 ACPI Board driver.
    need to clean this up and make stuff that's common with x86 live in the
    library.
    
    Change-Id: I775c546f6a8d730445cc41652151f8d26fe83aa6

--- src/devices/board/drivers/acpi-arm64/acpi-arm64.cc
+++ src/devices/board/drivers/acpi-arm64/acpi-arm64.cc
@@ -6,13 +6,63 @@
 
 #include <lib/ddk/debug.h>
 
+#include "lib/ddk/driver.h"
+#include "lib/fit/defer.h"
 #include "src/devices/board/drivers/acpi-arm64/acpi-arm64-bind.h"
+#include "src/devices/board/lib/acpi/acpi-impl.h"
+#include "src/devices/board/lib/acpi/pci.h"
+
+zx_handle_t root_resource_handle;
+
+zx_status_t pci_init(zx_device_t *parent, ACPI_HANDLE object, ACPI_DEVICE_INFO *info,
+                     acpi::Manager *acpi, std::vector<pci_bdf_t> acpi_bdfs) {
+  zxlogf(ERROR, "Hey look pci_init");
+  return ZX_OK;
+}
 
 namespace acpi_arm64 {
 
-zx_status_t AcpiArm64::Create(void* ctx, zx_device_t* parent) {
+namespace {}  // namespace
+
+zx_status_t AcpiArm64::Create(void *ctx, zx_device_t *parent) {
   zxlogf(INFO, "Hello world!");
-  return ZX_ERR_NOT_SUPPORTED;
+  auto device = std::make_unique<AcpiArm64>(parent);
+
+  zx_status_t status =
+      device->DdkAdd(ddk::DeviceAddArgs("acpi").set_flags(DEVICE_ADD_NON_BINDABLE));
+  if (status == ZX_OK) {
+    // The DDK now owns the device.
+    __UNUSED auto unused = device.release();
+  }
+  return status;
+}
+
+void AcpiArm64::DdkInit(ddk::InitTxn txn) {
+  manager_.emplace(&acpi_, zxdev_);
+
+  // TODO(simonshields): add the spiel here.
+  root_resource_handle = get_root_resource();
+
+  init_thread_ = std::thread([txn = std::move(txn), this]() mutable {
+    auto status = manager_->acpi()->InitializeAcpi();
+    if (status.is_error()) {
+      txn.Reply(status.zx_status_value());
+    } else {
+      txn.Reply(ZX_OK);
+    }
+
+    auto result = manager_->DiscoverDevices();
+    if (result.is_error()) {
+      zxlogf(INFO, "discover devices failed");
+    }
+    result = manager_->ConfigureDiscoveredDevices();
+    if (result.is_error()) {
+      zxlogf(INFO, "configure failed");
+    }
+    result = manager_->PublishDevices(parent_);
+
+    return AE_OK;
+  });
 }
 
 static constexpr zx_driver_ops_t driver_ops{
--- src/devices/board/drivers/acpi-arm64/acpi-arm64.h
+++ src/devices/board/drivers/acpi-arm64/acpi-arm64.h
@@ -6,10 +6,13 @@
 
 #include <ddktl/device.h>
 
+#include "src/devices/board/lib/acpi/acpi-impl.h"
+#include "src/devices/board/lib/acpi/manager.h"
+
 namespace acpi_arm64 {
 
 class AcpiArm64;
-using DeviceType = ddk::Device<AcpiArm64>;
+using DeviceType = ddk::Device<AcpiArm64, ddk::Initializable>;
 
 class AcpiArm64 : public DeviceType {
  public:
@@ -17,7 +20,14 @@ class AcpiArm64 : public DeviceType {
 
   static zx_status_t Create(void* ctx, zx_device_t* parent);
 
+  void DdkInit(ddk::InitTxn txn);
   void DdkRelease() { delete this; }
+
+ private:
+  std::optional<acpi::Manager> manager_;
+  acpi::AcpiImpl acpi_;
+
+  std::thread init_thread_;
 };
 
 }  // namespace acpi_arm64
