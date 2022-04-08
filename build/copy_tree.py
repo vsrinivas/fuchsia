ece6767d8d6d873c5ed3cb258ea130754af1066d
Joseph Ryan <josephry@google.com>
2022-04-08 22:32:02 +0000

    [rust] Generate a depfile for the toolchain copier
    We can't rely on the .cipd_version file existing in all cases where we
    want to copy the toolchain, so we have to generate a depfile detailing
    all of the files we copy.
    
    This also fixes the hermeticity checker by adding dependency edges from
    the toolchain source files to the toolchain output files.
    
    Change-Id: Id02bff26894b7e363dec2b9cbef56ec1f6b230d0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/666851
    Reviewed-by: Tyler Mandry <tmandry@google.com>
    Commit-Queue: David Koloski <dkoloski@google.com>

--- build/copy_tree.py
+++ build/copy_tree.py
@@ -4,6 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 import sys
 import argparse
 import shutil
@@ -13,11 +14,13 @@ from pathlib import Path
 
 def main():
     params = argparse.ArgumentParser(
-        description="Copy all files in a directory tree and touch a stamp file")
+        description="Copy all files in a directory tree and touch a stamp file"
+    )
     params.add_argument("source", type=Path)
     params.add_argument("target", type=Path)
     params.add_argument("stamp", type=Path)
     params.add_argument("--ignore_patterns", nargs="+")
+    params.add_argument("--depfile", nargs=1, type=Path)
     args = params.parse_args()
 
     if args.target.is_file():
@@ -29,10 +32,33 @@ def main():
     if args.ignore_patterns:
         ignore = shutil.ignore_patterns(*args.ignore_patterns)
 
-    shutil.copytree(args.source, args.target, symlinks=True, ignore=ignore)
+    file_list = [args.source]
 
-    stamp = Path(str(args.stamp))
-    stamp.touch()
+    def ignore_wrapper(current, children):
+        nonlocal file_list, ignore
+        for f in children:
+            file_list.append(Path(current) / f)
+        if ignore:
+            return ignore(current, children)
+        return []
+
+    shutil.copytree(
+        args.source,
+        args.target,
+        symlinks=True,
+        ignore=ignore_wrapper,
+    )
+
+    if args.depfile:
+        os.makedirs(os.path.dirname(args.depfile[0]), exist_ok=True)
+        with open(args.depfile[0], "w") as f:
+            f.write(str(args.stamp) + ": ")
+            f.write(" ".join(map(str, file_list)))
+            f.write("\n")
+            for file in file_list:
+                print(os.path.join(args.target, os.path.relpath(file, start=args.source)) + ": " + str(file), file=f)
+
+    args.stamp.touch()
 
 
 if __name__ == "__main__":
