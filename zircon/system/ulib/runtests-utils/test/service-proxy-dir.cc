7ba539a4c53b0b179f16a11dbe2d28df75a523ee
Tamir Duberstein <tamird@google.com>
2022-04-11 18:16:04 +0000

    [runtests] Use typed LLCPP
    Bug: 69585
    Bug: 95833
    Change-Id: If5fe09e97545a98d359dec77f56f09014c18f8d6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/668086
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/runtests-utils/test/service-proxy-dir.cc
+++ zircon/system/ulib/runtests-utils/test/service-proxy-dir.cc
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fidl/fidl.test.echo/cpp/wire.h>
 #include <fidl/fuchsia.io/cpp/wire.h>
-#include <fidl/test/echo/c/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
 #include <lib/async/cpp/wait.h>
-#include <lib/fidl-utils/bind.h>
 #include <lib/zx/channel.h>
 #include <zircon/status.h>
 
@@ -23,24 +22,12 @@ namespace fio = fuchsia_io;
 
 namespace {
 
-class Echo {
+class Echo : public fidl::WireServer<fidl_test_echo::Echo> {
  public:
-  using EchoBinder = fidl::Binder<Echo>;
+  Echo(std::string response) : response_(std::move(response)) {}
 
-  Echo(std::string response) : response_(response) {}
-  virtual ~Echo() {}
-
-  virtual zx_status_t EchoString(const char*, size_t, fidl_txn_t* txn) {
-    return fidl_test_echo_EchoEchoString_reply(txn, response_.c_str(), response_.size());
-  }
-
-  virtual zx_status_t Bind(async_dispatcher_t* dispatcher, zx::channel channel) {
-    static constexpr fidl_test_echo_Echo_ops_t kOps = {
-        .EchoString = EchoBinder::BindMember<&Echo::EchoString>,
-    };
-
-    return EchoBinder::BindOps<fidl_test_echo_Echo_dispatch>(dispatcher, std::move(channel), this,
-                                                             &kOps);
+  virtual void EchoString(EchoStringRequestView request, EchoStringCompleter::Sync& completer) {
+    completer.Reply(fidl::StringView::FromExternal(response_));
   }
 
  private:
@@ -59,51 +46,57 @@ TEST(ServiceProxyDirTest, Simple) {
 
   Echo echo(kEchoString);
   auto dir = fbl::MakeRefCounted<fs::PseudoDir>();
-  dir->AddEntry(kEchoString, fbl::MakeRefCounted<fs::Service>(
-                                 [&echo, dispatcher = loop.dispatcher()](zx::channel request) {
-                                   return echo.Bind(dispatcher, std::move(request));
-                                 }));
+  dir->AddEntry(
+      kEchoString,
+      fbl::MakeRefCounted<fs::Service>(
+          [&echo, dispatcher = loop.dispatcher()](fidl::ServerEnd<fidl_test_echo::Echo> request) {
+            fidl::BindServer(dispatcher, std::move(request), &echo);
+            return ZX_OK;
+          }));
   ASSERT_OK(loop.StartThread());
 
-  zx::channel dir_client, dir_server;
-  ASSERT_OK(zx::channel::create(0, &dir_client, &dir_server));
+  zx::status endpoints = fidl::CreateEndpoints<fio::Directory>();
+  ASSERT_OK(endpoints.status_value());
 
-  ASSERT_OK(vfs->ServeDirectory(std::move(dir), std::move(dir_server)));
+  ASSERT_OK(vfs->ServeDirectory(std::move(dir), std::move(endpoints->server)));
   ASSERT_OK(loop.StartThread());
 
   Echo proxy_echo(kProxyEchoString);
-  auto proxy_dir = fbl::MakeRefCounted<runtests::ServiceProxyDir>(std::move(dir_client));
-  proxy_dir->AddEntry(kProxyEchoString,
-                      fbl::MakeRefCounted<fs::Service>(
-                          [&proxy_echo, dispatcher = loop.dispatcher()](zx::channel request) {
-                            return proxy_echo.Bind(dispatcher, std::move(request));
-                          }));
+  auto proxy_dir =
+      fbl::MakeRefCounted<runtests::ServiceProxyDir>(std::move(endpoints->client).TakeChannel());
+  proxy_dir->AddEntry(
+      kProxyEchoString,
+      fbl::MakeRefCounted<fs::Service>([&proxy_echo, dispatcher = loop.dispatcher()](
+                                           fidl::ServerEnd<fidl_test_echo::Echo> request) {
+        fidl::BindServer(dispatcher, std::move(request), &proxy_echo);
+        return ZX_OK;
+      }));
   ASSERT_OK(loop.StartThread());
 
-  zx::channel proxy_dir_client, proxy_dir_server;
-  ASSERT_OK(zx::channel::create(0, &proxy_dir_client, &proxy_dir_server));
+  zx::status proxy_endpoints = fidl::CreateEndpoints<fio::Directory>();
+  ASSERT_OK(proxy_endpoints.status_value());
 
-  ASSERT_OK(vfs->ServeDirectory(std::move(proxy_dir), std::move(proxy_dir_server)));
+  ASSERT_OK(vfs->ServeDirectory(std::move(proxy_dir), std::move(proxy_endpoints->server)));
   ASSERT_OK(loop.StartThread());
 
   // First check the service served directly by the proxy.
   {
-    zx::channel h1, h2;
-    ASSERT_OK(zx::channel::create(0, &h1, &h2));
+    zx::status endpoints = fidl::CreateEndpoints<fio::Node>();
+    ASSERT_OK(endpoints.status_value());
 
-    ASSERT_OK(fidl::WireCall<fio::Directory>(zx::unowned_channel(proxy_dir_client))
+    ASSERT_OK(fidl::WireCall(proxy_endpoints->client)
                   ->Open(fio::wire::OpenFlags::kRightReadable |
                              fio::wire::OpenFlags::kRightWritable | fio::wire::OpenFlags::kDescribe,
-                         0755, fidl::StringView(kProxyEchoString), std::move(h1))
+                         0755, fidl::StringView(kProxyEchoString), std::move(endpoints->server))
                   .status());
 
-    class EventHandler : public fidl::WireSyncEventHandler<fio::Directory> {
+    class EventHandler : public fidl::WireSyncEventHandler<fio::Node> {
      public:
       EventHandler() = default;
 
       zx_status_t status() const { return status_; }
 
-      void OnOpen(fidl::WireEvent<fio::Directory::OnOpen>* event) override { status_ = event->s; }
+      void OnOpen(fidl::WireEvent<fio::Node::OnOpen>* event) override { status_ = event->s; }
 
       zx_status_t Unknown() override { return ZX_ERR_NOT_SUPPORTED; }
 
@@ -112,36 +105,35 @@ TEST(ServiceProxyDirTest, Simple) {
     };
 
     EventHandler event_handler;
-    ASSERT_OK(event_handler.HandleOneEvent(zx::unowned_channel(h2)));
+    ASSERT_OK(event_handler.HandleOneEvent(endpoints->client));
     ASSERT_OK(event_handler.status());
 
-    char response_buffer[sizeof(kProxyEchoString)] = {};
-    size_t response_size;
-    ASSERT_OK(fidl_test_echo_EchoEchoString(h2.get(), kTestString, strlen(kTestString),
-                                            response_buffer, sizeof(response_buffer),
-                                            &response_size));
-    ASSERT_EQ(strlen(kProxyEchoString), response_size);
-    ASSERT_STREQ(kProxyEchoString, response_buffer);
+    const fidl::WireResult result = fidl::WireCall(fidl::UnownedClientEnd<fidl_test_echo::Echo>{
+                                                       endpoints->client.channel().borrow()})
+                                        ->EchoString(kTestString);
+    ASSERT_OK(result.status());
+    const fidl::WireResponse response = result.value();
+    ASSERT_EQ(response.response.get(), kProxyEchoString);
   }
 
   // Second check the service that's being proxied by the proxy.
   {
-    zx::channel h1, h2;
-    ASSERT_OK(zx::channel::create(0, &h1, &h2));
+    zx::status endpoints = fidl::CreateEndpoints<fio::Node>();
+    ASSERT_OK(endpoints.status_value());
 
-    ASSERT_OK(fidl::WireCall<fio::Directory>(zx::unowned_channel(proxy_dir_client))
+    ASSERT_OK(fidl::WireCall(proxy_endpoints->client)
                   ->Open(fio::wire::OpenFlags::kRightReadable |
                              fio::wire::OpenFlags::kRightWritable | fio::wire::OpenFlags::kDescribe,
-                         0755, fidl::StringView(kEchoString), std::move(h1))
+                         0755, fidl::StringView(kEchoString), std::move(endpoints->server))
                   .status());
 
-    class EventHandler : public fidl::WireSyncEventHandler<fio::Directory> {
+    class EventHandler : public fidl::WireSyncEventHandler<fio::Node> {
      public:
       EventHandler() = default;
 
       zx_status_t status() const { return status_; }
 
-      void OnOpen(fidl::WireEvent<fio::Directory::OnOpen>* event) override { status_ = event->s; }
+      void OnOpen(fidl::WireEvent<fio::Node::OnOpen>* event) override { status_ = event->s; }
 
       zx_status_t Unknown() override { return ZX_ERR_NOT_SUPPORTED; }
 
@@ -150,16 +142,15 @@ TEST(ServiceProxyDirTest, Simple) {
     };
 
     EventHandler event_handler;
-    ASSERT_OK(event_handler.HandleOneEvent(zx::unowned_channel(h2)));
+    ASSERT_OK(event_handler.HandleOneEvent(endpoints->client));
     ASSERT_OK(event_handler.status());
 
-    char response_buffer[sizeof(kEchoString)] = {};
-    size_t response_size;
-    ASSERT_OK(fidl_test_echo_EchoEchoString(h2.get(), kTestString, strlen(kTestString),
-                                            response_buffer, sizeof(response_buffer),
-                                            &response_size));
-    ASSERT_EQ(strlen(kEchoString), response_size);
-    ASSERT_STREQ(kEchoString, response_buffer);
+    const fidl::WireResult result = fidl::WireCall(fidl::UnownedClientEnd<fidl_test_echo::Echo>{
+                                                       endpoints->client.channel().borrow()})
+                                        ->EchoString(kTestString);
+    ASSERT_OK(result.status());
+    const fidl::WireResponse response = result.value();
+    ASSERT_EQ(response.response.get(), kEchoString);
   }
 
   loop.Shutdown();
