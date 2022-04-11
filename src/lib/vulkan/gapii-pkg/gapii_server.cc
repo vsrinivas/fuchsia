e9869df63ae7b5adb4d0f1df9b23132a3009c662
John Rosasco <rosasco@google.com>
2022-04-11 21:33:34 +0000

    [vulkan] Move to sdk dependencies.
    Change-Id: Id8dd0a1c82e7c0244c77faf1cb08f13a5c8a4913
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/668148
    Reviewed-by: John Bauman <jbauman@google.com>
    Commit-Queue: John Rosasco <rosasco@google.com>

--- src/lib/vulkan/gapii-pkg/gapii_server.cc
+++ src/lib/vulkan/gapii-pkg/gapii_server.cc
@@ -1,4 +1,4 @@
-// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,37 +6,34 @@
 #include <lib/async-loop/default.h>
 #include <lib/fdio/directory.h>
 #include <lib/fdio/io.h>
-#include <lib/svc/outgoing.h>
-#include <lib/syslog/cpp/macros.h>
-
-#include "src/lib/fxl/command_line.h"
-#include "src/lib/storage/vfs/cpp/remote_dir.h"
+#include <lib/vfs/cpp/pseudo_dir.h>
+#include <lib/vfs/cpp/remote_dir.h>
+#include <zircon/processargs.h>
 
 // Serve /pkg as /pkg in the outgoing directory.
 int main(int argc, const char* const* argv) {
   async::Loop loop(&kAsyncLoopConfigAttachToCurrentThread);
-  auto endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
-  if (!endpoints.is_ok()) {
-    fprintf(stderr, "Couldn't create endpoints, %d\n", endpoints.error_value());
-    return -1;
-  }
-  zx_status_t status;
-  status = fdio_open("/pkg",
-                     static_cast<uint32_t>(fuchsia_io::wire::OpenFlags::kRightReadable |
-                                           fuchsia_io::wire::OpenFlags::kRightExecutable),
-                     endpoints->server.TakeChannel().release());
+  zx::channel client_end, server_end;
+  zx_status_t status = zx::channel::create(0, &client_end, &server_end);
   if (status != ZX_OK) {
-    fprintf(stderr, "Failed to open package");
+    fprintf(stderr, "Couldn't create channel, %d\n", status);
+    return -1;
+  }
+  status = fdio_open("/pkg",
+                     static_cast<uint32_t>(fuchsia::io::OpenFlags::RIGHT_READABLE |
+                                           fuchsia::io::OpenFlags::RIGHT_EXECUTABLE),
+                     server_end.release());
+  if (status != ZX_OK) {
+    fprintf(stderr, "Failed to open /pkg");
     return -1;
   }
 
-  // sys::OutgoingDirectory doesn't support executable rights, so use svc::Outgoing.
-  svc::Outgoing outgoing(loop.dispatcher());
+  vfs::PseudoDir root_dir;
+  root_dir.AddEntry("pkg", std::make_unique<vfs::RemoteDir>(std::move(client_end)));
 
-  auto pkg_outgoing_dir = fbl::MakeRefCounted<fs::RemoteDir>(std::move(endpoints->client));
-  outgoing.root_dir()->AddEntry("pkg", pkg_outgoing_dir);
-
-  status = outgoing.ServeFromStartupInfo();
+  status = root_dir.Serve(
+      fuchsia::io::OpenFlags::RIGHT_READABLE | fuchsia::io::OpenFlags::RIGHT_EXECUTABLE,
+      zx::channel(zx_take_startup_handle(PA_DIRECTORY_REQUEST)));
 
   if (status != ZX_OK) {
     fprintf(stderr, "Failed to serve outgoing.");
