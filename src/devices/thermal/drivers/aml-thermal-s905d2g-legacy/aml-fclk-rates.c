24df3132f4c1b6595b6fb2bd60908566fcdce208
Daniel Thornburgh <dthorn@google.com>
2022-04-12 20:09:41 +0000

    [Clang] Ensure C functions are prototyped.
    ttps://reviews.llvm.org/D122895 changed the semantics of
    -Wstrict-prototypes, which causes -Werror build failures in the core
    builders. To fix the warning, this series of changes ensures that C
    functions have prototypes.
    
    Bug: 97746
    Change-Id: I6b9abecdd795bb8806848e6075cf8977c9af7a38
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/668689
    Reviewed-by: Braden Kell <bradenkell@google.com>
    Commit-Queue: Daniel Thornburgh <dthorn@google.com>

--- src/devices/thermal/drivers/aml-thermal-s905d2g-legacy/aml-fclk-rates.c
+++ src/devices/thermal/drivers/aml-thermal-s905d2g-legacy/aml-fclk-rates.c
@@ -16,6 +16,6 @@ static const aml_fclk_rate_table_t fclk_pll_rate_table[] = {
     FCLK_PLL_RATE(1000000000, 1, 0, 0),
 };
 
-const aml_fclk_rate_table_t* s905d2_fclk_get_rate_table() { return fclk_pll_rate_table; }
+const aml_fclk_rate_table_t* s905d2_fclk_get_rate_table(void) { return fclk_pll_rate_table; }
 
-size_t s905d2_fclk_get_rate_table_count() { return countof(fclk_pll_rate_table); }
+size_t s905d2_fclk_get_rate_table_count(void) { return countof(fclk_pll_rate_table); }
