6091ce93970eb04b0b2c5268b8aecaaddba0784c
Omer <omerlevran@google.com>
2022-04-21 18:40:12 +0000

    [ffx][doctor] replace doctor with doctor-new
    Bug: 97826
    Change-Id: I57e20e5c94361b125b39cf271fd722f6d8d5de72
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/671523
    Reviewed-by: Amit Uttamchandani <amituttam@google.com>
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Omer Lev-ran <omerlevran@google.com>

--- src/developer/ffx/plugins/doctor/src/constants.rs
+++ src/developer/ffx/plugins/doctor/src/constants.rs
@@ -3,47 +3,4 @@
 // found in the LICENSE file.
 
 pub const BUG_URL: &str = "https://fxbug.dev/new/ffx+User+Bug";
-
 pub const SUCCESS: &str = "success";
-pub const FOUND: &str = "found";
-pub const FAILED_TIMEOUT: &str = "FAILED. Timed out.";
-pub const FAILED_WITH_ERROR: &str = "FAILED. Error was:";
-
-pub const DAEMON_CHECK_INTRO: &str = "First, bringing up a working daemon...";
-pub const DAEMON_RUNNING_CHECK: &str = "Checking for a running daemon...";
-pub const FORCE_DAEMON_RESTART_MESSAGE: &str =
-    "restart-daemon is true; killing any running daemons...";
-pub const NONE_RUNNING: &str = "none running.";
-pub const KILLING_ZOMBIE_DAEMONS: &str = "Attempting to kill any zombie daemons...";
-pub const ZOMBIE_KILLED: &str = "killed at least one daemon.";
-pub const SPAWNING_DAEMON: &str = "Starting a new daemon instance, this will take a few seconds...";
-pub const CONNECTING_TO_DAEMON: &str =
-    "Attempting to connect to the daemon. This may take a couple seconds...";
-pub const COMMUNICATING_WITH_DAEMON: &str = "Attempting to communicate with the daemon...";
-pub const DAEMON_CHECKS_FAILED: &str =
-    "✗ Failed to spawn and connect to a daemon. Please file a bug to the
-ffx team using the link below and include all above output.";
-pub const LISTING_TARGETS_NO_FILTER: &str = "Attempting to list all targets...";
-pub const NO_TARGETS_FOUND_SHORT: &str = "No targets found. Killing the daemon and trying again...";
-pub const NO_TARGETS_FOUND_EXTENDED: &str =
-    "No targets found. Make sure your devices are connected and running and
-try again in a few seconds. If this persists after verifying your device's
-connection, please file a bug at the link below and include:
-
-   1) all output above and
-   2) device syslog if available.
-";
-pub const TARGET_CHOICE_HELP: &str =
-    "To choose a different target, use `ffx --target \"<nodename>\" doctor`";
-pub const TARGET_SUMMARY: &str = "Target summary:";
-pub const CONNECTING_TO_RCS: &str = "Attempting to get an RCS connection...";
-pub const COMMUNICATING_WITH_RCS: &str = "Attempting to communicate with RCS...";
-pub const RCS_TERMINAL_FAILURE: &str =
-    "To resolve repeated failures to connect to RCS, try rebooting failing
-devices.";
-pub const RCS_TERMINAL_FAILURE_BUG_INSTRUCTIONS: &str =
-    "If this persists, please file a bug at the link below and include:
-
-    1) all output above and
-    2) device syslog if available.
-";
