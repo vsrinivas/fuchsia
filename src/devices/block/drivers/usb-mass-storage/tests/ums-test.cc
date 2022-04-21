e03dc9dd15996a9c0267ad0006717ed3829ae22d
Sam Hansen <hansens@google.com>
2022-04-21 16:32:36 +0000

    [usb][ums] Disable ums-integration-test
    The integration test is flaky, and we're having a hard time resolving
    the race. Disabling the test to unwedge the tree while we sort out
    what's going on with the virtual bus. UMS doesn't see a lot of delta, so
    I'm ok turning off this test for the time being.
    
    Bug: 98566
    Change-Id: Ib316d3b79e1fa68505d291f2bea8527bc9a89298
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/671236
    Reviewed-by: Ruby Zhuang <rdzhuang@google.com>
    Commit-Queue: Sam Hansen <hansens@google.com>

--- src/devices/block/drivers/usb-mass-storage/tests/ums-test.cc
+++ src/devices/block/drivers/usb-mass-storage/tests/ums-test.cc
@@ -311,7 +311,8 @@ TEST_F(UmsTest, DISABLED_UncachedWriteShouldBePersistedToBlockDevice) {
   }
 }
 
-TEST_F(UmsTest, BlkdevTest) {
+// TODO(fxbug.dev/98566) re-enable when the race is resolved.
+TEST_F(UmsTest, DISABLED_BlkdevTest) {
   char errmsg[1024];
   fdio_spawn_action_t actions[1];
   actions[0] = {};
