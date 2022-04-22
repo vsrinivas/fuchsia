5ee51d9e507957725f724602fdd8cb9818b7a70c
Daniel Hwang <arkay@google.com>
2022-04-22 18:51:54 +0000

    [security] Add tests on ffx scrutiny commandline.
    This adds an integration test to use `ffx scrutiny` to extract a minimal
    blobfs and zbi. Ensures that ffx scrutiny changes respect the contract.
    
    b/222339195
    
    Change-Id: I088e6f0d8f7f89eafc5705cd1be20d89b265bcd0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/665862
    Reviewed-by: Mark Dittmer <markdittmer@google.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Commit-Queue: Daniel Hwang <arkay@google.com>

---
+++ src/security/ffx_test/src/lib.rs
@@ -0,0 +1,39 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {std::process::Command, tempfile::TempDir};
+
+const FFX_TOOL_PATH: &str = env!("FFX_TOOL_PATH");
+const BLOBFS_PATH: &str = env!("BLOBFS_PATH");
+const ZBI_PATH: &str = env!("ZBI_PATH");
+
+#[test]
+fn extract_blobfs() {
+    let tmp_dir = TempDir::new().unwrap();
+    let tmp_path = tmp_dir.path().to_str().unwrap();
+    assert!(Command::new(FFX_TOOL_PATH)
+        .args(vec![
+            "scrutiny",
+            "shell",
+            &format!("tool.blobfs.extract --input {} --output {}/blobfs", BLOBFS_PATH, tmp_path),
+        ])
+        .status()
+        .unwrap()
+        .success());
+}
+
+#[test]
+fn extract_zbi() {
+    let tmp_dir = TempDir::new().unwrap();
+    let tmp_path = tmp_dir.path().to_str().unwrap();
+    assert!(Command::new(FFX_TOOL_PATH)
+        .args(vec![
+            "scrutiny",
+            "shell",
+            &format!("tool.zbi.extract --input {} --output {}/zbi", ZBI_PATH, tmp_path),
+        ])
+        .status()
+        .unwrap()
+        .success());
+}
