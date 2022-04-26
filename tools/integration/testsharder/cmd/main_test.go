1261c548b2ac62e0ee7acf8e7695b2e923f48b54
Oliver Newman <olivernewman@google.com>
2022-04-26 17:57:17 +0000

    [testsharder] Add more golden tests
    Cover some common use cases that previously lacked tests, and clean up
    few existing bit of code.
    
    Change-Id: I16f956291703038683287f9bdad35604b7059eb6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/672563
    Fuchsia-Auto-Submit: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Ina Huh <ihuh@google.com>
    Commit-Queue: Oliver Newman <olivernewman@google.com>

--- tools/integration/testsharder/cmd/main_test.go
+++ tools/integration/testsharder/cmd/main_test.go
@@ -14,6 +14,7 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 
@@ -57,12 +58,13 @@ func TestExecute(t *testing.T) {
 		name          string
 		flags         testsharderFlags
 		testSpecs     []build.TestSpec
+		testDurations []build.TestDuration
 		testList      []build.TestListEntry
 		modifiers     []testsharder.TestModifier
 		affectedTests []string
 	}{
 		{
-			name: "basic",
+			name: "mixed device types",
 			testSpecs: []build.TestSpec{
 				fuchsiaTestSpec("foo"),
 				hostTestSpec("bar"),
@@ -72,6 +74,7 @@ func TestExecute(t *testing.T) {
 			name: "multiply",
 			testSpecs: []build.TestSpec{
 				fuchsiaTestSpec("foo"),
+				fuchsiaTestSpec("bar"),
 			},
 			modifiers: []testsharder.TestModifier{
 				{
@@ -86,7 +89,9 @@ func TestExecute(t *testing.T) {
 				fuchsiaTestSpec("affected"),
 				fuchsiaTestSpec("not-affected"),
 			},
-			affectedTests: []string{fuchsiaTestSpec("affected").Name},
+			affectedTests: []string{
+				packageURL("affected"),
+			},
 		},
 		{
 			name: "target test count",
@@ -99,7 +104,58 @@ func TestExecute(t *testing.T) {
 				fuchsiaTestSpec("foo3"),
 				fuchsiaTestSpec("foo4"),
 			},
-			affectedTests: []string{"foo"},
+		},
+		{
+			name: "sharding by time",
+			flags: testsharderFlags{
+				targetDurationSecs: int((4 * time.Minute).Seconds()),
+				perTestTimeoutSecs: int((10 * time.Minute).Seconds()),
+			},
+			testSpecs: []build.TestSpec{
+				fuchsiaTestSpec("slow"),
+				fuchsiaTestSpec("fast1"),
+				fuchsiaTestSpec("fast2"),
+				fuchsiaTestSpec("fast3"),
+			},
+			testDurations: []build.TestDuration{
+				{
+					Name:           "*",
+					MedianDuration: 2 * time.Second,
+				},
+				{
+					Name:           packageURL("slow"),
+					MedianDuration: 5 * time.Minute,
+				},
+			},
+		},
+		{
+			name: "hermetic deps",
+			flags: testsharderFlags{
+				hermeticDeps: true,
+			},
+			testSpecs: []build.TestSpec{
+				fuchsiaTestSpec("foo"),
+				fuchsiaTestSpec("bar"),
+				fuchsiaTestSpec("baz"),
+			},
+		},
+		{
+			name: "multiply affected test",
+			testSpecs: []build.TestSpec{
+				fuchsiaTestSpec("multiplied-affected-test"),
+				fuchsiaTestSpec("affected-test"),
+				fuchsiaTestSpec("unaffected-test"),
+			},
+			affectedTests: []string{
+				packageURL("multiplied-affected-test"),
+				packageURL("affected-test"),
+			},
+			modifiers: []testsharder.TestModifier{
+				{
+					Name:      "multiplied-affected-test",
+					TotalRuns: 100,
+				},
+			},
 		},
 		{
 			name: "test list with tags",
@@ -184,14 +240,16 @@ func TestExecute(t *testing.T) {
 			if len(tc.affectedTests) > 0 {
 				tc.flags.affectedTestsPath = writeTempFile(t, strings.Join(tc.affectedTests, "\n"))
 			}
+			// Write test-list.json.
 			if err := jsonutil.WriteToFile(
 				filepath.Join(tc.flags.buildDir, testListPath),
 				build.TestList{Tests: tc.testList},
 			); err != nil {
 				t.Fatal(err)
 			}
+			writeDepFiles(t, tc.flags.buildDir, tc.testSpecs)
 
-			m := &fakeModules{testSpecs: tc.testSpecs}
+			m := &fakeModules{testSpecs: tc.testSpecs, testDurations: tc.testDurations}
 			if err := execute(ctx, tc.flags, m); err != nil {
 				t.Fatal(err)
 			}
@@ -229,8 +287,18 @@ func (m *fakeModules) Platforms() []build.DimensionSet {
 		},
 	}
 }
+
+func (m *fakeModules) Images() []build.Image {
+	return []build.Image{
+		{
+			Name: "qemu-kernel",
+			Path: "multiboot.bin",
+			Type: "kernel",
+		},
+	}
+}
+
 func (m *fakeModules) TestListLocation() []string          { return []string{testListPath} }
-func (m *fakeModules) Images() []build.Image               { return m.images }
 func (m *fakeModules) TestSpecs() []build.TestSpec         { return m.testSpecs }
 func (m *fakeModules) TestDurations() []build.TestDuration { return m.testDurations }
 
@@ -262,11 +330,12 @@ func hostTestSpec(basename string) build.TestSpec {
 	testPath := fmt.Sprintf("host_x64/%s", basename)
 	return build.TestSpec{
 		Test: build.Test{
-			Name:  testPath,
-			Path:  testPath,
-			OS:    "linux",
-			CPU:   "x64",
-			Label: fmt.Sprintf("//tools/other:%s(//build/toolchain/host_x64)", basename),
+			Name:            testPath,
+			Path:            testPath,
+			OS:              "linux",
+			CPU:             "x64",
+			Label:           fmt.Sprintf("//tools/other:%s(//build/toolchain/host_x64)", basename),
+			RuntimeDepsFile: filepath.Join("runtime_deps", basename+".json"),
 		},
 		Envs: []build.Environment{
 			{
@@ -279,12 +348,29 @@ func hostTestSpec(basename string) build.TestSpec {
 	}
 }
 
+func writeDepFiles(t *testing.T, buildDir string, testSpecs []build.TestSpec) {
+	// Write runtime deps files.
+	for _, testSpec := range testSpecs {
+		if testSpec.RuntimeDepsFile == "" {
+			continue
+		}
+		absPath := filepath.Join(buildDir, testSpec.RuntimeDepsFile)
+		if err := os.MkdirAll(filepath.Dir(absPath), 0o700); err != nil {
+			t.Fatal(err)
+		}
+		runtimeDeps := []string{"host_x64/dep1", "host_x64/dep2"}
+		if err := jsonutil.WriteToFile(absPath, runtimeDeps); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
 // readShards deserializes testsharder output from a JSON file.
 func readShards(t *testing.T, path string) []testsharder.Shard {
 	var shards []testsharder.Shard
 	if err := jsonutil.ReadFromFile(path, &shards); err != nil {
 		if errors.Is(err, os.ErrNotExist) && strings.HasPrefix(path, *goldensDir) {
-			t.Fatalf("Golden file %s has not been created yet", path)
+			t.Fatalf("Golden file for case %q does not exist. To create it, run tools/integration/testsharder/update_goldens.sh", t.Name())
 		}
 		t.Fatal(err)
 	}
