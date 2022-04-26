a778a72b20a9116f5e939562f2efd07996bb45ea
Nick Eaton <nickeaton@google.com>
2022-04-26 18:29:34 +0000

    [fdf] Make `driver list-devices` work in DFv2
    Bug: 98816
    Change-Id: Ia186f7e347e115703477f1a9605e82532ec8d7f8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/672566
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Nick Eaton <nickeaton@google.com>

--- src/devices/bin/driver_tools/src/subcommands/list_devices/mod.rs
+++ src/devices/bin/driver_tools/src/subcommands/list_devices/mod.rs
@@ -5,71 +5,163 @@
 pub mod args;
 
 use {
-    super::common, anyhow::Result, args::ListDevicesCommand,
+    super::common::{self, DFv1Device, DFv2Node, Device},
+    anyhow::Result,
+    args::ListDevicesCommand,
     fidl_fuchsia_developer_remotecontrol as fremotecontrol, fidl_fuchsia_driver_development as fdd,
+    fidl_fuchsia_driver_framework as fdf,
 };
+
+trait DevicePrinter {
+    fn print(&self) -> Result<()>;
+    fn print_verbose(&self) -> Result<()>;
+}
+
+impl DevicePrinter for DFv1Device {
+    fn print(&self) -> Result<()> {
+        if let Some(ref topo_path) = self.0.topological_path {
+            println!("{}", topo_path);
+        }
+        Ok(())
+    }
+
+    fn print_verbose(&self) -> Result<()> {
+        let topo_path = self
+            .0
+            .topological_path
+            .as_deref()
+            .map(|s| s.strip_prefix("/dev/").unwrap().to_string())
+            .unwrap_or("".to_string());
+        let (_, name) = topo_path.rsplit_once('/').unwrap_or(("", &topo_path));
+        println!("{0: <9}: {1}", "Name", name);
+        println!("{0: <9}: {1}", "Topo Path", topo_path);
+        println!("{0: <9}: {1}", "Driver", self.0.bound_driver_libname.as_deref().unwrap_or(""));
+        println!(
+            "{0: <9}: {1:?}",
+            "Flags",
+            self.0.flags.as_ref().unwrap_or(&fdd::DeviceFlags::empty())
+        );
+        if let Some(ref property_list) = self.0.property_list {
+            let count = property_list.props.len();
+            println!("{} Properties", count);
+            let mut idx = 1;
+            for prop in property_list.props.iter() {
+                let id_name = bind::compiler::get_deprecated_key_identifiers()
+                    .get(&(prop.id as u32))
+                    .map(std::clone::Clone::clone)
+                    .unwrap_or_else(|| format!("{:#08}", prop.id));
+                println!(
+                    "[{0: >2}/ {1: >2}] : Value {2:#08x} Id {3}",
+                    idx, count, prop.value, id_name
+                );
+                idx += 1;
+            }
+            let count = property_list.str_props.len();
+            println!("{} String Properties", count);
+            idx = 1;
+            for prop in property_list.str_props.iter() {
+                println!(
+                    "[{0: >2}/ {1: >2}] : Key {2} Value {3:?}",
+                    idx, count, prop.key, prop.value
+                );
+                idx += 1;
+            }
+        } else {
+            println!("0 Properties");
+            println!("0 String Properties");
+        }
+        println!("");
+        Ok(())
+    }
+}
+
+impl DevicePrinter for DFv2Node {
+    fn print(&self) -> Result<()> {
+        println!("{}", self.0.moniker.as_ref().expect("DFv2 node does not have a moniker"));
+        Ok(())
+    }
+
+    fn print_verbose(&self) -> Result<()> {
+        let moniker = self.0.moniker.as_deref().expect("DFv2 node does not have a moniker");
+        let (_, name) = moniker.rsplit_once('.').unwrap_or(("", &moniker));
+        println!("{0: <9}: {1}", "Name", name);
+        println!("{0: <9}: {1}", "Moniker", moniker);
+        println!("{0: <9}: {1}", "Driver", &self.0.bound_driver_url.as_deref().unwrap_or("None"));
+        if let Some(ref node_property_list) = self.0.node_property_list {
+            println!("{} Properties", node_property_list.len());
+            for i in 0..node_property_list.len() {
+                let node_property = &node_property_list[i];
+                println!(
+                    "[{:>2}/ {:>2}] : Key {} Value {}",
+                    i + 1,
+                    node_property_list.len(),
+                    node_property
+                        .key
+                        .as_ref()
+                        .map(|key| match key {
+                            fdf::NodePropertyKey::IntValue(value) => value.to_string(),
+                            fdf::NodePropertyKey::StringValue(ref value) =>
+                                format!("\"{}\"", value),
+                        })
+                        .unwrap_or("None".to_owned()),
+                    node_property
+                        .value
+                        .as_ref()
+                        .map(|value| match value {
+                            fdf::NodePropertyValue::IntValue(value) => value.to_string(),
+                            fdf::NodePropertyValue::StringValue(ref value) =>
+                                format!("\"{}\"", value),
+                            fdf::NodePropertyValue::BoolValue(value) => value.to_string(),
+                            fdf::NodePropertyValue::EnumValue(ref value) =>
+                                format!("Enum({})", value),
+                        })
+                        .unwrap_or("None".to_owned()),
+                );
+            }
+        } else {
+            println!("0 Properties");
+        }
+        println!("");
+        Ok(())
+    }
+}
+
+impl DevicePrinter for Device {
+    fn print(&self) -> Result<()> {
+        match self {
+            Device::V1(device) => device.print(),
+            Device::V2(node) => node.print(),
+        }
+    }
+
+    fn print_verbose(&self) -> Result<()> {
+        match self {
+            Device::V1(device) => device.print_verbose(),
+            Device::V2(node) => node.print_verbose(),
+        }
+    }
+}
 
 pub async fn list_devices(
     remote_control: fremotecontrol::RemoteControlProxy,
     cmd: ListDevicesCommand,
 ) -> Result<()> {
     let service = common::get_development_proxy(remote_control, cmd.select).await?;
-    let device_info = match cmd.device {
+    let devices: Vec<Device> = match cmd.device {
         Some(device) => common::get_device_info(&service, &[device]).await?,
         None => common::get_device_info(&service, &[]).await?,
-    };
+    }
+    .into_iter()
+    .map(|device_info| Device::from(device_info))
+    .collect();
 
     if cmd.verbose {
-        for device in device_info {
-            let topo_path = device
-                .topological_path
-                .map(|s| s.strip_prefix("/dev/").unwrap().to_string())
-                .unwrap_or("".to_string());
-            let (_, name) = topo_path.rsplit_once('/').unwrap_or(("", &topo_path));
-            println!("{0: <9}: {1}", "Name", name);
-            println!("{0: <9}: {1}", "Topo Path", topo_path);
-            println!(
-                "{0: <9}: {1}",
-                "Driver",
-                device.bound_driver_libname.unwrap_or("".to_string()),
-            );
-            println!("{0: <9}: {1:?}", "Flags", device.flags.unwrap_or(fdd::DeviceFlags::empty()));
-            if let Some(property_list) = device.property_list {
-                let count = property_list.props.len();
-                println!("{} Properties", count);
-                let mut idx = 0;
-                for prop in property_list.props {
-                    let id_name = bind::compiler::get_deprecated_key_identifiers()
-                        .get(&(prop.id as u32))
-                        .map(std::clone::Clone::clone)
-                        .unwrap_or_else(|| format!("{:#08}", prop.id));
-                    println!(
-                        "[{0: >2}/ {1: >2}] : Value {2:#08x} Id {3}",
-                        idx, count, prop.value, id_name
-                    );
-                    idx += 1;
-                }
-                let count = property_list.str_props.len();
-                println!("{} String Properties", count);
-                idx = 0;
-                for prop in property_list.str_props {
-                    println!(
-                        "[{0: >2}/ {1: >2}] : Key {2} Value {3:?}",
-                        idx, count, prop.key, prop.value
-                    );
-                    idx += 1;
-                }
-            } else {
-                println!("0 Properties");
-                println!("0 String Properties");
-            }
-            println!("");
+        for device in devices {
+            device.print_verbose()?;
         }
     } else {
-        for device in device_info {
-            if let Some(path) = device.topological_path {
-                println!("{}", path);
-            }
+        for device in devices {
+            device.print()?;
         }
     }
     Ok(())
