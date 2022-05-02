9f085c90459490b0cd980ef227111e2f0ef7849e
Daniel Hwang <arkay@google.com>
2022-05-02 23:23:37 +0000

    [scrutiny] Add a ffx scrutiny shell test for recovery images.
    Recovery images package the blobfs in the zbi and trigger an additional
    blobfs extraction step as part of zbi extraction. This tests that
    workflow
    
    Change-Id: I2d52212ef76edea5d7810a485fa07108b2bbed6a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/675267
    Reviewed-by: Ben Wright <benwright@google.com>
    Commit-Queue: Daniel Hwang <arkay@google.com>

--- src/security/ffx_test/src/lib.rs
+++ src/security/ffx_test/src/lib.rs
@@ -7,6 +7,7 @@ use {std::process::Command, tempfile::TempDir};
 const FFX_TOOL_PATH: &str = env!("FFX_TOOL_PATH");
 const BLOBFS_PATH: &str = env!("BLOBFS_PATH");
 const ZBI_PATH: &str = env!("ZBI_PATH");
+const RECOVERY_ZBI_PATH: &str = env!("RECOVERY_ZBI_PATH");
 
 #[test]
 fn extract_blobfs() {
@@ -32,6 +33,21 @@ fn extract_zbi() {
             "scrutiny",
             "shell",
             &format!("tool.zbi.extract --input {} --output {}/zbi", ZBI_PATH, tmp_path),
+        ])
+        .status()
+        .unwrap()
+        .success());
+}
+
+#[test]
+fn extract_recovery_zbi() {
+    let tmp_dir = TempDir::new().unwrap();
+    let tmp_path = tmp_dir.path().to_str().unwrap();
+    assert!(Command::new(FFX_TOOL_PATH)
+        .args(vec![
+            "scrutiny",
+            "shell",
+            &format!("tool.zbi.extract --input {} --output {}/zbi", RECOVERY_ZBI_PATH, tmp_path),
         ])
         .status()
         .unwrap()
