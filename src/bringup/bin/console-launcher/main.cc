d03b4f667ef266e5f4eecb00904461626e9ad3e2
Alex Legg <alexlegg@google.com>
2022-05-02 23:15:34 +0000

    [console-launcher] Refactor
    Move console launching and autorun into separate functions.
    
    Change-Id: I764cb27bdd421c3545f0f5d3f3041a002c02f543
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/674810
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Alex Legg <alexlegg@google.com>

--- src/bringup/bin/console-launcher/main.cc
+++ src/bringup/bin/console-launcher/main.cc
@@ -122,6 +122,158 @@ zx_status_t CreateVirtualConsoles(const console_launcher::ConsoleLauncher& launc
   return ZX_OK;
 }
 
+std::vector<std::thread> LaunchAutorun(const console_launcher::ConsoleLauncher& launcher,
+                                       fs::FuchsiaVfs& vfs, const fbl::RefPtr<fs::Vnode>& root,
+                                       std::unordered_map<std::string_view, std::thread>& threads,
+                                       const console_launcher::Arguments& args) {
+  std::tuple<const char*, const std::string&, std::vector<std::string_view>> map[] = {
+      // NB: //tools/emulator/emulator.go expects these to be available in its boot autorun.
+      {"autorun:boot", args.autorun_boot, {"/dev", "/mnt"}},
+      {"autorun:system", args.autorun_system, {"/system"}},
+  };
+
+  std::vector<std::thread> autorun;
+  for (const auto& [name, args, paths] : map) {
+    if (args.empty()) {
+      continue;
+    }
+    zx::status endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+    if (endpoints.is_error()) {
+      FX_PLOGS(FATAL, endpoints.status_value()) << "failed to create endpoints";
+    }
+
+    if (zx_status_t status =
+            vfs.ServeDirectory(root, std::move(endpoints->server), fs::Rights::All());
+        status != ZX_OK) {
+      FX_PLOGS(FATAL, status) << "failed to serve root directory";
+    }
+
+    // Get the full commandline by splitting on '+'.
+    std::vector argv = fxl::SplitStringCopy(args, "+", fxl::WhiteSpaceHandling::kTrimWhitespace,
+                                            fxl::SplitResult::kSplitWantNonEmpty);
+    autorun.emplace_back([paths = paths, &threads, args = std::move(argv), name = name,
+                          client_end = std::move(endpoints->client),
+                          &job = launcher.shell_job()]() {
+      for (std::string_view path : paths) {
+        if (auto it = threads.find(path); it != threads.end()) {
+          it->second.join();
+        } else {
+          FX_LOGS(ERROR) << "unable to run '" << name << "': could not mount required path '"
+                         << path << "'";
+          return;
+        }
+      }
+
+      const char* argv[args.size() + 1];
+      argv[args.size()] = nullptr;
+      for (size_t i = 0; i < args.size(); ++i) {
+        argv[i] = args[i].c_str();
+      }
+
+      fdio_spawn_action_t actions[] = {
+          {
+              .action = FDIO_SPAWN_ACTION_SET_NAME,
+              .name =
+                  {
+                      .data = name,
+                  },
+          },
+          {
+              .action = FDIO_SPAWN_ACTION_ADD_NS_ENTRY,
+              .ns =
+                  {
+                      .prefix = "/",
+                      .handle = client_end.channel().get(),
+                  },
+          },
+      };
+
+      zx::process process;
+      char err_msg[FDIO_SPAWN_ERR_MSG_MAX_LENGTH];
+      constexpr uint32_t flags = FDIO_SPAWN_CLONE_ALL & ~FDIO_SPAWN_CLONE_NAMESPACE;
+      FX_LOGS(INFO) << "starting '" << name << "': " << args;
+      zx_status_t status =
+          fdio_spawn_etc(job.get(), flags, argv[0], argv, nullptr, std::size(actions), actions,
+                         process.reset_and_get_address(), err_msg);
+      if (status != ZX_OK) {
+        FX_PLOGS(ERROR, status) << "failed to start '" << name << "': " << err_msg;
+        return;
+      }
+      if (zx_status_t status =
+              process.wait_one(ZX_PROCESS_TERMINATED, zx::time::infinite(), nullptr);
+          status != ZX_OK) {
+        FX_PLOGS(ERROR, status) << "failed to wait for '" << name << "' termination";
+      }
+      FX_LOGS(INFO) << "completed '" << name << "': " << args;
+    });
+  }
+
+  return autorun;
+}
+
+[[noreturn]] void RunSerialConsole(const console_launcher::ConsoleLauncher& launcher,
+                                   fs::FuchsiaVfs& vfs, const fbl::RefPtr<fs::Vnode>& root,
+                                   const console_launcher::Arguments& args) {
+  while (true) {
+    zx::status fd = console_launcher::WaitForFile(args.device.path.c_str(), zx::time::infinite());
+    if (fd.is_error()) {
+      FX_PLOGS(FATAL, fd.status_value())
+          << "failed to wait for console '" << args.device.path << "'";
+    }
+
+    fdio_cpp::FdioCaller caller(std::move(fd).value());
+
+    zx::channel stdio;
+    // If the console is a virtio connection, then speak the
+    // fuchsia.hardware.virtioconsole.Device interface to get the real
+    // fuchsia.io.File connection
+    //
+    // TODO(fxbug.dev/33183): Clean this up once devhost stops speaking
+    // fuchsia.io.File on behalf of drivers.  Once that happens, the virtio-console
+    // driver should just speak that instead of this shim interface.
+    if (args.device.is_virtio) {
+      zx::status endpoints = fidl::CreateEndpoints<fuchsia_hardware_pty::Device>();
+      if (endpoints.is_error()) {
+        FX_PLOGS(FATAL, endpoints.status_value()) << "failed to create pty endpoints";
+      }
+      const fidl::WireResult result =
+          fidl::WireCall(caller.borrow_as<fuchsia_hardware_virtioconsole::Device>())
+              ->GetChannel(std::move(endpoints->server));
+      if (!result.ok()) {
+        FX_PLOGS(FATAL, result.status()) << "failed to get virtio console channel";
+      }
+      stdio = std::move(endpoints->client).TakeChannel();
+    } else {
+      zx::status channel = caller.take_channel();
+      if (channel.is_error()) {
+        FX_PLOGS(FATAL, channel.status_value()) << "failed to get console channel";
+      }
+      stdio = std::move(channel.value());
+    }
+
+    zx::status endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+    if (endpoints.is_error()) {
+      FX_PLOGS(FATAL, endpoints.status_value()) << "failed to create endpoints";
+    }
+    if (zx_status_t status =
+            vfs.ServeDirectory(root, std::move(endpoints->server), fs::Rights::All());
+        status != ZX_OK) {
+      FX_PLOGS(FATAL, status) << "failed to serve root directory";
+    }
+
+    zx::status process =
+        launcher.LaunchShell(std::move(endpoints->client), std::move(stdio), args.term);
+    if (process.is_error()) {
+      FX_PLOGS(FATAL, process.status_value()) << "failed to launch shell";
+    }
+
+    if (zx_status_t status = console_launcher::WaitForExit(std::move(process.value()));
+        status != ZX_OK) {
+      FX_PLOGS(FATAL, status) << "failed to wait for shell exit";
+    }
+  }
+}
+
 }  // namespace
 
 int main(int argv, char** argc) {
@@ -140,14 +292,15 @@ int main(int argv, char** argc) {
         << "failed to connect to " << fidl::DiscoverableProtocolName<fuchsia_boot::Arguments>;
   }
 
-  zx::status args = console_launcher::GetArguments(boot_args.value());
-  if (args.is_error()) {
-    FX_PLOGS(FATAL, args.status_value()) << "failed to get arguments";
+  zx::status get_args = console_launcher::GetArguments(boot_args.value());
+  if (get_args.is_error()) {
+    FX_PLOGS(FATAL, get_args.status_value()) << "failed to get arguments";
   }
+  console_launcher::Arguments args = get_args.value();
 
-  if (!args->run_shell) {
-    if (!args->autorun_boot.empty()) {
-      FX_LOGS(ERROR) << "cannot launch autorun command '" << args->autorun_boot << "'";
+  if (!args.run_shell) {
+    if (!args.autorun_boot.empty()) {
+      FX_LOGS(ERROR) << "cannot launch autorun command '" << args.autorun_boot << "'";
     }
     FX_LOGS(INFO) << "console.shell: disabled";
     // TODO(https://fxbug.dev/97657): Hang around. If we exit before archivist has started, our logs
@@ -253,152 +406,15 @@ int main(int argv, char** argc) {
   }
   const auto& launcher = result.value();
 
-  std::tuple<const char*, std::string&, std::vector<std::string_view>> map[] = {
-      // NB: //tools/emulator/emulator.go expects these to be available in its boot autorun.
-      {"autorun:boot", args->autorun_boot, {"/dev", "/mnt"}},
-      {"autorun:system", args->autorun_system, {"/system"}},
-  };
+  std::vector<std::thread> autorun = LaunchAutorun(launcher, vfs, root, threads, args);
 
-  std::vector<std::thread> autorun;
-  for (const auto& [name, args, paths] : map) {
-    if (args.empty()) {
-      continue;
-    }
-    zx::status endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
-    if (endpoints.is_error()) {
-      FX_PLOGS(FATAL, endpoints.status_value()) << "failed to create endpoints";
-    }
-
-    if (zx_status_t status =
-            vfs.ServeDirectory(root, std::move(endpoints->server), fs::Rights::All());
-        status != ZX_OK) {
-      FX_PLOGS(FATAL, status) << "failed to serve root directory";
-    }
-
-    // Get the full commandline by splitting on '+'.
-    std::vector argv = fxl::SplitStringCopy(args, "+", fxl::WhiteSpaceHandling::kTrimWhitespace,
-                                            fxl::SplitResult::kSplitWantNonEmpty);
-    autorun.emplace_back([paths = paths, &threads, args = std::move(argv), name = name,
-                          client_end = std::move(endpoints->client),
-                          &job = launcher.shell_job()]() {
-      for (std::string_view path : paths) {
-        if (auto it = threads.find(path); it != threads.end()) {
-          it->second.join();
-        } else {
-          FX_LOGS(ERROR) << "unable to run '" << name << "': could not mount required path '"
-                         << path << "'";
-          return;
-        }
-      }
-
-      const char* argv[args.size() + 1];
-      argv[args.size()] = nullptr;
-      for (size_t i = 0; i < args.size(); ++i) {
-        argv[i] = args[i].c_str();
-      }
-
-      fdio_spawn_action_t actions[] = {
-          {
-              .action = FDIO_SPAWN_ACTION_SET_NAME,
-              .name =
-                  {
-                      .data = name,
-                  },
-          },
-          {
-              .action = FDIO_SPAWN_ACTION_ADD_NS_ENTRY,
-              .ns =
-                  {
-                      .prefix = "/",
-                      .handle = client_end.channel().get(),
-                  },
-          },
-      };
-
-      zx::process process;
-      char err_msg[FDIO_SPAWN_ERR_MSG_MAX_LENGTH];
-      constexpr uint32_t flags = FDIO_SPAWN_CLONE_ALL & ~FDIO_SPAWN_CLONE_NAMESPACE;
-      FX_LOGS(INFO) << "starting '" << name << "': " << args;
-      zx_status_t status =
-          fdio_spawn_etc(job.get(), flags, argv[0], argv, nullptr, std::size(actions), actions,
-                         process.reset_and_get_address(), err_msg);
-      if (status != ZX_OK) {
-        FX_PLOGS(ERROR, status) << "failed to start '" << name << "': " << err_msg;
-        return;
-      }
-      if (zx_status_t status =
-              process.wait_one(ZX_PROCESS_TERMINATED, zx::time::infinite(), nullptr);
-          status != ZX_OK) {
-        FX_PLOGS(ERROR, status) << "failed to wait for '" << name << "' termination";
-      }
-      FX_LOGS(INFO) << "completed '" << name << "': " << args;
-    });
-  }
-
-  if (zx_status_t status = CreateVirtualConsoles(launcher, vfs, root,
-                                                 args->virtual_console_need_debuglog, args->term);
+  if (zx_status_t status =
+          CreateVirtualConsoles(launcher, vfs, root, args.virtual_console_need_debuglog, args.term);
       status != ZX_OK) {
     // If launching virtcon fails, we still should continue so that the autorun programs
     // and serial console are launched.
     FX_PLOGS(ERROR, status) << "failed to set up virtcon";
   }
 
-  while (true) {
-    zx::status fd = console_launcher::WaitForFile(args->device.path.c_str(), zx::time::infinite());
-    if (fd.is_error()) {
-      FX_PLOGS(FATAL, fd.status_value())
-          << "failed to wait for console '" << args->device.path << "'";
-    }
-
-    fdio_cpp::FdioCaller caller(std::move(fd).value());
-
-    zx::channel stdio;
-    // If the console is a virtio connection, then speak the
-    // fuchsia.hardware.virtioconsole.Device interface to get the real
-    // fuchsia.io.File connection
-    //
-    // TODO(fxbug.dev/33183): Clean this up once devhost stops speaking
-    // fuchsia.io.File on behalf of drivers.  Once that happens, the virtio-console
-    // driver should just speak that instead of this shim interface.
-    if (args->device.is_virtio) {
-      zx::status endpoints = fidl::CreateEndpoints<fuchsia_hardware_pty::Device>();
-      if (endpoints.is_error()) {
-        FX_PLOGS(FATAL, endpoints.status_value()) << "failed to create pty endpoints";
-      }
-      const fidl::WireResult result =
-          fidl::WireCall(caller.borrow_as<fuchsia_hardware_virtioconsole::Device>())
-              ->GetChannel(std::move(endpoints->server));
-      if (!result.ok()) {
-        FX_PLOGS(FATAL, result.status()) << "failed to get virtio console channel";
-      }
-      stdio = std::move(endpoints->client).TakeChannel();
-    } else {
-      zx::status channel = caller.take_channel();
-      if (channel.is_error()) {
-        FX_PLOGS(FATAL, channel.status_value()) << "failed to get console channel";
-      }
-      stdio = std::move(channel.value());
-    }
-
-    zx::status endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
-    if (endpoints.is_error()) {
-      FX_PLOGS(FATAL, endpoints.status_value()) << "failed to create endpoints";
-    }
-    if (zx_status_t status =
-            vfs.ServeDirectory(root, std::move(endpoints->server), fs::Rights::All());
-        status != ZX_OK) {
-      FX_PLOGS(FATAL, status) << "failed to serve root directory";
-    }
-
-    zx::status process =
-        launcher.LaunchShell(std::move(endpoints->client), std::move(stdio), args->term);
-    if (process.is_error()) {
-      FX_PLOGS(FATAL, process.status_value()) << "failed to launch shell";
-    }
-
-    if (zx_status_t status = console_launcher::WaitForExit(std::move(process.value()));
-        status != ZX_OK) {
-      FX_PLOGS(FATAL, status) << "failed to wait for shell exit";
-    }
-  }
+  RunSerialConsole(launcher, vfs, root, args);
 }
