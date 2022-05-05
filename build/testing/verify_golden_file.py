54ff50462be92d6328d977c8f17785e310f7a282
Adam Perry <adamperry@google.com>
2022-05-05 06:00:26 +0000

    [build] Add argument for blessing goldens.
    Change-Id: Ic4d8f10d05e2c57c3a1b504237baebab739ac93a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/674305
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Shai Barack <shayba@google.com>

--- build/testing/verify_golden_file.py
+++ build/testing/verify_golden_file.py
@@ -7,6 +7,7 @@ import argparse
 import filecmp
 import json
 import os
+import shutil
 import sys
 
 # Verifies that the current golden file matches the provided golden.
@@ -21,6 +22,10 @@ def main():
     parser.add_argument(
         '--stamp', help='Path to the victory file', required=True)
     parser.add_argument(
+        '--bless',
+        help="Overwrites current with golden if they don't match.",
+        action='store_true')
+    parser.add_argument(
         '--warn',
         help='Whether API changes should only cause warnings',
         action='store_true')
@@ -28,16 +33,22 @@ def main():
 
     if args.golden:
         if not filecmp.cmp(args.golden, args.current):
-            type = 'Warning' if args.warn else 'Error'
+            type = 'Warning' if args.warn or args.bless else 'Error'
             print('%s: Golden file mismatch' % type)
-            print('Please acknowledge this change by running:')
-            # Use abspath in cp command so it works regardless of current
-            # working directory.
-            print(
-                '  cp ' + os.path.abspath(args.current) + ' ' +
-                os.path.abspath(args.golden))
-            if not args.warn:
-                return 1
+
+            if args.bless:
+                shutil.copyfile(args.current, args.golden)
+            else:
+                print('Please acknowledge this change by updating the golden.\n')
+                print('You can rebuild with `bless_goldens=true` in your GN args,')
+                print('or you can run this command:')
+                # Use abspath in cp command so it works regardless of current
+                # working directory.
+                print(
+                    '  cp ' + os.path.abspath(args.current) + ' ' +
+                    os.path.abspath(args.golden))
+                if not args.warn:
+                    return 1
 
     with open(args.stamp, 'w') as stamp_file:
         stamp_file.write('Golden!\n')
