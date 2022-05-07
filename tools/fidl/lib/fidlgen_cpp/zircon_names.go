2ebb701dea0556ed760af39efa4437e5509d02b6
Alex Zaslavsky <azaslavsky@google.com>
2022-05-07 01:13:44 +0000

    [fidl][cpp] Support zx builtin values
    Two default zircon types known to FIDL, `obj_type` and `rights`, also
    have values that may be aliased as FIDL constants, like so:
    
    ```
    const FOO zx.obj_type = zx.obj_type.CHANNEL;
    const BAR zx.rights = zx.rights.READ;
    ```
    
    Previously, fidlgen_cpp would panic on the RHS of this declaration, as
    it assumed that identifiers into the `zx` library must only be types,
    not named bits/enum members. This change fixes the issue by adding
    support for value resolution and prefixing as well.
    
    Test: fx regen-goldens
    Fixed: 97296
    Change-Id: I0e5c357430ecc162c0b4c68ea3af7928a8db916c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/677089
    Commit-Queue: Alex Zaslavsky <azaslavsky@google.com>
    Reviewed-by: Mitchell Kember <mkember@google.com>
    Fuchsia-Auto-Submit: Alex Zaslavsky <azaslavsky@google.com>

--- tools/fidl/lib/fidlgen_cpp/zircon_names.go
+++ tools/fidl/lib/fidlgen_cpp/zircon_names.go
@@ -11,9 +11,20 @@ import (
 	"go.fuchsia.dev/fuchsia/tools/fidl/lib/fidlgen"
 )
 
-var zirconNames = map[string]string{
-	"rights":   "zx_rights_t",
-	"obj_type": "zx_obj_type_t",
+type zxName = struct {
+	typeName string
+	prefix   string
+}
+
+var zirconNames = map[string]zxName{
+	"rights": {
+		typeName: "zx_rights_t",
+		prefix:   "ZX_RIGHT",
+	},
+	"obj_type": {
+		typeName: "zx_obj_type_t",
+		prefix:   "ZX_OBJ_TYPE",
+	},
 }
 
 func isZirconIdentifier(ci fidlgen.CompoundIdentifier) bool {
@@ -21,18 +32,47 @@ func isZirconIdentifier(ci fidlgen.CompoundIdentifier) bool {
 }
 
 func zirconName(ci fidlgen.CompoundIdentifier) name {
-	name := string(ci.Name)
-	if ci.Member == "" {
-		n, ok := zirconNames[name]
-		if ok {
-			return makeName(n)
+	if ci.Member != "" {
+		if zn, ok := zirconValueMember(ci.Name, ci.Member); ok {
+			return zn
 		}
-
-		if name == strings.ToUpper(name) {
-			// all-caps names get a ZX_ prefix
-			return makeName(fmt.Sprintf("ZX_%s", name))
+	} else {
+		if zn, ok := zirconType(ci.Name); ok {
+			return zn
+		}
+		if zn, ok := zirconConst(ci.Name); ok {
+			return zn
 		}
 	}
 
 	panic(fmt.Sprintf("Unknown zircon identifier: %s", ci.Encode()))
+}
+
+func zirconType(id fidlgen.Identifier) (name, bool) {
+	n := string(id)
+	if zn, ok := zirconNames[n]; ok {
+		return makeName(zn.typeName), true
+	}
+
+	return name{}, false
+}
+
+func zirconValueMember(id fidlgen.Identifier, mem fidlgen.Identifier) (name, bool) {
+	n := string(id)
+	m := string(mem)
+	if zn, ok := zirconNames[n]; ok {
+		return makeName(fmt.Sprintf("%s_%s", zn.prefix, strings.ToUpper(m))), true
+	}
+
+	return name{}, false
+}
+
+func zirconConst(id fidlgen.Identifier) (name, bool) {
+	n := string(id)
+	if n == strings.ToUpper(n) {
+		// All-caps names like `CHANNEL_MAX_MSG_BYTES`` get a ZX_ prefix.
+		return makeName(fmt.Sprintf("ZX_%s", n)), true
+	}
+
+	return name{}, false
 }
