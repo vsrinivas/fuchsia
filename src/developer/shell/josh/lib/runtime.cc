000e22baebfdb55b9e2860a52bf61ffc865110fa
Jeremy Manson <jeremymanson@google.com>
2022-05-10 16:50:26 +0000

    [shell] Remove obsolete shell mirror code
    Change-Id: I5aa126798c1d5e8f52ff1080e705714d27d51633
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/677090
    Reviewed-by: Gabe Schine <thatguy@google.com>
    Commit-Queue: Jeremy Manson <jeremymanson@google.com>

--- src/developer/shell/josh/lib/runtime.cc
+++ src/developer/shell/josh/lib/runtime.cc
@@ -9,7 +9,6 @@
 
 #include "src/developer/shell/josh/lib/fdio.h"
 #include "src/developer/shell/josh/lib/fidl.h"
-#include "src/developer/shell/josh/lib/sys.h"
 #include "src/developer/shell/josh/lib/zx.h"
 #include "third_party/quickjs/quickjs-libc.h"
 #include "third_party/quickjs/quickjs.h"
@@ -130,13 +129,6 @@ bool Context::InitBuiltins(const std::string& fidl_path, const std::string& boot
   }
 
   js_std_eval_binary(ctx_, qjsc_zx, qjsc_zx_size, 0);
-
-  if (sys::SysModuleInit(ctx_, "sys") == nullptr) {
-    return false;
-  }
-  if (!Export("sys")) {
-    return false;
-  }
 
   if (!boot_js_path.empty()) {
     if (!Export("pp", boot_js_path)) {
