67eab18233fe9e3e942a474d1b8ab55eca59fd3c
Bruno Dal Bo <brunodalbo@google.com>
2022-05-10 00:49:24 +0000

    [netstack] Install thread profiles
    Request installation of thread profiles to improve latency. This
    especially affects UDP, which is a synchronous channel call with
    netstack. That reduces latency significantly and improves audio quality
    in streamed audio applications.
    
    Bug: 99550
    Change-Id: Ie5c5613a88e43a4cdb7333e2548f36bb69fbbf93
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/675266
    Fuchsia-Auto-Submit: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: James Robinson <jamesr@google.com>

--- src/connectivity/network/tests/integration/sys/src/lib.rs
+++ src/connectivity/network/tests/integration/sys/src/lib.rs
@@ -4,9 +4,13 @@
 
 #![cfg(test)]
 
-use fidl::endpoints::{DiscoverableProtocolMarker as _, Proxy as _};
+use fidl::{
+    endpoints::{DiscoverableProtocolMarker as _, Proxy as _},
+    AsHandleRef as _,
+};
 use fidl_fuchsia_io as fio;
 use fidl_fuchsia_logger as flogger;
+use fidl_fuchsia_netemul as fnetemul;
 use fidl_fuchsia_netstack as fnetstack;
 use fuchsia_component::server::ServiceFs;
 use fuchsia_component_test::{
@@ -14,8 +18,8 @@ use fuchsia_component_test::{
 };
 use fuchsia_zircon as zx;
 
-use futures::{FutureExt as _, StreamExt as _};
-use netstack_testing_common::realms::{Netstack, Netstack2};
+use futures::{FutureExt as _, StreamExt as _, TryStreamExt as _};
+use netstack_testing_common::realms::{Netstack, Netstack2, NetstackVersion};
 use std::convert::TryInto as _;
 use vfs::directory::entry::DirectoryEntry as _;
 
@@ -219,4 +223,71 @@ async fn start_with_cache_no_space() {
     let SyncNoSpaceEntryConstructor(this) = &*constructor;
     let NoSpaceEntryConstructor { paths } = &*this.lock().unwrap();
     assert_eq!(paths[..], ["pprof"]);
+}
+
+#[fuchsia::test]
+async fn ns2_sets_thread_profiles() {
+    use fuchsia_component::server::{ServiceFs, ServiceFsDir};
+
+    const MOCK_SERVICES_NAME: &str = "mock";
+    let sandbox = netemul::TestSandbox::new().expect("failed to create sandbox");
+    let mut netstack: fnetemul::ChildDef =
+        (&netstack_testing_common::realms::KnownServiceProvider::Netstack(
+            NetstackVersion::ProdNetstack2,
+        ))
+            .into();
+    {
+        let fnetemul::ChildUses::Capabilities(capabilities) =
+            netstack.uses.as_mut().expect("empty uses");
+        capabilities.push(fnetemul::Capability::ChildDep(fnetemul::ChildDep {
+            name: Some(MOCK_SERVICES_NAME.to_string()),
+            capability: Some(fnetemul::ExposedCapability::Protocol(
+                fidl_fuchsia_scheduler::ProfileProviderMarker::PROTOCOL_NAME.to_string(),
+            )),
+            ..fnetemul::ChildDep::EMPTY
+        }));
+    }
+
+    let (mock_dir, server_end) = fidl::endpoints::create_endpoints().expect("create endpoints");
+    let mut fs = ServiceFs::new();
+    let _: &mut ServiceFsDir<'_, _> = fs
+        .dir("svc")
+        .add_fidl_service(|rs: fidl_fuchsia_scheduler::ProfileProviderRequestStream| rs);
+    let _: &mut ServiceFs<_> =
+        fs.serve_connection(server_end.into_channel()).expect("serve connection");
+
+    let realm = sandbox
+        .create_realm(
+            "ns2_sets_thread_profiles",
+            [
+                netstack,
+                (&netstack_testing_common::realms::KnownServiceProvider::SecureStash).into(),
+                fnetemul::ChildDef {
+                    source: Some(fnetemul::ChildSource::Mock(mock_dir)),
+                    name: Some(MOCK_SERVICES_NAME.to_string()),
+                    ..fnetemul::ChildDef::EMPTY
+                },
+            ],
+        )
+        .expect("failed to create realm");
+
+    // Connect to any service to get netstack launched.
+    let _: fidl_fuchsia_net_interfaces::StateProxy = realm
+        .connect_to_protocol::<fidl_fuchsia_net_interfaces::StateMarker>()
+        .expect("connect to protocol");
+    // And expect that we'll see a connection to profile provider.
+    let (thread, profile, responder) = fs
+        .flatten()
+        .try_next()
+        .await
+        .expect("fs failure")
+        .expect("fs terminated unexpectedly")
+        .into_set_profile_by_role()
+        .expect("unexpected request");
+    assert_eq!(profile, "fuchsia.netstack.go-worker");
+    assert_eq!(
+        thread.basic_info().expect("failed to get basic info").rights,
+        zx::Rights::TRANSFER | zx::Rights::MANAGE_THREAD
+    );
+    responder.send(zx::Status::OK.into_raw()).expect("failed to respond");
 }
