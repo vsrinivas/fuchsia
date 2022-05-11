cc625aac97a463cf6927800333e46259a7939bd5
Nick Brown <nickbrow@google.com>
2022-05-11 17:31:18 +0000

    [udp_serde] Use simpler method to copy spans
    Advancing the destination span is unnecessary here.
    
    Change-Id: I00e9cd24cba1818c5fef50ed622d76ca97e9b288
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/678071
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Fuchsia-Auto-Submit: Nick Brown <nickbrow@google.com>
    Commit-Queue: Nick Brown <nickbrow@google.com>

--- src/connectivity/network/netstack/udp_serde.cc
+++ src/connectivity/network/netstack/udp_serde.cc
@@ -234,7 +234,7 @@ SerializeRecvMsgMetaError serialize_recv_msg_meta(RecvMsgMeta meta, Buffer from_
       if (from_addr_span.size() != to_addr.size()) {
         return SerializeRecvMsgMetaErrorFromAddrBufferTooSmall;
       }
-      copy_into_and_advance_by(to_addr, cpp20::span<uint8_t>(from_addr.buf, from_addr.buf_size));
+      copy_into(to_addr, cpp20::span<uint8_t>(from_addr.buf, from_addr.buf_size));
       ipv4_socket_addr.port = meta.port;
       socket_addr = fnet::wire::SocketAddress::WithIpv4(alloc, ipv4_socket_addr);
     } break;
@@ -248,7 +248,7 @@ SerializeRecvMsgMetaError serialize_recv_msg_meta(RecvMsgMeta meta, Buffer from_
       if (from_addr_span.size() != to_addr.size()) {
         return SerializeRecvMsgMetaErrorFromAddrBufferTooSmall;
       }
-      copy_into_and_advance_by(to_addr, from_addr_span);
+      copy_into(to_addr, from_addr_span);
       ipv6_socket_addr.port = meta.port;
       socket_addr = fnet::wire::SocketAddress::WithIpv6(alloc, ipv6_socket_addr);
     } break;
