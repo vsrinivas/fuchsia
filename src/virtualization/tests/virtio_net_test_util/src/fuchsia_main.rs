c5c940b1d547fbb13babd9678c43f683b2424b35
Amanda Tait <atait@google.com>
2022-05-12 20:12:50 +0000

    [netdevice] Add netdevice-migration tests
    Update virtio_net_test_util to support network-device interfaces.
    
    Multiply: virtualization_net_tests
    Test: fx test virtualization_net_tests
    Bug: 78293
    Fixed: 95259
    Change-Id: Ieca59c477933b5757ba85cbe2c523914d24f17e4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/646028
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: Tim Detwiler <tjdetwiler@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Peter Johnston <peterjohnston@google.com>
    Fuchsia-Auto-Submit: Joon-Hyuk Han <hanjh@google.com>

--- src/virtualization/tests/virtio_net_test_util/src/fuchsia_main.rs
+++ src/virtualization/tests/virtio_net_test_util/src/fuchsia_main.rs
@@ -6,12 +6,13 @@ use ethernet as eth;
 use fidl_fuchsia_hardware_ethernet_ext::MacAddress;
 use fuchsia_async as fasync;
 use fuchsia_zircon as zx;
-use futures::TryStreamExt;
+use futures::{FutureExt as _, StreamExt as _, TryStreamExt as _};
 use std::fs::{self, File};
 use std::str::FromStr;
 use structopt::StructOpt;
 
 const ETH_DIRECTORY: &str = "/dev/class/ethernet";
+const NETDEV_DIRECTORY: &str = "/dev/class/network";
 
 #[derive(StructOpt, Debug)]
 struct Config {
@@ -41,17 +42,73 @@ async fn find_ethernet_device(mac: MacAddress) -> Result<eth::Client, anyhow::Er
     return Err(anyhow::format_err!("Could not find {}", mac));
 }
 
-#[fasync::run_singlethreaded]
-async fn main() -> Result<(), anyhow::Error> {
-    fuchsia_syslog::init()?;
-    fuchsia_syslog::set_severity(-1);
+async fn find_network_device(
+    mac: MacAddress,
+) -> (netdevice_client::Client, netdevice_client::Port) {
+    let (directory, directory_server) =
+        fidl::endpoints::create_proxy::<fidl_fuchsia_io::DirectoryMarker>().expect("create proxy");
+    fdio::service_connect(NETDEV_DIRECTORY, directory_server.into_channel().into())
+        .expect("connect to netdevice devfs");
+    let devices =
+        files_async::readdir(&directory).await.expect("readdir failed").into_iter().map(|file| {
+            let filepath = std::path::Path::new(NETDEV_DIRECTORY).join(&file.name);
+            let filepath = filepath
+                .to_str()
+                .unwrap_or_else(|| panic!("{} failed to convert to str", filepath.display()));
+            let (netdevice, netdevice_server) = fidl::endpoints::create_proxy::<
+                fidl_fuchsia_hardware_network::DeviceInstanceMarker,
+            >()
+            .expect("create proxy");
+            fdio::service_connect(filepath, netdevice_server.into_channel())
+                .expect("connect to service");
+            netdevice
+        });
+    let results = futures::stream::iter(devices).filter_map(|netdev_device| async move {
+        let (device_proxy, device_server) =
+            fidl::endpoints::create_proxy::<fidl_fuchsia_hardware_network::DeviceMarker>()
+                .expect("create proxy");
+        netdev_device.get_device(device_server).expect("get device");
+        let device_proxy = &device_proxy;
+        let client = netdevice_client::Client::new(Clone::clone(device_proxy));
 
-    let config = Config::from_args();
-    let mut eth_client = find_ethernet_device(MacAddress::from_str(&config.mac)?).await?;
+        let mut port_id = match client
+            .device_port_event_stream()
+            .expect("failed to get port event stream")
+            .try_next()
+            .await
+            .expect("error observing ports")
+            .expect("port stream ended unexpectedly")
+        {
+            fidl_fuchsia_hardware_network::DevicePortEvent::Existing(port_id) => port_id,
+            e @ fidl_fuchsia_hardware_network::DevicePortEvent::Removed(_)
+            | e @ fidl_fuchsia_hardware_network::DevicePortEvent::Idle(_)
+            | e @ fidl_fuchsia_hardware_network::DevicePortEvent::Added(_) => {
+                unreachable!("unexpected event: {:?}", e);
+            }
+        };
+        let (port, port_server) =
+            fidl::endpoints::create_proxy::<fidl_fuchsia_hardware_network::PortMarker>()
+                .expect("failed to create proxy");
+        device_proxy.get_port(&mut port_id, port_server).expect("failed to get port");
+        let (mac_addressing, mac_addressing_server) =
+            fidl::endpoints::create_proxy::<fidl_fuchsia_hardware_network::MacAddressingMarker>()
+                .expect("failed to create proxy");
+        port.get_mac(mac_addressing_server).expect("failed to get mac addressing");
+        let addr = mac_addressing.get_unicast_address().await.expect("failed to get address");
 
+        (addr.octets == mac.octets).then(move || (client, port_id.into()))
+    });
+    futures::pin_mut!(results);
+    results.next().await.unwrap_or_else(|| panic!("netdevice with mac {} not found", mac))
+}
+
+async fn ethernet_device_send(
+    mut client: eth::Client,
+    config: Config,
+) -> Result<(), anyhow::Error> {
     let buf = vec![config.send_byte; config.length];
-    eth_client.send(&buf);
-    let mut events = eth_client.get_stream();
+    client.send(&buf);
+    let mut events = client.get_stream();
 
     // Wait for reply.
     let mut buf = vec![0; config.length];
@@ -71,4 +128,49 @@ async fn main() -> Result<(), anyhow::Error> {
         println!("PASS");
     }
     Ok(())
+}
+
+async fn network_device_send(
+    client: netdevice_client::Client,
+    port: netdevice_client::Port,
+    config: Config,
+) {
+    let info = client.device_info().await.expect("get device info");
+    let (session, task) = client
+        .primary_session("test", info.max_buffer_length.unwrap().get() as usize)
+        .await
+        .expect("open primary session");
+    let _task_handle = fuchsia_async::Task::spawn(task.map(|r| r.expect("session task failed")));
+    session
+        .attach(port, vec![fidl_fuchsia_hardware_network::FrameType::Ethernet])
+        .await
+        .expect("attach port");
+    let mut buffer = session.alloc_tx_buffer(config.length).await.expect("allocate tx buffer");
+    buffer.set_frame_type(fidl_fuchsia_hardware_network::FrameType::Ethernet);
+    buffer.set_port(port);
+    let write_scratch = vec![config.send_byte; config.length];
+    let written = buffer.write_at(0, &write_scratch).expect("write message");
+    assert_eq!(written, config.length);
+    session.send(buffer).await.expect("send");
+    let recv_buf = session.recv().await.expect("receive");
+    let mut scratch = vec![0; config.length];
+    let read = recv_buf.read_at(0, &mut scratch).expect("read from buffer");
+    assert_eq!(read, config.length);
+    assert!(scratch.iter().all(|b| *b == config.receive_byte))
+}
+
+#[fasync::run_singlethreaded]
+async fn main() -> Result<(), anyhow::Error> {
+    fuchsia_syslog::init()?;
+    fuchsia_syslog::set_severity(fuchsia_syslog::levels::DEBUG);
+
+    let config = Config::from_args();
+    match find_ethernet_device(MacAddress::from_str(&config.mac)?).await {
+        Ok(client) => ethernet_device_send(client, config).await,
+        Err(_) => {
+            let (client, port) = find_network_device(MacAddress::from_str(&config.mac)?).await;
+            network_device_send(client, port, config).await;
+            Ok(())
+        }
+    }
 }
