56f36c44d9cbfff510012de169967f5fede66c10
Kendal Harland <kjharland@google.com>
2022-05-13 18:23:52 +0000

    [fidl] Use bless_goldens to update compatibility test goldens
    This is more convenient than editing the python script and
    rebuilding each time.
    
    Bug: 100126
    Change-Id: I8bb182caaeceae3a69ffb80df520154143f859a7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/678924
    Fuchsia-Auto-Submit: Kendal Harland <kjharland@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Adam Barth <abarth@google.com>

--- sdk/cts/build/scripts/fidl_api_compatibility_test.py
+++ sdk/cts/build/scripts/fidl_api_compatibility_test.py
@@ -13,6 +13,10 @@ import plasa_differ
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        '--update_golden',
+        help="Overwrites current with golden if they don't match.",
+        action='store_true')
+    parser.add_argument(
         '--api-level', help='The API level being tested', required=True)
     parser.add_argument(
         '--golden', help='Path to the golden file', required=True)
@@ -26,38 +30,33 @@ def main():
         required=True)
     args = parser.parse_args()
 
-    exit_code = 0
-
     differ = plasa_differ.PlasaDiffer(args.fidl_api_diff_path)
-
     breaking_changes = differ.find_breaking_changes_in_fragment_file(
         args.golden, args.current)
 
     if breaking_changes:
-        exit_code = 1
-        print(
-            "These changes are incompatible with API level {}:".format(
-                args.api_level))
-        for breaking_change in breaking_changes:
-            print(" - " + breaking_change)
-        print()
-        print("If possible, please make a soft transition instead.")
-        print("To allow a hard transition please run:")
         update_cmd = "  cp {} {}".format(
             os.path.abspath(args.current), os.path.abspath(args.golden))
-        print(update_cmd)
-        print()
-
-        # Hint: to update all files, uncomment the following lines and rebuild.
-        #
-        # exit_code = 0
-        # import subprocess
-        # subprocess.run(update_cmd.split())
+        if args.update_golden:
+            import subprocess
+            subprocess.run(update_cmd.split())
+        else:
+            print(
+                "These changes are incompatible with API level {}:".format(
+                    args.api_level))
+            for breaking_change in breaking_changes:
+                print(" - " + breaking_change)
+            print()
+            print("If possible, please make a soft transition instead.")
+            print("To allow a hard transition please run:")
+            print(update_cmd)
+            print()
+            return 1
 
     with open(args.stamp, 'w') as stamp_file:
         stamp_file.write('Golden!\n')
 
-    return exit_code
+    return 0
 
 
 if __name__ == '__main__':
