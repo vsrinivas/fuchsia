d0209882dfd1f5f91ca09f100993e5b31401c0f7
Benjamin Lerman <qsr@chromium.org>
2022-05-13 17:28:02 +0000

    [starnix] Add test for foreground process group exiting
    This test check that the foreground process group associated to a
    terminal is not stopped when the foregroup process group itself
    disappear.
    
    Change-Id: I1bb2b224202ce5d5fb6f3e228f88d7c2dff2a158
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/678784
    Reviewed-by: Adam Barth <abarth@google.com>
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/proc/tests/chromiumos/syscalls/terminal_test.cc
+++ src/proc/tests/chromiumos/syscalls/terminal_test.cc
@@ -28,30 +28,96 @@ int received_signal = -1;
 
 void sig_hup(int signo) { received_signal = signo; }
 
-bool reap_children() {
+int reap_children() {
   for (;;) {
     int wstatus;
     if (waitpid(-1, &wstatus, 0) == -1) {
       if (errno == ECHILD) {
         // No more child, reaping is done.
-        return true;
+        return 0;
       }
       // Another error is unexpected.
       perror("reap_children");
-      return false;
+      return -1;
     }
     if (!WIFEXITED(wstatus)) {
       fprintf(stderr, "Child process did not exit normally.\n");
-      return false;
+      return -1;
     }
     if (WEXITSTATUS(wstatus) != 0) {
       fprintf(stderr, "Child process did exit with an error: %d.\n", WEXITSTATUS(wstatus));
-      return false;
+      return -1;
     }
   }
 }
 
-TEST(OrphanedProcessGroups, OrphanedProcessGroupsReceivesSignal) {
+int open_main_terminal() {
+  int fd = SAFE_SYSCALL(posix_openpt(O_RDWR));
+  SAFE_SYSCALL(grantpt(fd));
+  SAFE_SYSCALL(unlockpt(fd));
+  return fd;
+}
+
+TEST(JobControl, BackgroundProcessGroupDoNotUpdateOnDeath) {
+  // Reap children.
+  prctl(PR_SET_CHILD_SUBREAPER, 1);
+
+  struct sigaction action;
+  action.sa_handler = SIG_IGN;
+  SAFE_SYSCALL(sigaction(SIGTTOU, &action, nullptr));
+
+  if (SAFE_SYSCALL(fork()) == 0) {
+    SAFE_SYSCALL(setsid());
+    int main_terminal = open_main_terminal();
+    int replica_terminal = SAFE_SYSCALL(open(ptsname(main_terminal), O_RDWR));
+
+    if (SAFE_SYSCALL(tcgetpgrp(replica_terminal)) != getpid()) {
+      fprintf(stderr, "Expected foreground process group (%d) to be the current one (%d).\n",
+              tcgetpgrp(replica_terminal), getpid());
+      exit(-1);
+    }
+    pid_t child_pid;
+    if ((child_pid = SAFE_SYSCALL(fork())) == 0) {
+      SAFE_SYSCALL(setpgid(0, 0));
+      SAFE_SYSCALL(tcsetpgrp(replica_terminal, getpid()));
+
+      if (SAFE_SYSCALL(tcgetpgrp(replica_terminal)) != getpid()) {
+        fprintf(stderr, "Expected foreground process group (%d) to be the current one (%d).\n",
+                tcgetpgrp(replica_terminal), getpid());
+        exit(-1);
+      }
+    } else {
+      SAFE_SYSCALL(setpgid(child_pid, child_pid));
+      if (reap_children() != 0) {
+        exit(-1);
+      }
+
+      if (SAFE_SYSCALL(tcgetpgrp(replica_terminal)) != child_pid) {
+        fprintf(stderr, "Expected foreground process group (%d) to be the current one (%d).\n",
+                tcgetpgrp(replica_terminal), child_pid);
+        exit(-1);
+      }
+
+      if (setpgid(0, child_pid) != -1) {
+        fprintf(stderr,
+                "Expected not being able to join a process group that has no member anymore\n");
+        exit(-1);
+      }
+      if (errno != EPERM) {
+        fprintf(stderr, "Unexpected errnor. Expected EPERM (%d), got %d\n", EPERM, errno);
+        exit(-1);
+      }
+    }
+
+    // Ensure all forked process will exit and not reach back to gtest.
+    exit(0);
+  } else {
+    // Wait for all children to die.
+    ASSERT_EQ(0, reap_children());
+  }
+}
+
+TEST(JobControl, OrphanedProcessGroupsReceivesSignal) {
   // Reap children.
   prctl(PR_SET_CHILD_SUBREAPER, 1);
 
@@ -78,7 +144,7 @@ TEST(OrphanedProcessGroups, OrphanedProcessGroupsReceivesSignal) {
         SAFE_SYSCALL(getpid());
         if (received_signal != SIGHUP) {
           fprintf(stderr, "Did not received expected SIGHUP\n");
-          exit(1);
+          exit(-1);
         }
       } else {
         // Wait for the child to have stopped.
@@ -86,16 +152,14 @@ TEST(OrphanedProcessGroups, OrphanedProcessGroupsReceivesSignal) {
       }
     } else {
       // Wait for the child to die and check it exited normally.
-      if (!reap_children()) {
-        exit(1);
-      }
+      exit(reap_children());
     }
 
     // Ensure all forked process will exit and not reach back to gtest.
     exit(0);
   } else {
     // Wait for all children to die.
-    ASSERT_TRUE(reap_children());
+    ASSERT_EQ(0, reap_children());
   }
 }
 
