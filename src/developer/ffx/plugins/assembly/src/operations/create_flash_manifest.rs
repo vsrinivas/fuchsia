7449c1e210fb1b1c65d31766091cb47b60e68099
Aidan Wolter <awolter@google.com>
2022-05-18 15:41:29 +0000

    [assembly] Generate a shell FlashManifest v3
    ffx assembly create-flash-manifest now uses the ffx_fastboot crate to
    construct a FlashManifest. The contents of the FlashManifest are
    currently stubbed out.
    
    Bug: 97963
    Change-Id: I688111337228a5b615a8fca13e60b8241152c7cb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/680121
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>

--- src/developer/ffx/plugins/assembly/src/operations/create_flash_manifest.rs
+++ src/developer/ffx/plugins/assembly/src/operations/create_flash_manifest.rs
@@ -5,8 +5,8 @@
 use anyhow::{Context, Result};
 use assembly_partitions_config::PartitionsConfig;
 use ffx_assembly_args::CreateFlashManifestArgs;
+use ffx_fastboot::manifest::{v3, FlashManifestVersion};
 use std::fs::File;
-use std::io::Write;
 
 pub fn create_flash_manifest(args: CreateFlashManifestArgs) -> Result<()> {
     let mut file = File::open(&args.partitions)
@@ -14,9 +14,14 @@ pub fn create_flash_manifest(args: CreateFlashManifestArgs) -> Result<()> {
     let _partitions = PartitionsConfig::from_reader(&mut file)
         .context("Failed to parse the partitions config")?;
 
+    let manifest = FlashManifestVersion::V3(v3::FlashManifest {
+        hw_revision: "board".into(),
+        ..Default::default()
+    });
     let flash_manifest_path = args.outdir.join("flash.json");
     let mut flash_manifest_file = File::create(&flash_manifest_path)
         .context(format!("Failed to create: {}", flash_manifest_path.display()))?;
-    flash_manifest_file.write_all(b"{}")?;
+    manifest.write(&mut flash_manifest_file)?;
+
     Ok(())
 }
