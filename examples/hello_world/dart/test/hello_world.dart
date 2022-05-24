7af5f957e8978abb7ef6c1ef0775ec8ab0018b59
naudzghebre <naudzghebre@google.com>
2022-05-24 02:41:51 +0000

    [dart test runner] Dart test runner implementation.
    - The dart runner implements the suite protocol as of :
      https://github.com/flutter/engine/pull/32751.
    - This cl attaches the test_runtime shard to all dart v2
      tests.
    - An example hello world test is included.
    - Tests using dart_test_wrapper_component() build rule are replaced with
      this implementation.
    
    Bug: 92745
    
    Change-Id: I27e53622c52b1265759e04e7eb096f9de434d814
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/660762
    Reviewed-by: Ankur Mittal <anmittal@google.com>
    Commit-Queue: Naud Ghebre <naudzghebre@google.com>
    Reviewed-by: Alexander Biggs <akbiggs@google.com>
    Reviewed-by: Allison Pearce <ampearce@google.com>

---
+++ examples/hello_world/dart/test/hello_world.dart
@@ -0,0 +1,17 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+void main() {
+  test('Hello World: Dart Test 1', () {
+    print('Hello, World!');
+    expect(1, 1);
+  });
+
+  test('Hello World: Dart Test 2', () {
+    print('Hello, World!');
+    expect(2, 2);
+  });
+}
