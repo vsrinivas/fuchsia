8be4d65af7357ece345be651df0a16426527d887
Kevin Twidle <kpt@google.com>
2022-05-25 06:00:57 +0000

    [Recovery] Make progress bar move slowly and improve layout
    Working nicely, progress bar now moves smoothly. It is
    interruptible should a new value be set whilst it it still
    updating.
    
    Example layout includes Fuchsia logo and centred progress bar.
    
    Test: No new tests added
    Bug: b/226530276, b/216852554
    Fixes: b/226530276
    Change-Id: Ie8fbb096ad57e0596f5c37500efa88e748346f4e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/678626
    Reviewed-by: Mike "mc" Comfoltey <comfoltey@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Kevin Twidle <kpt@google.com>

--- src/recovery/system/src/progress_bar.rs
+++ src/recovery/system/src/progress_bar.rs
@@ -3,32 +3,66 @@
 // found in the LICENSE file.
 
 use anyhow::Error;
-use carnelian::scene::facets::TextHorizontalAlignment;
+use carnelian::scene::facets::SetTextMessage;
 use carnelian::{
     color::Color,
     drawing::{load_font, measure_text_width},
+    make_message,
     scene::{
         facets::{
-            FacetId, SetColorMessage, SetSizeMessage, TextFacetOptions, TextVerticalAlignment,
+            FacetId, SetColorMessage, SetSizeMessage, TextFacetOptions, TextHorizontalAlignment,
+            TextVerticalAlignment,
         },
         layout::{Alignment, Stack, StackOptions},
         scene::{Scene, SceneBuilder},
     },
-    Point, Size,
+    AppSender, MessageTarget, Point, Size, ViewKey,
 };
 use euclid::size2;
-use std::path::PathBuf;
+use fuchsia_async::futures::channel::mpsc::{channel as pipe, Sender};
+use fuchsia_async::futures::StreamExt;
+use fuchsia_async::{self as fasync};
+use std::sync::Mutex;
+use std::time::Duration;
+use std::{
+    path::PathBuf,
+    sync::{
+        atomic::{AtomicU32, Ordering},
+        Arc,
+    },
+};
+
+const PROGRESS_GRANULARITY: f32 = 1000.0;
+
+pub enum ProgressBarMessages {
+    #[allow(unused)]
+    SetProgress(f32),
+    #[allow(unused)]
+    SetProgressSmooth(f32),
+    SetInternalProgress(f32),
+    #[allow(unused)]
+    SetProgressBarText(String),
+}
 
 pub struct ProgressBar {
     label: Option<FacetId>,
     progress_bar_size: Size,
     progress_rectangle: FacetId,
     background_rectangle: FacetId,
+    app_sender: AppSender,
+    // Progress stored as 0 to PROGRESS_GRANULARITY
+    current_progress: Arc<AtomicU32>,
+    // Progress stored as 0 to PROGRESS_GRANULARITY
+    final_progress: Arc<AtomicU32>,
+    // To make sure that we are only starting one copy of the background task
+    task_running_mutex: Arc<Mutex<bool>>,
+    // Channel to send desired progress to the beckground task
+    task_sender: Option<Sender<f32>>,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 pub struct ProgressBarText {
-    text: &'static str,
+    text: String,
     font_size: f32,
     padding: f32,
     alignment: TextHorizontalAlignment,
@@ -40,6 +74,7 @@ impl ProgressBar {
         text: Option<ProgressBarText>,
         mut size: Option<Size>,
         builder: &mut SceneBuilder,
+        app_sender: AppSender,
     ) -> Result<ProgressBar, Error> {
         // We calculate the size from a size option or from displayed text, we must have at least one
         assert!(text.is_some() || size.is_some());
@@ -54,7 +89,7 @@ impl ProgressBar {
             let face = load_font(PathBuf::from("/pkg/data/fonts/Roboto-Regular.ttf"))?;
             label = Some(builder.text(
                 face.clone(),
-                progress_bar_text.text,
+                &progress_bar_text.text,
                 progress_bar_text.font_size,
                 Point::zero(),
                 TextFacetOptions {
@@ -66,7 +101,7 @@ impl ProgressBar {
             ));
             if size.is_none() {
                 let label_width =
-                    measure_text_width(&face, progress_bar_text.font_size, progress_bar_text.text);
+                    measure_text_width(&face, progress_bar_text.font_size, &progress_bar_text.text);
                 let width = label_width + progress_bar_text.padding * 2.0;
                 let height = progress_bar_text.font_size + progress_bar_text.padding * 2.0;
                 size = Some(size2(width, height));
@@ -82,27 +117,37 @@ impl ProgressBar {
             builder.rectangle(progress_bar_size, Color::from_hash_code("#000000")?);
         builder.end_group();
 
-        Ok(ProgressBar { label, progress_bar_size, progress_rectangle, background_rectangle })
+        Ok(ProgressBar {
+            label,
+            progress_bar_size,
+            progress_rectangle,
+            background_rectangle,
+            app_sender,
+            current_progress: Arc::new(AtomicU32::new(0)),
+            final_progress: Arc::new(AtomicU32::new(0)),
+            task_running_mutex: Arc::new(Mutex::new(false)),
+            task_sender: None,
+        })
     }
 
-    fn clamp<T: std::cmp::PartialOrd>(value: T, min: T, max: T) -> T {
-        if value < min {
-            min
-        } else if value > max {
-            max
-        } else {
-            value
-        }
-    }
-
+    /// Set progress between 0 to 100 percent
     #[allow(unused)]
     pub fn set_percent(&mut self, scene: &mut Scene, percent_filled: f32) {
         self.set_progress(scene, percent_filled / 100.0);
     }
 
+    /// Set progress from 0.0 to 1.0
     #[allow(unused)]
     pub fn set_progress(&mut self, scene: &mut Scene, progress: f32) {
-        let progress = ProgressBar::clamp(progress, 0.0, 1.0);
+        println!("Set progress {}", progress);
+        self.final_progress.store((progress * PROGRESS_GRANULARITY) as u32, Ordering::Release);
+        self.current_progress.store((progress * PROGRESS_GRANULARITY) as u32, Ordering::Release);
+        self.set_internal_progress(scene, progress);
+    }
+
+    /// Set progress from 0 to 1.0
+    fn set_internal_progress(&mut self, scene: &mut Scene, progress: f32) {
+        let progress = num_traits::clamp(progress, 0.0, 1.0);
         let width = self.progress_bar_size.width;
         let height = self.progress_bar_size.height;
         let filled = width * progress;
@@ -113,12 +158,80 @@ impl ProgressBar {
     }
 
     #[allow(unused)]
+    pub fn set_percent_smooth(&mut self, view_key: ViewKey, percent_filled: f32) {
+        self.set_progress_smooth(view_key, percent_filled / 100.0);
+    }
+
+    /// Set progress from 0.0 to 1.0
+    #[allow(unused)]
+    pub fn set_progress_smooth(&mut self, view_key: ViewKey, progress: f32) {
+        let mut running = self.task_running_mutex.lock().unwrap();
+        if !*running {
+            *running = true;
+            let app_sender = self.app_sender.clone();
+            let current_progress = self.current_progress.clone();
+            let final_progress = self.final_progress.clone();
+            let (tx, mut rx) = pipe::<f32>(1);
+            self.task_sender = Some(tx);
+            let f = async move {
+                let sleep_time = Duration::from_millis(5);
+                loop {
+                    let current = current_progress.load(Ordering::Acquire);
+
+                    let end = final_progress.load(Ordering::Acquire);
+                    let difference = end as i64 - current as i64;
+                    if difference == 0 {
+                        let progress = rx.next().await.unwrap();
+                        final_progress
+                            .store((progress * PROGRESS_GRANULARITY) as u32, Ordering::Release);
+                    } else {
+                        match rx.try_next() {
+                            Ok(value) => {
+                                if let Some(progress) = value {
+                                    final_progress.store(
+                                        (progress * PROGRESS_GRANULARITY) as u32,
+                                        Ordering::Release,
+                                    );
+                                }
+                            }
+                            Err(_) => { // Ignore}
+                            }
+                        }
+                        if difference > 0 {
+                            current_progress.fetch_add(1, Ordering::AcqRel);
+                        } else {
+                            current_progress.fetch_sub(1, Ordering::AcqRel);
+                        }
+                        app_sender.queue_message(
+                            MessageTarget::View(view_key),
+                            make_message(ProgressBarMessages::SetInternalProgress(
+                                current_progress.load(Ordering::Acquire) as f32
+                                    / PROGRESS_GRANULARITY,
+                            )),
+                        );
+                        fuchsia_async::Timer::new(sleep_time).await;
+                    }
+                }
+            };
+            fasync::Task::local(f).detach();
+        }
+        let _ = self.task_sender.as_mut().unwrap().start_send(progress);
+    }
+
+    #[allow(unused)]
     fn set_color(&mut self, scene: &mut Scene, fg_color: Color, bg_color: Color) {
         scene.send_message(&self.progress_rectangle, Box::new(SetColorMessage { color: fg_color }));
         scene.send_message(
             &self.background_rectangle,
             Box::new(SetColorMessage { color: bg_color }),
         );
+    }
+
+    #[allow(unused)]
+    fn set_text(&mut self, scene: &mut Scene, text: String) {
+        if let Some(label) = self.label.as_ref() {
+            scene.send_message(label, Box::new(SetTextMessage { text }));
+        }
     }
 
     #[allow(unused)]
@@ -154,12 +267,14 @@ impl ProgressBar {
 
 //#[cfg(progress_bar_stand_alone)]
 pub(crate) mod stand_alone {
-    use crate::progress_bar::{ProgressBar, ProgressBarText};
+    use crate::progress_bar::{ProgressBar, ProgressBarMessages, ProgressBarText};
     use crate::BG_COLOR;
     use anyhow::Error;
     use carnelian::app::Config;
     use carnelian::drawing::DisplayRotation;
-    use carnelian::scene::facets::TextHorizontalAlignment;
+    use carnelian::render::rive::load_rive;
+    use carnelian::scene::facets::{RiveFacet, TextHorizontalAlignment};
+    use carnelian::scene::layout::CrossAxisAlignment; //Flex, FlexOptions, MainAxisSize};
     use carnelian::{
         input, make_message,
         render::Context,
@@ -171,23 +286,22 @@ pub(crate) mod stand_alone {
         Message, MessageTarget, Size, ViewAssistant, ViewAssistantContext, ViewAssistantPtr,
         ViewKey,
     };
-    use euclid::size2;
-    use fuchsia_async::{self as fasync, DurationExt};
-    use fuchsia_zircon::{Duration, Event};
+    use euclid::{size2, Size2D};
+    use fuchsia_zircon::Event;
+    use rive_rs::File;
+    use std::time::Instant;
 
-    pub enum ProgressBarMessages {
-        SetProgress(f32),
-        EndTask,
-    }
+    const LOGO_IMAGE_PATH: &str = "/pkg/data/logo.riv";
 
     // This assistant is for using as an example
     pub struct ProgressBarViewAssistant {
         app_sender: AppSender,
         view_key: ViewKey,
         scene: Option<Scene>,
+        logo_file: Option<File>,
         progress_bar: Option<ProgressBar>,
         percent_complete: f32,
-        demo_running: bool,
+        touch_time: Instant,
     }
 
     impl ProgressBarViewAssistant {
@@ -196,13 +310,15 @@ pub(crate) mod stand_alone {
             view_key: ViewKey,
             percent_complete: f32,
         ) -> Result<ProgressBarViewAssistant, Error> {
+            let logo_file = load_rive(LOGO_IMAGE_PATH).ok();
             Ok(ProgressBarViewAssistant {
                 app_sender: app_sender.clone(),
                 view_key,
                 scene: None,
+                logo_file,
                 progress_bar: None,
                 percent_complete,
-                demo_running: false,
+                touch_time: Instant::now(),
             })
         }
     }
@@ -225,30 +341,44 @@ pub(crate) mod stand_alone {
             if self.scene.is_none() {
                 let target_size = view_context.size;
                 let min_dimension = target_size.width.min(target_size.height);
+                let logo_edge = min_dimension * 0.24;
                 let text_size = min_dimension / 14.0;
 
                 let mut builder =
                     SceneBuilder::new().background_color(BG_COLOR).round_scene_corners(true);
                 let text = ProgressBarText {
-                    text: "Progress Bar",
+                    text: format!("Progress Bar {}%   ", (self.percent_complete * 100.0) as i32),
                     font_size: text_size,
                     padding: 7.0,
-                    alignment: TextHorizontalAlignment::Right,
+                    alignment: TextHorizontalAlignment::Left,
                 };
                 builder
                     .group()
                     .column()
                     .max_size()
                     .main_align(MainAxisAlignment::SpaceEvenly)
+                    .cross_align(CrossAxisAlignment::Center)
                     .contents(|builder| {
+                        let logo_size: Size = size2(logo_edge, logo_edge);
+                        if let Some(logo_file) = self.logo_file.as_ref() {
+                            let facet = RiveFacet::new_from_file(logo_size, logo_file, None)
+                                .expect("facet_from_file");
+                            builder.facet(Box::new(facet));
+                        }
                         let progress_bar = ProgressBar::new(
                             self.percent_complete,
-                            Some(text),
+                            Some(text.clone()),
                             Some(size2(target_size.width / 2.0, target_size.height / 15.0)),
                             builder,
+                            self.app_sender.clone(),
                         )
                         .expect("Progress Bar");
                         self.progress_bar = Some(progress_bar);
+                        builder.space(Size2D {
+                            width: target_size.width,
+                            height: target_size.height / 5.0,
+                            _unit: Default::default(),
+                        });
                     });
                 let mut scene = builder.build();
                 scene.layout(target_size);
@@ -273,8 +403,26 @@ pub(crate) mod stand_alone {
                             progress_bar.set_progress(scene, progress);
                         }
                     }
-                    ProgressBarMessages::EndTask => {
-                        self.demo_running = false;
+                    ProgressBarMessages::SetProgressSmooth(progress) => {
+                        if let Some(progress_bar) = &mut self.progress_bar {
+                            self.percent_complete = progress * 100.0;
+                            progress_bar.set_progress_smooth(self.view_key, progress);
+                        }
+                    }
+                    // This is used by the slow progress task and needs to be included in code as-is
+                    ProgressBarMessages::SetInternalProgress(progress) => {
+                        if let (Some(progress_bar), Some(scene)) =
+                            (&mut self.progress_bar, &mut self.scene)
+                        {
+                            progress_bar.set_internal_progress(scene, progress);
+                        }
+                    }
+                    ProgressBarMessages::SetProgressBarText(text) => {
+                        if let (Some(progress_bar), Some(scene)) =
+                            (&mut self.progress_bar, &mut self.scene)
+                        {
+                            progress_bar.set_text(scene, text);
+                        }
                     }
                 }
             }
@@ -286,28 +434,20 @@ pub(crate) mod stand_alone {
             _event: &input::Event,
             _touch_event: &input::touch::Event,
         ) -> Result<(), Error> {
-            if !self.demo_running {
-                self.demo_running = true;
-                let view_key = self.view_key;
-                let app_sender = self.app_sender.clone();
-                let f = async move {
-                    let sleep_time = Duration::from_millis(50);
-                    let mut count = 0.0;
-                    while count <= 1.0 {
-                        app_sender.queue_message(
-                            MessageTarget::View(view_key),
-                            make_message(ProgressBarMessages::SetProgress(count as f32)),
-                        );
-                        fuchsia_async::Timer::new(sleep_time.after_now()).await;
-                        count += 0.0025;
-                    }
-                    app_sender.queue_message(
-                        MessageTarget::View(view_key),
-                        make_message(ProgressBarMessages::EndTask),
-                    );
-                };
-                fasync::Task::local(f).detach();
+            // Debounce the screen touch
+            if self.touch_time.elapsed().as_millis() > 100 {
+                let r = rand::random::<f32>();
+                self.app_sender.queue_message(
+                    MessageTarget::View(self.view_key),
+                    make_message(ProgressBarMessages::SetProgressSmooth(r)),
+                );
+                let text = format!("Progress Bar {}%", (r * 100.0) as i32);
+                self.app_sender.queue_message(
+                    MessageTarget::View(self.view_key),
+                    make_message(ProgressBarMessages::SetProgressBarText(text)),
+                );
             }
+            self.touch_time = Instant::now();
             Ok(())
         }
     }
