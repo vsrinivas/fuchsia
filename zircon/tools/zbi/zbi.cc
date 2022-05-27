gongcm/fuchisa-os

[zbi] Add support for riscv64
Change-Id: I39a7698670b59945d6b1e95a198f39be70966923

zircon/tools/zbi/zbi.cc
@@ -2506,10 +2506,12 @@ int main(int argc, char** argv) {
           complete_arch = ZBI_TYPE_KERNEL_X64;
         } else if (!strcmp(optarg, "arm64")) {
           complete_arch = ZBI_TYPE_KERNEL_ARM64;
+        } else if (!strcmp(optarg, "riscv64")) {
+          complete_arch = ZBI_TYPE_KERNEL_RISCV64;
         } else {
           fprintf(stderr,
                   "--complete architecture argument must be one"
-                  " of: x64, arm64\n");
+                  " of: x64, arm64, riscv64\n");
           exit(1);
         }
         continue;

vsrinivas/fuchsia

zircon/tools/zbi/zbi.cc
@@ -2612,10 +2612,12 @@ int main(int argc, char** argv) {
           bootable = ZBI_TYPE_KERNEL_X64;
         } else if (!strcmp(optarg, "arm64")) {
           bootable = ZBI_TYPE_KERNEL_ARM64;
+        } else if (!strcmp(optarg, "riscv64")) {
+          bootable = ZBI_TYPE_KERNEL_RISCV64;
         } else {
           fprintf(stderr,
                   "--bootable architecture argument must be one"
-                  " of: x64, arm64\n");
+                  " of: x64, arm64, riscv64\n");
           exit(1);
         }
         continue;