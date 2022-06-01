60618fbe6a5150e172ca9c5617a0e381b998d6a8
Martin Puryear <mpuryear@google.com>
2022-06-01 01:49:54 +0000

    [audio][test] Clarify HermeticPipelineTest logging
    For all test suites that use hermetic_pipeline_test_main, set
    a logging tag based on the binary name (after stripping off
    any prepended path).
    
    fx test -do audio-renderer-pipeline-test
                audio_core_fidelity_tests
    
    Change-Id: I2fa4c163f81720c2b48bc45dbfff4b6f78ac3a59
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/685425
    Reviewed-by: Tom Bergan <tombergan@google.com>
    Commit-Queue: Martin Puryear <mpuryear@google.com>
    Reviewed-by: Alper Gungormusler <alperg@google.com>

--- src/media/audio/lib/test/hermetic_pipeline_test_main.cc
+++ src/media/audio/lib/test/hermetic_pipeline_test_main.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/syslog/cpp/log_settings.h>
+
 #include <gtest/gtest.h>
 
 #include "src/lib/fxl/command_line.h"
@@ -9,17 +11,33 @@
 #include "src/lib/fxl/test/test_settings.h"
 #include "src/media/audio/lib/test/hermetic_pipeline_test.h"
 
+namespace {
+
+// If a binary name was specified, set it as the syslog tag (after stripping any prepended dirs).
+void SetSyslogTag(fxl::CommandLine cmdline) {
+  if (cmdline.has_argv0()) {
+    auto tag = cmdline.argv0();
+    auto last_separator_index = tag.rfind('/');
+    if (last_separator_index < tag.npos) {
+      tag = tag.substr(last_separator_index + 1);
+    }
+    syslog::SetTags({tag});
+  }
+}
+
+}  // namespace
+
 int main(int argc, char** argv) {
-  if (!fxl::SetTestSettings(argc, argv)) {
+  auto cmdline = fxl::CommandLineFromArgcArgv(argc, argv);
+  if (!fxl::SetTestSettings(cmdline)) {
     return EXIT_FAILURE;
   }
 
   testing::InitGoogleTest(&argc, argv);
+  SetSyslogTag(cmdline);
 
-  auto cl = fxl::CommandLineFromArgcArgv(argc, argv);
-  fxl::SetLogSettingsFromCommandLine(cl);
   media::audio::test::HermeticPipelineTest::save_input_and_output_files_ =
-      cl.HasOption("save-inputs-and-outputs");
+      cmdline.HasOption("save-inputs-and-outputs");
 
   auto result = RUN_ALL_TESTS();
 
