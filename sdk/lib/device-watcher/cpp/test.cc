670c2169a9c151a4b7491b64a2c5d27ab647b551
Michael Brunson <mbrunson@google.com>
2022-06-07 14:29:04 +0000

    [device-watcher][cpp] Add function to wait for device topological path.
    Adds a function to wait for a device to appear with a specific
    topological path in a directory.
    Adds test case.
    
    Test: fx set ... --with //sdk/lib/device-watcher/cpp:tests && fx build
          fx test device-watcher-cpp-test
    Fixed: 93928
    Change-Id: I264aefab573c7f9439c707cee05099eda3630ede
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/682028
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- sdk/lib/device-watcher/cpp/test.cc
+++ sdk/lib/device-watcher/cpp/test.cc
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fidl/fuchsia.device/cpp/wire.h>
+#include <fidl/fuchsia.device/cpp/wire_test_base.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
 #include <lib/fdio/directory.h>
 #include <lib/fdio/fd.h>
+#include <zircon/time.h>
 
 #include <memory>
 
@@ -16,6 +19,7 @@
 #include "src/lib/storage/vfs/cpp/managed_vfs.h"
 #include "src/lib/storage/vfs/cpp/pseudo_dir.h"
 #include "src/lib/storage/vfs/cpp/pseudo_file.h"
+#include "src/lib/storage/vfs/cpp/service.h"
 
 TEST(DeviceWatcherTest, Smoke) {
   async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
@@ -66,4 +70,67 @@ TEST(DeviceWatcherTest, OpenInNamespace) {
   ASSERT_EQ(device_watcher::RecursiveWaitForFile("/dev/sys/test", &f), ZX_OK);
 
   ASSERT_EQ(device_watcher::RecursiveWaitForFile("/other-test/file", &f), ZX_ERR_NOT_SUPPORTED);
+}
+
+constexpr std::string_view kTopoPath = "/dev/test/device/out";
+
+class ControllerImpl : public fidl::testing::WireTestBase<fuchsia_device::Controller>,
+                       public fs::Service {
+ public:
+  explicit ControllerImpl(std::string_view topo_path, async_dispatcher_t* dispatcher)
+      : fs::Service([dispatcher, this](fidl::ServerEnd<fuchsia_device::Controller> server) {
+          fidl::BindServer(dispatcher, std::move(server), this);
+          return ZX_OK;
+        }),
+        topo_path_(topo_path) {}
+
+  void GetTopologicalPath(GetTopologicalPathRequestView request,
+                          GetTopologicalPathCompleter::Sync& completer) override {
+    completer.ReplySuccess(fidl::StringView::FromExternal(topo_path_));
+  }
+
+  void NotImplemented_(const std::string& name, fidl::CompleterBase& completer) override {
+    FAIL("Unexpected call to ControllerImpl: %s", name.c_str());
+  }
+
+ private:
+  std::string topo_path_;
+};
+
+TEST(DeviceWatcherTest, WaitForDeviceTopologicalPath) {
+  async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
+
+  auto empty_file = fbl::MakeRefCounted<fs::UnbufferedPseudoFile>(
+      [](fbl::String* output) { return ZX_OK; }, [](std::string_view input) { return ZX_OK; });
+
+  auto controller1 =
+      fbl::MakeRefCounted<ControllerImpl>("/dev/test/not/the/one", loop.dispatcher());
+  auto controller2 = fbl::MakeRefCounted<ControllerImpl>(kTopoPath, loop.dispatcher());
+
+  auto first = fbl::MakeRefCounted<fs::PseudoDir>();
+  first->AddEntry("file", empty_file);
+  first->AddEntry("000", empty_file);
+  first->AddEntry("001", controller1);
+  first->AddEntry("002", controller2);
+  first->AddEntry("003", empty_file);
+
+  auto endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+  ASSERT_EQ(ZX_OK, endpoints.status_value());
+
+  loop.StartThread();
+  fs::ManagedVfs vfs(loop.dispatcher());
+
+  vfs.ServeDirectory(first, std::move(endpoints->server));
+
+  fbl::unique_fd dir;
+  ASSERT_EQ(ZX_OK,
+            fdio_fd_create(endpoints->client.TakeChannel().release(), dir.reset_and_get_address()));
+
+  zx::status<zx::channel> status =
+      device_watcher::WaitForDeviceTopologicalPath(dir, std::string(kTopoPath).c_str());
+  ASSERT_TRUE(status.is_ok());
+
+  auto ctrl_client_end = fidl::ClientEnd<fuchsia_device::Controller>(std::move(status.value()));
+  fidl::WireResult result = fidl::WireCall(ctrl_client_end)->GetTopologicalPath();
+  ASSERT_EQ(kTopoPath, result->value()->path.get());
 }
