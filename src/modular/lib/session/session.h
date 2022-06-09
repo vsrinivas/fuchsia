27f183345d4a548dfc4202c8ce13aac2d4f2cc92
Yegor Pomortsev <ypomortsev@google.com>
2022-06-09 18:38:47 +0000

    [modular][cleanup] Remove most of //src/modular/lib/session
    This library was used in basemgr_launcher to launch sessions,
    and is dead code now that basemgr_launcher has been removed.
    
    ConnectToBasemgrDebug is still used in sessionctl to restart the
    session. This will be removed once `sessionctl restart_session`
    is removed.
    
    Change-Id: I6194c3a2a74aafbfaad67f10945ac1b45919db40
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/688584
    Reviewed-by: Yaneury Fermin <yaneury@google.com>
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>

--- src/modular/lib/session/session.h
+++ src/modular/lib/session/session.h
@@ -6,67 +6,9 @@
 #define SRC_MODULAR_LIB_SESSION_SESSION_H_
 
 #include <fuchsia/modular/internal/cpp/fidl.h>
-#include <fuchsia/modular/session/cpp/fidl.h>
-#include <lib/async/dispatcher.h>
-#include <lib/fpromise/promise.h>
 #include <lib/fpromise/result.h>
 
 namespace modular::session {
-
-enum class BasemgrRuntimeState {
-  // basemgr is running as a v2 session.
-  kV2Session,
-
-  // basemgr is running as a v1 component.
-  kV1Component,
-};
-
-// Returns the state of a running Modular instance, or std::nullopt if not running.
-std::optional<BasemgrRuntimeState> GetBasemgrRuntimeState();
-
-// Returns true if basemgr is running, either as a v1 component or a v2 session.
-bool IsBasemgrRunning();
-
-// Launches Modular with the given configuration.
-//
-// If there is a running session that exposes the |fuchsia.modular.session.Launcher| protocol,
-// it is used to instruct the session to launch sessionmgr. The protocol must exist in a path
-// under /hub-v2 in the process namespace. If the protocol is not available, launches basemgr
-// as a v1 component.
-//
-// If basemgr is already running as a v1 component, it will be shut down first.
-// If basemgr is already running as a session, it will *not* be shut down to ensure that
-// it can be used to launch sessionmgr.
-//
-// |launcher| and |dispatcher| are only used to launch basemgr as a v1 component.
-// |dispatcher| is used to serve the component's incoming directory.
-// If |dispatcher| is null, the current thread must have a default async_dispatcher_t.
-fpromise::promise<void, zx_status_t> Launch(fuchsia::sys::Launcher* launcher,
-                                            fuchsia::modular::session::ModularConfig config,
-                                            async_dispatcher_t* dispatcher = nullptr);
-
-// Launches basemgr as a v1 component with the given configuration.
-//
-// |dispatcher| is used to serve the component's incoming directory.
-// If |dispatcher| is null, the current thread must have a default async_dispatcher_t.
-fpromise::promise<void, zx_status_t> LaunchBasemgrV1(
-    fuchsia::sys::Launcher* launcher, fuchsia::modular::session::ModularConfig config,
-    async_dispatcher_t* dispatcher = nullptr);
-
-// Launches an instance of sessionmgr with the given configuration using the
-// |fuchsia.modular.session.Launcher| protocol exposed by a session.
-//
-// The session's |Launcher| protocol is found in a path under /hub-v2
-// in this component's namespace.
-//
-// # Errors
-//
-// ZX_ERR_NOT_FOUND: session is not running or it does not expose |Launcher|
-fpromise::result<void, zx_status_t> LaunchSessionmgr(
-    fuchsia::modular::session::ModularConfig config);
-
-// Shuts down any currently running instance of basemgr.
-fpromise::promise<void, zx_status_t> MaybeShutdownBasemgr();
 
 // Connects to the BasemgrDebug protocol served by the currently running instance of basemgr.
 //
--- src/modular/lib/session/session_constants.h
+++ src/modular/lib/session/session_constants.h
@@ -5,9 +5,6 @@
 #ifndef SRC_MODULAR_LIB_SESSION_SESSION_CONSTANTS_H_
 #define SRC_MODULAR_LIB_SESSION_SESSION_CONSTANTS_H_
 
-// Components v1 URL for basemgr.
-constexpr char kBasemgrV1Url[] = "fuchsia-pkg://fuchsia.com/basemgr#meta/basemgr.cmx";
-
 // Glob pattern for the path to basemgr's debug service when basemgr is running as a v1 component.
 constexpr char kBasemgrDebugV1Glob[] = "/hub/c/basemgr.cmx/*/out/debug/basemgr";
 
@@ -15,10 +12,5 @@ constexpr char kBasemgrDebugV1Glob[] = "/hub/c/basemgr.cmx/*/out/debug/basemgr";
 constexpr char kBasemgrDebugSessionGlob[] =
     "/hub-v2/children/core/children/session-manager/children/session:session/"
     "exec/expose/fuchsia.modular.internal.BasemgrDebug";
-
-// Glob pattern for the path to the |Launcher| protocol exposed by a session.
-constexpr char kLauncherGlob[] =
-    "/hub-v2/children/core/children/session-manager/children/session:session/"
-    "exec/expose/fuchsia.modular.session.Launcher";
 
 #endif  // SRC_MODULAR_LIB_SESSION_SESSION_CONSTANTS_H_
