536d473ef71480af2e3ffd00f3745e4ce3a9a1b4
Braden Kell <bradenkell@google.com>
2022-06-11 05:09:28 +0000

    [vim3][mcu] Move vim3-mcu from Banjo to FIDL for I2C
    Use FIDL in vim3-mcu-test, and change the bind files so that the driver
    only binds to the FIDL I2C device.
    
    Bug: 98515
    Multiply: vim3-mcu-test: 500
    Test: dm dump showed vim3-mcu binding to a FIDL I2C device on vim3
    Change-Id: I550d97391008f3bb082eefae757ab7fcd167ae70
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/689624
    Reviewed-by: Sam Hansen <hansens@google.com>
    Commit-Queue: Braden Kell <bradenkell@google.com>

--- src/devices/mcu/drivers/vim3-mcu/vim3-mcu-test.cc
+++ src/devices/mcu/drivers/vim3-mcu/vim3-mcu-test.cc
@@ -4,6 +4,8 @@
 
 #include "vim3-mcu.h"
 
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
 #include <lib/mock-i2c/mock-i2c.h>
 
 #include <fbl/vector.h>
@@ -13,7 +15,16 @@ namespace stm {
 TEST(Vim3McuTest, FanLevel) {
   mock_i2c::MockI2c mock_i2c;
   mock_i2c.ExpectWriteStop({0x88, 1}).ExpectWriteStop({0x21, 0x03});
-  ddk::I2cChannel i2c(mock_i2c.GetProto());
+
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+
+  auto endpoints = fidl::CreateEndpoints<fuchsia_hardware_i2c::Device>();
+  EXPECT_TRUE(endpoints.is_ok());
+
+  fidl::BindServer(loop.dispatcher(), std::move(endpoints->server), &mock_i2c);
+  EXPECT_OK(loop.StartThread());
+
+  ddk::I2cChannel i2c(std::move(endpoints->client));
   StmMcu device(nullptr, std::move(i2c));
   device.Init();
   mock_i2c.VerifyAndClear();
