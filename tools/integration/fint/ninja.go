36b35d2b965cafb420914108cd8db850efe95ace
Oliver Newman <olivernewman@google.com>
2022-06-13 20:16:31 +0000

    [fint] Never consider installer_test affected
    It depends on a system image so it is practically always considered
    affected by changes. It doesn't matter that much because it only runs on
    CI-only "slow" builders, but it's confusing for it to show up in fint's
    affected tests list nonetheless.
    
    Change-Id: Idf6ae49b59de694a9d3df6c944009e07a1f08992
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/690105
    Reviewed-by: Shai Barack <shayba@google.com>
    Fuchsia-Auto-Submit: Oliver Newman <olivernewman@google.com>

--- tools/integration/fint/ninja.go
+++ tools/integration/fint/ninja.go
@@ -74,6 +74,7 @@ var (
 		"//src/connectivity/overnet/tests/serial:overnet_serial_tests",
 		"//src/recovery/simulator:recovery_simulator_boot_test",
 		"//src/recovery/simulator:recovery_simulator_serial_test",
+		"//src/tests/installer:installer_test",
 		"//tools/fvdl/e2e:fvdl_intree_test_slirp",
 		"//tools/fvdl/e2e:fvdl_intree_test_tuntap",
 		"//tools/fvdl/e2e:fvdl_sdk_test",
