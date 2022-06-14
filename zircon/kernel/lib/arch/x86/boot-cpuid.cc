c4d4e26a898dc936acfe8871b5cbd636738f450f
Roland McGrath <mcgrathr@google.com>
2022-06-14 08:00:10 +0000

    [kernel][x86] Revamp BootCpuidIo initialization magic
    Instead of depending on parallel array layout between two special
    sections (the data and the metadata), use only one special section:
    the metadata, which is slightly larger to point to the data (using
    PIC-friendly means so InitializeBootCpuid works before relocation).
    
    Fixed: 102324
    Change-Id: I4a2683a280766412cb40b447d91a6ebbe301fbf5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/689823
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Petr Hosek <phosek@google.com>

--- zircon/kernel/lib/arch/x86/boot-cpuid.cc
+++ zircon/kernel/lib/arch/x86/boot-cpuid.cc
@@ -7,26 +7,18 @@
 #include <lib/arch/x86/boot-cpuid.h>
 #include <lib/special-sections/special-sections.h>
 
-namespace arch {
-namespace internal {
+namespace arch::internal {
 
 // The zeroth basic, hypervisor, and extended leaves (0, 0x4000'0000, and
 // 0x8000'0000) are handled specially in InitializeBootCpuid itself.
-// Note that they are not in the special section.
+// Note that they are not described in the special section.
 CpuidIo gBootCpuid0;
 CpuidIo gBootCpuidHyp0;
 CpuidIo gBootCpuidExt0;
 
-// These leaves are used from assembly code that needs unmangled names for
-// them, but InitializeBootCpuid handles them like implicit instantiations.
+// These are defined with assembly-friendly names, but have standard metadata
+// to get them initialized by InitializeBootCpuid only if they're linked in.
+CpuidIo gBootCpuidFeature;
+CpuidIo gBootCpuidExtf;
 
-SPECIAL_SECTION("BootCpuidData", uint32_t) CpuidIo gBootCpuidFeature;
-SPECIAL_SECTION("BootCpuidLeaf", uint32_t)
-static const uint32_t kBootCpuidFeatureLeaf[2] = {CpuidFeatureFlagsC::kLeaf, 0};
-
-SPECIAL_SECTION("BootCpuidData", uint32_t) CpuidIo gBootCpuidExtf;
-SPECIAL_SECTION("BootCpuidLeaf", uint32_t)
-static const uint32_t kBootCpuidExtfLeaf[2] = {CpuidExtendedFeatureFlagsB::kLeaf, 0};
-
-}  // namespace internal
-}  // namespace arch
+}  // namespace arch::internal
--- zircon/kernel/lib/arch/x86/include/lib/arch/x86/boot-cpuid.h
+++ zircon/kernel/lib/arch/x86/include/lib/arch/x86/boot-cpuid.h
@@ -73,50 +73,105 @@ class BootCpuidIo {
   }
 
  private:
-  // The underlying instantiation is indexed by leaf and subleaf.
-  template <uint32_t Leaf, uint32_t Subleaf = 0>
-  const CpuidIo* GetLeaf() const {
-#ifdef __clang__
-    // The CpuidIo object for each instantiation goes into a special section
-    // that the InitializeBootCpuid() function processes when called at
-    // startup.  Each entry starts at compile time with the leaf and subleaf
-    // in the slots for the registers where the cpuid instruction takes them
-    // as operands, and gets filled by InitializeBootCpuid() with the register
-    // results, or cleared to all zero if the Leaf is not supported by the
-    // hardware.  The assembly code implementing InitializeBootCpuid() knows
-    // the exact layout of this data, so make sure the C++ types conform.
-    static_assert(alignof(CpuidIo) == alignof(uint32_t));
-    static_assert(sizeof(CpuidIo) == sizeof(uint32_t[4]));
-    static_assert(std::is_same_v<decltype(CpuidIo{}.values_), uint32_t[4]>);
+  // One of these is emitted in the BootCpuidLeaf special RODATA section for
+  // each instantiation of GetLeaf<Leaf, Subleaf>.  InitializeBootCpuid scans
+  // the section and fills in the corresponding CpuidIo items in data sections.
+  struct BootCpuidLeaf {
+    uint32_t leaf, subleaf;  // CPUID instruction arguments.
 
-    // "used" for kCpuidLeaf to prevent compiler-GC; "used" for kCpuidIo to be
-    // doubly sure that every BootCpuidLeaf entry has an associated one in
-    // BootCpuidData.
-    SPECIAL_SECTION("BootCpuidData", uint32_t) static CpuidIo gCpuidIo;
-    SPECIAL_SECTION("BootCpuidLeaf", uint32_t)
-    static const uint32_t kCpuidLeaf[2] = {Leaf, Subleaf};
-#else
-    // TODO(fxbug.dev/27083): GCC doesn't honor the section attribute in a
-    // COMDAT context.  Instead, just do on-demand initialization right here.
-    // This bloats the code at each use and is less efficient than simply
-    // preloading all the data at startup via the special section hack.
-    static CpuidIo gCpuidIo = []() {
-      using MaxLeaf = std::conditional_t<
-          (Leaf < CpuidMaximumHypervisorLeaf::kLeaf), CpuidMaximumLeaf,
-          std::conditional_t<(Leaf < CpuidMaximumExtendedLeaf::kLeaf), CpuidMaximumHypervisorLeaf,
-                             CpuidMaximumExtendedLeaf>>;
-      if (Leaf > BootCpuidIo{}.Read<MaxLeaf>().reg_value()) {
-        return CpuidIo{};
-      }
-      CpuidIo data;
-      __cpuid_count(Leaf, Subleaf,  //
-                    data.values_[CpuidIo::kEax], data.values_[CpuidIo::kEbx],
-                    data.values_[CpuidIo::kEcx], data.values_[CpuidIo::kEdx]);
-      return data;
-    }();
-#endif  // __clang__
-    return &gCpuidIo;
+#if defined(__ELF__)
+
+    // This is an offset from the start of this BootCpuidLeaf object to the
+    // CpuidIo variable holding the corresponding data to be filled in by
+    // InitializeBootCpuid.
+    int32_t offset;
+
+#elif defined(_WIN32)
+
+    // This is just a direct pointer, dynamically relocated before startup.
+    CpuidIo* data;
+
+#endif  // _WIN32
+  };
+
+  // These ensure assumptions made by the InitializeBootCpuid assembly code.
+#if defined(__ELF__)
+  static_assert(alignof(BootCpuidLeaf) == alignof(uint32_t));
+  static_assert(sizeof(BootCpuidLeaf) == sizeof(uint32_t[3]));
+#elif defined(_WIN32)
+  static_assert(alignof(BootCpuidLeaf) == alignof(CpuidIo*));
+  static_assert(sizeof(BootCpuidLeaf) == sizeof(uint32_t[2]) + sizeof(void*));
+#endif
+  static_assert(alignof(CpuidIo) == alignof(uint32_t));
+  static_assert(sizeof(CpuidIo) == sizeof(uint32_t[4]));
+  static_assert(std::is_same_v<decltype(CpuidIo{}.values_), uint32_t[4]>);
+
+  // This gets instantiated for each GetLeaf<Leaf, Subleaf> instantiation.
+  // Explicit specializations can instead yield a constexpr CpuidIo& that
+  // aliases a named CpuidIo object usable from assembly code.
+  template <uint32_t Leaf, uint32_t Subleaf = 0>
+  static inline CpuidIo gCpuidIo = {};
+
+#if defined(__ELF__)
+
+  // This really just returns &gCpuidIo<Leaf, Subleaf> and can be inlined away
+  // as that.  But it also emits the BootCpuidLeaf metadata on the side.
+  template <uint32_t Leaf, uint32_t Subleaf = 0>
+  [[gnu::const]] const CpuidIo* GetLeaf() const {
+    // There is no way to form a C++ constinit initializer (aka C initializer)
+    // value that has a PIC-friendly offset like this.  But it can be done in
+    // assembly easily enough.  Since this function may be inlined in multiple
+    // places, it needs to ensure only one BootCpuidLeaf entry is emitted for
+    // the leaf/subleaf pair.  This is accomplished by placing the fragment of
+    // the BootCpuidLeaf section into a COMDAT group keyed by the symbol for
+    // gCpuidIo<Leaf, Subleaf>.  Since that is separately COMDAT'd there will
+    // be exactly one data slot, and exactly one metadata slot pointing to it.
+    // As this can be inlined multiple times in the same translation unit, it
+    // explicitly avoids emitting the same COMDAT section data twice since
+    // that would just collect into a single longer BootCpuidLeaf section
+    // repeating the same entry multiple times.  The SHF_GNU_RETAIN (R) flag
+    // is important here to prevent linker GC removal of the section, as
+    // nothing else refers to it.
+    __asm__ volatile(
+        R"""(
+        .ifndef BootCpuidLeaf.%c[data]
+          .pushsection BootCpuidLeaf, "aGR", %%progbits, %c[data], comdat
+          .type BootCpuidLeaf.%c[data], %%object
+          BootCpuidLeaf.%c[data]:
+            .int %c[leaf], %c[subleaf], %c[data] - BootCpuidLeaf.%c[data]
+          .size BootCpuidLeaf.%c[data], 12
+          .popsection
+        .endif
+        )"""
+        :
+        : [leaf] "i"(Leaf), [subleaf] "i"(Subleaf), [data] "i"(&gCpuidIo<Leaf, Subleaf>));
+    return &gCpuidIo<Leaf, Subleaf>;
   }
+
+#elif defined(_WIN32)
+
+  // We don't know how to do the same trick for other formats.  PE-COFF (for
+  // UEFI) has equivalent COMDAT-like features but there is no assembler
+  // syntax like .pushsection, only .section, so it can't really be used
+  // inside inline __asm__.  However, it also does dynamic relocation before
+  // startup so PIC-friendliness isn't really required.
+
+  template <uint32_t Leaf, uint32_t Subleaf = 0>
+  [[gnu::const]] const CpuidIo* GetLeaf() const {
+    // Even though it's always just &gCpuidIo<Leaf, Subleaf>, do the runtime
+    // indirection as a means of ensuring kBootCpuidLeaf stays live at link
+    // time, since there is no [[gnu::retain]] for PE-COFF,
+    return kBootCpuidLeaf<Leaf, Subleaf>.data;
+  }
+
+  // Directly emit the metadata into the special section, complete with
+  // dynamically-relocated pointer to the data.
+  template <uint32_t Leaf, uint32_t Subleaf>
+  SPECIAL_SECTION("BootCpuidLeaf", BootCpuidLeaf)
+  static inline const BootCpuidLeaf kBootCpuidLeaf = {
+      .leaf = Leaf, .subleaf = Subleaf, .data = &gCpuidIo<Leaf, Subleaf>};
+
+#endif  // _WIN32
 };
 
 // Call this once early in startup, before any uses of arch::BootCpuIdIo.  It
@@ -144,6 +199,19 @@ inline bool BootCpuidSupports() {
 // to give them unmangled (extern "C") names that are usable from assembly.
 
 template <>
+inline constexpr CpuidIo& BootCpuidIo::gCpuidIo<CpuidFeatureFlagsC::kLeaf> =
+    internal::gBootCpuidFeature;
+
+template <>
+inline constexpr CpuidIo& BootCpuidIo::gCpuidIo<CpuidExtendedFeatureFlagsB::kLeaf> =
+    internal::gBootCpuidExtf;
+
+// Those two above were defined with assembly-friendly names, but have
+// standard metadata to get them initialized by InitializeBootCpuid only if
+// they're linked in.  These three are always specially initialized by
+// InitializeBootCpuid and don't need any metadata.
+
+template <>
 inline const CpuidIo* BootCpuidIo::GetLeaf<CpuidMaximumLeaf::kLeaf>() const {
   return &internal::gBootCpuid0;
 }
@@ -159,14 +227,15 @@ inline const CpuidIo* BootCpuidIo::GetLeaf<CpuidMaximumExtendedLeaf::kLeaf>() co
 }
 
 template <>
-inline const CpuidIo* BootCpuidIo::GetLeaf<CpuidFeatureFlagsC::kLeaf>() const {
-  return &internal::gBootCpuidFeature;
-}
+inline constexpr CpuidIo& BootCpuidIo::gCpuidIo<CpuidMaximumLeaf::kLeaf> = internal::gBootCpuid0;
 
 template <>
-inline const CpuidIo* BootCpuidIo::GetLeaf<CpuidExtendedFeatureFlagsB::kLeaf>() const {
-  return &internal::gBootCpuidExtf;
-}
+inline constexpr CpuidIo& BootCpuidIo::gCpuidIo<CpuidMaximumHypervisorLeaf::kLeaf> =
+    internal::gBootCpuidHyp0;
+
+template <>
+inline constexpr CpuidIo& BootCpuidIo::gCpuidIo<CpuidMaximumExtendedLeaf::kLeaf> =
+    internal::gBootCpuidExt0;
 
 }  // namespace arch
 
