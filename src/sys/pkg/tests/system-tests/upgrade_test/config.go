13f6898b4b2229198553cc7e49b16775b85eb4fd
Oliver Newman <olivernewman@google.com>
2022-06-14 02:48:59 +0000

    [upgrade_test] Double cycle timeout
    The sherlock-release-ota-min-version_system_tests_upgrade test has been
    hitting this internal timeout consistently since this morning, but it
    just seems to be running slowly as opposed to encountering a hang.
    
    Bump the timeout for now to unblock CI/CQ until we can get to the bottom
    of the failures.
    
    Bug: 99533
    Change-Id: I61f38ce1d6396650c8f85369a547d654cf537bf6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/690492
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Oliver Newman <olivernewman@google.com>
    Commit-Queue: Oliver Newman <olivernewman@google.com>

--- src/sys/pkg/tests/system-tests/upgrade_test/config.go
+++ src/sys/pkg/tests/system-tests/upgrade_test/config.go
@@ -47,7 +47,7 @@ func newConfig(fs *flag.FlagSet) (*config, error) {
 
 	fs.DurationVar(&c.paveTimeout, "pave-timeout", 5*time.Minute, "Err if a pave takes longer than this time (default is 5 minutes)")
 	fs.IntVar(&c.cycleCount, "cycle-count", 1, "How many cycles to run the test before completing (default is 1)")
-	fs.DurationVar(&c.cycleTimeout, "cycle-timeout", 10*time.Minute, "Err if a test cycle takes longer than this time (default is 10 minutes)")
+	fs.DurationVar(&c.cycleTimeout, "cycle-timeout", 20*time.Minute, "Err if a test cycle takes longer than this time (default is 10 minutes)")
 	fs.StringVar(&c.beforeInitScript, "before-init-script", "", "Run this script before initializing device for testing")
 	fs.StringVar(&c.afterInitScript, "after-init-script", "", "Run this script after initializing device for testing")
 	fs.StringVar(&c.afterTestScript, "after-test-script", "", "Run this script after a test step")
