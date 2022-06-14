0d4a20e33f6dd9a88551fe9699de3670be711608
Marina Ciocea <marinaciocea@google.com>
2022-06-14 21:28:06 +0000

    [dhcpv6] Re-enable test broken by float arithmetic
    Re-enable tests that were disabled due to floating-point arithmetic
    changes in the rust standard library. The behavior of floating-point
    arithmetic went back to rounding vs truncating in
    https://github.com/rust-lang/rust/pull/96051, so it's safe to enable
    these tests again.
    
    A new bug has been filed to track the work to avoid doing
    floating-point arithmetic. The complication is that `StepRng` is
    behaving in unexpected ways and preventing migration of the existing
    unit tests.
    
    Fixed: 92923
    Bug: 102199
    
    Change-Id: I3372193a8d59f1bc0d816e9921056bb85fb290d4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/643581
    Reviewed-by: Jake Fried <friedj@google.com>
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
    Commit-Queue: Tony Gong <gongt@google.com>

--- src/connectivity/network/dhcpv6/core/src/client.rs
+++ src/connectivity/network/dhcpv6/core/src/client.rs
@@ -5290,7 +5290,6 @@ mod tests {
     // NOTE: All comparisons are done on millisecond, so this test is not affected by precision
     // loss from floating point arithmetic.
     #[test]
-    #[ignore]
     fn retransmission_timeout() {
         let mut rng = StepRng::new(std::u64::MAX / 2, 0);
 
