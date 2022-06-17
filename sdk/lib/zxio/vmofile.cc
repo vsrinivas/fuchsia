3b2e5e0d06aaca6773b809bc42460e816abd94ef
Tamir Duberstein <tamird@google.com>
2022-06-17 22:11:13 +0000

    [zxio] Use references rather than pointers
    Change-Id: Idd65c0bae7617ee438cd3b379ba7f3b17addc281
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/691277
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Adam Barth <abarth@google.com>

--- sdk/lib/zxio/vmofile.cc
+++ sdk/lib/zxio/vmofile.cc
@@ -41,33 +41,33 @@ static_assert(sizeof(zxio_vmofile_t) <= sizeof(zxio_storage_t),
               "zxio_vmofile_t must fit inside zxio_storage_t.");
 
 static zx_status_t zxio_vmofile_close(zxio_t* io) {
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
-  zx_status_t status = file->control->Close().status();
-  file->~zxio_vmofile_t();
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
+  zx_status_t status = file.control->Close().status();
+  file.~zxio_vmofile_t();
   return status;
 }
 
 static zx_status_t zxio_vmofile_release(zxio_t* io, zx_handle_t* out_handle) {
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
 
-  mtx_lock(&file->lock);
-  uint64_t offset = file->offset;
-  mtx_unlock(&file->lock);
+  mtx_lock(&file.lock);
+  uint64_t offset = file.offset;
+  mtx_unlock(&file.lock);
 
-  const fidl::WireResult result = file->control->Seek(fio::wire::SeekOrigin::kStart, offset);
+  const fidl::WireResult result = file.control->Seek(fio::wire::SeekOrigin::kStart, offset);
   if (!result.ok()) {
     return ZX_ERR_BAD_STATE;
   }
   if (result->is_error()) {
     return ZX_ERR_BAD_STATE;
   }
-  *out_handle = file->control.TakeClientEnd().TakeChannel().release();
+  *out_handle = file.control.TakeClientEnd().TakeChannel().release();
   return ZX_OK;
 }
 
 static zx_status_t zxio_vmofile_reopen(zxio_t* io, zxio_reopen_flags_t zxio_flags,
                                        zx_handle_t* out_handle) {
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
   zx::status ends = fidl::CreateEndpoints<fio::Node>();
   if (ends.is_error()) {
     return ends.status_value();
@@ -76,7 +76,7 @@ static zx_status_t zxio_vmofile_reopen(zxio_t* io, zxio_reopen_flags_t zxio_flag
   if (zxio_flags & ZXIO_REOPEN_DESCRIBE) {
     flags |= fio::wire::OpenFlags::kDescribe;
   }
-  const fidl::WireResult result = file->control->Clone(flags, std::move(ends->server));
+  const fidl::WireResult result = file.control->Clone(flags, std::move(ends->server));
   if (!result.ok()) {
     return result.status();
   }
@@ -85,12 +85,12 @@ static zx_status_t zxio_vmofile_reopen(zxio_t* io, zxio_reopen_flags_t zxio_flag
 }
 
 static zx_status_t zxio_vmofile_attr_get(zxio_t* io, zxio_node_attributes_t* out_attr) {
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
   *out_attr = {};
   ZXIO_NODE_ATTR_SET(*out_attr, protocols, ZXIO_NODE_PROTOCOL_FILE | ZXIO_NODE_PROTOCOL_MEMORY);
   ZXIO_NODE_ATTR_SET(*out_attr, abilities,
                      ZXIO_OPERATION_READ_BYTES | ZXIO_OPERATION_GET_ATTRIBUTES);
-  ZXIO_NODE_ATTR_SET(*out_attr, content_size, file->size);
+  ZXIO_NODE_ATTR_SET(*out_attr, content_size, file.size);
   return ZX_OK;
 }
 
@@ -100,15 +100,15 @@ static zx_status_t zxio_vmofile_readv(zxio_t* io, const zx_iovec_t* vector, size
     return ZX_ERR_NOT_SUPPORTED;
   }
 
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
 
-  mtx_lock(&file->lock);
+  mtx_lock(&file.lock);
   zx_status_t status =
-      zxio_vmo_do_vector(file->start, file->size, &file->offset, vector, vector_count, out_actual,
+      zxio_vmo_do_vector(file.start, file.size, &file.offset, vector, vector_count, out_actual,
                          [&](void* buffer, zx_off_t offset, size_t capacity) {
-                           return file->vmo.read(buffer, offset, capacity);
+                           return file.vmo.read(buffer, offset, capacity);
                          });
-  mtx_unlock(&file->lock);
+  mtx_unlock(&file.lock);
   return status;
 }
 
@@ -119,45 +119,45 @@ static zx_status_t zxio_vmofile_readv_at(zxio_t* io, zx_off_t offset, const zx_i
     return ZX_ERR_NOT_SUPPORTED;
   }
 
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
 
-  return zxio_vmo_do_vector(file->start, file->size, &offset, vector, vector_count, out_actual,
+  return zxio_vmo_do_vector(file.start, file.size, &offset, vector, vector_count, out_actual,
                             [&](void* buffer, zx_off_t offset, size_t capacity) {
-                              return file->vmo.read(buffer, offset, capacity);
+                              return file.vmo.read(buffer, offset, capacity);
                             });
 }
 
 static zx_status_t zxio_vmofile_seek(zxio_t* io, zxio_seek_origin_t start, int64_t offset,
                                      size_t* out_offset) {
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
 
-  mtx_lock(&file->lock);
+  mtx_lock(&file.lock);
   zx_off_t origin;
   switch (start) {
     case ZXIO_SEEK_ORIGIN_START:
       origin = 0;
       break;
     case ZXIO_SEEK_ORIGIN_CURRENT:
-      origin = file->offset;
+      origin = file.offset;
       break;
     case ZXIO_SEEK_ORIGIN_END:
-      origin = file->size;
+      origin = file.size;
       break;
     default:
-      mtx_unlock(&file->lock);
+      mtx_unlock(&file.lock);
       return ZX_ERR_INVALID_ARGS;
   }
   zx_off_t at;
   if (add_overflow(origin, offset, &at)) {
-    mtx_unlock(&file->lock);
+    mtx_unlock(&file.lock);
     return ZX_ERR_INVALID_ARGS;
   }
-  if (at > file->size) {
-    mtx_unlock(&file->lock);
+  if (at > file.size) {
+    mtx_unlock(&file.lock);
     return ZX_ERR_INVALID_ARGS;
   }
-  file->offset = at;
-  mtx_unlock(&file->lock);
+  file.offset = at;
+  mtx_unlock(&file.lock);
 
   *out_offset = at;
   return ZX_OK;
@@ -239,12 +239,12 @@ static zx_status_t zxio_vmofile_vmo_get(zxio_t* io, zxio_vmo_flags_t flags, zx_h
   // a VMO with no other data - like a starting offset - to the user.
   // (Technically we could support any page aligned offset, but that's currently
   // unneeded.)
-  auto file = reinterpret_cast<zxio_vmofile_t*>(io);
-  if (file->start != 0) {
+  zxio_vmofile_t& file = *reinterpret_cast<zxio_vmofile_t*>(io);
+  if (file.start != 0) {
     return ZX_ERR_NOT_FOUND;
   }
 
-  return zxio_vmo_get_common(file->vmo, file->size, flags, out_vmo, out_size);
+  return zxio_vmo_get_common(file.vmo, file.size, flags, out_vmo, out_size);
 }
 
 static constexpr zxio_ops_t zxio_vmofile_ops = []() {
@@ -262,7 +262,7 @@ static constexpr zxio_ops_t zxio_vmofile_ops = []() {
 
 zx_status_t zxio_vmofile_init(zxio_storage_t* storage, fidl::WireSyncClient<fio::File> control,
                               zx::vmo vmo, zx_off_t offset, zx_off_t length, zx_off_t seek) {
-  auto file = new (storage) zxio_vmofile_t{
+  zxio_vmofile_t& file = *new (storage) zxio_vmofile_t{
       .io = storage->io,
       .vmo = std::move(vmo),
       .start = offset,
@@ -271,6 +271,6 @@ zx_status_t zxio_vmofile_init(zxio_storage_t* storage, fidl::WireSyncClient<fio:
       .lock = MTX_INIT,
       .control = std::move(control),
   };
-  zxio_init(&file->io, &zxio_vmofile_ops);
+  zxio_init(&file.io, &zxio_vmofile_ops);
   return ZX_OK;
 }
