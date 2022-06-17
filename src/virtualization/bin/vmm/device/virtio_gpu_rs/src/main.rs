947a5a2cea77ff4c7fdc1e500844ad086014e35a
Tim Detwiler <tjdetwiler@google.com>
2022-06-17 17:44:52 +0000

    [virtualization] Scaffold out a rust virtio_gpu devices
    Add the boilerplate to build and initialize a virtio-gpu device in rust.
    
    Bug: 102870
    Change-Id: I534ed7d38b332f31b59f2f78f86c229deb7a6b8f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/691945
    Reviewed-by: David Hastings <dahastin@google.com>
    Commit-Queue: Tim Detwiler <tjdetwiler@google.com>

---
+++ src/virtualization/bin/vmm/device/virtio_gpu_rs/src/main.rs
@@ -0,0 +1,64 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+mod wire;
+
+use {
+    anyhow::{anyhow, Context},
+    fidl::endpoints::RequestStream,
+    fidl_fuchsia_virtualization_hardware::VirtioGpuRequestStream,
+    fuchsia_component::server,
+    futures::{StreamExt, TryFutureExt, TryStreamExt},
+    tracing,
+};
+
+async fn run_virtio_gpu(mut virtio_gpu_fidl: VirtioGpuRequestStream) -> Result<(), anyhow::Error> {
+    // Receive start info as first message.
+    let (start_info, _keyboard_listener, _pointer_listener, responder) = virtio_gpu_fidl
+        .try_next()
+        .await?
+        .ok_or(anyhow!("Failed to read fidl message from the channel."))?
+        .into_start()
+        .ok_or(anyhow!("Start should be the first message sent."))?;
+
+    // Prepare the device builder
+    let (device_builder, guest_mem) = machina_virtio_device::from_start_info(start_info)?;
+
+    responder.send()?;
+
+    // Complete the setup of queues and get a device.
+    let mut virtio_device_fidl = virtio_gpu_fidl.cast_stream();
+    let (device, ready_responder) = machina_virtio_device::config_builder_from_stream(
+        device_builder,
+        &mut virtio_device_fidl,
+        &[wire::CONTROLQ, wire::CURSORQ][..],
+        &guest_mem,
+    )
+    .await
+    .context("Failed to initialize device.")?;
+
+    // Initialize all queues.
+    let _control_stream = device.take_stream(wire::CONTROLQ)?;
+    let _cursor_stream = device.take_stream(wire::CURSORQ)?;
+    ready_responder.send()?;
+
+    futures::try_join!(device
+        .run_device_notify(virtio_device_fidl)
+        .map_err(|e| anyhow!("run_device_notify: {}", e)),)?;
+    Ok(())
+}
+
+#[fuchsia::main(logging = true, threads = 1)]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut fs = server::ServiceFs::new();
+    fs.dir("svc").add_fidl_service(|stream: VirtioGpuRequestStream| stream);
+    fs.take_and_serve_directory_handle().context("Error starting server")?;
+    fs.for_each_concurrent(None, |stream| async {
+        if let Err(e) = run_virtio_gpu(stream).await {
+            tracing::error!("Error running virtio_gpu service: {}", e);
+        }
+    })
+    .await;
+    Ok(())
+}
---
+++ src/virtualization/bin/vmm/device/virtio_gpu_rs/src/wire.rs
@@ -0,0 +1,12 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Keep all consts and type defs for completeness.
+#![allow(dead_code)]
+
+//
+// 5.7.2 Virtqueues
+//
+pub const CONTROLQ: u16 = 0;
+pub const CURSORQ: u16 = 1;
