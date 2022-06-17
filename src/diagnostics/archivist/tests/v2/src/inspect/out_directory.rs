19499ad2efa4062da25226daaa823a377afa23cd
Xyan Bhatnagar <xbhatnag@google.com>
2022-06-17 00:26:21 +0000

    [archivist] Use RealmQuery instead of Hub in archivist integration test
    Bug: 100623
    Change-Id: Id302138a92f567b6c3c31b54247a17520ca2f411
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/691782
    Commit-Queue: Xyan Bhatnagar <xbhatnag@google.com>
    Fuchsia-Auto-Submit: Xyan Bhatnagar <xbhatnag@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>

--- src/diagnostics/archivist/tests/v2/src/inspect/out_directory.rs
+++ src/diagnostics/archivist/tests/v2/src/inspect/out_directory.rs
@@ -6,6 +6,7 @@ use crate::test_topology;
 use anyhow::Error;
 use fidl_fuchsia_diagnostics::ArchiveAccessorMarker;
 use fidl_fuchsia_io as fio;
+use fidl_fuchsia_sys2 as fsys;
 use fuchsia_async as fasync;
 use fuchsia_zircon::DurationNum;
 use std::path::Path;
@@ -20,46 +21,55 @@ async fn out_can_be_read() {
     let _accessor =
         instance.root.connect_to_protocol_at_exposed_dir::<ArchiveAccessorMarker>().unwrap();
 
-    let hub_out_path_str = format!(
-        "/hub/children/realm_builder:{}/children/test/children/archivist/exec/out/",
-        instance.root.child_name()
-    );
-    let hub_out_path = Path::new(&hub_out_path_str);
-    verify_out(hub_out_path).await.expect("verify - first");
+    let archivist_moniker =
+        format!("./realm_builder:{}/test/archivist", instance.root.child_name());
+
+    verify_out(&archivist_moniker).await.expect("verify - first");
 
     // Verify again to ensure we can continue to read.
-    verify_out(hub_out_path).await.expect("verify - second");
+    verify_out(&archivist_moniker).await.expect("verify - second");
 }
 
-async fn verify_out(hub_out_path: &Path) -> Result<(), Error> {
+async fn verify_out(moniker: &str) -> Result<(), Error> {
     // TODO(fxbug.dev/61350): We need to retry since the out dir might not be serving yet. A much
     // cleaner way to do this would be to snapshot_then_subscribe for lifecycle events in the
     // archivist but unfortunately snapshot is the only supported mode at the moment.
-    loop {
-        if let Ok(mut result) = read_entries(hub_out_path).await {
-            let mut expected = vec!["diagnostics".to_string(), "svc".to_string()];
-            result.sort();
-            expected.sort();
-            if result == expected {
-                break;
+    let realm_query =
+        fuchsia_component::client::connect_to_protocol::<fsys::RealmQueryMarker>().unwrap();
+    let out_dir = loop {
+        if let Ok((_, resolved)) = realm_query.get_instance_info(moniker).await.unwrap() {
+            if let Some(resolved) = resolved {
+                if let Some(started) = resolved.started {
+                    let out_dir = started.out_dir.unwrap().into_proxy().unwrap();
+                    if let Ok(mut result) = read_entries(&out_dir).await {
+                        let mut expected = vec!["diagnostics".to_string(), "svc".to_string()];
+                        result.sort();
+                        expected.sort();
+                        if result == expected {
+                            break out_dir;
+                        }
+                    };
+                }
             }
-        };
+        }
         fasync::Timer::new(fasync::Time::after(100.millis())).await;
-    }
+    };
 
-    let result = read_entries(&hub_out_path.join("diagnostics")).await.unwrap();
+    let diagnostics_dir = fuchsia_fs::open_directory(
+        &out_dir,
+        Path::new("diagnostics"),
+        fio::OpenFlags::RIGHT_READABLE,
+    )
+    .unwrap();
+    let result = read_entries(&diagnostics_dir).await.unwrap();
     let expected = vec!["fuchsia.inspect.Tree".to_string()];
     assert_eq!(expected, result,);
 
     Ok(())
 }
 
-async fn read_entries(path: &Path) -> Result<Vec<String>, Error> {
-    let directory = fuchsia_fs::open_directory_in_namespace(
-        path.to_str().unwrap(),
-        fio::OpenFlags::RIGHT_READABLE,
-    )?;
-    Ok(fuchsia_fs::directory::readdir(&directory)
+async fn read_entries(out_dir: &fio::DirectoryProxy) -> Result<Vec<String>, Error> {
+    Ok(fuchsia_fs::directory::readdir(out_dir)
         .await?
         .into_iter()
         .map(|e| e.name)
