86e15616948b8b09ab953c9516fe9a2b19932f85
Mitchell Kember <mkember@google.com>
2022-06-28 21:52:11 +0000

    [build][fidl] Rework GN templates
    This CL goes over all the FIDL GN templates and makes them more
    consistent. It also adds documentation to fidl.gni describing the
    overall toolchain approach.
    
    In particular:
    
    * Use consistent target name suffixes (except Rust: fxbug.dev/102954).
    * Only forward non_fidl_deps to the fidlc target, not to all the other
      targets (which already depend on the fidlc target).
    * Forward variables more precisely rather than marking every parameter
      as not_needed (as was done in fidl.gni).
    * Always run codegen actions in $fidl_toolchain.
    * Always place generated code in the $fidl_toolchain's target_gen_dir.
    * Always limit visibility of codgen targets to ":*".
    * Always forward `visibility` and `testonly` to the main target.
    * Remove the target_api_platform option because it is misleading: it
      still compiles for other platforms, and the templates hardcode
      assumptions about the version being for fuchsia anyway.
    
    Bug: 102954
    Change-Id: Ieadd9581625f62f60718ba4fd9a49e18c6e8a29e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/693950
    Reviewed-by: Ian McKellar <ianloic@google.com>
    Commit-Queue: Mitchell Kember <mkember@google.com>

--- build/fidl/gen_response_file.py
+++ build/fidl/gen_response_file.py
@@ -51,12 +51,8 @@ def main():
         help="The path for the C simple server file to generate, if any")
     parser.add_argument(
         "--target-api-level",
-        help="Compile only APIs available at this API level",
+        help="Only compile APIs available at this Fuchsia API level",
         default="HEAD")
-    parser.add_argument(
-        "--target-platform",
-        help="Compile only APIs available on this platform",
-        default="fuchsia")
     parser.add_argument(
         "--name", help="The name for the generated FIDL library, if any")
     parser.add_argument(
@@ -85,11 +81,7 @@ def main():
 
     response_file = []
 
-    response_file.append(
-        "--available {platform}:{api_level}".format(
-            platform=args.target_platform,
-            api_level=args.target_api_level,
-        ))
+    response_file.append("--available fuchsia:%s" % args.target_api_level)
 
     if args.json:
         response_file.append("--json %s" % args.json)
