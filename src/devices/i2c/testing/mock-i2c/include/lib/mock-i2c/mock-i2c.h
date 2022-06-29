64175263f974488ca97efc3fa4512d061b05365a
Braden Kell <bradenkell@google.com>
2022-06-29 19:02:47 +0000

    [devices][test] Drop Banjo support in mock-i2c
    All users of this library have been switched to FIDL.
    
    Bug: 98515
    Test: CQ
    Change-Id: I40184cef95272d318f8ccba474f68db1aff64cc3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/689590
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Fuchsia-Auto-Submit: Braden Kell <bradenkell@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/devices/i2c/testing/mock-i2c/include/lib/mock-i2c/mock-i2c.h
+++ src/devices/i2c/testing/mock-i2c/include/lib/mock-i2c/mock-i2c.h
@@ -6,18 +6,17 @@
 #define SRC_DEVICES_I2C_TESTING_MOCK_I2C_INCLUDE_LIB_MOCK_I2C_MOCK_I2C_H_
 
 #include <fidl/fuchsia.hardware.i2c/cpp/wire.h>
-#include <fuchsia/hardware/i2c/c/banjo.h>
-#include <fuchsia/hardware/i2c/cpp/banjo.h>
+#include <lib/zx/status.h>
 
 #include <fbl/vector.h>
 #include <zxtest/zxtest.h>
 
 namespace mock_i2c {
 
-// This class mocks an addressed I2C device by providing an i2c_protocol_t. Users can set
-// expectations that either return specified data on read or verify data on write. After the test,
-// use VerifyAndClear to reset the object and verify that all expectations were satisfied. See the
-// following example test:
+// This class mocks an addressed I2C device by providing a fuchsia.hardware.i2c WireServer
+// implementation. Users can set expectations that either return specified data on read or verify
+// data on write. After the test, use VerifyAndClear to reset the object and verify that all
+// expectations were satisfied. See the following example test:
 //
 // mock_i2c::MockI2c i2c;
 // i2c
@@ -25,17 +24,15 @@ namespace mock_i2c {
 //     .ExpectReadStop({0x3a, 0x07})
 //     .ExpectWriteStop({0x18, 0x57});
 //
-// SomeDriver dut(i2c.GetProto());
+// auto endpoints = fidl::CreateEndpoints<fuchsia_hardware_i2c::Device>();
+// fidl::BindServer(dispatcher(), std::move(endpoints->server), &i2c);
+// SomeDriver dut(std::move(endpoints->client));
 //
 // EXPECT_OK(dut.SomeMethod());
 // EXPECT_TRUE(i2c.VerifyAndClear());
 
-class MockI2c : ddk::I2cProtocol<MockI2c>, public fidl::WireServer<fuchsia_hardware_i2c::Device> {
+class MockI2c : public fidl::WireServer<fuchsia_hardware_i2c::Device> {
  public:
-  MockI2c() : proto_{&i2c_protocol_ops_, this} {}
-
-  const i2c_protocol_t* GetProto() { return &proto_; }
-
   // Checks that the next operation is a read and returns the contents of buf.
   MockI2c& ExpectRead(fbl::Vector<uint8_t> buf, zx_status_t status = ZX_OK) {
     I2cExpectation exp{std::move(buf), true, false, status};
@@ -71,48 +68,21 @@ class MockI2c : ddk::I2cProtocol<MockI2c>, public fidl::WireServer<fuchsia_hardw
     expectations_index_ = 0;
   }
 
-  // These are used by ddk::I2cProtocol but are not intended for use by tests.
-  void I2cTransact(const i2c_op_t* op_list, size_t op_count, i2c_transact_callback callback,
-                   void* cookie) {
-    fbl::Vector<i2c_op_t> read_ops;
-    zx_status_t status;
-    for (size_t i = 0; i < op_count; i++) {
-      CheckI2cOp(op_list[i], &read_ops, &status);
-    }
-
-    callback(cookie, status, read_ops.data(), read_ops.size());
-  }
-
-  zx_status_t I2cGetMaxTransferSize(size_t* out_size) { return ZX_ERR_NOT_SUPPORTED; }
-
   void Transfer(TransferRequestView request, TransferCompleter::Sync& completer) override {
-    fbl::Vector<i2c_op_t> read_ops;
+    fidl::Arena arena;
+    fbl::Vector<fidl::VectorView<uint8_t>> read_ops;
     zx_status_t status = ZX_OK;
 
     for (auto it = request->transactions.cbegin(); it != request->transactions.cend();) {
-      if (!it->has_data_transfer()) {
-        completer.ReplyError(ZX_ERR_INVALID_ARGS);
-        return;
-      }
-
-      i2c_op_t op{
-          .is_read = it->data_transfer().is_read_size(),
-          .stop = it->has_stop() ? it->stop() : false,
-      };
-
-      if (op.is_read) {
-        op.data_buffer = nullptr;
-        op.data_size = it->data_transfer().read_size();
-      } else {
-        op.data_buffer = it->data_transfer().write_data().data();
-        op.data_size = it->data_transfer().write_data().count();
-      }
-
-      if (++it == request->transactions.cend()) {
-        op.stop = true;
-      }
-
-      CheckI2cOp(op, &read_ops, &status);
+      CheckI2cOp(*it++, [&](const I2cExpectation& exp) {
+        if (exp.is_read) {
+          fidl::VectorView<uint8_t> read_data(arena, exp.data.size());
+          memcpy(read_data.mutable_data(), exp.data.data(), exp.data.size());
+          read_ops.push_back(read_data);
+        }
+        status = exp.status;
+        return it == request->transactions.cend();
+      });
     }
 
     if (status != ZX_OK) {
@@ -120,17 +90,9 @@ class MockI2c : ddk::I2cProtocol<MockI2c>, public fidl::WireServer<fuchsia_hardw
       return;
     }
 
-    fidl::Arena arena;
     fidl::ObjectView<fuchsia_hardware_i2c::wire::DeviceTransferResponse> response(arena);
-    response->read_data = {arena, read_ops.size()};
-
-    auto read_data_it = response->read_data.begin();
-    for (const i2c_op_t& op : read_ops) {
-      *read_data_it = {arena, op.data_size};
-      memcpy(read_data_it->mutable_data(), op.data_buffer, op.data_size);
-      read_data_it++;
-    }
-
+    response->read_data =
+        fidl::VectorView<fidl::VectorView<uint8_t>>::FromExternal(read_ops.data(), read_ops.size());
     completer.Reply(::fitx::ok(response.get()));
   }
 
@@ -142,28 +104,39 @@ class MockI2c : ddk::I2cProtocol<MockI2c>, public fidl::WireServer<fuchsia_hardw
     zx_status_t status;
   };
 
-  void CheckI2cOp(const i2c_op_t& op, fbl::Vector<i2c_op_t>* read_ops, zx_status_t* status) {
+  // Callback for Transfer to process an I2C expectation and copy read data out if needed. Returns
+  // true if this is the last Transaction in the list -- in this case the stop flag should be
+  // considered set regardless of its actual value in the table.
+  using ProcessI2cExpectation = fit::callback<bool(const I2cExpectation&)>;
+
+  void CheckI2cOp(const fuchsia_hardware_i2c::wire::Transaction& op,
+                  ProcessI2cExpectation exp_callback) {
     ASSERT_FALSE(expectations_.is_empty(), "No more transactions are expected");
     ASSERT_LT(expectations_index_, expectations_.size(), "No more transactions are expected");
 
-    const I2cExpectation& exp = expectations_[expectations_index_++];
-    EXPECT_EQ(exp.stop, op.stop, "Transaction stop flag mismatch");
-    ASSERT_EQ(exp.is_read, op.is_read, "Transaction read flag mismatch");
-    ASSERT_EQ(exp.data.size(), op.data_size, "Transaction size mismatch");
+    ASSERT_TRUE(op.has_data_transfer(), "Transaction has no data transfer field");
+    ASSERT_TRUE(op.data_transfer().is_write_data() || op.data_transfer().is_read_size(),
+                "Transaction has no write data or read size");
 
-    if (exp.is_read) {
-      read_ops->push_back({.data_buffer = exp.data.data(),
-                           .data_size = exp.data.size(),
-                           .is_read = true,
-                           .stop = exp.stop});
-    } else {
-      EXPECT_EQ(0, memcmp(exp.data.data(), op.data_buffer, exp.data.size()),
-                "Transaction data mismatch");
+    const bool is_read = op.data_transfer().is_read_size();
+    const size_t data_size =
+        is_read ? op.data_transfer().read_size() : op.data_transfer().write_data().count();
+
+    const I2cExpectation& exp = expectations_[expectations_index_++];
+    const bool override_last_stop = exp_callback(exp);
+
+    const bool stop = (op.has_stop() ? op.stop() : false) || override_last_stop;
+
+    EXPECT_EQ(exp.stop, stop, "Transaction stop flag mismatch");
+    ASSERT_EQ(exp.is_read, is_read, "Transaction read flag mismatch");
+    ASSERT_EQ(exp.data.size(), data_size, "Transaction size mismatch");
+
+    if (!is_read) {
+      EXPECT_BYTES_EQ(exp.data.data(), op.data_transfer().write_data().data(), exp.data.size(),
+                      "Transaction data mismatch");
     }
-    *status = exp.status;
   }
 
-  const i2c_protocol_t proto_;
   fbl::Vector<I2cExpectation> expectations_;
   size_t expectations_index_ = 0;
 };
