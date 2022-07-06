7e5e625b112b90f0cdb684a4e02ec016292420ec
Satsuki Ueno <satsukiu@google.com>
2022-07-06 17:34:36 +0000

    [test_manager] Expose kernel/early boot profile to scp
    This changes test_manager to serve kernel and early boot profile via
    /tmp instead of a virtual file system, which makes it available to pull
    of the device using scp. This is a workaround while ffx is not stable
    enough for use in coverage builders. Long term, we will remove flows
    that involve pulling coverage via scp.
    
    Files are accessible at /tmp/test_manager:0/data
    
    From fx shell...
    $ ls /tmp/test_manager:0/data
    dynamic
    static
    $ ls /tmp/test_manager:0/data/dynamic
    zircon.log
    zircon.profraw
    $ ls /tmp/test_manager:0/data/static
    physboot.log
    physboot.profraw
    
    Testing: existing unit tests, verify coverage works in CI
    This removes a unit test which was relevant for VFS but is not relevant
    for /tmp.
    
    Change-Id: I7c7cfd57a88cc462e13660dab721bda5eef7497e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/696506
    Reviewed-by: Ankur Mittal <anmittal@google.com>
    Commit-Queue: Satsuki Ueno <satsukiu@google.com>

--- src/sys/test_manager/src/debug_data_server.rs
+++ src/sys/test_manager/src/debug_data_server.rs
@@ -4,19 +4,15 @@
 
 use {
     crate::{run_events::RunEvent, self_diagnostics},
-    anyhow::Error,
-    fidl::endpoints::ClientEnd,
+    anyhow::{Context, Error},
+    fidl::endpoints::{create_endpoints, ClientEnd},
     fidl_fuchsia_io as fio, fidl_fuchsia_test_internal as ftest_internal,
     fidl_fuchsia_test_manager as ftest_manager,
     fidl_fuchsia_test_manager::{DebugData, DebugDataIteratorMarker, DebugDataIteratorRequest},
     fuchsia_async as fasync,
     futures::{channel::mpsc, future::join_all, prelude::*, Future, StreamExt, TryStreamExt},
-    std::{collections::VecDeque, path::PathBuf},
+    std::path::{Path, PathBuf},
     tracing::warn,
-    vfs::{
-        directory::entry::DirectoryEntry, execution_scope::ExecutionScope,
-        file::vmo::read_only_const,
-    },
 };
 
 struct DebugDataFile {
@@ -26,55 +22,86 @@ struct DebugDataFile {
 
 fn serve_debug_data(
     files: Vec<DebugDataFile>,
-) -> (ClientEnd<DebugDataIteratorMarker>, impl 'static + Future<Output = ()>) {
+    serve_root_dir: PathBuf,
+) -> (ClientEnd<DebugDataIteratorMarker>, impl 'static + Future<Output = Result<(), Error>>) {
+    let read_write_flags: fuchsia_fs::OpenFlags =
+        fuchsia_fs::OpenFlags::RIGHT_READABLE | fuchsia_fs::OpenFlags::RIGHT_WRITABLE;
+    let overwite_file_flag: fuchsia_fs::OpenFlags = fuchsia_fs::OpenFlags::RIGHT_WRITABLE
+        | fuchsia_fs::OpenFlags::RIGHT_READABLE
+        | fuchsia_fs::OpenFlags::TRUNCATE
+        | fuchsia_fs::OpenFlags::CREATE;
+
     let (client, server) = fidl::endpoints::create_endpoints::<DebugDataIteratorMarker>().unwrap();
-    let mut files = files.into_iter().collect::<VecDeque<_>>();
 
     let fut = async move {
-        let scope = ExecutionScope::new();
+        let (file_sender, mut file_recv) = mpsc::channel(0);
 
-        let mut stream = server.into_stream().unwrap();
+        let copy_to_tmp_task = fasync::Task::spawn(async move {
+            let tmp_dir_root = fuchsia_fs::open_directory_in_namespace(
+                serve_root_dir.to_str().unwrap(),
+                read_write_flags,
+            )?;
+            let tmp_dir_root_ref = &tmp_dir_root;
+            futures::stream::iter(files)
+                .map(Ok)
+                .try_for_each_concurrent(None, move |DebugDataFile { name, contents }| {
+                    let mut sender_clone = file_sender.clone();
+                    let rel_file_path = PathBuf::from(name);
+                    async move {
+                        if let Some(parent) = rel_file_path.parent() {
+                            if !parent.as_os_str().is_empty() {
+                                fuchsia_fs::create_sub_directories(tmp_dir_root_ref, parent)
+                                    .context("create subdirectories")?;
+                            }
+                        }
+                        let file = fuchsia_fs::open_file(
+                            tmp_dir_root_ref,
+                            &rel_file_path,
+                            overwite_file_flag,
+                        )
+                        .context("open file")?;
+                        fuchsia_fs::write_file_bytes(&file, &contents)
+                            .await
+                            .context("write file")?;
+                        let (node_client, node_server) =
+                            create_endpoints::<fio::NodeMarker>().context("create node")?;
+                        file.clone(fuchsia_fs::OpenFlags::RIGHT_READABLE, node_server)
+                            .context("clone file")?;
+                        // error sending here is okay, receiver could close if client isn't reading the files.
+                        let _ = sender_clone.send((rel_file_path, node_client)).await;
+                        Result::<_, Error>::Ok(())
+                    }
+                })
+                .await
+        });
+
+        let mut stream = server.into_stream()?;
 
         while let Ok(Some(req)) = stream.try_next().await {
             match req {
                 DebugDataIteratorRequest::GetNext { responder, .. } => {
-                    let DebugDataFile { name, contents } = match files.pop_front() {
-                        Some(value) => value,
-                        None => {
-                            // Everything has been send. Return empty batch in the iterator.
-                            let _ = responder.send(&mut vec![].into_iter());
-                            continue;
+                    match file_recv.next().await {
+                        Some((filename, file_client)) => {
+                            let mut data_iter = vec![DebugData {
+                                name: Some(filename.to_string_lossy().into_owned()),
+                                file: Some(ClientEnd::<fio::FileMarker>::new(
+                                    file_client.into_channel(),
+                                )),
+                                ..DebugData::EMPTY
+                            }]
+                            .into_iter();
+
+                            let _ = responder.send(&mut data_iter);
                         }
-                    };
-
-                    let (file_client, file_server) =
-                        fidl::endpoints::create_endpoints::<fio::NodeMarker>().unwrap();
-
-                    let file_impl = read_only_const(&contents);
-                    std::mem::drop(contents); // contents are copied; release unneeded memory
-
-                    file_impl.open(
-                        scope.clone(),
-                        fio::OpenFlags::RIGHT_READABLE,
-                        0,
-                        vfs::path::Path::dot(),
-                        file_server,
-                    );
-
-                    let mut data_iter = vec![DebugData {
-                        name: Some(name),
-                        file: Some(fidl::endpoints::ClientEnd::<fio::FileMarker>::new(
-                            file_client.into_channel(),
-                        )),
-                        ..DebugData::EMPTY
-                    }]
-                    .into_iter();
-
-                    let _ = responder.send(&mut data_iter);
+                        None => {
+                            let _ = responder.send(&mut vec![].into_iter());
+                        }
+                    }
                 }
             }
         }
-        scope.wait().await;
+        copy_to_tmp_task.await?;
+        Ok(())
     };
 
     (client, fut)
@@ -184,8 +211,13 @@ pub async fn send_kernel_debug_data(mut event_sender: mpsc::Sender<RunEvent>) {
         .collect::<Vec<_>>();
 
     if !files.is_empty() {
-        let (client, fut) = serve_debug_data(files);
-        let task = fasync::Task::spawn(fut);
+        // We copy the files to a well known directory in /tmp. This supports exporting the
+        // files off device via SCP. Once this flow is no longer needed, we can use something
+        // like an ephemeral directory which is torn down once we're done instead.
+        let (client, fut) = serve_debug_data(files, Path::new("/tmp").to_path_buf());
+        let task = fasync::Task::spawn(
+            fut.unwrap_or_else(|e| warn!("Error serving kernel debug data: {:?}", e)),
+        );
         let _ = event_sender.send(RunEvent::debug_data(client).into()).await;
         event_sender.disconnect(); // No need to hold this open while we serve the task.
         task.await;
@@ -194,16 +226,12 @@ pub async fn send_kernel_debug_data(mut event_sender: mpsc::Sender<RunEvent>) {
 
 #[cfg(test)]
 mod test {
-    use {
-        super::*,
-        fuchsia_async as fasync,
-        futures::{future::Either, FutureExt},
-        std::task::Poll,
-    };
+    use {super::*, fuchsia_async as fasync, tempfile::tempdir};
 
     #[fuchsia::test]
     async fn empty_data_returns_empty_repeatedly() {
-        let (client, task) = serve_debug_data(vec![]);
+        let dir = tempdir().unwrap();
+        let (client, task) = serve_debug_data(vec![], dir.path().to_path_buf());
         let task = fasync::Task::spawn(task);
 
         let proxy = client.into_proxy().expect("into proxy");
@@ -216,15 +244,16 @@ mod test {
 
         // Disconnecting stops the serving task.
         std::mem::drop(proxy);
-        task.await;
+        task.await.unwrap();
     }
 
     #[fuchsia::test]
     async fn single_response() {
-        let (client, task) = serve_debug_data(vec![DebugDataFile {
-            name: "file".to_string(),
-            contents: b"test".to_vec(),
-        }]);
+        let dir = tempdir().unwrap();
+        let (client, task) = serve_debug_data(
+            vec![DebugDataFile { name: "file".to_string(), contents: b"test".to_vec() }],
+            dir.path().to_path_buf(),
+        );
         let _task = fasync::Task::spawn(task);
 
         let proxy = client.into_proxy().expect("into proxy");
@@ -244,11 +273,16 @@ mod test {
 
     #[fuchsia::test]
     async fn multiple_responses() {
-        let (client, task) = serve_debug_data(vec![
-            DebugDataFile { name: "file".to_string(), contents: b"test".to_vec() },
-            DebugDataFile { name: "file2".to_string(), contents: b"test2".to_vec() },
-        ]);
-        let _task = fasync::Task::spawn(task);
+        let dir = tempdir().unwrap();
+        let (client, task) = serve_debug_data(
+            vec![
+                DebugDataFile { name: "file".to_string(), contents: b"test".to_vec() },
+                DebugDataFile { name: "file2".to_string(), contents: b"test2".to_vec() },
+            ],
+            dir.path().to_path_buf(),
+        );
+        let _task =
+            fasync::Task::spawn(task.unwrap_or_else(|e| panic!("Error from server: {:?}", e)));
 
         let proxy = client.into_proxy().expect("into proxy");
 
@@ -278,71 +312,6 @@ mod test {
                 .collect::<Vec<_>>(),
         )
         .await;
-
-        assert_eq!(
-            responses,
-            vec![("file".to_string(), b"test".to_vec()), ("file2".to_string(), b"test2".to_vec()),]
-        );
-    }
-
-    #[fuchsia::test]
-    fn serve_unfinished_files_after_proxy_closed() {
-        let mut executor = fasync::TestExecutor::new().expect("create executor");
-
-        let (client, debug_data_server) = serve_debug_data(vec![
-            DebugDataFile { name: "file".to_string(), contents: b"test".to_vec() },
-            DebugDataFile { name: "file2".to_string(), contents: b"test2".to_vec() },
-        ]);
-        let proxy = client.into_proxy().expect("into proxy");
-        let debug_data_server = debug_data_server.shared();
-
-        // Complete all requests for files and close the proxy before reading files.
-        let get_responses_fut = async move {
-            let mut responses = vec![];
-            responses.push(proxy.get_next().await.expect("get next"));
-            responses.push(proxy.get_next().await.expect("get next"));
-            for response in &responses {
-                assert_eq!(1usize, response.len());
-            }
-            assert!(proxy.get_next().await.expect("get next").is_empty());
-            drop(proxy);
-            responses
-        }
-        .boxed();
-
-        // Poll both. Server shouldn't terminate yet.
-        let mut select_fut = futures::future::select(debug_data_server.clone(), get_responses_fut);
-        let responses = match executor.run_until_stalled(&mut select_fut) {
-            Poll::Pending => panic!("Expected poll to complete"),
-            Poll::Ready(Either::Left(_)) => panic!("Server shouldn't terminate yet"),
-            Poll::Ready(Either::Right((responses, _))) => responses,
-        };
-
-        // Keep polling server to make sure it doesn't terminate.
-        assert!(executor.run_until_stalled(&mut debug_data_server.clone()).is_pending());
-
-        let responses_fut = futures::future::join_all(
-            responses
-                .into_iter()
-                .flatten()
-                .map(|response| async move {
-                    let _ = &response;
-                    let DebugData { name, file, .. } = response;
-                    let contents =
-                        fuchsia_fs::read_file_bytes(&file.expect("has file").into_proxy().unwrap())
-                            .await
-                            .expect("read file");
-                    (name.expect("has name"), contents)
-                })
-                .collect::<Vec<_>>(),
-        );
-
-        // Both should complete now, and files should be served.
-        let mut join_fut = futures::future::join(responses_fut, debug_data_server);
-        let responses = match executor.run_until_stalled(&mut join_fut) {
-            Poll::Ready((resp, ())) => resp,
-            Poll::Pending => panic!("Expected poll to complete"),
-        };
 
         assert_eq!(
             responses,
