15392b2b43764eed0c21679363457efb1477ba06
Garratt Gallagher <garratt@google.com>
2022-07-08 06:29:45 +0000

    Revert "[camera][usb] Delete deprecated USB driver"
    This reverts commit c2e7082ac3e823a9aa97672520a9c858bf435948.
    
    Reason for revert: Bringing back the usb camera.
    
    Original change's description:
    > [camera][usb] Delete deprecated USB driver
    >
    > Change-Id: I0b51f0e43bd8cb344a92527463d792a5cd462472
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/484285
    > Fuchsia-Auto-Submit: Nic Zoghb <nzo@google.com>
    > Reviewed-by: Ernest Hua <ernesthua@google.com>
    > Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    
    Change-Id: I9b0be56a1a736d34d4495073fecc371cb8f253e2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/695586
    Commit-Queue: Garratt Gallagher <garratt@google.com>
    Reviewed-by: Sam Hansen <hansens@google.com>

---
+++ src/camera/drivers/usb_video/uvc_format_unittest.cc
@@ -0,0 +1,196 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "uvc_format.h"
+
+#include <lib/fake_ddk/fake_ddk.h>
+
+#include <zxtest/zxtest.h>
+
+namespace video {
+namespace usb {
+
+// 8 bits for each RGB.
+static constexpr uint32_t NANOSECS_IN_SEC = 1e9;
+
+TEST(UvcFormat, ToFidl) {
+  auto CheckFormat = [](const UvcFormat& usb, const fuchsia::camera::VideoFormat& fidl) {
+    ASSERT_EQ(fidl.format.width, usb.width);
+    ASSERT_EQ(fidl.format.height, usb.height);
+    ASSERT_EQ(fidl.format.layers, 1);
+    ASSERT_EQ(fidl.rate.frames_per_sec_numerator, NANOSECS_IN_SEC / 100);
+    ASSERT_EQ(fidl.format.planes[0].bytes_per_row, usb.stride);
+    ASSERT_EQ(fidl.rate.frames_per_sec_denominator, usb.default_frame_interval);
+    switch (usb.pixel_format) {
+      case UvcPixelFormat::BGRA32:
+        ASSERT_EQ(fidl.format.pixel_format.type, fuchsia::sysmem::PixelFormatType::BGRA32);
+        break;
+      case UvcPixelFormat::I420:
+        ASSERT_EQ(fidl.format.pixel_format.type, fuchsia::sysmem::PixelFormatType::I420);
+        break;
+      case UvcPixelFormat::M420:
+        ASSERT_EQ(fidl.format.pixel_format.type, fuchsia::sysmem::PixelFormatType::M420);
+        break;
+      case UvcPixelFormat::NV12:
+        ASSERT_EQ(fidl.format.pixel_format.type, fuchsia::sysmem::PixelFormatType::NV12);
+        break;
+      case UvcPixelFormat::YUY2:
+        ASSERT_EQ(fidl.format.pixel_format.type, fuchsia::sysmem::PixelFormatType::YUY2);
+        break;
+      case UvcPixelFormat::MJPEG:
+        ASSERT_EQ(fidl.format.pixel_format.type, fuchsia::sysmem::PixelFormatType::MJPEG);
+        break;
+      default:
+        ASSERT_EQ(fidl.format.pixel_format.type, fuchsia::sysmem::PixelFormatType::INVALID);
+    }
+  };
+  {
+    UvcFormat format;
+    format.width = 28;
+    format.height = -80;
+    format.default_frame_interval = 70;
+    format.stride = 32;
+    format.pixel_format = UvcPixelFormat::BGRA32;
+    auto fidl = ToFidl(format);
+    CheckFormat(format, fidl);
+  }
+  {
+    UvcFormat format;
+    format.width = 98;
+    format.height = 20;
+    format.default_frame_interval = 10;
+    format.stride = 9001;
+    format.pixel_format = UvcPixelFormat::I420;
+    auto fidl = ToFidl(format);
+    CheckFormat(format, fidl);
+  }
+  {
+    UvcFormat format;
+    format.width = 60;
+    format.height = 120;
+    format.default_frame_interval = 8;
+    format.stride = -10;
+    format.pixel_format = UvcPixelFormat::M420;
+    auto fidl = ToFidl(format);
+    CheckFormat(format, fidl);
+  }
+  {
+    UvcFormat format;
+    format.width = 17;
+    format.height = 65534;
+    format.default_frame_interval = 16;
+    format.stride = 1;
+    format.pixel_format = UvcPixelFormat::NV12;
+    auto fidl = ToFidl(format);
+    CheckFormat(format, fidl);
+  }
+  {
+    UvcFormat format;
+    format.width = 17;
+    format.height = 65534;
+    format.default_frame_interval = 16;
+    format.stride = 1;
+    format.pixel_format = UvcPixelFormat::YUY2;
+    auto fidl = ToFidl(format);
+    CheckFormat(format, fidl);
+  }
+  {
+    UvcFormat format;
+    format.width = 117;
+    format.height = 60534;
+    format.default_frame_interval = 16;
+    format.stride = 1;
+    format.pixel_format = UvcPixelFormat::MJPEG;
+    auto fidl = ToFidl(format);
+    CheckFormat(format, fidl);
+  }
+  {
+    UvcFormat format;
+    format.width = 17;
+    format.height = 80;
+    format.default_frame_interval = 16;
+    format.stride = 1;
+    format.pixel_format = UvcPixelFormat::INVALID;
+    auto fidl = ToFidl(format);
+    CheckFormat(format, fidl);
+  }
+}
+
+TEST(UvcFormat, Compare) {
+  {
+    UvcFormat format;
+    format.width = 117;
+    format.height = 60534;
+    format.default_frame_interval = 16;
+    format.stride = 1;
+    format.pixel_format = UvcPixelFormat::MJPEG;
+    auto fidl = ToFidl(format);
+    ASSERT_TRUE(Compare(fidl, format));
+    format.stride = 2;
+    ASSERT_FALSE(Compare(fidl, format));
+    format.stride = 1;
+    format.width = 201;
+    ASSERT_FALSE(Compare(fidl, format));
+    format.width = 117;
+    format.pixel_format = NV12;
+    ASSERT_FALSE(Compare(fidl, format));
+  }
+}
+
+TEST(UvcFormat, ParseUsbDescriptor) {
+  {
+    UvcFormatList list;
+    struct {
+      usb_video_vs_uncompressed_format_desc fmt;
+      usb_video_vs_frame_desc vc_desc;
+    } __PACKED desc;
+    desc.fmt.bLength = sizeof(desc.fmt);
+    desc.fmt.bDescriptorSubType = USB_VIDEO_VS_FORMAT_UNCOMPRESSED;
+    desc.fmt.bFormatIndex = 5;
+    uint8_t guid[] = USB_VIDEO_GUID_YUY2_VALUE;
+    memcpy(desc.fmt.guidFormat, guid, 16);
+    desc.fmt.bBitsPerPixel = 12;
+    desc.fmt.bNumFrameDescriptors = 1;
+    desc.fmt.bDefaultFrameIndex = 2;
+    desc.vc_desc.bDescriptorType = USB_VIDEO_CS_INTERFACE;
+    desc.vc_desc.bLength = sizeof(desc.vc_desc);
+    desc.vc_desc.bDescriptorSubType = USB_VIDEO_VS_FRAME_UNCOMPRESSED;
+    desc.vc_desc.dwDefaultFrameInterval = 30;
+    desc.vc_desc.wWidth = 90;
+    desc.vc_desc.wHeight = 80;
+    usb_desc_iter_t iter;
+    iter.current = reinterpret_cast<uint8_t*>(&desc.vc_desc);
+    iter.desc_end = iter.current + sizeof(desc.vc_desc);
+    ASSERT_OK(list.ParseUsbDescriptor(reinterpret_cast<usb_video_vc_desc_header*>(&desc), &iter));
+  }
+  {
+    UvcFormatList list;
+    struct {
+      usb_video_vs_uncompressed_format_desc fmt;
+      usb_video_vs_frame_desc vc_desc;
+    } __PACKED desc;
+    desc.fmt.bLength = sizeof(desc.fmt);
+    desc.fmt.bDescriptorSubType = USB_VIDEO_VS_FORMAT_UNCOMPRESSED;
+    desc.fmt.bFormatIndex = 5;
+    uint8_t guid[] = USB_VIDEO_GUID_YUY2_VALUE;
+    memcpy(desc.fmt.guidFormat, guid, 16);
+    desc.fmt.bBitsPerPixel = 12;
+    desc.fmt.bNumFrameDescriptors = 1;
+    desc.fmt.bDefaultFrameIndex = 2;
+    desc.vc_desc.bDescriptorType = USB_VIDEO_CS_INTERFACE;
+    desc.vc_desc.bLength = sizeof(desc.vc_desc);
+    desc.vc_desc.bDescriptorSubType = USB_VIDEO_VS_FRAME_UNCOMPRESSED;
+    desc.vc_desc.dwDefaultFrameInterval = 30;
+    desc.vc_desc.wWidth = 90;
+    desc.vc_desc.wHeight = 0;
+    usb_desc_iter_t iter;
+    iter.current = reinterpret_cast<uint8_t*>(&desc.vc_desc);
+    iter.desc_end = iter.current + sizeof(desc.vc_desc);
+    ASSERT_EQ(list.ParseUsbDescriptor(reinterpret_cast<usb_video_vc_desc_header*>(&desc), &iter),
+              ZX_ERR_INVALID_ARGS);
+  }
+}
+
+}  // namespace usb
+}  // namespace video
---
+++ src/camera/drivers/usb_video/video_util.c
@@ -0,0 +1,77 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/camera/drivers/usb_video/video_util.h"
+
+#include <string.h>
+
+#include <ddk/debug.h>
+#include <usb/usb.h>
+
+static void print_controls(usb_video_vc_probe_and_commit_controls* proposal) {
+  zxlogf(DEBUG, "bmHint 0x%x", proposal->bmHint);
+  zxlogf(DEBUG, "bFormatIndex: %u", proposal->bFormatIndex);
+  zxlogf(DEBUG, "bFrameIndex: %u", proposal->bFrameIndex);
+  zxlogf(DEBUG, "dwFrameInterval: %u", proposal->dwFrameInterval);
+  zxlogf(DEBUG, "dwMaxVideoFrameSize: %u", proposal->dwMaxVideoFrameSize);
+  zxlogf(DEBUG, "dwMaxPayloadTransferSize: %u", proposal->dwMaxPayloadTransferSize);
+}
+
+zx_status_t usb_video_negotiate_probe(usb_protocol_t* usb, uint8_t vs_interface_num,
+                                      usb_video_vc_probe_and_commit_controls* proposal,
+                                      usb_video_vc_probe_and_commit_controls* out_result) {
+  zx_status_t status;
+  size_t out_length;
+
+  zxlogf(DEBUG, "usb_video_negotiate_probe: PROBE_CONTROL SET_CUR");
+  print_controls(proposal);
+  // The wValue field (the fourth parameter) specifies the Control Selector
+  // (in this case USB_VIDEO_VS_PROBE_CONTROL) in the high byte,
+  // and the low byte must be set to zero.
+  // See UVC 1.5 Spec. 4.2.1 Interface Control Requests.
+  status = usb_control_out(usb, USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE,
+                           USB_VIDEO_SET_CUR, USB_VIDEO_VS_PROBE_CONTROL << 8, vs_interface_num,
+                           ZX_TIME_INFINITE, (uint8_t*)proposal, sizeof(*proposal));
+  if (status != ZX_OK)
+    goto out;
+
+  // The length of returned result varies, so zero this out before hand.
+  memset(out_result, 0, sizeof(usb_video_vc_probe_and_commit_controls));
+
+  zxlogf(DEBUG, "usb_video_negotiate_probe: PROBE_CONTROL GET_CUR");
+  status = usb_control_in(usb, USB_DIR_IN | USB_TYPE_CLASS | USB_RECIP_INTERFACE, USB_VIDEO_GET_CUR,
+                          USB_VIDEO_VS_PROBE_CONTROL << 8, vs_interface_num, ZX_TIME_INFINITE,
+                          (uint8_t*)out_result, sizeof(*out_result), &out_length);
+  if (status != ZX_OK) {
+    goto out;
+  }
+  // Fields after dwMaxPayloadTransferSize are optional, only 26 bytes are
+  // guaranteed.
+  if (out_length < 26) {
+    zxlogf(ERROR, "usb_video_negotiate_probe: got length %lu, want >= 26", out_length);
+    goto out;
+  }
+  print_controls(out_result);
+
+out:
+  if (status == ZX_ERR_IO_REFUSED || status == ZX_ERR_IO_INVALID) {
+    // clear the stall/error
+    usb_reset_endpoint(usb, 0);
+  }
+  return status;
+}
+
+zx_status_t usb_video_negotiate_commit(usb_protocol_t* usb, uint8_t vs_interface_num,
+                                       usb_video_vc_probe_and_commit_controls* ctrls) {
+  zxlogf(DEBUG, "usb_video_negotiate_commit: COMMIT_CONTROL SET_CUR");
+  zx_status_t status =
+      usb_control_out(usb, USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE, USB_VIDEO_SET_CUR,
+                      USB_VIDEO_VS_COMMIT_CONTROL << 8, vs_interface_num, ZX_TIME_INFINITE,
+                      (uint8_t*)ctrls, sizeof(*ctrls));
+  if (status == ZX_ERR_IO_REFUSED || status == ZX_ERR_IO_INVALID) {
+    // clear the stall/error
+    usb_reset_endpoint(usb, 0);
+  }
+  return status;
+}
