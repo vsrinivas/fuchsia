dad92643aa4905dee0da575829a01f29b421d6be
Ankur Mittal <anmittal@google.com>
2022-07-08 20:51:17 +0000

    [debug_data] Fix test
    Instead of sending 1000 vmos fix test to only send vmos 2 times. This
    helps with timeout when the test is executed multiple times.
    
    Multiply: debug-data-test
    
    Change-Id: I2cf4a4ed0c36449e062dd96e48548250d48b1b7b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/698982
    Reviewed-by: Satsuki Ueno <satsukiu@google.com>
    Commit-Queue: Satsuki Ueno <satsukiu@google.com>

--- src/sys/test_manager/tests/test_data/debug_data_test.rs
+++ src/sys/test_manager/tests/test_data/debug_data_test.rs
@@ -3,21 +3,35 @@
 // found in the LICENSE file.
 
 use {
-    fidl_fuchsia_debugdata::PublisherMarker, fuchsia_component::client::connect_to_protocol,
+    fidl_fuchsia_debugdata::PublisherMarker,
+    fuchsia_component::client::connect_to_protocol,
     fuchsia_zircon as zx,
+    std::thread::sleep,
+    std::time::Duration,
+    zx::{AsHandleRef, HandleBased},
 };
 
 #[fuchsia::test]
 async fn can_connect_to_publisher_service() {
     // Here we want to test that we can find and connect to publisher service. As the service does
-    // not return anything the only way to do that is to call Publish again and again to make sure
-    // that connection is not closed.
-    // There is no other way to test correctness.
-    let publisher = connect_to_protocol::<PublisherMarker>().unwrap();
-    for i in 0..1000 {
-        let (_vmo_server, vmo_token) = zx::EventPair::create().unwrap();
-        let data = zx::Vmo::create(1024).unwrap();
+    // not return anything, the only way to do that is first wait for vmo_handle to close and then
+    // send a request again to make sure that fidl connection still works.
+    for i in 0..2 {
+        let publisher = connect_to_protocol::<PublisherMarker>().unwrap();
+        let (vmo_server, vmo_token) = zx::EventPair::create().unwrap();
+        let data = zx::Vmo::create(10).unwrap();
+        let vmo_handle = data.duplicate_handle(zx::Rights::SAME_RIGHTS).unwrap();
         let name = format!("data_sink_{}", i);
         publisher.publish(&name, data, vmo_token).expect("The connection should not die");
+        drop(vmo_server);
+        // wait for vmo handle to close.
+        loop {
+            let count = vmo_handle.count_info().unwrap();
+            if count.handle_count != 1 {
+                sleep(Duration::from_millis(1));
+            } else {
+                break;
+            };
+        }
     }
 }
