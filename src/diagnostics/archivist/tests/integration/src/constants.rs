85c26d92b121653e7162aaf5542e032f322c6911
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2022-07-09 01:04:33 +0000

    [archivist][tests] Rename integration test
    There's no more v1 integration tests, so no point on adding the v2
    prefix anymore.
    
    Fixed: 74502
    
    Change-Id: Ie814b00e098de7ec985face7b41ed127e3817454
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/698619
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/diagnostics/archivist/tests/integration/src/constants.rs
+++ src/diagnostics/archivist/tests/integration/src/constants.rs
@@ -12,20 +12,20 @@ pub const INTEGRATION_ARCHIVIST_URL: &str = "#meta/archivist.cm";
 pub const ARCHIVIST_WITH_KLOG_URL: &str = "#meta/archivist_with_klog.cm";
 pub const ARCHIVIST_FOR_V1_URL: &str = "#meta/archivist_for_v1.cm";
 pub const COMPONENT_WITH_CHILDREN_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/component_with_children.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/component_with_children.cm";
 pub const IQUERY_TEST_COMPONENT_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/test_component.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/test_component.cm";
 pub const LOG_AND_CRASH_COMPONENT_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/log-and-crash.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/log-and-crash.cm";
 pub const SOCKET_PUPPET_COMPONENT_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/socket-puppet.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/socket-puppet.cm";
 pub const LOG_AND_EXIT_COMPONENT_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/log-and-exit.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/log-and-exit.cm";
 pub const LOGGING_COMPONENT_URL: &str = "#meta/logging_component.cm";
 pub const STUB_INSPECT_COMPONENT_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/stub_inspect_component.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/stub_inspect_component.cm";
 pub const HANGING_INSPECT_COMPONENT_URL: &str = "#meta/hanging_inspect_component.cm";
 pub const LOGGER_COMPONENT_FOR_INTEREST_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/log-on-interest.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/log-on-interest.cm";
 pub const STDIO_PUPPET_URL: &str =
-    "fuchsia-pkg://fuchsia.com/archivist-integration-tests-v2#meta/stdio-puppet.cm";
+    "fuchsia-pkg://fuchsia.com/archivist-integration-tests#meta/stdio-puppet.cm";
