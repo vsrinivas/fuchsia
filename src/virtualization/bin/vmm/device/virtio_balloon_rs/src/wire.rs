4a3337e2ebf066d7a27d24b7ffba0523decaa714
Alexander Radchenko <alerad@google.com>
2022-07-11 05:25:03 +0000

    [virtualization] Add virtio_balloon_rs scaffolding
    Add the boilerplate to build virtio_balloon in rust
    
    Bug: 100513
    Change-Id: I5a4fc2324292854640ba73c4885d27c46f5854b7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/696262
    Commit-Queue: Alexander Radchenko <alerad@google.com>
    Reviewed-by: Tim Detwiler <tjdetwiler@google.com>
    Reviewed-by: David Hastings <dahastin@google.com>

---
+++ src/virtualization/bin/vmm/device/virtio_balloon_rs/src/wire.rs
@@ -0,0 +1,12 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+// Keep all consts and type defs for completeness.
+#![allow(dead_code)]
+
+//
+// 5.5.2 Virtqueues
+//
+pub const INFLATEQ: u16 = 0;
+pub const DEFLATEQ: u16 = 1;
+pub const STATSQ: u16 = 2;
