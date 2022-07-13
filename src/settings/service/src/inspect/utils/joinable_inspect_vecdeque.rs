d733da2ff8d90ab6ca72ed414725514d3fb61f14
William Xiao <wxyz@google.com>
2022-07-13 01:16:32 +0000

    [settings] Add JoinableInspectVecDeque utility
    This wrapper around VecDeque<String> implements the Unit trait from
    fuchsia_inspect_derive, which allows it to be wrapped in an IValue,
    which is a smart pointer that automatically updates a field in inspect
    when updated.
    
    The inspect representation will just be a comma-separated list of the
    strings in the VecDeque
    
    Bug: 103390
    Test: fx test setui_service_tests
    Change-Id: I2d022cfb29e28051d10a45be522cd2eadfcb4f7d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/698418
    Reviewed-by: Chip Fukuhara <cfukuhara@google.com>
    Reviewed-by: Paul Faria <paulfaria@google.com>
    Commit-Queue: William Xiao <wxyz@google.com>
    Reviewed-by: Sijie Chen <sijiec@google.com>

---
+++ src/settings/service/src/inspect/utils/joinable_inspect_vecdeque.rs
@@ -0,0 +1,68 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use fuchsia_inspect::{Node, Property, StringProperty};
+use fuchsia_inspect_derive::Unit;
+use itertools::Itertools;
+use std::collections::VecDeque;
+
+/// Wrapper around [std::collections::VecDeque] that only holds [String]. Implements
+/// [fuchsia_inspect_derive::Unit], which allows it to be written to inspect as a single property
+/// with its value being a comma-separated list that's concatenation of all of the items in the
+/// VecDeque.
+///
+/// To use this in a a structure that implements [fuchsia_inspect_derive::Inspect], wrap this in the
+/// [fuchsia_inspect_derive::IValue] smart pointer and it will automatically update the value of the
+/// inspect property when updated.
+// TODO(fxbug.dev/103390): remove once used
+#[allow(dead_code)]
+#[derive(Default)]
+struct JoinableInspectVecDeque(VecDeque<String>);
+
+impl Unit for JoinableInspectVecDeque {
+    type Data = StringProperty;
+
+    fn inspect_create(&self, parent: &Node, name: impl AsRef<str>) -> Self::Data {
+        parent.create_string(name.as_ref(), self.0.iter().join(","))
+    }
+
+    fn inspect_update(&self, data: &mut Self::Data) {
+        data.set(&self.0.iter().join(","));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::inspect::utils::joinable_inspect_vecdeque::JoinableInspectVecDeque;
+    use fuchsia_inspect::{assert_data_tree, Inspector, Node};
+    use fuchsia_inspect_derive::{IValue, Inspect, WithInspect};
+
+    #[derive(Default, Inspect)]
+    struct TestInspectWrapper {
+        inspect_node: Node,
+        pub test_vec_deque: IValue<JoinableInspectVecDeque>,
+    }
+
+    // Tests that adding and removing from JoinableInspectVecDeque updates the inspect tree.
+    #[test]
+    fn test_vec_deque() {
+        let inspector = Inspector::new();
+
+        let mut wrapper = TestInspectWrapper::default()
+            .with_inspect(inspector.root(), "wrapper_node")
+            .expect("Failed to attach wrapper_node");
+
+        let _ = wrapper.test_vec_deque.as_mut().0.push_back("test1".to_string());
+        let _ = wrapper.test_vec_deque.as_mut().0.push_back("test2".to_string());
+        let _ = wrapper.test_vec_deque.as_mut().0.push_back("test3".to_string());
+
+        let _ = wrapper.test_vec_deque.as_mut().0.pop_front();
+
+        assert_data_tree!(inspector, root: {
+            wrapper_node: {
+                "test_vec_deque": "test2,test3",
+            }
+        });
+    }
+}
