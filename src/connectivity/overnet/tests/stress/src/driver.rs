240621d835dde290b6604483d79484ecaaf49548
Satsuki Ueno <satsukiu@google.com>
2022-07-14 21:59:50 +0000

    [overnet] Stress test concurrent connections
    Bug: 99669
    Multiply: overnet-stress-test
    
    Change-Id: Idef883c37223b1c0af2a167f96fc8f5e0b498b74
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/700065
    Commit-Queue: Satsuki Ueno <satsukiu@google.com>
    Reviewed-by: Casey Dahlin <sadmac@google.com>

--- src/connectivity/overnet/tests/stress/src/driver.rs
+++ src/connectivity/overnet/tests/stress/src/driver.rs
@@ -3,9 +3,12 @@
 // found in the LICENSE file.
 
 use {
-    fidl::endpoints::create_proxy, fidl_fuchsia_overnet as fovernet,
-    fidl_test_proxy_stress as fstress, fuchsia_component::client::connect_to_protocol,
-    fuchsia_zircon as zx, futures::AsyncReadExt, tracing::info,
+    fidl::endpoints::create_proxy,
+    fidl_fuchsia_overnet as fovernet, fidl_test_proxy_stress as fstress,
+    fuchsia_component::client::connect_to_protocol,
+    fuchsia_zircon as zx,
+    futures::{AsyncReadExt, StreamExt},
+    tracing::info,
 };
 
 async fn connect_to_stressor() -> fstress::StressorProxy {
@@ -48,4 +51,48 @@ async fn test_stuff_socket() {
     let mut socket_data = vec![];
     async_socket.read_to_end(&mut socket_data).await.expect("read socket");
     assert_eq!(socket_data, vec![0xFF; bytes_written as usize]);
+}
+
+#[fuchsia::test]
+async fn test_parallel_connections() {
+    const NUM_CONNECTIONS: usize = 100;
+    const NUM_REQUESTS: usize = 100;
+
+    let connections: Vec<_> =
+        futures::stream::iter(0..NUM_CONNECTIONS).then(|_| connect_to_stressor()).collect().await;
+
+    // call echo simultaneously on each proxy.
+    let echo_futs: Vec<_> = connections.iter().map(|proxy| proxy.echo("echo")).collect();
+    for echo_result in futures::future::join_all(echo_futs).await {
+        assert_eq!(echo_result.unwrap(), "echo");
+    }
+
+    // call echo a few times on each proxy
+    futures::stream::iter(connections)
+        .for_each_concurrent(None, |proxy| async move {
+            for echo_num in 0..NUM_REQUESTS {
+                let echo_content = format!("echo-{:?}", echo_num);
+                assert_eq!(proxy.echo(&echo_content).await.unwrap(), echo_content);
+            }
+        })
+        .await;
+}
+
+#[fuchsia::test]
+async fn test_pipelined_connections() {
+    const NUM_PIPELINED: usize = 100;
+    const NUM_CONNECTIONS: usize = 100;
+
+    let connections: Vec<_> =
+        futures::stream::iter(0..NUM_CONNECTIONS).then(|_| connect_to_stressor()).collect().await;
+
+    // call echo a few times on each proxy
+    futures::stream::iter(connections)
+        .for_each_concurrent(None, |proxy| async move {
+            let echo_futs: Vec<_> = (0..NUM_PIPELINED).map(|_| proxy.echo("echo")).collect();
+            for echo_result in futures::future::join_all(echo_futs).await {
+                assert_eq!(echo_result.unwrap(), "echo");
+            }
+        })
+        .await;
 }
