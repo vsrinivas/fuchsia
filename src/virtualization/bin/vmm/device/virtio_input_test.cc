d966acf31fe6693627426c36ada6ab399885f935
Tim Detwiler <tjdetwiler@google.com>
2022-07-14 18:01:41 +0000

    [virtualization] Enable some virtio_input_tests for the rust device
    This parameterizes the test suite and enables the keyboard test for the
    rust device.
    
    We also make some changes to the test to support the rust
    implementation:
    
      * Initialize the status queue
      * Only place one descriptor/event per chain. The spec isn't very
        explicit here but the zircon and linux driver both follow the
        convention of one event per chain
      * Add descriptors to the ring before injecting the key event
      * Use the input3 event types for the rust device. Continue to use the
        older/deprecated types for the C++ device.
    
    Bug: 104229
    Test: fx test virtio_input_unittests
    Change-Id: Id56f2f037e5e340d61d387e02b5056aa8d56e14c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/700475
    Reviewed-by: David Hastings <dahastin@google.com>
    Commit-Queue: Tim Detwiler <tjdetwiler@google.com>

--- src/virtualization/bin/vmm/device/virtio_input_test.cc
+++ src/virtualization/bin/vmm/device/virtio_input_test.cc
@@ -5,22 +5,37 @@
 #include <fuchsia/tracing/provider/cpp/fidl.h>
 #include <lib/sys/component/cpp/testing/realm_builder.h>
 #include <lib/sys/component/cpp/testing/realm_builder_types.h>
+#include <lib/zx/status.h>
 
 #include <virtio/input.h>
 
+#include "fuchsia/input/cpp/fidl.h"
 #include "fuchsia/logger/cpp/fidl.h"
+#include "fuchsia/ui/input3/cpp/fidl.h"
 #include "src/virtualization/bin/vmm/device/input.h"
 #include "src/virtualization/bin/vmm/device/test_with_device.h"
 #include "src/virtualization/bin/vmm/device/virtio_queue_fake.h"
 
 namespace {
 
-constexpr uint16_t kNumQueues = 1;
+constexpr uint16_t kNumQueues = 2;
 constexpr uint16_t kQueueSize = 16;
 
-class VirtioInputTest : public TestWithDevice {
+constexpr auto kCppComponentUrl = "fuchsia-pkg://fuchsia.com/virtio_input#meta/virtio_input.cm";
+constexpr auto kRustComponentUrl =
+    "fuchsia-pkg://fuchsia.com/virtio_input_rs#meta/virtio_input_rs.cm";
+
+struct VirtioInputTestParam {
+  std::string test_name;
+  std::string component_url;
+};
+
+class VirtioInputTest : public TestWithDevice,
+                        public ::testing::WithParamInterface<VirtioInputTestParam> {
  protected:
-  VirtioInputTest() : event_queue_(phys_mem_, PAGE_SIZE * kNumQueues, kQueueSize) {}
+  VirtioInputTest()
+      : event_queue_(phys_mem_, PAGE_SIZE * kNumQueues, kQueueSize),
+        status_queue_(phys_mem_, event_queue_.end(), kQueueSize) {}
 
   void SetUp() override {
     using component_testing::ChildRef;
@@ -30,11 +45,10 @@ class VirtioInputTest : public TestWithDevice {
     using component_testing::RealmRoot;
     using component_testing::Route;
 
-    constexpr auto kComponentUrl = "fuchsia-pkg://fuchsia.com/virtio_input#meta/virtio_input.cm";
     constexpr auto kComponentName = "virtio_input";
 
     auto realm_builder = RealmBuilder::Create();
-    realm_builder.AddChild(kComponentName, kComponentUrl);
+    realm_builder.AddChild(kComponentName, GetParam().component_url);
 
     realm_builder
         .AddRoute(Route{.capabilities =
@@ -60,7 +74,7 @@ class VirtioInputTest : public TestWithDevice {
     zx_status_t status = MakeStartInfo(event_queue_.end(), &start_info);
     ASSERT_EQ(ZX_OK, status);
 
-    keyboard_listener_ = realm_->ConnectSync<fuchsia::virtualization::hardware::KeyboardListener>();
+    keyboard_listener_ = realm_->Connect<fuchsia::virtualization::hardware::KeyboardListener>();
     pointer_listener_ = realm_->ConnectSync<fuchsia::virtualization::hardware::PointerListener>();
     input_ = realm_->ConnectSync<fuchsia::virtualization::hardware::VirtioInput>();
 
@@ -68,7 +82,7 @@ class VirtioInputTest : public TestWithDevice {
     ASSERT_EQ(ZX_OK, status);
 
     // Configure device queues.
-    VirtioQueueFake* queues[kNumQueues] = {&event_queue_};
+    VirtioQueueFake* queues[kNumQueues] = {&event_queue_, &status_queue_};
     for (uint16_t i = 0; i < kNumQueues; i++) {
       auto q = queues[i];
       q->Configure(PAGE_SIZE * i, PAGE_SIZE);
@@ -81,42 +95,81 @@ class VirtioInputTest : public TestWithDevice {
     ASSERT_EQ(ZX_OK, status);
   }
 
+  zx::status<std::vector<virtio_input_event_t*>> AddEventDescriptorsToChain(size_t n) {
+    std::vector<virtio_input_event_t*> result(n);
+    // Note: virtio-input sends only one virtio_input_event_t per chain.
+    for (size_t i = 0; i < n; ++i) {
+      virtio_input_event_t* event;
+      zx_status_t status = DescriptorChainBuilder(event_queue_)
+                               .AppendWritableDescriptor(&event, sizeof(*event))
+                               .Build();
+      if (status != ZX_OK) {
+        return zx::error(status);
+      }
+      result[i] = event;
+    }
+    return zx::ok(std::move(result));
+  }
+
+  bool IsRustComponent() { return GetParam().component_url == kRustComponentUrl; }
+
   // Note: use of sync can be problematic here if the test environment needs to handle
   // some incoming FIDL requests.
   fuchsia::virtualization::hardware::VirtioInputSyncPtr input_;
-  fuchsia::virtualization::hardware::KeyboardListenerSyncPtr keyboard_listener_;
+  fuchsia::virtualization::hardware::KeyboardListenerPtr keyboard_listener_;
   fuchsia::virtualization::hardware::PointerListenerSyncPtr pointer_listener_;
   VirtioQueueFake event_queue_;
+  VirtioQueueFake status_queue_;
   std::unique_ptr<component_testing::RealmRoot> realm_;
 };
 
-TEST_F(VirtioInputTest, Keyboard) {
-  fuchsia::ui::input::KeyboardEvent keyboard = {
-      .phase = fuchsia::ui::input::KeyboardEventPhase::PRESSED,
-      .hid_usage = 4,
-  };
-  keyboard_listener_->OnKeyboardEvent(std::move(keyboard));
+TEST_P(VirtioInputTest, Keyboard) {
+  // Enqueue descriptors for the events. Do this before we inject the key event because the device
+  // may choose to drop input events if there are no descriptors available:
+  //
+  // 5.8.6.2 Device Requirements: Device Operation
+  //
+  // A device MAY drop input events if the eventq does not have enough available buffers.
+  auto events = AddEventDescriptorsToChain(2);
+  ASSERT_TRUE(events.is_ok());
 
-  virtio_input_event_t* event_1;
-  virtio_input_event_t* event_2;
-  zx_status_t status = DescriptorChainBuilder(event_queue_)
-                           .AppendWritableDescriptor(&event_1, sizeof(*event_1))
-                           .AppendWritableDescriptor(&event_2, sizeof(*event_2))
-                           .Build();
+  zx_status_t status = input_->NotifyQueue(0);
   ASSERT_EQ(ZX_OK, status);
 
-  status = input_->NotifyQueue(0);
-  ASSERT_EQ(ZX_OK, status);
+  // Inject a key event.
+  std::optional<fuchsia::ui::input3::KeyEventStatus> key_status = std::nullopt;
+  if (IsRustComponent()) {
+    fuchsia::ui::input3::KeyEvent keyboard;
+    keyboard.set_type(fuchsia::ui::input3::KeyEventType::PRESSED);
+    keyboard.set_key(fuchsia::input::Key::A);
+    keyboard_listener_->OnKeyEvent(std::move(keyboard), [&](auto result) { key_status = result; });
+  } else {
+    fuchsia::ui::input::KeyboardEvent keyboard = {
+        .phase = fuchsia::ui::input::KeyboardEventPhase::PRESSED,
+        .hid_usage = 4,
+    };
+    keyboard_listener_->OnKeyboardEvent(std::move(keyboard));
+  }
+
+  // Expect the virtio event.
   status = WaitOnInterrupt();
   ASSERT_EQ(ZX_OK, status);
 
+  // Verify we received 2 events; key press + sync.
+  auto event_1 = events.value()[0];
   EXPECT_EQ(VIRTIO_INPUT_EV_KEY, event_1->type);
   EXPECT_EQ(30, event_1->code);
   EXPECT_EQ(VIRTIO_INPUT_EV_KEY_PRESSED, event_1->value);
+  auto event_2 = events.value()[1];
   EXPECT_EQ(VIRTIO_INPUT_EV_SYN, event_2->type);
 }
 
-TEST_F(VirtioInputTest, PointerMove) {
+TEST_P(VirtioInputTest, PointerMove) {
+  // TODO(fxbug.dev/104229): Enable this test.
+  if (IsRustComponent()) {
+    GTEST_SKIP();
+  }
+
   pointer_listener_->OnSizeChanged({1, 1});
   fuchsia::ui::input::PointerEvent pointer = {
       .phase = fuchsia::ui::input::PointerEventPhase::MOVE,
@@ -149,7 +202,12 @@ TEST_F(VirtioInputTest, PointerMove) {
   EXPECT_EQ(VIRTIO_INPUT_EV_SYN, event_3->type);
 }
 
-TEST_F(VirtioInputTest, PointerUp) {
+TEST_P(VirtioInputTest, PointerUp) {
+  // TODO(fxbug.dev/104229): Enable this test.
+  if (IsRustComponent()) {
+    GTEST_SKIP();
+  }
+
   pointer_listener_->OnSizeChanged({1, 1});
   fuchsia::ui::input::PointerEvent pointer = {
       .phase = fuchsia::ui::input::PointerEventPhase::UP,
@@ -186,5 +244,12 @@ TEST_F(VirtioInputTest, PointerUp) {
   EXPECT_EQ(VIRTIO_INPUT_EV_KEY_RELEASED, event_3->value);
   EXPECT_EQ(VIRTIO_INPUT_EV_SYN, event_4->type);
 }
+
+INSTANTIATE_TEST_SUITE_P(VirtioInputComponentsTest, VirtioInputTest,
+                         testing::Values(VirtioInputTestParam{"cpp", kCppComponentUrl},
+                                         VirtioInputTestParam{"rust", kRustComponentUrl}),
+                         [](const testing::TestParamInfo<VirtioInputTestParam>& info) {
+                           return info.param.test_name;
+                         });
 
 }  // namespace
