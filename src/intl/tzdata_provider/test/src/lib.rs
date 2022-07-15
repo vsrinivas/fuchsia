e8d93ddd2ebbad70ff5d1e8a8e22fb2a0acfa4b7
Yegor Pomortsev <ypomortsev@google.com>
2022-07-15 00:45:58 +0000

    [intl] Add tzdata-provider
    The tzdata-provider component exposes ICU timezone data as a
    directory capability.
    
    Bug: 85845
    Change-Id: Ic70f3999153462d1ed7f3a31ce9acdbc564800cb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/700788
    Commit-Queue: Yegor Pomortsev <ypomortsev@google.com>
    Reviewed-by: Konstantin Pozin <kpozin@google.com>

---
+++ src/intl/tzdata_provider/test/src/lib.rs
@@ -0,0 +1,27 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {icu_data::Loader, rust_icu_ucal as ucal};
+
+#[fuchsia::test]
+async fn test_tzdata() {
+    const TZDATA_DIR: &str = "/tzdata/icu/44/le";
+    const REVISION_FILE_PATH: &str = "/tzdata/revision.txt";
+
+    let _loader = Loader::new_with_tz_resources_and_validation(TZDATA_DIR, REVISION_FILE_PATH)
+        .expect("Failed to create Loader");
+
+    let _version = ucal::get_tz_data_version().expect("Failed to get tzdata version");
+}
+
+#[fuchsia::test]
+async fn test_tzdata_icu_44_le() {
+    const TZDATA_DIR: &str = "/tzdata-icu-44-le";
+    const REVISION_FILE_PATH: &str = "/tzdata-icu-44-le/revision.txt";
+
+    let _loader = Loader::new_with_tz_resources_and_validation(TZDATA_DIR, REVISION_FILE_PATH)
+        .expect("Failed to create Loader");
+
+    let _version = ucal::get_tz_data_version().expect("Failed to get tzdata version");
+}
