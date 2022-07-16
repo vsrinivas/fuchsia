119723b848f74440903fcaa5d22b11dbec82197d
Natalie Eleftheriades <natlf@google.com>
2022-07-16 03:46:25 +0000

    [fx] Fix remote workflow bug.
    Passing the combined string of artifacts to rsync seemed to be causing
    it to fail for some users.
    This CL swaps the joined variable with a loop through each artifact.
    
    Change-Id: Id0f26c69a6c5a36eb3f5617cbfdba950707afbfd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/694022
    Fuchsia-Auto-Submit: Natalie Eleftheriades <natlf@google.com>
    Reviewed-by: Renato Mangini Dias <mangini@google.com>
    Commit-Queue: Natalie Eleftheriades <natlf@google.com>

--- tools/devshell/lib/fx-remote.sh
+++ tools/devshell/lib/fx-remote.sh
@@ -59,15 +59,21 @@ function fetch_remote_artifacts {
   local artifacts=("$@")
 
   local remote_build_dir="$(get_remote_build_dir "${host}" "${remote_checkout}")" || exit $?
-  local joined=$(printf "%s," "${artifacts[@]}")
   mkdir -p "${local_dir}"
+
+  local -a full_paths_array
+  for artifact in "${artifacts[@]}"
+  do
+    full_paths_array+=("${host}:${remote_build_dir}/./${artifact}")
+  done
+
   # --relative ensures that the artifacts are copied to out/fetched relative to
   # the '/./' (i.e., the build directory).
   # --times preserves the modification timestamp, which rsync uses in conjunction
   # with filesize to determine if the file changed (unless the --checksum
   # parameter is used). Without one of either --times or --checksum, identical
   # files will be re-transfered on each rsync invocation.
-  rsync --compress --partial --progress --relative --times "${host}:${remote_build_dir}/./{${joined}}" "${local_dir}" || exit $?
+  rsync --compress --partial --progress --relative --times "${full_paths_array[@]}" "${local_dir}" || exit $?
 }
 
 # Fetch all build artifacts for a given mode, optionally building them first
