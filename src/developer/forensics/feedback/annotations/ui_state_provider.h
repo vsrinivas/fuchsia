a9c277867aeaafc879064916a9965e53e60e28af
Thomas Gales <tgales@google.com>
2022-07-19 18:38:08 +0000

    [forensics] Connect UIStateProvider to server
    Connect UIStateProvider to server and use
    fuchsia::ui::activity::ProviderPtr to receive updates to ui activity
    state from said server.
    
    Bug: 95158
    
    Test: fx test ui_state_provider_unittest
    
    Change-Id: I481cba17cb2a78d484b86dda5260197314010de2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/701101
    Commit-Queue: Thomas Gales <tgales@google.com>
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>
    (cherry picked from commit a3cf5c14895bfce788b208a48b69b431b50be17f)

--- src/developer/forensics/feedback/annotations/ui_state_provider.h
+++ src/developer/forensics/feedback/annotations/ui_state_provider.h
@@ -6,16 +6,21 @@
 #define SRC_DEVELOPER_FORENSICS_FEEDBACK_ANNOTATIONS_UI_STATE_PROVIDER_H_
 
 #include <fuchsia/ui/activity/cpp/fidl.h>
+#include <lib/async/cpp/task.h>
+#include <lib/fidl/cpp/binding.h>
 #include <lib/fit/function.h>
+#include <lib/sys/cpp/service_directory.h>
 #include <lib/zx/time.h>
 
 #include <memory>
 #include <optional>
 #include <set>
 #include <string>
+#include <variant>
 
 #include "src/developer/forensics/feedback/annotations/provider.h"
 #include "src/developer/forensics/feedback/annotations/types.h"
+#include "src/lib/backoff/backoff.h"
 #include "src/lib/timekeeper/clock.h"
 
 namespace forensics::feedback {
@@ -25,7 +30,9 @@ class UIStateProvider : public fuchsia::ui::activity::Listener,
                         public CachedAsyncAnnotationProvider,
                         public DynamicSyncAnnotationProvider {
  public:
-  explicit UIStateProvider(std::unique_ptr<timekeeper::Clock> clock);
+  UIStateProvider(async_dispatcher_t* dispatcher, std::shared_ptr<sys::ServiceDirectory> services,
+                  std::unique_ptr<timekeeper::Clock> clock,
+                  std::unique_ptr<backoff::Backoff> backoff);
 
   std::set<std::string> GetKeys() const override;
 
@@ -38,12 +45,22 @@ class UIStateProvider : public fuchsia::ui::activity::Listener,
                       OnStateChangedCallback callback) override;
 
  private:
-  std::optional<std::string> current_state_;
-  std::optional<zx::time> last_transition_time_;
+  void OnDisconnect();
+  void StartListening();
+
+  async_dispatcher_t* dispatcher_;
+  const std::shared_ptr<sys::ServiceDirectory> services_;
+  std::unique_ptr<timekeeper::Clock> clock_;
+  std::unique_ptr<backoff::Backoff> backoff_;
+
+  std::optional<ErrorOr<std::string>> current_state_;
+  std::variant<std::monostate, Error, zx::time> last_transition_time_;
 
   ::fit::function<void(Annotations)> on_update_;
 
-  std::unique_ptr<timekeeper::Clock> clock_;
+  fuchsia::ui::activity::ProviderPtr provider_ptr_;
+  fidl::Binding<fuchsia::ui::activity::Listener> binding_{this};
+  async::TaskClosureMethod<UIStateProvider, &UIStateProvider::StartListening> reconnect_task_{this};
 };
 
 }  // namespace forensics::feedback
