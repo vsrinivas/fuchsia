d94dce58b23f3a0d761d0f18bface90209b9ca49
Braden Kell <bradenkell@google.com>
2022-07-19 22:24:07 +0000

    [visalia][boot] Add back boot shim and image package scripts for Visalia
    These were previously deleted in
    I27bd774381f5f16ff6824a665b758f411a136ba4. Add them back so that the
    visalia board can be used following changes in vendor/google.
    
    Bug: 94099
    Test: Flashed, paved Visalia with pending changes in vendor/google.
    Change-Id: I0c65ea2fe951720c35420154184a1c38f1196b65
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/701923
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Commit-Queue: Braden Kell <bradenkell@google.com>

--- zircon/kernel/target/arm64/board/as370/package-visalia-image.sh
+++ zircon/kernel/target/arm64/board/as370/package-visalia-image.sh
@@ -14,4 +14,4 @@ SCRIPTS_DIR="${ZIRCON_DIR}/scripts"
 
 "${SCRIPTS_DIR}/package-image.sh" -b visalia \
     -d "$ZIRCON_DIR/kernel/target/arm64/dtb/dummy-device-tree.dtb" -D mkbootimg \
-    -l -M 0x02000000 -K 0x00000000 $@
+    -l -M 0x02000000 -K 0x00000000 -B "$(pwd)" $@
