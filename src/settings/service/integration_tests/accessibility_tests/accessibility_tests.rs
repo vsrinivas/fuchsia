cbc9527d3f3652591d4bd55837163e3ce7080d62
Sijie Chen <sijiec@google.com>
2022-07-19 01:06:03 +0000

    [Settings][RB] Migrated a11y Set integration tests
    This CL migrates a11y Set integration tests to use RB.
    
    Fixed: 103794
    Test:
    fx test -o accessibility_integration_test
    
    Change-Id: Iedf1eaffaf6557863c07ede756f56360b8942be3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/700510
    Commit-Queue: Sijie Chen <sijiec@google.com>
    Reviewed-by: William Xiao <wxyz@google.com>

---
+++ src/settings/service/integration_tests/accessibility_tests/accessibility_tests.rs
@@ -0,0 +1,122 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use crate::lib::AccessibilityTest;
+use fidl_fuchsia_settings::*;
+use fidl_fuchsia_ui_types::ColorRgba;
+mod lib;
+
+#[fuchsia::test]
+async fn test_accessibility_set_all() {
+    const CHANGED_COLOR_BLINDNESS_TYPE: ColorBlindnessType = ColorBlindnessType::Tritanomaly;
+    const TEST_COLOR: ColorRgba = ColorRgba { red: 238.0, green: 23.0, blue: 128.0, alpha: 255.0 };
+    const CHANGED_FONT_STYLE: CaptionFontStyle = CaptionFontStyle {
+        family: Some(CaptionFontFamily::Casual),
+        color: Some(TEST_COLOR),
+        relative_size: Some(1.0),
+        char_edge_style: Some(EdgeStyle::Raised),
+        ..CaptionFontStyle::EMPTY
+    };
+    const CHANGED_CAPTION_SETTINGS: CaptionsSettings = CaptionsSettings {
+        for_media: Some(true),
+        for_tts: Some(true),
+        font_style: Some(CHANGED_FONT_STYLE),
+        window_color: Some(TEST_COLOR),
+        background_color: Some(TEST_COLOR),
+        ..CaptionsSettings::EMPTY
+    };
+
+    let initial_settings = AccessibilitySettings::EMPTY;
+
+    let mut expected_settings = AccessibilitySettings::EMPTY;
+    expected_settings.audio_description = Some(true);
+    expected_settings.screen_reader = Some(true);
+    expected_settings.color_inversion = Some(true);
+    expected_settings.enable_magnification = Some(true);
+    expected_settings.color_correction = Some(CHANGED_COLOR_BLINDNESS_TYPE.into());
+    expected_settings.captions_settings = Some(CHANGED_CAPTION_SETTINGS);
+
+    let instance = AccessibilityTest::create_realm().await.expect("setting up test realm");
+
+    {
+        let proxy = AccessibilityTest::connect_to_accessibilitymarker(&instance);
+
+        // Make a watch call.
+        let settings = proxy.watch().await.expect("watch completed");
+        assert_eq!(settings, initial_settings);
+
+        // Ensure setting interface propagates correctly
+        proxy.set(expected_settings.clone()).await.expect("set completed").expect("set successful");
+    }
+
+    {
+        let proxy = AccessibilityTest::connect_to_accessibilitymarker(&instance);
+        // Ensure retrieved value matches set value
+        let settings = proxy.watch().await.expect("watch completed");
+        assert_eq!(settings, expected_settings);
+    }
+
+    let _ = instance.destroy().await;
+}
+
+#[fuchsia::test]
+async fn test_accessibility_set_captions() {
+    const CHANGED_FONT_STYLE: CaptionFontStyle = CaptionFontStyle {
+        family: Some(CaptionFontFamily::Casual),
+        color: None,
+        relative_size: Some(1.0),
+        char_edge_style: None,
+        ..CaptionFontStyle::EMPTY
+    };
+    const EXPECTED_CAPTIONS_SETTINGS: CaptionsSettings = CaptionsSettings {
+        for_media: Some(true),
+        for_tts: None,
+        font_style: Some(CHANGED_FONT_STYLE),
+        window_color: Some(ColorRgba { red: 238.0, green: 23.0, blue: 128.0, alpha: 255.0 }),
+        background_color: None,
+        ..CaptionsSettings::EMPTY
+    };
+
+    let mut expected_settings = AccessibilitySettings::EMPTY;
+    expected_settings.captions_settings = Some(EXPECTED_CAPTIONS_SETTINGS);
+
+    let instance = AccessibilityTest::create_realm().await.expect("setting up test realm");
+
+    {
+        let proxy = AccessibilityTest::connect_to_accessibilitymarker(&instance);
+
+        // Set for_media and window_color in the top-level CaptionsSettings.
+        let mut first_set = AccessibilitySettings::EMPTY;
+        first_set.captions_settings = Some(CaptionsSettings {
+            for_media: Some(false),
+            for_tts: None,
+            font_style: None,
+            window_color: EXPECTED_CAPTIONS_SETTINGS.window_color,
+            background_color: None,
+            ..CaptionsSettings::EMPTY
+        });
+        proxy.set(first_set.clone()).await.expect("set completed").expect("set successful");
+
+        // Set FontStyle and overwrite for_media.
+        let mut second_set = AccessibilitySettings::EMPTY;
+        second_set.captions_settings = Some(CaptionsSettings {
+            for_media: EXPECTED_CAPTIONS_SETTINGS.for_media,
+            for_tts: None,
+            font_style: EXPECTED_CAPTIONS_SETTINGS.font_style,
+            window_color: None,
+            background_color: None,
+            ..CaptionsSettings::EMPTY
+        });
+        proxy.set(second_set.clone()).await.expect("set completed").expect("set successful");
+    }
+
+    {
+        let proxy = AccessibilityTest::connect_to_accessibilitymarker(&instance);
+        // Ensure retrieved value matches set value
+        let settings = proxy.watch().await.expect("watch completed");
+        assert_eq!(settings, expected_settings);
+    }
+
+    let _ = instance.destroy().await;
+}
--- src/settings/service/src/tests/accessibility_tests.rs
+++ src/settings/service/src/tests/accessibility_tests.rs
@@ -2,32 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use crate::accessibility::types::{AccessibilityInfo, ColorBlindnessType};
 use crate::agent::storage::storage_factory::testing::InMemoryStorageFactory;
 use crate::base::SettingType;
 use crate::ingress::fidl::Interface;
 use crate::tests::test_failure_utils::create_test_env_with_failures;
-use crate::EnvironmentBuilder;
 use assert_matches::assert_matches;
 use fidl::Error::ClientChannelClosed;
 use fidl_fuchsia_settings::*;
-use fidl_fuchsia_ui_types::ColorRgba;
 use fuchsia_zircon::Status;
 use std::sync::Arc;
 
 const ENV_NAME: &str = "settings_service_accessibility_test_environment";
-
-async fn create_test_accessibility_env(
-    storage_factory: Arc<InMemoryStorageFactory>,
-) -> AccessibilityProxy {
-    EnvironmentBuilder::new(storage_factory)
-        .fidl_interfaces(&[Interface::Accessibility])
-        .spawn_and_get_nested_environment(ENV_NAME)
-        .await
-        .unwrap()
-        .connect_to_protocol::<AccessibilityMarker>()
-        .unwrap()
-}
 
 // Creates an environment that will fail on a get request.
 async fn create_a11y_test_env_with_failures(
@@ -42,146 +27,6 @@ async fn create_a11y_test_env_with_failures(
     .await
     .connect_to_protocol::<AccessibilityMarker>()
     .unwrap()
-}
-
-#[fuchsia_async::run_until_stalled(test)]
-async fn test_accessibility_set_all() {
-    const CHANGED_COLOR_BLINDNESS_TYPE: ColorBlindnessType = ColorBlindnessType::Tritanomaly;
-    const TEST_COLOR: ColorRgba = ColorRgba { red: 238.0, green: 23.0, blue: 128.0, alpha: 255.0 };
-    const CHANGED_FONT_STYLE: CaptionFontStyle = CaptionFontStyle {
-        family: Some(CaptionFontFamily::Casual),
-        color: Some(TEST_COLOR),
-        relative_size: Some(1.0),
-        char_edge_style: Some(EdgeStyle::Raised),
-        ..CaptionFontStyle::EMPTY
-    };
-    const CHANGED_CAPTION_SETTINGS: CaptionsSettings = CaptionsSettings {
-        for_media: Some(true),
-        for_tts: Some(true),
-        font_style: Some(CHANGED_FONT_STYLE),
-        window_color: Some(TEST_COLOR),
-        background_color: Some(TEST_COLOR),
-        ..CaptionsSettings::EMPTY
-    };
-
-    let initial_settings = AccessibilitySettings::EMPTY;
-
-    let mut expected_settings = AccessibilitySettings::EMPTY;
-    expected_settings.audio_description = Some(true);
-    expected_settings.screen_reader = Some(true);
-    expected_settings.color_inversion = Some(true);
-    expected_settings.enable_magnification = Some(true);
-    expected_settings.color_correction = Some(CHANGED_COLOR_BLINDNESS_TYPE.into());
-    expected_settings.captions_settings = Some(CHANGED_CAPTION_SETTINGS);
-
-    let expected_info = AccessibilityInfo {
-        audio_description: expected_settings.audio_description,
-        screen_reader: expected_settings.screen_reader,
-        color_inversion: expected_settings.color_inversion,
-        enable_magnification: expected_settings.enable_magnification,
-        color_correction: Some(CHANGED_COLOR_BLINDNESS_TYPE),
-        captions_settings: Some(CHANGED_CAPTION_SETTINGS.into()),
-    };
-
-    // Create and fetch a store from device storage so we can read stored value for testing.
-    let factory = Arc::new(InMemoryStorageFactory::new());
-    let accessibility_proxy = create_test_accessibility_env(Arc::clone(&factory)).await;
-    let store = factory.get_device_storage().await;
-
-    // Fetch the initial value.
-    let settings = accessibility_proxy.watch().await.expect("watch completed");
-    assert_eq!(settings, initial_settings);
-
-    // Set the various settings values.
-    accessibility_proxy
-        .set(expected_settings.clone())
-        .await
-        .expect("set completed")
-        .expect("set successful");
-
-    // Verify the value we set is persisted in DeviceStorage.
-    let retrieved_struct = store.get().await;
-    assert_eq!(expected_info, retrieved_struct);
-
-    // Verify the value we set is returned when watching.
-    let settings = accessibility_proxy.watch().await.expect("watch completed");
-    assert_eq!(settings, expected_settings);
-}
-
-#[fuchsia_async::run_until_stalled(test)]
-async fn test_accessibility_set_captions() {
-    const CHANGED_FONT_STYLE: CaptionFontStyle = CaptionFontStyle {
-        family: Some(CaptionFontFamily::Casual),
-        color: None,
-        relative_size: Some(1.0),
-        char_edge_style: None,
-        ..CaptionFontStyle::EMPTY
-    };
-    const EXPECTED_CAPTIONS_SETTINGS: CaptionsSettings = CaptionsSettings {
-        for_media: Some(true),
-        for_tts: None,
-        font_style: Some(CHANGED_FONT_STYLE),
-        window_color: Some(ColorRgba { red: 238.0, green: 23.0, blue: 128.0, alpha: 255.0 }),
-        background_color: None,
-        ..CaptionsSettings::EMPTY
-    };
-
-    let mut expected_settings = AccessibilitySettings::EMPTY;
-    expected_settings.captions_settings = Some(EXPECTED_CAPTIONS_SETTINGS);
-
-    let expected_info = AccessibilityInfo {
-        audio_description: None,
-        screen_reader: None,
-        color_inversion: None,
-        enable_magnification: None,
-        color_correction: None,
-        captions_settings: Some(EXPECTED_CAPTIONS_SETTINGS.into()),
-    };
-
-    // Create and fetch a store from device storage so we can read stored value for testing.
-    let factory = Arc::new(InMemoryStorageFactory::new());
-    let accessibility_proxy = create_test_accessibility_env(Arc::clone(&factory)).await;
-    let store = factory.get_device_storage().await;
-
-    // Set for_media and window_color in the top-level CaptionsSettings.
-    let mut first_set = AccessibilitySettings::EMPTY;
-    first_set.captions_settings = Some(CaptionsSettings {
-        for_media: Some(false),
-        for_tts: None,
-        font_style: None,
-        window_color: EXPECTED_CAPTIONS_SETTINGS.window_color,
-        background_color: None,
-        ..CaptionsSettings::EMPTY
-    });
-    accessibility_proxy
-        .set(first_set.clone())
-        .await
-        .expect("set completed")
-        .expect("set successful");
-
-    // Set FontStyle and overwrite for_media.
-    let mut second_set = AccessibilitySettings::EMPTY;
-    second_set.captions_settings = Some(CaptionsSettings {
-        for_media: EXPECTED_CAPTIONS_SETTINGS.for_media,
-        for_tts: None,
-        font_style: EXPECTED_CAPTIONS_SETTINGS.font_style,
-        window_color: None,
-        background_color: None,
-        ..CaptionsSettings::EMPTY
-    });
-    accessibility_proxy
-        .set(second_set.clone())
-        .await
-        .expect("set completed")
-        .expect("set successful");
-
-    // Verify the value we set is persisted in DeviceStorage.
-    let retrieved_struct = store.get().await;
-    assert_eq!(expected_info, retrieved_struct);
-
-    // Verify the value we set is returned when watching.
-    let settings = accessibility_proxy.watch().await.expect("watch completed");
-    assert_eq!(settings, expected_settings);
 }
 
 #[fuchsia_async::run_until_stalled(test)]
