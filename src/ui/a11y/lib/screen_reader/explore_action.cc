1511674af865de3d7e9ab93e532694dd4bc89606
Alexander Brusher <abrusher@google.com>
2022-07-25 22:09:24 +0000

    [a11y] Remove hack to force screen reader to explore virtual keyboard view.
    With the flutter overlay hit testing fix, we no longer need to force hits to the virtual keyboard.
    
    Bug: 81122
    Test: manually verified; fx test a11y-lib-tests
    Change-Id: I933a7922c0231347ae6f386f4d26c493c33db96c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/703309
    Commit-Queue: Alexander Brusher <abrusher@google.com>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    (cherry picked from commit 37a975227f780c7dcc0e43c62f7ce3229e542830)

--- src/ui/a11y/lib/screen_reader/explore_action.cc
+++ src/ui/a11y/lib/screen_reader/explore_action.cc
@@ -93,12 +93,6 @@ void ExploreAction::Run(GestureContext gesture_context) {
   // TODO(fxbug.dev/95647): Use activity service to detect when user is using a fuchsia device.
   screen_reader_context_->set_last_interaction(async::Now(async_get_default_dispatcher()));
 
-  // TODO(fxbug.dev/81122): Remove workaround to hit virtual keyboard.
-  auto virtualkeyboard_view =
-      action_context_->semantics_source->GetViewWithVisibleVirtualkeyboard();
-  if (virtualkeyboard_view) {
-    gesture_context.view_ref_koid = *virtualkeyboard_view;
-  }
   auto promise =
       ExecuteHitTestingPromise(gesture_context)
           .and_then([this, view_koid = gesture_context.view_ref_koid](
