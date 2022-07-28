ccc710127e829ce50615fe1ea263d2582d5e86fd
Yifei Teng <yifeit@google.com>
2022-07-28 08:29:26 +0000

    [fidl][cpp] Client support for fpromise promises
    Today fpromise is the C++ promise library used in production. This CL
    adds a helper library to transform callback-style async FIDL calls to
    promises.
    
    Because there is a new C++ fasync futures library on the horizon, this
    CL opts to keep fpromise support in a separate library instead of built
    into the C++ bindings. We'd like to reserve the built-in support for
    fasync.
    
    Change-Id: I5b0076c998209d39a5dbabd7a5c292128bb11352
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/698173
    Reviewed-by: Zachary Stewart <zstewart@google.com>
    Fuchsia-Auto-Submit: Yifei Teng <yifeit@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Commit-Queue: Yifei Teng <yifeit@google.com>

---
+++ src/lib/fidl/cpp/contrib/fpromise/client.h
@@ -0,0 +1,81 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_LIB_FIDL_CPP_CONTRIB_FPROMISE_CLIENT_H_
+#define SRC_LIB_FIDL_CPP_CONTRIB_FPROMISE_CLIENT_H_
+
+#include <lib/fidl/cpp/internal/thenable.h>
+#include <lib/fpromise/bridge.h>
+
+namespace fidl_fpromise {
+
+namespace internal {
+
+// |fpromise::result| uses |void| for when the result does not have a success type.
+// On the other hand, |fitx::result| simply leaves the |value_type| alias absent.
+// This SFINAE converts one scheme to the other.
+template <typename T, typename V = void>
+struct ValueTypeOrVoid {
+  using type = V;
+};
+template <typename T>
+struct ValueTypeOrVoid<T, std::void_t<typename T::value_type>> {
+  using type = typename T::value_type;
+};
+
+}  // namespace internal
+
+// |as_promise| converts a FIDL asynchronous call in the new C++ bindings
+// into a promise. Example usage:
+//
+//     // Let's say an async FIDL call originally uses a callback.
+//     fidl::Client<MyProtocol> client(...);
+//     client->Foo({ ... }).Then([] (fidl::Result<MyProtocol::Foo>& result) {
+//       assert(result.is_ok());
+//     });
+//
+//     // It can be turned into a promise by wrapping the call with |as_promise|
+//     // as opposed to attaching a callback via |Then|:
+//     auto p1 = fidl_fpromise::as_promise(client->Foo({ ... }));
+//
+//     // And used like any other regular promise:
+//     auto p2 = p1.then([] (auto& result) {
+//       assert(result.is_ok());
+//     });
+//     some_executor.schedule_task(std::move(p2));
+//
+// The signature of the resulting promise is akin to
+//
+//     fpromise::promise<SuccessType, ErrorType>
+//
+// where |SuccessType| is the payload type for when the FIDL call succeeds, or
+// |void| if the FIDL call has an empty/zero-argument payload.
+// where |ErrorType| is |fidl::Error| if the FIDL call does not use application
+// errors, and |fidl::AnyErrorIn<MyProtocol::FidlMethod>| otherwise. |MyProtocol|
+// and |FidlMethod| are all placeholders to be replaced by the actual protocol
+// and method names.
+template <typename FidlMethod>
+auto as_promise(::fidl::internal::NaturalThenable<FidlMethod>&& thenable) {
+  using ResultType = typename FidlMethod::Protocol::Transport::template Result<FidlMethod>;
+  using E = typename ResultType::error_type;
+  using V = typename internal::ValueTypeOrVoid<ResultType>::type;
+  ::fpromise::bridge<V, E> bridge;
+  std::move(thenable).ThenExactlyOnce(
+      [completer = std::move(bridge.completer)](auto&& result) mutable {
+        if (result.is_ok()) {
+          if constexpr (std::is_same_v<V, void>) {
+            completer.complete_ok();
+          } else {
+            completer.complete_ok(std::move(result.value()));
+          }
+        } else {
+          completer.complete_error(std::move(result.error_value()));
+        }
+      });
+  return bridge.consumer.promise();
+}
+
+}  // namespace fidl_fpromise
+
+#endif  // SRC_LIB_FIDL_CPP_CONTRIB_FPROMISE_CLIENT_H_
---
+++ src/lib/fidl/cpp/contrib/tests/fpromise/client_test.cc
@@ -0,0 +1,230 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/lib/fidl/cpp/contrib/fpromise/client.h"
+
+#include <fidl/fidl.test.coding.fuchsia/cpp/fidl.h>
+#include <fidl/llcpptest.protocol.test/cpp/fidl.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async/cpp/executor.h>
+#include <lib/fpromise/promise.h>
+
+#include <zxtest/zxtest.h>
+
+namespace {
+
+constexpr int32_t kExpectedReply = 7;
+
+struct EchoServer : fidl::Server<fidl_test_coding_fuchsia::Simple> {
+  void Echo(EchoRequest& request, EchoCompleter::Sync& completer) override {
+    completer.Reply(request.request());
+  }
+  void Close(CloseRequest& request, CloseCompleter::Sync& completer) override {
+    ADD_FAILURE("Unused");
+  }
+};
+
+TEST(Client, Promisify) {
+  auto server = std::make_unique<EchoServer>();
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async::Executor executor(loop.dispatcher());
+  zx::status endpoints = fidl::CreateEndpoints<fidl_test_coding_fuchsia::Simple>();
+  ASSERT_OK(endpoints.status_value());
+  fidl::BindServer(loop.dispatcher(), std::move(endpoints->server), server.get());
+  fidl::Client client(std::move(endpoints->client), loop.dispatcher());
+
+  fpromise::promise<fidl_test_coding_fuchsia::SimpleEchoTopResponse, fidl::Error> p =
+      fidl_fpromise::as_promise(client->Echo({kExpectedReply}));
+
+  auto task = p.then(
+      [&](fpromise::result<fidl_test_coding_fuchsia::SimpleEchoTopResponse, fidl::Error>& result) {
+        ASSERT_TRUE(result.is_ok());
+        ASSERT_EQ(kExpectedReply, result.value());
+        loop.Quit();
+      });
+
+  executor.schedule_task(std::move(task));
+  ASSERT_STATUS(ZX_ERR_CANCELED, loop.Run());
+}
+
+TEST(Client, PromisifyChaining) {
+  auto server = std::make_unique<EchoServer>();
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async::Executor executor(loop.dispatcher());
+  zx::status endpoints = fidl::CreateEndpoints<fidl_test_coding_fuchsia::Simple>();
+  ASSERT_OK(endpoints.status_value());
+  fidl::BindServer(loop.dispatcher(), std::move(endpoints->server), server.get());
+  fidl::Client client(std::move(endpoints->client), loop.dispatcher());
+
+  // Chain another continuation which operates on the FIDL result.
+  auto p = fidl_fpromise::as_promise(client->Echo({kExpectedReply}))
+               .and_then([&](const fidl_test_coding_fuchsia::SimpleEchoTopResponse& payload) {
+                 // Convert response number to string
+                 return fpromise::ok(std::to_string(payload.reply()));
+               });
+
+  // |p| is now transformed to a promise that resolves to a string.
+  auto task = p.then([&](fpromise::result<std::string, fidl::Error>& result) {
+    ASSERT_TRUE(result.is_ok());
+    ASSERT_EQ("7", result.value());
+    loop.Quit();
+  });
+
+  executor.schedule_task(std::move(task));
+  ASSERT_STATUS(ZX_ERR_CANCELED, loop.Run());
+}
+
+TEST(Client, PromisifyTransportError) {
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async::Executor executor(loop.dispatcher());
+  zx::status endpoints = fidl::CreateEndpoints<fidl_test_coding_fuchsia::Simple>();
+  ASSERT_OK(endpoints.status_value());
+  fidl::Client client(std::move(endpoints->client), loop.dispatcher());
+
+  fpromise::promise<fidl_test_coding_fuchsia::SimpleEchoTopResponse, fidl::Error> p =
+      fidl_fpromise::as_promise(client->Echo({kExpectedReply}));
+  endpoints->server.reset();
+
+  auto task = p.then(
+      [&](fpromise::result<fidl_test_coding_fuchsia::SimpleEchoTopResponse, fidl::Error>& result) {
+        ASSERT_TRUE(result.is_error());
+        ASSERT_TRUE(result.error().is_peer_closed());
+        loop.Quit();
+      });
+
+  executor.schedule_task(std::move(task));
+  ASSERT_STATUS(ZX_ERR_CANCELED, loop.Run());
+}
+
+class ErrorServer : public fidl::Server<llcpptest_protocol_test::ErrorMethods> {
+  void NoArgsPrimitiveError(NoArgsPrimitiveErrorRequest& request,
+                            NoArgsPrimitiveErrorCompleter::Sync& completer) final {
+    completer.Reply(fitx::error(42));
+  }
+  void ManyArgsCustomError(ManyArgsCustomErrorRequest& request,
+                           ManyArgsCustomErrorCompleter::Sync& completer) final {
+    completer.Reply(fitx::error(llcpptest_protocol_test::MyError::kBadError));
+  }
+};
+
+TEST(Client, PromisifyApplicationErrorMethodCasePrimitiveError) {
+  auto server = std::make_unique<ErrorServer>();
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async::Executor executor(loop.dispatcher());
+  zx::status endpoints = fidl::CreateEndpoints<llcpptest_protocol_test::ErrorMethods>();
+  ASSERT_OK(endpoints.status_value());
+  fidl::BindServer(loop.dispatcher(), std::move(endpoints->server), server.get());
+  fidl::Client client(std::move(endpoints->client), loop.dispatcher());
+
+  fpromise::promise<void,
+                    fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::NoArgsPrimitiveError>>
+      p = fidl_fpromise::as_promise(client->NoArgsPrimitiveError({{.should_error = true}}));
+  endpoints->server.reset();
+
+  auto task = p.then(
+      [&](fpromise::result<
+          void, fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::NoArgsPrimitiveError>>&
+              result) {
+        ASSERT_TRUE(result.is_error());
+        ASSERT_TRUE(result.error().is_application_error());
+        ASSERT_EQ(42, result.error().application_error());
+        loop.Quit();
+      });
+
+  executor.schedule_task(std::move(task));
+  ASSERT_STATUS(ZX_ERR_CANCELED, loop.Run());
+}
+
+TEST(Client, PromisifyApplicationErrorMethodCaseCustomError) {
+  auto server = std::make_unique<ErrorServer>();
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async::Executor executor(loop.dispatcher());
+  zx::status endpoints = fidl::CreateEndpoints<llcpptest_protocol_test::ErrorMethods>();
+  ASSERT_OK(endpoints.status_value());
+  fidl::BindServer(loop.dispatcher(), std::move(endpoints->server), server.get());
+  fidl::Client client(std::move(endpoints->client), loop.dispatcher());
+
+  fpromise::promise<llcpptest_protocol_test::ErrorMethodsManyArgsCustomErrorResponse,
+                    fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::ManyArgsCustomError>>
+      p = fidl_fpromise::as_promise(client->ManyArgsCustomError({{.should_error = true}}));
+
+  auto task = p.then(
+      [&](fpromise::result<
+          llcpptest_protocol_test::ErrorMethodsManyArgsCustomErrorResponse,
+          fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::ManyArgsCustomError>>& result) {
+        ASSERT_TRUE(result.is_error());
+        ASSERT_TRUE(result.error().is_application_error());
+        ASSERT_EQ(llcpptest_protocol_test::MyError::kBadError, result.error().application_error());
+        loop.Quit();
+      });
+
+  executor.schedule_task(std::move(task));
+  ASSERT_STATUS(ZX_ERR_CANCELED, loop.Run());
+}
+
+class SuccessServer : public fidl::Server<llcpptest_protocol_test::ErrorMethods> {
+  void NoArgsPrimitiveError(NoArgsPrimitiveErrorRequest& request,
+                            NoArgsPrimitiveErrorCompleter::Sync& completer) final {
+    completer.Reply(fitx::ok());
+  }
+  void ManyArgsCustomError(ManyArgsCustomErrorRequest& request,
+                           ManyArgsCustomErrorCompleter::Sync& completer) final {
+    completer.Reply(fitx::ok(llcpptest_protocol_test::ErrorMethodsManyArgsCustomErrorResponse{
+        {.a = 1, .b = 2, .c = 3}}));
+  }
+};
+
+TEST(Client, PromisifyApplicationErrorMethodCaseNoArgsSuccess) {
+  auto server = std::make_unique<SuccessServer>();
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async::Executor executor(loop.dispatcher());
+  zx::status endpoints = fidl::CreateEndpoints<llcpptest_protocol_test::ErrorMethods>();
+  ASSERT_OK(endpoints.status_value());
+  fidl::BindServer(loop.dispatcher(), std::move(endpoints->server), server.get());
+  fidl::Client client(std::move(endpoints->client), loop.dispatcher());
+
+  fpromise::promise<void,
+                    fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::NoArgsPrimitiveError>>
+      p = fidl_fpromise::as_promise(client->NoArgsPrimitiveError({{.should_error = false}}));
+
+  auto task = p.then(
+      [&](fpromise::result<
+          void, fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::NoArgsPrimitiveError>>&
+              result) {
+        ASSERT_TRUE(result.is_ok());
+        loop.Quit();
+      });
+
+  executor.schedule_task(std::move(task));
+  ASSERT_STATUS(ZX_ERR_CANCELED, loop.Run());
+}
+
+TEST(Client, PromisifyApplicationErrorMethodCaseManyArgsSuccess) {
+  auto server = std::make_unique<SuccessServer>();
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async::Executor executor(loop.dispatcher());
+  zx::status endpoints = fidl::CreateEndpoints<llcpptest_protocol_test::ErrorMethods>();
+  ASSERT_OK(endpoints.status_value());
+  fidl::BindServer(loop.dispatcher(), std::move(endpoints->server), server.get());
+  fidl::Client client(std::move(endpoints->client), loop.dispatcher());
+
+  fpromise::promise<llcpptest_protocol_test::ErrorMethodsManyArgsCustomErrorResponse,
+                    fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::ManyArgsCustomError>>
+      p = fidl_fpromise::as_promise(client->ManyArgsCustomError({{.should_error = false}}));
+
+  auto task = p.then(
+      [&](fpromise::result<
+          llcpptest_protocol_test::ErrorMethodsManyArgsCustomErrorResponse,
+          fidl::AnyErrorIn<llcpptest_protocol_test::ErrorMethods::ManyArgsCustomError>>& result) {
+        ASSERT_TRUE(result.is_ok());
+        ASSERT_EQ(llcpptest_protocol_test::ErrorMethodsManyArgsCustomErrorResponse(1, 2, 3),
+                  result.value());
+        loop.Quit();
+      });
+
+  executor.schedule_task(std::move(task));
+  ASSERT_STATUS(ZX_ERR_CANCELED, loop.Run());
+}
+
+}  // namespace
