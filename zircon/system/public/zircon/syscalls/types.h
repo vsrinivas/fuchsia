44be5c39324f2554b7ae7b7a67dbbfd093f9075b
Roland McGrath <mcgrathr@google.com>
2022-08-01 23:21:26 +0000

    [syscalls] Remove unused zx_rrec_t typedef
    This is a long-obsolete type no longer used anywhere.
    
    Change-Id: I4f64faa1e513f68fad4e30c07fd539a4ff3bde7e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/705594
    API-Review: Roland McGrath <mcgrathr@google.com>
    API-Review: Carlos Pizano <cpu@google.com>
    Reviewed-by: Carlos Pizano <cpu@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>

--- zircon/system/public/zircon/syscalls/types.h
+++ zircon/system/public/zircon/syscalls/types.h
@@ -14,7 +14,6 @@ typedef struct zx_port_packet zx_port_packet_t;
 typedef struct zx_pci_bar zx_pci_bar_t;
 typedef struct zx_pcie_device_info zx_pcie_device_info_t;
 typedef struct zx_pci_init_arg zx_pci_init_arg_t;
-typedef union zx_rrec zx_rrec_t;
 typedef struct zx_system_powerctl_arg zx_system_powerctl_arg_t;
 typedef struct zx_profile_info zx_profile_info_t;
 typedef struct zx_smc_parameters zx_smc_parameters_t;
