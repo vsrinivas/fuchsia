bb8c175855c804fc62e377be088581ba32841dff
Sam Hansen <hansens@google.com>
2022-08-05 23:20:51 +0000

    [usb][lib] document USB_ROUNDUP and USB_ROUNDDOWN macros
    Change-Id: Ia0b0cfa288845f1ff8f23e55036d47f57811f05a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/708503
    Commit-Queue: Sam Hansen <hansens@google.com>
    Reviewed-by: Christopher Anderson <cja@google.com>

--- src/devices/usb/lib/usb/align.h
+++ src/devices/usb/lib/usb/align.h
@@ -5,12 +5,29 @@
 #ifndef SRC_DEVICES_USB_LIB_USB_ALIGN_H_
 #define SRC_DEVICES_USB_LIB_USB_ALIGN_H_
 
+// Returns min(`a` rounded up to `b * K`)
+//   s.t.
+//     K ∈ ℤ
+//     K > 0
+//     b > 0
+//
+// Do not copy these macros into new code, use fbl::round_up()
+// from fbl/algorithm.h.
 #define USB_ROUNDUP(a, b)       \
   ({                            \
     const __typeof(a) _a = (a); \
     const __typeof(b) _b = (b); \
     ((_a + _b - 1) / _b * _b);  \
   })
+
+// Returns max(`a` rounded down to `b * K`)
+//   s.t.
+//     K ∈ ℤ
+//     K > 0
+//     b > 0
+//
+// Do not copy these macros into new code, use fbl::round_down()
+// from fbl/algorithm.h.
 #define USB_ROUNDDOWN(a, b)     \
   ({                            \
     const __typeof(a) _a = (a); \
