19349595358d86a539255496bac89594ef2cc8b9
Prashanth Swaminathan <prashanthsw@google.com>
2022-08-08 23:51:19 +0000

    [build_info] Add shell package to query build info
    Bug: 106030
    Test: Confirmed with `fx shell build-info ...`.
    Change-Id: I0acea33213ab8be97d171a34966f74519722d19d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/708702
    Reviewed-by: Shai Barack <shayba@google.com>
    Reviewed-by: Xyan Bhatnagar <xbhatnag@google.com>
    Fuchsia-Auto-Submit: Prashanth Swaminathan <prashanthsw@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

---
+++ src/developer/build_info/shell.rs
@@ -0,0 +1,56 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::{Context as _, Error};
+use argh::FromArgs;
+use fidl_fuchsia_buildinfo::ProviderMarker;
+use fuchsia_component::client::connect_to_protocol;
+use futures as _;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Info {
+    BoardConfig,
+    LatestCommitDate,
+    ProductConfig,
+    Version,
+}
+
+impl std::str::FromStr for Info {
+    type Err = String;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value {
+            "board_config" => Ok(Info::BoardConfig),
+            "latest_commit_date" => Ok(Info::LatestCommitDate),
+            "product_config" => Ok(Info::ProductConfig),
+            "version" => Ok(Info::Version),
+            _ => Err(format!("Invalid key: {:?}", value)),
+        }
+    }
+}
+
+/// Build information key value.
+#[derive(Debug, PartialEq, FromArgs)]
+struct BuildInfoCmd {
+    #[argh(positional)]
+    info: Info,
+}
+
+#[fuchsia::main]
+async fn main() -> Result<(), Error> {
+    let provider = connect_to_protocol::<ProviderMarker>()
+        .context("Failed to connect to build info service")?;
+
+    let args: BuildInfoCmd = argh::from_env();
+    let info = provider.get_build_info().await?;
+    let output = match args.info {
+        Info::BoardConfig => info.board_config,
+        Info::LatestCommitDate => info.latest_commit_date,
+        Info::ProductConfig => info.product_config,
+        Info::Version => info.version,
+    };
+
+    println!("{}", output.unwrap());
+    Ok(())
+}
