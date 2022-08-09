c420b580e1b9b7afac1571abc661f028997e825d
Alexander Brusher <abrusher@google.com>
2022-08-09 20:19:44 +0000

    [test-ui-stack] Expose GraphicalPresenter
    Test: fx set ... --with //src/ui/testing/test_ui_stack:test-ui-stack; fx
    build
    
    Change-Id: I34b5ac0543c0a567b7751dee36375f379c0a9956
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/709285
    Commit-Queue: Alexander Brusher <abrusher@google.com>
    Reviewed-by: David Worsham <dworsham@google.com>

--- src/ui/testing/test_ui_stack/main.cc
+++ src/ui/testing/test_ui_stack/main.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <fuchsia/accessibility/semantics/cpp/fidl.h>
+#include <fuchsia/element/cpp/fidl.h>
 #include <fuchsia/ui/composition/cpp/fidl.h>
 #include <fuchsia/ui/input/cpp/fidl.h>
 #include <fuchsia/ui/input3/cpp/fidl.h>
@@ -64,6 +65,8 @@ int run_test_ui_stack(int argc, const char** argv) {
   // Base UI services.
   AddPublicService<fuchsia::accessibility::semantics::SemanticsManager>(
       context.get(), realm_exposed_services.get());
+  AddPublicService<fuchsia::element::GraphicalPresenter>(context.get(),
+                                                         realm_exposed_services.get());
   AddPublicService<fuchsia::ui::composition::Allocator>(context.get(),
                                                         realm_exposed_services.get());
   AddPublicService<fuchsia::ui::composition::Flatland>(context.get(), realm_exposed_services.get());
