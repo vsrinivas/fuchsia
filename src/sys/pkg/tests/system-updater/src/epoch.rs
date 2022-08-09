be305e03409745be918b2a30dd7f3f2b79dfeee4
Olivia Nordquist <nolivia@google.com>
2022-08-09 20:30:51 +0000

    [system-updater-tests] include UnsupportedDowngrade test
    Fixed: 72117
    
    Change-Id: I8a02b5e4992401999fa8c72efbd74824f6cd8283
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/709601
    Reviewed-by: Sen Jiang <senj@google.com>
    Commit-Queue: Olivia Nordquist <nolivia@google.com>

--- src/sys/pkg/tests/system-updater/src/epoch.rs
+++ src/sys/pkg/tests/system-updater/src/epoch.rs
@@ -35,25 +35,19 @@ async fn invalid_epoch() {
 }
 
 // When target epoch < current epoch, we should fail with the UnsupportedDowngrade reason.
-// Note: at this point, we can't yet integration test unsupported downgrades because the current
-// epoch is zero and the target epoch in update package cannot be lower than zero. Once we bump the
-// epoch, we can uncomment out this test.
-// TODO(fxbug.dev/72117): use this test.
-// ########################## BEGIN: CODE INTENTIONALLY COMMENTED OUT ##############################
-// #[fasync::run_singlethreaded(test)]
-// async fn unsupported_downgrade() {
-//     let env = TestEnv::builder().build();
-//     env.resolver
-//         .register_package("update", "upd4t3")
-//         .add_file("packages.json", make_packages_json([]))
-//         .add_file("epoch.json", make_epoch_json(SOURCE_EPOCH - 1));
+#[fasync::run_singlethreaded(test)]
+async fn unsupported_downgrade() {
+    let env = TestEnv::builder().build().await;
+    env.resolver
+        .register_package("update", "upd4t3")
+        .add_file("packages.json", make_packages_json([]))
+        .add_file("epoch.json", make_epoch_json(SOURCE_EPOCH - 1));
 
-//     let mut attempt = env.start_update().await.unwrap();
+    let mut attempt = env.start_update().await.unwrap();
 
-//     assert_eq!(attempt.next().await.unwrap().unwrap(), State::Prepare);
-//     assert_eq!(
-//         attempt.next().await.unwrap().unwrap(),
-//         State::FailPrepare(PrepareFailureReason::UnsupportedDowngrade)
-//     );
-// }
-// ########################## END: CODE INTENTIONALLY COMMENTED OUT ################################
+    assert_eq!(attempt.next().await.unwrap().unwrap(), State::Prepare);
+    assert_eq!(
+        attempt.next().await.unwrap().unwrap(),
+        State::FailPrepare(PrepareFailureReason::UnsupportedDowngrade)
+    );
+}
