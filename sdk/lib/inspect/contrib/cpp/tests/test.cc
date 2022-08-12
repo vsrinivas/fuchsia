0908039ec299b25368a98b190d7609dc007d5688
Xyan Bhatnagar <xbhatnag@google.com>
2022-08-12 21:09:33 +0000

    [inspect] Migrate archive_reader_integration_tests to v2
    This test now uses RealmBuilder in a CFv2 world.
    
    Bug: 102678
    Change-Id: I777ee8f6a6c28a129a1e537ae9e38271ca93a40d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/711804
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Shai Barack <shayba@google.com>
    Fuchsia-Auto-Submit: Xyan Bhatnagar <xbhatnag@google.com>
    Reviewed-by: Clayton McCray <claytonmccray@google.com>

--- sdk/lib/inspect/contrib/cpp/tests/test.cc
+++ sdk/lib/inspect/contrib/cpp/tests/test.cc
@@ -2,23 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async-loop/default.h>
 #include <lib/async/cpp/executor.h>
 #include <lib/async/default.h>
 #include <lib/inspect/contrib/cpp/archive_reader.h>
-#include <lib/sys/cpp/testing/test_with_environment_fixture.h>
+#include <lib/sys/component/cpp/testing/realm_builder.h>
+#include <lib/sys/cpp/component_context.h>
 
-#include <gmock/gmock.h>
+#include <memory>
+
 #include <rapidjson/pointer.h>
 #include <re2/re2.h>
 
-#include "lib/zx/time.h"
+#include "fuchsia/diagnostics/cpp/fidl.h"
+#include "lib/sys/cpp/service_directory.h"
+#include "rapidjson/document.h"
+#include "src/lib/testing/loop_fixture/real_loop_fixture.h"
 
 namespace component {
 namespace {
 
-using sys::testing::EnclosingEnvironment;
-using ::testing::Eq;
-using ::testing::Pointee;
+using namespace component_testing;
 
 std::string regex_replace(const std::string& input, re2::RE2& reg, const std::string& rewrite) {
   std::string output = input;
@@ -26,21 +31,14 @@ std::string regex_replace(const std::string& input, re2::RE2& reg, const std::st
   return output;
 }
 
-constexpr char kTestComponent1[] =
-    "fuchsia-pkg://fuchsia.com/archive_reader_integration_tests#meta/"
-    "archive_reader_test_app.cmx";
-constexpr char kTestComponent2[] =
-    "fuchsia-pkg://fuchsia.com/archive_reader_integration_tests#meta/"
-    "archive_reader_test_app_2.cmx";
-
-const char CMX1_EXPECTED_DATA[] = R"JSON({
+const char CM1_EXPECTED_DATA[] = R"JSON({
     "data_source": "Inspect",
     "metadata": {
-        "component_url": "fuchsia-pkg://fuchsia.com/archive_reader_integration_tests#meta/archive_reader_test_app.cmx",
+        "component_url": "REALM_BUILDER_URL_PREFIX/test_app",
         "filename": "fuchsia.inspect.Tree",
         "timestamp": TIMESTAMP
     },
-    "moniker": "test/archive_reader_test_app.cmx",
+    "moniker": "REALM_BUILDER_MONIKER_PREFIX/test_app",
     "payload": {
         "root": {
             "option_a": {
@@ -54,14 +52,14 @@ const char CMX1_EXPECTED_DATA[] = R"JSON({
     },
     "version": 1
 })JSON";
-const char CMX2_EXPECTED_DATA[] = R"JSON({
+const char CM2_EXPECTED_DATA[] = R"JSON({
     "data_source": "Inspect",
     "metadata": {
-        "component_url": "fuchsia-pkg://fuchsia.com/archive_reader_integration_tests#meta/archive_reader_test_app_2.cmx",
+        "component_url": "REALM_BUILDER_URL_PREFIX/test_app_2",
         "filename": "fuchsia.inspect.Tree",
         "timestamp": TIMESTAMP
     },
-    "moniker": "test/archive_reader_test_app_2.cmx",
+    "moniker": "REALM_BUILDER_MONIKER_PREFIX/test_app_2",
     "payload": {
         "root": {
             "option_a": {
@@ -76,64 +74,62 @@ const char CMX2_EXPECTED_DATA[] = R"JSON({
     "version": 1
 })JSON";
 
-class ArchiveReaderTest : public gtest::TestWithEnvironmentFixture {
+class ArchiveReaderTest : public gtest::RealLoopFixture {
  protected:
-  ArchiveReaderTest() : executor_(dispatcher()) {
-    environment_ = CreateNewEnclosingEnvironment("test", CreateServices());
+  ArchiveReaderTest() : executor_(dispatcher()) {}
 
-    {
-      fuchsia::sys::LaunchInfo launch_info;
-      launch_info.url = kTestComponent1;
-      bool ready = false;
-      environment_->CreateComponent(std::move(launch_info), controller_.NewRequest());
-      controller_.events().OnDirectoryReady = [&ready] { ready = true; };
-      RunLoopUntil([&ready] { return ready; });
-      if (!ready) {
-        printf("The output directory is not ready for component 1\n");
-      }
-      controller_->Detach();
-    }
+  void SetUp() override {
+    component_context_ = sys::ComponentContext::Create();
 
-    {
-      fuchsia::sys::LaunchInfo launch_info;
-      launch_info.url = kTestComponent2;
-      bool ready = false;
-      environment_->CreateComponent(std::move(launch_info), controller_.NewRequest());
-      controller_.events().OnDirectoryReady = [&ready] { ready = true; };
-      RunLoopUntil([&ready] { return ready; });
-      if (!ready) {
-        printf("The output directory is not ready for component 2\n");
-      }
-      controller_->Detach();
-    }
+    auto builder = RealmBuilder::Create();
+
+    builder.AddChild("test_app", "#meta/archive_reader_test_app.cm",
+                     ChildOptions{.startup_mode = StartupMode::EAGER});
+    builder.AddChild("test_app_2", "#meta/archive_reader_test_app.cm",
+                     ChildOptions{.startup_mode = StartupMode::EAGER});
+
+    builder.AddRoute(Route{.capabilities = {Protocol{"fuchsia.logger.LogSink"}},
+                           .source = ParentRef(),
+                           .targets = {ChildRef{"test_app"}, ChildRef{"test_app_2"}}});
+
+    realm_ = std::make_unique<RealmRoot>(builder.Build(dispatcher()));
   }
 
- protected:
-  async::Executor executor_;
+  void TearDown() override { realm_.reset(); }
+
+  async::Executor& executor() { return executor_; }
+
+  std::shared_ptr<sys::ServiceDirectory> svc() { return component_context_->svc(); }
+
+  std::string cm1_selector() {
+    return "realm_builder\\:" + realm_->GetChildName() + "/test_app:root";
+  }
+
+  std::string cm2_selector() {
+    return "realm_builder\\:" + realm_->GetChildName() + "/test_app_2:root";
+  }
 
  private:
-  std::unique_ptr<EnclosingEnvironment> environment_;
-  fuchsia::sys::ComponentControllerPtr controller_;
+  std::unique_ptr<RealmRoot> realm_;
+  async::Executor executor_;
+  std::unique_ptr<sys::ComponentContext> component_context_;
 };
 
 using ResultType = fpromise::result<std::vector<inspect::contrib::DiagnosticsData>, std::string>;
 
-constexpr char cmx1[] = "archive_reader_test_app.cmx";
-constexpr char cmx2[] = "archive_reader_test_app_2.cmx";
-
-constexpr char cmx1_selector[] = "test/archive_reader_test_app.cmx:root";
-constexpr char cmx2_selector[] = "test/archive_reader_test_app_2.cmx:root";
+constexpr char cm1[] = "test_app";
+constexpr char cm2[] = "test_app_2";
 
 TEST_F(ArchiveReaderTest, ReadHierarchy) {
-  inspect::contrib::ArchiveReader reader(
-      real_services()->Connect<fuchsia::diagnostics::ArchiveAccessor>(),
-      {cmx1_selector, cmx2_selector});
+  std::cerr << "RUNNING TEST" << std::endl;
+  inspect::contrib::ArchiveReader reader(svc()->Connect<fuchsia::diagnostics::ArchiveAccessor>(),
+                                         {cm1_selector(), cm2_selector()});
 
   ResultType result;
-  executor_.schedule_task(reader
-                              .SnapshotInspectUntilPresent(
-                                  {"archive_reader_test_app.cmx", "archive_reader_test_app_2.cmx"})
-                              .then([&](ResultType& r) { result = std::move(r); }));
+  executor().schedule_task(
+      reader.SnapshotInspectUntilPresent({"test_app", "test_app_2"}).then([&](ResultType& r) {
+        result = std::move(r);
+      }));
   RunLoopUntil([&] { return !!result; });
 
   ASSERT_TRUE(result.is_ok()) << "Error: " << result.error();
@@ -142,8 +138,8 @@ TEST_F(ArchiveReaderTest, ReadHierarchy) {
   std::sort(value.begin(), value.end(),
             [](auto& a, auto& b) { return a.component_name() < b.component_name(); });
 
-  EXPECT_EQ(cmx1, value[0].component_name());
-  EXPECT_EQ(cmx2, value[1].component_name());
+  EXPECT_EQ(cm1, value[0].component_name());
+  EXPECT_EQ(cm2, value[1].component_name());
 
   EXPECT_STREQ("v1", value[0].content()["root"]["version"].GetString());
   EXPECT_STREQ("v1", value[1].content()["root"]["version"].GetString());
@@ -152,15 +148,15 @@ TEST_F(ArchiveReaderTest, ReadHierarchy) {
 TEST_F(ArchiveReaderTest, ReadHierarchyWithAlternativeDispatcher) {
   async::Loop loop(&kAsyncLoopConfigNoAttachToCurrentThread);
   fuchsia::diagnostics::ArchiveAccessorPtr archive;
-  real_services()->Connect(archive.NewRequest(loop.dispatcher()));
+  svc()->Connect(archive.NewRequest(loop.dispatcher()));
   async::Executor local_executor(loop.dispatcher());
-  inspect::contrib::ArchiveReader reader(std::move(archive), {cmx1_selector, cmx2_selector});
+  inspect::contrib::ArchiveReader reader(std::move(archive), {cm1_selector(), cm2_selector()});
 
   ResultType result;
-  local_executor.schedule_task(reader
-                                   .SnapshotInspectUntilPresent({"archive_reader_test_app.cmx",
-                                                                 "archive_reader_test_app_2.cmx"})
-                                   .then([&](ResultType& r) { result = std::move(r); }));
+  local_executor.schedule_task(
+      reader.SnapshotInspectUntilPresent({"test_app", "test_app_2"}).then([&](ResultType& r) {
+        result = std::move(r);
+      }));
 
   // Use the alternate loop.
   while (true) {
@@ -176,23 +172,22 @@ TEST_F(ArchiveReaderTest, ReadHierarchyWithAlternativeDispatcher) {
   std::sort(value.begin(), value.end(),
             [](auto& a, auto& b) { return a.component_name() < b.component_name(); });
 
-  EXPECT_EQ(cmx1, value[0].component_name());
-  EXPECT_EQ(cmx2, value[1].component_name());
+  EXPECT_EQ(cm1, value[0].component_name());
+  EXPECT_EQ(cm2, value[1].component_name());
 
   EXPECT_STREQ("v1", value[0].content()["root"]["version"].GetString());
   EXPECT_STREQ("v1", value[1].content()["root"]["version"].GetString());
 }
 
 TEST_F(ArchiveReaderTest, Sort) {
-  inspect::contrib::ArchiveReader reader(
-      real_services()->Connect<fuchsia::diagnostics::ArchiveAccessor>(),
-      {cmx1_selector, cmx2_selector});
+  inspect::contrib::ArchiveReader reader(svc()->Connect<fuchsia::diagnostics::ArchiveAccessor>(),
+                                         {cm1_selector(), cm2_selector()});
 
   ResultType result;
-  executor_.schedule_task(reader
-                              .SnapshotInspectUntilPresent(
-                                  {"archive_reader_test_app.cmx", "archive_reader_test_app_2.cmx"})
-                              .then([&](ResultType& r) { result = std::move(r); }));
+  executor().schedule_task(
+      reader.SnapshotInspectUntilPresent({"test_app", "test_app_2"}).then([&](ResultType& r) {
+        result = std::move(r);
+      }));
   RunLoopUntil([&] { return !!result; });
 
   ASSERT_TRUE(result.is_ok()) << "Error: " << result.error();
@@ -205,11 +200,26 @@ TEST_F(ArchiveReaderTest, Sort) {
   value[0].Sort();
   value[1].Sort();
 
-  re2::RE2 reg("\"timestamp\": \\d+");
-  EXPECT_EQ(CMX1_EXPECTED_DATA,
-            regex_replace(value[0].PrettyJson(), reg, "\"timestamp\": TIMESTAMP"));
-  EXPECT_EQ(CMX2_EXPECTED_DATA,
-            regex_replace(value[1].PrettyJson(), reg, "\"timestamp\": TIMESTAMP"));
+  re2::RE2 timestamp_reg("\"timestamp\": \\d+");
+  re2::RE2 url_reg("\"component_url\": \"realm-builder://\\d+/");
+  re2::RE2 moniker_reg("\"moniker\": \"realm_builder.+/");
+
+  auto cm1_expected_json = value[0].PrettyJson();
+  cm1_expected_json = regex_replace(cm1_expected_json, timestamp_reg, "\"timestamp\": TIMESTAMP");
+  cm1_expected_json =
+      regex_replace(cm1_expected_json, url_reg, "\"component_url\": \"REALM_BUILDER_URL_PREFIX/");
+  cm1_expected_json =
+      regex_replace(cm1_expected_json, moniker_reg, "\"moniker\": \"REALM_BUILDER_MONIKER_PREFIX/");
+
+  auto cm2_expected_json = value[1].PrettyJson();
+  cm2_expected_json = regex_replace(cm2_expected_json, timestamp_reg, "\"timestamp\": TIMESTAMP");
+  cm2_expected_json =
+      regex_replace(cm2_expected_json, url_reg, "\"component_url\": \"REALM_BUILDER_URL_PREFIX/");
+  cm2_expected_json =
+      regex_replace(cm2_expected_json, moniker_reg, "\"moniker\": \"REALM_BUILDER_MONIKER_PREFIX/");
+
+  EXPECT_EQ(CM1_EXPECTED_DATA, cm1_expected_json);
+  EXPECT_EQ(CM2_EXPECTED_DATA, cm2_expected_json);
 }
 
 }  // namespace
