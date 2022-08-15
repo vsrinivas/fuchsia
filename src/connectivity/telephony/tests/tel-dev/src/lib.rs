e159d28eeaa1ed97cdd1b2b5244f49878c13e41d
David Gilhooley <dgilhooley@google.com>
2022-08-15 17:04:17 +0000

    [isolated_devmgr] Remove unused libraries
    Remove a number of libraries and dependencies that are no longer
    being used.
    
    Bug: 86318
    
    Change-Id: I66394241c8c47a22d5980ca53771706274108a85
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/711623
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Tom Bergan <tombergan@google.com>

--- src/connectivity/telephony/tests/tel-dev/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2019 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-pub mod at_test_common;
-pub mod component_test;
-pub mod isolated_devmgr;
-pub mod snooper_test;
