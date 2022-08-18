12717a541825122f1416f6df2ad464ca62d25abf
Ben Lawson <benlawson@google.com>
2022-08-18 19:57:22 +0000

    [bt][common] Use pw_assert for BT assertions
    Create pw_assert backend that uses ZX_* assertions.
    
    Map BT_* assertions to equivalent PW_* assertions.
    
    Fixed: 106889
    Test: fx test //src/connectivity/bluetooth/core/bt-host
    Change-Id: Ide9f090bbce754f785ca9820cbee3f7870d0750c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/714008
    Reviewed-by: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Ben Lawson <benlawson@google.com>
    Reviewed-by: Faraaz Sareshwala <fsareshwala@google.com>

--- src/connectivity/bluetooth/core/bt-host/common/assert.h
+++ src/connectivity/bluetooth/core/bt-host/common/assert.h
@@ -5,12 +5,12 @@
 #ifndef SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_ASSERT_H_
 #define SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_ASSERT_H_
 
-#include <zircon/assert.h>
+#include "pw_assert/check.h"
 
-#define BT_PANIC(args...) ZX_PANIC(args)
-#define BT_ASSERT(x) ZX_ASSERT(x)
-#define BT_ASSERT_MSG(x, msg, args...) ZX_ASSERT_MSG(x, msg, ##args)
-#define BT_DEBUG_ASSERT(x) ZX_DEBUG_ASSERT(x)
-#define BT_DEBUG_ASSERT_MSG(x, msg, args...) ZX_DEBUG_ASSERT_MSG(x, msg, ##args)
+#define BT_PANIC(msg, args...) PW_CRASH(msg, ##args)
+#define BT_ASSERT(x) PW_CHECK(x)
+#define BT_ASSERT_MSG(x, msg, args...) PW_CHECK(x, msg, ##args)
+#define BT_DEBUG_ASSERT(x) PW_DCHECK(x)
+#define BT_DEBUG_ASSERT_MSG(x, msg, args...) PW_DCHECK(x, msg, ##args)
 
 #endif  // SRC_CONNECTIVITY_BLUETOOTH_CORE_BT_HOST_COMMON_ASSERT_H_
