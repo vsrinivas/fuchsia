b7a2d27e87535451ccfc330efe771988dc2a6b16
Megan Batty <mgnb@google.com>
2022-08-18 22:31:41 +0000

    [ffx][config] Clean up bad lockfiles proactively.
    Do a few simple heuristic checks to deal with the most common situations
    where a lockfile might be left lying around and delete them when we can.
    
    This is both somewhat conservative -- it will only delete for specific
    situations -- and somewhat aggressive -- in that it assumes it's better
    to have incorrectly deleted a lockfile that was in active (but slow) use
    than to block someone. Since these are actually just config files with
    wide validity bands in terms of time, this is probably fine.
    
    Fixed: 107136
    Multiply: ffx-e2e
    
    Change-Id: I8076049ded355c40eea87527af5e40e14446ffbe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/712992
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Megan Batty <mgnb@google.com>

--- src/developer/ffx/config/src/lockfile.rs
+++ src/developer/ffx/config/src/lockfile.rs
@@ -2,14 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use nix::{
+    errno::Errno,
+    unistd::{self, Pid},
+};
 use serde::{Deserialize, Serialize};
 use std::{
-    fs::{remove_file, File, OpenOptions},
+    fs::{remove_file, File, Metadata, OpenOptions},
     io::{ErrorKind, Write},
     path::{Path, PathBuf},
-    time::{Duration, Instant},
+    time::{Duration, Instant, SystemTime},
 };
-use tracing::warn;
+use tracing::{error, info, warn};
 
 /// Opens and controls a lockfile against a specific filename using create-only
 /// file modes, deleting the file on drop. Writes the current pid to the file
@@ -33,6 +37,8 @@ pub struct LockfileCreateError {
     pub lock_path: PathBuf,
     /// Details about the lockfile's original owner, from the file.
     pub owner: Option<LockContext>,
+    /// Metadata about the lockfile if it was available
+    pub metadata: Option<Metadata>,
 }
 
 impl LockfileCreateError {
@@ -44,8 +50,56 @@ impl LockfileCreateError {
             }
             _ => None,
         };
+        let metadata = std::fs::metadata(lock_path).ok(); // purely advisory, ignore the error.
         let lock_path = lock_path.to_owned();
-        Self { error, lock_path, owner }
+        Self { error, lock_path, owner, metadata }
+    }
+
+    /// Validates if a lockfile's info is inherently invalid and should be removed or ignored.
+    pub fn is_valid(&self, at: SystemTime, my_pid: u32) -> bool {
+        let ctime = self.metadata.as_ref().and_then(|metadata| metadata.created().ok());
+        // just warnings/info
+        match self.owner.as_ref() {
+            Some(ctx) if ctx.pid == my_pid => {
+                info!("Overlapping access to the lockfile at {path} from the same process, this may be a sign of other issues.", path=self.lock_path.display());
+            }
+            _ => (),
+        };
+        // actual errors
+        match (self.owner.as_ref(), ctime) {
+            (_, Some(ctime)) if ctime > at + Duration::from_secs(2) => {
+                warn!("Lockfile {path} is in the future somehow, considering it invalid (ctime: {ctime:?}, now: {at:?})", path=self.lock_path.display());
+                false
+            }
+            (None, Some(ctime)) if ctime + Duration::from_secs(10) < at => {
+                warn!("Lockfile {path} is older than 10s, so is probably stale, considering it invalid (ctime {ctime:?}, now: {at:?})", path = self.lock_path.display());
+                false
+            }
+            (Some(ctx), _)
+                if unistd::getpgid(Some(Pid::from_raw(ctx.pid as i32)))
+                    == Err(nix::Error::Sys(Errno::ESRCH)) =>
+            {
+                warn!("Lockfile {path} was created by a pid that no longer exists ({pid}), considering it invalid.", path=self.lock_path.display(), pid=ctx.pid);
+                false
+            }
+            _ => true,
+        }
+    }
+
+    /// Removes the lockfile if it exists, consuming the error if it was removed
+    /// or returning the error back if it failed in any other way than already not-existing.
+    pub fn remove_lock(self) -> Result<(), Self> {
+        match remove_file(&self.lock_path) {
+            Ok(_) => Ok(()),
+            Err(e) if e.kind() == ErrorKind::NotFound => Ok(()),
+            Err(e) => {
+                error!(
+                    "Error trying to remove lockfile {lockfile}: {e:?}",
+                    lockfile = self.lock_path.display()
+                );
+                Err(self)
+            }
+        }
     }
 }
 
@@ -56,7 +110,7 @@ impl LockfileCreateError {
 /// It is not *guaranteed* to be unique across time, so it's not safe to assume that
 /// `LockContext::current() == some_other_context` means it *was* the current process
 /// or thread, but it should mean it wasn't if they're different.
-#[derive(Serialize, Deserialize, Debug, Hash, PartialOrd, PartialEq)]
+#[derive(Clone, Serialize, Deserialize, Debug, Hash, PartialOrd, PartialEq)]
 pub struct LockContext {
     pub pid: u32,
 }
@@ -78,7 +132,7 @@ impl Lockfile {
     /// Creates a lockfile at `filename` if possible. Returns the underlying error
     /// from the file create call. Note that this won't retry. Use [`Lockfile::lock`]
     /// or [`Lockfile::lock_for`] to do that.
-    pub fn new(lock_path: &Path) -> Result<Self, LockfileCreateError> {
+    pub fn new(lock_path: &Path, context: LockContext) -> Result<Self, LockfileCreateError> {
         // create the lock file -- if this succeeds the file exists now and is brand new
         // so we can write our details to it.
         OpenOptions::new()
@@ -92,7 +146,7 @@ impl Lockfile {
                 // in drop().
                 let path = lock_path.to_owned();
                 let mut lock_file = Self { handle, path };
-                LockContext::current().write_to(&lock_file.handle)?;
+                context.write_to(&lock_file.handle)?;
                 lock_file.handle.flush()?;
                 Ok(lock_file)
             })
@@ -100,27 +154,35 @@ impl Lockfile {
     }
 
     /// Creates a lockfile at `filename`.lock if possible. See [`Lockfile::new`] for details on the return value.
-    pub fn new_for(filename: &Path) -> Result<Self, LockfileCreateError> {
+    pub fn new_for(filename: &Path, context: LockContext) -> Result<Self, LockfileCreateError> {
         let mut lock_path = filename.to_owned();
         let filename = lock_path.file_name().map_or(".lock".to_string(), |name| {
             format!("{filename}.lock", filename = name.to_string_lossy())
         });
 
         lock_path.set_file_name(&filename);
-        Self::new(&lock_path)
+        Self::new(&lock_path, context)
     }
 
     /// Creates a lockfile at `filename` if possible, retrying until it succeeds or times out.
     ///
     /// If unable to lock within the constraints, will return the error from the last attempt. It will
     /// try with an increasing sleep between attempts up to `timeout`.
-    pub async fn lock(lock_path: &Path, timeout: Duration) -> Result<Self, LockfileCreateError> {
+    async fn lock(lock_path: &Path, timeout: Duration) -> Result<Self, LockfileCreateError> {
         let end_time = Instant::now() + timeout;
+        let context = LockContext::current();
         let mut sleep_time = 10;
         loop {
-            let lock_result = Self::new(lock_path);
+            let lock_result = Self::new(lock_path, context.clone());
             match lock_result {
                 Ok(lockfile) => return Ok(lockfile),
+                Err(e) if !e.is_valid(SystemTime::now(), context.pid) => {
+                    // try to remove the invalid lockfile and immediately continue. If we get
+                    // any error other than ENOTFOUND, there's likely an underlying issue
+                    // with the filesystem and we should just bail immediately.
+                    info!("Removing invalid lockfile {lockfile}", lockfile = lock_path.display());
+                    e.remove_lock()?;
+                }
                 Err(e) if Instant::now() > end_time => return Err(e),
                 _ => {
                     fuchsia_async::Timer::new(Duration::from_millis(sleep_time)).await;
@@ -189,45 +251,173 @@ mod test {
     #[test]
     fn create_lockfile_works() -> Result<(), anyhow::Error> {
         let dir = tempfile::TempDir::new()?;
-        let mut path = dir.path().to_owned();
-        path.push("lockfile");
-        let lock = Lockfile::new(&path)?;
+        let path = dir.path().join("lockedfile");
+        let lock = Lockfile::new(&path, LockContext::current())?;
         assert!(path.is_file(), "Lockfile {path:?} should exist");
 
         assert!(
-            Lockfile::new(&path).is_err(),
+            Lockfile::new(&path, LockContext::current()).is_err(),
             "Should not be able to create lockfile at {path:?} while one already exists."
         );
         lock.unlock()?;
         assert!(!path.is_file(), "Lockfile {path:?} shouldn't exist");
 
         assert!(
-            Lockfile::new(&path).is_ok(),
+            Lockfile::new(&path, LockContext::current()).is_ok(),
             "Should be able to make a new lockfile when the old one is unlocked."
         );
 
         Ok(())
     }
+
     #[test]
     fn create_lockfile_for_other_file_works() -> Result<(), anyhow::Error> {
         let dir = tempfile::TempDir::new()?;
-        let mut path = dir.path().to_owned();
-        path.push("lockedfile");
-        let lock = Lockfile::new_for(&path)?;
+        let mut path = dir.path().join("lockedfile");
+        let lock = Lockfile::new_for(&path, LockContext::current())?;
         path.set_file_name("lockedfile.lock");
         assert!(path.is_file(), "Lockfile {path:?} should exist");
 
         assert!(
-            Lockfile::new(&path).is_err(),
+            Lockfile::new(&path, LockContext::current()).is_err(),
             "Should not be able to create lockfile at {path:?} while one already exists."
         );
         lock.unlock()?;
         assert!(!path.is_file(), "Lockfile {path:?} shouldn't exist");
 
         assert!(
-            Lockfile::new(&path).is_ok(),
+            Lockfile::new(&path, LockContext::current()).is_ok(),
             "Should be able to make a new lockfile when the old one is unlocked."
         );
+
+        Ok(())
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn lock_with_timeout() -> Result<(), anyhow::Error> {
+        let dir = tempfile::TempDir::new()?;
+        let mut path = dir.path().join("lockedfile");
+        let lock = Lockfile::lock_for(&path, Duration::from_secs(1)).await?;
+        path.set_file_name("lockedfile.lock");
+        assert!(path.is_file(), "Lockfile {path:?} should exist");
+
+        Lockfile::lock(&path, Duration::from_secs(1))
+            .await
+            .err()
+            .expect("Shouldn't be able to re-lock lock file with timeout");
+
+        lock.unlock()?;
+        assert!(!path.is_file(), "Lockfile {path:?} shouldn't exist");
+
+        Lockfile::lock(&path, Duration::from_secs(1)).await?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn lock_file_validity() -> Result<(), anyhow::Error> {
+        let dir = tempfile::TempDir::new()?;
+        let path = dir.path().join("lockedfile.lock");
+        let _lock = Lockfile::new(&path, LockContext::current())?;
+        assert!(path.is_file(), "Lockfile {path:?} should exist");
+
+        let err = Lockfile::new(&path, LockContext::current())
+            .err()
+            .expect("Should not be able to re-create lockfile");
+
+        let now = SystemTime::now();
+        let real_pid = std::process::id();
+        assert!(
+            err.is_valid(now, real_pid),
+            "A just created lockfile should be valid from the same process (but may warn)"
+        );
+        assert!(
+            err.is_valid(now, real_pid + 1),
+            "A just created lockfile should be valid from another process"
+        );
+        assert!(
+            !err.is_valid(now - Duration::from_secs(9999), real_pid),
+            "A lockfile from the far future should be valid"
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn ownerless_lockfile_validity() -> Result<(), anyhow::Error> {
+        let dir = tempfile::TempDir::new()?;
+        let path = dir.path().join("lockedfile.lock");
+        let _lock = File::create(&path).expect("Creating empty lock file");
+        assert!(path.is_file(), "Lockfile {path:?} should exist");
+
+        let err = Lockfile::new(&path, LockContext::current())
+            .err()
+            .expect("Should not be able to re-create lockfile");
+
+        let now = SystemTime::now();
+        let real_pid = std::process::id();
+        assert!(
+            err.is_valid(now, real_pid),
+            "An ownerless lockfile that's fresh should be considered valid"
+        );
+
+        assert!(
+            !err.is_valid(now + Duration::from_secs(9999), real_pid),
+            "An ownerless lockfile from a long time ago should be invalid"
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn non_running_pid_lockfile_validity() -> Result<(), anyhow::Error> {
+        let dir = tempfile::TempDir::new()?;
+        let path = dir.path().join("lockedfile.lock");
+        let _lock = Lockfile::new(&path, LockContext { pid: u32::MAX })?;
+        assert!(path.is_file(), "Lockfile {path:?} should exist");
+
+        let err = Lockfile::new(&path, LockContext::current())
+            .err()
+            .expect("Should not be able to re-create lockfile");
+
+        let now = SystemTime::now();
+        let real_pid = std::process::id();
+        assert!(
+            !err.is_valid(now, real_pid),
+            "A lockfile owned by a pid that isn't running should be invalid"
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn force_delete_nonexistent_lockfile_ok() -> Result<(), anyhow::Error> {
+        let dir = tempfile::TempDir::new()?;
+        let path = dir.path().join("lockedfile.lock");
+        let bogus_error = LockfileCreateError::new(
+            &path,
+            std::io::Error::new(std::io::ErrorKind::Other, "stuff"),
+        );
+        bogus_error.remove_lock().expect("Removing non-existent lock file");
+        Ok(())
+    }
+
+    #[test]
+    fn force_delete_real_lockfile_ok() -> Result<(), anyhow::Error> {
+        let dir = tempfile::TempDir::new()?;
+        let mut path = dir.path().join("lockedfile");
+        let lock = Lockfile::new_for(&path, LockContext::current())?;
+        path.set_file_name("lockedfile.lock");
+        assert!(path.is_file(), "Lockfile {path:?} should exist");
+
+        let err = Lockfile::new(&path, LockContext::current())
+            .err()
+            .expect("Should not be able to re-create lockfile");
+        err.remove_lock().expect("Should be able to remove lock file from error");
+
+        assert!(!path.is_file(), "Lockfile {path:?} should have been deleted");
+
+        lock.unlock().expect("Unlock should have succeeded even though lock file was already gone");
 
         Ok(())
     }
