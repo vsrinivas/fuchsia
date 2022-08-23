36314d44a1117d9d7a080b57b7d2c004c23333ba
Marie Janssen <jamuraa@google.com>
2022-08-23 19:00:25 +0000

    [bt][le] Add some asserts to AdvertisingReportParser
    Add some extra debug asserts to ensure some math will not overflow.
    
    Bug: 55289
    Test: build; fx test bt-host-hci-tests
    Change-Id: Icf00a179f355aa290589ea01a9a2cf0fe37fe548
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/715265
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Marie Janssen <jamuraa@google.com>
    Reviewed-by: Faraaz Sareshwala <fsareshwala@google.com>

--- src/connectivity/bluetooth/core/bt-host/hci/advertising_report_parser.cc
+++ src/connectivity/bluetooth/core/bt-host/hci/advertising_report_parser.cc
@@ -12,14 +12,21 @@ namespace bt::hci {
 AdvertisingReportParser::AdvertisingReportParser(const EventPacket& event)
     : encountered_error_(false) {
   BT_DEBUG_ASSERT(event.event_code() == hci_spec::kLEMetaEventCode);
+
   const auto& params = event.params<hci_spec::LEMetaEventParams>();
   BT_DEBUG_ASSERT(params.subevent_code == hci_spec::kLEAdvertisingReportSubeventCode);
+
+  static const size_t report_packet_header_size =
+      sizeof(hci_spec::LEMetaEventParams) + sizeof(hci_spec::LEAdvertisingReportSubeventParams);
+
+  BT_DEBUG_ASSERT(event.view().payload_size() <= UINT8_MAX);
+  BT_DEBUG_ASSERT(event.view().payload_size() >= report_packet_header_size);
 
   auto subevent_params = event.subevent_params<hci_spec::LEAdvertisingReportSubeventParams>();
 
   remaining_reports_ = subevent_params->num_reports;
-  remaining_bytes_ = event.view().payload_size() - sizeof(hci_spec::LEMetaEventParams) -
-                     sizeof(hci_spec::LEAdvertisingReportSubeventParams);
+
+  remaining_bytes_ = event.view().payload_size() - report_packet_header_size;
   ptr_ = subevent_params->reports;
 }
 
