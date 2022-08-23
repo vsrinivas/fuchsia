ee5c58bc9fe228c321bc066686837914d9f5732b
David Gilhooley <dgilhooley@google.com>
2022-08-23 22:26:16 +0000

    [lowpan][openthread] Remove isolated_devmgr code
    This code is unused. It's not being included anywhere
    and it should be removed.
    
    Bug: 86318
    
    Change-Id: If6c3b92c94fbb72f58ef6d6d348fcdaa672b4705
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/711627
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Jiaming (Charlie) Wang <jiamingw@google.com>
    Commit-Queue: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Robert Quattlebaum <rquattle@google.com>

--- src/connectivity/openthread/tests/test-code/src/constants.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-pub const OT_RADIO_VERSION_RESP: &[u8; 13] =
-    &[0x81, 0x06, 0x02, 0x4f, 0x50, 0x45, 0x4E, 0x54, 0x48, 0x52, 0x45, 0x41, 0x44];
-pub const OT_RADIO_VERSION_REQ: &[u8; 3] = &[0x81, 0x02, 0x02];
-pub const OT_RADIO_SOFT_RESET: &[u8; 2] = &[0x81, 0x01];
--- src/connectivity/openthread/tests/test-code/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-mod constants;
-
-pub mod spinel_device_ncp_ver_query;
-pub mod spinel_device_soft_reset;
