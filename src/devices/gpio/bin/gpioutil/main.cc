87ccaacd988b4ed43e06d7103f3ce22c29b3ee9a
Kayce Basques <kayce@google.com>
2022-08-26 00:03:32 +0000

    [gpioutil][docs] Update help text
    Change-Id: Ief659ff0bd2ba1dd5b734d41c40da26fd3463483
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/716722
    Reviewed-by: Xushen Ma <xushen@google.com>
    Fuchsia-Auto-Submit: Kayce Basques <kayce@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Gurjant Kalsi <gkalsi@google.com>

--- src/devices/gpio/bin/gpioutil/main.cc
+++ src/devices/gpio/bin/gpioutil/main.cc
@@ -17,48 +17,51 @@
 #include "gpioutil.h"
 
 static void usage() {
-  printf("Usage: gpioutil <command> <device|name> [<args>]\n\n");
-  printf("Read from, write to, and configure the GPIO pin of <device|name>.\n\n");
-  printf("Commands:\n");
-  printf("  list | l          List the known GPIOs. Both pin and name are printed.\n");
-  printf("  name | n          Read the pin and name info of <device|name>.\n");
-  printf("  read | r          Read the current value of <device>. Possible return values:\n");
-  printf("                    0 (LOW), 1 (HIGH)\n");
-  printf("  write | w         Write to <device>. Accepted <args>:\n");
-  printf("                    0 (LOW), 1 (HIGH)\n");
-  printf("  in | i            Configure <device> as IN. <args> should be the resistor pull.\n");
-  printf("                    Accepted <args>: 0 (GPIO_PULL_DOWN), 1 (GPIO_PULL_UP),\n");
-  printf("                    2 (GPIO_NO_PULL)\n");
-  printf("  out | o           Configure <device> as OUT. <args> should be the initial\n");
-  printf("                    value. Accepted <args>: 0 (LOW), 1 (HIGH)\n");
-  printf("  drive | d         Set the drive strength of <device>. <args> should be the\n");
-  printf("                    drive strength value in microamps.\n");
-  printf("  help | h          Print this help text.\n\n");
-  printf("Examples:\n");
-  printf("  List GPIO pins.\n");
-  printf("  $ gpioutil list\n");
   printf(
+      "Usage: gpioutil <command> [<name>] [<args>]\n\n"
+      "List, read from, write to, and configure GPIOs.\n\n"
+      "Commands:\n"
+      "  list | l          List the known GPIOs. Each GPIO is represented by 2 values.\n"
+      "                    Example: `[gpio-0] GPIO_HW_ID_3`. The value inside the\n"
+      "                    brackets (`gpio-0`) can be ignored. The value after the brackets\n"
+      "                    (`GPIO_HW_ID_3`) is the <name> value to provide to other gpioutil\n"
+      "                    commands. GPIO names are defined in the driver source code and\n"
+      "                    usually match the datasheet's name for the GPIO. Example:\n"
+      "                    "
+      "https://cs.opensource.google/fuchsia/fuchsia/+/main:src/devices/board/drivers/vim3/"
+      "vim3-gpio.cc;l=72\n"
+      "  read | r          Read the current value of <name>. Possible return values are\n"
+      "                    `0` (LOW) or `1` (HIGH).\n"
+      "  write | w         Write to <name>. <args> should be `0` (LOW) or `1` (HIGH).\n"
+      "  in | i            Configure <name> as IN. <args> is the resistor pull and its value\n"
+      "                    should be `0` (GPIO_PULL_DOWN), `1` (GPIO_PULL_UP), or `2` "
+      "(GPIO_NO_PULL).\n"
+      "  out | o           Configure <name> as OUT. <args> is the initial OUT\n"
+      "                    state and its value should be `0` (LOW) or `1` (HIGH).\n"
+      "  drive | d         Set the drive strength of <device>. <args> should be the\n"
+      "                    drive strength value in microamps.\n"
+      "  help | h          Print this help text.\n\n"
+      "Examples:\n"
+      "  List GPIO pins.\n"
+      "  $ gpioutil list\n"
       "  > [gpio-0] GPIO_HW_ID_3\n"
-      "    [gpio-1] GPIO_SOC_TH_BOOT_MODE_L\n"
-      "    ...\n\n");
-  printf("  Get name from a GPIO pin.\n");
-  printf("  $ gpioutil name /dev/sys/platform/05:05:1/gpio/gpio-0\n");
-  printf("  > GPIO Name: [gpio-0] GPIO_HW_ID_3\n\n");
-  printf("  Read from a GPIO pin.\n");
-  printf("  $ gpioutil read /dev/sys/platform/05:05:1/gpio/gpio-0\n");
-  printf("  > GPIO Value: 1\n\n");
-  printf("  Write a LOW value to a GPIO pin.\n");
-  printf("  $ gpioutil write /dev/sys/platform/05:05:1/gpio/gpio-0 0\n\n");
-  printf("  Configure a GPIO pin as IN with a pull-down resistor.\n");
-  printf("  $ gpioutil in /dev/sys/platform/05:05:1/gpio/gpio-0 0\n\n");
-  printf("  Configure a GPIO pin as OUT with an initial value of HIGH.\n");
-  printf("  $ gpioutil out /dev/sys/platform/05:05:1/gpio/gpio-0 1\n\n");
-  printf("  Get the current drive strength in microamps of a GPIO pin.\n");
-  printf("  $ gpioutil drive /dev/sys/platform/05:05:1/gpio/gpio-0\n");
-  printf("  Drive Strength: 500 ua\n\n");
-  printf("  Set the drive strength of a GPIO pin to 500 microamps.\n");
-  printf("  $ gpioutil drive /dev/sys/platform/05:05:1/gpio/gpio-0 500\n");
-  printf("  > Set drive strength to 500\n\n");
+      "  > [gpio-1] GPIO_SOC_TH_BOOT_MODE_L\n"
+      "  > ...\n\n"
+      "  Read the current value of <name>.\n"
+      "  $ gpioutil read GPIO_HW_ID_3\n"
+      "  > GPIO Value: 1\n\n"
+      "  Write a LOW value to a GPIO pin.\n"
+      "  $ gpioutil write GPIO_HW_ID_3 0\n\n"
+      "  Configure a GPIO pin as IN with a pull-down resistor.\n"
+      "  $ gpioutil in GPIO_HW_ID_3 0\n\n"
+      "  Configure a GPIO pin as OUT with an initial value of HIGH.\n"
+      "  $ gpioutil out GPIO_HW_ID_3 1\n\n"
+      "  Get the current drive strength in microamps of a GPIO pin.\n"
+      "  $ gpioutil drive GPIO_HW_ID_3\n"
+      "  > Drive Strength: 500 ua\n\n"
+      "  Set the drive strength of a GPIO pin to 500 microamps.\n"
+      "  $ gpioutil drive GPIO_HW_ID_3 500\n"
+      "  > Set drive strength to 500\n\n");
 }
 
 int main(int argc, char** argv) {
