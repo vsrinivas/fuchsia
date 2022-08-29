06fcfd96069e67679e025256f1eb5e6eddf43182
Kevin Lindkvist <lindkvist@google.com>
2022-08-29 12:12:30 +0000

    [starnix][clippy] Enable clippy for starnix
    Change-Id: I8e40de10ed26803e93a1ac0d66a41540ee885063
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/717117
    Reviewed-by: Adam Barth <abarth@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Kevin Lindkvist <lindkvist@google.com>

--- src/proc/bin/starnix/main.rs
+++ src/proc/bin/starnix/main.rs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![warn(clippy::all)]
+#![allow(clippy::too_many_arguments)]
+
 use crate::execution::create_galaxy;
 use anyhow::Error;
 use fidl::endpoints::ControlHandle;
