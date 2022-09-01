943c1c0a9e783e8c8b34e7f30bd2f798506775cc
Brett Wilson <brettw@google.com>
2022-09-01 23:32:39 +0000

    [errors] Update comments for ZX_ERR definitions.
    Updates the comments for the definitions of ZX_ERR_* to make them more
    readable in the context of generated documentation. Mostly this removes
    the duplicated error name in every definition and adds some blank lines
    to separate paragraphs.
    
    There are more significant changes to the flow control comments so each
    one makes sense independently, without needing the header above all
    three of them.
    
    There is no behavior change.
    
    Change-Id: Idef2d29179caeae1c54ea63e1306b43604ba9579
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/703310
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    API-Review: Adam Barth <abarth@google.com>
    Commit-Queue: Brett Wilson <brettw@google.com>

--- zircon/system/public/zircon/errors.h
+++ zircon/system/public/zircon/errors.h
@@ -5,228 +5,239 @@
 #ifndef SYSROOT_ZIRCON_ERRORS_H_
 #define SYSROOT_ZIRCON_ERRORS_H_
 
-// ZX_OK: Indicates an operation was successful.
+// Indicates an operation was successful.
 #define ZX_OK (0)
 
 // ======= Internal failures =======
-// ZX_ERR_INTERNAL: The system encountered an otherwise unspecified error
-// while performing the operation.
+
+// The system encountered an otherwise unspecified error while performing the
+// operation.
 #define ZX_ERR_INTERNAL (-1)
 
-// ZX_ERR_NOT_SUPPORTED: The operation is not implemented, supported,
-// or enabled.
+// The operation is not implemented, supported, or enabled.
 #define ZX_ERR_NOT_SUPPORTED (-2)
 
-// ZX_ERR_NO_RESOURCES: The system was not able to allocate some resource
-// needed for the operation.
+// The system was not able to allocate some resource needed for the operation.
 #define ZX_ERR_NO_RESOURCES (-3)
 
-// ZX_ERR_NO_MEMORY: The system was not able to allocate memory needed
-// for the operation.
+// The system was not able to allocate memory needed for the operation.
 #define ZX_ERR_NO_MEMORY (-4)
 
 // -5 used to be ZX_ERR_CALL_FAILED.
 
-// ZX_ERR_INTERNAL_INTR_RETRY: The system call was interrupted, but should be
-// retried.  This should not be seen outside of the VDSO.
+// The system call was interrupted, but should be retried. This should not be
+// seen outside of the VDSO.
 #define ZX_ERR_INTERNAL_INTR_RETRY (-6)
 
 // ======= Parameter errors =======
-// ZX_ERR_INVALID_ARGS: an argument is invalid, ex. null pointer
+
+// An argument is invalid. For example, a null pointer when a null pointer is
+// not permitted.
 #define ZX_ERR_INVALID_ARGS (-10)
 
-// ZX_ERR_BAD_HANDLE: A specified handle value does not refer to a handle.
+// A specified handle value does not refer to a handle.
 #define ZX_ERR_BAD_HANDLE (-11)
 
-// ZX_ERR_WRONG_TYPE: The subject of the operation is the wrong type to
-// perform the operation.
-// Example: Attempting a message_read on a thread handle.
+// The subject of the operation is the wrong type to perform the operation.
+//
+// For example: Attempting a message_read on a thread handle.
 #define ZX_ERR_WRONG_TYPE (-12)
 
-// ZX_ERR_BAD_SYSCALL: The specified syscall number is invalid.
+// The specified syscall number is invalid.
 #define ZX_ERR_BAD_SYSCALL (-13)
 
-// ZX_ERR_OUT_OF_RANGE: An argument is outside the valid range for this
-// operation.
+// An argument is outside the valid range for this operation.
 #define ZX_ERR_OUT_OF_RANGE (-14)
 
-// ZX_ERR_BUFFER_TOO_SMALL: A caller provided buffer is too small for
-// this operation.
+// The caller-provided buffer is too small for this operation.
 #define ZX_ERR_BUFFER_TOO_SMALL (-15)
 
 // ======= Precondition or state errors =======
-// ZX_ERR_BAD_STATE: operation failed because the current state of the
-// object does not allow it, or a precondition of the operation is
-// not satisfied
+
+// The operation failed because the current state of the object does not allow
+// it, or a precondition of the operation is not satisfied.
 #define ZX_ERR_BAD_STATE (-20)
 
-// ZX_ERR_TIMED_OUT: The time limit for the operation elapsed before
-// the operation completed.
+// The time limit for the operation elapsed before the operation completed.
 #define ZX_ERR_TIMED_OUT (-21)
 
-// ZX_ERR_SHOULD_WAIT: The operation cannot be performed currently but
-// potentially could succeed if the caller waits for a prerequisite
-// to be satisfied, for example waiting for a handle to be readable
-// or writable.
-// Example: Attempting to read from a channel that has no
-// messages waiting but has an open remote will return ZX_ERR_SHOULD_WAIT.
-// Attempting to read from a channel that has no messages waiting
-// and has a closed remote end will return ZX_ERR_PEER_CLOSED.
+// The operation cannot be performed currently but potentially could succeed if
+// the caller waits for a prerequisite to be satisfied, like waiting for a
+// handle to be readable or writable.
+//
+// Example: Attempting to read from a channel that has no messages waiting but
+// has an open remote will return `ZX_ERR_SHOULD_WAIT`. In contrast, attempting
+// to read from a channel that has no messages waiting and has a closed remote
+// end will return `ZX_ERR_PEER_CLOSED`.
 #define ZX_ERR_SHOULD_WAIT (-22)
 
-// ZX_ERR_CANCELED: The in-progress operation (e.g. a wait) has been
-// canceled.
+// The in-progress operation, for example, a wait, has been canceled.
 #define ZX_ERR_CANCELED (-23)
 
-// ZX_ERR_PEER_CLOSED: The operation failed because the remote end of the
-// subject of the operation was closed.
+// The operation failed because the remote end of the subject of the operation
+// was closed.
 #define ZX_ERR_PEER_CLOSED (-24)
 
-// ZX_ERR_NOT_FOUND: The requested entity is not found.
+// The requested entity is not found.
 #define ZX_ERR_NOT_FOUND (-25)
 
-// ZX_ERR_ALREADY_EXISTS: An object with the specified identifier
-// already exists.
-// Example: Attempting to create a file when a file already exists
-// with that name.
+// An object with the specified identifier already exists.
+//
+// Example: Attempting to create a file when a file already exists with that
+// name.
 #define ZX_ERR_ALREADY_EXISTS (-26)
 
-// ZX_ERR_ALREADY_BOUND: The operation failed because the named entity
-// is already owned or controlled by another entity. The operation
-// could succeed later if the current owner releases the entity.
+// The operation failed because the named entity is already owned or controlled
+// by another entity. The operation could succeed later if the current owner
+// releases the entity.
 #define ZX_ERR_ALREADY_BOUND (-27)
 
-// ZX_ERR_UNAVAILABLE: The subject of the operation is currently unable
-// to perform the operation.
-// Note: This is used when there's no direct way for the caller to
-// observe when the subject will be able to perform the operation
-// and should thus retry.
+// The subject of the operation is currently unable to perform the operation.
+//
+// This is used when there's no direct way for the caller to observe when the
+// subject will be able to perform the operation and should thus retry.
 #define ZX_ERR_UNAVAILABLE (-28)
 
 // ======= Permission check errors =======
-// ZX_ERR_ACCESS_DENIED: The caller did not have permission to perform
-// the specified operation.
+
+// The caller did not have permission to perform the specified operation.
 #define ZX_ERR_ACCESS_DENIED (-30)
 
-// ======= Input-output errors =======
-// ZX_ERR_IO: Otherwise unspecified error occurred during I/O.
+// Otherwise-unspecified error occurred during I/O.
 #define ZX_ERR_IO (-40)
 
-// ZX_ERR_REFUSED: The entity the I/O operation is being performed on
-// rejected the operation.
-// Example: an I2C device NAK'ing a transaction or a disk controller
-// rejecting an invalid command, or a stalled USB endpoint.
+// The entity the I/O operation is being performed on rejected the operation.
+//
+// Example: an I2C device NAK'ing a transaction or a disk controller rejecting
+// an invalid command, or a stalled USB endpoint.
 #define ZX_ERR_IO_REFUSED (-41)
 
-// ZX_ERR_IO_DATA_INTEGRITY: The data in the operation failed an integrity
-// check and is possibly corrupted.
+// The data in the operation failed an integrity check and is possibly
+// corrupted.
+//
 // Example: CRC or Parity error.
 #define ZX_ERR_IO_DATA_INTEGRITY (-42)
 
-// ZX_ERR_IO_DATA_LOSS: The data in the operation is currently unavailable
-// and may be permanently lost.
+// The data in the operation is currently unavailable and may be permanently
+// lost.
+//
 // Example: A disk block is irrecoverably damaged.
 #define ZX_ERR_IO_DATA_LOSS (-43)
 
-// ZX_ERR_IO_NOT_PRESENT: The device is no longer available (has been
-// unplugged from the system, powered down, or the driver has been
-// unloaded)
+// The device is no longer available (has been unplugged from the system,
+// powered down, or the driver has been unloaded).
 #define ZX_ERR_IO_NOT_PRESENT (-44)
 
-// ZX_ERR_IO_OVERRUN: More data was received from the device than expected.
-// Example: a USB "babble" error due to a device sending more data than
-// the host queued to receive.
+// More data was received from the device than expected.
+//
+// Example: a USB "babble" error due to a device sending more data than the
+// host queued to receive.
 #define ZX_ERR_IO_OVERRUN (-45)
 
-// ZX_ERR_IO_MISSED_DEADLINE: An operation did not complete within the required timeframe.
-// Example: A USB isochronous transfer that failed to complete due to an overrun or underrun.
+// An operation did not complete within the required timeframe.
+//
+// Example: A USB isochronous transfer that failed to complete due to an
+// overrun or underrun.
 #define ZX_ERR_IO_MISSED_DEADLINE (-46)
 
-// ZX_ERR_IO_INVALID: The data in the operation is invalid parameter or is out of range.
+// The data in the operation is invalid parameter or is out of range.
+//
 // Example: A USB transfer that failed to complete with TRB Error
 #define ZX_ERR_IO_INVALID (-47)
 
 // ======== Filesystem Errors ========
-// ZX_ERR_BAD_PATH: Path name is too long.
+
+// Path name is too long.
 #define ZX_ERR_BAD_PATH (-50)
 
-// ZX_ERR_NOT_DIR: Object is not a directory or does not support
-// directory operations.
-// Example: Attempted to open a file as a directory or
-// attempted to do directory operations on a file.
+// The object is not a directory or does not support directory operations.
+//
+// Example: Attempted to open a file as a directory or attempted to do
+// directory operations on a file.
 #define ZX_ERR_NOT_DIR (-51)
 
-// ZX_ERR_NOT_FILE: Object is not a regular file.
+// Object is not a regular file.
 #define ZX_ERR_NOT_FILE (-52)
 
-// ZX_ERR_FILE_BIG: This operation would cause a file to exceed a
-// filesystem-specific size limit
+// This operation would cause a file to exceed a filesystem-specific size
+// limit.
 #define ZX_ERR_FILE_BIG (-53)
 
-// ZX_ERR_NO_SPACE: Filesystem or device space is exhausted.
+// The filesystem or device space is exhausted.
 #define ZX_ERR_NO_SPACE (-54)
 
-// ZX_ERR_NOT_EMPTY: Directory is not empty.
+// The directory is not empty for an operation that requires it to be empty.
+//
+// For example, non-recursively deleting a directory with files still in it.
 #define ZX_ERR_NOT_EMPTY (-55)
 
 // ======== Flow Control ========
-// These are not errors, as such, and will never be returned
-// by a syscall or public API.  They exist to allow callbacks
-// to request changes in operation.
+
+// An indicate to not call again.
 //
-// ZX_ERR_STOP: Do not call again.
-// Example: A notification callback will be called on every
-// event until it returns something other than ZX_OK.
-// This status allows differentiation between "stop due to
-// an error" and "stop because the work is done."
+// The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are
+// not errors and will never be returned by a system call or public API. They
+// allow callbacks to request their caller perform some other operation.
+//
+// For example, a callback might be called on every event until it returns
+// something other than `ZX_OK`. This status allows differentiation between
+// "stop due to an error" and "stop because work is done."
 #define ZX_ERR_STOP (-60)
 
-// ZX_ERR_NEXT: Advance to the next item.
-// Example: A notification callback will use this response
-// to indicate it did not "consume" an item passed to it,
-// but by choice, not due to an error condition.
+// Advance to the next item.
+//
+// The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are
+// not errors and will never be returned by a system call or public API. They
+// allow callbacks to request their caller perform some other operation.
+//
+// For example, a callback could use this value to indicate it did not consume
+// an item passed to it, but by choice, not due to an error condition.
 #define ZX_ERR_NEXT (-61)
 
-// ZX_ERR_ASYNC: Ownership of the item has moved to an asynchronous worker.
+// Ownership of the item has moved to an asynchronous worker.
 //
-// Unlike ZX_ERR_STOP, which implies that iteration on an object
-// should stop, and ZX_ERR_NEXT, which implies that iteration
-// should continue to the next item, ZX_ERR_ASYNC implies
+// The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are
+// not errors and will never be returned by a system call or public API. They
+// allow callbacks to request their caller perform some other operation.
+//
+// Unlike `ZX_ERR_STOP`, which implies that iteration on an object
+// should stop, and `ZX_ERR_NEXT`, which implies that iteration
+// should continue to the next item, `ZX_ERR_ASYNC` implies
 // that an asynchronous worker is responsible for continuing iteration.
 //
-// Example: A notification callback will be called on every
-// event, but one event needs to handle some work asynchronously
-// before it can continue. ZX_ERR_ASYNC implies the worker is
-// responsible for resuming iteration once its work has completed.
+// For example, a callback will be called on every event, but one event needs
+// to handle some work asynchronously before it can continue. `ZX_ERR_ASYNC`
+// implies the worker is responsible for resuming iteration once its work has
+// completed.
 #define ZX_ERR_ASYNC (-62)
 
 // ======== Network-related errors ========
 
-// ZX_ERR_PROTOCOL_NOT_SUPPORTED: Specified protocol is not
-// supported.
+// The specified protocol is not supported.
 #define ZX_ERR_PROTOCOL_NOT_SUPPORTED (-70)
 
-// ZX_ERR_ADDRESS_UNREACHABLE: Host is unreachable.
+// The host is unreachable.
 #define ZX_ERR_ADDRESS_UNREACHABLE (-71)
 
-// ZX_ERR_ADDRESS_IN_USE: Address is being used by someone else.
+// Address is being used by someone else.
 #define ZX_ERR_ADDRESS_IN_USE (-72)
 
-// ZX_ERR_NOT_CONNECTED: Socket is not connected.
+// The socket is not connected.
 #define ZX_ERR_NOT_CONNECTED (-73)
 
-// ZX_ERR_CONNECTION_REFUSED: Remote peer rejected the connection.
+// The remote peer rejected the connection.
 #define ZX_ERR_CONNECTION_REFUSED (-74)
 
-// ZX_ERR_CONNECTION_RESET: Connection was reset.
+// The connection was reset.
 #define ZX_ERR_CONNECTION_RESET (-75)
 
-// ZX_ERR_CONNECTION_ABORTED: Connection was aborted.
+// The connection was aborted.
 #define ZX_ERR_CONNECTION_ABORTED (-76)
 
 // ======= VDSO-private errors =======
 
-// ZX_ERR_INTERNAL_INTR_KILLED: A task was killed during an operation.
+// A task was killed during an operation. This is a private error that should
+// not be seen outside of the VDSO.
 #define ZX_ERR_INTERNAL_INTR_KILLED (-502)
 
 #endif  // SYSROOT_ZIRCON_ERRORS_H_
