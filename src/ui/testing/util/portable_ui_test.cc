cf34c43c8af80519926a420b734665c93ce28613
Alexander Brusher <abrusher@google.com>
2022-09-09 00:49:24 +0000

    [ui][test] PortableUITest base class
    Bug: 108250
    Test: touch-input-test-ip
    
    Change-Id: I6a11ba41a57f2f8990454b41d4810bf41bef2a2b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/719675
    Reviewed-by: Shivesh Ganju <shiveshganju@google.com>
    Commit-Queue: Alexander Brusher <abrusher@google.com>

---
+++ src/ui/testing/util/portable_ui_test.cc
@@ -0,0 +1,228 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/ui/testing/util/portable_ui_test.h"
+
+#include <fuchsia/logger/cpp/fidl.h>
+#include <fuchsia/scheduler/cpp/fidl.h>
+#include <fuchsia/tracing/provider/cpp/fidl.h>
+#include <fuchsia/ui/app/cpp/fidl.h>
+#include <fuchsia/vulkan/loader/cpp/fidl.h>
+#include <lib/async/cpp/task.h>
+#include <lib/sys/component/cpp/testing/realm_builder_types.h>
+#include <lib/syslog/cpp/macros.h>
+
+namespace ui_testing {
+
+namespace {
+
+// Types imported for the realm_builder library.
+using component_testing::ChildRef;
+using component_testing::ParentRef;
+using component_testing::Protocol;
+using component_testing::RealmRoot;
+using component_testing::Route;
+
+bool CheckViewExistsInSnapshot(const fuchsia::ui::observation::geometry::ViewTreeSnapshot& snapshot,
+                               zx_koid_t view_ref_koid) {
+  if (!snapshot.has_views()) {
+    return false;
+  }
+
+  auto snapshot_count = std::count_if(
+      snapshot.views().begin(), snapshot.views().end(),
+      [view_ref_koid](const auto& view) { return view.view_ref_koid() == view_ref_koid; });
+
+  return snapshot_count > 0;
+}
+
+}  // namespace
+
+void PortableUITest::SetUpRealmBase() {
+  FX_LOGS(INFO) << "Setting up realm base.";
+
+  // Add test UI stack component.
+  realm_builder_.AddChild(kTestUIStack, GetTestUIStackUrl());
+
+  // Route base system services to flutter and the test UI stack.
+  realm_builder_.AddRoute(
+      Route{.capabilities = {Protocol{fuchsia::logger::LogSink::Name_},
+                             Protocol{fuchsia::scheduler::ProfileProvider::Name_},
+                             Protocol{fuchsia::sys::Environment::Name_},
+                             Protocol{fuchsia::sysmem::Allocator::Name_},
+                             Protocol{fuchsia::vulkan::loader::Loader::Name_},
+                             Protocol{fuchsia::tracing::provider::Registry::Name_}},
+            .source = ParentRef{},
+            .targets = {kTestUIStackRef}});
+
+  // Capabilities routed to test driver.
+  realm_builder_.AddRoute(
+      Route{.capabilities = {Protocol{fuchsia::ui::test::input::Registry::Name_},
+                             Protocol{fuchsia::ui::test::scene::Controller::Name_}},
+            .source = kTestUIStackRef,
+            .targets = {ParentRef{}}});
+}
+
+void PortableUITest::SetUp() {
+  SetUpRealmBase();
+
+  ExtendRealm();
+
+  realm_ = std::make_unique<RealmRoot>(realm_builder_.Build());
+}
+
+void PortableUITest::ProcessViewGeometryResponse(
+    fuchsia::ui::observation::geometry::WatchResponse response) {
+  // Process update if no error.
+  if (!response.has_error()) {
+    std::vector<fuchsia::ui::observation::geometry::ViewTreeSnapshot>* updates =
+        response.mutable_updates();
+    if (updates && !updates->empty()) {
+      last_view_tree_snapshot_ = std::move(updates->back());
+    }
+  } else {
+    // Otherwise, process error.
+    const auto& error = response.error();
+
+    if (error | fuchsia::ui::observation::geometry::Error::CHANNEL_OVERFLOW) {
+      FX_LOGS(DEBUG) << "View Tree watcher channel overflowed";
+    } else if (error | fuchsia::ui::observation::geometry::Error::BUFFER_OVERFLOW) {
+      FX_LOGS(DEBUG) << "View Tree watcher buffer overflowed";
+    } else if (error | fuchsia::ui::observation::geometry::Error::VIEWS_OVERFLOW) {
+      // This one indicates some possible data loss, so we log with a high severity.
+      FX_LOGS(WARNING) << "View Tree watcher attempted to report too many views";
+    }
+  }
+}
+
+void PortableUITest::WatchViewGeometry() {
+  FX_CHECK(view_tree_watcher_) << "View Tree watcher must be registered before calling Watch()";
+
+  view_tree_watcher_->Watch([this](auto response) {
+    ProcessViewGeometryResponse(std::move(response));
+    WatchViewGeometry();
+  });
+}
+
+bool PortableUITest::HasViewConnected(zx_koid_t view_ref_koid) {
+  return last_view_tree_snapshot_.has_value() &&
+         CheckViewExistsInSnapshot(*last_view_tree_snapshot_, view_ref_koid);
+}
+
+void PortableUITest::LaunchClient() {
+  scene_provider_ = realm_->Connect<fuchsia::ui::test::scene::Controller>();
+  scene_provider_.set_error_handler(
+      [](auto) { FX_LOGS(ERROR) << "Error from test scene provider"; });
+  fuchsia::ui::test::scene::ControllerAttachClientViewRequest request;
+  request.set_view_provider(realm_->Connect<fuchsia::ui::app::ViewProvider>());
+  scene_provider_->RegisterViewTreeWatcher(view_tree_watcher_.NewRequest(), []() {});
+  scene_provider_->AttachClientView(std::move(request), [this](auto client_view_ref_koid) {
+    client_root_view_ref_koid_ = client_view_ref_koid;
+  });
+
+  FX_LOGS(INFO) << "Waiting for client view ref koid";
+  RunLoopUntil([this] { return client_root_view_ref_koid_.has_value(); });
+
+  WatchViewGeometry();
+
+  FX_LOGS(INFO) << "Waiting for client view to connect";
+  RunLoopUntil([this] { return HasViewConnected(*client_root_view_ref_koid_); });
+  FX_LOGS(INFO) << "Client view has rendered";
+}
+
+void PortableUITest::LaunchClientWithEmbeddedView() {
+  LaunchClient();
+
+  // At this point, the parent view must have rendered, so we just need to wait
+  // for the embedded view.
+  RunLoopUntil([this] {
+    if (!last_view_tree_snapshot_.has_value() || !last_view_tree_snapshot_->has_views()) {
+      return false;
+    }
+
+    if (!client_root_view_ref_koid_.has_value()) {
+      return false;
+    }
+
+    for (const auto& view : last_view_tree_snapshot_->views()) {
+      if (!view.has_view_ref_koid() || view.view_ref_koid() != *client_root_view_ref_koid_) {
+        continue;
+      }
+
+      if (view.children().empty()) {
+        return false;
+      }
+
+      // NOTE: We can't rely on the presence of the child view in
+      // `view.children()` to guarantee that it has rendered. The child view
+      // also needs to be present in `last_view_tree_snapshot_->views`.
+      return std::count_if(last_view_tree_snapshot_->views().begin(),
+                           last_view_tree_snapshot_->views().end(),
+                           [view_to_find = view.children().back()](const auto& view_to_check) {
+                             return view_to_check.has_view_ref_koid() &&
+                                    view_to_check.view_ref_koid() == view_to_find;
+                           }) > 0;
+    }
+
+    return false;
+  });
+
+  FX_LOGS(INFO) << "Embedded view has rendered";
+}
+
+void PortableUITest::RegisterTouchScreen() {
+  FX_LOGS(INFO) << "Registering fake touch screen";
+  input_registry_ = realm_->Connect<fuchsia::ui::test::input::Registry>();
+  input_registry_.set_error_handler([](auto) { FX_LOGS(ERROR) << "Error from input helper"; });
+
+  bool touchscreen_registered = false;
+  fuchsia::ui::test::input::RegistryRegisterTouchScreenRequest request;
+  request.set_device(fake_touchscreen_.NewRequest());
+  input_registry_->RegisterTouchScreen(
+      std::move(request), [&touchscreen_registered]() { touchscreen_registered = true; });
+
+  RunLoopUntil([&touchscreen_registered] { return touchscreen_registered; });
+  FX_LOGS(INFO) << "Touchscreen registered";
+}
+
+void PortableUITest::InjectTap(int32_t x, int32_t y) {
+  fuchsia::ui::test::input::TouchScreenSimulateTapRequest tap_request;
+  tap_request.mutable_tap_location()->x = x;
+  tap_request.mutable_tap_location()->y = y;
+
+  FX_LOGS(INFO) << "Injecting tap at (" << tap_request.tap_location().x << ", "
+                << tap_request.tap_location().y << ")";
+  fake_touchscreen_->SimulateTap(std::move(tap_request), [this]() {
+    ++touch_injection_request_count_;
+    FX_LOGS(INFO) << "*** Tap injected, count: " << touch_injection_request_count_;
+  });
+}
+
+void PortableUITest::InjectTapWithRetry(int32_t x, int32_t y) {
+  InjectTap(x, y);
+  async::PostDelayedTask(
+      dispatcher(), [this, x, y] { InjectTapWithRetry(x, y); }, kTapRetryInterval);
+}
+
+void PortableUITest::InjectSwipe(int start_x, int start_y, int end_x, int end_y,
+                                 int move_event_count) {
+  fuchsia::ui::test::input::TouchScreenSimulateSwipeRequest swipe_request;
+  swipe_request.mutable_start_location()->x = start_x;
+  swipe_request.mutable_start_location()->y = start_y;
+  swipe_request.mutable_end_location()->x = end_x;
+  swipe_request.mutable_end_location()->y = end_y;
+  swipe_request.set_move_event_count(move_event_count);
+
+  FX_LOGS(INFO) << "Injecting swipe from (" << swipe_request.start_location().x << ", "
+                << swipe_request.start_location().y << ") to (" << swipe_request.end_location().x
+                << ", " << swipe_request.end_location().y
+                << ") with move_event_count = " << swipe_request.move_event_count();
+
+  fake_touchscreen_->SimulateSwipe(std::move(swipe_request), [this]() {
+    touch_injection_request_count_++;
+    FX_LOGS(INFO) << "*** Swipe injected";
+  });
+}
+
+}  // namespace ui_testing
---
+++ src/ui/testing/util/portable_ui_test.h
@@ -0,0 +1,126 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_UI_TESTING_UTIL_PORTABLE_UI_TEST_H_
+#define SRC_UI_TESTING_UTIL_PORTABLE_UI_TEST_H_
+
+#include <fuchsia/sysmem/cpp/fidl.h>
+#include <fuchsia/ui/test/input/cpp/fidl.h>
+#include <fuchsia/ui/test/scene/cpp/fidl.h>
+#include <lib/sys/component/cpp/testing/realm_builder.h>
+#include <zircon/status.h>
+
+#include <optional>
+#include <vector>
+
+#include "src/lib/testing/loop_fixture/real_loop_fixture.h"
+
+namespace ui_testing {
+
+class PortableUITest : public ::loop_fixture::RealLoop, public ::testing::Test {
+ public:
+  static constexpr auto kTestUIStack = "ui";
+  static constexpr auto kTestUIStackRef = component_testing::ChildRef{kTestUIStack};
+
+  void SetUp() override;
+
+  // Attaches a client view to the scene, and waits for it to render.
+  void LaunchClient();
+
+  // Attaches a client view that embeds a child view to the scene, and waits for
+  // both to render.
+  void LaunchClientWithEmbeddedView();
+
+  // Returns true when the specified view is fully connected to the scene AND
+  // has presented at least one frame of content.
+  bool HasViewConnected(zx_koid_t view_ref_koid);
+
+  // Registers a fake touch screen device with an injection coordinate space
+  // spanning [-1000, 1000] on both axes.
+  void RegisterTouchScreen();
+
+  // Simulates a tap at location (x, y).
+  void InjectTap(int32_t x, int32_t y);
+
+  // Injects an input event, and posts a task to retry after `kTapRetryInterval`.
+  //
+  // We post the retry task because the first input event we send to Flutter may be lost.
+  // The reason the first event may be lost is that there is a race condition as the scene
+  // owner starts up.
+  //
+  // More specifically: in order for our app
+  // to receive the injected input, two things must be true before we inject touch input:
+  // * The Scenic root view must have been installed, and
+  // * The Input Pipeline must have received a viewport to inject touch into.
+  //
+  // The problem we have is that the `is_rendering` signal that we monitor only guarantees us
+  // the view is ready. If the viewport is not ready in Input Pipeline at that time, it will
+  // drop the touch event.
+  //
+  // TODO(fxbug.dev/96986): Improve synchronization and remove retry logic.
+  void InjectTapWithRetry(int32_t x, int32_t y);
+
+  // Injects a swipe from the given starting location to the given end location
+  // in injector coordinate space.
+  void InjectSwipe(int start_x, int start_y, int end_x, int end_y, int move_event_count);
+
+ protected:
+  component_testing::RealmBuilder* realm_builder() { return &realm_builder_; }
+  component_testing::RealmRoot* realm_root() { return realm_.get(); }
+
+  const std::optional<zx_koid_t>& client_root_view_ref_koid() { return client_root_view_ref_koid_; }
+
+  int touch_injection_request_count() const { return touch_injection_request_count_; }
+
+ private:
+  void SetUpRealmBase();
+
+  // Configures the test-specific component topology.
+  virtual void ExtendRealm() = 0;
+
+  // Returns the test-ui-stack component url to use in this test.
+  virtual std::string GetTestUIStackUrl() = 0;
+
+  // Helper method to watch watch for view geometry updates.
+  void WatchViewGeometry();
+
+  // Helper method to process a view geometry update.
+  void ProcessViewGeometryResponse(fuchsia::ui::observation::geometry::WatchResponse response);
+
+  fuchsia::ui::test::input::RegistryPtr input_registry_;
+  fuchsia::ui::test::input::TouchScreenPtr fake_touchscreen_;
+  fuchsia::ui::test::scene::ControllerPtr scene_provider_;
+  fuchsia::ui::observation::geometry::ViewTreeWatcherPtr view_tree_watcher_;
+
+  component_testing::RealmBuilder realm_builder_ = component_testing::RealmBuilder::Create();
+  std::unique_ptr<component_testing::RealmRoot> realm_;
+
+  // Counts the number of completed requests to inject touch reports into input
+  // pipeline.
+  int touch_injection_request_count_ = 0;
+
+  // The KOID of the client root view's `ViewRef`.
+  std::optional<zx_koid_t> client_root_view_ref_koid_;
+
+  // Holds the most recent view tree snapshot received from the view tree
+  // watcher.
+  //
+  // From this snapshot, we can retrieve relevant view tree state on demand,
+  // e.g. if the client view is rendering content.
+  std::optional<fuchsia::ui::observation::geometry::ViewTreeSnapshot> last_view_tree_snapshot_;
+
+  // The typical latency on devices we've tested is ~60 msec. The retry interval is chosen to be
+  // a) Long enough that it's unlikely that we send a new tap while a previous tap is still being
+  //    processed. That is, it should be far more likely that a new tap is sent because the first
+  //    tap was lost, than because the system is just running slowly.
+  // b) Short enough that we don't slow down tryjobs.
+  //
+  // The first property is important to avoid skewing the latency metrics that we collect.
+  // For an explanation of why a tap might be lost, see the documentation for TryInject().
+  static constexpr auto kTapRetryInterval = zx::sec(1);
+};
+
+}  // namespace ui_testing
+
+#endif  // SRC_UI_TESTING_UTIL_PORTABLE_UI_TEST_H_
