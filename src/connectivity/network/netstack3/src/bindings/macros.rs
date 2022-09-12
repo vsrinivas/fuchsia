d74aebb1dd391f20765ef2b875a4ba5ec725ee18
Jeff Martin <martinjeffrey@google.com>
2022-09-12 18:27:59 +0000

    [netstack3] `responder_send` logs warn when closed
    Multiply: netstack-interfaces-admin-fidl-test
    
    Fixed: 108746
    Change-Id: Ice950fa313f1d8b072ed6fb3b558edd2a2c916a7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/723383
    Commit-Queue: Jeff Martin <martinjeffrey@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>

--- src/connectivity/network/netstack3/src/bindings/macros.rs
+++ src/connectivity/network/netstack3/src/bindings/macros.rs
@@ -6,11 +6,21 @@
 // they are handled only by logging to error.
 macro_rules! responder_send {
     ($responder:expr, $arg:expr) => {
-        $responder.send($arg).unwrap_or_else(|e| log::error!("Responder send error: {:?}", e))
+        $responder.send($arg).unwrap_or_else(|e| {
+            log::log!(
+                crate::bindings::util::fidl_err_log_level(&e),
+                "Responder send error: {:?}",
+                e
+            )
+        })
     };
     ($responder:expr, $arg1:expr, $arg2:expr) => {
-        $responder
-            .send($arg1, $arg2)
-            .unwrap_or_else(|e| log::error!("Responder send error: {:?}", e))
+        $responder.send($arg1, $arg2).unwrap_or_else(|e| {
+            log::log!(
+                crate::bindings::util::fidl_err_log_level(&e),
+                "Responder send error: {:?}",
+                e
+            )
+        })
     };
 }
