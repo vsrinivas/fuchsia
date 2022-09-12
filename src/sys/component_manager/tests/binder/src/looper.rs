9b480a111648e6b1d8aa7bf23a39f47ab5a0a349
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2022-09-12 18:21:24 +0000

    [component_manager][tests] Migrate binder tests out of log to tracing
    Bug: 89292
    
    Change-Id: I83579a4d8bdb504396f630f1fc2d166422db1e32
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/723922
    Reviewed-by: Gary Bressler <geb@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/sys/component_manager/tests/binder/src/looper.rs
+++ src/sys/component_manager/tests/binder/src/looper.rs
@@ -16,7 +16,7 @@ async fn run_server(stream: ShutdownerRequestStream) -> Result<(), Error> {
             let _ = &request;
             match request {
                 ShutdownerRequest::Shutdown { .. } => {
-                    log::info!("Received request to shutdown. Exiting.");
+                    tracing::info!("Received request to shutdown. Exiting.");
                     std::process::exit(0);
                 }
             }
@@ -33,10 +33,10 @@ async fn main() -> Result<(), Error> {
     let mut fs = ServiceFs::new_local();
     fs.dir("svc").add_fidl_service(IncomingService::Shutdowner);
     fs.take_and_serve_directory_handle()?;
-    log::info!("Listening for incoming connections...");
+    tracing::info!("Listening for incoming connections...");
     const MAX_CONCURRENT: usize = 10_000;
     fs.for_each_concurrent(MAX_CONCURRENT, |IncomingService::Shutdowner(stream)| {
-        run_server(stream).unwrap_or_else(|e| log::error!("{:?}", e))
+        run_server(stream).unwrap_or_else(|err| tracing::error!(?err))
     })
     .await;
 
