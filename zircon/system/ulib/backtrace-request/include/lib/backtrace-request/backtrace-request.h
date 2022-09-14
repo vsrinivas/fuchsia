4fe5fe313af043b450d05dbba8a3a695f2d99b27
Bruno Dal Bo <brunodalbo@google.com>
2022-09-14 22:02:52 +0000

    [backtrace-request] Use zircon/compiler.h attrs
    Change-Id: I4b4b0a87dc1f373f420095c7749fc45dfa567de3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/725323
    Fuchsia-Auto-Submit: Bruno Dal Bo <brunodalbo@google.com>
    Reviewed-by: John Grossman <johngro@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/system/ulib/backtrace-request/include/lib/backtrace-request/backtrace-request.h
+++ zircon/system/ulib/backtrace-request/include/lib/backtrace-request/backtrace-request.h
@@ -6,13 +6,14 @@
 #define BACKTRACE_REQUEST_BACKTRACE_REQUEST_H_
 
 #include <stdint.h>
+#include <zircon/compiler.h>
 
 // Special value we put in the first register to let the exception handler know
 // that we are just requesting a backtrace and we should resume the thread.
 #define BACKTRACE_REQUEST_MAGIC ((uint64_t)0xee726573756d65ee)
 
 // Prints a backtrace, resuming the thread without killing the process.
-__attribute__((always_inline)) static inline void backtrace_request(void) {
+__ALWAYS_INLINE static inline void backtrace_request(void) {
   // Two instructions: one that sets a software breakpoint ("int3" on x64,
   // "brk" on arm64) and one that writes the "magic" value in the first
   // register ("a" on x64, "x0" on arm64).
