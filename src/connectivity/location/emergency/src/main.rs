1adeb0232065bc4a6ec8659a6618a89d4a229322
Andrew Hwang <anhwang@google.com>
2022-09-15 22:59:22 +0000

    [Cobalt 1.1] Migrate connectivity_location Fuchsia code to Cobalt 1.1
    Replaced Cobalt library imports and api calls with Cobalt 1.1
    MetricEventLogger api calls.
    
    Bug: 96145
    Change-Id: I0481e0c8c3a16767ac624669f2125ea94ac93601
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/724663
    Commit-Queue: Andrew Hwang <anhwang@google.com>
    Reviewed-by: Zach Bush <zmbush@google.com>
    Reviewed-by: Mukesh Agrawal <quiche@google.com>

--- src/connectivity/location/emergency/src/main.rs
+++ src/connectivity/location/emergency/src/main.rs
@@ -3,20 +3,25 @@
 // found in the LICENSE file.
 
 use {
-    anyhow::{self, Context, Error},
+    anyhow::{self, format_err, Context, Error},
     emergency_lib::{
         bss_cache::{Bss, BssCache, BssId, RealBssCache, UpdateError},
         bss_resolver::{BssResolver, RealBssResolver, ResolverError},
     },
     emergency_metrics_registry::{
-        self as metrics, EmergencyGetCurrentFailureMetricDimensionCause as GetCurrentFailure,
-        EmergencyGetCurrentResultMetricDimensionResult as GetCurrentResult,
-        WlanSensorReportMetricDimensionResult as WlanSensorReportResult,
-        EMERGENCY_GET_CURRENT_ACCURACY_METRIC_ID as GET_CURRENT_ACCURACY_METRIC_ID,
-        EMERGENCY_GET_CURRENT_FAILURE_METRIC_ID as GET_CURRENT_FAILURE_METRIC_ID,
-        EMERGENCY_GET_CURRENT_LATENCY_METRIC_ID as GET_CURRENT_LATENCY_METRIC_ID,
-        EMERGENCY_GET_CURRENT_RESULT_METRIC_ID as GET_CURRENT_RESULT_METRIC_ID,
-        WLAN_SENSOR_REPORT_METRIC_ID,
+        self as metrics,
+        EmergencyGetCurrentFailureMigratedMetricDimensionCause as GetCurrentFailure,
+        EmergencyGetCurrentResultMigratedMetricDimensionResult as GetCurrentResult,
+        WlanSensorReportMigratedMetricDimensionResult as WlanSensorReportResult,
+        EMERGENCY_GET_CURRENT_ACCURACY_MIGRATED_METRIC_ID as GET_CURRENT_ACCURACY_METRIC_ID,
+        EMERGENCY_GET_CURRENT_FAILURE_MIGRATED_METRIC_ID as GET_CURRENT_FAILURE_METRIC_ID,
+        EMERGENCY_GET_CURRENT_LATENCY_MIGRATED_METRIC_ID as GET_CURRENT_LATENCY_METRIC_ID,
+        EMERGENCY_GET_CURRENT_RESULT_MIGRATED_METRIC_ID as GET_CURRENT_RESULT_METRIC_ID,
+        WLAN_SENSOR_REPORT_MIGRATED_METRIC_ID,
+    },
+    fidl_contrib::{
+        protocol_connector::ConnectedProtocol, protocol_connector::ProtocolSender,
+        ProtocolConnector,
     },
     fidl_fuchsia_location::Error as LocationError,
     fidl_fuchsia_location_position::{
@@ -25,9 +30,12 @@ use {
     fidl_fuchsia_location_sensor::{
         WlanBaseStationWatcherRequest, WlanBaseStationWatcherRequestStream,
     },
+    fidl_fuchsia_metrics::{
+        MetricEvent, MetricEventLoggerFactoryMarker, MetricEventLoggerProxy, ProjectSpec,
+    },
     fidl_fuchsia_net_http::LoaderMarker as HttpLoaderMarker,
     fuchsia_async as fasync,
-    fuchsia_cobalt::{CobaltConnector, CobaltSender, ConnectionType},
+    fuchsia_cobalt_builders::MetricEventExt,
     fuchsia_component::{client::connect_to_protocol, server::ServiceFs},
     fuchsia_syslog::{self as syslog},
     futures::{lock::Mutex, prelude::*},
@@ -48,12 +56,55 @@ enum IncomingRequest {
     WlanBaseStationWatcherRequest(WlanBaseStationWatcherRequestStream),
 }
 
+struct CobaltConnectedService;
+impl ConnectedProtocol for CobaltConnectedService {
+    type Protocol = MetricEventLoggerProxy;
+    type ConnectError = Error;
+    type Message = MetricEvent;
+    type SendError = Error;
+
+    fn get_protocol<'a>(
+        &'a mut self,
+    ) -> future::BoxFuture<'a, Result<MetricEventLoggerProxy, Error>> {
+        async {
+            let (logger_proxy, server_end) =
+                fidl::endpoints::create_proxy().context("failed to create proxy endpoints")?;
+            let metric_event_logger_factory =
+                connect_to_protocol::<MetricEventLoggerFactoryMarker>()
+                    .context("Failed to connect to fuchsia::metrics::MetricEventLoggerFactory")?;
+
+            metric_event_logger_factory
+                .create_metric_event_logger(
+                    ProjectSpec { project_id: Some(metrics::PROJECT_ID), ..ProjectSpec::EMPTY },
+                    server_end,
+                )
+                .await?
+                .map_err(|e| format_err!("Connection to MetricEventLogger refused {e:?}"))?;
+            Ok(logger_proxy)
+        }
+        .boxed()
+    }
+
+    fn send_message<'a>(
+        &'a mut self,
+        protocol: &'a MetricEventLoggerProxy,
+        mut msg: MetricEvent,
+    ) -> future::BoxFuture<'a, Result<(), Error>> {
+        async move {
+            let fut = protocol.log_metric_events(&mut std::iter::once(&mut msg));
+            fut.await?.map_err(|e| format_err!("Failed to log metric {e:?}"))?;
+            Ok(())
+        }
+        .boxed()
+    }
+}
+
 #[fasync::run_singlethreaded]
 async fn main() -> Result<(), anyhow::Error> {
     syslog::init().context("failed to initialize logging")?;
 
     let (cobalt_api, cobalt_fut) =
-        CobaltConnector::default().serve(ConnectionType::project_id(metrics::PROJECT_ID));
+        ProtocolConnector::new(CobaltConnectedService).serve_and_log_errors();
     let _cobalt_task = fasync::Task::spawn(cobalt_fut);
     let bss_cache = Mutex::new(RealBssCache::new());
     let bss_resolver = RealBssResolver::new(
@@ -82,7 +133,7 @@ async fn handle_client_requests<C: BssCache, R: BssResolver>(
     bss_cache: &Mutex<C>,
     bss_resolver: &R,
     protocol: IncomingRequest,
-    cobalt_api: CobaltSender,
+    cobalt_api: ProtocolSender<MetricEvent>,
 ) -> Result<(), Error> {
     match protocol {
         IncomingRequest::EmergencyProviderRequest(client) => {
@@ -98,7 +149,7 @@ async fn process_location_queries<C: BssCache, R: BssResolver>(
     bss_cache: &Mutex<C>,
     bss_resolver: &R,
     mut stream: EmergencyProviderRequestStream,
-    mut cobalt_api: CobaltSender,
+    mut cobalt_api: ProtocolSender<MetricEvent>,
 ) -> Result<(), Error> {
     loop {
         match stream.try_next().await.context("failed to read emergency provider request")? {
@@ -136,7 +187,7 @@ async fn process_location_queries<C: BssCache, R: BssResolver>(
 async fn process_bss_updates<C: BssCache>(
     bss_cache: &Mutex<C>,
     mut stream: WlanBaseStationWatcherRequestStream,
-    mut cobalt_api: CobaltSender,
+    mut cobalt_api: ProtocolSender<MetricEvent>,
 ) -> Result<(), Error> {
     loop {
         match stream.try_next().await.context("failed to read base station watcher request")? {
@@ -164,48 +215,60 @@ async fn process_bss_updates<C: BssCache>(
 fn report_lookup_success_metrics(
     position: &Position,
     latency: &Duration,
-    cobalt_api: &mut CobaltSender,
+    cobalt_api: &mut ProtocolSender<MetricEvent>,
 ) {
     const METERS_TO_MILLIMETERS: f64 = 1000.0;
-    cobalt_api.log_event(GET_CURRENT_RESULT_METRIC_ID, GetCurrentResult::Success);
-    cobalt_api.log_elapsed_time(
-        GET_CURRENT_LATENCY_METRIC_ID,
-        (),
-        i64::try_from(latency.as_millis()).unwrap_or(i64::MAX),
+    cobalt_api.send(
+        MetricEvent::builder(GET_CURRENT_RESULT_METRIC_ID)
+            .with_event_codes(GetCurrentResult::Success)
+            .as_occurrence(1),
     );
-    cobalt_api.log_event_count(
-        GET_CURRENT_ACCURACY_METRIC_ID,
-        (),
-        0,
+    cobalt_api.send(
+        MetricEvent::builder(GET_CURRENT_LATENCY_METRIC_ID)
+            .as_integer(i64::try_from(latency.as_millis()).unwrap_or(i64::MAX)),
+    );
+    cobalt_api.send(MetricEvent::builder(GET_CURRENT_ACCURACY_METRIC_ID).as_integer(
         match position.extras.accuracy_meters {
             Some(accuracy_meters) => (accuracy_meters * METERS_TO_MILLIMETERS) as i64,
             None => i64::MAX,
         },
-    )
+    ));
 }
 
-fn report_lookup_failure_metrics(error: ResolverError, cobalt_api: &mut CobaltSender) {
-    cobalt_api.log_event(GET_CURRENT_RESULT_METRIC_ID, GetCurrentResult::Failure);
-    cobalt_api.log_event(
-        GET_CURRENT_FAILURE_METRIC_ID,
-        match error {
-            ResolverError::NoBsses => GetCurrentFailure::NoBsses,
-            ResolverError::Internal => GetCurrentFailure::Internal,
-            ResolverError::Lookup => GetCurrentFailure::Lookup,
-        },
+fn report_lookup_failure_metrics(
+    error: ResolverError,
+    cobalt_api: &mut ProtocolSender<MetricEvent>,
+) {
+    cobalt_api.send(
+        MetricEvent::builder(GET_CURRENT_RESULT_METRIC_ID)
+            .with_event_codes(GetCurrentResult::Failure)
+            .as_occurrence(1),
+    );
+    cobalt_api.send(
+        MetricEvent::builder(GET_CURRENT_FAILURE_METRIC_ID)
+            .with_event_codes(match error {
+                ResolverError::NoBsses => GetCurrentFailure::NoBsses,
+                ResolverError::Internal => GetCurrentFailure::Internal,
+                ResolverError::Lookup => GetCurrentFailure::Lookup,
+            })
+            .as_occurrence(1),
     );
 }
 
-fn report_bss_update_metrics(result: Result<(), UpdateError>, cobalt_api: &mut CobaltSender) {
-    cobalt_api.log_event(
-        WLAN_SENSOR_REPORT_METRIC_ID,
-        match result {
-            Ok(()) => WlanSensorReportResult::Success,
-            Err(UpdateError::NoBssIds) => WlanSensorReportResult::NoBssIds,
-            Err(UpdateError::NoBsses) => WlanSensorReportResult::NoBsses,
-            Err(UpdateError::Ipc) => WlanSensorReportResult::IpcError,
-            Err(UpdateError::Service) => WlanSensorReportResult::ServiceError,
-        },
+fn report_bss_update_metrics(
+    result: Result<(), UpdateError>,
+    cobalt_api: &mut ProtocolSender<MetricEvent>,
+) {
+    cobalt_api.send(
+        MetricEvent::builder(WLAN_SENSOR_REPORT_MIGRATED_METRIC_ID)
+            .with_event_codes(match result {
+                Ok(()) => WlanSensorReportResult::Success,
+                Err(UpdateError::NoBssIds) => WlanSensorReportResult::NoBssIds,
+                Err(UpdateError::NoBsses) => WlanSensorReportResult::NoBsses,
+                Err(UpdateError::Ipc) => WlanSensorReportResult::IpcError,
+                Err(UpdateError::Service) => WlanSensorReportResult::ServiceError,
+            })
+            .as_occurrence(1),
     );
 }
 
@@ -213,11 +276,7 @@ fn report_bss_update_metrics(result: Result<(), UpdateError>, cobalt_api: &mut C
 mod tests {
     use {
         super::*,
-        cobalt_client::traits::AsEventCode,
         fidl::endpoints::create_proxy_and_stream,
-        fidl_fuchsia_cobalt::CobaltEvent,
-        fuchsia_cobalt::cobalt_event_builder::CobaltEventExt,
-        fuchsia_cobalt::CobaltSender,
         futures::channel::mpsc,
         futures::{future, pin_mut},
         std::task::Poll,
@@ -323,13 +382,13 @@ mod tests {
                 assert_eq!(
                     cobalt_receiver
                         .filter(|event| {
-                            future::ready(event.metric_id == WLAN_SENSOR_REPORT_METRIC_ID)
+                            future::ready(event.metric_id == WLAN_SENSOR_REPORT_MIGRATED_METRIC_ID)
                         })
-                        .collect::<Vec<CobaltEvent>>()
+                        .collect::<Vec<MetricEvent>>()
                         .await,
-                    vec![CobaltEvent::builder(WLAN_SENSOR_REPORT_METRIC_ID)
-                        .with_event_code(cobalt_event.as_event_code())
-                        .as_event()]
+                    vec![MetricEvent::builder(WLAN_SENSOR_REPORT_MIGRATED_METRIC_ID)
+                        .with_event_codes(cobalt_event)
+                        .as_occurrence(1)]
                 );
             };
             pin_mut!(test_fut);
@@ -364,8 +423,10 @@ mod tests {
             let _ = server_fut.await;
             assert_eq!(
                 cobalt_receiver
-                    .filter(|event| future::ready(event.metric_id != WLAN_SENSOR_REPORT_METRIC_ID))
-                    .collect::<Vec<CobaltEvent>>()
+                    .filter(|event| future::ready(
+                        event.metric_id != WLAN_SENSOR_REPORT_MIGRATED_METRIC_ID
+                    ))
+                    .collect::<Vec<MetricEvent>>()
                     .await,
                 vec![]
             );
@@ -377,8 +438,8 @@ mod tests {
             super::*,
             assert_matches::assert_matches,
             emergency_lib::bss_resolver::ResolverError,
-            fidl_fuchsia_cobalt::EventPayload::ElapsedMicros,
             fidl_fuchsia_location_position::{EmergencyProviderMarker, Position, PositionExtras},
+            fidl_fuchsia_metrics::MetricEventPayload,
             test_case::test_case,
         };
 
@@ -459,11 +520,11 @@ mod tests {
             assert_eq!(
                 cobalt_receiver
                     .filter(|event| future::ready(event.metric_id == GET_CURRENT_RESULT_METRIC_ID))
-                    .collect::<Vec<CobaltEvent>>()
+                    .collect::<Vec<MetricEvent>>()
                     .await,
-                vec![CobaltEvent::builder(GET_CURRENT_RESULT_METRIC_ID)
-                    .with_event_code(GetCurrentResult::Success.as_event_code())
-                    .as_event()]
+                vec![MetricEvent::builder(GET_CURRENT_RESULT_METRIC_ID)
+                    .with_event_codes(GetCurrentResult::Success)
+                    .as_occurrence(1)]
             );
         }
 
@@ -486,11 +547,11 @@ mod tests {
             assert_eq!(
                 cobalt_receiver
                     .filter(|event| future::ready(event.metric_id == GET_CURRENT_RESULT_METRIC_ID))
-                    .collect::<Vec<CobaltEvent>>()
+                    .collect::<Vec<MetricEvent>>()
                     .await,
-                vec![CobaltEvent::builder(GET_CURRENT_RESULT_METRIC_ID)
-                    .with_event_code(GetCurrentResult::Failure.as_event_code())
-                    .as_event()]
+                vec![MetricEvent::builder(GET_CURRENT_RESULT_METRIC_ID)
+                    .with_event_codes(GetCurrentResult::Failure)
+                    .as_occurrence(1)]
             );
         }
 
@@ -515,10 +576,9 @@ mod tests {
                     .filter(|event| future::ready(
                         event.metric_id == GET_CURRENT_ACCURACY_METRIC_ID
                     ))
-                    .collect::<Vec<CobaltEvent>>()
+                    .collect::<Vec<MetricEvent>>()
                     .await,
-                vec![CobaltEvent::builder(GET_CURRENT_ACCURACY_METRIC_ID).as_count_event(
-                    0,
+                vec![MetricEvent::builder(GET_CURRENT_ACCURACY_METRIC_ID).as_integer(
                     1000 // In millimeters, for higher precision
                 )]
             );
@@ -545,10 +605,9 @@ mod tests {
                     .filter(|event| future::ready(
                         event.metric_id == GET_CURRENT_ACCURACY_METRIC_ID
                     ))
-                    .collect::<Vec<CobaltEvent>>()
+                    .collect::<Vec<MetricEvent>>()
                     .await,
-                vec![CobaltEvent::builder(GET_CURRENT_ACCURACY_METRIC_ID)
-                    .as_count_event(0, i64::MAX)]
+                vec![MetricEvent::builder(GET_CURRENT_ACCURACY_METRIC_ID).as_integer(i64::MAX)]
             );
         }
 
@@ -570,14 +629,14 @@ mod tests {
             let _ = server_fut.await;
             let latency_reports = cobalt_receiver
                 .filter(|event| future::ready(event.metric_id == GET_CURRENT_LATENCY_METRIC_ID))
-                .collect::<Vec<CobaltEvent>>()
+                .collect::<Vec<MetricEvent>>()
                 .await;
             assert_eq!(latency_reports.len(), 1);
             assert_matches!(
                 latency_reports[0],
-                CobaltEvent {
+                MetricEvent {
                     metric_id: GET_CURRENT_LATENCY_METRIC_ID,
-                    payload: ElapsedMicros(_),
+                    payload: MetricEventPayload::IntegerValue(0),
                     ..
                 }
             );
@@ -611,11 +670,11 @@ mod tests {
                         .filter(|event| {
                             future::ready(event.metric_id == GET_CURRENT_FAILURE_METRIC_ID)
                         })
-                        .collect::<Vec<CobaltEvent>>()
+                        .collect::<Vec<MetricEvent>>()
                         .await,
-                    vec![CobaltEvent::builder(GET_CURRENT_FAILURE_METRIC_ID)
-                        .with_event_code(cobalt_error.as_event_code())
-                        .as_event()]
+                    vec![MetricEvent::builder(GET_CURRENT_FAILURE_METRIC_ID)
+                        .with_event_codes(cobalt_error)
+                        .as_occurrence(1)]
                 );
             };
             pin_mut!(test_fut);
@@ -663,7 +722,7 @@ mod tests {
                         .filter(|event| future::ready(
                             expected_metric_ids.iter().find(|&&i| i == event.metric_id).is_none()
                         ))
-                        .collect::<Vec<CobaltEvent>>()
+                        .collect::<Vec<MetricEvent>>()
                         .await,
                     vec![]
                 );
@@ -678,11 +737,11 @@ mod tests {
         }
     }
 
-    fn make_fake_cobalt_connection() -> (CobaltSender, mpsc::Receiver<CobaltEvent>) {
+    fn make_fake_cobalt_connection() -> (ProtocolSender<MetricEvent>, mpsc::Receiver<MetricEvent>) {
         const MAX_METRICS_PER_QUERY: usize = 3;
         const MAX_QUERIES: usize = 1;
         let (sender, receiver) = mpsc::channel(MAX_METRICS_PER_QUERY * MAX_QUERIES);
-        (CobaltSender::new(sender), receiver)
+        (ProtocolSender::<MetricEvent>::new(sender), receiver)
     }
 }
 
