0c9768e76adc52b3cbcbf5cdc46727e04a21cb1d
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2022-09-15 19:56:20 +0000

    [moniker] Remove unused library
    Bug: 89292
    
    Change-Id: I2cb2772bcdea9159feaee4b489513d0f4a4641be
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/725103
    Reviewed-by: Gary Bressler <geb@google.com>
    Fuchsia-Auto-Submit: Miguel Flores <miguelfrde@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/sys/lib/moniker/src/error.rs
+++ src/sys/lib/moniker/src/error.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use {log::*, thiserror::Error};
+use thiserror::Error;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
