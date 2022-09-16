e03a3359e6568b673b6182fdefa37f72b8f68409
David Gilhooley <dgilhooley@google.com>
2022-09-16 18:31:28 +0000

    [hid-input-report][dfv2] Move to DriverBase
    Move the dfv2 driver to driver base to make things simpler
    and easier to read.
    
    This requires adding access to component::OutgoingDirectory
    from driver::OutgoingDirectory
    
    Change-Id: I6d869e91220a231d2bab6497930a42987e1e3c51
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/726471
    Reviewed-by: Novin Changizi <novinc@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: David Gilhooley <dgilhooley@google.com>

--- src/ui/input/drivers/hid-input-report/driver_v2.cc
+++ src/ui/input/drivers/hid-input-report/driver_v2.cc
@@ -7,7 +7,7 @@
 #include <lib/async/cpp/executor.h>
 #include <lib/ddk/debug.h>
 #include <lib/driver2/devfs_exporter.h>
-#include <lib/driver2/driver2.h>
+#include <lib/driver2/driver2_cpp.h>
 #include <lib/fpromise/scope.h>
 #include <lib/inspect/component/cpp/component.h>
 #include <lib/sys/component/cpp/outgoing_directory.h>
@@ -22,32 +22,18 @@ namespace fio = fuchsia_io;
 
 namespace {
 
-class InputReportDriver {
+class InputReportDriver : public driver::DriverBase {
  public:
-  InputReportDriver(async_dispatcher_t* dispatcher, fidl::WireSharedClient<fdf2::Node> node,
-                    driver::Namespace ns, component::OutgoingDirectory outgoing,
-                    driver::Logger logger)
-      : dispatcher_(dispatcher),
-        outgoing_(std::move(outgoing)),
-        node_(std::move(node)),
-        ns_(std::move(ns)),
-        logger_(std::move(logger)),
-        executor_(dispatcher) {}
+  InputReportDriver(driver::DriverStartArgs start_args, fdf::UnownedDispatcher dispatcher)
+      : DriverBase("InputReport", std::move(start_args), std::move(dispatcher)) {}
 
-  static constexpr const char* Name() { return "InputReport"; }
-
-  static zx::status<std::unique_ptr<InputReportDriver>> Start(
-      fdf2::wire::DriverStartArgs& start_args, fdf::UnownedDispatcher dispatcher,
-      fidl::WireSharedClient<fdf2::Node> node, driver::Namespace ns, driver::Logger logger) {
-    auto outgoing = component::OutgoingDirectory::Create(dispatcher->async_dispatcher());
-    auto driver =
-        std::make_unique<InputReportDriver>(dispatcher->async_dispatcher(), std::move(node),
-                                            std::move(ns), std::move(outgoing), std::move(logger));
-    auto parent_symbol = driver::GetSymbol<compat::device_t*>(start_args, compat::kDeviceSymbol);
+  zx::status<> Start() override {
+    executor_.emplace(async_dispatcher());
+    auto parent_symbol = driver::GetSymbol<compat::device_t*>(symbols(), compat::kDeviceSymbol);
 
     hid_device_protocol_t proto = {};
     if (parent_symbol->proto_ops.id != ZX_PROTOCOL_HID_DEVICE) {
-      FDF_LOGL(ERROR, driver->logger_, "Didn't find HID_DEVICE protocol");
+      FDF_LOG(ERROR, "Didn't find HID_DEVICE protocol");
       return zx::error(ZX_ERR_NOT_FOUND);
     }
     proto.ctx = parent_symbol->context;
@@ -55,45 +41,16 @@ class InputReportDriver {
 
     ddk::HidDeviceProtocolClient hiddev(&proto);
     if (!hiddev.is_valid()) {
-      FDF_LOGL(ERROR, driver->logger_, "Failed to create hiddev");
+      FDF_LOG(ERROR, "Failed to create hiddev");
       return zx::error(ZX_ERR_INTERNAL);
     }
-    driver->input_report_.emplace(std::move(hiddev));
+    input_report_.emplace(std::move(hiddev));
 
-    auto result = driver->Run(std::move(start_args.outgoing_dir()));
-    if (result.is_error()) {
-      return result.take_error();
-    }
-    return zx::ok(std::move(driver));
-  }
-
- private:
-  zx::status<> ConnectToDevfsExporter() {
-    // Connect to DevfsExporter.
-    auto endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
-    if (endpoints.is_error()) {
-      return endpoints.take_error();
-    }
-    // Serve a connection to outgoing.
-    auto status = outgoing_.Serve(std::move(endpoints->server));
-    if (status.is_error()) {
-      return status.take_error();
-    }
-
-    auto exporter = driver::DevfsExporter::Create(
-        ns_, dispatcher_, fidl::WireSharedClient(std::move(endpoints->client), dispatcher_));
-    if (exporter.is_error()) {
-      return zx::error(exporter.error_value());
-    }
-    exporter_ = std::move(*exporter);
-    return zx::ok();
-  }
-
-  zx::status<> Run(fidl::ServerEnd<fio::Directory> outgoing_dir) {
     // Expose the driver's inspect data.
-    exposed_inspector_.emplace(
-        inspect::ComponentInspector(outgoing_, dispatcher_, input_report_->Inspector()));
+    exposed_inspector_.emplace(inspect::ComponentInspector(
+        context().outgoing()->component(), async_dispatcher(), input_report_->Inspector()));
 
+    // Start the inner DFv1 driver.
     input_report_->Start();
 
     // Connect to DevfsExporter.
@@ -103,11 +60,12 @@ class InputReportDriver {
     }
 
     // Connect to our parent.
-    auto result = component::ConnectAt<fuchsia_driver_compat::Service::Device>(ns_.svc_dir());
+    auto result = component::ConnectAt<fuchsia_driver_compat::Service::Device>(
+        context().incoming()->svc_dir());
     if (result.is_error()) {
       return result.take_error();
     }
-    parent_client_.Bind(std::move(result.value()), dispatcher_);
+    parent_client_.Bind(std::move(result.value()), async_dispatcher());
 
     auto compat_connect =
         fpromise::make_result_promise<void, zx_status_t>(fpromise::ok())
@@ -130,14 +88,9 @@ class InputReportDriver {
             .and_then([this]() -> fpromise::promise<void, zx_status_t> {
               child_ = compat::DeviceServer("InputReport", ZX_PROTOCOL_INPUTREPORT,
                                             parent_topo_path_ + "/InputReport", {});
-              auto status = outgoing_.AddProtocol(
-                  [this](zx::channel channel) {
-                    fidl::BindServer<fidl::WireServer<fuchsia_input_report::InputDevice>>(
-                        dispatcher_,
-                        fidl::ServerEnd<fuchsia_input_report::InputDevice>(std::move(channel)),
-                        &input_report_.value());
-                  },
-                  "InputReport");
+              auto status =
+                  context().outgoing()->component().AddProtocol<fuchsia_input_report::InputDevice>(
+                      &input_report_.value(), "InputReport");
               if (status.is_error()) {
                 return fpromise::make_result_promise<void, zx_status_t>(
                     fpromise::error(status.error_value()));
@@ -155,18 +108,36 @@ class InputReportDriver {
             .or_else([this](zx_status_t& result) {
               FDF_LOG(WARNING, "Device setup failed with: %s", zx_status_get_string(result));
             });
-    executor_.schedule_task(std::move(compat_connect));
+    executor_->schedule_task(std::move(compat_connect));
 
-    return outgoing_.Serve(std::move(outgoing_dir));
+    return zx::ok();
   }
 
-  async_dispatcher_t* dispatcher_;
+ private:
+  zx::status<> ConnectToDevfsExporter() {
+    // Connect to DevfsExporter.
+    auto endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
+    if (endpoints.is_error()) {
+      return endpoints.take_error();
+    }
+    // Serve a connection to outgoing.
+    auto status = context().outgoing()->Serve(std::move(endpoints->server));
+    if (status.is_error()) {
+      return status.take_error();
+    }
+
+    auto exporter = driver::DevfsExporter::Create(
+        *context().incoming(), async_dispatcher(),
+        fidl::WireSharedClient(std::move(endpoints->client), async_dispatcher()));
+    if (exporter.is_error()) {
+      return zx::error(exporter.error_value());
+    }
+    exporter_ = std::move(*exporter);
+    return zx::ok();
+  }
+
   std::optional<hid_input_report_dev::InputReport> input_report_;
-  component::OutgoingDirectory outgoing_;
-  fidl::WireSharedClient<fdf2::Node> node_;
-  driver::Namespace ns_;
-  driver::Logger logger_;
-  async::Executor executor_;
+  std::optional<async::Executor> executor_;
 
   std::optional<inspect::ComponentInspector> exposed_inspector_;
 
@@ -191,4 +162,4 @@ bool driver_log_severity_enabled_internal(const zx_driver_t* drv, fx_log_severit
   return true;
 }
 
-FUCHSIA_DRIVER_RECORD_CPP_V1(InputReportDriver);
+FUCHSIA_DRIVER_RECORD_CPP_V2(driver::Record<InputReportDriver>);
