f4da9687ef3aea662a60ef511f9ff239e8238668
Brian Bosak <bbosak@google.com>
2022-09-16 19:24:59 +0000

    [cm] Migrate scoped instance tests
    This CL migrates the scoped instance tests
    to use events V2 per RFC-0121.
    MULTIPLY: fuchsia-pkg://fuchsia.com/scoped-instance-tests#meta/scoped_instance_test.cm
    
    Bug: 81980
    Change-Id: I10ba4c7495658ff44663c1d0bdedfc33da062b5b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/726355
    Reviewed-by: Gary Bressler <geb@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>

--- src/lib/fuchsia-component-test/tests/scoped_instance/lib.rs
+++ src/lib/fuchsia-component-test/tests/scoped_instance/lib.rs
@@ -29,12 +29,9 @@ async fn scoped_instances(root_component: &'static str) {
     let instance =
         builder.build_in_nested_component_manager("#meta/component_manager.cm").await.unwrap();
     let proxy =
-        instance.root.connect_to_protocol_at_exposed_dir::<fsys::EventSourceMarker>().unwrap();
+        instance.root.connect_to_protocol_at_exposed_dir::<fsys::EventStream2Marker>().unwrap();
 
-    let event_source = EventSource::from_proxy(proxy);
-
-    let mut event_stream =
-        event_source.subscribe(vec![EventSubscription::new(vec![Stopped::NAME])]).await.unwrap();
+    let mut event_stream = EventStream::new_v2(proxy);
 
     instance.start_component_tree().await.unwrap();
 
