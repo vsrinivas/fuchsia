2e980eb5a1e08ef532ccf4ba460b78d75416ea51
Novin Changizi <novinc@google.com>
2022-09-19 23:45:56 +0000

    [fdf] Migrate devfs exporter test to DriverBase
    Bug: 107786
    Change-Id: I870bec8041ae840766774c512b38c951ef0571a8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/727308
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>

--- src/devices/tests/v2/devfs_exporter/root_driver.cc
+++ src/devices/tests/v2/devfs_exporter/root_driver.cc
@@ -3,71 +3,29 @@
 // found in the LICENSE file.
 
 #include <fidl/fuchsia.devfs.test/cpp/wire.h>
-#include <fidl/fuchsia.driver.framework/cpp/wire.h>
-#include <lib/async/cpp/executor.h>
 #include <lib/driver2/devfs_exporter.h>
-#include <lib/driver2/logger.h>
-#include <lib/driver2/namespace.h>
-#include <lib/driver2/promise.h>
-#include <lib/driver2/record_cpp.h>
-#include <lib/fpromise/bridge.h>
-#include <lib/fpromise/scope.h>
-#include <lib/sys/component/cpp/outgoing_directory.h>
+#include <lib/driver2/driver2_cpp.h>
 
 namespace fdf {
 using namespace fuchsia_driver_framework;
 }  // namespace fdf
 
-namespace fcd = fuchsia_component_decl;
-namespace fio = fuchsia_io;
 namespace ft = fuchsia_devfs_test;
-
-using fpromise::error;
-using fpromise::ok;
-using fpromise::promise;
-using fpromise::result;
 
 namespace {
 
-class RootDriver : public fidl::WireServer<ft::Device> {
+class RootDriver : public driver::DriverBase, public fidl::WireServer<ft::Device> {
+  static constexpr std::string_view name = "root";
+
  public:
-  RootDriver(async_dispatcher_t* dispatcher, fidl::WireSharedClient<fdf::Node> node,
-             driver::Namespace ns, driver::Logger logger, component::OutgoingDirectory outgoing)
-      : dispatcher_(dispatcher),
-        executor_(dispatcher),
-        outgoing_(std::move(outgoing)),
-        node_(std::move(node)),
-        ns_(std::move(ns)),
-        logger_(std::move(logger)) {}
+  RootDriver(driver::DriverStartArgs start_args, fdf::UnownedDispatcher dispatcher)
+      : driver::DriverBase(name, std::move(start_args), std::move(dispatcher)) {}
 
-  static constexpr const char* Name() { return "root"; }
-
-  static zx::status<std::unique_ptr<RootDriver>> Start(fdf::wire::DriverStartArgs& start_args,
-                                                       fdf::UnownedDispatcher dispatcher,
-                                                       fidl::WireSharedClient<fdf::Node> node,
-                                                       driver::Namespace ns,
-                                                       driver::Logger logger) {
-    auto outgoing = component::OutgoingDirectory::Create(dispatcher->async_dispatcher());
-    auto driver =
-        std::make_unique<RootDriver>(dispatcher->async_dispatcher(), std::move(node), std::move(ns),
-                                     std::move(logger), std::move(outgoing));
-    auto result = driver->Run(std::move(start_args.outgoing_dir()));
-    if (result.is_error()) {
-      return result.take_error();
-    }
-    return zx::ok(std::move(driver));
-  }
-
- private:
-  zx::status<> Run(fidl::ServerEnd<fio::Directory> outgoing_dir) {
+  zx::status<> Start() override {
     // Setup the outgoing directory.
-    auto status = outgoing_.AddProtocol<ft::Device>(this, Name());
+    auto status = context().outgoing()->component().AddProtocol<ft::Device>(this, name);
     if (status.is_error()) {
       return status.take_error();
-    }
-    auto serve = outgoing_.Serve(std::move(outgoing_dir));
-    if (serve.is_error()) {
-      return serve.take_error();
     }
 
     // Create the devfs exporter.
@@ -75,19 +33,20 @@ class RootDriver : public fidl::WireServer<ft::Device> {
     if (endpoints.is_error()) {
       return zx::error(endpoints.status_value());
     }
-    status = outgoing_.Serve(std::move(endpoints->server));
+    status = context().outgoing()->Serve(std::move(endpoints->server));
     if (status.is_error()) {
       return status.take_error();
     }
     auto exporter = driver::DevfsExporter::Create(
-        ns_, dispatcher_, fidl::WireSharedClient(std::move(endpoints->client), dispatcher_));
+        *context().incoming(), async_dispatcher(),
+        fidl::WireSharedClient(std::move(endpoints->client), async_dispatcher()));
     if (exporter.is_error()) {
       return exporter.take_error();
     }
     exporter_ = std::move(*exporter);
 
     // Export "root-device" to devfs.
-    exporter_.Export(std::string("svc/").append(Name()), "root-device", {}, 0,
+    exporter_.Export(std::string("svc/").append(name), "root-device", {}, 0,
                      [this](zx_status_t status) {
                        if (status != ZX_OK) {
                          UnbindNode(status);
@@ -96,27 +55,18 @@ class RootDriver : public fidl::WireServer<ft::Device> {
     return zx::ok();
   }
 
+ private:
   void UnbindNode(zx_status_t status) {
     FDF_LOG(ERROR, "Failed to start root driver: %s", zx_status_get_string(status));
-    node_.AsyncTeardown();
+    node().reset();
   }
 
   // fidl::WireServer<ft::Device>
   void Ping(PingCompleter::Sync& completer) override { completer.Reply(); }
 
-  async_dispatcher_t* const dispatcher_;
-  async::Executor executor_;
-  component::OutgoingDirectory outgoing_;
-
-  fidl::WireSharedClient<fdf::Node> node_;
-  driver::Namespace ns_;
-  driver::Logger logger_;
   driver::DevfsExporter exporter_;
-
-  // NOTE: Must be the last member.
-  fpromise::scope scope_;
 };
 
 }  // namespace
 
-FUCHSIA_DRIVER_RECORD_CPP_V1(RootDriver);
+FUCHSIA_DRIVER_RECORD_CPP_V2(driver::Record<RootDriver>);
