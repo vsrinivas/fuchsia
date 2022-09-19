50e4938648c28379e227aa7b17d0776ef7e61873
Xyan Bhatnagar <xbhatnag@google.com>
2022-09-19 17:46:47 +0000

    [ffx] Remove unused component plugins
    We are removing the following plugins due to a lack of usage.
    Internal analytics on the `ffx component` plugins show little
    to no usage of the following plugins:
    
    * ffx component knock (replaced by `ffx component doctor`)
    * ffx component run-legacy (exists as `fx shell run`)
    * ffx component select moniker (exists as `ffx component show`)
    
    These plugins use outdated techniques to perform their functions
    and their functionality exists in other tools/plugins.
    
    Change-Id: I70533aa6b67bc9afbcf096a014fbb966ccdd60e8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/727122
    Reviewed-by: Gary Boone <gboone@google.com>
    Commit-Queue: Xyan Bhatnagar <xbhatnag@google.com>

--- src/developer/ffx/plugins/component/select/src/args.rs
+++ src/developer/ffx/plugins/component/select/src/args.rs
@@ -10,29 +10,10 @@ use {argh::FromArgs, ffx_core::ffx_command};
 #[argh(
     subcommand,
     name = "select",
-    description = "Lists component instances matching a selector",
-    example = "To show services exposed by remote-control:
+    description = "Lists component instances that expose/use a capability",
+    example = "To show all components that expose/use a capability:
 
-    $ ffx component select moniker remote-control:expose:*'
-
-Or to show all services offered by v1 components:
-
-    $ ffx component select moniker core/appmgr:out:*
-
-Or to show all components that expose a capability:
-
-    $ ffx component select capability fuchsia.sys.Loader",
-    note = "Component select allows for 
-1). looking up various services exposed by the
-component. The command expects a <selector> with the following format:
-
-`<component moniker>:(in|out|exposed)[:<service name>]`
-
-Wildcards may be used anywhere in the selector.
-
-2). looking up components that expose a capability. The command takes in 
-a capability name consists of a string containing the characters a to z, 
-A to Z, 0 to 9, underscore (_), hyphen (-), or the full stop character (.)."
+    $ ffx component select capability fuchsia.sys.Loader"
 )]
 pub struct ComponentSelectCommand {
     #[argh(subcommand)]
@@ -42,17 +23,7 @@ pub struct ComponentSelectCommand {
 #[derive(FromArgs, PartialEq, Debug)]
 #[argh(subcommand)]
 pub enum SubCommandEnum {
-    Moniker(MonikerStruct),
     Capability(CapabilityStruct),
-}
-
-#[derive(FromArgs, PartialEq, Debug)]
-/// subcommand moniker
-#[argh(subcommand, name = "moniker")]
-pub struct MonikerStruct {
-    #[argh(positional)]
-    /// output services exposed/used/offered by the moniker
-    pub moniker: String,
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -60,7 +31,7 @@ pub struct MonikerStruct {
 #[argh(subcommand, name = "capability")]
 pub struct CapabilityStruct {
     #[argh(positional)]
-    /// output all components that expose the capability
+    /// output all components that expose/use the capability
     pub capability: String,
 }
 
@@ -68,21 +39,6 @@ pub struct CapabilityStruct {
 mod tests {
     use super::*;
     const CMD_NAME: &'static [&'static str] = &["select"];
-
-    #[test]
-    fn test_moniker_command() {
-        fn check(args: &[&str], expected_moniker: String) {
-            assert_eq!(
-                ComponentSelectCommand::from_args(CMD_NAME, args),
-                Ok(ComponentSelectCommand {
-                    nested: SubCommandEnum::Moniker(MonikerStruct { moniker: expected_moniker },)
-                })
-            )
-        }
-
-        check(&["moniker", "remote-control:expose:*"], "remote-control:expose:*".to_string());
-        check(&["moniker", "core/appmgr:out:*"], "core/appmgr:out:*".to_string());
-    }
 
     #[test]
     fn test_capability_command() {
--- src/developer/ffx/plugins/component/select/src/lib.rs
+++ src/developer/ffx/plugins/component/select/src/lib.rs
@@ -3,19 +3,14 @@
 // found in the LICENSE file.
 
 use {
-    anyhow::{Context, Result},
+    anyhow::Result,
     component_hub::{
         select::find_instances_that_expose_or_use_capability, select::MatchingInstances,
     },
-    errors::ffx_error,
-    ffx_component::{connect_to_realm_explorer, connect_to_realm_query, SELECTOR_FORMAT_HELP},
-    ffx_component_select_args::{
-        CapabilityStruct, ComponentSelectCommand, MonikerStruct, SubCommandEnum,
-    },
+    ffx_component::{connect_to_realm_explorer, connect_to_realm_query},
+    ffx_component_select_args::{CapabilityStruct, ComponentSelectCommand, SubCommandEnum},
     ffx_core::ffx_plugin,
     fidl_fuchsia_developer_remotecontrol as rc,
-    selectors::{self, VerboseError},
-    std::io::{stdout, Write},
 };
 
 #[ffx_plugin()]
@@ -23,13 +18,9 @@ pub async fn select_cmd(
     remote_proxy: rc::RemoteControlProxy,
     cmd: ComponentSelectCommand,
 ) -> Result<()> {
-    let writer = Box::new(stdout());
     match &cmd.nested {
         SubCommandEnum::Capability(CapabilityStruct { capability: c }) => {
             select_capability(remote_proxy, c).await
-        }
-        SubCommandEnum::Moniker(MonikerStruct { moniker: m }) => {
-            select_moniker(remote_proxy, writer, m).await
         }
     }
 }
@@ -59,194 +50,4 @@ async fn select_capability(rcs_proxy: rc::RemoteControlProxy, capability: &str) 
     }
 
     Ok(())
-}
-
-async fn select_moniker<W: Write>(
-    remote_proxy: rc::RemoteControlProxy,
-    mut write: W,
-    selector: &str,
-) -> Result<()> {
-    let writer = &mut write;
-    let selector = selectors::parse_selector::<VerboseError>(selector).map_err(|e| {
-        ffx_error!("Invalid selector '{}': {}\n{}", selector, e, SELECTOR_FORMAT_HELP)
-    })?;
-
-    match remote_proxy.select(selector).await.context("awaiting select call")? {
-        Ok(paths) => {
-            if paths.is_empty() {
-                writeln!(writer, "No matching paths.")?;
-                return Ok(());
-            }
-            format_matches(writer, paths)
-        }
-        Err(e) => {
-            eprintln!("Failed to execute selector: {:?}", e);
-            Ok(())
-        }
-    }
-}
-
-fn format_subdir<W: Write>(writer: &mut W, subdir: &str) -> Result<()> {
-    writeln!(writer, "|")?;
-    writeln!(writer, "--{}", subdir)?;
-    writeln!(writer, "   |")?;
-    Ok(())
-}
-
-fn format_service<W: Write>(writer: &mut W, service: &str) -> Result<()> {
-    writeln!(writer, "   --{}", service)?;
-    Ok(())
-}
-
-fn format_matches<W: Write>(writer: &mut W, matches: Vec<rc::ServiceMatch>) -> Result<()> {
-    let mut sorted_paths = matches.iter().collect::<Vec<&rc::ServiceMatch>>();
-    sorted_paths.sort();
-
-    let mut prev_opt: Option<&rc::ServiceMatch> = None;
-
-    for m in sorted_paths.iter() {
-        if let Some(prev) = prev_opt {
-            if prev.moniker == m.moniker {
-                if prev.subdir == m.subdir {
-                    format_service(writer, &m.service)?;
-                } else {
-                    format_subdir(writer, &m.subdir)?;
-                    format_service(writer, &m.service)?;
-                }
-                prev_opt = Some(m);
-                continue;
-            } else {
-                write!(writer, "\n")?;
-            }
-        }
-
-        writeln!(writer, "{}", m.moniker.join("/"))?;
-        format_subdir(writer, &m.subdir)?;
-        format_service(writer, &m.service)?;
-        prev_opt = Some(m);
-    }
-    Ok(())
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// tests
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    const DEFAULT_MATCH_STR: &str = "\
-core/test
-|
---out
-   |
-   --fuchsia.myservice\n";
-
-    fn setup_fake_remote_server() -> rc::RemoteControlProxy {
-        setup_fake_remote_proxy(|req| match req {
-            rc::RemoteControlRequest::Select { selector: _, responder } => {
-                let _ = responder
-                    .send(&mut Ok(vec![rc::ServiceMatch {
-                        moniker: vec![String::from("core"), String::from("test")],
-                        subdir: String::from("out"),
-                        service: String::from("fuchsia.myservice"),
-                    }]))
-                    .unwrap();
-            }
-            _ => assert!(false, "got unexpected request: {:?}", req),
-        })
-    }
-
-    #[fuchsia_async::run_singlethreaded(test)]
-    async fn test_select_invalid_selector() -> Result<()> {
-        let mut output = Vec::new();
-        let remote_proxy = setup_fake_remote_server();
-        let response = select_moniker(remote_proxy, &mut output, "a:b:").await;
-        let e = response.unwrap_err();
-        assert!(e.to_string().contains(SELECTOR_FORMAT_HELP));
-        Ok(())
-    }
-
-    #[fuchsia_async::run_singlethreaded(test)]
-    async fn test_select_formats_rcs_response() -> Result<()> {
-        let mut output = Vec::new();
-        let remote_proxy = setup_fake_remote_server();
-        let _response = select_moniker(remote_proxy, &mut output, "a:valid:selector")
-            .await
-            .expect("select should not fail");
-        let output = String::from_utf8(output).expect("Invalid UTF-8 bytes");
-        assert_eq!(output, DEFAULT_MATCH_STR);
-        Ok(())
-    }
-
-    #[test]
-    fn test_format_matches_complex() -> Result<()> {
-        let expected = "\
-core/test
-|
---in
-   |
-   --fuchsia.myservice
-   --fuchsia.myservice2
-|
---out
-   |
-   --fuchsia.myservice3
-
-test
-|
---in
-   |
-   --fuchsia.myservice4
-|
---out
-   |
-   --fuchsia.myservice5\n";
-
-        let mut output_utf8 = Vec::new();
-        format_matches(
-            &mut output_utf8,
-            vec![
-                rc::ServiceMatch {
-                    moniker: vec![String::from("core"), String::from("test")],
-                    subdir: String::from("out"),
-                    service: String::from("fuchsia.myservice3"),
-                },
-                rc::ServiceMatch {
-                    moniker: vec![String::from("core"), String::from("test")],
-                    subdir: String::from("in"),
-                    service: String::from("fuchsia.myservice"),
-                },
-                rc::ServiceMatch {
-                    moniker: vec![String::from("core"), String::from("test")],
-                    subdir: String::from("in"),
-                    service: String::from("fuchsia.myservice2"),
-                },
-                rc::ServiceMatch {
-                    moniker: vec![String::from("test")],
-                    subdir: String::from("out"),
-                    service: String::from("fuchsia.myservice5"),
-                },
-                rc::ServiceMatch {
-                    moniker: vec![String::from("test")],
-                    subdir: String::from("in"),
-                    service: String::from("fuchsia.myservice4"),
-                },
-            ],
-        )
-        .unwrap();
-
-        let output = String::from_utf8(output_utf8).expect("Invalid UTF-8 bytes");
-
-        // assert won't expand newlines, which makes failures hard to read.
-        if output != expected {
-            println!("actual output: ");
-            println!("{}", output);
-            println!("expected: ");
-            println!("{}", expected);
-            assert_eq!(output, expected);
-        }
-
-        Ok(())
-    }
 }
