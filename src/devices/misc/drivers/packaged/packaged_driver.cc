cbba83d7485f9f6360ad71e4c0e7ab5fda6a5be1
Novin Changizi <novinc@google.com>
2022-09-19 18:47:57 +0000

    [fdf] Migrate misc/drivers/packaged to DriverBase
    Bug: 107786
    Change-Id: If5517e84a8f285817e1d1d04f3be316c11aa3604
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/726493
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>

--- src/devices/misc/drivers/packaged/packaged_driver.cc
+++ src/devices/misc/drivers/packaged/packaged_driver.cc
@@ -3,11 +3,8 @@
 // found in the LICENSE file.
 
 #include <fidl/fuchsia.driver.framework/cpp/wire.h>
-#include <lib/driver2/namespace.h>
-#include <lib/driver2/record_cpp.h>
-#include <lib/driver2/structured_logger.h>
+#include <lib/driver2/driver2_cpp.h>
 #include <lib/inspect/component/cpp/component.h>
-#include <lib/sys/component/cpp/outgoing_directory.h>
 
 #include <optional>
 
@@ -19,49 +16,26 @@ namespace fio = fuchsia_io;
 
 namespace {
 
-class PackagedDriver {
+class PackagedDriver : public driver::DriverBase {
  public:
-  PackagedDriver(async_dispatcher_t* dispatcher, fidl::WireSharedClient<fdf::Node> node,
-                 driver::Namespace ns, driver::Logger logger)
-      : outgoing_(component::OutgoingDirectory::Create(dispatcher)),
-        node_(std::move(node)),
-        ns_(std::move(ns)),
-        logger_(std::move(logger)) {}
+  PackagedDriver(driver::DriverStartArgs start_args, fdf::UnownedDispatcher dispatcher)
+      : driver::DriverBase("packaged", std::move(start_args), std::move(dispatcher)) {}
 
-  static constexpr const char* Name() { return "packaged"; }
-
-  static zx::status<std::unique_ptr<PackagedDriver>> Start(fdf::wire::DriverStartArgs& start_args,
-                                                           fdf::UnownedDispatcher dispatcher,
-                                                           fidl::WireSharedClient<fdf::Node> node,
-                                                           driver::Namespace ns,
-                                                           driver::Logger logger) {
-    auto driver = std::make_unique<PackagedDriver>(dispatcher->async_dispatcher(), std::move(node),
-                                                   std::move(ns), std::move(logger));
-    auto result = driver->Run(dispatcher->async_dispatcher(), std::move(start_args.outgoing_dir()));
-    if (result.is_error()) {
-      return result.take_error();
-    }
-    return zx::ok(std::move(driver));
-  }
-
- private:
-  zx::status<> Run(async_dispatcher* dispatcher, fidl::ServerEnd<fio::Directory> outgoing_dir) {
-    exposed_inspector_.emplace(inspect::ComponentInspector(outgoing_, dispatcher));
+  zx::status<> Start() override {
+    exposed_inspector_.emplace(
+        inspect::ComponentInspector(context().outgoing()->component(), async_dispatcher()));
     auto& root = exposed_inspector_->root();
     root.RecordString("hello", "world");
 
     FDF_SLOG(DEBUG, "Debug world");
     FDF_SLOG(INFO, "Hello world", KV("The answer is", 42));
-    return outgoing_.Serve(std::move(outgoing_dir));
+    return zx::ok();
   }
 
-  component::OutgoingDirectory outgoing_;
-  fidl::WireSharedClient<fdf::Node> node_;
-  driver::Namespace ns_;
-  driver::Logger logger_;
+ private:
   std::optional<inspect::ComponentInspector> exposed_inspector_ = std::nullopt;
 };
 
 }  // namespace
 
-FUCHSIA_DRIVER_RECORD_CPP_V1(PackagedDriver);
+FUCHSIA_DRIVER_RECORD_CPP_V2(driver::Record<PackagedDriver>);
