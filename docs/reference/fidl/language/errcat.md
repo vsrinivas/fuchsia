e01b933baa16d34d200c440a7a40541f9f9957a1
Alex Zaslavsky <azaslavsky@google.com>
2022-09-20 20:33:20 +0000

    [fidl][docs] Add conflicting modifiers error
    Test: fx test host_x64/fidlc-test
    Bug: 108248
    Change-Id: Ibadbbff033828697798a994706a416a9b440fb92
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/725802
    Commit-Queue: Alex Zaslavsky <azaslavsky@google.com>
    Reviewed-by: Kelly-Ann Dolor <kadolor@google.com>

--- docs/reference/fidl/language/errcat.md
+++ docs/reference/fidl/language/errcat.md
@@ -6,6 +6,8 @@ This document lists all errors emitted by the [FIDL compiler][docs-fidlc],
 
 <<error-catalog/_fi-0018.md>>
 
+<<error-catalog/_fi-0033.md>>
+
 <<error-catalog/_fi-0040.md>>
 
 <<error-catalog/_fi-0057.md>>
--- tools/fidl/fidlc/tests/strictness_tests.cc
+++ tools/fidl/fidlc/tests/strictness_tests.cc
@@ -37,18 +37,15 @@ type Three = strict strict strict union { 1: b bool; }; // line 6
 }
 
 TEST(StrictnessTests, BadConflictingModifiers) {
-  TestLibrary library(R"FIDL(
-library example;
+  TestLibrary library;
+  library.AddFile("bad/conflicting_modifiers.test.fidl");
 
-type SF = strict flexible union { 1: b bool; }; // line 4
-type FS = flexible strict union { 1: b bool; }; // line 5
-)FIDL");
   ASSERT_ERRORED_TWICE_DURING_COMPILE(library, fidl::ErrConflictingModifier,
                                       fidl::ErrConflictingModifier);
-  EXPECT_EQ(library.errors()[0]->span.position().line, 4);
+  EXPECT_EQ(library.errors()[0]->span.position().line, 6);
   ASSERT_SUBSTR(library.errors()[0]->msg.c_str(), "strict");
   ASSERT_SUBSTR(library.errors()[0]->msg.c_str(), "flexible");
-  EXPECT_EQ(library.errors()[1]->span.position().line, 5);
+  EXPECT_EQ(library.errors()[1]->span.position().line, 10);
   ASSERT_SUBSTR(library.errors()[1]->msg.c_str(), "strict");
   ASSERT_SUBSTR(library.errors()[1]->msg.c_str(), "flexible");
 }
@@ -138,24 +135,12 @@ type StrictFoo = strict table {};
 }
 
 TEST(StrictnessTests, GoodUnionStrictness) {
-  TestLibrary library(R"FIDL(library example;
+  TestLibrary library;
+  library.AddFile("good/simple_strictness.test.fidl");
 
-type Foo = strict union {
-    1: i int32;
-};
-
-type FlexibleFoo = flexible union {
-    1: i int32;
-};
-
-type StrictFoo = strict union {
-    1: i int32;
-};
-)FIDL");
   ASSERT_COMPILED(library);
-  EXPECT_EQ(library.LookupUnion("Foo")->strictness, fidl::types::Strictness::kStrict);
   EXPECT_EQ(library.LookupUnion("FlexibleFoo")->strictness, fidl::types::Strictness::kFlexible);
-  EXPECT_EQ(library.LookupUnion("StrictFoo")->strictness, fidl::types::Strictness::kStrict);
+  EXPECT_EQ(library.LookupUnion("StrictBar")->strictness, fidl::types::Strictness::kStrict);
 }
 
 TEST(StrictnessTests, GoodStrictUnionRedundant) {
