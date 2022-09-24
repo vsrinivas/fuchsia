8bc804988e97eeda7ad404c34b1793a5d002e6eb
Louis Yung-Chieh Lo <yjlou@google.com>
2022-09-24 01:41:57 +0000

    [iwlwifi] add types for porting
    Change-Id: Ib6e68f1b3407d96da15ac140fd8805c641e28965
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/729745
    Commit-Queue: Louis Lo <yjlou@google.com>
    Reviewed-by: John Tan <johntan@google.com>

--- src/connectivity/wlan/drivers/third_party/intel/iwlwifi/platform/compiler.h
+++ src/connectivity/wlan/drivers/third_party/intel/iwlwifi/platform/compiler.h
@@ -16,13 +16,20 @@
 
 #include "src/connectivity/wlan/drivers/third_party/intel/iwlwifi/platform/debug.h"
 
+typedef uint64_t __be64;
 typedef uint32_t __be32;
 typedef uint16_t __be16;
 typedef uint64_t __le64;
 typedef uint32_t __le32;
 typedef uint16_t __le16;
 typedef int8_t __s8;
+typedef int16_t __s16;
+typedef int32_t __s32;
+typedef int64_t __s64;
 typedef uint8_t __u8;
+typedef uint16_t __u16;
+typedef uint32_t __u32;
+typedef uint64_t __u64;
 
 #define U08 uint8_t
 #define U16 uint16_t
