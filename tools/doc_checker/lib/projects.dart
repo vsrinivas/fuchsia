f353b5794968580bba69d6bb4381f33b006f50ff
Nathan Mulcahey <nmulcahey@google.com>
2022-09-26 16:43:40 +0000

    [atom] Remove references to `atom-language-fidl`
    The editor is no longer supported by Github [1] and it doesn't sound like anyone on Fuchsia is supporting the integration [2].
    
    [1] https://github.blog/2022-06-08-sunsetting-atom/
    [2] http://b/244230745
    
    Bug: b/244230745
    Change-Id: I3865b853bbae1e947510cdeea8d0d1577dea6d99
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/730042
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    Commit-Queue: Nathan Mulcahey <nmulcahey@google.com>

--- tools/doc_checker/lib/projects.dart
+++ tools/doc_checker/lib/projects.dart
@@ -8,7 +8,6 @@
 /// List of active repos under fuchsia.googlesource.com which can be linked to.
 const List<String> validProjects = <String>[
   '', // root page of all projects
-  'atom-language-fidl',
   'cobalt',
   'drivers', // This is a family of projects.
   'experiences',
