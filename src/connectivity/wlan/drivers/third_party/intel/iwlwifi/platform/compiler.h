48495132d1fb00ea07afce898602fd85e71f8371
Louis Yung-Chieh Lo <yjlou@google.com>
2022-09-27 16:30:31 +0000

    [iwlwifi] add few more types for porting
    Change-Id: Ic09bd41cb7e561690dec01b7076dbd93d2fdf4fe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/730904
    Reviewed-by: John Tan <johntan@google.com>
    Commit-Queue: Louis Lo <yjlou@google.com>

--- src/connectivity/wlan/drivers/third_party/intel/iwlwifi/platform/compiler.h
+++ src/connectivity/wlan/drivers/third_party/intel/iwlwifi/platform/compiler.h
@@ -22,19 +22,28 @@ typedef uint16_t __be16;
 typedef uint64_t __le64;
 typedef uint32_t __le32;
 typedef uint16_t __le16;
+typedef int8_t s8;
 typedef int8_t __s8;
+typedef int16_t s16;
 typedef int16_t __s16;
+typedef int32_t s32;
 typedef int32_t __s32;
+typedef int64_t s64;
 typedef int64_t __s64;
 typedef uint8_t __u8;
+typedef uint8_t U08;
+typedef uint8_t u8;
+typedef uint8_t __u8;
+typedef uint16_t U16;
+typedef uint16_t u16;
 typedef uint16_t __u16;
+typedef uint32_t U32;
+typedef uint32_t u32;
 typedef uint32_t __u32;
+typedef uint64_t U64;
+typedef uint64_t u64;
 typedef uint64_t __u64;
 
-#define U08 uint8_t
-#define U16 uint16_t
-#define U32 uint32_t
-#define U64 uint64_t
 #define INLINE inline
 
 #if defined(__cplusplus)
