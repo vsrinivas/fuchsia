aad31933d3dafa9d6e6c1e063d5ead4f1a8fd8a7
Cameron Dale <camrdale@google.com>
2022-09-28 21:00:41 +0000

    [cobalt] Remove test metric constants for Cobalt 1.0.
    I need to remove the reference to the metric constants, so I can
    delete the metrics from the registry. But I also cleaned up the
    constants that are now unused.
    
    Bug: b/247019278
    Change-Id: I107bb05dc2fc5f930ad17aa981a0d86533714722
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/732066
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Cameron Dale <camrdale@google.com>
    Reviewed-by: Zach Bush <zmbush@google.com>

--- src/cobalt/bin/testapp/test_constants.h
+++ src/cobalt/bin/testapp/test_constants.h
@@ -11,56 +11,9 @@
 
 namespace cobalt::testapp {
 
-// error_occurred metric constants.
-const uint32_t kErrorOccurredIndicesToUse[] = {0, 1, 2, 9};
-const uint32_t kErrorOccurredInvalidIndex = 18;
-
 // Common metric constants.
 const std::string kApplicationComponentNames[] = {"audio_core", "logger", "scheduler", "scenic",
                                                   "unknown"};
-
-// file_system_cache_misses metric constants.
-const uint32_t kFileSystemCacheMissesIndices[] = {0, 1};
-const std::string kFileSystemCacheMissesComponentNames[] = {"blobfs", "minfs", "", "unknown"};
-const int64_t kFileSystemCacheMissesCountMax = 2;
-
-// update_duration metric constants.
-const int32_t kUpdateDurationIndices[] = {0, 1, 2};
-const std::string kUpdateDurationComponentNames[] = {"DownloadPayload", "VerifyUpdate", "",
-                                                     "unknown"};
-const int64_t kUpdateDurationValues[] = {-1, 0, 1, 2, 10, 37, 158, 702};
-
-// game_frame_rate metric constants.
-const int32_t kGameFrameRateIndices[] = {0, 1};
-const std::string kGameFrameRateComponentNames[] = {"Forest", "City", "", "unknown"};
-const float kGameFrameRateValues[] = {0.50, 1.23, 7.999, 8.0, 64.003, 415.235, 600.001};
-
-// application_memory metric constants
-const int32_t kApplicationMemoryIndices[] = {0, 1, 2};
-const int64_t kApplicationMemoryValues[] = {0, 1000, 4000, 16000, 128000, 512000};
-
-// power_usage metric constants.
-const int32_t kPowerUsageIndices[] = {0, 1};
-const int32_t kPowerUsageBuckets = 52;
-
-// bandwidth_usage metric constants.
-const int32_t kBandwidthUsageIndices[] = {0, 1};
-const int64_t kBandwidthUsageBuckets = 22;
-
-// features_active metric constants.
-const int32_t kFeaturesActiveIndices[] = {0, 1, 2, 3, 9};
-const int32_t kFeaturesActiveInvalidIndex = 20;
-
-// connection_attempts metric constants.
-const int32_t kConnectionAttemptsIndices[] = {0, 1};
-const std::string kConnectionAttemptsComponentNames[] = {"HostA", "HostB", "HostC"};
-const int kConnectionAttemptsNumWindowSizes = 2;
-
-// streaming_time metric constants.
-const int32_t kStreamingTimeIndices[] = {0, 1, 2};
-const std::string kStreamingTimeComponentNames[] = {"ModuleA", "ModuleB", "ModuleC"};
-const int kStreamingTimeNumWindowSizes = 2;
-
 // update_duration_new metric constants.
 const uint32_t kUpdateDurationNewErrorNameIndices[] = {0, 1, 2};
 const uint32_t kUpdateDurationNewStageIndices[] = {0, 1};
@@ -106,35 +59,6 @@ const uint32_t kErrorOccurredNewCounts[] = {0, 1, 100, 1000};
 
 // error_occurred_components metric constants.
 const uint32_t kErrorOccurredComponentsStatusIndices[] = {0, 1, 2};
-
-// The number of locally aggregated observations that should be generated for
-// each locally aggregated report in the test_app2 project for a day, assuming
-// that no events were logged for locally aggregated reports on that day.
-//
-// These numbers are calculated as follows:
-//
-// features_active_unique_devices: 20 UniqueActivesObservations
-//                  (10 event codes * 2 window sizes)
-// connection_attempts_per_device_count: 1 ReportParticipationObservation
-// connection_attempts_per_device_histogram: 1 ReportParticipationObservation
-// streaming_time_per_device_total: 1 ReportParticipationObservation
-// streaming_time_per_device_histogram: 1 ReportParticipationObservation
-std::map<std::pair<uint32_t, uint32_t>, uint64_t> kNumAggregatedObservations = {
-    {{cobalt_registry::kFeaturesActiveMetricId,
-      cobalt_registry::kFeaturesActiveFeaturesActiveUniqueDevicesReportId},
-     20},
-    {{cobalt_registry::kConnectionAttemptsMetricId,
-      cobalt_registry::kConnectionAttemptsConnectionAttemptsPerDeviceCountReportId},
-     1},
-    {{cobalt_registry::kConnectionAttemptsMetricId,
-      cobalt_registry::kConnectionAttemptsConnectionAttemptsPerDeviceHistogramReportId},
-     1},
-    {{cobalt_registry::kStreamingTimeMetricId,
-      cobalt_registry::kStreamingTimeStreamingTimePerDeviceTotalReportId},
-     1},
-    {{cobalt_registry::kStreamingTimeMetricId,
-      cobalt_registry::kStreamingTimeStreamingTimePerDeviceHistogramReportId},
-     1}};
 
 }  // namespace cobalt::testapp
 
