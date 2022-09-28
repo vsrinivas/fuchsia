4297c9a92c325443dc65ebf7fb8056b395ca0b68
Ani Ramakrishnan <aniramakri@google.com>
2022-09-28 22:27:07 +0000

    [bt][fastpair] Define Fast Pair as a static child of bt-init
    The Fast Pair Provider component will not exist on all products. It
    both uses and exposes the `sys.Pairing` capability.
    
    Define the Fast Pair Provider component as a static child of `bt-init`
    and add the relevant capability routes.
    
    Update bt-init to conditionally route `sys.Pairing` requests to Fast
    Pair if the component exists. This logic is identical to that used in
    bt-rfcomm & `bredr.Profile`.
    
    Downgrade the log message emitted when the child component cannot be
    resolved. This will likely occur fairly often when RFCOMM/FastPair
    functionality is not supported on the device.
    
    Update integration tests that define a test bt-init realm to point to
    the Fast Pair Provider component. However, because Fast Pair
    functionality is not relevant to this tests, omit it from the test
    package so that the URL doesn't resolve. Any URL resolving warnings can
    be safely ignored.
    
    Bug: 108241
    Test: fx test bt-init-smoke-test
    Test: fx test bt-access-integration-tests (Any integration test that
    relies on the bt-test-harness will exercise this new change)
    Test: fx test bt-init-unittests
    
    Change-Id: I6aba1f06c9231598c25dc109603ff2b790ee203e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/718322
    Commit-Queue: Ani Ramakrishnan <aniramakri@google.com>
    Fuchsia-Auto-Submit: Ani Ramakrishnan <aniramakri@google.com>
    Reviewed-by: Marie Janssen <jamuraa@google.com>

--- src/connectivity/bluetooth/core/bt-init/src/main.rs
+++ src/connectivity/bluetooth/core/bt-init/src/main.rs
@@ -2,23 +2,24 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use {
-    anyhow::{Context as _, Error},
-    async_trait::async_trait,
-    fdio,
-    fidl::endpoints::{DiscoverableProtocolMarker, Proxy},
-    fidl_fuchsia_bluetooth_bredr::ProfileMarker,
-    fidl_fuchsia_bluetooth_snoop::SnoopMarker,
-    fidl_fuchsia_io as fio, fuchsia_async as fasync,
-    fuchsia_component::{client, server},
-    futures::{future, StreamExt},
-    tracing::{error, info, warn},
-};
+use anyhow::{Context as _, Error};
+use async_trait::async_trait;
+use fdio;
+use fidl::endpoints::{DiscoverableProtocolMarker, Proxy};
+use fidl_fuchsia_bluetooth_bredr::ProfileMarker;
+use fidl_fuchsia_bluetooth_snoop::SnoopMarker;
+use fidl_fuchsia_bluetooth_sys::PairingMarker;
+use fidl_fuchsia_io as fio;
+use fuchsia_async as fasync;
+use fuchsia_component::{client, server};
+use futures::{future, StreamExt};
+use tracing::{error, info, warn};
 
 mod config;
 
 const BT_GAP_CHILD_NAME: &str = "bt-gap";
 const BT_RFCOMM_CHILD_NAME: &str = "bt-rfcomm";
+const BT_FASTPAIR_PROVIDER_CHILD_NAME: &str = "bt-fastpair-provider";
 
 #[async_trait]
 trait ComponentClientAdapter {
@@ -40,28 +41,29 @@ impl ComponentClientAdapter for ComponentClient {
     }
 }
 
-/// Open the directory of the child which will underlie the Profile service. bt-rfcomm is the
-/// preferred Profile service provider if present, but if it is not we fall back to bt-gap.
+/// Open the directory of the child which will underlie any services. The specified child is the
+/// preferred service provider if present, but if unavailable, fall back to `bt-gap`.
 //
-// TODO(fxbug.dev/71315): A single bt-rfcomm instance won't function correctly in the presence
+// TODO(fxbug.dev/71315): A single child instance won't function correctly in the presence
 // of multiple bt-host devices during its lifetime. When handling this is a priority, we will
-// likely need to either launch an instance of bt-rfcomm per-bt-host (e.g. inside bt-gap), or
-// modify bt-rfcomm to accommodate this issue.
+// likely need to either launch an instance of the child per-bt-host (e.g. inside bt-gap), or
+// modify the child component to accommodate this issue.
 async fn open_childs_service_directory<C: ComponentClientAdapter>(
+    child_name: &str,
     component_client: &mut C,
 ) -> Result<fio::DirectoryProxy, Error> {
-    let underlying_profile_svc =
-        component_client.open_childs_exposed_directory(BT_RFCOMM_CHILD_NAME.to_owned()).await;
-    match underlying_profile_svc {
+    let underlying_svc =
+        component_client.open_childs_exposed_directory(child_name.to_owned()).await;
+    match underlying_svc {
         Err(e) => {
-            warn!(
-                "failed to bind bt-rfcomm child ({:?}), falling back to bt-gap's Profile service",
-                e
+            info!(
+                "failed to bind child {}: {:?}, falling back to bt-gap's service directory",
+                child_name, e,
             );
             component_client.open_childs_exposed_directory(BT_GAP_CHILD_NAME.to_owned()).await
         }
         dir => {
-            info!("successfully opened bt-rfcomm svc directory");
+            info!("successfully opened `{}` svc directory", child_name);
             dir
         }
     }
@@ -87,26 +89,48 @@ fn main() -> Result<(), Error> {
     }
 
     let run_bluetooth = async move {
-        let underlying_profile_svc = open_childs_service_directory(&mut ComponentClient).await?;
+        // Get the backing service directory of the `bt-rfcomm` and `bt-fastpair-provider` child
+        // components.
+        let underlying_profile_svc =
+            open_childs_service_directory::<_>(BT_RFCOMM_CHILD_NAME, &mut ComponentClient).await?;
+        let underlying_pairing_svc = open_childs_service_directory::<_>(
+            BT_FASTPAIR_PROVIDER_CHILD_NAME,
+            &mut ComponentClient,
+        )
+        .await?;
+        // Expose the `bredr.Profile` and `sys.Pairing` protocols to the system.
         let mut fs = server::ServiceFs::new();
-        let _ = fs.dir("svc").add_service_at(ProfileMarker::PROTOCOL_NAME, |chan| {
-            Some((ProfileMarker::PROTOCOL_NAME, chan))
-        });
+        let _ = fs
+            .dir("svc")
+            .add_service_at(ProfileMarker::PROTOCOL_NAME, |chan| {
+                Some((ProfileMarker::PROTOCOL_NAME, chan))
+            })
+            .add_service_at(PairingMarker::PROTOCOL_NAME, |chan| {
+                Some((PairingMarker::PROTOCOL_NAME, chan))
+            });
         let _ = fs.take_and_serve_directory_handle()?;
 
-        info!("initialization complete, begin serving {}", ProfileMarker::PROTOCOL_NAME);
+        info!(
+            "initialization complete, begin serving [{}, {}]",
+            ProfileMarker::PROTOCOL_NAME,
+            PairingMarker::PROTOCOL_NAME
+        );
         let outer_fs = fs.for_each(move |(name, chan)| {
-            if name != ProfileMarker::PROTOCOL_NAME {
-                error!(
-                    "Received unexpected service {} when we only expect to serve {}",
-                    name,
-                    ProfileMarker::PROTOCOL_NAME
-                );
-                return future::ready(());
-            }
-            let _ =
-                fdio::service_connect_at(underlying_profile_svc.as_channel().as_ref(), name, chan)
-                    .map_err(|e| warn!("error passing {} handle to service: {:?}", name, e));
+            let directory = match name {
+                ProfileMarker::PROTOCOL_NAME => underlying_profile_svc.as_channel().as_ref(),
+                PairingMarker::PROTOCOL_NAME => underlying_pairing_svc.as_channel().as_ref(),
+                name => {
+                    error!(
+                        "Received unexpected service {} when we only expect to serve [{}, {}]",
+                        name,
+                        ProfileMarker::PROTOCOL_NAME,
+                        PairingMarker::PROTOCOL_NAME,
+                    );
+                    return future::ready(());
+                }
+            };
+            let _ = fdio::service_connect_at(directory, name, chan)
+                .map_err(|e| warn!("error passing {} handle to service: {:?}", name, e));
             future::ready(())
         });
         Ok::<(), Error>(outer_fs.await)
@@ -128,6 +152,7 @@ mod tests {
         pub children_to_fail_for: HashSet<String>,
         pub bt_gap_channel: Option<zx::Channel>,
         pub bt_rfcomm_channel: Option<zx::Channel>,
+        pub bt_fastpair_provider_channel: Option<zx::Channel>,
     }
 
     impl MockComponentClient {
@@ -136,7 +161,12 @@ mod tests {
                 children_to_fail_for: HashSet::new(),
                 bt_gap_channel: None,
                 bt_rfcomm_channel: None,
+                bt_fastpair_provider_channel: None,
             }
+        }
+
+        fn child_channels_empty(&self) -> bool {
+            self.bt_rfcomm_channel.is_none() && self.bt_fastpair_provider_channel.is_none()
         }
     }
 
@@ -147,12 +177,13 @@ mod tests {
             child_name: String,
         ) -> Result<fio::DirectoryProxy, Error> {
             if self.children_to_fail_for.contains(&child_name) {
-                return Err(format_err!("couldn't open {}'s directory :/", &child_name));
+                return Err(format_err!("couldn't open {}'s directory", &child_name));
             }
             let (local, client) = zx::Channel::create()?;
             match child_name.as_str() {
                 BT_RFCOMM_CHILD_NAME => self.bt_rfcomm_channel = Some(local),
                 BT_GAP_CHILD_NAME => self.bt_gap_channel = Some(local),
+                BT_FASTPAIR_PROVIDER_CHILD_NAME => self.bt_fastpair_provider_channel = Some(local),
                 _ => panic!("MockComponentClient received unexpected child name: {}", child_name),
             }
             Ok(fio::DirectoryProxy::from_channel(AsyncChannel::from_channel(client).unwrap()))
@@ -168,38 +199,69 @@ mod tests {
     }
 
     #[fuchsia::test]
+    async fn test_open_fastpair_provider_works() {
+        let mut mock_client = MockComponentClient::new();
+
+        // Directory should be connected to `bt-fastpair-provider`.
+        let child_directory =
+            open_childs_service_directory(BT_FASTPAIR_PROVIDER_CHILD_NAME, &mut mock_client)
+                .await
+                .unwrap();
+        assert!(mock_client.bt_fastpair_provider_channel.is_some());
+        assert_channels_connected(
+            mock_client.bt_fastpair_provider_channel.unwrap().as_ref(),
+            child_directory.as_channel().as_ref(),
+        );
+        // `bt-gap`s directory should not be used.
+        assert!(mock_client.bt_gap_channel.is_none());
+    }
+
+    #[fuchsia::test]
     async fn test_open_rfcomm_works() {
         let mut mock_client = MockComponentClient::new();
 
         // If opening bt-rfcomm's directory works, the directory should be connected to bt-rfcomm.
-        let profile_svc = open_childs_service_directory(&mut mock_client).await.unwrap();
+        let profile_svc =
+            open_childs_service_directory(BT_RFCOMM_CHILD_NAME, &mut mock_client).await.unwrap();
         assert!(mock_client.bt_rfcomm_channel.is_some());
         assert_channels_connected(
             mock_client.bt_rfcomm_channel.unwrap().as_ref(),
             profile_svc.as_channel().as_ref(),
         );
+        // `bt-gap`s directory should not be used.
+        assert!(mock_client.bt_gap_channel.is_none());
     }
 
     #[fuchsia::test]
-    async fn test_open_rfcomm_fails() {
-        // If opening bt-rfcomm's directory fails, the directory should be connected to bt-gap.
-        let mut mock_client = MockComponentClient::new();
-        let _ = mock_client.children_to_fail_for.insert(BT_RFCOMM_CHILD_NAME.to_owned());
-        let profile_svc = open_childs_service_directory(&mut mock_client).await.unwrap();
-        assert!(mock_client.bt_rfcomm_channel.is_none());
-        assert!(mock_client.bt_gap_channel.is_some());
-        assert_channels_connected(
-            mock_client.bt_gap_channel.unwrap().as_ref(),
-            profile_svc.as_channel().as_ref(),
-        );
+    async fn test_open_child_fails() {
+        // If opening the child directory fails, the directory should be connected to bt-gap.
+        let children = [BT_RFCOMM_CHILD_NAME, BT_FASTPAIR_PROVIDER_CHILD_NAME];
+        for child in children {
+            let mut mock_client = MockComponentClient::new();
+            let _ = mock_client.children_to_fail_for.insert(child.to_owned());
+            let child_directory =
+                open_childs_service_directory(child, &mut mock_client).await.unwrap();
+            assert!(mock_client.child_channels_empty());
+            assert!(mock_client.bt_gap_channel.is_some());
+            assert_channels_connected(
+                mock_client.bt_gap_channel.unwrap().as_ref(),
+                child_directory.as_channel().as_ref(),
+            );
+        }
     }
 
     #[fuchsia::test]
-    async fn test_open_rfcomm_and_gap_fail() {
-        // If opening both bt-gap and bt-rfcomm's directory fail, opening the profile service should fail.
+    async fn test_open_child_and_gap_fail() {
+        // If opening both bt-gap and child's directory fail, opening the service should fail.
         let mut mock_client = MockComponentClient::new();
         let _ = mock_client.children_to_fail_for.insert(BT_RFCOMM_CHILD_NAME.to_owned());
         let _ = mock_client.children_to_fail_for.insert(BT_GAP_CHILD_NAME.to_owned());
-        assert!(open_childs_service_directory(&mut mock_client).await.is_err());
+        let _ = mock_client.children_to_fail_for.insert(BT_FASTPAIR_PROVIDER_CHILD_NAME.to_owned());
+        assert!(open_childs_service_directory(BT_RFCOMM_CHILD_NAME, &mut mock_client)
+            .await
+            .is_err());
+        assert!(open_childs_service_directory(BT_FASTPAIR_PROVIDER_CHILD_NAME, &mut mock_client)
+            .await
+            .is_err());
     }
 }
