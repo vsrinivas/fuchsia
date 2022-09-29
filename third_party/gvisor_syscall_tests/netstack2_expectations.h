ed7a76d792587c8f482a4daaf02fdaf2a4b8b52e
Nick Brown <nickbrow@google.com>
2022-09-29 21:25:52 +0000

    [gvisor_syscall_tests] Add copyrights
    Bug: 106971
    Change-Id: I47773952c1b34b58e0c726eb6e9669472fe6fd43
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/732984
    Reviewed-by: Evan Wildenhain <wildenhain@google.com>
    Commit-Queue: Nick Brown <nickbrow@google.com>

--- third_party/gvisor_syscall_tests/netstack2_expectations.h
+++ third_party/gvisor_syscall_tests/netstack2_expectations.h
@@ -1,3 +1,7 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef THIRD_PARTY_GVISOR_SYSCALL_TESTS_NETSTACK2_EXPECTATIONS_H_
 #define THIRD_PARTY_GVISOR_SYSCALL_TESTS_NETSTACK2_EXPECTATIONS_H_
 
--- third_party/gvisor_syscall_tests/netstack2_with_fast_udp_expectations.cc
+++ third_party/gvisor_syscall_tests/netstack2_with_fast_udp_expectations.cc
@@ -1,3 +1,7 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "expectations.h"
 #include "netstack2_expectations.h"
 
