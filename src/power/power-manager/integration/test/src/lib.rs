754616ce8fb277dee6317aebb6bb237d83aabb7f
Patrick Shickel <pshickel@google.com>
2022-09-29 21:34:51 +0000

    [power][test] Add shutdown integration test
    Adds an integration test to verify correct Power Manager behavior for
    shutdown request handling and reboot reason dissemination.
    
    Bug: 104266
    Multiply: //src/power/power-manager/integration
    Change-Id: I77ddefaa00e1d33d69e9e116a5a1d04a546cc91b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/728465
    Fuchsia-Auto-Submit: Patrick Shickel <pshickel@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Didi She <didis@google.com>

--- src/power/power-manager/integration/test/src/lib.rs
+++ src/power/power-manager/integration/test/src/lib.rs
@@ -2,7 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use power_manager_integration_test_lib::{client_connectors::ThermalClient, TestEnvBuilder};
+use {
+    fidl_fuchsia_hardware_power_statecontrol as fpower, fuchsia_async as fasync,
+    power_manager_integration_test_lib::{
+        client_connectors::{RebootWatcherClient, ThermalClient},
+        TestEnvBuilder,
+    },
+};
 
 /// Integration test for Power Manager to verify correct behavior of the thermal client service.
 #[fuchsia::test]
@@ -33,6 +39,42 @@ async fn thermal_client_service_test() {
 
     // Verify client0 thermal state goes back to 0
     assert_eq!(client0.get_thermal_state().await.unwrap(), 0);
+
+    env.destroy().await;
+}
+
+/// Verifies that shutdown/reboot requests sent to the Power Manager are handled as expected:
+///     1) forward the reboot reason to connected RebootWatcher clients
+///     2) update Driver Manager with the appropriate termination SystemPowerState
+///     3) send a shutdown request to the system controller
+#[fuchsia::test]
+async fn shutdown_test() {
+    let mut env = TestEnvBuilder::new()
+        .power_manager_node_config_path(&"/pkg/shutdown_test/node_config.json5")
+        .build()
+        .await;
+
+    let mut reboot_watcher = RebootWatcherClient::new(&env).await;
+
+    // Send a reboot request to the Power Manager (in a separate Task because it never returns)
+    let shutdown_client = env.connect_to_protocol::<fpower::AdminMarker>();
+    let _shutdown_task =
+        fasync::Task::local(shutdown_client.reboot(fpower::RebootReason::SystemUpdate));
+
+    // Verify Power Manager forwards the reboot reason to the watcher client
+    assert_eq!(reboot_watcher.get_reboot_reason().await, fpower::RebootReason::SystemUpdate);
+
+    // Verify the system controller service gets the shutdown request from Power Manager
+    env.mocks.system_controller_service.wait_for_shutdown_request().await;
+
+    // Verify the Driver Manager received the correct termination state request from Power Manager.
+    // The termination state should only be checked after we know the Power Manager has sent the
+    // request to Driver Manager. In this case, we know that to be true once the previous
+    // `wait_for_shutdown_request` completes.
+    assert_eq!(
+        env.mocks.driver_manager.get_current_termination_state(),
+        fpower::SystemPowerState::Reboot
+    );
 
     env.destroy().await;
 }
