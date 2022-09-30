a4614f38a80823968fa9f986fab59b74e029c83e
Gulfem Savrun Yeniceri <gulfem@google.com>
2022-09-30 17:06:38 +0000

    [test] Extend coverage test for host
    Bug: 98805
    Change-Id: I72b37d9086675e914356ee8c15c3857d232d4750
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/728388
    Reviewed-by: Petr Hosek <phosek@google.com>
    Commit-Queue: Gulfem Savrun Yeniceri <gulfem@google.com>
    Reviewed-by: Ina Huh <ihuh@google.com>

--- src/tests/coverage/launcher_test.go
+++ src/tests/coverage/launcher_test.go
@@ -35,104 +35,14 @@ var (
 	coverageTestBinary = flag.String("coverage-test-binary", "", "Path to instrumented coverage test binary")
 	coverageTestName   = flag.String("coverage-test-name", "", "Name of coverage test")
 	goldenCoverageFile = flag.String("golden-coverage", "", "Path to golden coverage file")
-	llvmProfData       = flag.String("llvm-profdata", "", "Path to version of llvm-profdata tool")
+	host               = flag.Bool("host", false, "If set, run coverage test on host")
 	llvmCov            = flag.String("llvm-cov", "", "Path to llvm-cov tool")
+	llvmProfData       = flag.String("llvm-profdata", "", "Path to version of llvm-profdata tool")
 )
 
 func TestCoverage(t *testing.T) {
-	ctx := context.Background()
-	var addr net.IPAddr
-	if deviceAddr, ok := os.LookupEnv(constants.DeviceAddrEnvKey); ok {
-		addrPtr, err := net.ResolveIPAddr("ip", deviceAddr)
-		if err != nil {
-			t.Fatalf("failed to parse device address: %s", deviceAddr)
-		}
-		addr = *addrPtr
-	}
-
-	// Read SSH key which is required to run a test.
-	sshKeyFile := os.Getenv(constants.SSHKeyEnvKey)
 	testOutDir := t.TempDir()
-	// Create a new fuchsia tester that is responsible for executing the test.
-	// This is a v2 test, and it uses run-test-suite instead of runtests, so runtests=false.
-	// TODO(fxbug.dev/77634): When we start treating profiles as artifacts, start using ffx
-	// with testrunner.NewFFXTester().
-	tester, err := testrunner.NewFuchsiaSSHTester(ctx, addr, sshKeyFile, testOutDir, "", false)
-	if err != nil {
-		t.Fatalf("failed to initialize fuchsia tester: %s", err)
-	}
-	defer tester.Close()
-
-	test := testsharder.Test{
-		Test: build.Test{
-			Name:       *coverageTestName,
-			PackageURL: *coverageTestName,
-		},
-		RunAlgorithm: testsharder.StopOnFailure,
-		Runs:         1,
-	}
-	// Run the test over SSH.
-	result, err := tester.Test(ctx, test, os.Stdout, os.Stdout, "unused-out-dir")
-	if err != nil {
-		t.Fatalf("failed to run the test: %s", err)
-	}
-
-	// Create a test outputs object, responsible for producing TAP output,
-	// and recording data sinks.
-	outputs, err := testrunner.CreateTestOutputs(tap.NewProducer(io.Discard), testOutDir)
-	if err != nil {
-		t.Fatalf("failed to create test outputs: %s", err)
-	}
-
-	// Record data sinks.
-	if err := outputs.Record(ctx, *result); err != nil {
-		t.Fatalf("failed to record data sinks: %s", err)
-	}
-
-	// Copy profiles to the host. There might be a delay between when the test finishes and
-	// data sinks including profiles are available on the target to copy. When that's the case,
-	// EnsureSinks() does not return an error, and it logs the message.
-	// Therefore, check whether v2 sinks directory exists to ensure copying is successful.
-	// When there is a delay, retry.
-	// TODO(fxbug.dev/77634): When we start treating profiles as artifacts, remove retry.
-	var sinks []runtests.DataSinkReference
-	err = retry.Retry(ctx, retry.NewConstantBackoff(5*time.Second), func() error {
-		if err := tester.EnsureSinks(ctx, sinks, outputs); err != nil {
-			return err
-		}
-		v2SinksDir := filepath.Join(testOutDir, "v2")
-		_, err := os.ReadDir(v2SinksDir)
-		if err != nil {
-			return err
-		}
-		return nil
-	}, nil)
-	if err != nil {
-		t.Fatalf("failed to collect data sinks: %s", err)
-	}
-
-	// Close recording of test outputs.
-	if err := outputs.Close(); err != nil {
-		t.Fatalf("failed to save test outputs: %s", err)
-	}
-
-	// Find the raw profile that corresponds to the given coverage test name.
-	rawProfile := ""
-	if len(outputs.Summary.Tests) != 1 {
-		t.Fatalf("failed to find the test in the outputs")
-	}
-	outputTest := outputs.Summary.Tests[0]
-	for _, sinks := range outputTest.DataSinks {
-		// There should be one sink per test.
-		if len(sinks) != 1 {
-			t.Fatalf("there should be one sink per test")
-		}
-		rawProfile = filepath.Join(testOutDir, sinks[0].File)
-	}
-	if rawProfile == "" {
-		t.Fatalf("failed to find a raw profile")
-	}
-
+	rawProfile := runCoverageTest(t, testOutDir)
 	// Prepare llvm-profdata arguments.
 	args := []string{
 		"show",
@@ -269,4 +179,137 @@ func TestCoverage(t *testing.T) {
 	if diff != "" {
 		t.Fatalf("unexpected coverage (-golden-coverage +generated-coverage): %s", diff)
 	}
+}
+
+func runCoverageTest(t *testing.T, testOutDir string) string {
+	var test testsharder.Test
+	var tester testrunner.Tester
+	if *host {
+		test = getHostTest()
+		tester = getHostTester(t, testOutDir)
+	} else {
+		test = getTargetTest()
+		tester = getTargetTester(t, testOutDir)
+	}
+
+	defer tester.Close()
+	ctx := context.Background()
+	result, err := tester.Test(ctx, test, os.Stdout, os.Stdout, "unused-out-dir")
+	if err != nil {
+		t.Fatalf("failed to run the test: %s", err)
+	}
+	outputs, err := testrunner.CreateTestOutputs(tap.NewProducer(io.Discard), testOutDir)
+	if err != nil {
+		t.Fatalf("failed to create test outputs: %s", err)
+	}
+	// Record data sinks.
+	if err := outputs.Record(ctx, *result); err != nil {
+		t.Fatalf("failed to record data sinks: %s", err)
+	}
+
+	// If running on target, copy profiles to the host. There might be a delay between when
+	// the test finishes and data sinks including profiles are available on the target to copy.
+	// When that's the case, EnsureSinks() does not return an error, and it logs the message.
+	// Therefore, check whether v2 sinks directory exists to ensure copying is successful.
+	// When there is a delay, retry.
+	// TODO(fxbug.dev/77634): When we start treating profiles as artifacts, remove retry.
+	if !*host {
+		var sinks []runtests.DataSinkReference
+		err = retry.Retry(ctx, retry.NewConstantBackoff(5*time.Second), func() error {
+			if err := tester.EnsureSinks(ctx, sinks, outputs); err != nil {
+				return err
+			}
+			v2SinksDir := filepath.Join(testOutDir, "v2")
+			_, err := os.ReadDir(v2SinksDir)
+			if err != nil {
+				return err
+			}
+			return nil
+		}, nil)
+		if err != nil {
+			t.Fatalf("failed to collect data sinks: %s", err)
+		}
+	}
+
+	// Close recording of test outputs.
+	if err := outputs.Close(); err != nil {
+		t.Fatalf("failed to save test outputs: %s", err)
+	}
+
+	var rawProfile string
+	outputTest := outputs.Summary.Tests[0]
+	for _, sinks := range outputTest.DataSinks {
+		// There should be one sink per test.
+		if len(sinks) != 1 {
+			t.Fatalf("there should be one sink per test")
+		}
+		rawProfile = filepath.Join(testOutDir, sinks[0].File)
+	}
+	if rawProfile == "" {
+		t.Fatalf("failed to find a raw profile")
+	}
+
+	return rawProfile
+}
+
+func getHostTest() testsharder.Test {
+	test := testsharder.Test{
+		Test: build.Test{
+			Path: *coverageTestBinary,
+		},
+		RunAlgorithm: testsharder.StopOnFailure,
+		Runs:         1,
+	}
+
+	return test
+}
+
+func getHostTester(t *testing.T, testOutDir string) testrunner.Tester {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("failed to get current working directory: %s", err)
+	}
+	tester, err := testrunner.NewSubprocessTester(wd, os.Environ(), testOutDir, "", "")
+	if err != nil {
+		t.Fatalf("failed to initialize fuchsia tester: %s", err)
+	}
+
+	return tester
+}
+
+func getTargetTest() testsharder.Test {
+	test := testsharder.Test{
+		Test: build.Test{
+			Name:       *coverageTestName,
+			PackageURL: *coverageTestName,
+		},
+		RunAlgorithm: testsharder.StopOnFailure,
+		Runs:         1,
+	}
+	return test
+}
+
+func getTargetTester(t *testing.T, testOutDir string) testrunner.Tester {
+	var addr net.IPAddr
+	if deviceAddr, ok := os.LookupEnv(constants.DeviceAddrEnvKey); ok {
+		addrPtr, err := net.ResolveIPAddr("ip", deviceAddr)
+		if err != nil {
+			t.Fatalf("failed to parse device address: %s", deviceAddr)
+		}
+		addr = *addrPtr
+	}
+
+	ctx := context.Background()
+	// Read SSH key which is required to run a test.
+	sshKeyFile := os.Getenv(constants.SSHKeyEnvKey)
+	// Create a new fuchsia tester that is responsible for executing the test.
+	// This is a v2 test, and it uses run-test-suite instead of runtests, so runtests=false.
+	// TODO(fxbug.dev/77634): When we start treating profiles as artifacts, start using ffx
+	// with testrunner.NewFFXTester().
+	tester, err := testrunner.NewFuchsiaSSHTester(ctx, addr, sshKeyFile, testOutDir, "", false)
+	if err != nil {
+		t.Fatalf("failed to initialize fuchsia tester: %s", err)
+	}
+
+	return tester
 }
