7fb6d8613bf862c9836108e1d9a647e122b3eff5
Kevin Sakuma <sakuma@google.com>
2022-10-04 22:41:01 +0000

    [wlan][policy] Adds defect accounting.
    This change adds the ability to tally defects to eventually
    make recovery decisions.
    
    Bug: 60089
    Test: fx test wlancfg-tests
    Multiply: wlancfg-tests
    Change-Id: Id53171180b6c261e7c1051f4591af80b419c3c85
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/734665
    Reviewed-by: Marc Khouri <mnck@google.com>
    Commit-Queue: Kevin Sakuma <sakuma@google.com>

--- src/connectivity/wlan/wlancfg/src/mode_management/mod.rs
+++ src/connectivity/wlan/wlancfg/src/mode_management/mod.rs
@@ -13,6 +13,9 @@ use {
     void::Void,
 };
 
+#[cfg(test)]
+use fuchsia_async as fasync;
+
 mod iface_manager;
 pub mod iface_manager_api;
 mod iface_manager_types;
@@ -50,4 +53,256 @@ pub fn create_iface_manager(
     );
 
     (iface_manager_sender, iface_manager_service)
+}
+
+#[cfg(test)]
+#[derive(Debug, PartialEq)]
+struct Event<T: PartialEq> {
+    value: T,
+    time: fasync::Time,
+}
+
+#[cfg(test)]
+impl<T: PartialEq> Event<T> {
+    fn new(value: T, time: fasync::Time) -> Self {
+        Event { value, time }
+    }
+}
+
+#[cfg(test)]
+struct EventHistory<T: PartialEq> {
+    events: Vec<Event<T>>,
+    retention_time: fuchsia_zircon::Duration,
+}
+
+#[cfg(test)]
+impl<T: PartialEq> EventHistory<T> {
+    fn new(retention_seconds: u32) -> Self {
+        EventHistory {
+            events: Vec::new(),
+            retention_time: fuchsia_zircon::Duration::from_seconds(retention_seconds as i64),
+        }
+    }
+
+    fn add_event(&mut self, value: T) {
+        let curr_time = fasync::Time::now();
+        self.events.push(Event::new(value, curr_time));
+        self.retain_unexpired_events(curr_time);
+    }
+
+    fn event_count(&mut self, value: T) -> usize {
+        let curr_time = fasync::Time::now();
+        self.retain_unexpired_events(curr_time);
+        self.events.iter().filter(|event| event.value == value).count()
+    }
+
+    fn time_since_last_event(&mut self, value: T) -> Option<fuchsia_zircon::Duration> {
+        let curr_time = fasync::Time::now();
+        self.retain_unexpired_events(curr_time);
+
+        for event in self.events.iter().rev() {
+            if event.value == value {
+                return Some(curr_time - event.time);
+            }
+        }
+        None
+    }
+
+    fn retain_unexpired_events(&mut self, curr_time: fasync::Time) {
+        let oldest_allowed_time = curr_time - self.retention_time;
+        self.events.retain(|event| event.time > oldest_allowed_time)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        fuchsia_async::TestExecutor,
+        test_util::{assert_gt, assert_lt},
+    };
+
+    #[fuchsia::test]
+    fn test_event_retention() {
+        // Allow for events to be retained for at most 1s.
+        let mut event_history = EventHistory::<()>::new(1);
+
+        // Add events at 0, 1, 2, 2 and a little bit seconds, and 3s.
+        event_history.events = vec![
+            Event::<()> { value: (), time: fasync::Time::from_nanos(0) },
+            Event::<()> { value: (), time: fasync::Time::from_nanos(1_000_000_000) },
+            Event::<()> { value: (), time: fasync::Time::from_nanos(2_000_000_000) },
+            Event::<()> { value: (), time: fasync::Time::from_nanos(2_000_000_001) },
+            Event::<()> { value: (), time: fasync::Time::from_nanos(3_000_000_000) },
+        ];
+
+        // Retain those events within the retention window based on a current time of 3s.
+        event_history.retain_unexpired_events(fasync::Time::from_nanos(3_000_000_000));
+
+        // It is expected that the events at 2 and a little bit seconds and 3s are retained while
+        // the others are discarded.
+        assert_eq!(
+            event_history.events,
+            vec![
+                Event::<()> { value: (), time: fasync::Time::from_nanos(2_000_000_001) },
+                Event::<()> { value: (), time: fasync::Time::from_nanos(3_000_000_000) },
+            ]
+        );
+    }
+
+    #[derive(Debug, PartialEq)]
+    enum TestEnum {
+        Foo,
+        Bar,
+    }
+
+    #[fuchsia::test]
+    fn test_time_since_last_event() {
+        // An executor is required to enable querying time.
+        let _exec = TestExecutor::new().expect("failed to create an executor");
+
+        // Allow events to be stored basically forever.  The goal here is to ensure that the
+        // retention policy does not discard any of our events.
+        let mut event_history = EventHistory::<TestEnum>::new(u32::MAX);
+
+        // Add some events with known timestamps.
+        let foo_time: i64 = 1_123_123_123;
+        let bar_time: i64 = 2_222_222_222;
+        event_history.events = vec![
+            Event { value: TestEnum::Foo, time: fasync::Time::from_nanos(foo_time) },
+            Event { value: TestEnum::Bar, time: fasync::Time::from_nanos(bar_time) },
+        ];
+
+        // Get the time before and after the function calls were made.  This allows for some slack
+        // in evaluating whether the time calculations are in the realm of accurate.
+        let start_time = fasync::Time::now().into_nanos();
+        let time_since_foo =
+            event_history.time_since_last_event(TestEnum::Foo).expect("Foo was not retained");
+        let time_since_bar =
+            event_history.time_since_last_event(TestEnum::Bar).expect("Bar was not retained");
+        let end_time = fasync::Time::now().into_nanos();
+
+        // Make sure the returned durations are within bounds.
+        assert_lt!(time_since_foo.into_nanos(), end_time - foo_time);
+        assert_gt!(time_since_foo.into_nanos(), start_time - foo_time);
+
+        assert_lt!(time_since_bar.into_nanos(), end_time - bar_time);
+        assert_gt!(time_since_bar.into_nanos(), start_time - bar_time);
+    }
+
+    #[fuchsia::test]
+    fn test_time_since_last_event_retention() {
+        // An executor is required to enable querying time.
+        let _exec = TestExecutor::new().expect("failed to create an executor");
+
+        // Set the retention time to slightly less than the current time.  This number will be
+        // positive.  Since it will occupy the positive range of i64, it is safe to cast it as u32.
+        let curr_time_seconds = fasync::Time::now().into_nanos() / 1_000_000_000;
+        let mut event_history = EventHistory::<()>::new((curr_time_seconds - 1) as u32);
+
+        // Put in an event at time zero so that it will not be retained when querying recent
+        // events.
+        event_history.events.push(Event::<()> { value: (), time: fasync::Time::from_nanos(0) });
+
+        assert_eq!(event_history.time_since_last_event(()), None);
+    }
+    #[fuchsia::test]
+    fn test_add_event() {
+        // An executor is required to enable querying time.
+        let _exec = TestExecutor::new().expect("failed to create an executor");
+        let mut event_history = EventHistory::<()>::new(u32::MAX);
+
+        // Add a few events
+        let num_events = 3;
+        let start_time = fasync::Time::now().into_nanos();
+        for _ in 0..num_events {
+            event_history.add_event(());
+        }
+        let end_time = fasync::Time::now().into_nanos();
+
+        // All three of the recent events should have been retained.
+        assert_eq!(event_history.events.len(), num_events);
+
+        // Verify that all of the even timestamps are within range.
+        for event in event_history.events {
+            let event_time = event.time.into_nanos();
+            assert_lt!(event_time, end_time);
+            assert_gt!(event_time, start_time);
+        }
+    }
+
+    #[fuchsia::test]
+    fn test_add_event_retention() {
+        // An executor is required to enable querying time.
+        let _exec = TestExecutor::new().expect("failed to create an executor");
+
+        // Set the retention time to slightly less than the current time.  This number will be
+        // positive.  Since it will occupy the positive range of i64, it is safe to cast it as u32.
+        let curr_time_seconds = fasync::Time::now().into_nanos() / 1_000_000_000;
+        let mut event_history = EventHistory::<()>::new((curr_time_seconds - 1) as u32);
+
+        // Put in an event at time zero so that it will not be retained when querying recent
+        // events.
+        event_history.events.push(Event::<()> { value: (), time: fasync::Time::from_nanos(0) });
+
+        // Add an event and observe that the event from time 0 has been removed.
+        let start_time = fasync::Time::now().into_nanos();
+        event_history.add_event(());
+        assert_eq!(event_history.events.len(), 1);
+
+        // Add a couple more events.
+        event_history.add_event(());
+        event_history.add_event(());
+        let end_time = fasync::Time::now().into_nanos();
+
+        // All three of the recent events should have been retained.
+        assert_eq!(event_history.events.len(), 3);
+
+        // Verify that all of the even timestamps are within range.
+        for event in event_history.events {
+            let event_time = event.time.into_nanos();
+            assert_lt!(event_time, end_time);
+            assert_gt!(event_time, start_time);
+        }
+    }
+
+    #[fuchsia::test]
+    fn test_event_count() {
+        // An executor is required to enable querying time.
+        let _exec = TestExecutor::new().expect("failed to create an executor");
+        let mut event_history = EventHistory::<TestEnum>::new(u32::MAX);
+
+        event_history.events = vec![
+            Event { value: TestEnum::Foo, time: fasync::Time::from_nanos(0) },
+            Event { value: TestEnum::Foo, time: fasync::Time::from_nanos(1) },
+            Event { value: TestEnum::Bar, time: fasync::Time::from_nanos(2) },
+            Event { value: TestEnum::Bar, time: fasync::Time::from_nanos(3) },
+            Event { value: TestEnum::Foo, time: fasync::Time::from_nanos(4) },
+        ];
+
+        assert_eq!(event_history.event_count(TestEnum::Foo), 3);
+        assert_eq!(event_history.event_count(TestEnum::Bar), 2);
+    }
+
+    #[fuchsia::test]
+    fn test_event_count_retention() {
+        // An executor is required to enable querying time.
+        let _exec = TestExecutor::new().expect("failed to create an executor");
+
+        // Set the retention time to slightly less than the current time.  This number will be
+        // positive.  Since it will occupy the positive range of i64, it is safe to cast it as u32.
+        let curr_time_seconds = fasync::Time::now().into_nanos() / 1_000_000_000;
+        let mut event_history = EventHistory::<TestEnum>::new((curr_time_seconds - 1) as u32);
+
+        event_history.events = vec![
+            Event { value: TestEnum::Foo, time: fasync::Time::from_nanos(0) },
+            Event { value: TestEnum::Foo, time: fasync::Time::from_nanos(0) },
+            Event { value: TestEnum::Bar, time: fasync::Time::now() },
+            Event { value: TestEnum::Bar, time: fasync::Time::now() },
+            Event { value: TestEnum::Foo, time: fasync::Time::now() },
+        ];
+
+        assert_eq!(event_history.event_count(TestEnum::Foo), 1);
+        assert_eq!(event_history.event_count(TestEnum::Bar), 2);
+    }
 }
