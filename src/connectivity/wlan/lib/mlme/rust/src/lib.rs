173aee619c4164d9eb5419702ddb721e7bee9a91
Dylan Swiggett <swiggett@google.com>
2022-10-04 23:38:59 +0000

    [wlan][mlme] Improve logging when mlme handler fails.
    Log the request type as well as the failure.
    
    Test: N/A, logging change only.
    Bug: 111120
    Change-Id: I71240016c504856f8752ef6f25152de8fe1d0c88
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/735263
    Reviewed-by: Charles Celerier <chcl@google.com>
    Commit-Queue: Dylan Swiggett <swiggett@google.com>

--- src/connectivity/wlan/lib/mlme/rust/src/lib.rs
+++ src/connectivity/wlan/lib/mlme/rust/src/lib.rs
@@ -387,9 +387,19 @@ impl<T: 'static + MlmeImpl> Mlme<T> {
             select! {
                 // Process requests from SME.
                 mlme_request = self.mlme_request_stream.next() => match mlme_request {
-                    Some(req) => if let Err(e) = req.map_err(|e| e.into())
-                        .and_then(|r| self.mlme_impl.handle_mlme_message(r)) {
-                            info!("Failed to handle mlme request: {}", e);
+                    Some(req) => {
+                        match req {
+                            Ok(req) => {
+                                let method_name = req.method_name();
+                                if let Err(e) = self.mlme_impl.handle_mlme_message(req) {
+                                    info!("Failed to handle mlme {} request: {}", method_name, e);
+                                }
+                            }
+                            Err(e) => {
+                                info!("Failure while receiving mlme request: {}", e);
+                            }
+                        }
+
                     }
                     None => bail!("MLME request stream terminated unexpectedly."),
                 },
