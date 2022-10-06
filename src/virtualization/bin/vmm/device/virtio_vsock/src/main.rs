a9fc27ae3503dd8ebcf85b3f5e840e7471c7fb55
Tim Detwiler <tjdetwiler@google.com>
2022-10-06 10:42:40 +0000

    [virtualization] Enable debug logging for vsock
    Bug: 110903
    Change-Id: Iaf305d485f0de27eb981bfc5e442683ac2a4a4e1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/735908
    Fuchsia-Auto-Submit: Tim Detwiler <tjdetwiler@google.com>
    Reviewed-by: David Hastings <dahastin@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/virtualization/bin/vmm/device/virtio_vsock/src/main.rs
+++ src/virtualization/bin/vmm/device/virtio_vsock/src/main.rs
@@ -114,7 +114,7 @@ async fn handle_host_vsock_endpoint(
         .map_err(|err| anyhow!(err))
 }
 
-#[fuchsia::main(logging = true, threads = 1)]
+#[fuchsia::main(logging = true, logging_minimum_severity = "debug", threads = 1)]
 async fn main() -> Result<(), Error> {
     let vsock_device = VsockDevice::new();
 
