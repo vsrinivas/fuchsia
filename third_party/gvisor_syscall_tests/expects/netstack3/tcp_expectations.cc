41f0f36c8c4735fd8e7f32f3f5695e92d77516af
Nick Brown <nickbrow@google.com>
2022-10-07 20:30:30 +0000

    [gvisor_syscall_tests] Skip NS3 tcp NoDelayDefault
    This test sometimes panics.
    
    Bug: 111364
    Change-Id: I855a103458bc4b7bfe3e48b7471938b12726300b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/736142
    Commit-Queue: Nick Brown <nickbrow@google.com>
    Reviewed-by: Zeling Feng <zeling@google.com>

--- third_party/gvisor_syscall_tests/expects/netstack3/tcp_expectations.cc
+++ third_party/gvisor_syscall_tests/expects/netstack3/tcp_expectations.cc
@@ -18,6 +18,48 @@ void AddNonPassingTests(TestMap& tests) {
   SkipTest(tests, "AllUnixDomainSockets/*.*/*");
   SkipTest(tests, "AllInetTests/SimpleTcpSocketTest.*/*");
 
+  // Skip tests that sometimes crash the Netstack.
+  //
+  // https://fxbug.dev/111364
+  SkipTest(tests, "AllInetTests/TcpSocketTest.NoDelayDefault/*");
+
+  // Otherwise, expect failure.
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.ConnectedAcceptedPeerAndLocalAreReciprocals/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.ConnectOnEstablishedConnection/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.ShutdownWriteInTimeWait/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.ShutdownWriteInFinWait1/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.DataCoalesced/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.SenderAddressIgnored/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.SenderAddressIgnoredOnPeek/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.SendtoAddressIgnored/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.WritevZeroIovec/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.ZeroWriteAllowed/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.NonblockingLargeWrite/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.BlockingLargeWrite/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.LargeSendDontWait/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.NonblockingLargeSend/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.BlockingLargeSend/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.PollWithFullBufferBlocks/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.ClosedWriteBlockingSocket/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.ClosedReadBlockingSocket/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.MsgTrunc/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.MsgTruncWithCtrunc/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.MsgTruncWithCtruncOnly/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.MsgTruncLargeSize/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.MsgTruncPeek/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.NoDelayDefault/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.SetNoDelay/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.TcpInqSetSockOpt/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.TcpInq/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.Tiocinq/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.TcpSCMPriority/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.TimeWaitPollHUP/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.FullBuffer/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.PollAfterShutdown/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.GetSocketAcceptConnListener/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.GetSocketAcceptConnNonListener/*");
+  ExpectFailure(tests, "AllInetTests/TcpSocketTest.SendUnblocksOnSendBufferIncrease/*");
+
   for (const auto& index : {"2", "3", "6", "7"}) {
     // Expect failure for tests that set the TCP_NODELAY socket option.
     ExpectFailure(
@@ -30,8 +72,6 @@ void AddNonPassingTests(TestMap& tests) {
         tests, TestSelector::ParameterizedTest("NonBlockingTCPSockets", "NonBlockingSocketPairTest",
                                                "RecvMsgNothingAvailable", index));
   }
-
-  ExpectFailure(tests, "AllInetTests/TcpSocketTest.*/*");
 }  // NOLINT(readability/fn_size)
 
 }  // namespace netstack_syscall_test
