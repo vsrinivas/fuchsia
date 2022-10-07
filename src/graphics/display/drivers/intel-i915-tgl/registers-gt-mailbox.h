3db742505cd8fd1850ffadd7bfefc3a4bf8464e6
Victor Costan <costan@google.com>
2022-10-07 01:08:46 +0000

    [intel-i915-tgl] Add fields PARAM1-2 to GT Mailbox Interface register.
    These fields are used by SAGV (System Agent Geyserville) commands.
    
    Bug: 111054
    Tested: None, no functional changes; existing commands all fit in 1 byte
    Change-Id: I9fb780f627a0d9723a29a26a8f28d6cd773e7b5e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/734243
    Reviewed-by: Yilong Li <liyl@google.com>
    Fuchsia-Auto-Submit: Victor Costan <costan@google.com>
    Commit-Queue: Yilong Li <liyl@google.com>

--- src/graphics/display/drivers/intel-i915-tgl/registers-gt-mailbox.h
+++ src/graphics/display/drivers/intel-i915-tgl/registers-gt-mailbox.h
@@ -16,14 +16,13 @@ namespace tgl_registers {
 // Used for communication between the graphics driver and the PCODE (power
 // controller firmware code) running on the PCU (power controller).
 //
-// This register is documented in IHD-OS-TGL-Vol 2c-1.22-Rev2.0 Part 1 page 1090
-// and IHD-OS-DG1-Vol 2c-2.21 Part 1 page 1049, but the MMIO address is
-// incorrect.
+// This register's field breakdown was last documented in the Broadwell
+// documentation (IHD-OS-BDW-Vol 12-11.15 pages 31-32).
 //
-// Tiger Lake: IHD-OS-TGL-Vol 2c-1.22-Rev2.0 Part 1 page 1089
+// Tiger Lake: IHD-OS-TGL-Vol 2c-1.22-Rev2.0 Part 1 page 1090
 // DG1: IHD-OS-DG1-Vol 2c-2.21 Part 1 page 1049
-// Kaby Lake: IHD-OS-KBL-Vol 12-1.17 "Sequences for Changing CD Clock Frequency,
-//            pages 138-139
+// Kaby Lake: IHD-OS-KBL-Vol 12-1.17 "Sequences for Changing CD Clock
+//            Frequency", pages 138-139
 // Skylake: IHD-OS-SKL-Vol 12-05.16 "Skylake Sequences for Changing CD Clock
 //          Frequency", pages 135-136
 class PowerMailboxInterface : public hwreg::RegisterBase<PowerMailboxInterface, uint32_t> {
@@ -35,12 +34,47 @@ class PowerMailboxInterface : public hwreg::RegisterBase<PowerMailboxInterface, 
   // values before setting this to true.
   //
   // The PCU firmware sets this field to false when it completes the command.
+  //
+  // This field is also called RUN_BUSY (Run/Busy) in Intel's documentation.
   DEF_BIT(31, has_active_transaction);
+
+  // The Broadwell PCU firmware had bits 28:8 assigned to an Address Control
+  // field (documented in IHD-OS-BDW-Vol 12-11.15 pages 31-32). The Address
+  // Control field appears to still be in use on Kaby Lake and Skylake, because
+  // it's mentioned in section "System Agent Geyserville (SAGV)" > "Memory
+  // Values" > "Retrieve Memory Latency Data" in the display engine PRM.
+  // Kaby Lake: IHD-OS-KBL-Vol 12-1.17 page 208
+  // Skylake: IHD-OS-SKL-Vol 12-05.16 page 199
+  //
+  // We can get away with ignoring the Address Control field because the only
+  // command description that references it sets all its bits to zero.
+  DEF_RSVDZ_FIELD(30, 24);
+
+  // The PARAM2 field in Intel's documentation.
+  //
+  // This field is mentioned in the display engine PRMs, but its underlying bits
+  // are not documented. We deduced its placement by comparing the
+  // icl_pcode_read_qgv_point_info() function in the i915 OpenBSD driver with
+  // against the MAILBOX_GTRDIVER_CMD_MEM_SS_INFO_SUBCOMMAND_READ_QGV_POINT_INFO
+  // description in Tiger Lake documentation.
+  DEF_FIELD(23, 16, param2);
+
+  // The PARAM1 field in Intel documentation.
+  //
+  // This field is documented implicitly by a mention of "PARAM1[15:8]" in the
+  // MAILBOX_GTRDIVER_CMD_MEM_SS_INFO command description under the "Mailbox
+  // Commands" section of the display engine PRM.
+  // Tiger Lake: IHD-OS-TGL-Vol 12-1.22-Rev2.0 page 212
+  // DG1: IHD-OS-DG1-Vol 12-2.21 page 169
+  // Lakefield: IHD-OS-LKF-Vol 12-4.21 page 166
+  DEF_FIELD(15, 8, param1);
 
   // The command to be executed by the PCU.
   //
   // Valid commands are documented throughout the reference manuals.
-  DEF_FIELD(30, 0, command_code);
+  //
+  // This field is also called COMMAND in Intel's documentation.
+  DEF_FIELD(7, 0, command_code);
 
   static auto Get() { return hwreg::RegisterAddr<PowerMailboxInterface>(0x138124); }
 };
@@ -53,8 +87,12 @@ class PowerMailboxInterface : public hwreg::RegisterBase<PowerMailboxInterface, 
 // This register must not be modified while the PCU is executing a driver
 // command, as indicated in the PowerMailboxInterface register.
 //
-// Kaby Lake: IHD-OS-KBL-Vol 12-1.17 "Sequences for Changing CD Clock Frequency,
-//            pages 138-139
+// This register was last documented formally in the Broadwell documentation
+// (IHD-OS-BDW-Vol 12-11.15 page 33). Later PRMs document it indirectly, by
+// providing its MMIO address in programming sequences.
+//
+// Kaby Lake: IHD-OS-KBL-Vol 12-1.17 "Sequences for Changing CD Clock
+//            Frequency", pages 138-139
 // Skylake: IHD-OS-SKL-Vol 12-05.16 "Skylake Sequences for Changing CD Clock
 //          Frequency", pages 135-136
 class PowerMailboxData0 : public hwreg::RegisterBase<PowerMailboxData0, uint32_t> {
@@ -70,10 +108,10 @@ class PowerMailboxData0 : public hwreg::RegisterBase<PowerMailboxData0, uint32_t
 // This register must not be modified while the PCU is executing a driver
 // command, as indicated in the PowerMailboxInterface register.
 //
-// Tiger Lake: IHD-OS-TGL-Vol 2c-1.22-Rev2.0 Part 1 page 1090
+// Tiger Lake: IHD-OS-TGL-Vol 2c-1.22-Rev2.0 Part 1 page 1089
 // DG1: IHD-OS-DG1-Vol 2c-2.21 Part 1 page 1048
-// Kaby Lake: IHD-OS-KBL-Vol 12-1.17 "Sequences for Changing CD Clock Frequency,
-//            pages 138-139
+// Kaby Lake: IHD-OS-KBL-Vol 12-1.17 "Sequences for Changing CD Clock
+//            Frequency", pages 138-139
 // Skylake: IHD-OS-SKL-Vol 12-05.16 "Skylake Sequences for Changing CD Clock
 //          Frequency", pages 135-136
 class PowerMailboxData1 : public hwreg::RegisterBase<PowerMailboxData1, uint32_t> {
