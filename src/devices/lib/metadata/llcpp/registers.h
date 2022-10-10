a2736cf86697f224c8e10ef2055bc23e43e35795
Braden Kell <bradenkell@google.com>
2022-10-10 22:25:51 +0000

    [registers][fidl] Use builders instead of mutable tables
    Set up registers metadata using builders instead of (deprecated) mutable
    tables, and remove build allow list entries where possible.
    
    Test: CQ
    Change-Id: Iae3394ddea24f63060219af2cb7b6fe7fc76fc62
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/736668
    Commit-Queue: Braden Kell <bradenkell@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>

--- src/devices/lib/metadata/llcpp/registers.h
+++ src/devices/lib/metadata/llcpp/registers.h
@@ -43,34 +43,30 @@ RegistersMetadataEntry BuildMetadata(fidl::AnyArena& allocator, uint32_t bind_id
                                      std::vector<MaskEntryBuilder<T>> masks) {
   fidl::VectorView<MaskEntry> built_masks(allocator, masks.size());
   for (uint32_t i = 0; i < masks.size(); i++) {
-    built_masks[i].Allocate(allocator);
-    built_masks[i].set_mask(allocator, BuildMask<T>(allocator, masks[i].mask));
-    built_masks[i].set_mmio_offset(allocator, masks[i].mmio_offset);
-    built_masks[i].set_count(masks[i].reg_count);
-    built_masks[i].set_overlap_check_on(masks[i].overlap_check_on);
+    built_masks[i] = MaskEntry::Builder(allocator)
+                         .mask(BuildMask<T>(allocator, masks[i].mask))
+                         .mmio_offset(masks[i].mmio_offset)
+                         .count(masks[i].reg_count)
+                         .overlap_check_on(masks[i].overlap_check_on)
+                         .Build();
   }
 
-  RegistersMetadataEntry entry(allocator);
-  entry.set_bind_id(bind_id);
-  entry.set_mmio_id(mmio_id);
-  entry.set_masks(allocator, std::move(built_masks));
-  return entry;
+  return RegistersMetadataEntry::Builder(allocator)
+      .bind_id(bind_id)
+      .mmio_id(mmio_id)
+      .masks(built_masks)
+      .Build();
 }
 
 using fuchsia_hardware_registers::wire::MmioMetadataEntry;
 MmioMetadataEntry BuildMetadata(fidl::AnyArena& allocator, uint32_t id) {
-  MmioMetadataEntry entry(allocator);
-  entry.set_id(id);
-  return entry;
+  return MmioMetadataEntry::Builder(allocator).id(id).Build();
 }
 
 using fuchsia_hardware_registers::wire::Metadata;
 Metadata BuildMetadata(fidl::AnyArena& allocator, fidl::VectorView<MmioMetadataEntry> mmio,
                        fidl::VectorView<RegistersMetadataEntry> registers) {
-  Metadata metadata(allocator);
-  metadata.set_mmio(allocator, std::move(mmio));
-  metadata.set_registers(allocator, std::move(registers));
-  return metadata;
+  return Metadata::Builder(allocator).mmio(mmio).registers(registers).Build();
 }
 
 }  // namespace registers
