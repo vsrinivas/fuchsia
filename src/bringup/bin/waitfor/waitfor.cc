3be33b83ccf84b28a087b63efde8e0c63365380f
Tamir Duberstein <tamird@google.com>
2022-10-11 12:56:41 +0000

    [waitfor] Avoid globals
    Change-Id: Icedb2b2dfe396808e721cec7ab22702cc9b22371
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/737725
    Reviewed-by: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>

--- src/bringup/bin/waitfor/waitfor.cc
+++ src/bringup/bin/waitfor/waitfor.cc
@@ -10,7 +10,6 @@
 
 #include <memory>
 
-#include <fbl/intrusive_double_list.h>
 #include <fbl/unique_fd.h>
 
 #include "src/lib/uuid/uuid.h"
@@ -35,44 +34,48 @@ void usage() {
           "example: waitfor class=block part.name=system print\n");
 }
 
-static bool verbose = false;
-static bool print = false;
-static bool forever = false;
-static bool matched = false;
-static zx_duration_t timeout = 0;
-const char* devclass = nullptr;
+struct App;
 
-class Rule : public fbl::DoublyLinkedListable<std::unique_ptr<Rule>> {
+class Rule {
  public:
-  using Func = zx_status_t (*)(const char* arg, int fd);
+  using Func = zx_status_t (*)(const App& app, const char* arg, int fd);
 
-  Rule(Func func, const char* arg) : func_(func), arg_(arg) {}
+  Rule(const char* arg, Func func) : func_(func), arg_(arg) {}
 
-  zx_status_t CallWithFd(int fd) const { return func_(arg_, fd); }
+  zx_status_t CallWithFd(const App& app, int fd) const { return func_(app, arg_, fd); }
 
  private:
   Func func_;
   const char* arg_;
 };
 
-static fbl::DoublyLinkedList<std::unique_ptr<Rule>> rules;
+struct App {
+  bool matched;
+
+  const bool verbose;
+  const bool print;
+  const bool forever;
+  const char* devclass;
+  const std::vector<Rule> rules;
+};
 
 zx_status_t watchcb(int dirfd, int event, const char* fn, void* cookie) {
   if (event != WATCH_EVENT_ADD_FILE) {
     return ZX_OK;
   }
-  if (verbose) {
-    fprintf(stderr, "waitfor: device='/dev/class/%s/%s'\n", devclass, fn);
+  App& app = *static_cast<App*>(cookie);
+  if (app.verbose) {
+    fprintf(stderr, "waitfor: device='/dev/class/%s/%s'\n", app.devclass, fn);
   }
   const fbl::unique_fd fd(openat(dirfd, fn, O_RDONLY));
   if (!fd.is_valid()) {
-    fprintf(stderr, "waitfor: warning: failed to open '/dev/class/%s/%s': %s\n", devclass, fn,
+    fprintf(stderr, "waitfor: warning: failed to open '/dev/class/%s/%s': %s\n", app.devclass, fn,
             strerror(errno));
     return ZX_OK;
   }
 
-  for (const Rule& r : rules) {
-    switch (const zx_status_t status = r.CallWithFd(fd.get()); status) {
+  for (const Rule& r : app.rules) {
+    switch (const zx_status_t status = r.CallWithFd(app, fd.get()); status) {
       case ZX_OK:
         // rule matched
         continue;
@@ -85,13 +88,13 @@ zx_status_t watchcb(int dirfd, int event, const char* fn, void* cookie) {
     }
   }
 
-  matched = true;
+  app.matched = true;
 
-  if (print) {
-    printf("/dev/class/%s/%s\n", devclass, fn);
+  if (app.print) {
+    printf("/dev/class/%s/%s\n", app.devclass, fn);
   }
 
-  if (forever) {
+  if (app.forever) {
     return ZX_OK;
   }
   return ZX_ERR_STOP;
@@ -100,7 +103,7 @@ zx_status_t watchcb(int dirfd, int event, const char* fn, void* cookie) {
 // Expression evaluators return OK on match, NEXT on no-match
 // any other error is fatal
 
-zx_status_t expr_topo(const char* arg, int fd) {
+zx_status_t expr_topo(const App& app, const char* arg, int fd) {
   const fdio_cpp::UnownedFdioCaller caller(fd);
   const fidl::WireResult result =
       fidl::WireCall(caller.borrow_as<fuchsia_device::Controller>())->GetTopologicalPath();
@@ -118,7 +121,7 @@ zx_status_t expr_topo(const char* arg, int fd) {
   }
   const std::string_view got = res->value()->path.get();
   const std::string_view expected(arg);
-  if (verbose) {
+  if (app.verbose) {
     fprintf(stderr, "waitfor: topological path='%s'\n", std::string(got).c_str());
   }
   // Check if the topo path contains our needle.
@@ -128,7 +131,7 @@ zx_status_t expr_topo(const char* arg, int fd) {
   return ZX_ERR_NEXT;
 }
 
-zx_status_t expr_part_guid(const char* arg, int fd) {
+zx_status_t expr_part_guid(const App& app, const char* arg, int fd) {
   const fdio_cpp::UnownedFdioCaller caller(fd);
   const fidl::WireResult result =
       fidl::WireCall(caller.borrow_as<fuchsia_hardware_block_partition::Partition>())
@@ -147,7 +150,7 @@ zx_status_t expr_part_guid(const char* arg, int fd) {
   fidl::Array value = response.guid->value;
   static_assert(decltype(value)::size() == uuid::kUuidSize);
   const std::string text = uuid::Uuid(value.data()).ToString();
-  if (verbose) {
+  if (app.verbose) {
     fprintf(stderr, "waitfor: part.guid='%s'\n", text.c_str());
   }
   if (strcasecmp(text.c_str(), arg) == 0) {
@@ -156,7 +159,7 @@ zx_status_t expr_part_guid(const char* arg, int fd) {
   return ZX_ERR_NEXT;
 }
 
-zx_status_t expr_part_type_guid(const char* arg, int fd) {
+zx_status_t expr_part_type_guid(const App& app, const char* arg, int fd) {
   const fdio_cpp::UnownedFdioCaller caller(fd);
   const fidl::WireResult result =
       fidl::WireCall(caller.borrow_as<fuchsia_hardware_block_partition::Partition>())
@@ -174,7 +177,7 @@ zx_status_t expr_part_type_guid(const char* arg, int fd) {
   fidl::Array value = response.guid->value;
   static_assert(decltype(value)::size() == uuid::kUuidSize);
   const std::string text = uuid::Uuid(value.data()).ToString();
-  if (verbose) {
+  if (app.verbose) {
     fprintf(stderr, "waitfor: part.type.guid='%s'\n", text.c_str());
   }
   if (strcasecmp(text.c_str(), arg) == 0) {
@@ -183,7 +186,7 @@ zx_status_t expr_part_type_guid(const char* arg, int fd) {
   return ZX_ERR_NEXT;
 }
 
-zx_status_t expr_part_name(const char* arg, int fd) {
+zx_status_t expr_part_name(const App& app, const char* arg, int fd) {
   const fdio_cpp::UnownedFdioCaller caller(fd);
   const fidl::WireResult result =
       fidl::WireCall(caller.borrow_as<fuchsia_hardware_block_partition::Partition>())->GetName();
@@ -198,7 +201,7 @@ zx_status_t expr_part_name(const char* arg, int fd) {
     return result.status();
   }
   const std::string name(response.name.get());
-  if (verbose) {
+  if (app.verbose) {
     fprintf(stderr, "waitfor: part.name='%s'\n", name.c_str());
   }
   if (strcmp(arg, name.c_str()) == 0) {
@@ -207,21 +210,18 @@ zx_status_t expr_part_name(const char* arg, int fd) {
   return ZX_ERR_NEXT;
 }
 
-void new_rule(const char* arg, zx_status_t (*func)(const char* arg, int fd)) {
-  auto r = std::make_unique<Rule>(func, arg);
-  if (r == nullptr) {
-    fprintf(stderr, "waitfor: error: out of memory\n");
-    exit(1);
-  }
-  rules.push_back(std::move(r));
-}
-
 int main(int argc, char** argv) {
   if (argc == 1) {
     usage();
     exit(1);
   }
 
+  bool print = false;
+  bool verbose = false;
+  bool forever = false;
+  zx_duration_t timeout = 0;
+  const char* devclass = nullptr;
+  std::vector<Rule> rules;
   while (argc > 1) {
     if (!strcmp(argv[1], "print")) {
       print = true;
@@ -238,13 +238,13 @@ int main(int argc, char** argv) {
     } else if (!strncmp(argv[1], "class=", 6)) {
       devclass = argv[1] + 6;
     } else if (!strncmp(argv[1], "topo=", 5)) {
-      new_rule(argv[1] + 5, expr_topo);
+      rules.emplace_back(argv[1] + 5, expr_topo);
     } else if (!strncmp(argv[1], "part.guid=", 10)) {
-      new_rule(argv[1] + 10, expr_part_guid);
+      rules.emplace_back(argv[1] + 10, expr_part_guid);
     } else if (!strncmp(argv[1], "part.type.guid=", 15)) {
-      new_rule(argv[1] + 15, expr_part_guid);
+      rules.emplace_back(argv[1] + 15, expr_part_guid);
     } else if (!strncmp(argv[1], "part.name=", 10)) {
-      new_rule(argv[1] + 10, expr_part_name);
+      rules.emplace_back(argv[1] + 10, expr_part_name);
     } else {
       fprintf(stderr, "waitfor: error: unknown expr '%s'\n\n", argv[1]);
       usage();
@@ -259,15 +259,14 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  if (rules.is_empty()) {
+  if (rules.empty()) {
     fprintf(stderr, "waitfor: error: no match expressions specified\n");
     exit(1);
   }
 
-  char path[strlen(devclass) + strlen("/dev/class/") + 1];
-  sprintf(path, "/dev/class/%s", devclass);
+  const std::string path = std::string("/dev/class/") + devclass;
 
-  const fbl::unique_fd dirfd(open(path, O_DIRECTORY | O_RDONLY));
+  const fbl::unique_fd dirfd(open(path.c_str(), O_DIRECTORY | O_RDONLY));
   if (!dirfd.is_valid()) {
     fprintf(stderr, "waitfor: error: cannot watch class '%s': %s\n", devclass, strerror(errno));
     exit(1);
@@ -280,14 +279,21 @@ int main(int argc, char** argv) {
     deadline = zx_deadline_after(timeout);
   }
 
-  switch (const zx_status_t status = fdio_watch_directory(dirfd.get(), watchcb, deadline, nullptr);
+  App app = {
+      .verbose = verbose,
+      .print = print,
+      .forever = forever,
+      .devclass = devclass,
+      .rules = std::move(rules),
+  };
+  switch (const zx_status_t status = fdio_watch_directory(dirfd.get(), watchcb, deadline, &app);
           status) {
     case ZX_ERR_STOP:
       // clean exit on a match
       return 0;
     case ZX_ERR_TIMED_OUT:
       // timeout, but if we're in forever mode and matched any, its good
-      if (matched && forever) {
+      if (app.matched && app.forever) {
         return 0;
       }
       break;
