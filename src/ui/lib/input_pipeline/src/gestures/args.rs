f5a84e5b330e59bfba8a00945fabcdfc1e2a9bd0
chaopeng <chaopeng@google.com>
2022-10-12 20:55:04 +0000

    [ui][input-pipeline] update threshold of palm size
    Based on palm / thumb size data collected from adults.
    
    Min of max of contact size of palm sequence is 8.122mm.
    Max thumb contact is 7.752mm.
    Mean of max of contact size of thumb sequence is 5.853mm,
    standard deviation is 0.925mm. Mean + 2 stddev = 7.70mm.
    
    So we believe 7.8mm maybe a threshold of thumb to palm.
    
    Bug: 102898
    Change-Id: I131169fa45a3c283a074786585867cf6401492ad
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/738585
    Reviewed-by: Mukesh Agrawal <quiche@google.com>
    Commit-Queue: Jianpeng Chao <chaopeng@google.com>

--- src/ui/lib/input_pipeline/src/gestures/args.rs
+++ src/ui/lib/input_pipeline/src/gestures/args.rs
@@ -9,4 +9,12 @@ pub(super) const MAX_SPURIOUS_TO_INTENTIONAL_SCROLL_THRESHOLD_MM: f32 =
     5.0 * SPURIOUS_TO_INTENTIONAL_MOTION_THRESHOLD_MM;
 pub(super) const TAP_TIMEOUT: zx::Duration = zx::Duration::from_millis(1200);
 pub(super) const MAX_SCROLL_DIRECTION_SKEW_DEGREES: f32 = 40.0;
-pub(super) const MIN_PALM_SIZE_MM: f32 = 5.0;
+
+/// Based on palm / thumb size data collected from adults.
+///
+/// Min of max of contact size of palm sequence is 8.122mm.
+/// Max thumb contact is 7.752mm.
+/// Mean of max of contact size of thumb sequence is 5.853mm,
+/// standard deviation is 0.925mm. Mean + 2 stddev = 7.70mm.
+/// So we believe 7.8mm maybe a threshold of thumb to palm.
+pub(super) const MIN_PALM_SIZE_MM: f32 = 7.8;
