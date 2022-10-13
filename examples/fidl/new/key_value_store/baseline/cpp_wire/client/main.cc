f4b19a47cf868e280dbaeb58e0a3caee3c8c224b
Alex Zaslavsky <azaslavsky@google.com>
2022-10-13 20:58:03 +0000

    [fidl][docs] Add key_value_store baseline cpp_wire
    The scaffold of this change was generated via, and as a test of:
    `scaffold.py implement key_value_store --new --protocol=Store
    --binding=cpp_wire`.
    
    Test: fx test exammples_fidl; manual devsite staging
    Bug: 109277
    Bug: 111279
    Change-Id: I82e150aa893d9988b589c4bf9085f65dd4781abf
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/740203
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Alex Zaslavsky <azaslavsky@google.com>

---
+++ examples/fidl/new/key_value_store/baseline/cpp_wire/client/main.cc
@@ -0,0 +1,74 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/examples.keyvaluestore.baseline/cpp/wire.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/sys/component/cpp/service_client.h>
+#include <lib/syslog/cpp/macros.h>
+#include <unistd.h>
+
+#include <examples/fidl/new/key_value_store/baseline/cpp_wire/client/config.h>
+#include <src/lib/files/file.h>
+#include <src/lib/fxl/strings/string_printf.h>
+
+int main(int argc, const char** argv) {
+  FX_LOGS(INFO) << "Started";
+
+  // Retrieve component configuration.
+  auto conf = config::Config::TakeFromStartupHandle();
+
+  // Start up an async loop and dispatcher.
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async_dispatcher_t* dispatcher = loop.dispatcher();
+
+  // Connect to the protocol inside the component's namespace. This can fail so it's wrapped in a
+  // |zx::status| and it must be checked for errors.
+  zx::status client_end = component::Connect<examples_keyvaluestore_baseline::Store>();
+  if (!client_end.is_ok()) {
+    FX_LOGS(ERROR) << "Synchronous error when connecting to the |Store| protocol: "
+                   << client_end.status_string();
+    return -1;
+  }
+
+  // Create an asynchronous client using the newly-established connection.
+  fidl::WireClient client(std::move(*client_end), dispatcher);
+  FX_LOGS(INFO) << "Outgoing connection enabled";
+
+  for (const auto& key : conf.write_items()) {
+    std::string text;
+    if (!files::ReadFileToString(fxl::StringPrintf("/pkg/data/%s.txt", key.c_str()), &text)) {
+      FX_LOGS(ERROR) << "It looks like the correct `resource` dependency has not been packaged";
+      break;
+    }
+
+    auto value = std::vector<uint8_t>(text.begin(), text.end());
+    client
+        ->WriteItem(
+            {fidl::StringView::FromExternal(key), fidl::VectorView<uint8_t>::FromExternal(value)})
+        .ThenExactlyOnce(
+            [&](fidl::WireUnownedResult<examples_keyvaluestore_baseline::Store::WriteItem>&
+                    result) {
+              if (!result.ok()) {
+                FX_LOGS(ERROR) << "Unexpected framework error";
+              } else if (result->is_error()) {
+                FX_LOGS(INFO) << "WriteItem Error: " << fidl::ToUnderlying(result->error_value());
+              } else {
+                FX_LOGS(INFO) << "WriteItem Success";
+              }
+
+              // Quit the loop, thereby handing control back to the outer loop of actions being
+              // iterated over.
+              loop.Quit();
+            });
+
+    // Run the loop until the callback is resolved, at which point we can continue from here.
+    loop.Run();
+    loop.ResetQuit();
+  }
+
+  // TODO(fxbug.dev/76579): We need to sleep here to make sure all logs get drained. Once the
+  // referenced bug has been resolved, we can remove the sleep.
+  sleep(2);
+  return 0;
+}
---
+++ examples/fidl/new/key_value_store/baseline/cpp_wire/server/main.cc
@@ -0,0 +1,113 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fidl/examples.keyvaluestore.baseline/cpp/wire.h>
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/async/cpp/task.h>
+#include <lib/fidl/cpp/wire/channel.h>
+#include <lib/sys/component/cpp/outgoing_directory.h>
+#include <lib/syslog/cpp/macros.h>
+#include <unistd.h>
+
+#include <re2/re2.h>
+
+// An implementation of the |Store| protocol.
+class StoreImpl final : public fidl::WireServer<examples_keyvaluestore_baseline::Store> {
+ public:
+  // Bind this implementation to a channel.
+  StoreImpl(async_dispatcher_t* dispatcher,
+            fidl::ServerEnd<examples_keyvaluestore_baseline::Store> server_end)
+      : binding_(fidl::BindServer(
+            dispatcher, std::move(server_end), this,
+            [this](StoreImpl* impl, fidl::UnbindInfo info,
+                   fidl::ServerEnd<examples_keyvaluestore_baseline::Store> server_end) {
+              if (info.reason() != ::fidl::Reason::kPeerClosed) {
+                FX_LOGS(ERROR) << "Shutdown unexpectedly";
+              }
+              delete this;
+            })) {}
+
+  void WriteItem(WriteItemRequestView request, WriteItemCompleter::Sync& completer) override {
+    FX_LOGS(INFO) << "WriteItem request received";
+    std::string key = request->attempt.key.data();
+    const auto value =
+        std::vector<uint8_t>(request->attempt.value.begin(), request->attempt.value.end());
+
+    // Validate the key.
+    if (!RE2::FullMatch(key, "^[A-Za-z]\\w+[A-Za-z0-9]$")) {
+      FX_LOGS(INFO) << "Write error: INVALID_KEY, For key: " << key;
+      FX_LOGS(INFO) << "WriteItem response sent";
+      return completer.Reply(fit::error(examples_keyvaluestore_baseline::WriteError::kInvalidKey));
+    }
+
+    // Validate the value.
+    if (value.empty()) {
+      FX_LOGS(INFO) << "Write error: INVALID_VALUE, For key: " << key;
+      FX_LOGS(INFO) << "WriteItem response sent";
+      return completer.Reply(
+          fit::error(examples_keyvaluestore_baseline::WriteError::kInvalidValue));
+    }
+
+    if (key_value_store_.find(key) != key_value_store_.end()) {
+      FX_LOGS(INFO) << "Write error: ALREADY_EXISTS, For key: " << key;
+      FX_LOGS(INFO) << "WriteItem response sent";
+      return completer.Reply(
+          fit::error(examples_keyvaluestore_baseline::WriteError::kAlreadyExists));
+    }
+
+    // Ensure that the value does not already exist in the store.
+    key_value_store_.insert({key, value});
+    FX_LOGS(INFO) << "Wrote value at key: " << key;
+    FX_LOGS(INFO) << "WriteItem response sent";
+    return completer.Reply(fit::success());
+  }
+
+ private:
+  fidl::ServerBindingRef<examples_keyvaluestore_baseline::Store> binding_;
+
+  // The map that serves as the per-connection instance of the key-value store.
+  std::unordered_map<std::string, const std::vector<uint8_t>> key_value_store_ = {};
+};
+
+int main(int argc, char** argv) {
+  FX_LOGS(INFO) << "Started";
+
+  // The event loop is used to asynchronously listen for incoming connections and requests from the
+  // client. The following initializes the loop, and obtains the dispatcher, which will be used when
+  // binding the server implementation to a channel.
+  async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
+  async_dispatcher_t* dispatcher = loop.dispatcher();
+
+  // Create an |OutgoingDirectory| instance.
+  //
+  // The |component::OutgoingDirectory| class serves the outgoing directory for our component. This
+  // directory is where the outgoing FIDL protocols are installed so that they can be provided to
+  // other components.
+  component::OutgoingDirectory outgoing = component::OutgoingDirectory::Create(dispatcher);
+
+  // The `ServeFromStartupInfo()` function sets up the outgoing directory with the startup handle.
+  // The startup handle is a handle provided to every component by the system, so that they can
+  // serve capabilities (e.g. FIDL protocols) to other components.
+  zx::status result = outgoing.ServeFromStartupInfo();
+  if (result.is_error()) {
+    FX_LOGS(ERROR) << "Failed to serve outgoing directory: " << result.status_string();
+    return -1;
+  }
+
+  // Register a handler for components trying to connect to |Store|.
+  result = outgoing.AddProtocol<examples_keyvaluestore_baseline::Store>(
+      [dispatcher](fidl::ServerEnd<examples_keyvaluestore_baseline::Store> server_end) {
+        // Create an instance of our StoreImpl that destroys itself when the connection closes.
+        new StoreImpl(dispatcher, std::move(server_end));
+      });
+  if (result.is_error()) {
+    FX_LOGS(ERROR) << "Failed to add Store protocol: " << result.status_string();
+    return -1;
+  }
+
+  // Everything is wired up. Sit back and run the loop until an incoming connection wakes us up.
+  FX_LOGS(INFO) << "Listening for incoming connections";
+  loop.Run();
+  return 0;
+}
