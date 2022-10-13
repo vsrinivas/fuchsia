d869f97474f6451f0254a39f95060164b48ba5cf
Tamir Duberstein <tamird@google.com>
2022-10-13 23:52:40 +0000

    [amlogic_decoder][tests] Use typed LLCPP
    Bug: 69585
    Change-Id: I1c6f5ad9f78bb19eb1628e60bd4dc131cbaf9245
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/740506
    Fuchsia-Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/media/drivers/amlogic_decoder/tests/runner/test_runner.cc
+++ src/media/drivers/amlogic_decoder/tests/runner/test_runner.cc
@@ -5,7 +5,11 @@
 #include <fidl/fuchsia.device/cpp/wire.h>
 #include <fidl/fuchsia.hardware.mediacodec/cpp/wire.h>
 #include <lib/fdio/directory.h>
+#include <lib/sys/component/cpp/service_client.h>
 #include <lib/zx/channel.h>
+#include <zircon/errors.h>
+
+#include <iostream>
 
 #include <zxtest/zxtest.h>
 
@@ -15,49 +19,49 @@ namespace test {
 // TODO(fxbug.dev/104928) Remove the calls to fuchsia.device/Controller once upgraded to DFv2
 class TestDeviceBase {
  public:
-  TestDeviceBase() {}
-
-  void InitializeFromFileName(const char* device_name) {
-    zx::channel server_endpoint, client_endpoint;
-    EXPECT_EQ(ZX_OK, zx::channel::create(0, &server_endpoint, &client_endpoint));
-
-    EXPECT_EQ(ZX_OK, fdio_service_connect(device_name, server_endpoint.release()));
-    channel_ = std::move(client_endpoint);
+  static zx::status<TestDeviceBase> CreateFromFileName(std::string_view path) {
+    zx::status client_end =
+        component::Connect<fuchsia_device::Controller>(std::string(path).c_str());
+    if (client_end.is_error()) {
+      return client_end.take_error();
+    }
+    return zx::ok(TestDeviceBase(std::move(client_end.value())));
   }
 
   // Get a channel to the parent device, so we can rebind the driver to it. This
   // can require sandbox access to /dev/sys.
-  zx::channel GetParentDevice() {
-    char path[fuchsia_device::wire::kMaxDevicePathLen + 1];
-    auto res = fidl::WireCall<fuchsia_device::Controller>(channel())->GetTopologicalPath();
-
-    EXPECT_EQ(ZX_OK, res.status());
-    EXPECT_TRUE(res->is_ok());
-
-    auto& response = *res->value();
-    EXPECT_LE(response.path.size(), fuchsia_device::wire::kMaxDevicePathLen);
-
-    memcpy(path, response.path.data(), response.path.size());
-    path[response.path.size()] = 0;
-    fprintf(stderr, "Device path: %s\n", path);
+  zx::status<TestDeviceBase> GetParentDevice() const {
+    const fidl::WireResult result = fidl::WireCall(client_end_)->GetTopologicalPath();
+    if (!result.ok()) {
+      return zx::error(result.status());
+    }
+    const fit::result response = result.value();
+    if (response.is_error()) {
+      return zx::error(response.error_value());
+    }
+    std::string_view path = (*response.value()).path.get();
+    std::cout << "device path: " << path << std::endl;
     // Remove everything after the final slash.
-    *strrchr(path, '/') = 0;
-    printf("Parent device path: %s\n", path);
-    zx::channel local_channel, remote_channel;
-    EXPECT_EQ(ZX_OK, zx::channel::create(0u, &local_channel, &remote_channel));
-
-    EXPECT_EQ(ZX_OK, fdio_service_connect(path, remote_channel.release()));
-    return local_channel;
+    if (const size_t index = path.rfind('/'); index != std::string::npos) {
+      path = path.substr(0, index);
+    }
+    std::cout << "parent device path: " << path << std::endl;
+    return CreateFromFileName(path);
   }
 
-  static void UnbindChildren(const zx::channel& parent_device) {
-    auto res = fidl::WireCall<fuchsia_device::Controller>(zx::unowned_channel(parent_device))
-                   ->UnbindChildren();
-    EXPECT_EQ(ZX_OK, res.status());
-    EXPECT_TRUE(res->is_ok());
+  zx::status<> UnbindChildren() const {
+    const fidl::WireResult result = fidl::WireCall(client_end_)->UnbindChildren();
+    if (!result.ok()) {
+      return zx::error(result.status());
+    }
+    const fit::result response = result.value();
+    if (response.is_error()) {
+      return zx::error(response.error_value());
+    }
+    return zx::ok();
   }
 
-  static bool BindDriver(const zx::channel& parent_device, const std::string& path) {
+  zx::status<> BindDriver(std::string_view path) const {
     // Rebinding the device immediately after unbinding it sometimes causes the new device to be
     // created before the old one is released, which can cause problems since the old device can
     // hold onto interrupts and other resources. Delay recreation to make that less likely.
@@ -70,79 +74,98 @@ class TestDeviceBase {
     while (retry_count++ < kMaxRetryCount) {
       // Don't use rebind because we need the recreate delay above. Also, the parent device may have
       // other children that shouldn't be unbound.
-      auto res = fidl::WireCall<fuchsia_device::Controller>(zx::unowned_channel(parent_device))
-                     ->Bind(fidl::StringView::FromExternal(path));
-      EXPECT_EQ(ZX_OK, res.status());
-      if (res->is_error() && res->error_value() == ZX_ERR_ALREADY_BOUND) {
+      const fidl::WireResult result =
+          fidl::WireCall(client_end_)->Bind(fidl::StringView::FromExternal(path));
+      if (!result.ok()) {
+        return zx::error(result.status());
+      }
+      const fit::result response = result.value();
+      if (response.is_error()) {
+        if (zx_status_t status = response.error_value(); status != ZX_ERR_ALREADY_BOUND) {
+          return zx::error(status);
+        }
         zx::nanosleep(zx::deadline_after(zx::msec(10)));
         continue;
       }
-      // This only returns true if the test driver finished binding, which means it successfully ran
-      // its built-in tests.  Else the test driver won't succeed binding.
-      return res->is_ok();
+      return zx::ok();
     }
-    fprintf(stderr, "Timed out rebinding driver\n");
-    return false;
+    return zx::error(ZX_ERR_TIMED_OUT);
   }
 
-  void Unbind() {
-    auto res = fidl::WireCall<fuchsia_device::Controller>(channel())->ScheduleUnbind();
-    fprintf(stderr, "Result: %d\n", res.status());
+  zx::status<> Unbind() const {
+    const fidl::WireResult result = fidl::WireCall(client_end_)->ScheduleUnbind();
+    if (!result.ok()) {
+      return zx::error(result.status());
+    }
+    const fit::result response = result.value();
+    if (response.is_error()) {
+      return zx::error(response.error_value());
+    }
+    return zx::ok();
   }
 
-  zx::unowned_channel channel() { return zx::unowned_channel(channel_); }
-  ~TestDeviceBase() {}
+  fidl::UnownedClientEnd<fuchsia_hardware_mediacodec::Tester> tester() const {
+    return fidl::UnownedClientEnd<fuchsia_hardware_mediacodec::Tester>(
+        client_end_.channel().borrow());
+  }
 
  private:
-  zx::channel channel_;
+  explicit TestDeviceBase(fidl::ClientEnd<fuchsia_device::Controller> client_end)
+      : client_end_(std::move(client_end)) {}
+
+  fidl::ClientEnd<fuchsia_device::Controller> client_end_;
 };
+
+constexpr std::string_view kTopologicalPath = "/dev/aml-video/amlogic_video";
 
 // Requires the driver to be in the system image, so disabled by default.
 TEST(TestRunner, DISABLED_RunTests) {
-  auto test_device = std::make_unique<TestDeviceBase>();
-  test_device->InitializeFromFileName("/dev/aml-video/amlogic_video");
-  zx::channel parent_device = test_device->GetParentDevice();
-  test_device = nullptr;
+  zx::status test_device1 = TestDeviceBase::CreateFromFileName(kTopologicalPath);
+  ASSERT_OK(test_device1.status_value());
 
-  TestDeviceBase::UnbindChildren(parent_device);
+  zx::status parent_device = test_device1.value().GetParentDevice();
+  ASSERT_OK(parent_device.status_value());
 
-  bool success =
-      TestDeviceBase::BindDriver(parent_device, "/system/driver/amlogic_video_decoder_test.so");
-  EXPECT_TRUE(success);
-  auto test_device2 = std::make_unique<TestDeviceBase>();
-  test_device2->InitializeFromFileName("/dev/aml-video/test_amlogic_video");
+  ASSERT_OK(parent_device.value().UnbindChildren().status_value());
+  ASSERT_OK(parent_device.value()
+                .BindDriver("/system/driver/amlogic_video_decoder_test.so")
+                .status_value());
 
-  zx::channel local, remote;
-  ASSERT_OK(zx::channel::create(0, &local, &remote));
-  ASSERT_OK(fdio_service_connect("/tmp", remote.release()));
-  auto set_output_res = fidl::WireCall<fuchsia_hardware_mediacodec::Tester>(test_device2->channel())
-                            ->SetOutputDirectoryHandle(std::move(local));
-  EXPECT_OK(set_output_res.status());
-  auto res =
-      fidl::WireCall<fuchsia_hardware_mediacodec::Tester>(test_device2->channel())->RunTests();
-  EXPECT_OK(res.status());
-  if (res.ok())
-    EXPECT_OK(res.value().result);
+  zx::status test_device2 = TestDeviceBase::CreateFromFileName("/dev/aml-video/test_amlogic_video");
+  ASSERT_OK(test_device2.status_value());
+
+  zx::channel client, server;
+  ASSERT_OK(zx::channel::create(0, &client, &server));
+  ASSERT_OK(fdio_open("/tmp", static_cast<uint32_t>(fuchsia_io::OpenFlags::kRightWritable),
+                      server.release()));
+  ASSERT_OK(fidl::WireCall(test_device2.value().tester())
+                ->SetOutputDirectoryHandle(std::move(client))
+                .status());
+
+  const fidl::WireResult result = fidl::WireCall(test_device2.value().tester())->RunTests();
+  ASSERT_OK(result.status());
+  const fidl::WireResponse response = result.value();
+  ASSERT_OK(response.result);
 
   // UnbindChildren seems to block for some reason.
-  test_device2->Unbind();
-  test_device2 = nullptr;
+  ASSERT_OK(test_device2.value().Unbind().status_value());
 
   // Try to rebind the correct driver.
-  TestDeviceBase::BindDriver(parent_device, "");
+  ASSERT_OK(parent_device.value().BindDriver({}).status_value());
 }
 
 // Test that unbinding and rebinding the driver works.
 TEST(TestRunner, Rebind) {
-  auto test_device = std::make_unique<TestDeviceBase>();
-  test_device->InitializeFromFileName("/dev/aml-video/amlogic_video");
-  zx::channel parent_device = test_device->GetParentDevice();
-  test_device = nullptr;
+  zx::status test_device1 = TestDeviceBase::CreateFromFileName(kTopologicalPath);
+  ASSERT_OK(test_device1.status_value());
 
-  TestDeviceBase::UnbindChildren(parent_device);
+  zx::status parent_device = test_device1.value().GetParentDevice();
+  ASSERT_OK(parent_device.status_value());
+
+  ASSERT_OK(parent_device.value().UnbindChildren().status_value());
 
   // Use autobind to bind same driver.
-  TestDeviceBase::BindDriver(parent_device, "");
+  ASSERT_OK(parent_device.value().BindDriver({}).status_value());
 }
 
 }  // namespace test
