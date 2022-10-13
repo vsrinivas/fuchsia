f2d8b8c242d7bf79a08489c62e459dccab08c9ad
Nick Brown <nickbrow@google.com>
2022-10-13 17:04:29 +0000

    [gvisor_syscall_tests] Dedup tcp tests
    The `tcp` and `tcp_blocking` targets were erroneously sharing identical
    tests.
    
    Bug: 106971
    Change-Id: Ifc1579635cc7d837cc8a351b593ead2c1a0034a0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/740228
    Reviewed-by: Evan Wildenhain <wildenhain@google.com>
    Commit-Queue: Nick Brown <nickbrow@google.com>

--- third_party/gvisor_syscall_tests/expects/netstack2/tcp_blocking_expectations.cc
+++ third_party/gvisor_syscall_tests/expects/netstack2/tcp_blocking_expectations.cc
@@ -2,13 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/gvisor_syscall_tests/expects/common.h"
 #include "third_party/gvisor_syscall_tests/expects/expectations.h"
 
 namespace netstack_syscall_test {
 
 void AddNonPassingTests(TestMap& tests) {
-  AddCommonExpectsTcpNetstack2(tests);
   // https://fxbug.dev/46211
   // Deadlock? Test makes no progress even when run in isolation.
   SkipTest(tests, "BlockingTCPSockets/BlockingStreamSocketPairTest.SendMsgTooLarge/*");
--- third_party/gvisor_syscall_tests/expects/netstack2/tcp_expectations.cc
+++ third_party/gvisor_syscall_tests/expects/netstack2/tcp_expectations.cc
@@ -2,13 +2,67 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/gvisor_syscall_tests/expects/common.h"
 #include "third_party/gvisor_syscall_tests/expects/expectations.h"
 
 namespace netstack_syscall_test {
 
 void AddNonPassingTests(TestMap& tests) {
-  AddCommonExpectsTcpNetstack2(tests);
+  // https://fxbug.dev/73028
+  SkipTest(tests, "AllTCPSockets/TCPSocketPairTest.RSTCausesPollHUP/*");
+  // third_party/gvisor/test/syscalls/linux/socket_ip_tcp_generic.cc:125
+  // Value of: RetryEINTR(read)(sockets->first_fd(), buf, sizeof(buf))
+  // Expected: -1 (failure), with errno PosixError(errno=104 0)
+  //   Actual: 0 (of type long)
+  ExpectFailure(tests, "AllTCPSockets/TCPSocketPairTest.RSTSentOnCloseWithUnreadData/*");
+  // https://fxbug.dev/73031
+  SkipTest(tests,
+           "AllTCPSockets/"
+           "TCPSocketPairTest.RSTSentOnCloseWithUnreadDataAllowsReadBuffered/*");
+
+  // https://fxbug.dev/73032
+  ExpectFailure(tests,
+                "AllTCPSockets/"
+                "TCPSocketPairTest."
+                "ShutdownRdUnreadDataShouldCauseNoPacketsUnlessClosed/*");
+  // https://fxbug.dev/70837
+  // Skip this test as it flakes often because of reaching file descriptor
+  // resource limits on Fuchsia. Bumping up the resource limit in Fuchsia might
+  // be more involved.
+  SkipTest(tests, "AllTCPSockets/TCPSocketPairTest.TCPResetDuringClose/*");
+  // https://fxbug.dev/20628
+  ExpectFailure(tests, "AllTCPSockets/TCPSocketPairTest.MsgTruncMsgPeek/*");
+
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.BasicSendmmsg/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.BasicRecvmmsg/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.RecvmmsgTimeoutBeforeRecv/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.RecvmmsgInvalidTimeout/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.SendmmsgIsLimitedByMAXIOV/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.SendmsgRecvmsg10KB/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.SendmsgRecvmsg16KB/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.SendmsgRecvmsgMsgCtruncNoop/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.RecvmsgMsghdrFlagsCleared/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.RecvmsgPeekMsghdrFlagsCleared/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.RecvWaitAll/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.RecvWaitAllDontWait/*");
+  // Fuchsia does not support Unix sockets.
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.RecvTimeoutWaitAll/*");
+
+  // https://fxbug.dev/74836
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.SetAndGetBooleanSocketOptions/*");
+  // https://fxbug.dev/74639
+  ExpectFailure(tests, "AllUnixDomainSockets/AllSocketPairTest.GetSetSocketRcvlowatOption/*");
   // https://fxbug.dev/20628
   ExpectFailure(tests, "AllInetTests/TcpSocketTest.MsgTrunc/*");
   // https://fxbug.dev/20628
