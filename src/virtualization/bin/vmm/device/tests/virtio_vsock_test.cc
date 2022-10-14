d9bdd70a3d6e5dccaa9eff11cbce5067562ce241
Tim Detwiler <tjdetwiler@google.com>
2022-10-14 04:59:40 +0000

    [virtualization] Move virtio_vsock_test
    Change-Id: I875d5aacd2c20e7334375fe7de0932047747782f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/739768
    Commit-Queue: Tim Detwiler <tjdetwiler@google.com>
    Reviewed-by: David Hastings <dahastin@google.com>

--- src/virtualization/bin/vmm/device/tests/virtio_vsock_test.cc
+++ src/virtualization/bin/vmm/device/tests/virtio_vsock_test.cc
@@ -6,9 +6,10 @@
 #include <fuchsia/tracing/provider/cpp/fidl.h>
 #include <lib/sys/component/cpp/testing/realm_builder.h>
 
-#include <src/virtualization/bin/vmm/device/tests/test_with_device.h>
-#include <src/virtualization/bin/vmm/device/tests/virtio_queue_fake.h>
 #include <virtio/vsock.h>
+
+#include "src/virtualization/bin/vmm/device/tests/test_with_device.h"
+#include "src/virtualization/bin/vmm/device/tests/virtio_queue_fake.h"
 
 namespace {
 
