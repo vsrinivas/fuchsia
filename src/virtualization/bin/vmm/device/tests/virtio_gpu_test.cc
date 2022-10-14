b6b8faf2c16f1e1b2104940322cc3d9458d1b41d
Tim Detwiler <tjdetwiler@google.com>
2022-10-14 04:00:32 +0000

    [virtualization] Move virtio_gpu_test target
    Change-Id: I2869f9b2097f468c6962e6427e33c9b1a97dd2c3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/738985
    Reviewed-by: David Hastings <dahastin@google.com>
    Commit-Queue: Tim Detwiler <tjdetwiler@google.com>

--- src/virtualization/bin/vmm/device/tests/virtio_gpu_test.cc
+++ src/virtualization/bin/vmm/device/tests/virtio_gpu_test.cc
@@ -3,18 +3,18 @@
 // found in the LICENSE file.
 
 #include <fuchsia/element/cpp/fidl.h>
+#include <fuchsia/logger/cpp/fidl.h>
 #include <fuchsia/sysmem/cpp/fidl.h>
 #include <fuchsia/tracing/provider/cpp/fidl.h>
 #include <fuchsia/ui/composition/cpp/fidl.h>
 #include <fuchsia/ui/scenic/cpp/fidl_test_base.h>
+#include <fuchsia/virtualization/hardware/cpp/fidl.h>
 #include <lib/sys/component/cpp/testing/realm_builder.h>
 #include <lib/sys/component/cpp/testing/realm_builder_types.h>
 #include <lib/zx/status.h>
 
 #include <virtio/gpu.h>
 
-#include "fuchsia/logger/cpp/fidl.h"
-#include "fuchsia/virtualization/hardware/cpp/fidl.h"
 #include "src/ui/testing/ui_test_manager/ui_test_manager.h"
 #include "src/virtualization/bin/vmm/device/tests/test_with_device.h"
 #include "src/virtualization/bin/vmm/device/tests/virtio_queue_fake.h"
