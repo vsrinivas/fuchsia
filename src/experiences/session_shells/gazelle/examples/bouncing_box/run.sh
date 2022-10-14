c22cf30ed28ef6c8c98094e443e7c39c332e2824
Sanjay Chouksey <sanjayc@google.com>
2022-10-14 22:20:23 +0000

    [gazelle] Enable launching example apps from element manager
    Element manager assumes a graphical app will implement ViewProvider and
    thus expose it from its CML file. The example apps built using AppKit is
    eschewing ViewProvider, instead calling GraphicalPresenter directly as
    it provides richer semantics (annotations) and multiple windows per
    component.
    
    This change adds ViewProvider to the CML file to allow launching using
    `ffx session add <app_url>`.
    
    Change-Id: I67abf2b7289cbacaa3a31f2ab7370bb94fa71034
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/740703
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Sanjay Chouksey <sanjayc@google.com>
    Reviewed-by: Charles Whitten <cwhitten@google.com>

--- src/experiences/session_shells/gazelle/examples/bouncing_box/run.sh
+++ src/experiences/session_shells/gazelle/examples/bouncing_box/run.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
 # Copyright 2022 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-ffx component destroy /core/session-manager/session:session/workstation_session/login_shell/ermine_shell/elements:bouncing_box
-ffx component create /core/session-manager/session:session/workstation_session/login_shell/ermine_shell/elements:bouncing_box fuchsia-pkg://fuchsia.com/bouncing_box#meta/bouncing_box.cm
-ffx component start /core/session-manager/session:session/workstation_session/login_shell/ermine_shell/elements:bouncing_box
+ffx session add fuchsia-pkg://fuchsia.com/bouncing_box#meta/bouncing_box.cm
