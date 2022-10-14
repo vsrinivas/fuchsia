e866a52f16be200380d20046fe8721f04543d0cd
Nick Brown <nickbrow@google.com>
2022-10-14 18:58:39 +0000

    [gvisor_syscall_tests] Split generic syscall tests
    Define the same test targets as were introduced in
    https://fxrev.dev/735167. This change does not run the targets in CQ due
    to flakes arising from that change. We'll run in CQ in a later change
    after further analysis of the flakes.
    
    Bug: 106971
    Change-Id: I09ce4d0744a6e673a965fec05a008e1689c34848
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/740693
    Reviewed-by: Evan Wildenhain <wildenhain@google.com>
    Fuchsia-Auto-Submit: Nick Brown <nickbrow@google.com>
    Commit-Queue: Nick Brown <nickbrow@google.com>

---
+++ third_party/gvisor_syscall_tests/expects/netstack3/tcp_backlog_expectations.cc
@@ -0,0 +1,87 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "third_party/gvisor_syscall_tests/expects/common.h"
+#include "third_party/gvisor_syscall_tests/expects/expectations.h"
+
+namespace netstack_syscall_test {
+
+void AddNonPassingTests(TestMap& tests) {
+  AddSkippedTestsTcpBacklog(tests);
+  SkipTest(tests, "All/SocketInetLoopbackTest.TCPAcceptBacklogSizes/*");
+
+  ExpectFailure(tests, "All/SocketInetLoopbackTest.TCPBacklog/*");
+
+  // Dual-stack TCP sockets are not supported.
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Any_ConnectV4MappedAny");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Any_ConnectV4MappedAny");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Any_ConnectV4MappedLoopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Any_ConnectV4MappedLoopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Loopback_ConnectV4MappedLoopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Loopback_ConnectV4MappedLoopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4MappedAny_ConnectV4Any");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4MappedAny_ConnectV4Loopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4MappedAny_ConnectV4MappedAny");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4MappedAny_ConnectV4MappedLoopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4MappedLoopback_ConnectV4Any");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4MappedLoopback_ConnectV4Loopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4MappedLoopback_ConnectV4MappedLoopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV6Any_ConnectV4Any");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV6Any_ConnectV4MappedAny");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV6Any_ConnectV4MappedLoopback");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV6Any_ConnectV4Loopback");
+
+  // Netstack3 does not yet follow the Linux/BSD convention that connecting to
+  // the unspecified address is equivalent to connecting to loopback.
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Any_ConnectV4Any");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV4Loopback_ConnectV4Any");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV6Any_ConnectV6Any");
+  ExpectFailure(tests,
+                "All/SocketInetLoopbackTest.TCPBacklogAcceptAll/"
+                "ListenV6Loopback_ConnectV6Any");
+}  // NOLINT(readability/fn_size)
+
+}  // namespace netstack_syscall_test
