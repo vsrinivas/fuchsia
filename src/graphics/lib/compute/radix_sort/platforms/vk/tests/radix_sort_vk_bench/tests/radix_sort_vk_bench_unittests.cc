58d87e0d8ead9ab3760476c7db6cbf33dbe7d8c9
Allan MacKinnon <allanmac@google.com>
2022-10-17 17:32:19 +0000

    [graphics][compute][radix sort] Add more intensive tests
    Test sorting from 256K to 1M keyvals in steps of 256K.
    
    This test is repeated for both u32 and u64 keyvals as well as direct
    and indirect dispatch.  This adds 4 new test.
    
    Testability:
    
    Tests are on CQ.
    
    Explicitly run VIM3, G31 and G52 tryjobs.
    
    Change-Id: Ie8c81eaecc4da479ffc257c9f9d51878616d7296
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/741942
    Commit-Queue: Allan MacKinnon <allanmac@google.com>
    Reviewed-by: John Bauman <jbauman@google.com>

--- src/graphics/lib/compute/radix_sort/platforms/vk/tests/radix_sort_vk_bench/tests/radix_sort_vk_bench_unittests.cc
+++ src/graphics/lib/compute/radix_sort/platforms/vk/tests/radix_sort_vk_bench/tests/radix_sort_vk_bench_unittests.cc
@@ -95,6 +95,60 @@ TEST(RadixSortVK, Indirect_U32_Range_0_to_8)
 //
 //
 //
+TEST(RadixSortVK, Direct_U32_Range_256K_to_1M)
+{
+  struct radix_sort_vk_bench_info info = {
+    .exec_name      = "radix_sort_vk_bench",
+    .vendor_id      = 0,
+    .device_id      = 0,
+    .target_name    = NULL,
+    .keyval_dwords  = 1,
+    .is_direct      = true,
+    .is_indirect    = false,
+    .count_lo       = 1u << 18,
+    .count_hi       = 1u << 20,
+    .count_step     = 1u << 18,
+    .loops          = 1,
+    .warmup         = 0,
+    .is_verify      = true,
+    .is_validation  = true,
+    .is_debug_utils = true,
+    .is_verbose     = false,
+  };
+
+  EXPECT_EQ(radix_sort_vk_bench(&info), EXIT_SUCCESS);
+}
+
+//
+//
+//
+TEST(RadixSortVK, Indirect_U32_Range_256K_to_1M)
+{
+  struct radix_sort_vk_bench_info info = {
+    .exec_name      = "radix_sort_vk_bench",
+    .vendor_id      = 0,
+    .device_id      = 0,
+    .target_name    = NULL,
+    .keyval_dwords  = 1,
+    .is_direct      = false,
+    .is_indirect    = true,
+    .count_lo       = 1u << 18,
+    .count_hi       = 1u << 20,
+    .count_step     = 1u << 18,
+    .loops          = 1,
+    .warmup         = 0,
+    .is_verify      = true,
+    .is_validation  = true,
+    .is_debug_utils = true,
+    .is_verbose     = false,
+  };
+
+  EXPECT_EQ(radix_sort_vk_bench(&info), EXIT_SUCCESS);
+}
+
+//
+//
+//
 TEST(RadixSortVK, Direct_U64_Range_0_to_8)
 {
   struct radix_sort_vk_bench_info info = {
@@ -137,6 +191,60 @@ TEST(RadixSortVK, Indirect_U64_Range_0_to_8)
     .count_step     = 1,
     .loops          = 1,
     .warmup         = 1,
+    .is_verify      = true,
+    .is_validation  = true,
+    .is_debug_utils = true,
+    .is_verbose     = false,
+  };
+
+  EXPECT_EQ(radix_sort_vk_bench(&info), EXIT_SUCCESS);
+}
+
+//
+//
+//
+TEST(RadixSortVK, Direct_U64_Range_256K_to_1M)
+{
+  struct radix_sort_vk_bench_info info = {
+    .exec_name      = "radix_sort_vk_bench",
+    .vendor_id      = 0,
+    .device_id      = 0,
+    .target_name    = NULL,
+    .keyval_dwords  = 2,
+    .is_direct      = true,
+    .is_indirect    = false,
+    .count_lo       = 1u << 18,
+    .count_hi       = 1u << 20,
+    .count_step     = 1u << 18,
+    .loops          = 1,
+    .warmup         = 0,
+    .is_verify      = true,
+    .is_validation  = true,
+    .is_debug_utils = true,
+    .is_verbose     = false,
+  };
+
+  EXPECT_EQ(radix_sort_vk_bench(&info), EXIT_SUCCESS);
+}
+
+//
+//
+//
+TEST(RadixSortVK, Indirect_U64_Range_256K_to_1M)
+{
+  struct radix_sort_vk_bench_info info = {
+    .exec_name      = "radix_sort_vk_bench",
+    .vendor_id      = 0,
+    .device_id      = 0,
+    .target_name    = NULL,
+    .keyval_dwords  = 2,
+    .is_direct      = false,
+    .is_indirect    = true,
+    .count_lo       = 1u << 18,
+    .count_hi       = 1u << 20,
+    .count_step     = 1u << 18,
+    .loops          = 1,
+    .warmup         = 0,
     .is_verify      = true,
     .is_validation  = true,
     .is_debug_utils = true,
