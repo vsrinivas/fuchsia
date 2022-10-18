2e666819bd532a38a81221a60899d47f919b53bf
Gianfranco Valentino <gevalentino@google.com>
2022-10-18 18:54:41 +0000

    [zxc] Add lib/zx/stub.h
    Temporary header to used to allow migration while preserving
    git history of original file.
    
    Bug: 112096
    Test: fx build
    Change-Id: I57ab35c1a6ec7fe867246828b293244fee0f8c85
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/742088
    Commit-Queue: Gianfranco Valentino <gevalentino@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>

---
+++ zircon/system/ulib/zxc/include/lib/zx/stub.h
@@ -0,0 +1,13 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LIB_ZX_STUB_H_
+#define LIB_ZX_STUB_H_
+
+// The purpose of this header is to aid in migration while preserving git history
+// of status.h
+
+#include <lib/zx/status.h>
+
+#endif  // LIB_ZX_STUB_H_
