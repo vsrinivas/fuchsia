f022d8804bf4d661ea9d90de59cdd60ff9de8466
Satsuki Ueno <satsukiu@google.com>
2022-10-18 13:48:31 +0000

    [logs] Use user-specified severity as default
    Use any value specified by the devloper as the default severity value
    instead of a hardcoded INFO value. This is useful for cases where
    developers need finer grained logs, such as in tests.
    
    Change-Id: I7b91cf35b6c1fb857b92dfa1e4bf5f1ac99933d8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/742051
    Commit-Queue: Satsuki Ueno <satsukiu@google.com>
    Reviewed-by: Miguel Flores <miguelfrde@google.com>

--- src/lib/diagnostics/log/rust/src/filter.rs
+++ src/lib/diagnostics/log/rust/src/filter.rs
@@ -21,10 +21,11 @@ impl InterestFilter {
     /// Constructs a new `InterestFilter` and a future which should be polled to listen
     /// to changes in the LogSink's interest.
     pub fn new(proxy: LogSinkProxy, interest: Interest) -> (Self, impl Future<Output = ()>) {
-        let min_severity = Arc::new(RwLock::new(interest.min_severity.unwrap_or(Severity::Info)));
+        let default_severity = interest.min_severity.unwrap_or(Severity::Info);
+        let min_severity = Arc::new(RwLock::new(default_severity));
         let listener = Arc::new(Mutex::new(None));
         let filter = Self { min_severity: min_severity.clone(), listener: listener.clone() };
-        (filter, Self::listen_to_interest_changes(listener, min_severity, proxy))
+        (filter, Self::listen_to_interest_changes(listener, default_severity, min_severity, proxy))
     }
 
     /// Sets the interest listener.
@@ -37,11 +38,12 @@ impl InterestFilter {
 
     async fn listen_to_interest_changes(
         listener: Arc<Mutex<Option<Box<dyn OnInterestChanged + Send + Sync>>>>,
+        default_severity: Severity,
         min_severity: Arc<RwLock<Severity>>,
         proxy: LogSinkProxy,
     ) {
         while let Ok(Ok(interest)) = proxy.wait_for_interest_change().await {
-            *min_severity.write() = interest.min_severity.unwrap_or(Severity::Info);
+            *min_severity.write() = interest.min_severity.unwrap_or(default_severity);
             if let Some(callback) = &*listener.lock() {
                 callback.on_changed(&*min_severity.read());
             }
@@ -72,7 +74,9 @@ impl<S: Subscriber> Layer<S> for InterestFilter {
 mod tests {
     use super::*;
     use fidl::endpoints::create_proxy_and_stream;
-    use fidl_fuchsia_logger::LogSinkMarker;
+    use fidl_fuchsia_logger::{LogSinkMarker, LogSinkRequest, LogSinkRequestStream};
+    use fuchsia_async as fasync;
+    use futures::{channel::mpsc, StreamExt, TryStreamExt};
     use std::sync::Mutex;
     use tracing::{debug, error, info, trace, warn, Event};
     use tracing_subscriber::{layer::SubscriberExt, Registry};
@@ -105,8 +109,16 @@ mod tests {
         }
     }
 
+    struct InterestChangedListener(mpsc::UnboundedSender<()>);
+
+    impl OnInterestChanged for InterestChangedListener {
+        fn on_changed(&self, _: &Severity) {
+            self.0.unbounded_send(()).unwrap();
+        }
+    }
+
     #[fuchsia_async::run_singlethreaded(test)]
-    async fn default_filter_is_info() {
+    async fn default_filter_is_info_when_unspecified() {
         let (proxy, _requests) = create_proxy_and_stream::<LogSinkMarker>().unwrap();
         let (filter, _on_changes) = InterestFilter::new(proxy, Interest::EMPTY);
         let observed = Arc::new(Mutex::new(SeverityCount::default()));
@@ -127,6 +139,79 @@ mod tests {
         info!("ok");
         expected.num_info += 1;
         assert_eq!(&*observed.lock().unwrap(), &expected);
+
+        debug!("hint");
+        assert_eq!(&*observed.lock().unwrap(), &expected, "should not increment counters");
+
+        trace!("spew");
+        assert_eq!(&*observed.lock().unwrap(), &expected, "should not increment counters");
+    }
+
+    async fn send_interest_change(stream: &mut LogSinkRequestStream, severity: Option<Severity>) {
+        match stream.try_next().await {
+            Ok(Some(LogSinkRequest::WaitForInterestChange { responder })) => {
+                responder
+                    .send(&mut Ok(Interest { min_severity: severity, ..Interest::EMPTY }))
+                    .expect("send response");
+            }
+            other => panic!("Expected WaitForInterestChange but got {:?}", other),
+        }
+    }
+
+    #[fuchsia_async::run_singlethreaded(test)]
+    async fn default_filter_on_interest_changed() {
+        let (proxy, mut requests) = create_proxy_and_stream::<LogSinkMarker>().unwrap();
+        let (filter, on_changes) = InterestFilter::new(
+            proxy,
+            Interest { min_severity: Some(Severity::Warn), ..Interest::EMPTY },
+        );
+        let (send, mut recv) = mpsc::unbounded();
+        filter.set_interest_listener(InterestChangedListener(send));
+        let _on_changes_task = fasync::Task::spawn(on_changes);
+        let observed = Arc::new(Mutex::new(SeverityCount::default()));
+        tracing::subscriber::set_global_default(
+            Registry::default().with(SeverityTracker { counts: observed.clone() }).with(filter),
+        )
+        .unwrap();
+
+        // After overriding to info, filtering is at info level. The mpsc channel is used to
+        // get a signal as to when the filter has processed the update.
+        send_interest_change(&mut requests, Some(Severity::Info)).await;
+        let _ = recv.next().await;
+
+        let mut expected = SeverityCount::default();
+        error!("oops");
+        expected.num_error += 1;
+        assert_eq!(&*observed.lock().unwrap(), &expected);
+
+        warn!("maybe");
+        expected.num_warn += 1;
+        assert_eq!(&*observed.lock().unwrap(), &expected);
+
+        info!("ok");
+        expected.num_info += 1;
+        assert_eq!(&*observed.lock().unwrap(), &expected);
+
+        debug!("hint");
+        assert_eq!(&*observed.lock().unwrap(), &expected, "should not increment counters");
+
+        trace!("spew");
+        assert_eq!(&*observed.lock().unwrap(), &expected, "should not increment counters");
+
+        // After resetting to default, filtering is at warn level.
+        send_interest_change(&mut requests, None).await;
+        let _ = recv.next().await;
+
+        error!("oops");
+        expected.num_error += 1;
+        assert_eq!(&*observed.lock().unwrap(), &expected);
+
+        warn!("maybe");
+        expected.num_warn += 1;
+        assert_eq!(&*observed.lock().unwrap(), &expected);
+
+        info!("ok");
+        assert_eq!(&*observed.lock().unwrap(), &expected, "should not increment counters");
 
         debug!("hint");
         assert_eq!(&*observed.lock().unwrap(), &expected, "should not increment counters");
