07eef1912e51c800c6e5cf30a82d91394eb25b74
Alexander Radchenko <alerad@google.com>
2022-10-19 03:33:31 +0000

    [virt] Add memory reclaim integration test
    Add the integration test to exercise free page reporting in
    virtio-balloon on both guest and host sides.
    
    Bug: 100504
    Change-Id: I31191a3c4c27d1705c4045753a10251479f44fd3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/734764
    Reviewed-by: Tim Detwiler <tjdetwiler@google.com>
    Commit-Queue: Alexander Radchenko <alerad@google.com>

--- src/virtualization/tests/virtio_balloon_tests.cc
+++ src/virtualization/tests/virtio_balloon_tests.cc
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fidl/fuchsia.kernel/cpp/fidl.h>
+#include <lib/sys/component/cpp/service_client.h>
+
+#include "lib/zx/time.h"
+#include "src/lib/fxl/strings/string_printf.h"
 #include "src/virtualization/tests/lib/guest_test.h"
 
 namespace {
@@ -11,6 +16,14 @@ constexpr size_t kVirtioBalloonPageCount = 256;
 template <class T>
 class BalloonGuestTest : public GuestTest<T> {
  public:
+  BalloonGuestTest() {
+    auto client_end = component::Connect<fuchsia_kernel::Stats>();
+    if (!client_end.is_ok()) {
+      FX_PLOGS(ERROR, client_end.status_value()) << "Failed to connect to kernel stats";
+    }
+    stats.Bind(std::move(*client_end));
+  }
+
   void TestGetMemStats(
       const char* trace_context,
       const fuchsia::virtualization::BalloonControllerSyncPtr& balloon_controller) {
@@ -34,6 +47,16 @@ class BalloonGuestTest : public GuestTest<T> {
     EXPECT_LE(stats[VIRTIO_BALLOON_S_MEMFREE], stats[VIRTIO_BALLOON_S_MEMTOT]);
     EXPECT_LE(stats[VIRTIO_BALLOON_S_AVAIL], stats[VIRTIO_BALLOON_S_MEMTOT]);
   }
+
+  void PrintKernelMemoryStats(std::string_view header) {
+    auto memory_stats = stats->GetMemoryStats();
+    FX_LOGS(INFO) << header
+                  << ": total_bytes=" << memory_stats->stats().total_bytes().value() / 1024 / 1024
+                  << " MiB free_bytes=" << memory_stats->stats().free_bytes().value() / 1024 / 1024
+                  << " MiB";
+  }
+
+  fidl::SyncClient<fuchsia_kernel::Stats> stats;
 };
 
 // Zircon does not yet have a virtio balloon driver.
@@ -87,6 +110,32 @@ TYPED_TEST(BalloonGuestTest, InflateDeflate) {
     }
   }
   this->TestGetMemStats("After deflate", balloon_controller);
+}
+
+TYPED_TEST(BalloonGuestTest, VirtioBalloonFreePageReporting) {
+  const uint64_t starting_free_memory_mib =
+      this->stats->GetMemoryStats()->stats().free_bytes().value() / 1024 / 1024;
+
+  // Allocate 60% of the free memory on the guest side.
+  const uint64_t alloc_amount_mib = starting_free_memory_mib / 10 * 6;
+  this->PrintKernelMemoryStats("Before the guest alloc");
+  FX_LOGS(INFO) << fxl::StringPrintf("Allocate and release %lu MiB in the guest", alloc_amount_mib);
+
+  std::string result;
+  ASSERT_EQ(this->RunUtil(
+                fxl::StringPrintf("memory_test_util alloc --size-mb 1 --num %lu", alloc_amount_mib),
+                {}, &result),
+            ZX_OK);
+
+  this->PrintKernelMemoryStats("After the guest alloc");
+
+  FX_LOGS(INFO) << "Waiting for the virtio balloon to reclaim memory";
+  while (alloc_amount_mib >
+         this->stats->GetMemoryStats()->stats().free_bytes().value() / 1024 / 1024) {
+    zx::nanosleep(zx::deadline_after(zx::msec(100)));
+  }
+
+  this->PrintKernelMemoryStats("After the memory reclaim");
 }
 
 }  // namespace
