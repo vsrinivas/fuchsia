2442f57dc571b0b0a9a6640fe3e3c0574538437c
Stefan Bossbaly <stefanbossbaly@google.com>
2022-10-20 15:40:08 +0000

    [codec][test] Added H.264 decoder conformance test
    Just like with VP9 decoder conformance test, this allows the running of
    local files to verify conformance.
    
    Change-Id: Ib18529cb327dcc0c74e5a55effbb323783e08fad
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/740286
    Reviewed-by: Dustin Green <dustingreen@google.com>
    Commit-Queue: Stefan Bossbaly <stefanbossbaly@google.com>

--- src/media/codec/examples/use_media_decoder/test/h264_decoder_conformance_test.cc
+++ src/media/codec/examples/use_media_decoder/test/h264_decoder_conformance_test.cc
@@ -2,39 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This manual test is a basic integration test of the codec_factory +
-// amlogic_video_decoder driver.
+// This manual test is a basic integration test of the codec_factory + device specific codec
+// implementation.
 //
-// If this test breaks and it's not immediately obvoius why, please feel free to
+// If this test breaks and it's not immediately obvious why, please feel free to
 // involve dustingreen@ (me) in figuring it out.
 
-#include <lib/async-loop/cpp/loop.h>
-#include <lib/async-loop/default.h>
-#include <lib/media/codec_impl/fourcc.h>
-#include <lib/sys/cpp/component_context.h>
-#include <lib/syslog/cpp/macros.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <map>
-#include <set>
-
-#include <openssl/md5.h>
-
-#include "../in_stream_file.h"
-#include "../in_stream_http.h"
-#include "../in_stream_peeker.h"
-#include "../use_video_decoder.h"
-#include "../util.h"
 #include "decoder_conformance_test.h"
-#include "src/lib/fxl/command_line.h"
-#include "src/lib/fxl/log_settings_command_line.h"
 #include "use_video_decoder_test.h"
 
 namespace {
 
-// The actual contents of these files will vary per test package.  See files
-// under prebuilt/test_data/media/third_party/webm_vp9_conformance_streams.
+// The actual contents of these files will vary per test package.
 constexpr char kInputFilePath[] = "/pkg/data/foo.h264";
 constexpr char kI420Md5FilePath[] = "/pkg/data/foo.md5";
 
