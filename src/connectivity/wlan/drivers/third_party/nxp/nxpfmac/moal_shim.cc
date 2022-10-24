9e5bf02839d3e556cb4b43c9fc3cc45cf4e82be7
Bjoern Johansson <bjoernj@google.com>
2022-10-24 19:35:54 +0000

    [nxpfmac] Stop leaking EAPOL frame buffers
    Frames used for EAPOL transmission were not correctly returned to
    storage. This happened because the frames are placed in the same buffer
    used for the data and after transmission they were not extracted out
    correctly. This prevented the Frame object destructor from running which
    in turn meant the frame would not be returned to storage. Eventually
    this lead to a depletion of buffers and the driver would no longer be
    able to function.
    
    Fix this by correctly extracting the Frame object from the buffer after
    transmission, allowing it to be returned on destruction.
    
    Test: WlanSecurityComplianceABGTest ACTS tests
    Change-Id: Ie8fd244c203d7ae4b78973e2a714ad0e43d1ef03
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/745989
    Reviewed-by: Karthik Krishnaswami <karthikrish@google.com>
    Commit-Queue: Bjoern Johansson <bjoernj@google.com>

--- src/connectivity/wlan/drivers/third_party/nxp/nxpfmac/moal_shim.cc
+++ src/connectivity/wlan/drivers/third_party/nxp/nxpfmac/moal_shim.cc
@@ -203,8 +203,13 @@ mlan_status moal_shutdown_fw_complete(t_void *pmoal, mlan_status status) {
 
 mlan_status moal_send_packet_complete(t_void *pmoal, pmlan_buffer pmbuf, mlan_status status) {
   wlan::nxpfmac::DataPlane *plane = static_cast<wlan::nxpfmac::DeviceContext *>(pmoal)->data_plane_;
-  auto frame = reinterpret_cast<wlan::drivers::components::Frame *>(pmbuf->pdesc);
-  plane->CompleteTx(std::move(*frame), status == MLAN_STATUS_SUCCESS ? ZX_OK : ZX_ERR_INTERNAL);
+  // Make sure that we create an actual Frame object here, otherwise it won't be destructed since
+  // it's just stored in the data buffer. This ensures that if the frame is associated with any
+  // storage it will be returned properly.
+  auto frame_ptr = reinterpret_cast<wlan::drivers::components::Frame *>(pmbuf->pdesc);
+  wlan::drivers::components::Frame frame(std::move(*frame_ptr));
+
+  plane->CompleteTx(std::move(frame), status == MLAN_STATUS_SUCCESS ? ZX_OK : ZX_ERR_INTERNAL);
   return MLAN_STATUS_SUCCESS;
 }
 
