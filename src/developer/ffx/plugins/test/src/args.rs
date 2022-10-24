065b7615ab17e0d5fb462bcec0a4b7980ccb3ec5
Satsuki Ueno <satsukiu@google.com>
2022-10-24 23:33:37 +0000

    [ffx][test] Update help output
    Update the help output for ffx test run in preparation for running it
    through a design review. Reordering the options within the struct is
    relevant as the order in the source code determines the order in which
    the options are printed in the help message.
    
    Change-Id: Ifaf341e088f266ca956e64c95cbaf5b2b936de29
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/743082
    Commit-Queue: Satsuki Ueno <satsukiu@google.com>
    Fuchsia-Auto-Submit: Satsuki Ueno <satsukiu@google.com>
    Reviewed-by: Ankur Mittal <anmittal@google.com>

--- src/developer/ffx/plugins/test/src/args.rs
+++ src/developer/ffx/plugins/test/src/args.rs
@@ -29,95 +29,101 @@ pub enum TestSubCommand {
 #[argh(
     subcommand,
     name = "run",
-    description = "Entry point for executing tests",
+    description = "Execute test suites on a target device",
+    example = "\
+Run a test suite:
+    $ ffx test run fuchsia-pkg://fuchsia.com/my_test#meta/my_test.cm
+
+Run a test suite and pass arguments to the suite:
+    $ ffx test run fuchsia-pkg://fuchsia.com/my_test#meta/my_test.cm -- arg1 arg2
+
+Run test suites specified in a JSON file (currently unstable):
+    $ ffx test run --test-file test-list.json
+
+Given a suite that contains the test cases 'Foo.Test1', 'Foo.Test2',
+'Bar.Test1', and 'Bar.Test2':
+
+Run test cases that start with 'Foo.' ('Foo.Test1', 'Foo.Test2'):
+    $ ffx test run <suite-url> --test-filter 'Foo.*'
+
+Run test cases that do not start with 'Foo.' ('Bar.Test1', 'Bar.Test2'):
+    $ ffx test run <suite-url> --test-filter '-Foo.*'
+
+Run test cases that start with 'Foo.' and do not end with 'Test1' ('Foo.Test2'):
+    $ ffx test run <suite-url> --test-filter 'Foo.*' --test-filter '-*.Test1'",
     note = "Runs test suites implementing the `fuchsia.test.Suite` protocol.
 
-Tests are specified either as a command line argument, or via JSON file.
-
-For example, when specifying a test via command line:
-    ffx test run fuchsia-pkg://fuchsia.com/my_test#meta/my_test.cm
-When specifying a test via command line and passing arguments to the test:
-    ffx test run fuchsia-pkg://fuchsia.com/my_test#meta/my_test.cm -- arg1 arg2
-
-Specifying tests via JSON file is currently unstable, but may be done with the
---test-file option.
-
-Note that if running multiple iterations of a test and an iteration times
-out, no further iterations will be executed."
+When multiple test suites are run, either through the --count option or through
+--test-file, the default behavior of ffx test is:
+    If any test suite times out, halt execution and do not attempt to run any
+    unstarted suites.
+    If any test suite fails for any other reason, continue to run unstarted
+    suites."
 )]
 pub struct RunCommand {
-    /// test timeout in seconds
-    #[argh(option, short = 't')]
-    pub timeout: Option<u32>,
-
-    /// test url, and any arguments passed to tests, following `--`.
+    /// test suite url, and any arguments passed to tests, following `--`.
     /// When --test-file is specified test_args should not be specified.
     #[argh(positional)]
     pub test_args: Vec<String>,
 
     // TODO(satsukiu): once stable, document the format
-    /// read test url and options from the specified file instead of from the command line.
+    /// read test url and options from the specified file instead of from the
+    /// command line.
     /// Stdin may be specified by passing `--test-file -`.
-    /// May not be used in conjunction with `test_args`, `--test-file`, `--test-filter`,
-    /// `--run-disabled`, `--parallel`, `--max-severity-logs`
-    /// This option is currently unstable and the format of the file is subject to change.
+    /// May not be used in conjunction with `test_args`, `--test-file`,
+    /// `--test-filter`, `--run-disabled`, `--parallel`, `--max-severity-logs`
+    /// This option is currently unstable and the format of the file is subject
+    /// to change. Using this option requires setting the
+    /// 'test.experimental_json_input' configuration to true.
     ///
     /// For current details, see test-list.json format at
     /// https://fuchsia.googlesource.com/fuchsia/+/refs/heads/main/src/lib/testing/test_list/
     #[argh(option)]
     pub test_file: Option<String>,
 
-    /// test filter. Glob pattern for matching tests. Can be
-    /// specified multiple times to pass in multiple patterns.
-    /// Tests may be excluded by prepending a '-' to the glob pattern.
-    /// example: --test-filter glob1 --test-filter glob2 --test-filter -glob3.
+    // suite options
+    /// test suite timeout in seconds.
+    #[argh(option, short = 't')]
+    pub timeout: Option<u32>,
+
+    /// test case filter. This filter will match based on glob pattern. This
+    /// option may be specified multiple times. Only test cases matching at
+    /// least one pattern will be run. Negative filters may be specified by
+    /// prepending '-' and will exclude matching test cases.
     #[argh(option)]
     pub test_filter: Vec<String>,
 
-    /// whether to also run tests that have been marked disabled/ignored
-    /// by the test author.
+    /// also execute test cases that have been disabled by the test author.
     #[argh(switch)]
     pub run_disabled: bool,
 
-    /// whether to filter ANSI escape sequences from output
-    #[argh(switch)]
-    pub filter_ansi: bool,
+    /// maximum number of test cases to run in parallel. Defaults to a value
+    /// specified by the test runner.
+    #[argh(option)]
+    pub parallel: Option<u16>,
 
+    /// when set, fails tests that emit logs with a higher severity.
+    ///
+    /// For example, if --max-severity-logs WARN is specified, fails any test
+    /// that produces an ERROR level log.
+    #[argh(option)]
+    pub max_severity_logs: Option<Severity>,
+
+    // test run options
     /// continue running unfinished suites if a suite times out.
-    /// By default, unfinished suites are immediately terminated if a suite times out.
     /// This option is only relevant when multiple suites are run.
     #[argh(switch)]
     pub continue_on_timeout: bool,
 
-    /// stop running unfinished suites after the number of provided failures has occurred.
-    /// By default, all suites are run to completion if a suite fails.
+    /// stop running unfinished suites after the number of provided failures
+    /// has occurred. This option is only relevant when multiple suites are
+    /// run.
     #[argh(option)]
     pub stop_after_failures: Option<u32>,
 
-    /// run tests in parallel, up to the number provided.
-    #[argh(option)]
-    pub parallel: Option<u16>,
-
-    /// number of times to run the test. By default run 1 time. If an iteration
-    /// of the test times out, no further iterations are executed.
+    /// number of times to run the test suite. By default run the suite 1 time.
     #[argh(option)]
     pub count: Option<u32>,
-
-    /// when set, only logs with a severity equal to the given one or higher will be printed.
-    #[argh(option)]
-    pub min_severity_logs: Option<Severity>,
-
-    /// when set, the test will fail if any log with a higher severity is emitted.
-    #[argh(option)]
-    pub max_severity_logs: Option<Severity>,
-
-    /// when set, shows the full moniker in unstructured log output.
-    #[argh(switch)]
-    pub show_full_moniker_in_logs: bool,
-
-    /// when set, output test results to the specified directory.
-    #[argh(option)]
-    pub output_directory: Option<String>,
 
     /// enables experimental parallel test scheduler. The provided number
     /// specifies the max number of test suites to run in parallel.
@@ -126,7 +132,32 @@ pub struct RunCommand {
     #[argh(option)]
     pub experimental_parallel_execution: Option<u16>,
 
-    /// when set, disables structured output to a directory.
+    // output options
+    /// filter ANSI escape sequences from output.
+    #[argh(switch)]
+    pub filter_ansi: bool,
+
+    /// set the minimum log severity printed. Must be one of
+    /// FATAL|ERROR|WARN|INFO|DEBUG|TRACE.
+    #[argh(option)]
+    pub min_severity_logs: Option<Severity>,
+
+    /// show the full moniker in unstructured log output.
+    #[argh(switch)]
+    pub show_full_moniker_in_logs: bool,
+
+    /// output test results to the specified directory. The produced output
+    /// is in the format described in
+    /// https://fuchsia.dev/fuchsia-src/reference/platform-spec/testing/test-output-format
+    #[argh(option)]
+    pub output_directory: Option<String>,
+
+    /// disable structured output to a directory. Note that normally,
+    /// structured output is disabled by default and is only produced when
+    /// --output-directory is specified. --disable-output-directory is only
+    /// relevant when the 'test.experimental_managed_structured_output'
+    /// experimental configuration is set true, which changes the default
+    /// behavior to produce structured output.
     #[argh(switch)]
     pub disable_output_directory: bool,
 }
