98d29139857e60c68b85f6d95944ff11a83f29dd
Mike McCreavy <mccreavy@google.com>
2022-10-27 19:55:05 +0000

    [Gumshoe] Register templates via directory scan.
    Scan Gumshoe component's /templates/ directory for
    files ending ".hbs.html" and register them as templates.
    
    This gets rid of having to enumerate the templates
    during initial setup. Requested in review of fxr/738608.
    
    go/fuchsia-gumshoe-proposal
    
    BUG=b/253514278
    
    Change-Id: I3f70d7cb51ef4ed4b689be2d9a919c964786dd45
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/742092
    Reviewed-by: Eric Stone <ecstone@google.com>
    Reviewed-by: Mike Comfoltey <comfoltey@google.com>
    Commit-Queue: Mike McCreavy <mccreavy@google.com>

--- src/recovery/diagnostics/gumshoe/src/handlebars_utils.rs
+++ src/recovery/diagnostics/gumshoe/src/handlebars_utils.rs
@@ -3,9 +3,14 @@
 // found in the LICENSE file.
 
 use crate::device_info::DeviceInfoImpl;
-use anyhow::Error;
+use anyhow::{anyhow, Error};
 use handlebars::Handlebars;
 use mockall::automock;
+use std::path::Path;
+
+const FILE_NAME_NOT_AVAILABLE_ERROR: &str = "file_name not available";
+const FILE_NAME_NOT_UTF8_ERROR: &str = "file_name not UTF8";
+const FILE_NAME_MISSING_SUFFIX_ERROR: &str = "file_name missing suffix";
 
 /// Trait version of Handlebars useful for mocking.
 #[automock]
@@ -24,6 +29,18 @@ impl<'_a> TemplateEngine for Handlebars<'_a> {
     }
 }
 
+/// Returns the "template name" for a path (eg "foo" for "/bar/foo.hbs.html").
+fn template_name_from_path(path_str: &str) -> Result<String, Error> {
+    let path = Path::new(path_str);
+
+    let full_name = path.file_name().ok_or(anyhow!(FILE_NAME_NOT_AVAILABLE_ERROR))?;
+    let full_name_str = full_name.to_str().ok_or(anyhow!(FILE_NAME_NOT_UTF8_ERROR))?;
+    let prefix_end = full_name_str.find(".").ok_or(anyhow!(FILE_NAME_MISSING_SUFFIX_ERROR))?;
+    let prefix = &full_name_str[..prefix_end];
+
+    Ok(prefix.to_string())
+}
+
 /// Registers templates with Handlebars.
 ///
 /// Registration fails if a "$TEMPLATE_NAME.hbs.html" is not available as
@@ -31,22 +48,17 @@ impl<'_a> TemplateEngine for Handlebars<'_a> {
 ///
 /// See BUILD.gn's resource("templates") rule for adding new templates as
 /// component resources.
-pub fn register_template_resources(
+pub fn register_template_resources<'a>(
     template_engine: &mut Box<dyn TemplateEngine>,
-    template_names: Vec<&str>,
+    template_paths: impl Iterator<Item = &'a str>,
 ) -> Result<(), Error> {
-    for name in template_names {
-        let path = format!("/pkg/templates/{name}.hbs.html");
-        match (*template_engine).register_resource(name, &path) {
-            Ok(_) => {
-                println!("Registered template: {name:?} : {path:?}");
-            }
-            Err(e) => {
-                println!("Problem registering template: {name:?} : {path:?} : {e:?}");
-                println!("Is template file built into component as a resource?");
-                return Err(e.into());
-            }
-        };
+    for template_path in template_paths {
+        let template_name = template_name_from_path(template_path)?;
+
+        // Register the template at given path using the file name's prefix.
+        (*template_engine).register_resource(&template_name, template_path)?;
+
+        println!("Registered: {:?} from {:?}", template_name, template_path);
     }
 
     Ok(())
@@ -55,24 +67,27 @@ pub fn register_template_resources(
 #[cfg(test)]
 mod tests {
     use crate::handlebars_utils::{
-        register_template_resources, MockTemplateEngine, TemplateEngine,
+        register_template_resources, template_name_from_path, MockTemplateEngine, TemplateEngine,
+        FILE_NAME_MISSING_SUFFIX_ERROR,
     };
     use anyhow::{anyhow, Error};
-    use mockall::predicate::{always, eq};
+    use mockall::predicate::eq;
     use mockall::Sequence;
 
-    /// Verifies registering 3 templates results in 3 register_template_file() calls.
+    const TEMPLATE_ENGINE_ERROR: &str = "Template Engine Error!";
+
+    /// Verifies provided templates result in register_resource() calls.
     #[test]
-    fn register_templates_generates_engine_calls() -> Result<(), Error> {
+    fn register_template_resources_generates_engine_calls() -> Result<(), Error> {
         let mut template_engine = MockTemplateEngine::new();
 
         // Mock a Handlebars expecting 3 template files to be registered.
         let mut call_sequence = Sequence::new();
-        let resources = vec!["1", "2", "3"];
-        for resource in &resources {
+        let resources = vec![("a", "a.suffix"), ("b", "/dir/b.suffix"), ("c", "/dir.dir/c.suffix")];
+        for (name, path) in &resources {
             template_engine
                 .expect_register_resource()
-                .with(eq(*resource), always())
+                .with(eq(*name), eq(*path))
                 .times(1)
                 .returning(|_, _| Ok(()))
                 .in_sequence(&mut call_sequence);
@@ -80,24 +95,79 @@ mod tests {
 
         let mut boxed_template_engine: Box<dyn TemplateEngine> = Box::new(template_engine);
 
-        register_template_resources(&mut boxed_template_engine, resources)
+        register_template_resources(
+            &mut boxed_template_engine,
+            vec!["a.suffix", "/dir/b.suffix", "/dir.dir/c.suffix"].into_iter(),
+        )
     }
 
-    /// Verifies register_resource() errors percolated up.
+    /// Verifies register_template_resources() fails when given a file without a suffix.
+    /// Suffixes are required to disambiguate "filenames" from "logical template names."
     #[test]
-    fn register_templates_percolates_template_error() {
+    fn register_template_resources_fails_when_passed_file_without_suffix() {
+        let template_engine = MockTemplateEngine::new();
+
+        let mut boxed_template_engine: Box<dyn TemplateEngine> = Box::new(template_engine);
+
+        // Expect error since there's no "." in the path's filename.
+        let result =
+            register_template_resources(&mut boxed_template_engine, vec!["no_suffix"].into_iter());
+
+        assert!(result.is_err(), "register_template_resources should return an error.");
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            FILE_NAME_MISSING_SUFFIX_ERROR,
+            "register_template_resources should return a FILE_NAME_MISSING_SUFFIX_ERROR",
+        );
+    }
+
+    /// Verifies register_template_resources() percolates register_resource() errors.
+    #[test]
+    fn register_template_resources_percolates_template_error() {
         let mut template_engine = MockTemplateEngine::new();
 
         // Only 1 call expected because registering stops after first failure.
         template_engine
             .expect_register_resource()
-            .with(eq("1"), always())
+            .with(eq("a"), eq("a.suffix"))
             .times(1)
-            .returning(|_, _| Err(anyhow!("Error!")));
+            .returning(|_, _| Err(anyhow!(TEMPLATE_ENGINE_ERROR)));
 
         let mut boxed_template_engine: Box<dyn TemplateEngine> = Box::new(template_engine);
 
-        // Request to register 2 resources -- but should die at first failure.
-        assert!(register_template_resources(&mut boxed_template_engine, vec!["1", "2"]).is_err());
+        // Request to register 2 resources -- but should fail at first failure.
+        let result = register_template_resources(
+            &mut boxed_template_engine,
+            vec!["a.suffix", "b.suffix"].into_iter(),
+        );
+
+        assert!(result.is_err(), "register_template_resources should return an error");
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            TEMPLATE_ENGINE_ERROR,
+            "register_template_resources should return a TEMPLATE_ENGINE_ERROR",
+        );
+    }
+
+    #[test]
+    /// Verifies template_name_from_path() handles valid paths correctly.
+    fn names_successfully_resolved_from_valid_template_paths() {
+        for (name, path) in vec![
+            ("a", "a.foo"),
+            ("b", "b.foo.bar"),
+            ("c", "c.foo.bar.baz"),
+            ("d", "/z/d.foo.bar"),
+            ("e", "/z/y/e.foo.bar"),
+        ] {
+            assert_eq!(template_name_from_path(path).expect("Valid name from path"), name);
+        }
+    }
+
+    #[test]
+    /// Verifies template_name_from_path() handles invalid paths correctly.
+    fn names_not_resolved_from_invalid_template_paths() {
+        for bad_name in vec!["", "a", "/z/b", "/z/y/c"] {
+            assert!(template_name_from_path(&bad_name).is_err());
+        }
     }
 }
--- src/recovery/diagnostics/gumshoe/src/main.rs
+++ src/recovery/diagnostics/gumshoe/src/main.rs
@@ -13,26 +13,37 @@ use crate::responder::ResponderImpl;
 use crate::webserver::{WebServer, WebServerImpl};
 use anyhow::Error;
 use futures::lock::Mutex;
+use glob::glob;
 use handlebars::Handlebars;
 use std::sync::Arc;
+use std::vec::Vec;
 
 const LISTENING_PORT: u16 = 8080;
+const TEMPLATE_GLOB_PATH: Option<&str> = Some("/pkg/templates/*.hbs.html");
 
 /// Send gumshoe on a stakeout. While on a stakeout, gumshoe responds
 /// to HTTP requests on their webserver's listening port. Returns the
-/// WebServer's run() result: a setup error (eg, if bind() fails), or
-/// (more likely) run() never returns because waits indefinitely on
-/// new connections.
+/// WebServer's run() result: a setup error (eg, if bind() or template
+/// registration fails), or (more likely) run() never returns because
+/// the component waits indefinitely on new connections.
 async fn stakeout(
     web_server: &dyn WebServer,
     mut template_engine: Box<dyn TemplateEngine>,
+    templates_glob_path: Option<&str>,
 ) -> Result<(), Error> {
-    // Populate the template engine with templates.
-    handlebars_utils::register_template_resources(
-        &mut template_engine,
-        // TODO(b/253514278): Automatically register all files under component's template/ directory.
-        vec!["404", "chrome", "index", "info"],
-    )?;
+    if let Some(templates_glob_path) = templates_glob_path {
+        // Source resources by globbing paths into utf8 strings.
+        let resources: Vec<String> = glob(templates_glob_path)?
+            .filter_map(|glob_result| glob_result.ok())
+            .filter_map(|path_buf| path_buf.to_str().map_or(None, |s| Some(s.to_string())))
+            .collect();
+
+        // Register sourced resources, exiting if any resource can't be registered.
+        handlebars_utils::register_template_resources(
+            &mut template_engine,
+            resources.iter().map(AsRef::as_ref),
+        )?;
+    }
 
     // Gather stable device data (SerialNumber, Factory Info, etc) used when rendering templates.
     let boxed_device_info = Box::new(DeviceInfoImpl::new());
@@ -50,7 +61,7 @@ async fn main() {
     let web_server_impl = WebServerImpl {};
     let template_engine = Box::new(Handlebars::new());
 
-    match stakeout(&web_server_impl, template_engine).await {
+    match stakeout(&web_server_impl, template_engine, TEMPLATE_GLOB_PATH).await {
         Ok(_) => {
             eprintln!("Stakeout completed");
         }
@@ -63,52 +74,98 @@ async fn main() {
 #[cfg(test)]
 mod tests {
     use crate::handlebars_utils::MockTemplateEngine;
-    use crate::stakeout;
+    use crate::{stakeout, TEMPLATE_GLOB_PATH};
+
     use crate::webserver::MockWebServer;
-    use anyhow::anyhow;
+    use anyhow::{anyhow, Error};
     use fuchsia_async as fasync;
+    use mockall::predicate::eq;
+    use mockall::Sequence;
 
-    /// Verifies we run webserver after initializing templates.
+    const NO_TEMPLATES: Option<&str> = None;
+    const WEBSERVER_ERROR: &str = "WebServer Error!";
+    const REGISTRATION_ERROR: &str = "Registration Error!";
+
+    /// Verifies stakeout() runs webserver.
     #[fasync::run_singlethreaded(test)]
-    async fn gumshoe_starts_webserver_after_registering_templates() {
+    async fn stakeout_starts_webserver() -> Result<(), Error> {
         let mut web_server = MockWebServer::new();
-        let mut template_engine = MockTemplateEngine::new();
 
-        // Expect 4 templates to be registered during the stakeout.
-        template_engine.expect_register_resource().times(4).returning(|_, _| Ok(()));
-
-        // Expect the webserver to be started (and simulate exiting immediately).
+        // Expect the webserver to run (and simulate exiting immediately).
         web_server.expect_run().times(1).returning(|_, _| Ok(()));
 
-        assert!(!stakeout(&web_server, Box::new(template_engine)).await.is_err());
-    }
-
-    /// Verifies a failure to register a template terminates startup (and webserver doesn't start).
-    #[fasync::run_singlethreaded(test)]
-    async fn gumshoe_quits_when_resource_registration_fails() {
-        let web_server = MockWebServer::new();
-        let mut template_engine = MockTemplateEngine::new();
-
-        // Simulate failing to register the first template.
-        template_engine
-            .expect_register_resource()
-            .times(1)
-            .returning(|_, _| Err(anyhow!("Registration Failed!")));
-
-        assert!(stakeout(&web_server, Box::new(template_engine)).await.is_err());
+        stakeout(&web_server, Box::new(MockTemplateEngine::new()), NO_TEMPLATES).await
     }
 
     /// Verifies WebServer.run() errors are percolated up from stakeout().
     #[fasync::run_singlethreaded(test)]
     async fn stakeout_percolates_webserver_error() {
         let mut web_server = MockWebServer::new();
+
+        // Expect the webserver to run (and simulate exiting immediately with error).
+        web_server.expect_run().times(1).returning(|_, _| Err(anyhow!(WEBSERVER_ERROR)));
+
+        let result = stakeout(&web_server, Box::new(MockTemplateEngine::new()), NO_TEMPLATES).await;
+
+        assert!(result.is_err(), "stakeout should return an error");
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            WEBSERVER_ERROR,
+            "stakeout should return a WEBSERVER_ERROR"
+        );
+    }
+
+    /// Verifies stakeout() registers all files from resource("templates").
+    #[fasync::run_singlethreaded(test)]
+    async fn stakeout_registers_templates() -> Result<(), Error> {
+        let mut web_server = MockWebServer::new();
         let mut template_engine = MockTemplateEngine::new();
 
-        template_engine.expect_register_resource().times(4).returning(|_, _| Ok(()));
+        // Expect templates to be registered in alphanumeric order.
+        let resources = vec![
+            ("404", "/pkg/templates/404.hbs.html"),
+            ("chrome", "/pkg/templates/chrome.hbs.html"),
+            ("index", "/pkg/templates/index.hbs.html"),
+            ("info", "/pkg/templates/info.hbs.html"),
+        ];
+        let mut call_sequence = Sequence::new();
+        for (name, path) in &resources {
+            template_engine
+                .expect_register_resource()
+                .with(eq(*name), eq(*path))
+                .times(1)
+                .returning(|_, _| Ok(()))
+                .in_sequence(&mut call_sequence);
+        }
 
-        // Expect the webserver to be started (and simulate exiting immediately with error).
-        web_server.expect_run().times(1).returning(|_, _| Err(anyhow!("WebServer Error!")));
+        // Expect the webserver to be started (and simulate exiting immediately).
+        web_server.expect_run().times(1).returning(|_, _| Ok(()));
 
-        assert!(stakeout(&web_server, Box::new(template_engine)).await.is_err());
+        stakeout(&web_server, Box::new(template_engine), TEMPLATE_GLOB_PATH).await
+    }
+
+    /// Verifies stakeout percolates template registration errors.
+    #[fasync::run_singlethreaded(test)]
+    async fn stakeout_percolates_template_registration_error() {
+        // Template initialization error prevents Web Server run() call.
+        let web_server = MockWebServer::new();
+
+        let mut template_engine = MockTemplateEngine::new();
+
+        // Verify stakeout() fails when first resource fails to register.
+        template_engine
+            .expect_register_resource()
+            .with(eq("404"), eq("/pkg/templates/404.hbs.html"))
+            .times(1)
+            .returning(|_, _| Err(anyhow!(REGISTRATION_ERROR)));
+
+        let result = stakeout(&web_server, Box::new(template_engine), TEMPLATE_GLOB_PATH).await;
+
+        assert!(result.is_err(), "stakeout should return an error");
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            REGISTRATION_ERROR,
+            "stakeout should return a REGISTRATION_ERROR"
+        );
     }
 }
