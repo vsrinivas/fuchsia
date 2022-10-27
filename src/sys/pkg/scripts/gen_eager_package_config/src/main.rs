0897e52cdf6c3620ac15e14c4dea36a3ed1c08b2
James Buckland <jbuckland@google.com>
2022-10-27 18:02:13 +0000

    [pkg/scripts] Enforce clippy lints.
    Change-Id: Ic935275748e1f44cb78e48a696f169393d97f175
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/748468
    Commit-Queue: James Buckland <jbuckland@google.com>
    Reviewed-by: John Wittrock <wittrock@google.com>

--- src/sys/pkg/scripts/gen_eager_package_config/src/main.rs
+++ src/sys/pkg/scripts/gen_eager_package_config/src/main.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![warn(clippy::all)]
+
 use {
     lib::{
         generate_omaha_client_config, generate_pkg_resolver_config, Args, InputConfig,
