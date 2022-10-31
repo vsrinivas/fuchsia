ce22348113c345e7136ff4e812b0874cc1a05e1b
Simon Shields <simonshields@google.com>
2022-10-31 01:42:27 +0000

    [gigaboot] Synthesise EFI memory attributes table.
    On some systems, the EFI memory attributes table does not accurately
    reflect all of the regions the EFI runtime services actually need.
    
    For instance, on NUC11 the GetTime() call which we use to test EFI
    relies on a MMIO region which is not included in the memory attributes
    table.
    
    This CL makes gigaboot synthesise an EFI memory attributes table by
    combining the memory map returned by GetMemoryMap() and the memory
    attributes table.
    
    Bug: 113304
    Test: k ut efi on NUC11.
    Change-Id: I39b3d8810e6edbe5dea81299cf684cab7223650f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/748288
    Reviewed-by: David Pursell <dpursell@google.com>
    Commit-Queue: Simon Shields <simonshields@google.com>

--- src/firmware/gigaboot/src/zircon.c
+++ src/firmware/gigaboot/src/zircon.c
@@ -7,6 +7,7 @@
 #include <lib/ddk/platform-defs.h>
 #include <lib/zbi/zbi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <xefi.h>
 #include <zircon/boot/driver-config.h>
@@ -14,8 +15,10 @@
 #include <zircon/limits.h>
 #include <zircon/pixelformat.h>
 
+#include <efi/boot-services.h>
 #include <efi/protocol/graphics-output.h>
 #include <efi/runtime-services.h>
+#include <efi/types.h>
 
 #include "acpi.h"
 #include "osboot.h"
@@ -60,7 +63,140 @@ uint32_t to_mem_range_type(uint32_t efi_mem_type) {
   return ZBI_MEM_RANGE_RESERVED;
 }
 
-static unsigned char scratch[32768];
+static unsigned char scratch[32768] __attribute__((aligned(8)));
+
+// Tries to generate an EFI memory attributes table ZBI item and insert it in the given ramdisk.
+static efi_status generate_efi_memory_attributes_table_item(void* ramdisk,
+                                                            const size_t ramdisk_size,
+                                                            efi_system_table* sys, const void* mmap,
+                                                            size_t memory_map_size, size_t dsize) {
+  const efi_memory_attributes_table_header* efi_provided_table = NULL;
+
+  const efi_guid kMemoryAttributesGuid = EFI_MEMORY_ATTRIBUTES_GUID;
+  for (size_t i = 0; i < sys->NumberOfTableEntries; i++) {
+    if (!memcmp(&kMemoryAttributesGuid, &sys->ConfigurationTable[i].VendorGuid, sizeof(efi_guid))) {
+      efi_provided_table = sys->ConfigurationTable[i].VendorTable;
+      break;
+    }
+  }
+
+  efi_memory_attributes_table_header* generated_header;
+  uint32_t max_payload_length;
+
+  zbi_result_t zbi_result = zbi_get_next_entry_payload(
+      ramdisk, ramdisk_size, (void**)&generated_header, &max_payload_length);
+  if (zbi_result != ZBI_RESULT_OK) {
+    printf("Failed to create next entry payload: 0x%x", zbi_result);
+    return EFI_ABORTED;
+  }
+
+  const size_t max_items = (max_payload_length - sizeof(efi_memory_attributes_table_header)) /
+                           sizeof(efi_memory_descriptor);
+
+  efi_memory_descriptor* generated_items = (efi_memory_descriptor*)&generated_header[1];
+
+  size_t items = memory_map_size / dsize;
+
+  size_t item_count = 0;
+  for (size_t i = 0; i < items; i++) {
+    efi_memory_descriptor* item = (efi_memory_descriptor*)&mmap[dsize * i];
+    if (item->Attribute & EFI_MEMORY_RUNTIME) {
+      // Remove any ranges that are handled by the actual EFI memory attributes table.
+      size_t base = item->PhysicalStart;
+      size_t size = item->NumberOfPages * PAGE_SIZE;
+
+      for (size_t j = 0;
+           efi_provided_table != NULL && j < efi_provided_table->number_of_entries && size != 0;
+           j++) {
+        // This EMAT entry is either a sub-region or a full copy of the memory map region, per
+        // EFI 2.10 4.6.4: "Additionally, every memory region described by a Descriptor in
+        // EFI_MEMORY_ATTRIBUTES_TABLE must be a sub-region of, or equal to, a descriptor in the
+        // table produced by GetMemoryMap()."
+        //
+        // This means that we do not have to consider the case where the EMAT entry only overlaps
+        // the end of the memory map entry.
+
+        efi_memory_descriptor* emat_item =
+            (efi_memory_descriptor*)(((uint8_t*)&efi_provided_table[1]) +
+                                     (j * efi_provided_table->descriptor_size));
+        if (emat_item->PhysicalStart < base) {
+          continue;
+        }
+
+        // EMAT items are ordered by physical address, so once we go past |base| we can quit the
+        // loop.
+        if (emat_item->PhysicalStart >= (base + size)) {
+          break;
+        }
+
+        if (emat_item->PhysicalStart > base) {
+          // Create a region for [base ... emat_item->PhysicalStart), because that region is not
+          // covered by the EMAT.
+          generated_items[item_count].PhysicalStart = base;
+          generated_items[item_count].NumberOfPages = (emat_item->PhysicalStart - base) / PAGE_SIZE;
+          generated_items[item_count].VirtualStart = 0;
+          generated_items[item_count].Attribute = EFI_MEMORY_RUNTIME;
+          generated_items[item_count].Type = item->Type;
+
+          // Adjust base and size forward.
+          size -= emat_item->PhysicalStart - base;
+          base = emat_item->PhysicalStart;
+
+          item_count++;
+          if (item_count >= max_items) {
+            printf("ran out of zbi scratch space!\n");
+            return EFI_OUT_OF_RESOURCES;
+          }
+        }
+
+        if (emat_item->PhysicalStart == base) {
+          // Create a region for [base ... emat_item->NumberOfPages * PAGE_SIZE)
+          generated_items[item_count] = *emat_item;
+
+          // Adjust base and size forward.
+          base += emat_item->NumberOfPages * PAGE_SIZE;
+          size -= emat_item->NumberOfPages * PAGE_SIZE;
+          item_count++;
+          if (item_count >= max_items) {
+            printf("ran out of zbi scratch space!\n");
+            return EFI_OUT_OF_RESOURCES;
+          }
+        }
+      }
+
+      if (size != 0) {
+        // Create a region for the remaining space represented by this mmap region.
+        generated_items[item_count].PhysicalStart = base;
+        generated_items[item_count].NumberOfPages = size / PAGE_SIZE;
+        generated_items[item_count].VirtualStart = 0;
+        generated_items[item_count].Attribute = EFI_MEMORY_RUNTIME;
+        generated_items[item_count].Type = item->Type;
+        item_count++;
+        if (item_count >= max_items) {
+          printf("ran out of scratch space!\n");
+          return EFI_OUT_OF_RESOURCES;
+        }
+      }
+    }
+  }
+
+  generated_header->descriptor_size = sizeof(efi_memory_descriptor);
+  generated_header->number_of_entries = item_count;
+  generated_header->reserved = 0;
+  generated_header->version = 1;
+
+  zbi_result_t result = zbi_create_entry_with_payload(
+      ramdisk, ramdisk_size, ZBI_TYPE_EFI_MEMORY_ATTRIBUTES_TABLE, 0, 0, generated_header,
+      sizeof(*generated_header) + (item_count * sizeof(efi_memory_descriptor)));
+  if (result != ZBI_RESULT_OK) {
+    printf(
+        "warning: failed to create EFI memory attributes ZBI item."
+        " EFI runtime services won't work.\n");
+    return EFI_INVALID_PARAMETER;
+  }
+
+  return EFI_SUCCESS;
+}
 
 static void start_zircon(uint64_t entry, void* bootdata) {
 #if __x86_64__
@@ -414,29 +550,12 @@ int boot_zircon(efi_handle img, efi_system_table* sys, void* image, size_t isz, 
     }
   }
 
-  // Look for an EFI memory attributes table we can pass to the kernel.
-  const efi_guid kMemoryAttributesGuid = EFI_MEMORY_ATTRIBUTES_GUID;
-  for (size_t i = 0; i < sys->NumberOfTableEntries; i++) {
-    if (!memcmp(&kMemoryAttributesGuid, &sys->ConfigurationTable[i].VendorGuid, sizeof(efi_guid))) {
-      const efi_memory_attributes_table_header* hdr = sys->ConfigurationTable[i].VendorTable;
-      result = zbi_create_entry_with_payload(
-          ramdisk, rsz, ZBI_TYPE_EFI_MEMORY_ATTRIBUTES_TABLE, 0, 0, hdr,
-          sizeof(*hdr) + (hdr->number_of_entries * hdr->descriptor_size));
-      if (result != ZBI_RESULT_OK) {
-        printf(
-            "warning: failed to create EFI memory attributes ZBI item. EFI runtime services won't "
-            "work.\n");
-      }
-    }
-  }
-
   add_staged_zbi_files(ramdisk, rsz);
 
   printf("copying kernel image from %p to %p size %zu, entry at %p\n", image,
          (void*)kernel_zone_base, isz, (void*)entry);
   memcpy((void*)kernel_zone_base, image, isz);
 
-  // Obtain the system memory map
   size_t msize, dsize;
   for (int attempts = 0;; attempts++) {
     uint32_t dversion = 0;
@@ -447,6 +566,14 @@ int boot_zircon(efi_handle img, efi_system_table* sys, void* image, size_t isz, 
                                                    &dsize, &dversion);
     if (r != EFI_SUCCESS) {
       printf("boot: cannot GetMemoryMap()\n");
+      goto fail;
+    }
+
+    // Look for an EFI memory attributes table we can pass to the kernel.
+    r = generate_efi_memory_attributes_table_item(ramdisk, rsz, sys, scratch, msize, dsize);
+    if (result != EFI_SUCCESS) {
+      printf("failed to generate EFI memory attributes table: %s", xefi_strerror(r));
+
       goto fail;
     }
 
