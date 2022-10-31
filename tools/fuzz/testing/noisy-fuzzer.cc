cc4ac4b360b4ea7b35081f287b7dc5f616e724ca
Aaron Green <aarongreen@google.com>
2022-10-31 21:12:44 +0000

    [tools][fuzz] Add args to noisy-fuzzer
    This CL adds support to noisy-fuzzer for arguments of the form:
    --[no-]{stdout|stderr|syslog}. These can be set in
    meta/noisy_fuzzer.cml to provide greater flexibility while testing.
    The default remains `--stdout --syslog`.
    
    Change-Id: I4de0e187730bca3b750fca8f8497e81ec966d46d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/749253
    Commit-Queue: Aaron Green <aarongreen@google.com>
    Reviewed-by: Cameron Finucane <eep@google.com>

--- tools/fuzz/testing/noisy-fuzzer.cc
+++ tools/fuzz/testing/noisy-fuzzer.cc
@@ -11,6 +11,62 @@
 #include <iostream>
 #include <sstream>
 
+enum : uint8_t {
+  kStdout = (1 << 0),
+  kStderr = (1 << 1),
+  kSyslog = (1 << 2),
+};
+
+uint8_t gFlags = 0;
+
+// Set args via meta/noisy_fuzzer.cml, e.g.
+//
+// {
+//   include = [ ... ]
+//   args = [
+//     "test/noisy_fuzzer",
+//     <options>
+//   ]
+// }
+//
+// Options:
+//   --[no-]stdout   Whether to emit stdout noise (on by default).
+//   --[no-]stderr   Whether to emit stderr noise (off by default).
+//   --[no-]syslog   Whether to emit syslog noise (on by default).
+//
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
+  char **src = *argv;
+  char **dst = src;
+  int m = 0;
+  int n = *argc;
+  gFlags = kStdout | kSyslog;
+  for (int i = 0; i < n; ++i) {
+    if (strcmp(*src, "--stdout") == 0) {
+      gFlags |= kStdout;
+    } else if (strcmp(*src, "--no-stdout") == 0) {
+      gFlags &= ~kStdout;
+    } else if (strcmp(*src, "--stderr") == 0) {
+      gFlags |= kStderr;
+    } else if (strcmp(*src, "--no-stderr") == 0) {
+      gFlags &= ~kStderr;
+    } else if (strcmp(*src, "--syslog") == 0) {
+      gFlags |= kSyslog;
+    } else if (strcmp(*src, "--no-syslog") == 0) {
+      gFlags &= ~kSyslog;
+    } else {
+      // Pass through remaining arguments, e.g. libFuzzer flags.
+      if (*src != *dst) {
+        *dst = *src;
+      }
+      ++dst;
+      ++m;
+    }
+    ++src;
+  }
+  *argc = m;
+  return 0;
+}
+
 // A simple fuzzer that emits a lot of noise to stdout and syslog.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   zx::nanosleep(zx::deadline_after(zx::msec(1)));
@@ -20,7 +76,14 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     oss << std::setw(2) << std::setfill('0') << size_t(data[i]);
   }
   auto hex = oss.str();
-  std::cout << "stdout-noise: " << hex << std::endl;
-  FX_LOGS(INFO) << "syslog-noise: " << hex;
+  if (gFlags & kStdout) {
+    std::cout << "stdout-noise: " << hex << std::endl;
+  }
+  if (gFlags & kStderr) {
+    std::cerr << "stderr-noise: " << hex << std::endl;
+  }
+  if (gFlags & kSyslog) {
+    FX_LOGS(INFO) << "syslog-noise: " << hex;
+  }
   return 0;
 }
