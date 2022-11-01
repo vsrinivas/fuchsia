22d4d4c0901a1a0d144d6326cf9c24a2f2ed2140
Dustin Green <dustingreen@google.com>
2022-11-01 10:56:42 +0000

    [panic] Remove \n at end of ZX_PANIC() strings (12)
    After fxr/522940:
      * The string will output without the \n (regardless of context,
        assuming no unknown bugs).
      * A \n is not necessary to avoid run-on output (more stuff on same
        line).
    
    So now we can remove \n at the end of of any ZX_PANIC() strings that
    had \n.  Most didn't have the \n before.
    
    Change-Id: I1aba9506d1804850716ed9f5fa22a9bb4d49bf4b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/748618
    Commit-Queue: Dustin Green <dustingreen@google.com>
    Reviewed-by: Ali Saeed <saeedali@google.com>

--- third_party/pigweed/backends/pw_assert/public_overrides/pw_assert_backend/check_backend.h
+++ third_party/pigweed/backends/pw_assert/public_overrides/pw_assert_backend/check_backend.h
@@ -10,12 +10,12 @@
 #define PW_HANDLE_CRASH(fmt, ...) ZX_PANIC(fmt, ##__VA_ARGS__)
 
 #define PW_HANDLE_ASSERT_FAILURE(x, msg, ...) \
-  ZX_PANIC("ASSERT FAILED at (%s:%d): %s\n" msg "\n", __FILE__, __LINE__, #x, ##__VA_ARGS__)
+  ZX_PANIC("ASSERT FAILED at (%s:%d): %s\n" msg, __FILE__, __LINE__, #x, ##__VA_ARGS__)
 
 #define PW_HANDLE_ASSERT_BINARY_COMPARE_FAILURE(arg_a_str, arg_a_val, comparison_op_str,  \
                                                 arg_b_str, arg_b_val, type_fmt, msg, ...) \
   ZX_PANIC("ASSERT FAILED at (%s:%d): " arg_a_str " (=" type_fmt ") " comparison_op_str   \
-           " " arg_b_str " (=" type_fmt ") \n" msg "\n",                                  \
+           " " arg_b_str " (=" type_fmt ") \n" msg,                                  \
            __FILE__, __LINE__, arg_a_val, arg_b_val, ##__VA_ARGS__)
 
 #endif  // THIRD_PARTY_PIGWEED_BACKENDS_PW_ASSERT_PUBLIC_OVERRIDES_PW_ASSERT_BACKEND_CHECK_BACKEND_H_
