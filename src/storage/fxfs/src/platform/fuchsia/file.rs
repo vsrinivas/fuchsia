403e99e4e17bac5fbeb8233bdf19322cd5d36830
Chris Suter <csuter@google.com>
2022-11-01 21:03:31 +0000

    [fxfs] Disable test whilst we work on a proper fix
    Bug: 113879
    Change-Id: I6c3c1d0068b62f104978f1f46cdea7db0a26ca20
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/751582
    Reviewed-by: Ghanan Gowripalan <ghanan@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>

--- src/storage/fxfs/src/platform/fuchsia/file.rs
+++ src/storage/fxfs/src/platform/fuchsia/file.rs
@@ -1217,6 +1217,7 @@ mod tests {
     }
 
     #[fasync::run(10, test)]
+    #[ignore]
     async fn test_unlink_with_open_race() {
         let fixture = Arc::new(TestFixture::new().await);
         let fixture1 = fixture.clone();
