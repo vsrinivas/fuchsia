62e3fdc2c10290c34c6c7cda00972a1a304a6268
Kevin Lindkvist <lindkvist@google.com>
2022-11-10 19:08:02 +0000

    [starnix] Add /dev/fd
    Change-Id: I3d88be0c43a3762ebccbbcfe8a13ef1040d83d40
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/757643
    Reviewed-by: Theodore Dubois <tbodt@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Kevin Lindkvist <lindkvist@google.com>

--- src/proc/bin/starnix/fs/devtmpfs.rs
+++ src/proc/bin/starnix/fs/devtmpfs.rs
@@ -31,6 +31,7 @@ fn init_devtmpfs(current_task: &CurrentTask) -> FileSystemHandle {
     mkchr(b"full", DeviceType::FULL);
     mkchr(b"random", DeviceType::RANDOM);
     mkchr(b"urandom", DeviceType::URANDOM);
+    root.create_symlink(current_task, b"fd", b"/proc/self/fd", FsCred::root()).unwrap();
 
     mkdir(b"shm");
 
