3ce31609c6cce8008b0a97d3236c4c1aeae7966d
Filip Filmar <fmil@google.com>
2022-11-11 02:15:49 +0000

    [tcti] Cleans up the tpm context
    Attempts to resolve an ASAN complaint about a memory leak in
    the object returned by `fuchsia_tpm_init()`.
    
    Bug: 114924
    Change-Id: Ie648246dc60f04b695fe6125f13b5ae19b702fee
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/758123
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Laura Peskin <pesk@google.com>
    Fuchsia-Auto-Submit: Filip Filmar <fmil@google.com>

--- src/security/lib/fuchsia-tcti/src/fuchsia-tcti-unittests.cc
+++ src/security/lib/fuchsia-tcti/src/fuchsia-tcti-unittests.cc
@@ -4,32 +4,35 @@
 
 #include <fidl/fuchsia.tpm/cpp/wire.h>
 
+#include <memory>
+
 #include <gtest/gtest.h>
 #include <src/security/lib/fuchsia-tcti/include/fuchsia-tcti.h>
 
 TEST(FuchsiaTctiTests, TpmSendArgumentValidation) {
-  opaque_ctx_t* context = fuchsia_tpm_init();
+  std::unique_ptr<opaque_ctx_t> context = fuchsia_tpm_init();
   std::vector<uint8_t> command(20, 0);
   int result = fuchsia_tpm_send(nullptr, 1, command.data(), command.size());
   ASSERT_NE(result, 0);
-  result = fuchsia_tpm_send(context, 1, nullptr, command.size());
+  result = fuchsia_tpm_send(context.get(), 1, nullptr, command.size());
   ASSERT_NE(result, 0);
-  result = fuchsia_tpm_send(context, 1, command.data(), 0);
+  result = fuchsia_tpm_send(context.get(), 1, command.data(), 0);
   ASSERT_NE(result, 0);
-  result = fuchsia_tpm_send(context, 1, command.data(), fuchsia_tpm::wire::kMaxCommandLen + 1);
+  result = fuchsia_tpm_send(
+    context.get(), 1, command.data(), fuchsia_tpm::wire::kMaxCommandLen + 1);
   ASSERT_NE(result, 0);
 }
 
 TEST(FuchsiaTctiTests, TpmRecvArgumentValidation) {
-  opaque_ctx_t* context = fuchsia_tpm_init();
+  std::unique_ptr<opaque_ctx_t> context = fuchsia_tpm_init();
   std::vector<uint8_t> command(20, 0);
   size_t no_bytes = 0;
-  size_t bytes_read = fuchsia_tpm_recv(context, command.data(), command.size());
+  size_t bytes_read = fuchsia_tpm_recv(context.get(), command.data(), command.size());
   ASSERT_EQ(bytes_read, no_bytes);
   bytes_read = fuchsia_tpm_recv(nullptr, command.data(), command.size());
   ASSERT_EQ(bytes_read, no_bytes);
-  bytes_read = fuchsia_tpm_recv(context, nullptr, command.size());
+  bytes_read = fuchsia_tpm_recv(context.get(), nullptr, command.size());
   ASSERT_EQ(bytes_read, no_bytes);
-  bytes_read = fuchsia_tpm_recv(context, command.data(), 0);
+  bytes_read = fuchsia_tpm_recv(context.get(), command.data(), 0);
   ASSERT_EQ(bytes_read, no_bytes);
 }
