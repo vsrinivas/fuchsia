c6d5471e819d5ab501b65a253519a30ce64eada5
Michael Brunson <mbrunson@google.com>
2022-11-15 03:28:03 +0000

    [recovery] Wrap text in the console view.
    When text is too long in the console view, it is rendered off screen.
    In order to prevent this, this CL splits each line based on the console
    view width when it is available.
    
    Bug: b/255589478
    Test: Before: http://shortn/_SAX4WuC0Lx
          After: http://shortn/_IHa1yZjZBu
    Change-Id: I774755ce610b45ba4289131ceec3abaa62dd2dfd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/753225
    Commit-Queue: Michael Brunson <mbrunson@google.com>
    Reviewed-by: Kevin Twidle <kpt@google.com>

--- src/recovery/lib/recovery-ui/src/console.rs
+++ src/recovery/lib/recovery-ui/src/console.rs
@@ -5,7 +5,7 @@
 use anyhow::Error;
 use carnelian::{
     color::Color,
-    drawing::FontFace,
+    drawing::{linebreak_text, FontFace},
     render::Context as RenderContext,
     scene::{
         facets::{TextFacetOptions, TextHorizontalAlignment, TextVerticalAlignment},
@@ -16,9 +16,11 @@ use carnelian::{
 };
 use fuchsia_zircon::Event;
 
+const TEXT_FONT_SIZE: f32 = 24.0;
 const MAX_CONSOLE_LINE_COUNT: usize = 20;
 const CONSOLE_WELCOME_MESSAGE: &str = "Welcome. Tap top-left to exit debug console.";
 
+#[derive(Debug)]
 pub enum ConsoleMessages {
     // Appends provided text to the Console output.
     AddText(String),
@@ -35,6 +37,8 @@ pub struct ConsoleViewAssistant {
     scene_details: Option<SceneDetails>,
     // Console output lines.
     lines: Vec<String>,
+    // Width of the area where the view will be rendered.
+    layout_width: Option<f32>,
 }
 
 impl ConsoleViewAssistant {
@@ -46,13 +50,15 @@ impl ConsoleViewAssistant {
         }
         lines.push(CONSOLE_WELCOME_MESSAGE.to_string());
 
-        Ok(ConsoleViewAssistant { font_face, scene_details: None, lines })
+        Ok(ConsoleViewAssistant { font_face, scene_details: None, lines, layout_width: None })
     }
 
     // Returns cached SceneDetails if available, otherwise builds one from scratch and caches result.
     pub fn console_scene(&mut self, context: &ViewAssistantContext) -> SceneDetails {
         let scene_details = self.scene_details.take().unwrap_or_else(|| {
             let target_size = context.size;
+            self.layout_width = Some(target_size.width.into());
+
             let mut builder = SceneBuilder::new().background_color(Color::new());
             builder
                 .group()
@@ -71,7 +77,7 @@ impl ConsoleViewAssistant {
                     builder.text(
                         self.font_face.clone(),
                         &self.lines.join("\n"),
-                        24.0,
+                        TEXT_FONT_SIZE,
                         Point::zero(),
                         TextFacetOptions {
                             horizontal_alignment: TextHorizontalAlignment::Left,
@@ -126,6 +132,21 @@ impl ViewAssistant for ConsoleViewAssistant {
                         self.lines.push(line.to_string());
                     }
 
+                    if let Some(max_width) = self.layout_width {
+                        let mut wrapped_lines = Vec::new();
+
+                        for line in &self.lines {
+                            wrapped_lines.extend(linebreak_text(
+                                &self.font_face,
+                                TEXT_FONT_SIZE,
+                                &line,
+                                max_width,
+                            ));
+                        }
+
+                        self.lines = wrapped_lines;
+                    }
+
                     if self.lines.len() > MAX_CONSOLE_LINE_COUNT {
                         self.lines.drain(0..self.lines.len() - MAX_CONSOLE_LINE_COUNT);
                     }
@@ -148,6 +169,9 @@ mod tests {
     const SMALL_MULTILINE_TEST_MESSAGE: &str = "1\n2\n3\n4\n5";
     const GIANT_MULTILINE_TEST_MESSAGE: &str =
         "1\n2\n3\n4\n5\n6\n7\n8\n9\nA\nB\nC\nD\nE\nF\nG\nH\nI\nJ\nK\nL\nM\nN\nO\nP\nQ\nR\nS\nT";
+    const LONG_TEST_MESSAGE: &str =
+        "this is a very long message that will get split into multiple lines because the screen is \
+        not wide enough to hold it";
 
     #[test]
     fn test_add_text_message_modifies_lines() -> std::result::Result<(), anyhow::Error> {
@@ -214,6 +238,99 @@ mod tests {
             let actual = &console_view_assistant.lines
                 [console_view_assistant.lines.len() - expected_lines.len() + i];
             assert_eq!(expected, actual, "Multiline message not split as expected");
+        }
+
+        // Verify our overall line count still "fills the screen."
+        assert_eq!(console_view_assistant.lines.len(), MAX_CONSOLE_LINE_COUNT);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_long_messages_get_split() -> std::result::Result<(), anyhow::Error> {
+        let font_face = font::load_default_font_face()?;
+        let max_width = 100f32;
+        let expected_lines =
+            linebreak_text(&font_face, TEXT_FONT_SIZE, LONG_TEST_MESSAGE, max_width);
+        assert!(expected_lines.len() > 1);
+
+        let mut console_view_assistant = ConsoleViewAssistant::new(font_face).unwrap();
+        console_view_assistant.layout_width = Some(max_width);
+        console_view_assistant
+            .handle_message(make_message(ConsoleMessages::AddText(LONG_TEST_MESSAGE.to_string())));
+
+        // Verify the lines in our Console are split into multiple lines.
+        for (i, expected) in expected_lines.iter().enumerate() {
+            let actual = &console_view_assistant.lines
+                [console_view_assistant.lines.len() - expected_lines.len() + i];
+            assert_eq!(expected, actual, "Message not split as expected");
+        }
+
+        // Verify our overall line count still "fills the screen."
+        assert_eq!(console_view_assistant.lines.len(), MAX_CONSOLE_LINE_COUNT);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_long_messages_get_split_after_multiple_messages(
+    ) -> std::result::Result<(), anyhow::Error> {
+        let font_face = font::load_default_font_face()?;
+        let max_width = 300f32;
+        let mut expected_lines =
+            linebreak_text(&font_face, TEXT_FONT_SIZE, LONG_TEST_MESSAGE, max_width);
+
+        // Need two sets of lines for the two messages.
+        expected_lines.extend(expected_lines.clone());
+        assert!(expected_lines.len() > 1);
+
+        let mut console_view_assistant = ConsoleViewAssistant::new(font_face).unwrap();
+        console_view_assistant.layout_width = Some(max_width);
+        console_view_assistant
+            .handle_message(make_message(ConsoleMessages::AddText(LONG_TEST_MESSAGE.to_string())));
+
+        // A second message should still be split like the first.
+        console_view_assistant
+            .handle_message(make_message(ConsoleMessages::AddText(LONG_TEST_MESSAGE.to_string())));
+
+        // Verify the lines in our Console are split into multiple lines.
+        for (i, expected) in expected_lines.iter().enumerate() {
+            let actual = &console_view_assistant.lines
+                [console_view_assistant.lines.len() - expected_lines.len() + i];
+            assert_eq!(expected, actual, "Message not split as expected");
+        }
+
+        // Verify our overall line count still "fills the screen."
+        assert_eq!(console_view_assistant.lines.len(), MAX_CONSOLE_LINE_COUNT);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_long_paragraphs_get_split_preserving_paragraph_breaks(
+    ) -> std::result::Result<(), anyhow::Error> {
+        let font_face = font::load_default_font_face()?;
+        let paragraphs = format!("{msg}\n{msg}\n{msg}", msg = LONG_TEST_MESSAGE);
+
+        let max_width = 300f32;
+        let lines = linebreak_text(&font_face, TEXT_FONT_SIZE, LONG_TEST_MESSAGE, max_width);
+        let mut expected_lines = Vec::new();
+
+        // Create three paragraphs to match `paragraphs`.
+        expected_lines.extend(lines.clone());
+        expected_lines.extend(lines.clone());
+        expected_lines.extend(lines);
+        assert!(expected_lines.len() > 1);
+
+        let mut console_view_assistant = ConsoleViewAssistant::new(font_face).unwrap();
+        console_view_assistant.layout_width = Some(max_width);
+        console_view_assistant.handle_message(make_message(ConsoleMessages::AddText(paragraphs)));
+
+        // Verify the lines in our Console are split into multiple lines.
+        for (i, expected) in expected_lines.iter().enumerate() {
+            let actual = &console_view_assistant.lines
+                [console_view_assistant.lines.len() - expected_lines.len() + i];
+            assert_eq!(expected, actual, "Message not split as expected");
         }
 
         // Verify our overall line count still "fills the screen."
