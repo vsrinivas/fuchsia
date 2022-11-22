1d3a296a961126462412e26cb2cca6e89e5381f0
David Gilhooley <dgilhooley@google.com>
2022-11-22 18:30:32 +0000

    [hidctl] Migrate to mock-ddk
    Fixed: 115093
    Multiply: hidctl-test-package
    Change-Id: I8496f0b3f6357bae9f27986b738273017d9b2e65
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/764167
    Reviewed-by: Ian Fisher <iafisher@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Commit-Queue: David Gilhooley <dgilhooley@google.com>

--- src/ui/input/drivers/hidctl/hidctl-test.cc
+++ src/ui/input/drivers/hidctl/hidctl-test.cc
@@ -4,47 +4,58 @@
 
 #include "hidctl.h"
 
-#include <lib/fake_ddk/fake_ddk.h>
-
 #include <zxtest/zxtest.h>
+
+#include "src/devices/testing/mock-ddk/mock-device.h"
 
 namespace hidctl {
 
 TEST(HidctlTest, DdkLifecycle) {
-  fake_ddk::Bind ddk;
-
+  auto fake_parent = MockDevice::FakeRootParent();
   zx::socket local, remote;
   ASSERT_OK(zx::socket::create(ZX_SOCKET_DATAGRAM, &local, &remote));
   const fuchsia_hardware_hidctl::wire::HidCtlConfig config = {};
   fbl::Array<const uint8_t> report_desc;
 
-  auto hiddev = std::unique_ptr<hidctl::HidDevice>(new hidctl::HidDevice(
-      fake_ddk::kFakeParent, config, std::move(report_desc), std::move(local)));
+  auto hiddev = std::unique_ptr<hidctl::HidDevice>(
+      new hidctl::HidDevice(fake_parent.get(), config, std::move(report_desc), std::move(local)));
   ASSERT_OK(hiddev->DdkAdd("hidctl-dev"));
+  hiddev.release();
 
-  hiddev->DdkAsyncRemove();
-  ASSERT_OK(ddk.WaitUntilRemove());
-  ASSERT_TRUE(ddk.Ok());
+  auto* child = fake_parent->GetLatestChild();
+  child->InitOp();
+  child->WaitUntilInitReplyCalled();
+
+  child->UnbindOp();
+  child->WaitUntilUnbindReplyCalled();
+  mock_ddk::ReleaseFlaggedDevices(fake_parent.get());
 }
 
 // Tests that the device is removed if the worker thread exits on error.
 TEST(HidctlTest, DdkLifecycleWorkerThreadExit) {
-  fake_ddk::Bind ddk;
+  auto fake_parent = MockDevice::FakeRootParent();
 
   zx::socket local, remote;
   ASSERT_OK(zx::socket::create(ZX_SOCKET_DATAGRAM, &local, &remote));
   const fuchsia_hardware_hidctl::wire::HidCtlConfig config = {};
   fbl::Array<const uint8_t> report_desc;
 
-  auto hiddev = std::unique_ptr<hidctl::HidDevice>(new hidctl::HidDevice(
-      fake_ddk::kFakeParent, config, std::move(report_desc), std::move(local)));
+  auto hiddev = std::unique_ptr<hidctl::HidDevice>(
+      new hidctl::HidDevice(fake_parent.get(), config, std::move(report_desc), std::move(local)));
   ASSERT_OK(hiddev->DdkAdd("hidctl-dev"));
+  hiddev.release();
+
+  auto* child = fake_parent->GetLatestChild();
+  child->InitOp();
+  child->WaitUntilInitReplyCalled();
 
   // This should cause the worker thread to exit and call DdkAsyncRemove() on the device.
   remote.reset();
 
-  ASSERT_OK(ddk.WaitUntilRemove());
-  ASSERT_TRUE(ddk.Ok());
+  child->WaitUntilAsyncRemoveCalled();
+  child->UnbindOp();
+  child->WaitUntilUnbindReplyCalled();
+  mock_ddk::ReleaseFlaggedDevices(fake_parent.get());
 }
 
 }  // namespace hidctl
